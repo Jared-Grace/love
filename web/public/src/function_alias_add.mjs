843cfb345a6b7a389254621ada1459b2d88cb6b8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:12:05 -0400

    function_copy object_property_exists object_property_get

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -2,7 +2,8 @@ import { file_read } from "./file_read.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export async function function_alias_add(alias, f_name) {
   let data = await file_read("data.json");
-  if (object_property_exists(data, "alias")) {
+  let alias=object_property_exists(data, "alias")
+  if (object_property_exists(alias, alias)) {
     error();
   }
 }
