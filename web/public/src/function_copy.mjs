d63e3d046897e57d045de3f979e0ad48cd740c12
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:14:57 -0400

    noop

--- web/public/src/function_copy.mjs
+++ web/public/src/function_copy.mjs
@@ -1,6 +1,9 @@
 import { file_copy } from './file_copy.mjs'
 import { function_name_to_path } from './function_name_to_path.mjs'
+import { file_open } from './file_open.mjs'
 export async function function_copy(f_name_old, f_name_new) {
 
-   await file_copy(function_name_to_path(f_name_old), function_name_to_path(f_name_new))
+    const f_path_new = function_name_to_path(f_name_new);
+   await file_copy(function_name_to_path(f_name_old), f_path_new)
+     await file_open(f_path_new);
 }
