52f1675bd0fef3d97fdcd1b1907dca3c815f939d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:13:41 -0400

    2025-08-06T20:13:40.931Z

--- web/public/src/function_name_to_path.mjs
+++ web/public/src/function_name_to_path.mjs
@@ -1,5 +1,5 @@
-export 
-function function_name_to_path(funcName) {
+import {function_name_to_base} from './function_name_to_base.mjs'
+export function function_name_to_path(funcName) {
     let second = ['public', 'src', function_name_to_base(funcName)];
     let joined = path.join(...second);
     return joined;
