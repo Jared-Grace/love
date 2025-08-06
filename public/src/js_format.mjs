69428eb84ad1071ae09dc1c6cc3e67b373fcb405
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:32:01 -0400

    function_format js_format

--- public/src/js_format.mjs
+++ public/src/js_format.mjs
@@ -1,16 +1,16 @@
-
-
-  import prettier from 'prettier';
-import parserBabel from 'prettier/plugins/babel';
-  export async function js_format(code) {
-if (false) {
-    const prettier = await import('https://esm.sh/prettier@3.2.5');
-const parserBabel = await import('https://esm.sh/prettier@3.2.5/plugins/babel');
-}
+import prettier from "prettier";
+import parserBabel from "prettier/plugins/babel";
+export async function js_format(code) {
+  if (false) {
+    const prettier = await import("https://esm.sh/prettier@3.2.5");
+    const parserBabel = await import(
+      "https://esm.sh/prettier@3.2.5/plugins/babel"
+    );
+  }
   const formatted = await prettier.format(code, {
-    parser: 'babel',
+    parser: "babel",
     plugins: [parserBabel],
   });
 
- return formatted
+  return formatted;
 }
