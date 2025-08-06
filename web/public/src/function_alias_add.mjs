b1456b5112e30b15a8e4533c934c6ca6ef5969f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 17:05:35 -0400

    function_new object_property_exists

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -1,4 +1,6 @@
-import { function_read } from "./function_read.mjs";
+import { file_read } from "./file_read.mjs";
 export async function function_alias_add(alias, f_name) {
-  await function_read();
+  let data = await file_read('data.json');
+
+  Object.hasOwn(data, 'aliases')
 }
