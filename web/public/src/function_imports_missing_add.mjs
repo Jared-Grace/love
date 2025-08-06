993b9152ee0f08680cbf683d344cc55155466a28
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:49:49 -0400

    f function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -7,6 +7,18 @@ import { string_is } from "./string_is.mjs";
 export async function function_imports_missing_add(f_name) {
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
-  let on_each=console.log
-  visit(parsed, n=>{if(list_is(n)){return n}if(string_is(n)){return []} return object_properties(n)}, on_each)
+  let on_each = console.log;
+  visit(
+    parsed,
+    (n) => {
+      if (list_is(n)) {
+        return n;
+      }
+      if (string_is(n)) {
+        return [];
+      }
+      return object_properties(n);
+    },
+    on_each,
+  );
 }
