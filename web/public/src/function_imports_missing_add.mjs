76c8ed620e172aa04c5ee0d8e80156704d884b2c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:52:56 -0400

    f visit

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,28 +1,11 @@
 import { file_js_parse } from "./file_js_parse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
-import { visit } from "./visit.mjs";
-import { object_properties } from "./object_properties.mjs";
-import { list_is } from "./list_is.mjs";
-import { string_is } from "./string_is.mjs";
+import { js_visit } from "./js_visit.mjs";
+
 export async function function_imports_missing_add(f_name) {
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
-  let on_each = console.log;
-  js_visit(parsed, on_each);
-}
-function js_visit(parsed, on_each) {
-    visit(
-        parsed,
-        (n) => {
-            if (list_is(n)) {
-                return n;
-            }
-            if (string_is(n)) {
-                return [];
-            }
-            return object_properties(n);
-        },
-        on_each
-    );
-}
+  js_visit(parsed, n => {
 
+  });
+}
