fed52efc7f0c0bd06afd44ae22b0817daa79fd28
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:52:44 -0400

    noop

--- r.mjs
+++ r.mjs
@@ -1,8 +1,6 @@
 
 import {function_run} from './public/src/function_run.mjs'
-// Simulate __dirname
 (async () => {
   const [, , funcName, ...args] = process.argv;
-
   await function_run(funcName, args)
 })();
