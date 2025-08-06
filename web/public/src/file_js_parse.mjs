50d697b28538dfc1a009386a7a1a2d63eac63967
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:35:11 -0400

    f function_imports_missing_add

--- web/public/src/file_js_parse.mjs
+++ web/public/src/file_js_parse.mjs
@@ -1,5 +1,6 @@
-import { file_read } from "./file_read.mjs";
+import { file_read } from "./file_read.mjs";import { js_parse } from "./js_parse.mjs";
 
 export async function file_js_parse(f_path) {
   let code = await file_read(f_path);
+  return js_parse(code)
 }
--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,1 +1,3 @@
-export function function_imports_missing_add(f_name){}
+export function function_imports_missing_add(f_name) {
+  let parsed = file_js_parse();
+}
