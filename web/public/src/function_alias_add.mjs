9c34fc68c3465997c1d733e7af6b57bf3420df02
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 17:03:34 -0400

    function_format function_alias_add

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -1,1 +1,4 @@
-export function function_alias_add(alias,f_name){}
+import { function_read } from "./function_read.mjs";
+export async function function_alias_add(alias, f_name) {
+  await function_read();
+}
