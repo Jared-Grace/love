e96b23f1072744d40d71a3dca624289a6886bb79
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:14:43 -0400

    2025-08-06T20:14:43.268Z

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -1,6 +1,7 @@
 import {file_overwrite} from './file_overwrite.mjs'
 import { function_name_to_base } from './function_name_to_base.mjs'
+import { function_name_to_path } from './function_name_to_path.mjs'
 export async function function_new(f_name) {
 
-   await file_overwrite(function_name_to_base(f_name), 'export function '+f_name+'(){}')
+   await file_overwrite(function_name_to_path(f_name), 'export function '+f_name+'(){}')
 }
