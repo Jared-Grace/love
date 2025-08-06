5d5689ce8da0656adf9ca54d274b9fd841e6d4f5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:19:47 -0400

    noop

--- public/src/file_read_json.mjs
+++ public/src/file_read_json.mjs
@@ -1,1 +1,7 @@
-export function file_read_json(){}
+import { file_read } from "./file_read.mjs";
+import { json_from } from "./json_from.mjs";
+export async function file_read_json(file_path) {
+    let json = await file_read(file_path);
+    let data = json_from(json);
+    return data;
+}
--- public/src/function_alias_add.mjs
+++ public/src/function_alias_add.mjs
@@ -1,20 +1,14 @@
-import { file_read } from "./file_read.mjs";
-import { json_from } from "./json_from.mjs";
+
 import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export async function function_alias_add(alias, f_name) {
-    const file_path = "data.json";
-  let data = await file_read_json(file_path);
+  let data = await file_read_json("data.json");
   let d_alias = object_property_get(data, "alias");
   if (object_property_exists(d_alias, alias)) {
     error();
   }
   object_property_set(d_alias, alias, f_name);
 }
-async function file_read_json(file_path) {
-    let json = await file_read(file_path);
-    let data = json_from(json);
-    return data;
-}
+
 
