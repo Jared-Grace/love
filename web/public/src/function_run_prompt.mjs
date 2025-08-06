2cc0f04ff50413d74fd5f4ffd5807abd993fb8d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:04:03 -0400

    noop

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -1,5 +1,7 @@
 import {command_line_read} from './command_line_read.mjs'
+import {function_run} from './function_run.mjs'
 export async function function_run_prompt() {
-  let prompt = "";
- return await command_line_read(prompt);
+ let line= await command_line_read("");
+   const [funcName, ...args] = line.split(' ');
+   await function_run(funcName, args)
 }
