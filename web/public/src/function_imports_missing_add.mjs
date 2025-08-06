203eabfd4f434b59a636c8680ef909982d30e44e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:43:20 -0400

    n list_is

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,7 +1,9 @@
 import { file_js_parse } from "./file_js_parse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
+import { visit } from "./visit.mjs";
 export async function function_imports_missing_add(f_name) {
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
-  console.log(parsed)
+  let on_each=console.log
+  visit(parsed, n=>{if(a){}}, on_each)
 }
--- web/public/src/visit.mjs
+++ web/public/src/visit.mjs
@@ -1,4 +1,6 @@
+import {each} from './each.mjs'
 export function visit(node, children_get, on_each){
     on_each(node);
-    children=children_get(node);
+    let children=children_get(node);
+     each(children, c=>visit(c, children_get, on_each))
 }
