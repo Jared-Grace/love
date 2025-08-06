1a1f1a45cbc863bc8f169b014c52a929def6b6fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:33:59 -0400

    function_format function_run

--- public/src/function_run.mjs
+++ public/src/function_run.mjs
@@ -1,1 +1,42 @@
-export function function_run(){}
+import path from "path";
+import { fileURLToPath } from "url";
+import { git_acp } from "./public/src/git_acp.mjs";
+
+export async function function_run(funcName, args) {
+  // runFunction.js
+
+  await run(funcName, args);
+  await git_acp([funcName].concat(args).join(" "));
+
+  async function run(funcName, args) {
+    if (!funcName) {
+      console.error("❌ Please provide a function name as the first argument.");
+      process.exit(1);
+    }
+
+    try {
+      // Import the function module from ./public/
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      let joined = function_name_to_path(funcName);
+      const f_path = path.join(...[__dirname, joined]);
+      const imported = await import(`file://${f_path}`);
+
+      const fn = imported[funcName];
+
+      if (typeof fn !== "function") {
+        throw new Error(
+          `❌ The module "${funcName}" does not export a default function.`,
+        );
+      }
+
+      // Await the result of the function with the remaining args
+      const result = await fn(...args);
+
+      console.log("✅ Result:", result);
+    } catch (err) {
+      console.error("❌ Error:", err.message);
+      process.exit(1);
+    }
+  }
+}
