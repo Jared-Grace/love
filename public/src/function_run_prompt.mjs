66c92f9d4cd9ab826c9637a74e1b8d0d2b50f540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:56:18 -0400

    function_alias_add f function_format

--- public/src/function_run_prompt.mjs
+++ public/src/function_run_prompt.mjs
@@ -1,1 +1,15 @@
-export function function_run_prompt(){}
+import readline from 'readline';
+export async function function_run_prompt(){
+
+    await new Promise(resolve=>{
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter something: ', (answer) => {
+  console.log(`You entered: ${answer}`);
+  rl.close();
+});}
+    )}
