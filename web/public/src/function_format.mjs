363c06f6579b7e607c6c683238e43e428d120f89
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:26:20 -0400

    function_copy function_read function_format

---
+++ web/public/src/function_format.mjs
@@ -0,0 +1,6 @@
+import { function_name_to_path } from './function_name_to_path.mjs'
+import { file_read } from './file_read.mjs'
+export async function function_read(f_name) {
+
+   await file_read(function_name_to_path(f_name))
+}
--- web/public/src/js_format.mjs
+++ web/public/src/js_format.mjs
@@ -1,10 +1,8 @@
 
-      import prettier from 'https://esm.sh/prettier@3.2.5';
-  import parserBabel from 'https://esm.sh/prettier@3.2.5/plugins/babel';
   
-  export async function js_format() {
-
-  const code = `function test( ){console.log("Hello")}`;
+  export async function js_format(code) {
+const prettier = await import('https://esm.sh/prettier@3.2.5');
+const parserBabel = await import('https://esm.sh/prettier@3.2.5/plugins/babel');
   const formatted = await prettier.format(code, {
     parser: 'babel',
     plugins: [parserBabel],
