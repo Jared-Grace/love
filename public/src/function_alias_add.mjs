2077ef8ce57094f1428c228fa861dcace1258503
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:18:12 -0400

    function_new file_read_json

--- public/src/function_alias_add.mjs
+++ public/src/function_alias_add.mjs
@@ -4,11 +4,17 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export async function function_alias_add(alias, f_name) {
-  let json = await file_read("data.json");
-  let data = json_from(json);
+    const file_path = "data.json";
+  let data = await file_read_json(file_path);
   let d_alias = object_property_get(data, "alias");
   if (object_property_exists(d_alias, alias)) {
     error();
   }
   object_property_set(d_alias, alias, f_name);
 }
+async function file_read_json(file_path) {
+    let json = await file_read(file_path);
+    let data = json_from(json);
+    return data;
+}
+
