3b4b10a8e5bc7965f735cee19bd9c4add4fddfdf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:30:27 -0400

    noop

--- web/public/src/js_format.mjs
+++ web/public/src/js_format.mjs
@@ -1,8 +1,9 @@
 
+      import prettier from 'https://esm.sh/prettier@3.2.5';
+  import parserBabel from 'https://esm.sh/prettier@3.2.5/plugins/babel';
   
-  export async function js_format(code) {
-const prettier = await import('https://esm.sh/prettier@3.2.5');
-const parserBabel = await import('https://esm.sh/prettier@3.2.5/plugins/babel');
+  export async function js_format() {
+
   const formatted = await prettier.format(code, {
     parser: 'babel',
     plugins: [parserBabel],
