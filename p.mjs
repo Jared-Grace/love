17c9d7b83c41e3c78756de62566e5e873f5d3ecb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:12:09 -0400

    p

---
+++ p.mjs
@@ -0,0 +1,5 @@
+
+import { function_run_prompt } from './public/src/function_run_prompt.mjs';
+(async () => {
+  await function_run_prompt()
+})();
--- public/src/function_run_prompt.mjs
+++ public/src/function_run_prompt.mjs
@@ -1,7 +1,7 @@
 import {command_line_read} from './command_line_read.mjs'
-import {function_run} from './function_run.mjs'
+import {function_run_git} from './function_run_git.mjs'
 export async function function_run_prompt() {
  let line= await command_line_read("");
    const [funcName, ...args] = line.split(' ');
-   await function_run(funcName, args)
+   await function_run_git(funcName, args)
 }
