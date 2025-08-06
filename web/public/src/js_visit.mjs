5d6ca879e3ee1ab1c80ef1ba60568f5539820783
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:52:56 -0400

    f visit

--- web/public/src/js_visit.mjs
+++ web/public/src/js_visit.mjs
@@ -1,1 +1,21 @@
-export function js_visit(){}
+
+import { visit } from "./visit.mjs";
+import { object_properties } from "./object_properties.mjs";
+import { list_is } from "./list_is.mjs";
+import { string_is } from "./string_is.mjs";
+export function js_visit(parsed, on_each) {
+    visit(
+        parsed,
+        (n) => {
+            if (list_is(n)) {
+                return n;
+            }
+            if (string_is(n)) {
+                return [];
+            }
+            return object_properties(n);
+        },
+        on_each
+    );
+}
+
