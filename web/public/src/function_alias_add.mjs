f3895dfe07df34b231935a237762ca1741ee092f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:39:38 -0400

    noop

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -1,8 +1,6 @@
 
-import { object_property_exists } from "./object_property_exists.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { file_read_json } from "./file_read_json.mjs";
+import { function_alias_add_generic } from "./function_alias_add_generic.mjs";
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
 export async function function_alias_add(alias, f_name) {
     var { exists, aliases, file_path, data } = await function_alias_add_generic(alias);
@@ -14,11 +12,5 @@ export async function function_alias_add(alias, f_name) {
 }
 
 
-async function function_alias_add_generic(alias) {
-    const file_path = "data.json";
-    let data = await file_read_json(file_path);
-    let aliases = object_property_get(data, "aliases");
-    const exists = object_property_exists(aliases, alias);
-    return { exists, aliases, file_path, data };
-}
+
 
