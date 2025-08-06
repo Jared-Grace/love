111ef4b3d819a5985c8d6bed88bf257bec15e563
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:24:05 -0400

    noop

--- web/public/src/function_read.mjs
+++ web/public/src/function_read.mjs
@@ -1,6 +1,5 @@
-import { file_copy } from './file_copy.mjs'
 import { function_name_to_path } from './function_name_to_path.mjs'
-export async function function_copy(f_name_old, f_name_new) {
+export async function function_read(f_name) {
 
-   await file_copy(function_name_to_path(f_name_old), function_name_to_path(f_name_new))
+   await file_read(function_name_to_path(f_name))
 }
