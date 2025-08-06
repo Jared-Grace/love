4e78266ef5934fe1124e42759fa0f89f55963b8a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:01:36 -0400

    function_run_prompt

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -1,5 +1,5 @@
 import {command_line_read} from './command_line_read.mjs'
 export async function function_run_prompt() {
   let prompt = "";
-  await command_line_read(prompt);
+ return await command_line_read(prompt);
 }
