342974accb37447fe68777a598c1181219a3e647
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 15:47:51 -0400

    Auto commit at 2025-08-06T19:47:50.754Z

---
+++ web/public/src/js_format.mjs
@@ -0,0 +1,14 @@
+
+      import prettier from 'https://esm.sh/prettier@3.2.5';
+  import parserBabel from 'https://esm.sh/prettier@3.2.5/plugins/babel';
+  
+  export default async function js_format() {
+
+  const code = `function test( ){console.log("Hello")}`;
+  const formatted = await prettier.format(code, {
+    parser: 'babel',
+    plugins: [parserBabel],
+  });
+
+  console.log(formatted);
+}
