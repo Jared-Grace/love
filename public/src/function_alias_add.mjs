2b0120bfaa454788b05a65a8136558def29be15d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:36:02 -0400

    function_new function_alias_add_generic

--- public/src/function_alias_add.mjs
+++ public/src/function_alias_add.mjs
@@ -5,14 +5,20 @@ import { object_property_set } from "./object_property_set.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
 export async function function_alias_add(alias, f_name) {
-    const file_path = "data.json";
-  let data = await file_read_json(file_path);
-  let d_aliases = object_property_get(data, "aliases");
-  if (object_property_exists(d_aliases, alias)) {
+    var { exists, aliases, file_path, data } = await function_alias_add_generic(alias);
+  if (exists) {
     error();
   }
-  object_property_set(d_aliases, alias, f_name);
+  object_property_set(aliases, alias, f_name);
   await file_overwrite_json(file_path,data)
 }
 
+
+async function function_alias_add_generic(alias) {
+    const file_path = "data.json";
+    let data = await file_read_json(file_path);
+    let aliases = object_property_get(data, "aliases");
+    const exists = object_property_exists(aliases, alias);
+    return { exists, aliases, file_path, data };
+}
 
