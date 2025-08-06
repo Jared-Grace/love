dc70a43bd64f890f7bdb253cc34c13b3ec03f8ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:58:18 -0400

    function_format function_run_prompt

--- public/src/function_run_prompt.mjs
+++ public/src/function_run_prompt.mjs
@@ -1,18 +1,4 @@
-import readline from "readline";
 export async function function_run_prompt() {
-    prompt=''
+  prompt = "";
   await command_line_read(prompt);
 }
-async function command_line_read(prompt) {
-    return await new Promise((resolve) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-        rl.question(prompt, (answer) => {
-            rl.close();
-            resolve(answer);
-        });
-    });
-}
-
