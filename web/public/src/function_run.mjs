46d5df5b79b23ebdb4d974d614a4b3746e1d9e8f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:51:42 -0400

    function_format function_run

--- web/public/src/function_run.mjs
+++ web/public/src/function_run.mjs
@@ -6,38 +6,34 @@ import { function_name_to_base } from "./function_name_to_base.mjs";
 export async function function_run(funcName, args) {
   // runFunction.js
 
-  await run(funcName, args);
-  await git_acp([funcName].concat(args).join(" "));
-
-  async function run(funcName, args) {
-    if (!funcName) {
-      console.error("❌ Please provide a function name as the first argument.");
-      process.exit(1);
-    }
-
-    try {
-      // Import the function module from ./public/
-      const __filename = fileURLToPath(import.meta.url);
-      const __dirname = path.dirname(__filename);
-      let joined = function_name_to_base(funcName);
-      const f_path = path.join(...[__dirname, joined]);
-      const imported = await import(`file://${f_path}`);
-
-      const fn = imported[funcName];
-
-      if (typeof fn !== "function") {
-        throw new Error(
-          `❌ The module "${funcName}" does not export a default function.`,
-        );
-      }
-
-      // Await the result of the function with the remaining args
-      const result = await fn(...args);
-
-      console.log("✅ Result:", result);
-    } catch (err) {
-      console.error("❌ Error:", err.message);
-      process.exit(1);
-    }
+  if (!funcName) {
+    console.error("❌ Please provide a function name as the first argument.");
+    process.exit(1);
   }
+
+  try {
+    // Import the function module from ./public/
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    let joined = function_name_to_base(funcName);
+    const f_path = path.join(...[__dirname, joined]);
+    const imported = await import(`file://${f_path}`);
+
+    const fn = imported[funcName];
+
+    if (typeof fn !== "function") {
+      throw new Error(
+        `❌ The module "${funcName}" does not export a default function.`,
+      );
+    }
+
+    // Await the result of the function with the remaining args
+    const result = await fn(...args);
+
+    console.log("✅ Result:", result);
+  } catch (err) {
+    console.error("❌ Error:", err.message);
+    process.exit(1);
+  }
+  await git_acp([funcName].concat(args).join(" "));
 }
