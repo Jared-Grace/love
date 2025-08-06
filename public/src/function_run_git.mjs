4fc0843bf13725614b1d3d8237288be824f32151
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:06:31 -0400

    noop

--- public/src/function_run_git.mjs
+++ public/src/function_run_git.mjs
@@ -1,28 +1,9 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import { git_acp } from "./git_acp.mjs";
-import { function_name_to_base } from "./function_name_to_base.mjs";
-import { function_name_unalias } from "./function_name_unalias.mjs";
+import { function_run } from "./function_run.mjs";
 
-export async function function_run(funcName, args) {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    funcName=await function_name_unalias(funcName)
-    let joined =  function_name_to_base(funcName);
-    const f_path = path.join(...[__dirname, joined]);
-    const imported = await import(`file://${f_path}`);
-
-    const fn = imported[funcName];
-
-    if (typeof fn !== "function") {
-      throw new Error(
-        `❌ The module "${funcName}" does not export a default function.`,
-      );
-    }
-
-    // Await the result of the function with the remaining args
-    const result = await fn(...args);
-
+export async function function_run_git(funcName, args) {
+   let result=await function_run(funcName, args)
     console.log("✅ Result:", result);
   await git_acp([funcName].concat(args).join(" "));
+  return result
 }
