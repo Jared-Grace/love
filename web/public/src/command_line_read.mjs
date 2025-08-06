5fff8bfd6e6dc0df7a547c7f81d5e17b11876115
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 18:58:18 -0400

    function_format function_run_prompt

--- web/public/src/command_line_read.mjs
+++ web/public/src/command_line_read.mjs
@@ -1,9 +1,5 @@
 import readline from "readline";
-export async function function_run_prompt() {
-    prompt=''
-  await command_line_read(prompt);
-}
-async function command_line_read(prompt) {
+export async function command_line_read(prompt) {
     return await new Promise((resolve) => {
         const rl = readline.createInterface({
             input: process.stdin,
