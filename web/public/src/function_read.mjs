330fe2e629339b643b2c745b429a8586be969473
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:28:09 -0400

    noop

--- web/public/src/function_read.mjs
+++ web/public/src/function_read.mjs
@@ -2,5 +2,5 @@ import { function_name_to_path } from './function_name_to_path.mjs'
 import { file_read } from './file_read.mjs'
 export async function function_read(f_name) {
 
-   await file_read(function_name_to_path(f_name))
+  return await file_read(function_name_to_path(f_name))
 }
