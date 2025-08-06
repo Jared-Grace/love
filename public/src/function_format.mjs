d7c8fa7f0bcee6070a6ad36fc90f9d0d5c3d039d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 16:28:09 -0400

    noop

--- public/src/function_format.mjs
+++ public/src/function_format.mjs
@@ -1,6 +1,10 @@
 import { function_name_to_path } from './function_name_to_path.mjs'
 import { file_read } from './file_read.mjs'
-export async function function_read(f_name) {
+import { js_format } from './js_format.mjs'
+export async function function_format(f_name) {
 
-   await file_read(function_name_to_path(f_name))
+    const f_path = function_name_to_path(f_name)
+   let r=await file_read(f_path)
+  let formatted=js_format(r)
+  await file_overwrite(f_path,formatted)
 }
