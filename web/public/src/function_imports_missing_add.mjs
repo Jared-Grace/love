1de8e1c7b7d8a5581fa58313f70a1a70a4e5901b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:49:02 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -3,9 +3,10 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 import { visit } from "./visit.mjs";
 import { object_properties } from "./object_properties.mjs";
 import { list_is } from "./list_is.mjs";
+import { string_is } from "./string_is.mjs";
 export async function function_imports_missing_add(f_name) {
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
   let on_each=console.log
-  visit(parsed, n=>{if(list_is(n)){return n} return object_properties(n)}, on_each)
+  visit(parsed, n=>{if(list_is(n)){return n}if(string_is(n)){return []} return object_properties(n)}, on_each)
 }
