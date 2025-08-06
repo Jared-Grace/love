9fdf908d8d7b55ae867de0e98b1651d1cc0cb211
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 19:09:56 -0400

    function_run_prompt

--- web/public/src/function_run.mjs
+++ web/public/src/function_run.mjs
@@ -1,25 +1,8 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { function_name_to_base } from "./function_name_to_base.mjs";
-import { function_name_unalias } from "./function_name_unalias.mjs";
+import { function_import } from "./function_import.mjs";
 
 export async function function_run(funcName, args) {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    funcName=await function_name_unalias(funcName)
-    let joined =  function_name_to_base(funcName);
-    const f_path = path.join(...[__dirname, joined]);
-    const imported = await import(`file://${f_path}`);
+  const fn = await function_import(funcName);
 
-    const fn = imported[funcName];
-
-    if (typeof fn !== "function") {
-      throw new Error(
-        `❌ The module "${funcName}" does not export a default function.`,
-      );
-    }
-
-    // Await the result of the function with the remaining args
-    const result = await fn(...args);
-return result
+  const result = await fn(...args);
+  return result;
 }
