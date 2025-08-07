dc4c7bd492b26f0c3ba77ffcddb0b55453dc0d55
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 04:48:15 -0400

    f function_type_first

--- web/public/src/function_type_first.mjs
+++ web/public/src/function_type_first.mjs
@@ -1,6 +1,11 @@
 import { js_visit_nodes } from "./js_visit_nodes.mjs";
-export function function_type_first() {
-  js_visit_nodes(parsed, (node) => {
-    la(js_node_type(node));
+import { list_adder } from "./list_adder.mjs";
+import { function_parse } from "./function_parse.mjs";
+export async function function_type_first(f_name) {
+  let parsed = await function_parse(f_name);
+  list_adder((la) => {
+    js_visit_nodes(parsed, (node) => {
+      la(node);
+    });
   });
 }
