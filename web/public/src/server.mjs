4ace25f40de007535318ada8fcb059f1ec276eba
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:12:18 -0400

    i server

--- web/public/src/server.mjs
+++ web/public/src/server.mjs
@@ -1,19 +1,14 @@
+import {path_join} from './path_join.mjs';
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
-import { folder_public } from "./folder_public.mjs";
-
+import {fileURLToPath} from "url";
+import {folder_public} from "./folder_public.mjs";
 export function server() {
   const app = express();
   const port = 3000;
-
-  // __dirname workaround for ES modules
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-
-  // Serve static files from the "public" directory
-  app.use(express.static(path.join(__dirname, folder_public())));
-
+  app.use(express.static(path_join([__dirname, '..', '..', folder_public()])));
   app.listen(port, () => {
     console.log(`Static server running at http://localhost:${port}`);
   });
