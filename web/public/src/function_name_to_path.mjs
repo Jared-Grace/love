62abb8a1c995e51ec775e778976dc6aa7af8e677
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:20:46 -0400

    ia function_name_to_path_unalias

--- web/public/src/function_name_to_path.mjs
+++ web/public/src/function_name_to_path.mjs
@@ -1,9 +1,7 @@
-import {function_name_unalias} from './function_name_unalias.mjs';
 import {function_name_to_base} from './function_name_to_base.mjs';
 import {functions_path} from './functions_path.mjs';
 import path from 'path';
-export async function function_name_to_path_unalias(funcName) {
-  funcName = await function_name_unalias(funcName);
+export async function function_name_to_path(funcName) {
   let second = [functions_path(), function_name_to_base(funcName)];
   let joined = path.join(...second);
   return joined;
