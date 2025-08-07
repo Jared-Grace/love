d695968cc3fe32503a89f87c19b4483aac6bb705
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:34:39 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -6,6 +6,9 @@ import { list_difference } from "./list_difference.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { folder_read } from "./folder_read.mjs";
+import { function_name_extension } from "./function_name_extension.mjs";
+import { string_suffix_without } from "./string_suffix_without.mjs";
+import { list_map } from "./list_map.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
@@ -16,5 +19,6 @@ let imports_self = list_concat(imports,[f_name])
   let missing = list_difference(identifiers, imports_self);
 
   let paths=folder_read(functions_path())
-  console.log(paths);
+  let function_names = list_map(paths, p => string_suffix_without(p, function_name_extension()))
+  console.log(function_names);
 }
