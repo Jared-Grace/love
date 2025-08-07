7792756c33991c3e47e75ff9542462a1cff72e16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 15:30:23 -0400

    ia function_name_to_path

--- web/public/src/function_open.mjs
+++ web/public/src/function_open.mjs
@@ -2,5 +2,5 @@ import {file_open} from './file_open.mjs';
 import {function_name_to_path} from './function_name_to_path.mjs';
 export function function_open(f_name) {
   const f_path = function_name_to_path(f_name);
-  file_open();
+  file_open(f_path);
 }
