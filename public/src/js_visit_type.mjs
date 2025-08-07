aaec4109af19fd43389f7dab39e7fea56704703f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 12:57:00 -0400

    function_imports_missing_add function_imports_missing_add

--- public/src/js_visit_type.mjs
+++ public/src/js_visit_type.mjs
@@ -1,5 +1,6 @@
 import { visit_filter } from "./visit_filter.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 
 import { js_visit_filter } from "./js_visit_filter.mjs";
 
