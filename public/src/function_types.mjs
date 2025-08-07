68f134c661cd1eeec4ed3bf216b243cf76ad57b9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 23:47:00 -0400

    function_types function_types

--- public/src/function_types.mjs
+++ public/src/function_types.mjs
@@ -7,7 +7,6 @@ import { list_unique } from "./list_unique.mjs";
 
 export async function function_types(f_name) {
   let parsed = await function_parse(f_name);
-  return list_unique([1,1,1])
   return list_adder_unique((la) => {
     js_visit(parsed, (v) => {
       let { node } = v;
