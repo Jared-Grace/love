a3992065e94c13049f3da5a33d3656cf3f122a5d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 22:01:13 -0400

    n js_node_is

--- public/src/js_visit.mjs
+++ public/src/js_visit.mjs
@@ -1,6 +1,7 @@
 import { visit_filter } from "./visit_filter.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { visit_filter } from "./visit_filter.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 export function js_visit(parsed, on_each) {
-  visit_filter(parsed, js_visit_children_get, on_each);
+  visit_filter(parsed, js_visit_children_get, n=>object_property_exists(n,'node'),on_each);
 }
