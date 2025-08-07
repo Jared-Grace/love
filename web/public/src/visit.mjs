a274c3bd3940c4eef4b07c7c32c3176670704f13
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 21:48:18 -0400

    p

--- web/public/src/visit.mjs
+++ web/public/src/visit.mjs
@@ -1,4 +1,4 @@
-import { each } from "./each.mjs";
+import { tautology } from "./tautology.mjs";
 export function visit(node, children_get, on_each) {
-
+visit_filter(node, children_get, tautology, on_each)
 }
