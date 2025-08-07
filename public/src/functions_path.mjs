cfea2a357adf7d8a71699c1b48307fc1bab3fca7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:12:19 -0400

    s format

--- public/src/functions_path.mjs
+++ public/src/functions_path.mjs
@@ -1,11 +1,7 @@
 import {path_join} from './path_join.mjs';
 import {folder_public} from './folder_public.mjs';
-import path from "path";
 export function functions_path() {
   let second = [folder_public(), "src"];
   let joined = path_join(second);
   return joined;
-}
-function path_join(second) {
-  return path.join(...second);
 }
