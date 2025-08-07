494cd8091b370e2b0f59f8ffa9858cd93a39f447
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 04:43:44 -0400

    function_types function_types

--- web/public/src/function_types.mjs
+++ web/public/src/function_types.mjs
@@ -1,18 +1,13 @@
-import { js_visit } from "./js_visit.mjs";
+import { js_visit_nodes } from "./js_visit_nodes.mjs";
 import { function_parse } from "./function_parse.mjs";
-import { js_node_is } from "./js_node_is.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 
 export async function function_types(f_name) {
   let parsed = await function_parse(f_name);
   return list_adder_unique((la) => {
-    js_visit(parsed, (v) => {
-      let { node } = v;
-      if (!js_node_is(node)) {
-        return;
-      }
+    js_visit_nodes(parsed, (node) => {
       la(js_node_type(node));
-    });
+    })
   });
 }
