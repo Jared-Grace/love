d25f595a527e4d0f207a425c425758c0f4125778
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:37:39 -0400

    function_imports_missing_add function_imports_missing_add

--- public/src/function_imports_missing_add.mjs
+++ public/src/function_imports_missing_add.mjs
@@ -4,11 +4,8 @@ import { js_declaration_single } from "./js_declaration_single.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { list_difference } from "./list_difference.mjs";
 import { list_concat } from "./list_concat.mjs";
-import { functions_path } from "./functions_path.mjs";
-import { folder_read } from "./folder_read.mjs";
-import { function_name_extension } from "./function_name_extension.mjs";
-import { string_suffix_without } from "./string_suffix_without.mjs";
-import { list_map } from "./list_map.mjs";
+import { functions_names } from "./functions_names.mjs";
+import { list_intersect } from "./list_intersect.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
@@ -18,7 +15,9 @@ export async function function_imports_missing_add(f_name) {
 let imports_self = list_concat(imports,[f_name])
   let missing = list_difference(identifiers, imports_self);
 
-  let paths=folder_read(functions_path())
-  let function_names = list_map(paths, p => string_suffix_without(p, function_name_extension()))
-  console.log(function_names);
+  let f_names = functions_names();
+  let missing_imports = list_intersect(missing, f_names);
+  console.log(missing_imports);
 }
+
+
