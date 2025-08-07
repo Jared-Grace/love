c632346b866349144061fc68d1e571750f17ce91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:01:41 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -3,7 +3,6 @@ import { function_parse } from "./function_parse.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
-    console.log(parsed);
   js_visit_type(parsed, "ImportDeclaration", (v) => {
     let { node } = v;
     console.log(node);
