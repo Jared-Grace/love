5d54d0cc82057bab8744ba430acd0a77bf12abed
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:33:09 -0400

    i data_save

--- public/src/data_save.mjs
+++ public/src/data_save.mjs
@@ -1,1 +1,5 @@
-export function data_save(){}
+import {file_overwrite_json} from './file_overwrite_json.mjs';
+export async function data_save(a) {
+  let {file_path, data} = a;
+  await file_overwrite_json(file_path, data);
+}
