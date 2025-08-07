1e5f66a89de5fd978055ef2915404754a2b57f60
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:12:30 -0400

    c list_includes string_includes

--- web/public/src/functions_search.mjs
+++ web/public/src/functions_search.mjs
@@ -1,6 +1,6 @@
 import {list_filter} from './list_filter.mjs';
 import {functions_names} from './functions_names.mjs';
-export function functions_search() {
+export function functions_search(search) {
   let f_names = functions_names();
-  list_filter();
+  return list_filter(f_names, n=>string_includes(n,search));
 }
