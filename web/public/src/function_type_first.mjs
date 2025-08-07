37c65bd1de6a717b52b9bd126cd8db55507c4e59
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 04:44:16 -0400

    f function_type_first

--- web/public/src/function_type_first.mjs
+++ web/public/src/function_type_first.mjs
@@ -1,1 +1,6 @@
-export function function_type_first(){}
+import { js_visit_nodes } from "./js_visit_nodes.mjs";
+export function function_type_first() {
+  js_visit_nodes(parsed, (node) => {
+    la(js_node_type(node));
+  });
+}
