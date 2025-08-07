3f2b0060cda54ad70c4078ff48b3518f5379d06d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 23:16:13 -0400

    f list_adder

--- web/public/src/list_adder.mjs
+++ web/public/src/list_adder.mjs
@@ -1,1 +1,9 @@
-export function list_adder(){}
+import { list_add } from "./list_add.mjs";
+
+export function list_adder(lambda) {
+  let list = [];
+  lambda(function list_adder_inner(item) {
+    list_add(list, item);
+  });
+  return list;
+}
