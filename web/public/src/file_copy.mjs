9159325e08113941a2152dabaea00c7fbc5e5782
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:38:27 -0400

    p

--- web/public/src/file_copy.mjs
+++ web/public/src/file_copy.mjs
@@ -1,4 +1,5 @@
 export async function file_copy(file_path_old,file_path_new) {
    let fs= await import('fs')
    await fs.promises.copyFile(file_path_old,file_path_new)
+   console.log('file_copy')
 }
