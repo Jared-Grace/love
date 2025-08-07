3d54c8242569d4c79f025f5a740e8d5aa63babb3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:38:27 -0400

    p

--- public/src/function_imports_missing_add.mjs
+++ public/src/function_imports_missing_add.mjs
@@ -1,24 +1,17 @@
 import { function_parse } from "./function_parse.mjs";
 import { js_imports } from "./js_imports.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
-import { list_single } from "./list_single.mjs";
-import { js_visit_type } from "./js_visit_type.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { list_adder_unique } from "./list_adder_unique.mjs";
+import { js_identifiers_names } from "./js_identifiers_names.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
   let imports = js_imports(parsed);
 
     let declaration = js_declaration_single(parsed);
-   let identifiers= list_adder_unique(la=>{
-
-    js_visit_type(declaration,'Identifier',v=>{
-        let {node}=v
-        la(object_property_get(node,'name'))
-    })
-    })
+   let identifiers= js_identifiers_names(declaration)
 
   console.log(identifiers)
 }
+
+
 
