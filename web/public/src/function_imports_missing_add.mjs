c0133dadd7c34b93c5b644fbdbda6f1cb4a7acce
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 22:54:15 -0400

    f function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,11 +1,7 @@
-import { file_js_parse } from "./file_js_parse.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
+import { function_parse } from "./function_parse.mjs";
 
 export async function function_imports_missing_add(f_name) {
-  const f_path = function_name_to_path(f_name);
-  let parsed = await file_js_parse(f_path);
-  js_visit_type(parsed, v => {
-
-  });
+  let parsed = await function_parse(f_name);
+  js_visit_type(parsed, (v) => {});
 }
--- web/public/src/function_parse.mjs
+++ web/public/src/function_parse.mjs
@@ -1,1 +1,6 @@
-export function function_parse(){}
+import { file_js_parse } from "./file_js_parse.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
+export async function function_parse(f_name){
+    const f_path = function_name_to_path(f_name);
+    let parsed = await file_js_parse(f_path);
+    return parsed;}
