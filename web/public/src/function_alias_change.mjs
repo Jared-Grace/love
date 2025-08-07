116c36e9abfbbb40f036494c47ad2c4eeed22119
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:26:32 -0400

    c object_property_get object_property_delete

--- web/public/src/function_alias_change.mjs
+++ web/public/src/function_alias_change.mjs
@@ -2,11 +2,13 @@ import {error} from './error.mjs';
 import {object_property_set} from "./object_property_set.mjs";
 import {function_alias_add_generic} from "./function_alias_add_generic.mjs";
 import {file_overwrite_json} from "./file_overwrite_json.mjs";
-export async function function_alias_add(alias, f_name) {
-  var {exists, aliases, file_path, data} = await function_alias_add_generic(alias);
-  if (exists) {
+export async function function_alias_add(alias_old, alias_new) {
+  var {exists, aliases, file_path, data,unaliased} = await function_alias_add_generic(alias_old);
+  if (!exists) {
     error();
   }
-  object_property_set(aliases, alias, f_name);
+
+  object_property_delete(aliases, alias_old);
+  object_property_set(aliases, alias_new, unaliased);
   await file_overwrite_json(file_path, data);
 }
