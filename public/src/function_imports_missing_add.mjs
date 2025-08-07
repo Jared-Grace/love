f25fe1889c02e790a6eaa8c635b8f2611d507176
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 15:32:05 -0400

    f function_read

--- public/src/function_imports_missing_add.mjs
+++ public/src/function_imports_missing_add.mjs
@@ -19,7 +19,7 @@ export async function function_imports_missing_add(f_name) {
     let statement = js_parse_statement(code);
     list_add_first(body, statement);
   });
-      const f_path = function_name_to_path(f_name);
+      const f_path = await function_name_to_path(f_name);
   await file_js_unparse(f_path, parsed)
 }
 
