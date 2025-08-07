8fb4dfc4d0074773131f81d4ff549fcbfe16f655
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:37:39 -0400

    function_imports_missing_add function_imports_missing_add

---
+++ web/public/src/list_intersect.mjs
@@ -0,0 +1,13 @@
+import { each } from "./each.mjs";
+import { list_adder } from "./list_adder.mjs";
+import { list_includes } from "./list_includes.mjs";
+
+export function list_intersect(list, other) {
+  return list_adder((la) => {
+    each(list, (l) => {
+      if (list_includes(other, l)) {
+        la(l);
+      }
+    });
+  });
+}
