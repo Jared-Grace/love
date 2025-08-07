8cc3ef214d2920a3d0a3cb31ac977ac8d3d0e326
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 23:22:58 -0400

    f list_unique

--- public/src/list_unique.mjs
+++ public/src/list_unique.mjs
@@ -1,9 +1,11 @@
 import { each } from "./each.mjs";
+import { list_add } from "./list_add.mjs";
 
 export function list_unique(list) {
   let unique = [];
   each(list, (item) => {
-    if (!list_includes(unique)) {
+    if (!list_includes(unique, item)) {
+      list_add(unique, item);
     }
   });
 }
