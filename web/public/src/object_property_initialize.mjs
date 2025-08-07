0eebea3b9f80b4f5e8bd1c6d9971fe81a4fba75c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:27:45 -0400

    i object_property_initialize

--- web/public/src/object_property_initialize.mjs
+++ web/public/src/object_property_initialize.mjs
@@ -1,8 +1,11 @@
-import { object_property_get } from "./object_property_get.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
-export function object_property_initialize(object, property_name) {
+import {object_property_set} from './object_property_set.mjs';
+import {object_property_get} from "./object_property_get.mjs";
+import {object_property_exists} from "./object_property_exists.mjs";
+export function object_property_initialize(object, property_name, value) {
   const exists = object_property_exists(object, property_name);
   if (!exists) {
+    object_property_set(object, property_name, value);
   }
-  let aliases = object_property_get(object, property_name);
+  let result = object_property_get(object, property_name);
+  return result;
 }
