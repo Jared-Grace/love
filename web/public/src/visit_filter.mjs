e231ef807f0003321386e9d92d1d2feaa404264e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 21:45:00 -0400

    c visit visit_filter

---
+++ web/public/src/visit_filter.mjs
@@ -0,0 +1,6 @@
+import { each } from "./each.mjs";
+export function visit(node, children_get, on_each) {
+  on_each(node);
+  let children = children_get(node);
+  each(children, (c) => visit(c, children_get, on_each));
+}
