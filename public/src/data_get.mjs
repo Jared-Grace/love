427543394d35a30421cba753a3978d43bc98c0fb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:29:53 -0400

    i data_get

--- public/src/data_get.mjs
+++ public/src/data_get.mjs
@@ -1,1 +1,12 @@
-export function data_get(){}
+import {object_property_initialize} from './object_property_initialize.mjs';
+import {file_read_json} from './file_read_json.mjs';
+export async function data_get(property_name, value_initial) {
+  const file_path = "data.json";
+  let data = await file_read_json(file_path);
+  let value = object_property_initialize(data, property_name, value_initial);
+  return {
+    value,
+    file_path,
+    data
+  };
+}
