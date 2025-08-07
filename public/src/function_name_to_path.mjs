363df50bcef0b3a7640aec5cea18becfe7f7d1a3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:26:39 -0400

    function_imports_missing_add function_imports_missing_add

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -1,7 +1,8 @@
 import {function_name_to_base} from './function_name_to_base.mjs'
+import {functions_path} from './functions_path.mjs'
 import path from 'path';
 export function function_name_to_path(funcName) {
-    let second = ['public', 'src', function_name_to_base(funcName)];
+    let second = [functions_path(), function_name_to_base(funcName)];
     let joined = path.join(...second);
     return joined;
 }
