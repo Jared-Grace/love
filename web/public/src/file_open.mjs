724d5944fa84d27feebe804da1749ae24eb7908f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 15:42:01 -0400

    p

--- web/public/src/file_open.mjs
+++ web/public/src/file_open.mjs
@@ -1,16 +1,12 @@
 import { exec } from "child_process";
 
 export async function file_open(filePath) {
+    console.log(filePath)
   return await new Promise((resolve, reject) => {
     exec(`code "${filePath}"`, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error opening file: ${error.message}`);
         reject({ error });
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        reject({ stderr });
         return;
       }
       console.log(`Opened in VS Code: ${filePath}`);
