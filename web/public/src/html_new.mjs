54140bb8a38e617e965fcb4e0fc362f80833bf7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:12:42 -0400

    fa

--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -1,6 +1,8 @@
 import { file_overwrite } from "./file_overwrite.mjs";
 export async function html_new(name) {
-  await file_overwrite(name, `<!DOCTYPE html>
+  await file_overwrite(
+    name,
+    `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -9,5 +11,6 @@ export async function html_new(name) {
 <body>
 
 </body>
-</html>`);
+</html>`,
+  );
 }
