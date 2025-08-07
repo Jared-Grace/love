f70b1fa8f60a47ec34360aacc855320697ff1dcf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 22:08:39 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/js_visit_children_get.mjs
+++ web/public/src/js_visit_children_get.mjs
@@ -1,6 +1,8 @@
 import { object_properties } from "./object_properties.mjs";
 import { list_is } from "./list_is.mjs";
 import { string_is } from "./string_is.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_map } from "./list_map.mjs";
 export function js_visit_children_get(n) {
             if (list_is(n)) {
                 return n;
@@ -8,5 +10,5 @@ export function js_visit_children_get(n) {
             if (string_is(n)) {
                 return [];
             }
-            return object_properties(n);
+            return list_map(object_properties(n), p=>object_property_get(n,p));
         }
--- web/public/src/object_property_exists.mjs
+++ web/public/src/object_property_exists.mjs
@@ -1,3 +1,3 @@
 export function object_property_exists(object, property_name) {
-  return Object.hasOwn(object, property_name);
+  return object&&Object.hasOwn(object, property_name);
 }
