a80faf1e88187fc975792e80c79fa06f0fce0ded
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:19:30 -0400

    p

--- web/public/src/function_copy.mjs
+++ web/public/src/function_copy.mjs
@@ -1,9 +1,9 @@
 import { file_copy } from "./file_copy.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
+import { function_name_to_path_unalias } from "./function_name_to_path.mjs";
 import { file_open } from "./file_open.mjs";
 export async function function_copy(f_name_old, f_name_new) {
-  const f_path_new = await function_name_to_path(f_name_new);
-    const f_path_old = await function_name_to_path(f_name_old);
+  const f_path_new = await function_name_to_path_unalias(f_name_new);
+    const f_path_old = await function_name_to_path_unalias(f_name_old);
   await file_copy(f_path_old, f_path_new);
   await file_open(f_path_new);
 }
