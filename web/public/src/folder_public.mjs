7272a5312386bb6cc3703d2e398c803afb32f7c4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:12:42 -0400

    fa

--- web/public/src/folder_public.mjs
+++ web/public/src/folder_public.mjs
@@ -1,3 +1,3 @@
 export function folder_public() {
-    return "public";
+  return "public";
 }
--- web/public/src/path_join.mjs
+++ web/public/src/path_join.mjs
@@ -1,4 +1,4 @@
-
-import path from "path";export function path_join(second) {
-    return path.join(...second);
+import path from "path";
+export function path_join(second) {
+  return path.join(...second);
 }
