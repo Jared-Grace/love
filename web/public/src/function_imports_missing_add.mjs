13651013f8834cff834bc94bfceccd57811f859a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 12:57:19 -0400

    f function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -3,5 +3,8 @@ import { function_parse } from "./function_parse.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
-  js_visit_type(parsed, 'ImportDeclaration', (v) => {let {node}=v;console.log(node)});
+  js_visit_type(parsed, "ImportDeclaration", (v) => {
+    let { node } = v;
+    console.log(node);
+  });
 }
