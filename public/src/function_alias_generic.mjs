f67908a71136804ddb3a4b5777f7cdcd60a9f174
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:35:02 -0400

    a ac function_alias_change

--- public/src/function_alias_generic.mjs
+++ public/src/function_alias_generic.mjs
@@ -5,6 +5,7 @@ import {file_overwrite_json} from "./file_overwrite_json.mjs";
 export async function function_alias_generic(alias,lambda) {
   let a= await function_alias_add_generic(alias);
 lambda(a);
+let {file_path,data}=a;
   await file_overwrite_json(file_path, data);
   
 }
