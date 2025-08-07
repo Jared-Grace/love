cc74e779f97a9c6e7850ac71750cb1444a22e2ca
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 22:49:51 -0400

    n function_types

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,11 +1,11 @@
 import { file_js_parse } from "./file_js_parse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
-import { js_visit } from "./js_visit.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
 
 export async function function_imports_missing_add(f_name) {
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
-  js_visit(parsed, v => {
+  js_visit_type(parsed, v => {
 
   });
 }
