2020101a7abd8f20ebb25254fab8a9ca97e9c94d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 04:59:30 -0400

    function_type_first function_type_first Identifier

--- public/src/function_type_first.mjs
+++ public/src/function_type_first.mjs
@@ -1,11 +1,13 @@
 import { js_visit_nodes } from "./js_visit_nodes.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { function_parse } from "./function_parse.mjs";
+import { js_node_type } from "./js_node_type.mjs";
+import { list_first } from "./list_first.mjs";
 export async function function_type_first(f_name, type) {
   let parsed = await function_parse(f_name);
   let list = list_adder((la) => {
     js_visit_nodes(parsed, (node) => {
-      if (js_node_type(node) === type) la(node);
+      if (js_node_type(node) === type) {la(node);}
     });
   });
   return list_first(list);
