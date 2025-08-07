effda4ee5218c462cacd2c1ea58349ce964a8b7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:40:30 -0400

    f list_difference

--- web/public/src/list_difference.mjs
+++ web/public/src/list_difference.mjs
@@ -1,9 +1,13 @@
 import { each } from "./each.mjs";
+import { list_adder } from "./list_adder.mjs";
 import { list_includes } from "./list_includes.mjs";
 
 export function list_difference(list, other) {
-  each(list, (l) => {
-    if (!list_includes(other, l)) {
-    }
+  return list_adder((la) => {
+    each(list, (l) => {
+      if (!list_includes(other, l)) {
+        la(l);
+      }
+    });
   });
 }
