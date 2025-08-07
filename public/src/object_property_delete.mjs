d0b6e955924ae4cd7ee168395a1e14e61652134d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:27:13 -0400

    ia function_alias_add

--- public/src/object_property_delete.mjs
+++ public/src/object_property_delete.mjs
@@ -2,12 +2,9 @@ import { json_to } from "./json_to.mjs";
 import { error } from "./error.mjs";
 import { undefined_is } from "./undefined_is.mjs";
 
-export function object_property_get(object, property_name) {
-  let value = object[property_name];
-  if (undefined_is(value)) {
-    error(json_to({object,property_name}))
-  }
-  return value
+export function object_property_delete(object, property_name) {
+
+delete  object[property_name];
 }
 
 
