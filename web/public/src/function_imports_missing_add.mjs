e80055e595fca564e6407683b59155e99ed3bdab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:33:54 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,22 +1,24 @@
 import { function_parse } from "./function_parse.mjs";
 import { js_imports } from "./js_imports.mjs";
-import { list_adder } from "./list_adder.mjs";
+import { js_declaration_single } from "./js_declaration_single.mjs";
 import { list_single } from "./list_single.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_adder_unique } from "./list_adder_unique.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
   let imports = js_imports(parsed);
 
-let declarations=list_adder(la=>{
+    let declaration = js_declaration_single(parsed);
+   let identifiers= list_adder_unique(la=>{
 
-  js_visit_type(parsed, "ExportNamedDeclaration", (v) => {
-      let { node } = v;
-      
-      la(node);
-    });
-})
-let declaration=list_single(declarations)
+    js_visit_type(declaration,'Identifier',v=>{
+        let {node}=v
+        la(object_property_get(node,'name'))
+    })
+    })
 
-  console.log(declaration)
+  console.log(identifiers)
 }
+
---
+++ web/public/src/js_declaration_single.mjs
@@ -0,0 +1,16 @@
+import { list_adder } from "./list_adder.mjs";
+import { list_single } from "./list_single.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
+export function js_declaration_single(parsed) {
+    let nameds = list_adder(la => {
+
+        js_visit_type(parsed, "ExportNamedDeclaration", (v) => {
+            let { node } = v;
+
+            la(node);
+        });
+    });
+    let named = list_single(nameds);
+    let { declaration } = named;
+    return declaration;
+}
