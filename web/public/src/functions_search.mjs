9df1e2fced0a7dfb1cadf8e25ca234149ef1ab2b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:17:01 -0400

    ia functions_search

--- web/public/src/functions_search.mjs
+++ web/public/src/functions_search.mjs
@@ -1,9 +1,10 @@
+import {each_async} from './each_async.mjs';
 import {object_property_set} from './object_property_set.mjs';
 import {each} from './each.mjs';
 import {string_includes} from './string_includes.mjs';
 import {list_filter} from './list_filter.mjs';
 import {functions_names} from './functions_names.mjs';
-import { function_name_to_path } from './function_name_to_path.mjs';
+import {function_name_to_path} from './function_name_to_path.mjs';
 export function functions_search(search) {
   let f_names = functions_names();
   let f_names_search = list_filter(f_names, n => string_includes(n, search));
