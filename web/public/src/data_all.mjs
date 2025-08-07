c40cc7d0aab5c40d6d9bb9e20f0b20bb69654b72
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:36:51 -0400

    i data_all

--- web/public/src/data_all.mjs
+++ web/public/src/data_all.mjs
@@ -1,1 +1,9 @@
-export function data_all(){}
+import {file_read_json} from './file_read_json.mjs';
+export async function data_all() {
+  const file_path = "data.json";
+  let data = await file_read_json(file_path);
+  return {
+    data,
+    file_path
+  };
+}
