e539d094ba42acb74a0198ed720ca7805d88897a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:15:30 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/list_multiple_is.mjs
+++ web/public/src/list_multiple_is.mjs
@@ -1,4 +1,4 @@
 import { list_size } from "./list_size.mjs";
-export function list_size_1(list) {
-  return list_size(list) === 1;
+export function list_multiple_is(list) {
+  return list_size(list) >= 2;
 }
