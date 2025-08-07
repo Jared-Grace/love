2a0282c51a84dcd90a320affd4bac2a79c79f611
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:32:57 -0400

    n data_save

--- web/public/src/function_alias_add_generic.mjs
+++ web/public/src/function_alias_add_generic.mjs
@@ -4,9 +4,7 @@ import {object_property_exists} from "./object_property_exists.mjs";
 import {object_property_get} from "./object_property_get.mjs";
 import {file_read_json} from "./file_read_json.mjs";
 export async function function_alias_add_generic(alias) {
-  const property_name = "aliases";
-  const value_initial = {};
-  var {value: aliases, file_path, data} = await data_get(property_name, value_initial);
+  var {value: aliases, file_path, data} = await data_get("aliases", {});
   const exists = object_property_exists(aliases, alias);
   let unaliased = null;
   if (exists) {
