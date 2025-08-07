27043b1d48fb2a19c46728214f094edc0b7563ad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:00:30 -0400

    i functions_format

--- web/public/src/functions_format.mjs
+++ web/public/src/functions_format.mjs
@@ -1,1 +1,6 @@
-export function functions_format(){}
+import {function_format} from './function_format.mjs';
+import {functions_names} from './functions_names.mjs';
+import {list_map} from './list_map.mjs';
+export async function functions_format() {
+  list_map(await functions_names(), function_format);
+}
