3e818206d41c5cee2314135abc013ccc2186dc2b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 04:56:36 -0400

    f function_type_first

--- public/src/function_type_first.mjs
+++ public/src/function_type_first.mjs
@@ -1,11 +1,12 @@
 import { js_visit_nodes } from "./js_visit_nodes.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { function_parse } from "./function_parse.mjs";
-export async function function_type_first(f_name) {
+export async function function_type_first(f_name, type) {
   let parsed = await function_parse(f_name);
-  list_adder((la) => {
+  let list = list_adder((la) => {
     js_visit_nodes(parsed, (node) => {
-      la(node);
+      if (js_node_type(node) === type) la(node);
     });
   });
+  return list_first(list);
 }
