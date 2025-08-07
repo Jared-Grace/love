e9a3908d9f8c33944257cf35e1910336a797e079
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 15:32:05 -0400

    f function_read

--- web/public/src/function_parse.mjs
+++ web/public/src/function_parse.mjs
@@ -1,6 +1,6 @@
 import { file_js_parse } from "./file_js_parse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_parse(f_name){
-    const f_path = function_name_to_path(f_name);
+    const f_path =await function_name_to_path(f_name);
     let parsed = await file_js_parse(f_path);
     return parsed;}
--- web/public/src/function_read.mjs
+++ web/public/src/function_read.mjs
@@ -1,6 +1,6 @@
-import { function_name_to_path } from './function_name_to_path.mjs'
-import { file_read } from './file_read.mjs'
+import { function_name_to_path } from "./function_name_to_path.mjs";
+import { file_read } from "./file_read.mjs";
 export async function function_read(f_name) {
-
-  return await file_read(function_name_to_path(f_name))
+  const f_path = await function_name_to_path(f_name);
+  return await file_read(f_path);
 }
