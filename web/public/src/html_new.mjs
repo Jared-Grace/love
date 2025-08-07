48e1bcdc9028e0bcc731e930e1faf498f65ea540
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:16:46 -0400

    i html_new

--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -1,9 +1,12 @@
+import {folder_public_combine} from './folder_public_combine.mjs';
 import {file_name_html} from './file_name_html.mjs';
 import {folder_public} from './folder_public.mjs';
 import {path_join} from './path_join.mjs';
 import {file_overwrite} from "./file_overwrite.mjs";
 export async function html_new(name) {
-  await file_overwrite(path_join([folder_public(), file_name_html(name)]), `<!DOCTYPE html>
+  const file_name = file_name_html(name);
+  const file_path = folder_public_combine(file_name);
+  await file_overwrite(file_path, `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
