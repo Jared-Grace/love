8e89f0491f0fb7d9fd596631f5ac4fd743f12b3d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:01:27 -0400

    f js_visit_type

--- web/public/src/js_visit_type.mjs
+++ web/public/src/js_visit_type.mjs
@@ -1,14 +1,14 @@
-import { visit_filter } from "./visit_filter.mjs";
+import { js_visit } from "./js_visit.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 
 import { js_node_is } from "./js_node_is.mjs";
 
 export function js_visit_type(parsed, type, on_each) {
-  visit_filter(
-    parsed,
-    js_visit_children_get,
-    (n) => js_node_is(n) && js_node_type_is(n, type),
-    on_each,
-  );
+  js_visit(parsed, (v) => {
+    let { node } = v;
+    if (js_node_is(node) && js_node_type_is(node, type)) {
+      on_each(v);
+    }
+  });
 }
