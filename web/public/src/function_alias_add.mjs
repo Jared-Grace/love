9828fe239838b08ebf517d1c790c883584df5552
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:32:16 -0400

    ia function_alias_add

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -1,17 +1,14 @@
+import {function_alias_generic} from './function_alias_generic.mjs';
 import {error} from './error.mjs';
 import {object_property_set} from "./object_property_set.mjs";
 import {function_alias_add_generic} from "./function_alias_add_generic.mjs";
 import {file_overwrite_json} from "./file_overwrite_json.mjs";
 export async function function_alias_add(alias, f_name) {
-  let a= await function_alias_add_generic(alias);
-lambda(a);
-  await file_overwrite_json(file_path, data);
-  
-function lambda(a) {
-      let {exists, aliases, file_path, data, unaliased } = a;
-      if (exists) {
-        error();
-      }
-      object_property_set(aliases, alias, f_name);
-}
+  await function_alias_generic(alias, function lambda(a) {
+    let {exists, aliases, file_path, data, unaliased} = a;
+    if (exists) {
+      error();
+    }
+    object_property_set(aliases, alias, f_name);
+  });
 }
--- web/public/src/function_alias_generic.mjs
+++ web/public/src/function_alias_generic.mjs
@@ -1,1 +1,10 @@
-export function function_alias_generic(){}
+import {error} from './error.mjs';
+import {object_property_set} from "./object_property_set.mjs";
+import {function_alias_add_generic} from "./function_alias_add_generic.mjs";
+import {file_overwrite_json} from "./file_overwrite_json.mjs";
+export async function function_alias_generic(alias,lambda) {
+  let a= await function_alias_add_generic(alias);
+lambda(a);
+  await file_overwrite_json(file_path, data);
+  
+}
