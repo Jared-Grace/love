c3e114833d9739f59c5b86380ff471a0708d63bc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:29:55 -0400

    function_imports_missing_add function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,8 +1,22 @@
 import { function_parse } from "./function_parse.mjs";
-import { js_declarations } from "./js_declarations.mjs";
+import { js_imports } from "./js_imports.mjs";
+import { list_adder } from "./list_adder.mjs";
+import { list_single } from "./list_single.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
-  let declarations = js_declarations(parsed);
-  console.log(declarations)
+  let imports = js_imports(parsed);
+
+let declarations=list_adder(la=>{
+
+  js_visit_type(parsed, "ExportNamedDeclaration", (v) => {
+      let { node } = v;
+      
+      la(node);
+    });
+})
+let declaration=list_single(declarations)
+
+  console.log(declaration)
 }
