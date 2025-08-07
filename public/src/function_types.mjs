cd426150a27f04842de7e80e6410fddcc530ee8f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 23:12:56 -0400

    n list_adder

--- public/src/function_types.mjs
+++ public/src/function_types.mjs
@@ -1,5 +1,6 @@
 import { js_visit } from "./js_visit.mjs";
 import { function_parse } from "./function_parse.mjs";
+import { js_node_is } from "./js_node_is.mjs";
 
 export async function function_types(f_name) {
   let parsed = await function_parse(f_name);
