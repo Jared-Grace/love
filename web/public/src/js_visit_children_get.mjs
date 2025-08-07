10f9aff75957aed8c6a2e38b2d8d74fb86bb6b16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 21:58:38 -0400

    f js_visit

--- web/public/src/js_visit_children_get.mjs
+++ web/public/src/js_visit_children_get.mjs
@@ -1,1 +1,12 @@
-export function js_visit_children_get(){}
+import { object_properties } from "./object_properties.mjs";
+import { list_is } from "./list_is.mjs";
+import { string_is } from "./string_is.mjs";
+export function js_visit_children_get(n) {
+            if (list_is(n)) {
+                return n;
+            }
+            if (string_is(n)) {
+                return [];
+            }
+            return object_properties(n);
+        }
