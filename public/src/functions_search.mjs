d9f397182da85b93ce3aca5f88edb330a42f45a0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:10:51 -0400

    ia functions_search

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -1,4 +1,6 @@
+import {list_filter} from './list_filter.mjs';
 import {functions_names} from './functions_names.mjs';
 export function functions_search() {
   let f_names = functions_names();
+  list_filter();
 }
