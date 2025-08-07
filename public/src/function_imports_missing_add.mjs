2c2a2e2ffad388265c329f12f8ca5e06e013f637
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:10:37 -0400

    function_imports_missing_add function_imports_missing_add

--- public/src/function_imports_missing_add.mjs
+++ public/src/function_imports_missing_add.mjs
@@ -3,10 +3,12 @@ import { function_parse } from "./function_parse.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
+import { list_single } from "./list_single.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
   js_visit_type(parsed, "ImportDeclaration", (v) => {
+    let {node} = v
     let {specifiers,source} = node;
     if (!list_size_1(specifiers)) {
         return
@@ -19,5 +21,6 @@ export async function function_imports_missing_add(f_name) {
         return
     }
     let specifier = list_single(specifiers)
+    console.log(specifier)
   });
 }
