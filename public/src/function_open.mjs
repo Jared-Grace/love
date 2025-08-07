9a10db6eb87abb1f15c87bc7e28dcbd51bf00260
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:39:34 -0400

    data_all

--- public/src/function_open.mjs
+++ public/src/function_open.mjs
@@ -7,11 +7,11 @@ import { object_property_set } from './object_property_set.mjs';
 export async function function_open(f_name) {
   const f_path = await function_name_to_path_unalias(f_name);
   await file_open(f_path);
-    const property_name = "aliases";
+    const property_name = "function_current";
   var d = await data_get(property_name, null);
   let {
     data
   }=d
-  object_property_set(data,f_name)
+  object_property_set(data,f_name,f_name)
   await data_save(d);
 }
