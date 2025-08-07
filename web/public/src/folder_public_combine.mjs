1ac75cdfcf8974ffbcafd8e8a43f233d49f84e7f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:15:42 -0400

    i folder_public_combine

--- web/public/src/folder_public_combine.mjs
+++ web/public/src/folder_public_combine.mjs
@@ -1,1 +1,5 @@
-export function folder_public_combine(){}
+import {folder_public} from './folder_public.mjs';
+import {path_join} from './path_join.mjs';
+export function folder_public_combine(second) {
+  return path_join([folder_public(), second]);
+}
