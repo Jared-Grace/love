b7fbb987f138cffc63cb44fd97c608af24c15cf2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 21:57:26 -0400

    function_imports_missing_add function_imports_missing_add

--- public/src/function_imports_missing_add.mjs
+++ public/src/function_imports_missing_add.mjs
@@ -5,7 +5,7 @@ import { js_visit } from "./js_visit.mjs";
 export async function function_imports_missing_add(f_name) {
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
-  js_visit(parsed, n => {
-
+  js_visit(parsed, v => {
+console.log(v)
   });
 }
