c21f637491609763c7e9b528c103aca27fdeda86
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:27:46 -0400

    p

--- public/src/function_name_to_base.mjs
+++ public/src/function_name_to_base.mjs
@@ -1,3 +1,6 @@
+import { function_name_extension } from "./function_name_extension.mjs";
+
 export function function_name_to_base(funcName) {
-    return funcName + '.mjs';
+    return funcName + function_name_extension();
 }
+
