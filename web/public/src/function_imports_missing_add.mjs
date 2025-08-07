d477e0668bcaf154de007745690177cb081c9215
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:45:46 -0400

    f function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,6 +1,10 @@
 import { function_imports_missing } from "./function_imports_missing.mjs";
+import { js_parse } from "./js_parse.mjs";
 
 export async function function_imports_missing_add(f_name) {
-  let {imports_missing,parsed} = await function_imports_missing(f_name)
+  let { imports_missing, parsed } = await function_imports_missing(f_name);
+  let { body } = parsed;
+  let code = "import {} from ''";
+  js_parse(code);
   console.log(parsed);
 }
