c757f07d611909afea60ffb77b8624e40cbe24e9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:36:16 -0400

    n data_all

--- public/src/data_get.mjs
+++ public/src/data_get.mjs
@@ -1,8 +1,7 @@
 import {object_property_initialize} from './object_property_initialize.mjs';
 import {file_read_json} from './file_read_json.mjs';
 export async function data_get(property_name, value_initial) {
-  const file_path = "data.json";
-  let data = await file_read_json(file_path);
+  var { data, file_path } = await data_all();
   let value = object_property_initialize(data, property_name, value_initial);
   return {
     value,
@@ -10,3 +9,9 @@ export async function data_get(property_name, value_initial) {
     data
   };
 }
+async function data_all() {
+    const file_path = "data.json";
+    let data = await file_read_json(file_path);
+    return { data, file_path };
+}
+
