0a2f2a878b37f41198ab6c7b08f89bdfd2826f38
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 22:11:18 -0400

    c js_visit js_visit_type

--- web/public/src/js_visit_filter.mjs
+++ web/public/src/js_visit_filter.mjs
@@ -1,1 +1,2 @@
-export function js_visit_filter(){}
+import { js_node_is } from "./js_node_is.mjs";
+import { list_is } from "./list_is.mjs";export function js_visit_filter(n){return  js_node_is(n) || list_is(n)}
