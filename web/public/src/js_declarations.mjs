73c05a304d8a5ca90ad44c8ce88b1e26753cf36a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:26:35 -0400

    f js_declarations

--- web/public/src/js_declarations.mjs
+++ web/public/src/js_declarations.mjs
@@ -8,29 +8,28 @@ import { list_unique } from "./list_unique.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 
 export function js_declarations(parsed) {
-    return list_adder_unique((la) => {
-        js_visit_type(parsed, "ImportDeclaration", (v) => {
-            let { node } = v;
-            let { specifiers, source } = node;
-            if (!list_size_1(specifiers)) {
-                return;
-            }
-            if (!js_node_type_is(source, "Literal")) {
-                return;
-            }
-            let { value } = source;
-            if (!string_starts_with(value, ".")) {
-                return;
-            }
-            let specifier = list_single(specifiers);
-            let { imported, local } = specifier;
-            let both = [imported, local];
-            let mapped = list_map_property(both, "name");
-            let unique = list_unique(mapped);
-            ("the names should be the same");
-            let name = list_single(unique);
-            la(name);
-        });
+  return list_adder_unique((la) => {
+    js_visit_type(parsed, "ImportDeclaration", (v) => {
+      let { node } = v;
+      let { specifiers, source } = node;
+      if (!list_size_1(specifiers)) {
+        return;
+      }
+      if (!js_node_type_is(source, "Literal")) {
+        return;
+      }
+      let { value } = source;
+      if (!string_starts_with(value, ".")) {
+        return;
+      }
+      let specifier = list_single(specifiers);
+      let { imported, local } = specifier;
+      let both = [imported, local];
+      let mapped = list_map_property(both, "name");
+      let unique = list_unique(mapped);
+      ("the names should be the same");
+      let name = list_single(unique);
+      la(name);
     });
+  });
 }
-
