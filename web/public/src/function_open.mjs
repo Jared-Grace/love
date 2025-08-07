26e16c018ae695dee36cff0b8d0229f1f0b4d7e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:34:07 -0400

    i function_open

--- web/public/src/function_open.mjs
+++ web/public/src/function_open.mjs
@@ -1,9 +1,11 @@
+import {data_save} from './data_save.mjs';
+import {data_get} from './data_get.mjs';
 import {function_name_to_path_unalias} from './function_name_to_path_unalias.mjs';
 import {file_open} from "./file_open.mjs";
 import {function_name_to_path} from "./function_name_to_path.mjs";
 export async function function_open(f_name) {
-    
-      var { data} = await data_get("aliases", f_name);
+  var a = await data_get("aliases", f_name);
   const f_path = await function_name_to_path_unalias(f_name);
   await file_open(f_path);
+  await data_save(a);
 }
