d322f924dab9eab4ae5a5b8869eeb1881f2d4eff
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 23:40:17 -0400

    f function_types

--- web/public/src/list_adder_unique.mjs
+++ web/public/src/list_adder_unique.mjs
@@ -1,3 +1,5 @@
+import {list_adder} from './list_adder.mjs'
+import {list_unique} from './list_unique.mjs'
 export function list_adder_unique(lambda) {
   let items = list_adder(lambda);return list_unique(items)
 }
