d8ccb63b96c2a4d68cf8f9f230f8ce2a597ffc95
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 23:07:54 -0400

    p

--- web/public/src/js_visit_node.mjs
+++
@@ -1,8 +0,0 @@
-import { visit_filter } from "./visit_filter.mjs";
-import { js_visit_children_get } from "./js_visit_children_get.mjs";
-
-import { js_visit_filter } from "./js_visit_filter.mjs";
-
-export function js_visit(parsed, on_each) {
-  visit_filter(parsed, js_visit_children_get, js_visit_filter, on_each);
-}
