4094bc7279fd07d694dab59ca18214189e22f319
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 19:22:52 -0400

    p

--- web/public/src/function_run_git.mjs
+++ web/public/src/function_run_git.mjs
@@ -3,7 +3,7 @@ import { function_run } from "./function_run.mjs";
 
 export async function function_run_git(funcName, args) {
   let result = await function_run(funcName, args);
-  console.log("✅ Result:", result);
+  console.log("result:", result);
   await git_acp([funcName].concat(args).join(" "));
   return result;
 }
