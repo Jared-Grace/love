e3ced0a1a996da31f9d7c76cf14d4c1cb691b124
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 15:00:41 -0400

    p

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -2,6 +2,7 @@ import { each } from "./each.mjs";
 import { function_imports_missing } from "./function_imports_missing.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
+import { list_add_first } from "./list_add_first.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let { imports_missing, parsed } = await function_imports_missing(f_name);
@@ -14,8 +15,10 @@ export async function function_imports_missing_add(f_name) {
       function_name_to_base(import_missing) +
       "'";
     let statement = js_parse_statement(code);
-    console.log(statement);
+    list_add_first(body, statement);
   });
 }
+
+
 
 
