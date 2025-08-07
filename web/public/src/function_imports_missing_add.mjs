965ffc3a1e5d3f225f419c0c180ddc7b148c195e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 14:47:39 -0400

    f function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,10 +1,19 @@
+import { each } from "./each.mjs";
 import { function_imports_missing } from "./function_imports_missing.mjs";
 import { js_parse } from "./js_parse.mjs";
+import { function_name_to_base } from "./function_name_to_base.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let { imports_missing, parsed } = await function_imports_missing(f_name);
   let { body } = parsed;
-  let code = "import {} from ''";
-  js_parse(code);
-  console.log(parsed);
+  each(imports_missing, (import_missing) => {
+    let code =
+      "import { " +
+      import_missing +
+      " } from './" +
+      function_name_to_base(import_missing) +
+      "'";
+    let import_parsed = js_parse(code);
+    console.log(import_parsed);
+  });
 }
