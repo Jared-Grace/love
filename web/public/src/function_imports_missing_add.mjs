85e063185ac8e58432848e2db4a3d7c60bf3cb10
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 13:41:17 -0400

    f function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -2,16 +2,16 @@ import { function_parse } from "./function_parse.mjs";
 import { js_imports } from "./js_imports.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
+import { list_difference } from "./list_difference.mjs";
 
 export async function function_imports_missing_add(f_name) {
   let parsed = await function_parse(f_name);
   let imports = js_imports(parsed);
 
-    let declaration = js_declaration_single(parsed);
-   let identifiers= js_identifiers_names(declaration)
+  let declaration = js_declaration_single(parsed);
+  let identifiers = js_identifiers_names(declaration);
 
-  console.log(identifiers)
+  let missing = list_difference(identifiers, imports);
+
+  console.log(missing);
 }
-
-
-
