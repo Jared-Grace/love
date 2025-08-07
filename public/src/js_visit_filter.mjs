6327a420531e4ff367dac281d26bd80851c35d21
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 12:58:02 -0400

    f js_visit_filter

--- public/src/js_visit_filter.mjs
+++ public/src/js_visit_filter.mjs
@@ -1,2 +1,5 @@
 import { js_node_is } from "./js_node_is.mjs";
-import { list_is } from "./list_is.mjs";export function js_visit_filter(n){return  js_node_is(n) || list_is(n)}
+import { list_is } from "./list_is.mjs";
+export function js_visit_filter(n) {
+  return js_node_is(n) || list_is(n);
+}
