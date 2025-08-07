b2083eb43acf511a8597f6573fad5407b6f6a5e6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 22:04:27 -0400

    function_imports_missing_add function_imports_missing_add

--- public/src/js_visit.mjs
+++ public/src/js_visit.mjs
@@ -1,7 +1,8 @@
 import { visit_filter } from "./visit_filter.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_node_is } from "./js_node_is.mjs";
+import { list_is } from "./list_is.mjs";
 
 export function js_visit(parsed, on_each) {
-  visit_filter(parsed, js_visit_children_get, js_node_is,on_each);
+  visit_filter(parsed, js_visit_children_get, n => js_node_is(n) || list_is(n),on_each);
 }
