4b2e0b210785d2b105ffedc3017cb179286cd410
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-06 21:56:02 -0400

    p

--- web/public/src/visit_filter.mjs
+++ web/public/src/visit_filter.mjs
@@ -2,16 +2,20 @@ import { each } from "./each.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_pop } from "./list_pop.mjs";
 import { list_copy } from "./list_copy.mjs";
+import { error } from "./error.mjs";
 export function visit_filter(node, children_get, filter, on_each, stack) {
-  if (!filter(n)) {
+  if (!filter(node)) {
     return;
   }
-  list_add(stack, c);
+  list_add(stack, node);
   on_each({ node, stack });
   let children = children_get(node);
   each(children, (c) => {
     let copy = list_copy(stack);
     visit_filter(c, children_get, filter, on_each, copy);
   });
-  removed = list_pop(stack);
+ let removed = list_pop(stack);
+ if (removed !== node) {
+    error()
+ }
 }
