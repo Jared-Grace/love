0239b3f6904f071f0bf305f87a537caa5b44c81a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 04:35:53 -0400

    f js_visit_nodes

--- web/public/src/js_visit_nodes.mjs
+++ web/public/src/js_visit_nodes.mjs
@@ -1,7 +1,11 @@
-export function js_visit_nodes(parsed,on_each){    js_visit(parsed, (v) => {
-      let { node } = v;
-      if (!js_node_is(node)) {
-        return;
-      }
-      on_each((node));
-    });}
+import { js_visit } from "./js_visit.mjs";
+import { js_node_is } from "./js_node_is.mjs";
+export function js_visit_nodes(parsed, on_each) {
+  js_visit(parsed, (v) => {
+    let { node } = v;
+    if (!js_node_is(node)) {
+      return;
+    }
+    on_each(node);
+  });
+}
