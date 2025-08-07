062bd25b709da4c56bd11efdb41214026c51d992
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 17:15:53 -0400

    i functions_path

--- web/public/src/functions_path.mjs
+++ web/public/src/functions_path.mjs
@@ -1,11 +1,11 @@
-import { path_join } from "./path_join.mjs";
-import { folder_public } from "./folder_public.mjs";
+import {folder_public_combine} from './folder_public_combine.mjs';
+import {path_join} from "./path_join.mjs";
+import {folder_public} from "./folder_public.mjs";
 export function functions_path() {
-    const second = "src";
+  const second = "src";
   let joined = folder_public_combine(second);
   return joined;
 }
 function folder_public_combine(second) {
-    return path_join([folder_public(), second]);
+  return path_join([folder_public(), second]);
 }
-
