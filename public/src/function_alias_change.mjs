78a89c7672d71d12ed96e10fd21813c4e261b162
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 16:25:06 -0400

    c function_alias_add function_alias_change

---
+++ public/src/function_alias_change.mjs
@@ -0,0 +1,12 @@
+import {error} from './error.mjs';
+import {object_property_set} from "./object_property_set.mjs";
+import {function_alias_add_generic} from "./function_alias_add_generic.mjs";
+import {file_overwrite_json} from "./file_overwrite_json.mjs";
+export async function function_alias_add(alias, f_name) {
+  var {exists, aliases, file_path, data} = await function_alias_add_generic(alias);
+  if (exists) {
+    error();
+  }
+  object_property_set(aliases, alias, f_name);
+  await file_overwrite_json(file_path, data);
+}
