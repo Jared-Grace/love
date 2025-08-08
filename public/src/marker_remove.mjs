cb9370161f5e7fdb6bb3930209278cd22fd1ee66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:39:35 -0400

    i marker_remove

--- public/src/marker_remove.mjs
+++ public/src/marker_remove.mjs
@@ -1,3 +1,4 @@
+import {function_transform_marker} from './function_transform_marker.mjs';
 import {data_function_current_get} from './data_function_current_get.mjs';
 import {list_remove} from './list_remove.mjs';
 import {error} from './error.mjs';
@@ -16,10 +17,8 @@ import {list_is} from "./list_is.mjs";
 export async function marker_remove() {
   let f_name = await data_function_current_get();
   await function_transform_marker(f_name, lambda);
-
-    function lambda(a) {
-        let { stack2, stack1}=a;
-        list_remove( stack2, stack1);
-    }
+  function lambda(a) {
+    let {stack2, stack1} = a;
+    list_remove(stack2, stack1);
+  }
 }
-
