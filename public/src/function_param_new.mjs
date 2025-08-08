7720fafa0b5b8ec506119373ed70ee55b5dd4eb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 18:56:28 -0400

    mc function_transform

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -1,4 +1,6 @@
+import {function_transform} from './function_transform.mjs';
 import {marker} from './marker.mjs';
 export function function_param_new(f_name) {
   marker();
+  function_transform(f_name2, lambda);
 }
