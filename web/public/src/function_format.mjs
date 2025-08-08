1bd4603a4ba5d2ac03c6b2d0d0bbb5048e7afc07
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 21:53:25 -0400

    i function_format

--- web/public/src/function_format.mjs
+++ web/public/src/function_format.mjs
@@ -1,9 +1,10 @@
-import { function_name_to_path } from "./function_name_to_path.mjs";
-import { file_read } from "./file_read.mjs";
-import { js_format } from "./js_format.mjs";
-import { file_overwrite } from "./file_overwrite.mjs";
+import {function_name_to_path_unalias} from './function_name_to_path_unalias.mjs';
+import {function_name_to_path} from "./function_name_to_path.mjs";
+import {file_read} from "./file_read.mjs";
+import {js_format} from "./js_format.mjs";
+import {file_overwrite} from "./file_overwrite.mjs";
 export async function function_format(f_name) {
-  const f_path = await function_name_to_path(f_name);
+  const f_path = await function_name_to_path_unalias(f_name);
   let r = await file_read(f_path);
   let formatted = await js_format(r);
   await file_overwrite(f_path, formatted);
