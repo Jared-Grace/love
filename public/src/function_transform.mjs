cc186c10fb4a54bcb5560926139aa6de3fb60048
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:54:41 -0400

    i function_transform

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,9 +1,10 @@
-import { function_unparse } from "./function_unparse.mjs";
-import { function_parse } from "./function_parse.mjs";
+import {file_js_unparse} from './file_js_unparse.mjs';
+import {function_unparse} from "./function_unparse.mjs";
+import {function_parse} from "./function_parse.mjs";
 export async function function_transform(f_name, lambda) {
-  let parsed= await function_parse(f_name);
+  let parsed = await function_parse(f_name);
   let {ast} = parsed;
   lambda(ast);
   await function_unparse(parsed);
-    if (false) await file_js_unparse(parsed);
+  if (false) await file_js_unparse(parsed);
 }
