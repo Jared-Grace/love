2643ba49205a7af17d22c50650f40e3aa37afde4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:07:23 -0400

    n list_join

--- web/public/src/js_code_call.mjs
+++ web/public/src/js_code_call.mjs
@@ -1,6 +1,6 @@
 import {js_code_wrap_parenthesis} from './js_code_wrap_parenthesis.mjs';
 export function js_code_call(fn_name) {
   let args = [];
+  return fn_name + js_code_wrap_parenthesis('');
   let joined = list_join(args);
-  return fn_name + js_code_wrap_parenthesis(joined);
 }
