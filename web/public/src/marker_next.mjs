29904a3caab755e88f3d6914f144c8a059f81583
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:59:36 -0400

    i marker_next

--- web/public/src/marker_next.mjs
+++ web/public/src/marker_next.mjs
@@ -1,3 +1,6 @@
+import {js_unparse} from './js_unparse.mjs';
+import {list_get} from './list_get.mjs';
+import {list_adder_async} from './list_adder_async.mjs';
 import {log} from './log.mjs';
 import {list_insert} from './list_insert.mjs';
 import {list_remove} from './list_remove.mjs';
@@ -6,15 +9,14 @@ import {data_function_current_get} from './data_function_current_get.mjs';
 import {list_index_of} from './list_index_of.mjs';
 export async function marker_next() {
   let f_name = await data_function_current_get();
-  return list_adder_async(async la=>{
-
-  await function_transform_marker(f_name, lambda);
-  function lambda(a) {
-    let {stack2, stack1} = a;
-    let index = list_index_of(stack2, stack1);
-    list_remove(stack2, stack1);
-    let index_new = index + 1;
-    list_insert(stack2, index_new, stack1);
-  }
-  })
+  return list_adder_async(async la => {
+    await function_transform_marker(f_name, lambda);
+    function lambda(a) {
+      let {stack2, stack1} = a;
+      let index = list_index_of(stack2, stack1);
+      let index_new = index + 1;
+      let next = list_get(stack2, index_new);
+      la(js_unparse(next));
+    }
+  });
 }
