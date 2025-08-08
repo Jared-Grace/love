232f7cb7e137393fdbbc566d13ce97958f24b21f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 15:43:37 -0400

    n marker_next_index

--- web/public/src/marker_next_get.mjs
+++ web/public/src/marker_next_get.mjs
@@ -1,9 +1,15 @@
 import {list_get} from './list_get.mjs';
 import {list_index_of} from './list_index_of.mjs';
 export function marker_next_get(a) {
-  let {stack2, stack1} = a;
-  let index = list_index_of(stack2, stack1);
-  let index_new = index + 1;
+    let index_new = marker_next_index(a);
+    let {stack2} = a;
   let next = list_get(stack2, index_new);
   return next;
 }
+function marker_next_index(a) {
+  let {stack2, stack1} = a;
+    let index = list_index_of(stack2, stack1);
+    let index_new = index + 1;
+    return index_new;
+}
+
