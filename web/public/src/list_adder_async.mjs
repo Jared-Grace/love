a1dba36486549ba47d8e01ade84b6abe3695eef7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:00:27 -0400

    a mn marker_next

--- web/public/src/list_adder_async.mjs
+++ web/public/src/list_adder_async.mjs
@@ -1,6 +1,6 @@
 import { list_add } from "./list_add.mjs";
 
-export async function list_adder(lambda) {
+export async function list_adder_async(lambda) {
   let list = [];
   await lambda(function list_adder_inner(item) {
     list_add(list, item);
