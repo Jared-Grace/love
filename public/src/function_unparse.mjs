dd9df994a7970050a86f84e5543be6cecc4629e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 22:10:07 -0400

    i function_unparse

--- public/src/function_unparse.mjs
+++ public/src/function_unparse.mjs
@@ -1,1 +1,6 @@
-export function function_unparse(){}
+import {file_js_unparse} from './file_js_unparse.mjs';
+import {function_name_to_path_unalias} from './function_name_to_path_unalias.mjs';
+export async function function_unparse(f_name, parsed) {
+  const f_path = await function_name_to_path_unalias(f_name);
+  await file_js_unparse(f_path, parsed);
+}
