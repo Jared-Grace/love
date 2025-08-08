2d69a794af7f2c3afeaf435fc8bc74f279152608
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 15:44:00 -0400

    i marker_next_index

--- web/public/src/marker_next_index.mjs
+++ web/public/src/marker_next_index.mjs
@@ -1,1 +1,7 @@
-export function marker_next_index(){}
+import {list_index_of} from './list_index_of.mjs';
+export function marker_next_index(a) {
+  let {stack2, stack1} = a;
+  let index = list_index_of(stack2, stack1);
+  let index_new = index + 1;
+  return index_new;
+}
