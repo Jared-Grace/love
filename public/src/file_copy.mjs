bc319e84c23b7b0b2f94265db4471f8ac903a746
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:47:39 -0400

    i file_copy

--- public/src/file_copy.mjs
+++ public/src/file_copy.mjs
@@ -1,9 +1,7 @@
-import { error } from "./error.mjs";
-
+import {assert_file_exists_not} from './assert_file_exists_not.mjs';
+import {error} from "./error.mjs";
 export async function file_copy(file_path_old, file_path_new) {
   await assert_file_exists_not(file_path_new);
   let fs = await import("fs");
   await fs.promises.copyFile(file_path_old, file_path_new);
 }
-
-
