521760d2397f57f2e4b82aec7eb9a6878fad3040
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 23:12:23 -0400

    n list_index_of

--- web/public/src/list_remove.mjs
+++ web/public/src/list_remove.mjs
@@ -1,5 +1,9 @@
 export function list_remove(arr, item) {
-  const index = arr.indexOf(item);
+  const index = list_index_of(arr, item);
   if (index !== -1) {error()}
   arr.splice(index, 1);
 }
+function list_index_of(arr, item) {
+    return arr.indexOf(item);
+}
+
