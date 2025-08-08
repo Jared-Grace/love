23731a84efff65675982132942cbc86df52e4e75
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:23:38 -0400

    i data_function_current_get

--- web/public/src/data_function_current_get.mjs
+++ web/public/src/data_function_current_get.mjs
@@ -1,1 +1,9 @@
-export function data_function_current_get(){}
+import {object_property_get} from './object_property_get.mjs';
+import {data_get} from './data_get.mjs';
+export async function data_function_current_get() {
+  const property_name = "function_current";
+  var d = await data_get(property_name, null);
+  let {data} = d;
+  let f_name = object_property_get(data, property_name);
+  return f_name;
+}
