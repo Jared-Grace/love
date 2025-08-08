7936f06b6728d759bfb465effbe053915b6f847b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 21:55:11 -0400

    i marker_top

--- public/src/marker_top.mjs
+++ public/src/marker_top.mjs
@@ -1,1 +1,6 @@
-export function marker_top(){}
+import {js_declaration_single} from './js_declaration_single.mjs';
+import {function_parse} from './function_parse.mjs';
+export function marker_top(f_name) {
+  let ast = function_parse(f_name);
+  js_declaration_single(ast);
+}
