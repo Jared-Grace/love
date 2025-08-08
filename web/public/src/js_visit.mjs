a2d0fe9701c47759b2f6819ec31517423ecd9e7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 22:01:39 -0400

    n promise_is

--- web/public/src/js_visit.mjs
+++ web/public/src/js_visit.mjs
@@ -4,5 +4,6 @@ import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_visit_filter } from "./js_visit_filter.mjs";
 
 export function js_visit(parsed, on_each) {
+    
   visit_filter(parsed, js_visit_children_get, js_visit_filter, on_each);
 }
