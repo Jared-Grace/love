15f5404613164c7b6ebe0b189c7774bfc4891fac
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 18:57:29 -0400

    o mc

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -2,5 +2,5 @@ import {function_transform} from './function_transform.mjs';
 import {marker} from './marker.mjs';
 export function function_param_new(f_name) {
   marker();
-  function_transform(f_name2, lambda);
+  function_transform(f_name, lambda);
 }
