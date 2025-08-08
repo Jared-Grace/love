5a10ca1bb8670e7a31409e0a311eff82ee05c4f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 18:47:15 -0400

    m

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -1,4 +1,6 @@
+import {marker} from './marker.mjs';
 import {function_transform} from './function_transform.mjs';
 export function function_param_new(f_name) {
+  marker();
   function_transform();
 }
