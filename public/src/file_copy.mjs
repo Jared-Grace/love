e069604ece37cb0ddadd0f030de5d6596db39ea1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:45:52 -0400

    f file_copy

--- public/src/file_copy.mjs
+++ public/src/file_copy.mjs
@@ -1,4 +1,9 @@
+import { error } from "./error.mjs";
+
 export async function file_copy(file_path_old, file_path_new) {
+  if (await file_exists(file_path_new)) {
+    error();
+  }
   let fs = await import("fs");
   await fs.promises.copyFile(file_path_old, file_path_new);
 }
