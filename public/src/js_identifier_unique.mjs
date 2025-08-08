e69fc766ace837f32eef2d2dd1452dfc757145a9
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 18:58:53 -0400

    i js_identifier_unique

--- public/src/js_identifier_unique.mjs
+++ public/src/js_identifier_unique.mjs
@@ -1,1 +1,13 @@
-export function js_identifier_unique(  ){}
+import {list_add} from './list_add.mjs';
+import {list_includes} from './list_includes.mjs';
+export function js_identifier_unique(existing, name) {
+  let result = null;
+  let attempt = 1;
+  do {
+    const suffix = attempt === 1 ? "" : attempt;
+    result = name + suffix;
+    attempt++;
+  } while (list_includes(existing, result));
+  list_add(existing, result);
+  return result;
+}
