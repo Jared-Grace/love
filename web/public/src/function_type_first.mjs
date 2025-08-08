5cc389133364575b43296f8254bef69ba1f0d677
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:39:02 -0400

    p

--- web/public/src/function_type_first.mjs
+++ web/public/src/function_type_first.mjs
@@ -4,7 +4,7 @@ import { function_parse } from "./function_parse.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { list_first } from "./list_first.mjs";
 export async function function_type_first(f_name, type) {
-  let ast = await function_parse(f_name);
+  let {ast} = await function_parse(f_name);
   let list = list_adder((la) => {
     js_visit_nodes(ast, (node) => {
       if (js_node_type(node) === type) {
