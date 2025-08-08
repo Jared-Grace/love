ec55565fe23f90448c7b808811973dc9ad3de238
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 23:12:46 -0400

    i list_remove

--- web/public/src/list_remove.mjs
+++ web/public/src/list_remove.mjs
@@ -1,9 +1,9 @@
+import {error} from './error.mjs';
+import {list_index_of} from './list_index_of.mjs';
 export function list_remove(arr, item) {
   const index = list_index_of(arr, item);
-  if (index !== -1) {error()}
+  if (index !== -1) {
+    error();
+  }
   arr.splice(index, 1);
 }
-function list_index_of(arr, item) {
-    return arr.indexOf(item);
-}
-
