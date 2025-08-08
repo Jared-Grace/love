6ea10a8b854a5759796bf48be1e4fd209c53e245
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:38:03 -0400

    p

--- public/src/function_parse.mjs
+++ public/src/function_parse.mjs
@@ -3,6 +3,6 @@ import { file_js_parse } from "./file_js_parse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_parse(f_name) {
   const f_path = await function_name_to_path_unalias(f_name);
-  let parsed = await file_js_parse(f_path);
-  return parsed;
+  let ast = await file_js_parse(f_path);
+  return ast;
 }
