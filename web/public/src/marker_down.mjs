c08d85605084ec77e04f2dca1a15650c814bf26f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:53:38 -0400

    i marker_down

--- web/public/src/marker_down.mjs
+++ web/public/src/marker_down.mjs
@@ -1,11 +1,15 @@
+import {list_insert} from './list_insert.mjs';
 import {list_remove} from './list_remove.mjs';
 import {function_transform_marker} from './function_transform_marker.mjs';
 import {data_function_current_get} from './data_function_current_get.mjs';
+import {list_index_of} from './list_index_of.mjs';
 export async function marker_down() {
   let f_name = await data_function_current_get();
   await function_transform_marker(f_name, lambda);
   function lambda(a) {
     let {stack2, stack1} = a;
+    let index = list_index_of(stack2, stack1);
     list_remove(stack2, stack1);
+    list_insert();
   }
 }
