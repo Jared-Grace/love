d597bf995e8b9f1d9427a8235cd5620a42d19dd6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 22:41:33 -0400

    n function_transform

--- public/src/marker_top.mjs
+++ public/src/marker_top.mjs
@@ -10,12 +10,20 @@ import {function_parse} from './function_parse.mjs';
 import {list_add} from './list_add.mjs';
 export async function marker_top(f_name) {
   marker();
-  let ast = await function_parse(f_name);
-  let declaration = js_declaration_single(ast);
-  let {body} = declaration;
-  let {body: body2} = body;
-  let code = js_code_call_statement(marker.name);
-  let parsed = js_parse_statement(code);
-  list_add_first(body2, parsed);
-  await function_unparse(f_name, ast);
+  await function_transform(f_name, lambda);
+function lambda(ast) {
+    let declaration = js_declaration_single(ast);
+    let { body } = declaration;
+    let { body: body2 } = body;
+    let code = js_code_call_statement(marker.name);
+    let parsed = js_parse_statement(code);
+    list_add_first(body2, parsed);
 }
+}
+
+async function function_transform(f_name, lambda) {
+    let ast = await function_parse(f_name);
+    lambda(ast);
+    await function_unparse(f_name, ast);
+}
+
