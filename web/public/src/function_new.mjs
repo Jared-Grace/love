615f7112384b160e0ac9d52e6908386427c26599
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:49:41 -0400

    i function_new

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -1,3 +1,4 @@
+import {assert_file_exists_not} from './assert_file_exists_not.mjs';
 import {function_open} from './function_open.mjs';
 import {js_code_call} from './js_code_call.mjs';
 import {file_overwrite} from "./file_overwrite.mjs";
@@ -5,6 +6,8 @@ import {function_name_to_path} from "./function_name_to_path.mjs";
 import {file_open} from "./file_open.mjs";
 export async function function_new(f_name) {
   const f_path = function_name_to_path(f_name);
-  await file_overwrite(f_path, "export function " + js_code_call(f_name) + "{}");
+  const contents = "export function " + js_code_call(f_name) + "{}";
+  await assert_file_exists_not(f_path);
+  await file_overwrite(f_path, contents);
   await function_open(f_name);
 }
