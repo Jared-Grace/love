0bd4120a77b9d5d94a12bf44eadeb81bddf9266a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:08:10 -0400

    n list_join_comma_space

--- web/public/src/js_code_call.mjs
+++ web/public/src/js_code_call.mjs
@@ -2,5 +2,9 @@ import {js_code_wrap_parenthesis} from './js_code_wrap_parenthesis.mjs';
 export function js_code_call(fn_name) {
   let args = [];
   return fn_name + js_code_wrap_parenthesis('');
-  let joined = list_join(args);
+  let joined = list_join_comma_space(args);
 }
+function list_join_comma_space(args) {
+    return list_join(args, ', ');
+}
+
---
+++ web/public/src/list_join_comma_space.mjs
@@ -0,0 +1,1 @@
+export function list_join_comma_space(  ){}
