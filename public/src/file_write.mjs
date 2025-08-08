e45bd1d99bf8f398c4cf5e3320ddc149b47c8489
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:50:32 -0400

    i file_write

--- public/src/file_write.mjs
+++ public/src/file_write.mjs
@@ -1,1 +1,6 @@
-export function file_write(){}
+import {file_overwrite} from './file_overwrite.mjs';
+import {assert_file_exists_not} from './assert_file_exists_not.mjs';
+export async function file_write(f_path, contents) {
+  await assert_file_exists_not(f_path);
+  await file_overwrite(f_path, contents);
+}
