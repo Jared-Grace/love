944d6d1ae057aefeac029efd7fa0366727326552
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:27:04 -0400

    i function_parse

--- web/public/src/function_name_to_path_unalias.mjs
+++ web/public/src/function_name_to_path_unalias.mjs
@@ -1,7 +1,7 @@
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 export async function function_name_to_path_unalias(f_name) {
-  f_name = await function_name_unalias(f_name);
-    const f_path = function_name_to_path(f_name);
-  return {f_path};
+  let unaliased = await function_name_unalias(f_name);
+    const f_path = function_name_to_path(unaliased);
+  return {f_path,unaliased};
 }
--- web/public/src/function_parse.mjs
+++ web/public/src/function_parse.mjs
@@ -1,8 +1,11 @@
-import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
-import { file_js_parse } from "./file_js_parse.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
+import {function_name_to_path_unalias} from "./function_name_to_path_unalias.mjs";
+import {file_js_parse} from "./file_js_parse.mjs";
+import {function_name_to_path} from "./function_name_to_path.mjs";
+import {object_merge} from "./object_merge.mjs";
 export async function function_parse(f_name) {
-  const {f_path} = await function_name_to_path_unalias(f_name);
+  const {f_path, unaliased} = await function_name_to_path_unalias(f_name);
   let parsed = await file_js_parse(f_path);
-  return parsed;
+  return object_merge({
+    unaliased
+  }, parsed);
 }
