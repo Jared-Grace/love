c3ea574a272cfc6cfe69e0a1211846ce7f269e7a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 22:10:15 -0400

    i function_imports_missing_add

--- web/public/src/function_imports_missing_add.mjs
+++ web/public/src/function_imports_missing_add.mjs
@@ -1,28 +1,22 @@
-import { each } from "./each.mjs";
-import { function_imports_missing } from "./function_imports_missing.mjs";
-import { function_name_to_base } from "./function_name_to_base.mjs";
-import { js_parse_statement } from "./js_parse_statement.mjs";
-import { list_add_first } from "./list_add_first.mjs";
-import { file_js_unparse } from "./file_js_unparse.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
-
+import {function_unparse} from './function_unparse.mjs';
+import {each} from "./each.mjs";
+import {function_imports_missing} from "./function_imports_missing.mjs";
+import {function_name_to_base} from "./function_name_to_base.mjs";
+import {js_parse_statement} from "./js_parse_statement.mjs";
+import {list_add_first} from "./list_add_first.mjs";
+import {file_js_unparse} from "./file_js_unparse.mjs";
+import {function_name_to_path} from "./function_name_to_path.mjs";
 export async function function_imports_missing_add(f_name) {
-  let { imports_missing, parsed } = await function_imports_missing(f_name);
-  let { body } = parsed;
-  each(imports_missing, (import_missing) => {
-    let code =
-      "import { " +
-      import_missing +
-      " } from './" +
-      function_name_to_base(import_missing) +
-      "'";
+  let {imports_missing, parsed} = await function_imports_missing(f_name);
+  let {body} = parsed;
+  each(imports_missing, import_missing => {
+    let code = "import { " + import_missing + " } from './" + function_name_to_base(import_missing) + "'";
     let statement = js_parse_statement(code);
     list_add_first(body, statement);
   });
   await function_unparse(f_name, parsed);
 }
 async function function_unparse(f_name, parsed) {
-    const f_path = await function_name_to_path(f_name);
-    await file_js_unparse(f_path, parsed);
+  const f_path = await function_name_to_path(f_name);
+  await file_js_unparse(f_path, parsed);
 }
-
