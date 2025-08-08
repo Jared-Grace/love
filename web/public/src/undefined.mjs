5300e81dc18a1084cf281a3bba9a4b1cba60eae3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 15:52:23 -0400

    i i

--- web/public/src/undefined.mjs
+++ web/public/src/undefined.mjs
@@ -1,29 +1,3 @@
-import {object_merge} from './object_merge.mjs';
-import {function_parse} from "./function_parse.mjs";
-import {js_imports} from "./js_imports.mjs";
-import {js_declaration_single} from "./js_declaration_single.mjs";
-import {js_identifiers_names} from "./js_identifiers_names.mjs";
-import {list_difference} from "./list_difference.mjs";
-import {list_concat} from "./list_concat.mjs";
-import {functions_names} from "./functions_names.mjs";
-import {list_intersect} from "./list_intersect.mjs";
-export async function function_imports_missing(f_name) {
-  let parsed = await function_parse_declaration(f_name, ast);
-  let {ast,declaration} = parsed;
-  let imports = js_imports(ast);
-  let identifiers = js_identifiers_names(declaration);
-  let imports_self = list_concat(imports, [f_name]);
-  let missing = list_difference(identifiers, imports_self);
-  let f_names = functions_names();
-  let imports_missing = list_intersect(missing, f_names);
-  return object_merge({
-    imports_missing
-  }, parsed);
+export function undefined() {
+    'this is to prevent undefined from being created'
 }
-async function function_parse_declaration(f_name) {
-    let parsed = await function_parse(f_name);
-  let {ast} = parsed;
-    let declaration = js_declaration_single(ast);
-    return object_merge({ declaration, parsed });
-}
-
