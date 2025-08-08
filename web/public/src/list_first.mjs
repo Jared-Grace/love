95eca622ea3b92e6218f52c4eb148b2aa6803a9c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 23:02:45 -0400

    f list_get_end

--- web/public/src/list_first.mjs
+++ web/public/src/list_first.mjs
@@ -3,6 +3,3 @@ export function list_first(list) {
   const index = 0;
   return list_get(list, index);
 }
-function list_get(list, index) {
-  return list[index];
-}
--- web/public/src/list_get_end.mjs
+++ web/public/src/list_get_end.mjs
@@ -1,1 +1,6 @@
-export function list_get_end(stack, index_from_end){}
+import { list_get } from "./list_get.mjs";
+import { list_size } from "./list_size.mjs";
+
+export function list_get_end(stack, index_from_end) {
+  list_get(stack, list_size(stack) - 1);
+}
