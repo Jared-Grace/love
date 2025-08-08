f103669b1f9a731326e4822719fad7efcff80d66
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:43:11 -0400

    i function_imports_missing

--- web/public/src/object_merge.mjs
+++ web/public/src/object_merge.mjs
@@ -2,10 +2,14 @@ import {error} from './error.mjs';
 import {each} from "./each.mjs";
 import {object_properties} from "./object_properties.mjs";
 import {object_property_exists} from "./object_property_exists.mjs";
+import { object_property_set } from './object_property_set.mjs';
+import { object_property_get } from './object_property_get.mjs';
 export function object_merge(to, from) {
   each(object_properties(from), p => {
     if (object_property_exists(to, p)) {
       error();
     }
+    let value=object_property_get(from,p)
+    object_property_set(to,p,value)
   });
 }
