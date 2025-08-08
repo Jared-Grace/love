e80fb573fbad66fd383590904ed1af682be5dcdd
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:10:30 -0400

    i js_code_call

--- web/public/src/js_code_call.mjs
+++ web/public/src/js_code_call.mjs
@@ -1,11 +1,11 @@
+import {js_code_call_args} from './js_code_call_args.mjs';
 import {list_join_comma_space} from './list_join_comma_space.mjs';
 import {js_code_wrap_parenthesis} from './js_code_wrap_parenthesis.mjs';
 export function js_code_call(fn_name) {
   let args = [];
-  return js_code_call_args(fn_name,args);
+  return js_code_call_args(fn_name, args);
 }
-function js_code_call_args(fn_name,args) {
-    let joined = list_join_comma_space(args);
-    return fn_name + js_code_wrap_parenthesis(joined);
+function js_code_call_args(fn_name, args) {
+  let joined = list_join_comma_space(args);
+  return fn_name + js_code_wrap_parenthesis(joined);
 }
-
