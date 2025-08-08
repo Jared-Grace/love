6cf8bbb1ea2dd014b53c25eb3c43b1ea45e2eabc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 15:47:45 -0400

    i mn

--- web/public/src/marker_next_index.mjs
+++ web/public/src/marker_next_index.mjs
@@ -1,7 +1,8 @@
 import {list_index_of} from './list_index_of.mjs';
+import { object_merge } from './object_merge.mjs';
 export function marker_next_index(a) {
   let {stack2, stack1} = a;
-  let index = list_index_of(stack2, stack1);
-  let index_new = index + 1;
-  return index_new;
+  let index_old = list_index_of(stack2, stack1);
+  let index = index_old + 1;
+  return object_merge({index},a);
 }
