50656f2af069bbea69a4663f7dd0ccb8d1932d47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 15:42:02 -0400

    i marker_call

--- web/public/src/marker_call.mjs
+++ web/public/src/marker_call.mjs
@@ -1,1 +1,15 @@
-export function marker_call(){}
+import {js_unparse} from './js_unparse.mjs';
+import {marker_next_get} from './marker_next_get.mjs';
+import {function_transform_marker} from './function_transform_marker.mjs';
+import {list_adder_async} from './list_adder_async.mjs';
+import {data_function_current_get} from './data_function_current_get.mjs';
+export async function marker_call() {
+  let f_name = await data_function_current_get();
+  return list_adder_async(async la => {
+    await function_transform_marker(f_name, lambda);
+    function lambda(a) {
+      let next = marker_next_get(a);
+      la(js_unparse(next));
+    }
+  });
+}
