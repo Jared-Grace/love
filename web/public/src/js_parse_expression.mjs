0992caa6722ce10a702b6b8425c230e93bc44c3b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:35:41 -0400

    i js_parse_expression

--- web/public/src/js_parse_expression.mjs
+++ web/public/src/js_parse_expression.mjs
@@ -1,1 +1,7 @@
-export function js_parse_expression(){}
+import {object_property_get} from './object_property_get.mjs';
+import {js_parse_statement} from './js_parse_statement.mjs';
+export function js_parse_expression(init_code) {
+  let statement = js_parse_statement(init_code);
+  let expression = object_property_get(statement, 'expression');
+  return expression;
+}
--- web/public/src/marker_assign_replace.mjs
+++ web/public/src/marker_assign_replace.mjs
@@ -32,9 +32,5 @@ export async function marker_assign_replace(init_code) {
     }
   });
 }
-function js_parse_expression(init_code) {
-    let statement = js_parse_statement(init_code);
-    let expression = object_property_get(statement, 'expression');
-    return expression;
-}
+
 
