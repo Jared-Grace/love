6af36c150480ac443748418cd255ebe98c6671fc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:52:13 -0400

    i marker_down

--- public/src/marker_down.mjs
+++ public/src/marker_down.mjs
@@ -1,1 +1,11 @@
-export function marker_down(){}
+import {list_remove} from './list_remove.mjs';
+import {function_transform_marker} from './function_transform_marker.mjs';
+import {data_function_current_get} from './data_function_current_get.mjs';
+export async function marker_down() {
+  let f_name = await data_function_current_get();
+  await function_transform_marker(f_name, lambda);
+  function lambda(a) {
+    let {stack2, stack1} = a;
+    list_remove(stack2, stack1);
+  }
+}
