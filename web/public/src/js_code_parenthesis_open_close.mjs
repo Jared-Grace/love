adc0cb6c178fedb7d599599f268bdb808596179a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:05:48 -0400

    i js_code_parenthesis_open_close

--- web/public/src/js_code_parenthesis_open_close.mjs
+++ web/public/src/js_code_parenthesis_open_close.mjs
@@ -1,3 +1,4 @@
+import {js_code_wrap_parenthesis} from './js_code_wrap_parenthesis.mjs';
 export function js_code_parenthesis_open_close() {
-    return "()";
+  return js_code_wrap_parenthesis('');
 }
