27ffa0d1edce67bce474c448a652b8ef364d0732
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 22:01:39 -0400

    n promise_is

--- web/public/src/marker_top.mjs
+++ web/public/src/marker_top.mjs
@@ -1,8 +1,8 @@
 import {log} from './log.mjs';
 import {js_declaration_single} from './js_declaration_single.mjs';
 import {function_parse} from './function_parse.mjs';
-export function marker_top(f_name) {
-  let ast = function_parse(f_name);
+export async function marker_top(f_name) {
+  let ast = await function_parse(f_name);
   let declaration = js_declaration_single(ast);
   log(declaration);
 }
