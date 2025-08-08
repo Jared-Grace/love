192825d218b571e726043d2a46d0fc56dfe3f681
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 18:54:06 -0400

    i js_imports_missing

--- public/src/js_imports_missing_add.mjs
+++ public/src/js_imports_missing_add.mjs
@@ -2,7 +2,6 @@ import {function_imports_add} from './function_imports_add.mjs';
 import {js_imports_missing} from './js_imports_missing.mjs';
 import {js_declaration_single} from './js_declaration_single.mjs';
 export function js_imports_missing_add(ast) {
-  let declaration = js_declaration_single(ast);
-  let imports_missing = js_imports_missing(ast, declaration);
+  let imports_missing = js_imports_missing(ast);
   function_imports_add(ast, imports_missing);
 }
