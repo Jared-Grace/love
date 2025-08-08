8ad96441d3a1530dc3de81a3368516ae1b03920f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:54:28 -0400

    i function_imports_missing_add

--- public/src/file_js_unparse.mjs
+++ public/src/file_js_unparse.mjs
@@ -1,7 +1,11 @@
 import { file_overwrite } from "./file_overwrite.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 
-export async function file_js_unparse(f_path, parsed) {
-  let code = js_unparse(parsed);
-  await file_overwrite(f_path, code);
+export async function file_js_unparse(parsed) {
+    let {f_path, ast, code} = parsed;
+  let code_new = js_unparse(ast);
+  if (code_new===code) {
+    return;
+  }
+  await file_overwrite(f_path, code_new);
 }
--- public/src/function_unparse.mjs
+++ public/src/function_unparse.mjs
@@ -1,7 +1,5 @@
 import { file_js_unparse } from "./file_js_unparse.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 export async function function_unparse(parsed) {
-    let {f_name, ast} = parsed
-  const f_path = await function_name_to_path_unalias(f_name);
-  await file_js_unparse(f_path, ast);
+  await file_js_unparse(parsed);
 }
