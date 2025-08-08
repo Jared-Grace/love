177a23680bebafa25daaf1e3d541e0f04ec31ffe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:27:16 -0400

    i list_insert

--- public/src/list_insert.mjs
+++ public/src/list_insert.mjs
@@ -1,4 +1,6 @@
+import {number_is} from './number_is.mjs';
+import {assert} from './assert.mjs';
 export function list_insert(list, index, value) {
-    assert(number_is(index))
+  if (0) assert(number_is(index));
   list.splice(index, 0, value);
 }
