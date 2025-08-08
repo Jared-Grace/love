257b6eb8997f27939db1a831344f54088e4e695c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:05:54 -0400

    i marker_next_get

--- web/public/src/marker_next_get.mjs
+++ web/public/src/marker_next_get.mjs
@@ -1,1 +1,9 @@
-export function marker_next_get(){}
+import {list_get} from './list_get.mjs';
+import {list_index_of} from './list_index_of.mjs';
+export function marker_next_get(a) {
+  let {stack2, stack1} = a;
+  let index = list_index_of(stack2, stack1);
+  let index_new = index + 1;
+  let next = list_get(stack2, index_new);
+  return next;
+}
