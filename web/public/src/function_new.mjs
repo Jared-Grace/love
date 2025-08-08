90e07dc1fce6ede9e712cb507db3beb3aba4809e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:35:13 -0400

    i function_new

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -1,3 +1,4 @@
+import {function_open} from './function_open.mjs';
 import {js_code_call} from './js_code_call.mjs';
 import {file_overwrite} from "./file_overwrite.mjs";
 import {function_name_to_path} from "./function_name_to_path.mjs";
@@ -5,5 +6,5 @@ import {file_open} from "./file_open.mjs";
 export async function function_new(f_name) {
   const f_path = function_name_to_path(f_name);
   await file_overwrite(f_path, "export function " + js_code_call(f_name) + "{}");
-  await file_open(f_path);
+  await function_open(f_name);
 }
