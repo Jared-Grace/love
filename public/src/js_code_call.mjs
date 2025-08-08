6ec99f2daaec497df15b0081eda28ac3d9ab4291
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:06:50 -0400

    i js_code_call

--- public/src/js_code_call.mjs
+++ public/src/js_code_call.mjs
@@ -1,4 +1,6 @@
-import {js_code_parenthesis_open_close} from './js_code_parenthesis_open_close.mjs';
+import {js_code_wrap_parenthesis} from './js_code_wrap_parenthesis.mjs';
 export function js_code_call(fn_name) {
-  return fn_name + js_code_parenthesis_open_close();
+  let args = [];
+  let joined = list_join(args);
+  return fn_name + js_code_wrap_parenthesis(joined);
 }
