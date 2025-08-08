4f20a2ffe01625168f04311c2c2061d770098195
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:30:49 -0400

    i function_new

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -1,8 +1,9 @@
-import { file_overwrite } from "./file_overwrite.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
-import { file_open } from "./file_open.mjs";
+import {function_name_to_path_unalias} from './function_name_to_path_unalias.mjs';
+import {file_overwrite} from "./file_overwrite.mjs";
+import {function_name_to_path} from "./function_name_to_path.mjs";
+import {file_open} from "./file_open.mjs";
 export async function function_new(f_name) {
-  const f_path = await function_name_to_path(f_name);
+  const f_path = await function_name_to_path_unalias(f_name);
   await file_overwrite(f_path, "export function " + f_name + "(){}");
   await file_open(f_path);
 }
