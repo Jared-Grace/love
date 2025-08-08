3712cd9f4e54e79f079d9894d5bba775c1ab75d5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 15:51:01 -0400

    i function_parse_declaration

--- public/src/function_parse_declaration.mjs
+++ public/src/function_parse_declaration.mjs
@@ -1,1 +1,12 @@
-export function function_parse_declaration(){}
+import {object_merge} from './object_merge.mjs';
+import {js_declaration_single} from './js_declaration_single.mjs';
+import {function_parse} from './function_parse.mjs';
+export async function function_parse_declaration(f_name) {
+  let parsed = await function_parse(f_name);
+  let {ast} = parsed;
+  let declaration = js_declaration_single(ast);
+  return object_merge({
+    declaration,
+    parsed
+  });
+}
