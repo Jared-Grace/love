b212e7c1324f2b9a4288de8807369e3e0472f4d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:25:34 -0400

    p

--- web/public/src/function_open.mjs
+++ web/public/src/function_open.mjs
@@ -5,7 +5,7 @@ import { file_open } from "./file_open.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export async function function_open(f_name) {
-  const f_path = await function_name_to_path_unalias(f_name);
+  const {f_path} = await function_name_to_path_unalias(f_name);
   await file_open(f_path);
   const property_name = "function_current";
   var d = await data_get(property_name, null);
--- web/public/src/function_parse.mjs
+++ web/public/src/function_parse.mjs
@@ -2,7 +2,7 @@ import { function_name_to_path_unalias } from "./function_name_to_path_unalias.m
 import { file_js_parse } from "./file_js_parse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_parse(f_name) {
-  const f_path = await function_name_to_path_unalias(f_name);
+  const {f_path} = await function_name_to_path_unalias(f_name);
   let parsed = await file_js_parse(f_path);
   return parsed;
 }
