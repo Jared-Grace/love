b29803a8268eee89ef11da9f395956149584fc80
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:57:04 -0400

    i js_identifier_replace

--- web/public/src/js_identifier_replace.mjs
+++ web/public/src/js_identifier_replace.mjs
@@ -1,11 +1,12 @@
-import { js_visit_type } from "./js_visit_type.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { list_adder_unique } from "./list_adder_unique.mjs";
-export function js_identifiers_names(declaration) {
-  return list_adder_unique((la) => {
-    js_visit_type(declaration, "Identifier", (v) => {
-      let { node } = v;
-      la(object_property_get(node, "name"));
-    });
+import {object_property_set} from './object_property_set.mjs';
+import {js_visit_type} from "./js_visit_type.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+import {list_adder_unique} from "./list_adder_unique.mjs";
+export function js_identifier_replace(ast, name_from, name_to) {
+  js_visit_type(ast, "Identifier", v => {
+    let {node} = v;
+    if (object_property_get(node, "name") === name_from) {
+      object_property_set(node, "name", name_to);
+    }
   });
 }
