a5d05b8492cea8899c0a9a3726d7781cb994ff54
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 16:15:54 -0400

    i marker_call

--- web/public/src/marker_call.mjs
+++ web/public/src/marker_call.mjs
@@ -1,3 +1,4 @@
+import {list_map_property} from './list_map_property.mjs';
 import {js_parse_statement} from './js_parse_statement.mjs';
 import {marker} from './marker.mjs';
 import {js_code_call_args} from './js_code_call_args.mjs';
@@ -8,18 +9,22 @@ import {function_transform_marker} from './function_transform_marker.mjs';
 import {list_adder_async} from './list_adder_async.mjs';
 import {data_function_current_get} from './data_function_current_get.mjs';
 import {marker_next_index} from './marker_next_index.mjs';
+import {object_property_get} from './object_property_get.mjs';
 export async function marker_call(f_name_call) {
-  let parsed = await function_parse_declaration(f_name_call);
+  let {declaration, unaliased} = await function_parse_declaration(f_name_call);
   let f_name_current = await data_function_current_get();
   return list_adder_async(async la => {
     await function_transform_marker(f_name_current, lambda);
     function lambda(a) {
       let {index, stack2} = marker_next_index(a);
-      la(parsed);
-      return;{
-      let code = js_code_call_args(marker.name);
-      let parsed = js_parse_statement(code);
-      la(js_unparse(next));}
+      let args = list_map_property(object_property_get(declaration, 'params'), 'name');
+      let code = js_code_call_args(unaliased, args);
+      la(code);
+      return;
+      {
+        let parsed = js_parse_statement(code);
+        la(js_unparse(next));
+      }
     }
   });
 }
