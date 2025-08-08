baad6476a3b03225c78dd2d36d3944a352774b61
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 23:14:26 -0400

    i marker_remove

--- web/public/src/marker_remove.mjs
+++ web/public/src/marker_remove.mjs
@@ -1,21 +1,23 @@
-import { log } from "./log.mjs";
-import { list_get_end } from "./list_get_end.mjs";
-import { js_visit_type } from "./js_visit_type.mjs";
-import { list_add_first } from "./list_add_first.mjs";
-import { js_parse_statement } from "./js_parse_statement.mjs";
-import { js_code_call_statement } from "./js_code_call_statement.mjs";
-import { js_declaration_single } from "./js_declaration_single.mjs";
-import { function_transform } from "./function_transform.mjs";
-import { marker } from "./marker.mjs";
-import { js_node_is } from "./js_node_is.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
-import { list_is } from "./list_is.mjs";
+import {list_remove} from './list_remove.mjs';
+import {error} from './error.mjs';
+import {log} from "./log.mjs";
+import {list_get_end} from "./list_get_end.mjs";
+import {js_visit_type} from "./js_visit_type.mjs";
+import {list_add_first} from "./list_add_first.mjs";
+import {js_parse_statement} from "./js_parse_statement.mjs";
+import {js_code_call_statement} from "./js_code_call_statement.mjs";
+import {js_declaration_single} from "./js_declaration_single.mjs";
+import {function_transform} from "./function_transform.mjs";
+import {marker} from "./marker.mjs";
+import {js_node_is} from "./js_node_is.mjs";
+import {js_node_type_is} from "./js_node_type_is.mjs";
+import {list_is} from "./list_is.mjs";
 export async function marker_remove(f_name) {
   marker();
   await function_transform(f_name, lambda);
   function lambda(ast) {
-    js_visit_type(ast, "CallExpression", (v) => {
-      let { stack } = v;
+    js_visit_type(ast, "CallExpression", v => {
+      let {stack} = v;
       let s1 = list_get_end(stack, 1);
       if (!js_node_is(s1)) {
         return;
@@ -23,17 +25,16 @@ export async function marker_remove(f_name) {
       if (!js_node_type_is(s1, "ExpressionStatement")) {
         return;
       }
-      let { node } = v;
-      let { callee } = node;
+      let {node} = v;
+      let {callee} = node;
       if (!js_node_type_is(callee, "Identifier")) {
         return;
       }
-      let { name } = callee;
+      let {name} = callee;
       if (name !== marker.name) {
         return;
       }
       let s2 = list_get_end(stack, 2);
-
       if (!list_is(s2)) {
         error();
       }
