45cf00a7f8e78dbac9d1b83dfacc72ab2d6a024e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:45:52 -0400

    f file_copy

--- web/public/src/file_exists.mjs
+++ web/public/src/file_exists.mjs
@@ -1,9 +1,9 @@
-export async function file_exists() {
+export async function file_exists(file_path) {
   let fs = await import("fs");
   let { access } = fs.promises;
   let { constants } = fs;
   try {
-    await access(path, constants.F_OK);
+    await access(file_path, constants.F_OK);
     return true;
   } catch {
     return false;
