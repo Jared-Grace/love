5ab7b1761bc3d224fad018ad364a8b7c2a448403
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 19:08:12 -0400

    i marker_call

--- web/public/src/list_last.mjs
+++ web/public/src/list_last.mjs
@@ -1,1 +1,5 @@
-export function list_last(  ){}
+import { list_get } from "./list_get.mjs";
+import { list_get_end } from "./list_get_end.mjs";
+import { list_index_last } from "./list_index_last.mjs";
+
+export function list_last( list ){return list_get_end(list,0)}
