7a428cc756ce287c2e3d15705c9d460f963d3f9e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 13:46:18 -0400

    p

--- public/src/function_unparse.mjs
+++ public/src/function_unparse.mjs
@@ -1,6 +1,7 @@
 import { file_js_unparse } from "./file_js_unparse.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
-export async function function_unparse(f_name, parsed) {
+export async function function_unparse(parsed) {
+    let {f_name, ast} = parsed
   const f_path = await function_name_to_path_unalias(f_name);
-  await file_js_unparse(f_path, parsed);
+  await file_js_unparse(f_path, ast);
 }
