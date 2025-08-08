49aaa4e1ea807839ba058b614e2b7b3f472ee5f6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:39:24 -0400

    i function_transform_marker

--- web/public/src/marker_remove.mjs
+++ web/public/src/marker_remove.mjs
@@ -22,33 +22,4 @@ export async function marker_remove() {
         list_remove( stack2, stack1);
     }
 }
-async function function_transform_marker(f_name, lambda) {
-    await function_transform(f_name, lambda_marker);
-    function lambda_marker(ast) {
-        js_visit_type(ast, "CallExpression", v => {
-            let { stack } = v;
-            let stack1 = list_get_end(stack, 1);
-            if (!js_node_is(stack1)) {
-                return;
-            }
-            if (!js_node_type_is(stack1, "ExpressionStatement")) {
-                return;
-            }
-            let { node } = v;
-            let { callee } = node;
-            if (!js_node_type_is(callee, "Identifier")) {
-                return;
-            }
-            let { name } = callee;
-            if (name !== marker.name) {
-                return;
-            }
-            let stack2 = list_get_end(stack, 2);
-            if (!list_is(stack2)) {
-                error();
-            }
-            lambda({ stack2, stack1 });
-        });
-    }
-}
 
