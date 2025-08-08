49a9ddbd528200a91fca99ffae9e332fab5b6f65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:57:04 -0400

    i js_identifier_replace

--- public/src/js_identifiers_names.mjs
+++ public/src/js_identifiers_names.mjs
@@ -1,9 +1,9 @@
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
-export function js_identifiers_names(declaration) {
+export function js_identifiers_names(ast) {
   return list_adder_unique((la) => {
-    js_visit_type(declaration, "Identifier", (v) => {
+    js_visit_type(ast, "Identifier", (v) => {
       let { node } = v;
       la(object_property_get(node, "name"));
     });
