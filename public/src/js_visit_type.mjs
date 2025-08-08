cabb46ec08dcfe1eaa91e21b45d45c1baa3cd4d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-07 21:59:48 -0400

    i js_visit_type

--- public/src/js_visit_type.mjs
+++ public/src/js_visit_type.mjs
@@ -1,12 +1,12 @@
-import { js_visit } from "./js_visit.mjs";
-import { js_visit_children_get } from "./js_visit_children_get.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
-
-import { js_node_is } from "./js_node_is.mjs";
-
+import {log} from './log.mjs';
+import {js_visit} from "./js_visit.mjs";
+import {js_visit_children_get} from "./js_visit_children_get.mjs";
+import {js_node_type_is} from "./js_node_type_is.mjs";
+import {js_node_is} from "./js_node_is.mjs";
 export function js_visit_type(parsed, type, on_each) {
-  js_visit(parsed, (v) => {
-    let { node } = v;
+  js_visit(parsed, v => {
+    let {node} = v;
+    if (false) log(node);
     if (js_node_is(node) && js_node_type_is(node, type)) {
       on_each(v);
     }
