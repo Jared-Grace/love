55ab5e1e31a704aee437db7e1af4986a17c454af
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:39:55 -0400

    c o c

--- web/public/src/function_copy.mjs
+++ web/public/src/function_copy.mjs
@@ -1,9 +1,15 @@
-import { file_copy } from "./file_copy.mjs";
+import { data_save } from "./data_save.mjs";
+import { data_get } from "./data_get.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 import { file_open } from "./file_open.mjs";
-export async function function_copy(f_name_old, f_name_new) {
-  const f_path_new = await function_name_to_path_unalias(f_name_new);
-  const f_path_old = await function_name_to_path_unalias(f_name_old);
-  await file_copy(f_path_old, f_path_new);
-  await file_open(f_path_new);
+import { function_name_to_path } from "./function_name_to_path.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+export async function function_open(f_name) {
+  const f_path = await function_name_to_path_unalias(f_name);
+  await file_open(f_path);
+  const property_name = "function_current";
+  var d = await data_get(property_name, null);
+  let { data } = d;
+  object_property_set(data, property_name, f_name);
+  await data_save(d);
 }
