50d58f3935e7d2afde9a9cc90d502722fd67e100
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 14:24:36 -0400

    p

--- public/src/function_name_to_path_unalias.mjs
+++ public/src/function_name_to_path_unalias.mjs
@@ -1,6 +1,7 @@
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
-export async function function_name_to_path_unalias(funcName) {
-  funcName = await function_name_unalias(funcName);
-  return function_name_to_path(funcName);
+export async function function_name_to_path_unalias(f_name) {
+  f_name = await function_name_unalias(f_name);
+    const f_path = function_name_to_path(f_name);
+  return f_path;
 }
