9ced7c69783675b6cf63ca34072749e573a4060f
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 08:29:31 -0400

    m

--- web/public/src/server.mjs
+++ web/public/src/server.mjs
@@ -1,9 +1,10 @@
-import { path_join } from "./path_join.mjs";
+import {path_join} from "./path_join.mjs";
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
-import { folder_public } from "./folder_public.mjs";
+import {fileURLToPath} from "url";
+import {folder_public} from "./folder_public.mjs";
 export function server() {
+  marker();
   const app = express();
   const port = 3000;
   const __filename = fileURLToPath(import.meta.url);
