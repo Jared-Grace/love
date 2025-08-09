57e4905f56742160ece16868f8f6495872bec49b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:39:13 -0400

    p

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -3,11 +3,13 @@ import { data_transform } from "./data_transform.mjs";
 import { command_line_read } from "./command_line_read.mjs";
 import { function_run_git } from "./function_run_git.mjs";
 import { list_add } from "./list_add.mjs";
+import { list_unique } from "./list_unique.mjs";
 export async function function_run_prompt() {
   let line = await command_line_read("");
   const [funcName, ...args] = line.split(" ");
   await data_transform("prompts", [], (previous) => {
     list_add(previous, line);
+    previous =list_unique(previous)
     return previous;
   });
   await function_run_git(funcName, args);
