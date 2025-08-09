9dca2d949ec6efe6d768a223a7321268f9aa403c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:16:01 -0400

    o js_parse_statement

--- web/public/src/js_parse_statement_module.mjs
+++ web/public/src/js_parse_statement_module.mjs
@@ -2,12 +2,8 @@ import {js_code_declaration} from './js_code_declaration.mjs';
 import {js_parse} from "./js_parse.mjs";
 import {list_first} from "./list_first.mjs";
 export function js_parse_statement(code) {
-  let import_parsed = js_parse(js_code_declaration('a', code, true));
-  console.log(import_parsed)
-  let {body} = import_parsed;
-  let declaration = list_first(body);
-  let {body:block} = declaration;
-  let {body:statements} = block;
-    const statement = list_first(statements);
+  let ast = js_parse(code);
+  let {body} = ast;
+  let statement = list_first(body);
   return statement;
 }
