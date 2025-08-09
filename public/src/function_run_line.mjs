dacfaf38108073e902b8c9a5a3b141652ec2abd8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 14:47:11 -0400

    i function_run_line

--- public/src/function_run_line.mjs
+++ public/src/function_run_line.mjs
@@ -1,1 +1,13 @@
-export function function_run_line(  ){}
+import {function_run_git} from './function_run_git.mjs';
+import {list_unique} from './list_unique.mjs';
+import {list_add} from './list_add.mjs';
+import {data_transform} from './data_transform.mjs';
+export async function function_run_line(line) {
+  const [funcName, ...args] = line.split(" ");
+  await data_transform("prompts", [], previous => {
+    list_add(previous, line);
+    previous = list_unique(previous);
+    return previous;
+  });
+  await function_run_git(funcName, args);
+}
