5968b946f45101d1be77b69db5533e3a9ef1a08a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 14:54:01 -0400

    i list_copy_reverse

--- web/public/src/list_copy_reverse.mjs
+++ web/public/src/list_copy_reverse.mjs
@@ -1,1 +1,7 @@
-export function list_copy_reverse(  ){}
+import {list_reverse} from './list_reverse.mjs';
+import {list_copy} from './list_copy.mjs';
+export function list_copy_reverse(previous) {
+  let copy = list_copy(previous);
+  list_reverse(copy);
+  return copy;
+}
