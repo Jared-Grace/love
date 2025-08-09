a73a25239f14b560a19a14bc8478957497d289a1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:01:34 -0400

    n js_code_declaration

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -7,7 +7,11 @@ import {function_name_to_path} from "./function_name_to_path.mjs";
 import {file_open} from "./file_open.mjs";
 export async function function_new(f_name) {
   const f_path = function_name_to_path(f_name);
-  const contents = "export function " + js_code_call(f_name) + "{}";
+  const contents = "export "+js_code_declaration(f_name);
   await file_write(f_path, contents);
   await function_open(f_name);
 }
+function js_code_declaration(f_name) {
+    return "function " + js_code_call(f_name) + "{}";
+}
+
