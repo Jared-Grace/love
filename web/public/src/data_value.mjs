4a7a3d658d513df9baba64d71172960396768a47
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:27:53 -0400

    i data_value

--- web/public/src/data_value.mjs
+++ web/public/src/data_value.mjs
@@ -1,12 +1,9 @@
-import { data_all } from "./data_all.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
-import { file_read_json } from "./file_read_json.mjs";
-export async function data_get(property_name, value_initial) {
-  var { data, file_path } = await data_all();
-  let value = object_property_initialize(data, property_name, value_initial);
-  return {
-    value,
-    file_path,
-    data,
-  };
+import {object_property_get} from './object_property_get.mjs';
+import {data_all} from "./data_all.mjs";
+import {object_property_initialize} from "./object_property_initialize.mjs";
+import {file_read_json} from "./file_read_json.mjs";
+export async function data_value(property_name) {
+  var {data} = await data_all();
+  let value = object_property_get(data, property_name);
+  return value;
 }
