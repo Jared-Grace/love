dfd2f9ff5d59f3d03e93e1105895795432ee2e37
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:17:24 -0400

    i data_transform

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -1,12 +1,12 @@
 import {list_add_first} from './list_add_first.mjs';
-import {js_parse_statement} from './js_parse_statement.mjs';
+import {js_parse_statement_module} from './js_parse_statement_module.mjs';
 import {function_name_to_base} from './function_name_to_base.mjs';
 import {each} from './each.mjs';
 export function function_imports_add(ast, imports) {
   let {body} = ast;
   each(imports, import_ => {
     let code = "import { " + import_ + " } from './" + function_name_to_base(import_) + "'";
-    let statement = js_parse_statement(code);
+    let statement = js_parse_statement_module(code);
     list_add_first(body, statement);
   });
 }
