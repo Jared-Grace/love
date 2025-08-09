a1efcf95a83010c946f361ae6d3495774e2c8735
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:17:24 -0400

    i data_transform

--- web/public/src/data_transform.mjs
+++ web/public/src/data_transform.mjs
@@ -1,6 +1,9 @@
+import {data_save} from './data_save.mjs';
+import {object_property_set} from './object_property_set.mjs';
+import {data_get} from './data_get.mjs';
 export async function data_transform(property_name, value) {
-    var d = await data_get(property_name, null);
-    let { data } = d;
-    object_property_set(data, property_name, value);
-    await data_save(d);
+  var d = await data_get(property_name, null);
+  let {data} = d;
+  object_property_set(data, property_name, value);
+  await data_save(d);
 }
--- web/public/src/js_parse_statement.mjs
+++ web/public/src/js_parse_statement.mjs
@@ -2,9 +2,8 @@ import {js_code_declaration} from './js_code_declaration.mjs';
 import {js_parse} from "./js_parse.mjs";
 import {list_first} from "./list_first.mjs";
 export function js_parse_statement(code) {
-  let import_parsed = js_parse(js_code_declaration('a', code, true));
-  console.log(import_parsed)
-  let {body} = import_parsed;
+  let ast = js_parse(js_code_declaration('a', code, true));
+  let {body} = ast;
   let declaration = list_first(body);
   let {body:block} = declaration;
   let {body:statements} = block;
