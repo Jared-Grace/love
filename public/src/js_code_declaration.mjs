8193ff1a7ed51951eda59ba73ae8672425963ab2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:02:12 -0400

    i js_code_declaration

--- public/src/js_code_declaration.mjs
+++ public/src/js_code_declaration.mjs
@@ -1,1 +1,4 @@
-export function js_code_declaration(  ){}
+import {js_code_call} from './js_code_call.mjs';
+export function js_code_declaration(f_name) {
+  return "function " + js_code_call(f_name) + "{}";
+}
