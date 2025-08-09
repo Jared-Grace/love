ed0cc9c5b288bbbb9191846b199fdb458c32f3b0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:04:38 -0400

    i js_code_declaration

--- web/public/src/js_code_declaration.mjs
+++ web/public/src/js_code_declaration.mjs
@@ -1,8 +1,5 @@
+import {js_code_wrap_braces} from './js_code_wrap_braces.mjs';
 import {js_code_call} from './js_code_call.mjs';
-export function js_code_declaration(f_name,inside) {
-  return "function " + js_code_call(f_name) + (js_code_wrap_braces(inside));
+export function js_code_declaration(f_name, inside) {
+  return "function " + js_code_call(f_name) + js_code_wrap_braces(inside);
 }
-function js_code_wrap_braces(inside) {
-    return "{" + inside + "}";
-}
-
