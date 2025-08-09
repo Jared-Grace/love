7d9a8e03c2d8956ec11d91a68c2ebff76d7dad9b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:14:27 -0400

    o i

--- web/public/src/data_transform.mjs
+++ web/public/src/data_transform.mjs
@@ -1,1 +1,6 @@
-export function data_transform(  ){}
+export async function data_transform(property_name, value) {
+    var d = await data_get(property_name, null);
+    let { data } = d;
+    object_property_set(data, property_name, value);
+    await data_save(d);
+}
