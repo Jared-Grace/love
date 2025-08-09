f5c9f35cf69212b0b72bd8ae2364d7b2fbad2aae
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:38:48 -0400

    f function_run_prompt

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -1,11 +1,14 @@
-import {log} from './log.mjs';
-import {data_transform} from './data_transform.mjs';
-import {command_line_read} from "./command_line_read.mjs";
-import {function_run_git} from "./function_run_git.mjs";
-import {list_add} from "./list_add.mjs";
+import { log } from "./log.mjs";
+import { data_transform } from "./data_transform.mjs";
+import { command_line_read } from "./command_line_read.mjs";
+import { function_run_git } from "./function_run_git.mjs";
+import { list_add } from "./list_add.mjs";
 export async function function_run_prompt() {
   let line = await command_line_read("");
   const [funcName, ...args] = line.split(" ");
-  await data_transform("prompts", [], previous => {list_add(previous, line);return previous});
+  await data_transform("prompts", [], (previous) => {
+    list_add(previous, line);
+    return previous;
+  });
   await function_run_git(funcName, args);
 }
