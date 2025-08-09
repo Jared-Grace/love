77cef76753111887f3d345465fd1f92999cb7f6a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:07:54 -0400

    js_parse_statement await(1)

--- web/public/src/js_parse_statement.mjs
+++ web/public/src/js_parse_statement.mjs
@@ -2,7 +2,7 @@ import {js_code_declaration} from './js_code_declaration.mjs';
 import {js_parse} from "./js_parse.mjs";
 import {list_first} from "./list_first.mjs";
 export function js_parse_statement(code) {
-  let import_parsed = js_parse(js_code_declaration('a', code));
+  let import_parsed = js_parse(js_code_declaration('a', code, true));
   let {body} = import_parsed;
   let declaration = list_first(body);
   let {body:block} = declaration;
