dda08431bcd61b4447b1362d3d17c15757735c6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 14:52:55 -0400

    i function_run_line

--- public/src/function_run_line.mjs
+++ public/src/function_run_line.mjs
@@ -1,11 +1,15 @@
+import {list_reverse} from './list_reverse.mjs';
 import {function_run_git} from './function_run_git.mjs';
 import {list_unique} from './list_unique.mjs';
 import {list_add} from './list_add.mjs';
 import {data_transform} from './data_transform.mjs';
+import {list_copy} from './list_copy.mjs';
 export async function function_run_line(line) {
   const [funcName, ...args] = line.split(" ");
   await data_transform("prompts", [], previous => {
     list_add(previous, line);
+    let copy = list_copy(previous);
+    list_reverse(copy);
     previous = list_unique(previous);
     return previous;
   });
