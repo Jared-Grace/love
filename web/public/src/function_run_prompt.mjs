d1cc6984101a98bd986d0b560c4779d87eeb847d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 14:48:00 -0400

    i prompt_previous

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -9,12 +9,3 @@ export async function function_run_prompt() {
   let line = await command_line_read("");
   await function_run_line(line);
 }
-async function function_run_line(line) {
-  const [funcName, ...args] = line.split(" ");
-  await data_transform("prompts", [], previous => {
-    list_add(previous, line);
-    previous = list_unique(previous);
-    return previous;
-  });
-  await function_run_git(funcName, args);
-}
