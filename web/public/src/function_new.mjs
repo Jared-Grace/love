53dcca074d6aac4ba8975f97ef8f1e8fff80dfe7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:02:20 -0400

    i function_new

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -1,3 +1,4 @@
+import {js_code_declaration} from './js_code_declaration.mjs';
 import {file_write} from './file_write.mjs';
 import {assert_file_exists_not} from './assert_file_exists_not.mjs';
 import {function_open} from './function_open.mjs';
@@ -7,11 +8,10 @@ import {function_name_to_path} from "./function_name_to_path.mjs";
 import {file_open} from "./file_open.mjs";
 export async function function_new(f_name) {
   const f_path = function_name_to_path(f_name);
-  const contents = "export "+js_code_declaration(f_name);
+  const contents = "export " + js_code_declaration(f_name);
   await file_write(f_path, contents);
   await function_open(f_name);
 }
 function js_code_declaration(f_name) {
-    return "function " + js_code_call(f_name) + "{}";
+  return "function " + js_code_call(f_name) + "{}";
 }
-
