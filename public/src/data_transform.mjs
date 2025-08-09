9f423addee6fd3b5c6f16b1a5449348a67ebb6ee
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-08 20:21:04 -0400

    i data_transform

--- public/src/data_transform.mjs
+++ public/src/data_transform.mjs
@@ -1,9 +1,11 @@
+import {object_property_get} from './object_property_get.mjs';
 import {data_save} from './data_save.mjs';
 import {object_property_set} from './object_property_set.mjs';
 import {data_get} from './data_get.mjs';
-export async function data_transform(property_name, value) {
+export async function data_transform(property_name, value_get) {
   var d = await data_get(property_name, null);
   let {data} = d;
-  object_property_set(data, property_name, value);
+  let previous = object_property_get(data, property_name);
+  object_property_set(data, property_name, value_get(previous));
   await data_save(d);
 }
