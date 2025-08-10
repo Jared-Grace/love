300339855b715711a39310205c11e4df8d3417b3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:37:14 -0400

    au sb

---
+++ public/src/function_delete_if_exists.mjs
@@ -0,0 +1,7 @@
+import {function_delete} from './function_delete.mjs';
+import {file_exists} from './file_exists.mjs';
+export async function function_delete_if_exists(n) {
+  if (await file_exists(n)) {
+    await function_delete(n);
+  }
+}
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,10 @@
+import { function_delete_if_exists } from "./function_delete_if_exists.mjs";
+import { file_exists } from "./file_exists.mjs";
 import { function_delete } from "./function_delete.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function sandbox() {
+  await function_delete_if_exists(n);
   const n = "function_params_new";
   await function_wrap(function_param_new.name, n);
-  await function_delete(n);
 }
