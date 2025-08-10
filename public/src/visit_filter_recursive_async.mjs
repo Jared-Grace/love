e8882ab854bc8a4a73aa437662006fd628354772
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:55:36 -0400

    i visit_filter_recursive_async

--- public/src/visit_filter_recursive_async.mjs
+++ public/src/visit_filter_recursive_async.mjs
@@ -1,9 +1,10 @@
+import { each_async } from "./each_async.mjs";
 import { each } from "./each.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_pop } from "./list_pop.mjs";
 import { list_copy } from "./list_copy.mjs";
 import { error } from "./error.mjs";
-export function visit_filter_recursive(
+export async function visit_filter_recursive_async(
   node,
   children_get,
   filter,
@@ -14,11 +15,14 @@ export function visit_filter_recursive(
     return;
   }
   list_add(stack, node);
-    let copy = list_copy(stack);
-  on_each({ node, stack: copy });
-  let children = children_get(node);
-  each(children, (c) => {
-    visit_filter_recursive(c, children_get, filter, on_each, stack);
+  let copy = list_copy(stack);
+  await on_each({
+    node,
+    stack: copy,
+  });
+  let children = await children_get(node);
+  await each_async(children, async (c) => {
+    await visit_filter_recursive_async(c, children_get, filter, on_each, stack);
   });
   let removed = list_pop(stack);
   if (removed !== node) {
