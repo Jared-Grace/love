8ff724e1d68a73e549a077bcc9671dd4a0350a1c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 17:25:59 -0400

    i js_return_name

--- public/src/js_return_name.mjs
+++ public/src/js_return_name.mjs
@@ -1,3 +1,4 @@
+import { list_last } from "./list_last.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
@@ -6,6 +7,7 @@ export function js_return_name(ast) {
   let body_block = js_declaration_single_block_blody(ast);
   let name = null;
   if (list_empty_not_is(body_block)) {
+    let last = list_last(body_block);
   }
   return name;
 }
