7cc614a1fc721d96e9a5be2c6d109886519ae5d1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:54:55 -0400

    c visit_filter_recursive visit_filter_recursive_async

--- web/public/src/visit_filter_async.mjs
+++ web/public/src/visit_filter_async.mjs
@@ -1,4 +1,4 @@
 import { visit_filter_recursive } from "./visit_filter_recursive.mjs";
-export function visit_filter(node, children_get, filter, on_each) {
+export function visit_filter_async(node, children_get, filter, on_each) {
   visit_filter_recursive(node, children_get, filter, on_each, []);
 }
---
+++ web/public/src/visit_filter_recursive_async.mjs
@@ -0,0 +1,27 @@
+import { each } from "./each.mjs";
+import { list_add } from "./list_add.mjs";
+import { list_pop } from "./list_pop.mjs";
+import { list_copy } from "./list_copy.mjs";
+import { error } from "./error.mjs";
+export function visit_filter_recursive(
+  node,
+  children_get,
+  filter,
+  on_each,
+  stack,
+) {
+  if (!filter(node)) {
+    return;
+  }
+  list_add(stack, node);
+    let copy = list_copy(stack);
+  on_each({ node, stack: copy });
+  let children = children_get(node);
+  each(children, (c) => {
+    visit_filter_recursive(c, children_get, filter, on_each, stack);
+  });
+  let removed = list_pop(stack);
+  if (removed !== node) {
+    error();
+  }
+}
