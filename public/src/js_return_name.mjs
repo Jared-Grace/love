671050ef7340561459f91e0ce9f77afb70a7f319
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:23:05 -0400

    i js_return_name

--- public/src/js_return_name.mjs
+++ public/src/js_return_name.mjs
@@ -1,1 +1,4 @@
-export function js_return_name() {}
+import { js_declaration_single } from "./js_declaration_single.mjs";
+export function js_return_name(ast) {
+  let declaration = js_declaration_single(ast);
+}
