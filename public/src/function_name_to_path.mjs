fbf53844edcce2f2b95e1274c33abdae9ccd753c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:34:01 -0400

    c np noop2

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -4,7 +4,7 @@ import { function_name_to_base } from "./function_name_to_base.mjs";
 import { functions_path } from "./functions_path.mjs";
 import path from "path";
 export function function_name_to_path(f_name) {
-  if (0 && string_includes(f_name, ".")) {
+  if (string_includes(f_name, ".")) {
     error();
   }
   let second = [functions_path(), function_name_to_base(f_name)];
