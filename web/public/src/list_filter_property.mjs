dfc356e45599037391e47c637f92fb4ffbba2923
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:00:38 -0400

    i list_filter_property

--- web/public/src/list_filter_property.mjs
+++ web/public/src/list_filter_property.mjs
@@ -1,5 +1,8 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { list_map } from "./list_map.mjs";
-export function list_filter_property(list, property_name) {
-  return list_map(list, (item) => object_property_get(item, property_name));
+export function list_filter_property(list, property_name, property_value) {
+  return list_filter_property(
+    list,
+    (item) => object_property_get(item, property_name) === property_value,
+  );
 }
