e5575a205d72ee6373ba48f4993369f614f8303b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:37:58 -0400

    s exist

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -4,7 +4,7 @@ import { function_delete } from "./function_delete.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function sandbox() {
+    const n = "function_params_new";
   await function_delete_if_exists(n);
-  const n = "function_params_new";
   await function_wrap(function_param_new.name, n);
 }
