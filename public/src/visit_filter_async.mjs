38432aa8c46362f3dadde66e26a490035cd588b4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:54:37 -0400

    c visit_filter visit_filter_async

---
+++ public/src/visit_filter_async.mjs
@@ -0,0 +1,4 @@
+import { visit_filter_recursive } from "./visit_filter_recursive.mjs";
+export function visit_filter(node, children_get, filter, on_each) {
+  visit_filter_recursive(node, children_get, filter, on_each, []);
+}
