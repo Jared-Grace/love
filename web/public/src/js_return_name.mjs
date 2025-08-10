8363e8e17653ba10e10fd51b06e2fefe12748089
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:26:50 -0400

    f

--- web/public/src/js_return_name.mjs
+++ web/public/src/js_return_name.mjs
@@ -2,4 +2,9 @@ import { js_declaration_single_block_blody } from "./js_declaration_single_block
 import { js_declaration_single } from "./js_declaration_single.mjs";
 export function js_return_name(ast) {
   let body_block = js_declaration_single_block_blody(ast);
+  let name = null;
+  if (list_empty_not_is(body_block)) {
+    
+  }
+  return name;
 }
