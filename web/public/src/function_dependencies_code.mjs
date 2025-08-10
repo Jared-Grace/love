c4adf3acf6c9c134c13c32b6b582f025d4ba6be2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:04:52 -0400

    i function_dependencies_code

--- web/public/src/function_dependencies_code.mjs
+++ web/public/src/function_dependencies_code.mjs
@@ -1,4 +1,8 @@
+import { function_parse_declaration } from "./function_parse_declaration.mjs";
 import { function_dependencies } from "./function_dependencies.mjs";
+import { list_map } from "./list_map.mjs";
 export async function function_dependencies_code(f_name) {
   let ds = await function_dependencies(f_name);
+  let mapped = list_map(ds, function_parse_declaration);
+  let waited = list_wait(mapped);
 }
