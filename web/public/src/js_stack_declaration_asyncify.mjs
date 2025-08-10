b668464a25f133bf085370747553e18a6fd69693
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:19:17 -0400

    fm js_stack_declaration_asyncify

--- web/public/src/js_stack_declaration_asyncify.mjs
+++ web/public/src/js_stack_declaration_asyncify.mjs
@@ -11,5 +11,10 @@ export function js_stack_declaration_asyncify(stack, declaration) {
     js_node_type_is(n, "FunctionDeclaration"),
   );
   let last = list_last(fds);
-  object_property_set(last, "async", object_property_get(last, "async",)||object_property_get(declaration, "async"));
+  object_property_set(
+    last,
+    "async",
+    object_property_get(last, "async") ||
+      object_property_get(declaration, "async"),
+  );
 }
