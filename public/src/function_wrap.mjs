a64502a64fdf8569908d18297d195d687e4c7df6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:26:50 -0400

    au marker_call

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -5,12 +5,17 @@ import { function_new_transform } from "./function_new_transform.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
+import { js_declaration_single } from "./js_declaration_single.mjs";
+import { list_add } from "./list_add.mjs";
 export async function function_wrap(f_name, f_name_wrapped) {
-  let { declaration, unaliased } =
-    await function_parse_declaration(f_name_call);
+  let { declaration: declaration_call, unaliased } =
+    await function_parse_declaration(f_name);
   await function_new_transform(f_name_wrapped, lambda);
   async function lambda(ast) {
-    let arg_names = js_declaration_params_names(declaration);
-    let code = js_code_call_args_await_maybe(unaliased, args_code, declaration);
+    let arg_names = js_declaration_params_names(declaration_call);
+    let code = js_code_call_args_await_maybe(unaliased, arg_names, declaration_call);
+    let declaration=js_declaration_single(ast)
+    let {body}=declaration
+    list_add(body,code)
   }
 }
