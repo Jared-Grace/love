9d35a543319f7a99c7b16f6c86c959b5a9a60071
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 23:04:44 -0400

    ai

--- web/public/src/function_aliases_inverted.mjs
+++ web/public/src/function_aliases_inverted.mjs
@@ -2,5 +2,6 @@ import {object_invert} from './object_invert.mjs';
 import {function_aliases} from './function_aliases.mjs';
 export async function function_aliases_inverted() {
   let aliases = await function_aliases();
-  inverted = object_invert(aliases);
+  let inverted = object_invert(aliases);
+  return inverted
 }
