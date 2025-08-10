a06a510f97bb21d5f5bd8fe5aa2826ec9ae9ffc7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:46:32 -0400

    function_param_new function_dependencies

--- web/public/src/function_param_new.mjs
+++ web/public/src/function_param_new.mjs
@@ -1,10 +1,11 @@
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
+import { log } from "./log.mjs";
 export async function function_param_new(f_name) {
   await function_transform(f_name, (ast) => {
     marker();
-    js_declaration_single(parsed);
-    let declaration = js_declaration_single(ast);
+    let declaration=js_declaration_single(ast);
+    log(declaration)
   });
 }
