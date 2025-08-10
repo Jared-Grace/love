b578a5eed952a29861449d432d1465cd00f77fda
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:35:32 -0400

    i sandbox

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,5 @@
-export function sandbox() {}
+import { function_param_new } from "./function_param_new.mjs";
+import { function_wrap } from "./function_wrap.mjs";
+export async function sandbox() {
+  await function_wrap(function_param_new.name, "function_params_new");
+}
