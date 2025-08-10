f5ad17995ed7e75bd281b30219acbf494eb99e99
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:34:19 -0400

    md 1

--- web/public/src/function_new_js.mjs
+++ web/public/src/function_new_js.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_new_js(name) {
-  marker();
   await function_new(name);
+  marker();
 }
