76c61f4faf34b4eba0150423e5a27ca3353e10d7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 19:57:00 -0400

    md 1

--- web/public/src/function_new_js.mjs
+++ web/public/src/function_new_js.mjs
@@ -6,10 +6,10 @@ import { function_new } from "./function_new.mjs";
 export async function function_new_js(f_name_unprefixed) {
   let combined = function_name_combine("js", f_name_unprefixed);
   await function_new(combined);
-  marker();
   let result = await function_transform(
     combined,
     async function lambda2(ast2) {},
   );
+  marker();
   async function lambda(ast) {}
 }
