6d1edcf395f9be35dfe0fecef10799ec14115060
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 14:57:52 -0400

    i function_wrap

--- web/public/src/function_wrap.mjs
+++ web/public/src/function_wrap.mjs
@@ -1,8 +1,8 @@
+import { function_new_transform } from "./function_new_transform.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_wrap(f_name, f_name_wrapped) {
-  await function_new(f_name);
-  marker();
-  await function_transform(f_name, async function lambda(ast) {});
+  await function_new_transform(f_name, lambda);
+  async function lambda(ast) {}
 }
