769822bb5a598a89f847846c8f9a88f04225853e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:59:18 -0400

    i function_new_declaration

--- public/src/function_new_declaration.mjs
+++ public/src/function_new_declaration.mjs
@@ -3,10 +3,15 @@ import { js_code_export } from "./js_code_export.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { js_declaration_name } from "./js_declaration_name.mjs";
+import { js_parse } from "./js_parse.mjs";
+import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
 export async function function_new_declaration(declaration) {
   let name = js_declaration_name(declaration);
   const f_path = function_name_to_path(name);
   let code_declaration = js_unparse(declaration);
   const contents = js_code_export(code_declaration);
-  await file_write(f_path, contents);
+  let ast = js_parse(contents);
+  js_imports_missing_add(ast);
+  let contents_import = js_unparse(ast);
+  await file_write(f_path, contents_import);
 }
