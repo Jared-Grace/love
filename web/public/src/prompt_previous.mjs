47a895cb3cc137eebe561ffd342cbaa9ba2241d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 21:38:49 -0400

    i prompt_previous

--- web/public/src/prompt_previous.mjs
+++ web/public/src/prompt_previous.mjs
@@ -1,13 +1,16 @@
+import {function_aliases} from './function_aliases.mjs';
 import {function_run_line} from './function_run_line.mjs';
 import {data_value} from './data_value.mjs';
 import {data_transform} from './data_transform.mjs';
 import {function_run_prompt} from './function_run_prompt.mjs';
 import {list_last} from './list_last.mjs';
-import { list_filter } from './list_filter.mjs';
-import { list_difference } from './list_difference.mjs';
+import {list_filter} from './list_filter.mjs';
+import {list_difference} from './list_difference.mjs';
 export async function prompt_previous() {
+  aliases = function_aliases();
   let prompts = await data_value("prompts");
-  list_difference(prompts, [prompt_previous.name])
-  let previous = list_last(prompts);return previous
+  list_difference(prompts, [prompt_previous.name]);
+  let previous = list_last(prompts);
+  return previous;
   await function_run_line(previous);
 }
