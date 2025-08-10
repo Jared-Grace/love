73dcce434f69623b9bd872d28a1ba5041a79bf91
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:47:31 -0400

    au function_run_line

--- web/public/src/function_run_line.mjs
+++ web/public/src/function_run_line.mjs
@@ -1,17 +1,13 @@
-import {list_unique_reverse} from './list_unique_reverse.mjs';
-import {list_copy_reverse} from './list_copy_reverse.mjs';
-import {list_reverse} from './list_reverse.mjs';
-import {function_run_git} from './function_run_git.mjs';
-import {list_unique} from './list_unique.mjs';
-import {list_add} from './list_add.mjs';
-import {data_transform} from './data_transform.mjs';
-import {list_copy} from './list_copy.mjs';
+import { function_run_line_parse } from "./function_run_line_parse.mjs";
+import { list_unique_reverse } from "./list_unique_reverse.mjs";
+import { list_copy_reverse } from "./list_copy_reverse.mjs";
+import { list_reverse } from "./list_reverse.mjs";
+import { function_run_git } from "./function_run_git.mjs";
+import { list_unique } from "./list_unique.mjs";
+import { list_add } from "./list_add.mjs";
+import { data_transform } from "./data_transform.mjs";
+import { list_copy } from "./list_copy.mjs";
 export async function function_run_line(line) {
-  const [f_name, ...args] = line.split(" ");
-  await data_transform("prompts", [], previous => {
-    list_add(previous, line);
-    let unique = list_unique_reverse(previous);
-    return unique;
-  });
+  let { f_name, args } = await function_run_line_parse(line);
   await function_run_git(f_name, args);
 }
