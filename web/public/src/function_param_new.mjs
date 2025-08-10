58b3fd57dfebd44b9744fd7f7c1916e0e25a9d65
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 00:15:15 -0400

    mc js_declaration_single

--- web/public/src/function_param_new.mjs
+++ web/public/src/function_param_new.mjs
@@ -1,8 +1,9 @@
+import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 export async function function_param_new(f_name) {
   await function_transform(f_name, (ast) => {
-
-  marker();
+    marker();
+    js_declaration_single(parsed);
   });
 }
