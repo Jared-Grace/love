b45f824cf3420bdcbab3330f9f72ab105a7ae764
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:00:39 -0400

    i function_param_new

--- web/public/src/function_param_new.mjs
+++ web/public/src/function_param_new.mjs
@@ -1,3 +1,4 @@
+import { function_params_new } from "./function_params_new.mjs";
 import { js_declaration_param_add } from "./js_declaration_param_add.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
@@ -7,9 +8,5 @@ import { log } from "./log.mjs";
 import { list_add } from "./list_add.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 export async function function_param_new(param_name) {
-  let f_name = await data_function_current_get();
-  await function_transform(f_name, (ast) => {
-    let declaration = js_declaration_single(ast);
-    js_declaration_param_add(declaration, param_name);
-  });
+  function_params_new([param_name]);
 }
