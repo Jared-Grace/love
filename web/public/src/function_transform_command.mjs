ecd82ca3ea8da92693da36fcb1d48ab43855a315
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 17:27:58 -0400

    i function_transform_command

--- web/public/src/function_transform_command.mjs
+++ web/public/src/function_transform_command.mjs
@@ -1,4 +1,5 @@
+import { function_import } from "./function_import.mjs";
 import { function_transform } from "./function_transform.mjs";
-export async function function_transform_command(f_name, lambda$ast) {
-  return await function_transform(f_name, lambda$ast);
+export async function function_transform_command(f_name, transformer) {
+  return await function_transform(f_name, function_import());
 }
