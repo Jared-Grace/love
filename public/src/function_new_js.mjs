57fa843e16b69dfe3097f6a919baef9306c389b5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:34:27 -0400

    mc ft

--- public/src/function_new_js.mjs
+++ public/src/function_new_js.mjs
@@ -1,6 +1,8 @@
+import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_new_js(name) {
   await function_new(name);
   marker();
+  await function_transform(f_name, lambda);
 }
