cb8972ae525a090529546afdfbabc61bcf906640
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 19:52:17 -0400

    au list_remove

--- web/public/src/list_remove.mjs
+++ web/public/src/list_remove.mjs
@@ -1,7 +1,7 @@
+import { list_remove_at } from "./list_remove_at.mjs";
 import { error } from "./error.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 export function list_remove(list, item) {
   const index = list_index_of(list, item);
-
-  list.splice(index, 1);
+  list_remove_at(list, index);
 }
