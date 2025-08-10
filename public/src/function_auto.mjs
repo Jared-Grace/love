40199dfee8f4642ff0755f88f21026cc3def2f92
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:27:19 -0400

    ir lambda ast=>{}

--- public/src/function_auto.mjs
+++ public/src/function_auto.mjs
@@ -2,5 +2,5 @@ import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 export async function function_auto() {
   marker();
-  await function_transform(f_name, lambda);
+  await function_transform(f_name, (ast) => {});
 }
