aa44940a02ff977873ab50cbe682b2fd82d7f8f8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:50:37 -0400

    i function_dependencies

--- web/public/src/function_dependencies.mjs
+++ web/public/src/function_dependencies.mjs
@@ -1,6 +1,9 @@
+import { js_imports } from "./js_imports.mjs";
 import { function_parse } from "./function_parse.mjs";
 import { marker } from "./marker.mjs";
 export async function function_dependencies(f_name) {
   let parsed = await function_parse(f_name);
   let { ast } = parsed;
+  let imports = js_imports(ast);
+  return imports;
 }
