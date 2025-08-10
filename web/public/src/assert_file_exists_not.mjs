bf7e8e1a1a412c22310cc6fdb10a007cbe91544c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:50:14 -0400

    i function_delete_if_exists

--- web/public/src/assert_file_exists_not.mjs
+++ web/public/src/assert_file_exists_not.mjs
@@ -2,6 +2,6 @@ import {error} from './error.mjs';
 import {file_exists} from './file_exists.mjs';
 export async function assert_file_exists_not(file_path_new) {
   if (await file_exists(file_path_new)) {
-    error();
+    error(file_path_new);
   }
 }
--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -1,11 +1,12 @@
-import {function_run_line} from './function_run_line.mjs';
-import {log} from "./log.mjs";
-import {data_transform} from "./data_transform.mjs";
-import {command_line_read} from "./command_line_read.mjs";
-import {function_run_git} from "./function_run_git.mjs";
-import {list_add} from "./list_add.mjs";
-import {list_unique} from "./list_unique.mjs";
+import { function_run_line_git } from "./function_run_line_git.mjs";
+import { function_run_line } from "./function_run_line.mjs";
+import { log } from "./log.mjs";
+import { data_transform } from "./data_transform.mjs";
+import { command_line_read } from "./command_line_read.mjs";
+import { function_run_git } from "./function_run_git.mjs";
+import { list_add } from "./list_add.mjs";
+import { list_unique } from "./list_unique.mjs";
 export async function function_run_prompt() {
   let line = await command_line_read("");
-  await function_run_line(line);
+  await function_run_line_git(line);
 }
