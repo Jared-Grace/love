57342cac0568ed8d3c12a4577c75fac5a17cc238
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:03:14 -0400

    au function_name_combine

--- public/src/function_name_combine.mjs
+++ public/src/function_name_combine.mjs
@@ -1,3 +1,4 @@
+import { function_name_separator } from "./function_name_separator.mjs";
 export function function_name_combine(left, right) {
-  return left + "_" + right;
+  return left + function_name_separator() + right;
 }
