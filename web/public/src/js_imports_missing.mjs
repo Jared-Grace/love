ca86bd52690981623cffbc58eaa33213c6f7eed0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:48:26 -0400

    i js_imports_missing

--- web/public/src/js_imports_missing.mjs
+++ web/public/src/js_imports_missing.mjs
@@ -1,13 +1,14 @@
-import {js_declaration_single} from './js_declaration_single.mjs';
-import {list_intersect} from './list_intersect.mjs';
-import {functions_names} from './functions_names.mjs';
-import {list_difference} from './list_difference.mjs';
-import {list_concat} from './list_concat.mjs';
-import {object_property_get} from './object_property_get.mjs';
-import {js_identifiers_names} from './js_identifiers_names.mjs';
-import {js_imports} from './js_imports.mjs';
+import { js_declaration_name } from "./js_declaration_name.mjs";
+import { js_declaration_single } from "./js_declaration_single.mjs";
+import { list_intersect } from "./list_intersect.mjs";
+import { functions_names } from "./functions_names.mjs";
+import { list_difference } from "./list_difference.mjs";
+import { list_concat } from "./list_concat.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { js_identifiers_names } from "./js_identifiers_names.mjs";
+import { js_imports } from "./js_imports.mjs";
 export function js_imports_missing(ast) {
-    let declaration = js_declaration_single(ast);
+  let declaration = js_declaration_single(ast);
   let imports = js_imports(ast);
   let identifiers = js_identifiers_names(declaration);
   let name = js_declaration_name(declaration);
@@ -17,9 +18,8 @@ export function js_imports_missing(ast) {
   let imports_missing = list_intersect(missing, f_names);
   return imports_missing;
 }
-
 function js_declaration_name(declaration) {
-    let declaration_id = object_property_get(declaration, "id");
-    let name = object_property_get(declaration_id, "name");
-    return name;
+  let declaration_id = object_property_get(declaration, "id");
+  let name = object_property_get(declaration_id, "name");
+  return name;
 }
