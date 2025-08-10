1c112d6ee0154b23bcba051d637ee0e8352464b1
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:47:16 -0400

    i function_new

--- web/public/src/function_new.mjs
+++ web/public/src/function_new.mjs
@@ -1,3 +1,4 @@
+import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_code_export } from "./js_code_export.mjs";
 import { js_code_declaration } from "./js_code_declaration.mjs";
 import { file_write } from "./file_write.mjs";
@@ -8,8 +9,9 @@ import { file_overwrite } from "./file_overwrite.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_open } from "./file_open.mjs";
 export async function function_new(f_name) {
-    const code_declaration = js_code_declaration(f_name, "", false);
-    const f_path = function_name_to_path(f_name);
+  const code_declaration = js_code_declaration(f_name, "", false);
+  let declaration = js_parse_expression(code_declaration);
+  const f_path = function_name_to_path(f_name);
   const contents = js_code_export(code_declaration);
   await file_write(f_path, contents);
   await function_open(f_name);
