0450383442dad542f7e9549e9a6bdc99e94f3752
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:00:47 -0400

    i visit_unique_async

--- web/public/src/visit_unique_async.mjs
+++ web/public/src/visit_unique_async.mjs
@@ -1,6 +1,18 @@
+import { list_add } from "./list_add.mjs";
+import { list_difference } from "./list_difference.mjs";
 import { visit_async } from "./visit_async.mjs";
 export async function visit_unique_async(node, children_get, on_each) {
-    
   let found = [];
-  await visit_async(node, children_get, on_each);
+  await visit_async(
+    node,
+    (node) => {
+      let children = children_get();
+      return list_difference(children, found);
+    },
+    (v) => {
+      let { node } = v;
+      list_add(found, node);
+      on_each(v);
+    },
+  );
 }
