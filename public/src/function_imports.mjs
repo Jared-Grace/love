0bc519bef6a999d16320530052fd9fe503960d69
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:52:52 -0400

    i function_imports

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -1,1 +1,8 @@
-export function function_imports(  ){}
+import { js_imports } from "./js_imports.mjs";
+import { function_parse } from "./function_parse.mjs";
+export async function function_imports(f_name) {
+  let parsed = await function_parse(f_name);
+  let { ast } = parsed;
+  let imports = js_imports(ast);
+  return imports;
+}
