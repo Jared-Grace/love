02a3170e8be396f592a30711188b17d17cbf3121
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:48:20 -0400

    i js_declaration_name

--- web/public/src/js_declaration_name.mjs
+++ web/public/src/js_declaration_name.mjs
@@ -1,1 +1,6 @@
-export function js_declaration_name(  ){}
+import { object_property_get } from "./object_property_get.mjs";
+export function js_declaration_name(declaration) {
+  let declaration_id = object_property_get(declaration, "id");
+  let name = object_property_get(declaration_id, "name");
+  return name;
+}
