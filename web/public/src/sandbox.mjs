21fc3a92473e852fa5de5051b9e998e9a63a7545
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:36:05 -0400

    i sandbox

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,5 +1,8 @@
+import { function_delete } from "./function_delete.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function sandbox() {
-  await function_wrap(function_param_new.name, "function_params_new");
+  const n = "function_params_new";
+  await function_wrap(function_param_new.name, n);
+  await function_delete(n);
 }
