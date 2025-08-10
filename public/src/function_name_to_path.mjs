7f5dc0cfc6c9d9d8860845e3f136ff3c4b493897
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:33:12 -0400

    i function_name_to_path

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -1,10 +1,12 @@
+import { string_includes } from "./string_includes.mjs";
 import { error } from "./error.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { functions_path } from "./functions_path.mjs";
 import path from "path";
-export function function_name_to_path(f_name) {if (0&&string_includes(f_name,'.')) {error()
-
-}
+export function function_name_to_path(f_name) {
+  if (0 && string_includes(f_name, ".")) {
+    error();
+  }
   let second = [functions_path(), function_name_to_base(f_name)];
   let joined = path.join(...second);
   return joined;
