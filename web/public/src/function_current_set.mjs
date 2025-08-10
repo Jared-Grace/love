a7ac19b9c503f2ba8561f57e1687bb05b62bcfab
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:40:44 -0400

    i function_current_set

--- web/public/src/function_current_set.mjs
+++ web/public/src/function_current_set.mjs
@@ -1,1 +1,4 @@
-export function function_current_set(  ){}
+import {data_transform} from './data_transform.mjs';
+export async function function_current_set(f_name) {
+  await data_transform("function_current", null, previous => f_name);
+}
