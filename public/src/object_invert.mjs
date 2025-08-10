c1b50d84ee6c148bab3f782bc51d0d9ad4f315c2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 22:59:22 -0400

    s init

--- public/src/object_invert.mjs
+++ public/src/object_invert.mjs
@@ -1,1 +1,7 @@
-export function object_invert(  ){}
+import { each } from "./each.mjs";
+import { object_properties } from "./object_properties.mjs";
+
+export function object_invert( object ){each(object_properties(object), p=>{
+
+})
+}
