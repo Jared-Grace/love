db812bdb71e0b7720509de8abe0e9e8ac24f0792
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:05:29 -0400

    de function_identifier_replace_suffix

--- web/public/src/function_identifier_replace_suffix.mjs
+++
@@ -1,7 +0,0 @@
-import { function_identifier_replace } from "./function_identifier_replace.mjs";
-export async function function_identifier_replace_suffix(
-  identifier_name,
-  replacement,
-) {
-  return await function_identifier_replace(identifier_name, replacement);
-}
