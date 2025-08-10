aa195cb6bdec3782a2eedc520e45d1f8e8089261
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:50:00 -0400

    i function_identifier_replace

--- web/public/src/function_identifier_replace.mjs
+++ web/public/src/function_identifier_replace.mjs
@@ -1,22 +1,14 @@
-import { each } from "./each.mjs";
-import { function_transform } from "./function_transform.mjs";
-import { js_identifiers } from "./js_identifiers.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { marker } from "./marker.mjs";
-import { object_property_get } from "./object_property_get.mjs";
+import {js_identifiers_named} from './js_identifiers_named.mjs';
+import {each} from "./each.mjs";
+import {function_transform} from "./function_transform.mjs";
+import {js_identifiers} from "./js_identifiers.mjs";
+import {list_filter} from "./list_filter.mjs";
+import {marker} from "./marker.mjs";
+import {object_property_get} from "./object_property_get.mjs";
 export async function function_identifier_replace(identifier_name) {
-  await function_transform(f_name, (ast) => {
+  await function_transform(f_name, ast => {
     let identifiers_named = js_identifiers_named(ast, identifier_name);
     marker();
-    each(identifiers_named, (i) => object_replace(i));
+    each(identifiers_named, i => object_replace(i));
   });
 }
-function js_identifiers_named(ast, identifier_name) {
-    let identifiers = js_identifiers(ast);
-    let identifiers_named = list_filter(
-        identifiers,
-        (i) => object_property_get(i, "name") === identifier_name
-    );
-    return identifiers_named;
-}
-
