29507ba13354944e88979b8d214722a1de9ba02d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:10:04 -0400

    m

--- web/public/src/function_new_js.mjs
+++ web/public/src/function_new_js.mjs
@@ -3,6 +3,7 @@ import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_new_js(f_name_unprefixed) {
+  marker();
   await function_new_transform(f_name_unprefixed, lambda);
   async function lambda(ast) {}
 }
