6e0672c9e8dd57a41d35fe08c43550c2da32418e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:28:27 -0400

    au js_stack_declaration_asyncify

---
+++ public/src/js_declaration_asyncify.mjs
@@ -0,0 +1,5 @@
+import {object_property_get} from './object_property_get.mjs';
+import {object_property_set} from './object_property_set.mjs';
+export function js_declaration_asyncify(declaration, other) {
+  object_property_set(declaration, "async", object_property_get(other, "async"));
+}
--- public/src/js_stack_declaration_asyncify.mjs
+++ public/src/js_stack_declaration_asyncify.mjs
@@ -1,12 +1,15 @@
-import {object_property_get} from './object_property_get.mjs';
-import {object_property_set} from './object_property_set.mjs';
-import {list_last} from './list_last.mjs';
-import {js_node_type_is} from './js_node_type_is.mjs';
-import {js_node_is} from './js_node_is.mjs';
-import {list_filter} from './list_filter.mjs';
+import { js_declaration_asyncify } from "./js_declaration_asyncify.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { list_last } from "./list_last.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { js_node_is } from "./js_node_is.mjs";
+import { list_filter } from "./list_filter.mjs";
 export function js_stack_declaration_asyncify(stack, declaration) {
   let stack_nodes = list_filter(stack, js_node_is);
-  let fds = list_filter(stack_nodes, n => js_node_type_is(n, "FunctionDeclaration"));
+  let fds = list_filter(stack_nodes, (n) =>
+    js_node_type_is(n, "FunctionDeclaration"),
+  );
   let last = list_last(fds);
-  object_property_set(last, "async", object_property_get(declaration, "async"));
+  js_declaration_asyncify(last, declaration);
 }
