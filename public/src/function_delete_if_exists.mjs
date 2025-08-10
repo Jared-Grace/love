1e742da3468796e0a17b1ad849f1036cac6b095a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:50:14 -0400

    i function_delete_if_exists

--- public/src/function_delete_if_exists.mjs
+++ public/src/function_delete_if_exists.mjs
@@ -1,8 +1,10 @@
+import { log } from "./log.mjs";
 import { function_exists } from "./function_exists.mjs";
 import { function_delete } from "./function_delete.mjs";
 import { file_exists } from "./file_exists.mjs";
 export async function function_delete_if_exists(n) {
   if (await function_exists(n)) {
+    log("here");
     await function_delete(n);
   }
 }
