cea6ef2e42b1cb87492a45847ffde08bc56e2428
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:13:06 -0400

    i function_dependencies_code_copy

--- web/public/src/function_dependencies_code_copy.mjs
+++ web/public/src/function_dependencies_code_copy.mjs
@@ -1,1 +1,4 @@
-export function function_dependencies_code_copy(  ){}
+import { function_dependencies_code } from "./function_dependencies_code.mjs";
+export async function function_dependencies_code_copy(f_name) {
+  let code = await function_dependencies_code(f_name);
+}
