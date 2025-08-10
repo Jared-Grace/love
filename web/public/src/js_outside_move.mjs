9820f0f0b1be92d599b5d4b4ae534b7fb3036c48
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:55:08 -0400

    i js_outside_move

--- web/public/src/js_outside_move.mjs
+++ web/public/src/js_outside_move.mjs
@@ -1,10 +1,16 @@
+import { function_new_declaration } from "./function_new_declaration.mjs";
+import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 import { each_async } from "./each_async.mjs";
 import { log } from "./log.mjs";
 import { marker } from "./marker.mjs";
 export async function js_outside_move(ast) {
   let { body } = ast;
   let fds = list_filter_property(body, "type", "FunctionDeclaration");
+  await each_async(fds, async (fd) => {
+    await assert_file_exists_not();
+  });
   marker();
-  await each_async(fds, async(fd) => {
-    await function_new_declaration(declaration);});
+  await each_async(fds, async (fd) => {
+    await function_new_declaration();
+  });
 }
