c43352798b25fba7532a2c0269750a18226d8a6d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:54:26 -0400

    i object_properties_delete

--- web/public/src/object_properties_delete.mjs
+++ web/public/src/object_properties_delete.mjs
@@ -1,1 +1,6 @@
-export function object_properties_delete(  ){}
+import {object_property_delete} from './object_property_delete.mjs';
+import {each} from "./each.mjs";
+import {object_properties} from "./object_properties.mjs";
+export function object_properties_delete() {
+  each(object_properties(obj), p => object_property_delete(obj, p));
+}
