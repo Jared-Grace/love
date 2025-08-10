a774566778545b37e4444ce7f524855d62e87422
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:59:02 -0400

    s unique

--- web/public/src/visit_unique_async.mjs
+++ web/public/src/visit_unique_async.mjs
@@ -1,4 +1,6 @@
 import { visit_async } from "./visit_async.mjs";
 export async function visit_unique_async(node, children_get, on_each) {
+    
+  let found = [];
   await visit_async(node, children_get, on_each);
 }
