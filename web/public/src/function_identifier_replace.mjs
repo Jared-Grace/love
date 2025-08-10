ba841a58af538a80da17b018478c21aadd40861d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:45:26 -0400

    i function_identifier_replace

--- web/public/src/function_identifier_replace.mjs
+++ web/public/src/function_identifier_replace.mjs
@@ -3,6 +3,7 @@ import {js_identifiers} from './js_identifiers.mjs';
 import {marker} from './marker.mjs';
 export async function function_identifier_replace() {
   marker();
-  await function_transform(f_name, lambda);
-  js_identifiers(ast);
+  await function_transform(f_name, ast => {
+    let identifiers = js_identifiers(ast);
+  });
 }
