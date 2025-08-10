67615163be1c923f790c76e4de6ec1f1cb253f16
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:30:58 -0400

    s ht,n

--- web/public/src/functions_search.mjs
+++ web/public/src/functions_search.mjs
@@ -9,7 +9,7 @@ import { list_filter } from "./list_filter.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 export async function functions_search(search) {
-  let terms = string_split(s, ",");
+  let terms = string_split(search, ",");
   let f_names = functions_names();
   let f_names_search = list_filter(f_names, (n) =>
     list_all(terms, (term) => string_includes(n, term)),
