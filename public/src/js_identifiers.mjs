118ef66e3c1956c2419f34298a1bb52cca2293c5
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:44:37 -0400

    fcs function_identifier_replace

--- public/src/js_identifiers.mjs
+++ public/src/js_identifiers.mjs
@@ -1,11 +1,11 @@
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
-export function js_identifiers_names(ast) {
+export function js_identifiers(ast) {
   return list_adder_unique((la) => {
     js_visit_type(ast, "Identifier", (v) => {
       let { node } = v;
-      la(object_property_get(node, "name"));
+      la((node));
     });
   });
 }
