a3682c27913ddf518d8ddd347f0a5b7ff5957eb8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:28:53 -0400

    i list_all

--- public/src/list_all.mjs
+++ public/src/list_all.mjs
@@ -1,1 +1,5 @@
-export function list_all() {}
+import { list_empty_is } from "./list_empty_is.mjs";
+import { list_filter } from "./list_filter.mjs";
+export function list_all(list, predicate) {
+  return list_empty_is(list_filter(list, predicate));
+}
