aaaf3d91792f4840a2ad3ae2fc88fe42c2283acf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:14:16 -0400

    au data_prompts

--- public/src/data_prompts.mjs
+++ public/src/data_prompts.mjs
@@ -1,4 +1,9 @@
+import { list_slice_end } from "./list_slice_end.mjs";
 import { data_value } from "./data_value.mjs";
+import { list_size } from "./list_size.mjs";
+import { list_slice } from "./list_slice.mjs";
 export function data_prompts() {
-  return data_value("prompts");
+  let prompts = data_value("prompts");
+  let offset = 10;
+  list_slice_end(prompts, offset);
 }
---
+++ public/src/list_slice_end.mjs
@@ -0,0 +1,10 @@
+import {list_slice} from './list_slice.mjs';
+import {list_size} from './list_size.mjs';
+export function list_slice_end(prompts, offset) {
+  let end = list_size(prompts);
+  let start = end - offset;
+  if (start < 0) {
+    start = 0;
+  }
+  list_slice(prompts, start, end);
+}
