0b7086406d0e8acfdfe1da412c5bac01bcad2f5b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:21:22 -0400

    i function_delete

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -6,9 +6,8 @@ import { js_code_call } from "./js_code_call.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_open } from "./file_open.mjs";
+import { file_delete } from "./file_delete.mjs";
 export async function function_delete(f_name) {
   const f_path = function_name_to_path(f_name);
-  const contents = "export " + js_code_declaration(f_name, "", false);
-  await file_write(f_path, contents);
-  await function_open(f_name);
+  await file_delete(f_path);
 }
