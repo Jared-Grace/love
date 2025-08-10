0f7a4f4b46c58b7e1e4065854c612601c6052766
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:00:15 -0400

    c list_map_property list_filter_property

---
+++ web/public/src/list_filter_property.mjs
@@ -0,0 +1,5 @@
+import { object_property_get } from "./object_property_get.mjs";
+import { list_map } from "./list_map.mjs";
+export function list_filter_property(list, property_name) {
+  return list_map(list, (item) => object_property_get(item, property_name));
+}
