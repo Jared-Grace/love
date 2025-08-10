4f507964deb11bd98753ffd6e7b0f56c6d0a5b7d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:56:25 -0400

    i function_dependencies

--- web/public/src/visit_async.mjs
+++ web/public/src/visit_async.mjs
@@ -1,6 +1,5 @@
 import { visit_filter_async } from "./visit_filter_async.mjs";
 import { tautology } from "./tautology.mjs";
-import { visit_filter } from "./visit_filter.mjs";
 export async function visit_async(node, children_get, on_each) {
   await visit_filter_async(node, children_get, tautology, on_each, []);
 }
