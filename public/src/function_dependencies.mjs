eb32ca4fd11038a05e30fcf27acaa39ce47b6f1a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:56:25 -0400

    i function_dependencies

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,8 +1,11 @@
+import { visit_async } from "./visit_async.mjs";
 import { function_imports } from "./function_imports.mjs";
 import { js_imports } from "./js_imports.mjs";
 import { function_parse } from "./function_parse.mjs";
 import { marker } from "./marker.mjs";
 export async function function_dependencies(f_name) {
+  let found = [];
   let imports = await function_imports(f_name);
+  visit_async();
   return imports;
 }
