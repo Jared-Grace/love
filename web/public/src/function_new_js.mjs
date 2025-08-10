14c06d5b61decaeabdf33ee6318604cd791773cc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 13:34:45 -0400

    ir name f_name

--- web/public/src/function_new_js.mjs
+++ web/public/src/function_new_js.mjs
@@ -1,8 +1,8 @@
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
-export async function function_new_js(name) {
-  await function_new(name);
+export async function function_new_js(f_name) {
+  await function_new(f_name);
   marker();
   await function_transform(f_name, lambda);
 }
