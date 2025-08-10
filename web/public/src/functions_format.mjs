f702bc6df4daf56be3b223266e5883898386f765
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:07:06 -0400

    n list_wait

--- web/public/src/functions_format.mjs
+++ web/public/src/functions_format.mjs
@@ -3,5 +3,9 @@ import { functions_names } from "./functions_names.mjs";
 import { list_map } from "./list_map.mjs";
 export async function functions_format() {
   let mapped = list_map(await functions_names(), function_format);
-  await Promise.all(mapped);
+  await list_wait(mapped);
 }
+function list_wait(mapped) {
+    return Promise.all(mapped);
+}
+
