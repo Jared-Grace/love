7b8e1a3034ad409cd348290a63aba4ba2e43f973
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:51:37 -0400

    i object_replace

--- public/src/object_replace.mjs
+++ public/src/object_replace.mjs
@@ -1,1 +1,5 @@
-export function object_replace(  ){}
+import {object_merge} from "./object_merge.mjs";
+export function object_replace(to, from) {
+  object_delete(to);
+  object_merge(to, from);
+}
