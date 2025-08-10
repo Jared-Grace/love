1899e3d671b65094fb8e67117393855ae3a47d5b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:08:50 -0400

    fs function_new_js

--- web/public/src/function_identifier_replace.mjs
+++ web/public/src/function_identifier_replace.mjs
@@ -10,10 +10,11 @@ import {js_identifiers} from "./js_identifiers.mjs";
 import {list_filter} from "./list_filter.mjs";
 import {marker} from "./marker.mjs";
 import {object_property_get} from "./object_property_get.mjs";
+import { log } from './log.mjs';
 export async function function_identifier_replace(identifier_name, replacement) {
   let f_name = data_function_current_get();
   await function_transform(f_name, ast => {
-    let identifiers_named = js_identifiers_named(ast, identifier_name);
+    let identifiers_named = js_identifiers_named(ast, identifier_name);log(identifier_name)
     assert_not(list_empty_is(identifiers_named));
     marker();
     each(identifiers_named, i => object_replace(i, js_parse_expression(replacement)));
