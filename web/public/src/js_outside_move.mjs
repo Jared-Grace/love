2c70ec7d6adef85f50d05f6ce1bd4c9b141263b2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:41:53 -0400

    f js_outside_move

--- web/public/src/js_outside_move.mjs
+++ web/public/src/js_outside_move.mjs
@@ -1,6 +1,7 @@
 import { log } from "./log.mjs";
+import { marker } from "./marker.mjs";
 export function js_outside_move(ast) {
   let { body } = ast;
-  if (0) list_filter_property(body, "type", "FunctionDeclaration");
-  log(body);
+  let fds= list_filter_property(body, "type", "FunctionDeclaration");
+  marker()
 }
