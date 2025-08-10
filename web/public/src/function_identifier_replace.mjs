eddee8de6159e254dfc14c4908f90a76a390daef
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:56:34 -0400

    i function_identifier_replace

--- web/public/src/function_identifier_replace.mjs
+++ web/public/src/function_identifier_replace.mjs
@@ -1,4 +1,5 @@
-import {assert} from './assert.mjs';
+import {list_empty_is} from './list_empty_is.mjs';
+import {assert_not} from './assert_not.mjs';
 import {data_function_current_get} from './data_function_current_get.mjs';
 import {object_replace} from './object_replace.mjs';
 import {js_parse_expression} from './js_parse_expression.mjs';
@@ -9,12 +10,11 @@ import {js_identifiers} from "./js_identifiers.mjs";
 import {list_filter} from "./list_filter.mjs";
 import {marker} from "./marker.mjs";
 import {object_property_get} from "./object_property_get.mjs";
-import {assert} from 'console';
 export async function function_identifier_replace(identifier_name, replacement) {
   let f_name = data_function_current_get();
   await function_transform(f_name, ast => {
     let identifiers_named = js_identifiers_named(ast, identifier_name);
-    assert(list_empty_not_is(identifiers_named));
+    assert_not(list_empty_is(identifiers_named));
     marker();
     each(identifiers_named, i => object_replace(i, js_parse_expression(replacement)));
   });
