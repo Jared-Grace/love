b04132cfb8f01975a0b4cc7722437c69b128c712
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:52:41 -0400

    n function_imports

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -3,19 +3,19 @@ import { fileURLToPath } from "url";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 
-export async function function_import(funcName) {
+export async function function_import(f_name) {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  funcName = await function_name_unalias(funcName);
-  let joined = function_name_to_base(funcName);
+  f_name = await function_name_unalias(f_name);
+  let joined = function_name_to_base(f_name);
   const f_path = path.join(...[__dirname, joined]);
   const imported = await import(`file://${f_path}`);
 
-  const imported_fn = imported[funcName];
+  const imported_fn = imported[f_name];
 
   if (typeof imported_fn !== "function") {
     throw new Error(
-      `❌ The module "${funcName}" does not export a default function.`,
+      `❌ The module "${f_name}" does not export a default function.`,
     );
   }
 
