eea1b37da99100f57b282fd1cd69801b175ed585
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:53:02 -0400

    i fd

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,3 +1,4 @@
+import { function_imports } from "./function_imports.mjs";
 import { js_imports } from "./js_imports.mjs";
 import { function_parse } from "./function_parse.mjs";
 import { marker } from "./marker.mjs";
@@ -5,10 +6,3 @@ export async function function_dependencies(f_name) {
   let imports = await function_imports(f_name);
   return imports;
 }
-async function function_imports(f_name) {
-    let parsed = await function_parse(f_name);
-    let { ast } = parsed;
-    let imports = js_imports(ast);
-    return imports;
-}
-
