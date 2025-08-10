f5e0a8a6b99a0c5cdb376cefac3235fbfb863297
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:58:43 -0400

    sb

--- public/src/function_params_new.mjs
+++ public/src/function_params_new.mjs
@@ -1,4 +1,4 @@
 import { function_param_new } from "./function_param_new.mjs";
 export async function function_params_new(param_name) {
-  await function_param_new(param_name);
+  return await function_param_new(param_name);
 }
