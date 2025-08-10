3605783b3976939410f8c9305c2c54cd95dbbbc2
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 02:13:38 -0400

    i function_dependencies_code_copy

--- public/src/function_dependencies_code_copy.mjs
+++ public/src/function_dependencies_code_copy.mjs
@@ -1,4 +1,6 @@
+import { clipboard_copy } from "./clipboard_copy.mjs";
 import { function_dependencies_code } from "./function_dependencies_code.mjs";
 export async function function_dependencies_code_copy(f_name) {
   let code = await function_dependencies_code(f_name);
+  await clipboard_copy(code);
 }
