ab1e109d52abfd2830911554efc1ebba071e4a82
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:46:29 -0400

    mc each

--- web/public/src/function_identifier_replace.mjs
+++ web/public/src/function_identifier_replace.mjs
@@ -1,9 +1,11 @@
+import {each} from './each.mjs';
 import {function_transform} from './function_transform.mjs';
 import {js_identifiers} from './js_identifiers.mjs';
 import {marker} from './marker.mjs';
 export async function function_identifier_replace() {
-  marker();
   await function_transform(f_name, ast => {
     let identifiers = js_identifiers(ast);
+    marker();
+    each(list, lambda);
   });
 }
