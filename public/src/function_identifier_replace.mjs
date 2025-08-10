58812ce713ef05e755773badd1fcdf7b60c4d528
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:45:02 -0400

    mc ft

--- public/src/function_identifier_replace.mjs
+++ public/src/function_identifier_replace.mjs
@@ -1,6 +1,8 @@
+import {function_transform} from './function_transform.mjs';
 import {js_identifiers} from './js_identifiers.mjs';
 import {marker} from './marker.mjs';
 export async function function_identifier_replace() {
   marker();
+  await function_transform(f_name, lambda);
   js_identifiers(ast);
 }
