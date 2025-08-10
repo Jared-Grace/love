dabef67c97e9c2619e0909f9038194f06f1af28a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 14:56:42 -0400

    i function_wrap

--- web/public/src/function_wrap.mjs
+++ web/public/src/function_wrap.mjs
@@ -1,1 +1,8 @@
-export function function_wrap(f_name, f_name_wrapped) {}
+import { function_transform } from "./function_transform.mjs";
+import { marker } from "./marker.mjs";
+import { function_new } from "./function_new.mjs";
+export async function function_wrap(f_name, f_name_wrapped) {
+  await function_new(f_name);
+  marker();
+  await function_transform(f_name, async function lambda(ast) {});
+}
