1ce66ff9d27889c2aecd680de951b1a315f7873b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:58:41 -0400

    i visit_unique_async

--- public/src/visit_unique_async.mjs
+++ public/src/visit_unique_async.mjs
@@ -1,1 +1,4 @@
-export async function visit_async(node, children_get, on_each){await visit_async(node, children_get, on_each)}
+import { visit_async } from "./visit_async.mjs";
+export async function visit_unique_async(node, children_get, on_each) {
+  await visit_async(node, children_get, on_each);
+}
