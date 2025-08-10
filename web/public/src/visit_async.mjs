04c8a9f9e08ba4774f7467aac7af79311a4ef8d0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:56:10 -0400

    i visit_async

--- web/public/src/visit_async.mjs
+++ web/public/src/visit_async.mjs
@@ -1,5 +1,6 @@
+import { visit_filter_async } from "./visit_filter_async.mjs";
 import { tautology } from "./tautology.mjs";
 import { visit_filter } from "./visit_filter.mjs";
-export function visit_async(node, children_get, on_each) {
-  visit_filter(node, children_get, tautology, on_each, []);
+export async function visit_async(node, children_get, on_each) {
+  await visit_filter_async(node, children_get, tautology, on_each, []);
 }
--- web/public/src/visit_filter_async.mjs
+++ web/public/src/visit_filter_async.mjs
@@ -1,5 +1,4 @@
 import { visit_filter_recursive_async } from "./visit_filter_recursive_async.mjs";
-import { visit_filter_recursive } from "./visit_filter_recursive.mjs";
-export function visit_filter_async(node, children_get, filter, on_each) {
-  visit_filter_recursive_async(node, children_get, filter, on_each, []);
+export async function visit_filter_async(node, children_get, filter, on_each) {
+await  visit_filter_recursive_async(node, children_get, filter, on_each, []);
 }
