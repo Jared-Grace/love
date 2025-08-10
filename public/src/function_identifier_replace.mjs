306f167892857367f9a7914ed9b0d497b4ad1a6b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:50:24 -0400

    i function_identifier_replace

--- public/src/function_identifier_replace.mjs
+++ public/src/function_identifier_replace.mjs
@@ -1,3 +1,4 @@
+import {js_parse_expression} from './js_parse_expression.mjs';
 import {js_identifiers_named} from './js_identifiers_named.mjs';
 import {each} from "./each.mjs";
 import {function_transform} from "./function_transform.mjs";
@@ -5,10 +6,10 @@ import {js_identifiers} from "./js_identifiers.mjs";
 import {list_filter} from "./list_filter.mjs";
 import {marker} from "./marker.mjs";
 import {object_property_get} from "./object_property_get.mjs";
-export async function function_identifier_replace(identifier_name) {
+export async function function_identifier_replace(identifier_name, replacement) {
   await function_transform(f_name, ast => {
     let identifiers_named = js_identifiers_named(ast, identifier_name);
     marker();
-    each(identifiers_named, i => object_replace(i));
+    each(identifiers_named, i => object_replace(i, js_parse_expression(replacement)));
   });
 }
