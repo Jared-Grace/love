2d0cd12a4920763d06b484d9af0f509b730ea5e7
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 23:02:05 -0400

    i object_invert

--- web/public/src/object_invert.mjs
+++ web/public/src/object_invert.mjs
@@ -1,12 +1,12 @@
-import { each } from "./each.mjs";
-import { object_properties } from "./object_properties.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-
-export function object_invert( object ){
-    let inverted={}
-    each(object_properties(object), p=>{
-        let value=object_property_get(object,p);
-let list=object_property_initialize(inverted,value)
-})
-return inverted
+import {object_property_initialize} from './object_property_initialize.mjs';
+import {each} from "./each.mjs";
+import {object_properties} from "./object_properties.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+export function object_invert(object) {
+  let inverted = {};
+  each(object_properties(object), p => {
+    let value = object_property_get(object, p);
+    let list = object_property_initialize(inverted, value);
+  });
+  return inverted;
 }
