50d90ec7cadfe4c524f4aeddd08fb537edd1509e
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 23:14:42 -0400

    i prompt_previous

--- public/src/prompt_previous.mjs
+++ public/src/prompt_previous.mjs
@@ -1,3 +1,4 @@
+import {function_aliases_inverted} from './function_aliases_inverted.mjs';
 import {object_invert} from './object_invert.mjs';
 import {function_aliases} from './function_aliases.mjs';
 import {function_run_line} from './function_run_line.mjs';
@@ -8,8 +9,7 @@ import {list_last} from './list_last.mjs';
 import {list_filter} from './list_filter.mjs';
 import {list_difference} from './list_difference.mjs';
 export async function prompt_previous() {
-  let aliases = await function_aliases();
-  inverted = object_invert(aliases);
+  let inverted = await function_aliases_inverted();
   let prompts = await data_value("prompts");
   list_difference(prompts, [prompt_previous.name]);
   let previous = list_last(prompts);
