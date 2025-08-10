fcc3e85304f6d62cdbeb803391cc3aa06aa5f091
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 18:27:50 -0400

    i marker_call_replace

--- public/src/marker_call_replace.mjs
+++ public/src/marker_call_replace.mjs
@@ -1,3 +1,4 @@
+import { object_replace } from "./object_replace.mjs";
 import { integer_to } from "./integer_to.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
@@ -18,7 +19,7 @@ import { list_index_of } from "./list_index_of.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export async function marker_call_replace(arg_index) {
+export async function marker_call_replace(arg_index, code_replacement) {
   arg_index = integer_to(arg_index);
   let f_name = await data_function_current_get();
   return list_adder_async(async (la) => {
@@ -36,8 +37,10 @@ export async function marker_call_replace(arg_index) {
         return;
       }
       let { arguments: arguments2 } = expression;
-      let arg_index_at=list_get(arguments2,arg_index)
-      la(arg_index_at);
+      let arg_index_at = list_get(arguments2, arg_index);
+      let replacement = js_parse_expression(code_replacement);
+      object_replace(arg_index_at, replacement);
+      la(js_unparse(expression));
       return;
       let { declarations } = next;
       let declaration = list_single(declarations);
