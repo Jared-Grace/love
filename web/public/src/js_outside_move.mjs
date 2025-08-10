5b4c5cd125f6d4e3a8c3f8931b40d06d9b911107
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 12:42:02 -0400

    mc each_async

--- web/public/src/js_outside_move.mjs
+++ web/public/src/js_outside_move.mjs
@@ -1,7 +1,9 @@
+import { each_async } from "./each_async.mjs";
 import { log } from "./log.mjs";
 import { marker } from "./marker.mjs";
-export function js_outside_move(ast) {
+export async function js_outside_move(ast) {
   let { body } = ast;
-  let fds= list_filter_property(body, "type", "FunctionDeclaration");
-  marker()
+  let fds = list_filter_property(body, "type", "FunctionDeclaration");
+  marker();
+  await each_async(list, lambda);
 }
