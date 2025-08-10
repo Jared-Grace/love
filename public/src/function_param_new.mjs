b399fb2b43a38a2c8e2cfd0254f3e6754360a75b
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 20:43:14 -0400

    n function_identifier_replace

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -2,5 +2,5 @@ import {function_transform} from './function_transform.mjs';
 import {marker} from './marker.mjs';
 export async function function_param_new(f_name) {
   marker();
-  await function_transform(f_name2, lambda);
+  await function_transform(f_name, lambda);
 }
