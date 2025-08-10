badf9306769bf4d04ba0b7374fe42c0c3f04e482
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 19:58:00 -0400

    c function_identifier_replace_suffix_add function_identifier_replace_suffix_remove

---
+++ public/src/function_identifier_replace_suffix_remove.mjs
@@ -0,0 +1,11 @@
+import { function_name_combine } from "./function_name_combine.mjs";
+import { function_identifier_replace } from "./function_identifier_replace.mjs";
+export async function function_identifier_replace_suffix_remove(
+  identifier_name,
+  suffix_to_add,
+) {
+  return await function_identifier_replace(
+    identifier_name,
+    function_name_combine(identifier_name, suffix_to_add),
+  );
+}
