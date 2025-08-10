beea5e562de4c4455e4c05753bb20606e848b48a
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:18:04 -0400

    i ps

--- public/src/data_prompts.mjs
+++ public/src/data_prompts.mjs
@@ -1,9 +1,11 @@
+import { log } from "./log.mjs";
 import { list_slice_end } from "./list_slice_end.mjs";
 import { data_value } from "./data_value.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_slice } from "./list_slice.mjs";
 export function data_prompts() {
   let prompts = data_value("prompts");
+  log(prompts);
   let offset = 10;
-  list_slice_end(prompts, offset);
+  return list_slice_end(prompts, offset);
 }
