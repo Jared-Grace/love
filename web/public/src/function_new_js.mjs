59edef90ab4f23666f2eeadb9050cf719d7a7131
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:09:55 -0400

    irs f_name unprefixed

--- web/public/src/function_new_js.mjs
+++ web/public/src/function_new_js.mjs
@@ -2,7 +2,7 @@ import { function_new_transform } from "./function_new_transform.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
-export async function function_new_js(f_name) {
-  await function_new_transform(f_name, lambda);
+export async function function_new_js(f_name_unprefixed) {
+  await function_new_transform(f_name_unprefixed, lambda);
   async function lambda(ast) {}
 }
