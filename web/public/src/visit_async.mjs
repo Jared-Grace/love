9d0c38c83012beaa9401f26d4968197fae09ef23
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 01:54:37 -0400

    c visit_filter visit_filter_async

--- web/public/src/visit_async.mjs
+++ web/public/src/visit_async.mjs
@@ -1,5 +1,5 @@
 import { tautology } from "./tautology.mjs";
 import { visit_filter } from "./visit_filter.mjs";
-export function visit(node, children_get, on_each) {
+export function visit_async(node, children_get, on_each) {
   visit_filter(node, children_get, tautology, on_each, []);
 }
