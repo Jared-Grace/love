f7c97397cf3cfa15301a4e0b8cdcdd75f117db19
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-09 23:01:21 -0400

    i each_object

--- public/src/each_object.mjs
+++ public/src/each_object.mjs
@@ -1,12 +1,9 @@
-import { each } from "./each.mjs";
-import { object_properties } from "./object_properties.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-
-export function object_invert( object ){
-    let inverted={}
-    each(object_properties(object), p=>{
-        let value=object_property_get(object,p);
-let list=object_property_initialize(inverted,value)
-})
-return inverted
+import {each} from "./each.mjs";
+import {object_properties} from "./object_properties.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+export function each_object(object, lambda) {
+  each(object_properties(object), p => {
+    let value = object_property_get(object, p);
+    lambda(value, p);
+  });
 }
