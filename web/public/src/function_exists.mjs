f2d1fe57c1ca24bfa31e153ed229f15191fd3f52
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:38:55 -0400

    i function_exists

--- web/public/src/function_exists.mjs
+++ web/public/src/function_exists.mjs
@@ -1,3 +1,6 @@
-export async function function_exists() {
-  await file_exists(file_path);
+import { file_exists } from "./file_exists.mjs";
+import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
+export async function function_exists(f_name) {
+  let file_path = function_name_to_path_unalias(f_name);
+  return await file_exists(file_path);
 }
