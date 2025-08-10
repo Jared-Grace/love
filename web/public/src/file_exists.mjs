7ae6c5f8937e04293882d781e90dae22da42b03d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 15:53:04 -0400

    function_exists function_params_new

--- web/public/src/file_exists.mjs
+++ web/public/src/file_exists.mjs
@@ -1,6 +1,12 @@
+import { error } from "./error.mjs";
 import { log } from "./log.mjs";
+import { promise_is } from "./promise_is.mjs";
+import { string_is } from "./string_is.mjs";
 
 export async function file_exists(file_path) {
+    if (promise_is(file_path)) {
+        error()
+    }
   let fs = await import("fs");
   let { access } = fs.promises;
   let { constants } = fs;
