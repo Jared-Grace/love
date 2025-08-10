40631441fa836aba4aaed9f25610e835312726d4
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 17:31:54 -0400

    i js_return_name

--- web/public/src/js_return_name.mjs
+++ web/public/src/js_return_name.mjs
@@ -1,13 +1,18 @@
+import { log } from "./log.mjs";
 import { list_last } from "./list_last.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_return_name(ast) {
   let body_block = js_declaration_single_block_blody(ast);
   let name = null;
   if (list_empty_not_is(body_block)) {
     let last = list_last(body_block);
+    if (js_node_type_is(last, "ReturnStatement")) {
+      log(last);
+    }
   }
   return name;
 }
