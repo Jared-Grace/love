562afdecaad06b1720cbeb017a5dc6326eb39131
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 16:27:24 -0400

    au js_return_name

--- web/public/src/js_return_name.mjs
+++ web/public/src/js_return_name.mjs
@@ -1,10 +1,11 @@
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 export function js_return_name(ast) {
   let body_block = js_declaration_single_block_blody(ast);
   let name = null;
-  if (!list_empty_is(body_block)) {
+  if (list_empty_not_is(body_block)) {
   }
   return name;
 }
