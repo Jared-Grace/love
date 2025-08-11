7f4e8da7ec4c9869e38c5ed8195f2639b8231dd3
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 15:34:25 -0700

    o s

--- web/public/src/function_aliases_search.mjs
+++ web/public/src/function_aliases_search.mjs
@@ -1,4 +1,4 @@
 import { function_aliases_inverted } from "./function_aliases_inverted.mjs";
 export async function function_aliases_search() {
-  return await function_aliases_inverted();
+  let aliases= await function_aliases_inverted();
 }
