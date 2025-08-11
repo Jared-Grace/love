03aa8d2fdaea7ecb0380cfd5e701a67913c19036
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 14:43:10 -0700

    au data_function_current_get

--- web/public/src/data_function_current_get.mjs
+++ web/public/src/data_function_current_get.mjs
@@ -1,9 +1,8 @@
-import {object_property_get} from './object_property_get.mjs';
-import {data_get} from './data_get.mjs';
+import { data_property_get } from "./data_property_get.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { data_get } from "./data_get.mjs";
 export async function data_function_current_get() {
   const property_name = "function_current";
-  var d = await data_get(property_name, null);
-  let {data} = d;
-  let f_name = object_property_get(data, property_name);
+  let f_name = await data_property_get(property_name);
   return f_name;
 }
