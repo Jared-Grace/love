ae311c30a3a4d3aca933d4d893ed6aa764dd7bcf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 23:02:43 -0400

    au js_atomize

---
+++ web/public/src/list_next.mjs
@@ -0,0 +1,7 @@
+import {list_get} from './list_get.mjs';
+import {list_index_of_next} from './list_index_of_next.mjs';
+export function list_next(list, item) {
+  let index_next = list_index_of_next(list, item);
+  let next = list_get(list, index_next);
+  return next;
+}
