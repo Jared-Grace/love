d509605073c990baf97acedd8ac1c0c1d281e2c3
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 09:03:02 -0400

    mn

--- public/src/js_atomize.mjs
+++ public/src/js_atomize.mjs
@@ -36,6 +36,7 @@ export async function js_atomize(ast) {
     if (list_is(stack1)) {
       let variable_name = "v";
       marker();
+      marker(2);
       let { callee } = node;
       if (js_node_type_is(callee, "Identifier")) {
         let { name } = callee;
