2c0f9e91b21b981bf497031a89c05934377689aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 13:13:46 -0400

    au marker_expand

---
+++ public/src/each_reverse.mjs
@@ -0,0 +1,6 @@
+import {each} from './each.mjs';
+import {list_copy_reverse} from './list_copy_reverse.mjs';
+export function each_reverse(list, lambda) {
+  let reversed = list_copy_reverse(list);
+  each(reversed, lambda);
+}
