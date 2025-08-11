aa2c8f49085b76ce21832355d2003367481dc508
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 11:56:50 -0400

    i marker_expand

--- web/public/src/js_identifier_is.mjs
+++ web/public/src/js_identifier_is.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is } from "./js_node_type_is.mjs";
-export function js_identifier_is(node, type) {
-  return js_node_type_is(node, type);
+export function js_identifier_is(node,) {
+  return js_node_type_is(node, 'Identifier');
 }
