dee34cc683973c56f298721ca04c5bf81d47162c
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 16:15:26 -0700

    au function_transform_marker

---
+++ public/src/function_transform_marker_arg.mjs
@@ -0,0 +1,13 @@
+import {object_merge} from './object_merge.mjs';
+import {list_get_end} from './list_get_end.mjs';
+export function function_transform_marker_arg(v, ast) {
+  let {stack} = v;
+  let stack1 = list_get_end(stack, 1);
+  let stack2 = list_get_end(stack, 2);
+  let a = object_merge({
+    stack2,
+    stack1,
+    ast
+  }, v);
+  return a;
+}
