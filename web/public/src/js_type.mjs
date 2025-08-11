705ce83103e7b9bf12cb45afe2186dfc2129ab36
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 08:48:12 -0400

    p

--- web/public/src/js_type.mjs
+++ web/public/src/js_type.mjs
@@ -1,7 +1,8 @@
 import {js_visit_type} from './js_visit_type.mjs';
 import {list_adder} from './list_adder.mjs';
 export function js_type(ast, node_type) {
-  return list_adder(la => {
-    js_visit_type(ast, node_type, la);
-  });
+    const vs = list_adder(la => {
+        js_visit_type(ast, node_type, la);
+    });
+  return vs;
 }
