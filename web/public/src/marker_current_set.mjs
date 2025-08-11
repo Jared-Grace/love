f0ad48b2605014293ba5a8d967caf95d4744fff0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 09:22:15 -0400

    c function_current_set marker_current_set

---
+++ web/public/src/marker_current_set.mjs
@@ -0,0 +1,4 @@
+import { data_transform } from "./data_transform.mjs";
+export async function marker_current_set(f_name) {
+  await data_transform("function_current", null, (previous) => f_name);
+}
