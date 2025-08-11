1314c83326a30275f78ac443c7d75690c38f65bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 15:37:59 -0700

    au search_generic

--- public/src/function_aliases_search.mjs
+++ public/src/function_aliases_search.mjs
@@ -1,4 +1,7 @@
 import { function_aliases_inverted } from "./function_aliases_inverted.mjs";
-export async function function_aliases_search() {
-  let aliases= await function_aliases_inverted();
+import { object_properties } from "./object_properties.mjs";
+export async function function_aliases_search(search) {
+  let names_to_aliases= await function_aliases_inverted();
+let names=object_properties(names_to_aliases)
+  let names_search = search_generic(search, names);
 }
--- public/src/search_generic.mjs
+++ public/src/search_generic.mjs
@@ -1,9 +1,18 @@
-import {string_includes} from './string_includes.mjs';
-import {list_all} from './list_all.mjs';
-import {list_filter} from './list_filter.mjs';
-import {string_split} from './string_split.mjs';
-export function search_generic(search, f_names) {
+import { object_property_set } from "./object_property_set.mjs";
+import { each } from "./each.mjs";
+import { string_includes } from "./string_includes.mjs";
+import { list_all } from "./list_all.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { string_split } from "./string_split.mjs";
+export function search_generic(search, f_names, value_get) {
   let terms = string_split(search, ",");
-  let f_names_search = list_filter(f_names, n => list_all(terms, term => string_includes(n, term)));
-  return f_names_search;
+  let f_names_search = list_filter(f_names, (n) =>
+    list_all(terms, (term) => string_includes(n, term)),
+  );
+  let result = {};
+  each(f_names_search, (n) => {
+    let value = value_get(n);
+    object_property_set(result, n, value);
+  });
+  return result;
 }
