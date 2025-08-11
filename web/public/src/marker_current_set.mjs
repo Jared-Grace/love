52e2fd5e2cd324ffb1a82cac49686e62ff10b5fb
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 15:31:43 -0700

    o f

--- web/public/src/marker_current_set.mjs
+++ web/public/src/marker_current_set.mjs
@@ -1,4 +1,4 @@
 import { data_transform } from "./data_transform.mjs";
 export async function marker_current_set(name_marker) {
-  return await data_transform("marker_current", null, (previous) => name_marker);
+  await data_transform("marker_current", null, (previous) => name_marker);
 }
