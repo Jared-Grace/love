15f8ddb171ccd75c5083571d7ef0e30f124d4f4d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:47:53 -0400

    a fts function_transform_command_self

--- web/public/src/function_transform_command_self.mjs
+++ web/public/src/function_transform_command_self.mjs
@@ -1,7 +1,6 @@
 import { function_transform_command } from "./function_transform_command.mjs";
 export async function function_transform_command_self(
   f_name,
-  transformer_name,
 ) {
-  return await function_transform_command(f_name, transformer_name);
+  return await function_transform_command(f_name, f_name);
 }
