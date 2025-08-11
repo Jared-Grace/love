3a528362c138b62fb7d0a788f9c51e77374e03e8
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 11:54:30 -0400

    au js_declaration_params_names

--- web/public/src/js_declaration_params_names.mjs
+++ web/public/src/js_declaration_params_names.mjs
@@ -1,5 +1,8 @@
-import {object_property_get} from './object_property_get.mjs';
-import {list_map_property} from './list_map_property.mjs';
+import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_map_property } from "./list_map_property.mjs";
 export function js_declaration_params_names(declaration) {
-  return list_map_property(object_property_get(declaration, "params"), "name");
+  const params = object_property_get(declaration, "params");
+  const params_names = js_identifiers_to_names(params);
+  return params_names;
 }
