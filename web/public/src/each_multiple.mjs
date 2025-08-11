83f755e4fe3e41a54b132e6a5871cb7160f913ad
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 12:46:36 -0400

    au marker_expand

---
+++ web/public/src/each_multiple.mjs
@@ -0,0 +1,10 @@
+import {lists_get} from './lists_get.mjs';
+import {each_index} from './each_index.mjs';
+import {list_first} from './list_first.mjs';
+export function each_multiple(lists, lambda) {
+  let first = list_first(lists);
+  each_index(first, (item, index) => {
+    let items = lists_get(lists, index);
+    lambda(items);
+  });
+}
---
+++ web/public/src/each_pair.mjs
@@ -0,0 +1,8 @@
+export function each_pair(list_a, list_b, lambda3) {
+  let lists = [list_a, list_b];
+  each_multiple(lists, lambda);
+  function lambda(items) {
+    let [a, b] = items;
+    lambda3(a, b);
+  }
+}
