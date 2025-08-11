b33a910e70465c4e699f5ad7d9c9d8fd5bbdeba1
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 16:29:49 -0700

    md 1

--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -11,11 +11,11 @@ export async function html_new(name) {
   const file_name = file_name_html(name);
   const file_path = folder_public_combine(file_name);
   marker("1");
-  marker("2");
   const body = `<script type="module">
     import { sayHello } from './greetings.js';
     sayHello('World');
   </script>`;
+  marker("2");
   let contents = `<!DOCTYPE html>
 <html lang="en">
 <head>
