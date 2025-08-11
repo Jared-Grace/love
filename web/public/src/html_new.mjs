dc73b3bb8fdccb88c99897397e9cb91de18c8b6c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 08:36:55 -0400

    au html_new

--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -1,14 +1,16 @@
-import {file_write} from './file_write.mjs';
-import {file_open} from "./file_open.mjs";
-import {folder_public_combine} from "./folder_public_combine.mjs";
-import {file_name_html} from "./file_name_html.mjs";
-import {folder_public} from "./folder_public.mjs";
-import {path_join} from "./path_join.mjs";
-import {file_overwrite} from "./file_overwrite.mjs";
+import { file_write } from "./file_write.mjs";
+import { file_open } from "./file_open.mjs";
+import { folder_public_combine } from "./folder_public_combine.mjs";
+import { file_name_html } from "./file_name_html.mjs";
+import { folder_public } from "./folder_public.mjs";
+import { path_join } from "./path_join.mjs";
+import { file_overwrite } from "./file_overwrite.mjs";
 export async function html_new(name) {
   const file_name = file_name_html(name);
   const file_path = folder_public_combine(file_name);
-  await file_write(file_path, `<!DOCTYPE html>
+  await file_write(
+    file_path,
+    `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -17,6 +19,7 @@ export async function html_new(name) {
 <body>
 
 </body>
-</html>`);
+</html>`,
+  );
   await file_open(file_path);
 }
