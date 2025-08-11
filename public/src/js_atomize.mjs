9427934cd3ea6a319ad767213e3cd95ccf6917bf
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:29:16 -0400

    i js_atomize

--- public/src/js_atomize.mjs
+++ public/src/js_atomize.mjs
@@ -1,3 +1,4 @@
+import { js_stack_last } from "./js_stack_last.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { js_identifier_unique } from "./js_identifier_unique.mjs";
 import { js_identifiers } from "./js_identifiers.mjs";
@@ -20,7 +21,9 @@ export function js_atomize(ast) {
     const stack1 = list_get_end(stack, 1);
     if (list_is(stack1)) {
       let unique = js_identifier_unique(existing, "v");
-      let copy = object_copy(node)
+      let copy = object_copy(node);
+      let last = js_stack_last(stack, "BlockStatement");
+      let { body } = last;
     }
   });
 }
