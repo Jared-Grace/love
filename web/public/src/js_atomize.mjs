8b010eb930336b5dcf70b0049d43c62090d141aa
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:09:04 -0400

    ir ast2 ast

--- web/public/src/js_atomize.mjs
+++ web/public/src/js_atomize.mjs
@@ -2,5 +2,5 @@ import { js_type } from "./js_type.mjs";
 import { marker } from "./marker.mjs";
 export function js_atomize(ast) {
   marker();
-  let result = js_type(ast2, node_type);
+  let result = js_type(ast, node_type);
 }
