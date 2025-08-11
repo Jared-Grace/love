e9277bb0cf84fd47785884cb5f0937dd8616092d
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 16:12:34 -0700

    au marker_move

---
+++ web/public/src/js_marker_named_ast.mjs
@@ -0,0 +1,10 @@
+import {list_single} from './list_single.mjs';
+import {js_marker_named} from './js_marker_named.mjs';
+import {list_filter} from './list_filter.mjs';
+import {js_type} from './js_type.mjs';
+export function js_marker_named_ast(ast, m_name_from) {
+  let visitors = js_type(ast, "CallExpression");
+  let filtered = list_filter(visitors, v => js_marker_named(v, m_name_from));
+  let marker_v = list_single(filtered);
+  return marker_v;
+}
