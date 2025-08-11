ecab4f08deb0f379e5a584483f9b058c4741a9e0
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 11:47:07 -0400

    au marker_call_replace_generic

--- public/src/marker_call_replace_generic.mjs
+++ public/src/marker_call_replace_generic.mjs
@@ -1,60 +1,48 @@
-import {js_unparse} from './js_unparse.mjs';
-import {js_imports_missing_add} from './js_imports_missing_add.mjs';
-import {list_get} from './list_get.mjs';
-import {object_property_get} from './object_property_get.mjs';
-import {list_single} from './list_single.mjs';
-import {list_multiple_is} from './list_multiple_is.mjs';
-import {js_node_type_is} from './js_node_type_is.mjs';
-import {marker_next_get} from './marker_next_get.mjs';
-import {function_transform_marker} from './function_transform_marker.mjs';
-import {list_adder_async} from './list_adder_async.mjs';
-import {data_function_current_get} from './data_function_current_get.mjs';
-import {integer_to} from './integer_to.mjs';
-import { object_merge } from './object_merge.mjs';
+import { js_statement_call_get } from "./js_statement_call_get.mjs";
+import { js_unparse } from "./js_unparse.mjs";
+import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
+import { list_get } from "./list_get.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_single } from "./list_single.mjs";
+import { list_multiple_is } from "./list_multiple_is.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { marker_next_get } from "./marker_next_get.mjs";
+import { function_transform_marker } from "./function_transform_marker.mjs";
+import { list_adder_async } from "./list_adder_async.mjs";
+import { data_function_current_get } from "./data_function_current_get.mjs";
+import { integer_to } from "./integer_to.mjs";
+import { object_merge } from "./object_merge.mjs";
 export async function marker_call_replace_generic(input, lambda$a) {
   let arg_index = integer_to(input);
   let f_name = await data_function_current_get();
-  return list_adder_async(async la => {
+  return list_adder_async(async (la) => {
     await function_transform_marker(f_name, lambda);
     async function lambda(a) {
       let next = marker_next_get(a);
       let expression = js_statement_call_get(next);
-      if (expression===null){return}
-      let {arguments: arguments2} = expression;
+      if (expression === null) {
+        return;
+      }
+      let { arguments: arguments2 } = expression;
       let replaced = null;
       if (input === "c") {
-        let {callee} = expression;
+        let { callee } = expression;
         replaced = callee;
       } else {
         let arg_index_at = list_get(arguments2, arg_index);
         replaced = arg_index_at;
       }
-      await lambda$a(object_merge({replaced},a));
-      let {ast} = a;
+      let to = object_merge(
+        {
+          replaced,
+        },
+        a,
+      );
+      await lambda$a(to);
+      let { ast } = a;
       js_imports_missing_add(ast);
-      la(js_unparse(next));
+      let output = js_unparse(next);
+      la(output);
     }
   });
 }
-function js_statement_call_get(next) {
-    let expression = null;
-    if (js_node_type_is(next, "ExpressionStatement")) {
-        let { expression: expression_next } = next;
-        expression = expression_next;
-    } else if (js_node_type_is(next, "VariableDeclaration")) {
-        let { declarations } = next;
-        if (list_multiple_is(declarations)) {
-            return null;
-        }
-        let declaration = list_single(declarations);
-        expression = object_property_get(declaration, "init");
-    }
-    if (js_node_type_is(expression, "AwaitExpression")) {
-        expression = object_property_get(expression, "argument");
-    }
-    if (!js_node_type_is(expression, "CallExpression")) {
-        return null;
-    }
-    return expression;
-}
-
