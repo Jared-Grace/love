4188c31cd08c1bdde2550b84bdb74258960b79d6
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:19:53 -0400

    i js_atomize

--- web/public/src/js_atomize.mjs
+++ web/public/src/js_atomize.mjs
@@ -1,14 +1,16 @@
+import { list_get_end } from "./list_get_end.mjs";
 import { each } from "./each.mjs";
 import { js_type } from "./js_type.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
+import { list_get } from "./list_get.mjs";
 import { log } from "./log.mjs";
 import { marker } from "./marker.mjs";
 export function js_atomize(ast) {
   marker();
   js_visit_type(ast, "CallExpression", (v) => {
     let { node } = v;
-    let { parent } = node;
-    log(parent);
+    let { stack } = node;
+    log(list_get_end(stack, 1));
   });
 }
