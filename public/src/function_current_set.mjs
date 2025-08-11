bffa925c4a4420b097f05117881311e9909dca56
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 14:34:40 -0700

    i function_current_set

--- public/src/function_current_set.mjs
+++ public/src/function_current_set.mjs
@@ -1,4 +1,6 @@
-import {data_transform} from './data_transform.mjs';
+import { marker_current_set } from "./marker_current_set.mjs";
+import { data_transform } from "./data_transform.mjs";
 export async function function_current_set(f_name) {
-  await data_transform("function_current", null, previous => f_name);
+  await data_transform("function_current", null, (previous) => f_name);
+  marker_current_set("1");
 }
