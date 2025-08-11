fd6f8b77846f974d1c6ebe4619112fc253b00a68
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:31:13 -0400

    i function_run_line

--- public/src/function_run_line.mjs
+++ public/src/function_run_line.mjs
@@ -1,3 +1,4 @@
+import { function_run } from "./function_run.mjs";
 import { function_run_log } from "./function_run_log.mjs";
 import { function_run_line_parse } from "./function_run_line_parse.mjs";
 import { list_unique_reverse } from "./list_unique_reverse.mjs";
@@ -10,5 +11,5 @@ import { data_transform } from "./data_transform.mjs";
 import { list_copy } from "./list_copy.mjs";
 export async function function_run_line(line) {
   let { f_name, args } = await function_run_line_parse(line);
-  await function_run_log(f_name, args);
+  await function_run(f_name, args);
 }
