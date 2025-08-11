3c0c9764b89400aa295fd1522fcfea3ff2f73d74
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 09:06:26 -0400

    mn

--- web/public/src/marker_new.mjs
+++ web/public/src/marker_new.mjs
@@ -14,14 +14,18 @@ import { data_function_current_get } from "./data_function_current_get.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 export async function marker_new() {
   let f_name = await data_function_current_get();
   return list_adder_async(async (la) => {
     await function_transform_marker(f_name, lambda);
     function lambda(a) {
       let { index, stack2 } = marker_next_index(a);
+      let value_string='2'
       let parsed_string = js_parse_expression('""');
-      log(parsed_string);
+      object_property_set(parsed_string,'value',value_string)
+      let code_string = js_unparse(parsed_string)
+      log(code_string);
       let code = js_code_call_args(marker.name, ["2"]);
       let parsed = js_parse_statement(code);
       list_insert(stack2, index, parsed);
