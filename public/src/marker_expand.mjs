1a13e680e8ff4a214f8651a796cdc3fdaa58c01c
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 13:05:30 -0400

    i marker_expand

--- public/src/marker_expand.mjs
+++ public/src/marker_expand.mjs
@@ -1,3 +1,5 @@
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
+import { list_intersect } from "./list_intersect.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { each_pair } from "./each_pair.mjs";
 import { list_concat } from "./list_concat.mjs";
@@ -25,6 +27,7 @@ import { function_parse } from "./function_parse.mjs";
 import { assert } from "./assert.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_first } from "./list_first.mjs";
+import { error } from "./error.mjs";
 export async function marker_expand() {
   let f_name = await data_function_current_get();
   return list_adder_async(async (la) => {
@@ -41,6 +44,10 @@ export async function marker_expand() {
       let { name } = callee;
       let { declaration, ast } = await function_parse_declaration(name);
       let identifiers = js_identifiers_names(ast);
+      let intesection = list_intersect(identifiers, arguments2);
+      if (list_empty_not_is(intesection)) {
+        error("todo");
+      }
       let params_names = js_declaration_params_names(declaration);
       each_pair(params_names, a_names, lambda3);
       function lambda3(param_name, a_name) {}
