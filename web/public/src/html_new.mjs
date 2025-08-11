e36c5fabda0c515bd95d74a503ebe79c10569f27
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 09:30:24 -0400

    m

--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { file_write } from "./file_write.mjs";
 import { file_open } from "./file_open.mjs";
 import { folder_public_combine } from "./folder_public_combine.mjs";
@@ -6,6 +7,7 @@ import { folder_public } from "./folder_public.mjs";
 import { path_join } from "./path_join.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 export async function html_new(name) {
+  marker();
   const file_name = file_name_html(name);
   const file_path = folder_public_combine(file_name);
   await file_write(
