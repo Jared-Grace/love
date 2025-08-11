58525aab347b97a1264323533dbc527c412cc553
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 09:51:55 -0400

    au marker_call_atomize

--- public/src/marker_call_atomize.mjs
+++ public/src/marker_call_atomize.mjs
@@ -1,3 +1,4 @@
+import { marker_call_replace_generic } from "./marker_call_replace_generic.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { integer_to } from "./integer_to.mjs";
@@ -22,44 +23,9 @@ import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
 export async function marker_call_atomize(input, code_replacement) {
-  let arg_index = integer_to(input);
-  let f_name = await data_function_current_get();
-  return list_adder_async(async (la) => {
-    await function_transform_marker(f_name, lambda);
-    function lambda(a) {
-      let next = marker_next_get(a);
-      let expression = null;
-      if (js_node_type_is(next, "ExpressionStatement")) {
-        let { expression: expression_next } = next;
-        expression = expression_next;
-      } else if (js_node_type_is(next, "VariableDeclaration")) {
-        let { declarations } = next;
-        if (list_multiple_is(declarations)) {
-          return;
-        }
-        let declaration = list_single(declarations);
-        expression = object_property_get(declaration, "init");
-      }
-      if (js_node_type_is(expression, "AwaitExpression")) {
-        expression = object_property_get(expression, "argument");
-      }
-      if (!js_node_type_is(expression, "CallExpression")) {
-        return;
-      }
-      let { arguments: arguments2 } = expression;
-      let replaced = null;
-      let replacement = js_parse_expression(code_replacement);
-      if (input === "c") {
-        let { callee } = expression;
-        replaced = callee;
-      } else {
-        let arg_index_at = list_get(arguments2, arg_index);
-        replaced = arg_index_at;
-      }
-      object_replace(replaced, replacement);
-      let { ast } = a;
-      js_imports_missing_add(ast);
-      la(js_unparse(next));
-    }
-  });
+  return await marker_call_replace_generic(input, lambda2);
+  function lambda2(replaced) {
+    let replacement = js_parse_expression(code_replacement);
+    object_replace(replaced, replacement);
+  }
 }
