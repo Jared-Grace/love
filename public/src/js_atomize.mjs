c7da52b553672857e8ec79dc60718d18dda1cabc
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:24:31 -0400

    i js_atomize

--- public/src/js_atomize.mjs
+++ public/src/js_atomize.mjs
@@ -1,3 +1,5 @@
+import { js_identifier_unique } from "./js_identifier_unique.mjs";
+import { js_identifiers } from "./js_identifiers.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 import { each } from "./each.mjs";
 import { js_type } from "./js_type.mjs";
@@ -9,12 +11,13 @@ import { marker } from "./marker.mjs";
 import { list_is } from "./list_is.mjs";
 export function js_atomize(ast) {
   marker();
+  let existing = js_identifiers(ast);
   js_visit_type(ast, "CallExpression", (v) => {
     let { node } = v;
     let { stack } = v;
-      const stack1 = list_get_end(stack, 1);
-    if (list_is(stack1)){
-        
+    const stack1 = list_get_end(stack, 1);
+    if (list_is(stack1)) {
+      let unique = js_identifier_unique(existing, "v");
     }
   });
 }
