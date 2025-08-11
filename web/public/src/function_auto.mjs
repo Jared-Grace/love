cd222be98d4663cddc600f51ca776d9eb4d4b8bb
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 20:49:17 -0400

    i au

--- web/public/src/function_auto.mjs
+++ web/public/src/function_auto.mjs
@@ -1,3 +1,4 @@
+import { js_atomize } from "./js_atomize.mjs";
 import { js_outside_move } from "./js_outside_move.mjs";
 import { log } from "./log.mjs";
 import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
@@ -8,7 +9,7 @@ import { each_async } from "./each_async.mjs";
 export async function function_auto(f_name) {
   marker();
   await function_transform(f_name, async (ast) => {
-    let transforms = [js_outside_move, js_imports_missing_add];
+    let transforms = [js_outside_move, js_imports_missing_add, js_atomize];
     await each_async(transforms, async (t) => await t(ast));
   });
 }
