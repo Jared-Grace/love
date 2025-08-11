c37ba296c57ea4d6eda7703e93996f714f713b0d
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 12:00:40 -0400

    i marker_expand

--- public/src/each_index.mjs
+++ public/src/each_index.mjs
@@ -1,4 +1,8 @@
 import { each } from "./each.mjs";
 export function each_index(list, lambda) {
-  return each(list, lambda);
+    let index=0
+  return each(list, item=> {
+    lambda(item,index)
+    index++
+  });
 }
