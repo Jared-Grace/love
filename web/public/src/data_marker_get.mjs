f09f9e3ececd46b6b7e1d4d4f8b643bf04545dc2
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 15:32:03 -0700

    de data_marker_get

--- web/public/src/data_marker_get.mjs
+++
@@ -1,8 +0,0 @@
-import { data_property_get } from "./data_property_get.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { data_get } from "./data_get.mjs";
-export async function data_marker_get() {
-  const property_name = "function_current";
-  let f_name = await data_property_get(property_name);
-  return f_name;
-}
