7683a74e98af753eb4a365160c5e2825d2fb5425
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 15:51:38 -0700

    c function_transform_marker marker_move

---
+++ web/public/src/marker_move.mjs
@@ -0,0 +1,38 @@
+import { js_marker_named } from "./js_marker_named.mjs";
+import { data_marker_current_get } from "./data_marker_current_get.mjs";
+import { log } from "./log.mjs";
+import { each } from "./each.mjs";
+import { js_type } from "./js_type.mjs";
+import { error } from "./error.mjs";
+import { list_is } from "./list_is.mjs";
+import { marker } from "./marker.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { js_node_is } from "./js_node_is.mjs";
+import { list_get_end } from "./list_get_end.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
+import { function_transform } from "./function_transform.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { each_async } from "./each_async.mjs";
+import { list_size } from "./list_size.mjs";
+import { list_empty_is } from "./list_empty_is.mjs";
+import { list_first } from "./list_first.mjs";
+export async function marker_move(f_name, lambda$a) {
+  let marker_name = await data_marker_current_get();
+  await function_transform(f_name, lambda_marker);
+  async function lambda_marker(ast) {
+    let visitors = js_type(ast, "CallExpression");
+    await each_async(visitors, async (v) => {
+      if (js_marker_named(v, marker_name)) {
+        let to = object_merge(
+          {
+            stack2,
+            stack1,
+            ast,
+          },
+          v,
+        );
+        await lambda$a(to);
+      }
+    });
+  }
+}
