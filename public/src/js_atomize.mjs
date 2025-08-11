f4be7f3d9e448a05389f8f7d87a70031ee549ebe
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-11 08:45:21 -0400

    md 1

--- public/src/js_atomize.mjs
+++ public/src/js_atomize.mjs
@@ -26,8 +26,8 @@ import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { function_parse } from "./function_parse.mjs";
 export async function js_atomize(ast) {
-  marker();
   let existing = js_identifiers(ast);
+  marker();
   let ces = js_type(ast, "CallExpression");
   await each_async(ces, async (v) => {
     let { node } = v;
