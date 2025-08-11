ad219ee2d20aa7d272df68ae1ebb7d7f886ea83a
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 14:28:56 -0700

    au marker_new

--- web/public/src/marker_new.mjs
+++ web/public/src/marker_new.mjs
@@ -1,3 +1,4 @@
+import { js_marker_insert } from "./js_marker_insert.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 import { marker } from "./marker.mjs";
@@ -19,13 +20,10 @@ import { object_property_set } from "./object_property_set.mjs";
 import { object_property_delete } from "./object_property_delete.mjs";
 export async function marker_new() {
   let f_name = await data_function_current_get();
-    await function_transform_marker(f_name, lambda);
-    function lambda(a) {
-      let { index, stack2 } = marker_next_index(a);
-      let value_string = "2";
-      let code_string = js_code_string(value_string);
-      let code = js_code_call_args(marker.name, [code_string]);
-      let parsed = js_parse_statement(code);
-      list_insert(stack2, index, parsed);
-    }
+  await function_transform_marker(f_name, lambda);
+  function lambda(a) {
+    let { index, stack2 } = marker_next_index(a);
+    let value_string = "2";
+    js_marker_insert(value_string, stack2, index);
+  }
 }
