c25b0f23164057bad8a116f9173018934c63edb4
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 15:38:59 -0700

    i function_aliases_search

--- web/public/src/function_aliases_search.mjs
+++ web/public/src/function_aliases_search.mjs
@@ -1,7 +1,12 @@
+import { search_generic } from "./search_generic.mjs";
 import { function_aliases_inverted } from "./function_aliases_inverted.mjs";
 import { object_properties } from "./object_properties.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function function_aliases_search(search) {
-  let names_to_aliases= await function_aliases_inverted();
-let names=object_properties(names_to_aliases)
-  let names_search = search_generic(search, names);
+  let names_to_aliases = await function_aliases_inverted();
+  let names = object_properties(names_to_aliases);
+  let result = search_generic(search, names, (n) =>
+    object_property_get(names_to_aliases, n),
+  );
+  return result;
 }
