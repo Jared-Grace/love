4739794ea672ac8267c52f3b3dd613d4f1ac1f06
JESUSrose tolife <JESUSrosetolife@protonmail.com>
2025-08-10 22:54:38 -0400

    i marker_down

--- public/src/js_visit.mjs
+++ public/src/js_visit.mjs
@@ -3,9 +3,9 @@ import { promise_is } from "./promise_is.mjs";
 import { visit_filter } from "./visit_filter.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_visit_filter } from "./js_visit_filter.mjs";
-export function js_visit(parsed, on_each) {
-  if (promise_is(parsed)) {
+export function js_visit(ast, on_each) {
+  if (promise_is(ast)) {
     error();
   }
-  visit_filter(parsed, js_visit_children_get, js_visit_filter, on_each);
+  visit_filter(ast, js_visit_children_get, js_visit_filter, on_each);
 }
