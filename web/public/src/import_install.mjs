b561984210ace6da39618caa8ddfab7a2ae61383
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:05:40 -0700

    i import_install

--- web/public/src/import_install.mjs
+++ web/public/src/import_install.mjs
@@ -1,12 +1,17 @@
+import { npm_install } from "./npm_install.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function import_install(name) {
-      try {
-    await import(name);
+  try {
+    return await import(name);
     return true;
   } catch (err) {
-    if (err.code === 'ERR_MODULE_NOT_FOUND' || err.code === 'MODULE_NOT_FOUND') {
-      return false;
+    if (
+      err.code === "ERR_MODULE_NOT_FOUND" ||
+      err.code === "MODULE_NOT_FOUND"
+    ) {
+      npm_install(name);
+      return await import(name);
     }
-    throw err; // rethrow unexpected errors
+    throw err;
   }
 }
