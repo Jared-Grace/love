fe7a5a91f7ed9bd3f575b36c83b441fd53d975b1
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:22:46 -0700

    fw list_find list_find_property

--- public/src/list_find.mjs
+++ public/src/list_find.mjs
@@ -1,4 +1,6 @@
 import { list_filter } from "./list_filter.mjs";
+import { list_single } from "./list_single.mjs";
 export function list_find(list, filter) {
-  return list_filter(list, filter);
+  let filtered= list_filter(list, filter);
+  return list_single(filtered)
 }
