25bfb3f925badc493d65605a9200d5e4ab4843b6
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 11:00:07 -0700

    function_auto_path public\src\js_return_atomize.mjs

--- public/src/js_return_atomize.mjs
+++ public/src/js_return_atomize.mjs
@@ -1,13 +1,10 @@
 import { js_return_on } from "./js_return_on.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
+import { noop } from "./noop.mjs";
 export function js_return_atomize(ast) {
   js_visit_type(ast, "ReturnStatement", (v) => {
     let { node } = v;
-    js_return_on(node, identifier_if, identifier_not);
-    function identifier_if(argument) {
-      let { name: name_argument } = argument;
-      name = name_argument;
-    }
+    js_return_on(node, noop, identifier_not);
     function identifier_not() {
       name = "result";
     }
