2882b2d4c4d7edd6262358c7f4a6282e55fe142f
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 08:55:59 -0700

    mf 1 2 html_overwrite

--- public/src/html_new.mjs
+++ public/src/html_new.mjs
@@ -1,3 +1,4 @@
+import { html_overwrite } from "./html_overwrite.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 import { marker } from "./marker.mjs";
 import { file_write } from "./file_write.mjs";
@@ -16,17 +17,7 @@ export async function html_new(name) {
     sayHello('World');
   </script>`;
   marker("1");
-  let contents = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>${name}</title>
-</head>
-<body>
-  ${body}
-</body>
-</html>`;
-  await file_overwrite(file_path, contents);
+  await html_overwrite(name, body, file_path);
   marker("2");
   await file_open(file_path);
 }
