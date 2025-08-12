b249e911089904609651e50e266620beace741bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 16:55:53 -0700

    c string_suffix_without string_prefix_without

---
+++ public/src/string_prefix_without.mjs
@@ -0,0 +1,10 @@
+import { string_ends_with } from "./string_ends_with.mjs";
+import { error } from "./error.mjs";
+import { string_slice } from "./string_slice.mjs";
+import { string_size } from "./string_size.mjs";
+export function string_prefix_without(s, suffix) {
+  if (!string_ends_with(s, suffix)) {
+    error();
+  }
+  return string_slice(s, 0, string_size(s) - string_size(suffix));
+}
