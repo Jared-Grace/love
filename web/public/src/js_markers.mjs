2537d9d53e7ade34f34281e2a63de20182464ad0
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 13:55:54 -0700

    function_auto_path public\src\function_transform_marker_all.mjs

---
+++ web/public/src/js_markers.mjs
@@ -0,0 +1,8 @@
+import {js_marker_is} from './js_marker_is.mjs';
+import {list_filter} from './list_filter.mjs';
+import {js_type} from './js_type.mjs';
+export function js_markers(ast) {
+  let visitors = js_type(ast, "CallExpression");
+  let markers = list_filter(visitors, js_marker_is);
+  return markers;
+}
