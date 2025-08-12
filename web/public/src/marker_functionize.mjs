de4824984528b784299b6ff863d9e2f5674211c6
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 17:47:34 -0700

    s declarat

--- web/public/src/marker_functionize.mjs
+++ web/public/src/marker_functionize.mjs
@@ -49,9 +49,9 @@ export async function marker_functionize(m_name_from, m_name_to, f_name_new) {
     let { stack2: stack2_to } = a_to;
     assert(stack2_from === stack2_to);
     let range = list_range(stack2_from, index_from, index_to);
-    log(list_any(range, (r) => js_node_types_includes(r, "AwaitExpression")));
+    let async_is=(list_any(range, (r) => js_node_types_includes(r, "AwaitExpression")));
     js_node_types(ast);
-    const code_declaration = js_code_declaration(f_name_new, "", false);
+    const code_declaration = js_code_declaration(f_name_new, "", async_is);
     let declaration = js_parse_statement_module(code_declaration);
   }
 }
