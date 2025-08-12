c5e737d94184a8a245c014d1610debd918eee22b
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 08:43:31 -0700

    md 2

--- public/src/html_new.mjs
+++ public/src/html_new.mjs
@@ -9,9 +9,9 @@ import { path_join } from "./path_join.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
 export async function html_new(name) {
   let file_name = file_name_html(name);
-  marker("1");
   let file_path = folder_public_combine(file_name);
   await assert_file_exists_not(file_path);
+  marker("1");
   let body = `<script type="module">
     import { sayHello } from './greetings.js';
     sayHello('World');
