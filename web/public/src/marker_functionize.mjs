946a1809d3bb1686f0e258e428043c05a0dea474
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 20:41:21 -0700

    p

--- web/public/src/marker_functionize.mjs
+++ web/public/src/marker_functionize.mjs
@@ -104,6 +104,6 @@ export async function marker_functionize(m_name_from, m_name_to, f_name_new) {
     list_remove_multiple(range, stack2_from);
     let code = js_code_call_args_await_maybe(f_name_new, missing, declaration);
     let parsed = js_parse_statement(code);
-    list_insert(stack2_from, index_to, parsed);
+    list_insert(stack2_from, index_from, parsed);
   }
 }
