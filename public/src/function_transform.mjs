f1145bd6c24a97cfc13b89ca99dc3c400e99e62b
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 08:41:14 -0700

    i function_transform

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,9 +1,11 @@
-import {file_js_unparse} from './file_js_unparse.mjs';
-import {function_parse} from "./function_parse.mjs";
+import { log } from "./log.mjs";
+import { file_js_unparse } from "./file_js_unparse.mjs";
+import { function_parse } from "./function_parse.mjs";
 export async function function_transform(f_name, lambda$ast) {
+  log(f_name);
   let parsed = await function_parse(f_name);
-  let {ast} = parsed;
-  let result=await lambda$ast(ast);
+  let { ast } = parsed;
+  let result = await lambda$ast(ast);
   await file_js_unparse(parsed);
-  return result
+  return result;
 }
