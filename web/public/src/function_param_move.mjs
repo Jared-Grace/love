162533502a7040c7ac6afd0f2364fc89e1e9db81
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:11:50 -0700

    c function_params_new function_param_move

---
+++ web/public/src/function_param_move.mjs
@@ -0,0 +1,15 @@
+import { js_declaration_single_params_add } from "./js_declaration_single_params_add.mjs";
+import { string_split } from "./string_split.mjs";
+import { js_declaration_param_add } from "./js_declaration_param_add.mjs";
+import { js_declaration_single } from "./js_declaration_single.mjs";
+import { function_transform } from "./function_transform.mjs";
+import { data_function_current_get } from "./data_function_current_get.mjs";
+import { function_param_new } from "./function_param_new.mjs";
+import { each } from "./each.mjs";
+export async function function_param_move(param_names_comma) {
+  let param_names = string_split(param_names_comma, ",");
+  let f_name = await data_function_current_get();
+  await function_transform(f_name, (ast) => {
+    js_declaration_single_params_add(ast, param_names);
+  });
+}
