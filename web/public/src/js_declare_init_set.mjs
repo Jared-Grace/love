1286a055e51ff7f611ed19584f09074736090fad
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 13:45:23 -0700

    function_auto_path public\src\js_declare_init_set.mjs

--- web/public/src/js_declare_init_set.mjs
+++ web/public/src/js_declare_init_set.mjs
@@ -1,10 +1,11 @@
-import {object_property_set} from './object_property_set.mjs';
-import {list_single} from './list_single.mjs';
-import {js_node_type_is} from './js_node_type_is.mjs';
-import {assert} from './assert.mjs';
-export function js_declare_init_set(next, init) {
-  assert(js_node_type_is(next, 'VariableDeclaration'));
-  let {declarations} = next;
+import { object_property_set } from "./object_property_set.mjs";
+import { list_single } from "./list_single.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { assert } from "./assert.mjs";
+export function js_declare_init_set(vd, init) {
+  let b = js_node_type_is(vd, "VariableDeclaration");
+  assert(b);
+  let { declarations } = vd;
   let declaration = list_single(declarations);
-  object_property_set(declaration, 'init', init);
+  object_property_set(declaration, "init", init);
 }
