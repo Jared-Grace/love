8032e0d3d4593eaf2aaea98f27cce2ee177a04c8
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:22:19 -0700

    fw list_filter list_find

--- public/src/list_map_index.mjs
+++ public/src/list_map_index.mjs
@@ -1,4 +1,7 @@
 import { list_map } from "./list_map.mjs";
-export function list_map_index(list, mapper) {
-  return list_map(list, mapper);
+export function list_map_index(list, mapper) {let index=0
+  return list_map(list, item => {
+    mapper(item,index)
+    index++
+  });
 }
