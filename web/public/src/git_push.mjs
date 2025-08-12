33648d27b567c12ed2e5948f3250d29238812137
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 11:09:59 -0700

    function_auto_path public\src\git_push.mjs

--- web/public/src/git_push.mjs
+++ web/public/src/git_push.mjs
@@ -2,8 +2,11 @@ import { log } from "./log.mjs";
 import { command_line } from "./command_line.mjs";
 export function git_push() {
   try {
-    command_line("git push");
+    lambda();
   } catch (e) {
     log(e);
+  }
+  function lambda() {
+    command_line("git push");
   }
 }
