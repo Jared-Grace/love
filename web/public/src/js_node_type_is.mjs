96cf3cb579789968ce350c809605f209354dcc75
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 11:08:28 -0700

    w

--- web/public/src/js_node_type_is.mjs
+++ web/public/src/js_node_type_is.mjs
@@ -1,4 +1,4 @@
 import { js_node_type } from "./js_node_type.mjs";
 export function js_node_type_is(node, type) {
-  return js_node_type(node) === type;
+  return js_node_is(node) && js_node_type(node) === type;
 }
