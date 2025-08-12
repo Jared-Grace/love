e88ba3daa1d410c49ad5b662aa42c9af7c1cf68f
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:00:59 -0700

    i watch

--- web/public/src/watch.mjs
+++ web/public/src/watch.mjs
@@ -1,1 +1,10 @@
-export function watch() {}
+export async function watch() {
+  const chokidar = (await import("chokidar")).default;
+  const watcher = chokidar.watch("./my-folder", {
+    persistent: true,
+    ignoreInitial: true,
+  });
+  watcher.on("change", (path) => {
+    runMyFunction(path);
+  });
+}
