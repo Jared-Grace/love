8a6a76ab3b71ea6d3a467f47440db727f77e728d
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 15:56:11 -0700

    function_auto_path public\src\function_run_log.mjs

--- public/src/function_run_log.mjs
+++ public/src/function_run_log.mjs
@@ -1,9 +1,10 @@
+import { json_format_to } from "./json_format_to.mjs";
 import { log_keep } from "./log_keep.mjs";
 import { log } from "./log.mjs";
 import { function_run } from "./function_run.mjs";
 import { json_to } from "./json_to.mjs";
 export async function function_run_log(f_name, args) {
   let result = await function_run(f_name, args);
-  log_keep("result: " + json_to(result));
+  log_keep("result: " + json_format_to(result));
   return result;
 }
