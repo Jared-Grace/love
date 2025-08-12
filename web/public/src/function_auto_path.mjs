085ca1a29b8d748fdaadfffa7cc908bd2b346c8f
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:19:01 -0700

    ps

--- web/public/src/function_auto_path.mjs
+++ web/public/src/function_auto_path.mjs
@@ -1,5 +1,5 @@
 import { function_auto } from "./function_auto.mjs";
 export async function function_auto_path(f_name) {
-  return path_base(f_name);
+  return path_name(f_name);
   return await function_auto(f_name);
 }
