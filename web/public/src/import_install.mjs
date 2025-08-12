578022332143a94e65a95054fc1cc2f322992304
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:06:23 -0700

    i watch

--- web/public/src/import_install.mjs
+++ web/public/src/import_install.mjs
@@ -1,9 +1,7 @@
 import { npm_install } from "./npm_install.mjs";
-import { file_read_json } from "./file_read_json.mjs";
 export async function import_install(name) {
   try {
     return await import(name);
-    return true;
   } catch (err) {
     if (
       err.code === "ERR_MODULE_NOT_FOUND" ||
