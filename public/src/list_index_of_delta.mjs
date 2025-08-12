84e3beac0d3980ef449ae68bc9b0efd62c9be55e
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 17:29:26 -0700

    au list_index_of_next

---
+++ public/src/list_index_of_delta.mjs
@@ -0,0 +1,6 @@
+import {list_index_of} from './list_index_of.mjs';
+export function list_index_of_delta(list, item, delta) {
+  let index = list_index_of(list, item);
+  let index_next = index + delta;
+  return index_next;
+}
