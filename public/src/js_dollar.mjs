f14b0cdb0cd7b08ecc8b4d4b92e9d3c7716a5045
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 16:37:03 -0700

    function_auto_path public\src\js_dollar.mjs

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -1,3 +1,4 @@
+import { js_keyword_if } from "./js_keyword_if.mjs";
 import { js_code_braces_empty } from "./js_code_braces_empty.mjs";
 import { js_code_wrap_braces } from "./js_code_wrap_braces.mjs";
 import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
@@ -14,7 +15,10 @@ export function js_dollar(ast) {
       let { name } = expression;
       if (name === "$i") {
         let from = js_parse_statement(
-          "if " + js_code_wrap_parenthesis("false") + js_code_braces_empty(),
+          js_keyword_if() +
+            " " +
+            js_code_wrap_parenthesis("false") +
+            js_code_braces_empty(),
         );
         object_replace(node, from);
       }
