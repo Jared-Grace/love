e83d0293d71f3eee70aa9b20b6bbd36ddb3a8c66
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:12:53 -0700

    au function_params_new

--- public/src/function_param_move.mjs
+++ public/src/function_param_move.mjs
@@ -6,7 +6,7 @@ import { function_transform } from "./function_transform.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { each } from "./each.mjs";
-export async function function_param_move(param_names_comma) {
+export async function function_param_move(param_name) {
   let param_names = string_split(param_names_comma, ",");
   let f_name = await data_function_current_get();
   await function_transform(f_name, (ast) => {
