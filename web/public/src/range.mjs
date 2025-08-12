daaea109e3f3cba9d5d36d00b5ce4147cfe9e029
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 18:19:11 -0700

    fm range

--- web/public/src/range.mjs
+++ web/public/src/range.mjs
@@ -1,1 +1,9 @@
-export function range() {}
+import { list_add } from "./list_add.mjs";
+
+export function range(count) {
+  let r = [];
+  for (let i = 0; i < count; i++) {
+    list_add(r, i);
+  }
+  return r;
+}
