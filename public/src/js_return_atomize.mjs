b828fb06ade488f78b3300c3770c9449a902d2b2
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:58:12 -0700

    function_auto_path public\src\js_return_atomize.mjs

--- public/src/js_return_atomize.mjs
+++ public/src/js_return_atomize.mjs
@@ -1,1 +1,11 @@
-export function js_return_atomize(ast) {}
+import { js_return_on } from "./js_return_on.mjs";
+export function js_return_atomize(ast) {
+  js_return_on(last, identifier_if, identifier_not);
+  function identifier_not() {
+    name = "result";
+  }
+  function identifier_if(argument) {
+    let { name: name_argument } = argument;
+    name = name_argument;
+  }
+}
