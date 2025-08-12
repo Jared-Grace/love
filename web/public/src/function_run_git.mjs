828475111ff3652813b267012aa5db401161b1bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 16:41:53 -0700

    function_auto_path public\src\function_run_git.mjs

--- web/public/src/function_run_git.mjs
+++ web/public/src/function_run_git.mjs
@@ -1,8 +1,9 @@
 import { function_run_log } from "./function_run_log.mjs";
 import { git_acp } from "./git_acp.mjs";
 import { function_run } from "./function_run.mjs";
-export async function function_run_git(funcName, args) {
-  let result = await function_run_log(funcName, args);
-  await git_acp([funcName].concat(args).join(" "));
+export async function function_run_git(f_name, args) {
+  let result = await function_run_log(f_name, args);
+  let message = [f_name].concat(args).join(" ");
+  await git_acp(message);
   return result;
 }
