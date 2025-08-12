5ac5b121bde339239556a8e71526492dafa4ab45
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:26:33 -0700

    au function_param_swap

--- web/public/src/function_param_swap.mjs
+++ web/public/src/function_param_swap.mjs
@@ -1,3 +1,4 @@
+import { js_declaration_param_named } from "./js_declaration_param_named.mjs";
 import { function_transform_current } from "./function_transform_current.mjs";
 import { js_declaration_single_params_add } from "./js_declaration_single_params_add.mjs";
 import { string_split } from "./string_split.mjs";
@@ -7,11 +8,12 @@ import { function_transform } from "./function_transform.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { each } from "./each.mjs";
-export async function function_param_swap(param_name,param_name_other) {
+export async function function_param_swap(param_name_a, param_name_b) {
   await function_transform_current(lambda);
   function lambda(ast) {
-  let declaration = js_declaration_single(ast);
-  let {params} = declaration;
+    let declaration = js_declaration_single(ast);
+    let a = js_declaration_param_named(declaration, param_name_a);
+    let b = js_declaration_param_named(declaration, param_name_b);
     js_declaration_single_params_add(ast, param_names);
   }
 }
---
+++ web/public/src/js_declaration_param_named.mjs
@@ -0,0 +1,5 @@
+import {list_find_property} from './list_find_property.mjs';
+export function js_declaration_param_named(declaration, param_name) {
+  let {params} = declaration;
+  return list_find_property(params, 'name', param_name);
+}
