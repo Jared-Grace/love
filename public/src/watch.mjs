0414fb647f50bb6ba06657a729f39a7450d4efd9
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:20:40 -0700

    i watch

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,16 +1,16 @@
+import { function_auto_path } from "./function_auto_path.mjs";
 import { log } from "./log.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { import_install } from "./import_install.mjs";
+import { command_line } from "./command_line.mjs";
 export async function watch() {
   const chokidar = (await import_install("chokidar")).default;
   const watcher = chokidar.watch(functions_path(), {
     persistent: true,
     ignoreInitial: true,
   });
-  watcher.on("change", (path) => { 
-
-    
-
+  watcher.on("change", (path) => {
+    command_line("node r.mjs " + function_auto_path.name + " " + path);
     log(path);
   });
   log("here");
