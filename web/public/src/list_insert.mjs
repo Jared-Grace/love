2264437e750503a4fd208875b8852a49f9d32b79
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:29:04 -0700

    au list_insert

--- web/public/src/list_insert.mjs
+++ web/public/src/list_insert.mjs
@@ -1,6 +1,9 @@
-import {number_is} from './number_is.mjs';
-import {assert} from './assert.mjs';
+import { list_splice } from "./list_splice.mjs";
+import { number_is } from "./number_is.mjs";
+import { assert } from "./assert.mjs";
 export function list_insert(list, index, value) {
-  assert(number_is(index));
-  list.splice(index, 0, value);
+  let b = number_is(index);
+  assert(b);
+  const delete_count = 0;
+  list_splice(list, index, delete_count, value);
 }
