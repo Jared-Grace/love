119857fa3f9562f2f4bbc81bfb31e5260c87e574
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 13:50:17 -0700

    function_auto_path public\src\js_declare.mjs

--- web/public/src/js_declare.mjs
+++ web/public/src/js_declare.mjs
@@ -1,6 +1,6 @@
-import {js_declare_init_set} from './js_declare_init_set.mjs';
-import {js_parse_statement} from './js_parse_statement.mjs';
-import {js_code_let_assign} from './js_code_let_assign.mjs';
+import { js_declare_init_set } from "./js_declare_init_set.mjs";
+import { js_parse_statement } from "./js_parse_statement.mjs";
+import { js_code_let_assign } from "./js_code_let_assign.mjs";
 export function js_declare(name, init) {
   let assign_code = js_code_let_assign(name, "a");
   let assign = js_parse_statement(assign_code);
