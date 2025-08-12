cd5d651f7d557d5f6424e8e65ccad4c69f3c47d4
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:36:53 -0700

    p

--- web/public/src/html_overwrite.mjs
+++ web/public/src/html_overwrite.mjs
@@ -1,5 +1,5 @@
-import {file_overwrite} from './file_overwrite.mjs';
-export async function html_overwrite(name, body, file_path) {
+import { file_overwrite } from "./file_overwrite.mjs";
+export async function html_overwrite(name, file_path, body) {
   let contents = `<!DOCTYPE html>
 <html lang="en">
 <head>
