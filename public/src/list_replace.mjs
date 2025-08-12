ee459862d3991c8686557e724d29bc233aa59133
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 09:30:47 -0700

    i list_swap

--- public/src/list_replace.mjs
+++ public/src/list_replace.mjs
@@ -3,6 +3,6 @@ import { list_splice } from "./list_splice.mjs";
 import { number_is } from "./number_is.mjs";
 import { assert } from "./assert.mjs";
 export function list_replace(list, index, value) {
-  const delete_count = 0;
+  const delete_count = 1;
   list_insert_generic(index, list, delete_count, value);
 }
--- public/src/list_swap.mjs
+++ public/src/list_swap.mjs
@@ -1,1 +1,4 @@
-export function list_swap(list,a,b) {}
+import { list_replace } from "./list_replace.mjs";
+export function list_swap(list, a, b) {
+  list_replace();
+}
