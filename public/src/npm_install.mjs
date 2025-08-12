edd55db2b2340a09c85238971d794cd64a00bcba
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 16:44:46 -0700

    c catch_log catch_log_async

--- public/src/npm_install.mjs
+++ public/src/npm_install.mjs
@@ -1,4 +1,4 @@
 import { command_line } from "./command_line.mjs";
 export function npm_install(package_name) {
-  command_line("npm install " + package_name + "@latest");
+await   command_line("npm install " + package_name + "@latest");
 }
