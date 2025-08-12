a36d262675a5c6fe11962d918673187eeecba127
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 15:47:16 -0700

    function_auto_path public\src\function_run_log.mjs

--- public/src/function_run_log.mjs
+++ public/src/function_run_log.mjs
@@ -3,6 +3,6 @@ import { log } from "./log.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_run_log(funcName, args) {
   let result = await function_run(funcName, args);
-  log_keep("result:", result);
+  log_keep("result: " + result);
   return result;
 }
