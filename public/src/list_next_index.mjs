572de4505becac4a87c5cc420eefaaf76b44ed74
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 18:24:49 -0700

    i list_next_index

--- public/src/list_next_index.mjs
+++ public/src/list_next_index.mjs
@@ -1,6 +1,7 @@
-import {list_index_of} from './list_index_of.mjs';
+import { list_next } from "./list_next.mjs";
+import { list_index_of } from "./list_index_of.mjs";
 export function list_next_index(list, item) {
-  let next = list_next_index(list, item);
+  let next = list_next(list, item);
   let index = list_index_of(list, next);
   return index;
 }
