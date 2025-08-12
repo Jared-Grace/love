cff7b43f3fbbe9454ade15d69b337be45bd32676
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 15:48:29 -0700

    function_auto_path public\src\server.mjs

--- web/public/src/server.mjs
+++ web/public/src/server.mjs
@@ -1,15 +1,19 @@
-import {path_join} from "./path_join.mjs";
+import { log_keep } from "./log_keep.mjs";
+import { path_join } from "./path_join.mjs";
 import express from "express";
 import path from "path";
-import {fileURLToPath} from "url";
-import {folder_public} from "./folder_public.mjs";
+import { fileURLToPath } from "url";
+import { folder_public } from "./folder_public.mjs";
 export function server() {
   const app = express();
   const port = 8080;
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  app.use(express.static(path_join([__dirname, "..", "..", folder_public()])));
+  let result = folder_public();
+  let result2 = path_join([__dirname, "..", "..", result]);
+  let v = express.static(result2);
+  app.use(v);
   app.listen(port, () => {
-    console.log(`Static server running at http://localhost:${port}`);
+    log_keep(`Static server running at http://localhost:${port}`);
   });
 }
