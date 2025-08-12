6f220fdf804a6f872d7c1b6623a6b614f2d7acc8
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 14:13:21 -0700

    function_auto_path public\src\js_identifier_unique.mjs

--- web/public/src/js_identifier_unique.mjs
+++ web/public/src/js_identifier_unique.mjs
@@ -1,5 +1,5 @@
-import {list_add} from './list_add.mjs';
-import {list_includes} from './list_includes.mjs';
+import { list_add } from "./list_add.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function js_identifier_unique(existing, name) {
   let result = null;
   let attempt = 1;
@@ -7,7 +7,7 @@ export function js_identifier_unique(existing, name) {
     const suffix = attempt === 1 ? "" : attempt;
     result = name + suffix;
     attempt++;
-  } while (list_includes(existing, result));
+  } while (result === "" || list_includes(existing, result));
   list_add(existing, result);
   return result;
 }
