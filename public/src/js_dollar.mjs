bcd9a051dd49a92ece5d18bb20fe17de2d8e2e81
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 16:28:05 -0700

    function_auto_path public\src\js_dollar.mjs

--- public/src/js_dollar.mjs
+++ public/src/js_dollar.mjs
@@ -1,12 +1,15 @@
+import { object_replace } from "./object_replace.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_dollar(ast) {
   js_visit_type(ast, "ExpressionStatement", (v) => {
     let { node } = v;
-    if (js_identifier_is(node)) {
-      let { name } = node;
+    let { expression } = node;
+    if (js_identifier_is(expression)) {
+      let { name } = expression;
       if (name === "$i") {
+        object_replace(expression);
       }
     }
   });
