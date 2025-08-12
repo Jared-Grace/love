b4cb129623464b0eed49cd241d89a4c89bfa3e8b
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:58:04 -0700

    function_auto_path public\src\js_return_on.mjs

--- public/src/js_return_on.mjs
+++ public/src/js_return_on.mjs
@@ -1,7 +1,7 @@
-import {js_node_type_is} from './js_node_type_is.mjs';
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_return_on(last, identifier_if, identifier_not) {
   if (js_node_type_is(last, "ReturnStatement")) {
-    let {argument} = last;
+    let { argument } = last;
     if (js_node_type_is(argument, "Identifier")) {
       identifier_if(argument);
     } else {
