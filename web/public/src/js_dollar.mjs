406ae14dc38a2158cfbba612b3dc989ec12eced3
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 16:22:32 -0700

    function_auto_path public\src\js_dollar.mjs

--- web/public/src/js_dollar.mjs
+++ web/public/src/js_dollar.mjs
@@ -1,5 +1,7 @@
 import { js_visit } from "./js_visit.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_dollar(ast) {
-  js_visit_type(ast, "ExpressionStatement", (v) => {});
+  js_visit_type(ast, "ExpressionStatement", (v) => {
+    let node = v;
+  });
 }
