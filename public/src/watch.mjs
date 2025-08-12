268d87145cdfaa81da91fe9bcd28d631874080fa
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:21:55 -0700

    w

--- public/src/watch.mjs
+++ public/src/watch.mjs
@@ -1,5 +1,4 @@
 import { function_auto_path } from "./function_auto_path.mjs";
-import { log } from "./log.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { import_install } from "./import_install.mjs";
 import { command_line } from "./command_line.mjs";
@@ -10,8 +9,6 @@ export async function watch() {
     ignoreInitial: true,
   });
   watcher.on("change", (path) => {
-    command_line("node r.mjs " + function_auto_path.name + " " + path);
-    log(path);
+    command_line("node g.mjs " + function_auto_path.name + " " + path);
   });
-  log("here");
 }
