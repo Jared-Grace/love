337ff4a26ad230f2a311435a2df725962ac755a6
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:17:52 -0700

    n path_name

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -3,13 +3,11 @@ import { string_includes } from "./string_includes.mjs";
 import { error } from "./error.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { functions_path } from "./functions_path.mjs";
-import path from "path";
 export function function_name_to_path(f_name) {
   if (string_includes(f_name, ".")) {
     error();
   }
   let second = [functions_path(), function_name_to_base(f_name)];
-  let joined = path.join(...second);
+  let joined = path_join(second);
   return joined;
-  let joinedd = path_join(second);
 }
