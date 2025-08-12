073e2029ccdb857d663703715403c002bcb049e0
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:05:16 -0700

    u ni

--- web/public/src/import_install.mjs
+++ web/public/src/import_install.mjs
@@ -1,5 +1,12 @@
 import { file_read_json } from "./file_read_json.mjs";
 export async function import_install(name) {
-  return await file_read_json("package.json");
-  await import(name);
+      try {
+    await import(name);
+    return true;
+  } catch (err) {
+    if (err.code === 'ERR_MODULE_NOT_FOUND' || err.code === 'MODULE_NOT_FOUND') {
+      return false;
+    }
+    throw err; // rethrow unexpected errors
+  }
 }
