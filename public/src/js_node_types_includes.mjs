f3d3dd2cadc1db63779f5cd98f65cb476286b604
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 17:45:25 -0700

    i js_node_types_includes

--- public/src/js_node_types_includes.mjs
+++ public/src/js_node_types_includes.mjs
@@ -1,4 +1,5 @@
+import { list_includes } from "./list_includes.mjs";
 import { js_node_types } from "./js_node_types.mjs";
-export function js_node_types_includes(ast) {
-  return js_node_types(ast);
+export function js_node_types_includes(ast, node_type) {
+  return list_includes(js_node_types(ast), node_type);
 }
