7cce25f31321869fbb5460240861ee883790c567
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 18:30:44 -0700

    i marker_functionize

--- web/public/src/list_adder_multiple.mjs
+++ web/public/src/list_adder_multiple.mjs
@@ -1,7 +1,8 @@
 import { list_adder_generic } from "./list_adder_generic.mjs";
 import { list_add } from "./list_add.mjs";
+import { list_add_multiple } from "./list_add_multiple.mjs";
 export function list_adder_multiple(lambda) {
-  let fn = list_add;
+  let fn = list_add_multiple;
   let list = list_adder_generic(lambda, fn);
   return list;
 }
