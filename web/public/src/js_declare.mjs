9ccf18971df6e6661e067ca58e3c21626a82b79c
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 13:50:13 -0700

    function_auto_path public\src\js_node_atomize.mjs

---
+++ web/public/src/js_declare.mjs
@@ -0,0 +1,9 @@
+import {js_declare_init_set} from './js_declare_init_set.mjs';
+import {js_parse_statement} from './js_parse_statement.mjs';
+import {js_code_let_assign} from './js_code_let_assign.mjs';
+export function js_declare(name, init) {
+  let assign_code = js_code_let_assign(name, "a");
+  let assign = js_parse_statement(assign_code);
+  js_declare_init_set(assign, init);
+  return assign;
+}
