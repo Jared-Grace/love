28bf3769c3fa4dc1dd606406e00443abbb553c83
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 11:11:06 -0700

    function_auto_path public\src\git_acp.mjs

--- web/public/src/git_acp.mjs
+++ web/public/src/git_acp.mjs
@@ -1,10 +1,12 @@
 import { command_line } from "./command_line.mjs";
 import { git_push } from "./git_push.mjs";
-
 export function git_acp(message) {
   command_line("git add -A");
   try {
-    command_line(`git commit -m "${message}"`);
+    lambda();
   } catch (e) {}
   git_push();
+  function lambda() {
+    command_line(`git commit -m "${message}"`);
+  }
 }
