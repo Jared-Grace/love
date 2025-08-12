2040fda8212882194ec4f9a655202786d92b4814
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:23:01 -0700

    function_auto_path public\src\command_line.mjs

--- web/public/src/command_line.mjs
+++ web/public/src/command_line.mjs
@@ -1,5 +1,8 @@
 import { execSync } from "child_process";
-
 export function command_line(command) {
-  const output = execSync(command, { encoding: "utf8", stdio: "pipe" });
+  const output = execSync(command, {
+    encoding: "utf8",
+    stdio: "pipe",
+  });
+  return output;
 }
