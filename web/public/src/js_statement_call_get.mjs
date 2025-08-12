f66df7cbfd09a983cb3bed4e08c56f7f457b468d
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 13:34:14 -0700

    function_auto_path public\src\js_statement_call_get.mjs

--- web/public/src/js_statement_call_get.mjs
+++ web/public/src/js_statement_call_get.mjs
@@ -1,14 +1,15 @@
-import {object_property_get} from './object_property_get.mjs';
-import {list_single} from './list_single.mjs';
-import {list_multiple_is} from './list_multiple_is.mjs';
-import {js_node_type_is} from './js_node_type_is.mjs';
+import { object_property_get } from "./object_property_get.mjs";
+import { list_single } from "./list_single.mjs";
+import { list_multiple_is } from "./list_multiple_is.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_statement_call_get(next) {
   let expression = null;
+  let declaration = null;
   if (js_node_type_is(next, "ExpressionStatement")) {
-    let {expression: expression_next} = next;
+    let { expression: expression_next } = next;
     expression = expression_next;
   } else if (js_node_type_is(next, "VariableDeclaration")) {
-    let {declarations} = next;
+    let { declarations: declarations_next } = next;
     if (list_multiple_is(declarations)) {
       return null;
     }
