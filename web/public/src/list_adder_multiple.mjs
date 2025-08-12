f4465ef35e94e1f9bd76b66ec2449231a8e77109
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 18:29:29 -0700

    c list_adder list_adder_multiple

---
+++ web/public/src/list_adder_multiple.mjs
@@ -0,0 +1,7 @@
+import { list_adder_generic } from "./list_adder_generic.mjs";
+import { list_add } from "./list_add.mjs";
+export function list_adder_multiple(lambda) {
+  let fn = list_add;
+  let list = list_adder_generic(lambda, fn);
+  return list;
+}
