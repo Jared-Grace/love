85988fea144c146120c138e27cb3e9787ab2dbef
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 11:05:36 -0700

    function_auto_path public\src\js_visit_match.mjs

--- web/public/src/js_visit_match.mjs
+++ web/public/src/js_visit_match.mjs
@@ -1,14 +1,15 @@
-import {list_single} from './list_single.mjs';
-import {js_visit} from './js_visit.mjs';
-import {list_adder} from './list_adder.mjs';
+import { list_single } from "./list_single.mjs";
+import { js_visit } from "./js_visit.mjs";
+import { list_adder } from "./list_adder.mjs";
 export function js_visit_match(ast, search) {
-  let matches = list_adder(la => {
-    js_visit(ast, v => {
-      let {node} = v;
+  let matches = list_adder((la) => {
+    js_visit(ast, (v) => {
+      let { node } = v;
       if (node === search) {
         la(v);
       }
     });
   });
-  let v_match = list_single(matches);return v_match
+  let v_match = list_single(matches);
+  return v_match;
 }
