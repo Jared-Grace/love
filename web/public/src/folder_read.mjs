c6e9a245643de97d93dcd3267700a8ce425e6079
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:15:20 -0700

    p

--- web/public/src/folder_read.mjs
+++ web/public/src/folder_read.mjs
@@ -1,8 +1,9 @@
+import { path_join } from "./path_join.mjs";
 import fs from "fs";
 import path from "path";
-
 export function folder_read(dirPath) {
   return fs.readdirSync(dirPath).filter((file) => {
     return fs.statSync(path.join(dirPath, file)).isFile();
   });
+  path_join;
 }
