32318368f30dd90650c3ed6b1a81523949b72f75
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 17:46:20 -0700

    i list_any

--- web/public/src/list_any.mjs
+++ web/public/src/list_any.mjs
@@ -1,6 +1,7 @@
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_any(list, predicate) {
-  return list_size(list_filter(list, predicate)) === list_size(list);
+  return list_empty_not_is(list_filter(list, predicate));
 }
