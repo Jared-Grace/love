15a52a69a5a7ced1382d1c2251dc5cc833e753bd
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:59:05 -0700

    function_auto_path public\src\js_visit_type.mjs

--- public/src/js_visit_type.mjs
+++ public/src/js_visit_type.mjs
@@ -3,11 +3,11 @@ import { js_visit } from "./js_visit.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { js_node_is } from "./js_node_is.mjs";
-export function js_visit_type(parsed, type, on_each) {
+export function js_visit_type(parsed, type, on_each_v) {
   js_visit(parsed, (v) => {
     let { node } = v;
     if (js_node_is(node) && js_node_type_is(node, type)) {
-      on_each(v);
+      on_each_v(v);
     }
   });
 }
