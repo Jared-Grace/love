47a5d09d2980e21428250c32e7471c7fb5c6e440
Grace-Jared <christrosetolife@gmail.com>
2025-08-11 17:10:42 -0700

    au marker_move

---
+++ public/src/js_marker_named_ast_arg.mjs
@@ -0,0 +1,7 @@
+import {function_transform_marker_arg} from './function_transform_marker_arg.mjs';
+import {js_marker_named_ast} from './js_marker_named_ast.mjs';
+export function js_marker_named_ast_arg(ast, m_name_to) {
+  let marker_v_to = js_marker_named_ast(ast, m_name_to);
+  let a_to = function_transform_marker_arg(marker_v_to, ast);
+  return a_to;
+}
