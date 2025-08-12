a4283f73c7b855de5418d1a5f056b0ec968ef319
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 10:10:23 -0700

    fw fw function_wrap_suffix_add

---
+++ web/public/src/function_wrap_suffix_add.mjs
@@ -0,0 +1,4 @@
+import { function_wrap } from "./function_wrap.mjs";
+export async function function_wrap_suffix_add(f_name, f_name_wrapped) {
+  return await function_wrap(f_name, f_name_wrapped);
+}
