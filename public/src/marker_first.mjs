e83943a83beb7d0d2fe8b7cac3b8bb4019979048
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 11:20:34 -0700

    p

--- public/src/marker_first.mjs
+++ public/src/marker_first.mjs
@@ -2,5 +2,6 @@ import { marker_first_index } from "./marker_first_index.mjs";
 import { string_to } from "./string_to.mjs";
 export function marker_first() {
   let s = marker_first_index();
-  return string_to(s);
+  let s2 = string_to(s);
+  return s2;
 }
