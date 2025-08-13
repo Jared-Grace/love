5717610c2f6ed7d10cf9d29f17b4e32ecea8b627
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 08:15:39 -0400

    function_auto_path public\src\js_let_add.mjs

--- public/src/js_let_add.mjs
+++ public/src/js_let_add.mjs
@@ -1,3 +1,4 @@
+import { js_declare } from "./js_declare.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
@@ -12,5 +13,6 @@ export function js_let_add(ast) {
     if (!type_is) {
       return;
     }
+    let assign = js_declare(name, init);
   });
 }
