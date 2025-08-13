2280f0fdfcf00e4a0a813869c52b32d59d5af201
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:41:48 -0400

    function_auto_path public\src\js_call_insert.mjs

--- web/public/src/js_call_insert.mjs
+++ web/public/src/js_call_insert.mjs
@@ -1,8 +1,9 @@
-import {list_insert} from './list_insert.mjs';
-import {js_parse_statement} from './js_parse_statement.mjs';
-import {js_code_call_args_await_maybe} from './js_code_call_args_await_maybe.mjs';
+import { js_code_call_args } from "./js_code_call_args.mjs";
+import { list_insert } from "./list_insert.mjs";
+import { js_parse_statement } from "./js_parse_statement.mjs";
+import { js_code_call_args_await_maybe } from "./js_code_call_args_await_maybe.mjs";
 export function js_call_insert(f_name, args_code, list, index) {
-  let code = js_code_call_args_await_maybe(f_name, args_code);
+  let code = js_code_call_args(f_name, args_code);
   let parsed = js_parse_statement(code);
   list_insert(list, index, parsed);
 }
