ed4cb0d901344bf4486b84a61671433d1fd9563a
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 14:47:50 -0400

    function_auto_path public\src\js_call_fill.mjs

--- public/src/js_call_fill.mjs
+++ public/src/js_call_fill.mjs
@@ -30,7 +30,7 @@ export async function js_call_fill(ast) {
         return;
         let block = js_stack_last(stack, "BlockStatement");
         let index_previous = list_index_of_previous(stack, block);
-        let result2 = stack(list, index_previous);
+        let previous = stack(list, index_previous);
         let result = object_property_exists(object, property_name);
         assert(result);
       }
