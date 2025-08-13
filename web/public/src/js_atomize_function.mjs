38e5fcefb7676bda2d5a49896099ff15cfa9c4e9
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 13:39:04 -0400

    function_auto_path public\src\js_atomize_function.mjs

---
+++ web/public/src/js_atomize_function.mjs
@@ -0,0 +1,40 @@
+import { list_get_end_1 } from "./list_get_end_1.mjs";
+import { js_node_atomize } from "./js_node_atomize.mjs";
+import { list_next } from "./list_next.mjs";
+import { each_async } from "./each_async.mjs";
+import { js_return_name } from "./js_return_name.mjs";
+import { js_declare_init_set } from "./js_declare_init_set.mjs";
+import { js_code_let_assign } from "./js_code_let_assign.mjs";
+import { list_index_of } from "./list_index_of.mjs";
+import { list_index_of_next } from "./list_index_of_next.mjs";
+import { js_stack_last } from "./js_stack_last.mjs";
+import { object_copy } from "./object_copy.mjs";
+import { js_identifier_unique } from "./js_identifier_unique.mjs";
+import { js_identifiers } from "./js_identifiers.mjs";
+import { list_get_end } from "./list_get_end.mjs";
+import { each } from "./each.mjs";
+import { js_type } from "./js_type.mjs";
+import { js_visit } from "./js_visit.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
+import { list_get } from "./list_get.mjs";
+import { log } from "./log.mjs";
+import { marker } from "./marker.mjs";
+import { list_is } from "./list_is.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { js_parse_statement } from "./js_parse_statement.mjs";
+import { list_insert } from "./list_insert.mjs";
+import { object_replace } from "./object_replace.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { function_parse } from "./function_parse.mjs";
+export async function js_atomize_function(ast) {
+  let existing = js_identifiers(ast);
+  let ces = js_type(ast, "CallExpression");
+  await each_async(ces, async function lambda(v) {
+    let { stack } = v;
+    const stack1 = list_get_end_1(stack);
+    if (list_is(stack1)) {
+      await js_node_atomize(existing, v);
+    }
+  });
+}
