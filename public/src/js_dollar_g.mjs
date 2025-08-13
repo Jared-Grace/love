6936c089b1bb446153b45016539bfad157703651
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 15:39:31 -0400

    function_auto_path public\src\js_dollar_g.mjs

--- public/src/js_dollar_g.mjs
+++ public/src/js_dollar_g.mjs
@@ -1,16 +1,20 @@
-import {js_declare} from './js_declare.mjs';
-import {js_identifier_unique_ast} from './js_identifier_unique_ast.mjs';
-import {js_node_type_is} from './js_node_type_is.mjs';
-import {object_replace} from './object_replace.mjs';
-import {js_parse_expression} from './js_parse_expression.mjs';
-import {object_property_get} from './object_property_get.mjs';
-import {js_code_call_args} from './js_code_call_args.mjs';
-import {js_code_string} from './js_code_string.mjs';
-import {list_first_second} from './list_first_second.mjs';
-export async function js_dollar_g(remaining, node, stack1, ast) {
-  let {first: object_name, second: property_name} = list_first_second(remaining);
+import { js_declare } from "./js_declare.mjs";
+import { js_identifier_unique_ast } from "./js_identifier_unique_ast.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { object_replace } from "./object_replace.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { js_code_call_args } from "./js_code_call_args.mjs";
+import { js_code_string } from "./js_code_string.mjs";
+import { list_first_second } from "./list_first_second.mjs";
+export async function js_dollar_g({ remaining, node, stack1, ast }) {
+  let { first: object_name, second: property_name } =
+    list_first_second(remaining);
   let code_string = await js_code_string(property_name);
-  let code = js_code_call_args(object_property_get.name, [object_name, code_string]);
+  let code = js_code_call_args(object_property_get.name, [
+    object_name,
+    code_string,
+  ]);
   let parsed = js_parse_expression(code);
   object_replace(node, parsed);
   if (js_node_type_is(stack1, "ExpressionStatement")) {
