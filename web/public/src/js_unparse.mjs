92e84ead81564d1964d7a91626e8883303227635
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:44:48 -0700

    watch public\src\file_js_unparse.mjs

--- web/public/src/js_unparse.mjs
+++ web/public/src/js_unparse.mjs
@@ -1,11 +1,17 @@
 import { import_install } from "./import_install.mjs";
 import { generate } from "astring";
-export function js_unparse(ast) {
+export async function js_unparse(ast) {
   let output;
   try {
     output = generate(ast);
   } catch (e) {
-    import_install("shift-validator");
+    let  { Validator } = await import_install("shift-validator");
+    const validator = new Validator();
+const errors = validator.validate(ast);
+
+if (errors.length > 0) {
+  console.error("The AST is invalid. Errors:", errors);
+}
   }
   return output;
 }
