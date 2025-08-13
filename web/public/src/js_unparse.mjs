3eac52453ab0f7285ca4a3214b1b35a6347d2644
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:43:30 -0700

    watch public\src\js_unparse.mjs

--- web/public/src/js_unparse.mjs
+++ web/public/src/js_unparse.mjs
@@ -1,5 +1,11 @@
+import { import_install } from "./import_install.mjs";
 import { generate } from "astring";
 export function js_unparse(ast) {
-  const output = generate(ast);
+  let output;
+  try {
+    output = generate(ast);
+  } catch (e) {
+    import_install();
+  }
   return output;
 }
