3884be78c1da1eeac2d0161d9a973f427aa06da1
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:02:33 -0700

    function_auto_path public\src\git_push.mjs

--- public/src/git_push.mjs
+++ public/src/git_push.mjs
@@ -1,8 +1,9 @@
 import { catch_log_async } from "./catch_log_async.mjs";
 import { command_line } from "./command_line.mjs";
 export async function git_push() {
+  const command_git = "push";
   await catch_log_async(lambda);
   async function lambda() {
-    await command_line("git " + "push");
+    await command_line("git " + command_git);
   }
 }
