66f6f5d6e5c1c8b289023d821bb78890bbf29424
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 04:54:45 -0400

    watch public\src\js_visit_type_each_async.mjs

--- web/public/src/js_visit_type_each_async.mjs
+++ web/public/src/js_visit_type_each_async.mjs
@@ -1,1 +1,4 @@
-export function js_visit_type_each_async() {}
+import { js_visit_type } from "./js_visit_type.mjs";
+export function js_visit_type_each_async(lambda) {
+  js_visit_type(ast, "ExpressionStatement", lambda);
+}
