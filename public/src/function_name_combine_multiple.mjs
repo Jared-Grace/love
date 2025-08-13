d645cf54879c7c16023c4b90a605e095da86743f
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:46:26 -0400

    function_auto_path public\src\function_name_combine_multiple.mjs

--- public/src/function_name_combine_multiple.mjs
+++ public/src/function_name_combine_multiple.mjs
@@ -1,5 +1,7 @@
 import { function_name_separator } from "./function_name_separator.mjs";
+import { list_join } from "./list_join.mjs";
 export function function_name_combine_multiple() {
-  const combined = left + function_name_separator() + right;
+  let separator = function_name_separator();
+  const combined = list_join(parts, separator);
   return combined;
 }
