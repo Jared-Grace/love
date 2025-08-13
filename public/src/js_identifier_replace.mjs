2e2e44db9aad04d07664c1df2d22362b2f343f17
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 17:59:51 -0400

    function_auto_path public\src\js_identifier_replace.mjs

--- public/src/js_identifier_replace.mjs
+++ public/src/js_identifier_replace.mjs
@@ -1,10 +1,11 @@
+import { js_node_is } from "./js_node_is.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_identifier_replace(ast, name_from, replacement) {
-  let type_is = js_node_type_is(node2, type);
+  let type_is = js_node_is(name_from);
   if (false) {
   }
   function lambda(v) {
