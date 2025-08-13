1b3177e4adbf7bb6ac0cfa602bf123973325118f
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:27:50 -0400

    function_auto_path public\src\function_param_swap_task_simple.mjs

--- public/src/function_param_swap_task_simple.mjs
+++ public/src/function_param_swap_task_simple.mjs
@@ -1,5 +1,7 @@
+import { function_param_swap } from "./function_param_swap.mjs";
 import { todo } from "./todo.mjs";
 import { error } from "./error.mjs";
-export function function_param_swap_task_simple() {
+export async function function_param_swap_task_simple() {
+  await function_param_swap(f_name, param_name_a, param_name_b);
   todo();
 }
