3b70efab21051ff79d4d923b0c5819bd75f18845
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 13:22:48 -0400

    function_auto_path public\src\js_function_id_add.mjs

--- public/src/js_function_id_add.mjs
+++ public/src/js_function_id_add.mjs
@@ -2,10 +2,12 @@ import { object_property_get } from "./object_property_get.mjs";
 import { js_identifier_unique_ast } from "./js_identifier_unique_ast.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
+import { log } from "./log.mjs";
 export function js_function_id_add(ast) {
   js_visit_type(ast, "FunctionDeclaration", function lambda(v) {
     let { node } = v;
     let id = object_property_get(node, "id");
+    log(id);
     if (id === null) {
       let unique = js_identifier_unique_ast(ast, "lambda");
       object_property_set(node, "id", unique);
