3c4dc93d775354079a3ca49ed4d608c05437bb32
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 08:26:29 -0400

    function_auto_path public\src\list_skip.mjs

---
+++ public/src/list_skip.mjs
@@ -0,0 +1,7 @@
+import { string_slice } from "./string_slice.mjs";
+import { string_size } from "./string_size.mjs";
+export function list_skip(s, skip_count) {
+  let b = string_size(s);
+  const skipped = string_slice(s, skip_count, b);
+  return skipped;
+}
