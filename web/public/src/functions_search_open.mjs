d48456558b4cc42050bd229a15994eaa6c33d96a
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:19:55 -0400

    function_auto_path public\src\functions_search_open.mjs

--- web/public/src/functions_search_open.mjs
+++ web/public/src/functions_search_open.mjs
@@ -1,8 +1,9 @@
+import { functions_search } from "./functions_search.mjs";
 import { function_open } from "./function_open.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { list_single } from "./list_single.mjs";
 export async function functions_search_open() {
-  let f_names = functions_names();
+  let result = await functions_search(search);
   let f_name = list_single(f_names);
   await function_open(f_name);
 }
