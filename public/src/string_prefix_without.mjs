f7f2449d7215a71932ac39969245ad2206705bbd
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:33:59 -0700

    function_auto_path public\src\string_prefix_without.mjs

--- public/src/string_prefix_without.mjs
+++ public/src/string_prefix_without.mjs
@@ -3,9 +3,11 @@ import { string_ends_with } from "./string_ends_with.mjs";
 import { error } from "./error.mjs";
 import { string_slice } from "./string_slice.mjs";
 import { string_size } from "./string_size.mjs";
-export function string_prefix_without(s, suffix) {
-  if (!string_starts_with(s, suffix)) {
+export function string_prefix_without(s, prefix) {
+  if (!string_starts_with(s, prefix)) {
     error();
   }
-  return string_slice(s, 0, string_size(s) - string_size(suffix));
+  let a = string_size(prefix);
+  let b = string_size(prefix);
+  return string_slice(s, a, b);
 }
