d84b8d6019824a056abf28fcd8b5b29151c69caf
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 20:13:35 -0700

    watch public\src\js_call_fill.mjs

--- web/public/src/js_call_fill.mjs
+++ web/public/src/js_call_fill.mjs
@@ -1,10 +1,13 @@
+import { log } from "./log.mjs";
 import { functions_names_includes } from "./functions_names_includes.mjs";
 import { js_call_new } from "./js_call_new.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
+import { js_unparse } from "./js_unparse.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_replace } from "./object_replace.mjs";
-export function js_call_fill(ast) {
+export async function js_call_fill(ast) {
+  let a = null;
   js_visit_type(ast, "ExpressionStatement", (v) => {
     let { node } = v;
     let { expression } = node;
@@ -13,9 +16,13 @@ export function js_call_fill(ast) {
       const valid = functions_names_includes(name);
       if (valid) {
         let s = js_call_new(name, ast);
+        a = s;
+        return;
         object_replace(node, s);
       }
     }
   });
-  return;js_call_fill  
+  return;
+  ("js_call_fill");
+  log(await js_unparse(a));
 }
