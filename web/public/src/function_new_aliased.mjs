b2a1164f8c3aa7860d12a2b05cd598048ef1d2b7
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:15:22 -0400

    function_auto_path public\src\function_new_aliased.mjs

--- web/public/src/function_new_aliased.mjs
+++ web/public/src/function_new_aliased.mjs
@@ -1,6 +1,6 @@
 import { function_alias_add } from "./function_alias_add.mjs";
 import { function_new } from "./function_new.mjs";
-export async function function_new_aliased() {
+export async function function_new_aliased(f_name, alias) {
   await function_new(f_name);
-  await function_alias_add(alias, f_name2);
+  await function_alias_add(alias, f_name);
 }
