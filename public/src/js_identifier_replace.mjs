eff69832d8509a155783b7963121d5c433213c86
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 18:00:42 -0400

    function_auto_path public\src\js_identifier_replace.mjs

--- public/src/js_identifier_replace.mjs
+++ public/src/js_identifier_replace.mjs
@@ -1,14 +1,14 @@
-import { function_aliases_search } from "./function_aliases_search.mjs";
+import { assert } from "./assert.mjs";
 import { js_node_is } from "./js_node_is.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
-export async function js_identifier_replace(ast, name_from, replacement) {
+export function js_identifier_replace(ast, name_from, replacement) {
   let ni = js_node_is(name_from);
   if (ni) {
-    let result = await function_aliases_search(search);
+    assert(b);
   }
   function lambda(v) {
     let { node } = v;
