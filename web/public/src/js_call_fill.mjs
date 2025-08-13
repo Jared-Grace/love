bf03907142734cbbc530a02c74d9cd0939c6262c
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:34:15 -0700

    watch public\src\js_call_fill.mjs

--- web/public/src/js_call_fill.mjs
+++ web/public/src/js_call_fill.mjs
@@ -1,5 +1,8 @@
 import { js_visit } from "./js_visit.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_call_fill(ast) {
-  js_visit_type(ast, "ExpressionStatement", (v) => {});
+  js_visit_type(ast, "ExpressionStatement", (v) => {
+    if (false) {
+    }
+  });
 }
