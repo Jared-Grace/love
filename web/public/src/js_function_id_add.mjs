a40c15a9d84cb7fef1ddf60fdf76099d6764b228
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 17:40:03 -0400

    function_auto_path public\src\js_function_id_add.mjs

--- web/public/src/js_function_id_add.mjs
+++ web/public/src/js_function_id_add.mjs
@@ -5,14 +5,14 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { log } from "./log.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_function_id_add(ast) {
-  js_visit_type(ast, "FunctionExpression", function lambda(v) {
+  function lambda(v) {
     let { node } = v;
     let id = object_property_get(node, "id");
-    log(id);
     if (id === null) {
       let unique = js_identifier_unique_ast(ast, "lambda");
       let value = js_parse_expression(unique);
       object_property_set(node, "id", value);
     }
-  });
+  }
+  js_visit_type(ast, "FunctionExpression", lambda);
 }
