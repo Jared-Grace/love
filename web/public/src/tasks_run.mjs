122856d1fcc9d18c8f8128378c1f9c83cb881062
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:51:07 -0400

    function_auto_path public\src\tasks_run.mjs

--- web/public/src/tasks_run.mjs
+++ web/public/src/tasks_run.mjs
@@ -4,6 +4,6 @@ import { functions_search } from "./functions_search.mjs";
 export async function tasks_run() {
   let result2 = task_function_name_part();
   let result = await functions_search(result2);
-  let properties = object_properties(obj);
+  let properties = object_properties(result);
   return result;
 }
