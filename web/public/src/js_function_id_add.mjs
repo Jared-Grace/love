dbdf78248eaef998c8325e69ec3fea0467991472
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 13:06:03 -0400

    function_auto_path public\src\js_function_id_add.mjs

--- web/public/src/js_function_id_add.mjs
+++ web/public/src/js_function_id_add.mjs
@@ -3,6 +3,6 @@ import { js_visit_type } from "./js_visit_type.mjs";
 export function js_function_id_add(ast) {
   js_visit_type(ast, "FunctionDeclaration", function lambda(v) {
     let { node } = v;
-    object_property_set(node, "type", "FunctionDeclaration");
+    object_property_set(node, "id", "FunctionDeclaration");
   });
 }
