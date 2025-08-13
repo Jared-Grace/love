9e41349167eed776c83d622f5597854f26d479e3
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 04:56:09 -0400

    watch public\src\js_visit_type_each_async.mjs

--- web/public/src/js_visit_type_each_async.mjs
+++ web/public/src/js_visit_type_each_async.mjs
@@ -1,4 +1,7 @@
+import { list_adder } from "./list_adder.mjs";
+import { each_async } from "./each_async.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
-export function js_visit_type_each_async(ast, type, lambda) {
-  js_visit_type(ast, type, lambda);
+export async function js_visit_type_each_async(ast, type, lambda) {
+  let vs = list_adder((la) => js_visit_type(ast, type, la));
+  await each_async(vs, lambda);
 }
