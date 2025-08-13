1ea12933106c66b4e38215f22821c50b5164b5ca
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:27:29 -0700

    function_auto_path public\src\catch_only_async.mjs

--- web/public/src/catch_only_async.mjs
+++ web/public/src/catch_only_async.mjs
@@ -1,11 +1,13 @@
+import { json_to } from "./json_to.mjs";
 import { log } from "./log.mjs";
 export async function catch_only_async(lambda, message_fragment) {
   try {
     await lambda();
   } catch (e) {
-    log({
+    let message = json_to({
       A: e,
     });
+    log(message);
     if (!e.message.includes(message_fragment)) {
       throw e;
     }
