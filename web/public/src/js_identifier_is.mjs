572e7c1e7bc1c6a9a587594be4e0484893653fe3
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 10:20:13 -0400

    function_auto_path public\src\js_identifier_is.mjs

--- web/public/src/js_identifier_is.mjs
+++ web/public/src/js_identifier_is.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is } from "./js_node_type_is.mjs";
-export function js_identifier_is(node,) {
-  return js_node_type_is(node, 'Identifier');
+export function js_identifier_is(node) {
+  return js_node_type_is(node, "Identifier");
 }
