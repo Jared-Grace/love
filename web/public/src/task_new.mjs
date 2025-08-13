1e6cc7f98c201fd1e3ff6cc0e79772c46e7c9e67
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:11:31 -0400

    function_auto_path public\src\task_new.mjs

--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -1,1 +1,4 @@
-export function task_new() {}
+import { data_function_current_get } from "./data_function_current_get.mjs";
+export async function task_new() {
+  let f_name = await data_function_current_get();
+}
