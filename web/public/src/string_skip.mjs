5cc93d93a12a7f7f96b358bafcce8f086db52a06
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:34:53 -0700

    function_auto_path public\src\string_prefix_without.mjs

---
+++ web/public/src/string_skip.mjs
@@ -0,0 +1,6 @@
+import {string_slice} from './string_slice.mjs';
+import {string_size} from './string_size.mjs';
+export function string_skip(s, skipped) {
+  let b = string_size(s);
+  return string_slice(s, skipped, b);
+}
