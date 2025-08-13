46df498e6279c3bacb399d840684c4832572ea6e
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 13:42:25 -0400

    function_auto_path public\src\js_atomize_function.mjs

--- web/public/src/js_atomize_function.mjs
+++ web/public/src/js_atomize_function.mjs
@@ -36,7 +36,8 @@ export async function js_atomize_function(ast) {
     const stack1 = list_get_end_1(stack);
     if (list_is(stack1)) {
       const stack2 = list_get_end_2(stack);
-      $ib;
+      if (false) {
+      }
       await js_node_atomize(existing, v);
     }
   });
