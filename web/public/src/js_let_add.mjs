f289946f8cba220e76537786db5c70bda3bed5ce
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 08:16:50 -0400

    function_auto_path public\src\js_let_add.mjs

--- web/public/src/js_let_add.mjs
+++ web/public/src/js_let_add.mjs
@@ -5,8 +5,6 @@ import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 export function js_let_add(ast) {
-  return;
-  a = 1;
   js_visit_type(ast, "AssignmentExpression", function lambda(v) {
     let { stack } = v;
     let item = list_get_end_1(stack);
@@ -19,4 +17,6 @@ export function js_let_add(ast) {
     return;
     let assign = js_declare(name, init);
   });
+  return;
+  a = 1;
 }
