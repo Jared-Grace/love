cbf7dbac83654c48ef3b734c75cf9ba620ff1109
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:25:29 -0700

    function_auto_path public\src\catch_only_async.mjs

--- web/public/src/catch_only_async.mjs
+++ web/public/src/catch_only_async.mjs
@@ -3,7 +3,9 @@ export async function catch_only_async(lambda, message_fragment) {
   try {
     await lambda();
   } catch (e) {
-    log(e.message);
+    log({
+      A: e.message,
+    });
     if (!e.message.includes(message_fragment)) {
       throw e;
     }
