dcb548d08e43ff5314b65588cbcf951492a36c64
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:03:38 -0400

    function_auto_path public\src\js_declare_assign_null.mjs

--- public/src/js_declare_assign_null.mjs
+++ public/src/js_declare_assign_null.mjs
@@ -5,13 +5,16 @@ import { object_property_set } from "./object_property_set.mjs";
 import { js_parse } from "./js_parse.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_declare_assign_null(ast) {
-  js_visit_type(ast, "VariableDeclarator", function lambda(v) {
+  function lambda(v) {
     let { node } = v;
     let { init } = node;
     if (init === null) {
+      if (false) {
+      }
       let init_code = js_keyword_null();
       let value = js_parse_expression(init_code);
       object_property_set(node, "init", value);
     }
-  });
+  }
+  js_visit_type(ast, "VariableDeclarator", lambda);
 }
