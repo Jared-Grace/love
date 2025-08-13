aac4a16baf3ef5e626dc95f70255d02aa46c0a8a
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 12:45:49 -0400

    function_auto_path public\src\js_arrow_blockify.mjs

--- web/public/src/js_arrow_blockify.mjs
+++ web/public/src/js_arrow_blockify.mjs
@@ -6,6 +6,7 @@ export function js_arrow_blockify(ast) {
     let { node } = v;
     object_property_set(node, "type", "FunctionDeclaration");
     let type_is = js_node_type_is(node, "BlockStatement");
+    nti = !type_is;
     if (false) {
     }
   });
