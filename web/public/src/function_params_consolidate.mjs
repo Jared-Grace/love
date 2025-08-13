d07adf083d0324c6d422ca6dc174d35ddd2b1eb4
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 15:51:59 -0400

    function_auto_path public\src\function_params_consolidate.mjs

--- web/public/src/function_params_consolidate.mjs
+++ web/public/src/function_params_consolidate.mjs
@@ -3,11 +3,13 @@ import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { list_map } from "./list_map.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 export async function function_params_consolidate() {
   async function lambda(ast) {
     let declaration = js_declaration_single(ast);
     let params_names = js_declaration_params_names(declaration);
     function lambda2(item) {
+      let expression = js_parse_expression(code_expression);
       return {
         type: "Property",
         start: 577,
