f7478a8b17ca4f53d60d24876d652f51c1326c76
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 04:52:40 -0400

    watch public\src\js_call_fill.mjs

--- web/public/src/js_unparse.mjs
+++ web/public/src/js_unparse.mjs
@@ -2,6 +2,7 @@ import { js_visit_each_async } from "./js_visit_each_async.mjs";
 import { error } from "./error.mjs";
 import { log } from "./log.mjs";
 import { import_install } from "./import_install.mjs";
+import { json_format_to } from "./json_format_to.mjs";
 import { generate } from "astring";
 export async function js_unparse(ast) {
   let output;
@@ -18,7 +19,7 @@ export async function js_unparse(ast) {
         return true;
       }
     });
-    let message = json_formnat_to(current);
+    let message = json_format_to(current); 
     log(message);
     throw e;
   }
--- web/public/src/js_visit.mjs
+++ web/public/src/js_visit.mjs
@@ -4,8 +4,12 @@ import { visit_filter } from "./visit_filter.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_visit_filter } from "./js_visit_filter.mjs";
 export function js_visit(ast, on_each) {
-  if (promise_is(ast)) {
+  if (!promise_not_is(ast)) {
     error();
   }
   visit_filter(ast, js_visit_children_get, js_visit_filter, on_each);
 }
+function promise_not_is(ast) {
+  return !promise_is(ast);
+}
+
