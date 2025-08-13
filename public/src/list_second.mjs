0a3afe923e7e33ccd4934473e1e41d952ac018c1
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 08:23:51 -0400

    function_auto_path public\src\list_second.mjs

--- public/src/list_second.mjs
+++ public/src/list_second.mjs
@@ -1,6 +1,6 @@
 import { list_get } from "./list_get.mjs";
 export function list_second(list) {
-  const index = 0;
-  const first = list_get(list, index);
-  return first;
+  const index = 1;
+  const second = list_get(list, index);
+  return second;
 }
