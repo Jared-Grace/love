f2293c1234e849238a113321b2dc58f918ed591f
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:18:11 -0400

    function_auto_path public\src\js_stack_filtered_multiple.mjs

--- web/public/src/js_stack_filtered_multiple.mjs
+++ web/public/src/js_stack_filtered_multiple.mjs
@@ -1,3 +1,4 @@
+import { list_any } from "./list_any.mjs";
 import { list_all } from "./list_all.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { js_node_is } from "./js_node_is.mjs";
@@ -8,7 +9,7 @@ export function js_stack_filtered_multiple(stack, types) {
       let type_is = js_node_type_is(n, type);
       return type_is;
     }
-    list_all(types, lambda2);
+    list_any(types, lambda2);
   }
   let filtered = list_filter(stack, lambda);
   return filtered;
