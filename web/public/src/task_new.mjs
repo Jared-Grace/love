37858028c52f1123e56cfa7504fc0a83a45351df
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:45:24 -0400

    function_auto_path public\src\task_new.mjs

--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -10,7 +10,7 @@ import { function_name_combine } from "./function_name_combine.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 export async function task_new(task_name) {
   let f_name = await data_function_current_get();
-  let f_name_task = function_name_combine(f_name, task_name);
+  let f_name_task = function_name_combine_multiple([f_name, "task", task_name]);
   await function_new(f_name_task);
   let result = await function_transform(
     f_name_task,
