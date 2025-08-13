6c9bbd396fedc7356d841fce5c63fb4659ca9cfc
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:03:23 -0700

    function_auto_path public\src\git_acp.mjs

--- web/public/src/git_acp.mjs
+++ web/public/src/git_acp.mjs
@@ -2,10 +2,11 @@ import { catch_ignore_async } from "./catch_ignore_async.mjs";
 import { catch_ignore } from "./catch_ignore.mjs";
 import { command_line } from "./command_line.mjs";
 import { git_push } from "./git_push.mjs";
+import { command_line_git } from "./command_line_git.mjs";
 export async function git_acp(message) {
-  await command_line("git add -A");
+  await command_line_git("add -A");
   await catch_ignore_async(async function lambda() {
-    await command_line(`git commit -m "${message}"`);
+    await command_line(`commit -m "${message}"`);
   });
   await git_push();
 }
