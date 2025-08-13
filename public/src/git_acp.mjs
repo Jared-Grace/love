0d7667ba2f683b0709b930c0507f8ba896e93010
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:05:49 -0700

    function_auto_path public\src\git_acp.mjs

--- public/src/git_acp.mjs
+++ public/src/git_acp.mjs
@@ -5,10 +5,10 @@ import { git_push } from "./git_push.mjs";
 import { command_line_git } from "./command_line_git.mjs";
 export async function git_acp(message) {
   await command_line_git("add -A");
-  await command_line_git(`fetch origin main`);
   await catch_ignore_async(async function lambda() {
     await command_line_git(`commit -m "${message}"`);
   });
+  await command_line_git(`fetch origin main`);
   await command_line_git(
     `rebase --autostash --no-stat --no-verify --no-interactive origin/main`,
   );
