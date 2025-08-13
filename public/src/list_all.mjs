e5760446731ea156ba3be9a9291789d573ed4689
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:12:45 -0400

    function_auto_path public\src\list_all.mjs

--- public/src/list_all.mjs
+++ public/src/list_all.mjs
@@ -2,8 +2,8 @@ import { list_empty_is } from "./list_empty_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_all(list, lambda$item) {
-  let filtered = list_filter(list, lambda$item);
-  return list_size(filtered) === list_size(list);
+  let list = list_filter(list, lambda$item);
+  return list_size(list) === list_size(list);
   {
     let filtered = list_filter(list, lambda$item);
     return list_size(filtered) === list_size(list);
