31a4408399379b47838f4b7e0ef385db55e4ac05
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:01:29 -0400

    function_auto_path public\src\js_declare_assign_null.mjs

--- web/public/src/js_declare_assign_null.mjs
+++ web/public/src/js_declare_assign_null.mjs
@@ -4,6 +4,7 @@ export function js_declare_assign_null(ast) {
   let a;
   js_visit_type(ast, "VariableDeclarator", function lambda(v) {
     let { node } = v;
+    let { init } = node;
     log(node);
   });
   return;
