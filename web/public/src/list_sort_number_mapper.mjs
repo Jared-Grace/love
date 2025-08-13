580347f8ab3a64bdc8bdf261395c684edbfbf717
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 17:35:20 -0400

    function_auto_path public\src\list_sort_number_mapper.mjs

--- web/public/src/list_sort_number_mapper.mjs
+++ web/public/src/list_sort_number_mapper.mjs
@@ -1,6 +1,5 @@
 export function list_sort_number_mapper(list, lambda$item) {
   function lambda(a, b) {
-    return lambda$item(a) - lambda$item(b);
     const va = lambda$item(a);
     const vb = lambda$item(b);
     if (va < vb) return -1;
