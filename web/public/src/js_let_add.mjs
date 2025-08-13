c7f7ba6097ea5f248e4c2675a8a15cc8db251b0c
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:33:12 -0400

    function_auto_path public\src\js_let_add.mjs

--- web/public/src/js_let_add.mjs
+++ web/public/src/js_let_add.mjs
@@ -1,4 +1,6 @@
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_let_add(ast) {
-  js_visit_type(parsed, type, function lambda(v) {});
+  return;
+  a = 1;
+  js_visit_type(ast, type, function lambda(v) {});
 }
