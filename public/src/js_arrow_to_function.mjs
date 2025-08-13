abacc85b74517c448ed91fe353075c62527bf1f3
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 10:32:26 -0400

    function_auto_path public\src\js_arrow_to_function.mjs

--- public/src/js_arrow_to_function.mjs
+++ public/src/js_arrow_to_function.mjs
@@ -3,6 +3,6 @@ import { js_visit_type } from "./js_visit_type.mjs";
 export function js_arrow_to_function(ast) {
   js_visit_type(ast, "ArrowFunctionExpression", function lambda(v) {
     let { node } = v;
-    object_property_set(object, property_name, value);
+    object_property_set(node, "type", "FunctionDeclaration");
   });
 }
