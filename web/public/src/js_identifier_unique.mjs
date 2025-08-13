cb103bf23c4510c1900990eb40905af2d01ad5f0
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 12:51:30 -0400

    function_auto_path public\src\js_identifier_unique.mjs

--- web/public/src/js_identifier_unique.mjs
+++ web/public/src/js_identifier_unique.mjs
@@ -3,13 +3,13 @@ import { string_empty_is } from "./string_empty_is.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function js_identifier_unique(existing, name) {
-  let result = null;
+  let unique = null;
   let attempt = 1;
   do {
     const suffix = attempt === 1 && string_empty_not_is(name) ? "" : attempt;
-    result = name + suffix;
+    unique = name + suffix;
     attempt++;
-  } while (string_empty_is(result) || list_includes(existing, result));
-  list_add(existing, result);
-  return result;
+  } while (string_empty_is(unique) || list_includes(existing, unique));
+  list_add(existing, unique);
+  return unique;
 }
