256a5094c45ad20c5811616136eaa2f0725843e6
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 18:01:44 -0400

    function_auto_path public\src\js_identifier_replace.mjs

---
+++ web/public/src/js_node_type_is_assert.mjs
@@ -0,0 +1,6 @@
+import {assert} from './assert.mjs';
+import {js_node_type_is} from './js_node_type_is.mjs';
+export function js_node_type_is_assert(node, type) {
+  let type_is = js_node_type_is(node, type);
+  assert(type_is);
+}
