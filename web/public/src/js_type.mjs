dce921a07d9d15c952001dd3e61a21c82aacc165
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 05:41:44 -0400

    function_auto_path public\src\js_type.mjs

--- web/public/src/js_type.mjs
+++ web/public/src/js_type.mjs
@@ -1,8 +1,8 @@
-import {js_visit_type} from './js_visit_type.mjs';
-import {list_adder} from './list_adder.mjs';
+import { js_visit_type } from "./js_visit_type.mjs";
+import { list_adder } from "./list_adder.mjs";
 export function js_type(ast, node_type) {
-    const vs = list_adder(la => {
-        js_visit_type(ast, node_type, la);
-    });
+  const vs = list_adder((la) => {
+    js_visit_type(ast, node_type, la);
+  });
   return vs;
 }
