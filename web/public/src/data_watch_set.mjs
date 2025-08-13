d189a67a1790247d801c76a928de09bb90a339c1
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:45:01 -0400

    function_auto_path public\src\data_watch_set.mjs

---
+++ web/public/src/data_watch_set.mjs
@@ -0,0 +1,14 @@
+import { function_exists } from "./function_exists.mjs";
+import { assert } from "./assert.mjs";
+import { marker_first } from "./marker_first.mjs";
+import { marker_current_set } from "./marker_current_set.mjs";
+import { data_transform } from "./data_transform.mjs";
+export async function data_watch_set(f_name) {
+  assert(await function_exists(f_name));
+  function lambda(previous) {
+    f_name;
+  }
+  await data_transform("function_current", null, lambda);
+  let name = marker_first();
+  await marker_current_set(name);
+}
