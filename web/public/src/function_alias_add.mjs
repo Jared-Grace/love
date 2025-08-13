f86776358da64e23748f06f1a242790e94f27ec7
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 17:24:47 -0400

    function_auto_path public\src\function_alias_add.mjs

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -3,12 +3,15 @@ import { error } from "./error.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { function_alias_add_generic } from "./function_alias_add_generic.mjs";
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
-export async function function_alias_add(alias, f_name) {
-  await function_alias_generic(alias, function lambda(a) {
+export async function function_alias_add(first, second) {
+  list = [first, second];
+  (alias, f_name);
+  function lambda(a) {
     let { exists, aliases, unaliased } = a;
     if (exists) {
       error(unaliased);
     }
     object_property_set(aliases, alias, f_name);
-  });
+  }
+  await function_alias_generic(alias, lambda);
 }
