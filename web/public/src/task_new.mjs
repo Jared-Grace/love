e7ec35f4760a44e2d383dd2bebd452caf2944303
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:12:44 -0400

    function_auto_path public\src\task_new.mjs

--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -4,5 +4,5 @@ import { data_function_current_get } from "./data_function_current_get.mjs";
 export async function task_new(task_name) {
   let f_name = await data_function_current_get();
   let combined = function_name_combine(f_name, task_name);
-  await function_new(f_name2);
+  await function_new(combined);
 }
