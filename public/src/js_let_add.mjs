5ab02bb94948242b53345e6f8e4953de6acce5d3
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:33:33 -0400

    function_auto_path public\src\js_let_add.mjs

--- public/src/js_let_add.mjs
+++ public/src/js_let_add.mjs
@@ -2,5 +2,5 @@ import { js_visit_type } from "./js_visit_type.mjs";
 export function js_let_add(ast) {
   return;
   a = 1;
-  js_visit_type(ast, type, function lambda(v) {});
+  js_visit_type(ast, "AssignmentExpression", function lambda(v) {});
 }
