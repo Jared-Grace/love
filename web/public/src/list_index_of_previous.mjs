a4935339cf948c18cc55a541719370119fe6a7c7
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 14:47:10 -0400

    function_auto_path public\src\list_index_of_previous.mjs

--- web/public/src/list_index_of_previous.mjs
+++ web/public/src/list_index_of_previous.mjs
@@ -2,6 +2,6 @@ import { list_index_of_delta } from "./list_index_of_delta.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 export function list_index_of_previous(list, item) {
   const delta = -1;
-  const newLocal = list_index_of_delta(list, item, delta);
-  return newLocal;
+  const index_previous = list_index_of_delta(list, item, delta);
+  return index_previous;
 }
