8c7adf260f13a7ec929414895fd9507feadc8208
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 12:58:26 -0400

    function_auto_path public\src\object_copy.mjs

--- public/src/object_copy.mjs
+++ public/src/object_copy.mjs
@@ -1,4 +1,5 @@
 import { object_merge } from "./object_merge.mjs";
-export function object_copy( from) {
-  return object_merge({}, from);
+export function object_copy(from) {
+  const copy = object_merge({}, from);
+  return copy;
 }
