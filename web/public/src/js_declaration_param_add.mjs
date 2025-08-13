eed3750ccea31a158707ecabb677d8682fdedfe5
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 15:37:01 -0400

    m

--- web/public/src/js_declaration_param_add.mjs
+++ web/public/src/js_declaration_param_add.mjs
@@ -1,6 +1,8 @@
-import {js_parse_expression} from './js_parse_expression.mjs';
-import {list_add} from './list_add.mjs';
+import { marker } from "./marker.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
+import { list_add } from "./list_add.mjs";
 export function js_declaration_param_add(declaration, param_name) {
-  let {params} = declaration;
+  marker("1");
+  let { params } = declaration;
   list_add(params, js_parse_expression(param_name));
 }
