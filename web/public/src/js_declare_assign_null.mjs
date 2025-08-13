2e13bb4a3b5c447eb76376df0669ef2ddc0ce5cc
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:05:14 -0400

    function_auto_path public\src\js_declare_assign_null.mjs

--- web/public/src/js_declare_assign_null.mjs
+++ web/public/src/js_declare_assign_null.mjs
@@ -5,7 +5,6 @@ import { object_property_set } from "./object_property_set.mjs";
 import { js_parse } from "./js_parse.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_declare_assign_null(ast) {
-  let a = null;
   js_visit_type(ast, "VariableDeclarator", function lambda(v) {
     let { node } = v;
     let { init } = node;
