ff0d24569bb334cec1c5392967005abba5ca35c8
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:53:22 -0400

    function_auto_path public\src\function_run.mjs

--- web/public/src/function_run.mjs
+++ web/public/src/function_run.mjs
@@ -1,8 +1,6 @@
 import { function_import } from "./function_import.mjs";
-
-export async function function_run(funcName, args) {
-  const fn = await function_import(funcName);
-
+export async function function_run(f_name, args) {
+  const fn = await function_import(f_name);
   const result = await fn(...args);
   return result;
 }
