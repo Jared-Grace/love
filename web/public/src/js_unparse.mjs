72c0a063d0344ecaaf1c523e2dae524b0a2c6ad3
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 04:45:50 -0400

    watch public\src\js_unparse.mjs

--- web/public/src/js_unparse.mjs
+++ web/public/src/js_unparse.mjs
@@ -1,3 +1,4 @@
+import { js_visit_each_async } from "./js_visit_each_async.mjs";
 import { error } from "./error.mjs";
 import { log } from "./log.mjs";
 import { import_install } from "./import_install.mjs";
@@ -7,6 +8,18 @@ export async function js_unparse(ast) {
   try {
     output = generate(ast);
   } catch (e) {
+    let current = null;
+    await js_visit_each_async(a, async (v) => {
+      let { node } = v;
+      try {
+        await js_unparse(current);
+      } catch (e) {
+        current = a;
+        return true;
+      }
+    });
+    let message = json_formnat_to(a);
+    log(message);
     throw e;
   }
   return output;
