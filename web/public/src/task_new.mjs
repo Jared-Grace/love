c99d77117ea44c1bcd332bc7aabc213e2a232468
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:13:12 -0400

    function_auto_path public\src\task_new.mjs

--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -5,4 +5,5 @@ export async function task_new(task_name) {
   let f_name = await data_function_current_get();
   let f_name_task = function_name_combine(f_name, task_name);
   await function_new(f_name_task);
+  ft;
 }
