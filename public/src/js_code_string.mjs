409338fdb5de7fa00f8482e4fee98c69801b5dc3
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:46:13 -0700

    watch public\src\js_code_string.mjs

--- public/src/js_code_string.mjs
+++ public/src/js_code_string.mjs
@@ -1,11 +1,11 @@
-import {js_unparse} from './js_unparse.mjs';
-import {object_property_delete} from './object_property_delete.mjs';
-import {object_property_set} from './object_property_set.mjs';
-import {js_parse_expression} from './js_parse_expression.mjs';
-export function js_code_string(value_string) {
+import { js_unparse } from "./js_unparse.mjs";
+import { object_property_delete } from "./object_property_delete.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
+export async function js_code_string(value_string) {
   let parsed_string = js_parse_expression('""');
-  object_property_set(parsed_string, 'value', value_string);
-  object_property_delete(parsed_string, 'raw');
+  object_property_set(parsed_string, "value", value_string);
+  object_property_delete(parsed_string, "raw");
   let code_string = await js_unparse(parsed_string);
   return code_string;
 }
