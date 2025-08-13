b16bf7d53ed53f93430f26845d5aa9c00fb05093
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:31:36 -0700

    function_auto_path public\src\git_acp.mjs

---
+++ public/src/git_rebase.mjs
@@ -0,0 +1,15 @@
+import {log_keep} from './log_keep.mjs';
+import {command_line_git} from './command_line_git.mjs';
+export async function git_rebase() {
+  try {
+    await command_line_git("rebase --autostash --no-stat --no-verify origin/main");
+  } catch (e) {
+    log_keep("Rebase failed, aborting rebase");
+    try {
+      await command_line_git("rebase --abort");
+    } catch (abortErr) {
+      log_keep("No rebase in progress, nothing to abort.");
+    }
+    throw e;
+  }
+}
