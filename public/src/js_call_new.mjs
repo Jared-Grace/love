ac437f69639d691d67fe4579421f61c0453113c7
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 05:29:42 -0400

    function_auto_path public\src\js_call_new.mjs

--- public/src/js_call_new.mjs
+++ public/src/js_call_new.mjs
@@ -1,3 +1,4 @@
+import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 import { js_code_let_assign } from "./js_code_let_assign.mjs";
 import { js_return_name } from "./js_return_name.mjs";
@@ -26,26 +27,29 @@ export async function js_call_new(f_name_call, ast) {
   } = await function_parse_declaration(f_name_call);
   let existing = js_identifiers_names(ast);
   let arg_names = js_declaration_params_names(declaration);
-  let args_code = list_map(arg_names, (arg_name) => {
-    let arg_code = js_identifier_unique(existing, arg_name);
-    let split = string_split(arg_name, "$");
-    const lambda = "lambda";
-    if (list_first(split) === lambda) {
-      let skip_count = 1;
-      let b = list_size(split);
-      let remaining = list_slice(split, skip_count, b);
-      let lamda_name = js_identifier_unique(existing, lambda);
-      let async_is = object_property_get(declaration, "async");
-      let code = js_code_declaration(lamda_name, "", async_is);
-      let declaration_lambda = js_parse_statement_module(code);
-      each(remaining, (p) => {
-        let unique = js_identifier_unique(existing, p);
-        js_declaration_param_add(declaration_lambda, unique);
-      });
-      arg_code = js_unparse(declaration_lambda);
-    }
-    return arg_code;
-  });
+  let args_code = await list_map_unordered_async(
+    arg_names,
+    async (arg_name) => {
+      let arg_code = js_identifier_unique(existing, arg_name);
+      let split = string_split(arg_name, "$");
+      const lambda = "lambda";
+      if (list_first(split) === lambda) {
+        let skip_count = 1;
+        let b = list_size(split);
+        let remaining = list_slice(split, skip_count, b);
+        let lamda_name = js_identifier_unique(existing, lambda);
+        let async_is = object_property_get(declaration, "async");
+        let code = js_code_declaration(lamda_name, "", async_is);
+        let declaration_lambda = js_parse_statement_module(code);
+        each(remaining, (p) => {
+          let unique = js_identifier_unique(existing, p);
+          js_declaration_param_add(declaration_lambda, unique);
+        });
+        arg_code = await js_unparse(declaration_lambda);
+      }
+      return arg_code;
+    },
+  );
   let code = js_code_call_args_await_maybe(unaliased, args_code, declaration);
   let body_block = js_return_name(ast_call);
   if (body_block !== null) {
