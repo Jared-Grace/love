dcae6ebb7c72935af62a6dd8651cd25b24799bf8
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:42:44 -0400

    function_auto_path public\src\task_new.mjs

--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -11,8 +11,11 @@ export async function task_new(task_name) {
   let f_name = await data_function_current_get();
   let f_name_task = function_name_combine(f_name, task_name);
   await function_new(f_name_task);
-  let result = await function_transform(f_name, async function lambda(ast) {
-    let body_block = js_declaration_single_block_blody(ast);
-    js_call_insert(error.name, ["todo"], body_block, 0);
-  });
+  let result = await function_transform(
+    f_name_task,
+    async function lambda(ast) {
+      let body_block = js_declaration_single_block_blody(ast);
+      js_call_insert(error.name, ["todo"], body_block, 0);
+    },
+  );
 }
