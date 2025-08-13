b948c38d161408d51ceaca4a7a5d93be59c53103
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:22:45 -0400

    function_auto_path public\src\js_node_types_is.mjs

--- web/public/src/js_node_types_is.mjs
+++ web/public/src/js_node_types_is.mjs
@@ -1,9 +1,10 @@
-import {list_any} from './list_any.mjs';
-import {js_node_type_is} from './js_node_type_is.mjs';
+import { list_any } from "./list_any.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_node_types_is(item, types) {
   function lambda2(type) {
     let type_is = js_node_type_is(item, type);
     return type_is;
   }
-  return list_any(types, lambda2);
+  const match = list_any(types, lambda2);
+  return match;
 }
