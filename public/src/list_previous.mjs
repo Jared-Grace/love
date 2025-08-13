b182dd92b9b9c482f5f9d0725f73bda1128fd4c6
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 14:49:54 -0400

    function_auto_path public\src\list_previous.mjs

--- public/src/list_previous.mjs
+++ public/src/list_previous.mjs
@@ -1,7 +1,7 @@
-import {list_get} from './list_get.mjs';
-import {list_index_of_previous} from './list_index_of_previous.mjs';
-export function list_previous(stack, block) {
-  let index_previous = list_index_of_previous(stack, block);
+import { list_get } from "./list_get.mjs";
+import { list_index_of_previous } from "./list_index_of_previous.mjs";
+export function list_previous(list, item) {
+  let index_previous = list_index_of_previous(list, item);
   let previous = list_get(list, index_previous);
   return previous;
 }
