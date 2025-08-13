3bb453845bb1507cc5f3ec4ab22f1e0049b62e5e
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:17:21 -0400

    function_auto_path public\src\functions_search_open.mjs

--- web/public/src/functions_search_open.mjs
+++ web/public/src/functions_search_open.mjs
@@ -1,1 +1,6 @@
-export function functions_search_open() {}
+import { functions_names } from "./functions_names.mjs";
+import { list_single } from "./list_single.mjs";
+export function functions_search_open() {
+  let f_names = functions_names();
+  let result = list_single(list);
+}
