a96544457c2c1145761b405309100830ee21feec
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 18:08:23 -0400

    function_auto_path public\src\js_identifier_replace.mjs

--- web/public/src/js_identifier_replace.mjs
+++ web/public/src/js_identifier_replace.mjs
@@ -7,11 +7,6 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_identifier_replace(ast, name_from, name_to) {
-  let ni = js_node_is(name_from);
-  if (ni) {
-    js_node_type_is_assert(name_from, "Identifier");
-    name_from = object_property_get(name_from, "name");
-  }
   function lambda(v) {
     let { node } = v;
     if (object_property_get(node, "name") === name_from) {
