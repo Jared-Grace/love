dd99e4830c265ebaeebd2990560f86af9ef84318
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 17:18:14 -0700

    function_auto_path public\src\catch_log_async.mjs

--- web/public/src/catch_log_async.mjs
+++ web/public/src/catch_log_async.mjs
@@ -1,10 +1,14 @@
+import { identity } from "./identity.mjs";
 import { log } from "./log.mjs";
 import { log_keep } from "./log_keep.mjs";
 export async function catch_log_async(lambda) {
-  
-  try { 
+  try {
     await lambda();
-  } catch (e) {log_keep((e));
-  }return
-    log_keep(message_get(e));message_get=identity;      
+  } catch (e) {
+    log_keep(e);
+  }
+  return;
+  let message = message_get(e);
+  log_keep(message);
+  message_get = identity;
 }
