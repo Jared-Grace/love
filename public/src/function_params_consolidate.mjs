bfd548423f2ae13bbb496b46295461a81c07d534
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 15:38:40 -0400

    function_auto_path public\src\function_params_consolidate.mjs

--- public/src/function_params_consolidate.mjs
+++ public/src/function_params_consolidate.mjs
@@ -1,10 +1,11 @@
+import { js_declaration_params_names } from "./js_declaration_params_names.mjs";
 import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_transform } from "./function_transform.mjs";
 export async function function_params_consolidate() {
   async function lambda(ast) {
     let declaration = js_declaration_single(ast);
-    let params = js_declaration_params_get(declaration);
+    let params = js_declaration_params_names(declaration);
   }
   let result = await function_transform(f_name, lambda);
 }
