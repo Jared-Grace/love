4f808ec8cf623608e2eb383dd2559049980db086
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:50:26 -0400

    function_auto_path public\src\function_wrap.mjs

---
+++ web/public/src/js_statement_return.mjs
@@ -0,0 +1,7 @@
+import {js_parse_statement} from './js_parse_statement.mjs';
+import {js_code_return} from './js_code_return.mjs';
+export function js_statement_return(code) {
+  let code2 = js_code_return(code);
+  let item = js_parse_statement(code2);
+  return item;
+}
