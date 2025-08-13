e5b46fd3bd80d3652aab9ae6c6ac988bc24badd1
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:22:56 -0400

    function_auto_path public\src\function_name_unalias.mjs

--- web/public/src/function_name_unalias.mjs
+++ web/public/src/function_name_unalias.mjs
@@ -1,5 +1,7 @@
 import { function_alias_add_generic } from "./function_alias_add_generic.mjs";
 export async function function_name_unalias(f_name) {
-  var { exists, unaliased } = await function_alias_add_generic(f_name);
-  return exists ? unaliased : f_name;
+  var { exists, unaliased: unaliased_actual } =
+    await function_alias_add_generic(f_name);
+  const unaliased = exists ? unaliased_actual : f_name;
+  return unaliased;
 }
