1a6950525b6bcb157b1fb7d34fe4d8c51ff766c1
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:18:31 -0400

    function_auto_path public\src\list_single.mjs

--- web/public/src/list_single.mjs
+++ web/public/src/list_single.mjs
@@ -1,8 +1,9 @@
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_first } from "./list_first.mjs";
 import { error } from "./error.mjs";
+import { list_size_1 } from "./list_size_1.mjs";
 export function list_single(list) {
-  if (list_empty_is(list)) {
+  if (!list_size_1(list)) {
     error();
   }
   return list_first(list);
