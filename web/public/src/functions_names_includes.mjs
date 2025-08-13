c89984e5e4d097ac30d35212d2244ba3405fb79b
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:36:24 -0700

    watch public\src\js_node_atomize.mjs

---
+++ web/public/src/functions_names_includes.mjs
@@ -0,0 +1,7 @@
+import {list_includes} from './list_includes.mjs';
+import {functions_names} from './functions_names.mjs';
+export function functions_names_includes(name) {
+  let list = functions_names();
+  const valid = list_includes(list, name);
+  return valid;
+}
