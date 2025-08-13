1203902f90328cfc9703d1f41a2534842008df3c
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:20:19 -0700

    function_auto_path public\src\git_rebase.mjs

--- web/public/src/git_rebase.mjs
+++ web/public/src/git_rebase.mjs
@@ -1,8 +1,8 @@
-import {log_keep} from './log_keep.mjs';
-import {command_line_git} from './command_line_git.mjs';
+import { log_keep } from "./log_keep.mjs";
+import { command_line_git } from "./command_line_git.mjs";
 export async function git_rebase() {
   try {
-    await command_line_git("rebase --autostash --no-stat --no-verify origin/main");
+    await command_line_git("rebase --no-stat --no-verify origin/main");
   } catch (e) {
     log_keep("Rebase failed, aborting rebase");
     try {
