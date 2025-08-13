0f6cbf0966f2f6a0c1f9ecb99fb7200f981bc146
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:38:05 -0400

    function_auto_path public\src\list_get_end.mjs

--- web/public/src/list_get_end.mjs
+++ web/public/src/list_get_end.mjs
@@ -2,5 +2,6 @@ import { list_index_last } from "./list_index_last.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_get_end(stack, index_from_end) {
-  return list_get(stack, list_index_last(stack) - index_from_end);
+  const item = list_get(stack, list_index_last(stack) - index_from_end);
+  return item;
 }
