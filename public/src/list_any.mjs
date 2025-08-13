bbc4c0051a224524ee41bc04733bf050fc7a392a
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 19:40:24 -0400

    function_auto_path public\src\list_any.mjs

--- public/src/list_any.mjs
+++ public/src/list_any.mjs
@@ -2,7 +2,7 @@ import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
-export function list_any(list, predicate) {
-  let filtered = list_filter(list, predicate);
+export function list_any(list, lambda$item) {
+  let filtered = list_filter(list, lambda$item);
   return list_empty_not_is(filtered);
 }
