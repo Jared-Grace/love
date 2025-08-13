d7fd51f9793beb3d6a084f9d554c18b35a46beae
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:22:49 -0400

    function_auto_path public\src\list_any.mjs

--- public/src/list_any.mjs
+++ public/src/list_any.mjs
@@ -3,5 +3,6 @@ import { list_empty_is } from "./list_empty_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_any(list, predicate) {
-  return list_empty_not_is(list_filter(list, predicate));
+  let body_block = list_filter(list, predicate);
+  return list_empty_not_is(body_block);
 }
