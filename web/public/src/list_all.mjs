07ef756947a29684a8966924c6a47810ea63d1e6
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:08:16 -0400

    function_auto_path public\src\list_all.mjs

--- web/public/src/list_all.mjs
+++ web/public/src/list_all.mjs
@@ -1,6 +1,7 @@
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
-export function list_all(list, predicate) {
-  return list_size(list_filter(list, predicate))===list_size(list);
+export function list_all(list, lambda$item) {
+  let list = list_filter(list, lambda$item);
+  return list_size(list) === list_size(list);
 }
