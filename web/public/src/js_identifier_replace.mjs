b60b3dcc94383f7b2df50a56b10cd36f18214188
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 17:59:11 -0400

    function_auto_path public\src\js_identifier_replace.mjs

--- web/public/src/js_identifier_replace.mjs
+++ web/public/src/js_identifier_replace.mjs
@@ -1,12 +1,13 @@
-import {object_property_set} from './object_property_set.mjs';
-import {js_visit_type} from "./js_visit_type.mjs";
-import {object_property_get} from "./object_property_get.mjs";
-import {list_adder_unique} from "./list_adder_unique.mjs";
-export function js_identifier_replace(ast, name_from, name_to) {
-  js_visit_type(ast, "Identifier", v => {
-    let {node} = v;
+import { object_property_set } from "./object_property_set.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_adder_unique } from "./list_adder_unique.mjs";
+export function js_identifier_replace(ast, name_from, replacement) {
+  function lambda(v) {
+    let { node } = v;
     if (object_property_get(node, "name") === name_from) {
-      object_property_set(node, "name", name_to);
+      object_property_set(node, "name", replacement);
     }
-  });
+  }
+  js_visit_type(ast, "Identifier", lambda);
 }
