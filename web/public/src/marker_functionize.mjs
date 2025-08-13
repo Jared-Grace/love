711a7711975c003c19e86aaa8ae248c51554cc8b
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 18:23:46 -0700

    function_auto_path public\src\marker_functionize.mjs

--- web/public/src/marker_functionize.mjs
+++ web/public/src/marker_functionize.mjs
@@ -88,11 +88,12 @@ export async function marker_functionize(m_name_from, m_name_to, f_name_new) {
     list_add_multiple(body_block, range);
     let { body } = ast;
     list_add(body, declaration);
+    let message = js_unparse(body);
+    log(message);
     let missing = list_adder_unique((la) => {
       js_visit_type(declaration, "Identifier", (v) => {
         let defineds = js_identifier_defineds(v);
         let { node } = v;
-        log(node);
         let name = object_property_get(node, "name");
         if (!list_includes(defineds, name)) {
           la(name);
