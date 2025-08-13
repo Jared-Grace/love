527cb9495500f9afa650264511b69db16a1ccde0
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 13:05:56 -0400

    function_auto_path public\src\js_function_id_add.mjs

--- web/public/src/js_function_id_add.mjs
+++ web/public/src/js_function_id_add.mjs
@@ -1,7 +1,7 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_function_id_add(ast) {
-  js_visit_type(ast, "ArrowFunctionExpression", function lambda(v) {
+  js_visit_type(ast, "FunctionDeclaration", function lambda(v) {
     let { node } = v;
     object_property_set(node, "type", "FunctionDeclaration");
   });
