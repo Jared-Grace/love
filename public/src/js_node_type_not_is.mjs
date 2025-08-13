9aa87200842de866c2f49df20a6ac031e453d219
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 12:46:49 -0400

    function_auto_path public\src\js_node_type_not_is.mjs

--- public/src/js_node_type_not_is.mjs
+++ public/src/js_node_type_not_is.mjs
@@ -1,6 +1,6 @@
-import {js_node_type_is} from './js_node_type_is.mjs';
-export function js_node_type_not_is(node) {
-  let type_is = js_node_type_is(node, "BlockStatement");
+import { js_node_type_is } from "./js_node_type_is.mjs";
+export function js_node_type_not_is(node, type) {
+  let type_is = js_node_type_is(node, type);
   let nti = !type_is;
   return nti;
 }
