f525be7d3f92e3fd26841e8b9c2f65d57eb42738
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 05:52:16 -0400

    function_auto_path public\src\js_imports.mjs

--- public/src/js_imports.mjs
+++ public/src/js_imports.mjs
@@ -7,9 +7,8 @@ import { list_map_property } from "./list_map_property.mjs";
 import { list_unique } from "./list_unique.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 import { js_type } from "./js_type.mjs";
-
 export function js_imports(ast) {
-  js_type      
+  let vs = js_type(ast2, node_type);
   return list_adder_unique((la) => {
     js_visit_type(ast, "ImportDeclaration", (v) => {
       let { node } = v;
