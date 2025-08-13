f8fa195f88eb4603de034a9bccdfc37300d3911c
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:40:18 -0400

    function_auto_path public\src\list_get_end_2.mjs

--- web/public/src/list_get_end_2.mjs
+++ web/public/src/list_get_end_2.mjs
@@ -1,5 +1,5 @@
 import { list_get_end } from "./list_get_end.mjs";
 export function list_get_end_2(stack) {
-  const stack2 = list_get_end(stack, 2);
-  return stack2;
+  const e2 = list_get_end(stack, 2);
+  return e2;
 }
