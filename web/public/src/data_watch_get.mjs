ef12fe1334e7f20d5f2b91298016e6c71bbdce92
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 16:44:48 -0400

    function_auto_path public\src\data_watch_get.mjs

--- web/public/src/data_watch_get.mjs
+++ web/public/src/data_watch_get.mjs
@@ -3,6 +3,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { data_get } from "./data_get.mjs";
 export async function data_watch_get() {
   const property_name = "watch";
-  let f_name_current = await data_property_get(property_name);
-  return f_name_current;
+  let w = await data_property_get(property_name);
+  return w;
 }
