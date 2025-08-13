ca565f7b7c01215f96e399f2abf6d70a51bc705d
Grace-Jared <christrosetolife@gmail.com>
2025-08-12 19:21:39 -0700

    function_auto_path public\src\js_call_new.mjs

--- web/public/src/js_call_new.mjs
+++ web/public/src/js_call_new.mjs
@@ -1,29 +1,32 @@
-import {js_parse_statement} from './js_parse_statement.mjs';
-import {js_code_let_assign} from './js_code_let_assign.mjs';
-import {js_return_name} from './js_return_name.mjs';
-import {js_code_call_args_await_maybe} from './js_code_call_args_await_maybe.mjs';
-import {js_unparse} from './js_unparse.mjs';
-import {js_declaration_param_add} from './js_declaration_param_add.mjs';
-import {each} from './each.mjs';
-import {js_parse_statement_module} from './js_parse_statement_module.mjs';
-import {js_code_declaration} from './js_code_declaration.mjs';
-import {object_property_get} from './object_property_get.mjs';
-import {list_slice} from './list_slice.mjs';
-import {list_size} from './list_size.mjs';
-import {list_first} from './list_first.mjs';
-import {string_split} from './string_split.mjs';
-import {js_identifier_unique} from './js_identifier_unique.mjs';
-import {list_map} from './list_map.mjs';
-import {js_declaration_params_names} from './js_declaration_params_names.mjs';
-import {js_identifiers_names} from './js_identifiers_names.mjs';
-import {marker_next_index} from './marker_next_index.mjs';
-import {function_parse_declaration} from './function_parse_declaration.mjs';
-export async function js_call_new(f_name_call, a) {
-  let {declaration, unaliased, ast: ast_call} = await function_parse_declaration(f_name_call);
-  let { ast,} = marker_next_index(a);
+import { js_parse_statement } from "./js_parse_statement.mjs";
+import { js_code_let_assign } from "./js_code_let_assign.mjs";
+import { js_return_name } from "./js_return_name.mjs";
+import { js_code_call_args_await_maybe } from "./js_code_call_args_await_maybe.mjs";
+import { js_unparse } from "./js_unparse.mjs";
+import { js_declaration_param_add } from "./js_declaration_param_add.mjs";
+import { each } from "./each.mjs";
+import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
+import { js_code_declaration } from "./js_code_declaration.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { list_slice } from "./list_slice.mjs";
+import { list_size } from "./list_size.mjs";
+import { list_first } from "./list_first.mjs";
+import { string_split } from "./string_split.mjs";
+import { js_identifier_unique } from "./js_identifier_unique.mjs";
+import { list_map } from "./list_map.mjs";
+import { js_declaration_params_names } from "./js_declaration_params_names.mjs";
+import { js_identifiers_names } from "./js_identifiers_names.mjs";
+import { marker_next_index } from "./marker_next_index.mjs";
+import { function_parse_declaration } from "./function_parse_declaration.mjs";
+export async function js_call_new(f_name_call, ast) {
+  let {
+    declaration,
+    unaliased,
+    ast: ast_call,
+  } = await function_parse_declaration(f_name_call);
   let existing = js_identifiers_names(ast);
   let arg_names = js_declaration_params_names(declaration);
-  let args_code = list_map(arg_names, arg_name => {
+  let args_code = list_map(arg_names, (arg_name) => {
     let arg_code = js_identifier_unique(existing, arg_name);
     let split = string_split(arg_name, "$");
     const lambda = "lambda";
@@ -35,7 +38,7 @@ export async function js_call_new(f_name_call, a) {
       let async_is = object_property_get(declaration, "async");
       let code = js_code_declaration(lamda_name, "", async_is);
       let declaration_lambda = js_parse_statement_module(code);
-      each(remaining, p => {
+      each(remaining, (p) => {
         let unique = js_identifier_unique(existing, p);
         js_declaration_param_add(declaration_lambda, unique);
       });
