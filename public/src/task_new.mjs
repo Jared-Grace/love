18774a7493248ae993b3cea2b3bb1b2d52a8b06e
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 06:37:23 -0400

    function_auto_path public\src\task_new.mjs

--- public/src/task_new.mjs
+++ public/src/task_new.mjs
@@ -1,3 +1,5 @@
+import { js_declaration_to_block_body } from "./js_declaration_to_block_body.mjs";
+import { js_call_insert } from "./js_call_insert.mjs";
 import { error } from "./error.mjs";
 import { js_call_new } from "./js_call_new.mjs";
 import { function_transform } from "./function_transform.mjs";
@@ -9,6 +11,7 @@ export async function task_new(task_name) {
   let f_name_task = function_name_combine(f_name, task_name);
   await function_new(f_name_task);
   let result = await function_transform(f_name, async function lambda(ast) {
-    let parsed = await js_call_new(error.name, ast);
+    js_call_insert(f_name, args_code, list, index);
+    js_declaration_to_block_body();
   });
 }
