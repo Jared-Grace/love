05a47542f073eb7836296036aa973a04484caf9d
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 13:47:35 -0400

    function_auto_path public\src\js_atomize_function.mjs

--- public/src/js_atomize_function.mjs
+++ public/src/js_atomize_function.mjs
@@ -45,7 +45,8 @@ export async function js_atomize_function(ast) {
         let name = js_declaration_name(node);
         let copy = object_copy(node);
         js_block_insert(stack, copy);
-        object_replace(to, from);
+        let expression = js_parse_expression(init_code);
+        object_replace(node);
       }
     }
   });
