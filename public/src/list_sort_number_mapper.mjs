86982a91cbbf17fe197302438db0c4f8bdd961a1
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 17:27:58 -0400

    function_auto_path public\src\list_sort_number_mapper.mjs

--- public/src/list_sort_number_mapper.mjs
+++ public/src/list_sort_number_mapper.mjs
@@ -1,3 +1,6 @@
-export function list_sort_number_mapper(types) {
-  types.sort();
+export function list_sort_number_mapper(list) {
+  function lambda(a, b) {
+    return a.length - b.length;
+  }
+  arr.sort(lambda);
 }
