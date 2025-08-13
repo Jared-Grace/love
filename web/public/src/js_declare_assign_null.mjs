1961c67d64c2239eb4a8a449d9f33a0d19807083
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 07:02:54 -0400

    function_auto_path public\src\js_declare_assign_null.mjs

--- web/public/src/js_declare_assign_null.mjs
+++ web/public/src/js_declare_assign_null.mjs
@@ -1,3 +1,4 @@
+import { js_keyword_null } from "./js_keyword_null.mjs";
 import { log } from "./log.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_set } from "./object_property_set.mjs";
@@ -9,7 +10,8 @@ export function js_declare_assign_null(ast) {
     let { node } = v;
     let { init } = node;
     if (init === null) {
-      let value = js_parse_expression("null");
+      let init_code = js_keyword_null();
+      let value = js_parse_expression(init_code);
       object_property_set(node, "init", value);
     }
     log(node);
