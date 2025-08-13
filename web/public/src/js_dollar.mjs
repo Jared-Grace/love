9d9a29eaf37b868936f37982b3d7d03c855971ca
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 12:52:02 -0400

    function_auto_path public\src\js_dollar.mjs

--- web/public/src/js_dollar.mjs
+++ web/public/src/js_dollar.mjs
@@ -1,3 +1,4 @@
+import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { js_identifier_unique } from "./js_identifier_unique.mjs";
 import { js_declare } from "./js_declare.mjs";
 import { js_code_string } from "./js_code_string.mjs";
@@ -68,6 +69,7 @@ export async function js_dollar(ast) {
       let parsed = js_parse_expression(code);
       object_replace(node, parsed);
       if (js_node_type_is(stack1, "ExpressionStatement")) {
+        let result = js_identifiers_names(ast2);
         let unique = js_identifier_unique(existing, property_name);
         let assign = js_declare(unique, init);
       }
