c87a111d3cb1b6434302a87c9f1de673cdac3ccf
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:47:11 -0400

    fw c function_copy_suffix_add

---
+++ web/public/src/function_copy_suffix_add.mjs
@@ -0,0 +1,4 @@
+import { function_copy } from "./function_copy.mjs";
+export async function function_copy_suffix_add(f_name_old, f_name_new) {
+  return await function_copy(f_name_old, f_name_new);
+}
