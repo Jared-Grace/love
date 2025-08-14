c82e0528738e7bd7f6a169a356dd7b3eb9aff37f
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:23:11 -0400

    function_auto_path public\src\git_push_schedule.mjs

--- web/public/src/git_push_schedule.mjs
+++ web/public/src/git_push_schedule.mjs
@@ -1,5 +1,6 @@
 import { path_resolve } from "./path_resolve.mjs";
 export function git_push_schedule() {
-  path_resolve(paths);
-  ('schtasks /create /sc daily /st 08:00 /tn "GitPushDaily" /tr "cmd /c cd /d C:\\Users\\chris\\love && git push"');
+  path_resolve(
+    'schtasks /create /sc daily /st 08:00 /tn "GitPushDaily" /tr "cmd /c cd /d C:\\Users\\chris\\love && git push"',
+  );
 }
