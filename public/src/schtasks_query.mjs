b543505e9cdf26b4739f15dced034a1a261548cf
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:56:46 -0400

    function_auto_path public\src\schtasks_query.mjs

--- public/src/schtasks_query.mjs
+++ public/src/schtasks_query.mjs
@@ -1,6 +1,9 @@
 import { throws_not_async } from "./throws_not_async.mjs";
 import { command_line } from "./command_line.mjs";
 export async function schtasks_query(name) {
-  async function lambda2() {}
+  async function lambda2() {
+    let command = `schtasks /query /tn "${name}" >nul 2>&1`;
+    let stdout = await command_line(command);
+  }
   let success = await throws_not_async(lambda2);
 }
