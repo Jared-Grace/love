44609c43cd5bafeb9c663ddbb2d54de00cf840e4
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:59:42 -0400

    function_auto_path public\src\js_declaration_params_names_node.mjs

--- public/src/js_declaration_params_names_node.mjs
+++ public/src/js_declaration_params_names_node.mjs
@@ -2,6 +2,7 @@ import { each } from "./each.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
+import { list_map } from "./list_map.mjs";
 export function js_declaration_params_names_node(node) {
   let names = null;
   let ii = js_identifier_is(node);
@@ -12,7 +13,7 @@ export function js_declaration_params_names_node(node) {
     let type_is = js_node_type_is(node, "ObjectPattern");
     if (type_is) {
       let { properties } = node;
-      each(properties, js_declaration_params_names_node);
+      let mapped = list_map(properties, js_declaration_params_names_node);
     }
   }
   return names;
