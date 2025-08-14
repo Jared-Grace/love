dcd64fab822577ac43bdb852a7a3580b9f87b1da
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 13:03:47 -0400

    function_auto_path public\src\function_auto.mjs

---
+++ public/src/js_auto.mjs
@@ -0,0 +1,18 @@
+import {each_async} from './each_async.mjs';
+import {js_atomize} from './js_atomize.mjs';
+import {js_outside_move} from './js_outside_move.mjs';
+import {js_dollar} from './js_dollar.mjs';
+import {js_call_fill} from './js_call_fill.mjs';
+import {js_atomize_function} from './js_atomize_function.mjs';
+import {js_declare_assign_null} from './js_declare_assign_null.mjs';
+import {js_let_add} from './js_let_add.mjs';
+import {js_function_id_add} from './js_function_id_add.mjs';
+import {js_arrow_to_function} from './js_arrow_to_function.mjs';
+import {js_arrow_blockify} from './js_arrow_blockify.mjs';
+export async function js_auto(ast) {
+  let transforms = [js_arrow_blockify, js_arrow_to_function, js_function_id_add, js_let_add, js_declare_assign_null, js_atomize_function, js_call_fill, js_dollar, js_outside_move, js_atomize];
+  async function lambda(t) {
+    await t(ast);
+  }
+  await each_async(transforms, lambda);
+}
