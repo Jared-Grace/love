4905a6abc7755e3085ec021d28a8b78038b995b1
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:10:46 -0400

    function_auto_path public\src\list_to_dictionary.mjs

--- web/public/src/list_to_dictionary.mjs
+++ web/public/src/list_to_dictionary.mjs
@@ -1,5 +1,6 @@
 import { each } from "./each.mjs";
 export function list_to_dictionary(list) {
   let dictionary = {};
-  each(list2, function lambda(item) {});
+  function lambda(item) {}
+  each(list2, lambda);
 }
