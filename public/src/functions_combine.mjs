56289b56277526a6ebeb020802ee3c9fb0a3f47b
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 22:20:33 -0400

    function_auto_path public\src\functions_combine.mjs

--- public/src/functions_combine.mjs
+++ public/src/functions_combine.mjs
@@ -1,4 +1,5 @@
+import { string_split_comma } from "./string_split_comma.mjs";
 import { string_split } from "./string_split.mjs";
 export function functions_combine(f_names) {
-  let split = string_split(f_names, ",");
+  let split = string_split_comma(f_names);
 }
