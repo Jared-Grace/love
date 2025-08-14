7d7e043ae5f250cade8042a14d5a571b663db2ea
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:34:30 -0400

    function_auto_path public\src\js_declaration_params_names.mjs

--- public/src/js_declaration_params_names.mjs
+++ public/src/js_declaration_params_names.mjs
@@ -1,3 +1,4 @@
+import { each } from "./each.mjs";
 import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -6,4 +7,5 @@ export function js_declaration_params_names(declaration) {
   let params = js_declaration_params_get(declaration);
   const params_names = js_identifiers_to_names(params);
   return params_names;
+  each(list, function lambda(item) {});
 }
