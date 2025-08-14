455488bfc34b96f48f99ba553966efa39973adea
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 14:28:31 -0400

    function_auto_path public\src\function_name_to_parts.mjs

--- public/src/function_name_to_parts.mjs
+++ public/src/function_name_to_parts.mjs
@@ -1,7 +1,7 @@
 import { string_split } from "./string_split.mjs";
 import { function_name_separator } from "./function_name_separator.mjs";
-export function function_name_to_parts(s) {
+export function function_name_to_parts(f_name) {
   let separator = function_name_separator();
-  let parts = string_split(s, separator);
+  let parts = string_split(f_name, separator);
   return parts;
 }
