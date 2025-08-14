d730f58ba336aba6955aec26386098becfedf6e7
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:33:56 -0400

    function_auto_path public\src\functions_combine.mjs

---
+++ web/public/src/functions_combine_name.mjs
@@ -0,0 +1,13 @@
+import {function_name_combine_multiple} from './function_name_combine_multiple.mjs';
+import {function_name_unalias} from './function_name_unalias.mjs';
+import {list_map_unordered_async} from './list_map_unordered_async.mjs';
+import {string_split_comma} from './string_split_comma.mjs';
+export async function functions_combine_name(f_names_comma) {
+  let split = string_split_comma(f_names_comma);
+  let f_names = await list_map_unordered_async(split, function_name_unalias);
+  let combined = function_name_combine_multiple(f_names);
+  return {
+    f_names,
+    combined
+  };
+}
