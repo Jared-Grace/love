b53215da79d1617f7344b2570a480fe14a436aea
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:54:58 -0400

    function_auto_path public\src\functions_combine.mjs

--- public/src/functions_combine.mjs
+++ public/src/functions_combine.mjs
@@ -1,3 +1,4 @@
+import { js_visit_match } from "./js_visit_match.mjs";
 import { functions_combine_name } from "./functions_combine_name.mjs";
 import { js_function_last_asyncify } from "./js_function_last_asyncify.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -23,6 +24,7 @@ export async function functions_combine(f_names_comma) {
       let code = js_code_call_args_await_maybe(unaliased, [], declaration);
       let statement = js_parse_statement(code);
       let async_is = object_property_get(declaration, "async");
+      let v_match = js_visit_match(ast2, search);
       js_function_last_asyncify(stack, async_is);
       list_add(body_block, statement);
     }
