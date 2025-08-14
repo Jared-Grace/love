011f15a24b3ad554751bbc0260b8894ed3218174
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 14:44:20 -0400

    function_auto_path public\src\function_name_to_acronym.mjs

---
+++ public/src/list_join_empty.mjs
@@ -0,0 +1,7 @@
+import {list_join} from './list_join.mjs';
+import {string_empty} from './string_empty.mjs';
+export function list_join_empty(letters) {
+  const e = string_empty();
+  let joined = list_join(letters, e);
+  return joined;
+}
