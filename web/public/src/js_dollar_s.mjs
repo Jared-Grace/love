0a3ca06ec454b499b8d8055186dd288e51e09366
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:40:59 -0400

    function_auto_path public\src\js_dollar_s.mjs

--- web/public/src/js_dollar_s.mjs
+++ web/public/src/js_dollar_s.mjs
@@ -1,1 +1,29 @@
-export function js_dollar_s(ast) {}
+import { each } from "./each.mjs";
+import { object_replace } from "./object_replace.mjs";
+import { object_copy } from "./object_copy.mjs";
+import { js_identifiers_named } from "./js_identifiers_named.mjs";
+import { list_remove_multiple } from "./list_remove_multiple.mjs";
+import { js_dollar_a_generic } from "./js_dollar_a_generic.mjs";
+export function js_dollar_s({ stack1, stack2, ast, afters }) {
+  js_dollar_a_generic({
+    stack1,
+    stack2,
+    afters,
+    lambda,
+    ast,
+  });
+  function lambda({ stack1, next, stack2, ast, declarations }) {
+    list_remove_multiple([stack1, next], stack2);
+    function lambda2(declaration) {
+      let { id, init } = declaration;
+      let { name } = id;
+      let is = js_identifiers_named(ast, name);
+      function lambda3(item) {
+        let replacement = object_copy(init);
+        object_replace(item, replacement);
+      }
+      each(is, lambda3);
+    }
+    each(declarations, lambda2);
+  }
+}
