e996e10c43ded61480c0a0b60ef44f642fc63f3c
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 22:38:24 -0400

    function_auto_path public\src\functions_combine.mjs

--- public/src/functions_combine.mjs
+++ public/src/functions_combine.mjs
@@ -4,7 +4,7 @@ import { string_split_comma } from "./string_split_comma.mjs";
 import { string_split } from "./string_split.mjs";
 export async function functions_combine(f_names) {
   let split = string_split_comma(f_names);
-  let combined = function_name_combine_multiple(parts);
+  let combined = function_name_combine_multiple(split);
   async function lambda2() {}
   await function_new_transform(combined, lambda2);
 }
