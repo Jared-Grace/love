1fe10783472bbe5ef2cccc04e8931e9830f8e061
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 18:14:56 -0400

    function_auto_path public\src\list_adder_unique_async.mjs

--- web/public/src/list_adder_unique_async.mjs
+++ web/public/src/list_adder_unique_async.mjs
@@ -1,6 +1,7 @@
 import { list_adder } from "./list_adder.mjs";
+import { list_adder_async } from "./list_adder_async.mjs";
 import { list_unique } from "./list_unique.mjs";
-export function list_adder_unique_async(lambda) {
-  let items = list_adder(lambda);
+export async function list_adder_unique_async(lambda) {
+  let items = await list_adder_async(lambda);
   return list_unique(items);
 }
