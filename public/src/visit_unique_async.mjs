74123b908f00ced3192a18c7cc47b9c4b06ebbf5
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 18:09:02 -0400

    function_auto_path public\src\visit_unique_async.mjs

--- public/src/visit_unique_async.mjs
+++ public/src/visit_unique_async.mjs
@@ -3,16 +3,16 @@ import { list_difference } from "./list_difference.mjs";
 import { visit_async } from "./visit_async.mjs";
 export async function visit_unique_async(node, children_get, on_each) {
   let found = [];
-  await visit_async(
-    node,
-    async (node) => {
-      let children = await children_get(node);
-      return list_difference(children, found);
-    },
-    async (v) => {
-      let { node } = v;
-      list_add(found, node);
-      await on_each(v);
-    },
-  );
+  async function lambda(node) {
+    let children = await children_get(node);
+    return list_difference(children, found);
+  }
+  async function lambda2(v) {
+    let { node } = v;
+    if (false) {
+    }
+    list_add(found, node);
+    await on_each(v);
+  }
+  await visit_async(node, lambda, lambda2);
 }
