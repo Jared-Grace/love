5ffd6150eeb3d54b06ebbd716e0debd99b27e142
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 22:19:46 -0400

    function_auto_path public\src\functions_combine.mjs

--- public/src/functions_combine.mjs
+++ public/src/functions_combine.mjs
@@ -1,4 +1,4 @@
 import { string_split } from "./string_split.mjs";
 export function functions_combine(f_names) {
-  let split = string_split(s, separator);
+  let split = string_split(f_names, ",");
 }
