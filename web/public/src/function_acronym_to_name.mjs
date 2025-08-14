c54dff0774fa2bf3fb212344c54d749e15ecf698
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:31:05 -0400

    function_auto_path public\src\function_alias_add_generic.mjs

---
+++ web/public/src/function_acronym_to_name.mjs
@@ -0,0 +1,17 @@
+import {list_single} from './list_single.mjs';
+import {list_size_1} from './list_size_1.mjs';
+import {object_property_get} from './object_property_get.mjs';
+import {object_property_exists} from './object_property_exists.mjs';
+import {function_names_to_acronyms} from './function_names_to_acronyms.mjs';
+export function function_acronym_to_name(alias, unaliased) {
+  let acronyms = function_names_to_acronyms();
+  const exists2 = object_property_exists(acronyms, alias);
+  if (exists2) {
+    let list = object_property_get(acronyms, alias);
+    let s1 = list_size_1(list);
+    if (s1) {
+      unaliased = list_single(list);
+    }
+  }
+  return unaliased;
+}
