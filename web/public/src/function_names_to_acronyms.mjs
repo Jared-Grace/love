0febb6453e2c09e6e9bf8788dc8fd7a39656b80c
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:41:26 -0400

    function_auto_path public\src\function_names_to_acronyms.mjs

--- web/public/src/function_names_to_acronyms.mjs
+++ web/public/src/function_names_to_acronyms.mjs
@@ -2,5 +2,6 @@ import { list_to_dictionary } from "./list_to_dictionary.mjs";
 import { functions_names } from "./functions_names.mjs";
 export function function_names_to_acronyms() {
   let f_names = functions_names();
-  let dictionary = list_to_dictionary(list, function lambda(item) {});
+  function lambda(item) {}
+  let dictionary = list_to_dictionary(list, lambda);
 }
