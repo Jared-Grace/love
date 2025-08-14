cbb826ca5b4b60942470c2856044e5c0e3871057
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:42:35 -0400

    function_auto_path public\src\js_dollar_s.mjs

--- web/public/src/js_dollar_s.mjs
+++ web/public/src/js_dollar_s.mjs
@@ -15,7 +15,7 @@ export function js_dollar_s({ stack1, stack2, ast, afters }) {
     ast,
   });
   function lambda({ stack1, next, stack2, ast, declarations }) {
-    let s1 = list_size_1(list);
+    let s1 = list_size_1(declarations);
     assert(s1);
     list_remove_multiple([stack1, next], stack2);
     function lambda2(declaration) {
