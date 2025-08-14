ac849201a706f45b9a231a1be20d5395f1dd4901
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:13:27 -0400

    function_auto_path public\src\command_line_schtasks.mjs

--- public/src/command_line_schtasks.mjs
+++ public/src/command_line_schtasks.mjs
@@ -1,5 +1,6 @@
-import {command_line} from './command_line.mjs';
+import { command_line } from "./command_line.mjs";
 export async function command_line_schtasks(command) {
   let prefixed = `schtasks ` + command;
   let stdout = await command_line(prefixed);
+  return;
 }
