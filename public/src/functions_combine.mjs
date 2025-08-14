1aa8d56f67ebff16e9a6a0c27e07c43a18dd9f66
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 13:00:59 -0400

    function_auto_path public\src\functions_combine.mjs

--- public/src/functions_combine.mjs
+++ public/src/functions_combine.mjs
@@ -15,19 +15,14 @@ import { function_name_combine_multiple } from "./function_name_combine_multiple
 import { function_new_transform } from "./function_new_transform.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 import { string_split } from "./string_split.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 export async function functions_combine(f_names_comma) {
   let { f_names, combined } = await functions_combine_name(f_names_comma);
   async function lambda2(ast) {
     let body_block = js_declaration_single_block_blody(ast);
     async function lambda(f_name) {
-      let { declaration, unaliased } = await function_parse_declaration(f_name);
-      let code = js_code_call_args_await_maybe(unaliased, [], declaration);
-      let statement = js_parse_statement(code);
-      let async_is = object_property_get(declaration, "async");
-      let v_match = js_visit_match(ast, body_block);
-      let { stack } = v_match;
-      js_function_last_asyncify(stack, async_is);
-      list_add(body_block, statement);
+      let item = js_parse_expression(f_name);
+      list_add(body_block, item);
     }
     await each_async(f_names, lambda);
   }
