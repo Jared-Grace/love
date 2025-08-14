19cce8d76ac79016cd7b59042a4aafe5cff54e06
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:50:22 -0400

    function_auto_path public\src\functions_combine_test.mjs

--- web/public/src/functions_combine_test.mjs
+++ web/public/src/functions_combine_test.mjs
@@ -1,3 +1,4 @@
+import { list_join_comma } from "./list_join_comma.mjs";
 import { functions_combine } from "./functions_combine.mjs";
 import { function_delete_if_exists } from "./function_delete_if_exists.mjs";
 import { functions_combine_name } from "./functions_combine_name.mjs";
@@ -5,7 +6,7 @@ import { list_map_name } from "./list_map_name.mjs";
 import { list_join } from "./list_join.mjs";
 export async function functions_combine_test(list) {
   let names = list_map_name(list);
-  let f_names_comma = list_join(names, ",");
+  let f_names_comma = list_join_comma(names);
   let { combined } = await functions_combine_name(f_names_comma);
   await function_delete_if_exists(combined);
   await functions_combine(names);
