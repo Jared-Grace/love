13163dc4019a76672dceed35eb729a343bcf7317
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:52:56 -0400

    function_auto_path public\src\js_declaration_params_names_node.mjs

--- web/public/src/js_declaration_params_names_node.mjs
+++ web/public/src/js_declaration_params_names_node.mjs
@@ -1,10 +1,10 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
 export function js_declaration_params_names_node(node) {
+  let names = null;
   let ii = js_identifier_is(node);
   if (ii) {
     let name = object_property_get(node, "name");
-    let names = null;
     names = [name];
   }
   return names;
