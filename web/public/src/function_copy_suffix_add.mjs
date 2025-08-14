dc1dc58a6bf845f40c2962c4a20053131ddf7ee6
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:48:13 -0400

    function_auto_path public\src\function_copy_suffix_add.mjs

--- web/public/src/function_copy_suffix_add.mjs
+++ web/public/src/function_copy_suffix_add.mjs
@@ -1,6 +1,6 @@
 import { function_name_combine } from "./function_name_combine.mjs";
 import { function_copy } from "./function_copy.mjs";
 export async function function_copy_suffix_add(f_name_old, suffix) {
-  let combined = function_name_combine(left, right);
-  return await function_copy(f_name_old, f_name_new);
+  let combined = function_name_combine(f_name_old, suffix);
+  return await function_copy(f_name_old, combined);
 }
