2f87123ff3b1a37991fd148cc7a9a43f6ade8530
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:50:26 -0400

    function_auto_path public\src\js_dollar_s.mjs

--- public/src/js_dollar_s.mjs
+++ public/src/js_dollar_s.mjs
@@ -27,6 +27,7 @@ export function js_dollar_s({ stack1, stack2, ast, afters }) {
       let code = js_code_assign("a", "a");
       let expression = js_parse_expression(code);
       log(expression);
+      return;
       let { id, init } = declaration;
       let { name } = id;
       let is = js_identifiers_named(ast, name);
