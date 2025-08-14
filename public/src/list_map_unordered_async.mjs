9fdd049e89a2f5e45d54c497a14a3b10c4e672b4
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 09:34:24 -0400

    function_auto_path public\src\list_map_unordered_async.mjs

--- public/src/list_map_unordered_async.mjs
+++ public/src/list_map_unordered_async.mjs
@@ -1,8 +1,8 @@
 import { list_wait } from "./list_wait.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 import { list_map } from "./list_map.mjs";
-export function list_map_unordered_async(mapped2, filter) {
-  let mapped3 = list_map(mapped2, filter);
-  let waited2 = list_wait(mapped3);
-  return waited2;
+export function list_map_unordered_async(list, lambda$item) {
+  let mapped = list_map(list, lambda$item);
+  let waited = list_wait(mapped);
+  return waited;
 }
