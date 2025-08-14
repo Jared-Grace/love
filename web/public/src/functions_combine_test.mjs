273614f4de7da0b64c584f4c1419c5411271cb21
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:48:33 -0400

    function_auto_path public\src\sandbox.mjs

---
+++ web/public/src/functions_combine_test.mjs
@@ -0,0 +1,10 @@
+import {functions_combine} from './functions_combine.mjs';
+import {function_delete_if_exists} from './function_delete_if_exists.mjs';
+import {functions_combine_name} from './functions_combine_name.mjs';
+import {list_map_name} from './list_map_name.mjs';
+export async function functions_combine_test(list) {
+  let f_names_comma = list_map_name(list);
+  let {combined} = await functions_combine_name(f_names_comma);
+  await function_delete_if_exists(combined);
+  await functions_combine(f_names_comma);
+}
