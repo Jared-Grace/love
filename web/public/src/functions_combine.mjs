653978245a59b241901e3b92be16b44f6a7068f2
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 09:50:36 -0400

    function_auto_path public\src\functions_combine.mjs

--- web/public/src/functions_combine.mjs
+++ web/public/src/functions_combine.mjs
@@ -1,4 +1,3 @@
-import { function_wrap } from "./function_wrap.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { list_map } from "./list_map.mjs";
@@ -16,12 +15,11 @@ export async function functions_combine(f_names) {
   let split = string_split_comma(f_names);
   let waited = await list_map_unordered_async(split, function_name_unalias);
   let combined = function_name_combine_multiple(waited);
-  await function_wrap(f_name2, f_name_wrapped);
   async function lambda2(ast) {
     let body_block = js_declaration_single_block_blody(ast);
     async function lambda(f_name) {
       let { declaration, unaliased } = await function_parse_declaration(f_name);
-      let code = js_code_call_args_await_maybe(unaliased, "", declaration);
+      let code = js_code_call_args_await_maybe(unaliased, [], declaration);
       let statement = js_parse_statement(code);
       list_add(body_block, statement);
     }
