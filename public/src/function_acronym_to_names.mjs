0109b99d5db58d93bc973988288ebe67c44de4a9
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:03:44 -0400

    function_auto_path public\src\function_acronym_to_names.mjs

--- public/src/function_acronym_to_names.mjs
+++ public/src/function_acronym_to_names.mjs
@@ -2,5 +2,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { function_names_to_acronyms } from "./function_names_to_acronyms.mjs";
 export function function_acronym_to_names(acronym) {
   let acronyms = function_names_to_acronyms();
-  let a = object_property_get(acronyms, "a");
+  let list = object_property_get(acronyms, acronym);
+  return;
 }
