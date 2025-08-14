b744ef9e0b971be9410be2d604ebf6e030d7baff
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 18:10:38 -0400

    function_auto_path public\src\visit_unique_async.mjs

---
+++ web/public/src/list_add_if_not_includes.mjs
@@ -0,0 +1,8 @@
+import {list_add} from './list_add.mjs';
+import {list_includes} from './list_includes.mjs';
+export function list_add_if_not_includes(found, node) {
+  let includes = list_includes(found, node);
+  if (!includes) {
+    list_add(found, node);
+  }
+}
