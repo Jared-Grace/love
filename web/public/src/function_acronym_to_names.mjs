a014555468f9eb1a105df25baddc283459e311f2
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:02:21 -0400

    function_auto_path public\src\function_acronym_to_names.mjs

--- web/public/src/function_acronym_to_names.mjs
+++ web/public/src/function_acronym_to_names.mjs
@@ -1,4 +1,6 @@
 import { function_names_to_acronyms } from "./function_names_to_acronyms.mjs";
-export function function_acronym_to_names() {
+export function function_acronym_to_names(acronym) {
   let acronyms = function_names_to_acronyms();
+  if (false) {
+  }
 }
