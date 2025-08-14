1f10bfa22a47f53cbe5c736a8bbcb0423f049ae0
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:51:26 -0400

    function_auto_path public\src\function_alias_add_generic.mjs

--- web/public/src/function_alias_add_generic.mjs
+++ web/public/src/function_alias_add_generic.mjs
@@ -4,9 +4,9 @@ import { object_property_exists } from "./object_property_exists.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function function_alias_add_generic(alias) {
+  let unaliased = null;
   var { value: aliases, file_path, data } = await data_get("aliases", {});
   const exists = object_property_exists(aliases, alias);
-  let unaliased = null;
   if (exists) {
     unaliased = object_property_get(aliases, alias);
   }
