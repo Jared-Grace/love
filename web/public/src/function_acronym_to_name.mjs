262ef56e450cbd0e23cfd162211cc44112b8a32b
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:31:24 -0400

    function_auto_path public\src\function_acronym_to_name.mjs

--- web/public/src/function_acronym_to_name.mjs
+++ web/public/src/function_acronym_to_name.mjs
@@ -1,9 +1,10 @@
-import {list_single} from './list_single.mjs';
-import {list_size_1} from './list_size_1.mjs';
-import {object_property_get} from './object_property_get.mjs';
-import {object_property_exists} from './object_property_exists.mjs';
-import {function_names_to_acronyms} from './function_names_to_acronyms.mjs';
-export function function_acronym_to_name(alias, unaliased) {
+import { list_single } from "./list_single.mjs";
+import { list_size_1 } from "./list_size_1.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+import { function_names_to_acronyms } from "./function_names_to_acronyms.mjs";
+export function function_acronym_to_name(alias) {
+  let unaliased = null;
   let acronyms = function_names_to_acronyms();
   const exists2 = object_property_exists(acronyms, alias);
   if (exists2) {
