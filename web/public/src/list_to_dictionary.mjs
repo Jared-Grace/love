142cd4028172c31818f8cfb98b017698cb31c5c7
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:11:36 -0400

    function_auto_path public\src\list_to_dictionary.mjs

--- web/public/src/list_to_dictionary.mjs
+++ web/public/src/list_to_dictionary.mjs
@@ -1,9 +1,11 @@
+import { list_get } from "./list_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
 export function list_to_dictionary(list) {
   let dictionary = {};
   function lambda(item) {
-    object_property_set(object, property_name, value);
+    let result = list_get(list2, index);
+    object_property_set(dictionary, item, value);
   }
   each(list, lambda);
 }
