ddf989e41c7207802049ba0672d66d5f59ad6dcf
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:17:01 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,10 +1,16 @@
+import { task_new } from "./task_new.mjs";
+import { function_current_set } from "./function_current_set.mjs";
+import { functions_combine } from "./functions_combine.mjs";
 import { function_delete_if_exists } from "./function_delete_if_exists.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { function_delete } from "./function_delete.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function sandbox() {
-    const n = "function_params_new";
+  const n = "function_params_new";
   await function_delete_if_exists(n);
   await function_wrap(function_param_new.name, n);
+  await functions_combine(f_names);
+  await function_current_set(f_name);
+  await task_new(task_name);
 }
