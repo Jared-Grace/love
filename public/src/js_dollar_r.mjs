3dcd546a8918387125cd8bf3ed625da0fe3b0114
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:22:30 -0400

    function_auto_path public\src\js_dollar_r.mjs

--- public/src/js_dollar_r.mjs
+++ public/src/js_dollar_r.mjs
@@ -1,3 +1,4 @@
+import { list_second } from "./list_second.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { log } from "./log.mjs";
 import { object_replace } from "./object_replace.mjs";
@@ -7,6 +8,7 @@ export function js_dollar_r({ stack1, stack2 }) {
   log(stack2);
   let type_is = js_node_type_is(stack2, "SequenceExpression");
   if (type_is) {
+    let second = list_second(list);
   }
   let code = js_code_return_empty();
   let from = js_parse_statement(code);
