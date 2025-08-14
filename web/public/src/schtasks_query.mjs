3063023955b083b32c053af7c88c42f494de5acf
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 18:04:08 -0400

    function_auto_path public\src\schtasks_query.mjs

--- web/public/src/schtasks_query.mjs
+++ web/public/src/schtasks_query.mjs
@@ -1,11 +1,11 @@
+import { command_line_schtasks } from "./command_line_schtasks.mjs";
 import { throws_not_async } from "./throws_not_async.mjs";
 import { command_line } from "./command_line.mjs";
 export async function schtasks_query(name) {
   "todo rename query to exists";
   async function lambda2() {
     const command = `/query /tn "${name}" >nul 2>&1`;
-    let prefixed = `schtasks ` + command;
-    let stdout = await command_line(prefixed);
+    await command_line_schtasks(command);
   }
   let success = await throws_not_async(lambda2);
   return success;
