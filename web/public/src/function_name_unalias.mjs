2c99aa3be1834c6f8973cf0bf5bb6f3b3f578052
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:33:38 -0400

    function_auto_path public\src\function_name_unalias.mjs

--- web/public/src/function_name_unalias.mjs
+++ web/public/src/function_name_unalias.mjs
@@ -1,8 +1,9 @@
+import { function_acronym_to_name } from "./function_acronym_to_name.mjs";
 import { function_alias_add_generic } from "./function_alias_add_generic.mjs";
 export async function function_name_unalias(f_name) {
   var { exists, unaliased: unaliased_actual } =
     await function_alias_add_generic(f_name);
-    const unaliased = exists ? unaliased_actual : f_name;
-    return unaliased;
-    let expanded = function_acronym_to_name(alias);
+  const unaliased = exists ? unaliased_actual : f_name;
+  return unaliased;
+  let expanded = function_acronym_to_name(alias);
 }
