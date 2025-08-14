7bcc7ed3da7472ecc563b2d7cc2879f29156560a
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 18:10:38 -0400

    function_auto_path public\src\visit_unique_async.mjs

--- web/public/src/visit_unique_async.mjs
+++ web/public/src/visit_unique_async.mjs
@@ -1,3 +1,4 @@
+import { list_add_if_not_includes } from "./list_add_if_not_includes.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_difference } from "./list_difference.mjs";
@@ -10,10 +11,7 @@ export async function visit_unique_async(node, children_get, on_each) {
   }
   async function lambda2(v) {
     let { node } = v;
-    let includes = list_includes(found, node);
-    if (!includes) {
-      list_add(found, node);
-    }
+    list_add_if_not_includes(found, node);
     await on_each(v);
   }
   await visit_async(node, lambda, lambda2);
