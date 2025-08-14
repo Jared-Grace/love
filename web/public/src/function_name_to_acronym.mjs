154a38ce00c07fc9eb03b493bdd305c1eb48e6d8
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 14:44:20 -0400

    function_auto_path public\src\function_name_to_acronym.mjs

--- web/public/src/function_name_to_acronym.mjs
+++ web/public/src/function_name_to_acronym.mjs
@@ -1,3 +1,4 @@
+import { list_join_empty } from "./list_join_empty.mjs";
 import { string_empty } from "./string_empty.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
@@ -8,7 +9,6 @@ import { list_join } from "./list_join.mjs";
 export function function_name_to_acronym(f_name) {
   let parts = function_name_to_parts(f_name);
   let letters = list_map(parts, list_first);
-  const e = string_empty();
-  let joined = list_join(letters, e);
+  let joined = list_join_empty(letters);
   return joined;
 }
