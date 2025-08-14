d1bb8784133b039ec6cfabebed58b5b3d980e22c
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 13:14:47 -0400

    sb

--- public/src/function_current_set_task_new.mjs
+++ public/src/function_current_set_task_new.mjs
@@ -1,1 +1,6 @@
-export function function_current_set_task_new() {}
+import { task_new } from "./task_new.mjs";
+import { function_current_set } from "./function_current_set.mjs";
+export function function_current_set_task_new() {
+  function_current_set;
+  task_new;
+}
