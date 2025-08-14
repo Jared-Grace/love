9b9a70c92471575103b5c959b26ac9e0b7b9fcec
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:14:06 -0400

    function_auto_path public\src\js_dollar_r.mjs

--- web/public/src/js_dollar_r.mjs
+++ web/public/src/js_dollar_r.mjs
@@ -1,7 +1,9 @@
-import {object_replace} from './object_replace.mjs';
-import {js_parse_statement} from './js_parse_statement.mjs';
-import {js_code_return_empty} from './js_code_return_empty.mjs';
-export function js_dollar_r({stack1}) {
+import { log } from "./log.mjs";
+import { object_replace } from "./object_replace.mjs";
+import { js_parse_statement } from "./js_parse_statement.mjs";
+import { js_code_return_empty } from "./js_code_return_empty.mjs";
+export function js_dollar_r({ stack1, stack2 }) {
+  log(message);
   let code = js_code_return_empty();
   let from = js_parse_statement(code);
   object_replace(stack1, from);
