a021eaaacfee70430d5363b926c83d941666ebdd
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:35:29 -0400

    function_auto_path public\src\function_acronym_to_name.mjs

--- web/public/src/function_acronym_to_name.mjs
+++ web/public/src/function_acronym_to_name.mjs
@@ -3,6 +3,7 @@ import { list_size_1 } from "./list_size_1.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { function_names_to_acronyms } from "./function_names_to_acronyms.mjs";
+import { log } from "./log.mjs";
 export function function_acronym_to_name(alias) {
   let unaliased = null;
   let acronyms = function_names_to_acronyms();
@@ -12,6 +13,8 @@ export function function_acronym_to_name(alias) {
     let s1 = list_size_1(list);
     if (s1) {
       unaliased = list_single(list);
+    } else {
+      log(list);
     }
   }
   return unaliased;
