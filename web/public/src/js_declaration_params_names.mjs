4b1690b47e8eebdef7588438a7246767c9357acb
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 19:27:33 -0400

    function_auto_path public\src\js_declaration_params_names.mjs

--- web/public/src/js_declaration_params_names.mjs
+++ web/public/src/js_declaration_params_names.mjs
@@ -1,4 +1,3 @@
-import { import_install } from "./import_install.mjs";
 import { each } from "./each.mjs";
 import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
@@ -6,9 +5,8 @@ import { object_property_get } from "./object_property_get.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 export function js_declaration_params_names(declaration) {
   let params = js_declaration_params_get(declaration);
-  async function lambda(param) {
+  function lambda(param) {
     if (false) {
-      await import_install(name);
       jis;
     }
   }
