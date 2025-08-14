e0bc6c8429d34c3e5c5c655382d7afad727b66a7
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 18:11:20 -0400

    function_auto_path public\src\list_add_if_not_includes.mjs

--- web/public/src/list_add_if_not_includes.mjs
+++ web/public/src/list_add_if_not_includes.mjs
@@ -1,6 +1,7 @@
-import {list_add} from './list_add.mjs';
-import {list_includes} from './list_includes.mjs';
+import { list_add } from "./list_add.mjs";
+import { list_includes } from "./list_includes.mjs";
 export function list_add_if_not_includes(found, node) {
+  "rename includes to exists";
   let includes = list_includes(found, node);
   if (!includes) {
     list_add(found, node);
