d75fe286159d82d5e6fda5982027891e695328d9
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:20:32 -0400

    function_auto_path public\src\function_delete_if_exists.mjs

--- public/src/function_delete_if_exists.mjs
+++ public/src/function_delete_if_exists.mjs
@@ -2,8 +2,8 @@ import { log } from "./log.mjs";
 import { function_exists } from "./function_exists.mjs";
 import { function_delete } from "./function_delete.mjs";
 import { file_exists } from "./file_exists.mjs";
-export async function function_delete_if_exists(n) {
-  if (await function_exists(n)) {
-    await function_delete(n);
+export async function function_delete_if_exists(f_name) {
+  if (await function_exists(f_name)) {
+    await function_delete(f_name);
   }
 }
