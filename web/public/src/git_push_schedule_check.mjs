f139546b230aef33541b2d585d9e10763c26ca0a
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:57:45 -0400

    function_auto_path public\src\git_push_schedule_check.mjs

--- web/public/src/git_push_schedule_check.mjs
+++ web/public/src/git_push_schedule_check.mjs
@@ -10,5 +10,6 @@ import { marker } from "./marker.mjs";
 export async function git_push_schedule_check() {
   marker("1");
   let name = git_push_schedule_task_name();
+  return;
   await schtasks_query(name);
 }
