7335e66c202197fa9f7f88adec9232a639ff76e6
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 17:59:25 -0400

    function_auto_path public\src\git_push_schedule_command.mjs

--- public/src/git_push_schedule_command.mjs
+++ public/src/git_push_schedule_command.mjs
@@ -1,9 +1,11 @@
+import { git_push_schedule_check } from "./git_push_schedule_check.mjs";
 import { git_push_schedule_task_name } from "./git_push_schedule_task_name.mjs";
 import { git_push_command } from "./git_push_command.mjs";
 import { command_line_git_prefix } from "./command_line_git_prefix.mjs";
 import { path_resolve } from "./path_resolve.mjs";
 import { folder_current } from "./folder_current.mjs";
 export async function git_push_schedule_command() {
+  let result2 = await git_push_schedule_check();
   if (false) {
   }
   let paths = folder_current();
