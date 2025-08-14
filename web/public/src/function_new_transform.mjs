6d5cf19543567caaddb66818fea677adaf26f3e1
Grace-Jared <christrosetolife@gmail.com>
2025-08-13 22:47:13 -0400

    function_auto_path public\src\function_new_transform.mjs

--- web/public/src/function_new_transform.mjs
+++ web/public/src/function_new_transform.mjs
@@ -2,7 +2,7 @@ import { log } from "./log.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_new_transform(f_name, lambda$ast) {
-  log(message);
+  log(combined);
   await function_new(f_name);
   await function_transform(f_name, lambda$ast);
 }
