1ca7aa39597ee14e744fe48a973f043100dd2c21
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 15:12:45 -0400

    function_auto_path public\src\list_to_dictionary.mjs

--- web/public/src/list_to_dictionary.mjs
+++ web/public/src/list_to_dictionary.mjs
@@ -1,10 +1,10 @@
 import { list_get } from "./list_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { each } from "./each.mjs";
-export function list_to_dictionary(list) {
+export function list_to_dictionary(list, lambda$item) {
   let dictionary = {};
   function lambda(item) {
-    let result = list_get(list2, index);
+    let value = lambda$item(list);
     object_property_set(dictionary, item, value);
   }
   each(list, lambda);
