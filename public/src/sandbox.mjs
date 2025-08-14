2a1e7d931a3be07f9f055542e0bf9671b27f7911
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 12:19:10 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_map_property } from "./list_map_property.mjs";
 import { task_new } from "./task_new.mjs";
 import { function_current_set } from "./function_current_set.mjs";
 import { functions_combine } from "./functions_combine.mjs";
@@ -8,5 +9,6 @@ import { function_param_new } from "./function_param_new.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function sandbox() {
   const fns = [function_current_set, task_new];
+  let result = list_map_property(list, property_name);
   await functions_combine(fns);
 }
