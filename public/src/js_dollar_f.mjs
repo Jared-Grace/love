10a0bba8b37166978bb56db8f9e9e7b0888bb57e
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:33:42 -0400

    function_auto_path public\src\js_dollar_f.mjs

--- public/src/js_dollar_f.mjs
+++ public/src/js_dollar_f.mjs
@@ -5,7 +5,7 @@ import { js_code_braces_empty } from "./js_code_braces_empty.mjs";
 import { js_code_parenthesis_open_close } from "./js_code_parenthesis_open_close.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
-export function js_dollar_f({ remaining, node, stack1, ast, afters }) {
+export function js_dollar_f({ node, afters }) {
   const code = js_code_declaration(f_name, "", false);
   let parsed = js_parse_expression(code);
   list_add(afters, lambda);
