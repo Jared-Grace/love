25fcafa7e197a4f266069683320b51f0ab02e37a
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 14:15:15 -0400

    function_auto_path public\src\function_exists.mjs

--- web/public/src/function_exists.mjs
+++ web/public/src/function_exists.mjs
@@ -1,3 +1,4 @@
+import { object_merge } from "./object_merge.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 export async function function_exists(f_name) {
@@ -7,5 +8,6 @@ export async function function_exists(f_name) {
   let v = {
     exists,
   };
+  let to2 = object_merge(to, from);
   return v;
 }
