01c4fc367ec480941c42cd486624458cc1916173
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 11:48:19 -0400

    m

--- web/public/src/file_open.mjs
+++ web/public/src/file_open.mjs
@@ -1,4 +1,6 @@
+import { marker } from "./marker.mjs";
 import { command_line } from "./command_line.mjs";
 export async function file_open(filePath) {
+  marker("1");
   await command_line(`code "${filePath}"`);
 }
