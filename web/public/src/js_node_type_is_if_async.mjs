18817c53db4cba08a1a5edb6de12f444e5f888b7
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:27:29 -0400

    function_auto_path public\src\js_node_type_is_if_async.mjs

--- web/public/src/js_node_type_is_if_async.mjs
+++ web/public/src/js_node_type_is_if_async.mjs
@@ -1,7 +1,7 @@
 import { js_node_type_is } from "./js_node_type_is.mjs";
-export function js_node_type_is_if_async(node, type, lambda) {
+export async function js_node_type_is_if_async(node, type, lambda) {
   let type_is = js_node_type_is(node, type);
   if (type_is) {
-    lambda();
+    await lambda();
   }
 }
