e84aef80179fd4a67f299a8385a97fb9fe2d2602
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:05:39 -0400

    function_auto_path public\src\js_declaration_params_names_node.mjs

--- web/public/src/js_declaration_params_names_node.mjs
+++ web/public/src/js_declaration_params_names_node.mjs
@@ -19,8 +19,14 @@ export function js_declaration_params_names_node(node) {
       let { properties } = node;
       names = list_map_squash(properties, js_declaration_params_names_node);
     } else {
-      let message = json_format_to(node);
-      error(message);
+      let type_is = js_node_type_is(node, "ObjectPattern");
+      if (type_is) {
+        let { properties } = node;
+        names = list_map_squash(properties, js_declaration_params_names_node);
+      } else {
+        let message = json_format_to(node);
+        error(message);
+      }
     }
   }
   return names;
