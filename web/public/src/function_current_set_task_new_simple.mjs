ba42a2cde28fef44b712cf1f4ea1df9b05cdd894
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 07:29:16 -0400

    function_auto_path public\src\function_current_set_task_new_simple.mjs

--- web/public/src/function_current_set_task_new_simple.mjs
+++ web/public/src/function_current_set_task_new_simple.mjs
@@ -1,4 +1,4 @@
 import { function_current_set_task_new } from "./function_current_set_task_new.mjs";
-export async function function_current_set_task_new_simple(f_name, task_name) {
-  return await function_current_set_task_new(f_name, task_name);
+export async function function_current_set_task_new_simple(f_name) {
+  return await function_current_set_task_new(f_name, "simple");
 }
