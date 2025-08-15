93ff8ef6c4918aecff6345f86e2f37039cfa4eea
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:30:11 -0400

    function_auto_path public\src\list_size.mjs

--- public/src/list_size.mjs
+++ public/src/list_size.mjs
@@ -1,6 +1,8 @@
+import { list_is } from "./list_is.mjs";
 import { assert } from "./assert.mjs";
 export function list_size(list) {
   let v = list.length;
   return v;
+  let l = list_is(value);
   assert(b);
 }
