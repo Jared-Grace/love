ba5f82ba067f64284135dc02f71a5dae6eb21b27
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 12:52:12 -0400

    function_auto_path public\src\file_open.mjs

--- web/public/src/file_open.mjs
+++ web/public/src/file_open.mjs
@@ -1,10 +1,11 @@
 import { marker } from "./marker.mjs";
 import { command_line } from "./command_line.mjs";
 import { file_read } from "./file_read.mjs";
+import { log } from "./log.mjs";
 export async function file_open(filePath) {
   let terminal = true;
   if (terminal) {
-    await file_read(filePath);
+    log(await file_read(filePath));
   } else {
     await command_line(`code "${filePath}"`);
   }
