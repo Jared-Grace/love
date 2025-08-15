5e8f493ead97e5741f4ec6df798ab29f0cd4eb04
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 20:45:47 -0400

    function_auto_path public\src\list_squash.mjs

--- web/public/src/list_squash.mjs
+++ web/public/src/list_squash.mjs
@@ -1,8 +1,10 @@
+import { list_is } from "./list_is.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_squash(list) {
   function lambda2(la) {
     function lambda(item) {
+      let l = list_is(value);
       if (false) {
       }
     }
