6f4d8f4acee6f1a20eeacad14a9eb888efb19307
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:56:10 -0400

    function_auto_path public\src\js_if_else_if_combine.mjs

--- public/src/js_if_else_if_combine.mjs
+++ public/src/js_if_else_if_combine.mjs
@@ -1,3 +1,4 @@
+import { js_visit_type_each_async } from "./js_visit_type_each_async.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_left_right_set } from "./js_left_right_set.mjs";
 import { js_code_or } from "./js_code_or.mjs";
@@ -12,8 +13,8 @@ import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { log } from "./log.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
-export function js_if_else_if_combine(ast) {
-  function lambda(v) {
+export async function js_if_else_if_combine(ast) {
+  async function lambda(v) {
     let { node, stack } = v;
     let stack1 = list_get_end_1(stack);
     async function lambda3() {
@@ -30,7 +31,7 @@ export function js_if_else_if_combine(ast) {
         object_property_set(stack1, "alternate", null);
       }
     }
-    js_node_type_is_if_async(stack1, "IfStatement", lambda3);
+    await js_node_type_is_if_async(stack1, "IfStatement", lambda3);
   }
-  js_visit_type(ast, "IfStatement", lambda);
+  await js_visit_type_each_async(ast, "IfStatement", lambda);
 }
