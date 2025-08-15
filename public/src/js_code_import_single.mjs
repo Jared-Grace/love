e963b20ca7eaa7fc594158cf42d3d21cef58ea0c
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 09:25:02 -0400

    function_auto_path public\src\function_imports_add.mjs

---
+++ public/src/js_code_import_single.mjs
@@ -0,0 +1,7 @@
+import {js_keyword_from} from './js_keyword_from.mjs';
+import {js_code_wrap_braces} from './js_code_wrap_braces.mjs';
+import {js_keyword_import} from './js_keyword_import.mjs';
+export function js_code_import_single(import_, from) {
+  let v = js_keyword_import() + " " + js_code_wrap_braces(import_) + " " + js_keyword_from() + " " + from;
+  return v;
+}
