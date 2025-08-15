cf64e0b47444d2e01ae068f81ddb8b9c1e6e7b88
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 09:16:14 -0400

    function_auto_path public\src\js_marker_named_ast.mjs

--- web/public/src/js_marker_named_ast.mjs
+++ web/public/src/js_marker_named_ast.mjs
@@ -1,10 +1,16 @@
-import {list_single} from './list_single.mjs';
-import {js_marker_named} from './js_marker_named.mjs';
-import {list_filter} from './list_filter.mjs';
-import {js_type} from './js_type.mjs';
+import { log } from "./log.mjs";
+import { list_single } from "./list_single.mjs";
+import { js_marker_named } from "./js_marker_named.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { js_type } from "./js_type.mjs";
 export function js_marker_named_ast(ast, m_name_from) {
   let visitors = js_type(ast, "CallExpression");
-  let filtered = list_filter(visitors, v => js_marker_named(v, m_name_from));
+  function lambda(v) {
+    let result = js_marker_named(v, m_name_from);
+    return result;
+  }
+  let filtered = list_filter(visitors, lambda);
+  log(message);
   let marker_v = list_single(filtered);
   return marker_v;
 }
