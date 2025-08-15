4b3b99eef70b07397232858e166528c46b0971ab
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:40:15 -0400

    function_auto_path public\src\file_open.mjs

--- web/public/src/file_open.mjs
+++ web/public/src/file_open.mjs
@@ -9,7 +9,7 @@ export async function file_open(f_path) {
   let terminal = true;
   if (terminal) {
     let ext = function_name_extension();
-    let result = string_ends_with(s, ext);
+    let result = string_ends_with(f_path, ext);
     log(await file_read(f_path));
   } else {
     await file_open_editor(f_path);
