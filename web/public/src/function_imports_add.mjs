aa695b3b1492ae8aa89d68135e82e55b8f19534b
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 09:33:07 -0400

    function_auto_path public\src\function_imports_add.mjs

--- web/public/src/function_imports_add.mjs
+++ web/public/src/function_imports_add.mjs
@@ -1,4 +1,3 @@
-import { folder_current_join } from "folder_current_join.mjs";
 import { folder_current } from "./folder_current.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
@@ -10,6 +9,7 @@ import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { each } from "./each.mjs";
 import { each_async } from "./each_async.mjs";
+import { folder_current_join } from "./folder_current_join.mjs";
 import { path_join } from "./path_join.mjs";
 export async function function_imports_add(ast, imports) {
   let { body } = ast;
