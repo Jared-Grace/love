5ccc82d8a412ca5147b4a21d6998db571110698d
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 10:31:49 -0400

    function_auto_path public\src\function_imports_add.mjs

--- web/public/src/function_imports_add.mjs
+++ web/public/src/function_imports_add.mjs
@@ -1,8 +1,12 @@
+import { each_async } from "./each_async.mjs";
+import { js_code_import_single } from "./js_code_import_single.mjs";
+import { js_code_string } from "./js_code_string.mjs";
+import { folder_current_join } from "./folder_current_join.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { each } from "./each.mjs";
-export function function_imports_add(ast, imports) {
+export async function function_imports_add(ast, imports) {
   let { body } = ast;
   function lambda(import_) {
     let code =
@@ -16,4 +20,17 @@ export function function_imports_add(ast, imports) {
     list_add_first(body, statement);
   }
   each(imports, lambda);
+  return;
+  {
+    let { body } = ast;
+    async function lambda(import_) {
+      let result = function_name_to_base(import_);
+      let value_string = folder_current_join(result);
+      const from = await js_code_string(value_string);
+      let code = js_code_import_single(import_, from);
+      let statement = js_parse_statement_module(code);
+      list_add_first(body, statement);
+    }
+    await each_async(imports, lambda);
+  }
 }
