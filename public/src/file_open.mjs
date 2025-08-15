6e0c0c5d7db5ecaba45e9beeb0cb64d2a6513d7c
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:39:20 -0400

    function_auto_path public\src\file_open.mjs

--- public/src/file_open.mjs
+++ public/src/file_open.mjs
@@ -1,3 +1,4 @@
+import { string_ends_with } from "./string_ends_with.mjs";
 import { file_open_editor } from "./file_open_editor.mjs";
 import { marker } from "./marker.mjs";
 import { command_line } from "./command_line.mjs";
@@ -6,6 +7,7 @@ import { log } from "./log.mjs";
 export async function file_open(filePath) {
   let terminal = true;
   if (terminal) {
+    let result = string_ends_with(s, suffix);
     log(await file_read(filePath));
   } else {
     await file_open_editor(filePath);
