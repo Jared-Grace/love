2e06c7cbcb0d14d61f3317073ede57672a930b6e
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 19:16:36 -0400

    function_auto_path public\src\function_run_log.mjs

--- web/public/src/function_run_log.mjs
+++ web/public/src/function_run_log.mjs
@@ -1,3 +1,4 @@
+import { trinity_green } from "./trinity_green.mjs";
 import { json_format_to } from "./json_format_to.mjs";
 import { log_keep } from "./log_keep.mjs";
 import { log } from "./log.mjs";
@@ -6,7 +7,7 @@ import { json_to } from "./json_to.mjs";
 import { error } from "./error.mjs";
 export async function function_run_log(f_name, args) {
   let result = await function_run(f_name, args);
-  log_keep("🕊️✟❤️‍🔥 ");
+  log_keep(await trinity_green());
   log_keep(result);
   return result;
 }
