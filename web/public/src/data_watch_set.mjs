ec94bc1a0f1f42776ea399d4605f0578f4597914
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:48:20 -0400

    function_auto_path public\src\data_watch_set.mjs

--- web/public/src/data_watch_set.mjs
+++ web/public/src/data_watch_set.mjs
@@ -4,8 +4,10 @@ import { marker_first } from "./marker_first.mjs";
 import { marker_current_set } from "./marker_current_set.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function data_watch_set() {
+  const property_name = "watch";
   function lambda(previous) {
-    return true;
+    let v = true;
+    return v;
   }
-  await data_transform("watch", null, lambda);
+  await data_transform(property_name, null, lambda);
 }
