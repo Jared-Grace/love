26919ec89c222dbe700bffed03f378c8e0811a74
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 07:33:03 -0400

    function_auto_path public\src\js_calls_to_each.mjs

--- web/public/src/js_calls_to_each.mjs
+++ web/public/src/js_calls_to_each.mjs
@@ -2,5 +2,6 @@ import { js_visit_type } from "./js_visit_type.mjs";
 export function js_calls_to_each(ast) {
   "multiple calls line after line can be changed into each";
   let call_name = null;
-  js_visit_type(ast2, type, function lambda(v) {});
+  function lambda(v) {}
+  js_visit_type(ast2, type, lambda);
 }
