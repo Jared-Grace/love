6b21a3986500daa8811ee4739bd4062175e1b5f9
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 14:24:51 -0400

    m

--- web/public/src/data_terminal_set.mjs
+++ web/public/src/data_terminal_set.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { data_boolean_set } from "./data_boolean_set.mjs";
 import { function_exists } from "./function_exists.mjs";
 import { assert } from "./assert.mjs";
@@ -5,6 +6,7 @@ import { marker_first } from "./marker_first.mjs";
 import { marker_current_set } from "./marker_current_set.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function data_terminal_set(v) {
+  marker("1");
   const property_name = "watch";
   await data_boolean_set(property_name, v);
 }
