a39300a2fe64d295c2517c8401070c0ff8f16f1e
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 14:13:38 -0400

    function_auto_path public\src\function_current_set.mjs

--- web/public/src/function_current_set.mjs
+++ web/public/src/function_current_set.mjs
@@ -4,8 +4,12 @@ import { marker_first } from "./marker_first.mjs";
 import { marker_current_set } from "./marker_current_set.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function function_current_set(f_name) {
-  assert(await function_exists(f_name));
-  await data_transform("function_current", null, (previous) => f_name);
+  const exists = await function_exists(f_name);
+  assert(exists);
+  function lambda(previous) {
+    return f_name;
+  }
+  await data_transform("function_current", null, lambda);
   let name = marker_first();
   await marker_current_set(name);
 }
