92492fb7dcb6de8c25a97b775b66d25663efb80c
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 20:03:48 -0400

    function_auto_path public\src\list_squash.mjs

--- web/public/src/list_squash.mjs
+++ web/public/src/list_squash.mjs
@@ -2,7 +2,10 @@ import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function list_squash(list) {
   function lambda2(la) {
-    function lambda(item) {}
+    function lambda(item) {
+      if (false) {
+      }
+    }
     each(list2, lambda);
   }
   let squashed = list_adder(lambda2);
