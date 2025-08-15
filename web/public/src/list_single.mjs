5e872792edb173b5c75df71410e91bd93a42734b
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:20:56 -0400

    function_auto_path public\src\list_single.mjs

--- web/public/src/list_single.mjs
+++ web/public/src/list_single.mjs
@@ -1,10 +1,10 @@
+import { list_size_1_assert } from "./list_size_1_assert.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_first } from "./list_first.mjs";
 import { error } from "./error.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 export function list_single(list) {
-  if (!list_size_1(list)) {
-    error();
-  }
-  return list_first(list);
+  list_size_1_assert(list);
+  let first = list_first(list);
+  return first;
 }
