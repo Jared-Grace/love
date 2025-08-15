56077881a62cc8a243eb3500b135f4a91aa1f391
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 12:54:49 -0400

    function_auto_path public\src\function_open_editor.mjs

--- web/public/src/function_open_editor.mjs
+++ web/public/src/function_open_editor.mjs
@@ -6,8 +6,9 @@ import { function_name_to_path_unalias } from "./function_name_to_path_unalias.m
 import { file_open } from "./file_open.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { object_property_set } from "./object_property_set.mjs";
+import { file_open_editor } from "./file_open_editor.mjs";
 export async function function_open_editor(f_name) {
   const { f_path } = await function_name_to_path_unalias(f_name);
-  await file_open(f_path);
+  await file_open_editor(f_path);
   await function_current_set(f_name);
 }
