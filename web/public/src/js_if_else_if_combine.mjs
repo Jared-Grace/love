c3a592c3e05e82a97aa470684d520a97ba3bc9f9
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:22:52 -0400

    function_auto_path public\src\js_if_else_if_combine.mjs

--- web/public/src/js_if_else_if_combine.mjs
+++ web/public/src/js_if_else_if_combine.mjs
@@ -8,7 +8,7 @@ export function js_if_else_if_combine(ast) {
     let { node, stack } = v;
     let stack1 = list_get_end_1(stack);
     function lambda3() {}
-    js_node_type_is_if(stack1, type, lambda3);
+    js_node_type_is_if(stack1, "IfStatement", lambda3);
     log(stack1);
   }
   js_visit_type(ast, "IfStatement", lambda);
