cc5ec96d09fe65beab21fe9d3cffb3cbf909a626
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 14:14:56 -0400

    function_auto_path public\src\function_exists.mjs

--- public/src/function_exists.mjs
+++ public/src/function_exists.mjs
@@ -1,7 +1,8 @@
 import { file_exists } from "./file_exists.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 export async function function_exists(f_name) {
-  let { f_path } = await function_name_to_path_unalias(f_name);
+  let u = await function_name_to_path_unalias(f_name);
+  let { f_path } = u;
   const exists = await file_exists(f_path);
   let v = {
     exists,
