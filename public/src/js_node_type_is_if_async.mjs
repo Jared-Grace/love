282058e822e0bef0238e0a7375461d29b8ca6be3
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:27:05 -0400

    function_auto_path public\src\js_node_type_is_if_async.mjs

---
+++ public/src/js_node_type_is_if_async.mjs
@@ -0,0 +1,7 @@
+import { js_node_type_is } from "./js_node_type_is.mjs";
+export function js_node_type_is_if_async(node, type, lambda) {
+  let type_is = js_node_type_is(node, type);
+  if (type_is) {
+    lambda();
+  }
+}
