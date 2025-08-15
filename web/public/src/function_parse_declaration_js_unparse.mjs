40d02f74b88dd8219c8f4cabe990c652ec3d95a8
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 12:59:22 -0400

    function_auto_path public\src\function_parse_declaration_js_unparse.mjs

--- web/public/src/function_parse_declaration_js_unparse.mjs
+++ web/public/src/function_parse_declaration_js_unparse.mjs
@@ -1,6 +1,7 @@
 import { js_unparse } from "./js_unparse.mjs";
 import { function_parse_declaration } from "./function_parse_declaration.mjs";
 export async function function_parse_declaration_js_unparse() {
+  "todo rename remove js";
   let { declaration } = await function_parse_declaration(f_name);
   let output = await js_unparse(declaration);
   return output;
