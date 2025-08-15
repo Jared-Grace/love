82eab91ec9605f8de4c4a239bf929c68bfa24831
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:46:14 -0400

    c dwg data_terminal_get

---
+++ public/src/data_terminal_get.mjs
@@ -0,0 +1,8 @@
+import { data_property_get } from "./data_property_get.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { data_get } from "./data_get.mjs";
+export async function data_terminal_get() {
+  const property_name = "watch";
+  let w = await data_property_get(property_name);
+  return w;
+}
