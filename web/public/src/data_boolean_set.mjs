241885dfef26d3a6e36015a77cd7fb1627958eb3
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 14:06:34 -0400

    function_auto_path public\src\data_boolean_set.mjs

--- web/public/src/data_boolean_set.mjs
+++ web/public/src/data_boolean_set.mjs
@@ -1,7 +1,9 @@
-import {data_transform} from "./data_transform.mjs";
+import { data_transform } from "./data_transform.mjs";
 export async function data_boolean_set(property_name, value) {
   function lambda(previous) {
-    let v = value;
+    let v = null;
+    if (false) {
+    }
     return v;
   }
   await data_transform(property_name, null, lambda);
