b5db3d93caf63f4574d65e3dda68f7728bdbc244
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 12:51:47 -0400

    fc function_parse_declaration,ju

---
+++ web/public/src/function_parse_declaration_js_unparse.mjs
@@ -0,0 +1,6 @@
+import { js_unparse } from "./js_unparse.mjs";
+import { function_parse_declaration } from "./function_parse_declaration.mjs";
+export function function_parse_declaration_js_unparse() {
+  function_parse_declaration;
+  js_unparse;
+}
