31905803b64342629fc66ff4c406634350441c26
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 10:27:44 -0400

    function_auto_path public\src\folder_current_join.mjs

---
+++ public/src/folder_current_join.mjs
@@ -0,0 +1,7 @@
+import { path_join } from "./path_join.mjs";
+import { folder_current } from "./folder_current.mjs";
+export function folder_current_join(result) {
+  let current = folder_current();
+  let value_string = path_join([current, result]);
+  return value_string;
+}
