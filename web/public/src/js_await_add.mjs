bf2f5b93d0506d7cb6c3755ec19cb0a42af87765
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 10:33:11 -0400

    function_auto_path public\src\js_await_add.mjs

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -1,5 +1,3 @@
 import { log } from "./log.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
-export function js_await_add(ast) {
-  let imports = js_imports_all(ast);
-}
+export function js_await_add(ast) {}
