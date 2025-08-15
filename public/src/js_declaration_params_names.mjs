e6c5f78f7fa618c07624ca2a605be18c3fb4e8ab
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:03:15 -0400

    function_auto_path public\src\js_declaration_params_names.mjs

--- public/src/js_declaration_params_names.mjs
+++ public/src/js_declaration_params_names.mjs
@@ -6,16 +6,14 @@ import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_declaration_params_names_node } from "./js_declaration_params_names_node.mjs";
-export function js_declaration_params_names(declaration) {let params_names=null
+export function js_declaration_params_names(declaration) {
+  let params_names = null;
   if (false) {
     let params = js_declaration_params_get(declaration);
-   params_names = js_identifiers_to_names(params);
+    params_names = js_identifiers_to_names(params);
   } else {
     let params = js_declaration_params_get(declaration);
-    params_names = list_map_squash(
-      params,
-      js_declaration_params_names_node,
-    );
+    params_names = list_map_squash(params, js_declaration_params_names_node);
   }
   return params_names;
 }
