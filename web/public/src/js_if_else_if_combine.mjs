feb9031b3c437c91eebdf360bc39703d024ba3b8
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:07:47 -0400

    function_auto_path public\src\js_if_else_if_combine.mjs

--- web/public/src/js_if_else_if_combine.mjs
+++ web/public/src/js_if_else_if_combine.mjs
@@ -3,8 +3,8 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
 export function js_if_else_if_combine(ast) {
   function lambda(v) {
-    let { node } = v;
-    log(node);
+    let { node, stack } = v;
+    log(stack);
   }
   js_visit_type(ast, "IfStatement", lambda);
 }
