d1205461a91ed7a295d24583085603965869b449
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:41:54 -0400

    function_auto_path public\src\function_path_to_name.mjs

--- web/public/src/function_path_to_name.mjs
+++ web/public/src/function_path_to_name.mjs
@@ -1,4 +1,5 @@
-import {path_name} from './path_name.mjs';
-export function function_path_to_name(f_name) {
-  return path_name(f_name);
+import { path_name } from "./path_name.mjs";
+export function function_path_to_name(f_path) {
+  let name = path_name(f_path);
+  return name;
 }
