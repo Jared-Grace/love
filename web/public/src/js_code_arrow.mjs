14e87ac10b6f11fb24c78337b7abc53da4381772
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:27:19 -0400

    function_auto_path public\src\js_dollar_f.mjs

---
+++ web/public/src/js_code_arrow.mjs
@@ -0,0 +1,7 @@
+import {js_code_braces_empty} from './js_code_braces_empty.mjs';
+import {js_code_parenthesis_open_close} from './js_code_parenthesis_open_close.mjs';
+export function js_code_arrow() {
+  let oc = js_code_parenthesis_open_close();
+  const code = oc + "=>" + js_code_braces_empty();
+  return code;
+}
