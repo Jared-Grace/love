6e7ab67f34599e23f049d81ba295bf3bb971ded2
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:28:38 -0400

    function_auto_path public\src\list_size_1_assert.mjs

--- web/public/src/list_size_1_assert.mjs
+++ web/public/src/list_size_1_assert.mjs
@@ -1,9 +1,11 @@
+import { string_to } from "./string_to.mjs";
 import { error } from "./error.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 export function list_size_1_assert(list) {
   if (!list_size_1(list)) {
     let message = list_size(list);
-    error(message);
+    let message2 = string_to(message);
+    error(message2);
   }
 }
