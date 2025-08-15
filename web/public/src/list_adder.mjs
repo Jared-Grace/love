b89b936a09a230be4e58c2dcb589e25529af51f8
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 20:02:54 -0400

    function_auto_path public\src\list_adder.mjs

--- web/public/src/list_adder.mjs
+++ web/public/src/list_adder.mjs
@@ -1,7 +1,7 @@
 import { list_adder_generic } from "./list_adder_generic.mjs";
 import { list_add } from "./list_add.mjs";
-export function list_adder(lambda) {
+export function list_adder(lambda$la) {
   let fn = list_add;
-  let list = list_adder_generic(lambda, fn);
+  let list = list_adder_generic(lambda$la, fn);
   return list;
 }
