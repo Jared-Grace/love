114f3f28ef71454b37793ceca91a742999607a15
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 18:50:26 -0400

    function_auto_path public\src\function_wrap_suffix_add.mjs

--- web/public/src/function_wrap_suffix_add.mjs
+++ web/public/src/function_wrap_suffix_add.mjs
@@ -2,6 +2,8 @@ import { function_name_unalias } from "./function_name_unalias.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function function_wrap_suffix_add(f_name, suffix) {
-  f_name = await function_name_unalias(f_name);
-  return await function_wrap(f_name, function_name_combine(f_name, suffix));
+  let { unaliased } = await function_name_unalias(f_name);
+  let f_name_wrapped = function_name_combine(unaliased, suffix);
+  let v = await function_wrap(unaliased, f_name_wrapped);
+  return v;
 }
