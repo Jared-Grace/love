046e7b21e119febed3a48b00de779f7479ac1ea5
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 09:26:37 -0400

    function_auto_path public\src\function_imports_add.mjs

--- web/public/src/function_imports_add.mjs
+++ web/public/src/function_imports_add.mjs
@@ -1,3 +1,4 @@
+import { js_code_string } from "./js_code_string.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { js_keyword_from } from "./js_keyword_from.mjs";
 import { js_code_wrap_braces } from "./js_code_wrap_braces.mjs";
@@ -6,7 +7,7 @@ import { list_add_first } from "./list_add_first.mjs";
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { each } from "./each.mjs";
-export function function_imports_add(ast, imports) {
+export async function function_imports_add(ast, imports) {
   let { body } = ast;
   function lambda(import_) {
     const from = "'./" + function_name_to_base(import_) + "'";
@@ -16,4 +17,5 @@ export function function_imports_add(ast, imports) {
   }
   each(imports, lambda);
   return;
+  let code_string = await js_code_string(value_string);
 }
