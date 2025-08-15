5a2854107aac03ef87aada2302c6c1f2e58dadfb
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 10:31:57 -0400

    function_auto_path public\src\function_imports_add.mjs

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -8,29 +8,13 @@ import { function_name_to_base } from "./function_name_to_base.mjs";
 import { each } from "./each.mjs";
 export async function function_imports_add(ast, imports) {
   let { body } = ast;
-  function lambda(import_) {
-    let code =
-      "import" +
-      " { " +
-      import_ +
-      " } from './" +
-      function_name_to_base(import_) +
-      "'";
+  async function lambda(import_) {
+    let result = function_name_to_base(import_);
+    let value_string = folder_current_join(result);
+    const from = await js_code_string(value_string);
+    let code = js_code_import_single(import_, from);
     let statement = js_parse_statement_module(code);
     list_add_first(body, statement);
   }
-  each(imports, lambda);
-  return;
-  {
-    let { body } = ast;
-    async function lambda(import_) {
-      let result = function_name_to_base(import_);
-      let value_string = folder_current_join(result);
-      const from = await js_code_string(value_string);
-      let code = js_code_import_single(import_, from);
-      let statement = js_parse_statement_module(code);
-      list_add_first(body, statement);
-    }
-    await each_async(imports, lambda);
-  }
+  await each_async(imports, lambda);
 }
