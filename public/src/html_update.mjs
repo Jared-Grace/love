4b6a4a9c18690529e03d97a255248df6f54736ae
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 09:19:47 -0400

    function_auto_path public\src\html_update.mjs

--- public/src/html_update.mjs
+++ public/src/html_update.mjs
@@ -1,11 +1,13 @@
+import { function_name_extension } from "./function_name_extension.mjs";
 import { file_open } from "./file_open.mjs";
 import { html_overwrite } from "./html_overwrite.mjs";
 import { html_name_to_path } from "./html_name_to_path.mjs";
 export async function html_update(name) {
   let file_path = html_name_to_path(name);
   const name_prefixed = `app_${name}`;
+  let result = function_name_extension();
   let body = `<script type="module">
-    import { ${name_prefixed} } from './${name_prefixed}.js';
+    import { ${name_prefixed} } from './${result}.js';
     sayHello('World');
   </script>`;
   await html_overwrite(name, body);
