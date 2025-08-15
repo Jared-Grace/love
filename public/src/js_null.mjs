7b1cd85c766ef768caf0a2ae2983316113f378b2
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:52:58 -0400

    function_auto_path public\src\js_null.mjs

--- public/src/js_null.mjs
+++ public/src/js_null.mjs
@@ -1,7 +1,7 @@
-import {js_parse_expression} from './js_parse_expression.mjs';
-import {js_keyword_null} from './js_keyword_null.mjs';
+import { js_parse_expression } from "./js_parse_expression.mjs";
+import { js_keyword_null } from "./js_keyword_null.mjs";
 export function js_null() {
   let init_code = js_keyword_null();
-  let value = js_parse_expression(init_code);
-  return value;
+  let n = js_parse_expression(init_code);
+  return n;
 }
