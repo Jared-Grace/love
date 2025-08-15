147ca3cf13a02ee6b0315d743253f8753054fa80
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:50:53 -0400

    function_auto_path public\src\data_transform.mjs

--- public/src/data_transform.mjs
+++ public/src/data_transform.mjs
@@ -3,10 +3,14 @@ import { data_save } from "./data_save.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { data_get } from "./data_get.mjs";
 import { log } from "./log.mjs";
-export async function data_transform(property_name, value_initial, value_get) {
+export async function data_transform(
+  property_name,
+  value_initial,
+  lambda$previous,
+) {
   var d = await data_get(property_name, value_initial);
   let { data, value: value_previous } = d;
-  const value = value_get(value_previous);
+  const value = lambda$previous(value_previous);
   object_property_set(data, property_name, value);
   await data_save(d);
   return value;
