8115727ed12598bafccb1593f089bc181c08b27c
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 20:59:53 -0400

    function_auto_path public\src\list_map_squash.mjs

--- web/public/src/list_map_squash.mjs
+++ web/public/src/list_map_squash.mjs
@@ -1,7 +1,7 @@
-import {list_squash} from './list_squash.mjs';
-import {list_map} from './list_map.mjs';
-export function list_map_squash(properties, fn, names) {
+import { list_squash } from "./list_squash.mjs";
+import { list_map } from "./list_map.mjs";
+export function list_map_squash(properties, fn) {
   let mapped = list_map(properties, fn);
-  names = list_squash(mapped);
+  let names = list_squash(mapped);
   return names;
 }
