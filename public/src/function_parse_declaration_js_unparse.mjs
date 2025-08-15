c4985a31078bfc833a96607ff86b67d6d4a1f282
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 12:51:47 -0400

    function_auto_path public\src\function_parse_declaration_js_unparse.mjs

--- public/src/function_parse_declaration_js_unparse.mjs
+++ public/src/function_parse_declaration_js_unparse.mjs
@@ -1,6 +1,6 @@
 import { js_unparse } from "./js_unparse.mjs";
 import { function_parse_declaration } from "./function_parse_declaration.mjs";
-export function function_parse_declaration_js_unparse() {
-  function_parse_declaration;
-  js_unparse;
+export async function function_parse_declaration_js_unparse() {
+  let result = await function_parse_declaration(f_name);
+  let output = await js_unparse(ast);
 }
