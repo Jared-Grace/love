6b70732b1227e4a1ca81d6d79e162f50c763add4
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 18:50:27 -0400

    function_auto_path public\src\function_copy_suffix_add.mjs

--- public/src/function_copy_suffix_add.mjs
+++ public/src/function_copy_suffix_add.mjs
@@ -2,7 +2,7 @@ import { function_name_unalias } from "./function_name_unalias.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { function_copy } from "./function_copy.mjs";
 export async function function_copy_suffix_add(f_name_old, suffix) {
-  let unaliased = await function_name_unalias(f_name_old);
+  let { unaliased } = await function_name_unalias(f_name_old);
   let combined = function_name_combine(unaliased, suffix);
   let v = await function_copy(unaliased, combined);
   return v;
