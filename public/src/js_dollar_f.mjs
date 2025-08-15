5ce4174486ec6b3a09aa5462134acd9987be70a4
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:23:32 -0400

    function_auto_path public\src\js_dollar_f.mjs

--- public/src/js_dollar_f.mjs
+++ public/src/js_dollar_f.mjs
@@ -1,1 +1,6 @@
-export function js_dollar_f(ast) {}
+import { object_replace } from "./object_replace.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
+export function js_dollar_f({ remaining, node, stack1, ast, afters }) {
+  let parsed = js_parse_expression(code);
+  object_replace(node, parsed);
+}
