6369fd83092894e1282e0b6713c7f3180aaed772
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 13:19:51 -0400

    function_auto_path public\src\function_transform_marker.mjs

--- web/public/src/function_transform_marker.mjs
+++ web/public/src/function_transform_marker.mjs
@@ -20,16 +20,19 @@ import { list_empty_is } from "./list_empty_is.mjs";
 import { list_first } from "./list_first.mjs";
 export async function function_transform_marker(f_name, lambda$a) {
   let marker_name = await data_marker_current_get();
-  return list_adder_async(async (la) => {
+  async function lambda2(la) {
     await function_transform(f_name, lambda_marker);
     async function lambda_marker(ast) {
       let visitors = js_type(ast, "CallExpression");
-      await each_async(visitors, async (v) => {
+      async function lambda(v) {
         if (js_marker_named(v, marker_name)) {
           let a = function_transform_marker_arg(v, ast);
           la(await lambda$a(a));
         }
-      });
+      }
+      await each_async(visitors, lambda);
     }
-  });
+  }
+  let list = list_adder_async(lambda2);
+  return list;
 }
