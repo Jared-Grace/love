3958909e3e72cc7db2364290cccbc8ad47d9159f
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:16:38 -0400

    function_auto_path public\src\js_auto.mjs

--- web/public/src/js_node_type_is_if.mjs
+++ web/public/src/js_node_type_is_if.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_node_type_is_if(node, type) {
-  return js_node_type_is(node, type);
+  return js_node_type_is(node, type); 
 }
