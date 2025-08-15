86d793db9c94ee1a1d717821224fc92867fc57f4
Grace-Jared <christrosetolife@gmail.com>
2025-08-14 21:27:19 -0400

    function_auto_path public\src\js_dollar_f.mjs

--- public/src/js_dollar_f.mjs
+++ public/src/js_dollar_f.mjs
@@ -1,10 +1,10 @@
+import { js_code_arrow } from "./js_code_arrow.mjs";
 import { js_code_braces_empty } from "./js_code_braces_empty.mjs";
 import { js_code_parenthesis_open_close } from "./js_code_parenthesis_open_close.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_dollar_f({ remaining, node, stack1, ast, afters }) {
-  let oc = js_code_parenthesis_open_close();
-  const code = oc + "=>" + js_code_braces_empty();
+  const code = js_code_arrow();
   let parsed = js_parse_expression(code);
   object_replace(node, parsed);
 }
