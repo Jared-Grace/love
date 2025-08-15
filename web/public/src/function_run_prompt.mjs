6fd2793af32f95624e074ef77772429904c121a2
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 19:01:44 -0400

    function_auto_path public\src\function_run_prompt.mjs

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -1,3 +1,4 @@
+import { import_install } from "./import_install.mjs";
 import { function_run_line_git } from "./function_run_line_git.mjs";
 import { function_run_line } from "./function_run_line.mjs";
 import { log } from "./log.mjs";
@@ -7,6 +8,9 @@ import { function_run_git } from "./function_run_git.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_unique } from "./list_unique.mjs";
 export async function function_run_prompt() {
+  const chalk = (await import_install("chalk")).default;
+  let v = chalk.green("Hello, world in green!");
+  console.log(v);
   let line = await command_line_read("✟ ");
   await function_run_line_git(line);
 }
