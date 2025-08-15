aa340ed250a81fe29451845376f76463a1cf7b04
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 07:52:53 -0400

    md 1

--- web/public/src/js_if_else_if_combine.mjs
+++ web/public/src/js_if_else_if_combine.mjs
@@ -1,7 +1,7 @@
 import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
 export function js_if_else_if_combine(ast) {
-  marker("1");
   function lambda(v) {}
+  marker("1");
   js_visit_type(ast, "IfStatement", lambda);
 }
