606440c02e7946f313fe0fd0688da04a4840acd3
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 08:33:38 -0400

    function_auto_path public\src\js_if_else_if_combine.mjs

--- web/public/src/js_if_else_if_combine.mjs
+++ web/public/src/js_if_else_if_combine.mjs
@@ -14,10 +14,10 @@ export function js_if_else_if_combine(ast) {
     async function lambda3() {
       let consequent2 = object_property_get(node, "consequent");
       let consequent = object_property_get(stack1, "consequent");
-      async function lambda2(item) {
-        log("here");
-      }
       let v2 = await equal_by_async(consequent, consequent2, js_unparse);
+      log({
+        v2,
+      });
     }
     js_node_type_is_if_async(stack1, "IfStatement", lambda3);
     log(stack1);
