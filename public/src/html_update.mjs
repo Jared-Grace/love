82710fd1a36acaa3504af1934bdcfdda9202cdef
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 09:18:49 -0400

    function_auto_path public\src\html_update.mjs

--- public/src/html_update.mjs
+++ public/src/html_update.mjs
@@ -1,10 +1,10 @@
-import {file_open} from './file_open.mjs';
-import {html_overwrite} from './html_overwrite.mjs';
-import {html_name_to_path} from './html_name_to_path.mjs';
+import { file_open } from "./file_open.mjs";
+import { html_overwrite } from "./html_overwrite.mjs";
+import { html_name_to_path } from "./html_name_to_path.mjs";
 export async function html_update(name) {
   let file_path = html_name_to_path(name);
   let body = `<script type="module">
-    import { sayHello } from './greetings.js';
+    import { ${`app_${name}`} } from './greetings.js';
     sayHello('World');
   </script>`;
   await html_overwrite(name, body);
