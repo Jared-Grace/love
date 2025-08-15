6e6320115aa41fd7ff3fcf2cd068260bbb6ab63b
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 10:33:53 -0400

    m

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,3 +1,6 @@
+import { marker } from "marker.mjs";
 import { log } from "./log.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
-export function js_await_add(ast) {}
+export function js_await_add(ast) {
+  marker("1");
+}
