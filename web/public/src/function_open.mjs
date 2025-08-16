218d1edb4eff46dc59337d33949b504d060f547d
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 11:43:35 -0400

    o o

--- web/public/src/function_open.mjs
+++ web/public/src/function_open.mjs
@@ -1,13 +1,15 @@
-import {function_current_set} from './function_current_set.mjs';
-import {data_transform} from './data_transform.mjs';
-import {data_save} from "./data_save.mjs";
-import {data_get} from "./data_get.mjs";
-import {function_name_to_path_unalias} from "./function_name_to_path_unalias.mjs";
-import {file_open} from "./file_open.mjs";
-import {function_name_to_path} from "./function_name_to_path.mjs";
-import {object_property_set} from "./object_property_set.mjs";
+import { marker } from "./marker.mjs";
+import { function_current_set } from "./function_current_set.mjs";
+import { data_transform } from "./data_transform.mjs";
+import { data_save } from "./data_save.mjs";
+import { data_get } from "./data_get.mjs";
+import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
+import { file_open } from "./file_open.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
+import { object_property_set } from "./object_property_set.mjs";
 export async function function_open(f_name) {
-  const {f_path} = await function_name_to_path_unalias(f_name);
+  marker("1");
+  const { f_path } = await function_name_to_path_unalias(f_name);
   await file_open(f_path);
   await function_current_set(f_name);
 }
