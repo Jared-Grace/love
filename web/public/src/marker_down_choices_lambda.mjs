a31e7a180cd98d433f64c6c22cad1fba4ce02c19
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 14:11:54 -0400

    function_auto_path public\src\marker_down_choices_lambda.mjs

--- web/public/src/marker_down_choices_lambda.mjs
+++ web/public/src/marker_down_choices_lambda.mjs
@@ -1,8 +1,10 @@
+import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_stack_list_block_is } from "./js_stack_list_block_is.mjs";
 import { list_remove } from "./list_remove.mjs";
 export function marker_down_choices_lambda({ stack2, stack1, ast }) {
+  let body_block = js_declaration_single_block_blody(ast2);
   function lambda3(la) {
     function lambda2(v) {
       let { stack } = v;
