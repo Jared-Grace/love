09745cd22f8d1ffedbcbe0bbe5b9cec0306d6953
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 17:15:35 -0400

    function_auto_path public\src\js_marker_enter.mjs

--- web/public/src/js_marker_enter.mjs
+++ web/public/src/js_marker_enter.mjs
@@ -1,7 +1,9 @@
+import { function_types } from "./function_types.mjs";
 import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
 import { marker } from "./marker.mjs";
 export async function js_marker_enter(ast) {
   marker("1");
+  let f_types = await function_types();
   async function lambda(a) {}
   let v = await function_transform_marker_current(lambda);
 }
