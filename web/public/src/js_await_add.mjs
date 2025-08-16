f8bc89a45308842478a13676687f1d5fb16786fb
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 14:25:12 -0400

    md -1

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -4,8 +4,8 @@ import { log } from "./log.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
 export function js_await_add(ast) {
   function lambda(v) {
-    marker("1");
     marker("2");
+    marker("1");
   }
   js_visit_type(ast, "CallExpression", lambda);
 }
