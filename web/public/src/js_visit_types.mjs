ca080a687293fbeb2b0ff18a12a104bc0e5fbf0e
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 18:13:29 -0400

    c js_visit_type js_visit_types

---
+++ web/public/src/js_visit_types.mjs
@@ -0,0 +1,15 @@
+import { marker } from "./marker.mjs";
+import { log } from "./log.mjs";
+import { js_visit } from "./js_visit.mjs";
+import { js_visit_children_get } from "./js_visit_children_get.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { js_node_is } from "./js_node_is.mjs";
+export function js_visit_types(ast, type, lambda$v) {
+  marker("1");
+  js_visit(ast, (v) => {
+    let { node } = v;
+    if (js_node_is(node) && js_node_type_is(node, type)) {
+      lambda$v(v);
+    }
+  });
+}
