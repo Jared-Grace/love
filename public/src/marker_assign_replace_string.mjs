ff30b1aa48bb68b8e8a198d89207180b85e9f76d
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 21:11:11 -0400

    fw mar marker_assign_replace_string

---
+++ public/src/marker_assign_replace_string.mjs
@@ -0,0 +1,4 @@
+import { marker_assign_replace } from "./marker_assign_replace.mjs";
+export async function marker_assign_replace_string(init_code) {
+  return await marker_assign_replace(init_code);
+}
