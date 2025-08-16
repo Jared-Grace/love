5cdfdd209d5fdee0ef3ff95541205a1491062eff
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 22:12:45 -0400

    function_auto_path public\src\function_open_editor_current.mjs

--- public/src/function_open_editor_current.mjs
+++ public/src/function_open_editor_current.mjs
@@ -1,5 +1,7 @@
+import { data_function_current_get } from "./data_function_current_get.mjs";
 import { function_open_editor } from "./function_open_editor.mjs";
 export async function function_open_editor_current(f_name) {
+  let f_name_current = await data_function_current_get();
   let v = await function_open_editor(f_name);
   return v;
 }
