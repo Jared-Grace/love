3fed3d80369084d1756b65936ef91be952541520
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 13:37:28 -0400

    function_auto_path public\src\marker_bottom.mjs

--- public/src/marker_bottom.mjs
+++ public/src/marker_bottom.mjs
@@ -20,7 +20,8 @@ import { list_map_property } from "./list_map_property.mjs";
 import { assert } from "./assert.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 export async function marker_bottom() {
-  return await marker_down_generic(delta_get);
+  let v2 = await marker_down_generic(delta_get);
+  return v2;
   function delta_get(a) {
     let { choices, next_index } = a;
     let v = list_index_last(choices) - next_index;
