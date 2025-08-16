a6d5f7d41248655747f41ea950db2f2048e257c8
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 17:23:11 -0400

    function_auto_path public\src\js_marker_enter.mjs

---
+++ web/public/src/js_types_function_includes.mjs
@@ -0,0 +1,7 @@
+import {list_includes} from "./list_includes.mjs";
+import {js_types_function} from "./js_types_function.mjs";
+export function js_types_function_includes(item) {
+  let f_types = js_types_function();
+  let includes = list_includes(f_types, item);
+  return includes;
+}
