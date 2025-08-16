82b45062d624b32345d14ea2fb8db8197ae222e2
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 22:56:38 -0400

    m

--- public/src/data_terminal_get.mjs
+++ public/src/data_terminal_get.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { data_property_get } from "./data_property_get.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { data_get } from "./data_get.mjs";
 export async function data_terminal_get() {
+  marker("1");
   const property_name = "watch";
   let w = await data_property_get(property_name);
   return w;
