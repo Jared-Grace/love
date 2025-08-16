6d2b4f02e0065b6050f1c3a24920485257104118
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 16:04:31 -0400

    function_auto_path public\src\list_is_assert.mjs

--- web/public/src/list_is_assert.mjs
+++ web/public/src/list_is_assert.mjs
@@ -1,7 +1,9 @@
+import { assert_message } from "./assert_message.mjs";
 import { assert } from "./assert.mjs";
 import { list_is } from "./list_is.mjs";
 export function list_is_assert(stack1_v_new) {
   let result = list_is(stack1_v_new);
   assert(result);
   return;
+  assert_message(b, message);
 }
