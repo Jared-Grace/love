53dece22477489940d1de611f8799750ea002270
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 12:10:21 -0400

    function_auto_path public\src\marker_down_choices.mjs

--- web/public/src/marker_down_choices.mjs
+++ web/public/src/marker_down_choices.mjs
@@ -25,5 +25,20 @@ export async function marker_down_choices() {
   let f_name = await data_function_current_get();
   let v = await function_transform_marker(f_name, lambda);
   return v;
-  async function lambda(a) {}
+  async function lambda(a) {
+    let vs = marker_down_choices_lambda(a);
+    let nodes = list_map_property(vs, "node");
+    let { ast } = a;
+    async function lambda2(item) {
+      let l = list_is(item);
+      if (l) {
+        return item;
+      } else {
+        let code = await js_unparse(item);
+        return code;
+      }
+    }
+    let waited = await list_map_unordered_async(nodes, lambda2);
+    return waited;
+  }
 }
