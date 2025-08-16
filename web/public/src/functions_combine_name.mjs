49afce41ba234e85978404ac6a3b85247b477ea6
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 13:00:08 -0400

    function_auto_path public\src\functions_combine_name.mjs

--- web/public/src/functions_combine_name.mjs
+++ web/public/src/functions_combine_name.mjs
@@ -5,7 +5,7 @@ import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { string_split_comma } from "./string_split_comma.mjs";
 export async function functions_combine_name(f_names_comma) {
   let split = string_split_comma(f_names_comma);
-  let f_names = await list_map_unordered_async(split, function_name_unalias);
+  let results = await list_map_unordered_async(split, function_name_unalias);
   log(f_names);
   let combined = function_name_combine_multiple(f_names);
   let v = {
