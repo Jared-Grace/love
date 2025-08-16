def455153789a96a55c82e60e0c743c63ba3f0d3
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 15:13:20 -0400

    function_auto_path public\src\list_size_1_assert_message.mjs

--- web/public/src/list_size_1_assert_message.mjs
+++ web/public/src/list_size_1_assert_message.mjs
@@ -3,10 +3,8 @@ import { string_to } from "./string_to.mjs";
 import { error } from "./error.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
-export function list_size_1_assert_message(list) {
+export function list_size_1_assert_message(list, message) {
   if (!list_size_1(list)) {
-    let message = list_size(list);
-    let message2 = string_to(message);
-    error(message2);
+    error(message);
   }
 }
