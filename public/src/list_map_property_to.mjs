f1c294842bbfaf2255409b96752342d5d1442140
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 16:00:44 -0400

    function_auto_path public\src\list_map_property_to.mjs

--- public/src/list_map_property_to.mjs
+++ public/src/list_map_property_to.mjs
@@ -1,4 +1,5 @@
+import { list_map } from "./list_map.mjs";
 import { marker } from "./marker.mjs";
-export function list_map_property_to() {
-  marker("1");
+export function list_map_property_to(list, name) {
+  let mappeed = list_map(list2, function lambda(item) {});
 }
