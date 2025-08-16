f81d6fbc8971c1a3c0050ca712b5fa0fef7cd11d
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 17:13:49 -0400

    function_auto_path public\src\function_transform_marker_current.mjs

--- web/public/src/function_transform_marker_current.mjs
+++ web/public/src/function_transform_marker_current.mjs
@@ -1,9 +1,9 @@
 import { data_function_current_get } from "./data_function_current_get.mjs";
 import { function_transform_marker } from "./function_transform_marker.mjs";
 import { marker } from "./marker.mjs";
-export async function function_transform_marker_current(f_name, lambda$a) {
+export async function function_transform_marker_current(lambda$a) {
   marker("1");
   let f_name_current = await data_function_current_get();
-  let v = await function_transform_marker(f_name, lambda$a);
+  let v = await function_transform_marker(f_name_current, lambda$a);
   return v;
 }
