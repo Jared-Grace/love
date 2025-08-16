93200d8ccd6071d39c1053f59df5a1fff58bf8dd
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 23:05:03 -0400

    function_auto_path public\src\marker_top.mjs

--- web/public/src/marker_top.mjs
+++ web/public/src/marker_top.mjs
@@ -31,7 +31,7 @@ export async function marker_top() {
   async function lambda(ast) {
     let markers = js_markers(ast);
     let ne = list_empty_not_is(markers);
-    if (false) {
+    if (ne) {
     }
     let names = list_map(markers, js_marker_name_get);
     let name_next = js_identifier_unique(names, "");
