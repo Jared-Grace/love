d1e006ddf04f90098e737f9fbcb7f61f43ada3d9
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 22:11:24 -0400

    function_auto_path public\src\function_current_declaration_code.mjs

--- public/src/function_current_declaration_code.mjs
+++ public/src/function_current_declaration_code.mjs
@@ -1,4 +1,5 @@
 import { function_parse_declaration_js_unparse } from "./function_parse_declaration_js_unparse.mjs";
 export async function function_current_declaration_code(f_name) {
-  return await function_parse_declaration_js_unparse(f_name);
+  let v = await function_parse_declaration_js_unparse(f_name);
+  return v;
 }
