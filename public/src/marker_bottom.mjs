a15d3a0f1799b8a2c6a74c11749b32c43df837bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 14:16:50 -0400

    o mb

--- public/src/marker_bottom.mjs
+++ public/src/marker_bottom.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { marker_down_generic } from "./marker_down_generic.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_get } from "./list_get.mjs";
@@ -20,6 +21,7 @@ import { list_map_property } from "./list_map_property.mjs";
 import { assert } from "./assert.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 export async function marker_bottom() {
+  marker("1");
   let v2 = await marker_down_generic(delta_get);
   return v2;
   function delta_get(a) {
