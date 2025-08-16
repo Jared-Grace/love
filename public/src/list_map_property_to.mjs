746ab4fcd6f98ecf170ab7ef1ccdb57c3bd6e48f
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 16:01:04 -0400

    function_auto_path public\src\list_map_property_to.mjs

--- public/src/list_map_property_to.mjs
+++ public/src/list_map_property_to.mjs
@@ -1,8 +1,10 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { list_map } from "./list_map.mjs";
 import { marker } from "./marker.mjs";
 export function list_map_property_to(list, name) {
   function lambda(item) {
     let o = {};
+    object_property_set(object, property_name, value);
   }
   let mappeed = list_map(list, lambda);
 }
