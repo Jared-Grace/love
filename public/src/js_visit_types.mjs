956ea09805d4d74554b5a11e09737b727c106d36
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 18:14:06 -0400

    function_auto_path public\src\js_visit_types.mjs

--- public/src/js_visit_types.mjs
+++ public/src/js_visit_types.mjs
@@ -5,7 +5,7 @@ import { js_visit } from "./js_visit.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { js_node_is } from "./js_node_is.mjs";
-export function js_visit_types(ast, type, lambda$v) {
+export function js_visit_types(ast, types, lambda$v) {
   marker("1");
   function lambda(v) {
     let { node } = v;
