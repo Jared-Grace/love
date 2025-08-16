ef6e4a1842c0c641fe584079cfbbd65c561794ea
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 12:52:01 -0400

    function_auto_path public\src\marker_next_get.mjs

--- web/public/src/marker_next_get.mjs
+++ web/public/src/marker_next_get.mjs
@@ -1,8 +1,12 @@
-import {marker_next_index} from './marker_next_index.mjs';
-import {list_get} from './list_get.mjs';
-import {list_index_of} from './list_index_of.mjs';
+import { marker_next_index } from "./marker_next_index.mjs";
+import { list_get } from "./list_get.mjs";
+import { list_index_of } from "./list_index_of.mjs";
 export function marker_next_get(a) {
-  let {index,stack2} = marker_next_index(a);
+  let { index, stack2 } = marker_next_index(a);
   let next = list_get(stack2, index);
-  return {next,index};
+  let v = {
+    next,
+    index,
+  };
+  return v;
 }
