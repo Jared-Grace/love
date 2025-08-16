4e80a77d71630c7c5cdfe31a1bc657d9a63fe3dc
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 04:30:37 -0400

    ir type CallExpression

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -5,5 +5,5 @@ import { js_imports_all } from "./js_imports_all.mjs";
 export function js_await_add(ast) {
   marker("1");
   function lambda(v) {}
-  js_visit_type(ast, type, lambda);
+  js_visit_type(ast, "CallExpression", lambda);
 }
