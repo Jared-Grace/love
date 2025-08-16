736fde6313275250ac442f30430ac230145ace4c
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 14:56:53 -0400

    function_auto_path public\src\js_marker_name_get.mjs

--- web/public/src/js_marker_name_get.mjs
+++ web/public/src/js_marker_name_get.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { error } from "./error.mjs";
 import { list_is } from "./list_is.mjs";
 import { list_first } from "./list_first.mjs";
@@ -10,32 +11,39 @@ export function js_marker_name_get(v) {
   let { stack } = v;
   let stack1 = list_get_end(stack, 1);
   if (!js_node_is(stack1)) {
-    return null;
+    let v2 = null;
+    return v2;
   }
   if (!js_node_type_is(stack1, "ExpressionStatement")) {
-    return null;
+    let v3 = null;
+    return v3;
   }
   let { node } = v;
   let { callee } = node;
   if (!js_node_type_is(callee, "Identifier")) {
-    return null;
+    let v4 = null;
+    return v4;
   }
   let { name } = callee;
   if (name !== marker.name) {
-    return null;
+    let v5 = null;
+    return v5;
   }
   let { arguments: arguments2 } = node;
   if (list_empty_is(arguments2)) {
-    return null;
+    let v6 = null;
+    return v6;
   }
   let a_first = list_first(arguments2);
   if (!js_node_type_is(a_first, "Literal")) {
-    return null;
+    let v7 = null;
+    return v7;
   }
   let stack2 = list_get_end(stack, 2);
   if (!list_is(stack2)) {
     error();
   }
   let { value } = a_first;
+  log(message);
   return value;
 }
