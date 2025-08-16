ef68babab22dd5d890bfe9947c966e4e52a787c9
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 17:21:20 -0400

    function_auto_path public\src\js_marker_enter.mjs

--- web/public/src/js_marker_enter.mjs
+++ web/public/src/js_marker_enter.mjs
@@ -1,4 +1,4 @@
-import { lambda_invoke_multiple } from "./lambda_invoke_multiple.mjs";
+import { list_includes } from "./list_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 import { list_skip } from "./list_skip.mjs";
@@ -14,7 +14,7 @@ export async function js_marker_enter(ast) {
     let index_next = list_index_of_next(stack2, stack1);
     let skipped = list_skip(stack2, index_next);
     function lambda2(item) {
-      lambda_invoke_multiple(afters);
+      let includes = list_includes(list, item2);
     }
     let result = list_filter(skipped, lambda2);
   }
