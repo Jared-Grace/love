3d248f58f47730d8a5815fb908847c880c1eac4d
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 17:18:10 -0400

    function_auto_path public\src\list_skip.mjs

--- web/public/src/list_skip.mjs
+++ web/public/src/list_skip.mjs
@@ -1,7 +1,7 @@
 import { list_slice } from "./list_slice.mjs";
 import { list_size } from "./list_size.mjs";
-export function list_skip(s, skip_count) {
-  let b = list_size(s);
-  const skipped = list_slice(s, skip_count, b);
+export function list_skip(list, skip_count) {
+  let b = list_size(list);
+  const skipped = list_slice(list, skip_count, b);
   return skipped;
 }
