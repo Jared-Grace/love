dd0ddfe769137f2a024ef7167910943b7923bd59
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 16:06:20 -0400

    function_auto_path public\src\assert_json.mjs

--- web/public/src/assert_json.mjs
+++ web/public/src/assert_json.mjs
@@ -1,6 +1,6 @@
-import {assert_message} from "./assert_message.mjs";
-import {json_to} from "./json_to.mjs";
-export function assert_json(o) {
+import { assert_message } from "./assert_message.mjs";
+import { json_to } from "./json_to.mjs";
+export function assert_json(b, o) {
   let message = json_to(o);
   assert_message(b, message);
 }
