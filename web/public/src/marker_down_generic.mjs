a681c25be0036d81598bb48bff535e73a07581b2
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 16:32:24 -0400

    function_auto_path public\src\marker_down_generic.mjs

--- web/public/src/marker_down_generic.mjs
+++ web/public/src/marker_down_generic.mjs
@@ -26,7 +26,7 @@ export async function marker_down_generic(delta_get) {
   async function lambda(a) {
     let { stack2, stack1 } = a;
     let { next } = marker_next_get(a);
-    let index = list_index_of(nodes, stack1);
+    let index = list_index_of(stack2, stack1);
     list_remove(stack2, stack1);
     let choices = marker_down_choices_lambda(a);
     let nodes = list_map_property(choices, "node");
