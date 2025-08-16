355f6a533b29671626a311690822ffc5ad3ad7ab
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 11:10:58 -0400

    function_auto_path public\src\marker_down_choices.mjs

--- web/public/src/marker_down_choices.mjs
+++ web/public/src/marker_down_choices.mjs
@@ -27,8 +27,9 @@ export async function marker_down_choices() {
     let vs = marker_down_choices_lambda(a);
     let nodes = list_map_property(vs, "node");
     function lambda2(item) {
-      let l = list_is(value);
-      if (false) {
+      let l = list_is(item);
+      if (l) {
+        return item;
       }
     }
     let result = list_map(nodes, lambda2);
