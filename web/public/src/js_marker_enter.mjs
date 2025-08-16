185194e5c5a468f2a2963c6965b9a26a88960c9e
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 17:42:23 -0400

    function_auto_path public\src\js_marker_enter.mjs

--- web/public/src/js_marker_enter.mjs
+++ web/public/src/js_marker_enter.mjs
@@ -16,7 +16,7 @@ export async function js_marker_enter(ast) {
     let index_next = list_index_of_next(stack2, stack1);
     let skipped = list_skip(stack2, index_next);
     function lambda2(item) {
-      let result2 = js_node_type(n);
+      let result2 = js_node_type(item);
       let includes = js_types_function_includes(item);
       return includes;
     }
