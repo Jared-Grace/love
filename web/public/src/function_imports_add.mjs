72884dae51d2a72ebbc6f3da918f35d58af79771
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 04:19:43 -0400

    function_auto_path public\src\function_imports_add.mjs

--- web/public/src/function_imports_add.mjs
+++ web/public/src/function_imports_add.mjs
@@ -13,7 +13,6 @@ export async function function_imports_add(ast, imports) {
     let result = function_name_to_base(import_);
     let value_string = folder_current_join(result);
     const from = await js_code_string(value_string);
-    log(from);
     let code = js_code_import_single(import_, from);
     let statement = js_parse_statement_module(code);
     list_add_first(body, statement);
