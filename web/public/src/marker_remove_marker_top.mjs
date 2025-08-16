6645cdfc90b4e56648679bf89df72cfea0a42c6b
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 13:16:50 -0400

    function_auto_path public\src\marker_remove_marker_top.mjs

--- web/public/src/marker_remove_marker_top.mjs
+++ web/public/src/marker_remove_marker_top.mjs
@@ -1,8 +1,8 @@
 import { marker_top } from "./marker_top.mjs";
 import { marker_remove } from "./marker_remove.mjs";
 import { marker } from "./marker.mjs";
-export function marker_remove_marker_top() {
+export async function marker_remove_marker_top() {
   marker("1");
-  marker_remove;
-  marker_top;
+  await marker_remove();
+  await marker_top();
 }
