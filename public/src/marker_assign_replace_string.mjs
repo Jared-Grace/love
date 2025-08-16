d759c66d6057a2c7e5c31fd1fe15e1a213fa835d
Grace-Jared <christrosetolife@gmail.com>
2025-08-15 21:18:00 -0400

    function_auto_path public\src\marker_assign_replace_string.mjs

--- public/src/marker_assign_replace_string.mjs
+++ public/src/marker_assign_replace_string.mjs
@@ -1,7 +1,7 @@
 import { js_code_string } from "./js_code_string.mjs";
 import { marker_assign_replace } from "./marker_assign_replace.mjs";
-export async function marker_assign_replace_string(init_code) {
+export async function marker_assign_replace_string(value) {
   let code_string = await js_code_string(value_string);
-  let v = await marker_assign_replace(init_code);
+  let v = await marker_assign_replace(value);
   return v;
 }
