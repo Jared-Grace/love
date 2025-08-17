e87bebf0f4eb121cdbe18306a7fc0220f1513d5a
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:08:01 -0400

    function_auto_path public\src\js_if_blockify.mjs

--- web/public/src/js_if_blockify.mjs
+++ web/public/src/js_if_blockify.mjs
@@ -1,4 +1,24 @@
+import { js_visit_type } from "./js_visit_type.mjs";
+import { object_replace } from "./object_replace.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { js_statement_return } from "./js_statement_return.mjs";
+import { object_copy } from "./object_copy.mjs";
+import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 import { marker } from "./marker.mjs";
 export function js_if_blockify(ast) {
-  marker("1");
+  let { node } = v;
+  let body = object_property_get(node, "body");
+  const type = "BlockStatement";
+  let nti = js_node_type_not_is(body, type);
+  if (nti) {
+    let copy = object_copy(body);
+    let r = js_statement_return("");
+    object_property_set(r, "argument", copy);
+    object_replace(body, {
+      type: "BlockStatement",
+      body: [r],
+    });
+  }
+  js_visit_type(ast, "ArrowFunctionExpression", lambda);
 }
