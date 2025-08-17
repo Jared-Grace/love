6fae3de26f042a32e9b143a942a397a92a84774b
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:12:32 -0400

    function_auto_path public\src\list_index_last.mjs

--- public/src/list_index_last.mjs
+++ public/src/list_index_last.mjs
@@ -1,4 +1,5 @@
 import { list_size } from "./list_size.mjs";
 export function list_index_last(list) {
-  return list_size(list) - 1;
+  const index_last = list_size(list) - 1;
+  return index_last;
 }
