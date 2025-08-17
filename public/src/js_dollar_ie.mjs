a180e6be8375c716b901f45583bd34b39547342d
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 19:57:13 -0400

    function_auto_path public\src\js_dollar_ie.mjs

--- public/src/js_dollar_ie.mjs
+++ public/src/js_dollar_ie.mjs
@@ -5,7 +5,7 @@ import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
 import { js_keyword_if } from "./js_keyword_if.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 import { marker } from "./marker.mjs";
-export function js_dollar_ie({ stack1 }) {
+export function js_dollar_ie({ stack1, stack2 }) {
   let inside = js_keyword_false();
   let from = js_parse_statement(
     js_keyword_if() +
