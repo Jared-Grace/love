a8a2b7d89d8d0d7dff389f88f3fbbfba0887310c
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 18:37:32 -0400

    function_auto_path public\src\js_dollar_i.mjs

--- public/src/js_dollar_i.mjs
+++ public/src/js_dollar_i.mjs
@@ -1,13 +1,15 @@
+import { js_keyword_false } from "./js_keyword_false.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { js_code_braces_empty } from "./js_code_braces_empty.mjs";
 import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
 import { js_keyword_if } from "./js_keyword_if.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 export function js_dollar_i({ stack1 }) {
+  let inside = js_keyword_false();
   let from = js_parse_statement(
     js_keyword_if() +
       " " +
-      js_code_wrap_parenthesis("false") +
+      js_code_wrap_parenthesis(inside) +
       js_code_braces_empty(),
   );
   object_replace(stack1, from);
