90650ad8cdb58b47e1c136fa8413be8e4ea8c54d
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:23:55 -0400

    function_auto_path public\src\js_arrow_blockify.mjs

--- web/public/src/js_arrow_blockify.mjs
+++ web/public/src/js_arrow_blockify.mjs
@@ -1,26 +1,12 @@
 import { js_statement_return } from "./js_statement_return.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
+import { js_if_blockify_generic } from "./js_if_blockify_generic.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { object_copy } from "./object_copy.mjs";
-export function js_arrow_blockify(ast) {
-  function lambda(v) {
-    let { node } = v;
-    let body = object_property_get(node, "body");
-    const type = "BlockStatement";
-    let nti = js_node_type_not_is(body, type);
-    if (nti) {
-      let copy = object_copy(body);
-      let r = js_statement_return("");
-      object_property_set(r, "argument", copy);
-      object_replace(body, {
-        type: "BlockStatement",
-        body: [r],
-      });
-    }
-  }
-  js_visit_type(ast, "ArrowFunctionExpression", lambda);
+export async function js_arrow_blockify(ast) {
+  const type = "ArrowFunctionExpression";
+  await js_if_blockify_generic(ast, type);
 }
