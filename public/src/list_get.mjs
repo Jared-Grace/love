59339bad999b568aa7ecfcc2449ecdcf2219b782
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 13:01:25 -0400

    p

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -1,8 +1,8 @@
 import { undefined_not_is_assert_lambda } from "./undefined_not_is_assert_lambda.mjs";
 export function list_get(list, index) {
   let v = list[index];
-  return v;
   undefined_not_is_assert_lambda(value, object_get);
+  return v;
   function object_get() {
     let v = {
       list,
