102492b9c75baad14e86033f4f0cc616305e259f
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 12:59:37 -0400

    function_auto_path public\src\object_property_get.mjs

--- web/public/src/object_property_get.mjs
+++ web/public/src/object_property_get.mjs
@@ -4,12 +4,16 @@ import { undefined_is } from "./undefined_is.mjs";
 export function object_property_get(object, property_name) {
   let value = object[property_name];
   if (undefined_is(value)) {
-    const message = {
-      object,
-      property_name,
-    };
+    const message = message_get();
     let message2 = json_to(message);
     error(message2);
   }
   return value;
+  function message_get() {
+    let v = {
+      object,
+      property_name,
+    };
+    return v;
+  }
 }
