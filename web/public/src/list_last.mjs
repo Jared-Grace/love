efcd8c514c23212a69a48884e908733fef0f550e
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 12:57:48 -0400

    function_auto_path public\src\list_last.mjs

--- web/public/src/list_last.mjs
+++ web/public/src/list_last.mjs
@@ -1,5 +1,7 @@
 import { list_get } from "./list_get.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 import { list_index_last } from "./list_index_last.mjs";
-
-export function list_last( list ){return list_get_end(list,0)}
+export function list_last(list) {
+  let item = list_get_end(list, 0);
+  return item;
+}
