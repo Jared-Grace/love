25bbc46bb1385e30d37e725ed6a1a4fb243e47df
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 15:12:43 -0400

    function_auto_path public\src\js_block_child_graph.mjs

--- web/public/src/js_block_child_graph.mjs
+++ web/public/src/js_block_child_graph.mjs
@@ -12,7 +12,7 @@ export function js_block_child_graph(ast) {
     function lambda(v) {
       let { node, stack } = v;
       let last = js_stack_last(stack, "BlockStatement");
-      null_not_is(value);
+      let v2 = null_not_is(value);
       if (last) {
       }
       la([last, node]);
