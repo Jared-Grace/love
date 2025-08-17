51494e4f2d955e6947cfb3f16c7ab81617772b4d
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:12:03 -0400

    function_auto_path public\src\list_index_is.mjs

--- web/public/src/list_index_is.mjs
+++ web/public/src/list_index_is.mjs
@@ -2,11 +2,12 @@ import { list_empty_is } from "./list_empty_is.mjs";
 import { marker } from "./marker.mjs";
 export function list_index_is(list, index) {
   let e = list_empty_is(list);
+  let v = false;
   if (e) {
-    let v = false;
     return v;
   }
-  if (false) {
+  if (index < 0) {
+    return v;
   }
   marker("1");
 }
