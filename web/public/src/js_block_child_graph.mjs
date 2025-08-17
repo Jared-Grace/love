582342f384c9804a2e57a52e07402ec8cc40d003
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 21:01:22 -0400

    function_auto_path public\src\js_block_child_graph.mjs

--- web/public/src/js_block_child_graph.mjs
+++ web/public/src/js_block_child_graph.mjs
@@ -3,7 +3,7 @@ import { js_visitors } from "./js_visitors.mjs";
 import { marker } from "./marker.mjs";
 export function js_block_child_graph(ast) {
   marker("1");
-  let list = js_visitors(ast);
   function lambda(v) {}
+  let list = js_visitors(ast);
   each(list, lambda);
 }
