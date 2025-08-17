6c69adc9e24e2ae2056bfd83fe0217495df9231f
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 15:01:20 -0400

    function_auto_path public\src\list_last_or_null.mjs

--- web/public/src/list_last_or_null.mjs
+++ web/public/src/list_last_or_null.mjs
@@ -1,8 +1,8 @@
 import { list_empty_is } from "./list_empty_is.mjs";
 import { marker } from "./marker.mjs";
 export function list_last_or_null(list) {
-  let result = list_empty_is(list2);
-  if (false) {
+  let result = list_empty_is(list);
+  if (result) {
   }
   marker("1");
 }
