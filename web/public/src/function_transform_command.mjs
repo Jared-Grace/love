0b15d611e55779ae3ab83ec4a2f7be1266aa9d45
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 15:10:03 -0400

    function_auto_path public\src\function_transform_command.mjs

--- web/public/src/function_transform_command.mjs
+++ web/public/src/function_transform_command.mjs
@@ -1,6 +1,8 @@
+import { function_transform_result } from "./function_transform_result.mjs";
 import { function_import } from "./function_import.mjs";
 import { function_transform } from "./function_transform.mjs";
 export async function function_transform_command(transformer_name, f_name) {
   const transformer = await function_import(transformer_name);
-  return await function_transform(f_name, transformer);
+  let v = await function_transform_result(f_name, transformer);
+  return v;
 }
