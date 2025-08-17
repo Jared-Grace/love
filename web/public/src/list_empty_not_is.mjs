25720c607e70e73fd61eced86dfc51864b003f22
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 14:49:42 -0400

    function_auto_path public\src\list_empty_not_is.mjs

--- web/public/src/list_empty_not_is.mjs
+++ web/public/src/list_empty_not_is.mjs
@@ -1,5 +1,5 @@
 import { list_empty_is } from "./list_empty_is.mjs";
-export function list_empty_not_is(body_block) {
-  const ne = !list_empty_is(body_block);
+export function list_empty_not_is(list) {
+  const ne = !list_empty_is(list);
   return ne;
 }
