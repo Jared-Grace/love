181078f2b33606328ba3760178bd72b208485525
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:35:07 -0400

    function_auto_path public\src\equal_by.mjs

--- web/public/src/equal_by.mjs
+++ web/public/src/equal_by.mjs
@@ -1,6 +1,7 @@
+import { equal } from "./equal.mjs";
 export function equal_by(a, b, lambda$item) {
   const left = lambda$item(a);
   const right = lambda$item(b);
-  let v = left === right;
+  let v = equal(left, right);
   return v;
 }
