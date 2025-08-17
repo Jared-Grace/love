49d8ab1c11fd5ef32e89fa057e6ad7f8529e08b5
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 15:03:51 -0400

    function_auto_path public\src\js_stack_last_multiple.mjs

--- web/public/src/js_stack_last_multiple.mjs
+++ web/public/src/js_stack_last_multiple.mjs
@@ -1,3 +1,4 @@
+import { list_last_or_null } from "./list_last_or_null.mjs";
 import { js_stack_filtered_multiple } from "./js_stack_filtered_multiple.mjs";
 import { js_stack_filtered } from "./js_stack_filtered.mjs";
 import { list_last } from "./list_last.mjs";
@@ -6,6 +7,6 @@ import { js_node_is } from "./js_node_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function js_stack_last_multiple(stack, types) {
   let filtered = js_stack_filtered_multiple(stack, types);
-  let last = list_last(filtered);
+  let last = list_last_or_null(filtered);
   return last;
 }
