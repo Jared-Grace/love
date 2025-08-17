25d8f7d9f6a2b4dd139d138ba265257d3f0a94c8
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:51:12 -0400

    function_auto_path public\src\js_block_child_graph.mjs

--- web/public/src/js_block_child_graph.mjs
+++ web/public/src/js_block_child_graph.mjs
@@ -26,7 +26,6 @@ export function js_block_child_graph(ast) {
         let ii = list_index_is(stack, index_2);
         assert(ii);
         let item = list_get(stack, index_2);
-        exit();
         let ne = equal_not(item, right);
         if (ne) {
           left = item;
