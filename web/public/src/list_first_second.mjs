1e4872b2342dd631ae3421a6190bb655adcf0ea7
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 14:40:18 -0400

    i list_first_second

--- web/public/src/list_first_second.mjs
+++ web/public/src/list_first_second.mjs
@@ -1,10 +1,12 @@
-import {list_second} from './list_second.mjs';
-import {list_first} from './list_first.mjs';
+import { list_second } from "./list_second.mjs";
+import { list_first } from "./list_first.mjs";
 export function list_first_second(split) {
-  let first = null;first = list_first(split);
-  let second=null;second = list_second(split);
+  let first = null;
+  first = list_first(split);
+  let second = null;
+  second = list_second(split);
   return {
     first,
-    second
+    second,
   };
 }
