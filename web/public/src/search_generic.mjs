2a09b8ae6ea7cf603abbbd0777b878f9b784e2bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 12:47:53 -0400

    function_auto_path public\src\string_includes.mjs

--- web/public/src/search_generic.mjs
+++ web/public/src/search_generic.mjs
@@ -4,10 +4,12 @@ import { string_includes } from "./string_includes.mjs";
 import { list_all } from "./list_all.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { string_split } from "./string_split.mjs";
+import { log } from "./log.mjs";
 export function search_generic(search, f_names, value_get) {
-  let terms = string_split(search, ",");
+  let terms = string_split(search, ",");log(terms)
   let f_names_search = list_filter(f_names, (n) =>
-    list_all(terms, (term) => string_includes(n, term)),
+    {log({n})
+      return list_all(terms, (term) => string_includes(n, term))},
   );
   let result = {};
   each(f_names_search, (n) => {
