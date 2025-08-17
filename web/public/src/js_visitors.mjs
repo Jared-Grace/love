4f3e0d3916345be9cbcfeec2c743ab51d00f0c78
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 20:53:14 -0400

    function_auto_path public\src\js_visitors.mjs

--- web/public/src/js_visitors.mjs
+++ web/public/src/js_visitors.mjs
@@ -2,5 +2,6 @@ import { js_visit } from "./js_visit.mjs";
 import { marker } from "./marker.mjs";
 export function js_visitors(ast, lambda$v) {
   marker("1");
-  return js_visit(ast, lambda$v);
+  let v = js_visit(ast, lambda$v);
+  return v;
 }
