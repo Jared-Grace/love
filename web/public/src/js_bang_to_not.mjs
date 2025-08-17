70f3e6c03cbe470a3b37c9c8db6a24fd865debe2
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 18:33:02 -0400

    function_auto_path public\src\js_bang_to_not.mjs

--- web/public/src/js_bang_to_not.mjs
+++ web/public/src/js_bang_to_not.mjs
@@ -4,7 +4,7 @@ import { js_visit } from "./js_visit.mjs";
 import { marker } from "./marker.mjs";
 export function js_bang_to_not(ast) {
   marker("1");
-  !a;
+  !0;
   function lambda(v) {
     let { node } = v;
     log(node);
