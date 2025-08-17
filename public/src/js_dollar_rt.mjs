a24156182668565279669e71706c61dfb62a62d3
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:24:08 -0400

    function_auto_path public\src\js_dollar_rt.mjs

--- public/src/js_dollar_rt.mjs
+++ public/src/js_dollar_rt.mjs
@@ -1,3 +1,4 @@
+import { js_return_code_replace } from "./js_return_code_replace.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_return_empty } from "./js_return_empty.mjs";
 import { marker } from "./marker.mjs";
@@ -11,9 +12,6 @@ import { js_code_return_empty } from "./js_code_return_empty.mjs";
 export function js_dollar_rt({ stack1 }) {
   marker("1");
   const code = "true";
-  let from = js_return_empty();
-  let expression = js_parse_expression(code);
-  object_property_set(from, "argument", expression);
-  object_replace(stack1, from);
+  js_return_code_replace(code, stack1);
   return;
 }
