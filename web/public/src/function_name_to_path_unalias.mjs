310e02672e73f79eb8e222eb91ddf23c83aab12d
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:57:20 -0400

    function_auto_path public\src\function_name_to_path_unalias.mjs

--- web/public/src/function_name_to_path_unalias.mjs
+++ web/public/src/function_name_to_path_unalias.mjs
@@ -2,6 +2,9 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 import { log } from "./log.mjs";
 export async function function_name_to_path_unalias(f_name) {
+  log({
+    f_name,
+  });
   let { unaliased } = await function_name_unalias(f_name);
   const f_path = function_name_to_path(unaliased);
   let v = {
