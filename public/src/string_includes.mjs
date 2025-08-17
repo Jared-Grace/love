86d6a248beefdbf0a07457e189663c54f8a43726
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 21:09:20 -0400

    function_auto_path public\src\string_includes.mjs

--- public/src/string_includes.mjs
+++ public/src/string_includes.mjs
@@ -1,13 +1,13 @@
+import { string_is_assert_json } from "./string_is_assert_json.mjs";
 import { string_is_assert_message } from "./string_is_assert_message.mjs";
 import { assert_message } from "./assert_message.mjs";
 import { json_to } from "./json_to.mjs";
 export function string_includes(string, item) {
   let v = string.includes(item);
   return v;
-  const newLocal = {
+  const o = {
     string,
     item,
   };
-  let message = json_to(newLocal);
-  let v2 = string_is_assert_message(value, message);
+  string_is_assert_json(o);
 }
