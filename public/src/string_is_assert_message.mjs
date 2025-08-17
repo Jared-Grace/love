2e768c8ceab6506ea620f78b8031aaec54472af3
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 21:06:52 -0400

    function_auto_path public\src\string_is_assert_message.mjs

--- public/src/string_is_assert_message.mjs
+++ public/src/string_is_assert_message.mjs
@@ -1,9 +1,10 @@
 import { marker } from "./marker.mjs";
 import { assert } from "./assert.mjs";
 import { string_is } from "./string_is.mjs";
+import { assert_message } from "./assert_message.mjs";
 export function string_is_assert_message(value) {
   marker("1");
   let b = string_is(value);
-  let v = assert(b);
+  let v = assert_message(b);
   return v;
 }
