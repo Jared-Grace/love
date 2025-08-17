747bf7d9214bf5138a394832c7cf9881ece3e530
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 15:09:43 -0400

    function_auto_path public\src\function_transform.mjs

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,3 +1,4 @@
+import { function_transform_result } from "./function_transform_result.mjs";
 import { function_parse_declaration_js_unparse } from "./function_parse_declaration_js_unparse.mjs";
 import { data_terminal_get } from "./data_terminal_get.mjs";
 import { log_keep } from "./log_keep.mjs";
@@ -5,10 +6,7 @@ import { log } from "./log.mjs";
 import { file_js_unparse } from "./file_js_unparse.mjs";
 import { function_parse } from "./function_parse.mjs";
 export async function function_transform(f_name, lambda$ast) {
-  let parsed = await function_parse(f_name);
-  let { ast } = parsed;
-  let result = await lambda$ast(ast);
-  await file_js_unparse(parsed);
+  await function_transform_result(f_name, lambda$ast);
   let output = await function_parse_declaration_js_unparse(f_name);
   return output;
 }
---
+++ public/src/function_transform_result.mjs
@@ -0,0 +1,8 @@
+import {file_js_unparse} from "./file_js_unparse.mjs";
+import {function_parse} from "./function_parse.mjs";
+export async function function_transform_result(f_name, lambda$ast) {
+  let parsed = await function_parse(f_name);
+  let {ast} = parsed;
+  let result = await lambda$ast(ast);
+  await file_js_unparse(parsed);
+}
