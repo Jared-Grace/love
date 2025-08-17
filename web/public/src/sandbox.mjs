b51e15b3488f298d956b52e945aec984cb317483
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:51:28 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,6 +1,10 @@
+import { log } from "./log.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
   if (0) {
+    function test() {
+      log();
+    }
   }
 }
