ae2262afe0329da1f9c18ae2b50157f6d2082019
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:11:12 -0400

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { functions_combine_test } from "./functions_combine_test.mjs";
 import { functions_combine_name } from "./functions_combine_name.mjs";
 import { list_map_name } from "./list_map_name.mjs";
@@ -11,6 +12,7 @@ import { function_delete } from "./function_delete.mjs";
 import { function_param_new } from "./function_param_new.mjs";
 import { function_wrap } from "./function_wrap.mjs";
 export async function sandbox() {
+  marker("1");
   const list = [function_current_set, task_new];
   await functions_combine_test(list);
 }
