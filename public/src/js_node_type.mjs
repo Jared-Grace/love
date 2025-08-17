2518109c512bc4668f5d25ad59acd31f7f8e744f
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:18:51 -0400

    function_auto_path public\src\js_node_type.mjs

--- public/src/js_node_type.mjs
+++ public/src/js_node_type.mjs
@@ -1,4 +1,5 @@
 import { object_property_get } from "./object_property_get.mjs";
 export function js_node_type(n) {
-  return object_property_get(n, "type");
+  const nt = object_property_get(n, "type");
+  return nt;
 }
