79ee39b703356bdf17247b55eace31ace86ae0d1
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 13:00:33 -0400

    function_auto_path public\src\object_property_get.mjs

--- public/src/object_property_get.mjs
+++ public/src/object_property_get.mjs
@@ -1,13 +1,10 @@
+import { undefined_not_is_assert_lambda } from "./undefined_not_is_assert_lambda.mjs";
 import { json_to } from "./json_to.mjs";
 import { error } from "./error.mjs";
 import { undefined_is } from "./undefined_is.mjs";
 export function object_property_get(object, property_name) {
   let value = object[property_name];
-  if (undefined_is(value)) {
-    const object = object_get();
-    let message2 = json_to(object);
-    error(message2);
-  }
+  undefined_not_is_assert_lambda(value, object_get);
   return value;
   function object_get() {
     let v = {
---
+++ public/src/undefined_not_is_assert_lambda.mjs
@@ -0,0 +1,10 @@
+import {error} from "./error.mjs";
+import {json_to} from "./json_to.mjs";
+import {undefined_is} from "./undefined_is.mjs";
+export function undefined_not_is_assert_lambda(value, lambda) {
+  if (undefined_is(value)) {
+    const object = lambda();
+    let message2 = json_to(object);
+    error(message2);
+  }
+}
