b299ad9e11a415233f67c744812d9927058be2b1
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:47:21 -0400

    function_auto_path public\src\list_get.mjs

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -5,6 +5,7 @@ import { assert } from "./assert.mjs";
 import { integer_is } from "./integer_is.mjs";
 import { undefined_not_is_assert_lambda } from "./undefined_not_is_assert_lambda.mjs";
 export function list_get(list, index) {
+  integer_is_assert(index);
   let item = list[index];
   undefined_not_is_assert_lambda(item, object_get);
   function object_get() {
@@ -15,5 +16,4 @@ export function list_get(list, index) {
     return v;
   }
   return item;
-  integer_is_assert(index);
 }
