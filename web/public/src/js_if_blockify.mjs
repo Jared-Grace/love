f909dae7cd682b1edefae8ae3bc6c919380fb36c
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:12:03 -0400

    function_auto_path public\src\js_if_blockify.mjs

--- web/public/src/js_if_blockify.mjs
+++ web/public/src/js_if_blockify.mjs
@@ -12,15 +12,6 @@ export function js_if_blockify(ast) {
     let body = object_property_get(node, "consequent");
     const type = "BlockStatement";
     let nti = js_node_type_not_is(body, type);
-    if (nti) {
-      let copy = object_copy(body);
-      let r = js_statement_return("");
-      object_property_set(r, "argument", copy);
-      object_replace(body, {
-        type: "BlockStatement",
-        body: [r],
-      });
-    }
   }
   js_visit_type(ast, "IfStatement", lambda);
 }
