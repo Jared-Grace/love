bc780975159f331f3c763e4887bf1b18f6bf7a94
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:44:55 -0400

    function_auto_path public\src\integer_is.mjs

--- web/public/src/integer_is.mjs
+++ web/public/src/integer_is.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 export function integer_is() {
   marker("1");
-  let v = Number.isInteger(value);
-  return v;
+  let ii = Number.isInteger(value);
+  return ii;
 }
