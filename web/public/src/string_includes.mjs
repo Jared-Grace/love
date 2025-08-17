888790b44d680a6f2325caa15bf3e0a3cf96361e
Grace-Jared <christrosetolife@gmail.com>
2025-08-16 21:12:21 -0400

    ps

--- web/public/src/string_includes.mjs
+++ web/public/src/string_includes.mjs
@@ -3,11 +3,11 @@ import { string_is_assert_message } from "./string_is_assert_message.mjs";
 import { assert_message } from "./assert_message.mjs";
 import { json_to } from "./json_to.mjs";
 export function string_includes(string, item) {
-  let v = string.includes(item);
   const o = {
     string,
     item,
   };
   string_is_assert_json(o);
+  let v = string.includes(item);
   return v;
 }
