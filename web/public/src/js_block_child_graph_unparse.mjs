cf8270b24958c4c90ee47c496a8f24cf01ec75e8
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:46:13 -0400

    function_auto_path public\src\js_block_child_graph_unparse.mjs

---
+++ web/public/src/js_block_child_graph_unparse.mjs
@@ -0,0 +1,55 @@
+import { js_node_type_is_if } from "./js_node_type_is_if.mjs";
+import { js_node_types_is } from "./js_node_types_is.mjs";
+import { js_types_function } from "./js_types_function.mjs";
+import { exit } from "./exit.mjs";
+import { equal_not } from "./equal_not.mjs";
+import { list_get } from "./list_get.mjs";
+import { assert } from "./assert.mjs";
+import { list_index_is } from "./list_index_is.mjs";
+import { list_index_of } from "./list_index_of.mjs";
+import { list_includes } from "./list_includes.mjs";
+import { list_is } from "./list_is.mjs";
+import { null_not_is } from "./null_not_is.mjs";
+import { list_adder } from "./list_adder.mjs";
+import { log } from "./log.mjs";
+import { js_stack_last } from "./js_stack_last.mjs";
+import { each } from "./each.mjs";
+import { js_visitors } from "./js_visitors.mjs";
+import { marker } from "./marker.mjs";
+export function js_block_child_graph_unparse(ast) {
+  marker("1");
+  function lambda2(la) {
+    function lambda(v) {
+      let { node: right, stack } = v;
+      let left = js_stack_last(stack, "BlockStatement");
+      let nn = null_not_is(left);
+      if (nn) {
+        let { body } = left;
+        let includes = list_includes(body, right);
+        if (includes) {
+          la([left, right]);
+          return;
+        }
+        function lambda4() {
+          let index = list_index_of(stack, left);
+          let index_2 = index + 2;
+          let ii = list_index_is(stack, index_2);
+          if (ii) {
+            let item = list_get(stack, index_2);
+            let ne = equal_not(item, right);
+            if (ne) {
+              left = item;
+              la([left, right]);
+              return;
+            }
+          }
+        }
+        js_node_type_is_if(right, "BlockStatement", lambda4);
+      }
+    }
+    let vs = js_visitors(ast);
+    each(vs, lambda);
+  }
+  let edges = list_adder(lambda2);
+  return edges;
+}
