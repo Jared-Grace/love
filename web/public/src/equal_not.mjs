4c70aaadd6e18264c88aa5dbae1d0c9b2b6f4c32
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:35:37 -0400

    function_auto_path public\src\equal_not.mjs

--- web/public/src/equal_not.mjs
+++ web/public/src/equal_not.mjs
@@ -2,5 +2,6 @@ import { equal } from "./equal.mjs";
 import { marker } from "./marker.mjs";
 export function equal_not(left, right) {
   marker("1");
-  return equal(left, right);
+  let eq = equal(left, right);
+  return eq;
 }
