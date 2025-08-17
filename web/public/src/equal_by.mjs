b84a569e40d8f3d0c71c013df8e62f47cc452e77
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 16:35:24 -0400

    function_auto_path public\src\equal_by.mjs

--- web/public/src/equal_by.mjs
+++ web/public/src/equal_by.mjs
@@ -2,6 +2,6 @@ import { equal } from "./equal.mjs";
 export function equal_by(a, b, lambda$item) {
   const left = lambda$item(a);
   const right = lambda$item(b);
-  let v = equal(left, right);
-  return v;
+  let eq = equal(left, right);
+  return eq;
 }
