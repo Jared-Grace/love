a9d1b61347ef050ef68209fa67bef1acb1f477ea
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 15:06:37 -0400

    function_auto_path public\src\js_block_child_graph.mjs

--- web/public/src/js_block_child_graph.mjs
+++ web/public/src/js_block_child_graph.mjs
@@ -6,7 +6,8 @@ import { js_visitors } from "./js_visitors.mjs";
 import { marker } from "./marker.mjs";
 export function js_block_child_graph(ast) {
   marker("1");
-  let list2 = list_adder(function lambda2(la) {});
+  function lambda2(la) {}
+  let list2 = list_adder(lambda2);
   function lambda(v) {
     let { node, stack } = v;
     let last = js_stack_last(stack, "BlockStatement");
