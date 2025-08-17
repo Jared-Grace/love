f17acd0d211dd6743020011821b849ce9ccaf428
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 17:37:33 -0400

    function_auto_path public\src\js_if_blockify_generic.mjs

--- public/src/js_if_blockify_generic.mjs
+++ public/src/js_if_blockify_generic.mjs
@@ -1,17 +1,18 @@
-import {js_visit_type_each_async} from "./js_visit_type_each_async.mjs";
-import {object_replace} from "./object_replace.mjs";
-import {list_add} from "./list_add.mjs";
-import {object_property_set} from "./object_property_set.mjs";
-import {js_statement_return} from "./js_statement_return.mjs";
-import {list_includes} from "./list_includes.mjs";
-import {js_node_type} from "./js_node_type.mjs";
-import {object_copy} from "./object_copy.mjs";
-import {js_node_type_not_is} from "./js_node_type_not_is.mjs";
-import {object_property_get} from "./object_property_get.mjs";
+import { js_visit_type_each_async } from "./js_visit_type_each_async.mjs";
+import { object_replace } from "./object_replace.mjs";
+import { list_add } from "./list_add.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { js_statement_return } from "./js_statement_return.mjs";
+import { list_includes } from "./list_includes.mjs";
+import { js_node_type } from "./js_node_type.mjs";
+import { object_copy } from "./object_copy.mjs";
+import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export async function js_if_blockify_generic(ast, type) {
   async function lambda(v) {
-    let {node} = v;
-    let body = object_property_get(node, "consequent");
+    let { node } = v;
+    const property_name = "consequent";
+    let body = object_property_get(node, property_name);
     let nti = js_node_type_not_is(body, "BlockStatement");
     if (nti) {
       let copy = object_copy(body);
@@ -25,7 +26,7 @@ export async function js_if_blockify_generic(ast, type) {
       }
       object_replace(body, {
         type: "BlockStatement",
-        body: bs_body
+        body: bs_body,
       });
     }
   }
