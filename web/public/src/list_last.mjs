be1598f1cc561293a18ac4d422f9b78eccb183b4
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 14:50:24 -0400

    function_auto_path public\src\list_last.mjs

--- web/public/src/list_last.mjs
+++ web/public/src/list_last.mjs
@@ -1,3 +1,4 @@
+import { list_empty_not_is_assert } from "./list_empty_not_is_assert.mjs";
 import { assert } from "./assert.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { list_get } from "./list_get.mjs";
@@ -6,6 +7,5 @@ import { list_index_last } from "./list_index_last.mjs";
 export function list_last(list) {
   let item = list_get_end(list, 0);
   return item;
-  let ne = list_empty_not_is(list);
-  assert(ne);
+  list_empty_not_is_assert(list);
 }
