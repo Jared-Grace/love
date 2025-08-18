47967301e068755698d9711bd549320db4f4fc86
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 23:11:03 -0400

    function_auto_path public\src\js_block_child_graph.mjs

--- web/public/src/js_block_child_graph.mjs
+++ web/public/src/js_block_child_graph.mjs
@@ -26,7 +26,7 @@ export function js_block_child_graph(ast) {
     function lambda(v) {
       let { node: right, stack } = v;
       log_json(right);
-      let includes2 = list_includes(list, item2);
+      let includes2 = list_includes(["ImportSpecifier"], item2);
       log_unparse(right);
       let left = js_stack_last(stack, "BlockStatement");
       let nn = null_not_is(left);
