e7346a44564ccb0c65982fa85bdda0e8649fc3bf
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 19:27:44 -0400

    function_auto_path public\src\data_identifiers_file.mjs

--- public/src/data_identifiers_file.mjs
+++ public/src/data_identifiers_file.mjs
@@ -1,7 +1,7 @@
 import { data_transform } from "./data_transform.mjs";
 import { marker } from "./marker.mjs";
 export async function data_identifiers_file(parsed) {
-  marker("1");
+  let { f_path, ast, code } = parsed;
   let property_name = "identifiers";
   function lambda(previous) {}
   let v = await data_transform(property_name, {}, lambda);
