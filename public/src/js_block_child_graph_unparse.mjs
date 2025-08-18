daf64701d2de21d5091174347cab73d234d0b568
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 21:39:47 -0400

    function_auto_path public\src\js_block_child_graph_unparse.mjs

--- public/src/js_block_child_graph_unparse.mjs
+++ public/src/js_block_child_graph_unparse.mjs
@@ -21,7 +21,8 @@ import { each } from "./each.mjs";
 import { js_visitors } from "./js_visitors.mjs";
 import { marker } from "./marker.mjs";
 export async function js_block_child_graph_unparse(ast) {
-  let edges = js_block_child_graph(ast);
+  function lambda2() {}
+  let edges = js_block_child_graph(ast, lambda2);
   async function lambda(edge) {
     let waited = await list_map_unordered_async(edge, js_unparse);
     return waited;
