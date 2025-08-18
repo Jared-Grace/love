64b4938d596fddf867690965bd065947f2a37c81
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 20:00:16 -0400

    function_auto_path public\src\js_dollar_ie.mjs

--- web/public/src/js_dollar_ie.mjs
+++ web/public/src/js_dollar_ie.mjs
@@ -1,3 +1,4 @@
+import { js_call_new } from "./js_call_new.mjs";
 import { log } from "./log.mjs";
 import { js_dollar_i } from "./js_dollar_i.mjs";
 import { js_keyword_false } from "./js_keyword_false.mjs";
@@ -7,9 +8,10 @@ import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
 import { js_keyword_if } from "./js_keyword_if.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 import { marker } from "./marker.mjs";
-export function js_dollar_ie({ stack1 }) {
+export async function js_dollar_ie({ stack1 }) {
   js_dollar_i({
     stack1,
   });
   let { test } = stack1;
+  let { declaration, parsed } = await js_call_new(f_name_call, ast);
 }
