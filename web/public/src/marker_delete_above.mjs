c26061c7a18314c8642a181044bbf1b48af96b6d
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:26:29 -0400

    function_auto_path public\src\marker_delete_above.mjs

---
+++ web/public/src/marker_delete_above.mjs
@@ -0,0 +1,19 @@
+import { list_insert } from "./list_insert.mjs";
+import { list_includes } from "./list_includes.mjs";
+import { js_parse_statement } from "./js_parse_statement.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { marker_index } from "./marker_index.mjs";
+import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
+import { marker } from "./marker.mjs";
+export async function marker_delete_above(code) {
+  marker("1");
+  async function lambda(a) {
+    let i = marker_index(a);
+    let index = object_property_get(i, "index");
+    let stack2 = object_property_get(i, "stack2");
+    let statement = js_parse_statement(code);
+    list_insert(stack2, index, statement);
+  }
+  let v = await function_transform_marker_current(lambda);
+  return v;
+}
