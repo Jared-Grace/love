13946ba575d774fe2a207c16b9a1af99aad4206b
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:23:15 -0400

    function_auto_path public\src\marker_above.mjs

--- web/public/src/marker_above.mjs
+++ web/public/src/marker_above.mjs
@@ -1,4 +1,4 @@
-import { list_is } from "./list_is.mjs";
+import { list_includes } from "./list_includes.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { marker_index } from "./marker_index.mjs";
@@ -10,7 +10,7 @@ export async function marker_above(code) {
     let i = marker_index(a);
     let index = object_property_get(i, "index");
     let statement = js_parse_statement(code);
-    let l = list_is(value);
+    let includes = list_includes(list, item);
   }
   let v = await function_transform_marker_current(lambda);
 }
