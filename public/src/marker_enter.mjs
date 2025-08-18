7b66345a11d1bb66509acc5658591f00aa83b3c7
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:01:42 -0400

    function_auto_path public\src\marker_enter.mjs

--- public/src/marker_enter.mjs
+++ public/src/marker_enter.mjs
@@ -29,9 +29,9 @@ export async function marker_enter() {
     let { next, stack1, stack2 } = n;
     let nt = js_node_type(next);
     if (js_types_function_includes(nt)) {
-      list_remove(stack2, stack1);
       let body = js_declaration_to_block_body(next);
       list_add_first(body, stack1);
+      list_remove(stack2, stack1);
     }
   }
   let v = await function_transform_marker_current(lambda);
