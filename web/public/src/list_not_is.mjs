400e69aebf6cd97697c0d4b0151c6b168ac3f958
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 23:05:19 -0400

    function_auto_path public\src\list_not_is.mjs

--- web/public/src/list_not_is.mjs
+++ web/public/src/list_not_is.mjs
@@ -1,7 +1,9 @@
+import { not } from "./not.mjs";
 import { list_is } from "./list_is.mjs";
 import { marker } from "./marker.mjs";
 export function list_not_is(value) {
   marker("1");
-  let l = list_is(value);
+  let a = list_is(value);
+  let l = not(a);
   return l;
 }
