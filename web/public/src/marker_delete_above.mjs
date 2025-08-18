5990607b1b3963596a7993e0e653d2175fd201a3
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:26:45 -0400

    function_auto_path public\src\marker_delete_above.mjs

--- web/public/src/marker_delete_above.mjs
+++ web/public/src/marker_delete_above.mjs
@@ -1,3 +1,4 @@
+import { marker_previous_index } from "./marker_previous_index.mjs";
 import { list_insert } from "./list_insert.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
@@ -8,7 +9,7 @@ import { marker } from "./marker.mjs";
 export async function marker_delete_above(code) {
   marker("1");
   async function lambda(a) {
-    let i = marker_index(a);
+    let i = marker_previous_index(a);
     let index = object_property_get(i, "index");
     let stack2 = object_property_get(i, "stack2");
     let statement = js_parse_statement(code);
