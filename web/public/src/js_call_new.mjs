0abc006dc18f66112e45c7fc142648dd4a867776
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 20:03:58 -0400

    function_auto_path public\src\js_call_new.mjs

--- web/public/src/js_call_new.mjs
+++ web/public/src/js_call_new.mjs
@@ -22,6 +22,7 @@ import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { marker_next_index } from "./marker_next_index.mjs";
 import { function_parse_declaration } from "./function_parse_declaration.mjs";
 export async function js_call_new(f_name_call, ast) {
+  marker("1");
   let {
     declaration,
     unaliased,
