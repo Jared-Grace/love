8bebf9b211e580c0c3a5088f92334f6fa55a62e4
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:25:34 -0400

    function_auto_path public\src\js_await_add.mjs

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -1,10 +1,11 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
 import { log } from "./log.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
 export function js_await_add(ast) {
   function lambda(v) {
-    $g$v$stack;
+    let stack = object_property_get(v, "stack");
     marker("1");
   }
   js_visit_type(ast, "CallExpression", lambda);
