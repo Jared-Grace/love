e65f81b804d31bb2f2cadb3602b3de29af75bc95
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 22:53:34 -0400

    c js_visit_each_async js_visit_each

--- public/src/js_visit_each.mjs
+++ public/src/js_visit_each.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { list_adder } from "./list_adder.mjs";
 export async function js_visit_each(a, lambda) {
+  marker("1");
   function lambda2(la) {
     let v = js_visit(a, la);
     return v;
