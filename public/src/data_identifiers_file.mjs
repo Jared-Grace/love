4bab94f75ea2b5d824eec3b2b552c93add56ced3
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 19:41:28 -0400

    function_auto_path public\src\data_identifiers_file.mjs

--- public/src/data_identifiers_file.mjs
+++ public/src/data_identifiers_file.mjs
@@ -17,7 +17,7 @@ export async function data_identifiers_file(parsed) {
       list_add_if_not_includes(list, f_name);
     }
     each(i_names, lambda2);
-    log(message);
+    log(previous);
     return previous;
   }
   let v = await data_transform(property_name, {}, lambda);
