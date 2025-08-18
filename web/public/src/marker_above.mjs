3794e019b19298e021f476834796a2af0a7e0e7e
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:17:08 -0400

    function_auto_path public\src\marker_above.mjs

--- web/public/src/marker_above.mjs
+++ web/public/src/marker_above.mjs
@@ -2,5 +2,6 @@ import { function_transform_marker_current } from "./function_transform_marker_c
 import { marker } from "./marker.mjs";
 export async function marker_above(code) {
   marker("1");
-  let v = await function_transform_marker_current(async function lambda(a) {});
+  async function lambda(a) {}
+  let v = await function_transform_marker_current(lambda);
 }
