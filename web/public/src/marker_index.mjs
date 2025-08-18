1bfb1de6ea24b52802c5c7c0adb5ed10534e3515
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:18:48 -0400

    c marker_previous_index marker_index

--- web/public/src/marker_index.mjs
+++ web/public/src/marker_index.mjs
@@ -1,8 +1,10 @@
+import { marker } from "./marker.mjs";
 import { list_index_of_previous } from "./list_index_of_previous.mjs";
 import { list_index_of_next } from "./list_index_of_next.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function marker_index(a) {
+  marker("1");
   let { stack2, stack1 } = a;
   let index = list_index_of_previous(stack2, stack1);
   let to = object_merge(
