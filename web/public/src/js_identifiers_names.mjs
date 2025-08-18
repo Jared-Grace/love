c59d0c5d464ca0b365a508369de8ab10b23f7bb7
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 19:31:39 -0400

    function_auto_path public\src\data_identifiers_file.mjs

--- web/public/src/js_identifiers_names.mjs
+++ web/public/src/js_identifiers_names.mjs
@@ -2,10 +2,11 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_identifiers_names(ast) {
-  return list_adder_unique((la) => {
+  const names = list_adder_unique((la) => {
     js_visit_type(ast, "Identifier", (v) => {
       let { node } = v;
       la(object_property_get(node, "name"));
     });
   });
+  return names;
 }
