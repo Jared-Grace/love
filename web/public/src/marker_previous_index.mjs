ae0e783202763ca6dd9a257d65541a511ed52412
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:26:54 -0400

    function_auto_path public\src\marker_previous_index.mjs

--- web/public/src/marker_previous_index.mjs
+++ web/public/src/marker_previous_index.mjs
@@ -5,10 +5,11 @@ import { object_merge } from "./object_merge.mjs";
 export function marker_previous_index(a) {
   let { stack2, stack1 } = a;
   let index = list_index_of_previous(stack2, stack1);
-  return object_merge(
+  const p = object_merge(
     {
       index,
     },
     a,
   );
+  return p;
 }
