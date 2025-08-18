c39f161e9484f68e4f0719243814201d7e05bfc1
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 19:32:09 -0400

    function_auto_path public\src\data_identifiers_file.mjs

--- web/public/src/data_identifiers_file.mjs
+++ web/public/src/data_identifiers_file.mjs
@@ -1,3 +1,4 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
 import { each } from "./each.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { data_transform } from "./data_transform.mjs";
@@ -7,7 +8,9 @@ export async function data_identifiers_file(parsed) {
   let property_name = "identifiers";
   function lambda(previous) {
     let names = js_identifiers_names(ast);
-    function lambda2(name) {}
+    function lambda2(name) {
+      let result = object_property_initialize(object, property_name2, value);
+    }
     each(names, lambda2);
   }
   let v = await data_transform(property_name, {}, lambda);
