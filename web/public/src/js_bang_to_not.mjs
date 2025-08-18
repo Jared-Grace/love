a8d15de2eadfa135a12dcb5989be6f5e3316daf4
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 20:15:59 -0400

    function_auto_path public\src\js_bang_to_not.mjs

--- web/public/src/js_bang_to_not.mjs
+++ web/public/src/js_bang_to_not.mjs
@@ -1,26 +1,21 @@
-import { not } from "./not.mjs";
-import { js_visit_type_each_async } from "./js_visit_type_each_async.mjs";
-import { js_call_new_expression } from "./js_call_new_expression.mjs";
+import { js_code_call } from "./js_code_call.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal } from "./equal.mjs";
 import { log } from "./log.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { marker } from "./marker.mjs";
-export async function js_bang_to_not(ast) {
+export function js_bang_to_not(ast) {
   marker("1");
   !0;
-  async function lambda(v) {
+  function lambda(v) {
     let { node } = v;
     let { operator } = node;
     if (equal(operator, "!")) {
       let argument = object_property_get(node, "argument");
-      let expression = await js_call_new_expression(not.name, ast);
-      log({
-        expression,
-      });
+      js_code_call(f_name);
     }
     log(node);
   }
-  await js_visit_type_each_async(ast, "UnaryExpression", lambda);
+  js_visit_type(ast, "UnaryExpression", lambda);
 }
