d6f91d828e2244dce6e0cd53e8d9e3237cc83ace
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 22:58:20 -0400

    function_auto_path public\src\log_unparse.mjs

--- public/src/log_unparse.mjs
+++ public/src/log_unparse.mjs
@@ -1,6 +1,6 @@
-import {log} from "./log.mjs";
-import {js_unparse} from "./js_unparse.mjs";
-export async function log_unparse(next) {
-  let code = await js_unparse(next);
+import { log } from "./log.mjs";
+import { js_unparse } from "./js_unparse.mjs";
+export function log_unparse(next) {
+  let code = js_unparse(next);
   log(code);
 }
