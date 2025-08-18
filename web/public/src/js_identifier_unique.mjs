8e2c4fa2d747a0d5b135dd5032494dd6fbf92ccb
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 20:21:12 -0400

    function_auto_path public\src\js_identifier_unique.mjs

--- web/public/src/js_identifier_unique.mjs
+++ web/public/src/js_identifier_unique.mjs
@@ -6,17 +6,17 @@ import { string_empty_not_is } from "./string_empty_not_is.mjs";
 import { string_empty_is } from "./string_empty_is.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_includes } from "./list_includes.mjs";
-export function js_identifier_unique(existing, name) {
+export function js_identifier_unique(used, name) {
   let v2 = js_keyword_arguments();
-  existing = list_concat(existing, [v2]);
-  list_all(existing, string_is_assert);
+  used = list_concat(used, [v2]);
+  list_all(used, string_is_assert);
   let unique = null;
   let attempt = 1;
   do {
     const suffix = attempt === 1 && string_empty_not_is(name) ? "" : attempt;
     unique = name + suffix;
     attempt++;
-  } while (string_empty_is(unique) || list_includes(existing, unique));
-  list_add(existing, unique);
+  } while (string_empty_is(unique) || list_includes(used, unique));
+  list_add(used, unique);
   return unique;
 }
