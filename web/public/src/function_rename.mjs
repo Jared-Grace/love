b26c9fff57ae7a08245a9a35dd331ca5357fc9cf
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 13:48:15 -0400

    function_auto_path public\src\function_rename.mjs

--- web/public/src/function_rename.mjs
+++ web/public/src/function_rename.mjs
@@ -1,4 +1,6 @@
+import { functions_combine } from "./functions_combine.mjs";
 import { marker } from "./marker.mjs";
-export function function_rename(f_name_before, f_name_after) {
+export async function function_rename(f_name_before, f_name_after) {
+  await functions_combine(f_names_comma);
   marker("1");
 }
