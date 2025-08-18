dfca32cdc83a60fd1792c82219a728b9f3d59dab
Grace-Jared <christrosetolife@gmail.com>
2025-08-18 14:32:13 -0400

    mda

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -5,7 +5,6 @@ import { log } from "./log.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
 export function js_await_add(ast) {
   function lambda(v) {
-    let stack = object_property_get(v, "stack");
     marker("1");
   }
   js_visit_type(ast, "CallExpression", lambda);
