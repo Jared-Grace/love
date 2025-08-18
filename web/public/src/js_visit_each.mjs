179798b0e8587db2e8e70e121749989b18ede4b9
Grace-Jared <christrosetolife@gmail.com>
2025-08-17 23:03:43 -0400

    function_auto_path public\src\js_visit_each.mjs

--- web/public/src/js_visit_each.mjs
+++ web/public/src/js_visit_each.mjs
@@ -3,12 +3,12 @@ import { marker } from "./marker.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { list_adder } from "./list_adder.mjs";
-export async function js_visit_each(a, lambda) {
+export async function js_visit_each(a, lambda$item) {
   marker("1");
   function lambda2(la) {
     let v = js_visit(a, la);
     return v;
   }
   let vs = list_adder(lambda2);
-  each(vs, lambda);
+  each(vs, lambda$item);
 }
