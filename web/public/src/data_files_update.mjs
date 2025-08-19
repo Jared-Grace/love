f7247e9fd431f01e8029e5b2281a372b1c79a9d2
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 14:43:53 -0400

    function_auto_path public\src\data_files_update.mjs

--- web/public/src/data_files_update.mjs
+++ web/public/src/data_files_update.mjs
@@ -1,8 +1,10 @@
+import { list_map } from "./list_map.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { data_file_update } from "./data_file_update.mjs";
 import { marker } from "./marker.mjs";
 export async function data_files_update() {
   let f_names = functions_names();
+  let mapped = list_map(list, function lambda(item) {});
   marker("1");
   let v = await data_file_update(f_path);
   return v;
