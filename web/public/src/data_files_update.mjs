400a429988070a6d0c2a51461008f789ebf38f88
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 14:43:46 -0400

    function_auto_path public\src\data_files_update.mjs

--- web/public/src/data_files_update.mjs
+++ web/public/src/data_files_update.mjs
@@ -1,6 +1,8 @@
+import { functions_names } from "./functions_names.mjs";
 import { data_file_update } from "./data_file_update.mjs";
 import { marker } from "./marker.mjs";
-export async function data_files_update(f_path) {
+export async function data_files_update() {
+  let f_names = functions_names();
   marker("1");
   let v = await data_file_update(f_path);
   return v;
