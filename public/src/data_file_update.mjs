05ddfc638c512d92eece2325ccf25193c308aa34
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:27:03 -0400

    function_auto_path public\src\data_file_update.mjs

--- public/src/data_file_update.mjs
+++ public/src/data_file_update.mjs
@@ -1,4 +1,6 @@
+import { function_path_to_name } from "./function_path_to_name.mjs";
 import { marker } from "./marker.mjs";
 export function data_file_update(f_path) {
+  let f_name = function_path_to_name(f_path);
   marker("1");
 }
