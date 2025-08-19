ddb7d7e67278c1c8affce5d0681ea7b11aea9bb0
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 13:45:12 -0400

    function_auto_path public\src\function_watch_ending_change.mjs

---
+++ web/public/src/string_suffix_change.mjs
@@ -0,0 +1,6 @@
+import {string_suffix_without} from "./string_suffix_without.mjs";
+export function string_suffix_change(input, ending_old, ending_new) {
+  let without = string_suffix_without(input, ending_old);
+  let changed = without + ending_new;
+  return changed;
+}
