ea722e412a24967296cd647f87a2068eed56ceb1
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 14:43:32 -0400

    r data_file_update_all data_files_update

--- web/public/src/data_file_update_all.mjs
+++
@@ -1,7 +0,0 @@
-import { data_file_update } from "./data_file_update.mjs";
-import { marker } from "./marker.mjs";
-export async function data_file_update_all(f_path) {
-  marker("1");
-  let v = await data_file_update(f_path);
-  return v;
-}
