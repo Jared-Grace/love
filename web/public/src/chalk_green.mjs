e322bd6c2c453f9718df187c2868fd1454a410e8
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 13:30:29 -0400

    function_auto_path public\src\chalk_green.mjs

--- web/public/src/chalk_green.mjs
+++ web/public/src/chalk_green.mjs
@@ -1,6 +1,7 @@
-import {import_install} from "./import_install.mjs";
+import { import_install } from "./import_install.mjs";
 export async function chalk_green(prompt) {
   const chalk = (await import_install("chalk")).default;
-  let prompt_colored = chalk.green(prompt);
+  const fn = chalk.green;
+  let prompt_colored = fn(prompt);
   return prompt_colored;
 }
