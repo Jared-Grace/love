e1e072b065cc6852aca5752cb56e135ac926367c
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:26:54 -0400

    function_auto_path public\src\function_auto_path.mjs

--- web/public/src/function_auto_path.mjs
+++ web/public/src/function_auto_path.mjs
@@ -1,8 +1,8 @@
 import { function_path_to_name } from "./function_path_to_name.mjs";
 import { path_name } from "./path_name.mjs";
 import { function_auto } from "./function_auto.mjs";
-export async function function_auto_path(f_name) {
-  f_name = function_path_to_name(f_name);
+export async function function_auto_path(f_path) {
+  let f_name = function_path_to_name(f_path);
   let v = await function_auto(f_name);
   return v;
 }
