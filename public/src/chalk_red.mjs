c56754f9800f4621cbc288f332559fd26f0c2d53
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 13:31:35 -0400

    function_auto_path public\src\chalk_red.mjs

--- public/src/chalk_red.mjs
+++ public/src/chalk_red.mjs
@@ -4,7 +4,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { import_install } from "./import_install.mjs";
 export async function chalk_red(prompt) {
   marker("1");
-  const color = "green";
+  const color = "red";
   let v = await chalk_color(color, prompt);
   return v;
 }
