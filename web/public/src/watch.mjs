a6e1e150095e35df72ff805eb80cd566f76b4bbe
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:22:04 -0400

    function_auto_path public\src\watch.mjs

--- web/public/src/watch.mjs
+++ web/public/src/watch.mjs
@@ -1,3 +1,4 @@
+import { list_join_space } from "./list_join_space.mjs";
 import { list_join } from "./list_join.mjs";
 import { marker } from "./marker.mjs";
 import { object_property_exists_equals } from "./object_property_exists_equals.mjs";
@@ -32,7 +33,7 @@ export async function watch() {
       object_property_set(in_progress, path, value);
       const f_name = function_auto_path.name;
       let args = [path];
-      let result = list_join(args, " ");
+      let result = list_join_space(args);
       let output = await command_line("node g.mjs " + f_name + " " + path);
       log_keep(output);
       object_property_set(in_progress, path, false);
