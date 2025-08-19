1f77329417ced9eecda8b23d74b2d9bff4625d69
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:27:21 -0400

    function_auto_path public\src\data_file_update.mjs

--- web/public/src/data_file_update.mjs
+++ web/public/src/data_file_update.mjs
@@ -3,6 +3,6 @@ import { function_path_to_name } from "./function_path_to_name.mjs";
 import { marker } from "./marker.mjs";
 export async function data_file_update(f_path) {
   let f_name = function_path_to_name(f_path);
-  let result = await file_js_parse(f_path2);
+  let result = await file_js_parse(f_path);
   marker("1");
 }
