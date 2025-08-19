de136266c60c9dcb6a3dd69e6aab928f54ee3671
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:51:23 -0400

    function_auto_path public\src\undefined_not_is_assert_lambda.mjs

--- public/src/undefined_not_is_assert_lambda.mjs
+++ public/src/undefined_not_is_assert_lambda.mjs
@@ -1,6 +1,6 @@
-import {error} from "./error.mjs";
-import {json_to} from "./json_to.mjs";
-import {undefined_is} from "./undefined_is.mjs";
+import { error } from "./error.mjs";
+import { json_to } from "./json_to.mjs";
+import { undefined_is } from "./undefined_is.mjs";
 export function undefined_not_is_assert_lambda(value, lambda) {
   if (undefined_is(value)) {
     const object = lambda();
