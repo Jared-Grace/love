5cd5dca73c28ad451dead2965e2da616fde175ec
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:27:31 -0400

    function_auto_path public\src\file_js_parse.mjs

--- web/public/src/file_js_parse.mjs
+++ web/public/src/file_js_parse.mjs
@@ -1,8 +1,12 @@
 import { file_read } from "./file_read.mjs";
 import { js_parse } from "./js_parse.mjs";
-
 export async function file_js_parse(f_path) {
   let code = await file_read(f_path);
-    const ast = js_parse(code);
-  return {ast,code,f_path};
+  const ast = js_parse(code);
+  const parsed = {
+    ast,
+    code,
+    f_path,
+  };
+  return parsed;
 }
