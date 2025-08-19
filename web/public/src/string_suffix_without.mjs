fef053f656a18cd7e5b75e4f140956fa7854cbb3
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 13:42:25 -0400

    function_auto_path public\src\string_suffix_without.mjs

--- web/public/src/string_suffix_without.mjs
+++ web/public/src/string_suffix_without.mjs
@@ -1,10 +1,13 @@
+import { not } from "./not.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
 import { error } from "./error.mjs";
 import { string_slice } from "./string_slice.mjs";
 import { string_size } from "./string_size.mjs";
 export function string_suffix_without(s, suffix) {
-  if (!string_ends_with(s, suffix)) {
+  let a = string_ends_with(s, suffix);
+  if (not(a)) {
     error();
   }
-  return string_slice(s, 0, string_size(s) - string_size(suffix));
+  const newLocal = string_slice(s, 0, string_size(s) - string_size(suffix));
+  return newLocal;
 }
