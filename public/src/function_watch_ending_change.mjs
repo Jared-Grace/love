8fc3c17449992363e31ed0c21ef464b5ee2a5d70
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 13:42:04 -0400

    function_auto_path public\src\function_watch_ending_change.mjs

--- public/src/function_watch_ending_change.mjs
+++ public/src/function_watch_ending_change.mjs
@@ -1,3 +1,4 @@
+import { string_suffix_without } from "./string_suffix_without.mjs";
 import { marker } from "./marker.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
@@ -6,7 +7,7 @@ export async function function_watch_ending_change(f_name, suffix) {
   marker("1");
   let { unaliased } = await function_name_unalias(f_name);
   let f_name_wrapped = function_name_combine(unaliased, suffix);
-  function_watch_suffix_change;
+  let result = string_suffix_without(s, suffix2);
   let v = await function_wrap(unaliased, f_name_wrapped);
   return v;
 }
