8e90b0bb16bf85ac791459d428d08244e3ee1280
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 12:48:17 -0400

    function_auto_path public\src\data_file_update.mjs

--- public/src/data_file_update.mjs
+++ public/src/data_file_update.mjs
@@ -16,7 +16,8 @@ export async function data_file_update(f_path) {
   let parsed = await file_js_parse(f_path);
   let { ast } = parsed;
   let i_names = js_identifiers_names(ast);
-  let identifiers = object_property_initialize(data, "identifiers", {});
+  const property_name = "identifiers";
+  let identifiers = object_property_initialize(data, property_name, {});
   function lambda2(i_name) {
     let list = object_property_initialize(identifiers, i_name, []);
     list_add_if_not_includes(list, f_name);
