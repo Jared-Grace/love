c00ab36b498f22eca7b7ac9ada74eb88de0088ba
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 14:29:48 -0400

    de functions_paths

--- public/src/functions_paths.mjs
+++
@@ -1,6 +0,0 @@
-import {functions_path} from "./functions_path.mjs";
-import {folder_read} from "./folder_read.mjs";
-export function functions_paths() {
-  let result = folder_read(functions_path());
-  return result;
-}
