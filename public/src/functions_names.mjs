4546c5c2afc0bb6284fdb8f25f02b804d9e9b167
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 14:28:31 -0400

    function_auto_path public\src\functions_names.mjs

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,13 +1,16 @@
+import { functions_paths } from "./functions_paths.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { function_name_extension } from "./function_name_extension.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { list_map } from "./list_map.mjs";
-
 export function functions_names() {
-  let paths = folder_read(functions_path());
-  let f_names = list_map(paths, (p) =>
-    string_suffix_without(p, function_name_extension()),
-  );
+  let paths = functions_paths();
+  function lambda(p) {
+    let suffix = function_name_extension();
+    let without = string_suffix_without(p, suffix);
+    return without;
+  }
+  let f_names = list_map(paths, lambda);
   return f_names;
 }
