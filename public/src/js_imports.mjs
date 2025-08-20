8598facecb17ca09c1f299f60433886862ea6829
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:15:17 -0400

    function_auto_path public\src\js_imports.mjs

--- public/src/js_imports.mjs
+++ public/src/js_imports.mjs
@@ -12,38 +12,7 @@ import { list_adder_unique } from "./list_adder_unique.mjs";
 import { js_type } from "./js_type.mjs";
 import { each } from "./each.mjs";
 export function js_imports(ast) {
-  let vs = js_imports_all(ast);
-  function lambda2(la) {
-    function lambda(v) {
-      let { node } = v;
-      let { specifiers, source } = node;
-      let a = list_size_1(specifiers);
-      if (not(a)) {
-        return;
-      }
-      let a2 = js_node_type_is(source, "Literal");
-      if (not(a2)) {
-        return;
-      }
-      let { value } = source;
-      let a3 = string_starts_with(value, ".");
-      if (not(a3)) {
-        return;
-      }
-      let specifier = list_single(specifiers);
-      let { imported, local } = specifier;
-      let both = [imported, local];
-      let mapped = list_map_property(both, "name");
-      let unique = list_unique(mapped);
-      ("the names should be the same");
-      let name = list_single(unique);
-      la(name);
-    }
-    each(vs, lambda);
-  }
-  const imports = list_adder_unique(lambda2);
-  return imports;
-  let imports2 = js_imports_declarations(ast2);
+  let imports2 = js_imports_declarations(ast);
   let mapped2 = list_map_property(imports2, "name");
   return mapped2;
 }
