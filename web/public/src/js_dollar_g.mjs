3abbf04696e6a953d0ca63a53058ac869da25d6a
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:27:55 -0400

    function_auto_path public\src\js_dollar_g.mjs

--- web/public/src/js_dollar_g.mjs
+++ web/public/src/js_dollar_g.mjs
@@ -12,6 +12,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { list_first_second } from "./list_first_second.mjs";
+import { list_index_of_next } from "./list_index_of_next.mjs";
 export async function js_dollar_g({
   remaining,
   node,
@@ -37,6 +38,7 @@ export async function js_dollar_g({
     if (js_node_type_is(stack1, "ExpressionStatement")) {
       let unique = js_identifier_unique_ast(ast, property_name);
       let assign = js_declare(unique, parsed);
+      list_index_of_next(stack2, stack1);
       list_add(stack2, assign);
       function lambda() {
         object_replace(stack1, assign);
