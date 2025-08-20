a436e65404a5007882fd7981994751715e007e35
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:26:08 -0400

    function_auto_path public\src\data_files_update.mjs

--- web/public/src/data_files_update.mjs
+++ web/public/src/data_files_update.mjs
@@ -3,14 +3,11 @@ import { data_file_update_inner } from "./data_file_update_inner.mjs";
 import { data_all } from "./data_all.mjs";
 import { functions_paths } from "./functions_paths.mjs";
 import { each_async } from "./each_async.mjs";
-import { marker } from "./marker.mjs";
 export async function data_files_update() {
   let f_paths = functions_paths();
   var d = await data_all();
-  await data_file_update_inner(f_path, d);
-  marker("1");
   async function lambda(f_path) {
-    await data_file_update_inner(f_path2, d2);
+    await data_file_update_inner(f_path, d);
   }
   await each_async(f_paths, lambda);
   await data_save(d);
