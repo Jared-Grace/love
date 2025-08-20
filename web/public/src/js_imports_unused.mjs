bf9f2ae6b2fafa6f6f6c2b2a0f4d56d0ba7b1bf3
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:50:14 -0400

    function_auto_path public\src\js_imports_unused.mjs

--- web/public/src/js_imports_unused.mjs
+++ web/public/src/js_imports_unused.mjs
@@ -1,3 +1,4 @@
+import { js_visit_type } from "./js_visit_type.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { each } from "./each.mjs";
 import { js_imports } from "./js_imports.mjs";
@@ -6,8 +7,7 @@ export function js_imports_unused(ast) {
   marker("1");
   let imports = js_imports(ast);
   function lambda(i) {
-    function lambda2(v) {}
-    js_visit(ast2, lambda2);
+    js_visit_type(ast2, type, function lambda2(v) {});
   }
   each(imports, lambda);
 }
