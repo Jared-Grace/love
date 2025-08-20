8e1e39cf51cccd3c583569393841d62a9ab7b2e2
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 23:17:55 -0400

    function_auto_path public\src\js_imports_unused_remove.mjs

--- web/public/src/js_imports_unused_remove.mjs
+++ web/public/src/js_imports_unused_remove.mjs
@@ -1,9 +1,10 @@
+import { each_async } from "./each_async.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { each } from "./each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_imports_unused } from "./js_imports_unused.mjs";
 import { marker } from "./marker.mjs";
-export function js_imports_unused_remove(ast) {
+export async function js_imports_unused_remove(ast) {
   marker("1");
   let unuseds = js_imports_unused(ast);
   let body = object_property_get(ast, "body");
@@ -12,5 +13,6 @@ export function js_imports_unused_remove(ast) {
     list_remove(body, declaration);
   }
   each(unuseds, lambda);
+  await each_async(list, async function lambda2(item) {});
   return unuseds;
 }
