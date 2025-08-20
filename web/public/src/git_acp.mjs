2acd38654de4bfc86d2886c6547a1257db7c3ba2
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 13:18:04 -0400

    function_auto_path public\src\git_acp.mjs

--- web/public/src/git_acp.mjs
+++ web/public/src/git_acp.mjs
@@ -1,15 +1,8 @@
-import { git_rebase } from "./git_rebase.mjs";
 import { git_commit } from "./git_commit.mjs";
-import { catch_only_async } from "./catch_only_async.mjs";
-import { log_keep } from "./log_keep.mjs";
-import { catch_ignore_async } from "./catch_ignore_async.mjs";
-import { catch_ignore } from "./catch_ignore.mjs";
-import { command_line } from "./command_line.mjs";
 import { git_push } from "./git_push.mjs";
 import { command_line_git } from "./command_line_git.mjs";
 export async function git_acp(message) {
   await command_line_git("add -A");
   await git_commit(message);
-  return;
   await git_push();
 }
