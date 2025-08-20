2dffc29b176eebed06202582b4c5cf3a6f8b0cb6
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 12:57:03 -0400

    function_auto_path public\src\date_now_iso.mjs

--- public/src/date_now_iso.mjs
+++ public/src/date_now_iso.mjs
@@ -1,7 +1,7 @@
 import { date_iso_to } from "./date_iso_to.mjs";
 import { date_now } from "./date_now.mjs";
 export function date_now_iso() {
-  const d = date_now();
-  let now_iso = date_iso_to(d);
+  const now = date_now();
+  let now_iso = date_iso_to(now);
   return now_iso;
 }
