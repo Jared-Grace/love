df873afb06c53bf49da9a1d9ed0a623230099902
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:13:47 -0400

    function_auto_path public\src\js_imports_declarations.mjs

---
+++ web/public/src/js_imports_declarations.mjs
@@ -0,0 +1,45 @@
+import { not } from "./not.mjs";
+import { js_imports_all } from "./js_imports_all.mjs";
+import { js_visit_type } from "./js_visit_type.mjs";
+import { list_size_1 } from "./list_size_1.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+import { list_single } from "./list_single.mjs";
+import { list_map_property } from "./list_map_property.mjs";
+import { list_unique } from "./list_unique.mjs";
+import { list_adder_unique } from "./list_adder_unique.mjs";
+import { js_type } from "./js_type.mjs";
+import { each } from "./each.mjs";
+export function js_imports_declarations(ast) {
+  let vs = js_imports_all(ast);
+  function lambda2(la) {
+    function lambda(v) {
+      let { node } = v;
+      let { specifiers, source } = node;
+      let a = list_size_1(specifiers);
+      if (not(a)) {
+        return;
+      }
+      let a2 = js_node_type_is(source, "Literal");
+      if (not(a2)) {
+        return;
+      }
+      let { value } = source;
+      let a3 = string_starts_with(value, ".");
+      if (not(a3)) {
+        return;
+      }
+      let specifier = list_single(specifiers);
+      let { imported, local } = specifier;
+      let both = [imported, local];
+      let mapped = list_map_property(both, "name");
+      let unique = list_unique(mapped);
+      ("the names should be the same");
+      let name = list_single(unique);
+      la(name);
+    }
+    each(vs, lambda);
+  }
+  const imports = list_adder_unique(lambda2);
+  return imports;
+}
