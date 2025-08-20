c6d9ba6c528c0023df9310b9b9d7d3d8f895e4ba
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:19:17 -0400

    function_auto_path public\src\list_first_second.mjs

--- web/public/src/list_first_second.mjs
+++ web/public/src/list_first_second.mjs
@@ -1,3 +1,4 @@
+import { list_first_second_generic } from "./list_first_second_generic.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
@@ -5,17 +6,6 @@ import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function list_first_second(list) {
   const property_name = "second";
   let value_get = list_second;
-  let first = null;
-  let second = null;
-  if (list_empty_not_is(list)) {
-    first = list_first(list);
-    if (list_multiple_is(list)) {
-      second = value_get(list);
-    }
-  }
-  let result = {
-    first,
-    [property_name]: second,
-  };
+  let result = list_first_second_generic(list, value_get, property_name);
   return result;
 }
