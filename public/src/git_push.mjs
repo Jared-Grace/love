0f969f2bec39a3e6dcc9f66f7d8278e8ca3e0504
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 12:54:30 -0400

    function_auto_path public\src\git_push.mjs

--- public/src/git_push.mjs
+++ public/src/git_push.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { data_property_get } from "./data_property_get.mjs";
 import { date_now_iso } from "./date_now_iso.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
@@ -9,6 +10,7 @@ export async function git_push() {
   const command_git = git_push_command();
   await catch_log_async(lambda);
   async function lambda() {
+    marker("1");
     let now_iso = date_now_iso();
     let property_name = function_name_combine(git_push.name, "when");
     let before_iso = await data_property_get(property_name);
