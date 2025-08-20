19d61f471de4c0bb978d460ccc621d872b4a5ebf
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:48:06 -0400

    function_auto_path public\src\js_imports_unused.mjs

--- web/public/src/js_imports_unused.mjs
+++ web/public/src/js_imports_unused.mjs
@@ -2,5 +2,5 @@ import { js_imports } from "./js_imports.mjs";
 import { marker } from "./marker.mjs";
 export function js_imports_unused(ast) {
   marker("1");
-  let imports = js_imports(ast2);
+  let imports = js_imports(ast);
 }
