eb26bc924fc04900871dee114f45bbccb52e6a1d
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 23:19:27 -0400

    c function_imports_missing_add function_imports_fix

--- web/public/src/function_imports_fix.mjs
+++ web/public/src/function_imports_fix.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { function_imports_add } from "./function_imports_add.mjs";
 import { each } from "./each.mjs";
 import { function_imports_missing } from "./function_imports_missing.mjs";
@@ -7,6 +8,7 @@ import { list_add_first } from "./list_add_first.mjs";
 import { file_js_unparse } from "./file_js_unparse.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 export async function function_imports_fix(f_name) {
+  marker("1");
   let parsed = await function_imports_missing(f_name);
   let { imports_missing, ast } = parsed;
   await function_imports_add(ast, imports_missing);
