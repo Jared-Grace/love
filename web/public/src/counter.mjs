3f50e5c9209e7a191d3ce6b532fc3ea820dd3782
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:53:25 -0400

    function_auto_path public\src\counter.mjs

--- web/public/src/counter.mjs
+++ web/public/src/counter.mjs
@@ -1,4 +1,9 @@
 import { marker } from "./marker.mjs";
-export function counter() {
-  marker("1");
+export function counter(lambda) {
+  let i = 0;
+  lambda(c);
+  function c() {
+    i++;
+  }
+  return i;
 }
