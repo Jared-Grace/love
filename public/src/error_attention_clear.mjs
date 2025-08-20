262ce79c7d023611ffdd597d2d8cfc7654f54d2f
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:56:21 -0400

    function_auto_path public\src\error_attention_clear.mjs

--- public/src/error_attention_clear.mjs
+++ public/src/error_attention_clear.mjs
@@ -2,6 +2,6 @@ import { error_attention_set } from "./error_attention_set.mjs";
 import { marker } from "./marker.mjs";
 export async function error_attention_clear() {
   marker("1");
-  let v = await error_attention_set(null);
+  let v = await error_attention_set("test");
   return v;
 }
