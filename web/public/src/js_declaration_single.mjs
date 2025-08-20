02be8715a31a6ad1c7e77da09fa8aeca148df8f8
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:46:12 -0400

    function_auto_path public\src\js_declaration_single.mjs

--- web/public/src/js_declaration_single.mjs
+++ web/public/src/js_declaration_single.mjs
@@ -1,14 +1,15 @@
-import { log } from "./log.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { list_single } from "./list_single.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
-export function js_declaration_single(parsed) {
-  let nameds = list_adder((la) => {
-    js_visit_type(parsed, "ExportNamedDeclaration", (v) => {
+export function js_declaration_single(ast) {
+  function lambda2(la) {
+    function lambda(v) {
       let { node } = v;
       la(node);
-    });
-  });
+    }
+    js_visit_type(ast, "ExportNamedDeclaration", lambda);
+  }
+  let nameds = list_adder(lambda2);
   let named = list_single(nameds);
   let { declaration } = named;
   return declaration;
