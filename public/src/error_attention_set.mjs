65830fec1106b50abc845f5cb45bd66a0f5c0486
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:56:56 -0400

    function_auto_path public\src\error_attention_set.mjs

--- public/src/error_attention_set.mjs
+++ public/src/error_attention_set.mjs
@@ -1,8 +1,6 @@
-import { log } from "./log.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function error_attention_set(value) {
   async function lambda3(previous) {
-    log("here");
     return value;
   }
   let value2 = await data_transform("error_attention", null, lambda3);
