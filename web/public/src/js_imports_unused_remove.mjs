0d22876d62547ee740e4f548161df2489453749b
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 23:17:32 -0400

    function_auto_path public\src\js_imports_unused_remove.mjs

--- web/public/src/js_imports_unused_remove.mjs
+++ web/public/src/js_imports_unused_remove.mjs
@@ -1,12 +1,15 @@
+import { list_remove } from "./list_remove.mjs";
 import { each } from "./each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_imports_unused } from "./js_imports_unused.mjs";
 import { marker } from "./marker.mjs";
 export function js_imports_unused_remove(ast) {
   marker("1");
-  let unused = js_imports_unused(ast);
+  let unuseds = js_imports_unused(ast);
   let body = object_property_get(ast, "body");
-  function lambda(item) {}
-  each(list, lambda);
-  return unused;
+  function lambda(unused) {
+    list_remove(list, item);
+  }
+  each(unuseds, lambda);
+  return unuseds;
 }
