0b652a07b5453eb8048d69a75f09ae925dddc273
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 23:22:07 -0400

    function_auto_path public\src\js_imports_fix.mjs

--- web/public/src/js_imports_fix.mjs
+++ web/public/src/js_imports_fix.mjs
@@ -1,7 +1,9 @@
+import { js_imports_unused_remove } from "./js_imports_unused_remove.mjs";
 import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
 import { marker } from "./marker.mjs";
 export async function js_imports_fix(ast) {
   marker("1");
+  await js_imports_unused_remove(ast);
   let v = await js_imports_missing_add(ast);
   return v;
 }
