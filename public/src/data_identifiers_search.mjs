4867454efb18b1ed34ac310135871b5163ef8cee
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:33:47 -0400

    function_auto_path public\src\data_identifiers_search.mjs

--- public/src/data_identifiers_search.mjs
+++ public/src/data_identifiers_search.mjs
@@ -2,5 +2,6 @@ import { data_property_get } from "./data_property_get.mjs";
 import { marker } from "./marker.mjs";
 export async function data_identifiers_search(property_name) {
   marker("1");
-  return await data_property_get(property_name);
+  let v = await data_property_get(property_name);
+  return v;
 }
