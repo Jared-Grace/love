b385b4a7b6280a3d3ab908b428cf0668accf6b09
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 13:17:43 -0400

    function_auto_path public\src\git_push.mjs

---
+++ public/src/date_diff_hours.mjs
@@ -0,0 +1,7 @@
+import {date_ms_to_hours} from "./date_ms_to_hours.mjs";
+import {date_diff_ms} from "./date_diff_ms.mjs";
+export function date_diff_hours(now, before) {
+  let ms = date_diff_ms(now, before);
+  const hours = date_ms_to_hours(ms);
+  return hours;
+}
