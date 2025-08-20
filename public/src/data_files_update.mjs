6526a228c312e8d89bd6289472fb637ccc74be73
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:29:07 -0400

    function_auto_path public\src\data_files_update.mjs

--- public/src/data_files_update.mjs
+++ public/src/data_files_update.mjs
@@ -6,10 +6,8 @@ import { functions_paths } from "./functions_paths.mjs";
 import { each_async } from "./each_async.mjs";
 export async function data_files_update() {
   let f_paths = functions_paths();
-  let waited = await list_map_unordered_async(
-    list,
-    async function lambda2(item) {},
-  );
+  async function lambda2(item) {}
+  let waited = await list_map_unordered_async(list, lambda2);
   var d = await data_all();
   async function lambda(f_path) {
     await data_file_update_inner(f_path, d);
