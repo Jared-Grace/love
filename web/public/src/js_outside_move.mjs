b83aaf7e173e6b91097734b8c8da6673ebd47bf7
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 23:22:41 -0400

    function_auto_path public\src\js_outside_move.mjs

--- web/public/src/js_outside_move.mjs
+++ web/public/src/js_outside_move.mjs
@@ -1,16 +1,13 @@
 import { js_imports_fix } from "./js_imports_fix.mjs";
-import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
 import { each } from "./each.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { function_new_declaration } from "./function_new_declaration.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 import { each_async } from "./each_async.mjs";
-import { log } from "./log.mjs";
 import { marker } from "./marker.mjs";
 import { js_declaration_name } from "./js_declaration_name.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
-import { js_unparse } from "./js_unparse.mjs";
 export async function js_outside_move(ast) {
   let { body } = ast;
   let fds = list_filter_property(body, "type", "FunctionDeclaration");
@@ -29,7 +26,7 @@ export async function js_outside_move(ast) {
     list_remove(body, fd);
   }
   each(fds, lambda3);
-  await js_imports_missing_add(ast);
+  await js_imports_fix(ast);
   return;
   let v = await js_imports_fix(ast2);
 }
