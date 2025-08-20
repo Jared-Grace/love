f7d7e23df30793dddf85e535dc4e1c2391e5366a
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 23:21:48 -0400

    function_auto_path public\src\js_imports_fix.mjs

--- web/public/src/js_imports_fix.mjs
+++ web/public/src/js_imports_fix.mjs
@@ -2,5 +2,6 @@ import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
 import { marker } from "./marker.mjs";
 export async function js_imports_fix(ast) {
   marker("1");
-  return await js_imports_missing_add(ast);
+  let v = await js_imports_missing_add(ast);
+  return v;
 }
