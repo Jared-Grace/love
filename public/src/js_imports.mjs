ee19ef4f9f8e1f1e96d289b3027f2eb2d8f3759c
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:15:09 -0400

    function_auto_path public\src\js_imports.mjs

--- public/src/js_imports.mjs
+++ public/src/js_imports.mjs
@@ -44,5 +44,6 @@ export function js_imports(ast) {
   const imports = list_adder_unique(lambda2);
   return imports;
   let imports2 = js_imports_declarations(ast2);
-  let mapped2 = list_map_property(list, property_name);
+  let mapped2 = list_map_property(imports2, "name");
+  return mapped2;
 }
