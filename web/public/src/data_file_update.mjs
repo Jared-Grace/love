b7854c67b55aeb741849934db3078ad5ebba357f
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:31:43 -0400

    function_auto_path public\src\data_file_update.mjs

--- web/public/src/data_file_update.mjs
+++ web/public/src/data_file_update.mjs
@@ -5,8 +5,8 @@ import { data_all } from "./data_all.mjs";
 import { marker } from "./marker.mjs";
 export async function data_file_update(f_path) {
   var d = await data_all();
-  let parsed = await file_js_parse(f_path2);
-  await data_file_update_inner(f_path, d);
+  let parsed = await file_js_parse(f_path);
+  await data_file_update_inner(parsed, d);
   await data_save(d);
   marker("1");
 }
