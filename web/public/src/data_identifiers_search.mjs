13c5c04b2cdef6c610a9afaec23733c579d881ca
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:37:07 -0400

    function_auto_path public\src\data_identifiers_search.mjs

--- web/public/src/data_identifiers_search.mjs
+++ web/public/src/data_identifiers_search.mjs
@@ -7,6 +7,6 @@ export async function data_identifiers_search(s) {
   marker("1");
   let identifiers = await data_property_get("identifiers");
   let list = object_property_get(identifiers, s);
-  let result = list_to_dictionary(f_names_search, function_name_to_path);
-  return list;
+  let result = list_to_dictionary(list, function_name_to_path);
+  return result;
 }
