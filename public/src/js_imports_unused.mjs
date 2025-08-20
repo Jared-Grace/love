408bdece02e78ca7b679e920301432403e8e11cf
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:49:08 -0400

    function_auto_path public\src\js_imports_unused.mjs

--- public/src/js_imports_unused.mjs
+++ public/src/js_imports_unused.mjs
@@ -1,6 +1,8 @@
+import { each } from "./each.mjs";
 import { js_imports } from "./js_imports.mjs";
 import { marker } from "./marker.mjs";
 export function js_imports_unused(ast) {
   marker("1");
   let imports = js_imports(ast);
+  each(list, function lambda(item) {});
 }
