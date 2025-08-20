8e7ab36c52cbb92e51df34df81fa9c272f3f2d98
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 19:03:10 -0400

    function_auto_path public\src\marker_leave.mjs

--- public/src/marker_leave.mjs
+++ public/src/marker_leave.mjs
@@ -1,4 +1,6 @@
+import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
 import { marker } from "./marker.mjs";
-export function marker_leave() {
+export async function marker_leave() {
   marker("1");
+  let v = await function_transform_marker_current(async function lambda(a) {});
 }
