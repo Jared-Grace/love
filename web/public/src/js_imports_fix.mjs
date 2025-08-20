792e97a361a645e1263df778df7ddf2af7863c0a
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 12:54:40 -0400

    mx

--- web/public/src/js_imports_fix.mjs
+++ web/public/src/js_imports_fix.mjs
@@ -3,7 +3,14 @@ import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
 import { marker } from "./marker.mjs";
 export async function js_imports_fix(ast) {
   marker("1");
-  await js_imports_unused_remove(ast);
+  marker("1");
+  let unuseds = js_imports_unused(ast);
+  let body = object_property_get(ast, "body");
+  function lambda(unused) {
+    let declaration = object_property_get(unused, "declaration");
+    list_remove(body, declaration);
+  }
+  each(unuseds, lambda);
   let v = await js_imports_missing_add(ast);
   return v;
 }
