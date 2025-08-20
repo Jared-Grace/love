9fe3e6e7277b3c4b8e50237485afab906b0d9cd1
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 13:03:55 -0400

    function_auto_path public\src\git_push.mjs

--- public/src/git_push.mjs
+++ public/src/git_push.mjs
@@ -1,3 +1,4 @@
+import { date_to } from "./date_to.mjs";
 import { date_iso_to } from "./date_iso_to.mjs";
 import { date_now } from "./date_now.mjs";
 import { data_property_get } from "./data_property_get.mjs";
@@ -15,7 +16,7 @@ export async function git_push() {
     let now_iso = date_iso_to(now);
     let property_name = function_name_combine(git_push.name, "when");
     let before_iso = await data_property_get(property_name);
-    let before = new Date(before_iso);
+    let before = date_to(before_iso);
     await command_line_git(command_git);
     await data_set(date_now_iso, property_name);
   }
