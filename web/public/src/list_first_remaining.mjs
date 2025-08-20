aa2c1c1cd1dc12f549580f2e720d9d5cbc615107
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:17:00 -0400

    c list_first_second list_first_remaining

--- web/public/src/list_first_remaining.mjs
+++ web/public/src/list_first_remaining.mjs
@@ -1,8 +1,10 @@
+import { marker } from "./marker.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function list_first_remaining(list) {
+  marker("1");
   let first = null;
   let second = null;
   if (list_empty_not_is(list)) {
