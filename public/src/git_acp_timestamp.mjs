b5129eb2bd46d9f51764a48b5b8231ef22e6d75b
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 12:34:01 -0400

    function_auto_path public\src\git_acp_timestamp.mjs

--- public/src/git_acp_timestamp.mjs
+++ public/src/git_acp_timestamp.mjs
@@ -1,7 +1,7 @@
+import { date_now_iso } from "./date_now_iso.mjs";
 import { git_acp } from "./git_acp.mjs";
-import { date_now } from "./date_now.mjs";
 import { execSync } from "child_process";
 export async function git_acp_timestamp() {
-  const message = date_now().toISOString();
+  const message = date_now_iso();
   await git_acp(message);
 }
