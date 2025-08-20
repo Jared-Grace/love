1d635cfe22eeef63585796e5212837ff96fb183e
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:17:23 -0400

    function_auto_path public\src\list_first_remaining.mjs

--- web/public/src/list_first_remaining.mjs
+++ web/public/src/list_first_remaining.mjs
@@ -3,6 +3,7 @@ import { list_multiple_is } from "./list_multiple_is.mjs";
 import { list_second } from "./list_second.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
+import { list_skip } from "./list_skip.mjs";
 export function list_first_remaining(list) {
   marker("1");
   let first = null;
@@ -10,7 +11,7 @@ export function list_first_remaining(list) {
   if (list_empty_not_is(list)) {
     first = list_first(list);
     if (list_multiple_is(list)) {
-      second = list_second(list);
+      second = list_skip(list, 1);
     }
   }
   let v = {
