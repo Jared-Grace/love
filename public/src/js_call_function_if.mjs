f20613c44acaee3a762a67c84c36ac02a476ad2b
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 16:06:56 -0400

    function_auto_path public\src\js_node_atomize.mjs

---
+++ public/src/js_call_function_if.mjs
@@ -0,0 +1,12 @@
+import {functions_names_includes} from "./functions_names_includes.mjs";
+import {js_node_type_is} from "./js_node_type_is.mjs";
+export async function js_call_function_if(node, lambda) {
+  let {callee} = node;
+  if (js_node_type_is(callee, "Identifier")) {
+    let {name} = callee;
+    const valid = functions_names_includes(name);
+    if (valid) {
+      await lambda(name);
+    }
+  }
+}
