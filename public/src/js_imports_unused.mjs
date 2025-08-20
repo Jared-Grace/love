eae306fe2a89f9d5b2bda7d8008dbaff87b0206a
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 20:53:40 -0400

    function_auto_path public\src\js_imports_unused.mjs

--- public/src/js_imports_unused.mjs
+++ public/src/js_imports_unused.mjs
@@ -1,3 +1,4 @@
+import { counter } from "./counter.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
@@ -9,6 +10,7 @@ export function js_imports_unused(ast) {
   marker("1");
   let imports = js_imports(ast);
   function lambda(i_name) {
+    let i = counter(function lambda4() {});
     function lambda2(v) {
       let node = object_property_get(v, "node");
       let name = object_property_get(node, "name");
