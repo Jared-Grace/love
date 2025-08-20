b14f97ac5f9ec95022c3955eef0c8d22a19457ff
Grace-Jared <christrosetolife@gmail.com>
2025-08-19 21:28:02 -0400

    function_auto_path public\src\js_dollar_g.mjs

--- web/public/src/js_dollar_g.mjs
+++ web/public/src/js_dollar_g.mjs
@@ -38,7 +38,7 @@ export async function js_dollar_g({
     if (js_node_type_is(stack1, "ExpressionStatement")) {
       let unique = js_identifier_unique_ast(ast, property_name);
       let assign = js_declare(unique, parsed);
-      list_index_of_next(stack2, stack1);
+      let index = list_index_of_next(stack2, stack1);
       list_add(stack2, assign);
       function lambda() {
         object_replace(stack1, assign);
