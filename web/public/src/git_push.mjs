ac2c3c2fadf943ea9d36b9afa0aad42ed52e1ced
Grace-Jared <christrosetolife@gmail.com>
2025-08-20 15:59:57 -0400

    function_auto_path public\src\git_push.mjs

--- web/public/src/git_push.mjs
+++ web/public/src/git_push.mjs
@@ -23,7 +23,7 @@ export async function git_push() {
     let before = date_to(before_iso);
     const hours = date_diff_hours(now, before);
     if (hours < 1) {
-      if (false) {
+      if (true) {
         log("less than 1 hour:");
         log(hours);
       }
