abe18642e94315bd30bb345fbee721a65e2dba52
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:08:47 -0400

    function_auto_path public\src\function_alias_rename.mjs

--- public/src/function_alias_rename.mjs
+++ public/src/function_alias_rename.mjs
@@ -1,4 +1,5 @@
 import { data_get } from "./data_get.mjs";
 export async function function_alias_rename() {
   var { value: aliases, file_path, data } = await data_get("aliases", {});
+  return aliases;
 }
