0f144204b129835f3d7793d5f513c2e38e6961cb
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:11:43 -0400

    function_auto_path public\src\object_property_exists_not.mjs

--- web/public/src/object_property_exists_not.mjs
+++ web/public/src/object_property_exists_not.mjs
@@ -1,7 +1,7 @@
-import {not} from "./not.mjs";
-import {object_property_exists} from "./object_property_exists.mjs";
-export function object_property_exists_not(inverted, f_name) {
-  let result = object_property_exists(inverted, f_name);
+import { not } from "./not.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
+export function object_property_exists_not(object, property_name) {
+  let result = object_property_exists(object, property_name);
   let n = not(result);
   return n;
 }
