09f062873a97b10e425ee26db1519e691f927034
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 15:43:06 -0400

    function_auto_path public\src\js_imports_missing.mjs

--- web/public/src/js_imports_missing.mjs
+++ web/public/src/js_imports_missing.mjs
@@ -4,14 +4,13 @@ import { list_intersect } from "./list_intersect.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { list_difference } from "./list_difference.mjs";
 import { list_concat } from "./list_concat.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { js_imports } from "./js_imports.mjs";
 export function js_imports_missing(ast) {
   let declaration = js_declaration_single(ast);
+  let name = js_declaration_name(declaration);
   let imports = js_imports(ast);
   let identifiers = js_identifiers_names(declaration);
-  let name = js_declaration_name(declaration);
   let imports_self = list_concat(imports, [name]);
   let missing = list_difference(identifiers, imports_self);
   let f_names = functions_names();
