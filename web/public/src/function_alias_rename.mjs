97ea54277570966a82861a90db1f5c4f1cc860b6
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:15:48 -0400

    function_auto_path public\src\function_alias_rename.mjs

--- web/public/src/function_alias_rename.mjs
+++ web/public/src/function_alias_rename.mjs
@@ -1,10 +1,10 @@
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_invert } from "./object_invert.mjs";
 import { data_get } from "./data_get.mjs";
-export async function function_alias_rename(f_name) {
+export async function function_alias_rename(before, after) {
   var { value: aliases, file_path, data } = await data_get("aliases", {});
   let inverted = object_invert(aliases);
-  let n = object_property_exists_not(inverted, f_name);
+  let n = object_property_exists_not(inverted, before);
   if (n) {
     return;
   }
