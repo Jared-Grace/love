a2bd4d5e80edac2019eaa3ba9db145bea983272c
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 13:06:12 -0400

    function_auto_path public\src\marker_leave.mjs

--- web/public/src/marker_leave.mjs
+++ web/public/src/marker_leave.mjs
@@ -1,3 +1,4 @@
+import { list_is_assert } from "./list_is_assert.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 import { log } from "./log.mjs";
 import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
@@ -8,6 +9,7 @@ export async function marker_leave() {
     let { stack } = a;
     let stack4 = list_get_end(stack, 4);
     let stack5 = list_get_end(stack, 5);
+    let result = list_is_assert(list);
     log(stack4);
     log(stack5);
   }
