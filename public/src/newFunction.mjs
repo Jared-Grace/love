7735c51f0c8af4bc11f5518bfb5fe436d61fbf80
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 13:12:32 -0400

    function_auto_path public\src\newFunction.mjs

--- public/src/newFunction.mjs
+++ public/src/newFunction.mjs
@@ -1,6 +1,1 @@
-import {log} from "./log.mjs";
-import {js_unparse} from "./js_unparse.mjs";
-export async function newFunction(next) {
-  let code = await js_unparse(next);
-  log(code);
-}
+export async function newFunction(next) {}
