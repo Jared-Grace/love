50f05f888f976be5d0e43c1e323f583e8b9b2a08
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 14:14:06 -0400

    function_auto_path public\src\add_1.mjs

--- web/public/src/add_1.mjs
+++ web/public/src/add_1.mjs
@@ -2,5 +2,6 @@ import { add } from "./add.mjs";
 import { marker } from "./marker.mjs";
 export function add_1(left, right) {
   marker("1");
-  return add(left, right);
+  let sum = add(left, right);
+  return sum;
 }
