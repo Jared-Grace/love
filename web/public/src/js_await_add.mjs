59c39ecc8b53993dd26cc7043eb50aab24da0e9e
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 13:46:04 -0400

    mm 1 2

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -5,10 +5,10 @@ import { marker } from "./marker.mjs";
 export function js_await_add(ast) {
   async function lambda(v) {
     marker("1");
-    let stack = object_property_get(v, "stack");
     let node = object_property_get(v, "node");
     async function lambda3() {
       marker("2");
+      let stack = object_property_get(v, "stack");
     }
     await js_call_function_if(node, lambda3);
   }
