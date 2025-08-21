50e4b1372d927fc8a7ad2e78c849c07dc5384940
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:11:17 -0400

    de object_property_exists_not

--- web/public/src/function_alias_rename.mjs
+++ web/public/src/function_alias_rename.mjs
@@ -1,9 +1,16 @@
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
+import { not } from "./not.mjs";
+import { object_property_exists } from "./object_property_exists.mjs";
 import { object_invert } from "./object_invert.mjs";
 import { data_get } from "./data_get.mjs";
 export async function function_alias_rename(f_name) {
   var { value: aliases, file_path, data } = await data_get("aliases", {});
   let inverted = object_invert(aliases);
-  object_property_exists_not(inverted, f_name);
-  return inverted;
+  let n = object_property_exists_not(inverted, f_name);
+  return n;
 }
+function object_property_exists_not(inverted, f_name) {
+    let result = object_property_exists(inverted, f_name);
+    let n = not(result);
+    return n;
+}
+
--- web/public/src/object_property_exists_not.mjs
+++
@@ -1,6 +0,0 @@
-import {not} from "./not.mjs";
-import {object_property_exists} from "./object_property_exists.mjs";
-export function object_property_exists_not(inverted, f_name) {
-  let result = object_property_exists(inverted, f_name);
-  let n = not(result);
-}
