4d302f208a0de29e146629bacfb10636f14ac066
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 15:54:15 -0400

    function_auto_path public\src\js_await_add.mjs

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -1,11 +1,11 @@
+import { data_functions_get } from "./data_functions_get.mjs";
 import { js_declaration_single_name } from "./js_declaration_single_name.mjs";
-import { data_property_get } from "./data_property_get.mjs";
 import { js_call_function_if } from "./js_call_function_if.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
 export async function js_await_add(ast) {
-  let functions = await data_property_get("functions");
+  let functions = await data_functions_get();
   let name = js_declaration_single_name(ast);
   marker("1");
   let value2 = object_property_get(functions, property_name);
