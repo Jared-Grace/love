744cd84897052ea32449d8ac6d00c4a36e587e9a
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 13:45:13 -0400

    function_auto_path public\src\marker_up.mjs

---
+++ web/public/src/marker_up.mjs
@@ -0,0 +1,12 @@
+import { marker } from "./marker.mjs";
+import { marker_down_generic } from "./marker_down_generic.mjs";
+import { integer_to } from "./integer_to.mjs";
+export async function marker_up(delta) {
+  marker("1");
+  let v = await marker_down_generic(delta_get);
+  return v;
+  function delta_get() {
+    let i = integer_to(delta);
+    return i;
+  }
+}
