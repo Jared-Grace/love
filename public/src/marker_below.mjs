57a78e284527d2909d9cc38bf99072c00b567bb9
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 14:11:06 -0400

    function_auto_path public\src\marker_below.mjs

--- public/src/marker_below.mjs
+++ public/src/marker_below.mjs
@@ -1,9 +1,8 @@
 import { marker } from "./marker.mjs";
 import { marker_above_generic } from "./marker_above_generic.mjs";
-import { identity } from "./identity.mjs";
 export async function marker_below(code) {
   marker("1");
-  let fn = identity;
+  let fn = add_1;
   let v = await marker_above_generic(fn, code);
   return v;
 }
