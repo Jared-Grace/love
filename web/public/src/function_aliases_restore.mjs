54dac94f457489b212458c82f0387ab199f599a3
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:47:19 -0400

    data_file_update public\src\function_aliases_restore.mjs

--- web/public/src/function_aliases_restore.mjs
+++ web/public/src/function_aliases_restore.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { each } from "./each.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { data_transform } from "./data_transform.mjs";
@@ -5,6 +6,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists_not } from "./object_property_exists_not.mjs";
 import { object_invert } from "./object_invert.mjs";
 export async function function_aliases_restore(before, after) {
+  marker("1");
   async function lambda(aliases) {
     let inverted = object_invert(aliases);
     let n = object_property_exists_not(inverted, before);
