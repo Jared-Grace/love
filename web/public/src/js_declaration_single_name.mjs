47544945b76d72be9e3db2bb70d0692ea483e12d
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 15:43:35 -0400

    function_auto_path public\src\js_imports_missing.mjs

---
+++ web/public/src/js_declaration_single_name.mjs
@@ -0,0 +1,11 @@
+import {js_declaration_name} from "./js_declaration_name.mjs";
+import {js_declaration_single} from "./js_declaration_single.mjs";
+export function js_declaration_single_name(ast) {
+  let declaration = js_declaration_single(ast);
+  let name = js_declaration_name(declaration);
+  let v = {
+    declaration,
+    name
+  };
+  return v;
+}
