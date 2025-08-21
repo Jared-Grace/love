10a109e248b3b71fd8b6d61eb2a0e5822c1216f8
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:56:29 -0400

    function_auto_path public\src\function_rename.mjs

--- web/public/src/function_rename.mjs
+++ web/public/src/function_rename.mjs
@@ -1,8 +1,10 @@
+import { function_alias_rename } from "./function_alias_rename.mjs";
 import { function_delete } from "./function_delete.mjs";
 import { function_copy } from "./function_copy.mjs";
 import { marker } from "./marker.mjs";
 export async function function_rename(f_name_before, f_name_after) {
   await function_copy(f_name_before, f_name_after);
   await function_delete(f_name_before);
+  let v = await function_alias_rename(before, after);
   marker("1");
 }
