fd9d10c62bf5e27a4ef2b3b4880e2438661cfb1a
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 16:00:27 -0400

    function_auto_path public\src\js_visit_type.mjs

--- web/public/src/js_visit_type.mjs
+++ web/public/src/js_visit_type.mjs
@@ -1,9 +1,4 @@
 import { js_visit_types } from "./js_visit_types.mjs";
-import { log } from "./log.mjs";
-import { js_visit } from "./js_visit.mjs";
-import { js_visit_children_get } from "./js_visit_children_get.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
-import { js_node_is } from "./js_node_is.mjs";
 export function js_visit_type(ast, type, lambda$v) {
   js_visit_types(ast, [type], lambda$v);
 }
