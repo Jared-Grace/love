15e056daed3fdf1ec7b9b005f14fd4d08fc04196
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 19:09:14 -0400

    function_auto_path public\src\function_alias_rename.mjs

--- web/public/src/function_alias_rename.mjs
+++ web/public/src/function_alias_rename.mjs
@@ -2,6 +2,6 @@ import { object_invert } from "./object_invert.mjs";
 import { data_get } from "./data_get.mjs";
 export async function function_alias_rename() {
   var { value: aliases, file_path, data } = await data_get("aliases", {});
-  let inverted = object_invert(object);
+  let inverted = object_invert(aliases);
   return aliases;
 }
