f6d9fbe588b5b4d50647c937a89eb20bfd88ae9d
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:02:26 -0400

    mc oc

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -1,3 +1,4 @@
+import { object_copy } from "./object_copy.mjs";
 import { js_node_type_not_is_if } from "./js_node_type_not_is_if.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { log } from "./log.mjs";
@@ -25,6 +26,7 @@ export async function js_await_add(ast) {
       let stack1 = list_get_end_1(stack);
       function lambda4() {
         marker("1");
+        let copy = object_copy(from);
       }
       js_node_type_not_is_if(node, "AwaitExpression", lambda4);
       log(stack1);
