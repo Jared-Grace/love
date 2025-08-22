106e8f53b46a77b2cde27bb9978d9d425e2ee839
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:48:11 -0400

    function_auto_path public\src\functions_log_remove.mjs

--- public/src/functions_log_remove.mjs
+++ public/src/functions_log_remove.mjs
@@ -1,3 +1,4 @@
+import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { list_wait } from "./list_wait.mjs";
 import { function_format } from "./function_format.mjs";
@@ -5,6 +6,7 @@ import { functions_names } from "./functions_names.mjs";
 import { list_map } from "./list_map.mjs";
 export async function functions_log_remove() {
   marker("1");
+  let output = await function_transform(f_name, async function lambda(ast) {});
   let mapped = list_map(await functions_names(), function_format);
   await list_wait(mapped);
 }
