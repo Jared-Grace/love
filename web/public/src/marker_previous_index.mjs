3a5ffa42a53a75b22083f4c2d466f8e4fbe6e3b4
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:54:06 -0400

    function_auto_path public\src\marker_next_delete_multiple.mjs

--- web/public/src/marker_previous_index.mjs
+++ web/public/src/marker_previous_index.mjs
@@ -1,6 +1,4 @@
 import { list_index_of_previous } from "./list_index_of_previous.mjs";
-import { list_index_of_next } from "./list_index_of_next.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function marker_previous_index(a) {
   let { stack2, stack1 } = a;
