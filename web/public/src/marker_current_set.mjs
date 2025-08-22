9145340ac65cd47621c374dea8b7920a4dcbed7f
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:19 -0400

    function_auto_path public\src\list_wait.mjs

--- web/public/src/marker_current_set.mjs
+++ web/public/src/marker_current_set.mjs
@@ -1,4 +1,7 @@
 import { data_transform } from "./data_transform.mjs";
 export async function marker_current_set(name_marker) {
-  await data_transform("marker_current", null, (previous) => name_marker);
+  function lambda(previous) {
+    return name_marker;
+  }
+  await data_transform("marker_current", null, lambda);
 }
