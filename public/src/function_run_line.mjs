47d253469644b56a77b9103ef50bada162d1b108
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:07 -0400

    function_auto_path public\src\json_to.mjs

--- public/src/function_run_line.mjs
+++ public/src/function_run_line.mjs
@@ -1,15 +1,7 @@
 import { function_run } from "./function_run.mjs";
-import { function_run_log } from "./function_run_log.mjs";
 import { function_run_line_parse } from "./function_run_line_parse.mjs";
-import { list_unique_reverse } from "./list_unique_reverse.mjs";
-import { list_copy_reverse } from "./list_copy_reverse.mjs";
-import { list_reverse } from "./list_reverse.mjs";
-import { function_run_git } from "./function_run_git.mjs";
-import { list_unique } from "./list_unique.mjs";
-import { list_add } from "./list_add.mjs";
-import { data_transform } from "./data_transform.mjs";
-import { list_copy } from "./list_copy.mjs";
 export async function function_run_line(line) {
   let { f_name, args } = await function_run_line_parse(line);
-  return await function_run(f_name, args);
+  let v = await function_run(f_name, args);
+  return v;
 }
---
+++ public/src/js_code_statement.mjs
@@ -0,0 +1,4 @@
+export function js_code_statement(code) {
+  let v = code + ";";
+  return v;
+}
