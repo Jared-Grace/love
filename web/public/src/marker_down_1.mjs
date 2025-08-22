c1c0fe60fefce643f9143a33be7c90cfdd6fba6e
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:04:22 -0400

    p

--- web/public/src/marker_down_1.mjs
+++ web/public/src/marker_down_1.mjs
@@ -2,6 +2,6 @@ import { marker_down } from "./marker_down.mjs";
 import { marker } from "./marker.mjs";
 export async function marker_down_1() {
   marker("1");
-  let v = await marker_down(delta);
+  let v = await marker_down(1);
   return v;
 }
