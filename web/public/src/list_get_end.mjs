af643cb1b38f763bbc92c7ef33870e3ea579546e
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:53 -0400

    function_auto_path public\src\list_get_end_1.mjs

--- web/public/src/list_get_end.mjs
+++ web/public/src/list_get_end.mjs
@@ -1,6 +1,5 @@
 import { list_index_last } from "./list_index_last.mjs";
 import { list_get } from "./list_get.mjs";
-import { list_size } from "./list_size.mjs";
 export function list_get_end(stack, index_from_end) {
   const difference = list_index_last(stack) - index_from_end;
   const item = list_get(stack, difference);
