ee501b06875020dfd9f8f1c268dc4679792d4e18
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:42 -0400

    function_auto_path public\src\js_marker_named_ast_arg.mjs

--- web/public/src/js_node_types_includes.mjs
+++ web/public/src/js_node_types_includes.mjs
@@ -1,5 +1,7 @@
 import { list_includes } from "./list_includes.mjs";
 import { js_node_types } from "./js_node_types.mjs";
 export function js_node_types_includes(ast, node_type) {
-  return list_includes(js_node_types(ast), node_type);
+  let list = js_node_types(ast);
+  let includes = list_includes(list, node_type);
+  return includes;
 }
