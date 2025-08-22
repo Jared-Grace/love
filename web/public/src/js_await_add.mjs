9a80f8d38358b19da0bf7beba7863c768899a4d0
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 20:39:12 -0400

    ir property_name name

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -14,7 +14,7 @@ export async function js_await_add(ast) {
     let node = object_property_get(v, "node");
     async function lambda3(name) {
       marker("2");
-      let exists = object_property_exists(functions, property_name);
+      let exists = object_property_exists(functions, name);
       if (false) {
       }
       let stack = object_property_get(v, "stack");
