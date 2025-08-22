c51bc8988956662d7adb6db25b6456e286c6cb41
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:16:02 -0400

    function_auto_path public\src\function_exists_strict.mjs

--- public/src/function_exists_strict.mjs
+++ public/src/function_exists_strict.mjs
@@ -4,6 +4,8 @@ import { function_exists_inner } from "./function_exists_inner.mjs";
 export async function function_exists_strict(f_name) {
   marker("1");
   const f_path = function_name_to_path(f_name);
-  let e = await function_exists_inner(u);
+  let e = await function_exists_inner({
+    f_path,
+  });
   return e;
 }
