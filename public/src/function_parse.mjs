27c931b22353459c74053b3a371e13ac699427e3
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:05 -0400

    function_auto_path public\src\git_commit.mjs

--- public/src/function_parse.mjs
+++ public/src/function_parse.mjs
@@ -1,14 +1,14 @@
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
 import { object_merge } from "./object_merge.mjs";
 export async function function_parse(f_name) {
   const { f_path, unaliased } = await function_name_to_path_unalias(f_name);
   let parsed = await file_js_parse(f_path);
-  return object_merge(
+  let to = object_merge(
     {
       unaliased,
     },
     parsed,
   );
+  return to;
 }
--- public/src/function_run_line_parse.mjs
+++ public/src/function_run_line_parse.mjs
@@ -3,13 +3,15 @@ import { list_add } from "./list_add.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function function_run_line_parse(line) {
   const [f_name, ...args] = line.split(" ");
-  await data_transform("prompts", [], (previous) => {
+  function lambda(previous) {
     list_add(previous, line);
     let unique = list_unique_reverse(previous);
     return unique;
-  });
-  return {
+  }
+  await data_transform("prompts", [], lambda);
+  let v = {
     f_name,
     args,
   };
+  return v;
 }
