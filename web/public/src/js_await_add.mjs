2f5fb28a5eba1ab8081f163603671cef9d150157
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 15:09:44 -0400

    mc l

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -23,6 +23,7 @@ export async function js_await_add(ast) {
       js_function_last_asyncify(stack, async_is);
       let e1 = list_get_end_1(stack);
       marker("1");
+      log(message);
     }
     await js_call_function_if(node, lambda3);
   }
