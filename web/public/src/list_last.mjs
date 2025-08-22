ee9cf99bc4955e0627f0c9f86aed7e756f0c0426
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:54 -0400

    function_auto_path public\src\list_intersect.mjs

--- web/public/src/list_last.mjs
+++ web/public/src/list_last.mjs
@@ -1,9 +1,5 @@
 import { list_empty_not_is_assert } from "./list_empty_not_is_assert.mjs";
-import { assert } from "./assert.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { list_get } from "./list_get.mjs";
 import { list_get_end } from "./list_get_end.mjs";
-import { list_index_last } from "./list_index_last.mjs";
 export function list_last(list) {
   list_empty_not_is_assert(list);
   let last = list_get_end(list, 0);
