e0d397254d8fca3737849eaca229d86afa382931
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:43:13 -0400

    data_file_update public\src\js_log_remove.mjs

--- public/src/js_log_remove.mjs
+++ public/src/js_log_remove.mjs
@@ -2,5 +2,6 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
 export function js_log_remove(ast) {
   marker("1");
-  js_visit_type(ast2, type, function lambda(v) {});
+  function lambda(v) {}
+  js_visit_type(ast2, type, lambda);
 }
