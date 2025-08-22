f2e869c797402bc566978637de22ee60b3b22488
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:44 -0400

    function_auto_path public\src\function_path_to_name.mjs

--- web/public/src/js_code_let_assign.mjs
+++ web/public/src/js_code_let_assign.mjs
@@ -1,5 +1,6 @@
 import { js_code_assign } from "./js_code_assign.mjs";
 import { js_keyword_let } from "./js_keyword_let.mjs";
 export function js_code_let_assign(left, right) {
-  return js_keyword_let() + " " + js_code_assign(left, right) + ";";
+  let v = js_keyword_let() + " " + js_code_assign(left, right) + ";";
+  return v;
 }
