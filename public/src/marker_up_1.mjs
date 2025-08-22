e55c3160458308bd910a4fdf262fba5f5f2f03a2
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 18:08:42 -0400

    function_auto_path public\src\marker_up_1.mjs

--- public/src/marker_up_1.mjs
+++ public/src/marker_up_1.mjs
@@ -2,5 +2,6 @@ import { marker_up } from "./marker_up.mjs";
 import { marker } from "./marker.mjs";
 export async function marker_up_1(delta) {
   marker("1");
-  return await marker_up(delta);
+  let v = await marker_up(delta);
+  return v;
 }
