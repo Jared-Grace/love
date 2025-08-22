7dc0dabea3f2b22ea2fe786e068bce6b1eef236e
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:06 -0400

    function_auto_path public\src\list_adder_unique.mjs

--- public/src/command_line_read.mjs
+++ public/src/command_line_read.mjs
@@ -1,13 +1,15 @@
 import readline from "readline";
 export async function command_line_read(prompt) {
-  return await new Promise((resolve) => {
+  let v = await new Promise(function lambda2(resolve) {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
-    rl.question(prompt, (answer) => {
+    function lambda(answer) {
       rl.close();
       resolve(answer);
-    });
+    }
+    rl.question(prompt, lambda);
   });
+  return v;
 }
