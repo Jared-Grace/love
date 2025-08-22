3381b4c1da5c65558608f80f237b41e4ba9343fe
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:12:15 -0400

    function_auto_path public\src\function_parse_declaration.mjs

---
+++ web/public/src/function_parse_declaration_inner.mjs
@@ -0,0 +1,10 @@
+import {object_merge} from "./object_merge.mjs";
+import {js_declaration_single} from "./js_declaration_single.mjs";
+export function function_parse_declaration_inner(parsed) {
+  let {ast} = parsed;
+  let declaration = js_declaration_single(ast);
+  let to = object_merge({
+    declaration
+  }, parsed);
+  return to;
+}
