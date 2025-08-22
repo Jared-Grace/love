23c00239ee17fcd20823b71bcaae553a0578fea5
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 15:59:58 -0400

    function_auto_path public\src\js_node_type_not_is_if.mjs

--- web/public/src/js_node_type_not_is_if.mjs
+++ web/public/src/js_node_type_not_is_if.mjs
@@ -1,8 +1,8 @@
+import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
 import { marker } from "./marker.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_node_type_not_is_if(node, type, lambda) {
   marker("1");
-  let type_is = js_node_type_is(node, type);
+  let type_is = js_node_type_not_is(node, type);
   if (type_is) {
     lambda();
   }
