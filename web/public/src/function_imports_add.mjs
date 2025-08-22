815355880575c06254dceb1b75fd85f6ebbc7866
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:33 -0400

    function_auto_path public\src\equal_by_async.mjs

--- web/public/src/function_imports_add.mjs
+++ web/public/src/function_imports_add.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { js_code_string } from "./js_code_string.mjs";
@@ -6,7 +5,6 @@ import { folder_current_join } from "./folder_current_join.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
-import { each } from "./each.mjs";
 export async function function_imports_add(ast, imports) {
   let { body } = ast;
   async function lambda(import_) {
