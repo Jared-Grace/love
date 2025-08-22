80e90b24ef48307299c7e6b42d42dfd21b437a5c
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:52 -0400

    function_auto_path public\src\list_filter_property.mjs

--- public/src/list_first_remaining.mjs
+++ public/src/list_first_remaining.mjs
@@ -1,11 +1,6 @@
 import { list_first_second_generic } from "./list_first_second_generic.mjs";
 import { list_skip_1 } from "./list_skip_1.mjs";
 import { marker } from "./marker.mjs";
-import { list_multiple_is } from "./list_multiple_is.mjs";
-import { list_second } from "./list_second.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { list_skip } from "./list_skip.mjs";
 export function list_first_remaining(list) {
   const property_name = "remaining";
   let value_get = list_skip_1;
