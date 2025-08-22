dfe8010be14be82425adb19fd552b9d3ae1f91a4
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:35 -0400

    function_auto_path public\src\file_open.mjs

--- public/src/function_run_log.mjs
+++ public/src/function_run_log.mjs
@@ -1,9 +1,5 @@
-import { trinity_green } from "./trinity_green.mjs";
-import { json_format_to } from "./json_format_to.mjs";
 import { log_keep } from "./log_keep.mjs";
-import { log } from "./log.mjs";
 import { function_run } from "./function_run.mjs";
-import { json_to } from "./json_to.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 export async function function_run_log(f_name, args) {
   let { unaliased } = await function_name_unalias(f_name);
