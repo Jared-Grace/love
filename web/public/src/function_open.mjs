5b0decc0c7d91e86bd9429402e141b6b0cf8b9d4
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:35 -0400

    function_auto_path public\src\file_open.mjs

--- web/public/src/function_open.mjs
+++ web/public/src/function_open.mjs
@@ -1,12 +1,7 @@
 import { marker } from "./marker.mjs";
 import { function_current_set } from "./function_current_set.mjs";
-import { data_transform } from "./data_transform.mjs";
-import { data_save } from "./data_save.mjs";
-import { data_get } from "./data_get.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 import { file_open } from "./file_open.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 export async function function_open(f_name) {
   marker("1");
   const { f_path } = await function_name_to_path_unalias(f_name);
