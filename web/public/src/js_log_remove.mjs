f78dce73d1ca1d5a2e214939f7c3701e51d3117f
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:45:27 -0400

    function_auto_path public\src\js_log_remove.mjs

--- web/public/src/js_log_remove.mjs
+++ web/public/src/js_log_remove.mjs
@@ -5,9 +5,11 @@ import { marker } from "./marker.mjs";
 export function js_log_remove(ast) {
   marker("1");
   function lambda(v) {
-    let { stack } = v;
+    let { stack, node } = v;
     let e1 = list_get_end_1(stack);
-    function lambda3() {}
+    function lambda3() {
+      let name = node;
+    }
     js_node_type_is_if(e1, "ExpressionStatement", lambda3);
   }
   js_visit_type(ast, "CallExpression", lambda);
