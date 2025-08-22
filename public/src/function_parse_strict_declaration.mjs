6a9ff42dc65df3fde7e53854098f10ef511d28b2
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:11:52 -0400

    c function_parse_declaration function_parse_strict_declaration

--- public/src/function_parse_strict_declaration.mjs
+++ public/src/function_parse_strict_declaration.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_parse } from "./function_parse.mjs";
 export async function function_parse_strict_declaration(f_name) {
+  marker("1");
   let parsed = await function_parse(f_name);
   let { ast } = parsed;
   let declaration = js_declaration_single(ast);
