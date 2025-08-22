92416fc33ee074743ca78b34a2f27acd2d7bf0e9
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 20:07:46 -0400

    function_auto_path public\src\js_code_await.mjs

--- web/public/src/js_code_await.mjs
+++ web/public/src/js_code_await.mjs
@@ -1,4 +1,5 @@
+import { js_keyword_await } from "./js_keyword_await.mjs";
 export function js_code_await(code) {
-  let v = "await" + " " + code;
+  let v = js_keyword_await() + " " + code;
   return v;
 }
