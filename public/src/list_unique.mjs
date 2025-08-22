ecbe301dd4b78a4dc54c48aaa40b9e8b15812cfd
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:54:00 -0400

    function_auto_path public\src\list_wait.mjs

--- public/src/list_unique.mjs
+++ public/src/list_unique.mjs
@@ -1,12 +1,15 @@
+import { not } from "./not.mjs";
 import { each } from "./each.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function list_unique(list) {
   let unique = [];
-  each(list, (item) => {
-    if (!list_includes(unique, item)) {
+  function lambda(item) {
+    let a = list_includes(unique, item);
+    if (not(a)) {
       list_add(unique, item);
     }
-  });
+  }
+  each(list, lambda);
   return unique;
 }
