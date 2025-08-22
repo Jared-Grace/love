b0624757e25352f80284d4df647d20843622b9f6
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:04:21 -0400

    function_auto_path public\src\marker_down_1.mjs

--- web/public/src/marker_down_1.mjs
+++ web/public/src/marker_down_1.mjs
@@ -1,6 +1,6 @@
 import { marker_down } from "./marker_down.mjs";
 import { marker } from "./marker.mjs";
-export async function marker_down_1(delta) {
+export async function marker_down_1() {
   marker("1");
   let v = await marker_down(delta);
   return v;
