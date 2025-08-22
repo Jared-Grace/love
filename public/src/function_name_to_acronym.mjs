a11dcae27c9dcdb0b123c55f665a6ec2875d632b
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:33 -0400

    function_auto_path public\src\equal_not.mjs

--- public/src/function_name_to_acronym.mjs
+++ public/src/function_name_to_acronym.mjs
@@ -1,11 +1,7 @@
 import { list_join_empty } from "./list_join_empty.mjs";
-import { string_empty } from "./string_empty.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
 import { function_name_to_parts } from "./function_name_to_parts.mjs";
-import { function_name_separator } from "./function_name_separator.mjs";
-import { string_split } from "./string_split.mjs";
-import { list_join } from "./list_join.mjs";
 export function function_name_to_acronym(f_name) {
   let parts = function_name_to_parts(f_name);
   let letters = list_map(parts, list_first);
