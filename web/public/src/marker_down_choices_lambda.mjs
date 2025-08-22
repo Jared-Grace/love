6f0b993ab7ae4cd499cbf3323fd16a575a5e7c40
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:54:06 -0400

    function_auto_path public\src\marker_leave.mjs

--- web/public/src/marker_down_choices_lambda.mjs
+++ web/public/src/marker_down_choices_lambda.mjs
@@ -1,14 +1,9 @@
-import { list_map_property_to } from "./list_map_property_to.mjs";
-import { list_map_property } from "./list_map_property.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { js_declaration_to_block_body } from "./js_declaration_to_block_body.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
-import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_stack_list_block_is } from "./js_stack_list_block_is.mjs";
-import { list_remove } from "./list_remove.mjs";
-import { marker } from "./marker.mjs";
 export function marker_down_choices_lambda({ stack2, stack1, ast }) {
   "not using this as of now";
   let declaration = js_declaration_single(ast);
