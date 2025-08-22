37cc1c4ea33bb89737428ffa22dc6dbd0c588a8c
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:10 -0400

    function_auto_path public\src\function_alias_delete.mjs

--- public/src/list_get_end_3.mjs
+++ public/src/list_get_end_3.mjs
@@ -1,4 +1,5 @@
 import { list_get_end } from "./list_get_end.mjs";
 export function list_get_end_3(stack) {
-  return list_get_end(stack, 3);
+  let item = list_get_end(stack, 3);
+  return item;
 }
