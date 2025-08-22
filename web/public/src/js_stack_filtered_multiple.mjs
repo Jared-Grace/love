89d4bc76df176268d27c75831f6f8fadce488b60
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:57 -0400

    function_auto_path public\src\integer_is.mjs

--- web/public/src/js_stack_filtered_multiple.mjs
+++ web/public/src/js_stack_filtered_multiple.mjs
@@ -1,8 +1,4 @@
 import { js_node_types_is } from "./js_node_types_is.mjs";
-import { list_any } from "./list_any.mjs";
-import { list_all } from "./list_all.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
-import { js_node_is } from "./js_node_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function js_stack_filtered_multiple(stack, types) {
   function lambda(item) {
--- web/public/src/js_stack_last_multiple.mjs
+++ web/public/src/js_stack_last_multiple.mjs
@@ -1,10 +1,5 @@
 import { list_last_or_null } from "./list_last_or_null.mjs";
 import { js_stack_filtered_multiple } from "./js_stack_filtered_multiple.mjs";
-import { js_stack_filtered } from "./js_stack_filtered.mjs";
-import { list_last } from "./list_last.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
-import { js_node_is } from "./js_node_is.mjs";
-import { list_filter } from "./list_filter.mjs";
 export function js_stack_last_multiple(stack, types) {
   let filtered = js_stack_filtered_multiple(stack, types);
   let last = list_last_or_null(filtered);
