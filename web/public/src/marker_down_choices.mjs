0dbadf4f0772d8485dc9cefede4d13b1fe30e213
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:54:04 -0400

    function_auto_path public\src\marker_index.mjs

--- web/public/src/marker_down_choices.mjs
+++ web/public/src/marker_down_choices.mjs
@@ -1,25 +1,9 @@
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
-import { list_map } from "./list_map.mjs";
-import { marker } from "./marker.mjs";
 import { marker_down_choices_lambda } from "./marker_down_choices_lambda.mjs";
-import { list_is_assert } from "./list_is_assert.mjs";
-import { list_insert } from "./list_insert.mjs";
-import { assert } from "./assert.mjs";
-import { list_get_end } from "./list_get_end.mjs";
-import { list_add } from "./list_add.mjs";
 import { list_is } from "./list_is.mjs";
-import { list_get } from "./list_get.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 import { list_map_property } from "./list_map_property.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
-import { js_stack_list_block_is } from "./js_stack_list_block_is.mjs";
-import { js_visit } from "./js_visit.mjs";
-import { list_adder } from "./list_adder.mjs";
-import { list_remove } from "./list_remove.mjs";
-import { marker_next_get } from "./marker_next_get.mjs";
 import { function_transform_marker } from "./function_transform_marker.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
-import { log } from "./log.mjs";
 import { js_unparse } from "./js_unparse.mjs";
 export async function marker_down_choices() {
   let f_name = await data_function_current_get();
--- web/public/src/marker_index.mjs
+++ web/public/src/marker_index.mjs
@@ -1,6 +1,4 @@
 import { marker } from "./marker.mjs";
-import { list_index_of_previous } from "./list_index_of_previous.mjs";
-import { list_index_of_next } from "./list_index_of_next.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function marker_index(a) {
