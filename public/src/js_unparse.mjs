3098fbba439263207459788e7b51f844081c293b
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:58 -0400

    function_auto_path public\src\js_block_insert.mjs

--- public/src/js_unparse.mjs
+++ public/src/js_unparse.mjs
@@ -4,12 +4,8 @@ import { error_json } from "./error_json.mjs";
 import { js_visit_filter } from "./js_visit_filter.mjs";
 import { js_unparse_inner } from "./js_unparse_inner.mjs";
 import { promise_not_is } from "./promise_not_is.mjs";
-import { js_visit_each_async } from "./js_visit_each_async.mjs";
 import { error } from "./error.mjs";
-import { log } from "./log.mjs";
-import { import_install } from "./import_install.mjs";
-import { json_format_to } from "./json_format_to.mjs";
-export function js_unparse(ast) {
+export async function js_unparse(ast) {
   let a2 = promise_not_is(ast);
   if (not(a2)) {
     error();
--- public/src/js_visit_type_each_async.mjs
+++ public/src/js_visit_type_each_async.mjs
@@ -2,6 +2,10 @@ import { list_adder } from "./list_adder.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export async function js_visit_type_each_async(ast, type, lambda) {
-  let vs = list_adder((la) => js_visit_type(ast, type, la));
+  function lambda2(la) {
+    let v = js_visit_type(ast, type, la);
+    return v;
+  }
+  let vs = list_adder(lambda2);
   await each_async(vs, lambda);
 }
