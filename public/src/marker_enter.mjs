2912e8d5c1bfdb8819139710ceb4c162cb18ebc5
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 14:51:21 -0400

    function_auto_path public\src\marker_enter.mjs

--- public/src/marker_enter.mjs
+++ public/src/marker_enter.mjs
@@ -1,9 +1,10 @@
+import { list_get } from "./list_get.mjs";
+import { marker_next_index } from "./marker_next_index.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 import { js_declaration_to_block_body } from "./js_declaration_to_block_body.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { list_remove } from "./list_remove.mjs";
-import { marker_next_get } from "./marker_next_get.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 import { js_types_function_includes } from "./js_types_function_includes.mjs";
 import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
@@ -11,8 +12,9 @@ import { marker } from "./marker.mjs";
 export async function marker_enter() {
   async function lambda(a) {
     marker("1");
-    let n = marker_next_get(a);
-    let { next, stack1, stack2 } = n;
+    let ni = marker_next_index(a);
+    let { index, stack1, stack2 } = ni;
+    let next = list_get(stack2, index);
     let body = null;
     let nt = js_node_type(next);
     if (js_types_function_includes(nt)) {
