1d06d7269c368ce94603abfdc7ce30d5391a17b6
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:11:51 -0400

    function_auto_path public\src\function_parse_strict_declaration.mjs

---
+++ public/src/function_parse_strict_declaration.mjs
@@ -0,0 +1,15 @@
+import { object_merge } from "./object_merge.mjs";
+import { js_declaration_single } from "./js_declaration_single.mjs";
+import { function_parse } from "./function_parse.mjs";
+export async function function_parse_strict_declaration(f_name) {
+  let parsed = await function_parse(f_name);
+  let { ast } = parsed;
+  let declaration = js_declaration_single(ast);
+  let to = object_merge(
+    {
+      declaration,
+    },
+    parsed,
+  );
+  return to;
+}
