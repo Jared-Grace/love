7ab56418a50a9aad9acbcbe9752dfd06db974806
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:57 -0400

    function_auto_path public\src\list_next.mjs

--- web/public/src/list_replace.mjs
+++ web/public/src/list_replace.mjs
@@ -1,7 +1,4 @@
 import { list_insert_generic } from "./list_insert_generic.mjs";
-import { list_splice } from "./list_splice.mjs";
-import { number_is } from "./number_is.mjs";
-import { assert } from "./assert.mjs";
 export function list_replace(list, index, value) {
   const delete_count = 1;
   list_insert_generic(index, list, delete_count, value);
