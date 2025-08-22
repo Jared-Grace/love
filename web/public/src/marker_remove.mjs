fa2a37b082de8b7a4207db6c63fbf4c59f49ca0c
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:54:07 -0400

    function_auto_path public\src\marker_param_add.mjs

--- web/public/src/marker_remove.mjs
+++ web/public/src/marker_remove.mjs
@@ -1,8 +1,6 @@
 import { function_transform_marker_all } from "./function_transform_marker_all.mjs";
-import { function_transform_marker } from "./function_transform_marker.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 import { list_remove } from "./list_remove.mjs";
-import { log } from "./log.mjs";
 export async function marker_remove() {
   let f_name = await data_function_current_get();
   await function_transform_marker_all(f_name, lambda);
