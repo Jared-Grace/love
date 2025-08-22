a43b653b8380ee8a06d0524b0cc17205b9aa9d6c
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:02 -0400

    function_auto_path public\src\js_dollar_task_each_code_own_fn.mjs

--- public/src/js_dollar_a.mjs
+++ public/src/js_dollar_a.mjs
@@ -1,14 +1,9 @@
 import { js_dollar_a_generic } from "./js_dollar_a_generic.mjs";
-import { list_add } from "./list_add.mjs";
 import { each } from "./each.mjs";
 import { object_replace } from "./object_replace.mjs";
-import { log } from "./log.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { js_identifiers_named } from "./js_identifiers_named.mjs";
 import { list_remove_multiple } from "./list_remove_multiple.mjs";
-import { list_next } from "./list_next.mjs";
-import { list_is } from "./list_is.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_dollar_a({ stack1, stack2, ast, afters }) {
   js_dollar_a_generic({
     stack1,
--- public/src/js_imports_declarations.mjs
+++ public/src/js_imports_declarations.mjs
@@ -1,7 +1,6 @@
 import { marker } from "./marker.mjs";
 import { not } from "./not.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
-import { js_visit_type } from "./js_visit_type.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
@@ -9,7 +8,6 @@ import { list_single } from "./list_single.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_unique } from "./list_unique.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
-import { js_type } from "./js_type.mjs";
 import { each } from "./each.mjs";
 export function js_imports_declarations(ast) {
   marker("1");
