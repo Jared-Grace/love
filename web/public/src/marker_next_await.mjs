cec45a1f4d6adff71c50f26a5d12939bf2b839ba
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 20:17:07 -0400

    function_auto_path public\src\marker_next_await.mjs

--- web/public/src/marker_next_await.mjs
+++ web/public/src/marker_next_await.mjs
@@ -4,6 +4,7 @@ import { list_adder_async } from "./list_adder_async.mjs";
 import { marker_next_get } from "./marker_next_get.mjs";
 import { function_transform_marker } from "./function_transform_marker.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
+import { js_code_await } from "./js_code_await.mjs";
 export async function marker_next_await() {
   let f_name = await data_function_current_get();
   async function lambda2(la) {
@@ -11,6 +12,7 @@ export async function marker_next_await() {
     async function lambda(a) {
       let { next } = marker_next_get(a);
       js_node_type_is_assert(next, "ExpressionStatement");
+      let code_expression = js_code_await("a");
       let expression = js_parse_expression(code_expression);
       la(next);
     }
