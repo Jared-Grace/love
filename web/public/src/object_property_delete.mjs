4291ac0d4e45bdae6fa8daedfdd7033107ac78bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:17 -0400

    function_auto_path public\src\function_parse.mjs

--- web/public/src/object_property_delete.mjs
+++
@@ -1,6 +0,0 @@
-import { json_to } from "./json_to.mjs";
-import { error } from "./error.mjs";
-import { undefined_is } from "./undefined_is.mjs";
-export function object_property_delete(object, property_name) {
-  delete object[property_name];
-}
--- web/public/src/object_property_from.mjs
+++ web/public/src/object_property_from.mjs
@@ -1,9 +1,6 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export function object_property_from(to, property_name, from) {
-  object_property_set(
-    to,
-    property_name,
-    object_property_get(from, property_name),
-  );
+  let value = object_property_get(from, property_name);
+  object_property_set(to, property_name, value);
 }
