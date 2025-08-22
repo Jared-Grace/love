bdcf324f809d218d45813235e664d5259ea21e01
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:53 -0400

    function_auto_path public\src\list_get_end_1.mjs

--- web/public/src/list_first_second.mjs
+++ web/public/src/list_first_second.mjs
@@ -1,8 +1,5 @@
 import { list_first_second_generic } from "./list_first_second_generic.mjs";
-import { list_multiple_is } from "./list_multiple_is.mjs";
 import { list_second } from "./list_second.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function list_first_second(list) {
   const property_name = "second";
   let value_get = list_second;
