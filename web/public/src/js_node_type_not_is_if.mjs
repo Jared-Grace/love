5cc727479bcc6648d47eb0feb3b593eb3712f114
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:00:12 -0400

    md 1

--- web/public/src/js_node_type_not_is_if.mjs
+++ web/public/src/js_node_type_not_is_if.mjs
@@ -1,8 +1,8 @@
 import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
 import { marker } from "./marker.mjs";
 export function js_node_type_not_is_if(node, type, lambda) {
-  marker("1");
   let type_is = js_node_type_not_is(node, type);
+  marker("1");
   if (type_is) {
     lambda();
   }
