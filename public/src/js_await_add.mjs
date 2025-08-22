d0293519976488159c5288455c344894fce1e96a
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 14:55:35 -0400

    md 1

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -7,8 +7,8 @@ import { js_call_function_if } from "./js_call_function_if.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { marker } from "./marker.mjs";
 export async function js_await_add(ast) {
-  marker("1");
   let functions = await data_functions_get();
+  marker("1");
   let name = js_declaration_single_name(ast);
   let f = object_property_get(functions, name);
   async function lambda(v) {
