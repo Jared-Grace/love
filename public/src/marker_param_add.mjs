af2582c6a2320e0e701d1181504080f741bf8b72
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 20:33:47 -0400

    function_auto_path public\src\marker_param_add.mjs

--- public/src/marker_param_add.mjs
+++ public/src/marker_param_add.mjs
@@ -1,31 +1,15 @@
 import { js_declaration_param_add } from "./js_declaration_param_add.mjs";
 import { js_stack_last_function } from "./js_stack_last_function.mjs";
-import { js_call_new } from "./js_call_new.mjs";
-import { js_stack_declaration_asyncify } from "./js_stack_declaration_asyncify.mjs";
-import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
-import { list_insert } from "./list_insert.mjs";
-import { marker } from "./marker.mjs";
-import { js_unparse } from "./js_unparse.mjs";
 import { function_transform_marker } from "./function_transform_marker.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
-import { marker_next_index } from "./marker_next_index.mjs";
-export async function marker_param_add(f_name_call) {
+export async function marker_param_add(param_name) {
   let f_name_current = await data_function_current_get();
   async function lambda2(la) {
     await function_transform_marker(f_name_current, lambda);
     async function lambda(a) {
       let f = js_stack_last_function(stack);
-      js_declaration_param_add(declaration2, param_name);
-      marker("1");
-      let { stack, stack2, index, ast } = marker_next_index(a);
-      let { declaration, parsed } = await js_call_new(f_name_call, ast);
-      marker("2");
-      list_insert(stack2, index, parsed);
-      await js_imports_missing_add(ast);
-      let output = await js_unparse(parsed);
-      la(output);
-      js_stack_declaration_asyncify(stack, declaration);
+      js_declaration_param_add(f, param_name);
     }
   }
   let list = list_adder_async(lambda2);
