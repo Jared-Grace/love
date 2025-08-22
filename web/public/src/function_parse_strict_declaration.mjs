87f4055be2d85966ca03847c8f85afb89786c0bd
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:13:09 -0400

    function_auto_path public\src\function_parse_strict_declaration.mjs

--- web/public/src/function_parse_strict_declaration.mjs
+++ web/public/src/function_parse_strict_declaration.mjs
@@ -1,10 +1,10 @@
+import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
-import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 import { function_parse_declaration_inner } from "./function_parse_declaration_inner.mjs";
 import { marker } from "./marker.mjs";
 export async function function_parse_strict_declaration(f_name) {
   marker("1");
-  const { f_path, unaliased } = await function_name_to_path_unalias(f_name);
+  const f_path = function_name_to_path(unaliased);
   let parsed = await file_js_parse(f_path);
   let to = function_parse_declaration_inner(parsed);
   return to;
