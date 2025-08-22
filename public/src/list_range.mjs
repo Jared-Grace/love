bccf6eb0f3302089eca9dd02fcdcb8c2f4de9233
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:57 -0400

    function_auto_path public\src\list_single.mjs

--- public/src/list_range.mjs
+++ public/src/list_range.mjs
@@ -1,4 +1,5 @@
 import { list_slice } from "./list_slice.mjs";
 export function list_range(list, a, b) {
-  return list_slice(list, a, b + 1);
+  let result = list_slice(list, a, b + 1);
+  return result;
 }
