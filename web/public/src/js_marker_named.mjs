0acbb3469beaba256c091cc17fb70c5153a161af
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:50 -0400

    function_auto_path public\src\function_format.mjs

--- web/public/src/js_marker_named.mjs
+++ web/public/src/js_marker_named.mjs
@@ -1,14 +1,4 @@
-import { log } from "./log.mjs";
 import { js_marker_name_get } from "./js_marker_name_get.mjs";
-import { error } from "./error.mjs";
-import { list_is } from "./list_is.mjs";
-import { list_first } from "./list_first.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
-import { marker } from "./marker.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
-import { js_node_is } from "./js_node_is.mjs";
-import { list_get_end } from "./list_get_end.mjs";
-import { string_is } from "./string_is.mjs";
 export function js_marker_named(v, marker_name) {
   let marker_name_actual = js_marker_name_get(v);
   if (marker_name_actual !== marker_name) {
