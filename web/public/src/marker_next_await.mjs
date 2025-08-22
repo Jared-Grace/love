1928b2bd78822a080714a0b1cbee708f9f4f6ccc
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 20:10:02 -0400

    function_auto_path public\src\marker_next_await.mjs

---
+++ web/public/src/marker_next_await.mjs
@@ -0,0 +1,18 @@
+import { marker_current_set } from "./marker_current_set.mjs";
+import { js_marker_insert } from "./js_marker_insert.mjs";
+import { marker } from "./marker.mjs";
+import { marker_next_index } from "./marker_next_index.mjs";
+import { function_transform_marker } from "./function_transform_marker.mjs";
+import { data_function_current_get } from "./data_function_current_get.mjs";
+export async function marker_next_await() {
+  marker("1");
+  let f_name = await data_function_current_get();
+  let v = await function_transform_marker(f_name, lambda);
+  return v;
+  async function lambda(a) {
+    let { index, stack2 } = marker_next_index(a);
+    let name = "2";
+    await js_marker_insert(name, stack2, index);
+    marker_current_set(name);
+  }
+}
