dd32ce5f91ba142388eef1f3c1369f80dcd3dd44
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:53 -0400

    function_auto_path public\src\list_get_end_1.mjs

--- public/src/list_index_of_previous.mjs
+++ public/src/list_index_of_previous.mjs
@@ -1,5 +1,4 @@
 import { list_index_of_delta } from "./list_index_of_delta.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 export function list_index_of_previous(list, item) {
   const delta = -1;
   const index_previous = list_index_of_delta(list, item, delta);
