f0bb4c44ee52c877273760749c74489900b82220
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:49 -0400

    function_auto_path public\src\js_visit_type_each_async.mjs

--- web/public/src/list_adder_multiple.mjs
+++ web/public/src/list_adder_multiple.mjs
@@ -1,5 +1,4 @@
 import { list_adder_generic } from "./list_adder_generic.mjs";
-import { list_add } from "./list_add.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 export function list_adder_multiple(lambda) {
   let fn = list_add_multiple;
