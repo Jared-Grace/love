dff22e3598c259a9dd07833f470193d6b00ea337
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 14:56:37 -0400

    function_auto_path public\src\marker_next_delete_multiple.mjs

--- public/src/marker_next_delete_multiple.mjs
+++ public/src/marker_next_delete_multiple.mjs
@@ -1,7 +1,9 @@
+import { each_async } from "./each_async.mjs";
 import { marker_next_delete } from "./marker_next_delete.mjs";
 import { marker } from "./marker.mjs";
 export async function marker_next_delete_multiple() {
   marker("1");
+  await each_async(list, async function lambda(item) {});
   let v = await marker_next_delete();
   return v;
 }
