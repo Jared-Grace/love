ea4670a334e1a827a5271237966cf4886cccc559
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:47 -0400

    function_auto_path public\src\js_stack_filtered_multiple_each.mjs

--- web/public/src/js_visit.mjs
+++ web/public/src/js_visit.mjs
@@ -1,11 +1,12 @@
+import { not } from "./not.mjs";
 import { promise_not_is } from "./promise_not_is.mjs";
 import { error } from "./error.mjs";
-import { promise_is } from "./promise_is.mjs";
 import { visit_filter } from "./visit_filter.mjs";
 import { js_visit_children_get } from "./js_visit_children_get.mjs";
 import { js_visit_filter } from "./js_visit_filter.mjs";
 export function js_visit(ast, lambda$v) {
-  if (!promise_not_is(ast)) {
+  let a = promise_not_is(ast);
+  if (not(a)) {
     error();
   }
   visit_filter(ast, js_visit_children_get, js_visit_filter, lambda$v);
