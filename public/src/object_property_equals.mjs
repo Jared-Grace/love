92414a503f17903df29795602bedb7474c61ebc5
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:16 -0400

    function_auto_path public\src\js_code_return.mjs

--- public/src/object_property_equals.mjs
+++ public/src/object_property_equals.mjs
@@ -1,4 +1,5 @@
 import { object_property_get } from "./object_property_get.mjs";
 export function object_property_equals(item, property_name, property_value) {
-  return object_property_get(item, property_name) === property_value;
+  let v = object_property_get(item, property_name) === property_value;
+  return v;
 }
