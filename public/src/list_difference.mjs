2717e69c7e079f300ab4b95a00c96d0d92a79ccc
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:16 -0400

    function_auto_path public\src\js_parse_statement_module.mjs

--- public/src/list_difference.mjs
+++ public/src/list_difference.mjs
@@ -1,12 +1,17 @@
+import { not } from "./not.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function list_difference(list, other) {
-  return list_adder((la) => {
-    each(list, (l) => {
-      if (!list_includes(other, l)) {
+  function lambda2(la) {
+    function lambda(l) {
+      let a = list_includes(other, l);
+      if (not(a)) {
         la(l);
       }
-    });
-  });
+    }
+    each(list, lambda);
+  }
+  let list2 = list_adder(lambda2);
+  return list2;
 }
