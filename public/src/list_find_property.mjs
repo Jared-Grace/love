485d1e9a597c9644f7af3d631bf88d3595222019
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:51 -0400

    function_auto_path public\src\list_find_property.mjs

--- public/src/list_find_property.mjs
+++ public/src/list_find_property.mjs
@@ -2,5 +2,6 @@ import { object_property_equals_lambda } from "./object_property_equals_lambda.m
 import { list_find } from "./list_find.mjs";
 export function list_find_property(list, property_name, property_value) {
   let filter = object_property_equals_lambda(property_name, property_value);
-  return list_find(list, filter);
+  let first = list_find(list, filter);
+  return first;
 }
