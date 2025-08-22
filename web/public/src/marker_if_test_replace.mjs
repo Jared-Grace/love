88aefc52a20ee796ac24d292acfa43dfe95306b2
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 13:40:05 -0400

    c ml marker_if_test_replace

---
+++ web/public/src/marker_if_test_replace.mjs
@@ -0,0 +1,25 @@
+import { list_insert } from "./list_insert.mjs";
+import { list_remove } from "./list_remove.mjs";
+import { list_get_end_2 } from "./list_get_end_2.mjs";
+import { list_get_end_1 } from "./list_get_end_1.mjs";
+import { list_index_of } from "./list_index_of.mjs";
+import { list_is_assert } from "./list_is_assert.mjs";
+import { list_get_end } from "./list_get_end.mjs";
+import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
+import { marker } from "./marker.mjs";
+export async function marker_if_test_replace() {
+  marker("1");
+  async function lambda(a) {
+    let { stack } = a;
+    let stack4 = list_get_end(stack, 4);
+    let stack5 = list_get_end(stack, 5);
+    list_is_assert(stack5);
+    let index = list_index_of(stack5, stack4);
+    let stack1 = list_get_end_1(stack);
+    let stack2 = list_get_end_2(stack);
+    list_remove(stack2, stack1);
+    list_insert(stack5, index, stack1);
+  }
+  let v = await function_transform_marker_current(lambda);
+  return v;
+}
