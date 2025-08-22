6cd43685c578dfe6a1050b7b9fea560cadc901bf
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:53 -0400

    function_auto_path public\src\list_insert_generic.mjs

--- public/src/list_index_of_next.mjs
+++ public/src/list_index_of_next.mjs
@@ -1,6 +1,6 @@
 import { list_index_of_delta } from "./list_index_of_delta.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 export function list_index_of_next(list, item) {
   const delta = 1;
-  return list_index_of_delta(list, item, delta);
+  let index_next = list_index_of_delta(list, item, delta);
+  return index_next;
 }
