6ef9b0ccdee1be383ac2d94264a2d6ad44ee2be9
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:14 -0400

    function_auto_path public\src\function_identifier_replace_suffix_add.mjs

--- web/public/src/marker_next_index.mjs
+++ web/public/src/marker_next_index.mjs
@@ -1,13 +1,13 @@
 import { list_index_of_next } from "./list_index_of_next.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function marker_next_index(a) {
   let { stack2, stack1 } = a;
   let index = list_index_of_next(stack2, stack1);
-  return object_merge(
+  let to = object_merge(
     {
       index,
     },
     a,
   );
+  return to;
 }
