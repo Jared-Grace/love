c7a6a8f7f9f6d9a67f0f779bbe738ea4bb66a8ba
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:06 -0400

    function_auto_path public\src\list_adder_unique.mjs

--- web/public/src/js_stack_filtered.mjs
+++ web/public/src/js_stack_filtered.mjs
@@ -1,7 +1,10 @@
 import { js_node_type_is } from "./js_node_type_is.mjs";
-import { js_node_is } from "./js_node_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function js_stack_filtered(stack, type) {
-  let filtered = list_filter(stack, (n) => js_node_type_is(n, type));
+  function lambda(n) {
+    let type_is = js_node_type_is(n, type);
+    return type_is;
+  }
+  let filtered = list_filter(stack, lambda);
   return filtered;
 }
