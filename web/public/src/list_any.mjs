8bc516170811e6780ab9167345672e98bf589503
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:51 -0400

    function_auto_path public\src\list_adder_generic.mjs

--- web/public/src/list_any.mjs
+++ web/public/src/list_any.mjs
@@ -1,8 +1,7 @@
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { list_size } from "./list_size.mjs";
 export function list_any(list, lambda$item) {
   let filtered = list_filter(list, lambda$item);
-  return list_empty_not_is(filtered);
+  let ne = list_empty_not_is(filtered);
+  return ne;
 }
--- web/public/src/list_empty_not_is.mjs
+++ web/public/src/list_empty_not_is.mjs
@@ -1,5 +1,7 @@
+import { not } from "./not.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 export function list_empty_not_is(list) {
-  const ne = !list_empty_is(list);
+  let a = list_empty_is(list);
+  const ne = not(a);
   return ne;
 }
