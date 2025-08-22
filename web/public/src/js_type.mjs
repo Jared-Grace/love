0c74817a2b024281d8623f4857f2039143d0764e
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:42:48 -0400

    o js_type

--- web/public/src/js_type.mjs
+++ web/public/src/js_type.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function js_type(ast, node_type) {
+  marker("1");
   const vs = list_adder((la) => {
     js_visit_type(ast, node_type, la);
   });
