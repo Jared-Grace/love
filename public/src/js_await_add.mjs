d268187f7e9997155e2556b78f0a840b32a1ed62
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 14:27:45 -0400

    mm 1 2

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -10,7 +10,6 @@ export async function js_await_add(ast) {
   let name = js_declaration_single_name(ast);
   let f = object_property_get(functions, name);
   marker("1");
-  let async_is = object_property_get(f, "async");
   async function lambda(v) {
     let node = object_property_get(v, "node");
     async function lambda3(name) {
@@ -20,6 +19,7 @@ export async function js_await_add(ast) {
       }
       let f = object_property_get(functions, name);
       marker("2");
+      let async_is = object_property_get(f, "async");
       let stack = object_property_get(v, "stack");
     }
     await js_call_function_if(node, lambda3);
