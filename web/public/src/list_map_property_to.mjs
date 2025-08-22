0132795053ee7204c088e94992c709691355fa33
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:56 -0400

    function_auto_path public\src\list_not_is.mjs

--- web/public/src/list_map_property_to.mjs
+++ web/public/src/list_map_property_to.mjs
@@ -1,6 +1,5 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { list_map } from "./list_map.mjs";
-import { marker } from "./marker.mjs";
 export function list_map_property_to(list, name) {
   function lambda(item) {
     let o = {};
