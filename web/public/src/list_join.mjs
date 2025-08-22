a79799c7458ad1a63ae95d5df97004ced6548180
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:13 -0400

    function_auto_path public\src\function_alias_replace.mjs

--- web/public/src/list_join.mjs
+++ web/public/src/list_join.mjs
@@ -1,3 +1,4 @@
 export function list_join(list, separator) {
-  return list.join(separator);
+  let v = list.join(separator);
+  return v;
 }
--- web/public/src/list_multiple_is.mjs
+++ web/public/src/list_multiple_is.mjs
@@ -1,4 +1,5 @@
 import { list_size } from "./list_size.mjs";
 export function list_multiple_is(list) {
-  return list_size(list) >= 2;
+  let v = list_size(list) >= 2;
+  return v;
 }
