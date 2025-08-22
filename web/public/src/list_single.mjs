3f372dbb8b5f4f2567331fecf15e0bc6e03e299b
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:57 -0400

    function_auto_path public\src\list_single.mjs

--- web/public/src/list_single.mjs
+++ web/public/src/list_single.mjs
@@ -1,8 +1,5 @@
 import { list_size_1_assert } from "./list_size_1_assert.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
 import { list_first } from "./list_first.mjs";
-import { error } from "./error.mjs";
-import { list_size_1 } from "./list_size_1.mjs";
 export function list_single(list) {
   list_size_1_assert(list);
   let first = list_first(list);
