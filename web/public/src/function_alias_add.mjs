9ded2bd6674b84c6ebf2cdfe720ce66f4d65bf5e
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:33 -0400

    function_auto_path public\src\equal_by_async.mjs

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -1,12 +1,7 @@
 import { list_sort_string_size } from "./list_sort_string_size.mjs";
-import { list_sort_number_mapper } from "./list_sort_number_mapper.mjs";
-import { list_sort_string } from "./list_sort_string.mjs";
 import { function_alias_generic } from "./function_alias_generic.mjs";
 import { error } from "./error.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { function_alias_add_generic } from "./function_alias_add_generic.mjs";
-import { file_overwrite_json } from "./file_overwrite_json.mjs";
-import { string_size } from "./string_size.mjs";
 export async function function_alias_add(first, second) {
   let list = [first, second];
   list_sort_string_size(list);
