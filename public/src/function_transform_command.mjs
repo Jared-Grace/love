c87bc4e634e9bac3e38539d259dbfe7a9907e8d3
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:35 -0400

    function_auto_path public\src\file_delete.mjs

--- public/src/function_transform_command.mjs
+++ public/src/function_transform_command.mjs
@@ -1,6 +1,5 @@
 import { function_transform_result } from "./function_transform_result.mjs";
 import { function_import } from "./function_import.mjs";
-import { function_transform } from "./function_transform.mjs";
 export async function function_transform_command(transformer_name, f_name) {
   const transformer = await function_import(transformer_name);
   let v = await function_transform_result(f_name, transformer);
