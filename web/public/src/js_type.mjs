708a599576a14466cd8bf0f0265f611bb6ba5713
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:42:48 -0400

    function_auto_path public\src\js_type.mjs

--- web/public/src/js_type.mjs
+++ web/public/src/js_type.mjs
@@ -3,8 +3,9 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function js_type(ast, node_type) {
   marker("1");
-  const vs = list_adder((la) => {
+  function lambda(la) {
     js_visit_type(ast, node_type, la);
-  });
+  }
+  const vs = list_adder(lambda);
   return vs;
 }
