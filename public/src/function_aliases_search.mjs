81a33923fd63201de6146178c1d06876fcd937d5
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:33 -0400

    function_auto_path public\src\equal_by_async.mjs

--- public/src/function_aliases_search.mjs
+++ public/src/function_aliases_search.mjs
@@ -5,8 +5,10 @@ import { object_property_get } from "./object_property_get.mjs";
 export async function function_aliases_search(search) {
   let names_to_aliases = await function_aliases_inverted();
   let names = object_properties(names_to_aliases);
-  let result = search_generic(search, names, (n) =>
-    object_property_get(names_to_aliases, n),
-  );
+  function lambda(n) {
+    let value = object_property_get(names_to_aliases, n);
+    return value;
+  }
+  let result = search_generic(search, names, lambda);
   return result;
 }
