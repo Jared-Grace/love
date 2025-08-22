77ba2d72e8d844ee992aa9b41c1e9be40088e373
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:41 -0400

    function_auto_path public\src\git_push_command.mjs

--- web/public/src/function_new_transform.mjs
+++ web/public/src/function_new_transform.mjs
@@ -1,4 +1,3 @@
-import { log } from "./log.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_new_transform(f_name, lambda$ast) {
--- web/public/src/function_run_git.mjs
+++ web/public/src/function_run_git.mjs
@@ -1,7 +1,5 @@
 import { git_acp_call } from "./git_acp_call.mjs";
 import { function_run_log } from "./function_run_log.mjs";
-import { git_acp } from "./git_acp.mjs";
-import { function_run } from "./function_run.mjs";
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
   await git_acp_call(f_name, args);
