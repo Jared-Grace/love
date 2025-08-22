04e144fe134773ba42fcf488b962a4a622107b54
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:48:02 -0400

    c functions_format functions_log_remove

--- public/src/functions_log_remove.mjs
+++ public/src/functions_log_remove.mjs
@@ -1,8 +1,10 @@
+import { marker } from "./marker.mjs";
 import { list_wait } from "./list_wait.mjs";
 import { function_format } from "./function_format.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { list_map } from "./list_map.mjs";
 export async function functions_log_remove() {
+  marker("1");
   let mapped = list_map(await functions_names(), function_format);
   await list_wait(mapped);
 }
