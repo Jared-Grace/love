86423cb77a980b73e4ec03d0ed26c92513696ed9
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:50:22 -0400

    function_auto_path public\src\list_join_comma.mjs

--- public/src/object_property_exists_equals.mjs
+++ public/src/object_property_exists_equals.mjs
@@ -1,8 +1,8 @@
 import { object_property_equals } from "./object_property_equals.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function object_property_exists_equals(path, in_progress, value) {
-  return (
+  let v =
     object_property_exists(path) &&
-    object_property_equals(in_progress, path, value)
-  );
+    object_property_equals(in_progress, path, value);
+  return v;
 }
