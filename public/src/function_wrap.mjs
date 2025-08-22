1530b14bdf690808937f1fdb57fb16473b89edf1
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:24 -0400

    function_auto_path public\src\git_acp.mjs

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -1,23 +1,14 @@
 import { js_statement_return } from "./js_statement_return.mjs";
 import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
-import { js_code_return } from "./js_code_return.mjs";
 import { object_property_from } from "./object_property_from.mjs";
-import { log } from "./log.mjs";
 import { js_declaration_asyncify } from "./js_declaration_asyncify.mjs";
 import { js_code_call_args_await_maybe } from "./js_code_call_args_await_maybe.mjs";
 import { js_declaration_params_names } from "./js_declaration_params_names.mjs";
 import { function_parse_declaration } from "./function_parse_declaration.mjs";
 import { function_new_transform } from "./function_new_transform.mjs";
-import { function_transform } from "./function_transform.mjs";
-import { marker } from "./marker.mjs";
-import { function_new } from "./function_new.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { list_add } from "./list_add.mjs";
-import { js_parse_expression } from "./js_parse_expression.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 import { js_imports_missing_add } from "./js_imports_missing_add.mjs";
-import { js_parse_statement } from "./js_parse_statement.mjs";
 export async function function_wrap(f_name, f_name_wrapped) {
   let { declaration: declaration_call, unaliased } =
     await function_parse_declaration(f_name);
