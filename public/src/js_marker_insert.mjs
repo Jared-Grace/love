e2f6f1a843658cbe9de57ff40acd49620eb176dc
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:50 -0400

    function_auto_path public\src\function_format.mjs

--- public/src/js_marker_insert.mjs
+++ public/src/js_marker_insert.mjs
@@ -1,9 +1,5 @@
 import { js_call_insert } from "./js_call_insert.mjs";
-import { js_code_call_args_await_maybe } from "./js_code_call_args_await_maybe.mjs";
-import { list_insert } from "./list_insert.mjs";
-import { js_parse_statement } from "./js_parse_statement.mjs";
 import { marker } from "./marker.mjs";
-import { js_code_call_args } from "./js_code_call_args.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 export async function js_marker_insert(name, list, index) {
   let code_string = await js_code_string(name);
