50de6c7a68d07f8f7397b2a02347321baeadce59
Grace-Jared <christrosetolife@gmail.com>
2025-08-21 20:31:59 -0400

    function_auto_path public\src\js_function_last_asyncify.mjs

--- public/src/js_function_last_asyncify.mjs
+++ public/src/js_function_last_asyncify.mjs
@@ -1,13 +1,12 @@
-import {object_property_set} from './object_property_set.mjs';
-import {object_property_get} from './object_property_get.mjs';
-import {js_stack_last_multiple} from './js_stack_last_multiple.mjs';
-import {js_types_function} from './js_types_function.mjs';
+import { not } from "./not.mjs";
+import { js_stack_last_function } from "./js_stack_last_function.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { object_property_get } from "./object_property_get.mjs";
 export function js_function_last_asyncify(stack, async_is) {
-  let types = js_types_function();
-  let f = js_stack_last_multiple(stack, types);
+  let f = js_stack_last_function(stack);
   let property_name = "async";
   let async = object_property_get(f, property_name);
-  if (async_is && !async) {
+  if (async_is && not(async)) {
     object_property_set(f, property_name, true);
   }
 }
