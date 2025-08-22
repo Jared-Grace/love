3c4854ebb6fd49ae006bc0b92febe70abf8851c0
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:40 -0400

    function_auto_path public\src\js_if_else_if_combine.mjs

--- web/public/src/js_imports_unused.mjs
+++ web/public/src/js_imports_unused.mjs
@@ -2,14 +2,7 @@ import { list_filter_property } from "./list_filter_property.mjs";
 import { list_map } from "./list_map.mjs";
 import { js_imports_declarations } from "./js_imports_declarations.mjs";
 import { js_identifiers_named_count } from "./js_identifiers_named_count.mjs";
-import { log } from "./log.mjs";
-import { counter } from "./counter.mjs";
-import { list_adder } from "./list_adder.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { js_visit_type } from "./js_visit_type.mjs";
-import { js_visit } from "./js_visit.mjs";
-import { each } from "./each.mjs";
-import { js_imports } from "./js_imports.mjs";
 import { marker } from "./marker.mjs";
 import { object_merge } from "./object_merge.mjs";
 export function js_imports_unused(ast) {
