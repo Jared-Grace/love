0fb306704665d5b72c2ea62fffadd260df289e62
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:53 -0400

    function_auto_path public\src\function_identifier_replace_suffix_add.mjs

--- web/public/src/js_if_else_if_combine.mjs
+++ web/public/src/js_if_else_if_combine.mjs
@@ -2,17 +2,12 @@ import { js_visit_type_each_async } from "./js_visit_type_each_async.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { js_left_right_set } from "./js_left_right_set.mjs";
 import { js_code_or } from "./js_code_or.mjs";
-import { js_operator_or } from "./js_operator_or.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { equal_by_async } from "./equal_by_async.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_node_type_is_if_async } from "./js_node_type_is_if_async.mjs";
 import { js_unparse } from "./js_unparse.mjs";
-import { js_node_type_is_if } from "./js_node_type_is_if.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
-import { log } from "./log.mjs";
-import { js_visit_type } from "./js_visit_type.mjs";
-import { marker } from "./marker.mjs";
 export async function js_if_else_if_combine(ast) {
   async function lambda(v) {
     let { node, stack } = v;
