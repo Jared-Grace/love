450aa53c7a681a02fc6e5ac2e0a64bc639e9dcc5
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:44:12 -0400

    function_auto_path public\src\js_log_remove.mjs

--- web/public/src/js_log_remove.mjs
+++ web/public/src/js_log_remove.mjs
@@ -1,7 +1,10 @@
+import { js_node_type_is_if } from "./js_node_type_is_if.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
 export function js_log_remove(ast) {
   marker("1");
-  function lambda(v) {}
+  function lambda(v) {
+    js_node_type_is_if(node, type, function lambda3() {});
+  }
   js_visit_type(ast, "CallExpression", lambda);
 }
