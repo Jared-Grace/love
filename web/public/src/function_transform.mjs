7f9a3a2ec4b6738bdf6447c78c89aeb4213a0a99
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:52:36 -0400

    function_auto_path public\src\functions_names_includes.mjs

--- web/public/src/function_transform.mjs
+++ web/public/src/function_transform.mjs
@@ -1,10 +1,5 @@
 import { function_transform_result } from "./function_transform_result.mjs";
 import { function_parse_declaration_js_unparse } from "./function_parse_declaration_js_unparse.mjs";
-import { data_terminal_get } from "./data_terminal_get.mjs";
-import { log_keep } from "./log_keep.mjs";
-import { log } from "./log.mjs";
-import { file_js_unparse } from "./file_js_unparse.mjs";
-import { function_parse } from "./function_parse.mjs";
 export async function function_transform(f_name, lambda$ast) {
   await function_transform_result(f_name, lambda$ast);
   let output = await function_parse_declaration_js_unparse(f_name);
