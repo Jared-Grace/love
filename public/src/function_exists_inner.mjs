a1ce783391380d50a8fa123ecf134bf93438346a
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 16:14:51 -0400

    function_auto_path public\src\function_exists.mjs

---
+++ public/src/function_exists_inner.mjs
@@ -0,0 +1,11 @@
+import {object_merge} from "./object_merge.mjs";
+import {file_exists} from "./file_exists.mjs";
+export async function function_exists_inner(u) {
+  let {f_path} = u;
+  const exists = await file_exists(f_path);
+  let to = {
+    exists
+  };
+  let e = object_merge(to, u);
+  return e;
+}
