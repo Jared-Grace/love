bcb17201b97c4f647799a9a97b01a437a49a8ce6
Grace-Jared <christrosetolife@gmail.com>
2025-08-22 17:53:45 -0400

    function_auto_path public\src\js_return_empty.mjs

--- web/public/src/js_return_name.mjs
+++ web/public/src/js_return_name.mjs
@@ -1,13 +1,7 @@
 import { js_return_on } from "./js_return_on.mjs";
-import { js_identifiers_names } from "./js_identifiers_names.mjs";
-import { js_identifier_unique } from "./js_identifier_unique.mjs";
-import { log } from "./log.mjs";
 import { list_last } from "./list_last.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
 import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
-import { js_declaration_single } from "./js_declaration_single.mjs";
-import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_return_name(ast) {
   let body_block = js_declaration_single_block_blody(ast);
   let return_name = null;
