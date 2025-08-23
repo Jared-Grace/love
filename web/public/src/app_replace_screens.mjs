6e8d398204537c2dcaf2071abfbb0970c7a0a1e4
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:36:22 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_screens.mjs
+++ web/public/src/app_replace_screens.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_set } from "./app_replace_rule_set.mjs";
 import { app_replace_home } from "./app_replace_home.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_screens() {
