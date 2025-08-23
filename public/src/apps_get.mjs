aabbc91d8047827bddf3ba5576878ac17ff339a1
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:50:50 -0400

    mc fr

--- public/src/apps_get.mjs
+++ public/src/apps_get.mjs
@@ -1,4 +1,6 @@
+import { function_read } from "./function_read.mjs";
 import { marker } from "./marker.mjs";
-export function apps_get() {
+export async function apps_get() {
   marker("1");
+  let v = await function_read(f_name);
 }
