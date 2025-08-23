2e6447f7b593bd662426e66797f969372cee63ed
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:30:15 -0400

    function_auto_path public\src\app_replace_refresh.mjs

--- public/src/app_replace_refresh.mjs
+++ public/src/app_replace_refresh.mjs
@@ -1,15 +1,13 @@
+import { app_replace_screens } from "./app_replace_screens.mjs";
 import { storage_local_get_or } from "./storage_local_get_or.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { marker } from "./marker.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { html_document_body } from "./html_document_body.mjs";
-import { app_replace_home } from "./app_replace_home.mjs";
 import { app_replace } from "./app_replace.mjs";
 export function app_replace_refresh() {
   let app_fn = app_replace;
-  let screens = {
-    home: app_replace_home,
-  };
+  let screens = app_replace_screens();
   let body = html_document_body();
   html_clear(body);
   marker("1");
---
+++ public/src/app_replace_screens.mjs
@@ -0,0 +1,7 @@
+import {app_replace_home} from "./app_replace_home.mjs";
+export function app_replace_screens() {
+  let v = {
+    home: app_replace_home
+  };
+  return v;
+}
