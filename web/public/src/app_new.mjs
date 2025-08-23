edc3605cacc33242d75f6e655c93e2ad3c4c1221
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 14:56:46 -0400

    mc n

--- web/public/src/app_new.mjs
+++ web/public/src/app_new.mjs
@@ -1,6 +1,8 @@
+import { function_new } from "./function_new.mjs";
 import { html_new } from "./html_new.mjs";
 import { marker } from "./marker.mjs";
 export async function app_new(name) {
   marker("1");
+  await function_new(f_name);
   await html_new(name);
 }
