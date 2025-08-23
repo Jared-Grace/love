95b193b4ca03ecb16fdad48c9387f17c446d2659
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:24:54 -0400

    fws n app_screen

---
+++ public/src/function_new_app_screen.mjs
@@ -0,0 +1,6 @@
+import { function_new } from "./function_new.mjs";
+import { marker } from "./marker.mjs";
+export async function function_new_app_screen(f_name) {
+  marker("1");
+  return await function_new(f_name);
+}
