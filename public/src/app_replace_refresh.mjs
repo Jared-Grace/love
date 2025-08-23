5ac9ffdea860af9f886fd63114a1fa5b01989465
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:42:08 -0400

    de app_replace_refresh

--- public/src/app_replace_refresh.mjs
+++
@@ -1,8 +0,0 @@
-import { app_refresh } from "./app_refresh.mjs";
-import { app_replace_screens } from "./app_replace_screens.mjs";
-import { app_replace } from "./app_replace.mjs";
-export function app_replace_refresh() {
-  let app_fn = app_replace;
-  let screens = app_replace_screens();
-  app_refresh(app_fn, screens);
-}
