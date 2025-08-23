a8ed311b3d130cbe0ef5cc92c4ac843c9d5fb79d
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:28:45 -0400

    u u

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,6 +1,6 @@
 import { html_text_set } from "./html_text_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { html_button } from "./html_button.mjs";
+import { html_button_element } from "./html_button.mjs";
 import { each } from "./each.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
@@ -19,7 +19,7 @@ export function app_replace() {
   ];
   function lambda(rs) {
     let name2 = object_property_get(rs, "name");
-    let b = html_button(body);
+    let b = html_button_element(body);
     html_text_set(b, name2);
   }
   each(rule_sets, lambda);
