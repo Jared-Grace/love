370469b675574b40f34f39df41e77dc0c93b21d8
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:52:32 -0400

    function_auto_path public\src\html_text_set.mjs

--- web/public/src/html_text_set.mjs
+++ web/public/src/html_text_set.mjs
@@ -1,4 +1,6 @@
+import { object_property_get } from "./object_property_get.mjs";
 export function html_text_set(component, name2) {
   let { element } = component;
+  let element2 = object_property_get(component, "element");
   element.innerHTML = name2;
 }
