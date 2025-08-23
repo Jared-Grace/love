93a5b960c1e81788cdf31d832bd9a91f53675f11
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:36:18 -0400

    function_auto_path public\src\function_imports_add.mjs

--- web/public/src/function_imports_add.mjs
+++ web/public/src/function_imports_add.mjs
@@ -1,7 +1,6 @@
+import { folder_current_join_code } from "./folder_current_join_code.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
-import { js_code_string } from "./js_code_string.mjs";
-import { folder_current_join } from "./folder_current_join.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
@@ -9,8 +8,7 @@ export async function function_imports_add(ast, imports) {
   let { body } = ast;
   async function lambda(import_) {
     let f_name_ext = function_name_to_base(import_);
-    let value_string = folder_current_join(f_name_ext);
-    const from = js_code_string(value_string);
+    const from = folder_current_join_code(f_name_ext);
     let code = js_code_import_single(import_, from);
     let statement = js_parse_statement_module(code);
     list_add_first(body, statement);
