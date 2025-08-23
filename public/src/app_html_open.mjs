7f3447a5f231f3b5440159175626456c913aa190
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:03:19 -0400

    mc file_open

--- public/src/app_html_open.mjs
+++ public/src/app_html_open.mjs
@@ -1,8 +1,10 @@
+import { file_open } from "./file_open.mjs";
 import { folder_public_combine } from "./folder_public_combine.mjs";
 import { file_name_html } from "./file_name_html.mjs";
 import { marker } from "./marker.mjs";
-export function app_html_open(a_name) {
+export async function app_html_open(a_name) {
   let file_name = file_name_html(a_name);
   let combined = folder_public_combine(file_name);
   marker("1");
+  await file_open(f_path);
 }
