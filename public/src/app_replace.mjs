e72de49625a0fd6570db16ed64d82f3fcdaf0a0f
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 15:16:16 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -3,6 +3,7 @@ import { marker } from "./marker.mjs";
 export function app_replace() {
   let body = html_document_body();
   marker("1");
-  const newDiv = document.createElement("div");
+  const tag_name = "div";
+  const newDiv = document.createElement(tag_name);
   parent.appendChild(newDiv);
 }
