213935df4ea516aede8304674143508ddd17d5a0
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:38:30 -0400

    data_file_update public\src\js_imports_paths_fix.mjs

--- public/src/js_imports_paths_fix.mjs
+++ public/src/js_imports_paths_fix.mjs
@@ -3,6 +3,7 @@ import { js_imports_declarations } from "./js_imports_declarations.mjs";
 import { marker } from "./marker.mjs";
 export function js_imports_paths_fix(ast) {
   let imports_declarations = js_imports_declarations(ast);
-  each(list, function lambda(item) {});
+  function lambda(item) {}
+  each(list, lambda);
   marker("1");
 }
