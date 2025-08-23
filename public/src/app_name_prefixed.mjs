c77d3bcae0bc4dc900f5f26f1a0b003b6694c9c9
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:14:40 -0400

    function_auto_path public\src\app_name_prefixed.mjs

--- public/src/app_name_prefixed.mjs
+++ public/src/app_name_prefixed.mjs
@@ -1,4 +1,6 @@
+import { app_name_prefix } from "./app_name_prefix.mjs";
 export function app_name_prefixed(name) {
-  let a_name = `${"app"}_${name}`;
+  const p = app_name_prefix();
+  let a_name = `${p}_${name}`;
   return a_name;
 }
