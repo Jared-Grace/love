974e110bdd10e0e44d446e6539136285266d8365
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:07:28 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -1,8 +1,5 @@
+import { app_replace_home } from "./app_replace_home.mjs";
 import { html_clear } from "./html_clear.mjs";
-import { storage_local_set } from "./storage_local_set.mjs";
-import { html_button } from "./html_button.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { each } from "./each.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace() {
@@ -10,28 +7,5 @@ export function app_replace() {
   let body = html_document_body();
   html_clear(body);
   marker("1");
-  lambda();
-  function lambda() {
-    let body = html_document_body();
-    let rule_sets = [
-      {
-        name: "Grow",
-        rules: ["a > a a"],
-      },
-      {
-        name: "Shrink",
-        rules: ["a a > a"],
-      },
-    ];
-    function lambda2(rs) {
-      let name2 = object_property_get(rs, "name");
-      html_button(body, name2, lambda);
-      function lambda() {
-        let key = "screen";
-        const value = "rule_set";
-        storage_local_set(app_fn, key, value);
-      }
-    }
-    each(rule_sets, lambda2);
-  }
+  app_replace_home();
 }
