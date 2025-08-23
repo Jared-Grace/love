016abfe0933b18cf59dfeb0106a28bbaf6868233
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 15:15:54 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -2,14 +2,9 @@ import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace() {
   let body = html_document_body();
-  marker("1");const newDiv = document.createElement("div");
-
-// Set its text content (optional)
-newDiv.textContent = "Hello, world!";
-
-// Find the parent element
-const parent = document.getElementById("container");
-
-// Append new element to the parent
-parent.appendChild(newDiv);
+  marker("1");
+  const newDiv = document.createElement("div");
+  newDiv.textContent = "Hello, world!";
+  const parent = document.getElementById("container");
+  parent.appendChild(newDiv);
 }
