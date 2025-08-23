4d1f80f1ba01f6553e3f56dbdd30d3d71cc2b9a0
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:46:14 -0400

    function_auto_path public\src\js_imports_paths_fix.mjs

--- web/public/src/js_imports_paths_fix.mjs
+++ web/public/src/js_imports_paths_fix.mjs
@@ -1,6 +1,5 @@
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { js_string } from "./js_string.mjs";
 import { function_name_to_path_import } from "./function_name_to_path_import.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
@@ -13,9 +12,8 @@ export function js_imports_paths_fix(ast) {
     let name = object_property_get(i, "name");
     let declaration = object_property_get(i, "declaration");
     const from = function_name_to_path_import(name);
-    let expression = js_parse_expression(code_expression);
-    let s = js_string(from);
-    object_property_set(declaration, "source", s);
+    let expression = js_parse_expression(from);
+    object_property_set(declaration, "source", expression);
   }
   each(imports_declarations, lambda);
   marker("1");
