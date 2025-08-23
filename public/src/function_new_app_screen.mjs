bff8aff688781894a8e53be3ccc5834bbf9c55dd
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:26:12 -0400

    function_auto_path public\src\function_new_app_screen.mjs

--- public/src/function_new_app_screen.mjs
+++ public/src/function_new_app_screen.mjs
@@ -4,7 +4,7 @@ import { function_new } from "./function_new.mjs";
 import { marker } from "./marker.mjs";
 export async function function_new_app_screen(f_name) {
   let a_name = app_name_prefixed(f_name);
-  let combined = function_name_combine(left, right);
+  let combined = function_name_combine(a_name, "screen");
   marker("1");
   let v = await function_new(a_name);
   return v;
