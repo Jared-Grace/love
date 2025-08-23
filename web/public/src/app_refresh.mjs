3bd2c13c39f655e7a4cc99c68ec4682bca3aa710
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:38:25 -0400

    function_auto_path public\src\app_replace_refresh.mjs

---
+++ web/public/src/app_refresh.mjs
@@ -0,0 +1,13 @@
+import {object_property_get} from "./object_property_get.mjs";
+import {storage_local_get_or} from "./storage_local_get_or.mjs";
+import {marker} from "./marker.mjs";
+import {html_clear} from "./html_clear.mjs";
+import {html_document_body} from "./html_document_body.mjs";
+export function app_refresh(app_fn, screens) {
+  let body = html_document_body();
+  html_clear(body);
+  marker("1");
+  let screen_name = storage_local_get_or(app_fn, "screen", "home");
+  let screen = object_property_get(screens, screen_name);
+  screen();
+}
--- web/public/src/app_replace_refresh.mjs
+++ web/public/src/app_replace_refresh.mjs
@@ -1,17 +1,8 @@
+import { app_refresh } from "./app_refresh.mjs";
 import { app_replace_screens } from "./app_replace_screens.mjs";
-import { storage_local_get_or } from "./storage_local_get_or.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { marker } from "./marker.mjs";
-import { html_clear } from "./html_clear.mjs";
-import { html_document_body } from "./html_document_body.mjs";
 import { app_replace } from "./app_replace.mjs";
 export function app_replace_refresh() {
   let app_fn = app_replace;
   let screens = app_replace_screens();
-  let body = html_document_body();
-  html_clear(body);
-  marker("1");
-  let screen_name = storage_local_get_or(app_fn, "screen", "home");
-  let screen = object_property_get(screens, screen_name);
-  screen();
+  app_refresh(app_fn, screens);
 }
