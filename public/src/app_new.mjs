2175f607df49ddc66e0fb036b64239cc420c9e35
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 14:57:00 -0400

    mc anp

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,8 +1,10 @@
+import { app_name_prefixed } from "./app_name_prefixed.mjs";
 import { function_new } from "./function_new.mjs";
 import { html_new } from "./html_new.mjs";
 import { marker } from "./marker.mjs";
 export async function app_new(name) {
   marker("1");
+  let v = app_name_prefixed(name2);
   await function_new(f_name);
   await html_new(name);
 }
