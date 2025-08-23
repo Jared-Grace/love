67988adfeefb31f1431822893973c8d98e34cbc1
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:03:37 -0400

    function_auto_path public\src\html_button.mjs

--- web/public/src/html_button.mjs
+++ web/public/src/html_button.mjs
@@ -1,8 +1,8 @@
-import {html_on_click} from "./html_on_click.mjs";
-import {html_text_set} from "./html_text_set.mjs";
-import {html_button_element} from "./html_button_element.mjs";
-export function html_button(body, text, lambda) {
-  let component = html_button_element(body);
+import { html_on_click } from "./html_on_click.mjs";
+import { html_text_set } from "./html_text_set.mjs";
+import { html_button_element } from "./html_button_element.mjs";
+export function html_button(parent, text, lambda) {
+  let component = html_button_element(parent);
   html_text_set(component, text);
   html_on_click(component, lambda);
 }
