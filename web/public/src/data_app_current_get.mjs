9a30341f3f6d89028c895039206872e120c3e68e
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:12:21 -0400

    function_auto_path public\src\data_app_current_get.mjs

--- web/public/src/data_app_current_get.mjs
+++ web/public/src/data_app_current_get.mjs
@@ -1,5 +1,7 @@
-import {data_property_get} from "./data_property_get.mjs";
+import { null_not_is } from "./null_not_is.mjs";
+import { data_property_get } from "./data_property_get.mjs";
 export async function data_app_current_get() {
-  let v2 = await data_property_get("app_current");
-  return v2;
+  let a = await data_property_get("app_current");
+  let nn = null_not_is(value);
+  return a;
 }
