58fb924f4ea161e1faa95b26d267e46375ffa1d4
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:13:09 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { app_replace_home } from "./app_replace_home.mjs";
 import { html_clear } from "./html_clear.mjs";
@@ -11,6 +12,6 @@ export function app_replace() {
   let body = html_document_body();
   html_clear(body);
   marker("1");
-  let screen = storage_local_get(app_fn, "screen");
-  app_replace_home();
+  let screen_name = storage_local_get(app_fn, "screen");
+  let value = object_property_get(object, property_name);
 }
