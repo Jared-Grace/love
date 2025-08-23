47783ee4df4df5b5dd902316ff23869a1968401e
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:01:20 -0400

    mnd

--- public/src/app_html_open.mjs
+++ public/src/app_html_open.mjs
@@ -4,5 +4,4 @@ import { marker } from "./marker.mjs";
 export function app_html_open(a_name) {
   let file_name = file_name_html(a_name);
   marker("1");
-  let v = folder_public_combine(second);
 }
