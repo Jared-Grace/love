7984b08f3a77cde038bc3d048ee638b20870095a
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:39:04 -0400

    function_auto_path public\src\functions_path.mjs

--- public/src/functions_path.mjs
+++ public/src/functions_path.mjs
@@ -1,6 +1,7 @@
+import { folder_src } from "./folder_src.mjs";
 import { folder_public_combine } from "./folder_public_combine.mjs";
 export function functions_path() {
-  const second = "src";
+  const second = folder_src();
   let joined = folder_public_combine(second);
   return joined;
 }
