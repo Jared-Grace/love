ed46a64b7a5b6d93f909fd80ca59ca9481439a2a
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:49:56 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -1,9 +1,8 @@
-import { html_button } from "./html_button.mjs";
+import { html_button_screen } from "./html_button_screen.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_rule_set() {
   let body = html_document_body();
-  function lambda2() {}
-  html_button(body, "Home", lambda2);
+  html_button_screen(body, "Home", context, "home");
   marker("1");
 }
