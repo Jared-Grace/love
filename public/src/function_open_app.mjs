816e82fa457ce4f934d63b882a68f74567e9ad8a
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:56:46 -0400

    function_auto_path public\src\function_open_app.mjs

--- public/src/function_open_app.mjs
+++ public/src/function_open_app.mjs
@@ -2,5 +2,6 @@ import { function_open } from "./function_open.mjs";
 import { marker } from "./marker.mjs";
 export async function function_open_app(f_name) {
   marker("1");
-  return await function_open(f_name);
+  let v = await function_open(f_name);
+  return v;
 }
