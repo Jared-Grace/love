e204e031b250ddb20646ee4539a449286e8ad683
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:05:24 -0400

    function_auto_path public\src\html_element.mjs

--- web/public/src/html_element.mjs
+++ web/public/src/html_element.mjs
@@ -1,10 +1,9 @@
+import { html_component_wrap } from "./html_component_wrap.mjs";
 import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_element(parent, tag_name) {
   let parent_element = html_component_element_get(parent);
   const e = document.createElement(tag_name);
   parent_element.appendChild(e);
-  let component = {
-    element: e,
-  };
+  let component = html_component_wrap(e);
   return component;
 }
