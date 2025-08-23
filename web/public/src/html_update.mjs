692afce50c3a1c9e581b8834a8d71d6fd2c41837
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 14:50:40 -0400

    function_auto_path public\src\html_update.mjs

--- web/public/src/html_update.mjs
+++ web/public/src/html_update.mjs
@@ -3,7 +3,6 @@ import { folder_current_join_code } from "./folder_current_join_code.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { js_code_call_statement } from "./js_code_call_statement.mjs";
-import { function_name_extension } from "./function_name_extension.mjs";
 import { file_open } from "./file_open.mjs";
 import { html_overwrite } from "./html_overwrite.mjs";
 import { html_name_to_path } from "./html_name_to_path.mjs";
@@ -11,7 +10,6 @@ import { path_join } from "./path_join.mjs";
 export async function html_update(name) {
   let file_path = html_name_to_path(name);
   const name_prefixed = `app_${name}`;
-  let ext = function_name_extension();
   let call = js_code_call_statement(name_prefixed);
   let f_name_ext = function_name_to_base(name_prefixed);
   let src = folder_src();
