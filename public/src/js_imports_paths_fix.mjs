c2eae06c5f3076d4475ef0cc24d126258a0dab41
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:38:29 -0400

    function_auto_path public\src\js_imports_paths_fix.mjs

--- public/src/js_imports_paths_fix.mjs
+++ public/src/js_imports_paths_fix.mjs
@@ -1,6 +1,8 @@
+import { each } from "./each.mjs";
 import { js_imports_declarations } from "./js_imports_declarations.mjs";
 import { marker } from "./marker.mjs";
 export function js_imports_paths_fix(ast) {
   let imports_declarations = js_imports_declarations(ast);
+  each(list, function lambda(item) {});
   marker("1");
 }
