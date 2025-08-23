809ac25c7cc3751cd3cf408f968b126673dbcee8
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:08:23 -0400

    function_auto_path public\src\storage_local_set.mjs

--- public/src/storage_local_set.mjs
+++ public/src/storage_local_set.mjs
@@ -1,3 +1,5 @@
+import { storage_local_key_get } from "./storage_local_key_get.mjs";
 export function storage_local_set(app_fn, key, value) {
-  localStorage.setItem(app_fn.name + " " + key, value);
+  let v = storage_local_key_get(app_fn, key);
+  localStorage.setItem(v, value);
 }
