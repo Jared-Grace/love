34d83eac9b837fa3a6544446119c25c6a3951a0b
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:14:56 -0400

    function_auto_path public\src\app_name_prefixed.mjs

--- web/public/src/app_name_prefixed.mjs
+++ web/public/src/app_name_prefixed.mjs
@@ -1,6 +1,8 @@
+import { function_name_combine } from "./function_name_combine.mjs";
 import { app_name_prefix } from "./app_name_prefix.mjs";
 export function app_name_prefixed(name) {
   const p = app_name_prefix();
+  let combined = function_name_combine(left, right);
   let a_name = `${p}_${name}`;
   return a_name;
 }
