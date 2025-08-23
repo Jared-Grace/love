57a8782be30180d8a02908fda14a42a1fd999736
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:36:43 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -1,4 +1,6 @@
+import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_rule_set() {
+  let body = html_document_body();
   marker("1");
 }
