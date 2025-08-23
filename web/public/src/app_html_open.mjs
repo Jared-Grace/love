b4a3147d393aa2f16a50599fd732aff59ffc6390
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:03:28 -0400

    ir f_path combined

--- web/public/src/app_html_open.mjs
+++ web/public/src/app_html_open.mjs
@@ -6,5 +6,5 @@ export async function app_html_open(a_name) {
   let file_name = file_name_html(a_name);
   let combined = folder_public_combine(file_name);
   marker("1");
-  await file_open(f_path);
+  await file_open(combined);
 }
