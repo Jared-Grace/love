f2cf5e6822c5b276544e999d9a273c53e1b157d7
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:34:11 -0400

    function_auto_path public\src\js_property_identifier.mjs

--- web/public/src/js_property_identifier.mjs
+++ web/public/src/js_property_identifier.mjs
@@ -1,8 +1,8 @@
-import { js_property } from "./js_property.mjs";
+import { js_property_generic } from "./js_property_generic.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_property_identifier(param_name) {
   let key = js_parse_expression(param_name);
   let value = key;
-  let v = js_property(key, value);
+  let v = js_property_generic(true, key, value);
   return v;
 }
