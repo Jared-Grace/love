fa10d30d5334581637e50c48a79506d647ee2127
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:40:39 -0400

    ml

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -24,10 +24,10 @@ export async function js_await_add(ast) {
       let stack = object_property_get(v, "stack");
       js_function_last_asyncify(stack, async_is);
       let stack1 = list_get_end_1(stack);
+      marker("1");
       function lambda4() {
         let copy = object_copy(node);
         let awaited = js_await(copy);
-        marker("1");
         object_replace(node, awaited);
       }
       js_node_type_not_is_if(stack1, "AwaitExpression", lambda4);
