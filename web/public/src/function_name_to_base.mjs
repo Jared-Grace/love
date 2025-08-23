f1cfbbd8854a55b3b4b014f729c3c0aa9ff79c84
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:35:43 -0400

    function_auto_path public\src\function_name_to_base.mjs

--- web/public/src/function_name_to_base.mjs
+++ web/public/src/function_name_to_base.mjs
@@ -1,5 +1,5 @@
 import { function_name_extension } from "./function_name_extension.mjs";
 export function function_name_to_base(f_name) {
-  let v = f_name + function_name_extension();
-  return v;
+  let f_name_ext = f_name + function_name_extension();
+  return f_name_ext;
 }
