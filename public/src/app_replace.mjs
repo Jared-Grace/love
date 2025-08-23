a8d3bee42f5415fb78038114d7202d07e4d2cfe3
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 15:20:08 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -4,7 +4,10 @@ import { marker } from "./marker.mjs";
 export function app_replace() {
   let body = html_document_body();
   marker("1");
-  let set1 = ["a > a a"];
+  let set1 = {
+    name: "grow",
+    rules: ["a > a a"],
+  };
   let rule_sets = [];
   const tag_name = "div";
   html_element(body, tag_name);
