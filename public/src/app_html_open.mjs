1bf3f8fad2b9b975dd7d9f719e312b9aa78188c5
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:59:46 -0400

    ir name a_name

--- public/src/app_html_open.mjs
+++ public/src/app_html_open.mjs
@@ -2,5 +2,5 @@ import { file_name_html } from "./file_name_html.mjs";
 import { marker } from "./marker.mjs";
 export function app_html_open(a_name) {
   marker("1");
-  let file_name = file_name_html(name);
+  let file_name = file_name_html(a_name);
 }
