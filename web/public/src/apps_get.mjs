24d7d2af8df3b489e028faee3dbf39500bd3f252
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:53:48 -0400

    ir v fop

--- web/public/src/apps_get.mjs
+++ web/public/src/apps_get.mjs
@@ -3,6 +3,6 @@ import { folder_read } from "./folder_read.mjs";
 import { marker } from "./marker.mjs";
 export async function apps_get() {
   marker("1");
-  let v = folder_public();
+  let fop = folder_public();
   let v2 = folder_read(dirPath);
 }
