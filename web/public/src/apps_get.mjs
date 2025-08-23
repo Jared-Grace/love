04816881854cabaf5727bf91bc4437cbf5b5dd0b
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:54:14 -0400

    md 2

--- web/public/src/apps_get.mjs
+++ web/public/src/apps_get.mjs
@@ -2,7 +2,7 @@ import { folder_public } from "./folder_public.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { marker } from "./marker.mjs";
 export async function apps_get() {
-  marker("1");
   let fop = folder_public();
   let fr = folder_read(fop);
+  marker("1");
 }
