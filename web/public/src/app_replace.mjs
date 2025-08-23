13f8f64e0cf083c474922b9f2c630cc34d6d9633
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 15:13:55 -0400

    mc hdb

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -1,4 +1,6 @@
+import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace() {
   marker("1");
+  let body = html_document_body();
 }
