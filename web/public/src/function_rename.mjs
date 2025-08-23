48eb6efb1a439b4ccf02fa55f06db72d4f6e775c
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 16:08:06 -0400

    function_auto_path public\src\function_rename.mjs

--- web/public/src/function_rename.mjs
+++ web/public/src/function_rename.mjs
@@ -9,6 +9,6 @@ export async function function_rename(f_name_before, f_name_after) {
   await function_delete(f_name_before);
   let v = await function_alias_rename(f_name_before, f_name_after);
   let identifiers = await data_identifiers_get();
-  let value = object_property_get(identifiers, f_name_before);
+  let f_names = object_property_get(identifiers, f_name_before);
   marker("1");
 }
