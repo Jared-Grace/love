209e62635235ac76404b744ac83f7e6527095b74
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:22:37 -0400

    function_auto_path public\src\app_replace_refresh.mjs

--- web/public/src/app_replace_refresh.mjs
+++ web/public/src/app_replace_refresh.mjs
@@ -1,18 +1,18 @@
-import {object_property_get} from "./object_property_get.mjs";
-import {storage_local_get} from "./storage_local_get.mjs";
-import {marker} from "./marker.mjs";
-import {html_clear} from "./html_clear.mjs";
-import {html_document_body} from "./html_document_body.mjs";
-import {app_replace_home} from "./app_replace_home.mjs";
-import {app_replace} from "./app_replace.mjs";
+import { storage_local_get_or } from "./storage_local_get_or.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { marker } from "./marker.mjs";
+import { html_clear } from "./html_clear.mjs";
+import { html_document_body } from "./html_document_body.mjs";
+import { app_replace_home } from "./app_replace_home.mjs";
+import { app_replace } from "./app_replace.mjs";
 export function app_replace_refresh() {
   let app_fn = app_replace;
   let screens = {
-    home: app_replace_home
+    home: app_replace_home,
   };
   let body = html_document_body();
   html_clear(body);
   marker("1");
-  let screen_name = storage_local_get(app_fn, "screen");
+  let screen_name = storage_local_get_or(app_fn, "screen", "home");
   let screen = object_property_get(screens, screen_name);
 }
