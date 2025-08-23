489f36de3f512e60c433d78c363c281259242393
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:56:32 -0400

    o oe

--- web/public/src/function_open_editor.mjs
+++ web/public/src/function_open_editor.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 import { file_open_editor } from "./file_open_editor.mjs";
 export async function function_open_editor(f_name) {
+  marker("1");
   const { f_path } = await function_name_to_path_unalias(f_name);
   await file_open_editor(f_path);
 }
