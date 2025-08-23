384a7e8843bae4c4803770d01ca4442f2fbde4de
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:04:46 -0400

    function_auto_path public\src\html_element.mjs

--- web/public/src/html_element.mjs
+++ web/public/src/html_element.mjs
@@ -1,8 +1,8 @@
 import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_element(parent, tag_name) {
-  let element = html_component_element_get(parent);
+  let parent_element = html_component_element_get(parent);
   const e = document.createElement(tag_name);
-  element.appendChild(e);
+  parent_element.appendChild(e);
   let v = {
     element: e,
   };
