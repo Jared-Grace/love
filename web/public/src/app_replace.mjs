c1ff5db3120e9296887ddcb4833226fc38f36a41
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:59:33 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -19,7 +19,9 @@ export function app_replace() {
   function lambda(rs) {
     let name2 = object_property_get(rs, "name");
     html_button(body, name2, lambda);
-    function lambda() {}
+    function lambda() {
+      let body = html_document_body();
+    }
   }
   each(rule_sets, lambda);
 }
