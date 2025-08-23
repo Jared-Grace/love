6b7bab831c2a388ec257ebb8678d970cd6160e1f
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:35:12 -0400

    function_auto_path public\src\marker_screen_add.mjs

--- web/public/src/marker_screen_add.mjs
+++ web/public/src/marker_screen_add.mjs
@@ -1,7 +1,9 @@
+import { data_function_current_restore } from "./data_function_current_restore.mjs";
 import { marker_next_get } from "./marker_next_get.mjs";
 import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
 import { marker } from "./marker.mjs";
 export async function marker_screen_add(screen_name) {
+  await data_function_current_restore(lambda2);
   marker("1");
   async function lambda(a) {
     let { next } = marker_next_get(a);
