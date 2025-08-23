cd41407b0974b4463e325fd9d7451f32cf53ecf2
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:41:53 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -1,6 +1,9 @@
+import { app_refresh } from "./app_refresh.mjs";
+import { app_replace_screens } from "./app_replace_screens.mjs";
 import { marker } from "./marker.mjs";
-import { app_replace_refresh } from "./app_replace_refresh.mjs";
 export function app_replace() {
   marker("1");
-  app_replace_refresh();
+  let app_fn = app_replace;
+  let screens = app_replace_screens();
+  app_refresh(app_fn, screens);
 }
