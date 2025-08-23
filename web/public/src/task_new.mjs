a7073de3b7c0c34d29a44eb6577754c9da19b959
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:34:17 -0400

    function_auto_path public\src\task_new.mjs

--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -2,15 +2,10 @@ import { function_name_unalias } from "./function_name_unalias.mjs";
 import { function_current_set } from "./function_current_set.mjs";
 import { task_function_name_part } from "./task_function_name_part.mjs";
 import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
-import { js_code_string } from "./js_code_string.mjs";
 import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
-import { js_declaration_to_block_body } from "./js_declaration_to_block_body.mjs";
 import { js_call_insert } from "./js_call_insert.mjs";
-import { error } from "./error.mjs";
-import { js_call_new } from "./js_call_new.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { function_new } from "./function_new.mjs";
-import { function_name_combine } from "./function_name_combine.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 import { todo } from "./todo.mjs";
 export async function task_new(task_name) {
@@ -23,8 +18,11 @@ export async function task_new(task_name) {
     task_name,
   ]);
   let f_name_current = await data_function_current_get();
-  await function_new(f_name_task);
+  await lambda2();
   await function_current_set(f_name_current);
+  async function lambda2() {
+    await function_new(f_name_task);
+  }
   async function lambda(ast) {
     let body_block = js_declaration_single_block_blody(ast);
     js_call_insert(todo.name, [], body_block, 0);
