f9647dc10f397f4bef3d671cda879b049d72df0b
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:03:41 -0400

    function_auto_path public\src\html_button_element.mjs

--- web/public/src/html_button_element.mjs
+++ web/public/src/html_button_element.mjs
@@ -1,8 +1,8 @@
 import { marker } from "./marker.mjs";
 import { html_element } from "./html_element.mjs";
-export function html_button_element(body) {
+export function html_button_element(parent) {
   marker("1");
   const tag_name = "button";
-  let b = html_element(body, tag_name);
+  let b = html_element(parent, tag_name);
   return b;
 }
