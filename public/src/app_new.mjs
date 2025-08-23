06a209182c1b496ee3dc2476542397affe46bdfc
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 14:56:05 -0400

    mpa name

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,6 +1,6 @@
 import { html_new } from "./html_new.mjs";
 import { marker } from "./marker.mjs";
-export async function app_new() {
+export async function app_new(name) {
   marker("1");
   await html_new(name);
 }
