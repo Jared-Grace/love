0c98ea81f4a499940a792a47c940b259351cf770
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:17:29 -0400

    function_auto_path public\src\function_rename_suffix_add.mjs

--- public/src/function_rename_suffix_add.mjs
+++ public/src/function_rename_suffix_add.mjs
@@ -1,9 +1,9 @@
 import { function_name_combine } from "./function_name_combine.mjs";
 import { function_rename } from "./function_rename.mjs";
 import { marker } from "./marker.mjs";
-export async function function_rename_suffix_add(f_name_before, f_name_after) {
+export async function function_rename_suffix_add(f_name_before, suffix) {
   marker("1");
-  let combined = function_name_combine(left, right);
+  let f_name_after = function_name_combine(f_name_before, suffix);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
