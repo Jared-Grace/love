12c8ad74d7d3d7f0162a7366bb791eaae6ed604b
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:42:54 -0400

    function_auto_path public\src\js_imports_paths_fix.mjs

--- web/public/src/js_imports_paths_fix.mjs
+++ web/public/src/js_imports_paths_fix.mjs
@@ -1,7 +1,6 @@
 import { object_property_set } from "./object_property_set.mjs";
 import { js_string } from "./js_string.mjs";
 import { function_name_to_path_import } from "./function_name_to_path_import.mjs";
-import { log } from "./log.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { js_imports_declarations } from "./js_imports_declarations.mjs";
@@ -14,7 +13,6 @@ export function js_imports_paths_fix(ast) {
     const from = function_name_to_path_import(name);
     let s = js_string(from);
     object_property_set(declaration, "source", s);
-    log(declaration);
   }
   each(imports_declarations, lambda);
   marker("1");
