fea4a2b5764ecdc8a67329b85f74bd3bb897d1fc
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:24:40 -0400

    function_auto_path public\src\function_params_consolidate.mjs

--- web/public/src/function_params_consolidate.mjs
+++ web/public/src/function_params_consolidate.mjs
@@ -1,3 +1,4 @@
+import { js_property_identifier } from "./js_property_identifier.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { list_empty } from "./list_empty.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
@@ -5,7 +6,6 @@ import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { list_map } from "./list_map.mjs";
-import { js_parse_expression } from "./js_parse_expression.mjs";
 import { list_add } from "./list_add.mjs";
 export async function function_params_consolidate(f_name) {
   async function lambda(ast) {
@@ -13,16 +13,7 @@ export async function function_params_consolidate(f_name) {
     let params = js_declaration_params_get(declaration);
     const params_names = js_identifiers_to_names(params);
     function lambda2(param_name) {
-      let expression = js_parse_expression(param_name);
-      let v = {
-        type: "Property",
-        method: false,
-        shorthand: true,
-        computed: false,
-        key: expression,
-        value: expression,
-        kind: "init",
-      };
+      let v = js_property_identifier(param_name);
       return v;
     }
     let properties = list_map(params_names, lambda2);
