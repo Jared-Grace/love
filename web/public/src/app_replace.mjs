cc82608380bc8dccb13b3a5b01dd6f72d94232a4
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:07:37 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -3,7 +3,6 @@ import { html_clear } from "./html_clear.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace() {
-  let app_fn = app_replace;
   let body = html_document_body();
   html_clear(body);
   marker("1");
