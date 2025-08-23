0aeccd011a0319ec68b6f6cff127e3bd0a017397
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:41:16 -0400

    function_auto_path public\src\app_refresh.mjs

--- web/public/src/app_refresh.mjs
+++ web/public/src/app_refresh.mjs
@@ -1,13 +1,14 @@
-import {object_property_get} from "./object_property_get.mjs";
-import {storage_local_get_or} from "./storage_local_get_or.mjs";
-import {marker} from "./marker.mjs";
-import {html_clear} from "./html_clear.mjs";
-import {html_document_body} from "./html_document_body.mjs";
-export function app_refresh(app_fn, screens) {
+import { object_property_get } from "./object_property_get.mjs";
+import { storage_local_get_or } from "./storage_local_get_or.mjs";
+import { marker } from "./marker.mjs";
+import { html_clear } from "./html_clear.mjs";
+import { html_document_body } from "./html_document_body.mjs";
+export function app_refresh(context) {
+  let app_fn = context;
   let body = html_document_body();
   html_clear(body);
   marker("1");
   let screen_name = storage_local_get_or(app_fn, "screen", "home");
   let screen = object_property_get(screens, screen_name);
-  screen();
+  screen(context);
 }
