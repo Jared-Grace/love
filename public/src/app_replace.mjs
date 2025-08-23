60aab217e020adc427ef35070de0a32f6fbc8b58
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:13:14 -0400

    oa replace

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,4 +1,6 @@
+import { marker } from "./marker.mjs";
 import { app_replace_refresh } from "./app_replace_refresh.mjs";
 export function app_replace() {
+  marker("1");
   app_replace_refresh();
 }
