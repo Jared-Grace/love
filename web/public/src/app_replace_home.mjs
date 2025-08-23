c413ade783997619e752b1775c32d7552205225c
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:07:41 -0400

    function_auto_path public\src\app_replace_home.mjs

--- web/public/src/app_replace_home.mjs
+++ web/public/src/app_replace_home.mjs
@@ -1,17 +1,22 @@
-import {each} from "./each.mjs";
-import {storage_local_set} from "./storage_local_set.mjs";
-import {html_button} from "./html_button.mjs";
-import {object_property_get} from "./object_property_get.mjs";
-import {html_document_body} from "./html_document_body.mjs";
+import { app_replace } from "./app_replace.mjs";
+import { each } from "./each.mjs";
+import { storage_local_set } from "./storage_local_set.mjs";
+import { html_button } from "./html_button.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { html_document_body } from "./html_document_body.mjs";
 export function app_replace_home() {
+  let app_fn = app_replace;
   let body = html_document_body();
-  let rule_sets = [{
-    name: "Grow",
-    rules: ["a > a a"]
-  }, {
-    name: "Shrink",
-    rules: ["a a > a"]
-  }];
+  let rule_sets = [
+    {
+      name: "Grow",
+      rules: ["a > a a"],
+    },
+    {
+      name: "Shrink",
+      rules: ["a a > a"],
+    },
+  ];
   function lambda2(rs) {
     let name2 = object_property_get(rs, "name");
     html_button(body, name2, lambda);
