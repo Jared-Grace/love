870d28db2389a142104ceaff363cf4969b2079ac
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:19:38 -0400

    function_auto_path public\src\marker_screen_add.mjs

--- public/src/marker_screen_add.mjs
+++ public/src/marker_screen_add.mjs
@@ -4,22 +4,18 @@ import { log } from "./log.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { data_app_current_get } from "./data_app_current_get.mjs";
 import { function_current_set } from "./function_current_set.mjs";
-import { data_function_current_restore } from "./data_function_current_restore.mjs";
 import { marker_next_get } from "./marker_next_get.mjs";
 import { marker } from "./marker.mjs";
 export async function marker_screen_add(screen_name) {
-  async function lambda3() {
-    let a_name = await data_app_current_get();
-    let prefixed = app_name_prefixed(a_name);
-    let combined = function_name_combine(prefixed, "screens");
-    await function_current_set(combined);
-    async function lambda(a) {
-      let { next } = marker_next_get(a);
-      log(next);
-    }
-    let v2 = await function_transform_marker_specified("screens", lambda);
+  let a_name = await data_app_current_get();
+  let prefixed = app_name_prefixed(a_name);
+  let combined = function_name_combine(prefixed, "screens");
+  await function_current_set(combined);
+  async function lambda(a) {
+    let { next } = marker_next_get(a);
+    log(next);
   }
-  await data_function_current_restore(lambda3);
+  let v2 = await function_transform_marker_specified("screens", lambda);
   marker("1");
   return v;
 }
