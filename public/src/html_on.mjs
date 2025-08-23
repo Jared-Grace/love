62d41f54720c0b146781568fa2f16786c7ce12fa
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:45:28 -0400

    function_auto_path public\src\html_on.mjs

--- public/src/html_on.mjs
+++ public/src/html_on.mjs
@@ -1,5 +1,5 @@
-import {html_component_element_get} from "./html_component_element_get.mjs";
+import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_on(component, name_event, lambda) {
   let element = html_component_element_get(component);
-  element.on(name_event, lambda);
+  element.addEventListener(name_event, lambda);
 }
