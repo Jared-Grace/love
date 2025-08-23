55ed28a098496880cece83e01e64639551a60e90
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:42:31 -0400

    function_auto_path public\src\js_string.mjs

--- web/public/src/js_string.mjs
+++ web/public/src/js_string.mjs
@@ -1,9 +1,9 @@
-import {object_property_delete} from "./object_property_delete.mjs";
-import {object_property_set} from "./object_property_set.mjs";
-import {js_parse_expression} from "./js_parse_expression.mjs";
+import { object_property_delete } from "./object_property_delete.mjs";
+import { object_property_set } from "./object_property_set.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_string(value_string) {
-  let parsed_string = js_parse_expression('""');
-  object_property_set(parsed_string, "value", value_string);
-  object_property_delete(parsed_string, "raw");
-  return parsed_string;
+  let s = js_parse_expression('""');
+  object_property_set(s, "value", value_string);
+  object_property_delete(s, "raw");
+  return s;
 }
