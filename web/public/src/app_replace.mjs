7396123947eff0f4f754e7c9e9301a2be5052941
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 15:16:07 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -4,7 +4,5 @@ export function app_replace() {
   let body = html_document_body();
   marker("1");
   const newDiv = document.createElement("div");
-  newDiv.textContent = "Hello, world!";
-  const parent = document.getElementById("container");
   parent.appendChild(newDiv);
 }
