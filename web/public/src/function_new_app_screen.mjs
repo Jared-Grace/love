3cdf3d965ffcd6d48b23a5fb3805577f9165b724
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:25:32 -0400

    function_auto_path public\src\function_new_app_screen.mjs

--- web/public/src/function_new_app_screen.mjs
+++ web/public/src/function_new_app_screen.mjs
@@ -1,7 +1,9 @@
+import { app_name_prefixed } from "./app_name_prefixed.mjs";
 import { function_new } from "./function_new.mjs";
 import { marker } from "./marker.mjs";
 export async function function_new_app_screen(f_name) {
+  let a_name = app_name_prefixed(f_name);
   marker("1");
-  let v = await function_new(f_name);
+  let v = await function_new(a_name);
   return v;
 }
