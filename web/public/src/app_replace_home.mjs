0c943348917f7c68dbce0c44f32cf1c41d4cf5bb
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:07:28 -0400

    function_auto_path public\src\app_replace.mjs

---
+++ web/public/src/app_replace_home.mjs
@@ -0,0 +1,25 @@
+import {each} from "./each.mjs";
+import {storage_local_set} from "./storage_local_set.mjs";
+import {html_button} from "./html_button.mjs";
+import {object_property_get} from "./object_property_get.mjs";
+import {html_document_body} from "./html_document_body.mjs";
+export function app_replace_home() {
+  let body = html_document_body();
+  let rule_sets = [{
+    name: "Grow",
+    rules: ["a > a a"]
+  }, {
+    name: "Shrink",
+    rules: ["a a > a"]
+  }];
+  function lambda2(rs) {
+    let name2 = object_property_get(rs, "name");
+    html_button(body, name2, lambda);
+    function lambda() {
+      let key = "screen";
+      const value = "rule_set";
+      storage_local_set(app_fn, key, value);
+    }
+  }
+  each(rule_sets, lambda2);
+}
