2e36618666cb44accf08474b71a20afa546b0b0c
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:33:06 -0400

    function_auto_path public\src\js_property.mjs

--- web/public/src/js_property.mjs
+++ web/public/src/js_property.mjs
@@ -2,7 +2,7 @@ export function js_property(key, value) {
   let p = {
     type: "Property",
     method: false,
-    shorthand: true,
+    shorthand: false,
     computed: false,
     key: key,
     value: value,
