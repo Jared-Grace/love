32e500e4de4bfde430311eff904502a667ec1937
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 13:01:03 -0400

    mc fpc

--- web/public/src/app_html_open.mjs
+++ web/public/src/app_html_open.mjs
@@ -1,6 +1,8 @@
+import { folder_public_combine } from "./folder_public_combine.mjs";
 import { file_name_html } from "./file_name_html.mjs";
 import { marker } from "./marker.mjs";
 export function app_html_open(a_name) {
   let file_name = file_name_html(a_name);
   marker("1");
+  let v = folder_public_combine(second);
 }
