c3557847e49597ea655b550f382b18b34f58ce25
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:28:45 -0400

    u u

--- web/public/src/html_button_element.mjs
+++ web/public/src/html_button_element.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { html_element } from "./html_element.mjs";
-export function html_button(body) {
+export function html_button_element(body) {
+  marker("1");
   const tag_name = "button";
   let b = html_element(body, tag_name);
   return b;
