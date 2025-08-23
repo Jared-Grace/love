65651e5ba206160a07a9cc5f8c23e9d0055347d0
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:46:10 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -1,6 +1,8 @@
+import { html_button } from "./html_button.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_rule_set() {
   let body = html_document_body();
+  html_button(parent, text, function lambda2() {});
   marker("1");
 }
