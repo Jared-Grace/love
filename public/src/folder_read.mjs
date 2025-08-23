cd7ff2224b509e633c45b7a28b706434a44e6a0d
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:51:53 -0400

    function_auto_path public\src\folder_read.mjs

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,11 +1,11 @@
 import { path_join } from "./path_join.mjs";
 import fs from "fs";
-export function folder_read(dirPath) {
+export function folder_read(path_folder) {
   function lambda(file) {
-    let result = path_join([dirPath, file]);
+    let result = path_join([path_folder, file]);
     let v = fs.statSync(result).isFile();
     return v;
   }
-  let v2 = fs.readdirSync(dirPath).filter(lambda);
+  let v2 = fs.readdirSync(path_folder).filter(lambda);
   return v2;
 }
