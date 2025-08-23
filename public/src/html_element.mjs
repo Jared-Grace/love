067606453a80a62dee313a1c5e093b15171af3fb
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:04:42 -0400

    function_auto_path public\src\html_element.mjs

--- public/src/html_element.mjs
+++ public/src/html_element.mjs
@@ -1,6 +1,8 @@
+import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_element(parent, tag_name) {
+  let element = html_component_element_get(parent);
   const e = document.createElement(tag_name);
-  parent.appendChild(e);
+  element.appendChild(e);
   let v = {
     element: e,
   };
