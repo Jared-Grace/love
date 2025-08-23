a413e8374624cb0978859b9f96cb2d8bfd9599e1
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:39:16 -0400

    function_auto_path public\src\js_imports_paths_fix.mjs

--- web/public/src/js_imports_paths_fix.mjs
+++ web/public/src/js_imports_paths_fix.mjs
@@ -1,9 +1,13 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { each } from "./each.mjs";
 import { js_imports_declarations } from "./js_imports_declarations.mjs";
 import { marker } from "./marker.mjs";
 export function js_imports_paths_fix(ast) {
   let imports_declarations = js_imports_declarations(ast);
-  function lambda(i) {}
+  function lambda(i) {
+    let name = object_property_get(i, "name");
+    let declaration = object_property_get(i, "declaration");
+  }
   each(imports_declarations, lambda);
   marker("1");
 }
