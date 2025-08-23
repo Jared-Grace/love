a5e0da90d723a56177b0c2c199b7da1bbbcb56f4
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:21:03 -0400

    function_auto_path public\src\storage_local_get_or.mjs

--- public/src/storage_local_get_or.mjs
+++ public/src/storage_local_get_or.mjs
@@ -2,5 +2,6 @@ import { storage_local_get } from "./storage_local_get.mjs";
 import { marker } from "./marker.mjs";
 export function storage_local_get_or(app_fn, key) {
   marker("1");
-  return storage_local_get(app_fn, key);
+  let value = storage_local_get(app_fn, key);
+  return value;
 }
