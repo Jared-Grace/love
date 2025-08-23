be2084368867f35fa07f8e8939251ad8b651b4c6
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:48:01 -0400

    data_file_update public\src\lambda_value.mjs

--- web/public/src/lambda_value.mjs
+++ web/public/src/lambda_value.mjs
@@ -1,7 +1,8 @@
 import { marker } from "./marker.mjs";
 export function lambda_value(value) {
   marker("1");
-  return function lambda() {
+  let v = function lambda() {
     return value;
   };
+  return v;
 }
