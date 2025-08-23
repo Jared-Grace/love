2bdf5d47e29084d8b95f9e7bd0f4a24e91c885be
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 19:23:06 -0400

    function_auto_path public\src\js_declare_single.mjs

--- public/src/js_declare_single.mjs
+++ public/src/js_declare_single.mjs
@@ -1,10 +1,10 @@
-import {list_single} from "./list_single.mjs";
-import {assert} from "./assert.mjs";
-import {js_node_type_is} from "./js_node_type_is.mjs";
+import { list_single } from "./list_single.mjs";
+import { assert } from "./assert.mjs";
+import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_declare_single(vd) {
   let b = js_node_type_is(vd, "VariableDeclaration");
   assert(b);
-  let {declarations} = vd;
-  let declarators = list_single(declarations);
-  return declarators;
+  let { declarations } = vd;
+  let declarator = list_single(declarations);
+  return declarator;
 }
