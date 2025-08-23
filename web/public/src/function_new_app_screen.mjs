1a168a649b77ff718a5304d33fc11824e1cb6ca4
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:26:33 -0400

    function_auto_path public\src\function_new_app_screen.mjs

--- web/public/src/function_new_app_screen.mjs
+++ web/public/src/function_new_app_screen.mjs
@@ -1,8 +1,10 @@
+import { data_property_get } from "./data_property_get.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { app_name_prefixed } from "./app_name_prefixed.mjs";
 import { function_new } from "./function_new.mjs";
 import { marker } from "./marker.mjs";
 export async function function_new_app_screen(screen_name) {
+  let value = await data_property_get(property_name);
   let a_name = app_name_prefixed(screen_name);
   let combined = function_name_combine(a_name, screen_name);
   marker("1");
