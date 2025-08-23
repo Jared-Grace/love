99f414cb2c6a2d52dee3c1132bd1e31b116a150e
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:51:52 -0400

    function_auto_path public\src\html_element.mjs

--- web/public/src/html_element.mjs
+++ web/public/src/html_element.mjs
@@ -1,5 +1,8 @@
 export function html_element(parent, tag_name) {
   const e = document.createElement(tag_name);
   parent.appendChild(e);
-  return e;
+  let v = {
+    element: e,
+  };
+  return v;
 }
