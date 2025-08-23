53589fc3f6e3560ad1a09840bd69848edfe579ad
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 17:57:58 -0400

    function_auto_path public\src\function_open_app.mjs

--- web/public/src/function_open_app.mjs
+++ web/public/src/function_open_app.mjs
@@ -3,7 +3,7 @@ import { function_open } from "./function_open.mjs";
 import { marker } from "./marker.mjs";
 export async function function_open_app(f_name) {
   marker("1");
-  let a_name = app_name_prefixed(name);
-  let v = await function_open(f_name);
+  let a_name = app_name_prefixed(f_name);
+  let v = await function_open(a_name);
   return v;
 }
