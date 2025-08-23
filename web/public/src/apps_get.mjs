9563ec2fb6a9f1868bdc0e88db6ad6d423828cc5
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 12:53:54 -0400

    ir dirPath fop

--- web/public/src/apps_get.mjs
+++ web/public/src/apps_get.mjs
@@ -4,5 +4,5 @@ import { marker } from "./marker.mjs";
 export async function apps_get() {
   marker("1");
   let fop = folder_public();
-  let v2 = folder_read(dirPath);
+  let v2 = folder_read(fop);
 }
