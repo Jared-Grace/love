aeb992fa4dfad4c1f247a27cf5b49219a9864678
Grace-Jared <christrosetolife@gmail.com>
2025-08-23 18:21:19 -0400

    function_auto_path public\src\storage_local_get_or.mjs

--- web/public/src/storage_local_get_or.mjs
+++ web/public/src/storage_local_get_or.mjs
@@ -1,8 +1,10 @@
+import { npm_install } from "./npm_install.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { marker } from "./marker.mjs";
-export function storage_local_get_or(app_fn, key, value_or) {
+export async function storage_local_get_or(app_fn, key, value_or) {
   marker("1");
   let value = storage_local_get(app_fn, key);
+  await npm_install(package_name);
   if (false) {
   }
   return value;
