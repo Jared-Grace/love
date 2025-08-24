f2f285c0508f591b0e9da1b3a87c5d4a46f34d4f
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:37:58 -0400

    function_auto_path public\src\storage_local_initialize_context.mjs

--- web/public/src/storage_local_initialize_context.mjs
+++ web/public/src/storage_local_initialize_context.mjs
@@ -1,8 +1,8 @@
+import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { marker } from "./marker.mjs";
-import { storage_local_get } from "./storage_local_get.mjs";
 export function storage_local_initialize_context(context, key) {
   marker("1");
   let { app_fn } = context;
-  let value = storage_local_get(app_fn, key);
+  let value = storage_local_initialize(app_fn, key);
   return value;
 }
