852e34090a35200df0a434fc9c3c61c5fb40814e
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:31:10 -0400

    function_auto_path public\src\app_replace_home.mjs

--- web/public/src/app_replace_home.mjs
+++ web/public/src/app_replace_home.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_sets } from "./app_replace_rule_sets.mjs";
 import { each_index } from "./each_index.mjs";
 import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
@@ -5,16 +6,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 export function app_replace_home(context) {
   let body = html_document_body();
-  let rule_sets = [
-    {
-      name: "Grow",
-      rules: ["a > a a"],
-    },
-    {
-      name: "Shrink",
-      rules: ["a a > a"],
-    },
-  ];
+  let rule_sets = app_replace_rule_sets();
   function lambda2(item, index) {
     let name2 = object_property_get(item, "name");
     storage_local_set_context(context, "rule_set_index", index);
---
+++ web/public/src/app_replace_rule_sets.mjs
@@ -0,0 +1,10 @@
+export function app_replace_rule_sets() {
+  let v = [{
+    name: "Grow",
+    rules: ["a > a a"]
+  }, {
+    name: "Shrink",
+    rules: ["a a > a"]
+  }];
+  return v;
+}
