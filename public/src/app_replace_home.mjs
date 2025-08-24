eabf9ac257bb3ccc8709e241b4a19bec65eaaa1d
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:34:14 -0400

    function_auto_path public\src\app_replace_home.mjs

--- public/src/app_replace_home.mjs
+++ public/src/app_replace_home.mjs
@@ -4,7 +4,7 @@ import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function app_replace_home(context) {
-  let root = context;
+  let { root } = context;
   let rule_sets = app_replace_rule_sets();
   function lambda2(item, index) {
     let name2 = object_property_get(item, "name");
