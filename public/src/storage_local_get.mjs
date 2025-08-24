946261e3583ec6a43bd7bd2beba7afb69c85d70f
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:37:23 -0400

    function_auto_path public\src\storage_local_get.mjs

--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,8 +1,10 @@
+import { json_from } from "./json_from.mjs";
 import { marker } from "./marker.mjs";
 import { storage_local_key_get } from "./storage_local_key_get.mjs";
 export function storage_local_get(app_fn, key) {
   marker("1");
   let storage_local_key = storage_local_key_get(app_fn, key);
-  let value = localStorage.getItem(storage_local_key);
+  let json = localStorage.getItem(storage_local_key);
+  let v = json_from(json2);
   return value;
 }
