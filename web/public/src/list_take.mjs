1161bf597e153a3a5aae26ebb4f1920f8b4f2daf
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:04:29 -0400

    function_auto_path public\src\list_take.mjs

--- web/public/src/list_take.mjs
+++ web/public/src/list_take.mjs
@@ -2,6 +2,6 @@ import { list_slice } from "./list_slice.mjs";
 import { marker } from "./marker.mjs";
 export function list_take(list, count) {
   marker("1");
-  let v = list_slice(list, 0, count);
-  return v;
+  let taken = list_slice(list, 0, count);
+  return taken;
 }
