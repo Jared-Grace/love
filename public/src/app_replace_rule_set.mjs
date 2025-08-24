a2c665f61281d2a237f5240df84f77f7b2f61760
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 19:25:37 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -35,6 +35,7 @@ export function app_replace_rule_set(context) {
     let b = null;
     function lambda5() {
       html_style_set(b, "background-color", "lightgreen");
+      let index_selected = index;
     }
     b = html_button(root, letter, lambda5);
   }
