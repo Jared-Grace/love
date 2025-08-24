bcc284f9af14460ce381c2e2353f46c59dd5891a
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:23:30 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -2,7 +2,7 @@ import { emoji_home } from "./emoji_home.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
-export function app_replace_rule_set() {
+export function app_replace_rule_set(context) {
   let body = html_document_body();
   html_button_screen(body, emoji_home() + "Home", context, "home");
   marker("1");
