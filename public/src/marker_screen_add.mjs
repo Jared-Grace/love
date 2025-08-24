f3d8e3c8e42a762b6084547bf597b3bf6e59763d
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:24:32 -0400

    function_auto_path public\src\marker_screen_add.mjs

--- public/src/marker_screen_add.mjs
+++ public/src/marker_screen_add.mjs
@@ -1,3 +1,4 @@
+import { function_param_new } from "./function_param_new.mjs";
 import { function_new } from "./function_new.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { list_add } from "./list_add.mjs";
@@ -25,6 +26,7 @@ export async function marker_screen_add(screen_name) {
     let p = js_property(key, value);
     list_add(properties, p);
     await function_new(combined_screen);
+    await function_param_new(param_name);
   }
   let v2 = await function_transform_marker_specified(
     combined,
