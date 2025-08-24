b0f732393764a331ea1347a758d049116743c497
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:17:00 -0400

    function_auto_path public\src\app_replace_rule_sets.mjs

--- public/src/app_replace_rule_sets.mjs
+++ public/src/app_replace_rule_sets.mjs
@@ -1,10 +1,15 @@
 export function app_replace_rule_sets() {
-  let v = [{
-    name: "Grow",
-    rules: ["a > a a"]
-  }, {
-    name: "Shrink",
-    rules: ["a a > a"]
-  }];
+  let v = [
+    {
+      name: "Grow",
+      rules: ["a > a a"],
+      start: "a",
+    },
+    {
+      name: "Shrink",
+      rules: ["a a > a"],
+      start: "aaaaa",
+    },
+  ];
   return v;
 }
