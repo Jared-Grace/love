e88f48d570e5c565a1a936e3d7f875f9ec88c29c
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:25:51 -0400

    function_auto_path public\src\app_screen_set.mjs

--- web/public/src/app_screen_set.mjs
+++ web/public/src/app_screen_set.mjs
@@ -1,7 +1,6 @@
 import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { app_refresh } from "./app_refresh.mjs";
 export function app_screen_set(context, value) {
-  const key = "screen";
-  storage_local_set_context(context, key, value);
+  storage_local_set_context(context, "screen", value);
   app_refresh(context);
 }
