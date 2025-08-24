bd0408ad94ebd0226435f654ec5b517cbc9f98d3
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:35:10 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -1,3 +1,4 @@
+import { html_p } from "./html_p.mjs";
 import { html_text_set } from "./html_text_set.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
 import { list_get } from "./list_get.mjs";
@@ -5,7 +6,6 @@ import { app_replace_rule_sets } from "./app_replace_rule_sets.mjs";
 import { emoji_home } from "./emoji_home.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
 import { marker } from "./marker.mjs";
-import { html_element } from "./html_element.mjs";
 import { json_to } from "./json_to.mjs";
 export function app_replace_rule_set(context) {
   let { root } = context;
@@ -14,7 +14,7 @@ export function app_replace_rule_set(context) {
   let rule_sets = app_replace_rule_sets();
   let item = list_get(rule_sets, index);
   let text = json_to(item);
-  let p = html_element(root, "p");
+  let p = html_p(root);
   html_text_set(p, text);
   marker("1");
 }
---
+++ public/src/html_p.mjs
@@ -0,0 +1,5 @@
+import {html_element} from "./html_element.mjs";
+export function html_p(root) {
+  let component = html_element(root, "p");
+  return component;
+}
