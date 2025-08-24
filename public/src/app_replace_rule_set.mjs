d8721aec5b24e08238d289695833e34b82329215
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:35:19 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -1,5 +1,4 @@
-import { html_p } from "./html_p.mjs";
-import { html_text_set } from "./html_text_set.mjs";
+import { html_p_text } from "./html_p_text.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
 import { list_get } from "./list_get.mjs";
 import { app_replace_rule_sets } from "./app_replace_rule_sets.mjs";
@@ -14,7 +13,6 @@ export function app_replace_rule_set(context) {
   let rule_sets = app_replace_rule_sets();
   let item = list_get(rule_sets, index);
   let text = json_to(item);
-  let p = html_p(root);
-  html_text_set(p, text);
+  html_p_text(root, text);
   marker("1");
 }
