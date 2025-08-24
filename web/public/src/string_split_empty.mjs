1551c4228068cbf00b173966c0427a5f44c54c7a
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 17:01:39 -0400

    function_auto_path public\src\string_split_empty.mjs

--- web/public/src/string_split_empty.mjs
+++ web/public/src/string_split_empty.mjs
@@ -1,5 +1,5 @@
-import {string_split} from "./string_split.mjs";
-export function string_split_empty() {
-  let split2 = string_split(s, '');
+import { string_split } from "./string_split.mjs";
+export function string_split_empty(s) {
+  let split2 = string_split(s, "");
   return split2;
 }
