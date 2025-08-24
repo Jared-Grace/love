44048607046ef87b46950f3eceff1cdbb3e54f44
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:37:50 -0400

    c storage_local_get_context storage_local_initialize_context

--- web/public/src/storage_local_initialize_context.mjs
+++ web/public/src/storage_local_initialize_context.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 export function storage_local_initialize_context(context, key) {
+  marker("1");
   let { app_fn } = context;
   let value = storage_local_get(app_fn, key);
   return value;
