394bebb4a5fdf77f81b8699d447ae53418095cc3
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:22:27 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -3,6 +3,6 @@ import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_rule_set() {
   let body = html_document_body();
-  html_button_screen(body, "Home", context, "home");
+  html_button_screen(body, "🏠" + "Home", context, "home");
   marker("1");
 }
