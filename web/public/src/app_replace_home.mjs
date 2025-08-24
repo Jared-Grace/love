99448f43551ed29232791828b424f09eb0f8975d
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:34:08 -0400

    function_auto_path public\src\app_replace_home.mjs

--- web/public/src/app_replace_home.mjs
+++ web/public/src/app_replace_home.mjs
@@ -3,14 +3,13 @@ import { each_index } from "./each_index.mjs";
 import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { html_document_body } from "./html_document_body.mjs";
 export function app_replace_home(context) {
-  let body = html_document_body();
+  let root = context;
   let rule_sets = app_replace_rule_sets();
   function lambda2(item, index) {
     let name2 = object_property_get(item, "name");
     storage_local_set_context(context, "rule_set_index", index);
-    html_button_screen(body, name2, context, "rule_set");
+    html_button_screen(root, name2, context, "rule_set");
   }
   each_index(rule_sets, lambda2);
 }
