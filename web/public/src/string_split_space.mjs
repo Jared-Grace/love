81cf1e38a33784478782f36c94932fe260c6c458
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:59:56 -0400

    function_auto_path public\src\string_split_space.mjs

--- web/public/src/string_split_space.mjs
+++ web/public/src/string_split_space.mjs
@@ -1,5 +1,5 @@
-import {string_split} from "./string_split.mjs";
-export function string_split_space(rule) {
-  let split2 = string_split(rule, ' ');
+import { string_split } from "./string_split.mjs";
+export function string_split_space(s) {
+  let split2 = string_split(s, " ");
   return split2;
 }
