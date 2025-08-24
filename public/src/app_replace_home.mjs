bc2fb592bf192fef9f1ba8952b855be1359b372c
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:27:21 -0400

    function_auto_path public\src\app_replace_home.mjs

--- public/src/app_replace_home.mjs
+++ public/src/app_replace_home.mjs
@@ -1,7 +1,6 @@
 import { each_index } from "./each_index.mjs";
 import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
-import { each } from "./each.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 export function app_replace_home(context) {
@@ -16,12 +15,10 @@ export function app_replace_home(context) {
       rules: ["a a > a"],
     },
   ];
-  function lambda2(rs) {
+  function lambda2(item, index) {
     let name2 = object_property_get(rs, "name");
     storage_local_set_context(context, "rule_set_index", value);
     html_button_screen(body, name2, context, "rule_set");
   }
-  each(rule_sets, lambda2);
-  function lambda(item, index) {}
-  let v = each_index(list, lambda);
+  each_index(rule_sets, lambda2);
 }
