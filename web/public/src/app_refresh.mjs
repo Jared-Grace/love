723adf63698854b7841400684286a36e6b977da6
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:34:56 -0400

    function_rename_identifiers storage_local_get_or storage_local_initialize

--- web/public/src/app_refresh.mjs
+++ web/public/src/app_refresh.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from "./object_property_get.mjs";
-import { storage_local_get_or } from "./storage_local_get_or.mjs";
+import { storage_local_initialize } from "./storage_local_initialize.mjs";
 import { marker } from "./marker.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { html_document_body } from "./html_document_body.mjs";
@@ -8,7 +8,7 @@ export function app_refresh(context) {
   let body = html_document_body();
   html_clear(body);
   marker("1");
-  let screen_name = storage_local_get_or(app_fn, "screen", "home");
+  let screen_name = storage_local_initialize(app_fn, "screen", "home");
   let screen = object_property_get(screens, screen_name);
   screen(context);
 }
