38e1a2bf83f5b796d1d092795281e559b90bbf8d
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:34:56 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -6,13 +6,15 @@ import { emoji_home } from "./emoji_home.mjs";
 import { html_button_screen } from "./html_button_screen.mjs";
 import { marker } from "./marker.mjs";
 import { html_element } from "./html_element.mjs";
+import { json_to } from "./json_to.mjs";
 export function app_replace_rule_set(context) {
   let { root } = context;
   html_button_screen(root, emoji_home() + "Home", context, "home");
   let index = storage_local_get_context(context, "rule_set_index");
   let rule_sets = app_replace_rule_sets();
   let item = list_get(rule_sets, index);
-  html_element(root, "p");
-  html_text_set(component, text);
+  let text = json_to(item);
+  let p = html_element(root, "p");
+  html_text_set(p, text);
   marker("1");
 }
