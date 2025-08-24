848806ae85729f2b966344813e8dd9d32155d420
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:37:06 -0400

    function_auto_path public\src\storage_local_initialize.mjs

--- public/src/storage_local_initialize.mjs
+++ public/src/storage_local_initialize.mjs
@@ -2,13 +2,13 @@ import { storage_local_set } from "./storage_local_set.mjs";
 import { null_is } from "./null_is.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { marker } from "./marker.mjs";
-export function storage_local_initialize(app_fn, key, value_or) {
+export function storage_local_initialize(app_fn, key, value_initial) {
   marker("1");
   let value = storage_local_get(app_fn, key);
   let n = null_is(value);
   if (n) {
-    storage_local_set(app_fn, key, value_or);
-    return value_or;
+    storage_local_set(app_fn, key, value_initial);
+    return value_initial;
   }
   return value;
 }
