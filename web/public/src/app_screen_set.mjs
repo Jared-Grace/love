5ad7a0c5c348e937edc8bce19ce9ca9e7f968dfb
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:25:37 -0400

    function_auto_path public\src\app_screen_set.mjs

--- web/public/src/app_screen_set.mjs
+++ web/public/src/app_screen_set.mjs
@@ -1,7 +1,8 @@
-import {app_refresh} from "./app_refresh.mjs";
-import {storage_local_set} from "./storage_local_set.mjs";
+import { app_refresh } from "./app_refresh.mjs";
+import { storage_local_set } from "./storage_local_set.mjs";
 export function app_screen_set(context, value) {
-  let {app_fn} = context;
-  storage_local_set(app_fn, "screen", value);
+  const key = "screen";
+  let { app_fn } = context;
+  storage_local_set(app_fn, key, value);
   app_refresh(context);
 }
