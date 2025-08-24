05032b73a510b2b5eb5f18e4e5afd3e589e927bf
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 15:52:58 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -14,7 +14,8 @@ export function app_replace_rule_set(context) {
   let rule_sets = app_replace_rule_sets();
   let item = list_get(rule_sets, index);
   let { rules } = item;
-  each(list, function lambda(item2) {});
+  function lambda(item2) {}
+  each(list, lambda);
   let text = json_to(item);
   html_p_text(root, text);
   marker("1");
