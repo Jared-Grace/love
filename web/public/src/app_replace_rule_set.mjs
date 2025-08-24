41023ae30250ab675ec2f4a520c8d367effe63aa
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:07:58 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -1,8 +1,5 @@
+import { app_replace_rule_parse } from "./app_replace_rule_parse.mjs";
 import { list_map } from "./list_map.mjs";
-import { list_skip } from "./list_skip.mjs";
-import { list_take } from "./list_take.mjs";
-import { list_index_of } from "./list_index_of.mjs";
-import { string_split_space } from "./string_split_space.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { storage_local_get_context } from "./storage_local_get_context.mjs";
 import { list_get } from "./list_get.mjs";
@@ -18,17 +15,6 @@ export function app_replace_rule_set(context) {
   let rule_sets = app_replace_rule_sets();
   let item = list_get(rule_sets, index);
   let { rules } = item;
-  function app_replace_rule_parse(rule) {
-    let split = string_split_space(rule);
-    let middle = list_index_of(split, ">");
-    let left = list_take(split, middle);
-    let right = list_skip(split, middle + 1);
-    let v = {
-      left,
-      right,
-    };
-    return v;
-  }
   let mapped = list_map(rules, app_replace_rule_parse);
   let text = json_to(item);
   html_p_text(root, text);
