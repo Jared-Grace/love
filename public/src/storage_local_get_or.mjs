7cfd43b70656c2964e6bed9d14c151598a50a898
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 16:28:25 -0400

    function_auto_path public\src\storage_local_get_or.mjs

--- public/src/storage_local_get_or.mjs
+++ public/src/storage_local_get_or.mjs
@@ -1,3 +1,4 @@
+import { storage_local_set } from "./storage_local_set.mjs";
 import { null_is } from "./null_is.mjs";
 import { storage_local_get } from "./storage_local_get.mjs";
 import { marker } from "./marker.mjs";
@@ -6,7 +7,7 @@ export function storage_local_get_or(app_fn, key, value_or) {
   let value = storage_local_get(app_fn, key);
   let n = null_is(value);
   if (n) {
-    let value2 = storage_local_get(app_fn2, key2);
+    storage_local_set(app_fn2, key2, value2);
     return value_or;
   }
   return value;
