c90dd3841e7520c13e1bbd1ca1b52cdd5ce09883
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:46:00 -0400

    function_auto_path public\src\list_any.mjs

--- public/src/list_any.mjs
+++ public/src/list_any.mjs
@@ -4,6 +4,6 @@ import { list_filter } from "./list_filter.mjs";
 export function list_any(list, lambda$item) {
   marker("1");
   let filtered = list_filter(list, lambda$item);
-  let ne = list_empty_not_is(filtered);
-  return ne;
+  let any = list_empty_not_is(filtered);
+  return any;
 }
