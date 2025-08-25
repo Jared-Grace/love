6964daa78010dec6fbd1596bc4b868a71c3734b5
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:24:45 -0400

    c object_property_initialize object_property_lambda

--- web/public/src/object_property_lambda.mjs
+++ web/public/src/object_property_lambda.mjs
@@ -1,8 +1,10 @@
+import { marker } from "./marker.mjs";
 import { not } from "./not.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function object_property_lambda(object, property_name, value) {
+  marker("1");
   const exists = object_property_exists(object, property_name);
   if (not(exists)) {
     object_property_set(object, property_name, value);
