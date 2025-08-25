46c03a68528692bf57d3e8ef7c8a57e86794c2e2
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:56:35 -0400

    function_auto_path public\src\string_replace.mjs

--- web/public/src/string_replace.mjs
+++ web/public/src/string_replace.mjs
@@ -4,5 +4,6 @@ import { marker } from "./marker.mjs";
 export function string_replace(s, from, to) {
   marker("1");
   let split = string_split(s, from);
-  let v = list_join(list, separator);
+  let replaced = list_join(split, to);
+  return replaced;
 }
