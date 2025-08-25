4ad96e9cd6c72d197e1ddb9955d5fd1b6ade81cc
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 22:14:20 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -73,12 +73,9 @@ export function app_replace_rule_set(context) {
     }
     let b = html_button(root, text, lambda3);
     html_disable(b);
-    let to = object_merge(
-      {
-        rule,
-      },
-      b,
-    );
+    let to = object_merge(b, {
+      rule,
+    });
     return to;
   }
   let rules_buttons = list_map(mapped, lambda);
