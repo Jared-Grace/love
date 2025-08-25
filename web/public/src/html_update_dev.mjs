e2e51798d63c81ac3db3de23eb6243791c71e3ec
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:07:47 -0400

    function_auto_path public\src\html_update_dev.mjs

--- web/public/src/html_update_dev.mjs
+++ web/public/src/html_update_dev.mjs
@@ -1,4 +1,4 @@
-import { function_parse_declaration_js_unparse } from "./function_parse_declaration_js_unparse.mjs";
+import { folder_previous } from "./folder_previous.mjs";
 import { folder_src } from "./folder_src.mjs";
 import { html_name_to_path_dev } from "./html_name_to_path_dev.mjs";
 import { html_update_generic } from "./html_update_generic.mjs";
@@ -6,9 +6,9 @@ import { marker } from "./marker.mjs";
 export async function html_update_dev(name) {
   marker("1");
   let file_path = html_name_to_path_dev(name);
-  async function paths_get(f_name_ext) {
+  function paths_get(f_name_ext) {
     let src = folder_src();
-    let output = await function_parse_declaration_js_unparse(f_name);
+    let v = folder_previous();
     const from_paths = [src, f_name_ext];
     return from_paths;
   }
