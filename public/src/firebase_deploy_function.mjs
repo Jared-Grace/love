bc95ebe93e9457b13ab8330d8466f1ad0a37e245
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:42:23 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- public/src/firebase_deploy_function.mjs
+++ public/src/firebase_deploy_function.mjs
@@ -1,5 +1,5 @@
+import { list_find } from "./list_find.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { marker } from "./marker.mjs";
@@ -12,6 +12,6 @@ export function firebase_deploy_function() {
     let sw = string_starts_with(item, prefix);
     return sw;
   }
-  let filtered = list_filter(files, lambda);
-  return files;
+  let filtered = list_find(files, lambda);
+  return filtered;
 }
