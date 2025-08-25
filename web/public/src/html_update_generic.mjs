aa72bb34c9ed2541ba26405844e5ce44c7357b18
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:05:16 -0400

    function_auto_path public\src\html_update_generic.mjs

--- web/public/src/html_update_generic.mjs
+++ web/public/src/html_update_generic.mjs
@@ -12,6 +12,7 @@ export async function html_update_generic(name, file_path) {
   let call = js_code_call_statement(name_prefixed);
   let f_name_ext = function_name_to_base(name_prefixed);
   let src = folder_src();
+  const from_paths = [src, f_name_ext];
   let f_path = path_join(from_paths);
   const from = folder_current_join_code(f_path);
   let code = js_code_import_single(name_prefixed, from);
