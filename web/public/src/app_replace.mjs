fa76fa13913804d77524f5b32f9357514b86a9b1
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:45:32 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -1,6 +1,8 @@
+import { http_json } from "./http_json.mjs";
 import { app_replace_main } from "./app_replace_main.mjs";
 import { marker } from "./marker.mjs";
-export function app_replace() {
+export async function app_replace() {
   marker("1");
   app_replace_main();
+  let json = await http_json(url);
 }
