0c460d4e9167d1be76571b55e94c0c6f97d280a3
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:26:23 -0400

    function_auto_path public\src\object_property_lambda_async.mjs

--- web/public/src/object_property_lambda_async.mjs
+++ web/public/src/object_property_lambda_async.mjs
@@ -3,11 +3,15 @@ import { not } from "./not.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-export function object_property_lambda_async(object, property_name, lambda) {
+export async function object_property_lambda_async(
+  object,
+  property_name,
+  lambda,
+) {
   marker("1");
   const exists = object_property_exists(object, property_name);
   if (not(exists)) {
-    let value_set = lambda();
+    let value_set = await lambda();
     object_property_set(object, property_name, value_set);
   }
   let value = object_property_get(object, property_name);
