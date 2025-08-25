95f2671171db584218333f59122363eeb49ee16a
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:02:29 -0400

    cs html_name_to_path dev

--- web/public/src/html_name_to_path_dev.mjs
+++ web/public/src/html_name_to_path_dev.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { folder_public_combine } from "./folder_public_combine.mjs";
 import { file_name_html } from "./file_name_html.mjs";
 export function html_name_to_path_dev(name) {
+  marker("1");
   let file_name = file_name_html(name);
   let file_path = folder_public_combine(file_name);
   return file_path;
