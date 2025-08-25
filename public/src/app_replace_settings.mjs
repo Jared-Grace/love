a23c3b4af415704e49883162198c4c569de08007
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:17:42 -0400

    function_auto_path public\src\app_replace_settings.mjs

--- public/src/app_replace_settings.mjs
+++ public/src/app_replace_settings.mjs
@@ -1,6 +1,7 @@
+import { emoji_font_larger } from "./emoji_font_larger.mjs";
 import { html_button } from "./html_button.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_settings(context) {
   marker("1");
-  html_button(root, "🗚" + " Font size larger", lambda4);
+  html_button(root, emoji_font_larger() + " Font size larger", lambda4);
 }
