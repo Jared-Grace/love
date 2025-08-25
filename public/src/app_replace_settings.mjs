3cc2e5f30bad939ae31f18682eec38e112dbcef3
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:20:08 -0400

    function_auto_path public\src\app_replace_settings.mjs

--- public/src/app_replace_settings.mjs
+++ public/src/app_replace_settings.mjs
@@ -1,10 +1,13 @@
+import { app_replace_font_size_refresh } from "./app_replace_font_size_refresh.mjs";
 import { emoji_font_smaller } from "./emoji_font_smaller.mjs";
 import { emoji_font_larger } from "./emoji_font_larger.mjs";
 import { html_button } from "./html_button.mjs";
 import { marker } from "./marker.mjs";
 export function app_replace_settings(context) {
   marker("1");
-  function lambda2() {}
+  function lambda2() {
+    app_replace_font_size_refresh();
+  }
   html_button(root, emoji_font_larger() + " Font size larger", lambda2);
   function lambda3() {}
   let component = html_button(
