87e81106fe381131789e9b646364b92849d15c94
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:13:56 -0400

    function_auto_path public\src\firebase_storage_download.mjs

--- web/public/src/firebase_storage_download.mjs
+++ web/public/src/firebase_storage_download.mjs
@@ -1,7 +1,7 @@
-import {http_json} from "./http_json.mjs";
-import {firebase_storage_url} from "./firebase_storage_url.mjs";
+import { http_json } from "./http_json.mjs";
+import { firebase_storage_url } from "./firebase_storage_url.mjs";
 export async function firebase_storage_download(destination_version) {
   let url = firebase_storage_url(destination_version);
-  let json = await http_json(url);
-  return json;
+  let parsed = await http_json(url);
+  return parsed;
 }
