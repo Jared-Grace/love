6fb703fba20bab6e4e7ea336a5f54117841c93e7
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:20:28 -0400

    function_auto_path public\src\firebase_storage_download_property.mjs

--- public/src/firebase_storage_download_property.mjs
+++ public/src/firebase_storage_download_property.mjs
@@ -1,7 +1,10 @@
-import {object_property_get} from "./object_property_get.mjs";
-import {firebase_storage_download} from "./firebase_storage_download.mjs";
-export async function firebase_storage_download_property(destination_version, property_name) {
-  let parsed = await firebase_storage_download(destination_version);
-  let destination = object_property_get(parsed, property_name);
-  return destination;
+import { object_property_get } from "./object_property_get.mjs";
+import { firebase_storage_download } from "./firebase_storage_download.mjs";
+export async function firebase_storage_download_property(
+  storage_path,
+  property_name,
+) {
+  let parsed = await firebase_storage_download(storage_path);
+  let value = object_property_get(parsed, property_name);
+  return value;
 }
