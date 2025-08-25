6225f4c8178da60ff859399002dd03e3739189bc
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:27:21 -0400

    function_auto_path public\src\global_function_async.mjs

--- web/public/src/global_function_async.mjs
+++ web/public/src/global_function_async.mjs
@@ -2,13 +2,14 @@ import { object_property_lambda_async } from "./object_property_lambda_async.mjs
 import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_get } from "./global_get.mjs";
 import { marker } from "./marker.mjs";
-export async function global_function_async(fn, property_name) {
+export async function global_function_async(fn, property_name, lambda) {
   let global = global_get();
   marker("1");
   let fn_object = object_property_initialize(global, fn.name, {});
-  let value2 = await object_property_lambda_async(
+  let value = await object_property_lambda_async(
     fn_object,
     property_name,
-    null,
+    lambda,
   );
+  return value;
 }
