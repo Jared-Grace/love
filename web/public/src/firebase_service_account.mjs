01db8226e938a12d2286d78d5d853511b6da63b9
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:54:31 -0400

    function_auto_path public\src\firebase_service_account.mjs

--- web/public/src/firebase_service_account.mjs
+++ web/public/src/firebase_service_account.mjs
@@ -8,6 +8,6 @@ export async function firebase_service_account() {
   let path_folder = path_join([v, "secret"]);
   let only = folder_find_starts_with(path_folder, prefix);
   let file_path = path_join([path_folder, only]);
-  let data = await file_read_json(file_path);
-  return data;
+  let service_account = await file_read_json(file_path);
+  return service_account;
 }
