d83a010440675cfdc6f14a9c124a8ca1c8557a57
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:22:43 -0400

    function_auto_path public\src\global_function.mjs

--- public/src/global_function.mjs
+++ public/src/global_function.mjs
@@ -1,6 +1,8 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_get } from "./global_get.mjs";
 import { marker } from "./marker.mjs";
 export function global_function(fn, property_name) {
   let global = global_get();
   marker("1");
+  let value2 = object_property_initialize(object, property_name2, value);
 }
