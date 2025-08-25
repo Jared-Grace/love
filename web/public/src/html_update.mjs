034dafdb97f18724ffddac8e8b3a07cdbf84b5a8
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:03:49 -0400

    function_auto_path public\src\html_update.mjs

--- web/public/src/html_update.mjs
+++ web/public/src/html_update.mjs
@@ -1,28 +1,8 @@
+import { html_update_generic } from "./html_update_generic.mjs";
 import { marker } from "./marker.mjs";
-import { app_name_prefixed } from "./app_name_prefixed.mjs";
-import { folder_src } from "./folder_src.mjs";
-import { folder_current_join_code } from "./folder_current_join_code.mjs";
-import { function_name_to_base } from "./function_name_to_base.mjs";
-import { js_code_import_single } from "./js_code_import_single.mjs";
-import { js_code_call_statement } from "./js_code_call_statement.mjs";
-import { file_open } from "./file_open.mjs";
-import { html_overwrite } from "./html_overwrite.mjs";
 import { html_name_to_path } from "./html_name_to_path.mjs";
-import { path_join } from "./path_join.mjs";
 export async function html_update(name) {
   marker("1");
   let file_path = html_name_to_path(name);
-  const name_prefixed = app_name_prefixed(name);
-  let call = js_code_call_statement(name_prefixed);
-  let f_name_ext = function_name_to_base(name_prefixed);
-  let src = folder_src();
-  let f_path = path_join([src, f_name_ext]);
-  const from = folder_current_join_code(f_path);
-  let code = js_code_import_single(name_prefixed, from);
-  let body = `<script type="module"> 
-    ${code}
-    ${call}
-  </script>`;
-  await html_overwrite(name, body);
-  await file_open(file_path);
+  await html_update_generic(name, file_path);
 }
---
+++ web/public/src/html_update_generic.mjs
@@ -0,0 +1,24 @@
+import {file_open} from "./file_open.mjs";
+import {html_overwrite} from "./html_overwrite.mjs";
+import {js_code_import_single} from "./js_code_import_single.mjs";
+import {folder_current_join_code} from "./folder_current_join_code.mjs";
+import {path_join} from "./path_join.mjs";
+import {folder_src} from "./folder_src.mjs";
+import {function_name_to_base} from "./function_name_to_base.mjs";
+import {js_code_call_statement} from "./js_code_call_statement.mjs";
+import {app_name_prefixed} from "./app_name_prefixed.mjs";
+export async function html_update_generic(name, file_path) {
+  const name_prefixed = app_name_prefixed(name);
+  let call = js_code_call_statement(name_prefixed);
+  let f_name_ext = function_name_to_base(name_prefixed);
+  let src = folder_src();
+  let f_path = path_join([src, f_name_ext]);
+  const from = folder_current_join_code(f_path);
+  let code = js_code_import_single(name_prefixed, from);
+  let body = `<script type="module"> 
+    ${code}
+    ${call}
+  </script>`;
+  await html_overwrite(name, body);
+  await file_open(file_path);
+}
