1b843bc5be320462b2fc67515a3a954fb3d11897
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 22:19:07 -0400

    function_auto_path public\src\list_concat_multiple.mjs

--- web/public/src/list_concat_multiple.mjs
+++ web/public/src/list_concat_multiple.mjs
@@ -1,8 +1,12 @@
+import { list_concat } from "./list_concat.mjs";
 import { each } from "./each.mjs";
 import { marker } from "./marker.mjs";
 export function list_concat_multiple(lists) {
   marker("1");
   let result = [];
-  function lambda(item) {}
+  function lambda(item) {
+    result = list_concat(result, item);
+  }
   each(lists, lambda);
+  return result;
 }
