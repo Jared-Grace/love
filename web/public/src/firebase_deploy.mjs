a4a705567b294f5ba076a94fbfb44d1ab11b297f
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 12:48:31 -0400

    function_auto_path public\src\firebase_deploy.mjs

--- web/public/src/firebase_deploy.mjs
+++ web/public/src/firebase_deploy.mjs
@@ -2,5 +2,5 @@ import { command_line } from "./command_line.mjs";
 import { marker } from "./marker.mjs";
 export async function firebase_deploy() {
   marker("1");
-  let stdout = await command_line(command);
+  let stdout = await command_line("firebase deploy");
 }
