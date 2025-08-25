3d1b93a017925a20ac7128a42023f5d440dcaedb
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 20:17:11 -0400

    function_auto_path public\src\html_p_text.mjs

--- web/public/src/html_p_text.mjs
+++ web/public/src/html_p_text.mjs
@@ -1,6 +1,7 @@
-import {html_text_set} from "./html_text_set.mjs";
-import {html_p} from "./html_p.mjs";
+import { html_text_set } from "./html_text_set.mjs";
+import { html_p } from "./html_p.mjs";
 export function html_p_text(root, text) {
   let p = html_p(root);
   html_text_set(p, text);
+  return p;
 }
