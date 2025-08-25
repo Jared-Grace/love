5da4553455774abfc890b0d053b493db8db37a36
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:41:51 -0400

    function_auto_path public\src\http.mjs

--- public/src/http.mjs
+++ public/src/http.mjs
@@ -1,4 +1,11 @@
-import { marker } from "./marker.mjs";
-export function http() {
-  marker("1");
+import { error } from "./error.mjs";
+import { log } from "./log.mjs";
+import { not } from "./not.mjs";
+export async function http(url) {
+  const response = await fetch(url);
+  if (not(response.ok)) {
+    error("Failed to fetch file");
+  }
+  const text = await response.text();
+  console.log(text);
 }
