3498441103a99f6c517c64fec16288d020f1a3ad
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:28:33 -0400

    data_file_update public\src\firebase_admin.mjs

--- public/src/firebase_admin.mjs
+++ public/src/firebase_admin.mjs
@@ -2,10 +2,7 @@ import { global_function_async } from "./global_function_async.mjs";
 import { firebase_admin_get } from "./firebase_admin_get.mjs";
 export async function firebase_admin() {
   const admin = await firebase_admin_get();
-  let value = await global_function_async(
-    fn,
-    property_name,
-    async function lambda2() {},
-  );
+  async function lambda2() {}
+  let value = await global_function_async(fn, property_name, lambda2);
   return admin;
 }
