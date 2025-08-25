db4aedc95addfccdf6bc46099eba2560e7218bfe
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 17:12:07 -0400

    function_auto_path public\src\html_document_head.mjs

--- web/public/src/html_document_head.mjs
+++ web/public/src/html_document_head.mjs
@@ -2,7 +2,7 @@ import { html_component_wrap } from "./html_component_wrap.mjs";
 import { marker } from "./marker.mjs";
 export function html_document_head() {
   marker("1");
-  let body = document.head;
-  let component = html_component_wrap(body);
+  let head = document.head;
+  let component = html_component_wrap(head);
   return component;
 }
