2f9addfe42e536b6a070199cd157ed84c008b6c2
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:01:00 -0400

    o fd

--- web/public/src/function_dependencies_code_copy.mjs
+++ web/public/src/function_dependencies_code_copy.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { clipboard_copy } from "./clipboard_copy.mjs";
 import { function_dependencies_code } from "./function_dependencies_code.mjs";
 export async function function_dependencies_code_copy(f_name) {
+  marker("1");
   let code = await function_dependencies_code(f_name);
   await clipboard_copy(code);
 }
