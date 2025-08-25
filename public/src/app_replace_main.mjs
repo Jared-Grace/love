bb38ba133efb6cb0493854205ff0c848b1702778
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:30:30 -0400

    function_auto_path public\src\app_replace_main.mjs

--- public/src/app_replace_main.mjs
+++ public/src/app_replace_main.mjs
@@ -1,11 +1,10 @@
-import { html_style_set } from "./html_style_set.mjs";
+import { html_style_font_size } from "./html_style_font_size.mjs";
 import { html_document_root } from "./html_document_root.mjs";
 import { html_meta_viewport } from "./html_meta_viewport.mjs";
 import { app_refresh } from "./app_refresh.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { app_replace_screens } from "./app_replace_screens.mjs";
 import { app_replace } from "./app_replace.mjs";
-const value = "20px";
 export function app_replace_main() {
   let app_fn = app_replace;
   let screens = app_replace_screens();
@@ -17,5 +16,6 @@ export function app_replace_main() {
     root: root,
   });
   let html = html_document_root();
-  html_style_set(html, "font-size", value);
+  const value = "20px";
+  html_style_font_size(html, value);
 }
---
+++ public/src/html_style_font_size.mjs
@@ -0,0 +1,4 @@
+import {html_style_set} from "./html_style_set.mjs";
+export function html_style_font_size(html, value) {
+  html_style_set(html, "font-size", value);
+}
