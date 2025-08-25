9a059a10e899931e7bf0c79cc23771dccc707f3a
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:01:31 -0400

    function_auto_path public\src\date_now_file.mjs

--- web/public/src/date_now_file.mjs
+++ web/public/src/date_now_file.mjs
@@ -2,5 +2,6 @@ import { date_now_iso } from "./date_now_iso.mjs";
 import { marker } from "./marker.mjs";
 export function date_now_file() {
   marker("1");
-  return date_now_iso();
+  let now_iso = date_now_iso();
+  return now_iso;
 }
