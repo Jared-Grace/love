92e3a4219c30ddd389c820c87a0996bd0f18a7a2
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:49:17 -0400

    function_auto_path public\src\firebase_admin_get.mjs

--- web/public/src/firebase_admin_get.mjs
+++ web/public/src/firebase_admin_get.mjs
@@ -1,12 +1,13 @@
-import {firebase_service_account} from "./firebase_service_account.mjs";
-import {marker} from "./marker.mjs";
+import { firebase_storage_url } from "./firebase_storage_url.mjs";
+import { firebase_service_account } from "./firebase_service_account.mjs";
+import { marker } from "./marker.mjs";
 export async function firebase_admin_get() {
   const admin = (await import("firebase-admin")).default;
   marker("1");
   let service_account = await firebase_service_account();
   admin.initializeApp({
     credential: admin.credential.cert(service_account),
-    storageBucket: "jared-grace.firebasestorage.app"
+    storageBucket: firebase_storage_url(),
   });
   return admin;
 }
