871bc87c13f705d41dfcd4311fffeb1f56621f1d
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:47:52 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- public/src/firebase_deploy_function.mjs
+++ public/src/firebase_deploy_function.mjs
@@ -1,17 +1,9 @@
-import { file_read_json } from "./file_read_json.mjs";
-import { folder_find_starts_with } from "./folder_find_starts_with.mjs";
-import { folder_previous } from "./folder_previous.mjs";
+import { firebase_service_account } from "./firebase_service_account.mjs";
 import { marker } from "./marker.mjs";
-import { path_join } from "./path_join.mjs";
 export async function firebase_deploy_function() {
   const admin = await import("firebase-admin");
   marker("1");
-  let prefix = "jared-grace-firebase-adminsdk-";
-  let v = folder_previous();
-  let path_folder = path_join([v, "secret"]);
-  let only = folder_find_starts_with(path_folder, prefix);
-  let file_path = path_join([path_folder, only]);
-  let data = await file_read_json(file_path);
+  let data = await firebase_service_account();
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
