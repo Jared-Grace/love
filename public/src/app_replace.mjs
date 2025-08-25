ab9c4d0f44b766047a48b40b04321235c6be702b
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:10:23 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,5 +1,4 @@
 import { firebase_storage_url } from "./firebase_storage_url.mjs";
-import { string_replace } from "./string_replace.mjs";
 import { log } from "./log.mjs";
 import { firebase_deploy_function_destination_version } from "./firebase_deploy_function_destination_version.mjs";
 import { http_json } from "./http_json.mjs";
@@ -10,7 +9,6 @@ export async function app_replace() {
   let f_name = app_replace_main.name;
   let destination_version =
     firebase_deploy_function_destination_version(f_name);
-  let replaced = string_replace(destination_version, "/", "%2F");
   let url = firebase_storage_url(replaced);
   let json = await http_json(url);
   log(json);
