f39483493bdb59ab20a4e03578c5a26ecc33fd47
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:01:59 -0400

    function_auto_path public\src\date_now_file.mjs

--- web/public/src/date_now_file.mjs
+++ web/public/src/date_now_file.mjs
@@ -3,6 +3,9 @@ import { marker } from "./marker.mjs";
 export function date_now_file() {
   marker("1");
   let now_iso = date_now_iso();
-  let now_file = iso.replace(/:/g, "-").replace(/\./g, "-").replace(/Z$/, "");
+  let now_file = now_iso
+    .replace(/:/g, "-")
+    .replace(/\./g, "-")
+    .replace(/Z$/, "");
   return now_file;
 }
