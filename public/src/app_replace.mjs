539f40cecdc1ef225414b0b04861ae6a0e1f633e
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:11:32 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,7 +1,6 @@
-import { firebase_storage_url } from "./firebase_storage_url.mjs";
+import { firebase_storage_download } from "./firebase_storage_download.mjs";
 import { log } from "./log.mjs";
 import { firebase_deploy_function_destination_version } from "./firebase_deploy_function_destination_version.mjs";
-import { http_json } from "./http_json.mjs";
 import { app_replace_main } from "./app_replace_main.mjs";
 import { marker } from "./marker.mjs";
 export async function app_replace() {
@@ -9,7 +8,6 @@ export async function app_replace() {
   let f_name = app_replace_main.name;
   let destination_version =
     firebase_deploy_function_destination_version(f_name);
-  let url = firebase_storage_url(destination_version);
-  let json = await http_json(url);
+  let json = await firebase_storage_download(destination_version);
   log(json);
 }
