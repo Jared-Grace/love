459ededa753a4fd66bcd5e4d37161ef5e81684ce
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 20:23:27 -0400

    c html_disable html_enable

--- web/public/src/html_enable.mjs
+++ web/public/src/html_enable.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { html_disable_set } from "./html_disable_set.mjs";
 export function html_enable(b) {
+  marker("1");
   const disabled = true;
   html_disable_set(b, disabled);
 }
