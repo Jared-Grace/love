a0dcc1cb53a30ce35490d34f9e24ad329b2d594c
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:33:24 -0400

    function_auto_path public\src\firebase_deploy_function_app_current.mjs

--- web/public/src/firebase_deploy_function_app_current.mjs
+++ web/public/src/firebase_deploy_function_app_current.mjs
@@ -2,5 +2,6 @@ import { firebase_deploy_function } from "./firebase_deploy_function.mjs";
 import { marker } from "./marker.mjs";
 export async function firebase_deploy_function_app_current(f_name) {
   marker("1");
-  return await firebase_deploy_function(f_name);
+  let v = await firebase_deploy_function(f_name);
+  return v;
 }
