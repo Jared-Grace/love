b581c713d8990e70267492e00186d43906f4ff1e
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 20:41:57 -0400

    c html_style_set html_style_remove

--- public/src/html_style_remove.mjs
+++ public/src/html_style_remove.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_style_remove(b, style_key, style_value) {
+  marker("1");
   let b_element = html_component_element_get(b);
   b_element.style[style_key] = style_value;
 }
