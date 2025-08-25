b545dca3a776fb6ea5fd14a77685a4e83bf332e8
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:40:03 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- public/src/firebase_deploy_function.mjs
+++ public/src/firebase_deploy_function.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "./list_first.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { marker } from "./marker.mjs";
@@ -6,5 +7,6 @@ export function firebase_deploy_function() {
   let prefix = "jared-grace-firebase-adminsdk-";
   let path_folder = folder_previous();
   let files = folder_read(path_folder);
+  let first = list_first(list);
   return files;
 }
