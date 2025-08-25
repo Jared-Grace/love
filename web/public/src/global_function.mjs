4fb9f413cf956a2444066a3e9831ec31a6721e92
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:22:35 -0400

    function_auto_path public\src\global_function.mjs

--- web/public/src/global_function.mjs
+++ web/public/src/global_function.mjs
@@ -1,4 +1,6 @@
+import { global_get } from "./global_get.mjs";
 import { marker } from "./marker.mjs";
 export function global_function(fn, property_name) {
+  let global = global_get();
   marker("1");
 }
