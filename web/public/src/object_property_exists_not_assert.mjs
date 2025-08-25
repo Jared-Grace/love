cd79293609948e2d1c9f29754ab93e0416ecdc9d
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:54:56 -0400

    c object_property_exists_assert object_property_exists_not_assert

--- web/public/src/object_property_exists_not_assert.mjs
+++ web/public/src/object_property_exists_not_assert.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { assert } from "./assert.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 export function object_property_exists_not_assert(object, property_name) {
+  marker("1");
   let result = object_property_exists(object, property_name);
   assert(result);
 }
