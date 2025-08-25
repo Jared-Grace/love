8fca954f1bb564e514508f55e41679a9a8e36f16
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:54:26 -0400

    function_auto_path public\src\firebase_service_account.mjs

--- public/src/firebase_service_account.mjs
+++ public/src/firebase_service_account.mjs
@@ -1,7 +1,7 @@
-import {file_read_json} from "./file_read_json.mjs";
-import {folder_find_starts_with} from "./folder_find_starts_with.mjs";
-import {path_join} from "./path_join.mjs";
-import {folder_previous} from "./folder_previous.mjs";
+import { file_read_json } from "./file_read_json.mjs";
+import { folder_find_starts_with } from "./folder_find_starts_with.mjs";
+import { path_join } from "./path_join.mjs";
+import { folder_previous } from "./folder_previous.mjs";
 export async function firebase_service_account() {
   let prefix = "jared-grace-firebase-adminsdk-";
   let v = folder_previous();
