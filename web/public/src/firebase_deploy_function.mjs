faec3702bc3d81aa94d250fe21ca2e7bdc14f56f
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:44:59 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -2,10 +2,12 @@ import { file_read_json } from "./file_read_json.mjs";
 import { folder_find_starts_with } from "./folder_find_starts_with.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 import { marker } from "./marker.mjs";
+import { path_join } from "./path_join.mjs";
 export async function firebase_deploy_function() {
   marker("1");
   let prefix = "jared-grace-firebase-adminsdk-";
-  let path_folder = folder_previous();
+  let v = folder_previous();
+  let path_folder = path_join([v, "secret"]);
   let only = folder_find_starts_with(path_folder, prefix);
   let data = await file_read_json(only);
   return only;
