21794ecf8087df7763359b071d54d5f79c3d42b3
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:30:08 -0400

    function_auto_path public\src\firebase_admin.mjs

--- public/src/firebase_admin.mjs
+++ public/src/firebase_admin.mjs
@@ -1,5 +1,7 @@
+import { global_function_async } from "./global_function_async.mjs";
 import { firebase_admin_get } from "./firebase_admin_get.mjs";
 export async function firebase_admin() {
+  let value = await global_function_async(fn, async function lambda2() {});
   const admin = await firebase_admin_get();
   return admin;
 }
