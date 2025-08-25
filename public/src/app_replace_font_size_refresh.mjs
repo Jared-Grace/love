7bb219f807cb9651de31260f78e2360f10a9ecf4
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:20:43 -0400

    function_auto_path public\src\app_replace_font_size_refresh.mjs

--- public/src/app_replace_font_size_refresh.mjs
+++ public/src/app_replace_font_size_refresh.mjs
@@ -1,7 +1,7 @@
 import { storage_local_initialize_context } from "./storage_local_initialize_context.mjs";
 import { html_style_font_size } from "./html_style_font_size.mjs";
 import { html_document_root } from "./html_document_root.mjs";
-export function app_replace_font_size_refresh() {
+export function app_replace_font_size_refresh(context) {
   let html = html_document_root();
   let value = storage_local_initialize_context(context, key, value_initial);
   html_style_font_size(html, "20px");
