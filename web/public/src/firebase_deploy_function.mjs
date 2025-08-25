b4dca5e260217a7113edc102e518d840b62f31a1
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:45:41 -0400

    o ff

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -1,9 +1,11 @@
+import { marker } from "./marker.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { firebase_deploy_function_destination } from "./firebase_deploy_function_destination.mjs";
 import { date_now_file } from "./date_now_file.mjs";
 import { function_dependencies_code } from "./function_dependencies_code.mjs";
 import { firebase_upload_string } from "./firebase_upload_string.mjs";
 export async function firebase_deploy_function(f_name) {
+  marker("1");
   let code = await function_dependencies_code(f_name);
   let now_file = date_now_file();
   let destination = firebase_deploy_function_destination(now_file, f_name);
