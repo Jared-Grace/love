d991c4729076353d8cf5994ab8f353830f7bb511
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:10:42 -0400

    function_auto_path public\src\firebase_storage_url.mjs

--- web/public/src/firebase_storage_url.mjs
+++ web/public/src/firebase_storage_url.mjs
@@ -1,5 +1,12 @@
-import {firebase_storage_url_project} from "./firebase_storage_url_project.mjs";
-export function firebase_storage_url(replaced) {
-  let v = "https://firebasestorage.googleapis.com/v0/b/" + firebase_storage_url_project() + "/o/" + replaced + "?alt=media";
+import { string_replace } from "./string_replace.mjs";
+import { firebase_storage_url_project } from "./firebase_storage_url_project.mjs";
+export function firebase_storage_url(destination_version) {
+  let replaced = string_replace(destination_version, "/", "%2F");
+  let v =
+    "https://firebasestorage.googleapis.com/v0/b/" +
+    firebase_storage_url_project() +
+    "/o/" +
+    replaced +
+    "?alt=media";
   return v;
 }
