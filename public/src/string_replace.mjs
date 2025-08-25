c0ca5e846e0337bfff1d50dc389ff85f562fddfa
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:56:03 -0400

    function_auto_path public\src\string_replace.mjs

--- public/src/string_replace.mjs
+++ public/src/string_replace.mjs
@@ -1,4 +1,6 @@
+import { string_split } from "./string_split.mjs";
 import { marker } from "./marker.mjs";
-export function string_replace() {
+export function string_replace(s, from, to) {
   marker("1");
+  let split = string_split(s2, separator);
 }
