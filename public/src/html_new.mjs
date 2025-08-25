2963d7c95d61d5fdfbe366bdd56f0fdd46ea9d4d
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:18:09 -0400

    rs html_update public

--- public/src/html_new.mjs
+++ public/src/html_new.mjs
@@ -1,10 +1,10 @@
 import { html_update_dev } from "./html_update_dev.mjs";
-import { html_update } from "./html_update.mjs";
+import { html_update_public } from "./html_update_public.mjs";
 import { html_name_to_path } from "./html_name_to_path.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 export async function html_new(name) {
   let file_path = html_name_to_path(name);
   await assert_file_exists_not(file_path);
-  await html_update(name);
+  await html_update_public(name);
   await html_update_dev(name);
 }
