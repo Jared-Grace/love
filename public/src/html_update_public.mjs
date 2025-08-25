7de98a1c87471aa210aea9d0aab2a61527386040
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:18:08 -0400

    function_auto_path public\src\html_update_public.mjs

---
+++ public/src/html_update_public.mjs
@@ -0,0 +1,15 @@
+import { app_name_prefixed } from "./app_name_prefixed.mjs";
+import { folder_src } from "./folder_src.mjs";
+import { html_update_generic } from "./html_update_generic.mjs";
+import { marker } from "./marker.mjs";
+import { html_name_to_path } from "./html_name_to_path.mjs";
+export async function html_update_public(name) {
+  marker("1");
+  let file_path = html_name_to_path(name);
+  function paths_get(f_name_ext) {
+    let src = folder_src();
+    const from_paths = [src, f_name_ext];
+    return from_paths;
+  }
+  await html_update_generic(name, file_path, paths_get, app_name_prefixed);
+}
