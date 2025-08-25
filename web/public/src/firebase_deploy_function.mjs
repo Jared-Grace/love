3722b0f44a0cfcda9118bfca7512051f8cc32e96
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:38:16 -0400

    u for

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -1,7 +1,5 @@
-import { function_read } from "./function_read.mjs";
 import { marker } from "./marker.mjs";
-export async function firebase_deploy_function() {
+export function firebase_deploy_function() {
   marker("1");
-  let prefix = "jared-grace-firebase-adminsdk-";
-  let v = await function_read(f_name);
+  let prefix = "jared-grace-firebase-adminsdk-";for
 }
