5fac0b8caad9e3b807401c596bc49a03cb9b1d9a
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:25:48 -0400

    function_auto_path public\src\app_replace_font_size_adjust.mjs

--- web/public/src/app_replace_font_size_adjust.mjs
+++ web/public/src/app_replace_font_size_adjust.mjs
@@ -1,9 +1,9 @@
-import {app_replace_font_size_refresh} from "./app_replace_font_size_refresh.mjs";
-import {storage_local_set_context} from "./storage_local_set_context.mjs";
-import {app_replace_font_size} from "./app_replace_font_size.mjs";
-export function app_replace_font_size_adjust(context, factor) {
+import { app_replace_font_size_refresh } from "./app_replace_font_size_refresh.mjs";
+import { storage_local_set_context } from "./storage_local_set_context.mjs";
+import { app_replace_font_size } from "./app_replace_font_size.mjs";
+export function app_replace_font_size_adjust(context, value_get) {
   let value = app_replace_font_size(context);
-  value *= factor;
+  value = value_get(value);
   storage_local_set_context(context, "font_size", value);
   app_replace_font_size_refresh(context);
 }
