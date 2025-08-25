e5b893eec77ddb61daecb8aa5b51078be2b86ddf
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:15:02 -0400

    function_auto_path public\src\html_update_dev.mjs

--- web/public/src/html_update_dev.mjs
+++ web/public/src/html_update_dev.mjs
@@ -1,3 +1,4 @@
+import { app_name_main } from "./app_name_main.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 import { folder_src } from "./folder_src.mjs";
 import { html_name_to_path_dev } from "./html_name_to_path_dev.mjs";
@@ -12,5 +13,5 @@ export async function html_update_dev(name) {
     const from_paths = [previous, src, f_name_ext];
     return from_paths;
   }
-  await html_update_generic(name, file_path, paths_get);
+  await html_update_generic(name, file_path, paths_get, app_name_main);
 }
