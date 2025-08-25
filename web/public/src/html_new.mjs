ba7ea51eae05763ccc27c13df16dbc854a0e6d0b
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:16:29 -0400

    function_auto_path public\src\html_new.mjs

--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -1,3 +1,4 @@
+import { html_update_dev } from "./html_update_dev.mjs";
 import { html_update } from "./html_update.mjs";
 import { html_name_to_path } from "./html_name_to_path.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
@@ -5,4 +6,5 @@ export async function html_new(name) {
   let file_path = html_name_to_path(name);
   await assert_file_exists_not(file_path);
   await html_update(name);
+  await html_update_dev(name);
 }
