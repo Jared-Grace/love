6cadd3e34158cf190d0597769b077d8fe198ad0d
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:07:17 -0400

    function_auto_path public\src\html_update_dev.mjs

--- public/src/html_update_dev.mjs
+++ public/src/html_update_dev.mjs
@@ -1,8 +1,14 @@
+import { folder_src } from "./folder_src.mjs";
 import { html_name_to_path_dev } from "./html_name_to_path_dev.mjs";
 import { html_update_generic } from "./html_update_generic.mjs";
 import { marker } from "./marker.mjs";
 export async function html_update_dev(name) {
   marker("1");
   let file_path = html_name_to_path_dev(name);
-  await html_update_generic(name, file_path);
+  function paths_get(f_name_ext) {
+    let src = folder_src();
+    const from_paths = [src, f_name_ext];
+    return from_paths;
+  }
+  await html_update_generic(name, file_path, paths_get);
 }
