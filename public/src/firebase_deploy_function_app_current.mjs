871f899335664f3da25564c7579af703dd136150
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:36:52 -0400

    function_auto_path public\src\firebase_deploy_function_app_current.mjs

--- public/src/firebase_deploy_function_app_current.mjs
+++ public/src/firebase_deploy_function_app_current.mjs
@@ -1,3 +1,4 @@
+import { app_name_prefixed } from "./app_name_prefixed.mjs";
 import { functions_combine_name } from "./functions_combine_name.mjs";
 import { data_app_current_get } from "./data_app_current_get.mjs";
 import { firebase_deploy_function } from "./firebase_deploy_function.mjs";
@@ -5,7 +6,8 @@ import { marker } from "./marker.mjs";
 export async function firebase_deploy_function_app_current(f_name) {
   marker("1");
   let a = await data_app_current_get();
-  let v2 = await functions_combine_name(f_names_comma);
+  let a_name = app_name_prefixed();
+  let v2 = await functions_combine_name([a_name]);
   let v = await firebase_deploy_function(f_name);
   return v;
 }
