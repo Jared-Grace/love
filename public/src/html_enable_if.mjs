2decae8645a7b964e4793a5d5557fff43b42e310
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 20:57:09 -0400

    function_auto_path public\src\html_enable_if.mjs

--- public/src/html_enable_if.mjs
+++ public/src/html_enable_if.mjs
@@ -1,8 +1,8 @@
 import { html_disable } from "./html_disable.mjs";
 import { html_enable } from "./html_enable.mjs";
-export function html_enable_if(component) {
+export function html_enable_if(component, condition) {
   let fn = null;
-  if (nn) {
+  if (condition) {
     fn = html_enable;
   } else {
     fn = html_disable;
