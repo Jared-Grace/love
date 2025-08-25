b99599f7d525c30290766afffd5d62a03e9a0db0
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 17:51:43 -0400

    function_auto_path public\src\app_replace_main.mjs

--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -1,6 +1,4 @@
-import { html_attribute_set } from "./html_attribute_set.mjs";
-import { html_element } from "./html_element.mjs";
-import { html_document_head } from "./html_document_head.mjs";
+import { html_meta_viewport } from "./html_meta_viewport.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { app_refresh } from "./app_refresh.mjs";
 import { html_document_body } from "./html_document_body.mjs";
@@ -9,14 +7,7 @@ import { app_replace } from "./app_replace.mjs";
 export function app_replace_main() {
   let app_fn = app_replace;
   let screens = app_replace_screens();
-  const head = html_document_head();
-  let component = html_element(head, "meta");
-  html_attribute_set(component, "name", "viewport");
-  html_attribute_set(
-    component,
-    "content",
-    "width=device-width, initial-scale=1.0",
-  );
+  html_meta_viewport();
   const root = html_document_body();
   app_refresh({
     app_fn,
