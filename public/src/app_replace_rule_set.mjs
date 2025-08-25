0d1b0b810870bdc184bd700666b65b0537fd9b95
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:49:40 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -120,7 +120,8 @@ export function app_replace_rule_set(context) {
       );
     }
     each_index(symbols_buttons, lambda4);
-    each(list, function lambda8(item2) {});
+    function lambda8(item2) {}
+    each(list, lambda8);
   }
   refresh();
   marker("1");
