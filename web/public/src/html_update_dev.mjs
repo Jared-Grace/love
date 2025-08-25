fa213f5bcb6325215e6adf09ad51da2ba22ff226
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:03:56 -0400

    function_auto_path public\src\html_update_dev.mjs

---
+++ web/public/src/html_update_dev.mjs
@@ -0,0 +1,8 @@
+import { html_update_generic } from "./html_update_generic.mjs";
+import { marker } from "./marker.mjs";
+import { html_name_to_path } from "./html_name_to_path.mjs";
+export async function html_update_dev(name) {
+  marker("1");
+  let file_path = html_name_to_path(name);
+  await html_update_generic(name, file_path);
+}
