00e5157a99e91231f1208285f1d2319d0c76ddb4
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 20:47:21 -0400

    function_auto_path public\src\list_map_index.mjs

--- public/src/list_map_index.mjs
+++ public/src/list_map_index.mjs
@@ -2,8 +2,9 @@ import { list_map } from "./list_map.mjs";
 export function list_map_index(list, lambda$item$index) {
   let index = 0;
   function lambda(item) {
-    lambda$item$index(item, index);
+    let value = lambda$item$index(item, index);
     index++;
+    return value;
   }
   let mapped = list_map(list, lambda);
   return mapped;
