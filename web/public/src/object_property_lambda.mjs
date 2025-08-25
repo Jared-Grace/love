9db27ddad6194b087e458ae48bfa6d44636e1ab3
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:26:02 -0400

    function_auto_path public\src\object_property_lambda.mjs

--- web/public/src/object_property_lambda.mjs
+++ web/public/src/object_property_lambda.mjs
@@ -6,11 +6,10 @@ import { object_property_exists } from "./object_property_exists.mjs";
 export function object_property_lambda(object, property_name, lambda) {
   marker("1");
   const exists = object_property_exists(object, property_name);
-  let value = null;
   if (not(exists)) {
-    value = lambda();
-    object_property_set(object, property_name, value2);
+    let value_set = lambda();
+    object_property_set(object, property_name, value_set);
   }
-  value = object_property_get(object, property_name);
+  let value = object_property_get(object, property_name);
   return value;
 }
