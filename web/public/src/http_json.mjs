a49fc2e22186fbafc0f99834a44a0d9fbcd735d1
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:44:25 -0400

    function_auto_path public\src\http_json.mjs

--- web/public/src/http_json.mjs
+++ web/public/src/http_json.mjs
@@ -1,9 +1,8 @@
 import { http } from "./http.mjs";
-import { json_to } from "./json_to.mjs";
 import { marker } from "./marker.mjs";
 export async function http_json(url) {
   marker("1");
   let text = await http(url);
-  let v = json_to(object);
+  jf;
   return text;
 }
