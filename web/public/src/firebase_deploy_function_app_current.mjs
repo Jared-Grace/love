17fbde1255f788f68d67ac151e11fcd43882da9b
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:14:20 -0400

    function_auto_path public\src\firebase_deploy_function_app_current.mjs

--- web/public/src/firebase_deploy_function_app_current.mjs
+++ web/public/src/firebase_deploy_function_app_current.mjs
@@ -1,12 +1,10 @@
-import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
-import { app_name_prefixed } from "./app_name_prefixed.mjs";
+import { app_name_main } from "./app_name_main.mjs";
 import { data_app_current_get } from "./data_app_current_get.mjs";
 import { firebase_deploy_function } from "./firebase_deploy_function.mjs";
 import { marker } from "./marker.mjs";
 export async function firebase_deploy_function_app_current() {
   marker("1");
   let a = await data_app_current_get();
-  let a_name = app_name_prefixed(a);
-  let combined = function_name_combine_multiple([a_name, "main"]);
+  let combined = app_name_main(a);
   await firebase_deploy_function(combined);
 }
