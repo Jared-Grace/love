d66cf5b7087ab1f4d12154566cefe94eccada125
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:56:56 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -1,10 +1,10 @@
 import { firebase_admin } from "./firebase_admin.mjs";
 import { log } from "./log.mjs";
 export async function firebase_deploy_function() {
-  async function uploadString(content, destinationPath) {
+  async function uploadString(content, destination) {
     const admin = await firebase_admin();
     const bucket = admin.storage().bucket();
-    const file = bucket.file(destinationPath);
+    const file = bucket.file(destination);
     let v = Buffer.from(content);
     await file.save(v, {
       contentType: "text/plain",
@@ -13,7 +13,7 @@ export async function firebase_deploy_function() {
         cacheControl: "no-cache",
       },
     });
-    console.log(`Uploaded string to ${destinationPath}`);
+    console.log(`Uploaded string to ${destination}`);
     const url = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
     console.log("Accessible at:", url);
   }
