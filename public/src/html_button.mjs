0b283a42a49d4d72d607c212ae5d49d339945323
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 18:30:53 -0400

    function_auto_path public\src\html_button.mjs

--- public/src/html_button.mjs
+++ public/src/html_button.mjs
@@ -1,3 +1,4 @@
+import { html_style_font_size } from "./html_style_font_size.mjs";
 import { html_on_click } from "./html_on_click.mjs";
 import { html_text_set } from "./html_text_set.mjs";
 import { html_button_element } from "./html_button_element.mjs";
@@ -5,5 +6,6 @@ export function html_button(parent, text, lambda) {
   let component = html_button_element(parent);
   html_text_set(component, text);
   html_on_click(component, lambda);
+  html_style_font_size(html, "inherit");
   return component;
 }
