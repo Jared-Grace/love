270ef8072130f8dbc5aaae2c5bfb1b71baa33b2a
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:06:02 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -1,14 +1,12 @@
-import { file_name_json } from "./file_name_json.mjs";
+import { firebase_deploy_function_destination } from "./firebase_deploy_function_destination.mjs";
 import { date_now_file } from "./date_now_file.mjs";
 import { function_dependencies_code } from "./function_dependencies_code.mjs";
 import { firebase_upload_string } from "./firebase_upload_string.mjs";
-import { list_join } from "./list_join.mjs";
 import { json_to } from "./json_to.mjs";
 export async function firebase_deploy_function(f_name) {
   let code = await function_dependencies_code(f_name);
   let now_file = date_now_file();
-  let file_name = file_name_json(now_file);
-  let destination = list_join(["function", f_name, file_name], "/");
+  let destination = firebase_deploy_function_destination(now_file, f_name);
   await firebase_upload_string(code, destination);
   let version = {
     destination,
