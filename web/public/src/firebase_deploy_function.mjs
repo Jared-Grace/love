0b79929cdcd8a71c8cac4e4e9bde98fc4d08c765
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:59:41 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -3,5 +3,5 @@ import { firebase_upload_string } from "./firebase_upload_string.mjs";
 export async function firebase_deploy_function(f_name) {
   let code = await function_dependencies_code(f_name);
   const myString = "Hello, Firebase Storage!";
-  firebase_upload_string(myString, "uploads/hello.txt");
+  firebase_upload_string(code, "function/");
 }
