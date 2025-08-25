da3644e62e2a8da11edb1cc295704352e5c6beab
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:26:09 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,3 +1,4 @@
+import { list_join_newline } from "./list_join_newline.mjs";
 import { js_code_call_statement } from "./js_code_call_statement.mjs";
 import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 import { firebase_deploy_function_destination_version } from "./firebase_deploy_function_destination_version.mjs";
@@ -14,4 +15,5 @@ export async function app_replace() {
   );
   let code = await firebase_storage_download_property(destination, "code");
   let result = js_code_call_statement(f_name);
+  let code2 = list_join_newline(waited2);
 }
