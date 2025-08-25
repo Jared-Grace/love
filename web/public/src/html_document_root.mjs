33ec16f51e59d7a82313a5399433aa4a4314c7c0
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 17:58:12 -0400

    function_auto_path public\src\html_document_root.mjs

---
+++ web/public/src/html_document_root.mjs
@@ -0,0 +1,8 @@
+import { html_component_wrap } from "./html_component_wrap.mjs";
+import { marker } from "./marker.mjs";
+export function html_document_root() {
+  marker("1");
+  let body = document.body;
+  let component = html_component_wrap(body);
+  return component;
+}
