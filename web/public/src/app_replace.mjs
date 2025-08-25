97d1ca298c93bc7eeee5edee282ac12283638249
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 15:48:47 -0400

    function_auto_path public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -7,9 +7,8 @@ export async function app_replace() {
   let f_name = app_replace_main.name;
   let destination_version =
     firebase_deploy_function_destination_version(f_name);
-  let url = downloadJSON(
-    "https://firebasestorage.googleapis.com/v0/b/jared-grace.firebasestorage.app/o/myfile.json?alt=media",
-  );
+  let url =
+    "https://firebasestorage.googleapis.com/v0/b/jared-grace.firebasestorage.app/o/myfile.json?alt=media";
   app_replace_main();
   let json = await http_json(url);
 }
