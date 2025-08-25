f09e74382096a30c41d93d836bcf765c7661eb8e
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 12:54:02 -0400

    function_auto_path public\src\html_overwrite.mjs

--- web/public/src/html_overwrite.mjs
+++ web/public/src/html_overwrite.mjs
@@ -7,6 +7,7 @@ export async function html_overwrite(name, body) {
 <head>
   <meta charset="UTF-8">
   <title>${name}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
   ${body}
