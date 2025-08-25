59a4222eb6b43ffd4b0adf9bd47ddd5f428c7be3
Grace-Jared <christrosetolife@gmail.com>
2025-08-24 22:38:03 -0400

    function_auto_path public\src\app_replace_rule_sets.mjs

--- web/public/src/app_replace_rule_sets.mjs
+++ web/public/src/app_replace_rule_sets.mjs
@@ -20,6 +20,11 @@ export function app_replace_rule_sets() {
       rules: ["= > 1 = 1", "1 1 > 1 + 1"],
       start: "1=1",
     },
+    {
+      name: "Binary Counting",
+      rules: ["0 a > 1", "1 a > a 0", "c > a c", "b a > b 1"],
+      start: "1=1",
+    },
   ];
   return v;
 }
