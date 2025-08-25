6f6ff281b448247db653abb54227accd5f7bebf8
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:10:09 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,6 +1,6 @@
+import { firebase_storage_url } from "./firebase_storage_url.mjs";
 import { string_replace } from "./string_replace.mjs";
 import { log } from "./log.mjs";
-import { firebase_storage_url_project } from "./firebase_storage_url_project.mjs";
 import { firebase_deploy_function_destination_version } from "./firebase_deploy_function_destination_version.mjs";
 import { http_json } from "./http_json.mjs";
 import { app_replace_main } from "./app_replace_main.mjs";
@@ -11,12 +11,7 @@ export async function app_replace() {
   let destination_version =
     firebase_deploy_function_destination_version(f_name);
   let replaced = string_replace(destination_version, "/", "%2F");
-  let url =
-    "https://firebasestorage.googleapis.com/v0/b/" +
-    firebase_storage_url_project() +
-    "/o/" +
-    replaced +
-    "?alt=media";
+  let url = firebase_storage_url(replaced);
   let json = await http_json(url);
   log(json);
 }
