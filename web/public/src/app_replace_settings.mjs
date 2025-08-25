e86c2f6de9db2778a6ae58ad192b8ce24bbe2f8b
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:18:20 -0400

    function_auto_path public\src\app_replace_settings.mjs

--- web/public/src/app_replace_settings.mjs
+++ web/public/src/app_replace_settings.mjs
@@ -6,5 +6,9 @@ export function app_replace_settings(context) {
   function lambda2() {}
   html_button(root, emoji_font_larger() + " Font size larger", lambda2);
   function lambda3() {}
-  let component = html_button(parent, text, lambda3);
+  let component = html_button(
+    parent,
+    emoji_font_larger() + " Font size smaller",
+    lambda3,
+  );
 }
