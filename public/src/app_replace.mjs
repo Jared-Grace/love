2aaeba56958124fed2f0079ff0952f1b8fd848d4
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 16:19:58 -0400

    function_auto_path public\src\app_replace.mjs

--- public/src/app_replace.mjs
+++ public/src/app_replace.mjs
@@ -1,5 +1,4 @@
-import { object_property_get } from "./object_property_get.mjs";
-import { firebase_storage_download } from "./firebase_storage_download.mjs";
+import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 import { log } from "./log.mjs";
 import { firebase_deploy_function_destination_version } from "./firebase_deploy_function_destination_version.mjs";
 import { app_replace_main } from "./app_replace_main.mjs";
@@ -10,7 +9,9 @@ export async function app_replace() {
   let destination_version =
     firebase_deploy_function_destination_version(f_name);
   const property_name = "destination";
-  let parsed = await firebase_storage_download(destination_version);
-  let destination = object_property_get(parsed, property_name);
+  let destination = await firebase_storage_download_property(
+    destination_version,
+    property_name,
+  );
   log(destination);
 }
