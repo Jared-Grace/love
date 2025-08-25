7a5c9c78c4965b36ee71cf15a144f7c074a89665
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 14:57:43 -0400

    function_auto_path public\src\firebase_deploy_function.mjs

--- public/src/firebase_deploy_function.mjs
+++ public/src/firebase_deploy_function.mjs
@@ -1,22 +1,5 @@
-import { firebase_admin } from "./firebase_admin.mjs";
-import { log } from "./log.mjs";
+import { firebase_upload_string } from "./firebase_upload_string.mjs";
 export async function firebase_deploy_function() {
-  async function firebase_upload_string(content, destination) {
-    const admin = await firebase_admin();
-    const bucket = admin.storage().bucket();
-    const file = bucket.file(destination);
-    let v = Buffer.from(content);
-    await file.save(v, {
-      contentType: "text/plain",
-      gzip: true,
-      metadata: {
-        cacheControl: "no-cache",
-      },
-    });
-    console.log(`Uploaded string to ${destination}`);
-    const url = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
-    console.log("Accessible at:", url);
-  }
   const myString = "Hello, Firebase Storage!";
   firebase_upload_string(myString, "uploads/hello.txt");
 }
---
+++ public/src/firebase_upload_string.mjs
@@ -0,0 +1,18 @@
+import {log} from "./log.mjs";
+import {firebase_admin} from "./firebase_admin.mjs";
+export async function firebase_upload_string(content, destination) {
+  const admin = await firebase_admin();
+  const bucket = admin.storage().bucket();
+  const file = bucket.file(destination);
+  let v = Buffer.from(content);
+  await file.save(v, {
+    contentType: "text/plain",
+    gzip: true,
+    metadata: {
+      cacheControl: "no-cache"
+    }
+  });
+  console.log(`Uploaded string to ${destination}`);
+  const url = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
+  console.log("Accessible at:", url);
+}
