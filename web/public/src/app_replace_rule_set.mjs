4f465de4a222f9ce8247893107e439f7f28d415c
Grace-Jared <christrosetolife@gmail.com>
2025-08-25 19:44:43 -0400

    function_auto_path public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -105,6 +105,7 @@ export function app_replace_rule_set(context) {
       function lambda7(item2) {
         app_replace_rule_valid(rule2, index_selected, current_list);
       }
+      let size2 = list_size(list);
       let r = range(count);
       let ne = list_any(current_list, lambda7);
     }
