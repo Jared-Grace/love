feea0909bb052383b3612947310de711330e6b7b
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 17:10:16 -0400

    function_auto_path public\src\js_dollar.mjs

--- web/public/src/js_dollar.mjs
+++ web/public/src/js_dollar.mjs
@@ -1,4 +1,3 @@
-import { marker_top } from "./marker_top.mjs";
 import { js_dollar_choices } from "./js_dollar_choices.mjs";
 import { list_get_end_3 } from "./list_get_end_3.mjs";
 import { lambda_invoke_multiple } from "./lambda_invoke_multiple.mjs";
@@ -30,7 +29,7 @@ export async function js_dollar(ast) {
         return;
       }
       let remaining = list_skip(split, 2);
-      await marker_top();
+      mk;
       if (second === choice_name) {
         const a = {
           remaining,
