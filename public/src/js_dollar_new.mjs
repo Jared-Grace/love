48c4bd8e20d37f0390882313f456e55c008d5706
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:03:43 -0400

    function_auto_path public\src\js_dollar_new.mjs

--- public/src/js_dollar_new.mjs
+++ public/src/js_dollar_new.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { marker_next_get } from "./marker_next_get.mjs";
 import { js_dollar_choices } from "./js_dollar_choices.mjs";
 import { function_transform_marker_specified } from "./function_transform_marker_specified.mjs";
@@ -7,6 +8,7 @@ export async function js_dollar_new(code) {
   async function lambda(a) {
     let { next } = marker_next_get(a);
     let oe = js_declare_single_init(next);
+    log(message);
   }
   let code2 = await function_transform_marker_specified(
     js_dollar_choices.name,
