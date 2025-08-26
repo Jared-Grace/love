6681921b8648c6d169cb1662a5513b8090763bb6
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:19:48 -0400

    r js_declaration_single_block_blody js_declaration_single_block_body

--- web/public/src/functions_combine.mjs
+++ web/public/src/functions_combine.mjs
@@ -3,7 +3,7 @@ import { list_size_1_assert } from "./list_size_1_assert.mjs";
 import { js_auto } from "./js_auto.mjs";
 import { functions_combine_name } from "./functions_combine_name.mjs";
 import { list_add } from "./list_add.mjs";
-import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
+import { js_declaration_single_block_body } from "./js_declaration_single_block_body.mjs";
 import { each_async } from "./each_async.mjs";
 import { function_new_transform } from "./function_new_transform.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
@@ -12,7 +12,7 @@ export async function functions_combine(f_names_comma) {
   list_size_1_assert(list);
   let { f_names, combined } = await functions_combine_name(f_names_comma);
   async function lambda2(ast) {
-    let body_block = js_declaration_single_block_blody(ast);
+    let body_block = js_declaration_single_block_body(ast);
     async function lambda(f_name) {
       let item = js_parse_expression(f_name);
       list_add(body_block, item);
--- web/public/src/task_new.mjs
+++ web/public/src/task_new.mjs
@@ -2,7 +2,7 @@ import { data_function_current_restore } from "./data_function_current_restore.m
 import { function_name_unalias } from "./function_name_unalias.mjs";
 import { task_function_name_part } from "./task_function_name_part.mjs";
 import { function_name_combine_multiple } from "./function_name_combine_multiple.mjs";
-import { js_declaration_single_block_blody } from "./js_declaration_single_block_blody.mjs";
+import { js_declaration_single_block_body } from "./js_declaration_single_block_body.mjs";
 import { js_call_insert } from "./js_call_insert.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { function_new } from "./function_new.mjs";
@@ -22,7 +22,7 @@ export async function task_new(task_name) {
     await function_new(f_name_task);
   }
   async function lambda(ast) {
-    let body_block = js_declaration_single_block_blody(ast);
+    let body_block = js_declaration_single_block_body(ast);
     js_call_insert(todo.name, [], body_block, 0);
   }
   let result = await function_transform(f_name_task, lambda);
