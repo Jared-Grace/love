6734c20e2a97d4a65995973a85b4bc9750f455aa
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:13:05 -0400

    function_auto_path public\src\function_new_js.mjs

--- public/src/function_new_js.mjs
+++ public/src/function_new_js.mjs
@@ -1,10 +1,10 @@
+import { function_new_js_name } from "./function_new_js_name.mjs";
 import { js_declaration_single_params_add } from "./js_declaration_single_params_add.mjs";
-import { function_name_combine } from "./function_name_combine.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { marker } from "./marker.mjs";
 import { function_new } from "./function_new.mjs";
 export async function function_new_js(f_name_unprefixed) {
-  let combined = function_name_combine("js", f_name_unprefixed);
+  let combined = function_new_js_name(f_name_unprefixed);
   await function_new(combined);
   async function lambda(ast) {
     marker("1");
