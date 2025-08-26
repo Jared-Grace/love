6c42eb82224221003217e341c20ddf6f50208aae
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 17:13:03 -0400

    function_auto_path public\src\js_dollar_new_args.mjs

--- web/public/src/js_dollar_new_args.mjs
+++ web/public/src/js_dollar_new_args.mjs
@@ -1,4 +1,6 @@
+import { js_dollar_new_name } from "./js_dollar_new_name.mjs";
 import { marker } from "./marker.mjs";
-export function js_dollar_new_args() {
+export function js_dollar_new_args(code) {
+  let combined = js_dollar_new_name(code);
   marker("1");
 }
