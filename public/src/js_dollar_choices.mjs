fb78bc7c840d8033a45800d063d8a95c8927109a
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 15:57:00 -0400

    function_auto_path public\src\js_dollar_choices.mjs

--- public/src/js_dollar_choices.mjs
+++ public/src/js_dollar_choices.mjs
@@ -1,40 +1,50 @@
-import {js_dollar_f} from "./js_dollar_f.mjs";
-import {js_dollar_s} from "./js_dollar_s.mjs";
-import {js_dollar_g} from "./js_dollar_g.mjs";
-import {js_dollar_a} from "./js_dollar_a.mjs";
-import {js_dollar_rf} from "./js_dollar_rf.mjs";
-import {js_dollar_rt} from "./js_dollar_rt.mjs";
-import {js_dollar_r} from "./js_dollar_r.mjs";
-import {js_dollar_ie} from "./js_dollar_ie.mjs";
-import {js_dollar_i} from "./js_dollar_i.mjs";
+import { js_dollar_f } from "./js_dollar_f.mjs";
+import { js_dollar_s } from "./js_dollar_s.mjs";
+import { js_dollar_g } from "./js_dollar_g.mjs";
+import { js_dollar_a } from "./js_dollar_a.mjs";
+import { js_dollar_rf } from "./js_dollar_rf.mjs";
+import { js_dollar_rt } from "./js_dollar_rt.mjs";
+import { js_dollar_r } from "./js_dollar_r.mjs";
+import { js_dollar_ie } from "./js_dollar_ie.mjs";
+import { js_dollar_i } from "./js_dollar_i.mjs";
 export function js_dollar_choices() {
-  let v2 = [{
-    name: "i",
-    fn: js_dollar_i
-  }, {
-    name: "ie",
-    fn: js_dollar_ie
-  }, {
-    name: "r",
-    fn: js_dollar_r
-  }, {
-    name: "rt",
-    fn: js_dollar_rt
-  }, {
-    name: "rf",
-    fn: js_dollar_rf
-  }, {
-    name: "a",
-    fn: js_dollar_a
-  }, {
-    name: "g",
-    fn: js_dollar_g
-  }, {
-    name: "s",
-    fn: js_dollar_s
-  }, {
-    name: "f",
-    fn: js_dollar_f
-  }];
-  return v2;
+  let choices = [
+    {
+      name: "i",
+      fn: js_dollar_i,
+    },
+    {
+      name: "ie",
+      fn: js_dollar_ie,
+    },
+    {
+      name: "r",
+      fn: js_dollar_r,
+    },
+    {
+      name: "rt",
+      fn: js_dollar_rt,
+    },
+    {
+      name: "rf",
+      fn: js_dollar_rf,
+    },
+    {
+      name: "a",
+      fn: js_dollar_a,
+    },
+    {
+      name: "g",
+      fn: js_dollar_g,
+    },
+    {
+      name: "s",
+      fn: js_dollar_s,
+    },
+    {
+      name: "f",
+      fn: js_dollar_f,
+    },
+  ];
+  return choices;
 }
