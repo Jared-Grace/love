d14bf9bb4836d7b5ddd57765d167fb1ce979ff3a
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:17:22 -0400

    function_auto_path public\src\function_new_js.mjs

--- web/public/src/function_new_js.mjs
+++ web/public/src/function_new_js.mjs
@@ -1,14 +1,6 @@
+import { function_new_js_inner } from "./function_new_js_inner.mjs";
 import { function_new_js_name } from "./function_new_js_name.mjs";
-import { js_declaration_single_params_add } from "./js_declaration_single_params_add.mjs";
-import { function_transform } from "./function_transform.mjs";
-import { marker } from "./marker.mjs";
-import { function_new } from "./function_new.mjs";
 export async function function_new_js(f_name_unprefixed) {
   let combined = function_new_js_name(f_name_unprefixed);
-  await function_new(combined);
-  async function lambda(ast) {
-    marker("1");
-    js_declaration_single_params_add(ast, ["ast"]);
-  }
-  let result = await function_transform(combined, lambda);
+  await function_new_js_inner(combined);
 }
---
+++ web/public/src/function_new_js_inner.mjs
@@ -0,0 +1,12 @@
+import {function_transform} from "./function_transform.mjs";
+import {js_declaration_single_params_add} from "./js_declaration_single_params_add.mjs";
+import {marker} from "./marker.mjs";
+import {function_new} from "./function_new.mjs";
+export async function function_new_js_inner(combined) {
+  await function_new(combined);
+  async function lambda(ast) {
+    marker("1");
+    js_declaration_single_params_add(ast, ["ast"]);
+  }
+  let result = await function_transform(combined, lambda);
+}
