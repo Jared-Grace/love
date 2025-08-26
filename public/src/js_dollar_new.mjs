fcb484d7dbee2fe4ead257ceb4dd5ba1f87083d8
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 15:59:04 -0400

    data_file_update public\src\js_dollar_new.mjs

--- public/src/js_dollar_new.mjs
+++ public/src/js_dollar_new.mjs
@@ -2,9 +2,10 @@ import { function_transform_marker_specified } from "./function_transform_marker
 import { marker } from "./marker.mjs";
 export async function js_dollar_new(code) {
   marker("1");
+  async function lambda(a) {}
   let code2 = await function_transform_marker_specified(
     f_name,
     marker_name,
-    async function lambda(a) {},
+    lambda,
   );
 }
