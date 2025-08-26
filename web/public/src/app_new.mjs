92cc0450d901a6fd3ae84050f1c7300d179237de
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 15:46:55 -0400

    function_auto_path public\src\app_new.mjs

--- web/public/src/app_new.mjs
+++ web/public/src/app_new.mjs
@@ -6,7 +6,8 @@ import { marker } from "./marker.mjs";
 export async function app_new(name) {
   marker("1");
   let a_name = app_name_prefixed(name);
-  await function_new(a_name);
   let combined = app_name_main(name);
+  await function_new(a_name);
+  await function_new(combined);
   await html_new(name);
 }
