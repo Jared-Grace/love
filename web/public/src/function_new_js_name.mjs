b8227ab892a1b6007c48dde55c9bad4c55706f1c
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:13:05 -0400

    function_auto_path public\src\function_new_js.mjs

---
+++ web/public/src/function_new_js_name.mjs
@@ -0,0 +1,5 @@
+import {function_name_combine} from "./function_name_combine.mjs";
+export function function_new_js_name(f_name_unprefixed) {
+  let combined2 = function_name_combine("js", f_name_unprefixed);
+  return combined2;
+}
