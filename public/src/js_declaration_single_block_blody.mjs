02a208b96d20ac96af0dd286df7a4fe567adcd2d
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:19:48 -0400

    r js_declaration_single_block_blody js_declaration_single_block_body

--- public/src/js_declaration_single_block_blody.mjs
+++
@@ -1,7 +0,0 @@
-import { js_declaration_to_block_body } from "./js_declaration_to_block_body.mjs";
-import { js_declaration_single } from "./js_declaration_single.mjs";
-export function js_declaration_single_block_blody(ast) {
-  let declaration = js_declaration_single(ast);
-  let body_block = js_declaration_to_block_body(declaration);
-  return body_block;
-}
--- public/src/js_declaration_single_block_body.mjs
+++ public/src/js_declaration_single_block_body.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { js_declaration_to_block_body } from "./js_declaration_to_block_body.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 export function js_declaration_single_block_body(ast) {
+  marker("1");
   let declaration = js_declaration_single(ast);
   let body_block = js_declaration_to_block_body(declaration);
   return body_block;
