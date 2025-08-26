e1dd331bd7cc59216399dc365421c58a109993f4
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:15:26 -0400

    function_auto_path public\src\js_dollar_new.mjs

--- public/src/js_dollar_new.mjs
+++ public/src/js_dollar_new.mjs
@@ -1,7 +1,7 @@
+import { list_add } from "./list_add.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { js_property_parse_expression_add } from "./js_property_parse_expression_add.mjs";
-import { js_string } from "./js_string.mjs";
 import { marker_next_declare_single_init } from "./marker_next_declare_single_init.mjs";
 import { log } from "./log.mjs";
 import { js_dollar_choices } from "./js_dollar_choices.mjs";
@@ -9,8 +9,8 @@ import { function_transform_marker_specified } from "./function_transform_marker
 import { marker } from "./marker.mjs";
 export async function js_dollar_new(code) {
   marker("1");
+  let combined = function_name_combine(js_dollar_choices.name, code);
   async function lambda(a) {
-    let combined = function_name_combine(js_dollar_choices.name, code);
     let array_expression = marker_next_declare_single_init(a);
     let { elements } = array_expression;
     let oe = {
@@ -18,11 +18,11 @@ export async function js_dollar_new(code) {
       properties: [],
     };
     let { properties } = oe;
-    let s = js_string(code);
     let code_string = js_code_string(value_string);
     js_property_parse_expression_add("name", code_string, properties);
-    js_property_parse_expression_add("fn", code_string, properties);
+    js_property_parse_expression_add("fn", combined, properties);
     log(array_expression);
+    list_add(list, item);
   }
   let code2 = await function_transform_marker_specified(
     js_dollar_choices.name,
