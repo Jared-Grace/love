3c12914fe6da1b1264381a4a5ded775e0fe37f1d
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:29:04 -0400

    function_auto_path public\src\each_range.mjs

--- web/public/src/each_range.mjs
+++ web/public/src/each_range.mjs
@@ -2,6 +2,5 @@ import { range } from "./range.mjs";
 import { each } from "./each.mjs";
 export function each_range(count, lambda) {
   let list = range(count);
-  let v = each(list, lambda);
-  return v;
+  each(list, lambda);
 }
