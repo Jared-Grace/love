0ad5ec055bfe9870a9e8d4351b490a2118abd225
Grace-Jared <christrosetolife@gmail.com>
2025-08-26 16:07:41 -0400

    function_auto_path public\src\js_dollar_new.mjs

--- web/public/src/js_dollar_new.mjs
+++ web/public/src/js_dollar_new.mjs
@@ -1,3 +1,5 @@
+import { js_parse_expression } from "./js_parse_expression.mjs";
+import { js_string } from "./js_string.mjs";
 import { js_property } from "./js_property.mjs";
 import { marker_next_declare_single_init } from "./marker_next_declare_single_init.mjs";
 import { log } from "./log.mjs";
@@ -13,7 +15,9 @@ export async function js_dollar_new(code) {
       type: "ObjectExpression",
       properties: [],
     };
-    let p = js_property("name", value);
+    let s = js_string(value_string);
+    let key = js_parse_expression("name");
+    let p = js_property(key, value);
     log(array_expression);
   }
   let code2 = await function_transform_marker_specified(
