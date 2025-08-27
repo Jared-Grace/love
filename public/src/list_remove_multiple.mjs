20c3c1738031bb6cfe4e1fd1c0ba5d0c83450822
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:38:24 -0400

    r list_remove_multiple list_remove_all_multiple

--- public/src/list_remove_multiple.mjs
+++
@@ -1,8 +0,0 @@
-import { list_remove_all } from "./list_remove_all.mjs";
-import { each } from "./each.mjs";
-export function list_remove_multiple(removals, list) {
-  function lambda(r) {
-    list_remove_all(list, r);
-  }
-  each(removals, lambda);
-}
