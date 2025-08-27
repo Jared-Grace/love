fdb14c4fb39f200f0346bd4005f6ecdae6045f4c
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 17:10:32 -0400

    function_auto_path public\src\js_code_declaration.mjs

--- web/public/src/js_code_declaration.mjs
+++ web/public/src/js_code_declaration.mjs
@@ -1,7 +1,6 @@
 import { js_code_call_args } from "./js_code_call_args.mjs";
 import { js_keyword_function } from "./js_keyword_function.mjs";
 import { js_code_wrap_braces } from "./js_code_wrap_braces.mjs";
-import { js_code_call } from "./js_code_call.mjs";
 export function js_code_declaration(f_name, inside, async_is) {
   let args = [];
   let prefix = async_is ? "async " : "";
@@ -9,8 +8,7 @@ export function js_code_declaration(f_name, inside, async_is) {
     prefix +
     js_keyword_function() +
     " " +
-    js_code_call(f_name) +
+    js_code_call_args(f_name, args) +
     js_code_wrap_braces(inside);
   return v;
-  js_code_call_args(f_name, args);
 }
