b298c888f593e4b3b5a5527e29ba2d442761a81c
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:31:34 -0400

    cs list_remove_multiple if_exists

--- public/src/list_remove_multiple_if_exists.mjs
+++ public/src/list_remove_multiple_if_exists.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { each } from "./each.mjs";
 export function list_remove_multiple_if_exists(removals, list) {
+  marker("1");
   function lambda(r) {
     list_remove(list, r);
   }
