e6cce14de1a001b4c33663c8492862fe2d6a9bd9
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:39:15 -0400

    function_auto_path public\src\js_dollar_a.mjs

--- web/public/src/js_dollar_a.mjs
+++ web/public/src/js_dollar_a.mjs
@@ -1,3 +1,4 @@
+import { list_remove_multiple } from "./list_remove_multiple.mjs";
 import { list_first } from "./list_first.mjs";
 import { integer_to } from "./integer_to.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
@@ -7,7 +8,6 @@ import { each } from "./each.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { js_identifiers_named } from "./js_identifiers_named.mjs";
-import { list_remove_all_multiple } from "./list_remove_all_multiple.mjs";
 export function js_dollar_a({ stack1, stack2, ast, afters, remaining }) {
   let count = 1;
   let s1 = list_size_1(remaining);
@@ -25,7 +25,7 @@ export function js_dollar_a({ stack1, stack2, ast, afters, remaining }) {
     count,
   });
   function lambda({ stack1, next, stack2, ast, declarations }) {
-    list_remove_all_multiple([stack1, next], stack2);
+    list_remove_multiple([stack1, next], stack2);
     function lambda2(declaration) {
       let { id, init } = declaration;
       let { name } = id;
