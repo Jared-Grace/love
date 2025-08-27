aafc44f635b870c137b5a702314f1b142b192339
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 17:05:24 -0400

    function_auto_path public\src\js_dollar_new_args.mjs

--- web/public/src/js_dollar_new_args.mjs
+++ web/public/src/js_dollar_new_args.mjs
@@ -1,3 +1,4 @@
+import { js_declaration_param_add_node } from "./js_declaration_param_add_node.mjs";
 import { js_dollar_choice_arguments } from "./js_dollar_choice_arguments.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { js_dollar_new_name } from "./js_dollar_new_name.mjs";
@@ -6,7 +7,9 @@ export async function js_dollar_new_args(code) {
   let names = await js_dollar_choice_arguments();
   return names;
   let combined = js_dollar_new_name(code);
-  async function lambda2(ast) {}
+  async function lambda2(ast) {
+    js_declaration_param_add_node(declaration, item);
+  }
   await function_transform(combined, lambda2);
   marker("1");
 }
