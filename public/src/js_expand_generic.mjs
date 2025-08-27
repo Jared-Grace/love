53ffb0e479f418364d59e364da878cdc76344c17
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:34:04 -0400

    function_auto_path public\src\js_expand_generic.mjs

--- public/src/js_expand_generic.mjs
+++ public/src/js_expand_generic.mjs
@@ -44,7 +44,7 @@ export async function js_expand_generic(next, stack2, index, ast) {
     let f_names = functions_names();
     list_remove_multiple_if_exists(f_names, identifiers_all);
     log({
-      identifiers_alf_names,
+      identifiers_all,
       f_names,
     });
     function lambda2(i) {
