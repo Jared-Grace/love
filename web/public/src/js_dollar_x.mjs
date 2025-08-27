fad666ae886b172efec89f04736799c84c2bb7e3
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:27:15 -0400

    function_auto_path public\src\js_dollar_x.mjs

--- web/public/src/js_dollar_x.mjs
+++ web/public/src/js_dollar_x.mjs
@@ -1,3 +1,4 @@
+import { list_remove } from "./list_remove.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_next_index } from "./list_next_index.mjs";
 import { list_is } from "./list_is.mjs";
@@ -18,5 +19,6 @@ export async function js_dollar_x({
     let index = list_next_index(stack2, stack1);
     let next = list_get(stack2, index);
     let inserted = await js_expand_generic(next, stack2, index, ast);
+    list_remove(list, item);
   }
 }
