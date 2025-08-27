bff29ba6227cee05136c02052b5308ec3345d321
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:37:46 -0400

    function_auto_path public\src\list_remove_multiple.mjs

--- public/src/list_remove_multiple.mjs
+++ public/src/list_remove_multiple.mjs
@@ -1,4 +1,4 @@
-import { list_remove } from "./list_remove.mjs";
+import { list_remove_all } from "./list_remove_all.mjs";
 import { each } from "./each.mjs";
 export function list_remove_multiple(removals, list) {
   function lambda(r) {
