16e58985aa05e76eb699765c1a42bcc499771d06
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:15:37 -0400

    function_auto_path public\src\js_dollar_x.mjs

--- public/src/js_dollar_x.mjs
+++ public/src/js_dollar_x.mjs
@@ -1,3 +1,4 @@
+import { list_get } from "./list_get.mjs";
 import { list_next_index } from "./list_next_index.mjs";
 import { list_is } from "./list_is.mjs";
 import { js_expand_generic } from "./js_expand_generic.mjs";
@@ -15,7 +16,8 @@ export async function js_dollar_x({
   return;
   let l = list_is(stack2);
   if (l) {
-    let next = list_next_index(stack2, stack1);
+    let index = list_next_index(stack2, stack1);
+    let item = list_get(list, index2);
     let inserted = await js_expand_generic(next, stack2, index);
   }
 }
