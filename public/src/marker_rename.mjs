84c040983875f3f97c5d9d009ce0333f00cf0173
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:22:22 -0400

    function_auto_path public\src\js_expand_generic.mjs

--- public/src/marker_rename.mjs
+++ public/src/marker_rename.mjs
@@ -1,13 +1,8 @@
-import { function_transform_marker } from "./function_transform_marker.mjs";
-import { data_function_current_get } from "./data_function_current_get.mjs";
+import { function_transform_marker_current } from "./function_transform_marker_current.mjs";
 import { marker } from "./marker.mjs";
 export async function marker_rename() {
   marker("1");
   marker("2");
-  async function lambda(a) {}
-  $x;
-  marker("1");
-  let f_name_current = await data_function_current_get();
-  let v = await function_transform_marker(f_name_current, lambda);
-  let v = v;
+  async function lambda(a) {}$x
+  let v = await function_transform_marker_current(lambda);
 }
