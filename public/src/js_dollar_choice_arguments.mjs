f441ae30ae2bfe023b7c0583d97ffe6723f2ca92
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 17:16:48 -0400

    function_auto_path public\src\js_dollar_choice_arguments.mjs

--- public/src/js_dollar_choice_arguments.mjs
+++ public/src/js_dollar_choice_arguments.mjs
@@ -4,8 +4,7 @@ import { marker_next_declare_single_init } from "./marker_next_declare_single_in
 export async function js_dollar_choice_arguments() {
   let result = null;
   async function lambda(a) {
-    let oe = marker_next_declare_single_init(a);
-    result = oe;
+    result = marker_next_declare_single_init(a);
   }
   let code2 = await function_transform_marker_specified(
     js_dollar.name,
