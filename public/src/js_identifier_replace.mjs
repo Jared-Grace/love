534b47643ef0a523b5b8a1e5c1ffcd8ff30b11e7
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:24:24 -0400

    function_auto_path public\src\js_identifier_replace.mjs

--- public/src/js_identifier_replace.mjs
+++ public/src/js_identifier_replace.mjs
@@ -2,6 +2,8 @@ import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function js_identifier_replace(ast, name_from, name_to) {
+  if (false) {
+  }
   function lambda(v) {
     let { node } = v;
     if (object_property_get(node, "name") === name_from) {
