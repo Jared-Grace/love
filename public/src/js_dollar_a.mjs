d0f8458a1b8cd59982e47d3eadb86346cd529ddc
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:38:24 -0400

    r list_remove_multiple list_remove_all_multiple

--- public/src/js_dollar_a.mjs
+++ public/src/js_dollar_a.mjs
@@ -7,7 +7,7 @@ import { each } from "./each.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { js_identifiers_named } from "./js_identifiers_named.mjs";
-import { list_remove_multiple } from "./list_remove_multiple.mjs";
+import { list_remove_all_multiple } from "./list_remove_all_multiple.mjs";
 export function js_dollar_a({ stack1, stack2, ast, afters, remaining }) {
   let count = 1;
   let s1 = list_size_1(remaining);
@@ -25,7 +25,7 @@ export function js_dollar_a({ stack1, stack2, ast, afters, remaining }) {
     count,
   });
   function lambda({ stack1, next, stack2, ast, declarations }) {
-    list_remove_multiple([stack1, next], stack2);
+    list_remove_all_multiple([stack1, next], stack2);
     function lambda2(declaration) {
       let { id, init } = declaration;
       let { name } = id;
--- public/src/list_remove_all_multiple.mjs
+++ public/src/list_remove_all_multiple.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_remove_all } from "./list_remove_all.mjs";
 import { each } from "./each.mjs";
 export function list_remove_all_multiple(removals, list) {
+  marker("1");
   function lambda(r) {
     list_remove_all(list, r);
   }
