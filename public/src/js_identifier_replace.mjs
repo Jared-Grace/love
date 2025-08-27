8c22cf2bc842421437bbf5eb4ba56b1c349ed37f
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:24:37 -0400

    function_auto_path public\src\js_identifier_replace.mjs

--- public/src/js_identifier_replace.mjs
+++ public/src/js_identifier_replace.mjs
@@ -3,7 +3,8 @@ import { object_property_set } from "./object_property_set.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function js_identifier_replace(ast, name_from, name_to) {
-  if (equal(left, right)) {
+  if (equal(name_from, name_to)) {
+    return;
   }
   function lambda(v) {
     let { node } = v;
