3e43f7a1fae55221b763bbe4f5409f931dceb191
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 16:54:53 -0400

    function_auto_path public\src\js_dollar_new_args.mjs

--- web/public/src/js_dollar_new_args.mjs
+++ web/public/src/js_dollar_new_args.mjs
@@ -5,17 +5,16 @@ import { function_transform } from "./function_transform.mjs";
 import { js_dollar_new_name } from "./js_dollar_new_name.mjs";
 import { marker } from "./marker.mjs";
 export async function js_dollar_new_args(code) {
+  let result = null;
   async function lambda(a) {
-    let result = null;
     result = marker_next_declare_single_init(a);
-    return result;
   }
   let code2 = await function_transform_marker_specified(
     js_dollar.name,
     "choice_arguments",
     lambda,
   );
-  return code2;
+  return result;
   let combined = js_dollar_new_name(code);
   async function lambda2(ast) {}
   await function_transform(combined, lambda2);
