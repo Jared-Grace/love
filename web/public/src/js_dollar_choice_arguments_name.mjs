1eaa77ad9578c398d079101b1a9edb3e915f0f49
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 17:16:09 -0400

    cs js_dollar_choice_arguments name

--- web/public/src/js_dollar_choice_arguments_name.mjs
+++ web/public/src/js_dollar_choice_arguments_name.mjs
@@ -1,9 +1,11 @@
+import { marker } from "./marker.mjs";
 import { js_dollar } from "./js_dollar.mjs";
 import { function_transform_marker_specified } from "./function_transform_marker_specified.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { marker_next_declare_single_init } from "./marker_next_declare_single_init.mjs";
 export async function js_dollar_choice_arguments_name() {
+  marker("1");
   let names = null;
   async function lambda(a) {
     let oe = marker_next_declare_single_init(a);
