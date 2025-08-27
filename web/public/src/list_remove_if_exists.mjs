5404eb41b0d1260f843611f29f5e57ba248a4631
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:33:12 -0400

    function_auto_path public\src\list_remove_if_exists.mjs

--- web/public/src/list_remove_if_exists.mjs
+++ web/public/src/list_remove_if_exists.mjs
@@ -1,8 +1,8 @@
-import {list_remove} from "./list_remove.mjs";
-import {list_includes} from "./list_includes.mjs";
-export function list_remove_if_exists(f_names, f_name_before) {
-  let includes = list_includes(f_names, f_name_before);
+import { list_remove } from "./list_remove.mjs";
+import { list_includes } from "./list_includes.mjs";
+export function list_remove_if_exists(list, item) {
+  let includes = list_includes(list, item);
   if (includes) {
-    list_remove(f_names, f_name_before);
+    list_remove(list, item);
   }
 }
