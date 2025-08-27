83f7c4a8ee3e400cc9a4f708f1dd8a4a950f4255
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 19:32:29 -0400

    function_auto_path public\src\js_expand_generic.mjs

--- public/src/js_expand_generic.mjs
+++ public/src/js_expand_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_remove_multiple_if_exists } from "./list_remove_multiple_if_exists.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { list_concat } from "./list_concat.mjs";
@@ -42,6 +43,7 @@ export async function js_expand_generic(next, stack2, index, ast) {
     let identifiers_all = list_concat(identifiers, identifiers_call);
     let f_names = functions_names();
     list_remove_multiple_if_exists(f_names, identifiers_all);
+    log(message);
     function lambda2(i) {
       let unique = js_identifier_unique(identifiers_all, i);
       js_identifier_replace(ast_call, i, unique);
