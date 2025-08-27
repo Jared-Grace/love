c46c818d76e18c15268cef37812912b4183bfe45
Grace-Jared <christrosetolife@gmail.com>
2025-08-27 17:15:25 -0400

    function_auto_path public\src\js_dollar_x.mjs

--- web/public/src/js_dollar_x.mjs
+++ web/public/src/js_dollar_x.mjs
@@ -1,6 +1,6 @@
 import { js_expand_generic } from "./js_expand_generic.mjs";
 import { marker } from "./marker.mjs";
-export async function js_dollar_x(ast) {
+export async function js_dollar_x() {
   marker("1");
   return;
   let inserted = await js_expand_generic(next, stack2, index);
