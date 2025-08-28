7908ab47d55d998b4c9f83a410e3c57f3c13cf9d
Grace-Jared <christrosetolife@gmail.com>
2025-08-28 13:41:43 -0400

    function_auto_path public\src\js_auto_transform_new.mjs

--- web/public/src/js_auto_transform_new.mjs
+++ web/public/src/js_auto_transform_new.mjs
@@ -3,7 +3,6 @@ import { js_parse_expression } from "./js_parse_expression.mjs";
 import { function_new_js_name } from "./function_new_js_name.mjs";
 import { function_new_js } from "./function_new_js.mjs";
 import { marker_next_declare_single_init_elements } from "./marker_next_declare_single_init_elements.mjs";
-import { log } from "./log.mjs";
 import { js_auto_transforms } from "./js_auto_transforms.mjs";
 import { function_transform_marker_specified } from "./function_transform_marker_specified.mjs";
 import { marker } from "./marker.mjs";
@@ -15,7 +14,6 @@ export async function js_auto_transform_new(f_name_unprefixed) {
     let elements = marker_next_declare_single_init_elements(a);
     let expression = js_parse_expression(combined);
     list_add(elements, expression);
-    log(ae);
   }
   let code = await function_transform_marker_specified(
     js_auto_transforms.name,
