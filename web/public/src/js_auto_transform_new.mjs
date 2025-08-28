eab0553ea54ce49f9ec25e68e9a3ab88c7773776
Grace-Jared <christrosetolife@gmail.com>
2025-08-28 13:35:42 -0400

    function_auto_path public\src\js_auto_transform_new.mjs

--- web/public/src/js_auto_transform_new.mjs
+++ web/public/src/js_auto_transform_new.mjs
@@ -1,4 +1,10 @@
+import { function_transform_marker_specified } from "./function_transform_marker_specified.mjs";
 import { marker } from "./marker.mjs";
-export function js_auto_transform_new() {
+export async function js_auto_transform_new() {
   marker("1");
+  let code = await function_transform_marker_specified(
+    f_name,
+    marker_name,
+    async function lambda(a) {},
+  );
 }
