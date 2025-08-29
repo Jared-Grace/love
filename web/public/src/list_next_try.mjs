fe27d7917bc2c87787fa4c230859385b88a0ccf7
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 18:09:09 -0400

    cs list_next try

--- web/public/src/list_next_try.mjs
+++ web/public/src/list_next_try.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index_of_next } from "./list_index_of_next.mjs";
 export function list_next_try(list, item) {
+  marker("1");
   let index_next = list_index_of_next(list, item);
   let next = list_get(list, index_next);
   return next;
