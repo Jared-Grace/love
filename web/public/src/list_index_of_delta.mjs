4981a0523055a83920b9fbb4704d6957e3040021
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 18:10:29 -0400

    function_auto_path public\src\list_index_of_delta.mjs

--- web/public/src/list_index_of_delta.mjs
+++ web/public/src/list_index_of_delta.mjs
@@ -4,9 +4,10 @@ import { list_index_of } from "./list_index_of.mjs";
 export function list_index_of_delta(list, item, delta) {
   let index = list_index_of(list, item);
   let index_next = index + delta;
-  return index_next;
   let ii = list_index_is(list, index_next);
   if (not(ii)) {
-    return;
+    let v = null;
+    return v;
   }
+  return index_next;
 }
