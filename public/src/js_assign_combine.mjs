3a1a12ddfaf2f5b2e9d38d94d20bf9f39418ea6d
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 17:47:20 -0400

    function_auto_path public\src\js_assign_combine.mjs

--- public/src/js_assign_combine.mjs
+++ public/src/js_assign_combine.mjs
@@ -1,3 +1,4 @@
+import { null_is } from "./null_is.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { js_declare_single } from "./js_declare_single.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
@@ -8,6 +9,8 @@ export function js_assign_combine(ast) {
   function lambda(v) {
     let { node, stack } = v;
     let declaration = js_declare_single(node);
+    if (null_is(value)) {
+    }
     let e1 = list_get_end_1(stack);
   }
   js_visit_type(ast, "VariableDeclaration", lambda);
