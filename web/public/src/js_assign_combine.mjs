db3d67269ce4355ccca4938534b709b0f4ae0bde
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 17:41:59 -0400

    function_auto_path public\src\js_assign_combine.mjs

--- web/public/src/js_assign_combine.mjs
+++ web/public/src/js_assign_combine.mjs
@@ -6,7 +6,7 @@ export function js_assign_combine(ast) {
   marker("1");
   function lambda(v) {
     let { node } = v;
-    let declarator = js_declare_single(vd);
+    let result = js_declare_single(vd);
   }
   js_visit_type(ast, "VariableDeclaration", lambda);
 }
