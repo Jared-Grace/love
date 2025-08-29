ba99373afbe5e9c436e179ce6d6fa56f0e73ceb2
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 17:40:11 -0400

    function_auto_path public\src\js_declare_single.mjs

--- web/public/src/js_declare_single.mjs
+++ web/public/src/js_declare_single.mjs
@@ -1,9 +1,10 @@
+import { not } from "./not.mjs";
 import { list_single } from "./list_single.mjs";
 import { assert } from "./assert.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_declare_single(vd) {
   let b = js_node_type_is(vd, "VariableDeclaration");
-  if (false) {
+  if (not(b)) {
   }
   assert(b);
   let { declarations } = vd;
