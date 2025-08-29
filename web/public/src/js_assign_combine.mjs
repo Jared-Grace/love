2646932a9366e280015bc418c66a6439d8b4c41f
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 17:37:31 -0400

    function_auto_path public\src\js_assign_combine.mjs

--- web/public/src/js_assign_combine.mjs
+++ web/public/src/js_assign_combine.mjs
@@ -1,3 +1,4 @@
+import { list_second } from "./list_second.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { marker } from "./marker.mjs";
@@ -9,6 +10,7 @@ export function js_assign_combine(ast) {
     let { declarations } = node;
     let s1 = list_size_1(declarations);
     if (s1) {
+      let second = list_second(list);
     }
   }
   js_visit_type(ast, "VariableDeclaration", lambda);
