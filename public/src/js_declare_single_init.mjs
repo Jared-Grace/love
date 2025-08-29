d287d39a01e695abbf622bdf8ed89f69aec7a641
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 17:49:42 -0400

    function_auto_path public\src\js_declare_single_init.mjs

--- public/src/js_declare_single_init.mjs
+++ public/src/js_declare_single_init.mjs
@@ -2,6 +2,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { js_declare_single } from "./js_declare_single.mjs";
 export function js_declare_single_init(next) {
   let declarator = js_declare_single(next);
-  let oe = object_property_get(declarator, "init");
-  return oe;
+  let init = object_property_get(declarator, "init");
+  return init;
 }
