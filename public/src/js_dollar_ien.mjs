9dd200d96fbe3d0e594aa9a329b2a751b5d004ef
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 17:46:42 -0400

    function_auto_path public\src\js_dollar_ien.mjs

--- public/src/js_dollar_ien.mjs
+++ public/src/js_dollar_ien.mjs
@@ -1,5 +1,8 @@
-import { marker } from "./marker.mjs";
-export function js_dollar_ien({
+import { object_property_set } from "./object_property_set.mjs";
+import { equal } from "./equal.mjs";
+import { js_call_new_expression } from "./js_call_new_expression.mjs";
+import { js_dollar_i } from "./js_dollar_i.mjs";
+export async function js_dollar_ien({
   remaining,
   node,
   stack1,
@@ -8,6 +11,9 @@ export function js_dollar_ien({
   ast,
   afters,
 }) {
-  marker("1");
-  return;
+  js_dollar_i({
+    stack1,
+  });
+  let expression = await js_call_new_expression(equal.name, ast);
+  object_property_set(stack1, "test", expression);
 }
