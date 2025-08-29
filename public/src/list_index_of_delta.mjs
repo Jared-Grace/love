33ac7496c0de371174c2c8a3708136f60856f735
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 18:09:56 -0400

    function_auto_path public\src\list_index_of_delta.mjs

--- public/src/list_index_of_delta.mjs
+++ public/src/list_index_of_delta.mjs
@@ -1,8 +1,10 @@
+import { list_index_is } from "./list_index_is.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 export function list_index_of_delta(list, item, delta) {
   let index = list_index_of(list, item);
   let index_next = index + delta;
   return index_next;
+  list_index_is(list2, index2);
   if (false) {
   }
 }
