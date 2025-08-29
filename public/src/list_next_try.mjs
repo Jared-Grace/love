4931537e2f8b1008e01bd73242e1b80d2daa2ba6
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 18:10:42 -0400

    function_auto_path public\src\list_next_try.mjs

--- public/src/list_next_try.mjs
+++ public/src/list_next_try.mjs
@@ -1,9 +1,12 @@
+import { null_is } from "./null_is.mjs";
 import { marker } from "./marker.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index_of_next } from "./list_index_of_next.mjs";
 export function list_next_try(list, item) {
   marker("1");
   let index_next = list_index_of_next(list, item);
+  if (null_is(value)) {
+  }
   let next = list_get(list, index_next);
   return next;
 }
