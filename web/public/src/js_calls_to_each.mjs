12e8cd19c49fddb2f466f64707e747028f514b6a
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 21:05:15 -0400

    function_auto_path public\src\js_calls_to_each.mjs

--- web/public/src/js_calls_to_each.mjs
+++ web/public/src/js_calls_to_each.mjs
@@ -9,7 +9,7 @@ export function js_calls_to_each(ast) {
     let { expression } = node;
     function lambda3() {
       let a = null;
-      let a = f;
+      a = f;
     }
     js_node_type_is_if(expression, "AwaitExpression", lambda3);
     log(expression);
