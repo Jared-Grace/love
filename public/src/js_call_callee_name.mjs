f04fa93c04a74931bb0ca3c6709fd766856a04ec
Grace-Jared <christrosetolife@gmail.com>
2025-08-30 16:25:36 -0400

    function_auto_path public\src\js_call_callee_name.mjs

--- public/src/js_call_callee_name.mjs
+++ public/src/js_call_callee_name.mjs
@@ -1,7 +1,9 @@
+import { js_identifier_not_is } from "./js_identifier_not_is.mjs";
 import { object_property_get_name } from "./object_property_get_name.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function js_call_callee_name(expression) {
   let callee = object_property_get(expression, "callee");
+  let jin = js_identifier_not_is(id);
   let name = object_property_get_name(callee);
   return name;
 }
