89e9794c0a38d0e637ec4e50ae369d2575745057
Grace-Jared <christrosetolife@gmail.com>
2025-08-30 16:07:05 -0400

    function_auto_path public\src\js_declaration_single_block_body_add_return.mjs

--- web/public/src/js_declaration_single_block_body_add_return.mjs
+++ web/public/src/js_declaration_single_block_body_add_return.mjs
@@ -1,8 +1,6 @@
-import { list_add } from "./list_add.mjs";
+import { js_statement_return_empty_add } from "./js_statement_return_empty_add.mjs";
 import { js_declaration_single_block_body } from "./js_declaration_single_block_body.mjs";
-import { js_statement_return_empty } from "./js_statement_return_empty.mjs";
 export function js_declaration_single_block_body_add_return(ast) {
   let body_block = js_declaration_single_block_body(ast);
-  let r = js_statement_return_empty();
-  list_add(body_block, r);
+  js_statement_return_empty_add(body_block);
 }
