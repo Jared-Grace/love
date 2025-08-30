d3985bb182cdd63bc980ad1c59a51d7e310717ff
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 21:19:26 -0400

    function_auto_path public\src\list_index_of_delta.mjs

--- web/public/src/list_index_of_delta.mjs
+++ web/public/src/list_index_of_delta.mjs
@@ -1,9 +1,8 @@
+import { list_index_of_delta_outside } from "./list_index_of_delta_outside.mjs";
 import { not } from "./not.mjs";
 import { list_index_is } from "./list_index_is.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 export function list_index_of_delta(list, item, delta) {
-  let index = list_index_of(list, item);
-  let index_next = index + delta;
+  let index_next = list_index_of_delta_outside(list, item, delta);
   let ii = list_index_is(list, index_next);
   if (not(ii)) {
     let v = null;
