376f242c37a7fa4a45390e3a53ec557d9f2a7133
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 21:00:41 -0400

    function_auto_path public\src\js_calls_to_each.mjs

--- public/src/js_calls_to_each.mjs
+++ public/src/js_calls_to_each.mjs
@@ -1,3 +1,4 @@
+import { js_node_type_is_if } from "./js_node_type_is_if.mjs";
 import { log } from "./log.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_calls_to_each(ast) {
@@ -6,6 +7,7 @@ export function js_calls_to_each(ast) {
   function lambda(v) {
     let { node } = v;
     let { expression } = node;
+    js_node_type_is_if(node2, type, function lambda3() {});
     log(expression);
   }
   js_visit_type(ast, "ExpressionStatement", lambda);
