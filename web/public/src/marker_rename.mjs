3338c29bb9b9ab07e1fd341d6f33e5f3102f6109
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 20:46:13 -0400

    function_auto_path public\src\marker_rename.mjs

--- web/public/src/marker_rename.mjs
+++ web/public/src/marker_rename.mjs
@@ -5,7 +5,7 @@ import { marker } from "./marker.mjs";
 export async function marker_rename(from, to) {
   async function lambda(a) {
     let { v } = a;
-    log(message);
+    log(v);
   }
   marker("1");
   let f_name_current = await data_function_current_get();
