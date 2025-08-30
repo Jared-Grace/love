72172b1a76a4cc358fa364f96ecf34f5acd09b88
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 20:43:29 -0400

    function_auto_path public\src\js_assign_combine.mjs

--- public/src/js_assign_combine.mjs
+++ public/src/js_assign_combine.mjs
@@ -9,7 +9,6 @@ import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { js_declare_single } from "./js_declare_single.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 export function js_assign_combine(ast) {
-  return;
   function lambda(v) {
     let { node, stack } = v;
     let declaration = js_declare_single(node);
