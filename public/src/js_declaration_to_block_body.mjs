f9d2b070ef465dd8d7aac7cb67897d33d8adc6b9
Grace-Jared <christrosetolife@gmail.com>
2025-08-30 16:04:49 -0400

    function_auto_path public\src\js_declaration_to_block_body.mjs

--- public/src/js_declaration_to_block_body.mjs
+++ public/src/js_declaration_to_block_body.mjs
@@ -1,5 +1,5 @@
 export function js_declaration_to_block_body(declaration) {
-  let { body } = declaration;
-  let { body: body_block } = body;
+  let { body: block } = declaration;
+  let { body: body_block } = block;
   return body_block;
 }
