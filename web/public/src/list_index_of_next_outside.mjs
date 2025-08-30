b013d298405e9796562439e20883d5fe4566870f
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 21:19:54 -0400

    function_auto_path public\src\list_index_of_next_outside.mjs

--- web/public/src/list_index_of_next_outside.mjs
+++ web/public/src/list_index_of_next_outside.mjs
@@ -1,8 +1,8 @@
 import { marker } from "./marker.mjs";
-import { list_index_of_delta } from "./list_index_of_delta.mjs";
+import { list_index_of_delta_outside } from "./list_index_of_delta_outside.mjs";
 export function list_index_of_next_outside(list, item) {
   marker("1");
   const delta = 1;
-  let index_next = list_index_of_delta(list, item, delta);
+  let index_next = list_index_of_delta_outside(list, item, delta);
   return index_next;
 }
