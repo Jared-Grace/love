40152808283b8017a6e5ff05a8b75217db42309d
Grace-Jared <christrosetolife@gmail.com>
2025-08-29 20:50:52 -0400

    function_auto_path public\src\js_identifier_not_is.mjs

--- web/public/src/js_identifier_not_is.mjs
+++ web/public/src/js_identifier_not_is.mjs
@@ -1,5 +1,5 @@
 import { js_node_type_not_is } from "./js_node_type_not_is.mjs";
 export function js_identifier_not_is(id) {
-  let nti3 = js_node_type_not_is(id, "Identifier");
-  return nti3;
+  let jin = js_node_type_not_is(id, "Identifier");
+  return jin;
 }
