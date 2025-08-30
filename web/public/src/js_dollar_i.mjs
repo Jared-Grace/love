ae565072521abd3857bb09229c14be7045f0401a
Grace-Jared <christrosetolife@gmail.com>
2025-08-30 16:03:15 -0400

    od I

--- web/public/src/js_dollar_i.mjs
+++ web/public/src/js_dollar_i.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { js_keyword_false } from "./js_keyword_false.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { js_code_braces_empty } from "./js_code_braces_empty.mjs";
@@ -5,6 +6,7 @@ import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
 import { js_keyword_if } from "./js_keyword_if.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
 export function js_dollar_i({ stack1 }) {
+  marker("1");
   let inside = js_keyword_false();
   let from = js_parse_statement(
     js_keyword_if() +
