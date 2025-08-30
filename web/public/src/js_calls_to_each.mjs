c7c9e485963457aa4046602ba67dd46c2a93cfe4
Grace-Jared <christrosetolife@gmail.com>
2025-08-30 16:20:55 -0400

    function_auto_path public\src\js_calls_to_each.mjs

--- web/public/src/js_calls_to_each.mjs
+++ web/public/src/js_calls_to_each.mjs
@@ -41,7 +41,8 @@ export function js_calls_to_each(ast) {
     } else {
       call2 = expression2;
     }
-    let eq = equal_by(a2, b2, function lambda2(item) {});
+    function lambda2(item) {}
+    let eq = equal_by(a2, b2, lambda2);
     log({
       call,
       call2,
