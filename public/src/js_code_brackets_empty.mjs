15c6e2643dc8979c7fb01fa98795f0fe9db938cd
Grace-Jared <christrosetolife@gmail.com>
2025-08-30 23:12:06 -0400

    function_auto_path public\src\js_code_brackets_empty.mjs

--- public/src/js_code_brackets_empty.mjs
+++ public/src/js_code_brackets_empty.mjs
@@ -2,6 +2,6 @@ import { js_code_wrap_brackets } from "./js_code_wrap_brackets.mjs";
 import { marker } from "./marker.mjs";
 export function js_code_brackets_empty(inside) {
   marker("1");
-  let v = js_code_wrap_brackets("");
-  return v;
+  let code = js_code_wrap_brackets("");
+  return code;
 }
