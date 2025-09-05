8ee49a62c6462f7656b05afb8f919ffadedb6ccd
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 14:20:35 -0400

    cs list_join_newline 2

--- web/public/src/list_join_newline_2.mjs
+++ web/public/src/list_join_newline_2.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_join } from "./list_join.mjs";
 import { newline } from "./newline.mjs";
 export function list_join_newline_2(waited2) {
+  marker("1");
   let separator = newline();
   let code = list_join(waited2, separator);
   return code;
