f6530e496cb821797446a49c84f2b2eb8e694804
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 14:20:48 -0400

    function_auto_path public\src\list_join_newline_2.mjs

--- web/public/src/list_join_newline_2.mjs
+++ web/public/src/list_join_newline_2.mjs
@@ -1,9 +1,9 @@
 import { marker } from "./marker.mjs";
 import { list_join } from "./list_join.mjs";
 import { newline } from "./newline.mjs";
-export function list_join_newline_2(waited2) {
+export function list_join_newline_2(list) {
   marker("1");
   let separator = newline();
-  let code = list_join(waited2, separator + separator);
+  let code = list_join(list, separator + separator);
   return code;
 }
