b5f0dbf44d05d2d62e3b5ebb28e08805743df23e
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 16:25:03 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -36,7 +36,9 @@ export function app_reply_main() {
       let joined = list_join_newline_2(copied);
       await clipboard_copy(joined);
       html_clear(body);
-      function lambda2(item) {}
+      function lambda2(item) {
+        let component2 = html_p(root2);
+      }
       each(copied, lambda2);
       html_text_set(preview, joined);
       html_display_none(component);
