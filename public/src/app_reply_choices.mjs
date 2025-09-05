6c0287cda108e5c36281b1b78e4f4d75931f5df7
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 16:29:32 -0400

    function_auto_path public\src\app_reply_choices.mjs

--- public/src/app_reply_choices.mjs
+++ public/src/app_reply_choices.mjs
@@ -84,8 +84,7 @@ export function app_reply_choices() {
     },
     {
       text: emoji_pray() + " Preach",
-      response: 
-        emoji_pray() +"I cannot preach at this time",
+      response: emoji_pray() + "I cannot preach at this time",
     },
     {
       text: emoji_pray() + " Provide",
