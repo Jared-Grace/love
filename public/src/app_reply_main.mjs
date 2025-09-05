0fd6ed9df3a548af33622000a9a1b9f3768b7933
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 13:58:48 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -1,3 +1,4 @@
+import { emoji_smile } from "./emoji_smile.mjs";
 import { emoji_wave } from "./emoji_wave.mjs";
 import { emoji_cross } from "./emoji_cross.mjs";
 import { marker } from "./marker.mjs";
@@ -8,7 +9,7 @@ export function app_reply_main() {
       text: "greetings",
       response:
         emoji_wave() +
-        "😁" +
+        emoji_smile() +
         " Greetings in the name of our LORD Jesus Christ " +
         emoji_cross(),
     },
