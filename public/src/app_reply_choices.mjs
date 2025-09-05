c66f8136ad40e9a507a66f23f2c8d9cc3ee5433a
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 17:01:57 -0400

    function_auto_path public\src\app_reply_choices.mjs

--- public/src/app_reply_choices.mjs
+++ public/src/app_reply_choices.mjs
@@ -22,7 +22,7 @@ import { emoji_fire } from "./emoji_fire.mjs";
 import { prayer_start } from "./prayer_start.mjs";
 import { prayer_end } from "./prayer_end.mjs";
 import { emoji_pray } from "./emoji_pray.mjs";
-export function app_reply_choices() {
+export function app_reply_choices() {$a
   let v2 = app_reply_pray("Provide", "provide what is needed");
   let v = [
     {
