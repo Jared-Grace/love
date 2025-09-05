91624e83964c72cab533e25ec531acfe534360e0
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 13:57:05 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -1,10 +1,12 @@
+import { emoji_cross } from "./emoji_cross.mjs";
 import { marker } from "./marker.mjs";
 export function app_reply_main() {
   marker("1");
   let choices = [
     {
       text: "greetings",
-      response: "Greetings in the name of our LORD Jesus Christ " + "✝️",
+      response:
+        "Greetings in the name of our LORD Jesus Christ " + emoji_cross(),
     },
   ];
 }
