957d63559215bbe36cbc5f0aef85d3bee48d9956
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 14:01:29 -0400

    function_auto_path public\src\clipboard_copy.mjs

--- public/src/clipboard_copy.mjs
+++ public/src/clipboard_copy.mjs
@@ -1,6 +1,6 @@
 import { browser_is } from "./browser_is.mjs";
 export async function clipboard_copy(text) {
-  let v = browser_is();
+  let b = browser_is();
   const clipboard = await import("clipboardy");
   await clipboard.default.write(text);
 }
