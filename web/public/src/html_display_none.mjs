24293258286bd4640ac634320d7e004f3a6fdefa
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 15:40:28 -0400

    function_auto_path public\src\html_display_none.mjs

--- web/public/src/html_display_none.mjs
+++ web/public/src/html_display_none.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 import { html_style_set } from "./html_style_set.mjs";
-export function html_display_none(html, value) {
+export function html_display_none(element) {
   marker("1");
-  html_style_set(html, "font-size", value);
+  html_style_set(element, "display", "none");
 }
