2c280e4ee4a4c933e7520f0d3c04f3ca85212950
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 17:31:45 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -36,7 +36,6 @@ export async function app_reply_main() {
     each(buttons, html_display_block);
   }
   let component3 = html_button(root, "Reset", lambda4);
-  let component2 = html_button(root, "Copy", copy);
   marker("1");
   let choices = app_reply_choices();
   function lambda(choice) {
