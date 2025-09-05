19f24f5eb4defe88ff73305c1e60135d4a3f7f0a
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 14:28:12 -0400

    function_auto_path public\src\html_remove.mjs

--- public/src/html_remove.mjs
+++ public/src/html_remove.mjs
@@ -1,7 +1,7 @@
 import { marker } from "./marker.mjs";
 import { html_component_element_get } from "./html_component_element_get.mjs";
-export function html_remove(b) {
+export function html_remove(component) {
   marker("1");
-  let b_element = html_component_element_get(b);
-  b_element.remove();
+  let element = html_component_element_get(component);
+  element.remove();
 }
