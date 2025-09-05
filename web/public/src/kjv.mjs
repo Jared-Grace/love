ae49526bbf62a1588951961fa7d42fdcee06748e
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 17:19:49 -0400

    function_auto_path public\src\kjv.mjs

--- web/public/src/kjv.mjs
+++ web/public/src/kjv.mjs
@@ -1,5 +1,5 @@
 export function kjv() {
-  return {
+  let v = {
     "Genesis 1:1": "In the beginning God created the heaven and the earth.",
     "Genesis 1:2":
       "And the earth was without form, and void; and darkness [was] upon the face of the deep. And the Spirit of God moved upon the face of the waters.",
@@ -60781,4 +60781,5 @@ export function kjv() {
     "Revelation 22:21":
       "The grace of our Lord Jesus Christ [be] with you all. Amen.",
   };
+  return v;
 }
