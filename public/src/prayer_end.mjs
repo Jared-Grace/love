ee281413667531d899186ef3a10b930011b3a0f0
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 14:48:43 -0400

    r prayer_ending prayer_end

--- public/src/prayer_end.mjs
+++ public/src/prayer_end.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { emoji_pray } from "./emoji_pray.mjs";
 export function prayer_end() {
+  marker("1");
   let v = " Amen! " + emoji_pray();
   return v;
 }
