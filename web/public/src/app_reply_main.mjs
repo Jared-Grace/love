33422a92ade665e0f075245368afb0e0efc2598f
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 14:46:10 -0400

    function_auto_path public\src\app_reply_main.mjs

--- web/public/src/app_reply_main.mjs
+++ web/public/src/app_reply_main.mjs
@@ -70,11 +70,11 @@ export function app_reply_main() {
       response:
         emoji_pray() +
         " May God bless you, your family, your church and your country exceedingly abundantly more than anyone can ask or think " +
-        "in the name of the Father, and of the Son, and of the Holy Spirit" +
-        " " +
-        emoji_trinity() +
-        " Amen! " +
-        emoji_pray(),
+        ("in the name of the Father, and of the Son, and of the Holy Spirit" +
+          " " +
+          emoji_trinity() +
+          " Amen! " +
+          emoji_pray()),
     },
     {
       text: emoji_fire() + " Glory",
