5b928e902f1c44089c1a9b24ab2a4efe418d259f
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 15:40:10 -0400

    c html_style_font_size html_display_none

--- public/src/html_display_none.mjs
+++ public/src/html_display_none.mjs
@@ -1,4 +1,6 @@
+import { marker } from "./marker.mjs";
 import { html_style_set } from "./html_style_set.mjs";
 export function html_display_none(html, value) {
+  marker("1");
   html_style_set(html, "font-size", value);
 }
