bc11d6db201732d67a8b5175398645310e938359
Grace-Jared <christrosetolife@gmail.com>
2025-09-05 16:23:40 -0400

    function_auto_path public\src\html_clear.mjs

--- public/src/html_clear.mjs
+++ public/src/html_clear.mjs
@@ -1,4 +1,4 @@
 import { html_text_set } from "./html_text_set.mjs";
-export function html_clear(body) {
-  html_text_set(body, "");
+export function html_clear(element) {
+  html_text_set(element, "");
 }
