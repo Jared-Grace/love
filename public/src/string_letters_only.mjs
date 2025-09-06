c0cad9fd035da623efd7c35d51af0fd5d5c63ed8
Grace-Jared <christrosetolife@gmail.com>
2025-09-06 19:06:35 -0400

    function_auto_path public\src\string_letters_only.mjs

--- public/src/string_letters_only.mjs
+++ public/src/string_letters_only.mjs
@@ -1,4 +1,6 @@
 import { marker } from "./marker.mjs";
-export function string_letters_only() {
+export function string_letters_only(str) {
   marker("1");
+  let v = str.replace(/[^a-zA-Z]/g, "");
+  return v;
 }
