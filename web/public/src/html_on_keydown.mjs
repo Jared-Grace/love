4e5ea1565b96b006ccac450a7bfe556fe37ce8d9
Grace-Jared <christrosetolife@gmail.com>
2025-09-06 18:55:34 -0400

    c html_on_click html_on_keydown

--- web/public/src/html_on_keydown.mjs
+++ web/public/src/html_on_keydown.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { html_on } from "./html_on.mjs";
 export function html_on_keydown(component, lambda) {
+  marker("1");
   const name_event = "click";
   html_on(component, name_event, lambda);
 }
