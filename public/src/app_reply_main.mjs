f0e42c7874edfe0dcfe7f84fef503a17f48ea2a8
Grace-Jared <christrosetolife@gmail.com>
2025-09-06 19:08:50 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -34,7 +34,9 @@ export async function app_reply_main() {
   let preview = null;
   let chosens = [];
   let typed = "";
-  function lambda6() {}
+  function lambda6(event) {
+    let key = object_property_get(event, "key");
+  }
   html_on_keydown(root, lambda6);
   function lambda4() {
     reference = list_random_item(properties);
