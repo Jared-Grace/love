1dc8dc5b239fe037174d8e22bac2455ebddcee45
Grace-Jared <christrosetolife@gmail.com>
2025-09-06 18:57:45 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -28,6 +28,7 @@ export async function app_reply_main() {
   let copied = [];
   let buttons = null;
   let preview = null;
+  let chosens = [];
   function lambda4() {
     reference = list_random_item(properties);
     verse = object_property_get(bible, reference);
