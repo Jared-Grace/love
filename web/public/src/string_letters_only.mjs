b936e48768e2443e9e33f8041e6b3827dc6d193c
Grace-Jared <christrosetolife@gmail.com>
2025-09-06 19:07:16 -0400

    function_auto_path public\src\string_letters_only.mjs

--- web/public/src/string_letters_only.mjs
+++ web/public/src/string_letters_only.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 export function string_letters_only(str) {
   marker("1");
-  let v = str.replace(/[^a-zA-Z]/g, "");
-  return v;
+  let letters = str.replace(/[^a-zA-Z]/g, "");
+  return letters;
 }
