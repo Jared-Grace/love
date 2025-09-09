b2c605f9268c321dab6dcd77cc7b5d3465531648
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:57:27 -0400

    function_auto_path public\src\firebase_storage_download.mjs

--- web/public/src/firebase_storage_download.mjs
+++ web/public/src/firebase_storage_download.mjs
@@ -1,7 +1,9 @@
+import { firebase_path_fix } from "./firebase_path_fix.mjs";
 import { http_json } from "./http_json.mjs";
 import { firebase_storage_url } from "./firebase_storage_url.mjs";
-export async function firebase_storage_download(destination_version) {
-  let url = firebase_storage_url(destination_version);
+export async function firebase_storage_download(destination) {
+  destination = firebase_path_fix(destination);
+  let url = firebase_storage_url(destination);
   let parsed = await http_json(url);
   return parsed;
 }
