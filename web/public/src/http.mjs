3dbc032dddeb97322ab202f586de8600c8a24da5
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:06:24 -0400

    function_auto_path public\src\http.mjs

--- web/public/src/http.mjs
+++ web/public/src/http.mjs
@@ -3,12 +3,12 @@ import { error } from "./error.mjs";
 import { not } from "./not.mjs";
 export async function http(url) {
   let b = browser_is();
-  if (false) {
+  if (b) {
+    const response = await fetch(url);
+    if (not(response.ok)) {
+      error("Failed to fetch file");
+    }
+    const text = await response.text();
+    return text;
   }
-  const response = await fetch(url);
-  if (not(response.ok)) {
-    error("Failed to fetch file");
-  }
-  const text = await response.text();
-  return text;
 }
