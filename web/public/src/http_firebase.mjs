fbb94c2ef13f9e134aa54164c7bfb744a65e141f
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:28:09 -0400

    function_auto_path public\src\http_firebase.mjs

--- web/public/src/http_firebase.mjs
+++ web/public/src/http_firebase.mjs
@@ -1,9 +1,11 @@
+import { path_join } from "./path_join.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { http } from "./http.mjs";
 import { marker } from "./marker.mjs";
 export async function http_firebase(url) {
   marker("1");
   let text = await http(url);
+  let joined = path_join(list_of_segments);
   await firebase_upload_object(version, destination_version);
   return text;
 }
