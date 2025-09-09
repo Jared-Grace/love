f6edf084ca1b758ca6e8b22583b7790363be04ac
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:44:55 -0400

    function_auto_path public\src\firebase_storage_exists.mjs

--- public/src/firebase_storage_exists.mjs
+++ public/src/firebase_storage_exists.mjs
@@ -1,6 +1,14 @@
+import { error } from "./error.mjs";
 import { firebase_bucket } from "./firebase_bucket.mjs";
-import { marker } from "./marker.mjs";
-export async function firebase_storage_exists() {
+export async function firebase_storage_exists(path) {
   const bucket = await firebase_bucket();
-  marker("1");
+  const file = bucket.file(path);
+  try {
+    const [exists] = await file.exists();
+    return exists;
+  } catch (err) {
+    console.error("Error checking file:", err);
+    let v = false;
+    return v;
+  }
 }
