8a186cfb6b9b08f1bcc36be11e6a19dc430a3b69
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:52:08 -0400

    function_auto_path public\src\file_path_safe_to.mjs

--- public/src/file_path_safe_to.mjs
+++ public/src/file_path_safe_to.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 export function file_path_safe_to(fp) {
   marker("1");
-  let safe = urlToFirebaseKey(fp);
+  let safe = encodeURIComponent(fp);
   return safe;
 }
