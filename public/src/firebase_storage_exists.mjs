7327714a2602e40273e2c436176c51000dd27e9d
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:56:25 -0400

    function_auto_path public\src\firebase_storage_exists.mjs

--- public/src/firebase_storage_exists.mjs
+++ public/src/firebase_storage_exists.mjs
@@ -1,6 +1,8 @@
+import { string_replace } from "./string_replace.mjs";
 import { error } from "./error.mjs";
 import { firebase_bucket } from "./firebase_bucket.mjs";
 export async function firebase_storage_exists(path) {
+  let replaced = string_replace(s, from, to);
   const bucket = await firebase_bucket();
   const file = bucket.file(path);
   let exists = null;
