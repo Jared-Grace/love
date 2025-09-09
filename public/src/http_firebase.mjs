890968327fe03e0906c6ef7af42cb1f9fc4668c6
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:45:08 -0400

    function_auto_path public\src\http_firebase.mjs

--- public/src/http_firebase.mjs
+++ public/src/http_firebase.mjs
@@ -1,9 +1,11 @@
+import { firebase_storage_exists } from "./firebase_storage_exists.mjs";
 import { file_path_safe_to } from "./file_path_safe_to.mjs";
 import { path_join } from "./path_join.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { http } from "./http.mjs";
 import { marker } from "./marker.mjs";
 export async function http_firebase(url) {
+  await firebase_storage_exists(path);
   if (false) {
   }
   marker("1");
