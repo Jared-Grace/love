89c56fa95034144f2da74fbac7edb8411c0d7157
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:41:20 -0400

    function_auto_path public\src\firebase_storage_exists.mjs

--- web/public/src/firebase_storage_exists.mjs
+++ web/public/src/firebase_storage_exists.mjs
@@ -1,4 +1,6 @@
+import { firebase_bucket } from "./firebase_bucket.mjs";
 import { marker } from "./marker.mjs";
 export function firebase_storage_exists() {
+  const bucket = firebase_bucket();
   marker("1");
 }
