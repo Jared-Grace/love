13152e0a7e1e122679835939843ceddc787d8dda
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 18:27:18 -0400

    function_auto_path public\src\http_firebase.mjs

--- public/src/http_firebase.mjs
+++ public/src/http_firebase.mjs
@@ -1,7 +1,9 @@
+import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { http } from "./http.mjs";
 import { marker } from "./marker.mjs";
 export async function http_firebase(url) {
   marker("1");
   let text = await http(url);
+  await firebase_upload_object(version, destination_version);
   return text;
 }
