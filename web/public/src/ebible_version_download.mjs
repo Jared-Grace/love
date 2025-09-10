a6d9245e7b05aedd69a5542ad2739bc53f3df16d
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:31:13 -0400

    function_auto_path public\src\ebible_version_download.mjs

--- web/public/src/ebible_version_download.mjs
+++ web/public/src/ebible_version_download.mjs
@@ -1,5 +1,5 @@
 import { marker } from "./marker.mjs";
 export function ebible_version_download(bible_folder) {
   marker("1");
-  ("https://ebible.org/Scriptures/engbsb_html.zip");
+  "https://ebible.org/Scriptures/" + bible_folder + "_html.zip";
 }
