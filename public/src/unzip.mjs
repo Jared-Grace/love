b84bac4171fd732d97dcba4d2676117478bafb93
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:41:34 -0400

    function_auto_path public\src\unzip.mjs

--- public/src/unzip.mjs
+++ public/src/unzip.mjs
@@ -1,8 +1,7 @@
 import { import_install } from "./import_install.mjs";
 import { log } from "./log.mjs";
 export async function unzip(file_path, buffer) {
-  await import_install(name);
-  const AdmZip = (await import("adm-zip")).default;
+  const AdmZip = (await import_install("adm-zip")).default;
   const zip = new AdmZip(buffer);
   zip.extractAllTo(file_path, true);
   log(`Extracted zip to folder: ${file_path}`);
