497dea947f5ad2523af62c91d50280ef39bb1894
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:20:00 -0400

    function_auto_path public\src\file_write_json.mjs

--- web/public/src/file_write_json.mjs
+++ web/public/src/file_write_json.mjs
@@ -1,8 +1,8 @@
+import { file_write } from "./file_write.mjs";
 import { marker } from "./marker.mjs";
-import { file_overwrite } from "./file_overwrite.mjs";
 import { json_format_to } from "./json_format_to.mjs";
 export async function file_write_json(file_path, object) {
   marker("1");
   let json = json_format_to(object);
-  await file_overwrite(file_path, json);
+  await file_write(file_path, json);
 }
