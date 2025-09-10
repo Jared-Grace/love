368ca2243cb094ca680ce41899afd347907c0254
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:35:06 -0400

    function_auto_path public\src\buffer_string_to.mjs

--- web/public/src/buffer_string_to.mjs
+++ web/public/src/buffer_string_to.mjs
@@ -1,17 +1,17 @@
 import { browser_is } from "./browser_is.mjs";
 export function buffer_string_to(buffer) {
   let b = browser_is();
-  if (false) {
+  if (b) {
+    if (buffer instanceof ArrayBuffer) {
+      let v2 = new TextDecoder("utf-8").decode(new Uint8Array(buffer));
+      return v2;
+    }
+    if (buffer instanceof Uint8Array) {
+      let v3 = new TextDecoder("utf-8").decode(buffer);
+      return v3;
+    }
+    throw new Error("Unsupported buffer type");
   }
-  if (buffer instanceof ArrayBuffer) {
-    let v2 = new TextDecoder("utf-8").decode(new Uint8Array(buffer));
-    return v2;
-  }
-  if (buffer instanceof Uint8Array) {
-    let v3 = new TextDecoder("utf-8").decode(buffer);
-    return v3;
-  }
-  throw new Error("Unsupported buffer type");
   let v = buffer.toString("utf8");
   return v;
 }
