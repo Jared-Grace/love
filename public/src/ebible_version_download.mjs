5fadbc8111d319d9c6c45fbb5514754b1435cafe
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:31:57 -0400

    function_auto_path public\src\ebible_version_download.mjs

--- public/src/ebible_version_download.mjs
+++ public/src/ebible_version_download.mjs
@@ -4,4 +4,5 @@ export async function ebible_version_download(bible_folder) {
   marker("1");
   let url = "https://ebible.org/Scriptures/" + bible_folder + "_html.zip";
   let result = await http_local(url);
+  return result;
 }
