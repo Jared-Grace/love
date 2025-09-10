ffbb7b8dbe7da1559245f33ddf53612820946b6a
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:40:46 -0400

    function_auto_path public\src\ebible_version_download.mjs

--- web/public/src/ebible_version_download.mjs
+++ web/public/src/ebible_version_download.mjs
@@ -1,3 +1,4 @@
+import { local_function_path } from "./local_function_path.mjs";
 import { unzip } from "./unzip.mjs";
 import { http_local } from "./http_local.mjs";
 import { marker } from "./marker.mjs";
@@ -5,6 +6,7 @@ export async function ebible_version_download(bible_folder) {
   marker("1");
   let url = "https://ebible.org/Scriptures/" + bible_folder + "_html.zip";
   let result = await http_local(url);
-  await unzip(file_name, buffer);
+  let file_path = local_function_path(ebible_version_download, bible_folder);
+  await unzip(file_path, buffer);
   return result;
 }
