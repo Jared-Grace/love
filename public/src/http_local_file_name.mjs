d452fe38db16a68e93bafe3af85130e930bf09f0
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:16:50 -0400

    function_auto_path public\src\http_local_file_name.mjs

--- public/src/http_local_file_name.mjs
+++ public/src/http_local_file_name.mjs
@@ -1,5 +1,5 @@
 import { http_local } from "./http_local.mjs";
 import { path_join } from "./path_join.mjs";
-export function http_local_file_name() {
+export function http_local_file_name(url) {
   let joined = path_join(["D:\\user\\storage\\function", http_local.name]);
 }
