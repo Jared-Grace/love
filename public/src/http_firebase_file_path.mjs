1480fb535199761aea61dc834068fc8401b65d47
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:18:04 -0400

    function_auto_path public\src\http_firebase_file_path.mjs

--- public/src/http_firebase_file_path.mjs
+++ public/src/http_firebase_file_path.mjs
@@ -1,12 +1,10 @@
+import { http_firebase_file_name } from "./http_firebase_file_name.mjs";
 import { marker } from "./marker.mjs";
 import { firebase_path_fix } from "./firebase_path_fix.mjs";
 import { path_join } from "./path_join.mjs";
-import { file_name_json } from "./file_name_json.mjs";
-import { string_base64_to } from "./string_base64_to.mjs";
 export function http_firebase_file_path(url) {
   marker("1");
-  let safe = string_base64_to(url);
-  let file_name = file_name_json(safe);
+  let file_name = http_firebase_file_name(url);
   let joined = path_join(["http", file_name]);
   joined = firebase_path_fix(joined);
   return joined;
