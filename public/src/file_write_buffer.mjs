7ebefa60a0a00966a94123811280cb7d1946cacb
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 16:10:38 -0400

    function_auto_path public\src\file_write_buffer.mjs

--- public/src/file_write_buffer.mjs
+++ public/src/file_write_buffer.mjs
@@ -1,8 +1,8 @@
+import { file_overwrite_buffer } from "./file_overwrite_buffer.mjs";
 import { marker } from "./marker.mjs";
-import { file_overwrite } from "./file_overwrite.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 export async function file_write_buffer(f_path, contents) {
   marker("1");
   await assert_file_exists_not(f_path);
-  await file_overwrite(f_path, contents);
+  await file_overwrite_buffer(f_path, contents);
 }
