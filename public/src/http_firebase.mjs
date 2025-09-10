905c3e5a5c94debf4ebc26984a021e952f0d6729
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:04:47 -0400

    function_auto_path public\src\http_firebase.mjs

--- public/src/http_firebase.mjs
+++ public/src/http_firebase.mjs
@@ -1,20 +1,15 @@
 import { http_firebase_file_name } from "./http_firebase_file_name.mjs";
-import { log } from "./log.mjs";
 import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 import { firebase_storage_exists } from "./firebase_storage_exists.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { http } from "./http.mjs";
-import { marker } from "./marker.mjs";
 export async function http_firebase(url) {
   let joined = http_firebase_file_name(url);
   let exists = await firebase_storage_exists(url);
   if (exists) {
-    log("not downloading");
     let result = await firebase_storage_download_property(joined, "text");
     return result;
   }
-  log("downloading");
-  marker("1");
   let text = await http(url);
   await firebase_upload_object(
     {
