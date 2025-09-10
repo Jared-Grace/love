f8267fcd41470f3dd27aca68dddce999dbb093b6
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 16:06:56 -0400

    function_auto_path public\src\file_overwrite_buffer.mjs

--- public/src/file_overwrite_buffer.mjs
+++ public/src/file_overwrite_buffer.mjs
@@ -4,5 +4,5 @@ export async function file_overwrite_buffer(file_path, contents) {
   marker("1");
   let fs = await import("fs");
   await file_parent_exists_ensure(file_path);
-  await fs.promises.writeFile(file_path, contents, "utf-8");
+  await fs.promises.writeFile(file_path, contents);
 }
