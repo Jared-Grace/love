07a0989d7fb709b0e551f80390e782ce58999d3a
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:33:26 -0400

    function_auto_path public\src\http_json.mjs

--- web/public/src/http_json.mjs
+++ web/public/src/http_json.mjs
@@ -1,9 +1,11 @@
+import { buffer_string_to } from "./buffer_string_to.mjs";
 import { json_from } from "./json_from.mjs";
 import { http } from "./http.mjs";
 import { marker } from "./marker.mjs";
 export async function http_json(url) {
   marker("1");
   let buffer = await http(url);
+  buffer_string_to(buffer);
   let parsed = json_from(text);
   return parsed;
 }
