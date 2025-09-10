cf9ae35312a57239661a025bad072cb6a5a873a2
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:35:37 -0400

    function_auto_path public\src\buffer_string_to.mjs

--- public/src/buffer_string_to.mjs
+++ public/src/buffer_string_to.mjs
@@ -3,8 +3,9 @@ export function buffer_string_to(buffer) {
   let b = browser_is();
   if (b) {
     if (buffer instanceof ArrayBuffer) {
-      let result = new TextDecoder("utf-8").decode(new Uint8Array(buffer));
-      return result;
+      let s = null;
+      s = new TextDecoder("utf-8").decode(new Uint8Array(buffer));
+      return s;
     }
     if (buffer instanceof Uint8Array) {
       let v3 = new TextDecoder("utf-8").decode(buffer);
