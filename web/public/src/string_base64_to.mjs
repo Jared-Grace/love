c5c4e54a79a616793949e9942e19bbc9a7d5f6c2
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 20:01:16 -0400

    function_auto_path public\src\string_base64_to.mjs

--- web/public/src/string_base64_to.mjs
+++ web/public/src/string_base64_to.mjs
@@ -1,8 +1,10 @@
 import { browser_is } from "./browser_is.mjs";
 export function string_base64_to() {
   let b = browser_is();
-  if (false) {
+  if (b) {
+    const b64 = btoa(str);
+    return b64;
   }
-  const b64 = btoa(str);
   const b64node = Buffer.from(str, "utf-8").toString("base64");
+  return b64node;
 }
