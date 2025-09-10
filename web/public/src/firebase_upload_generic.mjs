a5e8e2237718850dad8d453a27af9cedfec76f5a
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:50:43 -0400

    function_auto_path public\src\firebase_upload_string.mjs

---
+++ web/public/src/firebase_upload_generic.mjs
@@ -0,0 +1,21 @@
+import { log } from "./log.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { firebase_bucket } from "./firebase_bucket.mjs";
+import { firebase_path_fix } from "./firebase_path_fix.mjs";
+export async function firebase_upload_generic(destination, settings, buffer) {
+  destination = firebase_path_fix(destination);
+  const bucket = await firebase_bucket();
+  const file = bucket.file(destination);
+  let merged = object_merge(
+    {
+      metadata: {
+        cacheControl: "no-cache",
+      },
+    },
+    settings,
+  );
+  await file.save(buffer, merged);
+  console.log(`Uploaded data to ${destination}`);
+  const url = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
+  console.log("Accessible at:", url);
+}
