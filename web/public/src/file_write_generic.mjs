d8d3688233c2c9f32aecbcefe03e197b123f7991
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 16:11:15 -0400

    function_auto_path public\src\file_write.mjs

---
+++ web/public/src/file_write_generic.mjs
@@ -0,0 +1,5 @@
+import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
+export async function file_write_generic(f_path, overwrite, contents) {
+  await assert_file_exists_not(f_path);
+  await overwrite(f_path, contents);
+}
