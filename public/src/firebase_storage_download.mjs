cde83385743e3847710bd5ee59dd4c25da2e8717
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 23:24:19 -0400

    function_auto_path public\src\firebase_storage_download.mjs

--- public/src/firebase_storage_download.mjs
+++ public/src/firebase_storage_download.mjs
@@ -1,10 +1,10 @@
+import { firebase_storage_download_path } from "./firebase_storage_download_path.mjs";
 import { firebase_bucket } from "./firebase_bucket.mjs";
 import { browser_is } from "./browser_is.mjs";
 import { log } from "./log.mjs";
 import { firebase_path_fix } from "./firebase_path_fix.mjs";
 import { http_json } from "./http_json.mjs";
 import { firebase_storage_url } from "./firebase_storage_url.mjs";
-import { path_join } from "./path_join.mjs";
 export async function firebase_storage_download(destination) {
   let b = browser_is();
   if (b) {
@@ -18,6 +18,6 @@ export async function firebase_storage_download(destination) {
   }
   const bucket = await firebase_bucket();
   await bucket.file(destination).download({
-    destination: path_join(["D:\\user\\storage", destination]),
+    destination: firebase_storage_download_path(destination),
   });
 }
