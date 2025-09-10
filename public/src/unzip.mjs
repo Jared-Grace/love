dc1c39d046500d39bd288a07dd6c231b2299f181
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:40:03 -0400

    function_auto_path public\src\unzip.mjs

--- public/src/unzip.mjs
+++ public/src/unzip.mjs
@@ -2,5 +2,5 @@ import { log } from "./log.mjs";
 export async function unzip(file_path, buffer) {
   const zip = new AdmZip(buffer);
   zip.extractAllTo(file_path, true);
-  console.log(`Extracted zip to folder: ${file_path}`);
+  log(`Extracted zip to folder: ${file_path}`);
 }
