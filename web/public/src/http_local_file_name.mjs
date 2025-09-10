d6d17c248143be0653cafa7d3490cb5ff0696119
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:37:21 -0400

    function_auto_path public\src\http_local_file_name.mjs

--- web/public/src/http_local_file_name.mjs
+++ web/public/src/http_local_file_name.mjs
@@ -1,9 +1,9 @@
+import { local_function_path } from "./local_function_path.mjs";
 import { http_firebase_file_name } from "./http_firebase_file_name.mjs";
 import { http_local } from "./http_local.mjs";
-import { path_join } from "./path_join.mjs";
 export function http_local_file_name(url) {
   let file_name = http_firebase_file_name(url);
   let fn = http_local;
-  let joined = path_join(["D:\\user\\storage\\function", fn.name, file_name]);
+  let joined = local_function_path(fn, file_name);
   return joined;
 }
