1546c70976b6cb35ec0c49d6125a3262279670d7
Grace-Jared <christrosetolife@gmail.com>
2025-09-09 20:01:31 -0400

    function_auto_path public\src\http_firebase.mjs

--- public/src/http_firebase.mjs
+++ public/src/http_firebase.mjs
@@ -1,13 +1,13 @@
+import { string_base64_to } from "./string_base64_to.mjs";
 import { log } from "./log.mjs";
 import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 import { firebase_storage_exists } from "./firebase_storage_exists.mjs";
-import { file_path_safe_to } from "./file_path_safe_to.mjs";
 import { path_join } from "./path_join.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { http } from "./http.mjs";
 import { marker } from "./marker.mjs";
 export async function http_firebase(url) {
-  let safe = file_path_safe_to(url);
+  let safe = string_base64_to(url);
   let joined = path_join(["http", safe]);
   let exists = await firebase_storage_exists(joined);
   if (exists) {
