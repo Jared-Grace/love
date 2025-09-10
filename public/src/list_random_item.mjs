382fecf17c20215fd86e11664144861e2c6c4d58
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 17:47:49 -0400

    function_auto_path public\src\list_random_item.mjs

--- public/src/list_random_item.mjs
+++ public/src/list_random_item.mjs
@@ -1,7 +1,8 @@
 import { floor } from "./floor.mjs";
+import { list_size } from "./list_size.mjs";
 import { random } from "./random.mjs";
 export function list_random_item(arr) {
-  const p = random() * arr.length;
+  const p = random() * list_size(arr);
   let r = arr[floor(p)];
   return r;
 }
