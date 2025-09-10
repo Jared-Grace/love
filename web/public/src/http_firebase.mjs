6937d7355f7bd13060eab287e09789e26db6e3a7
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:57:25 -0400

    function_auto_path public\src\http_firebase.mjs

--- web/public/src/http_firebase.mjs
+++ web/public/src/http_firebase.mjs
@@ -1,17 +1,13 @@
+import { firebase_storage_download } from "./firebase_storage_download.mjs";
 import { firebase_upload_buffer } from "./firebase_upload_buffer.mjs";
 import { cache_generic } from "./cache_generic.mjs";
 import { http_firebase_file_path } from "./http_firebase_file_path.mjs";
-import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 import { firebase_storage_exists } from "./firebase_storage_exists.mjs";
 import { http } from "./http.mjs";
 export async function http_firebase(url) {
   let key_get = http_firebase_file_path;
   let cached_exists = firebase_storage_exists;
-  const property_name = "buffer";
-  let cached_get = async function lambda(key) {
-    let result = await firebase_storage_download_property(key, property_name);
-    return result;
-  };
+  let cached_get = firebase_storage_download;
   let value_get = http;
   async function cache_save(key, value) {
     await firebase_upload_buffer(value, key);
