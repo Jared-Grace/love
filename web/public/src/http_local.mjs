7ee92c247f76c401147921ab31c6f07b76f7dd35
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 16:05:31 -0400

    function_auto_path public\src\http_local.mjs

--- web/public/src/http_local.mjs
+++ web/public/src/http_local.mjs
@@ -1,19 +1,15 @@
+import { file_read_buffer } from "./file_read_buffer.mjs";
 import { file_write_json } from "./file_write_json.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { http_local_file_name } from "./http_local_file_name.mjs";
 import { http_firebase } from "./http_firebase.mjs";
 import { marker } from "./marker.mjs";
 import { cache_generic } from "./cache_generic.mjs";
-import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 export async function http_local(url) {
   marker("1");
   let key_get = http_local_file_name;
   let cached_exists = file_exists;
-  const property_name = "text";
-  let cached_get = async function lambda(key) {
-    let result = await firebase_storage_download_property(key, property_name);
-    return result;
-  };
+  let cached_get = file_read_buffer;
   let value_get = http_firebase;
   async function cache_save(key, value) {
     await file_write_json(key, value);
