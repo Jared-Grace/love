ca46dc1ae8c9a84255ba6718a1ef432cbb9eb838
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 17:47:12 -0400

    function_auto_path public\src\list_random_item.mjs

--- public/src/list_random_item.mjs
+++ public/src/list_random_item.mjs
@@ -1,4 +1,5 @@
+import { random } from "./random.mjs";
 export function list_random_item(arr) {
-  let r = arr[Math.floor(Math.random() * arr.length)];
+  let r = arr[Math.floor(random() * arr.length)];
   return r;
 }
