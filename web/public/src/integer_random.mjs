96d6b8279809773453c45139c492c1e6d9161e49
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 17:49:08 -0400

    function_auto_path public\src\integer_random.mjs

--- web/public/src/integer_random.mjs
+++ web/public/src/integer_random.mjs
@@ -1,6 +1,6 @@
 import { random } from "./random.mjs";
 import { floor } from "./floor.mjs";
 export function integer_random(min, max) {
-  let v = floor(random() * (max - min + 1)) + min;
-  return v;
+  let r = floor(random() * (max - min + 1)) + min;
+  return r;
 }
