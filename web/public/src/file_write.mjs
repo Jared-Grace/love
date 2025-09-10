cb6f4df268d4ac5d0d2566005992b8e732d79216
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 16:11:02 -0400

    function_auto_path public\src\file_write.mjs

--- web/public/src/file_write.mjs
+++ web/public/src/file_write.mjs
@@ -1,6 +1,7 @@
 import { file_overwrite } from "./file_overwrite.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 export async function file_write(f_path, contents) {
+  let overwrite = file_overwrite;
   await assert_file_exists_not(f_path);
   await file_overwrite(f_path, contents);
 }
