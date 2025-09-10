d2fdadd2794cb239edc4c1e946b2a3dfcc214ac1
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:49:25 -0400

    function_auto_path public\src\firebase_upload_string.mjs

--- web/public/src/firebase_upload_string.mjs
+++ web/public/src/firebase_upload_string.mjs
@@ -3,14 +3,14 @@ import { firebase_path_fix } from "./firebase_path_fix.mjs";
 import { firebase_bucket } from "./firebase_bucket.mjs";
 import { log } from "./log.mjs";
 export async function firebase_upload_string(content, destination) {
-  destination = firebase_path_fix(destination);
-  const bucket = await firebase_bucket();
-  const file = bucket.file(destination);
   let b = Buffer.from(content);
   const settings = {
     contentType: "text/plain",
     gzip: true,
   };
+  destination = firebase_path_fix(destination);
+  const bucket = await firebase_bucket();
+  const file = bucket.file(destination);
   let merged = object_merge(
     {
       metadata: {
