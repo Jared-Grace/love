94a2d35d93c0632301747b5d6c865d98316dd5c8
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:36:26 -0400

    function_auto_path public\src\unzip.mjs

--- web/public/src/unzip.mjs
+++ web/public/src/unzip.mjs
@@ -1,7 +1,8 @@
+import { file_write_buffer } from "./file_write_buffer.mjs";
 export async function unzip(buffer) {
   const { gunzip } = await import("zlib");
   const { promisify } = await import("util");
   const gunzipAsync = promisify(gunzip);
   const unzipped = await gunzipAsync(buffer);
-  return unzipped;
+  await file_write_buffer(f_path, contents);
 }
