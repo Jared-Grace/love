b65507142dd0c024cfcd0a6aaa642144cacf2bed
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 18:34:27 -0400

    function_auto_path public\src\unzip.mjs

--- public/src/unzip.mjs
+++ public/src/unzip.mjs
@@ -1,4 +1,7 @@
-import { marker } from "./marker.mjs";
-export function unzip() {
-  marker("1");
+export async function unzip(buffer) {
+  const { gunzip } = await import("zlib");
+  const { promisify } = await import("util");
+  const gunzipAsync = promisify(gunzip);
+  const uncompressed = await gunzipAsync(buffer);
+  return uncompressed;
 }
