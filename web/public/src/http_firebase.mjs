f40c74a8da16539f380d31c3bd67665597a34054
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 15:17:21 -0400

    r http_firebase_file_name http_firebase_file_path

--- web/public/src/http_firebase.mjs
+++ web/public/src/http_firebase.mjs
@@ -1,11 +1,11 @@
 import { cache_generic } from "./cache_generic.mjs";
-import { http_firebase_file_name } from "./http_firebase_file_name.mjs";
+import { http_firebase_file_path } from "./http_firebase_file_path.mjs";
 import { firebase_storage_download_property } from "./firebase_storage_download_property.mjs";
 import { firebase_storage_exists } from "./firebase_storage_exists.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { http } from "./http.mjs";
 export async function http_firebase(url) {
-  let key_get = http_firebase_file_name;
+  let key_get = http_firebase_file_path;
   let cached_exists = firebase_storage_exists;
   const property_name = "text";
   let cached_get = async function lambda(key) {
