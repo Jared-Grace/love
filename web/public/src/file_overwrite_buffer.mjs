94c897da2b8fe76399ae1a1b60cdb9709698594a
Grace-Jared <christrosetolife@gmail.com>
2025-09-10 16:06:50 -0400

    cs file_overwrite buffer

--- web/public/src/file_overwrite_buffer.mjs
+++ web/public/src/file_overwrite_buffer.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { file_parent_exists_ensure } from "./file_parent_exists_ensure.mjs";
 export async function file_overwrite_buffer(file_path, contents) {
+  marker("1");
   let fs = await import("fs");
   await file_parent_exists_ensure(file_path);
   await fs.promises.writeFile(file_path, contents, "utf-8");
