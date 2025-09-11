2acb6cdb792b0cbbe7a20352c3f41a571b48086e
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 18:14:59 -0400

    function_auto_path public\src\catch_call.mjs

--- web/public/src/catch_call.mjs
+++ web/public/src/catch_call.mjs
@@ -1,14 +1,10 @@
-import { log } from "./log.mjs";
 import { marker } from "./marker.mjs";
 export function catch_call(reject, lambda) {
   let i = function inner() {
     let result = null;
     try {
-      log(1);
       result = lambda(...arguments);
-      log(3);
     } catch (e) {
-      log(2);
       reject(e);
     }
     marker("1");
