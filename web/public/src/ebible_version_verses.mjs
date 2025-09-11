7d96ed999eadea17602f2e01208f0d5afbb279c3
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:54:22 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- web/public/src/ebible_version_verses.mjs
+++ web/public/src/ebible_version_verses.mjs
@@ -19,6 +19,7 @@ export async function ebible_version_verses(bible_folder) {
   let { d, root } = await html_parse_read(joined);
   let list = html_parse_find_list_to(root, "a");
   let mapped = html_parse_href_text_map(d, list);
-  let filtered = list_filter(list2, function lambda(item) {});
+  function lambda(item) {}
+  let filtered = list_filter(list2, lambda);
   return mapped;
 }
