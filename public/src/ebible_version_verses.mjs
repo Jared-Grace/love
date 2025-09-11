e8239cb0bbda0f27f1c18ef59c4154f668051b77
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 13:49:44 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- public/src/ebible_version_verses.mjs
+++ public/src/ebible_version_verses.mjs
@@ -1,4 +1,6 @@
+import { ebible_version_download } from "./ebible_version_download.mjs";
 import { marker } from "./marker.mjs";
-export function ebible_version_verses(bible_folder) {
+export async function ebible_version_verses(bible_folder) {
+  let file_path = await ebible_version_download(bible_folder2);
   marker("1");
 }
