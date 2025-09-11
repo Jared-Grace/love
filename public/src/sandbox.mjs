53a7e01e0263d90468a0a918b59de49f741a7ec0
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:20:25 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { string_prefix_without } from "./string_prefix_without.mjs";
 import { html_parse_find_a_href_starts_with } from "./html_parse_find_a_href_starts_with.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { http_local_text } from "./http_local_text.mjs";
@@ -9,6 +10,7 @@ export async function sandbox() {
   let { d, root } = await html_parse(text);
   const prefix = "details.php?id=";
   let filtered = html_parse_find_a_href_starts_with(root, d, prefix);
+  let result = string_prefix_without(s, prefix2);
   return filtered;
   marker("1");
   let contents = await ebible_version_verses("engbsb");
