ca8285f3a21e2e25abb99ab8d548c0346c934c31
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:40:34 -0400

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { ebible_version_verses } from "./ebible_version_verses.mjs";
 export async function sandbox() {
+  marker("1");
   let contents = await ebible_version_verses("engbsb");
   return contents;
 }
