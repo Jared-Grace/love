2cfbcf23f217cf15be9db85bc537a6b47807faf5
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:21:44 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,4 @@
-import { list_map } from "./list_map.mjs";
-import { string_prefix_without } from "./string_prefix_without.mjs";
+import { list_map_prefix_without } from "./list_map_prefix_without.mjs";
 import { html_parse_find_a_href_starts_with } from "./html_parse_find_a_href_starts_with.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { http_local_text } from "./http_local_text.mjs";
@@ -11,11 +10,7 @@ export async function sandbox() {
   let { d, root } = await html_parse(text);
   const prefix = "details.php?id=";
   let list = html_parse_find_a_href_starts_with(root, d, prefix);
-  function lambda(item) {
-    let result = string_prefix_without(item, prefix);
-    return result;
-  }
-  let mapped = list_map(list, lambda);
+  let mapped = list_map_prefix_without(list, prefix);
   return mapped;
   marker("1");
   let contents = await ebible_version_verses("engbsb");
