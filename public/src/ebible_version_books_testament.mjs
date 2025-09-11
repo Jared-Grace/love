0776004b00d50cb9a05efed043160b2186216f83
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 14:29:21 -0400

    function_auto_path public\src\ebible_version_verses.mjs

---
+++ public/src/ebible_version_books_testament.mjs
@@ -0,0 +1,29 @@
+import { list_map } from "./list_map.mjs";
+import { html_parse_href } from "./html_parse_href.mjs";
+import { html_parse_text } from "./html_parse_text.mjs";
+import { html_parse_list_to } from "./html_parse_list_to.mjs";
+import { html_parse_find } from "./html_parse_find.mjs";
+import { html_parse } from "./html_parse.mjs";
+import { file_read } from "./file_read.mjs";
+import { path_join } from "./path_join.mjs";
+import { ebible_version_download } from "./ebible_version_download.mjs";
+export async function ebible_version_books_testament(bible_folder, class_old) {
+  let file_path = await ebible_version_download(bible_folder);
+  let joined = path_join([file_path, "index.htm"]);
+  let contents = await file_read(joined);
+  let { d, root } = await html_parse(contents);
+  let bl = html_parse_find(root, ".bookList");
+  let query = html_parse_find(bl, "." + class_old);
+  let list = html_parse_list_to(query);
+  function lambda(item) {
+    let text = html_parse_text(d, item);
+    let href = html_parse_href(d, item);
+    let v = {
+      text,
+      href,
+    };
+    return v;
+  }
+  let mapped = list_map(list, lambda);
+  return mapped;
+}
--- public/src/ebible_version_verses.mjs
+++ public/src/ebible_version_verses.mjs
@@ -1,32 +1,8 @@
-import { html_parse_href } from "./html_parse_href.mjs";
-import { html_parse_text } from "./html_parse_text.mjs";
-import { list_map } from "./list_map.mjs";
-import { html_parse_list_to } from "./html_parse_list_to.mjs";
-import { html_parse_find } from "./html_parse_find.mjs";
-import { html_parse } from "./html_parse.mjs";
-import { path_join } from "./path_join.mjs";
-import { file_read } from "./file_read.mjs";
-import { ebible_version_download } from "./ebible_version_download.mjs";
+import { ebible_version_books_testament } from "./ebible_version_books_testament.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_version_verses(bible_folder) {
   const class_old = "oo";
-  let file_path = await ebible_version_download(bible_folder);
-  let joined = path_join([file_path, "index.htm"]);
-  let contents = await file_read(joined);
-  let { d, root } = await html_parse(contents);
-  let bl = html_parse_find(root, ".bookList");
-  let query = html_parse_find(bl, "." + class_old);
-  let list = html_parse_list_to(query);
-  function lambda(item) {
-    let text = html_parse_text(d, item);
-    let href = html_parse_href(d, item);
-    let v = {
-      text,
-      href,
-    };
-    return v;
-  }
-  let mapped = list_map(list, lambda);
+  let mapped = await ebible_version_books_testament(bible_folder, class_old);
   return mapped;
   marker("1");
 }
