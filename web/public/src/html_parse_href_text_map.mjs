1c7ad19f4589c5b7401f580ee625ae9a800b72c3
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:47:42 -0400

    function_auto_path public\src\html_parse_href_text_map.mjs

--- web/public/src/html_parse_href_text_map.mjs
+++ web/public/src/html_parse_href_text_map.mjs
@@ -1,4 +1,10 @@
-import { marker } from "./marker.mjs";
-export function html_parse_href_text_map() {
-  marker("1");
+import { list_map } from "./list_map.mjs";
+import { html_parse_href_text } from "./html_parse_href_text.mjs";
+export function html_parse_href_text_map(d, list) {
+  function lambda(item) {
+    let both = html_parse_href_text(d, item);
+    return both;
+  }
+  let mapped = list_map(list, lambda);
+  return mapped;
 }
