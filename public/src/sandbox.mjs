7d76283c3ce4388465815d59c326241ff89b8372
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:24:15 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,10 +1,13 @@
+import { ebible_version_download } from "./ebible_version_download.mjs";
 import { each_async } from "./each_async.mjs";
 import { ebible_versions } from "./ebible_versions.mjs";
 import { marker } from "./marker.mjs";
 import { ebible_version_verses } from "./ebible_version_verses.mjs";
 export async function sandbox() {
   let unique = await ebible_versions();
-  async function lambda(item) {}
+  async function lambda(item) {
+    let file_path = await ebible_version_download(bible_folder);
+  }
   await each_async(list, lambda);
   return unique;
   marker("1");
