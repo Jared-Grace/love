e8e05288c62c3a6c2b195c17c77bfff575cb2795
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 12:53:13 -0400

    function_auto_path public\src\app_reply_choices.mjs

--- web/public/src/app_reply_choices.mjs
+++ web/public/src/app_reply_choices.mjs
@@ -128,7 +128,7 @@ export function app_reply_choices() {
         emoji_100() +
         " through the grace of God, because " +
         emoji_book_open() +
-        " all things work together for good to them that love God and are called according to the purpose of God! (Romans 8:28) However please pray that I will stop hearing demons" +
+        " all things work together for good to them that love God and are called according to the purpose of God! (Romans 8:28) However please pray that the servant of God will stop hearing demons" +
         emoji_pray(),
     },
     {
