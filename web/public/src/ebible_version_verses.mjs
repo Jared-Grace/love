dc444dc1635ca7b5e2bba9edb1b52b829f4f357a
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 14:24:26 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- web/public/src/ebible_version_verses.mjs
+++ web/public/src/ebible_version_verses.mjs
@@ -5,12 +5,10 @@ import { html_parse_find } from "./html_parse_find.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { path_join } from "./path_join.mjs";
 import { file_read } from "./file_read.mjs";
-import { folder_read } from "./folder_read.mjs";
 import { ebible_version_download } from "./ebible_version_download.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_version_verses(bible_folder) {
   let file_path = await ebible_version_download(bible_folder);
-  let files = folder_read(file_path);
   let joined = path_join([file_path, "index.htm"]);
   let contents = await file_read(joined);
   let { d, root } = await html_parse(contents);
