f8e5ba2e7c7eb3052d40343ca62fa64cbc9bd0ca
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 13:57:01 -0400

    function_auto_path public\src\file_read.mjs

--- web/public/src/file_read.mjs
+++ web/public/src/file_read.mjs
@@ -1,5 +1,5 @@
 export async function file_read(file_path) {
   let fs = await import("fs");
-  let v = await fs.promises.readFile(file_path, "utf-8");
-  return v;
+  let contents = await fs.promises.readFile(file_path, "utf-8");
+  return contents;
 }
