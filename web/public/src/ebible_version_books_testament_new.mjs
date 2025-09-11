2708a0ae1d30a2b1f709a215a330edacbc5e02af
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 14:29:46 -0400

    function_auto_path public\src\ebible_version_books_testament_new.mjs

--- web/public/src/ebible_version_books_testament_new.mjs
+++ web/public/src/ebible_version_books_testament_new.mjs
@@ -2,7 +2,7 @@ import { marker } from "./marker.mjs";
 import { ebible_version_books_testament } from "./ebible_version_books_testament.mjs";
 export async function ebible_version_books_testament_new(bible_folder) {
   marker("1");
-  const class_old = "oo";
+  const class_old = "nn";
   let mapped = await ebible_version_books_testament(bible_folder, class_old);
   return mapped;
 }
