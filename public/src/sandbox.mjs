cc4a7363d6ea67fced7c3bc66a08939269589a3c
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:11:18 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,9 @@
+import { http_local_text } from "./http_local_text.mjs";
 import { marker } from "./marker.mjs";
 import { ebible_version_verses } from "./ebible_version_verses.mjs";
 export async function sandbox() {
   let url = "https://ebible.org/download.php";
-  return;
+  http_local_text: return;
   marker("1");
   let contents = await ebible_version_verses("engbsb");
   return contents;
