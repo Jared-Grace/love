dad7452218107a077ad29cb17c1fc48b4e2d52bf
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:56:12 -0400

    function_auto_path public\src\ebible_version_verses.mjs

---
+++ web/public/src/list_filter_starts_with.mjs
@@ -0,0 +1,10 @@
+import { list_filter } from "./list_filter.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
+export function list_filter_starts_with(mapped, book_code) {
+  function lambda(item) {
+    let sw = string_starts_with(item, book_code);
+    return sw;
+  }
+  let filtered = list_filter(mapped, lambda);
+  return filtered;
+}
