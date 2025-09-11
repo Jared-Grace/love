4399e4806963deec9fd34a5b1eb64d15f39ae47b
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:03:17 -0400

    function_auto_path public\src\list_filter_starts_with_not.mjs

--- web/public/src/list_filter_starts_with_not.mjs
+++ web/public/src/list_filter_starts_with_not.mjs
@@ -1,9 +1,12 @@
+import { not } from "./not.mjs";
 import { marker } from "./marker.mjs";
 import { list_filter } from "./list_filter.mjs";
+import { string_starts_with } from "./string_starts_with.mjs";
 export function list_filter_starts_with_not(mapped, book_code) {
   marker("1");
   function lambda(item) {
-    let sw = string_starts_with_not(item, book_code);
+    let a = string_starts_with(item, book_code);
+    let sw = not(a);
     return sw;
   }
   let filtered = list_filter(mapped, lambda);
