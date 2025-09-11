1492b1188637f936044479515bf67f4d786900f2
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 14:19:11 -0400

    function_auto_path public\src\html_parse_list_text_to.mjs

--- public/src/html_parse_list_text_to.mjs
+++ public/src/html_parse_list_text_to.mjs
@@ -1,4 +1,9 @@
 import { marker } from "./marker.mjs";
-export function html_parse_list_text_to() {
+export function html_parse_list_text_to(query) {
   marker("1");
+  function lambda(i, el) {
+    let v = $(el).text();
+    return v;
+  }
+  return query.map(lambda).get();
 }
