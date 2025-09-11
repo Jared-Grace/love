1805279a82deb393ce778c9fdf7a6cc24c8bb05f
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:50:11 -0400

    function_auto_path public\src\ebible_versions_download.mjs

--- web/public/src/ebible_versions_download.mjs
+++ web/public/src/ebible_versions_download.mjs
@@ -1,3 +1,4 @@
+import { catch_ignore_async } from "./catch_ignore_async.mjs";
 import { each_async } from "./each_async.mjs";
 import { ebible_version_download } from "./ebible_version_download.mjs";
 import { ebible_versions } from "./ebible_versions.mjs";
@@ -5,6 +6,7 @@ export async function ebible_versions_download() {
   let list = await ebible_versions();
   async function lambda(item) {
     let file_path = await ebible_version_download(item);
+    await catch_ignore_async(async function lambda3() {});
   }
   await each_async(list, lambda);
 }
