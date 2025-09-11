51728ce39b1b8332de7439891cfb53166645fa80
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:26:33 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- web/public/src/ebible_version_verses.mjs
+++ web/public/src/ebible_version_verses.mjs
@@ -1,3 +1,4 @@
+import { ebible_version_download } from "./ebible_version_download.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_first } from "./list_first.mjs";
 import { ebible_class_old } from "./ebible_class_old.mjs";
@@ -5,7 +6,10 @@ import { list_join_comma_space } from "./list_join_comma_space.mjs";
 import { ebible_version_books_testament } from "./ebible_version_books_testament.mjs";
 import { ebible_class_new } from "./ebible_class_new.mjs";
 import { marker } from "./marker.mjs";
+import { folder_read } from "./folder_read.mjs";
 export async function ebible_version_verses(bible_folder) {
+  let file_path = await ebible_version_download(bible_folder);
+  return folder_read(file_path);
   const n = ebible_class_new();
   let o = ebible_class_old();
   let classes = list_join_comma_space([o, n]);
