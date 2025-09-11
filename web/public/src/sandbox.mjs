047945f9f91b3405953bd56a387bfe34461080d2
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:19:38 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { html_parse_find_a_href_starts_with } from "./html_parse_find_a_href_starts_with.mjs";
 import { html_parse } from "./html_parse.mjs";
 import { http_local_text } from "./http_local_text.mjs";
 import { marker } from "./marker.mjs";
@@ -6,7 +7,8 @@ export async function sandbox() {
   let url = "https://ebible.org/download.php";
   let text = await http_local_text(url);
   let { d, root } = await html_parse(text);
-  return;
+  let filtered = html_parse_find_a_href_starts_with(root, d, "details.php?id=");
+  return filtered;
   marker("1");
   let contents = await ebible_version_verses("engbsb");
   return contents;
