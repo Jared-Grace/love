43b7ba97dd3172497457f0154af001fe63ebd56f
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:41:07 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- web/public/src/ebible_version_verses.mjs
+++ web/public/src/ebible_version_verses.mjs
@@ -1,3 +1,4 @@
+import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
 import { ebible_version_books } from "./ebible_version_books.mjs";
 import { html_parse_read } from "./html_parse_read.mjs";
 import { path_join } from "./path_join.mjs";
@@ -6,7 +7,6 @@ import { object_property_get } from "./object_property_get.mjs";
 import { list_first } from "./list_first.mjs";
 import { marker } from "./marker.mjs";
 import { folder_read } from "./folder_read.mjs";
-import { html_parse_find } from "./html_parse_find.mjs";
 export async function ebible_version_verses(bible_folder) {
   let file_path = await ebible_version_download(bible_folder);
   let files = folder_read(file_path);
@@ -17,6 +17,6 @@ export async function ebible_version_verses(bible_folder) {
   let chapters_name = book_code + ".htm";
   let joined = path_join([file_path, chapters_name]);
   let { d, root } = await html_parse_read(joined);
-  let result = html_parse_find(root, "a");
+  let result = html_parse_find_list_to(root, "a");
   return joined;
 }
