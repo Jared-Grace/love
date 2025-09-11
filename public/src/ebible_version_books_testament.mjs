ae1a2280ff857c4553fc77394e9492b635e71e90
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:45:18 -0400

    function_auto_path public\src\ebible_version_books_testament.mjs

--- public/src/ebible_version_books_testament.mjs
+++ public/src/ebible_version_books_testament.mjs
@@ -1,9 +1,8 @@
+import { html_parse_href_text } from "./html_parse_href_text.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
 import { html_parse_read } from "./html_parse_read.mjs";
 import { list_map } from "./list_map.mjs";
-import { html_parse_href } from "./html_parse_href.mjs";
-import { html_parse_text } from "./html_parse_text.mjs";
 import { html_parse_find } from "./html_parse_find.mjs";
 import { path_join } from "./path_join.mjs";
 import { ebible_version_download } from "./ebible_version_download.mjs";
@@ -14,12 +13,7 @@ export async function ebible_version_books_testament(bible_folder, selector) {
   let bl = html_parse_find(root, ".bookList");
   let list = html_parse_find_list_to(bl, selector);
   function lambda(item) {
-    let text = html_parse_text(d, item);
-    let href = html_parse_href(d, item);
-    let both = {
-      text,
-      href,
-    };
+    let both = html_parse_href_text(d, item);
     return both;
   }
   let mapped = list_map(list, lambda);
