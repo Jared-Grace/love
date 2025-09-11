367e7272851be3115be0711f5a859900bf9186ca
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 17:06:09 -0400

    function_auto_path public\src\list_filter_starts_with_not_multiple.mjs

--- web/public/src/list_filter_starts_with_not_multiple.mjs
+++ web/public/src/list_filter_starts_with_not_multiple.mjs
@@ -1,9 +1,11 @@
+import { list_add } from "./list_add.mjs";
 import { string_starts_with_not } from "./string_starts_with_not.mjs";
 import { marker } from "./marker.mjs";
 import { list_filter } from "./list_filter.mjs";
-export function list_filter_starts_with_not_multiple(mapped, book_code) {
+export function list_filter_starts_with_not_multiple(mapped, prefixes) {
   marker("1");
   function lambda(item) {
+    list_add(list, item2);
     let sw = string_starts_with_not(item, book_code);
     return sw;
   }
