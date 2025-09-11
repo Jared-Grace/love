f8dc76fd2b5f647b397feb710a1b837b66b10918
Grace-Jared <christrosetolife@gmail.com>
2025-09-11 16:28:56 -0400

    function_auto_path public\src\ebible_version_books_testament.mjs

--- web/public/src/ebible_version_books_testament.mjs
+++ web/public/src/ebible_version_books_testament.mjs
@@ -1,18 +1,16 @@
+import { html_parse_read } from "./html_parse_read.mjs";
 import { string_take } from "./string_take.mjs";
 import { list_map } from "./list_map.mjs";
 import { html_parse_href } from "./html_parse_href.mjs";
 import { html_parse_text } from "./html_parse_text.mjs";
 import { html_parse_list_to } from "./html_parse_list_to.mjs";
 import { html_parse_find } from "./html_parse_find.mjs";
-import { html_parse } from "./html_parse.mjs";
-import { file_read } from "./file_read.mjs";
 import { path_join } from "./path_join.mjs";
 import { ebible_version_download } from "./ebible_version_download.mjs";
 export async function ebible_version_books_testament(bible_folder, selector) {
   let file_path = await ebible_version_download(bible_folder);
   let joined = path_join([file_path, "index.htm"]);
-  let contents = await file_read(joined);
-  let { d, root } = await html_parse(contents);
+  let { d, root } = await html_parse_read(joined);
   let bl = html_parse_find(root, ".bookList");
   let query = html_parse_find(bl, selector);
   let list = html_parse_list_to(query);
