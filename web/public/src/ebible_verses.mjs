9ff13e516db14f4fbe2a7c825ccf172c45b361bc
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:17:04 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -1,3 +1,4 @@
+import { html_parse_attr } from "./html_parse_attr.mjs";
 import { list_map } from "./list_map.mjs";
 import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
 import { html_parse_find } from "./html_parse_find.mjs";
@@ -8,7 +9,9 @@ export async function ebible_verses(bible_folder, chapter_code) {
   let { d, root } = await html_parse_read(joined);
   let main = html_parse_find(root, ".main");
   let descendants = html_parse_find_list_to(root, "*");
-  function lambda(item) {}
+  function lambda(item) {
+    let v2 = html_parse_attr(d2, item2, name);
+  }
   let mapped = list_map(list, lambda);
   return v;
 }
