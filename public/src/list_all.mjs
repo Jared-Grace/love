a5c20d2871a76bdb6ba37c0fedd840d9f0069f9b
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:45:05 -0400

    function_auto_path public\src\list_all.mjs

--- public/src/list_all.mjs
+++ public/src/list_all.mjs
@@ -2,8 +2,8 @@ import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_all(list, lambda$item) {
   let list2 = list_filter(list, lambda$item);
-  let v = list_size(list2) === list_size(list);
-  return v;
+  let a = list_size(list2) === list_size(list);
+  return a;
   {
     let filtered = list_filter(list, lambda$item);
     let v2 = list_size(filtered) === list_size(list);
