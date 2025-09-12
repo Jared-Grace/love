d9b7f80d9498c1d4e91df6a2cd0242ec15385d30
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:21:36 -0400

    function_auto_path public\src\html_parse_descendants_classes.mjs

--- public/src/html_parse_descendants_classes.mjs
+++ public/src/html_parse_descendants_classes.mjs
@@ -1,19 +1,20 @@
+import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { string_is } from "./string_is.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_unique } from "./list_unique.mjs";
-import { list_map } from "./list_map.mjs";
 import { html_parse_attr } from "./html_parse_attr.mjs";
 import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
 export function html_parse_descendants_classes(item, d) {
   let descendants = html_parse_find_list_to(item, "*");
-  function lambda2(la) {}
-  let list = list_adder(lambda2);
-  function lambda(item) {
-    let c = html_parse_attr(d, item, "class");
-    return c;
+  function lambda2(la) {
+    function lambda(item) {
+      let c = html_parse_attr(d, item, "class");
+      return c;
+    }
+    let mapped = each(descendants, lambda);
   }
-  let mapped = list_map(descendants, lambda);
+  let list = list_adder(lambda2);
   let unique = list_unique(mapped);
   let filtered = list_filter(unique, string_is);
   return filtered;
