845e3b188a3a4095057842e9145d46bc02aba009
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:47:45 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- web/public/src/ebible_version_verses.mjs
+++ web/public/src/ebible_version_verses.mjs
@@ -2,15 +2,14 @@ import { each_async } from "./each_async.mjs";
 import { ebible_chapters } from "./ebible_chapters.mjs";
 import { ebible_version_books } from "./ebible_version_books.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { list_first } from "./list_first.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_version_verses(bible_folder) {
   let books = await ebible_version_books(bible_folder);
   await each_async(books, lambda);
-  async function lambda(book) {}
+  async function lambda(book) {
+    let book_code = object_property_get(book, "book_code");
+    let chapters = await ebible_chapters(bible_folder, book_code);
+  }
   marker("1");
-  let first = list_first(books);
-  let book_code = object_property_get(first, "book_code");
-  let chapters = await ebible_chapters(bible_folder, book_code);
   return chapters;
 }
