6d5aed2ba478f88f2207d718dc5672b5d8928812
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:05:35 -0400

    function_auto_path public\src\ebible_verses.mjs

--- public/src/ebible_verses.mjs
+++ public/src/ebible_verses.mjs
@@ -1,7 +1,7 @@
+import { html_parse_read } from "./html_parse_read.mjs";
 import { ebible_version_download_path_combine } from "./ebible_version_download_path_combine.mjs";
-import { marker } from "./marker.mjs";
-export function ebible_verses(bible_folder, chapter_code) {
+export async function ebible_verses(bible_folder, chapter_code) {
   let joined = ebible_version_download_path_combine(bible_folder, chapter_code);
-  marker("1");
+  let parsed = await html_parse_read(file_path);
   return joined;
 }
