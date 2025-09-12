d0fde38e426d0424209a6f3e004703f8025a0e47
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 15:45:43 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -15,7 +15,19 @@ export async function ebible_verses(bible_folder, chapter_code) {
   let { d, root } = await html_parse_read(joined);
   let main = html_parse_find(root, ".main");
   let classes = html_parse_descendants_classes(main, d);
-  let include = ["d", "it", "li", "li2", "m", "pc", "pmo", "q", "q2", "qr"];
+  let include = [
+    "d",
+    "it",
+    "li",
+    "li2",
+    "m",
+    "p",
+    "pc",
+    "pmo",
+    "q",
+    "q2",
+    "qr",
+  ];
   let exclude = [
     "b",
     "chapterlabel",
