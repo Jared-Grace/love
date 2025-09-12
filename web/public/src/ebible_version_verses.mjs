57c46bfc111eb9e4a6875008fad8afe3218da9ee
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:50:35 -0400

    function_auto_path public\src\ebible_version_verses.mjs

--- web/public/src/ebible_version_verses.mjs
+++ web/public/src/ebible_version_verses.mjs
@@ -4,13 +4,14 @@ import { ebible_chapters } from "./ebible_chapters.mjs";
 import { ebible_version_books } from "./ebible_version_books.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function ebible_version_verses(bible_folder) {
-  function lambda2(la) {}
-  let list = await list_adder_multiple_async(lambda2);
-  let books = await ebible_version_books(bible_folder);
-  await each_async(books, lambda);
-  async function lambda(book) {
-    let book_code = object_property_get(book, "book_code");
-    let chapters = await ebible_chapters(bible_folder, book_code);
+  async function lambda2(la) {
+    let books = await ebible_version_books(bible_folder);
+    await each_async(books, lambda);
+    async function lambda(book) {
+      let book_code = object_property_get(book, "book_code");
+      let chapters = await ebible_chapters(bible_folder, book_code);
+    }
   }
+  let list = await list_adder_multiple_async(lambda2);
   return chapters;
 }
