bb3918a4081dcbd7888a1e581777816188cbee76
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:20:05 -0400

    function_auto_path public\src\ebible_verses.mjs

--- public/src/ebible_verses.mjs
+++ public/src/ebible_verses.mjs
@@ -1,9 +1,4 @@
-import { string_is } from "./string_is.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { list_unique } from "./list_unique.mjs";
-import { html_parse_attr } from "./html_parse_attr.mjs";
-import { list_map } from "./list_map.mjs";
-import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
+import { html_parse_descendants_classes } from "./html_parse_descendants_classes.mjs";
 import { html_parse_find } from "./html_parse_find.mjs";
 import { html_parse_read } from "./html_parse_read.mjs";
 import { ebible_version_download_path_combine } from "./ebible_version_download_path_combine.mjs";
@@ -11,13 +6,6 @@ export async function ebible_verses(bible_folder, chapter_code) {
   let joined = ebible_version_download_path_combine(bible_folder, chapter_code);
   let { d, root } = await html_parse_read(joined);
   let main = html_parse_find(root, ".main");
-  let descendants = html_parse_find_list_to(main, "*");
-  function lambda(item) {
-    let c = html_parse_attr(d, item, "class");
-    return c;
-  }
-  let mapped = list_map(descendants, lambda);
-  let unique = list_unique(mapped);
-  let filtered = list_filter(unique, string_is);
+  let filtered = html_parse_descendants_classes(main, d);
   return filtered;
 }
