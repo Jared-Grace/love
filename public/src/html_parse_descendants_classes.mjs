b83136eac91604d658c6b034609f5ec6f4d869c2
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:22:53 -0400

    function_auto_path public\src\html_parse_descendants_classes.mjs

--- public/src/html_parse_descendants_classes.mjs
+++ public/src/html_parse_descendants_classes.mjs
@@ -2,7 +2,6 @@ import { string_split } from "./string_split.mjs";
 import { each } from "./each.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { string_is } from "./string_is.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { list_unique } from "./list_unique.mjs";
 import { html_parse_attr } from "./html_parse_attr.mjs";
 import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
@@ -11,15 +10,14 @@ export function html_parse_descendants_classes(item, d) {
   function lambda2(la) {
     function lambda(item) {
       let c = html_parse_attr(d, item, "class");
-      if (false) {
+      if (string_is(c)) {
+        let split = string_split(c, " ");
+        each(split, la);
       }
-      let split = string_split(c, " ");
-      each(split, la);
     }
     each(descendants, lambda);
   }
   let list = list_adder(lambda2);
   let unique = list_unique(list);
-  let filtered = list_filter(unique, string_is);
-  return filtered;
+  return unique;
 }
