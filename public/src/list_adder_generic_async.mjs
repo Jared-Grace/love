5ff01c89975fd19eaa5ce203afb061341f136ee3
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:49:40 -0400

    function_auto_path public\src\list_adder_generic_async.mjs

--- public/src/list_adder_generic_async.mjs
+++ public/src/list_adder_generic_async.mjs
@@ -1,10 +1,10 @@
 import { marker } from "./marker.mjs";
-export function list_adder_generic_async(lambda, fn) {
+export async function list_adder_generic_async(lambda, fn) {
   marker("1");
   let list = [];
   function list_adder_inner(item) {
     fn(list, item);
   }
-  lambda(list_adder_inner);
+  await lambda(list_adder_inner);
   return list;
 }
