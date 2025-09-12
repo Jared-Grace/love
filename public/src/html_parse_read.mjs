8b0313a633bcddfeef808cb05bf7914c5c7a4b2f
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:06:04 -0400

    function_auto_path public\src\html_parse_read.mjs

--- public/src/html_parse_read.mjs
+++ public/src/html_parse_read.mjs
@@ -3,7 +3,7 @@ import { html_parse } from "./html_parse.mjs";
 import { file_read } from "./file_read.mjs";
 export async function html_parse_read(file_path) {
   let contents = await file_read(file_path);
-  log(message);
+  log(contents);
   let parsed = await html_parse(contents);
   return parsed;
 }
