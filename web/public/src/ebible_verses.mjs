a4d79c26c5643761d66210039007f6d4f9db035e
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:30:34 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -1,6 +1,3 @@
-import { html_parse_text } from "./html_parse_text.mjs";
-import { list_to_dictionary } from "./list_to_dictionary.mjs";
-import { html_parse_descendants_classes } from "./html_parse_descendants_classes.mjs";
 import { html_parse_find } from "./html_parse_find.mjs";
 import { html_parse_read } from "./html_parse_read.mjs";
 import { ebible_version_download_path_combine } from "./ebible_version_download_path_combine.mjs";
@@ -8,12 +5,25 @@ export async function ebible_verses(bible_folder, chapter_code) {
   let joined = ebible_version_download_path_combine(bible_folder, chapter_code);
   let { d, root } = await html_parse_read(joined);
   let main = html_parse_find(root, ".main");
-  let classes = html_parse_descendants_classes(main, d);
-  function lambda(c) {
-    let e = html_parse_find(root, "." + c);
-    let text = html_parse_text(d, e);
-    return text;
-  }
-  let dictionary = list_to_dictionary(classes, lambda);
-  return dictionary;
+  let include = [
+    "mt",
+    "chapterlabel",
+    "s",
+    "r",
+    "m",
+    "verse",
+    "b",
+    "s2",
+    "pmo",
+    "notemark",
+    "popup",
+    "q",
+    "q2",
+    "tnav",
+    "footnote",
+    "f",
+    "notebackref",
+    "ft",
+    "copyright",
+  ];
 }
