97c29cb465ddeeecc6014fd7109214ee5871c711
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:28:28 -0400

    function_auto_path public\src\ebible_chapter_classes.mjs

---
+++ web/public/src/ebible_chapter_classes.mjs
@@ -0,0 +1,19 @@
+import { html_parse_text } from "./html_parse_text.mjs";
+import { list_to_dictionary } from "./list_to_dictionary.mjs";
+import { html_parse_descendants_classes } from "./html_parse_descendants_classes.mjs";
+import { html_parse_find } from "./html_parse_find.mjs";
+import { html_parse_read } from "./html_parse_read.mjs";
+import { ebible_version_download_path_combine } from "./ebible_version_download_path_combine.mjs";
+export async function ebible_chapter_classes(bible_folder, chapter_code) {
+  let joined = ebible_version_download_path_combine(bible_folder, chapter_code);
+  let { d, root } = await html_parse_read(joined);
+  let main = html_parse_find(root, ".main");
+  let classes = html_parse_descendants_classes(main, d);
+  function lambda(c) {
+    let e = html_parse_find(root, "." + c);
+    let text = html_parse_text(d, e);
+    return text;
+  }
+  let dictionary = list_to_dictionary(classes, lambda);
+  return dictionary;
+}
