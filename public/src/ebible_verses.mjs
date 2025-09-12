df5c8427b9746f4af1e2fffcd8359896991ab57a
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:46:31 -0400

    function_auto_path public\src\ebible_verses.mjs

--- public/src/ebible_verses.mjs
+++ public/src/ebible_verses.mjs
@@ -1,6 +1,5 @@
 import { list_empty_is } from "./list_empty_is.mjs";
 import { list_difference } from "./list_difference.mjs";
-import { assert } from "./assert.mjs";
 import { html_parse_descendants_classes } from "./html_parse_descendants_classes.mjs";
 import { html_parse_find_remove } from "./html_parse_find_remove.mjs";
 import { each } from "./each.mjs";
@@ -8,6 +7,7 @@ import { html_parse_classes_preview } from "./html_parse_classes_preview.mjs";
 import { html_parse_find } from "./html_parse_find.mjs";
 import { html_parse_read } from "./html_parse_read.mjs";
 import { ebible_version_download_path_combine } from "./ebible_version_download_path_combine.mjs";
+import { assert_json } from "./assert_json.mjs";
 export async function ebible_verses(bible_folder, chapter_code) {
   let joined = ebible_version_download_path_combine(bible_folder, chapter_code);
   let { d, root } = await html_parse_read(joined);
@@ -33,8 +33,10 @@ export async function ebible_verses(bible_folder, chapter_code) {
   ];
   let list2 = list_difference(classes, include);
   let list3 = list_difference(list2, exclude);
-  let e = list_empty_is(list);
-  assert(a);
+  let e = list_empty_is(list3);
+  assert_json(e, {
+    e,
+  });
   let dictionary = html_parse_classes_preview(main, d, include);
   function lambda(item) {
     html_parse_find_remove(main, "." + item);
