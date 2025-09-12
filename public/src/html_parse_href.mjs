ccff05be3fff92ec9316eeaf9b90ca18153bed62
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:16:28 -0400

    function_auto_path public\src\html_parse_href.mjs

--- public/src/html_parse_href.mjs
+++ public/src/html_parse_href.mjs
@@ -1,7 +1,8 @@
+import { html_parse_attr } from "./html_parse_attr.mjs";
 import { marker } from "./marker.mjs";
 export function html_parse_href(d, item) {
   marker("1");
   const name = "href";
-  let href = d(item).attr(name);
+  let href = html_parse_attr(d, item, name);
   return href;
 }
