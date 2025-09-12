115f760d03fa15bd4005208dac78c80e5a7e0c71
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:42:57 -0400

    function_auto_path public\src\ebible_verses.mjs

--- public/src/ebible_verses.mjs
+++ public/src/ebible_verses.mjs
@@ -1,3 +1,5 @@
+import { assert } from "./assert.mjs";
+import { html_parse_descendants_classes } from "./html_parse_descendants_classes.mjs";
 import { html_parse_find_remove } from "./html_parse_find_remove.mjs";
 import { each } from "./each.mjs";
 import { html_parse_classes_preview } from "./html_parse_classes_preview.mjs";
@@ -8,6 +10,7 @@ export async function ebible_verses(bible_folder, chapter_code) {
   let joined = ebible_version_download_path_combine(bible_folder, chapter_code);
   let { d, root } = await html_parse_read(joined);
   let main = html_parse_find(root, ".main");
+  let classes = html_parse_descendants_classes(main, d);
   let include = ["m", "pmo", "q", "q2"];
   let exclude = [
     "b",
@@ -26,6 +29,7 @@ export async function ebible_verses(bible_folder, chapter_code) {
     "tnav",
     "verse",
   ];
+  assert(b);
   let dictionary = html_parse_classes_preview(main, d, include);
   function lambda(item) {
     html_parse_find_remove(main, "." + item);
