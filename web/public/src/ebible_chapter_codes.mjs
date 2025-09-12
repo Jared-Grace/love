8de46a64cb66d9b1fe1405790ac3ada781e5a6a0
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:51:04 -0400

    r ebible_version_verses ebible_chapter_codes

--- web/public/src/ebible_chapter_codes.mjs
+++ web/public/src/ebible_chapter_codes.mjs
@@ -1,9 +1,11 @@
+import { marker } from "./marker.mjs";
 import { list_adder_multiple_async } from "./list_adder_multiple_async.mjs";
 import { each_async } from "./each_async.mjs";
 import { ebible_chapters } from "./ebible_chapters.mjs";
 import { ebible_version_books } from "./ebible_version_books.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function ebible_chapter_codes(bible_folder) {
+  marker("1");
   async function lambda2(la) {
     let books = await ebible_version_books(bible_folder);
     await each_async(books, lambda);
