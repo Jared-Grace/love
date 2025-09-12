a23e87b42b079c28f98769f65e8b2c903f0b3321
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:04:00 -0400

    function_auto_path public\src\ebible_chapters.mjs

--- web/public/src/ebible_chapters.mjs
+++ web/public/src/ebible_chapters.mjs
@@ -1,13 +1,10 @@
+import { ebible_version_download_path_combine } from "./ebible_version_download_path_combine.mjs";
 import { html_parse_find_a_href_starts_with } from "./html_parse_find_a_href_starts_with.mjs";
 import { list_filter_starts_with_not_multiple } from "./list_filter_starts_with_not_multiple.mjs";
 import { list_map } from "./list_map.mjs";
 import { html_parse_read } from "./html_parse_read.mjs";
-import { path_join } from "./path_join.mjs";
-import { ebible_version_download_path } from "./ebible_version_download_path.mjs";
 export async function ebible_chapters(bible_folder, book_code) {
-  let chapters_name = book_code + ".htm";
-  let file_path = ebible_version_download_path(bible_folder);
-  let joined = path_join([file_path, chapters_name]);
+  let joined = ebible_version_download_path_combine(bible_folder, book_code);
   let { d, root } = await html_parse_read(joined);
   let filtered = html_parse_find_a_href_starts_with(root, d, book_code);
   function lambda(item) {
