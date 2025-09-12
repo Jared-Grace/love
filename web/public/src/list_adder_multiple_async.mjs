a3f3c7bc4156c350e11ff62685a292d4d1a60fe1
Grace-Jared <christrosetolife@gmail.com>
2025-09-12 14:49:00 -0400

    cs list_adder_multiple async

--- web/public/src/list_adder_multiple_async.mjs
+++ web/public/src/list_adder_multiple_async.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_adder_generic } from "./list_adder_generic.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 export function list_adder_multiple_async(lambda$la) {
+  marker("1");
   let fn = list_add_multiple;
   let list = list_adder_generic(lambda$la, fn);
   return list;
