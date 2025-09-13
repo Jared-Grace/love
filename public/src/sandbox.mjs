38664cefb25ade204e388e88a6ebdc25bcd45ef2
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:58:19 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { file_name_json } from "./file_name_json.mjs";
 import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { ebible_verses } from "./ebible_verses.mjs";
@@ -12,7 +13,8 @@ export async function sandbox() {
     let verses = await ebible_verses(bible_folder, chapter_code);
     async function lambda2(v) {
       let verse_number = object_property_get(v, "verse_number");
-      list_join_slash_forward(list);
+      let file_name = file_name_json(verse_number);
+      list_join_slash_forward(["bible", bible_folder, chapter_code, file_name]);
     }
     await each_async(verses, lambda2);
   }
