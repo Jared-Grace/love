c7a1eef9590ef2a1ae94f0f3c18fed25d9ba5e63
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:50:50 -0400

    c list_index_of list_index_of_last

--- web/public/src/list_index_of_last.mjs
+++ web/public/src/list_index_of_last.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { error_json } from "./error_json.mjs";
 export function list_index_of_last(list, item) {
+  marker("1");
   let index = list.indexOf(item);
   if (index <= -1) {
     error_json({
