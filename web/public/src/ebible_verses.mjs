9a636730b374580301f769014b2599115e57552c
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:19:20 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -2,6 +2,6 @@ import { ebible_chapter_text } from "./ebible_chapter_text.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_verses(bible_folder, chapter_code) {
   marker("1");
-  let v = await ebible_chapter_text(bible_folder, chapter_code);
-  return v;
+  let text = await ebible_chapter_text(bible_folder, chapter_code);
+  return text;
 }
