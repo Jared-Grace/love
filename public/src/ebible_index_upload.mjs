37b21b9d885fe5b1b066f95456a3b7fc9cd25554
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 18:09:23 -0400

    function_auto_path public\src\ebible_index_upload.mjs

--- public/src/ebible_index_upload.mjs
+++ public/src/ebible_index_upload.mjs
@@ -3,5 +3,4 @@ import { ebible_index } from "./ebible_index.mjs";
 export async function ebible_index_upload(bible_folder) {
   let index = await ebible_index(bible_folder);
   await ebible_firebase_upload(bible_folder, "index", index);
-  return index;
 }
