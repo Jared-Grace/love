11eb31cb9889bdd0ecc382f1fbd9b48dced91e14
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:39:28 -0400

    function_auto_path public\src\list_index_of.mjs

--- web/public/src/list_index_of.mjs
+++ web/public/src/list_index_of.mjs
@@ -1,8 +1,12 @@
-import { error } from "./error.mjs";
+import { error_json } from "./error_json.mjs";
 export function list_index_of(list, item) {
   let index = list.indexOf(item);
   if (index <= -1) {
-    error();
+    error_json({
+      list,
+      item,
+      index,
+    });
   }
   return index;
 }
