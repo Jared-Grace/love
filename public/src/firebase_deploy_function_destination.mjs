785c1b2bc6714242840d33f97e97d17ee0a872bf
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:55:46 -0400

    function_auto_path public\src\firebase_deploy_function_destination.mjs

--- public/src/firebase_deploy_function_destination.mjs
+++ public/src/firebase_deploy_function_destination.mjs
@@ -1,7 +1,8 @@
-import { list_join } from "./list_join.mjs";
+import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 export function firebase_deploy_function_destination(name, f_name) {
   let file_name = file_name_json(name);
-  let destination = list_join(["function", f_name, file_name], "/");
+  const list = ["function", f_name, file_name];
+  let destination = list_join_slash_forward(list);
   return destination;
 }
