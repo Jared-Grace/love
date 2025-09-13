fc39183f8723ae065958a6a38c3b33c558facdda
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:22:11 -0400

    r html_parse_find_list_text html_parse_find_list_href_text

--- public/src/html_parse_find_list_href_text.mjs
+++ public/src/html_parse_find_list_href_text.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { html_parse_href_text_map } from "./html_parse_href_text_map.mjs";
 import { html_parse_find_list_to } from "./html_parse_find_list_to.mjs";
 export function html_parse_find_list_href_text(bl, selector, d) {
+  marker("1");
   let list = html_parse_find_list_to(bl, selector);
   let mapped = html_parse_href_text_map(d, list);
   return mapped;
