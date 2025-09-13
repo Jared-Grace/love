d6ad111ad745389bf6886c455085772ca1af0b23
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 18:08:39 -0400

    function_auto_path public\src\ebible_verses_upload.mjs

--- public/src/ebible_verses_upload.mjs
+++ public/src/ebible_verses_upload.mjs
@@ -1,11 +1,9 @@
-import { ebible_firebase_folder } from "./ebible_firebase_folder.mjs";
+import { ebible_firebase_upload } from "./ebible_firebase_upload.mjs";
 import { ebible_chapters_each_verses } from "./ebible_chapters_each_verses.mjs";
 import { list_wait } from "./list_wait.mjs";
 import { list_map } from "./list_map.mjs";
-import { firebase_upload_object } from "./firebase_upload_object.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
-import { file_name_json } from "./file_name_json.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function ebible_verses_upload(bible_folder) {
   await ebible_chapters_each_verses(bible_folder, each_chapter);
@@ -23,10 +21,7 @@ export async function ebible_verses_upload(bible_folder) {
       const object = {
         verse: merged,
       };
-      let file_name = file_name_json(joined2);
-      let joined = ebible_firebase_folder(bible_folder);
-      let destination = list_join_slash_forward([joined, file_name]);
-      await firebase_upload_object(object, destination);
+      await ebible_firebase_upload(bible_folder, file_name, object);
     }
     let mapped = list_map(verses, lambda);
     await list_wait(mapped);
