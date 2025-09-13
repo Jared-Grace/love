a9df5c6079d2e3841d0524f5eb80c7a91007c315
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 19:45:13 -0400

    function_auto_path public\src\app_reply_main.mjs

--- web/public/src/app_reply_main.mjs
+++ web/public/src/app_reply_main.mjs
@@ -1,3 +1,10 @@
+import { log } from "./log.mjs";
+import { json_from } from "./json_from.mjs";
+import { buffer_string_to } from "./buffer_string_to.mjs";
+import { ebible_index_upload_name } from "./ebible_index_upload_name.mjs";
+import { ebible_folder_english } from "./ebible_folder_english.mjs";
+import { ebible_firebase_upload_path } from "./ebible_firebase_upload_path.mjs";
+import { firebase_storage_download } from "./firebase_storage_download.mjs";
 import { not } from "./not.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { string_lower_to } from "./string_lower_to.mjs";
@@ -28,6 +35,13 @@ import { html_button } from "./html_button.mjs";
 import { marker } from "./marker.mjs";
 export async function app_reply_main() {
   if (false) {
+    let bible_folder = ebible_folder_english();
+    let file_name = ebible_index_upload_name();
+    let destination = ebible_firebase_upload_path(bible_folder, file_name);
+    let buffer = await firebase_storage_download(destination);
+    let s = buffer_string_to(buffer);
+    let index = json_from(s);
+    log(message);
   }
   let bible = kjv();
   let properties = object_properties(bible);
