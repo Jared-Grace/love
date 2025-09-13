89a4523b41e9fa7677bd56ffb78dfa150bd0a7db
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 18:16:59 -0400

    function_auto_path public\src\ebible_firebase_upload.mjs

--- web/public/src/ebible_firebase_upload.mjs
+++ web/public/src/ebible_firebase_upload.mjs
@@ -1,6 +1,6 @@
 import { ebible_firebase_upload_path } from "./ebible_firebase_upload_path.mjs";
 import { firebase_upload_object } from "./firebase_upload_object.mjs";
 export async function ebible_firebase_upload(bible_folder, file_name, object) {
-  let destination = ebible_firebase_upload_path(file_name, bible_folder);
+  let destination = ebible_firebase_upload_path(bible_folder, file_name);
   await firebase_upload_object(object, destination);
 }
