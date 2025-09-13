dbd1d53b9f69ff6db019005fd28eacb9448504e1
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:50:58 -0400

    function_auto_path public\src\list_index_of_last.mjs

--- web/public/src/list_index_of_last.mjs
+++ web/public/src/list_index_of_last.mjs
@@ -2,7 +2,7 @@ import { marker } from "./marker.mjs";
 import { error_json } from "./error_json.mjs";
 export function list_index_of_last(list, item) {
   marker("1");
-  let index = list.indexOf(item);
+  let index = list.lastIndexOf(item);
   if (index <= -1) {
     error_json({
       list,
