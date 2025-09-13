d1dd25d9c305c958de0552f79fe5d7b93b460d2c
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 19:50:17 -0400

    function_auto_path public\src\ebible_index.mjs

--- web/public/src/ebible_index.mjs
+++ web/public/src/ebible_index.mjs
@@ -6,7 +6,8 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { ebible_chapter_code_to_book } from "./ebible_chapter_code_to_book.mjs";
 import { ebible_chapters_each_verses } from "./ebible_chapters_each_verses.mjs";
 export async function ebible_index(bible_folder) {
-  let index = {};
+  let books = {};
+  let index = [];
   await ebible_chapters_each_verses(bible_folder, each_chapter);
   async function each_chapter(chapter_code, verses) {
     let book_code = ebible_chapter_code_to_book(chapter_code);
