742c17df18582c3fc4cbc5a294559fa22312ca8e
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:30:27 -0400

    function_auto_path public\src\string_split_space.mjs

--- public/src/string_split_space.mjs
+++ public/src/string_split_space.mjs
@@ -1,5 +1,5 @@
 import { string_split } from "./string_split.mjs";
 export function string_split_space(s) {
-  let split2 = string_split(s, " ");
-  return split2;
+  let split = string_split(s, " ");
+  return split;
 }
