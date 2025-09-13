291e32fcb628cac30c3d4f05800c217e3ee2e2d1
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:50:49 -0400

    function_auto_path public\src\list_index_of_last.mjs

---
+++ web/public/src/list_index_of_last.mjs
@@ -0,0 +1,12 @@
+import { error_json } from "./error_json.mjs";
+export function list_index_of_last(list, item) {
+  let index = list.indexOf(item);
+  if (index <= -1) {
+    error_json({
+      list,
+      item,
+      index,
+    });
+  }
+  return index;
+}
