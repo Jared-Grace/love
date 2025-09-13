a4cb08f145a2cc24ecf5b12b03e484cb6fdcb4f0
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:51:10 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -1,9 +1,9 @@
+import { list_index_of_last } from "./list_index_of_last.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { ebible_verse } from "./ebible_verse.mjs";
 import { list_adder } from "./list_adder.mjs";
 import { list_take } from "./list_take.mjs";
 import { list_skip } from "./list_skip.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 import { each_reverse } from "./each_reverse.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
@@ -23,7 +23,7 @@ export async function ebible_verses(bible_folder, chapter_code) {
   let filtered = list_filter(split, string_empty_not_is);
   function lambda(la) {
     function lambda2(verse_number) {
-      let index = list_index_of(filtered, verse_number);
+      let index = list_index_of_last(filtered, verse_number);
       let skipped = list_skip(filtered, index + 1);
       const v = ebible_verse(skipped, verse_number);
       la(v);
