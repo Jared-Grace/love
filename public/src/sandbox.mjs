7864e66a37d697cb25340d79a1d7f88cbc498fbf
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:17:47 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,13 +1,13 @@
+import { ebible_chapter_text } from "./ebible_chapter_text.mjs";
 import { each_async } from "./each_async.mjs";
 import { ebible_chapter_codes } from "./ebible_chapter_codes.mjs";
-import { ebible_verses } from "./ebible_verses.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
   const bible_folder = "urdgvu";
   let list = await ebible_chapter_codes(bible_folder);
   async function lambda(item) {
-    let contents = await ebible_verses(bible_folder, item);
+    let contents = await ebible_chapter_text(bible_folder, item);
   }
   await each_async(list, lambda);
 }
