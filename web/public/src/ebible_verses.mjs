1ba613d6b10f457d98861bf2eca528e9e92c285f
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:01:31 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -44,7 +44,7 @@ export async function ebible_verses(bible_folder, chapter_code) {
   log({
     result,
   });
-  let text = html_parse_text(d, result);
+  let text = html_parse_text(d, root);
   log({
     text,
   });
