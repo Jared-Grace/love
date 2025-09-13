57d10a5cbef744216dc8afc280b3e293f0266585
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 17:57:57 -0400

    function_auto_path public\src\ebible_chapter_code_to_book.mjs

--- public/src/ebible_chapter_code_to_book.mjs
+++ public/src/ebible_chapter_code_to_book.mjs
@@ -1,7 +1,9 @@
+import { ebible_book_code_size } from "./ebible_book_code_size.mjs";
 import { marker } from "./marker.mjs";
 import { string_take } from "./string_take.mjs";
 export function ebible_chapter_code_to_book(href) {
   marker("1");
-  let book_code = string_take(href, 3);
+  let count = ebible_book_code_size();
+  let book_code = string_take(href, count);
   return book_code;
 }
