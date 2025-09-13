36fef5e4a61a422ac28caf743da35ca109cfa751
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:18:57 -0400

    fw ebible_chapter_text ebible_verses

---
+++ web/public/src/ebible_verses.mjs
@@ -0,0 +1,6 @@
+import { ebible_chapter_text } from "./ebible_chapter_text.mjs";
+import { marker } from "./marker.mjs";
+export async function ebible_verses(bible_folder, chapter_code) {
+  marker("1");
+  return await ebible_chapter_text(bible_folder, chapter_code);
+}
