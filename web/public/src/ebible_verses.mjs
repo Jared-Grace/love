7f643c43b5c37d3fd3cc4880ba9a268edd4b0cb6
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 16:44:23 -0400

    function_auto_path public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -11,6 +11,7 @@ import { list_filter } from "./list_filter.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { ebible_chapter_text } from "./ebible_chapter_text.mjs";
 import { marker } from "./marker.mjs";
+import { list_reverse } from "./list_reverse.mjs";
 export async function ebible_verses(bible_folder, chapter_code) {
   marker("1");
   let { verse_numbers, text } = await ebible_chapter_text(
@@ -36,5 +37,6 @@ export async function ebible_verses(bible_folder, chapter_code) {
     each_reverse(verse_numbers, lambda2);
   }
   let list = list_adder(lambda);
-  return filtered;
+  list_reverse(list);
+  return list;
 }
