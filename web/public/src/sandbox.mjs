e6c48c3adf95637d7afc768199b109e3e8d589dc
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 17:34:48 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -2,6 +2,6 @@ import { ebible_verses_upload } from "./ebible_verses_upload.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
-  const bible_folder = "urdgvu";
+  const bible_folder = "engbsb";
   await ebible_verses_upload(bible_folder);
 }
