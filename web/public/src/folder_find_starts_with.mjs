095be5f1edb421300aae9b60368da3cbdca8bff9
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 19:44:51 -0400

    function_auto_path public\src\folder_find_starts_with.mjs

--- web/public/src/folder_find_starts_with.mjs
+++ web/public/src/folder_find_starts_with.mjs
@@ -1,8 +1,8 @@
+import { folder_read_async } from "./folder_read_async.mjs";
 import { list_find } from "./list_find.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
-import { folder_read } from "./folder_read.mjs";
-export function folder_find_starts_with(path_folder, prefix) {
-  let files = folder_read(path_folder);
+export async function folder_find_starts_with(path_folder, prefix) {
+  let files = await folder_read_async(path_folder);
   function lambda(item) {
     let sw = string_starts_with(item, prefix);
     return sw;
