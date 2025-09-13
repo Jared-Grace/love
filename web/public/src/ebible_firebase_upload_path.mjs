5e0d1c1cde16a68391a3f5f8027890809de62e11
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 18:17:18 -0400

    function_auto_path public\src\ebible_firebase_upload_path.mjs

--- web/public/src/ebible_firebase_upload_path.mjs
+++ web/public/src/ebible_firebase_upload_path.mjs
@@ -1,6 +1,6 @@
 import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
 import { file_name_json } from "./file_name_json.mjs";
-export function ebible_firebase_upload_path(file_name, bible_folder) {
+export function ebible_firebase_upload_path(bible_folder, file_name) {
   let file_name_with_extension = file_name_json(file_name);
   let joined = list_join_slash_forward(["bible", bible_folder]);
   let destination = list_join_slash_forward([joined, file_name_with_extension]);
