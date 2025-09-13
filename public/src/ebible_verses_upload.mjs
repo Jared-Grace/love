ec17c164a342c69e8dd4af5b43c6111ba09ac37c
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 17:49:32 -0400

    function_auto_path public\src\ebible_verses_upload.mjs

--- public/src/ebible_verses_upload.mjs
+++ public/src/ebible_verses_upload.mjs
@@ -6,7 +6,6 @@ import { object_merge } from "./object_merge.mjs";
 import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
 import { file_name_json } from "./file_name_json.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { log } from "./log.mjs";
 import { ebible_verses } from "./ebible_verses.mjs";
 import { ebible_chapter_codes } from "./ebible_chapter_codes.mjs";
 export async function ebible_verses_upload(bible_folder) {
@@ -14,7 +13,6 @@ export async function ebible_verses_upload(bible_folder) {
   async function lambda(chapter_code) {
     let verses = await ebible_verses(bible_folder, chapter_code);
     async function lambda2(v) {
-      log(v);
       let verse_number = object_property_get(v, "verse_number");
       let file_name = file_name_json(verse_number);
       let destination = list_join_slash_forward([
@@ -34,7 +32,6 @@ export async function ebible_verses_upload(bible_folder) {
         verse: merged,
       };
       await firebase_upload_object(object, destination);
-      log(destination);
     }
     let mapped = list_map(verses, lambda2);
     await list_wait(mapped);
