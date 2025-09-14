5274d666d2203b7a1610e4d12d88db229dc12e23
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:07:02 -0400

    function_auto_path public\src\html_loading.mjs

--- public/src/html_loading.mjs
+++ public/src/html_loading.mjs
@@ -1,4 +1,6 @@
+import { html_document_body } from "./html_document_body.mjs";
 import { marker } from "./marker.mjs";
 export function html_loading() {
   marker("1");
+  let component = html_document_body();
 }
