15cc196f2d0c3fe1dc21c82d805efbc07d6fe123
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:22:01 -0400

    function_auto_path public\src\html_style_assign.mjs

---
+++ public/src/html_style_assign.mjs
@@ -0,0 +1,8 @@
+import { object_assign } from "./object_assign.mjs";
+import { marker } from "./marker.mjs";
+import { html_component_element_get } from "./html_component_element_get.mjs";
+export function html_style_assign(b, s) {
+  marker("1");
+  let b_element = html_component_element_get(b);
+  object_assign(b_element.style, s);
+}
