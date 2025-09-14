93ef91f82af882eed62730791f883e8659f9537b
Grace-Jared <christrosetolife@gmail.com>
2025-09-13 20:10:06 -0400

    function_auto_path public\src\ebible_index_flat_upload.mjs

--- web/public/src/ebible_index_flat_upload.mjs
+++ web/public/src/ebible_index_flat_upload.mjs
@@ -1,11 +1,11 @@
+import { ebible_index_flat } from "./ebible_index_flat.mjs";
 import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
 import { marker } from "./marker.mjs";
 import { ebible_index_upload_name } from "./ebible_index_upload_name.mjs";
 import { ebible_firebase_upload } from "./ebible_firebase_upload.mjs";
-import { ebible_index } from "./ebible_index.mjs";
 export async function ebible_index_flat_upload(bible_folder) {
   marker("1");
-  let index = await ebible_index(bible_folder);
+  let index = await ebible_index_flat(bible_folder);
   let v = ebible_index_upload_name();
   let file_name = list_join_slash_forward([v, "flat"]);
   await ebible_firebase_upload(bible_folder, file_name, index);
