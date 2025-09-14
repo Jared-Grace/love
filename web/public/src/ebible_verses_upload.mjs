9d2bf90dd77ef94bb8a995753f76068327ee48d0
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:41:44 -0400

    function_auto_path public\src\ebible_verses_upload.mjs

--- web/public/src/ebible_verses_upload.mjs
+++ web/public/src/ebible_verses_upload.mjs
@@ -1,3 +1,4 @@
+import { ebible_version_download } from "./ebible_version_download.mjs";
 import { ebible_verses_upload_name } from "./ebible_verses_upload_name.mjs";
 import { ebible_firebase_upload } from "./ebible_firebase_upload.mjs";
 import { ebible_chapters_each_verses } from "./ebible_chapters_each_verses.mjs";
@@ -6,6 +7,7 @@ import { list_map } from "./list_map.mjs";
 import { object_merge } from "./object_merge.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export async function ebible_verses_upload(bible_folder) {
+  await ebible_version_download(bible_folder);
   await ebible_chapters_each_verses(bible_folder, each_chapter);
   async function each_chapter(chapter_code, verses) {
     async function lambda(v) {
