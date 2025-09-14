bd1ee2bdf8cb8e6958e71523fc24c73c3c884f51
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:22:02 -0400

    r html_style_merge html_style_assign

--- public/src/html_style_merge.mjs
+++
@@ -1,8 +0,0 @@
-import { object_assign } from "./object_assign.mjs";
-import { marker } from "./marker.mjs";
-import { html_component_element_get } from "./html_component_element_get.mjs";
-export function html_style_merge(b, s) {
-  marker("1");
-  let b_element = html_component_element_get(b);
-  object_assign(b_element.style, s);
-}
