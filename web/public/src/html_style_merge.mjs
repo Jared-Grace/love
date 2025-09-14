f6aa117d387be89416719848c1a1ef6ffb8da28f
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:21:53 -0400

    function_auto_path public\src\html_style_merge.mjs

--- web/public/src/html_style_merge.mjs
+++ web/public/src/html_style_merge.mjs
@@ -1,8 +1,8 @@
-import { object_merge } from "./object_merge.mjs";
+import { object_assign } from "./object_assign.mjs";
 import { marker } from "./marker.mjs";
 import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_style_merge(b, s) {
   marker("1");
   let b_element = html_component_element_get(b);
-  object_merge(b_element.style, s);
+  object_assign(b_element.style, s);
 }
