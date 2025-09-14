ef8bec5292373110d752c54632cd385d336d337d
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 16:43:40 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { ebible_verse_download } from "./ebible_verse_download.mjs";
 import { list_join_space } from "./list_join_space.mjs";
@@ -72,6 +73,7 @@ export async function app_reply_main() {
     let chapter_code2 = object_property_get(verse, "chapter_code");
     let verse_number2 = object_property_get(verse, "verse_number");
     let u = await ebible_verse_download(ur, chapter_code2, verse_number2);
+    log(message);
     list_add_first(verses, u);
   }
   let component3 = html_button(root, "Reset", lambda4);
