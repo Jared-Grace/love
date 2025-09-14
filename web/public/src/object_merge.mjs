75f6907d64994c19690a60dfa9500795e1c84d9b
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:19:27 -0400

    function_auto_path public\src\object_merge.mjs

--- web/public/src/object_merge.mjs
+++ web/public/src/object_merge.mjs
@@ -1,21 +1,6 @@
-import { error } from "./error.mjs";
-import { each } from "./each.mjs";
-import { object_properties } from "./object_properties.mjs";
-import { object_property_exists } from "./object_property_exists.mjs";
-import { object_property_set } from "./object_property_set.mjs";
-import { object_property_get } from "./object_property_get.mjs";
+import { object_merge_generic } from "./object_merge_generic.mjs";
 export function object_merge(to, from) {
   let strict = true;
-  function lambda(p) {
-    if (strict) {
-      if (object_property_exists(to, p)) {
-        error();
-      }
-    }
-    let value = object_property_get(from, p);
-    object_property_set(to, p, value);
-  }
-  let list = object_properties(from);
-  each(list, lambda);
+  object_merge_generic(strict, to, from);
   return to;
 }
