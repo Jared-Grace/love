5aedeb913ad2ddf074478379cd3bcc841657d387
Grace-Jared <christrosetolife@gmail.com>
2025-09-14 17:30:01 -0400

    function_auto_path public\src\string_take.mjs

--- web/public/src/string_take.mjs
+++ web/public/src/string_take.mjs
@@ -1,5 +1,5 @@
 import { string_slice } from "./string_slice.mjs";
-export function string_take(href, count) {
-  let taken = string_slice(href, 0, count);
+export function string_take(s, count) {
+  let taken = string_slice(s, 0, count);
   return taken;
 }
