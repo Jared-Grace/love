e78c0f7c9f9c11a0490056faa41192535b34c9b6
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 16:57:31 -0400

    function_auto_path public\src\app_reply_choices.mjs

--- public/src/app_reply_choices.mjs
+++ public/src/app_reply_choices.mjs
@@ -253,7 +253,7 @@ export function app_reply_choices() {
     {
       text: emoji_pray() + " Testimony",
       response:
-        emoji_pray() +
+        emoji_cross() +
         " Jesus died for my sins. Jesus was buried. Jesus rose to life. I confessed this truth since I was about 7 years old. But I never decided to fully obey Jesus as LORD until I was about 26 years old. Then eventually God called me to minstry. " +
         emoji_cross() +
         emoji_smile(),
