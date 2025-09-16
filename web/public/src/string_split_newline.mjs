ec2884b8cdd418dd4fbbaf4bbac88650a08691a2
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:12:23 -0400

    c string_split_comma string_split_newline

--- web/public/src/string_split_newline.mjs
+++ web/public/src/string_split_newline.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { string_split } from "./string_split.mjs";
 export function string_split_newline(f_names) {
+  marker("1");
   let split = string_split(f_names, ",");
   return split;
 }
