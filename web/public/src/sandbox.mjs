20789b08ee639eacfeae19aa389b6d47133cc71d
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 14:41:45 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -36,7 +36,8 @@ export async function sandbox() {
   let v2 = ebible_folder_urdu();
   let bible_folders = [v2, bible_folder];
   let books = await ebible_version_books(bible_folder);
-  let mapped5 = list_map(list2, function lambda5(item2) {});
+  function lambda5(item2) {}
+  let mapped5 = list_map(list2, lambda5);
   let mapped = list_map_property(books, "text");
   let file_path = "C:\\Users\\chris\\Documents\\god_created_man_why.txt";
   let contents = await file_read(file_path);
