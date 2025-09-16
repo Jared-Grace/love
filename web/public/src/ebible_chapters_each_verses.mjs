2f754c260236c57a3e3c53c6089c86da3c158f95
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 18:09:41 -0400

    function_auto_path public\src\ebible_chapters_each_verses.mjs

--- web/public/src/ebible_chapters_each_verses.mjs
+++ web/public/src/ebible_chapters_each_verses.mjs
@@ -1,13 +1,6 @@
-import { ebible_verses } from "./ebible_verses.mjs";
-import { each_async } from "./each_async.mjs";
+import { ebible_chapters_each_verses_list } from "./ebible_chapters_each_verses_list.mjs";
 import { ebible_chapter_codes } from "./ebible_chapter_codes.mjs";
-import { log_keep } from "./log_keep.mjs";
 export async function ebible_chapters_each_verses(bible_folder, each_chapter) {
   let list = await ebible_chapter_codes(bible_folder);
-  await each_async(list, lambda);
-  async function lambda(chapter_code) {
-    log_keep(chapter_code);
-    let verses = await ebible_verses(bible_folder, chapter_code);
-    await each_chapter(chapter_code, verses);
-  }
+  await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
 }
