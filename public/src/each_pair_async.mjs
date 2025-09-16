dbac51a7d368641c129ce4648345aee7039bc0b9
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 23:01:29 -0400

    cs each_pair async

--- public/src/each_pair_async.mjs
+++ public/src/each_pair_async.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { each_multiple } from "./each_multiple.mjs";
 export function each_pair_async(list_a, list_b, lambda$a$b) {
+  marker("1");
   let lists = [list_a, list_b];
   each_multiple(lists, lambda);
   function lambda(items) {
