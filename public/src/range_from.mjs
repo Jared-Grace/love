bef5d044e40721f384c1a75fdb8814e57c7c2f72
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 14:21:17 -0400

    function_auto_path public\src\range_from.mjs

--- public/src/range_from.mjs
+++ public/src/range_from.mjs
@@ -12,7 +12,7 @@ export function range_from(from, to) {
   let count = to - from + 1;
   let r = range(count);
   function lambda(item) {
-    let s = add(from, item);
+    let s = add(from, item) - 1;
     return s;
   }
   let mapped = list_map(r, lambda);
