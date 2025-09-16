579f829df6c0fc5710d0ea03d1a952b5200dd560
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:19:46 -0400

    function_auto_path public\src\list_map_prefix_without_any.mjs

--- web/public/src/list_map_prefix_without_any.mjs
+++ web/public/src/list_map_prefix_without_any.mjs
@@ -1,11 +1,11 @@
-import { list_filter_starts_with_any } from "./list_filter_starts_with_any.mjs";
+import { list_any_starts_with } from "./list_any_starts_with.mjs";
 import { marker } from "./marker.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 export function list_map_prefix_without_any(list, prefixes) {
   marker("1");
   function lambda(item) {
-    let verse_references = list_filter_starts_with_any(prefixes2, list2);
+    let verse_references = list_any_starts_with(prefixes, list);
     let result = string_prefix_without(item, prefix);
     return result;
   }
