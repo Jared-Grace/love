98af503f7d3f3e7b05481eab570a24edb090f30a
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:46:37 -0400

    function_auto_path public\src\each_range_from_async.mjs

--- web/public/src/each_range_from_async.mjs
+++ web/public/src/each_range_from_async.mjs
@@ -1,8 +1,10 @@
+import { each_async } from "./each_async.mjs";
 import { range_from } from "./range_from.mjs";
 import { marker } from "./marker.mjs";
 import { each } from "./each.mjs";
-export function each_range_from_async(from, to, lambda) {
+export async function each_range_from_async(from, to, lambda) {
   marker("1");
   let list = range_from(from, to);
   each(list, lambda);
+  await each_async(list2, async function lambda2(item) {});
 }
