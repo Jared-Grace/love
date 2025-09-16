2950ce2d242741a6a3e4768cf4493563273b7ec5
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 12:42:07 -0400

    function_auto_path public\src\each_pair_async.mjs

--- web/public/src/each_pair_async.mjs
+++ web/public/src/each_pair_async.mjs
@@ -4,8 +4,8 @@ export async function each_pair_async(list_a, list_b, lambda$a$b) {
   marker("1");
   let lists = [list_a, list_b];
   await each_multiple_async(lists, lambda);
-  function lambda(items) {
+  async function lambda(items) {
     let [a, b] = items;
-    lambda$a$b(a, b);
+    await lambda$a$b(a, b);
   }
 }
