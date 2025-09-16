68da9a6d405578722c3c01b33e791092f44399b8
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:22:51 -0400

    function_auto_path public\src\list_find_starts_with.mjs

--- web/public/src/list_find_starts_with.mjs
+++ web/public/src/list_find_starts_with.mjs
@@ -1,12 +1,12 @@
 import { marker } from "./marker.mjs";
-import { list_any } from "./list_any.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
+import { list_find } from "./list_find.mjs";
 export function list_find_starts_with(item, prefixes) {
   marker("1");
   function lambda2(item2) {
     let sw = string_starts_with(item, item2);
     return sw;
   }
-  let any = list_any(prefixes, lambda2);
+  let any = list_find(prefixes, lambda2);
   return any;
 }
