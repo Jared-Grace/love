d9d15c0cc4ae8fada0ef8e522f12a7bfb0abebb5
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 08:41:39 -0400

    data_file_update public\src\retry.mjs

--- public/src/retry.mjs
+++ public/src/retry.mjs
@@ -1,4 +1,5 @@
 import { each_range_async } from "./each_range_async.mjs";
 export async function retry() {
-  await each_range_async(count, async function lambda2() {});
+  async function lambda2() {}
+  await each_range_async(count, lambda2);
 }
