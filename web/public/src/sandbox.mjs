c843786dfe43c8ff9cbca8d2b5e1f27144598464
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:07:32 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,6 +1,6 @@
 import { ebible_version_books } from "./ebible_version_books.mjs";
 import { ebible_folder_english } from "./ebible_folder_english.mjs";
 export async function sandbox() {
-  let v = ebible_folder_english();
+  let bible_folder = ebible_folder_english();
   let books = await ebible_version_books(bible_folder);
 }
