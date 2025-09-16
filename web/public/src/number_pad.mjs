38a826702f84c04a460faa8c33196e761b4ff51c
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:49:51 -0400

    function_auto_path public\src\number_pad.mjs

--- web/public/src/number_pad.mjs
+++ web/public/src/number_pad.mjs
@@ -1,4 +1,6 @@
 import { marker } from "./marker.mjs";
-export function number_pad() {
+export function number_pad(num) {
   marker("1");
+  let v = String(num).padStart(length, "0");
+  return v;
 }
