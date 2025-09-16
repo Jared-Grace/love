8db8004a2b0f2587b8dcc32d234deb9d4a52d5a9
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:14:42 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_map_property } from "./list_map_property.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { file_read } from "./file_read.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
@@ -6,10 +7,11 @@ import { ebible_folder_english } from "./ebible_folder_english.mjs";
 export async function sandbox() {
   let bible_folder = ebible_folder_english();
   let books = await ebible_version_books(bible_folder);
+  let mapped = list_map_property(list, property_name);
   let file_path = "C:\\Users\\chris\\Documents\\god_created_man_why.txt";
   let contents = await file_read(file_path);
   let split = string_split_newline(contents);
   function lambda(item) {}
-  let filtered = list_filter(list, lambda);
+  let filtered = list_filter(split, lambda);
   return split;
 }
