cb2ad95facbf16646c19ef157b1d2606cc94d66b
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:27:54 -0400

    function_auto_path public\src\list_map_string_empty_not_is.mjs

--- web/public/src/list_map_string_empty_not_is.mjs
+++ web/public/src/list_map_string_empty_not_is.mjs
@@ -3,10 +3,8 @@ import { list_map } from "./list_map.mjs";
 import { string_empty_not_is } from "./string_empty_not_is.mjs";
 export function list_map_string_empty_not_is(mapped3) {
   function lambda(item) {
-    let ne = string_empty_not_is(item);
-    function lambda2(item2) {}
-    let filtered = list_filter(list, lambda2);
-    return ne;
+    let filtered = list_filter(list, string_empty_not_is);
+    return filtered;
   }
   let mapped4 = list_map(mapped3, lambda);
   return mapped4;
