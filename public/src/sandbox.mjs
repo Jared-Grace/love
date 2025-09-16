5bc33c2d48a74cc42ae52bdc1374e292ccb8e6cb
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:28:49 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "./list_map.mjs";
 import { list_map_filter_string_empty_not_is } from "./list_map_filter_string_empty_not_is.mjs";
 import { list_map_split_space } from "./list_map_split_space.mjs";
 import { list_map_prefix_without_any } from "./list_map_prefix_without_any.mjs";
@@ -20,5 +21,6 @@ export async function sandbox() {
   let mapped2 = list_map_prefix_without_any(verse_references, mapped);
   let mapped3 = list_map_split_space(mapped2);
   let mapped4 = list_map_filter_string_empty_not_is(mapped3);
+  let mapped5 = list_map(list, function lambda(item) {});
   return mapped4;
 }
