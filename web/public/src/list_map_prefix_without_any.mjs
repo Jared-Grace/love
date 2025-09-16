e6ac12f8a427077db7f68813a1b0ab982acc5d47
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:24:23 -0400

    function_auto_path public\src\list_map_prefix_without_any.mjs

--- web/public/src/list_map_prefix_without_any.mjs
+++ web/public/src/list_map_prefix_without_any.mjs
@@ -1,11 +1,11 @@
-import { list_any_starts_with } from "./list_any_starts_with.mjs";
+import { list_find_starts_with } from "./list_find_starts_with.mjs";
 import { marker } from "./marker.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 export function list_map_prefix_without_any(list, prefixes) {
   marker("1");
   function lambda(item) {
-    let verse_references = list_any_starts_with(prefixes, list);
+    let prefix = list_find_starts_with(item, prefixes);
     let result = string_prefix_without(item, prefix);
     return result;
   }
