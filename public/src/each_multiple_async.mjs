e562dab6d29ad6ec9ccb52c5480afb45cdb4fa2d
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 23:01:48 -0400

    function_auto_path public\src\each_multiple_async.mjs

---
+++ public/src/each_multiple_async.mjs
@@ -0,0 +1,11 @@
+import { lists_get } from "./lists_get.mjs";
+import { each_index } from "./each_index.mjs";
+import { list_first } from "./list_first.mjs";
+export function each_multiple_async(lists, lambda) {
+  let first = list_first(lists);
+  function lambda2(item, index) {
+    let items = lists_get(lists, index);
+    lambda(items);
+  }
+  each_index(first, lambda2);
+}
