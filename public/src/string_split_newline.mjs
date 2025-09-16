20ed38c80cb9e40b0819d9f01cfd20c2f8021a4f
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:12:39 -0400

    function_auto_path public\src\string_split_newline.mjs

--- public/src/string_split_newline.mjs
+++ public/src/string_split_newline.mjs
@@ -1,7 +1,9 @@
+import { newline } from "./newline.mjs";
 import { marker } from "./marker.mjs";
 import { string_split } from "./string_split.mjs";
 export function string_split_newline(f_names) {
   marker("1");
-  let split = string_split(f_names, ",");
+  let separator = newline();
+  let split = string_split(f_names, separator);
   return split;
 }
