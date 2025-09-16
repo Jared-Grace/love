239e22b9643d1ca8264649627d4b3e90cfb57478
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 12:40:36 -0400

    function_auto_path public\src\list_adder_async.mjs

--- web/public/src/list_adder_async.mjs
+++ web/public/src/list_adder_async.mjs
@@ -1,7 +1,9 @@
+import { log } from "./log.mjs";
 import { list_add } from "./list_add.mjs";
 export async function list_adder_async(lambda$la) {
   let list = [];
   function list_adder_inner(item) {
+    log(message);
     list_add(list, item);
   }
   await lambda$la(list_adder_inner);
