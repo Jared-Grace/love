e3f80f19a95ab83bc17d829e67102a3ff74da118
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:23:58 -0400

    function_auto_path public\src\list_map_prefix_without.mjs

--- web/public/src/list_map_prefix_without.mjs
+++ web/public/src/list_map_prefix_without.mjs
@@ -1,9 +1,7 @@
-import { list_find_starts_with } from "./list_find_starts_with.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 export function list_map_prefix_without(list, prefix) {
   function lambda(item) {
-    prefix = list_find_starts_with(item, prefixes);
     let result = string_prefix_without(item, prefix);
     return result;
   }
