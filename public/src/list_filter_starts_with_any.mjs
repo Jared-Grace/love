d11d46e72184046f933bf321566c64723ace1903
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:20:41 -0400

    function_auto_path public\src\list_filter_starts_with_any.mjs

--- public/src/list_filter_starts_with_any.mjs
+++ public/src/list_filter_starts_with_any.mjs
@@ -1,5 +1,5 @@
 import { list_filter } from "./list_filter.mjs";
-export function list_filter_starts_with_any(prefixes, list) {
+export function list_filter_starts_with_any(item, prefixes) {
   function lambda(item) {}
   let verse_references = list_filter(list, lambda);
   return verse_references;
