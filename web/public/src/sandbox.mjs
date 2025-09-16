d1b8d019d02092bd8907595347315e8350edf428
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:11:18 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -3,4 +3,5 @@ import { ebible_folder_english } from "./ebible_folder_english.mjs";
 export async function sandbox() {
   let bible_folder = ebible_folder_english();
   let books = await ebible_version_books(bible_folder);
+  ("C:\\Users\\chris\\Documents\\god_created_man_why.txt");
 }
