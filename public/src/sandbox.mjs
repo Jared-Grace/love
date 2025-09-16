400154502e61430df08cc36e822dcd840fa1b6f2
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 18:19:30 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,8 @@
+import { list_map_property } from "./list_map_property.mjs";
 import { ebible_version_books_testament_apocrypha } from "./ebible_version_books_testament_apocrypha.mjs";
 import { ebible_books_to_chapter_codes } from "./ebible_books_to_chapter_codes.mjs";
 import { ebible_chapters_each_verses_list } from "./ebible_chapters_each_verses_list.mjs";
 import { ebible_chapters_each_verses_check } from "./ebible_chapters_each_verses_check.mjs";
-import { log } from "./log.mjs";
 export async function sandbox() {
   const bible_folder = "engwebu";
   await ebible_chapters_each_verses_check(bible_folder);
@@ -10,6 +10,6 @@ export async function sandbox() {
   let list = await ebible_books_to_chapter_codes(books, bible_folder);
   await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
   async function each_chapter(chapter_code, verses) {
-    log(chapter_code);
+    let mapped = list_map_property(list2, property_name);
   }
 }
