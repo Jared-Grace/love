793a15ca661f315dbffdb7815d409455968e0a02
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:42:39 -0400

    function_auto_path public\src\range_from.mjs

--- public/src/range_from.mjs
+++ public/src/range_from.mjs
@@ -2,5 +2,6 @@ import { range } from "./range.mjs";
 import { marker } from "./marker.mjs";
 export function range_from(count) {
   marker("1");
-  return range(count);
+  let r = range(count);
+  return r;
 }
