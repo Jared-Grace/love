9ff721a3c7990e5e987b0819017a2b1fef6c23da
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:44:42 -0400

    cs each_range from

--- public/src/each_range_from.mjs
+++ public/src/each_range_from.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { range } from "./range.mjs";
 import { each } from "./each.mjs";
 export function each_range_from(count, lambda) {
+  marker("1");
   let list = range(count);
   each(list, lambda);
 }
