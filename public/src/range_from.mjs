8f8939da5fb35ef37801456b95216fee21ed0021
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:42:42 -0400

    function_auto_path public\src\range_from.mjs

--- public/src/range_from.mjs
+++ public/src/range_from.mjs
@@ -1,7 +1,9 @@
+import { list_map } from "./list_map.mjs";
 import { range } from "./range.mjs";
 import { marker } from "./marker.mjs";
 export function range_from(count) {
   marker("1");
   let r = range(count);
+  let mapped = list_map(list, function lambda(item) {});
   return r;
 }
