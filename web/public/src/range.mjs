45fd389fb72669715cc4df6313385e64231998d8
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:45:11 -0400

    o rg

--- web/public/src/range.mjs
+++ web/public/src/range.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { list_add } from "./list_add.mjs";
 export function range(count) {
+  marker("1");
   let r = [];
   for (let i = 0; i < count; i++) {
     list_add(r, i);
