6e752b213b6b85c759b4c3792b8c71088d9e2633
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 09:39:47 -0400

    function_auto_path public\src\each_index_async.mjs

--- public/src/each_index_async.mjs
+++ public/src/each_index_async.mjs
@@ -1,11 +1,11 @@
 import { marker } from "./marker.mjs";
-import { each } from "./each.mjs";
-export function each_index_async(list, lambda$item$index) {
+import { each_async } from "./each_async.mjs";
+export async function each_index_async(list, lambda$item$index) {
   marker("1");
   let index = 0;
   function lambda2(item) {
     lambda$item$index(item, index);
     index++;
   }
-  each(list, lambda2);
+  await each_async(list, lambda2);
 }
