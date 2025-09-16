6968b2e8f0a413d355021a8da98eee4c8cfbc13b
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:42:05 -0400

    function_auto_path public\src\each_pair.mjs

--- public/src/each_pair.mjs
+++ public/src/each_pair.mjs
@@ -1,9 +1,9 @@
 import { each_multiple } from "./each_multiple.mjs";
-export function each_pair(list_a, list_b, lambda3) {
+export function each_pair(list_a, list_b, lambda$a$b) {
   let lists = [list_a, list_b];
   each_multiple(lists, lambda);
   function lambda(items) {
     let [a, b] = items;
-    lambda3(a, b);
+    lambda$a$b(a, b);
   }
 }
