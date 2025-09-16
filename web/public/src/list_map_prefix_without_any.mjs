8e1bfedc710afbe82fc761e84be27791ed1d9e18
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:18:32 -0400

    cs list_map_prefix_without any

--- web/public/src/list_map_prefix_without_any.mjs
+++ web/public/src/list_map_prefix_without_any.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_map } from "./list_map.mjs";
 import { string_prefix_without } from "./string_prefix_without.mjs";
 export function list_map_prefix_without_any(list, prefix) {
+  marker("1");
   function lambda(item) {
     let result = string_prefix_without(item, prefix);
     return result;
