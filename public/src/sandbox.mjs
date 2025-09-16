cbf804e1f96b19f201878a7600f5ab51215f3988
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 18:10:18 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,9 +3,10 @@ import { ebible_chapters_each_verses_check } from "./ebible_chapters_each_verses
 import { log } from "./log.mjs";
 import { ebible_chapters_each_verses_check_with } from "./ebible_chapters_each_verses_check_with.mjs";
 export async function sandbox() {
+  const bible_folder = "engwebu";
   await ebible_chapters_each_verses_check(bible_folder);
   await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
-  await ebible_chapters_each_verses_check_with("engwebu", each_chapter);
+  await ebible_chapters_each_verses_check_with(bible_folder, each_chapter);
   async function each_chapter(chapter_code, verses) {
     log(verses);
   }
