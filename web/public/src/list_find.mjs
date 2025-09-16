c72f278749b337aca6ac1ac67874990275e61110
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:22:57 -0400

    function_auto_path public\src\list_find.mjs

--- web/public/src/list_find.mjs
+++ web/public/src/list_find.mjs
@@ -1,7 +1,7 @@
 import { list_filter } from "./list_filter.mjs";
 import { list_single } from "./list_single.mjs";
-export function list_find(list, filter) {
-  let filtered = list_filter(list, filter);
+export function list_find(list, predicate) {
+  let filtered = list_filter(list, predicate);
   let only = list_single(filtered);
   return only;
 }
