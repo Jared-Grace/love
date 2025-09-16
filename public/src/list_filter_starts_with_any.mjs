fb8fc14905a5ef2e1f3d1e7d86d571e917c6ce7e
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:19:37 -0400

    function_auto_path public\src\list_filter_starts_with_any.mjs

--- public/src/list_filter_starts_with_any.mjs
+++ public/src/list_filter_starts_with_any.mjs
@@ -2,7 +2,7 @@ import { list_filter } from "./list_filter.mjs";
 import { list_any } from "./list_any.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export function list_filter_starts_with_any(prefixes, list) {
-  function lambda(item) {
+  function list_any_starts_with(item) {
     function lambda2(item2) {
       let sw = string_starts_with(item, item2);
       return sw;
@@ -10,6 +10,6 @@ export function list_filter_starts_with_any(prefixes, list) {
     let any = list_any(prefixes, lambda2);
     return any;
   }
-  let verse_references = list_filter(list, lambda);
+  let verse_references = list_filter(list, list_any_starts_with);
   return verse_references;
 }
