e3ac8c796272a69e0ee92d3404899bb25e93f33d
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 09:39:38 -0400

    cs each_index async

--- public/src/each_index_async.mjs
+++ public/src/each_index_async.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { each } from "./each.mjs";
 export function each_index_async(list, lambda$item$index) {
+  marker("1");
   let index = 0;
   function lambda2(item) {
     lambda$item$index(item, index);
