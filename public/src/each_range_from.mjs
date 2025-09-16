58adab0c35cdfeec6717747a98aa0125877bb1bb
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 22:45:46 -0400

    function_auto_path public\src\each_range_from.mjs

--- public/src/each_range_from.mjs
+++ public/src/each_range_from.mjs
@@ -1,8 +1,8 @@
-import { each_range } from "./each_range.mjs";
+import { range_from } from "./range_from.mjs";
 import { marker } from "./marker.mjs";
 import { each } from "./each.mjs";
 export function each_range_from(count, lambda) {
   marker("1");
-  let list = each_range(count);
+  let list = range_from(count);
   each(list, lambda);
 }
