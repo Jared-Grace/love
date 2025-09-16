7de60795802f52634169b061821e09fa0a749da4
Grace-Jared <christrosetolife@gmail.com>
2025-09-15 20:22:10 -0400

    c list_any_starts_with list_find_starts_with

--- web/public/src/list_find_starts_with.mjs
+++ web/public/src/list_find_starts_with.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_any } from "./list_any.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 export function list_find_starts_with(item, prefixes) {
+  marker("1");
   function lambda2(item2) {
     let sw = string_starts_with(item, item2);
     return sw;
