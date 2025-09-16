f44afd986af24b7904451f6771049e7b6032094a
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 08:43:37 -0400

    function_auto_path public\src\retry.mjs

--- web/public/src/retry.mjs
+++ web/public/src/retry.mjs
@@ -2,9 +2,9 @@ import { each_range_async } from "./each_range_async.mjs";
 export async function retry(lambda) {
   let count = 5;
   let wait = 1000;
+  let result = null;
   async function lambda2() {
     try {
-      let result = null;
       result = await lambda();
       let v = true;
       return v;
