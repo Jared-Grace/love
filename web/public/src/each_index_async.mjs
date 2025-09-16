329004372124891f58a3bb8653b366b185036af1
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 12:42:29 -0400

    function_auto_path public\src\each_index_async.mjs

--- web/public/src/each_index_async.mjs
+++ web/public/src/each_index_async.mjs
@@ -3,8 +3,8 @@ import { each_async } from "./each_async.mjs";
 export async function each_index_async(list, lambda$item$index) {
   marker("1");
   let index = 0;
-  function lambda2(item) {
-    lambda$item$index(item, index);
+  async function lambda2(item) {
+    await lambda$item$index(item, index);
     index++;
   }
   await each_async(list, lambda2);
