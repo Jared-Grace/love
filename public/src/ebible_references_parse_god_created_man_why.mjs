ba1416d8dc2bbfdbfc3f37b24a147067c0189777
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 17:56:12 -0400

    function_auto_path public\src\sandbox.mjs

---
+++ public/src/ebible_references_parse_god_created_man_why.mjs
@@ -0,0 +1,10 @@
+import { ebible_references_parse } from "./ebible_references_parse.mjs";
+import { ebible_folder_english } from "./ebible_folder_english.mjs";
+import { ebible_folder_urdu } from "./ebible_folder_urdu.mjs";
+export async function ebible_references_parse_god_created_man_why() {
+  let v2 = ebible_folder_urdu();
+  let v = ebible_folder_english();
+  let file_path = "C:\\Users\\chris\\Documents\\god_created_man_why.txt";
+  let bible_folders = [v2, v];
+  let list = await ebible_references_parse(bible_folders, file_path);
+}
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,13 +1,6 @@
-import { ebible_references_parse } from "./ebible_references_parse.mjs";
-import { ebible_folder_urdu } from "./ebible_folder_urdu.mjs";
+import { ebible_references_parse_god_created_man_why } from "./ebible_references_parse_god_created_man_why.mjs";
 import { marker } from "./marker.mjs";
-import { ebible_folder_english } from "./ebible_folder_english.mjs";
 export async function sandbox() {
   marker("1");
-  let v2 = ebible_folder_urdu();
-  let v = ebible_folder_english();
-  let file_path = "C:\\Users\\chris\\Documents\\god_created_man_why.txt";
-  let bible_folders = [v2, v];
-  let list = await ebible_references_parse(bible_folders, file_path);
-  return list;
+  await ebible_references_parse_god_created_man_why();
 }
