0dc89b1de134c38b8b1734514a7395595ca3ae7b
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 09:20:25 -0400

    function_auto_path public\src\retry.mjs

--- public/src/retry.mjs
+++ public/src/retry.mjs
@@ -3,8 +3,7 @@ import { error_json } from "./error_json.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { log } from "./log.mjs";
 import { each_range_async } from "./each_range_async.mjs";
-export async function retry(lambda) {
-  let count = 5;
+export async function retry(count, lambda) {
   let wait = 1000;
   let result = null;
   let success = false;
