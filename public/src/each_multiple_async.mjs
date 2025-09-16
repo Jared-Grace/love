38fa8d34eee0272f33317983d938e077b8866158
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 12:42:18 -0400

    function_auto_path public\src\each_multiple_async.mjs

--- public/src/each_multiple_async.mjs
+++ public/src/each_multiple_async.mjs
@@ -5,9 +5,9 @@ import { list_first } from "./list_first.mjs";
 export async function each_multiple_async(lists, lambda) {
   marker("1");
   let first = list_first(lists);
-  function lambda2(item, index) {
+  async function lambda2(item, index) {
     let items = lists_get(lists, index);
-    lambda(items);
+    await lambda(items);
   }
   await each_index_async(first, lambda2);
 }
