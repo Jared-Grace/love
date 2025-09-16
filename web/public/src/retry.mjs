3eda8906507d0d81c44f79cfe0be77d7678e82b3
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 09:18:04 -0400

    function_auto_path public\src\retry.mjs

--- web/public/src/retry.mjs
+++ web/public/src/retry.mjs
@@ -7,20 +7,24 @@ export async function retry(lambda) {
   let wait = 1000;
   let result = null;
   let success = false;
-  async function lambda3(la) {}
-  let list = await list_adder_async(lambda3);
-  async function lambda2() {
-    try {
-      result = await lambda();
-      success = true;
-      return success;
-    } catch (e) {
-      log(message);
+  async function lambda3(la) {
+    async function lambda2() {
+      try {
+        result = await lambda();
+        success = true;
+        return success;
+      } catch (e) {
+        log(message);
+        la(e);
+      }
     }
+    await each_range_async(count, lambda2);
   }
-  await each_range_async(count, lambda2);
+  let errors = await list_adder_async(lambda3);
   if (success) {
     return result;
   }
-  error();
+  error({
+    errors,
+  });
 }
