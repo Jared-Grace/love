1e505dc2e2c59ca5d2f57e215d116fc15bd6b9d3
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 15:01:46 -0400

    function_auto_path public\src\file_delete_if_exists.mjs

--- public/src/file_delete_if_exists.mjs
+++ public/src/file_delete_if_exists.mjs
@@ -2,6 +2,7 @@ import { file_delete } from "./file_delete.mjs";
 import { marker } from "./marker.mjs";
 export async function file_delete_if_exists(file_path) {
   marker("1");
+  fe;
   if (false) {
   }
   let v = await file_delete(file_path);
