e45319ddc927fbc0c20f56e9668ca941418896ea
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 16:32:04 -0400

    function_auto_path public\src\command_line.mjs

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -1,3 +1,4 @@
+import { object_merge } from "./object_merge.mjs";
 import { exec } from "child_process";
 import { promisify } from "util";
 const execAsync = promisify(exec);
@@ -6,6 +7,7 @@ export async function command_line(command) {
   const options = {
     encoding: "utf8",
   };
+  object_merge(options, extra);
   const { stdout } = await execAsync(command, options);
   return stdout;
 }
