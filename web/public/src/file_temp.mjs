420a057e61846f3663a6fec0f42c3e96e89e1a7f
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 15:05:28 -0400

    function_auto_path public\src\sandbox_2.mjs

---
+++ web/public/src/file_temp.mjs
@@ -0,0 +1,13 @@
+import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
+import { folder_user_combine } from "./folder_user_combine.mjs";
+import { uuid } from "./uuid.mjs";
+export async function file_temp(lambda) {
+  let u = await uuid();
+  let temp_path = folder_user_combine("temp", u);
+  let result = null;
+  try {
+    result = await lambda(temp_path);
+  } finally {
+    await file_delete_if_exists(temp_path);
+  }
+}
