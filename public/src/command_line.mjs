3462992379f613e2c97e70c12e8e19fd02e46f48
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 16:27:49 -0400

    function_auto_path public\src\command_line.mjs

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -2,6 +2,7 @@ import { exec } from "child_process";
 import { promisify } from "util";
 const execAsync = promisify(exec);
 export async function command_line(command) {
+  let extra = {};
   const options = {
     encoding: "utf8",
   };
