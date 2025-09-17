d6f3eb033b12375a9879f2e6ae4096f6d821c0d8
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 21:33:56 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { range } from "./range.mjs";
 import { list_map } from "./list_map.mjs";
 import { equal } from "./equal.mjs";
@@ -7,7 +8,9 @@ export async function sandbox() {
   let right = 2;
   if (equal(left, right)) {
   }
-  let rr = range(count);
-  function lambda(item) {}
+  let list = range(count);
+  function lambda(item) {
+    object_property_set(object, property_name, value);
+  }
   let mapped = list_map(list, lambda);
 }
