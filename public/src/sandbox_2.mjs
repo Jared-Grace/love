c6d476028fc346bed5f8173d8d69104cdde820af
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 14:59:53 -0400

    function_auto_path public\src\sandbox_2.mjs

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,3 +1,4 @@
+import { folder_user_combine } from "./folder_user_combine.mjs";
 import { uuid } from "./uuid.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { log } from "./log.mjs";
@@ -10,6 +11,7 @@ import { command_line } from "./command_line.mjs";
 export async function sandbox_2() {
   const bible_folder = "engwebu";
   let u = await uuid();
+  let joined2 = folder_user_combine(folder, destination);
   let stdout = await command_line(
     "D:\\programs\\WPy64-312100\\python>python.exe ./py/kokoro.py " + file_path,
   );
