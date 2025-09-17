260cb1adde5d294a8938e5b9f66111448dd8e39c
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 16:39:17 -0400

    function_auto_path public\src\command_line.mjs

--- web/public/src/command_line.mjs
+++ web/public/src/command_line.mjs
@@ -1,7 +1,4 @@
 import { command_line_generic } from "./command_line_generic.mjs";
-import { exec } from "child_process";
-import { promisify } from "util";
-const execAsync = promisify(exec);
 export async function command_line(command) {
   let extra = {};
   const stdout = await command_line_generic(command, extra);
