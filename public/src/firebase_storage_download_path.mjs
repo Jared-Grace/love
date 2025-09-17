62598e4b99e2fcafbd9bbbef1c1431050a854446
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 14:58:46 -0400

    function_auto_path public\src\firebase_storage_download_path.mjs

--- public/src/firebase_storage_download_path.mjs
+++ public/src/firebase_storage_download_path.mjs
@@ -1,5 +1,6 @@
 import { path_join } from "./path_join.mjs";
 export function firebase_storage_download_path(destination) {
-  let joined = path_join(["D:\\user", "storage", destination]);
+  let result = path_join(["D:\\user", "storage"]);
+  let joined = path_join([result, destination]);
   return joined;
 }
