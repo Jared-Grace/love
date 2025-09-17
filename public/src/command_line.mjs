ea53ad7870b8098df7cb77c5df6237aced74bbf3
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 16:39:34 -0400

    p

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { command_line_generic } from "./command_line_generic.mjs";
 export async function command_line(command) {
+  marker("1");
   let extra = {};
   const stdout = await command_line_generic(command, extra);
   return stdout;
