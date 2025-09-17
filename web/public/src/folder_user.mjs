6db0b8f8e2b45fcd5b850efa2f591eae713f31d8
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 15:36:08 -0400

    function_auto_path public\src\folder_user.mjs

--- web/public/src/folder_user.mjs
+++ web/public/src/folder_user.mjs
@@ -1,5 +1,5 @@
 import { path_join } from "./path_join.mjs";
 export function folder_user(folder) {
-  let result2 = path_join(["D:\\user", folder]);
-  return result2;
+  let f = path_join(["D:\\user", folder]);
+  return f;
 }
