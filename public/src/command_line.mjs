a6529bf64bf1a739c04d24af6ceb4cd6479c97f6
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 16:25:01 -0400

    function_auto_path public\src\command_line.mjs

--- public/src/command_line.mjs
+++ public/src/command_line.mjs
@@ -2,8 +2,9 @@ import { exec } from "child_process";
 import { promisify } from "util";
 const execAsync = promisify(exec);
 export async function command_line(command) {
-  const { stdout } = await execAsync(command, {
+  const options = {
     encoding: "utf8",
-  });
+  };
+  const { stdout } = await execAsync(command, options);
   return stdout;
 }
