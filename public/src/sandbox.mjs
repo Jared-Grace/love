df4681fac81c4ff1374e9fc492ecd77bb9d77242
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 21:28:50 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,19 +1,4 @@
-import { command_line } from "./command_line.mjs";
-import { log } from "./log.mjs";
-import { list_map_property } from "./list_map_property.mjs";
-import { ebible_version_books_testament_apocrypha } from "./ebible_version_books_testament_apocrypha.mjs";
-import { ebible_books_to_chapter_codes } from "./ebible_books_to_chapter_codes.mjs";
-import { ebible_chapters_each_verses_list } from "./ebible_chapters_each_verses_list.mjs";
-import { ebible_chapters_each_verses_check } from "./ebible_chapters_each_verses_check.mjs";
+import { sandbox_2 } from "./sandbox_2.mjs";
 export async function sandbox() {
-  const bible_folder = "engwebu";
-  let stdout = await command_line("command");
-  await ebible_chapters_each_verses_check(bible_folder);
-  let books = await ebible_version_books_testament_apocrypha(bible_folder);
-  let list = await ebible_books_to_chapter_codes(books, bible_folder);
-  await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
-  async function each_chapter(chapter_code, verses) {
-    let mapped = list_map_property(verses, "text");
-    log(mapped);
-  }
+  await sandbox_2();
 }
---
+++ public/src/sandbox_2.mjs
@@ -0,0 +1,19 @@
+import { log } from "./log.mjs";
+import { list_map_property } from "./list_map_property.mjs";
+import { ebible_chapters_each_verses_list } from "./ebible_chapters_each_verses_list.mjs";
+import { ebible_books_to_chapter_codes } from "./ebible_books_to_chapter_codes.mjs";
+import { ebible_version_books_testament_apocrypha } from "./ebible_version_books_testament_apocrypha.mjs";
+import { ebible_chapters_each_verses_check } from "./ebible_chapters_each_verses_check.mjs";
+import { command_line } from "./command_line.mjs";
+export async function sandbox_2() {
+  const bible_folder = "engwebu";
+  let stdout = await command_line("command");
+  await ebible_chapters_each_verses_check(bible_folder);
+  let books = await ebible_version_books_testament_apocrypha(bible_folder);
+  let list = await ebible_books_to_chapter_codes(books, bible_folder);
+  await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
+  async function each_chapter(chapter_code, verses) {
+    let mapped = list_map_property(verses, "text");
+    log(mapped);
+  }
+}
