24c04d5024e67b6f1af326fe4c128d6a792f2226
Grace-Jared <christrosetolife@gmail.com>
2025-09-17 14:55:57 -0400

    function_auto_path public\src\uuid.mjs

--- web/public/src/uuid.mjs
+++ web/public/src/uuid.mjs
@@ -1,4 +1,8 @@
-import { marker } from "./marker.mjs";
-export function uuid() {
-  marker("1");
+import { log } from "./log.mjs";
+import { import_install } from "./import_install.mjs";
+export async function uuid() {
+  await import_install(name);
+  const { v4: uuidv4 } = import("uuid");
+  let v = uuidv4();
+  console.log(v);
 }
