65e5ebe52eb68a8e4c2ef66d14a2f6455b628059
Grace-Jared <christrosetolife@gmail.com>
2025-09-16 21:32:08 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/range.mjs
+++ web/public/src/range.mjs
@@ -2,9 +2,9 @@ import { marker } from "./marker.mjs";
 import { list_add } from "./list_add.mjs";
 export function range(count) {
   marker("1");
-  let r = [];
+  let rr = [];
   for (let i = 0; i < count; i++) {
-    list_add(r, i);
+    list_add(rr, i);
   }
-  return r;
+  return rr;
 }
