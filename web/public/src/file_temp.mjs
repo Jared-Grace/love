c8da51e6c969847532adaa609e26e6cf90f3a65a
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 11:32:49 -0400

    function_auto_path public\src\file_temp.mjs

--- web/public/src/file_temp.mjs
+++ web/public/src/file_temp.mjs
@@ -1,7 +1,7 @@
 import { file_delete_if_exists } from "./file_delete_if_exists.mjs";
 import { folder_user_combine } from "./folder_user_combine.mjs";
 import { uuid } from "./uuid.mjs";
-export async function file_temp(lambda) {
+export async function file_temp(lambda$temp_path) {
   let u = await uuid();
   let temp_path = folder_user_combine("temp", u);
   let result = null;
