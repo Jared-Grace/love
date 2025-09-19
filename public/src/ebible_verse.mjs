9cf57e6ab203447704da7d002ee11d5e12e42436
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:15:35 -0400

    data_file_update public\src\ebible_verse.mjs

---
+++ public/src/ebible_verse.mjs
@@ -0,0 +1,7 @@
+import { ebible_verses } from "./ebible_verses.mjs";
+import { marker } from "./marker.mjs";
+export async function ebible_verse(bible_folder, chapter_code) {
+  marker("1");
+  let v = await ebible_verses(bible_folder, chapter_code);
+  return v;
+}
