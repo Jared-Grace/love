9bf8dd08abbb8f53e837df61cc753acba573b343
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:31:59 -0400

    function_auto_path public\src\ebible_verse.mjs

--- public/src/ebible_verse.mjs
+++ public/src/ebible_verse.mjs
@@ -7,13 +7,15 @@ import { ebible_verses } from "./ebible_verses.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_verse(language_codes, chapter_code, verse_number) {
   marker("1");
-  async function lambda(item) {}
-  let waited = await list_map_unordered_async(list, lambda);
-  chapter_code = ebible_chapter_code_normalize(chapter_code);
-  let languages = ebible_languages();
-  let l = list_find_property(languages, "language_code", language_code);
-  let bible_folder = object_property_get(l, "bible_folder");
-  let vs = await ebible_verses(bible_folder, chapter_code);
-  let v = list_find_property(vs, "verse_number", verse_number);
-  return v;
+  async function lambda(language_code) {
+    chapter_code = ebible_chapter_code_normalize(chapter_code);
+    let languages = ebible_languages();
+    let l = list_find_property(languages, "language_code", language_code);
+    let bible_folder = object_property_get(l, "bible_folder");
+    let vs = await ebible_verses(bible_folder, chapter_code);
+    let v = list_find_property(vs, "verse_number", verse_number);
+    return v;
+  }
+  let waited = await list_map_unordered_async(language_codes, lambda);
+  return waited;
 }
