9b3a8b2537a9827f858af85bb69d80813c45797b
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 13:10:47 -0400

    function_auto_path public\src\ebible_version_books_upload.mjs

--- public/src/ebible_version_books_upload.mjs
+++ public/src/ebible_version_books_upload.mjs
@@ -1,10 +1,12 @@
+import { ebible_version_books_upload_name } from "./ebible_version_books_upload_name.mjs";
 import { ebible_firebase_upload } from "./ebible_firebase_upload.mjs";
 import { ebible_version_books } from "./ebible_version_books.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_version_books_upload(bible_folder) {
   marker("1");
   let books = await ebible_version_books(bible_folder);
-  await ebible_firebase_upload(bible_folder, "books", {
+  let file_name = ebible_version_books_upload_name();
+  await ebible_firebase_upload(bible_folder, file_name, {
     books,
   });
   return books;
