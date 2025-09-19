e1d60544acecbfb17dfd27061d381795c0f86bc8
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:14:36 -0400

    o ebible_verse

--- public/src/ebible_verse.mjs
+++ public/src/ebible_verse.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export function ebible_verse(tokens, verse_number) {
+  marker("1");
   let joined = list_join_space(tokens);
   const v = {
     verse_number,
