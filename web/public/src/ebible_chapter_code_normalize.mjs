f399f48154d62b3e1c243bdedc355c6e6508e468
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:26:59 -0400

    function_auto_path public\src\ebible_chapter_code_normalize.mjs

--- web/public/src/ebible_chapter_code_normalize.mjs
+++ web/public/src/ebible_chapter_code_normalize.mjs
@@ -1,5 +1,6 @@
 import { string_upper_to } from "./string_upper_to.mjs";
 export function ebible_chapter_code_normalize(chapter_code) {
+  "consider doing more like mk mark";
   let u = string_upper_to(chapter_code);
   return u;
 }
