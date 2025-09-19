3121fe7fa255ede6994d4afebcb28babd3e2f050
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 13:10:27 -0400

    function_auto_path public\src\ebible_verse_download.mjs

--- public/src/ebible_verse_download.mjs
+++ public/src/ebible_verse_download.mjs
@@ -1,5 +1,4 @@
-import { firebase_storage_download_json } from "./firebase_storage_download_json.mjs";
-import { ebible_firebase_upload_path } from "./ebible_firebase_upload_path.mjs";
+import { firebase_storage_download_ebible } from "./firebase_storage_download_ebible.mjs";
 import { ebible_verses_upload_name } from "./ebible_verses_upload_name.mjs";
 export async function ebible_verse_download(
   bible_folder,
@@ -7,7 +6,6 @@ export async function ebible_verse_download(
   verse_number,
 ) {
   let n = ebible_verses_upload_name(chapter_code, verse_number);
-  let destination2 = ebible_firebase_upload_path(bible_folder, n);
-  let verse = await firebase_storage_download_json(destination2);
-  return verse;
+  let index = await firebase_storage_download_ebible(bible_folder, n);
+  return index;
 }
