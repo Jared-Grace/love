cca1ac1f1b7178bbd447db7dd8d9b5543236e010
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 05:56:39 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,7 @@
+import { each_async } from "./each_async.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
+  let versions = ["engbsb", "urdgvu"];
+  await each_async(list, async function lambda(item) {});
 }
