2b50975290c550d0c2cfc5b147c4b132b5f8a965
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:24:29 -0400

    function_auto_path public\src\list_filter_property.mjs

--- public/src/list_filter_property.mjs
+++ public/src/list_filter_property.mjs
@@ -2,6 +2,6 @@ import { object_property_equals_lambda } from "./object_property_equals_lambda.m
 import { list_filter } from "./list_filter.mjs";
 export function list_filter_property(list, property_name, property_value) {
   let filter = object_property_equals_lambda(property_name, property_value);
-  let result = list_filter(list, filter);
-  return result;
+  let filtered = list_filter(list, filter);
+  return filtered;
 }
