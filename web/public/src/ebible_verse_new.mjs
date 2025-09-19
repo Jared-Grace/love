13c5b62a258dfe679df2e1cef64c0dad686c4094
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:15:16 -0400

    function_auto_path public\src\ebible_verse_new.mjs

---
+++ web/public/src/ebible_verse_new.mjs
@@ -0,0 +1,11 @@
+import { marker } from "./marker.mjs";
+import { list_join_space } from "./list_join_space.mjs";
+export function ebible_verse_new(tokens, verse_number) {
+  marker("1");
+  let joined = list_join_space(tokens);
+  const v = {
+    verse_number,
+    text: joined,
+  };
+  return v;
+}
