bcac3a199da3e62ce42202094b3ee44aec84b3c9
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:27:38 -0400

    function_auto_path public\src\ebible_verse.mjs

--- public/src/ebible_verse.mjs
+++ public/src/ebible_verse.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { ebible_chapter_code_normalize } from "./ebible_chapter_code_normalize.mjs";
 import { list_find_property } from "./list_find_property.mjs";
 import { ebible_languages } from "./ebible_languages.mjs";
@@ -7,7 +8,8 @@ export async function ebible_verse(language_code, chapter_code, verse_number) {
   marker("1");
   chapter_code = ebible_chapter_code_normalize(chapter_code);
   let languages = ebible_languages();
-  let filtered = list_find_property(languages, "language_code", language_code);
+  let l = list_find_property(languages, "language_code", language_code);
+  let bible_folder2 = object_property_get(l, "bible_folder");
   let v = await ebible_verses(bible_folder, chapter_code);
   return v;
 }
