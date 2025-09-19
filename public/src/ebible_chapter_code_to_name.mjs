eaf916fbcc45ed30d1c68b5dab2b57d93f1ee540
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 13:40:29 -0400

    function_auto_path public\src\ebible_chapter_code_to_name.mjs

--- public/src/ebible_chapter_code_to_name.mjs
+++ public/src/ebible_chapter_code_to_name.mjs
@@ -1,9 +1,11 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { integer_to } from "./integer_to.mjs";
 import { string_skip } from "./string_skip.mjs";
 import { ebible_book_code_size } from "./ebible_book_code_size.mjs";
 export function ebible_chapter_code_to_name(chapter_code) {
   let count = ebible_book_code_size();
-  let chapter_name = string_skip(chapter_code, count);
-  let i = integer_to(input);
+  let s = string_skip(chapter_code, count);
+  let i = integer_to(s);
+  object_property_set(object, property_name, value);
   return chapter_name;
 }
