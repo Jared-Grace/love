d8eb8bc489d3ce99a0e69e0b8802695f910eff20
Grace-Jared <christrosetolife@gmail.com>
2025-09-19 09:30:18 -0400

    function_auto_path public\src\ebible_languages.mjs

--- public/src/ebible_languages.mjs
+++ public/src/ebible_languages.mjs
@@ -9,30 +9,37 @@ export function ebible_languages() {
     {
       name: "Swhahili",
       bible_folder: "swhonen",
+      language_code: "TODO",
     },
     {
       name: "Luganda",
       bible_folder: "lug",
+      language_code: "TODO",
     },
     {
       name: "Bengali",
       bible_folder: "benirv",
+      language_code: "TODO",
     },
     {
       name: "Telugu",
       bible_folder: "tel2017",
+      language_code: "TODO",
     },
     {
       name: "Yoruba",
       bible_folder: "yor",
+      language_code: "TODO",
     },
     {
       name: "Cebuano",
       bible_folder: "cebulb",
+      language_code: "TODO",
     },
     {
       name: "Tagalog",
       bible_folder: "tglulb",
+      language_code: "TODO",
     },
   ];
   return languages;
