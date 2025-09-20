5ac32aa967dcac79140b7dd513632365c60da772
Grace-Jared <christrosetolife@gmail.com>
2025-09-20 18:25:47 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,7 +1,8 @@
+import { file_open } from "./file_open.mjs";
 import { folder_user_docs_path } from "./folder_user_docs_path.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
   let file_path = folder_user_docs_path("nations_mentioned.carm.org.txt");
-  flo;
+  await file_open(f_path);
 }
