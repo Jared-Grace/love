dca3f25a05edf00e852d0a6c2be6a61659f112a9
Grace-Jared <christrosetolife@gmail.com>
2025-09-20 18:28:12 -0400

    function_auto_path public\src\list_map_split_comma.mjs

--- public/src/list_map_split_comma.mjs
+++ public/src/list_map_split_comma.mjs
@@ -1,8 +1,8 @@
+import { string_split_comma } from "./string_split_comma.mjs";
 import { list_map } from "./list_map.mjs";
-import { string_split_space } from "./string_split_space.mjs";
 export function list_map_split_comma(mapped2) {
   function lambda(item) {
-    let split2 = string_split_space(item);
+    let split2 = string_split_comma(item);
     return split2;
   }
   let mapped3 = list_map(mapped2, lambda);
