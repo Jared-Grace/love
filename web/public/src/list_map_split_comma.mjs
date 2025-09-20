ccb937391bc3cf84489a328cbff1e6f405af9f3a
Grace-Jared <christrosetolife@gmail.com>
2025-09-20 18:30:05 -0400

    function_auto_path public\src\list_map_split_comma.mjs

--- web/public/src/list_map_split_comma.mjs
+++ web/public/src/list_map_split_comma.mjs
@@ -1,10 +1,6 @@
 import { string_split_comma } from "./string_split_comma.mjs";
 import { list_map } from "./list_map.mjs";
 export function list_map_split_comma(mapped2) {
-  function lambda(item) {
-    let split2 = string_split_comma(item);
-    return split2;
-  }
-  let mapped3 = list_map(mapped2, lambda);
+  let mapped3 = list_map(mapped2, string_split_comma);
   return mapped3;
 }
