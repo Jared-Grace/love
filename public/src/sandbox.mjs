b48331bca4dac76febb0c193cdd86e408f9f4f6f
Grace-Jared <christrosetolife@gmail.com>
2025-09-20 20:38:05 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,8 @@
+import { list_map_whitespace_normalize } from "./list_map_whitespace_normalize.mjs";
 import { log } from "./log.mjs";
 import { list_set } from "./list_set.mjs";
 import { list_second } from "./list_second.mjs";
 import { string_split_semicolon } from "./string_split_semicolon.mjs";
-import { whitespace_normalize } from "./whitespace_normalize.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_map_split_comma } from "./list_map_split_comma.mjs";
 import { string_split_newline } from "./string_split_newline.mjs";
@@ -14,7 +14,7 @@ export async function sandbox() {
   let file_path = folder_user_docs_path("nations_mentioned.carm.org.txt");
   let contents = await file_read(file_path);
   let split = string_split_newline(contents);
-  let split2 = list_map(split, whitespace_normalize);
+  let split2 = list_map_whitespace_normalize(split);
   let mapped3 = list_map_split_comma(split2);
   function lambda(item) {
     let second = list_second(item);
