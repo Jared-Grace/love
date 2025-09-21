df8f86834b8eff35f0938ea94907f63faca45e92
Grace-Jared <christrosetolife@gmail.com>
2025-09-20 20:35:31 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { list_second } from "./list_second.mjs";
 import { string_split_semicolon } from "./string_split_semicolon.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
@@ -18,7 +19,7 @@ export async function sandbox() {
   function lambda(item) {
     let second = list_second(item);
     let split3 = string_split_semicolon(second);
-    list_set;
+    object_property_set(object, property_name, value);
   }
   let mapped = list_map(mapped3, lambda);
   return mapped3;
