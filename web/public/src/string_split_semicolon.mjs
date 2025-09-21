97c146e7617eed26e57939020eb11207abd433d4
Grace-Jared <christrosetolife@gmail.com>
2025-09-20 20:31:43 -0400

    data_file_update public\src\string_split_semicolon.mjs

--- web/public/src/string_split_semicolon.mjs
+++ web/public/src/string_split_semicolon.mjs
@@ -2,6 +2,6 @@ import { marker } from "./marker.mjs";
 import { string_split } from "./string_split.mjs";
 export function string_split_semicolon(s) {
   marker("1");
-  let split3 = string_split(s, "; ");
+  let split3 = string_split(s, ";");
   return split3;
 }
