709fbfe445c60f05a1042e13c17d4b3407ae882a
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 19:38:40 -0400

    function_auto_path public\src\function_rename_suffix_add.mjs

--- public/src/function_rename_suffix_add.mjs
+++ public/src/function_rename_suffix_add.mjs
@@ -1,8 +1,10 @@
+import { string_starts_with } from "./string_starts_with.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { function_rename } from "./function_rename.mjs";
 import { marker } from "./marker.mjs";
 export async function function_rename_suffix_add(f_name_before, suffix) {
   marker("1");
+  let sw = string_starts_with(s, prefix);
   let f_name_after = function_name_combine(f_name_before, suffix);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
