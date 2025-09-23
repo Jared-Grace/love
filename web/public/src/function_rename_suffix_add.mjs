5ea84061a69a32959d8843ca4cf864f66dad47f5
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 19:40:31 -0400

    function_auto_path public\src\function_rename_suffix_add.mjs

--- web/public/src/function_rename_suffix_add.mjs
+++ web/public/src/function_rename_suffix_add.mjs
@@ -6,8 +6,7 @@ import { marker } from "./marker.mjs";
 export async function function_rename_suffix_add(f_name_before, suffix) {
   marker("1");
   let s = suffix;
-  let padding = "_";
-  let p = string_padded_is(s, padding);
+  let p = string_padded_is(s, "_");
   assert_not(p);
   let f_name_after = function_name_combine(f_name_before, suffix);
   let v = await function_rename(f_name_before, f_name_after);
