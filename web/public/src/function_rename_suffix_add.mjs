1c638f8362f1b10cb32bd4defe0edd5763a71506
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 19:40:15 -0400

    function_auto_path public\src\function_rename_suffix_add.mjs

--- web/public/src/function_rename_suffix_add.mjs
+++ web/public/src/function_rename_suffix_add.mjs
@@ -1,5 +1,4 @@
 import { assert_not } from "./assert_not.mjs";
-import { not } from "./not.mjs";
 import { string_ends_with } from "./string_ends_with.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
@@ -7,12 +6,12 @@ import { function_rename } from "./function_rename.mjs";
 import { marker } from "./marker.mjs";
 export async function function_rename_suffix_add(f_name_before, suffix) {
   marker("1");
+  let s = suffix;
   let padding = "_";
   let sw = string_starts_with(s, padding);
   let ew = string_ends_with(s, padding);
   let p = sw || ew;
-  let b = not(p);
-  assert_not(b);
+  assert_not(p);
   let f_name_after = function_name_combine(f_name_before, suffix);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
