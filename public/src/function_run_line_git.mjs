a128b63b66467501f86e19102aa9030487732ea1
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 19:37:43 -0400

    o rs

--- public/src/function_run_line_git.mjs
+++ public/src/function_run_line_git.mjs
@@ -1,6 +1,8 @@
 import { function_run_line_parse } from "./function_run_line_parse.mjs";
 import { function_run_git } from "./function_run_git.mjs";
+ Error.stackTraceLimit = Infinity;
 export async function function_run_line_git(line) {
+ 
   let { f_name, args } = await function_run_line_parse(line);
   await function_run_git(f_name, args);
 }
