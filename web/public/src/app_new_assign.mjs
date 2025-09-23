ac318bee59a98518dcde6ee16bb22dcdcdd9a235
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 18:22:54 -0400

    function_auto_path public\src\app_new_assign.mjs

--- web/public/src/app_new_assign.mjs
+++ web/public/src/app_new_assign.mjs
@@ -5,7 +5,9 @@ import { js_code_dot } from "./js_code_dot.mjs";
 export function app_new_assign(combined) {
   const v = "f_name";
   let code = js_code_dot(combined, "name");
-  log(code);
+  log({
+    code,
+  });
   let expression = js_parse_expression(code);
   let assign = js_declare(v, expression);
   return assign;
