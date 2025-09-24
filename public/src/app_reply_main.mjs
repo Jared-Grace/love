dc9ff3bcd537a182388f740853e12d49c2dec1d6
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:12:29 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -58,7 +58,7 @@ export async function app_reply_main() {
   let typed = "";
   async function verse_random_get() {
     let encouragement = bible_verses_encouragement();
-    let list = await ebible_references_parse_lines(bible_folders, lines);
+    let list = await ebible_references_parse_lines([en], lines);
     let { chapter_code, verse_number } = list_random_item(index);
     let verse = await ebible_verse_download(en, chapter_code, verse_number);
     let v = [verse];
