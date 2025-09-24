35d0bdc0898fe317717f317647ce5c574e0193e0
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 06:38:13 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -39,6 +39,7 @@ export async function sandbox() {
   let books = await ebible_version_books(bible_folder);
   let mapped2 = list_map_property(books, "text");
   let verse_references = list_filter_starts_with_any(mapped2, mapped);
+  fe;
   if (false) {
   }
   return verse_references;
