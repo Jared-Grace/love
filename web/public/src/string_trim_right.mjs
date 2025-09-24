9d769ac5ffa38d8b35fae40d7e548325d7ac0bf9
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:21:57 -0400

    c string_skip_while string_trim_right

--- web/public/src/string_trim_right.mjs
+++ web/public/src/string_trim_right.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { string_skip } from "./string_skip.mjs";
 import { not } from "./not.mjs";
 export function string_trim_right(lambda$s, s) {
+  marker("1");
   while (true) {
     let any = lambda$s(s);
     if (not(any)) {
