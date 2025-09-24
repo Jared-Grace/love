d6a83b917ce93650ab82ec3b60dd35b53f54d758
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:07:39 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { string_split_newline } from "./string_split_newline.mjs";
 import { list_any_starts_with } from "./list_any_starts_with.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
 import { file_read } from "./file_read.mjs";
@@ -7,6 +8,7 @@ export async function sandbox() {
   marker("1");
   let file_path = folder_user_docs_path("bible_references.hopenation.org.txt");
   let contents = await file_read(file_path);
+  let split = string_split_newline(list);
   let digits = "1234567890";
   function lambda(item) {
     let sw = string_starts_with(s, item);
