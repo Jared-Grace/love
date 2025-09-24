0688af70282bb4f322fdb4c8a1e3908323c6055c
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:46:45 -0400

    o ebible_version_books

--- public/src/ebible_version_books.mjs
+++ public/src/ebible_version_books.mjs
@@ -1,3 +1,4 @@
+import { marker } from "./marker.mjs";
 import { firebase_storage_download_ebible } from "./firebase_storage_download_ebible.mjs";
 import { ebible_version_books_upload_name } from "./ebible_version_books_upload_name.mjs";
 import { browser_is } from "./browser_is.mjs";
@@ -7,6 +8,7 @@ import { list_join_comma_space } from "./list_join_comma_space.mjs";
 import { ebible_class_old } from "./ebible_class_old.mjs";
 import { ebible_class_new } from "./ebible_class_new.mjs";
 export async function ebible_version_books(bible_folder) {
+  marker("1");
   let b = browser_is();
   if (b) {
     let file_name2 = ebible_version_books_upload_name();
