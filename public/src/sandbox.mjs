2bb4c1d1b0d7c3c68bb9af040ee5f07a87ef1906
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:02:55 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,5 +2,5 @@ import { folder_user_docs_path } from "./folder_user_docs_path.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
-  let file_path = folder_user_docs_path("nations_mentioned.carm.org.txt");
+  let file_path = folder_user_docs_path("bible_references.hopenation.org.txt");
 }
