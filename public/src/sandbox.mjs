a34c20e4bab687ce0adaf951107c7ee3cfa05787
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 06:55:36 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,6 @@
-import { bible_verses_encouragement_generate } from "./bible_verses_encouragement_generate.mjs";
+import { bible_verses_encouragement } from "./bible_verses_encouragement.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
-  await bible_verses_encouragement_generate();
+  await bible_verses_encouragement();
 }
