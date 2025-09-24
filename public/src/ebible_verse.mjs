28c7a7e427c5f37d59361efac785d7d845b1bb69
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:32:10 -0400

    function_auto_path public\src\ebible_verse.mjs

--- public/src/ebible_verse.mjs
+++ public/src/ebible_verse.mjs
@@ -3,6 +3,8 @@ import { ebible_verses } from "./ebible_verses.mjs";
 import { marker } from "./marker.mjs";
 export async function ebible_verse(bible_folder, chapter_code, verse_number) {
   marker("1");
+  if (false) {
+  }
   let verses = await ebible_verses(bible_folder, chapter_code);
   let result = list_find_property(verses, "verse_number", verse_number);
   return result;
