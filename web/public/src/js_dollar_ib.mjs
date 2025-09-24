ad5d67b03729129438299d8c3822647b0263bf33
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:33:40 -0400

    function_auto_path public\src\js_dollar_ib.mjs

--- web/public/src/js_dollar_ib.mjs
+++ web/public/src/js_dollar_ib.mjs
@@ -1,11 +1,11 @@
+import { ebible_verse } from "./ebible_verse.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { equal } from "./equal.mjs";
 import { js_call_new_expression } from "./js_call_new_expression.mjs";
 import { js_dollar_i } from "./js_dollar_i.mjs";
 export async function js_dollar_ib({ stack1, ast }) {
   js_dollar_i({
     stack1,
   });
-  let expression = await js_call_new_expression(equal.name, ast);
+  let expression = await js_call_new_expression(ebible_verse.name, ast);
   object_property_set(stack1, "test", expression);
 }
