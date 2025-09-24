4ff61b4261ea75ccae8b7e55cc6d96d6e299497f
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:14:37 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,11 @@
+import { ebible_references_parse_lines } from "./ebible_references_parse_lines.mjs";
+import { ebible_folder_english } from "./ebible_folder_english.mjs";
 import { bible_verses_encouragement } from "./bible_verses_encouragement.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
   let v = bible_verses_encouragement();
+  let en = ebible_folder_english();
+  let r = "Proverbs 3:5-6";
+  let list = await ebible_references_parse_lines([en], [r]);
 }
