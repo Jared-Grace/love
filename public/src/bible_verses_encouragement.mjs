db164ce2ea526d9a76c266337231b21cf73b17d4
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 06:47:07 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/bible_verses_encouragement.mjs
+++ public/src/bible_verses_encouragement.mjs
@@ -1,4 +1,33 @@
 import { marker } from "./marker.mjs";
 export function bible_verses_encouragement() {
-  marker("1");
+  [
+    "Deuteronomy 31:6",
+    "Deuteronomy 31:8",
+    "Joshua 1:9",
+    "Proverbs 3:5-6",
+    "Proverbs 12:25",
+    "Proverbs 29:25",
+    "Isaiah 26:3",
+    "Isaiah 35:4",
+    "Isaiah 40:31",
+    "Isaiah 41:10",
+    "Isaiah 41:13",
+    "Isaiah 43:1",
+    "Jeremiah 29:11",
+    "Zephaniah 3:17",
+    "Matthew 6:25-34",
+    "Matthew 11:28-30",
+    "Mark 5:36",
+    "John 14:1",
+    "John 14:27",
+    "John 16:33",
+    "Romans 8:38-39",
+    "Colossians 3:15",
+    "Philippians 4:6-9",
+    "Philippians 4:13",
+    "1 Peter 5:6-8",
+    "2 Timothy 1:7",
+    "1 John 4:18",
+    "Revelation 1:17",
+  ];
 }
