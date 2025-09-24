509fe2c79f27c8b6ffeecfab3c9e044360ee7d35
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:34:13 -0400

    function_auto_path public\src\js_dollar_ie.mjs

--- public/src/js_dollar_ie.mjs
+++ public/src/js_dollar_ie.mjs
@@ -7,6 +7,6 @@ export async function js_dollar_ie({ stack1, ast }) {
   js_dollar_i({
     stack1,
   });
-  let expression = await js_call_new_expression(equal.name, ast);
+  let expression = await js_call_new_expression(fn.name, ast);
   object_property_set(stack1, "test", expression);
 }
