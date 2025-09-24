6969697e8420539e30acf5d9595cb8ec3d948fcb
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:19:55 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -21,10 +21,5 @@ export async function sandbox() {
     }
   }
   let mapped = list_map(split, lambda2);
-  function lambda2(item2) {
-    item2 = string_skip_while(string_starts_with_dot, item2);
-    return item2;
-  }
-  let mapped2 = list_map(mapped, lambda2);
   return mapped;
 }
