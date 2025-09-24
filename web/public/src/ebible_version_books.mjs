8e179fe25e6f8a25f7c4543f0edcc1ce769e414b
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:05:17 -0400

    function_auto_path public\src\ebible_version_books.mjs

--- web/public/src/ebible_version_books.mjs
+++ web/public/src/ebible_version_books.mjs
@@ -1,10 +1,11 @@
+import { browser_is } from "./browser_is.mjs";
 import { ebible_class_apocrypha } from "./ebible_class_apocrypha.mjs";
 import { ebible_version_books_testament } from "./ebible_version_books_testament.mjs";
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
 import { ebible_class_old } from "./ebible_class_old.mjs";
 import { ebible_class_new } from "./ebible_class_new.mjs";
 export async function ebible_version_books(bible_folder) {
-  wi;
+  let b = browser_is();
   if (false) {
   }
   const n = ebible_class_new();
