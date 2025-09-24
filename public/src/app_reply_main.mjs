2676319190e1cb656e83b4c8d84cae271e6b648c
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 13:24:09 -0400

    function_auto_path public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -153,7 +153,8 @@ export async function app_reply_main() {
   buttons_refresh();
   async function preview_refresh() {
     let verses_list_first = list_first(verses_list);
-    let reference = object_property_get(verses_list_first, "reference");e
+    let reference = object_property_get(verses_list_first, "reference");
+    each(list2, function lambda10(item4) {});
     function lambda9(item3) {}
     each(list, lambda9);
     let chapter_code2 = object_property_get(verse, "chapter_code");
