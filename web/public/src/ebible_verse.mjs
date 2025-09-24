72093f6c0e2719e1c7d2f7dc6724b9d7e3cdb878
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:22:31 -0400

    function_auto_path public\src\ebible_verse.mjs

--- web/public/src/ebible_verse.mjs
+++ web/public/src/ebible_verse.mjs
@@ -1,12 +1,7 @@
 import { list_find_property } from "./list_find_property.mjs";
 import { ebible_verses } from "./ebible_verses.mjs";
 import { marker } from "./marker.mjs";
-export async function ebible_verse(
-  bible_folder,
-  book_code,
-  chapter_code,
-  verse_number,
-) {
+export async function ebible_verse(bible_folder, chapter_code, verse_number) {
   marker("1");
   let verses = await ebible_verses(bible_folder, chapter_code);
   let result = list_find_property(verses, "verse_number", verse_number);
