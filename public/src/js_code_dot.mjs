09120cd3ce59289e4b82363b3445d5ced0a9a3a1
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:15:26 -0400

    function_auto_path public\src\js_code_dot.mjs

--- public/src/js_code_dot.mjs
+++ public/src/js_code_dot.mjs
@@ -1,6 +1,7 @@
+import { string_dot } from "./string_dot.mjs";
 import { marker } from "./marker.mjs";
 export function js_code_dot(left, right) {
   marker("1");
-  let code = "(" + left + "." + right + ")";
+  let code = "(" + left + string_dot() + right + ")";
   return code;
 }
