2a6054d40c51ddf45c1436d3d5a75ac95a680dfa
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 07:32:29 -0400

    function_auto_path public\src\ebible_verse.mjs

--- web/public/src/ebible_verse.mjs
+++ web/public/src/ebible_verse.mjs
@@ -8,7 +8,7 @@ export async function ebible_verse(bible_folder, chapter_code, verse_number) {
   let b = browser_is();
   if (b) {
     let verse = await ebible_verse_download(en, chapter_code, verse_number);
-    return b;
+    return verse;
   }
   let verses = await ebible_verses(bible_folder, chapter_code);
   let result = list_find_property(verses, "verse_number", verse_number);
