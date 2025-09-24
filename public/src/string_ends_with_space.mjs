6fabbcee6e4f966cc7beaaa822c2dedbbfbdcfd0
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:24:01 -0400

    function_auto_path public\src\string_ends_with_space.mjs

--- public/src/string_ends_with_space.mjs
+++ public/src/string_ends_with_space.mjs
@@ -1,6 +1,7 @@
+import { string_ends_with } from "./string_ends_with.mjs";
 import { marker } from "./marker.mjs";
 export function string_ends_with_space(s) {
   marker("1");
-  let sw2 = string_endss_with(s, " ");
+  let sw2 = string_ends_with(s, " ");
   return sw2;
 }
