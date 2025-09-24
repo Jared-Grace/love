6525baa83f2459a3ada2ec95b1c91527ffa7770d
Grace-Jared <christrosetolife@gmail.com>
2025-09-24 06:38:59 -0400

    function_auto_path public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -40,6 +40,7 @@ export async function sandbox() {
   let books = await ebible_version_books(bible_folder);
   let mapped2 = list_map_property(books, "text");
   let verse_references = list_filter_starts_with_any(mapped2, mapped);
+  let f_name = "bible_verses_encouragement";
   let e = await function_exists(f_name);
   if (false) {
   }
