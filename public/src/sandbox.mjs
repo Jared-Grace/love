6d9a3e119835aa4c4e12417658f59b8c9440fcf6
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:03:20 -0400

    function_auto_path public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,8 @@
-import { file_open } from "./file_open.mjs";
+import { file_read } from "./file_read.mjs";
 import { folder_user_docs_path } from "./folder_user_docs_path.mjs";
 import { marker } from "./marker.mjs";
 export async function sandbox() {
   marker("1");
   let file_path = folder_user_docs_path("bible_references.hopenation.org.txt");
-  await file_open(file_path);
+  let contents = await file_read(file_path2);
 }
