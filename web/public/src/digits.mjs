83fc7156e62d9dfd6404738a5ab25bbe07a804ac
Grace-Jared <christrosetolife@gmail.com>
2025-09-23 20:09:45 -0400

    function_auto_path public\src\digits.mjs

--- web/public/src/digits.mjs
+++ web/public/src/digits.mjs
@@ -1,6 +1,6 @@
 import { string_split_empty } from "./string_split_empty.mjs";
 export function digits() {
   let d = "1234567890";
-  let split2 = string_split_empty(s);
-  return d;
+  let split = string_split_empty(s);
+  return split;
 }
