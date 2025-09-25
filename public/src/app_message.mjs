90d1b12d5a41efe1576e4a813293e9b722f6c33a
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 12:00:26 -0400

    de app_message

--- public/src/app_message.mjs
+++
@@ -1,8 +0,0 @@
-import { firebase_storage_function_run } from "./firebase_storage_function_run.mjs";
-import { app_message_main } from "./app_message_main.mjs";
-import { marker } from "./marker.mjs";
-export async function app_message() {
-  marker("1");
-  let f_name = app_message_main.name;
-  await firebase_storage_function_run(f_name);
-}
