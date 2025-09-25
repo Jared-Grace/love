25dce89214914f7922727b4a3c8138965dab4c03
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 19:01:17 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -12,7 +12,12 @@ export async function messenger_reply() {
   });
   const page = await browser.newPage();
   await page.goto("https://www.google.com");
-  const unreadSpanSelector = 'textarea[name="q"]';
+  const unreadSpanSelector = '//textarea[name="q"]';
+  await page.waitForSelector(unreadSpanSelector, {
+    timeout: 1000,
+  });
+  console.log("Found unread span!");
+  await sleep(100000);
   const [span] = await page.$x(unreadSpanSelector);
   if (span) {
     console.log("Found unread span!");
