b61f98bfebec330a42530cbb4bf4e23bb8b80e93
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 18:22:29 -0400

    function_auto_path public\src\messenger_reply.mjs

--- web/public/src/messenger_reply.mjs
+++ web/public/src/messenger_reply.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
 import { import_install } from "./import_install.mjs";
 import { marker } from "./marker.mjs";
@@ -10,4 +11,11 @@ export async function messenger_reply() {
   });
   const page = await browser.newPage();
   await page.goto("https://www.facebook.com/messages/e2ee/t/");
+  const [span] = await page.$x("//span[contains(text(), 'unread')]");
+  if (span) {
+    console.log("Found unread span!");
+    await span.click();
+  } else {
+    console.log("Unread span not found");
+  }
 }
