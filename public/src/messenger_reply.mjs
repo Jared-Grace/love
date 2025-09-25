ec5939c9c6bdcf863b48f8435dc1d4f571b9ba24
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 18:13:16 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -6,7 +6,7 @@ export async function messenger_reply() {
   const browser = await puppeteer.launch({
     headless: false,
     userDataDir:
-      "C:\\Users\\YourName\\AppData\\Local\\Google\\Chrome\\User Data\\Default",
+      "C:\\Users\\chris\\AppData\\Local\\Google\\Chrome\\User Data\\Default",
   });
   const page = await browser.newPage();
   await page.goto("https://www.facebook.com/");
