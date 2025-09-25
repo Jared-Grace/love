b5fea5b56ff987dbacc2768f3e71865cdef02dc1
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 12:11:42 -0400

    function_auto_path public\src\js_assign_combine.mjs

--- web/public/src/js_assign_combine.mjs
+++ web/public/src/js_assign_combine.mjs
@@ -1,3 +1,4 @@
+import { log } from "./log.mjs";
 import { js_identifier_not_is } from "./js_identifier_not_is.mjs";
 import { list_next_try } from "./list_next_try.mjs";
 import { list_remove } from "./list_remove.mjs";
@@ -20,6 +21,7 @@ export function js_assign_combine(ast) {
     let next = list_next_try(e1, node);
     if (null_is(next)) {
       return;
+      log(message);
     }
     let declaration2 = js_declare_single(next);
     if (null_is(declaration2)) {
