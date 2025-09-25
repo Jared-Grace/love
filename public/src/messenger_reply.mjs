b544580ff6b5f2044863d9c9e4007ca409203df6
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 19:21:01 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -1,3 +1,5 @@
+import { sleep } from "./sleep.mjs";
+import { log } from "./log.mjs";
 import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
 import { marker } from "./marker.mjs";
 export async function messenger_reply() {
@@ -10,6 +12,15 @@ export async function messenger_reply() {
   });
   const page = await browser.newPage();
   await page.goto("https://www.facebook.com/messages/e2ee/t/9895223143834311");
+  const unreadSpanSelector = 'p[dir="auto"]';
+  log("here2");
+  let p = await page.waitForSelector(unreadSpanSelector, {
+    timeout: 10000,
+  });
+  await sleep(2000);
+  log("here");
+  await p.type("test");
+  await sleep(100000);
   const inputBox = await page.waitForSelector(
     'div[aria-label="Press Enter to send"]',
     {
