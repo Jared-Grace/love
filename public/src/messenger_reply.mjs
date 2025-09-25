4574c2631be05ee6b8d78889df66f8ee0a31493b
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 19:31:18 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -20,7 +20,7 @@ export async function messenger_reply() {
   await sleep(2000);
   log("here");
   await p.type("test");
-  let s = await page.waitForSelector('div[aria-label="Press Enter to send"]', {
+  let s = await page.waitForSelector('div[aria-label="Press enter to send"]', {
     timeout: 10000,
   });
   await sleep(100000);
