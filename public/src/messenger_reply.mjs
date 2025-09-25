3a5fe822fb3ef0e97c3a549fc0868c8d0ea74231
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 18:10:49 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -1,4 +1,12 @@
 import { marker } from "./marker.mjs";
-export function messenger_reply() {
+export async function messenger_reply() {
   marker("1");
+  const puppeteer = await import("puppeteer");
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir:
+      "C:\\Users\\YourName\\AppData\\Local\\Google\\Chrome\\User Data\\Default",
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.facebook.com/");
 }
