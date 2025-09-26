6df92f78ad10123764e9b16d91442449a445660b
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:14:37 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -1,16 +1,10 @@
+import { keyboard_typing_delay } from "./keyboard_typing_delay.mjs";
 import { log } from "./log.mjs";
-import { integer_random } from "./integer_random.mjs";
 import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
 import { marker } from "./marker.mjs";
 export async function messenger_reply() {
   marker("1");
-  let characters_per_minute = 190;
-  let seconds_per_minute = 60;
-  let ms_per_second = 1000;
-  let ms_per_minute = seconds_per_minute * ms_per_second;
-  let ms_per_character = ms_per_minute / characters_per_minute;
-  let high = ms_per_character * 1.5;
-  let r = integer_random(ms_per_character, high);
+  let r = keyboard_typing_delay();
   log(r);
   return;
   const puppeteerModule = await import("puppeteer");
