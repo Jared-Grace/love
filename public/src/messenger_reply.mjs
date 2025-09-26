1c17cc67e0fbd574fb571e18b705854782a14367
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:32:52 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -2,12 +2,13 @@ import { catch_only_run_async } from "./catch_only_run_async.mjs";
 import { import_install } from "./import_install.mjs";
 export async function messenger_reply() {
   const { chromium, firefox, webkit } = await import_install("playwright");
-  async function lambda2() {}
-  await catch_only_run_async(lambda2, message_fragment, on_error);
   let browser = null;
-  browser = await chromium.launch({
-    headless: false,
-  });
+  async function lambda2() {
+    browser = await chromium.launch({
+      headless: false,
+    });
+  }
+  await catch_only_run_async(lambda2, message_fragment, on_error);
   const page = await browser.newPage();
   await page.goto("https://facebook.com");
   await browser.close();
