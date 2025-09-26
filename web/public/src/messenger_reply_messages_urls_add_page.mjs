e7809e630ca3b97d6a90230724e71aa13703a2d3
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:49:34 -0400

    function_auto_path public\src\messenger_reply_messages_urls_add_page.mjs

--- web/public/src/messenger_reply_messages_urls_add_page.mjs
+++ web/public/src/messenger_reply_messages_urls_add_page.mjs
@@ -3,8 +3,8 @@ import { list_remove_if_exists } from "./list_remove_if_exists.mjs";
 import { puppeteer_hrefs_starts_with } from "./puppeteer_hrefs_starts_with.mjs";
 export async function messenger_reply_messages_urls_add_page(page) {
   let prefix = "https://www.facebook.com/messages/";
-  let filtered = await puppeteer_hrefs_starts_with(page, prefix);
-  list_remove_if_exists(filtered, "https://www.facebook.com/messages/new/");
-  await messenger_reply_messages_urls_add_multiple(filtered);
-  return filtered;
+  let urls = await puppeteer_hrefs_starts_with(page, prefix);
+  list_remove_if_exists(urls, "https://www.facebook.com/messages/new/");
+  await messenger_reply_messages_urls_add_multiple(urls);
+  return urls;
 }
