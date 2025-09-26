6b4d0ee49ca890a62b8a06618376e24923c65203
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:45:42 -0400

    function_auto_path public\src\reply.mjs

--- web/public/src/reply.mjs
+++ web/public/src/reply.mjs
@@ -1,3 +1,4 @@
+import { list_filter_empty_not_is } from "./list_filter_empty_not_is.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { list_map } from "./list_map.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
@@ -5,7 +6,7 @@ import { marker } from "./marker.mjs";
 export function reply(input) {
   input = whitespace_normalize(input);
   input = string_split_space(input);
-  function lambda(item) {}
+  let filtered2 = list_filter_empty_not_is(item);
   let mapped = list_map(list, lambda);
   marker("1");
 }
