6abc1c1c8c196a0067df4237b47d68d2221d923c
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:27:43 -0400

    function_auto_path public\src\list_filter_starts_with.mjs

--- web/public/src/list_filter_starts_with.mjs
+++ web/public/src/list_filter_starts_with.mjs
@@ -1,10 +1,10 @@
 import { list_filter } from "./list_filter.mjs";
 import { string_starts_with } from "./string_starts_with.mjs";
-export function list_filter_starts_with(mapped, book_code) {
+export function list_filter_starts_with(list, prefix) {
   function lambda(item) {
-    let sw = string_starts_with(item, book_code);
+    let sw = string_starts_with(item, prefix);
     return sw;
   }
-  let filtered = list_filter(mapped, lambda);
+  let filtered = list_filter(list, lambda);
   return filtered;
 }
