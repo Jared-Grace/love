42d3c86c6c0a58f9578dd0c11448828308c00e30
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 22:28:06 -0400

    function_auto_path public\src\messenger_reply_login.mjs

--- web/public/src/messenger_reply_login.mjs
+++ web/public/src/messenger_reply_login.mjs
@@ -1,3 +1,4 @@
+import { messenger_reply_url } from "./messenger_reply_url.mjs";
 import { command_line_read_empty } from "./command_line_read_empty.mjs";
 import { import_install } from "./import_install.mjs";
 export async function messenger_reply_login() {
@@ -7,8 +8,11 @@ export async function messenger_reply_login() {
   });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto("https://www.facebook.com/");
-  let v = await command_line_read_empty();
+  let v = messenger_reply_url();
+  await page.goto(v);
+  await command_line_read_empty();
+  await page.goto(v);
+  await command_line_read_empty();
   await context.storageState({
     path: "fb-session.json",
   });
