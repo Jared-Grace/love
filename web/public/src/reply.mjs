e97df3e6751c2634ffd36a0bd8e811e198c7db57
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 16:01:02 -0400

    function_auto_path public\src\reply.mjs

--- web/public/src/reply.mjs
+++ web/public/src/reply.mjs
@@ -4,8 +4,8 @@ import { string_split_space } from "./string_split_space.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
 export function reply(input) {
   input = whitespace_normalize(input);
-  input = list_map_lower(input);
   input = string_split_space(input);
+  input = list_map_lower(input);
   input = list_filter_empty_not_is(input);
   return input;
 }
