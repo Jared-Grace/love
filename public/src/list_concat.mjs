73866d06ef1ea6e6d2c6a16af8412d8f95bbdbc0
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 19:21:05 -0400

    function_auto_path public\src\list_concat.mjs

--- public/src/list_concat.mjs
+++ public/src/list_concat.mjs
@@ -1,8 +1,8 @@
 import { list_is } from "./list_is.mjs";
 import { assert } from "./assert.mjs";
 export function list_concat(a, b) {
+  let l = list_is(a);
+  assert(l);
   let concated = a.concat(b);
   return concated;
-  let l = list_is(value);
-  assert(b2);
 }
