1b185511c7555b068bf527d2bfc2045b1c6f317f
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 19:33:33 -0400

    function_auto_path public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,6 +1,8 @@
 export function reply_on_match(wrapped_fn, on_match) {
   let fn = function reply_on_match_inner(a) {
     let matches = wrapped_fn(a);
+    if (false) {
+    }
   };
   return fn;
 }
