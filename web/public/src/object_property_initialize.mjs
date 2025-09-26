9d7d9d0b3b5bac466faa730d5a10e9bd6ad9e748
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:17:43 -0400

    function_auto_path public\src\object_property_initialize.mjs

--- web/public/src/object_property_initialize.mjs
+++ web/public/src/object_property_initialize.mjs
@@ -2,11 +2,15 @@ import { not } from "./not.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
-export function object_property_initialize(object, property_name, value_initial) {
+export function object_property_initialize(
+  object,
+  property_name,
+  value_initial,
+) {
   const exists = object_property_exists(object, property_name);
   if (not(exists)) {
     object_property_set(object, property_name, value_initial);
   }
-  value = object_property_get(object, property_name);
+  let value = object_property_get(object, property_name);
   return value;
 }
