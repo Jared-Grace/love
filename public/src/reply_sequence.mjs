d63988ca1cb9bb59806ca51e891456b6602f76aa
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 16:05:28 -0400

    data_file_update public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -2,7 +2,8 @@ import { each_range } from "./each_range.mjs";
 export function reply_sequence(items) {
   let v = function reply_sequence_matches(input) {
     let index = 0;
-    each_range(count, function lambda2() {});
+    function lambda2() {}
+    each_range(count, lambda2);
   };
   return v;
 }
