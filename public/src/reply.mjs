de4e35d006f8fa878118644efc92b11dd641f00b
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 16:11:34 -0400

    function_auto_path public\src\reply.mjs

--- public/src/reply.mjs
+++ public/src/reply.mjs
@@ -1,9 +1,11 @@
+import { reply_sequence } from "./reply_sequence.mjs";
 import { list_map_lower } from "./list_map_lower.mjs";
 import { list_filter_empty_not_is } from "./list_filter_empty_not_is.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
 export function reply(input) {
-  let r = reply_sequence(["good", "evening"]);
+  let fn = reply_sequence(["good", "evening"]);
+  let r = reply_on_match(fn);
   input = whitespace_normalize(input);
   input = string_split_space(input);
   input = list_filter_empty_not_is(input);
