8fde5793e73bd2bd2167e184ba6070b8b7ba5bfe
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:29:53 -0400

    function_auto_path public\src\messenger_reply_messages.mjs

--- web/public/src/messenger_reply_messages.mjs
+++ web/public/src/messenger_reply_messages.mjs
@@ -1,11 +1,11 @@
-import { messenger_reply_messages_urls_add_multiple } from "./messenger_reply_messages_urls_add_multiple.mjs";
+import { messenger_reply_messages_urls_add } from "./messenger_reply_messages_urls_add.mjs";
 import { string_empty_is } from "./string_empty_is.mjs";
 import { string_trim } from "./string_trim.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 export async function messenger_reply_messages(page, url) {
   await page.goto(url);
-  await messenger_reply_messages_urls_add_multiple(url);
+  await messenger_reply_messages_urls_add(url);
   const s = 'p[dir="auto"]';
   let p = await page.waitForSelector(s, {
     timeout: 10000,
