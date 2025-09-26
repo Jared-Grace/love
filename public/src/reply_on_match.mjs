f35262a5b5ecf1bf7928243c7704358eee51249a
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 19:24:38 -0400

    function_auto_path public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,5 +1,6 @@
 import { marker } from "./marker.mjs";
 export function reply_on_match(fn, on_match) {
-  return function reply_on_match_inner() {};
+  let v = function reply_on_match_inner() {};
+  return v;
   marker("1");
 }
