d0b2fb2a76e3b0ad48ed957eec93c18bd469ecfb
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:28:30 -0400

    function_auto_path public\src\messenger_reply.mjs

---
+++ public/src/messenger_reply_puppeteer.mjs
@@ -0,0 +1,30 @@
+import { each_async } from "./each_async.mjs";
+import { sleep } from "./sleep.mjs";
+import { keyboard_typing_delay } from "./keyboard_typing_delay.mjs";
+import { string_list_to } from "./string_list_to.mjs";
+import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
+export async function messenger_reply_puppeteer() {
+  const puppeteerModule = await import("puppeteer");
+  const puppeteer = puppeteerModule.default ?? puppeteerModule;
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: messenger_reply_user_data_path(),
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.facebook.com/messages/e2ee/t/9895223143834311");
+  const unreadSpanSelector = 'p[dir="auto"]';
+  let p = await page.waitForSelector(unreadSpanSelector, {
+    timeout: 10000,
+  });
+  await p.focus();
+  const message = "Greetings!";
+  let list = string_list_to(message);
+  async function lambda(item) {
+    let delay_ms = keyboard_typing_delay();
+    await sleep(delay_ms);
+    await page.keyboard.type(item);
+  }
+  await each_async(list, lambda);
+  return;
+  await page.keyboard.press("Enter");
+}
