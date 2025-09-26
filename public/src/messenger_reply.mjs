e8ac81637311ee4f1fd3d77e59af03bfbc4ab3c5
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 14:47:10 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -1,3 +1,5 @@
+import { messenger_reply_messages } from "./messenger_reply_messages.mjs";
+import { messenger_reply_url } from "./messenger_reply_url.mjs";
 import { messenger_reply_puppeteer } from "./messenger_reply_puppeteer.mjs";
 import { messenger_reply_unread_collect } from "./messenger_reply_unread_collect.mjs";
 import { command_line_read_empty } from "./command_line_read_empty.mjs";
@@ -8,7 +10,10 @@ import { keyboard_type_delay } from "./keyboard_type_delay.mjs";
 export async function messenger_reply() {
   marker("1");
   await messenger_reply_unread_collect();
-  async function lambda2() {}
+  async function lambda2() {
+    let v = messenger_reply_url();
+    let messages = await messenger_reply_messages(page, v);
+  }
   let page2 = await messenger_reply_puppeteer(lambda2);
   let answer = await command_line_read_empty();
   return;
