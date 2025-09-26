9261756cb93854ff2639c7bca263e2994222c02e
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:39:52 -0400

    function_auto_path public\src\reply.mjs

--- public/src/reply.mjs
+++ public/src/reply.mjs
@@ -1,8 +1,6 @@
-import { string_trim } from "./string_trim.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
 import { marker } from "./marker.mjs";
 export function reply(input) {
   input = whitespace_normalize(input);
-  let v7 = string_trim(message);
   marker("1");
 }
