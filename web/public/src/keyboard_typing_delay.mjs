a85acfa784b22cdd6bb7e90db7fbcec118d0d641
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:14:37 -0400

    function_auto_path public\src\messenger_reply.mjs

---
+++ web/public/src/keyboard_typing_delay.mjs
@@ -0,0 +1,11 @@
+import { integer_random } from "./integer_random.mjs";
+export function keyboard_typing_delay() {
+  let characters_per_minute = 190;
+  let seconds_per_minute = 60;
+  let ms_per_second = 1000;
+  let ms_per_minute = seconds_per_minute * ms_per_second;
+  let ms_per_character = ms_per_minute / characters_per_minute;
+  let high = ms_per_character * 1.5;
+  let r = integer_random(ms_per_character, high);
+  return r;
+}
