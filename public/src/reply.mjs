3d7050c4c9c7e98603bb00e863c4035efa288b89
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:46:34 -0400

    function_auto_path public\src\reply.mjs

--- public/src/reply.mjs
+++ public/src/reply.mjs
@@ -1,10 +1,9 @@
 import { list_filter_empty_not_is } from "./list_filter_empty_not_is.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
-import { marker } from "./marker.mjs";
 export function reply(input) {
   input = whitespace_normalize(input);
   input = string_split_space(input);
   input = list_filter_empty_not_is(input);
-  marker("1");
+  return input;
 }
