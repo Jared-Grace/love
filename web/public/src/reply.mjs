62e4f548bd828ce436b8292c5f3ccdde087a2f79
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 16:00:08 -0400

    function_auto_path public\src\reply.mjs

--- web/public/src/reply.mjs
+++ web/public/src/reply.mjs
@@ -1,11 +1,11 @@
+import { string_lower_to } from "./string_lower_to.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_filter_empty_not_is } from "./list_filter_empty_not_is.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
 export function reply(input) {
   input = whitespace_normalize(input);
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
+  input = list_map(list, string_lower_to);
   input = string_split_space(input);
   input = list_filter_empty_not_is(input);
   return input;
