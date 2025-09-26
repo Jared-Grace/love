f00ae1a6e2e23bf7de241c504773b97fa7a85ea6
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:10:03 -0400

    function_auto_path public\src\messenger_reply_unread_click.mjs

--- public/src/messenger_reply_unread_click.mjs
+++ public/src/messenger_reply_unread_click.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { list_get } from "./list_get.mjs";
 import { puppeteer_matches } from "./puppeteer_matches.mjs";
 export async function messenger_reply_unread_click(page) {
+  marker("1");
   const tag_name = "span";
   const text = "Unread";
   const matches = await puppeteer_matches(page, tag_name, text);
