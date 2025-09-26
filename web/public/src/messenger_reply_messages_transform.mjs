70429c10ee45875df2bfca296dad4a9bf743eea0
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:49:44 -0400

    function_auto_path public\src\messenger_reply_messages_transform.mjs

---
+++ web/public/src/messenger_reply_messages_transform.mjs
@@ -0,0 +1,11 @@
+import { messenger_reply_messages_urls_get } from "./messenger_reply_messages_urls_get.mjs";
+import { file_json_transform } from "./file_json_transform.mjs";
+import { folder_user_docs_path } from "./folder_user_docs_path.mjs";
+export async function messenger_reply_messages_transform(transform_inner) {
+  let fb_path = folder_user_docs_path("fb.json");
+  await file_json_transform(fb_path, transform);
+  function transform(data) {
+    let messages_urls = messenger_reply_messages_urls_get(data);
+    transform_inner(messages_urls);
+  }
+}
