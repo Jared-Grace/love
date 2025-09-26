97f61329fc16cf985ef889fb55eef2746bca2b70
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:32:37 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -2,11 +2,8 @@ import { catch_only_run_async } from "./catch_only_run_async.mjs";
 import { import_install } from "./import_install.mjs";
 export async function messenger_reply() {
   const { chromium, firefox, webkit } = await import_install("playwright");
-  await catch_only_run_async(
-    async function lambda2() {},
-    message_fragment,
-    on_error,
-  );
+  async function lambda2() {}
+  await catch_only_run_async(lambda2, message_fragment, on_error);
   const browser = await chromium.launch({
     headless: false,
   });
