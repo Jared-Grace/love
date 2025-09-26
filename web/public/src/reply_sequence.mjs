c8dc1f6ef9942d94463120ad49544227a04e1678
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 16:08:53 -0400

    function_auto_path public\src\reply_sequence.mjs

--- web/public/src/reply_sequence.mjs
+++ web/public/src/reply_sequence.mjs
@@ -1,8 +1,10 @@
+import { list_get } from "./list_get.mjs";
 import { each_index } from "./each_index.mjs";
 export function reply_sequence(items) {
   let v = function reply_sequence_matches(input) {
-    let index = 0;
-    function lambda(item, index2) {}
+    function lambda(item, index) {
+      let item2 = list_get(list2, index2);
+    }
     each_index(list, lambda);
   };
   return v;
