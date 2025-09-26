7418ddc878f1d39abc978366364a3332873de805
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:09:42 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -1,5 +1,4 @@
 import { sleep } from "./sleep.mjs";
-import { log } from "./log.mjs";
 import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
 import { marker } from "./marker.mjs";
 export async function messenger_reply() {
@@ -13,12 +12,10 @@ export async function messenger_reply() {
   const page = await browser.newPage();
   await page.goto("https://www.facebook.com/messages/e2ee/t/9895223143834311");
   const unreadSpanSelector = 'p[dir="auto"]';
-  log("here2");
   let p = await page.waitForSelector(unreadSpanSelector, {
     timeout: 10000,
   });
   await sleep(2000);
-  log("here");
   await p.focus();
   await page.keyboard.type("Greetings!");
   await page.keyboard.press("Enter");
