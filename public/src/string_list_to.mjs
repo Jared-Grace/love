c0393c51bade32610ec48f931466defb0d0f8964
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:16:35 -0400

    function_auto_path public\src\string_list_to.mjs

--- public/src/string_list_to.mjs
+++ public/src/string_list_to.mjs
@@ -1,4 +1,13 @@
-import { marker } from "./marker.mjs";
-export function string_list_to() {
-  marker("1");
+import { undefined } from "./undefined.mjs";
+export function string_list_to(str) {
+  const segmenter = new Intl.Segmenter(undefined, {
+    granularity: "grapheme",
+  });
+  function lambda(s) {
+    let v = s.segment;
+    return v;
+  }
+  let v2 = segmenter.segment(str);
+  const chars = Array.from(v2, lambda);
+  return chars;
 }
