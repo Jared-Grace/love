7e6893b2f59dd3469e533461774a0c3cedcea7fe
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 14:46:41 -0400

    function_auto_path public\src\messenger_reply_unread_collect.mjs

---
+++ web/public/src/messenger_reply_puppeteer.mjs
@@ -0,0 +1,16 @@
+import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
+export async function messenger_reply_puppeteer(lambda) {
+  const puppeteer = await import("puppeteer");
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: messenger_reply_user_data_path(),
+    protocolTimeout: 120000,
+  });
+  const page = await browser.newPage();
+  try {
+    await lambda();
+  } finally {
+    await browser.close();
+  }
+  return page;
+}
--- web/public/src/messenger_reply_unread_collect.mjs
+++ web/public/src/messenger_reply_unread_collect.mjs
@@ -1,3 +1,4 @@
+import { messenger_reply_puppeteer } from "./messenger_reply_puppeteer.mjs";
 import { http_sleep } from "./http_sleep.mjs";
 import { each } from "./each.mjs";
 import { list_add_if_not_includes } from "./list_add_if_not_includes.mjs";
@@ -10,16 +11,9 @@ import { messenger_reply_wait } from "./messenger_reply_wait.mjs";
 import { messenger_reply_unread_click } from "./messenger_reply_unread_click.mjs";
 import { messenger_reply_messages } from "./messenger_reply_messages.mjs";
 import { messenger_reply_url } from "./messenger_reply_url.mjs";
-import { messenger_reply_user_data_path } from "./messenger_reply_user_data_path.mjs";
 export async function messenger_reply_unread_collect() {
-  const puppeteer = await import("puppeteer");
-  const browser = await puppeteer.launch({
-    headless: false,
-    userDataDir: messenger_reply_user_data_path(),
-    protocolTimeout: 120000,
-  });
-  const page = await browser.newPage();
-  try {
+  const page = await messenger_reply_puppeteer(lambda);
+  async function lambda() {
     let v = messenger_reply_url();
     let messages = await messenger_reply_messages(page, v);
     await messenger_reply_unread_click(page);
@@ -49,7 +43,5 @@ export async function messenger_reply_unread_collect() {
       each(urls_new, lambda2);
       await http_sleep();
     }
-  } finally {
-    await browser.close();
   }
 }
