a0908b995db85a42ed93a12b4b2e7120f4cf7634
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:29:10 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -1,3 +1,4 @@
+import { list_last } from "./list_last.mjs";
 import { messenger_reply_messages_me } from "./messenger_reply_messages_me.mjs";
 import { messenger_reply_messages_name } from "./messenger_reply_messages_name.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
@@ -36,6 +37,7 @@ export async function messenger_reply() {
     let property_name = messenger_reply_messages_name();
     let property_value = messenger_reply_messages_me();
     let mine = list_filter_property(messages, property_name, property_value);
+    let last = list_last(list);
     log(messages);
     let answer = await command_line_read_empty();
   }
