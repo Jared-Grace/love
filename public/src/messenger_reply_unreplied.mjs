46004c9a03c72fea15b44371aad842973cbe715d
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:34:57 -0400

    function_auto_path public\src\messenger_reply.mjs

---
+++ public/src/messenger_reply_unreplied.mjs
@@ -0,0 +1,15 @@
+import { list_skip } from "./list_skip.mjs";
+import { list_index_of } from "./list_index_of.mjs";
+import { list_last } from "./list_last.mjs";
+import { list_filter_property } from "./list_filter_property.mjs";
+import { messenger_reply_messages_me } from "./messenger_reply_messages_me.mjs";
+import { messenger_reply_messages_name } from "./messenger_reply_messages_name.mjs";
+export function messenger_reply_unreplied(messages) {
+  let property_name = messenger_reply_messages_name();
+  let property_value = messenger_reply_messages_me();
+  let mine = list_filter_property(messages, property_name, property_value);
+  let last = list_last(mine);
+  let index_last = list_index_of(messages, last);
+  let skipped = list_skip(messages, index_last + 1);
+  return skipped;
+}
