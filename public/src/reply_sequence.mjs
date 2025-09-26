49760ec20511bb72de47a15562c217c31e1b4ce2
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 19:32:34 -0400

    function_auto_path public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -1,7 +1,9 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { list_get } from "./list_get.mjs";
 import { each_index } from "./each_index.mjs";
 export function reply_sequence(sequence) {
-  let fn = function reply_sequence_matches(input) {
+  let fn = function reply_sequence_matches(a) {
+    let input = object_property_get(a, "input");
     let matches = true;
     function lambda(sequence_item, index) {
       let input_item = list_get(input, index);
