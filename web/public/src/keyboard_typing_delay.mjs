2368dda40fe2d224bd106ca3d856ddf615f513cb
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:19:48 -0400

    function_auto_path public\src\keyboard_typing_delay.mjs

--- web/public/src/keyboard_typing_delay.mjs
+++ web/public/src/keyboard_typing_delay.mjs
@@ -5,7 +5,7 @@ export function keyboard_typing_delay() {
   let ms_per_second = 1000;
   let ms_per_minute = seconds_per_minute * ms_per_second;
   let ms_per_character = ms_per_minute / characters_per_minute;
-  let high = ms_per_character * 1.5;
+  let high = ms_per_character * 2;
   let delay_ms = integer_random(ms_per_character, high);
   return delay_ms;
 }
