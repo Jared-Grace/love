8d7320bcab445a236d8a0256d1cf299210d63456
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:50:37 -0400

    function_auto_path public\src\puppeteer_hrefs_starts_with.mjs

--- web/public/src/puppeteer_hrefs_starts_with.mjs
+++ web/public/src/puppeteer_hrefs_starts_with.mjs
@@ -1,7 +1,9 @@
+import { log } from "./log.mjs";
 import { puppeteer_hrefs } from "./puppeteer_hrefs.mjs";
 import { list_filter_starts_with } from "./list_filter_starts_with.mjs";
 export async function puppeteer_hrefs_starts_with(page, prefix) {
   const hrefs = await puppeteer_hrefs(page);
   let filtered = list_filter_starts_with(hrefs, prefix);
+  log(message);
   return filtered;
 }
