1c37d5b982e4f94de5700e34b73ade5fa770bb56
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 19:41:11 -0400

    function_auto_path public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,11 +1,8 @@
-import { list_add } from "./list_add.mjs";
-import { object_property_get } from "./object_property_get.mjs";
 export function reply_on_match(wrapped_fn, on_match) {
   let fn = async function reply_on_match_inner(a) {
     let matches = wrapped_fn(a);
     if (matches) {
-      let outputs = object_property_get(a, "outputs");
-      list_add(outputs, await on_match());
+      await on_match(a);
     }
   };
   return fn;
