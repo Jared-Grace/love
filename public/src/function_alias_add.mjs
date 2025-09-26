da85eaa66709f44663d4878bc5f03bf014589e77
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:40:38 -0400

    o a

--- public/src/function_alias_add.mjs
+++ public/src/function_alias_add.mjs
@@ -1,8 +1,10 @@
+import { marker } from "./marker.mjs";
 import { list_sort_string_size } from "./list_sort_string_size.mjs";
 import { function_alias_generic } from "./function_alias_generic.mjs";
 import { error } from "./error.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 export async function function_alias_add(first, second) {
+  marker("1");
   let list = [first, second];
   list_sort_string_size(list);
   let [alias, f_name] = list;
