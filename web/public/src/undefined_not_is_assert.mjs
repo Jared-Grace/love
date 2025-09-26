764b1a2b9cc4c5e359fcb80c84747d7e13dfd6f1
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:43:36 -0400

    function_auto_path public\src\undefined_not_is_assert.mjs

--- web/public/src/undefined_not_is_assert.mjs
+++ web/public/src/undefined_not_is_assert.mjs
@@ -2,6 +2,10 @@ import { undefined_not_is_assert_lambda } from "./undefined_not_is_assert_lambda
 import { marker } from "./marker.mjs";
 export function undefined_not_is_assert(value, lambda) {
   marker("1");
-  let v = undefined_not_is_assert_lambda(value, lambda);
+  function lambda2() {
+    let v2 = {};
+    return v2;
+  }
+  let v = undefined_not_is_assert_lambda(value, lambda2);
   return v;
 }
