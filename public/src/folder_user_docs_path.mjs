5462dbcdb22c9ff097773fe60cf2afe76bc6c4af
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 11:09:55 -0400

    function_auto_path public\src\folder_user_docs_path.mjs

--- public/src/folder_user_docs_path.mjs
+++ public/src/folder_user_docs_path.mjs
@@ -1,4 +1,4 @@
 export function folder_user_docs_path(file_name) {
-  let v3 = "C:\\Users\\chris\\Documents\\" + file_name;
-  return v3;
+  let p = "C:\\Users\\chris\\Documents\\" + file_name;
+  return p;
 }
