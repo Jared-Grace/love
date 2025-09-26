eb611e7d5f1956d602749e781db77ecb03d3a6da
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 22:22:33 -0400

    function_auto_path public\src\messenger_reply_login.mjs

--- public/src/messenger_reply_login.mjs
+++ public/src/messenger_reply_login.mjs
@@ -1,6 +1,5 @@
 import { command_line_read } from "./command_line_read.mjs";
 import { import_install } from "./import_install.mjs";
-import { marker } from "./marker.mjs";
 export async function messenger_reply_login() {
   const { chromium, firefox, webkit } = await import_install("playwright");
   const browser = await chromium.launch({
@@ -14,5 +13,4 @@ export async function messenger_reply_login() {
     path: "fb-session.json",
   });
   await browser.close();
-  marker("1");
 }
