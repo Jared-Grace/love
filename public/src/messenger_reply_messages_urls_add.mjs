c1326b6ae342e007ccb67cc5d68244a1ff813986
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 13:28:55 -0400

    function_auto_path public\src\messenger_reply_messages_urls_add.mjs

--- public/src/messenger_reply_messages_urls_add.mjs
+++ public/src/messenger_reply_messages_urls_add.mjs
@@ -3,6 +3,7 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { file_json_transform } from "./file_json_transform.mjs";
 import { folder_user_docs_path } from "./folder_user_docs_path.mjs";
 export async function messenger_reply_messages_urls_add(url) {
+  let urls = [url];
   let fb_path = folder_user_docs_path("fb.json");
   await file_json_transform(fb_path, transform);
   function transform(data) {
