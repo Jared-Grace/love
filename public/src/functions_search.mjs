d58aa71fe125988f12601da18b8de03b51df3f2e
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:41:24 -0400

    o s

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { search_generic } from "./search_generic.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { functions_names } from "./functions_names.mjs";
 export async function functions_search(search) {
+  marker("1");
   let f_names = functions_names();
   let result = search_generic(search, f_names, function_name_to_path);
   return result;
