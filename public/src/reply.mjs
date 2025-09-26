a8fc0eeec4179203887a6fa872cc7767cb926b0d
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:44:26 -0400

    function_auto_path public\src\reply.mjs

--- public/src/reply.mjs
+++ public/src/reply.mjs
@@ -1,9 +1,10 @@
+import { string_split_space } from "./string_split_space.mjs";
 import { list_map } from "./list_map.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
 import { marker } from "./marker.mjs";
 export function reply(input) {
   input = whitespace_normalize(input);
-  sss;
+  let split = string_split_space(s);
   function lambda(item) {}
   let mapped = list_map(list, lambda);
   marker("1");
