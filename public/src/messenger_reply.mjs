ec077f10cbceaffd92f40b033b22898794c0a13c
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 14:36:15 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -22,6 +22,7 @@ export async function messenger_reply() {
   const browser = await puppeteer.launch({
     headless: false,
     userDataDir: messenger_reply_user_data_path(),
+    protocolTimeout: 120000,
   });
   const page = await browser.newPage();
   let v = messenger_reply_url();
