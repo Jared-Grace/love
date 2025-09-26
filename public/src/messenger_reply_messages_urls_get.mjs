bab4d2cd816437b6c7d6934c03d136b60c8c8b08
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 14:48:41 -0400

    function_auto_path public\src\messenger_reply_messages_urls_add_multiple.mjs

---
+++ public/src/messenger_reply_messages_urls_get.mjs
@@ -0,0 +1,5 @@
+import { object_property_initialize } from "./object_property_initialize.mjs";
+export function messenger_reply_messages_urls_get(data) {
+  let value = object_property_initialize(data, "messages_urls", {});
+  return value;
+}
