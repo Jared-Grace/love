a8718b8053152d7d148f7fa3147687644e8ba8ab
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:54:29 -0400

    function_auto_path public\src\reply_test.mjs

--- public/src/reply_test.mjs
+++ public/src/reply_test.mjs
@@ -1,9 +1,9 @@
 import { messenger_reply_messages_transform } from "./messenger_reply_messages_transform.mjs";
-import { object_property_set } from "./object_property_set.mjs";
 import { marker } from "./marker.mjs";
 export async function reply_test() {
-  async function lambda(messages2) {
-    object_property_set(messages2, joined, 1);
+  let messages = null;
+  async function lambda(m) {
+    messages = m;
   }
   await messenger_reply_messages_transform(lambda);
   marker("1");
