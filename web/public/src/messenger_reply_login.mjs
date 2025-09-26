35d718821c25e6c257e1a37f3bfd5fd0cf747ac9
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 22:22:01 -0400

    function_auto_path public\src\messenger_reply_login.mjs

--- web/public/src/messenger_reply_login.mjs
+++ web/public/src/messenger_reply_login.mjs
@@ -1,6 +1,6 @@
 import { import_install } from "./import_install.mjs";
 import { marker } from "./marker.mjs";
 export async function messenger_reply_login() {
-  await import_install(name);
+  const { chromium, firefox, webkit } = await import_install("playwright");
   marker("1");
 }
