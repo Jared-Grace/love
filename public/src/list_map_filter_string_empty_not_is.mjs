123d63914f136951fcf65a444a1f0e672a96c668
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:45:33 -0400

    function_auto_path public\src\list_map_filter_string_empty_not_is.mjs

--- public/src/list_map_filter_string_empty_not_is.mjs
+++ public/src/list_map_filter_string_empty_not_is.mjs
@@ -3,10 +3,6 @@ import { marker } from "./marker.mjs";
 import { list_map } from "./list_map.mjs";
 export function list_map_filter_string_empty_not_is(list) {
   marker("1");
-  function lambda(item) {
-    let filtered = list_filter_empty_not_is(item);
-    return filtered;
-  }
-  let mapped4 = list_map(list, lambda);
+  let mapped4 = list_map(list, list_filter_empty_not_is);
   return mapped4;
 }
