95bb4864e961337ee83dfabc78f7046691b49339
Grace-Jared <christrosetolife@gmail.com>
2025-09-25 20:31:09 -0400

    function_auto_path public\src\catch_only_run_async.mjs

---
+++ public/src/catch_only_run_async.mjs
@@ -0,0 +1,15 @@
+import { not } from "./not.mjs";
+import { json_to } from "./json_to.mjs";
+export async function catch_only_run_async(lambda, message_fragment) {
+  try {
+    await lambda();
+  } catch (e) {
+    let json = json_to(e);
+    const message = e.message;
+    let a = message.includes(message_fragment);
+    let a2 = json.includes(message_fragment);
+    if (not(a) && not(a2)) {
+      throw e;
+    }
+  }
+}
