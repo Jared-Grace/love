42558ce840f0e8ee9770868d2081a0c8eead5a47
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 11:06:36 -0400

    function_auto_path public\src\bind_property.mjs

--- public/src/bind_property.mjs
+++ public/src/bind_property.mjs
@@ -1,4 +1,6 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { marker } from "./marker.mjs";
-export function bind_property() {
+export function bind_property(object, property_name) {
   marker("1");
+  let property_name2 = object_property_get(object, "property_name");
 }
