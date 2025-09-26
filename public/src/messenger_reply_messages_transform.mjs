89e8821e73ef8629dfc4d8b209c867f3c8196183
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 15:50:43 -0400

    function_auto_path public\src\messenger_reply_messages_transform.mjs

--- public/src/messenger_reply_messages_transform.mjs
+++ public/src/messenger_reply_messages_transform.mjs
@@ -2,12 +2,12 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { marker } from "./marker.mjs";
 import { file_json_transform } from "./file_json_transform.mjs";
 import { folder_user_docs_path } from "./folder_user_docs_path.mjs";
-export async function messenger_reply_messages_transform(transform_inner) {
+export async function messenger_reply_messages_transform(lambda$messages) {
   marker("1");
   let fb_path = folder_user_docs_path("fb.json");
   await file_json_transform(fb_path, transform);
   function transform(data) {
     let value = object_property_initialize(data, "messages", {});
-    transform_inner(value);
+    lambda$messages(value);
   }
 }
