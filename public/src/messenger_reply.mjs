8b482e63248857913d58db4cf471df6af70a120b
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 14:19:31 -0400

    function_auto_path public\src\messenger_reply.mjs

--- public/src/messenger_reply.mjs
+++ public/src/messenger_reply.mjs
@@ -26,7 +26,12 @@ export async function messenger_reply() {
   await messenger_reply_unread_click(page);
   await messenger_reply_wait(page);
   let urls = await messenger_reply_messages_urls_add_page(page);
-  async function lambda(url) {
+  async function lambda(url) {}
+  while (true) {
+    let e = list_empty_is(urls);
+    if (e) {
+      break;
+    }
     let prefix = "https://www.facebook.com";
     let without = string_prefix_without(url, prefix);
     const selector = `a[href="${without}"]`;
@@ -37,12 +42,6 @@ export async function messenger_reply() {
     });
     await http_sleep();
     await messenger_reply_messages_urls_add_page(page);
-  }
-  while (true) {
-    let e = list_empty_is(urls);
-    if (e) {
-      break;
-    }
   }
   await each_async(urls, lambda);
   let answer = await command_line_read_empty();
