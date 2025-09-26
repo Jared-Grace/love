5f2c94069787fd3b54940e953036e568fda72c3a
Grace-Jared <christrosetolife@gmail.com>
2025-09-26 16:14:54 -0400

    function_auto_path public\src\reply.mjs

--- public/src/reply.mjs
+++ public/src/reply.mjs
@@ -1,10 +1,12 @@
+import { ebible_references_parse_lines } from "./ebible_references_parse_lines.mjs";
 import { reply_sequence } from "./reply_sequence.mjs";
 import { list_map_lower } from "./list_map_lower.mjs";
 import { list_filter_empty_not_is } from "./list_filter_empty_not_is.mjs";
 import { string_split_space } from "./string_split_space.mjs";
 import { whitespace_normalize } from "./whitespace_normalize.mjs";
-export function reply(input) {
+export async function reply(input) {
   let reference = "Isaiah 26:9";
+  let list = await ebible_references_parse_lines(bible_folders, lines);
   let fn = reply_sequence(["good", "evening"]);
   let r = reply_on_match(fn);
   input = whitespace_normalize(input);
