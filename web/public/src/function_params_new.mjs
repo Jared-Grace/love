972f647715243f60802603f32697e2a89491cf7c
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:04:20 -0400

    function_auto_path public\src\function_params_new.mjs

--- web/public/src/function_params_new.mjs
+++ web/public/src/function_params_new.mjs
@@ -1,4 +1,3 @@
-import { null_is } from "./null_is.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { data_function_current_get } from "./data_function_current_get.mjs";
 import { each_async } from "./each_async.mjs";
@@ -29,7 +28,7 @@ export async function function_params_new(
       function lambda2(v) {
         let { node } = v;
         let name = js_call_callee_name(node);
-        if (null_is(value)) {
+        if (false) {
         }
         let arguments2 = object_property_get(node, "arguments");
         function lambda3(value_default) {
