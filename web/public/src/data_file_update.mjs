65d6dd29013426b3bc29205efc09a87af53a3d17
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:17:38 -0400

    pn f_path data.json

--- web/public/src/data_file_update.mjs
+++ web/public/src/data_file_update.mjs
@@ -4,7 +4,7 @@ import { data_save } from "./data_save.mjs";
 import { data_all } from "./data_all.mjs";
 import { marker } from "./marker.mjs";
 export async function data_file_update(f_path) {
-  var d = await data_all();
+  var d = await data_all("data.json");
   let parsed = await file_js_parse(f_path);
   await data_file_update_inner(parsed, d);
   await data_save(d);
