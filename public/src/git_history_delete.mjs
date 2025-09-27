9916b6591f09ad0921090d7292459a50d983ef46
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 17:41:10 -0400

    function_auto_path public\src\git_history_delete.mjs

--- public/src/git_history_delete.mjs
+++ public/src/git_history_delete.mjs
@@ -2,5 +2,7 @@ import { command_line } from "./command_line.mjs";
 import { marker } from "./marker.mjs";
 export async function git_history_delete() {
   marker("1");
-  let stdout = await command_line(command);
+  let stdout = await command_line(
+    "git clone --mirror https://github.com/<user>/<repo>.git repo-clean.git",
+  );
 }
