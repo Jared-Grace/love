8d1e9fcdbc5c75e9e70a78e8eabd1645bcf9e541
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:19:13 -0400

    function_auto_path public\src\git_repo_rename.mjs

--- public/src/git_repo_rename.mjs
+++ public/src/git_repo_rename.mjs
@@ -3,6 +3,6 @@ import { marker } from "./marker.mjs";
 export async function git_repo_rename(from, to) {
   marker("1");
   let stdout = await command_line(
-    "gh repo rename <old-repo-name> --new-name <new-repo-name>",
+    "gh repo rename " + from + " --new-name " + to,
   );
 }
