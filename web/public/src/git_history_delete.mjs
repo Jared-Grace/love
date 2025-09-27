e7c614072c6bf19759aae8e01bf2b62b579d8b93
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 17:42:36 -0400

    function_auto_path public\src\git_history_delete.mjs

--- web/public/src/git_history_delete.mjs
+++ web/public/src/git_history_delete.mjs
@@ -3,6 +3,7 @@ import { command_line } from "./command_line.mjs";
 import { marker } from "./marker.mjs";
 export async function git_history_delete(user, repo) {
   marker("1");
+  ("make sure all changes are in repo first like pushing");
   let stdout = await command_line(
     "git clone --mirror https://github.com/" +
       user +
