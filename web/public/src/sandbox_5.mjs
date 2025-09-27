d231778464e748284ed61226308770caa3845d2d
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 17:36:41 -0400

    function_auto_path public\src\sandbox_5.mjs

--- web/public/src/sandbox_5.mjs
+++ web/public/src/sandbox_5.mjs
@@ -1,9 +1,1 @@
-import { command_line } from "./command_line.mjs";
-import { log } from "./log.mjs";
-import { marker } from "./marker.mjs";
-export async function sandbox_5() {
-  marker("1");
-  log("hi ish");
-  let stdout = await command_line("dir");
-  return stdout;
-}
+export async function sandbox_5() {}
