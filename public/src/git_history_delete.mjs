cea05c25f6f30a32e3f63eb328b66d1b0e6066c4
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 17:31:26 -0400

    function_auto_path public\src\git_history_delete.mjs

--- public/src/git_history_delete.mjs
+++ public/src/git_history_delete.mjs
@@ -1,6 +1,4 @@
-import { command_line } from "./command_line.mjs";
 import { marker } from "./marker.mjs";
-export async function git_history_delete() {
+export function git_history_delete() {
   marker("1");
-  let stdout = await command_line(command);
 }
