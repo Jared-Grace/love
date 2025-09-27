6ec9b1035d20647f8426d6d60f2f1da0483e749a
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:13:29 -0400

    fs data_all

--- public/src/data_all.mjs
+++ public/src/data_all.mjs
@@ -1,5 +1,7 @@
+import { marker } from "./marker.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function data_all() {
+  marker("1");
   const file_path = "data.json";
   let data = await file_read_json(file_path);
   let v = {
