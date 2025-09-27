0f5ac985917d5979ba5bb0081c78ed84cdfb2612
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:07:29 -0400

    function_auto_path public\src\command_line_node_g.mjs

--- web/public/src/command_line_node_g.mjs
+++ web/public/src/command_line_node_g.mjs
@@ -6,5 +6,5 @@ export async function command_line_node_g(f_name, args) {
   let result = list_join_space(args);
   let output = await command_line("node g.mjs " + f_name + " " + result);
   let stdout = object_property_get(output, "stdout");
-  log_keep(output);
+  log_keep(stdout);
 }
