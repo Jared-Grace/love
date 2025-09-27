7b14c83a31bb7023c17b28d76266310803081146
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:31:02 -0400

    fs data_get

--- web/public/src/data_get.mjs
+++ web/public/src/data_get.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { data_all } from "./data_all.mjs";
 import { data_path } from "./data_path.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export async function data_get(property_name, value_initial) {
+  marker("1");
   let d_path = data_path();
   var { data, file_path } = await data_all(d_path);
   let value = object_property_initialize(data, property_name, value_initial);
