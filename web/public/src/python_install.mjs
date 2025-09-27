283f5df7c3bee64de09ead84b47b5b8a887bedd6
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 15:20:24 -0400

    function_auto_path public\src\python_install.mjs

--- web/public/src/python_install.mjs
+++ web/public/src/python_install.mjs
@@ -1,7 +1,7 @@
 import { command_line } from "./command_line.mjs";
 import { marker } from "./marker.mjs";
-export async function python_install(command) {
+export async function python_install() {
   marker("1");
-  let v = await command_line(command);
+  let v = await command_line("winget install -e --id Python.Python.3.12");
   return v;
 }
