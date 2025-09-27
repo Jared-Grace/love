d40d990c89a96656ba360515541bc423e4c2bca1
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:53:48 -0400

    function_auto_path public\src\function_params_new.mjs

--- public/src/function_params_new.mjs
+++ public/src/function_params_new.mjs
@@ -1,4 +1,3 @@
-import { list_adder } from "./list_adder.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { js_call_callee_name } from "./js_call_callee_name.mjs";
@@ -6,7 +5,10 @@ import { data_identifiers_search } from "./data_identifiers_search.mjs";
 import { function_transform_current } from "./function_transform_current.mjs";
 import { js_declaration_single_params_add } from "./js_declaration_single_params_add.mjs";
 import { string_split } from "./string_split.mjs";
-export async function function_params_new(param_names_comma, value_default) {
+export async function function_params_new(
+  param_names_comma,
+  values_default_comma,
+) {
   let param_names = string_split(param_names_comma, ",");
   await function_transform_current(lambda);
   function lambda(ast) {
@@ -16,8 +18,6 @@ export async function function_params_new(param_names_comma, value_default) {
   function lambda2(v) {
     let { node } = v;
     let arguments2 = object_property_get(node, "arguments");
-    function lambda3(la) {}
-    let list = list_adder(lambda3);
   }
   js_visit_type(ast2, "CallExpression", lambda2);
   let name = js_call_callee_name(expression);
