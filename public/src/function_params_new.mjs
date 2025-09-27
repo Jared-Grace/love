42017b2741bd665167759e80987d8bf0f9d0da11
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 15:12:57 -0400

    function_auto_path public\src\function_params_new.mjs

--- public/src/function_params_new.mjs
+++ public/src/function_params_new.mjs
@@ -1,3 +1,4 @@
+import { data_identifiers_search } from "./data_identifiers_search.mjs";
 import { function_transform_current } from "./function_transform_current.mjs";
 import { js_declaration_single_params_add } from "./js_declaration_single_params_add.mjs";
 import { string_split } from "./string_split.mjs";
@@ -7,4 +8,5 @@ export async function function_params_new(param_names_comma) {
   function lambda(ast) {
     js_declaration_single_params_add(ast, param_names);
   }
+  let result = await data_identifiers_search(s);
 }
