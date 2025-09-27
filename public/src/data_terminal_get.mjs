62ec29f0bc9531ec00af3dcef7b031f66e3a28e9
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:30:40 -0400

    function_auto_path public\src\data_terminal_get.mjs

--- public/src/data_terminal_get.mjs
+++ public/src/data_terminal_get.mjs
@@ -1,6 +1,8 @@
+import { data_path } from "./data_path.mjs";
 import { marker } from "./marker.mjs";
 import { data_property_get } from "./data_property_get.mjs";
 export async function data_terminal_get() {
+  let d_path = data_path();
   marker("1");
   const property_name = "terminal";
   let w = await data_property_get(property_name, d_path);
