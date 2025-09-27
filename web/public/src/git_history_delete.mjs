6d9c1f2166d4c013f29cee2696bc6a8c263c8de9
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:40:25 -0400

    function_auto_path public\src\git_history_delete.mjs

--- web/public/src/git_history_delete.mjs
+++ web/public/src/git_history_delete.mjs
@@ -1,3 +1,4 @@
+import { git_repo_url } from "./git_repo_url.mjs";
 import { folder_delete } from "./folder_delete.mjs";
 import { todo } from "./todo.mjs";
 import { command_line_git } from "./command_line_git.mjs";
@@ -9,13 +10,13 @@ export async function git_history_delete(user, repo, f_path) {
     "make sure all changes are in repo first like pushing; may need to coordinate with other users",
   );
   todo("make sure this is run from the correct directory");
-  const git_repo_url = "https://github.com/" + user + "/" + repo + ".git";
+  const url = git_repo_url(user, repo);
   const repo_folder = repo + "-clean-" + (await uuid()) + ".git";
   let stdout = await command_line_git(
-    "clone --mirror " + git_repo_url + " " + repo_folder,
+    "clone --mirror " + url + " " + repo_folder,
   );
   process.chdir(repo_folder);
-  let stdout2 = await command_line_git("remote add origin " + git_repo_url);
+  let stdout2 = await command_line_git("remote add origin " + url);
   await command_line_git("filter-repo --path " + f_path + " --invert-paths");
   await command_line_git("push --force --all origin");
   await command_line_git("push --force --tags origin");
