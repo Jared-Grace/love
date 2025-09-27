7519286a33991f461a339ac1c847760288c8396c
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 15:21:26 -0400

    function_auto_path public\src\git_filter_repo_install.mjs

--- web/public/src/git_filter_repo_install.mjs
+++ web/public/src/git_filter_repo_install.mjs
@@ -1,6 +1,6 @@
 import { command_line } from "./command_line.mjs";
 import { marker } from "./marker.mjs";
-export async function git_filter_repo_install(command) {
+export async function git_filter_repo_install() {
   marker("1");
   let v = await command_line("python -m pip install git-filter-repo");
   return v;
