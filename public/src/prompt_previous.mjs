9b8c058526f13eb922e4c37b94fa2ed99c15dfee
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:29:43 -0400

    function_auto_path public\src\prompt_previous.mjs

--- public/src/prompt_previous.mjs
+++ public/src/prompt_previous.mjs
@@ -2,18 +2,11 @@ import { marker } from "./marker.mjs";
 import { log_keep } from "./log_keep.mjs";
 import { data_prompts } from "./data_prompts.mjs";
 import { function_aliases_inverted } from "./function_aliases_inverted.mjs";
-import { object_invert } from "./object_invert.mjs";
-import { function_aliases } from "./function_aliases.mjs";
 import { function_run_line } from "./function_run_line.mjs";
-import { data_value } from "./data_value.mjs";
-import { data_transform } from "./data_transform.mjs";
-import { function_run_prompt } from "./function_run_prompt.mjs";
 import { list_last } from "./list_last.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { list_difference } from "./list_difference.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-import { log } from "./log.mjs";
 export async function prompt_previous() {
   marker("1");
   let inverted = await function_aliases_inverted();
