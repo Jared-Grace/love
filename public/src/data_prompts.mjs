c2f86d6c651d9d42e27601420ee349e5e4b026fa
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:29:42 -0400

    pn d_path d_path

--- public/src/data_prompts.mjs
+++ public/src/data_prompts.mjs
@@ -1,7 +1,7 @@
 import { list_slice_end } from "./list_slice_end.mjs";
 import { data_value } from "./data_value.mjs";
 export async function data_prompts() {
-  let prompts = await data_value("prompts");
+  let prompts = await data_value("prompts", d_path);
   let offset = 25;
   let result = list_slice_end(prompts, offset);
   return result;
