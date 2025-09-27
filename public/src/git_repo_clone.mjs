5f3c45f90f953bcbe2a375ea0ede0ab03862c25f
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:42:50 -0400

    function_auto_path public\src\git_repo_clone.mjs

--- public/src/git_repo_clone.mjs
+++ public/src/git_repo_clone.mjs
@@ -1,6 +1,8 @@
+import { command_line_git } from "./command_line_git.mjs";
 import { git_repo_url } from "./git_repo_url.mjs";
 import { marker } from "./marker.mjs";
-export function git_repo_clone(user, repo) {
+export async function git_repo_clone(user, repo) {
   marker("1");
   const url = git_repo_url(user, repo);
+  await command_line_git(command_git);
 }
