7a07d0d2d39f5d64e9282836e8f94f21565a1646
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:23:25 -0400

    pn d_path d_path

--- public/src/data_value.mjs
+++ public/src/data_value.mjs
@@ -1,7 +1,7 @@
 import { object_property_get } from "./object_property_get.mjs";
 import { data_all } from "./data_all.mjs";
 export async function data_value(property_name) {
-  var { data } = await data_all();
+  var { data } = await data_all(d_path);
   let value = object_property_get(data, property_name);
   return value;
 }
