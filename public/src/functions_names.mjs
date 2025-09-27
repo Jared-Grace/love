bcde24e773ec01044412affa8ecf053dcf6d56a2
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:36:19 -0400

    r folder_read folder_read_files

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,11 +1,11 @@
 import { functions_path } from "./functions_path.mjs";
-import { folder_read } from "./folder_read.mjs";
+import { folder_read_files } from "./folder_read_files.mjs";
 import { function_name_extension } from "./function_name_extension.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { list_map } from "./list_map.mjs";
 export function functions_names() {
   let dirPath = functions_path();
-  let paths = folder_read(dirPath);
+  let paths = folder_read_files(dirPath);
   function lambda(p) {
     let suffix = function_name_extension();
     let without = string_suffix_without(p, suffix);
--- public/src/repos.mjs
+++ public/src/repos.mjs
@@ -1,7 +1,7 @@
-import { folder_read } from "./folder_read.mjs";
+import { folder_read_files } from "./folder_read_files.mjs";
 import { marker } from "./marker.mjs";
 export function repos() {
   marker("1");
-  let files = folder_read("..");
+  let files = folder_read_files("..");
   return files;
 }
