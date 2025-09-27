70096f7b36807377692e81fbff15b9553fd6685a
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:01:54 -0400

    js_dollar_new ine

---
+++ public/src/js_dollar_ine.mjs
@@ -0,0 +1,13 @@
+import { marker } from "./marker.mjs";
+export function js_dollar_ine({
+  remaining,
+  node,
+  stack1,
+  stack2,
+  stack3,
+  ast,
+  afters,
+}) {
+  marker("1");
+  return;
+}
