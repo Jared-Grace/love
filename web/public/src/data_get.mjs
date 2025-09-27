14c0dd2a506c81c1748a527581e984dfd91ec2a5
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:21:14 -0400

    function_auto_path public\src\data_get.mjs

--- web/public/src/data_get.mjs
+++ web/public/src/data_get.mjs
@@ -1,7 +1,7 @@
 import { data_all } from "./data_all.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export async function data_get(property_name, value_initial) {
-  var { data, file_path } = await data_all("data.json");
+  var { data, file_path } = await data_all();
   let value = object_property_initialize(data, property_name, value_initial);
   let v = {
     value,
