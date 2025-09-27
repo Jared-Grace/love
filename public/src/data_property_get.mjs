d6e4a8be156fed230b0eb7c117952bd1921e3100
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:25:08 -0400

    function_auto_path public\src\data_property_get.mjs

--- public/src/data_property_get.mjs
+++ public/src/data_property_get.mjs
@@ -3,8 +3,7 @@ import { object_property_get } from "./object_property_get.mjs";
 import { data_get } from "./data_get.mjs";
 export async function data_property_get(property_name) {
   marker("1");
-  var d = await data_get(property_name, null);
-  let { data } = d;
+  let { data } = await data_get(property_name, null);
   let value = object_property_get(data, property_name);
   return value;
 }
