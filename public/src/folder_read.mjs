1535e876a8765633a9d4a72f2b8774201a56c9cf
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:36:58 -0400

    function_auto_path public\src\folder_read.mjs

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,4 +1,11 @@
+import { path_join } from "./path_join.mjs";
 import { marker } from "./marker.mjs";
 export function folder_read() {
   marker("1");
+  function lambda(file) {
+    let result = path_join([path_folder, file]);
+    let v = fs.statSync(result).isFile();
+    return v;
+  }
+  const all = fs.readdirSync(path_folder);
 }
