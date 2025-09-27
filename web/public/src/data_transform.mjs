a9a844d33f85ec8b96a53f378542f843d610779a
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:53:54 -0400

    s combine

--- web/public/src/data_transform.mjs
+++ web/public/src/data_transform.mjs
@@ -7,12 +7,8 @@ export async function data_transform(
   value_initial,
   lambda$previous,
 ) {
-  let d_path = data_path();
-  let { data, value: value_previous } = await data_get(
-    property_name,
-    value_initial,
-    d_path,
-  );
+  var d = await data_get(property_name, value_initial, data_path());
+  let { data, value: value_previous } = d;
   const value = await lambda$previous(value_previous);
   object_property_set(data, property_name, value);
   await data_save(d);
