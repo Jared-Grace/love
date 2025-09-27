e0a198f248f9fa75727d31dc25e641587eca2fe5
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:37:12 -0400

    function_auto_path public\src\folder_read.mjs

--- web/public/src/folder_read.mjs
+++ web/public/src/folder_read.mjs
@@ -1,6 +1,6 @@
 import { path_join } from "./path_join.mjs";
 import { marker } from "./marker.mjs";
-export function folder_read() {
+export function folder_read(path_folder) {
   marker("1");
   function lambda(file) {
     let result = path_join([path_folder, file]);
