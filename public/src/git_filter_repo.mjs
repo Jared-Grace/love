9f283e5e49c6f9e2eb57a9b6de074582c6c91554
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 15:21:22 -0400

    r git_filter_repo git_filter_repo_install

--- public/src/git_filter_repo.mjs
+++
@@ -1,7 +0,0 @@
-import { command_line } from "./command_line.mjs";
-import { marker } from "./marker.mjs";
-export async function git_filter_repo(command) {
-  marker("1");
-  let v = await command_line("python -m pip install git-filter-repo");
-  return v;
-}
