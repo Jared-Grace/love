347c9452012c540c368433da26eeeeb2aa78e148
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 19:25:08 -0400

    fs data_property_get

--- web/public/src/data_property_get.mjs
+++ web/public/src/data_property_get.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { data_get } from "./data_get.mjs";
 export async function data_property_get(property_name) {
+  marker("1");
   var d = await data_get(property_name, null);
   let { data } = d;
   let value = object_property_get(data, property_name);
