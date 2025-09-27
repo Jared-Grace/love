64738f5b3f9953e5975bd0766e89e0d3a3eaa3a9
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:59:25 -0400

    function_auto_path public\src\function_params_new.mjs

--- public/src/function_params_new.mjs
+++ public/src/function_params_new.mjs
@@ -24,18 +24,19 @@ export async function function_params_new(
   let f_name_current = await data_function_current_get();
   let result = await data_identifiers_search(f_name_current);
   async function lambda4(f_name) {
-    function lambda2(v) {
-      let { node } = v;
-      let arguments2 = object_property_get(node, "arguments");
-      function lambda3(value_default) {
-        let expression2 = js_parse_expression(value_default);
-        list_add(arguments2, expression2);
+    async function lambda5(ast) {
+      function lambda2(v) {
+        let { node } = v;
+        let arguments2 = object_property_get(node, "arguments");
+        function lambda3(value_default) {
+          let expression2 = js_parse_expression(value_default);
+          list_add(arguments2, expression2);
+        }
+        each(values_default, lambda3);
       }
-      each(values_default, lambda3);
+      js_visit_type(ast2, "CallExpression", lambda2);
     }
-    js_visit_type(ast2, "CallExpression", lambda2);
-    async function lambda5(ast3) {}
-    let output = await function_transform(f_name2, lambda5);
+    let output = await function_transform(f_name, lambda5);
   }
   await each_async(result, lambda4);
   let name = js_call_callee_name(expression);
