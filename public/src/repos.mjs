e359c9b309c92e4c3bac70998fbf40f25ab914b7
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:35:40 -0400

    function_auto_path public\src\repos.mjs

--- public/src/repos.mjs
+++ public/src/repos.mjs
@@ -2,5 +2,5 @@ import { folder_read } from "./folder_read.mjs";
 import { marker } from "./marker.mjs";
 export function repos() {
   marker("1");
-  let files = folder_read(path_folder);
+  let files = folder_read("..");
 }
