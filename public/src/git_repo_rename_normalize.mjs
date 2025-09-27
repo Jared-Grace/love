afbdaf5002af25c9d21c67455f1b5846fb6ce41e
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:30:28 -0400

    function_auto_path public\src\git_repo_rename_normalize.mjs

--- public/src/git_repo_rename_normalize.mjs
+++ public/src/git_repo_rename_normalize.mjs
@@ -1,10 +1,10 @@
 import { string_replace } from "./string_replace.mjs";
 import { git_repo_rename } from "./git_repo_rename.mjs";
 import { marker } from "./marker.mjs";
-export async function git_repo_rename_normalize(owner, from) {
+export async function git_repo_rename_normalize(owner, name_old) {
   marker("1");
-  let name = string_replace(from, "-", "_");
-  let stdout = await git_repo_rename(owner, from, name);
+  let name = string_replace(name_old, "-", "_");
+  let stdout = await git_repo_rename(owner, name_old, name);
   let result = {
     name,
     stdout,
