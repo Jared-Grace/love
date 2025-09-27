ebc991442362fca4c7c4bbb9b8946b29010ecf5c
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:54:13 -0400

    function_auto_path public\src\function_params_new.mjs

--- web/public/src/function_params_new.mjs
+++ web/public/src/function_params_new.mjs
@@ -1,3 +1,4 @@
+import { js_parse_expression } from "./js_parse_expression.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
 import { js_call_callee_name } from "./js_call_callee_name.mjs";
@@ -15,9 +16,11 @@ export async function function_params_new(
     js_declaration_single_params_add(ast, param_names);
   }
   let result = await data_identifiers_search(s);
+  let values_default = string_split(values_default_comma, ",");
   function lambda2(v) {
     let { node } = v;
     let arguments2 = object_property_get(node, "arguments");
+    let expression2 = js_parse_expression(code_expression);
   }
   js_visit_type(ast2, "CallExpression", lambda2);
   let name = js_call_callee_name(expression);
