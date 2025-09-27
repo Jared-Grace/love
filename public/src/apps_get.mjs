41d65a45c3fda129c6a04b835aaf7bd887cfff1e
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 18:36:19 -0400

    r folder_read folder_read_files

--- public/src/apps_get.mjs
+++ public/src/apps_get.mjs
@@ -1,9 +1,9 @@
 import { folder_public } from "./folder_public.mjs";
-import { folder_read } from "./folder_read.mjs";
+import { folder_read_files } from "./folder_read_files.mjs";
 import { marker } from "./marker.mjs";
 export async function apps_get() {
   let fop = folder_public();
-  let fr = folder_read(fop);
+  let fr = folder_read_files(fop);
   marker("1");
   return fr;
 }
--- public/src/folder_read_files.mjs
+++ public/src/folder_read_files.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { path_join } from "./path_join.mjs";
 import fs from "fs";
 export function folder_read_files(path_folder) {
+  marker("1");
   function lambda(file) {
     let result = path_join([path_folder, file]);
     let v = fs.statSync(result).isFile();
