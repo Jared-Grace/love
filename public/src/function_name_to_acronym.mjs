01c6093d588b8e692ccdd017d600b648900b25b6
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:59:09 -0400

    function_auto_path public\src\function_name_to_acronym.mjs

--- public/src/function_name_to_acronym.mjs
+++ public/src/function_name_to_acronym.mjs
@@ -2,13 +2,9 @@ import { list_join_empty } from "./list_join_empty.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_map } from "./list_map.mjs";
 import { function_name_to_parts } from "./function_name_to_parts.mjs";
-import { log } from "./log.mjs";
 export function function_name_to_acronym(f_name) {
   let parts = function_name_to_parts(f_name);
   ("if this error, then maybe string empty, __ instead of _ or ends with _");
-  log({
-    f_name,
-  });
   let letters = list_map(parts, list_first);
   let acronym = list_join_empty(letters);
   return acronym;
