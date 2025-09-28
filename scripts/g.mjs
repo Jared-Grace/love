f8de129b13f41de47fa5b8b87143559fdf7d5cdf
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:55:32 -0400

    repos

--- scripts/g.mjs
+++ scripts/g.mjs
@@ -1,5 +1,5 @@
 
-import { function_run_git } from './public/src/function_run_git.mjs';
+import { function_run_git } from '../public/src/function_run_git.mjs';
 (async () => {
   const [, , funcName, ...args] = process.argv;
   await function_run_git(funcName, args)
