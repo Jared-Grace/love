29bef7a8e0bb85160ad19d69e8bba8f6b68df7d8
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:20:12 -0400

    function_auto_path public\src\assert_arguments.mjs

--- web/public/src/assert_arguments.mjs
+++ web/public/src/assert_arguments.mjs
@@ -1,6 +1,8 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { equal } from "./equal.mjs";
 import { assert } from "./assert.mjs";
 export function assert_arguments(args, count) {
+  let length = object_property_get(args, "length");
   let eq2 = equal(left, right);
   assert(b);
 }
