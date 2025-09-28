29ce620cdd031a2901f6e5d3dccd906e8721c433
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:18:31 -0400

    function_auto_path public\src\list_to_dictionary_async.mjs

--- public/src/list_to_dictionary_async.mjs
+++ public/src/list_to_dictionary_async.mjs
@@ -1,13 +1,13 @@
+import { each_async } from "./each_async.mjs";
 import { marker } from "./marker.mjs";
 import { object_property_set } from "./object_property_set.mjs";
-import { each } from "./each.mjs";
-export function list_to_dictionary_async(list, lambda$item) {
+export async function list_to_dictionary_async(list, lambda$item) {
   marker("1");
   let dictionary = {};
   function lambda(item) {
     let value = lambda$item(item);
     object_property_set(dictionary, item, value);
   }
-  each(list, lambda);
+  await each_async(list, lambda);
   return dictionary;
 }
