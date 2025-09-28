91f1b8c2abf5a861128cade52f93dece2712e189
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:41:23 -0400

    data_file_update public\src\error_attention_set.mjs

--- public/src/function_run_line_parse.mjs
+++ public/src/function_run_line_parse.mjs
@@ -1,3 +1,4 @@
+import { data_path } from "./data_path.mjs";
 import { list_unique_reverse } from "./list_unique_reverse.mjs";
 import { list_add } from "./list_add.mjs";
 import { data_transform } from "./data_transform.mjs";
@@ -8,7 +9,8 @@ export async function function_run_line_parse(line) {
     let unique = list_unique_reverse(previous);
     return unique;
   }
-  await data_transform("prompts", [], lambda, data_path());
+  let d_path = data_path();
+  await data_transform("prompts", [], lambda, d_path);
   let v = {
     f_name,
     args,
