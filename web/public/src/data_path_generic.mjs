ffed3b1c3629887a23e5cd3a2ccb28076bca75e2
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:59:02 -0400

    function_auto_path public\src\function_name_to_acronym.mjs

--- web/public/src/data_path_generic.mjs
+++ web/public/src/data_path_generic.mjs
@@ -1,5 +1,5 @@
 import { file_name_json } from "./file_name_json.mjs";
 export function data_path_generic(inner) {
-  let f_path = file_name_json("data" + inner);
+  let f_path = file_name_json("data/data" + inner);
   return f_path;
 }
