bcaf29cfba1c877a41112bcef4067d18526bbb0e
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:35:53 -0400

    function_auto_path public\src\data_partition.mjs

--- public/src/data_partition.mjs
+++ public/src/data_partition.mjs
@@ -1,3 +1,4 @@
+import { file_json_property_delete } from "./file_json_property_delete.mjs";
 import { file_json_isolate } from "./file_json_isolate.mjs";
 import { data_copy } from "./data_copy.mjs";
 import { marker } from "./marker.mjs";
@@ -5,4 +6,5 @@ export async function data_partition(property_name) {
   marker("1");
   let f_path_to = await data_copy(property_name);
   await file_json_isolate(f_path_to, property_name);
+  await file_json_property_delete(f_path, property_name2);
 }
