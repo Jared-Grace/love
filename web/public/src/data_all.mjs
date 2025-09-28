6233511663130656934d20129a9928aaf62af414
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:30:56 -0400

    function_auto_path public\src\data_all.mjs

--- web/public/src/data_all.mjs
+++ web/public/src/data_all.mjs
@@ -1,9 +1,7 @@
-import { data_path } from "./data_path.mjs";
 import { marker } from "./marker.mjs";
 import { file_read_json } from "./file_read_json.mjs";
-export async function data_all(d_path) {
+export async function data_all(file_path) {
   marker("1");
-  const file_path = data_path();
   let data = await file_read_json(file_path);
   let v = {
     data,
