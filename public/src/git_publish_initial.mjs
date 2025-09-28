cb3ff9136484916e31c3150b14ab8a0c50923f93
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:02:28 -0400

    function_auto_path public\src\git_publish_initial.mjs

--- public/src/git_publish_initial.mjs
+++ public/src/git_publish_initial.mjs
@@ -1,5 +1,5 @@
 import { command_line_git_folder } from "./command_line_git_folder.mjs";
 export async function git_publish_initial(acronym) {
-  let commands = ["checkout -b main"];
+  let commands = ["checkout -b main", "push -u origin main"];
   await command_line_git_folder(folder, command_git);
 }
