e77010c0336481808cab90e4b732059e0298b0d2
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:33:10 -0400

    function_auto_path public\src\sandbox_5_a.mjs

--- public/src/sandbox_5_a.mjs
+++ public/src/sandbox_5_a.mjs
@@ -1,7 +1,6 @@
-import { data_transform } from "./data_transform.mjs";
 import { assert_arguments } from "./assert_arguments.mjs";
-export function sandbox_5_a(a, data_transform, data_transform) {
-  assert_arguments(arguments, 2);
+export function sandbox_5_a(a) {
+  assert_arguments(arguments, 1);
   let v = a * 2;
   return v;
 }
