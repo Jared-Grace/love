c6b5da3bc690f2ba268f03bc5f258ba11f00617f
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:32:03 -0400

    function_auto_path public\src\data_prompts_path.mjs

---
+++ public/src/data_prompts_path.mjs
@@ -0,0 +1,7 @@
+import { data_path_generic } from "./data_path_generic.mjs";
+import { marker } from "./marker.mjs";
+export function data_prompts_path() {
+  marker("1");
+  let f_path = data_path_generic("", "aliases");
+  return f_path;
+}
