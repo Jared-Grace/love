eb211180c7641661291b24ac487a1c7c6c6309cc
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:40:23 -0400

    function_auto_path public\src\repos_names.mjs

--- public/src/function_run_git.mjs
+++ public/src/function_run_git.mjs
@@ -1,11 +1,11 @@
 import { path_repo } from "./path_repo.mjs";
 import { git_acp_call_folder } from "./git_acp_call_folder.mjs";
 import { each_async } from "./each_async.mjs";
-import { repos } from "./repos.mjs";
+import { repos_names } from "./repos_names.mjs";
 import { function_run_log } from "./function_run_log.mjs";
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
-  let all = await repos();
+  let all = await repos_names();
   async function lambda(repo_name) {
     let folder = path_repo(repo_name);
     await git_acp_call_folder(f_name, args, folder);
