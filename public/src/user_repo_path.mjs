77ce2c71c5217b47f87b9fda5b760d5f209a00e2
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:08:35 -0400

    function_auto_path public\src\user_repo_set.mjs

---
+++ public/src/user_repo_path.mjs
@@ -0,0 +1,5 @@
+import { file_name_json_folder } from "./file_name_json_folder.mjs";
+export function user_repo_path() {
+  let f_path2 = file_name_json_folder("gitignore", "user");
+  return f_path2;
+}
--- public/src/user_repo_set.mjs
+++ public/src/user_repo_set.mjs
@@ -1,9 +1,9 @@
-import { file_name_json_folder } from "./file_name_json_folder.mjs";
+import { user_repo_path } from "./user_repo_path.mjs";
 import { data_set } from "./data_set.mjs";
 import { marker } from "./marker.mjs";
 export async function user_repo_set(value) {
   marker("1");
-  let f_path = file_name_json_folder("gitignore", "user");
+  let f_path = user_repo_path();
   async function lambda(previous) {
     return value;
   }
