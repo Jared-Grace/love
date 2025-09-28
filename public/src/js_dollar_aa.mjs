8d9c0e32457a52949f110cce31b5b0e4262289dc
Grace-Jared <christrosetolife@gmail.com>
2025-09-27 22:38:41 -0400

    function_auto_path public\src\js_dollar_aa.mjs

--- public/src/js_dollar_aa.mjs
+++ public/src/js_dollar_aa.mjs
@@ -1,3 +1,4 @@
+import { list_add_first } from "./list_add_first.mjs";
 import { js_declaration_single_block_body } from "./js_declaration_single_block_body.mjs";
 import { marker } from "./marker.mjs";
 export function js_dollar_aa({
@@ -9,7 +10,8 @@ export function js_dollar_aa({
   ast,
   afters,
 }) {
-  let body_block = js_declaration_single_block_body(ast2);
+  let body_block = js_declaration_single_block_body(ast);
+  list_add_first(list, item);
   marker("1");
   return;
 }
