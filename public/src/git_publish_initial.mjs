8021518d1e5f26a45c38ec3f76ab48267bd4e396
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:18:55 -0400

    function_auto_path public\src\git_publish_initial.mjs

--- public/src/git_publish_initial.mjs
+++ public/src/git_publish_initial.mjs
@@ -1,3 +1,4 @@
+import { list_to_dictionary } from "./list_to_dictionary.mjs";
 import { list_map } from "./list_map.mjs";
 import { repos } from "./repos.mjs";
 import { list_map_combine } from "./list_map_combine.mjs";
@@ -5,6 +6,8 @@ import { each_async } from "./each_async.mjs";
 import { command_line_git_folder } from "./command_line_git_folder.mjs";
 export async function git_publish_initial(acronym) {
   let all = await repos();
+  function lambda3(item2) {}
+  let dictionary = list_to_dictionary(list2, lambda3);
   function lambda(item) {}
   let mapped2 = list_map(list, lambda);
   let branch_name = " main";
