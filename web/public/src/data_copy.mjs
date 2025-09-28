a1538ecc22d4bc3bc09a5fa5c7eaa0469a974793
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:35:29 -0400

    function_auto_path public\src\data_copy.mjs

--- web/public/src/data_copy.mjs
+++ web/public/src/data_copy.mjs
@@ -2,10 +2,10 @@ import { file_copy } from "./file_copy.mjs";
 import { data_path_generic } from "./data_path_generic.mjs";
 import { data_path } from "./data_path.mjs";
 import { marker } from "./marker.mjs";
-export async function data_copy(f_name) {
+export async function data_copy(property_name) {
   marker("1");
   let f_path_from = data_path();
-  let f_path_to = data_path_generic("", f_name);
+  let f_path_to = data_path_generic("", property_name);
   await file_copy(f_path_from, f_path_to);
   return f_path_to;
 }
