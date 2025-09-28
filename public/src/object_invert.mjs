49b56875f785fcc028cd5831239095e286b1cefe
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:23:48 -0400

    function_auto_path public\src\object_invert.mjs

--- public/src/object_invert.mjs
+++ public/src/object_invert.mjs
@@ -5,10 +5,10 @@ import { list_add } from "./list_add.mjs";
 export function object_invert(object) {
   let inverted = {};
   function lambda(value, key) {
+    undefined_not_is_assert(value);
     let list = object_property_initialize(inverted, value, []);
     list_add(list, key);
   }
   each_object(object, lambda);
   return inverted;
-  undefined_not_is_assert(value2);
 }
