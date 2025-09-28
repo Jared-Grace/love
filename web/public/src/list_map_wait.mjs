fd01c55362b1c81c304dea95954fb61bf6cb49c6
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:21:31 -0400

    de list_map_wait

--- web/public/src/list_map_wait.mjs
+++
@@ -1,6 +0,0 @@
-import { list_wait } from "./list_wait.mjs";
-import { list_map } from "./list_map.mjs";
-export async function list_map_wait(list, lambda) {
-  let mapped = list_map(list, lambda);
-  let v = await list_wait(mapped);
-}
