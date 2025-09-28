7b99c5f1608b3fc13d57b2f3434c17a4bb4a51a3
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:46:34 -0400

    function_auto_path public\src\user_repo_set.mjs

--- public/src/user_repo_set.mjs
+++ public/src/user_repo_set.mjs
@@ -3,7 +3,7 @@ import { data_set } from "./data_set.mjs";
 import { marker } from "./marker.mjs";
 export async function user_repo_set() {
   marker("1");
-  let f_path = data_path_generic(inner, "data");
+  let f_path = data_path_generic(inner, "user");
   async function lambda(previous) {}
   await data_set(lambda, property_name, d_path);
 }
