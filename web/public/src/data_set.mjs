3101274fa9444b5d630a7da310d36a468905c194
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:01:06 -0400

    pn d_path data_path()

--- web/public/src/data_set.mjs
+++ web/public/src/data_set.mjs
@@ -1,7 +1,7 @@
 import { marker } from "./marker.mjs";
 import { data_path } from "./data_path.mjs";
 import { data_transform } from "./data_transform.mjs";
-export async function data_set(lambda$previous, property_name) {
+export async function data_set(lambda$previous, property_name, d_path) {
   marker("1");
   function lambda(previous) {
     let v2 = lambda$previous(previous);
