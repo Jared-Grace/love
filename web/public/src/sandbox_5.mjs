c9ce66c87af81306278e0d4788641abe3e3d4d7f
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:43:28 -0400

    function_auto_path public\src\sandbox_5.mjs

--- web/public/src/sandbox_5.mjs
+++ web/public/src/sandbox_5.mjs
@@ -2,6 +2,6 @@ import { path_join } from "./path_join.mjs";
 import { marker } from "./marker.mjs";
 export function sandbox_5() {
   marker("1");
-  let joined = path_join(["a", "b"]);
+  let joined = path_join("a", "b");
   return joined;
 }
