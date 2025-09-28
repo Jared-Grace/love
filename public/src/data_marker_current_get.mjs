d477a61b6ed9d92165feff62d3f58e0a64087cdc
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:21:04 -0400

    function_auto_path public\src\data_marker_current_get.mjs

--- public/src/data_marker_current_get.mjs
+++ public/src/data_marker_current_get.mjs
@@ -2,7 +2,7 @@ import { user_repo_path } from "./user_repo_path.mjs";
 import { data_property_get } from "./data_property_get.mjs";
 export async function data_marker_current_get() {
   const property_name = "marker_current";
-  let f_path2 = user_repo_path();
+  let d_path = user_repo_path();
   let f_name = await data_property_get(property_name, d_path);
   return f_name;
 }
