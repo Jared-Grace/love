72c67452564aa2059da75c2d8bbf8f145a1978cd
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:44:48 -0400

    rs object_properties_delete all

--- public/src/file_json_isolate.mjs
+++ public/src/file_json_isolate.mjs
@@ -1,5 +1,5 @@
 import { object_property_set } from "./object_property_set.mjs";
-import { object_properties_delete } from "./object_properties_delete.mjs";
+import { object_properties_delete_all } from "./object_properties_delete_all.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { file_json_transform } from "./file_json_transform.mjs";
 import { marker } from "./marker.mjs";
@@ -7,7 +7,7 @@ export async function file_json_isolate(f_path, property_name) {
   marker("1");
   async function lambda(data) {
     let value = object_property_get(data, property_name);
-    object_properties_delete(data);
+    object_properties_delete_all(data);
     object_property_set(data, property_name, value);
   }
   await file_json_transform(f_path, lambda);
