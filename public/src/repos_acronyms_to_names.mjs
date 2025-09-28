df6bbd479d7fb56fe88f3e5b57faa84b806b22c2
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:41:05 -0400

    function_auto_path public\src\repos_acronyms_to_names.mjs

--- public/src/repos_acronyms_to_names.mjs
+++ public/src/repos_acronyms_to_names.mjs
@@ -1,9 +1,9 @@
+import { repos_names } from "./repos_names.mjs";
 import { object_invert } from "./object_invert.mjs";
 import { repo_acronym_get } from "./repo_acronym_get.mjs";
 import { list_to_dictionary_async } from "./list_to_dictionary_async.mjs";
-import { repos } from "./repos.mjs";
 export async function repos_acronyms_to_names() {
-  let all = await repos();
+  let all = await repos_names();
   let dictionary = await list_to_dictionary_async(all, repo_acronym_get);
   let inverted = object_invert(dictionary);
   return inverted;
