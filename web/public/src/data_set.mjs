7323040c3e444f26db75d726a3b74add10e438f9
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:00:46 -0400

    function_auto_path public\src\data_set.mjs

--- web/public/src/data_set.mjs
+++ web/public/src/data_set.mjs
@@ -5,6 +5,6 @@ export async function data_set(lambda$previous, property_name) {
     let v2 = lambda$previous(previous);
     return v2;
   }
-  let d_path = data_path();
-  await data_transform(property_name, null, lambda, d_path);
+  let dp = data_path();
+  await data_transform(property_name, null, lambda, dp);
 }
