4748356f2e001f397acb91db30f275db5df65813
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:12:35 -0400

    function_auto_path public\src\git_publish_initial.mjs

--- public/src/git_publish_initial.mjs
+++ public/src/git_publish_initial.mjs
@@ -1,9 +1,12 @@
+import { list_map } from "./list_map.mjs";
 import { repos } from "./repos.mjs";
 import { list_map_combine } from "./list_map_combine.mjs";
 import { each_async } from "./each_async.mjs";
 import { command_line_git_folder } from "./command_line_git_folder.mjs";
 export async function git_publish_initial(acronym) {
   let all = await repos();
+  function lambda(item) {}
+  let mapped2 = list_map(list, lambda);
   let branch_name = " main";
   let commands = ["checkout -b", "push -u origin"];
   let mapped = list_map_combine(branch_name, commands);
