203f5119624f76fc37ca3111a4589c7c16e707c8
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:52:55 -0400

    function_auto_path public\src\data_all.mjs

--- web/public/src/data_all.mjs
+++ web/public/src/data_all.mjs
@@ -1,16 +1,18 @@
-import { not } from "./not.mjs";
 import { file_exists } from "./file_exists.mjs";
 import { marker } from "./marker.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function data_all(file_path) {
   marker("1");
-  let data = await file_read_json(file_path);
+  let exists = await file_exists(file_path);
+  let data = null;
+  if (exists) {
+    data = await file_read_json(file_path);
+  } else {
+    data = {};
+  }
   let v = {
     data,
     file_path,
   };
   return v;
-  let exists = await file_exists(file_path);
-  if (not(exists)) {
-  }
 }
