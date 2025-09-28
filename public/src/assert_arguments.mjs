375e52b685636ee40e6040663665a8b58a90d949
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:21:06 -0400

    r assert_equal equal_assert

--- public/src/assert_arguments.mjs
+++ public/src/assert_arguments.mjs
@@ -1,6 +1,6 @@
-import { assert_equal } from "./assert_equal.mjs";
+import { equal_assert } from "./equal_assert.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 export function assert_arguments(args, count) {
   let length = object_property_get(args, "length");
-  assert_equal(length, count);
+  equal_assert(length, count);
 }
--- public/src/assert_equal.mjs
+++
@@ -1,6 +0,0 @@
-import { assert } from "./assert.mjs";
-import { equal } from "./equal.mjs";
-export function assert_equal(length, count) {
-  let eq = equal(length, count);
-  assert(eq);
-}
