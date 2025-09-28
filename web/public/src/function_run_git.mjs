7a8e9b8ac3bd7cf40317f2aa5d40d8592a3e8057
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:33:07 -0400

    function_auto_path public\src\function_run_git.mjs

--- web/public/src/function_run_git.mjs
+++ web/public/src/function_run_git.mjs
@@ -1,7 +1,9 @@
+import { repos } from "./repos.mjs";
 import { git_acp_call } from "./git_acp_call.mjs";
 import { function_run_log } from "./function_run_log.mjs";
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
+  let all = await repos();
   await git_acp_call(f_name, args);
   return result;
 }
