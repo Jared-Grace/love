f9d1c4871ae34b6981ed4ab4d8c9ccc75c41a422
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:43:22 -0400

    function_auto_path public\src\data_files_update.mjs

--- web/public/src/data_files_update.mjs
+++ web/public/src/data_files_update.mjs
@@ -1,4 +1,3 @@
-import { file_delete } from "./file_delete.mjs";
 import { data_path } from "./data_path.mjs";
 import { marker } from "./marker.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
@@ -11,7 +10,6 @@ import { each_async } from "./each_async.mjs";
 export async function data_files_update() {
   marker("1");
   let d_path = data_path();
-  await file_delete(d_path);
   var d = await data_all(d_path);
   let f_paths = functions_paths();
   let parseds = await list_map_unordered_async(f_paths, file_js_parse);
