a2c1f30e8aaa27ecb64268fca0e4b0865749061d
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:13:53 -0400

    function_auto_path public\src\path_repo.mjs

--- public/src/path_repo.mjs
+++ public/src/path_repo.mjs
@@ -2,6 +2,6 @@ import { path_join } from "./path_join.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 export function path_repo(repo_name) {
   let previous = folder_previous();
-  let joined = path_join([previous, repo_name]);
-  return joined;
+  let r_path = path_join([previous, repo_name]);
+  return r_path;
 }
