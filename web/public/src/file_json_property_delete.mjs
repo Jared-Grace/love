131f38c66cee99c43abb568e2a3bb6410e1c214a
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:28:40 -0400

    function_auto_path public\src\file_json_property_delete.mjs

--- web/public/src/file_json_property_delete.mjs
+++ web/public/src/file_json_property_delete.mjs
@@ -1,10 +1,10 @@
 import { object_property_delete } from "./object_property_delete.mjs";
 import { file_json_transform } from "./file_json_transform.mjs";
 import { marker } from "./marker.mjs";
-export async function file_json_property_delete(fb_path, property_name) {
+export async function file_json_property_delete(f_path, property_name) {
   marker("1");
   async function lambda(data) {
     object_property_delete(data, property_name);
   }
-  await file_json_transform(fb_path, lambda);
+  await file_json_transform(f_path, lambda);
 }
