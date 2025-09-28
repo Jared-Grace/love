3abd48adc720a74253f4271c69e83578f57e0b9a
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:21:06 -0400

    r assert_equal equal_assert

--- public/src/equal_assert.mjs
+++ public/src/equal_assert.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { assert } from "./assert.mjs";
 import { equal } from "./equal.mjs";
 export function equal_assert(length, count) {
+  marker("1");
   let eq = equal(length, count);
   assert(eq);
 }
