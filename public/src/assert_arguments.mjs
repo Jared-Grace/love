c8a95a28fa56590aab9f08c3abd14a020ce8b7c4
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:19:40 -0400

    function_auto_path public\src\assert_arguments.mjs

--- public/src/assert_arguments.mjs
+++ public/src/assert_arguments.mjs
@@ -1,4 +1,6 @@
+import { emoji_question } from "./emoji_question.mjs";
 import { assert } from "./assert.mjs";
 export function assert_arguments(args, count) {
+  let v2 = emoji_question();
   assert(b);
 }
