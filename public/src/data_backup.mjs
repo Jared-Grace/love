af8f1c1769a790298a2cb513afc491fe40a14cb6
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:47:54 -0400

    function_auto_path public\src\data_backup.mjs

--- public/src/data_backup.mjs
+++ public/src/data_backup.mjs
@@ -1,11 +1,12 @@
+import { function_copy } from "./function_copy.mjs";
 import { date_now_file } from "./date_now_file.mjs";
 import { data_path_generic } from "./data_path_generic.mjs";
 import { data_path } from "./data_path.mjs";
 import { marker } from "./marker.mjs";
-export function data_backup() {
+export async function data_backup() {
   marker("1");
   let f_path_from = data_path();
   let inner = date_now_file();
   let f_path_to = data_path_generic("." + inner);
-  cp;
+  await function_copy(f_name_old, f_name_new);
 }
