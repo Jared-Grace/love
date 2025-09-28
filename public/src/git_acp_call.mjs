22d114618c4c439e9199502d2f240006f00c6c54
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:56:26 -0400

    function_auto_path public\src\git_acp_call.mjs

--- public/src/git_acp_call.mjs
+++ public/src/git_acp_call.mjs
@@ -1,5 +1,6 @@
-import { git_acp } from "./git_acp.mjs";
+import { git_acp_folder } from "./git_acp_folder.mjs";
 export async function git_acp_call(f_name, args) {
+  let folder = ".";
   let message = [f_name].concat(args).join(" ");
-  await git_acp(message);
+  await git_acp_folder(folder, message);
 }
