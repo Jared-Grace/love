43c45a8f00e193cb4bb8312e37168691a3f9a218
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:47:06 -0400

    function_auto_path public\src\data_backup.mjs

--- public/src/data_backup.mjs
+++ public/src/data_backup.mjs
@@ -1,6 +1,8 @@
+import { data_path_generic } from "./data_path_generic.mjs";
 import { data_path } from "./data_path.mjs";
 import { marker } from "./marker.mjs";
 export function data_backup() {
   marker("1");
   let f_path = data_path();
+  let f_path2 = data_path_generic(inner);
 }
