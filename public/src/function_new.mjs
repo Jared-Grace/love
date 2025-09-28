9b86df0bfb85c979531932ae9e946e8af6647042
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:49:30 -0400

    o n

--- public/src/function_new.mjs
+++ public/src/function_new.mjs
@@ -1,8 +1,10 @@
+import { marker } from "./marker.mjs";
 import { function_new_declaration } from "./function_new_declaration.mjs";
 import { js_code_declaration } from "./js_code_declaration.mjs";
 import { function_open } from "./function_open.mjs";
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 export async function function_new(f_name) {
+  marker("1");
   const code_declaration = js_code_declaration(f_name, "", false);
   let declaration = js_parse_statement_module(code_declaration);
   await function_new_declaration(declaration);
