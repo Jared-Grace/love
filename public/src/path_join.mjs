49d67444172e951041df0f2104009e76a1f9d31a
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:23:21 -0400

    function_auto_path public\src\js_dollar_aa.mjs

--- public/src/path_join.mjs
+++ public/src/path_join.mjs
@@ -1,9 +1,6 @@
-import { assert_arguments } from "./assert_arguments.mjs";
 import { not } from "./not.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 export function path_join(segments) {
-  let v2 = assert_arguments();
-  v2;
   let parts = [];
   for (let seg of segments) {
     if (not(seg)) {
@@ -27,6 +24,5 @@ export function path_join(segments) {
     }
   }
   let v = parts.join("/");
-  return v;
-  $aa;
+  return v;$aa
 }
