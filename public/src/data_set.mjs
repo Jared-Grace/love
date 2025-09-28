ce7385e94015df4ccb1b3cf55c0a9dc7b0958153
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:41:23 -0400

    data_file_update public\src\error_attention_set.mjs

--- public/src/data_set.mjs
+++ public/src/data_set.mjs
@@ -1,8 +1,10 @@
+import { data_path } from "./data_path.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function data_set(lambda$previous, property_name) {
   function lambda(previous) {
     let v2 = lambda$previous(previous);
     return v2;
   }
-  await data_transform(property_name, null, lambda, data_path());
+  let d_path = data_path();
+  await data_transform(property_name, null, lambda, d_path);
 }
