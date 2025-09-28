ffafba8d0c0358f322a167b77d683a915e009653
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:21:58 -0400

    function_auto_path public\src\data_app_current_get.mjs

--- web/public/src/data_app_current_get.mjs
+++ web/public/src/data_app_current_get.mjs
@@ -1,6 +1,8 @@
 import { null_not_is_assert } from "./null_not_is_assert.mjs";
 import { data_property_get } from "./data_property_get.mjs";
+import { user_repo_path } from "./user_repo_path.mjs";
 export async function data_app_current_get() {
+  let f_path2 = user_repo_path();
   let a = await data_property_get("app_current", d_path);
   null_not_is_assert(a);
   return a;
