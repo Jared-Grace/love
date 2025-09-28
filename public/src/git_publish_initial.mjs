3de3d2f40217d585126ba04ce23a099d1cc76c20
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:28:26 -0400

    function_auto_path public\src\git_publish_initial.mjs

--- public/src/git_publish_initial.mjs
+++ public/src/git_publish_initial.mjs
@@ -1,19 +1,10 @@
-import { list_single } from "./list_single.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { object_invert } from "./object_invert.mjs";
-import { list_to_dictionary_async } from "./list_to_dictionary_async.mjs";
-import { repo_acronym_get } from "./repo_acronym_get.mjs";
+import { repo_acronym_to_name } from "./repo_acronym_to_name.mjs";
 import { list_map } from "./list_map.mjs";
-import { repos } from "./repos.mjs";
 import { list_map_combine } from "./list_map_combine.mjs";
 import { each_async } from "./each_async.mjs";
 import { command_line_git_folder } from "./command_line_git_folder.mjs";
 export async function git_publish_initial(acronym) {
-  let all = await repos();
-  let dictionary = await list_to_dictionary_async(all, repo_acronym_get);
-  let inverted = object_invert(dictionary);
-  let repos_matching = object_property_get(inverted, acronym);
-  let only = list_single(repos_matching);
+  let only = await repo_acronym_to_name(acronym);
   return only;
   function lambda(item) {}
   let mapped2 = list_map(list, lambda);
---
+++ public/src/repo_acronym_to_name.mjs
@@ -0,0 +1,14 @@
+import { list_single } from "./list_single.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { object_invert } from "./object_invert.mjs";
+import { repo_acronym_get } from "./repo_acronym_get.mjs";
+import { list_to_dictionary_async } from "./list_to_dictionary_async.mjs";
+import { repos } from "./repos.mjs";
+export async function repo_acronym_to_name(acronym) {
+  let all = await repos();
+  let dictionary = await list_to_dictionary_async(all, repo_acronym_get);
+  let inverted = object_invert(dictionary);
+  let repos_matching = object_property_get(inverted, acronym);
+  let only = list_single(repos_matching);
+  return only;
+}
