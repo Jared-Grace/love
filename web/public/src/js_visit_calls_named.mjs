8f37e93696d2b8cad5c46a29f1880be08e7d23e2
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:30:19 -0400

    function_auto_path public\src\js_visit_calls_named.mjs

--- web/public/src/js_visit_calls_named.mjs
+++ web/public/src/js_visit_calls_named.mjs
@@ -2,11 +2,11 @@ import { js_visit_type } from "./js_visit_type.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { equal_not } from "./equal_not.mjs";
 import { js_call_callee_name } from "./js_call_callee_name.mjs";
-export function js_visit_calls_named(f_name_current, lambda, ast) {
-  function lambda2(v) {
+export function js_visit_calls_named(f_name, lambda, ast) {
+  function lambda_inner(v) {
     let { node } = v;
     let name = js_call_callee_name(node);
-    if (equal_not(name, f_name_current)) {
+    if (equal_not(name, f_name)) {
       return;
     }
     let args = object_property_get(node, "arguments");
@@ -15,5 +15,5 @@ export function js_visit_calls_named(f_name_current, lambda, ast) {
       args,
     });
   }
-  js_visit_type(ast, "CallExpression", lambda2);
+  js_visit_type(ast, "CallExpression", lambda_inner);
 }
