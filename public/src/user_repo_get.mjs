dc4731f9df214b4b91f049097e0da7d42ac31052
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:09:25 -0400

    function_auto_path public\src\user_repo_get.mjs

--- public/src/user_repo_get.mjs
+++ public/src/user_repo_get.mjs
@@ -1,8 +1,9 @@
 import { data_get } from "./data_get.mjs";
 import { user_repo_path } from "./user_repo_path.mjs";
 import { marker } from "./marker.mjs";
-export async function user_repo_get(value) {
+export async function user_repo_get() {
   marker("1");
   let f_path = user_repo_path();
-  let v = await data_get("repo_current", null, f_path);
+  let value = await data_get("repo_current", null, f_path);
+  return value;
 }
