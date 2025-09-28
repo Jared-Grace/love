a8e29247b883c791c6c7dd354545c47eed97f4b7
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:16:33 -0400

    function_auto_path public\src\repo_acronym_set.mjs

--- public/src/repo_acronym_set.mjs
+++ public/src/repo_acronym_set.mjs
@@ -3,9 +3,9 @@ import { data_set } from "./data_set.mjs";
 import { marker } from "./marker.mjs";
 export async function repo_acronym_set(repo, value) {
   marker("1");
-  let joined = path_repo_about(repo);
+  let a_path = path_repo_about(repo);
   async function lambda(previous) {
     return value;
   }
-  await data_set(lambda, "acronym", joined);
+  await data_set(lambda, "acronym", a_path);
 }
