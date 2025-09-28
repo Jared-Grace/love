95ba0aa1d71036c5889ebb96a1ac832d617141cd
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:59:56 -0400

    function_auto_path public\src\task_new.mjs

---
+++ public/src/string_empty_not_is_assert.mjs
@@ -0,0 +1,6 @@
+import { assert } from "./assert.mjs";
+import { string_empty_not_is } from "./string_empty_not_is.mjs";
+export function string_empty_not_is_assert(task_name) {
+  let ne = string_empty_not_is(task_name);
+  assert(ne);
+}
