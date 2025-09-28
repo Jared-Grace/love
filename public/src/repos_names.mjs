d5827596165c6fc85e15a5ff1746755c0d3041c1
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:40:23 -0400

    function_auto_path public\src\repos_names.mjs

--- public/src/repos_names.mjs
+++ public/src/repos_names.mjs
@@ -1,7 +1,7 @@
 import { repos_folder } from "./repos_folder.mjs";
 import { folder_read } from "./folder_read.mjs";
 import { marker } from "./marker.mjs";
-export async function repos() {
+export async function repos_names() {
   marker("1");
   let path_folder = repos_folder();
   let all = await folder_read(path_folder);
