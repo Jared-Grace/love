dc9c67677b0810cf6f2f6698c719df5d62618546
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:04:02 -0400

    pn f_name data

--- public/src/data_path.mjs
+++ public/src/data_path.mjs
@@ -1,6 +1,6 @@
 import { data_path_generic } from "./data_path_generic.mjs";
 export function data_path() {
   let inner = "";
-  let f_path = data_path_generic(inner);
+  let f_path = data_path_generic(inner, "data");
   return f_path;
 }
