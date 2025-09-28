6d547f805f3c2266ee7ea56fb0794392edc49a49
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:47:48 -0400

    function_auto_path public\src\data_path_generic.mjs

--- web/public/src/data_path_generic.mjs
+++ web/public/src/data_path_generic.mjs
@@ -1,11 +1,9 @@
+import { file_name_json_folder } from "./file_name_json_folder.mjs";
 import { marker } from "./marker.mjs";
-import { path_join } from "./path_join.mjs";
-import { file_name_json } from "./file_name_json.mjs";
 export function data_path_generic(suffix, f_name_unsuffixed) {
   marker("1");
   const f_name = f_name_unsuffixed + suffix;
   const folder = "data";
-  let joined = path_join([folder, f_name]);
-  let f_path = file_name_json(joined);
+  let f_path = file_name_json_folder(folder, f_name);
   return f_path;
 }
