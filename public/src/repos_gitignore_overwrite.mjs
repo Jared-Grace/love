b18fd2a7910369992eb0d021a18539b2e9e534c1
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:48:08 -0400

    function_auto_path public\src\repos_gitignore_overwrite.mjs

--- public/src/repos_gitignore_overwrite.mjs
+++ public/src/repos_gitignore_overwrite.mjs
@@ -1,3 +1,4 @@
+import { path_resolve } from "./path_resolve.mjs";
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { list_map_path_join } from "./list_map_path_join.mjs";
 import { repo_path } from "./repo_path.mjs";
@@ -10,7 +11,9 @@ export async function repos_gitignore_overwrite() {
   let f_name = ".gitignore";
   let mapped = list_map(repos, repo_path);
   let mapped2 = list_map_path_join(mapped, f_name);
-  async function lambda(item) {}
+  async function lambda(item) {
+    let result = await path_resolve(paths);
+  }
   let waited = await list_map_unordered_async(list, lambda);
   return mapped2;
 }
