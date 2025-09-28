a018917c215a0ff43579fe8c6f5f3b507fc34ded
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:31:13 -0400

    function_auto_path public\src\js_assert_arguments_args.mjs

--- public/src/js_assert_arguments_args.mjs
+++ public/src/js_assert_arguments_args.mjs
@@ -1,8 +1,9 @@
+import { assert_arguments } from "./assert_arguments.mjs";
 import { js_visit_calls_named } from "./js_visit_calls_named.mjs";
 import { marker } from "./marker.mjs";
 export function js_assert_arguments_args(ast) {
   marker("1");
   return;
   function lambda2() {}
-  js_visit_calls_named(f_name, lambda2, ast2);
+  js_visit_calls_named(assert_arguments.name, lambda2, ast);
 }
