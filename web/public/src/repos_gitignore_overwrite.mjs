daaa64ae35030f76013b33f4d37f40030ea2021f
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:40:23 -0400

    function_auto_path public\src\repos_names.mjs

--- web/public/src/repos_gitignore_overwrite.mjs
+++ web/public/src/repos_gitignore_overwrite.mjs
@@ -1,12 +1,12 @@
 import { repos_acronyms_to_names } from "./repos_acronyms_to_names.mjs";
 import { list_map } from "./list_map.mjs";
-import { repos } from "./repos.mjs";
+import { repos_names } from "./repos_names.mjs";
 import { marker } from "./marker.mjs";
 export async function repos_gitignore_overwrite() {
   marker("1");
   let inverted = await repos_acronyms_to_names();
   let f_name = ".gitignore";
-  let all = await repos();
+  let all = await repos_names();
   function lambda(item) {}
   let mapped = list_map(list, lambda);
 }
