3811fce73f53c4f48999c2a1fa9398da5e49d557
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:22:08 -0400

    function_auto_path public\src\git_publish_initial.mjs

--- web/public/src/git_publish_initial.mjs
+++ web/public/src/git_publish_initial.mjs
@@ -1,3 +1,4 @@
+import { object_invert } from "./object_invert.mjs";
 import { list_to_dictionary_async } from "./list_to_dictionary_async.mjs";
 import { repo_acronym_get } from "./repo_acronym_get.mjs";
 import { list_map } from "./list_map.mjs";
@@ -8,6 +9,7 @@ import { command_line_git_folder } from "./command_line_git_folder.mjs";
 export async function git_publish_initial(acronym) {
   let all = await repos();
   let dictionary = await list_to_dictionary_async(list2, repo_acronym_get);
+  let inverted = object_invert(object);
   function lambda(item) {}
   let mapped2 = list_map(list, lambda);
   let branch_name = " main";
