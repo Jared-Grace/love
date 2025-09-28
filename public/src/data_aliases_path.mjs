241e7cf8326cbf338aa5e325407f3e6646e79eea
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:05:00 -0400

    function_auto_path public\src\data_aliases_path.mjs

--- public/src/data_aliases_path.mjs
+++ public/src/data_aliases_path.mjs
@@ -2,6 +2,6 @@ import { data_path_generic } from "./data_path_generic.mjs";
 import { marker } from "./marker.mjs";
 export function data_aliases_path() {
   marker("1");
-  let f_path = data_path_generic(inner, f_name);
+  let f_path = data_path_generic("", "aliases");
   return f_path;
 }
