fca119415de707e38849b18b3a22b48f7a2d3ad8
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:44:48 -0400

    rs object_properties_delete all

--- public/src/object_properties_delete_all.mjs
+++ public/src/object_properties_delete_all.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { object_property_delete } from "./object_property_delete.mjs";
 import { each } from "./each.mjs";
 import { object_properties } from "./object_properties.mjs";
 export function object_properties_delete_all(obj) {
+  marker("1");
   function lambda(p) {
     let v = object_property_delete(obj, p);
     return v;
