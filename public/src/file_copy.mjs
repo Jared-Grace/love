894675091e5d62cf09017c6dab2557157284308c
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:50:48 -0400

    function_auto_path public\src\file_copy.mjs

--- public/src/file_copy.mjs
+++ public/src/file_copy.mjs
@@ -1,8 +1,6 @@
-import { file_parent_exists_ensure } from "./file_parent_exists_ensure.mjs";
+import { file_copy_overwrite } from "./file_copy_overwrite.mjs";
 import { assert_file_exists_not } from "./assert_file_exists_not.mjs";
 export async function file_copy(file_path_old, file_path_new) {
   await assert_file_exists_not(file_path_new);
-  await file_parent_exists_ensure(file_path_new);
-  let fs = await import("fs");
-  await fs.promises.copyFile(file_path_old, file_path_new);
+  await file_copy_overwrite(file_path_new, file_path_old);
 }
