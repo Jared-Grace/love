c834cba51579e8b5182e846cdac6a59d2e91e6dd
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:16:42 -0400

    function_auto_path public\src\path_repo_about.mjs

--- public/src/path_repo_about.mjs
+++ public/src/path_repo_about.mjs
@@ -2,6 +2,6 @@ import { path_repo_combine } from "./path_repo_combine.mjs";
 import { data_path_generic } from "./data_path_generic.mjs";
 export function path_repo_about(repo) {
   let f_path = data_path_generic("", "about");
-  let joined = path_repo_combine(repo, f_path);
-  return joined;
+  let a_path = path_repo_combine(repo, f_path);
+  return a_path;
 }
