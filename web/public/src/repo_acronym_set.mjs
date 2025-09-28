ced74844413d5f9545f4114bc89043478a305f1a
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:04:21 -0400

    function_auto_path public\src\repo_acronym_set.mjs

--- web/public/src/repo_acronym_set.mjs
+++ web/public/src/repo_acronym_set.mjs
@@ -2,5 +2,5 @@ import { data_path_generic } from "./data_path_generic.mjs";
 import { marker } from "./marker.mjs";
 export function repo_acronym_set() {
   marker("1");
-  let f_path = data_path_generic(suffix, f_name_unsuffixed);
+  let f_path = data_path_generic("", "me");
 }
