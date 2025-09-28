bcd8522563473104103125904df5f40178c79079
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:54:30 -0400

    function_auto_path public\src\git_commit.mjs

--- web/public/src/git_commit.mjs
+++ web/public/src/git_commit.mjs
@@ -1,8 +1,8 @@
 import { command_line_git_folder } from "./command_line_git_folder.mjs";
 import { catch_only_async } from "./catch_only_async.mjs";
 export async function git_commit(message) {
+  let folder = ".";
   async function lambda() {
-    let folder = ".";
     await command_line_git_folder(folder, `commit -m "${message}"`);
   }
   await catch_only_async(lambda, "nothing to commit");
