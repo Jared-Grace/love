11a9902636d842a3803d0ce462e21ef3970def20
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:28:24 -0400

    function_auto_path public\src\file_json_transform.mjs

--- public/src/file_json_transform.mjs
+++ public/src/file_json_transform.mjs
@@ -1,7 +1,7 @@
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
 import { file_read_json } from "./file_read_json.mjs";
-export async function file_json_transform(fb_path, lambda$data) {
-  let data = await file_read_json(fb_path);
+export async function file_json_transform(f_path, lambda$data) {
+  let data = await file_read_json(f_path);
   lambda$data(data);
-  await file_overwrite_json(fb_path, data);
+  await file_overwrite_json(f_path, data);
 }
