00d4c6ebb23295f6b342f5e5c03bae255e3127df
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:54:09 -0400

    function_auto_path public\src\git_acp.mjs

--- web/public/src/git_acp.mjs
+++ web/public/src/git_acp.mjs
@@ -1,9 +1,9 @@
+import { command_line_git_folder } from "./command_line_git_folder.mjs";
 import { git_commit } from "./git_commit.mjs";
 import { git_push } from "./git_push.mjs";
-import { command_line_git } from "./command_line_git.mjs";
 export async function git_acp(message) {
   let folder = ".";
-  await command_line_git("add -A");
+  await command_line_git_folder(folder, "add -A");
   await git_commit(message);
   await git_push();
 }
