8baacd06c15d271e16e5844f327884d780f82b44
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:42:41 -0400

    function_auto_path public\src\path_join.mjs

--- public/src/path_join.mjs
+++ public/src/path_join.mjs
@@ -1,8 +1,6 @@
-import { assert_arguments } from "./assert_arguments.mjs";
 import { not } from "./not.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 export function path_join(segments) {
-  assert_arguments(arguments, 1);
   let parts = [];
   for (let seg of segments) {
     if (not(seg)) {
