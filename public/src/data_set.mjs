f603aecf5fcd4235a1186dfcb45e426ec11b5ae3
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:00:51 -0400

    fs data_set

--- public/src/data_set.mjs
+++ public/src/data_set.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { data_path } from "./data_path.mjs";
 import { data_transform } from "./data_transform.mjs";
 export async function data_set(lambda$previous, property_name) {
+  marker("1");
   function lambda(previous) {
     let v2 = lambda$previous(previous);
     return v2;
