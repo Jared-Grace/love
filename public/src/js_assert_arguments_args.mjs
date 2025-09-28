ffeb25e6f3ea4cf06a41a533d0b08294afb8b987
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 11:35:08 -0400

    function_auto_path public\src\js_assert_arguments_args.mjs

--- public/src/js_assert_arguments_args.mjs
+++ public/src/js_assert_arguments_args.mjs
@@ -1,10 +1,9 @@
-import { list_add_multiple } from "./list_add_multiple.mjs";
+import { list_replace_all } from "./list_replace_all.mjs";
 import { list_size } from "./list_size.mjs";
 import { js_declaration_params_get } from "./js_declaration_params_get.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
 import { js_keyword_arguments } from "./js_keyword_arguments.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
-import { list_empty } from "./list_empty.mjs";
 import { assert_arguments } from "./assert_arguments.mjs";
 import { js_visit_calls_named } from "./js_visit_calls_named.mjs";
 import { marker } from "./marker.mjs";
@@ -18,8 +17,7 @@ export function js_assert_arguments_args(ast) {
     let code_expression = js_keyword_arguments();
     let expression = js_parse_expression(code_expression);
     let args_new = [expression, size];
-    list_empty(args);
-    list_add_multiple(args, args_new);
+    list_replace_all(args, args_new);
   }
   js_visit_calls_named(assert_arguments.name, lambda2, ast);
 }
