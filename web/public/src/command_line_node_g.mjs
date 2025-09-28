56937040a71a3c65337f5b08364a1932be5f33de
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:03:57 -0400

    function_auto_path public\src\repo_acronym_set.mjs

--- web/public/src/command_line_node_g.mjs
+++ web/public/src/command_line_node_g.mjs
@@ -4,7 +4,7 @@ import { command_line } from "./command_line.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 export async function command_line_node_g(f_name, args) {
   let result = list_join_space(args);
-  let output = await command_line("node g.mjs " + f_name + " " + result);
+  let output = await command_line("node scripts/g.mjs " + f_name + " " + result);
   let stdout = object_property_get(output, "stdout");
   log_keep(stdout);
 }
--- web/public/src/repo_acronym_set.mjs
+++ web/public/src/repo_acronym_set.mjs
@@ -1,4 +1,6 @@
+import { data_path_generic } from "./data_path_generic.mjs";
 import { marker } from "./marker.mjs";
 export function repo_acronym_set() {
-  marker("1");data_path_generic
+  marker("1");
+  let f_path = data_path_generic(suffix, f_name_unsuffixed);
 }
