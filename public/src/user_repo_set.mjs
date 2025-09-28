86f838daec01352cda7573e5ab62f190a293210e
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:48:55 -0400

    function_auto_path public\src\user_repo_set.mjs

--- public/src/user_repo_set.mjs
+++ public/src/user_repo_set.mjs
@@ -1,9 +1,9 @@
 import { file_name_json_folder } from "./file_name_json_folder.mjs";
 import { data_set } from "./data_set.mjs";
 import { marker } from "./marker.mjs";
-export async function user_repo_set() {
+export async function user_repo_set(value) {
   marker("1");
   let f_path = file_name_json_folder("gitignore", "user");
   async function lambda(previous) {}
-  await data_set(lambda, property_name, f_path);
+  await data_set(lambda, "repo_current", f_path);
 }
