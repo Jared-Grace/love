87c4cd80c8ef67f361902add2c8fb7dfdace578c
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 13:46:15 -0400

    function_auto_path public\src\data_path.mjs

--- public/src/data_path.mjs
+++ public/src/data_path.mjs
@@ -1,5 +1,6 @@
+import { data_path_generic } from "./data_path_generic.mjs";
 export function data_path() {
   let inner = "";
-  let f_path = "data" + inner + ".json";
+  let f_path = data_path_generic(inner);
   return f_path;
 }
