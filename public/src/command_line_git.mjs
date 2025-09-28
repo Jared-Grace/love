1c4fff2d6284ee3242c321d3adfa1bed1e3e24ae
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 15:53:31 -0400

    function_auto_path public\src\command_line_git.mjs

--- public/src/command_line_git.mjs
+++ public/src/command_line_git.mjs
@@ -1,5 +1,6 @@
 import { command_line_git_prefix } from "./command_line_git_prefix.mjs";
 import { command_line } from "./command_line.mjs";
 export async function command_line_git(command_git) {
+  let folder = ".";
   await command_line(command_line_git_prefix() + command_git);
 }
