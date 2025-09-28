dc0bb90797f5f846203c5f0a8e240291ea89fc72
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:46:17 -0400

    data_file_update public\src\user_repo_set.mjs

--- public/src/user_repo_set.mjs
+++ public/src/user_repo_set.mjs
@@ -2,5 +2,6 @@ import { data_set } from "./data_set.mjs";
 import { marker } from "./marker.mjs";
 export async function user_repo_set() {
   marker("1");
-  await data_set(async function lambda(previous) {}, property_name, d_path);
+  async function lambda(previous) {}
+  await data_set(lambda, property_name, d_path);
 }
