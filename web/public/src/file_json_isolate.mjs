b4cc234b0e3d4034fa7fcf13b60cf9f9abd472a6
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:21:45 -0400

    function_auto_path public\src\file_json_isolate.mjs

--- web/public/src/file_json_isolate.mjs
+++ web/public/src/file_json_isolate.mjs
@@ -1,7 +1,10 @@
+import { object_property_get } from "./object_property_get.mjs";
 import { file_json_transform } from "./file_json_transform.mjs";
 import { marker } from "./marker.mjs";
 export async function file_json_isolate(f_path, property_name) {
   marker("1");
-  async function lambda(data) {}
+  async function lambda(data) {
+    let value = object_property_get(object, property_name2);
+  }
   await file_json_transform(fb_path, lambda);
 }
