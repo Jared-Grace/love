01859408e83ec18aaea8de9a59d2fd94605f3f6f
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 14:51:30 -0400

    function_auto_path public\src\data_all.mjs

--- web/public/src/data_all.mjs
+++ web/public/src/data_all.mjs
@@ -1,3 +1,4 @@
+import { file_exists } from "./file_exists.mjs";
 import { marker } from "./marker.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 export async function data_all(file_path) {
@@ -8,6 +9,7 @@ export async function data_all(file_path) {
     file_path,
   };
   return v;
+  let exists = await file_exists(file_path2);
   if (false) {
   }
 }
