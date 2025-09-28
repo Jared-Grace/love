b0d7d3e6afe617d05be7715d7197b6f66fa8f121
Grace-Jared <christrosetolife@gmail.com>
2025-09-28 16:41:37 -0400

    data_file_update public\src\repos_gitignore_overwrite.mjs

--- web/public/src/repos_gitignore_overwrite.mjs
+++ web/public/src/repos_gitignore_overwrite.mjs
@@ -5,7 +5,6 @@ export async function repos_gitignore_overwrite() {
   marker("1");
   let repos = await repos_names();
   let f_name = ".gitignore";
-  let all = await repos_names();
   function lambda(item) {}
   let mapped = list_map(list, lambda);
 }
