17d8a41f0efae37b7da9d3d6a96a544a724e34e8
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:59:30 -0400

    function_auto_path public\src\data_files_update.mjs

--- public/src/function_names_to_acronyms.mjs
+++ public/src/function_names_to_acronyms.mjs
@@ -2,8 +2,8 @@ import { object_invert } from "./object_invert.mjs";
 import { function_name_to_acronym } from "./function_name_to_acronym.mjs";
 import { list_to_dictionary } from "./list_to_dictionary.mjs";
 import { functions_names } from "./functions_names.mjs";
-export function function_names_to_acronyms() {
-  let f_names = functions_names();
+export async function function_names_to_acronyms() {
+  let f_names = await functions_names();
   let dictionary = list_to_dictionary(f_names, function_name_to_acronym);
   let acronyms = object_invert(dictionary);
   return acronyms;
