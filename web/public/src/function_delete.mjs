95a31abddd406f4c65eba4d4928d6e4e3aec27f4
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:02:31 -0400

    function_auto_path public\src\function_delete.mjs

--- web/public/src/function_delete.mjs
+++ web/public/src/function_delete.mjs
@@ -1,12 +1,12 @@
+import { assert } from "../../../love/public/src/assert.mjs";
 import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { data_files_update } from "./data_files_update.mjs";
 import { marker } from "./marker.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_delete } from "./file_delete.mjs";
 export async function function_delete(f_name) {
   marker("1");
   const { f_path, exists } = await function_exists(f_name);
-  function_name_to_path(f_name);
+  assert(b);
   await file_delete(f_path);
   await data_files_update();
 }
