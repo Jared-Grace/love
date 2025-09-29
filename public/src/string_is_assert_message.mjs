f738e7d4558c96c22b3c12a5c73cff0c699e1785
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:15:28 -0400

    function_auto_path public\src\string_is_assert_json.mjs

--- public/src/string_is_assert_message.mjs
+++ public/src/string_is_assert_message.mjs
@@ -1,5 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { assert } from "../../../love/public/src/assert.mjs";
 import { string_is } from "../../../love/public/src/string_is.mjs";
 import { assert_message } from "../../../love/public/src/assert_message.mjs";
 export function string_is_assert_message(value, message) {
