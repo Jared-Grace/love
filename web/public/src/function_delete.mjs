548ff95ee3f2a06ecb3c1507d23af8c76b552027
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 15:22:48 -0400

    o de

--- web/public/src/function_delete.mjs
+++ web/public/src/function_delete.mjs
@@ -1,6 +1,8 @@
+import { marker } from "./marker.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_delete } from "./file_delete.mjs";
 export async function function_delete(f_name) {
+  marker("1");
   const f_path = function_name_to_path(f_name);
   await file_delete(f_path);
 }
