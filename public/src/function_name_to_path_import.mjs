663c6e853a78e2cda3b2335d66d468898ab6ed80
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:56:46 -0400

    function_auto_path public\src\function_name_to_path_import.mjs

--- public/src/function_name_to_path_import.mjs
+++ public/src/function_name_to_path_import.mjs
@@ -1,15 +1,8 @@
 import { js_code_string } from "./js_code_string.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { path_join } from "./path_join.mjs";
-import { marker } from "./marker.mjs";
-import { folder_current_join_code } from "./folder_current_join_code.mjs";
-import { function_name_to_base } from "./function_name_to_base.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 export function function_name_to_path_import(import_, dictionary) {
-  marker("1");
-  let f_name_ext = function_name_to_base(import_);
-  const from = folder_current_join_code(f_name_ext);
-  return from;
   let value = object_property_get(dictionary, import_);
   let previous = folder_previous();
   let previous2 = folder_previous();
