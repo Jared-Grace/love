b9cad43d6cfda36cd9cf692e7ac36d40622309cc
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:45:14 -0400

    function_auto_path public\src\function_name_to_path_unalias.mjs

--- web/public/src/function_name_to_path_unalias.mjs
+++ web/public/src/function_name_to_path_unalias.mjs
@@ -1,10 +1,22 @@
+import { object_property_set } from "./object_property_set.mjs";
+import { list_single } from "./list_single.mjs";
+import { list_size_1 } from "./list_size_1.mjs";
+import { list_multiple_is } from "./list_multiple_is.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { true_is } from "./true_is.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { repos_paths_map_unordered_combine } from "./repos_paths_map_unordered_combine.mjs";
+import { file_exists } from "./file_exists.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 export async function function_name_to_path_unalias(f_name) {
   let { unaliased } = await function_name_unalias(f_name);
   let f_path = function_name_to_path(unaliased);
-
-
+  let v = {
+    f_path,
+    unaliased,
+  };
+  return v;
   async function lambda(joined) {
     let exists = await file_exists(joined);
     let v = {
@@ -23,18 +35,12 @@ export async function function_name_to_path_unalias(f_name) {
   let multiple = list_multiple_is(filtered);
   let exists = list_size_1(filtered);
   let result = {
-      exists,
-      multiple,
-    };
+    exists,
+    multiple,
+  };
   if (exists) {
     let only = list_single(filtered);
     let f_path2 = object_property_get(only, "f_path");
     object_property_set(result, "f_path", f_path2);
   }
-
-  let v = {
-    f_path,
-    unaliased,
-  };
-  return v;
 }
