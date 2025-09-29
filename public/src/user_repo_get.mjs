31307ec84a02b02df13c9ce890e604f3e3a7b6e7
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 15:13:57 -0400

    function_auto_path public\src\user_repo_get.mjs

--- public/src/user_repo_get.mjs
+++ public/src/user_repo_get.mjs
@@ -1,5 +1,4 @@
-import { list_includes_assert } from "./list_includes_assert.mjs";
-import { repos_names } from "./repos_names.mjs";
+import { repo_exists_assert } from "./repo_exists_assert.mjs";
 import { data_get } from "./data_get.mjs";
 import { user_repo_path } from "./user_repo_path.mjs";
 import { marker } from "./marker.mjs";
@@ -7,7 +6,6 @@ export async function user_repo_get() {
   marker("1");
   let f_path = user_repo_path();
   let { value: repo_name } = await data_get("repo_current", null, f_path);
-  let all = await repos_names();
-  list_includes_assert(all, repo_name);
+  await repo_exists_assert(repo_name);
   return repo_name;
 }
