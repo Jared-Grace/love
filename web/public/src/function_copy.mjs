88b201eebe238510b2f024380c539e3aa353f1c3
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:33:56 -0400

    function_auto_path public\src\function_copy.mjs

--- web/public/src/function_copy.mjs
+++ web/public/src/function_copy.mjs
@@ -1,3 +1,4 @@
+import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 import { function_current_set } from "./function_current_set.mjs";
 import { js_identifier_replace } from "./js_identifier_replace.mjs";
@@ -9,6 +10,7 @@ export async function function_copy(f_name_old, f_name_new) {
   const { f_path: f_path_old, unaliased: unaliased_old } =
     await function_name_to_path_unalias(f_name_old);
   let { unaliased: unaliased_new } = await function_name_unalias(f_name);
+  let v = function_name_to_path(f_name2);
   await file_copy(f_path_old, f_path_new);
   function lambda(ast) {
     js_identifier_replace(ast, unaliased_old, unaliased_new);
