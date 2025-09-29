0af35c8bb7b01f7c819a99f34034d425eb7ec537
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:13:57 -0400

    function_auto_path public\src\js_property.mjs

--- web/public/src/js_parse_statement_module.mjs
+++ web/public/src/js_parse_statement_module.mjs
@@ -1,8 +1,7 @@
 import { js_parse } from "../../../love/public/src/js_parse.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function js_parse_statement_module(code) {
-  let ast = js_parse(code);
-  let { body } = ast;
+  let { body } = js_parse(code);
   let statement = list_first(body);
   return statement;
 }
