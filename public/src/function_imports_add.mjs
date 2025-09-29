8ff94d5c6eea2390933b6c4a66569d835628d081
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:53:10 -0400

    pnd dictionary

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -8,7 +8,7 @@ export async function function_imports_add(ast, imports) {
   let dictionary = await functions_names_to_paths();
   let { body } = ast;
   async function lambda(import_) {
-    const from = function_name_to_path_import(import_);
+    const from = function_name_to_path_import(import_, dictionary);
     let code = js_code_import_single(import_, from);
     let statement = js_parse_statement_module(code);
     list_add_first(body, statement);
