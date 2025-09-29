6c0013309c36474a0ce73489e836c0af6d07fe6a
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:02:27 -0400

    function_auto_path public\src\function_delete.mjs

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -5,8 +5,8 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_delete } from "./file_delete.mjs";
 export async function function_delete(f_name) {
   marker("1");
-  await function_exists(f_name);
-  const f_path = function_name_to_path(f_name);
+  const { f_path, exists } = await function_exists(f_name);
+  function_name_to_path(f_name);
   await file_delete(f_path);
   await data_files_update();
 }
