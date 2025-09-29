b58200fb881ba449f5936dae0202b920331e114b
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:27:03 -0400

    function_auto_path public\src\functions_names.mjs

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,11 +1,10 @@
-import { sleep } from "./sleep.mjs";
 import { marker } from "./marker.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { folder_read_files } from "./folder_read_files.mjs";
 import { function_name_extension } from "./function_name_extension.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { list_map } from "./list_map.mjs";
-export async function functions_names() {
+export function functions_names() {
   marker("1");
   let dirPath = functions_path();
   let paths = folder_read_files(dirPath);
@@ -16,5 +15,4 @@ export async function functions_names() {
   }
   let f_names = list_map(paths, lambda);
   return f_names;
-  await sleep(ms);
 }
