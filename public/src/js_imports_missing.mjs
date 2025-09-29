aa512a74629bd952710dfbabd135cd4c1adbe4df
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:59:33 -0400

    data_file_update public\src\function_acronym_to_name.mjs

--- public/src/js_imports_missing.mjs
+++ public/src/js_imports_missing.mjs
@@ -6,14 +6,14 @@ import { list_concat } from "./list_concat.mjs";
 import { js_identifiers_names } from "./js_identifiers_names.mjs";
 import { js_imports } from "./js_imports.mjs";
 import { js_declaration_single } from "./js_declaration_single.mjs";
-export function js_imports_missing(ast) {
+export async function js_imports_missing(ast) {
   let name = js_declaration_single_name(ast);
   let declaration = js_declaration_single(ast);
   let imports = js_imports(ast);
   let identifiers = js_identifiers_names(declaration);
   let imports_self = list_concat(imports, [name]);
   let missing = list_difference(identifiers, imports_self);
-  let f_names = functions_names();
+  let f_names = await functions_names();
   let imports_missing = list_intersect(missing, f_names);
   return imports_missing;
 }
