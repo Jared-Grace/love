8a621f004c811bd568337d00f11abcb87296647e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:08:01 -0400

    function_auto_path public\src\functions_imports_fix.mjs

---
+++ web/public/src/functions_imports_fix.mjs
@@ -0,0 +1,9 @@
+import { functions_transform } from "./functions_transform.mjs";
+import { marker } from "./marker.mjs";
+import { js_log_remove } from "./js_log_remove.mjs";
+export async function functions_imports_fix() {
+  marker("1");
+  let lambda = js_log_remove;
+  let v = await functions_transform(lambda);
+  return v;
+}
