7978966afab613080969a3b921ce0ff37566339f
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:15:20 -0400

    function_auto_path public\src\sandbox_3.mjs

--- public/src/schtasks_query.mjs
+++ public/src/schtasks_query.mjs
@@ -1,6 +1,5 @@
 import { command_line_schtasks } from "../../../love/public/src/command_line_schtasks.mjs";
 import { throws_not_async } from "../../../love/public/src/throws_not_async.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function schtasks_query(name) {
   "todo rename query to exists";
   async function lambda2() {
