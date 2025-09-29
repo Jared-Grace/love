5913a87330820be6b440943bb1b7e089b873e967
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:15:30 -0400

    function_auto_path public\src\functions_names.mjs

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,18 +1,16 @@
+import { repos_paths_map_unordered_combine } from "./repos_paths_map_unordered_combine.mjs";
 import { list_squash } from "./list_squash.mjs";
-import { path_join } from "./path_join.mjs";
-import { repos_paths_map_unordered } from "./repos_paths_map_unordered.mjs";
 import { functions_names_from_path } from "./functions_names_from_path.mjs";
 import { marker } from "./marker.mjs";
 import { functions_path } from "./functions_path.mjs";
 export async function functions_names() {
   marker("1");
   let path = functions_path();
-  let result = await repos_paths_map_unordered(each_folder);
-  async function each_folder(folder) {
-    let joined = path_join([folder, path]);
-    let f_names = functions_names_from_path(joined);
-    return f_names;
-  }
+  let result = await repos_paths_map_unordered_combine(path, mapper);
   let squashed = list_squash(result);
   return squashed;
+  function mapper(joined) {
+    let f_names2 = functions_names_from_path(joined);
+    return f_names2;
+  }
 }
