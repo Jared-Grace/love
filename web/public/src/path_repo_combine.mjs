0d02cf661c3420f3318640166f73606238d22917
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:33:22 -0400

    function_auto_path public\src\path_repo_combine.mjs

--- web/public/src/path_repo_combine.mjs
+++ web/public/src/path_repo_combine.mjs
@@ -1,7 +1,7 @@
+import { repo_path } from "./repo_path.mjs";
 import { path_join } from "./path_join.mjs";
-import { path_repo } from "./path_repo.mjs";
 export function path_repo_combine(repo, f_path) {
-  let r_path = path_repo(repo);
+  let r_path = repo_path(repo);
   let joined = path_join([r_path, f_path]);
   return joined;
 }
