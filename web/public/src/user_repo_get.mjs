741668718eb26a66ddc7be81590cefc24cc08ea9
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 15:12:41 -0400

    function_auto_path public\src\user_repo_get.mjs

--- web/public/src/user_repo_get.mjs
+++ web/public/src/user_repo_get.mjs
@@ -1,3 +1,4 @@
+import { list_includes } from "./list_includes.mjs";
 import { data_get } from "./data_get.mjs";
 import { user_repo_path } from "./user_repo_path.mjs";
 import { marker } from "./marker.mjs";
@@ -5,5 +6,6 @@ export async function user_repo_get() {
   marker("1");
   let f_path = user_repo_path();
   let { value: repo_name } = await data_get("repo_current", null, f_path);
+  let includes = list_includes(list, item);
   return repo_name;
 }
