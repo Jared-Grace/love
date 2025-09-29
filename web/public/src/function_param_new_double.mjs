fbf514c650fe09eabf1d89d0ab488f0cca6e61d0
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:06:11 -0400

    function_auto_path public\src\function_param_new_double.mjs

---
+++ web/public/src/function_param_new_double.mjs
@@ -0,0 +1,4 @@
+import { function_params_new } from "./function_params_new.mjs";
+export async function function_param_new_double(param_name, default_value) {
+  await function_params_new(param_name, default_value);
+}
