85a9687db968d8adc3d493dbc8943961711b7ec2
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:03:57 -0400

    function_auto_path public\src\repos_paths_map_unordered.mjs

--- public/src/repos_paths_map_unordered.mjs
+++ public/src/repos_paths_map_unordered.mjs
@@ -2,12 +2,12 @@ import { marker } from "./marker.mjs";
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { repo_path } from "./repo_path.mjs";
 import { repos_names } from "./repos_names.mjs";
-export async function repos_paths_map_unordered(each_folder) {
+export async function repos_paths_map_unordered(lambda$folder) {
   marker("1");
   let all = await repos_names();
   async function lambda(repo_name) {
     let folder = repo_path(repo_name);
-    await each_folder(folder);
+    await lambda$folder(folder);
   }
   let result = await list_map_unordered_async(all, lambda);
   return result;
