b39125504a4024d99c4732cadf4734903c6b55e3
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:59:33 -0400

    data_file_update public\src\function_acronym_to_name.mjs

--- web/public/src/function_imports_missing.mjs
+++ web/public/src/function_imports_missing.mjs
@@ -4,7 +4,7 @@ import { object_merge } from "./object_merge.mjs";
 export async function function_imports_missing(f_name) {
   let parsed = await function_parse_declaration(f_name);
   let { ast } = parsed;
-  let imports_missing = js_imports_missing(ast);
+  let imports_missing = await js_imports_missing(ast);
   let to = object_merge(
     {
       imports_missing,
