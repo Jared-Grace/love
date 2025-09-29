1ea6bc7714119717b7bc458a7d7f78605dca3689
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:16:01 -0400

    function_auto_path public\src\functions_paths.mjs

--- public/src/functions_paths.mjs
+++ public/src/functions_paths.mjs
@@ -1,8 +1,9 @@
-import { function_name_to_path } from "./function_name_to_path.mjs";
-import { list_map } from "./list_map.mjs";
-import { functions_names } from "./functions_names.mjs";
+import { list_squash } from "./list_squash.mjs";
+import { repos_paths_map_unordered_combine } from "./repos_paths_map_unordered_combine.mjs";
+import { functions_path } from "./functions_path.mjs";
 export async function functions_paths() {
-  let f_names = await functions_names();
-  let f_paths = list_map(f_names, function_name_to_path);
-  return f_paths;
+  let path = functions_path();
+  let result = await repos_paths_map_unordered_combine(path, mapper);
+  let squashed = list_squash(result);
+  return squashed;
 }
