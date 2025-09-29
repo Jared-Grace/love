eebe15aa0eece2135dda037bf30227cf88e91713
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 13:48:49 -0400

    function_auto_path public\src\user_repo_get.mjs

--- public/src/user_repo_get.mjs
+++ public/src/user_repo_get.mjs
@@ -4,6 +4,6 @@ import { marker } from "./marker.mjs";
 export async function user_repo_get() {
   marker("1");
   let f_path = user_repo_path();
-  let { value: repo } = await data_get("repo_current", null, f_path);
-  return repo;
+  let { value: repo_name } = await data_get("repo_current", null, f_path);
+  return repo_name;
 }
