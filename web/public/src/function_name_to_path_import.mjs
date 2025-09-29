f2113986755e2d412dcd238a4d4f38e2ee94b09b
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:55:42 -0400

    data_file_update public\src\function_name_to_path_import.mjs

--- web/public/src/function_name_to_path_import.mjs
+++ web/public/src/function_name_to_path_import.mjs
@@ -3,10 +3,10 @@ import { path_join } from "./path_join.mjs";
 import { marker } from "./marker.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 export function function_name_to_path_import(import_, dictionary) {
-  marker("1");l
+  marker("1");
   let value = object_property_get(dictionary, import_);
   let previous = folder_previous();
   let previous2 = folder_previous();
   let joined = path_join([previous, previous2, value]);
-  return joined;
+  return joined;l
 }
