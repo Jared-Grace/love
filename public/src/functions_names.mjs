02e6cadfe41be0bb59f65ba5b03de3364df6c27e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:07:38 -0400

    functions_names

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -7,8 +7,6 @@ import { functions_path } from "./functions_path.mjs";
 export async function functions_names() {
   marker("1");
   let path = functions_path();
-  let v = functions_names_from_path(path);
-  return v;
   let result = await repos_paths_map_unordered(each_folder);
   async function each_folder(folder) {
     let joined = path_join([folder, path]);
