47b724bbaed9a38e4683f43b12615a3afd24ddd4
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:53:48 -0400

    function_auto_path public\src\js_await_add.mjs

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -2,7 +2,7 @@ import { data_functions_get } from "./data_functions_get.mjs";
 import { js_await_add_inner } from "./js_await_add_inner.mjs";
 export async function js_await_add(ast) {
   let functions = await data_functions_get();
+  let visited = [];
   let v2 = await js_await_add_inner(functions, ast);
   return v2;
-  let visited = [];
 }
