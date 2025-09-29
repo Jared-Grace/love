d424db1798c961581abea8b97e77f028e1eb8c15
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:14:15 -0400

    function_auto_path public\src\list_find_starts_with.mjs

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -1,8 +1,4 @@
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
-import { assert_json } from "../../../love/public/src/assert_json.mjs";
-import { assert_message } from "../../../love/public/src/assert_message.mjs";
-import { assert } from "../../../love/public/src/assert.mjs";
-import { integer_is } from "../../../love/public/src/integer_is.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
 export function list_get(list, index) {
   integer_is_assert(index);
