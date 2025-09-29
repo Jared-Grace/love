ea1e44e62525d784a0eddb2f2e88fc7e7499b15e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:50:51 -0400

    data_file_update public\src\function_exists_strict.mjs

--- public/src/function_exists_strict.mjs
+++ public/src/function_exists_strict.mjs
@@ -1,14 +1,7 @@
 import { function_name_to_path_search } from "./function_name_to_path_search.mjs";
-import { function_name_to_path } from "./function_name_to_path.mjs";
 import { marker } from "./marker.mjs";
-import { function_exists_inner } from "./function_exists_inner.mjs";
 export async function function_exists_strict(f_name) {
   marker("1");
-  const f_path = function_name_to_path(f_name);
-  let e = await function_exists_inner({
-    f_path,
-  });
-  return e;
   let result = await function_name_to_path_search(f_name);
   return result;
 }
