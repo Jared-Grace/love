329a37ec1132e607b82324bb52fcb0485b181332
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:02:34 -0400

    function_auto_path public\src\function_delete.mjs

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -6,7 +6,7 @@ import { file_delete } from "./file_delete.mjs";
 export async function function_delete(f_name) {
   marker("1");
   const { f_path, exists } = await function_exists(f_name);
-  assert(b);
+  assert(exists);
   await file_delete(f_path);
   await data_files_update();
 }
