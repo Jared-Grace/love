55abb26963964550460fca7e296c8657acb217eb
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:45:09 -0400

    data_file_update public\src\function_name_to_path_unalias.mjs

--- public/src/function_name_to_path_unalias.mjs
+++ public/src/function_name_to_path_unalias.mjs
@@ -2,7 +2,7 @@ import { function_name_to_path } from "./function_name_to_path.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
 export async function function_name_to_path_unalias(f_name) {
   let { unaliased } = await function_name_unalias(f_name);
-  const f_path = function_name_to_path(unaliased);
+  let f_path = function_name_to_path(unaliased);
 
 
   async function lambda(joined) {
