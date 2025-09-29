dafa774f3324bf6e86a3c799968837aedf6b3db0
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:10:10 -0400

    pnd ast

--- public/src/js_function_last_asyncify.mjs
+++ public/src/js_function_last_asyncify.mjs
@@ -3,7 +3,7 @@ import { not } from "./not.mjs";
 import { js_stack_last_function } from "./js_stack_last_function.mjs";
 import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
-export function js_function_last_asyncify(stack, async_is) {
+export function js_function_last_asyncify(stack, async_is, ast) {
   marker("1");
   let f = js_stack_last_function(stack);
   let property_name = "async";
