7296bd741aeeb416fee8d65d837500b58f0fe764
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:03:22 -0400

    r repos_path_each_unordered repos_paths_map_unordered

--- web/public/src/repos_paths_map_unordered.mjs
+++ web/public/src/repos_paths_map_unordered.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { repo_path } from "./repo_path.mjs";
 import { repos_names } from "./repos_names.mjs";
 export async function repos_paths_map_unordered(each_folder) {
+  marker("1");
   let all = await repos_names();
   async function lambda(repo_name) {
     let folder = repo_path(repo_name);
