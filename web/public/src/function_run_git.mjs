209daba5fc1e6a05e218c49a88dde991d59dd4e4
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:01:37 -0400

    function_auto_path public\src\function_run_git.mjs

--- web/public/src/function_run_git.mjs
+++ web/public/src/function_run_git.mjs
@@ -8,8 +8,11 @@ export async function function_run_git(f_name, args) {
   let all = await repos_names();
   async function lambda(repo_name) {
     let folder = repo_path(repo_name);
-    await git_acp_call_folder(f_name, args, folder);
+    await each_folder(folder);
   }
   await list_map_unordered_async(all, lambda);
   return result;
+  async function each_folder(folder) {
+    await git_acp_call_folder(f_name, args, folder);
+  }
 }
