24d32b5cf847af61884e2fbeb2f9054e526df916
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:51:04 -0400

    function_auto_path public\src\function_imports_add.mjs

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -1,9 +1,10 @@
+import { functions_names_to_paths } from "./functions_names_to_paths.mjs";
 import { function_name_to_path_import } from "./function_name_to_path_import.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
-export async function function_imports_add(ast, imports) {functions_names_to_paths
+export async function function_imports_add(ast, imports) {
   let { body } = ast;
   async function lambda(import_) {
     const from = function_name_to_path_import(import_);
@@ -12,4 +13,6 @@ export async function function_imports_add(ast, imports) {functions_names_to_pat
     list_add_first(body, statement);
   }
   await each_async(imports, lambda);
+  return;
+  let reduced = await functions_names_to_paths();
 }
