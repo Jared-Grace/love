cfb07de98d8929a4adc35a8b0ceaf4364ecc4a56
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:20:15 -0400

    function_auto_path public\src\function_exists_inner.mjs

--- public/src/function_exists_inner.mjs
+++ public/src/function_exists_inner.mjs
@@ -12,6 +12,5 @@ export async function function_exists_inner(u) {
   let e = object_merge(to, u);
   return e;
   let bools = await repos_paths_map_unordered_combine(f_path, file_exists);
-  function lambda(item) {}
   let filtered = list_filter(bools, true_is);
 }
