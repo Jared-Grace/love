a82bb7964f46f217558265930d25dde759e7fa18
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:15:29 -0400

    function_auto_path public\src\roman_to_integer.mjs

--- public/src/string_prefix_without.mjs
+++ public/src/string_prefix_without.mjs
@@ -1,13 +1,14 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { string_skip } from "../../../love/public/src/string_skip.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
-import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
-import { string_slice } from "../../../love/public/src/string_slice.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_prefix_without(s, prefix) {
-  if (!string_starts_with(s, prefix)) {
+  let a = string_starts_with(s, prefix);
+  if (not(a)) {
     error();
   }
   let skipped = string_size(prefix);
-  return string_skip(s, skipped);
+  let skipped2 = string_skip(s, skipped);
+  return skipped2;
 }
