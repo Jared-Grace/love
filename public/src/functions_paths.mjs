4e867db01e608b819c2d6a30496f23e22e59a115
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:19:18 -0400

    data_file_update public\src\functions_paths.mjs

--- public/src/functions_paths.mjs
+++ public/src/functions_paths.mjs
@@ -1,9 +1,11 @@
-import { identity } from "./identity.mjs";
-import { list_squash } from "./list_squash.mjs";
-import { repos_paths_map_unordered_combine } from "./repos_paths_map_unordered_combine.mjs";
-import { functions_path } from "./functions_path.mjs";
+import { function_name_to_path } from "./function_name_to_path.mjs";
+import { list_map } from "./list_map.mjs";
+import { functions_names } from "./functions_names.mjs";
 export async function functions_paths() {
-  let path = functions_path();
+  let f_names = await functions_names();
+  let f_paths = list_map(f_names, function_name_to_path);
+  return f_paths;
+    let path = functions_path();
   let result = await repos_paths_map_unordered_combine(path, r=>{
     let v = function_name_folder_to_path(f_name, folder);});
   let squashed = list_squash(result);
