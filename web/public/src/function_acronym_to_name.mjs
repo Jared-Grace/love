2104be47d3d69c57b3f42c5712bd36dce696ee36
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:59:29 -0400

    function_auto_path public\src\functions_paths.mjs

--- web/public/src/function_acronym_to_name.mjs
+++ web/public/src/function_acronym_to_name.mjs
@@ -3,10 +3,10 @@ import { list_size_1 } from "./list_size_1.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { function_names_to_acronyms } from "./function_names_to_acronyms.mjs";
-export function function_acronym_to_name(alias) {
+export async function function_acronym_to_name(alias) {
   let expanded = null;
   let expandeds = null;
-  let acronyms = function_names_to_acronyms();
+  let acronyms = await function_names_to_acronyms();
   const exists2 = object_property_exists(acronyms, alias);
   if (exists2) {
     expandeds = object_property_get(acronyms, alias);
