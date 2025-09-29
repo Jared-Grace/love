c9d6ff90c79395fee085319740ed45655fdc1ca3
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:08:13 -0400

    function_auto_path public\src\marker_expand.mjs

--- web/public/src/marker_next_delete.mjs
+++ web/public/src/marker_next_delete.mjs
@@ -2,9 +2,9 @@ import { list_remove_at } from "./list_remove_at.mjs";
 import { marker_next_index } from "./marker_next_index.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
 import { function_transform_marker } from "./function_transform_marker.mjs";
-import { data_function_current_get } from "./data_function_current_get.mjs";
+import { function_current_get } from "./function_current_get.mjs";
 export async function marker_next_delete() {
-  let f_name = await data_function_current_get();
+  let f_name = await function_current_get();
   async function lambda2(la) {
     await function_transform_marker(f_name, lambda);
     function lambda(a) {
