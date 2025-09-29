ac29d8053bed47aac147a29b79f31133640ab901
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:46:19 -0400

    function_auto_path public\src\functions_names_to_paths.mjs

--- public/src/functions_names_to_paths.mjs
+++ public/src/functions_names_to_paths.mjs
@@ -5,11 +5,10 @@ import { functions_names_from_path } from "./functions_names_from_path.mjs";
 import { function_name_folder_to_path } from "./function_name_folder_to_path.mjs";
 export async function functions_names_to_paths() {
   marker("1");
-  function lambda(item) {}
   function mapper(folder) {
     let f_names = functions_names_from_path(folder);
     let dictionary = list_to_dictionary(f_names, function_name_folder_to_path);
-    return mapped;
+    return dictionary;
   }
   let squashed = await repos_paths_map_unordered_combine_squash(mapper);
   return squashed;
