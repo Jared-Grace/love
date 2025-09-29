9365a01661848b150b4ec7d50bad13572d58795e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:56:58 -0400

    function_auto_path public\src\function_name_to_path_import.mjs

--- public/src/function_name_to_path_import.mjs
+++ public/src/function_name_to_path_import.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { path_join } from "./path_join.mjs";
@@ -10,5 +9,4 @@ export function function_name_to_path_import(import_, dictionary) {
   let joined = path_join([previous, previous2, value]);
   const c = js_code_string(joined);
   return c;
-  log(message);
 }
