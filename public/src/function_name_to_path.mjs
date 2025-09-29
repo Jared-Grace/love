bbc86e98958ed8da3c647c959c30165f53658331
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:18:28 -0400

    function_auto_path public\src\function_name_to_path.mjs

--- public/src/function_name_to_path.mjs
+++ public/src/function_name_to_path.mjs
@@ -1,15 +1,12 @@
-import { path_join } from "./path_join.mjs";
+import { function_name_folder_to_path } from "./function_name_folder_to_path.mjs";
 import { string_includes } from "./string_includes.mjs";
 import { error } from "./error.mjs";
-import { function_name_to_base } from "./function_name_to_base.mjs";
 import { functions_path } from "./functions_path.mjs";
 export function function_name_to_path(f_name) {
   if (string_includes(f_name, ".")) {
     error();
   }
-  let joined2 = functions_path();
-  let v = function_name_to_base(f_name);
-  let second = [joined2, v];
-  let joined = path_join(second);
-  return joined;
+  let folder = functions_path();
+  let v = function_name_folder_to_path(f_name, folder);
+  return v;
 }
