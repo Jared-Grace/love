539773549f284f9fa681770032338b83dca8be1a
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:34:38 -0400

    s await

--- public/src/js_await_add.mjs
+++ public/src/js_await_add.mjs
@@ -1,48 +1,6 @@
-import { not } from "./not.mjs";
-import { object_replace } from "./object_replace.mjs";
-import { js_await } from "./js_await.mjs";
-import { object_copy } from "./object_copy.mjs";
-import { js_node_type_not_is_if } from "./js_node_type_not_is_if.mjs";
-import { list_get_end_1 } from "./list_get_end_1.mjs";
-import { js_function_last_asyncify } from "./js_function_last_asyncify.mjs";
-import { object_property_exists_not } from "./object_property_exists_not.mjs";
-import { js_visit_type_each_async } from "./js_visit_type_each_async.mjs";
 import { data_functions_get } from "./data_functions_get.mjs";
-import { js_call_function_if } from "./js_call_function_if.mjs";
-import { object_property_get } from "./object_property_get.mjs";
-import { marker } from "./marker.mjs";
 export async function js_await_add(ast) {
   let functions = await data_functions_get();
-  return await js_await_add_inner(functions, ast);
-}
-
-async function js_await_add_inner(functions, ast) {
-  async function lambda(v) {
-    let node = object_property_get(v, "node");
-    async function lambda3(name) {
-      let en = object_property_exists_not(functions, name);
-      if (en) {
-        return;
-      }
-      let f = object_property_get(functions, name);
-      let async_is = object_property_get(f, "async");
-      let n = not(async_is);
-      if (n) {
-        marker("1");
-        return;
-      }
-      let stack = object_property_get(v, "stack");
-      js_function_last_asyncify(stack, async_is, ast);
-      let stack1 = list_get_end_1(stack);
-      function lambda4() {
-        let copy = object_copy(node);
-        let awaited = js_await(copy);
-        object_replace(node, awaited);
-      }
-      js_node_type_not_is_if(stack1, "AwaitExpression", lambda4);
-    }
-    await js_call_function_if(node, lambda3);
-  }
-  await js_visit_type_each_async(ast, "CallExpression", lambda);
-  return;
+  let v2 = await js_await_add_inner(functions, ast); 
+  return v2; 
 }
