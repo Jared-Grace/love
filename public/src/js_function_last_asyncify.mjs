e8d26fe73185cd06185184ebd031782698cf436f
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:10:44 -0400

    function_auto_path public\src\js_function_last_asyncify.mjs

--- public/src/js_function_last_asyncify.mjs
+++ public/src/js_function_last_asyncify.mjs
@@ -10,7 +10,7 @@ export function js_function_last_asyncify(stack, async_is, ast) {
   let property_name = "async";
   let async = object_property_get(f, property_name);
   if (async_is && not(async)) {
-    let declaration = js_declaration_single(ast2);
+    let declaration = js_declaration_single(ast);
     object_property_set(f, property_name, true);
   }
 }
