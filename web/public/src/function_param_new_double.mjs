6dd8dfd4aa804ccf6cabed9aebb2b0b60d18497d
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:06:22 -0400

    function_auto_path public\src\function_param_new_double.mjs

--- web/public/src/function_param_new_double.mjs
+++ web/public/src/function_param_new_double.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 import { function_params_new } from "./function_params_new.mjs";
-export async function function_param_new_double(param_name, default_value) {
+export async function function_param_new_double(param_name) {
   marker("1");
-  await function_params_new(param_name, default_value);
+  await function_params_new(param_name, param_name);
 }
