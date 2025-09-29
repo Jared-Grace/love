fec55d3d901609d5e362421dd734448dfbaad434
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:56:48 -0400

    data_file_update public\src\object_merge_generic.mjs

--- public/src/object_merge_generic.mjs
+++ public/src/object_merge_generic.mjs
@@ -4,9 +4,7 @@ import { object_property_set } from "./object_property_set.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { object_property_exists } from "./object_property_exists.mjs";
 import { error_json } from "./error_json.mjs";
-import { list_map } from "./list_map.mjs";
 export function object_merge_generic(strict, to, from) {
-  let ps = list_map([to, from], object_properties);
   function lambda(property_name) {
     if (strict) {
       if (object_property_exists(to, property_name)) {
