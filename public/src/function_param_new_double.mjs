5d44435c8984e0bb9889066b9e81e20f10126b2e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:06:34 -0400

    pnd ast

--- public/src/function_param_new_double.mjs
+++ public/src/function_param_new_double.mjs
@@ -1,6 +1,6 @@
 import { marker } from "./marker.mjs";
 import { function_params_new } from "./function_params_new.mjs";
-export async function function_param_new_double(param_name) {
+export async function function_param_new_double(param_name, ast) {
   marker("1");
   await function_params_new(param_name, param_name);
 }
