d20f22cc6b24f6d94022fb661187941a313044e6
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 15:11:00 -0400

    o git_acp_folder

--- public/src/git_acp_folder.mjs
+++ public/src/git_acp_folder.mjs
@@ -1,7 +1,9 @@
+import { marker } from "./marker.mjs";
 import { git_push_folder } from "./git_push_folder.mjs";
 import { git_commit_folder } from "./git_commit_folder.mjs";
 import { command_line_git_folder } from "./command_line_git_folder.mjs";
 export async function git_acp_folder(folder, message) {
+  marker("1");
   await command_line_git_folder(folder, "add -A");
   await git_commit_folder(folder, message);
   await git_push_folder(folder);
