fe8c9dd6fc64c336c96b213f8b3814e44b4b176c
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:48:53 -0400

    function_auto_path public\src\reduce.mjs

--- public/src/reduce.mjs
+++ public/src/reduce.mjs
@@ -1,4 +1,6 @@
+import { each } from "./each.mjs";
 import { marker } from "./marker.mjs";
-export function reduce() {
+export function reduce(start, list, lambda$before$current) {
+  each(list2, function lambda(item) {});
   marker("1");
 }
