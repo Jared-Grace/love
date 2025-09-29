d5c4f8766732ed21512f26316a26a0a06a492305
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 15:22:50 -0400

    function_auto_path public\src\function_delete.mjs

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -1,3 +1,4 @@
+import { data_files_update } from "./data_files_update.mjs";
 import { marker } from "./marker.mjs";
 import { function_name_to_path } from "./function_name_to_path.mjs";
 import { file_delete } from "./file_delete.mjs";
@@ -5,4 +6,5 @@ export async function function_delete(f_name) {
   marker("1");
   const f_path = function_name_to_path(f_name);
   await file_delete(f_path);
+  await data_files_update();
 }
