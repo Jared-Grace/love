7be4e2ddc35f70971736dda95deb80c1132df3d8
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:09:35 -0400

    function_auto_path public\src\repo_acronym_to_path.mjs

--- public/src/repo_acronym_to_path.mjs
+++ public/src/repo_acronym_to_path.mjs
@@ -1,7 +1,7 @@
-import { path_repo } from "./path_repo.mjs";
 import { repo_acronym_to_name } from "./repo_acronym_to_name.mjs";
+import { repo_path } from "./repo_path.mjs";
 export async function repo_acronym_to_path(acronym) {
   let repo_name = await repo_acronym_to_name(acronym);
-  let folder = path_repo(repo_name);
+  let folder = repo_path(repo_name);
   return folder;
 }
