ba01d19fd27105e8a9ddcfa3c2d1a011ad880725
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:19:31 -0400

    function_auto_path public\src\function_name_folder_to_path.mjs

---
+++ public/src/function_name_folder_to_path.mjs
@@ -0,0 +1,8 @@
+import { path_join } from "./path_join.mjs";
+import { function_name_to_base } from "./function_name_to_base.mjs";
+export function function_name_folder_to_path(f_name, folder) {
+  let base = function_name_to_base(f_name);
+  let second = [folder, base];
+  let joined = path_join(second);
+  return joined;
+}
