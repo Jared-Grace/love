51cb91bbcac5946d18b37fccc6b0fc232be4433e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:03:22 -0400

    r repos_path_each_unordered repos_paths_map_unordered

--- public/src/function_run_git.mjs
+++ public/src/function_run_git.mjs
@@ -1,9 +1,9 @@
-import { repos_path_each_unordered } from "./repos_path_each_unordered.mjs";
+import { repos_paths_map_unordered } from "./repos_paths_map_unordered.mjs";
 import { git_acp_call_folder } from "./git_acp_call_folder.mjs";
 import { function_run_log } from "./function_run_log.mjs";
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
-  await repos_path_each_unordered(each_folder);
+  await repos_paths_map_unordered(each_folder);
   return result;
   async function each_folder(folder) {
     await git_acp_call_folder(f_name, args, folder);
