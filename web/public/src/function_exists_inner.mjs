f768ea44a4da4deb24204b890c3340c9ef72fe71
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:27:45 -0400

    function_auto_path public\src\function_exists_inner.mjs

--- web/public/src/function_exists_inner.mjs
+++ web/public/src/function_exists_inner.mjs
@@ -1,3 +1,4 @@
+import { list_single } from "./list_single.mjs";
 import { true_is } from "./true_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
@@ -25,12 +26,13 @@ export async function function_exists_inner(u) {
   let mapped = await repos_paths_map_unordered_combine(f_path, lambda);
   function lambda2(m) {
     let exists2 = object_property_get(m, "exists");
-    let ti2 = true_is(value);
+    let ti2 = true_is(exists2);
     return ti2;
   }
   let filtered = list_filter(mapped, lambda2);
   let multiple = list_multiple_is(filtered);
   exists = list_size_1(filtered);
+  let only = list_single(list);
   to = {
     exists,
   };
