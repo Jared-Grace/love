2d66381b32154e89038673b2c37da3bdda189579
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:23:12 -0400

    function_auto_path public\src\repos_paths_map_unordered_combine_squash.mjs

---
+++ public/src/repos_paths_map_unordered_combine_squash.mjs
@@ -0,0 +1,9 @@
+import { list_squash } from "./list_squash.mjs";
+import { repos_paths_map_unordered_combine } from "./repos_paths_map_unordered_combine.mjs";
+import { functions_path } from "./functions_path.mjs";
+export async function repos_paths_map_unordered_combine_squash(mapper) {
+  let path = functions_path();
+  let result = await repos_paths_map_unordered_combine(path, mapper);
+  let squashed = list_squash(result);
+  return squashed;
+}
