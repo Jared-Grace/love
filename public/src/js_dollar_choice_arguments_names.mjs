166892744240e327d4730ba198a4184e9b52c3d4
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:13:24 -0400

    function_auto_path public\src\js_dollar_choice_arguments.mjs

--- public/src/js_dollar_choice_arguments_names.mjs
+++ public/src/js_dollar_choice_arguments_names.mjs
@@ -4,8 +4,7 @@ import { js_identifiers_to_names } from "../../../love/public/src/js_identifiers
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export async function js_dollar_choice_arguments_names() {
   marker("1");
-  let oe = await js_dollar_choice_arguments();
-  let { properties } = oe;
+  let { properties } = await js_dollar_choice_arguments();
   let mapped = list_map_property(properties, "key");
   let names = js_identifiers_to_names(mapped);
   return names;
