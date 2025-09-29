637093855f9f0610c8f3aa8651747fa68fd61c54
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:53:10 -0400

    pnd dictionary

--- public/src/function_name_to_path_import.mjs
+++ public/src/function_name_to_path_import.mjs
@@ -1,7 +1,7 @@
 import { marker } from "./marker.mjs";
 import { folder_current_join_code } from "./folder_current_join_code.mjs";
 import { function_name_to_base } from "./function_name_to_base.mjs";
-export function function_name_to_path_import(import_) {
+export function function_name_to_path_import(import_, dictionary) {
   marker("1");
   let f_name_ext = function_name_to_base(import_);
   const from = folder_current_join_code(f_name_ext);
