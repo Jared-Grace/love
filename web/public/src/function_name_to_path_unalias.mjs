8ea17d82735914051c52a02ba0e8774965af3848
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:14:16 -0400

    function_auto_path public\src\function_name_to_path_unalias.mjs

--- web/public/src/function_name_to_path_unalias.mjs
+++ web/public/src/function_name_to_path_unalias.mjs
@@ -1,7 +1,10 @@
 import { function_name_to_path_search } from "./function_name_to_path_search.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
+import { object_merge } from "./object_merge.mjs";
 export async function function_name_to_path_unalias(f_name) {
   let u = await function_name_unalias(f_name);
+  let { unaliased } = u;
   let result = await function_name_to_path_search(unaliased);
-  return result;let { unaliased }=u;om
+  let to = object_merge(u, result);
+  return to;
 }
