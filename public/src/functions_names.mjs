9d34790400a1975764eaee03a71a0d6cddf893ea
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:04:19 -0400

    function_auto_path public\src\functions_names.mjs

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,7 +1,6 @@
 import { git_acp_call_folder } from "./git_acp_call_folder.mjs";
 import { repos_paths_map_unordered } from "./repos_paths_map_unordered.mjs";
 import { functions_names_from_path } from "./functions_names_from_path.mjs";
-import { sleep } from "./sleep.mjs";
 import { marker } from "./marker.mjs";
 import { functions_path } from "./functions_path.mjs";
 export async function functions_names() {
@@ -9,7 +8,6 @@ export async function functions_names() {
   let path = functions_path();
   let v = functions_names_from_path(path);
   return v;
-  await sleep(ms);
   await repos_paths_map_unordered(each_folder);
   async function each_folder(folder) {
     await git_acp_call_folder(f_name, args, folder);
