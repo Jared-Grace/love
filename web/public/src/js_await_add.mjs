9a269a89932fa1e464a8a6322ee5fc1366b34dd0
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:54:03 -0400

    pnd visited

--- web/public/src/js_await_add.mjs
+++ web/public/src/js_await_add.mjs
@@ -3,6 +3,6 @@ import { js_await_add_inner } from "./js_await_add_inner.mjs";
 export async function js_await_add(ast) {
   let functions = await data_functions_get();
   let visited = [];
-  let v2 = await js_await_add_inner(functions, ast);
+  let v2 = await js_await_add_inner(functions, ast, visited);
   return v2;
 }
