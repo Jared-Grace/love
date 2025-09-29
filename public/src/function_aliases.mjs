bfff60399cc21f9347bb1d7680dbfb21ce258f73
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:12:02 -0400

    function_auto_path public\src\function_auto.mjs

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,6 +1,5 @@
 import { function_alias_add_generic } from "../../../love/public/src/function_alias_add_generic.mjs";
 export async function function_aliases() {
-  let a = await function_alias_add_generic("");
-  let { aliases } = a;
+  let { aliases } = await function_alias_add_generic("");
   return aliases;
 }
