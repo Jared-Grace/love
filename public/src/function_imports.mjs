11d70ded4d357a97dbadc714ab6ed18b5a0785f7
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:12:17 -0400

    function_auto_path public\src\function_imports_missing.mjs

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -1,8 +1,7 @@
 import { js_imports } from "../../../love/public/src/js_imports.mjs";
 import { function_parse } from "../../../love/public/src/function_parse.mjs";
 export async function function_imports(f_name) {
-  let parsed = await function_parse(f_name);
-  let { ast } = parsed;
+  let { ast } = await function_parse(f_name);
   let imports = js_imports(ast);
   return imports;
 }
