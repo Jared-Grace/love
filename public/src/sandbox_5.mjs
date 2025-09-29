b9510218880645b38342c7531356ebae6314a0d8
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:11:30 -0400

    function_auto_path public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,7 +1,6 @@
+import { sleep } from "./sleep.mjs";
 import { marker } from "./marker.mjs";
-import { sandbox_5_a } from "./sandbox_5_a.mjs";
-export function sandbox_5() {
+export async function sandbox_5() {
   marker("1");
-  let v = sandbox_5_a(5);
-  return v;
+  await sleep(ms);
 }
