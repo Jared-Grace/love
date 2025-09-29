60725cd05d01f8fffc1d227f8976b96d580ae79a
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 17:11:39 -0400

    data_file_update public\src\object_property_get.mjs

--- public/src/object_property_get.mjs
+++ public/src/object_property_get.mjs
@@ -1,7 +1,4 @@
 import { undefined_not_is_assert_lambda } from "./undefined_not_is_assert_lambda.mjs";
-import { json_to } from "./json_to.mjs";
-import { error } from "./error.mjs";
-import { undefined_is } from "./undefined_is.mjs";
 export function object_property_get(object, property_name) {
   let value = object[property_name];
   undefined_not_is_assert_lambda(value, object_get);
