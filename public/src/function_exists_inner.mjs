bbfc78d18d84b6cb4c18f84086b5e8b1b4797ef2
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:37:18 -0400

    function_auto_path public\src\function_exists_inner.mjs

--- public/src/function_exists_inner.mjs
+++ public/src/function_exists_inner.mjs
@@ -1,5 +1,5 @@
+import { object_property_set } from "./object_property_set.mjs";
 import { object_merge } from "./object_merge.mjs";
-import { object_property_set_exists_not } from "./object_property_set_exists_not.mjs";
 import { list_single } from "./list_single.mjs";
 import { true_is } from "./true_is.mjs";
 import { object_property_get } from "./object_property_get.mjs";
@@ -27,15 +27,17 @@ export async function function_exists_inner(u) {
   let filtered = list_filter(mapped, lambda2);
   let multiple = list_multiple_is(filtered);
   let exists = list_size_1(filtered);
-  let to2 = object_merge(to, from);
-  let result = {
-    exists,
-    multiple,
-  };
+  let result = object_merge(
+    {
+      exists,
+      multiple,
+    },
+    u,
+  );
   if (exists) {
     let only = list_single(filtered);
     let f_path2 = object_property_get(only, "f_path");
-    object_property_set_exists_not(result, "f_path", f_path2);
+    object_property_set(result, "f_path", f_path2);
   }
   return result;
 }
