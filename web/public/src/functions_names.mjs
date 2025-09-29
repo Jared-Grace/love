0e520e367d891174eddc549dd9ce47c18654c852
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:24:38 -0400

    o functions_names

--- web/public/src/functions_names.mjs
+++ web/public/src/functions_names.mjs
@@ -1,9 +1,11 @@
+import { marker } from "./marker.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { folder_read_files } from "./folder_read_files.mjs";
 import { function_name_extension } from "./function_name_extension.mjs";
 import { string_suffix_without } from "./string_suffix_without.mjs";
 import { list_map } from "./list_map.mjs";
 export function functions_names() {
+  marker("1");
   let dirPath = functions_path();
   let paths = folder_read_files(dirPath);
   function lambda(p) {
