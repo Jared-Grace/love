2a5fea0beaa89a7dc996033a68b99a192f58d8f5
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:08:23 -0400

    function_auto_path public\src\functions_imports_fix.mjs

--- public/src/functions_imports_fix.mjs
+++ public/src/functions_imports_fix.mjs
@@ -1,9 +1,9 @@
 import { functions_transform } from "./functions_transform.mjs";
 import { marker } from "./marker.mjs";
-import { js_log_remove } from "./js_log_remove.mjs";
+import { js_imports_paths_fix } from "./js_imports_paths_fix.mjs";
 export async function functions_imports_fix() {
   marker("1");
-  let lambda = js_log_remove;
+  let lambda = js_imports_paths_fix;
   let v = await functions_transform(lambda);
   return v;
 }
