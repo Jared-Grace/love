5699a476250c33e720e9066caeb91a01668409e9
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 16:02:05 -0400

    function_auto_path public\src\function_run_git.mjs

---
+++ public/src/repos_path_each_unordered.mjs
@@ -0,0 +1,11 @@
+import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
+import { repo_path } from "./repo_path.mjs";
+import { repos_names } from "./repos_names.mjs";
+export async function repos_path_each_unordered(each_folder) {
+  let all = await repos_names();
+  async function lambda(repo_name) {
+    let folder = repo_path(repo_name);
+    await each_folder(folder);
+  }
+  await list_map_unordered_async(all, lambda);
+}
