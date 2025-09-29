df4ee61be7e4d934aff46abd984bad01ccda9f2e
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:01:43 -0400

    o function_exists

--- web/public/src/function_exists.mjs
+++ web/public/src/function_exists.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_name_to_path_unalias } from "./function_name_to_path_unalias.mjs";
 export async function function_exists(f_name) {
+  marker("1");
   let u = await function_name_to_path_unalias(f_name);
   return u;
 }
