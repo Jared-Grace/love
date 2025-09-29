0a38ad0e9c2bcf2280e1892ecc2d30b4757448ba
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 18:15:42 -0400

    data_file_update public\src\add_1.mjs

--- public/src/visit_unique_async.mjs
+++ public/src/visit_unique_async.mjs
@@ -1,13 +1,12 @@
 import { list_add_if_not_includes } from "../../../love/public/src/list_add_if_not_includes.mjs";
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { visit_async } from "../../../love/public/src/visit_async.mjs";
 export async function visit_unique_async(node, children_get, on_each) {
   let found = [];
   async function lambda(node) {
     let children = await children_get(node);
-    return list_difference(children, found);
+    let difference = list_difference(children, found);
+    return difference;
   }
   async function lambda2(v) {
     let { node } = v;
