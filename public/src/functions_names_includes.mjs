93a1438168c502d7daf7452169aff9516b045e29
Grace-Jared <christrosetolife@gmail.com>
2025-09-29 14:59:28 -0400

    function_auto_path public\src\functions_names_includes.mjs

--- public/src/functions_names_includes.mjs
+++ public/src/functions_names_includes.mjs
@@ -1,7 +1,7 @@
 import { list_includes } from "./list_includes.mjs";
 import { functions_names } from "./functions_names.mjs";
-export function functions_names_includes(name) {
-  let list = functions_names();
+export async function functions_names_includes(name) {
+  let list = await functions_names();
   const valid = list_includes(list, name);
   return valid;
 }
