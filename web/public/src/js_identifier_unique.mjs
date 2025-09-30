4d18c0f2dbb673372f2929c9ea9328d3c161b809
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 19:52:03 -0400

    function_auto_path ..\love\public\src\js_identifier_unique.mjs

--- web/public/src/js_identifier_unique.mjs
+++ web/public/src/js_identifier_unique.mjs
@@ -1,3 +1,4 @@
+import { js_keyword_default } from "../../../love/public/src/js_keyword_default.mjs";
 import { js_keyword_arguments } from "../../../love/public/src/js_keyword_arguments.mjs";
 import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
@@ -6,9 +7,10 @@ import { string_empty_not_is } from "../../../love/public/src/string_empty_not_i
 import { string_empty_is } from "../../../love/public/src/string_empty_is.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-export async function js_identifier_unique(existing, name) {
+export function js_identifier_unique(existing, name) {
   let v2 = js_keyword_arguments();
-  let used = list_concat(existing, [v2, await newFunction()]);
+  let v = js_keyword_default();
+  let used = list_concat(existing, [v2, v]);
   list_all(used, string_is_assert);
   let unique = null;
   let attempt = 1;
@@ -19,7 +21,4 @@ export async function js_identifier_unique(existing, name) {
   } while (string_empty_is(unique) || list_includes(used, unique));
   list_add(existing, unique);
   return unique;
-}
-function newFunction() {
-  return "default";
 }
