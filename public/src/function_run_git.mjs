0e1039396b27a2744ab741c2cbf9c032464d6720
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:23:07 -0400

    data_file_update ..\love\public\src\function_run_git.mjs

--- public/src/function_run_git.mjs
+++ public/src/function_run_git.mjs
@@ -1,6 +1,6 @@
 import { repos_paths_map_unordered } from "../../../love/public/src/repos_paths_map_unordered.mjs";
 import { git_acp_call_folder } from "../../../love/public/src/git_acp_call_folder.mjs";
-import { function_run_log } from "../../../love/public/src/function_run_log.mjs";
+import { function_run_log } from "../../../love/public/src/function_run_log.mjs";Error.stackTraceLimit = Infinity;
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
   await repos_paths_map_unordered(each_folder);
