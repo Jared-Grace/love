c8fb3e5905ffd820b5b1f60c92d6de134a37b3d7
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 14:06:16 -0400

    function_auto_path public\src\file_root_exists.mjs

--- public/src/file_root_exists.mjs
+++ public/src/file_root_exists.mjs
@@ -1,15 +1,14 @@
-import { string_is } from "../../../love/public/src/string_is.mjs";
+import { string_empty_not_is } from "../../../love/public/src/string_empty_not_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function file_root_exists(file_path) {
   let path = await import("path");
   let parsed = path.parse(file_path);
   let root = object_property_get(parsed, "root");
-  let si = string_is(root);
+  let en = string_empty_not_is(root);
   let exists = false;
-  if (si) {
+  if (en) {
     let fs = await import("fs");
     exists = await fs.promises.access(parsed.root);
   }
-  seni
   return exists;
 }
