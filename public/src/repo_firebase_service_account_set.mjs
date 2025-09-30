d61d06a29ca0e008e32dd842bc053995184ede44
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 18:35:44 -0400

    function_auto_path ..\love\public\src\repo_firebase_service_account_set.mjs

---
+++ public/src/repo_firebase_service_account_set.mjs
@@ -0,0 +1,7 @@
+import { repo_about_set } from "../../../love/public/src/repo_about_set.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function repo_firebase_service_account_set(repo, value) {
+  marker("1");
+  const key = "acronym";
+  await repo_about_set(repo, value, key);
+}
