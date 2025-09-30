3939e016843151bf4befeef7ae7b8ce8bae992d0
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:20:01 -0400

    function_auto_path ..\love\public\src\list_map.mjs

--- web/public/src/list_map.mjs
+++ web/public/src/list_map.mjs
@@ -1,6 +1,6 @@
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 export function list_map(list, lambda$item) {
+  list_is_assert(list);
   let mapped = list.map(lambda$item);
   return mapped;
-  list_is_assert(list2);
 }
