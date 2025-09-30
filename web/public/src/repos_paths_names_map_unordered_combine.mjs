827f2e4502af360a1863f0c35f4fe87782c2d04b
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:28:31 -0400

    data_file_update ..\love\public\src\repos_paths_names_map_unordered_combine.mjs

--- web/public/src/repos_paths_names_map_unordered_combine.mjs
+++ web/public/src/repos_paths_names_map_unordered_combine.mjs
@@ -6,7 +6,7 @@ import { path_join } from "../../../love/public/src/path_join.mjs";
 export async function repos_paths_names_map_unordered_combine(path, mapper) {
   marker("1");
   let result = await repos_paths_names_map_unordered(each_folder);
-  async function each_folder(name, r_path) {
+  async function each_folder(name, r_path) {string_is_assert
     let joined = path_join([r_path, path]);
     let f_names = mapper(joined);
     function lambda(f_path) {
