3cb5a7352acc4dac89a08e5cc2abfc099bd71d42
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:50:32 -0400

    function_auto_path public\src\function_run_output_file_temp.mjs

--- public/src/function_run_output_file_temp.mjs
+++ public/src/function_run_output_file_temp.mjs
@@ -1,4 +1,6 @@
+import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function function_run_output_file_temp() {
+export async function function_run_output_file_temp() {
   marker("1");
+  await file_temp(async function lambda(temp_path) {});
 }
