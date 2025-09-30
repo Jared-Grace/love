57fea5d2c9f93877f543fa586319a4c2ad64c2ee
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 19:46:07 -0400

    function_auto_path ..\love\public\src\firebase_storage_url_project.mjs

--- web/public/src/firebase_storage_url_project.mjs
+++ web/public/src/firebase_storage_url_project.mjs
@@ -1,7 +1,8 @@
+import { firebase_name } from "../../../karate_code/public/src/firebase_name.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function firebase_storage_url_project() {
   marker("1");
-  const prefix = "jared-grace";
+  const prefix = firebase_name();
   let url = prefix + ".firebasestorage.app";
   return url;
 }
