9f04caa4a98b97c3661f46c0b5dcfaa41b658cb3
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 18:48:03 -0400

    function_auto_path ..\love\public\src\function_import.mjs

--- web/public/src/function_import.mjs
+++ web/public/src/function_import.mjs
@@ -2,11 +2,13 @@ import { path_join } from "../../../love/public/src/path_join.mjs";
 import { fileURLToPath } from "url";
 import { function_name_to_base } from "../../../love/public/src/function_name_to_base.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
+import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 export async function function_import(f_name) {
   const path = await import("path");
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   let { unaliased } = await function_name_unalias(f_name);
+  let { f_path: f } = await function_name_to_path_search(unaliased);
   let joined = function_name_to_base(unaliased);
   const f_path = path_join([__dirname, joined]);
   const imported = await import(`file://${f_path}`);
