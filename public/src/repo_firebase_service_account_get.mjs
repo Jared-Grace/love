12bd1ca68e31db14888d9fd43858d7494bb92963
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 18:35:03 -0400

    c repo_acronym_get repo_firebase_service_account_get

--- public/src/repo_firebase_service_account_get.mjs
+++ public/src/repo_firebase_service_account_get.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { repo_about_get } from "../../../love/public/src/repo_about_get.mjs";
 export async function repo_firebase_service_account_get(repo) {
+  marker("1");
   const key = "acronym";
   let v = await repo_about_get(repo, key);
   return v;
