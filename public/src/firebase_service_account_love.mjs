7e2e9cfd00617f19e551fba4ceb0b4ec6e312fe7
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 18:40:35 -0400

    function_auto_path ..\love\public\src\firebase_service_account_love.mjs

--- public/src/firebase_service_account_love.mjs
+++ public/src/firebase_service_account_love.mjs
@@ -2,7 +2,7 @@ import { folder_find_starts_with } from "../../../love/public/src/folder_find_st
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
 export async function firebase_service_account_love() {
-  let prefix = "jared-grace-firebase-adminsdk-";
+  let prefix = "jared-grace" + "-firebase-adminsdk-";
   let v = folder_previous();
   let path_folder = path_join([v, "secret"]);
   let only = await folder_find_starts_with(path_folder, prefix);
