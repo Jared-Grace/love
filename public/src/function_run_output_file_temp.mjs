b096e992cadad240be4180b02d3068b80a99d2c5
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:52:24 -0400

    function_auto_path public\src\function_run_output_file_temp.mjs

--- public/src/function_run_output_file_temp.mjs
+++ public/src/function_run_output_file_temp.mjs
@@ -1,3 +1,4 @@
+import { file_open } from "../../../love/public/src/file_open.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
@@ -7,8 +8,9 @@ export async function function_run_output_file_temp(f_name) {
   marker("1");
   let result = await function_run(f_name, []);
   async function lambda(temp_path) {
-    let v = json_to(object);
-    await file_overwrite(temp_path, contents);
+    let json = json_to(result);
+    await file_overwrite(temp_path, json);
+    await file_open(f_path);
   }
   await file_temp(lambda);
 }
