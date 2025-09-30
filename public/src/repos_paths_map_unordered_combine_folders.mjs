6d1acfc757070957cf5da8ace6be10c7e334004f
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:07:12 -0400

    function_auto_path ..\love\public\src\repos_paths_map_unordered_combine_folders.mjs

---
+++ public/src/repos_paths_map_unordered_combine_folders.mjs
@@ -0,0 +1,11 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
+import { repos_paths_map_unordered } from "../../../love/public/src/repos_paths_map_unordered.mjs";
+export async function repos_paths_map_unordered_combine_folders(path, mapper) {
+  let result = await repos_paths_map_unordered(each_folder);
+  async function each_folder(folder) {
+    let joined = path_join([folder, path]);
+    let f_names = mapper(joined);
+    return f_names;
+  }
+  return result;
+}
