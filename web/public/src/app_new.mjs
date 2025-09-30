8ecc58e8d6a864f96751737b8f4b524b7ea7a3e1
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:13:20 -0400

    data_file_update public\src\app_new.mjs

--- web/public/src/app_new.mjs
+++ web/public/src/app_new.mjs
@@ -1,7 +1,7 @@
 import { app_new_step_1 } from "../../../love/public/src/app_new_step_1.mjs";
 import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_new(name) {
+export async function app_new(name) {$aa
   marker("1");
   await app_new_step_1(name);
   await app_new_step_2(name);
