e629b71b1ae7379365701431ea7a3dcce56d61ee
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:46:16 -0400

    function_auto_path public\src\app_new_step_2.mjs

--- web/public/src/app_new_step_2.mjs
+++ web/public/src/app_new_step_2.mjs
@@ -1,6 +1,8 @@
+import { string_and_empty_not_is_assert } from "../../../love/public/src/string_and_empty_not_is_assert.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 export async function app_new_step_2(name) {
+  string_and_empty_not_is_assert(name);
   let combined = app_name_main(name);
   await function_new(combined);
 }
