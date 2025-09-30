0f324432b7fb62865fa55c30146fba30bacaa2dc
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:48:27 -0400

    data_file_update ..\love\public\src\repos_paths_names_map_unordered.mjs

--- public/src/repos_paths_names_map_unordered.mjs
+++ public/src/repos_paths_names_map_unordered.mjs
@@ -5,9 +5,9 @@ import { repos_names } from "../../../love/public/src/repos_names.mjs";
 export async function repos_paths_names_map_unordered(lambda$name$path) {
   marker("1");
   let all = await repos_names();
-  async function lambda(name) {
-    let path = repo_path(name);
-    let mapped = await lambda$name$path(name, path);
+  async function lambda(repo_name) {
+    let path = repo_path(repo_name);
+    let mapped = await lambda$name$path(repo_name, path);
     return mapped;
   }
   let result = await list_map_unordered_async(all, lambda);
