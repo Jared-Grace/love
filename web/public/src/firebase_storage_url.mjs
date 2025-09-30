989aad8df95dbe2026e7ffd900e8191fdbebbcbf
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 19:46:31 -0400

    function_auto_path ..\love\public\src\firebase_admin_get.mjs

--- web/public/src/firebase_storage_url.mjs
+++ web/public/src/firebase_storage_url.mjs
@@ -1,10 +1,10 @@
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
 import { firebase_storage_url_project } from "../../../love/public/src/firebase_storage_url_project.mjs";
-export function firebase_storage_url(storage_path) {
+export async function firebase_storage_url(storage_path) {
   let replaced = string_replace(storage_path, "/", "%2F");
   let url =
     "https://firebasestorage.googleapis.com/v0/b/" +
-    firebase_storage_url_project() +
+    (await firebase_storage_url_project()) +
     "/o/" +
     replaced +
     "?alt=media";
