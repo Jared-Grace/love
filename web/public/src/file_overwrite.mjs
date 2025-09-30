2a0042d4a80b9819b3eb607787a6ae98f0c68ea9
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 14:02:33 -0400

    function_auto_path public\src\file_overwrite.mjs

--- web/public/src/file_overwrite.mjs
+++ web/public/src/file_overwrite.mjs
@@ -7,9 +7,10 @@ export async function file_overwrite(file_path, contents) {
   return;
   let path = await import("path");
   let parsed = path.parse(file_path);
+  let root = object_property_get(parsed, "root");
+  si;
   if (false) {
   }
-  let root = object_property_get(parsed, "root");
   if (false) {
     await fs.access(parsed.root);
   }
