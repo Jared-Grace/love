83e2bd862bf3c5145ae2b16ee47128c371a9bc53
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:17:49 -0400

    function_auto_path public\src\app_new_step_2_next.mjs

---
+++ public/src/app_new_step_2_next.mjs
@@ -0,0 +1,28 @@
+import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
+import { html_new } from "../../../love/public/src/html_new.mjs";
+import { function_new } from "../../../love/public/src/function_new.mjs";
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { js_declaration_single_block_body } from "../../../love/public/src/js_declaration_single_block_body.mjs";
+import { firebase_storage_function_run } from "../../../love/public/src/firebase_storage_function_run.mjs";
+import { js_call_new } from "../../../love/public/src/js_call_new.mjs";
+import { app_new_assign } from "../../../love/public/src/app_new_assign.mjs";
+import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
+import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
+export async function app_new_step_2_next(name) {
+  let a_name = app_name_prefixed(name);
+  let combined = app_name_main(name);
+  async function lambda(ast) {
+    let declaration = js_declaration_single(ast);
+    declaration.async = true;
+    let assign = app_new_assign(combined);
+    let { parsed } = await js_call_new(firebase_storage_function_run.name, ast);
+    let p = parsed;
+    let body_block = js_declaration_single_block_body(ast);
+    list_add(body_block, assign);
+    list_add(body_block, p);
+  }
+  let output = await function_transform(a_name, lambda);
+  await function_new(combined);
+  await html_new(name);
+}
