c154fbcfb5664cfea6ff948667fe47ffed9e8154
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 14:00:15 -0400

    function_auto_path public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -2,10 +2,10 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_overwrite(file_path, contents) {
   let fs = await import("fs");
-  let path = await import("path");
   await file_parent_exists_ensure(file_path);
   await fs.promises.writeFile(file_path, contents, "utf-8");
   return;
+  let path = await import("path");
   let parsed = path.parse(file_path);
   let root = object_property_get(parsed, "root");
 }
