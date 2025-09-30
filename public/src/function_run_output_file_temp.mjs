e475d5b3cc21e4533d6a051518ea45adf757e6e3
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:51:38 -0400

    function_auto_path public\src\function_run_output_file_temp.mjs

--- public/src/function_run_output_file_temp.mjs
+++ public/src/function_run_output_file_temp.mjs
@@ -1,9 +1,10 @@
+import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_run_output_file_temp() {
   marker("1");
   async function lambda(temp_path) {
-    fo;
+    await file_overwrite(file_path, contents);
   }
   await file_temp(lambda);
 }
