17d8c3179e5aa038960bffd6806f23972e1710ed
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:10:59 -0400

    data_file_update public\src\app_new.mjs

--- web/public/src/app_new.mjs
+++ web/public/src/app_new.mjs
@@ -4,7 +4,11 @@ import { function_new } from "../../../love/public/src/function_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_new(name) {
   marker("1");
-  let a_name = app_name_prefixed(name);
-  await function_new(a_name);
+  await app_new_step_1(name);
   await app_new_step_2(name);
 }
+async function app_new_step_1(name) {
+  let a_name = app_name_prefixed(name);
+  await function_new(a_name);
+}
+
