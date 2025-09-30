d7ef6071bdbcb651bf4491f0ec95bfd00d422583
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 19:51:20 -0400

    function_auto_path ..\love\public\src\js_call_new.mjs

--- web/public/src/js_expand_generic.mjs
+++ web/public/src/js_expand_generic.mjs
@@ -42,8 +42,8 @@ export async function js_expand_generic(next, stack2, index, ast) {
     let identifiers_all = list_concat(identifiers, identifiers_call);
     let f_names = await functions_names();
     list_remove_all_multiple(f_names, identifiers_all);
-    function lambda2(i) {
-      let unique = js_identifier_unique(identifiers_all, i);
+    async function lambda2(i) {
+      let unique = await js_identifier_unique(identifiers_all, i);
       js_identifier_replace(ast_call, i, unique);
     }
     each(identifiers, lambda2);
