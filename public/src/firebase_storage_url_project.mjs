5ca526202145fc90ab9be6df25c95ff03f149df5
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 19:45:28 -0400

    function_auto_path ..\love\public\src\firebase_storage_url_project.mjs

--- public/src/firebase_storage_url_project.mjs
+++ public/src/firebase_storage_url_project.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function firebase_storage_url_project() {
   marker("1");
-  let url = "jared-grace.firebasestorage.app";
+  let url = "jared-grace" + ".firebasestorage.app";
   return url;
 }
