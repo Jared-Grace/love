4776a4699b9f9ee92ded48bb8cc0e1203859dd98
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:10:42 -0400

    function_auto_path ..\love\public\src\repos_paths_map_unordered_combine_folders.mjs

--- public/src/repos_paths_map_unordered_combine_folders.mjs
+++ public/src/repos_paths_map_unordered_combine_folders.mjs
@@ -1,11 +1,11 @@
+import { repos_paths_names_map_unordered } from "../../../love/public/src/repos_paths_names_map_unordered.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { repos_paths_map_unordered } from "../../../love/public/src/repos_paths_map_unordered.mjs";
 export async function repos_paths_map_unordered_combine_folders(path, mapper) {
   marker("1");
   mapper = identity;
-  let result = await repos_paths_map_unordered(each_folder);
+  let result = await repos_paths_names_map_unordered(each_folder);
   async function each_folder(folder) {
     let joined = path_join([folder, path]);
     let f_names = mapper(joined);
