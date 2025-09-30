530911e3d99c2f845481545e269da2cdd7bbc63e
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 18:32:52 -0400

    function_auto_path ..\love\public\src\repo_acronym_get.mjs

--- public/src/repo_acronym_get.mjs
+++ public/src/repo_acronym_get.mjs
@@ -1,11 +1,8 @@
-import { path_repo_about } from "../../../love/public/src/path_repo_about.mjs";
-import { data_get } from "../../../love/public/src/data_get.mjs";
+import { repo_about_get } from "../../../love/public/src/repo_about_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function repo_acronym_get(repo) {
   marker("1");
   const key = "acronym";
-  let a_path = path_repo_about(repo);
-  let { value } = await data_get(key, null, a_path);
-  let v = value;
+  let v = await repo_about_get(repo, key);
   return v;
 }
