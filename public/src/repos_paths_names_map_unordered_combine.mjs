f5f270c5ffc26e1cbee10fa01a3092e55402cae2
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:25:44 -0400

    data_file_update ..\love\public\src\repos_paths_names_map_unordered_combine.mjs

--- public/src/repos_paths_names_map_unordered_combine.mjs
+++ public/src/repos_paths_names_map_unordered_combine.mjs
@@ -5,7 +5,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 export async function repos_paths_names_map_unordered_combine(path, mapper) {
   marker("1");
-  mapper = identity;
   let result = await repos_paths_names_map_unordered(each_folder);
   async function each_folder(name, r_path) {
     let joined = path_join([r_path, path]);
