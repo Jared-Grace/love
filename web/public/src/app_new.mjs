5813c358277da6202ced832781ca79cec62c8429
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:13:44 -0400

    function_auto_path public\src\app_new.mjs

--- web/public/src/app_new.mjs
+++ web/public/src/app_new.mjs
@@ -1,7 +1,9 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { app_new_step_1 } from "../../../love/public/src/app_new_step_1.mjs";
 import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_new(name) {$aa
+export async function app_new(name) {
+  assert_arguments(arguments, 1);
   marker("1");
   await app_new_step_1(name);
   await app_new_step_2(name);
