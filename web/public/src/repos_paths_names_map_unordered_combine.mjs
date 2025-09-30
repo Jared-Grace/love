f649dfd12acc0ab540bb6a0069701ec204c4808d
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:14:27 -0400

    function_auto_path ..\love\public\src\repos_paths_names_map_unordered_combine.mjs

---
+++ web/public/src/repos_paths_names_map_unordered_combine.mjs
@@ -0,0 +1,24 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { repos_paths_names_map_unordered } from "../../../love/public/src/repos_paths_names_map_unordered.mjs";
+import { identity } from "../../../love/public/src/identity.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+import { path_join } from "../../../love/public/src/path_join.mjs";
+export async function repos_paths_names_map_unordered_combine(path, mapper) {
+  marker("1");
+  mapper = identity;
+  let result = await repos_paths_names_map_unordered(each_folder);
+  async function each_folder(name, r_path) {
+    let joined = path_join([r_path, path]);
+    let f_names = mapper(joined);
+    function lambda(f_path) {
+      let v = {
+        name,
+        f_path,
+      };
+      return v;
+    }
+    let mapped = list_map(f_names, lambda);
+    return mapped;
+  }
+  return result;
+}
