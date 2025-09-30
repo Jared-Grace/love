4a0ee8e264e3954e93b8b43f7dc25e9bdce08a63
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:50:27 -0400

    function_auto_path public\src\function_run_output_file_temp.mjs

--- public/src/function_run_output_file_temp.mjs
+++ public/src/function_run_output_file_temp.mjs
@@ -1,6 +1,4 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function function_run_output_file_temp() {
   marker("1");
-  log(message);
 }
