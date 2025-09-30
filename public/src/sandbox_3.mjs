d97e800da68742de65b44b633d3df186ff42db4e
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 14:30:34 -0400

    function_auto_path public\src\sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function sandbox_3() {
-  marker("1");
+import { repos_paths_map_unordered_combine_squash } from "../../../love/public/src/repos_paths_map_unordered_combine_squash.mjs";
+export async function sandbox_3() {
+  let squashed = await repos_paths_map_unordered_combine_squash(mapper);
 }
