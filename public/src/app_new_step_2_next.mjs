5a77227f855b6840e1ab078133dd67e30ef083bc
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:45:10 -0400

    function_auto_path public\src\app_new_step_2_next.mjs

--- public/src/app_new_step_2_next.mjs
+++ public/src/app_new_step_2_next.mjs
@@ -1,12 +1,10 @@
+import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
 import { app_new_step_3 } from "../../../love/public/src/app_new_step_3.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_new } from "../../../love/public/src/html_new.mjs";
-import { function_new } from "../../../love/public/src/function_new.mjs";
-import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 export async function app_new_step_2_next(name) {
   marker("1");
-  let combined = app_name_main(name);
-  await function_new(combined);
+  await app_new_step_2(name);
   await app_new_step_3(name);
   await html_new(name);
 }
