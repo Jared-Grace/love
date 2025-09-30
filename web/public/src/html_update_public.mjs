925473435711f480b82160a42a937fa176c74cbf
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 16:50:08 -0400

    function_auto_path ..\love\public\src\html_update_public.mjs

--- web/public/src/html_update_public.mjs
+++ web/public/src/html_update_public.mjs
@@ -1,3 +1,4 @@
+import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 import { folder_src } from "../../../love/public/src/folder_src.mjs";
 import { html_update_generic } from "../../../love/public/src/html_update_generic.mjs";
@@ -5,6 +6,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
 export async function html_update_public(name) {
   let a_name = app_name_prefixed(name);
+  let result = await function_name_to_path_search(unaliased);
   marker("1");
   let file_path = html_name_to_path(name);
   function paths_get(f_name_ext) {
