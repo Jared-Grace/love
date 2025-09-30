2ed0d8ee93a4c2480ae850fc43c5b5a631b5d46c
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 13:14:09 -0400

    function_auto_path public\src\app_new.mjs

--- web/public/src/app_new.mjs
+++ web/public/src/app_new.mjs
@@ -1,9 +1,11 @@
+import { string_empty_not_is_assert } from "../../../love/public/src/string_empty_not_is_assert.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { app_new_step_1 } from "../../../love/public/src/app_new_step_1.mjs";
 import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_new(name) {
   assert_arguments(arguments, 1);
+  string_empty_not_is_assert(task_name);
   marker("1");
   await app_new_step_1(name);
   await app_new_step_2(name);
