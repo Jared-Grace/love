189760bf311e584f359b7b95f9792a0b2ad51024
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 18:40:55 -0400

    function_auto_path ..\love\public\src\html_name_to_path_dev.mjs

--- public/src/html_name_to_path_dev.mjs
+++ public/src/html_name_to_path_dev.mjs
@@ -4,8 +4,9 @@ import { folder_public_combine } from "../../../love/public/src/folder_public_co
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 export function html_name_to_path_dev(name) {
   marker("1");
+  const folder = "dev";
   let file_name = file_name_html(name);
-  let joined = path_join(["dev", file_name]);
+  let joined = path_join([folder, file_name]);
   let file_path = folder_public_combine(joined);
   return file_path;
 }
