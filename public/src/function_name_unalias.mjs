0be897d7c1546382f362ef6dfe351c869b166302
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 18:03:29 -0400

    o u

--- public/src/function_name_unalias.mjs
+++ public/src/function_name_unalias.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_acronym_to_name } from "../../../love/public/src/function_acronym_to_name.mjs";
 import { function_alias_add_generic } from "../../../love/public/src/function_alias_add_generic.mjs";
 export async function function_name_unalias(f_name) {
+  marker("1");
   var { exists, unaliased: unaliased_actual } =
     await function_alias_add_generic(f_name);
   let { expanded, expandeds } = await function_acronym_to_name(f_name);
