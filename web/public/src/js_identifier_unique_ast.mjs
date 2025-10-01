1e97650742f65262cf70785696d99ec867a34bd4
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 20:00:10 -0400

    function_auto_path ..\love\public\src\js_identifier_unique_ast.mjs

--- web/public/src/js_identifier_unique_ast.mjs
+++ web/public/src/js_identifier_unique_ast.mjs
@@ -1,7 +1,7 @@
 import { js_identifier_unique } from "../../../love/public/src/js_identifier_unique.mjs";
 import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
-export async function js_identifier_unique_ast(ast, property_name) {
+export function js_identifier_unique_ast(ast, property_name) {
   let existing = js_identifiers_names(ast);
-  let unique = await js_identifier_unique(existing, property_name);
+  let unique = js_identifier_unique(existing, property_name);
   return unique;
 }
