e20dc9f971ea98d31c533dda542600fd2b51e4bb
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 08:42:35 -0400

    function_auto_path ..\love\public\src\repos_paths_map_unordered_combine.mjs

--- scripts/r.mjs
+++ scripts/r.mjs
@@ -1,4 +1,5 @@
 import { function_run } from '../public/src/function_run.mjs';
+Error.stackTraceLimit = Infinity;
 (async () => {
   const [, , funcName, ...args] = process.argv;
   await function_run(funcName, args)
