f034ab26dffcd59f2dc00e18a91ec490f6adda67
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 06:49:38 -0400

    function_auto_path ..\love\public\src\firebase_name.mjs

--- public/src/firebase_name.mjs
+++ public/src/firebase_name.mjs
@@ -1,6 +1,6 @@
+import { firebase_name_repo } from "../../../love/public/src/firebase_name_repo.mjs";
 import { global_function_get } from "./global_function_get.mjs";
 import { browser_is } from "./browser_is.mjs";
-import { firebase_name_repo } from "../../../karate_code/public/src/firebase_name_repo.mjs";
 import { user_repo_get } from "./user_repo_get.mjs";
 export async function firebase_name() {
   if (browser_is()) {
