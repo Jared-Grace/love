685fc9d44c8dfc2592355dee79a9c5070bce63f4
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 06:34:10 -0400

    function_auto_path ..\love\public\src\function_new_app_screen.mjs

--- web/public/src/function_new_app_screen.mjs
+++ web/public/src/function_new_app_screen.mjs
@@ -1,3 +1,4 @@
+import { marker_screen_add } from "../../../love/public/src/marker_screen_add.mjs";
 import { data_app_current_get } from "../../../love/public/src/data_app_current_get.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
@@ -7,5 +8,6 @@ export async function function_new_app_screen(screen_name) {
   let combined = function_name_combine(a_name, screen_name);
   marker("1");
   let v = await function_new(combined);
+  let v2 = await marker_screen_add(screen_name2);
   return v;
 }
