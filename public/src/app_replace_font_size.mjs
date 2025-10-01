a61a85717a9751a1b7e071a7d2e1984e7ec64686
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 06:09:53 -0400

    function_auto_path ..\love\public\src\app_replace_font_size.mjs

--- public/src/app_replace_font_size.mjs
+++ public/src/app_replace_font_size.mjs
@@ -1,5 +1,5 @@
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
-export async function app_replace_font_size(context) {
+export function app_replace_font_size(context) {
   let value2 = storage_local_initialize_context(context, "font_size", 20);
   return value2;
 }
