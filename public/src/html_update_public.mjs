e2055142dcd3aa92da8273618b6f8e6fe48f822b
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 23:22:08 -0400

    function_auto_path ..\love\public\src\html_update_public.mjs

--- public/src/html_update_public.mjs
+++ public/src/html_update_public.mjs
@@ -1,8 +1,8 @@
+import { repo_path_combine } from "../../../love/public/src/repo_path_combine.mjs";
 import { js_code_braces_empty } from "../../../love/public/src/js_code_braces_empty.mjs";
 import { js_code_let_assign } from "../../../love/public/src/js_code_let_assign.mjs";
 import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { repo_path } from "../../../love/public/src/repo_path.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_open } from "../../../love/public/src/file_open.mjs";
 import { html_overwrite } from "../../../love/public/src/html_overwrite.mjs";
@@ -15,7 +15,7 @@ export async function html_update_public(name) {
   let a_name = app_name_prefixed(name);
   let search = await function_name_to_path_search(a_name);
   let repo_name = object_property_get(search, "repo_name");
-  let r_path = repo_path(repo_name);
+  let r_path = repo_path_combine(repo_name);
   let file_path = html_name_to_path(name);
   let joined = path_join([r_path, file_path]);
   let code = await function_dependencies_code(a_name);
