afcbd5201afb30ec1e251982504e03c5c234bf2c
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 08:42:35 -0400

    function_auto_path ..\love\public\src\repos_paths_map_unordered_combine.mjs

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,12 +1,13 @@
 import { repos_paths_map_unordered_combine_squash } from "../../../love/public/src/repos_paths_map_unordered_combine_squash.mjs";
 import { functions_names_from_path } from "../../../love/public/src/functions_names_from_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { log } from "./log.mjs";
 export async function functions_names() {
   marker("1");
   let squashed = await repos_paths_map_unordered_combine_squash(mapper);
   return squashed;
-  async function mapper(joined) {
-    let f_names = await functions_names_from_path(joined);
+  async function mapper(joined) {log({joined})
+    let f_names = await functions_names_from_path(joined);log({f_names})
     return f_names;
   }
 }
