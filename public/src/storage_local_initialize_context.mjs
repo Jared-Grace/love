c2842c38b9b12e4adbc5138155336245fe7fdaab
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 20:34:26 -0400

    function_auto_path ..\love\public\src\storage_local_initialize_context.mjs

--- public/src/storage_local_initialize_context.mjs
+++ public/src/storage_local_initialize_context.mjs
@@ -1,8 +1,12 @@
 import { storage_local_initialize } from "../../../love/public/src/storage_local_initialize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_initialize_context(context, key, value_initial) {
+export async function storage_local_initialize_context(
+  context,
+  key,
+  value_initial,
+) {
   marker("1");
   let { app_fn } = context;
-  let value = storage_local_initialize(app_fn, key, value_initial);
+  let value = await storage_local_initialize(app_fn, key, value_initial);
   return value;
 }
