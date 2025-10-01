824486c97b8a1d20f9ced23800e9921dfea689ca
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 08:25:07 -0400

    function_auto_path ..\love\public\src\function_wrap.mjs

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -1,5 +1,5 @@
+import { js_declaration_single_block_body_add } from "../../../karate_code/public/src/js_declaration_single_block_body_add.mjs";
 import { js_statement_return } from "../../../love/public/src/js_statement_return.mjs";
-import { js_declaration_single_block_body } from "../../../love/public/src/js_declaration_single_block_body.mjs";
 import { object_property_from } from "../../../love/public/src/object_property_from.mjs";
 import { js_declaration_asyncify } from "../../../love/public/src/js_declaration_asyncify.mjs";
 import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
@@ -7,7 +7,6 @@ import { js_declaration_params_names } from "../../../love/public/src/js_declara
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 export async function function_wrap(f_name, f_name_wrapped) {
   let { declaration: declaration_call, unaliased } =
@@ -22,8 +21,7 @@ export async function function_wrap(f_name, f_name_wrapped) {
       declaration_call,
     );
     let item = js_statement_return(code);
-    let body_block = js_declaration_single_block_body(ast);
-    list_add(body_block, item);
+    js_declaration_single_block_body_add(ast, item);
     let declaration = js_declaration_single(ast);
     js_declaration_asyncify(declaration, declaration_call);
     object_property_from(declaration, "params", declaration_call);
