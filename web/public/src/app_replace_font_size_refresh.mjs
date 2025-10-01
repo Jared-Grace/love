897c972992b7d7fa679f53b451087148d38dff2e
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 20:34:23 -0400

    data_file_update ..\love\public\src\app_refresh.mjs

--- web/public/src/app_replace_font_size_refresh.mjs
+++ web/public/src/app_replace_font_size_refresh.mjs
@@ -1,8 +1,8 @@
 import { app_replace_font_size } from "../../../love/public/src/app_replace_font_size.mjs";
 import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
 import { html_document_root } from "../../../love/public/src/html_document_root.mjs";
-export function app_replace_font_size_refresh(context) {
+export async function app_replace_font_size_refresh(context) {
   let html = html_document_root();
-  let value = app_replace_font_size(context);
+  let value = await app_replace_font_size(context);
   html_style_font_size(html, value + "px");
 }
