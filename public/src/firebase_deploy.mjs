16e5f862036907ae8004a204661843a727e0a425
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 16:56:51 -0400

    function_auto_path ..\love\public\src\firebase_deploy.mjs

--- public/src/firebase_deploy.mjs
+++ public/src/firebase_deploy.mjs
@@ -1,11 +1,11 @@
 import { user_repo_path_combine } from "../../../love/public/src/user_repo_path_combine.mjs";
 import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_deploy() {
   marker("1");
   let combined = await user_repo_path_combine(".");
-  let v = await command_line_generic("dir", {});
-  let stdout = await command_line("firebase deploy");
+  let stdout = await command_line_generic("dir", {
+    cwd: combined,
+  });
   return stdout;
 }
