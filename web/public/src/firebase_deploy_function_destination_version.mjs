4447f4b34ef68cdd7288ef2975d36060ab0d00d4
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 17:59:49 -0400

    function_auto_path ..\love\public\src\firebase_deploy_function_destination_version.mjs

--- web/public/src/firebase_deploy_function_destination_version.mjs
+++ web/public/src/firebase_deploy_function_destination_version.mjs
@@ -1,5 +1,8 @@
 import { firebase_deploy_function_destination } from "../../../love/public/src/firebase_deploy_function_destination.mjs";
 export function firebase_deploy_function_destination_version(f_name) {
-  let destination2 = firebase_deploy_function_destination("version", f_name);
+  let destination2 = firebase_deploy_function_destination(
+    "version_latest",
+    f_name,
+  );
   return destination2;
 }
