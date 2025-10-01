0ae46dd403dc91dc44f60d3b3ceef2a7efa9104f
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 06:52:00 -0400

    function_auto_path ..\love\public\src\function_import.mjs

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,9 +1,9 @@
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { fileURLToPath } from "url";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { folder_previous } from "./folder_previous.mjs";
 export async function function_import(f_name) {
+  const { fileURLToPath } = await import("url");
   const path = await import("path");
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
