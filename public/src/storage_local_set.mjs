ff65997ff426015b7c97993911b3005a99bb4aee
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 20:34:21 -0400

    function_auto_path ..\love\public\src\app_replace_main.mjs

--- public/src/storage_local_set.mjs
+++ public/src/storage_local_set.mjs
@@ -2,6 +2,10 @@ import { json_to } from "../../../love/public/src/json_to.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
 export function storage_local_set(app_fn, key, value) {
   let storage_local_key = storage_local_key_get(app_fn, key);
+  newFunction(value, storage_local_key);
+}
+
+function newFunction(value, storage_local_key) {
   let v = json_to({
     value,
   });
