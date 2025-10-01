98ffefb8f9140197df6b71107a5b9ed6cb088885
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 16:55:12 -0400

    function_auto_path ..\love\public\src\firebase_deploy.mjs

--- public/src/firebase_deploy.mjs
+++ public/src/firebase_deploy.mjs
@@ -1,8 +1,10 @@
+import { user_repo_get } from "../../../love/public/src/user_repo_get.mjs";
 import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_deploy() {
   marker("1");
+  let repo_name = await user_repo_get();
   let v = await command_line_generic("dir", {});
   let stdout = await command_line("firebase deploy");
   return stdout;
