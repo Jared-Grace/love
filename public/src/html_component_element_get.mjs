97751a6316f232af40645c5cfeb1be0467458ea2
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 15:50:31 -0400

    function_auto_path ..\love\public\src\html_component_element_get.mjs

--- public/src/html_component_element_get.mjs
+++ public/src/html_component_element_get.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function html_component_element_get(component) {
-  let value = object_property_get(component, "element");
-  return value;
+  let element = object_property_get(component, "element");
+  return element;
 }
