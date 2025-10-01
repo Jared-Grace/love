58866fa632807d0dfa68f502552a7109f95ddfb9
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 18:00:05 -0400

    function_auto_path ..\karate_code\public\src\firebase_deploy_function_destination_latest.mjs

--- web/public/src/firebase_deploy_function_destination_version.mjs
+++
@@ -1,8 +0,0 @@
-import { firebase_deploy_function_destination } from "../../../love/public/src/firebase_deploy_function_destination.mjs";
-export function firebase_deploy_function_destination_version(f_name) {
-  let destination2 = firebase_deploy_function_destination(
-    "version_latest",
-    f_name,
-  );
-  return destination2;
-}
