2829d8abf6a61658aad374611c86bd6881fc2833
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 15:31:24 -0400

    function_auto_path ..\love\public\src\string_includes.mjs

--- public/src/string_includes.mjs
+++ public/src/string_includes.mjs
@@ -1,11 +1,11 @@
 import { string_is_assert_json } from "../../../love/public/src/string_is_assert_json.mjs";
-export function string_includes(string, item) {
+export function string_includes(string, part) {
   const o = {
     string,
-    item,
+    part,
   };
   string_is_assert_json(string, o);
-  string_is_assert_json(item, o);
-  let v = string.includes(item);
+  string_is_assert_json(part, o);
+  let v = string.includes(part);
   return v;
 }
