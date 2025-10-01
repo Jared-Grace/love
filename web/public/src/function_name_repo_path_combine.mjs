dcd5b763b30e4b56dc3754b67e35d0adeb35922c
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 06:16:00 -0400

    fld C:/Users/chris/repos/love/.git/index.lock

--- web/public/src/function_name_repo_path_combine.mjs
+++ web/public/src/function_name_repo_path_combine.mjs
@@ -1,6 +1,6 @@
-import { repo_path_combine } from "../../../love/public/src/repo_path_combine.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
+import { repo_path_combine } from "./repo_path_combine.mjs";
+import { object_property_get } from "./object_property_get.mjs";
+import { function_name_to_path_search } from "./function_name_to_path_search.mjs";
 export async function function_name_repo_path_combine(a_name, file_path) {
   let search = await function_name_to_path_search(a_name);
   let repo_name = object_property_get(search, "repo_name");
