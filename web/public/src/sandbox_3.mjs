b9011478d2b26e0e969279fb1a73e0218d02319c
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 16:54:30 -0400

    function_auto_path ..\love\public\src\sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -2,7 +2,8 @@ import { command_line_generic } from "../../../love/public/src/command_line_gene
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox_3() {
   marker("1");
-  return await command_line_generic("dir", {
+  let v = await command_line_generic("dir", {
     cwd: "C:/Users/chris/repos/karate_code",
   });
+  return v;
 }
