4c9d065ebbace42d76c1f8e199ffce45dd1b70f0
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 18:37:11 -0400

    r app_new_step_4 app_new_step_5

--- web/public/src/app_new_step_2_next.mjs
+++ web/public/src/app_new_step_2_next.mjs
@@ -1,6 +1,6 @@
 import { function_new } from "../../../love/public/src/function_new.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
-import { app_new_step_4 } from "../../../karate_code/public/src/app_new_step_4.mjs";
+import { app_new_step_5 } from "../../../karate_code/public/src/app_new_step_5.mjs";
 import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
 import { app_new_step_3 } from "../../../love/public/src/app_new_step_3.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -11,6 +11,6 @@ export async function app_new_step_2_next(name) {
   await app_new_step_3(name);
   let combined = app_name_main(name);
   await function_new(combined);
-  await app_new_step_4(name);
+  await app_new_step_5(name);
   await html_new(name);
 }
