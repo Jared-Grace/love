44aa7a50680b3932ac454f287d3f7e6b7b0fd1a9
Grace-Jared <christrosetolife@gmail.com>
2025-10-01 17:31:52 -0400

    function_auto_path ..\love\public\src\firebase_storage_function_run.mjs

--- public/src/firebase_storage_function_run.mjs
+++ public/src/firebase_storage_function_run.mjs
@@ -1,3 +1,4 @@
+import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
@@ -19,7 +20,8 @@ export async function firebase_storage_function_run(f_name) {
   if (browser_is()) {
     let body = html_document_body();
     let component = html_element(body, "script");
-    html_attribute_set(component2, key, value);
+    html_attribute_set(component, "type", "module");
+    html_text_set(component2, text);
     return;
   }
   eval(joined);
