4d0e7f1729bd69af6330c4844e316770bf30e635
Grace-Jared <christrosetolife@gmail.com>
2025-09-30 20:34:26 -0400

    function_auto_path ..\love\public\src\storage_local_initialize.mjs

--- public/src/storage_local_set_context.mjs
+++ public/src/storage_local_set_context.mjs
@@ -1,5 +1,5 @@
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-export function storage_local_set_context(context, key, value) {
+export async function storage_local_set_context(context, key, value) {
   let { app_fn } = context;
-  storage_local_set(app_fn, key, value);
+  await storage_local_set(app_fn, key, value);
 }
