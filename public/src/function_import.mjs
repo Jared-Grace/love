a46bf718efcfc6d9007b10494ef1fb0e9b9d0ed4
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 19:26:39 -0400

    function_auto_path ..\love\public\src\function_import.mjs

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -2,23 +2,15 @@ import { path_join } from "../../../love/public/src/path_join.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { folder_previous } from "./folder_previous.mjs";
-import { log } from "./log.mjs";
 export async function function_import(f_name) {
   const { fileURLToPath } = await import("url");
   const path = await import("path");
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   let { unaliased } = await function_name_unalias(f_name);
-  let s = await function_name_to_path_search(unaliased);
-  let { f_path: f } = s;
+  let { f_path: f } = await function_name_to_path_search(unaliased);
   let previous = folder_previous();
   const f_path = path_join([__dirname, previous, previous, f]);
-  log({
-    s,
-    f_path,
-    f,
-    unaliased,
-  });
   const imported = await import(`file://${f_path}`);
   const imported_fn = imported[unaliased];
   if (typeof imported_fn !== "function") {
