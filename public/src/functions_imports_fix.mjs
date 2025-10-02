ec344534504ce2af11fc239f5a2f5ebdd68bea70
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 12:36:08 -0400

    function_auto_path ..\love\public\src\functions_imports_fix.mjs

--- public/src/functions_imports_fix.mjs
+++ public/src/functions_imports_fix.mjs
@@ -5,12 +5,12 @@ import { functions_names_to_paths } from "../../../love/public/src/functions_nam
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_imports_fix() {
   marker("1");
+  const list = await functions_names();
   let dictionary = await functions_names_to_paths();
   let lambda = function lambda2(ast) {
     let v2 = js_imports_paths_fix_inner(ast, dictionary);
     return v2;
   };
-  const list = await functions_names();
   let v = await functions_transform_list(lambda, list);
   return v;
 }
