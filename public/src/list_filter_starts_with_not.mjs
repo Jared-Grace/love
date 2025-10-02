aa90a6c3be2e3c789513b4837accd4b237846d76
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 00:06:59 -0400

    function_auto_path ..\love\public\src\list_filter_starts_with_not.mjs

--- public/src/list_filter_starts_with_not.mjs
+++ public/src/list_filter_starts_with_not.mjs
@@ -1,10 +1,10 @@
 import { string_starts_with_not } from "../../../love/public/src/string_starts_with_not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-export function list_filter_starts_with_not(mapped, book_code) {
+export function list_filter_starts_with_not(mapped, prefix) {
   marker("1");
   function lambda(item) {
-    let sw = string_starts_with_not(item, book_code);
+    let sw = string_starts_with_not(item, prefix);
     return sw;
   }
   let filtered = list_filter(mapped, lambda);
