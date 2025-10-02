9452ac21e674660828bebcf228cff7abd48e0e32
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 19:10:04 -0400

    function_auto_path ..\love\public\src\js_arrow_blockify.mjs

--- public/src/js_arrow_blockify.mjs
+++ public/src/js_arrow_blockify.mjs
@@ -1,7 +1,12 @@
 import { js_if_blockify_generic } from "../../../love/public/src/js_if_blockify_generic.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
+import { js_statement_return_add } from "../../../love/public/src/js_statement_return_add.mjs";
 export async function js_arrow_blockify(ast) {
   const type = "ArrowFunctionExpression";
   const property_name = "body";
-  await js_if_blockify_generic(ast, type, property_name, js_statement_return_add);
+  await js_if_blockify_generic(
+    ast,
+    type,
+    property_name,
+    js_statement_return_add,
+  );
 }
