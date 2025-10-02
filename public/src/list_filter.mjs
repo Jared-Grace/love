ae38596b13d7dc9087c0b3772ff92d72923a4fb0
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 19:16:36 -0400

    function_auto_path ..\love\public\src\list_filter.mjs

--- public/src/list_filter.mjs
+++ public/src/list_filter.mjs
@@ -4,8 +4,8 @@ export function list_filter(list, lambda$item) {
   let l = list_is(list);
   assert(l);
   function lambda(item) {
-    let v = lambda$item(item);
-    return v;
+    let match = lambda$item(item);
+    return match;
   }
   let filtered = list.filter(lambda);
   return filtered;
