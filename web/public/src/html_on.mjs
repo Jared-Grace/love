a6b7ab521e8c38855d9a8f349800cd4841a08c8e
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 15:37:18 -0400

    function_auto_path ..\love\public\src\html_on.mjs

--- web/public/src/html_on.mjs
+++ web/public/src/html_on.mjs
@@ -2,8 +2,8 @@ import { function_is } from "../../../karate_code/public/src/function_is.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_on(component, name_event, lambda) {
-  let fi = function_is(f);
-  assert(b);
+  let fi = function_is(lambda);
+  assert(fi);
   let element = html_component_element_get(component);
   element.addEventListener(name_event, lambda);
 }
