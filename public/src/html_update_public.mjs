ac402ee622340727ecad9c4509f52f759e1a4245
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 12:41:00 -0400

    data_file_update ..\love\public\src\repo_functions_move_if_includes_not.mjs

--- public/src/html_update_public.mjs
+++ public/src/html_update_public.mjs
@@ -1,4 +1,4 @@
-import { html_update_public_generic } from "../../../karate_code/public/src/html_update_public_generic.mjs";
+import { html_update_public_generic } from "../../../love/public/src/html_update_public_generic.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
 export async function html_update_public(name) {
