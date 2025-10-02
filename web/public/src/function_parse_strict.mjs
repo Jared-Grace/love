404dbe63ba6f5728e73b48c7fd904a6b4b017f90
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 13:08:23 -0400

    function_auto_path ..\love\public\src\function_parse_strict.mjs

--- web/public/src/function_parse_strict.mjs
+++ web/public/src/function_parse_strict.mjs
@@ -1,6 +1,8 @@
+import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 export async function function_parse_strict(f_name) {
+  let search = await function_name_to_path_search(unaliased);
   const f_path = function_name_to_path(f_name);
   let parsed = await file_js_parse(f_path);
   return parsed;
