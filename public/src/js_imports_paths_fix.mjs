ede43e240c7e1cb36a00675433663c8ebdf8e39f
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 12:40:49 -0400

    function_auto_path ..\love\public\src\firebase_deploy_function_destination_latest.mjs

--- public/src/js_imports_paths_fix.mjs
+++ public/src/js_imports_paths_fix.mjs
@@ -1,4 +1,4 @@
-import { js_imports_paths_fix_inner } from "../../../karate_code/public/src/js_imports_paths_fix_inner.mjs";
+import { js_imports_paths_fix_inner } from "../../../love/public/src/js_imports_paths_fix_inner.mjs";
 import { functions_names_to_paths } from "../../../love/public/src/functions_names_to_paths.mjs";
 export async function js_imports_paths_fix(ast) {
   let dictionary = await functions_names_to_paths();
