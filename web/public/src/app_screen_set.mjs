5c1833cb81d0700e9d33a55047a385ab32a375db
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 18:14:16 -0400

    function_auto_path ..\love\public\src\app_screen_set.mjs

--- web/public/src/app_screen_set.mjs
+++ web/public/src/app_screen_set.mjs
@@ -2,5 +2,5 @@ import { storage_local_set_context } from "../../../love/public/src/storage_loca
 import { app_refresh } from "../../../love/public/src/app_refresh.mjs";
 export async function app_screen_set(context, value) {
   storage_local_set_context(context, "screen", value);
-  await app_refresh(context);
+  app_refresh(context);
 }
