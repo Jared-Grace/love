1085211d4eb50e742b02e1688f4eccfbd69bfeb2
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 18:18:02 -0400

    function_auto_path ..\love\public\src\js_await_add_inner.mjs

--- public/src/js_await_add_inner.mjs
+++ public/src/js_await_add_inner.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_visit_type_each_async } from "../../../love/public/src/js_visit_type_each_async.mjs";
 import { js_call_function_if } from "../../../love/public/src/js_call_function_if.mjs";
 import { js_node_type_not_is_if } from "../../../love/public/src/js_node_type_not_is_if.mjs";
@@ -24,7 +25,7 @@ export async function js_await_add_inner(functions, ast, visited) {
       if (n) {
         marker("1");
         return;
-      }l
+      }
       let stack = object_property_get(v, "stack");
       await js_function_last_asyncify(stack, async_is, ast, functions, visited);
       let stack1 = list_get_end_1(stack);
@@ -39,4 +40,5 @@ export async function js_await_add_inner(functions, ast, visited) {
   }
   await js_visit_type_each_async(ast, "CallExpression", lambda);
   return;
+  log(message);
 }
