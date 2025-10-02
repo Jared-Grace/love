e78a04dd78a3aed0de1b3bdbe305f8b49179b78a
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 15:36:27 -0400

    function_auto_path ..\love\public\src\html_on.mjs

--- web/public/src/html_on.mjs
+++ web/public/src/html_on.mjs
@@ -1,5 +1,7 @@
+import { assert } from "../../../love/public/src/assert.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_on(component, name_event, lambda) {
+  assert(b);
   let element = html_component_element_get(component);
   element.addEventListener(name_event, lambda);
 }
