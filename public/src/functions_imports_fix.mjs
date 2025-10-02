4ab4e11fdff92099bdfb0f7cb0ced2fcb0e66adf
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 12:36:03 -0400

    function_auto_path ..\love\public\src\functions_imports_fix.mjs

--- public/src/functions_imports_fix.mjs
+++ public/src/functions_imports_fix.mjs
@@ -1,6 +1,7 @@
+import { functions_transform_list } from "../../../karate_code/public/src/functions_transform_list.mjs";
+import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { js_imports_paths_fix_inner } from "../../../karate_code/public/src/js_imports_paths_fix_inner.mjs";
 import { functions_names_to_paths } from "../../../love/public/src/functions_names_to_paths.mjs";
-import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_imports_fix() {
   marker("1");
@@ -9,6 +10,7 @@ export async function functions_imports_fix() {
     let v2 = js_imports_paths_fix_inner(ast, dictionary);
     return v2;
   };
-  let v = await functions_transform(lambda);
+  const list = await functions_names();
+  let v = await functions_transform_list(lambda, list);
   return v;
 }
