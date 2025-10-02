6410bbd0b1b44cb78ba23a477fcf2bda65299f8d
Grace-Jared <christrosetolife@gmail.com>
2025-10-02 12:33:16 -0400

    function_auto_path ..\love\public\src\functions_transform.mjs

--- public/src/functions_transform.mjs
+++ public/src/functions_transform.mjs
@@ -1,16 +1,13 @@
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { list_wait } from "../../../love/public/src/list_wait.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function functions_transform(lambda) {
   async function lambda2(f_name) {
     let output = await function_transform(f_name, lambda);
     return output;
   }
-  let mapped = list_map(await functions_names(), lambda2);
-  async function lambda3(item) {}
-  let waited = await list_map_unordered_async(list, lambda3);
+  let mapped = await list_map_unordered_async(await functions_names(), lambda2);
   let v = await list_wait(mapped);
   return v;
 }
