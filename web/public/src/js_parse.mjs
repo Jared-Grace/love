53bf052ecaf5bb89fce03b60f93d91c5a61a4c1e
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 13:04:56 -0400

    km

--- web/public/src/js_parse.mjs
+++ web/public/src/js_parse.mjs
@@ -1,4 +1,4 @@
-import { js_parse_generic } from "../../../karate_code/public/src/js_parse_generic.mjs";
+import { js_parse_generic } from "../../../love/public/src/js_parse_generic.mjs";
 import * as acorn from "acorn";
 export function js_parse(code) {
   let v = js_parse_generic(acorn, code);
--- web/public/src/js_parse_async.mjs
+++ web/public/src/js_parse_async.mjs
@@ -1,5 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { js_parse_generic } from "../../../karate_code/public/src/js_parse_generic.mjs";
+import { js_parse_generic } from "../../../love/public/src/js_parse_generic.mjs";
 export async function js_parse_async(code) {
   const acorn = await import("acorn");
   marker("1");
