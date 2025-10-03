33f5b24592ce1f034c7a0c068fabd58b21301a4b
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 11:22:13 -0400

    function_auto_path ..\love\public\src\storage_local_set.mjs

--- public/src/storage_local_set.mjs
+++ public/src/storage_local_set.mjs
@@ -1,6 +1,11 @@
+import { global_function_property_set } from "../../../karate_code/public/src/global_function_property_set.mjs";
 import { storage_local_specify_set } from "./storage_local_specify_set.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
-export function storage_local_set(app_fn, key, value) {
+import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
+export async function storage_local_set(app_fn, key, value) {
   let storage_local_key = storage_local_key_get(app_fn, key);
-  storage_local_specify_set(value, storage_local_key);
+  if (storage_local_enabled()) {
+    storage_local_specify_set(value, storage_local_key);
+  }
+  await global_function_property_set(storage_local_set, property_name, value);
 }
