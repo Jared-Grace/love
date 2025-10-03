27b26072c3596d6d1b164acf268359024788bfa1
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 13:17:18 -0400

    function_auto_path ..\love\public\src\storage_local_get.mjs

--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,3 +1,4 @@
+import { global_function_property_exists } from "../../../karate_code/public/src/global_function_property_exists.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
@@ -11,6 +12,7 @@ export function storage_local_get(app_fn, key) {
     let result = storage_local_specify_get(storage_local_key);
     return result;
   }
+  let value2 = global_function_property_exists(fn, property_name);
   let value = global_function_property_get(storage_local_set, key);
   return value;
 }
