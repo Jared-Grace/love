a51b7a5988000efa34e045c7a556b80da51ac256
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 07:17:10 -0400

    function_auto_path ..\love\public\src\global_function_initialize.mjs

--- public/src/global_function_initialize.mjs
+++ public/src/global_function_initialize.mjs
@@ -2,6 +2,6 @@ import { object_property_initialize } from "./object_property_initialize.mjs";
 import { global_get } from "./global_get.mjs";
 export function global_function_initialize(fn, initial) {
   let global = global_get();
-  let fn_object = object_property_initialize(global, fn.name, initial);
-  return fn_object;
+  let value = object_property_initialize(global, fn.name, initial);
+  return value;
 }
