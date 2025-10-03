0d438f67826e52362533ad87256cc9531e5ea699
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 08:07:53 -0400

    np

--- public/src/app_screen_set.mjs
+++ public/src/app_screen_set.mjs
@@ -1,6 +1,6 @@
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { app_refresh } from "../../../love/public/src/app_refresh.mjs";
-export async function app_screen_set(context, value) {
+export  function app_screen_set(context, value) {
   storage_local_set_context(context, "screen", value);
-  await app_refresh(context);
+   app_refresh(context);
 }
