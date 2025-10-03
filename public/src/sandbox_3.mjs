347c94f11bfce964648750d9886e492bb7a9bd12
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 15:32:07 -0400

    function_auto_path ..\love\public\src\sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,6 @@
 import { marker_screen_add } from "../../../love/public/src/marker_screen_add.mjs";
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
-export async function sandbox_3(f_path) {
+export async function sandbox_3() {
   await function_delete("app_karate_settings");
   let v2 = await marker_screen_add("settings");
 }
