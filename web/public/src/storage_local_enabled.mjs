8e2c0630b79b683f3e508b30aece0f23fc736b1e
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 13:25:16 -0400

    function_auto_path ..\love\public\src\storage_local_enabled.mjs

--- web/public/src/storage_local_enabled.mjs
+++ web/public/src/storage_local_enabled.mjs
@@ -1,11 +1,10 @@
 import { global_function_get } from "../../../love/public/src/global_function_get.mjs";
-import { storage_local_disable } from "../../../love/public/src/storage_local_disable.mjs";
 import { global_function_exists } from "../../../love/public/src/global_function_exists.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_enabled() {
   marker("1");
-  let set = global_function_exists(storage_local_disable);
-  let enabled = not(set) || global_function_get(storage_local_disable) === true;
+  let set = global_function_exists(storage_local_enabled);
+  let enabled = not(set) || global_function_get(storage_local_enabled) === true;
   return enabled;
 }
