cc1d87da184104613a587331422584107fddaa99
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 16:21:21 -0400

    function_auto_path ..\love\public\src\sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -3,6 +3,6 @@ import { marker_screen_add } from "../../../love/public/src/marker_screen_add.mj
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function sandbox_3() {
   marker("1");
-  await function_delete("app_karate_settings");
-  let v2 = await marker_screen_add("settings");
+  await function_delete("app_karate_settings_computer_save");
+  let v2 = await marker_screen_add("settings_computer_save");
 }
