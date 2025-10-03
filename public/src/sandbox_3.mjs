6d7f8fb6aa990cfa61d2eea55d69c5c47c96abb5
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 15:30:42 -0400

    function_auto_path ..\love\public\src\sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,7 +1,4 @@
-import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox_3() {
   marker("1");
-  let v = await command_line_generic("dir", {});
-  return v;
 }
