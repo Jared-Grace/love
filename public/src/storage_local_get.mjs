597036daf664f48731e8cb379f6551cf449d72f4
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 07:29:19 -0400

    function_auto_path ..\love\public\src\storage_local_get.mjs

--- public/src/storage_local_get.mjs
+++ public/src/storage_local_get.mjs
@@ -1,10 +1,11 @@
+import { storage_local_enabled } from "../../../karate_code/public/src/storage_local_enabled.mjs";
 import { storage_local_specify_get } from "./storage_local_specify_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
 export function storage_local_get(app_fn, key) {
   marker("1");
   let storage_local_key = storage_local_key_get(app_fn, key);
-  if (false) {
+  if (storage_local_enabled()) {
   }
   let result = storage_local_specify_get(storage_local_key);
   return result;
