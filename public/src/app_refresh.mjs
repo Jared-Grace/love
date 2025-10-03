e78eacca899d0d602995fe1487488c7f3f9b9b81
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 07:34:31 -0400

    function_auto_path ..\love\public\src\app_screen_set.mjs

--- public/src/app_refresh.mjs
+++ public/src/app_refresh.mjs
@@ -3,12 +3,12 @@ import { storage_local_initialize } from "../../../love/public/src/storage_local
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-export function app_refresh(context) {
+export async function app_refresh(context) {
   let { app_fn, screens } = context;
   let body = html_document_body();
   html_clear(body);
   marker("1");
-  let screen_name = storage_local_initialize(app_fn, "screen", "home");
+  let screen_name = await storage_local_initialize(app_fn, "screen", "home");
   let screen = object_property_get(screens, screen_name);
   screen(context);
 }
