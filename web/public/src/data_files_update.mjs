78fd45dfc716baa3e5c893379b279f3a6f68ad4e
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 08:51:05 -0400

    function_auto_path ..\love\public\src\data_files_update.mjs

--- web/public/src/data_files_update.mjs
+++ web/public/src/data_files_update.mjs
@@ -1,13 +1,9 @@
+import { data_generate } from "../../../karate_code/public/src/data_generate.mjs";
 import { object_properties_delete } from "../../../love/public/src/object_properties_delete.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
-import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { data_save } from "../../../love/public/src/data_save.mjs";
-import { data_file_update_inner } from "../../../love/public/src/data_file_update_inner.mjs";
 import { data_all } from "../../../love/public/src/data_all.mjs";
-import { functions_paths } from "../../../love/public/src/functions_paths.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function data_files_update() {
   marker("1");
   let d_path = data_path();
@@ -15,11 +11,6 @@ export async function data_files_update() {
   let { data } = d;
   let properties = ["identifiers", "functions"];
   object_properties_delete(data, properties);
-  let f_paths = await functions_paths();
-  let parseds = await list_map_unordered_async(f_paths, file_js_parse);
-   function lambda(parsed) {
-     data_file_update_inner(parsed, d);
-  }
-   each(parseds, lambda);
+  await data_generate(data);
   await data_save(d);
 }
