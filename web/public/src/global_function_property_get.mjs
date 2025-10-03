76422b6352d48d9f22846426b31d8a92d3f2de86
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 13:10:41 -0400

    function_auto_path ..\love\public\src\global_function_property_get.mjs

--- web/public/src/global_function_property_get.mjs
+++ web/public/src/global_function_property_get.mjs
@@ -1,9 +1,9 @@
-import { global_function_get } from "../../../love/public/src/global_function_get.mjs";
+import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function global_function_property_get(fn, property_name) {
   marker("1");
-  let fn_object = global_function_get(fn);
+  let fn_object = global_function_initialize(fn);
   let value = object_property_get(fn_object, property_name);
   return value;
 }
