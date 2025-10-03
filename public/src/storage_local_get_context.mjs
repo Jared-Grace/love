ac9bbbdd4b9d7bd79234544bcdfb1bf3b10e38e0
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 14:59:31 -0400

    function_auto_path ..\love\public\src\storage_local_get_context.mjs

--- public/src/storage_local_get_context.mjs
+++ public/src/storage_local_get_context.mjs
@@ -1,6 +1,6 @@
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
-export async function storage_local_get_context(context, key) {
+export function storage_local_get_context(context, key) {
   let { app_fn } = context;
-  let value = await storage_local_get(app_fn, key);
+  let value = storage_local_get(app_fn, key);
   return value;
 }
