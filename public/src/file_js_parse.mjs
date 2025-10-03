b2389180032020c402663d869d403022e316a6cb
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 13:01:24 -0400

    p

--- public/src/file_js_parse.mjs
+++ public/src/file_js_parse.mjs
@@ -2,7 +2,7 @@ import { file_read } from "../../../love/public/src/file_read.mjs";
 import { js_parse } from "../../../love/public/src/js_parse.mjs";
 export async function file_js_parse(f_path) {
   let code = await file_read(f_path);
-  const ast = js_parse(code);
+  const ast = js_parse_async(code);
   const parsed = {
     ast,
     code,
