ccfd751437c2ffd1950d23d6e7125c8abc7342ba
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 22:05:05 -0400

    function_auto_path ..\love\public\src\function_rename_suffix_add.mjs

--- public/src/function_rename_suffix_add.mjs
+++ public/src/function_rename_suffix_add.mjs
@@ -1,3 +1,4 @@
+import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 import { string_padded_is } from "../../../love/public/src/string_padded_is.mjs";
 import { assert_not } from "../../../love/public/src/assert_not.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
@@ -5,7 +6,8 @@ import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_suffix_add(f_name_before, suffix) {
   marker("1");
-  let p = string_padded_is(suffix, "_");
+  let padding = function_name_separator();
+  let p = string_padded_is(suffix, padding);
   assert_not(p);
   let f_name_after = function_name_combine(f_name_before, suffix);
   let v = await function_rename(f_name_before, f_name_after);
