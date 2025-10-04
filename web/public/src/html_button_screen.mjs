2e31e7d7b1b1af03363ee5577198f1d96e10b947
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 22:14:48 -0400

    function_auto_path ..\love\public\src\html_button_screen.mjs

--- web/public/src/html_button_screen.mjs
+++ web/public/src/html_button_screen.mjs
@@ -1,7 +1,7 @@
 import { app_screen_set_lambda } from "../../../love/public/src/app_screen_set_lambda.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-export function html_button_screen(parent, text, context, screen_name) {
-  let lambda = app_screen_set_lambda(context, screen_name);
+export function html_button_screen(parent, text, context, screen_fn) {
+  let lambda = app_screen_set_lambda(context, screen_fn);
   let component = html_button(parent, text, lambda);
   return component;
 }
