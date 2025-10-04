2ad491cd22117e8b028c4617d921c11534cc2293
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 22:14:40 -0400

    function_auto_path ..\love\public\src\app_screen_set_lambda.mjs

--- public/src/app_screen_set_lambda.mjs
+++ public/src/app_screen_set_lambda.mjs
@@ -1,7 +1,7 @@
 import { app_screen_set } from "../../../love/public/src/app_screen_set.mjs";
-export function app_screen_set_lambda(context, screen_name) {
+export function app_screen_set_lambda(context, screen_fn) {
   let l = function lambda() {
-    app_screen_set(context, screen_name);
+    app_screen_set(context, screen_fn);
   };
   return l;
 }
