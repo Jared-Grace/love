e6c4da1f0965295e9fe3c72ba1c19a9df89ed2c7
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 22:07:54 -0400

    function_auto_path ..\love\public\src\app_screen_set.mjs

--- public/src/app_screen_set.mjs
+++ public/src/app_screen_set.mjs
@@ -5,6 +5,6 @@ import { app_refresh } from "../../../love/public/src/app_refresh.mjs";
 export function app_screen_set(context, fn) {
   let app_fn = object_property_get(context, "app_fn");
   let without = function_name_prefix_without(fn, app_fn);
-  storage_local_set_context(context, "screen", value);
+  storage_local_set_context(context, "screen", without);
   app_refresh(context);
 }
