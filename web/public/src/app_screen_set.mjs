d193fb2f5997fb5bd26a8b0c2021b74194f0d4c9
Grace-Jared <christrosetolife@gmail.com>
2025-10-03 22:07:15 -0400

    function_auto_path ..\love\public\src\app_screen_set.mjs

--- web/public/src/app_screen_set.mjs
+++ web/public/src/app_screen_set.mjs
@@ -1,6 +1,8 @@
+import { function_name_prefix_without } from "../../../karate_code/public/src/function_name_prefix_without.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { app_refresh } from "../../../love/public/src/app_refresh.mjs";
-export function app_screen_set(context, value) {
+export function app_screen_set(context, fn) {
+  let without = function_name_prefix_without(fn2, fn_prefix);
   storage_local_set_context(context, "screen", value);
   app_refresh(context);
 }
