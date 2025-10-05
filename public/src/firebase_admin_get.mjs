c160835196270656908e06d383425eb1ae06e14a
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 08:30:33 -0400

    function_auto_path ..\love\public\src\firebase_admin_get.mjs

--- public/src/firebase_admin_get.mjs
+++ public/src/firebase_admin_get.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { firebase_storage_url_project } from "../../../love/public/src/firebase_storage_url_project.mjs";
 import { firebase_service_account } from "../../../love/public/src/firebase_service_account.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,9 +6,11 @@ export async function firebase_admin_get() {
   const admin = (await import("firebase-admin")).default;
   marker("1");
   let service_account = await firebase_service_account();
+  const sb = await firebase_storage_url_project();
+  log(message);
   admin.initializeApp({
     credential: admin.credential.cert(service_account),
-    storageBucket: await firebase_storage_url_project(),
+    storageBucket: sb,
   });
   return admin;
 }
