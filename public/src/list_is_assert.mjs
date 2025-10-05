be18d8fc373f5610672743f07a81c0b36a9ee065
Grace-Jared <christrosetolife@gmail.com>
2025-10-04 22:44:41 -0400

    function_auto_path ..\love\public\src\list_is_assert.mjs

--- public/src/list_is_assert.mjs
+++ public/src/list_is_assert.mjs
@@ -2,10 +2,9 @@ import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
 export function list_is_assert(list) {
   let result = list_is(list);
-  let type = typeof list;
   function lambda() {
     let v = {
-      type,
+      list,
     };
     return v;
   }
