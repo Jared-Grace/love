439edabaa7ec0c2f70a76986e20aa7758ba66520
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 07:36:11 -0400

    function_auto_path ..\love\public\src\app_new_step_3.mjs

--- web/public/src/app_new_step_3.mjs
+++ web/public/src/app_new_step_3.mjs
@@ -1,8 +1,7 @@
 import { app_new_step_3_generic } from "../../../love/public/src/app_new_step_3_generic.mjs";
-import { firebase_deploy_function_destination_production } from "../../../love/public/src/firebase_deploy_function_destination_production.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
+import { app_main_production } from "../../../karate_code/public/src/app_main_production.mjs";
 export async function app_new_step_3(name) {
   let a_name = app_name_prefixed(name);
-  let version_get = firebase_deploy_function_destination_production.name;
-  await app_new_step_3_generic(name, version_get, a_name);
+  await app_new_step_3_generic(name, app_main_production, a_name);
 }
