8a2e8b9f4d71af90eaa06dd8028f3559d7db83a0
Grace-Jared <christrosetolife@gmail.com>
2025-10-04 21:02:29 -0400

    function_auto_path ..\love\public\src\html_parse_find_list_to.mjs

--- public/src/html_parse_find_list_to.mjs
+++ public/src/html_parse_find_list_to.mjs
@@ -1,9 +1,11 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { html_parse_list_to } from "../../../love/public/src/html_parse_list_to.mjs";
 import { html_parse_find } from "../../../love/public/src/html_parse_find.mjs";
+import { list_is } from "./list_is.mjs";
 export function html_parse_find_list_to(bl, selector) {
   let query = html_parse_find(bl, selector);
   let list = html_parse_list_to(query);
-  log(list);
+  let message = list_is(list);
+  log(message);
   return list;
 }
