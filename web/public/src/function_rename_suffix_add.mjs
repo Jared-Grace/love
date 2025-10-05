c577cdd1a5bbfca73168d0b8b65b62a7b7e62624
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 08:11:45 -0400

    function_auto_path ..\love\public\src\function_rename_suffix_add.mjs

--- web/public/src/function_rename_suffix_add.mjs
+++ web/public/src/function_rename_suffix_add.mjs
@@ -1,12 +1,10 @@
-import { string_padded_not_is_assert } from "../../../karate_code/public/src/string_padded_not_is_assert.mjs";
-import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
+import { function_name_separator_padded_not_is_assert } from "../../../karate_code/public/src/function_name_separator_padded_not_is_assert.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_suffix_add(f_name_before, suffix) {
   marker("1");
-  let padding = function_name_separator();
-  string_padded_not_is_assert(suffix, padding);
+  function_name_separator_padded_not_is_assert(suffix);
   let f_name_after = function_name_combine(f_name_before, suffix);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
