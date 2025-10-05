8a4885912ff54d612ef8339133026667b03076b9
Grace-Jared <christrosetolife@gmail.com>
2025-10-04 22:44:27 -0400

    km

---
+++ public/src/bible_verse_grace.mjs
@@ -0,0 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
+import { list_to } from "../../../love/public/src/list_to.mjs";
+import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
+import { ebible_references_parse_lines } from "../../../love/public/src/ebible_references_parse_lines.mjs";
+export async function bible_verse_grace(reference) {
+  let args = list_to(arguments);
+  let joined = list_join_space(args);
+  let v = await ebible_references_parse_lines(["engbsb"], [joined]);
+  log(v);
+}
---
+++ public/src/function_rename_replace_last.mjs
@@ -0,0 +1,11 @@
+import { list_last } from "../../../love/public/src/list_last.mjs";
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_rename_replace_last(f_name_before, to) {
+  marker("1");
+  let parts = function_name_to_parts(f_name_before);
+  let from = list_last(parts);
+  let v = await function_rename_replace(f_name_before, from, to);
+  return v;
+}
