9ea8c5895ba78ad64649efa1054b68a713dd7c1d
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 05:08:50 -0400

    data_file_update ..\love\public\src\function_copy_replace.mjs

--- public/src/function_copy_replace.mjs
+++ public/src/function_copy_replace.mjs
@@ -3,7 +3,7 @@ import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_copy_replace(f_name_old, from, to) {
   marker("1");
-  let replaced = string_replace(s, from, to);
-  let v = await function_copy(f_name_old, replaced);
+  let f_name_new = string_replace(s, from, to);
+  let v = await function_copy(f_name_old, f_name_new);
   return v;
 }
