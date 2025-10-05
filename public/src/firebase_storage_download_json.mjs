32af021084a096b151a1608c91262c9f2bcf940b
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 13:32:54 -0400

    data_file_update ..\love\public\src\function_rename_suffix_add_generic.mjs

--- public/src/firebase_storage_download_json.mjs
+++ public/src/firebase_storage_download_json.mjs
@@ -1,4 +1,4 @@
-import { firebase_storage_download_string } from "../../../karate_code/public/src/firebase_storage_download_string.mjs";
+import { firebase_storage_download_string } from "../../../love/public/src/firebase_storage_download_string.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export async function firebase_storage_download_json(destination) {
   let s = await firebase_storage_download_string(destination);
