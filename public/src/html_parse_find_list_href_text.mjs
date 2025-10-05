d2a000b79a92e23157fc4afecdfdaa0b8fb0080f
Grace-Jared <christrosetolife@gmail.com>
2025-10-04 21:02:46 -0400

    function_auto_path ..\love\public\src\html_parse_find_list_href_text.mjs

--- public/src/html_parse_find_list_href_text.mjs
+++ public/src/html_parse_find_list_href_text.mjs
@@ -1,9 +1,13 @@
+import { log } from "../../../love/public/src/log.mjs";
+import { list_is } from "../../../love/public/src/list_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_parse_href_text_map } from "../../../love/public/src/html_parse_href_text_map.mjs";
 import { html_parse_find_list_to } from "../../../love/public/src/html_parse_find_list_to.mjs";
 export function html_parse_find_list_href_text(bl, selector, d) {
   marker("1");
   let list = html_parse_find_list_to(bl, selector);
+  let message = list_is(list);
+  log(message);
   let mapped = html_parse_href_text_map(d, list);
   return mapped;
 }
