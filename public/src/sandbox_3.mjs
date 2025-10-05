867e4f40bb69679837ab26ce53b7a7fdf33f02a1
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 09:02:47 -0400

    o s3

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function sandbox_3() {
+  marker("1");
   const messenger = "JESUS";
   if (browser_is()) {
   }
