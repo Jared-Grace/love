518faee801093338ddf35f1741d94af2928b6a08
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 08:28:07 -0400

    function_auto_path ..\love\public\src\firebase_service_account.mjs

--- web/public/src/firebase_service_account.mjs
+++ web/public/src/firebase_service_account.mjs
@@ -2,8 +2,8 @@ import { function_run } from "../../../love/public/src/function_run.mjs";
 import { user_repo_firebase_service_account_get } from "../../../karate_code/public/src/user_repo_firebase_service_account_get.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 export async function firebase_service_account() {
-  let fn = await user_repo_firebase_service_account_get();
-  let file_path = await function_run(fn, []);
+  let f_name = await user_repo_firebase_service_account_get();
+  let file_path = await function_run(f_name, []);
   let service_account = await file_read_json(file_path);
   return service_account;
 }
