5594d8877cf045dfdc0a067073a3ffbfd588c153
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 08:20:36 -0400

    rp repo_firebase_service_account_get user

--- web/public/src/firebase_service_account.mjs
+++ web/public/src/firebase_service_account.mjs
@@ -1,8 +1,8 @@
 import { function_run } from "../../../love/public/src/function_run.mjs";
-import { repo_firebase_service_account_get } from "../../../love/public/src/repo_firebase_service_account_get.mjs";
+import { user_repo_firebase_service_account_get } from "../../../karate_code/public/src/user_repo_firebase_service_account_get.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 export async function firebase_service_account() {
-  let fn = await repo_firebase_service_account_get();
+  let fn = await user_repo_firebase_service_account_get();
   let file_path = await function_run(fn, []);
   let service_account = await file_read_json(file_path);
   return service_account;
