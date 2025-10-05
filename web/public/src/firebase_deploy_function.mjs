d9386616892e0bc23cdfa73b12f322a9b8b87ef1
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 08:32:14 -0400

    function_auto_path ..\love\public\src\firebase_deploy_function.mjs

--- web/public/src/firebase_deploy_function.mjs
+++ web/public/src/firebase_deploy_function.mjs
@@ -7,10 +7,10 @@ import { date_now_file } from "../../../love/public/src/date_now_file.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function firebase_deploy_function(f_name) {
   marker("1");
-  let code = await function_dependencies_code(f_name);
-  let v = await function_name_unalias(f_name2);
+  let { unaliased } = await function_name_unalias(f_name);
+  let code = await function_dependencies_code(unaliased);
   let now_file = date_now_file();
-  let destination = firebase_deploy_function_destination(now_file, f_name);
+  let destination = firebase_deploy_function_destination(now_file, unaliased);
   await firebase_upload_object(
     {
       code,
@@ -20,6 +20,7 @@ export async function firebase_deploy_function(f_name) {
   let version = {
     destination,
   };
-  let destination_version = firebase_deploy_function_destination_latest(f_name);
+  let destination_version =
+    firebase_deploy_function_destination_latest(unaliased);
   await firebase_upload_object(version, destination_version);
 }
