a0758471c7ee9e693635e6e3217699dcc70b16ee
Grace-Jared <christrosetolife@gmail.com>
2025-10-04 21:07:29 -0400

    ps

--- public/src/list_is_assert.mjs
+++ public/src/list_is_assert.mjs
@@ -3,9 +3,10 @@ import { assert_json } from "../../../love/public/src/assert_json.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
 export function list_is_assert(list) {
   let result = list_is(list);
+  log(
+    {result},);
   assert_json(result, {
     list,
   });
   return;
-  log(message);
 }
