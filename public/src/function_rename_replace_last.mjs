fe5e10165813432290e9baf0e60247e2807c71a2
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 07:33:01 -0400

    function_auto_path ..\love\public\src\function_rename_replace_last.mjs

--- public/src/function_rename_replace_last.mjs
+++ public/src/function_rename_replace_last.mjs
@@ -1,11 +1,9 @@
-import { list_last } from "../../../love/public/src/list_last.mjs";
-import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { function_name_to_part_last } from "../../../karate_code/public/src/function_name_to_part_last.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_replace_last(f_name_before, to) {
   marker("1");
-  let parts = function_name_to_parts(f_name_before);
-  let from = list_last(parts);
+  let from = function_name_to_part_last(f_name_before);
   let v = await function_rename_replace(f_name_before, from, to);
   return v;
 }
