bcccc7359bc16025eea22914f8ca9e0da19a00d7
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 07:43:39 -0400

    function_auto_path ..\love\public\src\sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,5 +1,6 @@
 import { log } from "../../../love/public/src/log.mjs";
 export async function sandbox_3() {
-  alert("JESUS");
-  log();
+  const messenger = "JESUS";
+  alert(messenger);
+  log(messenger);
 }
