77307caf978871dc7da335b32641eba92f07e725
Grace-Jared <christrosetolife@gmail.com>
2025-10-05 04:46:21 -0400

    function_auto_path ..\love\public\src\sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,8 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-import { marker_screen_add } from "../../../love/public/src/marker_screen_add.mjs";
-import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function sandbox_3() {
-  marker("1");
-  await function_delete("app_karate_email_verify");
-  let v2 = await marker_screen_add("email_verify");
+  alert("JESUS");
 }
