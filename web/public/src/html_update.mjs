740a01d4a160b56ec15d177c644533bc693b96e2
Grace-Jared <christrosetolife@gmail.com>
2025-10-06 14:54:13 -0400

    r html_update html_app_update

--- web/public/src/html_update.mjs
+++
@@ -1,6 +0,0 @@
-import { html_update_dev } from "../../../love/public/src/html_update_dev.mjs";
-import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
-export async function html_update(name) {
-  await html_update_public(name);
-  await html_update_dev(name);
-}
