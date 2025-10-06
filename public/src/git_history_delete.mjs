fabb12c6e5ec785688bb0652b975134035c36a9e
Grace-Jared <christrosetolife@gmail.com>
2025-10-06 17:05:14 -0400

    function_auto_path ..\love\public\src\git_history_delete.mjs

--- public/src/git_history_delete.mjs
+++ public/src/git_history_delete.mjs
@@ -1,16 +1,15 @@
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
 import { git_repo_url } from "../../../love/public/src/git_repo_url.mjs";
 import { folder_delete } from "../../../love/public/src/folder_delete.mjs";
-import { todo } from "../../../love/public/src/todo.mjs";
 import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
 import { uuid } from "../../../love/public/src/uuid.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function git_history_delete(user, repo, f_path) {
   marker("1");
-  todo(
+  comment(
     "make sure all changes are in repo first like pushing; may need to coordinate with other users",
   );
-  todo("make sure this is run from the correct directory");
+  comment("make sure this is run from the correct directory");
   const url = git_repo_url(user, repo);
   const repo_folder = repo + "-clean-" + (await uuid()) + ".git";
   let stdout = await command_line_git(
