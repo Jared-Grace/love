54d8f4339f9c083ce14d15c278fa1ad740b11ec6
Grace-Jared <christrosetolife@gmail.com>
2025-10-06 17:11:29 -0400

    function_auto_path ..\love\public\src\git_history_delete.mjs

--- public/src/git_history_delete.mjs
+++ public/src/git_history_delete.mjs
@@ -7,7 +7,6 @@ import { command_line_git } from "../../../love/public/src/command_line_git.mjs"
 import { uuid } from "../../../love/public/src/uuid.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function git_history_delete(user, repo, f_path, repo_path) {
-  let folder = ".";
   await git_push_folder(folder);
   marker("1");
   ("make sure all changes are in repo first like pushing; may need to coordinate with other users");
