3603ae501a461e5a263461853e1e1c48183f4657
Grace-Jared <christrosetolife@gmail.com>
2025-10-06 19:05:09 -0400

    function_auto_path ..\love\public\src\storage_local_get.mjs

--- web/public/src/storage_local_get.mjs
+++ web/public/src/storage_local_get.mjs
@@ -12,7 +12,6 @@ export function storage_local_get(app_fn, key) {
   let storage_local_key = storage_local_key_get(app_fn, key);
   if (storage_local_enabled()) {
     let result = storage_local_specify_get(storage_local_key);
-    log(result);
     return result;
   }
   let exists = global_function_property_exists(
@@ -28,5 +27,9 @@ export function storage_local_get(app_fn, key) {
     storage_local_set,
     storage_local_key,
   );
+  log({
+    value,
+    key,
+  });
   return value;
 }
