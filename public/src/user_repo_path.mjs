e7ac2fbbd4f8646b0fc30cced2da4c941a7635ff
Grace-Jared <christrosetolife@gmail.com>
2025-10-06 16:59:59 -0400

    function_auto_path ..\love\public\src\user_repo_path.mjs

--- public/src/user_repo_path.mjs
+++ public/src/user_repo_path.mjs
@@ -1,5 +1,6 @@
-import { file_name_json_folder } from "../../../love/public/src/file_name_json_folder.mjs";
+import { file_name_json_folder_gitignore } from "../../../karate_code/public/src/file_name_json_folder_gitignore.mjs";
 export function user_repo_path() {
-  let f_path = file_name_json_folder("gitignore", "user");
+  const name = "user";
+  let f_path = file_name_json_folder_gitignore(name);
   return f_path;
 }
