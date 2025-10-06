648ddcbf9737b19702c37d2977d1b9ad026e35d3
Grace-Jared <christrosetolife@gmail.com>
2025-10-06 14:54:13 -0400

    r html_update html_app_update

--- web/public/src/app_new_update.mjs
+++ web/public/src/app_new_update.mjs
@@ -1,10 +1,10 @@
 import { app_new_step_5 } from "../../../love/public/src/app_new_step_5.mjs";
 import { app_new_step_3 } from "../../../love/public/src/app_new_step_3.mjs";
-import { html_update } from "../../../love/public/src/html_update.mjs";
+import { html_app_update } from "../../../karate_code/public/src/html_app_update.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_new_update(name) {
   marker("1");
   await app_new_step_3(name);
   await app_new_step_5(name);
-  await html_update(name);
+  await html_app_update(name);
 }
