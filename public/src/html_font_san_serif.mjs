b12332595cc856509a774928b4cdd812737040d1
Grace-Jared <christrosetolife@gmail.com>
2025-10-08 16:37:58 -0400

    function_auto_path ..\love\public\src\html_font_san_serif.mjs

--- public/src/html_font_san_serif.mjs
+++ public/src/html_font_san_serif.mjs
@@ -1,6 +1,8 @@
+import { html_document_root } from "../../../love/public/src/html_document_root.mjs";
 import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
-export function html_font_san_serif(context, html) {
+export function html_font_san_serif(context) {
+  let html = html_document_root();
   let value = storage_local_initialize_context(
     context,
     "font_family",
