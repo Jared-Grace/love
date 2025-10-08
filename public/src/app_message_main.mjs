f0ac063f883be7ff1965b8d4b6251d1e5e93fa02
Grace-Jared <christrosetolife@gmail.com>
2025-10-08 14:18:19 -0400

    function_auto_path ..\love\public\src\app_message_main.mjs

--- public/src/app_message_main.mjs
+++ public/src/app_message_main.mjs
@@ -1,3 +1,4 @@
+import { app_karate_style_control } from "../../../karate_code/public/src/app_karate_style_control.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,4 +6,5 @@ export function app_message_main() {
   marker("1");
   const root = html_document_body();
   let component = html_element(root, "textarea");
+  app_karate_style_control(component2);
 }
