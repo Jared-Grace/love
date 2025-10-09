c3fb6e875860dda71afc07e3455accf6a2c793c9
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 15:07:19 -0400

    function_auto_path ..\love\public\src\firebase_upload_string.mjs

--- web/public/src/firebase_upload_string.mjs
+++ web/public/src/firebase_upload_string.mjs
@@ -1,34 +1,6 @@
-import { log } from "../../../love/public/src/log.mjs";
-import { firebase_config_get } from "../../../love/public/src/firebase_config_get.mjs";
-import { browser_is } from "../../../love/public/src/browser_is.mjs";
-import { firebase_upload_generic } from "../../../love/public/src/firebase_upload_generic.mjs";
+import { firebase_upload_string_generic } from "../../../love/public/src/firebase_upload_string_generic.mjs";
 export async function firebase_upload_string(content, destination) {
-  const contentType = "text/plain";
-  async function firebase_upload_string_generic(params) {
-    if (browser_is()) {
-      const firebase = await import(
-        "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"
-      );
-      const storageMod = await import(
-        "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js"
-      );
-      const { ref, uploadString } = firebaseStorage;
-      let firebase_config = firebase_config_get();
-      const app = firebase.initializeApp(firebase_config);
-      const storage = storageMod.getStorage(app);
-      const jsonRef = ref(storage, destination);
-      await uploadString(jsonRef, content, "raw", {
-        contentType: "application/json",
-      });
-      console.log("✅ JSON uploaded successfully");
-      return;
-    }
-    let buffer = Buffer.from(content);
-    const settings = {
-      contentType: contentType,
-      gzip: true,
-    };
-    await firebase_upload_generic(destination, settings, buffer);
-  }
+  const content_type = "text/plain";
+  await firebase_upload_string_generic(content, destination, content_type);
   return destination;
 }
