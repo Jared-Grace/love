5410249cc1c6b2697b4889d0114398e17ebe3278
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 16:07:08 -0400

    function_auto_path ..\love\public\src\uuid.mjs

--- public/src/uuid.mjs
+++ public/src/uuid.mjs
@@ -1,5 +1,8 @@
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { import_install } from "../../../love/public/src/import_install.mjs";
 export async function uuid() {
+  if (browser_is()) {
+  }
   const { v4: uuidv4 } = await import_install("uuid");
   let u = uuidv4();
   return u;
