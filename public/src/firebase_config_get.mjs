7699b70fa30640bad0bd3f346de13a70a332f74d
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 14:51:33 -0400

    function_auto_path ..\love\public\src\firebase_config_get.mjs

--- public/src/firebase_config_get.mjs
+++ public/src/firebase_config_get.mjs
@@ -1,4 +1,14 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function firebase_config_get() {
   marker("1");
+  const firebase_config = {
+    apiKey: "AIzaSyABi8aveEhWeD6GyRXj9FUqu_aDWxhuBGQ",
+    authDomain: "jared-grace.firebaseapp.com",
+    projectId: "jared-grace",
+    storageBucket: "jared-grace.firebasestorage.app",
+    messagingSenderId: "879920885833",
+    appId: "1:879920885833:web:2f5a3cb39a398516690f98",
+    measurementId: "G-B2XTX9NJCZ",
+  };
+  return firebase_config;
 }
