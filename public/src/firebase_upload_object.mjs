8a16ae82258aa34918758f29918bb09e1cee5851
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 14:54:47 -0400

    function_auto_path ..\love\public\src\firebase_upload_object.mjs

--- public/src/firebase_upload_object.mjs
+++ public/src/firebase_upload_object.mjs
@@ -3,7 +3,7 @@ import { log } from "../../../love/public/src/log.mjs";
 import { firebase_upload_string } from "../../../love/public/src/firebase_upload_string.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export async function firebase_upload_object(object, destination) {
-  firebase_config_get();
+  let firebase_config = firebase_config_get();
   const app = firebase.initializeApp(firebaseConfig);
   const storage = storageMod.getStorage(app);
   const data = {
