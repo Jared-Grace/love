1fca26098aeb34646a0110dd34ed6e42e61649a5
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 15:06:16 -0400

    function_auto_path ..\love\public\src\firebase_upload_string.mjs

--- web/public/src/firebase_upload_string.mjs
+++ web/public/src/firebase_upload_string.mjs
@@ -4,29 +4,31 @@ import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { firebase_upload_generic } from "../../../love/public/src/firebase_upload_generic.mjs";
 export async function firebase_upload_string(content, destination) {
   const contentType = "text/plain";
-  if (browser_is()) {
-    const firebase = await import(
-      "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"
-    );
-    const storageMod = await import(
-      "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js"
-    );
-    const { ref, uploadString } = firebaseStorage;
-    let firebase_config = firebase_config_get();
-    const app = firebase.initializeApp(firebase_config);
-    const storage = storageMod.getStorage(app);
-    const jsonRef = ref(storage, destination);
-    await uploadString(jsonRef, content, "raw", {
-      contentType: "application/json",
-    });
-    console.log("✅ JSON uploaded successfully");
-    return;
+  async function firebase_upload_string_generic(params) {
+    if (browser_is()) {
+      const firebase = await import(
+        "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"
+      );
+      const storageMod = await import(
+        "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js"
+      );
+      const { ref, uploadString } = firebaseStorage;
+      let firebase_config = firebase_config_get();
+      const app = firebase.initializeApp(firebase_config);
+      const storage = storageMod.getStorage(app);
+      const jsonRef = ref(storage, destination);
+      await uploadString(jsonRef, content, "raw", {
+        contentType: "application/json",
+      });
+      console.log("✅ JSON uploaded successfully");
+      return;
+    }
+    let buffer = Buffer.from(content);
+    const settings = {
+      contentType: contentType,
+      gzip: true,
+    };
+    await firebase_upload_generic(destination, settings, buffer);
   }
-  let buffer = Buffer.from(content);
-  const settings = {
-    contentType: contentType,
-    gzip: true,
-  };
-  await firebase_upload_generic(destination, settings, buffer);
   return destination;
 }
