d11be3555de6d107f8fa7379b578e6f835de435f
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:55:34 -0400

    function_auto_path ..\love\public\src\functions_rename_replace.mjs

--- public/src/functions_rename_replace.mjs
+++ public/src/functions_rename_replace.mjs
@@ -1,8 +1,10 @@
+import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_rename_replace() {
   marker("1");
+  let f_names = await functions_names();
   async function lambda(item) {}
   await each_async(list, lambda);
   let v = await function_rename_replace(f_name_before, from, to);
