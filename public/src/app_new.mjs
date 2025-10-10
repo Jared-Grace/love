a6d34dae7b93ba7f13e857cb3ae9f9b68458d604
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:45:48 -0400

    function_auto_path ..\love\public\src\app_new.mjs

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,16 +1,21 @@
-import { function_new } from "../../../love/public/src/function_new.mjs";
-import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
+import { html_new } from "../../../love/public/src/html_new.mjs";
+import { app_new_staging_update } from "../../../love/public/src/app_new_staging_update.mjs";
+import { app_new_production_update } from "../../../love/public/src/app_new_production_update.mjs";
+import { app_new_step_4 } from "../../../love/public/src/app_new_step_4.mjs";
+import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
 import { string_and_empty_not_is_assert } from "../../../love/public/src/string_and_empty_not_is_assert.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-import { app_new_step_2_next } from "../../../love/public/src/app_new_step_2_next.mjs";
+import { app_new_fn } from "../../../love/public/src/app_new_fn.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_new(name) {
   assert_arguments(arguments, 1);
   string_and_empty_not_is_assert(name);
   marker("1");
+  await app_new_fn(name);
   marker("1");
-  string_and_empty_not_is_assert(name);
-  let a_name = app_name_prefixed(name);
-  await function_new(a_name);
-  await app_new_step_2_next(name);
+  await app_new_step_2(name);
+  await app_new_step_4(name);
+  await app_new_production_update(name);
+  await app_new_staging_update(name);
+  await html_new(name);
 }
