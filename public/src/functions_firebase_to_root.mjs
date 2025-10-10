bf205006492906b1a39603ff885247302710e702
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 22:03:02 -0400

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/functions_firebase_to_root.mjs
+++ public/src/functions_firebase_to_root.mjs
@@ -1,3 +1,4 @@
+import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { functions_paths } from "../../../love/public/src/functions_paths.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -6,6 +7,7 @@ export async function functions_firebase_to_root() {
   marker("1");
   let f_names = await functions_names();
   async function lambda(item) {
+    let search = await function_name_to_path_search(unaliased);
     let joined = await functions_paths();
   }
   await each_async(f_names, lambda);
