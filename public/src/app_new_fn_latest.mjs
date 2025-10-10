5db25f35898bb4d83fcdc7869ec005394dfbc6e2
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:06:22 -0400

    function_auto_path ..\love\public\src\app_new_fn_latest.mjs

--- public/src/app_new_fn_latest.mjs
+++ public/src/app_new_fn_latest.mjs
@@ -1,8 +1,8 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { app_name_staging } from "../../../love/public/src/app_name_staging.mjs";
+import { app_name_latest } from "../../../love/public/src/app_name_latest.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function app_new_fn_latest(name) {
   marker("1");
-  let combined = app_name_staging(name);
+  let combined = app_name_latest(name);
   await function_new(combined);
 }
