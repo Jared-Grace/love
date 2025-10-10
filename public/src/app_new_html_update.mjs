7dcf630ada6156beda501639a68f0770fb1caee7
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:15:45 -0400

    rrm staging latest

--- public/src/app_new_html_update.mjs
+++ public/src/app_new_html_update.mjs
@@ -1,4 +1,4 @@
-import { html_update_staging } from "../../../love/public/src/html_update_staging.mjs";
+import { html_update_latest } from "../../../love/public/src/html_update_latest.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_update_dev } from "../../../love/public/src/html_update_dev.mjs";
 import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
@@ -6,5 +6,5 @@ export async function app_new_html_update(name) {
   marker("1");
   await html_update_public(name);
   await html_update_dev(name);
-  await html_update_staging(name);
+  await html_update_latest(name);
 }
