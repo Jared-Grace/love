e722deaeb95b442ccc8a0ddbbba4caa2b190ab33
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:45:32 -0400

    function_auto_path ..\love\public\src\app_new_fn.mjs

--- public/src/app_new_fn.mjs
+++ public/src/app_new_fn.mjs
@@ -1,17 +1,10 @@
-import { function_open } from "../../../love/public/src/function_open.mjs";
-import { function_new_declaration } from "../../../love/public/src/function_new_declaration.mjs";
-import { js_parse_statement_module } from "../../../love/public/src/js_parse_statement_module.mjs";
-import { js_code_declaration } from "../../../love/public/src/js_code_declaration.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { string_and_empty_not_is_assert } from "../../../love/public/src/string_and_empty_not_is_assert.mjs";
+import { function_new } from "../../../love/public/src/function_new.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 export async function app_new_fn(name) {
   marker("1");
   string_and_empty_not_is_assert(name);
   let a_name = app_name_prefixed(name);
-  marker("1");
-  const code_declaration = js_code_declaration(a_name, "", false);
-  let declaration = js_parse_statement_module(code_declaration);
-  await function_new_declaration(declaration);
-  await function_open(a_name);
+  await function_new(a_name);
 }
