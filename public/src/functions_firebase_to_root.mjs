02eb759e55034ff2deb3579fd7c9b25ab8798773
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 22:03:31 -0400

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/functions_firebase_to_root.mjs
+++ public/src/functions_firebase_to_root.mjs
@@ -1,5 +1,4 @@
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
-import { functions_paths } from "../../../love/public/src/functions_paths.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -8,7 +7,6 @@ export async function functions_firebase_to_root() {
   let f_names = await functions_names();
   async function lambda(f_name) {
     let search = await function_name_to_path_search(f_name);
-    let joined = await functions_paths();
   }
   await each_async(f_names, lambda);
 }
