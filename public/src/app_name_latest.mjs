0a5f1c9b59a54c0d702e3be89effec9898729076
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:02:39 -0400

    function_auto_path ..\love\public\src\app_name_latest.mjs

---
+++ public/src/app_name_latest.mjs
@@ -0,0 +1,7 @@
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
+export function app_name_latest(name) {
+  let a_name = app_name_prefixed(name);
+  let combined = function_name_combine(a_name, "staging");
+  return combined;
+}
