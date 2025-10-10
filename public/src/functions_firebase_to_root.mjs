f5047c8b12960f4c2e91f013e3c50e36d7da9f51
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 22:22:27 -0400

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/functions_firebase_to_root.mjs
+++ public/src/functions_firebase_to_root.mjs
@@ -1,4 +1,4 @@
-import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
+import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
 import { function_name_to_base } from "../../../love/public/src/function_name_to_base.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_join_slash_forward } from "../../../love/public/src/list_join_slash_forward.mjs";
@@ -22,7 +22,7 @@ export async function functions_firebase_to_root() {
     list_add_multiple(sliced, [js, f_name_ext]);
     let joined = list_join_slash_forward(sliced);
     log(joined);
-    let result = await file_overwrite(file_path, contents);
+    let result = await file_copy_overwrite(file_path, contents);
   }
   await each_async(f_names, lambda);
 }
