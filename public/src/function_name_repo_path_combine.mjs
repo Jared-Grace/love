32984222035f9a593c643e88efc6c0ae50994ff6
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:29:56 -0400

    function_auto_path ..\love\public\src\function_name_repo_path_combine.mjs

--- public/src/function_name_repo_path_combine.mjs
+++ public/src/function_name_repo_path_combine.mjs
@@ -1,10 +1,13 @@
-import { log } from "../../../love/public/src/log.mjs";
+import { log_json } from "../../../love/public/src/log_json.mjs";
 import { repo_path_combine } from "./repo_path_combine.mjs";
 import { object_property_get } from "./object_property_get.mjs";
 import { function_name_to_path_search } from "./function_name_to_path_search.mjs";
 export async function function_name_repo_path_combine(f_name, file_path) {
   let search = await function_name_to_path_search(f_name);
-  log(message);
+  log_json({
+    f_name,
+    file_path,
+  });
   let repo_name = object_property_get(search, "repo_name");
   let joined = repo_path_combine(repo_name, file_path);
   return joined;
