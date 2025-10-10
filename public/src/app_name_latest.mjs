a6213b49db8b054bf1704d0b1f19534f32142dee
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:02:40 -0400

    data_file_update ..\love\public\src\app_name_latest.mjs

--- public/src/app_name_latest.mjs
+++ public/src/app_name_latest.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 export function app_name_latest(name) {
+  marker("1");
   let a_name = app_name_prefixed(name);
   let combined = function_name_combine(a_name, "staging");
   return combined;
