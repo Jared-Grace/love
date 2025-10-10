37e0c1eb5b6c530f6be5b62d9a82a4d4ab9d8c66
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:48:20 -0400

    function_auto_path ..\love\public\src\app_new_fn_staging.mjs

---
+++ public/src/app_new_fn_staging.mjs
@@ -0,0 +1,6 @@
+import { app_name_staging } from "../../../love/public/src/app_name_staging.mjs";
+import { function_new } from "../../../love/public/src/function_new.mjs";
+export async function app_new_fn_staging(name) {
+  let combined = app_name_staging(name);
+  await function_new(combined);
+}
