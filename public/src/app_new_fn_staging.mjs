7ad804751cd98fa87b3d2ea96e038c768d4db1ae
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:02:52 -0400

    data_file_update ..\love\public\src\app_new_fn_latest.mjs

--- public/src/app_new_fn_staging.mjs
+++
@@ -1,8 +0,0 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-import { app_name_staging } from "../../../love/public/src/app_name_staging.mjs";
-import { function_new } from "../../../love/public/src/function_new.mjs";
-export async function app_new_fn_staging(name) {
-  marker("1");
-  let combined = app_name_staging(name);
-  await function_new(combined);
-}
