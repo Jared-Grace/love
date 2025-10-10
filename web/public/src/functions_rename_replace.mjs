281ff82d45a84f18934ab969e140bbbeea9a1a8c
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:57:53 -0400

    function_auto_path ..\love\public\src\functions_rename_replace.mjs

--- web/public/src/functions_rename_replace.mjs
+++ web/public/src/functions_rename_replace.mjs
@@ -5,7 +5,7 @@ import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function functions_rename_replace(from) {
+export async function functions_rename_replace(from, to) {
   marker("1");
   let f_names = await functions_names();
   async function lambda(f_name) {
@@ -15,7 +15,7 @@ export async function functions_rename_replace(from) {
     if (n) {
       return;
     }
-    let v = await function_rename_replace(f_name_before, from, to);
+    let v = await function_rename_replace(f_name, from, to);
   }
   await each_async(f_names, lambda);
 }
