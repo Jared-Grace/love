cbc8516731b506840c84f14603c65123d2edbd2c
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:41:54 -0400

    function_auto_path ..\love\public\src\list_slice_end.mjs

--- public/src/list_slice_end.mjs
+++ public/src/list_slice_end.mjs
@@ -1,11 +1,11 @@
 import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-export function list_slice_end(prompts, offset) {
-  let end = list_size(prompts);
+export function list_slice_end(list, offset) {
+  let end = list_size(list);
   let start = end - offset;
   if (start < 0) {
     start = 0;
   }
-  let result = list_slice(prompts, start, end);
+  let result = list_slice(list, start, end);
   return result;
 }
