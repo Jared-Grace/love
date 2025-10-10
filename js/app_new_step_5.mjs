4fd9c8f756c81a8461db46e6af2ccba73ec297c6
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:06:22 -0400

    function_auto_path ..\love\public\src\app_new_fn_latest.mjs

--- js/app_new_step_5.mjs
+++ js/app_new_step_5.mjs
@@ -1,9 +1,9 @@
 import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
-import { app_name_staging } from "../../../love/public/src/app_name_staging.mjs";
+import { app_name_latest } from "../../../love/public/src/app_name_latest.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_new_step_3_generic } from "../../../love/public/src/app_new_step_3_generic.mjs";
 export async function app_new_step_5(name) {
   marker("1");
-  let combined = app_name_staging(name);
+  let combined = app_name_latest(name);
   await app_new_step_3_generic(name, app_main_latest, combined);
 }
--- public/src/app_new_latest_update.mjs
+++ public/src/app_new_latest_update.mjs
@@ -1,9 +1,9 @@
 import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
-import { app_name_staging } from "../../../love/public/src/app_name_staging.mjs";
+import { app_name_latest } from "../../../love/public/src/app_name_latest.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_new_update_generic } from "../../../love/public/src/app_new_update_generic.mjs";
 export async function app_new_latest_update(name) {
   marker("1");
-  let combined = app_name_staging(name);
+  let combined = app_name_latest(name);
   await app_new_update_generic(name, app_main_latest, combined);
 }
