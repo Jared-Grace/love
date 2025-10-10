0d0ab4246935c80d9e500faa6419f16c7016bc2a
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:39:13 -0400

    o au

--- web/public/src/app_update.mjs
+++ web/public/src/app_update.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { app_new_html_update } from "../../../love/public/src/app_new_html_update.mjs";
 import { app_new_latest_update } from "../../../love/public/src/app_new_latest_update.mjs";
 import { app_new_production_update } from "../../../love/public/src/app_new_production_update.mjs";
 export async function app_update(name) {
+  marker("1");
   await app_new_production_update(name);
   await app_new_latest_update(name);
   await app_new_html_update(name);
