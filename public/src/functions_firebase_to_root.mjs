42f7931b095c26de80a6301a26418bfc228c483d
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 22:17:26 -0400

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/functions_firebase_to_root.mjs
+++ public/src/functions_firebase_to_root.mjs
@@ -1,3 +1,4 @@
+import { list_join_slash_forward } from "../../../love/public/src/list_join_slash_forward.mjs";
 import { folder_js } from "../../../love/public/src/folder_js.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_slice } from "../../../love/public/src/list_slice.mjs";
@@ -16,6 +17,7 @@ export async function functions_firebase_to_root() {
     let sliced = list_slice(split, 0, 2);
     let js = folder_js();
     list_add(sliced, js);
+    let joined = list_join_slash_forward(list);
     log(sliced);
   }
   await each_async(f_names, lambda);
