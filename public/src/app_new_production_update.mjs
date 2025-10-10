5b2cf42d900f7e2d2adfb77dedfbc3051606d267
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:45:12 -0400

    data_file_update ..\love\public\src\app_new_update_generic.mjs

--- public/src/app_new_production_update.mjs
+++ public/src/app_new_production_update.mjs
@@ -1,9 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { app_new_step_3_generic } from "../../../love/public/src/app_new_step_3_generic.mjs";
+import { app_new_update_generic } from "../../../love/public/src/app_new_update_generic.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_new_production_update(name) {
   marker("1");
   let a_name = app_name_prefixed(name);
-  await app_new_step_3_generic(name, app_main_production, a_name);
+  await app_new_update_generic(name, app_main_production, a_name);
 }
