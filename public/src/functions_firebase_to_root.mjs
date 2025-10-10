52bbaa6be740c93282ed86de7005dbcc1daa68cf
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 22:21:03 -0400

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/functions_firebase_to_root.mjs
+++ public/src/functions_firebase_to_root.mjs
@@ -1,7 +1,7 @@
+import { function_name_to_base } from "../../../love/public/src/function_name_to_base.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_join_slash_forward } from "../../../love/public/src/list_join_slash_forward.mjs";
 import { folder_js } from "../../../love/public/src/folder_js.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { string_split_slash_forward } from "../../../love/public/src/string_split_slash_forward.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -17,8 +17,8 @@ export async function functions_firebase_to_root() {
     let split = string_split_slash_forward(f_path);
     let sliced = list_slice(split, 0, 2);
     let js = folder_js();
-    list_add(sliced, js);
-    list_add_multiple(list, items);
+    let f_name_ext = function_name_to_base(f_name);
+    list_add_multiple(list, [sliced, f_name_ext]);
     let joined = list_join_slash_forward(sliced);
     log(joined);
   }
