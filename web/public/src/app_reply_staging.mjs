c3a574c9f8eb36cc58bf08848d1e8302c6d16b9f
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:32:04 -0400

    function_auto_path ..\love\public\src\app_reply_staging.mjs

--- web/public/src/app_reply_staging.mjs
+++ web/public/src/app_reply_staging.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
 export async function app_reply_staging() {
   await app_main_latest("app_reply_main", "jared-grace");
 }
