b1535319bd6fdd79283afdf68fa4f866ac744887
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:36:07 -0400

    function_auto_path ..\love\public\src\app_reply_latest.mjs

--- web/public/src/app_reply.mjs
+++ web/public/src/app_reply.mjs
@@ -1,6 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_reply() {
-  marker("1");
   await app_main_production("app_reply_main", "jared-grace");
 }
--- web/public/src/app_reply_latest.mjs
+++ web/public/src/app_reply_latest.mjs
@@ -1,6 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
 export async function app_reply_latest() {
-  marker("1");
   await app_main_latest("app_reply_main", "jared-grace");
 }
