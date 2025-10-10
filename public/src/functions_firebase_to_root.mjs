6cd5cc8eeee987866154a5bfe9cfe9b47e68feb4
Grace-Jared <christrosetolife@gmail.com>
2025-10-09 21:57:11 -0400

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/functions_firebase_to_root.mjs
+++ public/src/functions_firebase_to_root.mjs
@@ -2,5 +2,6 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_firebase_to_root() {
   marker("1");
-  await each_async(list, async function lambda(item) {});
+  async function lambda(item) {}
+  await each_async(list, lambda);
 }
