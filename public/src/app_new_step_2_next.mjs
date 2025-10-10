e8dc52e3984ddbd2d9c6feddcc96307702773934
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:48:00 -0400

    r app_new_step_2 app_new_fn_main

--- public/src/app_new_step_2_next.mjs
+++ public/src/app_new_step_2_next.mjs
@@ -1,12 +1,12 @@
 import { app_new_step_4 } from "../../../love/public/src/app_new_step_4.mjs";
 import { app_new_staging_update } from "../../../love/public/src/app_new_staging_update.mjs";
-import { app_new_step_2 } from "../../../love/public/src/app_new_step_2.mjs";
+import { app_new_fn_main } from "../../../love/public/src/app_new_fn_main.mjs";
 import { app_new_production_update } from "../../../love/public/src/app_new_production_update.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_new } from "../../../love/public/src/html_new.mjs";
 export async function app_new_step_2_next(name) {
   marker("1");
-  await app_new_step_2(name);
+  await app_new_fn_main(name);
   await app_new_step_4(name);
   await app_new_production_update(name);
   await app_new_staging_update(name);
