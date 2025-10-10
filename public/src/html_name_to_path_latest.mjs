e4b428577dd43da1d4f648bea1bb45233760c290
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 15:35:55 -0400

    function_auto_path ..\love\public\src\html_name_to_path_latest.mjs

--- public/src/html_name_to_path_latest.mjs
+++ public/src/html_name_to_path_latest.mjs
@@ -2,7 +2,7 @@ import { html_name_to_path_folder } from "../../../love/public/src/html_name_to_
 import { marker } from "../../../love/public/src/marker.mjs";
 export function html_name_to_path_latest(name) {
   marker("1");
-  const folder = "staging";
+  const folder = "latest";
   let file_path = html_name_to_path_folder(name, folder);
   return file_path;
 }
