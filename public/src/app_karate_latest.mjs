8e2dc0b3f094ef8ef287b8d1b5e1b4055e151d03
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:59:49 -0400

    data_file_update ..\love\public\src\app_karate_latest.mjs

--- public/src/app_karate_latest.mjs
+++ public/src/app_karate_latest.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { firebase_deploy_function_destination_latest } from "../../../love/public/src/firebase_deploy_function_destination_latest.mjs";
 import { app_main } from "../../../love/public/src/app_main.mjs";
 export async function app_karate_latest() {
+  marker("1");
   await app_main(
     "app_karate_main",
     "karate-code",
