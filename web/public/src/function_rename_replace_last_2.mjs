66f1cbc46d69510ddf8df2fe30195ffe99529f40
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:41:13 -0400

    function_auto_path ..\love\public\src\function_rename_replace_last_2.mjs

---
+++ web/public/src/function_rename_replace_last_2.mjs
@@ -0,0 +1,9 @@
+import { function_name_to_part_last } from "../../../love/public/src/function_name_to_part_last.mjs";
+import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_rename_replace_last_2(f_name_before, to) {
+  marker("1");
+  let from = function_name_to_part_last(f_name_before);
+  let v = await function_rename_replace(f_name_before, from, to);
+  return v;
+}
