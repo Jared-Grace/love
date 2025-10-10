f7389c41a6417ddd62dd211be1ee8bfcfa60f773
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:41:48 -0400

    function_auto_path ..\love\public\src\function_rename_replace_last_2.mjs

--- public/src/function_rename_replace_last_2.mjs
+++ public/src/function_rename_replace_last_2.mjs
@@ -1,3 +1,4 @@
+import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
@@ -5,6 +6,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_replace_last_2(f_name_before, to) {
   marker("1");
   let parts = function_name_to_parts(f_name_before);
+  let result = list_slice_end(prompts, offset);
   let from = list_last(parts);
   let v = await function_rename_replace(f_name_before, from, to);
   return v;
