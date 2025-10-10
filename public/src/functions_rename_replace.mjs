904c71f1f310b5a04ea356e45943966ffa45a5a1
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 14:55:16 -0400

    function_auto_path ..\love\public\src\functions_rename_replace.mjs

--- public/src/functions_rename_replace.mjs
+++ public/src/functions_rename_replace.mjs
@@ -1,4 +1,6 @@
+import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function functions_rename_replace() {
+export async function functions_rename_replace() {
   marker("1");
+  let v = await function_rename_replace(f_name_before, from, to);
 }
