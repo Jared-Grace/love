23834455f12670286326bb9fdaa9c2c939ea88b1
Grace-Jared <christrosetolife@gmail.com>
2025-10-11 15:18:05 -0400

    function_auto_path ..\love\public\src\app_message_download.mjs

--- public/src/app_message_download.mjs
+++ public/src/app_message_download.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { firebase_storage_download } from "../../../love/public/src/firebase_storage_download.mjs";
 import { app_message_firebase_path } from "../../../love/public/src/app_message_firebase_path.mjs";
@@ -10,10 +11,12 @@ export async function app_message_download() {
   const [files] = await bucket.getFiles({
     prefix: app_message_firebase_path(),
   });
-  async function lambda(item) {}
+  async function lambda(item) {
+    let name2 = object_property_get(item, "name");
+    let buffer = await firebase_storage_download(destination);
+  }
   await each_async(list, lambda);
   for (const file of files) {
     console.log(file.name);
-    let buffer = await firebase_storage_download(destination);
   }
 }
