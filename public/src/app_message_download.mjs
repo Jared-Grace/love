b665b621f83d71f64da3aff7aa42d0974a31d21c
Grace-Jared <christrosetolife@gmail.com>
2025-10-11 15:17:19 -0400

    function_auto_path ..\love\public\src\app_message_download.mjs

--- public/src/app_message_download.mjs
+++ public/src/app_message_download.mjs
@@ -1,3 +1,4 @@
+import { app_message_firebase_path } from "../../../love/public/src/app_message_firebase_path.mjs";
 import { firebase_bucket } from "../../../love/public/src/firebase_bucket.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,7 +6,7 @@ export async function app_message_download() {
   marker("1");
   const bucket = await firebase_bucket();
   const [files] = await bucket.getFiles({
-    prefix: "folder-name/",
+    prefix: app_message_firebase_path(),
   });
   for (const file of files) {
     console.log(file.name);
