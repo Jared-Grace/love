9ed082bf1fb54a02d0ce91bfa61ae1aff9a83e57
Grace-Jared <christrosetolife@gmail.com>
2025-10-10 23:39:01 -0400

    function_auto_path ..\love\public\src\app_promote.mjs

--- public/src/app_promote.mjs
+++ public/src/app_promote.mjs
@@ -1,10 +1,9 @@
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
-import { app_karate_main } from "../../../karate_code/public/src/app_karate_main.mjs";
 import { firebase_promote_function } from "../../../love/public/src/firebase_promote_function.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_promote() {
+export async function app_promote(name) {
   marker("1");
-  let combined = app_name_main(a);
-  let v = await firebase_promote_function(app_karate_main.name);
+  let combined = app_name_main(name);
+  let v = await firebase_promote_function(combined);
   return v;
 }
