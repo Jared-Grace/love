2cbee9bc6ddd02e011509110e07069a3c479deda
Grace-Jared <christrosetolife@gmail.com>
2025-10-11 13:40:17 -0400

    function_auto_path ..\love\public\src\app_message_download.mjs

--- public/src/app_message_download.mjs
+++ public/src/app_message_download.mjs
@@ -1,4 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_message_download() {
+export async function app_message_download() {
   marker("1");
+  const [files] = await bucket.getFiles({
+    prefix: "folder-name/",
+  });
+  for (const file of files) {
+    console.log(file.name);
+  }
 }
