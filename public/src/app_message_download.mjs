623eb76758bfea53e2e3edc83297ca3e2c06102e
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 19:30:58 -0400

    function_auto_path ..\love\public\src\app_message_download.mjs

--- public/src/app_message_download.mjs
+++ public/src/app_message_download.mjs
@@ -1,10 +1,10 @@
+import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { firebase_storage_download } from "../../../love/public/src/firebase_storage_download.mjs";
 import { app_message_firebase_path } from "../../../love/public/src/app_message_firebase_path.mjs";
 import { firebase_bucket } from "../../../love/public/src/firebase_bucket.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_message_download() {
   marker("1");
@@ -17,7 +17,7 @@ export async function app_message_download() {
     let buffer = await firebase_storage_download(name2);
     let s = buffer_string_to(buffer);
     let json = json_to(s);
-    console.log(json);
+    let code = js_unparse(ast);
   }
   let downloads = await list_map_async(files, lambda);
   return downloads;
