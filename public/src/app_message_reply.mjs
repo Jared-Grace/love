25f9341304bb9697492e208d8626fac8c1ec8b5a
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 19:44:18 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -13,7 +13,7 @@ export async function app_message_reply() {
   let result = await http_local_text(
     "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json",
   );
-  let v = json_from(json);
+  let v = json_from(result);
   marker("1");
-  return result;
+  return v;
 }
