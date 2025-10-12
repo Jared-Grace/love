459be55b9e2802ee4508be2776f9ee3f333f3f1a
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 19:29:42 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -1,6 +1,8 @@
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_message_download } from "../../../love/public/src/app_message_download.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_message_reply() {
   let downloads = await app_message_download();
+  let mapped = list_map_property(list, property_name);
   marker("1");
 }
