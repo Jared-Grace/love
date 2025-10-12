bbe36673f52e224919116bf5947be5f1de8a0b02
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 19:37:16 -0400

    fws a auto

--- web/public/src/function_alias_add_auto.mjs
+++ web/public/src/function_alias_add_auto.mjs
@@ -1,1 +1,6 @@
-export function function_alias_add_auto() {}
+import { function_alias_add } from "../../../love/public/src/function_alias_add.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_alias_add_auto(first, second) {
+  marker("1");
+  return await function_alias_add(first, second);
+}
