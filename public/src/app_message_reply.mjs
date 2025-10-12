1ac0f00be35d5447844628194f07f82d538897a6
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 19:43:03 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -1,3 +1,4 @@
+import { http_local } from "../../../love/public/src/http_local.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
@@ -8,6 +9,7 @@ export async function app_message_reply() {
   let mapped = list_map_property(downloads, "message");
   let first = list_first(mapped);
   let lower = string_lower_to(first);
+  let result = await http_local(url);
   marker("1");
   return first;
 }
