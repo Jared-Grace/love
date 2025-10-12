7e137dd6ae48c6864ed44d2bab619c558f914fc7
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 19:37:47 -0400

    function_auto_path ..\love\public\src\function_alias_add_auto.mjs

--- public/src/function_alias_add_auto.mjs
+++ public/src/function_alias_add_auto.mjs
@@ -3,7 +3,7 @@ import { function_alias_add } from "../../../love/public/src/function_alias_add.
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_alias_add_auto(f_name) {
   marker("1");
-  let acronym = function_name_to_acronym(f_name2);
-  let v = await function_alias_add(first, f_name);
+  let acronym = function_name_to_acronym(f_name);
+  let v = await function_alias_add(acronym, f_name);
   return v;
 }
