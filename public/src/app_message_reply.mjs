386305f16e94b2f7060efb38fd0c233b39b45875
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 17:57:59 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -1,4 +1,4 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_response_how_r_u.mjs";
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -31,7 +31,7 @@ export async function app_message_reply() {
       let outputs = object_property_get(a, "outputs");
       let item = app_reply_response_how_r_u();
       list_add(outputs, item);
-      object_property_set(a, "success", true);
+      object_property_set_exists_not(a, "success", true);
     }
     let fn = reply_sequence(["how", "are", "you"]);
     let r = reply_on_match(fn, lambda);
