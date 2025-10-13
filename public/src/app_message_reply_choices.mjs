80442664a9853475ebc990d27e09ba3a066eb41e
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 18:52:28 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -1,4 +1,3 @@
-import { function_rename_suffix_add } from "../../../love/public/src/function_rename_suffix_add.mjs";
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
@@ -6,7 +5,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_response_how_r_u.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { reply_choice } from "./reply_choice.mjs";
-export async function app_message_reply_choices() {
+export function app_message_reply_choices() {
   async function lambda(a) {
     let outputs = object_property_get(a, "outputs");
     let item = app_reply_response_how_r_u();
@@ -14,7 +13,7 @@ export async function app_message_reply_choices() {
     object_property_set(a, "success", true);
   }
   let fn = reply_sequence(["how", "are", "you"]);
-  let v = await function_rename_suffix_add(f_name_before, suffix);
+  let fn3 = reply_sequence(sequence);
   let fn2 = reply_choice([fn]);
   let r = reply_on_match(fn, lambda);
   return r;
