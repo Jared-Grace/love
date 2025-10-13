3abb401b22854fc9560ccd6ad24e10ad6baa0ac5
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 20:58:06 -0400

    function_auto_path ..\love\public\src\string_chunk.mjs

--- public/src/string_chunk.mjs
+++ public/src/string_chunk.mjs
@@ -4,7 +4,7 @@ import { each_range_from } from "../../../love/public/src/each_range_from.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 import { string_slice } from "../../../love/public/src/string_slice.mjs";
-export function string_chunk(input, dictionary, index_left, result) {
+export function string_chunk(input, dictionary, index_left) {
   marker("1");
   let index_last = string_size(input);
   function lambda3(index_right) {
