2a3389bfa0d60f1720622abce5f72f7c8a4f0756
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 21:11:19 -0400

    data_file_update ..\love\public\src\string_tokens.mjs

--- public/src/string_tokens.mjs
+++ public/src/string_tokens.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { string_chunk_recursive } from "../../../love/public/src/string_chunk_recursive.mjs";
 export function string_tokens(input, dictionary) {
+  marker("1");
   let index_left = 0;
   let result = [];
   let current = [];
