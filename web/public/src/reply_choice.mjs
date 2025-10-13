46f59c64f1b901e8a5bc2c73572f21604c3c85fd
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 18:47:58 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- web/public/src/reply_choice.mjs
+++ web/public/src/reply_choice.mjs
@@ -1,22 +1,10 @@
-import { each_index } from "../../../love/public/src/each_index.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
-import { list_size_less_than } from "../../../love/public/src/list_size_less_than.mjs";
+import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function reply_choice() {
+export function reply_choice(choices) {
   let fn = function reply_sequence_matches(a) {
     let tokens = object_property_get(a, "tokens");
     let matches = true;
-    if (list_size_less_than(tokens, sequence)) {
-      matches = false;
-    } else {
-      function lambda(sequence_item, index) {
-        let token = list_get(tokens, index);
-        if (sequence_item !== token) {
-          matches = false;
-        }
-      }
-      each_index(sequence, lambda);
-    }
+    each(list, function lambda(item) {});
     return matches;
   };
   return fn;
