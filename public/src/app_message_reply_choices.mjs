0336b80e103f1dc5a60e49dc2e4265f7b4edd4ac
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 19:01:43 -0400

    o app_message_reply_choices

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -1,3 +1,4 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
@@ -6,6 +7,7 @@ import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_r
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { reply_choice } from "./reply_choice.mjs";
 export function app_message_reply_choices() {
+  marker("1");
   let item = app_reply_response_how_r_u();
   function lambda(a) {
     let outputs = object_property_get(a, "outputs");
