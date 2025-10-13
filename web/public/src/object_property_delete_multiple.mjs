ec54f3ff6f0519699375cf29f7c6033c4bdd4d5e
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 21:08:09 -0400

    function_auto_path ..\love\public\src\object_property_delete_multiple.mjs

--- web/public/src/object_property_delete_multiple.mjs
+++ web/public/src/object_property_delete_multiple.mjs
@@ -1,6 +1,6 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
-export function object_property_delete_multiple(o) {
+export function object_property_delete_multiple(o, list) {
   function lambda(item) {
     object_property_delete(o, item);
   }
