28f8cb30aec35d39a59e6792ca4785d0b33b9f43
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 18:51:09 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- web/public/src/reply_choice.mjs
+++ web/public/src/reply_choice.mjs
@@ -1,8 +1,6 @@
 import { each } from "../../../love/public/src/each.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function reply_choice(choices) {
   let fn = function reply_sequence_matches(a) {
-    let tokens = object_property_get(a, "tokens");
     let matches = false;
     function lambda(choice) {
       matches = choice(a);
