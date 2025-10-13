8c0a9441a0acda1429934d3c0ba1c1a345cea263
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 21:11:31 -0400

    rr string_chunk_recursive chunk tokens

--- web/public/src/string_tokens.mjs
+++ web/public/src/string_tokens.mjs
@@ -1,10 +1,10 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { string_chunk_recursive } from "../../../love/public/src/string_chunk_recursive.mjs";
+import { string_tokens_recursive } from "../../../love/public/src/string_tokens_recursive.mjs";
 export function string_tokens(input, dictionary) {
   marker("1");
   let index_left = 0;
   let result = [];
   let current = [];
-  string_chunk_recursive(input, dictionary, index_left, result, current);
+  string_tokens_recursive(input, dictionary, index_left, result, current);
   return result;
 }
