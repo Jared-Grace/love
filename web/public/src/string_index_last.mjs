daff8c5798f31c1b76a26f873d00580ba0e0048a
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 20:51:19 -0400

    cr list_index_last list string

--- web/public/src/string_index_last.mjs
+++ web/public/src/string_index_last.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function string_index_last(list) {
+  marker("1");
   const index_last = list_size(list) - 1;
   return index_last;
 }
