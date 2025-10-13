a2df181977c1fa51e8e6672fa51e7918676208f3
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 19:09:37 -0400

    function_auto_path ..\love\public\src\reply_on_match_output.mjs

--- public/src/reply_on_match_output.mjs
+++ public/src/reply_on_match_output.mjs
@@ -1,9 +1,11 @@
+import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function reply_on_match_output(item) {
-  let fn = function lambda(a) {
+  function lambda(a) {
     let outputs = object_property_get(a, "outputs");
     list_add(outputs, item);
-  };
+  }
+  let fn = reply_on_match(fn2, lambda);
   return fn;
 }
