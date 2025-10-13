25699135d534800e8388aa4d54f4b742a8cb1635
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 18:01:38 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

---
+++ public/src/app_message_reply_choices.mjs
@@ -0,0 +1,17 @@
+import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
+import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_response_how_r_u.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export function app_message_reply_choices() {
+  async function lambda(a) {
+    let outputs = object_property_get(a, "outputs");
+    let item = app_reply_response_how_r_u();
+    list_add(outputs, item);
+    object_property_set(a, "success", true);
+  }
+  let fn = reply_sequence(["how", "are", "you"]);
+  let r = reply_on_match(fn, lambda);
+  return r;
+}
