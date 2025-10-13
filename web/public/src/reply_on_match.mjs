3214229adbe084e28555be22f35cb631ec7a98b8
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 18:00:14 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- web/public/src/reply_on_match.mjs
+++ web/public/src/reply_on_match.mjs
@@ -1,10 +1,6 @@
-import { log } from "../../../love/public/src/log.mjs";
 export function reply_on_match(fn, on_match) {
   let matcher = function reply_on_match_inner(a) {
     let matches = fn(a);
-    log({
-      matches,
-    });
     if (matches) {
       on_match(a);
     }
