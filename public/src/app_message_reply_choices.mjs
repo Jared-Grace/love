9f35169fb80ca0fc4781af9b12493ef3f5002d73
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 19:03:48 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -1,22 +1,12 @@
+import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_response_how_r_u.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { reply_choice } from "./reply_choice.mjs";
 export function app_message_reply_choices() {
   marker("1");
   let item = app_reply_response_how_r_u();
-  function reply_on_match_output(item) {
-    let v2 = function lambda(a) {
-      let outputs = object_property_get(a, "outputs");
-      list_add(outputs, item);
-      object_property_set(a, "success", true);
-    };
-    return v2;
-  }
   let fn = reply_sequence(["how", "are", "you"]);
   let fn3 = reply_sequence(["hi"]);
   let v = reply_on_match_output(fn);
