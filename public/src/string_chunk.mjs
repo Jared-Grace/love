c9556fe76644fc8783784257c70bab75589a7a32
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 20:55:56 -0400

    function_auto_path ..\love\public\src\string_chunk.mjs

--- public/src/string_chunk.mjs
+++ public/src/string_chunk.mjs
@@ -4,7 +4,7 @@ import { log } from "../../../love/public/src/log.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 import { string_slice } from "../../../love/public/src/string_slice.mjs";
 export function string_chunk(input, dictionary, index_left) {
-  let index_last = string_size(lower);
+  let index_last = string_size(input);
   function lambda3(index_right) {
     let sliced = string_slice(input, index_left, index_right);
     let exists = object_property_exists(dictionary, sliced);
