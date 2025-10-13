1182bbb3ee5bb5e13d583ddbc25cbf1afe1b1b9a
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 17:56:52 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- web/public/src/app_message_reply.mjs
+++ web/public/src/app_message_reply.mjs
@@ -27,7 +27,8 @@ export async function app_message_reply() {
   let excludes = ["h", "w", "e", "wa", "ey", "ar", "ware", "re"];
   object_property_delete_multiple(o, excludes);
   let tokens_matches = string_tokens(lower, o);
-  each(list, function lambda2(item2) {});
+  function lambda2(item2) {}
+  each(list, lambda2);
   let tokens = list_last(tokens_matches);
   async function lambda(a) {
     let outputs = object_property_get(a, "outputs");
