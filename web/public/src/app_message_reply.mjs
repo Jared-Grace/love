2778d6fd8f4432ee7842f32f4f0716d5d1cae12d
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 21:00:11 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- web/public/src/app_message_reply.mjs
+++ web/public/src/app_message_reply.mjs
@@ -18,8 +18,8 @@ export async function app_message_reply() {
   let o = json_from(text);
   let properties = object_properties(o);
   let index_left = 0;
-  let chunk = [];
+  let result = [];
   let chunks = [];
-  string_chunk(lower, o, index_left, chunk, chunks);
+  string_chunk(lower, o, index_left, result, chunks);
   marker("1");
 }
