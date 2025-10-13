893a3e78dfd593b0962b9584e5d40563a244f3f1
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 19:04:35 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- web/public/src/app_message_reply_choices.mjs
+++ web/public/src/app_message_reply_choices.mjs
@@ -1,6 +1,5 @@
 import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_response_how_r_u.mjs";
 import { reply_choice } from "./reply_choice.mjs";
@@ -9,8 +8,7 @@ export function app_message_reply_choices() {
   let item = app_reply_response_how_r_u();
   let fn = reply_sequence(["how", "are", "you"]);
   let fn3 = reply_sequence(["hi"]);
-  let v = reply_on_match_output(fn);
   let fn2 = reply_choice([v, fn3]);
-  let r = reply_on_match(fn2, lambda);
+  let r = reply_on_match_output(fn2, item);
   return r;
 }
