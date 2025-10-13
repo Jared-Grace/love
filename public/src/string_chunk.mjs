87fa25d9e4063e790e10559582bfa2f7627fd309
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 20:55:42 -0400

    function_auto_path ..\love\public\src\string_chunk.mjs

--- public/src/string_chunk.mjs
+++ public/src/string_chunk.mjs
@@ -9,7 +9,7 @@ export function string_chunk(input, dictionary, index_left) {
     let sliced = string_slice(input, index_left, index_right);
     let exists = object_property_exists(dictionary, sliced);
     if (exists) {
-      string_chunk(input, dictionary, index_last);
+      string_chunk(input, dictionary, index_right);
       log({
         sliced,
       });
