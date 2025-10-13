0d8c43d3c5b52379605653b2fc19daef834cb932
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 18:07:02 -0400

    function_auto_path ..\love\public\src\function_dependency_path.mjs

--- public/src/function_dependency_path.mjs
+++ public/src/function_dependency_path.mjs
@@ -1,11 +1,10 @@
+import { list_size_less_than } from "../../../love/public/src/list_size_less_than.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { less_than_by } from "../../../love/public/src/less_than_by.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_exists_assert } from "../../../love/public/src/function_exists_assert.mjs";
 import { visit_unique_async } from "../../../love/public/src/visit_unique_async.mjs";
 import { function_imports } from "../../../love/public/src/function_imports.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
 export async function function_dependency_path(f_name_from, f_name_to) {
   let result = null;
   marker("1");
@@ -16,7 +15,7 @@ export async function function_dependency_path(f_name_from, f_name_to) {
     let { node } = v;
     if (node === to) {
       let { stack } = v;
-      if (result === null || less_than_by(stack, result, list_size)) {
+      if (result === null || list_size_less_than(stack, result)) {
         result = stack;
       }
     }
