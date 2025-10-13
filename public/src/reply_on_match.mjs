f4c79a5fdfecb858325c32f785be317e1ada2ab5
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 17:56:46 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,12 +1,12 @@
 import { log } from "../../../love/public/src/log.mjs";
 export function reply_on_match(fn, on_match) {
-  let matcher = async function reply_on_match_inner(a) {
+  let matcher = function reply_on_match_inner(a) {
     let matches = fn(a);
     log({
       matches,
     });
     if (matches) {
-      await on_match(a);
+      on_match(a);
     }
   };
   return matcher;
