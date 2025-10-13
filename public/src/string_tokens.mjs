4455534441af9c0ad0bda621ec3958ee9b34da0e
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 21:11:18 -0400

    function_auto_path ..\love\public\src\string_tokens.mjs

---
+++ public/src/string_tokens.mjs
@@ -0,0 +1,8 @@
+import { string_chunk_recursive } from "../../../love/public/src/string_chunk_recursive.mjs";
+export function string_tokens(input, dictionary) {
+  let index_left = 0;
+  let result = [];
+  let current = [];
+  string_chunk_recursive(input, dictionary, index_left, result, current);
+  return result;
+}
