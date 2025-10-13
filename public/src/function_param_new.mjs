adced9b8c2f5650091281f3584e6cee5635c99e1
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 20:57:29 -0400

    function_auto_path ..\love\public\src\function_param_new.mjs

--- public/src/function_param_new.mjs
+++ public/src/function_param_new.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
 export async function function_param_new(param_name, default_value) {
+  assert_arguments(arguments, 2);
   marker("1");
   await function_params_new(param_name, default_value);
 }
