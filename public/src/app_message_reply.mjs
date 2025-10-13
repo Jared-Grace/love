4ca2f8a618770c30725db1c1099d6a6eeba3e549
Grace-Jared <christrosetolife@gmail.com>
2025-10-12 21:09:27 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -17,7 +17,7 @@ export async function app_message_reply() {
     "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json",
   );
   let o = json_from(text);
-  let excludes = ["h", "w", "e", "wa", "ey", "ar"];
+  let excludes = ["h", "w", "e", "wa", "ey", "ar", "ware"];
   object_property_delete_multiple(o, excludes);
   let properties = object_properties(o);
   let index_left = 0;
