c777d002836adc87d38c266e55ca5ab6a03e95e9
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 19:53:46 -0400

    function_auto_path ..\love\public\src\reply_last.mjs

--- public/src/reply_last.mjs
+++ public/src/reply_last.mjs
@@ -1,7 +1,7 @@
 import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_last() {
-  let value = global_function_initialize(reply_last, {});
+  let last = global_function_initialize(reply_last, {});
   marker("1");
-  return value;
+  return last;
 }
