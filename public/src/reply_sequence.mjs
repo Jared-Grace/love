a2a726ae742d6341d7e51f450f47129a4a890c1d
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 20:28:58 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -1,3 +1,4 @@
+import { function_is } from "../../../love/public/src/function_is.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_size_less_than } from "../../../love/public/src/list_size_less_than.mjs";
@@ -13,7 +14,7 @@ export function reply_sequence(sequence) {
     } else {
       function lambda(sequence_item, index) {
         let token = list_get(tokens, index);
-        fi;
+        let fi = function_is(f);
         if (not(a2)) {
         }
         if (sequence_item !== token) {
