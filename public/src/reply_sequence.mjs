2297debbb5d788537a372ce2f269ba54877fa041
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 15:18:57 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -7,36 +7,37 @@ import { list_get } from "../../../love/public/src/list_get.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 export function reply_sequence(sequence) {
   let fn = function reply_sequence_matches(a) {
-    function lambda2(ai) {}
-    each(a, lambda2);
-    let index_start = object_property_get(a, "index");
-    let tokens = object_property_get(a, "tokens");
-    let matches = true;
-    let sequence_size = list_size(sequence);
-    if (list_size(tokens) - index_start < sequence_size) {
-      matches = false;
-    } else {
-      function lambda(sequence_item, index) {
-        let token = list_get(tokens, index + index_start);
-        let fi = function_is(sequence_item);
-        if (fi) {
-          let r = sequence_item(a);
+    function lambda2(ai) {
+      let index_start = object_property_get(a, "index");
+      let tokens = object_property_get(a, "tokens");
+      let matches = true;
+      let sequence_size = list_size(sequence);
+      if (list_size(tokens) - index_start < sequence_size) {
+        matches = false;
+      } else {
+        function lambda(sequence_item, index) {
+          let token = list_get(tokens, index + index_start);
+          let fi = function_is(sequence_item);
+          if (fi) {
+            let r = sequence_item(a);
+          }
+          if (sequence_item !== token) {
+            log({
+              sequence_item,
+              token,
+            });
+            matches = false;
+          }
         }
-        if (sequence_item !== token) {
-          log({
-            sequence_item,
-            token,
-          });
-          matches = false;
-        }
+        each_index(sequence, lambda);
       }
-      each_index(sequence, lambda);
+      let r = {
+        matches,
+        index: index_start + sequence_size,
+      };
+      return r;
     }
-    let r = {
-      matches,
-      index: index_start + sequence_size,
-    };
-    return r;
+    each(a, lambda2);
   };
   return fn;
 }
