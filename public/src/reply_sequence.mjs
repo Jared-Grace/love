91b82ad8723f1bd089a3bb4c3ec1a97a4d52cf32
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 15:59:11 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -7,33 +7,33 @@ import { log } from "../../../love/public/src/log.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
-export function reply_sequence(sequence) {
+export function reply_sequence(sequence_fns) {
   let fn = function reply_sequence_matches(a) {
     function lambda2(ai) {
       let index_start = object_property_get(ai, "index");
       let tokens = object_property_get(ai, "tokens");
       let matches = true;
-      let sequence_size = list_size(sequence);
+      let sequence_size = list_size(sequence_fns);
       if (list_size(tokens) - index_start < sequence_size) {
         matches = false;
       } else {
-        function lambda(sequence_item, index) {
-          let fi = function_is(sequence_item);
+        function lambda(sequence_fn, index) {
+          let fi = function_is(sequence_fn);
           if (not(fi)) {
-            sequence_item = function lambda3(a) {
+            sequence_fn = function lambda3(a) {
               let token = list_get(tokens, index + index_start);
-              if (sequence_item !== token) {
+              if (sequence_fn !== token) {
                 log({
-                  sequence_item,
+                  sequence_item: sequence_fn,
                   token,
                 });
                 matches = false;
               }
             };
           }
-          let r = sequence_item(a);
+          let r = sequence_fn(ai);
         }
-        each_index(sequence, lambda);
+        each_index(sequence_fns, lambda);
       }
       let r = {
         matches,
