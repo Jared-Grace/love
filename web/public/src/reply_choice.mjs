7cc2b97a811e17f7ad2bc912c990d85fc5bf5b36
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:16:35 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- web/public/src/reply_choice.mjs
+++ web/public/src/reply_choice.mjs
@@ -1,14 +1,12 @@
+import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function reply_choice(choices) {
-  let fn = function reply_sequence_matches(a) {
+  let fn = function reply_sequence_matches(possbilities) {
     let matches_result = false;
     function lambda(choice) {
-      let { matches } = choice(a);
-      if (matches) {
-        let v = true;
-        matches_result = v;
-        return v;
-      }
+      possbilities = choice(possbilities);
+      let filtered = reply_matches(possbilities);
+      return filtered;
     }
     each(choices, lambda);
     let r = {
