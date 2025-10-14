4903c96e74a9165b1833d81c9bb2c9b7f66d15d8
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:04:51 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

---
+++ public/src/reply_dictionary.mjs
@@ -0,0 +1,13 @@
+import { object_property_delete_multiple } from "../../../love/public/src/object_property_delete_multiple.mjs";
+import { json_from } from "../../../love/public/src/json_from.mjs";
+import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
+export async function reply_dictionary() {
+  let text = await http_local_text(
+    "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json",
+  );
+  let dictionary = json_from(text);
+  let excludes = ["h", "w", "e", "wa", "ey", "ar", "ware", "re"];
+  object_property_delete_multiple(dictionary, excludes);
+  let includes = [];
+  return dictionary;
+}
