c4c80ce5020a23c25fdc609f7bcaaf14f04ff68b
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:14:59 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,8 +1,10 @@
+import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 export function reply_on_match(fn, on_match) {
   let matcher = function reply_on_match_inner(possbilities) {
     possbilities = fn(possbilities);
-    let filtered = list_filter_property(list, property_name, property_value);
+    let filtered = list_filter_property(possbilities, "matches", true);
+    let ne = list_empty_not_is(list);
     if (matches) {
       on_match(possbilities);
     }
