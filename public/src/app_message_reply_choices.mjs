79709ccc44759e69c834c6fff3a9f8e411b8f1ca
Grace-Jared <christrosetolife@gmail.com>
2025-10-13 20:21:44 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -12,6 +12,7 @@ export function app_message_reply_choices() {
   let item = app_reply_response_how_r_u();
   let fn4 = reply_on_match_output(fn, item);
   let fn2 = reply_choice([fn4, fn3]);
+  let fn5 = reply_sequence(sequence);
   function lambda(a) {
     object_property_set(a, "success", true);
   }
