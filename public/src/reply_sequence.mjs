9cd0d6bd5111de7b507af6ac4b526f15525c32dd
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 18:21:13 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { match_wrap_invoke } from "../../../love/public/src/match_wrap_invoke.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
@@ -7,6 +8,7 @@ export function reply_sequence(sequence_fns) {
     list_is_assert(possibilities);
     function lambda(sequence_fn) {
       possibilities = match_wrap_invoke(sequence_fn, possibilities);
+      log(message);
       let e = list_empty_is(possibilities);
       if (e) {
         let v = true;
