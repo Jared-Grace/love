87f3478df170b39106412e4ee537a48ca6914937
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 18:09:58 -0400

    function_auto_path ..\love\public\src\reply_on_match_output.mjs

--- public/src/reply_on_match_output.mjs
+++ public/src/reply_on_match_output.mjs
@@ -1,10 +1,12 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function reply_on_match_output(fn_a, item) {
   assert_arguments(arguments, 2);
-  function lambda(a) {
+  function lambda(possibilities) {
+    each(list, function lambda2(item2) {});
     let outputs = object_property_get(a, "outputs");
     list_add(outputs, item);
   }
