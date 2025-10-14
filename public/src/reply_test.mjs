0d0e4847b920d16c7f5d3b0aef182091bdc361ea
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:29:20 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- public/src/reply_test.mjs
+++ public/src/reply_test.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_messages } from "../../../love/public/src/reply_messages.mjs";
 import { reply_sequence } from "./reply_sequence.mjs";
@@ -9,6 +10,7 @@ export async function reply_test() {
       start: reply_sequence(["a"]),
     },
   ];
+  await each_async(list, async function lambda(item) {});
   let result = await reply_messages(messages, start);
   return result;
 }
