0b73d231c081e926a17f05220d3dc84d08eea79b
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:11:10 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- web/public/src/reply_sequence.mjs
+++ web/public/src/reply_sequence.mjs
@@ -1,7 +1,6 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
 import { function_is } from "../../../love/public/src/function_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
@@ -9,36 +8,23 @@ import { each_index } from "../../../love/public/src/each_index.mjs";
 export function reply_sequence(sequence_fns) {
   let fn = function reply_sequence_matches(a) {
     function lambda2(ai) {
-      let index_start = object_property_get(ai, "index");
-      let tokens = object_property_get(ai, "tokens");
-      let matches = true;
-      let sequence_size = list_size(sequence_fns);
-      if (list_size(tokens) - index_start < sequence_size) {
-        matches = false;
-      } else {
-        function lambda(sequence_fn, index) {
-          let fi = function_is(sequence_fn);
-          if (not(fi)) {
-            sequence_fn = function reply_wrap(a) {
-              let index_start = object_property_get(a, "index");
-              let token = list_get(tokens, index + index_start);
-              matches = sequence_fn === token;
-              let r = {
-                matches,
-                index: index_start + 1,
-              };
-              object_assign(ai, r);
+      function lambda(sequence_fn, index) {
+        let fi = function_is(sequence_fn);
+        if (not(fi)) {
+          sequence_fn = function reply_wrap_inner(a) {
+            let index_start = object_property_get(a, "index");
+            let token = list_get(tokens, index + index_start);
+            let matches = sequence_fn === token;
+            let r = {
+              matches,
+              index: index_start + 1,
             };
-          }
-          ai = sequence_fn(ai);
+            object_assign(ai, r);
+          };
         }
-        each_index(sequence_fns, lambda);
+        ai = sequence_fn(ai);
       }
-      let r = {
-        matches,
-        index: index_start + sequence_size,
-      };
-      object_assign(ai, r);
+      each_index(sequence_fns, lambda);
     }
     each(a, lambda2);
   };
