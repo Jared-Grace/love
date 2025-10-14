675bf6e113c6d0ca698da92c28f3401fc29b00f1
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:49:57 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

---
+++ web/public/src/match_wrap_invoke.mjs
@@ -0,0 +1,27 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { object_assign } from "../../../love/public/src/object_assign.mjs";
+import { list_get } from "../../../love/public/src/list_get.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { function_is } from "../../../love/public/src/function_is.mjs";
+export function match_wrap_invoke(item, possibilities) {
+  let fi = function_is(item);
+  if (not(fi)) {
+    item = function reply_wrap_inner(possibilities) {
+      function lambda2(p) {
+        let tokens = object_property_get(p, "tokens");
+        let index_start = object_property_get(p, "index");
+        let token = list_get(tokens, index_start);
+        let matches = item === token;
+        let r = {
+          matches,
+          index: index_start + 1,
+        };
+        object_assign(p, r);
+      }
+      each(possibilities, lambda2);
+    };
+  }
+  possibilities = item(possibilities);
+  return possibilities;
+}
--- web/public/src/reply_sequence.mjs
+++ web/public/src/reply_sequence.mjs
@@ -1,32 +1,11 @@
+import { match_wrap_invoke } from "../../../love/public/src/match_wrap_invoke.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { function_is } from "../../../love/public/src/function_is.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 export function reply_sequence(sequence_fns) {
   let fn = function reply_sequence_matches(possibilities) {
     list_is_assert(possibilities);
-    let tokens = object_property_get(p, "tokens");
     function lambda(sequence_fn) {
-      let fi = function_is(sequence_fn);
-      if (not(fi)) {
-        sequence_fn = function reply_wrap_inner(possibilities) {
-          function lambda2(p) {
-            let index_start = object_property_get(p, "index");
-            let token = list_get(tokens, index_start);
-            let matches = sequence_fn === token;
-            let r = {
-              matches,
-              index: index_start + 1,
-            };
-            object_assign(p, r);
-          }
-          each(possibilities, lambda2);
-        };
-      }
-      possibilities = sequence_fn(possibilities);
+      possibilities = match_wrap_invoke(sequence_fn, possibilities);
     }
     each(sequence_fns, lambda);
   };
