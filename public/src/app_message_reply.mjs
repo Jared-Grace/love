37933223e69fcaaa74cd291b21a17049bf735bb3
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:07:23 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -1,3 +1,4 @@
+import { app_message_reply_choices } from "../../../love/public/src/app_message_reply_choices.mjs";
 import { reply_messages } from "../../../love/public/src/reply_messages.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
@@ -6,6 +7,7 @@ export async function app_message_reply() {
   let downloads = await app_message_download();
   let messages = list_map_property(downloads, "message");
   messages = ["howareyou"];
+  let start = app_message_reply_choices();
   let result = await reply_messages(messages);
   log(result);
 }
