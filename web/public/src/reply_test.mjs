b99f24fd2cf0622a0d5bc881ead5080990f61762
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:02:35 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- web/public/src/reply_test.mjs
+++ web/public/src/reply_test.mjs
@@ -1,1 +1,4 @@
-export async function reply_test() {}
+import { match_wrap_invoke } from "../../../love/public/src/match_wrap_invoke.mjs";
+export async function reply_test() {
+  match_wrap_invoke(sequence_fn, possibilities);
+}
