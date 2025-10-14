327fd2fca493a5ceb7c47b7c4ad51016cb791555
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:12:52 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- web/public/src/reply_choice.mjs
+++ web/public/src/reply_choice.mjs
@@ -1,6 +1,5 @@
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { match_wrap_invoke } from "../../../love/public/src/match_wrap_invoke.mjs";
-import { log_json } from "../../../love/public/src/log_json.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { list_map_squash } from "../../../love/public/src/list_map_squash.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
@@ -14,9 +13,6 @@ export function reply_choice(choices) {
     }
     ("maybe only need flatten one level, squash save time");
     let squashed = list_map_squash(choices, lambda);
-    log_json({
-      squashed,
-    });
     let filtered = reply_matches(squashed);
     return filtered;
   };
