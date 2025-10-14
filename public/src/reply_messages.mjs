c12b6a984b0f5b7a431800b42188455a55e98ea2
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:10:35 -0400

    function_auto_path ..\love\public\src\reply_messages.mjs

--- public/src/reply_messages.mjs
+++ public/src/reply_messages.mjs
@@ -1,3 +1,5 @@
+import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
@@ -12,6 +14,10 @@ import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { reply_dictionary } from "../../../love/public/src/reply_dictionary.mjs";
 import { reply_last } from "./reply_last.mjs";
 export async function reply_messages(messages, start) {
+  function lambda(a) {
+    object_property_set(a, "success", true);
+  }
+  let r = reply_on_match(start, lambda);
   let last = reply_last();
   let dictionary = await reply_dictionary();
   function lambda(message) {
