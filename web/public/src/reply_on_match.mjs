3ff32e694b5d4cf9333dbe6a71b1ee133d2e3249
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:14:35 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- web/public/src/reply_on_match.mjs
+++ web/public/src/reply_on_match.mjs
@@ -2,7 +2,8 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function reply_on_match(fn, on_match) {
   let matcher = function reply_on_match_inner(possbilities) {
     possbilities = fn(possbilities);
-    let filtered = list_filter(list, function lambda(item) {});
+    function lambda(item) {}
+    let filtered = list_filter(list, lambda);
     if (matches) {
       on_match(possbilities);
     }
