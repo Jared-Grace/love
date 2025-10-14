2d88aeed8459637d5dba850a36dd9ca18dfed8d6
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 15:53:31 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- web/public/src/reply_sequence.mjs
+++ web/public/src/reply_sequence.mjs
@@ -1,4 +1,4 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { function_is } from "../../../love/public/src/function_is.mjs";
@@ -32,12 +32,11 @@ export function reply_sequence(sequence) {
         }
         each_index(sequence, lambda);
       }
-      object_property_set(object, property_name, value);
       let r = {
         matches,
         index: index_start + sequence_size,
       };
-      return r;
+      object_assign(ai, r);
     }
     each(a, lambda2);
   };
