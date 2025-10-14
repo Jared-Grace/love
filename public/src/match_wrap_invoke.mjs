9b9db311fe2ebb2306532947ce2d18d78adaa34a
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 18:10:33 -0400

    function_auto_path ..\love\public\src\match_wrap_invoke.mjs

--- public/src/match_wrap_invoke.mjs
+++ public/src/match_wrap_invoke.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { list_is_assert_json } from "../../../love/public/src/list_is_assert_json.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -40,6 +39,5 @@ export function match_wrap_invoke(item, possibilities) {
     return v;
   }
   list_is_assert_json(result, lambda);
-  log(result);
   return result;
 }
