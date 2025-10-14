5d18489ceec6ec83e12f2a8584e586ff53d2799e
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:19:11 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- public/src/reply_choice.mjs
+++ public/src/reply_choice.mjs
@@ -1,13 +1,13 @@
+import { list_map_squash } from "../../../love/public/src/list_map_squash.mjs";
 import { list_squash } from "../../../love/public/src/list_squash.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
-import { list_map } from "./list_map.mjs";
 export function reply_choice(choices) {
   let fn = function reply_sequence_matches(possbilities) {
     function lambda(choice) {
       possbilities = choice(possbilities);
     }
-    let mapped = list_map(choices, lambda);
     ("maybe only need flatten one level, squash save time");
+    let mapped = list_map_squash(choices, lambda);
     let squashed = list_squash(mapped);
     let filtered = reply_matches(squashed);
     return filtered;
