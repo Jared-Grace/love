362cd4a96984aa09d74856b4e09666c3bc564ad4
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:30:10 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,12 +1,9 @@
-import { log } from "../../../love/public/src/log.mjs";
+import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 export function reply_on_match(fn, on_match) {
   let matcher = function reply_on_match_inner(possibilities) {
-    log({
-      possibilities,
-      c: "",
-    });
+    list_is_assert(possibilities);
     possibilities = fn(possibilities);
     let filtered = reply_matches(possibilities);
     let ne = list_empty_not_is(filtered);
