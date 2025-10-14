b49aec14a6655f30d890bb14c169b777a87f6207
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:06:09 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- web/public/src/reply_sequence.mjs
+++ web/public/src/reply_sequence.mjs
@@ -3,7 +3,6 @@ import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { function_is } from "../../../love/public/src/function_is.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
@@ -23,18 +22,12 @@ export function reply_sequence(sequence_fns) {
             sequence_fn = function lambda3(a) {
               let index_start = object_property_get(a, "index");
               let token = list_get(tokens, index + index_start);
-              if (sequence_fn !== token) {
-                log({
-                  sequence_item: sequence_fn,
-                  token,
-                });
-                matches = false;
-                let r = {
-                  matches,
-                  index: index_start + 1,
-                };
-                object_assign(ai, r);
-              }
+              matches = sequence_fn === token;
+              let r = {
+                matches,
+                index: index_start + 1,
+              };
+              object_assign(ai, r);
             };
           }
           let r = sequence_fn(ai);
