05aef6a75556eb6a5a9384ca59460b34eef8e2bb
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:20:16 -0400

    o rt

--- public/src/reply_test.mjs
+++ public/src/reply_test.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_messages } from "../../../love/public/src/reply_messages.mjs";
 import { reply_sequence } from "./reply_sequence.mjs";
 export async function reply_test() {
+  marker("1");
   let messages = ["a"];
   let start = reply_sequence(["a"]);
   let result = await reply_messages(messages, start);
