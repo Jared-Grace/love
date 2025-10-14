999c91e0d83d7e0681b7ac97ebd872e1cd1129ba
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:17:50 -0400

    function_auto_path ..\love\public\src\bible_verse_grace.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,5 +1,4 @@
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 export async function sandbox_3() {
   const messenger = "JESUS";
   if (browser_is()) {
