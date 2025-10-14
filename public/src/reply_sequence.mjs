774d5074f41f6843375ed0ad818ca2a8d95307e8
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:50:31 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -9,7 +9,8 @@ export function reply_sequence(sequence_fns) {
       possibilities = match_wrap_invoke(sequence_fn, possibilities);
       let e = list_empty_is(possibilities);
       if (e) {
-        return true;
+        let v = true;
+        return v;
       }
     }
     each(sequence_fns, lambda);
