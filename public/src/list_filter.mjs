394d99892c5e13b0fc49881ae41a327dce7edd2f
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:56:12 -0400

    function_auto_path ..\love\public\src\list_filter.mjs

--- public/src/list_filter.mjs
+++ public/src/list_filter.mjs
@@ -1,14 +1,10 @@
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
-import { list_is } from "../../../love/public/src/list_is.mjs";
-import { assert } from "../../../love/public/src/assert.mjs";
 export function list_filter(list, lambda$item) {
-  let l = list_is(list);
-  assert(l);
+  list_is_assert(list);
   function lambda(item) {
     let match = lambda$item(item);
     return match;
   }
   let filtered = list.filter(lambda);
   return filtered;
-  list_is_assert(list);
 }
