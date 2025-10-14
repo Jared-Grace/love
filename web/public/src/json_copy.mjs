69f9411e13133a7181e41d4c352dadbeabdd5b00
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 18:17:53 -0400

    function_auto_path ..\love\public\src\json_copy.mjs

--- web/public/src/json_copy.mjs
+++ web/public/src/json_copy.mjs
@@ -1,4 +1,6 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function json_copy() {
-  marker("1");
+import { json_from } from "./json_from.mjs";
+import { json_to } from "./json_to.mjs";
+export function json_copy(o) {
+  let json = json_to(o);
+  return json_from(json);
 }
