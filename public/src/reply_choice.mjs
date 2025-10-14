010286afa4f861028828056596603c483a52588a
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 17:16:52 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- public/src/reply_choice.mjs
+++ public/src/reply_choice.mjs
@@ -2,17 +2,12 @@ import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function reply_choice(choices) {
   let fn = function reply_sequence_matches(possbilities) {
-    let matches_result = false;
     function lambda(choice) {
       possbilities = choice(possbilities);
       let filtered = reply_matches(possbilities);
       return filtered;
     }
     each(choices, lambda);
-    let r = {
-      matches: matches_result,
-    };
-    return r;
   };
   return fn;
 }
