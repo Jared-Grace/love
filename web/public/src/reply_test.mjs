9e93f013f5886b3784e8c9b47fa723dbdc960d81
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 19:29:17 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- web/public/src/reply_test.mjs
+++ web/public/src/reply_test.mjs
@@ -3,8 +3,12 @@ import { reply_messages } from "../../../love/public/src/reply_messages.mjs";
 import { reply_sequence } from "./reply_sequence.mjs";
 export async function reply_test() {
   marker("1");
-  let messages = ["a"];
-  let start = reply_sequence(["a"]);
+  let cases = [
+    {
+      messages: ["a"],
+      start: reply_sequence(["a"]),
+    },
+  ];
   let result = await reply_messages(messages, start);
   return result;
 }
