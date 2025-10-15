0404c568c53b2e49c948b431c2207cfa2f2bc62c
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:26:45 -0400

    function_auto_path ..\love\public\src\range_1.mjs

--- web/public/src/range_1.mjs
+++ web/public/src/range_1.mjs
@@ -2,5 +2,6 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { range } from "../../../love/public/src/range.mjs";
 export function range_1(count) {
   let r = range(count);
-  let mapped = list_map(list, function lambda(item) {});
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
 }
