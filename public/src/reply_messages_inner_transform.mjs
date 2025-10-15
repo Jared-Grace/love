51803bac6a1165ee2dd829516438a622f065a43e
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:15:27 -0400

    function_auto_path ..\love\public\src\reply_messages_inner_transform.mjs

--- public/src/reply_messages_inner_transform.mjs
+++ public/src/reply_messages_inner_transform.mjs
@@ -1,7 +1,7 @@
-import { string_letters_is } from "../../../love/public/src/string_letters_is.mjs";
+import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 export function reply_messages_inner_transform(message) {
   let lower = string_lower_to(message);
-  let li = string_letters_is(str);
+  let tokens = string_split_empty(input);
   return lower;
 }
