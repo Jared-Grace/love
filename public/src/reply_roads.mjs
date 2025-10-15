3e7af19d2e054c174f7871cada36abd882518a47
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 19:36:10 -0400

    function_auto_path ..\love\public\src\reply_roads.mjs

--- public/src/reply_roads.mjs
+++ public/src/reply_roads.mjs
@@ -2,5 +2,5 @@ import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_roads() {
   marker("1");
-  let fn = reply_sequence(sequence_fns);
+  let fn = reply_sequence(["w", "walta"]);
 }
