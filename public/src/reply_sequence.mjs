e99c11633e314e405306fdf6ae7782e43d7bf65f
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 19:55:16 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -2,9 +2,9 @@ import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 import { reply_wrap_invoke } from "../../../love/public/src/reply_wrap_invoke.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
-import { each } from "../../../love/public/src/each.mjs";
+import { each_async } from "./each_async.mjs";
 export function reply_sequence(sequence_fns) {
-  let fn = function reply_sequence_matches(possibilities) {
+  let fn = async function reply_sequence_matches(possibilities) {
     list_is_assert(possibilities);
     function lambda(sequence_fn) {
       possibilities = reply_wrap_invoke(sequence_fn, possibilities);
@@ -15,7 +15,7 @@ export function reply_sequence(sequence_fns) {
         return v;
       }
     }
-    each(sequence_fns, lambda);
+    await each_async(sequence_fns, lambda);
     return possibilities;
   };
   return fn;
