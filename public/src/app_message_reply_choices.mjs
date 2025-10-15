1c0a2340c429cc6f767d5fae0fa69366732bf675
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:12:23 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -47,7 +47,7 @@ export function app_message_reply_choices() {
     "preacher",
     "teacher",
   ]);
-  let rs = reply_sequence(sequence_fns);
+  let rs = reply_sequence([iam, rc]);
   let choices_main = reply_choice([fn4, fn6, fn2, fn7, fn10]);
   let room = reply_once_or_more(choices_main);
   let last = reply_last();
