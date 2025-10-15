b58c84344552ba9442d9c9493186615ef9d228f0
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 13:05:02 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -21,6 +21,7 @@ export function app_message_reply_choices() {
   let fn2 = reply_on_match_output("jesus", "✝️ Jesus is Lord! 👑");
   let fn7 = reply_on_match_output("god", "❤️‍🔥 God is love! ✝️");
   let choices_main = reply_choice([fn4, fn6, fn2, fn7]);
+  let fn10 = reply_sequence(sequence_fns);
   let last = reply_last();
   let fn5 = reply_sequence([choices_main, last]);
   return fn5;
