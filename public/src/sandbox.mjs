49eec7da58a3e423109b416b8feca25730f22089
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 13:43:56 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { http_local_json } from "../../../love/public/src/http_local_json.mjs";
 export async function sandbox() {
   let url =
     "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/exports/json/?lang=en&timezone=America%2FNew_York";
+  await http_local_json(url);
 }
