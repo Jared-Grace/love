71872b74834fdce283a01b3325105bdb0065e6e6
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 13:18:18 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -14,7 +14,8 @@ export function app_message_reply_choices() {
   const hello = reply_sequence(["hell", fn8]);
   let hi_word = reply_choice(["hi", hello, "hey"]);
   let fn13 = reply_sequence(["dear"]);
-  let fn12 = reply_choice(["brother", fn13]);
+  let fn14 = reply_choice(choices);
+  let fn12 = reply_choice(["brother", fn13, "my"]);
   let fn9 = reply_optional(fn12);
   let fn3 = reply_sequence([hi_word, fn9]);
   let fn6 = reply_on_match_output(fn3, greetings);
