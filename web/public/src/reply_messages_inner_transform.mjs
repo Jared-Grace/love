248a6feced8f23676849942525d9010f65d75b1e
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:40:24 -0400

    function_auto_path ..\love\public\src\reply_messages_inner_transform.mjs

--- web/public/src/reply_messages_inner_transform.mjs
+++ web/public/src/reply_messages_inner_transform.mjs
@@ -7,8 +7,8 @@ import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 export function reply_messages_inner_transform(message) {
   let lower = string_lower_to(message);
   let tokens = string_split_empty(lower);
-  let string_includes_curry_right = string_includes_curry_right_get(" ");
-  const choices = [string_letters_is, string_includes_curry_right];
+  let string_includes_curry_right = string_includes_curry_right_get("todo");
+  const choices = [string_letters_is];
   function lambda(item) {
     function lambda2(fn) {
       let v = fn(item);
