9f67d6b1e6409ca4faa061f89af38eeed56ec1b1
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:18:08 -0400

    function_auto_path ..\love\public\src\reply_messages_inner_transform.mjs

--- web/public/src/reply_messages_inner_transform.mjs
+++ web/public/src/reply_messages_inner_transform.mjs
@@ -1,3 +1,4 @@
+import { list_any } from "../../../love/public/src/list_any.mjs";
 import { string_letters_is } from "../../../love/public/src/string_letters_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
@@ -7,6 +8,7 @@ export function reply_messages_inner_transform(message) {
   let tokens = string_split_empty(lower);
   function lambda(item) {
     let li = string_letters_is(item);
+    let any = list_any(list, function lambda2(item2) {});
   }
   let filtered = list_filter(tokens, lambda);
   return filtered;
