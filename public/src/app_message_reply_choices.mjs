a0cf41523369b439ba46fd2e64cb5ab8db36ddf3
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 13:20:36 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -15,7 +15,8 @@ export function app_message_reply_choices() {
   let hi_word = reply_choice(["hi", hello, "hey"]);
   let fn15 = reply_optional("y");
   let my = reply_sequence(["m", fn15]);
-  let fn13 = reply_sequence([my, "dear"]);
+  let fn14 = reply_optional(my);
+  let fn13 = reply_sequence([fn14, "dear"]);
   let fn12 = reply_choice(["brother", fn13, my]);
   let fn9 = reply_optional(fn12);
   let fn3 = reply_sequence([hi_word, fn9]);
