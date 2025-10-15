53564f8b8bf1d21b63299efc78337d3018024c5f
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 19:56:26 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- public/src/reply_choice.mjs
+++ public/src/reply_choice.mjs
@@ -1,12 +1,12 @@
 import { list_squash } from "../../../love/public/src/list_squash.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { reply_wrap_invoke } from "../../../love/public/src/reply_wrap_invoke.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
+import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 export function reply_choice(choices) {
   list_is_assert(choices);
-  let fn = function reply_choice_matches(possibilities) {
+  let fn = async function reply_choice_matches(possibilities) {
     list_is_assert(possibilities);
     async function lambda(choice) {
       let copy = json_copy(possibilities);
@@ -14,7 +14,7 @@ export function reply_choice(choices) {
       return copy;
     }
     ("maybe only need flatten one level, squash save time");
-    let mapped = list_map(choices, lambda);
+    let mapped = await list_map_unordered_async(choices, lambda);
     let squashed = list_squash(mapped);
     let filtered = reply_matches(squashed);
     return filtered;
