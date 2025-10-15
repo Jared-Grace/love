b2f1ae55d981442004b5a4bd4db1f7018b1a0bf7
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:43:12 -0400

    function_auto_path ..\love\public\src\reply_cities.mjs

--- public/src/reply_cities.mjs
+++ public/src/reply_cities.mjs
@@ -2,7 +2,7 @@ import { reply_choice } from "../../../love/public/src/reply_choice.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_cities() {
   marker("1");
-  let v = ["bungomain", "lahore"];
+  let v = ["bungoma", "lahore"];
   let fn20 = reply_choice(v);
   return fn20;
 }
