806fc5339c3ed7c097a21ff3b8de07317550b677
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 21:37:59 -0400

    function_auto_path ..\love\public\src\reply_sequence.mjs

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { match_wrap_invoke } from "../../../love/public/src/match_wrap_invoke.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -5,6 +6,7 @@ export function reply_sequence(sequence_fns) {
   let fn = function reply_sequence_matches(possibilities) {
     list_is_assert(possibilities);
     function lambda(sequence_fn) {
+      log(message);
       possibilities = match_wrap_invoke(sequence_fn, possibilities);
     }
     each(sequence_fns, lambda);
