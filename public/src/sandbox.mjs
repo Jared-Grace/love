855eb7d70c139e12f17a335a61432bc6ad8874a4
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:29:18 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { http_local_json } from "../../../love/public/src/http_local_json.mjs";
@@ -7,5 +8,6 @@ export async function sandbox() {
     "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/exports/json/?lang=en&timezone=America%2FNew_York";
   let list = await http_local_json(url);
   let mapped = list_map_property(list, "ascii_name");
+  let mapped2 = list_map(list2, function lambda(item) {});
   return mapped;
 }
