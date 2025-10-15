55b42124fc28705be1cfb10850d1f97ff7e8e976
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 12:57:45 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- web/public/src/reply_test.mjs
+++ web/public/src/reply_test.mjs
@@ -1,5 +1,4 @@
 import { reply_optional } from "../../../love/public/src/reply_optional.mjs";
-import { reply_dictionary } from "../../../love/public/src/reply_dictionary.mjs";
 import { reply_messages_inner } from "../../../love/public/src/reply_messages_inner.mjs";
 import { reply_last } from "../../../love/public/src/reply_last.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -9,7 +8,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_sequence } from "./reply_sequence.mjs";
 import { reply_choice } from "./reply_choice.mjs";
 export async function reply_test() {
-  let dictionary = await reply_dictionary();
   let last = reply_last();
   marker("1");
   const choice_a_k = reply_choice(["a", "k"]);
@@ -276,7 +274,7 @@ export async function reply_test() {
     let message = object_property_get(item, "message");
     let start2 = object_property_get(item, "start");
     let expected = object_property_get(item, "expected");
-    let actual = reply_messages_inner(message, dictionary, start2);
+    let actual = reply_messages_inner(message, start2);
     json_equal_assert(actual, expected);
   }
   await each_async(cases, lambda);
