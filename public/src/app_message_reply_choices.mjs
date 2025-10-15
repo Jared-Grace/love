b688643eb8807ac56c385b55ce0ddacadc36ff5b
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 18:06:05 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -60,7 +60,8 @@ export function app_message_reply_choices() {
   let fn9 = reply_optional("a");
   let title = reply_sequence([iam, fn9, titles]);
   let titled_name = reply_sequence_optional([titles, names_once_or_more]);
-  let fn19 = reply_sequence([iam, titled_name, "from", fn20, "in", countries]);
+  let fn18 = reply_optional("in");
+  let fn19 = reply_sequence([iam, titled_name, "from", fn20, fn18, countries]);
   let fn25 = reply_optional("ing");
   let supporting = reply_sequence(["request", fn25]);
   const us = "us";
