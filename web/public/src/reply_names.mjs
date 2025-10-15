f207be257c77874a3029c5fa8bd5d86c94b33f46
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 18:02:06 -0400

    function_auto_path ..\love\public\src\reply_names.mjs

--- web/public/src/reply_names.mjs
+++ web/public/src/reply_names.mjs
@@ -1,6 +1,8 @@
+import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_names() {
   marker("1");
   let names = ["***REMOVED***", "***REMOVED***", "***REMOVED***"];
+  let fn = reply_sequence(sequence_fns);
   return names;
 }
