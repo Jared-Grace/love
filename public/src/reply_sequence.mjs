c97ee2f79efa3a1007c9fce932e43b7bb05ff2de
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 12:58:49 -0400

    rr match_wrap_invoke match reply

--- public/src/reply_sequence.mjs
+++ public/src/reply_sequence.mjs
@@ -1,13 +1,13 @@
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
-import { match_wrap_invoke } from "../../../love/public/src/match_wrap_invoke.mjs";
+import { reply_wrap_invoke } from "../../../love/public/src/reply_wrap_invoke.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function reply_sequence(sequence_fns) {
   let fn = function reply_sequence_matches(possibilities) {
     list_is_assert(possibilities);
     function lambda(sequence_fn) {
-      possibilities = match_wrap_invoke(sequence_fn, possibilities);
+      possibilities = reply_wrap_invoke(sequence_fn, possibilities);
       possibilities = reply_matches(possibilities);
       let e = list_empty_is(possibilities);
       if (e) {
