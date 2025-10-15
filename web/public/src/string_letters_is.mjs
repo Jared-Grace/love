fbd782851cdf57a8fa002bc9da176368a68752a3
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:11:45 -0400

    function_auto_path ..\love\public\src\string_letters_is.mjs

---
+++ web/public/src/string_letters_is.mjs
@@ -0,0 +1,8 @@
+import { regex_letters } from "../../../love/public/src/regex_letters.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function string_letters_is(str) {
+  marker("1");
+  let v = regex_letters();
+  let letters = str.replace(v, "");
+  return letters;
+}
