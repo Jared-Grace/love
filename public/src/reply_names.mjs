e837d91543921fec954d3b9358c3fe2a3d7a6f5d
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 18:01:58 -0400

    function_auto_path ..\love\public\src\reply_names.mjs

--- public/src/reply_names.mjs
+++ public/src/reply_names.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_names() {
   marker("1");
-  let v = ["dilshad", "khan", "yaqoob"];
-  return v;
+  let names = ["dilshad", "khan", "yaqoob"];
+  return names;
 }
