9297554a401d301932a76c409db2a10d7ebbbf73
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 19:38:00 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- web/public/src/app_message_reply_choices.mjs
+++ web/public/src/app_message_reply_choices.mjs
@@ -90,10 +90,10 @@ export function app_message_reply_choices() {
   ]);
   let thank_you = reply_sequence(["thank", "you"]);
   let thanks = reply_choice(["thanks", thank_you]);
-  let split = digits();
-  let fn26 = reply_choice(split);
-  let room2 = reply_once_or_more(fn26);
-  let fn24 = reply_sequence(["contact", room2]);
+  let d = digits();
+  let rc_d = reply_choice(d);
+  let d_oom = reply_once_or_more(rc_d);
+  let fn24 = reply_sequence(["contact", d_oom]);
   let choices_main = reply_choice([
     fn4,
     fn6,
