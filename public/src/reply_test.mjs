5934e8b032c10f2a8455e51be94c98c6bee3da39
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 20:54:56 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- public/src/reply_test.mjs
+++ public/src/reply_test.mjs
@@ -21,6 +21,7 @@ export async function reply_test() {
   ];
   async function lambda(item) {
     let messages2 = object_property_get(item, "messages");
+    let start2 = object_property_get(item, "start");
     let actual = await reply_messages(messages2, start);
     json_equal_assert(actual);
   }
