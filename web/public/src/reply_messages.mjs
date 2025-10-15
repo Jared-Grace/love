2c7fba203051134a60a45622cf365c0927ab6084
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:27:43 -0400

    function_auto_path ..\love\public\src\reply_messages.mjs

--- web/public/src/reply_messages.mjs
+++ web/public/src/reply_messages.mjs
@@ -1,9 +1,9 @@
+import { range_1 } from "../../../love/public/src/range_1.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_reverse } from "../../../love/public/src/list_reverse.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
-import { range } from "../../../love/public/src/range.mjs";
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { reply_messages_inner } from "../../../love/public/src/reply_messages_inner.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -13,7 +13,7 @@ export function reply_messages(messages, start) {
     let value = object_property_get(object, "matches");
     if (equal(value, false)) {
       let size = string_size(message);
-      let r = range(size);
+      let r = range_1(size);
       list_reverse(r);
       function lambda2(item) {}
       each(list, lambda2);
