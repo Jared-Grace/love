ce70c3a76e5329a78e570dd9fa4bb565cd8433ed
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 21:01:11 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -1,6 +1,4 @@
 import { reply_last } from "../../../love/public/src/reply_last.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { app_reply_response_how_r_u } from "../../../love/public/src/app_reply_response_how_r_u.mjs";
@@ -15,9 +13,5 @@ export function app_message_reply_choices() {
   let fn2 = reply_choice([fn4, fn3]);
   let last = reply_last();
   let fn5 = reply_sequence([fn2, last]);
-  function lambda(a) {
-    object_property_set(a, "success", true);
-  }
-  let r = reply_on_match(fn5, lambda);
-  return r;
+  return fn5;
 }
