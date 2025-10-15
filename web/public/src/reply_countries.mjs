72c9f7c7ef4fcd9aa4269bc2846356e2ebfc56f0
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:45:31 -0400

    function_auto_path ..\love\public\src\reply_countries.mjs

--- web/public/src/reply_countries.mjs
+++ web/public/src/reply_countries.mjs
@@ -1,8 +1,8 @@
-import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { reply_choice } from "./reply_choice.mjs";
 export function reply_countries() {
   marker("1");
   let v = ["kenya", "pakistan"];
-  let countries = reply_sequence(v);
+  let countries = reply_choice(v);
   return countries;
 }
