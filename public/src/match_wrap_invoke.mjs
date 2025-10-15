5f833b3864a7242e143f5f6b3c943ee3b9afabe1
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 21:34:46 -0400

    function_auto_path ..\love\public\src\match_wrap_invoke.mjs

--- public/src/match_wrap_invoke.mjs
+++ public/src/match_wrap_invoke.mjs
@@ -24,8 +24,6 @@ export function match_wrap_invoke(item, possibilities) {
           index: index_start + 1,
         };
         log({
-          item,
-          token,
           r,
         });
         object_assign(p, r);
