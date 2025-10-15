f3acfdf2154f041bf2dbc5f3b30d8be7a36a5aed
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 20:53:56 -0400

    function_auto_path ..\love\public\src\json_equal_assert.mjs

--- public/src/json_equal_assert.mjs
+++ public/src/json_equal_assert.mjs
@@ -1,6 +1,6 @@
 import { assert } from "../../../love/public/src/assert.mjs";
 import { json_equal } from "../../../love/public/src/json_equal.mjs";
-export function json_equal_assert() {
-  let eq = json_equal(left, sliced);
+export function json_equal_assert(left, right) {
+  let eq = json_equal(left, right);
   assert(eq);
 }
