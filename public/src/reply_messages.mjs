10750cdb312f130b9be274b48d147323dcce348d
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:20:19 -0400

    function_auto_path ..\love\public\src\reply_messages.mjs

--- public/src/reply_messages.mjs
+++ public/src/reply_messages.mjs
@@ -6,8 +6,8 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 export function reply_messages(messages, start) {
   function lambda(message) {
     let result = reply_messages_inner(message, start);
-    let size = string_size(s);
-    let r = range(count);
+    let size = string_size(message);
+    let r = range(size);
     return result;
   }
   let result = list_map(messages, lambda);
