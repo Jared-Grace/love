87615b20c9a6c4f7c9a17e3ccc55a4d96e4bd27b
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:19:47 -0400

    function_auto_path ..\love\public\src\range.mjs

--- web/public/src/range.mjs
+++ web/public/src/range.mjs
@@ -2,9 +2,9 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function range(count) {
   marker("1");
-  let rr = [];
+  let r = [];
   for (let i = 0; i < count; i++) {
-    list_add(rr, i);
+    list_add(r, i);
   }
-  return rr;
+  return r;
 }
