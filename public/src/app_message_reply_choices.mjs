2c36084c51eca023cb2533f843675b4888898369
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 12:25:08 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -8,13 +8,13 @@ import { reply_on_match_output } from "./reply_on_match_output.mjs";
 export function app_message_reply_choices() {
   marker("1");
   let greetings = app_reply_response_greetings();
-  let fn7 = reply_choice(["hello", "hell"]);
   let fn3 = reply_choice(["hi", "hello", "hey"]);
   let fn6 = reply_on_match_output(fn3, greetings);
   let fn = reply_sequence(["how", "are", "you"]);
   let hru = app_reply_response_how_r_u();
   let fn4 = reply_on_match_output(fn, hru);
   let fn2 = reply_on_match_output("jesus", "✝️ Jesus is Lord! 👑");
+  let fn7 = reply_on_match_output(fn_a, item);
   let choices_main = reply_choice([fn4, fn6, fn2]);
   let last = reply_last();
   let fn5 = reply_sequence([choices_main, last]);
