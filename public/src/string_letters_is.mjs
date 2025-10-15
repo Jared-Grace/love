37cbbcc7d25027e5c1f9fe76775d3c96e67a9451
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:14:34 -0400

    function_auto_path ..\love\public\src\string_letters_is.mjs

--- public/src/string_letters_is.mjs
+++ public/src/string_letters_is.mjs
@@ -1,8 +1,6 @@
-import { regex_letters_not } from "../../../love/public/src/regex_letters_not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_letters_is(str) {
   marker("1");
-  let v = regex_letters_not();
-  let letters = str.replace(v, "");
-  return letters;
+  let v = /^[A-Za-z]+$/;
+  return v.test(str);
 }
