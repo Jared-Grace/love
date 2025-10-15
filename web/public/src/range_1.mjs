27ae8b1ad858a3db6dbd465c006a8aec21365db1
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:26:26 -0400

    function_auto_path ..\love\public\src\range_1.mjs

---
+++ web/public/src/range_1.mjs
@@ -0,0 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+export function range_1(count) {
+  marker("1");
+  let r = [];
+  for (let i = 0; i < count; i++) {
+    list_add(r, i);
+  }
+  return r;
+}
