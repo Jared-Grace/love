b66e5a2981987a045fca2312c84b4913a2a880c2
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 13:42:13 -0400

    function_auto_path ..\love\public\src\reply_dictionary.mjs

--- public/src/reply_dictionary.mjs
+++ public/src/reply_dictionary.mjs
@@ -1,11 +1,9 @@
+import { http_local_json } from "../../../love/public/src/http_local_json.mjs";
 import { object_property_delete_multiple } from "../../../love/public/src/object_property_delete_multiple.mjs";
-import { json_from } from "../../../love/public/src/json_from.mjs";
-import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
 export async function reply_dictionary() {
   const url =
     "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json";
-  let text = await http_local_text(url);
-  let dictionary = json_from(text);
+  let dictionary = await http_local_json(url);
   let excludes_letters = ["h", "e", "d", "l", "t", "w"];
   object_property_delete_multiple(dictionary, excludes_letters);
   let excludes_pairs = ["ar", "ii", "ey", "ot", "pr", "re", "wa"];
