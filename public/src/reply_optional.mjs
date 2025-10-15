480b8b025e1b487ed5f3732617b72f77860596ef
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:08:22 -0400

    function_auto_path ..\love\public\src\reply_optional.mjs

--- public/src/reply_optional.mjs
+++ public/src/reply_optional.mjs
@@ -1,5 +1,5 @@
 import { reply_choice } from "../../../love/public/src/reply_choice.mjs";
 export function reply_optional(item) {
-  let fn = reply_choice([item, ""]);
-  return fn;
+  let ro = reply_choice([item, ""]);
+  return ro;
 }
