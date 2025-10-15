31f7937c9897c575e7c05513e52943b64fdeee5f
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:19:14 -0400

    function_auto_path ..\love\public\src\reply_messages_inner_transform.mjs

--- web/public/src/reply_messages_inner_transform.mjs
+++ web/public/src/reply_messages_inner_transform.mjs
@@ -6,12 +6,13 @@ import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 export function reply_messages_inner_transform(message) {
   let lower = string_lower_to(message);
   let tokens = string_split_empty(lower);
+  const choices = [string_letters_is];
   function lambda(item) {
     function lambda2(fn) {
       let v = fn(item);
       return v;
     }
-    let any = list_any([string_letters_is], lambda2);
+    let any = list_any(choices, lambda2);
     return any;
   }
   let filtered = list_filter(tokens, lambda);
