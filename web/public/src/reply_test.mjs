31482a8b12912aa43f578fbcd3be3fffb66fd027
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 21:06:49 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- web/public/src/reply_test.mjs
+++ web/public/src/reply_test.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { reply_messages_inner } from "../../../love/public/src/reply_messages_inner.mjs";
 import { reply_last } from "../../../love/public/src/reply_last.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -121,7 +122,8 @@ export async function reply_test() {
     let messages2 = object_property_get(item, "messages");
     let start2 = object_property_get(item, "start");
     let expected = object_property_get(item, "expected");
-    let result = reply_messages_inner(message, dictionary, start);
+    each(list, function lambda2(item2) {});
+    let result = reply_messages_inner(messages2, dictionary, start);
     let actual = await reply_messages(messages2, start2);
     json_equal_assert(actual, expected);
   }
