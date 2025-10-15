76a1c016859acfcbf4f9a9cc5028b69e1dabc935
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:03:27 -0400

    function_auto_path ..\love\public\src\reply_countries.mjs

--- public/src/reply_countries.mjs
+++ public/src/reply_countries.mjs
@@ -1,6 +1,8 @@
+import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_countries() {
   marker("1");
   let v = ["kenya", "pakistan"];
+  let fn = reply_sequence(sequence_fns);
   return v;
 }
