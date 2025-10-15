256a2b7c1014ec32f728af3e0654dd0ebb5a1329
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 13:07:03 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

---
+++ public/src/reply_once_or_more.mjs
@@ -0,0 +1,9 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { reply_optional } from "../../../love/public/src/reply_optional.mjs";
+import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
+export function reply_once_or_more(choices_main) {
+  const sequence = [choices_main];
+  let fn10 = reply_sequence(sequence);
+  let fn11 = reply_optional(fn10);
+  list_add(sequence, fn11);
+}
