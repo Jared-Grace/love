77ee7742dd9df07994482fb437f580485112b8dc
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:14:50 -0400

    function_auto_path ..\love\public\src\string_letters_is.mjs

--- web/public/src/string_letters_is.mjs
+++ web/public/src/string_letters_is.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_letters_is(str) {
   marker("1");
-  let v = /^[A-Za-z]+$/;
-  let v2 = v.test(str);
-  return v2;
+  let r = /^[A-Za-z]+$/;
+  let li = r.test(str);
+  return li;
 }
