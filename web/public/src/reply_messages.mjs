1293e5f571f6d84cdf97240e561ec0401fdd912f
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 12:57:28 -0400

    function_auto_path ..\love\public\src\reply_messages.mjs

--- web/public/src/reply_messages.mjs
+++ web/public/src/reply_messages.mjs
@@ -1,11 +1,9 @@
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { reply_messages_inner } from "../../../love/public/src/reply_messages_inner.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { reply_dictionary } from "../../../love/public/src/reply_dictionary.mjs";
 export async function reply_messages(messages, start) {
-  let dictionary = await reply_dictionary();
   function lambda(message) {
-    let result = reply_messages_inner(message, dictionary, start);
+    let result = reply_messages_inner(message, start);
     return result;
   }
   let result = list_map(messages, lambda);
