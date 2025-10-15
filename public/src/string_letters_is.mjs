6ca43e0409e54e682bee25f74cf5153000696b58
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:13:35 -0400

    rs regex_letters not

--- public/src/string_letters_is.mjs
+++ public/src/string_letters_is.mjs
@@ -1,8 +1,8 @@
-import { regex_letters } from "../../../love/public/src/regex_letters.mjs";
+import { regex_letters_not } from "../../../love/public/src/regex_letters_not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_letters_is(str) {
   marker("1");
-  let v = regex_letters();
+  let v = regex_letters_not();
   let letters = str.replace(v, "");
   return letters;
 }
