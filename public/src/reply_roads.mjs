5a7cea92e557214ae90e4a138e75da791d40702e
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 19:44:10 -0400

    function_auto_path ..\love\public\src\reply_roads.mjs

--- public/src/reply_roads.mjs
+++ public/src/reply_roads.mjs
@@ -6,7 +6,7 @@ export function reply_roads() {
   marker("1");
   let fn2 = reply_optional("w");
   let fn = reply_sequence([fn2, "walta"]);
-  let fn3 = reply_choice(fn);
+  let fn3 = reply_choice([fn]);
   let fn5 = reply_choice([
     "avenue",
     "blvd",
