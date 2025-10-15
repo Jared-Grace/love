687d7f2238564bf05fe4d1a5b36285bec327b668
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:46:22 -0400

    function_auto_path ..\love\public\src\reply_cities.mjs

--- web/public/src/reply_cities.mjs
+++ web/public/src/reply_cities.mjs
@@ -1,5 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_cities() {
   marker("1");
-  return ["***REMOVED***", "***REMOVED***"];
+  let v = ["***REMOVED***", "***REMOVED***"];
+  return v;
 }
