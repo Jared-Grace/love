33e3b04abe16043e9748927e6bf0c6dd4b96170e
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 19:44:20 -0400

    function_auto_path ..\love\public\src\reply_choice.mjs

--- public/src/reply_choice.mjs
+++ public/src/reply_choice.mjs
@@ -4,6 +4,7 @@ import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { list_map_squash } from "../../../love/public/src/list_map_squash.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 export function reply_choice(choices) {
+  lia;
   let fn = function reply_choice_matches(possibilities) {
     list_is_assert(possibilities);
     function lambda(choice) {
