f115f953e0292bf23bd7f45fc08c8e9139daa296
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 14:15:02 -0400

    function_auto_path ..\love\public\src\reply_messages_inner_transform.mjs

--- public/src/reply_messages_inner_transform.mjs
+++ public/src/reply_messages_inner_transform.mjs
@@ -1,5 +1,7 @@
+import { string_letters_is } from "../../../love/public/src/string_letters_is.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 export function reply_messages_inner_transform(message) {
   let lower = string_lower_to(message);
+  let li = string_letters_is(str);
   return lower;
 }
