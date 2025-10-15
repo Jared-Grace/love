b38d22145d7908fd5a1836003fa0c337d0718cc3
Grace-Jared <christrosetolife@gmail.com>
2025-10-14 21:42:11 -0400

    function_auto_path ..\love\public\src\reply_test.mjs

--- public/src/reply_test.mjs
+++ public/src/reply_test.mjs
@@ -164,6 +164,17 @@ export async function reply_test() {
       start: reply_sequence([choice_a_k, "l"]),
       expected: [],
     },
+    {
+      message: "ak",
+      start: reply_sequence([choice_a_k, last]),
+      expected: {
+        tokens: ["a", "k", {}],
+        outputs: [],
+        index: 2,
+        matches: true,
+        input: "ak",
+      },
+    },
   ];
   async function lambda(item) {
     let message = object_property_get(item, "message");
