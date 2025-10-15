e0912ff1a459f65e0cb9834feb08b4c43a89dd7f
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 15:19:11 -0400

    o amr

--- web/public/src/app_message_reply.mjs
+++ web/public/src/app_message_reply.mjs
@@ -1,8 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { app_message_reply_choices } from "../../../love/public/src/app_message_reply_choices.mjs";
 import { reply_messages } from "../../../love/public/src/reply_messages.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_message_download } from "../../../love/public/src/app_message_download.mjs";
 export async function app_message_reply() {
+  marker("1");
   let downloads = await app_message_download();
   let messages = list_map_property(downloads, "message");
   let start = app_message_reply_choices();
