ee297aa90a83944227e3d7b88cb0983d286e9000
Grace-Jared <christrosetolife@gmail.com>
2025-10-15 21:28:10 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- web/public/src/reply_on_match.mjs
+++ web/public/src/reply_on_match.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { uuid } from "../../../love/public/src/uuid.mjs";
@@ -5,12 +6,17 @@ import { reply_wrap_invoke } from "../../../love/public/src/reply_wrap_invoke.mj
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
+import { object_property_initialize } from "./object_property_initialize.mjs";
 export function reply_on_match(fn, on_match) {
   let matcher = async function reply_on_match_inner(possibilities) {
     list_is_assert(possibilities);
     let u = await uuid();
     function lambda(item) {
-      object_property_set(object, property_name, value);
+      let index = object_property_get(item, "index");
+      let d = object_property_initialize(item, "data", {});
+      object_property_set(d, u, {
+        index_before,
+      });
     }
     let mapped = list_map(possibilities, lambda);
     possibilities = await reply_wrap_invoke(fn, possibilities);
