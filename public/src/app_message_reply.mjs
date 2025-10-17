ed8875b86e0564b41d0ebfec1d3207262cd70ac1
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 15:39:47 -0400

    function_auto_path ..\love\public\src\app_message_reply.mjs

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -1,12 +1,10 @@
+import { app_message_download_messages } from "../../../love/public/src/app_message_download_messages.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_message_reply_choices } from "../../../love/public/src/app_message_reply_choices.mjs";
 import { reply_messages_matches_not } from "../../../love/public/src/reply_messages_matches_not.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { app_message_download } from "../../../love/public/src/app_message_download.mjs";
 export async function app_message_reply() {
   marker("1");
-  let downloads = await app_message_download();
-  let messages = list_map_property(downloads, "message");
+  let messages = await app_message_download_messages();
   let start = app_message_reply_choices();
   let result = await reply_messages_matches_not(messages, start);
   return result;
