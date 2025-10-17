0a8a056c84041e237b4eaf9eeb7d84bd6a7618c8
Grace-Jared <christrosetolife@gmail.com>
2025-10-16 21:15:54 -0400

    amr

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -117,7 +117,7 @@ export function app_message_reply_choices() {
       let { before, after } = value;
       const sliced = string_slice(message, before + 2, after + 2);
       log({
-        value: sliced,message
+        value: sliced,message,item
       });
     }
     each(filtered, lambda2);
