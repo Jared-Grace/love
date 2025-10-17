7398ffad09c72ae8e4ba0b1ee717009cdd933cbf
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 15:38:38 -0400

    rs reply_messages matches_not

--- public/src/app_message_reply.mjs
+++ public/src/app_message_reply.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_message_reply_choices } from "../../../love/public/src/app_message_reply_choices.mjs";
-import { reply_messages } from "../../../love/public/src/reply_messages.mjs";
+import { reply_messages_matches_not } from "../../../love/public/src/reply_messages_matches_not.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_message_download } from "../../../love/public/src/app_message_download.mjs";
 export async function app_message_reply() {
@@ -8,6 +8,6 @@ export async function app_message_reply() {
   let downloads = await app_message_download();
   let messages = list_map_property(downloads, "message");
   let start = app_message_reply_choices();
-  let result = reply_messages(messages, start);
+  let result = reply_messages_matches_not(messages, start);
   return result;
 }
