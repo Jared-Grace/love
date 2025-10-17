0d33386f34f2f6f808a43ffd50477d545fbd33c6
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 16:41:55 -0400

    function_auto_path ..\love\public\src\app_reply_choices.mjs

---
+++ public/src/app_reply_choices_praise.mjs
@@ -0,0 +1,8 @@
+import { emoji_voice } from "../../../love/public/src/emoji_voice.mjs";
+export function app_reply_choices_praise() {
+  let v2 =
+    "🎵 " +
+    emoji_voice() +
+    " Praise the LORD our God in the name of Jesus Christ! 🎶";
+  return v2;
+}
