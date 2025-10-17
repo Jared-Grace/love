6ab0ed6240485d8ab2453de98c53ec67a4937600
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 17:47:57 -0400

    function_auto_path ..\love\public\src\reply_choice_output.mjs

--- public/src/reply_choice_output.mjs
+++ public/src/reply_choice_output.mjs
@@ -1,7 +1,9 @@
+import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
 import { reply_choice } from "../../../love/public/src/reply_choice.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_choice_output(choices) {
   marker("1");
-  let fn = reply_choice(choices);
+  let fn_rc = reply_choice(choices);
+  let fn = reply_on_match_output(fn_rc, item);
   return fn;
 }
