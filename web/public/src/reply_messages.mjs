0730d5500420dbac4187a8ac92a3171c746dddb0
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 15:37:44 -0400

    function_auto_path ..\love\public\src\reply_messages.mjs

--- web/public/src/reply_messages.mjs
+++ web/public/src/reply_messages.mjs
@@ -1,36 +1,7 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { range_1 } from "../../../love/public/src/range_1.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { list_reverse } from "../../../love/public/src/list_reverse.mjs";
-import { string_size } from "../../../love/public/src/string_size.mjs";
+import { reply_messages_all } from "../../../love/public/src/reply_messages_all.mjs";
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
-import { reply_messages_inner } from "../../../love/public/src/reply_messages_inner.mjs";
-import { string_take } from "./string_take.mjs";
-import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 export async function reply_messages(messages, start) {
-  marker("1");
-  async function lambda(message) {
-    let result = await reply_messages_inner(message, start);
-    let value = object_property_get(result, "matches");
-    if (equal(value, false)) {
-      let size = string_size(message);
-      let r = range_1(size);
-      list_reverse(r);
-      async function lambda2(t) {
-        let taken = string_take(message, t);
-        let result_loop = await reply_messages_inner(taken, start);
-        value = object_property_get(result_loop, "matches");
-        if (equal(value, true)) {
-          return value;
-        }
-      }
-      await each_async(r, lambda2);
-    }
-    return result;
-  }
-  let result = await list_map_unordered_async(messages, lambda);
+  let result = await reply_messages_all(start, messages);
   let filtered = list_filter_property(result, "matches", false);
   return filtered;
 }
---
+++ web/public/src/reply_messages_all.mjs
@@ -0,0 +1,34 @@
+import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { string_take } from "../../../love/public/src/string_take.mjs";
+import { list_reverse } from "../../../love/public/src/list_reverse.mjs";
+import { range_1 } from "../../../love/public/src/range_1.mjs";
+import { string_size } from "../../../love/public/src/string_size.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { reply_messages_inner } from "../../../love/public/src/reply_messages_inner.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function reply_messages_all(start, messages) {
+  marker("1");
+  async function lambda(message) {
+    let result = await reply_messages_inner(message, start);
+    let value = object_property_get(result, "matches");
+    if (equal(value, false)) {
+      let size = string_size(message);
+      let r = range_1(size);
+      list_reverse(r);
+      async function lambda2(t) {
+        let taken = string_take(message, t);
+        let result_loop = await reply_messages_inner(taken, start);
+        value = object_property_get(result_loop, "matches");
+        if (equal(value, true)) {
+          return value;
+        }
+      }
+      await each_async(r, lambda2);
+    }
+    return result;
+  }
+  let result = await list_map_unordered_async(messages, lambda);
+  return result;
+}
