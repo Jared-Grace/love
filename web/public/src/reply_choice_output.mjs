ec5bfe1a7f9a7af9281d959df29d62a5f447dc3f
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 17:47:32 -0400

    function_auto_path ..\love\public\src\reply_choice_output.mjs

--- web/public/src/reply_choice_output.mjs
+++ web/public/src/reply_choice_output.mjs
@@ -2,5 +2,6 @@ import { reply_choice } from "../../../love/public/src/reply_choice.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_choice_output(choices) {
   marker("1");
-  return reply_choice(choices);
+  let fn = reply_choice(choices);
+  return fn;
 }
