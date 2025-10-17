3a0f6428e843c5e0f43440f7369e35c543f7b529
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 17:39:26 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -112,14 +112,10 @@ export function app_message_reply_choices() {
     ],
     item,
   );
-  let fn12 = reply_sequence([
-    "come",
-    "and",
-    "fellowship",
-    "with",
-    us,
-    according_to_gods_will,
-  ]);
+  let fn12 = reply_sequence_output(
+    ["come", "and", "fellowship", "with", us, according_to_gods_will],
+    "If God wills! Right now no money for travel",
+  );
   let thank_you = reply_sequence(["thank", "you"]);
   let thanks = reply_choice(["thanks", thank_you]);
   let d = digits();
