a6f8c70fd8c6f0bb86fc0477af6a2cda0dd2bd0a
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 16:29:32 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

---
+++ public/src/reply_sequence_output.mjs
@@ -0,0 +1,7 @@
+import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
+import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
+export function reply_sequence_output(sequence, greetings) {
+  let fn3 = reply_sequence(sequence);
+  let fn6 = reply_on_match_output(fn3, greetings);
+  return fn6;
+}
