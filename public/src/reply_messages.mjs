3a908ff2666a0c60d769e2ff00144ffa4352ca17
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 15:38:38 -0400

    rs reply_messages matches_not

--- public/src/reply_messages.mjs
+++
@@ -1,7 +0,0 @@
-import { reply_messages_all } from "../../../love/public/src/reply_messages_all.mjs";
-import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
-export async function reply_messages(messages, start) {
-  let result = await reply_messages_all(start, messages);
-  let filtered = list_filter_property(result, "matches", false);
-  return filtered;
-}
