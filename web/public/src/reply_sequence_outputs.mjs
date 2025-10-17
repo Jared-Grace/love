4035559f859779710c4920897262e1adcfab37ea
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 16:32:08 -0400

    c reply_sequence_output reply_sequence_outputs

--- web/public/src/reply_sequence_outputs.mjs
+++ web/public/src/reply_sequence_outputs.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 export function reply_sequence_outputs(sequence, greetings) {
+  marker("1");
   let fn3 = reply_sequence(sequence);
   let fn6 = reply_on_match_output(fn3, greetings);
   return fn6;
