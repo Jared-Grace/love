299ffaeb23f62316eec106dcebf97d4d936488f0
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 15:38:50 -0400

    function_auto_path ..\love\public\src\reply_messages_matches.mjs

---
+++ web/public/src/reply_messages_matches.mjs
@@ -0,0 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { reply_messages_all } from "../../../love/public/src/reply_messages_all.mjs";
+import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
+export async function reply_messages_matches(messages, start) {
+  marker("1");
+  let result = await reply_messages_all(start, messages);
+  let filtered = list_filter_property(result, "matches", false);
+  return filtered;
+}
