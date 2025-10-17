ae02be218a406988d096fa102dfb094acd045602
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 16:35:06 -0400

    function_auto_path ..\love\public\src\reply_on_match_output.mjs

--- web/public/src/reply_on_match_output.mjs
+++ web/public/src/reply_on_match_output.mjs
@@ -4,9 +4,10 @@ import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs"
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 export function reply_on_match_output(fn_a, item) {
   assert_arguments(arguments, 2);
+  let fn_item = reply_on_match_output_add;
   function lambda(possibilities) {
     function lambda2(possibility) {
-      reply_on_match_output_add(possibility, item);
+      fn_item(possibility, item);
     }
     each(possibilities, lambda2);
     return possibilities;
