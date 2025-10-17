8877ba2061ea7598095b9a236591c3da2cc84187
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 17:48:58 -0400

    function_auto_path ..\love\public\src\trinity_green.mjs

--- public/src/trinity_green.mjs
+++ public/src/trinity_green.mjs
@@ -1,5 +1,6 @@
+import { emoji_trinity } from "../../../love/public/src/emoji_trinity.mjs";
 import { chalk_green } from "../../../love/public/src/chalk_green.mjs";
 export async function trinity_green() {
-  let s = await chalk_green("❤️‍🔥✟🕊️ ");
+  let s = await chalk_green(emoji_trinity() + " ");
   return s;
 }
