282fa8c44cda35d2aca38091b0cffddd93b3fab7
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 17:47:30 -0400

    fws rc output

--- web/public/src/reply_choice_output.mjs
+++ web/public/src/reply_choice_output.mjs
@@ -1,4 +1,6 @@
+import { reply_choice } from "../../../love/public/src/reply_choice.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function reply_choice_output() {
+export function reply_choice_output(choices) {
   marker("1");
+  return reply_choice(choices);
 }
