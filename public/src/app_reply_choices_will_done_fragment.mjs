91296cb234bd7c09bea8223d85010ca254fcece0
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 17:41:45 -0400

    function_auto_path ..\love\public\src\app_reply_choices.mjs

---
+++ public/src/app_reply_choices_will_done_fragment.mjs
@@ -0,0 +1,5 @@
+import { emoji_dove } from "../../../love/public/src/emoji_dove.mjs";
+export function app_reply_choices_will_done_fragment() {
+  let v2 = "have His will done " + emoji_dove();
+  return v2;
+}
