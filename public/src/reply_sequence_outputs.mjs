8165906a167911524a3d92df12b75a72a454f1a0
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 16:36:00 -0400

    function_auto_path ..\love\public\src\reply_sequence_outputs.mjs

--- public/src/reply_sequence_outputs.mjs
+++ public/src/reply_sequence_outputs.mjs
@@ -1,9 +1,9 @@
 import { reply_on_match_output_multiple } from "../../../love/public/src/reply_on_match_output_multiple.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
-export function reply_sequence_outputs(sequence, greetings) {
+export function reply_sequence_outputs(sequence, outputs) {
   marker("1");
   let fn3 = reply_sequence(sequence);
-  let fn6 = reply_on_match_output_multiple(fn3, greetings);
+  let fn6 = reply_on_match_output_multiple(fn3, outputs);
   return fn6;
 }
