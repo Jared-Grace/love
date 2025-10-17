877615692fdad35e3d242aca74046879dc5b5069
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 16:35:34 -0400

    data_file_update ..\love\public\src\reply_on_match_output_multiple.mjs

--- public/src/reply_on_match_output_multiple.mjs
+++ public/src/reply_on_match_output_multiple.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_on_match_output_generic } from "../../../love/public/src/reply_on_match_output_generic.mjs";
 import { reply_on_match_output_add } from "../../../love/public/src/reply_on_match_output_add.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export function reply_on_match_output_multiple(fn_a, item) {
+  marker("1");
   assert_arguments(arguments, 2);
   let fn_item = reply_on_match_output_add;
   let fn = reply_on_match_output_generic(fn_item, item, fn_a);
