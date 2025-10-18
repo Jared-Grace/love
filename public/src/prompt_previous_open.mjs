b99a4186d2063be38c68fef766766e0299c7b76f
Grace-Jared <christrosetolife@gmail.com>
2025-10-17 22:48:03 -0400

    function_auto_path ..\love\public\src\prompt_previous_open.mjs

--- public/src/prompt_previous_open.mjs
+++ public/src/prompt_previous_open.mjs
@@ -1,9 +1,11 @@
+import { function_run } from "../../../love/public/src/function_run.mjs";
+import { function_run_line_parse } from "../../../love/public/src/function_run_line_parse.mjs";
 import { prompt_previous_get } from "../../../love/public/src/prompt_previous_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { function_run_line } from "../../../love/public/src/function_run_line.mjs";
 export async function prompt_previous_open() {
   marker("1");
   let previous = await prompt_previous_get();
-  let v = await function_run_line(previous);
+  let { f_name, args } = await function_run_line_parse(previous);
+  let v = await function_run(f_name, args);
   return v;
 }
