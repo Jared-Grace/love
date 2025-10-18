84da9d118f9f1a720625f59adce5c7b0a0d35fe7
Grace-Jared <christrosetolife@gmail.com>
2025-10-18 18:17:54 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

---
+++ public/src/app_message_reply_jesus.mjs
@@ -0,0 +1,7 @@
+import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
+import { reply_phrase_jesus } from "../../../love/public/src/reply_phrase_jesus.mjs";
+export function app_message_reply_jesus() {
+  let phrase_jesus = reply_phrase_jesus();
+  let fn2 = reply_on_match_output(phrase_jesus, "✝️ Jesus is Lord! 👑");
+  return fn2;
+}
