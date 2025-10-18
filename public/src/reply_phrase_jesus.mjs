ccf826e71592b392f1c5b8842357feec2af1e094
Grace-Jared <christrosetolife@gmail.com>
2025-10-18 14:30:06 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

---
+++ public/src/reply_phrase_jesus.mjs
@@ -0,0 +1,7 @@
+import { reply_either_both } from "../../../love/public/src/reply_either_both.mjs";
+export function reply_phrase_jesus() {
+  const jesus = "jesus";
+  const christ = "christ";
+  let phrase_jesus = reply_either_both(jesus, christ);
+  return phrase_jesus;
+}
