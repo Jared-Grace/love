8479433c67e4008b47107a54523388b1a57b26f3
Grace-Jared <christrosetolife@gmail.com>
2025-10-18 19:41:40 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- web/public/src/app_message_reply_choices.mjs
+++ web/public/src/app_message_reply_choices.mjs
@@ -54,8 +54,8 @@ export function app_message_reply_choices() {
   let names = reply_names();
   let names_once_or_more = reply_once_or_more(names);
   let o_n = reply_optional("n");
-  let o_a = reply_sequence_optional(["a", o_n]);
-  let title = reply_sequence([iam, o_a, titles]);
+  let o_a_an = reply_sequence_optional(["a", o_n]);
+  let title = reply_sequence([iam, o_a_an, titles]);
   let item3 = prayer_blessing("your ministry");
   let fn3 = reply_on_match_output(title, item3);
   let item4 = app_reply_choices_name();
