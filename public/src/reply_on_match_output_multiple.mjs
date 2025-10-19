54beda56f131f6eb5c804e2c5bcd8f8043ae5694
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:11:58 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_multiple.mjs

--- public/src/reply_on_match_output_multiple.mjs
+++ public/src/reply_on_match_output_multiple.mjs
@@ -6,6 +6,7 @@ export function reply_on_match_output_multiple(fn_a, items) {
   marker("1");
   assert_arguments(arguments, 2);
   let on_match = reply_on_match_output_multiple_lambda(items);
+  let on_match2 = reply_on_match_output_multiple_lambda(items);
   let fn = reply_on_match(fn_a, [on_match]);
   return fn;
 }
