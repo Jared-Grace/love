44b34656d549b935e60a36b464f38b95522c7c0b
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:09:48 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_add_multiple.mjs

--- web/public/src/reply_on_match_output_add_multiple.mjs
+++ web/public/src/reply_on_match_output_add_multiple.mjs
@@ -3,6 +3,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function reply_on_match_output_add_multiple(possibility, items) {
   marker("1");
-  let outputs = object_property_get(possibility, "outputs");
+  const property_name = "outputs";
+  let outputs = object_property_get(possibility, property_name);
   list_add_multiple(outputs, items);
 }
