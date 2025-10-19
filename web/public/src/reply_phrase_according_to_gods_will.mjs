c27b96aaed37c61b20c5b2724ad1ee11ecf15f4e
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 09:58:26 -0400

    function_auto_path ..\love\public\src\reply_phrase_according_to_gods_will.mjs

--- web/public/src/reply_phrase_according_to_gods_will.mjs
+++ web/public/src/reply_phrase_according_to_gods_will.mjs
@@ -1,7 +1,9 @@
+import { reply_word_to } from "../../../love/public/src/reply_word_to.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 import { reply_sequence_optional } from "../../../love/public/src/reply_sequence_optional.mjs";
 export function reply_phrase_according_to_gods_will() {
-  let o_according_to = reply_sequence_optional(["according", "to"]);
+  let v = reply_word_to();
+  let o_according_to = reply_sequence_optional(["according", v]);
   let according_to_gods_will = reply_sequence([
     o_according_to,
     "god's",
