bc1eb8ac0ff0486764b7227d5d39d5a7b00dd57e
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:31:17 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_generic.mjs

--- public/src/reply_on_match_output_generic.mjs
+++ public/src/reply_on_match_output_generic.mjs
@@ -1,13 +1,13 @@
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function reply_on_match_output_generic(fn_item, item, fn_a) {
-  function lambda(possibilities) {
+  function on_match(possibilities) {
     function lambda2(possibility) {
       fn_item(possibility, item);
     }
     each(possibilities, lambda2);
     return possibilities;
   }
-  let fn = reply_on_match(fn_a, lambda);
+  let fn = reply_on_match(fn_a, on_match);
   return fn;
 }
