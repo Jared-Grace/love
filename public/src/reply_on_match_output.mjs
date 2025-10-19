cf0e4421000b5bea2b901ca2c167cf7b5801e81c
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:07:01 -0400

    o reply_on_match_output

--- public/src/reply_on_match_output.mjs
+++ public/src/reply_on_match_output.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { reply_on_match_output_multiple } from "../../../love/public/src/reply_on_match_output_multiple.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export function reply_on_match_output(fn_a, item) {
+  marker("1");
   assert_arguments(arguments, 2);
   let fn = reply_on_match_output_multiple(fn_a, [item]);
   return fn;
