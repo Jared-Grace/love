caca27f07e1314611c8cb1123425dacae4c95b19
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:11:02 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_multiple_lambda.mjs

--- web/public/src/reply_on_match_output_multiple_lambda.mjs
+++ web/public/src/reply_on_match_output_multiple_lambda.mjs
@@ -1,9 +1,13 @@
+import { reply_on_match_property_add_multiple } from "../../../love/public/src/reply_on_match_property_add_multiple.mjs";
+import { reply_property_outputs } from "../../../love/public/src/reply_property_outputs.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { reply_on_match_output_add_multiple } from "../../../love/public/src/reply_on_match_output_add_multiple.mjs";
 export function reply_on_match_output_multiple_lambda(items) {
   let v = function lambda(possibilities) {
     function lambda2(possibility) {
-      reply_on_match_output_add_multiple(possibility, items);
+      marker("1");
+      const property_name = reply_property_outputs();
+      reply_on_match_property_add_multiple(possibility, property_name, items);
     }
     each(possibilities, lambda2);
     return possibilities;
