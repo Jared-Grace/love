417c5e4b590033a52483338828485df713bb27fb
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:55:36 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- public/src/reply_on_match.mjs
+++ public/src/reply_on_match.mjs
@@ -1,25 +1,9 @@
-import { reply_wrap_invoke } from "../../../love/public/src/reply_wrap_invoke.mjs";
-import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
-import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
-import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
+import { reply_on_match_generic } from "../../../love/public/src/reply_on_match_generic.mjs";
 import { noop } from "./noop.mjs";
 export function reply_on_match(fn, lambda) {
   let before = noop;
   let after = noop;
   let on_args = noop;
-  let matcher = async function reply_on_match_inner(possibilities) {
-    list_is_assert(possibilities);
-    possibilities = await reply_wrap_invoke(fn, possibilities);
-    before(possibilities);
-    let filtered = reply_matches(possibilities);
-    after(filtered);
-    let ne = list_empty_not_is(filtered);
-    if (ne) {
-      let args = [filtered];
-      on_args(args);
-      lambda(...args);
-    }
-    return filtered;
-  };
+  let matcher = reply_on_match_generic(fn, before, after, on_args, lambda);
   return matcher;
 }
