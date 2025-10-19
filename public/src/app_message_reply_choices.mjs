63d044f75a28d0ab64981f7ae8c88752a1f2340f
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:25:58 -0400

    function_auto_path ..\love\public\src\app_message_reply_choices.mjs

--- public/src/app_message_reply_choices.mjs
+++ public/src/app_message_reply_choices.mjs
@@ -53,7 +53,7 @@ export function app_message_reply_choices() {
     us,
     o_according_to_gods_will,
   ]);
-  let o = reply_on_match_outputs_codes_multiple(s, will_be_done);
+  let o = reply_on_match_outputs_codes_multiple(s, [will_be_done]);
   let you2 = reply_phrase_you();
   let thank_you = reply_sequence(["thank", you2]);
   let item6 = app_reply_choices_glory();
