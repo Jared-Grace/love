1f471ac5618473e7753ad65acaa0c5e8e2d55f09
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:33:51 -0400

    function_auto_path ..\love\public\src\reply_on_match_output.mjs

--- public/src/reply_on_match_output.mjs
+++ public/src/reply_on_match_output.mjs
@@ -1,9 +1,7 @@
-import { reply_on_match_output_generic } from "../../../love/public/src/reply_on_match_output_generic.mjs";
-import { reply_on_match_output_add } from "../../../love/public/src/reply_on_match_output_add.mjs";
+import { reply_on_match_output_multiple } from "../../../love/public/src/reply_on_match_output_multiple.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export function reply_on_match_output(fn_a, item) {
   assert_arguments(arguments, 2);
-  let fn_item = reply_on_match_output_add;
-  let fn = reply_on_match_output_generic(fn_item, item, fn_a);
+  let fn = reply_on_match_output_multiple(fn_a, [item]);
   return fn;
 }
