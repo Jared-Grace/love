5563cc72fc02cbfacfac796b37572a3b74ac671b
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:10:56 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_add_multiple.mjs

--- public/src/reply_on_match_output_add_multiple.mjs
+++ public/src/reply_on_match_output_add_multiple.mjs
@@ -1,7 +1,8 @@
+import { reply_property_outputs } from "../../../love/public/src/reply_property_outputs.mjs";
 import { reply_on_match_property_add_multiple } from "../../../love/public/src/reply_on_match_property_add_multiple.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_on_match_output_add_multiple(possibility, items) {
   marker("1");
-  const property_name = "outputs";
+  const property_name = reply_property_outputs();
   reply_on_match_property_add_multiple(possibility, property_name, items);
 }
