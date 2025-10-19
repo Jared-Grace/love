f8e36639d75774775a3998594839f3247d3161b4
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:08:39 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_multiple.mjs

--- public/src/reply_on_match_output_multiple.mjs
+++ public/src/reply_on_match_output_multiple.mjs
@@ -1,18 +1,11 @@
+import { reply_on_match_output_multiple_lambda } from "../../../love/public/src/reply_on_match_output_multiple_lambda.mjs";
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { reply_on_match_output_add_multiple } from "../../../love/public/src/reply_on_match_output_add_multiple.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export function reply_on_match_output_multiple(fn_a, items) {
   marker("1");
   assert_arguments(arguments, 2);
-  let on_match = function lambda(possibilities) {
-    function lambda2(possibility) {
-      reply_on_match_output_add_multiple(possibility, items);
-    }
-    each(possibilities, lambda2);
-    return possibilities;
-  };
+  let on_match = reply_on_match_output_multiple_lambda(items);
   let fn = reply_on_match(fn_a, [on_match]);
   return fn;
 }
