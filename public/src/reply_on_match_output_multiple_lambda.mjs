d2d58f926e308807282873b7644d1d9e944e5b80
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:11:28 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_multiple_lambda.mjs

--- public/src/reply_on_match_output_multiple_lambda.mjs
+++ public/src/reply_on_match_output_multiple_lambda.mjs
@@ -1,16 +1,7 @@
-import { reply_on_match_property_add_multiple } from "../../../love/public/src/reply_on_match_property_add_multiple.mjs";
+import { reply_on_match_property_multiple_lambda } from "../../../love/public/src/reply_on_match_property_multiple_lambda.mjs";
 import { reply_property_outputs } from "../../../love/public/src/reply_property_outputs.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 export function reply_on_match_output_multiple_lambda(items) {
   const property_name = reply_property_outputs();
-  let v = function lambda(possibilities) {
-    function lambda2(possibility) {
-      marker("1");
-      reply_on_match_property_add_multiple(possibility, property_name, items);
-    }
-    each(possibilities, lambda2);
-    return possibilities;
-  };
+  let v = reply_on_match_property_multiple_lambda(property_name, items);
   return v;
 }
