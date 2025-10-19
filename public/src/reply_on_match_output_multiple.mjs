fbcb5e2bb5f90486e91bc99b6534f627d58cb306
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:33:25 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_multiple.mjs

--- public/src/reply_on_match_output_multiple.mjs
+++ public/src/reply_on_match_output_multiple.mjs
@@ -1,11 +1,18 @@
+import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
+import { each } from "../../../love/public/src/each.mjs";
 import { reply_on_match_output_add_multiple } from "../../../love/public/src/reply_on_match_output_add_multiple.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { reply_on_match_output_generic } from "../../../love/public/src/reply_on_match_output_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-export function reply_on_match_output_multiple(fn_a, item) {
+export function reply_on_match_output_multiple(fn_a, items) {
   marker("1");
   assert_arguments(arguments, 2);
-  let fn_item = reply_on_match_output_add_multiple;
-  let fn = reply_on_match_output_generic(fn_item, item, fn_a);
+  function on_match(possibilities) {
+    function lambda2(possibility) {
+      reply_on_match_output_add_multiple(possibility, items);
+    }
+    each(possibilities, lambda2);
+    return possibilities;
+  }
+  let fn = reply_on_match(fn_a, on_match);
   return fn;
 }
