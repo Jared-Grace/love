66652f1859fca01ebfa4380f076bcb67d95f9b81
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:28:56 -0400

    function_auto_path ..\love\public\src\reply_sequence_output.mjs

--- public/src/reply_sequence_output.mjs
+++ public/src/reply_sequence_output.mjs
@@ -1,7 +1,7 @@
 import { reply_on_match_output } from "../../../love/public/src/reply_on_match_output.mjs";
 import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 export function reply_sequence_output(sequence, item) {
-  let fn3 = reply_sequence(sequence);
-  let fn6 = reply_on_match_output(fn3, item);
-  return fn6;
+  let s = reply_sequence(sequence);
+  let o = reply_on_match_output(s, item);
+  return o;
 }
