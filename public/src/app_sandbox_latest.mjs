f554767e3d4d0bad973a551378d5f9aab591ab96
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:53:38 -0400

    function_auto_path ..\love\public\src\app_sandbox_latest.mjs

--- public/src/app_sandbox_latest.mjs
+++ public/src/app_sandbox_latest.mjs
@@ -1,4 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_sandbox_latest() {
-  marker("1");
+export async function app_sandbox_latest() {
+  await app_main_latest("app_sandbox_main", "jared-grace");
 }
