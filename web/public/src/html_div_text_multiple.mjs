f61660dad78daf52e8566e4ab3bb7d2573f2f01d
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 15:14:38 -0400

    data_file_update ..\love\public\src\html_div_text_multiple.mjs

--- web/public/src/html_div_text_multiple.mjs
+++ web/public/src/html_div_text_multiple.mjs
@@ -4,7 +4,7 @@ import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 export function html_div_text_multiple(parent, list) {
   marker("1");
   function lambda2(item) {
-    html_p_text(parent, item);
+    html_div_text(parent, item);
   }
   each(list, lambda2);
 }
