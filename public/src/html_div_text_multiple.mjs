745211fc9da0c8c794bca89209adbb522ad5dd41
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 15:14:39 -0400

    function_auto_path ..\love\public\src\html_div_text_multiple.mjs

--- public/src/html_div_text_multiple.mjs
+++ public/src/html_div_text_multiple.mjs
@@ -1,6 +1,6 @@
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 export function html_div_text_multiple(parent, list) {
   marker("1");
   function lambda2(item) {
