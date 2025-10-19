1a5bce15bba4e24854c38e49b47b0d5318dd8de1
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:58:50 -0400

    function_auto_path ..\love\public\src\reply_on_match_capture.mjs

--- public/src/reply_on_match_capture.mjs
+++ public/src/reply_on_match_capture.mjs
@@ -1,14 +1,9 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { reply_on_match_generic } from "../../../love/public/src/reply_on_match_generic.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { uuid } from "../../../love/public/src/uuid.mjs";
-import { reply_wrap_invoke } from "../../../love/public/src/reply_wrap_invoke.mjs";
-import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
-import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
-import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
 export function reply_on_match_capture(fn, lambda) {
   function capture(possibilities, property, u) {
@@ -32,18 +27,5 @@ export function reply_on_match_capture(fn, lambda) {
     list_add(args, u);
   };
   let matcher = reply_on_match_generic(fn, before, after, on_args, lambda);
-  return matcher;
-  marker("1");
-  async function reply_on_match_inner(possibilities) {
-    list_is_assert(possibilities);
-    possibilities = await reply_wrap_invoke(fn, possibilities);
-    let filtered = reply_matches(possibilities);
-    capture(filtered, "after");
-    let ne = list_empty_not_is(filtered);
-    if (ne) {
-      lambda(filtered, u);
-    }
-    return filtered;
-  }
   return matcher;
 }
