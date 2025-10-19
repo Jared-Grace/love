795415ad46a9db85f540c0fc108f5d21a6048529
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:14:11 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_codes_multiple.mjs

--- public/src/reply_on_match_output_codes_multiple.mjs
+++ public/src/reply_on_match_output_codes_multiple.mjs
@@ -3,12 +3,15 @@ import { reply_on_match_output_multiple_lambda } from "../../../love/public/src/
 import { reply_on_match } from "../../../love/public/src/reply_on_match.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-export function reply_on_match_output_codes_multiple(fn_a, items) {
+export function reply_on_match_output_codes_multiple(fn_a, outputs, codes) {
   marker("1");
-  assert_arguments(arguments, 2);
-  let on_match = reply_on_match_output_multiple_lambda(items);
+  assert_arguments(arguments, 3);
+  let on_match = reply_on_match_output_multiple_lambda(outputs);
   const property_name = "codes";
-  let on_match2 = reply_on_match_property_multiple_lambda(property_name, items);
+  let on_match2 = reply_on_match_property_multiple_lambda(
+    property_name,
+    outputs,
+  );
   let fn = reply_on_match(fn_a, [on_match]);
   return fn;
 }
