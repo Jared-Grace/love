43d4a5d65cc43882067cd155f9012c2013b8077d
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:11:07 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_multiple_lambda.mjs

--- public/src/reply_on_match_output_multiple_lambda.mjs
+++ public/src/reply_on_match_output_multiple_lambda.mjs
@@ -3,10 +3,10 @@ import { reply_property_outputs } from "../../../love/public/src/reply_property_
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function reply_on_match_output_multiple_lambda(items) {
+  const property_name = reply_property_outputs();
   let v = function lambda(possibilities) {
     function lambda2(possibility) {
       marker("1");
-      const property_name = reply_property_outputs();
       reply_on_match_property_add_multiple(possibility, property_name, items);
     }
     each(possibilities, lambda2);
