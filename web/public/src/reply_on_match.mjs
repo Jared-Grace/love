267f331f25b5bfa27a7319964ab6bb7117fa8c69
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 12:32:10 -0400

    function_auto_path ..\love\public\src\reply_on_match.mjs

--- web/public/src/reply_on_match.mjs
+++ web/public/src/reply_on_match.mjs
@@ -7,7 +7,7 @@ import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { reply_matches } from "../../../love/public/src/reply_matches.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { object_property_initialize } from "./object_property_initialize.mjs";
-export function reply_on_match(fn, on_match) {
+export function reply_on_match(fn, lambda) {
   let matcher = async function reply_on_match_inner(possibilities) {
     list_is_assert(possibilities);
     let u = await uuid();
@@ -26,7 +26,7 @@ export function reply_on_match(fn, on_match) {
     capture(filtered, "after");
     let ne = list_empty_not_is(filtered);
     if (ne) {
-      on_match(filtered, u);
+      lambda(filtered, u);
     }
     return filtered;
   };
