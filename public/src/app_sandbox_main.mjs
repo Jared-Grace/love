901ba4cde0ec174fab28a620981ceef799ee037e
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 16:51:42 -0400

    function_auto_path ..\love\public\src\app_sandbox_main.mjs

--- public/src/app_sandbox_main.mjs
+++ public/src/app_sandbox_main.mjs
@@ -1,7 +1,12 @@
 import { html_checkboxes } from "../../../love/public/src/html_checkboxes.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { html_document_body } from "./html_document_body.mjs";
 export function app_sandbox_main() {
+  let root = html_document_body();
   marker("1");
+  let context = {
+    root,
+  };
   html_checkboxes(
     context,
     button_back,
