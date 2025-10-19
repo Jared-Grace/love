db69e926c40405c320de54d85c88a6c9f88eceef
Grace-Jared <christrosetolife@gmail.com>
2025-10-19 13:36:10 -0400

    function_auto_path ..\love\public\src\reply_on_match_output_code.mjs

--- public/src/reply_on_match_output_code.mjs
+++ public/src/reply_on_match_output_code.mjs
@@ -2,6 +2,7 @@ import { reply_on_match_outputs_codes_multiple } from "../../../love/public/src/
 import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_on_match_output_code(fn_a, outputs, codes) {
   marker("1");
-  let fn = reply_on_match_outputs_codes_multiple(fn_a, outputs, codes);
+  let fn = reply_on_match_outputs_codes_multiple(fn_a, [outputs], [codes]);
   return fn;
+  [];
 }
