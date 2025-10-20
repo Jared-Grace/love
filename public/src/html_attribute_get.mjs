8f4fca100ec14e8405af0aac5790b1e9abf44ff2
Grace-Jared <christrosetolife@gmail.com>
2025-10-20 15:44:05 -0400

    function_auto_path ..\love\public\src\html_attribute_get.mjs

--- public/src/html_attribute_get.mjs
+++ public/src/html_attribute_get.mjs
@@ -1,8 +1,8 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_attribute_get(component, key, value) {
+export function html_attribute_get(component, key) {
   marker("1");
   let element = html_component_element_get(component);
-  let v = element.getAttribute(key);
-  return v;
+  let value = element.getAttribute(key);
+  return value;
 }
