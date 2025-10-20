8b567a28d3c76ada4f53f8d6f3624094646400c4
Grace-Jared <christrosetolife@gmail.com>
2025-10-20 14:43:40 -0400

    function_auto_path ..\karate_code\public\src\app_karate_button_back.mjs

---
+++ public/src/app_karate_button_back_text.mjs
@@ -0,0 +1,5 @@
+import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs";
+export function app_karate_button_back_text() {
+  let v = emoji_arrow_left() + " Back";
+  return v;
+}
