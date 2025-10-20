8cf5edac5bd78696440e70b6c6b539f8b3243713
Grace-Jared <christrosetolife@gmail.com>
2025-10-20 15:43:17 -0400

    c html_attribute_set html_attribute_get

--- public/src/html_attribute_get.mjs
+++ public/src/html_attribute_get.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_attribute_get(component, key, value) {
+  marker("1");
   let element = html_component_element_get(component);
   element.setAttribute(key, value);
 }
