678122a001c0c2094c5b5c21233227b9ed4e8cbb
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 13:59:26 -0400

    fw string_lower_to string_first_lower_to

--- web/public/src/string_first_lower_to.mjs
+++ web/public/src/string_first_lower_to.mjs
@@ -1,4 +1,6 @@
+import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function string_first_lower_to() {
+export function string_first_lower_to(s) {
   marker("1");
+  return string_lower_to(s);
 }
