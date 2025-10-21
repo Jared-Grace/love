d7a637a77305adfc19c82852fd74882ac77a0c63
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 17:48:52 -0400

    function_auto_path ..\love\public\src\each_object.mjs

--- public/src/each_object.mjs
+++ public/src/each_object.mjs
@@ -2,10 +2,10 @@ import { each } from "../../../love/public/src/each.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function each_object(object, lambda) {
-  function lambda2(p) {
-    let value = object_property_get(object, p);
-    lambda(value, p);
+  function lambda2(property) {
+    let value = object_property_get(object, property);
+    lambda(value, property);
   }
-  let list = object_properties(object);
-  each(list, lambda2);
+  let properties = object_properties(object);
+  each(properties, lambda2);
 }
