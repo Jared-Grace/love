1d1e0750f68289a245508e884ed987bdbf547ecd
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 17:53:04 -0400

    function_auto_path ..\love\public\src\function_alias_add.mjs

--- public/src/function_alias_add.mjs
+++ public/src/function_alias_add.mjs
@@ -1,5 +1,4 @@
-import { assert } from "../../../love/public/src/assert.mjs";
-import { js_identifier_is } from "../../../love/public/src/js_identifier_is.mjs";
+import { js_identifier_is_assert } from "../../../love/public/src/js_identifier_is_assert.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { undefined_not_is_assert } from "../../../love/public/src/undefined_not_is_assert.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -13,8 +12,7 @@ export async function function_alias_add(first, second) {
   list_sort_string_size(list);
   let [alias, f_name] = list;
   let expression = js_parse_expression(f_name);
-  let ii = js_identifier_is(expression);
-  assert(ii);
+  js_identifier_is_assert(expression);
   undefined_not_is_assert(f_name);
   function lambda(a) {
     let { exists, aliases, unaliased } = a;
