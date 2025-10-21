0e57b3ca42e9aabc420ac0e71bc5af7e0e221e01
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 13:59:28 -0400

    function_auto_path ..\love\public\src\string_first_lower_to.mjs

--- public/src/string_first_lower_to.mjs
+++ public/src/string_first_lower_to.mjs
@@ -2,5 +2,6 @@ import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_first_lower_to(s) {
   marker("1");
-  return string_lower_to(s);
+  let lower = string_lower_to(s);
+  return lower;
 }
