9f9853dd49fdd91563d8877e13f2f2ec1bee9608
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 17:47:14 -0400

    function_auto_path ..\love\public\src\app_autopray_latest.mjs

--- public/src/app_autopray_latest.mjs
+++ public/src/app_autopray_latest.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
 export async function app_autopray_latest() {
   await app_main_latest("app_autopray_main", "jared-grace");
 }
