a70278683169dc221c2b8725fcf20e2d8ecb7948
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 17:50:15 -0400

    function_auto_path ..\love\public\src\app_autopray_main.mjs

--- web/public/src/app_autopray_main.mjs
+++ web/public/src/app_autopray_main.mjs
@@ -1,7 +1,7 @@
-import { each_object } from "../../../love/public/src/each_object.mjs";
+import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
 import { kjv } from "../../../love/public/src/kjv.mjs";
-export function app_autopray_main() {
+export async function app_autopray_main() {
   let v = kjv();
-  function lambda2() {}
-  each_object(object, lambda2);
+  function lambda2(verse_text, verse_reference) {}
+  await each_object_async(object, lambda2);
 }
