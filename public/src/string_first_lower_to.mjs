00144c0a810af6818ba94e15576c8c4407b0d678
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 13:59:49 -0400

    function_auto_path ..\love\public\src\string_first_lower_to.mjs

--- public/src/string_first_lower_to.mjs
+++ public/src/string_first_lower_to.mjs
@@ -1,8 +1,10 @@
+import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_first_lower_to(s) {
   marker("1");
+  let split = string_split_empty(s2);
   let result = list_first_remaining(list);
   let lower = string_lower_to(s);
   return lower;
