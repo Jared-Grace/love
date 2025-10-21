cb4fb431169bfb8b429f4b51ceb81729413269f8
Grace-Jared <christrosetolife@gmail.com>
2025-10-21 17:54:17 -0400

    function_auto_path ..\love\public\src\app_autopray_main.mjs

--- public/src/app_autopray_main.mjs
+++ public/src/app_autopray_main.mjs
@@ -1,3 +1,4 @@
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
@@ -7,6 +8,7 @@ export async function app_autopray_main() {
   let body = html_document_body();
   let v = kjv();
   async function lambda2(verse_text, verse_reference) {
+    html_clear(element);
     html_p_text_multiple(body, [verse_text, verse_reference]);
     await sleep(10);
   }
