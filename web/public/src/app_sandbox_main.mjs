ac932e40088f8a91d9f1ff1fc69f4b7113e95155
Grace-Jared <christrosetolife@gmail.com>
2025-10-22 18:54:58 -0400

    function_auto_path ..\love\public\src\app_sandbox_main.mjs

--- web/public/src/app_sandbox_main.mjs
+++ web/public/src/app_sandbox_main.mjs
@@ -3,6 +3,7 @@ import { app_message } from "../../../love/public/src/app_message.mjs";
 import { app_message_provide_travel } from "../../../love/public/src/app_message_provide_travel.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_document_body } from "./html_document_body.mjs";
+import { app_message_provide_food } from "./app_message_provide_food.mjs";
 export function app_sandbox_main() {
   let root = html_document_body();
   marker("1");
@@ -10,6 +11,10 @@ export function app_sandbox_main() {
   let context = {
     app_fn,
     root,
+    screens: {
+      provide_travel: app_message_provide_travel,
+      app_message_provide_food: app_message_provide_food,
+    },
   };
   let screen_name = storage_local_initialize(
     app_fn,
