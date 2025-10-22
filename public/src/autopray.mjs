41b145b11a385489c75a65e6ea2f63208d0bc4f2
Grace-Jared <christrosetolife@gmail.com>
2025-10-22 12:17:22 -0400

    function_auto_path ..\love\public\src\autopray.mjs

--- public/src/autopray.mjs
+++ public/src/autopray.mjs
@@ -2,22 +2,18 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { string_may_the_lord } from "../../../love/public/src/string_may_the_lord.mjs";
 import { prayer_end } from "../../../love/public/src/prayer_end.mjs";
 import { prayer_start } from "../../../love/public/src/prayer_start.mjs";
-import { html_clear } from "../../../love/public/src/html_clear.mjs";
-import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
 import { kjv } from "../../../love/public/src/kjv.mjs";
 import { sleep } from "./sleep.mjs";
+import { list_join_newline } from "./list_join_newline.mjs";
 export async function autopray() {
   marker("1");
-  let body = html_document_body();
   let v = kjv();
   async function lambda2(verse_text, verse_reference) {
-    html_clear(body);
     let v2 = prayer_start();
     let v3 = prayer_end();
     let v4 = string_may_the_lord();
-    html_p_text_multiple(body, [
+    list_join_newline(body, [
       v2,
       v4,
       "lead all creation to hear, believe, obey, enjoy and proclaim the word of God:",
