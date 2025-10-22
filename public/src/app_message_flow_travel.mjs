f80f53f17cffa82b81da0f05f95d17dc4e919536
Grace-Jared <christrosetolife@gmail.com>
2025-10-22 18:38:47 -0400

    function_auto_path ..\love\public\src\app_message_provide_travel.mjs

---
+++ public/src/app_message_flow_travel.mjs
@@ -0,0 +1,12 @@
+import { app_message_provide_travel } from "../../../love/public/src/app_message_provide_travel.mjs";
+export function app_message_flow_travel() {
+  let v3 = [
+    {
+      fn: app_message_provide_travel,
+    },
+    {
+      fn: app_message_provide_travel,
+    },
+  ];
+  return v3;
+}
