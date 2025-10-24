6a99a5cc99114bb23c30f3306a5ccf1a733ca6eb
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 09:15:07 -0400

    function_auto_path ..\love\public\src\list_all.mjs

--- public/src/list_all.mjs
+++ public/src/list_all.mjs
@@ -4,9 +4,4 @@ export function list_all(list, lambda$item) {
   let list2 = list_filter(list, lambda$item);
   let a = list_size(list2) === list_size(list);
   return a;
-  {
-    let filtered = list_filter(list, lambda$item);
-    let v2 = list_size(filtered) === list_size(list);
-    return v2;
-  }
 }
