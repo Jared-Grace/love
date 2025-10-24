fb7d1fdbb17391041c02443d5b3c4207dd1693fd
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 09:48:34 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { npm_install } from "../../../love/public/src/npm_install.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
@@ -7,6 +8,7 @@ export async function sandbox() {
   let app_fn = sandbox;
   const key = "test";
   let value = storage_local_get(app_fn, key);
+  await npm_install(package_name);
   assert(b);
   storage_local_set(app_fn, key, 123);
 }
