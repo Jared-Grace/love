58e6be6fd8a5c8ceda0bf4412025b04a30f8fc8b
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 08:45:51 -0400

    function_auto_path ..\love\public\src\app_message_provide_housing.mjs

---
+++ web/public/src/app_message_provide_housing.mjs
@@ -0,0 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { app_message_provide_generic } from "../../../love/public/src/app_message_provide_generic.mjs";
+export function app_message_provide_housing(context) {
+  marker("1");
+  let emoji = "🌽";
+  let category = "food";
+  let verse =
+    "...whoever sows generously will also reap generously (Corinthians 9:6)";
+  app_message_provide_generic(category, emoji, context, verse);
+}
