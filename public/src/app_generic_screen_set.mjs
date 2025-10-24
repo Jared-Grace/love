80657954a3d3c4dec98a11dca10480a0e894d025
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 09:31:06 -0400

    function_auto_path ..\love\public\src\app_generic_screen_set.mjs

--- public/src/app_generic_screen_set.mjs
+++ public/src/app_generic_screen_set.mjs
@@ -9,6 +9,8 @@ export function app_generic_screen_set(context, fn) {
   let app_fn = object_property_get(context, "app_fn");
   let without = function_name_prefix_without(fn, app_fn);
   storage_local_set_context(context, "screen", without);
-  log(message);
+  log({
+    without,
+  });
   app_generic_refresh(context);
 }
