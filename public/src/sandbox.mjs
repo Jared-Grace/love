38ac1fd9d4b186985684719d401291c363195f93
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 12:59:12 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { storage_local_keys_global_app } from "../../../karate_code/public/src/storage_local_keys_global_app.mjs";
 import { storage_local_disable } from "../../../love/public/src/storage_local_disable.mjs";
 import { true_is_assert } from "../../../love/public/src/true_is_assert.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
@@ -42,5 +43,6 @@ export async function sandbox() {
   equal_assert(v, value2);
   let enabled = storage_local_enabled();
   true_is_assert(enabled);
+  let keys = storage_local_keys_global_app(context);
   storage_local_disable();
 }
