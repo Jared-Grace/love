4db4a9fc1f43cd384a0b94a1bf5f215c253169fa
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 08:47:10 -0400

    function_auto_path ..\love\public\src\app_message_flow_travel.mjs

--- web/public/src/app_message_flow_travel.mjs
+++ web/public/src/app_message_flow_travel.mjs
@@ -1,3 +1,4 @@
+import { app_message_provide_housing } from "../../../love/public/src/app_message_provide_housing.mjs";
 import { app_message_provide_food } from "../../../love/public/src/app_message_provide_food.mjs";
 import { app_message_provide_travel } from "../../../love/public/src/app_message_provide_travel.mjs";
 export function app_message_flow_travel() {
@@ -7,6 +8,9 @@ export function app_message_flow_travel() {
     },
     {
       fn: app_message_provide_food,
+    },
+    {
+      fn: app_message_provide_housing,
     },
   ];
   return v3;
