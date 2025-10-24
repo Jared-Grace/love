2b28006714f1b0286269306b631d92ac3639ab1e
Grace-Jared <christrosetolife@gmail.com>
2025-10-24 09:47:18 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
+  storage_local_set(app_fn, key, value);
 }
