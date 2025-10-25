883f72823e91882846d7d4461c3853cd7b500e56
Grace-Jared <christrosetolife@gmail.com>
2025-10-25 16:36:21 -0400

    function_auto_path ..\love\public\src\granddad.mjs

--- public/src/granddad.mjs
+++ public/src/granddad.mjs
@@ -1,7 +1,9 @@
+import { range } from "../../../love/public/src/range.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export function granddad() {
   log("hi");
+  let r = range(count);
   function lambda(item) {}
   each(list, lambda);
 }
