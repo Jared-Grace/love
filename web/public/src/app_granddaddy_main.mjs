5eca7786fb37a13829df37cf1c9422fde7bc49a3
Grace-Jared <christrosetolife@gmail.com>
2025-10-25 16:43:02 -0400

    function_auto_path ..\love\public\src\app_granddaddy_main.mjs

--- web/public/src/app_granddaddy_main.mjs
+++ web/public/src/app_granddaddy_main.mjs
@@ -1,5 +1,7 @@
+import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_granddaddy_main() {
+  let body = html_document_body();
   function lambda2() {}
   let component = html_button(parent, text, lambda2);
 }
