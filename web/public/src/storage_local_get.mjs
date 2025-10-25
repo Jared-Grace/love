054c66c261007f526e62f9f76478024d4453359b
Grace-Jared <christrosetolife@gmail.com>
2025-10-25 17:52:12 -0400

    function_auto_path ..\love\public\src\storage_local_get.mjs

--- web/public/src/storage_local_get.mjs
+++ web/public/src/storage_local_get.mjs
@@ -1,4 +1,3 @@
-import { not } from "../../../love/public/src/not.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
@@ -17,10 +16,9 @@ export function storage_local_get(app_fn, key) {
     storage_local_set,
     storage_local_key,
   );
+  ("localStorage has this behavior, so the in-memory version also mirrors api");
   let value = null;
-  if (not(exists)) {
-    ("localStorage has this behavior, so the in-memory version also mirrors api");
-  } else {
+  if (exists) {
     value = global_function_property_get(storage_local_set, storage_local_key);
   }
   return value;
