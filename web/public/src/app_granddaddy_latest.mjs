71c6e509f560069a2ba70968dd2bc5912b7aca65
Grace-Jared <christrosetolife@gmail.com>
2025-10-25 16:41:42 -0400

    function_auto_path ..\love\public\src\app_granddaddy_latest.mjs

--- web/public/src/app_granddaddy_latest.mjs
+++ web/public/src/app_granddaddy_latest.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
 export async function app_granddaddy_latest() {
   await app_main_latest("app_granddaddy_main", "jared-grace");
 }
