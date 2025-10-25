9c411f5c837c53f13b11704249dd41cd4cdf1511
Grace-Jared <christrosetolife@gmail.com>
2025-10-25 16:36:12 -0400

    function_auto_path ..\love\public\src\granddad.mjs

--- public/src/granddad.mjs
+++ public/src/granddad.mjs
@@ -1,7 +1,6 @@
-import { log_keep } from "../../../love/public/src/log_keep.mjs";
+import { each } from "../../../love/public/src/each.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export function granddad() {
   log("hi");
-  log_keep(message);
-  let a = 1;
+  each(list, function lambda(item) {});
 }
