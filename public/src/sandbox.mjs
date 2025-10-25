0e5c34cdee517a00773771dc87467e4063d6fc08
Grace-Jared <christrosetolife@gmail.com>
2025-10-25 17:50:30 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -56,11 +56,11 @@ export async function sandbox() {
   true_is_assert(enabled);
   let dictionary1 = storage_local_keys_values(context, keys);
   storage_local_disable();
-  let dictionary = storage_local_keys_values(context2, keys2);
+  let dictionary = storage_local_keys_values(context, keys);
   function lambda4(object, property) {
     storage_local_set(app_fn, property, object);
   }
   each_object(dictionary1, lambda4);
-  let dictionary2 = storage_local_keys_values(context3, keys3);
+  let dictionary2 = storage_local_keys_values(context, keys);
   return dictionary1;
 }
