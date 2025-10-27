5a135bcdc52492a8d5d0db0707ce01fed9b8c548
Grace-Jared <christrosetolife@gmail.com>
2025-10-26 20:24:12 -0400

    function_auto_path ..\love\public\src\storage_local_remove.mjs

--- public/src/storage_local_remove.mjs
+++ public/src/storage_local_remove.mjs
@@ -1,3 +1,14 @@
+import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
+import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
+import { storage_local_specify_set } from "../../../love/public/src/storage_local_specify_set.mjs";
+import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
+import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
 export function storage_local_remove(app_fn, key) {
+  let storage_local_key = storage_local_key_get(app_fn, key);
+  if (storage_local_enabled()) {
+    storage_local_specify_set(value, storage_local_key);
+    return;
+  }
+  global_function_property_set(storage_local_set, storage_local_key, value);
   localStorage.removeItem(key);
 }
