cb8eb8ebfc328a755bef7f6fd581b55d70702c5f
Grace-Jared <christrosetolife@gmail.com>
2025-10-26 20:34:06 -0400

    function_auto_path ..\love\public\src\object_property_exists_assert.mjs

--- public/src/object_property_exists_assert.mjs
+++ public/src/object_property_exists_assert.mjs
@@ -2,7 +2,6 @@ import { object_property_exists } from "../../../love/public/src/object_property
 import { assert_json_get } from "./assert_json_get.mjs";
 export function object_property_exists_assert(object, property_name) {
   let result = object_property_exists(object, property_name);
-  return;
   function lambda() {
     let v = {
       object,
