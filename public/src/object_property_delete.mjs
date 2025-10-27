49755ae69ae925a080c011e8a66b361acd155f8b
Grace-Jared <christrosetolife@gmail.com>
2025-10-26 20:26:40 -0400

    function_auto_path ..\love\public\src\object_property_delete.mjs

--- public/src/object_property_delete.mjs
+++ public/src/object_property_delete.mjs
@@ -1,6 +1,5 @@
 import { object_property_exists_assert } from "../../../love/public/src/object_property_exists_assert.mjs";
 export function object_property_delete(object, property_name) {
+  object_property_exists_assert(object, property_name);
   delete object[property_name];
-  return;
-  object_property_exists_assert(object2, property_name2);
 }
