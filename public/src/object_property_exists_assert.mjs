c499c93f4532d9547f7d04b35631475d047a002a
Grace-Jared <christrosetolife@gmail.com>
2025-10-26 20:32:31 -0400

    data_file_update ..\love\public\src\object_property_exists_assert.mjs

--- public/src/object_property_exists_assert.mjs
+++ public/src/object_property_exists_assert.mjs
@@ -1,6 +1,7 @@
 import { assert } from "../../../love/public/src/assert.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+import { assert_json_get } from "./assert_json_get.mjs";
 export function object_property_exists_assert(object, property_name) {
   let result = object_property_exists(object, property_name);
-  assert(result);
+  assert_json_get(result,$f);
 }
