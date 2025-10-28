9008a4ccdf7c172a9a3a3ff9bdfc233fb87174d7
Grace-Jared <christrosetolife@gmail.com>
2025-10-28 17:57:51 -0400

    function_auto_path ..\love\public\src\false_is_assert.mjs

--- web/public/src/false_is_assert.mjs
+++ web/public/src/false_is_assert.mjs
@@ -1,8 +1,8 @@
+import { false_is } from "../../../love/public/src/false_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
-import { true_is } from "../../../love/public/src/true_is.mjs";
 export function false_is_assert(enabled) {
   marker("1");
-  let ti2 = true_is(enabled);
+  let ti2 = false_is(enabled);
   assert(ti2);
 }
