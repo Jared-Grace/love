5a570022620d9b5e52743cd2895988e705e49b6b
Grace-Jared <christrosetolife@gmail.com>
2025-10-28 17:57:21 -0400

    data_file_update ..\love\public\src\false_is.mjs

--- public/src/false_is.mjs
+++ public/src/false_is.mjs
@@ -2,6 +2,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 export function false_is(value) {
   marker("1");
-  let ti = equal(value, true);
+  let ti = equal(value, false);
   return ti;
 }
