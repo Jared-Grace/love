a64c5cbb7fdb809ee99670d76fc946a0cb8fe766
Grace-Jared <christrosetolife@gmail.com>
2025-10-28 16:12:01 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,5 @@
+import { storage_local_keys_context } from "../../../love/public/src/storage_local_keys_context.mjs";
 import { storage_local_keys_browser_context } from "../../../love/public/src/storage_local_keys_browser_context.mjs";
-import { storage_local_keys } from "../../../love/public/src/storage_local_keys.mjs";
-import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
-import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
-import { storage_local_key_prefix } from "../../../karate_code/public/src/storage_local_key_prefix.mjs";
 import { storage_local_remove } from "../../../love/public/src/storage_local_remove.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { json_equal_assert } from "../../../love/public/src/json_equal_assert.mjs";
@@ -68,11 +65,7 @@ export async function sandbox() {
   let enabled = storage_local_enabled();
   true_is_assert(enabled);
   ("migrating from local storage to global");
-  let prefix = storage_local_key_prefix(context);
-  marker("1");
-  let keys3 = storage_local_keys();
-  let keys2 = list_filter_starts_with(keys3, prefix);
-  let keys = list_map_prefix_without(keys2, prefix);
+  let keys = storage_local_keys_context(context);
   ("confirm local storage values");
   let local_enabled = storage_local_keys_values(context, keys);
   function lambda3(key) {
