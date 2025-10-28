b757a2d67afbee97212ac2afcbaa8050987582fe
Grace-Jared <christrosetolife@gmail.com>
2025-10-28 18:43:36 -0400

    function_auto_path ..\love\public\src\storage_local_keys_context_empty_is_assert.mjs

--- web/public/src/storage_local_keys_context_empty_is_assert.mjs
+++ web/public/src/storage_local_keys_context_empty_is_assert.mjs
@@ -1,6 +1,6 @@
 import { list_empty_is_assert } from "../../../love/public/src/list_empty_is_assert.mjs";
 import { storage_local_keys_context } from "../../../love/public/src/storage_local_keys_context.mjs";
 export function storage_local_keys_context_empty_is_assert(context) {
-  let keys_disabled = storage_local_keys_context(context);
-  list_empty_is_assert(keys_disabled);
+  let keys = storage_local_keys_context(context);
+  list_empty_is_assert(keys);
 }
