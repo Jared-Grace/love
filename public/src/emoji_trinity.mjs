9d17716f652fc7fdad7265c73583f167ea689024
Grace-Jared <christrosetolife@gmail.com>
2025-11-01 12:41:29 -0400

    function_auto_path ..\love\public\src\emoji_trinity.mjs

--- public/src/emoji_trinity.mjs
+++ public/src/emoji_trinity.mjs
@@ -1,7 +1,6 @@
 import { emoji_dove } from "../../../love/public/src/emoji_dove.mjs";
 import { emoji_cross } from "../../../love/public/src/emoji_cross.mjs";
-import { emoji_heart_fire } from "../../../love/public/src/emoji_heart_fire.mjs";
 export function emoji_trinity() {
-  let v = emoji_heart_fire() + emoji_cross() + emoji_dove();
+  let v = "❤️" + emoji_cross() + emoji_dove();
   return v;
 }
