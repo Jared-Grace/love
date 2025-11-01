b7943d05c6ad3fa91d037b9cd113a6c2c7bceb8e
Grace-Jared <christrosetolife@gmail.com>
2025-11-01 11:02:42 -0400

    function_auto_path ..\love\public\src\app_reply_choices.mjs

--- public/src/app_reply_choices.mjs
+++ public/src/app_reply_choices.mjs
@@ -284,7 +284,7 @@ export function app_reply_choices() {
     },
     {
       text: emoji_voice() + " Voice",
-      response: emoji_phone() + " WhatsApp: +1-904-314-4052",
+      response: emoji_voice() + " WhatsApp: +1-904-314-4052",
     },
     {
       text: emoji_question() + " What",
