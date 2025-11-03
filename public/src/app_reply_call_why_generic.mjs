3633b9c2f8f0449859448158e2a13270b8d37448
Grace-Jared <christrosetolife@gmail.com>
2025-11-02 19:33:43 -0500

    function_auto_path ..\love\public\src\app_reply_choices.mjs

---
+++ public/src/app_reply_call_why_generic.mjs
@@ -0,0 +1,11 @@
+import { emoji_phone } from "../../../love/public/src/emoji_phone.mjs";
+export function app_reply_call_why_generic(a, b) {
+  let v2 =
+    emoji_phone() +
+    " Why " +
+    a +
+    " you " +
+    b +
+    " call me? What did you want to talk about?";
+  return v2;
+}
