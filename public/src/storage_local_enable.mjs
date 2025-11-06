926c0b59291e73a4eeef3e65046f27bba84aaf15
Grace-Jared <christrosetolife@gmail.com>
2025-11-06 14:02:49 -0500

    pnd context

--- public/src/storage_local_enable.mjs
+++ public/src/storage_local_enable.mjs
@@ -1,6 +1,6 @@
 import { storage_local_enable_set } from "../../../love/public/src/storage_local_enable_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_enable() {
+export function storage_local_enable(context) {
   marker("1");
   const v = true;
   let dictionary = storage_local_enable_set(v, context);
