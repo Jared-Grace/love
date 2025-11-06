5b6348989ace42c381543d9d32726d021901e146
Grace-Jared <christrosetolife@gmail.com>
2025-11-06 14:10:13 -0500

    function_auto_path ..\love\public\src\global_function_property_exists.mjs

--- web/public/src/global_function_property_exists.mjs
+++ web/public/src/global_function_property_exists.mjs
@@ -1,9 +1,9 @@
-import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
+import { global_function_initialize_object } from "../../../love/public/src/global_function_initialize_object.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 export function global_function_property_exists(fn, property_name) {
   marker("1");
-  let fn_object = global_function_initialize(fn, {});
+  let fn_object = global_function_initialize_object(fn);
   let exists = object_property_exists(fn_object, property_name);
   return exists;
 }
