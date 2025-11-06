7d67e8d30f71504b6924dd4c5cae4f2ffd41fd4f
Grace-Jared <christrosetolife@gmail.com>
2025-11-06 14:19:15 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { storage_local_mock_enable } from "../../../love/public/src/storage_local_mock_enable.mjs";
 import { global_function_initialize_object } from "../../../love/public/src/global_function_initialize_object.mjs";
 import { storage_local_enable } from "../../../love/public/src/storage_local_enable.mjs";
 import { false_is_assert } from "../../../love/public/src/false_is_assert.mjs";
@@ -5,47 +6,18 @@ import { json_equal_assert } from "../../../love/public/src/json_equal_assert.mj
 import { storage_local_get_global } from "../../../love/public/src/storage_local_get_global.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
 import { storage_local_keys_values } from "../../../karate_code/public/src/storage_local_keys_values.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
-import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
-import { object_properties_size } from "../../../love/public/src/object_properties_size.mjs";
 import { storage_local_disable } from "../../../love/public/src/storage_local_disable.mjs";
 import { true_is_assert } from "../../../love/public/src/true_is_assert.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
-import { string_to } from "../../../love/public/src/string_to.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { null_is_assert } from "../../../love/public/src/null_is_assert.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 export async function sandbox() {
   marker("1");
-  let s = {};
-  let localStorage = {
-    getItem: function getItem(storage_local_key) {
-      let value3 = object_property_get_or(null, s, storage_local_key);
-      return value3;
-    },
-    setItem: function setItem(storage_local_key, v) {
-      v = string_to(v);
-      object_property_set(s, storage_local_key, v);
-    },
-    get length() {
-      let size = object_properties_size(s);
-      return size;
-    },
-    key: function lambda2(index) {
-      let properties = object_properties(s);
-      let item = list_get(properties, index);
-      return item;
-    },
-    removeItem: function lambda5(storage_local_key) {
-      object_property_delete(s, storage_local_key);
-    },
-  };
-  global.localStorage = localStorage;
+  storage_local_mock_enable();
   let app_fn = sandbox;
   let context = {
     app_fn,
