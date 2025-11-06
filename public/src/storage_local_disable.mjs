651ba274f57fdd0c08e1c97fbbfed5942c2931d3
Grace-Jared <christrosetolife@gmail.com>
2025-11-06 14:01:18 -0500

    pnd context

--- public/src/storage_local_disable.mjs
+++ public/src/storage_local_disable.mjs
@@ -1,6 +1,6 @@
 import { storage_local_enable_set } from "../../../love/public/src/storage_local_enable_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_disable() {
+export function storage_local_disable(context) {
   marker("1");
   const v = false;
   storage_local_enable_set(v);
