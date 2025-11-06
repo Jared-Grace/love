6465f887c7ec1b9b663e6925e24206433adf9e07
Grace-Jared <christrosetolife@gmail.com>
2025-11-06 14:03:33 -0500

    function_auto_path ..\love\public\src\storage_local_enable_set.mjs

--- public/src/storage_local_enable_set.mjs
+++ public/src/storage_local_enable_set.mjs
@@ -5,7 +5,7 @@ import { storage_local_enabled } from "../../../love/public/src/storage_local_en
 import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
 export function storage_local_enable_set(value_new, context) {
   marker("1");
-  let dictionary = storage_local_remove_app();
+  let dictionary = storage_local_remove_app(context);
   global_function_set(storage_local_enabled, value_new);
   storage_local_set_dictionary(context, dictionary);
   return dictionary;
