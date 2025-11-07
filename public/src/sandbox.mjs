f106f162bf28889a93d86d42a4325d5f0069a812
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:21:31 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,17 +1,4 @@
-import { bible_interlinear_json_path } from "../../../love/public/src/bible_interlinear_json_path.mjs";
-import { bible_interlinear_excel_path } from "../../../love/public/src/bible_interlinear_excel_path.mjs";
-import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
-import { json_format_to } from "./json_format_to.mjs";
+import { bible_interlinear_json_generate } from "../../../love/public/src/bible_interlinear_json_generate.mjs";
 export async function sandbox() {
-  const { default: XLSX } = await import("xlsx");
-  ("todo: download from");
-  ("https://bereanbible.com/bsb_tables.xlsx");
-  ("that way more is automated for other users of this script");
-  let path_input = bible_interlinear_excel_path();
-  const wb = XLSX.readFile(path_input);
-  const ws = wb.Sheets["biblosinterlinear96"];
-  const json = XLSX.utils.sheet_to_json(ws);
-  let contents = json_format_to(json);
-  let path_output = bible_interlinear_json_path();
-  let result = await file_overwrite(path_output, contents);
+  await bible_interlinear_json_generate();
 }
