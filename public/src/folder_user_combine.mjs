9dee4201860e70a1fe8c5b3e39199cfd9e4d3aa6
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:06:07 -0500

    function_auto_path ..\love\public\src\folder_user_combine.mjs

--- public/src/folder_user_combine.mjs
+++ public/src/folder_user_combine.mjs
@@ -1,7 +1,7 @@
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_user } from "../../../love/public/src/folder_user.mjs";
-export function folder_user_combine(folder, destination) {
+export function folder_user_combine(folder, file_path) {
   let result = folder_user(folder);
-  let joined = path_join([result, destination]);
+  let joined = path_join([result, file_path]);
   return joined;
 }
