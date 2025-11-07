8fb73375f40b377d1fe99277449eb2321f832835
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:25:03 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,9 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { bible_interlinear_json_path } from "../../../love/public/src/bible_interlinear_json_path.mjs";
 export async function sandbox() {
   let path_output = bible_interlinear_json_path();
   let data = await file_read_json(path_output);
+  each(list, function lambda(item) {});
   return data;
 }
