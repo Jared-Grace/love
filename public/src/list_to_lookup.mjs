c7c81e776c30c0f5647aed792f0ad54438862867
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:33:27 -0500

    function_auto_path ..\love\public\src\list_to_lookup.mjs

--- public/src/list_to_lookup.mjs
+++ public/src/list_to_lookup.mjs
@@ -3,17 +3,17 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { object_property_initialize_list } from "../../../love/public/src/object_property_initialize_list.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
-export function list_to_lookup(vid_property, words) {
+export function list_to_lookup(property_key, list) {
   let verses = {};
   function lambda(word) {
-    let n = object_property_exists_not(word, vid_property);
+    let n = object_property_exists_not(word, property_key);
     if (n) {
       return;
     }
-    let vid = object_property_get(word, vid_property);
+    let vid = object_property_get(word, property_key);
     let verse_words = object_property_initialize_list(verses, vid);
     list_add(verse_words, word);
   }
-  each(words, lambda);
+  each(list, lambda);
   return verses;
 }
