f2c25e31f19062ca20f64ec1765c71a4cc2cb0c4
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:04:26 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,1 @@
-import { storage_local_enabling_test } from "../../../love/public/src/storage_local_enabling_test.mjs";
-export async function sandbox() {
-  storage_local_enabling_test();
-}
+export async function sandbox() {}
