7326a213281f02c1168dc39722a6ffc117e67ce4
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:04:49 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,7 @@
-export async function sandbox() {}
+export async function sandbox() {
+  const wb = XLSX.readFile("input.xlsx");
+  const ws = wb.Sheets[wb.SheetNames[0]];
+  const json = XLSX.utils.sheet_to_json(ws);
+  let v = JSON.stringify(json, null, 2);
+  writeFileSync("output.json", v);
+}
