ebcf4cf82d3727fa645ee98785ecfc3231c0885a
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:26:21 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ web/public/src/object_property_initialize_list.mjs
@@ -0,0 +1,5 @@
+import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
+export function object_property_initialize_list(verses, vid) {
+  let value2 = object_property_initialize(verses, vid, []);
+  return value2;
+}
