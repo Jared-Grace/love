027130efa4663600392654943b192f8ee4af42ac
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:05:52 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { folder_user_combine } from "../../../love/public/src/folder_user_combine.mjs";
 export async function sandbox() {
+  let joined = folder_user_combine(folder, destination);
   const XLSX = await import("xlsx");
   const { writeFileSync } = await import("fs");
   const wb = XLSX.readFile("input.xlsx");
