34a572881db778a417581ff5eb1a74d29938ec0e
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:07:59 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { folder_user_combine } from "../../../love/public/src/folder_user_combine.mjs";
 export async function sandbox() {
   const XLSX = await import("xlsx");
@@ -9,5 +10,6 @@ export async function sandbox() {
   const ws = wb.Sheets[wb.SheetNames[0]];
   const json = XLSX.utils.sheet_to_json(ws);
   let v = JSON.stringify(json, null, 2);
-  writeFileSync(joined + ".json", v);
+  let file_name = file_name_json(joined);
+  writeFileSync(file_name, v);
 }
