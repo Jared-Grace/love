0b8327610debe865196dd32715e74153f17ec983
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 17:10:03 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -4,7 +4,6 @@ import { folder_user_combine } from "../../../love/public/src/folder_user_combin
 import { json_format_to } from "./json_format_to.mjs";
 export async function sandbox() {
   const XLSX = await import("xlsx");
-  const { writeFileSync } = await import("fs");
   ("todo: download from");
   ("https://bereanbible.com/bsb_tables.xlsx");
   let path_input = folder_user_combine("downloads", "bsb_tables.xlsx");
