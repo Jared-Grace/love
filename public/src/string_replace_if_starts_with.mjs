7aa2b5020742cbd30ef6073c68ddbb9f8e9604f3
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 22:02:55 -0500

    function_auto_path ..\love\public\src\ebible_references_names.mjs

---
+++ public/src/string_replace_if_starts_with.mjs
@@ -0,0 +1,9 @@
+import { string_replace } from "../../../love/public/src/string_replace.mjs";
+import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
+export function string_replace_if_starts_with(prefix, item, replacement) {
+  let sw = string_starts_with(s, prefix);
+  if (sw) {
+    item = string_replace(item, prefix, replacement);
+  }
+  return item;
+}
