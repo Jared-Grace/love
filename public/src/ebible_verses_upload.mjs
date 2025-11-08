ed9b50437536162a1ac3f6d269c8a29fd6e577db
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 22:13:51 -0500

    function_auto_path ..\love\public\src\ebible_verses_upload.mjs

--- public/src/ebible_verses_upload.mjs
+++ public/src/ebible_verses_upload.mjs
@@ -1,12 +1,9 @@
+import { ebible_firebase_upload_verse } from "../../../love/public/src/ebible_firebase_upload_verse.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
-import { ebible_verses_upload_name } from "../../../love/public/src/ebible_verses_upload_name.mjs";
-import { ebible_firebase_upload } from "../../../love/public/src/ebible_firebase_upload.mjs";
 import { list_wait } from "../../../love/public/src/list_wait.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function ebible_verses_upload(bible_folder) {
   marker("1");
   await ebible_version_download(bible_folder);
@@ -14,16 +11,7 @@ export async function ebible_verses_upload(bible_folder) {
   await ebible_chapters_each_verses_check_with(bible_folder, each_chapter);
   async function each_chapter(chapter_code, verses) {
     async function lambda(v) {
-      let verse_number = object_property_get(v, "verse_number");
-      let joined2 = ebible_verses_upload_name(chapter_code, verse_number);
-      let verse = object_merge(
-        {
-          bible_folder,
-          chapter_code,
-        },
-        v,
-      );
-      await ebible_firebase_upload(bible_folder, joined2, verse);
+      await ebible_firebase_upload_verse(v, chapter_code, bible_folder);
     }
     let mapped = list_map(verses, lambda);
     await list_wait(mapped);
