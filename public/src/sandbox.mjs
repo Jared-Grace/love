5c549b309fbd14c0cf7346b4227bf8b136c09e89
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 20:40:12 -0500

    data_file_update ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -37,7 +37,7 @@ export async function sandbox() {
     let filtered = list_filter(verse_words, lambda4);
     let mapped = list_map_property(filtered, original_property);
     object_property_set(verses2, vid, mapped);
-    let p = await ebible_references_parse(vid);
+    let p = await ebible_references_parse(vid);l
   }
   each_object(verses, lambda);
 }
