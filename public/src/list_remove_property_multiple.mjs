69c0a989a828eb7ac2019f0610e45529a308ef14
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 23:07:14 -0500

    function_auto_path ..\love\public\src\list_remove_property_multiple.mjs

--- public/src/list_remove_property_multiple.mjs
+++ public/src/list_remove_property_multiple.mjs
@@ -1,12 +1,8 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { list_remove_property } from "../../../love/public/src/list_remove_property.mjs";
-export function list_remove_property_multiple(
-  languages,
-  property_name,
-  removals,
-) {
+export function list_remove_property_multiple(list, property_name, removals) {
   function lambda10(r) {
-    list_remove_property(languages, property_name, r);
+    list_remove_property(list, property_name, r);
   }
   each(removals, lambda10);
 }
