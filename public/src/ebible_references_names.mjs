72e1a79a3a229d79189405fd070e289dd75fef40
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 22:00:37 -0500

    function_auto_path ..\love\public\src\ebible_references_names.mjs

--- public/src/ebible_references_names.mjs
+++ public/src/ebible_references_names.mjs
@@ -7,7 +7,8 @@ import { list_map_prefix_any } from "../../../love/public/src/list_map_prefix_an
 import { list_filter_starts_with_any } from "../../../love/public/src/list_filter_starts_with_any.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export function ebible_references_names(books, lines) {
-  let mapped = list_map(list, function lambda(item) {});
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
   let books_names = list_map_property(books, "text");
   let verse_references = list_filter_starts_with_any(books_names, lines);
   let book_names = list_map_prefix_any(verse_references, books_names);
