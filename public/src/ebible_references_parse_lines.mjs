2f1c6795b97dfac518bc15775eeb3e54db3155d0
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 20:43:51 -0500

    function_auto_path ..\love\public\src\ebible_references_parse_lines.mjs

--- public/src/ebible_references_parse_lines.mjs
+++ public/src/ebible_references_parse_lines.mjs
@@ -1,20 +1,11 @@
+import { ebible_reference_parts } from "../../../love/public/src/ebible_reference_parts.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { each_pair_async } from "../../../love/public/src/each_pair_async.mjs";
 import { each_range_from_async } from "../../../love/public/src/each_range_from_async.mjs";
 import { string_to } from "../../../love/public/src/string_to.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
-import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
-import { list_second } from "../../../love/public/src/list_second.mjs";
-import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
-import { string_split_dash } from "../../../love/public/src/string_split_dash.mjs";
-import { number_pad } from "../../../love/public/src/number_pad.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
-import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
-import { string_split_colon } from "../../../love/public/src/string_split_colon.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { list_map_first } from "../../../love/public/src/list_map_first.mjs";
 import { list_map_filter_string_empty_not_is } from "../../../love/public/src/list_map_filter_string_empty_not_is.mjs";
 import { list_map_split_space } from "../../../love/public/src/list_map_split_space.mjs";
@@ -41,26 +32,8 @@ export async function ebible_references_parse_lines(bible_folders, lines) {
   let chapter_verses_list = list_map_first(mapped4);
   async function lambda2(la) {
     async function lambda(book_name, chapter_verses) {
-      let book = list_find_property(books, "text", book_name);
-      let book_code = object_property_get(book, "book_code");
-      let split2 = string_split_colon(chapter_verses);
-      let { first: chapter_name, second } = list_first_second(split2);
-      let pad_count = 2;
-      if (equal(book_code, "PSA")) {
-        pad_count = 3;
-      }
-      let chapter_padded = number_pad(chapter_name, pad_count);
-      let chapter_code = book_code + chapter_padded;
-      let verse_range = string_split_dash(second);
-      let verse_start = list_first(verse_range);
-      let verse_end = null;
-      let m = list_multiple_is(verse_range);
-      if (m) {
-        verse_end = list_second(verse_range);
-      } else {
-        verse_end = verse_start;
-      }
-      let index = list_index_of(books, book);
+      let { index, chapter_code, verse_start, verse_end } =
+        ebible_reference_parts(books, book_name, chapter_verses);
       async function lambda5(bible_folder, books) {
         let book2 = list_get(books, index);
         let book_name = object_property_get(book2, "text");
