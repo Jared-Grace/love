c218785f4f3c4ceb3125771b01db9ff1f41fedce
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 21:52:51 -0500

    function_auto_path ..\love\public\src\ebible_references_names.mjs

--- web/public/src/ebible_references_names.mjs
+++ web/public/src/ebible_references_names.mjs
@@ -1,7 +1,7 @@
 import { list_map_prefix_any } from "../../../love/public/src/list_map_prefix_any.mjs";
 import { list_filter_starts_with_any } from "../../../love/public/src/list_filter_starts_with_any.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-export async function ebible_references_names(books, lines) {
+export function ebible_references_names(books, lines) {
   let books_names = list_map_property(books, "text");
   let verse_references = list_filter_starts_with_any(books_names, lines);
   let book_names = list_map_prefix_any(verse_references, books_names);
