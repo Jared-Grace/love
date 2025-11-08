59098c3c1f1cb58471807af5e3a65cc4e2ae985c
Grace-Jared <christrosetolife@gmail.com>
2025-11-07 22:03:35 -0500

    function_auto_path ..\love\public\src\string_replace_if_starts_with.mjs

--- web/public/src/string_replace_if_starts_with.mjs
+++ web/public/src/string_replace_if_starts_with.mjs
@@ -1,7 +1,7 @@
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 export function string_replace_if_starts_with(prefix, item, replacement) {
-  let sw = string_starts_with(s, prefix);
+  let sw = string_starts_with(item, prefix);
   if (sw) {
     item = string_replace(item, prefix, replacement);
   }
