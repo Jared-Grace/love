52b96ed0bcef1bc21b11ad4d4e2260bf11d987f8
Grace-Jared <christrosetolife@gmail.com>
2025-11-10 14:43:45 -0500

    function_auto_path ..\love\public\src\html_span_text.mjs

---
+++ public/src/html_span.mjs
@@ -0,0 +1,5 @@
+import { html_element } from "../../../love/public/src/html_element.mjs";
+export function html_span(container) {
+  let component = html_element(container, "span");
+  return component;
+}
--- public/src/html_span_text.mjs
+++ public/src/html_span_text.mjs
@@ -1,7 +1,7 @@
+import { html_span } from "../../../love/public/src/html_span.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
-import { html_element } from "../../../love/public/src/html_element.mjs";
 export function html_span_text(container, text) {
-  let span = html_element(container, "span");
+  let span = html_span(container);
   html_text_set(span, text);
   return span;
 }
