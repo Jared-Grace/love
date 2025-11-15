cd159f5b5ca427300e7e6219804007cf87217f33
Grace-Jared <christrosetolife@gmail.com>
2025-11-15 11:57:52 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -101,7 +101,8 @@ export async function app_reply_main() {
     languages_reset();
   }
   let component3 = html_button(root, "Reset", lambda4);
-  let component4 = html_button(parent, text3, function lambda11() {});
+  function lambda11() {}
+  let component4 = html_button(parent, text3, lambda11);
   let component2 = html_button(root, "Copy", preview_refresh);
   let component_languages = html_span(root);
   languages_reset();
