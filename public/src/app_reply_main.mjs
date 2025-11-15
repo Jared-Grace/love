2a7f7f163184751ff241cbdf6b2f63b253480416
Grace-Jared <christrosetolife@gmail.com>
2025-11-15 12:14:49 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -126,7 +126,9 @@ export async function app_reply_main() {
     let list2 = await list_adder_multiple_async(lambda10);
     await list_join_newline_2_copy(list2);
   }
-  function lambda12() {}
+  async function lambda12() {
+    await reset_language_keep();
+  }
   let component5 = html_button(parent, "Extra", lambda12);
   let component2 = html_button(root, "Copy", preview_refresh);
   let component_languages = html_span(root);
