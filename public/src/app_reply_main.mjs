0eadb9e1c2512206f792fc1896e5734a902a9fbd
Grace-Jared <christrosetolife@gmail.com>
2025-11-15 15:47:08 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -108,6 +108,7 @@ export async function app_reply_main() {
     buttons_refresh();
     preview_refresh();
     languages_reset();
+    preview_refresh();
   }
   let component4 = html_button(root, "❤️", love);
   html_button(root, "Reset 1", verse_random_reset_1);
