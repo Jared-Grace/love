af1fb376705978575af1f6c393bcf9f990a08b41
Grace-Jared <christrosetolife@gmail.com>
2025-11-15 17:01:17 -0500

    function_auto_path ..\love\public\src\app_reply_response_how_r_u_skip.mjs

--- public/src/app_reply_response_how_r_u_skip.mjs
+++ public/src/app_reply_response_how_r_u_skip.mjs
@@ -1,13 +1,10 @@
+import { app_reply_response_how_r_u_skip_before } from "../../../love/public/src/app_reply_response_how_r_u_skip_before.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
-import { emoji_100 } from "../../../love/public/src/emoji_100.mjs";
-import { string_the_servant_of_god_is } from "../../../love/public/src/string_the_servant_of_god_is.mjs";
 export function app_reply_response_how_r_u_skip() {
+  const before = app_reply_response_how_r_u_skip_before();
   let v =
-    string_the_servant_of_god_is() +
-    " doing good " +
-    emoji_100() +
-    " through the grace of God, because " +
+    before +
     emoji_book_open() +
     " all things work together for good to them that love God and are called according to the purpose of God! (Romans 8:28) However, please pray that the servant of God will be perfectly loving and healed" +
     emoji_pray();
