c71610fc5998e93066073de9384d5049b565b25e
Grace-Jared <christrosetolife@gmail.com>
2025-11-15 14:43:12 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -211,8 +211,7 @@ export async function app_reply_main() {
       let verses3 = object_property_get(v, "verses");
       let verse_texts = list_map_property(verses3, "text");
       let verse_text = list_join_newline_2(verse_texts);
-      const other = [verse_text];
-      list_add(list3, item2);
+      list_add(other, verse_text);
     }
     each(verses_list, lambda11);
     let squashed = list_squash(verses);
