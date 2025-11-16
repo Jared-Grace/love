cad61eb0377830ce58de9d77c232a06ad425e9ff
Grace-Jared <christrosetolife@gmail.com>
2025-11-16 13:12:40 -0500

    function_auto_path ..\love\public\src\list_last_remaining.mjs

--- public/src/list_last_remaining.mjs
+++ public/src/list_last_remaining.mjs
@@ -1,11 +1,11 @@
+import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_one_other } from "../../../love/public/src/list_one_other.mjs";
-import { list_skip_1 } from "../../../love/public/src/list_skip_1.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_last_remaining(list) {
   const property_name = "remaining";
-  let value_get = list_skip_1;
-  let property_name_first = "first";
+  let value_get = list_take_less_1;
+  let property_name_first = "last";
   let one_get = list_last;
   let result = list_one_other(
     list,
