11a88aef181d60407399ef0b2e3b6ee95041f0b5
Grace-Jared <christrosetolife@gmail.com>
2025-11-16 13:10:47 -0500

    fws list_take less_1

--- public/src/list_take_less_1.mjs
+++ public/src/list_take_less_1.mjs
@@ -1,4 +1,6 @@
+import { list_take } from "../../../love/public/src/list_take.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_take_less_1() {
+export function list_take_less_1(list, count) {
   marker("1");
+  return list_take(list, count);
 }
