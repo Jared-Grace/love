2a009982f5645422bfbdaad8fa6b1ac7fc4dcacc
Grace-Jared <christrosetolife@gmail.com>
2025-11-16 13:10:03 -0500

    function_auto_path ..\love\public\src\list_last_remaining.mjs

--- web/public/src/list_last_remaining.mjs
+++ web/public/src/list_last_remaining.mjs
@@ -1,12 +1,12 @@
+import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_one_other } from "../../../love/public/src/list_one_other.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_skip_1 } from "../../../love/public/src/list_skip_1.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_last_remaining(list) {
   const property_name = "remaining";
   let value_get = list_skip_1;
   let property_name_first = "first";
-  let one_get = list_first;
+  let one_get = list_last;
   let result = list_one_other(
     list,
     one_get,
