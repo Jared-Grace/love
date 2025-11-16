bd5fa8453143247f1fca994c0d055e72ccb33713
Grace-Jared <christrosetolife@gmail.com>
2025-11-16 13:08:19 -0500

    function_auto_path ..\love\public\src\list_first_second_generic.mjs

--- public/src/list_first_second_generic.mjs
+++ public/src/list_first_second_generic.mjs
@@ -1,6 +1,5 @@
-import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
+import { list_one_other } from "../../../love/public/src/list_one_other.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
-import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 export function list_first_second_generic(
   list,
   other_get,
@@ -8,17 +7,12 @@ export function list_first_second_generic(
 ) {
   let property_name_first = "first";
   let one_get = list_first;
-  let first = null;
-  let second = null;
-  if (list_empty_not_is(list)) {
-    first = one_get(list);
-    if (list_multiple_is(list)) {
-      second = other_get(list);
-    }
-  }
-  let result = {
-    [property_name_first]: first,
-    [property_name_second]: second,
-  };
+  let result = list_one_other(
+    list,
+    one_get,
+    other_get,
+    property_name_first,
+    property_name_second,
+  );
   return result;
 }
