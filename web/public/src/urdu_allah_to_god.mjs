d4c6c6c5feeba675db5fc83b419ecce057f22e2b
Grace-Jared <christrosetolife@gmail.com>
2025-11-18 16:10:31 -0500

    function_auto_path ..\love\public\src\urdu_allah_to_god.mjs

--- web/public/src/urdu_allah_to_god.mjs
+++ web/public/src/urdu_allah_to_god.mjs
@@ -1,4 +1,4 @@
 export function urdu_allah_to_god(text) {
-  let v = text.replace(/الل[ّٰ]*(ہ|ه)|اللہ|الله/g, "خدا");
+  let v = text.replace(/ا\s*ل\s*ل[\u0651\u0670]*[ہه]|الله/g, "خدا");
   return v;
 }
