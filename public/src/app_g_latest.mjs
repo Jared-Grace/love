a34fe153612fed57ef8dee9b6f4d330630e55fae
Grace-Jared <christrosetolife@gmail.com>
2025-11-17 21:37:50 -0500

    data_file_update ..\love\public\src\app_g_latest.mjs

--- public/src/app_g_latest.mjs
+++ public/src/app_g_latest.mjs
@@ -1,4 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_g_latest() {
-  marker("1");
+export async function app_g_latest() {
+  await app_main_latest("app_g_main", "jared-grace");
 }
