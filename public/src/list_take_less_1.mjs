f0fd57b3852590d5571e3aa07fbb80c5abeff034
Grace-Jared <christrosetolife@gmail.com>
2025-11-18 11:03:23 -0500

    function_auto_path ..\love\public\src\list_take_less_1.mjs

--- public/src/list_take_less_1.mjs
+++ public/src/list_take_less_1.mjs
@@ -1,9 +1,9 @@
 import { list_size_less_1 } from "../../../love/public/src/list_size_less_1.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_take_less_1(list, count) {
+export function list_take_less_1(list) {
   marker("1");
   let sz1 = list_size_less_1(list);
-  let taken = list_take(list, count);
+  let taken = list_take(list, 1);
   return taken;
 }
