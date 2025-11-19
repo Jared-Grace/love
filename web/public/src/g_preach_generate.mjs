3c16aaef19655c8febbdf85d3dbf3b5e691bd68a
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 15:36:51 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -1,4 +1,6 @@
+import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function g_preach_generate() {
+  let p = folder_user_path() + "ChristGPT\\";
   marker("1");
 }
