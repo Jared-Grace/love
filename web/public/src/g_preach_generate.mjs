b20003b68cde9f0b970317f0fdb02bb7ef89fbcc
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 17:26:33 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -1,3 +1,4 @@
+import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { bible_interlinear_chapters } from "../../../love/public/src/bible_interlinear_chapters.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { openai_chat } from "../../../love/public/src/openai_chat.mjs";
@@ -5,6 +6,7 @@ import { file_overwrite_json } from "../../../love/public/src/file_overwrite_jso
 import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function g_preach_generate() {
+  let list = await ebible_verses(bible_folder, chapter_code);
   let chapters = await bible_interlinear_chapters();
   return chapters;
   let verse =
