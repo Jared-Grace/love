8ea38911d77371b51c8f49c5cd40422f1866a088
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 17:00:55 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- public/src/g_preach_generate.mjs
+++ public/src/g_preach_generate.mjs
@@ -1,6 +1,8 @@
+import { command_line } from "../../../love/public/src/command_line.mjs";
 import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function g_preach_generate() {
+export async function g_preach_generate() {
   let p = folder_user_path() + "ChristGPT\\";
+  let stdout = await command_line(command);
   marker("1");
 }
