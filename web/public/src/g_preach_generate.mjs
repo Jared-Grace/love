3fbcc315d6bc2f62c391be10159b2a018069f7e3
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 17:03:25 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -1,3 +1,4 @@
+import { list_map_combine_left } from "../../../love/public/src/list_map_combine_left.mjs";
 import { string_split_space } from "../../../love/public/src/string_split_space.mjs";
 import { py_exe_name } from "../../../love/public/src/py_exe_name.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
@@ -7,7 +8,8 @@ export async function g_preach_generate() {
   let p = folder_user_path() + "ChristGPT\\";
   let v = py_exe_name();
   let file_names = "openai.txt input.txt output.txt";
-  let split = string_split_space(s);
+  let split = string_split_space(file_names);
+  let combineds = list_map_combine_left(left, list);
   let parts = [v];
   let command = py_exe_name();
   let stdout = await command_line(command);
