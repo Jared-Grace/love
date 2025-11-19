edd2fed3be58c615277fc92d041c565598e454be
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 17:03:08 -0500

    cs list_map_combine left

--- web/public/src/list_map_combine_left.mjs
+++ web/public/src/list_map_combine_left.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_combine_left(right, list) {
+  marker("1");
   function lambda(left) {
     let combined = left + right;
     return combined;
