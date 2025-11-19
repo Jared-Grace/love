d24bcaffa90fb16c03c451be06a58ce7f941a895
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 15:36:23 -0500

    function_auto_path ..\love\public\src\messenger_reply_user_data_path.mjs

--- web/public/src/messenger_reply_user_data_path.mjs
+++ web/public/src/messenger_reply_user_data_path.mjs
@@ -1,5 +1,7 @@
+import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
 export function messenger_reply_user_data_path() {
   let v =
-    "C:\\Users\\chris\\AppData\\Local\\Google\\Chrome\\User Data\\puppeteer-profile";
+    folder_user_path() +
+    "AppData\\Local\\Google\\Chrome\\User Data\\puppeteer-profile";
   return v;
 }
