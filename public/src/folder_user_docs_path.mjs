d28e242049307633824a6d35b320bb2385dd0ab7
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 15:36:11 -0500

    function_auto_path ..\love\public\src\folder_user_docs_path.mjs

--- public/src/folder_user_docs_path.mjs
+++ public/src/folder_user_docs_path.mjs
@@ -1,4 +1,5 @@
+import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
 export function folder_user_docs_path(file_name) {
-  let p = "C:\\Users\\chris\\" + "Documents\\" + file_name;
+  let p = folder_user_path() + "Documents\\" + file_name;
   return p;
 }
