0021af76d63333da4daf8f4d3d81c81783c4b6b4
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 17:15:03 -0500

    data_file_update ..\love\public\src\openai_key_folder.mjs

--- public/src/openai_key_path.mjs
+++
@@ -1,5 +0,0 @@
-import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
-export function openai_key_path() {
-  let v2 = folder_user_path() + "ChristGPT\\";
-  return v2;
-}
