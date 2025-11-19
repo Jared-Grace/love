e5688828a14c301d6d64292592b5747dee69e526
Grace-Jared <christrosetolife@gmail.com>
2025-11-19 17:16:18 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -1,9 +1,11 @@
+import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { openai_key_folder } from "../../../love/public/src/openai_key_folder.mjs";
 import { py_script_run_cmd } from "../../../love/public/src/py_script_run_cmd.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function g_preach_generate() {
+  let result = await file_temp(async function lambda(temp_path) {});
   let v = py_script_run_cmd("openai_chat");
   let k = openai_key_folder();
   let concated = [v, k + "openai.txt", input_file_path, output_file_path];
