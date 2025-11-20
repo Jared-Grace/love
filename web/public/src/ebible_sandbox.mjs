3e04a7e0f6d08a2b66bef3a83611c3623d98c96e
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 07:14:32 -0500

    function_auto_path ..\love\public\src\ebible_sandbox.mjs

--- web/public/src/ebible_sandbox.mjs
+++ web/public/src/ebible_sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { ebible_chapters_each_verses } from "../../../love/public/src/ebible_chapters_each_verses.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -9,6 +10,7 @@ export async function ebible_sandbox() {
   ("loop through to ensure parse correct before begin upload");
   await ebible_chapters_each_verses(bible_folder, each_chapter);
   async function each_chapter(chapter_code, verses) {
+    let mapped = list_map_property(list, property_name);
     log(verses);
   }
 }
