475678658a31b28bc9efd924475656ebb09ad14a
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 14:28:59 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- public/src/g_preach_generate.mjs
+++ public/src/g_preach_generate.mjs
@@ -1,7 +1,6 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { range_from } from "../../../love/public/src/range_from.mjs";
-import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
@@ -69,8 +68,7 @@ export async function g_preach_generate() {
       return item2;
     }
     let mapped2 = list_map(filtered, lambda2);
-    let sliced = list_slice(list, index - nearness, index + nearness + 1);
-    return sliced;
+    return mapped2;
   }
   let mapped = list_map_index(list, lambda);
   let file_name = file_name_json(chapter_code);
