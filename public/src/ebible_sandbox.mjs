a8a8bd80000acaeaebf40f3844472d438f2a80b4
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 07:14:08 -0500

    function_auto_path ..\love\public\src\ebible_sandbox.mjs

--- public/src/ebible_sandbox.mjs
+++ public/src/ebible_sandbox.mjs
@@ -2,7 +2,8 @@ import { log } from "../../../love/public/src/log.mjs";
 import { ebible_chapters_each_verses } from "../../../love/public/src/ebible_chapters_each_verses.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
-export async function ebible_sandbox(bible_folder) {
+export async function ebible_sandbox() {
+  let bible_folder = "engbsb";
   marker("1");
   await ebible_version_download(bible_folder);
   ("loop through to ensure parse correct before begin upload");
