a59921d0eca050d96b906def950680d2c77006f9
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 07:21:07 -0500

    function_auto_path ..\love\public\src\list_adder_multiple_unique_async.mjs

--- web/public/src/list_adder_multiple_unique_async.mjs
+++ web/public/src/list_adder_multiple_unique_async.mjs
@@ -2,5 +2,6 @@ import { list_adder_multiple_async } from "../../../love/public/src/list_adder_m
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function list_adder_multiple_unique_async(lambda$la) {
   marker("1");
-  return await list_adder_multiple_async(lambda$la);
+  let v = await list_adder_multiple_async(lambda$la);
+  return v;
 }
