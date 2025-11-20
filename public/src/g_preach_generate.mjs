ced664c42473c1ef3b75b245abeca4c911ee2f68
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 08:16:58 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- public/src/g_preach_generate.mjs
+++ public/src/g_preach_generate.mjs
@@ -1,3 +1,4 @@
+import { list_any } from "../../../love/public/src/list_any.mjs";
 import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
@@ -20,6 +21,7 @@ export async function g_preach_generate() {
     async function lambda5(la2) {
       async function lambda3(verse) {
         let text = object_property_get(verse, "text");
+        let any = list_any(list3, function lambda6(item2) {});
         let ew = string_ends_with(s, suffix);
         let verse_number = object_property_get(verse, "verse_number");
         let item = list_find_property(
