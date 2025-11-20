52faec96945192606fb82d5702d2226690c8a697
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 14:52:03 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- public/src/g_preach_generate.mjs
+++ public/src/g_preach_generate.mjs
@@ -97,7 +97,9 @@ export async function g_preach_generate() {
       joined +
       " :::: Here is the passage to rewrite: " +
       user_prompt;
-    log(prompt);
+    log({
+      prompt,
+    });
     return;
     let sermon = await g_preach_generate_passage(prompt);
     let v = {
