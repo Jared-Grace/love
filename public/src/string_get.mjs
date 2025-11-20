2b7598497d01388e9484eddc4646e6cc9a5a1457
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 07:19:02 -0500

    function_auto_path ..\love\public\src\string_get.mjs

--- public/src/string_get.mjs
+++ public/src/string_get.mjs
@@ -1,14 +1,14 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
-export function string_get(list, index) {
+export function string_get(s, index) {
   marker("1");
   integer_is_assert(index);
-  let item = list[index];
+  let item = s[index];
   undefined_not_is_assert_lambda(item, object_get);
   function object_get() {
     let v = {
-      list,
+      s,
       index,
     };
     return v;
