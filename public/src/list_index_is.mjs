63015d73903559368c83d88a943880975f48e703
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 14:38:23 -0500

    data_file_update ..\love\public\src\g_preach_generate.mjs

--- public/src/list_index_is.mjs
+++ public/src/list_index_is.mjs
@@ -1,6 +1,6 @@
 import { list_index_last } from "../../../love/public/src/list_index_last.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
-export function list_index_is(list, index) {
+export function list_index_is(list, index) {$aa
   let e = list_empty_is(list);
   let v = false;
   if (e) {
