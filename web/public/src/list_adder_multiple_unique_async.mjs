9deb362bad0739f08d7bb0952bfeafb043af650c
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 07:21:04 -0500

    fw list_adder_multiple_async list_adder_multiple_unique_async

--- web/public/src/list_adder_multiple_unique_async.mjs
+++ web/public/src/list_adder_multiple_unique_async.mjs
@@ -1,1 +1,6 @@
-export function list_adder_multiple_unique_async() {}
+import { list_adder_multiple_async } from "../../../love/public/src/list_adder_multiple_async.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function list_adder_multiple_unique_async(lambda$la) {
+  marker("1");
+  return await list_adder_multiple_async(lambda$la);
+}
