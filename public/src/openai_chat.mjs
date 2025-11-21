41e1630d317a363a50b9891b8defb08f00bc20f5
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 23:29:32 -0500

    function_auto_path ..\love\public\src\openai_chat.mjs

--- public/src/openai_chat.mjs
+++ public/src/openai_chat.mjs
@@ -2,6 +2,7 @@ import { command_line } from "../../../love/public/src/command_line.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { openai_key_folder } from "../../../love/public/src/openai_key_folder.mjs";
 import { py_script_run_cmd } from "../../../love/public/src/py_script_run_cmd.mjs";
+import { json_to } from "./json_to.mjs";
 export async function openai_chat(input_file_path, output_file_path) {
   let v = py_script_run_cmd("openai_chat");
   let k = openai_key_folder();
@@ -10,6 +11,7 @@ export async function openai_chat(input_file_path, output_file_path) {
   try {
     let stdout = await command_line(joined);
   } catch (e) {
+    let json = json_to(object);
     throw e;
   }
 }
