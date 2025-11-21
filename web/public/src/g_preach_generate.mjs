38b3d158931dd3a628968c99761848a4886ede38
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 23:11:01 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -1,3 +1,4 @@
+import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
@@ -121,8 +122,9 @@ export async function g_preach_generate() {
         skip: false,
       });
     }
-    function lambda11(item4) {
-      let mapped2 = list_filter_property(item4, "chapter_code", chapter_code);
+    function lambda11(group) {
+      let mapped2 = list_filter_property(group, "chapter_code", chapter_code);
+      let e = list_empty_is(list2);
     }
     let filtered2 = list_filter(list, lambda11);
     let mapped2 = list_filter_property(mapped, "chapter_code", chapter_code);
