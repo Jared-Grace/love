74835b6add8dd95f9d031f58e49850551ddbcfa4
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 21:18:05 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -34,7 +34,8 @@ export async function g_preach_generate() {
   let file_name = file_name_json(chapter_code);
   let path = local_function_path(g_preach_generate, file_name);
   let exists = await file_exists(path);
-  if (false) {
+  if (exists) {
+    return;
   }
   let chapters = await bible_interlinear_chapters();
   let interlinear = object_property_get(chapters, chapter_code);
