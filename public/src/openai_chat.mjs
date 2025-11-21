fa80f28b7a68bccbb9fa3ef70c5b35ab1315568e
Grace-Jared <christrosetolife@gmail.com>
2025-11-20 23:29:56 -0500

    function_auto_path ..\love\public\src\openai_chat.mjs

--- public/src/openai_chat.mjs
+++ public/src/openai_chat.mjs
@@ -13,7 +13,9 @@ export async function openai_chat(input_file_path, output_file_path) {
     let stdout = await command_line(joined);
   } catch (e) {
     let json = json_to(e);
-    log(json);
+    log({
+      json,
+    });
     throw e;
   }
 }
