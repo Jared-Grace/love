5362e2dd97096f6ba547bae430cad0465241a55a
Grace-Jared <christrosetolife@gmail.com>
2025-11-25 18:41:37 -0500

    function_auto_path ..\love\public\src\g_preach_generate.mjs

--- web/public/src/g_preach_generate.mjs
+++ web/public/src/g_preach_generate.mjs
@@ -5,9 +5,8 @@ export async function g_preach_generate() {
   const bible_folder = "engbsb";
   let books = await ebible_version_books(bible_folder);
   async function lambda(b) {
-    let book_code = b;
+    let { book_code } = b;
+    await g_preach_generate_book(bible_folder, book_code);
   }
   await each_async(list, lambda);
-  return books;
-  await g_preach_generate_book(bible_folder, book_code);
 }
