2c6eaede7294672493853e0db41fa7bc510d5936
Grace-Jared <christrosetolife@gmail.com>
2025-11-26 16:44:15 -0500

    function_auto_path ..\love\public\src\g_preach_generate_passage.mjs

--- public/src/g_preach_generate_passage.mjs
+++ public/src/g_preach_generate_passage.mjs
@@ -1,7 +1,4 @@
-import { file_temp } from "../../../love/public/src/file_temp.mjs";
-import { file_read } from "../../../love/public/src/file_read.mjs";
-import { openai_chat } from "../../../love/public/src/openai_chat.mjs";
-import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
+import { g_generate_openai } from "../../../love/public/src/g_generate_openai.mjs";
 export async function g_preach_generate_passage(passage) {
   const prompt = `You are a Christian preacher. You will be given a passage and its context. Rewrite the passage as follows:
 
@@ -19,20 +16,6 @@ export async function g_preach_generate_passage(passage) {
 
 Output each sentence separated by '\\r\\n'. Follow these instructions exactly.`;
   let user = passage;
-  let input = {
-    system: prompt,
-    user,
-  };
-  let data = null;
-  async function lambda(input_file_path) {
-    async function lambda2(output_file_path) {
-      await file_overwrite_json(input_file_path, input);
-      await openai_chat(input_file_path, output_file_path);
-      data = await file_read(output_file_path);
-    }
-    let result2 = await file_temp(lambda2);
-  }
-  let result = await file_temp(lambda);
-  let sermon = data;
+  let sermon = await g_generate_openai(prompt, user);
   return sermon;
 }
