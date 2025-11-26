2db8f892246bcf902993ac87b04c8373cc3e6772
Grace-Jared <christrosetolife@gmail.com>
2025-11-26 16:40:33 -0500

    data_file_update ..\love\public\src\g_objection_generate.mjs

--- public/src/g_objections_generate.mjs
+++
@@ -1,6 +0,0 @@
-import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function g_objections_generate() {
-  marker("1");
-  let path = local_function_path_json(chapter_code, g_objections_generate);
-}
