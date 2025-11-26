da2f052797f2e17e3c474ca8956fb2b60ce612a9
Grace-Jared <christrosetolife@gmail.com>
2025-11-26 04:34:23 -0500

    cs list_map async

--- public/src/list_map_async.mjs
+++ public/src/list_map_async.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 export function list_map_async(list, lambda$item) {
+  marker("1");
   list_is_assert(list);
   let mapped = list.map(lambda$item);
   return mapped;
