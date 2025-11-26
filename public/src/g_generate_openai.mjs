abe433f293f02477ef2d2b3bc9830a2636f67bb3
Grace-Jared <christrosetolife@gmail.com>
2025-11-26 16:44:15 -0500

    function_auto_path ..\love\public\src\g_preach_generate_passage.mjs

---
+++ public/src/g_generate_openai.mjs
@@ -0,0 +1,21 @@
+import { file_temp } from "../../../love/public/src/file_temp.mjs";
+import { file_read } from "../../../love/public/src/file_read.mjs";
+import { openai_chat } from "../../../love/public/src/openai_chat.mjs";
+import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
+export async function g_generate_openai(system, user) {
+  let input = {
+    system,
+    user,
+  };
+  let data = null;
+  async function lambda(input_file_path) {
+    async function lambda2(output_file_path) {
+      await file_overwrite_json(input_file_path, input);
+      await openai_chat(input_file_path, output_file_path);
+      data = await file_read(output_file_path);
+    }
+    let result2 = await file_temp(lambda2);
+  }
+  let result = await file_temp(lambda);
+  return data;
+}
