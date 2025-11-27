9deadc5a0aa281d1d100c0e32dd7f06d75b9d325
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 12:48:44 -0500

    function_auto_path ..\love\public\src\folder_files_names_underscore.mjs

--- public/src/folder_files_names_underscore.mjs
+++ public/src/folder_files_names_underscore.mjs
@@ -5,22 +5,20 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_move } from "../../../love/public/src/file_move.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
-export async function folder_files_names_underscore(path, matches, prefix) {
+export async function folder_files_names_underscore(path) {
   marker("1");
   let files = await folder_read_files(path);
   let index = 1;
   async function lambda2(la) {
     async function lambda(name_old) {
-      if (matches(name_old)) {
-        let name_new = prefix + index + ".zip";
-        let path_new = path_join([path, name_new]);
-        if (equal_not(name_old, name_new)) {
-          let path_old = path_join([path, name_old]);
-          await file_move(path_old, path_new);
-        }
-        la(path_new);
-        index++;
+      let name_new = prefix + index + ".zip";
+      let path_new = path_join([path, name_new]);
+      if (equal_not(name_old, name_new)) {
+        let path_old = path_join([path, name_old]);
+        await file_move(path_old, path_new);
       }
+      la(path_new);
+      index++;
     }
     await each_async(files, lambda);
   }
