21c43fd519906e616f44b277f61747ca2cb9e8d3
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:39:07 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,10 +1,17 @@
+import { string_replace } from "../../../love/public/src/string_replace.mjs";
+import { folder_files_names_transform } from "../../../love/public/src/folder_files_names_transform.mjs";
 import { g_folder_tiles } from "../../../love/public/src/g_folder_tiles.mjs";
-import { folder_files_names_underscore } from "../../../love/public/src/folder_files_names_underscore.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { path_join } from "./path_join.mjs";
 export async function sandbox() {
   marker("1");
   const tiles_path = g_folder_tiles("");
   let joined = path_join(["public", tiles_path]);
-  let list = await folder_files_names_underscore(joined);
+  marker("1");
+  let list2 = await folder_files_names_transform(joined, transform);
+  return list2;
+  function transform(name_old) {
+    let replaced = string_replace(name_old, " ", "_");
+    return replaced;
+  }
 }
