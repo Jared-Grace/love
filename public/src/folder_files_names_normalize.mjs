a554d5f908d152ff03c7ca700dc0c7eb200759b4
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:10:39 -0500

    function_auto_path ..\love\public\src\folder_files_names_normalize.mjs

--- public/src/folder_files_names_normalize.mjs
+++ public/src/folder_files_names_normalize.mjs
@@ -1,3 +1,4 @@
+import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_move } from "../../../love/public/src/file_move.mjs";
@@ -7,6 +8,7 @@ import { folder_read_files } from "../../../love/public/src/folder_read_files.mj
 export async function folder_files_names_normalize(path, include, prefix) {
   let files = await folder_read_files(path);
   let index = 1;
+  let list = await list_adder_async(async function lambda2(la) {});
   async function lambda(name_old) {
     let i = string_includes(name_old, include);
     if (i) {
