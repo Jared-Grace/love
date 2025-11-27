acd37dbbc0b314a311727bf34363fa49dee77d2c
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 09:56:31 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
+import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   let p = "C:\\Users\\chris\\Downloads\\characters";
+  let files = await folder_read_files(path_folder);
 }
