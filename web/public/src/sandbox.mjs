1e8b1e5f5ed2ae0b9c3aff1d0f4699f563ff7ad7
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 10:11:18 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -4,5 +4,6 @@ export async function sandbox() {
   marker("1");
   let path = "C:\\Users\\chris\\Downloads\\characters";
   await folder_files_names_normalize(path, "woman", "woman_");
+  await folder_files_names_normalize(path, "woman", "woman_");
   return files;
 }
