e826ebc8baa198fc56408b1da13c59c6e01f9bc8
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:24:29 -0500

    data_file_update ..\love\public\src\unzip_self_multiple.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -7,10 +7,6 @@ export async function sandbox() {
   marker("1");
   let path = "C:\\Users\\chris\\Downloads\\characters";
   let files = await folder_files_names_normalize(path, "woman", "woman_");
-  async function lambda(file_path) {
-    let name = await unzip_self(file_path);
-    log(name);
-  }
   await unzip_self_multiple(files);
   await folder_files_names_normalize(path, "_man_", "man_");
 }
