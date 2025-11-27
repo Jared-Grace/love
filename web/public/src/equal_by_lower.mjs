002b85a4114801d7c312c966fa5778e2422332d8
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:43:22 -0500

    function_auto_path ..\love\public\src\equal_by_lower.mjs

--- web/public/src/equal_by_lower.mjs
+++ web/public/src/equal_by_lower.mjs
@@ -2,5 +2,6 @@ import { equal_by } from "../../../love/public/src/equal_by.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function equal_by_lower(a, b, lambda$item) {
   marker("1");
-  return equal_by(a, b, lambda$item);
+  let eq = equal_by(a, b, lambda$item);
+  return eq;
 }
