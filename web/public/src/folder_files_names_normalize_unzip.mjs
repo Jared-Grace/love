9044a5098c2c5691a55b7e0cb721fd5c60e873e9
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:29:30 -0500

    function_auto_path ..\love\public\src\folder_files_names_normalize_unzip.mjs

--- web/public/src/folder_files_names_normalize_unzip.mjs
+++ web/public/src/folder_files_names_normalize_unzip.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { unzip_self_multiple } from "../../../love/public/src/unzip_self_multiple.mjs";
 import { folder_files_names_normalize } from "../../../love/public/src/folder_files_names_normalize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -8,6 +9,7 @@ export async function folder_files_names_normalize_unzip(
 ) {
   marker("1");
   let files = await folder_files_names_normalize(path, include, prefix);
+  log(message);
   await unzip_self_multiple(files);
   return files;
 }
