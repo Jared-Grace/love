ff8cca054506ae96a645ac8f40a607cee4f30efd
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 10:09:28 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,21 +6,24 @@ import { folder_read_files } from "../../../love/public/src/folder_read_files.mj
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
-  let p = "C:\\Users\\chris\\Downloads\\characters";
-  let files = await folder_read_files(p);
+  let path = "C:\\Users\\chris\\Downloads\\characters";
   const include = "woman";
   const prefix = "woman_";
-  let index = 1;
-  async function lambda(name_old) {
-    let i = string_includes(name_old, include);
-    if (i) {
-      let name_new = prefix + index + ".zip";
-      let path_old = path_join([p, name_old]);
-      let path_new = path_join([p, name_new]);
-      await file_move(path_old, path_new);
-      i++;
-    }
-  }
-  await each_async(files, lambda);
+  await sandbox_rename(path, include, prefix);
   return files;
+  async function sandbox_rename(path, include, prefix) {
+    let files = await folder_read_files(path);
+    let index = 1;
+    async function lambda(name_old) {
+      let i = string_includes(name_old, include);
+      if (i) {
+        let name_new = prefix + index + ".zip";
+        let path_old = path_join([path, name_old]);
+        let path_new = path_join([path, name_new]);
+        await file_move(path_old, path_new);
+        i++;
+      }
+    }
+    await each_async(files, lambda);
+  }
 }
