b70a932114f765adcfdcd3771f1987bb18bc5dce
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:46:44 -0500

    function_auto_path ..\love\public\src\folder_files_names_lower.mjs

--- web/public/src/folder_files_names_lower.mjs
+++ web/public/src/folder_files_names_lower.mjs
@@ -1,10 +1,6 @@
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { folder_files_names_transform } from "../../../love/public/src/folder_files_names_transform.mjs";
 export async function folder_files_names_lower(joined) {
-  let list2 = await folder_files_names_transform(joined, transform);
-  function transform(name_old) {
-    let replaced = string_lower_to(name_old);
-    return replaced;
-  }
+  let list2 = await folder_files_names_transform(joined, string_lower_to);
   return list2;
 }
