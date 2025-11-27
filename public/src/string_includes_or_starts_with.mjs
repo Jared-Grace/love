9b2a415869835dee8a72e5e469557517177222f0
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:33:20 -0500

    function_auto_path ..\love\public\src\string_includes_or_starts_with.mjs

--- public/src/string_includes_or_starts_with.mjs
+++ public/src/string_includes_or_starts_with.mjs
@@ -1,7 +1,7 @@
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
-export function string_includes_or_starts_with(item) {
-  let i = string_includes(input, item);
+export function string_includes_or_starts_with(s, item) {
+  let i = string_includes(s, item);
   let sw = string_starts_with(s, item);
   let v = sw || i;
   return v;
