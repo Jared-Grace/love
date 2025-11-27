4f5e98493eb7d82e15678a3a129ab31113c3d1b1
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:46:46 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

---
+++ public/src/html_img.mjs
@@ -0,0 +1,6 @@
+import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
+import { html_element } from "../../../love/public/src/html_element.mjs";
+export function html_img(body, src) {
+  let component = html_element(body, "img");
+  html_attribute_set(component, "src", src);
+}
