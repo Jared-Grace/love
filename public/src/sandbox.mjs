da450974751506d53e75a36ab50c6a09d6463383
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:22:16 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,4 @@
-import { unzip } from "../../../love/public/src/unzip.mjs";
-import { path_without_extension } from "../../../love/public/src/path_without_extension.mjs";
+import { unzip_self } from "../../../love/public/src/unzip_self.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { folder_files_names_normalize } from "../../../love/public/src/folder_files_names_normalize.mjs";
@@ -9,8 +8,7 @@ export async function sandbox() {
   let path = "C:\\Users\\chris\\Downloads\\characters";
   let files = await folder_files_names_normalize(path, "woman", "woman_");
   async function lambda(file_path) {
-    let name = path_without_extension(file_path);
-    await unzip(name, file_path);
+    let name = await unzip_self(file_path);
     log(name);
   }
   await each_async(files, lambda);
