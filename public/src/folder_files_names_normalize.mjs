7355feec4bd46e7ea5da1a3dae5184144dffa856
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:11:01 -0500

    function_auto_path ..\love\public\src\folder_files_names_normalize.mjs

--- public/src/folder_files_names_normalize.mjs
+++ public/src/folder_files_names_normalize.mjs
@@ -8,19 +8,22 @@ import { folder_read_files } from "../../../love/public/src/folder_read_files.mj
 export async function folder_files_names_normalize(path, include, prefix) {
   let files = await folder_read_files(path);
   let index = 1;
-  async function lambda2(la) {}
-  let list = await list_adder_async(lambda2);
-  async function lambda(name_old) {
-    let i = string_includes(name_old, include);
-    if (i) {
-      let name_new = prefix + index + ".zip";
-      if (equal_not(name_old, name_new)) {
-        let path_old = path_join([path, name_old]);
-        let path_new = path_join([path, name_new]);
-        await file_move(path_old, path_new);
+  async function lambda2(la) {
+    async function lambda(name_old) {
+      let i = string_includes(name_old, include);
+      if (i) {
+        let name_new = prefix + index + ".zip";
+        if (equal_not(name_old, name_new)) {
+          let path_old = path_join([path, name_old]);
+          let path_new = path_join([path, name_new]);
+          await file_move(path_old, path_new);
+        }
+        la(path_new);
+        index++;
       }
-      index++;
     }
+    await each_async(files, lambda);
   }
-  await each_async(files, lambda);
+  let list = await list_adder_async(lambda2);
+  return list;
 }
