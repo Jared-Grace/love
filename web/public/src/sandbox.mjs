c4bd7a0df6afe292945a0b37f960d39b6a97c42b
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 09:54:38 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,6 +1,5 @@
-import { bible_interlinear_verses_upload } from "../../../love/public/src/bible_interlinear_verses_upload.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
-  await bible_interlinear_verses_upload();
+  let p = "C:\\Users\\chris\\Downloads\\characters";
 }
