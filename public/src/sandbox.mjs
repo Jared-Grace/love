f51e6c41b5a00a5740f099e977795a4f0f1f4be7
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:11:41 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,10 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { folder_files_names_normalize } from "../../../love/public/src/folder_files_names_normalize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   let path = "C:\\Users\\chris\\Downloads\\characters";
   let files = await folder_files_names_normalize(path, "woman", "woman_");
+  each(list, function lambda(item) {});
   await folder_files_names_normalize(path, "_man_", "man_");
 }
