8e8f5e57fb46d6be9ce4556b8fe31a7646f1c532
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:26:21 -0500

    function_auto_path ..\love\public\src\file_move.mjs

--- web/public/src/file_move.mjs
+++ web/public/src/file_move.mjs
@@ -1,5 +1,9 @@
+import { equal } from "../../../love/public/src/equal.mjs";
 import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
 export async function file_move(old_path, new_path) {
+  if (equal(old_path, new_path)) {
+    return;
+  }
   await assert_file_exists_not(new_path);
   const fs = await import("fs");
   await fs.promises.rename(old_path, new_path);
