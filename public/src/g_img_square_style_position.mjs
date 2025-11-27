2ba416628bb8be2a8092d3f589dd8723b433ae49
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 15:47:16 -0500

    function_auto_path ..\love\public\src\g_img_square_style_position.mjs

--- public/src/g_img_square_style_position.mjs
+++ public/src/g_img_square_style_position.mjs
@@ -2,6 +2,6 @@ import { html_style_assign } from "../../../love/public/src/html_style_assign.mj
 export function g_img_square_style_position(tile, x, square_count, y) {
   html_style_assign(tile, {
     left: "calc(" + x + " * (100vh / " + rows + "))",
-    top: y * square_count + "px",
+    top: "calc(" + y + " * (100vh / " + rows + "))",
   });
 }
