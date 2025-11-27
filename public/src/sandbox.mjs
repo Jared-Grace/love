c66ca9a13d1b85c02a2465e5651165106dfafc2d
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:31:34 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -9,6 +9,9 @@ export async function sandbox() {
     return i;
   }
   await folder_files_names_normalize_unzip(path, lambda, "woman_");
-  function lambda2() {}
+  function lambda2() {
+    let i = string_includes(input, "woman");
+    return i;
+  }
   await folder_files_names_normalize_unzip(path, lambda2, "man_");
 }
