936ceb59387c9e4ff032beddb90229ad58458675
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:24:43 -0500

    fws folder_files_names_normalize unzip

--- public/src/folder_files_names_normalize_unzip.mjs
+++ public/src/folder_files_names_normalize_unzip.mjs
@@ -1,1 +1,10 @@
-export function folder_files_names_normalize_unzip() {}
+import { folder_files_names_normalize } from "../../../love/public/src/folder_files_names_normalize.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function folder_files_names_normalize_unzip(
+  path,
+  include,
+  prefix,
+) {
+  marker("1");
+  return await folder_files_names_normalize(path, include, prefix);
+}
