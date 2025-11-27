22a7a5e438370c28c2b19feeacd022d62d1cb715
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 14:50:49 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -9,8 +9,6 @@ import { each_range } from "../../../love/public/src/each_range.mjs";
 import { g_folder_tiles } from "../../../love/public/src/g_folder_tiles.mjs";
 import { g_folder_img } from "../../../love/public/src/g_folder_img.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
-import { html_style_size_square } from "../../../love/public/src/html_style_size_square.mjs";
-import { html_img } from "../../../love/public/src/html_img.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -72,12 +70,6 @@ export function app_g_main() {
     each_range(10, lambda);
   }
   each_range(10, lambda2);
-  const src2 = game_prefix + "characters\\man_1\\rotations\\south.png";
-  let c = html_img(div, src2);
-  html_style_size_square(c, square_size);
-  html_style_assign(c, {
-    position: "absolute",
-    x: "0px",
-    y: "0px",
-  });
+  const c_src = game_prefix + "characters\\man_1\\rotations\\south.png";
+  g_img_square(div, c_src, square_size, x, square_count, y);
 }
