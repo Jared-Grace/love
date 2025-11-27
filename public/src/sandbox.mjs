02545ae07cb6f96e5a726a4bfd9cf26e13a8e898
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 10:11:02 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,4 @@
-import { path_join } from "../../../love/public/src/path_join.mjs";
-import { file_move } from "../../../love/public/src/file_move.mjs";
-import { string_includes } from "../../../love/public/src/string_includes.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
+import { folder_files_names_normalize } from "../../../love/public/src/folder_files_names_normalize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
@@ -11,19 +7,4 @@ export async function sandbox() {
   const prefix = "woman_";
   await folder_files_names_normalize(path, include, prefix);
   return files;
-  async function folder_files_names_normalize(path, include, prefix) {
-    let files = await folder_read_files(path);
-    let index = 1;
-    async function lambda(name_old) {
-      let i = string_includes(name_old, include);
-      if (i) {
-        let name_new = prefix + index + ".zip";
-        let path_old = path_join([path, name_old]);
-        let path_new = path_join([path, name_new]);
-        await file_move(path_old, path_new);
-        i++;
-      }
-    }
-    await each_async(files, lambda);
-  }
 }
