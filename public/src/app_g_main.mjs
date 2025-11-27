0bd2f8f339a2565878e9e31a8239c36bb8f10cb3
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 15:59:19 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -64,8 +64,8 @@ export function app_g_main() {
   function lambda8() {
     app_g_refresh(game_prefix, div, tiles, tiles_path);
   }
-  let v = html_component_wrap(element);
-  html_on(window, "resize", lambda8);
+  let v = html_component_wrap(window);
+  html_on(v, "resize", lambda8);
   function app_g_refresh(game_prefix, div, tiles, tiles_path) {
     const c_src = game_prefix + "characters\\man_1\\rotations\\south.png";
     let c = g_img_square(div, c_src, 5, 5, "character");
