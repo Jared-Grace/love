fda0a401646f3981dcd290dccd1aa6fc8a8d46a3
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 18:51:49 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -232,6 +232,7 @@ export function app_g_main() {
               left: "0",
               width: "100vw",
               height: "100vh",
+              padding: "1vw",
               background: "rgba(0,0,0,0.4)",
               "z-index": g_z("overlay"),
             };
