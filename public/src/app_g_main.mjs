8ebca56c7e41994bb6a43f96f6facd586d2f32cb
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:40:30 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -1,9 +1,11 @@
+import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_g_main() {
   marker("1");
   let body = html_document_body();
-  let component = html_element(body, "img");has;
+  let component = html_element(body, "img");
+  html_attribute_set(component2, key, value);
   ("img\\game\\tiles\\seamless");
 }
