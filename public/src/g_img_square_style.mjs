a7691a6d84c61913fa2df6b9b55997149606d0f0
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 15:19:33 -0500

    function_auto_path ..\love\public\src\g_img_square_style.mjs

--- public/src/g_img_square_style.mjs
+++ public/src/g_img_square_style.mjs
@@ -1,3 +1,4 @@
+import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_size_square } from "../../../love/public/src/html_style_size_square.mjs";
@@ -7,8 +8,5 @@ export function g_img_square_style(tile, square_size, x, square_count, y, z) {
     position: "absolute",
     "z-index": list_index_of(["tile", "character", "click"], z),
   });
-  html_style_assign(tile, {
-    left: x * square_count + "px",
-    top: y * square_count + "px",
-  });
+  g_img_square_style_position(tile, x, square_count, y);
 }
---
+++ public/src/g_img_square_style_position.mjs
@@ -0,0 +1,7 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function g_img_square_style_position(tile, x, square_count, y) {
+  html_style_assign(tile, {
+    left: x * square_count + "px",
+    top: y * square_count + "px",
+  });
+}
