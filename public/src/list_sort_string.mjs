6e540ac7b82531b22249ad2b11c7902c021c69f9
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:16:00 -0500

    function_auto_path ..\love\public\src\list_sort_string.mjs

--- public/src/list_sort_string.mjs
+++ public/src/list_sort_string.mjs
@@ -1,3 +1,9 @@
 export function list_sort_string(list) {
-  list.sort();
+  function lambda(a, b) {
+    const [na, ia] = a.split(/[_\.]/);
+    const [nb, ib] = b.split(/[_\.]/);
+    let v = na.localeCompare(nb) || Number(ia) - Number(ib);
+    return v;
+  }
+  list.sort(lambda);
 }
