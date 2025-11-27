a7e01a3ca9de37b4b7b443719ecee67fc8a794e1
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:39:39 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -1,5 +1,6 @@
+import { html_element } from "../../../love/public/src/html_element.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_g_main() {
   marker("1");
-  alert("test");
+  let component = html_element(parent, tag_name);
 }
