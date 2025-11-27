3c11300d5aee64c546353b464b81d08a3a940e6c
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:46:37 -0500

    function_auto_path ..\love\public\src\folder_files_names_underscore.mjs

--- public/src/folder_files_names_underscore.mjs
+++ public/src/folder_files_names_underscore.mjs
@@ -3,10 +3,9 @@ import { folder_files_names_transform } from "../../../love/public/src/folder_fi
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function folder_files_names_underscore(path) {
   marker("1");
-  let list = await folder_files_names_transform(path, transform);
+  let list = await folder_files_names_transform(
+    path,
+    string_replace_space_underscore,
+  );
   return list;
-  function transform(name_old) {
-    let replaced = string_replace_space_underscore(name_old);
-    return replaced;
-  }
 }
