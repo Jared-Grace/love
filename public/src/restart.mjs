451128de649bde379c25e641a742d308e56a99ad
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 14:11:59 -0500

    function_auto_path ..\love\public\src\restart.mjs

--- public/src/restart.mjs
+++ public/src/restart.mjs
@@ -2,5 +2,5 @@ import { command_line } from "../../../love/public/src/command_line.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function restart() {
   marker("1");
-  let stdout = await command_line(command);
+  let stdout = await command_line("shutdown -t 0 -r");
 }
