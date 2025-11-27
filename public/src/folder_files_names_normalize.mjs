2bfbe8868bf73d1ec9f41d354f119b410be25e9f
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 10:15:24 -0500

    function_auto_path ..\love\public\src\folder_files_names_normalize.mjs

--- public/src/folder_files_names_normalize.mjs
+++ public/src/folder_files_names_normalize.mjs
@@ -1,4 +1,4 @@
-import { equal } from "../../../love/public/src/equal.mjs";
+import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_move } from "../../../love/public/src/file_move.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
@@ -11,12 +11,11 @@ export async function folder_files_names_normalize(path, include, prefix) {
     let i = string_includes(name_old, include);
     if (i) {
       let name_new = prefix + index + ".zip";
-      if (equal(name_old, name_new)) {
-        return;
+      if (equal_not(name_old, name_new)) {
+        let path_old = path_join([path, name_old]);
+        let path_new = path_join([path, name_new]);
+        await file_move(path_old, path_new);
       }
-      let path_old = path_join([path, name_old]);
-      let path_new = path_join([path, name_new]);
-      await file_move(path_old, path_new);
       index++;
     }
   }
