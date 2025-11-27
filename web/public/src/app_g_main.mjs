94d10b38b474c48325a0bad1ee5db6768dca2b6f
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 17:48:50 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- web/public/src/app_g_main.mjs
+++ web/public/src/app_g_main.mjs
@@ -1,5 +1,3 @@
-import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { each_object } from "../../../love/public/src/each_object.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { html_document_head } from "../../../love/public/src/html_document_head.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
@@ -170,15 +168,7 @@ export function app_g_main() {
             y,
           };
           if (false) {
-            function lambda17(item) {
-              let eq = true;
-              function lambda18(xy, value) {
-                if (equal_not(left, right2)) {
-                }
-              }
-              each_object(clicked_coordinates, lambda18);
-            }
-            let filtered2 = list_filter(list, lambda17);
+            let filtered2 = list_filter(npcs, () => {});
           }
           object_assign(c, clicked_coordinates);
           function handler(e) {
