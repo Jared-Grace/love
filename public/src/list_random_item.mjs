f833cf781e55df4a12bbd44df76abf3156d6a3e4
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:56:39 -0500

    function_auto_path ..\love\public\src\list_random_item.mjs

--- public/src/list_random_item.mjs
+++ public/src/list_random_item.mjs
@@ -2,9 +2,9 @@ import { list_get } from "../../../love/public/src/list_get.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { random } from "../../../love/public/src/random.mjs";
-export function list_random_item(arr) {
-  const p = random() * list_size(arr);
+export function list_random_item(list) {
+  const p = random() * list_size(list);
   let index = floor(p);
-  let r = list_get(arr, index);
+  let r = list_get(list, index);
   return r;
 }
