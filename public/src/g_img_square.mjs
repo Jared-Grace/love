32f0f0cc3892e41718972f35b73eb699a6678fe6
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 14:50:25 -0500

    function_auto_path ..\love\public\src\g_img_square.mjs

--- public/src/g_img_square.mjs
+++ public/src/g_img_square.mjs
@@ -1,8 +1,8 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_size_square } from "../../../love/public/src/html_style_size_square.mjs";
 import { html_img } from "../../../love/public/src/html_img.mjs";
-export function g_img_square(div, src, square_size, x, square_count, y) {
-  let tile = html_img(div, src);
+export function g_img_square(parent, src, square_size, x, square_count, y) {
+  let tile = html_img(parent, src);
   html_style_size_square(tile, square_size);
   html_style_assign(tile, {
     position: "absolute",
