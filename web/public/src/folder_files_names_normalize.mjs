970eb3425e99f7146907ef99ad18b0c5a36f36a0
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:30:42 -0500

    function_auto_path ..\love\public\src\folder_files_names_normalize.mjs

--- web/public/src/folder_files_names_normalize.mjs
+++ web/public/src/folder_files_names_normalize.mjs
@@ -3,15 +3,13 @@ import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_move } from "../../../love/public/src/file_move.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
-export async function folder_files_names_normalize(path, include, prefix) {
+export async function folder_files_names_normalize(path, matches, prefix) {
   let files = await folder_read_files(path);
   let index = 1;
   async function lambda2(la) {
     async function lambda(name_old) {
-      let i = string_includes(name_old, include);
-      if (i) {
+      if (matches(name_old)) {
         let name_new = prefix + index + ".zip";
         let path_new = path_join([path, name_new]);
         if (equal_not(name_old, name_new)) {
