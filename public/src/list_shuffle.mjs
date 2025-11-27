b35ff41295cff142f6738e58c22fd9cd6e50e53e
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 14:03:23 -0500

    function_auto_path ..\love\public\src\list_shuffle.mjs

--- public/src/list_shuffle.mjs
+++ public/src/list_shuffle.mjs
@@ -2,7 +2,7 @@ import { random } from "../../../love/public/src/random.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
 export function list_shuffle(list) {
   for (let i = list.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = floor(random() * (i + 1));
     [list[i], list[j]] = [list[j], list[i]];
   }
   return list;
