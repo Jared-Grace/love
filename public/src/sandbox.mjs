148aa6b14b2e382a7cb6444dad98c406088eee64
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 13:37:54 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,6 +5,6 @@ import { path_join } from "./path_join.mjs";
 export async function sandbox() {
   marker("1");
   const tiles_path = g_folder_tiles("");
-  let joined = path_join(segments);
+  let joined = path_join(["public", tiles_path]);
   let list = await folder_files_names_underscore(tiles_path);
 }
