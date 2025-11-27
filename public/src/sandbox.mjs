72136b2a5f7223cbfcb71e21e0eba9ddecaf9772
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 09:58:19 -0500

    data_file_update ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,3 @@
-import { string_is } from "../../../love/public/src/string_is.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,9 +5,7 @@ export async function sandbox() {
   marker("1");
   let p = "C:\\Users\\chris\\Downloads\\characters";
   let files = await folder_read_files(p);
-  async function lambda(item) {
-    let si2 = string_is(value);
-  }
+  async function lambda(item) {sin}
   await each_async(list, lambda);
   return files;
 }
