58087090d809ee3e211ab67b94af0022913cb397
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 15:13:26 -0500

    function_auto_path ..\love\public\src\g_img_square_style.mjs

--- public/src/g_img_square_style.mjs
+++ public/src/g_img_square_style.mjs
@@ -1,10 +1,11 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_size_square } from "../../../love/public/src/html_style_size_square.mjs";
-export function g_img_square_style(tile, square_size, x, square_count, y) {
+export function g_img_square_style(tile, square_size, x, square_count, y, z) {
   html_style_size_square(tile, square_size);
   html_style_assign(tile, {
     position: "absolute",
     left: x * square_count + "px",
     top: y * square_count + "px",
+    "z-index": z,
   });
 }
