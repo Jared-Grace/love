c372744862d57cb921b11d650d075e34304eae79
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 17:03:26 -0500

    function_auto_path ..\love\public\src\g_character_img.mjs

--- web/public/src/g_character_img.mjs
+++ web/public/src/g_character_img.mjs
@@ -3,7 +3,7 @@ import { g_img_square_style_position_object } from "../../../love/public/src/g_i
 import { g_img_square } from "../../../love/public/src/g_img_square.mjs";
 export function g_character_img(game_prefix, div, c) {
   let img = object_property_get(c, "img");
-  const c_src = game_prefix + "characters\\" + p + "\\rotations\\south.png";
+  const c_src = game_prefix + "characters\\" + img + "\\rotations\\south.png";
   let ci = g_img_square(div, c_src, -1, -1, "character");
   g_img_square_style_position_object(c, ci);
   return ci;
