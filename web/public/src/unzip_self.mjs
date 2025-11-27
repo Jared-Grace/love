e88f1cd523d6e588a6739c749514a80a628907ff
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:26:27 -0500

    function_auto_path ..\love\public\src\unzip_self.mjs

--- web/public/src/unzip_self.mjs
+++ web/public/src/unzip_self.mjs
@@ -1,7 +1,9 @@
+import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { unzip } from "../../../love/public/src/unzip.mjs";
 import { path_without_extension } from "../../../love/public/src/path_without_extension.mjs";
 export async function unzip_self(file_path) {
   let name = path_without_extension(file_path);
+  await file_exists(file_path);
   await unzip(name, file_path);
   return name;
 }
