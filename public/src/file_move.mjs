83c873b7e9b020903f6615087024a95a6c473346
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 10:01:17 -0500

    function_auto_path ..\love\public\src\file_move.mjs

--- public/src/file_move.mjs
+++ public/src/file_move.mjs
@@ -1,8 +1,8 @@
+import { file_delete } from "../../../love/public/src/file_delete.mjs";
+import { file_copy } from "../../../love/public/src/file_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { function_delete } from "../../../love/public/src/function_delete.mjs";
-import { function_copy } from "../../../love/public/src/function_copy.mjs";
 export async function file_move(f_name_before, f_name_after) {
   marker("1");
-  await function_copy(f_name_before, f_name_after);
-  await function_delete(f_name_before);
+  await file_copy(f_name_before, f_name_after);
+  await file_delete(f_name_before);
 }
