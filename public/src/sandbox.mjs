dc248c2a83a237ea50a20e5091fc8f64b8c8ab71
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 11:20:47 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
+import { path_without_extension } from "../../../love/public/src/path_without_extension.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { path_name } from "../../../love/public/src/path_name.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { folder_files_names_normalize } from "../../../love/public/src/folder_files_names_normalize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -8,7 +8,7 @@ export async function sandbox() {
   let path = "C:\\Users\\chris\\Downloads\\characters";
   let files = await folder_files_names_normalize(path, "woman", "woman_");
   async function lambda(file_path) {
-    let name = path_name(file_path);
+    let name = path_without_extension(file_path);
     log(name);
   }
   await each_async(files, lambda);
