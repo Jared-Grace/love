ef52618f243d0dc9dc4fef4ee986b03d5c7e8eb7
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 20:10:14 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -219,6 +219,10 @@ export function app_g_main() {
             let text = emoji_pray() + " Pray";
             function lambda7() {
               html_clear(overlay);
+              let button2 = app_karate_button_back(
+                container2,
+                function lambda23() {},
+              );
               function lambda22() {}
               let component3 = app_karate_button_uncolored(
                 overlay,
