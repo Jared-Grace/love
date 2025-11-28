48a40aeff3d39261dca9d6dbaa2e22f6814bfb86
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 23:15:59 -0500

    function_auto_path ..\love\public\src\app_g_menu_refresh.mjs

--- web/public/src/app_g_menu_refresh.mjs
+++ web/public/src/app_g_menu_refresh.mjs
@@ -5,7 +5,6 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
 import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { emoji_bow } from "../../../love/public/src/emoji_bow.mjs";
-import { app_karate_button_uncolored } from "../../../karate_code/public/src/app_karate_button_uncolored.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { html_remove } from "../../../love/public/src/html_remove.mjs";
@@ -29,7 +28,7 @@ export function app_g_menu_refresh(overlay) {
       app_g_player_save(player);
       close();
     }
-    let component3 = app_karate_button_uncolored(
+    app_g_button_uncolored(
       overlay,
       emoji_bow() +
         " Heavenly Father, please bless this next conversation, in Jesus' name, amen! " +
