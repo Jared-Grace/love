09358309d58649daf6e9e4f04b3a99b0c91260ae
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 20:02:39 -0500

    function_auto_path ..\love\public\src\g_img_square_style_position.mjs

--- public/src/g_img_square_style_position_object.mjs
+++ public/src/g_img_square_style_position_object.mjs
@@ -2,7 +2,6 @@ import { log } from "../../../love/public/src/log.mjs";
 import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function g_img_square_style_position_object(c, ci) {
-  log("message");
   let x2 = object_property_get(c, "x");
   let y2 = object_property_get(c, "y");
   g_img_square_style_position(ci, x2, y2);
