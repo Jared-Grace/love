52f6ac76b837d07218f640162198c281c5889a9e
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 23:18:26 -0500

    r app_g_button_style app_g_button_uncolored_style

--- public/src/app_g_button_back.mjs
+++ public/src/app_g_button_back.mjs
@@ -1,6 +1,6 @@
-import { app_g_button_style } from "../../../love/public/src/app_g_button_style.mjs";
+import { app_g_button_uncolored_style } from "../../../love/public/src/app_g_button_uncolored_style.mjs";
 import { app_karate_button_back } from "../../../karate_code/public/src/app_karate_button_back.mjs";
 export function app_g_button_back(overlay, lambda21) {
   let component2 = app_karate_button_back(overlay, lambda21);
-  app_g_button_style(component2);
+  app_g_button_uncolored_style(component2);
 }
