107f89027c98050251cc7c9d98ea307aef602971
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 20:10:59 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -1,5 +1,4 @@
-import { emoji_bow } from "../../../love/public/src/emoji_bow.mjs";
-import { app_karate_button_uncolored } from "../../../karate_code/public/src/app_karate_button_uncolored.mjs";
+import { app_g_menu_refresh } from "../../../love/public/src/app_g_menu_refresh.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { app_g_overlay } from "../../../love/public/src/app_g_overlay.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
@@ -212,25 +211,7 @@ export function app_g_main() {
           let distance2 = g_distance(player, clicked_coordinates);
           if (equal(distance2, 0)) {
             let overlay = app_g_overlay(div);
-            function lambda21() {
-              html_remove(overlay);
-            }
-            let component2 = app_karate_button_back(overlay, lambda21);
-            let text = emoji_pray() + " Pray";
-            function lambda7() {
-              html_clear(overlay);
-              function lambda23() {}
-              let button2 = app_karate_button_back(overlay, lambda23);
-              function lambda22() {}
-              let component3 = app_karate_button_uncolored(
-                overlay,
-                emoji_bow() +
-                  " Heavenly Father, please bless this next conversation, in Jesus' name, amen! " +
-                  emoji_pray(),
-                lambda22,
-              );
-            }
-            let button = app_karate_button_uncolored(overlay, text, lambda7);
+            app_g_menu_refresh(overlay);
             return;
           }
           function lambda17(npc) {
