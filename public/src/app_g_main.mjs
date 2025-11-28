82183acf1a87dc96826b4d21ccd3f673122a63e6
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 19:55:27 -0500

    data_file_update ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -240,7 +240,7 @@ export function app_g_main() {
           } else {
             coordinates_move_to = clicked_coordinates;
           }
-          log(message);
+          log({coordinates_move_to});
           object_assign(player, coordinates_move_to);
           let properties = ["left", "top"];
           function on_transition_begin() {
