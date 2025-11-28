9ccb6ad0f5592d6c7e438fe16e6accc1fb795774
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 00:01:25 -0500

    function_auto_path ..\love\public\src\list_filter_empty_not_is.mjs

--- web/public/src/list_filter_empty_not_is.mjs
+++ web/public/src/list_filter_empty_not_is.mjs
@@ -1,6 +1,6 @@
 import { string_empty_not_is } from "../../../love/public/src/string_empty_not_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_empty_not_is(item) {
-  let filtered2 = list_filter(item, string_empty_not_is);
-  return filtered2;
+  let filtered = list_filter(item, string_empty_not_is);
+  return filtered;
 }
