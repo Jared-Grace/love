906ed2f1a00b95bd2079f0562abc4134f9a7efce
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 00:07:29 -0500

    function_auto_path ..\love\public\src\bible_names_men.mjs

--- public/src/bible_names_men.mjs
+++ public/src/bible_names_men.mjs
@@ -1,8 +1,4 @@
-import { string_size_greater_than_1 } from "../../../love/public/src/string_size_greater_than_1.mjs";
-import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { list_map_first } from "../../../love/public/src/list_map_first.mjs";
-import { list_map_split_comma } from "../../../love/public/src/list_map_split_comma.mjs";
-import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
+import { bible_names_text_parse } from "../../../love/public/src/bible_names_text_parse.mjs";
 export function bible_names_men() {
   let text = `A
 Aaron
@@ -1797,9 +1793,6 @@ Zuph
 Zur
 Zuriel
 Zurishaddai`;
-  let split = string_split_newline(text);
-  let filtered2 = list_filter(split, string_size_greater_than_1);
-  let mapped3 = list_map_split_comma(filtered2);
-  let mapped6 = list_map_first(mapped3);
+  let mapped6 = bible_names_text_parse(text);
   return mapped6;
 }
---
+++ public/src/bible_names_text_parse.mjs
@@ -0,0 +1,12 @@
+import { list_map_first } from "../../../love/public/src/list_map_first.mjs";
+import { list_map_split_comma } from "../../../love/public/src/list_map_split_comma.mjs";
+import { string_size_greater_than_1 } from "../../../love/public/src/string_size_greater_than_1.mjs";
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
+export function bible_names_text_parse(text) {
+  let split = string_split_newline(text);
+  let filtered2 = list_filter(split, string_size_greater_than_1);
+  let mapped3 = list_map_split_comma(filtered2);
+  let mapped6 = list_map_first(mapped3);
+  return mapped6;
+}
