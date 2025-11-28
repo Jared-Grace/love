7b7f2a8b3bb0c2f61d43d0ae9ac3553eb6151fc6
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 00:08:36 -0500

    function_auto_path ..\love\public\src\bible_names_text_parse.mjs

--- public/src/bible_names_text_parse.mjs
+++ public/src/bible_names_text_parse.mjs
@@ -1,3 +1,5 @@
+import { log_unparse } from "../../../love/public/src/log_unparse.mjs";
+import { list_map_split_space } from "../../../love/public/src/list_map_split_space.mjs";
 import { list_map_first } from "../../../love/public/src/list_map_first.mjs";
 import { list_map_split_comma } from "../../../love/public/src/list_map_split_comma.mjs";
 import { string_size_greater_than_1 } from "../../../love/public/src/string_size_greater_than_1.mjs";
@@ -8,5 +10,8 @@ export function bible_names_text_parse(text) {
   let filtered2 = list_filter(split, string_size_greater_than_1);
   let mapped3 = list_map_split_comma(filtered2);
   let mapped6 = list_map_first(mapped3);
-  return mapped6;
+  let mapped = list_map_split_space(mapped6);
+  let mapped2 = list_map_first(mapped);
+  log_unparse(node);
+  return mapped2;
 }
