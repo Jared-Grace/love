f645590aefca5c9164319a6ef8276a78c97b8c0f
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 00:00:31 -0500

    function_auto_path ..\love\public\src\bible_names_men.mjs

--- web/public/src/bible_names_men.mjs
+++ web/public/src/bible_names_men.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_map_split_comma } from "../../../love/public/src/list_map_split_comma.mjs";
 import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
 export function bible_names_men() {
@@ -1795,5 +1796,6 @@ Zur
 Zuriel
 Zurishaddai`;
   let split = string_split_newline(text);
-  let mapped3 = list_map_split_comma(mapped2);
+  let mapped3 = list_map_split_comma(split);
+  let mapped = list_map(list, function lambda(item) {});
 }
