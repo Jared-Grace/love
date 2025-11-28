41d1d527257378d2b7a93c66a09b4493f2635738
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 19:33:45 -0500

    function_auto_path ..\love\public\src\g_img_square_style.mjs

--- public/src/g_img_square_style.mjs
+++ public/src/g_img_square_style.mjs
@@ -3,7 +3,7 @@ import { g_rows_count } from "../../../love/public/src/g_rows_count.mjs";
 import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_size_square } from "../../../love/public/src/html_style_size_square.mjs";
-export function g_img_square_style(tile, x, y, z) {
+export function g_img_square_style(tile, { x, y }, z) {
   let rows = g_rows_count();
   const square_size = "calc(100vh / " + rows + ")";
   html_style_size_square(tile, square_size);
