0b286c571c5c70c64e940b1455b938501c67b7ef
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 20:17:30 -0500

    data_file_update ..\love\public\src\app_g_player_get.mjs

--- web/public/src/app_g_player_get.mjs
+++ web/public/src/app_g_player_get.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 export function app_g_player_get() {
+  marker("1");
   let game = storage_local_get(app_g, "game");
   let player2 = object_property_get(game, "player");
   return player2;
