428e93a5f97cae74918869a91986393fd579d5bb
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 20:09:11 -0500

    function_auto_path ..\love\public\src\prayer_start.mjs

--- web/public/src/prayer_start.mjs
+++ web/public/src/prayer_start.mjs
@@ -1,7 +1,8 @@
 import { emoji_trinity } from "../../../love/public/src/emoji_trinity.mjs";
 export function prayer_start() {
   let v =
-    "🙇 In the name of the Father, and of the Son, and of the Holy Spirit" +
+    "🙇" +
+    " In the name of the Father, and of the Son, and of the Holy Spirit" +
     " " +
     emoji_trinity() +
     " : ";
