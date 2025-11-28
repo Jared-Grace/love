eff01d7855c23f9a73797549ad30651b76df34f7
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 19:33:37 -0500

    function_auto_path ..\love\public\src\g_img_square.mjs

--- public/src/g_img_square.mjs
+++ public/src/g_img_square.mjs
@@ -2,6 +2,14 @@ import { g_img_square_style } from "../../../love/public/src/g_img_square_style.
 import { html_img } from "../../../love/public/src/html_img.mjs";
 export function g_img_square(parent, src, square_size, x, y, z) {
   let tile = html_img(parent, src);
-  g_img_square_style(tile, square_size, x, y, z);
+  g_img_square_style(
+    tile,
+    square_size,
+    {
+      x,
+      y,
+    },
+    z,
+  );
   return tile;
 }
