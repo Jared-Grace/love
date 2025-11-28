a54d20a6623de8809113b2986b3397805b107c69
Grace-Jared <christrosetolife@gmail.com>
2025-11-27 23:19:46 -0500

    function_auto_path ..\love\public\src\app_g_button_green_style.mjs

--- public/src/app_g_button_green_style.mjs
+++ public/src/app_g_button_green_style.mjs
@@ -1,9 +1,9 @@
+import { app_karate_button_background } from "../../../karate_code/public/src/app_karate_button_background.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { app_karate_button_uncolored_background_color } from "../../../karate_code/public/src/app_karate_button_uncolored_background_color.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_g_button_green_style(component2) {
   marker("1");
   html_style_assign(component2, {
-    "background-color": app_karate_button_uncolored_background_color() + "dd",
+    "background-color": app_karate_button_background() + "dd",
   });
 }
