da35bf68bb06b22b9ebd31bedd94b9aa4ab16493
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 22:49:43 -0500

    data_file_update ..\love\public\src\app_g_refresh_click_overlay.mjs

--- public/src/app_g_refresh_click_overlay.mjs
+++ public/src/app_g_refresh_click_overlay.mjs
@@ -26,7 +26,7 @@ export function app_g_refresh_click_overlay(
 ) {
   marker("1");
   let overlay = app_g_overlay(body);
-  log(message);
+  log(body);
   let prayer2 = object_property_get(player, "prayer");
   let conversation2 = object_property_get(prayer2, "conversation");
   if (conversation2) {
