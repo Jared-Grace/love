675bab4e993310d2f0e9409b33339853ef3011a7
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 14:41:30 -0500

    function_auto_path ..\love\public\src\function_params_new.mjs

--- public/src/function_params_new.mjs
+++ public/src/function_params_new.mjs
@@ -1,18 +1,10 @@
+import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
 import { list_intersect_empty_is_assert } from "../../../love/public/src/list_intersect_empty_is_assert.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-import { js_visit_calls_named } from "../../../love/public/src/js_visit_calls_named.mjs";
-import { object_properties } from "../../../love/public/src/object_properties.mjs";
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
-import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
-import { function_transform_current } from "../../../love/public/src/function_transform_current.mjs";
 import { js_declaration_single_params_add } from "../../../love/public/src/js_declaration_single_params_add.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
 export async function function_params_new(
@@ -22,24 +14,7 @@ export async function function_params_new(
   assert_arguments(arguments, 2);
   let param_names = string_split(param_names_comma, ",");
   let values_default = string_split(values_default_comma, ",");
-  await function_transform_current(function_transform_current_lambda);
-  let f_name_current = await function_current_get();
-  let result = await data_identifiers_search(f_name_current);
-  let properties = object_properties(result);
-  async function lambda4(f_name) {
-    let { exists } = await function_exists(f_name);
-    if (not(exists)) {
-      return;
-    }
-    async function lambda5(ast) {
-      js_visit_calls_named(f_name_current, lambda, ast);
-      function lambda({ args }) {
-        on_call(args);
-      }
-    }
-    let output = await function_transform(f_name, lambda5);
-  }
-  await each_async(properties, lambda4);
+  await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
     function lambda3(value_default) {
       let expression2 = js_parse_expression(value_default);
