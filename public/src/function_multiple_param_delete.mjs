edf73dbd28c9986bdbc6fa5d02da96e56fe7593c
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:02:12 -0500

    function_auto_path ..\love\public\src\function_multiple_param_delete.mjs

--- public/src/function_multiple_param_delete.mjs
+++ public/src/function_multiple_param_delete.mjs
@@ -1,19 +1,9 @@
-import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
+import { function_multiple_param_delete_generic } from "../../../love/public/src/function_multiple_param_delete_generic.mjs";
 import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_param_delete(
   f_names_comma,
   param_name,
 ) {
   let fn = function_param_delete;
-  let f_names = string_split_comma(f_names_comma);
-  marker("1");
-  async function lambda(f_name) {
-    let v2 = await function_current_set(f_name);
-    let v = await function_param_delete(param_name);
-  }
-  await each_async(f_names, lambda);
-  return v;
+  await function_multiple_param_delete_generic(f_names_comma, param_name);
 }
