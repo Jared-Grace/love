c2b44e7fbf24353f931103b2a9e91671aa679cba
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 22:03:26 -0500

    function_auto_path ..\love\public\src\html_data_set.mjs

--- public/src/html_data_set.mjs
+++ public/src/html_data_set.mjs
@@ -1,9 +1,9 @@
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_data_set(tile, tile_class) {
+export function html_data_set(component, property_name, value) {
   marker("1");
-  let element = html_component_element_get(tile);
+  let element = html_component_element_get(component);
   element.dataset;
-  object_property_set(object, property_name, value);
+  object_property_set(element.dataset, property_name, value);
 }
