a6444683febaf98b504e9c8fe284c3aad1967e6e
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 21:58:35 -0500

    function_auto_path ..\love\public\src\app_g_refresh.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -96,127 +96,119 @@ export function app_g_refresh(
       const src = tiles_path + r + ".png";
       let tile = html_img(div, src);
       g_img_square_style(tile);
-      let clickable = html_div(div);
-      const clicked_coordinates = {
-        x,
-        y,
-      };
-      g_img_square_style_position(clickable, clicked_coordinates, "click");
-      async function on_tile_click() {
-        let tutorial = global_function_property_get(app_g_refresh, "tutorial");
-        let player2 = app_g_player_get();
-        if (equal_not(tutorial, null)) {
-          html_remove(tutorial);
-        }
-        let distance2 = g_distance(player2, clicked_coordinates);
-        log({
-          distance2,
-        });
-        if (equal(distance2, 0)) {
-          let overlay = app_g_overlay(div);
-          app_g_menu_refresh(overlay);
-          return;
-        }
-        function lambda17(npc) {
-          let e = object_includes(npc, clicked_coordinates);
-          return e;
-        }
-        let npcs_matched = list_filter(npcs, lambda17);
-        let e2 = list_empty_not_is(npcs_matched);
-        let coordinates_move_to = null;
-        if (e2) {
-          function lambda18(item) {
-            let distance = g_distance(clicked_coordinates, item);
-            let v2 = distance === 1;
-            return v2;
-          }
-          let filtered3 = list_filter(coordinates, lambda18);
-          list_shuffle(filtered3);
-          function lambda19(item3) {
-            let distance = g_distance(player2, item3);
-            return distance;
-          }
-          list_sort_number_mapper(filtered3, lambda19);
-          coordinates_move_to = list_first(filtered3);
-        } else {
-          coordinates_move_to = clicked_coordinates;
-        }
-        let distance = g_distance(player2, coordinates_move_to);
-        object_assign(player2, coordinates_move_to);
-        const away = distance >= 1;
-        if (away) {
-          let properties = ["left", "top"];
-          function on_transition_begin() {
-            g_img_square_style_position_object(player2, player_img_c);
-          }
-          await html_on_transitionend(
-            properties,
-            player_img_c,
-            on_transition_begin,
-          );
-        }
-        let element2 = html_component_element_get(player_img_c);
-        element2.scrollIntoView({
-          behavior: "smooth",
-          block: "center",
-          inline: "center",
-        });
-        if (e2) {
-          let overlay = app_g_overlay(div);
-          let prayer2 = object_property_get(player2, "prayer");
-          let conversation2 = object_property_get(prayer2, "conversation");
-          if (conversation2) {
-            let s = list_single(npcs_matched);
-            let name = object_property_get(s, "name");
-            let container = app_karate_container_centered(overlay);
-            html_style_assign(container, {
-              "background-color":
-                app_karate_container_background_color() + "bc",
-              padding: "0",
-            });
-          } else {
-            let container = app_karate_container_centered(overlay);
-            html_style_assign(container, {
-              "background-color":
-                app_karate_container_background_color() + "bc",
-              padding: "0",
-            });
-            html_p_text(
-              container,
-              emoji_pray() +
-                " You remember that you have not prayed, yet, before your next conversation!",
-            );
-            html_p_text(
-              container,
-              " To pray, tap or click on yourself (You glow with white)",
-            );
-            app_karate_style_control(container);
-            function lambda21() {
-              html_remove(overlay);
-              tutorial = html_div(div);
-              global_function_property_set(app_g_refresh, "tutorial", tutorial);
-              g_img_square_style_position(tutorial, player2, "tutorial");
-              let text = emoji_pray();
-              html_text_set(tutorial, text);
-              let rows = g_rows_count();
-              const square_size = "calc(100vh / " + rows + " * .7)";
-              html_style_assign(tutorial, {
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                textAlign: "center",
-                fontSize: square_size,
-                animation: "upDown 1.25s infinite ease-in-out alternate",
-              });
-            }
-            app_g_button_back(overlay, lambda21);
-          }
-        }
-        app_g_player_save(player2);
-      }
-      html_on_click(clickable, on_tile_click);
     }
     each_index(columns, lambda);
   }
   each_index(rows, lambda2);
+  async function on_tile_click() {
+    let tutorial = global_function_property_get(app_g_refresh, "tutorial");
+    let player2 = app_g_player_get();
+    if (equal_not(tutorial, null)) {
+      html_remove(tutorial);
+    }
+    let distance2 = g_distance(player2, clicked_coordinates);
+    log({
+      distance2,
+    });
+    if (equal(distance2, 0)) {
+      let overlay = app_g_overlay(div);
+      app_g_menu_refresh(overlay);
+      return;
+    }
+    function lambda17(npc) {
+      let e = object_includes(npc, clicked_coordinates);
+      return e;
+    }
+    let npcs_matched = list_filter(npcs, lambda17);
+    let e2 = list_empty_not_is(npcs_matched);
+    let coordinates_move_to = null;
+    if (e2) {
+      function lambda18(item) {
+        let distance = g_distance(clicked_coordinates, item);
+        let v2 = distance === 1;
+        return v2;
+      }
+      let filtered3 = list_filter(coordinates, lambda18);
+      list_shuffle(filtered3);
+      function lambda19(item3) {
+        let distance = g_distance(player2, item3);
+        return distance;
+      }
+      list_sort_number_mapper(filtered3, lambda19);
+      coordinates_move_to = list_first(filtered3);
+    } else {
+      coordinates_move_to = clicked_coordinates;
+    }
+    let distance = g_distance(player2, coordinates_move_to);
+    object_assign(player2, coordinates_move_to);
+    const away = distance >= 1;
+    if (away) {
+      let properties = ["left", "top"];
+      function on_transition_begin() {
+        g_img_square_style_position_object(player2, player_img_c);
+      }
+      await html_on_transitionend(
+        properties,
+        player_img_c,
+        on_transition_begin,
+      );
+    }
+    let element2 = html_component_element_get(player_img_c);
+    element2.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+    if (e2) {
+      let overlay = app_g_overlay(div);
+      let prayer2 = object_property_get(player2, "prayer");
+      let conversation2 = object_property_get(prayer2, "conversation");
+      if (conversation2) {
+        let s = list_single(npcs_matched);
+        let name = object_property_get(s, "name");
+        let container = app_karate_container_centered(overlay);
+        html_style_assign(container, {
+          "background-color": app_karate_container_background_color() + "bc",
+          padding: "0",
+        });
+      } else {
+        let container = app_karate_container_centered(overlay);
+        html_style_assign(container, {
+          "background-color": app_karate_container_background_color() + "bc",
+          padding: "0",
+        });
+        html_p_text(
+          container,
+          emoji_pray() +
+            " You remember that you have not prayed, yet, before your next conversation!",
+        );
+        html_p_text(
+          container,
+          " To pray, tap or click on yourself (You glow with white)",
+        );
+        app_karate_style_control(container);
+        function lambda21() {
+          html_remove(overlay);
+          tutorial = html_div(div);
+          global_function_property_set(app_g_refresh, "tutorial", tutorial);
+          g_img_square_style_position(tutorial, player2, "tutorial");
+          let text = emoji_pray();
+          html_text_set(tutorial, text);
+          let rows = g_rows_count();
+          const square_size = "calc(100vh / " + rows + " * .7)";
+          html_style_assign(tutorial, {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            fontSize: square_size,
+            animation: "upDown 1.25s infinite ease-in-out alternate",
+          });
+        }
+        app_g_button_back(overlay, lambda21);
+      }
+    }
+    app_g_player_save(player2);
+  }
+  html_on_click(clickable, on_tile_click);
 }
