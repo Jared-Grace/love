25f49dc6b85293b5012deffb026b0129df289d25
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:02:12 -0500

    function_auto_path ..\love\public\src\function_multiple_param_delete.mjs

---
+++ web/public/src/function_multiple_param_delete_generic.mjs
@@ -0,0 +1,17 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
+import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+export async function function_multiple_param_delete_generic(
+  f_names_comma,
+  param_name,
+) {
+  let f_names = string_split_comma(f_names_comma);
+  marker("1");
+  async function lambda(f_name) {
+    let v2 = await function_current_set(f_name);
+    let v = await function_param_delete(param_name);
+  }
+  await each_async(f_names, lambda);
+}
