65862963ae409d7496c88a6dc1d45a67970503c6
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 00:56:44 -0500

    function_auto_path ..\love\public\src\app_g_player_save.mjs

--- public/src/app_g_player_save.mjs
+++ public/src/app_g_player_save.mjs
@@ -1,6 +1,7 @@
 import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 export function app_g_player_save(player) {
+  let g = storage_local_set(app_g, "game");
   storage_local_set(app_g, "game", {
     player,
   });
