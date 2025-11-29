e1a5672f7170a04d9afe9441b7d32e4979dd0faf
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 17:59:22 -0500

    fw fpd function_multiple_param_delete

--- web/public/src/function_multiple_param_delete.mjs
+++ web/public/src/function_multiple_param_delete.mjs
@@ -1,4 +1,6 @@
+import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function function_multiple_param_delete() {
+export async function function_multiple_param_delete(param_name) {
   marker("1");
+  return await function_param_delete(param_name);
 }
