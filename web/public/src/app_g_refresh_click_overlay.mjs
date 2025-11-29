84d4f3b72a35d15b2b86aa699035afa48664b6fe
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 00:20:13 -0500

    function_auto_path ..\love\public\src\app_g_refresh_click_overlay.mjs

--- web/public/src/app_g_refresh_click_overlay.mjs
+++ web/public/src/app_g_refresh_click_overlay.mjs
@@ -39,7 +39,7 @@ export function app_g_refresh_click_overlay(
     });
     let name2 = object_property_get(player, "name");
     const greet = list_random_item(["hi", "hello", "greetings", "hey"]);
-    let v = string_first_upper_to(s2);
+    let v = string_first_upper_to(greet);
     app_g_p_text(
       container,
       name + " says: " + v + " " + name2 + list_random_item([".", "!"]),
