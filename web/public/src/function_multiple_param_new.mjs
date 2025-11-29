62a96620bd7b06958e5d151191e6df8d1f933ae8
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:03:07 -0500

    function_auto_path ..\love\public\src\function_multiple_param_new.mjs

--- web/public/src/function_multiple_param_new.mjs
+++ web/public/src/function_multiple_param_new.mjs
@@ -1,7 +1,8 @@
+import { function_multiple_param_delete_generic } from "../../../love/public/src/function_multiple_param_delete_generic.mjs";
 import { function_param_new } from "../../../love/public/src/function_param_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_param_new(param_name, default_value) {
   marker("1");
-  let v = await function_param_new(param_name, default_value);
-  return v;
+  let fn = function_param_new;
+  await function_multiple_param_delete_generic(f_names_comma, param_name, fn);
 }
