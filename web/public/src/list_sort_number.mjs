32915f6963241d6e3be0f2567152938420a46ad1
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 16:59:09 -0500

    function_auto_path ..\love\public\src\list_sort_number.mjs

--- web/public/src/list_sort_number.mjs
+++ web/public/src/list_sort_number.mjs
@@ -1,5 +1,5 @@
 import { identity } from "../../../love/public/src/identity.mjs";
 import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
-export function list_sort_number(m) {
-  list_sort_number_mapper(m, identity);
+export function list_sort_number(list) {
+  list_sort_number_mapper(list, identity);
 }
