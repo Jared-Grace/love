0a9a1fd71a5378f270d907377ca9b4007df4b81b
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 22:03:13 -0500

    function_auto_path ..\love\public\src\html_data_set.mjs

--- web/public/src/html_data_set.mjs
+++ web/public/src/html_data_set.mjs
@@ -1,8 +1,9 @@
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { add } from "../../../love/public/src/add.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_data_set(tile, tile_class) {
   marker("1");
   let element = html_component_element_get(tile);
-  element.classList.add(tile_class);
+  element.dataset;
+  object_property_set(object, property_name, value);
 }
