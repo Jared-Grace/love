02d95cc5dbd97dd27a50062a34c2f1550eaccacc
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 21:56:00 -0500

    rs g_img_square_style position

--- public/src/g_img_square.mjs
+++ public/src/g_img_square.mjs
@@ -1,8 +1,8 @@
-import { g_img_square_style } from "../../../love/public/src/g_img_square_style.mjs";
+import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
 import { html_img } from "../../../love/public/src/html_img.mjs";
 export function g_img_square(parent, src, x, y, z) {
   let tile = html_img(parent, src);
-  g_img_square_style(
+  g_img_square_style_position(
     tile,
     {
       x,
