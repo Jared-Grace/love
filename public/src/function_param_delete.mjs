4de90ccf9cfcddbe6fed02cbff6e7af39360bea8
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 16:28:07 -0500

    function_auto_path ..\love\public\src\function_param_delete.mjs

--- public/src/function_param_delete.mjs
+++ public/src/function_param_delete.mjs
@@ -6,20 +6,13 @@ import { js_declaration_single } from "../../../love/public/src/js_declaration_s
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 export async function function_param_delete(param_name) {
   marker("1");
   assert_arguments(arguments, 1);
   let index = null;
   await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
-    function lambda3(value_default) {
-      let expression2 = js_parse_expression(value_default);
-      list_add(args, expression2);
-    }
-    each(values_default, lambda3);
+    list_remove_at(args, index);
   }
   function function_transform_current_lambda(ast) {
     let declaration = js_declaration_single(ast);
