71ec21d81706b69569e7a5535301aaa2dbaf9e72
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:08:44 -0500

    function_auto_path ..\love\public\src\function_param_swap.mjs

--- public/src/function_param_swap.mjs
+++ public/src/function_param_swap.mjs
@@ -1,9 +1,9 @@
+import { function_transform_current } from "../../../love/public/src/function_transform_current.mjs";
 import { list_swap } from "../../../love/public/src/list_swap.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
-export async function function_param_swap(f_name, param_name_a, param_name_b) {
-  await function_transform(f_name, lambda);
+export async function function_param_swap(param_name_a, param_name_b) {
+  await function_transform_current(lambda);
   function lambda(ast) {
     let declaration = js_declaration_single(ast);
     let a = js_declaration_param_named(declaration, param_name_a);
