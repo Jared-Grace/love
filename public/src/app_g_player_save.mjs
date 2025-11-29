42aa63be54d645eb5f5bc13f540111bdb36c4c02
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 00:58:00 -0500

    function_auto_path ..\love\public\src\app_g_player_save.mjs

--- public/src/app_g_player_save.mjs
+++ public/src/app_g_player_save.mjs
@@ -1,8 +1,9 @@
+import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 export function app_g_player_save(player) {
-  let g = storage_local_set(app_g, "game");
+  let g = storage_local_get(app_g, "game");
   let a = object_assign(g, {
     player,
   });
