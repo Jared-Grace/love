30d3dcb0c31c6e2c738171697d970a93d24620f3
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 17:02:33 -0500

    function_auto_path ..\love\public\src\list_remove_at_multiple_reverse.mjs

--- web/public/src/list_remove_at_multiple_reverse.mjs
+++ web/public/src/list_remove_at_multiple_reverse.mjs
@@ -1,6 +1,6 @@
 import { list_remove_at_multiple } from "../../../love/public/src/list_remove_at_multiple.mjs";
 import { list_copy_reverse } from "../../../love/public/src/list_copy_reverse.mjs";
-export function list_remove_at_multiple_reverse(m) {
-  let c = list_copy_reverse(m);
-  list_remove_at_multiple(c);
+export function list_remove_at_multiple_reverse(list) {
+  let removals = list_copy_reverse(list);
+  list_remove_at_multiple(list, removals);
 }
