c96b288f2a1b06d64115faa0f142e8f47c62716f
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:05:39 -0500

    pnd f_names_comma

--- web/public/src/function_multiple_param_new.mjs
+++ web/public/src/function_multiple_param_new.mjs
@@ -1,7 +1,11 @@
 import { function_multiple_param_delete_generic } from "../../../love/public/src/function_multiple_param_delete_generic.mjs";
 import { function_param_new } from "../../../love/public/src/function_param_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_multiple_param_new(param_name, default_value) {
+export async function function_multiple_param_new(
+  param_name,
+  default_value,
+  f_names_comma,
+) {
   marker("1");
   let fn = async function lambda(param_name) {
     let v = await function_param_new(param_name, default_value);
