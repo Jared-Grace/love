c2fc2b5157c8020f973b93246b6380be2f88a54a
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 17:07:10 -0500

    function_auto_path ..\love\public\src\js_declaration_param_named.mjs

--- public/src/js_declaration_param_named.mjs
+++ public/src/js_declaration_param_named.mjs
@@ -1,11 +1,6 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 export function js_declaration_param_named(declaration, param_name) {
   let { params } = declaration;
-  log({
-    param_name,
-    params,
-  });
   let result = list_find_property(params, "name", param_name);
   return result;
 }
