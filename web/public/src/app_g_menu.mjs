5b5149d0e181587b5f141e7f9cf9005cb980e950
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:18:12 -0500

    function_auto_path ..\love\public\src\app_g_menu.mjs

--- web/public/src/app_g_menu.mjs
+++ web/public/src/app_g_menu.mjs
@@ -5,7 +5,6 @@ import { app_g_button_back } from "../../../love/public/src/app_g_button_back.mj
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
-import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { emoji_bow } from "../../../love/public/src/emoji_bow.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
@@ -25,7 +24,6 @@ export function app_g_menu(overlay) {
     }
     let button2 = app_g_button_back(overlay, lambda23);
     function lambda22() {
-      let player = app_g_player_get();
       let prayer = object_property_get(player, "prayer");
       object_property_set(prayer, "conversation", true);
       app_g_player_save(player);
