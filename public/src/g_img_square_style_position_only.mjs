1f2697b9f5456525d1a2488737628d2ae23865e3
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 21:55:39 -0500

    data_file_update ..\love\public\src\g_img_square_style_position_only.mjs

--- public/src/g_img_square_style_position_only.mjs
+++ public/src/g_img_square_style_position_only.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { g_rows_count } from "../../../love/public/src/g_rows_count.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function g_img_square_style_position_only(tile, x, y) {
+  marker("1");
   let rows = g_rows_count();
   html_style_assign(tile, {
     left: "calc(" + x + " * (100vh / " + rows + "))",
