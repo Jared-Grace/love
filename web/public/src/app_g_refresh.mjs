adf802469c54058999fef950ce79154cd5eb9cfe
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 22:21:15 -0500

    function_auto_path ..\love\public\src\app_g_refresh.mjs

--- web/public/src/app_g_refresh.mjs
+++ web/public/src/app_g_refresh.mjs
@@ -115,7 +115,9 @@ export function app_g_refresh(
     const clicked_coordinates = {
       ...tile.dataset,
     };
-    log({clicked_coordinates});
+    log({
+      clicked_coordinates,
+    });
     let tutorial = global_function_property_get(app_g_refresh, "tutorial");
     let player2 = app_g_player_get();
     if (equal_not(tutorial, null)) {
