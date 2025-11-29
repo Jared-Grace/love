d788c17783378865083dd0153e8959ad6ff073e9
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:15:47 -0500

    function_auto_path ..\love\public\src\function_multiple_param_new_double.mjs

--- web/public/src/function_multiple_param_new_double.mjs
+++ web/public/src/function_multiple_param_new_double.mjs
@@ -1,6 +1,9 @@
 import { function_multiple_param_new } from "../../../love/public/src/function_multiple_param_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_multiple_param_new_double(param_name) {
+export async function function_multiple_param_new_double(
+  f_names_comma,
+  param_name,
+) {
   marker("1");
   let v = await function_multiple_param_new(
     f_names_comma,
