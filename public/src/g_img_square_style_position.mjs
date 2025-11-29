891ec934057c576aeb9143769ecd0c6e92416edf
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 21:56:00 -0500

    data_file_update ..\love\public\src\g_img_square_style_position.mjs

--- public/src/g_img_square_style_position.mjs
+++ public/src/g_img_square_style_position.mjs
@@ -1,9 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { g_z } from "../../../love/public/src/g_z.mjs";
 import { g_rows_count } from "../../../love/public/src/g_rows_count.mjs";
 import { g_img_square_style_position_only } from "../../../love/public/src/g_img_square_style_position_only.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_size_square } from "../../../love/public/src/html_style_size_square.mjs";
 export function g_img_square_style_position(tile, { x, y }, z) {
+  marker("1");
   let rows = g_rows_count();
   const square_size = "calc(100vh / " + rows + ")";
   html_style_size_square(tile, square_size);
