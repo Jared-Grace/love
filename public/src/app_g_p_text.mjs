951cb6dc8963cbfdae612ed012580a3824d1a727
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 23:37:24 -0500

    data_file_update ..\love\public\src\app_g_p_text.mjs

--- public/src/app_g_p_text.mjs
+++ public/src/app_g_p_text.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 export function app_g_p_text(container, name) {
+  marker("1");
   let p = html_p_text(container, name);
   html_style_assign(p, {
     marginTop: "0.5em",
