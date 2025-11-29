5201b1ffaa09fb693f647454303cf6122969593b
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 22:28:10 -0500

    function_auto_path ..\love\public\src\html_data_set_json.mjs

--- public/src/html_data_set_json.mjs
+++ public/src/html_data_set_json.mjs
@@ -1,6 +1,6 @@
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
-export function html_data_set_json(coordinates, tile) {
-  let json = json_to(coordinates);
-  html_data_set(tile, json);
+export function html_data_set_json(component, property_name, object) {
+  let json = json_to(object);
+  html_data_set(component, property_name, json);
 }
