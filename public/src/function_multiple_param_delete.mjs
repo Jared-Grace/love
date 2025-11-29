fabbe7868e461963acce222ce68920c98ca5533a
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:00:07 -0500

    function_auto_path ..\love\public\src\function_multiple_param_delete.mjs

--- public/src/function_multiple_param_delete.mjs
+++ public/src/function_multiple_param_delete.mjs
@@ -1,3 +1,4 @@
+import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,6 +6,7 @@ export async function function_multiple_param_delete(
   f_names_comma,
   param_name,
 ) {
+  let split = string_split_comma(f_names);
   marker("1");
   async function lambda(item) {}
   await each_async(list, lambda);
