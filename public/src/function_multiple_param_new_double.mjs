28d432a9cdc67682cc41ddf1e789b2395c6a1bd9
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 18:04:37 -0500

    data_file_update ..\love\public\src\function_multiple_param_new_double.mjs

--- public/src/function_multiple_param_new_double.mjs
+++ public/src/function_multiple_param_new_double.mjs
@@ -1,1 +1,4 @@
-export function function_multiple_param_new_double() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function function_multiple_param_new_double() {
+  marker("1");
+}
