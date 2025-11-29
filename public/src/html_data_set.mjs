9bf5ac6b9047524f50ad70f4af8ed93db23c90aa
Grace-Jared <christrosetolife@gmail.com>
2025-11-28 22:03:02 -0500

    c html_class_add html_data_set

--- public/src/html_data_set.mjs
+++ public/src/html_data_set.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { add } from "../../../love/public/src/add.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_data_set(tile, tile_class) {
+  marker("1");
   let element = html_component_element_get(tile);
   element.classList.add(tile_class);
 }
