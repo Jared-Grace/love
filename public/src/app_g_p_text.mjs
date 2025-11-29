6b2f4403d5c9c210b4006997f6726b4c409febf7
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 00:16:39 -0500

    function_auto_path ..\love\public\src\app_g_p_text.mjs

--- public/src/app_g_p_text.mjs
+++ public/src/app_g_p_text.mjs
@@ -2,6 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 export function app_g_p_text(container, name) {
+  $a;
   marker("1");
   let p = html_p_text(container, name);
   html_style_assign(p, {
