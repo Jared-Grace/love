0ff9496d12aa25cd80b3fa66ff54e6400bc791aa
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 17:59:21 -0500

    data_file_update ..\love\public\src\function_multiple_param_delete.mjs

--- web/public/src/function_multiple_param_delete.mjs
+++ web/public/src/function_multiple_param_delete.mjs
@@ -1,1 +1,4 @@
-export function function_multiple_param_delete() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function function_multiple_param_delete() {
+  marker("1");
+}
