71b38e1aebd9558fedc62ef3438e5b28c1441c09
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 17:54:30 -0500

    function_auto_path ..\love\public\src\html_scroll_center_generic.mjs

---
+++ web/public/src/html_scroll_center_generic_wait.mjs
@@ -0,0 +1,24 @@
+import { not } from "../../../love/public/src/not.mjs";
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+export async function html_scroll_center_generic_wait(player_img_c) {
+  let el = html_component_element_get(player_img_c);
+  if (el.tagName === "IMG" && not(el.complete)) {
+    await new Promise(function lambda(r) {
+      let v = (el.onload = r);
+      return v;
+    });
+  }
+  await new Promise(function lambda4(r) {
+    function lambda3() {
+      function lambda2() {
+        let v2 = requestAnimationFrame(r);
+        return v2;
+      }
+      let v3 = requestAnimationFrame(lambda2);
+      return v3;
+    }
+    let v4 = requestAnimationFrame(lambda3);
+    return v4;
+  });
+  return el;
+}
