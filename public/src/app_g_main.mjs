f874b419e203cbb141c9e7a0178966a49b672b34
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:08:40 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -45,7 +45,8 @@ export function app_g_main() {
   let path_prefix = "";
   let v = localhost_is();
   let s = location_pathname(sw);
-  let sw = string_starts_with(s, "/", "latest", "/");
+  const path_part = "latest";
+  let sw = string_starts_with(s, "/", path_part, "/");
   if (v || sw) {
     path_prefix = "..\\";
   }
