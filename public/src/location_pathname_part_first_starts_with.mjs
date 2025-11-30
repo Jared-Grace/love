f68d4b9a827e4adc24df6f30c32d589635896d8c
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:13:41 -0500

    function_auto_path ..\love\public\src\location_pathname_part_first_starts_with.mjs

--- public/src/location_pathname_part_first_starts_with.mjs
+++ public/src/location_pathname_part_first_starts_with.mjs
@@ -2,6 +2,6 @@ import { string_starts_with } from "../../../love/public/src/string_starts_with.
 import { location_pathname } from "../../../love/public/src/location_pathname.mjs";
 export function location_pathname_part_first_starts_with(path_part) {
   let s = location_pathname(sw);
-  let sw = string_starts_with(s, "/", path_part, "/");
+  let sw = string_starts_with(s, "/" + path_part + "/");
   return sw;
 }
