d6687c39bb96312f10c861a96faf264ddf073b8f
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 21:00:28 -0500

    data_file_update ..\love\public\src\app_g_click.mjs

--- public/src/html_scroll_center.mjs
+++ public/src/html_scroll_center.mjs
@@ -1,4 +1,5 @@
-export function html_scroll_center(element2) {
+export function html_scroll_center(player_img_c) {
+    let element2 = html_component_element_get(player_img_c);
   element2.scrollIntoView({
     behavior: "smooth",
     block: "center",
