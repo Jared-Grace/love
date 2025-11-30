45954dea197e4e99beeed11a4c71d30d7eb36368
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 01:16:34 -0500

    function_auto_path ..\love\public\src\app_g_refresh.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -90,6 +90,7 @@ export async function app_g_refresh(
     await app_g_click(e, tile_class, div, player_img_c, body, map);
   }
   function lambda3() {
-    html_scroll_center_now(player_img_c);}
+    html_scroll_center_now(player_img_c);
+  }
   html_on_load(lambda3);
 }
