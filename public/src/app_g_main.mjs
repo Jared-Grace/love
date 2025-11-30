91a765b98f7ba397a2aeb6bfef75b41882e5b72c
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 01:24:02 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -71,7 +71,7 @@ export async function app_g_main() {
     position: "relative",
     display: "grid",
   });
-  let row_count = 20;
+  let row_count = 6;
   let column_count = row_count;
   function lambda10(la2) {
     function lambda6(i) {
