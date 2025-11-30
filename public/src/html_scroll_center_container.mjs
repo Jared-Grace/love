2832115c5f2e26e7becb2ea4b065326824075848
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 18:06:33 -0500

    function_auto_path ..\love\public\src\html_scroll_center_container.mjs

---
+++ public/src/html_scroll_center_container.mjs
@@ -0,0 +1,5 @@
+import { html_scroll_center_generic } from "../../../love/public/src/html_scroll_center_generic.mjs";
+export async function html_scroll_center_container(player_img_c) {
+  const behavior = "smooth";
+  await html_scroll_center_generic(player_img_c, behavior);
+}
