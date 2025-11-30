2fcfdf0fe9cb4032189aa303d900724a047e1da8
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 10:33:44 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- public/src/app_g_conversation.mjs
+++ public/src/app_g_conversation.mjs
@@ -27,8 +27,8 @@ export function app_g_conversation(prayer, npcs_matched, overlay, player) {
   let gender = object_property_get(npc, "gender");
   let container = app_g_container(overlay);
   let map = {
-    [g_gender_male()]: "#ff80ea",
-    [g_gender_female()]: "#80a0ff",
+    [g_gender_female()]: "#ff80ea",
+    [g_gender_male()]: "#80a0ff",
   };
   html_style_assign(container, {
     "background-color": color_male + "bc",
