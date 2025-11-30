53787aa7fc777ed9a499602e613978f5f5766870
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:49:29 -0500

    fws fd app_main

--- public/src/function_dependencies_code_copy_app_main.mjs
+++ public/src/function_dependencies_code_copy_app_main.mjs
@@ -1,1 +1,6 @@
-export function function_dependencies_code_copy_app_main() {}
+import { function_dependencies_code_copy } from "../../../love/public/src/function_dependencies_code_copy.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_dependencies_code_copy_app_main(f_name) {
+  marker("1");
+  return await function_dependencies_code_copy(f_name);
+}
