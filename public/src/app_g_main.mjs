c45c38ef7e7eec26522b9db960fde07810dcd952
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 18:26:02 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -1,3 +1,4 @@
+import { random } from "../../../love/public/src/random.mjs";
 import { html_document_head } from "../../../love/public/src/html_document_head.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 import { html_z_max } from "../../../love/public/src/html_z_max.mjs";
@@ -90,6 +91,8 @@ export async function app_g_main() {
     "pointer-events": "auto",
   });
   let component = html_document_head();
+  const uid = "scroll-hidden-" + Math.random().toString(36).slice(2);
+  container.setAttribute("data-scroll-hide", uid);
   let div_map = html_div(div_map_container);
   object_property_set_exists_not(div_map, "container", div_map_container);
   html_style_assign(div_map, {
