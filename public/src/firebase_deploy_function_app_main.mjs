45ae156191912f01ebb082c6a48a23191c60cbe1
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:11:28 -0500

    function_auto_path ..\love\public\src\firebase_deploy_function_app_main.mjs

--- public/src/firebase_deploy_function_app_main.mjs
+++ public/src/firebase_deploy_function_app_main.mjs
@@ -1,9 +1,9 @@
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 import { firebase_deploy_function } from "../../../love/public/src/firebase_deploy_function.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function firebase_deploy_function_app_main(f_name) {
+export async function firebase_deploy_function_app_main(a) {
   marker("1");
   let combined = app_name_main(a);
-  let v = await firebase_deploy_function(f_name);
+  let v = await firebase_deploy_function(combined);
   return v;
 }
