a23f53d281be96c834235f417f21e3d84388bcf2
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:49:41 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_copy_app_main.mjs

--- web/public/src/function_dependencies_code_copy_app_main.mjs
+++ web/public/src/function_dependencies_code_copy_app_main.mjs
@@ -1,7 +1,7 @@
+import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 import { function_dependencies_code_copy } from "../../../love/public/src/function_dependencies_code_copy.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_dependencies_code_copy_app_main(f_name) {
-  marker("1");
-  let v = await function_dependencies_code_copy(f_name);
+export async function function_dependencies_code_copy_app_main(a) {
+  let combined = app_name_main(a);
+  let v = await function_dependencies_code_copy(combined);
   return v;
 }
