83799842414fd69ab696774c9e5ead43ba62865b
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 22:46:14 -0500

    function_auto_path ..\love\public\src\integer_random_1.mjs

--- public/src/integer_random_1.mjs
+++ public/src/integer_random_1.mjs
@@ -1,6 +1,5 @@
-import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { integer_random } from "../../../love/public/src/integer_random.mjs";
 export function integer_random_1(column_count) {
-  let r6 = integer_random(add_1, column_count);
+  let r6 = integer_random(1, column_count);
   return r6;
 }
