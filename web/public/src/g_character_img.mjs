212ede3a9b358e1a3e6f927445acf5c5c9d9a99a
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 10:36:36 -0500

    function_auto_path ..\love\public\src\g_character_img.mjs

--- web/public/src/g_character_img.mjs
+++ web/public/src/g_character_img.mjs
@@ -1,10 +1,9 @@
+import { g_character_img_url } from "../../../love/public/src/g_character_img_url.mjs";
 import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { g_img_square_style_position_object } from "../../../love/public/src/g_img_square_style_position_object.mjs";
 import { g_img_square } from "../../../love/public/src/g_img_square.mjs";
 export function g_character_img(game_prefix, div, c) {
-  let img = object_property_get(c, "img");
-  const c_src = game_prefix + "characters\\" + img + "\\rotations\\south.png";
+  const c_src = g_character_img_url(c, game_prefix);
   let ci = g_img_square(div, c_src, -1, -1, "character");
   g_img_square_style_position_object(c, ci);
   html_click_none(ci);
