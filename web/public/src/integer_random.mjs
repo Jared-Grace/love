95f2de23958da4f3e74d9adc46d198254e7f1245
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 22:47:14 -0500

    function_auto_path ..\love\public\src\integer_random.mjs

--- web/public/src/integer_random.mjs
+++ web/public/src/integer_random.mjs
@@ -1,9 +1,9 @@
-import { import_install } from "../../../love/public/src/import_install.mjs";
+import { integer_is } from "../../../love/public/src/integer_is.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { random } from "../../../love/public/src/random.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
-export async function integer_random(min, max) {
-  await import_install(name);
+export function integer_random(min, max) {
+  let ii = integer_is(value);
   assert(b);
   let r = floor(random() * (max - min + 1)) + min;
   return r;
