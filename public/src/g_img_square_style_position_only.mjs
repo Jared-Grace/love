10b290828488dd0711219f25b256b27b6e25a75c
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 16:27:36 -0500

    function_auto_path ..\love\public\src\g_img_square_style_position_only.mjs

--- public/src/g_img_square_style_position_only.mjs
+++ public/src/g_img_square_style_position_only.mjs
@@ -1,3 +1,4 @@
+import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { g_rows_count } from "../../../love/public/src/g_rows_count.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
@@ -5,8 +6,8 @@ export function g_img_square_style_position_only(tile, x, y) {
   marker("1");
   let rows = g_rows_count();
   html_style_assign(tile, {
-    left: "calc(" + x + ` * (max(100vh / ${rows}, 100vw / ${rows})))`,
-    top: "calc(" + y + ` * (max(100vh / ${rows}, 100vw / ${rows})))`,
+    left: "calc(" + x + ` * (` + g_img_square_size_css() + `))`,
+    top: "calc(" + y + ` * (` + g_img_square_size_css() + `))`,
     transition: "left 0.3s ease, top 0.3s ease",
   });
 }
