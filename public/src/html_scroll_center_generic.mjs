c92500e68f8003b6c9739f5e6022c2b4ab66508b
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 16:13:01 -0500

    function_auto_path ..\love\public\src\html_scroll_center_generic.mjs

--- public/src/html_scroll_center_generic.mjs
+++ public/src/html_scroll_center_generic.mjs
@@ -1,17 +1,25 @@
-import { sleep_0 } from "../../../love/public/src/sleep_0.mjs";
+import { sleep } from "../../../love/public/src/sleep.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_scroll_center_generic(player_img_c, behavior) {
-  let element2 = html_component_element_get(player_img_c);
-  function lambda3() {
-    async function lambda2() {
-      await sleep_0();
-      element2.scrollIntoView({
-        behavior: behavior,
-        block: "center",
-        inline: "center",
-      });
-    }
-    requestAnimationFrame(lambda2);
+  async function html_scroll_center_generic(player_img_c, behavior) {
+    let el = html_component_element_get(player_img_c);
+    await new Promise(function lambda3(r) {
+      function lambda2() {
+        function lambda() {
+          let v = requestAnimationFrame(r);
+          return v;
+        }
+        let v2 = requestAnimationFrame(lambda);
+        return v2;
+      }
+      let v3 = requestAnimationFrame(lambda2);
+      return v3;
+    });
+    await sleep(10);
+    el.scrollIntoView({
+      behavior,
+      block: "center",
+      inline: "center",
+    });
   }
-  requestAnimationFrame(lambda3);
 }
