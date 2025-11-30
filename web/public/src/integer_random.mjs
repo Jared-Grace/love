c1426a181a8c897101f9e8ddf86f6da87afe1f0e
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 22:48:38 -0500

    function_auto_path ..\love\public\src\integer_random.mjs

--- web/public/src/integer_random.mjs
+++ web/public/src/integer_random.mjs
@@ -5,9 +5,10 @@ import { random } from "../../../love/public/src/random.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
 export function integer_random(min, max) {
   let fn = integer_is;
+  let list = [min, max];
   let ii = fn(min);
   assert(ii);
-  function lambda(item) {}
+  function lambda(l) {}
   each(list, lambda);
   let r = floor(random() * (max - min + 1)) + min;
   return r;
