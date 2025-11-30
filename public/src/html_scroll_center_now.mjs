8f70088aafb8f23f98c3123c077e60995c110557
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 00:59:03 -0500

    function_auto_path ..\love\public\src\html_scroll_center_now.mjs

---
+++ public/src/html_scroll_center_now.mjs
@@ -0,0 +1,5 @@
+import { html_scroll_center_generic } from "../../../love/public/src/html_scroll_center_generic.mjs";
+export function html_scroll_center_now(player_img_c) {
+  const behavior = "smooth";
+  html_scroll_center_generic(player_img_c, behavior);
+}
