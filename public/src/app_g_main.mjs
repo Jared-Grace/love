df5e337981681a9930c4029d4aebf57d2045d05c
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 18:27:31 -0500

    s style

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -90,12 +90,13 @@ export async function app_g_main() {
     "pointer-events": "auto",
   });
   let head = html_document_head();
-  const style = document.createElement("style");
-  style.textContent = `
-  [data-scroll-hide="${uid}"]::-webkit-scrollbar {
+const style = document.createElement("style");
+style.textContent = `
+  #${container.id}::-webkit-scrollbar {
     display: none;
   }
 `;
+document.head.appendChild(style);
   document.head.appendChild(style);
   let div_map = html_div(div_map_container);
   object_property_set_exists_not(div_map, "container", div_map_container);
