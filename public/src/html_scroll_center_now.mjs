fe5855ee8bbbcc714b195c78723914c8a7d5c6f3
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 00:59:04 -0500

    cs html_scroll_center now

--- public/src/html_scroll_center_now.mjs
+++ public/src/html_scroll_center_now.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_scroll_center_generic } from "../../../love/public/src/html_scroll_center_generic.mjs";
 export function html_scroll_center_now(player_img_c) {
+  marker("1");
   const behavior = "smooth";
   html_scroll_center_generic(player_img_c, behavior);
 }
