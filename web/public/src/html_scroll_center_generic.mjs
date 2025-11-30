9430ba12f105c7da909546426938df79f9a28ec3
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 10:00:08 -0500

    function_auto_path ..\love\public\src\html_scroll_center_generic.mjs

--- web/public/src/html_scroll_center_generic.mjs
+++ web/public/src/html_scroll_center_generic.mjs
@@ -1,10 +1,10 @@
-import { sleep } from "../../../love/public/src/sleep.mjs";
+import { sleep_0 } from "../../../love/public/src/sleep_0.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_scroll_center_generic(player_img_c, behavior) {
   let element2 = html_component_element_get(player_img_c);
   function lambda3() {
     async function lambda2() {
-      await sleep(0);
+      await sleep_0();
       element2.scrollIntoView({
         behavior: behavior,
         block: "center",
