4d3bff9b0eeadfc3695a36d52c6b0fb4c09a9aef
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 00:58:40 -0500

    function_auto_path ..\love\public\src\html_scroll_center.mjs

--- web/public/src/html_scroll_center.mjs
+++ web/public/src/html_scroll_center.mjs
@@ -1,8 +1,9 @@
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_scroll_center(player_img_c) {
+  const behavior = "smooth";
   let element2 = html_component_element_get(player_img_c);
   element2.scrollIntoView({
-    behavior: "smooth",
+    behavior: behavior,
     block: "center",
     inline: "center",
   });
