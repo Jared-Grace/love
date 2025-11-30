63519e3baccfaf16457e00505ed56d804eb7ff1d
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 16:28:15 -0500

    function_auto_path ..\love\public\src\g_img_square_style_position_only.mjs

--- public/src/g_img_square_style_position_only.mjs
+++ public/src/g_img_square_style_position_only.mjs
@@ -1,6 +1,5 @@
 import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { g_rows_count } from "../../../love/public/src/g_rows_count.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function g_img_square_style_position_only(tile, x, y) {
   marker("1");
