8a37c70b2b7218b21b27752447c63e125e9a6d98
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 18:08:15 -0500

    fs html_scroll_center_generic

--- public/src/html_scroll_center_generic.mjs
+++ public/src/html_scroll_center_generic.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_scroll_center_generic_wait } from "../../../love/public/src/html_scroll_center_generic_wait.mjs";
 export async function html_scroll_center_generic(player_img_c, behavior) {
+  marker("1");
   let el = await html_scroll_center_generic_wait(player_img_c);
   el.scrollIntoView({
     behavior,
