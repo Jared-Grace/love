db7bb339fe4057e2dcc7272262ce83535b3fded1
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 19:01:24 -0500

    function_auto_path ..\love\public\src\app_g_click_npc.mjs

--- public/src/app_g_click_npc.mjs
+++ public/src/app_g_click_npc.mjs
@@ -53,6 +53,7 @@ export function app_g_click_npc(div, npcs_matched, tutorial, body, player) {
           string_random_or_empty(a) + s2 + " to meet you" + g_random_dot_bang(),
         );
     } else {
+      meet_message = "";
     }
     app_g_p_text(
       container,
