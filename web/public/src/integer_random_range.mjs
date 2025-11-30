e06d09b8abcdcd6eb0e4cbf534e36edbfa7813f5
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 01:25:07 -0500

    function_auto_path ..\love\public\src\integer_random_range.mjs

---
+++ web/public/src/integer_random_range.mjs
@@ -0,0 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { integer_random } from "../../../love/public/src/integer_random.mjs";
+export function integer_random_range(column_count) {
+  marker("1");
+  let r6 = integer_random(0, column_count);
+  return r6;
+}
