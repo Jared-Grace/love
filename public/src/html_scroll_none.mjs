64c0498c7134f4e02388e90464581bcd8a645c1e
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 18:36:27 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

---
+++ public/src/html_scroll_none.mjs
@@ -0,0 +1,17 @@
+import { html_id_set } from "../../../love/public/src/html_id_set.mjs";
+import { html_style_head } from "../../../love/public/src/html_style_head.mjs";
+import { uuid } from "../../../love/public/src/uuid.mjs";
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export async function html_scroll_none(div_map_container) {
+  html_style_assign(div_map_container, {
+    scrollbarWidth: "none",
+    msOverflowStyle: "none",
+  });
+  let u = await uuid();
+  html_style_head(`
+  #${u}::-webkit-scrollbar {
+    display: none;
+  }
+`);
+  html_id_set(div_map_container, u);
+}
