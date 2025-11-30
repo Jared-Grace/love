1b2847568afc8970b3efbfeb8780de0dc0ae84ab
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 19:45:45 -0500

    function_auto_path ..\love\public\src\localhost_is.mjs

--- web/public/src/localhost_is.mjs
+++ web/public/src/localhost_is.mjs
@@ -1,4 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function localhost_is() {
   marker("1");
+  return (
+    location.hostname === "localhost" ||
+    location.hostname === "127.0.0.1" ||
+    location.hostname === "::1"
+  );
 }
