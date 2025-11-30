faecaa0b7402541c1d3e1d7820c1d79a4a79bace
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:40:04 -0500

    function_auto_path ..\love\public\src\app_g_click_npc.mjs

--- public/src/app_g_click_npc.mjs
+++ public/src/app_g_click_npc.mjs
@@ -50,7 +50,7 @@ export function app_g_click_npc(div, npcs_matched, tutorial, body, player) {
       let text = emoji_pray();
       html_text_set(tutorial, text);
       let rows = g_rows_count();
-      const square_size = "calc(100vh / " + rows + " * .7)";
+      const square_size = "calc(100vh / " + rows + " * .5)";
       html_style_assign(tutorial, {
         display: "flex",
         justifyContent: "center",
