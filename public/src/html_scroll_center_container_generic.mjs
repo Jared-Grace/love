11e253a8194c946c3a21b03f4d4087340c149d22
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 18:21:04 -0500

    function_auto_path ..\love\public\src\html_scroll_center_container_generic.mjs

--- public/src/html_scroll_center_container_generic.mjs
+++ public/src/html_scroll_center_container_generic.mjs
@@ -27,8 +27,8 @@ export async function html_scroll_center_container_generic(
     container_e.clientHeight / 2 +
     tileRect.height / 2;
   const s = {
-    left: 0,
-    top: 0,
+    left: scrollLeft,
+    top: scrollTop,
     behavior,
   };
   container_e.scrollTo(s);
