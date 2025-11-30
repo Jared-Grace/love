14761cae1903cf70dda834e6e68d4cb00fe2bfdf
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 09:59:43 -0500

    function_auto_path ..\love\public\src\html_scroll_center_generic.mjs

--- web/public/src/html_scroll_center_generic.mjs
+++ web/public/src/html_scroll_center_generic.mjs
@@ -1,8 +1,9 @@
+import { sleep } from "../../../love/public/src/sleep.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_scroll_center_generic(player_img_c, behavior) {
   let element2 = html_component_element_get(player_img_c);
   function lambda3() {
-    function lambda2() {
+    async function lambda2() {
       function lambda() {
         element2.scrollIntoView({
           behavior: behavior,
@@ -10,6 +11,7 @@ export function html_scroll_center_generic(player_img_c, behavior) {
           inline: "center",
         });
       }
+      await sleep(ms);
       setTimeout(lambda, 0);
     }
     requestAnimationFrame(lambda2);
