86e886db5e3026cefb2b1474bb390badf8e9e7c5
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 17:54:30 -0500

    function_auto_path ..\love\public\src\html_scroll_center_generic.mjs

--- public/src/html_scroll_center_generic.mjs
+++ public/src/html_scroll_center_generic.mjs
@@ -1,25 +1,6 @@
-import { not } from "../../../love/public/src/not.mjs";
-import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+import { html_scroll_center_generic_wait } from "../../../love/public/src/html_scroll_center_generic_wait.mjs";
 export async function html_scroll_center_generic(player_img_c, behavior) {
-  let el = html_component_element_get(player_img_c);
-  if (el.tagName === "IMG" && not(el.complete)) {
-    await new Promise(function lambda(r) {
-      let v = (el.onload = r);
-      return v;
-    });
-  }
-  await new Promise(function lambda4(r) {
-    function lambda3() {
-      function lambda2() {
-        let v2 = requestAnimationFrame(r);
-        return v2;
-      }
-      let v3 = requestAnimationFrame(lambda2);
-      return v3;
-    }
-    let v4 = requestAnimationFrame(lambda3);
-    return v4;
-  });
+  let el = await html_scroll_center_generic_wait(player_img_c);
   el.scrollIntoView({
     behavior,
     block: "center",
