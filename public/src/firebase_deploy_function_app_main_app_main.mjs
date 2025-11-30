22d69652c631c1eceba20e1cf8c0704b6e3bfd79
Grace-Jared <christrosetolife@gmail.com>
2025-11-29 20:49:17 -0500

    de firebase_deploy_function_app_main_app_main

--- public/src/firebase_deploy_function_app_main_app_main.mjs
+++
@@ -1,7 +0,0 @@
-import { firebase_deploy_function_app_main } from "../../../love/public/src/firebase_deploy_function_app_main.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function firebase_deploy_function_app_main_app_main(a) {
-  marker("1");
-  let v = await firebase_deploy_function_app_main(a);
-  return v;
-}
