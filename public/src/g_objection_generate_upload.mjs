43f3d49d1dc613b7d5ab5034496b4395ea16bdc2
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 22:33:20 -0500

    rs firebase_deploy_function_destination json

--- public/src/g_objection_generate_upload.mjs
+++ public/src/g_objection_generate_upload.mjs
@@ -1,5 +1,5 @@
 import { log } from "../../../love/public/src/log.mjs";
-import { firebase_deploy_function_destination } from "../../../love/public/src/firebase_deploy_function_destination.mjs";
+import { firebase_deploy_function_destination_json } from "../../../love/public/src/firebase_deploy_function_destination_json.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
 import { path_name } from "../../../love/public/src/path_name.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
@@ -7,7 +7,7 @@ import { g_objection_generate } from "../../../love/public/src/g_objection_gener
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { g_objection_generate_migrate_generic } from "../../../love/public/src/g_objection_generate_migrate_generic.mjs";
-import { path_base } from "./path_base.mjs";
+import { path_base } from "../../../love/public/src/path_base.mjs";
 export async function g_objection_generate_upload() {
   marker("1");
   await g_objection_generate_migrate_generic(file_each);
@@ -16,11 +16,14 @@ export async function g_objection_generate_upload() {
     let chapter_code = path_name(fb);
     let fn = g_objection_generate;
     let path = local_function_path_json(chapter_code, fn);
-    let destination = firebase_deploy_function_destination(
+    let destination = firebase_deploy_function_destination_json(
       fb,
       g_objection_generate_upload.name,
     );
-    let joined = firebase_deploy_function_destination(destination, fn.name);
+    let joined = firebase_deploy_function_destination_json(
+      destination,
+      fn.name,
+    );
     log({
       joined,
     });
