6b31fd4dd1217d504350630a969cf3cbc4210733
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:21:39 -0500

    cs list_map_path_join left

--- web/public/src/list_map_path_join_left.mjs
+++ web/public/src/list_map_path_join_left.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 export function list_map_path_join_left(list, f_name) {
+  marker("1");
   function lambda(item) {
     let joined = path_join([item, f_name]);
     return joined;
