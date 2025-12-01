c10672dc3ca22acfb9e72d51a7ef7446b0373df4
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 22:33:20 -0500

    rs firebase_deploy_function_destination json

--- public/src/firebase_deploy_function_destination_latest.mjs
+++ public/src/firebase_deploy_function_destination_latest.mjs
@@ -1,8 +1,8 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { firebase_deploy_function_destination } from "../../../love/public/src/firebase_deploy_function_destination.mjs";
+import { firebase_deploy_function_destination_json } from "../../../love/public/src/firebase_deploy_function_destination_json.mjs";
 export function firebase_deploy_function_destination_latest(f_name) {
   marker("1");
-  let destination2 = firebase_deploy_function_destination(
+  let destination2 = firebase_deploy_function_destination_json(
     "version_latest",
     f_name,
   );
