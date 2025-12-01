dd4e6179fa68fe306881fa0c2d58874fee37d8dc
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 22:27:18 -0500

    function_auto_path ..\love\public\src\g_objection_generate_upload.mjs

--- public/src/g_objection_generate_upload.mjs
+++ public/src/g_objection_generate_upload.mjs
@@ -1,7 +1,7 @@
+import { firebase_deploy_function_destination } from "../../../love/public/src/firebase_deploy_function_destination.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
 import { path_name } from "../../../love/public/src/path_name.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
-import { path_join } from "../../../love/public/src/path_join.mjs";
 import { g_objection_generate } from "../../../love/public/src/g_objection_generate.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -15,7 +15,7 @@ export async function g_objection_generate_upload() {
     let chapter_code = path_name(fb);
     let fn = g_objection_generate;
     let path = local_function_path_json(chapter_code, fn);
-    let joined = path_join([fn.name, fb]);
+    let joined = firebase_deploy_function_destination(fn.name, fb);
     let data = await file_read_json(path);
     await firebase_upload_object(data, joined);
   }
