32478c6f8f7182f4fba9c2dc2ed066bcf2fd36b7
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 22:43:53 -0500

    function_auto_path ..\love\public\src\g_objection_generate_migrate.mjs

--- public/src/g_objection_generate_migrate.mjs
+++ public/src/g_objection_generate_migrate.mjs
@@ -1,7 +1,7 @@
+import { object_property_rename } from "../../../love/public/src/object_property_rename.mjs";
 import { g_objection_generate_migrate_generic } from "../../../love/public/src/g_objection_generate_migrate_generic.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function g_objection_generate_migrate() {
@@ -13,8 +13,8 @@ export async function g_objection_generate_migrate() {
         log({
           item,
         });
-        const property_name_before = "sermon";
-        object_property_delete(item, property_name_before);
+        const property_name_before = "objection";
+        object_property_rename(item, property_name_before, "objections");
       }
       each(passages, lambda3);
     }
