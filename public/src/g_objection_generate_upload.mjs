0e4194f16dedf2b293c22b5bdb50dc89eef78061
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:57:51 -0500

    function_auto_path ..\love\public\src\g_objection_generate_upload.mjs

--- public/src/g_objection_generate_upload.mjs
+++ public/src/g_objection_generate_upload.mjs
@@ -1,3 +1,4 @@
+import { function_path_to_name } from "../../../love/public/src/function_path_to_name.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { g_objection_generate } from "../../../love/public/src/g_objection_generate.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
@@ -8,6 +9,7 @@ export async function g_objection_generate_upload() {
   let fn = g_objection_generate;
   await g_objection_generate_migrate_generic(file_each);
   async function file_each(file) {
+    let f_name = function_path_to_name(f_path);
     log(file);
     return;
     await firebase_upload_object(object, fn.name);
