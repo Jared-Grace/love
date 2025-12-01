6bb21cef36d696b8043b6ac20ce1ea9c8bcddcf9
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:23:52 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,12 +1,10 @@
-import { list_map_path_join_left } from "../../../love/public/src/list_map_path_join_left.mjs";
+import { folder_read_paths_async } from "../../../love/public/src/folder_read_paths_async.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { folder_read_async } from "../../../love/public/src/folder_read_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 export async function sandbox() {
   const path = "D:\\user\\storage\\function\\g_objection_generate";
-  let file_names = await folder_read_async(path);
-  let combineds = list_map_path_join_left(file_names, path);
+  let combineds = await folder_read_paths_async(path);
   async function lambda2(file) {
     log({
       file,
