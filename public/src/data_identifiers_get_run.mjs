e7e5dd76833da68582c81bf930c29bbf60f1f129
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 17:20:11 -0500

    function_auto_path ..\love\public\src\data_identifiers_get_run.mjs

--- public/src/data_identifiers_get_run.mjs
+++ public/src/data_identifiers_get_run.mjs
@@ -3,5 +3,4 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function data_identifiers_get_run() {
   marker("1");
   let v = await data_identifiers_get();
-  return v;
 }
