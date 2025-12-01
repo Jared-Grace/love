15e73b1ff67a4a43e65893b3ab1c2b6930d8d307
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 17:14:55 -0500

    function_auto_path ..\love\public\src\js_dollar_s.mjs

--- web/public/src/js_dollar_s.mjs
+++ web/public/src/js_dollar_s.mjs
@@ -1,5 +1,5 @@
+import { js_literal_is } from "../../../love/public/src/js_literal_is.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
-import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 import { js_declare } from "../../../love/public/src/js_declare.mjs";
 import { list_next_index } from "../../../love/public/src/list_next_index.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
@@ -34,7 +34,7 @@ export function js_dollar_s({ stack1, stack2, ast, afters }) {
       let s = js_parse_statement(code);
       let expression = object_property_get(s, "expression");
       let { id, init } = declaration;
-      let type_is = js_node_type_is(init, "Literal");
+      let type_is = js_literal_is(init);
       if (type_is) {
         let value = object_property_get(init, "value");
         if (null_is(value)) {
