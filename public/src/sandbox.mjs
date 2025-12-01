d1b5c4c12ee682568630c0e14db80c28512f480e
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:51:03 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { g_objection_generate } from "../../../love/public/src/g_objection_generate.mjs";
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -6,7 +7,8 @@ import { log } from "../../../love/public/src/log.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 export async function sandbox() {
-  const path = "D:\\user\\storage\\function\\g_objection_generate";
+  let fn = g_objection_generate;
+  const path = "D:\\user\\storage\\function\\" + fn.name;
   let combineds = await folder_read_paths_async(path);
   async function lambda2(file) {
     async function lambda(data) {
