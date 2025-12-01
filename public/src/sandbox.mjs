7b59c8bbdf544de42481203f0efdb69b6c618f7c
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:16:47 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,9 @@
+import { folder_read_async } from "../../../love/public/src/folder_read_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 export async function sandbox() {
   const path = "D:\\user\\storage\\function\\g_objection_generate";
+  let files = await folder_read_async(path_folder);
   async function lambda2(item) {
     async function lambda(data) {}
     await file_json_transform(f_path, lambda);
