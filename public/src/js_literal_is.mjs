79fa7dcd91534eee4bfbdf9539c75355d84cc3be
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 17:15:30 -0500

    function_auto_path ..\love\public\src\js_literal_is.mjs

--- public/src/js_literal_is.mjs
+++ public/src/js_literal_is.mjs
@@ -1,5 +1,5 @@
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
-export function js_literal_is(init) {
-  let li = js_node_type_is(init, "Literal");
+export function js_literal_is(node) {
+  let li = js_node_type_is(node, "Literal");
   return li;
 }
