d2b0681de4b4b1ec9d76d2c0778f304ff53cc559
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:48:48 -0500

    function_auto_path ..\love\public\src\object_property_rename.mjs

--- public/src/object_property_rename.mjs
+++ public/src/object_property_rename.mjs
@@ -1,3 +1,4 @@
+import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function object_property_rename(
@@ -7,4 +8,5 @@ export function object_property_rename(
 ) {
   let value = object_property_get(item, property_name_before);
   object_property_set_exists_not(item, property_name_after, value);
+  object_property_delete(object, property_name);
 }
