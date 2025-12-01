a036ab69708fdd25eb83bf01b9adf21ee45e12e2
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 22:53:27 -0500

    function_auto_path ..\love\public\src\newline_windows_code.mjs

--- web/public/src/newline_windows_code.mjs
+++ web/public/src/newline_windows_code.mjs
@@ -4,7 +4,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export function newline_windows_code() {
   marker("1");
   let n = newline_windows();
-  let code_string = js_code_string(value_string);
-  let v = `\\r\\n`;
-  return v;
+  let code_string = js_code_string(n);
+  return code_string;
 }
