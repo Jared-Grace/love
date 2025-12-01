ab54e16b2df9e57a34137517ccb3145346ecca48
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:41:26 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { folder_read_paths_async } from "../../../love/public/src/folder_read_paths_async.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -10,7 +11,9 @@ export async function sandbox() {
       file,
     });
     return;
-    async function lambda(data) {}
+    async function lambda(data) {
+      let value = object_property_get(object, property_name);
+    }
     await file_json_transform(file, lambda);
   }
   await each_async(combineds, lambda2);
