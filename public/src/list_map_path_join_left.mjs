be7ac61a0ed11cc8465a1b6272147cb7f8fd5da5
Grace-Jared <christrosetolife@gmail.com>
2025-11-30 20:21:56 -0500

    function_auto_path ..\love\public\src\list_map_path_join_left.mjs

--- public/src/list_map_path_join_left.mjs
+++ public/src/list_map_path_join_left.mjs
@@ -4,7 +4,7 @@ import { path_join } from "../../../love/public/src/path_join.mjs";
 export function list_map_path_join_left(list, f_name) {
   marker("1");
   function lambda(item) {
-    let joined = path_join([item, f_name]);
+    let joined = path_join([f_name, item]);
     return joined;
   }
   let mapped2 = list_map(list, lambda);
