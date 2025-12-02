448fcf96bf2c1e69ba37511150e6fd0b32b7ee69
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 18:05:50 -0500

    function_auto_path ..\love\public\src\object_property_exists_if_async.mjs

--- web/public/src/object_property_exists_if_async.mjs
+++ web/public/src/object_property_exists_if_async.mjs
@@ -5,7 +5,7 @@ export async function object_property_exists_if_async(obj, property, on_exist) {
   marker("1");
   let e = object_property_exists(obj, property);
   if (e) {
-    let f_names = object_property_get(obj, property);
-    await on_exist(f_names);
+    let value = object_property_get(obj, property);
+    await on_exist(value);
   }
 }
