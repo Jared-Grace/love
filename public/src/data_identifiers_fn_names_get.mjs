7a8d763cfc6d4de756693f91a0e7db316d34d47c
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 20:51:53 -0500

    function_auto_path ..\love\public\src\data_identifiers_fn_names_get.mjs

---
+++ public/src/data_identifiers_fn_names_get.mjs
@@ -0,0 +1,7 @@
+import { data_path } from "../../../love/public/src/data_path.mjs";
+import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
+export async function data_identifiers_fn_names_get() {
+  let d_path = data_path();
+  let v = await data_property_get("identifiers", d_path);
+  return v;
+}
