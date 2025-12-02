cccaa279989d4a6a7eb9a203554a6237b45f5c0f
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 20:51:54 -0500

    cr data_identifiers_get identifiers identifiers_fn_names

--- public/src/data_identifiers_fn_names_get.mjs
+++ public/src/data_identifiers_fn_names_get.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
 export async function data_identifiers_fn_names_get() {
+  marker("1");
   let d_path = data_path();
   let v = await data_property_get("identifiers", d_path);
   return v;
