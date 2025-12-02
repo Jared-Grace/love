0332ae733bc629d2e5969ad30beead2a74c748a6
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 21:04:06 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- public/src/function_rename_identifiers.mjs
+++ public/src/function_rename_identifiers.mjs
@@ -1,5 +1,4 @@
 import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
-import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
@@ -8,7 +7,6 @@ import { js_identifier_replace } from "../../../love/public/src/js_identifier_re
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
-  let n = object_property_exists_not(identifiers, f_name_before);
   let f_names = object_property_get_or(null, identifiers, f_name_before);
   if (f_names !== null) {
     return;
