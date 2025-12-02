ea83aa828e8a9ac1568cfe07d3f9a2301d103dca
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 18:02:15 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- web/public/src/function_rename_identifiers.mjs
+++ web/public/src/function_rename_identifiers.mjs
@@ -1,18 +1,13 @@
-import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+import { object_property_exists_if } from "../../../love/public/src/object_property_exists_if.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_imports_paths_fix } from "../../../love/public/src/js_imports_paths_fix.mjs";
 import { js_identifier_replace } from "../../../love/public/src/js_identifier_replace.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
-  let e = object_property_exists(identifiers, f_name_before);
-  if (e) {
-    let f_names = object_property_get(identifiers, f_name_before);
-    await on_exist(f_names);
-  }
+  await object_property_exists_if(identifiers, f_name_before, on_exist);
   async function on_exist(f_names) {
     list_remove_if_exists(f_names, f_name_before);
     async function lambda(f_name) {
