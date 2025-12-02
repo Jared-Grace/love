67cbf6d39ef44aaf3e82d7ee1f19427e84693eea
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 18:04:02 -0500

    function_auto_path ..\love\public\src\object_property_exists_if_async.mjs

--- web/public/src/object_property_exists_if_async.mjs
+++ web/public/src/object_property_exists_if_async.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 export async function object_property_exists_if_async(obj, property, on_exist) {
+  marker("1");
   let e = object_property_exists(obj, property);
   if (e) {
     let f_names = object_property_get(obj, property);
