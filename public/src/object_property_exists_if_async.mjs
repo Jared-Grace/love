07dbc3d9b3524c3dee9d4b2dd97e23e223e2ad8c
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 18:03:57 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

---
+++ public/src/object_property_exists_if_async.mjs
@@ -0,0 +1,9 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+export async function object_property_exists_if_async(obj, property, on_exist) {
+  let e = object_property_exists(obj, property);
+  if (e) {
+    let f_names = object_property_get(obj, property);
+    await on_exist(f_names);
+  }
+}
