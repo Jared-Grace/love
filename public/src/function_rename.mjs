e02f2cb9bc8f03da2262b286d39febaf269943d2
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 18:03:51 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -1,15 +1,11 @@
-import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
-import { data_identifiers_fn_names_get } from "../../../love/public/src/data_identifiers_fn_names_get.mjs";
 import { function_move } from "../../../love/public/src/function_move.mjs";
 import { function_rename_identifiers } from "../../../love/public/src/function_rename_identifiers.mjs";
 import { function_alias_rename } from "../../../love/public/src/function_alias_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename(f_name_before, f_name_after) {
-  let i = await data_identifiers_fn_names_get();
-  let f_names = object_property_get_or(null, i, f_name_before);
-  return f_names;
   await function_move(f_name_before, f_name_after);
   let v = await function_alias_rename(f_name_before, f_name_after);
   await function_rename_identifiers(f_name_before, f_name_after);
   marker("1");
+  return;
 }
