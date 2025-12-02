4ea0d0f15d412924c55a7929c070b4a8e6150c42
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 18:04:03 -0500

    p

--- public/src/function_rename_identifiers.mjs
+++ public/src/function_rename_identifiers.mjs
@@ -1,4 +1,4 @@
-import { object_property_exists_if } from "../../../love/public/src/object_property_exists_if.mjs";
+import { object_property_exists_if_async } from "../../../love/public/src/object_property_exists_if_async.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
@@ -7,7 +7,7 @@ import { js_identifier_replace } from "../../../love/public/src/js_identifier_re
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
-  await object_property_exists_if(identifiers, f_name_before, on_exist);
+  await object_property_exists_if_async(identifiers, f_name_before, on_exist);
   async function on_exist(f_names) {
     list_remove_if_exists(f_names, f_name_before);
     async function lambda(f_name) {
--- public/src/object_property_exists_if.mjs
+++
@@ -1,9 +0,0 @@
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-export async function object_property_exists_if(obj, property, on_exist) {
-  let e = object_property_exists(obj, property);
-  if (e) {
-    let f_names = object_property_get(obj, property);
-    await on_exist(f_names);
-  }
-}
