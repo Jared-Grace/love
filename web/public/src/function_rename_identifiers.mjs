38d2203309723454379201a1855e96380aee4365
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 21:03:10 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- web/public/src/function_rename_identifiers.mjs
+++ web/public/src/function_rename_identifiers.mjs
@@ -1,10 +1,10 @@
+import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
 import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_imports_paths_fix } from "../../../love/public/src/js_imports_paths_fix.mjs";
 import { js_identifier_replace } from "../../../love/public/src/js_identifier_replace.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
@@ -12,7 +12,7 @@ export async function function_rename_identifiers(f_name_before, f_name_after) {
   if (n) {
     return;
   }
-  let f_names = object_property_get(identifiers, f_name_before);
+  let f_names = object_property_get_or(identifiers, f_name_before);
   list_remove_if_exists(f_names, f_name_before);
   async function lambda(f_name) {
     async function lambda2(ast) {
