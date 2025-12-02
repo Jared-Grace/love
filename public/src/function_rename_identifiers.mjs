55df2e030a47a4ccc0cb74109a66966d2858a1b2
Grace-Jared <christrosetolife@gmail.com>
2025-12-01 21:09:04 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- public/src/function_rename_identifiers.mjs
+++ public/src/function_rename_identifiers.mjs
@@ -1,23 +1,24 @@
-import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
+import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_imports_paths_fix } from "../../../love/public/src/js_imports_paths_fix.mjs";
 import { js_identifier_replace } from "../../../love/public/src/js_identifier_replace.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
-  let f_names = object_property_get_or(null, identifiers, f_name_before);
-  if (f_names !== null) {
-    return;
-  }
-  list_remove_if_exists(f_names, f_name_before);
-  async function lambda(f_name) {
-    async function lambda2(ast) {
-      js_identifier_replace(ast, f_name_before, f_name_after);
-      await js_imports_paths_fix(ast);
+  let e = object_property_exists(identifiers, f_name_before);
+  if (e) {
+    let f_names = object_property_get(identifiers, f_name_before);
+    list_remove_if_exists(f_names, f_name_before);
+    async function lambda(f_name) {
+      async function lambda2(ast) {
+        js_identifier_replace(ast, f_name_before, f_name_after);
+        await js_imports_paths_fix(ast);
+      }
+      let output = await function_transform(f_name, lambda2);
     }
-    let output = await function_transform(f_name, lambda2);
+    await each_async(f_names, lambda);
   }
-  await each_async(f_names, lambda);
 }
