b6f16cdd0a9b868e133933d06eac1b0c164b17ec
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 22:55:55 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- web/public/src/app_g_conversation.mjs
+++ web/public/src/app_g_conversation.mjs
@@ -86,7 +86,8 @@ export async function app_g_conversation(
     app_g_button_green(overlay, text2, lambda3);
   }
   let choices = app_g_container(overlay);
-  app_g_p_text(choices, "What would you like to do?");
+  const text = "What would you like to do?";
+  app_g_p_text(choices, text);
   let name_npc2 = object_property_get(npc, "name");
   app_g_button_green(
     overlay,
