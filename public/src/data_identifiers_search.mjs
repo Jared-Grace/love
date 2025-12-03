f0a60d5908373724997f630205d970445ad2fa1a
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 15:26:56 -0500

    function_auto_path ..\love\public\src\data_identifiers_search.mjs

--- public/src/data_identifiers_search.mjs
+++ public/src/data_identifiers_search.mjs
@@ -1,3 +1,4 @@
+import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
@@ -5,6 +6,8 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function data_identifiers_search(s) {
   marker("1");
+  const { unaliased } = await function_name_to_path_unalias(s);
+  s = unaliased;
   let identifiers = await data_identifiers_get();
   let list = object_property_get(identifiers, s);
   let result = list_to_dictionary(list, function_name_to_path);
