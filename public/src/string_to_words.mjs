91f55747491f26482c123659e2bc60b4df2e0968
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 15:30:14 -0500

    o stw

--- public/src/string_to_words.mjs
+++ public/src/string_to_words.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { string_split_space } from "../../../love/public/src/string_split_space.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { whitespace_normalize } from "../../../love/public/src/whitespace_normalize.mjs";
 export function string_to_words(text) {
+  marker("1");
   let v2 = whitespace_normalize(text);
   let lower = string_lower_to(v2);
   let split2 = string_split_space(lower);
