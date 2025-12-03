696272668a13eb78c9a74c5ae0f14d8c3143909d
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 15:21:14 -0500

    data_file_update ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,5 +2,4 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { g_objection_generate_migrate } from "../../../love/public/src/g_objection_generate_migrate.mjs";
 export async function sandbox() {
   marker("1");
-  await g_objection_generate_migrate();
 }
