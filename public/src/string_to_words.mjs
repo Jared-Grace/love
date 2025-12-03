99abe12cbaca7527c9656d146fceef5ea7dc072a
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 16:23:07 -0500

    function_auto_path ..\love\public\src\string_to_words.mjs

--- public/src/string_to_words.mjs
+++ public/src/string_to_words.mjs
@@ -1,3 +1,4 @@
+import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { string_letters_only } from "../../../love/public/src/string_letters_only.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -10,5 +11,6 @@ export function string_to_words(text) {
   let lower = string_lower_to(v2);
   let words = string_split_space(lower);
   words = list_map(words, string_letters_only);
+  let unique = list_unique(list);
   return words;
 }
