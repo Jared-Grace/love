5122169a84d728452ccc0acd6a843dc801be644d
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 19:51:53 -0500

    function_auto_path ..\love\public\src\list_remove_last.mjs

--- public/src/list_remove_last.mjs
+++ public/src/list_remove_last.mjs
@@ -1,5 +1,5 @@
 import { list_remove_end } from "../../../love/public/src/list_remove_end.mjs";
-export function list_remove_last(coordinates) {
-  let removed = list_remove_end(coordinates, 1);
+export function list_remove_last(list) {
+  let removed = list_remove_end(list, 1);
   return removed;
 }
