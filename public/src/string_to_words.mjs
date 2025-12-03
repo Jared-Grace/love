01a4d012fa445a5d61aa00721af3db585382cc16
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 16:16:44 -0500

    function_auto_path ..\love\public\src\string_to_words.mjs

--- public/src/string_to_words.mjs
+++ public/src/string_to_words.mjs
@@ -1,3 +1,4 @@
+import { string_letters_only } from "../../../love/public/src/string_letters_only.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { string_split_space } from "../../../love/public/src/string_split_space.mjs";
@@ -8,7 +9,6 @@ export function string_to_words(text) {
   let v2 = whitespace_normalize(text);
   let lower = string_lower_to(v2);
   let words = string_split_space(lower);
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
+  words = list_map(words, string_letters_only);
   return words;
 }
