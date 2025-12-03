c41315752f1db6cecbdd2404e1b25d114e0172bc
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 23:39:40 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

---
+++ web/public/src/ebible_parts_to_reference.mjs
@@ -0,0 +1,23 @@
+import { list_last } from "../../../love/public/src/list_last.mjs";
+import { list_first } from "../../../love/public/src/list_first.mjs";
+import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
+import { ebible_book_code_to_name } from "../../../love/public/src/ebible_book_code_to_name.mjs";
+export function ebible_parts_to_reference(
+  books,
+  book_code,
+  verse_numbers,
+  chapter_name,
+) {
+  let book_name = ebible_book_code_to_name(books, book_code);
+  let verse_numbers_s = null;
+  let s1 = list_size_1(verse_numbers);
+  let first = list_first(list);
+  if (s1) {
+    verse_numbers_s = first;
+  } else {
+    let last = list_last(list);
+    verse_numbers_s = first + "-" + last;
+  }
+  const reference = book_name + " " + chapter_name + ":" + verse_numbers_s;
+  return reference;
+}
