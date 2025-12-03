4d75f5f81ebc962f8434ef59587897c283d802b9
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 11:29:01 -0500

    function_auto_path ..\love\public\src\app_g_menu.mjs

--- web/public/src/app_g_menu.mjs
+++ web/public/src/app_g_menu.mjs
@@ -10,32 +10,33 @@ import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { html_remove } from "../../../love/public/src/html_remove.mjs";
 export function app_g_menu(overlay, player) {
-  let v = new Promise(function lambda(resolve, reject) {});
+  let v = new Promise(function lambda(resolve, reject) {
+    html_clear(overlay);
+    function close() {
+      html_remove(overlay);
+    }
+    let component2 = app_g_button_back(overlay, close);
+    let text = emoji_pray() + " Pray";
+    function lambda7() {
+      html_clear(overlay);
+      function lambda23() {
+        app_g_menu(overlay, player);
+      }
+      let button2 = app_g_button_back(overlay, lambda23);
+      function lambda22() {
+        let prayer = object_property_get(player, "prayer");
+        object_property_set(prayer, "conversation", true);
+        app_g_player_save(player);
+        close();
+      }
+      const text =
+        emoji_bow() +
+        " Heavenly Father, please bless this next conversation, in Jesus' name, amen! " +
+        emoji_pray();
+      app_g_button_green(overlay, text, lambda22);
+    }
+    app_g_button_uncolored(overlay, text, lambda7);
+  });
   return v;
   marker("1");
-  html_clear(overlay);
-  function close() {
-    html_remove(overlay);
-  }
-  let component2 = app_g_button_back(overlay, close);
-  let text = emoji_pray() + " Pray";
-  function lambda7() {
-    html_clear(overlay);
-    function lambda23() {
-      app_g_menu(overlay, player);
-    }
-    let button2 = app_g_button_back(overlay, lambda23);
-    function lambda22() {
-      let prayer = object_property_get(player, "prayer");
-      object_property_set(prayer, "conversation", true);
-      app_g_player_save(player);
-      close();
-    }
-    const text =
-      emoji_bow() +
-      " Heavenly Father, please bless this next conversation, in Jesus' name, amen! " +
-      emoji_pray();
-    app_g_button_green(overlay, text, lambda22);
-  }
-  app_g_button_uncolored(overlay, text, lambda7);
 }
