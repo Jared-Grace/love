d2ee1d848e0b86db1f6e9430b5ad644504e1b979
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 23:22:45 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- web/public/src/app_g_conversation.mjs
+++ web/public/src/app_g_conversation.mjs
@@ -1,3 +1,5 @@
+import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
+import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 import { app_g_container_text } from "../../../love/public/src/app_g_container_text.mjs";
 import { app_g_npc_says } from "../../../love/public/src/app_g_npc_says.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
@@ -82,6 +84,7 @@ export async function app_g_conversation(
     let item3 = list_random_item(split);
     app_g_npc_says(npc, overlay, game_prefix, item3);
     app_g_container_text(overlay, "What would you like to say?");
+    let books = global_function_property_get(app_g_main, "books");
     function lambda3() {}
     app_g_button_green(overlay, text2, lambda3);
   }
