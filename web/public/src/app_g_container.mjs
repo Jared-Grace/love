342c154a26287ac03bfd41b84569e2db5a9b80ef
Grace-Jared <christrosetolife@gmail.com>
2025-12-02 21:47:21 -0500

    function_auto_path ..\love\public\src\app_g_container.mjs

--- web/public/src/app_g_container.mjs
+++ web/public/src/app_g_container.mjs
@@ -1,3 +1,4 @@
+import { app_karate_container_background_color } from "../../../love/public/src/app_karate_container_background_color.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { app_karate_style_control } from "../../../karate_code/public/src/app_karate_style_control.mjs";
 import { app_karate_container_centered } from "../../../karate_code/public/src/app_karate_container_centered.mjs";
@@ -6,6 +7,8 @@ export function app_g_container(overlay) {
   app_karate_style_control(container);
   html_style_assign(container, {
     padding: "0.25em",
+    "background-color": app_karate_container_background_color() + "bc",
   });
+  html_style_assign(container, {});
   return container;
 }
