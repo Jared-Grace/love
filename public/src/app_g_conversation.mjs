97e33de5c27f12ab2d6d5a01e3c17e628fc2cb12
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 16:36:58 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- public/src/app_g_conversation.mjs
+++ public/src/app_g_conversation.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_intersect } from "../../../love/public/src/list_intersect.mjs";
@@ -115,8 +116,9 @@ export function app_g_conversation(
       let text2 = object_property_get(p, "text");
       let words3 = string_to_words(text2);
       let list2 = list_intersect(words3, unique);
-      let e = list_empty_is(list);
-      if (false) {
+      let e = list_empty_is(list2);
+      if (e) {
+        log(message);
       }
     }
     let filtered = list_filter(passages, lambda2);
