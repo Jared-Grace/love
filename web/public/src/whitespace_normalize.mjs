03b5d93d6d0d71f645dcfe58d791f27bad6a6fb5
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 15:29:14 -0500

    function_auto_path ..\love\public\src\whitespace_normalize.mjs

--- web/public/src/whitespace_normalize.mjs
+++ web/public/src/whitespace_normalize.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function whitespace_normalize(str) {
   marker("1");
-  let v = str.replace(/\s+/g, " ").trim();
-  return v;
+  let n = str.replace(/\s+/g, " ").trim();
+  return n;
 }
