416705f1a85320d33bc2a7852e10fac74417aa9f
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 11:50:47 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- public/src/app_g_conversation.mjs
+++ public/src/app_g_conversation.mjs
@@ -75,7 +75,7 @@ export async function app_g_conversation(
     async function lambda2() {
       html_clear(overlay);
       let books = global_function_property_get(app_g_main, "books");
-      let value = global_function_property_get(fn, property_name);
+      let value = global_function_property_get(app_g_conversation, "");
       const chapter_code = "JAS01";
       let destination = g_objection_generate_upload_path(chapter_code);
       let o = await firebase_storage_download_json(destination);
