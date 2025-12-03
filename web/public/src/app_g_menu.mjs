b3d7c289e96f5f1ef828075aa6e50063ad42ba82
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 11:45:25 -0500

    function_auto_path ..\love\public\src\app_g_menu.mjs

--- web/public/src/app_g_menu.mjs
+++ web/public/src/app_g_menu.mjs
@@ -1,3 +1,4 @@
+import { promise_new } from "../../../love/public/src/promise_new.mjs";
 import { app_g_button_green } from "../../../love/public/src/app_g_button_green.mjs";
 import { app_g_button_uncolored } from "../../../love/public/src/app_g_button_uncolored.mjs";
 import { app_g_button_back } from "../../../love/public/src/app_g_button_back.mjs";
@@ -9,6 +10,7 @@ import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { html_remove } from "../../../love/public/src/html_remove.mjs";
 export async function app_g_menu(overlay, player) {
+  let result = await promise_new(async function lambda2(resolve2, reject2) {});
   let v = await new Promise(function lambda(resolve, reject) {
     html_clear(overlay);
     function close() {
