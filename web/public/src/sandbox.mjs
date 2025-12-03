2a5e5be3f0e0258b2575d6d60ba0e24ef16f2a54
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 15:21:56 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -2,7 +2,7 @@ import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
-  const WordNet = (await import("wordnetjs")).default;
+  const WordNet = await import("wordnetjs");
   const wn = new WordNet();
   const word = "happy";
   const synonyms = wn.synonyms(word);
