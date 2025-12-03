edb4fcee8f5ac91e1b4154e991c2a77ab2752912
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 15:21:12 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { g_objection_generate_migrate } from "../../../love/public/src/g_objection_generate_migrate.mjs";
 export async function sandbox() {
+  marker("1");
   await g_objection_generate_migrate();
 }
