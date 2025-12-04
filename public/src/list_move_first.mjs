ff045354787236b60d5ea693f4631d979c8635e7
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:52:36 -0500

    function_auto_path ..\love\public\src\list_move_first.mjs

--- public/src/list_move_first.mjs
+++ public/src/list_move_first.mjs
@@ -3,6 +3,6 @@ import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
 import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
 export function list_move_first(params, index) {
   let r = list_remove_at(params, index);
-  log(message);
+  log(r);
   list_add_first(params, r);
 }
