a1224a9570672a600bf177132fefb2597e94f0a7
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:45:37 -0500

    fs function_param_index

--- web/public/src/function_param_index.mjs
+++ web/public/src/function_param_index.mjs
@@ -1,8 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
 import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export function function_param_index(ast, param_name, index) {
+  marker("1");
   let declaration = js_declaration_single(ast);
   let params = js_declaration_params_get(declaration);
   let p = js_declaration_param_named(declaration, param_name);
