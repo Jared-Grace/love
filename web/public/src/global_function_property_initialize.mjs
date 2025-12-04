b0e8a0d21b453b2240d0e12ab4a1af089ed4f1f7
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:58:18 -0500

    function_auto_path ..\love\public\src\global_function_property_initialize.mjs

--- web/public/src/global_function_property_initialize.mjs
+++ web/public/src/global_function_property_initialize.mjs
@@ -1,11 +1,7 @@
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export  function global_function_property_initialize(
-  fn,
-  property_name,
-  lambda,
-) {
+export function global_function_property_initialize(fn, property_name, lambda) {
   let global = global_get();
   marker("1");
   let fn_object = object_property_initialize(global, fn.name, {});
