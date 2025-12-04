89c3c8da47fd1ca2ecee6279559b517f7c71b647
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 11:30:02 -0500

    data_file_update ..\love\public\src\object_property_initialize_lambda_async.mjs

--- web/public/src/global_function_property_async.mjs
+++ web/public/src/global_function_property_async.mjs
@@ -1,4 +1,4 @@
-import { object_property_lambda_async } from "../../../love/public/src/object_property_lambda_async.mjs";
+import { object_property_initialize_lambda_async } from "../../../love/public/src/object_property_initialize_lambda_async.mjs";
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -10,7 +10,7 @@ export async function global_function_property_async(
   let global = global_get();
   marker("1");
   let fn_object = object_property_initialize(global, fn.name, {});
-  let value = await object_property_lambda_async(
+  let value = await object_property_initialize_lambda_async(
     fn_object,
     property_name,
     lambda,
