08ad7b67c7bc2cdd2ef35a08601432add972faa4
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:53:52 -0500

    p

--- public/src/global_function_property_nested_lambda.mjs
+++ public/src/global_function_property_nested_lambda.mjs
@@ -3,10 +3,10 @@ import { object_property_lambda_async } from "../../../love/public/src/object_pr
 import { app_g_conversation } from "../../../love/public/src/app_g_conversation.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 export async function global_function_property_nested_lambda(
+  fn,
   property_name,
   property_name_nested,
   lambda,
-  fn,
 ) {
   marker("1");
   let value = global_function_property_get(app_g_conversation, property_name);
