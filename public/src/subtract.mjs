8dc2c7c6b2fe527140169f3b04b23b2d6c1daadf
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 21:34:19 -0500

    function_auto_path ..\love\public\src\subtract.mjs

--- public/src/subtract.mjs
+++ public/src/subtract.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function subtract(left, right) {
   marker("1");
-  const sum = left + right;
-  return sum;
+  const difference = left - right;
+  return difference;
 }
