9fa0f90793ceee71127f65ae113383f36f1b9bea
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:16:27 -0500

    function_auto_path ..\love\public\src\list_remove_first.mjs

--- public/src/list_remove_first.mjs
+++ public/src/list_remove_first.mjs
@@ -2,6 +2,6 @@ import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_remove_first(list) {
   marker("1");
-  let v = list_remove_at(list, 0);
-  return v;
+  let e = list_remove_at(list, 0);
+  return e;
 }
