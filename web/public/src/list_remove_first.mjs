b5bb97c7591df2299c615a2aa53c1c17f646d744
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:15:36 -0500

    fw list_remove_at list_remove_first

--- web/public/src/list_remove_first.mjs
+++ web/public/src/list_remove_first.mjs
@@ -1,1 +1,6 @@
-export function list_remove_first() {}
+import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function list_remove_first(list, index) {
+  marker("1");
+  return list_remove_at(list, index);
+}
