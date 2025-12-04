5419577ca163bd34a870c27ebe7e6dea39adfd16
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:45:43 -0500

    pd index

--- public/src/function_param_index.mjs
+++ public/src/function_param_index.mjs
@@ -3,7 +3,7 @@ import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
 import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
-export function function_param_index(ast, param_name, index) {
+export function function_param_index(ast, param_name) {
   marker("1");
   let declaration = js_declaration_single(ast);
   let params = js_declaration_params_get(declaration);
