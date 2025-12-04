07b720d4b778c3f660853c82e66f4e1181147a2a
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 22:33:53 -0500

    function_auto_path ..\love\public\src\trinity_name_prayer.mjs

--- web/public/src/trinity_name_prayer.mjs
+++ web/public/src/trinity_name_prayer.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function trinity_name_prayer() {
   marker("1");
-  let v2 = " of the Father, and of the Son, and of the Holy Spirit";
+  let v2 = "the Father, and of the Son, and of the Holy Spirit";
   return v2;
 }
