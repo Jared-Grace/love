66eb61591847db50a032242fbcd5c57c081f13c1
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 17:41:15 -0500

    function_auto_path ..\love\public\src\app_g_button_conversation_end.mjs

--- public/src/app_g_button_conversation_end.mjs
+++ public/src/app_g_button_conversation_end.mjs
@@ -1,12 +1,12 @@
 import { emoji_wave } from "../../../love/public/src/emoji_wave.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { app_g_button_green } from "../../../love/public/src/app_g_button_green.mjs";
-export function app_g_button_conversation_end(overlay, overlay_close) {
+export function app_g_button_conversation_end(overlay, conversation_end) {
   app_g_button_green(
     overlay,
     emoji_pray() +
       emoji_wave() +
       " Pray and then politely end the conversation",
-    overlay_close,
+    conversation_end,
   );
 }
