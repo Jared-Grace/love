56885ba9a2fbea4860f0a870642cdec98993d2de
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:42:25 -0500

    function_auto_path ..\love\public\src\function_param_swap.mjs

--- public/src/function_param_swap.mjs
+++ public/src/function_param_swap.mjs
@@ -1,9 +1,11 @@
+import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_transform_current } from "../../../love/public/src/function_transform_current.mjs";
 import { list_swap } from "../../../love/public/src/list_swap.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export async function function_param_swap(param_name_a, param_name_b) {
+  "TODO: finish this; consider similar code in " + function_params_new.name;
   marker("1");
   await function_transform_current(lambda);
   function lambda(ast) {
