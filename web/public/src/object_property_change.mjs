815f5de318211de66e6a1e8941c021fd1b220248
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 21:43:44 -0500

    function_auto_path ..\love\public\src\object_property_change.mjs

--- web/public/src/object_property_change.mjs
+++ web/public/src/object_property_change.mjs
@@ -4,4 +4,5 @@ export function object_property_change(npc, property, change) {
   let objection = object_property_get(npc, property);
   objection = change(objection);
   object_property_set(npc, property, objection);
+  return objection;
 }
