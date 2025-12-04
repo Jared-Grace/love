a466491895aca9c6d5cb01d4d417b03a08473cf6
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 09:23:17 -0500

    function_auto_path ..\love\public\src\app_g_gospel.mjs

--- public/src/app_g_gospel.mjs
+++ public/src/app_g_gospel.mjs
@@ -124,6 +124,7 @@ export async function app_g_gospel(overlay, npc, game_prefix) {
         " in the name of " + list_random_item(["Jesus", t]) + " ",
       ) +
       "amen";
+    () => {};
     const jesus_christ = "Jesus" + string_random_or_empty(" Christ");
     const choices = [
       "Thank you" +
