b28a8b6327af170c91fc66c33d9682aaac2b262e
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:32:51 -0500

    function_auto_path ..\love\public\src\app_g_click_npc.mjs

--- web/public/src/app_g_click_npc.mjs
+++ web/public/src/app_g_click_npc.mjs
@@ -38,55 +38,54 @@ export function app_g_click_npc(div_map, npcs_matched, player, game_prefix) {
     gap: "0px",
   });
   let review = object_property_get(player, "review");
-  if (e2) {
-    let prayer = object_property_get(player, "prayer");
-    let conversation = object_property_get(prayer, "conversation");
-    if (conversation) {
-      app_g_conversation(
-        prayer,
-        npcs_matched,
-        overlay,
-        player,
-        game_prefix,
-        overlay_close,
-      );
-    } else {
-      let container = app_g_container(overlay);
+  let prayer = object_property_get(player, "prayer");
+  let conversation = object_property_get(prayer, "conversation");
+  if (not(conversation)) {
+    let container = app_g_container(overlay);
+    app_g_p_text(
+      container,
+      emoji_pray() +
+        " You remember that you have not prayed, yet, before your next conversation!",
+    );
+    let conversed = object_property_get(player, "conversed");
+    if (not(conversed)) {
       app_g_p_text(
         container,
-        emoji_pray() +
-          " You remember that you have not prayed, yet, before your next conversation!",
+        " To pray, tap or click on yourself (You glow with white)",
       );
-      let conversed = object_property_get(player, "conversed");
-      if (not(conversed)) {
-        app_g_p_text(
-          container,
-          " To pray, tap or click on yourself (You glow with white)",
-        );
-      }
-      function lambda21() {
-        overlay_close();
-        let conversed = object_property_get(player, "conversed");
-        if (conversed) {
-          return;
-        }
-        let tutorial = html_div(div_map);
-        html_click_none(tutorial);
-        global_function_property_set(app_g_refresh, "tutorial", tutorial);
-        g_img_square_style_position(tutorial, player, "tutorial");
-        let text = emoji_pray();
-        html_text_set(tutorial, text);
-        const square_size = `calc(` + g_img_square_size_css() + `*.4)`;
-        html_style_assign(tutorial, {
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          fontSize: square_size,
-          animation: "upDown 1.25s infinite ease-in-out alternate",
-        });
-      }
-      app_g_button_back(overlay, lambda21);
     }
+    function lambda21() {
+      overlay_close();
+      let conversed = object_property_get(player, "conversed");
+      if (conversed) {
+        return;
+      }
+      let tutorial = html_div(div_map);
+      html_click_none(tutorial);
+      global_function_property_set(app_g_refresh, "tutorial", tutorial);
+      g_img_square_style_position(tutorial, player, "tutorial");
+      let text = emoji_pray();
+      html_text_set(tutorial, text);
+      const square_size = `calc(` + g_img_square_size_css() + `*.4)`;
+      html_style_assign(tutorial, {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        fontSize: square_size,
+        animation: "upDown 1.25s infinite ease-in-out alternate",
+      });
+    }
+    app_g_button_back(overlay, lambda21);
+  } else if (e2) {
+    app_g_conversation(
+      prayer,
+      npcs_matched,
+      overlay,
+      player,
+      game_prefix,
+      overlay_close,
+    );
+  } else {
   }
 }
