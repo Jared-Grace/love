24f60ca4cfcd947d5ab3a3e0d10b5829cfab3426
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:49:20 -0500

    function_auto_path ..\love\public\src\function_param_move_first.mjs

--- web/public/src/function_param_move_first.mjs
+++ web/public/src/function_param_move_first.mjs
@@ -1,6 +1,5 @@
 import { list_move_first } from "../../../love/public/src/list_move_first.mjs";
 import { function_param_index } from "../../../love/public/src/function_param_index.mjs";
-import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
@@ -10,7 +9,7 @@ export async function function_param_move_first(param_name) {
   let index = null;
   await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
-    let r = list_remove_at(args, index);
+    list_move_first(args, index);
   }
   function function_transform_current_lambda(ast) {
     let params = null;
