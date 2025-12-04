0331ff0891ab816702f6cc49c19c766d71f2c56f
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 21:48:37 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- public/src/app_g_conversation.mjs
+++ public/src/app_g_conversation.mjs
@@ -160,7 +160,7 @@ export function app_g_conversation(
         npc,
         overlay,
         game_prefix,
-        "Thank you! Glory to God! Praise God! Hallelujah! Amen!",
+        "Thank you! Glory to God! Praise God! Hallelujah! Amen! I believe!",
       );
     }
   }
