7fa3734c3ed6abcdf9996553f180857dc6f9af6b
Grace-Jared <christrosetolife@gmail.com>
2025-12-03 22:32:55 -0500

    p

--- web/public/src/prayer_start.mjs
+++ web/public/src/prayer_start.mjs
@@ -1,11 +1,11 @@
-import { trinity_name } from "../../../love/public/src/trinity_name.mjs";
+import { trinity_name_prayer } from "../../../love/public/src/trinity_name_prayer.mjs";
 import { emoji_bow } from "../../../love/public/src/emoji_bow.mjs";
 import { emoji_trinity } from "../../../love/public/src/emoji_trinity.mjs";
 export function prayer_start() {
   let v =
     emoji_bow() +
     " In the name" +
-    trinity_name() +
+    trinity_name_prayer() +
     " " +
     emoji_trinity() +
     " : ";
