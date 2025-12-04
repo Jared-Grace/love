4535fc51d838612c182cae31797d20e173d26fc1
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:55:09 -0500

    function_auto_path ..\love\public\src\global_function_property_nested_lambda.mjs

--- public/src/global_function_property_nested_lambda.mjs
+++ public/src/global_function_property_nested_lambda.mjs
@@ -1,6 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_lambda_async } from "../../../love/public/src/object_property_lambda_async.mjs";
-import { app_g_conversation } from "../../../love/public/src/app_g_conversation.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 export async function global_function_property_nested_lambda(
   fn,
@@ -9,7 +8,7 @@ export async function global_function_property_nested_lambda(
   lambda,
 ) {
   marker("1");
-  let value = global_function_property_get(app_g_conversation, property_name);
+  let value = global_function_property_get(fn, property_name);
   let v = await object_property_lambda_async(
     value,
     property_name_nested,
