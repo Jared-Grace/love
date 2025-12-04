9f5604ddd63e0d7e4e2409a9dcd1b1dc7e6b4711
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 18:47:16 -0500

    function_auto_path ..\love\public\src\function_param_move.mjs

--- public/src/function_param_move.mjs
+++ public/src/function_param_move.mjs
@@ -3,9 +3,9 @@ import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-export async function function_param_move(param_name, param_name_other) {
+export async function function_param_move(param_name) {
   marker("1");
-  assert_arguments(arguments, 2);
+  assert_arguments(arguments, 1);
   let index = null;
   await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
