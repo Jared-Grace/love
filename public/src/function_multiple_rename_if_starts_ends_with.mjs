b9f7f265fefb93b2778031972497f578964e6242
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:34:11 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_ends_with.mjs

--- public/src/function_multiple_rename_if_starts_ends_with.mjs
+++ public/src/function_multiple_rename_if_starts_ends_with.mjs
@@ -1,7 +1,7 @@
+import { string_suffix_change } from "../../../love/public/src/string_suffix_change.mjs";
 import { list_filter_starts_ends_with } from "../../../love/public/src/list_filter_starts_ends_with.mjs";
 import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-import { string_prefix_change } from "../../../love/public/src/string_prefix_change.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_rename_if_starts_ends_with(
   f_name_prefix,
@@ -20,7 +20,7 @@ export async function function_multiple_rename_if_starts_ends_with(
     return filtered;
   }
   function name_change(f_name_before) {
-    let together2 = string_prefix_change(
+    let together2 = string_suffix_change(
       f_name_before,
       f_name_prefix_before,
       f_name_prefix_after,
