fbdb6cbcc110ded88e192932722c8dc3b0efce3f
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:40:41 -0500

    function_auto_path ..\love\public\src\g_objection_generate_upload.mjs

---
+++ public/src/g_generate_upload_generic.mjs
@@ -0,0 +1,17 @@
+import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
+import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
+import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
+import { path_name } from "../../../love/public/src/path_name.mjs";
+import { path_base } from "../../../love/public/src/path_base.mjs";
+import { g_objection_generate_migrate_generic } from "../../../love/public/src/g_objection_generate_migrate_generic.mjs";
+export async function g_generate_upload_generic(path_get, fn) {
+  await g_objection_generate_migrate_generic(file_each);
+  async function file_each(file) {
+    let fb = path_base(file);
+    let chapter_code = path_name(fb);
+    let destination = path_get(chapter_code);
+    let path = local_function_path_json(chapter_code, fn);
+    let data = await file_read_json(path);
+    await firebase_upload_object(data, destination);
+  }
+}
