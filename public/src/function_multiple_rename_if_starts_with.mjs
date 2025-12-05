ff6bfef1739e39e210d4c75f5689e5330b6cb663
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:12:16 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_with.mjs

--- public/src/function_multiple_rename_if_starts_with.mjs
+++ public/src/function_multiple_rename_if_starts_with.mjs
@@ -5,5 +5,6 @@ export async function function_multiple_rename_if_starts_with(
   f_name_after,
 ) {
   marker("1");
-  return await function_rename(f_name_before, f_name_after);
+  let v = await function_rename(f_name_before, f_name_after);
+  return v;
 }
