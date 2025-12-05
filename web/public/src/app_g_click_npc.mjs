3933916d2eba1e5479137ab8894348a6bdc3220e
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 18:00:04 -0500

    function_auto_path ..\love\public\src\app_g_click_npc.mjs

--- web/public/src/app_g_click_npc.mjs
+++ web/public/src/app_g_click_npc.mjs
@@ -1,19 +1,13 @@
-import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
+import { app_g_tutorial } from "../../../love/public/src/app_g_tutorial.mjs";
 import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
 import { app_g_conversation } from "../../../love/public/src/app_g_conversation.mjs";
 import { app_g_container } from "../../../love/public/src/app_g_container.mjs";
 import { app_g_p_text } from "../../../love/public/src/app_g_p_text.mjs";
-import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_g_button_back } from "../../../love/public/src/app_g_button_back.mjs";
-import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
-import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
-import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
-import { html_div } from "../../../love/public/src/html_div.mjs";
 import { html_remove } from "../../../love/public/src/html_remove.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
@@ -57,23 +51,7 @@ export async function app_g_click_npc(
       let text = emoji_pray();
       const player_property = "conversed";
       const tutorial_property = "tutorial_converse";
-      let already = object_property_get(player, player_property);
-      if (not(already)) {
-        let tutorial = html_div(div_map);
-        html_click_none(tutorial);
-        global_function_property_set(app_g_main, tutorial_property, tutorial);
-        g_img_square_style_position(tutorial, player, "tutorial");
-        html_text_set(tutorial, text);
-        const square_size = `calc(` + g_img_square_size_css() + `*.4)`;
-        html_style_assign(tutorial, {
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          fontSize: square_size,
-          animation: "upDown 1.25s infinite ease-in-out alternate",
-        });
-      }
+      app_g_tutorial(player, player_property, div_map, tutorial_property, text);
     }
     app_g_button_back(overlay, lambda21);
   } else {
