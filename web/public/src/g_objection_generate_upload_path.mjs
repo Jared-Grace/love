52087c03735b7bf45a605e85093b71b4189bde77
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:42:52 -0500

    function_auto_path ..\love\public\src\g_objection_generate_upload_path.mjs

--- web/public/src/g_objection_generate_upload_path.mjs
+++ web/public/src/g_objection_generate_upload_path.mjs
@@ -1,11 +1,10 @@
+import { g_objection_generate_upload_path_generic } from "../../../love/public/src/g_objection_generate_upload_path_generic.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
-import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
-import { firebase_deploy_function_destination } from "../../../love/public/src/firebase_deploy_function_destination.mjs";
-import { path_join } from "../../../love/public/src/path_join.mjs";
 export function g_objection_generate_upload_path(chapter_code) {
   let f_name = fn_name("g_objection_generate_upload");
-  let file_name = file_name_json(chapter_code);
-  let joined2 = path_join(["uploads", file_name]);
-  let destination = firebase_deploy_function_destination(f_name, joined2);
+  let destination = g_objection_generate_upload_path_generic(
+    chapter_code,
+    f_name,
+  );
   return destination;
 }
