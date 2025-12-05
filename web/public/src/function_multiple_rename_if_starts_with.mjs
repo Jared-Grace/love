926230bc5a4720da8e87b4957b5d5725b43c3444
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:12:32 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_with.mjs

--- web/public/src/function_multiple_rename_if_starts_with.mjs
+++ web/public/src/function_multiple_rename_if_starts_with.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_rename_if_starts_with(
@@ -5,7 +6,7 @@ export async function function_multiple_rename_if_starts_with(
   f_name_after,
 ) {
   marker("1");
-  $ea;
+  await each_async(list, async function lambda(item) {});
   let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
