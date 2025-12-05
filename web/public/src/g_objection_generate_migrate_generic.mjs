13c6d32816391cfcdee9eea46f65c7a24070c55e
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:51:17 -0500

    de g_objection_generate_migrate_generic

--- web/public/src/g_objection_generate_migrate_generic.mjs
+++
@@ -1,6 +0,0 @@
-import { folder_user_storage_function_each } from "../../../love/public/src/folder_user_storage_function_each.mjs";
-import { g_objection_generate } from "../../../love/public/src/g_objection_generate.mjs";
-export async function g_objection_generate_migrate_generic(file_each) {
-  let fn = g_objection_generate;
-  await folder_user_storage_function_each(fn, file_each);
-}
