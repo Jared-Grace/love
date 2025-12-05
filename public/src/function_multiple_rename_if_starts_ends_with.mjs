82da9d287872fc01e35c1d9fea6ff819fce3c1bd
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:28:05 -0500

    pf list

--- public/src/function_multiple_rename_if_starts_ends_with.mjs
+++ public/src/function_multiple_rename_if_starts_ends_with.mjs
@@ -14,7 +14,7 @@ export async function function_multiple_rename_if_starts_ends_with(
   marker("1");
   function filter(f_names) {
     let filtered2 = list_filter_starts_with(f_names, f_name_suffix_before);
-    let filtered = list_filter_ends_with_any([f_name_suffix_after], filtered2);
+    let filtered = list_filter_ends_with_any(filtered2, [f_name_suffix_after]);
     return filtered2;
   }
   function name_change(f_name_before) {
