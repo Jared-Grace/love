cb53782ef87701208109fdb441655d4393229ef1
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 17:58:23 -0500

    function_auto_path ..\love\public\src\app_g_refresh.mjs

--- web/public/src/app_g_refresh.mjs
+++ web/public/src/app_g_refresh.mjs
@@ -28,7 +28,8 @@ export async function app_g_refresh(
   let { npcs } = map;
   marker("1");
   let ts = ["tutorial_converse"];
-  each(list, function lambda4(item) {});
+  function lambda4(item) {}
+  each(list, lambda4);
   global_function_property_set(app_g_refresh, "tutorial_converse", null);
   html_clear(div_map);
   let player = app_g_player_get();
