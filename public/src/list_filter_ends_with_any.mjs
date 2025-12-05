49dbef1a84995515ec4e5ebb350e05deab174e7a
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:27:56 -0500

    fs list_filter_ends_with_any

--- public/src/list_filter_ends_with_any.mjs
+++ public/src/list_filter_ends_with_any.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_any } from "../../../love/public/src/list_any.mjs";
 import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
 export function list_filter_ends_with_any(suffixes, list) {
+  marker("1");
   function lambda(item) {
     function lambda3(suffix) {
       let ew = string_ends_with(item, suffix);
