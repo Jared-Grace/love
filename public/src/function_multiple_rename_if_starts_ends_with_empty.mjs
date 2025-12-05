80797177bb1e631b4b18b5eb00e24df9818e5e69
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:35:18 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_ends_with_empty.mjs

--- public/src/function_multiple_rename_if_starts_ends_with_empty.mjs
+++ public/src/function_multiple_rename_if_starts_ends_with_empty.mjs
@@ -3,13 +3,12 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_rename_if_starts_ends_with_empty(
   f_name_prefix,
   f_name_suffix_before,
-  f_name_suffix_after,
 ) {
   marker("1");
   let v = await function_multiple_rename_if_starts_ends_with(
     f_name_prefix,
     f_name_suffix_before,
-    f_name_suffix_after,
+    "",
   );
   return v;
 }
