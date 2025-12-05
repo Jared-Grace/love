df455c7ac786d6cb687c759d1c5cdcdc6820a167
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 20:10:12 -0500

    function_auto_path ..\love\public\src\app_g_bible_passage_button.mjs

---
+++ public/src/app_g_passage_to_reference.mjs
@@ -0,0 +1,13 @@
+import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src/ebible_parts_chapter_code_to_reference.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export function app_g_passage_to_reference(passage, chapter_code, books) {
+  let verse_numbers = object_property_get(passage, "verse_numbers");
+  let text2 = object_property_get(passage, "text");
+  const reference = ebible_parts_chapter_code_to_reference(
+    chapter_code,
+    books,
+    verse_numbers,
+  );
+  const button_text = reference + " " + text2;
+  return button_text;
+}
