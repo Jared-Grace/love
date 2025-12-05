daf91fab14733746dead5883ce2898ab971aa33f
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:00:22 -0500

    rr object_property_lambda lambda initialize_lambda

--- web/public/src/global_function_property_lambda.mjs
+++ web/public/src/global_function_property_lambda.mjs
@@ -1,10 +1,14 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
-import { object_property_lambda } from "./object_property_lambda.mjs";
+import { object_property_initialize_lambda } from "../../../love/public/src/object_property_initialize_lambda.mjs";
 export function global_function_property_lambda(fn, property_name, lambda) {
   marker("1");
   const initial = {};
   let fn_object = global_function_initialize(fn, initial);
-  let value = object_property_lambda(fn_object, property_name, lambda);
+  let value = object_property_initialize_lambda(
+    fn_object,
+    property_name,
+    lambda,
+  );
   return value;
 }
