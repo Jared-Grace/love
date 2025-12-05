36086a8709ebe22ca1b10c7989d7932ca2542ff8
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:35:34 -0500

    function_auto_path ..\love\public\src\g_sermon_generate.mjs

--- public/src/g_sermon_generate.mjs
+++ public/src/g_sermon_generate.mjs
@@ -2,7 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { g_sermon_generate_bookundefined } from "../../../love/public/src/g_sermon_generate_bookundefined.mjs";
-export async function g_sermon_generateundefined() {
+export async function g_sermon_generate() {
   marker("1");
   const bible_folder = "engbsb";
   let books = await ebible_version_books(bible_folder);
