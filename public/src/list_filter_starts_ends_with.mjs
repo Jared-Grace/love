99b3d766dd76658a2ff7ed837e46cdf271695858
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:33:58 -0500

    function_auto_path ..\love\public\src\list_filter_starts_ends_with.mjs

--- public/src/list_filter_starts_ends_with.mjs
+++ public/src/list_filter_starts_ends_with.mjs
@@ -1,7 +1,7 @@
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 export function list_filter_starts_ends_with(list, prefix, suffix) {
-  let filtered2 = list_filter_starts_with(list, prefix);
-  let filtered = list_filter_ends_with(filtered2, suffix);
-  return filtered;
+  let filtered = list_filter_starts_with(list, prefix);
+  let filtered2 = list_filter_ends_with(filtered, suffix);
+  return filtered2;
 }
