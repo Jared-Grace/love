479ba6829da1a6fe4b67e9b649176ca32fd433f7
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 20:11:54 -0500

    r g_main_books app_g_main_books

--- public/src/g_main_books.mjs
+++
@@ -1,6 +0,0 @@
-import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
-import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
-export function g_main_books() {
-  let value = global_function_property_get(app_g_main, "books");
-  return value;
-}
