615d6d8b5dfce1ad850ed2470e9c8bb66af0e9a6
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:50:18 -0500

    function_auto_path ..\love\public\src\g_objection_generate_migrate_generic.mjs

--- public/src/g_objection_generate_migrate_generic.mjs
+++ public/src/g_objection_generate_migrate_generic.mjs
@@ -4,8 +4,7 @@ import { folder_read_paths_async } from "../../../love/public/src/folder_read_pa
 import { g_objection_generate } from "../../../love/public/src/g_objection_generate.mjs";
 export async function g_objection_generate_migrate_generic(file_each) {
   let fn = g_objection_generate;
-  let f = folder_user(folder);
-  const path = "D:\\user\\storage\\function\\" + fn.name;
+  let path = folder_user("\\storage\\function\\" + fn.name);
   let combineds = await folder_read_paths_async(path);
   await each_async(combineds, file_each);
 }
