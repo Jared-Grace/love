68537dd60bad6b9c0f2bddfbecbf6a82081f5521
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:28:05 -0500

    pf list

--- web/public/src/list_filter_ends_with_any.mjs
+++ web/public/src/list_filter_ends_with_any.mjs
@@ -2,7 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_any } from "../../../love/public/src/list_any.mjs";
 import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
-export function list_filter_ends_with_any(suffixes, list) {
+export function list_filter_ends_with_any(list, suffixes) {
   marker("1");
   function lambda(item) {
     function lambda3(suffix) {
