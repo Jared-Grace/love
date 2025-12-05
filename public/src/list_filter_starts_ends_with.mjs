e09580d9d2f0c820a5f77d2f54a3df1d8d7b7746
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:31:55 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_ends_with.mjs

---
+++ public/src/list_filter_starts_ends_with.mjs
@@ -0,0 +1,11 @@
+import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
+import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
+export function list_filter_starts_ends_with(
+  f_names,
+  f_name_suffix_before,
+  f_name_suffix_after,
+) {
+  let filtered2 = list_filter_starts_with(f_names, f_name_suffix_before);
+  let filtered = list_filter_ends_with(filtered2, f_name_suffix_after);
+  return filtered;
+}
