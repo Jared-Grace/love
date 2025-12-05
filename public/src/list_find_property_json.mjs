19305b44e192591b23f783c674de8c335cfef154
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 18:09:25 -0500

    function_auto_path ..\love\public\src\list_find_property_json.mjs

---
+++ public/src/list_find_property_json.mjs
@@ -0,0 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { object_property_equals_lambda } from "../../../love/public/src/object_property_equals_lambda.mjs";
+import { list_find } from "../../../love/public/src/list_find.mjs";
+export function list_find_property_json(list, property_name, property_value) {
+  marker("1");
+  let filter = object_property_equals_lambda(property_name, property_value);
+  let item = list_find(list, filter);
+  return item;
+}
