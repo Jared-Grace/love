bc0a2a518fe4d1f02d7b712c60676be3bc647336
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:13:34 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_with.mjs

--- public/src/function_multiple_rename_if_starts_with.mjs
+++ public/src/function_multiple_rename_if_starts_with.mjs
@@ -1,3 +1,4 @@
+import { list_empty_not_is_assert } from "../../../love/public/src/list_empty_not_is_assert.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
@@ -8,10 +9,11 @@ export async function function_multiple_rename_if_starts_with(
   f_name_after,
 ) {
   let f_names = await functions_names();
-  let filtered = list_filter_starts_with(list2, prefix);
+  let filtered = list_filter_starts_with(f_names, f_name_prefix);
+  list_empty_not_is_assert(list);
   marker("1");
   async function lambda(item) {}
-  await each_async(list, lambda);
+  await each_async(filtered, lambda);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
