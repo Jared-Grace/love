4c93cee3cb98c0c1d97eb4a62bfa7cddaeb05426
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 17:52:54 -0500

    function_auto_path ..\love\public\src\app_g_click_npc.mjs

--- web/public/src/app_g_click_npc.mjs
+++ web/public/src/app_g_click_npc.mjs
@@ -1,15 +1,6 @@
 import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
-import { app_g_passage_to_reference } from "../../../love/public/src/app_g_passage_to_reference.mjs";
-import { app_g_main_books } from "../../../love/public/src/app_g_main_books.mjs";
-import { app_g_container_text } from "../../../love/public/src/app_g_container_text.mjs";
-import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
-import { firebase_storage_download_json } from "../../../love/public/src/firebase_storage_download_json.mjs";
-import { app_g_chapter_code } from "../../../love/public/src/app_g_chapter_code.mjs";
-import { app_g_gospel } from "../../../love/public/src/app_g_gospel.mjs";
-import { global_function_property_nested_lambda } from "../../../love/public/src/global_function_property_nested_lambda.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
-import { list_remove_first } from "../../../love/public/src/list_remove_first.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
 import { app_g_conversation } from "../../../love/public/src/app_g_conversation.mjs";
@@ -94,29 +85,6 @@ export async function app_g_click_npc(
         emoji_book_open() + " You remember that you need to study!",
       );
       app_g_button_back(overlay, overlay_close);
-      let chapter_code = app_g_chapter_code();
-      async function lambda5() {
-        let destination = g_sermon_generate_upload_path(chapter_code);
-        let o = await firebase_storage_download_json(destination);
-        return o;
-      }
-      let s = await global_function_property_nested_lambda(
-        app_g_gospel,
-        "sermons",
-        chapter_code,
-        lambda5,
-      );
-      let r = list_remove_first(review);
-      let passages = object_property_get(s, "passages");
-      let text2 = object_property_get(s, "text");
-      app_g_container_text(overlay, text2);
-      let books = app_g_main_books();
-      const text = app_g_passage_to_reference(s, chapter_code, books);
-      app_g_container_text(overlay, text);
-      app_g_container_text(
-        overlay,
-        "What would you like to say about this Bible passage?",
-      );
     } else {
       app_g_conversation(
         prayer,
