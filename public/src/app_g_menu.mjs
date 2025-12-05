db2d68ccb3700243220ec612fec20ab666851c23
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 23:11:40 -0500

    function_auto_path ..\love\public\src\app_g_menu.mjs

--- public/src/app_g_menu.mjs
+++ public/src/app_g_menu.mjs
@@ -36,6 +36,8 @@ export function app_g_menu(overlay, player) {
   }
   app_g_button_uncolored(overlay, text, lambda7);
   let text2 = emoji_book_open() + " Study";
-  function lambda() {}
+  function lambda() {
+    html_clear(overlay);
+  }
   app_g_button_uncolored(overlay, text2, lambda);
 }
