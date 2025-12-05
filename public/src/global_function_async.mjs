a52368688f65380a71aaa45d51cff55a710c6cc5
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:00:22 -0500

    rr object_property_lambda lambda initialize_lambda

--- public/src/global_function_async.mjs
+++ public/src/global_function_async.mjs
@@ -1,10 +1,10 @@
-import { object_property_lambda } from "../../../love/public/src/object_property_lambda.mjs";
+import { object_property_initialize_lambda } from "../../../love/public/src/object_property_initialize_lambda.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function global_function_async(fn, lambda) {
   let global = global_get();
   marker("1");
-  let value = object_property_lambda(global, fn.name, lambda);
+  let value = object_property_initialize_lambda(global, fn.name, lambda);
   let awaited = await value;
   return awaited;
 }
