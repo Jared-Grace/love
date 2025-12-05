b5c899dd4b39f4659d0b49492aafb7c7841d0797
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:12:13 -0500

    fw r function_multiple_rename_if_starts_with

--- public/src/function_multiple_rename_if_starts_with.mjs
+++ public/src/function_multiple_rename_if_starts_with.mjs
@@ -1,1 +1,9 @@
-export function function_multiple_rename_if_starts_with() {}
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_multiple_rename_if_starts_with(
+  f_name_before,
+  f_name_after,
+) {
+  marker("1");
+  return await function_rename(f_name_before, f_name_after);
+}
