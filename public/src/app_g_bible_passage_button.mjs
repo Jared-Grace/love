e68cdc2e075a350a5e2f729991cc5879d50501ac
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 20:10:12 -0500

    function_auto_path ..\love\public\src\app_g_bible_passage_button.mjs

--- public/src/app_g_bible_passage_button.mjs
+++ public/src/app_g_bible_passage_button.mjs
@@ -1,7 +1,6 @@
-import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src/ebible_parts_chapter_code_to_reference.mjs";
+import { app_g_passage_to_reference } from "../../../love/public/src/app_g_passage_to_reference.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_g_button_green } from "../../../love/public/src/app_g_button_green.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_g_bible_passage_button(
   passage,
   chapter_code,
@@ -10,14 +9,7 @@ export function app_g_bible_passage_button(
   lambda,
 ) {
   marker("1");
-  let verse_numbers = object_property_get(passage, "verse_numbers");
-  let text2 = object_property_get(passage, "text");
-  const reference = ebible_parts_chapter_code_to_reference(
-    chapter_code,
-    books,
-    verse_numbers,
-  );
-  const button_text = reference + " " + text2;
+  const button_text = app_g_passage_to_reference(passage, chapter_code, books);
   let b = app_g_button_green(overlay, button_text, lambda);
   return b;
 }
