c1656062a112542a831a8b19192faed792324b11
Grace-Jared <christrosetolife@gmail.com>
2025-12-04 19:00:20 -0500

    function_auto_path ..\love\public\src\object_property_initialize_lambda.mjs

---
+++ web/public/src/object_property_initialize_lambda.mjs
@@ -0,0 +1,19 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+export function object_property_initialize_lambda(
+  object,
+  property_name,
+  lambda,
+) {
+  marker("1");
+  const exists = object_property_exists(object, property_name);
+  if (not(exists)) {
+    let value_set = lambda();
+    object_property_set(object, property_name, value_set);
+  }
+  let value = object_property_get(object, property_name);
+  return value;
+}
