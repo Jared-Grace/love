333cab65dbeea4ef29492dbc2167a1ef68b2f193
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 20:05:15 -0500

    function_auto_path ..\love\public\src\string_split_multiple.mjs

--- public/src/string_split_multiple.mjs
+++ public/src/string_split_multiple.mjs
@@ -1,16 +1,11 @@
-import { list_join } from "../../../love/public/src/list_join.mjs";
-import { string_split } from "../../../love/public/src/string_split.mjs";
-import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function string_split_multiple(str, delimiters) {
-  marker("1");
-  let { first, remaining } = list_first_remaining(delimiters);
-  function lambda(delimiter) {
-    let split = string_split(str, delimiter);
-    str = list_join(split, first);
+  function lambda(d) {
+    let v = d.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    return v;
   }
-  each(remaining, lambda);
-  let parts = string_split(str, first);
-  return parts;
+  const escaped = delimiters.map(lambda);
+  let v3 = escaped.join("|");
+  const regex = new RegExp(v3, "g");
+  let v2 = str.split(regex);
+  return v2;
 }
