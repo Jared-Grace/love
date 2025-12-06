5a8aaac374c1d65ab18528cd20852658a181d02e
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 00:02:55 -0500

    function_auto_path ..\love\public\src\invoke_once.mjs

--- public/src/invoke_once.mjs
+++ public/src/invoke_once.mjs
@@ -1,7 +1,8 @@
 export function invoke_once(fn) {
   let invoked = false;
   function invoke_once_lambda() {
-    if (false) {
+    if (invoked) {
+      return null;
     }
   }
   return invoke_once_lambda;
