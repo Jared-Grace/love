a5981762767ebf6b43c67de44ef0d87259cf17c6
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 20:00:32 -0500

    function_auto_path ..\love\public\src\string_split_multiple.mjs

--- public/src/string_split_multiple.mjs
+++ public/src/string_split_multiple.mjs
@@ -1,10 +1,13 @@
+import { string_split } from "../../../love/public/src/string_split.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_split_multiple(str, delimiters) {
   marker("1");
   let { first, remaining } = list_first_remaining(delimiters);
-  function lambda(delimiter) {}
-  each(delimiters, lambda);
+  function lambda(delimiter) {
+    let split = string_split(s, separator);
+  }
+  each(remaining, lambda);
   return parts;
 }
