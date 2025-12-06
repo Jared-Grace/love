1b3d2c7eb90924b84a95119dcc68bbd24fa53bf9
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:21:42 -0500

    function_auto_path ..\love\public\src\app_g_bible_passage_button.mjs

--- public/src/app_g_bible_passage_button.mjs
+++ public/src/app_g_bible_passage_button.mjs
@@ -12,6 +12,6 @@ export function app_g_bible_passage_button(
   marker("1");
   const button_text = app_g_passage_to_reference(passage, chapter_code, books);
   let b = app_g_button_green(overlay, button_text, lambda);
-  html_style_set(b2, style_key, style_value);
+  html_style_set(b2, "font-weight", "bold");
   return b;
 }
