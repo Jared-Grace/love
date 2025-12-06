61bdd573fd8c80ef6fb682b0bc806e19286682d1
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:57:07 -0500

    function_auto_path ..\love\public\src\string_split_multiple.mjs

--- public/src/string_split_multiple.mjs
+++ public/src/string_split_multiple.mjs
@@ -1,19 +1,7 @@
-import { log_json } from "../../../love/public/src/log_json.mjs";
+import { each } from "../../../love/public/src/each.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_split_multiple(str, delimiters) {
   marker("1");
-  function lambda(d) {
-    let v = d.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-    return v;
-  }
-  const escaped = delimiters.map(lambda);
-  let v2 = escaped.join("");
-  const regex = new RegExp(`[${v2}]`);
-  const parts = str.split(regex);
-  log_json({
-    str,
-    delimiters,
-    parts,
-  });
+  each(list, function lambda(item) {});
   return parts;
 }
