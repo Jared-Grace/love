ebd01ba9ea7e22897823ee4d3caaa9cd16ddc561
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 23:05:17 -0500

    function_auto_path ..\love\public\src\app_g_click.mjs

--- public/src/app_g_click.mjs
+++ public/src/app_g_click.mjs
@@ -40,12 +40,13 @@ export async function app_g_click(
   let tile = html_component_wrap(tile_e);
   let json = html_data_get(tile, "coordinates");
   let clicked_coordinates = json_from(json);
-  function lambda2() {}
-  g_tutorials_each(lambda2);
-  let tutorial = global_function_property_get(app_g_main, "tutorial_converse");
-  if (equal_not(tutorial, null)) {
-    html_remove(tutorial);
+  function lambda2(tutorial) {
+    let value = global_function_property_get(app_g_main, tutorial);
+    if (equal_not(value, null)) {
+      html_remove(value);
+    }
   }
+  g_tutorials_each(lambda2);
   let distance2 = g_distance(player, clicked_coordinates);
   if (equal(distance2, 0)) {
     let overlay = app_g_overlay(div_map);
