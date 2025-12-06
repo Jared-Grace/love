8b89a7f7124a866618e495f92e164a5d35db81e9
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 20:24:48 -0500

    function_auto_path ..\love\public\src\app_g_gospel.mjs

--- public/src/app_g_gospel.mjs
+++ public/src/app_g_gospel.mjs
@@ -123,7 +123,11 @@ export async function app_g_gospel(
               ", the Son of" + string_random_or_empty(" the living") + " God",
             ),
         ),
-      "I " + "confess" + " " + jesus_christ() + " as my Lord and Savior",
+      "I " +
+        list_random_item(["confess", "recieve"]) +
+        " " +
+        jesus_christ() +
+        " as my Lord and Savior",
     ]);
     const blessing =
       "God bless you " +
