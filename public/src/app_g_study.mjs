b9e0e252f76e92464b6b986fc42a7205da39638d
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:12:24 -0500

    data_file_update ..\love\public\src\app_g_study.mjs

--- public/src/app_g_study.mjs
+++ public/src/app_g_study.mjs
@@ -58,7 +58,7 @@ export function app_g_study(player, overlay) {
       );
       let books = app_g_main_books();
       const text = app_g_passage_to_reference(passage, chapter_code, books);
-      let c = app_g_container_text(overlay, text);
+      let c = app_g_container_text(overlay, text);html_style_background_color
       app_g_container_text(
         overlay,
         "If you were preaching from this Bible passage, what would you say?",
