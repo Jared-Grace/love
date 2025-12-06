4e9c8c771bde88de3ce63ceb57389030d1640537
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 18:57:29 -0500

    function_auto_path ..\love\public\src\app_reply_choices_give.mjs

--- public/src/app_reply_choices_give.mjs
+++ public/src/app_reply_choices_give.mjs
@@ -2,6 +2,6 @@ import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 export function app_reply_choices_give() {
   let v2 =
     emoji_pray() +
-    " Sorry, I have nothing to give you at this time. According to the desire of the LORD: May the LORD provide and may the LORD lead you to someone who will abundantly provide whatever you ask the same day you ask.";
+    " Sorry, I have nothing extra to give you at this time. According to the desire of the LORD: May the LORD provide and may the LORD lead you to someone who will abundantly provide whatever you ask the same day you ask.";
   return v2;
 }
