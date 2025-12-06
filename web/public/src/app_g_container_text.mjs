86dd375135c4c45a6a814041074ae564b1579c5b
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:13:35 -0500

    function_auto_path ..\love\public\src\app_g_container_text.mjs

--- web/public/src/app_g_container_text.mjs
+++ web/public/src/app_g_container_text.mjs
@@ -3,4 +3,5 @@ import { app_g_container } from "../../../love/public/src/app_g_container.mjs";
 export function app_g_container_text(overlay, text) {
   let choices = app_g_container(overlay);
   app_g_p_text(choices, text);
+  return choices;
 }
