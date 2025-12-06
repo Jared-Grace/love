0acfab36b86aada5e044ce767579277f58cad0e1
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:49:50 -0500

    data_file_update ..\love\public\src\string_split_multiple.mjs

--- public/src/string_split_multiple.mjs
+++ public/src/string_split_multiple.mjs
@@ -1,4 +1,8 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function string_split_multiple() {
-  marker("1");
+  marker("1");const escaped = delimiters.map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+const regex = new RegExp(`[${escaped.join('')}]`);
+
+// Split string
+const parts = str.split(regex);
 }
