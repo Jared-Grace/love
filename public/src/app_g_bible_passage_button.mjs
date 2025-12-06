51f1cb745a8295359243d96a6daca10d5dd9d447
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:21:26 -0500

    function_auto_path ..\love\public\src\app_g_bible_passage_button.mjs

--- public/src/app_g_bible_passage_button.mjs
+++ public/src/app_g_bible_passage_button.mjs
@@ -1,3 +1,4 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { app_g_passage_to_reference } from "../../../love/public/src/app_g_passage_to_reference.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_g_button_green } from "../../../love/public/src/app_g_button_green.mjs";
@@ -11,5 +12,6 @@ export function app_g_bible_passage_button(
   marker("1");
   const button_text = app_g_passage_to_reference(passage, chapter_code, books);
   let b = app_g_button_green(overlay, button_text, lambda);
+  html_style_set(b2, style_key, style_value);
   return b;
 }
