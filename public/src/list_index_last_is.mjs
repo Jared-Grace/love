dc2ae4a53d0468375f414b9bba16b3f38f598417
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 20:41:49 -0500

    function_auto_path ..\love\public\src\list_index_last_is.mjs

--- public/src/list_index_last_is.mjs
+++ public/src/list_index_last_is.mjs
@@ -1,6 +1,6 @@
 import { list_index_last } from "../../../love/public/src/list_index_last.mjs";
-export function list_index_last_is(sermon_correct_list, sermon_index) {
-  let index_last = list_index_last(sermon_correct_list);
-  let li = sermon_index === index_last;
+export function list_index_last_is(list, index) {
+  let index_last = list_index_last(list);
+  let li = index === index_last;
   return li;
 }
