066bb4b2af8fcecb619a6b41da558ae542faa35a
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:50:51 -0500

    r string_split_newline_windows_escaped app_g_openai_split

--- public/src/string_split_newline_windows_escaped.mjs
+++
@@ -1,9 +0,0 @@
-import { newline_windows } from "../../../love/public/src/newline_windows.mjs";
-import { string_split_multiple } from "../../../love/public/src/string_split_multiple.mjs";
-import { newline_windows_escaped } from "../../../love/public/src/newline_windows_escaped.mjs";
-export function string_split_newline_windows_escaped(objections) {
-  let separator = newline_windows_escaped();
-  let n = newline_windows();
-  let split = string_split_multiple(objections, [separator, n]);
-  return split;
-}
