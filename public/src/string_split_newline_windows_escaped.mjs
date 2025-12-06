2b4f36a575b937cebc7774016543cc50c72515b9
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:50:22 -0500

    function_auto_path ..\love\public\src\string_split_newline_windows_escaped.mjs

--- public/src/string_split_newline_windows_escaped.mjs
+++ public/src/string_split_newline_windows_escaped.mjs
@@ -1,7 +1,9 @@
+import { newline_windows } from "../../../love/public/src/newline_windows.mjs";
 import { string_split_multiple } from "../../../love/public/src/string_split_multiple.mjs";
 import { newline_windows_escaped } from "../../../love/public/src/newline_windows_escaped.mjs";
 export function string_split_newline_windows_escaped(objections) {
   let separator = newline_windows_escaped();
-  let split = string_split_multiple(objections, separator);
+  let n = newline_windows();
+  let split = string_split_multiple(objections, [separator]);
   return split;
 }
