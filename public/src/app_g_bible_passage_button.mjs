41b3d7921dd47275cdf081bd9a7cd2340f33307a
Grace-Jared <christrosetolife@gmail.com>
2025-12-05 19:22:02 -0500

    function_auto_path ..\love\public\src\app_g_bible_passage_button.mjs

--- public/src/app_g_bible_passage_button.mjs
+++ public/src/app_g_bible_passage_button.mjs
@@ -1,4 +1,4 @@
-import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+import { html_bold } from "../../../love/public/src/html_bold.mjs";
 import { app_g_passage_to_reference } from "../../../love/public/src/app_g_passage_to_reference.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_g_button_green } from "../../../love/public/src/app_g_button_green.mjs";
@@ -12,6 +12,6 @@ export function app_g_bible_passage_button(
   marker("1");
   const button_text = app_g_passage_to_reference(passage, chapter_code, books);
   let b = app_g_button_green(overlay, button_text, lambda);
-  html_style_set(b2, "font-weight", "bold");
+  html_bold(b);
   return b;
 }
