19c9003be9060a6710efa7ddd25a2ca5283b5f0d
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:25:34 -0500

    function_auto_path ..\love\public\src\g_themes_verb.mjs

--- web/public/src/g_themes_verb.mjs
+++ web/public/src/g_themes_verb.mjs
@@ -1,7 +1,8 @@
+import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function g_themes_verb(root) {
   marker("1");
   let r = [root, root + "s", root + "ing"];
-  let v = "tell tells telling";
+  let joined = list_join_space(args);
   return v;
 }
