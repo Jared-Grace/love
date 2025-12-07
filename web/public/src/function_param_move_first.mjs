07f046233fd221a5c597d78ec47863f33c61340e
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 20:02:50 -0500

    function_auto_path ..\love\public\src\function_param_move_first.mjs

--- web/public/src/function_param_move_first.mjs
+++ web/public/src/function_param_move_first.mjs
@@ -1,20 +1,10 @@
+import { function_param_move_generic } from "../../../love/public/src/function_param_move_generic.mjs";
 import { list_move_first } from "../../../love/public/src/list_move_first.mjs";
-import { function_param_index } from "../../../love/public/src/function_param_index.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export async function function_param_move_first(param_name) {
   marker("1");
   assert_arguments(arguments, 1);
   let fn = list_move_first;
-  let index = null;
-  await function_params_new_generic(function_transform_current_lambda, on_call);
-  function on_call(args) {
-    fn(args, index);
-  }
-  function function_transform_current_lambda(ast) {
-    let params = null;
-    ({ params, index } = function_param_index(ast, param_name));
-    fn(params, index);
-  }
+  await function_param_move_generic(fn, param_name);
 }
