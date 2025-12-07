6ad77d4b50d50c51dbc1b5d4dd944a5bee611e54
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 20:08:32 -0500

    function_param_move_last value_not

--- public/src/object_property_get_or.mjs
+++ public/src/object_property_get_or.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-export function object_property_get_or(value_not, obj, key) {
+export function object_property_get_or(obj, key, value_not) {
   marker("1");
   let value = value_not;
   let exists = object_property_exists(obj, key);
