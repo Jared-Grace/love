59144acc054782431927dca62eee61e4ba82a0c1
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:25:23 -0500

    function_auto_path ..\love\public\src\g_themes_verb.mjs

--- public/src/g_themes_verb.mjs
+++ public/src/g_themes_verb.mjs
@@ -1,6 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function g_themes_verb(root) {
   marker("1");
+  let r = [root, root + "s", root + "ing"];
   let v = "tell tells telling";
   return v;
 }
