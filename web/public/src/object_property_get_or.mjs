c4ffb06b229edf9ff7968a58127c2bbe532cc9ad
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 20:08:21 -0500

    fs object_property_get_or

--- web/public/src/object_property_get_or.mjs
+++ web/public/src/object_property_get_or.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 export function object_property_get_or(value_not, obj, key) {
+  marker("1");
   let value = value_not;
   let exists = object_property_exists(obj, key);
   if (exists) {
