4faac9aab152a008ee10a225e45416594cb7f06e
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:25:50 -0500

    function_auto_path ..\love\public\src\list_join_space.mjs

--- public/src/g_themes_verb.mjs
+++ public/src/g_themes_verb.mjs
@@ -2,7 +2,7 @@ import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function g_themes_verb(root) {
   marker("1");
-  let r = [root, root + "s", root + "ing"];
-  let joined = list_join_space(args);
-  return v;
+  let list = [root, root + "s", root + "ing"];
+  let r = list_join_space(list);
+  return r;
 }
