bcf66d2262e9b946c19ae21e5433a06931be9430
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:28:56 -0500

    function_auto_path ..\love\public\src\g_themes_verb.mjs

--- public/src/g_themes_verb.mjs
+++ public/src/g_themes_verb.mjs
@@ -1,10 +1,14 @@
+import { string_take_less_1 } from "../../../love/public/src/string_take_less_1.mjs";
 import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function g_themes_verb(root) {
   marker("1");
+  let root_ing = root;
   let ew = string_ends_with(root, "e");
-  if (false) {
+  if (ew) {
+    root_ing = list;
+    let typed2 = string_take_less_1(typed);
   }
   let list = [root, root + "s", root + "ing"];
   let r = list_join_space(list);
