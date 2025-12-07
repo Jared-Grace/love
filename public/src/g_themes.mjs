1bee3c8e7c6eddbd4d916acbfb638b035b0a781f
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 20:13:08 -0500

    function_auto_path ..\love\public\src\app_g_wrong.mjs

---
+++ public/src/g_themes.mjs
@@ -0,0 +1,20 @@
+import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
+import { list_unique } from "../../../love/public/src/list_unique.mjs";
+export function g_themes(concated) {
+  let words_correct = list_unique(concated);
+  let themes = {
+    evil: "morality",
+    good: "morality",
+  };
+  function lambda(w) {
+    let theme = object_property_get_or(themes, w, null);
+    return theme;
+  }
+  let mapped = list_map(words_correct, lambda);
+  let themes_correct = list_filter(mapped, null_not_is);
+  themes_correct = list_unique(themes_correct);
+  return themes_correct;
+}
