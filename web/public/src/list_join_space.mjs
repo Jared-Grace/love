8a95f9976d6d52626c79a4e2d082886bed053895
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:25:50 -0500

    function_auto_path ..\love\public\src\list_join_space.mjs

--- web/public/src/list_join_space.mjs
+++ web/public/src/list_join_space.mjs
@@ -1,5 +1,5 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
-export function list_join_space(args) {
-  let joined = list_join(args, " ");
+export function list_join_space(list) {
+  let joined = list_join(list, " ");
   return joined;
 }
