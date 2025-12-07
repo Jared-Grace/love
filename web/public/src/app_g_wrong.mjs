4ab5ae57f95f23178ad664423a8fbdf854f334e7
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 20:13:08 -0500

    function_auto_path ..\love\public\src\app_g_wrong.mjs

--- web/public/src/app_g_wrong.mjs
+++ web/public/src/app_g_wrong.mjs
@@ -1,7 +1,4 @@
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
+import { g_themes } from "../../../love/public/src/g_themes.mjs";
 import { app_g_openai_split } from "../../../love/public/src/app_g_openai_split.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
@@ -9,7 +6,6 @@ import { integer_random_0 } from "../../../love/public/src/integer_random_0.mjs"
 import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_intersect } from "../../../love/public/src/list_intersect.mjs";
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
 import { string_to_words } from "../../../love/public/src/string_to_words.mjs";
@@ -23,18 +19,7 @@ export function app_g_wrong(passage, passages, property) {
   let ob = list_random_item(split);
   let words2 = string_to_words(ob);
   let concated = list_concat(words, words2);
-  let words_correct = list_unique(concated);
-  let themes = {
-    evil: "morality",
-    good: "morality",
-  };
-  function lambda(w) {
-    let theme = object_property_get_or(themes, w, null);
-    return theme;
-  }
-  let mapped = list_map(words_correct, lambda);
-  let themes_correct = list_filter(mapped, null_not_is);
-  themes_correct = list_unique(themes_correct);
+  let themes_correct = g_themes(concated);
   function lambda2(p) {
     let text_candidate = object_property_get(p, "text");
     let words_candidate = string_to_words(text_candidate);
