2485e0326c0cbae67ca62dc1b89c581ad6e06447
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:29:05 -0500

    function_auto_path ..\love\public\src\string_take_less_1.mjs

--- public/src/string_take_less_1.mjs
+++ public/src/string_take_less_1.mjs
@@ -1,7 +1,7 @@
 import { string_take } from "../../../love/public/src/string_take.mjs";
 import { string_size_less_1 } from "../../../love/public/src/string_size_less_1.mjs";
-export function string_take_less_1(typed) {
-  const sz1 = string_size_less_1(typed);
-  typed = string_take(typed, sz1);
-  return typed;
+export function string_take_less_1(s) {
+  const sz1 = string_size_less_1(s);
+  s = string_take(s, sz1);
+  return s;
 }
