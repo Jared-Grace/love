5d38115709ab7c3e132dbc07e59bfa99c62e111f
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 21:28:22 -0500

    function_auto_path ..\love\public\src\g_themes_verb.mjs

--- public/src/g_themes_verb.mjs
+++ public/src/g_themes_verb.mjs
@@ -1,7 +1,9 @@
+import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function g_themes_verb(root) {
   marker("1");
+  let ew = string_ends_with(s, suffix);
   let list = [root, root + "s", root + "ing"];
   let r = list_join_space(list);
   return r;
