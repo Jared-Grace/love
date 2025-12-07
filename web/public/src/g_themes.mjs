125c4fc799148f9e613080a7791081cefe551f5e
Grace-Jared <christrosetolife@gmail.com>
2025-12-06 20:26:55 -0500

    function_auto_path ..\love\public\src\g_themes.mjs

--- web/public/src/g_themes.mjs
+++ web/public/src/g_themes.mjs
@@ -1,11 +1,9 @@
-import { string_letters_is } from "../../../love/public/src/string_letters_is.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_property_get_or } from "../../../love/public/src/object_property_get_or.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 export function g_themes(text) {
-  let li = string_letters_is(str);
   let themes = {
     evil: "morality",
     good: "morality",
