efeb1a7ed54a00df6d8c827e83317c72cac51760
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:44:24 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- web/public/src/app_a_main.mjs
+++ web/public/src/app_a_main.mjs
@@ -1,9 +1,11 @@
-import { http } from "../../../love/public/src/http.mjs";
+import { http_generic } from "../../../love/public/src/http_generic.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_main() {
   marker("1");
-  let v = await http("/api/time");
+  let v = await http_generic("/api", {
+    test: 123,
+  });
   log({
     v,
   });
