06ea5bb64cc0294fe0009c0d91ef5d0ae0c2d733
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:34:06 -0500

    function_auto_path ..\love\public\src\server.mjs

---
+++ web/public/src/server.mjs
@@ -0,0 +1,22 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
+import { log_keep } from "../../../love/public/src/log_keep.mjs";
+import { path_join } from "../../../love/public/src/path_join.mjs";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+export function server() {
+  marker("1");
+  const app = express();
+  const port = 8080;
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  let previous = folder_previous();
+  let result2 = path_join([__dirname, previous, previous, previous]);
+  let v = express.static(result2);
+  app.use(v);
+  function lambda() {
+    log_keep(`Static server running at http://localhost:${port}`);
+  }
+  app.listen(port, lambda);
+}
