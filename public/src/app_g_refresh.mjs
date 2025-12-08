cdfa65b75affa8c8ab57cc0e14e71f8b56377b27
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 20:10:36 -0500

    function_auto_path ..\love\public\src\app_g_refresh.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -27,7 +27,7 @@ export async function app_g_refresh(
 ) {
   html_clear(div_map);
   async function refresh() {
-    await app_g_refresh(div_map2, game_prefix2, tiles_path2, rows2, map2);
+    await app_g_refresh(div_map, game_prefix, tiles_path, rows, map);
   }
   let { npcs } = map;
   marker("1");
