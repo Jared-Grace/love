e5e78e492b32f67f03030dcd24c3b79ec7a35316
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:02:37 -0500

    function_auto_path ..\love\public\src\app_g_tutorial.mjs

--- public/src/app_g_tutorial.mjs
+++ public/src/app_g_tutorial.mjs
@@ -1,11 +1,8 @@
+import { g_icon } from "../../../love/public/src/g_icon.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
-import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
-import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
 import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
-import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
-import { html_div } from "../../../love/public/src/html_div.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_g_tutorial(
@@ -17,10 +14,7 @@ export function app_g_tutorial(
 ) {
   let already = object_property_get(player, player_property);
   if (not(already)) {
-    let tutorial = html_div(div_map);
-    html_click_none(tutorial);
-    g_img_square_style_position(tutorial, player, "tutorial");
-    html_text_set(tutorial, emoji);
+    let tutorial = g_icon(div_map, player, emoji);
     global_function_property_set(app_g_main, tutorial_property, tutorial);
     const square_size = `calc(` + g_img_square_size_css() + `*.4)`;
     html_style_assign(tutorial, {
