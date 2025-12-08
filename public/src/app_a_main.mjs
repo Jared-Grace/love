c27bc5419bd0fd258460d941fabf9e1cdd10e25e
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 18:01:03 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -1,13 +1,12 @@
 import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { http_generic } from "../../../love/public/src/http_generic.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_main() {
   marker("1");
-  let json = json_to({
+  let json = {
     test: 123,
-  });
+  };
   let buffer = await http_generic("/api", {
     method: "POST",
     body: json,
