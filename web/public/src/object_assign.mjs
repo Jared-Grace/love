0af76673507817a75918bdfad337e5e989d5b6b5
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 18:00:27 -0500

    function_auto_path ..\love\public\src\object_assign.mjs

--- web/public/src/object_assign.mjs
+++ web/public/src/object_assign.mjs
@@ -1,6 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function object_assign(to, from) {
   marker("1");
-  let a = Object.assign(to, from);
-  return a;
+  Object.assign(to, from);
 }
