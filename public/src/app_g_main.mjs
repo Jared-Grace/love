43958d1ce48506bf3a82a6d207de8e5811ac4dd6
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 23:10:54 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -1,3 +1,4 @@
+import { app_g_map_generate } from "../../../love/public/src/app_g_map_generate.mjs";
 import { html_hide_loadable } from "../../../love/public/src/html_hide_loadable.mjs";
 import { g_icon_cross_unpositioned } from "../../../love/public/src/g_icon_cross_unpositioned.mjs";
 import { g_tutorials_each } from "../../../love/public/src/g_tutorials_each.mjs";
@@ -26,7 +27,6 @@ import { list_size } from "../../../love/public/src/list_size.mjs";
 import { mod } from "../../../love/public/src/mod.mjs";
 import { bible_names_men } from "../../../love/public/src/bible_names_men.mjs";
 import { bible_names_women } from "../../../love/public/src/bible_names_women.mjs";
-import { g_tiles_grasses } from "../../../love/public/src/g_tiles_grasses.mjs";
 import { app_g_refresh } from "../../../love/public/src/app_g_refresh.mjs";
 import { html_font_san_serif_value } from "../../../love/public/src/html_font_san_serif_value.mjs";
 import { html_document_root } from "../../../love/public/src/html_document_root.mjs";
@@ -36,10 +36,8 @@ import { list_map_combine_left } from "../../../love/public/src/list_map_combine
 import { range_1 } from "../../../love/public/src/range_1.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
-import { list_take } from "../../../love/public/src/list_take.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
-import { each_range } from "../../../love/public/src/each_range.mjs";
 import { g_folder_tiles } from "../../../love/public/src/g_folder_tiles.mjs";
 import { g_folder_img } from "../../../love/public/src/g_folder_img.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
@@ -95,36 +93,7 @@ export async function app_g_main() {
   });
   await html_scroll_none(div_map_container);
   const tiles_path = g_folder_tiles(path_prefix);
-  let grasses = g_tiles_grasses();
-  list_shuffle(grasses);
-  let taken = list_take(grasses, 3);
-  function lambda5(la) {
-    function lambda3(g, index) {
-      function lambda4(i3) {
-        la(g);
-      }
-      each_range(index + 1, lambda4);
-    }
-    each_index(taken, lambda3);
-  }
-  let tiles = list_adder(lambda5);
-  let row_count = 15;
-  let column_count = row_count;
-  function lambda10(la2) {
-    function lambda6(i) {
-      function lambda11(la3) {
-        function lambda9(i2) {
-          let r = list_random_item(tiles);
-          la3(r);
-        }
-        each_range(column_count, lambda9);
-      }
-      let list2 = list_adder(lambda11);
-      la2(list2);
-    }
-    each_range(row_count, lambda6);
-  }
-  let rows = list_adder(lambda10);
+  let rows = app_g_map_generate();
   let r2 = range_1(10);
   let imgs_men = list_map_combine_left("man_", r2);
   let imgs_women = list_map_combine_left("woman_", r2);
