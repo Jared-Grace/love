a9e3ad0792ba9f70b7a230bf076ae77e1815ec8d
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 23:23:34 -0500

    function_auto_path ..\love\public\src\list_without.mjs

--- public/src/list_without.mjs
+++ public/src/list_without.mjs
@@ -1,9 +1,9 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-export function list_without(list, right) {
-  function lambda12(item) {
-    let b = equal(item, right);
+export function list_without(list, item) {
+  function lambda12(other) {
+    let b = equal(other, item);
     let eq2 = not(b);
     return eq2;
   }
