50f0e92749dc19245570143c07d49dbab941c9f9
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 20:38:38 -0500

    function_auto_path ..\love\public\src\g_themes_verb.mjs

--- public/src/g_themes_verb.mjs
+++ public/src/g_themes_verb.mjs
@@ -10,8 +10,8 @@ export function g_themes_verb(root) {
   if (ew) {
     root_ing = string_take_less_1(root_ing);
   }
-  let list = [root, root + "s"][root_ing + "ing"];
-  list_add(list2, item);
+  let list = [root, root + "s"];
+  list_add(list, root_ing + "ing");
   let r = list_join_space(list);
   return r;
 }
