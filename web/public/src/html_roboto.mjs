1a0b1f1101fb209c956f75aa348612f907f74b9b
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:44:12 -0500

    function_auto_path ..\love\public\src\html_roboto.mjs

--- web/public/src/html_roboto.mjs
+++ web/public/src/html_roboto.mjs
@@ -1,9 +1,7 @@
+import { html_link } from "../../../love/public/src/html_link.mjs";
 import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
-import { html_element } from "../../../love/public/src/html_element.mjs";
-import { html_document_head } from "../../../love/public/src/html_document_head.mjs";
 export function html_roboto() {
-  let head = html_document_head();
-  let l = html_element(head, "link");
+  let l = html_link();
   html_attribute_set(
     l,
     "href",
