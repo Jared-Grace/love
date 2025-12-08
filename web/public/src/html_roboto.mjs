bce47f8bba3faa3ea1639357a7763e5f1dfbff54
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:44:54 -0500

    function_auto_path ..\love\public\src\html_roboto.mjs

--- web/public/src/html_roboto.mjs
+++ web/public/src/html_roboto.mjs
@@ -1,11 +1,6 @@
-import { html_link } from "../../../love/public/src/html_link.mjs";
-import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
+import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
 export function html_roboto() {
-  let l = html_link();
-  html_attribute_set(
-    l,
-    "href",
-    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap",
-  );
-  html_attribute_set(l, "rel", "stylesheet");
+  const href =
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
+  html_stylesheet(href);
 }
