41cb101723bebabf5eeb52a5cb9faada1dd40617
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:50:40 -0500

    function_auto_path ..\love\public\src\html_link.mjs

--- public/src/html_link.mjs
+++ public/src/html_link.mjs
@@ -1,11 +1,15 @@
+import { html_loading } from "../../../love/public/src/html_loading.mjs";
+import { lambda_invoke } from "../../../love/public/src/lambda_invoke.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { html_document_head } from "../../../love/public/src/html_document_head.mjs";
-export function html_link() {
+export async function html_link() {
   let head = html_document_head();
   let link = html_element(head, "link");
   link.onload = function lambda() {
     console.log("Stylesheet loaded!");
   };
+  lambda_invoke(function lambda3() {});
+  await html_loading();
   return l;
 }
