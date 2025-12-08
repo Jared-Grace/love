05ab0d67fb89081880b9ef89a7ebc467469391c4
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:52:08 -0500

    function_auto_path ..\love\public\src\html_link.mjs

--- public/src/html_link.mjs
+++ public/src/html_link.mjs
@@ -1,18 +1,16 @@
 import { promise_new } from "../../../love/public/src/promise_new.mjs";
 import { html_loading } from "../../../love/public/src/html_loading.mjs";
 import { lambda_invoke } from "../../../love/public/src/lambda_invoke.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { html_document_head } from "../../../love/public/src/html_document_head.mjs";
 export function html_link() {
   let head = html_document_head();
   let link = html_element(head, "link");
-  link.onload = function lambda() {
-    console.log("Stylesheet loaded!");
-  };
   async function lambda3() {
     async function lambda4() {
-      async function lambda2(resolve, reject) {}
+      async function lambda2(resolve, reject) {
+        link.onload = resolve;
+      }
       let result2 = await promise_new(lambda2);
     }
     let result = await html_loading(lambda4);
