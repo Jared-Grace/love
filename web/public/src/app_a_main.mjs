85154dc565b37261195b28e993396e3faf490297
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:41:55 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- web/public/src/app_a_main.mjs
+++ web/public/src/app_a_main.mjs
@@ -1,5 +1,7 @@
+import { http } from "../../../love/public/src/http.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_main() {
+export async function app_a_main() {
   marker("1");
+  let v = await http(url);
   alert("here");
 }
