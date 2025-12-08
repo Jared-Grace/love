854194404e55bff4f8b4083ee67115e3bdf78f09
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:02:37 -0500

    function_auto_path ..\love\public\src\app_g_tutorial.mjs

---
+++ public/src/g_icon.mjs
@@ -0,0 +1,11 @@
+import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
+import { g_img_square_style_position } from "../../../love/public/src/g_img_square_style_position.mjs";
+import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+export function g_icon(div_map, coordinates, emoji) {
+  let tutorial = html_div(div_map);
+  html_click_none(tutorial);
+  g_img_square_style_position(tutorial, coordinates, "tutorial");
+  html_text_set(tutorial, emoji);
+  return tutorial;
+}
