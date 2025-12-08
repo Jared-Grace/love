de9bd3648b8406e4874b7bc500efe1f77df3b95b
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 23:14:22 -0500

    function_auto_path ..\love\public\src\integer_random_less_1.mjs

--- public/src/integer_random_less_1.mjs
+++ public/src/integer_random_less_1.mjs
@@ -1,5 +1,5 @@
 import { integer_random_1 } from "../../../love/public/src/integer_random_1.mjs";
-export function integer_random_less_1(row_count) {
-  let v = integer_random_1(row_count) - 1;
+export function integer_random_less_1(count) {
+  let v = integer_random_1(count) - 1;
   return v;
 }
