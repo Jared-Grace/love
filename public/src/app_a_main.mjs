deed964be553ee20932094cac6afb10214a2694c
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:48:58 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -4,7 +4,7 @@ import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_main() {
   marker("1");
-  let v = await http_generic("/api", {
+  let buffer = await http_generic("/api", {
     method: "POST",
     body: {
       test: 123,
