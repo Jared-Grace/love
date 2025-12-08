937cd4387eb0aa6021e575bf6e62ffcb6183a08d
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 21:05:11 -0500

    data_file_update ..\love\public\src\firebase_storage_download.mjs

--- public/src/firebase_storage_download.mjs
+++ public/src/firebase_storage_download.mjs
@@ -22,7 +22,7 @@ export async function firebase_storage_download(destination) {
   let b2 = browser_is();
   if (b2) {
   } else {
-  }
+  }$s
   let result = await html_loading(lambda2);
   return result;
 }
