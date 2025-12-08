2a41ae6a94c8e9ab076ba53f40e36eb6e788a6f3
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:18:05 -0500

    function_auto_path ..\love\public\src\app_g_refresh.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -70,7 +70,7 @@ export async function app_g_refresh(
       alignItems: "flex-start",
       "padding-right": padding_size,
       textShadow: "0 0 3px white, 0 0 3px white, 0 0 3px white, 0 0 3px white",
-      color: "#1dbd00ff",
+      color: "#22e300ff",
       textAlign: "right",
       fontSize: square_size,
     });
