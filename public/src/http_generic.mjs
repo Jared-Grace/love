cb2c9fd408b41ec794b70fefd5bf4e49b7ef704c
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:57:51 -0500

    function_auto_path ..\love\public\src\http_generic.mjs

--- public/src/http_generic.mjs
+++ public/src/http_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 import { assert_json } from "../../../love/public/src/assert_json.mjs";
@@ -28,6 +29,7 @@ export async function http_generic(url, options) {
           body: json_to(body),
         });
       }
+      log(message);
       const response = await fetch(url, r);
       if (not(response.ok)) {
         error("Failed to fetch file");
