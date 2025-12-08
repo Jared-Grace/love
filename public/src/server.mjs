bed1647f203c3571fe914518e1250bc2da58c800
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:45:56 -0500

    function_auto_path ..\love\public\src\server.mjs

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -1,4 +1,3 @@
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
@@ -16,9 +15,8 @@ export function server() {
   let result2 = path_join([__dirname, previous, previous, previous]);
   let v = express.static(result2);
   function lambda2(req, res) {
-    let body = object_property_get(req, "body");
     res.json({
-      body,
+      req,
     });
   }
   app.post("/api", lambda2);
