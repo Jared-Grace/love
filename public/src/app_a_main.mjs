fdfe7988c3e0ebefce83bacbbd858c676ac4d3fb
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:43:14 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -1,7 +1,9 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { http_json } from "../../../love/public/src/http_json.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_main() {
   marker("1");
   let v = await http_json("/api/time");
+  log(message);
   alert(v);
 }
