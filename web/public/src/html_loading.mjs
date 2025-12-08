030032d7f03e56577176821b26555d75fdc9f1bf
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 21:06:01 -0500

    function_auto_path ..\love\public\src\html_loading.mjs

--- web/public/src/html_loading.mjs
+++ web/public/src/html_loading.mjs
@@ -7,26 +7,23 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function html_loading(lambda) {
   let b2 = browser_is();
   if (b2) {
-    let result = await html_loading(lambda2);
-  } else {
-    let result = await lambda2();
+    marker("1");
+    let body = html_document_body();
+    let div = html_div(body);
+    let s = {
+      position: "fixed",
+      top: "0",
+      left: "0",
+      width: "100vw",
+      height: "100vh",
+      background: "rgba(0, 0, 0, 0.6)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: "1000",
+    };
+    html_style_assign(div, s);
   }
-  marker("1");
-  let body = html_document_body();
-  let div = html_div(body);
-  let s = {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    width: "100vw",
-    height: "100vh",
-    background: "rgba(0, 0, 0, 0.6)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: "1000",
-  };
-  html_style_assign(div, s);
   let result = await lambda();
   html_remove(div);
   return result;
