e7bda01e9b51dce116186ad3c57566fb9c2adc25
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:23:46 -0500

    function_auto_path ..\love\public\src\app_a.mjs

--- web/public/src/app_a.mjs
+++ web/public/src/app_a.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_a() {
   await app_main_production("app_a_main", "jared-grace");
 }
