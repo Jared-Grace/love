9877ea3b549184c705685f1effb18f84ac44be0f
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:39:29 -0500

    function_auto_path ..\love\public\src\http_generic.mjs

--- public/src/http_generic.mjs
+++ public/src/http_generic.mjs
@@ -1,4 +1,77 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function http_generic() {
-  marker("1");
+import { assert_json } from "../../../love/public/src/assert_json.mjs";
+import { round } from "../../../love/public/src/round.mjs";
+import { catch_call } from "../../../love/public/src/catch_call.mjs";
+import { promise_wrap } from "../../../love/public/src/promise_wrap.mjs";
+import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
+import { http_sleep } from "../../../love/public/src/http_sleep.mjs";
+import { html_loading } from "../../../love/public/src/html_loading.mjs";
+import { error } from "../../../love/public/src/error.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
+export async function http_generic(url, method, body) {
+  const b = browser_is();
+  if (b) {
+    async function lambda3() {
+      const response = await fetch(url, {
+        method,
+        body,
+        headers: options.headers || {},
+      });
+      if (not(response.ok)) {
+        error("Failed to fetch file");
+      }
+      const buf = await response.arrayBuffer();
+      return buf;
+    }
+    let v = await html_loading(lambda3);
+    return v;
+  }
+  await http_sleep();
+  let h = null;
+  let swHttps = string_starts_with(url, "https://");
+  if (swHttps) {
+    h = await import("https");
+  } else {
+    h = await import("http");
+  }
+  let buffer = await promise_wrap(lambda);
+  function lambda(resolve, reject) {
+    const urlObj = new URL(url);
+    function lambda5(res) {
+      const chunks = [];
+      function lambda2(chunk) {
+        chunks.push(chunk);
+      }
+      let i = catch_call(reject, lambda2);
+      res.on("data", i);
+      function lambda4() {
+        const { statusCode } = res;
+        const d = statusCode / 100;
+        const rounded = round(d);
+        assert_json(rounded === 2, {
+          url,
+          statusCode,
+        });
+        let v2 = Buffer.concat(chunks);
+        resolve(v2);
+      }
+      let i2 = catch_call(reject, lambda4);
+      res.on("end", i2);
+    }
+    const req = h.request(
+      {
+        hostname: urlObj.hostname,
+        path: urlObj.pathname + urlObj.search,
+        method,
+        headers: options.headers || {},
+      },
+      lambda5,
+    );
+    req.on("error", reject);
+    if (body) {
+      req.write(body);
+    }
+    req.end();
+  }
+  return buffer;
 }
