34bf0eda818c18564fe82c4ebea1cba4c7d982c8
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 17:41:36 -0500

    function_auto_path ..\love\public\src\http.mjs

--- web/public/src/http.mjs
+++ web/public/src/http.mjs
@@ -1,57 +1,7 @@
-import { http_sleep } from "../../../love/public/src/http_sleep.mjs";
-import { html_loading } from "../../../love/public/src/html_loading.mjs";
-import { catch_call } from "../../../love/public/src/catch_call.mjs";
-import { promise_wrap } from "../../../love/public/src/promise_wrap.mjs";
-import { round } from "../../../love/public/src/round.mjs";
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
-import { browser_is } from "../../../love/public/src/browser_is.mjs";
-import { error } from "../../../love/public/src/error.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { assert_json } from "../../../love/public/src/assert_json.mjs";
+import { http_generic } from "../../../love/public/src/http_generic.mjs";
 export async function http(url) {
-  let b = browser_is();
-  if (b) {
-    async function lambda3() {
-      const response = await fetch(url);
-      if (not(response.ok)) {
-        error("Failed to fetch file");
-      }
-      const buf = await response.arrayBuffer();
-      return buf;
-    }
-    let v = await html_loading(lambda3);
-    return v;
-  }
-  await http_sleep();
-  let h = null;
-  let sw = string_starts_with(url, "https://");
-  if (sw) {
-    h = await import("https");
-  }
-  let buffer = await promise_wrap(lambda);
-  function lambda(resolve, reject) {
-    function lambda2(res) {
-      const chunks = [];
-      function on_data(chunk) {
-        let v2 = chunks.push(chunk);
-        return v2;
-      }
-      let result = catch_call(reject, on_data);
-      res.on("data", result);
-      function on_end() {
-        const { statusCode } = res;
-        const d = statusCode / 100;
-        const rounded = round(d);
-        assert_json(rounded === 2, {
-          url,
-        });
-        let v3 = Buffer.concat(chunks);
-        resolve(v3);
-      }
-      let result2 = catch_call(reject, on_end);
-      res.on("end", result2);
-    }
-    h.get(url, lambda2).on("error", reject);
-  }
-  return buffer;
+  let v = await http_generic(url, {
+    method: "GET",
+  });
+  return v;
 }
