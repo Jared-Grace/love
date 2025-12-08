aded41adce5205afebae808f2e30d5a7325e5bc2
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 20:45:30 -0500

    function_auto_path ..\love\public\src\g_icon_cross.mjs

---
+++ public/src/g_icon_cross_unpositioned.mjs
@@ -0,0 +1,29 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
+import { html_class_add } from "../../../love/public/src/html_class_add.mjs";
+import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
+import { html_element } from "../../../love/public/src/html_element.mjs";
+export function g_icon_cross_unpositioned(parent) {
+  let i = html_element(parent, "i");
+  html_click_none(i);
+  html_class_add(i, "ri-cross-fill");
+  const square_size = `calc(` + g_img_square_size_css() + `*.4)`;
+  const glow_size = `calc(` + g_img_square_size_css() + `*.05)`;
+  html_style_assign(i, {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    textShadow:
+      "0 0 " +
+      glow_size +
+      " white, 0 0 " +
+      glow_size +
+      " white, 0 0 " +
+      glow_size +
+      " white",
+    color: "#1fd000ff",
+    textAlign: "right",
+    fontSize: square_size,
+  });
+  return i;
+}
