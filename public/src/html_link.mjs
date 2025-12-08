532f60293baa963c9ed82955abba2d76bc0325c2
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:51:36 -0500

    function_auto_path ..\love\public\src\html_link.mjs

--- public/src/html_link.mjs
+++ public/src/html_link.mjs
@@ -1,3 +1,4 @@
+import { promise_new } from "../../../love/public/src/promise_new.mjs";
 import { html_loading } from "../../../love/public/src/html_loading.mjs";
 import { lambda_invoke } from "../../../love/public/src/lambda_invoke.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -10,7 +11,11 @@ export function html_link() {
     console.log("Stylesheet loaded!");
   };
   async function lambda3() {
-    async function lambda4() {}
+    async function lambda4() {
+      let result2 = await promise_new(
+        async function lambda2(resolve, reject) {},
+      );
+    }
     let result = await html_loading(lambda4);
   }
   lambda_invoke(lambda3);
