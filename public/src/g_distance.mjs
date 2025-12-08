0ef88bc0107c843c2ed7fbbe3cd577441278fa52
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 23:25:16 -0500

    function_auto_path ..\love\public\src\g_distance.mjs

--- public/src/g_distance.mjs
+++ public/src/g_distance.mjs
@@ -1,8 +1,8 @@
 import { abs } from "../../../love/public/src/abs.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function g_distance(clicked_coordinates, item) {
-  let x3 = object_property_get(clicked_coordinates, "x");
-  let y3 = object_property_get(clicked_coordinates, "y");
+export function g_distance(coordinates, item) {
+  let x3 = object_property_get(coordinates, "x");
+  let y3 = object_property_get(coordinates, "y");
   let x2 = object_property_get(item, "x");
   let y2 = object_property_get(item, "y");
   let distance = abs(x2 - x3) + abs(y2 - y3);
