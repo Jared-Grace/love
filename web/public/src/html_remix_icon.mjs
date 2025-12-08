0a74e7622a4348921795d33da5ba2bb18b3437ca
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:45:23 -0500

    function_auto_path ..\love\public\src\html_remix_icon.mjs

---
+++ web/public/src/html_remix_icon.mjs
@@ -0,0 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
+export function html_remix_icon() {
+  marker("1");
+  const href =
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
+  html_stylesheet(href);
+}
