bc48e562b69929639e95b96b10d714ac71850468
Grace-Jared <christrosetolife@gmail.com>
2025-12-07 19:03:34 -0500

    function_auto_path ..\love\public\src\g_icon.mjs

--- public/src/g_icon.mjs
+++ public/src/g_icon.mjs
@@ -3,9 +3,9 @@ import { g_img_square_style_position } from "../../../love/public/src/g_img_squa
 import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function g_icon(div_map, coordinates, text) {
-  let tutorial = html_div(div_map);
-  html_click_none(tutorial);
-  g_img_square_style_position(tutorial, coordinates, "tutorial");
-  html_text_set(tutorial, text);
-  return tutorial;
+  let div = html_div(div_map);
+  html_click_none(div);
+  g_img_square_style_position(div, coordinates, "tutorial");
+  html_text_set(div, text);
+  return div;
 }
