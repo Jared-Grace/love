5a70406588947e4f7e6be8dff023dcdbdce617f8
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:44:46 -0500

    function_auto_path ..\love\public\src\html_input.mjs

--- web/public/src/html_input.mjs
+++ web/public/src/html_input.mjs
@@ -1,5 +1,5 @@
 import { html_element } from "../../../love/public/src/html_element.mjs";
-export function html_input(div) {
-  let component2 = html_element(div, "input");
-  return component2;
+export function html_input(parent) {
+  let component = html_element(parent, "input");
+  return component;
 }
