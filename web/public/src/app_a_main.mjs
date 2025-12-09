8ed85266119ebd3bd2cdc950ea9e82d5b4e440bd
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:45:29 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- web/public/src/app_a_main.mjs
+++ web/public/src/app_a_main.mjs
@@ -19,7 +19,7 @@ export async function app_a_main() {
   let result = object_property_get(o, "result");
   list_sort_string(result);
   let component2 = html_input(body2);
-    app_a_control_style(component2);
+  app_a_control_style(component2);
   function lambda(f_name) {
     function lambda3() {}
     marker("1");
