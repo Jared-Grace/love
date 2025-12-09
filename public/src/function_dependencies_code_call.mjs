d8c0b2f75e6b11beb19f6e6153fe3718e50ddfc7
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:06:31 -0500

    function_auto_path ..\love\public\src\html_update_public_generic.mjs

---
+++ public/src/function_dependencies_code_call.mjs
@@ -0,0 +1,12 @@
+import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
+import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
+import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
+export async function function_dependencies_code_call(f_name) {
+  let code = await function_dependencies_code(f_name);
+  let call = js_code_call_statement(f_name);
+  let global_init = js_code_global_init();
+  const middle = `${global_init}
+    ${call}
+    ${code}`;
+  return middle;
+}
