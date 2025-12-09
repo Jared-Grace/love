3b196bd7b764451ff9ae5d66c663dba8a04e466d
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 20:02:26 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -1,5 +1,4 @@
 import { html_on_input } from "../../../love/public/src/html_on_input.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { list_sort_string_alpha_size } from "../../../love/public/src/list_sort_string_alpha_size.mjs";
@@ -35,14 +34,12 @@ export async function app_a_main() {
   let f_names = null;
   let filtered = null;
   function lambda4() {
-    log("here");
     let value = html_value_get(input);
     function lambda2(f_name) {
       let v3 = match(value, f_name);
       return v3;
     }
     filtered = list_filter(f_names, lambda2);
-    log(filtered);
     refresh();
     function match(s, target) {
       const escaped = s.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
