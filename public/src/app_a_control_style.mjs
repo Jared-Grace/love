e70c2e444441dd78cd94ce575257880f0ebe8fd9
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:45:56 -0500

    function_auto_path ..\love\public\src\app_a_control_style.mjs

--- public/src/app_a_control_style.mjs
+++ public/src/app_a_control_style.mjs
@@ -1,5 +1,4 @@
 import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
-import { app_karate_button_uncolored_style_assign } from "../../../karate_code/public/src/app_karate_button_uncolored_style_assign.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_a_control_style(component) {
   html_style_assign(component, {
@@ -7,6 +6,5 @@ export function app_a_control_style(component) {
     width: "100%",
     "border-width": "0px",
   });
-  app_karate_button_uncolored_style_assign(component);
   html_style_margin_y(component, "2px");
 }
