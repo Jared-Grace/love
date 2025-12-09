24afa87e51b54562ea497430a5e1f41684ad0e72
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:06:31 -0500

    function_auto_path ..\love\public\src\html_update_public_generic.mjs

--- public/src/html_update_public_generic.mjs
+++ public/src/html_update_public_generic.mjs
@@ -1,18 +1,11 @@
-import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
+import { function_dependencies_code_call } from "../../../love/public/src/function_dependencies_code_call.mjs";
 import { file_open } from "../../../love/public/src/file_open.mjs";
 import { html_overwrite } from "../../../love/public/src/html_overwrite.mjs";
 import { html_code_script_module } from "../../../love/public/src/html_code_script_module.mjs";
-import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
-import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 import { function_name_repo_path_combine } from "../../../love/public/src/function_name_repo_path_combine.mjs";
 export async function html_update_public_generic(f_name, file_path, name) {
   let joined = await function_name_repo_path_combine(f_name, file_path);
-  let code = await function_dependencies_code(f_name);
-  let call = js_code_call_statement(f_name);
-  let global_init = js_code_global_init();
-  const middle = `${global_init}
-    ${call}
-    ${code}`;
+  const middle = await function_dependencies_code_call(f_name);
   let body = html_code_script_module(middle);
   await html_overwrite(name, joined, body);
   await file_open(joined);
