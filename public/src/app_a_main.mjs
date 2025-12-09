a69f92c4c9d9f6ee783b9599f4f4bbe272d4bc2b
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:22:06 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -2,7 +2,6 @@ import { app_karate_margin_y_set } from "../../../karate_code/public/src/app_kar
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { app_karate_button_uncolored_style_assign } from "../../../karate_code/public/src/app_karate_button_uncolored_style_assign.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -23,7 +22,6 @@ export async function app_a_main() {
   let body2 = html_document_body();
   function lambda(f_name) {
     function lambda3() {}
-    assert_arguments(arguments, 0);
     marker("1");
     let component = html_button(body2, f_name, lambda3);
     html_style_assign(component, {
