ab7437e2cbe0fdcb07ccd827daa80528485353df
Grace-Jared <christrosetolife@gmail.com>
2025-12-08 19:00:38 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 export async function sandbox() {
   marker("1");
-  const wn = await import("wordnetjs");
-  const word = "happy";
-  const synonyms = wn.synonyms(word);
-  console.log(`Synonyms for "${word}":`, synonyms);
 }
