e6a4516e310ed9559a43a5b3b0b534abd951429e
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:48:03 -0500

    function_auto_path ..\love\public\src\storage_local_get_global.mjs

--- public/src/storage_local_get_global.mjs
+++ public/src/storage_local_get_global.mjs
@@ -1,11 +1,8 @@
+import { storage_local_exists_global } from "../../../love/public/src/storage_local_exists_global.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 export function storage_local_get_global(storage_local_key) {
-  let exists = global_function_property_exists(
-    storage_local_set,
-    storage_local_key,
-  );
+  let exists = storage_local_exists_global(storage_local_key);
   ("localStorage has this behavior of null if not exists, so the in-memory version also mirrors api");
   let value = null;
   if (exists) {
