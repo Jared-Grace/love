54591b373d34c4c5a44e48890e148c88752020ce
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:09:45 -0500

    function_auto_path ..\love\public\src\storage_local_exists.mjs

--- public/src/storage_local_exists.mjs
+++ public/src/storage_local_exists.mjs
@@ -1,3 +1,4 @@
+import { storage_local_specify_get_json } from "../../../love/public/src/storage_local_specify_get_json.mjs";
 import { storage_local_exists_global } from "../../../love/public/src/storage_local_exists_global.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
@@ -6,8 +7,9 @@ export function storage_local_exists(app_fn, key) {
   marker("1");
   let storage_local_key = storage_local_key_get(app_fn, key);
   if (storage_local_enabled()) {
-    let result = storage_local_specify_exists(storage_local_key);
-    return result;
+    let json = storage_local_specify_get_json(storage_local_key);
+    let v = json === nul;
+    return v;
   }
   let value = storage_local_exists_global(storage_local_key);
   return value;
