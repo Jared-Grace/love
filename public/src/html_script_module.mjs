8b0972691072faa2e178156a5b4227e7ecfd0e23
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 16:36:15 -0500

    function_auto_path ..\love\public\src\html_script_module.mjs

--- public/src/html_script_module.mjs
+++ public/src/html_script_module.mjs
@@ -1,10 +1,8 @@
 import { html_script } from "../../../love/public/src/html_script.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
 export function html_script_module(script_body_text) {
   marker("1");
   let component = html_script(script_body_text);
-  html_text_set(component, script_body_text);
   html_attribute_set(component, "type", "module");
 }
