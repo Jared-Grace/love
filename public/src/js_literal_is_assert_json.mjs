e915b4b60d2fca724ee8f986cf0a5068e76a57c8
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:05:56 -0500

    function_auto_path ..\love\public\src\js_literal_is_assert_json.mjs

---
+++ public/src/js_literal_is_assert_json.mjs
@@ -0,0 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { assert } from "../../../love/public/src/assert.mjs";
+import { js_literal_is } from "../../../love/public/src/js_literal_is.mjs";
+export function js_literal_is_assert_json(node) {
+  marker("1");
+  let li = js_literal_is(node);
+  assert(li);
+}
