4afc50bdba2485a50cdef7cac77cc024f222778a
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:13:13 -0500

    function_auto_path ..\love\public\src\storage_local_set_exists_not.mjs

--- web/public/src/storage_local_set_exists_not.mjs
+++ web/public/src/storage_local_set_exists_not.mjs
@@ -1,6 +1,8 @@
+import { assert_not } from "../../../love/public/src/assert_not.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_set_exists_not(app_fn, key, value) {
   marker("1");
-  storage_local_set(app_fn2, key2, value2);
+  assert_not(b);
+  storage_local_set(app_fn, key, value);
 }
