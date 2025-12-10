72ca164abba97beb2e60c97a2181dc61f98afe60
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:52:45 -0500

    function_auto_path ..\love\public\src\storage_local_exists.mjs

--- public/src/storage_local_exists.mjs
+++ public/src/storage_local_exists.mjs
@@ -1,5 +1,4 @@
-import { storage_local_get_global } from "../../../love/public/src/storage_local_get_global.mjs";
-import { storage_local_specify_get } from "../../../love/public/src/storage_local_specify_get.mjs";
+import { storage_local_exists_global } from "../../../love/public/src/storage_local_exists_global.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -7,9 +6,9 @@ export function storage_local_exists(app_fn, key) {
   marker("1");
   let storage_local_key = storage_local_key_get(app_fn, key);
   if (storage_local_enabled()) {
-    let result = storage_local_specify_get(storage_local_key);
+    let result = storage_local_specify_exists(storage_local_key);
     return result;
   }
-  let value = storage_local_get_global(storage_local_key);
+  let value = storage_local_exists_global(storage_local_key);
   return value;
 }
