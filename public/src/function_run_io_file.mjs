867d22dee4555a2787bbccab7c83b28d6ea37875
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:25:03 -0500

    function_auto_path ..\love\public\src\function_run_io_file.mjs

--- public/src/function_run_io_file.mjs
+++ public/src/function_run_io_file.mjs
@@ -1,7 +1,9 @@
+import { function_read } from "../../../love/public/src/function_read.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 export async function function_run_io_file(temp_path_input, temp_path_output) {
   let data = await file_read_json(temp_path_input);
   let f_name = object_property_get(data, "f_name");
   let args = object_property_get(data, "args");
+  let v = await function_read(f_name2);
 }
