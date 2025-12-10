b289ddfd1c6290cd0a7c17e2f5ad1e80ab798b27
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:13:38 -0500

    function_auto_path ..\love\public\src\storage_local_set_exists_not.mjs

--- public/src/storage_local_set_exists_not.mjs
+++ public/src/storage_local_set_exists_not.mjs
@@ -4,7 +4,7 @@ import { storage_local_set } from "../../../love/public/src/storage_local_set.mj
 import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_set_exists_not(app_fn, key, value) {
   marker("1");
-  let value2 = storage_local_exists(app_fn2, key2);
-  assert_not(b);
+  let e = storage_local_exists(app_fn, key);
+  assert_not(e);
   storage_local_set(app_fn, key, value);
 }
