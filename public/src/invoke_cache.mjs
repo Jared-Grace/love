a34d2beffcad5264ba3f5f09d15d8881b1883dcf
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:35:30 -0500

    function_auto_path ..\love\public\src\invoke_cache.mjs

--- public/src/invoke_cache.mjs
+++ public/src/invoke_cache.mjs
@@ -1,3 +1,4 @@
+import { function_run } from "../../../love/public/src/function_run.mjs";
 import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
@@ -7,7 +8,9 @@ export async function invoke_cache(f_name, args) {
     return json;
   };
   let cached_exists = async function lambda2(key) {
-    async function lambda4() {}
+    async function lambda4() {
+      let result2 = await function_run(f_name2, args2);
+    }
     let value = await global_function_property_initialize_async(
       invoke_cache,
       key,
