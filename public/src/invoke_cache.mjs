69492ccc06400f46a6e3dd45a474d0e9243425fb
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:33:06 -0500

    function_auto_path ..\love\public\src\invoke_cache.mjs

--- public/src/invoke_cache.mjs
+++ public/src/invoke_cache.mjs
@@ -1,6 +1,5 @@
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function invoke_cache(f_name, args) {
   let key_get = function lambda() {
     let json = json_to([f_name, args]);
@@ -13,5 +12,5 @@ export async function invoke_cache(f_name, args) {
     value_get,
     cache_save,
   );
-  marker("1");
+  return result;
 }
