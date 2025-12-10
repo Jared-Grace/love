4de5e32e4f809a4cc7b5c94eb71b16ef42afacae
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:10:10 -0500

    de exists_not

--- public/src/exists_not.mjs
+++
@@ -1,7 +0,0 @@
-import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function exists_not(app_fn, key, value) {
-  marker("1");
-  let v = storage_local_set(app_fn, key, value);
-  return v;
-}
