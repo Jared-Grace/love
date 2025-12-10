eef7ece369be5ff4a5e270f7dca180905b82ef8d
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:09:28 -0500

    function_auto_path ..\love\public\src\storage_local_specify_get.mjs

--- public/src/storage_local_specify_get.mjs
+++ public/src/storage_local_specify_get.mjs
@@ -1,7 +1,8 @@
+import { storage_local_specify_get_json } from "../../../love/public/src/storage_local_specify_get_json.mjs";
 import { json_from } from "./json_from.mjs";
 import { null_is } from "./null_is.mjs";
 export function storage_local_specify_get(storage_local_key) {
-  let json = localStorage.getItem(storage_local_key);
+  let json = storage_local_specify_get_json(storage_local_key);
   let n = null_is(json);
   let result = null;
   if (n) {
