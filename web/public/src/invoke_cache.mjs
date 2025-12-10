176ee2f26b075edc4de9a4da59da9178b02053dd
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:37:15 -0500

    function_auto_path ..\love\public\src\invoke_cache.mjs

--- web/public/src/invoke_cache.mjs
+++ web/public/src/invoke_cache.mjs
@@ -1,4 +1,3 @@
-import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache(f_name, args) {
@@ -6,17 +5,13 @@ export async function invoke_cache(f_name, args) {
     let json = json_to([f_name, args]);
     return json;
   };
-  let cached_exists = async function lambda2(key) {
-    async function lambda4() {
-      let f = eval(f_name);
-      let v = await f(...args);
-      return v;
-    }
-    let value = await global_function_property_initialize_async(
-      invoke_cache,
-      key,
-      lambda4,
-    );
+  async function value_get() {
+    let f = eval(f_name);
+    let v = await f(...args);
+    return v;
+  }
+  let cached_get = async function lambda2(key) {
+    let value = await global_function_property(invoke_cache, key, lambda4);
     return value;
   };
   let result = await cache_generic(
