0f53f04917e67e2418aa37a92d1767c9e541adda
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 15:01:46 -0500

    function_auto_path ..\love\public\src\function_run.mjs

--- public/src/function_run.mjs
+++ public/src/function_run.mjs
@@ -1,8 +1,8 @@
-import { function_import } from "../../../love/public/src/function_import.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { function_import_unalias } from "./function_import_unalias.mjs";
 export async function function_run(f_name, args) {
   marker("1");
-  const fn = await function_import(f_name);
+  const fn = await function_import_unalias(f_name);
   const result = await fn(...args);
   return result;
 }
