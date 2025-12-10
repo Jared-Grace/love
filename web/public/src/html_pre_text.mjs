da037ed1ff70b698f7eebde85c10ec934f8d6e72
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:55:52 -0500

    data_file_update ..\love\public\src\html_pre_text.mjs

--- web/public/src/html_pre_text.mjs
+++ web/public/src/html_pre_text.mjs
@@ -3,7 +3,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 export function html_pre_text(root, text) {
   marker("1");
-  let p = html_element(root, "p");
+  let p = html_element(root, "pre");
   html_text_set(p, text);
   return p;
 }
