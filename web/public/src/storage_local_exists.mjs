574981196e4f504ca1ee5390c761588b37599071
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:46:43 -0500

    fw storage_local_get storage_local_exists

--- web/public/src/storage_local_exists.mjs
+++ web/public/src/storage_local_exists.mjs
@@ -1,4 +1,6 @@
+import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_exists() {
+export function storage_local_exists(app_fn, key) {
   marker("1");
+  return storage_local_get(app_fn, key);
 }
