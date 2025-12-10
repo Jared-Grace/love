255d8b222e1653c813f3b150fd67a75006d9673b
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 12:57:01 -0500

    function_auto_path ..\love\public\src\invoke_cache_storage_local.mjs

---
+++ public/src/invoke_cache_storage_local.mjs
@@ -0,0 +1,41 @@
+import { storage_local_set_exists_not } from "../../../love/public/src/storage_local_set_exists_not.mjs";
+import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
+import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+import { json_to } from "../../../love/public/src/json_to.mjs";
+import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
+export async function invoke_cache_storage_local(fn, args) {
+  marker("1");
+  let key_get = function lambda() {
+    let json = json_to([fn.name, args]);
+    return json;
+  };
+  async function value_get() {
+    let v = await fn(...args);
+    return v;
+  }
+  let cached_exists = function lambda3(key) {
+    let exists = storage_local_exists(invoke_cache_storage_local, key);
+    return exists;
+  };
+  let cached_get = async function lambda2(key) {
+    let value = await storage_local_get(invoke_cache_storage_local, key);
+    return value;
+  };
+  let cache_save = function lambda4(key, value) {
+    let v2 = storage_local_set_exists_not(
+      invoke_cache_storage_local,
+      key,
+      value,
+    );
+    return v2;
+  };
+  let result = await cache_generic(
+    key_get,
+    cached_exists,
+    cached_get,
+    value_get,
+    cache_save,
+  );
+  return result;
+}
