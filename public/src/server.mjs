f1df5f86de315d02a7783eade5f346489722d79e
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 16:14:58 -0500

    function_auto_path ..\love\public\src\server.mjs

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -25,7 +25,7 @@ export function server() {
   let v = express.static(result2);
   async function api(req, res) {
     let body2 = object_property_get(req, "body");
-    let args2 = object_property_get(req, "args");
+    let args = object_property_get(req, "args");
     let f_name = object_property_get(body2, "function_name");
     let { unaliased } = await function_name_unalias(f_name);
     let global_init = js_code_global_init();
@@ -34,7 +34,6 @@ export function server() {
     log({
       f_name,
     });
-    const args = [];
     let code2 = js_code_call_args(f_name, args);
     let middle = `${global_init}
     ${code}
