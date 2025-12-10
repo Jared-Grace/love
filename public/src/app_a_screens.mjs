9597881bdaecc33206cad8e4392ee606c2cda1bc
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 18:08:52 -0500

    function_auto_path ..\love\public\src\app_a_screens.mjs

--- public/src/app_a_screens.mjs
+++ public/src/app_a_screens.mjs
@@ -1,9 +1,11 @@
-import { app_karate_home } from "../../../karate_code/public/src/app_karate_home.mjs";
+import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
+import { app_a_home } from "../../../love/public/src/app_a_home.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_screens() {
   marker("screens");
   let v = {
-    home: app_karate_home,
+    home: app_a_home,
+    function: app_a_function,
   };
   return v;
 }
