7ff6832ff7453c8752fec7b16f083f5e17d321ab
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 16:35:49 -0500

    data_file_update ..\love\public\src\html_script.mjs

--- public/src/html_script.mjs
+++ public/src/html_script.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 export function html_script() {
+  marker("1");
   let body = html_document_body();
   let s = html_element(body, "script");
   html_text_set(component, script_body_text);
