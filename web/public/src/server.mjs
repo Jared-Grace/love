9399d367b9d68d1c5f47a18d80f99d5272aefa68
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:02:24 -0500

    function_auto_path ..\love\public\src\server.mjs

--- web/public/src/server.mjs
+++ web/public/src/server.mjs
@@ -1,6 +1,3 @@
-import { command_line } from "../../../love/public/src/command_line.mjs";
-import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
-import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
 import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
@@ -15,7 +12,6 @@ import { path_join } from "../../../love/public/src/path_join.mjs";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { fn_name } from "./fn_name.mjs";
 export function server() {
   marker("1");
   const app = express();
@@ -29,14 +25,6 @@ export function server() {
   let v = express.static(result2);
   async function api(req, res) {
     let body = object_property_get(req, "body");
-    async function lambda2(temp_path_input) {
-      let result5 = await file_overwrite_json(temp_path_input, body);
-      async function lambda3(temp_path_output) {
-        let stdout = await command_line('node r.mjs ' + fn_name(function_run_io_file) + ' ' + temp_path_input+' '+temp_path_output);
-      }
-      let result4 = await file_temp(lambda3);
-    }
-    let result3 = await file_temp(lambda2);
     let args = object_property_get(body, "args");
     let f_name = object_property_get(body, "function_name");
     let { unaliased } = await function_name_unalias(f_name);
