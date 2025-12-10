f8828ae8d8ef0156e265d79a26259bf4e8f50bfa
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:22:37 -0500

    function_auto_path ..\love\public\src\function_import.mjs

--- web/public/src/function_import.mjs
+++ web/public/src/function_import.mjs
@@ -1,10 +1,11 @@
 import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
+import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
-import { import_fresh } from "../../../love/public/src/import_fresh.mjs";
-export async function function_import(unaliased) {
+export async function function_import(f_name) {
+  let { unaliased } = await function_name_unalias(f_name);
   let { f_path: f } = await function_name_to_path_search(unaliased);
   let f_path = await path_resolve(f);
-  const imported = await import_fresh(`file://${f_path}`);
+  const imported = await import(`file://${f_path}`);
   const imported_fn = imported[unaliased];
   if (typeof imported_fn !== "function") {
     throw new Error(
