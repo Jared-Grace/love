92fdd798d15078f88135f0be8fe37f51aa64b29c
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:36:09 -0500

    data_file_update ..\love\public\src\function_run_unalias.mjs

--- public/src/function_run_line.mjs
+++ public/src/function_run_line.mjs
@@ -1,7 +1,7 @@
-import { function_run } from "../../../love/public/src/function_run.mjs";
+import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
 import { function_run_line_parse } from "../../../love/public/src/function_run_line_parse.mjs";
 export async function function_run_line(line) {
   let { f_name, args } = await function_run_line_parse(line);
-  let v = await function_run(f_name, args);
+  let v = await function_run_unalias(f_name, args);
   return v;
 }
