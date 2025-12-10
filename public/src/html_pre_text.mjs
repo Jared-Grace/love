6d301c42c304cbf2982863e3d4fd8c57f91158d5
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:55:48 -0500

    function_auto_path ..\love\public\src\html_pre_text.mjs

--- public/src/html_pre_text.mjs
+++ public/src/html_pre_text.mjs
@@ -1,9 +1,9 @@
+import { html_element } from "../../../love/public/src/html_element.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
-import { html_p } from "../../../love/public/src/html_p.mjs";
 export function html_pre_text(root, text) {
   marker("1");
-  let p = html_p(root);
+  let p = html_element(root, "p");
   html_text_set(p, text);
   return p;
 }
