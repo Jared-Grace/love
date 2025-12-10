c8511c2b467b91d5ca5941b35b889121d8590cfa
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:40:24 -0500

    function_auto_path ..\love\public\src\global_function_property_set_exists_not.mjs

--- public/src/global_function_property_set_exists_not.mjs
+++ public/src/global_function_property_set_exists_not.mjs
@@ -1,3 +1,4 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
@@ -9,6 +10,7 @@ export function global_function_property_set_exists_not(
 ) {
   marker("1");
   let exists = global_function_property_exists(fn, property_name);
+  let b = not(exists);
   assert(b);
   let v = global_function_property_set(fn, property_name, value);
   return v;
