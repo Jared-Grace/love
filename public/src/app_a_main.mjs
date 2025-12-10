2ba9bb69933fcb6cbb1964d2c7bc70a9cd4acd1e
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 18:11:56 -0500

    data_file_update ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -2,7 +2,7 @@ import { app_a_screens } from "../../../love/public/src/app_a_screens.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 import { app_karate } from "../../../karate_code/public/src/app_karate.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-export  function app_a_main() {
+export function app_a_main() {
   let app_fn = app_karate;
   const root = html_document_body();
   let screens = app_a_screens();
