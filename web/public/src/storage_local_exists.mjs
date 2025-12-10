69d774305571fd7142067a46a5b641ed1105cd5e
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:46:45 -0500

    function_auto_path ..\love\public\src\storage_local_exists.mjs

--- web/public/src/storage_local_exists.mjs
+++ web/public/src/storage_local_exists.mjs
@@ -2,5 +2,6 @@ import { storage_local_get } from "../../../love/public/src/storage_local_get.mj
 import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_exists(app_fn, key) {
   marker("1");
-  return storage_local_get(app_fn, key);
+  let value = storage_local_get(app_fn, key);
+  return value;
 }
