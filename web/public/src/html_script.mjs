18601df31a9465cd8e9e8a6abfccfcb8f68aa818
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 16:30:16 -0500

    function_auto_path ..\love\public\src\html_script.mjs

--- web/public/src/html_script.mjs
+++ web/public/src/html_script.mjs
@@ -2,6 +2,6 @@ import { html_element } from "../../../love/public/src/html_element.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 export function html_script() {
   let body = html_document_body();
-  let component = html_element(body, "script");
-  return component;
+  let s = html_element(body, "script");
+  return s;
 }
