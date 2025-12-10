ab0671c429720991e7f71ac4152a54833855985d
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:36:09 -0500

    data_file_update ..\love\public\src\function_run_unalias.mjs

--- public/src/function_run.mjs
+++
@@ -1,6 +0,0 @@
-import { function_import } from "../../../love/public/src/function_import.mjs";
-export async function function_run(f_name, args) {
-  const fn = await function_import(f_name);
-  const result = await fn(...args);
-  return result;
-}
