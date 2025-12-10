86a1d826b002d4ed84094d9d54e6f78fc175a09c
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:36:31 -0500

    function_auto_path ..\love\public\src\function_import_unalias.mjs

---
+++ public/src/function_import_unalias.mjs
@@ -0,0 +1,18 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
+import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
+import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
+export async function function_import_unalias(f_name) {
+  marker("1");
+  let { unaliased } = await function_name_unalias(f_name);
+  let { f_path: f } = await function_name_to_path_search(unaliased);
+  let f_path = await path_resolve(f);
+  const imported = await import(`file://${f_path}`);
+  const imported_fn = imported[unaliased];
+  if (typeof imported_fn !== "function") {
+    throw new Error(
+      `❌ The module "${unaliased}" does not export a default function.`,
+    );
+  }
+  return imported_fn;
+}
