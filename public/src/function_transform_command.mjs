b6bd3b28127ea5ab45bc8ce3cc7621e2b312d5ef
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:36:33 -0500

    rs function_import unalias

--- public/src/function_transform_command.mjs
+++ public/src/function_transform_command.mjs
@@ -1,7 +1,7 @@
 import { function_transform_result } from "../../../love/public/src/function_transform_result.mjs";
-import { function_import } from "../../../love/public/src/function_import.mjs";
+import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 export async function function_transform_command(transformer_name, f_name) {
-  const transformer = await function_import(transformer_name);
+  const transformer = await function_import_unalias(transformer_name);
   let v = await function_transform_result(f_name, transformer);
   return v;
 }
