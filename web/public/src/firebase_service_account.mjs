901409fc8a3490c95fe65bfc075b9eb485843f22
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 21:36:09 -0500

    data_file_update ..\love\public\src\function_run_unalias.mjs

--- web/public/src/firebase_service_account.mjs
+++ web/public/src/firebase_service_account.mjs
@@ -1,9 +1,9 @@
-import { function_run } from "../../../love/public/src/function_run.mjs";
+import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
 import { user_repo_firebase_service_account_get } from "../../../love/public/src/user_repo_firebase_service_account_get.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 export async function firebase_service_account() {
   let f_name = await user_repo_firebase_service_account_get();
-  let file_path = await function_run(f_name, []);
+  let file_path = await function_run_unalias(f_name, []);
   let service_account = await file_read_json(file_path);
   return service_account;
 }
--- web/public/src/function_run_log.mjs
+++ web/public/src/function_run_log.mjs
@@ -1,10 +1,10 @@
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { function_run } from "../../../love/public/src/function_run.mjs";
+import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 export async function function_run_log(f_name, args) {
   let { unaliased } = await function_name_unalias(f_name);
   log_keep("🏃 " + unaliased);
-  let result = await function_run(f_name, args);
+  let result = await function_run_unalias(f_name, args);
   log_keep("🔥");
   log_keep(result);
   return result;
