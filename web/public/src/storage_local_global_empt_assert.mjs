df1ecbaf1235f44fa2e1a745ad6b83745721cc33
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:38:53 -0500

    data_file_update ..\love\public\src\storage_local_global_empty_assert.mjs

--- web/public/src/storage_local_global_empt_assert.mjs
+++
@@ -1,7 +0,0 @@
-import { json_equal_assert_empty } from "../../../love/public/src/json_equal_assert_empty.mjs";
-import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-import { global_function_initialize_object } from "../../../love/public/src/global_function_initialize_object.mjs";
-export function storage_local_global_empt_assert() {
-  let fn_object2 = global_function_initialize_object(storage_local_set);
-  json_equal_assert_empty(fn_object2);
-}
