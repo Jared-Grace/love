b1c6e61ee190726bf8b744e675f20674f01daf10
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:23:06 -0500

    function_auto_path ..\love\public\src\function_run_io_file.mjs

--- web/public/src/function_run_io_file.mjs
+++ web/public/src/function_run_io_file.mjs
@@ -1,4 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function function_run_io_file() {
+export function function_run_io_file(temp_path_input, temp_path_output) {
   marker("1");
 }
