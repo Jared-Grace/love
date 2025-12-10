4cce862a4faecef0cd53b68256d85d40ac3d5df9
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 18:11:51 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -1,5 +1,4 @@
 import { app_a_screens } from "../../../love/public/src/app_a_screens.mjs";
-import { app_a_home } from "../../../love/public/src/app_a_home.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 import { app_karate } from "../../../karate_code/public/src/app_karate.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
@@ -13,5 +12,4 @@ export async function app_a_main() {
     root: root,
   };
   app_generic_refresh(context);
-  await app_a_home();
 }
