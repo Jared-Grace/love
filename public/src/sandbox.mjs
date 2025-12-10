46dd1225260877c40fbed2d6361a3283ba3b6f2e
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 17:47:15 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,7 @@
 import { function_run_io_file_wrapper } from "../../../love/public/src/function_run_io_file_wrapper.mjs";
 export async function sandbox() {
   let r = await function_run_io_file_wrapper({
-    f_name: "string_is",
-    args: ["s"],
+    f_name: "function_read",
+    args: ["function_read"],
   });
 }
