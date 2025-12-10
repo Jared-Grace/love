b2c41088076b80220ea9182dd4d81ea7d358953f
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:32:48 -0500

    function_auto_path ..\love\public\src\cache_generic.mjs

--- web/public/src/cache_generic.mjs
+++ web/public/src/cache_generic.mjs
@@ -7,13 +7,13 @@ export async function cache_generic(
   cache_save,
 ) {
   marker("1");
-  let key = key_get(url);
+  let key = key_get();
   let e = await cached_exists(key);
   let result = null;
   if (e) {
     result = await cached_get(key);
   } else {
-    let value = await value_get(url);
+    let value = await value_get();
     await cache_save(key, value);
     result = await cached_get(key);
   }
