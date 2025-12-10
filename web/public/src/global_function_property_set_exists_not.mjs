0af35459d4f02b59404ad81ba97fbaba6a68c543
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:39:52 -0500

    fws global_function_property_set exists_not

--- web/public/src/global_function_property_set_exists_not.mjs
+++ web/public/src/global_function_property_set_exists_not.mjs
@@ -1,4 +1,10 @@
+import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function global_function_property_set_exists_not() {
+export function global_function_property_set_exists_not(
+  fn,
+  property_name,
+  value,
+) {
   marker("1");
+  return global_function_property_set(fn, property_name, value);
 }
