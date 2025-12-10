c35f184b05c904fa3c2bced692288021119dd7b7
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 15:05:13 -0500

    function_auto_path ..\love\public\src\js_code_call_args.mjs

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,6 +1,9 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
-export function js_code_call_args(fn_name, args) {$aa
+export function js_code_call_args(fn_name, args) {
+  assert_arguments(arguments, 2);
   let joined = list_join_comma_space(args);
   let code = fn_name + js_code_wrap_parenthesis(joined);
   return code;
