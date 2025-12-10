0da5087f3e5c512d8734dfd518742d2ff8a6b94d
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:39:52 -0500

    data_file_update ..\love\public\src\global_function_property_set_exists_not.mjs

--- web/public/src/global_function_property_set_exists_not.mjs
+++ web/public/src/global_function_property_set_exists_not.mjs
@@ -1,1 +1,4 @@
-export function global_function_property_set_exists_not() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function global_function_property_set_exists_not() {
+  marker("1");
+}
