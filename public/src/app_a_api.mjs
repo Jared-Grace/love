7379345c0c3935f06fa9e72a13008d5dc3fc18fa
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 16:42:23 -0500

    function_auto_path ..\love\public\src\app_a_api.mjs

--- public/src/app_a_api.mjs
+++ public/src/app_a_api.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { http_post_json_cache } from "../../../love/public/src/http_post_json_cache.mjs";
 export async function app_a_api(function_name, args) {
   let api_body = {
@@ -6,5 +7,6 @@ export async function app_a_api(function_name, args) {
   };
   const url = "/api";
   let o = await http_post_json_cache(url, api_body);
-  return o;
+  let result = object_property_get(o, "result");
+  return result;
 }
