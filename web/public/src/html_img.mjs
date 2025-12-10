380a59bbdab6f2b1d675f0b921020f8081ee2c15
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 16:30:57 -0500

    function_auto_path ..\love\public\src\html_img.mjs

--- web/public/src/html_img.mjs
+++ web/public/src/html_img.mjs
@@ -1,7 +1,7 @@
-import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
+import { html_src_set } from "../../../love/public/src/html_src_set.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 export function html_img(body, src) {
   let component = html_element(body, "img");
-  html_attribute_set(component, "src", src);
+  html_src_set(component, src);
   return component;
 }
---
+++ web/public/src/html_src_set.mjs
@@ -0,0 +1,4 @@
+import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
+export function html_src_set(component, src) {
+  html_attribute_set(component, "src", src);
+}
