2e307fff4471b592bca0660fe735230d33beaf4a
Grace-Jared <christrosetolife@gmail.com>
2025-12-09 20:30:10 -0500

    function_auto_path ..\love\public\src\invoke_cache.mjs

--- web/public/src/invoke_cache.mjs
+++ web/public/src/invoke_cache.mjs
@@ -1,6 +1,7 @@
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function invoke_cache(f_name, args) {
+  let key_get = () => {};
   let result = await cache_generic(
     key_get,
     url,
