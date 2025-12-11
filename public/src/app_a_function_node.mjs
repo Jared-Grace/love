96b8880522f4bf2d8d7d6d508a3da21d9a119cb7
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 15:38:12 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -96,9 +96,7 @@ export function app_a_function_node(a) {
       let body2 = object_property_get(node, "body");
       let span2 = app_a_brace_right(parent);
     },
-    ["BlockStatement"]: function lambda7() { 
-      $l;
-    },
+    ["BlockStatement"]: function lambda7() {},
   };
   let value = object_property_get(lookup, type);
   value();
