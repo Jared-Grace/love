51e0ea3dad23a66a97851422309d9497bdd68c33
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:58:14 -0500

    function_auto_path ..\love\public\src\html_jetbrains_mono_include.mjs

--- web/public/src/html_jetbrains_mono_include.mjs
+++ web/public/src/html_jetbrains_mono_include.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
-export function html_jetbrains_mono() {
+export function html_jetbrains_mono_include() {
   marker("1");
   const href =
     "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap";
