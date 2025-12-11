d8c48749246a508f488252bce2bebc38b63002e0
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 16:28:00 -0500

    function_auto_path ..\love\public\src\app_a_keyword_purple.mjs

---
+++ web/public/src/app_a_keyword_purple.mjs
@@ -0,0 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+export function app_a_keyword_purple(parent, keyword) {
+  marker("1");
+  let s = html_span_text(parent, keyword);
+  const color = "purple";
+  html_font_color_set(s, color);
+}
