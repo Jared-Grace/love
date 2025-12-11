9969ead788c87b0256f5051975ee062296eee123
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 15:55:49 -0500

    data_file_update ..\love\public\src\git_commit_folder.mjs

--- public/src/git_commit_folder.mjs
+++ public/src/git_commit_folder.mjs
@@ -4,5 +4,5 @@ export async function git_commit_folder(folder, message) {
   async function lambda() {
     await command_line_git_folder(folder, `commit -m "${message}"`);
   }
-  await catch_ignore_async(lambda, "nothing to commit");
+  await catch_ignore_async(lambda);
 }
