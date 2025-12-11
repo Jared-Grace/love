5f22079c6c2f103d568ed0515cfc1c495929631a
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 17:27:54 -0500

    de functions_names_client

--- web/public/src/functions_names_client.mjs
+++
@@ -1,7 +0,0 @@
-import { app_a_api_cache_global } from "../../../love/public/src/app_a_api_cache_global.mjs";
-import { fn_name } from "../../../love/public/src/fn_name.mjs";
-export async function functions_names_client() {
-  let function_name = fn_name("functions_names");
-  let f_names = await app_a_api_cache_global(function_name, []);
-  return f_names;
-}
