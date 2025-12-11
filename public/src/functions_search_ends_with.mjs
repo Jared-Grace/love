dff716dcd0e962e1c7663e934a3ac4f99ef999bc
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:55:27 -0500

    function_auto_path ..\love\public\src\functions_search_ends_with.mjs

--- public/src/functions_search_ends_with.mjs
+++ public/src/functions_search_ends_with.mjs
@@ -1,9 +1,9 @@
+import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
 import { functions_search_generic } from "../../../love/public/src/functions_search_generic.mjs";
-import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_search_ends_with(search) {
   marker("1");
-  let fn = string_includes;
+  let fn = string_ends_with;
   let result = await functions_search_generic(search, fn);
   return result;
 }
