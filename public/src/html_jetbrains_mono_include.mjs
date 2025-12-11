ee0464ff8196874a88fbb898c4fe970b32bd3e31
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:04:06 -0500

    function_auto_path ..\love\public\src\html_jetbrains_mono_include.mjs

--- public/src/html_jetbrains_mono_include.mjs
+++ public/src/html_jetbrains_mono_include.mjs
@@ -2,11 +2,14 @@ import { global_function_initialize_lambda } from "../../../love/public/src/glob
 import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
 export function html_jetbrains_mono_include() {
   function lambda2() {
-    const href =
-      "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap";
-    html_stylesheet(href);
+    fn();
     let v = true;
     return v;
   }
   global_function_initialize_lambda(html_jetbrains_mono_include, lambda2);
+  function fn() {
+    const href =
+      "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap";
+    html_stylesheet(href);
+  }
 }
