58f9bdde927b3e102f28d833c5d90fa396819bf7
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:03:27 -0500

    function_auto_path ..\love\public\src\function_param_swap.mjs

--- web/public/src/function_param_swap.mjs
+++ web/public/src/function_param_swap.mjs
@@ -1,11 +1,5 @@
-import { js_declaration_single_params_add } from "../../../love/public/src/js_declaration_single_params_add.mjs";
-import { list_intersect_empty_is_assert } from "../../../love/public/src/list_intersect_empty_is_assert.mjs";
-import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
+import { function_param_index } from "../../../love/public/src/function_param_index.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
-import { string_split } from "../../../love/public/src/string_split.mjs";
 import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_transform_current } from "../../../love/public/src/function_transform_current.mjs";
@@ -26,21 +20,14 @@ export async function function_param_swap(param_name_a, param_name_b) {
     list_swap(params, a, b);
   }
   marker("1");
-  let param_names = string_split(param_names_comma, ",");
-  let values_default = string_split(values_default_comma, ",");
+  let index = null;
   await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
-    function lambda3(value_default) {
-      let expression2 = js_parse_expression(value_default);
-      list_add(args, expression2);
-    }
-    each(values_default, lambda3);
+    fn(args, index);
   }
   function function_transform_current_lambda(ast) {
-    if (false) {
-      let names = js_identifiers_names(ast);
-      list_intersect_empty_is_assert(names, param_names);
-    }
-    js_declaration_single_params_add(ast, param_names);
+    let params = null;
+    ({ params, index } = function_param_index(ast, param_name));
+    fn(params, index);
   }
 }
