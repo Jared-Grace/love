2f573c6afe053d58da03c071f38f4c184359e6fd
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:19:31 -0500

    function_auto_path ..\love\public\src\app_a_function_node_child_parent.mjs

---
+++ public/src/app_a_function_node_child_parent.mjs
@@ -0,0 +1,9 @@
+import { app_a_function_node_replace } from "../../../love/public/src/app_a_function_node_replace.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function app_a_function_node_child_parent(a, node_replacement) {
+  marker("1");
+  const replacements = {
+    node: node_replacement,
+  };
+  app_a_function_node_replace(a, replacements);
+}
