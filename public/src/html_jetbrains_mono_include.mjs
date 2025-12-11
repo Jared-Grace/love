56f013fa43598a070526be05199355bc0b002999
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:03:25 -0500

    function_auto_path ..\love\public\src\html_jetbrains_mono_include.mjs

--- public/src/html_jetbrains_mono_include.mjs
+++ public/src/html_jetbrains_mono_include.mjs
@@ -1,8 +1,14 @@
+import { global_function_initialize_lambda } from "../../../love/public/src/global_function_initialize_lambda.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
 export function html_jetbrains_mono_include() {
+  function lambda2() {
+    const href =
+      "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap";
+    html_stylesheet(href);
+    let v = true;
+    return v;
+  }
+  global_function_initialize_lambda(html_jetbrains_mono_include, lambda2);
   marker("1");
-  const href =
-    "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap";
-  html_stylesheet(href);
 }
