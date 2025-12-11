fd40e122ab5a4ccb8c6444715fa1f3b11a59cae5
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:09:42 -0500

    function_auto_path ..\love\public\src\app_a_control_style_wide.mjs

--- public/src/app_a_control_style_wide.mjs
+++ public/src/app_a_control_style_wide.mjs
@@ -1,13 +1,7 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
+import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_a_control_style_wide(component) {
-  marker("1");
-  html_style_assign(component, {
-    "border-radius": "0.8em",
-    "border-width": "0px",
-  });
-  html_style_margin_y(component, "2px");
+  app_a_control_style(component);
   html_style_assign(component, {
     width: "100%",
   });
