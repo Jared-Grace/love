bfd057aaffd11adc754113408cff3695dcb5f234
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:04:44 -0500

    function_auto_path ..\love\public\src\list_swap.mjs

--- public/src/list_swap.mjs
+++ public/src/list_swap.mjs
@@ -1,8 +1,7 @@
+import { list_swap_inner } from "../../../love/public/src/list_swap_inner.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
-import { list_replace } from "../../../love/public/src/list_replace.mjs";
 export function list_swap(list, a, b) {
   let ai = list_index_of(list, a);
   let bi = list_index_of(list, b);
-  list_replace(list, ai, b);
-  list_replace(list, bi, a);
+  list_swap_inner(list, ai, b, bi, a);
 }
