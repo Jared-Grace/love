c9bb250f90f9739d065a40c4a7cbd51ac9e57074
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 17:22:13 -0500

    function_auto_path ..\love\public\src\app_a_api_cache_storage_local.mjs

--- public/src/app_a_api_cache_storage_local.mjs
+++ public/src/app_a_api_cache_storage_local.mjs
@@ -4,6 +4,6 @@ import { http_post_json_cache_storage_local } from "../../../love/public/src/htt
 export async function app_a_api_cache_storage_local(f_name, args) {
   marker("1");
   let fn = http_post_json;
-  let result = await http_post_json_cache_storage_local(f_name, args, fn);
+  let result = await http_post_json_cache_storage_local(f_name, args);
   return result;
 }
