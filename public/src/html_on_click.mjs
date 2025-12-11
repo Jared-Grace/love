72e6c86c016744547dd1532100ec7f6a14a554ce
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 17:11:55 -0500

    data_file_update ..\love\public\src\html_on_pointerdown.mjs

--- public/src/html_on_click.mjs
+++
@@ -1,5 +0,0 @@
-import { html_on } from "../../../love/public/src/html_on.mjs";
-export function html_on_click(component, lambda) {
-  const name_event = "pointerdown";
-  html_on(component, name_event, lambda);
-}
