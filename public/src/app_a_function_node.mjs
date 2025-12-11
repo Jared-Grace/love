869c4713172c14cceed33614646f01d206812d70
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 16:30:04 -0500

    data_file_update ..\love\public\src\js_keyword_await.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -101,7 +101,7 @@ export function app_a_function_node(a) {
     ["AwaitExpression"]: function lambda() {
       app_a_keyword_purple_space(parent, text4);
       let v22 = js_keyword_await();
-      let argument = object_property_get(node, "argument");
+      let argument = object_property_get(v22, "argument");
       log({
         node,
       });
