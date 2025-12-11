1e06ed46890f18d72ce8c5d6996f9b6e082f4f93
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:10:23 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -1,9 +1,11 @@
+import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { html_pre_text } from "../../../love/public/src/html_pre_text.mjs";
 import { app_a_api } from "../../../love/public/src/app_a_api.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 export async function app_a_function(context) {
+  app_a_control_style(component);
   let { app_fn, root } = context;
   let f_name = storage_local_get(app_fn, "f_name_selected");
   html_clear(root);
