a040456f5e90e5aaee759d297f8f388383ef799d
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 18:07:08 -0500

    function_auto_path ..\love\public\src\app_a_api_cache_global.mjs

--- public/src/app_a_api_cache_global.mjs
+++ public/src/app_a_api_cache_global.mjs
@@ -1,10 +1,9 @@
+import { app_a_api_generic } from "../../../love/public/src/app_a_api_generic.mjs";
 import { http_post_json_cache_global } from "../../../love/public/src/http_post_json_cache_global.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_api_cache_global(f_name, args) {
   marker("1");
-  let result = await http_post_json_cache_global("api", {
-    f_name,
-    args,
-  });
+  let fn = http_post_json_cache_global;
+  let result = await app_a_api_generic(f_name, args, fn);
   return result;
 }
