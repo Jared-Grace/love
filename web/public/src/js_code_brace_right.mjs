dd0c275fc02b7f7d137cd82d392953283bcd6a43
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:17:38 -0500

    function_auto_path ..\love\public\src\js_code_wrap_braces.mjs

---
+++ web/public/src/js_code_brace_right.mjs
@@ -0,0 +1,4 @@
+export function js_code_brace_right() {
+  let v2 = "}";
+  return v2;
+}
--- web/public/src/js_code_wrap_braces.mjs
+++ web/public/src/js_code_wrap_braces.mjs
@@ -1,5 +1,6 @@
+import { js_code_brace_right } from "../../../love/public/src/js_code_brace_right.mjs";
 import { js_code_brace_left } from "../../../love/public/src/js_code_brace_left.mjs";
 export function js_code_wrap_braces(inside) {
-  let v = js_code_brace_left() + inside + "}";
+  let v = js_code_brace_left() + inside + js_code_brace_right();
   return v;
 }
