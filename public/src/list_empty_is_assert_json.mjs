c7ed5b3a0edb8f9e8c32e481e0de32539d1ff502
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:40:39 -0500

    function_auto_path ..\love\public\src\list_empty_is_assert_json.mjs

---
+++ public/src/list_empty_is_assert_json.mjs
@@ -0,0 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { assert_json } from "../../../love/public/src/assert_json.mjs";
+import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
+export function list_empty_is_assert_json(list, json) {
+  marker("1");
+  let e = list_empty_is(list);
+  assert_json(e, json);
+}
