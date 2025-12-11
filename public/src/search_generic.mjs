0ca11d5962ce27da8dbdb3598d6d707b75e6c985
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:51:01 -0500

    fs search_generic

--- public/src/search_generic.mjs
+++ public/src/search_generic.mjs
@@ -1,9 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
 export function search_generic(search, list, value_get) {
+  marker("1");
   let terms = string_split(search, ",");
   function lambda2(n) {
     function lambda(term) {
