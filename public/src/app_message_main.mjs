6fba24bcb4e0452ba50e5c310de63a155054945b
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 23:32:46 -0500

    data_file_update ..\love\public\src\html_font_sans_serif.mjs

--- public/src/app_message_main.mjs
+++ public/src/app_message_main.mjs
@@ -24,7 +24,7 @@ import { html_check_empty_not } from "../../../love/public/src/html_check_empty_
 import { app_karate_screen_input_validate } from "../../../love/public/src/app_karate_screen_input_validate.mjs";
 import { emoji_email } from "../../../love/public/src/emoji_email.mjs";
 import { app_karate_button_green } from "../../../karate_code/public/src/app_karate_button_green.mjs";
-import { html_font_san_serif } from "../../../love/public/src/html_font_san_serif.mjs";
+import { html_font_sans_serif } from "../../../love/public/src/html_font_sans_serif.mjs";
 import { html_focus } from "../../../love/public/src/html_focus.mjs";
 import { html_placeholder } from "../../../love/public/src/html_placeholder.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
@@ -35,8 +35,8 @@ import { app_karate_style_control } from "../../../karate_code/public/src/app_ka
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { list_map } from "./list_map.mjs";
-import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 export async function app_message_main() {
   marker("1");
   const messages_property = "messages";
@@ -50,7 +50,7 @@ export async function app_message_main() {
   };
   storage_local_initialize_context(context, user_id_property, u);
   app_replace_font_size_refresh(context);
-  html_font_san_serif(context);
+  html_font_sans_serif(context);
   let div_messages = html_div(root);
   let start = app_message_reply_choices();
   await refresh();
