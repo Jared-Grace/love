d6a787653ebb137fe4a251751e33d29abf8efa3d
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:36:17 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -5,7 +5,9 @@ export function app_a_function_node(ast) {
   let lookup = {
     Program: function lambda3() {
       let body = object_property_get(ast, "body");
-      function lambda(b) {}
+      function lambda(b) {
+        app_a_function_node(b);
+      }
       each(body, lambda);
     },
     ["ImportDeclaration"]: function lambda4() {},
