4304151223c93f96437e7a1de56a8e9a0bf9660b
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:16:53 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -1,3 +1,5 @@
+import { app_a_home } from "../../../love/public/src/app_a_home.mjs";
+import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
 import { emoji_search } from "../../../love/public/src/emoji_search.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
@@ -10,7 +12,9 @@ export async function app_a_function(context) {
   let { app_fn, root } = context;
   let f_name = storage_local_get(app_fn, "f_name_selected");
   html_clear(root);
-  function lambda2() {}
+  function lambda2() {
+    app_generic_screen_set(context, app_a_home);
+  }
   let text = emoji_search();
   let b = html_button(root, text, lambda2);
   app_a_control_style(b);
