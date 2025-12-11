74e9298f5ac13dc0315d488823c1569c7556826e
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:00:27 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

---
+++ web/public/src/object_property_get_double_equal_assert.mjs
@@ -0,0 +1,12 @@
+import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export function object_property_get_double_equal_assert(
+  imported,
+  property_name,
+  local,
+) {
+  let name = object_property_get(imported, property_name);
+  let name2 = object_property_get(local, property_name);
+  equal_assert(name, name2);
+  return name;
+}
