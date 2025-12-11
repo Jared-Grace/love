a064f17061d88ef18b05a3e8b3163af3d64abb94
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:04:58 -0500

    function_auto_path ..\love\public\src\html_jetbrains_mono_include.mjs

--- public/src/html_jetbrains_mono_include.mjs
+++ public/src/html_jetbrains_mono_include.mjs
@@ -1,13 +1,8 @@
-import { global_function_initialize_lambda } from "../../../love/public/src/global_function_initialize_lambda.mjs";
+import { global_function_once } from "../../../love/public/src/global_function_once.mjs";
 import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
 export function html_jetbrains_mono_include() {
   let fn = html_jetbrains_mono_include;
-  function value_get() {
-    lambda();
-    let v = true;
-    return v;
-  }
-  global_function_initialize_lambda(fn, value_get);
+  global_function_once(lambda, fn);
   function lambda() {
     const href =
       "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap";
