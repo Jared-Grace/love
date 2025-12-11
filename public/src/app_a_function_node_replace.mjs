c5481d02dc655f538cfd80fcda00ffa1da2b3f21
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:54:15 -0500

    rr app_a_function_node_replace replace child

--- public/src/app_a_function_node_replace.mjs
+++
@@ -1,8 +0,0 @@
-import { app_a_function_node } from "../../../love/public/src/app_a_function_node.mjs";
-import { object_copy_assign } from "../../../love/public/src/object_copy_assign.mjs";
-export function app_a_function_node_replace(a, node_replacement) {
-  let a2 = object_copy_assign(a, {
-    node: node_replacement,
-  });
-  app_a_function_node(a2);
-}
