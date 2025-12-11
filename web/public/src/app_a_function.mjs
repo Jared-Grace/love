72a3195988e6a37c985d874559d9ecf37ac76a09
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:32:23 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_function.mjs
+++ web/public/src/app_a_function.mjs
@@ -34,7 +34,7 @@ export async function app_a_function(context) {
       function lambda(b) {}
       each(body, lambda);
     },
-    ["ImportDeclaration"]: () => {},
+    ["ImportDeclaration"]: function lambda4() {},
   };
   let value = object_property_get(lookup, type);
   value();
