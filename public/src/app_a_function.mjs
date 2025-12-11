b611bcd7b77b35b2a5e8240c7724bc50686dac0b
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:13:06 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -7,13 +7,13 @@ import { app_a_api } from "../../../love/public/src/app_a_api.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 export async function app_a_function(context) {
-  function lambda2() {}
-  let text = emoji_search();
-  let b = html_button(parent, text, lambda2);
-  app_a_control_style(b);
   let { app_fn, root } = context;
   let f_name = storage_local_get(app_fn, "f_name_selected");
   html_clear(root);
+  function lambda2() {}
+  let text = emoji_search();
+  let b = html_button(root, text, lambda2);
+  app_a_control_style(b);
   let function_name = fn_name("function_read");
   let code = await app_a_api(function_name, [f_name]);
   let p = html_pre_text(root, code);
