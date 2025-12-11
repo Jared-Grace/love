0bf309a033ae25bdce0f336606c1f396e68403f7
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:47:47 -0500

    function_auto_path ..\love\public\src\app_a_function_node_replace.mjs

--- public/src/app_a_function_node_replace.mjs
+++ public/src/app_a_function_node_replace.mjs
@@ -1,8 +1,8 @@
 import { app_a_function_node } from "../../../love/public/src/app_a_function_node.mjs";
 import { object_copy_assign } from "../../../love/public/src/object_copy_assign.mjs";
-export function app_a_function_node_replace(a, b) {
+export function app_a_function_node_replace(a, node_replacement) {
   let a2 = object_copy_assign(a, {
-    node: b,
+    node: node_replacement,
   });
   app_a_function_node(a2);
 }
