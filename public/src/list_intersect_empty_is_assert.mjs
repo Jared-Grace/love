584e08f40bb520b61626808b0eba89596011a8f3
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:40:41 -0500

    data_file_update ..\love\public\src\list_empty_is_assert_json.mjs

--- public/src/list_intersect_empty_is_assert.mjs
+++ public/src/list_intersect_empty_is_assert.mjs
@@ -1,8 +1,8 @@
-import { list_empty_is_assert } from "../../../love/public/src/list_empty_is_assert.mjs";
+import { list_empty_is_assert_json } from "../../../love/public/src/list_empty_is_assert_json.mjs";
 import { list_intersect } from "../../../love/public/src/list_intersect.mjs";
 export function list_intersect_empty_is_assert(include, exclude) {
   let i = list_intersect(include, exclude);
-  list_empty_is_assert(i, {
+  list_empty_is_assert_json(i, {
     include,
     exclude,
   });
