fddbe6fa1b32226f4e6cf00db9485c330bb64b64
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:57:35 -0500

    function_auto_path ..\love\public\src\html_font_jetbrains_mono.mjs

--- web/public/src/html_font_jetbrains_mono.mjs
+++ web/public/src/html_font_jetbrains_mono.mjs
@@ -1,3 +1,4 @@
+import { html_jetbrains_mono } from "../../../love/public/src/html_jetbrains_mono.mjs";
 import { html_font_set } from "../../../love/public/src/html_font_set.mjs";
 export function html_font_jetbrains_mono(div) {
   html_jetbrains_mono();
