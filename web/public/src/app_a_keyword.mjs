3eb3412bbf3e8765f20e5f3a8dd5295bb9ccbc76
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:36:14 -0500

    function_auto_path ..\love\public\src\app_a_keyword.mjs

--- web/public/src/app_a_keyword.mjs
+++ web/public/src/app_a_keyword.mjs
@@ -1,7 +1,7 @@
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function app_a_keyword(parent, keyword) {
-  let s2 = html_span_text(parent, keyword);
+  let s = html_span_text(parent, keyword);
   const color = "purple";
-  html_font_color_set(s2, color);
+  html_font_color_set(s, color);
 }
