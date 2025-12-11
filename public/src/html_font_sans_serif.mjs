b81c9a50be21820e22451018ac2b5f95a20fb0fe
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 23:33:52 -0500

    function_auto_path ..\love\public\src\html_font_sans_serif.mjs

--- public/src/html_font_sans_serif.mjs
+++ public/src/html_font_sans_serif.mjs
@@ -1,7 +1,7 @@
+import { html_font_set } from "../../../love/public/src/html_font_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_font_sans_serif_value } from "../../../love/public/src/html_font_sans_serif_value.mjs";
 import { html_document_root } from "../../../love/public/src/html_document_root.mjs";
-import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
 export function html_font_sans_serif(context) {
   marker("1");
@@ -12,5 +12,5 @@ export function html_font_sans_serif(context) {
     "font_family",
     value_initial,
   );
-  html_style_set(html, "font-family", value);
+  html_font_set(html, value);
 }
