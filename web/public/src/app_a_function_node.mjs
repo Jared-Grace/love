9f4ff0cac66f94cf4e6576ce9c7d1b7e72f94f72
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:20:00 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -1,3 +1,4 @@
+import { app_a_function_node_child_parent } from "../../../love/public/src/app_a_function_node_child_parent.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { js_code_brace_right } from "../../../love/public/src/js_code_brace_right.mjs";
 import { js_code_brace_left } from "../../../love/public/src/js_code_brace_left.mjs";
@@ -19,7 +20,7 @@ export function app_a_function_node(a) {
       let body = object_property_get(node, "body");
       function lambda(b) {
         let div = html_div(parent);
-        app_a_function_node_child(a, b);
+        app_a_function_node_child_parent(a, b);
       }
       each(body, lambda);
     },
