3f2f9b0065462f2acd38e4d962bc35d266deac26
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 16:31:11 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -99,7 +99,6 @@ export function app_a_function_node(a) {
       app_a_function_node_child(a, expression);
     },
     ["AwaitExpression"]: function lambda() {
-      app_a_keyword_purple_space(parent, text4);
       let k = js_keyword_await();
       app_a_keyword_purple_space(parent, k);
       let argument = object_property_get(v22, "argument");
