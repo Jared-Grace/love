e5688e9a00862299a6d503c2c941dc3927e53aa2
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:37:55 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -1,18 +1,22 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function app_a_function_node(ast) {
-  let type = object_property_get(ast, "type");
+export function app_a_function_node(node) {
+  let type = object_property_get(node, "type");
   let lookup = {
     Program: function lambda3() {
-      let body = object_property_get(ast, "body");
+      let body = object_property_get(node, "body");
       function lambda(b) {
         app_a_function_node(b);
       }
       each(body, lambda);
     },
-    ["ImportDeclaration"]: log,
-    ["ExportNamedDeclaration"]: log,
+    ["ImportDeclaration"]: function lambda4() {
+      log(node);
+    },
+    ["ExportNamedDeclaration"]: function lambda5() {
+      log(node);
+    },
   };
   let value = object_property_get(lookup, type);
   value();
