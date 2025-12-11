bbc8008389e78ffc1277bea0f6414cd6895d60b0
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:25:43 -0500

    function_auto_path ..\love\public\src\js_code_statement.mjs

--- public/src/js_code_statement.mjs
+++ public/src/js_code_statement.mjs
@@ -1,4 +1,5 @@
+import { js_code_semicolon } from "../../../love/public/src/js_code_semicolon.mjs";
 export function js_code_statement(code) {
-  let v = code + ";";
+  let v = code + js_code_semicolon();
   return v;
 }
