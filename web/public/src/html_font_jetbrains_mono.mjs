9ab9af5d1bc0413ef14ccd9ff3ccbe0389f844bd
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:58:15 -0500

    rs html_jetbrains_mono include

--- web/public/src/html_font_jetbrains_mono.mjs
+++ web/public/src/html_font_jetbrains_mono.mjs
@@ -1,6 +1,6 @@
-import { html_jetbrains_mono } from "../../../love/public/src/html_jetbrains_mono.mjs";
+import { html_jetbrains_mono_include } from "../../../love/public/src/html_jetbrains_mono_include.mjs";
 import { html_font_set } from "../../../love/public/src/html_font_set.mjs";
 export function html_font_jetbrains_mono(div) {
-  html_jetbrains_mono();
+  html_jetbrains_mono_include();
   html_font_set(div, '"JetBrains Mono", monospace');
 }
