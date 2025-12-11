b1b30664fdd68b14845f4d76512c90227ce33c91
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:19:40 -0500

    function_auto_path ..\love\public\src\app_a_function_node_child_parent.mjs

--- public/src/app_a_function_node_child_parent.mjs
+++ public/src/app_a_function_node_child_parent.mjs
@@ -4,6 +4,7 @@ export function app_a_function_node_child_parent(a, node_replacement) {
   marker("1");
   const replacements = {
     node: node_replacement,
+    parent: parent_replacement,
   };
   app_a_function_node_replace(a, replacements);
 }
