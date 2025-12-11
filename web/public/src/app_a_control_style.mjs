dc9920ac75727417c7024db12e2f4b74dbe51414
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:09:11 -0500

    function_auto_path ..\love\public\src\app_a_control_style.mjs

--- web/public/src/app_a_control_style.mjs
+++ web/public/src/app_a_control_style.mjs
@@ -3,8 +3,10 @@ import { html_style_assign } from "../../../love/public/src/html_style_assign.mj
 export function app_a_control_style(component) {
   html_style_assign(component, {
     "border-radius": "0.8em",
-    width: "100%",
     "border-width": "0px",
   });
   html_style_margin_y(component, "2px");
+  html_style_assign(component, {
+    width: "100%",
+  });
 }
