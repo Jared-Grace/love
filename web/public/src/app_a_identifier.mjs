94363a1923965fd06b8b0aabdeeeebb0981d5b77
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:48:08 -0500

    data_file_update ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_identifier.mjs
+++ web/public/src/app_a_identifier.mjs
@@ -1,8 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_identifier_is_assert } from "../../../love/public/src/js_identifier_is_assert.mjs";
 export function app_a_identifier(local, parent) {
+  marker("1");
   js_identifier_is_assert(local);
   let name = object_property_get(local, "name");
   let span = html_span_text(parent, name);
