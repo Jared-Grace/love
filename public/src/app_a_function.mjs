93ebfd09def174055f095ae7159e571b58df33a1
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:11:15 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -1,3 +1,4 @@
+import { html_button } from "../../../love/public/src/html_button.mjs";
 import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { html_pre_text } from "../../../love/public/src/html_pre_text.mjs";
@@ -5,7 +6,7 @@ import { app_a_api } from "../../../love/public/src/app_a_api.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 export async function app_a_function(context) {
-  hb;
+  let component2 = html_button(parent, text, function lambda2() {});
   app_a_control_style(component);
   let { app_fn, root } = context;
   let f_name = storage_local_get(app_fn, "f_name_selected");
