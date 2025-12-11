25f42469b9edd659890f4cf58f1a1880f814076e
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:41:46 -0500

    data_file_update ..\love\public\src\list_empty_is_assert.mjs

--- web/public/src/storage_local_keys_context_empty_is_assert.mjs
+++ web/public/src/storage_local_keys_context_empty_is_assert.mjs
@@ -2,5 +2,5 @@ import { list_empty_is_assert_json } from "../../../love/public/src/list_empty_i
 import { storage_local_keys_context } from "../../../love/public/src/storage_local_keys_context.mjs";
 export function storage_local_keys_context_empty_is_assert(context) {
   let keys = storage_local_keys_context(context);
-  list_empty_is_assert_json(keys);
+  list_empty_is_assert(keys);
 }
