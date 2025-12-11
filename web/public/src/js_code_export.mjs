b758a8bacd3be89b53830538339defc69d52feba
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:34:00 -0500

    function_auto_path ..\love\public\src\js_code_export.mjs

--- web/public/src/js_code_export.mjs
+++ web/public/src/js_code_export.mjs
@@ -1,4 +1,5 @@
+import { js_keyword_export } from "../../../love/public/src/js_keyword_export.mjs";
 export function js_code_export(code_declaration) {
-  let v = "export" + " " + code_declaration;
+  let v = js_keyword_export() + " " + code_declaration;
   return v;
 }
