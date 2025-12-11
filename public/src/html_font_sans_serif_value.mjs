ccf9752fa4a170321db4ca373d94e7c92d77de49
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 23:32:53 -0500

    function_auto_path ..\love\public\src\html_font_sans_serif_value.mjs

--- public/src/html_font_sans_serif_value.mjs
+++ public/src/html_font_sans_serif_value.mjs
@@ -1,4 +1,6 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 export function html_font_sans_serif_value() {
+  marker("1");
   let v = '"Roboto", Arial, Helvetica, sans-serif';
   return v;
 }
