b615ed637680684a9c5b34947ec165600b9e6d03
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:41:42 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

---
+++ web/public/src/list_empty_is_assert.mjs
@@ -0,0 +1,4 @@
+import { list_empty_is_assert_json } from "../../../love/public/src/list_empty_is_assert_json.mjs";
+export function list_empty_is_assert(specifiers) {
+  list_empty_is_assert_json(specifiers, {});
+}
