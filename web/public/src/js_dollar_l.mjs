e80916c8708523adbe87250d5601a7c60bdfc5f0
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 16:01:59 -0500

    data_file_update ..\love\public\src\js_dollar.mjs

--- web/public/src/js_dollar_l.mjs
+++ web/public/src/js_dollar_l.mjs
@@ -4,6 +4,7 @@ import { js_parse_expression } from "../../../love/public/src/js_parse_expressio
 import { js_code_braces_empty } from "../../../love/public/src/js_code_braces_empty.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
+import { object_replace } from "../../../love/public/src/object_replace.mjs";
 export function js_dollar_l({
   remaining,
   node,
@@ -16,8 +17,5 @@ export function js_dollar_l({
   let v = js_code_braces_empty();
   let code = js_code_call_args(log.name, [v]);
   let parsed = js_parse_expression(code);
-  js_node_type_is_assert(stack1, "ExpressionStatement");
-  object_property_set(stack1, "expression", parsed);
-  return;
-  log("jg");
+  object_replace(node, parsed);
 }
