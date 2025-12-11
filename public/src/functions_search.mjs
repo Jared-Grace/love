061ae8a630ad437143067894d468d44bcbcfe9b1
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:53:38 -0500

    function_auto_path ..\love\public\src\functions_search.mjs

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -1,12 +1,11 @@
+import { functions_search_generic } from "../../../love/public/src/functions_search_generic.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { search_generic } from "../../../love/public/src/search_generic.mjs";
-import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function functions_search(search) {
   marker("1");
   let fn = string_includes;
   let f_names = await functions_names();
-  let result = search_generic(search, f_names, function_name_to_path, fn);
+  let result = functions_search_generic(search, f_names, fn);
   return result;
 }
