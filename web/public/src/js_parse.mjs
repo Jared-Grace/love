ce4b2009e19c430261c0a04e3114ce0b0c1cc202
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 19:23:37 -0500

    function_auto_path ..\love\public\src\js_parse.mjs

--- web/public/src/js_parse.mjs
+++ web/public/src/js_parse.mjs
@@ -1,6 +1,6 @@
 import { js_parse_generic } from "../../../love/public/src/js_parse_generic.mjs";
 import * as acorn from "acorn";
 export function js_parse(code) {
-  let v = js_parse_generic(acorn, code);
-  return v;
+  let ast = js_parse_generic(acorn, code);
+  return ast;
 }
