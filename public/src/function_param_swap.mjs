1e867780bfc6188028a3798ab920b75c5e6c6aa7
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:05:58 -0500

    function_auto_path ..\love\public\src\function_param_swap.mjs

--- public/src/function_param_swap.mjs
+++ public/src/function_param_swap.mjs
@@ -1,3 +1,4 @@
+import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 import { function_param_index } from "../../../love/public/src/function_param_index.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
@@ -8,6 +9,8 @@ import { js_declaration_param_named } from "../../../love/public/src/js_declarat
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 import { error } from "./error.mjs";
 export async function function_param_swap(param_name_a, param_name_b) {
+  if (false) {
+  }
   error(
     "TODO: finish this; consider similar code in " + function_params_new.name,
   );
@@ -20,14 +23,16 @@ export async function function_param_swap(param_name_a, param_name_b) {
     list_swap(params, a, b);
   }
   marker("1");
-  let index = null;
+  let index_a = null;
+  let index_b = null;
   await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
-    fn(args, index);
+    list_swap_at(args, index_a, index_b);
   }
   function function_transform_current_lambda(ast) {
     let params = null;
-    ({ params, index } = function_param_index(ast, param_name));
-    fn(params, index);
+    ({ params, index_a } = function_param_index(ast, param_name_a));
+    ({ index_b } = function_param_index(ast, param_name_a));
+    list_swap_at(params, index_a, index_b);
   }
 }
