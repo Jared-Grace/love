eb91fc6866d88c7ea13f6e0277470875a0b36dc2
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:00:31 -0500

    function_auto_path ..\love\public\src\global_function_initialize_lambda.mjs

--- public/src/global_function_initialize_lambda.mjs
+++ public/src/global_function_initialize_lambda.mjs
@@ -1,9 +1,7 @@
 import { object_property_initialize_lambda } from "../../../love/public/src/object_property_initialize_lambda.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { global_get } from "./global_get.mjs";
 export function global_function_initialize_lambda(fn, lambda) {
-  marker("1");
   assert_arguments(arguments, 2);
   let global = global_get();
   let value = object_property_initialize_lambda(global, fn.name, lambda);
