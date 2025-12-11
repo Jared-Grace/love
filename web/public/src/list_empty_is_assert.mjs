77376754a6d0bf0db634ddc4401e9f2d790a792e
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:40:41 -0500

    data_file_update ..\love\public\src\list_empty_is_assert_json.mjs

--- web/public/src/list_empty_is_assert.mjs
+++
@@ -1,6 +0,0 @@
-import { assert_json } from "../../../love/public/src/assert_json.mjs";
-import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
-export function list_empty_is_assert(list, json) {
-  let e = list_empty_is(list);
-  assert_json(e, json);
-}
