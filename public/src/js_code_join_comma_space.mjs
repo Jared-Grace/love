b18a4d6f2c2807b98d9b9badc3e4a01078356269
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 16:39:07 -0500

    function_auto_path ..\love\public\src\js_code_join_comma_space.mjs

--- public/src/js_code_join_comma_space.mjs
+++ public/src/js_code_join_comma_space.mjs
@@ -1,7 +1,8 @@
+import { js_code_comma } from "../../../love/public/src/js_code_comma.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
 export function js_code_join_comma_space(args) {
   marker("1");
-  let v = list_join(args, "," + " ");
+  let v = list_join(args, js_code_comma() + " ");
   return v;
 }
