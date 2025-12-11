6ad1f8401f5a3a655d5520a27d900021aba7ee1f
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 17:16:19 -0500

    rs http_post_json_cache storage_local

--- public/src/app_a_api_cache.mjs
+++ public/src/app_a_api_cache.mjs
@@ -1,7 +1,7 @@
 import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
-import { http_post_json_cache } from "../../../love/public/src/http_post_json_cache.mjs";
+import { http_post_json_cache_storage_local } from "../../../love/public/src/http_post_json_cache_storage_local.mjs";
 export async function app_a_api_cache(f_name, args) {
   let fn = http_post_json;
-  let result = await http_post_json_cache(f_name, args, fn);
+  let result = await http_post_json_cache_storage_local(f_name, args, fn);
   return result;
 }
