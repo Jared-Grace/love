fc23e3d8e7df42dc4d938594b600d33ab7e9fb0d
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:14:16 -0500

    rs html_roboto include

--- web/public/src/app_g_main.mjs
+++ web/public/src/app_g_main.mjs
@@ -4,7 +4,7 @@ import { html_hide_loadable } from "../../../love/public/src/html_hide_loadable.
 import { g_icon_cross_unpositioned } from "../../../love/public/src/g_icon_cross_unpositioned.mjs";
 import { g_tutorials_each } from "../../../love/public/src/g_tutorials_each.mjs";
 import { html_remix_icon } from "../../../love/public/src/html_remix_icon.mjs";
-import { html_roboto } from "../../../love/public/src/html_roboto.mjs";
+import { html_roboto_include } from "../../../love/public/src/html_roboto_include.mjs";
 import { html_style_head } from "../../../love/public/src/html_style_head.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
@@ -57,7 +57,7 @@ export async function app_g_main() {
     "font-family": style_value,
     "font-size": "18px",
   });
-  html_roboto();
+  html_roboto_include();
   html_remix_icon();
   html_style_head(
     "button { font-family: " + html_font_sans_serif_value() + " }",
