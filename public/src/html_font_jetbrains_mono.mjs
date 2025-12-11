7ff0f50ae6843f5cc51598156b50b86f8bb928cb
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:01:57 -0500

    function_auto_path ..\love\public\src\html_font_jetbrains_mono.mjs

--- public/src/html_font_jetbrains_mono.mjs
+++ public/src/html_font_jetbrains_mono.mjs
@@ -4,7 +4,8 @@ import { html_font_set } from "../../../love/public/src/html_font_set.mjs";
 export function html_font_jetbrains_mono(div) {
   function lambda2() {
     html_jetbrains_mono_include();
-    return true;
+    let v = true;
+    return v;
   }
   let value = global_function_initialize_lambda(fn, lambda2);
   html_font_set(div, '"JetBrains Mono", monospace');
