57207f80832e31b55b92b00766bdb271b1b21897
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:14:27 -0500

    function_auto_path ..\love\public\src\html_roboto_include.mjs

--- public/src/html_roboto_include.mjs
+++ public/src/html_roboto_include.mjs
@@ -3,11 +3,11 @@ import { global_function_once } from "../../../love/public/src/global_function_o
 import { html_stylesheet } from "../../../love/public/src/html_stylesheet.mjs";
 export function html_roboto_include() {
   marker("1");
+  const href =
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
   let fn = html_roboto_include;
   global_function_once(lambda, fn);
   function lambda() {
-    const href =
-      "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
     html_stylesheet(href);
   }
 }
