309c2172831c66a855613bbdec5baf1771ec3da4
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 18:58:32 -0500

    data_file_update ..\love\public\src\app_a_body.mjs

--- web/public/src/app_a_body.mjs
+++ web/public/src/app_a_body.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_a_function_node_child_parent } from "../../../love/public/src/app_a_function_node_child_parent.mjs";
@@ -7,8 +6,7 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 export function app_a_body(node, parent, a) {
   let body = object_property_get(node, "body");
   let imports = true;
-  function lambda(b) {
-    log(message);
+  function lambda(b) {$l
     if (imports) {
       let type = object_property_get(b, "type");
       let ti = js_node_type_is(b, "ExportNamedDeclaration");
