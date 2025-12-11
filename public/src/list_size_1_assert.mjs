cae3d1f6c9a4ef6e4a7d0e21016a98897c34293b
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:07:18 -0500

    function_auto_path ..\love\public\src\list_size_1_assert.mjs

--- public/src/list_size_1_assert.mjs
+++ public/src/list_size_1_assert.mjs
@@ -2,7 +2,7 @@ import { list_size_1_assert_message } from "../../../love/public/src/list_size_1
 import { string_to } from "../../../love/public/src/string_to.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_size_1_assert(list) {
-  let message = list_size(list);
-  let message2 = string_to(message);
+  let size = list_size(list);
+  let message2 = string_to(size);
   list_size_1_assert_message(list, message2);
 }
