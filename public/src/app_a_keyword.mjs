15e1a7509d9defcc2270a50eb8cc2157ff72211c
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:24:23 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

---
+++ public/src/app_a_keyword.mjs
@@ -0,0 +1,7 @@
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+export function app_a_keyword(parent, text) {
+  let s2 = html_span_text(parent, text);
+  const color = "purple";
+  html_font_color_set(s2, color);
+}
