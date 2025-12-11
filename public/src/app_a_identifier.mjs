3ee3434777840a423a2651fc83b82155812d4b95
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 14:48:02 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

---
+++ public/src/app_a_identifier.mjs
@@ -0,0 +1,10 @@
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { js_identifier_is_assert } from "../../../love/public/src/js_identifier_is_assert.mjs";
+export function app_a_identifier(local, parent) {
+  js_identifier_is_assert(local);
+  let name = object_property_get(local, "name");
+  let span = html_span_text(parent, name);
+  html_font_color_set(span, "blue");
+}
