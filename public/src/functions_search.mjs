5777c6442ccb316ce1aa782d9cc44ff8ab7ad769
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:51:50 -0500

    pn include string_includes

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -5,6 +5,11 @@ import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function functions_search(search) {
   marker("1");
   let f_names = await functions_names();
-  let result = search_generic(search, f_names, function_name_to_path);
+  let result = search_generic(
+    search,
+    f_names,
+    function_name_to_path,
+    string_includes,
+  );
   return result;
 }
