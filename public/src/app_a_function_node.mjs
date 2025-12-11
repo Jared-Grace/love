c71598d4dbc0a6dcabe921f0292b752035c62913
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 16:26:28 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -98,7 +98,7 @@ export function app_a_function_node(a) {
       let expression = object_property_get(node, "expression");
       app_a_function_node_child(a, expression);
     },
-    ["AwaitExpression"]: () => {},
+    ["AwaitExpression"]: function lambda() {},
   };
   let value = object_property_get(lookup, type);
   value();
