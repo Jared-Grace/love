218d133bc731a230b5a16b5464f2eb822a8e6fe2
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 15:55:37 -0500

    data_file_update ..\love\public\src\git_commit_folder.mjs

--- web/public/src/git_commit_folder.mjs
+++ web/public/src/git_commit_folder.mjs
@@ -1,8 +1,8 @@
-import { catch_only_async } from "../../../love/public/src/catch_only_async.mjs";
+import { catch_ignore } from "../../../love/public/src/catch_ignore.mjs";
 import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
 export async function git_commit_folder(folder, message) {
   async function lambda() {
     await command_line_git_folder(folder, `commit -m "${message}"`);
   }
-  await catch_only_async(lambda, "nothing to commit");
+  await catch_ignore(lambda, "nothing to commit");
 }
