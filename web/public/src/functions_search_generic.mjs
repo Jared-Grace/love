10241873837d616a76160a54d401266483b2d54a
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:54:19 -0500

    function_auto_path ..\love\public\src\functions_search_generic.mjs

--- web/public/src/functions_search_generic.mjs
+++ web/public/src/functions_search_generic.mjs
@@ -1,8 +1,8 @@
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { search_generic } from "../../../love/public/src/search_generic.mjs";
-export async function functions_search_generic(search, f_names, fn) {
-  let f_names2 = await functions_names();
+export async function functions_search_generic(search, f_names2, fn) {
+  let f_names = await functions_names();
   let result2 = search_generic(search, f_names, function_name_to_path, fn);
   return result2;
 }
