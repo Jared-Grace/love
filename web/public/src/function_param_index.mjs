7b5c0570abc70e6f3eff6e602f7c6d4c9b40abab
Grace-Jared <christrosetolife@gmail.com>
2025-12-10 20:09:14 -0500

    function_auto_path ..\love\public\src\function_param_index.mjs

--- web/public/src/function_param_index.mjs
+++ web/public/src/function_param_index.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
@@ -7,6 +8,7 @@ export function function_param_index(ast, param_name) {
   marker("1");
   let declaration = js_declaration_single(ast);
   let params = js_declaration_params_get(declaration);
+  log(message);
   let p = js_declaration_param_named(declaration, param_name);
   let index = list_index_of(params, p);
   let v = {
