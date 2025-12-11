2a00c1daf38dbfed7b2f0718ac75b571cdc44260
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:54:29 -0500

    fs functions_search_generic

--- public/src/functions_search_generic.mjs
+++ public/src/functions_search_generic.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { search_generic } from "../../../love/public/src/search_generic.mjs";
 export async function functions_search_generic(search, f_names2, fn) {
+  marker("1");
   let f_names = await functions_names();
   let result2 = search_generic(search, f_names, function_name_to_path, fn);
   return result2;
