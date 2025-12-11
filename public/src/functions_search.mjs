43226ddfefa151cdfa9e6eecfe115f59804859cd
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 13:53:00 -0500

    function_auto_path ..\love\public\src\functions_search.mjs

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -5,6 +5,7 @@ import { function_name_to_path } from "../../../love/public/src/function_name_to
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function functions_search(search) {
   marker("1");
+  let fn = string_includes;
   let f_names = await functions_names();
   let result = search_generic(
     search,
