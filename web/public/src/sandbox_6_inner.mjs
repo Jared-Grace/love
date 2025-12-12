3af437b41779b3bcf6b549a4bd77662b18d196d3
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 21:15:49 -0500

    function_auto_path ..\love\public\src\sandbox_6_inner.mjs

--- web/public/src/sandbox_6_inner.mjs
+++ web/public/src/sandbox_6_inner.mjs
@@ -1,6 +1,4 @@
-import { sleep } from "../../../love/public/src/sleep.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-export async function sandbox_6_inner() {
+export function sandbox_6_inner() {
   log("hello");
-  await sleep();
 }
