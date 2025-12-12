09da1b30df2103a24093d1a5ac76e9f5e3d040a3
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 21:02:41 -0500

    function_auto_path ..\love\public\src\app_a_body.mjs

--- public/src/app_a_body.mjs
+++ public/src/app_a_body.mjs
@@ -17,7 +17,7 @@ export function app_a_body(node, parent, a) {
       let type = object_property_get(b, "type");
       if (equal(type, t)) {
         imports = true;
-        let div2 = html_div(root);
+        parent_new = html_div(root);
       }
     }
     if (imports) {
