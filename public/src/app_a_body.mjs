b66f9c2ecbd46591080870be901f2a59531897dc
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 21:01:00 -0500

    function_auto_path ..\love\public\src\app_a_body.mjs

--- public/src/app_a_body.mjs
+++ public/src/app_a_body.mjs
@@ -1,3 +1,4 @@
+import { equal } from "../../../love/public/src/equal.mjs";
 import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
@@ -13,7 +14,8 @@ export function app_a_body(node, parent, a) {
     let v = list_first_is(list, item);
     if (v) {
       let type = object_property_get(b, "type");
-      $ie;
+      if (equal(left, right)) {
+      }
     }
     if (imports) {
       let ti = js_node_type_is(b, t);
