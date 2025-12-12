ba2d7d0562efcfa3b70b510ccfb5d6394e57df13
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 18:29:00 -0500

    function_auto_path ..\love\public\src\html_display_none_or_block.mjs

--- web/public/src/html_display_none_or_block.mjs
+++ web/public/src/html_display_none_or_block.mjs
@@ -1,7 +1,7 @@
 import { html_display_block } from "../../../love/public/src/html_display_block.mjs";
 import { html_display_none } from "../../../love/public/src/html_display_none.mjs";
-export function html_display_none_or_block(condition, item) {
-  if (condition) {
+export function html_display_none_or_block(hidden, item) {
+  if (hidden) {
     html_display_none(item);
   } else {
     html_display_block(item);
