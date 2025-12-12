8cc79c2968714225fe25a7583c57720415e4580f
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 18:26:18 -0500

    function_auto_path ..\love\public\src\app_a_body.mjs

---
+++ public/src/html_style_background_color_yellow.mjs
@@ -0,0 +1,4 @@
+import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
+export function html_style_background_color_yellow(parent_new) {
+  html_style_background_color(parent_new, "yellow");
+}
