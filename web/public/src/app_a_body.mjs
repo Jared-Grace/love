12609b8ee013685ee26528332a57e4eb46b57ccd
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 21:03:48 -0500

    function_auto_path ..\love\public\src\app_a_body.mjs

--- web/public/src/app_a_body.mjs
+++ web/public/src/app_a_body.mjs
@@ -1,7 +1,7 @@
+import { js_node_type_not_is } from "../../../love/public/src/js_node_type_not_is.mjs";
 import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
-import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_a_function_node_child_parent } from "../../../love/public/src/app_a_function_node_child_parent.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
@@ -22,10 +22,8 @@ export function app_a_body(node, parent, a) {
       }
     }
     if (imports) {
-      let ti = js_node_type_is(b, t);
+      let ti = js_node_type_not_is(b, t);
       if (ti) {
-        return;
-      } else {
         imports = false;
         parent_new = parent;
       }
