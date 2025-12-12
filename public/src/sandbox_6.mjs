b705a3c659020aaf9ecaa3b0e98abf651036c26e
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 21:15:50 -0500

    data_file_update ..\love\public\src\sandbox_6_inner.mjs

--- public/src/sandbox_6.mjs
+++ public/src/sandbox_6.mjs
@@ -1,8 +1,7 @@
 import { sandbox_6_inner } from "../../../love/public/src/sandbox_6_inner.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function sandbox_6() {
+export function sandbox_6() {
   marker("1");
-  await sandbox_6_inner();
-  let v = 1;
-  return v;
+  sandbox_6_inner();
+  return 1;
 }
