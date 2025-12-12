aae720b7ad9e80e21466d751718716b7c3fd6eca
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 19:53:42 -0500

    function_auto_path ..\love\public\src\list_first_is.mjs

--- web/public/src/list_first_is.mjs
+++ web/public/src/list_first_is.mjs
@@ -1,8 +1,8 @@
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { list_last } from "../../../love/public/src/list_last.mjs";
 export function list_first_is(list, item) {
   marker("1");
-  let last = list_last(list);
+  let last = list_first(list);
   let v = last === item;
   return v;
 }
