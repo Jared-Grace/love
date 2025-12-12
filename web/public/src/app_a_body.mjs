61fc853e00040b5506054206a1f19219c098b127
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 18:29:54 -0500

    function_auto_path ..\love\public\src\app_a_body.mjs

--- web/public/src/app_a_body.mjs
+++ web/public/src/app_a_body.mjs
@@ -26,6 +26,7 @@ export function app_a_body(node, parent, a) {
           html_display_none_or_block(hidden, parent_new);
         }
         app_a_button_wide(parent, "text", imports_refresh);
+        imports_refresh();
       }
     }
     if (imports) {
