6c3e16a4cd887515e56942cf92ace5e82f5a6a05
Grace-Jared <christrosetolife@gmail.com>
2025-12-11 21:14:16 -0500

    function_auto_path ..\love\public\src\sandbox_6.mjs

--- web/public/src/sandbox_6.mjs
+++ web/public/src/sandbox_6.mjs
@@ -1,4 +1,6 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function sandbox_6() {
   marker("1");
+  log(message);
 }
