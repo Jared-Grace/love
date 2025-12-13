520a0458c7f91ebcee552cd099e6156841c357ae
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:03:29 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

--- web/public/src/function_rename.mjs
+++ web/public/src/function_rename.mjs
@@ -1,35 +1,13 @@
+import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
-import { object_property_exists_if_async } from "../../../love/public/src/object_property_exists_if_async.mjs";
-import { data_identifiers_fn_names_get } from "../../../love/public/src/data_identifiers_fn_names_get.mjs";
 import { function_move } from "../../../love/public/src/function_move.mjs";
 import { function_rename_identifiers } from "../../../love/public/src/function_rename_identifiers.mjs";
 import { function_alias_rename } from "../../../love/public/src/function_alias_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { error_json } from "./error_json.mjs";
-import { fn_name } from "./fn_name.mjs";
 export async function function_rename(f_name_before, f_name_after) {
   const { unaliased } = await function_name_to_path_unalias(f_name_before);
   f_name_before = unaliased;
-  let i = await data_identifiers_fn_names_get();
-  async function lambda(value) {
-    error_json({
-      message:
-        "You are trying to rename: " +
-        f_name_before +
-        ". However that is referenced by " +
-        value +
-        " through " +
-        fn_name.name +
-        ". TODO: " +
-        function_rename.name +
-        " needs enhancing to rename " +
-        fn_name.name +
-        " references.",
-      f_name_before,
-      value,
-    });
-  }
-  await object_property_exists_if_async(i, f_name_before, lambda);
+  await function_rename_fn_names_check(f_name_before);
   await function_move(f_name_before, f_name_after);
   let v = await function_alias_rename(f_name_before, f_name_after);
   await function_rename_identifiers(f_name_before, f_name_after);
---
+++ web/public/src/function_rename_fn_names_check.mjs
@@ -0,0 +1,27 @@
+import { object_property_exists_if_async } from "../../../love/public/src/object_property_exists_if_async.mjs";
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
+import { error_json } from "../../../love/public/src/error_json.mjs";
+import { data_identifiers_fn_names_get } from "../../../love/public/src/data_identifiers_fn_names_get.mjs";
+export async function function_rename_fn_names_check(f_name_before) {
+  let i = await data_identifiers_fn_names_get();
+  async function lambda(value) {
+    error_json({
+      message:
+        "You are trying to rename: " +
+        f_name_before +
+        ". However that is referenced by " +
+        value +
+        " through " +
+        fn_name.name +
+        ". TODO: " +
+        function_rename.name +
+        " needs enhancing to rename " +
+        fn_name.name +
+        " references.",
+      f_name_before,
+      value,
+    });
+  }
+  await object_property_exists_if_async(i, f_name_before, lambda);
+}
