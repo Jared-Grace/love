61917b0d154de6a11d89d35a5236fbbe3e6043f8
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:24:52 -0500

    function_auto_path ..\love\public\src\invoke_cache_file.mjs

--- web/public/src/invoke_cache_file.mjs
+++ web/public/src/invoke_cache_file.mjs
@@ -1,8 +1,9 @@
+import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function invoke_cache_file(fn, args) {
   marker("1");
-    let key_get = invoke_cache_key_get(fn, args);
+  let key_get = invoke_cache_key_get(fn, args);
   let v = await cache_generic(
     key_get,
     cached_exists,
