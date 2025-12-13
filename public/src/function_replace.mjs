e92faa6af419fa3d306fb5a3564aabaaebb4b19f
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:05:21 -0500

    function_auto_path ..\love\public\src\function_replace.mjs

--- public/src/function_replace.mjs
+++ public/src/function_replace.mjs
@@ -1,15 +1,12 @@
-import { function_rename_identifiers } from "../../../love/public/src/function_rename_identifiers.mjs";
-import { function_alias_rename } from "../../../love/public/src/function_alias_rename.mjs";
-import { function_move } from "../../../love/public/src/function_move.mjs";
 import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_replace(f_name_from, f_name_to) {
+import { function_delete } from "./function_delete.mjs";
+export async function function_replace(f_name_before, f_name_after) {
   const { unaliased } = await function_name_to_path_unalias(f_name_before);
-  let f_name_before = unaliased;
+  f_name_before = unaliased;
   await function_rename_fn_names_check(f_name_before);
-  await function_move(f_name_before, f_name_after);
-  let v = await function_alias_rename(f_name_before, f_name_after);
-  await function_rename_identifiers(f_name_before, f_name_after);
+  await function_delete(f_name_before);
+  await function_rename_identifiers_alias(f_name_before, f_name_after);
   marker("1");
 }
