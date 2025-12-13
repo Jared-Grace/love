a37c4144d6cb4c59acc10d3bc5ec6118fdd2c2f8
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:13:27 -0500

    function_auto_path ..\love\public\src\bible_interlinear_verses_upload.mjs

---
+++ web/public/src/bible_interlinear_verses.mjs
@@ -0,0 +1,23 @@
+import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
+import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
+import { bible_interlinear_verses_upload_folder } from "../../../love/public/src/bible_interlinear_verses_upload_folder.mjs";
+import { bible_interlinear_chapters } from "../../../love/public/src/bible_interlinear_chapters.mjs";
+export async function bible_interlinear_verses() {
+  let chapters = await bible_interlinear_chapters();
+  let bible_folder = bible_interlinear_verses_upload_folder();
+  async function lambda(la) {
+    async function lambda6(verses, chapter_code) {
+      la({
+        chapter_code,
+        verses,
+      });
+    }
+    await each_object_async(chapters, lambda6);
+  }
+  let list = await list_adder_async(lambda);
+  let v = {
+    bible_folder,
+    list,
+  };
+  return v;
+}
