a797f97659a0b1aaa67997885facad050579249e
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:24:50 -0500

    data_file_update ..\love\public\src\invoke_cache_global.mjs

--- public/src/invoke_cache_file.mjs
+++ public/src/invoke_cache_file.mjs
@@ -2,6 +2,7 @@ import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function invoke_cache_file(fn, args) {
   marker("1");
+    let key_get = invoke_cache_key_get(fn, args);
   let v = await cache_generic(
     key_get,
     cached_exists,
