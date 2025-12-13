9742b690f214b60640e351869914ea0a9b27b46e
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 14:58:00 -0500

    function_auto_path ..\love\public\src\app_reply_local.mjs

--- public/src/app_reply_local.mjs
+++ public/src/app_reply_local.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_reply_local() {
   await app_main_production("app_reply_local_main", "jared-grace");
 }
