f18c5d8acd3e882c79201e3f604d0f3394f4db8b
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 21:49:22 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -154,7 +154,8 @@ export function app_a_function_node(a) {
       let key = object_property_get(node, "key");
       let shorthand = object_property_get(node, "shorthand");
       let method = object_property_get(node, "method");
-      let computed = object_property_get(node, "computed");app_a_braces_wrap
+      let computed = object_property_get(node, "computed");
+      app_a_braces_wrap(parent2, function lambda16() {});
       log({
         node,
       });
