24c7e23ef3a2099f925ac5ed42e230bfadb827a3
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:25:54 -0500

    function_auto_path ..\love\public\src\function_cache.mjs

--- public/src/function_cache.mjs
+++ public/src/function_cache.mjs
@@ -2,6 +2,6 @@ import { function_name_combine } from "../../../love/public/src/function_name_co
 import { marker } from "../../../love/public/src/marker.mjs";
 export function function_cache(f_name) {
   f_name_cache;
-  let combined = function_name_combine(left, right);
+  let combined = function_name_combine(f_name, "cache");
   marker("1");
 }
