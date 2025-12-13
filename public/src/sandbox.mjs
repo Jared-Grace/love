2b5a55045e7691cd1fc397d0bdc071a174a93bdc
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 16:09:40 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_run_io_file_wrapper } from "../../../love/public/src/function_run_io_file_wrapper.mjs";
 export async function sandbox() {
+  marker("1");
   let r = await function_run_io_file_wrapper({
     f_name: "function_read",
     args: ["function_read"],
