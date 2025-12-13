01f000ea3b7f60cf0344adbf76fc3a846f256380
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 00:16:42 -0500

    data_file_update ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -197,7 +197,7 @@ export function app_a_function_node(a) {
       }
       app_a_parenthesis_wrap(parent, lambda19);
       let consequent = object_property_get(node, "consequent");
-      let nn = null_not_is(value4);
+      let nn = null_not_is(consequent);$i
       $inn;
       app_a_braces_wrap_node(a, consequent, parent);
       app_a_braces_wrap_node(a, alternate, parent);
