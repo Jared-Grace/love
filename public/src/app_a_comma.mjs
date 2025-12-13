1d6d8b9f6d7f89c097e9ea643a2930c336969a16
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 22:07:48 -0500

    function_auto_path ..\love\public\src\app_a_nodes_list.mjs

---
+++ public/src/app_a_comma.mjs
@@ -0,0 +1,6 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+import { js_code_comma } from "../../../love/public/src/js_code_comma.mjs";
+export function app_a_comma(parent) {
+  let c = js_code_comma();
+  let span = html_span_text(parent, c);
+}
