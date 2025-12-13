df7710ac966a233ded8483116bf24f5b2dab4c15
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 22:05:38 -0500

    function_auto_path ..\love\public\src\js_code_wrap_brackets.mjs

--- web/public/src/js_code_wrap_brackets.mjs
+++ web/public/src/js_code_wrap_brackets.mjs
@@ -1,6 +1,8 @@
+import { js_code_bracket_close } from "../../../love/public/src/js_code_bracket_close.mjs";
+import { js_code_bracket_open } from "../../../love/public/src/js_code_bracket_open.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_code_wrap_brackets(inside) {
   marker("1");
-  let code = "[" + inside + "]";
+  let code = js_code_bracket_open() + inside + js_code_bracket_close();
   return code;
 }
