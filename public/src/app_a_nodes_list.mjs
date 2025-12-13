be6b6d5d395891834d8be8376397101362ec4db6
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 22:07:48 -0500

    function_auto_path ..\love\public\src\app_a_nodes_list.mjs

--- public/src/app_a_nodes_list.mjs
+++ public/src/app_a_nodes_list.mjs
@@ -1,16 +1,14 @@
+import { app_a_comma } from "../../../love/public/src/app_a_comma.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { html_span_space } from "../../../love/public/src/html_span_space.mjs";
-import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { list_last_not_is } from "../../../love/public/src/list_last_not_is.mjs";
-import { js_code_comma } from "../../../love/public/src/js_code_comma.mjs";
 import { app_a_function_node_child } from "../../../love/public/src/app_a_function_node_child.mjs";
 export function app_a_nodes_list(a, nodes, parent) {
   function lambda(arg) {
     app_a_function_node_child(a, arg);
-    let c = js_code_comma();
     let n = list_last_not_is(nodes, arg);
     if (n) {
-      let span = html_span_text(parent, c);
+      app_a_comma(parent);
       html_span_space(parent);
     }
   }
