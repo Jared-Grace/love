5a41c90790c7573fadb0706fe83914d0f8747ecd
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 21:57:41 -0500

    function_auto_path ..\love\public\src\app_a_body.mjs

--- public/src/app_a_body.mjs
+++ public/src/app_a_body.mjs
@@ -1,64 +1,6 @@
-import { log } from "../../../love/public/src/log.mjs";
-import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { html_display_none_or_block } from "../../../love/public/src/html_display_none_or_block.mjs";
-import { app_a_button_wide } from "../../../love/public/src/app_a_button_wide.mjs";
-import { js_node_type_not_is } from "../../../love/public/src/js_node_type_not_is.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
-import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { app_a_function_node_child_parent } from "../../../love/public/src/app_a_function_node_child_parent.mjs";
-import { html_div } from "../../../love/public/src/html_div.mjs";
+import { app_a_body_inner } from "../../../love/public/src/app_a_body_inner.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_body(node, parent, a) {
   let body = object_property_get(node, "body");
-  let parent_new = parent;
-  let imports = false;
-  function lambda(b) {
-    const t = "ImportDeclaration";
-    let v = list_first_is(body, b);
-    if (v) {
-      let type = object_property_get(b, "type");
-      if (equal(type, t)) {
-        imports = true;
-        parent_new = html_div(parent);
-        let parent_new_saved = parent_new;
-        let hidden = true;
-        let buttons = null;
-        function imports_refresh() {
-          function lambda3(b) {
-            let text = null;
-            if (hidden) {
-              text = "Show";
-            } else {
-              text = "Hide";
-            }
-            text += " imports";
-            html_text_set(b, text);
-          }
-          each(buttons, lambda3);
-          html_display_none_or_block(hidden, parent_new_saved);
-          hidden = not(hidden);
-        }
-        function lambda2(item) {
-          let v2 = app_a_button_wide(item, "", imports_refresh);
-          return v2;
-        }
-        buttons = list_map([parent, parent_new_saved], lambda2);
-        log(buttons);
-        imports_refresh();
-      }
-    }
-    if (imports) {
-      let ti = js_node_type_not_is(b, t);
-      if (ti) {
-        imports = false;
-        parent_new = parent;
-      }
-    }
-    let div = html_div(parent_new);
-    app_a_function_node_child_parent(a, b, div);
-  }
-  each(body, lambda);
+  app_a_body_inner(parent, body, a);
 }
