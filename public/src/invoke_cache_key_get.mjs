40180dc11ca8b0c0a9bd5963a422b23edee7557d
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:54:57 -0500

    function_auto_path ..\love\public\src\invoke_cache_key_get.mjs

--- public/src/invoke_cache_key_get.mjs
+++ public/src/invoke_cache_key_get.mjs
@@ -1,8 +1,8 @@
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export function invoke_cache_key_get(fn, args) {
-  let f = function lambda() {
+  let l = function lambda() {
     let json = json_to([fn.name, args]);
     return json;
   };
-  return f;
+  return l;
 }
