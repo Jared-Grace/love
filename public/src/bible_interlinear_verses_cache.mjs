b5e144591c8f412ed70da8526d10c7af28efc0c7
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:27:03 -0500

    de bible_interlinear_verses_cache

--- public/src/bible_interlinear_verses_cache.mjs
+++
@@ -1,8 +0,0 @@
-import { bible_interlinear_verses } from "../../../love/public/src/bible_interlinear_verses.mjs";
-import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function bible_interlinear_verses_cache() {
-  marker("1");
-  let v = await invoke_cache_file(bible_interlinear_verses, []);
-  return v;
-}
