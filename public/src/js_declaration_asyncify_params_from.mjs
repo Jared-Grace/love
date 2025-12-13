b5d68dabb9368a1d500ca8e395f9ae9fe37d100d
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:35:48 -0500

    function_auto_path ..\love\public\src\function_wrap.mjs

---
+++ public/src/js_declaration_asyncify_params_from.mjs
@@ -0,0 +1,13 @@
+import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
+import { object_property_from } from "../../../love/public/src/object_property_from.mjs";
+import { js_declaration_asyncify } from "../../../love/public/src/js_declaration_asyncify.mjs";
+import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
+export async function js_declaration_asyncify_params_from(
+  ast,
+  declaration_call,
+) {
+  let declaration = js_declaration_single(ast);
+  js_declaration_asyncify(declaration, declaration_call);
+  object_property_from(declaration, "params", declaration_call);
+  await js_imports_missing_add(ast);
+}
