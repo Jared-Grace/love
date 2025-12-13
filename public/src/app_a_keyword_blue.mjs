459ae6054a5c973dbe6028bc451986a5966cfa15
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 21:07:40 -0500

    function_auto_path ..\love\public\src\app_a_keyword_blue.mjs

--- public/src/app_a_keyword_blue.mjs
+++ public/src/app_a_keyword_blue.mjs
@@ -1,7 +1,7 @@
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-export function app_a_keyword_blue(parent, v) {
-  let s2 = html_span_text(parent, v);
+export function app_a_keyword_blue(parent, text) {
+  let s2 = html_span_text(parent, text);
   const color = "#0000baff";
   html_font_color_set(s2, color);
 }
