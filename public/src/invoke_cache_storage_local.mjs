d2756a0bb13f3e9ba9a257bb32876afe20107eb9
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:24:37 -0500

    function_auto_path ..\love\public\src\invoke_cache_storage_local.mjs

--- public/src/invoke_cache_storage_local.mjs
+++ public/src/invoke_cache_storage_local.mjs
@@ -1,15 +1,12 @@
+import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
 import { storage_local_set_exists_not } from "../../../love/public/src/storage_local_set_exists_not.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache_storage_local(fn, args) {
   marker("1");
-  let key_get = function lambda() {
-    let json = json_to([fn.name, args]);
-    return json;
-  };
+  let key_get = invoke_cache_key_get(fn, args);
   async function value_get() {
     let v = await fn(...args);
     return v;
