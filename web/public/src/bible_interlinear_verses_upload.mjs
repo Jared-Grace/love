d86518f2eed123884e2a79663b21355bce777bea
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:11:08 -0500

    function_auto_path ..\love\public\src\bible_interlinear_verses_upload.mjs

--- web/public/src/bible_interlinear_verses_upload.mjs
+++ web/public/src/bible_interlinear_verses_upload.mjs
@@ -5,15 +5,16 @@ import { each_object_async } from "../../../love/public/src/each_object_async.mj
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { ebible_firebase_upload_verse } from "../../../love/public/src/ebible_firebase_upload_verse.mjs";
 export async function bible_interlinear_verses_upload() {
-  async function lambda(la) {}
+  async function lambda(la) {
+    async function lambda6(verses, chapter_code) {
+      async function lambda5(verse) {
+        await ebible_firebase_upload_verse(verse, chapter_code, bible_folder);
+      }
+      let waited = await list_map_unordered_async(verses, lambda5);
+    }
+    await each_object_async(chapters, lambda6);
+  }
   let list = await list_adder_async(lambda);
   let chapters = await bible_interlinear_chapters();
   let bible_folder = bible_interlinear_verses_upload_folder();
-  async function lambda6(verses, chapter_code) {
-    async function lambda5(verse) {
-      await ebible_firebase_upload_verse(verse, chapter_code, bible_folder);
-    }
-    let waited = await list_map_unordered_async(verses, lambda5);
-  }
-  await each_object_async(chapters, lambda6);
 }
