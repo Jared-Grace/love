7834e6ca4c030202a5a9245caf3c0ec27419ae99
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:15:40 -0500

    o function_alias_replace

--- web/public/src/function_alias_replace.mjs
+++ web/public/src/function_alias_replace.mjs
@@ -1,9 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { function_alias_generic } from "../../../love/public/src/function_alias_generic.mjs";
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 export async function function_alias_replace(alias_old, f_name) {
+  marker("1");
   function lambda(a) {
     let { exists, aliases } = a;
     if (not(exists)) {
