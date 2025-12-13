26b8108e3e044a5dfc588b857e76d9a555045ce7
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 16:10:31 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
 export async function sandbox() {
-  let v = await ebible_version_chapters_cache(bible_folder);
+  each(list, function lambda(item) {});
+  let v = await ebible_version_chapters_cache("bible_folder");
 }
