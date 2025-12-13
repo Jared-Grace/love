404453c39231b27e6cccb3637edd0264b4794fcb
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:44:59 -0500

    function_auto_path ..\love\public\src\invoke_cache_global.mjs

--- public/src/invoke_cache_global.mjs
+++ public/src/invoke_cache_global.mjs
@@ -1,17 +1,14 @@
+import { invoke_value_get } from "../../../love/public/src/invoke_value_get.mjs";
 import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { global_function_property_set_exists_not } from "../../../love/public/src/global_function_property_set_exists_not.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
-export async function invoke_cache_global(f_name, args) {
+export async function invoke_cache_global(fn, args) {
   marker("1");
   let key_get = invoke_cache_key_get(fn, args);
-  async function value_get() {
-    let f = eval(f_name);
-    let v = await f(...args);
-    return v;
-  }
+  let value_get = invoke_value_get(fn, args);
   let cached_exists = function lambda3(key) {
     let exists = global_function_property_exists(invoke_cache_global, key);
     return exists;
