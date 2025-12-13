078607a3808780de8fff619b47689f540860536c
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 16:02:59 -0500

    function_auto_path ..\love\public\src\invoke_cache_file.mjs

--- public/src/invoke_cache_file.mjs
+++ public/src/invoke_cache_file.mjs
@@ -17,8 +17,8 @@ export async function invoke_cache_file(fn, args) {
     let file_name = file_name_json(json);
     let f_path = folder_user_storage_function_path(fn);
     let joined = path_join([f_path, file_name]);
-    let safe = file_path_safe_to(fp);
-    return joined;
+    let safe = file_path_safe_to(joined);
+    return safe;
   };
   let value_get = invoke_cache_value_get(fn, args);
   let cached_exists = async function lambda3(key) {
