c39cad62d2f46a6a98154af30db54a0358ffd404
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 16:11:47 -0500

    data_file_update ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,11 +5,8 @@ import { each } from "../../../love/public/src/each.mjs";
 import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
 export async function sandbox() {
   let languages = ebible_languages();
-  async function lambda(language) {
+  async function lambda2(item) {
     let bible_folder = object_property_get(language, "bible_folder");
-    let v = await ebible_version_chapters_cache(bible_folder);
-  }
-  each(languages, lambda);
-  async function lambda2(item) {}
+    let v = await ebible_version_chapters_cache(bible_folder);}
   await each_async(list, lambda2);
 }
