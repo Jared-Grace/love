a6f96946ceee93c2afcdddef6a8a47a92be07b23
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 00:14:40 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -207,7 +207,7 @@ export function app_a_function_node(a) {
       app_a_braces_wrap_node(a, alternate, parent);
     },
     ["LogicalExpression"]: binary,
-    ["AssignmentExpression"]: () => {},
+    ["AssignmentExpression"]: function lambda15() {},
   };
   function binary() {
     let left = object_property_get(node, "left");
