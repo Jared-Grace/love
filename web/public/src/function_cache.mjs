9e352052064301d13080e53e383c853f50a37628
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:32:12 -0500

    function_auto_path ..\love\public\src\function_cache.mjs

--- web/public/src/function_cache.mjs
+++ web/public/src/function_cache.mjs
@@ -1,3 +1,4 @@
+import { js_code_wrap_brackets } from "../../../love/public/src/js_code_wrap_brackets.mjs";
 import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
@@ -6,17 +7,17 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { invoke_cache_file } from "./invoke_cache_file.mjs";
 export async function function_cache(f_name) {
   marker("1");
-  let { declaration: declaration_cache, unaliased } =
+  let { declaration: declaration_call, unaliased } =
     await function_parse_declaration(f_name);
-  let { declaration: declaration_call } = await function_parse_declaration(
-    invoke_cache_file.name,
-  );
+  const c = invoke_cache_file.name;
+  let { declaration: declaration_cache } = await function_parse_declaration(c);
   let f_name_cache = function_name_combine(unaliased, "cache");
   async function lambda(ast) {
+    let code = js_code_wrap_brackets(inside);
     let return_argument_code = js_code_call_args_await_maybe(
-      unaliased,
-      arg_names,
-      declaration_call,
+      c,
+      [unaliased],
+      declaration_cache,
     );
   }
   let v = await function_new_transform(f_name_cache, lambda);
