8e5a6707f4a0ea3b2a3f9c97095b7bbd6e5f5697
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:50:26 -0500

    function_auto_path ..\love\public\src\folder_user_storage_function_each.mjs

--- public/src/folder_user_storage_function_each.mjs
+++ public/src/folder_user_storage_function_each.mjs
@@ -1,8 +1,8 @@
-import { firebase_storage_download_path } from "../../../love/public/src/firebase_storage_download_path.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { folder_read_paths_async } from "../../../love/public/src/folder_read_paths_async.mjs";
+import { folder_user } from "../../../love/public/src/folder_user.mjs";
 export async function folder_user_storage_function_each(fn, file_each) {
-  let path = firebase_storage_download_path("\\storage\\function\\" + fn.name);
+  let path = folder_user("\\storage\\function\\" + fn.name);
   let combineds = await folder_read_paths_async(path);
   await each_async(combineds, file_each);
 }
