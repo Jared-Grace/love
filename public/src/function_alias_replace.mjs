f9083577da64ce6052cc4fde157b9c8bf11d634b
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:15:50 -0500

    function_auto_path ..\love\public\src\function_alias_replace.mjs

--- public/src/function_alias_replace.mjs
+++ public/src/function_alias_replace.mjs
@@ -1,3 +1,4 @@
+import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { function_alias_generic } from "../../../love/public/src/function_alias_generic.mjs";
@@ -5,6 +6,7 @@ import { object_property_delete } from "../../../love/public/src/object_property
 import { error } from "../../../love/public/src/error.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 export async function function_alias_replace(alias_old, f_name) {
+  string_is_assert(value);
   marker("1");
   function lambda(a) {
     let { exists, aliases } = a;
