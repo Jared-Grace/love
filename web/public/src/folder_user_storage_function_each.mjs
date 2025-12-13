170612cdbcdda07ceef8ecd2268f977bbd24c176
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:52:18 -0500

    r firebase_storage_download_path folder_user_storage_path

--- web/public/src/folder_user_storage_function_each.mjs
+++ web/public/src/folder_user_storage_function_each.mjs
@@ -1,8 +1,8 @@
-import { firebase_storage_download_path } from "../../../love/public/src/firebase_storage_download_path.mjs";
+import { folder_user_storage_path } from "../../../love/public/src/folder_user_storage_path.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { folder_read_paths_async } from "../../../love/public/src/folder_read_paths_async.mjs";
 export async function folder_user_storage_function_each(fn, file_each) {
-  let path = firebase_storage_download_path("function\\" + fn.name);
+  let path = folder_user_storage_path("function\\" + fn.name);
   let combineds = await folder_read_paths_async(path);
   await each_async(combineds, file_each);
 }
