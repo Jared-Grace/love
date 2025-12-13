a22f36f78eb9ca94e99c05adcc366a71d3b7e014
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:32:55 -0500

    function_auto_path ..\love\public\src\function_cache.mjs

--- web/public/src/function_cache.mjs
+++ web/public/src/function_cache.mjs
@@ -1,3 +1,5 @@
+import { json_to } from "../../../love/public/src/json_to.mjs";
+import { js_declaration_params_names } from "../../../love/public/src/js_declaration_params_names.mjs";
 import { js_code_wrap_brackets } from "../../../love/public/src/js_code_wrap_brackets.mjs";
 import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
@@ -13,6 +15,8 @@ export async function function_cache(f_name) {
   let { declaration: declaration_cache } = await function_parse_declaration(c);
   let f_name_cache = function_name_combine(unaliased, "cache");
   async function lambda(ast) {
+    let arg_names = js_declaration_params_names(declaration_call);
+    let json = json_to(object);
     let args = js_code_wrap_brackets(inside);
     let return_argument_code = js_code_call_args_await_maybe(
       c,
