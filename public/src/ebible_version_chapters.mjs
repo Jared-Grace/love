3da458a25b4394bc0f2d51b9b6963a64329034f1
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:20:36 -0500

    function_auto_path ..\love\public\src\ebible_version_chapters.mjs

--- public/src/ebible_version_chapters.mjs
+++ public/src/ebible_version_chapters.mjs
@@ -1,18 +1,11 @@
+import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { ebible_chapters_each_verses } from "../../../love/public/src/ebible_chapters_each_verses.mjs";
-import { ebible_firebase_upload_verse } from "../../../love/public/src/ebible_firebase_upload_verse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
-import { list_wait } from "../../../love/public/src/list_wait.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 export async function ebible_version_chapters(bible_folder) {
   marker("1");
   await ebible_version_download(bible_folder);
+  let list = await list_adder_async(async function lambda(la) {});
   await ebible_chapters_each_verses(bible_folder, each_chapter);
-  async function each_chapter(chapter_code, verses) {
-    async function lambda(v) {
-      await ebible_firebase_upload_verse(v, chapter_code, bible_folder);
-    }
-    let mapped = list_map(verses, lambda);
-    await list_wait(mapped);
-  }
+  async function each_chapter(chapter_code, verses) {}
 }
