6b4f7c428d2df5763f0dd74d3ad16445b58a771c
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 18:13:27 -0500

    function_auto_path ..\love\public\src\bible_interlinear_verses_upload.mjs

--- public/src/bible_interlinear_verses_upload.mjs
+++ public/src/bible_interlinear_verses_upload.mjs
@@ -1,23 +1,9 @@
+import { bible_interlinear_verses } from "../../../love/public/src/bible_interlinear_verses.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
-import { bible_interlinear_chapters } from "../../../love/public/src/bible_interlinear_chapters.mjs";
-import { bible_interlinear_verses_upload_folder } from "../../../love/public/src/bible_interlinear_verses_upload_folder.mjs";
-import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { ebible_firebase_upload_verse } from "../../../love/public/src/ebible_firebase_upload_verse.mjs";
 export async function bible_interlinear_verses_upload() {
-  let chapters = await bible_interlinear_chapters();
-  let bible_folder = bible_interlinear_verses_upload_folder();
-  async function lambda(la) {
-    async function lambda6(verses, chapter_code) {
-      la({
-        chapter_code,
-        verses,
-      });
-    }
-    await each_object_async(chapters, lambda6);
-  }
-  let list = await list_adder_async(lambda);
+  let { bible_folder, list } = await bible_interlinear_verses();
   async function lambda2({ chapter_code, verses }) {
     async function lambda5(verse) {
       await ebible_firebase_upload_verse(verse, chapter_code, bible_folder);
