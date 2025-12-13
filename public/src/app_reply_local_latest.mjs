e8367447d11d4e49dd54427cfb63d61f545952e9
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 14:57:58 -0500

    data_file_update ..\love\public\src\app_reply_local_latest.mjs

--- public/src/app_reply_local_latest.mjs
+++ public/src/app_reply_local_latest.mjs
@@ -1,4 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_reply_local_latest() {
-  marker("1");
+export async function app_reply_local_latest() {
+  await app_main_latest("app_reply_local_main", "jared-grace");
 }
