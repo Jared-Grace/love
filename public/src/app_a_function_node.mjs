5ec5214151e0a5d747e2cde1028037bf2752c6d6
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 23:52:46 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -195,7 +195,7 @@ export function app_a_function_node(a) {
         node,
       });
     },
-    ["BinaryExpression"]: () => {},
+    ["BinaryExpression"]: function lambda15() {},
   };
   let value = object_property_get(lookup, type);
   value();
