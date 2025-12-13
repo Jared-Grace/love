893a149424c1720d663a1b737411761dddccacba
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:12:15 -0500

    function_auto_path ..\love\public\src\app_reply_local_main.mjs

--- public/src/app_reply_local_main.mjs
+++ public/src/app_reply_local_main.mjs
@@ -1,4 +1,6 @@
+import { app_api } from "../../../love/public/src/app_api.mjs";
 import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
 export async function app_reply_local_main() {
+  let result = await app_api(f_name, args);
   await app_reply_main();
 }
