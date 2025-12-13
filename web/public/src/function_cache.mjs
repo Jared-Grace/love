8c0eb452029a3e58e5a9cc0c26b2545cce8befea
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:34:37 -0500

    function_auto_path ..\love\public\src\function_cache.mjs

--- web/public/src/function_cache.mjs
+++ web/public/src/function_cache.mjs
@@ -1,7 +1,6 @@
 import { js_code_call_args_await_maybe_declaration_return_add } from "../../../love/public/src/js_code_call_args_await_maybe_declaration_return_add.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { js_declaration_params_names } from "../../../love/public/src/js_declaration_params_names.mjs";
-import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
@@ -17,15 +16,10 @@ export async function function_cache(f_name) {
   async function lambda(ast) {
     let arg_names = js_declaration_params_names(declaration_call);
     let arg_names_code = json_to(arg_names);
-    let return_argument_code = js_code_call_args_await_maybe(
+    js_code_call_args_await_maybe_declaration_return_add(
       c,
       [unaliased, arg_names_code],
       declaration_cache,
-    );
-    js_code_call_args_await_maybe_declaration_return_add(
-      unaliased,
-      arg_names,
-      declaration_call,
       ast,
     );
   }
