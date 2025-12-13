1576d50b0b0324743f8cc36acd4311c9fee395e9
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:24:28 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ public/src/ebible_languages_chapters.mjs
@@ -0,0 +1,27 @@
+import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
+import { ebible_language_original } from "../../../love/public/src/ebible_language_original.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
+export async function ebible_languages_chapters() {
+  let languages = ebible_languages();
+  async function lambda(la) {
+    async function lambda2(language) {
+      let bible_folder = object_property_get(language, "bible_folder");
+      let right = ebible_language_original();
+      if (equal(bible_folder, right)) {
+        return;
+      }
+      let chapters = await ebible_version_chapters_cache(bible_folder);
+      la({
+        bible_folder,
+        chapters,
+      });
+    }
+    await each_async(languages, lambda2);
+  }
+  let all = await list_adder_async(lambda);
+  return all;
+}
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,27 +1,5 @@
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { ebible_language_original } from "../../../love/public/src/ebible_language_original.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
-import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
-import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
+import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
 export async function sandbox() {
-  let languages = ebible_languages();
-  async function lambda(la) {
-    async function lambda2(language) {
-      let bible_folder = object_property_get(language, "bible_folder");
-      let right = ebible_language_original();
-      if (equal(bible_folder, right)) {
-        return;
-      }
-      let chapters = await ebible_version_chapters_cache(bible_folder);
-      la({
-        bible_folder,
-        chapters,
-      });
-    }
-    await each_async(languages, lambda2);
-  }
-  let all = await list_adder_async(lambda);
+  let all = await ebible_languages_chapters();
   return all;
 }
