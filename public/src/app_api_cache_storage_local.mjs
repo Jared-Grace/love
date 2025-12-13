4d308f3305037831de8e3fac0a86422ed2d03347
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:11:55 -0500

    function_multiple_rename_if_starts_with app_a_api app_api

--- public/src/app_api_cache_storage_local.mjs
+++ public/src/app_api_cache_storage_local.mjs
@@ -1,9 +1,9 @@
-import { app_a_api_generic } from "../../../love/public/src/app_a_api_generic.mjs";
+import { app_api_generic } from "../../../love/public/src/app_api_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { http_post_json_cache_storage_local } from "../../../love/public/src/http_post_json_cache_storage_local.mjs";
 export async function app_api_cache_storage_local(f_name, args) {
   marker("1");
   let fn = http_post_json_cache_storage_local;
-  let result = await app_a_api_generic(f_name, args, fn);
+  let result = await app_api_generic(f_name, args, fn);
   return result;
 }
