ba822d5865b8db3ddc8b8e0c62d22d9e5df7714f
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 00:01:40 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -171,15 +171,7 @@ export function app_a_function_node(a) {
       let kind2 = object_property_get(node, "kind");
       equal_assert(kind2, "init");
       let key = object_property_get(node, "key");
-      if (false) {
-        let b = js_code_bracket_open();
-        let span5 = html_span_text(parent, b);
-      }
       app_a_function_node_child(a, key);
-      if (false) {
-        let v3 = js_code_bracket_close();
-        let span6 = html_span_text(parent, v3);
-      }
       let c = js_code_colon();
       let span4 = html_span_text(parent, c);
       html_span_space(parent);
@@ -203,6 +195,15 @@ export function app_a_function_node(a) {
       app_a_function_node_child(a, right);
     },
     ["ArrayExpression"]: function lambda17() {
+      if (false) {
+        let b = js_code_bracket_open();
+        let span5 = html_span_text(parent, b);
+      }
+      app_a_function_node_child(a, key);
+      if (false) {
+        let v3 = js_code_bracket_close();
+        let span6 = html_span_text(parent, v3);
+      }
       log({
         node,
       });
