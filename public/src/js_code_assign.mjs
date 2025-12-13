61d12dcd428a4017287d0ffd55983e65cac78a6c
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 21:12:37 -0500

    function_auto_path ..\love\public\src\js_code_assign.mjs

--- public/src/js_code_assign.mjs
+++ public/src/js_code_assign.mjs
@@ -1,4 +1,5 @@
+import { js_code_equals_padded } from "../../../love/public/src/js_code_equals_padded.mjs";
 export function js_code_assign(left, right) {
-  const code = left + " = " + right;
+  const code = left + js_code_equals_padded() + right;
   return code;
 }
---
+++ public/src/js_code_equals_padded.mjs
@@ -0,0 +1,4 @@
+export function js_code_equals_padded() {
+  let v = " = ";
+  return v;
+}
