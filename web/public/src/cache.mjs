bdc4199fba0d944903225886c861e6937f3e5eb6
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:58:44 -0500

    fw ebible_version_chapters cache

--- web/public/src/cache.mjs
+++ web/public/src/cache.mjs
@@ -1,4 +1,6 @@
+import { ebible_version_chapters } from "../../../love/public/src/ebible_version_chapters.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function cache() {
+export async function cache(bible_folder) {
   marker("1");
+  return await ebible_version_chapters(bible_folder);
 }
