2d6f1f78f669ceb2e83561f783bb102a871bf741
Grace-Jared <christrosetolife@gmail.com>
2025-12-12 21:05:12 -0500

    data_file_update ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -141,7 +141,7 @@ export function app_a_function_node(a) {
       }
       html_on_pointerdown(span3, lambda12);
     },
-    ["VariableDeclaration"]: function lambda11() {},
+    ["VariableDeclaration"]: function lambda11() {$l},
   };
   let value = object_property_get(lookup, type);
   value();
