d8716f085da72816fe2d46f4ec26ca02d6041513
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:22:48 -0500

    fw cache_generic cache_file

--- web/public/src/cache_file.mjs
+++ web/public/src/cache_file.mjs
@@ -1,1 +1,18 @@
-export function cache_file() {}
+import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function cache_file(
+  key_get,
+  cached_exists,
+  cached_get,
+  value_get,
+  cache_save,
+) {
+  marker("1");
+  return await cache_generic(
+    key_get,
+    cached_exists,
+    cached_get,
+    value_get,
+    cache_save,
+  );
+}
