54b28bd98f4801e6d755453cea6d11b0072581b1
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 17:26:53 -0500

    function_auto_path ..\love\public\src\function_cache.mjs

--- public/src/function_cache.mjs
+++ public/src/function_cache.mjs
@@ -1,9 +1,12 @@
+import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_cache(f_name) {
   let f_name_cache = function_name_combine(f_name, "cache");
   marker("1");
+  let { declaration: declaration_call, unaliased } =
+    await function_parse_declaration(f_name);
   async function lambda(ast) {}
   let v = await function_new_transform(f_name2, lambda);
 }
