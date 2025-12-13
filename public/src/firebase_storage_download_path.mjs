b81e131ba4052cb617f37898d3a9570a2163168a
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 15:52:18 -0500

    r firebase_storage_download_path folder_user_storage_path

--- public/src/firebase_storage_download_path.mjs
+++
@@ -1,6 +0,0 @@
-import { folder_user_combine } from "../../../love/public/src/folder_user_combine.mjs";
-export function firebase_storage_download_path(destination) {
-  const folder = "storage";
-  let joined = folder_user_combine(folder, destination);
-  return joined;
-}
