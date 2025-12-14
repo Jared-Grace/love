9d80c59cf3a8d7c8ae2369b3bda1fa4f764c8830
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 18:26:41 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- web/public/src/file_overwrite.mjs
+++ web/public/src/file_overwrite.mjs
@@ -1,8 +1,11 @@
+import { http_post_json_cache_clear } from "../../../love/public/src/http_post_json_cache_clear.mjs";
+import { app_api_cache_global_fn } from "../../../love/public/src/app_api_cache_global_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_overwrite(file_path, contents) {
   if (browser_is()) {
     let r = await app_api_cache_global_fn(file_overwrite, arguments);
+    http_post_json_cache_clear(url, body);
     return r;
   }
   await file_parent_exists_ensure(file_path);
