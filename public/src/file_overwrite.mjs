d82329f8b29f7b46231cb0791e55fbe5ce35b1f4
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 17:46:42 -0500

    data_file_update ..\love\public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,12 +1,12 @@
 import { file_root_exists_assert } from "../../../love/public/src/file_root_exists_assert.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_overwrite(file_path, contents) {
-  if (browser_is()) {
-    let r = await app_api_cache_global_fn(file_overwrite, arguments);
-    return r;
-  }
+
   await file_parent_exists_ensure(file_path);
   let fs = await import("fs");
   await fs.promises.writeFile(file_path, contents, "utf-8");
-  return;
+  return;  if (browser_is()) {
+    let r = await app_api_cache_global_fn(file_overwrite, arguments);
+    return r;
+  }
 }
