6fbf1d546f71bec2c1b6c6b11582aa231f3fc012
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 17:44:57 -0500

    data_file_update ..\love\public\src\js_format.mjs

--- web/public/src/js_format.mjs
+++ web/public/src/js_format.mjs
@@ -1,7 +1,7 @@
 import prettier from "prettier";
 import parserBabel from "prettier/plugins/babel";
-export async function js_format(code) {bi
-  if (false) {
+export async function js_format(code) {
+  if (browser_is()) {
     const prettier = await import("https://esm.sh/prettier@3.2.5");
     const parserBabel = await import(
       "https://esm.sh/prettier@3.2.5/plugins/babel"
