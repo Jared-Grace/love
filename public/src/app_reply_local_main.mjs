8bd43d6b89421ce2e5ed1ca8be69eee06cac72e7
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:44:53 -0500

    function_auto_path ..\love\public\src\app_reply_local_main.mjs

--- public/src/app_reply_local_main.mjs
+++ public/src/app_reply_local_main.mjs
@@ -1,4 +1,3 @@
-import { ebible_verse_download } from "../../../love/public/src/ebible_verse_download.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
 import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
@@ -7,12 +6,12 @@ import { ebible_languages_chapters } from "../../../love/public/src/ebible_langu
 import { app_api } from "../../../love/public/src/app_api.mjs";
 export async function app_reply_local_main() {
   let result = await app_api(ebible_languages_chapters.name, []);
-  function lambda(bible_folder, chapter_code, verse_number) {
+  function verse_get(bible_folder, chapter_code, verse_number) {
     let { chapters } = list_find_property(result, "bible_folder", bible_folder);
     let { verses } = list_find_property(chapters, "chapter_code", chapter_code);
     let verse = list_find_property(verses, "verse_number", verse_number);
     return verse;
   }
-  global_function_set(ebible_verse, ebible_verse_download);
+  global_function_set(ebible_verse, verse_get);
   await app_reply_main();
 }
