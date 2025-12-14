b9f4eb96f7b61a46bd1024159e94eaae721f882c
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 10:31:21 -0500

    function_auto_path ..\love\public\src\app_a_button_wide.mjs

--- web/public/src/app_a_button_wide.mjs
+++ web/public/src/app_a_button_wide.mjs
@@ -1,9 +1,9 @@
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { app_a_button } from "../../../love/public/src/app_a_button.mjs";
-import { app_karate_button_uncolored_style_assign } from "../../../karate_code/public/src/app_karate_button_uncolored_style_assign.mjs";
 import { app_a_control_style_wide } from "../../../love/public/src/app_a_control_style_wide.mjs";
 export function app_a_button_wide(parent, text, lambda) {
   let component = app_a_button(parent, text, lambda);
   app_a_control_style_wide(component);
-  app_karate_button_uncolored_style_assign(component);
+  html_width_full(component);
   return component;
 }
