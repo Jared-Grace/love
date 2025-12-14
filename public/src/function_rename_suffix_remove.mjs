64c7aea41b6adfa6d9d9b6bdda2fd2b6eea3b2c9
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:45:15 -0500

    function_auto_path ..\love\public\src\function_rename_suffix_remove.mjs

--- public/src/function_rename_suffix_remove.mjs
+++ public/src/function_rename_suffix_remove.mjs
@@ -6,7 +6,8 @@ export async function function_rename_suffix_remove(f_name_before, suffix) {
   marker("1");
   function lambda(f_name_before, suffix) {
     let separator = function_name_separator();
-    let sw = string_starts_with(s, prefix);
+    let sw = string_starts_with(f_name_before, "" + separator + suffix);
+    return sw;
   }
   let v = await function_rename_suffix_add_generic(
     suffix,
