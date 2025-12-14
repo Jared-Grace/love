49001189bb9e7161b0e7dbc7168a92d837c7c424
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 09:38:47 -0500

    function_auto_path ..\love\public\src\html_remove_lambda.mjs

--- web/public/src/html_remove_lambda.mjs
+++ web/public/src/html_remove_lambda.mjs
@@ -1,9 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_remove } from "../../../love/public/src/html_remove.mjs";
-export function html_remove_lambda(overlay) {
+export function html_remove_lambda(component) {
   marker("1");
   let r = function lambda() {
-    html_remove(overlay);
+    html_remove(component);
   };
   return r;
 }
