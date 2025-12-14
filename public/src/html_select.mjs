406eb449f8011995d3a6a7b34dbf7bcd73a2d50e
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:00:18 -0500

    function_auto_path ..\love\public\src\html_select.mjs

--- public/src/html_select.mjs
+++ public/src/html_select.mjs
@@ -1,6 +1,8 @@
+import { html_focus } from "../../../love/public/src/html_focus.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_select(input) {
+  html_focus(input2);
   let element = html_component_element_get(input);
   element.select();
   log({
