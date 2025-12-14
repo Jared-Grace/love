0e67854a59169423e08b4c71bf56ec6f6765247c
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:46:13 -0500

    function_auto_path ..\love\public\src\function_rename_suffix_remove.mjs

--- public/src/function_rename_suffix_remove.mjs
+++ public/src/function_rename_suffix_remove.mjs
@@ -1,13 +1,12 @@
+import { string_suffix_without } from "../../../love/public/src/string_suffix_without.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 import { function_rename_suffix_add_generic } from "../../../love/public/src/function_rename_suffix_add_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_suffix_remove(f_name_before, suffix) {
   marker("1");
   function lambda(f_name_before, suffix) {
     let separator = function_name_separator();
-    let sw = string_starts_with(f_name_before, "" + separator + suffix);
-    ssr;
+    let sw = string_suffix_without(f_name_before, "" + separator + suffix);
     return sw;
   }
   let v = await function_rename_suffix_add_generic(
