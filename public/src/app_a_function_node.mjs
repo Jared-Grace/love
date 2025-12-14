d3c2286226a84b4aca8e02ed4dec3c7decde787d
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 20:04:39 -0500

    pd indent

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -158,7 +158,7 @@ export function app_a_function_node(a) {
     ["ObjectExpression"]: function lambda14() {
       function lambda16() {
         let properties = object_property_get(node, "properties");
-        app_a_body_inner(parent, properties, a, true);
+        app_a_body_inner(parent, properties, a);
       }
       app_a_braces_wrap(parent, lambda16);
     },
