2ed1db4cd80b1e9f1242ddf9f35ce5fa238aed9c
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 21:24:41 -0500

    function_auto_path ..\love\public\src\app_a_brace_right.mjs

--- public/src/app_a_brace_right.mjs
+++ public/src/app_a_brace_right.mjs
@@ -1,6 +1,7 @@
 import { js_code_brace_right } from "../../../love/public/src/js_code_brace_right.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function app_a_brace_right(parent) {
-  let span6 = html_span_text(parent, " " + js_code_brace_right() + " ");
+  let text = js_code_brace_right();
+  let span6 = html_span_text(parent, text);
   return span6;
 }
