6524e2ffe6f533ba35fd26117e8c308d9609566d
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:23:52 -0500

    function_auto_path ..\love\public\src\app_reply_local_main.mjs

--- public/src/app_reply_local_main.mjs
+++ public/src/app_reply_local_main.mjs
@@ -6,6 +6,7 @@ export async function app_reply_local_main() {
   let result = await app_api(ebible_languages_chapters.name, []);
   function lambda(bible_folder, chapter_code, verse_number) {
     let item = list_find_property(result, "bible_folder", bible_folder);
+    let item2 = list_find_property(list, property_name, property_value);
   }
   await app_reply_generic(lambda);
 }
