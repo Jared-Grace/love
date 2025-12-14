f03af9ed84c704cbb526c47ffc7d6528afc69f3c
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 17:24:32 -0500

    function_auto_path ..\love\public\src\folder_read_files.mjs

--- web/public/src/folder_read_files.mjs
+++ web/public/src/folder_read_files.mjs
@@ -1,3 +1,5 @@
+import { app_api_cache_global_fn } from "../../../love/public/src/app_api_cache_global_fn.mjs";
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
@@ -13,4 +15,8 @@ export async function folder_read_files(path_folder) {
   let files = all.filter(lambda);
   list_sort_string(files);
   return files;
+  if (browser_is()) {
+    let r = await app_api_cache_global_fn(folder_read_files, arguments);
+    return r;
+  }
 }
