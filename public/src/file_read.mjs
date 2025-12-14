d4f30e64e136ac4a1ca4d5994f209c201b2494d7
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 18:35:39 -0500

    o flr

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,4 +1,6 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read(file_path) {
+  marker("1");
   let fs = await import("fs");
   let contents = await fs.promises.readFile(file_path, "utf-8");
   return contents;
