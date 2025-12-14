3b2571a38e7a4bc1e852c75649adbcaea80941f2
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:27:15 -0500

    function_auto_path ..\love\public\src\on_keydown_stop.mjs

--- public/src/on_keydown_stop.mjs
+++ public/src/on_keydown_stop.mjs
@@ -1,12 +1,10 @@
 import { html_on_keydown } from "../../../love/public/src/html_on_keydown.mjs";
-import { invoke_multiple_arg } from "../../../love/public/src/invoke_multiple_arg.mjs";
-export function on_keydown_stop(e) {
-  function lambda(e) {
-    on_keydown_stop(e);
-    invoke_multiple_arg(on_keydowns, e);
+export function on_keydown_stop(lambda) {
+  function on_k(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    lambda(e);
   }
-  html_on_keydown(root, lambda);
-  e.preventDefault();
-  e.stopPropagation();
-  e.stopImmediatePropagation();
+  html_on_keydown(root, on_k);
 }
