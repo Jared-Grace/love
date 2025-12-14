f5fadeba9c42b3631f4ccb94e9b9a0dc3ce19ada
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 12:01:16 -0500

    data_file_update ..\love\public\src\invoke_multiple.mjs

---
+++ public/src/invoke_multiple_shuffle.mjs
@@ -0,0 +1,6 @@
+import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
+import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
+export function invoke_multiple_shuffle(choices) {
+  list_shuffle(choices);
+  invoke_multiple(choices);
+}
