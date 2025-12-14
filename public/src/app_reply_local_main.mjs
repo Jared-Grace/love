80ac19812ab5d5a89854bac026a6feeaf7a85e12
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:22:42 -0500

    function_auto_path ..\love\public\src\app_reply_local_main.mjs

--- public/src/app_reply_local_main.mjs
+++ public/src/app_reply_local_main.mjs
@@ -1,7 +1,7 @@
+import { app_reply_generic } from "../../../love/public/src/app_reply_generic.mjs";
 import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
 import { app_api } from "../../../love/public/src/app_api.mjs";
-import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
 export async function app_reply_local_main() {
   let result = await app_api(ebible_languages_chapters.name, []);
-  await app_reply_main();
+  await app_reply_generic(() => {});
 }
