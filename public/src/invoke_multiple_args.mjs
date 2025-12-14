18d08a6f535298f0a0df32237fdbf5b91cbe9c72
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 12:28:23 -0500

    function_auto_path ..\love\public\src\invoke_multiple_args.mjs

--- public/src/invoke_multiple_args.mjs
+++ public/src/invoke_multiple_args.mjs
@@ -2,5 +2,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function invoke_multiple_args(list_fns, args) {
   marker("1");
-  each(list_fns, () => {});
+  function lambda() {}
+  each(list_fns, lambda);
 }
