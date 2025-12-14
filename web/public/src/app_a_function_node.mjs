aad7e727f4da926c01200fd13c5ca232d8720208
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 15:28:30 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -164,7 +164,7 @@ export function app_a_function_node(a) {
             fn: function lambda15() {
               overlay_close();
               let overlay = html_overlay_z_max(root);
-              aab;
+              aabw;
               let div3 = html_div(overlay);
               app_a_control_style(div3);
               html_style_background_color(div3, "white");
