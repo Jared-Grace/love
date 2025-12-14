7659cea8a5971c394b45cc36d669c7d91f150b22
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:06:53 -0500

    function_auto_path ..\love\public\src\html_select.mjs

--- public/src/html_select.mjs
+++ public/src/html_select.mjs
@@ -1,7 +1,9 @@
+import { html_request_animation_frame } from "../../../love/public/src/html_request_animation_frame.mjs";
 import { html_focus } from "../../../love/public/src/html_focus.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_select(input) {
+export async function html_select(input) {
   html_focus(input);
   let element = html_component_element_get(input);
+  await html_request_animation_frame();
   element.select();
 }
