f6ed4f36bb947ba675727f73d4010214445083f6
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 10:31:21 -0500

    function_auto_path ..\love\public\src\app_a_button_wide.mjs

---
+++ public/src/html_width_full.mjs
@@ -0,0 +1,6 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function html_width_full(component) {
+  html_style_assign(component, {
+    width: "100%",
+  });
+}
