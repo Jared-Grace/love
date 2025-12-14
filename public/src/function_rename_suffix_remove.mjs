af4c3dec66fd566e192766f1dbc99c15d4850dd7
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:43:49 -0500

    function_auto_path ..\love\public\src\function_rename_suffix_remove.mjs

--- public/src/function_rename_suffix_remove.mjs
+++ public/src/function_rename_suffix_remove.mjs
@@ -4,6 +4,10 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_suffix_remove(f_name_before, suffix) {
   marker("1");
   let fn = function_name_combine;
-  let v = await function_rename_suffix_add_generic(suffix, fn, f_name_before);
+  let v = await function_rename_suffix_add_generic(
+    suffix,
+    () => {},
+    f_name_before,
+  );
   return v;
 }
