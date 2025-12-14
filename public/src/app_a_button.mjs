0d478ba82a27fe2e6899d9f6e3b0a932ae6a58a5
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 10:18:05 -0500

    function_auto_path ..\love\public\src\app_a_button.mjs

--- public/src/app_a_button.mjs
+++ public/src/app_a_button.mjs
@@ -1,8 +1,9 @@
+import { app_karate_button_uncolored_style_assign } from "../../../karate_code/public/src/app_karate_button_uncolored_style_assign.mjs";
 import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_a_button(root, text, lambda2) {
   let b = html_button(root, text, lambda2);
   app_a_control_style(b);
-    app_karate_button_uncolored_style_assign(b);
+  app_karate_button_uncolored_style_assign(b);
   return b;
 }
