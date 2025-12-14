ff4f9f220cb47ee77b42b565b289c5cf5e98b54e
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 14:36:27 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -12,10 +12,10 @@ import { app_api } from "../../../love/public/src/app_api.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 export async function app_a_function(context) {
-  let function_name = fn_name("function_read");
-  let code = await app_api(function_name, [f_name]);
   let { app_fn, root } = context;
   let f_name = storage_local_get(app_fn, "f_name_selected");
+  let function_name = fn_name("function_read");
+  let code = await app_api(function_name, [f_name]);
   html_clear(root);
   function lambda2() {
     app_generic_screen_set(context, app_a_home);
