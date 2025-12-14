14feaef5424f61f61bc88edaf205b8183cf7f2ba
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:32:16 -0500

    function_auto_path ..\love\public\src\global_function_initialize_lambda_async.mjs

--- web/public/src/global_function_initialize_lambda_async.mjs
+++ web/public/src/global_function_initialize_lambda_async.mjs
@@ -1,11 +1,15 @@
+import { object_property_initialize_lambda_async } from "../../../love/public/src/object_property_initialize_lambda_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { object_property_initialize_lambda } from "../../../love/public/src/object_property_initialize_lambda.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { global_get } from "./global_get.mjs";
-export function global_function_initialize_lambda_async(fn, lambda) {
+export async function global_function_initialize_lambda_async(fn, lambda) {
   marker("1");
   assert_arguments(arguments, 2);
   let global = global_get();
-  let value = object_property_initialize_lambda(global, fn.name, lambda);
+  let value = await object_property_initialize_lambda_async(
+    global,
+    fn.name,
+    lambda,
+  );
   return value;
 }
