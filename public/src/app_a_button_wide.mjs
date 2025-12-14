c330b4b2c42be0a2f0acb94440640345edc45642
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 10:31:33 -0500

    function_auto_path ..\love\public\src\app_a_button_wide.mjs

--- public/src/app_a_button_wide.mjs
+++ public/src/app_a_button_wide.mjs
@@ -1,9 +1,7 @@
 import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { app_a_button } from "../../../love/public/src/app_a_button.mjs";
-import { app_a_control_style_wide } from "../../../love/public/src/app_a_control_style_wide.mjs";
 export function app_a_button_wide(parent, text, lambda) {
   let component = app_a_button(parent, text, lambda);
-  app_a_control_style_wide(component);
   html_width_full(component);
   return component;
 }
