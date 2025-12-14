52559c27acfb8e89f616daa3d1d6bc189bde936e
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 18:06:39 -0500

    function_auto_path ..\love\public\src\cache_clear.mjs

--- public/src/cache_clear.mjs
+++ public/src/cache_clear.mjs
@@ -1,3 +1,4 @@
+import { global_function_property_delete } from "../../../love/public/src/global_function_property_delete.mjs";
 import { http_post_json_cache_global_args } from "../../../love/public/src/http_post_json_cache_global_args.mjs";
 import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
 import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
@@ -7,4 +8,5 @@ export function cache_clear() {
   let fn = http_post_json;
   let args = http_post_json_cache_global_args(url, body);
   let key_get = invoke_cache_key_get(fn, args);
+  global_function_property_delete(fn2, property_name);
 }
