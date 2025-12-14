f854d866fb8026c373322838ece947e023321b24
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 15:53:11 -0500

    function_auto_path ..\love\public\src\html_select.mjs

--- web/public/src/html_select.mjs
+++ web/public/src/html_select.mjs
@@ -1,5 +1,7 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_select(input) {
   let element = html_component_element_get(input);
   element.select();
+  log({});
 }
