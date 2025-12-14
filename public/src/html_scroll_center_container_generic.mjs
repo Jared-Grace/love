a4dd166ed33e5e9fdd57f9b118f12e0827e86199
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:03:04 -0500

    function_auto_path ..\love\public\src\html_scroll_center_container_generic.mjs

--- public/src/html_scroll_center_container_generic.mjs
+++ public/src/html_scroll_center_container_generic.mjs
@@ -1,3 +1,4 @@
+import { html_request_animation_frame } from "../../../love/public/src/html_request_animation_frame.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_scroll_center_generic_wait } from "../../../love/public/src/html_scroll_center_generic_wait.mjs";
@@ -9,10 +10,7 @@ export async function html_scroll_center_container_generic(
   marker("1");
   let e = await html_scroll_center_generic_wait(player_img_c);
   let container_e = html_component_element_get(container);
-  await new Promise(function lambda(r) {
-    let v = requestAnimationFrame(r);
-    return v;
-  });
+  await html_request_animation_frame();
   const containerRect = container_e.getBoundingClientRect();
   const tileRect = e.getBoundingClientRect();
   const scrollLeft =
