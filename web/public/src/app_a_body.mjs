00633dd7441c8e7603034efab964f7b3c25ca0b5
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 20:02:05 -0500

    fs app_a_body

--- web/public/src/app_a_body.mjs
+++ web/public/src/app_a_body.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_body_inner } from "../../../love/public/src/app_a_body_inner.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_body(node, parent, a) {
+  marker("1");
   let body = object_property_get(node, "body");
   app_a_body_inner(parent, body, a);
 }
