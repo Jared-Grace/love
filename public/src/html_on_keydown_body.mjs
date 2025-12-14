ae179783f99bd6027c86e41dbd3877fc59980341
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 11:58:25 -0500

    function_auto_path ..\love\public\src\html_on_keydown_body.mjs

--- public/src/html_on_keydown_body.mjs
+++ public/src/html_on_keydown_body.mjs
@@ -1,6 +1,6 @@
 import { html_on_keydown } from "../../../love/public/src/html_on_keydown.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-export function html_on_keydown_body(lambda6) {
+export function html_on_keydown_body(lambda) {
   const body = html_document_body();
-  html_on_keydown(body, lambda6);
+  html_on_keydown(body, lambda);
 }
