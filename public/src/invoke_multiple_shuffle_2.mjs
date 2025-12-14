01d7d5a184f4384edae7af176eba93e44c207aee
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 12:01:26 -0500

    function_auto_path ..\love\public\src\invoke_multiple_shuffle_2.mjs

--- public/src/invoke_multiple_shuffle_2.mjs
+++ public/src/invoke_multiple_shuffle_2.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { invoke_multiple_shuffle } from "../../../love/public/src/invoke_multiple_shuffle.mjs";
 export function invoke_multiple_shuffle_2(correct, wrong) {
+  marker("1");
   let choices = [correct, wrong];
   invoke_multiple_shuffle(choices);
 }
