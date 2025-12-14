e54af00a8774df59676c0d6c2f0bc78aa15aaa84
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:47:02 -0500

    function_auto_path ..\love\public\src\ebible_firebase_upload_verse.mjs

--- public/src/ebible_firebase_upload_verse.mjs
+++ public/src/ebible_firebase_upload_verse.mjs
@@ -1,5 +1,5 @@
+import { ebible_verse_merge } from "../../../love/public/src/ebible_verse_merge.mjs";
 import { ebible_firebase_upload } from "../../../love/public/src/ebible_firebase_upload.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { ebible_verses_upload_name } from "../../../love/public/src/ebible_verses_upload_name.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function ebible_firebase_upload_verse(
@@ -9,12 +9,6 @@ export async function ebible_firebase_upload_verse(
 ) {
   let verse_number = object_property_get(v, "verse_number");
   let joined2 = ebible_verses_upload_name(chapter_code, verse_number);
-  let verse = object_merge(
-    {
-      bible_folder,
-      chapter_code,
-    },
-    v,
-  );
+  let verse = ebible_verse_merge(bible_folder, chapter_code, v);
   await ebible_firebase_upload(bible_folder, joined2, verse);
 }
