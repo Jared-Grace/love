f0e28249f2eb4f85730242f6c97c633792138dfd
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:23:19 -0500

    rrl js_identifier_replace rename

--- public/src/function_rename_identifiers.mjs
+++ public/src/function_rename_identifiers.mjs
@@ -3,7 +3,7 @@ import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_e
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_imports_paths_fix } from "../../../love/public/src/js_imports_paths_fix.mjs";
-import { js_identifier_replace } from "../../../love/public/src/js_identifier_replace.mjs";
+import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
@@ -12,7 +12,7 @@ export async function function_rename_identifiers(f_name_before, f_name_after) {
     list_remove_if_exists(f_names, f_name_before);
     async function lambda(f_name) {
       async function lambda2(ast) {
-        js_identifier_replace(ast, f_name_before, f_name_after);
+        js_identifier_rename(ast, f_name_before, f_name_after);
         await js_imports_paths_fix(ast);
       }
       let output = await function_transform(f_name, lambda2);
