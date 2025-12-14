c6e7629388f2c43476d052ae415a6642e4729cf4
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 17:42:24 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- web/public/src/file_overwrite.mjs
+++ web/public/src/file_overwrite.mjs
@@ -1,8 +1,10 @@
-import { file_root_exists_assert } from "../../../love/public/src/file_root_exists_assert.mjs";
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_overwrite(file_path, contents) {
   await file_parent_exists_ensure(file_path);
   let fs = await import("fs");
   await fs.promises.writeFile(file_path, contents, "utf-8");
   return;
+  if (browser_is()) {
+  }
 }
