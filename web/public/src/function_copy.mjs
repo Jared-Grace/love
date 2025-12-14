84bb7807a71921e61b6371cc626093001dfc7ba4
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:23:19 -0500

    rrl js_identifier_replace rename

--- web/public/src/function_copy.mjs
+++ web/public/src/function_copy.mjs
@@ -2,7 +2,7 @@ import { user_repo_path_combine } from "../../../love/public/src/user_repo_path_
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
-import { js_identifier_replace } from "../../../love/public/src/js_identifier_replace.mjs";
+import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { file_copy } from "../../../love/public/src/file_copy.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
@@ -15,7 +15,7 @@ export async function function_copy(f_name_old, f_name_new) {
   let f_path_new = await user_repo_path_combine(f_path);
   await file_copy(f_path_old, f_path_new);
   function lambda(ast) {
-    js_identifier_replace(ast, unaliased_old, unaliased_new);
+    js_identifier_rename(ast, unaliased_old, unaliased_new);
   }
   await function_transform(unaliased_new, lambda);
   await file_open(f_path_new);
