cb94b11621977d9ad073a58ae5029f92fb6d0ce1
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 21:53:35 -0500

    function_auto_path ..\love\public\src\js_code_parenthesis_right.mjs

--- public/src/js_code_parenthesis_right.mjs
+++ public/src/js_code_parenthesis_right.mjs
@@ -1,4 +1,4 @@
 export function js_code_parenthesis_right() {
-  let v2 = " )";
+  let v2 = ")";
   return v2;
 }
--- public/src/js_code_wrap_parenthesis.mjs
+++ public/src/js_code_wrap_parenthesis.mjs
@@ -1,6 +1,6 @@
 import { js_code_parenthesis_right } from "../../../love/public/src/js_code_parenthesis_right.mjs";
 import { js_code_parenthesis_left } from "../../../love/public/src/js_code_parenthesis_left.mjs";
 export function js_code_wrap_parenthesis(inside) {
-  let v = js_code_parenthesis_left() + inside + js_code_parenthesis_right();
+  let v = js_code_parenthesis_left() +' ' + inside+' ' + js_code_parenthesis_right();
   return v;
 }
