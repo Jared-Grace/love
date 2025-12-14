d7e3fb218ee74a174ed7cee1668f28630e2bf897
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 09:38:10 -0500

    function_auto_path ..\love\public\src\html_overlay_close_get.mjs

--- public/src/html_overlay_close_get.mjs
+++ public/src/html_overlay_close_get.mjs
@@ -1,7 +1,7 @@
 import { html_remove } from "../../../love/public/src/html_remove.mjs";
 export function html_overlay_close_get(overlay) {
-  let v = function lambda() {
+  let r = function lambda() {
     html_remove(overlay);
   };
-  return v;
+  return r;
 }
