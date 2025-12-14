94f1478f0c6ce40c980f5e7719332e726c5e519a
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:44:41 -0500

    function_auto_path ..\love\public\src\app_reply_local_main.mjs

--- public/src/app_reply_local_main.mjs
+++ public/src/app_reply_local_main.mjs
@@ -1,3 +1,6 @@
+import { ebible_verse_download } from "../../../love/public/src/ebible_verse_download.mjs";
+import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
+import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
 import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
@@ -10,5 +13,6 @@ export async function app_reply_local_main() {
     let verse = list_find_property(verses, "verse_number", verse_number);
     return verse;
   }
+  global_function_set(ebible_verse, ebible_verse_download);
   await app_reply_main();
 }
