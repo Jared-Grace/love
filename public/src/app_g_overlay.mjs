05a5edf7acc37c3f606b3bfc1b29f7dc5c74621c
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 09:28:51 -0500

    function_auto_path ..\love\public\src\app_g_overlay.mjs

--- public/src/app_g_overlay.mjs
+++ public/src/app_g_overlay.mjs
@@ -4,6 +4,7 @@ import { g_z } from "../../../love/public/src/g_z.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_g_overlay(div_map) {
   let container = object_property_get(div_map, "container");
+  const z_index = g_z("overlay");
   let overlay = html_div(container);
   let s = {
     position: "fixed",
@@ -13,7 +14,7 @@ export function app_g_overlay(div_map) {
     height: "100vh",
     padding: "1vw",
     background: "rgba(0,0,0,0.4)",
-    "z-index": g_z("overlay"),
+    "z-index": z_index,
     display: "flex",
     flexDirection: "column",
     gap: "0px",
