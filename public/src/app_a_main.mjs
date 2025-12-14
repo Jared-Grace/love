23ad9234fb5092ea8f04046d526c07a0ffb36b7f
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 12:36:16 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -1,4 +1,4 @@
-import { invoke_multiple_args } from "../../../love/public/src/invoke_multiple_args.mjs";
+import { invoke_multiple_arg } from "../../../love/public/src/invoke_multiple_arg.mjs";
 import { html_on_keydown } from "../../../love/public/src/html_on_keydown.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { app_a_screens } from "../../../love/public/src/app_a_screens.mjs";
@@ -15,8 +15,8 @@ export function app_a_main() {
     root,
     on_keydowns,
   };
-  function lambda() {
-    invoke_multiple_args(on_keydowns);
+  function lambda(k) {
+    invoke_multiple_arg(on_keydowns, k);
   }
   html_on_keydown(root, lambda);
   app_generic_refresh(context);
