031fca4635b676783133dc86c288dc17822dfde0
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 19:15:00 -0500

    data_file_update ..\love\public\src\ebible_languages_chapters_cache.mjs

--- web/public/src/ebible_languages_chapters_cache.mjs
+++ web/public/src/ebible_languages_chapters_cache.mjs
@@ -1,1 +1,7 @@
-export function ebible_languages_chapters_cache() {}
+import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
+import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function ebible_languages_chapters_cache() {
+  marker("1");
+  return await invoke_cache_file(ebible_languages_chapters, []);
+}
