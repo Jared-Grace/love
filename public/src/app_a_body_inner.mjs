f100a3b73b73a74bae42744f5f1a50aead4c1c7d
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 21:04:01 -0500

    function_auto_path ..\love\public\src\app_a_body_inner.mjs

--- public/src/app_a_body_inner.mjs
+++ public/src/app_a_body_inner.mjs
@@ -63,7 +63,7 @@ export function app_a_body_inner(parent, body, a) {
     }
     let line = html_div(parent_new);
     let indent = object_property_get(a, "indent");
-    string_multiply(" ", indent);
+    let indentation = string_multiply(" ", indent);
     let s = html_span_text(line, " ");
     html_style_set(s, "white-space", "pre");
     app_a_function_node_child_parent(a, b, line);
