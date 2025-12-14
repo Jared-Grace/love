4cc87e2ad5976280bd551e86b78841f376899b13
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 20:04:54 -0500

    pd indent

--- public/src/app_a_body.mjs
+++ public/src/app_a_body.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_body_inner } from "../../../love/public/src/app_a_body_inner.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function app_a_body(node, parent, a, indent) {
+export function app_a_body(node, parent, a) {
   marker("1");
   let body = object_property_get(node, "body");
   app_a_body_inner(parent, body, a);
