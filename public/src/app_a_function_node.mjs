c8068883e086dfa94a6ee8eb19ff71da0d8f4745
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 21:32:18 -0500

    data_file_update ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -159,7 +159,7 @@ export function app_a_function_node(a) {
     ["ObjectExpression"]: function lambda14() {
       function lambda16() {
         let properties = object_property_get(node, "properties");
-        app_a_body_inner(parent, properties, a, false);
+          app_a_body_inner(parent, properties, a, false);
       }
       app_a_braces_wrap(parent, lambda16);
     },
