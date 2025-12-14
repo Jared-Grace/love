7fa64269a8f677355ff3c74fbc63786481d4e985
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 11:58:14 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

---
+++ public/src/html_on_keydown_body.mjs
@@ -0,0 +1,6 @@
+import { html_on_keydown } from "../../../love/public/src/html_on_keydown.mjs";
+import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
+export function html_on_keydown_body(lambda6) {
+  const body = html_document_body();
+  html_on_keydown(body, lambda6);
+}
