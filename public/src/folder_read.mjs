eab16a4858ccb04b1fecd26223989db1e71fc9c0
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 17:08:09 -0500

    function_auto_path ..\love\public\src\folder_read.mjs

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,7 +1,10 @@
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function folder_read(path_folder) {
   marker("1");
   let fs = await import("fs");
   const all = await fs.promises.readdir(path_folder);
   return all;
+  if (browser_is()) {
+  }
 }
