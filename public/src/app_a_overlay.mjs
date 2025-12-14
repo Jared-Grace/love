0d68de89411c69f6566830e1c3bad897645c1fa2
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:47:53 -0500

    function_auto_path ..\love\public\src\app_a_overlay.mjs

--- public/src/app_a_overlay.mjs
+++ public/src/app_a_overlay.mjs
@@ -3,9 +3,9 @@ import { html_remove } from "../../../love/public/src/html_remove.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 export function app_a_overlay(on_keydowns, on_keydown, overlay) {
   marker("1");
-  let v3 = function lambda22() {
+  function overlay_close() {
     list_remove(on_keydowns, on_keydown);
     html_remove(overlay);
-  };
-  return v3;
+  }
+  return overlay_close;
 }
