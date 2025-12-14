a50a03bcf5a10c86511ad72e61a0659fa5661592
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 12:17:42 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -143,7 +143,7 @@ export function app_a_function_node(a) {
         }
         list_add(on_keydowns, on_keydown);
         function lambda15() {
-          list_remove(list, item);
+          list_remove(on_keydowns, on_keydown);
           html_remove(component);
         }
         let b = app_a_button(overlay, "Close", lambda15);
