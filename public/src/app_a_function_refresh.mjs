c80cbbcb52f5d6d1ca4d43dd93056dcc7c12b8ee
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 14:37:03 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

---
+++ public/src/app_a_function_refresh.mjs
@@ -0,0 +1,31 @@
+import { app_a_function_node } from "../../../love/public/src/app_a_function_node.mjs";
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+import { app_a_button } from "../../../love/public/src/app_a_button.mjs";
+import { emoji_search } from "../../../love/public/src/emoji_search.mjs";
+import { app_a_home } from "../../../love/public/src/app_a_home.mjs";
+import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
+export function app_a_function_refresh(root, context, ast) {
+  html_clear(root);
+  function lambda2() {
+    app_generic_screen_set(context, app_a_home);
+  }
+  let text = emoji_search();
+  let b = app_a_button(root, text, lambda2);
+  let div = html_div(root);
+  html_font_jetbrains_mono(div);
+  html_style_assign(div, {
+    "overflow-wrap": "break-word",
+    "word-break": "break-word",
+    "font-weight": "500",
+  });
+  app_a_function_node({
+    node: ast,
+    parent: div,
+    context,
+    indent: 0,
+    root,
+  });
+}
