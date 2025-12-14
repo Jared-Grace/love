07e06cdfeb507c0a0799cfe711ed4382189dbb0b
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 18:19:25 -0500

    function_auto_path ..\love\public\src\cache_clear.mjs

--- public/src/cache_clear.mjs
+++ public/src/cache_clear.mjs
@@ -1,13 +1,10 @@
-import { global_function_property_delete } from "../../../love/public/src/global_function_property_delete.mjs";
+import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
 import { http_post_json_cache_global_args } from "../../../love/public/src/http_post_json_cache_global_args.mjs";
 import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
-import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function cache_clear() {
   marker("1");
   let fn = http_post_json;
   let args = http_post_json_cache_global_args(url, body);
-  let key_get = invoke_cache_key_get(fn, args);
-  let property_name = key_get();
-  global_function_property_delete(fn, property_name);
+  invoke_cache_clear(fn, args);
 }
