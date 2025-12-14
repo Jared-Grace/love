ea4b07731da8b45299eac7aa043094d48386a65e
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 17:46:20 -0500

    data_file_update ..\love\public\src\js_format.mjs

--- public/src/js_format.mjs
+++ public/src/js_format.mjs
@@ -1,11 +1,13 @@
-import prettier from "prettier";
-import parserBabel from "prettier/plugins/babel";
-export async function js_format(code) {
+export async function js_format(code) {let prettierModule, babelPlugin;
   if (browser_is()) {
-    const prettier = await import("https://esm.sh/prettier@3.2.5");
-    const parserBabel = await import(
+     prettier = await import("https://esm.sh/prettier@3.2.5");
+     parserBabel = await import(
       "https://esm.sh/prettier@3.2.5/plugins/babel"
     );
+  } else {
+    // Node.js fallback (optional)
+    prettierModule = await import("prettier");
+    babelPlugin = await import("prettier/plugins/babel");
   }
   const formatted = await prettier.format(code, {
     parser: "babel",
