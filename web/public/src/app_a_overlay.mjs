aade509760c60e39fa811fb3a5cf3621b2ad88fd
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 16:59:03 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_overlay.mjs
+++ web/public/src/app_a_overlay.mjs
@@ -4,6 +4,7 @@ import { html_remove } from "../../../love/public/src/html_remove.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 export function app_a_overlay(root, on_keydowns, on_keydown) {
   marker("1");
+        list_add(on_keydowns, on_keydown);
   let overlay = html_overlay_z_max(root);
   function overlay_close() {
     list_remove(on_keydowns, on_keydown);
