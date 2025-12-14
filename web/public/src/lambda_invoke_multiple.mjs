027c71f062fb5b417cc46904665e38b724a4b683
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 12:01:12 -0500

    data_file_update ..\love\public\src\invoke_multiple.mjs

--- web/public/src/lambda_invoke_multiple.mjs
+++
@@ -1,5 +0,0 @@
-import { lambda_invoke } from "../../../love/public/src/lambda_invoke.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-export function lambda_invoke_multiple(afters) {
-  each(afters, lambda_invoke);
-}
