e85f367bf8d4922440f1fb2b05b4cdec09226469
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 18:35:42 -0500

    data_file_update ..\love\public\src\file_read.mjs

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -4,4 +4,10 @@ export async function file_read(file_path) {
   let fs = await import("fs");
   let contents = await fs.promises.readFile(file_path, "utf-8");
   return contents;
+  if (browser_is()) {
+    let r = await app_api_cache_global_fn(file_overwrite, arguments);
+    var { url, body } = app_api_generic_url_body(file_read, args);
+    http_post_json_cache_clear(url, body);
+    return r;
+  }
 }
