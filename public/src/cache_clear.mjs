cb6f8eb1b37a80f42d4f5e3404413aeb91ff7bc6
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 18:04:55 -0500

    function_auto_path ..\love\public\src\cache_clear.mjs

--- public/src/cache_clear.mjs
+++ public/src/cache_clear.mjs
@@ -1,6 +1,8 @@
+import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
 import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function cache_clear() {
   marker("1");
+  let fn = http_post_json;
   let key_get = invoke_cache_key_get(fn, args);
 }
