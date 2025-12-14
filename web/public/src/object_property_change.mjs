2dfe7c801285d9f17e4f937004cd0d0f2688391b
Grace-Jared <christrosetolife@gmail.com>
2025-12-13 20:07:43 -0500

    function_auto_path ..\love\public\src\object_property_change.mjs

--- web/public/src/object_property_change.mjs
+++ web/public/src/object_property_change.mjs
@@ -1,8 +1,8 @@
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function object_property_change(o, property, change) {
-  let objection = object_property_get(o, property);
-  objection = change(objection);
-  object_property_set(o, property, objection);
-  return objection;
+  let value = object_property_get(o, property);
+  value = change(value);
+  object_property_set(o, property, value);
+  return value;
 }
