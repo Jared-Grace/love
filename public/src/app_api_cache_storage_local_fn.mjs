ef80531a5bfb2e30a90764547f9f6e4e33af8761
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 18:48:31 -0500

    function_auto_path ..\love\public\src\app_api_cache_storage_local_fn.mjs

--- public/src/app_api_cache_storage_local_fn.mjs
+++ public/src/app_api_cache_storage_local_fn.mjs
@@ -1,7 +1,8 @@
 import { app_api_cache_storage_local } from "../../../love/public/src/app_api_cache_storage_local.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_api_cache_storage_local_fn(f_name, args) {
+export async function app_api_cache_storage_local_fn(fn, args) {
   marker("1");
+  let f_name = fn.name;
   let v = await app_api_cache_storage_local(f_name, args);
   return v;
 }
