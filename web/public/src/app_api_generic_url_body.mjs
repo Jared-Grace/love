916fb8b0baedb14d9f70b2c5ccc7222792410152
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 17:35:18 -0500

    function_auto_path ..\love\public\src\app_api_generic_url_body.mjs

--- web/public/src/app_api_generic_url_body.mjs
+++ web/public/src/app_api_generic_url_body.mjs
@@ -1,7 +1,8 @@
+import { server_api_url } from "../../../love/public/src/server_api_url.mjs";
 import { app_api_generic_f_name_args } from "../../../love/public/src/app_api_generic_f_name_args.mjs";
 export function app_api_generic_url_body(f_name, args) {
   let body = app_api_generic_f_name_args(f_name, args);
-  const url = "/api";
+  const url = server_api_url();
   let v = {
     url,
     body,
