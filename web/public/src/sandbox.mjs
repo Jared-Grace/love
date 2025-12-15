6e1ce4fecef7478a09d71b549d7da676797c55a2
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 23:14:26 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -3,7 +3,6 @@ import { file_read } from "../../../love/public/src/file_read.mjs";
 import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
 import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
 export async function sandbox() {
-  '[\"http_post_json\",[\"/api\",{\"f_name\":\"file_read\",\"args\":[\"../love/public/src/app_g_main.mjs\"]}]]';
   let a = app_api_generic_f_name_args(file_read.name, [
     "../love/public/src/app_g_main.mjs",
   ]);
