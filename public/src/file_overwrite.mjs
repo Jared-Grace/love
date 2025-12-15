e56461d77690bbe368783ebfaaaa3957f0eb9d53
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 17:38:39 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,7 +1,6 @@
-import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
+import { file_read } from "../../../love/public/src/file_read.mjs";
+import { app_api_cache_clear } from "../../../love/public/src/app_api_cache_clear.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { app_api_generic_url_body } from "../../../love/public/src/app_api_generic_url_body.mjs";
-import { http_post_json_cache_clear } from "../../../love/public/src/http_post_json_cache_clear.mjs";
 import { app_api_cache_global_fn } from "../../../love/public/src/app_api_cache_global_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
@@ -9,8 +8,8 @@ export async function file_overwrite(file_path, contents) {
   marker("1");
   if (browser_is()) {
     let r = await app_api_cache_global_fn(file_overwrite, arguments);
-    var { url, body } = app_api_generic_url_body(http_post_json, [file_path]);
-    http_post_json_cache_clear(url, body);
+    let args = [file_path];
+    app_api_cache_clear(file_read, args);
     return r;
   }
   await file_parent_exists_ensure(file_path);
