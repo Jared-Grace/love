cf1b2ce1784de78d43a28850be48e7d82cc4eea8
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 17:59:50 -0500

    fws app_api fn

--- public/src/app_api_fn.mjs
+++ public/src/app_api_fn.mjs
@@ -1,4 +1,6 @@
+import { app_api } from "../../../love/public/src/app_api.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_api_fn() {
+export async function app_api_fn(f_name, args) {
   marker("1");
+  return await app_api(f_name, args);
 }
