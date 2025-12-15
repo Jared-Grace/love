4f0b0e28297541619a4ad6ba480a748abaa64ce4
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 22:17:28 -0500

    data_file_update ..\love\public\src\function_run_io_file.mjs

--- public/src/function_run_io_file.mjs
+++ public/src/function_run_io_file.mjs
@@ -8,7 +8,7 @@ export async function function_run_io_file(temp_path_input, temp_path_output) {
   let data = await file_read_json(temp_path_input);
   let f_name = object_property_get(data, "f_name");
   let args = object_property_get(data, "args");
-  let result = await function_run(f_name, args);
+  let result = await function_run(f_name, args);undefined_is
   if (false) {
   }
   await file_overwrite_json(temp_path_output, {
