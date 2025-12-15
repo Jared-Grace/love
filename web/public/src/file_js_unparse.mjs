f0512bff5a46b1333325b4cfd522995372ae9b39
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 18:05:16 -0500

    o file_js_unparse

--- web/public/src/file_js_unparse.mjs
+++ web/public/src/file_js_unparse.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { js_format } from "../../../love/public/src/js_format.mjs";
 export async function file_js_unparse(parsed) {
+  marker("1");
   let { f_path, ast, code } = parsed;
   let code_unparsed = await js_unparse(ast);
   let code_new = await js_format(code_unparsed);
