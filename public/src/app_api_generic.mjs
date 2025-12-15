233191e3725701c51bcc951059ecba68da28b152
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 20:30:55 -0500

    function_auto_path ..\love\public\src\app_api_generic.mjs

--- public/src/app_api_generic.mjs
+++ public/src/app_api_generic.mjs
@@ -1,10 +1,8 @@
 import { app_api_generic_url_body } from "../../../love/public/src/app_api_generic_url_body.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_api_generic(f_name, args, fn) {
   marker("1");
   var { url, body } = app_api_generic_url_body(f_name, args);
   let o = await fn(url, body);
-  let result = object_property_get(o, "result");
-  return result;
+  return o;
 }
