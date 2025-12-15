5f68b61baecf0072765c60c654cefd5c44c00041
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 22:55:53 -0500

    function_auto_path ..\love\public\src\invoke_cache_clear.mjs

--- web/public/src/invoke_cache_clear.mjs
+++ web/public/src/invoke_cache_clear.mjs
@@ -4,5 +4,5 @@ import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_
 export function invoke_cache_clear(fn, args) {
   let key_get = invoke_cache_key_get(fn, args);
   let property_name = key_get();
-  global_function_property_delete(invoke_cache_global.name, property_name);
+  global_function_property_delete(invoke_cache_global, property_name);
 }
