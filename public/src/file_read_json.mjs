0d5ad2130ca9616b7b615125f01ee850999a47fc
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 20:32:23 -0500

    o file_read_json

--- public/src/file_read_json.mjs
+++ public/src/file_read_json.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export async function file_read_json(file_path) {
+  marker("1");
   let json = await file_read(file_path);
   let data = json_from(json);
   return data;
