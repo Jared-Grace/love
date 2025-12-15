f150ae09f6b27ee57e5b4ac0e72aa0981937a2a3
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 17:55:18 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,13 +1,13 @@
+import { app_api } from "../../../love/public/src/app_api.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { app_api_cache_clear } from "../../../love/public/src/app_api_cache_clear.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { app_api_cache_global_fn } from "../../../love/public/src/app_api_cache_global_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_overwrite(file_path, contents) {
   marker("1");
   if (browser_is()) {
-    let r = await app_api_cache_global_fn(file_overwrite, arguments);
+    let r = await app_api(file_overwrite, arguments);
     app_api_cache_clear(file_read, [file_path]);
     return r;
   }
