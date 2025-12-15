bf8b7a22e33f06f64af4be0bbd22f164b8b96b28
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 18:00:47 -0500

    function_auto_path ..\love\public\src\app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -66,10 +66,10 @@ export async function app_g_main() {
   }
   each([body, html], lambda);
   let path_prefix = "";
-  let li = localhost_is();
+  let ll = localhost_is();
   const path_part = "latest";
   let sw = location_pathname_part_first_starts_with(path_part);
-  if (li || sw) {
+  if (ll || sw) {
     path_prefix = "..\\";
   }
   const game_prefix = g_folder_img(path_prefix);
