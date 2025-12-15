fd6e9f735a6dd5578d302673e2eca07d5a47d7f7
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 23:06:00 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,5 @@
 import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
+import { file_read } from "./file_read.mjs";
 export async function sandbox() {
-  invoke_cache_clear(fn, args);
+  invoke_cache_clear(file_read, "test.mjs");
 }
