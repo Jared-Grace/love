b5427d55dbcb2548e1d3356e33201b2e34452420
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 17:56:53 -0500

    function_auto_path ..\love\public\src\app_api_generic_f_name_args.mjs

--- web/public/src/app_api_generic_f_name_args.mjs
+++ web/public/src/app_api_generic_f_name_args.mjs
@@ -1,7 +1,9 @@
+import { list_to } from "../../../love/public/src/list_to.mjs";
 export function app_api_generic_f_name_args(f_name, args) {
+  let args_list = list_to(args);
   let v2 = {
     f_name: f_name,
-    args: args,
+    args: args_list,
   };
   return v2;
 }
