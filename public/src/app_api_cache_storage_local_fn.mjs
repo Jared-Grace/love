993a540ea1799b28ea16a14fec79daeb17d25227
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 18:48:13 -0500

    data_file_update ..\love\public\src\app_api_cache_storage_local_fn.mjs

--- public/src/app_api_cache_storage_local_fn.mjs
+++ public/src/app_api_cache_storage_local_fn.mjs
@@ -1,1 +1,4 @@
-export function app_api_cache_storage_local_fn() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function app_api_cache_storage_local_fn() {
+  marker("1");
+}
