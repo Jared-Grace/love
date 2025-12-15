ed48f3c5d9f0ed7c67e3ff561f7b1959b77c5357
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 22:17:17 -0500

    o function_run_io_file

--- public/src/app_api_generic.mjs
+++ public/src/app_api_generic.mjs
@@ -1,5 +1,3 @@
-import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { app_api_generic_url_body } from "../../../love/public/src/app_api_generic_url_body.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -7,7 +5,6 @@ export async function app_api_generic(f_name, args, fn) {
   marker("1");
   var { url, body } = app_api_generic_url_body(f_name, args);
   let o = await fn(url, body);
-  let n = object_property_exists_not(object, property_name);
   log({
     o,
     url,
