85028082f1c2c2c64ae4c53a7d8169d0e781c607
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 20:32:49 -0500

    function_auto_path ..\love\public\src\app_api_cache_global_fn.mjs

--- public/src/app_api_cache_global_fn.mjs
+++ public/src/app_api_cache_global_fn.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_api_cache_global } from "../../../love/public/src/app_api_cache_global.mjs";
 import { list_to } from "../../../love/public/src/list_to.mjs";
@@ -6,5 +7,6 @@ export async function app_api_cache_global_fn(fn, args) {
   let function_name = fn.name;
   let args_list = list_to(args);
   let r = await app_api_cache_global(function_name, args_list);
+  log(message);
   return r;
 }
