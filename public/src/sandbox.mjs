aa4fa2d85edd6d4462a73325367c8382e260f80e
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 23:05:23 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
 export async function sandbox() {
+  marker("1");
   let all = await ebible_languages_chapters();
   return all;
 }
