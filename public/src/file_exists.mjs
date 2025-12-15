4f3e5b152bf5ad7c249654ad7f79736e29f2e306
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 18:48:54 -0500

    function_auto_path ..\love\public\src\file_exists.mjs

--- public/src/file_exists.mjs
+++ public/src/file_exists.mjs
@@ -1,4 +1,4 @@
-import { app_api_cache_global_fn } from "../../../love/public/src/app_api_cache_global_fn.mjs";
+import { app_api_cache_storage_local_fn } from "../../../love/public/src/app_api_cache_storage_local_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { throws_not_async } from "../../../love/public/src/throws_not_async.mjs";
@@ -6,7 +6,7 @@ import { error } from "../../../love/public/src/error.mjs";
 import { promise_is } from "../../../love/public/src/promise_is.mjs";
 export async function file_exists(file_path) {
   if (browser_is()) {
-    let r = await app_api_cache_global_fn(file_exists, arguments);
+    let r = await app_api_cache_storage_local_fn(file_exists, arguments);
     return r;
   }
   marker("1");
