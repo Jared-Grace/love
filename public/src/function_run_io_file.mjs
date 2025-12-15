7777b1bf400536024051adefcc4cc802867a85ce
Grace-Jared <christrosetolife@gmail.com>
2025-12-14 22:15:54 -0500

    o function_run_io_file

--- public/src/function_run_io_file.mjs
+++ public/src/function_run_io_file.mjs
@@ -1,8 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 export async function function_run_io_file(temp_path_input, temp_path_output) {
+  marker("1");
   let data = await file_read_json(temp_path_input);
   let f_name = object_property_get(data, "f_name");
   let args = object_property_get(data, "args");
