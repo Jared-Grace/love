dba909d1fd2020211e6ea46ec3626f1d5cb1a4f5
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 16:55:04 -0500

    function_auto_path ..\love\public\src\html_on_enter_lambda.mjs

--- web/public/src/html_on_enter_lambda.mjs
+++ web/public/src/html_on_enter_lambda.mjs
@@ -3,7 +3,7 @@ import { equal } from "../../../love/public/src/equal.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function html_on_enter_lambda(lambda) {
   marker("1");
-  let v3 = function html_on_enter_inner(e) {
+  let v3 = function html_on_enter_lambda_inner(e) {
     let k = object_property_get(e, "key");
     if (equal(k, "Enter")) {
       lambda();
