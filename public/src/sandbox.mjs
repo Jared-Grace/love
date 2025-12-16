70b7ba836428cf0b20c1970a0cc168ea88318f8c
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 16:53:21 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { app_api_cache_clear } from "../../../love/public/src/app_api_cache_clear.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 export async function sandbox() {
+  marker("1");
   let fn = file_read;
   const args = ["../love/public/src/app_g_main.mjs"];
   app_api_cache_clear(fn, args);
