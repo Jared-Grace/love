f4776c289748ca7fa84f6461209ff8eb63957b0a
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 16:54:17 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

--- web/public/src/function_rename.mjs
+++ web/public/src/function_rename.mjs
@@ -11,7 +11,10 @@ export async function function_rename(f_name_before, f_name_after) {
   f_name_before = unaliased;
   let { ast } = await function_parse(f_name_before);
   let identifiers_named = js_identifiers_named(ast, f_name_after);
-  list_empty_is_assert(identifiers_named);
+  list_empty_is_assert(identifiers_named, {
+    f_name_after,
+    msg: "already exists in file as identifier",
+  });
   await function_rename_fn_names_check(f_name_before);
   await function_move(f_name_before, f_name_after);
   await function_rename_identifiers_alias(f_name_before, f_name_after);
