6031bffbdf2fb4360b18a259a13eee94c0827ae2
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 16:45:16 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -1,3 +1,4 @@
+import { function_parse } from "../../../love/public/src/function_parse.mjs";
 import { function_rename_identifiers_alias } from "../../../love/public/src/function_rename_identifiers_alias.mjs";
 import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
@@ -6,6 +7,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename(f_name_before, f_name_after) {
   const { unaliased } = await function_name_to_path_unalias(f_name_before);
   f_name_before = unaliased;
+  let { ast } = await function_parse(f_name_before);
   await function_rename_fn_names_check(f_name_before);
   await function_move(f_name_before, f_name_after);
   await function_rename_identifiers_alias(f_name_before, f_name_after);
