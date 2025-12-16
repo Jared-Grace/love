107b62e19541eb90eda784c9c36d4b2e9dafa3c0
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 17:31:02 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- web/public/src/js_destructure_functionize.mjs
+++ web/public/src/js_destructure_functionize.mjs
@@ -1,4 +1,7 @@
+import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_destructure_functionize(ast) {
-  marker("1");js_visit_type
+  marker("1");
+  return;
+  js_visit_type(ast2, type, function lambda(v) {});
 }
