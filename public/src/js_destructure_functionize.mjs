91837a4ca729de90715077db92403d919c04484c
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 17:32:37 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- public/src/js_destructure_functionize.mjs
+++ public/src/js_destructure_functionize.mjs
@@ -3,11 +3,12 @@ import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_destructure_functionize(ast) {
   marker("1");
-  return;
   function lambda(v) {
     log({
       v,
     });
   }
   js_visit_type(ast, "VariableDeclarator", lambda);
+  return;
+  let { node, parent, context } = a;
 }
