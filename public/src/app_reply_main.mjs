886388a5d5b6de592eebdc97d03a0a837890df5d
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 19:58:56 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -195,16 +195,20 @@ export async function app_reply_main() {
   }
   buttons = list_map(choices, lambda);
   let original_include = true;
-  let component5 = html_button(parent, "Original Exclude", lambda15);
+  let component5 = html_button(parent, "", lambda15);
+  original_text_update();
   function lambda15() {
     original_include = not(original_include);
-    html_text_set(
-      component5,
-      '"Original ' + (original_include ? "In" : "Ex") + "clude",
-    );
+    original_text_update();
   }
   preview = html_p(root);
   buttons_refresh();
+  function original_text_update() {
+    html_text_set(
+      component5,
+      "Original " + (original_include ? "In" : "Ex") + "clude",
+    );
+  }
   async function preview_refresh() {
     let concated = concated_get();
     let joined = await list_join_newline_2_copy(concated);
