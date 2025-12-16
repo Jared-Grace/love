50e0bb3c20e31922e0689c9752affaa8ad35fc8a
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 19:54:05 -0500

    data_file_update ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -193,7 +193,7 @@ export async function app_reply_main() {
     }
     return component;
   }
-  buttons = list_map(choices, lambda);
+  buttons = list_map(choices, lambda);original=false
   function lambda15() {}
   let component5 = html_button(parent, text3, lambda15);
   preview = html_p(root);
