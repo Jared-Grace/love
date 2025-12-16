f54658bf4663c49a2d0684d93fe55302da9179f6
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 16:59:58 -0500

    function_auto_path ..\love\public\src\app_a_home.mjs

--- public/src/app_a_home.mjs
+++ public/src/app_a_home.mjs
@@ -63,7 +63,7 @@ export async function app_a_home(context) {
   }
   html_focus(input);
   function f_name_select(f_name) {
-    list_remove(context, on_keydown);
+    list_remove(on_keydowns, on_keydown);
     app_a_function_select(context, f_name);
   }
 }
