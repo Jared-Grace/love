cd38a64beb799335862f0d504627621e873beb2b
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 23:10:30 -0500

    function_auto_path ..\love\public\src\html_font_sans_serif_set_html.mjs

--- web/public/src/html_font_sans_serif_set_html.mjs
+++ web/public/src/html_font_sans_serif_set_html.mjs
@@ -1,4 +1,4 @@
-import { html_style_head } from "../../../love/public/src/html_style_head.mjs";
+import { html_style_button } from "../../../love/public/src/html_style_button.mjs";
 import { html_roboto_include } from "../../../love/public/src/html_roboto_include.mjs";
 import { html_font_set } from "../../../love/public/src/html_font_set.mjs";
 import { html_font_sans_serif_value } from "../../../love/public/src/html_font_sans_serif_value.mjs";
@@ -9,5 +9,5 @@ export function html_font_sans_serif_set_html() {
   let style_value = html_font_sans_serif_value();
   html_font_set(html2, style_value);
   const style = ":font-family: " + html_font_sans_serif_value();
-  html_style_head("button { " + style + " }");
+  html_style_button(style);
 }
