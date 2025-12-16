2e74e77747e2c1a5cb22fa6f2a34a54558028a74
Grace-Jared <christrosetolife@gmail.com>
2025-12-15 23:08:59 -0500

    function_auto_path ..\love\public\src\html_font_sans_serif_set_html.mjs

--- public/src/html_font_sans_serif_set_html.mjs
+++ public/src/html_font_sans_serif_set_html.mjs
@@ -6,7 +6,6 @@ import { html_document_root } from "../../../love/public/src/html_document_root.
 export function html_font_sans_serif_set_html() {
   html_roboto_include();
   let html2 = html_document_root();
-  html_roboto_include();
   let style_value = html_font_sans_serif_value();
   html_font_set(html2, style_value);
   html_style_head(
