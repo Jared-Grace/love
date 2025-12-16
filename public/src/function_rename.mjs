074794340b36e3dd7228b1eb43fa903c1dea8d3d
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 16:47:32 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -1,3 +1,5 @@
+import { list_empty_is_assert } from "../../../love/public/src/list_empty_is_assert.mjs";
+import { js_identifiers_named } from "../../../love/public/src/js_identifiers_named.mjs";
 import { function_parse } from "../../../love/public/src/function_parse.mjs";
 import { function_rename_identifiers_alias } from "../../../love/public/src/function_rename_identifiers_alias.mjs";
 import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
@@ -8,6 +10,8 @@ export async function function_rename(f_name_before, f_name_after) {
   const { unaliased } = await function_name_to_path_unalias(f_name_before);
   f_name_before = unaliased;
   let { ast } = await function_parse(f_name_before);
+  let identifiers_named = js_identifiers_named(ast, f_name_after);
+  list_empty_is_assert(specifiers);
   await function_rename_fn_names_check(f_name_before);
   await function_move(f_name_before, f_name_after);
   await function_rename_identifiers_alias(f_name_before, f_name_after);
