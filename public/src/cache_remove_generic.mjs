cee74f0291250f8f6d8a2f3ea4065fd5a3604e52
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 17:19:28 -0500

    data_file_update ..\love\public\src\cache_remove_generic.mjs

--- public/src/cache_remove_generic.mjs
+++ public/src/cache_remove_generic.mjs
@@ -2,5 +2,5 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function cache_remove_generic(key_get, cached_exists) {
   marker("1");
   let key = key_get();
-  let e = await cached_exists(key);
+  let e = await cached_exists(key);false_is_assert
 }
