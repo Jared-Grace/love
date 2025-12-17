419fff012714194ddd34d01f7f53f8c829f786ca
Grace-Jared <christrosetolife@gmail.com>
2025-12-16 20:43:47 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- web/public/src/js_destructure_functionize.mjs
+++ web/public/src/js_destructure_functionize.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -14,6 +15,7 @@ export function js_destructure_functionize(ast) {
     let properties = object_property_get(node2, "properties");
     let mi = list_multiple_is(properties);
     if (mi) {
+      each(list, function lambda2(item) {});
     }
     let stack = object_property_get(v, "stack");
     let id = object_property_get(node2, "id");
