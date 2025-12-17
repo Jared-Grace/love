994b7ca69ae8713b80c6f370aca45227ab15abf5
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 17:19:38 -0500

    function_auto_path ..\love\public\src\cache_remove_generic.mjs

--- public/src/false_is_assert.mjs
+++ public/src/false_is_assert.mjs
@@ -1,8 +1,8 @@
 import { false_is } from "../../../love/public/src/false_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
-export function false_is_assert(enabled) {
+export function false_is_assert(b) {
   marker("1");
-  let ti2 = false_is(enabled);
+  let ti2 = false_is(b);
   assert(ti2);
 }
