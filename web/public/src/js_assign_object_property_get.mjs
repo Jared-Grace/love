6feaf48d23f9f8bf8573938b88a22d2d731f1167
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 18:34:14 -0500

    function_auto_path ..\love\public\src\js_assign_object_property_get.mjs

--- web/public/src/js_assign_object_property_get.mjs
+++ web/public/src/js_assign_object_property_get.mjs
@@ -1,3 +1,4 @@
+import { js_call_object_property_get } from "../../../love/public/src/js_call_object_property_get.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { list_index_of_next_outside } from "../../../love/public/src/list_index_of_next_outside.mjs";
 import { js_declare } from "../../../love/public/src/js_declare.mjs";
@@ -5,10 +6,11 @@ import { js_identifier_unique_ast } from "../../../love/public/src/js_identifier
 export function js_assign_object_property_get(
   ast,
   property_name,
-  parsed,
+  parsed2,
   stack2,
   stack1,
 ) {
+  let parsed = js_call_object_property_get(property_name, object_name);
   let unique = js_identifier_unique_ast(ast, property_name);
   let assign = js_declare(unique, parsed);
   let index = list_index_of_next_outside(stack2, stack1);
