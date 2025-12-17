0cd974611bd6c837a09fc8bfc48d4eae585e847b
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 17:23:41 -0500

    function_auto_path ..\love\public\src\ebible_languages_chapters_cache_remove.mjs

---
+++ web/public/src/ebible_languages_chapters_cache_remove.mjs
@@ -0,0 +1,8 @@
+import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
+import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function ebible_languages_chapters_cache_remove() {
+  marker("1");
+  let v = await invoke_cache_file(ebible_languages_chapters, []);
+  return v;
+}
