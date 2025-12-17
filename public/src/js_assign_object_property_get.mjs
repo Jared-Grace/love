2ca6c6d0db3b5192a63646962c472014ef8c56f0
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 18:34:42 -0500

    function_auto_path ..\love\public\src\js_assign_object_property_get.mjs

--- public/src/js_assign_object_property_get.mjs
+++ public/src/js_assign_object_property_get.mjs
@@ -7,13 +7,13 @@ export function js_assign_object_property_get(
   ast,
   property_name,
   object_name,
-  stack2,
+  block_body,
   stack1,
 ) {
   let parsed = js_call_object_property_get(property_name, object_name);
   let unique = js_identifier_unique_ast(ast, property_name);
   let assign = js_declare(unique, parsed);
-  let index = list_index_of_next_outside(stack2, stack1);
-  list_insert(stack2, index, assign);
+  let index = list_index_of_next_outside(block_body, stack1);
+  list_insert(block_body, index, assign);
   return assign;
 }
