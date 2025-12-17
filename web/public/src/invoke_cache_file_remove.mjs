3b60ddb55e35c329e459fc85645b8b407da42242
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 17:31:22 -0500

    function_auto_path ..\love\public\src\invoke_cache_file_remove.mjs

--- web/public/src/invoke_cache_file_remove.mjs
+++ web/public/src/invoke_cache_file_remove.mjs
@@ -8,5 +8,5 @@ export async function invoke_cache_file_remove(fn, args) {
   let key_get = invoke_cache_file_key_get(fn, args);
   let cached_exists = file_exists;
   let cache_remove = file_delete;
-  await cache_remove_generic(key_get, cached_exists);
+  await cache_remove_generic(key_get, cached_exists,cache_remove);
 }
