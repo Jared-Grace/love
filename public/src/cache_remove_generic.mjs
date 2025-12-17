b91b259badc300c596b2ee9c9842cdd0c79e4f72
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 17:19:31 -0500

    function_auto_path ..\love\public\src\cache_remove_generic.mjs

--- public/src/cache_remove_generic.mjs
+++ public/src/cache_remove_generic.mjs
@@ -1,6 +1,8 @@
+import { false_is_assert } from "../../../love/public/src/false_is_assert.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function cache_remove_generic(key_get, cached_exists) {
   marker("1");
   let key = key_get();
-  let e = await cached_exists(key);false_is_assert
+  let e = await cached_exists(key);
+  false_is_assert(enabled);
 }
