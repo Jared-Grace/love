3c9be50a1f2318cf30e2c3119b6b0874a7ce7308
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 18:33:50 -0500

    function_auto_path ..\love\public\src\js_dollar_g.mjs

---
+++ web/public/src/js_assign_object_property_get.mjs
@@ -0,0 +1,17 @@
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+import { list_index_of_next_outside } from "../../../love/public/src/list_index_of_next_outside.mjs";
+import { js_declare } from "../../../love/public/src/js_declare.mjs";
+import { js_identifier_unique_ast } from "../../../love/public/src/js_identifier_unique_ast.mjs";
+export function js_assign_object_property_get(
+  ast,
+  property_name,
+  parsed,
+  stack2,
+  stack1,
+) {
+  let unique = js_identifier_unique_ast(ast, property_name);
+  let assign = js_declare(unique, parsed);
+  let index = list_index_of_next_outside(stack2, stack1);
+  list_insert(stack2, index, assign);
+  return assign;
+}
