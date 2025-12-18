fe08938c9a2930315e9b3b2aea4631bfe7c4b562
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 19:36:14 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- web/public/src/js_destructure_functionize.mjs
+++ web/public/src/js_destructure_functionize.mjs
@@ -9,7 +9,6 @@ import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { js_node_atomize_name } from "../../../love/public/src/js_node_atomize_name.mjs";
 import { js_identifier_unique_ast } from "../../../love/public/src/js_identifier_unique_ast.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -19,32 +18,29 @@ export function js_destructure_functionize(ast) {
   function lambda(v) {
     let node2 = object_property_get(v, "node");
     let properties = object_property_get(node2, "properties");
-    let mi = list_multiple_is(properties);
-    if (mi) {
-      let stack = object_property_get(v, "stack");
-      let e1 = list_get_end_1(stack);
-      js_node_type_is_assert(e1, "VariableDeclarator");
-      let block_body = list_get_end(stack, 4);
-      list_is_assert(block_body);
-      let block_body_item = list_get_end(stack, 3);
-      let unique = js_identifier_unique_ast(ast, variable_name);
-      function lambda2(p) {
-        let key = object_property_get(p, "key");
-        let name3 = js_identifier_name(key);
-        let value = object_property_get(p, "value");
-        let name2 = js_identifier_name(value);
-        let assign = js_assign_object_property_get(
-          name3,
-          unique,
-          block_body,
-          block_body_item,
-          name2,
-        );
-      }
-      each(properties, lambda2);
-      let expression = js_parse_expression(unique);
-      object_property_set(e1, "id", expression);
+    let stack = object_property_get(v, "stack");
+    let e1 = list_get_end_1(stack);
+    js_node_type_is_assert(e1, "VariableDeclarator");
+    let block_body = list_get_end(stack, 4);
+    list_is_assert(block_body);
+    let block_body_item = list_get_end(stack, 3);
+    let unique = js_identifier_unique_ast(ast, variable_name);
+    function lambda2(p) {
+      let key = object_property_get(p, "key");
+      let name3 = js_identifier_name(key);
+      let value = object_property_get(p, "value");
+      let name2 = js_identifier_name(value);
+      let assign = js_assign_object_property_get(
+        name3,
+        unique,
+        block_body,
+        block_body_item,
+        name2,
+      );
     }
+    each(properties, lambda2);
+    let expression = js_parse_expression(unique);
+    object_property_set(e1, "id", expression);
   }
   js_visit_type(ast, "ObjectPattern", lambda);
 }
