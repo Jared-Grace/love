a01f7c61d2738f2752a73550a9d75b61c4f2d95e
Grace-Jared <christrosetolife@gmail.com>
2025-12-17 19:41:53 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- public/src/js_destructure_functionize.mjs
+++ public/src/js_destructure_functionize.mjs
@@ -9,7 +9,6 @@ import { js_parse_expression } from "../../../love/public/src/js_parse_expressio
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { js_node_atomize_name } from "../../../love/public/src/js_node_atomize_name.mjs";
-import { js_identifier_unique_ast } from "../../../love/public/src/js_identifier_unique_ast.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
@@ -26,16 +25,16 @@ export function js_destructure_functionize(ast) {
       e1,
     });
     return;
+    let name4 = null;
     let init = object_property_get(e1, "init");
     let ii = js_identifier_is(init);
     if (ii) {
     }
+    $el;
     js_node_type_is_assert(e1, "VariableDeclarator");
     let block_body = list_get_end(stack, 4);
     list_is_assert(block_body);
     let block_body_item = list_get_end(stack, 3);
-    let name4 = null;
-    name4 = js_identifier_unique_ast(ast, variable_name);
     function lambda2(p) {
       let key = object_property_get(p, "key");
       let name3 = js_identifier_name(key);
