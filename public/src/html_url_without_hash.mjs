375a5a98bf91b67f4ce612ee86437df739920800
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 20:18:13 -0500

    data_file_update ..\love\public\src\html_url_without_hash.mjs

--- public/src/html_url_without_hash.mjs
+++ public/src/html_url_without_hash.mjs
@@ -1,4 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function html_url_without_hash() {
-  marker("1");
+  marker("1");const url = new URL(window.location.href);
+url.hash = "";
+const urlWithoutHash = url.toString();
+$r,urlWithoutHash
 }
