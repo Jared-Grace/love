89029c2738388149dc05bb4325d5dec3591fbd2b
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 18:52:35 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- public/src/app_a_download.mjs
+++ public/src/app_a_download.mjs
@@ -1,11 +1,9 @@
-import { list_map_path_join_left } from "../../../love/public/src/list_map_path_join_left.mjs";
+import { folder_read_recursive_paths_async } from "../../../love/public/src/folder_read_recursive_paths_async.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
-import { folder_read_recursive_async } from "../../../love/public/src/folder_read_recursive_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_download() {
   marker("1");
   let path_folder = folder_public();
-  let result = await folder_read_recursive_async(path_folder);
-  let combineds = list_map_path_join_left(result, path);
+  let combineds = await folder_read_recursive_paths_async(path_folder);
   return combineds;
 }
