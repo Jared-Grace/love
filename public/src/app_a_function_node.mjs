a98953573a538567a5adaec2c2a812a34edeb45e
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 16:50:01 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -231,7 +231,8 @@ export function app_a_function_node(a) {
       let source2 = object_property_get(node, "source");
       let k2 = js_keyword_import();
       app_a_keyword_blue(parent, k2);
-      app_a_parenthesis_wrap(parent, () => {});
+      function lambda20() {}
+      app_a_parenthesis_wrap(parent, lambda20);
       log({
         node,
       });
