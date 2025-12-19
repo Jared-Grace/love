686c38f597f9ba888a10a6360e0c20aeb07220cb
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 19:36:59 -0500

    function_auto_path ..\love\public\src\object_adder.mjs

--- public/src/object_adder.mjs
+++ public/src/object_adder.mjs
@@ -1,6 +1,6 @@
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function object_adder(lambda$oa) {
+export function object_adder(lambda) {
   marker("1");
   let reuslt = {};
   function oa(key, value) {
