3baa166708677e05f89d866437ec1409be7cf4d6
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 16:49:36 -0500

    function_auto_path ..\love\public\src\app_a_parenthesis_wrap.mjs

--- public/src/app_a_parenthesis_wrap.mjs
+++ public/src/app_a_parenthesis_wrap.mjs
@@ -1,7 +1,7 @@
 import { app_a_parenthesis_right } from "../../../love/public/src/app_a_parenthesis_right.mjs";
 import { app_a_parenthesis_left } from "../../../love/public/src/app_a_parenthesis_left.mjs";
-export function app_a_parenthesis_wrap(parent, inner) {
+export function app_a_parenthesis_wrap(parent, lambda) {
   app_a_parenthesis_left(parent);
-  inner();
+  lambda();
   app_a_parenthesis_right(parent);
 }
