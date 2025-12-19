7e1b02f447c9744ff223d2e28eb975eee31807f2
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 20:49:40 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -226,7 +226,7 @@ export function app_a_function_node(a) {
     app_a_function_node_child(a, right);
   }
   let n = object_property_exists_not(lookup, type);
-  if (n) {
+  if (n) {jt
     alert();
   }
   let value = object_property_get(lookup, type);
