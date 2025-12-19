6055ebd4a41e8ad59754bc25bb9dfce810d6544e
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 20:15:42 -0500

    function_auto_path ..\love\public\src\app_next_main.mjs

---
+++ web/public/src/html_hash_object_get.mjs
@@ -0,0 +1,25 @@
+import { object_adder } from "../../../love/public/src/object_adder.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
+import { string_split_equal } from "../../../love/public/src/string_split_equal.mjs";
+import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
+import { html_hash_get } from "../../../love/public/src/html_hash_get.mjs";
+export function html_hash_object_get() {
+  let hash = html_hash_get();
+  let without = string_prefix_without(hash, "#");
+  let split = string_split_comma(without);
+  function lambda3(oa) {
+    function lambda(s) {
+      let split2 = string_split_equal(s);
+      let v = list_first_second(split2);
+      let second = object_property_get(v, "second");
+      let first = object_property_get(v, "first");
+      oa(first, second);
+    }
+    each(split, lambda);
+  }
+  let result = object_adder(lambda3);
+  return result;
+}
