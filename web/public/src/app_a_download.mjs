6072ba485235c087df9a66126eb7f291d4de7042
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 18:48:22 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- web/public/src/app_a_download.mjs
+++ web/public/src/app_a_download.mjs
@@ -1,4 +1,6 @@
+import { folder_read_recursive_async } from "../../../love/public/src/folder_read_recursive_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_download() {
+export async function app_a_download() {
   marker("1");
+  let result = await folder_read_recursive_async(path_folder);
 }
