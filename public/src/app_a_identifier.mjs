c5f6f2c1f509e78187dea289d593794d91a419a5
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 17:04:31 -0500

    s keywo

--- public/src/app_a_identifier.mjs
+++ public/src/app_a_identifier.mjs
@@ -30,7 +30,7 @@ import { html_font_color_set } from "../../../love/public/src/html_font_color_se
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_identifier(node, parent, context, a, ast, parsed) {
-  let name = object_property_get(node, "name");
+  let name = object_property_get(node, "name");$i
   let span = html_span_text(parent, name);
   html_font_color_set(span, "#4a4affff");
   async function lambda20() {
