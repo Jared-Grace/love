8eeb2e2a97652c13e8b3c2606133436ae8bd683b
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 19:32:11 -0500

    function_auto_path ..\love\public\src\app_next_main.mjs

--- public/src/app_next_main.mjs
+++ public/src/app_next_main.mjs
@@ -1,9 +1,11 @@
+import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { html_hash_get } from "../../../love/public/src/html_hash_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_next_main() {
   marker("1");
   let hash = html_hash_get();
+  let without = string_prefix_without(s, prefix);
   log({
     hash,
   });
