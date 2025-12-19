4b9a8551abc0005082b27f8ce7342df24a08d7d0
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 19:31:13 -0500

    function_auto_path ..\love\public\src\app_next_main.mjs

--- web/public/src/app_next_main.mjs
+++ web/public/src/app_next_main.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { html_hash_get } from "../../../love/public/src/html_hash_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_next_main() {
   marker("1");
   let hash = html_hash_get();
+  log({
+    hash,
+  });
 }
