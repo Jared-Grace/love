70bff538b54026300a65fbe3f748a1b86883d8bb
Grace-Jared <christrosetolife@gmail.com>
2025-12-18 20:51:29 -0500

    function_auto_path ..\love\public\src\js_code_return.mjs

--- public/src/js_code_return.mjs
+++ public/src/js_code_return.mjs
@@ -1,4 +1,5 @@
+import { js_keyword_return } from "../../../love/public/src/js_keyword_return.mjs";
 export function js_code_return(return_argument_code) {
-  let v = "return " + return_argument_code;
+  let v = js_keyword_return() + " " + return_argument_code;
   return v;
 }
