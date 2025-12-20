363fb1788bd3dbef5045b36e40c52a258e6e8e6a
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:24:31 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- public/src/app_a_download.mjs
+++ public/src/app_a_download.mjs
@@ -5,8 +5,9 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_download() {
   marker("1");
   let filtered = await app_a_download_paths();
-  let contents = await file_read(file_path);
-  function lambda() {}
+  async function lambda() {
+    let contents = await file_read(file_path);
+  }
   let waited = await list_map_unordered_async(filtered, lambda);
   return waited;
 }
