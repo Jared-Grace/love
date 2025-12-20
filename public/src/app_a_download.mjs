a23bf6b88e40d3a2fa7989ef72c46bd30909cb6a
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:22:25 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- public/src/app_a_download.mjs
+++ public/src/app_a_download.mjs
@@ -1,7 +1,12 @@
+import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { app_a_download_paths } from "../../../love/public/src/app_a_download_paths.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_download() {
   marker("1");
   let filtered = await app_a_download_paths();
+  let waited = await list_map_unordered_async(
+    list,
+    async function lambda(item) {},
+  );
   return filtered;
 }
