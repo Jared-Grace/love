0e9a531992b5ac601eb73613204d2e88752b1bb6
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 20:16:06 -0500

    function_auto_path ..\love\public\src\indexeddb_initialize.mjs

--- web/public/src/indexeddb_initialize.mjs
+++ web/public/src/indexeddb_initialize.mjs
@@ -6,27 +6,25 @@ export function indexeddb_initialize() {
   const DB_NAME = "ide_fs";
   const DB_VERSION = 1;
   const STORE_FILES = "files";
-  function idb_init() {
-    let v3 = new Promise(function lambda4(resolve, reject) {
-      const req = indexedDB.open(DB_NAME, DB_VERSION);
-      req.onupgradeneeded = function lambda() {
-        const db = req.result;
-        let b = db.objectStoreNames.contains(STORE_FILES);
-        if (not(b)) {
-          db.createObjectStore(STORE_FILES, {
-            keyPath: "path",
-          });
-        }
-      };
-      req.onsuccess = function lambda2() {
-        let v = resolve(req.result);
-        return v;
-      };
-      req.onerror = function lambda3() {
-        let v2 = reject(req.error);
-        return v2;
-      };
-    });
-    return v3;
-  }
+  let v3 = new Promise(function lambda4(resolve, reject) {
+    const req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onupgradeneeded = function lambda() {
+      const db = req.result;
+      let b = db.objectStoreNames.contains(STORE_FILES);
+      if (not(b)) {
+        db.createObjectStore(STORE_FILES, {
+          keyPath: "path",
+        });
+      }
+    };
+    req.onsuccess = function lambda2() {
+      let v = resolve(req.result);
+      return v;
+    };
+    req.onerror = function lambda3() {
+      let v2 = reject(req.error);
+      return v2;
+    };
+  });
+  return v3;
 }
