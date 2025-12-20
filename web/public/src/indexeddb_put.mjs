588da26476e79e366136c514b70f3897c1c0465e
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 18:46:41 -0500

    function_auto_path ..\love\public\src\indexeddb_put.mjs

--- web/public/src/indexeddb_put.mjs
+++ web/public/src/indexeddb_put.mjs
@@ -1,11 +1,13 @@
+import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_put(db_get, store, path, content) {
   marker("1");
   let db = await db_get();
+  let normalized = path_normalize(path);
   const tx = db.transaction(store, "readwrite");
   const s = tx.objectStore(store);
   s.put({
-    path,
+    path: normalized,
     content,
     mtime: Date.now(),
   });
