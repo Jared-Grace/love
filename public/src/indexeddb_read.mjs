273eafa8c70e33abda7bebdff2c4990ee25be11c
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 18:46:51 -0500

    data_file_update ..\love\public\src\indexeddb_put.mjs

--- public/src/indexeddb_read.mjs
+++ public/src/indexeddb_read.mjs
@@ -3,10 +3,11 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_read(db_get, store, path) {
   marker("1");
   let db = await db_get();
+    let normalized = path_normalize(path);
   const tx = db.transaction(store, "readonly");
   const s = tx.objectStore(store);
   let v3 = await new Promise(function lambda3(resolve, reject) {
-    const req = s.get(path);
+    const req = s.get(normalized);
     req.onsuccess = function lambda() {
       let v = resolve(req.result?.content ?? null);
       return v;
