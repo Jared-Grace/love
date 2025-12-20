18cb102c0efb799da1b6a2916dabc22e859fc5e0
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 20:18:32 -0500

    function_auto_path ..\love\public\src\app_a_indexeddb_initialize.mjs

--- public/src/app_a_indexeddb_initialize.mjs
+++ public/src/app_a_indexeddb_initialize.mjs
@@ -2,12 +2,12 @@ import { app_a } from "../../../love/public/src/app_a.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_indexeddb_initialize() {
+export async function app_a_indexeddb_initialize() {
   marker("1");
   const db_name = app_a.name;
   const version = 1;
   const store_files = "files";
-  let v3 = new Promise(function lambda4(resolve, reject) {
+  let v3 = await new Promise(function lambda4(resolve, reject) {
     const req = indexedDB.open(db_name, version);
     req.onupgradeneeded = function lambda() {
       const db = req.result;
