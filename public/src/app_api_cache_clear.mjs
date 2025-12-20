df64f3dcce4d01311a72f8d1fa13669d452caa26
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:40:22 -0500

    function_auto_path ..\love\public\src\app_api_cache_clear.mjs

--- public/src/app_api_cache_clear.mjs
+++ public/src/app_api_cache_clear.mjs
@@ -1,10 +1,7 @@
+import { app_api_cache_args } from "../../../love/public/src/app_api_cache_args.mjs";
 import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
 import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
-import { server_api_url } from "../../../love/public/src/server_api_url.mjs";
-import { app_api_generic_f_name_args } from "../../../love/public/src/app_api_generic_f_name_args.mjs";
 export function app_api_cache_clear(fn, f_args) {
-  let a = app_api_generic_f_name_args(fn.name, f_args);
-  let a2 = server_api_url();
-  const args_cache = [a2, a];
+  const args_cache = app_api_cache_args(fn, f_args);
   invoke_cache_clear(http_post_json, args_cache);
 }
