ba02bd2caf33e8861663c21653994c46ac817fdc
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:27:10 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- public/src/app_a_download.mjs
+++ public/src/app_a_download.mjs
@@ -1,21 +1,17 @@
 import { list_to_dictionary_async } from "../../../love/public/src/list_to_dictionary_async.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
-import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { app_a_download_paths } from "../../../love/public/src/app_a_download_paths.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_download() {
   marker("1");
   let filtered = await app_a_download_paths();
-  async function lambda(file_path) {}
-  let waited = await list_map_unordered_async(filtered, lambda);
-  async function lambda2(item) {
+  async function lambda2(file_path) {
     let contents = await file_read(file_path);
     let v = {
-      file_path,
       contents,
     };
     return v;
   }
-  let dictionary = await list_to_dictionary_async(list, lambda2);
+  let dictionary = await list_to_dictionary_async(filtered, lambda2);
   return waited;
 }
