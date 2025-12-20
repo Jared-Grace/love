9fc187c094530590ce3957c0f9d653eb69c04f27
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:22:44 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- public/src/app_a_download.mjs
+++ public/src/app_a_download.mjs
@@ -1,10 +1,12 @@
+import { file_read } from "../../../love/public/src/file_read.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { app_a_download_paths } from "../../../love/public/src/app_a_download_paths.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_download() {
   marker("1");
   let filtered = await app_a_download_paths();
+  let contents = await file_read(file_path);
   async function lambda(item) {}
-  let waited = await list_map_unordered_async(list, lambda);
+  let waited = await list_map_unordered_async(filtered, lambda);
   return filtered;
 }
