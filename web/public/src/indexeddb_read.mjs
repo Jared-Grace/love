45a15e675ff662b254021225079d30ecc97b7e8a
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 20:48:11 -0500

    function_auto_path ..\love\public\src\indexeddb_read.mjs

--- web/public/src/indexeddb_read.mjs
+++ web/public/src/indexeddb_read.mjs
@@ -1,13 +1,20 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function indexeddb_read(db_get, store,path) {
+export async function indexeddb_read(db_get, store, path) {
   marker("1");
   let db = await db_get();
-const tx = db.transaction(store, "readonly");
+  const tx = db.transaction(store, "readonly");
   const s = tx.objectStore(store);
-
-  return await new Promise((resolve, reject) => {
+  let v3 = await new Promise(function lambda3(resolve, reject) {
     const req = s.get(path);
-    req.onsuccess = () => resolve(req.result?.content ?? null);
-    req.onerror = () => reject(req.error);
+    req.onsuccess = function lambda() {
+      let v = resolve(req.result?.content ?? null);
+      return v;
+    };
+    req.onerror = function lambda2() {
+      let v2 = reject(req.error);
+      return v2;
+    };
   });
+  return v3;
 }
