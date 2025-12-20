c0183f8480322461892768f88674c085e1a1cbce
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 18:46:54 -0500

    function_auto_path ..\love\public\src\indexeddb_read.mjs

--- web/public/src/indexeddb_read.mjs
+++ web/public/src/indexeddb_read.mjs
@@ -1,9 +1,10 @@
+import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_read(db_get, store, path) {
   marker("1");
   let db = await db_get();
-    let normalized = path_normalize(path);
+  let normalized = path_normalize(path);
   const tx = db.transaction(store, "readonly");
   const s = tx.objectStore(store);
   let v3 = await new Promise(function lambda3(resolve, reject) {
