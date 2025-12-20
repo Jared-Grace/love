8e7b1ca5ef522cd69325fa07bd5527c33e443631
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 20:24:03 -0500

    function_auto_path ..\love\public\src\indexeddb_put.mjs

--- public/src/indexeddb_put.mjs
+++ public/src/indexeddb_put.mjs
@@ -1,4 +1,13 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function indexeddb_put() {
   marker("1");
+  const tx = db.transaction(STORE_FILES, "readwrite");
+  const store = tx.objectStore(STORE_FILES);
+  store.put({
+    path,
+    content,
+    mtime: Date.now(),
+  });
+  let v = tx.complete;
+  return v;
 }
