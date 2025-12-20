439f75fd573fcbde07dc12c83e3fcb01174fbd08
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:22:15 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

---
+++ public/src/app_a_download_paths.mjs
@@ -0,0 +1,13 @@
+import { list_filter_ends_with_any } from "../../../love/public/src/list_filter_ends_with_any.mjs";
+import { html_extension } from "../../../love/public/src/html_extension.mjs";
+import { function_name_extension } from "../../../love/public/src/function_name_extension.mjs";
+import { folder_read_recursive_paths_async } from "../../../love/public/src/folder_read_recursive_paths_async.mjs";
+import { folder_public } from "../../../love/public/src/folder_public.mjs";
+export async function app_a_download_paths() {
+  let path_folder = folder_public();
+  let combineds = await folder_read_recursive_paths_async(path_folder);
+  let ext_f = function_name_extension();
+  let ext_h = html_extension();
+  let filtered = list_filter_ends_with_any(combineds, [ext_f, ext_h]);
+  return filtered;
+}
