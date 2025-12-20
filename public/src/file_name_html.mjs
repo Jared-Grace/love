8e49733b01f680e52c016b9562d94307dfb2b238
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:18:00 -0500

    function_auto_path ..\love\public\src\file_name_html.mjs

--- public/src/file_name_html.mjs
+++ public/src/file_name_html.mjs
@@ -1,4 +1,5 @@
+import { html_extension } from "../../../love/public/src/html_extension.mjs";
 export function file_name_html(name) {
-  let file_name = name + ".html";
+  let file_name = name + html_extension();
   return file_name;
 }
