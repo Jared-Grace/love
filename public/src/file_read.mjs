05a482851efb13733bbe4e5cc1fcedc5b9a7e8ba
Grace-Jared <christrosetolife@gmail.com>
2025-12-19 19:43:39 -0500

    function_auto_path ..\love\public\src\file_read.mjs

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,12 +1,10 @@
-import { app_api_cache_args } from "../../../love/public/src/app_api_cache_args.mjs";
 import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
 import { app_api_cache_storage_local_fn } from "../../../love/public/src/app_api_cache_storage_local_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read(file_path) {
   if (browser_is()) {
-    let fs = await app_api_cache_storage_local_fn(app_a_download, []);
-    const args_cache = app_api_cache_args(fn, f_args);
+    let file_system = await app_api_cache_storage_local_fn(app_a_download, []);
     let r = await app_api_cache_storage_local_fn(file_read, arguments);
     return r;
   }
