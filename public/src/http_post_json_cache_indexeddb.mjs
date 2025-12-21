bc4000e1ce7b3ec453884a40b34141689a474672
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 19:13:00 -0500

    fw invoke_cache_indexeddb http_post_json_cache_indexeddb

--- public/src/http_post_json_cache_indexeddb.mjs
+++ public/src/http_post_json_cache_indexeddb.mjs
@@ -1,1 +1,6 @@
-export function http_post_json_cache_indexeddb() {}
+import { invoke_cache_indexeddb } from "../../../love/public/src/invoke_cache_indexeddb.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function http_post_json_cache_indexeddb(fn, args, db_get, store) {
+  marker("1");
+  return await invoke_cache_indexeddb(fn, args, db_get, store);
+}
