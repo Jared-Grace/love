611680cdee0a5cf2fc65fb4e5ccc283de17d3c31
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 21:35:32 -0500

    function_auto_path ..\love\public\src\file_read.mjs

--- web/public/src/file_read.mjs
+++ web/public/src/file_read.mjs
@@ -1,11 +1,11 @@
+import { app_api_cache_indexeddb_fn } from "../../../love/public/src/app_api_cache_indexeddb_fn.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
-import { app_api_cache_storage_local_fn } from "../../../love/public/src/app_api_cache_storage_local_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read(file_path) {
   if (browser_is()) {
-    let file_system = await app_api_cache_storage_local_fn(app_a_download, []);
+    let file_system = await app_api_cache_indexeddb_fn(app_a_download, []);
     let r = object_property_get(file_system, file_path);
     return r;
   }
