1c6e4b52f15229313d4fceabb7ecc44984f32999
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 21:32:47 -0500

    fw function_multiple_param_new_double function_multiple_param_new_double_multiple

--- web/public/src/function_multiple_param_new_double_multiple.mjs
+++ web/public/src/function_multiple_param_new_double_multiple.mjs
@@ -1,1 +1,9 @@
-export function function_multiple_param_new_double_multiple() {}
+import { function_multiple_param_new_double } from "../../../love/public/src/function_multiple_param_new_double.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_multiple_param_new_double_multiple(
+  f_names_comma,
+  param_name,
+) {
+  marker("1");
+  return await function_multiple_param_new_double(f_names_comma, param_name);
+}
