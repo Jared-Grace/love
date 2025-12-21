d1b3a2fd61b0bb472b46b2b30acf63fad4eaee4e
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 19:14:03 -0500

    function_auto_path ..\love\public\src\http_post_json_cache_indexeddb.mjs

--- public/src/http_post_json_cache_indexeddb.mjs
+++ public/src/http_post_json_cache_indexeddb.mjs
@@ -1,7 +1,13 @@
+import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
 import { invoke_cache_indexeddb } from "../../../love/public/src/invoke_cache_indexeddb.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function http_post_json_cache_indexeddb(fn, args, db_get, store) {
+export async function http_post_json_cache_indexeddb(url, body, db_get, store) {
   marker("1");
-  let v = await invoke_cache_indexeddb(fn, args, db_get, store);
+  let v = await invoke_cache_indexeddb(
+    http_post_json,
+    [url, body],
+    db_get,
+    store,
+  );
   return v;
 }
