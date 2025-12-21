de9072d617465200a6fc049065b69e1b9800c1d2
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 21:31:02 -0500

    function_auto_path ..\love\public\src\app_api_cache_indexeddb.mjs

--- public/src/app_api_cache_indexeddb.mjs
+++ public/src/app_api_cache_indexeddb.mjs
@@ -3,7 +3,10 @@ import { app_api_generic } from "../../../love/public/src/app_api_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_api_cache_indexeddb(f_name, args) {
   marker("1");
-  let fn = http_post_json_cache_indexeddb;
+  let fn = async function lambda(url, body) {
+    let v = await http_post_json_cache_indexeddb(url, body, db_get, store);
+    return v;
+  };
   let result = await app_api_generic(f_name, args, fn);
   return result;
 }
