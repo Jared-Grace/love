d856884e5469a17ce8061ea946d14e6dbba4d316
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 18:03:47 -0500

    function_auto_path ..\love\public\src\app_reply_response_how_family.mjs

--- web/public/src/app_reply_response_how_family.mjs
+++ web/public/src/app_reply_response_how_family.mjs
@@ -1,5 +1,7 @@
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 export function app_reply_response_how_family() {
-  let how_family = emoji_pray() + "Please pray for my family";
+  let how_family =
+    emoji_pray() +
+    "My family are those who do the will of God! Please pray for my family";
   return how_family;
 }
