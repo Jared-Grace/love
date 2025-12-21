d24840f9e61d6bda0fd21e9b1ef866109f5432fc
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 21:33:31 -0500

    function_auto_path ..\love\public\src\function_multiple_param_new_double_multiple.mjs

--- public/src/function_multiple_param_new_double_multiple.mjs
+++ public/src/function_multiple_param_new_double_multiple.mjs
@@ -1,9 +1,11 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_multiple_param_new_double } from "../../../love/public/src/function_multiple_param_new_double.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_param_new_double_multiple(
   f_names_comma,
   param_name,
 ) {
+  await each_async(list, async function lambda(item) {});
   marker("1");
   let v = await function_multiple_param_new_double(f_names_comma, param_name);
   return v;
