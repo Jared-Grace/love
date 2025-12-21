5791cb5a86f052a023e9669442567d3540018440
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 19:15:16 -0500

    fws app_api_cache_indexeddb fn

--- public/src/app_api_cache_indexeddb_fn.mjs
+++ public/src/app_api_cache_indexeddb_fn.mjs
@@ -1,1 +1,6 @@
-export function app_api_cache_indexeddb_fn() {}
+import { app_api_cache_indexeddb } from "../../../love/public/src/app_api_cache_indexeddb.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function app_api_cache_indexeddb_fn(f_name, args) {
+  marker("1");
+  return await app_api_cache_indexeddb(f_name, args);
+}
