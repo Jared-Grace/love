b0ae239f537578a16fbf8c99a3606003b7c20617
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 21:34:09 -0500

    function_auto_path ..\love\public\src\function_multiple_param_new_double_multiple.mjs

--- public/src/function_multiple_param_new_double_multiple.mjs
+++ public/src/function_multiple_param_new_double_multiple.mjs
@@ -1,4 +1,4 @@
-import { string_split_multiple } from "../../../love/public/src/string_split_multiple.mjs";
+import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_multiple_param_new_double } from "../../../love/public/src/function_multiple_param_new_double.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,7 +6,7 @@ export async function function_multiple_param_new_double_multiple(
   f_names_comma,
   param_names,
 ) {
-  let parts = string_split_multiple(str, delimiters);
+  let split = string_split_comma(f_names);
   async function lambda(item) {
     let v = await function_multiple_param_new_double(f_names_comma, param_name);
     return v;
