9b4956b38ff7d336c1a4e8755bcbd7a50a622fab
Grace-Jared <christrosetolife@gmail.com>
2025-12-20 21:34:50 -0500

    data_file_update ..\love\public\src\app_api_cache_indexeddb_fn.mjs

--- public/src/app_api_cache_indexeddb_fn.mjs
+++ public/src/app_api_cache_indexeddb_fn.mjs
@@ -1,6 +1,6 @@
 import { app_api_cache_indexeddb } from "../../../love/public/src/app_api_cache_indexeddb.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_api_cache_indexeddb_fn(f_name, args, db_get) {
+export async function app_api_cache_indexeddb_fn(f_name, args, db_get, store) {
   marker("1");
   let v = await app_api_cache_indexeddb(f_name, args, db_get, store);
   return v;
