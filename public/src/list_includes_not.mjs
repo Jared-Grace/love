da7d252014031a0b4a340f87156e0940481643c5
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:26:41 -0500

    function_auto_path ..\love\public\src\list_includes_not.mjs

--- public/src/list_includes_not.mjs
+++ public/src/list_includes_not.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-export function list_includes_not(list, name) {
-  let b = list_includes(list, name);
+export function list_includes_not(list, item) {
+  let b = list_includes(list, item);
   let n = not(b);
   return n;
 }
