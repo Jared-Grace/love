3fef82f879a16c206a2e8115cdcefcb0e39d1da6
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 18:17:52 -0500

    function_auto_path ..\love\public\src\file_exists.mjs

--- public/src/file_exists.mjs
+++ public/src/file_exists.mjs
@@ -11,7 +11,8 @@ import { promise_is } from "../../../love/public/src/promise_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function file_exists(file_path) {
   if (browser_is()) {
-    let sw = string_starts_with(s, "../");
+    const prefix = "../";
+    let sw = string_starts_with(s, prefix);
     if (false) {
     }
     await app_a_file_system_initialize();
