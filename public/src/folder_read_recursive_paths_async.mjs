42ed32b1588f748d0aa987a9f075a5d276850513
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 23:04:57 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_paths_async.mjs

--- public/src/folder_read_recursive_paths_async.mjs
+++ public/src/folder_read_recursive_paths_async.mjs
@@ -1,7 +1,10 @@
+import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_map_path_join_left } from "../../../love/public/src/list_map_path_join_left.mjs";
 import { folder_read_recursive_async } from "../../../love/public/src/folder_read_recursive_async.mjs";
 export async function folder_read_recursive_paths_async(path_folder) {
   let result = await folder_read_recursive_async(path_folder);
   let combineds = list_map_path_join_left(result, path_folder);
-  return combineds;
+  let mapped = list_map(combineds, path_normalize);
+  return mapped;
 }
