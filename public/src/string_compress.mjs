f9f4ee6a95e7d4d4451fa4f3443c8c252fb81e24
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:43:55 -0500

    n string_compress

--- public/src/string_compress.mjs
+++ public/src/string_compress.mjs
@@ -1,1 +1,5 @@
-export function string_compress() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function string_compress() {
+  marker("1");
+  return LZString.compressToUTF16(text);
+}
