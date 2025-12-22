793dfad70a739f1810583c745056fcd616a794c1
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 18:44:47 -0500

    function_auto_path ..\love\public\src\object_property_change.mjs

--- public/src/object_property_change.mjs
+++ public/src/object_property_change.mjs
@@ -2,7 +2,7 @@ import { object_property_set } from "../../../love/public/src/object_property_se
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function object_property_change(o, property, lambda$value) {
   let value = object_property_get(o, property);
-  value = change(value);
+  value = lambda$value(value);
   object_property_set(o, property, value);
   return value;
 }
