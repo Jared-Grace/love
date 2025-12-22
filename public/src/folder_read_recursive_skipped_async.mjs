892b311c2e030e0022f3e17845d7275f39f04706
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:24:26 -0500

    p

--- public/src/folder_read_recursive_skipped_async.mjs
+++ public/src/folder_read_recursive_skipped_async.mjs
@@ -1,8 +1,10 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { folder_read_recursive_async } from "../../../love/public/src/folder_read_recursive_async.mjs";
 import { list_includes_not } from "../../../love/public/src/list_includes_not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { error } from "console";
 export async function folder_read_recursive_skipped_async(
   path_folder,
   folders_skipped,
@@ -25,6 +27,7 @@ export async function folder_read_recursive_skipped_async(
           log({
             name,
           });
+          error();
         }
         const subFiles = await folder_read_recursive_async(fullPath);
         function lambda(f) {
