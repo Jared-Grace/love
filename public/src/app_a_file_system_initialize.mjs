d9f7585e11fe2569786e2efbcd7262a3ab590a5e
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:33:24 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize.mjs

--- public/src/app_a_file_system_initialize.mjs
+++ public/src/app_a_file_system_initialize.mjs
@@ -1,6 +1,7 @@
+import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_file_system_initialize() {
-  let value = storage_local_exists(app_fn, key);
+  let value = storage_local_exists(app_a, app_a_file_system_initialize.nam);
   marker("1");
 }
