f0f3916a8ac725c26256c31ef7d4ea04d51839d8
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:58:51 -0500

    function_auto_path ..\love\public\src\file_read.mjs

--- web/public/src/file_read.mjs
+++ web/public/src/file_read.mjs
@@ -1,17 +1,19 @@
+import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
+import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
 import { indexeddb_read } from "../../../love/public/src/indexeddb_read.mjs";
 import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
-import { error } from "../../../love/public/src/error.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read(file_path) {
   if (browser_is()) {
     await app_a_file_system_initialize();
-    let item = await indexeddb_read(db_get, store, key);
-    error();
-    return;
-    let r = object_property_get(file_system, file_path);
-    return r;
+    let store = app_a_file_system_store();
+    let item = await indexeddb_read(
+      app_a_indexeddb_initialize,
+      store,
+      file_path,
+    );
+    return item;
   }
   marker("1");
   let fs = await import("fs");
