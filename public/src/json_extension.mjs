5d9116a1bb864773008b64bcaa3055c95c011a94
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 22:15:46 -0500

    function_auto_path ..\love\public\src\json_extension.mjs

--- public/src/json_extension.mjs
+++ public/src/json_extension.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function json_extension() {
   marker("1");
-  let v = ".json";
-  return v;
+  let ext_j = ".json";
+  return ext_j;
 }
