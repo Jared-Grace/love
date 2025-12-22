f9ce4ddb166099fc76f69ece34c0b9e97f2cd565
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:51:26 -0500

    function_auto_path ..\love\public\src\file_read.mjs

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,3 +1,4 @@
+import { indexeddb_read } from "../../../love/public/src/indexeddb_read.mjs";
 import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -6,6 +7,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read(file_path) {
   if (browser_is()) {
     await app_a_file_system_initialize();
+    let item = await indexeddb_read(db_get, store, key);
     error();
     return;
     let r = object_property_get(file_system, file_path);
