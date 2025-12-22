4877470917870a2114a340e3fc7fa738b75b0e32
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:18:57 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_skipped_paths_async.mjs

--- web/public/src/folder_read_recursive_skipped_paths_async.mjs
+++ web/public/src/folder_read_recursive_skipped_paths_async.mjs
@@ -1,11 +1,15 @@
+import { folder_read_recursive_paths_to } from "../../../love/public/src/folder_read_recursive_paths_to.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { folder_read_recursive_skipped_async } from "../../../love/public/src/folder_read_recursive_skipped_async.mjs";
-export async function folder_read_recursive_skipped_paths_async(path_folder) {
+export async function folder_read_recursive_skipped_paths_async(
+  path_folder,
+  folders_skipped,
+) {
   marker("1");
-  let folders_skipped = [];
   let result = await folder_read_recursive_skipped_async(
     path_folder,
     folders_skipped,
   );
-  return result;
+  let mapped = folder_read_recursive_paths_to(result, path_folder);
+  return mapped;
 }
