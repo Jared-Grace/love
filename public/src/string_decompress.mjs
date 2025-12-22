7b0b5ac2c9777fb8c9ffdd151053533544137bf8
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:45:14 -0500

    function_auto_path ..\love\public\src\string_decompress.mjs

--- public/src/string_decompress.mjs
+++ public/src/string_decompress.mjs
@@ -1,7 +1,5 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import LZString from "lz-string";
 export function string_decompress() {
-  marker("1");
-  let v = LZString.compressToUTF16(text);
+  let v = LZString.decompressFromUTF16(compressed);
   return v;
 }
