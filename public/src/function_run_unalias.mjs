cb30deab74d856a639597a7bcb47de8aba7dda92
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 17:10:07 -0500

    data_file_update ..\love\public\src\function_run_unalias.mjs

--- public/src/function_run_unalias.mjs
+++ public/src/function_run_unalias.mjs
@@ -3,11 +3,11 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 export async function function_run_unalias(f_name, args) {
   marker("1");
-  const fn = await function_import_unalias(f_name);
-  const result = await fn(...args);
-  return result;
   log({
     f_name,
     args,
   });
+  const fn = await function_import_unalias(f_name);
+  const result = await fn(...args);
+  return result;
 }
