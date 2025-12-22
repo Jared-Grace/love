7f2a1b988c2872f05ae57f70ffac982fdb66b215
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 17:11:29 -0500

    function_auto_path ..\love\public\src\function_import.mjs

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,5 +1,4 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
@@ -7,9 +6,6 @@ export async function function_import(f_name) {
   let v = await function_name_unalias(f_name);
   let unaliased = object_property_get(v, "unaliased");
   let v2 = await function_name_to_path_search(unaliased);
-  log({
-    unaliased,
-  });
   let f = object_property_get(v2, "f_path");
   let f_path = await path_resolve(f);
   const imported = await import(`file://${f_path}`);
