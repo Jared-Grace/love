bd4ca9ceb4fee1e3a970a1b675f162b27fded671
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:44:41 -0500

    data_file_update ..\love\public\src\string_decompress.mjs

--- public/src/string_decompress.mjs
+++ public/src/string_decompress.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import LZString from "lz-string";
 export function string_decompress() {
+  marker("1");
   let v = LZString.compressToUTF16(text);
   return v;
 }
