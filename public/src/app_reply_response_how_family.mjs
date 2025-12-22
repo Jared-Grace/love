978375f623f0d43dc4552eb8aedf024dc7d46c63
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 12:46:52 -0500

    function_auto_path ..\love\public\src\app_reply_response_how_family.mjs

--- public/src/app_reply_response_how_family.mjs
+++ public/src/app_reply_response_how_family.mjs
@@ -2,6 +2,8 @@ import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 export function app_reply_response_how_family() {
   let how_family =
     emoji_pray() +
-    "My family are those who do the will of God! - And stretching out his hand toward his disciples, he said, “Here are my mother and my brothers! For whoever does the will of my Father in heaven is my brother and sister and mother.” Matthew 12:49-50";
+    "My family are those who do the will of God! The family of God needs prayer " +
+    emoji_pray() +
+    " - And stretching out his hand toward his disciples, he said, “Here are my mother and my brothers! For whoever does the will of my Father in heaven is my brother and sister and mother.” Matthew 12:49-50";
   return how_family;
 }
