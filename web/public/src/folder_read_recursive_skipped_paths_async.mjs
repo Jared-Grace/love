3281a5b26bc500b31d3d597f0754b24708266a58
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:25:39 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_skipped_paths_async.mjs

--- web/public/src/folder_read_recursive_skipped_paths_async.mjs
+++ web/public/src/folder_read_recursive_skipped_paths_async.mjs
@@ -1,11 +1,13 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { folder_read_recursive_paths_to } from "../../../love/public/src/folder_read_recursive_paths_to.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { folder_read_recursive_skipped_async } from "../../../love/public/src/folder_read_recursive_skipped_async.mjs";
 export async function folder_read_recursive_skipped_paths_async(
   path_folder,
   folders_skipped,
 ) {
-  marker("1");
+  log({
+    folders_skipped,
+  });
   let result = await folder_read_recursive_skipped_async(
     path_folder,
     folders_skipped,
