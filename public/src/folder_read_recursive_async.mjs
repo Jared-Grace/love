1692947b31ee0bddf0969addd1aa5012364991f5
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:15:59 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_async.mjs

--- public/src/folder_read_recursive_async.mjs
+++ public/src/folder_read_recursive_async.mjs
@@ -1,4 +1,6 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export async function folder_read_recursive_async(path_folder) {
   let folders_skipped = [];
   const fs = await import("fs/promises");
@@ -13,7 +15,10 @@ export async function folder_read_recursive_async(path_folder) {
     if (entry.isFile()) {
       result.push(name);
     } else if (entry.isDirectory()) {
-      let includes = list_includes_not(folders_skipped, name);
+      let b = list_includes(folders_skipped, name);
+      let includes = not(b);
+      if (false) {
+      }
       const subFiles = await folder_read_recursive_async(fullPath);
       function lambda(f) {
         let v = path.join(name, f);
