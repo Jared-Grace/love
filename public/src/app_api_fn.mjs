e3ee46f52b7852511a540a4fec8c1b457b2de7f9
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:41:24 -0500

    function_auto_path ..\love\public\src\app_api_fn.mjs

--- public/src/app_api_fn.mjs
+++ public/src/app_api_fn.mjs
@@ -3,6 +3,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_api_fn(fn, args) {
   marker("1");
   let f_name = fn.name;
-  let v = await app_api(f_name, args);
-  return v;
+  let r = await app_api(f_name, args);
+  return r;
 }
