2719b822a60fb50cdf0a172b5bbcd81cbfb0946a
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:56:17 -0500

    function_auto_path ..\love\public\src\file_exists.mjs

--- public/src/file_exists.mjs
+++ public/src/file_exists.mjs
@@ -1,4 +1,7 @@
-import { app_api_cache_storage_local_fn } from "../../../love/public/src/app_api_cache_storage_local_fn.mjs";
+import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
+import { indexeddb_read } from "../../../love/public/src/indexeddb_read.mjs";
+import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
+import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { throws_not_async } from "../../../love/public/src/throws_not_async.mjs";
@@ -6,16 +9,23 @@ import { error } from "../../../love/public/src/error.mjs";
 import { promise_is } from "../../../love/public/src/promise_is.mjs";
 export async function file_exists(file_path) {
   if (browser_is()) {
-    let r = await app_api_cache_storage_local_fn(file_exists, arguments);
-    return r;
+    await app_a_file_system_initialize();
+    let store = app_a_file_system_store();
+    let item = await indexeddb_read(
+      app_a_indexeddb_initialize,
+      store,
+      file_path,
+    );
+    return item;
   }
   marker("1");
   if (promise_is(file_path)) {
     error();
   }
   let fs = await import("fs");
-  let { access } = fs.promises;
-  let { constants } = fs;
+  let v = fs.promises;
+  let access = object_property_get(v, "access");
+  let constants = object_property_get(fs, "constants");
   let exists = await throws_not_async(lambda);
   async function lambda() {
     await access(file_path, constants.F_OK);
