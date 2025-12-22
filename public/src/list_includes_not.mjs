16a129adf17345f2385282102b7c008c6acbcf62
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:16:12 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_async.mjs

---
+++ public/src/list_includes_not.mjs
@@ -0,0 +1,7 @@
+import { not } from "../../../love/public/src/not.mjs";
+import { list_includes } from "../../../love/public/src/list_includes.mjs";
+export function list_includes_not(folders_skipped, name) {
+  let b = list_includes(folders_skipped, name);
+  let includes = not(b);
+  return includes;
+}
