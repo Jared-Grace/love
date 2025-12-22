e15a3cb7c3cc620fc6c726fe6535ae65ae5da201
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 22:14:46 -0500

    function_auto_path ..\love\public\src\file_name_json.mjs

--- web/public/src/file_name_json.mjs
+++ web/public/src/file_name_json.mjs
@@ -1,4 +1,6 @@
+import { json_extension } from "../../../love/public/src/json_extension.mjs";
 export function file_name_json(name) {
-  let file_name = name + json_extension()+'';
+  let file_name = name + ".json";
   return file_name;
+  json_extension();
 }
