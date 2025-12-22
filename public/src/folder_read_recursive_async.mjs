4fec9f3e0304bc0de705f6cb8e23c5a19b3b1ccf
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:14:16 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_async.mjs

--- public/src/folder_read_recursive_async.mjs
+++ public/src/folder_read_recursive_async.mjs
@@ -11,7 +11,7 @@ export async function folder_read_recursive_async(path_folder) {
     if (entry.isFile()) {
       result.push(entry.name);
     } else if (entry.isDirectory()) {
-      log(message);
+      log(entry.name);
       const subFiles = await folder_read_recursive_async(fullPath);
       function lambda(f) {
         let v = path.join(entry.name, f);
