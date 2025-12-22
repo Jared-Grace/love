4e48d4a6dc01b5bee70f63c766cc7eb16a60d67f
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:59:28 -0500

    function_auto_path ..\love\public\src\file_exists.mjs

--- web/public/src/indexeddb_exists.mjs
+++ web/public/src/indexeddb_exists.mjs
@@ -1,4 +1,16 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function indexeddb_exists() {
+export async function indexeddb_exists(db_get, store, key) {
   marker("1");
+
+  const db = await db_get();
+  const tx = db.transaction(store, "readonly");
+  const s = tx.objectStore(store);
+
+  const exists = await new Promise((resolve, reject) => {
+    const req = s.count(key);
+    req.onsuccess = () => resolve(req.result > 0);
+    req.onerror = () => reject(req.error);
+  });
+
+  return exists;
 }
