c76e4bd167ef2b7b6917ac7892603c7709631433
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 18:37:23 -0500

    function_auto_path ..\love\public\src\file_read.mjs

--- public/src/file_read.mjs
+++ public/src/file_read.mjs
@@ -1,3 +1,5 @@
+import { string_decompress } from "../../../love/public/src/string_decompress.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_path_normalize } from "../../../love/public/src/file_path_normalize.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
 import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
@@ -15,6 +17,8 @@ export async function file_read(file_path) {
       store,
       file_path,
     );
+    let compressed2 = object_property_get(item, "compressed");
+    let text = string_decompress(compressed);
     return item;
   }
   marker("1");
