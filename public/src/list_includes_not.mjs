ebf79b8ec89da1704a7f70c9becbd535cc3cdcdb
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:26:36 -0500

    function_auto_path ..\love\public\src\list_includes_not.mjs

--- public/src/list_includes_not.mjs
+++ public/src/list_includes_not.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-export function list_includes_not(folders_skipped, name) {
-  let b = list_includes(folders_skipped, name);
+export function list_includes_not(list, name) {
+  let b = list_includes(list, name);
   let n = not(b);
   return n;
 }
