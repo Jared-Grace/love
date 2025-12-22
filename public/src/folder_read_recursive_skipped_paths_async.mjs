774cdac2aca7cd17a1a69e45837832287e80ae3a
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:27:38 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_skipped_paths_async.mjs

--- public/src/folder_read_recursive_skipped_paths_async.mjs
+++ public/src/folder_read_recursive_skipped_paths_async.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { folder_read_recursive_paths_to } from "../../../love/public/src/folder_read_recursive_paths_to.mjs";
 import { folder_read_recursive_skipped_async } from "../../../love/public/src/folder_read_recursive_skipped_async.mjs";
 export async function folder_read_recursive_skipped_paths_async(
