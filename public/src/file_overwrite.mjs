77551f11c3a4733e7024b3bbd32118b4f844fd94
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 18:41:26 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,15 +1,30 @@
-import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
-import { file_read } from "../../../love/public/src/file_read.mjs";
-import { app_api_cache_clear } from "../../../love/public/src/app_api_cache_clear.mjs";
+import { list_last } from "../../../love/public/src/list_last.mjs";
+import { json_decompress } from "../../../love/public/src/json_decompress.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
+import { indexeddb_read } from "../../../love/public/src/indexeddb_read.mjs";
+import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
+import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
+import { file_path_normalize } from "../../../love/public/src/file_path_normalize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_overwrite(file_path, contents) {
   marker("1");
   if (browser_is()) {
-    let r = await app_api_fn(file_overwrite, arguments);
-    app_api_cache_clear(file_read, [file_path]);
-    return r;
+    file_path = file_path_normalize(file_path);
+    await app_a_file_system_initialize();
+    let store = app_a_file_system_store();
+    let item = await indexeddb_read(
+      app_a_indexeddb_initialize,
+      store,
+      file_path,
+    );
+    let compressed = object_property_get(item, "compressed");
+    let f = json_decompress(compressed);
+    let versions = object_property_get(f, "versions");
+    let last = list_last(versions);
+    return last;
   }
   await file_parent_exists_ensure(file_path);
   let fs = await import("fs");
