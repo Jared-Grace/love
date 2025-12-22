46f8e0715e99a2dcadb6a5faafed263c518b2479
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 22:00:22 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize.mjs

--- web/public/src/app_a_file_system_initialize.mjs
+++ web/public/src/app_a_file_system_initialize.mjs
@@ -1,3 +1,4 @@
+import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -26,6 +27,7 @@ export async function app_a_file_system_initialize() {
       let value3 = await indexeddb_put(db_get, store, path, value_get);
     }
     await each_async(r, lambda);
+    storage_local_set(app_a, app_a_file_system_initialize.name, true);
     let v = true;
     return v;
   }
