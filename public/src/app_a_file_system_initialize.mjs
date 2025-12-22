faa381b3344bb389850a6726196933aee8175265
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:46:07 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize.mjs

--- public/src/app_a_file_system_initialize.mjs
+++ public/src/app_a_file_system_initialize.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
 import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
@@ -13,10 +14,11 @@ export async function app_a_file_system_initialize() {
     return;
   }
   async function lambda2() {
-    let r = await app_api_fn(app_a_download, []);
     let db = await app_a_indexeddb_initialize();
-    let value_get2 = lambda_get(value4);
-    let value3 = await indexeddb_put(db_get, store, key, value_get);
+    let value_get2 = lambda_get(db);
+    let r = await app_api_fn(app_a_download, []);
+    await each_async(list, async function lambda(item) {});
+    let value3 = await indexeddb_put(value_get2, "files", key, value_get);
   }
   let value2 = await global_function_initialize_lambda_async(
     app_a_file_system_initialize,
