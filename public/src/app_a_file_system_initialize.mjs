09ad61865cd3e9f33fae285f0f0e5982ad9db1fd
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:33:58 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize.mjs

--- public/src/app_a_file_system_initialize.mjs
+++ public/src/app_a_file_system_initialize.mjs
@@ -1,10 +1,15 @@
+import { global_function_initialize_lambda_async } from "../../../love/public/src/global_function_initialize_lambda_async.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_file_system_initialize() {
+export async function app_a_file_system_initialize() {
   let value = storage_local_exists(app_a, app_a_file_system_initialize.name);
   if (value) {
     return;
   }
+  let value2 = await global_function_initialize_lambda_async(
+    fn,
+    async function lambda2() {},
+  );
   marker("1");
 }
