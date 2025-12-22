49e91ba2739dcb3c075ab842bf69ba37dff05e40
Grace-Jared <christrosetolife@gmail.com>
2025-12-21 21:40:48 -0500

    function_auto_path ..\love\public\src\app_a_download.mjs

--- public/src/app_a_download.mjs
+++ public/src/app_a_download.mjs
@@ -5,10 +5,10 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_download() {
   marker("1");
   let filtered = await app_a_download_paths();
-  async function lambda2(file_path) {
-    let contents = await file_read(file_path);
+  async function lambda2(path) {
+    let contents = await file_read(path);
     let v = {
-      path: file_path,
+      path,
       versions: [contents],
     };
     return v;
