be0d74bd0f88853a28338a94b2ee9df603b85972
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 16:15:40 -0500

    function_auto_path ..\love\public\src\folder_read_recursive_async.mjs

--- public/src/folder_read_recursive_async.mjs
+++ public/src/folder_read_recursive_async.mjs
@@ -1,6 +1,4 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 export async function folder_read_recursive_async(path_folder) {
   let folders_skipped = [];
   const fs = await import("fs/promises");
@@ -10,16 +8,15 @@ export async function folder_read_recursive_async(path_folder) {
     withFileTypes: true,
   });
   for (const entry of entries) {
-    let name2 = object_property_get(entry, "name");
-    const fullPath = path.join(path_folder, entry.name);
+    let name = object_property_get(entry, "name");
+    const fullPath = path.join(path_folder, name);
     if (entry.isFile()) {
-      let includes = list_includes(folders_skipped, entry.name);
-      result.push(entry.name);
+      result.push(name);
     } else if (entry.isDirectory()) {
-      log(entry.name);
+      let includes = list_includes_not(folders_skipped, name);
       const subFiles = await folder_read_recursive_async(fullPath);
       function lambda(f) {
-        let v = path.join(entry.name, f);
+        let v = path.join(name, f);
         return v;
       }
       result.push(...subFiles.map(lambda));
