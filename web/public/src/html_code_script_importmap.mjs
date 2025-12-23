c09544fe8331d060c046e9040e0d10cbc62ed937
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:32:08 -0500

    function_auto_path ..\love\public\src\html_code_script_importmap.mjs

---
+++ web/public/src/html_code_script_importmap.mjs
@@ -0,0 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { html_code_script_module_generic } from "../../../love/public/src/html_code_script_module_generic.mjs";
+export function html_code_script_importmap(middle) {
+  marker("1");
+  const script_type = "module";
+  let code = html_code_script_module_generic(script_type, middle);
+  return code;
+}
