d64354451cd9fe5936291ddad5b07a2df18f5917
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:10:11 -0500

    function_auto_path ..\love\public\src\indexeddb_get_all.mjs

--- public/src/indexeddb_get_all.mjs
+++ public/src/indexeddb_get_all.mjs
@@ -1,12 +1,18 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { error } from "../../../love/public/src/error.mjs";
 export async function indexeddb_get_all(db_get, store) {
   const db = await db_get();
   const tx = db.transaction(store, "readonly");
   const s = tx.objectStore(store);
-
-  return new Promise((resolve, reject) => {
+  let v3 = new Promise(function lambda3(resolve, reject) {
     const req = s.getAll();
-    req.onsuccess = () => resolve(req.result);
-    req.onerror = () => reject(req.error);
+    req.onsuccess = function lambda() {
+      let v = resolve(req.result);
+      return v;
+    };
+    req.onerror = function lambda2() {
+      let v2 = reject(req.error);
+      return v2;
+    };
   });
+  return v3;
 }
