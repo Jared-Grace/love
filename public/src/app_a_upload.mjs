bfe022eead67a0f6e1941a443fb1565b18f49162
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:47:08 -0500

    function_auto_path ..\love\public\src\app_a_upload.mjs

--- public/src/app_a_upload.mjs
+++ public/src/app_a_upload.mjs
@@ -1,4 +1,6 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_upload(deltas) {
+  each(list, function lambda(item) {});
   marker("1");
 }
