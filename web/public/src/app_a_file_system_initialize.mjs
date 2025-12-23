391ce368dbff801911d1da6eb99956e5be782063
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 22:04:01 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize.mjs

--- web/public/src/app_a_file_system_initialize.mjs
+++ web/public/src/app_a_file_system_initialize.mjs
@@ -1,12 +1,4 @@
-import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
-import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
-import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
-import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
-import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
+import { app_a_file_system_initialize_download } from "../../../love/public/src/app_a_file_system_initialize_download.mjs";
 import { global_function_initialize_lambda_async } from "../../../love/public/src/global_function_initialize_lambda_async.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
@@ -17,17 +9,7 @@ export async function app_a_file_system_initialize() {
     return;
   }
   async function lambda2() {
-    let db = await app_a_indexeddb_initialize();
-    let db_get = lambda_get(db);
-    let r = await app_api_fn(app_a_download, []);
-    async function lambda(item) {
-      let value_get = lambda_get(item);
-      let key = object_property_get(item, "key");
-      let store = app_a_file_system_store();
-      let value3 = await indexeddb_put(db_get, store, key, value_get);
-    }
-    await each_async(r, lambda);
-    storage_local_set(app_a, app_a_file_system_initialize.name, true);
+    await app_a_file_system_initialize_download();
     let v = true;
     return v;
   }
---
+++ web/public/src/app_a_file_system_initialize_download.mjs
@@ -0,0 +1,24 @@
+import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
+import { app_a } from "../../../love/public/src/app_a.mjs";
+import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
+import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
+import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
+import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
+import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
+export async function app_a_file_system_initialize_download() {
+  let db = await app_a_indexeddb_initialize();
+  let db_get = lambda_get(db);
+  let r = await app_api_fn(app_a_download, []);
+  async function lambda(item) {
+    let value_get = lambda_get(item);
+    let key = object_property_get(item, "key");
+    let store = app_a_file_system_store();
+    let value3 = await indexeddb_put(db_get, store, key, value_get);
+  }
+  await each_async(r, lambda);
+  storage_local_set(app_a, app_a_file_system_initialize.name, true);
+}
