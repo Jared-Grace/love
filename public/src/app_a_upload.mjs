c211f9f4b946022efb93f9352da834a61d19a9ca
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:53:48 -0500

    function_auto_path ..\love\public\src\app_a_upload.mjs

--- public/src/app_a_upload.mjs
+++ public/src/app_a_upload.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -5,8 +6,9 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_upload(deltas) {
   async function lambda(d) {
     let key = object_property_get(d, "key");
-    let contents = await file_read(file_path);
+    let contents = await file_read(key);
     let versions = object_property_get(d, "versions");
+    let first = list_first(list);
   }
   await each_async(deltas, lambda);
   marker("1");
