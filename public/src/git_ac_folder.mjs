829d942445c4611b5d332249bae60c0ea49a28f0
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 22:29:34 -0500

    function_auto_path ..\love\public\src\git_ac_folder.mjs

--- public/src/git_ac_folder.mjs
+++ public/src/git_ac_folder.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { git_commit_folder } from "../../../love/public/src/git_commit_folder.mjs";
 import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
 export async function git_ac_folder(folder, message) {
+  marker("1");
   await command_line_git_folder(folder, "add -A");
   await git_commit_folder(folder, message);
 }
