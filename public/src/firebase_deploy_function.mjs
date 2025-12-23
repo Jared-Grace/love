81f4d93735e774ac8611311b2c1c677352980f28
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:01:44 -0500

    function_auto_path ..\love\public\src\firebase_deploy_function.mjs

--- public/src/firebase_deploy_function.mjs
+++ public/src/firebase_deploy_function.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { firebase_deploy_function_destination_latest } from "../../../love/public/src/firebase_deploy_function_destination_latest.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -7,8 +8,10 @@ import { date_now_file } from "../../../love/public/src/date_now_file.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function firebase_deploy_function(f_name) {
   marker("1");
-  let { unaliased } = await function_name_unalias(f_name);
-  let code = await function_dependencies_code(unaliased);
+  let v = await function_name_unalias(f_name);
+  let unaliased = object_property_get(v, "unaliased");
+  let v2 = await function_dependencies_code(unaliased);
+  let code = object_property_get(v2, "code");
   let now_file = date_now_file();
   let destination = firebase_deploy_function_destination_json(
     now_file,
