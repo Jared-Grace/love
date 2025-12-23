aadd80d092c2351aae57abd2d3327e057d67b717
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:29:40 -0500

    function_auto_path ..\love\public\src\html_code_script_module.mjs

--- public/src/html_code_script_module.mjs
+++ public/src/html_code_script_module.mjs
@@ -1,7 +1,6 @@
+import { html_code_script_module_generic } from "../../../love/public/src/html_code_script_module_generic.mjs";
 export function html_code_script_module(middle) {
   const script_type = "module";
-  let code = `<script type="${script_type}"> 
-    ${middle}
-  </script>`;
+  let code = html_code_script_module_generic(script_type, middle);
   return code;
 }
