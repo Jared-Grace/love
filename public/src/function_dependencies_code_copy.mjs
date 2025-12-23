d5c72e30dc0495eca2c273cd3cdea0a355f9f389
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:02:05 -0500

    function_auto_path ..\love\public\src\function_dependencies_code.mjs

--- public/src/function_dependencies_code_copy.mjs
+++ public/src/function_dependencies_code_copy.mjs
@@ -1,8 +1,10 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function function_dependencies_code_copy(f_name) {
   marker("1");
-  let {code} = await function_dependencies_code(f_name);
+  let v = await function_dependencies_code(f_name);
+  let code = object_property_get(v, "code");
   await clipboard_copy(code);
 }
