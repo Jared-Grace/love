a124c3d1505df473dc17d50a123ee327a7a0684f
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 15:46:21 -0500

    function_auto_path ..\love\public\src\function_run_log.mjs

--- public/src/function_run_log.mjs
+++ public/src/function_run_log.mjs
@@ -1,10 +1,12 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { json_to } from "./json_to.mjs";
 export async function function_run_log(f_name, args) {
-  let { unaliased } = await function_name_unalias(f_name);
-  log_keep("🏃" +" " + unaliased + " " + json_to(args));
+  let v = await function_name_unalias(f_name);
+  let unaliased = object_property_get(v, "unaliased");
+  log_keep("🏃" + " " + unaliased + " " + json_to(args));
   let result = await function_run_unalias(f_name, args);
   log_keep("🔥");
   log_keep(result);
