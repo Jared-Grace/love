ba8fa6b6d89b15fcc32755a9c723bdac5aff9262
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:09:14 -0500

    data_file_update ..\love\public\src\function_parse_declaration.mjs

--- web/public/src/function_parse_declaration.mjs
+++ web/public/src/function_parse_declaration.mjs
@@ -1,7 +1,9 @@
+import { function_parse_imports_packages } from "../../../love/public/src/function_parse_imports_packages.mjs";
 import { function_parse_declaration_inner } from "../../../love/public/src/function_parse_declaration_inner.mjs";
 import { function_parse } from "../../../love/public/src/function_parse.mjs";
 export async function function_parse_declaration(f_name) {
   let parsed = await function_parse(f_name);
-  let to = function_parse_declaration_inner(parsed);
+  let to2 = function_parse_declaration_inner(parsed);
+  let to = function_parse_imports_packages(to2);
   return to;
 }
