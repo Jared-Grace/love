25ff8153f7b6f1601425bd6ed295c3f0ae8b2a1a
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:14:25 -0500

    function_auto_path ..\love\public\src\function_dependencies_code.mjs

--- public/src/function_dependencies_code.mjs
+++ public/src/function_dependencies_code.mjs
@@ -1,5 +1,4 @@
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
-import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { list_wait } from "../../../love/public/src/list_wait.mjs";
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { function_dependencies } from "../../../love/public/src/function_dependencies.mjs";
@@ -12,7 +11,7 @@ export async function function_dependencies_code(f_name) {
   let waited = await list_wait(mapped);
   let mapped2 = list_map_property(waited, "declaration");
   let externals = list_map_property(waited, "externals");
-  let waited2 = await list_map_unordered_async(mapped2, js_unparse);
+  let waited2 = list_map(mapped2, js_unparse);
   let code = list_join_newline(waited2);
   let v = {
     code,
