3a130a481e6066eb152fb8e229faef587b9632e1
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:33:56 -0500

    function_auto_path ..\love\public\src\html_update_public_generic.mjs

--- public/src/html_update_public_generic.mjs
+++ public/src/html_update_public_generic.mjs
@@ -7,9 +7,9 @@ import { function_name_repo_path_combine } from "../../../love/public/src/functi
 export async function html_update_public_generic(f_name, file_path, name) {
   let joined = await function_name_repo_path_combine(f_name, file_path);
   const v = await function_dependencies_code_call(f_name);
+  let externals = object_property_get(v, "externals");
   let code = object_property_get(v, "code");
   let body = html_code_script_module(code);
-  let externals = object_property_get(v, "externals");
   await html_overwrite(name, joined, body);
   await file_open(joined);
 }
