4d782c3f34cd9a3ecaa39527663f871e3852f43f
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:20:01 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -58,7 +58,7 @@ export async function app_a_function(context) {
       let versions = object_property_get(f, "versions");
       object_property_set_exists_not(item, "versions", versions);
       let m = list_multiple_is(versions);
-      object_property_set_exists_not(item, "changed", versions);
+      object_property_set_exists_not(item, "changed", m);
     }
     each(list, lambda);
     function lambda2(item2) {}
