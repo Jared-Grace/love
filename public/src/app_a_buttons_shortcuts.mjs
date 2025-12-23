3ec392236efd5a24f92a580e4b360569b6a84306
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 09:58:18 -0500

    function_auto_path ..\love\public\src\app_a_buttons_shortcuts.mjs

--- public/src/app_a_buttons_shortcuts.mjs
+++ public/src/app_a_buttons_shortcuts.mjs
@@ -1,9 +1,9 @@
 import { app_a_button_shortcut } from "../../../love/public/src/app_a_button_shortcut.mjs";
 import { app_a_shortcuts_each } from "../../../love/public/src/app_a_shortcuts_each.mjs";
-export function app_a_buttons_shortcuts(choices, overlay) {
+export function app_a_buttons_shortcuts(choices, parent) {
   app_a_shortcuts_each(choices, on_choice);
   function on_choice(shortcut, text, fn) {
-    let b3 = app_a_button_shortcut(overlay, shortcut, text, fn);
+    let b3 = app_a_button_shortcut(parent, shortcut, text, fn);
     return b3;
   }
 }
