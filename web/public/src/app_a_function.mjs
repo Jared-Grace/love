a7332fd4c3d36e5756a350f72677c677316fb71d
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:06:44 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_function.mjs
+++ web/public/src/app_a_function.mjs
@@ -41,7 +41,8 @@ export async function app_a_function(context) {
   let text = emoji_search();
   let b = app_a_button(root, text, search);
   let a = emoji_add();
-  let b2 = app_a_button(root, a, () => {});
+  function lambda() {}
+  let b2 = app_a_button(root, a, lambda);
   let div = html_div(root);
   html_font_jetbrains_mono(div);
   html_style_assign(div, {
