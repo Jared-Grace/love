fcab3922a8e4c5ec868da6c9b3604b86f29a3d58
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 20:15:10 -0500

    pd result

--- public/src/function_run_git.mjs
+++ public/src/function_run_git.mjs
@@ -3,6 +3,6 @@ import { function_run_log } from "../../../love/public/src/function_run_log.mjs"
 Error.stackTraceLimit = Infinity;
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
-  let v = await git_acp_call_repos(result, f_name, args);
+  let v = await git_acp_call_repos(f_name, args);
   return v;
 }
