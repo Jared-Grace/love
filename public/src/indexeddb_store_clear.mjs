048e46b5e89fd0d16d5c7b118677d4fddb2632be
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 22:05:33 -0500

    function_auto_path ..\love\public\src\indexeddb_store_clear.mjs

--- public/src/indexeddb_store_clear.mjs
+++ public/src/indexeddb_store_clear.mjs
@@ -1,4 +1,17 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function indexeddb_store_clear() {
-  marker("1");
+import { error } from "../../../love/public/src/error.mjs";
+export async function indexeddb_store_clear(db_get, store) {
+  const db = await db_get();
+  const tx = db.transaction(store, "readwrite");
+  const s = tx.objectStore(store);
+  await new Promise(function lambda3(resolve, reject) {
+    const req = s.clear();
+    req.onsuccess = function lambda() {
+      let v = resolve();
+      return v;
+    };
+    req.onerror = function lambda2() {
+      let v2 = reject(req.error);
+      return v2;
+    };
+  });
 }
