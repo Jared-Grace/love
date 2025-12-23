aec0a2772799879e8026c53bdb74a4a98df53a43
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:33:34 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -64,7 +64,8 @@ export async function app_a_function(context) {
     }
     each(all, lambda);
     let filtered = list_filter_property(all, "changed", true);
-    let mapped = list_map(list, function lambda2(item2) {});
+    function lambda2(item2) {}
+    let mapped = list_map(list, lambda2);
     log({
       filtered,
     });
