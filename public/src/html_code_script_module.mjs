e232be4af8bf0fb2e1c9379da7e38358bd3fb5cb
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:29:27 -0500

    function_auto_path ..\love\public\src\html_code_script_module.mjs

--- public/src/html_code_script_module.mjs
+++ public/src/html_code_script_module.mjs
@@ -1,7 +1,7 @@
 export function html_code_script_module(middle) {
   const script_type = "module";
-  let v = `<script type="${script_type}"> 
+  let code = `<script type="${script_type}"> 
     ${middle}
   </script>`;
-  return v;
+  return code;
 }
