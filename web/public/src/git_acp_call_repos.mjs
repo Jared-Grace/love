ce3d9c79ef1c498e6d6272fe83bd91ca372634ba
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 20:15:10 -0500

    pd result

--- web/public/src/git_acp_call_repos.mjs
+++ web/public/src/git_acp_call_repos.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { git_acp_call_folder } from "../../../love/public/src/git_acp_call_folder.mjs";
 import { repos_paths_map_unordered } from "../../../love/public/src/repos_paths_map_unordered.mjs";
-export async function git_acp_call_repos(result, f_name, args) {
+export async function git_acp_call_repos(f_name, args) {
   marker("1");
   await repos_paths_map_unordered(each_folder);
   return result;
