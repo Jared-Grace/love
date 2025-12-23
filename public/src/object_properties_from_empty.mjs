391f1e9740bc1cc0aa3e7ba37104393d2dff5862
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:55:14 -0500

    fs object_properties_from_empty

--- public/src/object_properties_from_empty.mjs
+++ public/src/object_properties_from_empty.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { object_properties_from } from "../../../love/public/src/object_properties_from.mjs";
 export function object_properties_from_empty(properties, item2) {
+  marker("1");
   let to = object_properties_from({}, properties, item2);
   return to;
 }
