622d3209852267fc4ddce6f69ddf745b3935fbce
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:54:06 -0500

    function_auto_path ..\love\public\src\app_a_upload.mjs

--- public/src/app_a_upload.mjs
+++ public/src/app_a_upload.mjs
@@ -1,3 +1,4 @@
+import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -9,7 +10,7 @@ export async function app_a_upload(deltas) {
     let contents = await file_read(key);
     let versions = object_property_get(d, "versions");
     let first = list_first(versions);
-    eqa;
+    equal_assert(left, right);
   }
   await each_async(deltas, lambda);
   marker("1");
