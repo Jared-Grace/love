f24eb403f45dec17a8f2d67c1f40d3d4f10b4bf5
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:54:17 -0500

    function_auto_path ..\love\public\src\function_param_index.mjs

--- public/src/function_param_index.mjs
+++ public/src/function_param_index.mjs
@@ -1,12 +1,13 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { js_declaration_params_ast_get } from "../../../love/public/src/js_declaration_params_ast_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
-import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
-import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export function function_param_index(ast, param_name) {
   marker("1");
-  let declaration = js_declaration_single(ast);
-  let params = js_declaration_params_get(declaration);
+  let v2 = js_declaration_params_ast_get(ast);
+  let params = object_property_get(v2, "params");
+  let declaration = object_property_get(v2, "declaration");
   let p = js_declaration_param_named(declaration, param_name);
   let index = list_index_of(params, p);
   let v = {
