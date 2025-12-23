2955e94341c3488085f5adab3e01098fe4e0fec6
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:09:39 -0500

    data_file_update ..\love\public\src\function_parse_imports_packages.mjs

--- public/src/function_parse_imports_packages.mjs
+++ public/src/function_parse_imports_packages.mjs
@@ -1,9 +1,9 @@
+import { js_imports_declarations_externals } from "../../../love/public/src/js_imports_declarations_externals.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export function function_parse_imports_packages(parsed) {
   let ast = object_property_get(parsed, "ast");
-  let declaration = js_declaration_single(ast);
+  let declaration = js_imports_declarations_externals(ast);
   let to = object_merge(
     {
       declaration,
