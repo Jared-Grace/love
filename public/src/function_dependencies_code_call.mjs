c34cc4d6b52a348014863ac8d6e6fcac36079f2d
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:02:02 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,9 +1,11 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
 import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function function_dependencies_code_call(f_name) {
   let global_init = js_code_global_init();
-  let {code} = await function_dependencies_code(f_name);
+  let v = await function_dependencies_code(f_name);
+  let code = object_property_get(v, "code");
   let call = js_code_call_statement(f_name);
   const middle = `${global_init}
     ${code}
