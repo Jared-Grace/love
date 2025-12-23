b141c1b22817b591a94ace41d1db33cf402c3cfc
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:19:49 -0500

    function_auto_path ..\love\public\src\list_multiple_is.mjs

--- public/src/list_multiple_is.mjs
+++ public/src/list_multiple_is.mjs
@@ -1,5 +1,5 @@
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_multiple_is(list) {
-  let v = list_size(list) >= 2;
-  return v;
+  let m = list_size(list) >= 2;
+  return m;
 }
