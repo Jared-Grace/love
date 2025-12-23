c5684549a53d59273c73fca2b94135daee8bc3a7
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:54:48 -0500

    function_auto_path ..\love\public\src\function_param_swap_beginning.mjs

--- public/src/function_param_swap_beginning.mjs
+++ public/src/function_param_swap_beginning.mjs
@@ -1,22 +1,22 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { js_declaration_params_ast_get } from "../../../love/public/src/js_declaration_params_ast_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
-import { function_param_index } from "../../../love/public/src/function_param_index.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 export async function function_param_swap_beginning(
   param_name_a,
   param_name_b,
 ) {
   marker("1");
-  let index_a = null;
-  let index_b = null;
+  let index_a = 0;
+  let index_b = 1;
   await function_params_new_generic(function_transform_current_lambda, on_call);
   function on_call(args) {
     list_swap_at(args, index_a, index_b);
   }
   function function_transform_current_lambda(ast) {
-    let params = null;
-    ({ params, index: index_a } = function_param_index(ast, param_name_a));
-    ({ index: index_b } = function_param_index(ast, param_name_b));
+    let v2 = js_declaration_params_ast_get(ast);
+    let params = object_property_get(v2, "params");
     list_swap_at(params, index_a, index_b);
   }
 }
