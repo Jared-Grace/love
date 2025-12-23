97a8e9f2fdc61a9bb9bb1f4418699100f81c9481
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 20:15:14 -0500

    function_auto_path

--- web/public/src/git_acp_call_repos.mjs
+++ web/public/src/git_acp_call_repos.mjs
@@ -4,7 +4,6 @@ import { repos_paths_map_unordered } from "../../../love/public/src/repos_paths_
 export async function git_acp_call_repos(f_name, args) {
   marker("1");
   await repos_paths_map_unordered(each_folder);
-  return result;
   async function each_folder(folder) {
     await git_acp_call_folder(f_name, args, folder);
   }
