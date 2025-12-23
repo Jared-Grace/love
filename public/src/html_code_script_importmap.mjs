780fef357399c220769120aac626bb253a931c0f
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:32:13 -0500

    data_file_update ..\love\public\src\html_code_script_importmap.mjs

--- public/src/html_code_script_importmap.mjs
+++ public/src/html_code_script_importmap.mjs
@@ -2,7 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { html_code_script_module_generic } from "../../../love/public/src/html_code_script_module_generic.mjs";
 export function html_code_script_importmap(middle) {
   marker("1");
-  const script_type = "module";
+  const script_type = "importmap";
   let code = html_code_script_module_generic(script_type, middle);
   return code;
 }
