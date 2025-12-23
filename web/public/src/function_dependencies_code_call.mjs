0f86d626f1c5778f31b882ea0c433d9a042c9d0d
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:01:52 -0500

    data_file_update ..\love\public\src\firebase_deploy_function.mjs

--- web/public/src/function_dependencies_code_call.mjs
+++ web/public/src/function_dependencies_code_call.mjs
@@ -3,7 +3,7 @@ import { js_code_call_statement } from "../../../love/public/src/js_code_call_st
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function function_dependencies_code_call(f_name) {
   let global_init = js_code_global_init();
-  let code = await function_dependencies_code(f_name);
+  let {code} = await function_dependencies_code(f_name);
   let call = js_code_call_statement(f_name);
   const middle = `${global_init}
     ${code}
