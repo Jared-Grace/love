3b35c414920e66c9e87a6fe4c8e2337d59daa344
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:02:00 -0500

    data_file_update ..\love\public\src\function_dependencies_code.mjs

--- web/public/src/function_dependencies_code_copy.mjs
+++ web/public/src/function_dependencies_code_copy.mjs
@@ -3,6 +3,6 @@ import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function function_dependencies_code_copy(f_name) {
   marker("1");
-  let code = await function_dependencies_code(f_name);
+  let {code} = await function_dependencies_code(f_name);
   await clipboard_copy(code);
 }
