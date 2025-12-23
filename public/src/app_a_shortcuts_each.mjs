e3db3dca3d40d4d2aaceacdd7505b9d6757a1081
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 09:36:14 -0500

    fpsw on_choice choices

--- public/src/app_a_shortcuts_each.mjs
+++ public/src/app_a_shortcuts_each.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function app_a_shortcuts_each(on_choice, choices) {
+export function app_a_shortcuts_each(choices, on_choice) {
   marker("1");
   function lambda21(c) {
     let shortcut = object_property_get(c, "shortcut");
