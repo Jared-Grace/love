73985a7c0783c827941270333a79968f9f8a6f64
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:54:17 -0500

    function_auto_path ..\love\public\src\function_param_index.mjs

---
+++ web/public/src/js_declaration_params_ast_get.mjs
@@ -0,0 +1,11 @@
+import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
+import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
+export function js_declaration_params_ast_get(ast) {
+  let declaration = js_declaration_single(ast);
+  let params = js_declaration_params_get(declaration);
+  let v2 = {
+    declaration,
+    params,
+  };
+  return v2;
+}
