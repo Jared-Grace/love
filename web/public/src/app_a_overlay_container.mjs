76bdb980ec80ece48e03781a976743f5ffe37905
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 15:58:07 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_overlay_container.mjs
+++ web/public/src/app_a_overlay_container.mjs
@@ -2,8 +2,8 @@ import { html_style_background_color } from "../../../love/public/src/html_style
 import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_a_overlay_container(overlay) {
-  let div3 = html_div(overlay);
-  app_a_control_style(div3);
-  html_style_background_color(div3, "white");
-  return div3;
+  let div = html_div(overlay);
+  app_a_control_style(div);
+  html_style_background_color(div, "white");
+  return div;
 }
