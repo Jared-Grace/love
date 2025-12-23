9d542ec6750189254ee5a9d3037be46d78055552
Grace-Jared <christrosetolife@gmail.com>
2025-12-22 19:04:21 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_function.mjs
+++ web/public/src/app_a_function.mjs
@@ -39,6 +39,7 @@ export async function app_a_function(context) {
   }
   let text = emoji_search();
   let b = app_a_button(root, text, search);
+  aab;
   let div = html_div(root);
   html_font_jetbrains_mono(div);
   html_style_assign(div, {
