71ad8b872caf6af5d5451dcd4f8f7de51bfddf4a
Grace-Jared <christrosetolife@gmail.com>
2025-12-23 18:55:33 -0500

    psb

--- public/src/object_properties_from_empty.mjs
+++ public/src/object_properties_from_empty.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_properties_from } from "../../../love/public/src/object_properties_from.mjs";
-export function object_properties_from_empty(properties, item2) {
+export function object_properties_from_empty(item2, properties) {
   marker("1");
   let to = object_properties_from({}, properties, item2);
   return to;
