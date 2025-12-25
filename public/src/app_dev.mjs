4217e12119cce97a4cb5fb07f7bc4b2a81b74e9f
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:18:47 -0500

    rrl app_dev context_initialize

--- public/src/app_dev.mjs
+++
@@ -1,8 +0,0 @@
-import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-export function app_dev(fn) {
-  const root = html_document_body();
-  let context = {
-    root,
-  };
-  fn(context);
-}
