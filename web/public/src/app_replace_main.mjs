a1f478adaf9a159c2c1a088fa6d9de75a11b9c63
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:22:13 -0500

    function_auto_path ..\love\public\src\app_replace_main.mjs

--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -1,19 +1,17 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_font_size_refresh } from "../../../love/public/src/app_replace_font_size_refresh.mjs";
 import { html_meta_viewport } from "../../../love/public/src/html_meta_viewport.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
-import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { app_replace_screens } from "../../../love/public/src/app_replace_screens.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
-export async function app_replace_main() {
+export async function app_replace_main(context) {
   let app_fn = app_replace;
   let screens = app_replace_screens();
   html_meta_viewport();
-  const root = html_document_body();
-  const context = {
+  object_merge(context, {
     app_fn,
     screens,
-    root: root,
-  };
+  });
   app_generic_refresh(context);
   app_replace_font_size_refresh(context);
 }
