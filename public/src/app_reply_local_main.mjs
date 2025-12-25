7709e8925f9f1f808dab64df4fac3a75a4af69bd
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:22:59 -0500

    function_auto_path ..\love\public\src\app_reply_local_main.mjs

--- public/src/app_reply_local_main.mjs
+++ public/src/app_reply_local_main.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { ebible_verse_merge } from "../../../love/public/src/ebible_verse_merge.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
@@ -5,15 +6,17 @@ import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { ebible_languages_chapters } from "../../../love/public/src/ebible_languages_chapters.mjs";
 import { app_api } from "../../../love/public/src/app_api.mjs";
-export async function app_reply_local_main() {
+export async function app_reply_local_main(context) {
   let result = await app_api(ebible_languages_chapters.name, []);
   function verse_get(bible_folder, chapter_code, verse_number) {
-    let { chapters } = list_find_property(result, "bible_folder", bible_folder);
-    let { verses } = list_find_property(chapters, "chapter_code", chapter_code);
+    let v2 = list_find_property(result, "bible_folder", bible_folder);
+    let chapters = object_property_get(v2, "chapters");
+    let v3 = list_find_property(chapters, "chapter_code", chapter_code);
+    let verses = object_property_get(v3, "verses");
     let v = list_find_property(verses, "verse_number", verse_number);
     let verse = ebible_verse_merge(bible_folder, chapter_code, v);
     return verse;
   }
   global_function_set(ebible_verse, verse_get);
-  await app_reply_main();
+  await app_reply_main(context);
 }
