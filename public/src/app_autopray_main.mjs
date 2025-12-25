788a2a77f80afadb8754c8f442256ba6bbc1e621
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:11:55 -0500

    function_auto_path ..\love\public\src\app_autopray_main.mjs

--- public/src/app_autopray_main.mjs
+++ public/src/app_autopray_main.mjs
@@ -3,14 +3,12 @@ import { string_may_the_lord } from "../../../love/public/src/string_may_the_lor
 import { prayer_end } from "../../../love/public/src/prayer_end.mjs";
 import { prayer_start } from "../../../love/public/src/prayer_start.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
-import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
 import { kjv } from "../../../love/public/src/kjv.mjs";
 import { sleep } from "./sleep.mjs";
 export async function app_autopray_main(context) {
-  let root2 = object_property_get(context, "root");
-  let root = html_document_body();
+  let root = object_property_get(context, "root");
   let v = kjv();
   async function lambda2(verse_text, verse_reference) {
     html_clear(root);
