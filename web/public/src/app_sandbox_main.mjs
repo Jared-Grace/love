167269a06f1fe679dcb31ec065130e16bb185abf
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:28:32 -0500

    function_auto_path ..\love\public\src\app_sandbox_main.mjs

--- web/public/src/app_sandbox_main.mjs
+++ web/public/src/app_sandbox_main.mjs
@@ -1,20 +1,20 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 import { app_message } from "../../../love/public/src/app_message.mjs";
 import { app_message_provide_travel } from "../../../love/public/src/app_message_provide_travel.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { html_document_body } from "./html_document_body.mjs";
 import { app_message_provide_food } from "./app_message_provide_food.mjs";
-export function app_sandbox_main() {
-  let root = html_document_body();
+export function app_sandbox_main(context) {
+  let root = object_property_get(context, "root");
   marker("1");
   let app_fn = app_message;
-  let context = {
+  object_merge(context, {
     app_fn,
-    root,
     screens: {
       provide_travel: app_message_provide_travel,
       provide_food: app_message_provide_food,
     },
-  };
+  });
   app_generic_refresh(context);
 }
