604340e088ea85d08770bece33e2fcccf3f92ffc
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:05:22 -0500

    data_file_update ..\love\public\src\html_update_dev.mjs

--- web/public/src/html_update_dev.mjs
+++ web/public/src/html_update_dev.mjs
@@ -1,4 +1,3 @@
-import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { function_code_import_dev } from "../../../love/public/src/function_code_import_dev.mjs";
 import { app_dev } from "../../../love/public/src/app_dev.mjs";
 import { js_code_call_args_statement } from "../../../love/public/src/js_code_call_args_statement.mjs";
@@ -22,7 +21,7 @@ export async function html_update_dev(name) {
   let call = js_code_call_args_statement(app_dev.name, [name_prefixed]);
   let code = function_code_import_dev(name_prefixed);
   let code2 = function_code_import_dev(app_dev.name);
-  let joined3 = list_join_space(list);
+ljn
   let body = html_code_script_module(middle);
   var v = await html_update_externals(name_prefixed);
   let scripts = object_property_get(v, "scripts");
