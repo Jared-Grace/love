b9fdc9cafbc7e3c07d2ce4ab42068e037c893346
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 16:59:54 -0500

    function_auto_path ..\love\public\src\js_code_call_args_statement.mjs

--- public/src/js_code_call_args_statement.mjs
+++ public/src/js_code_call_args_statement.mjs
@@ -1,9 +1,9 @@
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { js_code_statement } from "../../../love/public/src/js_code_statement.mjs";
-export function js_code_call_args_statement(f_name) {
+export function js_code_call_args_statement(f_name, args) {
   marker("1");
-  let code = js_code_call_args(f_name);
+  let code = js_code_call_args(f_name, args);
   let result = js_code_statement(code);
   return result;
 }
