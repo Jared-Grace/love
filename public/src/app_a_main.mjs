7796e7c0dbc42969f5b5b71a65f0939956a8e64c
Grace-Jared <christrosetolife@gmail.com>
2025-12-25 17:07:02 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -1,21 +1,20 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { html_on_keydown } from "../../../love/public/src/html_on_keydown.mjs";
 import { html_font_sans_serif_set_html } from "../../../love/public/src/html_font_sans_serif_set_html.mjs";
 import { invoke_multiple_arg } from "../../../love/public/src/invoke_multiple_arg.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { app_a_screens } from "../../../love/public/src/app_a_screens.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
-import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-export async function app_a_main() {
+export async function app_a_main(context2) {
   let app_fn = app_a;
-  const root = html_document_body();
   let screens = app_a_screens();
   let on_keydowns = [];
   const context = {
     app_fn,
     screens,
-    root,
     on_keydowns,
   };
+  let to2 = object_merge(to, from);
   function lambda(e) {
     invoke_multiple_arg(on_keydowns, e);
   }
