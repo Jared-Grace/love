c211fbf4762565fdd55eaf3b63189c45047940b0
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 18:38:21 -0500

    function_auto_path ..\love\public\src\app_a_home.mjs

--- public/src/app_a_home.mjs
+++ public/src/app_a_home.mjs
@@ -1,3 +1,4 @@
+import { function_name_extension } from "../../../love/public/src/function_name_extension.mjs";
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
 import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
 import { function_path_to_name } from "../../../love/public/src/function_path_to_name.mjs";
@@ -40,9 +41,10 @@ export async function app_a_home(context) {
   let all = await indexeddb_get_all(app_a_indexeddb_initialize, store);
   let property_name = app_a_indexeddb_path_key();
   let mapped = list_map_property(all, property_name);
-  let filtered3 = list_filter_ends_with(filtered2, f_name_suffix_after);
+  let f_name_suffix_after = function_name_extension();
+  let filtered3 = list_filter_ends_with(mapped, f_name_suffix_after);
   log(mapped);
-  let f_names = list_map(mapped, function_path_to_name);
+  let f_names = list_map(filtered3, function_path_to_name);
   let body = html_document_body();
   let input = app_a_input(body);
   let f_names_div = html_div(body);
