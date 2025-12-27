4e56ec7c509e498ecbc968ce278093d237c49c42
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 17:53:55 -0500

    function_auto_path ..\love\public\src\ebible_references_parse_folder_user_docs.mjs

--- web/public/src/ebible_references_parse_folder_user_docs.mjs
+++ web/public/src/ebible_references_parse_folder_user_docs.mjs
@@ -1,3 +1,4 @@
+import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
 import { ebible_references_parse } from "../../../love/public/src/ebible_references_parse.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
@@ -9,5 +10,6 @@ export async function ebible_references_parse_folder_user_docs(
   let file_path = folder_user_docs_path(file_name);
   let bible_folders = [second_language, v];
   let list = await ebible_references_parse(bible_folders, file_path);
+  await clipboard_copy(list);
   return list;
 }
