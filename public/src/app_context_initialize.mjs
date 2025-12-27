1ecff5cbb807a69be483cdb647a6404e2d0c346b
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 18:06:44 -0500

    function_auto_path ..\love\public\src\app_context_initialize.mjs

--- public/src/app_context_initialize.mjs
+++ public/src/app_context_initialize.mjs
@@ -1,10 +1,8 @@
+import { app_context_initialize_root } from "../../../love/public/src/app_context_initialize_root.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 export function app_context_initialize(fn) {
   marker("1");
   const root = html_document_body();
-  let context = {
-    root,
-  };
-  fn(context);
+  app_context_initialize_root(root, fn);
 }
