1845d646aae2c0224cc50390db511581d4c9b54e
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 18:58:02 -0500

    function_auto_path ..\love\public\src\ebible_references_parse_pray.mjs

--- public/src/ebible_references_parse_pray.mjs
+++ public/src/ebible_references_parse_pray.mjs
@@ -1,10 +1,10 @@
-import { ebible_folder_swahili } from "../../../love/public/src/ebible_folder_swahili.mjs";
+import { ebible_folder_urdu } from "../../../love/public/src/ebible_folder_urdu.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_references_parse_folder_user_docs } from "../../../love/public/src/ebible_references_parse_folder_user_docs.mjs";
 export async function ebible_references_parse_pray() {
   marker("1");
   let file_name = "pray.txt";
-  let v2 = ebible_folder_swahili();
+  let v2 = ebible_folder_urdu();
   let list = await ebible_references_parse_folder_user_docs(file_name, v2);
   return list;
 }
