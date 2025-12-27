ee9aa06396a153f85298858054606db47aa91083
Grace-Jared <christrosetolife@gmail.com>
2025-12-26 19:03:01 -0500

    function_auto_path ..\love\public\src\app_component.mjs

--- web/public/src/app_component.mjs
+++ web/public/src/app_component.mjs
@@ -1,4 +1,6 @@
+import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 export async function app_component(a_name, root) {
+  let combined = app_name_main(a);
   let context = {
     root,
   };
