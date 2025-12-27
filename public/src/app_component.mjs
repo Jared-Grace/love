f2ff1a05eac32abf4f063f38a2b99eb986674a8e
Grace-Jared <christrosetolife@gmail.com>
2025-12-26 19:09:56 -0500

    function_auto_path ..\love\public\src\app_component.mjs

--- public/src/app_component.mjs
+++ public/src/app_component.mjs
@@ -1,8 +1,9 @@
+import { function_dependencies_code_unaliased } from "../../../love/public/src/function_dependencies_code_unaliased.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 export async function app_component(a_name, root) {
-  let v2 = await function_dependencies_code(unaliased);
+  let v = await function_dependencies_code_unaliased(a_name);
+  let d = object_property_get(v, "d");
   let code = object_property_get(v2, "code");
   let combined = app_name_main(a);
   let context = {
