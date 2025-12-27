90cadef000ae2251d80e4267ede13a5c49e58a2f
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 18:31:25 -0500

    function_auto_path ..\love\public\src\app_a_home.mjs

--- public/src/app_a_home.mjs
+++ public/src/app_a_home.mjs
@@ -1,3 +1,4 @@
+import { app_a_indexeddb_path_key } from "../../../love/public/src/app_a_indexeddb_path_key.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
 import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
@@ -36,7 +37,8 @@ export async function app_a_home(context) {
   await app_a_file_system_initialize_download();
   let store = app_a_file_system_store();
   let all = await indexeddb_get_all(app_a_indexeddb_initialize, store);
-  let mapped = list_map_property(list, app_a_indexeddb_path_key());
+  let property_name = app_a_indexeddb_path_key();
+  let mapped = list_map_property(list, property_name);
   let body = html_document_body();
   let input = app_a_input(body);
   let f_names_div = html_div(body);
