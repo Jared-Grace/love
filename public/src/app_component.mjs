ddd6d169726d5760b89793a516ec58bbec3bbfff
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 18:07:42 -0500

    function_auto_path ..\love\public\src\app_component.mjs

--- public/src/app_component.mjs
+++ public/src/app_component.mjs
@@ -1,3 +1,4 @@
+import { newline } from "../../../love/public/src/newline.mjs";
 import { app_context_initialize_root } from "../../../love/public/src/app_context_initialize_root.mjs";
 import { function_dependencies_code_unaliased } from "../../../love/public/src/function_dependencies_code_unaliased.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -7,5 +8,6 @@ export async function app_component(a_name, root) {
   let v = await function_dependencies_code_unaliased(combined);
   let d = object_property_get(v, "d");
   let code = object_property_get(v2, "code");
+  code += newline() + a_name;
   await app_context_initialize_root(root, fn);
 }
