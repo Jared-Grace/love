4d5777c6851e6967ba1522e31828bbc77b5c4499
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 17:51:10 -0500

    function_auto_path ..\love\public\src\ebible_references_parse_lines.mjs

--- public/src/ebible_references_parse_lines.mjs
+++ public/src/ebible_references_parse_lines.mjs
@@ -1,3 +1,4 @@
+import { ebible_parts_to_reference } from "../../../love/public/src/ebible_parts_to_reference.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { ebible_references_names } from "../../../love/public/src/ebible_references_names.mjs";
 import { ebible_reference_parts } from "../../../love/public/src/ebible_reference_parts.mjs";
@@ -37,6 +38,12 @@ export async function ebible_references_parse_lines(bible_folders, lines) {
             bible_folder,
             chapter_code,
             verse_number,
+          );
+          let reference = ebible_parts_to_reference(
+            books2,
+            book_code,
+            verse_numbers,
+            chapter_name,
           );
           let to2 = object_merge(result, {
             from,
