5f1f2d976b7bc1b13273ef7f9708cec2ab802127
Grace-Jared <christrosetolife@gmail.com>
2025-12-26 19:07:35 -0500

    function_auto_path ..\love\public\src\firebase_deploy_function.mjs

--- public/src/firebase_deploy_function.mjs
+++ public/src/firebase_deploy_function.mjs
@@ -1,16 +1,15 @@
+import { function_dependencies_code_unaliased } from "../../../love/public/src/function_dependencies_code_unaliased.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { firebase_deploy_function_destination_latest } from "../../../love/public/src/firebase_deploy_function_destination_latest.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { firebase_deploy_function_destination_json } from "../../../love/public/src/firebase_deploy_function_destination_json.mjs";
 import { date_now_file } from "../../../love/public/src/date_now_file.mjs";
-import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function firebase_deploy_function(f_name) {
   marker("1");
-  let v = await function_name_unalias(f_name);
+  let v = await function_dependencies_code_unaliased(f_name);
   let unaliased = object_property_get(v, "unaliased");
-  let d = await function_dependencies_code(unaliased);
+  let d = object_property_get(v, "d");
   let code = object_property_get(d, "code");
   let now_file = date_now_file();
   let destination = firebase_deploy_function_destination_json(
