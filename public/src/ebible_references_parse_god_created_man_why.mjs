60927580f5905cf0e3ae3d5b6c31366c0345b753
Grace-Jared <christrosetolife@gmail.com>
2025-12-27 17:42:54 -0500

    function_auto_path ..\love\public\src\ebible_references_parse_god_created_man_why.mjs

--- public/src/ebible_references_parse_god_created_man_why.mjs
+++ public/src/ebible_references_parse_god_created_man_why.mjs
@@ -4,8 +4,8 @@ import { ebible_folder_english } from "../../../love/public/src/ebible_folder_en
 import { ebible_folder_urdu } from "../../../love/public/src/ebible_folder_urdu.mjs";
 export async function ebible_references_parse_god_created_man_why() {
   let v = ebible_folder_english();
-  let v2 = ebible_folder_urdu();
   let file_name = "god_created_man_why.txt";
+  let v2 = ebible_folder_urdu();
   let file_path = folder_user_docs_path(file_name);
   let bible_folders = [v2, v];
   let list = await ebible_references_parse(bible_folders, file_path);
