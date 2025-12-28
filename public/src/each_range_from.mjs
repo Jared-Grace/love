d3cbfb1f0ef14f0b0091da1377076e1ecadda0fe
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 16:55:27 -0500

    function_auto_path ..\love\public\src\each_range_from.mjs

--- public/src/each_range_from.mjs
+++ public/src/each_range_from.mjs
@@ -1,7 +1,7 @@
 import { range_from } from "../../../love/public/src/range_from.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function each_range_from(from, to, lambda) {
+export function each_range_from(from, to, lambda$item) {
   marker("1");
   let list = range_from(from, to);
   each(list, lambda);
