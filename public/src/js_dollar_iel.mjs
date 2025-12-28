25cb7837702518c21ab04523c404dd90b285e7d2
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 15:08:44 -0500

    function_auto_path ..\love\public\src\js_dollar_iel.mjs

--- public/src/js_dollar_iel.mjs
+++ public/src/js_dollar_iel.mjs
@@ -1,6 +1,5 @@
 import { js_statement_block_new } from "../../../love/public/src/js_statement_block_new.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { js_dollar_i } from "../../../love/public/src/js_dollar_i.mjs";
 export function js_dollar_iel({
   remaining,
@@ -14,8 +13,10 @@ export function js_dollar_iel({
   js_dollar_i({
     stack1,
   });
-  let b = js_statement_block_new(bs_body);
-  object_property_set(stack1, 'alternate', b);
+  let b = js_statement_block_new(null);
+  object_property_set(stack1, "alternate", b);
   return;
-  $iel
+  if (false) {
+  } else {
+  }
 }
