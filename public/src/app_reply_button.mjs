96cc88cd34eb40bda56ce609f1f780de7bb6593f
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 17:12:59 -0500

    pd component

--- public/src/app_reply_button.mjs
+++ public/src/app_reply_button.mjs
@@ -3,13 +3,7 @@ import { html_button } from "../../../love/public/src/html_button.mjs";
 import { html_style_background_color_set_or_remove } from "../../../love/public/src/html_style_background_color_set_or_remove.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { list_toggle } from "../../../love/public/src/list_toggle.mjs";
-export function app_reply_button(
-  languages_chosen,
-  language,
-  component,
-  root,
-  name,
-) {
+export function app_reply_button(languages_chosen, language, root, name) {
   marker("1");
   function lambda3() {
     list_toggle(languages_chosen, language);
