90e92d5aa0869704863409a2336a7f05e8a66e27
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 16:54:27 -0500

    function_auto_path ..\love\public\src\reply_2.mjs

--- public/src/reply_2.mjs
+++ public/src/reply_2.mjs
@@ -45,5 +45,6 @@ export async function reply_2(context) {
   }
   each(languages, lambda);
   html_p_text(root, "2. How many Bible passages do you want?");
-  each_range(count, function lambda2(i) {});
+  function lambda2(i) {}
+  each_range(count, lambda2);
 }
