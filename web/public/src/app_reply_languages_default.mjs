26811b4693d592d7551b54526a56a640b5b86301
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 17:20:54 -0500

    function_auto_path ..\love\public\src\app_reply_languages_default.mjs

--- web/public/src/app_reply_languages_default.mjs
+++ web/public/src/app_reply_languages_default.mjs
@@ -3,6 +3,6 @@ import { ebible_language_en_name } from "../../../love/public/src/ebible_languag
 export function app_reply_languages_default() {
   let v2 = ebible_language_en_name();
   let v = ebible_language_original_name();
-  let v4 = [v2, v];
-  return v4;
+  let ds = [v2, v];
+  return ds;
 }
