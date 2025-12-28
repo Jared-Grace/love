9b313089e13a35d527d18149efacaf64b58f530f
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 14:49:32 -0500

    function_auto_path ..\love\public\src\html_click.mjs

--- public/src/html_click.mjs
+++ public/src/html_click.mjs
@@ -1,5 +1,10 @@
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_click(c) {
   let element = html_component_element_get(c);
-  element.click();
+  element.dispatchEvent(
+    new PointerEvent("pointerdown", {
+      bubbles: true,
+      cancelable: true,
+    }),
+  );
 }
