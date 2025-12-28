adef3652e8b61d5f76a7f23c9a9c36458a335f3f
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 14:47:05 -0500

    function_auto_path ..\love\public\src\html_click.mjs

--- public/src/html_click.mjs
+++ public/src/html_click.mjs
@@ -1,4 +1,5 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function html_click() {
-  marker("1");
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+export function html_click(c) {
+  let element = html_component_element_get(c);
+  element.click();
 }
