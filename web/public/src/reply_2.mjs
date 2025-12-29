ae1bf7f3fa1451cd605234b14bad077bd0acb92b
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 19:41:55 -0500

    function_auto_path ..\love\public\src\reply_2.mjs

--- web/public/src/reply_2.mjs
+++ web/public/src/reply_2.mjs
@@ -1,4 +1,4 @@
-import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { list_take } from "../../../love/public/src/list_take.mjs";
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { app_reply_button } from "../../../love/public/src/app_reply_button.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -19,8 +19,7 @@ export async function reply_2(context) {
   let encouragement = object_property_get(r, "encouragement");
   let filtered = list_filter_property(list, property_name, property_value);
   let languages_chosen = app_reply_languages_default();
-  function lambda3(item2) {}
-  let filtered2 = list_filter(list2, lambda3);
+  let taken = list_take(list2, count);
   let p = html_p_text(
     root,
     "1. Choose the language or languages you want the Bible verses to be translated into",
