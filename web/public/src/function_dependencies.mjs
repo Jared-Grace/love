832acbd12fd2652f957a9d52dac9284392208a61
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:25:22 -0500

    function_auto_path ..\love\public\src\function_dependencies.mjs

--- web/public/src/function_dependencies.mjs
+++ web/public/src/function_dependencies.mjs
@@ -9,13 +9,14 @@ export async function function_dependencies(f_names) {
   let split = string_split_comma(f_names);
   await function_exists_assert(f_names);
   async function lambda2(la) {
-    function lambda(v) {
-      let node = object_property_get(v, "node");
-      la(node);
+    async function lambda3(item) {
+      function lambda(v) {
+        let node = object_property_get(v, "node");
+        la(node);
+      }
+      await visit_unique_async(f_names, function_imports, lambda);
     }
-    async function lambda3(item) {}
-    await each_async(list, lambda3);
-    await visit_unique_async(f_names, function_imports, lambda);
+    await each_async(split, lambda3);
   }
   let v2 = await list_adder_unique_async(lambda2);
   return v2;
