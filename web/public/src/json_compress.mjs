020c5db5dc95c718db49b898000e8be3a9b7a82d
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:14:32 -0500

    function_auto_path ..\love\public\src\json_compress.mjs

--- web/public/src/json_compress.mjs
+++ web/public/src/json_compress.mjs
@@ -1,7 +1,7 @@
 import { string_compress } from "../../../love/public/src/string_compress.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
-export function json_compress(data) {
+export async function json_compress(data) {
   let json = json_to(data);
-  let compressed = string_compress(json);
+  let compressed = await string_compress(json);
   return compressed;
 }
