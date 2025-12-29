e86f4cb723d0a1627fe843ddae46fc5e415ed6a4
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:23:41 -0500

    function_auto_path ..\love\public\src\function_dependencies.mjs

--- web/public/src/function_dependencies.mjs
+++ web/public/src/function_dependencies.mjs
@@ -1,15 +1,16 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_exists_assert } from "../../../love/public/src/function_exists_assert.mjs";
 import { list_adder_unique_async } from "../../../love/public/src/list_adder_unique_async.mjs";
 import { visit_unique_async } from "../../../love/public/src/visit_unique_async.mjs";
 import { function_imports } from "../../../love/public/src/function_imports.mjs";
-export async function function_dependencies(f_name) {
-  await function_exists_assert(f_name);
+export async function function_dependencies(f_names) {
+  await function_exists_assert(f_names);
   async function lambda2(la) {
     function lambda(v) {
-      let { node } = v;
+      let node = object_property_get(v, "node");
       la(node);
     }
-    await visit_unique_async(f_name, function_imports, lambda);
+    await visit_unique_async(f_names, function_imports, lambda);
   }
   let v2 = await list_adder_unique_async(lambda2);
   return v2;
