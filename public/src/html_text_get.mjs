aab9870cee8fc003b6f86fb242172b7c9603df02
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 09:02:32 -0500

    data_file_update ..\love\public\src\html_text_get.mjs

--- public/src/html_text_get.mjs
+++ public/src/html_text_get.mjs
@@ -1,8 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_text_get(component, text) {
+export function html_text_get(component) {
   marker("1");
   let element = html_component_element_get(component);
-  let v = element.innerHTML;
-  return v;
+  return element.innerHTML;
 }
