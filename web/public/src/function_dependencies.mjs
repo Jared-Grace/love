87cf0c21c88d0f8247ba84939a64fb1c40c79ed5
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 15:02:34 -0500

    ps

--- web/public/src/function_dependencies.mjs
+++ web/public/src/function_dependencies.mjs
@@ -6,9 +6,10 @@ import { list_adder_unique_async } from "../../../love/public/src/list_adder_uni
 import { visit_unique_async } from "../../../love/public/src/visit_unique_async.mjs";
 import { function_imports } from "../../../love/public/src/function_imports.mjs";
 export async function function_dependencies(f_names) {
-  async function lambda4(item2) {}
-  await each_unordered_async(list, lambda4);
-  await function_exists_assert(f_names);
+  async function lambda4(f_name) {
+    await function_exists_assert(f_name);
+  }
+  await each_unordered_async(f_names, lambda4);
   async function lambda2(la) {
     async function lambda3(item) {
       function lambda(v) {
