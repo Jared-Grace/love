749b4dadf92e74c2f391840ae9c454fb4fb7b669
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:39:34 -0500

    data_file_update ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,4 @@
 import { object_property_change } from "../../../love/public/src/object_property_change.mjs";
-export async function sandbox() {
+export async function sandbox() {$x
   object_property_change(previous, p, lambda2);
 }
