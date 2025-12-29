afd5f8d0cef737203f60e393f99e46059b830709
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:07:15 -0500

    function_auto_path ..\love\public\src\html_element_parse.mjs

--- web/public/src/html_element_parse.mjs
+++ web/public/src/html_element_parse.mjs
@@ -2,11 +2,11 @@ import { html_component_element_get } from "../../../love/public/src/html_compon
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function html_element_parse(html_outer) {
+export function html_element_parse(parent, html_outer) {
   marker("1");
   let component = html_element(parent, "template");
   html_text_set(component, html_outer);
-  let element = html_component_element_get(component2);
-  const importMapScript = tpl.content.firstElementChild;
-  document.head.appendChild(importMapScript);
+  let element = html_component_element_get(component);
+  const s = element.content.firstElementChild;
+  parent.appendChild(s);
 }
