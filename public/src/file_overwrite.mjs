33a870819308aacc2174555a28aee7dd260691ca
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:15:16 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -1,3 +1,4 @@
+import { object_property_change_async } from "../../../love/public/src/object_property_change_async.mjs";
 import { json_compress } from "../../../love/public/src/json_compress.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
@@ -16,7 +17,7 @@ export async function file_overwrite(file_path, contents) {
     file_path = file_path_normalize(file_path);
     await app_a_file_system_initialize();
     let store = app_a_file_system_store();
-    function lambda(previous) {
+    async function lambda(previous) {
       async function lambda2(compressed_before) {
         let f = await json_decompress(compressed_before);
         let versions = object_property_get(f, "versions");
@@ -24,7 +25,7 @@ export async function file_overwrite(file_path, contents) {
         let compressed_after = await json_compress(f);
         return compressed_after;
       }
-      object_property_change_async(previous, "compressed", lambda2);
+      await object_property_change_async(previous, "compressed", lambda2);
       return previous;
     }
     let item = await indexeddb_put(
