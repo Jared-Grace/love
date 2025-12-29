a8a863f7754646761e98485f7708bd743f802c03
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:14:25 -0500

    function_auto_path ..\love\public\src\json_decompress.mjs

--- public/src/json_decompress.mjs
+++ public/src/json_decompress.mjs
@@ -1,7 +1,7 @@
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { string_decompress } from "../../../love/public/src/string_decompress.mjs";
-export function json_decompress(compressed) {
-  let json = string_decompress(compressed);
+export async function json_decompress(compressed) {
+  let json = await string_decompress(compressed);
   let text = json_from(json);
   return text;
 }
