7446053ccc9fdae920c666b16df747c831120b4d
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:48:27 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,4 @@
-import { object_property_change } from "../../../love/public/src/object_property_change.mjs";
-export async function sandbox() {$x
- a= object_property_get(previous, p, lambda2);
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export async function sandbox() {
+  let a = object_property_get(previous, p, lambda2);
 }
