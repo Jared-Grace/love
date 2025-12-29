a9d3ec9fa48c4b9e9ef9e89157b81b5d288fe7cf
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:16:25 -0500

    function_auto_path ..\love\public\src\firebase_storage_function_run.mjs

--- public/src/firebase_storage_function_run.mjs
+++ public/src/firebase_storage_function_run.mjs
@@ -10,13 +10,13 @@ import { js_code_call_statement } from "../../../love/public/src/js_code_call_st
 import { firebase_storage_download_property } from "../../../love/public/src/firebase_storage_download_property.mjs";
 export async function firebase_storage_function_run(f_name, version_get) {
   marker("1");
+  let call = js_code_call_statement(f_name);
   let destination_version = version_get(f_name);
   let destination = await firebase_storage_download_property(
     destination_version,
     "destination",
   );
   let code = await firebase_storage_download_property(destination, "code");
-  let call = js_code_call_statement(f_name);
   let global_init = js_code_global_init();
   let joined = list_join_newline([global_init, code, call]);
   if (browser_is()) {
