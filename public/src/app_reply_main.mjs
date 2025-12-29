f96a45dbf0b5f89fc0a00800fb59c40d6806d3b7
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 13:59:33 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -1,7 +1,4 @@
-import { app_reply_main_old } from "../../../love/public/src/app_reply_main_old.mjs";
 import { reply_2 } from "../../../love/public/src/reply_2.mjs";
 export async function app_reply_main(context) {
   await reply_2(context);
-  return;
-  await app_reply_main_old(context);
 }
