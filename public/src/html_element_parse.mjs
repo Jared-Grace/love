433a383dbe8584a2d6d8235faac4fd40cdae11ec
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:06:21 -0500

    function_auto_path ..\love\public\src\html_element_parse.mjs

--- public/src/html_element_parse.mjs
+++ public/src/html_element_parse.mjs
@@ -4,8 +4,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export function html_element_parse(html_outer) {
   marker("1");
   let component = html_element(parent, "template");
-  html_text_set(component2, text);
-  tpl.innerHTML = html_outer.trim();
+  html_text_set(component, html_outer);
   const importMapScript = tpl.content.firstElementChild;
   document.head.appendChild(importMapScript);
 }
