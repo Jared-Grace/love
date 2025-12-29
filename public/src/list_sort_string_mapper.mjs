7feab1efc6a17645983eb1ba6270c27d38c0527b
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 00:45:20 -0500

    function_auto_path ..\love\public\src\list_sort_string_mapper.mjs

--- public/src/list_sort_string_mapper.mjs
+++ public/src/list_sort_string_mapper.mjs
@@ -1,9 +1,11 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_sort_string_mapper(list) {
+export function list_sort_string_mapper(list, mapper) {
   marker("1");
   function lambda(a, b) {
-    const [na, ia] = a.split(/[_\.]/);
-    const [nb, ib] = b.split(/[_\.]/);
+    const va = lambda$item(a);
+    const vb = lambda$item(b);
+    const [na, ia] = va.split(/[_\.]/);
+    const [nb, ib] = vb.split(/[_\.]/);
     let v = na.localeCompare(nb) || Number(ia) - Number(ib);
     return v;
   }
