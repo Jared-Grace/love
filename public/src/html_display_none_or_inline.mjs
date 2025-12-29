8f235e5342b7a9b8b9be9f7c5f90f996d8e6c87d
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 09:29:40 -0500

    function_auto_path ..\love\public\src\html_display_none_or_inline.mjs

--- public/src/html_display_none_or_inline.mjs
+++ public/src/html_display_none_or_inline.mjs
@@ -1,11 +1,12 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { html_display_block } from "../../../love/public/src/html_display_block.mjs";
 import { html_display_none } from "../../../love/public/src/html_display_none.mjs";
 export function html_display_none_or_inline(hidden, item) {
   marker("1");
   if (hidden) {
     html_display_none(item);
   } else {
-    html_display_block(item);
+    marker("1");
+    html_style_set(item, "display", "block");
   }
 }
