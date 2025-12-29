e2fd3dc8a0aefd6dd3775308d8eb152fa1fbe77d
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 15:08:41 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_unaliased.mjs

--- public/src/function_dependencies_code_unaliased.mjs
+++ public/src/function_dependencies_code_unaliased.mjs
@@ -1,9 +1,9 @@
 import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
-import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
+import { function_dependencies_code_multiple } from "./function_dependencies_code_multiple.mjs";
 export async function function_dependencies_code_unaliased(f_names) {
   let split = string_split_comma(f_names);
   async function lambda(f_name) {
@@ -13,7 +13,7 @@ export async function function_dependencies_code_unaliased(f_names) {
   }
   let waited = await list_map_unordered_async(split, lambda);
   let first = list_first(waited);
-  let d = await function_dependencies_code(waited);
+  let d = await function_dependencies_code_multiple(waited);
   let v2 = {
     d,
     unaliased: first,
