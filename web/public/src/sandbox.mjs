0acc1bee2d0c923974096d8916b5370a8fb64dd7
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 17:48:38 -0500

    data_file_update ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,4 +1,4 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export async function sandbox() {
+export async function sandbox() {$x
   let a = object_property_get(previous, p, lambda2);
 }
