ffeee9ad808f03ce2950eedc3f2b198d94aed49e
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 00:53:24 -0500

    pb

--- web/public/src/reply_2.mjs
+++ web/public/src/reply_2.mjs
@@ -24,7 +24,7 @@ export async function reply_2(context) {
   let books = object_property_get(r, "books");
   let choices = object_property_get(r, "choices");
   let languages = object_property_get(r, "languages");
-  list_sort_string_property("name", languages);
+  list_sort_string_property(languages, "name");
   let root = object_property_get(r, "root");
   let original = object_property_get(r, "original");
   let en = object_property_get(r, "en");
