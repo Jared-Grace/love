ec0744d70e69dc6ceebb60c7708207c7516e4202
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 19:52:35 -0500

    function_auto_path ..\love\public\src\reply_2.mjs

--- public/src/reply_2.mjs
+++ public/src/reply_2.mjs
@@ -45,7 +45,7 @@ export async function reply_2(context) {
   each(choices_verse_count, lambda2);
   async function update(verse_count_new) {
     async function lambda6() {}
-    await each_range_from_async(from, to, lambda6);
+    await each_range_from_async(verse_count + 1, verse_count_new, lambda6);
     verse_count = c;
   }
 }
