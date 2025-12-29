45a3d9b5a4cb8b786f53962fdffd864860ce70a4
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 19:46:29 -0500

    function_auto_path ..\love\public\src\reply_2.mjs

--- web/public/src/reply_2.mjs
+++ web/public/src/reply_2.mjs
@@ -36,7 +36,7 @@ export async function reply_2(context) {
   list_add(choices_verse_count, 20);
   let verse_count_chosen = [10];
   function lambda2(c) {
-    let component = html_button(verse_count_chosen, c, root, c);
+    let component = html_button(verse_count_chosen, () => {});
   }
   each(choices_verse_count, lambda2);
 }
