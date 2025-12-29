fb50c5f6cc7590242574918a08217cc3986a8a7c
Grace-Jared <christrosetolife@gmail.com>
2025-12-28 19:40:51 -0500

    function_auto_path ..\love\public\src\app_reply_languages_default.mjs

--- public/src/app_reply_languages_default.mjs
+++ public/src/app_reply_languages_default.mjs
@@ -1,8 +1,8 @@
-import { ebible_language_original } from "../../../love/public/src/ebible_language_original.mjs";
-import { ebible_language_en } from "../../../love/public/src/ebible_language_en.mjs";
+import { ebible_language_original_name } from "../../../love/public/src/ebible_language_original_name.mjs";
+import { ebible_language_en_name } from "../../../love/public/src/ebible_language_en_name.mjs";
 export function app_reply_languages_default() {
-  let v2 = ebible_language_en();
-  let v = ebible_language_original();
+  let v2 = ebible_language_en_name();
+  let v = ebible_language_original_name();
   let ds = [v2, v];
   return ds;
 }
