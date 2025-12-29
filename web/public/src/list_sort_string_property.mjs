91618079708dc467126b2bdc83a1e84fc125a2e6
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 00:54:38 -0500

    function_auto_path ..\love\public\src\list_sort_string_property.mjs

--- web/public/src/list_sort_string_property.mjs
+++ web/public/src/list_sort_string_property.mjs
@@ -5,6 +5,7 @@ export function list_sort_string_property(languages, property_name) {
   marker("1");
   function lambda10(item) {
     let value = object_property_get(item, property_name);
+    return value;
   }
   list_sort_string_mapper(languages, lambda10);
 }
