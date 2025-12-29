2cd0fc817ddcd7dbcce6d302d9a2d14c06899655
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 09:14:04 -0500

    function_auto_path ..\love\public\src\reply_2.mjs

--- web/public/src/reply_2.mjs
+++ web/public/src/reply_2.mjs
@@ -57,6 +57,7 @@ export async function reply_2(context) {
       list_add(languages_chosen, language);
     }
     await app_reply_love(languages, lambda13);
+    update(4);
   }
   let component4 = html_button(root, "❤️", love);
   function lambda(language) {
