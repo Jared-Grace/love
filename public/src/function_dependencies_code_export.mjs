18ce993c1b9be50a2c132084dc1c31efa6b7a519
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 16:52:06 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_export.mjs

--- public/src/function_dependencies_code_export.mjs
+++ public/src/function_dependencies_code_export.mjs
@@ -8,20 +8,22 @@ export async function function_dependencies_code_export(f_name) {
   marker("1");
   let global_init = js_code_global_init();
   let v = await function_dependencies_code(f_name);
-  let dependencies = object_property_get(v, "code");
   let externals = object_property_get(v, "externals");
-  let e = js_code_export(f_name);
-  const code = `${global_init}
+  let v2 = {
+    fn_get: async function lambda() {
+      let dependencies = object_property_get(v, "code");
+      let e = js_code_export(f_name);
+      const code = `${global_init}
     ${dependencies}
     ${e}`;
-  const blob = new Blob([code], {
-    type: "text/javascript",
-  });
-  const url = URL.createObjectURL(blob);
-  const mod = await import(url);
-  let fn = object_property_get(mod, f_name);
-  let v2 = {
-    fn_get: function lambda() {},
+      const blob = new Blob([code], {
+        type: "text/javascript",
+      });
+      const url = URL.createObjectURL(blob);
+      const mod = await import(url);
+      let fn = object_property_get(mod, f_name);
+      return fn;
+    },
     externals,
   };
   return v2;
