04cd1e6de93c9d7ac473e19709c757073862267a
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 16:57:05 -0500

    function_auto_path ..\love\public\src\html_code_script_importmap.mjs

--- public/src/html_code_script_importmap.mjs
+++ public/src/html_code_script_importmap.mjs
@@ -1,7 +1,16 @@
+import { object_properties_from_empty } from "../../../love/public/src/object_properties_from_empty.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_code_script_module_generic } from "../../../love/public/src/html_code_script_module_generic.mjs";
 import { json_format_to } from "./json_format_to.mjs";
-export function html_code_script_importmap(imports) {
+export function html_code_script_importmap(externals) {
+  let imports = object_properties_from_empty(
+    {
+      acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
+      astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
+      "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
+    },
+    externals,
+  );
   marker("1");
   let j = json_format_to({
     imports,
