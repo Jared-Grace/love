d1f49621d19cc694120cec137bff5d10f13e5ca9
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:17:10 -0500

    function_auto_path ..\love\public\src\firebase_storage_function_run.mjs

---
+++ public/src/firebase_storage_function_run_generic.mjs
@@ -0,0 +1,30 @@
+import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
+import { html_attribute_set } from "../../../love/public/src/html_attribute_set.mjs";
+import { html_element } from "../../../love/public/src/html_element.mjs";
+import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
+import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
+import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
+import { firebase_storage_download_property } from "../../../love/public/src/firebase_storage_download_property.mjs";
+export async function firebase_storage_function_run_generic(
+  version_get,
+  f_name,
+  call,
+) {
+  let destination_version = version_get(f_name);
+  let destination = await firebase_storage_download_property(
+    destination_version,
+    "destination",
+  );
+  let code = await firebase_storage_download_property(destination, "code");
+  let global_init = js_code_global_init();
+  let joined = list_join_newline([global_init, code, call]);
+  if (browser_is()) {
+    let body = html_document_body();
+    let component = html_element(body, "script");
+    html_attribute_set(component, "type", "module");
+    html_text_set(component, joined);
+  } else {
+    eval(joined);
+  }
+}
