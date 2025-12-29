22f9ff3e160fcff034dfe405fe36775a9c9dff03
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 09:02:01 -0500

    function_auto_path ..\love\public\src\html_text_get.mjs

---
+++ public/src/html_text_get.mjs
@@ -0,0 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+export function html_text_get(component, text) {
+  marker("1");
+  let element = html_component_element_get(component);
+  element.innerHTML = text;
+}
