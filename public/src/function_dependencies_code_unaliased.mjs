22e70a295ff0f03354e6cbb3eece43b9f37aee08
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:26:10 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_unaliased.mjs

--- public/src/function_dependencies_code_unaliased.mjs
+++ public/src/function_dependencies_code_unaliased.mjs
@@ -1,9 +1,11 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 export async function function_dependencies_code_unaliased(f_names) {
   let v = await function_name_unalias(f_names);
   let unaliased = object_property_get(v, "unaliased");
+  let mapped = list_map(list, function lambda(item) {});
   let d = await function_dependencies_code(unaliased);
   let v2 = {
     d,
