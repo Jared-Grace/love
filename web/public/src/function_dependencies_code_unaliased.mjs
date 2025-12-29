db6a01315ccdf9c9b41c11d2d4dcd088260dfb45
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:53:44 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_unaliased.mjs

--- web/public/src/function_dependencies_code_unaliased.mjs
+++ web/public/src/function_dependencies_code_unaliased.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -9,6 +10,7 @@ export async function function_dependencies_code_unaliased(f_names) {
     return unaliased;
   }
   let waited = await list_map_unordered_async(f_names, lambda);
+  let first = list_first(list);
   let d = await function_dependencies_code(waited);
   let v2 = {
     d,
