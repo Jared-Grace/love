77ebc3b1b59d9666b13c085cdb8d418db28732e1
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 14:23:53 -0500

    function_auto_path ..\love\public\src\function_dependencies.mjs

--- public/src/function_dependencies.mjs
+++ public/src/function_dependencies.mjs
@@ -1,9 +1,11 @@
+import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_exists_assert } from "../../../love/public/src/function_exists_assert.mjs";
 import { list_adder_unique_async } from "../../../love/public/src/list_adder_unique_async.mjs";
 import { visit_unique_async } from "../../../love/public/src/visit_unique_async.mjs";
 import { function_imports } from "../../../love/public/src/function_imports.mjs";
 export async function function_dependencies(f_names) {
+  let split = string_split_comma(f_names2);
   await function_exists_assert(f_names);
   async function lambda2(la) {
     function lambda(v) {
