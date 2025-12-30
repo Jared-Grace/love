d8b3c129fbdfd92f8c09dc93774a3a1bf617c6db
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:13:21 -0500

    data_file_update ..\love\public\src\global_import_exists.mjs

--- web/public/src/js_unparse_inner.mjs
+++ web/public/src/js_unparse_inner.mjs
@@ -1,12 +1,12 @@
 import { global_external_get } from "../../../love/public/src/global_external_get.mjs";
-import { global_external_exists } from "../../../love/public/src/global_external_exists.mjs";
+import { global_import_exists } from "../../../love/public/src/global_import_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { generate } from "astring";
 export function js_unparse_inner(ast) {
   marker("1");
   let g = null;
   let module_name = "astring";
-  let e = global_external_exists(module_name);
+  let e = global_import_exists(module_name);
   if (e) {
     g = global_external_get(module_name);
   } else {
