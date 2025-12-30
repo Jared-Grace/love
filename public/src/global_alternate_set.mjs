e1b174da729145a9bb409911ac7c3eae94ac9937
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:25:40 -0500

    function_auto_path ..\love\public\src\global_alternate_set.mjs

--- public/src/global_alternate_set.mjs
+++ public/src/global_alternate_set.mjs
@@ -4,8 +4,8 @@ import { global_function_set } from "../../../love/public/src/global_function_se
 export function global_alternate_set(value) {
   let global = global_get();
   global_function_set(global_alternate_set, value);
-  let v = function lambda() {
+  let unset = function lambda() {
     object_property_delete(global, global_alternate_set.name);
   };
-  return v;
+  return unset;
 }
