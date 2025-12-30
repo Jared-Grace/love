ef6346c86548306f0f1119671ff8e46f9d1a21bd
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 19:55:43 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
 export async function sandbox() {
+  marker("1");
   let value = previous[p];
   undefined_not_is_assert_lambda(value, object_get);
   function object_get() {
