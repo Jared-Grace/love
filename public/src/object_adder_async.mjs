6286a5c1a23a8ece18957e44c7e7aa7fd033814d
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 15:57:58 -0500

    function_auto_path ..\love\public\src\object_adder_async.mjs

--- public/src/object_adder_async.mjs
+++ public/src/object_adder_async.mjs
@@ -1,11 +1,11 @@
-import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { object_adder_generic } from "../../../love/public/src/object_adder_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function object_adder_async(lambda$oad) {
   marker("1");
-  let reuslt = {};
-  function oa(key, value) {
-    object_property_set_exists_not(reuslt, key, value);
-  }
+  let v = object_adder_generic();
+  let result = object_property_get(v, "result");
+  let oa = object_property_get(v, "oa");
   await lambda$oad(oa);
-  return reuslt;
+  return result;
 }
