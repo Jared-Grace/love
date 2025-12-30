8aeb99f2db07728b3dd0348e11b24d2000326cc8
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:13:30 -0500

    data_file_update ..\love\public\src\global_import_get.mjs

---
+++ public/src/global_import_set.mjs
@@ -0,0 +1,6 @@
+import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function global_import_set(module_name, value) {
+  marker("1");
+  global_function_property_set(global_import_set, module_name, value);
+}
