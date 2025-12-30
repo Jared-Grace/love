ea32ca4f8e85c6c70793cfd9b88122fc032e1582
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:33:17 -0500

    function_auto_path ..\love\public\src\global_get.mjs

--- web/public/src/global_get.mjs
+++ web/public/src/global_get.mjs
@@ -1,6 +1,8 @@
+import { global_function_exists } from "../../../love/public/src/global_function_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 let global = {};
-export function global_get() {global_function_exists
+export function global_get() {
   marker("1");
   return global;
+  let exists = global_function_exists(fn);
 }
