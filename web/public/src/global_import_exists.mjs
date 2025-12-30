b3398b591e27b7648ffd9fe2849b5131d2750b5a
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:13:38 -0500

    ri global_external global_import

--- web/public/src/global_import_exists.mjs
+++ web/public/src/global_import_exists.mjs
@@ -1,8 +1,8 @@
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
-import { global_external_set } from "../../../love/public/src/global_external_set.mjs";
+import { global_import_set } from "../../../love/public/src/global_import_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function global_import_exists(module_name) {
   marker("1");
-  let e = global_function_property_exists(global_external_set, module_name);
+  let e = global_function_property_exists(global_import_set, module_name);
   return e;
 }
