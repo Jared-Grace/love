15de6d91f5ea63307632749246a384e05bb07571
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:19:26 -0500

    function_auto_path ..\love\public\src\html_code_script_importmap.mjs

--- public/src/html_code_script_importmap.mjs
+++ public/src/html_code_script_importmap.mjs
@@ -1,16 +1,9 @@
-import { object_properties_from_empty } from "../../../love/public/src/object_properties_from_empty.mjs";
+import { function_dependencies_externals_to_urls } from "../../../love/public/src/function_dependencies_externals_to_urls.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_code_script_module_generic } from "../../../love/public/src/html_code_script_module_generic.mjs";
 import { json_format_to } from "./json_format_to.mjs";
 export function html_code_script_importmap(externals) {
-  let imports = object_properties_from_empty(
-    {
-      acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
-      astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
-      "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
-    },
-    externals,
-  );
+  let imports = function_dependencies_externals_to_urls(externals);
   marker("1");
   let j = json_format_to({
     imports,
