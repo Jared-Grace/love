e467bc523cdca203038abdf7d9deaa4e615ef1c1
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:23:41 -0500

    function_auto_path ..\love\public\src\global_alternate_set.mjs

--- public/src/global_alternate_set.mjs
+++ public/src/global_alternate_set.mjs
@@ -1,4 +1,6 @@
+import { global_get } from "../../../love/public/src/global_get.mjs";
 import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
 export function global_alternate_set(value) {
+  let global = global_get();
   global_function_set(global_alternate_set, value);
 }
