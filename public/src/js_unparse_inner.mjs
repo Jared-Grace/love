d598e9303b70f5df68f66834af64918ae9fd8165
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 17:05:26 -0500

    function_auto_path ..\love\public\src\js_unparse_inner.mjs

--- public/src/js_unparse_inner.mjs
+++ public/src/js_unparse_inner.mjs
@@ -1,6 +1,4 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { global_get } from "../../../love/public/src/global_get.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { global_import_get } from "../../../love/public/src/global_import_get.mjs";
 import { global_import_exists } from "../../../love/public/src/global_import_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -9,12 +7,7 @@ export function js_unparse_inner(ast) {
   marker("1");
   let g = null;
   let module_name = "astring";
-  let global = global_get();
   let e = global_import_exists(module_name);
-  log({
-    globall: global,
-    e,
-  });
   if (e) {
     let v = global_import_get(module_name);
     g = object_property_get(v, "generate");
