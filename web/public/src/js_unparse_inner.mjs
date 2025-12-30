aad49da9940fd2f7d913a7451ebadf7a1b3d75ff
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:07:58 -0500

    data_file_update ..\love\public\src\js_unparse_inner.mjs

--- web/public/src/js_unparse_inner.mjs
+++ web/public/src/js_unparse_inner.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { generate } from "astring";
 export function js_unparse_inner(ast) {
-  marker("1");
+  marker("1");global_external_exists
   let output = generate(ast);
   return output;
 }
