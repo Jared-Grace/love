a3c0879b994d802fb3db689e0995d67208666681
Grace-Jared <christrosetolife@gmail.com>
2025-12-29 19:46:59 -0500

    data_file_update ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -72,7 +72,7 @@ export async function app_a_function(context) {
       let m = list_multiple_is(versions);
       object_property_set_exists_not(item, "changed", m);
     }
-    each(all, lambda);
+    each_async(all, lambda);
     let filtered = list_filter_property(all, "changed", true);
     function lambda2(item2) {
       let o = object_properties_from_empty(item2, ["key", "versions"]);
