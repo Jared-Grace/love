73c677da5f7c7b747be36c7e54b146acb5b76803
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:24:48 -0500

    function_auto_path ..\love\public\src\global_function_delete.mjs

--- public/src/global_function_delete.mjs
+++ public/src/global_function_delete.mjs
@@ -1,7 +1,7 @@
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
-export function global_function_delete(fn, value) {
+export function global_function_delete(fn) {
   marker("1");
   let global = global_get();
   object_property_delete(global, fn.name);
