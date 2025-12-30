c70be7f875d2e17e62bafb61f69bfc4842d7cefa
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:13:37 -0500

    data_file_update ..\love\public\src\js_unparse_inner.mjs

--- public/src/global_external_set.mjs
+++
@@ -1,6 +0,0 @@
-import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function global_external_set(module_name, value) {
-  marker("1");
-  global_function_property_set(global_external_set, module_name, value);
-}
