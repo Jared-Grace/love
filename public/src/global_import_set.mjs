446fef8ec98d2a264d399c98032ddc9fe2ad609f
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:37:13 -0500

    function_auto_path ..\love\public\src\global_import_set.mjs

--- public/src/global_import_set.mjs
+++ public/src/global_import_set.mjs
@@ -1,6 +1,9 @@
+import { global_alternate_set } from "../../../love/public/src/global_alternate_set.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function global_import_set(module_name, value, global) {
   marker("1");
+  let unset = global_alternate_set(global);
   global_function_property_set(global_import_set, module_name, value);
+  unset();
 }
