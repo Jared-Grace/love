8d7d3533c66d2f7bc24980708abdd01dfe8794c3
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:34:24 -0500

    data_file_update ..\love\public\src\global_get.mjs

--- public/src/global_get.mjs
+++ public/src/global_get.mjs
@@ -4,11 +4,10 @@ import { global_function_exists } from "../../../love/public/src/global_function
 import { marker } from "../../../love/public/src/marker.mjs";
 let global = {};
 export function global_get() {
-  marker("1");
-  return global;
   let exists = global_function_exists(global_alternate_set);
   if (exists) {
     let value = global_function_get(global_alternate_set);
     return value;
   }
+  return global;
 }
