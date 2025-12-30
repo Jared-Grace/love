788a81364b0f233c18028360fb49ccad43712a61
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 15:52:33 -0500

    function_auto_path ..\love\public\src\each_object_async.mjs

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,14 +1,8 @@
+import { each_object_generic_async } from "../../../love/public/src/each_object_generic_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { object_properties } from "../../../love/public/src/object_properties.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each_async } from "./each_async.mjs";
 export async function each_object_async(object, lambda) {
   marker("1");
   let each_lambda = each_async;
-  async function lambda2(property) {
-    let value = object_property_get(object, property);
-    await lambda(value, property);
-  }
-  let properties = object_properties(object);
-  await each_lambda(properties, lambda2);
+  await each_object_generic_async(object, lambda, each_lambda);
 }
