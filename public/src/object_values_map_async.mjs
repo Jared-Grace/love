473ed80894308fbb5035d0d79318c588d75f0831
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 15:56:22 -0500

    function_auto_path ..\love\public\src\object_values_map_async.mjs

--- public/src/object_values_map_async.mjs
+++ public/src/object_values_map_async.mjs
@@ -2,5 +2,6 @@ import { each_object_unordered_async } from "../../../love/public/src/each_objec
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function object_values_map_async() {
   marker("1");
-  await each_object_unordered_async(object, async function lambda2() {});
+  async function lambda2() {}
+  await each_object_unordered_async(object, lambda2);
 }
