7a88cecdbdbb1ededdb0a1757cf894194c608319
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:06:27 -0500

    function_auto_path ..\love\public\src\global_external_get.mjs

--- web/public/src/global_external_get.mjs
+++ web/public/src/global_external_get.mjs
@@ -1,6 +1,7 @@
-import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
+import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function global_external_get(module_name, value) {
+export function global_external_get(module_name) {
   marker("1");
-  global_function_property_set(global_external_get, module_name, value);
+  let value = global_function_property_get(global_external_get, module_name);
+  return value;
 }
