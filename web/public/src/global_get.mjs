79badd7ae367e5f01d4e1886c08170bf4c115fc3
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 16:35:54 -0500

    function_auto_path ..\love\public\src\global_get.mjs

--- web/public/src/global_get.mjs
+++ web/public/src/global_get.mjs
@@ -4,11 +4,11 @@ import { global_alternate_set } from "../../../love/public/src/global_alternate_
 import { marker } from "../../../love/public/src/marker.mjs";
 let global = {};
 export function global_get() {
-  return global;
   marker("1");
   let exists = object_property_exists(global, global_alternate_set.name);
   if (exists) {
     let value = object_property_get(global, global_alternate_set.name);
     return value;
   }
+  return global;
 }
