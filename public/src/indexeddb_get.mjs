d1cb354cc3122ec787ab4f0de31718586cf9fd84
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:10:47 -0500

    function_auto_path ..\love\public\src\indexeddb_get.mjs

--- public/src/indexeddb_get.mjs
+++ public/src/indexeddb_get.mjs
@@ -1,25 +1,12 @@
+import { indexeddb_get_backend } from "../../../love/public/src/indexeddb_get_backend.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
-import { error } from "../../../love/public/src/error.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_get(db_get, store, key) {
   marker("1");
   let all = await indexeddb_get_all(db_get, store);
   let f = list_find_property(all, "key", key);
   return f;
-  let db = await db_get();
-  const tx = db.transaction(store, "readonly");
-  const s = tx.objectStore(store);
-  let item = await new Promise(function lambda3(resolve, reject) {
-    const req = s.get(key);
-    req.onsuccess = function lambda() {
-      let v = resolve(req.result ?? null);
-      return v;
-    };
-    req.onerror = function lambda2() {
-      let v2 = reject(req.error);
-      return v2;
-    };
-  });
-  return item;
+  let v3 = await indexeddb_get_backend(db_get, store, key);
+  return v3;
 }
