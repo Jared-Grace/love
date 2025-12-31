01ac367be8a560a44a835e0d3a7b71676de558dd
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:22:15 -0500

    function_auto_path ..\love\public\src\love_initialize.mjs

--- web/public/src/love_initialize.mjs
+++ web/public/src/love_initialize.mjs
@@ -2,7 +2,6 @@ import { data_folder } from "../../../love/public/src/data_folder.mjs";
 import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
 import { folder_vscode } from "../../../love/public/src/folder_vscode.mjs";
-import { folder_public_combine } from "../../../love/public/src/folder_public_combine.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -12,8 +11,7 @@ export async function love_initialize() {
   let vc = folder_vscode();
   let f_path = file_name_json_folder(vc, "tasks");
   let d_path = data_folder();
-  let joined = path_join([d_path, f_path]);
-  let file_path_old = folder_public_combine(joined);
+  let file_path_old = path_join([d_path, f_path]);
   let previous = folder_previous();
   let file_path_new = path_join([previous, f_path]);
   let v = await path_resolve(file_path_old);
