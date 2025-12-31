c6b3c938abd376faa161796983c511f863a91b45
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 16:44:25 -0500

    function_auto_path ..\love\public\src\indexeddb_get_all.mjs

--- public/src/indexeddb_get_all.mjs
+++ public/src/indexeddb_get_all.mjs
@@ -1,5 +1,8 @@
+import { indexeddb_put_multiple } from "../../../love/public/src/indexeddb_put_multiple.mjs";
+import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 export async function indexeddb_get_all(db_get, store) {
+  let dictionary = global_function_set(indexeddb_put_multiple);
   const db = await db_get();
   const tx = db.transaction(store, "readonly");
   const s = tx.objectStore(store);
