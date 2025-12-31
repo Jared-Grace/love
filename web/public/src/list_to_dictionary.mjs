21630bdd961d046103c231676cd0c89574584f50
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:58:25 -0500

    function_auto_path ..\love\public\src\list_to_dictionary.mjs

--- web/public/src/list_to_dictionary.mjs
+++ web/public/src/list_to_dictionary.mjs
@@ -2,13 +2,13 @@ import { each } from "../../../love/public/src/each.mjs";
 import { identity } from "./identity.mjs";
 import { object_property_set_exists_not } from "./object_property_set_exists_not.mjs";
 export function list_to_dictionary(list, lambda$item) {
+  let key_get = identity;
   let dictionary = {};
   function lambda(item) {
+    let key = key_get(item);
     let value = lambda$item(item);
-    object_property_set_exists_not(dictionary, item, value);
+    object_property_set_exists_not(dictionary, key, value);
   }
   each(list, lambda);
   return dictionary;
-  let key_get = identity;
-  let key = key_get(item);
 }
