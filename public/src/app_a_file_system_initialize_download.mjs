4ca64a0a8b73fdd574b9611d52997578d9e1fee4
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 16:57:32 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize_download.mjs

--- public/src/app_a_file_system_initialize_download.mjs
+++ public/src/app_a_file_system_initialize_download.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { indexeddb_put_multiple } from "../../../love/public/src/indexeddb_put_multiple.mjs";
 import { list_to_dictionary_key } from "../../../love/public/src/list_to_dictionary_key.mjs";
@@ -26,5 +27,8 @@ export async function app_a_file_system_initialize_download() {
   let store = app_a_file_system_store();
   await indexeddb_put_multiple(db_get, store, dictionary);
   global_function_property_set(indexeddb_put_multiple, store, dictionary);
+  log({
+    dictionary,
+  });
   storage_local_set(app_a, app_a_file_system_initialize.name, true);
 }
