6f513702656840656c8c67e7173c9d8c2e89a2ed
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:07:27 -0500

    function_auto_path ..\love\public\src\each_object_async.mjs

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,8 +1,8 @@
 import { each_object_generic_async } from "../../../love/public/src/each_object_generic_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each_async } from "./each_async.mjs";
-export async function each_object_async(object, lambda) {
+export async function each_object_async(object, lambda$value$key) {
   marker("1");
   let each_lambda = each_async;
-  await each_object_generic_async(object, lambda, each_lambda);
+  await each_object_generic_async(object, lambda$value$key, each_lambda);
 }
