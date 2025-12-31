be13d144b528071db04e1ea5d44474a987f4b943
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:15:09 -0500

    function_auto_path ..\love\public\src\data_path_generic.mjs

--- public/src/data_path_generic.mjs
+++ public/src/data_path_generic.mjs
@@ -1,9 +1,10 @@
+import { data_folder } from "../../../love/public/src/data_folder.mjs";
 import { file_name_json_folder } from "../../../love/public/src/file_name_json_folder.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function data_path_generic(suffix, f_name_unsuffixed) {
   marker("1");
   const f_name = f_name_unsuffixed + suffix;
-  const folder = "data";
+  const folder = data_folder();
   let f_path = file_name_json_folder(folder, f_name);
   return f_path;
 }
