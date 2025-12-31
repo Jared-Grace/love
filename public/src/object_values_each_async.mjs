4d2483c6c5852728006bd8925f0e1d0ca2f6f6bd
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:21:08 -0500

    fw object_values_each_generic_async object_values_each_async

--- public/src/object_values_each_async.mjs
+++ public/src/object_values_each_async.mjs
@@ -1,4 +1,6 @@
+import { object_values_each_generic_async } from "../../../love/public/src/object_values_each_generic_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function object_values_each_async() {
+export async function object_values_each_async(lambda$value$key, oad, object) {
   marker("1");
+  return await object_values_each_generic_async(lambda$value$key, oad, object);
 }
