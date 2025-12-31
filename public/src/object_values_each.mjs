6f3cd49805815ef9539c1f9c383037a710d30c5b
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:39:07 -0500

    data_file_update ..\love\public\src\each_object_values.mjs

--- public/src/object_values_each.mjs
+++
@@ -1,9 +0,0 @@
-import { each_object } from "../../../love/public/src/each_object.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function object_values_each(object, lambda$item) {
-  marker("1");
-  function lambda(value, property) {
-    lambda$item(value);
-  }
-  each_object(object, lambda);
-}
