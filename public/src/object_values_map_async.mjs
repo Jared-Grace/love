06d9db99e103ef79e6a89001cbf752d9f107777c
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:13:42 -0500

    data_file_update ..\love\public\src\object_values_map_async.mjs

--- public/src/object_values_map_async.mjs
+++ public/src/object_values_map_async.mjs
@@ -5,7 +5,7 @@ export async function object_values_map_async(object, lambda$value) {
   marker("1");
   async function lambda(oad) {
     async function lambda2(value, key) {
-      let mapped = await lambda$value$key(value, key);
+      let mapped = await lambda$value$key(value);
       oad(key, mapped);
     }
     await each_object_unordered_async(object, lambda2);
