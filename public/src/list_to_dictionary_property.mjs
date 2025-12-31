62806f9e4d832e4c3f910abcc276b3029daa2422
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:21:02 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize_download.mjs

---
+++ public/src/list_to_dictionary_property.mjs
@@ -0,0 +1,10 @@
+import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export function list_to_dictionary_property(property, r) {
+  function key_get(item) {
+    let key = object_property_get(item, property);
+    return key;
+  }
+  let dictionary = list_to_dictionary(r, key_get);
+  return dictionary;
+}
