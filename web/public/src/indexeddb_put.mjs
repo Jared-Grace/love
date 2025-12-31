6a442e37df0cf94e1de63ecfc024e047bd472ac2
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:12:39 -0500

    function_auto_path ..\love\public\src\indexeddb_put.mjs

--- web/public/src/indexeddb_put.mjs
+++ web/public/src/indexeddb_put.mjs
@@ -1,3 +1,4 @@
+import { invoke } from "../../../love/public/src/invoke.mjs";
 import { indexeddb_put_backend } from "../../../love/public/src/indexeddb_put_backend.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
@@ -7,6 +8,7 @@ export async function indexeddb_put(db_get, store, key, value_get) {
   let f = list_find_property(all, "key", key);
   const next = await value_get(f);
   object_replace(f, next);
+  invoke(function lambda2() {});
   await indexeddb_put_backend(db_get, store, key, next);
   return next;
 }
