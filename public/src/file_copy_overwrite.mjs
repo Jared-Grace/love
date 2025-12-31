ca0b943e5cc43acdeaf89eb1d7fae0ddc6e25ac2
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:22:58 -0500

    fs file_copy_overwrite

--- public/src/file_copy_overwrite.mjs
+++ public/src/file_copy_overwrite.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 export async function file_copy_overwrite(file_path_new, file_path_old) {
+  marker("1");
   await file_parent_exists_ensure(file_path_new);
   let fs = await import("fs");
   await fs.promises.copyFile(file_path_old, file_path_new);
