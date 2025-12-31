745d81f69b5873a72129cd5f3ec7895658de9392
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:58:53 -0500

    fs list_to_dictionary_generic

--- web/public/src/list_to_dictionary_generic.mjs
+++ web/public/src/list_to_dictionary_generic.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 export function list_to_dictionary_generic(key_get, lambda$item, list) {
+  marker("1");
   let dictionary = {};
   function lambda(item) {
     let key = key_get(item);
