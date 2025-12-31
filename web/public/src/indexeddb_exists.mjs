488a27f00fcd164bbf5d3433a7062f0aac733d46
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:10:37 -0500

    function_auto_path ..\love\public\src\indexeddb_exists.mjs

--- web/public/src/indexeddb_exists.mjs
+++ web/public/src/indexeddb_exists.mjs
@@ -1,6 +1,6 @@
+import { indexeddb_exists_backend } from "../../../love/public/src/indexeddb_exists_backend.mjs";
 import { list_find_property_exists } from "../../../love/public/src/list_find_property_exists.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
-import { error } from "../../../love/public/src/error.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_exists(db_get, store, key) {
   marker("1");
@@ -8,19 +8,6 @@ export async function indexeddb_exists(db_get, store, key) {
   const property = "key";
   let s1 = list_find_property_exists(all, property, key);
   return s1;
-  const db = await db_get();
-  const tx = db.transaction(store, "readonly");
-  const s = tx.objectStore(store);
-  const exists = await new Promise(function lambda3(resolve, reject) {
-    const req = s.count(key);
-    req.onsuccess = function lambda() {
-      let v = resolve(req.result > 0);
-      return v;
-    };
-    req.onerror = function lambda2() {
-      let v2 = reject(req.error);
-      return v2;
-    };
-  });
-  return exists;
+  let v3 = await indexeddb_exists_backend(db_get, store, key);
+  return v3;
 }
