411bb39f8b04d4a993a7d5066358dfd7cf4012a6
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:21:11 -0500

    pb

--- public/src/list_to_dictionary_property.mjs
+++ public/src/list_to_dictionary_property.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function list_to_dictionary_property(property, r) {
+export function list_to_dictionary_property(r, property) {
   marker("1");
   function key_get(item) {
     let key = object_property_get(item, property);
