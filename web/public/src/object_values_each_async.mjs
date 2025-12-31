1c70cdfe991c7fba825c068b16de7f98f810e09c
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:21:31 -0500

    pb

--- web/public/src/object_values_each_async.mjs
+++ web/public/src/object_values_each_async.mjs
@@ -1,7 +1,7 @@
 import { noop } from "../../../love/public/src/noop.mjs";
 import { object_values_each_generic_async } from "../../../love/public/src/object_values_each_generic_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function object_values_each_async(lambda$value$key, object) {
+export async function object_values_each_async(object, lambda$value$key) {
   marker("1");
   let v = await object_values_each_generic_async(
     lambda$value$key,
