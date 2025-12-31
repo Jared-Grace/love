77842aeac6ca7996a348b403a618f22ca390834b
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:51:55 -0500

    function_auto_path ..\love\public\src\folder_read.mjs

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -4,10 +4,8 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { string_slash_forward } from "../../../love/public/src/string_slash_forward.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { app_a_files_paths } from "../../../love/public/src/app_a_files_paths.mjs";
 import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
-import { app_api_cache_storage_local_fn } from "../../../love/public/src/app_api_cache_storage_local_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function folder_read(path_folder) {
@@ -24,12 +22,7 @@ export async function folder_read(path_folder) {
     }
     let mapped2 = list_map(mapped, lambda);
     let unique = list_unique(mapped2);
-    log({
-      n,
-      unique,
-    });
-    let r = await app_api_cache_storage_local_fn(folder_read, arguments);
-    return r;
+    return unique;
   }
   marker("1");
   let fs = await import("fs");
