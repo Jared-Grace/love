f73b7218385f8b54a8770b7724feceda4997dcf5
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:58:37 -0500

    function_auto_path ..\love\public\src\list_to_dictionary.mjs

---
+++ public/src/list_to_dictionary_generic.mjs
@@ -0,0 +1,12 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
+export function list_to_dictionary_generic(key_get, lambda$item, list) {
+  let dictionary = {};
+  function lambda(item) {
+    let key = key_get(item);
+    let value = lambda$item(item);
+    object_property_set_exists_not(dictionary, key, value);
+  }
+  each(list, lambda);
+  return dictionary;
+}
