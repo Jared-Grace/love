ee665a943882f9c7a520cf5bb83dcb8a7cd2d485
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 20:03:46 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize_download.mjs

--- public/src/app_a_file_system_initialize_download.mjs
+++ public/src/app_a_file_system_initialize_download.mjs
@@ -1,10 +1,8 @@
+import { indexeddb_put_multiple } from "../../../love/public/src/indexeddb_put_multiple.mjs";
 import { list_to_dictionary_key } from "../../../love/public/src/list_to_dictionary_key.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
 import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
@@ -24,13 +22,7 @@ export async function app_a_file_system_initialize_download() {
     return key;
   }
   let dictionary = list_to_dictionary_key(r, lambda2, lambda3);
-  log("he");
-  async function lambda(item) {
-    let value_get = lambda_get(item);
-    let key = object_property_get(item, "key");
-    let store = app_a_file_system_store();
-    let value3 = await indexeddb_put(db_get, store, key, value_get);
-  }
-  await each_async(r, lambda);
+  let store = app_a_file_system_store();
+  await indexeddb_put_multiple(db_get, store, dictionary);
   storage_local_set(app_a, app_a_file_system_initialize.name, true);
 }
