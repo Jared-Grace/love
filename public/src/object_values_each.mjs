3c912ff8497e2ef7915de8ea1f5e86994568fbd8
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:38:22 -0500

    function_auto_path ..\love\public\src\object_values_each.mjs

--- public/src/object_values_each.mjs
+++ public/src/object_values_each.mjs
@@ -2,5 +2,6 @@ import { each_object } from "../../../love/public/src/each_object.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function object_values_each(object, lambda$item) {
   marker("1");
-  each_object(object2, function lambda(value, property) {});
+  function lambda(value, property) {}
+  each_object(object2, lambda);
 }
