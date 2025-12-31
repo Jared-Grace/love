36e120d0e49487b132ffa6cdfeb9f64626709906
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:23:03 -0500

    data_file_update ..\love\public\src\file_copy_overwrite.mjs

--- web/public/src/file_copy_overwrite.mjs
+++ web/public/src/file_copy_overwrite.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
-export async function file_copy_overwrite(file_path_new, file_path_old) {
+export async function file_copy_overwrite(file_path_old, file_path_new) {
   marker("1");
   await file_parent_exists_ensure(file_path_new);
   let fs = await import("fs");
