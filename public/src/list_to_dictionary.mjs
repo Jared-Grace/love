21a6027c0aaa85813a4e4155b00be7ad7e908dd2
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:59:04 -0500

    fpsw key_get list

--- public/src/list_to_dictionary.mjs
+++ public/src/list_to_dictionary.mjs
@@ -2,6 +2,6 @@ import { list_to_dictionary_generic } from "../../../love/public/src/list_to_dic
 import { identity } from "./identity.mjs";
 export function list_to_dictionary(list, lambda$item) {
   let key_get = identity;
-  let dictionary = list_to_dictionary_generic(key_get, lambda$item, list);
+  let dictionary = list_to_dictionary_generic(list, lambda$item, key_get);
   return dictionary;
 }
