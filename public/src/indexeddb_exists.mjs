77666ee4d22243ab55d1570c1725eb89534d65d1
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:02:53 -0500

    function_auto_path ..\love\public\src\indexeddb_exists.mjs

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -1,7 +1,14 @@
+import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
+import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
+import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_exists(db_get, store, key) {
   marker("1");
+  let all = await indexeddb_get_all(db_get, store);
+  let f = list_filter_property(all, "key", key);
+  let s1 = list_size_1(list);
+  return f;
   const db = await db_get();
   const tx = db.transaction(store, "readonly");
   const s = tx.objectStore(store);
