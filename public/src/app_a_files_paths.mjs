777a18235cbc05eca87ffc3d653538c859291776
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:41:39 -0500

    function_auto_path ..\love\public\src\app_a_files_paths.mjs

--- public/src/app_a_files_paths.mjs
+++ public/src/app_a_files_paths.mjs
@@ -1,6 +1,5 @@
 import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_a_indexeddb_path_key } from "../../../love/public/src/app_a_indexeddb_path_key.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
@@ -12,8 +11,5 @@ export async function app_a_files_paths() {
   let property_name = app_a_indexeddb_path_key();
   let mapped = list_map_property(all, property_name);
   let files_paths = list_map(mapped, path_normalize);
-  log({
-    files_paths,
-  });
   return files_paths;
 }
