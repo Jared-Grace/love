5a3f1a5d58323164db1bcfc637ddea0fb96cc4a9
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:56:47 -0500

    pb

--- public/src/g_sermon_generate_sandbox.mjs
+++ public/src/g_sermon_generate_sandbox.mjs
@@ -11,7 +11,7 @@ export async function g_sermon_generate_sandbox() {
   let v = py_script_run_cmd("openai_chat");
   let file_names = "openai.txt input.txt output.txt";
   let split = string_split_space(file_names);
-  let combineds = list_map_combine_left(p, split);
+  let combineds = list_map_combine_left(split, p);
   let concated = list_concat([v], combineds);
   let joined = list_join_space(concated);
   let stdout = await command_line(joined);
--- public/src/list_map_combine_left.mjs
+++ public/src/list_map_combine_left.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function list_map_combine_left(left, list) {
+export function list_map_combine_left(list, left) {
   marker("1");
   function lambda(right) {
     let combined = left + right;
