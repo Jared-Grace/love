0aa28f20d70cf371665a610f7af417448e8a8289
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:54:45 -0500

    function_auto_path ..\love\public\src\folder_read.mjs

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,27 +1,11 @@
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
-import { string_split_first } from "../../../love/public/src/string_split_first.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
-import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
-import { string_slash_forward } from "../../../love/public/src/string_slash_forward.mjs";
-import { app_a_files_paths } from "../../../love/public/src/app_a_files_paths.mjs";
-import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { folder_read_browser } from "../../../love/public/src/folder_read_browser.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function folder_read(path_folder) {
   if (browser_is()) {
-    let n = path_normalize(path_folder);
-    let s = string_slash_forward();
-    let prefix = "" + n + s;
-    let files_paths = await app_a_files_paths();
-    let filtered = list_filter_starts_with(files_paths, prefix);
-    let mapped = list_map_prefix_without(filtered, prefix);
-    function lambda(item) {
-      let first = string_split_first(item, s);
-      return first;
-    }
-    let mapped2 = list_map(mapped, lambda);
-    let unique = list_unique(mapped2);
+    let v = await folder_read_browser(path_folder);
+    let unique = object_property_get(v, "unique");
     return unique;
   }
   marker("1");
---
+++ public/src/folder_read_browser.mjs
@@ -0,0 +1,26 @@
+import { list_unique } from "../../../love/public/src/list_unique.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { string_split_first } from "../../../love/public/src/string_split_first.mjs";
+import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
+import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
+import { app_a_files_paths } from "../../../love/public/src/app_a_files_paths.mjs";
+import { string_slash_forward } from "../../../love/public/src/string_slash_forward.mjs";
+import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
+export async function folder_read_browser(path_folder) {
+  let n = path_normalize(path_folder);
+  let s = string_slash_forward();
+  let prefix = "" + n + s;
+  let files_paths = await app_a_files_paths();
+  let filtered = list_filter_starts_with(files_paths, prefix);
+  let mapped = list_map_prefix_without(filtered, prefix);
+  function lambda(item) {
+    let first = string_split_first(item, s);
+    return first;
+  }
+  let mapped2 = list_map(mapped, lambda);
+  let unique = list_unique(mapped2);
+  let v = {
+    unique,
+  };
+  return v;
+}
