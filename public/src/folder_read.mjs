1a86076b15713247e8063673c67b930bc045fe17
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:40:29 -0500

    function_auto_path ..\love\public\src\folder_read.mjs

--- public/src/folder_read.mjs
+++ public/src/folder_read.mjs
@@ -1,10 +1,12 @@
+import { app_a_files_paths } from "../../../love/public/src/app_a_files_paths.mjs";
 import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
 import { app_api_cache_storage_local_fn } from "../../../love/public/src/app_api_cache_storage_local_fn.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function folder_read(path_folder) {
   if (browser_is()) {
-    let n = path_normalize(path);
+    await app_a_files_paths();
+    let n = path_normalize(path_folder);
     let r = await app_api_cache_storage_local_fn(folder_read, arguments);
     return r;
   }
