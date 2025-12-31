f0b71ba852404903dae69800d4ec7a7952d6ee9d
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:30:13 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple.mjs

--- public/src/indexeddb_put_multiple.mjs
+++ public/src/indexeddb_put_multiple.mjs
@@ -5,8 +5,8 @@ import { indexeddb_put_multiple_backend } from "../../../love/public/src/indexed
 import { indexeddb_next } from "../../../love/public/src/indexeddb_next.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_put_multiple(db_get, store, lookup) {
-  let v = object_values(o);
-  global_function_property_set(indexeddb_get_all, store, r);
+  let v = object_values(lookup);
+  global_function_property_set(indexeddb_get_all, store, v);
   marker("1");
   async function lambda_async() {
     await indexeddb_put_multiple_backend(db_get, store, lookup);
