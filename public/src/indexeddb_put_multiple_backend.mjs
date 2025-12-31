77cba604ee24b4df9d4b719ef4b1418938b9fbdb
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:23:24 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple_backend_transform.mjs

---
+++ public/src/indexeddb_put_multiple_backend.mjs
@@ -0,0 +1,21 @@
+import { error } from "../../../love/public/src/error.mjs";
+import { each_object_values } from "../../../love/public/src/each_object_values.mjs";
+export async function indexeddb_put_multiple_backend(db, store, nexts) {
+  const tx = db.transaction(store, "readwrite");
+  const s = tx.objectStore(store);
+  function lambda2(n) {
+    s.put(n);
+  }
+  each_object_values(nexts, lambda2);
+  await new Promise(function lambda6(resolve, reject) {
+    tx.oncomplete = resolve;
+    tx.onerror = function lambda4() {
+      let v3 = reject(tx.error);
+      return v3;
+    };
+    tx.onabort = function lambda5() {
+      let v4 = reject(tx.error);
+      return v4;
+    };
+  });
+}
