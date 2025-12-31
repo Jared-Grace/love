5f226c4e547460747c7abb75ef2b196cfe1ee894
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:15:22 -0500

    function_auto_path ..\love\public\src\object_properties_each_async.mjs

--- web/public/src/object_properties_each_async.mjs
+++ web/public/src/object_properties_each_async.mjs
@@ -1,6 +1,6 @@
 import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
-export async function object_properties_each_async(lookup, lambda2) {
+export async function object_properties_each_async(lookup, lambda$key) {
   let properties = object_properties(lookup);
-  await each_unordered_async(properties, lambda2);
+  await each_unordered_async(properties, lambda$key);
 }
