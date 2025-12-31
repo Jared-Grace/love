00990341d17d1f08b1e86ffcc13623dcd3329c5e
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:22:14 -0500

    function_auto_path ..\love\public\src\data_prompts.mjs

--- web/public/src/data_prompts.mjs
+++ web/public/src/data_prompts.mjs
@@ -1,8 +1,10 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
-export async function data_prompts() {$aa
+export async function data_prompts() {
+  assert_arguments(arguments, 0);
   marker("1");
   let d_path = data_prompts_path();
   let prompts = await data_value("prompts", d_path);
