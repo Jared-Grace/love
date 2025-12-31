322308f0cbc14555ff9d0318669f81bf50c93248
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:09:41 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple.mjs

--- public/src/indexeddb_put_multiple.mjs
+++ public/src/indexeddb_put_multiple.mjs
@@ -1,31 +1,30 @@
 import { each_object_unordered_async } from "../../../love/public/src/each_object_unordered_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 export async function indexeddb_put_multiple(db_get, store, lookup) {
   marker("1");
   const db = await db_get();
-  const previous = await new Promise(async function lambda3(resolve, reject) {
+  {
     const tx = db.transaction(store, "readonly");
     const s = tx.objectStore(store);
-    async function lambda7(value, key2) {}
-    await each_object_unordered_async(object, lambda7);
-    const req = s.get(key);
-    req.onsuccess = function lambda() {
-      let v = resolve(req.result ?? null);
-      return v;
-    };
-    req.onerror = function lambda2() {
-      let v2 = reject(req.error);
-      return v2;
-    };
-  });
+    let previous = null;
+    previous = await new Promise(async function lambda3(resolve, reject) {
+      async function lambda7(value_get, key) {}
+      await each_object_unordered_async(object, lambda7);
+      const req = s.get(key);
+      req.onsuccess = function lambda() {
+        let v = resolve(req.result ?? null);
+        return v;
+      };
+      req.onerror = function lambda2() {
+        let v2 = reject(req.error);
+        return v2;
+      };
+    });
+  }
   const next = await value_get(previous);
   const tx = db.transaction(store, "readwrite");
   const s = tx.objectStore(store);
-  log({
-    next,
-  });
   s.put(next);
   await new Promise(function lambda6(resolve, reject) {
     tx.oncomplete = resolve;
