3fa2a8925e6a4ab628fb61c61cdfc3e8e4ecdeb1
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:12:52 -0500

    function_auto_path ..\love\public\src\love_initialize.mjs

--- public/src/love_initialize.mjs
+++ public/src/love_initialize.mjs
@@ -1,4 +1,6 @@
+import { file_copy } from "../../../love/public/src/file_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function love_initialize() {
+export async function love_initialize() {
   marker("1");
+  await file_copy(file_path_old, file_path_new);
 }
