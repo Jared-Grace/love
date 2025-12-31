4d8bdc7480e910892024264947f00bf391d7dc76
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:39:17 -0500

    ri object_values_each each_object_values

--- public/src/object_values_map_async.mjs
+++ public/src/object_values_map_async.mjs
@@ -1,10 +1,10 @@
-import { object_values_each_generic_async } from "../../../love/public/src/object_values_each_generic_async.mjs";
+import { each_object_values_generic_async } from "../../../love/public/src/each_object_values_generic_async.mjs";
 import { object_adder_async } from "../../../love/public/src/object_adder_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function object_values_map_async(object, lambda$value$key) {
   marker("1");
   async function lambda(oad) {
-    await object_values_each_generic_async(object, lambda$value$key, oad);
+    await each_object_values_generic_async(object, lambda$value$key, oad);
   }
   let result = await object_adder_async(lambda);
   return result;
