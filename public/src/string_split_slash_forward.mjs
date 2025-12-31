23c549c74a558891cffee767c8bd4f6070c62a56
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:43:20 -0500

    function_auto_path ..\love\public\src\string_split_slash_forward.mjs

--- public/src/string_split_slash_forward.mjs
+++ public/src/string_split_slash_forward.mjs
@@ -1,5 +1,7 @@
+import { string_slash_forward } from "../../../love/public/src/string_slash_forward.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
 export function string_split_slash_forward(f_path) {
-  let split2 = string_split(f_path, "/");
+  let separator = string_slash_forward();
+  let split2 = string_split(f_path, separator);
   return split2;
 }
