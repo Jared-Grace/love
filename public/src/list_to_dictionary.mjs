5bccfaf571a18e74954ea788b0169dd9324c6bdb
Grace-Jared <christrosetolife@gmail.com>
2025-12-30 19:56:54 -0500

    function_auto_path ..\love\public\src\list_to_dictionary.mjs

--- public/src/list_to_dictionary.mjs
+++ public/src/list_to_dictionary.mjs
@@ -1,10 +1,10 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { each } from "../../../love/public/src/each.mjs";
+import { object_property_set_exists_not } from "./object_property_set_exists_not.mjs";
 export function list_to_dictionary(list, lambda$item) {
   let dictionary = {};
   function lambda(item) {
     let value = lambda$item(item);
-    object_property_set(dictionary, item, value);
+    object_property_set_exists_not(dictionary, item, value);
   }
   each(list, lambda);
   return dictionary;
