74aa5fb3fb8fb2343e17344036fe85cf00ee28e2
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:21:06 -0500

    data_file_update ..\love\public\src\app_a_file_system_initialize_download.mjs

--- public/src/list_to_dictionary_property.mjs
+++ public/src/list_to_dictionary_property.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function list_to_dictionary_property(property, r) {
+  marker("1");
   function key_get(item) {
     let key = object_property_get(item, property);
     return key;
