0021d5b869d6ab72b0412f4e3629b04b6c209583
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:11:36 -0500

    function_auto_path ..\love\public\src\indexeddb_get_all.mjs

--- public/src/indexeddb_get_all.mjs
+++ public/src/indexeddb_get_all.mjs
@@ -1,25 +1,12 @@
+import { indexeddb_get_all_backend } from "../../../love/public/src/indexeddb_get_all_backend.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
-import { error } from "../../../love/public/src/error.mjs";
 export async function indexeddb_get_all(db_get, store) {
   let exists = global_function_property_exists(indexeddb_get_all, store);
   if (not(exists)) {
-    const db = await db_get();
-    const tx = db.transaction(store, "readonly");
-    const s = tx.objectStore(store);
-    let all = await new Promise(function lambda3(resolve, reject) {
-      const req = s.getAll();
-      req.onsuccess = function lambda() {
-        let v = resolve(req.result);
-        return v;
-      };
-      req.onerror = function lambda2() {
-        let v2 = reject(req.error);
-        return v2;
-      };
-    });
+    let all = await indexeddb_get_all_backend(db_get, store);
     global_function_property_set(indexeddb_get_all, store, all);
   }
   let list = global_function_property_get(indexeddb_get_all, store);
