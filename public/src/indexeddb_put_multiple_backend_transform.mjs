9f4313b6c7535e61a4c8f18cf461baa10e087ccd
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 17:23:24 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple_backend_transform.mjs

--- public/src/indexeddb_put_multiple_backend_transform.mjs
+++ public/src/indexeddb_put_multiple_backend_transform.mjs
@@ -1,6 +1,5 @@
+import { indexeddb_put_multiple_backend } from "../../../love/public/src/indexeddb_put_multiple_backend.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { error } from "../../../love/public/src/error.mjs";
-import { each_object_values } from "../../../love/public/src/each_object_values.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_values_map_async } from "../../../love/public/src/object_values_map_async.mjs";
 import { indexeddb_put_item } from "../../../love/public/src/indexeddb_put_item.mjs";
@@ -28,23 +27,7 @@ export async function indexeddb_put_multiple_backend_transform(
   }
   let nexts = await object_values_map_async(previouses, lambda3);
   {
-    const tx = db.transaction(store, "readwrite");
-    const s = tx.objectStore(store);
-    function lambda2(n) {
-      s.put(n);
-    }
-    each_object_values(nexts, lambda2);
-    await new Promise(function lambda6(resolve, reject) {
-      tx.oncomplete = resolve;
-      tx.onerror = function lambda4() {
-        let v3 = reject(tx.error);
-        return v3;
-      };
-      tx.onabort = function lambda5() {
-        let v4 = reject(tx.error);
-        return v4;
-      };
-    });
+    await indexeddb_put_multiple_backend(db, store, nexts);
   }
   return nexts;
 }
