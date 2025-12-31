602f84c501e75149f5c7a6e89982975fd13ac640
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:15:36 -0500

    function_auto_path ..\love\public\src\love_initialize.mjs

--- public/src/love_initialize.mjs
+++ public/src/love_initialize.mjs
@@ -1,3 +1,4 @@
+import { data_path } from "../../../love/public/src/data_path.mjs";
 import { folder_vscode } from "../../../love/public/src/folder_vscode.mjs";
 import { folder_public_combine } from "../../../love/public/src/folder_public_combine.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
@@ -5,8 +6,9 @@ import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrit
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function love_initialize() {
   marker("1");
+  let d_path = data_path();
   let f = folder_vscode();
-  let joined = path_join([segments]);
+  let joined = path_join([d_path, f]);
   let combined = folder_public_combine(f_path);
   await file_copy_overwrite(file_path_old, file_path_new);
 }
