c36361707436883ce478872d9b064662c7a8665d
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 15:14:16 -0500

    function_auto_path ..\love\public\src\love_initialize.mjs

--- public/src/love_initialize.mjs
+++ public/src/love_initialize.mjs
@@ -1,8 +1,10 @@
+import { folder_public_combine } from "../../../love/public/src/folder_public_combine.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function love_initialize() {
   marker("1");
-  let joined = path_join(segments);
+  let combined = folder_public_combine(f_path);
+  let joined = path_join([segments]);
   await file_copy_overwrite(file_path_old, file_path_new);
 }
