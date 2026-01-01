1d95e7b2886adc3b5688ebb8344f36ca5e463477
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:43:12 -0500

    function_auto_path ..\love\public\src\string_split_first.mjs

--- public/src/string_split_first.mjs
+++ public/src/string_split_first.mjs
@@ -1,10 +1,10 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { string_take } from "../../../love/public/src/string_take.mjs";
 import { string_index_of } from "../../../love/public/src/string_index_of.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
 export function string_split_first(item, s) {
   let index = string_index_of(item, s);
-  object_property_set(object, property_name, value);
+  let taken = string_take(s2, count);
   let split = string_split(item, s);
   let first = list_first(split);
   return first;
