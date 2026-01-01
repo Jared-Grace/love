629cfb4f734557fd201891664ed940399871cf53
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 16:52:21 -0500

    function_auto_path ..\love\public\src\function_type_first.mjs

--- web/public/src/function_type_first.mjs
+++ web/public/src/function_type_first.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { js_visit_nodes } from "../../../love/public/src/js_visit_nodes.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
@@ -6,7 +7,8 @@ import { js_node_type } from "../../../love/public/src/js_node_type.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export async function function_type_first(type) {
   let f_name = await function_current_get();
-  let { ast } = await function_parse_unaliased(f_name);
+  let v = await function_parse_unaliased(f_name);
+  let ast = object_property_get(v, "ast");
   function lambda2(la) {
     function lambda(node) {
       if (js_node_type(node) === type) {
