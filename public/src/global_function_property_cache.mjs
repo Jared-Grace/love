ef32886b6f690130c3d5225e57b98d422dc16d8a
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 15:45:52 -0500

    function_auto_path ..\love\public\src\global_function_property_cache.mjs

--- public/src/global_function_property_cache.mjs
+++ public/src/global_function_property_cache.mjs
@@ -1,13 +1,13 @@
+import { global_function_property_initialize } from "../../../love/public/src/global_function_property_initialize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export function global_function_property_cache(fn, key, value_get) {
   marker("1");
   let json = json_to(key);
-  let c = global_function_initialize(fn, {
+  let c = global_function_property_initialize(fn, {
     json: null,
     result: null,
   });
