f22e152024006f5f17c919d567a1b122b8c8aeb2
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:33:19 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple.mjs

--- public/src/indexeddb_put_multiple.mjs
+++ public/src/indexeddb_put_multiple.mjs
@@ -6,7 +6,6 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { global_function_property_initialize } from "../../../love/public/src/global_function_property_initialize.mjs";
 import { object_values } from "../../../love/public/src/object_values.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
-import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { indexeddb_put_multiple_backend } from "../../../love/public/src/indexeddb_put_multiple_backend.mjs";
 import { indexeddb_next } from "../../../love/public/src/indexeddb_next.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -26,7 +25,6 @@ export async function indexeddb_put_multiple(db_get, store, lookup) {
   }
   let filtered = list_filter(existing, lambda);
   list_remove_multiple(existing, filtered);
-  global_function_property_set(indexeddb_get_all, store, v);
   marker("1");
   async function lambda_async() {
     await indexeddb_put_multiple_backend(db_get, store, lookup);
