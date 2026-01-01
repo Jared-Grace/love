f020dd2982dfb97772a6a766d68ff2aaf133266c
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 17:16:56 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple.mjs

--- web/public/src/indexeddb_put_multiple.mjs
+++ web/public/src/indexeddb_put_multiple.mjs
@@ -5,7 +5,6 @@ import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { global_function_property_initialize } from "../../../love/public/src/global_function_property_initialize.mjs";
 import { object_values } from "../../../love/public/src/object_values.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
 import { indexeddb_put_multiple_backend } from "../../../love/public/src/indexeddb_put_multiple_backend.mjs";
@@ -16,11 +15,7 @@ export async function indexeddb_put_multiple(db_get, store, lookup) {
   let key = "key";
   let v = object_values(lookup);
   let keys = list_map_property(v, key);
-  let existing = global_function_property_initialize(
-    indexeddb_get_all,
-    store,
-    [],
-  );
+  let existing = await indexeddb_get_all(db_get, store);
   function lambda(item) {
     let k = object_property_get(item, key);
     let includes = list_includes(keys, k);
@@ -31,6 +26,7 @@ export async function indexeddb_put_multiple(db_get, store, lookup) {
   log({
     filtered,
     v,
+    existing,
   });
   list_add_multiple(existing, v);
   marker("1");
