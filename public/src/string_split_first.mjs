6b853b9e8651c08c59b92644c4ca4ffb28cf00fc
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:42:22 -0500

    function_auto_path ..\love\public\src\string_split_first.mjs

--- public/src/string_split_first.mjs
+++ public/src/string_split_first.mjs
@@ -1,6 +1,8 @@
+import { string_index_of } from "../../../love/public/src/string_index_of.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
 export function string_split_first(item, s) {
+  let index = string_index_of(s2, item2);
   let split = string_split(item, s);
   let first = list_first(split);
   return first;
