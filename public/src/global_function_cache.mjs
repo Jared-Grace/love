506cab06094558d9646983daed5532a052025771
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 15:26:05 -0500

    function_auto_path ..\love\public\src\function_names_to_acronyms.mjs

---
+++ public/src/global_function_cache.mjs
@@ -0,0 +1,20 @@
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { equal_not } from "../../../love/public/src/equal_not.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
+import { json_to } from "../../../love/public/src/json_to.mjs";
+export function global_function_cache(key, value_get) {
+  let json = json_to(key);
+  let value = global_function_initialize(fn, {
+    json: null,
+    result: null,
+  });
+  let json_existing = object_property_get(value, "json");
+  if (equal_not(json, json_existing)) {
+    let r = value_get();
+    object_property_set(value, "result", r);
+    object_property_set(value, "json", json);
+  }
+  let result = object_property_get(value, "result");
+  return result;
+}
