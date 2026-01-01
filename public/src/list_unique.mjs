13af7c8da250579bdf2a4b7bd5568565fb23aa1f
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:57:05 -0500

    function_auto_path ..\love\public\src\list_unique.mjs

--- public/src/list_unique.mjs
+++ public/src/list_unique.mjs
@@ -1,4 +1,3 @@
-import { set_includes_not } from "../../../love/public/src/set_includes_not.mjs";
 import { set_add } from "../../../love/public/src/set_add.mjs";
 import { set_new } from "../../../love/public/src/set_new.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -7,10 +6,7 @@ export function list_unique(list) {
   marker("1");
   let found = set_new();
   function lambda(item) {
-    const b = set_includes_not(found, item);
-    if (b) {
-      set_add(found, item);
-    }
+    set_add(found, item);
   }
   each(list, lambda);
   let unique = [...found];
