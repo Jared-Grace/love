c56044a19149ba65783e5a6d83a98a82e39cdae1
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:40:28 -0500

    r list_to_dictionary_key list_to_dictionary

--- web/public/src/list_to_dictionary_value.mjs
+++ web/public/src/list_to_dictionary_value.mjs
@@ -1,9 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { list_to_dictionary_key } from "../../../love/public/src/list_to_dictionary_key.mjs";
+import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 export function list_to_dictionary_value(list, lambda$item) {
   marker("1");
   let key_get = identity;
-  let dictionary = list_to_dictionary_key(list, lambda$item, key_get);
+  let dictionary = list_to_dictionary(list, lambda$item, key_get);
   return dictionary;
 }
