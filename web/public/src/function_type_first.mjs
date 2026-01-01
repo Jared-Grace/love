ceb3a995e9935a9a732955270812fb5aa8797a3f
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 16:52:16 -0500

    rs function_parse unaliased

--- web/public/src/function_type_first.mjs
+++ web/public/src/function_type_first.mjs
@@ -1,12 +1,12 @@
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { js_visit_nodes } from "../../../love/public/src/js_visit_nodes.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { function_parse } from "../../../love/public/src/function_parse.mjs";
+import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 import { js_node_type } from "../../../love/public/src/js_node_type.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export async function function_type_first(type) {
   let f_name = await function_current_get();
-  let { ast } = await function_parse(f_name);
+  let { ast } = await function_parse_unaliased(f_name);
   function lambda2(la) {
     function lambda(node) {
       if (js_node_type(node) === type) {
