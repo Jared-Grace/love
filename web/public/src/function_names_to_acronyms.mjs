70dcc6f9a8c7a09eb6ae567f9b75276ff3dc061d
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 15:20:03 -0500

    function_auto_path ..\love\public\src\function_names_to_acronyms.mjs

--- web/public/src/function_names_to_acronyms.mjs
+++ web/public/src/function_names_to_acronyms.mjs
@@ -1,3 +1,4 @@
+import { json_to } from "../../../love/public/src/json_to.mjs";
 import { object_invert } from "../../../love/public/src/object_invert.mjs";
 import { function_name_to_acronym } from "../../../love/public/src/function_name_to_acronym.mjs";
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
@@ -7,4 +8,5 @@ export async function function_names_to_acronyms() {
   let dictionary = list_to_dictionary_value(f_names, function_name_to_acronym);
   let acronyms = object_invert(dictionary);
   return acronyms;
+  let json = json_to(object);
 }
