73b4e2f203d35f0b0a6aa8fb31197a21b438f85b
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 16:52:16 -0500

    rs function_parse unaliased

--- public/src/function_imports.mjs
+++ public/src/function_imports.mjs
@@ -1,7 +1,7 @@
 import { js_imports } from "../../../love/public/src/js_imports.mjs";
-import { function_parse } from "../../../love/public/src/function_parse.mjs";
+import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 export async function function_imports(f_name) {
-  let { ast } = await function_parse(f_name);
+  let { ast } = await function_parse_unaliased(f_name);
   let imports = js_imports(ast);
   return imports;
 }
