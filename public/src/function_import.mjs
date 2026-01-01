13aea728aa4bb0f60e010bb29fbe1f569e0b7f44
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 15:14:57 -0500

    function_auto_path ..\love\public\src\function_import.mjs

--- public/src/function_import.mjs
+++ public/src/function_import.mjs
@@ -1,18 +1,15 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
-import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 export async function function_import(f_name) {
-  let v = await function_name_unalias(f_name);
-  let unaliased = object_property_get(v, "unaliased");
-  let v2 = await function_name_to_path_search(unaliased);
+  let v2 = await function_name_to_path_search(f_name);
   let f = object_property_get(v2, "f_path");
   let f_path = await path_resolve(f);
   const imported = await import(`file://${f_path}`);
-  const imported_fn = imported[unaliased];
+  const imported_fn = imported[f_name];
   if (typeof imported_fn !== "function") {
     throw new Error(
-      `❌ The module "${unaliased}" does not export a default function.`,
+      `❌ The module "${f_name}" does not export a default function.`,
     );
   }
   return imported_fn;
