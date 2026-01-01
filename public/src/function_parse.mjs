9e3b3a7df4af6b2d6db6c635ef0d75b0eb829e38
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 21:10:37 -0500

    o function_parse

--- public/src/function_parse.mjs
+++ public/src/function_parse.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export async function function_parse(f_name) {
+  marker("1");
   const { f_path, unaliased } = await function_name_to_path_unalias(f_name);
   let parsed = await file_js_parse(f_path);
   let to = object_merge(
