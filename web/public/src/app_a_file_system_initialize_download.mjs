2b98aef324521b364ea0c0c5029b794f96b45406
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:44:59 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize_download.mjs

--- web/public/src/app_a_file_system_initialize_download.mjs
+++ web/public/src/app_a_file_system_initialize_download.mjs
@@ -6,14 +6,11 @@ import { storage_local_set } from "../../../love/public/src/storage_local_set.mj
 import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
 import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
 import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
-import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
 export async function app_a_file_system_initialize_download() {
-  let db = await app_a_indexeddb_initialize();
-  let db_get = lambda_get(db);
   let r = await app_api_fn(app_a_download, []);
   let dictionary = list_to_dictionary_property(r, "key");
   let store = app_a_file_system_store();
-  await indexeddb_put_multiple(db_get, store, dictionary);
+  await indexeddb_put_multiple(app_a_indexeddb_initialize, store, dictionary);
   storage_local_set(app_a, app_a_file_system_initialize.name, true);
 }
