6bf44f1f8fbab7d9c713c245c4663d6f158f531c
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 15:26:40 -0500

    function_auto_path ..\love\public\src\function_names_to_acronyms.mjs

--- web/public/src/function_names_to_acronyms.mjs
+++ web/public/src/function_names_to_acronyms.mjs
@@ -5,10 +5,11 @@ import { list_to_dictionary_value } from "../../../love/public/src/list_to_dicti
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function function_names_to_acronyms() {
   let f_names = await functions_names();
-  let dictionary = list_to_dictionary_value(f_names, function_name_to_acronym);
-  let acronyms = object_invert(dictionary);
-  return acronyms;
-  let result2 = global_function_cache(f_names, value_get);
+  let result2 = global_function_cache(
+    function_names_to_acronyms,
+    f_names,
+    value_get,
+  );
   return result2;
   function value_get() {
     let dictionary = list_to_dictionary_value(
