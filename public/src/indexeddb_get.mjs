1484a2d784def1c693e1780c83dda64cd85149d3
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 21:21:35 -0500

    function_auto_path ..\love\public\src\indexeddb_get.mjs

--- public/src/indexeddb_get.mjs
+++ public/src/indexeddb_get.mjs
@@ -1,4 +1,3 @@
-import { indexeddb_get_backend } from "../../../love/public/src/indexeddb_get_backend.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -7,6 +6,4 @@ export async function indexeddb_get(db_get, store, key) {
   let all = await indexeddb_get_all(db_get, store);
   let f = list_find_property(all, "key", key);
   return f;
-  let item = await indexeddb_get_backend(db_get, store, key);
-  return item;
 }
