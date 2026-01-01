c10c7a58b84d3c4bcbff7182607018acf9ee23e3
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:39:13 -0500

    cr list_index_of list string

--- public/src/string_index_of.mjs
+++ public/src/string_index_of.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 export function string_index_of(list, item) {
+  marker("1");
   let index = list.indexOf(item);
   if (index <= -1) {
     error_json({
