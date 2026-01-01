c34f0c449f112a3c67eaadbd59d7c4fb39a4ae56
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:33:25 -0500

    function_auto_path ..\love\public\src\indexeddb_put_multiple.mjs

--- public/src/indexeddb_put_multiple.mjs
+++ public/src/indexeddb_put_multiple.mjs
@@ -25,7 +25,8 @@ export async function indexeddb_put_multiple(db_get, store, lookup) {
   }
   let filtered = list_filter(existing, lambda);
   list_remove_multiple(existing, filtered);
-  marker("1");
+  let v2 = marker("1");
+  (lambda, v2);
   async function lambda_async() {
     await indexeddb_put_multiple_backend(db_get, store, lookup);
   }
