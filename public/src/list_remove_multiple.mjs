d7e85155190d0ac02d3d64bab44056b57025a190
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 17:25:39 -0500

    pb

--- public/src/list_remove_multiple.mjs
+++ public/src/list_remove_multiple.mjs
@@ -1,7 +1,7 @@
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function list_remove_multiple(removals, list) {
+export function list_remove_multiple(list, removals) {
   marker("1");
   function lambda(r) {
     list_remove(list, r);
