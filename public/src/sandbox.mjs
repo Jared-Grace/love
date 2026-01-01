3fcb48b35d7a4ca30860b89c0efe3a8843a7d317
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:53:13 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { app_component } from "../../../love/public/src/app_component.mjs";
 export async function sandbox() {
+  marker("1");
   await app_component("sandbox");
 }
