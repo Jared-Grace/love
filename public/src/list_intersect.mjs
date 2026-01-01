b4a93954094904d62dc642b68c024bd4a9dca269
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:58:32 -0500

    function_auto_path ..\love\public\src\list_intersect.mjs

--- public/src/list_intersect.mjs
+++ public/src/list_intersect.mjs
@@ -1,3 +1,4 @@
+import { set_includes } from "../../../love/public/src/set_includes.mjs";
 import { list_unique_set } from "../../../love/public/src/list_unique_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -5,6 +6,7 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export function list_intersect(list, other) {
   marker("1");
+  let set = list_unique_set(other);
   function lambda2(la) {
     function lambda(l) {
       if (list_includes(other, l)) {
@@ -15,5 +17,5 @@ export function list_intersect(list, other) {
   }
   let i = list_adder(lambda2);
   return i;
-  let set = list_unique_set(list2);
+  let v = set_includes(set2, item);
 }
