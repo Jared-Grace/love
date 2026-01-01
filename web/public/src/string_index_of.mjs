6efda4f35d11f79303f0e5f4260eeacff349eca0
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:39:25 -0500

    function_auto_path ..\love\public\src\string_index_of.mjs

--- web/public/src/string_index_of.mjs
+++ web/public/src/string_index_of.mjs
@@ -1,11 +1,11 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
-export function string_index_of(list, item) {
+export function string_index_of(s, item) {
   marker("1");
-  let index = list.indexOf(item);
+  let index = s.indexOf(item);
   if (index <= -1) {
     error_json({
-      list,
+      s,
       item,
       index,
     });
