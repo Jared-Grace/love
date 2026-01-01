9a33b06108794aa84f73e7424c450ba65adb5509
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:40:17 -0500

    data_file_update ..\love\public\src\list_to_dictionary_value.mjs

--- web/public/src/list_to_dictionary.mjs
+++
@@ -1,7 +0,0 @@
-import { list_to_dictionary_key } from "../../../love/public/src/list_to_dictionary_key.mjs";
-import { identity } from "../../../love/public/src/identity.mjs";
-export function list_to_dictionary(list, lambda$item) {
-  let key_get = identity;
-  let dictionary = list_to_dictionary_key(list, lambda$item, key_get);
-  return dictionary;
-}
