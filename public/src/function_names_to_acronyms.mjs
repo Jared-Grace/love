00699609ae0e39dd3cada633799d801ce1f29ef0
Grace-Jared <christrosetolife@gmail.com>
2025-12-31 20:40:17 -0500

    data_file_update ..\love\public\src\list_to_dictionary_value.mjs

--- public/src/function_names_to_acronyms.mjs
+++ public/src/function_names_to_acronyms.mjs
@@ -1,10 +1,10 @@
 import { object_invert } from "../../../love/public/src/object_invert.mjs";
 import { function_name_to_acronym } from "../../../love/public/src/function_name_to_acronym.mjs";
-import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
+import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function function_names_to_acronyms() {
   let f_names = await functions_names();
-  let dictionary = list_to_dictionary(f_names, function_name_to_acronym);
+  let dictionary = list_to_dictionary_value(f_names, function_name_to_acronym);
   let acronyms = object_invert(dictionary);
   return acronyms;
 }
