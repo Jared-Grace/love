6dffbf3e1b7a98bdae0e971097910b296704e887
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 14:57:31 -0500

    function_auto_path ..\love\public\src\list_unique_set.mjs

--- web/public/src/list_unique_set.mjs
+++ web/public/src/list_unique_set.mjs
@@ -2,10 +2,10 @@ import { each } from "../../../love/public/src/each.mjs";
 import { set_add } from "../../../love/public/src/set_add.mjs";
 import { set_new } from "../../../love/public/src/set_new.mjs";
 export function list_unique_set(list) {
-  let found = set_new();
+  let set = set_new();
   function lambda(item) {
-    set_add(found, item);
+    set_add(set, item);
   }
   each(list, lambda);
-  return found;
+  return set;
 }
