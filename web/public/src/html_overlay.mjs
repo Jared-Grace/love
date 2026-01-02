ca832240855aed4adc117aea84c366529a26ebd5
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 17:56:30 -0500

    function_auto_path ..\love\public\src\html_overlay.mjs

--- web/public/src/html_overlay.mjs
+++ web/public/src/html_overlay.mjs
@@ -3,17 +3,14 @@ import { html_div } from "../../../love/public/src/html_div.mjs";
 export function html_overlay(container, z_index) {
   let overlay = html_div(container);
   let s = {
-    position: "absolute",
+    position: "sticky",
     top: "0",
     left: "0",
-    right: "0",
-    bottom: "0",
-    padding: "1vw",
+    width: "100%",
+    height: "100%",
     background: "rgba(0,0,0,0.4)",
+    padding: "1vw",
     "z-index": z_index,
-    display: "flex",
-    flexDirection: "column",
-    gap: "0px",
   };
   html_style_assign(overlay, s);
   return overlay;
