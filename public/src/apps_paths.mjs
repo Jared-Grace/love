b1c0b2b82141bc5eb78a40727fb8d71978a5d1ad
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:24:00 -0500

    function_auto_path ..\love\public\src\apps_paths.mjs

---
+++ public/src/apps_paths.mjs
@@ -0,0 +1,9 @@
+import { folder_public } from "../../../love/public/src/folder_public.mjs";
+import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function apps_paths() {
+  let fop = folder_public();
+  let as = await folder_read_files(fop);
+  marker("1");
+  return as;
+}
