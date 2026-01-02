11799984db835dfcf2ee0a25347d343ee04436e0
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:31:21 -0500

    msa home

--- public/src/app_a_home.mjs
+++ public/src/app_a_home.mjs
@@ -1,4 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_home() {
+export function app_a_home(context) {
   marker("1");
+  let root = html_clear_context(context);
 }
