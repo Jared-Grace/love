3698dc018cd4c53a70340ad783e2b99717aa012c
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 21:04:33 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -2,5 +2,5 @@ import { app_component } from "../../../love/public/src/app_component.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
-  await app_component("sandbox", $f);
+  await app_component("sandbox", () => {});
 }
