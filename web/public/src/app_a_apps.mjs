199f668876039e15a59ea227caca15f3e4330aec
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:21:37 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- web/public/src/app_a_apps.mjs
+++ web/public/src/app_a_apps.mjs
@@ -1,8 +1,12 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
 import { apps_get } from "../../../love/public/src/apps_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_apps(context) {
   let as = await apps_get();
+  log({
+    as,
+  });
   marker("1");
   app_a_functions_generic(context, as);
 }
