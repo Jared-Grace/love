29699247756380eb096f26f0d1d8c12e87b0f7ca
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 19:15:41 -0500

    function_auto_path ..\love\public\src\marker_screen_add.mjs

---
+++ public/src/marker_screen_add_generic.mjs
@@ -0,0 +1,22 @@
+import { function_transform_marker_specified } from "../../../love/public/src/function_transform_marker_specified.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { marker_next_declare_single_init } from "../../../love/public/src/marker_next_declare_single_init.mjs";
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
+import { data_app_current_get } from "../../../love/public/src/data_app_current_get.mjs";
+export async function marker_screen_add_generic(lambda) {
+  let a_name = await data_app_current_get();
+  let prefixed = app_name_prefixed(a_name);
+  let combined = function_name_combine(prefixed, "screens");
+  async function lambda_ftms(a) {
+    let v2 = marker_next_declare_single_init(a);
+    let properties = object_property_get(v2, "properties");
+    await lambda(properties, prefixed);
+  }
+  let result = await function_transform_marker_specified(
+    combined,
+    "screens",
+    lambda_ftms,
+  );
+  return result;
+}
