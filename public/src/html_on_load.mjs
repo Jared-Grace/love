1b434bd02b8d0bc49f9bb5b7e604cea24bed792b
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 18:41:51 -0500

    function_auto_path ..\love\public\src\html_on_load.mjs

--- public/src/html_on_load.mjs
+++ public/src/html_on_load.mjs
@@ -1,5 +1,7 @@
+import { html_overlay } from "../../../love/public/src/html_overlay.mjs";
 import { html_component_wrap } from "../../../love/public/src/html_component_wrap.mjs";
 export function html_on_load(lambda) {
-  let v = html_component_wrap(element);
+  let w = html_component_wrap(window);
+  let overlay = html_overlay(container, z_index);
   window.addEventListener("load", lambda);
 }
