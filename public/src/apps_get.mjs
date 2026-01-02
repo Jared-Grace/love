44206000a069da2351072e6e316114faa6cb9728
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:24:02 -0500

    rrl apps_get paths

--- public/src/apps_get.mjs
+++
@@ -1,9 +0,0 @@
-import { folder_public } from "../../../love/public/src/folder_public.mjs";
-import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function apps_get() {
-  let fop = folder_public();
-  let as = await folder_read_files(fop);
-  marker("1");
-  return as;
-}
