1ea65eefb303cb164cff05128869e2251c3b9b0c
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:25:03 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -1,3 +1,4 @@
+import { path_name } from "../../../love/public/src/path_name.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
@@ -5,11 +6,10 @@ import { apps_paths } from "../../../love/public/src/apps_paths.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_apps(context) {
   let aps = await apps_paths();
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
+  let mapped = list_map(list, path_name);
   log({
     as: aps,
   });
   marker("1");
-  app_a_functions_generic(context, aps);
+  app_a_functions_generic(context, mapped);
 }
