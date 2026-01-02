d4a9a7cc48f465126db10c7676cf973c5fb8e4a9
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 18:36:23 -0500

    function_auto_path ..\love\public\src\html_overlay.mjs

--- public/src/html_overlay.mjs
+++ public/src/html_overlay.mjs
@@ -1,15 +1,11 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { html_on } from "../../../love/public/src/html_on.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function html_overlay(container, z_index) {
   let overlay = html_div(container);
   let element = html_component_element_get(container);
-  log({
-    element,
-  });
   let s = {
     position: "absolute",
     top: element.scrollTop + "px",
@@ -20,14 +16,12 @@ export function html_overlay(container, z_index) {
     padding: "1vw",
     zIndex: z_index,
   };
-  let es = ["scroll", "resize"];
-  function lambda2(item) {}
-  each(list, lambda2);
-  html_on(component, name_event, lambda);
-  container.addEventListener("scroll", updateOverlay);
-  log({
-    s,
-  });
   html_style_assign(overlay, s);
+  let es = ["scroll", "resize"];
+  function lambda2(e) {
+    html_on(container, e, lambda);
+  }
+  each(es, lambda2);
+  container.addEventListener("scroll", updateOverlay);
   return overlay;
 }
