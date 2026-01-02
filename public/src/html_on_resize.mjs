625d7eb1983a59cdbba8111c354dd3ed10fa3f30
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 18:42:57 -0500

    function_auto_path ..\love\public\src\html_on_resize.mjs

--- public/src/html_on_resize.mjs
+++ public/src/html_on_resize.mjs
@@ -1,4 +1,4 @@
-import { html_on } from "../../../love/public/src/html_on.mjs";
-export function html_on_resize(update) {
-  html_on(window, "resize", update);
+import { html_on_window } from "../../../love/public/src/html_on_window.mjs";
+export function html_on_resize(lambda) {
+  html_on_window("resize", lambda);
 }
