324aa9cba6f9bbcb298334e1d56281b457fef18a
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 17:43:21 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- web/public/src/app_a_apps.mjs
+++ web/public/src/app_a_apps.mjs
@@ -1,8 +1,6 @@
+import { app_a_preview_back_function } from "../../../love/public/src/app_a_preview_back_function.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
-import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
-import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
-import { app_component } from "../../../love/public/src/app_component.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -10,10 +8,7 @@ export async function app_a_apps(context) {
   let mapped = await apps_names();
   marker("1");
   async function on_select(a_name) {
-    function lambda() {
-      app_generic_screen_set(context, app_a_function);
-    }
-    await app_component(a_name, lambda);
+    await app_a_preview_back_function(context, a_name);
     storage_local_set(app_a, "app_selected", a_name);
   }
   app_a_functions_generic(context, "app", mapped, on_select);
