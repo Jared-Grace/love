284d44aeac689fa8aab549f8b7b28027fb58f6b3
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 18:42:20 -0500

    function_auto_path ..\love\public\src\html_on_load.mjs

--- public/src/html_on_load.mjs
+++ public/src/html_on_load.mjs
@@ -2,7 +2,5 @@ import { html_on } from "../../../love/public/src/html_on.mjs";
 import { html_component_wrap } from "../../../love/public/src/html_component_wrap.mjs";
 export function html_on_load(lambda) {
   let w = html_component_wrap(window);
-  function lambda3() {}
-  html_on(component, name_event, lambda3);
-  window.addEventListener("load", lambda);
+  html_on(w, "load", lambda);
 }
