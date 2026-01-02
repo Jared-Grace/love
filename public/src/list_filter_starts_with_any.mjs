e02176a6c0deb8a95ce2960d15bb0cb3f5042444
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:37:31 -0500

    fs list_filter_starts_with_any

--- public/src/list_filter_starts_with_any.mjs
+++ public/src/list_filter_starts_with_any.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_starts_with_any(prefixes, list) {
+  marker("1");
   function lambda(item) {
     let any = list_any_starts_with(item, prefixes);
     return any;
