6fc625a81158b20b543b3d1f9d93a336d3c2d6bc
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 18:42:41 -0500

    function_auto_path ..\love\public\src\html_on_load.mjs

--- public/src/html_on_load.mjs
+++ public/src/html_on_load.mjs
@@ -1,5 +1,5 @@
 import { html_on_window } from "../../../love/public/src/html_on_window.mjs";
-export function html_on_load(lambda) {
+export function html_on_load(lambda) {$a
   const name = "load";
   html_on_window(name, lambda);
 }
