dfcd4dcfb178ff016aee7f4adc9ba3165275c0bc
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:31:25 -0500

    function_auto_path ..\love\public\src\app_a_screens.mjs

--- public/src/app_a_screens.mjs
+++ public/src/app_a_screens.mjs
@@ -1,3 +1,4 @@
+import { app_a_home } from "../../../love/public/src/app_a_home.mjs";
 import { app_a_apps } from "../../../love/public/src/app_a_apps.mjs";
 import { app_a_functions } from "../../../love/public/src/app_a_functions.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
