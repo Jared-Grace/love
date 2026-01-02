1b15a745008201fac69c3e365e2057dffc1de99b
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 21:05:12 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -1,3 +1,5 @@
+import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
+import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
 import { app_component } from "../../../love/public/src/app_component.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
@@ -6,7 +8,9 @@ export async function app_a_apps(context) {
   let mapped = await apps_names();
   marker("1");
   async function on_select(a_name) {
-    function lambda() {}
+    function lambda() {
+      app_generic_screen_set(context, app_a_function);
+    }
     await app_component(a_name, lambda);
   }
   app_a_functions_generic(context, "app", mapped, on_select);
