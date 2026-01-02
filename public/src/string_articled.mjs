7f8ed95b7430b3b4224b286c7ae95a302ffbcb05
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:37:55 -0500

    function_auto_path ..\love\public\src\string_articled.mjs

--- public/src/string_articled.mjs
+++ public/src/string_articled.mjs
@@ -1,8 +1,10 @@
+import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 export function string_articled(noun) {
   const article = "a";
   let vowels = "aeiou";
-  let sw = string_starts_with(s, prefix);
+  let sw = string_starts_with(noun, prefix);
+  let any = list_any_starts_with(item, prefixes);
   if (false) {
   }
   const articled = article + " " + noun;
