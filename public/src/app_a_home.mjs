8f79d25951548205f69eb43e111208a8609caae5
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:31:25 -0500

    function_auto_path ..\love\public\src\app_a_screens.mjs

--- public/src/app_a_home.mjs
+++ public/src/app_a_home.mjs
@@ -1,3 +1,4 @@
+import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_home(context) {
   marker("1");
