e63115093d13e298c885ee182d300de40074e8b8
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 22:53:46 -0500

    function_auto_path ..\love\public\src\app_a_main.mjs

--- web/public/src/app_a_main.mjs
+++ web/public/src/app_a_main.mjs
@@ -1,3 +1,4 @@
+import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -23,6 +24,7 @@ export async function app_a_main(context) {
     invoke_multiple_arg(on_keydowns, e);
   }
   let root = object_property_get(context, "root");
+  html_margin_0(root);
   html_on_keydown(root, lambda);
   html_font_sans_serif_set_html();
   app_generic_refresh(context);
