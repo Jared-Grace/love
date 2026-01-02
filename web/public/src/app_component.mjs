f5be1e305ce7a665bde33cac1e76dcaf52c44636
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 21:08:59 -0500

    function_auto_path ..\love\public\src\app_component.mjs

--- web/public/src/app_component.mjs
+++ web/public/src/app_component.mjs
@@ -1,10 +1,10 @@
+import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_karate_button_back_text } from "../../../love/public/src/app_karate_button_back_text.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
-import { newline } from "../../../love/public/src/newline.mjs";
 import { app_context_initialize_root } from "../../../love/public/src/app_context_initialize_root.mjs";
 import { function_dependencies_code_unaliased } from "../../../love/public/src/function_dependencies_code_unaliased.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -15,7 +15,8 @@ export async function app_component(a_name, back) {
   let v = await function_dependencies_code_unaliased(combined);
   let d = object_property_get(v, "d");
   let code = object_property_get(d, "code");
-  code += newline() + combined;
+  let code_assign = js_code_global_init();
+  let list = [code, code_assign, combined];
   let fn = eval(code);
   let root = html_document_body();
   let text = app_karate_button_back_text();
