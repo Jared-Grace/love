8762a17cef2f88306e6e62a75dc834b95958adcf
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:16:21 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -1,8 +1,8 @@
+import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
 import { apps_get } from "../../../love/public/src/apps_get.mjs";
-import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_apps(context) {
-  let fr = await apps_get();
+  let as = await apps_get();
   marker("1");
-  let root = html_clear_context(context);
+  app_a_functions_generic(context, as);
 }
