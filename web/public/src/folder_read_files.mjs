e0d1d689272c730ad985511978f78d7ae4cd120b
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:23:00 -0500

    function_auto_path ..\love\public\src\folder_read_files.mjs

--- web/public/src/folder_read_files.mjs
+++ web/public/src/folder_read_files.mjs
@@ -1,3 +1,4 @@
+import { file_path_normalize } from "../../../love/public/src/file_path_normalize.mjs";
 import { list_intersect } from "../../../love/public/src/list_intersect.mjs";
 import { list_map_combine_left } from "../../../love/public/src/list_map_combine_left.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -8,6 +9,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 export async function folder_read_files(path_folder) {
   if (browser_is()) {
+    path_folder = file_path_normalize(path_folder);
     let r = await folder_read_browser(path_folder);
     let filtered = object_property_get(r, "filtered");
     let prefix = object_property_get(r, "prefix");
