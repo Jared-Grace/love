c0bd7183bcc8c81b81a68bab8a9902fa3e4727c4
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:24:02 -0500

    rrl apps_get paths

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -1,9 +1,9 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
-import { apps_get } from "../../../love/public/src/apps_get.mjs";
+import { apps_paths } from "../../../love/public/src/apps_paths.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_apps(context) {
-  let as = await apps_get();
+  let as = await apps_paths();
   log({
     as,
   });
