524ec3a1f796f729cbad363159a6dfe922359123
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 21:04:30 -0500

    pn back $f

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -6,7 +6,7 @@ export async function app_a_apps(context) {
   let mapped = await apps_names();
   marker("1");
   async function on_select(a_name) {
-    await app_component(a_name);
+    await app_component(a_name, $f);
   }
   app_a_functions_generic(context, "app", mapped, on_select);
 }
