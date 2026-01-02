40072ba3434dbf5330b349e93df71e28322870d1
Grace-Jared <christrosetolife@gmail.com>
2026-01-01 20:37:09 -0500

    function_auto_path ..\love\public\src\string_articled.mjs

--- web/public/src/string_articled.mjs
+++ web/public/src/string_articled.mjs
@@ -1,6 +1,8 @@
+import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 export function string_articled(noun) {
   const article = "a";
   let vowels = "aeiou";
+  let sw = string_starts_with(s, prefix);
   if (false) {
   }
   const articled = article + " " + noun;
