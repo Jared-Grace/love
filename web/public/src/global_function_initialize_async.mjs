6a26970b7c29c3e672839452582327c4550e0299
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:58:07 -0500

    function_auto_path ..\love\public\src\global_function_initialize_async.mjs

---
+++ web/public/src/global_function_initialize_async.mjs
@@ -0,0 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { global_function_initialize_lambda } from "../../../love/public/src/global_function_initialize_lambda.mjs";
+import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
+export function global_function_initialize_async(fn, initial) {
+  marker("1");
+  assert_arguments(arguments, 2);
+  let value_get = lambda_get(initial);
+  let value = global_function_initialize_lambda(fn, value_get);
+  return value;
+}
