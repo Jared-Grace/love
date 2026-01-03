368e0c890666125ccd098e1e5baabd80a9f36e5f
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:17:01 -0500

    function_auto_path ..\love\public\src\ebible_versions.mjs

--- public/src/ebible_versions.mjs
+++ public/src/ebible_versions.mjs
@@ -14,6 +14,6 @@ export async function ebible_versions() {
   let list = html_parse_find_a_href_starts_with(root, d, "details.php?id=");
   let mapped = list_map_prefix_without(list, "details.php?id=");
   let unique = list_unique(mapped);
-  list_sort_string(list2);
+  list_sort_string(unique);
   return unique;
 }
