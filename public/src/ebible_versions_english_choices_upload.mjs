94083069f849859737e3511e8065c00d0ab6baa9
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:55:09 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_upload.mjs

--- public/src/ebible_versions_english_choices_upload.mjs
+++ public/src/ebible_versions_english_choices_upload.mjs
@@ -2,12 +2,11 @@ import { list_join_slash_forward } from "../../../love/public/src/list_join_slas
 import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
-import { ebible_index_flat_upload_name } from "../../../love/public/src/ebible_index_flat_upload_name.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_choices_upload() {
   marker("1");
   let index = await ebible_versions_english_choices();
-  let file_name = ebible_index_flat_upload_name();
+  let file_name = "choices";
   let file_name_with_extension = file_name_json(file_name);
   let destination = list_join_slash_forward([
     "bibles",
