9e26309bfe9d0afac241d951c221c25ec589d033
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 22:44:00 -0500

    function_auto_path ..\love\public\src\app_a_preview_back_function.mjs

--- public/src/app_a_preview_back_function.mjs
+++ public/src/app_a_preview_back_function.mjs
@@ -3,7 +3,7 @@ import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
 export async function app_a_preview_back_function(context, a_name) {
   function lambda() {
-    $a;
+    $s;
     app_generic_screen_set(context, app_a_function);
   }
   await app_component(a_name, lambda);
