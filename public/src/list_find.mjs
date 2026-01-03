4b1a0e6b7e42b8f982f3ef3201329d27081e4e61
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:09:22 -0500

    function_auto_path ..\love\public\src\list_find.mjs

--- public/src/list_find.mjs
+++ public/src/list_find.mjs
@@ -2,6 +2,6 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export function list_find(list, lambda$item) {
   let filtered = list_filter(list, lambda$item);
-  let only = list_single(filtered);
-  return only;
+  let found = list_single(filtered);
+  return found;
 }
