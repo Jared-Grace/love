8956b70570361a20412a97d5d7622f7691f744ac
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:31:57 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

--- web/public/src/ebible_versions_english_full_sample.mjs
+++ web/public/src/ebible_versions_english_full_sample.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { object_values_map_async } from "../../../love/public/src/object_values_map_async.mjs";
 import { ebible_versions_english_full } from "../../../love/public/src/ebible_versions_english_full.mjs";
@@ -6,6 +7,7 @@ export async function ebible_versions_english_full_sample() {
   marker("1");
   let v = await ebible_versions_english_full();
   async function lambda(value, key) {
+    log(message);
     let result2 = await ebible_verse(key, "GEN01", "1");
     return result2;
   }
