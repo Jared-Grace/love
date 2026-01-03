2b8d3202069be4f828c3df95fecf36292a2c5581
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:53:21 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full.mjs

--- web/public/src/ebible_versions_english_full.mjs
+++ web/public/src/ebible_versions_english_full.mjs
@@ -1,11 +1,13 @@
+import { isaiah_chapters_count } from "../../../love/public/src/isaiah_chapters_count.mjs";
 import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
 import { ebible_versions_english_books_count_cache } from "../../../love/public/src/ebible_versions_english_books_count_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_full() {
   marker("1");
+  const c = isaiah_chapters_count();
   let v = await ebible_versions_english_books_count_cache();
-  let filter = function lambda3() {};
+  let filter = function lambda3(value, key) {};
   function lambda(oad) {
     function lambda2(value, key) {
       if (filter(value, key)) {
