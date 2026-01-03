17510937d9abf6904c48d0dab19c1ac653980d18
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:29:25 -0500

    function_auto_path ..\love\public\src\object_values_map.mjs

--- public/src/object_values_map.mjs
+++ public/src/object_values_map.mjs
@@ -1,11 +1,11 @@
+import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { each_object_values_generic_async } from "../../../love/public/src/each_object_values_generic_async.mjs";
-import { object_adder_async } from "../../../love/public/src/object_adder_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function object_values_map(object, lambda$value$key) {
   marker("1");
   async function lambda(oad) {
     await each_object_values_generic_async(object, lambda$value$key, oad);
   }
-  let result = await object_adder_async(lambda);
+  let result = await object_adder(lambda);
   return result;
 }
