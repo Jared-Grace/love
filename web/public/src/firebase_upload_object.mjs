401c54498a2236a4807343357d9145b6e9ec1d49
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:59:27 -0500

    fs firebase_upload_object

--- web/public/src/firebase_upload_object.mjs
+++ web/public/src/firebase_upload_object.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { firebase_upload_string_generic } from "../../../love/public/src/firebase_upload_string_generic.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export async function firebase_upload_object(object, destination) {
+  marker("1");
   let content = json_to(object);
   const content_type = "application/json";
   await firebase_upload_string_generic(content, destination, content_type);
