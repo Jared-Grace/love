7be13c6691f2d3abe8189741e0535a68d8428e76
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:44:18 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_books_count_cache.mjs

--- public/src/ebible_versions_english_books_count_cache.mjs
+++ public/src/ebible_versions_english_books_count_cache.mjs
@@ -1,4 +1,7 @@
+import { ebible_versions_english_books_count } from "../../../love/public/src/ebible_versions_english_books_count.mjs";
+import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_versions_english_books_count_cache() {
+export async function ebible_versions_english_books_count_cache() {
   marker("1");
+  return await invoke_cache_file(ebible_versions_english_books_count, []);
 }
