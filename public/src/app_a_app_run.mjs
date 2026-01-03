cf8309707b9ef0b9c2094cf43fce826fe4d72eb2
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 22:45:54 -0500

    function_auto_path ..\love\public\src\app_a_app_run.mjs

--- public/src/app_a_app_run.mjs
+++ public/src/app_a_app_run.mjs
@@ -1,6 +1,13 @@
-import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
+import { app_component } from "../../../love/public/src/app_component.mjs";
+import { app_generic_refresh_screen } from "../../../love/public/src/app_generic_refresh_screen.mjs";
+import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
+import { app_generic_screen_name } from "../../../love/public/src/app_generic_screen_name.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_app_run(context) {
-  marker("1");
-  let root = html_clear_context(context);
+export async function app_a_app_run(context) {
+  function lambda() {
+    marker("1");
+    let without = app_generic_screen_name(context, app_a_function);
+    app_generic_refresh_screen(context, without);
+  }
+  await app_component(a_name, lambda);
 }
