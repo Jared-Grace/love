b10f3b0107a4c5af1f36ebbdb7425ff8428ad09d
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:38:42 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

--- public/src/ebible_versions_english_full_sample.mjs
+++ public/src/ebible_versions_english_full_sample.mjs
@@ -1,5 +1,4 @@
-import { not } from "../../../love/public/src/not.mjs";
-import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
+import { list_any_starts_with_not } from "../../../love/public/src/list_any_starts_with_not.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
 import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -12,8 +11,7 @@ export async function ebible_versions_english_full_sample() {
   let v = await ebible_versions_english_full();
   let excluded_prefixes = ["engweb,eng-web"];
   let filter = function lambda4(value, property) {
-    let b = list_any_starts_with(value, excluded_prefixes);
-    let any = not(b);
+    let any = list_any_starts_with_not(value, excluded_prefixes);
     return any;
   };
   function lambda2(oad2) {
