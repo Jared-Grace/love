cf0d0673e03cbc5630f41be098c7a1e2f3390a7d
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:35:47 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_books.mjs

--- public/src/ebible_versions_english_books.mjs
+++ public/src/ebible_versions_english_books.mjs
@@ -1,5 +1,5 @@
+import { ebible_version_books_count } from "../../../love/public/src/ebible_version_books_count.mjs";
 import { ebible_versions_english_downloadable } from "../../../love/public/src/ebible_versions_english_downloadable.mjs";
-import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { list_to_dictionary_async } from "../../../love/public/src/list_to_dictionary_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_books() {
@@ -7,7 +7,7 @@ export async function ebible_versions_english_books() {
   let downloadable = await ebible_versions_english_downloadable();
   let dictionary = await list_to_dictionary_async(
     downloadable,
-    ebible_version_books,
+    ebible_version_books_count,
   );
   return dictionary;
 }
