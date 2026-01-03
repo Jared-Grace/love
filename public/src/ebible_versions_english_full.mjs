8f739b83ed71bc7d1bc7c506080ea782ace00992
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:51:52 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full.mjs

--- public/src/ebible_versions_english_full.mjs
+++ public/src/ebible_versions_english_full.mjs
@@ -5,7 +5,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_full() {
   marker("1");
   let v = await ebible_versions_english_books_count_cache();
-  let filter = null;
+  let filter = () => {};
   function lambda(oad) {
     function lambda2(value, key) {
       if (filter(value, key)) {
