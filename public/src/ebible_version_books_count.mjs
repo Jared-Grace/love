314846b7bd8a4990a6875c2b19d8d0304d923232
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:35:27 -0500

    function_auto_path ..\love\public\src\ebible_version_books_count.mjs

--- public/src/ebible_version_books_count.mjs
+++ public/src/ebible_version_books_count.mjs
@@ -2,5 +2,6 @@ import { ebible_version_books } from "../../../love/public/src/ebible_version_bo
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_books_count(bible_folder) {
   marker("1");
-  return await ebible_version_books(bible_folder);
+  let v = await ebible_version_books(bible_folder);
+  return v;
 }
