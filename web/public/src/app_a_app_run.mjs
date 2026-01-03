c99b1071289e7c8485a2a801bf25c6e20e78d655
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 22:47:46 -0500

    function_auto_path ..\love\public\src\app_a_app_run.mjs

--- web/public/src/app_a_app_run.mjs
+++ web/public/src/app_a_app_run.mjs
@@ -1,3 +1,5 @@
+import { app_a } from "../../../love/public/src/app_a.mjs";
+import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
 import { app_component } from "../../../love/public/src/app_component.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
@@ -7,5 +9,6 @@ export async function app_a_app_run(context) {
     marker("1");
     app_generic_screen_set(context, app_a_function);
   }
+  let a_name = storage_local_get(app_a, "app_selected");
   await app_component(a_name, lambda);
 }
