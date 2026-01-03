cb71bc7ff10d137eb002fd7451effa58bfd3d5db
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:39:17 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

--- web/public/src/object_filter.mjs
+++ web/public/src/object_filter.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
 export function object_filter(filter, object) {
+  marker("1");
   function lambda2(oad2) {
     function lambda3(value, property) {
       if (filter(value, property)) {
