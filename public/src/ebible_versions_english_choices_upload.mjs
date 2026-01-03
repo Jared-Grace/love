bb19a6308600304d3a9935916404d59295d8dfaa
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 16:13:37 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_upload.mjs

--- public/src/ebible_versions_english_choices_upload.mjs
+++ public/src/ebible_versions_english_choices_upload.mjs
@@ -1,12 +1,14 @@
+import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { ebible_verses_upload } from "../../../love/public/src/ebible_verses_upload.mjs";
 import { ebible_versions_english_choices_upload_path } from "../../../love/public/src/ebible_versions_english_choices_upload_path.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_choices_upload() {
+  let cs = await ebible_versions_english_choices();
+  await each_unordered_async(list, async function lambda(item) {});
   await ebible_verses_upload(bible_folder);
   marker("1");
-  let index = await ebible_versions_english_choices();
   let destination = ebible_versions_english_choices_upload_path();
-  await firebase_upload_object(destination, index);
+  await firebase_upload_object(destination, cs);
 }
