609b8d7f7e5a5bbc12065dc75ea3922e92f59309
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 22:44:40 -0500

    function_auto_path ..\love\public\src\app_component.mjs

--- public/src/app_component.mjs
+++ public/src/app_component.mjs
@@ -37,7 +37,7 @@ export async function app_component(a_name, back) {
     flex: "0 0 auto",
   });
   let text = app_karate_button_back_text();
-  let component = html_button(bar, text, back);
+  let component = html_button(bar, text, () => {});
   html_width_full(component);
   let div = html_div(shell);
   html_style_assign(div, {
