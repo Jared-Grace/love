cc5d5ac006e5a3e8d43f22e2c620d6c4f343458f
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:28:34 -0500

    fws ebible_versions_english_full sample

--- public/src/ebible_versions_english_full_sample.mjs
+++ public/src/ebible_versions_english_full_sample.mjs
@@ -1,4 +1,6 @@
+import { ebible_versions_english_full } from "../../../love/public/src/ebible_versions_english_full.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_versions_english_full_sample() {
+export async function ebible_versions_english_full_sample() {
   marker("1");
+  return await ebible_versions_english_full();
 }
