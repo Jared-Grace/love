4b4055764c9e010fa9743955e8d1e89a64832e4c
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:21:56 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_books.mjs

--- public/src/ebible_versions_english_books.mjs
+++ public/src/ebible_versions_english_books.mjs
@@ -1,10 +1,10 @@
+import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { list_to_dictionary_async } from "../../../love/public/src/list_to_dictionary_async.mjs";
 import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_books() {
   marker("1");
   let v = await ebible_versions_english();
-  async function lambda(item) {}
-  let dictionary = await list_to_dictionary_async(list, lambda);
-  return v;
+  let dictionary = await list_to_dictionary_async(v, ebible_version_books);
+  return dictionary;
 }
