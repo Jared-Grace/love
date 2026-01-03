6852edbc06af7279f9e98bb00f70c575ca631419
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 22:48:30 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -1,4 +1,4 @@
-import { app_a_preview_back_function } from "../../../love/public/src/app_a_preview_back_function.mjs";
+import { app_a_app_run } from "../../../love/public/src/app_a_app_run.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
@@ -9,7 +9,7 @@ export async function app_a_apps(context) {
   marker("1");
   async function on_select(a_name) {
     storage_local_set(app_a, "app_selected", a_name);
-    await app_a_preview_back_function(context, a_name);
+    await app_a_app_run(context);
   }
   app_a_functions_generic(context, "app", mapped, on_select);
 }
