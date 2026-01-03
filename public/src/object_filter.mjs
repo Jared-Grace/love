c5296ac583f34ac9a797fbddd8681a8a3a68f093
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:39:15 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

---
+++ public/src/object_filter.mjs
@@ -0,0 +1,14 @@
+import { object_adder } from "../../../love/public/src/object_adder.mjs";
+import { each_object } from "../../../love/public/src/each_object.mjs";
+export function object_filter(filter, object) {
+  function lambda2(oad2) {
+    function lambda3(value, property) {
+      if (filter(value, property)) {
+        oad2(property, value);
+      }
+    }
+    each_object(object, lambda3);
+  }
+  let result3 = object_adder(lambda2);
+  return result3;
+}
