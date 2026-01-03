17046fad5a5a60086090665f6306882d133fb238
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:25:06 -0500

    function_auto_path ..\love\public\src\ebible_versions_english.mjs

--- public/src/ebible_versions_english.mjs
+++ public/src/ebible_versions_english.mjs
@@ -1,4 +1,3 @@
-import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { ebible_versions } from "../../../love/public/src/ebible_versions.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,6 +5,5 @@ export async function ebible_versions_english() {
   marker("1");
   let v = await ebible_versions();
   let filtered = list_filter_starts_with(v, "eng");
-  let file_path = await ebible_version_download(bible_folder);
   return filtered;
 }
