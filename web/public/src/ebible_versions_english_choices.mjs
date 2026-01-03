0498e6cc03d64ecebf854b19841c9595500830e2
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:48:55 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices.mjs

--- web/public/src/ebible_versions_english_choices.mjs
+++ web/public/src/ebible_versions_english_choices.mjs
@@ -1,8 +1,10 @@
+import { object_properties } from "../../../love/public/src/object_properties.mjs";
 import { object_filter } from "../../../love/public/src/object_filter.mjs";
 import { list_any_starts_with_not } from "../../../love/public/src/list_any_starts_with_not.mjs";
 import { ebible_versions_english_full } from "../../../love/public/src/ebible_versions_english_full.mjs";
 export async function ebible_versions_english_choices() {
   let object = await ebible_versions_english_full();
+  let properties = object_properties(obj);
   let excluded_prefixes = ["engweb", "eng-web"];
   let filter = function lambda4(value, property) {
     let any = list_any_starts_with_not(property, excluded_prefixes);
