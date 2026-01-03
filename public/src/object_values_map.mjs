a634114d93b6a476d5012505db5cce534ede15ff
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:30:16 -0500

    function_auto_path ..\love\public\src\object_values_map.mjs

--- public/src/object_values_map.mjs
+++ public/src/object_values_map.mjs
@@ -1,9 +1,14 @@
+import { each_object } from "../../../love/public/src/each_object.mjs";
 import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function object_values_map(object, lambda$value$key) {
   marker("1");
   function lambda(oad) {
-    each_object_values_generic(object, lambda$value$key, oad);
+    function lambda2(value, key) {
+      let mapped = lambda$value$key(value, key);
+      oad(key, mapped);
+    }
+    each_object(object, lambda2);
   }
   let result = object_adder(lambda);
   return result;
