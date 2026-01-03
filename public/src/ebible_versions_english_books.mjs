9072afda6c1832f7b0dd3eb0290c3ec36e2aa408
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:18:24 -0500

    fws ebible_versions_english books

--- public/src/ebible_versions_english_books.mjs
+++ public/src/ebible_versions_english_books.mjs
@@ -1,4 +1,6 @@
+import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_versions_english_books() {
+export async function ebible_versions_english_books() {
   marker("1");
+  return await ebible_versions_english();
 }
