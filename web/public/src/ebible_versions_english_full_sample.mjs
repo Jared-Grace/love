7b161e622ac7155d3c56a5683e30d37b11f98240
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:39:15 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

--- web/public/src/ebible_versions_english_full_sample.mjs
+++ web/public/src/ebible_versions_english_full_sample.mjs
@@ -1,6 +1,5 @@
+import { object_filter } from "../../../love/public/src/object_filter.mjs";
 import { list_any_starts_with_not } from "../../../love/public/src/list_any_starts_with_not.mjs";
-import { each_object } from "../../../love/public/src/each_object.mjs";
-import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { object_values_map_async } from "../../../love/public/src/object_values_map_async.mjs";
@@ -8,21 +7,13 @@ import { ebible_versions_english_full } from "../../../love/public/src/ebible_ve
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_full_sample() {
   marker("1");
-  let v = await ebible_versions_english_full();
+  let object = await ebible_versions_english_full();
   let excluded_prefixes = ["engweb,eng-web"];
   let filter = function lambda4(value, property) {
     let any = list_any_starts_with_not(value, excluded_prefixes);
     return any;
   };
-  function lambda2(oad2) {
-    function lambda3(value, property) {
-      if (filter(value, property)) {
-        oad2(property, value);
-      }
-    }
-    each_object(object, lambda3);
-  }
-  let result3 = object_adder(lambda2);
+  let result3 = object_filter(filter, object);
   async function lambda(value, key) {
     log(key);
     let result2 = await ebible_verse(key, "GEN01", "1");
