b46fc8137be46cac90120c436340e2af736c4a34
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:09:51 -0500

    data_file_update ..\love\public\src\list_find_json_next.mjs

--- web/public/src/app_next_main.mjs
+++ web/public/src/app_next_main.mjs
@@ -48,7 +48,7 @@ export async function app_next_main(context) {
   ]);
   let mapped = await list_map_unordered_async(languages_chosen, lambda2);
   list_add_first(mapped, reference);
-  let list = await ebible_index_flat(version_english);
+  let list = await ebible_index_flat(version_english);$a
   const target = {
     chapter_code,
     verse_number,
