2f24d9ff7b6f97501ce10f76cba2f65a1c888a68
Grace-Jared <christrosetolife@gmail.com>
2026-01-02 22:53:06 -0500

    function_auto_path ..\love\public\src\app_generic_screen_set.mjs

--- public/src/app_generic_screen_set.mjs
+++ public/src/app_generic_screen_set.mjs
@@ -1,8 +1,12 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { app_generic_screen_name } from "../../../love/public/src/app_generic_screen_name.mjs";
 import { app_generic_refresh_screen } from "../../../love/public/src/app_generic_refresh_screen.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_generic_screen_set(context, fn) {
   marker("1");
   let without = app_generic_screen_name(context, fn);
+  log({
+    without,
+  });
   app_generic_refresh_screen(context, without);
 }
