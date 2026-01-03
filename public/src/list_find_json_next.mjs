91f3539e5c7b930333a0e12df74edb419757b3b6
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:10:23 -0500

    function_auto_path ..\love\public\src\list_find_json_next.mjs

--- public/src/list_find_json_next.mjs
+++ public/src/list_find_json_next.mjs
@@ -2,5 +2,6 @@ import { list_next } from "../../../love/public/src/list_next.mjs";
 import { list_find_json } from "../../../love/public/src/list_find_json.mjs";
 export function list_find_json_next(list, target) {
   let found = list_find_json(list, target);
-  return list_next(list, found);
+  let next = list_next(list, found);
+  return next;
 }
