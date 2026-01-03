f508f5c1d8c77a8cfccc12d7a349f8742e4d28ee
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:33:03 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_books.mjs

---
+++ public/src/ebible_versions_english_downloadable.mjs
@@ -0,0 +1,11 @@
+import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
+import { list_filter_try_async } from "../../../love/public/src/list_filter_try_async.mjs";
+import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
+export async function ebible_versions_english_downloadable() {
+  let bible_folders = await ebible_versions_english();
+  let list = await list_filter_try_async(
+    bible_folders,
+    ebible_version_download,
+  );
+  return list;
+}
