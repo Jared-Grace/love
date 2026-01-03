4739ba6bfaf766c73d2a8f62be6e9bd65b1476e5
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:48:21 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

---
+++ public/src/ebible_versions_english_choices.mjs
@@ -0,0 +1,13 @@
+import { object_filter } from "../../../love/public/src/object_filter.mjs";
+import { list_any_starts_with_not } from "../../../love/public/src/list_any_starts_with_not.mjs";
+import { ebible_versions_english_full } from "../../../love/public/src/ebible_versions_english_full.mjs";
+export async function ebible_versions_english_choices() {
+  let object = await ebible_versions_english_full();
+  let excluded_prefixes = ["engweb", "eng-web"];
+  let filter = function lambda4(value, property) {
+    let any = list_any_starts_with_not(property, excluded_prefixes);
+    return any;
+  };
+  let result3 = object_filter(object, filter);
+  return result3;
+}
