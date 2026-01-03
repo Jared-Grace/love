6aac10ec9b7a9fc590f198785384b3c5a929ae5e
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:17:33 -0500

    function_auto_path ..\love\public\src\ebible_versions_english.mjs

--- web/public/src/ebible_versions_english.mjs
+++ web/public/src/ebible_versions_english.mjs
@@ -1,7 +1,9 @@
+import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { ebible_versions } from "../../../love/public/src/ebible_versions.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english() {
   marker("1");
   let v = await ebible_versions();
+  let filtered = list_filter_starts_with(list, prefix);
   return v;
 }
