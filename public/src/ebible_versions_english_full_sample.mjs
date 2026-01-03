b9cd70574fa156a706341765d6d2176c0cf10269
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:28:33 -0500

    data_file_update ..\love\public\src\ebible_versions_english_full_sample.mjs

--- public/src/ebible_versions_english_full_sample.mjs
+++ public/src/ebible_versions_english_full_sample.mjs
@@ -1,1 +1,4 @@
-export function ebible_versions_english_full_sample() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function ebible_versions_english_full_sample() {
+  marker("1");
+}
