cc3ac5c0a78517b8adb9a9e3360ebfbcbb7faadb
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:49:04 -0500

    fw ebible_versions_english_books_count_cache ebible_versions_english_full

--- web/public/src/ebible_versions_english_full.mjs
+++ web/public/src/ebible_versions_english_full.mjs
@@ -1,1 +1,6 @@
-export function ebible_versions_english_full() {}
+import { ebible_versions_english_books_count_cache } from "../../../love/public/src/ebible_versions_english_books_count_cache.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function ebible_versions_english_full() {
+  marker("1");
+  return await ebible_versions_english_books_count_cache();
+}
