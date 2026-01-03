c5e7f65f2173391a0b64f0508e4e6963e32547d3
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:53:00 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_upload.mjs

--- public/src/ebible_versions_english_choices_upload.mjs
+++ public/src/ebible_versions_english_choices_upload.mjs
@@ -1,10 +1,10 @@
+import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
 import { ebible_firebase_upload } from "../../../love/public/src/ebible_firebase_upload.mjs";
 import { ebible_index_flat_upload_name } from "../../../love/public/src/ebible_index_flat_upload_name.mjs";
-import { ebible_index_flat } from "../../../love/public/src/ebible_index_flat.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_choices_upload() {
   marker("1");
-  let index = await ebible_index_flat(bible_folder);
+  let index = await ebible_versions_english_choices();
   let file_name = ebible_index_flat_upload_name();
   await ebible_firebase_upload(bible_folder, file_name, index);
 }
