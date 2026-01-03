7792f477ee399b17f3cde86cd296794656ccc863
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:07:18 -0500

    data_file_update ..\love\public\src\app_next_main.mjs

--- public/src/list_find_json.mjs
+++ public/src/list_find_json.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { equal_by } from "../../../love/public/src/equal_by.mjs";
 export function list_find_json(expected, list) {
+  marker("1");
   function lambda4(item) {
     let eq = equal_by(item, expected, json_to);
     return eq;
