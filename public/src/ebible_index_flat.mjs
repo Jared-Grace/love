7dcd11daec2a18bcd21c395c93314a1dfe7caefa
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:01:23 -0500

    function_auto_path ..\love\public\src\ebible_index_flat.mjs

--- public/src/ebible_index_flat.mjs
+++ public/src/ebible_index_flat.mjs
@@ -1,9 +1,13 @@
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_chapters_each_verses } from "../../../love/public/src/ebible_chapters_each_verses.mjs";
 export async function ebible_index_flat(bible_folder) {
+  let b = browser_is();
+  if (b) {
+  }
   marker("1");
   async function lambda(la) {
     await ebible_chapters_each_verses(bible_folder, each_chapter);
