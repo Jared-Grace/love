079df660d7269b8cde146eef17e0e11e8edaf732
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 14:35:33 -0500

    function_auto_path ..\love\public\src\ebible_version_books_count.mjs

--- public/src/ebible_version_books_count.mjs
+++ public/src/ebible_version_books_count.mjs
@@ -1,7 +1,9 @@
+import { list_size } from "../../../love/public/src/list_size.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_books_count(bible_folder) {
   marker("1");
   let v = await ebible_version_books(bible_folder);
+  let size = list_size(list);
   return v;
 }
