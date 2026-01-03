0aa3001697ebda634ff9f883ae22791626f67c2f
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:54:07 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_upload.mjs

--- web/public/src/ebible_versions_english_choices_upload.mjs
+++ web/public/src/ebible_versions_english_choices_upload.mjs
@@ -1,5 +1,6 @@
+import { list_join_slash_forward } from "../../../love/public/src/list_join_slash_forward.mjs";
+import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
-import { ebible_firebase_upload_path } from "../../../love/public/src/ebible_firebase_upload_path.mjs";
 import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
 import { ebible_index_flat_upload_name } from "../../../love/public/src/ebible_index_flat_upload_name.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -7,6 +8,8 @@ export async function ebible_versions_english_choices_upload() {
   marker("1");
   let index = await ebible_versions_english_choices();
   let file_name = ebible_index_flat_upload_name();
-  let destination = ebible_firebase_upload_path(bible_folder, file_name);
+  let file_name_with_extension = file_name_json(file_name);
+  let joined = list_join_slash_forward(["bible", bible_folder]);
+  let destination = list_join_slash_forward([joined, file_name_with_extension]);
   await firebase_upload_object(index, destination);
 }
