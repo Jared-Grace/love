a9e743241fcfd9eee2d2aa5556cabe4baf0cb041
Grace-Jared <christrosetolife@gmail.com>
2026-01-03 15:30:41 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_full_sample.mjs

--- public/src/ebible_versions_english_full_sample.mjs
+++ public/src/ebible_versions_english_full_sample.mjs
@@ -1,10 +1,12 @@
-import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
+import { object_values_map_async } from "../../../love/public/src/object_values_map_async.mjs";
 import { ebible_versions_english_full } from "../../../love/public/src/ebible_versions_english_full.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_full_sample() {
   marker("1");
   let v = await ebible_versions_english_full();
-  function lambda(value, key) {}
-  let result = object_values_map(object, lambda);
+  let result = await object_values_map_async(
+    object,
+    async function lambda(value, key) {},
+  );
   return v;
 }
