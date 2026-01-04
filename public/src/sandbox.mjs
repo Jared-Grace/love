cdb6b145e12295852a18e3f1a0bd7d5cf21759c6
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:38:06 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,6 +3,6 @@ import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
   let list = await ebible_verses("engbsb", "GEN01");
   function lambda(item) {}
-  each(list2, lambda);
+  each(list, lambda);
   return list;
 }
