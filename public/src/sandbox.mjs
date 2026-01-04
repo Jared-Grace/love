d8a0e0d662924b270f6c56742b1bd31e4fe0c5ac
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:52:38 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,15 +6,17 @@ import { string_symbols_unique } from "../../../love/public/src/string_symbols_u
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
-  async function lambda3(la) {}
-  let list2 = await list_adder_async(lambda3);
-  async function lambda(bible_folder) {
-    async function lambda2(chapter_code, verses) {
-      let list = await ebible_verses("engbsb", "GEN01");
-      let mapped = list_map_property(list, "text");
-      let joined2 = string_symbols_unique(mapped);
+  async function lambda3(la) {
+    async function lambda(bible_folder) {
+      async function lambda2(chapter_code, verses) {
+        let list = await ebible_verses("engbsb", "GEN01");
+        let mapped = list_map_property(list, "text");
+        let joined2 = string_symbols_unique(mapped);
+        la(joined2);
+      }
+      await ebible_chapters_each_verses_check_with(bible_folder, lambda2);
     }
-    await ebible_chapters_each_verses_check_with(bible_folder, lambda2);
+    await each_async(await ebible_versions_english(), lambda);
   }
-  await each_async(await ebible_versions_english(), lambda);
+  let list2 = await list_adder_async(lambda3);
 }
