d92ef9ef5955fdcfeae0608b66703d52115914a7
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:10:58 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { ebible_version_symbols_unique_cache } from "../../../love/public/src/ebible_version_symbols_unique_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
@@ -6,6 +7,10 @@ export async function sandbox() {
   marker("1");
   async function lambda(bible_folder) {
     let unique = await ebible_version_symbols_unique_cache(bible_folder);
+    log({
+      bible_folder,
+      unique,
+    });
     return unique;
   }
   await each_async(await ebible_versions_english(), lambda);
