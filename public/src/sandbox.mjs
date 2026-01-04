26d231bf5a366f4784c5347989ec9a90088fefbc
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:43:35 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -7,7 +8,7 @@ import { each } from "../../../love/public/src/each.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
   let list = await ebible_verses("engbsb", "GEN01");
-  ldu;
+  let unique = list_adder_unique(function lambda4() {});
   function lambda(item) {
     let text = object_property_get(item, "text");
     let lower = string_lower_to(s);
