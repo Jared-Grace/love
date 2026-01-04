2cb22f99bc848748fc33cde057ceff533f055b90
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:10:22 -0500

    function_auto_path ..\love\public\src\js_code_call_args_await_maybe.mjs

--- web/public/src/js_code_call_args_await_maybe.mjs
+++ web/public/src/js_code_call_args_await_maybe.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { js_code_await } from "../../../love/public/src/js_code_await.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
@@ -8,10 +7,6 @@ export function js_code_call_args_await_maybe(
   declaration,
 ) {
   let code = js_code_call_args(unaliased, args_code);
-  log({
-    code,
-    args_code,
-  });
   if (object_property_get(declaration, "async")) {
     code = js_code_await(code);
   }
