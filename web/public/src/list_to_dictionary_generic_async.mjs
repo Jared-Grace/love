e1583a395c92c4e38793ebd6656b5b7e3e720e4c
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:24:45 -0500

    function_auto_path ..\love\public\src\list_to_dictionary_async.mjs

---
+++ web/public/src/list_to_dictionary_generic_async.mjs
@@ -0,0 +1,13 @@
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+export function list_to_dictionary_generic_async(lambda$item) {
+  let dictionary = {};
+  let lambda = async function lambda2(item) {
+    let value = await lambda$item(item);
+    object_property_set(dictionary, item, value);
+  };
+  let v = {
+    lambda,
+    dictionary,
+  };
+  return v;
+}
