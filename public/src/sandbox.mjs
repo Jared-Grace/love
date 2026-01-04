7a0a1639821db742fff04a217f0825b6ea44caa2
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:49:55 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,10 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
 import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
+  await each_async(list2, async function lambda(item) {});
   await ebible_chapters_each_verses_check_with(bible_folder, each_chapter);
   let list = await ebible_verses("engbsb", "GEN01");
   let mapped = list_map_property(list, "text");
