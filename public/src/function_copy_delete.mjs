267df58758d42f0977895e12b75052aa18cfbafb
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 18:55:20 -0500

    function_auto_path ..\love\public\src\function_copy_delete.mjs

--- public/src/function_copy_delete.mjs
+++ public/src/function_copy_delete.mjs
@@ -1,3 +1,4 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
@@ -5,8 +6,9 @@ import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_copy_delete(f_name_old, deleted) {
   marker("1");
-  let split = string_split_comma(f_names);
+  let split = string_split_comma(deleted);
   let parts = function_name_to_parts(f_name_old);
+  let list = list_adder(function lambda(la) {});
   let f_name_new = string_replace(f_name_old, from, to);
   let v = await function_copy(f_name_old, f_name_new);
   return v;
