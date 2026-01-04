3ca4b698a69f5db43c82bf6701d23672d0c21786
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:48:25 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,20 +1,9 @@
-import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
-import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
+import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
-import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
   let list = await ebible_verses("engbsb", "GEN01");
   let mapped = list_map_property(list, "text");
-  let joined = list_join_empty(mapped);
-  function lambda4(la) {
-    let split = string_split_empty(joined);
-    each(split, la);
-  }
-  let unique = list_adder_unique(lambda4);
-  list_sort_string(unique);
-  let joined2 = list_join_empty(unique);
+  let joined2 = string_symbols_unique(mapped);
   return joined2;
 }
