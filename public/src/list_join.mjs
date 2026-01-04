53261964a22b00af262079eb37e956da9e050359
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:14:01 -0500

    function_auto_path ..\love\public\src\list_join.mjs

--- public/src/list_join.mjs
+++ public/src/list_join.mjs
@@ -1,10 +1,10 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
-import { error } from "./error.mjs";
+import { error_json } from "./error_json.mjs";
 export function list_join(list, separator) {
   let l = list_is(list);
   if (not(l)) {
-    error({
+    error_json({
       list,
     });
   }
