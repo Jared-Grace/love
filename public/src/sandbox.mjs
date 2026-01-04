00f99ba5b044650e15373f8c7ae39c66735bc40f
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:35:40 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,5 @@
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
-  let list = await ebible_verses(bible_folder, chapter_code);
+  let list = await ebible_verses("engbsb", "GEN01");
+  return list;
 }
