f94efde7769c924d0267688485d6716a50d68f93
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:38:51 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { string_split_multiple } from "../../../love/public/src/string_split_multiple.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
@@ -5,6 +6,7 @@ export async function sandbox() {
   let list = await ebible_verses("engbsb", "GEN01");
   function lambda(item) {
     let text = object_property_get(item, "text");
+    let parts = string_split_multiple(str, delimiters);
   }
   each(list, lambda);
   return list;
