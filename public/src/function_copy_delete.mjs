9eddc55a766198e39689b11988a572d134f54c81
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 18:54:15 -0500

    function_auto_path ..\love\public\src\function_copy_delete.mjs

--- public/src/function_copy_delete.mjs
+++ public/src/function_copy_delete.mjs
@@ -1,7 +1,7 @@
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
 import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_copy_delete(f_name_old, from, to) {
+export async function function_copy_delete(f_name_old, deleted) {
   marker("1");
   let f_name_new = string_replace(f_name_old, from, to);
   let v = await function_copy(f_name_old, f_name_new);
