2da154620009586ecde4c10abd277906133b2d0e
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:56:24 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,4 @@
-import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
-import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
+import { string_symbols_unique_adder } from "../../../love/public/src/string_symbols_unique_adder.mjs";
 import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
@@ -7,13 +6,8 @@ import { string_symbols_unique } from "../../../love/public/src/string_symbols_u
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export async function sandbox() {
   async function lambda(bible_folder) {
-    async function lambda3(la) {
-      await lambda$la(la);
-    }
-    let list = await list_adder_async(lambda3);
-    let joined = list_join_empty(list);
-    let unique = string_symbols_unique(joined);
-    return unique;
+    let v = await string_symbols_unique_adder(lambda$la);
+    return v;
     async function lambda$la(la) {
       async function lambda2(chapter_code, verses) {
         let mapped = list_map_property(verses, "text");
