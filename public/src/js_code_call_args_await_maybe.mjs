5ed089bc6bb36603fb90db43d3a0fc761246a575
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:02:43 -0500

    function_auto_path ..\love\public\src\js_code_call_args_await_maybe.mjs

--- public/src/js_code_call_args_await_maybe.mjs
+++ public/src/js_code_call_args_await_maybe.mjs
@@ -10,6 +10,7 @@ export function js_code_call_args_await_maybe(
   let code = js_code_call_args(unaliased, args_code);
   log({
     code,
+    args_code,
   });
   if (object_property_get(declaration, "async")) {
     code = js_code_await(code);
