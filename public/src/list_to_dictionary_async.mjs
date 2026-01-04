df2b0ab23657fd47e2518f9897871ed517a5b9e3
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:24:45 -0500

    function_auto_path ..\love\public\src\list_to_dictionary_async.mjs

--- public/src/list_to_dictionary_async.mjs
+++ public/src/list_to_dictionary_async.mjs
@@ -1,13 +1,12 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { list_to_dictionary_generic_async } from "../../../love/public/src/list_to_dictionary_generic_async.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 export async function list_to_dictionary_async(list, lambda$item) {
   marker("1");
-  let dictionary = {};
-  let lambda = async function lambda2(item) {
-    let value = await lambda$item(item);
-    object_property_set(dictionary, item, value);
-  };
+  let v = list_to_dictionary_generic_async(lambda$item);
+  let dictionary = object_property_get(v, "dictionary");
+  let lambda = object_property_get(v, "lambda");
   await list_map_unordered_async(list, lambda);
   return dictionary;
 }
