0e13ae63eb7c79762ccacbe17399c6bd8b9a3639
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:20:24 -0500

    function_auto_path ..\love\public\src\ebible_version_symbols_unique.mjs

--- public/src/ebible_version_symbols_unique.mjs
+++ public/src/ebible_version_symbols_unique.mjs
@@ -1,9 +1,13 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
 import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { string_symbols_unique_adder } from "../../../love/public/src/string_symbols_unique_adder.mjs";
 export async function ebible_version_symbols_unique(bible_folder) {
+  log({
+    bible_folder,
+  });
   let unique = await string_symbols_unique_adder(lambda$la);
   async function lambda$la(la) {
     async function lambda2(chapter_code, verses) {
