892a354bdd33f48b5e02280ee4b7e4172bd2065a
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 18:56:44 -0500

    function_auto_path ..\love\public\src\function_copy_delete.mjs

--- web/public/src/function_copy_delete.mjs
+++ web/public/src/function_copy_delete.mjs
@@ -1,15 +1,11 @@
-import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
-import { list_difference } from "../../../love/public/src/list_difference.mjs";
+import { function_name_parts_remove } from "../../../love/public/src/function_name_parts_remove.mjs";
 import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
-import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_copy_delete(f_name_old, deleted) {
   marker("1");
   let split = string_split_comma(deleted);
-  let parts = function_name_to_parts(f_name_old);
-  let difference = list_difference(parts, split);
-  let f_name_new = function_name_combine_multiple(difference);
+  let f_name_new = function_name_parts_remove(f_name_old, split);
   let v = await function_copy(f_name_old, f_name_new);
   return v;
 }
