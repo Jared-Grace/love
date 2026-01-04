a347a91fb6361f68bd127e763ce8d68f8f1aef70
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:50:52 -0500

    data_file_update ..\love\public\src\ebible_chapters_each_verses.mjs

--- web/public/src/ebible_chapters_each_verses_check_with.mjs
+++ web/public/src/ebible_chapters_each_verses_check_with.mjs
@@ -2,8 +2,8 @@ import { ebible_chapters_each_verses } from "../../../love/public/src/ebible_cha
 import { ebible_chapters_each_verses_check } from "../../../love/public/src/ebible_chapters_each_verses_check.mjs";
 export async function ebible_chapters_each_verses_check_with(
   bible_folder,
-  each_chapter,
+  lambda$chapter_code$verses,
 ) {
   await ebible_chapters_each_verses_check(bible_folder);
-  await ebible_chapters_each_verses(bible_folder, each_chapter);
+  await ebible_chapters_each_verses(bible_folder, lambda$chapter_code$verses);
 }
