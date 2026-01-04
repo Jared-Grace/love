28d9bde3274f25d9d3e05f96dff5661c5bd41bbb
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:12:44 -0500

    function_auto_path ..\love\public\src\list_join.mjs

--- public/src/list_join.mjs
+++ public/src/list_join.mjs
@@ -1,6 +1,8 @@
+import { list_is } from "../../../love/public/src/list_is.mjs";
 export function list_join(list, separator) {
   let joined = list.join(separator);
   return joined;
+  let l = list_is(value);
   if (false) {
   }
 }
