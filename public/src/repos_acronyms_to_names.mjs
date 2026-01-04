60fb37c7d053424545d6ffb9262675a57293a447
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:25:15 -0500

    r list_to_dictionary_async list_to_dictionary_unordered_async

--- public/src/repos_acronyms_to_names.mjs
+++ public/src/repos_acronyms_to_names.mjs
@@ -1,10 +1,13 @@
 import { repos_names } from "../../../love/public/src/repos_names.mjs";
 import { object_invert } from "../../../love/public/src/object_invert.mjs";
 import { repo_acronym_get } from "../../../love/public/src/repo_acronym_get.mjs";
-import { list_to_dictionary_async } from "../../../love/public/src/list_to_dictionary_async.mjs";
+import { list_to_dictionary_unordered_async } from "../../../love/public/src/list_to_dictionary_unordered_async.mjs";
 export async function repos_acronyms_to_names() {
   let repos = await repos_names();
-  let dictionary = await list_to_dictionary_async(repos, repo_acronym_get);
+  let dictionary = await list_to_dictionary_unordered_async(
+    repos,
+    repo_acronym_get,
+  );
   let inverted = object_invert(dictionary);
   return inverted;
 }
