4ee369a764afb2919e3d7e97b7350060cfda86c9
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:12:50 -0500

    function_auto_path ..\love\public\src\list_join.mjs

--- web/public/src/list_join.mjs
+++ web/public/src/list_join.mjs
@@ -1,8 +1,9 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
 export function list_join(list, separator) {
   let joined = list.join(separator);
   return joined;
   let l = list_is(value);
-  if (false) {
+  if (not(l)) {
   }
 }
