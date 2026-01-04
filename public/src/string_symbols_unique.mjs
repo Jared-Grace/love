1f621bf26120981c7ab85e7808d9d61545b602d4
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:48:25 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ public/src/string_symbols_unique.mjs
@@ -0,0 +1,16 @@
+import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
+import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
+import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
+export function string_symbols_unique(mapped) {
+  let joined = list_join_empty(mapped);
+  function lambda4(la) {
+    let split = string_split_empty(joined);
+    each(split, la);
+  }
+  let unique = list_adder_unique(lambda4);
+  list_sort_string(unique);
+  let joined2 = list_join_empty(unique);
+  return joined2;
+}
