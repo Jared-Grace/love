81d50859abce79c1e43eeb0855df6fcda45085fa
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:01:18 -0500

    de ebible_version_symbols_unique_cache

--- web/public/src/ebible_version_symbols_unique_cache.mjs
+++
@@ -1,8 +0,0 @@
-import { ebible_version_symbols_unique } from "../../../love/public/src/ebible_version_symbols_unique.mjs";
-import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function ebible_version_symbols_unique_cache(bible_folder) {
-  marker("1");
-  let v = await invoke_cache_file(ebible_version_symbols_unique, bible_folder);
-  return v;
-}
