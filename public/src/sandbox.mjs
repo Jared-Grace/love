4207a3dc4310fe72923294370ece79d4f43dc478
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:26:18 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,11 +1,11 @@
-import { list_to_dictionary_unordered_async } from "../../../love/public/src/list_to_dictionary_unordered_async.mjs";
+import { list_to_dictionary_async } from "../../../love/public/src/list_to_dictionary_async.mjs";
 import { ebible_version_symbols_unique_cache } from "../../../love/public/src/ebible_version_symbols_unique_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
 export async function sandbox() {
   marker("1");
   const versions = await ebible_versions_english();
-  let dictionary = await list_to_dictionary_unordered_async(
+  let dictionary = await list_to_dictionary_async(
     versions,
     ebible_version_symbols_unique_cache,
   );
