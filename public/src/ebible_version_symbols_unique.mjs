79a3d6deef3a8bc24266b60ca1bcebbb953f117f
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:57:04 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ public/src/ebible_version_symbols_unique.mjs
@@ -0,0 +1,16 @@
+import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
+import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+import { string_symbols_unique_adder } from "../../../love/public/src/string_symbols_unique_adder.mjs";
+export async function ebible_version_symbols_unique(bible_folder) {
+  let unique = await string_symbols_unique_adder(lambda$la);
+  async function lambda$la(la) {
+    async function lambda2(chapter_code, verses) {
+      let mapped = list_map_property(verses, "text");
+      let unique_chapter = string_symbols_unique(mapped);
+      la(unique_chapter);
+    }
+    await ebible_chapters_each_verses_check_with(bible_folder, lambda2);
+  }
+  return unique;
+}
