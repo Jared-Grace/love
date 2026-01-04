24bb85020681e7e7810f71f54a6f98a94c5f318a
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:46:59 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,23 +2,18 @@ import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
   let list = await ebible_verses("engbsb", "GEN01");
   let mapped = list_map_property(list, "text");
-  let joined = list_join_empty(letters);
+  let joined = list_join_empty(mapped);
   function lambda4(la) {
-    function lambda(item) {
-      let text = object_property_get(item);
-      function lambda2(item2) {
-        let split = string_split_empty(item2);
-        each(split, la);
-      }
-      each(parts, lambda2);
+    function lambda2(item2) {
+      let split = string_split_empty(item2);
+      each(split, la);
     }
-    each(list, lambda);
+    each(parts, lambda2);
   }
   let unique = list_adder_unique(lambda4);
   return unique;
