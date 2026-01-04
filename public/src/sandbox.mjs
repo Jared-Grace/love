b5561a148060bbc1c66745dc8088e6984c053d07
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:52:19 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
@@ -5,7 +6,7 @@ import { string_symbols_unique } from "../../../love/public/src/string_symbols_u
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
-  lda;
+  let list2 = await list_adder_async(async function lambda3(la) {});
   async function lambda(bible_folder) {
     async function lambda2(chapter_code, verses) {
       let list = await ebible_verses("engbsb", "GEN01");
