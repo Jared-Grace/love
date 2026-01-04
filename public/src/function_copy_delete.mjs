3e4200c9b2e1ff6a57651069b65d66bc23bd3fc3
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 18:56:24 -0500

    function_auto_path ..\love\public\src\function_copy_delete.mjs

--- public/src/function_copy_delete.mjs
+++ public/src/function_copy_delete.mjs
@@ -1,3 +1,4 @@
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
@@ -9,6 +10,7 @@ export async function function_copy_delete(f_name_old, deleted) {
   let split = string_split_comma(deleted);
   let parts = function_name_to_parts(f_name_old);
   let difference = list_difference(parts, split);
+  let combined = function_name_combine_multiple(parts2);
   let f_name_new = string_replace(f_name_old, from, to);
   let v = await function_copy(f_name_old, f_name_new);
   return v;
