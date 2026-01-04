cf57089e696ecd9a4e7345a32b2c56752c9a5175
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:44:35 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
@@ -8,13 +9,14 @@ import { each } from "../../../love/public/src/each.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox() {
   let list = await ebible_verses("engbsb", "GEN01");
-  function lambda4() {
+  function lambda4(la) {
     function lambda(item) {
       let text = object_property_get(item, "text");
       let lower = string_lower_to(s);
       let parts = string_split_multiple(lower, ["—", " "]);
       function lambda2(item2) {
         let split = string_split_empty(item2);
+        list_add(list2, item3);
       }
       let mapped = list_map(parts, lambda2);
     }
