42f761b39288063653aabc7ce0dc233636e64be4
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 16:59:33 -0500

    function_auto_path ..\love\public\src\function_cache.mjs

--- public/src/function_cache.mjs
+++ public/src/function_cache.mjs
@@ -1,5 +1,3 @@
-import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
@@ -27,10 +25,9 @@ export async function function_cache(f_name) {
     if (e) {
       args_none = true;
     }
-    let mapped = list_map(list, js_parse_expression);
     js_code_call_args_await_maybe_declaration_return_add(
       c,
-      [unaliased, arg_names_code],
+      [unaliased, arg_names],
       declaration_cache,
       ast,
     );
