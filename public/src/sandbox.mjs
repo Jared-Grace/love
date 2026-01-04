018c2ff6c29e7cb5a3312d40e231e88f9c6e2794
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 17:18:33 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,21 +1,13 @@
 import { list_to_dictionary_async } from "../../../love/public/src/list_to_dictionary_async.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { ebible_version_symbols_unique_cache } from "../../../love/public/src/ebible_version_symbols_unique_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_versions_english } from "../../../love/public/src/ebible_versions_english.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function sandbox() {
   marker("1");
-  async function lambda(bible_folder) {
-    let unique = await ebible_version_symbols_unique_cache(bible_folder);
-    log({
-      bible_folder,
-      unique,
-    });
-    return unique;
-  }
   const versions = await ebible_versions_english();
-  async function lambda2(item) {}
-  let dictionary = await list_to_dictionary_async(list, lambda2);
-  await each_async(versions, lambda);
+  let dictionary = await list_to_dictionary_async(
+    versions,
+    ebible_version_symbols_unique_cache,
+  );
+  return dictionary;
 }
