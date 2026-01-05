948a121b5a1a9ae4c821f6a1b735e2f1845da391
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:27:35 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_verses_upload } from "../../../love/public/src/ebible_verses_upload.mjs";
 import { range } from "../../../love/public/src/range.mjs";
@@ -5,6 +6,7 @@ import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 import { ebible_versions_english_downloadable_words_search_upload } from "../../../love/public/src/ebible_versions_english_downloadable_words_search_upload.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
+  let english_choices = await ebible_versions_english_choices();
   async function lambda(item) {}
   await each_async(list, lambda);
   await ebible_verses_upload(bible_folder);
