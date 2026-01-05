781bbdf0c319bdb475835d8cc16114761d6b5476
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:32:32 -0500

    function_auto_path ..\love\public\src\retry_until_success.mjs

--- public/src/retry_until_success.mjs
+++ public/src/retry_until_success.mjs
@@ -1,5 +1,7 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function retry_until_success() {
   marker("1");
   let success = false;
+  while (not(success)) {}
 }
