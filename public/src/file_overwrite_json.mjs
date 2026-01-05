c629e1357521dc22dc8d2a52ada67dfacc595c40
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 22:27:03 -0500

    function_auto_path ..\love\public\src\file_overwrite_json.mjs

--- public/src/file_overwrite_json.mjs
+++ public/src/file_overwrite_json.mjs
@@ -1,3 +1,4 @@
+import { import_install } from "../../../love/public/src/import_install.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
@@ -7,7 +8,8 @@ export async function file_overwrite_json(file_path, object) {
     let json = json_format_to(object);
     await file_overwrite(file_path, json);
   } else {
-    let fs = await import("fs");ii
+    let fs = await import("fs");
+    await import_install(name);
     const out = fs.createWriteStream(file_path);
     let json = streamJsonStringify(object);
     await pipeline(json, out);
