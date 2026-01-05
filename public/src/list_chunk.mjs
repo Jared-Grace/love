2978f78490ec059b26f116a821cd3aa6e870e2b0
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:11:33 -0500

    function_auto_path ..\love\public\src\list_chunk.mjs

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -4,8 +4,8 @@ import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_chunk(list, chunk_size) {
-  let v2 = positive_is(objections);
-  assert(b2);
+  let v2 = positive_is(chunk_size);
+  assert(v2);
   let size = list_size(list);
   let position = 0;
   function lambda(la) {
