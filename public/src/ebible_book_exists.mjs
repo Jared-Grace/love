6eae13c5b78246fb975cd590d0766ef07f966292
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:47:03 -0500

    function_auto_path ..\love\public\src\ebible_book_exists.mjs

--- public/src/ebible_book_exists.mjs
+++ public/src/ebible_book_exists.mjs
@@ -1,9 +1,8 @@
+import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 export function ebible_book_exists(books, book_code) {
   marker("1");
-  let book = list_find_property(books, "book_code", book_code);
-  let book_name = object_property_get(book, "text");
-  return book_name;
+  let book = list_find_property_or_null(books, "book_code", book_code);
+  let v = book != null;
+  return v;
 }
