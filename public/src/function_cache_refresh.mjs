8023c608fc3632d635c245976e0ad0a2b669fae8
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:17:06 -0500

    function_auto_path ..\love\public\src\function_cache_refresh.mjs

--- public/src/function_cache_refresh.mjs
+++ public/src/function_cache_refresh.mjs
@@ -1,9 +1,11 @@
+import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_cache_name } from "../../../love/public/src/function_cache_name.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_cache_refresh(f_name) {
   marker("1");
+  invoke_cache_clear(fn, args);
   let v3 = await function_cache_name(f_name);
   let f_name_cache = object_property_get(v3, "f_name_cache");
   let unaliased = object_property_get(v3, "unaliased");
