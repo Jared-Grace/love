a0faf79aff788f9c43375142d266597f52055b43
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 14:51:32 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_words_search_upload.mjs

--- public/src/ebible_versions_english_downloadable_words_search_upload.mjs
+++ public/src/ebible_versions_english_downloadable_words_search_upload.mjs
@@ -1,4 +1,3 @@
-import { object_properties } from "../../../love/public/src/object_properties.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
 import { ebible_versions_english_downloadable_words_lookup_cache } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs";
@@ -9,8 +8,6 @@ import { each_object_async } from "../../../love/public/src/each_object_async.mj
 export async function ebible_versions_english_downloadable_words_search_upload() {
   marker("1");
   let result = await ebible_versions_english_downloadable_words_lookup_cache();
-  let properties = object_properties(result);
-  return properties;
   async function lambda3(value, word) {
     log({
       value,
