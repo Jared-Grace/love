6e729c6983af4dd18202bca7a4bc11a230f11be2
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:48:12 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,17 +1,8 @@
-import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { ebible_verses_upload } from "../../../love/public/src/ebible_verses_upload.mjs";
 import { range } from "../../../love/public/src/range.mjs";
 import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 import { ebible_versions_english_downloadable_words_search_upload } from "../../../love/public/src/ebible_versions_english_downloadable_words_search_upload.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
-  let english_choices = await ebible_versions_english_choices();
-  async function lambda(bible_folder) {
-    await ebible_verses_upload(bible_folder);
-  }
-  await each_async(english_choices, lambda);
-  return;
   marker("1");
   let r = range(10);
   let chunks = list_chunk(r, 3);
