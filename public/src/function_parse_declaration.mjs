bf5da2d3e4998d1178dd87f587512063ce407833
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:14:52 -0500

    function_auto_path ..\love\public\src\function_parse_declaration.mjs

--- public/src/function_parse_declaration.mjs
+++ public/src/function_parse_declaration.mjs
@@ -2,8 +2,8 @@ import { function_parse_imports_packages } from "../../../love/public/src/functi
 import { function_parse_declaration_inner } from "../../../love/public/src/function_parse_declaration_inner.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 export async function function_parse_declaration(f_name) {
-  let parsed = await function_parse_unaliased(f_name);
-  let to2 = function_parse_declaration_inner(parsed);
-  let to = function_parse_imports_packages(to2);
-  return to;
+  let p = await function_parse_unaliased(f_name);
+  let to = function_parse_declaration_inner(p);
+  let parsed = function_parse_imports_packages(to);
+  return parsed;
 }
