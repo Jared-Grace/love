06ea1d15e4046dd9ecabeba751ae24e52224fd8b
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:03:56 -0500

    function_auto_path ..\love\public\src\string_transform_lookup.mjs

--- public/src/string_transform_lookup.mjs
+++ public/src/string_transform_lookup.mjs
@@ -2,15 +2,15 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { string_transform } from "../../../love/public/src/string_transform.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-export function string_transform_lookup(lower, normalize) {
+export function string_transform_lookup(s, lookup) {
   marker("1");
   function lambda3(c) {
-    let exists = object_property_exists(normalize, c);
+    let exists = object_property_exists(lookup, c);
     if (exists) {
-      let value = object_property_get(normalize, c);
+      let value = object_property_get(lookup, c);
       return value;
     }
     return c;
   }
-  let joined2 = string_transform(lower, lambda3);
+  let joined2 = string_transform(s, lambda3);
 }
