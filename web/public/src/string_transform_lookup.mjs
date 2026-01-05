22436d5ec1360e032029a33bb5c30af94f3b4fd3
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:03:42 -0500

    fs string_transform_lookup

--- web/public/src/string_transform_lookup.mjs
+++ web/public/src/string_transform_lookup.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { string_transform } from "../../../love/public/src/string_transform.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 export function string_transform_lookup(normalize, lower) {
+  marker("1");
   function lambda3(c) {
     let exists = object_property_exists(normalize, c);
     if (exists) {
