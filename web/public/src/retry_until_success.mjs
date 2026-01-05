2fe6d044449e9a852ff812f3227111486310e3f3
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:33:47 -0500

    function_auto_path ..\love\public\src\retry_until_success.mjs

--- web/public/src/retry_until_success.mjs
+++ web/public/src/retry_until_success.mjs
@@ -1,8 +1,10 @@
+import { catch_ignore_async } from "../../../love/public/src/catch_ignore_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function retry_until_success(lambda) {
   marker("1");
   let r = null;
   while (true) {
+    await catch_ignore_async(async function lambda3() {});
     try {
       r = await lambda();
       break;
