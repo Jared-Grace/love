d0937ba2e38de29e5bfb5ec53fd709f79a570bc5
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:19:23 -0500

    function_auto_path ..\love\public\src\list_chunk.mjs

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -1,13 +1,13 @@
 import { list_each_by } from "../../../love/public/src/list_each_by.mjs";
 import { positive_is_assert } from "../../../love/public/src/positive_is_assert.mjs";
-import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
+import { list_slice_count } from "./list_slice_count.mjs";
 export function list_chunk(list, chunk_size) {
   positive_is_assert(chunk_size);
   function lambda(la) {
     list_each_by(list, lambda, chunk_size);
     function lambda(position) {
-      let sliced = list_slice(list, position, chunk_size);
+      let sliced = list_slice_count(list, position, chunk_size);
       la(sliced);
     }
   }
