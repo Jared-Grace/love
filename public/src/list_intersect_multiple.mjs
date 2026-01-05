f22129db9d2bf5c7b671a6a63c9a23c7ee690cc5
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:04:12 -0500

    function_auto_path ..\love\public\src\list_intersect_multiple.mjs

--- public/src/list_intersect_multiple.mjs
+++ public/src/list_intersect_multiple.mjs
@@ -7,7 +7,7 @@ import { list_unique_set } from "../../../love/public/src/list_unique_set.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-export function list_intersect_multiple(list, other) {
+export function list_intersect_multiple(list) {
   marker("1");
   let fr = list_first_remaining(list);
   let remaining = object_property_get(fr, "remaining");
