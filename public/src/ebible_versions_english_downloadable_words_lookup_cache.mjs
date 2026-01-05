b31d756d5d5bb237145f3e5d7266eaf4bfa7ba1b
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 22:53:36 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_words_lookup_cache.mjs

--- public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs
+++ public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs
@@ -1,4 +1,10 @@
+import { ebible_versions_english_downloadable_words_lookup } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup.mjs";
+import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_versions_english_downloadable_words_lookup_cache() {
+export async function ebible_versions_english_downloadable_words_lookup_cache() {
   marker("1");
+  return await invoke_cache_file(
+    ebible_versions_english_downloadable_words_lookup,
+    [],
+  );
 }
