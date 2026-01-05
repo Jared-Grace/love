7409cff2f2e419a3009df90eee4de4250d4bb521
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 19:03:50 -0500

    function_auto_path ..\love\public\src\ebible_version_symbols_unique.mjs

--- public/src/ebible_version_symbols_unique.mjs
+++ public/src/ebible_version_symbols_unique.mjs
@@ -1,7 +1,6 @@
+import { string_symbols_unique_multiple } from "../../../love/public/src/string_symbols_unique_multiple.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
-import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { string_symbols_unique_adder } from "../../../love/public/src/string_symbols_unique_adder.mjs";
 export async function ebible_version_symbols_unique(bible_folder) {
@@ -13,8 +12,7 @@ export async function ebible_version_symbols_unique(bible_folder) {
         chapter_code,
       });
       let mapped = list_map_property(verses, "text");
-      let joined = list_join_empty(mapped);
-      let unique_chapter = string_symbols_unique(joined);
+      let unique_chapter = string_symbols_unique_multiple(mapped);
       la(unique_chapter);
     }
     await ebible_chapters_each_verses_check_with(bible_folder, lambda2);
