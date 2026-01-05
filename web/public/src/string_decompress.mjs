249456883e0881d8cf8ca2cff58646486c675118
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:41:03 -0500

    function_auto_path ..\love\public\src\string_decompress.mjs

--- web/public/src/string_decompress.mjs
+++ web/public/src/string_decompress.mjs
@@ -1,10 +1,10 @@
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import LZString from "lz-string";
 export async function string_decompress(compressed) {
-  if (browser_is()) {
-  }
   let LZString = null;
-  LZString = (await import("lz-string")).default;
+  if (browser_is()) {
+    LZString = (await import("lz-string")).default;
+  }
   let v = LZString.decompressFromUTF16(compressed);
   return v;
 }
