4e1d24877bec144179fb3f05e6deba09e28ff17c
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 21:07:58 -0500

    rrl ebible_versions_english_downloadable_words_lookup search_upload

--- public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs
+++ public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs
@@ -1,10 +1,10 @@
-import { ebible_versions_english_downloadable_words_lookup } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup.mjs";
+import { ebible_versions_english_downloadable_words_search_upload } from "../../../love/public/src/ebible_versions_english_downloadable_words_search_upload.mjs";
 import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_downloadable_words_lookup_cache() {
   marker("1");
   let v = await invoke_cache_file(
-    ebible_versions_english_downloadable_words_lookup,
+    ebible_versions_english_downloadable_words_search_upload,
     [],
   );
   return v;
