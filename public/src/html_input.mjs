3d53b8512ee08d1e6f0d3821b977d2b183c1a2a5
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:21:01 -0500

    function_auto_path ..\love\public\src\html_input.mjs

--- public/src/html_input.mjs
+++ public/src/html_input.mjs
@@ -1,5 +1,7 @@
+import { html_style_font_size_inherit } from "../../../love/public/src/html_style_font_size_inherit.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 export function html_input(parent) {
   let component = html_element(parent, "input");
+  html_style_font_size_inherit(component);
   return component;
 }
