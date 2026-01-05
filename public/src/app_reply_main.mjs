9d5b0bdb38225b838c721e5e25672ea7d87cf839
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:39:03 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -125,7 +125,8 @@ export async function app_reply_main(context) {
           if (equal(bible_folder, right)) {
             bible_folder = list_pop(copy2);
           }
-          lambda11(() => {});
+          function lambda() {}
+          lambda11(lambda);
           let d = await ebible_verse(bible_folder, chapter_code, verse_number);
           return d;
         }
