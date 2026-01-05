4dda8bff7fa41f5752ec96b4f0d05c29b61bf402
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:16:56 -0500

    function_auto_path ..\love\public\src\list_chunk.mjs

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -6,12 +6,15 @@ import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_chunk(list, chunk_size) {
   positive_is_assert(chunk_size);
   let size = list_size(list);
-  let position = 0;
   function lambda(la) {
+    let position = 0;
     while (list_index_is(position)) {
+      lambda();
+      position += chunk_size;
+    }
+    function lambda() {
       let sliced = list_slice(list, position, chunk_size);
       la(sliced);
-      position += chunk_size;
     }
   }
   let chunks = list_adder(lambda);
