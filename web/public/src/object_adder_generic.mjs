6e875bc23d6900aea7a668a43ad71b4d5caf66d3
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:53:31 -0500

    function_auto_path ..\love\public\src\object_adder_generic.mjs

--- web/public/src/object_adder_generic.mjs
+++ web/public/src/object_adder_generic.mjs
@@ -1,10 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 export function object_adder_generic(fn_set) {
   marker("1");
   let result = {};
   let oa = function lambda(key, value) {
-    object_property_set_exists_not(result, key, value);
+    fn_set(result, key, value);
   };
   let r = {
     oa,
