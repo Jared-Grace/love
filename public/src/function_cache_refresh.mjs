a949019c7d641847a4d1f5321bf33c4656f8d83b
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:20:37 -0500

    function_auto_path ..\love\public\src\function_cache_refresh.mjs

--- public/src/function_cache_refresh.mjs
+++ public/src/function_cache_refresh.mjs
@@ -1,5 +1,4 @@
-import { file_delete } from "../../../love/public/src/file_delete.mjs";
-import { invoke_cache_file_key_get } from "../../../love/public/src/invoke_cache_file_key_get.mjs";
+import { invoke_cache_file_clear } from "../../../love/public/src/invoke_cache_file_clear.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_cache_name } from "../../../love/public/src/function_cache_name.mjs";
@@ -11,9 +10,7 @@ export async function function_cache_refresh(f_name) {
   let fn = await function_import(f_name);
   let v3 = await function_cache_name(f_name);
   let f_name_cache = object_property_get(v3, "f_name_cache");
-  let key_get = invoke_cache_file_key_get(fn, args);
-  let k = key_get();
-  await file_delete(k);
+  await invoke_cache_file_clear(fn, args);
   let result = await function_run(f_name_cache, args);
   return result;
 }
