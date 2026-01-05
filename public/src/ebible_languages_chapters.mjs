4fb73b6c72de24e29d3b57d615c23f3dbf9102b8
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:49:56 -0500

    function_auto_path ..\love\public\src\ebible_languages_chapters.mjs

--- public/src/ebible_languages_chapters.mjs
+++ public/src/ebible_languages_chapters.mjs
@@ -28,7 +28,8 @@ export async function ebible_languages_chapters() {
     await each_async(languages, lambda2);
     let i = await bible_interlinear_verses_cache();
     la(i);
-    await each_async(list, async function lambda3(item) {});
+    async function lambda3(item) {}
+    await each_async(list, lambda3);
   }
   let all = await list_adder_async(lambda);
   return all;
