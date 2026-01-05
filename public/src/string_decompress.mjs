bfa1c431f58a725088ca76fb13a79993807423cb
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:40:52 -0500

    function_auto_path ..\love\public\src\string_decompress.mjs

--- public/src/string_decompress.mjs
+++ public/src/string_decompress.mjs
@@ -1,5 +1,7 @@
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
+import LZString from "lz-string";
 export async function string_decompress(compressed) {
-  if (false) {
+  if (browser_is()) {
   }
   const LZString = (await import("lz-string")).default;
   let v = LZString.decompressFromUTF16(compressed);
