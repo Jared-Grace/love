247ecf9f70bfad14c3d741e03ef7885061cec29f
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:13:51 -0500

    de invoke_cache_clear_name

--- web/public/src/invoke_cache_clear_name.mjs
+++
@@ -1,6 +0,0 @@
-import { function_import } from "../../../love/public/src/function_import.mjs";
-import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
-export async function invoke_cache_clear_name(f_name) {
-  let imported_fn = await function_import(f_name);
-  invoke_cache_clear(imported_fn, []);
-}
