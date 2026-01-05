bb0441c46bc946def34af0019752829388ff0f31
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:52:29 -0500

    function_auto_path ..\love\public\src\object_adder_duplicates.mjs

--- web/public/src/object_adder_duplicates.mjs
+++ web/public/src/object_adder_duplicates.mjs
@@ -1,10 +1,10 @@
-import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_adder_generic } from "../../../love/public/src/object_adder_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function object_adder_duplicates(lambda$oad) {
   marker("1");
-  let v = object_adder_generic(object_property_set_exists_not);
+  let v = object_adder_generic(object_property_set);
   let result = object_property_get(v, "result");
   let oa = object_property_get(v, "oa");
   lambda$oad(oa);
