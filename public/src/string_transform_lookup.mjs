0009fcc288828698183f2ec8f40dce4754521cc4
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:03:44 -0500

    data_file_update ..\love\public\src\string_transform_lookup.mjs

--- public/src/string_transform_lookup.mjs
+++ public/src/string_transform_lookup.mjs
@@ -2,7 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { string_transform } from "../../../love/public/src/string_transform.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-export function string_transform_lookup(normalize, lower) {
+export function string_transform_lookup(lower, normalize) {
   marker("1");
   function lambda3(c) {
     let exists = object_property_exists(normalize, c);
