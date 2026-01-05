ed85051dc156137ec361c4311ca571b775454b69
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 19:47:26 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_cache.mjs

--- public/src/ebible_versions_english_downloadable_cache.mjs
+++ public/src/ebible_versions_english_downloadable_cache.mjs
@@ -3,6 +3,9 @@ import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mj
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_downloadable_cache() {
   marker("1");
-  let v = await invoke_cache_file(ebible_versions_english_downloadable, []);
-  return v;
+  let bible_folders = await invoke_cache_file(
+    ebible_versions_english_downloadable,
+    [],
+  );
+  return bible_folders;
 }
