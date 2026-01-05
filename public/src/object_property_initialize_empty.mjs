b4fcef9485966f85957aba447e19b6fbe3241b61
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 19:59:22 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ public/src/object_property_initialize_empty.mjs
@@ -0,0 +1,5 @@
+import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
+export function object_property_initialize_empty(result, chapter_code) {
+  let value2 = object_property_initialize(result, chapter_code, {});
+  return value2;
+}
