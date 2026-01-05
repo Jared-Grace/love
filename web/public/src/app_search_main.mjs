7b88942151cb7dbcf6b0fe8398060f01448f30b6
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:12:40 -0500

    function_auto_path ..\love\public\src\app_search_main.mjs

--- web/public/src/app_search_main.mjs
+++ web/public/src/app_search_main.mjs
@@ -1,9 +1,11 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_reply_languages_chosen_reset } from "../../../love/public/src/app_reply_languages_chosen_reset.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
 import { app_reply_buttons_languages } from "../../../love/public/src/app_reply_buttons_languages.mjs";
 import { app_reply_languages_prompt } from "../../../love/public/src/app_reply_languages_prompt.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 export function app_search_main() {
+  let root = object_property_get(context, "root");
   let languages = ebible_languages();
   let languages_chosen = [];
   let p = app_reply_languages_prompt(root);
