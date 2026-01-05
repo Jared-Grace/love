8c9242cb6154722eb55e1b49f1c68605c9156615
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:36:42 -0500

    function_auto_path ..\love\public\src\html_update_dev.mjs

--- public/src/html_update_dev.mjs
+++ public/src/html_update_dev.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_code_call_app_context_initialize } from "../../../love/public/src/js_code_call_app_context_initialize.mjs";
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
 import { function_code_import_dev } from "../../../love/public/src/function_code_import_dev.mjs";
@@ -25,6 +26,10 @@ export async function html_update_dev(name) {
   let middle = list_join_newline([code, code2, call]);
   let body = html_code_script_module(middle);
   var v = await html_update_externals(name_prefixed);
+  log({
+    v,
+    name_prefixed,
+  });
   let scripts = object_property_get(v, "scripts");
   let joined2 = list_add_join_newline(scripts, body);
   await html_overwrite(name, joined, joined2);
