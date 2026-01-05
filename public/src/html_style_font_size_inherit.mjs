5a30b8b278c726e0ae28ae3a553f55c7926ff772
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:20:40 -0500

    function_auto_path ..\love\public\src\html_button_notext.mjs

---
+++ public/src/html_style_font_size_inherit.mjs
@@ -0,0 +1,4 @@
+import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
+export function html_style_font_size_inherit(component) {
+  html_style_font_size(component, "inherit");
+}
