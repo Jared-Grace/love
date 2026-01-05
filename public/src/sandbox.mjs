1c8ab2dde4c7807c9e7ba6adc697cf5d190ffd05
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 19:46:52 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,14 @@
+import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
 import { ebible_versions_english_downloadable_cache } from "../../../love/public/src/ebible_versions_english_downloadable_cache.mjs";
 import { string_only_or_space } from "../../../love/public/src/string_only_or_space.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   const versions = await ebible_versions_english_downloadable_cache();
+  await ebible_chapters_each_verses_check_with(
+    bible_folder,
+    async function lambda(chapter_code, verses) {},
+  );
   let symbols_all =
     "._​ּׁׂ -–—,;:!?…·'‘’\"“”()[]{}¶*/&#%•`°+=|⌃⌞⌟►◄$01½¼23¾456789aAæÆbBcCdDeEéèëfFﬁﬂgGhHiIïjJkKlLmMnNoOöœpPqQrRsStTuUüvVʋwWxXyYzZʼΑΩאבגדהוזחטיכלמנסעפצקרשת";
   let normalize = {
