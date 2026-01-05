d67d497745d146dbadcf615c68b4b8825664c19a
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 18:49:31 -0500

    function_auto_path ..\love\public\src\ebible_languages_chapters.mjs

--- web/public/src/ebible_languages_chapters.mjs
+++ web/public/src/ebible_languages_chapters.mjs
@@ -1,9 +1,9 @@
+import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { bible_interlinear_verses_cache } from "../../../love/public/src/bible_interlinear_verses_cache.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
 import { bible_interlinear_verses_upload_folder } from "../../../love/public/src/bible_interlinear_verses_upload_folder.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
@@ -13,9 +13,10 @@ export async function ebible_languages_chapters() {
     async function lambda2(language) {
       let bible_folder = object_property_get(language, "bible_folder");
       let v = bible_interlinear_verses_upload_folder();
-      let right = [v];
-      let includes = list_includes(list, item);
-      if (equal(bible_folder, right)) {
+      let v2 = ebible_folder_english();
+      let skips = [v, v2];
+      let includes = list_includes(skips, bible_folder);
+      if (includes) {
         return;
       }
       let chapters = await ebible_version_chapters_cache(bible_folder);
