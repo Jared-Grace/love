20b08164f51ed97a9a27157d1a82620a74e935bb
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:02:17 -0500

    function_auto_path ..\love\public\src\string_only_or.mjs

---
+++ web/public/src/string_transform.mjs
@@ -0,0 +1,9 @@
+import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { list_to } from "../../../love/public/src/list_to.mjs";
+export function string_transform(s, lambda$c) {
+  let l = list_to(s);
+  let mapped = list_map(l, lambda$c);
+  let joined = list_join_empty(mapped);
+  return joined;
+}
