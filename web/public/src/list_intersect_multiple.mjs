a2375ab8205a7204d8e7fdc2f79390dbd5939ab6
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:30:09 -0500

    data_file_update ..\love\public\src\list_intersect_multiple.mjs

--- web/public/src/list_intersect_multiple.mjs
+++ web/public/src/list_intersect_multiple.mjs
@@ -10,7 +10,7 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_intersect_multiple(list) {
   marker("1");
   let fr = list_first_remaining(list);
-  let remaining = object_property_get(fr, "remaining");
+  let remaining = object_property_get(fr, "remaining");lei
   let first = object_property_get(fr, "first");
   let uniques = list_map(remaining, list_unique_set);
   function lambda2(la) {
