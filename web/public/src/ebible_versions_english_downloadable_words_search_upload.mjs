b3c65cb21b5f245360545c2d6557dea95c2c15ab
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:34:57 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_words_search_upload.mjs

--- web/public/src/ebible_versions_english_downloadable_words_search_upload.mjs
+++ web/public/src/ebible_versions_english_downloadable_words_search_upload.mjs
@@ -6,7 +6,6 @@ import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
 import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
 import { app_bible_search_word_path } from "../../../love/public/src/app_bible_search_word_path.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
 import { ebible_versions_english_downloadable_words_lookup_cache } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -23,10 +22,6 @@ export async function ebible_versions_english_downloadable_words_search_upload()
         return properties;
       }
       let m = object_values_map(value, lambda);
-      log({
-        key,
-      });
-      return;
       await firebase_upload_object_compressed(destination, m);
     }
     await each_unordered_async(c, lambda4);
