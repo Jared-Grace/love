6519f6d6d5f1cc2c7770dc6bcbeda513cb757c53
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:10:01 -0500

    function_auto_path ..\love\public\src\app_search_main.mjs

--- web/public/src/app_search_main.mjs
+++ web/public/src/app_search_main.mjs
@@ -1,4 +1,6 @@
+import { app_reply_languages_prompt } from "../../../love/public/src/app_reply_languages_prompt.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 export function app_search_main() {
   let languages = ebible_languages();
+  let p = app_reply_languages_prompt(root);
 }
