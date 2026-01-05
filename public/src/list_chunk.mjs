61ff2e37012be6e7de8e04a8ccb029eee2ce1922
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:17:55 -0500

    data_file_update ..\love\public\src\list_each_by.mjs

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -5,8 +5,8 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_chunk(list, chunk_size) {
   positive_is_assert(chunk_size);
   function lambda(la) {
-    let position = list_each_by(list, lambda, chunk_size);
-    function lambda() {
+    list_each_by(list, lambda, chunk_size);
+    function lambda(position) {
       let sliced = list_slice(list, position, chunk_size);
       la(sliced);
     }
