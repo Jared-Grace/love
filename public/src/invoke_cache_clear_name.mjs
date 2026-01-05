c48ee4932f4bf5987d199c0f50502c7d6afd577f
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:11:52 -0500

    function_auto_path ..\love\public\src\invoke_cache_clear_name.mjs

--- public/src/invoke_cache_clear_name.mjs
+++ public/src/invoke_cache_clear_name.mjs
@@ -1,9 +1,6 @@
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function invoke_cache_clear_name(f_name, args) {
+export async function invoke_cache_clear_name(f_name) {
   let imported_fn = await function_import(f_name);
-  marker("1");
-  let v = invoke_cache_clear(fn, args);
-  return v;
+  invoke_cache_clear(imported_fn, []);
 }
