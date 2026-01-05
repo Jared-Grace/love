d7c2258bf522ad0cbf23fc29a253637b5dd5f79d
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:02:17 -0500

    function_auto_path ..\love\public\src\string_only_or.mjs

--- public/src/string_only_or.mjs
+++ public/src/string_only_or.mjs
@@ -1,9 +1,7 @@
-import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
+import { string_transform } from "../../../love/public/src/string_transform.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
-import { list_to } from "../../../love/public/src/list_to.mjs";
 export function string_only_or(s, valid, replacement) {
-  let l = list_to(s);
+  let joined = string_transform(s, lambda);
   function lambda(item) {
     let i = string_includes(valid, item);
     if (i) {
@@ -12,7 +10,5 @@ export function string_only_or(s, valid, replacement) {
     let v = replacement;
     return v;
   }
-  let mapped = list_map(l, lambda);
-  let joined = list_join_empty(mapped);
   return joined;
 }
