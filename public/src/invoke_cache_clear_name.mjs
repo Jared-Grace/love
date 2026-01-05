9801beed5849b1f140e68af04dd7eae59a6f5eb6
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:10:46 -0500

    fws invoke_cache_clear name

--- public/src/invoke_cache_clear_name.mjs
+++ public/src/invoke_cache_clear_name.mjs
@@ -1,4 +1,6 @@
+import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function invoke_cache_clear_name() {
+export function invoke_cache_clear_name(fn, args) {
   marker("1");
+  return invoke_cache_clear(fn, args);
 }
