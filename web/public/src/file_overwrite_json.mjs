0d3860ac8f962381e2b60168921cc65714d20615
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 22:26:03 -0500

    function_auto_path ..\love\public\src\file_overwrite_json.mjs

--- web/public/src/file_overwrite_json.mjs
+++ web/public/src/file_overwrite_json.mjs
@@ -4,15 +4,15 @@ import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
 export async function file_overwrite_json(file_path, object) {
   if (browser_is()) {
+    let json = json_format_to(object);
+    await file_overwrite(file_path, json);
   } else {
+    async function writeBigJson(largeObject) {
+      const out = fs.createWriteStream("big.json");
+      let v = streamJsonStringify(largeObject);
+      await pipeline(v, out);
+      console.log("JSON write complete");
+    }
+    await writeBigJson(largeObject);
   }
-  let json = json_format_to(object);
-  await file_overwrite(file_path, json);
-  async function writeBigJson(largeObject) {
-    const out = fs.createWriteStream("big.json");
-    let v = streamJsonStringify(largeObject);
-    await pipeline(v, out);
-    console.log("JSON write complete");
-  }
-  await writeBigJson(largeObject);
 }
