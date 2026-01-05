3a7946c44faf855f66dc342c3cb2c6433bcd048d
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:52:59 -0500

    function_auto_path ..\love\public\src\app_search_main.mjs

--- public/src/app_search_main.mjs
+++ public/src/app_search_main.mjs
@@ -1,3 +1,4 @@
+import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src/ebible_parts_chapter_code_to_reference.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { json_decompress_object } from "../../../love/public/src/json_decompress_object.mjs";
@@ -20,9 +21,10 @@ import { app_reply_languages_chosen_reset } from "../../../love/public/src/app_r
 import { app_reply_buttons_languages } from "../../../love/public/src/app_reply_buttons_languages.mjs";
 import { app_reply_languages_prompt } from "../../../love/public/src/app_reply_languages_prompt.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
-export function app_search_main(context) {
+export async function app_search_main(context) {
   let root = html_mobile_default(context);
   firebase_name_jg();
+  let books = await ebible_version_books(version_english);
   let languages = ebible_languages();
   let languages_chosen = [];
   let languages_chosen_default = list_slice_count(languages, 1, 1);
