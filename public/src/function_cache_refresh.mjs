6de58e419f85f05a1a43b828ac2889af6cba865f
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:19:37 -0500

    function_auto_path ..\love\public\src\function_cache_refresh.mjs

--- public/src/function_cache_refresh.mjs
+++ public/src/function_cache_refresh.mjs
@@ -1,4 +1,4 @@
-import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
+import { invoke_cache_file_key_get } from "../../../love/public/src/invoke_cache_file_key_get.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_cache_name } from "../../../love/public/src/function_cache_name.mjs";
@@ -10,7 +10,7 @@ export async function function_cache_refresh(f_name) {
   let fn = await function_import(f_name);
   let v3 = await function_cache_name(f_name);
   let f_name_cache = object_property_get(v3, "f_name_cache");
-  invoke_cache_clear(fn, args);
+  let key_get = invoke_cache_file_key_get(fn, args);
   let result = await function_run(f_name_cache, args);
   return result;
 }
