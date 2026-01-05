4b43d23e91bc31e1f5f3011c0cf936d526d8e54a
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 19:51:37 -0500

    function_auto_path ..\love\public\src\string_only_or_space.mjs

--- public/src/string_only_or_space.mjs
+++ public/src/string_only_or_space.mjs
@@ -1,5 +1,5 @@
 import { string_only_or } from "../../../love/public/src/string_only_or.mjs";
 export function string_only_or_space(s, symbols_split_non) {
-  let joined2 = string_only_or(s, symbols_split_non, " ");
-  return joined2;
+  let replaced = string_only_or(s, symbols_split_non, " ");
+  return replaced;
 }
