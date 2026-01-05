557976bc180c969140db62a40208e0e5a578608b
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 20:11:08 -0500

    function_auto_path ..\love\public\src\invoke_cache_clear_name.mjs

--- public/src/invoke_cache_clear_name.mjs
+++ public/src/invoke_cache_clear_name.mjs
@@ -1,6 +1,8 @@
+import { function_is } from "../../../love/public/src/function_is.mjs";
 import { invoke_cache_clear } from "../../../love/public/src/invoke_cache_clear.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function invoke_cache_clear_name(fn, args) {
+  let fi2 = function_is(f);
   marker("1");
   let v = invoke_cache_clear(fn, args);
   return v;
