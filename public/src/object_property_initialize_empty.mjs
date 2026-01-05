997bad12297aa5456d44e957b443086c36ce446f
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 19:59:40 -0500

    function_auto_path ..\love\public\src\object_property_initialize_empty.mjs

--- public/src/object_property_initialize_empty.mjs
+++ public/src/object_property_initialize_empty.mjs
@@ -1,5 +1,5 @@
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
-export function object_property_initialize_empty(result, chapter_code) {
-  let value2 = object_property_initialize(result, chapter_code, {});
+export function object_property_initialize_empty(object, property_name) {
+  let value2 = object_property_initialize(object, property_name, {});
   return value2;
 }
