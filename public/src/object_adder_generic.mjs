6d13b251c3ceafb792ecd0058693b1e689ab2943
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:51:41 -0500

    pn fn_set object_property_set_exists_not

--- public/src/object_adder_generic.mjs
+++ public/src/object_adder_generic.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
-export function object_adder_generic() {
+export function object_adder_generic(fn_set) {
   marker("1");
   let result = {};
   let oa = function lambda(key, value) {
