1ed5f997c72d650289ef1e8b0493e06938e63b29
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:06:03 -0500

    function_auto_path ..\love\public\src\app_search.mjs

--- public/src/app_search.mjs
+++ public/src/app_search.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_search() {
   await app_main_production("app_search_main", "jared-grace");
 }
