f783ce9808890537d8a37d0e1a9d9c94b4f93b62
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 14:46:31 -0500

    fs firebase_upload_object_compressed

--- web/public/src/firebase_upload_object_compressed.mjs
+++ web/public/src/firebase_upload_object_compressed.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { json_compress_object } from "../../../love/public/src/json_compress_object.mjs";
 export async function firebase_upload_object_compressed(value, destination) {
+  marker("1");
   let c = await json_compress_object(value);
   await firebase_upload_object(destination, c);
 }
