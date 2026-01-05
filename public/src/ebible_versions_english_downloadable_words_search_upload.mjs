5a9a3f1e555624a337a30d28fd0d2cc9020e25f6
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 09:14:21 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_words_search_upload.mjs

--- public/src/ebible_versions_english_downloadable_words_search_upload.mjs
+++ public/src/ebible_versions_english_downloadable_words_search_upload.mjs
@@ -1,4 +1,4 @@
-import { ebible_versions_english_downloadable_words_lookup } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup.mjs";
+import { ebible_versions_english_downloadable_words_lookup_cache } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { list_join_slash_forward } from "../../../love/public/src/list_join_slash_forward.mjs";
@@ -6,7 +6,7 @@ import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
 export async function ebible_versions_english_downloadable_words_search_upload() {
   marker("1");
-  let result = await ebible_versions_english_downloadable_words_lookup();
+  let result = await ebible_versions_english_downloadable_words_lookup_cache();
   async function lambda3(value, word) {
     let file_name_with_extension = file_name_json(word);
     let destination = list_join_slash_forward([
