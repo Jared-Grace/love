8679fd9a73b0ab92accecb90cf5a40b9cdb02e94
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 21:07:58 -0500

    rrl ebible_versions_english_downloadable_words_lookup search_upload

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,7 @@
-import { ebible_versions_english_downloadable_words_lookup } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup.mjs";
+import { ebible_versions_english_downloadable_words_search_upload } from "../../../love/public/src/ebible_versions_english_downloadable_words_search_upload.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
-  let result = await ebible_versions_english_downloadable_words_lookup();
+  let result = await ebible_versions_english_downloadable_words_search_upload();
   return result;
 }
