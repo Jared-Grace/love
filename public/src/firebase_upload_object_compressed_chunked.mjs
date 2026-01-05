dbaf4908c8e9ad775961286d1fed0391deedbc3e
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:40:46 -0500

    function_auto_path ..\love\public\src\firebase_upload_object_compressed_chunked.mjs

--- public/src/firebase_upload_object_compressed_chunked.mjs
+++ public/src/firebase_upload_object_compressed_chunked.mjs
@@ -1,4 +1,3 @@
-import { list_size } from "../../../love/public/src/list_size.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
@@ -6,7 +5,6 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 export async function firebase_upload_object_compressed_chunked(list, get) {
   let cs = list_chunk(list, 20);
-  let size = list_size(list2);
   async function lambda2(c) {
     async function lambda4(i) {
       let v4 = get(i);
