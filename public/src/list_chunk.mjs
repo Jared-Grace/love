9c844d273ca812991149e9d007413db6485fe27e
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:11:02 -0500

    function_auto_path ..\love\public\src\list_chunk.mjs

--- public/src/list_chunk.mjs
+++ public/src/list_chunk.mjs
@@ -1,11 +1,13 @@
+import { assert } from "../../../love/public/src/assert.mjs";
 import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_chunk(list, chunk_size) {
+  assert(b2);
   let size = list_size(list);
   let position = 0;
   function lambda(la) {
-    while (size > chunk_size) {
+    while (position < chunk_size) {
       let sliced = list_slice(list3, a, b);
     }
   }
