5a7fa4ac6996ece320dc2a51c8b78742a7ddd64d
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:37:51 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_words_search_upload.mjs

--- web/public/src/ebible_versions_english_downloadable_words_search_upload.mjs
+++ web/public/src/ebible_versions_english_downloadable_words_search_upload.mjs
@@ -1,33 +1,16 @@
+import { firebase_upload_object_compressed_chunked } from "../../../love/public/src/firebase_upload_object_compressed_chunked.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { sleep } from "../../../love/public/src/sleep.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
-import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
 import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
 import { app_bible_search_word_path } from "../../../love/public/src/app_bible_search_word_path.mjs";
-import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
 import { ebible_versions_english_downloadable_words_lookup_cache } from "../../../love/public/src/ebible_versions_english_downloadable_words_lookup_cache.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_versions_english_downloadable_words_search_upload() {
   marker("1");
   let result = await ebible_versions_english_downloadable_words_lookup_cache();
   let v2 = object_to_list(result);
-  let cs = list_chunk(v2, 20);
-  async function lambda2(c) {
-    async function lambda4(i) {
-      let v4 = get(i);
-      let value = object_property_get(v4, "value");
-      let destination = object_property_get(v4, "destination");
-      return;
-      await firebase_upload_object_compressed(destination, value);
-    }
-    await each_unordered_async(c, lambda4);
-    await sleep(2000);
-  }
-  await each_async(cs, lambda2);
+  await firebase_upload_object_compressed_chunked(v2, get);
   function get({ value: v, key }) {
     log({
       key,
