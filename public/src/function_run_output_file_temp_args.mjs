be95d2458d474ecf3c3cc0d32986d0779580a93b
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:38:54 -0500

    function_auto_path ..\love\public\src\function_run_output_file_temp_args.mjs

--- public/src/function_run_output_file_temp_args.mjs
+++ public/src/function_run_output_file_temp_args.mjs
@@ -1,7 +1,9 @@
+import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { function_run_output_file_temp_generic } from "../../../love/public/src/function_run_output_file_temp_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_run_output_file_temp_args(f_name) {
+export async function function_run_output_file_temp_args(f_name, args_comma) {
   marker("1");
+  let split = string_split_comma(f_names);
   const args = [];
   await function_run_output_file_temp_generic(f_name, args);
 }
