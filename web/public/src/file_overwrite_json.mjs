ed7ee3b7176079f697b1c67649e4ea2f53949e12
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:04:31 -0500

    function_auto_path ..\love\public\src\file_overwrite_json.mjs

--- web/public/src/file_overwrite_json.mjs
+++ web/public/src/file_overwrite_json.mjs
@@ -3,12 +3,16 @@ import { import_install } from "../../../love/public/src/import_install.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
+import { json_to } from "./json_to.mjs";
 export async function file_overwrite_json(file_path, object) {
   if (browser_is()) {
     let json = json_format_to(object);
     await file_overwrite(file_path, json);
     return;
   }
+  "Using " +
+    json_to.name +
+    " did not work on sufficiently large object, whereas this did:";
   let fs = await import("fs");
   const v = await import("stream/promises");
   let pipeline = object_property_get(v, "pipeline");
