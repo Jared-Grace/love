82c5498312fd0a89e2f22198eb6486a51d5d61e1
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 16:01:49 -0500

    function_auto_path ..\love\public\src\list_intersect_multiple.mjs

--- web/public/src/list_intersect_multiple.mjs
+++ web/public/src/list_intersect_multiple.mjs
@@ -1,4 +1,3 @@
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { set_includes } from "../../../love/public/src/set_includes.mjs";
 import { list_unique_set } from "../../../love/public/src/list_unique_set.mjs";
@@ -7,9 +6,7 @@ import { each } from "../../../love/public/src/each.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_intersect_multiple(list, other) {
   marker("1");
-  let v = list_first_remaining(list);
-  let remainin = object_property_get(v, "remainin");
-  let first = object_property_get(v, "first");
+  let {first,remaining} = list_first_remaining(list);
   let set = list_unique_set(other);
   function lambda2(la) {
     function lambda(l) {
