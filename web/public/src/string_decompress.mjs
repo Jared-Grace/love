949aaec25a5188d1d68cd628f033529a867fb225
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 15:40:29 -0500

    function_auto_path ..\love\public\src\string_decompress.mjs

--- web/public/src/string_decompress.mjs
+++ web/public/src/string_decompress.mjs
@@ -1,4 +1,6 @@
 export async function string_decompress(compressed) {
+  if (false) {
+  }
   const LZString = (await import("lz-string")).default;
   let v = LZString.decompressFromUTF16(compressed);
   return v;
