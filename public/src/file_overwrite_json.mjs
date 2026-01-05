21b0a9b280723bc0447acffbcd5b58cd6ced8dc8
Grace-Jared <christrosetolife@gmail.com>
2026-01-04 22:25:49 -0500

    function_auto_path ..\love\public\src\file_overwrite_json.mjs

--- public/src/file_overwrite_json.mjs
+++ public/src/file_overwrite_json.mjs
@@ -4,7 +4,7 @@ import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
 export async function file_overwrite_json(file_path, object) {
   if (browser_is()) {
-  }
+  }$e
   let json = json_format_to(object);
   await file_overwrite(file_path, json);
   async function writeBigJson(largeObject) {
