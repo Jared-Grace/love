fa227ec6a1140bed479ff44263d9143b9c4fd697
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 17:20:36 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,11 @@
+import { range } from "../../../love/public/src/range.mjs";
 import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 import { ebible_versions_english_downloadable_words_search_upload } from "../../../love/public/src/ebible_versions_english_downloadable_words_search_upload.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   let result = await ebible_versions_english_downloadable_words_search_upload();
-  let chunks = list_chunk(list, chunk_size);
-  return result;
+  let r = range(count);
+  let chunks = list_chunk(result, 20);
+  return chunks;
 }
