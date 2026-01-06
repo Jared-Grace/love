a938ec7828cb381b97305333d302b8835fc281ba
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 13:43:17 -0500

    function_auto_path ..\love\public\src\app_search_home.mjs

--- public/src/app_search_home.mjs
+++ public/src/app_search_home.mjs
@@ -34,7 +34,7 @@ export function app_search_home(context) {
   let input = html_input_text(root, search_instructions);
   html_width_full(input);
   html_focus(input);
-  html_value_set(input, value);
+  html_value_set(input, "believe");
   const text = "Search";
   html_button_width_full(root, text, search);
   let div_results = html_div(root);
