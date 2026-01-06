893f4e0d6242867964f3397d77063044bdc13250
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 10:55:58 -0500

    function_auto_path ..\love\public\src\firebase_deploy_function_app_main_promote.mjs

--- public/src/firebase_deploy_function_app_main_promote.mjs
+++ public/src/firebase_deploy_function_app_main_promote.mjs
@@ -1,7 +1,9 @@
+import { app_promote } from "../../../love/public/src/app_promote.mjs";
 import { firebase_deploy_function_app_main } from "../../../love/public/src/firebase_deploy_function_app_main.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_deploy_function_app_main_promote(a) {
   marker("1");
+  let v2 = await app_promote(name);
   let v = await firebase_deploy_function_app_main(a);
   return v;
 }
