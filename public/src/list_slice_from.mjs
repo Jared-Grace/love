0cfa6b42a4885b16da48dd3c426169fc5e2227e8
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 17:58:35 -0500

    function_auto_path ..\love\public\src\list_slice_from.mjs

--- public/src/list_slice_from.mjs
+++ public/src/list_slice_from.mjs
@@ -1,17 +1,18 @@
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
 import { list_sort_number } from "../../../love/public/src/list_sort_number.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { list_slice_include } from "../../../love/public/src/list_slice_include.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { list_last } from "./list_last.mjs";
 export function list_slice_from(list, item_from, item_to) {
   marker("1");
   let mapped = list_map([item_from, item_to], list_index_of);
   list_sort_number(mapped);
   let v = list_first_last(list);
-  let last = object_property_get(v, "last");
-  let first = object_property_get(v, "first");
+  let last = list_first(v, "last");
+  let first = list_last(v, "first");
   let sliced = list_slice_include(list, first, last);
   return sliced;
 }
