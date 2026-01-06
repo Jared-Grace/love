27e60568cf0aec6abded5c28dcd91856d849f00e
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 17:38:51 -0500

    oa search

--- web/public/src/app_search_main.mjs
+++ web/public/src/app_search_main.mjs
@@ -1,8 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { app_search_home } from "../../../love/public/src/app_search_home.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
 export async function app_search_main(context) {
+  marker("1");
   html_mobile_default(context);
   firebase_name_jg();
   let languages_chosen = [];
