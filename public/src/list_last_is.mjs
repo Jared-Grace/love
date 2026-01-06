a7522dc4b9e7839103966a87dc2f3e0099e8f1be
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:39:14 -0500

    function_auto_path ..\love\public\src\list_last_is.mjs

--- public/src/list_last_is.mjs
+++ public/src/list_last_is.mjs
@@ -1,6 +1,6 @@
 import { list_last } from "../../../love/public/src/list_last.mjs";
 export function list_last_is(list, item) {
   let last = list_last(list);
-  let v = last === item;
-  return v;
+  let li = last === item;
+  return li;
 }
