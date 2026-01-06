af7316ee29f91332b67a9bfc79c5ebd26ac9ed57
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:02:32 -0500

    function_auto_path ..\love\public\src\list_slice_from.mjs

--- web/public/src/list_slice_from.mjs
+++ web/public/src/list_slice_from.mjs
@@ -1,3 +1,4 @@
+import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
 import { list_sort_number } from "../../../love/public/src/list_sort_number.mjs";
@@ -7,7 +8,9 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { list_last } from "./list_last.mjs";
 export function list_slice_from(list, item_from, item_to) {
   marker("1");
-  function lambda() {list_index_of}
+  function lambda() {
+    let index = list_index_of(list2, item);
+  }
   let mapped = list_map([item_from, item_to], lambda);
   list_sort_number(mapped);
   let v = list_first_last(list);
