733eb4bde8965199a92ea98b5278c04724bd2d29
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:19:46 -0500

    function_auto_path ..\love\public\src\list_map_find_property.mjs

--- web/public/src/list_map_find_property.mjs
+++ web/public/src/list_map_find_property.mjs
@@ -1,8 +1,8 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
-export function list_map_find_property(lv, property, sliced) {
+export function list_map_find_property(list_other, property, sliced) {
   function lambda4(item) {
-    let item2 = list_find_property(lv, property, item);
+    let item2 = list_find_property(list_other, property, item);
     return item2;
   }
   let mapped2 = list_map(sliced, lambda4);
