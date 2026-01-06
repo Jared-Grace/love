cebc7e813650f1489629847beaf1f306c87946f2
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:21:01 -0500

    function_auto_path ..\love\public\src\app_chapter_main.mjs

--- web/public/src/app_chapter_main.mjs
+++ web/public/src/app_chapter_main.mjs
@@ -81,8 +81,7 @@ export async function app_chapter_main(context) {
       let last = list_first(v);
       let first = list_last(v);
       let sliced = list_slice_from(verse_numbers, first, last);
-      const property = "verse_number";
-      let mapped2 = list_map_find_property(sliced, lv, property);
+      let mapped2 = list_map_find_property(sliced, lv, "verse_number");
       let mapped3 = list_map_property(mapped2, "text");
     }
     each(languages_verses, lambda3);
