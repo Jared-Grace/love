a7495e3ca4c3e1e684decbbe6fc1b386dcb52d52
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:13:42 -0500

    fs ebible_language_to_bible_folder

--- web/public/src/ebible_language_to_bible_folder.mjs
+++ web/public/src/ebible_language_to_bible_folder.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 export function ebible_language_to_bible_folder(languages_list2, language) {
+  marker("1");
   let languages_list = ebible_languages();
   let filtered = list_find_property(languages_list, "language_code", language);
   let bible_folder = object_property_get(filtered, "bible_folder");
