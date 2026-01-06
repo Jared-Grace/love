b22703a01c17cfb27e3db932c4f4817e41eec054
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:20:37 -0500

    p

--- public/src/list_map_find_property.mjs
+++ public/src/list_map_find_property.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 export function list_map_find_property(list_other, property, list) {
+  marker("1");
   function lambda4(item) {
     let r = list_find_property(list_other, property, item);
     return r;
