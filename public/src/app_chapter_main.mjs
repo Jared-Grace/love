28bab96e40bbd699bc8a6ec66a0c16182c08f7b5
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:08:37 -0500

    function_auto_path ..\love\public\src\app_chapter_main.mjs

--- public/src/app_chapter_main.mjs
+++ public/src/app_chapter_main.mjs
@@ -1,13 +1,15 @@
+import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { app_next_hash_to_languages_chosen } from "../../../love/public/src/app_next_hash_to_languages_chosen.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_chapter_main() {
+export async function app_chapter_main() {
   marker("1");
   firebase_name_jg();
   let hash = html_hash_object_get();
   let chapter_code = object_property_get(hash, "c");
   let verse_number = object_property_get(hash, "v");
   let languages_chosen = app_next_hash_to_languages_chosen(hash);
+  let list = await ebible_verses(bible_folder, chapter_code2);
 }
