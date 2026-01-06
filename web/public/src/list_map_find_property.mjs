c6874b373e85360af8733c438c46171aeddfaac3
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:20:52 -0500

    function_param_move_first list

--- web/public/src/list_map_find_property.mjs
+++ web/public/src/list_map_find_property.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
-export function list_map_find_property(list_other, property, list) {
+export function list_map_find_property(list, list_other, property) {
   marker("1");
   function lambda4(item) {
     let r = list_find_property(list_other, property, item);
