e807a9ced138f28fdf5af07fac1ceab885b9e304
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:41:24 -0500

    function_auto_path ..\love\public\src\list_toggle.mjs

--- public/src/list_toggle.mjs
+++ public/src/list_toggle.mjs
@@ -1,11 +1,11 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-export function list_toggle(languages_chosen, language) {
-  let includes2 = list_includes(languages_chosen, language);
+export function list_toggle(chosen, item) {
+  let includes2 = list_includes(chosen, item);
   if (includes2) {
-    list_remove(languages_chosen, language);
+    list_remove(chosen, item);
   } else {
-    list_add(languages_chosen, language);
+    list_add(chosen, item);
   }
 }
