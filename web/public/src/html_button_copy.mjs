25c0f704e9f9d9705728c79e5890b2a46a746750
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 09:55:46 -0500

    function_auto_path ..\love\public\src\html_button_copy.mjs

--- web/public/src/html_button_copy.mjs
+++ web/public/src/html_button_copy.mjs
@@ -1,7 +1,7 @@
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { html_button_copy_text } from "../../../love/public/src/html_button_copy_text.mjs";
 export function html_button_copy(root, lambda12) {
-  let text3 = html_button_copy_text();
-  let component = html_button(root, text3, lambda12);
+  let t = html_button_copy_text();
+  let component = html_button(root, t, lambda12);
   return component;
 }
