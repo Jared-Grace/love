05237e8aaeccee2988195df09710964cf78848aa
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:29:45 -0500

    data_file_update ..\love\public\src\list_first_last.mjs

--- web/public/src/list_first_last.mjs
+++ web/public/src/list_first_last.mjs
@@ -2,9 +2,8 @@ import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_first_last(list) {
-  marker("1");
+  marker("1");lmi
   let first = list_first(list);
   let last = list_last(list);
-  let v = [first, last];
-  return v;
+  return [first, last];
 }
