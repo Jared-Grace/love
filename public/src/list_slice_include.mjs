c4353e1ad2bc00d327c523ba35d284a47a878d45
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 17:51:44 -0500

    function_auto_path ..\love\public\src\list_slice_include.mjs

--- public/src/list_slice_include.mjs
+++ public/src/list_slice_include.mjs
@@ -2,6 +2,6 @@ import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_slice_include(list, a, b) {
   marker("1");
-  let sliced = list_slice(list, a, b);
+  let sliced = list_slice(list, a, b + 1);
   return sliced;
 }
