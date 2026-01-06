181525adef9e6b374cceb2c429816c1fb13e32b3
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 09:25:14 -0500

    function_auto_path ..\love\public\src\app_search_main.mjs

--- public/src/app_search_main.mjs
+++ public/src/app_search_main.mjs
@@ -90,7 +90,7 @@ export async function app_search_main(context) {
     }
     let dictionary = list_to_dictionary_value(chapter_codes_match, lambda5);
     html_clear(root);
-    let p3 = html_p_text(root2, text2);
+    let p3 = html_p_text(root, value);
     async function back() {
       await app_search_main(context);
     }
