fbe83ccb5f4b5e1032748c9b9fc043184405a56f
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:49:57 -0500

    function_auto_path ..\love\public\src\app_search_results.mjs

---
+++ public/src/window_open_app.mjs
@@ -0,0 +1,6 @@
+import { window_open } from "../../../love/public/src/window_open.mjs";
+import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
+export function window_open_app(app_fn) {
+  let file_name = file_name_html(app_fn.name);
+  window_open(file_name);
+}
