10336b3efa398e5df0c92d46a3d373efaf8f858c
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 15:29:28 -0500

    function_auto_path ..\love\public\src\ebible_chapters_upload.mjs

--- public/src/ebible_chapters_upload.mjs
+++ public/src/ebible_chapters_upload.mjs
@@ -1,8 +1,6 @@
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function ebible_chapters_upload(
-  bible_folder,
-) {
+export async function ebible_chapters_upload(bible_folder) {
   marker("1");
   async function lambda(chapter_code, verses) {}
   let v = await ebible_chapters_each_verses_check_with(bible_folder, lambda);
