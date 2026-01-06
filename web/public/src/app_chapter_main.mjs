7dcab1c5888ddd4c04b520239c8f88e33ac83e37
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 15:21:29 -0500

    function_auto_path ..\love\public\src\app_chapter_main.mjs

--- web/public/src/app_chapter_main.mjs
+++ web/public/src/app_chapter_main.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,4 +6,5 @@ export function app_chapter_main() {
   marker("1");
   firebase_name_jg();
   let hash = html_hash_object_get();
+  let chapter_code = object_property_get(hash, "c");
 }
