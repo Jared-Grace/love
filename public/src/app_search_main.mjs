d922089bd7e44f796308fe2d228ca3d865d18494
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 19:22:10 -0500

    function_auto_path ..\love\public\src\app_search_main.mjs

--- public/src/app_search_main.mjs
+++ public/src/app_search_main.mjs
@@ -132,8 +132,8 @@ export async function app_search_main(context) {
         let b = null;
         async function lambda3() {
           html_remove(b);
-          html_button_copy(div_verse, lambda12);
-          html_width_full(component);
+          let c = html_button_copy(div_verse, lambda12);
+          html_width_full(c);
           let p3 = html_p_text(div_verse, reference);
           let o = await ebible_verse_download(
             "engbsb",
