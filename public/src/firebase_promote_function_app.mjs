484114418b4cc40e28029d3ba998281e270fd2bc
Grace-Jared <christrosetolife@gmail.com>
2026-01-05 23:02:18 -0500

    data_file_update ..\love\public\src\firebase_promote_function_app.mjs

--- public/src/firebase_promote_function_app.mjs
+++ public/src/firebase_promote_function_app.mjs
@@ -1,7 +1,7 @@
 import { firebase_promote_function } from "../../../love/public/src/firebase_promote_function.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_promote_function_app(f_name) {
-  marker("1");
+  marker("1");app_name_main
   let v = await firebase_promote_function(f_name);
   return v;
 }
