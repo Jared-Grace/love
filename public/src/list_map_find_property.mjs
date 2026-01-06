dcdd50c555dc2bbac09b568b728426cdb09c8269
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:19:19 -0500

    function_auto_path ..\love\public\src\app_chapter_main.mjs

---
+++ public/src/list_map_find_property.mjs
@@ -0,0 +1,10 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
+export function list_map_find_property(lv, property, sliced) {
+  function lambda4(item) {
+    let item2 = list_find_property(lv, property, item);
+    return item2;
+  }
+  let mapped2 = list_map(sliced, lambda4);
+  return mapped2;
+}
