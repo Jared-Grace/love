2a41186d8a6661e2e5dae4b4a852f32b2e14a042
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 09:44:11 -0500

    function_auto_path ..\love\public\src\object_to_list.mjs

--- public/src/object_to_list.mjs
+++ public/src/object_to_list.mjs
@@ -9,6 +9,6 @@ export function object_to_list(o) {
     return v;
   }
   let result2 = object_values_map(o, lambda3);
-  let v2 = object_values(result2);
-  return v2;
+  let list = object_values(result2);
+  return list;
 }
