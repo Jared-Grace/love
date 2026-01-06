92ec014a87d517b8108a4da139bcf446607c5ab0
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 13:05:29 -0500

    function_auto_path ..\love\public\src\app_search_home.mjs

--- public/src/app_search_home.mjs
+++ public/src/app_search_home.mjs
@@ -1,3 +1,4 @@
+import { html_div } from "../../../love/public/src/html_div.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -34,6 +35,7 @@ export function app_search_home(context) {
   html_focus(input);
   const text = "Search";
   html_button_width_full(root, text, search);
+  let div = html_div(root2);
   async function search() {
     let query = html_value_get(input);
     object_property_set(context, "query", query);
