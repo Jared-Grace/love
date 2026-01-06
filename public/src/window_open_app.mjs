b803ee916e27218fb0dd51ad07d4bc777badd858
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:54:14 -0500

    function_auto_path ..\love\public\src\window_open_app.mjs

--- public/src/window_open_app.mjs
+++ public/src/window_open_app.mjs
@@ -1,14 +1,12 @@
-import { app_name_prefix } from "../../../love/public/src/app_name_prefix.mjs";
-import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
+import { app_prefix_without } from "../../../love/public/src/app_prefix_without.mjs";
 import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { window_open } from "../../../love/public/src/window_open.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 export function window_open_app(app_fn, hash) {
   marker("1");
-  let prefix = app_name_prefix();
-  let without = string_prefix_without(app_fn.name, prefix);
-  let file_name = file_name_html(app_fn.name);
+  let without = app_prefix_without(app_fn);
+  let file_name = file_name_html(without);
   let h2 = hash_to_url(hash);
   window_open(file_name + h2);
 }
