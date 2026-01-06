b7f42fee10fbda0b83bef504221a8607f465f954
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:53:37 -0500

    function_auto_path ..\love\public\src\window_open_app.mjs

--- web/public/src/window_open_app.mjs
+++ web/public/src/window_open_app.mjs
@@ -1,9 +1,11 @@
+import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
 import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { window_open } from "../../../love/public/src/window_open.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 export function window_open_app(app_fn, hash) {
   marker("1");
+  let without = string_prefix_without(s, prefix);
   let file_name = file_name_html(app_fn.name);
   let h2 = hash_to_url(hash);
   window_open(file_name + h2);
