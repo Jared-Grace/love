604235d045e4248e784152a83b1a6d1800fd21a1
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 13:49:26 -0500

    function_auto_path ..\love\public\src\ebible_languages_books_upload.mjs

--- public/src/ebible_languages_books_upload.mjs
+++ public/src/ebible_languages_books_upload.mjs
@@ -1,6 +1,6 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_remove_property_multiple } from "../../../love/public/src/list_remove_property_multiple.mjs";
 import { bible_interlinear_verses_upload_folder } from "../../../love/public/src/bible_interlinear_verses_upload_folder.mjs";
-import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_version_books_upload } from "../../../love/public/src/ebible_version_books_upload.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
@@ -14,5 +14,5 @@ export async function ebible_languages_books_upload() {
   async function lambda(bible_folder) {
     let books = await ebible_version_books_upload(bible_folder);
   }
-  let waited = await list_map_unordered_async(bible_folders, lambda);
+  await each_async(bible_folders, lambda);
 }
