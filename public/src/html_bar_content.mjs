347a581e29ce0b081c487eecabe012e68b66fece
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 17:40:30 -0500

    function_auto_path ..\love\public\src\app_component.mjs

---
+++ public/src/html_bar_content.mjs
@@ -0,0 +1,26 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+export function html_bar_content(root) {
+  let shell = html_div(root);
+  html_style_assign(shell, {
+    display: "flex",
+    "flex-direction": "column",
+    height: "100dvh",
+  });
+  let bar = html_div(shell);
+  html_style_assign(bar, {
+    flex: "0 0 auto",
+  });
+  let content = html_div(shell);
+  html_style_assign(content, {
+    flex: "1 1 auto",
+    "min-height": "0",
+    position: "relative",
+    overflow: "hidden",
+  });
+  let v2 = {
+    bar,
+    content,
+  };
+  return v2;
+}
