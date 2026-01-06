e45002e89e5bc28270171125740208621b31618e
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:02:26 -0500

    function_auto_path ..\love\public\src\list_slice_from.mjs

--- public/src/list_slice_from.mjs
+++ public/src/list_slice_from.mjs
@@ -2,13 +2,12 @@ import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
 import { list_sort_number } from "../../../love/public/src/list_sort_number.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { list_slice_include } from "../../../love/public/src/list_slice_include.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_last } from "./list_last.mjs";
 export function list_slice_from(list, item_from, item_to) {
   marker("1");
-  let mapped = list_map([item_from, item_to], list_index_of);
+  let mapped = list_map([item_from, item_to], () => {});
   list_sort_number(mapped);
   let v = list_first_last(list);
   let last = list_first(v);
