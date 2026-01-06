15c5709b1131d827fe1cb397140b8dabcf628294
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:29:41 -0500

    function_auto_path ..\love\public\src\list_first_last.mjs

--- public/src/list_first_last.mjs
+++ public/src/list_first_last.mjs
@@ -1,8 +1,9 @@
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_first_last() {
+export function list_first_last(list) {
   marker("1");
   let first = list_first(list);
-  let last = list_last(list2);
+  let last = list_last(list);
+  return [first, last];
 }
