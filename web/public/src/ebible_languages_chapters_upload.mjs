52b88d06349158b16b072667132d3150c8a8a36a
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 15:48:44 -0500

    function_auto_path ..\love\public\src\ebible_languages_chapters_upload.mjs

--- web/public/src/ebible_languages_chapters_upload.mjs
+++ web/public/src/ebible_languages_chapters_upload.mjs
@@ -1,4 +1,10 @@
+import { ebible_chapters_upload } from "../../../love/public/src/ebible_chapters_upload.mjs";
+import { ebible_languages_without_original_bible_folders_each } from "../../../love/public/src/ebible_languages_without_original_bible_folders_each.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_languages_chapters_upload() {
+export async function ebible_languages_chapters_upload() {
   marker("1");
+  await ebible_languages_without_original_bible_folders_each(lambda);
+  async function lambda(bible_folder) {
+    let books = await ebible_chapters_upload(bible_folder);
+  }
 }
