a8bdf3fd285f91f6a5df5cf0620f7ac4dc3e5518
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:50:56 -0500

    function_auto_path ..\love\public\src\window_open_app.mjs

--- web/public/src/window_open_app.mjs
+++ web/public/src/window_open_app.mjs
@@ -1,8 +1,10 @@
+import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { window_open } from "../../../love/public/src/window_open.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 export function window_open_app(app_fn, hash) {
   marker("1");
   let file_name = file_name_html(app_fn.name);
+  let h2 = hash_to_url(hash2);
   window_open(file_name);
 }
