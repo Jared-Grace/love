d4b1837c68afa7d59ca027cd58a9a81e1fb14e3a
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 15:39:31 -0500

    function_auto_path ..\love\public\src\ebible_languages_books_upload.mjs

--- web/public/src/ebible_languages_books_upload.mjs
+++ web/public/src/ebible_languages_books_upload.mjs
@@ -1,15 +1,11 @@
-import { list_remove_property } from "../../../love/public/src/list_remove_property.mjs";
+import { ebible_languages_without_original } from "../../../love/public/src/ebible_languages_without_original.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { bible_interlinear_verses_upload_folder } from "../../../love/public/src/bible_interlinear_verses_upload_folder.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_version_books_upload } from "../../../love/public/src/ebible_version_books_upload.mjs";
-import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_languages_books_upload() {
   marker("1");
-  let languages = ebible_languages();
-  let original = bible_interlinear_verses_upload_folder();
-  list_remove_property(languages, "language_code", original);
+  let languages = ebible_languages_without_original();
   let bible_folders = list_map_property(languages, "bible_folder");
   async function lambda(bible_folder) {
     let books = await ebible_version_books_upload(bible_folder);
