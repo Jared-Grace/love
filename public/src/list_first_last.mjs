40d326678889bcae49eb2406d1c050836a2a576e
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:30:11 -0500

    function_auto_path ..\love\public\src\list_first_last.mjs

--- public/src/list_first_last.mjs
+++ public/src/list_first_last.mjs
@@ -1,12 +1,10 @@
-import { assert } from "../../../love/public/src/assert.mjs";
-import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
+import { list_multiple_is_assert } from "../../../love/public/src/list_multiple_is_assert.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_first_last(list) {
   marker("1");
-  let m = list_multiple_is(list);
-  assert(b);
+  list_multiple_is_assert(list);
   let first = list_first(list);
   let last = list_last(list);
   let v = [first, last];
