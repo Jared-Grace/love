de346c055c5c85fa16e20bfb2132f4f5280a0e65
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:50:40 -0500

    pnd hash

--- web/public/src/window_open_app.mjs
+++ web/public/src/window_open_app.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { window_open } from "../../../love/public/src/window_open.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
-export function window_open_app(app_fn) {
+export function window_open_app(app_fn, hash) {
   marker("1");
   let file_name = file_name_html(app_fn.name);
   window_open(file_name);
