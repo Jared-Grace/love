b56592ac38e9760ab3bd719d8984aba7a04c7cff
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 16:09:37 -0500

    function_auto_path ..\love\public\src\app_chapter_main.mjs

--- public/src/app_chapter_main.mjs
+++ public/src/app_chapter_main.mjs
@@ -1,3 +1,5 @@
+import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
+import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { app_next_hash_to_languages_chosen } from "../../../love/public/src/app_next_hash_to_languages_chosen.mjs";
@@ -12,6 +14,8 @@ export async function app_chapter_main() {
   let chapter_code = object_property_get(hash, "c");
   let verse_number = object_property_get(hash, "v");
   let languages_chosen = app_next_hash_to_languages_chosen(hash);
-  let first = list_first(list2);
+  let languages_list = ebible_languages();
+  let first = list_first(languages_chosen);
+  let f = list_find_property(languages_list, "language_code", first);
   let list = await ebible_verses(bible_folder, chapter_code2);
 }
