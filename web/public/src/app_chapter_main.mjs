a4fc564362fe7a203d708d3c185a9b0c70e70d71
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 15:23:23 -0500

    function_auto_path ..\love\public\src\app_chapter_main.mjs

--- web/public/src/app_chapter_main.mjs
+++ web/public/src/app_chapter_main.mjs
@@ -1,3 +1,4 @@
+import { app_next_hash_to_languages_chosen } from "../../../love/public/src/app_next_hash_to_languages_chosen.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
@@ -8,4 +9,5 @@ export function app_chapter_main() {
   let hash = html_hash_object_get();
   let chapter_code = object_property_get(hash, "c");
   let verse_number = object_property_get(hash, "v");
+  let languages_chosen = app_next_hash_to_languages_chosen(hash);
 }
