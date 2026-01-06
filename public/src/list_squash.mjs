dd6a93e763010176e5df09a0b2902872152dd3b6
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 18:26:19 -0500

    function_auto_path ..\love\public\src\list_squash.mjs

--- public/src/list_squash.mjs
+++ public/src/list_squash.mjs
@@ -1,9 +1,9 @@
 import { list_is } from "../../../love/public/src/list_is.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-export function list_squash(list) {
+export function list_squash(lists) {
   function lambda2(la) {
-    list_process(list);
+    list_process(lists);
     function list_process(list) {
       function lambda(item) {
         let l = list_is(item);
