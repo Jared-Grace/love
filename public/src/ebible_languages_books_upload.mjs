36b7155fe1d257f65442bb8de2ebb1fd3c88ab8e
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 13:45:36 -0500

    function_auto_path ..\love\public\src\ebible_languages_books_upload.mjs

--- public/src/ebible_languages_books_upload.mjs
+++ public/src/ebible_languages_books_upload.mjs
@@ -1,3 +1,4 @@
+import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_version_books_upload } from "../../../love/public/src/ebible_version_books_upload.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
@@ -5,7 +6,11 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_languages_books_upload() {
   marker("1");
   let languages = ebible_languages();
-  let mapped = list_map_property(list, property_name);
+  let mapped = list_map_property(languages, "bible_folder");
+  let waited = await list_map_unordered_async(
+    list,
+    async function lambda(item) {},
+  );
   let books = await ebible_version_books_upload(bible_folder);
   return languages;
 }
