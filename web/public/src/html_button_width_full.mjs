7b13c06b68b4b4dd60d032691c7645d751258247
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 09:56:13 -0500

    function_auto_path ..\love\public\src\app_search_results.mjs

--- web/public/src/html_button_width_full.mjs
+++ web/public/src/html_button_width_full.mjs
@@ -1,7 +1,7 @@
 import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-export function html_button_width_full(root, text, lambda2) {
-  let component = html_button(root, text, lambda2);
+export function html_button_width_full(root, text, lambda) {
+  let component = html_button(root, text, lambda);
   html_width_full(component);
   return component;
 }
