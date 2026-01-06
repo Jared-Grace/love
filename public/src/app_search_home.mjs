38bd3f4132ad24515434c1b8b2b52674ec2d14af
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 13:42:58 -0500

    function_auto_path ..\love\public\src\app_search_home.mjs

--- public/src/app_search_home.mjs
+++ public/src/app_search_home.mjs
@@ -1,3 +1,4 @@
+import { html_value_set } from "../../../love/public/src/html_value_set.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
@@ -33,6 +34,7 @@ export function app_search_home(context) {
   let input = html_input_text(root, search_instructions);
   html_width_full(input);
   html_focus(input);
+  html_value_set(input2, value);
   const text = "Search";
   html_button_width_full(root, text, search);
   let div_results = html_div(root);
