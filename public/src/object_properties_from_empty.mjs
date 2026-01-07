4b158b63072b64b628372c0075da394a83ea90f8
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:34:51 -0500

    function_auto_path ..\love\public\src\object_properties_from_empty.mjs

--- public/src/object_properties_from_empty.mjs
+++ public/src/object_properties_from_empty.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_properties_from } from "../../../love/public/src/object_properties_from.mjs";
-export function object_properties_from_empty(item2, properties) {
+export function object_properties_from_empty(o, properties) {
   marker("1");
-  let to = object_properties_from({}, properties, item2);
-  return to;
+  let r = object_properties_from({}, properties, o);
+  return r;
 }
