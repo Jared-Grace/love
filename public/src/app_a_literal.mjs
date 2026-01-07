24085d778bca60c6ccac2c09a6e2c5ae176e7afd
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:30:39 -0500

    function_auto_path ..\love\public\src\app_a_literal.mjs

--- public/src/app_a_literal.mjs
+++ public/src/app_a_literal.mjs
@@ -1,6 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { app_a_identifier_generic } from "../../../love/public/src/app_a_identifier_generic.mjs";
 export function app_a_literal(a, component, node) {
   marker("1");
@@ -16,7 +15,4 @@ export function app_a_literal(a, component, node) {
     },
     true,
   );
-  function on_change(value_new) {
-    object_property_set(node, "raw", value_new);
-  }
 }
