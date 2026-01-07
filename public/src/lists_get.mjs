ef9df2a6344dea01fa24824f47f45552cb7408a2
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:58:28 -0500

    function_auto_path ..\love\public\src\lists_get.mjs

--- public/src/lists_get.mjs
+++ public/src/lists_get.mjs
@@ -1,8 +1,9 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function lists_get(lists, index) {
+  let getter = list_get;
   function lambda(list) {
-    let item = list_get(list, index);
+    let item = getter(list, index);
     return item;
   }
   const items = list_map(lists, lambda);
