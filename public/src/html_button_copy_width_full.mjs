06eb3a642868cd49095a076a4c08f74d9b6d9aac
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 19:17:24 -0500

    function_auto_path ..\love\public\src\html_button_copy_width_full.mjs

--- public/src/html_button_copy_width_full.mjs
+++ public/src/html_button_copy_width_full.mjs
@@ -3,4 +3,5 @@ import { html_button_copy } from "../../../love/public/src/html_button_copy.mjs"
 export function html_button_copy_width_full(div_verse, copy) {
   let c = html_button_copy(div_verse, copy);
   html_width_full(c);
+  return c;
 }
