cf2f286caa14e1f614e7b036458b305592646dcc
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 18:15:08 -0500

    function_auto_path ..\love\public\src\indexeddb_exists.mjs

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -1,5 +1,4 @@
-import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
-import { each_index } from "../../../love/public/src/each_index.mjs";
+import { list_property_exists_not_debug } from "../../../love/public/src/list_property_exists_not_debug.mjs";
 import { indexeddb_exists_backend } from "../../../love/public/src/indexeddb_exists_backend.mjs";
 import { list_find_property_exists } from "../../../love/public/src/list_find_property_exists.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
@@ -8,13 +7,7 @@ export async function indexeddb_exists(db_get, store, key) {
   marker("1");
   const property = "key";
   let all = await indexeddb_get_all(db_get, store);
-  function lambda(item, index) {
-    let n = object_property_exists_not(item, property);
-    if (n) {
-      debugger;
-    }
-  }
-  each_index(all, lambda);
+  list_property_exists_not_debug(property, all);
   let s1 = list_find_property_exists(all, property, key);
   return s1;
   let exists = await indexeddb_exists_backend(db_get, store, key);
---
+++ public/src/list_property_exists_not_debug.mjs
@@ -0,0 +1,11 @@
+import { each_index } from "../../../love/public/src/each_index.mjs";
+import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
+export function list_property_exists_not_debug(property, all) {
+  function lambda(item, index) {
+    let n = object_property_exists_not(item, property);
+    if (n) {
+      debugger;
+    }
+  }
+  each_index(all, lambda);
+}
