ac3fe7880bce94176cd760c3c22c4b5402b531ea
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:31:12 -0500

    function_auto_path ..\love\public\src\function_dependencies_externals_to_urls.mjs

--- web/public/src/function_dependencies_externals_to_urls.mjs
+++ web/public/src/function_dependencies_externals_to_urls.mjs
@@ -5,7 +5,7 @@ export function function_dependencies_externals_to_urls(externals) {
       acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
       astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
       "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
-      prettier: "https://cdn.jsdelivr.net/npm/prettier@3.0.0/esm.mjs",
+      prettier: "https://unpkg.com/prettier@3.7.4/standalone.mjs",
       "prettier/plugins/babel": [
         {
           "": "https://cdn.jsdelivr.net/npm/prettier@3.0.0/esm/plugins/babel.mjs",
