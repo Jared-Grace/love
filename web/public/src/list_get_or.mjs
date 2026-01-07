9f003ce1dee2785631ae34d315e0aad761de079c
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:59:03 -0500

    function_auto_path ..\love\public\src\list_get_or.mjs

--- web/public/src/list_get_or.mjs
+++ web/public/src/list_get_or.mjs
@@ -1,10 +1,10 @@
+import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 export function list_get_or(obj, key, value_not) {
   marker("1");
   let value = value_not;
-  let exists = object_property_exists(obj, key);
+  let exists = list_index_is(obj, key);
   if (exists) {
     value = object_property_get(obj, key);
   }
