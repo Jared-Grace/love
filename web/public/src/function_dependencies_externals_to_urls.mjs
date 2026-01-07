0cc4403c17e8d02ebe9c9660a69d2bbd5c3855c9
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:37:07 -0500

    function_auto_path ..\love\public\src\function_dependencies_externals_to_urls.mjs

--- web/public/src/function_dependencies_externals_to_urls.mjs
+++ web/public/src/function_dependencies_externals_to_urls.mjs
@@ -1,10 +1,10 @@
 import { object_properties_from } from "../../../love/public/src/object_properties_from.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export function function_dependencies_externals_to_urls(externals) {
-  let r = null;
+  let o = null;
   let includes = list_includes(list, "prettier/plugins/babel");
   if (includes) {
-    r = {
+    o = {
       parserBabel: "https://unpkg.com/prettier@3.7.4/plugins/babel.mjs",
       parserEstree: "https://unpkg.com/prettier@3.7.4/plugins/estree.mjs",
     };
@@ -16,6 +16,6 @@ export function function_dependencies_externals_to_urls(externals) {
     "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
     prettier: "https://unpkg.com/prettier@3.7.4/standalone.mjs",
   };
-  let to = object_properties_from(r, externals, lookup);
+  let to = object_properties_from(o, externals, lookup);
   return to;
 }
