ac3be27f938f9d2e6bf2931f66e9a1f71943db53
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:01:24 -0500

    function_auto_path ..\love\public\src\js_format.mjs

--- public/src/js_format.mjs
+++ public/src/js_format.mjs
@@ -1,22 +1,22 @@
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import parserBabel from "prettier/plugins/babel";
+import prettier from "prettier";
 export async function js_format(code) {
   marker("1");
-  let prettier = null,
-    parserBabel = null;
-  prettier = await import("prettier");
-  let babel = null;
+  let pr = null,
+    pb = null;
   let b = browser_is();
   if (b) {
-    babel = "parser-babel";
+    pr = "parser-babel";
+    pb = "prettier/plugins/babel";
   } else {
-    babel = "prettier/plugins/babel";
+    pr = prettier;
+    pb = parserBabel;
   }
-  parserBabel = await import(babel);
-  const formatted = await prettier.format(code, {
+  const formatted = await pr.format(code, {
     parser: "babel",
-    plugins: [parserBabel],
+    plugins: [pb],
     braceStyle: "allman",
   });
   return formatted;
