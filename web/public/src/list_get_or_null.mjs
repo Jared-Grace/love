f166aca6727e9f72121441f035fcfc84ebb353ab
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:59:51 -0500

    function_auto_path ..\love\public\src\list_get_or_null.mjs

--- web/public/src/list_get_or_null.mjs
+++ web/public/src/list_get_or_null.mjs
@@ -1,7 +1,7 @@
 import { list_get_or } from "../../../love/public/src/list_get_or.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_get_or_null(list, index, value_not) {
+export function list_get_or_null(list, index) {
   marker("1");
-  let value = list_get_or(list, index, value_not);
+  let value = list_get_or(list, index, null);
   return value;
 }
