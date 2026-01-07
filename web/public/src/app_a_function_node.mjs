14e0174c6add617572da8d043b0f9d02ad352a1e
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:54:08 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- web/public/src/app_a_function_node.mjs
+++ web/public/src/app_a_function_node.mjs
@@ -1,3 +1,4 @@
+import { each_pair } from "../../../love/public/src/each_pair.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { app_a_identifier_generic } from "../../../love/public/src/app_a_identifier_generic.mjs";
 import { app_a_keyword_blue } from "../../../love/public/src/app_a_keyword_blue.mjs";
@@ -242,7 +243,7 @@ export function app_a_function_node(a) {
       let quasis = object_property_get(node, "quasis");
       let expressions = object_property_get(node, "expressions");
       function lambda22(q) {}
-      each(quasis, lambda22);
+      each_pair(quasis, lambda22);
     },
   };
   function o_props() {
