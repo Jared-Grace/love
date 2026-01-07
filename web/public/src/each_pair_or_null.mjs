a8fe50e953b7451ce4d184ecbb039c12f14ea471
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 21:02:41 -0500

    function_auto_path ..\love\public\src\each_pair_or_null.mjs

--- web/public/src/each_pair_or_null.mjs
+++ web/public/src/each_pair_or_null.mjs
@@ -1,8 +1,8 @@
+import { each_multiple_or_null } from "../../../love/public/src/each_multiple_or_null.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each_pair_generic } from "../../../love/public/src/each_pair_generic.mjs";
-import { each_multiple } from "../../../love/public/src/each_multiple.mjs";
 export function each_pair_or_null(list_a, list_b, lambda$a$b) {
   marker("1");
-  let each_fn = each_multiple;
+  let each_fn = each_multiple_or_null;
   each_pair_generic(list_a, list_b, each_fn, lambda$a$b);
 }
