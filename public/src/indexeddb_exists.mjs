af028cce767c72c7d7614d54e7ef2d95827a7900
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:54:55 -0500

    function_auto_path ..\love\public\src\indexeddb_exists.mjs

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -1,4 +1,4 @@
-import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { each_index } from "../../../love/public/src/each_index.mjs";
 import { indexeddb_exists_backend } from "../../../love/public/src/indexeddb_exists_backend.mjs";
 import { list_find_property_exists } from "../../../love/public/src/list_find_property_exists.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
@@ -6,8 +6,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_exists(db_get, store, key) {
   marker("1");
   let all = await indexeddb_get_all(db_get, store);
-  function lambda(item) {}
-  let filtered = list_filter(list, lambda);
+  each_index(list, function lambda(item, index) {});
   const property = "key";
   let s1 = list_find_property_exists(all, property, key);
   return s1;
