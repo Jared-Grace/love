1ede56e46e59097557dc9b967934b77cadf527b6
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 18:15:17 -0500

    pb

--- web/public/src/list_property_exists_not_debug.mjs
+++ web/public/src/list_property_exists_not_debug.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
-export function list_property_exists_not_debug(property, all) {
+export function list_property_exists_not_debug(all, property) {
   marker("1");
   function lambda(item, index) {
     let n = object_property_exists_not(item, property);
