10e145ac90fe6038d654e6536dc85c3fac779f60
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:55:53 -0500

    function_auto_path ..\love\public\src\indexeddb_exists.mjs

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -6,14 +6,15 @@ import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mj
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function indexeddb_exists(db_get, store, key) {
   marker("1");
+  const property = "key";
   let all = await indexeddb_get_all(db_get, store);
   function lambda(item, index) {
-    let n = object_property_exists_not(item, property_name);
-    if (false) {
+    let n = object_property_exists_not(item, property);
+    if (n) {
+      debugger;
     }
   }
   each_index(all, lambda);
-  const property = "key";
   let s1 = list_find_property_exists(all, property, key);
   return s1;
   let exists = await indexeddb_exists_backend(db_get, store, key);
