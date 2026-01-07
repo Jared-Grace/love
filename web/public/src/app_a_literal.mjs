abd2ab2a137bd15ac811606297a251eb789c8755
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:29:11 -0500

    function_auto_path ..\love\public\src\app_a_literal.mjs

--- web/public/src/app_a_literal.mjs
+++ web/public/src/app_a_literal.mjs
@@ -1,7 +1,8 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { app_a_identifier_generic } from "../../../love/public/src/app_a_identifier_generic.mjs";
 export function app_a_literal(a, component, raw, node) {
-      let raw2 = object_property_get(v3, "raw");
+  let raw2 = object_property_get(v3, "raw");
   app_a_identifier_generic(
     a,
     component,
