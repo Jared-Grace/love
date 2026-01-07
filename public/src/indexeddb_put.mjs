ddfc0cb137d316f5739050adfeed9ceb6bbc6f17
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 18:38:10 -0500

    function_auto_path ..\love\public\src\indexeddb_put.mjs

--- public/src/indexeddb_put.mjs
+++ public/src/indexeddb_put.mjs
@@ -1,4 +1,3 @@
-import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { indexeddb_next } from "../../../love/public/src/indexeddb_next.mjs";
 import { indexeddb_put_backend } from "../../../love/public/src/indexeddb_put_backend.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
@@ -8,8 +7,6 @@ export async function indexeddb_put(db_get, store, key, value_get) {
   let all = await indexeddb_get_all(db_get, store);
   let f = list_find_property(all, "key", key);
   const next = await value_get(f);
-  if (equal_not(left, right)) {
-  }
   object_replace(f, next);
   async function lambda_async() {
     await indexeddb_put_backend(db_get, store, key, next);
