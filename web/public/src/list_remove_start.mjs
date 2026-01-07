1a44e3e42cd1535f76fa0bbdcb264a23c77f2396
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 19:33:46 -0500

    pnd list

--- web/public/src/list_remove_start.mjs
+++ web/public/src/list_remove_start.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_splice } from "../../../love/public/src/list_splice.mjs";
-export function list_remove_start(s) {
+export function list_remove_start(s, list) {
   marker("1");
   list_splice(list, 0, s);
 }
