8a82db376f6b76d2918055cb0bf0db26585101c9
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 21:00:21 -0500

    function_auto_path ..\love\public\src\lists_get_or_null.mjs

--- public/src/lists_get_or_null.mjs
+++ public/src/lists_get_or_null.mjs
@@ -1,9 +1,9 @@
+import { list_get_or_null } from "../../../love/public/src/list_get_or_null.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { lists_get_generic } from "../../../love/public/src/lists_get_generic.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 export function lists_get_or_null(lists, index) {
   marker("1");
-  let getter = list_get;
+  let getter = list_get_or_null;
   const items = lists_get_generic(getter, index, lists);
   return items;
 }
