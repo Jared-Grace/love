ac3a4ce2a5145abf107b2f86f3cf45eba1c6ae26
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 21:01:35 -0500

    o each_multiple

--- public/src/each_multiple.mjs
+++ public/src/each_multiple.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { lists_get } from "../../../love/public/src/lists_get.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function each_multiple(lists, lambda) {
+  marker("1");
   let first = list_first(lists);
   function lambda2(item, index) {
     let items = lists_get(lists, index);
