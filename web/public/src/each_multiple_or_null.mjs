6cfe885b93f44a3035f8d8f4a1fddd66efafb2a7
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:55:34 -0500

    function_auto_path ..\love\public\src\each_multiple_or_null.mjs

--- web/public/src/each_multiple_or_null.mjs
+++ web/public/src/each_multiple_or_null.mjs
@@ -1,9 +1,11 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { lists_get } from "../../../love/public/src/lists_get.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function each_multiple_or_null(lists, lambda) {
   marker("1");
+  let mapped = list_map(list, function lambda3(item2) {});
   let first = list_first(lists);
   function lambda2(item, index) {
     let items = lists_get(lists, index);
