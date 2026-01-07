502365d683d530716469dd16d82fce5e99315aaf
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 18:16:20 -0500

    rrl list_property_exists_not_debug error

--- web/public/src/app_a_download.mjs
+++ web/public/src/app_a_download.mjs
@@ -1,4 +1,4 @@
-import { list_property_exists_not_debug } from "../../../love/public/src/list_property_exists_not_debug.mjs";
+import { list_property_exists_not_error } from "../../../love/public/src/list_property_exists_not_error.mjs";
 import { json_compress_object } from "../../../love/public/src/json_compress_object.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
@@ -20,6 +20,6 @@ export async function app_a_download() {
     return c;
   }
   let files = await list_map_unordered_async(filtered, lambda2);
-  list_property_exists_not_debug(all, "key");
+  list_property_exists_not_error(all, "key");
   return files;
 }
