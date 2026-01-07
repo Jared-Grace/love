0714b54c48764a275d84f7b9b8646a216e25ecec
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 18:16:20 -0500

    rrl list_property_exists_not_debug error

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -1,4 +1,4 @@
-import { list_property_exists_not_debug } from "../../../love/public/src/list_property_exists_not_debug.mjs";
+import { list_property_exists_not_error } from "../../../love/public/src/list_property_exists_not_error.mjs";
 import { indexeddb_exists_backend } from "../../../love/public/src/indexeddb_exists_backend.mjs";
 import { list_find_property_exists } from "../../../love/public/src/list_find_property_exists.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
@@ -7,7 +7,7 @@ export async function indexeddb_exists(db_get, store, key) {
   marker("1");
   const property = "key";
   let all = await indexeddb_get_all(db_get, store);
-  list_property_exists_not_debug(all, property);
+  list_property_exists_not_error(all, property);
   let s1 = list_find_property_exists(all, property, key);
   return s1;
   let exists = await indexeddb_exists_backend(db_get, store, key);
