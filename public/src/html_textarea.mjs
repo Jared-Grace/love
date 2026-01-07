22c5f26dc97325123de78feac96b0a099cc39d04
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:40:27 -0500

    function_auto_path ..\love\public\src\app_message_main.mjs

---
+++ public/src/html_textarea.mjs
@@ -0,0 +1,5 @@
+import { html_element } from "../../../love/public/src/html_element.mjs";
+export function html_textarea(div) {
+  let component = html_element(div, "textarea");
+  return component;
+}
