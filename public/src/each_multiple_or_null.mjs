a3d0176890439d5bb1f62fcf899820fed665b97a
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 21:01:11 -0500

    function_auto_path ..\love\public\src\each_multiple_or_null.mjs

--- public/src/each_multiple_or_null.mjs
+++ public/src/each_multiple_or_null.mjs
@@ -6,10 +6,11 @@ import { lists_get } from "../../../love/public/src/lists_get.mjs";
 import { list_size } from "./list_size.mjs";
 export function each_multiple_or_null(lists, lambda) {
   marker("1");
+  let getter = lists_get;
   let mapped = list_map(lists, list_size);
   let max = list_max(mapped);
   function lambda2(index) {
-    let items = lists_get(lists, index);
+    let items = getter(lists, index);
     lambda(items);
   }
   each_range(max, lambda2);
