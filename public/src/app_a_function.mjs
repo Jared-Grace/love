47fbd4c92a079960f5df577124a3bd3f7e930ec7
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:11:16 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -205,7 +205,8 @@ export async function app_a_function(context) {
     "word-break": "break-word",
     "font-weight": "500",
   });
-  let list = list_adder(function lambda8(la) {});
+  function lambda8(la) {}
+  let list = list_adder(lambda8);
   function lambda7(v) {
     let n = object_property_get(v, "node");
     let id = object_property_get(n, "id");
