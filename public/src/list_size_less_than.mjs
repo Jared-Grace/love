477e56fb063fe7989f2a2c7d1a8678033735c1a8
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 19:25:01 -0500

    function_auto_path ..\love\public\src\list_size_less_than.mjs

--- public/src/list_size_less_than.mjs
+++ public/src/list_size_less_than.mjs
@@ -1,6 +1,6 @@
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { less_than_by } from "../../../love/public/src/less_than_by.mjs";
-export function list_size_less_than(stack, result) {
-  let v2 = less_than_by(stack, result, list_size);
+export function list_size_less_than(list_a, list_b) {
+  let v2 = less_than_by(list_a, list_b, list_size);
   return v2;
 }
