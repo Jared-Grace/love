45e0f77432a3e8b50edec7e38d344ebf7b4f4120
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:59:13 -0500

    data_file_update ..\love\public\src\list_get_or.mjs

--- web/public/src/list_get_or.mjs
+++ web/public/src/list_get_or.mjs
@@ -1,12 +1,12 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_get_or(obj, key, value_not) {
+export function list_get_or(list, key, value_not) {
   marker("1");
   let value = value_not;
-  let exists = list_index_is(obj, key);
+  let exists = list_index_is(list, key);
   if (exists) {
-    value = list_get(obj, key);
+    value = list_get(list, key);
   }
   return value;
 }
