8d49f1fcde0794f2a731392d0edd1da7f100402f
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 19:33:03 -0500

    function_auto_path ..\love\public\src\list_size_max_skip.mjs

--- public/src/list_size_max_skip.mjs
+++ public/src/list_size_max_skip.mjs
@@ -1,12 +1,11 @@
-import { each } from "../../../love/public/src/each.mjs";
+import { list_splice } from "../../../love/public/src/list_splice.mjs";
 import { greater_than } from "../../../love/public/src/greater_than.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_size_max_skip(verse_numbers_chosen, max) {
   let size = list_size(verse_numbers_chosen);
   if (greater_than(size, max)) {
     const s = size - max;
-    function lambda(item) {}
-    each(list, lambda);
+    list_splice(list, index, delete_count, value);
   }
   return verse_numbers_chosen;
 }
