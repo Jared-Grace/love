531b2aa4ecfdadfce049a49fc63425abb69a3412
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:37:56 -0500

    function_auto_path ..\love\public\src\app_a_button_shortcut_wide.mjs

--- web/public/src/app_a_button_shortcut_wide.mjs
+++ web/public/src/app_a_button_shortcut_wide.mjs
@@ -1,7 +1,9 @@
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { app_a_button_shortcut } from "../../../love/public/src/app_a_button_shortcut.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_button_shortcut_wide(parent, shortcut, text, fn) {
   marker("1");
   let b2 = app_a_button_shortcut(parent, shortcut, text, fn);
+  html_width_full(component);
   return b2;
 }
