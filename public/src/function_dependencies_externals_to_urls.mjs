03de81d99d69dfe9b5a83bbf79680a82f14863c4
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:36:29 -0500

    function_auto_path ..\love\public\src\function_dependencies_externals_to_urls.mjs

--- public/src/function_dependencies_externals_to_urls.mjs
+++ public/src/function_dependencies_externals_to_urls.mjs
@@ -1,16 +1,14 @@
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { object_properties_from_empty } from "../../../love/public/src/object_properties_from_empty.mjs";
 export function function_dependencies_externals_to_urls(externals) {
-  let to = object_properties_from_empty(
-    {
-      acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
-      astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
-      "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
-      prettier: "https://unpkg.com/prettier@3.7.4/standalone.mjs",
-      "prettier/plugins/babel": [,],
-    },
-    externals,
-  );
+  const lookup = {
+    acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
+    astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
+    "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
+    prettier: "https://unpkg.com/prettier@3.7.4/standalone.mjs",
+    "prettier/plugins/babel": [,],
+  };
+  let to = object_properties_from_empty(lookup, externals);
   let r = null;
   let includes = list_includes(list, "prettier/plugins/babel");
   if (includes) {
