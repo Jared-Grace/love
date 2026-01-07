708a94d380448ec56686a18b18c4e0e8801454b8
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 15:24:31 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

---
+++ public/src/app_a_symbol_string_template.mjs
@@ -0,0 +1,6 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+import { js_symbol_string_template } from "../../../love/public/src/js_symbol_string_template.mjs";
+export function app_a_symbol_string_template(parent) {
+  let text5 = js_symbol_string_template();
+  let span2 = html_span_text(parent, text5);
+}
