2dcd268b0c18f309c3366a69d0b501ac08174131
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:29:22 -0500

    pd raw

--- public/src/app_a_literal.mjs
+++ public/src/app_a_literal.mjs
@@ -2,7 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { app_a_identifier_generic } from "../../../love/public/src/app_a_identifier_generic.mjs";
-export function app_a_literal(a, component, raw, node) {
+export function app_a_literal(a, component, node) {
   marker("1");
   let raw2 = object_property_get(v3, "raw");
   app_a_identifier_generic(
