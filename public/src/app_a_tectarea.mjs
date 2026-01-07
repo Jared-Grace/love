98a5b8de7744702bb2a94d3ff38618a8a691e379
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:39:26 -0500

    rrl app_a_tectarea textarea

--- public/src/app_a_tectarea.mjs
+++
@@ -1,9 +0,0 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-import { app_a_input_generic } from "../../../love/public/src/app_a_input_generic.mjs";
-import { html_input } from "../../../love/public/src/html_input.mjs";
-export function app_a_tectarea(body) {
-  marker("1");
-  let fn = html_input;
-  let input = app_a_input_generic(fn, body);
-  return input;
-}
