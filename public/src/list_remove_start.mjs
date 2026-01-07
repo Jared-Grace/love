6e5b325f053e321445fd5d2dc4fc445fb7282a20
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 19:33:30 -0500

    fs list_remove_start

--- public/src/list_remove_start.mjs
+++ public/src/list_remove_start.mjs
@@ -1,4 +1,6 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_splice } from "../../../love/public/src/list_splice.mjs";
 export function list_remove_start(s) {
+  marker("1");
   list_splice(list, 0, s);
 }
