32e6df21910ff445091d983c1b585a902e347d27
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 17:36:37 -0500

    function_auto_path ..\love\public\src\function_dependencies_externals_to_urls.mjs

--- web/public/src/function_dependencies_externals_to_urls.mjs
+++ web/public/src/function_dependencies_externals_to_urls.mjs
@@ -1,14 +1,15 @@
+import { object_properties_from } from "../../../love/public/src/object_properties_from.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-import { object_properties_from_empty } from "../../../love/public/src/object_properties_from_empty.mjs";
 export function function_dependencies_externals_to_urls(externals) {
   const lookup = {
     acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
     astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
     "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
     prettier: "https://unpkg.com/prettier@3.7.4/standalone.mjs",
-    "prettier/plugins/babel": [,],
   };
-  let to = object_properties_from_empty(lookup, externals);
+  marker("1");
+  let to = object_properties_from({}, externals, lookup);
   let r = null;
   let includes = list_includes(list, "prettier/plugins/babel");
   if (includes) {
