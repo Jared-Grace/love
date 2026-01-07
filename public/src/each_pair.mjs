5606f7a8b2bf26bfda56295ed7bb640f4a6fdbab
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 21:02:13 -0500

    function_auto_path ..\love\public\src\each_pair.mjs

--- public/src/each_pair.mjs
+++ public/src/each_pair.mjs
@@ -1,10 +1,6 @@
+import { each_pair_generic } from "../../../love/public/src/each_pair_generic.mjs";
 import { each_multiple } from "../../../love/public/src/each_multiple.mjs";
 export function each_pair(list_a, list_b, lambda$a$b) {
   let each_fn = each_multiple;
-  let lists = [list_a, list_b];
-  each_fn(lists, lambda);
-  function lambda(items) {
-    let [a, b] = items;
-    lambda$a$b(a, b);
-  }
+  each_pair_generic(list_a, list_b, each_fn, lambda$a$b);
 }
