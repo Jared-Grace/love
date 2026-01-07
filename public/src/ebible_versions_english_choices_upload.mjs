5338447d9aa432e9a7b7014bec146f4bd066458b
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 13:04:49 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_upload.mjs

--- public/src/ebible_versions_english_choices_upload.mjs
+++ public/src/ebible_versions_english_choices_upload.mjs
@@ -1,6 +1,8 @@
 import { ebible_versions_english_choices_upload_path } from "../../../love/public/src/ebible_versions_english_choices_upload_path.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
+import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
 export async function ebible_versions_english_choices_upload() {
+  let cs = await ebible_versions_english_choices();
   let destination = ebible_versions_english_choices_upload_path();
   await firebase_upload_object(destination, cs);
 }
