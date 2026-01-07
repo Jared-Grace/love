a154a13bf555bd91fada477208338d1fd1ad764c
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 13:19:16 -0500

    function_auto_path ..\love\public\src\ebible_language_original.mjs

--- public/src/ebible_language_original.mjs
+++ public/src/ebible_language_original.mjs
@@ -1,10 +1,11 @@
+import { ebible_language_original_code } from "../../../love/public/src/ebible_language_original_code.mjs";
 import { bible_interlinear_verses_upload_folder } from "../../../love/public/src/bible_interlinear_verses_upload_folder.mjs";
 import { ebible_language_original_name } from "../../../love/public/src/ebible_language_original_name.mjs";
 export function ebible_language_original() {
   let v = {
     name: ebible_language_original_name(),
     bible_folder: bible_interlinear_verses_upload_folder(),
-    language_code: "original",
+    language_code: ebible_language_original_code(),
   };
   return v;
 }
