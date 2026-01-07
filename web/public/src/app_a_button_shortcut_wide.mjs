db20bc23be87ebfc9d9db8ebf3c5f29d15594164
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:38:01 -0500

    function_auto_path ..\love\public\src\app_a_button_shortcut_wide.mjs

--- web/public/src/app_a_button_shortcut_wide.mjs
+++ web/public/src/app_a_button_shortcut_wide.mjs
@@ -4,6 +4,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_button_shortcut_wide(parent, shortcut, text, fn) {
   marker("1");
   let b2 = app_a_button_shortcut(parent, shortcut, text, fn);
-  html_width_full(component);
+  html_width_full(b2);
   return b2;
 }
