e02e80ae25f1e72c7b58ac81eb9acc291d5736ec
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 22:40:39 -0500

    function_auto_path ..\love\public\src\list_remove_start.mjs

--- public/src/list_remove_start.mjs
+++ public/src/list_remove_start.mjs
@@ -1,5 +1,7 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_remove_start(list, delete_count) {
   marker("1");
+  log({});
   list.splice(list, 0, delete_count);
 }
