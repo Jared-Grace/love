6ea8aa675794ad7a6d3eee8626105b0f1f8332ba
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 13:18:44 -0500

    function_auto_path ..\love\public\src\ebible_version_books.mjs

--- web/public/src/ebible_version_books.mjs
+++ web/public/src/ebible_version_books.mjs
@@ -1,3 +1,5 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
 import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { firebase_storage_download_ebible } from "../../../love/public/src/firebase_storage_download_ebible.mjs";
@@ -9,15 +11,15 @@ import { list_join_comma_space } from "../../../love/public/src/list_join_comma_
 import { ebible_class_old } from "../../../love/public/src/ebible_class_old.mjs";
 import { ebible_class_new } from "../../../love/public/src/ebible_class_new.mjs";
 export async function ebible_version_books(bible_folder) {
+  if (equal(left, right)) {
+  }
   marker("1");
   let b = browser_is();
   if (b) {
     async function lambda2() {
       let file_name2 = ebible_version_books_upload_name();
-      let { books } = await firebase_storage_download_ebible(
-        bible_folder,
-        file_name2,
-      );
+      let v = await firebase_storage_download_ebible(bible_folder, file_name2);
+      let books = object_property_get(v, "books");
       return books;
     }
     let value = await global_function_property_initialize_async(
