d7ec308d04d2b37f9e0576d4e3c954c2dc343f3c
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 20:55:36 -0500

    function_auto_path ..\love\public\src\each_multiple_or_null.mjs

--- public/src/each_multiple_or_null.mjs
+++ public/src/each_multiple_or_null.mjs
@@ -5,7 +5,8 @@ import { each_index } from "../../../love/public/src/each_index.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function each_multiple_or_null(lists, lambda) {
   marker("1");
-  let mapped = list_map(list, function lambda3(item2) {});
+  function lambda3(item2) {}
+  let mapped = list_map(list, lambda3);
   let first = list_first(lists);
   function lambda2(item, index) {
     let items = lists_get(lists, index);
