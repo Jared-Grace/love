be72b70f5c0e37ef825fd6c7ca6e5b21087f9a69
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 15:28:03 -0500

    function_auto_path ..\love\public\src\js_dollar_inn.mjs

--- public/src/js_dollar_inn.mjs
+++ public/src/js_dollar_inn.mjs
@@ -1,13 +1,7 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function js_dollar_inn({
-  remaining,
-  node,
-  stack1,
-  stack2,
-  stack3,
-  ast,
-  afters,
-}) {
-  marker("1");
+import { js_dollar_if_call } from "../../../love/public/src/js_dollar_if_call.mjs";
+import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
+export async function js_dollar_inn({ stack1, ast }) {
+  let fn = null_not_is;
+  await js_dollar_if_call(stack1, fn, ast);
   return;
 }
