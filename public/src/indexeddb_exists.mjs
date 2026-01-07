cb7e29c8642bc066415fe8e7d74b6e909c700d6d
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 18:15:17 -0500

    pb

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -7,7 +7,7 @@ export async function indexeddb_exists(db_get, store, key) {
   marker("1");
   const property = "key";
   let all = await indexeddb_get_all(db_get, store);
-  list_property_exists_not_debug(property, all);
+  list_property_exists_not_debug(all, property);
   let s1 = list_find_property_exists(all, property, key);
   return s1;
   let exists = await indexeddb_exists_backend(db_get, store, key);
