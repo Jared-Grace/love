c38e228ddcf67ef6fdec9aec16cdc747d55a9b7f
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:56:53 -0500

    data_file_update ..\love\public\src\js_format.mjs

--- web/public/src/js_format.mjs
+++ web/public/src/js_format.mjs
@@ -3,8 +3,9 @@ export async function js_format(code) {
   marker("1");
   let prettier = null,
     parserBabel = null;
-  prettier = await import("prettier");
-  parserBabel = await import("prettier/plugins/babel");
+  prettier = await import("prettier");$s
+  const babel = "prettier/plugins/babel";
+  parserBabel = await import(babel);
   const formatted = await prettier.format(code, {
     parser: "babel",
     plugins: [parserBabel],
