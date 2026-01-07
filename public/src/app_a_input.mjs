57965627cece99072dd1f923ecfb0259489d3244
Grace-Jared <christrosetolife@gmail.com>
2026-01-07 16:38:57 -0500

    function_auto_path ..\love\public\src\app_a_input.mjs

--- public/src/app_a_input.mjs
+++ public/src/app_a_input.mjs
@@ -1,17 +1,7 @@
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
-import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
-import { html_focus } from "../../../love/public/src/html_focus.mjs";
+import { app_a_input_generic } from "../../../love/public/src/app_a_input_generic.mjs";
 import { html_input } from "../../../love/public/src/html_input.mjs";
 export function app_a_input(body) {
   let fn = html_input;
-  let input = fn(body);
-  html_focus(input);
-  app_a_control_style(input);
-  html_width_full(input);
-  html_style_assign(input, {
-    "border-width": "1px",
-    "border-color": "gray",
-  });
+  let input = app_a_input_generic(fn, body);
   return input;
 }
---
+++ public/src/app_a_input_generic.mjs
@@ -0,0 +1,15 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
+import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
+import { html_focus } from "../../../love/public/src/html_focus.mjs";
+export function app_a_input_generic(fn, body) {
+  let input = fn(body);
+  html_focus(input);
+  app_a_control_style(input);
+  html_width_full(input);
+  html_style_assign(input, {
+    "border-width": "1px",
+    "border-color": "gray",
+  });
+  return input;
+}
