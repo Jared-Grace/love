5d4283aa5e64d199401ab6f2eb1b4dea21211590
Grace-Jared <christrosetolife@gmail.com>
2026-01-06 21:03:14 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/app_a_function_node.mjs
+++ public/src/app_a_function_node.mjs
@@ -243,7 +243,7 @@ export function app_a_function_node(a) {
       let quasis = object_property_get(node, "quasis");
       let expressions = object_property_get(node, "expressions");
       function lambda22(q) {}
-      each_pair_or_null(quasis, lambda22);
+      each_pair_or_null(quasis, expressions, lambda22);
     },
   };
   function o_props() {
