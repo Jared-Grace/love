0bf1d01a5f9ffe6f1622089138c132945333bbf6
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:01:40 -0500

    function_auto_path ..\love\public\src\data_identifiers_get.mjs

--- web/public/src/data_identifiers_get.mjs
+++ web/public/src/data_identifiers_get.mjs
@@ -1,7 +1,8 @@
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
 export async function data_identifiers_get() {
+  const property = "identifiers";
   let d_path = data_path();
-  let v = await data_property_get("identifiers", d_path);
+  let v = await data_property_get(property, d_path);
   return v;
 }
