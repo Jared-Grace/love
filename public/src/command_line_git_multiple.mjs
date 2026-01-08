1ee78e7640b513eb172828ba2bb5ea3e00e7691e
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:25:36 -0500

    function_auto_path ..\love\public\src\command_line_git_multiple.mjs

--- public/src/command_line_git_multiple.mjs
+++ public/src/command_line_git_multiple.mjs
@@ -1,8 +1,8 @@
+import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function command_line_git_multiple(commands) {
   marker("1");
-  let results = await list_map_async(commands, command_line);
+  let results = await list_map_async(commands, command_line_git);
   return results;
 }
