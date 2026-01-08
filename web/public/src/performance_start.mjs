f61bc99a7085a534071cfd82af863c7dbd8b93c7
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:40:04 -0500

    data_file_update ..\love\public\src\performance_start.mjs

--- web/public/src/performance_start.mjs
+++ web/public/src/performance_start.mjs
@@ -1,7 +1,6 @@
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
-export function performance_start() {
-  const name = "start";
+export function performance_start(category) {
   let p = [];
- performance_next(p, name);
+ performance_next(p, category);
   return p;
 }
