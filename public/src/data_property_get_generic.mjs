a9c6dc85cfce0781df4b3fd41357e1201b5c796d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:03:29 -0500

    function_auto_path ..\love\public\src\data_property_get_generic.mjs

---
+++ public/src/data_property_get_generic.mjs
@@ -0,0 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { data_get } from "../../../love/public/src/data_get.mjs";
+export async function data_property_get_generic(d_path, property_name) {
+  marker("1");
+  let v = await data_get(property_name, null, d_path);
+  let data = object_property_get(v, "data");
+  let value = object_property_get(data, property_name);
+  return value;
+}
