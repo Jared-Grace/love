bac65c22135907d5eefbadcf475b44f993b7d106
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:35:16 -0500

    pndb p

--- public/src/performance_start.mjs
+++ public/src/performance_start.mjs
@@ -2,6 +2,6 @@ import { performance_next } from "../../../love/public/src/performance_next.mjs"
 export function performance_start() {
   const name = "start";
   let measurements = [];
-  const p = performance_next(name, p);
+  const p = performance_next(p, name);
   return p;
 }
