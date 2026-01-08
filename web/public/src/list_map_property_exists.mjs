9decdad08ef4bfabddbf4cba4e7e4e75f3173423
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:16:37 -0500

    fws list_map_property exists

--- web/public/src/list_map_property_exists.mjs
+++ web/public/src/list_map_property_exists.mjs
@@ -1,4 +1,6 @@
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_map_property_exists() {
+export function list_map_property_exists(list, property_name) {
   marker("1");
+  return list_map_property(list, property_name);
 }
