644a32ab8a24a4cc905486d63e51fdb72c41643d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:02:47 -0500

    function_auto_path ..\love\public\src\data_watch_get.mjs

--- public/src/data_watch_get.mjs
+++ public/src/data_watch_get.mjs
@@ -1,5 +1,7 @@
+import { data_path } from "../../../love/public/src/data_path.mjs";
 import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
 export async function data_watch_get() {
+  let d_path = data_path();
   const property_name = "watch";
   let w = await data_property_get(d_path, property_name);
   return w;
