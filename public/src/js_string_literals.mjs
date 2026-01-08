4b0edf16b6d13c3e260c3fb06d9c3a0590c3486d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:27:41 -0500

    function_auto_path ..\love\public\src\js_string_literals.mjs

--- public/src/js_string_literals.mjs
+++ public/src/js_string_literals.mjs
@@ -1,7 +1,7 @@
 import { string_is } from "../../../love/public/src/string_is.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
-import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { js_visit_types } from "./js_visit_types.mjs";
 export function js_string_literals(ast) {
   function lambda2(la) {
     function lambda(v) {
@@ -12,7 +12,7 @@ export function js_string_literals(ast) {
         la(value);
       }
     }
-    js_visit_type(ast, "Literal", lambda);
+    js_visit_types(ast, ["Literal", "TemplateLiteral"], lambda);
   }
   const names = list_adder_unique(lambda2);
   return names;
