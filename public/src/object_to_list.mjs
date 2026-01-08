d2e805df5427cbef7be3668c77207e38928bea95
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:23:05 -0500

    function_auto_path ..\love\public\src\object_to_list.mjs

--- public/src/object_to_list.mjs
+++ public/src/object_to_list.mjs
@@ -1,16 +1,7 @@
-import { object_values } from "../../../love/public/src/object_values.mjs";
-import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
+import { object_to_list_names } from "../../../love/public/src/object_to_list_names.mjs";
 export function object_to_list(o) {
   const value_name = "value";
   const key_name = "key";
-  function lambda3(value, key) {
-    let v = {
-      [value_name]: value,
-      [key_name]: key,
-    };
-    return v;
-  }
-  let result = object_values_map(o, lambda3);
-  let list = object_values(result);
+  let list = object_to_list_names(value_name, key_name, o);
   return list;
 }
