c38555e8744c7f9c66e317eec5d68d737156ced6
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:27:28 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- web/public/src/git_remove.mjs
+++ web/public/src/git_remove.mjs
@@ -1,3 +1,4 @@
+import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitignore_overwrite.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { git_add } from "../../../love/public/src/git_add.mjs";
 import { file_transform } from "../../../love/public/src/file_transform.mjs";
@@ -15,4 +16,5 @@ export async function git_remove(f_path) {
   let added = g_name;
   await git_add(added);
   await git_commit("Remove " + f_path + " and add to " + g_name);
+  await repos_gitignore_overwrite();
 }
