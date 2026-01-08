be072ba701e88a4ec23149aa0df90e5fefbbb20d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:40:01 -0500

    function_auto_path ..\love\public\src\js_auto.mjs

--- public/src/js_auto.mjs
+++ public/src/js_auto.mjs
@@ -1,6 +1,5 @@
-import { file_overwrite_cached } from "../../../love/public/src/file_overwrite_cached.mjs";
+import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { performance_end } from "../../../love/public/src/performance_end.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
@@ -8,11 +7,12 @@ import { performance_start } from "../../../love/public/src/performance_start.mj
 import { js_auto_transforms } from "../../../love/public/src/js_auto_transforms.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function js_auto(ast) {
+  await lambda();
+  return;
   let d_path = data_path();
   let r = await file_transform_cached(d_path, lambda);
   return;
   log(r);
-
   async function lambda() {
     const p = performance_start(js_auto.name);
     let transforms = js_auto_transforms();
@@ -25,10 +25,3 @@ export async function js_auto(ast) {
     return r;
   }
 }
-async function file_transform_cached(d_path, lambda) {
-  await file_read_cached(d_path);
-  let r = await lambda();
-  await file_overwrite_cached(d_path);
-  return r;
-}
-
