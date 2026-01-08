5eb3cd0a9de52ef7b2770754743c59010a54d00b
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:21:03 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- public/src/function_open_app.mjs
+++ public/src/function_open_app.mjs
@@ -1,16 +1,11 @@
+import { data_app_current_set } from "../../../love/public/src/data_app_current_set.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
-import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
-import { data_set } from "../../../love/public/src/data_set.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_open_app(f_name) {
   marker("1");
   let a_name = app_name_main(f_name);
   let v = await function_open(a_name);
-  async function lambda(previous) {
-    return f_name;
-  }
-  let d_path = user_repo_path();
-  await data_set(lambda, "app_current", d_path);
+  await data_app_current_set(f_name);
   return v;
 }
