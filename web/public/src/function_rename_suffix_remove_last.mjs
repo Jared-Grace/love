6f22b4e32a71e06de4d79d90a71264f54a57a71d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:48:23 -0500

    function_auto_path ..\love\public\src\function_rename_suffix_remove_last.mjs

--- web/public/src/function_rename_suffix_remove_last.mjs
+++ web/public/src/function_rename_suffix_remove_last.mjs
@@ -1,11 +1,8 @@
 import { function_name_to_part_last } from "../../../love/public/src/function_name_to_part_last.mjs";
 import { function_rename_suffix_remove } from "../../../love/public/src/function_rename_suffix_remove.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_rename_suffix_remove_last(
-  f_name_before,
-  suffix,
-) {
-  let from = function_name_to_part_last(f_name_before2);
+export async function function_rename_suffix_remove_last(f_name_before) {
+  let suffix = function_name_to_part_last(f_name_before);
   marker("1");
   let v = await function_rename_suffix_remove(f_name_before, suffix);
   return v;
