8343b41575ae4a462374e85184d2b7137278780e
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:26:38 -0500

    function_auto_path ..\love\public\src\js_auto.mjs

--- public/src/js_auto.mjs
+++ public/src/js_auto.mjs
@@ -1,6 +1,7 @@
 import { js_auto_transforms } from "../../../love/public/src/js_auto_transforms.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function js_auto(ast) {
+  const start = performance.now();
   let transforms = js_auto_transforms();
   async function lambda(t) {
     await t(ast);
