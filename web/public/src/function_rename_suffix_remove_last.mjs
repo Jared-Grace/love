2d73249f44a406eb73411855f13d22901df32d46
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:47:50 -0500

    fws rsr last

--- web/public/src/function_rename_suffix_remove_last.mjs
+++ web/public/src/function_rename_suffix_remove_last.mjs
@@ -1,4 +1,9 @@
+import { function_rename_suffix_remove } from "../../../love/public/src/function_rename_suffix_remove.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function function_rename_suffix_remove_last() {
+export async function function_rename_suffix_remove_last(
+  f_name_before,
+  suffix,
+) {
   marker("1");
+  return await function_rename_suffix_remove(f_name_before, suffix);
 }
