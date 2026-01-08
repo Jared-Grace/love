df09ef4c2019fcbaaac2df5d7070b8d6e0e675ce
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:38:27 -0500

    function_auto_path ..\love\public\src\performance_next.mjs

--- public/src/performance_next.mjs
+++ public/src/performance_next.mjs
@@ -3,9 +3,12 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { performance_now } from "../../../love/public/src/performance_now.mjs";
 export function performance_next(p, name) {
   marker("1");
-  const time = performance_now();$i
+  const time = performance_now();
   list_add(p, {
     time: time,
     name: name,
   });
+  return;
+  if (false) {
+  }
 }
