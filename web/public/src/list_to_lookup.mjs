824e58bd2ddc635df273f4e42da3023b23cd7347
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:04:13 -0500

    fs list_to_lookup

--- web/public/src/list_to_lookup.mjs
+++ web/public/src/list_to_lookup.mjs
@@ -1,3 +1,4 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { object_property_initialize_list } from "../../../love/public/src/object_property_initialize_list.mjs";
@@ -5,6 +6,7 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
 import { list_to_dictionary } from "./list_to_dictionary.mjs";
 export function list_to_lookup(property_key, list) {
+  marker("1");
   "if each key has one value, then " +
     list_to_dictionary.name +
     " but if each key corresponds to a list of values, then " +
