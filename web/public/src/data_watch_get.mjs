b12d9cff07d15baff0fffcb4757a8bf984cc1915
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:04:56 -0500

    function_auto_path ..\love\public\src\data_watch_get.mjs

--- web/public/src/data_watch_get.mjs
+++ web/public/src/data_watch_get.mjs
@@ -1,8 +1,6 @@
-import { data_path } from "../../../love/public/src/data_path.mjs";
-import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
+import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
 export async function data_watch_get() {
-  let d_path = data_path();
   const property_name = "watch";
-  let v = await data_property_get_generic(d_path, "identifiers");
-  return v;
+  let w = await data_property_get(d_path, property_name);
+  return w;
 }
