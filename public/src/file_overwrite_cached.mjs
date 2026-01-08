099a74f113036a24650f0120aa056f024caa9c44
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:55:21 -0500

    data_file_update ..\love\public\src\file_overwrite_cached.mjs

--- public/src/file_overwrite_cached.mjs
+++ public/src/file_overwrite_cached.mjs
@@ -2,9 +2,9 @@ import { global_function_property_set } from "../../../love/public/src/global_fu
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function file_overwrite_in_memory(f_path) {
+export async function file_overwrite_cached(f_path) {
   marker("1");
   let contents = await file_read(f_path);
-  global_function_property_set(file_overwrite_in_memory, f_path, contents);
+  global_function_property_set(file_overwrite_cached, f_path, contents);
   let result = await file_overwrite(f_path, after);
 }
