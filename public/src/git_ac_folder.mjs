c3d12dd402facbcdde85c67b6d09bbdd563a3ef3
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:22:20 -0500

    rs git_add_folder all

--- public/src/git_ac_folder.mjs
+++ public/src/git_ac_folder.mjs
@@ -1,8 +1,8 @@
-import { git_add_folder } from "../../../love/public/src/git_add_folder.mjs";
+import { git_add_folder_all } from "../../../love/public/src/git_add_folder_all.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { git_commit_folder } from "../../../love/public/src/git_commit_folder.mjs";
 export async function git_ac_folder(folder, message) {
   marker("1");
-  await git_add_folder(folder);
+  await git_add_folder_all(folder);
   await git_commit_folder(folder, message);
 }
--- public/src/git_add_folder.mjs
+++
@@ -1,4 +0,0 @@
-import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
-export async function git_add_folder(folder) {
-  await command_line_git_folder(folder, "add -A");
-}
