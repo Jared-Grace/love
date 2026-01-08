df2f047db7dabd14dae6d3408026e23bfe49b3da
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:03:32 -0500

    rs data_property_get generic

--- public/src/function_run_prompt.mjs
+++ public/src/function_run_prompt.mjs
@@ -1,6 +1,6 @@
 import { log_error } from "../../../love/public/src/log_error.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
+import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
 import { chalk_green } from "../../../love/public/src/chalk_green.mjs";
 import { function_run_line_git } from "../../../love/public/src/function_run_line_git.mjs";
 import { command_line_read } from "../../../love/public/src/command_line_read.mjs";
@@ -9,7 +9,7 @@ import { data_path } from "../../../love/public/src/data_path.mjs";
 export async function function_run_prompt() {
   const property_name = "error_attention";
   let d_path = data_path();
-  let ea = await data_property_get(d_path, property_name);
+  let ea = await data_property_get_generic(d_path, property_name);
   let nn = null_not_is(ea);
   if (nn) {
     await log_error(property_name);
