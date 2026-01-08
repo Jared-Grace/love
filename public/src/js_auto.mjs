65b8c218a4a6b07560a2e443488eae08da58d607
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:29:39 -0500

    function_auto_path ..\love\public\src\js_auto.mjs

--- public/src/js_auto.mjs
+++ public/src/js_auto.mjs
@@ -1,8 +1,10 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
 import { performance_now } from "../../../love/public/src/performance_now.mjs";
 import { subtract } from "../../../love/public/src/subtract.mjs";
 import { js_auto_transforms } from "../../../love/public/src/js_auto_transforms.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function js_auto(ast) {
+  let measurements = [];
   const start = performance_now();
   let transforms = js_auto_transforms();
   async function lambda(t) {
@@ -13,5 +15,5 @@ export async function js_auto(ast) {
   const end = performance_now();
   let difference = subtract(end, start);
   return;
-  let measurements = {};
+  list_add(list, item);
 }
