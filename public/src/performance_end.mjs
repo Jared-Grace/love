ff4f0f10fdf1700dce1f27f0059e77063642092a
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:05:53 -0500

    function_auto_path ..\love\public\src\performance_end.mjs

--- public/src/performance_end.mjs
+++ public/src/performance_end.mjs
@@ -3,9 +3,9 @@ import { list_to_lookup } from "../../../love/public/src/list_to_lookup.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
 export function performance_end(p) {
   performance_next(p, "end");
-  let lookup = list_to_lookup(p, "category");
+  let categories = list_to_lookup(p, "category");
   log({
-    lookup,
+    lookup: categories,
   });
   return;
 }
