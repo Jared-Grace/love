a21e8c5f0f164db72941982e85c66d8e9bbfe400
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:30:56 -0500

    function_auto_path ..\love\public\src\performance_start.mjs

---
+++ public/src/performance_next.mjs
@@ -0,0 +1,11 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { performance_now } from "../../../love/public/src/performance_now.mjs";
+export function performance_next(name) {
+  let measurements = [];
+  const start = performance_now();
+  list_add(measurements, {
+    time: start,
+    name: name,
+  });
+  return start;
+}
