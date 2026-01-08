e706147347335326a5a5ebd9f513039bdf57e096
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:07:43 -0500

    function_auto_path ..\love\public\src\performance_end.mjs

--- public/src/performance_end.mjs
+++ public/src/performance_end.mjs
@@ -1,3 +1,4 @@
+import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_to_lookup } from "../../../love/public/src/list_to_lookup.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
@@ -8,5 +9,5 @@ export function performance_end(p) {
     lookup: categories,
   });
   return;
-  omv;
+  let result = object_values_map(object, function lambda(value, key) {});
 }
