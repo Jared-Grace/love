ff61a4e6512ae18df674441032351981819760be
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:17:00 -0500

    function_auto_path ..\love\public\src\list_map_property_exists.mjs

--- public/src/list_map_property_exists.mjs
+++ public/src/list_map_property_exists.mjs
@@ -2,7 +2,8 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_map_property_exists(list, property_name) {
-  let filtered = list_filter(list2, function lambda(item) {});
+  function lambda(item) {}
+  let filtered = list_filter(list2, lambda);
   marker("1");
   let mapped = list_map_property(list, property_name);
   return mapped;
