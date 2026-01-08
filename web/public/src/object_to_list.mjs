6e201ffa49fe987fdd5c789794a3c4276c9ec191
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:23:38 -0500

    psw value_name o

--- web/public/src/object_to_list.mjs
+++ web/public/src/object_to_list.mjs
@@ -2,6 +2,6 @@ import { object_to_list_names } from "../../../love/public/src/object_to_list_na
 export function object_to_list(o) {
   const value_name = "value";
   const key_name = "key";
-  let list = object_to_list_names(value_name, o, key_name);
+  let list = object_to_list_names(o, value_name, key_name);
   return list;
 }
