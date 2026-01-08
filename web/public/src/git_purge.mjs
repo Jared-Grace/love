ca0acc1b8b5b1e5d31683bf19294f800a19c5640
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:28:16 -0500

    function_auto_path ..\love\public\src\git_purge.mjs

--- web/public/src/git_purge.mjs
+++ web/public/src/git_purge.mjs
@@ -1,18 +1,9 @@
-import { git_purge_everyone } from "../../../love/public/src/git_purge_everyone.mjs";
-import { command_line_git_multiple } from "../../../love/public/src/command_line_git_multiple.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
+import { git_purge_only } from "../../../love/public/src/git_purge_only.mjs";
 import { git_remove } from "../../../love/public/src/git_remove.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function git_purge() {
   marker("1");
   const f_path = "firebase-debug.log";
   await git_remove(f_path);
-  await command_line("pip install git-filter-repo");
-  let commands = [
-    "filter-repo --" + f_path + " --invert-paths",
-    "push --force --all",
-    "push --force --tags",
-  ];
-  await command_line_git_multiple(commands);
-  await git_purge_everyone();
+  await git_purge_only(f_path);
 }
---
+++ web/public/src/git_purge_only.mjs
@@ -0,0 +1,13 @@
+import { git_purge_everyone } from "../../../love/public/src/git_purge_everyone.mjs";
+import { command_line_git_multiple } from "../../../love/public/src/command_line_git_multiple.mjs";
+import { command_line } from "../../../love/public/src/command_line.mjs";
+export async function git_purge_only(f_path) {
+  await command_line("pip install git-filter-repo");
+  let commands = [
+    "filter-repo --" + f_path + " --invert-paths",
+    "push --force --all",
+    "push --force --tags",
+  ];
+  await command_line_git_multiple(commands);
+  await git_purge_everyone();
+}
