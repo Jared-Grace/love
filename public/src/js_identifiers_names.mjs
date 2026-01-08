b697b9a09f27e1b348353199aaba009d39cb6b8f
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:23:53 -0500

    fs js_identifiers_names

--- public/src/js_identifiers_names.mjs
+++ public/src/js_identifiers_names.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 export function js_identifiers_names(ast) {
+  marker("1");
   function lambda2(la) {
     function lambda(v) {
       let { node } = v;
