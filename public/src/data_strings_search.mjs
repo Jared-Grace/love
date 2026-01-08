396b9308dd7a92a5d5d30ab8f5bc8008142461ed
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:00:55 -0500

    function_auto_path ..\love\public\src\data_strings_search.mjs

--- public/src/data_strings_search.mjs
+++ public/src/data_strings_search.mjs
@@ -1,9 +1,8 @@
 import { data_identifiers_search_generic } from "../../../love/public/src/data_identifiers_search_generic.mjs";
-import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function data_strings_search(s) {
   marker("1");
-  let fn = data_identifiers_get;
+  let fn = data_strings_get;
   let result = await data_identifiers_search_generic(fn, s);
   return result;
 }
