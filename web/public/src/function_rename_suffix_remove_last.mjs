7d7f0dadd177984896a63e1b8e0542b97b4cacae
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:47:50 -0500

    data_file_update ..\love\public\src\function_rename_suffix_remove_last.mjs

--- web/public/src/function_rename_suffix_remove_last.mjs
+++ web/public/src/function_rename_suffix_remove_last.mjs
@@ -1,1 +1,4 @@
-export function function_rename_suffix_remove_last() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function function_rename_suffix_remove_last() {
+  marker("1");
+}
