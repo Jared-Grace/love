919ea3b2175ade6f342b02be01cfbdfc1df71ba5
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:19:06 -0500

    pb

--- web/public/src/list_map_property_exists.mjs
+++ web/public/src/list_map_property_exists.mjs
@@ -2,7 +2,7 @@ import { list_filter_property_exists } from "../../../love/public/src/list_filte
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_map_property_exists(list, property_name) {
-  let filtered = list_filter_property_exists(property_name, list);
+  let filtered = list_filter_property_exists(list, property_name);
   marker("1");
   let mapped = list_map_property(filtered, property_name);
   return mapped;
