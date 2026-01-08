e4fde100b5e47d5fac3ba138849f19fc95e70a08
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:20:13 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- web/public/src/file_overwrite.mjs
+++ web/public/src/file_overwrite.mjs
@@ -1,3 +1,4 @@
+import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { json_compress } from "../../../love/public/src/json_compress.mjs";
@@ -12,7 +13,7 @@ import { file_path_normalize } from "../../../love/public/src/file_path_normaliz
 import { marker } from "../../../love/public/src/marker.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
-export async function file_overwrite(file_path, contents) {at
+export async function file_overwrite(file_path, contents) {
   marker("1");
   if (browser_is()) {
     file_path = file_path_normalize(file_path);
@@ -51,4 +52,5 @@ export async function file_overwrite(file_path, contents) {at
   let fs = await import("fs");
   await fs.promises.writeFile(file_path, contents, "utf-8");
   return;
+  string_is_assert(value);
 }
