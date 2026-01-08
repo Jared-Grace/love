fa00882da8c7bad4dd97a048a0a74959de140c52
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:26:02 -0500

    function_auto_path ..\love\public\src\js_string_literals.mjs

--- web/public/src/js_string_literals.mjs
+++ web/public/src/js_string_literals.mjs
@@ -1,4 +1,15 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
+import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function js_string_literals(ast) {
-  marker("1");
+  function lambda2(la) {
+    function lambda(v) {
+      let node = object_property_get(v, "node");
+      let value = object_property_get(node, "name");
+      la(value);
+    }
+    js_visit_type(ast, "Identifier", lambda);
+  }
+  const names = list_adder_unique(lambda2);
+  return names;
 }
