3bbc7730d063491146b68f79b3c4b982234f1e12
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 14:17:46 -0500

    function_auto_path ..\love\public\src\app_bible_screens.mjs

--- web/public/src/app_bible_screens.mjs
+++ web/public/src/app_bible_screens.mjs
@@ -1,6 +1,6 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_bible_screens() {
   marker("screens");
-  let v = {};
-  return v;
+  let screens = {};
+  return screens;
 }
