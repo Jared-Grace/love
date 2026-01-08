8244011ef3cdc3aa1de3240b134b45d6aec1eeba
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:32:39 -0500

    fws function_param_new_double beginning

--- web/public/src/function_param_new_double_beginning.mjs
+++ web/public/src/function_param_new_double_beginning.mjs
@@ -1,1 +1,6 @@
-export function function_param_new_double_beginning() {}
+import { function_param_new_double } from "../../../love/public/src/function_param_new_double.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_param_new_double_beginning(param_name) {
+  marker("1");
+  return await function_param_new_double(param_name);
+}
