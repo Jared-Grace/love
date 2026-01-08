25cd0156c73c0c7eef094c400bf3d30553b371c5
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:27:40 -0500

    function_auto_path ..\love\public\src\list_sort_string_property.mjs

--- web/public/src/list_sort_string_property.mjs
+++ web/public/src/list_sort_string_property.mjs
@@ -1,11 +1,8 @@
+import { object_property_get_curry_right } from "../../../love/public/src/object_property_get_curry_right.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_sort_string_mapper } from "../../../love/public/src/list_sort_string_mapper.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function list_sort_string_property(languages, property_name) {
   marker("1");
-  let lambda10 = function lambda(item) {
-    let value = object_property_get(item, property_name);
-    return value;
-  };
+  let lambda10 = object_property_get_curry_right(property_name);
   list_sort_string_mapper(languages, lambda10);
 }
