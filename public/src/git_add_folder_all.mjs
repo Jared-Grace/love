08a86bb5ae107bf8ffa83f1760d48b9b68288cd6
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:22:18 -0500

    function_auto_path ..\love\public\src\git_add_folder_all.mjs

---
+++ public/src/git_add_folder_all.mjs
@@ -0,0 +1,6 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
+export async function git_add_folder_all(folder) {
+  marker("1");
+  await command_line_git_folder(folder, "add -A");
+}
