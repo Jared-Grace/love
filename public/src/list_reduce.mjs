74052bea79e333520bbaa417516b34be9b78c29c
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:12:28 -0500

    fs list_reduce

--- public/src/list_reduce.mjs
+++ public/src/list_reduce.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function list_reduce(inital, reducer, mapped) {
+  marker("1");
   let value = inital;
   function lambda2(item) {
     value = reducer(item, value);
