ba6f4fc8ee33dab97e3b0441251fc70bc8dde603
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 13:59:48 -0500

    function_auto_path ..\love\public\src\app_bible_main.mjs

--- public/src/app_bible_main.mjs
+++ public/src/app_bible_main.mjs
@@ -1,3 +1,5 @@
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { html_bar_content } from "../../../love/public/src/html_bar_content.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
@@ -11,14 +13,18 @@ export async function app_bible_main(context) {
   let root = html_mobile_default(context);
   firebase_name_jg();
   html_margin_0(root);
+  let bc = html_bar_content(root);
+  let content = object_property_get(bc, "content");
+  let bar = object_property_get(bc, "bar");
   let e = ebible_folder_english();
   let hash = html_hash_object_get();
   let chapter_code = object_property_get(hash, "c");
+  let div = html_div_text(root2, text2);
   let verses = await ebible_verses(e, chapter_code);
   function lambda(v) {
     let verse_number_v = object_property_get(v, "verse_number");
     let text = object_property_get(v, "text");
-    let p = html_p_text(root, verse_number_v + " " + text);
+    let p = html_p_text(content, verse_number_v + " " + text);
   }
   each(verses, lambda);
 }
