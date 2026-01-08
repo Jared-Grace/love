5eb7b389cf134ea54df805a047a9a2163d2769f6
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:03:31 -0500

    data_file_update ..\love\public\src\data_property_get_generic.mjs

--- web/public/src/data_marker_current_get.mjs
+++ web/public/src/data_marker_current_get.mjs
@@ -1,8 +1,8 @@
 import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
-import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
+import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
 export async function data_marker_current_get() {
   const property_name = "marker_current";
   let d_path = user_repo_path();
-  let f_name = await data_property_get(d_path, property_name);
+  let f_name = await data_property_get_generic(d_path, property_name);
   return f_name;
 }
