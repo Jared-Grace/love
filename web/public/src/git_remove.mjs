7c58c69fdea0f69eb7999ba9f409583ea6044974
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:12:04 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- web/public/src/git_remove.mjs
+++ web/public/src/git_remove.mjs
@@ -1,12 +1,14 @@
+import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
 export async function git_remove() {
   const f_path = "firebase-debug.log";
+  let g_name = git_ignore_name();
   await command_line_git("rm --cached " + f_path);
   await command_line_git(command_git);
   async function lambda(item) {
-    await git_commit(message);
+    await git_commit("Remove " + f_path + " and add to " + g_name);
   }
   await each_async(list, lambda);
 }
