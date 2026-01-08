6db6ec112600e967457a063fcebb0b1bba36bdba
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:31:40 -0500

    data_file_update ..\love\public\src\performance_next.mjs

--- public/src/performance_next.mjs
+++ public/src/performance_next.mjs
@@ -1,7 +1,6 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { performance_now } from "../../../love/public/src/performance_now.mjs";
 export function performance_next(name) {
-  let measurements = [];
   const start = performance_now();
   list_add(measurements, {
     time: start,
