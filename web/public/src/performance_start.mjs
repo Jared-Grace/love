9dfda3718b15021dea68fbb0ebf8716308180676
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:31:11 -0500

    function_auto_path ..\love\public\src\performance_start.mjs

--- web/public/src/performance_start.mjs
+++ web/public/src/performance_start.mjs
@@ -2,4 +2,5 @@ import { performance_next } from "../../../love/public/src/performance_next.mjs"
 export function performance_start() {
   const name = "start";
   const p = performance_next(name);
+  return p;
 }
