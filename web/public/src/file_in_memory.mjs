4eb2c7082881f4cb716998c53b7b6e0ec39e9c2d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:48:44 -0500

    function_auto_path ..\love\public\src\file_in_memory.mjs

--- web/public/src/file_in_memory.mjs
+++ web/public/src/file_in_memory.mjs
@@ -1,7 +1,10 @@
-import { file_transform } from "../../../love/public/src/file_transform.mjs";
+import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
+import { file_read } from "../../../love/public/src/file_read.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_in_memory(f_path) {
   marker("1");
   async function lambda2() {}
-  await file_transform(f_path, lambda2);
+  let contents = await file_read(f_path);
+  let after = lambda2(contents);
+  let result = await file_overwrite(f_path, after);
 }
