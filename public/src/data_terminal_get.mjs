49c214922c01959191996bb5f129591d157f725e
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:03:38 -0500

    data_file_update ..\love\public\src\git_push_folder.mjs

--- public/src/data_terminal_get.mjs
+++ public/src/data_terminal_get.mjs
@@ -2,9 +2,9 @@ import { data_path } from "../../../love/public/src/data_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
 export async function data_terminal_get() {
-  let d_path = data_path();
   marker("1");
   const property_name = "terminal";
+  let d_path = data_path();
   let w = await data_property_get_generic(d_path, property_name);
   return w;
 }
