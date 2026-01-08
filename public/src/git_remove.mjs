e4c144aa27a896d8add876fec3f56bc0b75ff5bf
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:18:40 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -1,16 +1,16 @@
-import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitignore_overwrite.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
+import { newline } from "./newline.mjs";
 export async function git_remove() {
   const f_path = "firebase-debug.log";
   let g_name = git_ignore_name();
   await command_line_git(command_git);
   function lambda(before) {
-    let joined = list_join_newline(list);
+    before + newline() + after;
   }
   let contents = await file_read(g_name);
   let result = await file_overwrite(g_name, contents);
