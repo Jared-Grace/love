310a6896acfaaa21e52cb50d35f43f03b723cf0c
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:28:04 -0500

    function_auto_path ..\love\public\src\object_property_get_curry_right.mjs

--- public/src/object_property_get_curry_right.mjs
+++ public/src/object_property_get_curry_right.mjs
@@ -1,8 +1,8 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function object_property_get_curry_right(property_name) {
-  let v = function lambda(item) {
+  let f = function lambda(item) {
     let value = object_property_get(item, property_name);
     return value;
   };
-  return v;
+  return f;
 }
