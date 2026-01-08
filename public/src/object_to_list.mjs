e774fad224afc34361ed5385c4e59f9ea721d2e8
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:23:54 -0500

    data_file_update ..\love\public\src\object_to_list.mjs

--- public/src/object_to_list.mjs
+++ public/src/object_to_list.mjs
@@ -1,7 +1,7 @@
 import { object_to_list_names } from "../../../love/public/src/object_to_list_names.mjs";
 export function object_to_list(o) {
-  const value_name = "value";
   const key_name = "key";
+  const value_name = "value";
   let list = object_to_list_names(o, key_name, value_name);
   return list;
 }
