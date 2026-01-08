a7277e0578d67f00f0c9ff02f15844de1f31cab5
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:33:49 -0500

    function_auto_path ..\love\public\src\function_param_new_double_beginning.mjs

--- web/public/src/function_param_new_double_beginning.mjs
+++ web/public/src/function_param_new_double_beginning.mjs
@@ -2,5 +2,6 @@ import { function_param_new_double } from "../../../love/public/src/function_par
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_param_new_double_beginning(param_name) {
   marker("1");
-  return v;v=await function_param_new_double(param_name);
+  return v;
+  let v = await function_param_new_double(param_name);
 }
