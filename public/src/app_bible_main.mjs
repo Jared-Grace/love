909d08ee0fc76526528ed0489b90f8c628a12f09
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 13:56:57 -0500

    function_auto_path ..\love\public\src\app_bible_main.mjs

--- public/src/app_bible_main.mjs
+++ public/src/app_bible_main.mjs
@@ -1,3 +1,4 @@
+import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
@@ -6,6 +7,7 @@ import { html_hash_object_get } from "../../../love/public/src/html_hash_object_
 import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
 export async function app_bible_main(context) {
+  let v2 = ebible_folder_english();
   let root = html_mobile_default(context);
   html_margin_0(root);
   let hash = html_hash_object_get();
