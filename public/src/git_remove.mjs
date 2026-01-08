1187a35769331e08c726f390f8b8953fe3c1bfa1
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:15:48 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -2,17 +2,13 @@ import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitign
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
 export async function git_remove() {
   const f_path = "firebase-debug.log";
   let g_name = git_ignore_name();
   await command_line_git(command_git);
-  async function lambda(item) {
-    let contents = await file_read(f_path);
-    await command_line_git("rm --cached " + f_path);
-    await git_commit("Remove " + f_path + " and add to " + g_name);
-  }
-  await each_async(list, lambda);
+  let contents = await file_read(f_path);
+  await command_line_git("rm --cached " + f_path);
+  await git_commit("Remove " + f_path + " and add to " + g_name);
   await repos_gitignore_overwrite();
 }
