55b71d9a190fbae7e5ff23b438f7a870a5a0b184
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:08:28 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -2,4 +2,5 @@ import { command_line_git } from "../../../love/public/src/command_line_git.mjs"
 export async function git_remove() {
   const f_path = "firebase-debug.log";
   await command_line_git("rm --cached " + f_path);
+  await command_line_git(command_git);
 }
