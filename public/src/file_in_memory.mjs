1f35b47221c48f96c4391b00210766315f230ae3
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:48:27 -0500

    function_auto_path ..\love\public\src\file_in_memory.mjs

--- public/src/file_in_memory.mjs
+++ public/src/file_in_memory.mjs
@@ -1,4 +1,6 @@
+import { file_transform } from "../../../love/public/src/file_transform.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function file_in_memory(f_path) {
+export async function file_in_memory(f_path) {
   marker("1");
+  await file_transform(g_name, async function lambda2() {});
 }
