5610ccdd03d6382c8126845f8d752c4ec0a0c1a6
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:26:30 -0500

    function_auto_path ..\love\public\src\performance_end.mjs

--- public/src/performance_end.mjs
+++ public/src/performance_end.mjs
@@ -1,5 +1,5 @@
+import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { list_map_property_exists } from "../../../love/public/src/list_map_property_exists.mjs";
 import { list_sum } from "../../../love/public/src/list_sum.mjs";
 import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
@@ -20,7 +20,6 @@ export function performance_end(p) {
     summary,
   });
   return;
-  let list = object_to_list(summary);
-  let mapped2 = list_map_property(list, "value");
-  let value2 = list_sum(mapped2);
+  let list = object_to_list(summary, "category", "delta");
+  list_sort_number_mapper(list2, function lambda2(item) {});
 }
