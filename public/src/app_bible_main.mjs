043ce664df650b6c8cea865c075830b536255243
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 13:51:47 -0500

    function_auto_path ..\love\public\src\app_bible_main.mjs

--- public/src/app_bible_main.mjs
+++ public/src/app_bible_main.mjs
@@ -10,6 +10,9 @@ export async function app_bible_main(context) {
   let hash = html_hash_object_get();
   let chapter_code = object_property_get(hash, "c");
   let verses = await ebible_verses(bible_folder, chapter_code);
-  function lambda(item) {}
-  each(list, lambda);
+  function lambda(item) {
+    let verse_number_v = object_property_get(v, "verse_number");
+    let text = object_property_get(v, "text");
+  }
+  each(verses, lambda);
 }
