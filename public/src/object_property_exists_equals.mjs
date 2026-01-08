217eaa658a9a7eb446c6df62d5692136ab9aa1f9
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:18:16 -0500

    data_file_update ..\love\public\src\object_property_exists_equals.mjs

--- public/src/object_property_exists_equals.mjs
+++ public/src/object_property_exists_equals.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_equals } from "../../../love/public/src/object_property_equals.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-export function object_property_exists_equals(path, in_progress, value) {
+export function object_property_exists_equals(in_progress, path, value) {
   marker("1");
   let v =
     object_property_exists(path) &&
