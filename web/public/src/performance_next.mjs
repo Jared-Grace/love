d399e4d541bf8b114e4ca517257caac1182f99dc
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:39:58 -0500

    function_auto_path ..\love\public\src\performance_next.mjs

--- web/public/src/performance_next.mjs
+++ web/public/src/performance_next.mjs
@@ -1,6 +1,6 @@
+import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
-import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { performance_now } from "../../../love/public/src/performance_now.mjs";
@@ -12,10 +12,11 @@ export function performance_next(p, name) {
     name: name,
   });
   return;
-  let ne = list_empty_not_is(p);
-  if (ne) {
+  let e = list_empty_is(p);
+  if (e) {
     let last = list_last(p);
     let time_previous = object_property_get(last, "time");
     let delta = time - time_previous;
+  } else {
   }
 }
