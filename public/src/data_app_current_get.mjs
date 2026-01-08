505b1dc2cb7e08b6e9d09bd5e53a3b3b5ee21b4f
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:03:31 -0500

    data_file_update ..\love\public\src\data_property_get_generic.mjs

--- public/src/data_app_current_get.mjs
+++ public/src/data_app_current_get.mjs
@@ -1,9 +1,9 @@
 import { null_not_is_assert } from "../../../love/public/src/null_not_is_assert.mjs";
-import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
+import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
 import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
 export async function data_app_current_get() {
   let d_path = user_repo_path();
-  let a = await data_property_get(d_path, "app_current");
+  let a = await data_property_get_generic(d_path, "app_current");
   null_not_is_assert(a);
   return a;
 }
--- public/src/data_identifiers_fn_names_get.mjs
+++ public/src/data_identifiers_fn_names_get.mjs
@@ -1,9 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
+import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
 export async function data_identifiers_fn_names_get() {
   marker("1");
   let d_path = data_path();
-  let v = await data_property_get(d_path, "identifiers_fn_names");
+  let v = await data_property_get_generic(d_path, "identifiers_fn_names");
   return v;
 }
