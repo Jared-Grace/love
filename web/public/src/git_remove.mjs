bbe673dd1ee4b536dc82fd3464544d8308e4b469
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:12:52 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- web/public/src/git_remove.mjs
+++ web/public/src/git_remove.mjs
@@ -1,3 +1,4 @@
+import { function_read } from "../../../love/public/src/function_read.mjs";
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -7,6 +8,7 @@ export async function git_remove() {
   let g_name = git_ignore_name();
   await command_line_git(command_git);
   async function lambda(item) {
+    let v = await function_read(f_name);
     await command_line_git("rm --cached " + f_path);
     await git_commit("Remove " + f_path + " and add to " + g_name);
   }
