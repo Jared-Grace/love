e25ab8e884081c97f312069e2523e05bd54868eb
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:35:15 -0500

    data_file_update ..\love\public\src\performance_next.mjs

--- web/public/src/performance_next.mjs
+++ web/public/src/performance_next.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { performance_now } from "../../../love/public/src/performance_now.mjs";
-export function performance_next(name) {
+export function performance_next(p, name) {
   marker("1");
   const start = performance_now();
   list_add(p, {
