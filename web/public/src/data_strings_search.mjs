5c1c7844df5b2d87076e40fbd5c3b6dc6dc8e488
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:10:35 -0500

    function_auto_path ..\love\public\src\data_strings_search.mjs

--- web/public/src/data_strings_search.mjs
+++ web/public/src/data_strings_search.mjs
@@ -1,3 +1,4 @@
+import { data_strings_get } from "../../../love/public/src/data_strings_get.mjs";
 import { data_identifiers_search_generic } from "../../../love/public/src/data_identifiers_search_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function data_strings_search(s) {
