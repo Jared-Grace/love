5caf0e8dd3c3b10b8d96ce1f8e50516d32b50e66
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:29:14 -0500

    function_auto_path ..\love\public\src\performance_end.mjs

--- web/public/src/performance_end.mjs
+++ web/public/src/performance_end.mjs
@@ -1,5 +1,4 @@
-import { object_property_get_curry_right } from "../../../love/public/src/object_property_get_curry_right.mjs";
-import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
+import { list_sort_number_property } from "../../../love/public/src/list_sort_number_property.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 import { list_map_property_exists } from "../../../love/public/src/list_map_property_exists.mjs";
 import { list_sum } from "../../../love/public/src/list_sum.mjs";
@@ -23,6 +22,5 @@ export function performance_end(p) {
   return;
   let list = object_to_list(summary, "category", "delta");
   function lambda2(item) {}
-  let f = object_property_get_curry_right("delta");
-  list_sort_number_mapper(list, f);
+  list_sort_number_property(list);
 }
