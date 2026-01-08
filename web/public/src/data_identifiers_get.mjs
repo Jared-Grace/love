25ee3f6c207095a6f7ca68ffb0cd9a3c184a0421
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:03:31 -0500

    data_file_update ..\love\public\src\data_property_get_generic.mjs

--- web/public/src/data_identifiers_get.mjs
+++ web/public/src/data_identifiers_get.mjs
@@ -1,8 +1,8 @@
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
+import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
 export async function data_identifiers_get() {
   const property = "identifiers";
   let d_path = data_path();
-  let v = await data_property_get(d_path, property);
+  let v = await data_property_get_generic(d_path, property);
   return v;
 }
