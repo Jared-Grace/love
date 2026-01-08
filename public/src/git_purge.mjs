3bf8311910c8430bb5764192ea96d438c486e87f
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:24:05 -0500

    function_auto_path ..\love\public\src\git_purge.mjs

--- public/src/git_purge.mjs
+++ public/src/git_purge.mjs
@@ -1,5 +1,6 @@
+import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
+import { command_line } from "../../../love/public/src/command_line.mjs";
 import { git_remove } from "../../../love/public/src/git_remove.mjs";
-import { command_line_multiple } from "../../../love/public/src/command_line_multiple.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitignore_overwrite.mjs";
 export async function git_purge() {
@@ -7,9 +8,7 @@ export async function git_purge() {
   const f_path = "firebase-debug.log";
   await git_remove(f_path);
   await repos_gitignore_overwrite();
-  let commands = [
-    "pip install git-filter-repo",
-    "git filter-repo --" + f_path + " --invert-paths",
-  ];
-  await command_line_multiple(commands);
+  let commands = [, "git filter-repo --" + f_path + " --invert-paths"];
+  await command_line("pip install git-filter-repo");
+  await command_line_git(command_git);
 }
