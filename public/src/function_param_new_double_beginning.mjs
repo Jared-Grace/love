305aaa9560f124358bdbb13612acbc133155fb38
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 15:34:08 -0500

    data_file_update ..\love\public\src\function_param_new_double_beginning.mjs

--- public/src/function_param_new_double_beginning.mjs
+++ public/src/function_param_new_double_beginning.mjs
@@ -4,6 +4,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_param_new_double_beginning(param_name) {
   marker("1");
   let v = await function_param_new_double(param_name);
-  await function_param_move_first(param_name2);
+  await function_param_move_first(param_name);
   return v;
 }
