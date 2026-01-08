40c97650c7724cedb60fa1716bfd159f86868c1a
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:32:06 -0500

    function_auto_path ..\love\public\src\git_purge_only.mjs

--- web/public/src/git_purge_only.mjs
+++ web/public/src/git_purge_only.mjs
@@ -3,9 +3,10 @@ import { command_line_git_multiple } from "../../../love/public/src/command_line
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function git_purge_only(f_path) {
   await command_line("pip install git-filter-repo");
-  let stdout = await command_line(command);
-  let commands = [
+  let stdout = await command_line(
     "filter-repo  --force --path " + f_path + " --invert-paths",
+  );
+  let commands = [
     "remote add origin https://github.com/Jared-Grace/love.git",
     "push --force --all",
     "push --force --tags",
