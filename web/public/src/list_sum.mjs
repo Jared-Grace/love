0211143a10bf64ed41fef41525f69d8433938dd5
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:15:01 -0500

    data_file_update ..\love\public\src\performance_end.mjs

--- web/public/src/list_sum.mjs
+++ web/public/src/list_sum.mjs
@@ -1,8 +1,8 @@
 import { list_reduce } from "../../../love/public/src/list_reduce.mjs";
 import { add } from "../../../love/public/src/add.mjs";
-export function list_sum(mapped) {
+export function list_sum(list) {
   let inital = 0;
   let reducer = add;
-  let value = list_reduce(mapped, reducer, inital);
+  let value = list_reduce(list, reducer, inital);
   return value;
 }
