e080ae85397e631926e14a8c6e713ba2828f1208
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:07:00 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function git_remove() {
-  marker("1");
+import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
+export async function git_remove() {
+  await command_line_git(command_git);
 }
