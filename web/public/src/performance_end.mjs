3dd8ca256d103c1fdecda4870733059faf8c7d21
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:05:18 -0500

    function_auto_path ..\love\public\src\performance_end.mjs

--- web/public/src/performance_end.mjs
+++ web/public/src/performance_end.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_to_lookup } from "../../../love/public/src/list_to_lookup.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
 export function performance_end(p) {
   performance_next(p, "end");
+  let lookup = list_to_lookup(p, "category");
   return;
-  let lookup = list_to_lookup(list, "category");
+  log({
+    lookup,
+  });
 }
