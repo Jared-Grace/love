e37cf88b489b0afd9bc62cf57ae3b6613838f0fe
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 08:41:16 -0500

    function_auto_path ..\love\public\src\app_bible_main.mjs

--- web/public/src/app_bible_main.mjs
+++ web/public/src/app_bible_main.mjs
@@ -1,4 +1,6 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function app_bible_main() {
-  marker("1");
+import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
+import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
+export function app_bible_main(context) {
+  let root = html_mobile_default(context);
+  html_margin_0(root);
 }
