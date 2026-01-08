53be045b116ce630584779ea8fca63cfafefedb9
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:48:37 -0500

    function_auto_path ..\love\public\src\file_transform.mjs

--- public/src/file_transform.mjs
+++ public/src/file_transform.mjs
@@ -1,7 +1,7 @@
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
-export async function file_transform(g_name, lambda) {
-  let contents = await file_read(g_name);
+export async function file_transform(f_path, lambda) {
+  let contents = await file_read(f_path);
   let after = lambda(contents);
-  let result = await file_overwrite(g_name, after);
+  let result = await file_overwrite(f_path, after);
 }
