fb820fc2f42d0228505666370a58e01ddf80df75
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 17:17:19 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- web/public/src/git_remove.mjs
+++ web/public/src/git_remove.mjs
@@ -1,3 +1,4 @@
+import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitignore_overwrite.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
@@ -8,7 +9,7 @@ export async function git_remove() {
   let g_name = git_ignore_name();
   await command_line_git(command_git);
   let contents = await file_read(f_path);
-  fov;
+  let result = await file_overwrite(file_path, contents2);
   await command_line_git("rm --cached " + f_path);
   await git_commit("Remove " + f_path + " and add to " + g_name);
   await repos_gitignore_overwrite();
