e12a27c74805791ac0a239b7c1dc1d08e5ec45dd
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 16:03:23 -0500

    function_auto_path ..\love\public\src\list_to_dictionary.mjs

--- public/src/performance_end.mjs
+++ public/src/performance_end.mjs
@@ -1,4 +1,5 @@
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
 export function performance_end(p) {
-  performance_next(p, "end");
+  performance_next(p, "end");return
+  list_to_lookup
 }
