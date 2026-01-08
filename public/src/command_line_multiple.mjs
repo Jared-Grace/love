7c1cefd682f5e431839adfc0c7a5fe5c0c533d4d
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 18:22:59 -0500

    function_auto_path ..\love\public\src\command_line_multiple.mjs

--- public/src/command_line_multiple.mjs
+++ public/src/command_line_multiple.mjs
@@ -1,6 +1,6 @@
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function command_line_multiple(commands) {
-  let v = await list_map_async(commands, command_line);
-  return v;
+  let results = await list_map_async(commands, command_line);
+  return results;
 }
