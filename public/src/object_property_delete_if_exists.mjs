87a2494a555e60e693cc62b3e64b4c7dd765a449
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 18:01:51 -0500

    function_auto_path ..\love\public\src\object_properties_delete_if_exists.mjs

---
+++ public/src/object_property_delete_if_exists.mjs
@@ -0,0 +1,8 @@
+import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
+import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+export function object_property_delete_if_exists(data, p) {
+  let exists = object_property_exists(data, p);
+  if (exists) {
+    object_property_delete(data, p);
+  }
+}
