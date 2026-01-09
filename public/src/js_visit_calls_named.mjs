b3955bce3d64bd12cf17f7ba776ef136e1c5a24b
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 16:09:32 -0500

    fs js_visit_calls_named

--- public/src/js_visit_calls_named.mjs
+++ public/src/js_visit_calls_named.mjs
@@ -1,8 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
 export function js_visit_calls_named(f_name, lambda, ast) {
+  marker("1");
   function lambda_inner(v) {
     let { node } = v;
     let name = js_call_callee_name(node);
