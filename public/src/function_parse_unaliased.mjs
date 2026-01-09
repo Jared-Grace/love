8506768cbefde2fd92296d20451eaa16d047d47a
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:23:48 -0500

    function_auto_path ..\love\public\src\function_parse_unaliased.mjs

--- public/src/function_parse_unaliased.mjs
+++ public/src/function_parse_unaliased.mjs
@@ -1,19 +1,9 @@
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { function_parse_unaliased_second } from "../../../love/public/src/function_parse_unaliased_second.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
-import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export async function function_parse_unaliased(f_name) {
   marker("1");
   const v = await function_name_to_path_unalias(f_name);
-  let unaliased = object_property_get(v, "unaliased");
-  let f_path = object_property_get(v, "f_path");
-  let parsed = await file_js_parse(f_path);
-  let to = object_merge(
-    {
-      unaliased,
-    },
-    parsed,
-  );
+  let to = await function_parse_unaliased_second(v);
   return to;
 }
---
+++ public/src/function_parse_unaliased_second.mjs
@@ -0,0 +1,15 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export async function function_parse_unaliased_second(v) {
+  let unaliased = object_property_get(v, "unaliased");
+  let f_path = object_property_get(v, "f_path");
+  let parsed = await file_js_parse(f_path);
+  let to = object_merge(
+    {
+      unaliased,
+    },
+    parsed,
+  );
+  return to;
+}
