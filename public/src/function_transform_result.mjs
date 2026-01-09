3a9cf82af8c00f17cdc9adfdfd3ce495edce1525
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:50:40 -0500

    s not,exists,file

--- public/src/function_transform_result.mjs
+++ public/src/function_transform_result.mjs
@@ -1,20 +1,18 @@
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { function_parse_unaliased_second } from "../../../love/public/src/function_parse_unaliased_second.mjs";
+import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 export async function function_transform_result(f_name, lambda$ast) {
-  const v = await function_name_to_path_unalias(f_name);
-  let f_path = object_property_get(v, "f_path");
   async function lambda2() {
-    let parsed = await function_parse_unaliased_second(v);
+    let parsed = await function_parse_unaliased(f_name);
     let ast = object_property_get(parsed, "ast");
     let result = await lambda$ast(ast);
     await file_js_unparse(parsed);
     return result;
   }
-  let r = await file_transform_cached(f_path, lambda2);
-  return r;
   let d_path = data_path();
+  let r = await file_transform_cached(d_path, lambda2);
+  return r;
 }
