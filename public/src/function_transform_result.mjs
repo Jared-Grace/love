839e8acffaf385a7ccae2a7183a7d33d76ba34f5
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:24:07 -0500

    function_auto_path ..\love\public\src\function_transform_result.mjs

--- public/src/function_transform_result.mjs
+++ public/src/function_transform_result.mjs
@@ -1,3 +1,4 @@
+import { function_parse_unaliased_second } from "../../../love/public/src/function_parse_unaliased_second.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -13,7 +14,7 @@ export async function function_transform_result(f_name, lambda$ast) {
   const v = await function_name_to_path_unalias(f_name);
   {
     async function lambda2() {
-      let parsed = await function_parse_unaliased(f_name);
+      let parsed = await function_parse_unaliased_second(v);
       let ast = object_property_get(parsed, "ast");
       let f_path = object_property_get(parsed, "f_path");
       let result = await lambda$ast(ast);
