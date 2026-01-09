315e2aeadbeee58fb790452cfcb72a17af65cc62
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 22:11:51 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize_download.mjs

--- public/src/app_a_file_system_initialize_download.mjs
+++ public/src/app_a_file_system_initialize_download.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
 import { list_property_exists_not_error } from "../../../love/public/src/list_property_exists_not_error.mjs";
@@ -13,7 +14,9 @@ import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_index
 export async function app_a_file_system_initialize_download() {
   let db = await app_a_indexeddb_initialize();
   let db_get = lambda_get(db);
-  let r = await app_api_fn(app_a_download);
+  let r = await app_api_fn({
+    fn_name: app_a_download,
+  });
   list_property_exists_not_error(r, "key");
   let dictionary = list_to_dictionary_property(r, "key");
   let store = app_a_file_system_store();
