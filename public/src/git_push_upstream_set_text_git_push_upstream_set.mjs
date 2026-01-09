36807eb3dcc8f4236551bef57c68eb0967ed068b
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 19:18:19 -0500

    function_auto_path ..\love\public\src\git_push_upstream_set_text_git_push_upstream_set.mjs

--- public/src/git_push_upstream_set_text_git_push_upstream_set.mjs
+++ public/src/git_push_upstream_set_text_git_push_upstream_set.mjs
@@ -2,5 +2,6 @@ import { git_push_upstream_set_text } from "../../../love/public/src/git_push_up
 import { marker } from "../../../love/public/src/marker.mjs";
 export function git_push_upstream_set_text_git_push_upstream_set() {
   marker("1");
-  return git_push_upstream_set_text();
+  let v = git_push_upstream_set_text();
+  return v;
 }
