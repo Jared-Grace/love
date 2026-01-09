f7830adde2b2b6fa5d209c7ba75779f0284ef633
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 18:00:40 -0500

    cs object_properties_delete if_exists

--- web/public/src/object_properties_delete_if_exists.mjs
+++ web/public/src/object_properties_delete_if_exists.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
 export function object_properties_delete_if_exists(data, properties) {
+  marker("1");
   function lambda2(p) {
     object_property_delete(data, p);
   }
