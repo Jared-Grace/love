e0e9e0b220cbcbbed413c70f48c69f512044358b
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:11:33 -0500

    function_auto_path ..\love\public\src\function_transform_result.mjs

--- web/public/src/function_transform_result.mjs
+++ web/public/src/function_transform_result.mjs
@@ -1,3 +1,6 @@
+import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
+import { js_auto } from "../../../love/public/src/js_auto.mjs";
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
@@ -7,9 +10,8 @@ export async function function_transform_result(f_name, lambda$ast) {
   let result = await lambda$ast(ast);
   await file_js_unparse(parsed);
   return result;
-
-    async function lambda2() {
-      await function_transform(f_name, js_auto);
-    }
-    let r = await file_transform_cached(d_path, lambda2);
+  async function lambda2() {
+    await function_transform(f_name, js_auto);
+  }
+  let r = await file_transform_cached(d_path, lambda2);
 }
