c10eab95c7f4425038ca92d5ecdb716a585f4d6c
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 13:23:57 -0500

    function_auto_path ..\love\public\src\list_join_comma.mjs

--- public/src/list_join_comma.mjs
+++ public/src/list_join_comma.mjs
@@ -1,5 +1,5 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
-export function list_join_comma(names) {
-  let result = list_join(names, ",");
+export function list_join_comma(list) {
+  let result = list_join(list, ",");
   return result;
 }
