dca46cc659806a4ebd6caffe688ea2f8e0b8194f
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:05:50 -0500

    function_auto_path ..\love\public\src\file_read_cached.mjs

--- web/public/src/file_read_cached.mjs
+++ web/public/src/file_read_cached.mjs
@@ -1,4 +1,4 @@
-import { assert } from "../../../love/public/src/assert.mjs";
+import { assert_not } from "../../../love/public/src/assert_not.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
@@ -6,7 +6,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read_cached(f_path) {
   marker("1");
   let exists = global_function_property_exists(file_read_cached, f_path);
-  assert(exists);
+  assert_not(exists);
   let contents = await file_read(f_path);
   global_function_property_set(file_read_cached, f_path, contents);
 }
