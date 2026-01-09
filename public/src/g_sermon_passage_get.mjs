0cf534a1a9ab3c7caaf5183e8d7d5ab7ab3f7e3b
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 13:36:26 -0500

    function_auto_path ..\love\public\src\g_sermon_passage_get.mjs

--- public/src/g_sermon_passage_get.mjs
+++ public/src/g_sermon_passage_get.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_join_newline_2 } from "../../../love/public/src/list_join_newline_2.mjs";
 import { app_g_openai_split } from "../../../love/public/src/app_g_openai_split.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -15,6 +16,7 @@ export async function g_sermon_passage_get(chapter_code) {
   let passages = object_property_get(chapter, "passages");
   function lambda(v) {
     let verse_numbers = object_property_get(v, "verse_numbers");
+    let first = list_first(list);
     let result = list_join_comma_space(verse_numbers);
     let text = object_property_get(v, "text");
     let sermon = object_property_get(v, "sermon");
