79b7d8db6a5a54bc18000b0666830b1ba1e74435
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 14:24:21 -0500

    function_auto_path ..\love\public\src\g_sermon_passage_get.mjs

--- web/public/src/g_sermon_passage_get.mjs
+++ web/public/src/g_sermon_passage_get.mjs
@@ -1,19 +1,14 @@
+import { g_sermon_generate_chapter_passages_get } from "../../../love/public/src/g_sermon_generate_chapter_passages_get.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_join_newline_2 } from "../../../love/public/src/list_join_newline_2.mjs";
 import { app_g_openai_split } from "../../../love/public/src/app_g_openai_split.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
-import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
-import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export async function g_sermon_passage_get(chapter_code, verse_number) {
   marker("1");
-  let fn = g_sermon_generate;
-  let path = local_function_path_json(chapter_code, fn);
-  let chapter = await file_read_json(path);
-  let passages = object_property_get(chapter, "passages");
+  let passages = await g_sermon_generate_chapter_passages_get(chapter_code);
   function lambda(v) {
     let verse_numbers = object_property_get(v, "verse_numbers");
     let first = list_first(verse_numbers);
