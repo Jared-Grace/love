dc765125e058bd1725be0cc1bfa50d7e1990c1d2
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 20:17:59 -0500

    function_auto_path ..\love\public\src\server.mjs

--- web/public/src/server.mjs
+++ web/public/src/server.mjs
@@ -37,7 +37,8 @@ export function server() {
   let uo = server_url_api_ordered();
   app.post(uo, api_ordered);
   async function api_ordered(req, res) {
-    ordering = ordering.then(() => {});
+    function lambda2() {}
+    ordering = ordering.then(lambda2);
     await api_generic(req, res);
   }
   app.use(v);
