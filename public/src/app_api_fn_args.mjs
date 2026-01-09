a9238d24b8515b5e2a9e43029fea8cca768322d2
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 22:12:57 -0500

    function_auto_path ..\love\public\src\app_api_fn_args.mjs

--- public/src/app_api_fn_args.mjs
+++ public/src/app_api_fn_args.mjs
@@ -1,7 +1,9 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_api } from "../../../love/public/src/app_api.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_api_fn_args(fn, args) {
+export async function app_api_fn_args(a) {
   marker("1");
+  let fn2 = object_property_get(a, "fn");
   let f_name = fn.name;
   let r = await app_api(f_name, args);
   return r;
