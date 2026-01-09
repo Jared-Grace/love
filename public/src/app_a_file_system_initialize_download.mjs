8a7348dc055d89c958158f717aa0cfaf85e1df37
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 22:09:52 -0500

    rs app_api_fn args

--- public/src/app_a_file_system_initialize_download.mjs
+++ public/src/app_a_file_system_initialize_download.mjs
@@ -7,13 +7,13 @@ import { app_a } from "../../../love/public/src/app_a.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 import { app_a_file_system_store } from "../../../love/public/src/app_a_file_system_store.mjs";
 import { app_a_download } from "../../../love/public/src/app_a_download.mjs";
-import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
+import { app_api_fn_args } from "../../../love/public/src/app_api_fn_args.mjs";
 import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
 export async function app_a_file_system_initialize_download() {
   let db = await app_a_indexeddb_initialize();
   let db_get = lambda_get(db);
-  let r = await app_api_fn(app_a_download, []);
+  let r = await app_api_fn_args(app_a_download, []);
   list_property_exists_not_error(r, "key");
   let dictionary = list_to_dictionary_property(r, "key");
   let store = app_a_file_system_store();
--- public/src/app_api_fn.mjs
+++
@@ -1,8 +0,0 @@
-import { app_api } from "../../../love/public/src/app_api.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_api_fn(fn, args) {
-  marker("1");
-  let f_name = fn.name;
-  let r = await app_api(f_name, args);
-  return r;
-}
