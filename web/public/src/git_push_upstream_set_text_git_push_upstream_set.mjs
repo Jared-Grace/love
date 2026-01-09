0ea09f991861c91b8b45cb9871bd948f00eb7729
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 19:19:24 -0500

    r git_push_upstream_set_text_git_push_upstream_set git_push_upstream_set

--- web/public/src/git_push_upstream_set_text_git_push_upstream_set.mjs
+++
@@ -1,9 +0,0 @@
-import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
-import { git_push_upstream_set_text } from "../../../love/public/src/git_push_upstream_set_text.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function git_push_upstream_set_text_git_push_upstream_set() {
-  marker("1");
-  let c = git_push_upstream_set_text();
-  let v = await command_line_git(c);
-  return v;
-}
