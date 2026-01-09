fb7eba00d66b089181cfbb3a1bd9d069ebd738e0
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 22:09:51 -0500

    function_auto_path ..\love\public\src\app_api_fn_args.mjs

---
+++ public/src/app_api_fn_args.mjs
@@ -0,0 +1,8 @@
+import { app_api } from "../../../love/public/src/app_api.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function app_api_fn_args(fn, args) {
+  marker("1");
+  let f_name = fn.name;
+  let r = await app_api(f_name, args);
+  return r;
+}
