1d1deb6b25e890361b314271fbb03a6eb1f0e19a
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 19:17:50 -0500

    function_auto_path ..\love\public\src\git_purge_only_after.mjs

--- web/public/src/git_purge_only_after.mjs
+++ web/public/src/git_purge_only_after.mjs
@@ -1,9 +1,11 @@
+import { git_push_upstream_set_text } from "../../../love/public/src/git_push_upstream_set_text.mjs";
 import { git_purge_everyone } from "../../../love/public/src/git_purge_everyone.mjs";
 import { command_line_git_multiple } from "../../../love/public/src/command_line_git_multiple.mjs";
 export async function git_purge_only_after() {
+  let v = git_push_upstream_set_text();
   let commands = [
     "remote add origin https://github.com/Jared-Grace/love.git",
-    "push --set-upstream origin main",
+    v,
     "push --force --all",
     "push --force --tags",
   ];
