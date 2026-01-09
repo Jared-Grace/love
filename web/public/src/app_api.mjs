91b26da3a2dc838a5903913c724d8d0ccd1e865f
Grace-Jared <christrosetolife@gmail.com>
2026-01-08 22:13:51 -0500

    function_auto_path ..\love\public\src\app_api.mjs

--- web/public/src/app_api.mjs
+++ web/public/src/app_api.mjs
@@ -1,3 +1,4 @@
+import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -8,6 +9,7 @@ export async function app_api(a) {
   string_is_assert(f_name);
   marker("1");
   let fn = http_post_json;
+  object_property_set_exists_not(object, property_name, value);
   let result = await app_api_generic(f_name, args, fn);
   return result;
 }
