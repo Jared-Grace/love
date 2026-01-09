6f4124465d434d3775a39a67cd34a7fc7c938bb2
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:08:47 -0500

    function_auto_path ..\love\public\src\function_auto.mjs

--- public/src/function_auto.mjs
+++ public/src/function_auto.mjs
@@ -1,3 +1,4 @@
+import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { js_auto } from "../../../love/public/src/js_auto.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,4 +6,5 @@ export async function function_auto(f_name) {
   marker("1");
   await function_transform(f_name, js_auto);
   return;
+  let r = await file_transform_cached(d_path, async function lambda2() {});
 }
