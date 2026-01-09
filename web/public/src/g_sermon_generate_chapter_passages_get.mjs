c2c26ff080bb9a63b404cb3912abf757d1310f5b
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 14:26:26 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_chapter_passages_get.mjs

--- web/public/src/g_sermon_generate_chapter_passages_get.mjs
+++ web/public/src/g_sermon_generate_chapter_passages_get.mjs
@@ -1,11 +1,7 @@
+import { g_sermon_generate_chapter_get } from "../../../love/public/src/g_sermon_generate_chapter_get.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
-import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
-import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
 export async function g_sermon_generate_chapter_passages_get(chapter_code) {
-  let fn = g_sermon_generate;
-  let path = local_function_path_json(chapter_code, fn);
-  let chapter = await file_read_json(path);
+  let chapter = await g_sermon_generate_chapter_get(chapter_code);
   let passages = object_property_get(chapter, "passages");
   return passages;
 }
