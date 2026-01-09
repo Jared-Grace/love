56980f79dd46cfe5d7bfc43639e06dc74bbae9c5
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 13:31:39 -0500

    function_auto_path ..\love\public\src\g_sermon_chapter_get.mjs

--- web/public/src/g_sermon_chapter_get.mjs
+++ web/public/src/g_sermon_chapter_get.mjs
@@ -1,10 +1,11 @@
-import { list_join_comma } from "../../../love/public/src/list_join_comma.mjs";
+import { app_g_openai_split } from "../../../love/public/src/app_g_openai_split.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
 import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { list_join_comma_space } from "./list_join_comma_space.mjs";
 export async function g_sermon_chapter_get(chapter_code) {
   marker("1");
   let fn = g_sermon_generate;
@@ -12,9 +13,10 @@ export async function g_sermon_chapter_get(chapter_code) {
   let chapter = await file_read_json(path);
   function lambda(v) {
     let verse_numbers = object_property_get(v, "verse_numbers");
-    let result = list_join_comma(names);
+    let result = list_join_comma_space(verse_numbers);
     let text = object_property_get(v, "text");
     let sermon = object_property_get(v, "sermon");
+    let mapped2 = app_g_openai_split(objections);
   }
-  let mapped = list_map(list, lambda);
+  let mapped = list_map(chapter, lambda);
 }
