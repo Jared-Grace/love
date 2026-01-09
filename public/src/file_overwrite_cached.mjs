d0aad618d52c5d44e116d41182a3f32f5310d375
Grace-Jared <christrosetolife@gmail.com>
2026-01-09 17:44:03 -0500

    function_auto_path ..\love\public\src\file_overwrite_uncached.mjs

--- public/src/file_overwrite_cached.mjs
+++ public/src/file_overwrite_cached.mjs
@@ -1,12 +1,12 @@
 import { global_function_property_delete } from "../../../love/public/src/global_function_property_delete.mjs";
 import { global_function_property_get } from "../../../love/public/src/global_function_property_get.mjs";
-import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { global_function_get } from "../../../love/public/src/global_function_get.mjs";
 import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
+import { file_overwrite_uncached } from "../../../love/public/src/file_overwrite_uncached.mjs";
 export async function file_overwrite_cached(f_path) {
 console.log(global_function_get(file_read_cached))
   let contents = global_function_property_get(file_read_cached, f_path);
-  await file_overwrite(f_path, contents);
-  
+  await file_overwrite_uncached(f_path, contents);
+
   global_function_property_delete(file_read_cached, f_path);
 }
