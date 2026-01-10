f8c128946eb62005e512a6f358b817015c1caa1e
Grace-Jared <christrosetolife@gmail.com>
2026-01-10 11:10:53 -0500

    function_auto_path ..\love\public\src\file_overwrite_uncached.mjs

--- web/public/src/data_file_update.mjs
+++ web/public/src/data_file_update.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
 import { data_file_update_inner } from "../../../love/public/src/data_file_update_inner.mjs";
 import { data_save } from "../../../love/public/src/data_save.mjs";
@@ -5,10 +6,12 @@ import { data_all } from "../../../love/public/src/data_all.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 export async function data_file_update(f_path) {
+  console.log(f_path);
   let d_path = data_path();
   var d = await data_all(d_path);
   let parsed = await file_js_parse(f_path);
-  data_file_update_inner(parsed, d);
+  await data_file_update_inner(parsed, d);
   await data_save(d);
   marker("1");
+  return;
 }
