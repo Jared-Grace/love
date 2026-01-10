2390ae8a6e8c0915678ca035fd0ac385868536e1
Grace-Jared <christrosetolife@gmail.com>
2026-01-10 11:16:16 -0500

    o file_read_cached

--- web/public/src/file_read_cached.mjs
+++ web/public/src/file_read_cached.mjs
@@ -5,7 +5,8 @@ import { file_read } from "../../../love/public/src/file_read.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { global_function_get } from "../../../love/public/src/global_function_get.mjs";
 export async function file_read_cached(f_path) {
-  marker("1");console.log('here')
+  marker("1");
+  console.log("here");
   let exists = global_function_property_exists(file_read_cached, f_path);
   assert_not(exists);
   let contents = await file_read(f_path);
