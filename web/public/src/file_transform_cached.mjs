23ec23518bc516d49036a65769263366d01c19cb
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 15:04:34 -0500

    function_auto_path ..\love\public\src\file_transform_cached.mjs

--- web/public/src/file_transform_cached.mjs
+++ web/public/src/file_transform_cached.mjs
@@ -1,8 +1,10 @@
 import { file_overwrite_cached } from "../../../love/public/src/file_overwrite_cached.mjs";
 import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
 export async function file_transform_cached(d_path, lambda) {
-  await file_read_cached(d_path);
+  let overwrite = await file_read_cached(d_path);
   let r = await lambda();
   await file_overwrite_cached(d_path);
   return r;
+  if (false) {
+  }
 }
