9e7688238819a7b50141559008844057c9cf08ad
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 15:04:14 -0500

    function_auto_path ..\love\public\src\file_read_cached.mjs

--- public/src/file_read_cached.mjs
+++ public/src/file_read_cached.mjs
@@ -6,11 +6,13 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read_cached(f_path) {
   marker("1");
   let exists = global_function_property_exists(file_read_cached, f_path);
+  if (exists) {
+    let v2 = false;
+    return v2;
+  }
   assert_not(exists);
   let contents = await file_read(f_path);
   global_function_property_set(file_read_cached, f_path, contents);
   let v = true;
   return v;
-  if (false) {
-  }
 }
