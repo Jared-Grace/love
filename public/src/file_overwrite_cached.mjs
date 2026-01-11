3306b21482a7ce4473329d1c5847766be4826661
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 17:38:51 -0500

    is file_transform_cached

--- public/src/file_overwrite_cached.mjs
+++ public/src/file_overwrite_cached.mjs
@@ -3,7 +3,7 @@ import { global_function_property_get } from "../../../love/public/src/global_fu
 import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
 import { file_overwrite_uncached } from "../../../love/public/src/file_overwrite_uncached.mjs";
 export async function file_overwrite_cached(f_path) {
-  let contents = global_function_property_get(file_read_cached, f_path);
+  let contents = await global_function_property_get(file_read_cached, f_path);
   await file_overwrite_uncached(f_path, contents);
   global_function_property_delete(file_read_cached, f_path);
 }
