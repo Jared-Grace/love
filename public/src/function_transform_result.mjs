279473d78146937d69eec7b02d2b10273436fff8
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 17:40:09 -0500

    function_auto_path ..\love\public\src\function_transform_result.mjs

--- public/src/function_transform_result.mjs
+++ public/src/function_transform_result.mjs
@@ -1,3 +1,5 @@
+import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
+import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 import { data_all_initialize } from "../../../love/public/src/data_all_initialize.mjs";
@@ -12,8 +14,11 @@ export async function function_transform_result(f_name, lambda$ast) {
     await file_js_unparse(parsed);
     return result;
   }
+  await lambda2();
+  return;
   let d_path = data_path();
-  data_all_initialize(d_path);
+  await data_all_initialize(d_path);
   let r = await file_transform_cached(d_path, lambda2);
   return r;
+  let exists = global_function_property_set(file_read_cached, () => {});
 }
