e13cc42df2a402c3b286f9b71c784d6bda29290d
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 17:42:09 -0500

    function_auto_path ..\love\public\src\data_all_initialize.mjs

--- web/public/src/data_all_initialize.mjs
+++ web/public/src/data_all_initialize.mjs
@@ -1,12 +1,7 @@
-import { data_generate } from "../../../love/public/src/data_generate.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
 import { file_write } from "../../../love/public/src/file_write.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
-import { data_path } from "../../../love/public/src/data_path.mjs";
 export async function data_all_initialize(file_path) {
   let exists = await file_exists(file_path);
   if (not(exists)) {
@@ -14,4 +9,3 @@ export async function data_all_initialize(file_path) {
     await file_write(file_path, contents);
   }
 }
-
