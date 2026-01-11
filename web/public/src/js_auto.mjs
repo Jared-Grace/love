9c8cc7278d33c266f56e8dc7455e7a722c35ca36
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 17:45:07 -0500

    function_auto_path ..\love\public\src\js_auto.mjs

--- web/public/src/js_auto.mjs
+++ web/public/src/js_auto.mjs
@@ -1,3 +1,7 @@
+import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
+import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
+import { data_generate } from "../../../love/public/src/data_generate.mjs";
+import { null_is } from "../../../love/public/src/null_is.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { performance_end } from "../../../love/public/src/performance_end.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
@@ -19,4 +23,13 @@ export async function js_auto(ast) {
   await lambda();
   return;
   log(r);
+  let data = null;
+  async function lambda2() {
+    if (null_is(data)) {
+      data = {};
+      await data_generate(data);
+    }
+    return data;
+  }
+  global_function_property_set(file_read_cached, lambda2);
 }
