cdf93ec5d538b7065bd3e909ae1d885d35de307b
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 16:00:33 -0500

    function_auto_path ..\love\public\src\http_post_options.mjs

--- public/src/http_post_options.mjs
+++ public/src/http_post_options.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { http_generic } from "../../../love/public/src/http_generic.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export async function http_post_options(body, options_extra, url) {
+  marker("1");
   const options = {
     method: "POST",
     body: body,
