31ef484d1b7bb3bc587c14c6e850bb18fa831ef7
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:10:07 -0500

    function_auto_path ..\love\public\src\server_url_data.mjs

--- public/src/server_url_data_ending.mjs
+++ public/src/server_url_data_ending.mjs
@@ -1,6 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 export function server_url_data_ending() {
   marker("1");
+  let d_path = data_path();
   let v = "/" + d_path;
   return v;
 }
