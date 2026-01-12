fb08a730546b0efd29ce5ffd2437e0a65439f5d7
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:10:00 -0500

    pd d_path

--- public/src/server_url_data.mjs
+++ public/src/server_url_data.mjs
@@ -4,6 +4,6 @@ import { data_path } from "../../../love/public/src/data_path.mjs";
 export function server_url_data() {
   let d_path = data_path();
   let u = server_url_api();
-  let v3 = u + server_url_data_ending(d_path);
+  let v3 = u + server_url_data_ending();
   return v3;
 }
