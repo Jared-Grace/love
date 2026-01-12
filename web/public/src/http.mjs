474e41a07a5c093bd1131e927456468384d082aa
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:15:31 -0500

    function_auto_path ..\love\public\src\http.mjs

--- web/public/src/http.mjs
+++ web/public/src/http.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { http_generic } from "../../../love/public/src/http_generic.mjs";
 export async function http(url) {
+  marker("1");
   let v = await http_generic(url, {
     method: "GET",
   });
