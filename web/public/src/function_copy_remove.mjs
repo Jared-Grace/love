e756931724503e3c1d890b9eaa3eff66102ec9ac
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:21:05 -0500

    function_auto_path ..\love\public\src\function_copy_remove.mjs

--- web/public/src/function_copy_remove.mjs
+++ web/public/src/function_copy_remove.mjs
@@ -2,5 +2,6 @@ import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_copy_remove(f_name_old, f_name_new) {
   marker("1");
-  return await function_copy(f_name_old, f_name_new);
+  let v = await function_copy(f_name_old, f_name_new);
+  return v;
 }
