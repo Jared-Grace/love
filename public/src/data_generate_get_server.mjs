cc3fe0febf8890eccaf86a5000a88737320fed60
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:07:36 -0500

    function_auto_path ..\love\public\src\data_generate_get_server.mjs

--- public/src/data_generate_get_server.mjs
+++ public/src/data_generate_get_server.mjs
@@ -1,11 +1,11 @@
-import { app_api_generic_url_body } from "../../../love/public/src/app_api_generic_url_body.mjs";
+import { server_url_get } from "../../../love/public/src/server_url_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_generate } from "../../../love/public/src/data_generate.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { data_get } from "../../../love/public/src/data_get.mjs";
 export function data_generate_get_server() {
   marker("1");
-  let v = app_api_generic_url_body(f_name, args);
+  let url = server_url_get();
   let data = null;
   let data_get = async function lambda2() {
     if (null_is(data)) {
