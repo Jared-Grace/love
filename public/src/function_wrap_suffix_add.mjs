2a5656f55cc1139e78c32be4ce0d69116d9a7c75
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:21:13 -0500

    function_auto_path ..\love\public\src\function_wrap_suffix_add.mjs

--- public/src/function_wrap_suffix_add.mjs
+++ public/src/function_wrap_suffix_add.mjs
@@ -1,8 +1,12 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_suffix_add(f_name, suffix) {
-  let { unaliased } = await function_name_unalias(f_name);
+  marker("1");
+  let v2 = await function_name_unalias(f_name);
+  let unaliased = object_property_get(v2, "unaliased");
   let f_name_wrapped = function_name_combine(unaliased, suffix);
   let v = await function_wrap(unaliased, f_name_wrapped);
   return v;
