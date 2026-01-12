d3d9d35e6ec84d0edd6364d7b3ccf5039ffba8cd
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:25:40 -0500

    function_auto_path ..\love\public\src\function_name_parts_remove.mjs

--- web/public/src/function_name_parts_remove.mjs
+++ web/public/src/function_name_parts_remove.mjs
@@ -1,9 +1,9 @@
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
-export function function_name_parts_remove(f_name_old, parts) {
+export function function_name_parts_remove(f_name_old, removals) {
   let parts = function_name_to_parts(f_name_old);
-  let difference = list_difference(parts, parts);
+  let difference = list_difference(parts, removals);
   let f_name_new = function_name_combine_multiple(difference);
   return f_name_new;
 }
