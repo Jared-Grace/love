04987ca1d6873ee0d6744c91ba2e6edc9e1ebd39
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:11:47 -0500

    function_auto_path ..\love\public\src\list_find_starts_with.mjs

--- public/src/list_find_starts_with.mjs
+++ public/src/list_find_starts_with.mjs
@@ -1,12 +1,12 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
-export function list_find_starts_with(item, prefixes) {
+export function list_find_starts_with(prefix, list) {
   marker("1");
   function lambda2(item2) {
-    let sw = string_starts_with(item, item2);
+    let sw = string_starts_with(prefix, item2);
     return sw;
   }
-  let any = list_find(prefixes, lambda2);
+  let any = list_find(list, lambda2);
   return any;
 }
