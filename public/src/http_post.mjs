9ca872d89f4aecc9668b393f0a8d3f865b7c6b83
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 15:59:06 -0500

    function_auto_path ..\love\public\src\http_post.mjs

--- public/src/http_post.mjs
+++ public/src/http_post.mjs
@@ -1,8 +1,9 @@
 import { http_generic } from "../../../love/public/src/http_generic.mjs";
 export async function http_post(url, body) {
-  let v = await http_generic(url, {
+  const options = {
     method: "POST",
     body: body,
-  });
+  };
+  let v = await http_generic(url, options);
   return v;
 }
