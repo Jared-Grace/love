436c9c8f9a0bfa034fddc05a655bba4222c3ffa7
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:38:32 -0500

    function_auto_path ..\love\public\src\server.mjs

--- public/src/server.mjs
+++ public/src/server.mjs
@@ -49,7 +49,8 @@ export function server() {
   let d_path = data_path();
   let data_get = data_generate_get();
   async function lambda3() {
-    return await data_get();
+    let v3 = await data_get();
+    return v3;
   }
   app.post(d_path, lambda3);
   app.use(v);
