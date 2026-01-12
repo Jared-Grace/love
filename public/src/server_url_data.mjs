5b59022afb19be8e22714e4dd54ac97b8290753c
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:09:41 -0500

    function_auto_path ..\love\public\src\server_url_data.mjs

--- public/src/server_url_data.mjs
+++ public/src/server_url_data.mjs
@@ -1,8 +1,9 @@
+import { server_url_data_ending } from "../../../love/public/src/server_url_data_ending.mjs";
 import { server_url_api } from "../../../love/public/src/server_url_api.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 export function server_url_data() {
   let d_path = data_path();
   let u = server_url_api();
-  let v3 = u + "/" + d_path;
+  let v3 = u + server_url_data_ending(d_path);
   return v3;
 }
---
+++ public/src/server_url_data_ending.mjs
@@ -0,0 +1,4 @@
+export function server_url_data_ending(d_path) {
+  let v = "/" + d_path;
+  return v;
+}
