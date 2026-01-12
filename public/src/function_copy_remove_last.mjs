6afd6c53dea52f0c87ecf430388cacc8d46096c0
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:27:40 -0500

    function_auto_path ..\love\public\src\function_copy_remove_last.mjs

--- public/src/function_copy_remove_last.mjs
+++ public/src/function_copy_remove_last.mjs
@@ -3,7 +3,7 @@ import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs"
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_copy } from "../../../love/public/src/function_copy.mjs";
 export async function function_copy_remove_last(f_name_before) {
-  let parts = function_name_to_parts(f_name_before, removals);
+  let parts = function_name_to_parts(f_name_before);
   let removed = list_remove_last(parts);
   let f_name_after = function_name_combine_multiple(parts);
   let v = await function_copy(f_name_before, f_name_after);
