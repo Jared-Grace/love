eb17ee7533fbfff82c41ed975dbe99e041a865a0
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 18:58:49 -0500

    function_auto_path ..\love\public\src\app_name_main_get.mjs

--- web/public/src/app_name_main_get.mjs
+++ web/public/src/app_name_main_get.mjs
@@ -3,19 +3,8 @@ import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_exists } from "../../../love/public/src/function_exists.mjs";
-import { data_app_current_set } from "../../../love/public/src/data_app_current_set.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
-import { function_open } from "../../../love/public/src/function_open.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function function_open_app(f_name) {
-  marker("1");
-  let a_name;
-  ({ a_name, f_name } = await app_name_main_get(f_name));
-  let v = await function_open(a_name);
-  await data_app_current_set(f_name);
-  return v;
-}
-async function app_name_main_get(f_name) {
+export async function app_name_main_get(f_name) {
   let a_name = app_name_main(f_name);
   let v2 = await function_exists(a_name);
   let exists = object_property_get(v2, "exists");
@@ -24,6 +13,9 @@ async function app_name_main_get(f_name) {
     f_name = list_find_starts_with(mapped, f_name);
     a_name = app_name_main(f_name);
   }
-  return { a_name, f_name };
+  let v3 = {
+    a_name,
+    f_name,
+  };
+  return v3;
 }
-
