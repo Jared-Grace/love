8f2f0d2771064bd992e59f87f72b2f797c94e553
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:10:57 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- web/public/src/function_open_app.mjs
+++ web/public/src/function_open_app.mjs
@@ -1,3 +1,4 @@
+import { list_find_starts_with } from "../../../love/public/src/list_find_starts_with.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -6,14 +7,13 @@ import { data_app_current_set } from "../../../love/public/src/data_app_current_
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { list_filter_starts_with } from "./list_filter_starts_with.mjs";
 export async function function_open_app(f_name) {
   marker("1");
   let v2 = await function_exists(f_name);
   let exists = object_property_get(v2, "exists");
   if (not(exists)) {
     let mapped = await apps_names();
-    let filtered = list_filter_starts_with(list, prefix);
+    let any = list_find_starts_with(item, prefixes);
   }
   let a_name = app_name_main(f_name);
   let v = await function_open(a_name);
