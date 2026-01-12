1721edac578ea7541989b98f24a477edd865695a
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:09:30 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- web/public/src/function_open_app.mjs
+++ web/public/src/function_open_app.mjs
@@ -1,9 +1,11 @@
+import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { data_app_current_set } from "../../../love/public/src/data_app_current_set.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_open_app(f_name) {
   marker("1");
+  let u = await function_exists(f_name2);
   let a_name = app_name_main(f_name);
   let v = await function_open(a_name);
   await data_app_current_set(f_name);
