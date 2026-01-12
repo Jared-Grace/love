b02d563429328f8cf3ad40f40bc91a8bd65e8486
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 13:08:41 -0500

    function_auto_path ..\love\public\src\object_to_list.mjs

--- public/src/object_to_list.mjs
+++ public/src/object_to_list.mjs
@@ -1,5 +1,9 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
 import { object_to_list_names } from "../../../love/public/src/object_to_list_names.mjs";
-export function object_to_list(o) {$aa
+export function object_to_list(o) {
+  assert_arguments(arguments, 1);
+  marker("1");
   const key_name = "key";
   const value_name = "value";
   let list = object_to_list_names(o, key_name, value_name);
