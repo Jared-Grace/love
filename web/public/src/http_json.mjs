8520966f780d68652ba5c0d9118528a5757a7085
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 14:08:14 -0500

    function_auto_path ..\love\public\src\http_json.mjs

--- web/public/src/http_json.mjs
+++ web/public/src/http_json.mjs
@@ -1,9 +1,12 @@
+import { http_generic } from "../../../love/public/src/http_generic.mjs";
 import { buffer_to_json } from "../../../love/public/src/buffer_to_json.mjs";
-import { http } from "../../../love/public/src/http.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function http_json(url) {
   marker("1");
-  let buffer = await http(url);
+  marker("1");
+  let buffer = await http_generic(url, {
+    method: "GET",
+  });
   let parsed = buffer_to_json(buffer);
   return parsed;
 }
