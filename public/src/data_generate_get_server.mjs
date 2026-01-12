6839c8056a416302cd77ecfa60e5f1d15f67fbfe
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:04:03 -0500

    function_auto_path ..\love\public\src\data_generate_get_server.mjs

---
+++ public/src/data_generate_get_server.mjs
@@ -0,0 +1,16 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { data_generate } from "../../../love/public/src/data_generate.mjs";
+import { null_is } from "../../../love/public/src/null_is.mjs";
+import { data_get } from "../../../love/public/src/data_get.mjs";
+export function data_generate_get_server() {
+  marker("1");
+  let data = null;
+  let data_get = async function lambda2() {
+    if (null_is(data)) {
+      data = {};
+      await data_generate(data);
+    }
+    return data;
+  };
+  return data_get;
+}
