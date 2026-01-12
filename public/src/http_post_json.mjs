52cbdbc11043a5bd6a3d761249e76b01c9d20f25
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 15:45:57 -0500

    function_auto_path ..\love\public\src\http_post_json.mjs

--- public/src/http_post_json.mjs
+++ public/src/http_post_json.mjs
@@ -1,10 +1,7 @@
+import { http_post } from "../../../love/public/src/http_post.mjs";
 import { buffer_to_json } from "../../../love/public/src/buffer_to_json.mjs";
-import { http_generic } from "../../../love/public/src/http_generic.mjs";
 export async function http_post_json(url, body) {
-  let buffer = await http_generic(url, {
-    method: "POST",
-    body: body,
-  });
+  let buffer = await http_post(url, body);
   let o = buffer_to_json(buffer);
   return o;
 }
