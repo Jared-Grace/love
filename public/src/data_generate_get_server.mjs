829d9701f5722c87b0f095bac8468ed6e0bf2003
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 14:01:14 -0500

    function_auto_path ..\love\public\src\data_generate_get_server.mjs

--- public/src/data_generate_get_server.mjs
+++ public/src/data_generate_get_server.mjs
@@ -1,3 +1,4 @@
+import { performance_start } from "../../../love/public/src/performance_start.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { http_json } from "../../../love/public/src/http_json.mjs";
 import { server_url_data_ending } from "../../../love/public/src/server_url_data_ending.mjs";
@@ -9,5 +10,6 @@ export async function data_generate_get_server() {
   log({
     url,
   });
+  let p = performance_start(category);
   let parsed = await http_json(url);
 }
