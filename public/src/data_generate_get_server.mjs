f70f4bf169ad0d3ca3a58a595253ee1a689dff32
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:11:17 -0500

    function_auto_path ..\love\public\src\data_generate_get_server.mjs

--- public/src/data_generate_get_server.mjs
+++ public/src/data_generate_get_server.mjs
@@ -1,12 +1,14 @@
+import { http_json } from "../../../love/public/src/http_json.mjs";
 import { server_url_data_ending } from "../../../love/public/src/server_url_data_ending.mjs";
 import { server_url_get } from "../../../love/public/src/server_url_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_generate } from "../../../love/public/src/data_generate.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { data_get } from "../../../love/public/src/data_get.mjs";
-export function data_generate_get_server() {
+export async function data_generate_get_server() {
   marker("1");
   let url = server_url_get() + server_url_data_ending() + "";
+  let parsed = await http_json(url2);
   let data = null;
   let data_get = async function lambda2() {
     if (null_is(data)) {
