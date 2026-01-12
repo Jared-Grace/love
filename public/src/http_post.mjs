bdbbd00309138b531b2f77ca5ad05431038c84d8
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 16:00:14 -0500

    function_auto_path ..\love\public\src\http_post.mjs

--- public/src/http_post.mjs
+++ public/src/http_post.mjs
@@ -1,12 +1,6 @@
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { http_generic } from "../../../love/public/src/http_generic.mjs";
+import { http_post_options } from "../../../love/public/src/http_post_options.mjs";
 export async function http_post(url, body) {
   const options_extra = {};
-  const options = {
-    method: "POST",
-    body: body,
-  };
-  object_merge(options, options_extra);
-  let v = await http_generic(url, options);
+  let v = await http_post_options(body, options_extra, url);
   return v;
 }
