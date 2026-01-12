967bafab287bab98af29376c5105305ede6115d3
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 21:10:00 -0500

    pd d_path

--- web/public/src/server_url_data_ending.mjs
+++ web/public/src/server_url_data_ending.mjs
@@ -1,5 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function server_url_data_ending(d_path) {
+export function server_url_data_ending() {
   marker("1");
   let v = "/" + d_path;
   return v;
