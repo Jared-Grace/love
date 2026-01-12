e8f058a3cb5810151615f781bdec317e81bcaaa6
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 16:37:43 -0500

    function_auto_path ..\love\public\src\data_generate_get_server.mjs

--- public/src/data_generate_get_server.mjs
+++ public/src/data_generate_get_server.mjs
@@ -1,16 +1,15 @@
+import { server_url_data_full } from "../../../love/public/src/server_url_data_full.mjs";
 import { http_post_options } from "../../../love/public/src/http_post_options.mjs";
 import { http_option_sleep_none } from "../../../love/public/src/http_option_sleep_none.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
 import { performance_end } from "../../../love/public/src/performance_end.mjs";
 import { performance_start } from "../../../love/public/src/performance_start.mjs";
 import { http_json } from "../../../love/public/src/http_json.mjs";
-import { server_url_data_ending } from "../../../love/public/src/server_url_data_ending.mjs";
-import { server_url_get } from "../../../love/public/src/server_url_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { data_generate } from "./data_generate.mjs";
 export async function data_generate_get_server() {
   marker("1");
-  let url = server_url_get() + server_url_data_ending() + "";
+  let url = server_url_data_full();
   let p = performance_start("http_json");
   let options = http_option_sleep_none();
   let parsed = await http_json(url, options);
