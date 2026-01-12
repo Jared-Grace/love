fc815d6f2eade15162d8745204bd215d4a7d3b53
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 14:10:56 -0500

    function_auto_path ..\love\public\src\data_generate_get_server.mjs

--- public/src/data_generate_get_server.mjs
+++ public/src/data_generate_get_server.mjs
@@ -2,7 +2,6 @@ import { sleep } from "../../../love/public/src/sleep.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
 import { performance_end } from "../../../love/public/src/performance_end.mjs";
 import { performance_start } from "../../../love/public/src/performance_start.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { http_json } from "../../../love/public/src/http_json.mjs";
 import { server_url_data_ending } from "../../../love/public/src/server_url_data_ending.mjs";
 import { server_url_get } from "../../../love/public/src/server_url_get.mjs";
@@ -11,9 +10,6 @@ import { data_generate } from "./data_generate.mjs";
 export async function data_generate_get_server() {
   marker("1");
   let url = server_url_get() + server_url_data_ending() + "";
-  log({
-    url,
-  });
   let p = performance_start("http_json");
   let parsed = await http_json(url, {
     sleep: false,
