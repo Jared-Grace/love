c442dd57b520741242092bb0aba159fa18a17f56
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 14:10:28 -0500

    function_auto_path ..\love\public\src\http_json.mjs

--- public/src/http_json.mjs
+++ public/src/http_json.mjs
@@ -2,7 +2,7 @@ import { http_get_options } from "../../../love/public/src/http_get_options.mjs"
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { http_generic } from "../../../love/public/src/http_generic.mjs";
 import { buffer_to_json } from "../../../love/public/src/buffer_to_json.mjs";
-export async function http_json(options) {
+export async function http_json(url, options) {
   let g = http_get_options();
   let to = object_merge(g, options);
   let buffer = await http_generic(url, to);
