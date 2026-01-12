cb944318a6517de7b27befbee5fd1e3fdeeac638
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:29:14 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- public/src/function_open_app.mjs
+++ public/src/function_open_app.mjs
@@ -1,5 +1,4 @@
 import { list_find_starts_with } from "../../../love/public/src/list_find_starts_with.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -15,10 +14,6 @@ export async function function_open_app(f_name) {
   let exists = object_property_get(v2, "exists");
   if (not(exists)) {
     let mapped = await apps_names();
-    log({
-      f_name,
-      mapped,
-    });
     f_name = list_find_starts_with(mapped, f_name);
     a_name = app_name_main(f_name);
   }
