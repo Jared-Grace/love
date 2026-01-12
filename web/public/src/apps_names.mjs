ee654d03a74e48e2e224bbe572d9c8b9a0ff0575
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:10:34 -0500

    function_auto_path ..\love\public\src\apps_names.mjs

--- web/public/src/apps_names.mjs
+++ web/public/src/apps_names.mjs
@@ -3,6 +3,6 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { apps_paths } from "../../../love/public/src/apps_paths.mjs";
 export async function apps_names() {
   let aps = await apps_paths();
-  let mapped = list_map(aps, path_name);
-  return mapped;
+  let ans = list_map(aps, path_name);
+  return ans;
 }
