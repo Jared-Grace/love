19fd265810022c50dfe164bbb5530a07f86d3939
Grace-Jared <christrosetolife@gmail.com>
2026-01-11 20:17:58 -0500

    rs list_find_starts_with prefixes

--- public/src/list_map_prefix_without_any.mjs
+++ public/src/list_map_prefix_without_any.mjs
@@ -1,11 +1,11 @@
-import { list_find_starts_with } from "../../../love/public/src/list_find_starts_with.mjs";
+import { list_find_starts_with_prefixes } from "../../../love/public/src/list_find_starts_with_prefixes.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
 export function list_map_prefix_without_any(list, prefixes) {
   marker("1");
   function lambda(item) {
-    let prefix = list_find_starts_with(prefixes, item);
+    let prefix = list_find_starts_with_prefixes(prefixes, item);
     let result = string_prefix_without(item, prefix);
     return result;
   }
