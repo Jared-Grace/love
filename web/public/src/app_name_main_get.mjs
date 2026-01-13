79be981883e08b841eeaac4c3d0d15e81b3c4e7c
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:02:57 -0500

    function_auto_path ..\love\public\src\app_name_main_get.mjs

--- web/public/src/app_name_main_get.mjs
+++ web/public/src/app_name_main_get.mjs
@@ -1,23 +1,21 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { list_find_starts_with } from "../../../love/public/src/list_find_starts_with.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
-export async function app_name_main_get(f_name) {
-  marker("1");
-  let a_name = app_name_main(f_name);
-  let v2 = await function_exists(a_name);
+export async function app_name_main_get(a_name) {
+  let f_name = app_name_main(a_name);
+  let v2 = await function_exists(f_name);
   let exists = object_property_get(v2, "exists");
   if (not(exists)) {
     let mapped = await apps_names();
-    f_name = list_find_starts_with(mapped, f_name);
-    a_name = app_name_main(f_name);
+    a_name = list_find_starts_with(mapped, a_name);
+    f_name = app_name_main(a_name);
   }
-  let v3 = {
-    a_name,
+  let v = {
     f_name,
+    a_name,
   };
-  return v3;
+  return v;
 }
