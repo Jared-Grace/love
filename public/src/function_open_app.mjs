e8464488a123881e5e5482be23f10fdb2351fa05
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:00:42 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- public/src/function_open_app.mjs
+++ public/src/function_open_app.mjs
@@ -9,15 +9,15 @@ import { function_open } from "../../../love/public/src/function_open.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_open_app(a_name) {
   marker("1");
-  let f = app_name_main(a_name);
-  let v2 = await function_exists(f);
+  let f_name = app_name_main(a_name);
+  let v2 = await function_exists(f_name);
   let exists = object_property_get(v2, "exists");
   if (not(exists)) {
     let mapped = await apps_names();
     a_name = list_find_starts_with(mapped, a_name);
-    f = app_name_main(a_name);
+    f_name = app_name_main(a_name);
   }
-  let v = await function_open(f);
+  let v = await function_open(f_name);
   await data_app_current_set(a_name);
   return v;
 }
