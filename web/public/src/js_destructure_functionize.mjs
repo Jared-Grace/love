43028569b02b6299cb1c91b2f6d56626bbfa29d1
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:01:41 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- web/public/src/js_destructure_functionize.mjs
+++ web/public/src/js_destructure_functionize.mjs
@@ -1,3 +1,4 @@
+import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
@@ -28,6 +29,7 @@ export async function js_destructure_functionize(ast) {
     if (l) {
       return;
     }
+    js_node_type_is_if(node, type, function lambda4() {});
     let name4 = null;
     let init = object_property_get(e1, "init");
     let ii = js_identifier_is(init);
