1dbda3444e2127d379fefba5554510858be98741
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:19:32 -0500

    function_auto_path ..\love\public\src\ebible_chapter_codes_upload.mjs

--- public/src/ebible_chapter_codes_upload.mjs
+++ public/src/ebible_chapter_codes_upload.mjs
@@ -1,10 +1,10 @@
+import { ebible_chapter_codes_upload_name } from "../../../love/public/src/ebible_chapter_codes_upload_name.mjs";
 import { ebible_chapter_codes } from "../../../love/public/src/ebible_chapter_codes.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { ebible_index_upload_name } from "../../../love/public/src/ebible_index_upload_name.mjs";
 import { ebible_firebase_upload } from "../../../love/public/src/ebible_firebase_upload.mjs";
 export async function ebible_chapter_codes_upload(bible_folder) {
   marker("1");
-  let index = await ebible_chapter_codes(bible_folder);
-  let file_name = ebible_index_upload_name();
-  await ebible_firebase_upload(bible_folder, file_name, index);
+  let o = await ebible_chapter_codes(bible_folder);
+  let file_name = ebible_chapter_codes_upload_name();
+  await ebible_firebase_upload(bible_folder, file_name, o);
 }
