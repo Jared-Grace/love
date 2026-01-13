a66bd4dd549628f01658777311e7deaaa9a2bb44
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:44:46 -0500

    function_auto_path ..\love\public\src\app_bible_home.mjs

--- public/src/app_bible_home.mjs
+++ public/src/app_bible_home.mjs
@@ -40,7 +40,9 @@ export async function app_bible_home(context) {
   let book_code = object_property_get(v2, "book_code");
   let books = await ebible_version_books(e);
   let book_name = ebible_book_code_to_name(books, book_code);
-  function lambda6() {}
+  async function lambda6() {
+    await on_arrow(list_next_wrap);
+  }
   let text3 = emoji_arrow_left();
   let component4 = html_button(parent, text3, lambda6);
   function lambda5() {}
