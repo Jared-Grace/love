510812861d31de9193b5b7e2071682a5701676bf
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:40:35 -0500

    function_auto_path ..\love\public\src\list_next_wrap.mjs

--- web/public/src/list_next_wrap.mjs
+++ web/public/src/list_next_wrap.mjs
@@ -1,11 +1,11 @@
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_next } from "../../../love/public/src/list_next.mjs";
 import { list_last_is } from "../../../love/public/src/list_last_is.mjs";
-export function list_next_wrap(list, chapter_code) {
+export function list_next_wrap(list, item) {
   let next = null;
-  let li = list_last_is(list, chapter_code);
+  let li = list_last_is(list, item);
   if (li) {
-    next = list_next(list, chapter_code);
+    next = list_next(list, item);
   } else {
     next = list_first(list);
   }
