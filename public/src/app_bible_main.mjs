b6bac8e78d661261b16f54ac5a5eca42815d735f
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:54:53 -0500

    function_auto_path ..\love\public\src\app_bible_main.mjs

--- public/src/app_bible_main.mjs
+++ public/src/app_bible_main.mjs
@@ -1,10 +1,11 @@
+import { app_bible } from "../../../love/public/src/app_bible.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_bible_screens } from "../../../love/public/src/app_bible_screens.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 export async function app_bible_main(context) {
   marker("1");
-  let app_fn = app_bible_main;
+  let app_fn = app_bible;
   let screens = app_bible_screens();
   object_merge(context, {
     app_fn,
