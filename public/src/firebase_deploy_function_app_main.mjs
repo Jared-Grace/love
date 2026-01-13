15f0f98aa186e6809e2001dd1382641cf31f09bb
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:05:15 -0500

    function_auto_path ..\love\public\src\firebase_deploy_function_app_main.mjs

--- public/src/firebase_deploy_function_app_main.mjs
+++ public/src/firebase_deploy_function_app_main.mjs
@@ -1,13 +1,11 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_name_main_get } from "../../../love/public/src/app_name_main_get.mjs";
-import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 import { firebase_deploy_function } from "../../../love/public/src/firebase_deploy_function.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_deploy_function_app_main(a) {
   marker("1");
-  let combined = app_name_main(a);
   let v2 = await app_name_main_get(a_name);
-  let a_name_final = object_property_get(v2, "a_name");
+  let combined = object_property_get(v2, "f_name");
   let v = await firebase_deploy_function(combined);
   return v;
 }
