cd3f6f986dba34516b5ec9d21567ade9b62ea937
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:49:08 -0500

    function_auto_path ..\love\public\src\app_bible_books.mjs

--- web/public/src/app_bible_books.mjs
+++ web/public/src/app_bible_books.mjs
@@ -1,6 +1,8 @@
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_bible_books(context) {
   marker("1");
   let root = object_property_get(context, "root");
+  html_clear(root);
 }
