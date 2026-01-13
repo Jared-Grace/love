f9bec34ff8b995197b31767f8d3e1e80e9059660
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 20:06:33 -0500

    function_auto_path ..\love\public\src\app_bible_chapters.mjs

--- public/src/app_bible_screens.mjs
+++ public/src/app_bible_screens.mjs
@@ -1,3 +1,4 @@
+import { app_bible_chapters } from "../../../love/public/src/app_bible_chapters.mjs";
 import { app_bible_books } from "../../../love/public/src/app_bible_books.mjs";
 import { app_bible_home } from "../../../love/public/src/app_bible_home.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
