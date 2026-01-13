8e5fce90f1a96df4f13a84b015e3e7b865fc5ffb
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 20:06:31 -0500

    msa chapters

--- public/src/app_bible_chapters.mjs
+++ public/src/app_bible_chapters.mjs
@@ -1,4 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_bible_chapters() {
+export function app_bible_chapters(context) {
   marker("1");
+  let root = html_clear_context(context);
 }
