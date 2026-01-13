1d05c6bb9d0b71cd55c633cf61db92b47fd83696
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:18:58 -0500

    function_auto_path ..\love\public\src\ebible_chapter_codes_upload.mjs

---
+++ public/src/ebible_chapter_codes_upload.mjs
@@ -0,0 +1,10 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { ebible_index_upload_name } from "../../../love/public/src/ebible_index_upload_name.mjs";
+import { ebible_firebase_upload } from "../../../love/public/src/ebible_firebase_upload.mjs";
+import { ebible_index } from "../../../love/public/src/ebible_index.mjs";
+export async function ebible_chapter_codes_upload(bible_folder) {
+  marker("1");
+  let index = await ebible_index(bible_folder);
+  let file_name = ebible_index_upload_name();
+  await ebible_firebase_upload(bible_folder, file_name, index);
+}
