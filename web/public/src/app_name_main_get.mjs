1a8c9dc5efcf8ef653cdcefa4900d7248fd493a8
Grace-Jared <christrosetolife@gmail.com>
2026-01-12 19:02:48 -0500

    function_auto_path ..\love\public\src\app_name_main_get.mjs

--- web/public/src/app_name_main_get.mjs
+++ web/public/src/app_name_main_get.mjs
@@ -1,3 +1,4 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_find_starts_with } from "../../../love/public/src/list_find_starts_with.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { not } from "../../../love/public/src/not.mjs";
@@ -5,6 +6,7 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
 export async function app_name_main_get(f_name) {
+  marker("1");
   let a_name = app_name_main(f_name);
   let v2 = await function_exists(a_name);
   let exists = object_property_get(v2, "exists");
