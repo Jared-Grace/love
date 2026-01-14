553bbd82e0d9a2d588a9d9967a100a03cf9d0c4e
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 16:40:14 -0500

    function_auto_path ..\love\public\src\app_name_main_get.mjs

--- public/src/app_name_main_get.mjs
+++ public/src/app_name_main_get.mjs
@@ -5,13 +5,13 @@ import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
-export async function app_name_main_get(a_name) {
-  let f_name = app_name_main(a_name);
+export async function app_name_main_get(a_search) {
+  let f_name = app_name_main(a_search);
   let v2 = await function_exists(f_name);
   let exists = object_property_get(v2, "exists");
   if (not(exists)) {
     let mapped = await apps_names();
-    a_name = list_find_starts_with(mapped, a_name);
+    let a_name = list_find_starts_with(mapped, a_name);
     f_name = app_name_main(a_name);
   }
   log_keep({
