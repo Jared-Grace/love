fa3151ffae62db80676e46656908993b3d7135c1
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:47:59 -0500

    function_auto_path ..\love\public\src\html_p.mjs

--- public/src/html_p.mjs
+++ public/src/html_p.mjs
@@ -1,5 +1,5 @@
 import { html_element } from "../../../love/public/src/html_element.mjs";
 export function html_p(root) {
-  let component = html_element(root, "p");
-  return component;
+  let p = html_element(root, "p");
+  return p;
 }
