950026ddcf69091c06445c6e791866618f7299b7
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 15:43:02 -0500

    function_auto_path ..\love\public\src\app_bible_chapters.mjs

--- public/src/app_bible_chapters.mjs
+++ public/src/app_bible_chapters.mjs
@@ -1,3 +1,4 @@
+import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { ebible_chapter_code_to_book } from "../../../love/public/src/ebible_chapter_code_to_book.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
@@ -13,4 +14,5 @@ export async function app_bible_chapters(context) {
   let hash = html_hash_object_get();
   let chapter_code = object_property_get(hash, "c");
   let book_code = ebible_chapter_code_to_book(chapter_code);
+  let filtered = list_filter_starts_with(list2, prefix);
 }
