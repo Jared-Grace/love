8860c58edefb238100dd5266c16b6789f9bb99d7
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:32:34 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_ebible_cache.mjs

--- web/public/src/firebase_storage_download_ebible_cache.mjs
+++ web/public/src/firebase_storage_download_ebible_cache.mjs
@@ -1,5 +1,4 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
 import { firebase_storage_download_ebible } from "../../../love/public/src/firebase_storage_download_ebible.mjs";
 export async function firebase_storage_download_ebible_cache(
@@ -9,7 +8,6 @@ export async function firebase_storage_download_ebible_cache(
 ) {
   marker("1");
   async function get() {
-    log({});
     let v = await firebase_storage_download_ebible(bible_folder, file_name);
     return v;
   }
