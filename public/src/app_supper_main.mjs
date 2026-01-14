bfdbfdf667575c83ee9cb73e015d2bc687be87dc
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:41:07 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -1,9 +1,9 @@
+import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
 import { ebible_verse_merge } from "../../../love/public/src/ebible_verse_merge.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
@@ -21,9 +21,8 @@ export async function app_supper_main(context) {
   });
   function verse_get(bible_folder, chapter_code, verse_number) {
     equal_assert(bible_folder, e);
-    let v3 = list_find_property(chapters, "chapter_code", chapter_code);
-    let verses = object_property_get(v3, "verses");
-    let v = list_find_property(verses, "verse_number", verse_number);
+    let filtered = list_filter_property(list, "chapter_code", chapter_code);
+    let filtered2 = list_find_property(filtered, "verse_number", verse_number);
     let verse = ebible_verse_merge(bible_folder, chapter_code, v);
     return verse;
   }
