910dd8ad36f91f2151aa45f6b1494bb626daece2
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 15:57:34 -0500

    function_auto_path ..\love\public\src\app_bible_chapters.mjs

--- web/public/src/app_bible_chapters.mjs
+++ web/public/src/app_bible_chapters.mjs
@@ -19,12 +19,12 @@ export async function app_bible_chapters(context) {
   marker("1");
   let root = html_clear_context(context);
   let e = ebible_folder_english();
-  let chapter_codes = await ebible_chapter_codes(e);
+  let chapter_codes_all = await ebible_chapter_codes(e);
   let hash = html_hash_object_get();
   let chapter_code = object_property_get(hash, "c");
   let book_code = ebible_chapter_code_to_book(chapter_code);
-  let filtered = list_filter_starts_with(chapter_codes, book_code);
-  let mapped = list_map_prefix_without(filtered, book_code);
+  let chapter_codes = list_filter_starts_with(chapter_codes_all, book_code);
+  let mapped = list_map_prefix_without(chapter_codes, book_code);
   let chapter_numbers = list_map(mapped, integer_to);
   let books = await ebible_version_books(e);
   let book_name = ebible_book_code_to_name(books, book_code);
@@ -33,7 +33,7 @@ export async function app_bible_chapters(context) {
     function lambda3() {}
     let component = html_button(root, chapter_number, lambda3);
   }
-  each(chapter_codes, lambda);
+  each(chapter_codes_all, lambda);
   log({
     mapped2: chapter_numbers,
   });
