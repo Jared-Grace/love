9e43cae71aee72c6a818ec33f70ae02226b81145
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 15:34:44 -0500

    function_auto_path ..\love\public\src\list_shuffle_take.mjs

--- web/public/src/list_shuffle_take.mjs
+++ web/public/src/list_shuffle_take.mjs
@@ -1,7 +1,7 @@
 import { list_take } from "../../../love/public/src/list_take.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
-export function list_shuffle_take(encouragement, verse_count) {
-  list_shuffle(encouragement);
-  let taken = list_take(encouragement, verse_count);
+export function list_shuffle_take(list, count) {
+  list_shuffle(list);
+  let taken = list_take(list, count);
   return taken;
 }
