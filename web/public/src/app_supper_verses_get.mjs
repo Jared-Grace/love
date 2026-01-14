7b9a60b05ba6b6bfae02eeb87198f71973762dc2
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:32:24 -0500

    function_auto_path ..\love\public\src\app_supper_verses_get.mjs

--- web/public/src/app_supper_verses_get.mjs
+++ web/public/src/app_supper_verses_get.mjs
@@ -1,7 +1,21 @@
+import { ebible_chapter_codes } from "../../../love/public/src/ebible_chapter_codes.mjs";
+import { firebase_storage_download_ebible_cache } from "../../../love/public/src/firebase_storage_download_ebible_cache.mjs";
+import { ebible_chapter_codes_upload_name } from "../../../love/public/src/ebible_chapter_codes_upload_name.mjs";
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { ebible_references_parse_lines } from "../../../love/public/src/ebible_references_parse_lines.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
 export async function app_supper_verses_get() {
+  let b = browser_is();
+  if (b) {
+    let file_name = ebible_chapter_codes_upload_name();
+    let value = await firebase_storage_download_ebible_cache(
+      ebible_chapter_codes,
+      bible_folder,
+      file_name,
+    );
+    return value;
+  }
   let references = `Matthew 26:26-30
 Mark 14:22-26
 Luke 22:14-20
