4d1fa60b5105b1a176156a6d23bee42d38abd911
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:29:35 -0500

    function_auto_path ..\love\public\src\app_supper_verses_get_upload.mjs

--- public/src/app_supper_verses_get_upload.mjs
+++ public/src/app_supper_verses_get_upload.mjs
@@ -1,3 +1,4 @@
+import { app_prefix_without } from "../../../love/public/src/app_prefix_without.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { ebible_firebase_upload_path } from "../../../love/public/src/ebible_firebase_upload_path.mjs";
 import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
@@ -5,6 +6,7 @@ import { app_supper_verses_get } from "../../../love/public/src/app_supper_verse
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_supper_verses_get_upload() {
   marker("1");
+  let without = app_prefix_without(app_fn);
   let verses = await app_supper_verses_get();
   let e = ebible_folder_english();
   let destination = ebible_firebase_upload_path(e, "supper");
