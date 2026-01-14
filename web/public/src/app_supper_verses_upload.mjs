f5e19473e4b3c87479f95efe9ff3f567e7d12e68
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:25:46 -0500

    rrl app_supper_verses_upload get_upload

--- web/public/src/app_supper_verses_upload.mjs
+++
@@ -1,6 +0,0 @@
-import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_supper_verses_upload() {
-  marker("1");
-  let list = await app_supper_verses_get();
-}
