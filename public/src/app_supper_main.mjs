50fe9ff38a305b926bd49b93a306fa786d7dc02f
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:49:37 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -16,6 +16,7 @@ export async function app_supper_main(context) {
   let verses = await app_supper_verses_get();
   function lambda2(v) {
     let v2 = list_first_is(verses, v);
+    no;
     let text = object_property_get(v, "text");
     let reference = object_property_get(v, "reference");
     let p = html_p(root);
