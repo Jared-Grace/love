0d9ed78e6961d855a4723f1c9851e9fce4b9ab80
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:47:44 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -1,9 +1,10 @@
-import { html_centered } from "../../../love/public/src/html_centered.mjs";
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { html_div_text_centered } from "../../../love/public/src/html_div_text_centered.mjs";
+import { html_p } from "../../../love/public/src/html_p.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -15,10 +16,10 @@ export async function app_supper_main(context) {
   function lambda2(v) {
     let text = object_property_get(v, "text");
     let reference = object_property_get(v, "reference");
-    let p = html_p_text(root, reference);
-    html_centered(p);
+    let component = html_p(root2);
+    let p = html_div_text_centered(root, reference);
     html_font_color_set(p, "#aaa");
-    html_p_text(root, text);
+    html_div_text(root, text);
   }
   each(list, lambda2);
 }
