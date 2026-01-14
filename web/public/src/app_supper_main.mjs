d782f0a60a8dbbc990d09fc8e92e362d0ce3aff1
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:39:20 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- web/public/src/app_supper_main.mjs
+++ web/public/src/app_supper_main.mjs
@@ -1,3 +1,4 @@
+import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { global_function_set } from "../../../love/public/src/global_function_set.mjs";
@@ -12,15 +13,14 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_supper_main(context) {
   marker("1");
   firebase_name_jg();
+  let e = ebible_folder_english();
   let root = html_clear_context(context);
   let list = await app_supper_verses_get();
   log({
     list,
   });
   function verse_get(bible_folder, chapter_code, verse_number) {
-    equal_assert(bible_folder, right);
-    let v2 = list_find_property(result, "bible_folder", bible_folder);
-    let chapters = object_property_get(v2, "chapters");
+    equal_assert(bible_folder, e);
     let v3 = list_find_property(chapters, "chapter_code", chapter_code);
     let verses = object_property_get(v3, "verses");
     let v = list_find_property(verses, "verse_number", verse_number);
