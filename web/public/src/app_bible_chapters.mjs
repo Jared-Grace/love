690d452bb8e3c18285dbb79dcb2ff957aff3a19e
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 15:58:43 -0500

    function_auto_path ..\love\public\src\app_bible_chapters.mjs

--- web/public/src/app_bible_chapters.mjs
+++ web/public/src/app_bible_chapters.mjs
@@ -31,7 +31,7 @@ export async function app_bible_chapters(context) {
   let book_name = ebible_book_code_to_name(books, book_code);
   html_div_text_centered(root, book_name);
   function lambda(chapter_code) {
-    let chapter_name = ebible_chapter_code_to_name(chapter_code2);
+    let chapter_name = ebible_chapter_code_to_name(chapter_code);
     let i = integer_to(input);
     function lambda3() {}
     let component = html_button(root, chapter_number, lambda3);
