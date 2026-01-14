17c554cc59106114a602940c78e2fae79b95dcf9
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:48:37 -0500

    function_auto_path ..\love\public\src\html_div_text_centered.mjs

--- web/public/src/html_div_text_centered.mjs
+++ web/public/src/html_div_text_centered.mjs
@@ -3,4 +3,5 @@ import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 export function html_div_text_centered(root, text) {
   let div = html_div_text(root, text);
   html_centered(div);
+  return div;
 }
