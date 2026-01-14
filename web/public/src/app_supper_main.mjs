4d5f1a7f461d7787fc0e74b9cb27d7f663c9135c
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:44:15 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- web/public/src/app_supper_main.mjs
+++ web/public/src/app_supper_main.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
@@ -9,8 +10,11 @@ export async function app_supper_main(context) {
   firebase_name_jg();
   let root = html_clear_context(context);
   let list = await app_supper_verses_get();
-  function lambda2(item) {
-    html_p_text(root, item);
+  function lambda2(v) {
+    let text = object_property_get(v, "text");
+    let reference = object_property_get(v, "reference");
+    html_p_text(root, v);
+    html_p_text(root, v);
   }
   each(list, lambda2);
 }
