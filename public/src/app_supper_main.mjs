b33287d7ce000e9307971e0998c8e691c0c50b70
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:45:04 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -1,14 +1,14 @@
+import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
-import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_supper_main(context) {
   marker("1");
   firebase_name_jg();
-  let root = html_clear_context(context);
+  let root = html_mobile_default(context);
   let list = await app_supper_verses_get();
   function lambda2(v) {
     let text = object_property_get(v, "text");
