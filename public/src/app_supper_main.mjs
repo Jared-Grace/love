0ce47f8eb96079d734955b6772b6fcffe7046ea4
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:13:26 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -1,3 +1,4 @@
+import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
@@ -17,6 +18,7 @@ Acts 2:42
 Acts 20:7
 1 Corinthians 10:16-22
 1 Corinthians 11:17-34`;
+  let split = string_split_newline(list2);
   let e = ebible_folder_english();
   let list = await ebible_references_parse_lines([e], references);
   log({
