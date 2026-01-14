187b47fea4b50b4bd1c9c06a9f06f847dc25c3fc
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 15:35:57 -0500

    function_auto_path ..\love\public\src\ebible_chapter_codes.mjs

--- public/src/ebible_chapter_codes.mjs
+++ public/src/ebible_chapter_codes.mjs
@@ -1,3 +1,4 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { ebible_chapter_codes_upload_name } from "../../../love/public/src/ebible_chapter_codes_upload_name.mjs";
 import { firebase_storage_download_ebible_cache } from "../../../love/public/src/firebase_storage_download_ebible_cache.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
@@ -5,6 +6,7 @@ import { ebible_books_to_chapter_codes } from "../../../love/public/src/ebible_b
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function ebible_chapter_codes(bible_folder) {
+  assert_arguments(arguments, 1);
   let b = browser_is();
   if (b) {
     let file_name = ebible_chapter_codes_upload_name();
