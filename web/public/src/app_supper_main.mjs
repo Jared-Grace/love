091b41fe311b239c4b6d4faea49c52cba64d275e
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:02:05 -0500

    function_auto_path ..\love\public\src\app_supper_main.mjs

--- web/public/src/app_supper_main.mjs
+++ web/public/src/app_supper_main.mjs
@@ -1,4 +1,6 @@
+import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_supper_main() {
+export function app_supper_main(context) {
   marker("1");
+  let root = html_clear_context(context);
 }
