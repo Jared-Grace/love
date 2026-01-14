ef69481ba43ba081aa75643004f0de5f117801dd
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 18:27:28 -0500

    function_auto_path ..\love\public\src\app_supper_verses_get_upload.mjs

--- web/public/src/app_supper_verses_get_upload.mjs
+++ web/public/src/app_supper_verses_get_upload.mjs
@@ -1,6 +1,8 @@
+import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_supper_verses_get_upload() {
   marker("1");
   let list = await app_supper_verses_get();
+  await firebase_upload_object_compressed(destination, value);
 }
