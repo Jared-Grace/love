333dcec3bad2edba4ea68ef46734240a332184ea
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 15:36:59 -0500

    function_auto_path ..\love\public\src\app_bible_chapters.mjs

--- public/src/app_bible_chapters.mjs
+++ public/src/app_bible_chapters.mjs
@@ -1,8 +1,10 @@
+import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { ebible_chapter_codes } from "../../../love/public/src/ebible_chapter_codes.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_bible_chapters(context) {
   marker("1");
   let root = html_clear_context(context);
-  let list = await ebible_chapter_codes(bible_folder);
+  let e = ebible_folder_english();
+  let list = await ebible_chapter_codes(e);
 }
