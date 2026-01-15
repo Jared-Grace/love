79e792d781e1be07019ebb1102a48a79ba9e78d8
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 14:05:11 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
+import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_book_code_to_chapter_codes } from "../../../love/public/src/ebible_book_code_to_chapter_codes.mjs";
 import { ebible_chapter_videos_generate } from "../../../love/public/src/ebible_chapter_videos_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -15,5 +15,5 @@ export async function sandbox() {
     let v = await ebible_chapter_videos_generate(bible_folder, chapter_code);
     let path_video = object_property_get(v, "path_video");
   }
-  await each_async(chapter_codes, lambda);
+  let path_videos = await list_map_async(chapter_codes, lambda);
 }
