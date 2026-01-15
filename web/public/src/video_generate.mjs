353a31e1d633104a28030729b22dba689a83aced
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 22:14:08 -0500

    function_auto_path ..\love\public\src\video_generate.mjs

--- web/public/src/video_generate.mjs
+++ web/public/src/video_generate.mjs
@@ -7,14 +7,12 @@ ffmpeg -y
 -loop 1
 -i "${path_image}"
 -i "${path_audio}"
--c:v libx264
--tune stillimage
--c:a aac
--b:a 192k
--pix_fmt yuv420p
+-c:v libx264 
+-tune stillimage 
+-c:a aac 
+-b:a 192k 
+-pix_fmt yuv420p 
 -shortest
--fflags 
-+shortest
 "${path_output}"
 `.replace(/\s+/g, " ");
   let stdout2 = await command_line(cmd);
