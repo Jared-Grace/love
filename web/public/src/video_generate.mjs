d15fdc7014ade66010366741ca3ce1c3ca92ae06
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 16:18:15 -0500

    function_auto_path ..\love\public\src\video_generate.mjs

--- web/public/src/video_generate.mjs
+++ web/public/src/video_generate.mjs
@@ -1,9 +1,11 @@
+import { file_parent_exists_ensure } from "../../../love/public/src/file_parent_exists_ensure.mjs";
 import { audio_duration } from "../../../love/public/src/audio_duration.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { exec } from "child_process";
 import path from "path";
 export async function video_generate(path_image, path_audio, path_output) {
-  let d = await audio_duration(path_audio);await file_parent_exists_ensure(path_output);
+  let d = await audio_duration(path_audio);
+  await file_parent_exists_ensure(path_output);
   const cmd = `
 ffmpeg -y
 -loop 1
