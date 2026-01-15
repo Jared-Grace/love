a0faa5de15270038e30a8f0a79d6aa0b8ed9bcf8
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 19:56:12 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,7 @@
+import { ebible_chapters_each_verses_list } from "../../../love/public/src/ebible_chapters_each_verses_list.mjs";
+import { ebible_books_to_chapter_codes } from "../../../love/public/src/ebible_books_to_chapter_codes.mjs";
+import { ebible_version_books_testament_apocrypha } from "../../../love/public/src/ebible_version_books_testament_apocrypha.mjs";
+import { ebible_chapters_each_verses_check } from "../../../love/public/src/ebible_chapters_each_verses_check.mjs";
 import { folder_user } from "../../../love/public/src/folder_user.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
@@ -8,6 +12,15 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   const newLocal = "D:\\programs\\WPy64-312100\\python\\";
+  await ebible_chapters_each_verses_check(bible_folder);
+  let books = await ebible_version_books_testament_apocrypha(bible_folder);
+  let list = await ebible_books_to_chapter_codes(books, bible_folder);
+  await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
+  async function each_chapter(chapter_code, verses) {
+    let mapped = list_map_property(verses, "text");
+    let joined = list_join_space(mapped);
+  }
+  return;
   const bible_folder = "engwebu";
   const chapter_code = "SIR01";
   let verses = await ebible_verses(bible_folder, chapter_code);
