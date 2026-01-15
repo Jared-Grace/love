56d3a5f456913f096a5c81c90426f8df43ffabab
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 08:32:01 -0500

    function_auto_path ..\love\public\src\text_to_speech.mjs

--- public/src/text_to_speech.mjs
+++ public/src/text_to_speech.mjs
@@ -1,5 +1,4 @@
 import { command_line } from "../../../love/public/src/command_line.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { file_write } from "../../../love/public/src/file_write.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { py_script_run_cmd } from "../../../love/public/src/py_script_run_cmd.mjs";
@@ -13,9 +12,6 @@ export async function text_to_speech(args) {
     const script_name = "text_to_speech";
     const c = py_script_run_cmd(script_name) + " " + temp_path;
     let stdout = await command_line(c);
-    log({
-      stdout,
-    });
   }
   let result = await file_temp(lambda);
 }
