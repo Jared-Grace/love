6c6c29607be6b6d7f4414ab8834ac79e7bbec84d
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 19:08:27 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { text_to_speech } from "../../../love/public/src/text_to_speech.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
+  await text_to_speech(args);
 }
