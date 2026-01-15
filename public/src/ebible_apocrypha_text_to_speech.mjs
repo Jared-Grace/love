5bb4a5673b3dca64ff269d9e23d09f5f3df91efd
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 08:15:50 -0500

    function_auto_path ..\love\public\src\ebible_apocrypha_text_to_speech.mjs

--- public/src/ebible_apocrypha_text_to_speech.mjs
+++ public/src/ebible_apocrypha_text_to_speech.mjs
@@ -1,6 +1,5 @@
-import { bible_audio_folder } from "../../../love/public/src/bible_audio_folder.mjs";
+import { ebible_apocrypha_text_to_speech_chapter } from "../../../love/public/src/ebible_apocrypha_text_to_speech_chapter.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
-import { text_to_speech } from "../../../love/public/src/text_to_speech.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_chapters_each_verses_list } from "../../../love/public/src/ebible_chapters_each_verses_list.mjs";
@@ -15,14 +14,7 @@ export async function ebible_apocrypha_text_to_speech() {
   let list = await ebible_books_to_chapter_codes(books, bible_folder);
   await ebible_chapters_each_verses_list(list, bible_folder, each_chapter);
   async function each_chapter(chapter_code) {
-    let verses = await ebible_verses(bible_folder, chapter_code);
-    let mapped = list_map_property(verses, "text");
-    let text = list_join_space(mapped);
-    let f = bible_audio_folder(bible_folder, chapter_code);
-    await text_to_speech({
-      text: text,
-      path_output: f,
-    });
+    await ebible_apocrypha_text_to_speech_chapter(bible_folder, chapter_code);
   }
   return;
   const chapter_code = "SIR01";
