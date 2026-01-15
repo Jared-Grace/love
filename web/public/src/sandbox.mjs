c79e645763a093cf6143fe32bde11e7cfa255ddd
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 19:29:45 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,3 +1,5 @@
+import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { text_to_speech } from "../../../love/public/src/text_to_speech.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,6 +8,8 @@ export async function sandbox() {
   const bible_folder = "engwebu";
   const chapter_code = "SIR01";
   let verses = await ebible_verses(bible_folder, chapter_code);
+  let mapped = list_map_property(verses, "text");
+  let text = list_join_space(mapped);
   await text_to_speech({
     text: "test",
     path_output: "C:/test.wav",
