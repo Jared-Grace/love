1a5e543cb4604fd26be4c0f22c77e8fde8e58751
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 21:59:22 -0500

    function_auto_path ..\love\public\src\video_generate.mjs

--- public/src/video_generate.mjs
+++ public/src/video_generate.mjs
@@ -1,8 +1,10 @@
+import { command_line } from "../../../love/public/src/command_line.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { exec } from "child_process";
 import path from "path";
-export function video_generate(path_image, path_audio, path_output) {cl
+export async function video_generate(path_image, path_audio, path_output) {
+  let stdout2 = await command_line(command);
   const cmd = `
 ffmpeg -y
 -loop 1
