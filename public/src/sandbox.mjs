201d6a312013613bbb181d1c851c8fc0405d24a6
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 21:40:45 -0500

    o sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,6 @@
 import { ebible_apocrypha_text_to_speech } from "../../../love/public/src/ebible_apocrypha_text_to_speech.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
-  marker("1");
-  let v = await ebible_apocrypha_text_to_speech();
+  marker("1");image_generate
   return v;
 }
