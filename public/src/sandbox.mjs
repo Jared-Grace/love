c39f10de9833808a679f13faa04894160ba7f4b1
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 21:41:31 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,11 @@
+import { file_read } from "../../../love/public/src/file_read.mjs";
 import { image_generate } from "../../../love/public/src/image_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   const bible_folder = "engwebu";
-  const chapter_code = "SIR01";flr
+  const chapter_code = "SIR01";
+  let contents = await file_read(file_path);
   await image_generate(text, path_output);
   return v;
 }
