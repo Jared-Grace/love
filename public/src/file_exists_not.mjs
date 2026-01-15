ada8152677b96a477e5140daa420d80cd2eec013
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 23:23:02 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/file_exists_not.mjs
+++ public/src/file_exists_not.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
-export async function file_exists_not(joined_image) {
-  let exists = await file_exists(joined_image);
+export async function file_exists_not(path) {
+  let exists = await file_exists(path);
   const n = not(exists);
   return n;
 }
