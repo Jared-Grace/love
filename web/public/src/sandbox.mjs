49f9d43f875a3a22d1473a528e22fc07883a38a8
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 16:38:24 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -2,6 +2,53 @@ import { ebible_book_video_generate } from "../../../love/public/src/ebible_book
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
+  let list = [
+    {
+      book_code: "TOB",
+      text: "Tobit",
+      href: "TOB01.htm",
+    },
+    {
+      book_code: "JDT",
+      text: "Judith",
+      href: "JDT01.htm",
+    },
+    {
+      book_code: "ESG",
+      text: "Esther (Greek)",
+      href: "ESG01.htm",
+    },
+    {
+      book_code: "DAG",
+      text: "Daniel (Greek)",
+      href: "DAG01.htm",
+    },
+    {
+      book_code: "WIS",
+      text: "Wisdom",
+      href: "WIS01.htm",
+    },
+    {
+      book_code: "SIR",
+      text: "Sirach",
+      href: "SIR01.htm",
+    },
+    {
+      book_code: "BAR",
+      text: "Baruch",
+      href: "BAR01.htm",
+    },
+    {
+      book_code: "1MA",
+      text: "1 Maccabees",
+      href: "1MA01.htm",
+    },
+    {
+      book_code: "2MA",
+      text: "2 Maccabees",
+      href: "2MA01.htm",
+    },
+  ];
   const bible_folder = "engwebu";
   const book_code = "TOB";
   await ebible_book_video_generate(bible_folder, book_code);
