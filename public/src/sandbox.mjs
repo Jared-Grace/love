4c0191d6db092728f59ade1a01af710e445d598c
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 21:41:25 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,6 +2,8 @@ import { image_generate } from "../../../love/public/src/image_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
+  const bible_folder = "engwebu";
+  const chapter_code = "SIR01";flr
   await image_generate(text, path_output);
   return v;
 }
