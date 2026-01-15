f7b0e528964a7483b24f4a8a4977eeccb5226e63
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 19:54:33 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -7,6 +7,7 @@ import { text_to_speech } from "../../../love/public/src/text_to_speech.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
+  const newLocal = "D:\\programs\\WPy64-312100\\python\\";
   const bible_folder = "engwebu";
   const chapter_code = "SIR01";
   let verses = await ebible_verses(bible_folder, chapter_code);
