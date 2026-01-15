8b7407c83ee602dc652a3fcceb89af705948bc22
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 14:02:17 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,8 +1,13 @@
+import { ebible_book_code_to_chapter_codes } from "../../../love/public/src/ebible_book_code_to_chapter_codes.mjs";
 import { ebible_chapter_videos_generate } from "../../../love/public/src/ebible_chapter_videos_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
   const bible_folder = "engwebu";
-  const book_code = "SIR";ebible_book_code_to_chapter_codes
+  const book_code = "SIR";
+  let chapter_codes = await ebible_book_code_to_chapter_codes(
+    bible_folder2,
+    book_code2,
+  );
   await ebible_chapter_videos_generate(bible_folder, chapter_code);
 }
