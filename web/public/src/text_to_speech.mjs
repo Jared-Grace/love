2f5baeee341c83feba019d20dca6be3ad3b2fb5c
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 19:07:42 -0500

    function_auto_path ..\love\public\src\text_to_speech.mjs

--- web/public/src/text_to_speech.mjs
+++ web/public/src/text_to_speech.mjs
@@ -2,5 +2,6 @@ import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function text_to_speech(args) {
   marker("1");
-  let result = await file_temp(async function lambda(temp_path) {});
+  async function lambda(temp_path) {}
+  let result = await file_temp(lambda);
 }
