3b4a89a9f2cc03d79bb8590af9ec008ad1cb0787
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 21:42:49 -0500

    function_auto_path ..\love\public\src\ebible_apocrypha_text_to_speech.mjs

--- web/public/src/ebible_apocrypha_text_to_speech.mjs
+++ web/public/src/ebible_apocrypha_text_to_speech.mjs
@@ -1,7 +1,6 @@
+import { bible_audio_folder } from "../../../love/public/src/bible_audio_folder.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { text_to_speech } from "../../../love/public/src/text_to_speech.mjs";
-import { folder_user } from "../../../love/public/src/folder_user.mjs";
-import { path_join } from "../../../love/public/src/path_join.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_chapters_each_verses_list } from "../../../love/public/src/ebible_chapters_each_verses_list.mjs";
@@ -18,8 +17,7 @@ export async function ebible_apocrypha_text_to_speech() {
   async function each_chapter(chapter_code, verses) {
     let mapped = list_map_property(verses, "text");
     let text = list_join_space(mapped);
-    let folder = path_join(["audio", "bible", bible_folder, chapter_code]);
-    let f = folder_user(folder);
+    let f = bible_audio_folder(bible_folder, chapter_code);
     await text_to_speech({
       text: text,
       path_output: f,
