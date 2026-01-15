c8714bfef2ef3beaeb83d79a481cf271590c1f7e
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 16:39:55 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_book_video_generate } from "../../../love/public/src/ebible_book_video_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -51,8 +52,9 @@ export async function sandbox() {
     },
   ];
   const bible_folder = "engwebu";
-  const book_code = "TOB";
-  async function lambda(item) {}
-  await each_async(list2, lambda);
-  await ebible_book_video_generate(bible_folder, book_code);
+  async function lambda(item) {
+    let book_code2 = object_property_get(item, "book_code");
+    await ebible_book_video_generate(bible_folder, book_code);
+  }
+  await each_async(list, lambda);
 }
