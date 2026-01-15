c19fa1702f5a83645cc2910fe23b192e3c6a8f3c
Grace-Jared <christrosetolife@gmail.com>
2026-01-14 19:31:11 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,5 @@
 import { folder_user } from "../../../love/public/src/folder_user.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
@@ -13,13 +12,10 @@ export async function sandbox() {
   let verses = await ebible_verses(bible_folder, chapter_code);
   let mapped = list_map_property(verses, "text");
   let text = list_join_space(mapped);
-  async function lambda(temp_path) {
-    let folder = path_join(["audio", "bible", bible_folder, chapter_code]);
-    let f = folder_user(folder);
-    await text_to_speech({
-      text: text,
-      path_output: f,
-    });
-  }
-  let v2 = await file_temp(lambda);
+  let folder = path_join(["audio", "bible", bible_folder, chapter_code]);
+  let f = folder_user(folder);
+  await text_to_speech({
+    text: text,
+    path_output: f,
+  });
 }
