9007834ad3643c9da926b398d113a12c59f30d97
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:34:15 -0500

    rr ebible_apocrypha_text_to_speech_chapter _apocrypha_ _

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,8 +1,8 @@
-import { ebible_apocrypha_text_to_speech_chapter } from "../../../love/public/src/ebible_apocrypha_text_to_speech_chapter.mjs";
+import { ebible_text_to_speech_chapter } from "../../../love/public/src/ebible_text_to_speech_chapter.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox_2() {
   marker("1");
   const bible_folder = "engwebu";
   const chapter_code = "SIR01";
-  await ebible_apocrypha_text_to_speech_chapter(bible_folder, chapter_code);
+  await ebible_text_to_speech_chapter(bible_folder, chapter_code);
 }
