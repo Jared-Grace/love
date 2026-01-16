64935c93ea1cffaf3ba102f7d8b70470e56db947
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 15:21:24 -0500

    function_auto_path ..\love\public\src\app_bible_home.mjs

--- public/src/html_stxle_margin_x.mjs
+++
@@ -1,9 +0,0 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export function html_stxle_margin_x(component, value) {
-  marker("1");
-  html_style_assign(component, {
-    "margin-left": value,
-    "margin-right": value,
-  });
-}
