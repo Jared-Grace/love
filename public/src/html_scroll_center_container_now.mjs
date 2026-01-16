b9b79ef5d40e526ebba73b2319aea1ff8c529c93
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 17:54:17 -0500

    function_auto_path ..\love\public\src\html_scroll_center_container_generic.mjs

--- public/src/html_scroll_center_container_now.mjs
+++ public/src/html_scroll_center_container_now.mjs
@@ -1,10 +1,10 @@
 import { html_scroll_center_container_generic } from "../../../love/public/src/html_scroll_center_container_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function html_scroll_center_container_now(
-  player_img_c,
+  component,
   container,
 ) {
   marker("1");
   const behavior = "auto";
-  await html_scroll_center_container_generic(player_img_c, behavior, container);
+  await html_scroll_center_container_generic(component, behavior, container);
 }
