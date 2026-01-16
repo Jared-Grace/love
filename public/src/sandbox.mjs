ecb6f9c2bdfa3f8aca6fdf1fa239c7089fbcf5ea
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:06:30 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_version_books_testament_apocrypha } from "../../../love/public/src/ebible_version_books_testament_apocrypha.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -5,6 +6,6 @@ export async function sandbox() {
   marker("1");
   const bible_folder = "engwebu";
   let books = await ebible_version_books_testament_apocrypha(bible_folder);
-  return books;
-  let mapped = list_map_property(list, property_name);
+  let mapped = list_map_property(books, "text");
+  let joined = list_join_newline(list);
 }
