38702cc46c46d932667b5a6a53110a959faa0b01
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 23:30:22 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,27 +1,6 @@
-import { bible_audio_folder_book_video_join } from "../../../love/public/src/bible_audio_folder_book_video_join.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
-import { ebible_version_books_testament_apocrypha } from "../../../love/public/src/ebible_version_books_testament_apocrypha.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { ebible_book_video_generate } from "../../../love/public/src/ebible_book_video_generate.mjs";
+import { ebible_apocrypha_videos_generate } from "../../../love/public/src/ebible_apocrypha_videos_generate.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox() {
   marker("1");
-  const bible_folder = "engwebu";
-  let books = await ebible_version_books_testament_apocrypha(bible_folder);
-  async function lambda(item) {
-    let book_code = object_property_get(item, "book_code");
-    log({
-      book_code,
-    });
-    await ebible_book_video_generate(bible_folder, book_code);
-  }
-  let m = await list_map_async(books, lambda);
-  let mapped = list_map_property(m, "path_video");
-  let path_video = await bible_audio_folder_book_video_join(
-    bible_folder,
-    "apocrypha",
-    mapped,
-  );
+  await ebible_apocrypha_videos_generate();
 }
