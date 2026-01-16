4e3f3f82dfd3c3108652bf0e4a5966eac069554b
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 15:23:15 -0500

    function_auto_path ..\love\public\src\html_style_padding_x.mjs

---
+++ web/public/src/html_style_padding_x.mjs
@@ -0,0 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function html_style_padding_x(component, value) {
+  marker("1");
+  html_style_assign(component, {
+    "margin-left": value,
+    "margin-right": value,
+  });
+}
