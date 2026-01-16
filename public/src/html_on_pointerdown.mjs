ec1c50b20581b74d6eb918d08ffdea712d3524c4
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 15:39:52 -0500

    function_auto_path ..\love\public\src\html_on_pointerdown.mjs

--- public/src/html_on_pointerdown.mjs
+++ public/src/html_on_pointerdown.mjs
@@ -2,6 +2,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { html_on } from "../../../love/public/src/html_on.mjs";
 export function html_on_pointerdown(component, lambda) {
   marker("1");
-  const name_event = "pointerdown";
+  const name_event = "click";
   html_on(component, name_event, lambda);
 }
