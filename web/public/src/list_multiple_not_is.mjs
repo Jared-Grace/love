9d0543651e426ab2e964e5b11a0859675d210caf
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 18:09:11 -0500

    function_auto_path ..\love\public\src\list_multiple_not_is.mjs

--- web/public/src/list_multiple_not_is.mjs
+++ web/public/src/list_multiple_not_is.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
-export function list_multiple_not_is() {
-  let m2 = list_multiple_is(list);
-  let n = not(m2);
+export function list_multiple_not_is(list) {
+  let m = list_multiple_is(list);
+  let n = not(m);
   return n;
 }
