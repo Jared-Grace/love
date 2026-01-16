35a182466e9752e5730bc9a0fa0b5ca174b31006
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 18:11:53 -0500

    function_auto_path ..\love\public\src\list_replace_all.mjs

--- public/src/list_replace_all.mjs
+++ public/src/list_replace_all.mjs
@@ -1,6 +1,6 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_empty } from "../../../love/public/src/list_empty.mjs";
-export function list_replace_all(args, args_new) {
-  list_empty(args);
-  list_add_multiple(args, args_new);
+export function list_replace_all(list, list_new) {
+  list_empty(list);
+  list_add_multiple(list, list_new);
 }
