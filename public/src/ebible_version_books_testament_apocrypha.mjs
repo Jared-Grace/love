46aa87e3109163cbcef72e157427f66d8cbdc20f
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 22:20:28 -0500

    function_auto_path ..\love\public\src\ebible_version_books_testament_apocrypha.mjs

--- public/src/ebible_version_books_testament_apocrypha.mjs
+++ public/src/ebible_version_books_testament_apocrypha.mjs
@@ -4,6 +4,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_books_testament_apocrypha(bible_folder) {
   marker("1");
   let selector = ebible_class_apocrypha();
-  let v = await ebible_version_books_testament(bible_folder, selector);
-  return v;
+  let books = await ebible_version_books_testament(bible_folder, selector);
+  return books;
 }
