fde6d0204231a43c7bf7f35c0a586c9825f30ece
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:35:00 -0500

    function_auto_path ..\love\public\src\ebible_text_to_speech_chapter_generic.mjs

---
+++ public/src/ebible_text_to_speech_chapter_generic.mjs
@@ -0,0 +1,19 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { text_to_speech } from "../../../love/public/src/text_to_speech.mjs";
+import { bible_audio_folder } from "../../../love/public/src/bible_audio_folder.mjs";
+import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+export async function ebible_text_to_speech_chapter_generic(
+  bible_folder,
+  verses,
+  chapter_code,
+) {
+  marker("1");
+  let mapped = list_map_property(verses, "text");
+  let text = list_join_space(mapped);
+  let f = bible_audio_folder(bible_folder, chapter_code);
+  await text_to_speech({
+    text: text,
+    path_output: f,
+  });
+}
