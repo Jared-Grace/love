f4d9dc44777809e8e8688db04af9ba61a24b6ae2
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 18:08:14 -0500

    function_auto_path ..\love\public\src\list_map_add_async.mjs

--- public/src/list_map_add_async.mjs
+++ public/src/list_map_add_async.mjs
@@ -1,3 +1,4 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 export async function list_map_add_async(
@@ -5,6 +6,7 @@ export async function list_map_add_async(
   lambda2,
   languages_verses,
 ) {
+  assert_arguments(arguments, 3);
   let languages_verses_add = await list_map_async(languages_chosen, lambda2);
   list_add_multiple(languages_verses, languages_verses_add);
 }
