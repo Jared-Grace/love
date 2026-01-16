c8145217082fa03596a8cf6edce1cb38ae1490aa
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:31:40 -0500

    function_auto_path ..\love\public\src\file_parent_exists_ensure.mjs

--- public/src/file_parent_exists_ensure.mjs
+++ public/src/file_parent_exists_ensure.mjs
@@ -1,11 +1,9 @@
+import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { file_root_exists_assert } from "../../../love/public/src/file_root_exists_assert.mjs";
 export async function file_parent_exists_ensure(file_path) {
   await file_root_exists_assert(file_path);
-  const dir = path.dirname(file_path);
-  let fs = await import("fs");
   let path = await import("path");
-  await fs.promises.mkdir(dir, {
-    recursive: true,
-  });
+  const dir = path.dirname(file_path);
+  await folder_exists_ensure(dir);
   return;
 }
