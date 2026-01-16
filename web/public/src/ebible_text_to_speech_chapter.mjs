92264c2afc1a1a32bbbbc5f15580c21bf759b61b
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:35:06 -0500

    rr ebible_apocrypha_text_to_speech_chapter_generic _apocrypha_ _

--- web/public/src/ebible_text_to_speech_chapter.mjs
+++ web/public/src/ebible_text_to_speech_chapter.mjs
@@ -1,5 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-import { ebible_apocrypha_text_to_speech_chapter_generic } from "../../../love/public/src/ebible_apocrypha_text_to_speech_chapter_generic.mjs";
+import { ebible_text_to_speech_chapter_generic } from "../../../love/public/src/ebible_text_to_speech_chapter_generic.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function ebible_text_to_speech_chapter(
   bible_folder,
@@ -7,7 +7,7 @@ export async function ebible_text_to_speech_chapter(
 ) {
   marker("1");
   let verses = await ebible_verses(bible_folder, chapter_code);
-  await ebible_apocrypha_text_to_speech_chapter_generic(
+  await ebible_text_to_speech_chapter_generic(
     bible_folder,
     verses,
     chapter_code,
