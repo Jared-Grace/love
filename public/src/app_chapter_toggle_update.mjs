6718fb2712c734f2bedf2f0358db1d7be842a5bd
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 17:25:09 -0500

    function_auto_path ..\love\public\src\app_chapter_toggle_update.mjs

--- public/src/app_chapter_toggle_update.mjs
+++ public/src/app_chapter_toggle_update.mjs
@@ -1,3 +1,17 @@
+import { list_join_newline_2_copy } from "../../../love/public/src/list_join_newline_2_copy.mjs";
+import { list_squash } from "../../../love/public/src/list_squash.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { list_concat } from "../../../love/public/src/list_concat.mjs";
+import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src/ebible_parts_chapter_code_to_reference.mjs";
+import { list_map_find_property } from "../../../love/public/src/list_map_find_property.mjs";
+import { list_slice_from } from "../../../love/public/src/list_slice_from.mjs";
+import { list_last } from "../../../love/public/src/list_last.mjs";
+import { list_first } from "../../../love/public/src/list_first.mjs";
+import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { integer_to } from "../../../love/public/src/integer_to.mjs";
+import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_style_background_color_set_or_remove_list } from "../../../love/public/src/html_style_background_color_set_or_remove_list.mjs";
 import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
@@ -19,6 +33,7 @@ export function app_chapter_toggle_update(
   function choose() {
     toggle();
     invoke_multiple(updates);
+    copy;
   }
   html_on_click(component_clicked, choose);
   let toggle = function lambda5() {
@@ -39,5 +54,29 @@ export function app_chapter_toggle_update(
     toggle,
     update,
   };
+  async function copy() {
+    list_sort_number_mapper(verse_numbers_chosen, integer_to);
+    function lambda3(bv) {
+      let books2 = object_property_get(bv, "books");
+      let verses2 = object_property_get(bv, "verses");
+      let verse_numbers = list_map_property(verses2, "verse_number");
+      let v = list_first_last(verse_numbers_chosen);
+      let first = list_first(v);
+      let last = list_last(v);
+      let sliced = list_slice_from(verse_numbers, first, last);
+      let mapped2 = list_map_find_property(sliced, verses2, "verse_number");
+      let mapped3 = list_map_property(mapped2, "text");
+      let reference = ebible_parts_chapter_code_to_reference(
+        chapter_code,
+        books2,
+        [first, last],
+      );
+      let concated2 = list_concat([reference], mapped3);
+      return concated2;
+    }
+    let m = list_map(languages_verses, lambda3);
+    let squashed = list_squash(m);
+    let joined = await list_join_newline_2_copy(squashed);
+  }
   return v3;
 }
