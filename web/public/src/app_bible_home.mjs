20ac6105aee7dddcb6a0d0816b16c7a3d05da603
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 15:17:11 -0500

    function_auto_path ..\love\public\src\app_bible_home.mjs

--- web/public/src/app_bible_home.mjs
+++ web/public/src/app_bible_home.mjs
@@ -47,7 +47,7 @@ export async function app_bible_home(context) {
   html_centered(bar);
   let e = ebible_folder_english();
   let hash = html_hash_object_get();
-  let n = object_property_exists_not(hash, "c");
+  let n = object_property_exists_not(hash, "c");$i
   let chapter_code = null;
   chapter_code = object_property_get(hash, "c");
   let v2 = ebible_chapter_code_parse(chapter_code);
