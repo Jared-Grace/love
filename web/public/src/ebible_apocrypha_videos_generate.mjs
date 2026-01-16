72ebcbbdcc685b65dc80ac2f10e7c40c3fa92060
Grace-Jared <christrosetolife@gmail.com>
2026-01-15 23:39:27 -0500

    function_auto_path ..\love\public\src\ebible_apocrypha_videos_generate.mjs

--- web/public/src/ebible_apocrypha_videos_generate.mjs
+++ web/public/src/ebible_apocrypha_videos_generate.mjs
@@ -13,7 +13,8 @@ export async function ebible_apocrypha_videos_generate() {
     log({
       book_code,
     });
-    return await ebible_book_video_generate(bible_folder, book_code);
+    let v = await ebible_book_video_generate(bible_folder, book_code);
+    return v;
   }
   let m = await list_map_async(books, lambda);
   let mapped = list_map_property(m, "path_video");
