4e37d2f936b78b6d9ba60c975b97d19642f13fc2
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 15:14:47 -0500

    function_auto_path ..\love\public\src\html_hash_object_get.mjs

--- public/src/html_hash_object_get.mjs
+++ public/src/html_hash_object_get.mjs
@@ -12,9 +12,9 @@ export function html_hash_object_get() {
   let hash_url = html_hash_get();
   let prefix = html_hash_symbol();
   let n = string_includes_not(hash_url, prefix);
-  if (false) {
+  if (n) {
+    return;
   }
-  return;
   let without = string_prefix_without(hash_url, prefix);
   let split = string_split_comma(without);
   function lambda3(oa) {
