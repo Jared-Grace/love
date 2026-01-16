63f0f9d92f53ef99e230f0a6a7e94744cb3e81f4
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 18:22:28 -0500

    function_auto_path ..\love\public\src\list_size_max_skip_replace.mjs

--- public/src/list_size_max_skip_replace.mjs
+++ public/src/list_size_max_skip_replace.mjs
@@ -1,10 +1,6 @@
 import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { list_size_max_skip_copy } from "../../../love/public/src/list_size_max_skip_copy.mjs";
 export function list_size_max_skip_replace(verse_numbers_chosen, max) {
   let copy = list_size_max_skip_copy(verse_numbers_chosen, max);
-  log({
-    copy,
-  });
   list_replace_all(verse_numbers_chosen, copy);
 }
