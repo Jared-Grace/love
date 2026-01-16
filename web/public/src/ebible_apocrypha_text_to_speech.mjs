67c88f4f44b126e141ec5020c07836247eb4f658
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:40:55 -0500

    function_auto_path ..\love\public\src\ebible_apocrypha_text_to_speech.mjs

--- web/public/src/ebible_apocrypha_text_to_speech.mjs
+++ web/public/src/ebible_apocrypha_text_to_speech.mjs
@@ -1,9 +1,7 @@
-import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
-import { ebible_book_code_to_chapter_codes } from "../../../love/public/src/ebible_book_code_to_chapter_codes.mjs";
+import { ebible_text_to_speech_book } from "../../../love/public/src/ebible_text_to_speech_book.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { ebible_text_to_speech_chapter_generic } from "../../../love/public/src/ebible_text_to_speech_chapter_generic.mjs";
 import { ebible_version_books_testament_apocrypha } from "../../../love/public/src/ebible_version_books_testament_apocrypha.mjs";
 import { ebible_chapters_each_verses_check } from "../../../love/public/src/ebible_chapters_each_verses_check.mjs";
 export async function ebible_apocrypha_text_to_speech() {
@@ -16,22 +14,7 @@ export async function ebible_apocrypha_text_to_speech() {
     log({
       book_code,
     });
-    let chapter_codes = await ebible_book_code_to_chapter_codes(
-      bible_folder,
-      book_code,
-    );
-    async function lambda2(chapter_code) {
-      log({
-        chapter_code,
-      });
-      let verses = await ebible_verses(bible_folder, chapter_code);
-      await ebible_text_to_speech_chapter_generic(
-        bible_folder,
-        verses,
-        chapter_code,
-      );
-    }
-    await each_async(chapter_codes, lambda2);
+    await ebible_text_to_speech_book(bible_folder, book_code);
   }
   await each_async(books, lambda);
   return;
