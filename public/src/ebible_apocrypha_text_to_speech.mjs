62de094ab0df984ed7787298c9d27d2080e23713
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 14:37:15 -0500

    function_auto_path ..\love\public\src\ebible_apocrypha_text_to_speech.mjs

--- public/src/ebible_apocrypha_text_to_speech.mjs
+++ public/src/ebible_apocrypha_text_to_speech.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_text_to_speech_chapter_generic } from "../../../love/public/src/ebible_text_to_speech_chapter_generic.mjs";
@@ -11,6 +12,7 @@ export async function ebible_apocrypha_text_to_speech() {
   await ebible_chapters_each_verses_check(bible_folder);
   let books = await ebible_version_books_testament_apocrypha(bible_folder);
   async function lambda(book) {
+    let book_code = object_property_get(book, "book_code");
     log({
       book,
     });
