e4d460eea3d0b064181c372402ca49f934708a8c
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 15:54:19 -0500

    function_auto_path ..\love\public\src\ebible_verses_references.mjs

--- public/src/ebible_verses_references.mjs
+++ public/src/ebible_verses_references.mjs
@@ -1,11 +1,13 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src/ebible_parts_chapter_code_to_reference.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_verses_references(bible_folder, chapter_code) {
   marker("1");
-  let books2 = await ebible_version_books(bible_folder2);
+  let books = await ebible_version_books(bible_folder);
   let v = await ebible_verses(bible_folder, chapter_code);
+  each(list, function lambda(item) {});
   let reference = ebible_parts_chapter_code_to_reference(
     chapter_code2,
     books,
