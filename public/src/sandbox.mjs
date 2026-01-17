08824d89ff34116aea97622d12c9ff3157cff222
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 16:43:31 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,6 @@
-import { g_distance } from "../../../love/public/src/g_distance.mjs";
-import { function_dependencies } from "../../../love/public/src/function_dependencies.mjs";
+import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
+import { function_dependencies_code_unaliased } from "../../../love/public/src/function_dependencies_code_unaliased.mjs";
 export async function sandbox() {
-  let v2 = await function_dependencies([g_distance.name]);
+  let v2 = await function_dependencies_code_unaliased([app_g_main.name]);
   return v2;
 }
