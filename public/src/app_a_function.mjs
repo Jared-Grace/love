edd22dd2c9c2f25777eb7243385dd5f6d2c96445
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 23:24:30 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- public/src/app_a_function.mjs
+++ public/src/app_a_function.mjs
@@ -1,10 +1,10 @@
+import { html_bar_content } from "../../../love/public/src/html_bar_content.mjs";
 import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
-import { html_bar_content_padded } from "../../../love/public/src/html_bar_content_padded.mjs";
 import { app_a_app_run } from "../../../love/public/src/app_a_app_run.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
@@ -186,9 +186,9 @@ export async function app_a_function(context) {
   if (e) {
     list_add(choices, preview_app);
   }
-  let v5 = html_bar_content_padded(root);
-  let content = object_property_get(v5, "content");
-  let bar = object_property_get(v5, "bar");
+  let bc = html_bar_content(root);
+  let content = object_property_get(bc, "content");
+  let bar = object_property_get(bc, "bar");
   app_a_buttons_shortcuts(bar, choices);
   function screen_choose(screen) {
     let f = async function screen_choose_inner() {
