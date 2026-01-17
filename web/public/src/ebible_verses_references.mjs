efa1b96d8a36408648d1ea22ad2eaaa5590e8546
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 15:55:03 -0500

    function_auto_path ..\love\public\src\ebible_verses_references.mjs

--- web/public/src/ebible_verses_references.mjs
+++ web/public/src/ebible_verses_references.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src/ebible_parts_chapter_code_to_reference.mjs";
@@ -8,6 +9,8 @@ export async function ebible_verses_references(bible_folder, chapter_code) {
   let books = await ebible_version_books(bible_folder);
   let vs = await ebible_verses(bible_folder, chapter_code);
   function lambda(v) {
+    let text = object_property_get(v, "text");
+    let verse_number = object_property_get(v, "verse_number");
     let reference = ebible_parts_chapter_code_to_reference(
       chapter_code2,
       books,
