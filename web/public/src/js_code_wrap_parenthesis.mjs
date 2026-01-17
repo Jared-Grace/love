815f807e9322093bfa3f6d9ab60740eb66a855ae
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 15:56:35 -0500

    function_auto_path ..\love\public\src\js_code_wrap_parenthesis.mjs

--- web/public/src/js_code_wrap_parenthesis.mjs
+++ web/public/src/js_code_wrap_parenthesis.mjs
@@ -1,8 +1,6 @@
-import { js_code_parenthesis_right } from "../../../love/public/src/js_code_parenthesis_right.mjs";
-import { js_code_parenthesis_left } from "../../../love/public/src/js_code_parenthesis_left.mjs";
+import { string_wrap_parenthesis } from "../../../love/public/src/string_wrap_parenthesis.mjs";
 export function js_code_wrap_parenthesis(inside) {
   const inside_larger = " " + inside + " ";
-  let v =
-    js_code_parenthesis_left() + inside_larger + js_code_parenthesis_right();
+  let v = string_wrap_parenthesis(inside_larger);
   return v;
 }
