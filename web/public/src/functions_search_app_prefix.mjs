8f74fae0589c77fc85380fac512ffacf853384dd
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 19:29:27 -0500

    function_auto_path ..\love\public\src\functions_search_app_prefix.mjs

--- web/public/src/functions_search_app_prefix.mjs
+++ web/public/src/functions_search_app_prefix.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_name_main_get } from "../../../love/public/src/app_name_main_get.mjs";
 import { functions_search } from "../../../love/public/src/functions_search.mjs";
@@ -6,6 +7,9 @@ export async function functions_search_app_prefix(a_name) {
   marker("1");
   let v2 = await app_name_main_get(a_name);
   let f_name = object_property_get(v2, "f_name");
+  log({
+    f_name,
+  });
   let v = await functions_search(f_name);
   return v;
 }
