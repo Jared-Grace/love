d88b4bac7abecd7e61ac7cc4cd95bd4b4e8a2d66
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 18:54:40 -0500

    function_auto_path ..\love\public\src\function_parse_declaration_unalised.mjs

--- public/src/function_parse_declaration_unalised.mjs
+++ public/src/function_parse_declaration_unalised.mjs
@@ -1,7 +1,7 @@
-import { undefined } from "../../../love/public/src/undefined.mjs";
+import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_parse_declaration_unalised(f_name) {
   marker("1");
-  let v = await undefined(f_name);
+  let v = await function_parse_declaration(f_name);
   return v;
 }
