07ba6682b33fe669b25057460d53a9d0179c9489
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 16:42:03 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function sandbox(b) {
+  marker("1");
   let c = null;
   log(a);
   log(b);
