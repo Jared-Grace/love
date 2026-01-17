c31c778097c54af69abef673f6baa72699eda86c
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 23:23:21 -0500

    function_auto_path ..\love\public\src\html_bar_content_padded.mjs

--- public/src/html_bar_content_padded.mjs
+++ public/src/html_bar_content_padded.mjs
@@ -1,28 +1,12 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { html_bar_content } from "../../../love/public/src/html_bar_content.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_bar_content_padding } from "../../../love/public/src/html_bar_content_padding.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-import { html_div } from "../../../love/public/src/html_div.mjs";
 export function html_bar_content_padded(root) {
   marker("1");
-  let shell = html_div(root);
-  html_style_assign(shell, {
-    display: "flex",
-    "flex-direction": "column",
-    height: "100dvh",
-    "box-sizing": "border-box",
-  });
-  let bar = html_div(shell);
-  html_style_assign(bar, {
-    flex: "0 0 auto",
-  });
-  let content = html_div(shell);
-  html_style_assign(content, {
-    flex: "1 1 auto",
-    "min-height": "0",
-    position: "relative",
-    "overflow-y": "scroll",
-    "overflow-x": "auto",
-  });
+  let v = html_bar_content(root);
+  let content = object_property_get(v, "content");
+  let bar = object_property_get(v, "bar");
   let bc = {
     bar,
     content,
