ea511b48af41620808065254ecc320f24cbead3c
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 15:53:31 -0500

    function_auto_path ..\love\public\src\ebible_verses_references.mjs

--- web/public/src/ebible_verses_references.mjs
+++ web/public/src/ebible_verses_references.mjs
@@ -2,5 +2,6 @@ import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_verses_references(bible_folder, chapter_code) {
   marker("1");
-  return await ebible_verses(bible_folder, chapter_code);
+  let v = await ebible_verses(bible_folder, chapter_code);
+  return v;
 }
