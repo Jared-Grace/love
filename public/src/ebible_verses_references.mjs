92ab8cb378a91473374ccc073bb0eb357c9bf104
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 15:53:28 -0500

    fws evs references

--- public/src/ebible_verses_references.mjs
+++ public/src/ebible_verses_references.mjs
@@ -1,4 +1,6 @@
+import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_verses_references() {
+export async function ebible_verses_references(bible_folder, chapter_code) {
   marker("1");
+  return await ebible_verses(bible_folder, chapter_code);
 }
