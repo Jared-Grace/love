175845486aaa8845951b80905a50eb3b2f6c48a1
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 19:30:41 -0500

    function_auto_path ..\love\public\src\functions_search_app_prefix.mjs

--- public/src/functions_search_app_prefix.mjs
+++ public/src/functions_search_app_prefix.mjs
@@ -1,11 +1,13 @@
+import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_name_main_get } from "../../../love/public/src/app_name_main_get.mjs";
 import { functions_search } from "../../../love/public/src/functions_search.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function functions_search_app_prefix(a_name) {
+export async function functions_search_app_prefix(s) {
   marker("1");
-  let v2 = await app_name_main_get(a_name);
-  let f_name = object_property_get(v2, "f_name");
-  let v = await functions_search(f_name);
+  let v2 = await app_name_main_get(s);
+  s = object_property_get(v2, "a_name");
+  let a_name = app_name_prefixed(s);
+  let v = await functions_search(a_name);
   return v;
 }
