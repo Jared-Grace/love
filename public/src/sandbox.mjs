97fe09e080d4731a541df30242fb6569704f5726
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 16:42:40 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,6 @@
 import { g_distance } from "../../../love/public/src/g_distance.mjs";
 import { function_dependencies } from "../../../love/public/src/function_dependencies.mjs";
-export async function sandbox(b) {
+export async function sandbox() {
   let v2 = await function_dependencies([g_distance.name]);
   return v2;
 }
