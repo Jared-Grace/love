acfd1ac4b292b0e47537e7581e666e7956a5e603
Grace-Jared <christrosetolife@gmail.com>
2026-01-16 19:27:41 -0500

    fws s app_prefix

--- web/public/src/functions_search_app_prefix.mjs
+++ web/public/src/functions_search_app_prefix.mjs
@@ -1,4 +1,6 @@
+import { functions_search } from "../../../love/public/src/functions_search.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function functions_search_app_prefix() {
+export async function functions_search_app_prefix(search) {
   marker("1");
+  return await functions_search(search);
 }
