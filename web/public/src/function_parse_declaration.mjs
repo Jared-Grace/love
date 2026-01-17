898d957c698f9ae99abeff085fec855f55f0b1c2
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 18:55:48 -0500

    function_auto_path ..\love\public\src\function_parse_declaration.mjs

--- web/public/src/function_parse_declaration.mjs
+++ web/public/src/function_parse_declaration.mjs
@@ -3,7 +3,7 @@ import { function_parse_imports_packages } from "../../../love/public/src/functi
 import { function_parse_declaration_inner } from "../../../love/public/src/function_parse_declaration_inner.mjs";
 export async function function_parse_declaration(f_name) {
   let fn_parse = function_parse;
-  let p = await function_parse(f_name);
+  let p = await fn_parse(f_name);
   let to = function_parse_declaration_inner(p);
   let parsed = function_parse_imports_packages(to);
   return parsed;
