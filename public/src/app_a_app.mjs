afb5de5f7ed8a957683129ac9d0493f57fa09cdc
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:37:06 -0500

    function_auto_path ..\love\public\src\app_a_app.mjs

--- public/src/app_a_app.mjs
+++ public/src/app_a_app.mjs
@@ -1,8 +1,10 @@
+import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
+import { app_a_app_selected_key } from "../../../love/public/src/app_a_app_selected_key.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_app(context) {
   marker("1");
   let root = html_clear_context(context);
-      let key = app_a_app_selected_key();
-      a_name=storage_local_get_context(context, key, );
+  let key = app_a_app_selected_key();
+  let a_name = storage_local_get_context(context, key);
 }
