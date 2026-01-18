6e40aa03b81a271731b1dbe0b2da0fe2d3a70dac
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:39:26 -0500

    function_auto_path ..\love\public\src\app_generic_name_latest.mjs

---
+++ web/public/src/app_generic_name_latest.mjs
@@ -0,0 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
+export function app_generic_name_latest(name) {
+  marker("1");
+  let a_name = app_name_prefixed(name);
+  let combined = function_name_combine(a_name, "latest");
+  return combined;
+}
