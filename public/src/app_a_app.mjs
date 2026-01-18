94f7274085f09f0743b1ae7692a8285ad1a378f1
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 20:35:56 -0500

    function_auto_path ..\love\public\src\app_a_app.mjs

--- public/src/app_a_app.mjs
+++ public/src/app_a_app.mjs
@@ -1,3 +1,4 @@
+import { app_a_button_wide } from "../../../love/public/src/app_a_button_wide.mjs";
 import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -12,6 +13,7 @@ export async function app_a_app(context) {
   let a_name = storage_local_get_context(context, key);
   let combined = app_generic_name_main(a_name);
   let u = await function_exists(combined);
+  let component = app_a_button_wide(parent, text, function lambda2() {});
   if (false) {
   }
   log({
