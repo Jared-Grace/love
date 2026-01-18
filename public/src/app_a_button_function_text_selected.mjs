23ff2c77ddf3baf8485ba7f1148a3e9505272eaa
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 23:02:04 -0500

    function_auto_path ..\love\public\src\app_a_button_function_text_selected.mjs

--- public/src/app_a_button_function_text_selected.mjs
+++ public/src/app_a_button_function_text_selected.mjs
@@ -1,11 +1,9 @@
+import { app_a_function_name_selected } from "../../../love/public/src/app_a_function_name_selected.mjs";
 import { app_a_button_function_text } from "../../../love/public/src/app_a_button_function_text.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
-import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
 export function app_a_button_function_text_selected(context) {
   marker("1");
-  const key = app_a_function_name_selected_key();
-  let f_name = storage_local_get_context(context, key);
+  let f_name = app_a_function_name_selected(context);
   const text = app_a_button_function_text(f_name);
   return text;
 }
