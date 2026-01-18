7335048819f2ad39d6d7fd193934612ac64482ed
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 20:34:22 -0500

    function_auto_path ..\love\public\src\app_a_app.mjs

--- web/public/src/app_a_app.mjs
+++ web/public/src/app_a_app.mjs
@@ -1,15 +1,15 @@
-import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
+import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { app_a_app_selected_key } from "../../../love/public/src/app_a_app_selected_key.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function app_a_app(context) {
+export function app_a_app(context) {
   marker("1");
   let root = html_clear_context(context);
   let key = app_a_app_selected_key();
   let a_name = storage_local_get_context(context, key);
-  let r = await app_generic_name_main_get(search);
+  let combined = app_generic_name_main(a);
   log({
     a_name,
   });
