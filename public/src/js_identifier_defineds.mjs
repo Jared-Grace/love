17a5cef0bcb1b32d64d722b5cdc786941d3c6421
Grace-Jared <christrosetolife@gmail.com>
2026-01-18 16:12:00 -0500

    function_auto_path ..\love\public\src\js_identifier_defineds.mjs

--- public/src/js_identifier_defineds.mjs
+++ public/src/js_identifier_defineds.mjs
@@ -15,26 +15,26 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { error } from "../../../love/public/src/error.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export function js_identifier_defineds(v) {
-  let { stack } = v;
+  let stack = object_property_get(v, "stack");
   function lambda4(la) {
     js_stack_filtered_each(stack, "BlockStatement", lambda3);
     function lambda3(bs) {
       let list = list_next(stack, bs);
       let item = list_next(stack, list);
       let index = list_index_of_next_outside(list, item);
-      function lambda2(i) {
+      function each_statement(i) {
         let list_item = list_get(list, i);
         if (js_node_type_is(list_item, "VariableDeclaration")) {
-          let { declarations } = list_item;
+          let declarations = object_property_get(list_item, "declarations");
           let ids = list_map_property(declarations, "id");
           function lambda(id) {
             if (js_node_type_is(id, "ObjectPattern")) {
-              let { properties } = id;
+              let properties = object_property_get(id, "properties");
               let values = list_map_property(properties, "value");
               let names = js_identifiers_to_names(values);
               la(names);
             } else if (js_node_type_is(id, "ArrayPattern")) {
-              let { elements } = id;
+              let elements = object_property_get(id, "elements");
               let names = js_identifiers_to_names(elements);
               la(names);
             } else if (js_node_type_is(id, "Identifier")) {
@@ -48,7 +48,7 @@ export function js_identifier_defineds(v) {
           each(ids, lambda);
         }
       }
-      each_range(index, lambda2);
+      each_range(index, each_statement);
     }
     let types = js_types_function();
     function lambda5(node) {
