05efdbb66f520f5ae7b3405df5c95bf7bccfd99a
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:31:37 -0500

    function_auto_path ..\love\public\src\app_a_button_function.mjs

--- public/src/app_a_button_function.mjs
+++ public/src/app_a_button_function.mjs
@@ -1,9 +1,8 @@
+import { app_a_button_function_text } from "../../../love/public/src/app_a_button_function_text.mjs";
 import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
-import { emoji_computer } from "../../../love/public/src/emoji_computer.mjs";
 import { app_a_button } from "../../../love/public/src/app_a_button.mjs";
-import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { storage_local_exists_context } from "../../../love/public/src/storage_local_exists_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_button_function(context) {
@@ -11,9 +10,7 @@ export function app_a_button_function(context) {
   const key = app_a_function_name_selected_key();
   let exists = storage_local_exists_context(context, key);
   if (exists) {
-    const key = app_a_function_name_selected_key();
-    let f_name = storage_local_get_context(context, key);
-    const text = emoji_computer() + " function: " + f_name;
+    const text = app_a_button_function_text(context);
     let component = app_a_button(root, text, lambda2);
     function lambda2() {
       app_generic_screen_set(context, app_a_function);
