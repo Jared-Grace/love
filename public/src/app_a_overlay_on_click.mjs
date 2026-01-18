83ebc9ba039125ebe92e42d0ebeabe1fcc0e8b24
Grace-Jared <christrosetolife@gmail.com>
2026-01-18 17:35:03 -0500

    function_auto_path ..\love\public\src\app_a_overlay_on_click.mjs

--- public/src/app_a_overlay_on_click.mjs
+++ public/src/app_a_overlay_on_click.mjs
@@ -2,9 +2,9 @@ import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
 import { app_a_on_keydown } from "../../../love/public/src/app_a_on_keydown.mjs";
 import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
 export function app_a_overlay_on_click(a, component, lambda$o) {
-  function on_click() {
+  async function on_click() {
     let o = app_a_overlay(a, on_keydown);
-    let choices = lambda$o(o);
+    let choices = await lambda$o(o);
     function on_keydown(e) {
       app_a_on_keydown(e, choices);
     }
