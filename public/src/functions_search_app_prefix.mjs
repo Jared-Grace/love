71fa18e3d80260d89437c4108125aba3941622cb
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:40:30 -0500

    function_auto_path ..\love\public\src\app_promote.mjs

--- public/src/functions_search_app_prefix.mjs
+++ public/src/functions_search_app_prefix.mjs
@@ -3,12 +3,12 @@ import { function_name_to_path } from "../../../love/public/src/function_name_to
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 import { app_name_prefixed } from "../../../love/public/src/app_name_prefixed.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_name_main_get } from "../../../love/public/src/app_name_main_get.mjs";
+import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
 import { functions_search } from "../../../love/public/src/functions_search.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_search_app_prefix(s) {
   marker("1");
-  let v2 = await app_name_main_get(s);
+  let v2 = await app_generic_name_main_get(s);
   s = object_property_get(v2, "a_name");
   let a_name = app_name_prefixed(s);
   let separator = function_name_separator();
