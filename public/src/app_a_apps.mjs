59313adf557d0667964315cd90e8a99d708b53d0
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:10:05 -0500

    function_auto_path ..\love\public\src\app_a_apps.mjs

--- public/src/app_a_apps.mjs
+++ public/src/app_a_apps.mjs
@@ -1,3 +1,5 @@
+import { app_a_button_wide } from "../../../love/public/src/app_a_button_wide.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
 import { app_a_app_run } from "../../../love/public/src/app_a_app_run.mjs";
 import { app_a } from "../../../love/public/src/app_a.mjs";
@@ -6,6 +8,8 @@ import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_apps(context) {
+  let root = object_property_get(context, "root");
+  let component = app_a_button_wide(parent, text, function lambda2() {});
   let mapped = await apps_names();
   marker("1");
   async function on_select(a_name) {
