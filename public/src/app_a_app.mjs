287c8f5f54d156f09cb6576e94a483d789ec6254
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 20:34:04 -0500

    function_auto_path ..\love\public\src\app_a_app.mjs

--- public/src/app_a_app.mjs
+++ public/src/app_a_app.mjs
@@ -1,15 +1,15 @@
-import { app_generic_name_prefixed } from "../../../love/public/src/app_generic_name_prefixed.mjs";
+import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { app_a_app_selected_key } from "../../../love/public/src/app_a_app_selected_key.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_app(context) {
+export async function app_a_app(context) {
   marker("1");
   let root = html_clear_context(context);
   let key = app_a_app_selected_key();
   let a_name = storage_local_get_context(context, key);
-  let a_name2 = app_generic_name_prefixed(name);
+  let r = await app_generic_name_main_get(search);
   log({
     a_name,
   });
