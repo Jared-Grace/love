7c6728f300cca363f3a776495d1c8d866ecba59e
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 23:00:01 -0500

    function_auto_path ..\love\public\src\app_a_app_run.mjs

--- web/public/src/app_a_button_function_text_selected.mjs
+++ web/public/src/app_a_button_function_text_selected.mjs
@@ -6,6 +6,10 @@ export function app_a_button_function_text_selected(context) {
   marker("1");
   const key = app_a_function_name_selected_key();
   let f_name = storage_local_get_context(context, key);
-  const text = emoji_computer() + " function: " + f_name;
+  const text = app_a_button_function_text(f_name);
   return text;
 }
+function app_a_button_function_text(f_name) {
+  return emoji_computer() + " function: " + f_name;
+}
+
