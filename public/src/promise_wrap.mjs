3919c37483127f79fb5d0ee8910d133838ea1af6
Grace-Jared <christrosetolife@gmail.com>
2026-01-18 17:17:56 -0500

    function_auto_path ..\love\public\src\promise_wrap.mjs

--- public/src/promise_wrap.mjs
+++ public/src/promise_wrap.mjs
@@ -1,10 +1,10 @@
 export function promise_wrap(lambda) {
-  let v = new Promise(function lambda4(resolve, reject) {
+  let p = new Promise(function lambda4(resolve, reject) {
     try {
       lambda(resolve, reject);
     } catch (e) {
       reject(e);
     }
   });
-  return v;
+  return p;
 }
