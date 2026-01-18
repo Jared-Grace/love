73537e5099ceec32910b318483413d8e513b7e71
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:40:30 -0500

    function_auto_path ..\love\public\src\app_promote.mjs

--- web/public/src/function_open_app.mjs
+++ web/public/src/function_open_app.mjs
@@ -1,11 +1,11 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_name_main_get } from "../../../love/public/src/app_name_main_get.mjs";
+import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
 import { data_app_current_set } from "../../../love/public/src/data_app_current_set.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_open_app(a_name) {
   marker("1");
-  let v2 = await app_name_main_get(a_name);
+  let v2 = await app_generic_name_main_get(a_name);
   let a_name_final = object_property_get(v2, "a_name");
   let f_name = object_property_get(v2, "f_name");
   let v = await function_open(f_name);
--- web/public/src/html_update_latest.mjs
+++ web/public/src/html_update_latest.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_name_main_get } from "../../../love/public/src/app_name_main_get.mjs";
+import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
 import { app_generic_name_latest } from "../../../love/public/src/app_generic_name_latest.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_update_public_generic } from "../../../love/public/src/html_update_public_generic.mjs";
@@ -7,7 +7,7 @@ import { html_name_to_path_latest } from "../../../love/public/src/html_name_to_
 export async function html_update_latest(name) {
   marker("1");
   ("todo: maybe rename staging everywhere to latest to be consistent with naming, one less name (staging not needed) to remember");
-  let v2 = await app_name_main_get(name);
+  let v2 = await app_generic_name_main_get(name);
   name = object_property_get(v2, "a_name");
   let f_name = app_generic_name_latest(name);
   let file_path = html_name_to_path_latest(name);
