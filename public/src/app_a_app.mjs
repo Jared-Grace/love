c5f6e75af404ad09ae427c0ba8387155c8058fc8
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 22:36:46 -0500

    function_auto_path ..\love\public\src\app_a_app.mjs

--- public/src/app_a_app.mjs
+++ public/src/app_a_app.mjs
@@ -12,6 +12,7 @@ import { html_clear_context } from "../../../love/public/src/html_clear_context.
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_a_app(context) {
   marker("1");
+  return;
   app_a_button_function(context);
   let root = html_clear_context(context);
   function lambda3() {}
