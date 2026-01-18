2259050e43f66c4ff0987956d71df5e60d4e9599
Grace-Jared <christrosetolife@gmail.com>
2026-01-18 17:31:15 -0500

    function_auto_path ..\love\public\src\app_a_overlay_on_click.mjs

--- public/src/app_a_overlay_on_click.mjs
+++ public/src/app_a_overlay_on_click.mjs
@@ -1,4 +1,12 @@
-import { marker } from "../../../love/public/src/marker.mjs";
+import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
+import { app_a_on_keydown } from "../../../love/public/src/app_a_on_keydown.mjs";
+import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
 export function app_a_overlay_on_click() {
-  marker("1");
+  function on_click() {
+    let o = app_a_overlay(a, on_keydown);
+    function on_keydown(e) {
+      app_a_on_keydown(e, choices);
+    }
+  }
+  html_on_click(keyword, on_click);
 }
