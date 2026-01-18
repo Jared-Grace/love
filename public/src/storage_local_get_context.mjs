40d2a7a4048f2ae7422c67389cce0c11f15c3b5c
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:24:50 -0500

    function_auto_path ..\love\public\src\storage_local_get_context.mjs

--- public/src/storage_local_get_context.mjs
+++ public/src/storage_local_get_context.mjs
@@ -1,6 +1,7 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 export function storage_local_get_context(context, key) {
-  let { app_fn } = context;
+  let app_fn = object_property_get(context, "app_fn");
   let value = storage_local_get(app_fn, key);
   return value;
 }
