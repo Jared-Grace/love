e79ce5ba53dbeef049364e373721ac75c46813d1
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 23:00:04 -0500

    function_auto_path ..\love\public\src\app_a_button_function_text_selected.mjs

---
+++ public/src/app_a_button_function_text.mjs
@@ -0,0 +1,5 @@
+import { emoji_computer } from "../../../love/public/src/emoji_computer.mjs";
+export function app_a_button_function_text(f_name) {
+  let v = emoji_computer() + " function: " + f_name;
+  return v;
+}
--- public/src/app_a_button_function_text_selected.mjs
+++ public/src/app_a_button_function_text_selected.mjs
@@ -1,5 +1,5 @@
+import { app_a_button_function_text } from "../../../love/public/src/app_a_button_function_text.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { emoji_computer } from "../../../love/public/src/emoji_computer.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
 export function app_a_button_function_text_selected(context) {
@@ -9,7 +9,3 @@ export function app_a_button_function_text_selected(context) {
   const text = app_a_button_function_text(f_name);
   return text;
 }
-function app_a_button_function_text(f_name) {
-  return emoji_computer() + " function: " + f_name;
-}
-
