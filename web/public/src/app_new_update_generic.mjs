f535f8f9f76693b835844873e63cce47fa418071
Grace-Jared <christrosetolife@gmail.com>
2026-01-17 19:40:03 -0500

    function_auto_path ..\love\public\src\app_name_main_get.mjs

--- web/public/src/app_new_update_generic.mjs
+++ web/public/src/app_new_update_generic.mjs
@@ -4,7 +4,7 @@ import { list_empty } from "../../../love/public/src/list_empty.mjs";
 import { js_declaration_single_block_body } from "../../../love/public/src/js_declaration_single_block_body.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
-import { app_name_main } from "../../../love/public/src/app_name_main.mjs";
+import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
 import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
@@ -30,7 +30,7 @@ export async function app_new_update_generic(
   async function lambda(ast) {
     let declaration = js_declaration_single(ast);
     declaration.async = true;
-    let value_string = app_name_main(name);
+    let value_string = app_generic_name_main(name);
     let main_name = js_code_string(value_string);
     let code = js_code_call_args_await_maybe(
       unaliased,
