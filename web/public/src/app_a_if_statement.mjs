bb60d40877f3e528d109fc039359316ed39d9099
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 11:23:38 -0500

    function_auto_path ..\love\public\src\app_a_if_statement.mjs

--- web/public/src/app_a_if_statement.mjs
+++ web/public/src/app_a_if_statement.mjs
@@ -13,7 +13,10 @@ export function app_a_if_statement(a, node, parent) {
   let k = js_keyword_if();
   let v4 = app_a_keyword_purple_space(parent, k);
   let keyword = object_property_get(v4, "keyword");
-  async function lambda(o) {}
+  async function lambda(o) {
+    let v = [];
+    return v;
+  }
   app_a_overlay_on_click(a2, keyword, lambda);
   function lambda19() {
     let test = object_property_get(node, "test");
