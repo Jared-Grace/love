9156c42dc818f1c8af57eabf1144bdfe73ad7a84
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:50:31 -0500

    function_auto_path ..\love\public\src\app_a_function_on_change.mjs

--- public/src/app_a_function_on_change.mjs
+++ public/src/app_a_function_on_change.mjs
@@ -2,10 +2,10 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_a_function_on_keydown_remove } from "../../../love/public/src/app_a_function_on_keydown_remove.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export async function app_a_function_on_change(o2, a) {
+export async function app_a_function_on_change(o, a) {
   marker("1");
   let context = object_property_get(a, "context");
-  let rename_overlay_close = object_property_get(o2, "overlay_close");
+  let rename_overlay_close = object_property_get(o, "overlay_close");
   rename_overlay_close();
   app_a_function_on_keydown_remove(a);
   await app_a_function(context);
