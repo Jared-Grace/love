5c49cd95d7c8ec71fe1de30ae8d03dfe9d87f25b
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:27:47 -0500

    function_auto_path ..\love\public\src\marker_call.mjs

---
+++ public/src/js_call_new_insert.mjs
@@ -0,0 +1,20 @@
+import { js_stack_declaration_asyncify } from "../../../love/public/src/js_stack_declaration_asyncify.mjs";
+import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { js_call_new } from "../../../love/public/src/js_call_new.mjs";
+export async function js_call_new_insert(
+  f_name_call,
+  ast,
+  stack2,
+  index,
+  stack,
+) {
+  let v2 = await js_call_new(f_name_call, ast);
+  let parsed = object_property_get(v2, "parsed");
+  let declaration = object_property_get(v2, "declaration");
+  list_insert(stack2, index, parsed);
+  await js_imports_missing_add(ast);
+  js_stack_declaration_asyncify(stack, declaration);
+  return parsed;
+}
