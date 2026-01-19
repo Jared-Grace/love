cefe95bf7d9716146427a1f6a71b6850fe3e9dfc
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:43:35 -0500

    function_auto_path ..\love\public\src\list_remove_at.mjs

--- public/src/list_remove_at.mjs
+++ public/src/list_remove_at.mjs
@@ -1,6 +1,7 @@
+import { list_remove_at_count } from "../../../love/public/src/list_remove_at_count.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export function list_remove_at(list, index) {
-  let e = list.splice(index, 1);
+  let e = list_remove_at_count(list, index, 1);
   let only = list_single(e);
   return only;
 }
