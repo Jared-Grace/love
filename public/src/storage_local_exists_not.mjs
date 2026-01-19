1609c8de778f21b530ea7971900ad9b73835d89d
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:43:10 -0500

    de storage_local_exists_not

--- public/src/storage_local_exists_not.mjs
+++
@@ -1,7 +0,0 @@
-import { undefined } from "../../../love/public/src/undefined.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_exists_not(app_fn, key) {
-  marker("1");
-  let v = undefined(app_fn, key);
-  return v;
-}
