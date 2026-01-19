0bd720a3623c928685f31f6b4d214e2f77c960bb
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:18:38 -0500

    function_auto_path ..\love\public\src\app_a_if_statement.mjs

--- web/public/src/app_a_if_statement.mjs
+++ web/public/src/app_a_if_statement.mjs
@@ -28,22 +28,23 @@ export function app_a_if_statement(a, node, parent) {
         text: "Add above",
         fn: async function lambda2() {
           overlay_close();
-          function lambda4() {}
-          await app_a_functions_generic(context, lambda4);
-          let ast = object_property_get(a, "ast");
-          let v_match = js_visit_match(ast, node);
-          let stack = object_property_get(v_match, "stack");
-          function lambda3(item) {
-            let index_end = list_index_of_end(stack, item);
-            let i = js_stack_list_block_is(stack, index_end);
-            return i;
+          function lambda4() {
+            let ast = object_property_get(a, "ast");
+            let v_match = js_visit_match(ast, node);
+            let stack = object_property_get(v_match, "stack");
+            function lambda3(item) {
+              let index_end = list_index_of_end(stack, item);
+              let i = js_stack_list_block_is(stack, index_end);
+              return i;
+            }
+            let list = list_filter_last(stack, lambda3);
+            let statement = list_next(stack, list);
+            let index = list_index_of(list, statement);
+            log({
+              statement,
+            });
           }
-          let list = list_filter_last(stack, lambda3);
-          let statement = list_next(stack, list);
-          let index = list_index_of(list, statement);
-          log({
-            statement,
-          });
+          await app_a_functions_generic(context, lambda4);
         },
       },
     ];
