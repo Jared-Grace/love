63b2d182611c34769a333e71d628790ff38a1a22
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:47:11 -0500

    function_auto_path ..\love\public\src\storage_local_exists_not_context.mjs

--- public/src/storage_local_exists_not_context.mjs
+++ public/src/storage_local_exists_not_context.mjs
@@ -2,5 +2,6 @@ import { storage_local_exists_not } from "../../../love/public/src/storage_local
 import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_exists_not_context(app_fn, key) {
   marker("1");
-  return storage_local_exists_not(app_fn, key);
+  let exists = storage_local_exists_not(app_fn, key);
+  return exists;
 }
