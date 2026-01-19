7c13719143b18f1705be23fde6ca4d79dca328ec
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:13:04 -0500

    function_auto_path ..\love\public\src\app_a_function_on_change.mjs

--- public/src/app_a_function_on_change.mjs
+++ public/src/app_a_function_on_change.mjs
@@ -19,5 +19,5 @@ export async function app_a_function_on_change(o, a) {
   let r = await app_a_function(context);
   let a2 = object_property_get(r, "a");
   let content2 = object_property_get(a2, "content");
-  html_scroll_top_set(content, scroll_top);
+  html_scroll_top_set(content2, scroll_top);
 }
