b3404abc0f6ef9c067cd9a632704e3d7530c1d2f
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:51:29 -0500

    function_auto_path ..\love\public\src\html_input_integer.mjs

--- public/src/html_input_integer.mjs
+++ public/src/html_input_integer.mjs
@@ -3,4 +3,5 @@ import { html_input_number } from "../../../love/public/src/html_input_number.mj
 export function html_input_integer(overlay) {
   let input = html_input_number(overlay);
   html_attribute_set(input, "step", "1");
+  return input;
 }
