953f9ce33ec99d4e5af641eabe0e8392efe7ab20
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 11:35:35 -0500

    function_auto_path ..\love\public\src\app_a_overlay_choices.mjs

---
+++ public/src/app_a_overlay_choices.mjs
@@ -0,0 +1,21 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
+import { app_a_choice_close } from "../../../love/public/src/app_a_choice_close.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
+import { app_a_on_keydown } from "../../../love/public/src/app_a_on_keydown.mjs";
+import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
+export function app_a_overlay_choices(a, component, lambda$o) {
+  marker("1");
+  async function on_click() {
+    let o = app_a_overlay(a, on_keydown);
+    let choices = await lambda$o(o);
+    let overlay_close = object_property_get(o, "overlay_close");
+    let choice_x = app_a_choice_close(overlay_close);
+    list_add_first(choices, choice_x);
+    function on_keydown(e) {
+      app_a_on_keydown(e, choices);
+    }
+  }
+  html_on_click(component, on_click);
+}
