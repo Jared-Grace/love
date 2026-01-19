1525c3379eb29976183358b63c1ba2587aeb526b
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:44:53 -0500

    function_auto_path ..\love\public\src\object_copy_assign.mjs

--- web/public/src/object_copy_assign.mjs
+++ web/public/src/object_copy_assign.mjs
@@ -1,7 +1,7 @@
 import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { object_copy } from "../../../love/public/src/object_copy.mjs";
-export function object_copy_assign(a, from) {
-  let a2 = object_copy(a);
-  object_assign(a2, from);
-  return a2;
+export function object_copy_assign(to_before, from) {
+  let to = object_copy(to_before);
+  object_assign(to, from);
+  return to;
 }
