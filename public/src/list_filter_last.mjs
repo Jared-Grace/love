677b73fde0686210228ffc6febc77b7ff67ff277
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 13:50:04 -0500

    function_auto_path ..\love\public\src\list_filter_last.mjs

--- public/src/list_filter_last.mjs
+++ public/src/list_filter_last.mjs
@@ -3,9 +3,5 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_last(stack, lambda3) {
   let list = list_filter(stack, lambda3);
   let last = list_last(list);
-  let v = {
-    list,
-    last,
-  };
-  return v;
+  return last;
 }
