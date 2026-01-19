f21da4eb9774592097cb421a3845d798dfdbe167
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:47:57 -0500

    de object_copy_right_assign

--- public/src/object_copy_right_assign.mjs
+++
@@ -1,7 +0,0 @@
-import { object_assign } from "../../../love/public/src/object_assign.mjs";
-import { object_copy } from "../../../love/public/src/object_copy.mjs";
-export function object_copy_right_assign(to, from_before) {
-  let from = object_copy(from_before);
-  object_assign(to, from);
-  return to;
-}
