ffad6184e165cc4752853c60e22722c35bfa4dbe
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 11:32:41 -0500

    function_auto_path ..\love\public\src\app_a_identifier_generic.mjs

--- public/src/app_a_identifier_generic.mjs
+++ public/src/app_a_identifier_generic.mjs
@@ -40,6 +40,7 @@ export function app_a_identifier_generic(a, span, name, c, lines_multiple) {
         let lambda22 = html_on_enter_lambda(lambda23);
         let o2 = app_a_overlay(a, lambda22);
         app_a_overlay_close_button(o2);
+        let overlay = object_property_get(o2, "overlay");
         let oc = app_a_overlay_container_centered(overlay);
         let text2 = object_property_get(change, "text");
         let div2 = html_div_text(oc, text2 + " from:");
