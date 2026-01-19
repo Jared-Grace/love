6c8a3a6e648fe2064bcc582b5e4852104d65970b
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 17:36:51 -0500

    function_auto_path ..\love\public\src\html_button.mjs

--- public/src/html_button.mjs
+++ public/src/html_button.mjs
@@ -1,7 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { html_button_notext } from "../../../love/public/src/html_button_notext.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 export function html_button(parent, text, lambda) {
+  marker("1");
   assert_arguments(arguments, 3);
   let component = html_button_notext(parent, lambda);
   html_text_set(component, text);
