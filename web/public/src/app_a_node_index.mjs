8d3ab455ae808fa5b8ad75380967d965c88bb360
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:53:16 -0500

    fs app_a_node_index

--- web/public/src/app_a_node_index.mjs
+++ web/public/src/app_a_node_index.mjs
@@ -1,3 +1,4 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { list_next } from "../../../love/public/src/list_next.mjs";
 import { list_filter_last } from "../../../love/public/src/list_filter_last.mjs";
@@ -6,6 +7,7 @@ import { list_index_of_end } from "../../../love/public/src/list_index_of_end.mj
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_node_index(a, node) {
+  marker("1");
   let ast = object_property_get(a, "ast");
   let node2 = object_property_get(a, "node");
   let v_match = js_visit_match(ast, node);
