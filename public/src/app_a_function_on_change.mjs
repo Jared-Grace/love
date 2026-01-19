f40a1f94a34e8ed477d3189b7b5680a51b600af0
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:50:15 -0500

    pd context

--- public/src/app_a_function_on_change.mjs
+++ public/src/app_a_function_on_change.mjs
@@ -2,7 +2,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_a_function_on_keydown_remove } from "../../../love/public/src/app_a_function_on_keydown_remove.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export async function app_a_function_on_change(o2, a, context) {
+export async function app_a_function_on_change(o2, a) {
   marker("1");
   let rename_overlay_close = object_property_get(o2, "overlay_close");
   rename_overlay_close();
