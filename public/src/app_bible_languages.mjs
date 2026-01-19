675274081c39a9d202d1944de69adabbd823417e
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:40:24 -0500

    function_auto_path ..\love\public\src\app_bible_languages.mjs

--- public/src/app_bible_languages.mjs
+++ public/src/app_bible_languages.mjs
@@ -1,6 +1,10 @@
+import { app_reply_buttons_languages } from "../../../love/public/src/app_reply_buttons_languages.mjs";
+import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_bible_languages(context) {
   marker("1");
   let root = html_clear_context(context);
+  let languages = ebible_languages();
+  app_reply_buttons_languages(languages_chosen, root, languages);
 }
