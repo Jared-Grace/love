d739232ca5b697cdd66313a7ce2398bf56760f33
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:50:00 -0500

    function_auto_path ..\love\public\src\app_a_identifier_generic.mjs

---
+++ web/public/src/app_a_function_on_change.mjs
@@ -0,0 +1,9 @@
+import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
+import { app_a_function_on_keydown_remove } from "../../../love/public/src/app_a_function_on_keydown_remove.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export async function app_a_function_on_change(o2, a, context) {
+  let rename_overlay_close = object_property_get(o2, "overlay_close");
+  rename_overlay_close();
+  app_a_function_on_keydown_remove(a);
+  await app_a_function(context);
+}
