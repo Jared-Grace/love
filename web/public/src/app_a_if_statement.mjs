0e431cfbe5e1b0a4b8a8b35e3356b72caf35a7de
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 14:38:50 -0500

    function_auto_path ..\love\public\src\app_a_if_statement.mjs

--- web/public/src/app_a_if_statement.mjs
+++ web/public/src/app_a_if_statement.mjs
@@ -32,7 +32,7 @@ export function app_a_if_statement(a, node, parent) {
         text: "Add above",
         fn: async function lambda2() {
           overlay_close();
-          function lambda4() {}
+          function lambda4(e) {}
           let v = app_a_overlay(a, lambda4);
           async function on_select(f_name_call) {
             let ast = object_property_get(a, "ast");
