f05736e6a3383fea990e8dcc30b568429ae16f0b
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:11:59 -0500

    function_auto_path ..\love\public\src\app_a_function_on_change.mjs

--- public/src/html_scroll_top_set.mjs
+++ public/src/html_scroll_top_set.mjs
@@ -1,8 +1,8 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_scroll_top_set(content, scrollTop) {
+export function html_scroll_top_set(component, scrollTop) {
   marker("1");
-  let element2 = html_component_element_get(content);
+  let element2 = html_component_element_get(component);
   object_property_set(element2, "scrollTop", scrollTop);
 }
