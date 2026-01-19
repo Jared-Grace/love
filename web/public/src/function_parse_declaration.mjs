1713e094913b8644bd6070573474cc5bd4477970
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:44:22 -0500

    function_auto_path ..\love\public\src\function_parse_declaration.mjs

--- web/public/src/function_parse_declaration.mjs
+++ web/public/src/function_parse_declaration.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_parse_declaration_generic } from "../../../love/public/src/function_parse_declaration_generic.mjs";
 import { function_parse } from "../../../love/public/src/function_parse.mjs";
 export async function function_parse_declaration(f_name) {
+  marker("1");
   let fn_parse = function_parse;
   let parsed = await function_parse_declaration_generic(fn_parse, f_name);
   return parsed;
