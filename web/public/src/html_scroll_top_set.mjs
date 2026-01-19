e9396ced4246e8008a72cdc44a348d97e8e447ab
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:11:55 -0500

    pd element

--- web/public/src/html_scroll_top_set.mjs
+++ web/public/src/html_scroll_top_set.mjs
@@ -1,7 +1,7 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_scroll_top_set(content, element, scrollTop) {
+export function html_scroll_top_set(content, scrollTop) {
   marker("1");
   let element2 = html_component_element_get(content);
   object_property_set(element2, "scrollTop", scrollTop);
