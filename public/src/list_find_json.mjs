a535e96da38fcc3a661957d6f051e8e7a6af80a9
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:51:05 -0500

    rs equal_by_json lambda

--- public/src/list_find_json.mjs
+++ public/src/list_find_json.mjs
@@ -1,9 +1,9 @@
-import { equal_by_json } from "../../../love/public/src/equal_by_json.mjs";
+import { equal_by_json_lambda } from "../../../love/public/src/equal_by_json_lambda.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 export function list_find_json(list, expected) {
   marker("1");
-  let l = equal_by_json(expected);
+  let l = equal_by_json_lambda(expected);
   let found = list_find(list, l);
   return found;
 }
