2e740fb9c095657cfbecbb78c4c559d84c16ce93
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 15:11:10 -0500

    function_auto_path ..\love\public\src\app_a_function_on_change.mjs

--- web/public/src/app_a_function_on_change.mjs
+++ web/public/src/app_a_function_on_change.mjs
@@ -1,4 +1,4 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { html_scroll_top_set } from "../../../love/public/src/html_scroll_top_set.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -14,9 +14,10 @@ export async function app_a_function_on_change(o, a) {
   overlay_close();
   app_a_function_on_keydown_remove(a);
   let content = object_property_get(a, "content");
-  let element = html_component_element_get(component);
+  let element = html_component_element_get(content);
   let scrollTop = object_property_get(element, "scrollTop");
   let r = await app_a_function(context);
   let a2 = object_property_get(r, "a");
-  object_property_set(element, "scrollTop", scrollTop);
+  let content2 = object_property_get(a2, "content");
+  html_scroll_top_set(content, element, scrollTop);
 }
