4816b60b4604f4f2bb78fa19c59e655ae08a9cbc
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 12:00:49 -0500

    function_auto_path ..\love\public\src\app_a_if_statement.mjs

--- public/src/app_a_if_statement.mjs
+++ public/src/app_a_if_statement.mjs
@@ -22,8 +22,8 @@ export function app_a_if_statement(a, node, parent) {
         text: "Add above",
         fn: function lambda2() {
           let ast = object_property_get(a, "ast");
-          let v_match = js_visit_match(ast2, search);
-          let stack = object_property_get(a, "stack");
+          let v_match = js_visit_match(ast, node);
+          let stack = object_property_get(v_match, "stack");
           log({
             stack,
           });
