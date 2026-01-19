b7cef768c8d11afbe348da6e80a05cd87412a4f3
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:51:29 -0500

    function_auto_path ..\love\public\src\list_filter_json.mjs

--- public/src/list_filter_json.mjs
+++ public/src/list_filter_json.mjs
@@ -1,9 +1,9 @@
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { equal_by_json_lambda } from "../../../love/public/src/equal_by_json_lambda.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { list_find } from "../../../love/public/src/list_find.mjs";
 export function list_filter_json(list, expected) {
   marker("1");
   let l = equal_by_json_lambda(expected);
-  let found = list_find(list, l);
+  let found = list_filter(list, l);
   return found;
 }
