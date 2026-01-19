83ecbe268148fcd51625a161304bc24956f39f94
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:47:08 -0500

    fws storage_local_exists_not context

--- public/src/storage_local_exists_not_context.mjs
+++ public/src/storage_local_exists_not_context.mjs
@@ -1,4 +1,6 @@
+import { storage_local_exists_not } from "../../../love/public/src/storage_local_exists_not.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_exists_not_context() {
+export function storage_local_exists_not_context(app_fn, key) {
   marker("1");
+  return storage_local_exists_not(app_fn, key);
 }
