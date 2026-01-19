539cc72945301346fde53d2032e5208147e79487
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:45:15 -0500

    rrl function_parse_declaration_unalised unaliased

--- public/src/function_parse_declaration_unalised.mjs
+++
@@ -1,7 +0,0 @@
-import { function_parse_declaration_generic } from "../../../love/public/src/function_parse_declaration_generic.mjs";
-import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
-export async function function_parse_declaration_unalised(f_name) {
-  let fn_parse = function_parse_unaliased;
-  let parsed = await function_parse_declaration_generic(fn_parse, f_name);
-  return parsed;
-}
