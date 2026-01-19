4fc42be85dce8f8eddb4dc7e77847bb93b34bdde
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:36:15 -0500

    msa languages

--- public/src/app_bible_languages.mjs
+++ public/src/app_bible_languages.mjs
@@ -1,4 +1,5 @@
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_bible_languages() {
+export function app_bible_languages(context) {
   marker("1");
+  let root = html_clear_context(context);
 }
