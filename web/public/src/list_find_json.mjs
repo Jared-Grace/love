0960f30e8b779ca2bad34b0c034e101c6b01105f
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 18:49:53 -0500

    function_auto_path ..\love\public\src\list_find_json.mjs

--- web/public/src/list_find_json.mjs
+++ web/public/src/list_find_json.mjs
@@ -3,7 +3,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 export function list_find_json(list, expected) {
   marker("1");
-  let lambda$item = equal_by_json(expected);
-  let found = list_find(list, lambda$item);
+  let l = equal_by_json(expected);
+  let found = list_find(list, l);
   return found;
 }
