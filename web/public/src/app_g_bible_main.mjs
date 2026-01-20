7baf7c126ff83a504218f8f67a07c4ce075c2ddd
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:48:52 -0500

    function_auto_path ..\love\public\src\app_g_bible_main.mjs

--- web/public/src/app_g_bible_main.mjs
+++ web/public/src/app_g_bible_main.mjs
@@ -1,14 +1,16 @@
+import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
+import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { firebase_login } from "../../../love/public/src/firebase_login.mjs";
 import { app_g_bible_screens } from "../../../love/public/src/app_g_bible_screens.mjs";
 import { app_g_bible } from "../../../love/public/src/app_g_bible.mjs";
-import { app_bible_main_generic } from "../../../love/public/src/app_bible_main_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_g_bible_main(context) {
   marker("1");
   function lambda() {
     let app_fn = app_g_bible;
     let screens = app_g_bible_screens();
-    app_bible_main_generic(context, app_fn, screens);
+    app_bible_main_generic_before(context, app_fn, screens);
+    app_generic_refresh(context);
   }
   await firebase_login(lambda);
 }
