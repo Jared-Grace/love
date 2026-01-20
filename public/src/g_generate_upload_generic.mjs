dbc523f0607aa5cf899606a0d6516174f1bb64ad
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:03:31 -0500

    function_auto_path ..\love\public\src\g_generate_upload_generic.mjs

--- public/src/g_generate_upload_generic.mjs
+++ public/src/g_generate_upload_generic.mjs
@@ -1,6 +1,4 @@
 import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { folder_user_storage_function_each } from "../../../love/public/src/folder_user_storage_function_each.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
@@ -9,11 +7,6 @@ import { path_base } from "../../../love/public/src/path_base.mjs";
 export async function g_generate_upload_generic(path_get, fn) {
   await folder_user_storage_function_each(fn, file_each);
   async function file_each(file) {
-    let fragment = "JAS01";
-    let i = string_includes(file, fragment);
-    if (not(i)) {
-      return;
-    }
     let fb = path_base(file);
     let chapter_code = path_name(fb);
     let destination = path_get(chapter_code);
