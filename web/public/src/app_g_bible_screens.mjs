cbad31ebfac0211bceee6ddbc5c31136ed2f6c87
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 15:34:18 -0500

    function_auto_path ..\love\public\src\app_g_bible_screens.mjs

--- web/public/src/app_g_bible_screens.mjs
+++ web/public/src/app_g_bible_screens.mjs
@@ -1,12 +1,12 @@
+import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs";
 import { app_bible_languages } from "../../../love/public/src/app_bible_languages.mjs";
 import { app_bible_chapters } from "../../../love/public/src/app_bible_chapters.mjs";
 import { app_bible_books } from "../../../love/public/src/app_bible_books.mjs";
-import { app_bible_home } from "../../../love/public/src/app_bible_home.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_g_bible_screens() {
   marker("screens");
   let screens = {
-    home: app_bible_home,
+    home: app_g_bible_home,
     books: app_bible_books,
     chapters: app_bible_chapters,
     languages: app_bible_languages,
