665508ef56d89685888b0c8e53a54cad91dd526e
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:10:15 -0500

    pb

--- web/public/src/html_display_none_or_inline.mjs
+++ web/public/src/html_display_none_or_inline.mjs
@@ -1,7 +1,7 @@
 import { html_display_inline } from "../../../love/public/src/html_display_inline.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_display_none } from "../../../love/public/src/html_display_none.mjs";
-export function html_display_none_or_inline(hidden, item) {
+export function html_display_none_or_inline(item, hidden) {
   marker("1");
   if (hidden) {
     html_display_none(item);
