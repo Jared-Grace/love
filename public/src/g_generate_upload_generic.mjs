6b24605e7ef65f998877df5062961974249f7967
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:00:52 -0500

    function_auto_path ..\love\public\src\g_generate_upload_generic.mjs

--- public/src/g_generate_upload_generic.mjs
+++ public/src/g_generate_upload_generic.mjs
@@ -1,7 +1,7 @@
+import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { folder_user_storage_function_each } from "../../../love/public/src/folder_user_storage_function_each.mjs";
-import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
 import { path_name } from "../../../love/public/src/path_name.mjs";
@@ -19,6 +19,6 @@ export async function g_generate_upload_generic(path_get, fn) {
     let destination = path_get(chapter_code);
     let path = local_function_path_json(chapter_code, fn);
     let data = await file_read_json(path);
-    await firebase_upload_object(destination, data);
+    await firebase_upload_object_compressed(destination, data);
   }
 }
