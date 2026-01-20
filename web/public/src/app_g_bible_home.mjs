c21a87466b90f3023934017a55238f80cfa47829
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 16:58:55 -0500

    function_auto_path ..\love\public\src\app_g_bible_home.mjs

--- web/public/src/app_g_bible_home.mjs
+++ web/public/src/app_g_bible_home.mjs
@@ -26,7 +26,7 @@ export async function app_g_bible_home(context) {
       if (equal(s, verse_number)) {
         let text = object_property_get(passage, "text");
         let o = json_to(passage);
-        let p2 = html_p_text(p, text);
+        let p2 = html_p_text(p, o);
       }
     }
     each(passages, lambda2);
