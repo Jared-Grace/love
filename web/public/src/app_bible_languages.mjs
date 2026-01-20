05f4ea7c5d8ff6c01b82a79c66a6726d63226349
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 20:04:44 -0500

    function_auto_path ..\love\public\src\app_bible_languages.mjs

--- web/public/src/app_bible_languages.mjs
+++ web/public/src/app_bible_languages.mjs
@@ -1,3 +1,4 @@
+import { ebible_language_en } from "../../../love/public/src/ebible_language_en.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { storage_local_exists_not_context } from "../../../love/public/src/storage_local_exists_not_context.mjs";
 import { html_scroll_top_set } from "../../../love/public/src/html_scroll_top_set.mjs";
@@ -12,6 +13,8 @@ export function app_bible_languages(context) {
   let key = "languages_chosen";
   let n = storage_local_exists_not_context(context, key);
   if (n) {
+    let en_l = ebible_language_en();
+    let languages_chosen_default = [en_l];
     storage_local_set_context(context, key);
     html_scroll_top_set(content, s);
   }
