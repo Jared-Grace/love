0309964130e91a152c9c7b590123570898002538
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:32:34 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -1,25 +1,22 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { firebase_app_initialize } from "../../../love/public/src/firebase_app_initialize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_login(on_logged_in, on_logged_out) {
   const app = await firebase_app_initialize();
-  const firebaseAuth = await import(
+  const firebase_auth = await import(
     "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js"
   );
-  const auth = firebaseAuth.getAuth(app);
+  const auth = firebase_auth.getAuth(app);
   function lambda(user) {
     if (user) {
-      loginDiv.style.display = "none";
-      appDiv.style.display = "block";
-      console.log("User logged in:", user.uid);
+      on_logged_in({
+        user,
+      });
     } else {
-      loginDiv.style.display = "block";
-      appDiv.style.display = "none";
-      console.log("User not logged in");
+      on_logged_out({});
     }
   }
-  firebaseAuth.onAuthStateChanged(auth, lambda);
-  const userCredential = await firebaseAuth.signInWithEmailAndPassword(
+  firebase_auth.onAuthStateChanged(auth, lambda);
+  const userCredential = await firebase_auth.signInWithEmailAndPassword(
     auth,
     username,
     password,
