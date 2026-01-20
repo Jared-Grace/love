ead83c0d0b3d544eb5c82b932291d7d66ace112d
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:43:39 -0500

    function_auto_path ..\love\public\src\app_bible_main_generic.mjs

---
+++ public/src/app_bible_main_generic_before.mjs
@@ -0,0 +1,13 @@
+import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
+import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
+import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+export function app_bible_main_generic_before(context, app_fn, screens) {
+  object_merge(context, {
+    app_fn,
+    screens,
+  });
+  firebase_name_jg();
+  let root = html_mobile_default(context);
+  html_margin_0(root);
+}
