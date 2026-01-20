314b880be30c171b60d2ec10830910dd93e34199
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 20:04:11 -0500

    function_auto_path ..\love\public\src\app_bible_languages.mjs

--- public/src/app_bible_languages.mjs
+++ public/src/app_bible_languages.mjs
@@ -1,6 +1,6 @@
+import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { storage_local_exists_not_context } from "../../../love/public/src/storage_local_exists_not_context.mjs";
 import { html_scroll_top_set } from "../../../love/public/src/html_scroll_top_set.mjs";
-import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { app_reply_buttons_languages } from "../../../love/public/src/app_reply_buttons_languages.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
@@ -12,7 +12,7 @@ export function app_bible_languages(context) {
   let key = "languages_chosen";
   let n = storage_local_exists_not_context(context, key);
   if (n) {
-    let s = storage_local_get_context(context, key);
+    storage_local_set_context(context, key);
     html_scroll_top_set(content, s);
   }
   app_reply_buttons_languages(languages_chosen, root, languages);
