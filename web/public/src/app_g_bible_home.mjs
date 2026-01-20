3dfc87d63c32f1d94505cf820d793861b57ef5ef
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:06:38 -0500

    function_auto_path ..\love\public\src\app_g_bible_home.mjs

--- web/public/src/app_g_bible_home.mjs
+++ web/public/src/app_g_bible_home.mjs
@@ -1,7 +1,6 @@
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
 import { html_value_set } from "../../../love/public/src/html_value_set.mjs";
 import { html_textarea } from "../../../love/public/src/html_textarea.mjs";
-import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 import { app_g_openai_split } from "../../../love/public/src/app_g_openai_split.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { string_to } from "../../../love/public/src/string_to.mjs";
@@ -9,7 +8,6 @@ import { list_max } from "../../../love/public/src/list_max.mjs";
 import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { g_sermon_generate_download } from "../../../love/public/src/g_sermon_generate_download.mjs";
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -30,11 +28,9 @@ export async function app_g_bible_home(context) {
       if (equal(s, verse_number)) {
         let sermon = object_property_get(passage, "sermon");
         let mapped2 = app_g_openai_split(sermon);
-        let joined = list_join_newline(list);
+        let joined = list_join_newline(mapped2);
         let component = html_textarea(div);
-        html_value_set(component, value2);
-        let o = json_to(passage);
-        let p2 = html_p_text_multiple(p, mapped2);
+        html_value_set(component, joined);
       }
     }
     each(passages, lambda2);
