6daac2c080a07e5435e7bd29b2516f8ca6ace6c7
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:20:16 -0500

    function_auto_path ..\love\public\src\string_size_less_1.mjs

--- web/public/src/string_size_less_1.mjs
+++ web/public/src/string_size_less_1.mjs
@@ -2,7 +2,7 @@ import { less_than } from "../../../love/public/src/less_than.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_size_less_1(s) {
   let sz = string_size(s);
-  let v2 = less_than(ma, mb);
+  let l = less_than(ma, mb);
   const sz1 = sz - 1;
   return sz1;
 }
