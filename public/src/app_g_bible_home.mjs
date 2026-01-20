da7619d78832a5245d3f03519889ebdcf5626fa4
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 15:51:06 -0500

    function_auto_path ..\love\public\src\app_g_bible_home.mjs

--- public/src/app_g_bible_home.mjs
+++ public/src/app_g_bible_home.mjs
@@ -13,7 +13,8 @@ export async function app_g_bible_home(context) {
     let value = await g_sermon_generate_download(chapter_code);
     let o = json_to(value);
     let passages = object_property_get(o, "passages");
-    each(list, function lambda2(item) {});
+    function lambda2(item) {}
+    each(list, lambda2);
     let p2 = html_p_text(p, o);
   }
   await app_bible_home_generic(context, lambda);
