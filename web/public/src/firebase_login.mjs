ca50b132bc85f71b879451d00d31d5df410024c0
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:32:04 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -1,7 +1,7 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { firebase_app_initialize } from "../../../love/public/src/firebase_app_initialize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export async function firebase_login() {
+export async function firebase_login(on_logged_in, on_logged_out) {
   const app = await firebase_app_initialize();
   const firebaseAuth = await import(
     "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js"
