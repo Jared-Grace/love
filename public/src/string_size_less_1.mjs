85d0be382410653d25d76ea79a5c7230d9e95fbf
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:13:20 -0500

    function_auto_path ..\love\public\src\string_size_less_1.mjs

--- public/src/string_size_less_1.mjs
+++ public/src/string_size_less_1.mjs
@@ -1,6 +1,8 @@
 import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_size_less_1(s) {
   let sz = string_size(s);
+  if (false) {
+  }
   const sz1 = sz - 1;
   return sz1;
 }
