71cf23fd3838fc337a13d2b16ee3df762d43bfe0
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 16:54:55 -0500

    function_auto_path ..\love\public\src\ebible_version_upload.mjs

--- public/src/ebible_version_upload.mjs
+++ public/src/ebible_version_upload.mjs
@@ -1,10 +1,8 @@
-import { ebible_version_books_upload } from "../../../love/public/src/ebible_version_books_upload.mjs";
-import { ebible_chapters_upload } from "../../../love/public/src/ebible_chapters_upload.mjs";
+import { ebible_version_upload_second } from "../../../love/public/src/ebible_version_upload_second.mjs";
 import { ebible_verses_upload } from "../../../love/public/src/ebible_verses_upload.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_upload(bible_folder) {
   marker("1");
   await ebible_verses_upload(bible_folder);
-  let books = await ebible_version_books_upload(bible_folder);
-  await ebible_chapters_upload(bible_folder);
+  await ebible_version_upload_second(bible_folder);
 }
