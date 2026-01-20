9a23ad2398a89866b810b570045a5928f3b998f2
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 11:37:22 -0500

    function_auto_path ..\love\public\src\equal_by_json_lambda.mjs

--- public/src/equal_by_json_lambda.mjs
+++ public/src/equal_by_json_lambda.mjs
@@ -3,7 +3,7 @@ import { json_to } from "../../../love/public/src/json_to.mjs";
 import { equal_by } from "../../../love/public/src/equal_by.mjs";
 export function equal_by_json_lambda(expected) {
   marker("1");
-  function lambda() {
+  function lambda(item) {
     let eq = equal_by(item, expected, json_to);
     return eq;
   }
