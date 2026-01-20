4c441745bae429b07c34600e9a8e03624dc95a10
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 15:26:33 -0500

    function_auto_path ..\love\public\src\app_g_bible_main.mjs

--- public/src/app_g_bible_main.mjs
+++ public/src/app_g_bible_main.mjs
@@ -1,7 +1,10 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_g_bible_main() {
   marker("1");
-  async function lambda(a) {}
+  async function lambda(a) {
+    let chapter_code = object_property_get(p, "chapter_code");
+  }
   await app_bible_home_generic(context, lambda);
 }
