684a933c588ec57734921bc95caaa2051b9f3c9b
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:23:12 -0500

    function_auto_path ..\love\public\src\string_size_less_1.mjs

--- web/public/src/string_size_less_1.mjs
+++ web/public/src/string_size_less_1.mjs
@@ -3,7 +3,9 @@ import { greater_than_or_equal } from "../../../love/public/src/greater_than_or_
 import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_size_less_1(s) {
   let sz = string_size(s);
-  let l = greater_than_or_equal(sz, 1);
+  const right = 1;
+  let left = sz;
+  let l = greater_than_or_equal(sz, right);
   function lambda() {
     let v = {
       left,
