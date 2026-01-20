711993e1b657c9ac055ba477448be9028848c1a0
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:31:23 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { firebase_app_initialize } from "../../../love/public/src/firebase_app_initialize.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function firebase_login() {
@@ -6,6 +7,18 @@ export async function firebase_login() {
     "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js"
   );
   const auth = firebaseAuth.getAuth(app);
+  function lambda(user) {
+    if (user) {
+      loginDiv.style.display = "none";
+      appDiv.style.display = "block";
+      console.log("User logged in:", user.uid);
+    } else {
+      loginDiv.style.display = "block";
+      appDiv.style.display = "none";
+      console.log("User not logged in");
+    }
+  }
+  firebaseAuth.onAuthStateChanged(auth, lambda);
   const userCredential = await firebaseAuth.signInWithEmailAndPassword(
     auth,
     username,
