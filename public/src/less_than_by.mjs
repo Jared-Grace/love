e741c0ecbf3ee7419390ec3f94d416532c798e08
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:18:16 -0500

    function_auto_path ..\love\public\src\less_than_by.mjs

--- public/src/less_than_by.mjs
+++ public/src/less_than_by.mjs
@@ -1,6 +1,7 @@
+import { less_than } from "../../../love/public/src/less_than.mjs";
 export function less_than_by(a, b, mapper) {
   const mb = mapper(b);
   const ma = mapper(a);
-  let v = ma < mb;
+  let v = less_than(ma, mb);
   return v;
 }
