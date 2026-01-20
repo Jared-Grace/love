6f4205acfaa398865f93736b228b4177bc069c73
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:22:20 -0500

    function_auto_path ..\love\public\src\string_size_less_1.mjs

--- public/src/string_size_less_1.mjs
+++ public/src/string_size_less_1.mjs
@@ -4,7 +4,7 @@ import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_size_less_1(s) {
   let sz = string_size(s);
   let l = greater_than_or_equal(sz, 1);
-  assert_json_get(l);
+  assert_json_get(l, () => {});
   const sz1 = sz - 1;
   return sz1;
 }
