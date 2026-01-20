9ad94f8a5ffad0fabf8ed385237460fe2dc150e4
Grace-Jared <christrosetolife@gmail.com>
2026-01-19 20:10:03 -0500

    function_auto_path ..\love\public\src\app_reply_buttons_languages.mjs

---
+++ public/src/app_reply_buttons_languages_on_toggle.mjs
@@ -0,0 +1,34 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { html_button } from "../../../love/public/src/html_button.mjs";
+import { html_style_background_color_set_or_remove_list } from "../../../love/public/src/html_style_background_color_set_or_remove_list.mjs";
+import { list_toggle } from "../../../love/public/src/list_toggle.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export function app_reply_buttons_languages_on_toggle(
+  languages_chosen,
+  on_toggle,
+  root,
+  languages,
+) {
+  function lambda(language) {
+    let name = object_property_get(language, "name");
+    marker("1");
+    let component = null;
+    function on_click() {
+      list_toggle(languages_chosen, language);
+      html_style_background_color_set_or_remove_list(
+        component,
+        languages_chosen,
+        language,
+      );
+      on_toggle();
+    }
+    component = html_button(root, name, on_click);
+    html_style_background_color_set_or_remove_list(
+      component,
+      languages_chosen,
+      language,
+    );
+  }
+  each(languages, lambda);
+}
