1f921ec1fab436df65b06446bc00b80177eba73a
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 18:16:46 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -17,8 +17,8 @@ export async function firebase_login(context, on_logged_in) {
       });
     } else {
       let root = object_property_get(context, "root");
-      let input_username = html_input_email(parent);
-      let input_password = html_input_type(parent, input_type);
+      let input_username = html_input_email(root);
+      let input_password = html_input_type(root, input_type);
       async function login() {
         let username = html_value_get(input_username);
         let password = html_value_get(input_password);
