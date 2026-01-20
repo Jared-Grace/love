7f05bdf94707e462c61fc021843082b022c0613e
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 17:10:54 -0500

    function_auto_path ..\love\public\src\html_mobile_default.mjs

--- web/public/src/html_mobile_default.mjs
+++ web/public/src/html_mobile_default.mjs
@@ -1,10 +1,10 @@
+import { html_mobile_default_font_size } from "../../../love/public/src/html_mobile_default_font_size.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { html_font_sans_serif_set_html } from "../../../love/public/src/html_font_sans_serif_set_html.mjs";
 import { html_meta_viewport } from "../../../love/public/src/html_meta_viewport.mjs";
-import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
 export function html_mobile_default(context) {
   let root = object_property_get(context, "root");
-  html_style_font_size(root, "25px");
+  html_mobile_default_font_size(root);
   html_meta_viewport();
   html_font_sans_serif_set_html();
   return root;
