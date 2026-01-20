e2a235d67351e101142d94fd5b046ebf211d88e7
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:27:39 -0500

    function_auto_path ..\love\public\src\app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -147,7 +147,7 @@ export async function app_reply_main(context) {
   function lambda6(event) {
     let key = object_property_get(event, "key");
     if (equal(key, "Backspace")) {
-      let ne = string_empty_not_is(name);
+      let ne = string_empty_not_is(name);$i
       typed = string_take_less_1(typed);
     } else {
       if (visible_count > 0) {
