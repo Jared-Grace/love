ac6c610616be964a43c05f8f71b051cd4f25bef1
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:54:53 -0500

    function_auto_path ..\love\public\src\app_new_update_generic.mjs

--- public/src/app_new_update_generic.mjs
+++ public/src/app_new_update_generic.mjs
@@ -1,3 +1,4 @@
+import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -26,7 +27,7 @@ export async function app_new_update_generic(
   let default2 = await firebase_name_repo(repo_name);
   let f_name = js_code_string(default2);
   let call_name = fn_call.name;
-  let v2 = await function_parse_declaration_unalised(call_name);
+  let v2 = await function_parse_declaration_unaliased(call_name);
   let unaliased = object_property_get(v2, "unaliased");
   let declaration_call = object_property_get(v2, "declaration");
   async function lambda(ast) {
