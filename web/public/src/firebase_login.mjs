66afb02400ee47f01c236c7506ea1a584b000756
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 18:58:54 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -1,11 +1,4 @@
-import { html_placeholder } from "../../../love/public/src/html_placeholder.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
-import { html_input_password } from "../../../love/public/src/html_input_password.mjs";
-import { html_value_get } from "../../../love/public/src/html_value_get.mjs";
-import { html_input_email } from "../../../love/public/src/html_input_email.mjs";
-import { html_button_width_full } from "../../../love/public/src/html_button_width_full.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { firebase_login_email } from "../../../love/public/src/firebase_login_email.mjs";
 import { firebase_app_initialize } from "../../../love/public/src/firebase_app_initialize.mjs";
 export async function firebase_login(context, on_logged_in) {
   const app = await firebase_app_initialize();
@@ -19,22 +12,7 @@ export async function firebase_login(context, on_logged_in) {
         user,
       });
     } else {
-      let root = object_property_get(context, "root");
-      let input_username = html_input_email(root);
-      let input_password = html_input_password(root);
-      html_placeholder(input_password, "Password");
-      each([input_username, input_password], html_width_full);
-      async function login() {
-        let username = html_value_get(input_username);
-        let password = html_value_get(input_password);
-        let v = await firebase_auth.signInWithEmailAndPassword(
-          auth,
-          username,
-          password,
-        );
-        return v;
-      }
-      let component = html_button_width_full(root, "Login", login);
+      firebase_login_email(context, firebase_auth, auth);
     }
   }
   firebase_auth.onAuthStateChanged(auth, lambda);
