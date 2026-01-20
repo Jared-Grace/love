a3fc692a8e53933e75d3e5e76a3b3f1e1d3d00eb
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 14:23:31 -0500

    function_auto_path ..\love\public\src\string_size_less_1.mjs

--- public/src/string_size_less_1.mjs
+++ public/src/string_size_less_1.mjs
@@ -1,19 +1,10 @@
-import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
-import { greater_than_or_equal } from "../../../love/public/src/greater_than_or_equal.mjs";
+import { greater_than_or_equal_assert } from "../../../love/public/src/greater_than_or_equal_assert.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_size_less_1(s) {
   let sz = string_size(s);
   const right = 1;
   let left = sz;
-  let l = greater_than_or_equal(left, right);
-  function lambda() {
-    let r = {
-      left,
-      right,
-    };
-    return r;
-  }
-  assert_json_get(l, lambda);
+  greater_than_or_equal_assert(left, right);
   const sz1 = sz - 1;
   return sz1;
 }
