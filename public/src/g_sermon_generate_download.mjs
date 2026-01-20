d09b4bef8bba1de55e75bf9f1108ba0a36c7ed25
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 15:47:14 -0500

    function_auto_path ..\love\public\src\app_g_bible_home.mjs

---
+++ public/src/g_sermon_generate_download.mjs
@@ -0,0 +1,16 @@
+import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
+import { firebase_storage_download_json } from "../../../love/public/src/firebase_storage_download_json.mjs";
+import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
+export async function g_sermon_generate_download(chapter_code) {
+  async function get() {
+    let destination = g_sermon_generate_upload_path(chapter_code);
+    let o = await firebase_storage_download_json(destination);
+    return o;
+  }
+  let value = await global_function_property_initialize_async(
+    fn,
+    chapter_code,
+    get,
+  );
+  return value;
+}
