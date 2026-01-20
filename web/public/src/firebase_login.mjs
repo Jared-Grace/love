eb4c54ab41e00db4938fec374b9a4df256de82f8
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 18:18:30 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -1,3 +1,4 @@
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_input_password } from "../../../love/public/src/html_input_password.mjs";
 import { html_value_get } from "../../../love/public/src/html_value_get.mjs";
@@ -20,7 +21,9 @@ export async function firebase_login(context, on_logged_in) {
       let root = object_property_get(context, "root");
       let input_username = html_input_email(root);
       let input_password = html_input_password(root);
-      function lambda2(item) {hwf}
+      function lambda2(item) {
+        html_width_full(component2);
+      }
       let mapped = list_map(list, lambda2);
       async function login() {
         let username = html_value_get(input_username);
