68be1a0aa9ee8e2b71ad861557f1ca8a3c9a4533
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 15:46:18 -0500

    function_auto_path ..\love\public\src\object_property_initialize_lambda_async.mjs

--- web/public/src/object_property_initialize_lambda_async.mjs
+++ web/public/src/object_property_initialize_lambda_async.mjs
@@ -11,9 +11,10 @@ export async function object_property_initialize_lambda_async(
   marker("1");
   const exists = object_property_exists(object, property_name);
   if (not(exists)) {
-    let value_set = lambda();
+    let value_set = lambda;
     object_property_set(object, property_name, value_set);
   }
-  let value = await object_property_get(object, property_name);
+  const lamda_get = object_property_get(object, property_name);
+  let value = await lamda_get();
   return value;
 }
