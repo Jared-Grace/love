97c04b1154cf54fad36383cf64ee2c99c34a95b3
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 15:57:43 -0500

    function_auto_path ..\love\public\src\app_g_bible_home.mjs

--- public/src/app_g_bible_home.mjs
+++ public/src/app_g_bible_home.mjs
@@ -18,7 +18,7 @@ export async function app_g_bible_home(context) {
     function lambda2(passage) {
       let verse_numbers = object_property_get(passage, "verse_numbers");
       function lambda3(verse_number) {}
-      let mapped = list_map(verse_numbers, integer_to);
+      let mapped = list_map(verse_numbers, integer_to);list_max
     }
     each(passages, lambda2);
     let p2 = html_p_text(p, o);
