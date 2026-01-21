6af3d99dfe1c6d1fed556bcdb7d6b291bca78daa
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:21:48 -0500

    function_auto_path ..\love\public\src\app_g_bible_main.mjs

--- web/public/src/app_g_bible_main.mjs
+++ web/public/src/app_g_bible_main.mjs
@@ -1,5 +1,4 @@
 import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { firebase_login } from "../../../love/public/src/firebase_login.mjs";
