2e320464695d6b7bbc70ae64803d6c4913780fdc
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:21:40 -0500

    pn screen_home app_g_bible_home

--- web/public/src/app_bible_main_generic_before.mjs
+++ web/public/src/app_bible_main_generic_before.mjs
@@ -3,7 +3,12 @@ import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
 import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-export function app_bible_main_generic_before(context, app_fn, screens) {
+export function app_bible_main_generic_before(
+  context,
+  app_fn,
+  screens,
+  screen_home,
+) {
   marker("1");
   object_merge(context, {
     app_fn,
