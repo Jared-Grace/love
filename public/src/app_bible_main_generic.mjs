0a954044a200f4ba04f248b790826d995f8c6aab
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:21:45 -0500

    function_auto_path ..\love\public\src\app_bible_main_generic.mjs

--- public/src/app_bible_main_generic.mjs
+++ public/src/app_bible_main_generic.mjs
@@ -1,3 +1,4 @@
+import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs";
 import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 export function app_bible_main_generic(context, app_fn, screens) {
