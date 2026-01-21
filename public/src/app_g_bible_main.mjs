eeb3fff61ff4f7a16bebe0bfea134c6529341b94
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:21:01 -0500

    function_auto_path ..\love\public\src\app_g_bible_main.mjs

--- public/src/app_g_bible_main.mjs
+++ public/src/app_g_bible_main.mjs
@@ -1,3 +1,5 @@
+import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs";
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
 import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { firebase_login } from "../../../love/public/src/firebase_login.mjs";
@@ -7,6 +9,7 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export async function app_g_bible_main(context) {
   let app_fn = app_g_bible;
   let screens = app_g_bible_screens();
+  object_property_set(context, "screen_home", app_g_bible_home);
   app_bible_main_generic_before(context, app_fn, screens);
   marker("1");
   function lambda() {
