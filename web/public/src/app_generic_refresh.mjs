387b8e2da61067ff96dd4b5c3f9483126cf85ef8
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:18:28 -0500

    function_auto_path ..\love\public\src\app_generic_refresh.mjs

--- web/public/src/app_generic_refresh.mjs
+++ web/public/src/app_generic_refresh.mjs
@@ -1,10 +1,10 @@
-import { list_find } from "../../../love/public/src/list_find.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
+import { list_find_property } from "./list_find_property.mjs";
 export function app_generic_refresh(context) {
   let screens = object_property_get(context, "screens");
   let app_fn = object_property_get(context, "app_fn");
@@ -18,8 +18,6 @@ export function app_generic_refresh(context) {
     "screen",
     combined,
   );
-  function lambda(item) {}
-  let found = list_find(list, lambda);
-  let screen = object_property_get(screens, screen_name);
+  let screen = list_find_property(list, "name", screen_name);
   screen(context);
 }
