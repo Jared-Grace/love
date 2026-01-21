506e402cba050e46920dd1495b7c6d1578e184f7
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:08:46 -0500

    function_auto_path ..\love\public\src\app_generic_screen_set.mjs

--- public/src/app_generic_screen_set.mjs
+++ public/src/app_generic_screen_set.mjs
@@ -1,8 +1,9 @@
-import { app_generic_screen_name } from "../../../love/public/src/app_generic_screen_name.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { app_generic_refresh_screen } from "../../../love/public/src/app_generic_refresh_screen.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_generic_screen_set(context, fn) {
   marker("1");
-  let without = app_generic_screen_name(context, fn);
-  app_generic_refresh_screen(context, without);
+  let fn_name = object_property_get(fn, "name");
+  app_generic_refresh_screen(context, fn_name);
 }
