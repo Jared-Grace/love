8f94fc3e7a2a1b2f3e9ec43897a5cba9be1ae2cd
Grace-Jared <christrosetolife@gmail.com>
2026-01-21 16:17:45 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_upload.mjs

--- public/src/g_sermon_generate_upload.mjs
+++ public/src/g_sermon_generate_upload.mjs
@@ -3,8 +3,12 @@ import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon
 import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
+import { app_g_bible } from "./app_g_bible.mjs";
 export async function g_sermon_generate_upload() {
-  error(message);
+  error(
+    "this maybe should not be ran because it would overwrite edits from " +
+      app_g_bible,
+  );
   marker("1");
   let path_get = g_sermon_generate_upload_path;
   let fn = g_sermon_generate;
