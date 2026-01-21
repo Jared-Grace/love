4803ac123a2e43f96c44f520e5bf6cf11819e121
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 20:20:00 -0500

    function_auto_path ..\love\public\src\app_bible_chapter_open.mjs

--- public/src/app_bible_chapter_open.mjs
+++ public/src/app_bible_chapter_open.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_bible_home } from "../../../love/public/src/app_bible_home.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
@@ -5,5 +6,6 @@ import { app_bible_chapter_set } from "../../../love/public/src/app_bible_chapte
 export function app_bible_chapter_open(context, hash, chapter_code) {
   marker("1");
   app_bible_chapter_set(hash, chapter_code);
+  let home = object_property_get(context, "home");
   app_generic_screen_set(context, app_bible_home);
 }
