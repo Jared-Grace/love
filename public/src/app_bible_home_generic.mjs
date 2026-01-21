312577daa58ef53dd0ffe389e336494385a86890
Grace-Jared <christrosetolife@gmail.com>
2026-01-20 22:58:37 -0500

    function_auto_path ..\love\public\src\app_bible_home_generic.mjs

--- public/src/app_bible_home_generic.mjs
+++ public/src/app_bible_home_generic.mjs
@@ -1,3 +1,4 @@
+import { html_hash_set_object } from "../../../love/public/src/html_hash_set_object.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { html_scroll_top_set } from "../../../love/public/src/html_scroll_top_set.mjs";
@@ -174,7 +175,8 @@ export async function app_bible_home_generic(context, lambda$a) {
   function lambda11() {
     let scroll_top = html_scroll_top_get(content);
     let hash2 = html_hash_object_get();
-    object_property_set(object, property_name, value);
+    object_property_set(hash2, scroll_top_key, scroll_top);
+    html_hash_set_object(hash3);
     storage_local_set_context(context, scroll_top_key, scroll_top);
   }
   html_on_scroll(content, lambda11);
