1aad688de9680ac9eda89c047ffc0d7327d559a1
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:17:01 -0500

    o ftc

--- web/public/src/function_transform_command.mjs
+++ web/public/src/function_transform_command.mjs
@@ -1,6 +1,8 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_transform_result } from "../../../love/public/src/function_transform_result.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 export async function function_transform_command(transformer_name, f_name) {
+  marker("1");
   const transformer = await function_import_unalias(transformer_name);
   let v = await function_transform_result(f_name, transformer);
   return v;
