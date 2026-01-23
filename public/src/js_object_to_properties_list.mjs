bac211c9cbf08f0dada4554867a2d33cfde027ac
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:15:58 -0500

    function_auto_path ..\love\public\src\js_object_to_properties_list.mjs

--- public/src/js_object_to_properties_list.mjs
+++ public/src/js_object_to_properties_list.mjs
@@ -13,7 +13,10 @@ export function js_object_to_properties_list(ast) {
     let mapped = list_map_property(properties, "value");
     let expression = js_expression_array(mapped);
     object_replace(node, expression);
-    log([mapped]);
+    log({
+      mapped,
+      a: b,
+    });
   }
   js_visit_type(ast, "ObjectExpression", lambda);
 }
