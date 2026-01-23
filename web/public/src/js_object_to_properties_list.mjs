e302308ec848157995a607928add2b749a251397
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:02:42 -0500

    function_auto_path ..\love\public\src\js_object_to_properties_list.mjs

---
+++ web/public/src/js_object_to_properties_list.mjs
@@ -0,0 +1,14 @@
+import { log } from "../../../love/public/src/log.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function js_object_to_properties_list(ast) {
+  marker("1");
+  function lambda(v) {
+    let node = object_property_get(v, "node");
+    log({
+      node,
+    });
+  }
+  js_visit_type(ast, "ObjectExpression", lambda);
+}
