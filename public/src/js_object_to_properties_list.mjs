64953dc326d26748e8d48e3c1bf0f3a9c61b25be
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:13:24 -0500

    function_auto_path ..\love\public\src\js_object_to_properties_list.mjs

--- public/src/js_object_to_properties_list.mjs
+++ public/src/js_object_to_properties_list.mjs
@@ -1,3 +1,5 @@
+import { json_to } from "../../../love/public/src/json_to.mjs";
+import { js_expression_list } from "../../../love/public/src/js_expression_list.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -9,8 +11,10 @@ export function js_object_to_properties_list(ast) {
     let node = object_property_get(v, "node");
     let properties = object_property_get(node, "properties");
     let mapped = list_map_property(properties, "value");
+    let expression = js_expression_list(mapped);
+    let json = json_to(object);
     log({
-      properties,
+      mapped,
     });
   }
   js_visit_type(ast, "ObjectExpression", lambda);
