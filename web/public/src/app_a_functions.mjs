9deb70aaa56a7437704f3b71ba6f152818faceba
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:53:02 -0500

    rs app_a_button_function refresh

--- web/public/src/app_a_functions.mjs
+++ web/public/src/app_a_functions.mjs
@@ -1,8 +1,8 @@
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
-import { app_a_button_function } from "../../../love/public/src/app_a_button_function.mjs";
+import { app_a_button_function_refresh } from "../../../love/public/src/app_a_button_function_refresh.mjs";
 import { app_a_function_select } from "../../../love/public/src/app_a_function_select.mjs";
 export async function app_a_functions(context) {
-  app_a_button_function(context);
+  app_a_button_function_refresh(context);
   await app_a_functions_generic(context, on_select);
   function on_select(f_name) {
     app_a_function_select(context, f_name);
