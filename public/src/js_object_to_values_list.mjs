2777fec7059953457b656c7a9548db9a219454a8
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 14:51:13 -0500

    function_auto_path ..\love\public\src\js_object_to_values_list.mjs

--- public/src/js_object_to_values_list.mjs
+++ public/src/js_object_to_values_list.mjs
@@ -1,7 +1,10 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_object_to_values_list(ast) {
   marker("1");
-  function lambda(v) {}
+  function lambda(v) {
+    let node = object_property_get(v, "node");
+  }
   js_visit_type(ast, "ObjectExpression", lambda);
 }
