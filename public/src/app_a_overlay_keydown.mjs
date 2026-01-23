071cc628e5e4ea921673825cb181797a6f1665da
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 18:58:58 -0500

    function_auto_path ..\love\public\src\app_a_overlay_keydown.mjs

---
+++ public/src/app_a_overlay_keydown.mjs
@@ -0,0 +1,29 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { html_overlay_z_max } from "../../../love/public/src/html_overlay_z_max.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+import { html_remove } from "../../../love/public/src/html_remove.mjs";
+import { list_remove } from "../../../love/public/src/list_remove.mjs";
+export function app_a_overlay_keydown(a, lambda$e) {
+  marker("1");
+  let root = object_property_get(a, "root");
+  let context = object_property_get(a, "context");
+  let on_keydowns = object_property_get(context, "on_keydowns");
+  let app_a_function_on_keydown = object_property_get(
+    a,
+    "app_a_function_on_keydown",
+  );
+  list_remove(on_keydowns, app_a_function_on_keydown);
+  list_add(on_keydowns, lambda$e);
+  let overlay = html_overlay_z_max(root);
+  function overlay_close() {
+    list_add(on_keydowns, app_a_function_on_keydown);
+    list_remove(on_keydowns, lambda$e);
+    html_remove(overlay);
+  }
+  let o = {
+    overlay,
+    overlay_close,
+  };
+  return o;
+}
