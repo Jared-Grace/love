9a9649e29b977795bef89347c46018c30fba65e8
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:14:05 -0500

    function_auto_path ..\love\public\src\js_expression_array.mjs

--- public/src/js_expression_list.mjs
+++
@@ -1,9 +0,0 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
-import { js_code_brackets_empty } from "../../../love/public/src/js_code_brackets_empty.mjs";
-export function js_expression_list(elements) {
-  let code_expression = js_code_brackets_empty();
-  let expression = js_parse_expression(code_expression);
-  object_property_set(expression, "elements", elements);
-  return expression;
-}
