50f4afe3ec2faf96736325f44909f1691a23cb9b
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:54:50 -0500

    function_auto_path ..\love\public\src\app_a_button_function_refresh.mjs

--- public/src/app_a_button_function_refresh.mjs
+++ public/src/app_a_button_function_refresh.mjs
@@ -1,9 +1,8 @@
+import { app_a_button_function } from "../../../love/public/src/app_a_button_function.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { app_a_button_function_text_selected } from "../../../love/public/src/app_a_button_function_text_selected.mjs";
 import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
-import { app_a_button } from "../../../love/public/src/app_a_button.mjs";
 import { storage_local_exists_context } from "../../../love/public/src/storage_local_exists_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_button_function_refresh(context) {
@@ -13,8 +12,7 @@ export function app_a_button_function_refresh(context) {
   let exists = storage_local_exists_context(context, key);
   let button = null;
   if (exists) {
-    const text = app_a_button_function_text_selected(context);
-    let b = app_a_button(root, text, lambda2);
+    let b = app_a_button_function(context, root, lambda2);
     button = b;
     function lambda2() {
       app_generic_screen_set(context, app_a_function);
