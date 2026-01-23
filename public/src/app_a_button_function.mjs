6cc28f7e86e1d72219b9b6dc8cec2d0fbe397a3d
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:55:26 -0500

    function_auto_path ..\love\public\src\app_a_button_function.mjs

--- public/src/app_a_button_function.mjs
+++ public/src/app_a_button_function.mjs
@@ -1,9 +1,9 @@
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_button } from "../../../love/public/src/app_a_button.mjs";
 import { app_a_button_function_text_selected } from "../../../love/public/src/app_a_button_function_text_selected.mjs";
-export function app_a_button_function(root, lambda) {
+export function app_a_button_function(parent, lambda) {
   marker("1");
   const text = app_a_button_function_text_selected(context);
-  let b = app_a_button(root, text, lambda);
+  let b = app_a_button(parent, text, lambda);
   return b;
 }
