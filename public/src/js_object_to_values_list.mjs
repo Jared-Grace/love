985af993aa7122327a479aa45724101ea75820ff
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 14:50:56 -0500

    function_auto_path ..\love\public\src\js_object_to_values_list.mjs

--- public/src/js_object_to_values_list.mjs
+++ public/src/js_object_to_values_list.mjs
@@ -2,5 +2,6 @@ import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_object_to_values_list(ast) {
   marker("1");
-  js_visit_type(ast2, type, function lambda(v) {});
+  function lambda(v) {}
+  js_visit_type(ast2, type, lambda);
 }
