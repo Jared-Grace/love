d1383d69fe772bf9d4763485a9c26e2cb43e184e
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 15:35:35 -0500

    function_auto_path ..\love\public\src\js_call_fill.mjs

--- web/public/src/js_call_fill.mjs
+++ web/public/src/js_call_fill.mjs
@@ -1,7 +1,7 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { data_functions_get } from "../../../love/public/src/data_functions_get.mjs";
 import { js_function_last_asyncify } from "../../../love/public/src/js_function_last_asyncify.mjs";
-import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { js_visit_type_each_async } from "../../../love/public/src/js_visit_type_each_async.mjs";
 import { functions_names_includes } from "../../../love/public/src/functions_names_includes.mjs";
 import { js_call_new } from "../../../love/public/src/js_call_new.mjs";
@@ -16,11 +16,10 @@ export async function js_call_fill(ast) {
     let expression = object_property_get(node, "expression");
     if (js_identifier_is(expression)) {
       let name = object_property_get(expression, "name");
-      let v2 = await function_name_unalias(name);
-      let unaliased = object_property_get(v2, "unaliased");
+      let unaliased = await function_name_unalias_only(name);
       const valid = await functions_names_includes(unaliased);
       if (valid) {
-        let v3 = await js_call_new(name, ast);
+        let v3 = await js_call_new(unaliased, ast);
         let async_is = object_property_get(v3, "async_is");
         let parsed = object_property_get(v3, "parsed");
         object_replace(node, parsed);
