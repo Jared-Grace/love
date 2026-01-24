5730dfaf6e80ac271f9315ae9a2c505a3bc226fa
Grace-Jared <christrosetolife@gmail.com>
2026-01-24 15:37:47 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_function.mjs
+++ web/public/src/app_a_function.mjs
@@ -1,5 +1,4 @@
 import { function_new_js_name } from "../../../love/public/src/function_new_js_name.mjs";
-import { html_value_set } from "../../../love/public/src/html_value_set.mjs";
 import { app_a_functions_overlay } from "../../../love/public/src/app_a_functions_overlay.mjs";
 import { app_a_function_on_change } from "../../../love/public/src/app_a_function_on_change.mjs";
 import { function_parse } from "../../../love/public/src/function_parse.mjs";
@@ -196,10 +195,9 @@ export async function app_a_function(context) {
         let v6 = await app_a_functions_overlay(a, lambda11);
         let overlay_result = object_property_get(v6, "overlay_result");
         let functions_result = object_property_get(v6, "functions_result");
-        let input = object_property_get(functions_result, "input");
-        let on_input = object_property_get(functions_result, "on_input");
+        let input_set = object_property_get(functions_result, "input_set");
         let combined2 = function_new_js_name("");
-        html_value_set(input, combined2);
+        input_set(input, combined2);
         async function lambda11(f_name_call) {
           let transformer = await function_parse(f_name_call);
           transformer(ast);
