69a07bcfe8a79a98a6356ddce161e9de3d6df506
Grace-Jared <christrosetolife@gmail.com>
2026-01-24 15:45:47 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_function.mjs
+++ web/public/src/app_a_function.mjs
@@ -1,3 +1,4 @@
+import { app_a_function_import } from "../../../love/public/src/app_a_function_import.mjs";
 import { function_new_js_name } from "../../../love/public/src/function_new_js_name.mjs";
 import { app_a_functions_overlay } from "../../../love/public/src/app_a_functions_overlay.mjs";
 import { app_a_function_on_change } from "../../../love/public/src/app_a_function_on_change.mjs";
@@ -18,12 +19,7 @@ import { storage_local_exists } from "../../../love/public/src/storage_local_exi
 import { app_a } from "../../../love/public/src/app_a.mjs";
 import { app_a_apps } from "../../../love/public/src/app_a_apps.mjs";
 import { emoji_mobile } from "../../../love/public/src/emoji_mobile.mjs";
-import { function_dependencies_externals_to_urls } from "../../../love/public/src/function_dependencies_externals_to_urls.mjs";
-import { global_import_set } from "../../../love/public/src/global_import_set.mjs";
-import { each_object } from "../../../love/public/src/each_object.mjs";
-import { object_values_map_async } from "../../../love/public/src/object_values_map_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { function_dependencies_code_export } from "../../../love/public/src/function_dependencies_code_export.mjs";
 import { app_a_function_on_keydown_remove } from "../../../love/public/src/app_a_function_on_keydown_remove.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { object_properties_from_empty } from "../../../love/public/src/object_properties_from_empty.mjs";
@@ -158,22 +154,7 @@ export async function app_a_function(context) {
             app_a_on_keydown(e, choices);
           }
           let div = app_a_overlay_container(overlay);
-          let v2 = await function_dependencies_code_export(f_name);
-          let externals = object_property_get(v2, "externals");
-          let get = object_property_get(v2, "get");
-          let v3 = await get();
-          let global = object_property_get(v3, "global");
-          let imports = function_dependencies_externals_to_urls(externals);
-          async function lambda6(url) {
-            let v4 = await import(url);
-            return v4;
-          }
-          let modules = await object_values_map_async(imports, lambda6);
-          function lambda4(m, name) {
-            global_import_set(name, m, global);
-          }
-          each_object(modules, lambda4);
-          let fn = object_property_get(v3, "fn");
+          let fn = await app_a_function_import(f_name);
           let r = await fn();
           let j = json_format_to(r);
           let pre = html_pre_text(div, j);
