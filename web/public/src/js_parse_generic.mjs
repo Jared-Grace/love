daa7a8bb6dfd5becb5c1eab9aceb571cfddfeb00
Grace-Jared <christrosetolife@gmail.com>
2026-01-24 16:03:48 -0500

    function_auto_path ..\love\public\src\js_parse.mjs

--- web/public/src/js_parse_generic.mjs
+++ web/public/src/js_parse_generic.mjs
@@ -1,18 +1,8 @@
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { global_import_get } from "../../../love/public/src/global_import_get.mjs";
-import { global_import_exists } from "../../../love/public/src/global_import_exists.mjs";
 export function js_parse_generic(acorn, code) {
   let ast = null;
   try {
-    let a = null;
-    let module_name = "acorn";
-    let e = global_import_exists(module_name);
-    if (e) {
-      a = global_import_get(module_name);
-    } else {
-      a = acorn;
-    }
-    ast = a.parse(code, {
+    ast = acorn.parse(code, {
       ecmaVersion: 2020,
       sourceType: "module",
     });
