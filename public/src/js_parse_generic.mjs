73650cc788ed8dd2de0d84a8b239bf331de6751b
Grace-Jared <christrosetolife@gmail.com>
2026-01-24 16:01:45 -0500

    function_auto_path ..\love\public\src\js_parse_generic.mjs

--- public/src/js_parse_generic.mjs
+++ public/src/js_parse_generic.mjs
@@ -1,3 +1,5 @@
+import { global_import_get } from "../../../love/public/src/global_import_get.mjs";
+import { global_import_exists } from "../../../love/public/src/global_import_exists.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export function js_parse_generic(acorn, code) {
   let ast = null;
@@ -11,4 +13,10 @@ export function js_parse_generic(acorn, code) {
     throw e;
   }
   return ast;
+  let e = global_import_exists(module_name);
+  if (e) {
+    let a = global_import_get(module_name);
+  } else {
+    let a = acorn;
+  }
 }
