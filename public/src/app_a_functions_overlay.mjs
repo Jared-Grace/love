37903318faf5e4793640d4e44cf48b1d32a94ac5
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 21:00:37 -0500

    function_auto_path ..\love\public\src\app_a_functions_overlay.mjs

--- public/src/app_a_functions_overlay.mjs
+++ public/src/app_a_functions_overlay.mjs
@@ -5,8 +5,8 @@ import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
 export async function app_a_functions_overlay(a, on_select) {
-  let o2 = app_a_overlay(a);
-  let overlay = object_property_get(o2, "overlay");
+  let o = app_a_overlay(a);
+  let overlay = object_property_get(o, "overlay");
   html_style_set(overlay, "overflow", "hidden");
   const replacement = {
     root: overlay,
@@ -14,10 +14,10 @@ export async function app_a_functions_overlay(a, on_select) {
   let context = object_property_get(a, "context");
   let copy = object_copy_assign(context, replacement);
   function lambda3() {
-    let overlay_close = object_property_get(o2, "overlay_close");
+    let overlay_close = object_property_get(o, "overlay_close");
     overlay_close();
   }
   app_a_button_function(context, overlay, lambda3);
   let r = await app_a_functions_generic(copy, on_select);
-  return o2;
+  return o;
 }
