1a855ff214164edd8edf6d77404ba4f588bb1555
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 21:33:11 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/app_a_function.mjs
+++ web/public/src/app_a_function.mjs
@@ -197,8 +197,8 @@ export async function app_a_function(context) {
         let overlay_result = object_property_get(v6, "overlay_result");
         let functions_result = object_property_get(v6, "functions_result");
         let input = object_property_get(functions_result, "input");
-        let combined2 = function_new_js_name(f_name_unprefixed);
-        html_value_set(input, value);
+        let combined2 = function_new_js_name("");
+        html_value_set(input, combined2);
         async function lambda11(f_name_call) {
           let transformer = await function_parse(f_name_call);
           transformer(ast);
