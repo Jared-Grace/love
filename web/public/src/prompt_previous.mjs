8b283330a8b94c5bd7a7214087bade4b982248b8
Grace-Jared <christrosetolife@gmail.com>
2026-01-23 21:32:19 -0500

    o p

--- web/public/src/prompt_previous.mjs
+++ web/public/src/prompt_previous.mjs
@@ -1,7 +1,7 @@
 import { prompt_previous_get } from "../../../love/public/src/prompt_previous_get.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_run_line } from "../../../love/public/src/function_run_line.mjs";
-export async function prompt_previous() {
+export async function prompt_previous() {$aa
   marker("1");
   let previous = await prompt_previous_get();
   let v = await function_run_line(previous);
