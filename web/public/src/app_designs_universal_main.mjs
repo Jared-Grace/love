e9b1b36ba36aa3aff3250684685fa80fd7a97676
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:16:23 -0500

    p

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -7,8 +7,8 @@ import { each } from "../../../love/public/src/each.mjs";
 export function app_designs_universal_main() {
   let colors = ["black", "white"];
   let slots = 2;
+  let possbilities = [[]];
   function lambda3(i) {
-    let possbilities = [[]];
     function lambda(color) {
       let copy = json_copy(possbilities);
       function lambda2(p) {
