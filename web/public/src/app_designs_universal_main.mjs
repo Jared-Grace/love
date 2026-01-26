3119b2109cca29eb9e6a5afbba8d33e127f627f4
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:15:12 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_concat_multiple } from "../../../love/public/src/list_concat_multiple.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -15,6 +16,9 @@ export function app_designs_universal_main() {
     each(copy, lambda2);
   }
   let m = list_map(colors, lambda);
+  log({
+    m,
+  });
   let combined = list_concat_multiple(m);
   return combined;
 }
