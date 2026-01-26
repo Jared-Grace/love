fc287e9a314260baa2bb852db3fdc3df84c9ed03
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 17:07:38 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- public/src/app_designs_universal_main.mjs
+++ public/src/app_designs_universal_main.mjs
@@ -40,10 +40,8 @@ export function app_designs_universal_main(context) {
     },
   ];
   function lambda4(dimension) {
-    let columns2 = object_property_get(dimension, "columns");
-    let rows2 = object_property_get(dimension, "rows");
-    let columns = 3;
-    let rows = 2;
+    let columns = object_property_get(dimension, "columns");
+    let rows = object_property_get(dimension, "rows");
     let slots = rows * columns;
     let possbilities = list_cartesian_product_self(colors, slots);
     function lambda3(possibility) {
