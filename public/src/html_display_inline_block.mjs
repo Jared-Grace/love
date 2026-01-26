a3037bddd843c33e82b75e7f1070a4be80a20f64
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:48:47 -0500

    function_auto_path ..\love\public\src\html_display_inline_block.mjs

--- public/src/html_display_inline_block.mjs
+++ public/src/html_display_inline_block.mjs
@@ -2,5 +2,5 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 export function html_display_inline_block(item) {
   marker("1");
-  html_style_set(item, "display", "inline");
+  html_style_set(item, "display", "inline-block");
 }
