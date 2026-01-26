e49457f902a3980f4394a5ab154c8175b16d3e3e
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:11:03 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -1,8 +1,11 @@
+import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function app_designs_universal_main() {
   let colors = ["black", "white"];
   let slots = 2;
   let possbilities = [];
-  function lambda(color) {}
+  function lambda(color) {
+    let concated = list_concat(a, b);
+  }
   each(colors, lambda);
 }
