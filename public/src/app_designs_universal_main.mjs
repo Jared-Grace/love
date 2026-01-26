84c9a2387319a134b67bc8d9c1639d4f253808a3
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:22:29 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- public/src/app_designs_universal_main.mjs
+++ public/src/app_designs_universal_main.mjs
@@ -3,7 +3,7 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { list_cartesian_product_self } from "../../../love/public/src/list_cartesian_product_self.mjs";
 export function app_designs_universal_main(context) {
   let root = object_property_get(context, "root");
-  html_style_background_color(div, background);
+  html_style_background_color(root, "gray");
   let colors = ["black", "white"];
   let slots = 2;
   let possbilities = list_cartesian_product_self(colors, slots);
