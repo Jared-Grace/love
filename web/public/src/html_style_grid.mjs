98215454cacfc4dd96d8bc886257539d7d3a5006
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:56:48 -0500

    function_auto_path ..\love\public\src\html_style_grid.mjs

--- web/public/src/html_style_grid.mjs
+++ web/public/src/html_style_grid.mjs
@@ -2,8 +2,7 @@ import { html_style_assign } from "../../../love/public/src/html_style_assign.mj
 export function html_style_grid(shape, columns, rows) {
   html_style_assign(shape, {
     display: "inline-grid",
-    "grid-template-columns": "repeat(" + columns + ", 1fr)",
-    "grid-template-rows": "repeat(" + rows + ", auto)",
+    "grid-template-columns": "repeat(" + columns + ", auto)",
     gap: "0",
   });
 }
