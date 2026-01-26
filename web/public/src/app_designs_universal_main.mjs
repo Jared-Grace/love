5e163178a2fc45bd4143df70c03b0c3dc3229275
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:19:05 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -1,25 +1,7 @@
-import { each_range } from "../../../love/public/src/each_range.mjs";
-import { list_concat_multiple } from "../../../love/public/src/list_concat_multiple.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { json_copy } from "../../../love/public/src/json_copy.mjs";
-import { each } from "../../../love/public/src/each.mjs";
+import { list_cartesian_product_self } from "../../../love/public/src/list_cartesian_product_self.mjs";
 export function app_designs_universal_main() {
   let colors = ["black", "white"];
   let slots = 2;
-  let possbilities = [[]];
-  function lambda3(i) {
-    function lambda(color) {
-      let copy = json_copy(possbilities);
-      function lambda2(p) {
-        list_add(p, color);
-      }
-      each(copy, lambda2);
-      return copy;
-    }
-    let m = list_map(colors, lambda);
-    possbilities = list_concat_multiple(m);
-  }
-  each_range(slots, lambda3);
+  let possbilities = list_cartesian_product_self(colors, slots);
   return possbilities;
 }
