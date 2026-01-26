fae4a5724f2da3d36dde1a2cc94809dcdbfee3d3
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:56:34 -0500

    function_auto_path ..\love\public\src\html_style_grid.mjs

--- public/src/html_style_grid.mjs
+++ public/src/html_style_grid.mjs
@@ -1,7 +1,7 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function html_style_grid(shape, columns, rows) {
   html_style_assign(shape, {
-    display: "grid",
+    display: "inline-grid",
     "grid-template-columns": "repeat(" + columns + ", 1fr)",
     "grid-template-rows": "repeat(" + rows + ", auto)",
     gap: "0",
