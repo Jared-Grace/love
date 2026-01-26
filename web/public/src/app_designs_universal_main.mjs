2bc9b33c88be5052e7fc66a6ebfcc22c3c46de11
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:11:48 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -1,3 +1,4 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function app_designs_universal_main() {
@@ -6,6 +7,7 @@ export function app_designs_universal_main() {
   let possbilities = [];
   function lambda(color) {
     let copy = json_copy(possbilities);
+    list_add(list, item);
   }
   each(colors, lambda);
 }
