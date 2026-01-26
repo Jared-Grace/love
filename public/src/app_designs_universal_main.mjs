e66d46a3aa092197e00086dd92b7ca3b7c0dc2f6
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:10:36 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- public/src/app_designs_universal_main.mjs
+++ public/src/app_designs_universal_main.mjs
@@ -2,6 +2,6 @@ import { each } from "../../../love/public/src/each.mjs";
 export function app_designs_universal_main() {
   let colors = ["black", "white"];
   let slots = 2;
-  function lambda(item) {}
-  each(list, lambda);
+  function lambda(color) {}
+  each(colors, lambda);
 }
