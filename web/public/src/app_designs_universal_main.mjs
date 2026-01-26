f6f3f0f6b7aabc27453ec7b8fbd793578beef03b
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 16:11:31 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -1,11 +1,11 @@
-import { list_copy } from "../../../love/public/src/list_copy.mjs";
+import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function app_designs_universal_main() {
   let colors = ["black", "white"];
   let slots = 2;
   let possbilities = [];
   function lambda(color) {
-    let copy = list_copy(original);
+    let copy = json_copy(possbilities);
   }
   each(colors, lambda);
 }
