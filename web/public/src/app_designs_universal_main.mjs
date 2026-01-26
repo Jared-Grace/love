59ce19458ea819c56db91e3379b512694991d584
Grace-Jared <christrosetolife@gmail.com>
2026-01-26 17:03:15 -0500

    function_auto_path ..\love\public\src\app_designs_universal_main.mjs

--- web/public/src/app_designs_universal_main.mjs
+++ web/public/src/app_designs_universal_main.mjs
@@ -9,10 +9,11 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { list_cartesian_product_self } from "../../../love/public/src/list_cartesian_product_self.mjs";
 export function app_designs_universal_main(context) {
   let root = object_property_get(context, "root");
+  const newLocal = "5px";
   html_style_assign(root, {
     display: "flex",
     "flex-wrap": "wrap",
-    gap: "5px",
+    gap: newLocal,
   });
   html_style_background_color(root, "gray");
   let colors = ["black", "white"];
