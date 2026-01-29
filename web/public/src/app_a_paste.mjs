fd1bd6ac7035792af4b9db6ddef0cc839f1a6f59
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 15:18:56 -0500

    function_auto_path ..\love\public\src\app_a_paste.mjs

--- web/public/src/app_a_paste.mjs
+++ web/public/src/app_a_paste.mjs
@@ -1,3 +1,4 @@
+import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { storage_local_exists_context } from "../../../love/public/src/storage_local_exists_context.mjs";
@@ -7,6 +8,7 @@ export function app_a_paste(choices, a, overlay_close, body) {
   let context = object_property_get(a, "context");
   let exists = storage_local_exists_context(context, app_a_paste.name);
   if (exists) {
+    let value = storage_local_get_context(context2, key);
     list_add(choices, {
       shortcut: "v",
       text: "Paste",
