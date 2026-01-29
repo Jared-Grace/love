f20f6be011515e41e3275450e5bcaba81dfc7796
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 09:48:07 -0500

    function_auto_path ..\love\public\src\function_transform_result.mjs

--- web/public/src/function_transform_result.mjs
+++ web/public/src/function_transform_result.mjs
@@ -6,8 +6,10 @@ import { data_all_initialize } from "../../../love/public/src/data_all_initializ
 import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
+import { each_async } from "./each_async.mjs";
 export async function function_transform_result(f_names, lambda$ast) {
   let split = string_split_comma(f_names);
+ async function lambda(f_name) {
   async function lambda2() {
     let parsed = await function_parse_unaliased(f_names);
     let ast = object_property_get(parsed, "ast");
@@ -15,10 +17,9 @@ export async function function_transform_result(f_names, lambda$ast) {
     await file_js_unparse(parsed);
     return result;
   }
-  await lambda2();
+  await lambda2();}
+  each_async(split, lambda);
   return;
-  function lambda(item) {}
-  each(list, lambda);
   let d_path = data_path();
   await data_all_initialize(d_path);
   let r = await file_transform_cached(d_path, lambda2);
