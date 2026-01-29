9a37cc64fb124e8f5abe55aa226cdbfbda15893f
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 14:45:45 -0500

    function_auto_path ..\love\public\src\app_a_braces_wrap.mjs

--- public/src/app_a_braces_wrap.mjs
+++ public/src/app_a_braces_wrap.mjs
@@ -1,7 +1,7 @@
 import { app_a_brace_right } from "../../../love/public/src/app_a_brace_right.mjs";
 import { app_a_brace_left } from "../../../love/public/src/app_a_brace_left.mjs";
 export function app_a_braces_wrap(parent, lambda) {
-  let s = app_a_brace_left(parent);
+  let left = app_a_brace_left(parent);
   lambda();
-  let span2 = app_a_brace_right(parent);
+  let right = app_a_brace_right(parent);
 }
