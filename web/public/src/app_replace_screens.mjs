3a2cff88a7ee48277a475c44c46857978a605853
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 09:49:15 -0500

    ftc js_object_to_properties_list app_replace_screens,app_bible_screens

--- web/public/src/app_replace_screens.mjs
+++ web/public/src/app_replace_screens.mjs
@@ -4,10 +4,6 @@ import { app_replace_home } from "../../../love/public/src/app_replace_home.mjs"
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_replace_screens() {
   marker("screens");
-  let v = {
-    home: app_replace_home,
-    rule_set: app_replace_rule_set,
-    settings: app_replace_settings,
-  };
+  let v = [app_replace_home, app_replace_rule_set, app_replace_settings];
   return v;
 }
