23ad071636facf1b33ba64e4676b35ddafca97c0
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 14:50:28 -0500

    function_auto_path ..\love\public\src\app_a_function_declaration.mjs

--- public/src/app_a_function_declaration.mjs
+++ public/src/app_a_function_declaration.mjs
@@ -30,5 +30,5 @@ export function app_a_function_declaration(a) {
   }
   let body2 = object_property_get(node, "body");
   html_span_space(parent);
-  let r = app_a_function_node_child(a, body2);
+  let r = app_a_function_node_child(a, body2);$L$r
 }
