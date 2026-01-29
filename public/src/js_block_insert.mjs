fdd9412804438606f53a662c0caa55b0b23d9f9c
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 15:31:24 -0500

    function_auto_path ..\love\public\src\js_block_insert.mjs

--- public/src/js_block_insert.mjs
+++ public/src/js_block_insert.mjs
@@ -4,8 +4,8 @@ import { list_next } from "../../../love/public/src/list_next.mjs";
 import { js_stack_last } from "../../../love/public/src/js_stack_last.mjs";
 export function js_block_insert(stack, inserted) {
   let block = js_stack_last(stack, "BlockStatement");
-  let block_body = list_next(stack, block);
-  let block_body_item = list_next(stack, block_body);
-  let block_body_item_index = list_index_of(block_body, block_body_item);
-  list_insert(block_body, block_body_item_index, inserted);
+  let body = list_next(stack, block);
+  let item = list_next(stack, body);
+  let index = list_index_of(body, item);
+  list_insert(body, index, inserted);
 }
