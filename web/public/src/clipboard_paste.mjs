cc097559aaa5b9c3f7243af92ba62998b37474c3
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 16:39:27 -0500

    function_auto_path ..\love\public\src\clipboard_paste.mjs

--- web/public/src/clipboard_paste.mjs
+++ web/public/src/clipboard_paste.mjs
@@ -6,6 +6,7 @@ export async function clipboard_paste() {
     return v;
   }
   const clipboard = await import("clipboardy");
-  let paste = await clipboard.default.read();
+  let paste = null;
+  paste = await clipboard.default.read();
   return paste;
 }
