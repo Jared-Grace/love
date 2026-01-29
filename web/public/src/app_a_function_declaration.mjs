c189002ff546f0fc0302b0c331049fa495241672
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 15:12:41 -0500

    function_auto_path ..\love\public\src\app_a_function_declaration.mjs

--- web/public/src/app_a_function_declaration.mjs
+++ web/public/src/app_a_function_declaration.mjs
@@ -38,8 +38,8 @@ export function app_a_function_declaration(a) {
   function lambda(lr) {
     function lambda2(o) {
       let overlay_close = object_property_get(o, "overlay_close");
-      let v3 = [];
-      return v3;
+      let choices = [];
+      return choices;
     }
     app_a_overlay_choices(a, lr, lambda2);
   }
