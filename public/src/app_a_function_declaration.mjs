18d7b2e74934725501c47a5bbd5ecfbb8cd6bd80
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 17:14:55 -0500

    function_auto_path ..\love\public\src\app_a_function_declaration.mjs

--- public/src/app_a_function_declaration.mjs
+++ public/src/app_a_function_declaration.mjs
@@ -20,7 +20,8 @@ export function app_a_function_declaration(a) {
   let kf = js_keyword_function();
   let k = app_a_keyword_blue_space(parent, kf);
   let keyword = object_property_get(k, "keyword");
-  app_a_overlay_choices(a2, component, function lambda3(o2) {});
+  function lambda3(o2) {}
+  app_a_overlay_choices(a2, component, lambda3);
   let async = object_property_get(node, "async");
   if (async) {
     let ka = js_keyword_async();
