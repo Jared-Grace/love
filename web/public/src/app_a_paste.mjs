563820974f25a2d7bf970040affd64e9f5fdbdbd
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 15:14:34 -0500

    function_auto_path ..\love\public\src\app_a_paste.mjs

--- web/public/src/app_a_paste.mjs
+++ web/public/src/app_a_paste.mjs
@@ -1,4 +1,6 @@
+import { storage_local_exists_context } from "../../../love/public/src/storage_local_exists_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function app_a_paste(choices) {
   marker("1");
+  let exists = storage_local_exists_context(context, key);
 }
