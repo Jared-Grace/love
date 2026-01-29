22d9b055ecd522b0e89d743b2e104658677a51f2
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 16:36:45 -0500

    function_auto_path ..\love\public\src\app_a_identifier_generic.mjs

--- web/public/src/app_a_identifier_generic.mjs
+++ web/public/src/app_a_identifier_generic.mjs
@@ -1,3 +1,4 @@
+import { assert } from "../../../love/public/src/assert.mjs";
 import { clipboard_paste } from "../../../love/public/src/clipboard_paste.mjs";
 import { html_rows_set } from "../../../love/public/src/html_rows_set.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -125,6 +126,7 @@ export function app_a_identifier_generic(
         text: "Paste replace",
         fn: async function lambda2() {
           let name_new = await clipboard_paste();
+          assert(b);
           replace(name_new);
           await app_a_function_on_change(o, a);
           overlay_close();
