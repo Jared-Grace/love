a5f9e6261a692b3545285966be2a7a4bbfdeaa2c
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 18:10:12 -0500

    function_auto_path ..\love\public\src\app_a_functionize.mjs

--- public/src/app_a_functionize.mjs
+++ public/src/app_a_functionize.mjs
@@ -3,4 +3,6 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 export function app_a_functionize(a) {
   let context = object_property_get(a, "context");
   let a_first = storage_local_get_context(context, app_a_functionize.name);
+  let node = object_property_get(a, "node");
+  let node2 = object_property_get(a_first, "node");
 }
