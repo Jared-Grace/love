f3e544a2c63c33dbdeb4a2bc9f948bdef1d2f925
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 18:18:55 -0500

    function_auto_path ..\love\public\src\app_a_functionize.mjs

--- web/public/src/app_a_functionize.mjs
+++ web/public/src/app_a_functionize.mjs
@@ -1,9 +1,10 @@
+import { js_functionize } from "../../../love/public/src/js_functionize.mjs";
 import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function app_a_functionize(a) {
+export async function app_a_functionize(a) {
   let context = object_property_get(a, "context");
   let a_first = storage_local_get_context(context, app_a_functionize.name);
   function lambda(ai) {
@@ -16,4 +17,5 @@ export function app_a_functionize(a) {
     return index;
   }
   let mapped = list_map([a, a_first], lambda);
+  await js_functionize(ast2, f_name_new, stack2, index_from, index_to);
 }
