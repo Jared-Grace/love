abd14a5db1afa0515ca7489916a12ae155404474
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 18:26:11 -0500

    fs app_a_functionize

--- public/src/app_a_functionize.mjs
+++ public/src/app_a_functionize.mjs
@@ -1,9 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { js_functionize } from "../../../love/public/src/js_functionize.mjs";
 import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function app_a_functionize(a) {
+  marker("1");
   let context = object_property_get(a, "context");
   let a_first = storage_local_get_context(context, app_a_functionize.name);
   function lambda(ai) {
