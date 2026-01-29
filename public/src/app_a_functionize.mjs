036f416f8c94b5fa525f1a7db68cb618e531dd4f
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 18:10:37 -0500

    function_auto_path ..\love\public\src\app_a_functionize.mjs

--- public/src/app_a_functionize.mjs
+++ public/src/app_a_functionize.mjs
@@ -1,8 +1,10 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_functionize(a) {
   let context = object_property_get(a, "context");
   let a_first = storage_local_get_context(context, app_a_functionize.name);
+  let mapped = list_map(list, function lambda(item) {});
   let node = object_property_get(a, "node");
   let node2 = object_property_get(a_first, "node");
 }
