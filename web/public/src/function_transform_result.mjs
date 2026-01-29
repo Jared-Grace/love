8992228adf41cbcaac4f48105be9fbffa94337a4
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 09:47:23 -0500

    function_auto_path ..\love\public\src\function_transform_result.mjs

--- web/public/src/function_transform_result.mjs
+++ web/public/src/function_transform_result.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
@@ -6,6 +7,7 @@ import { file_transform_cached } from "../../../love/public/src/file_transform_c
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 export async function function_transform_result(f_names, lambda$ast) {
+  let split = string_split_comma(f_names);
   async function lambda2() {
     let parsed = await function_parse_unaliased(f_names);
     let ast = object_property_get(parsed, "ast");
@@ -15,7 +17,7 @@ export async function function_transform_result(f_names, lambda$ast) {
   }
   await lambda2();
   return;
-  let split = string_split_comma(f_names2);
+  each(list, function lambda(item) {});
   let d_path = data_path();
   await data_all_initialize(d_path);
   let r = await file_transform_cached(d_path, lambda2);
