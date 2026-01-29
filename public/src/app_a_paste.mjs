7cd3a688e22a0850e654f45c7b7fba0c1272782f
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 15:18:26 -0500

    function_auto_path ..\love\public\src\app_a_paste.mjs

--- public/src/app_a_paste.mjs
+++ public/src/app_a_paste.mjs
@@ -2,7 +2,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { storage_local_exists_context } from "../../../love/public/src/storage_local_exists_context.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function app_a_paste(choices, a, overlay_close) {
+export function app_a_paste(choices, a, overlay_close, body) {
   marker("1");
   let context = object_property_get(a, "context");
   let exists = storage_local_exists_context(context, app_a_paste.name);
