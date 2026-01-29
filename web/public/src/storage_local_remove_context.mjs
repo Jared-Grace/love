a8d9db49a8c95b135702599b3a9047b64eaa9f94
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 17:19:02 -0500

    function_auto_path ..\love\public\src\storage_local_remove_context.mjs

--- web/public/src/storage_local_remove_context.mjs
+++ web/public/src/storage_local_remove_context.mjs
@@ -1,7 +1,9 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { storage_local_remove } from "../../../love/public/src/storage_local_remove.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function storage_local_remove_context(app_fn, key) {
+export function storage_local_remove_context(context, key) {
   marker("1");
+  let app_fn = object_property_get(context, "app_fn");
   let v = storage_local_remove(app_fn, key);
   return v;
 }
