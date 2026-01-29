a3fe6644866734783d1d86140e66533b57ed9096
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 17:18:55 -0500

    function_auto_path ..\love\public\src\storage_local_remove_context.mjs

--- public/src/storage_local_remove_context.mjs
+++ public/src/storage_local_remove_context.mjs
@@ -2,5 +2,6 @@ import { storage_local_remove } from "../../../love/public/src/storage_local_rem
 import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_remove_context(app_fn, key) {
   marker("1");
-  return storage_local_remove(app_fn, key);
+  let v = storage_local_remove(app_fn, key);
+  return v;
 }
