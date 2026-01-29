a14a9dcca2ff1b9092f27f89af6e8b97cf3b63fd
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 17:12:50 -0500

    function_auto_path ..\love\public\src\app_a_keyword_blue_space.mjs

--- public/src/app_a_keyword_blue_space.mjs
+++ public/src/app_a_keyword_blue_space.mjs
@@ -1,6 +1,9 @@
 import { html_span_space } from "../../../love/public/src/html_span_space.mjs";
 import { app_a_keyword_blue } from "../../../love/public/src/app_a_keyword_blue.mjs";
 export function app_a_keyword_blue_space(parent, text) {
-  app_a_keyword_blue(parent, text);
+  let keyword = app_a_keyword_blue(parent, text);
   html_span_space(parent);
+  return {
+    keyword,
+  };
 }
