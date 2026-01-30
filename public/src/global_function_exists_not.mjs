acf718307eef39e5276452491128c037c4b82838
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:17:47 -0500

    function_auto_path ..\love\public\src\global_function_exists_not.mjs

--- public/src/global_function_exists_not.mjs
+++ public/src/global_function_exists_not.mjs
@@ -4,6 +4,6 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export function global_function_exists_not(fn) {
   marker("1");
   let exists = global_function_exists(fn);
-  let n = not(exists);
-  return n;
+  let ne = not(exists);
+  return ne;
 }
