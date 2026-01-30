9e37e0e82e6aefd9e37b46febf4a52c0113623a6
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:32:45 -0500

    function_auto_path ..\love\public\src\function_delete_if_exists.mjs

--- web/public/src/function_exists_assert.mjs
+++ web/public/src/function_exists_assert.mjs
@@ -1,7 +1,9 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_exists_unalias } from "../../../love/public/src/function_exists_unalias.mjs";
 import { assert_json } from "../../../love/public/src/assert_json.mjs";
 export async function function_exists_assert(f_name) {
-  let { exists } = await function_exists_unalias(f_name);
+  let v = await function_exists_unalias(f_name);
+  let exists = object_property_get(v, "exists");
   assert_json(exists, {
     f_name,
   });
