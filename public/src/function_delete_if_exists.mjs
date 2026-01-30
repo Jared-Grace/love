80af5b41b3907aabffe0b51b2ac09ab83636c9b6
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:32:39 -0500

    rs function_exists unalias

--- public/src/function_delete_if_exists.mjs
+++ public/src/function_delete_if_exists.mjs
@@ -1,7 +1,7 @@
-import { function_exists } from "../../../love/public/src/function_exists.mjs";
+import { function_exists_unalias } from "../../../love/public/src/function_exists_unalias.mjs";
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function function_delete_if_exists(f_name) {
-  const { exists } = await function_exists(f_name);
+  const { exists } = await function_exists_unalias(f_name);
   if (exists) {
     await function_delete(f_name);
   }
