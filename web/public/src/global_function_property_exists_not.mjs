2612144ea66ed1ae342a5ea44354e6dd4280e07a
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:10:03 -0500

    fws global_function_property_exists not

--- web/public/src/global_function_property_exists_not.mjs
+++ web/public/src/global_function_property_exists_not.mjs
@@ -1,4 +1,6 @@
+import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function global_function_property_exists_not() {
+export function global_function_property_exists_not(fn, property_name) {
   marker("1");
+  return global_function_property_exists(fn, property_name);
 }
