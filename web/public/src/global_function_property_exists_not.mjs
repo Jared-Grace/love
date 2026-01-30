fba9f84fa62ac1b77799fbd30a88451287db0ca7
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:10:12 -0500

    function_auto_path ..\love\public\src\global_function_property_exists_not.mjs

--- web/public/src/global_function_property_exists_not.mjs
+++ web/public/src/global_function_property_exists_not.mjs
@@ -1,7 +1,9 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function global_function_property_exists_not(fn, property_name) {
   marker("1");
   let exists = global_function_property_exists(fn, property_name);
-  return exists;
+  let n = not(exists);
+  return n;
 }
