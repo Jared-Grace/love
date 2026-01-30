e5af0739a1024f491aec4d3bfe8d92070a884707
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:10:00 -0500

    function_auto_path ..\love\public\src\global_function_property_exists_not.mjs

--- web/public/src/global_function_property_exists_not.mjs
+++ web/public/src/global_function_property_exists_not.mjs
@@ -1,1 +1,4 @@
-export function global_function_property_exists_not() {}
+import { marker } from "../../../love/public/src/marker.mjs";
+export function global_function_property_exists_not() {
+  marker("1");
+}
