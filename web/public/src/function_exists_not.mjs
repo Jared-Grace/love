3867da6e2dd6b20797859fbfaadf0ba7bfd98f9e
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:43:13 -0500

    function_auto_path ..\love\public\src\function_exists_not.mjs

--- web/public/src/function_exists_not.mjs
+++ web/public/src/function_exists_not.mjs
@@ -1,8 +1,8 @@
+import { function_exists } from "../../../love/public/src/function_exists.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 export async function function_exists_not(f_name) {
-  let v = await function_unalias_exists(f_name);
+  let v = await function_exists(f_name);
   let exists = object_property_get(v, "exists");
   let n = not(exists);
   return n;
