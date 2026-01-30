9333b3ba2e82193fed52fd10ba4082a99bff4819
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:45:49 -0500

    rr function_exists_not_assert exists unalias_exists

--- public/src/function_exists_not_assert.mjs
+++
@@ -1,6 +0,0 @@
-import { function_unalias_exists_not } from "../../../love/public/src/function_unalias_exists_not.mjs";
-import { assert } from "../../../love/public/src/assert.mjs";
-export async function function_exists_not_assert(f_name) {
-  let n = await function_unalias_exists_not(f_name);
-  assert(n);
-}
