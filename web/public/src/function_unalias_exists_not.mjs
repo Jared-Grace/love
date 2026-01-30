2bd67bc54ecdba8f292d3a0c11f44a63cd4d98dc
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:43:04 -0500

    function_auto_path ..\love\public\src\function_unalias_exists_not.mjs

---
+++ web/public/src/function_unalias_exists_not.mjs
@@ -0,0 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
+export async function function_unalias_exists_not(f_name) {
+  marker("1");
+  let v = await function_unalias_exists(f_name);
+  let exists = object_property_get(v, "exists");
+  let n = not(exists);
+  return n;
+}
