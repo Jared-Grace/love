51658681263cc49ad033383eaa3536fb03114e92
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:18:21 -0500

    function_auto_path ..\love\public\src\app_a_functionize_start_choice_add.mjs

--- web/public/src/app_a_functionize_start_choice_add.mjs
+++ web/public/src/app_a_functionize_start_choice_add.mjs
@@ -6,13 +6,13 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_a_functionize } from "../../../love/public/src/app_a_functionize.mjs";
 export function app_a_functionize_start_choice_add(choices, a, o) {
   marker("1");
-  let ne = global_function_exists_not(app_a_functionize.name);
+  let context = object_property_get(a, "context");
+  let ne = global_function_exists_not(context, app_a_functionize.name);
   if (ne) {
     list_add(choices, {
       shortcut: "f",
       text: "Functionize start",
       fn: async function lambda() {
-        let context = object_property_get(a, "context");
         global_function_property_set(context, app_a_functionize.name, a);
         let overlay_close = object_property_get(o, "overlay_close");
         overlay_close();
