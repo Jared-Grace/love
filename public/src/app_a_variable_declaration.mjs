0fc62f3369676b9bf46980cb7eddcf237d08a9c1
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:13:32 -0500

    function_auto_path ..\love\public\src\app_a_variable_declaration.mjs

--- public/src/app_a_variable_declaration.mjs
+++ public/src/app_a_variable_declaration.mjs
@@ -1,17 +1,8 @@
-import { app_a_function_on_change } from "../../../love/public/src/app_a_function_on_change.mjs";
-import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
-import { app_a_button_wide } from "../../../love/public/src/app_a_button_wide.mjs";
-import { html_focus } from "../../../love/public/src/html_focus.mjs";
-import { html_value_get } from "../../../love/public/src/html_value_get.mjs";
-import { app_a_input } from "../../../love/public/src/app_a_input.mjs";
-import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
-import { app_a_overlay_on_enter } from "../../../love/public/src/app_a_overlay_on_enter.mjs";
+import { app_a_functionize_end_choice_add } from "../../../love/public/src/app_a_functionize_end_choice_add.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_semicolon } from "../../../love/public/src/app_a_semicolon.mjs";
 import { app_a_nodes_list } from "../../../love/public/src/app_a_nodes_list.mjs";
 import { app_a_overlay_choices } from "../../../love/public/src/app_a_overlay_choices.mjs";
-import { app_a_functionize } from "../../../love/public/src/app_a_functionize.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_a_keyword_blue_space } from "../../../love/public/src/app_a_keyword_blue_space.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_variable_declaration(a) {
@@ -23,29 +14,7 @@ export function app_a_variable_declaration(a) {
   let keyword = object_property_get(k, "keyword");
   function lambda19(o2) {
     let choices = [];
-    let context = object_property_get(a, "context");
-    let e = global_function_property_exists(context, app_a_functionize.name);
-    if (e) {
-      list_add(choices, {
-        shortcut: "f",
-        text: "Functionize end",
-        fn: async function lambda() {
-          let r = app_a_overlay_on_enter(on_enter, o2, a);
-          let o = object_property_get(r, "overlay_result");
-          let overlay = object_property_get(o, "overlay");
-          let oc = object_property_get(r, "container");
-          let div = html_div_text(oc, "Name of new function:");
-          let input = app_a_input(overlay);
-          html_focus(input);
-          async function on_enter() {
-            let value_new = html_value_get(input);
-            await app_a_functionize(a, value_new);
-            await app_a_function_on_change(o, a);
-          }
-          let component = app_a_button_wide(overlay, "Functionize", on_enter);
-        },
-      });
-    }
+    app_a_functionize_end_choice_add(a, choices, o2);
     return choices;
   }
   app_a_overlay_choices(a, keyword, lambda19);
