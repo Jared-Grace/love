d8fb0d1fae7a3c51053a606da5c9684cdd0747b2
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 19:40:44 -0500

    app_a_upload

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -113,7 +113,7 @@ export async function app_g_refresh(
       html_on_load(resolve);
     }
   }
-  let p = promise_wrap(lambda4);
+  let p = await promise_wrap(lambda4);
   async function lambda3() {
     let container = object_property_get(div_map, "container");
     await html_scroll_center_container_now(player_img_c, container);
