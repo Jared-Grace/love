44097b5f99a617b9fac6b517bfd273b4c123a842
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:42:40 -0500

    r function_exists_unalias function_unalias_exists

--- web/public/src/function_exists_not.mjs
+++ web/public/src/function_exists_not.mjs
@@ -1,8 +1,8 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { function_exists_unalias } from "../../../love/public/src/function_exists_unalias.mjs";
+import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 export async function function_exists_not(f_name) {
-  let v = await function_exists_unalias(f_name);
+  let v = await function_unalias_exists(f_name);
   let exists = object_property_get(v, "exists");
   let n = not(exists);
   return n;
