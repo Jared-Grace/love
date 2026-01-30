8994fa7d79c37b36ea959d774bf563eba9609be5
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:42:40 -0500

    r function_exists_unalias function_unalias_exists

--- public/src/app_a_app.mjs
+++ public/src/app_a_app.mjs
@@ -11,7 +11,7 @@ import { app_a_function_select } from "../../../love/public/src/app_a_function_s
 import { app_a_app_run } from "../../../love/public/src/app_a_app_run.mjs";
 import { app_generic_screen_set } from "../../../love/public/src/app_generic_screen_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { function_exists_unalias } from "../../../love/public/src/function_exists_unalias.mjs";
+import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { app_a_app_selected_key } from "../../../love/public/src/app_a_app_selected_key.mjs";
@@ -25,7 +25,7 @@ export async function app_a_app(context) {
   let a_name = storage_local_get_context(context, key);
   let f_name = app_a_function_name_selected(context);
   let combined = app_generic_name_main(a_name);
-  let v = await function_exists_unalias(combined);
+  let v = await function_unalias_exists(combined);
   let unaliased = object_property_get(v, "unaliased");
   const text = app_a_button_function_text(unaliased);
   let choices = [
--- public/src/app_generic_name_main_get.mjs
+++ public/src/app_generic_name_main_get.mjs
@@ -4,12 +4,12 @@ import { list_find_starts_with } from "../../../love/public/src/list_find_starts
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { function_exists_unalias } from "../../../love/public/src/function_exists_unalias.mjs";
+import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
 export async function app_generic_name_main_get(search) {
   marker("1");
   let f_name = app_generic_name_main(search);
-  let v2 = await function_exists_unalias(f_name);
+  let v2 = await function_unalias_exists(f_name);
   let exists = object_property_get(v2, "exists");
   let app_name = search;
   if (not(exists)) {
