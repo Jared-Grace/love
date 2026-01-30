5cbb37c7576216ef62e118c916f4e8af22fdf204
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 19:10:44 -0500

    function_auto_path ..\love\public\src\list_first_last_slice.mjs

--- web/public/src/list_first_last_slice.mjs
+++ web/public/src/list_first_last_slice.mjs
@@ -4,10 +4,10 @@ import { list_slice_from } from "../../../love/public/src/list_slice_from.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
-export function list_first_last_slice(list_first_last, list) {
+export function list_first_last_slice(list_rg, list) {
   marker("1");
-  list_sort_number(list_first_last);
-  let v = list_first_last(list_first_last);
+  list_sort_number(list_rg);
+  let v = list_first_last(list_rg);
   let first = list_first(v);
   let last = list_last(v);
   let sliced = list_slice_from(list, first, last);
