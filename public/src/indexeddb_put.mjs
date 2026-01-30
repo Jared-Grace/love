c3da13ba129ebbe77d85fe5a8147aa12695b7df5
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 19:34:41 -0500

    function_auto_path ..\love\public\src\indexeddb_put.mjs

--- public/src/indexeddb_put.mjs
+++ public/src/indexeddb_put.mjs
@@ -1,11 +1,11 @@
+import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
 import { indexeddb_next } from "../../../love/public/src/indexeddb_next.mjs";
 import { indexeddb_put_backend } from "../../../love/public/src/indexeddb_put_backend.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
-import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { indexeddb_get_all } from "../../../love/public/src/indexeddb_get_all.mjs";
 export async function indexeddb_put(db_get, store, key, value_get) {
   let all = await indexeddb_get_all(db_get, store);
-  let f = list_find_property(all, "key", key);
+  let f = list_find_property_or_null(all, "key", key);
   const next = await value_get(f);
   object_replace(f, next);
   async function lambda_async() {
