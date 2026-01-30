5f51dc467fb370e58a808e0d42fcc577f4b6b4a1
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:07:00 -0500

    function_auto_path ..\love\public\src\html_on_load_wait.mjs

--- web/public/src/html_on_load_wait.mjs
+++ web/public/src/html_on_load_wait.mjs
@@ -1,4 +1,12 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function html_on_load_wait() {
-  marker("1");
+import { promise_wrap } from "../../../love/public/src/promise_wrap.mjs";
+import { html_on_load } from "../../../love/public/src/html_on_load.mjs";
+export async function html_on_load_wait(document, readyState) {
+  function lambda4(resolve, reject) {
+    if (document.readyState === "complete") {
+      resolve();
+    } else {
+      html_on_load(resolve);
+    }
+  }
+  let p = await promise_wrap(lambda4);
 }
