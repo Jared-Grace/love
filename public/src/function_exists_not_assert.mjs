bb2c9d73e4ea50f55552ff9ce158d5c6e1e33a99
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:44:36 -0500

    function_auto_path ..\love\public\src\function_exists_not_assert.mjs

--- public/src/function_exists_not_assert.mjs
+++ public/src/function_exists_not_assert.mjs
@@ -1,6 +1,6 @@
+import { function_unalias_exists_not } from "../../../love/public/src/function_unalias_exists_not.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
-import { function_exists_not } from "../../../love/public/src/function_exists_not.mjs";
 export async function function_exists_not_assert(f_name) {
-  let n = await function_exists_not(f_name);
+  let n = await function_unalias_exists_not(f_name);
   assert(n);
 }
