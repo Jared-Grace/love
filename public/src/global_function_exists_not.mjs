4fbf34e1aeea9c5d16abbcea09709c59b507ecfb
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:17:31 -0500

    fws global_function_exists not

--- public/src/global_function_exists_not.mjs
+++ public/src/global_function_exists_not.mjs
@@ -1,4 +1,6 @@
+import { global_function_exists } from "../../../love/public/src/global_function_exists.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function global_function_exists_not() {
+export function global_function_exists_not(fn) {
   marker("1");
+  return global_function_exists(fn);
 }
