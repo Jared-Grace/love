3a195204c11518edb626d19310471435d6e3d839
Grace-Jared <christrosetolife@gmail.com>
2026-01-29 19:14:48 -0500

    function_auto_path ..\love\public\src\list_slice_from_indices.mjs

--- public/src/list_slice_from_indices.mjs
+++ public/src/list_slice_from_indices.mjs
@@ -1,9 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
 import { list_slice_include } from "../../../love/public/src/list_slice_include.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
 import { list_sort_number } from "../../../love/public/src/list_sort_number.mjs";
 export function list_slice_from_indices(indices, list) {
+  marker("1");
   list_sort_number(indices);
   let v = list_first_last(indices);
   let first = list_first(v);
