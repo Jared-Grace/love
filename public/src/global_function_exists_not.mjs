2fdca65fe30f8d860c038c07f6672460b4dddeed
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:17:33 -0500

    function_auto_path ..\love\public\src\global_function_exists_not.mjs

--- public/src/global_function_exists_not.mjs
+++ public/src/global_function_exists_not.mjs
@@ -2,5 +2,6 @@ import { global_function_exists } from "../../../love/public/src/global_function
 import { marker } from "../../../love/public/src/marker.mjs";
 export function global_function_exists_not(fn) {
   marker("1");
-  return global_function_exists(fn);
+  let exists = global_function_exists(fn);
+  return exists;
 }
