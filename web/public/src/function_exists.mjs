e9343b71302112d482b1508c72173650b278cc7a
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 15:33:31 -0500

    function_auto_path ..\love\public\src\function_exists.mjs

--- web/public/src/function_exists.mjs
+++ web/public/src/function_exists.mjs
@@ -1,7 +1,7 @@
+import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 export async function function_exists(f_name) {
   marker("1");
-  let u = await function_name_to_path_unalias(f_name);
+  let u = await function_name_to_path(f_name);
   return u;
 }
