6be3dde122d88fe6c2d35d7302b68f5e274f7557
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 18:57:59 -0500

    function_auto_path ..\love\public\src\list_filter_property_if_exists.mjs

---
+++ web/public/src/list_filter_property_if_exists.mjs
@@ -0,0 +1,12 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+export function list_filter_property_if_exists(list, property_name) {
+  marker("1");
+  function lambda(item) {
+    let exists = object_property_exists(item, property_name);
+    return exists;
+  }
+  let filtered = list_filter(list, lambda);
+  return filtered;
+}
