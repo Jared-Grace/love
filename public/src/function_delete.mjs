d955693ff7793a046e771e4225fdbb34b7783496
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 17:10:51 -0500

    function_auto_path ..\love\public\src\function_delete.mjs

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -1,3 +1,4 @@
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
@@ -6,6 +7,8 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { file_delete } from "../../../love/public/src/file_delete.mjs";
 export async function function_delete(f_name) {
   marker("1");
+  if (browser_is()) {
+  }
   const v = await function_unalias_exists(f_name);
   let exists = object_property_get(v, "exists");
   let f_path = object_property_get(v, "f_path");
