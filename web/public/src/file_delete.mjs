8b07313b16fa1a963cefcb919b2e9ee088ea4a5e
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 17:12:42 -0500

    function_auto_path ..\love\public\src\file_delete.mjs

--- web/public/src/file_delete.mjs
+++ web/public/src/file_delete.mjs
@@ -1,6 +1,9 @@
+import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 export async function file_delete(file_path) {
   if (browser_is()) {
+    await file_overwrite(file_path, "");
+    return;
   }
   let fs = await import("fs");
   await fs.promises.unlink(file_path);
