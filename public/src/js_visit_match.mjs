cdf66f23853be55b2eaa73ade30a3efd649298fa
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 20:21:02 -0500

    pf ast

--- public/src/js_visit_match.mjs
+++ public/src/js_visit_match.mjs
@@ -1,7 +1,7 @@
 import { js_visit_matches } from "../../../love/public/src/js_visit_matches.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_visit_match(ast, node_search) {
-  let matches = js_visit_matches(node_search, ast);
+  let matches = js_visit_matches(ast, node_search);
   let v_match = list_single(matches);
   return v_match;
 }
