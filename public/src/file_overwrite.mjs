0d3c793bca94cd8b5008bd59f3246eaf1eebc446
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 17:09:55 -0500

    function_auto_path ..\love\public\src\file_overwrite.mjs

--- public/src/file_overwrite.mjs
+++ public/src/file_overwrite.mjs
@@ -9,9 +9,9 @@ export async function file_overwrite(file_path, contents) {
   let exists = global_function_property_exists(file_read_cached, file_path);
   if (exists) {
     global_function_property_set(file_read_cached, file_path, contents);
-    if (not(b)) {
-    }
     return;
   }
-  await file_overwrite_uncached(file_path, contents);
+  if (not(exists)) {
+    await file_overwrite_uncached(file_path, contents);
+  }
 }
