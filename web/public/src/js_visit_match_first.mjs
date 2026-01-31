8f2c56af5c308bcc5553b9438df0464b9acdf03a
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 20:21:21 -0500

    function_auto_path ..\love\public\src\js_visit_match_first.mjs

--- web/public/src/js_visit_match_first.mjs
+++ web/public/src/js_visit_match_first.mjs
@@ -1,9 +1,9 @@
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { js_visit_matches } from "../../../love/public/src/js_visit_matches.mjs";
-import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_visit_match_first(ast, node_search) {
   marker("1");
   let matches = js_visit_matches(ast, node_search);
-  let v_match = list_single(matches);
+  let v_match = list_first(matches);
   return v_match;
 }
