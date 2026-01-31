02f1f03af173553bd840b6b76acfcb60d3ff8ae6
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 20:33:24 -0500

    function_auto_path ..\love\public\src\file_overwrite_uncached.mjs

--- public/src/file_overwrite_uncached.mjs
+++ public/src/file_overwrite_uncached.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { data_file_update } from "../../../love/public/src/data_file_update.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
@@ -34,6 +35,11 @@ export async function file_overwrite_uncached(file_path, contents) {
       } else {
         let compressed_before = object_property_get(previous, p);
         f = await json_decompress(compressed_before);
+      }
+      if (created) {
+        log({
+          f,
+        });
       }
       let list = object_property_get(f, "versions");
       list_add(list, contents);
