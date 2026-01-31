91a06ad174abbb8b94026f034dbb9d696fc8464b
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 17:12:24 -0500

    function_auto_path ..\love\public\src\file_delete.mjs

--- public/src/file_delete.mjs
+++ public/src/file_delete.mjs
@@ -1,4 +1,7 @@
+import { browser_is } from "../../../love/public/src/browser_is.mjs";
 export async function file_delete(file_path) {
+  if (browser_is()) {
+  }
   let fs = await import("fs");
   await fs.promises.unlink(file_path);
 }
