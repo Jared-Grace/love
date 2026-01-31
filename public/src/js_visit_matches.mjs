97789ea5c80d3c098daa68fa645bbe2ab51aaab6
Grace-Jared <christrosetolife@gmail.com>
2026-01-30 20:20:52 -0500

    function_auto_path ..\love\public\src\js_visit_match.mjs

---
+++ public/src/js_visit_matches.mjs
@@ -0,0 +1,18 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
+import { js_visit } from "../../../love/public/src/js_visit.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function js_visit_matches(node_search, ast) {
+  marker("1");
+  function lambda2(la) {
+    function lambda(v) {
+      let node = object_property_get(v, "node");
+      if (node === node_search) {
+        la(v);
+      }
+    }
+    js_visit(ast, lambda);
+  }
+  let matches = list_adder(lambda2);
+  return matches;
+}
