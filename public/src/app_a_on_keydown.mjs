d319453e4974bb63e71409a08d2206ea1b0a37dd
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:24:22 -0500

    function_auto_path ..\love\public\src\app_a_on_keydown.mjs

--- public/src/app_a_on_keydown.mjs
+++ public/src/app_a_on_keydown.mjs
@@ -1,3 +1,4 @@
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { app_a_shortcuts_each } from "../../../love/public/src/app_a_shortcuts_each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -5,6 +6,7 @@ import { html_on_keydown_stop_logic } from "../../../love/public/src/html_on_key
 export function app_a_on_keydown(e, choices) {
   html_on_keydown_stop_logic(e);
   let key = object_property_get(e, "key");
+  let filtered = list_filter(list, function lambda(item) {});
   app_a_shortcuts_each(choices, on_choice);
   function on_choice(shortcut, text, fn) {
     if (equal(key, shortcut)) {
