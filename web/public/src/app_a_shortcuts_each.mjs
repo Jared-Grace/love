20afd984a02a33fd41726289e85c8f27ac692beb
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:23:28 -0500

    function_auto_path ..\love\public\src\app_a_shortcuts_each.mjs

--- web/public/src/app_a_shortcuts_each.mjs
+++ web/public/src/app_a_shortcuts_each.mjs
@@ -3,11 +3,11 @@ import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function app_a_shortcuts_each(choices, on_choice) {
   marker("1");
-  function lambda21(c) {
+  function on_each(c) {
     let shortcut = object_property_get(c, "shortcut");
     let text = object_property_get(c, "text");
     let fn = object_property_get(c, "fn");
     let b = on_choice(shortcut, text, fn);
   }
-  each(choices, lambda21);
+  each(choices, on_each);
 }
