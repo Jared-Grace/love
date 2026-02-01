360bea362ab31a2bdcaf825d2739014f7c41cbf3
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:33:52 -0500

    function_auto_path ..\love\public\src\app_a_functions_overlay.mjs

--- web/public/src/app_a_functions_overlay.mjs
+++ web/public/src/app_a_functions_overlay.mjs
@@ -1,23 +1,10 @@
-import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
-import { app_a_button_function } from "../../../love/public/src/app_a_button_function.mjs";
-import { object_copy_assign } from "../../../love/public/src/object_copy_assign.mjs";
-import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
+import { app_a_functions_overlay_first } from "../../../love/public/src/app_a_functions_overlay_first.mjs";
+import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
 export async function app_a_functions_overlay(a, lambda$f_name) {
-  let overlay_result = app_a_overlay(a);
-  let overlay = object_property_get(overlay_result, "overlay");
-  html_style_set(overlay, "overflow", "hidden");
-  const replacement = {
-    root: overlay,
-  };
-  let context = object_property_get(a, "context");
-  let context_copy = object_copy_assign(context, replacement);
-  function lambda3() {
-    let overlay_close = object_property_get(overlay_result, "overlay_close");
-    overlay_close();
-  }
-  app_a_button_function(context, overlay, lambda3);
+  let v2 = app_a_functions_overlay_first(a);
+  let overlay_result = object_property_get(v2, "overlay_result");
+  let context_copy = object_property_get(v2, "context_copy");
   let functions_result = await app_a_functions_generic(
     context_copy,
     lambda$f_name,
