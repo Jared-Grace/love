ecbf3118ec52caba951e749af7f4321d8ce5ec7b
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:55:33 -0500

    pd html_on_load_wait document

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -35,7 +35,7 @@ export async function app_g_refresh(
     display: "grid",
   });
   async function refresh() {
-    await app_g_refresh(rows, map);
+    await app_g_refresh(map);
   }
   let npcs = object_property_get(map, "npcs");
   marker("1");
