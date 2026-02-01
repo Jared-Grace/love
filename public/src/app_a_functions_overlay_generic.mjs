a8a5ff14e2144d17e74fe687708cfe9e629ab18c
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:37:28 -0500

    function_auto_path ..\love\public\src\app_a_functions_overlay_generic.mjs

--- public/src/app_a_functions_overlay_generic.mjs
+++ public/src/app_a_functions_overlay_generic.mjs
@@ -4,7 +4,7 @@ import { object_copy_assign } from "../../../love/public/src/object_copy_assign.
 import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
-export function app_a_functions_overlay_generic(a, f_names, lambda$f_name) {
+export function app_a_functions_overlay_generic(a, texts, lambda$text) {
   let overlay_result = app_a_overlay(a);
   let overlay = object_property_get(overlay_result, "overlay");
   html_style_set(overlay, "overflow", "hidden");
@@ -21,8 +21,8 @@ export function app_a_functions_overlay_generic(a, f_names, lambda$f_name) {
   let functions_result = app_a_list_chooser(
     copy,
     "function",
-    f_names,
-    lambda$f_name,
+    texts,
+    lambda$text,
   );
   let v = {
     overlay_result,
