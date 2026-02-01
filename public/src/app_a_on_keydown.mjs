1dbd3e6ffcff27e8e3cbd6f8ebc349e2c99ef330
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:41:48 -0500

    function_auto_path ..\love\public\src\app_a_on_keydown.mjs

--- public/src/app_a_on_keydown.mjs
+++ public/src/app_a_on_keydown.mjs
@@ -1,4 +1,3 @@
-import { list_to_dictionary_property } from "../../../love/public/src/list_to_dictionary_property.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { app_a_shortcuts_each } from "../../../love/public/src/app_a_shortcuts_each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -6,8 +5,6 @@ import { html_on_keydown_stop_logic } from "../../../love/public/src/html_on_key
 export function app_a_on_keydown(e, choices) {
   html_on_keydown_stop_logic(e);
   let key = object_property_get(e, "key");
-  ("this makes sure there is at most one choice per shortcut");
-  list_to_dictionary_property(choices, "shortcut");
   app_a_shortcuts_each(choices, on_choice);
   function on_choice(shortcut, text, fn) {
     if (equal(key, shortcut)) {
