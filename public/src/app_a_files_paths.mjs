bc2def828a24863cf79bfe10849544699f6884a1
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 19:01:04 -0500

    function_auto_path ..\love\public\src\app_a_files_paths.mjs

--- public/src/app_a_files_paths.mjs
+++ public/src/app_a_files_paths.mjs
@@ -1,3 +1,4 @@
+import { list_filter_property_exclude_if_exists } from "../../../love/public/src/list_filter_property_exclude_if_exists.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_a_indexeddb_path_key } from "../../../love/public/src/app_a_indexeddb_path_key.mjs";
 import { app_a_indexeddb_initialize } from "../../../love/public/src/app_a_indexeddb_initialize.mjs";
@@ -6,6 +7,11 @@ import { app_a_file_system_store } from "../../../love/public/src/app_a_file_sys
 export async function app_a_files_paths() {
   let store = app_a_file_system_store();
   let all = await indexeddb_get_all(app_a_indexeddb_initialize, store);
+  let filtered = list_filter_property_exclude_if_exists(
+    list,
+    property_name2,
+    value,
+  );
   let property_name = app_a_indexeddb_path_key();
   let files_paths = list_map_property(all, property_name);
   return files_paths;
