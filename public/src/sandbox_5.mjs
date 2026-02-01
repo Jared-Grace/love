369066a68b22a90a07cbe0ab75d6f4d2bc12587c
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 19:34:32 -0500

    de s5

--- public/src/sandbox_5.mjs
+++
@@ -1,9 +0,0 @@
-import { s7 } from "../../../love/public/src/s7.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export async function sandbox_5() {
-  marker("1");
-  log(1);
-  s7();
-  log(4);
-}
