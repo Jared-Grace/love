3028101bdec8ec4137b578d006bf875fac89cac9
Grace-Jared <christrosetolife@gmail.com>
2026-01-31 19:33:03 -0500

    function_auto_path ..\love\public\src\function_aliases.mjs

--- public/src/function_aliases.mjs
+++ public/src/function_aliases.mjs
@@ -1,5 +1,9 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
 import { function_alias_add_generic } from "../../../love/public/src/function_alias_add_generic.mjs";
 export async function function_aliases() {
-  let { aliases } = await function_alias_add_generic("");
+  marker("1");
+  let v = await function_alias_add_generic("");
+  let aliases = object_property_get(v, "aliases");
   return aliases;
 }
