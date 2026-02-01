ae517a6143c0aac0df633addfe502939fe83e7ca
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 14:37:32 -0500

    function_auto_path ..\love\public\src\app_a_functions_overlay.mjs

--- public/src/app_a_functions_overlay.mjs
+++ public/src/app_a_functions_overlay.mjs
@@ -1,7 +1,7 @@
 import { app_a_functions_overlay_generic } from "../../../love/public/src/app_a_functions_overlay_generic.mjs";
 import { app_a_functions_generic_f_names } from "../../../love/public/src/app_a_functions_generic_f_names.mjs";
-export async function app_a_functions_overlay(a, on_select) {
+export async function app_a_functions_overlay(a, lambda$text) {
   let f_names = await app_a_functions_generic_f_names();
-  let v = app_a_functions_overlay_generic(a, f_names, on_select);
+  let v = app_a_functions_overlay_generic(a, f_names, lambda$text);
   return v;
 }
