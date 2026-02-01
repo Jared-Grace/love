c3d64824a6251c7dd84315ac2542acaa432d6fab
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 15:07:01 -0500

    function_auto_path ..\love\public\src\function_params_new_generic.mjs

--- web/public/src/function_params_new_generic.mjs
+++ web/public/src/function_params_new_generic.mjs
@@ -19,14 +19,14 @@ export async function function_params_new_generic(
     result,
   });
   let properties = object_properties(result);
-  async function lambda4(f_name) {
-    let v = await function_unalias_exists(f_name);
+  async function lambda4(f_name_caller) {
+    let v = await function_unalias_exists(f_name_caller);
     let exists = object_property_get(v, "exists");
     if (not(exists)) {
       return;
     }
     async function lambda5(ast) {
-      js_visit_calls_named(ast, f_name, lambda);
+      js_visit_calls_named(ast, f_name_caller, lambda);
       function lambda({ args, v }) {
         let node = object_property_get(v, "node");
         log({
@@ -37,7 +37,7 @@ export async function function_params_new_generic(
         on_call(args);
       }
     }
-    let output = await function_transform(f_name, lambda5);
+    let output = await function_transform(f_name_caller, lambda5);
   }
   await each_async(properties, lambda4);
   await function_transform(f_name, function_transform_current_lambda);
