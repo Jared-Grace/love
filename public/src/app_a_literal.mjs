fb9c3d7f78f7d134215c2885657322e130463e9e
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 15:50:34 -0500

    function_auto_path ..\love\public\src\app_a_literal.mjs

--- public/src/app_a_literal.mjs
+++ public/src/app_a_literal.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_identifier_generic } from "../../../love/public/src/app_a_identifier_generic.mjs";
 export function app_a_literal(a, component, node, on_change, raw) {
   marker("1");
+  log({
+    node,
+  });
   app_a_identifier_generic(
     a,
     component,
