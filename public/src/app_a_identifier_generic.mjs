312cf17fd9bd4d8f6fade9df161f8db9eeb8a0f1
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 00:01:18 -0500

    function_auto_path ..\love\public\src\app_a_identifier_generic.mjs

--- public/src/app_a_identifier_generic.mjs
+++ public/src/app_a_identifier_generic.mjs
@@ -43,7 +43,7 @@ export function app_a_identifier_generic(
   async function on_click(o3) {
     let node = object_property_get(a, "node");
     let ast = object_property_get(a, "ast");
-    let v_match = js_visit_match_first(ast2, node_search);
+    let v_match = js_visit_match_first(ast, node);
     let overlay_close = object_property_get(o3, "overlay_close");
     let overlay = object_property_get(o3, "overlay");
     let oc = app_a_overlay_container_centered(overlay);
