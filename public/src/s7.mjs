b624377967fd84f61a0d160b5b7d1b17c35bac00
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 18:24:57 -0500

    de s7

--- public/src/s7.mjs
+++
@@ -1,9 +0,0 @@
-import { log } from "../../../love/public/src/log.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function s7() {
-  marker("1");
-  log("1");
-  log("2 hi");
-  log("3");
-  log("4");
-}
