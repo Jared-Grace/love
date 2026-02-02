bf442dde3179dbb05c822f6801c032e67d102f15
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 17:54:09 -0500

    app_a_upload

--- public/src/s7.mjs
+++ public/src/s7.mjs
@@ -2,10 +2,8 @@ import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function s7() {
   marker("1");
-  function lambda() {
-    log("1");
-    log("2 hi");
-  }
+  log("1");
+  log("2 hi");
   function lambda2() {
     log("3");
     log("4");
