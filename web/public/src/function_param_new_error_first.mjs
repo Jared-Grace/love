f73fcd6ebf69633562dec4815ef58eb788a95f8a
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:53:37 -0500

    function_auto_path ..\love\public\src\function_param_new_error_first.mjs

---
+++ web/public/src/function_param_new_error_first.mjs
@@ -0,0 +1,9 @@
+import { function_param_move_first } from "../../../love/public/src/function_param_move_first.mjs";
+import { function_param_new_error } from "../../../love/public/src/function_param_new_error.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export async function function_param_new_error_first(f_name, param_name) {
+  marker("1");
+  let v = await function_param_new_error(f_name, param_name);
+  await function_param_move_first(f_name, param_name);
+  return v;
+}
