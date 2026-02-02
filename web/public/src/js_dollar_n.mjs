96d459881d13550525a6d6a2bc67bf5db56e00cf
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 13:21:26 -0500

    function_auto_path ..\love\public\src\js_dollar_n.mjs

--- web/public/src/js_dollar_n.mjs
+++ web/public/src/js_dollar_n.mjs
@@ -22,12 +22,12 @@ export function js_dollar_n({
   let n = list_next(stack2, stack1);
   let test = object_property_get(n, "test");
   function lambda2() {
-    let name = js_call_callee_name(node);
+    let name = js_call_callee_name(test);
     log({
       name,
     });
     if (equal(name, not.name)) {
-      let arguments2 = object_property_get(node, "arguments");
+      let arguments2 = object_property_get(test, "arguments");
       let only = list_single(arguments2);
       object_property_set(n, "test", only);
       const p1 = "alternate";
