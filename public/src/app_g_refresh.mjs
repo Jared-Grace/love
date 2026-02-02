43674987d40679467bbe16ae4330801a7063b182
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:02:28 -0500

    app_a_upload

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -105,9 +105,7 @@ export async function app_g_refresh(
       refresh,
     );
   }
-  async function lambda3() {
-    let container = object_property_get(div_map, "container");
-    await html_scroll_center_container_now(player_img_c, container);
-  }
+  let container = object_property_get(div_map, "container");
+  await html_scroll_center_container_now(player_img_c, container);
   await html_on_load_wait();
 }
