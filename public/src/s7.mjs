4b6d021a77d20a5389c6d9935931b4becff7c2c6
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 17:52:18 -0500

    function_auto_path ..\love\public\src\s7.mjs

--- public/src/s7.mjs
+++ public/src/s7.mjs
@@ -1,5 +1,8 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function s7() {
   marker("1");
-  function lambda() {}
+  function lambda() {
+    log(message);
+  }
 }
