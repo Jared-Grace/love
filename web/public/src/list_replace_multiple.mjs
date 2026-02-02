41791d7e76ae6196fb05caaa156c2bb4ad555c74
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:40:34 -0500

    function_auto_path ..\love\public\src\list_replace_multiple.mjs

--- web/public/src/list_replace_multiple.mjs
+++ web/public/src/list_replace_multiple.mjs
@@ -1,8 +1,8 @@
 import { list_insert_at_multiple } from "../../../love/public/src/list_insert_at_multiple.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
-export function list_replace_multiple(e1, node, body_block) {
-  let index = list_index_of(e1, node);
-  list_remove(e1, node);
-  list_insert_at_multiple(body_block, e1, index);
+export function list_replace_multiple(list, item, list_replacement) {
+  let index = list_index_of(list, item);
+  list_remove(list, item);
+  list_insert_at_multiple(list_replacement, list, index);
 }
