5feec4e197ea31000b3b71c70b2f227c925ea86f
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:54:24 -0500

    pnef function_param_move_last f_name

--- web/public/src/function_param_move_last.mjs
+++ web/public/src/function_param_move_last.mjs
@@ -2,7 +2,7 @@ import { function_param_move_generic } from "../../../love/public/src/function_p
 import { list_move_last } from "../../../love/public/src/list_move_last.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-export async function function_param_move_last(param_name) {
+export async function function_param_move_last(f_name, param_name) {
   marker("1");
   assert_arguments(arguments, 1);
   let fn = list_move_last;
