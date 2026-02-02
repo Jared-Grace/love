a89c6cd7d1389010da537b29cc228737ad2abdff
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:47:49 -0500

    function_auto_path ..\love\public\src\function_param_move_first.mjs

--- web/public/src/function_param_move_first.mjs
+++ web/public/src/function_param_move_first.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { function_param_move_generic } from "../../../love/public/src/function_param_move_generic.mjs";
 import { list_move_first } from "../../../love/public/src/list_move_first.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,5 +7,6 @@ export async function function_param_move_first(param_name) {
   marker("1");
   assert_arguments(arguments, 1);
   let fn = list_move_first;
-  await function_param_move_generic(fn, param_name, error());
+  let f_name = error();
+  await function_param_move_generic(fn, param_name, f_name);
 }
