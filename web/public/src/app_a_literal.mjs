e19ba27f8e45875a9031906bdff596d1ae6014df
Grace-Jared <christrosetolife@gmail.com>
2026-02-01 19:50:09 -0500

    function_auto_path ..\love\public\src\app_a_literal.mjs

--- web/public/src/app_a_literal.mjs
+++ web/public/src/app_a_literal.mjs
@@ -1,3 +1,4 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 import { string_is } from "../../../love/public/src/string_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -5,7 +6,9 @@ import { marker } from "../../../love/public/src/marker.mjs";
 import { app_a_identifier_generic } from "../../../love/public/src/app_a_identifier_generic.mjs";
 export function app_a_literal(a, component, node, on_change, raw) {
   marker("1");
-  let type_is = js_node_type_is(node2, type);
+  let type_is = js_node_type_is(node, "TemplateLiteral");
+  if (not(b)) {
+  }
   let value = object_property_get(node, "value");
   let si2 = string_is(value);
   app_a_identifier_generic(
