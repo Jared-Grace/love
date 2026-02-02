9ce722fc8226288523cf8f9fcd09f5a376864ceb
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 13:13:57 -0500

    function_auto_path ..\love\public\src\js_dollar_n.mjs

--- public/src/js_dollar_n.mjs
+++ public/src/js_dollar_n.mjs
@@ -1,3 +1,4 @@
+import { list_next } from "../../../love/public/src/list_next.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_dollar_n({
@@ -10,8 +11,9 @@ export function js_dollar_n({
   afters,
 }) {
   marker("1");
+  let n = list_next(stack2, stack1);
   log({
-    node,
+    n,
   });
   return;
 }
