030c66624fdb305578b689d33bee1e6a4fb1bc8f
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:47:33 -0500

    function_auto_path ..\love\public\src\function_param_new_error.mjs

--- public/src/function_param_new_error.mjs
+++ public/src/function_param_new_error.mjs
@@ -2,9 +2,8 @@ import { js_code_call } from "../../../love/public/src/js_code_call.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
 import { error } from "./error.mjs";
-export async function function_param_new_error(param_name) {
+export async function function_param_new_error(f_name, param_name) {
   marker("1");
   let values_default_comma = js_code_call(error.name);
-  let f_name = error();
   await function_params_new(param_name, values_default_comma, f_name);
 }
