f37ebb84642e2124ac9f5e05e5dd388c18803717
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:47:50 -0500

    function_auto_path ..\love\public\src\function_param_move_last.mjs

--- public/src/function_param_move_last.mjs
+++ public/src/function_param_move_last.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { function_param_move_generic } from "../../../love/public/src/function_param_move_generic.mjs";
 import { list_move_last } from "../../../love/public/src/list_move_last.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,5 +7,6 @@ export async function function_param_move_last(param_name) {
   marker("1");
   assert_arguments(arguments, 1);
   let fn = list_move_last;
-  await function_param_move_generic(fn, param_name, error());
+  let f_name = error();
+  await function_param_move_generic(fn, param_name, f_name);
 }
