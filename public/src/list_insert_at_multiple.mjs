d4c60bcea8a68ee5e2e9e6f68487ed053200e751
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 16:39:54 -0500

    function_auto_path ..\love\public\src\app_a_function_declaration.mjs

---
+++ public/src/list_insert_at_multiple.mjs
@@ -0,0 +1,10 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+import { list_copy_reverse } from "../../../love/public/src/list_copy_reverse.mjs";
+export function list_insert_at_multiple(body_block, e1, index) {
+  let copy = list_copy_reverse(body_block);
+  function lambda5(item) {
+    list_insert(e1, index, item);
+  }
+  each(copy, lambda5);
+}
