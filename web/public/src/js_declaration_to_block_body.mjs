7ed30d1e6b85cc34618188941c40ad1642b0bb25
Grace-Jared <christrosetolife@gmail.com>
2026-02-02 15:43:10 -0500

    function_auto_path ..\love\public\src\js_declaration_to_block_body.mjs

--- web/public/src/js_declaration_to_block_body.mjs
+++ web/public/src/js_declaration_to_block_body.mjs
@@ -1,5 +1,6 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function js_declaration_to_block_body(declaration) {
-  let { body: block } = declaration;
-  let { body: body_block } = block;
+  let block = object_property_get(declaration, "body");
+  let body_block = object_property_get(block, "body");
   return body_block;
 }
