9a3f4cb0770c7190fdb57ade87b988688230d47a
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 16:51:27 -0500

    fws ebible_version_upload_refresh all

--- public/src/ebible_version_upload_refresh_all.mjs
+++ public/src/ebible_version_upload_refresh_all.mjs
@@ -1,4 +1,6 @@
+import { ebible_version_upload_refresh } from "../../../love/public/src/ebible_version_upload_refresh.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_version_upload_refresh_all() {
+export async function ebible_version_upload_refresh_all(bible_folder) {
   marker("1");
+  return await ebible_version_upload_refresh(bible_folder);
 }
