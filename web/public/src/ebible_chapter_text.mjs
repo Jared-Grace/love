c0fe77e9480075ed2313d8f34ed5049cceab7fd2
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 18:41:56 -0500

    function_auto_path ..\love\public\src\ebible_chapter_text.mjs

--- web/public/src/ebible_chapter_text.mjs
+++ web/public/src/ebible_chapter_text.mjs
@@ -1,3 +1,13 @@
+import { whitespace_normalize } from "../../../love/public/src/whitespace_normalize.mjs";
+import { roman_to_integer } from "../../../love/public/src/roman_to_integer.mjs";
+import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
+import { html_parse_attr } from "../../../love/public/src/html_parse_attr.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { html_parse_find_list_to } from "../../../love/public/src/html_parse_find_list_to.mjs";
+import { html_parse_find } from "../../../love/public/src/html_parse_find.mjs";
+import { html_parse_read } from "../../../love/public/src/html_parse_read.mjs";
+import { ebible_version_download_path_combine } from "../../../love/public/src/ebible_version_download_path_combine.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { html_parse_find_remove } from "../../../love/public/src/html_parse_find_remove.mjs";
 import { css_class_prefix_combine } from "../../../love/public/src/css_class_prefix_combine.mjs";
@@ -9,10 +19,37 @@ import { ebible_verses_exclude } from "../../../love/public/src/ebible_verses_ex
 import { ebible_verses_include } from "../../../love/public/src/ebible_verses_include.mjs";
 import { html_parse_descendants_classes } from "../../../love/public/src/html_parse_descendants_classes.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { ebible_chapter_verse_numbers } from "../../../love/public/src/ebible_chapter_verse_numbers.mjs";
 import { html_parse_text } from "../../../love/public/src/html_parse_text.mjs";
 export async function ebible_chapter_text(bible_folder, chapter_code) {
-  let v = await ebible_chapter_verse_numbers(bible_folder, chapter_code);
+  marker("1");
+  let chapter_path = ebible_version_download_path_combine(
+    bible_folder,
+    chapter_code,
+  );
+  let v2 = await html_parse_read(chapter_path);
+  let root = object_property_get(v2, "root");
+  let d2 = object_property_get(v2, "d");
+  let main2 = html_parse_find(root, ".main");
+  let list = html_parse_find_list_to(main2, ".verse");
+  let verse_numbers2 = list_map(list, lambda2);
+  function lambda2(item2) {
+    let t = html_parse_text(d2, item2);
+    const name = "id";
+    let id = html_parse_attr(d2, item2, name);
+    let without = string_prefix_without(id, "V");
+    let i = roman_to_integer(without);
+    let n = whitespace_normalize(t);
+    let v4 = {
+      number: i,
+      name: n,
+    };
+    return v4;
+  }
+  let v = {
+    d2,
+    main2,
+    verse_numbers2,
+  };
   let verse_numbers = object_property_get(v, "verse_numbers");
   let main = object_property_get(v, "main");
   let d = object_property_get(v, "d");
