9a33fc8390fee905d5176e2f633a61d99ffec8ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 17:02:00 -0500

    function_auto_path ..\love\public\src\ebible_languages_english_each.mjs

--- public/src/ebible_languages_english_each.mjs
+++ public/src/ebible_languages_english_each.mjs
@@ -1,3 +1,4 @@
+import { list_remove_property } from "../../../love/public/src/list_remove_property.mjs";
 import { ebible_languages_without_original } from "../../../love/public/src/ebible_languages_without_original.mjs";
 import { ebible_versions_english_choices_each } from "../../../love/public/src/ebible_versions_english_choices_each.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -6,9 +7,10 @@ import { ebible_folder_english } from "../../../love/public/src/ebible_folder_en
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function ebible_languages_english_each(lambda$bible_folder) {
   let languages = ebible_languages_without_original();
+  let v2 = ebible_folder_english();
+  list_remove_property(languages, "language_code", original);
   async function lambda2(language) {
     let bible_folder = object_property_get(language, "bible_folder");
-    let v2 = ebible_folder_english();
     let skips = [v2];
     let includes = list_includes(skips, bible_folder);
     if (includes) {
