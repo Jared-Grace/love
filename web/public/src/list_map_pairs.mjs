69390615ddc3603af4a4ab0942688c21cc80c3fc
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 16:11:20 -0500

    function_auto_path ..\love\public\src\list_map_pairs.mjs

--- web/public/src/list_map_pairs.mjs
+++ web/public/src/list_map_pairs.mjs
@@ -2,8 +2,8 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 export function list_map_pairs(list_a, list_b, mapper) {
   function lambda2(la) {
-    function lambda(nn, text) {
-      const v = mapper(nn, text);
+    function lambda(a, b) {
+      const v = mapper(a, b);
       la(v);
     }
     each_pair(list_a, list_b, lambda);
