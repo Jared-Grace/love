3c72ef768ba59f3f8dbe965bd140c88b8064131e
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 14:59:12 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

--- public/src/ebible_verses_readaloud.mjs
+++ public/src/ebible_verses_readaloud.mjs
@@ -1,9 +1,9 @@
+import { ebible_version_readaloud_download_path } from "../../../love/public/src/ebible_version_readaloud_download_path.mjs";
 import { list_find_includes } from "../../../love/public/src/list_find_includes.mjs";
 import { ebible_chapter_code_to_name_code } from "../../../love/public/src/ebible_chapter_code_to_name_code.mjs";
 import { ebible_chapter_code_to_book } from "../../../love/public/src/ebible_chapter_code_to_book.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
 import { ebible_chapter_verse_numbers } from "../../../love/public/src/ebible_chapter_verse_numbers.mjs";
 import { ebible_verses_browser } from "../../../love/public/src/ebible_verses_browser.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
@@ -15,7 +15,7 @@ export async function ebible_verses_readaloud(bible_folder, chapter_code) {
     return verses;
   }
   marker("1");
-  let file_path = await ebible_version_readaloud_download(bible_folder);
+  let file_path = await ebible_version_readaloud_download_path(bible_folder);
   let files = await folder_read_files(file_path);
   let book_code = ebible_chapter_code_to_book(chapter_code);
   let name_code = ebible_chapter_code_to_name_code(chapter_code);
