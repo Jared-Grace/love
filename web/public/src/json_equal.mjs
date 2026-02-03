cd8721198d3a443a7bc044058f5da9b11c3d9293
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 16:16:55 -0500

    function_auto_path ..\love\public\src\json_equal.mjs

--- web/public/src/json_equal.mjs
+++ web/public/src/json_equal.mjs
@@ -1,6 +1,6 @@
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { equal_by } from "../../../love/public/src/equal_by.mjs";
-export function json_equal(left, sliced) {
-  let eq2 = equal_by(left, sliced, json_to);
+export function json_equal(left, right) {
+  let eq2 = equal_by(left, right, json_to);
   return eq2;
 }
