41058292392bee2d32b75311e70fb93b08cf6dfb
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 16:23:38 -0500

    pb ebible_verse_new_text

--- public/src/ebible_verse_new.mjs
+++ public/src/ebible_verse_new.mjs
@@ -4,6 +4,6 @@ import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 export function ebible_verse_new(tokens, verse_number) {
   marker("1");
   let joined = list_join_space(tokens);
-  const v = ebible_verse_new_text(verse_number, joined);
+  const v = ebible_verse_new_text(joined, verse_number);
   return v;
 }
