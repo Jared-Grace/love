e99f780819590d37a023d1640cb7e59e95b5b88d
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 14:56:22 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

---
+++ web/public/src/list_find_includes.mjs
@@ -0,0 +1,7 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
+import { list_filter_includes } from "../../../love/public/src/list_filter_includes.mjs";
+export function list_find_includes(files, search) {
+  let filtered = list_filter_includes(files, search);
+  let only = list_single(filtered);
+  return only;
+}
