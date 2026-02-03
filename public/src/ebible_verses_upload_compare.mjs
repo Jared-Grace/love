4ce0942d9de72791e6237bdc894e134dd789d685
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 16:21:09 -0500

    function_auto_path ..\love\public\src\ebible_verses_upload_compare.mjs

--- public/src/ebible_verses_upload_compare.mjs
+++ public/src/ebible_verses_upload_compare.mjs
@@ -3,9 +3,6 @@ import { json_equal_not } from "../../../love/public/src/json_equal_not.mjs";
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
-import { retry } from "../../../love/public/src/retry.mjs";
-import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
-import { ebible_firebase_upload_verse } from "../../../love/public/src/ebible_firebase_upload_verse.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
@@ -28,12 +25,5 @@ export async function ebible_verses_upload_compare(bible_folder) {
     }
     each_pair(verses_readaloud, verses, lambda2);
     return;
-    async function lambda(v) {
-      async function lambda3() {
-        await ebible_firebase_upload_verse(v, chapter_code, bible_folder);
-      }
-      await retry(5, lambda3);
-    }
-    await each_unordered_async(verses_readaloud, lambda);
   }
 }
