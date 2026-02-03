29c27d2d52ebeabd59fd04fc4f1c0faca311cd3d
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 14:44:14 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

--- public/src/ebible_verses_readaloud.mjs
+++ public/src/ebible_verses_readaloud.mjs
@@ -1,3 +1,4 @@
+import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
 import { ebible_chapter_verse_numbers } from "../../../love/public/src/ebible_chapter_verse_numbers.mjs";
 import { ebible_verses_browser } from "../../../love/public/src/ebible_verses_browser.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
@@ -9,6 +10,7 @@ export async function ebible_verses_readaloud(bible_folder, chapter_code) {
     return verses;
   }
   marker("1");
+  let file_path = await ebible_version_readaloud_download(bible_folder2);
   let v2 = await ebible_chapter_verse_numbers(bible_folder, chapter_code);
   let verse_numbers = object_property_get(v2, "verse_numbers");
   return list;
