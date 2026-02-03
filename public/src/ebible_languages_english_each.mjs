407de9611767773fbeaa4d6e42244f5745742a93
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 17:02:30 -0500

    function_auto_path ..\love\public\src\ebible_languages_english_each.mjs

--- public/src/ebible_languages_english_each.mjs
+++ public/src/ebible_languages_english_each.mjs
@@ -3,11 +3,13 @@ import { ebible_languages_without_original } from "../../../love/public/src/ebib
 import { ebible_versions_english_choices_each } from "../../../love/public/src/ebible_versions_english_choices_each.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function ebible_languages_english_each(lambda$bible_folder) {
   let languages = ebible_languages_without_original();
   let e = ebible_folder_english();
   list_remove_property(languages, "language_code", e);
   async function lambda2(language) {
+    let bible_folder = object_property_get(language, "bible_folder");
     await lambda$bible_folder(bible_folder);
   }
   await each_async(languages, lambda2);
