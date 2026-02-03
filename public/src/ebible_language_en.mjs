aa94af72cc71fbac6eff1bf93885e184801e0d6d
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 17:05:20 -0500

    function_auto_path ..\love\public\src\ebible_language_en.mjs

--- public/src/ebible_language_en.mjs
+++ public/src/ebible_language_en.mjs
@@ -1,10 +1,11 @@
+import { ebible_language_en_code } from "../../../love/public/src/ebible_language_en_code.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { ebible_language_en_name } from "../../../love/public/src/ebible_language_en_name.mjs";
 export function ebible_language_en() {
   let v2 = {
     name: ebible_language_en_name(),
     bible_folder: ebible_folder_english(),
-    language_code: "en",
+    language_code: ebible_language_en_code(),
   };
   return v2;
 }
