a0380069a3c3d11bf9c356e26f3b21b4730464c1
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 16:10:13 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

--- public/src/ebible_verses_readaloud.mjs
+++ public/src/ebible_verses_readaloud.mjs
@@ -1,6 +1,5 @@
+import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
 import { ebible_verse_new } from "../../../love/public/src/ebible_verse_new.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { each_pair } from "../../../love/public/src/each_pair.mjs";
 import { string_trim } from "../../../love/public/src/string_trim.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
@@ -37,17 +36,14 @@ export async function ebible_verses_readaloud(bible_folder, chapter_code) {
   let skipped = list_skip(lines, 2);
   let mapped = list_map(skipped, string_trim);
   let filtered = list_filter_empty_not_is(mapped);
-  function lambda2(la) {
-    function lambda(nn, text) {
-      let number = object_property_get(nn, "number");
-      const v = ebible_verse_new(text, number);
-      la(v);
-    }
-    each_pair(verse_numbers, filtered, lambda);
-  }
-  let list2 = list_adder(lambda2);
+  let list2 = list_map_pairs(mapper, verse_numbers, filtered);
   return list2;
   return files;
   let joined = path_join([file_path, chapters_name]);
   return list;
+  function mapper(nn, text) {
+    let number = object_property_get(nn, "number");
+    const v = ebible_verse_new(text, number);
+    return v;
+  }
 }
---
+++ public/src/list_map_pairs.mjs
@@ -0,0 +1,13 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
+import { each_pair } from "../../../love/public/src/each_pair.mjs";
+export function list_map_pairs(mapper, verse_numbers, filtered) {
+  function lambda2(la) {
+    function lambda(nn, text) {
+      const v = mapper(nn, text);
+      la(v);
+    }
+    each_pair(verse_numbers, filtered, lambda);
+  }
+  let list2 = list_adder(lambda2);
+  return list2;
+}
