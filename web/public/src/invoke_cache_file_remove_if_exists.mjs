c2be37cc878ed5c93db369fc4eea6237ac802ea1
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 17:11:24 -0500

    fws invoke_cache_file_remove if_exists

--- web/public/src/invoke_cache_file_remove_if_exists.mjs
+++ web/public/src/invoke_cache_file_remove_if_exists.mjs
@@ -1,4 +1,6 @@
+import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_file_remove.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function invoke_cache_file_remove_if_exists() {
+export async function invoke_cache_file_remove_if_exists(fn, args) {
   marker("1");
+  return await invoke_cache_file_remove(fn, args);
 }
