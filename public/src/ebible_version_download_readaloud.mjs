aca9c8f35859ebb2d6a2d7bd6a688e6b370599d4
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 14:40:27 -0500

    function_auto_path ..\love\public\src\ebible_version_download_readaloud.mjs

--- public/src/ebible_version_download_readaloud.mjs
+++ public/src/ebible_version_download_readaloud.mjs
@@ -4,12 +4,9 @@ import { http_local } from "../../../love/public/src/http_local.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_download_readaloud(bible_folder) {
   marker("1");
+  const name = "readaloud";
   let url =
-    "https://ebible.org/Scriptures/" +
-    bible_folder +
-    "_" +
-    "readaloud" +
-    ".zip";
+    "https://ebible.org/Scriptures/" + bible_folder + "_" + name + ".zip";
   let buffer = await http_local(url);
   let file_path = ebible_version_download_path(bible_folder);
   await unzip(file_path, buffer);
