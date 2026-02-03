42bcf7d0b214ba8be322ef45a7a8f70342372632
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 14:41:55 -0500

    function_auto_path ..\love\public\src\ebible_version_download.mjs

--- public/src/ebible_version_download.mjs
+++ public/src/ebible_version_download.mjs
@@ -1,10 +1,11 @@
+import { ebible_version_download_url } from "../../../love/public/src/ebible_version_download_url.mjs";
 import { ebible_version_download_path } from "../../../love/public/src/ebible_version_download_path.mjs";
 import { unzip } from "../../../love/public/src/unzip.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_download(bible_folder) {
   marker("1");
-  let url = "https://ebible.org/Scriptures/" + bible_folder + "_html.zip";
+  let url = ebible_version_download_url(bible_folder, "html");
   let buffer = await http_local(url);
   let file_path = ebible_version_download_path(bible_folder);
   await unzip(file_path, buffer);
