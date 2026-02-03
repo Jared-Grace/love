2e20ef7f19fe043927c0360208ce74a6a5d7aadc
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 17:05:58 -0500

    function_auto_path ..\love\public\src\ebible_languages_without_original_english.mjs

--- web/public/src/ebible_languages_without_original_english.mjs
+++ web/public/src/ebible_languages_without_original_english.mjs
@@ -1,9 +1,9 @@
+import { ebible_language_en_code } from "../../../love/public/src/ebible_language_en_code.mjs";
 import { list_remove_property } from "../../../love/public/src/list_remove_property.mjs";
-import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { ebible_languages_without_original } from "../../../love/public/src/ebible_languages_without_original.mjs";
 export function ebible_languages_without_original_english() {
   let languages = ebible_languages_without_original();
-  let e = ebible_folder_english();
+  let e = ebible_language_en_code();
   list_remove_property(languages, "language_code", e);
   return languages;
 }
