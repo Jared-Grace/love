c35df94466e8f45891cfba51561ed38927378700
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 18:15:50 -0500

    function_auto_path ..\love\public\src\ebible_languages_without_original_english_bible_folders_each.mjs

--- web/public/src/ebible_languages_without_original_english_bible_folders_each.mjs
+++ web/public/src/ebible_languages_without_original_english_bible_folders_each.mjs
@@ -1,6 +1,5 @@
 import { each_index_async } from "../../../love/public/src/each_index_async.mjs";
 import { list_index_of_next_property } from "../../../love/public/src/list_index_of_next_property.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { ebible_languages_without_original_english } from "../../../love/public/src/ebible_languages_without_original_english.mjs";
 export async function ebible_languages_without_original_english_bible_folders_each(
@@ -10,15 +9,10 @@ export async function ebible_languages_without_original_english_bible_folders_ea
   const property = "bible_folder";
   const value = "gaz";
   let index_next = list_index_of_next_property(languages, property, value);
-  log({
-    index_next,
-  });
   async function lambda2(language, i) {
     if (i < index_next) {
       return;
     }
-    log(language);
-    return;
     let bible_folder = object_property_get(language, "bible_folder");
     await lambda$bible_folder(bible_folder);
   }
