19c780951e0ebf276d916ad09c3f4a19eec04397
Grace-Jared <christrosetolife@gmail.com>
2026-02-03 18:50:23 -0500

    function_auto_path ..\love\public\src\ebible_verses.mjs

--- web/public/src/ebible_verses.mjs
+++ web/public/src/ebible_verses.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { ebible_verses_browser } from "../../../love/public/src/ebible_verses_browser.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
@@ -50,6 +51,9 @@ export async function ebible_verses(bible_folder, chapter_code) {
     list_add(verses_unfiltered, v);
   }
   list_reverse(verses_unfiltered);
+  log({
+    verses_unfiltered,
+  });
   function lambda(item) {
     let value = object_property_get(item, property);
     let n = string_empty_not_is(value);
