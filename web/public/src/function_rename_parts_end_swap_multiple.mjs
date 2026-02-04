c5b9278e094f477a46d0bdfa0dc9e1d869f8338a
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:33:49 -0500

    fws function_rename_parts_end_swap multiple

--- web/public/src/function_rename_parts_end_swap_multiple.mjs
+++ web/public/src/function_rename_parts_end_swap_multiple.mjs
@@ -1,4 +1,6 @@
+import { function_rename_parts_end_swap } from "../../../love/public/src/function_rename_parts_end_swap.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function function_rename_parts_end_swap_multiple() {
+export async function function_rename_parts_end_swap_multiple(f_name_before) {
   marker("1");
+  return await function_rename_parts_end_swap(f_name_before);
 }
