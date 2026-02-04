f1adef20a6527434fe0f0308a96347306600cb30
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 09:51:21 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_each.mjs

--- public/src/ebible_versions_english_choices_each.mjs
+++ public/src/ebible_versions_english_choices_each.mjs
@@ -1,8 +1,10 @@
+import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_versions_english_choices } from "../../../love/public/src/ebible_versions_english_choices.mjs";
 export async function ebible_versions_english_choices_each(
   lambda$bible_folder,
 ) {
   let english_choices = await ebible_versions_english_choices();
+  let index = list_index_of(list, item);
   await each_async(english_choices, lambda$bible_folder);
 }
