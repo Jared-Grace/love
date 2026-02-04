5f9ad4919f29f85e6b5f56801db0e8173fcfa1f1
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 09:44:29 -0500

    function_auto_path ..\love\public\src\list_map_replace.mjs

--- public/src/list_map_replace.mjs
+++ public/src/list_map_replace.mjs
@@ -1,10 +1,10 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
-export function list_map_replace(mapped, from, to) {
+export function list_map_replace(list, from, to) {
   function lambda(item) {
     let replaced = string_replace(item, from, to);
     return replaced;
   }
-  let mapped2 = list_map(mapped, lambda);
+  let mapped2 = list_map(list, lambda);
   return mapped2;
 }
