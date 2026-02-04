8183a17e54db8dfc1139c83187353c84b3bc8198
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:07:44 -0500

    pb function_name_parts_remove_comma

--- public/src/function_copy_parts_delete.mjs
+++ public/src/function_copy_parts_delete.mjs
@@ -3,7 +3,7 @@ import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_copy_parts_delete(f_name_old, deleted) {
   marker("1");
-  let f_name_new = function_name_parts_remove_comma(deleted, f_name_old);
+  let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
   let v = await function_copy(f_name_old, f_name_new);
   return v;
 }
