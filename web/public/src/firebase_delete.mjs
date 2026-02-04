015c4a592d95412e26ec62d2d6804e1e3d725b29
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 18:37:52 -0500

    function_auto_path ..\love\public\src\firebase_delete.mjs

--- web/public/src/firebase_delete.mjs
+++ web/public/src/firebase_delete.mjs
@@ -1,11 +1,10 @@
+import { firebase_bucket_file_get } from "../../../love/public/src/firebase_bucket_file_get.mjs";
 import { retry_standard } from "../../../love/public/src/retry_standard.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { firebase_bucket } from "../../../love/public/src/firebase_bucket.mjs";
-import { firebase_path_fix } from "../../../love/public/src/firebase_path_fix.mjs";
 export async function firebase_delete(destination) {
-  destination = firebase_path_fix(destination);
-  const bucket = await firebase_bucket();
-  const file = bucket.file(destination);
+  let bucket = null;
+  let file = null;
+  ({ bucket, file, destination } = await firebase_bucket_file_get(destination));
   await retry_standard(lambda);
   log_keep(`Deleted data at ${destination}`);
   async function lambda() {
