72d7ab239982d1fd6d45b39bd2451832e2214f80
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:22:23 -0500

    function_auto_path ..\love\public\src\list_index_last.mjs

--- public/src/list_index_last.mjs
+++ public/src/list_index_last.mjs
@@ -1,5 +1,5 @@
 import { list_index_end } from "../../../love/public/src/list_index_end.mjs";
 export function list_index_last(list) {
-  const index_last = list_index_end(list, 1);
+  const index_last = list_index_end(list, 0);
   return index_last;
 }
