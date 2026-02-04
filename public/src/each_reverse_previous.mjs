63d6526d2e2606c7af8126b4f721dfc6df1b58d7
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:24:01 -0500

    fws each_reverse previous

--- public/src/each_reverse_previous.mjs
+++ public/src/each_reverse_previous.mjs
@@ -1,4 +1,6 @@
+import { each_reverse } from "../../../love/public/src/each_reverse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function each_reverse_previous() {
+export function each_reverse_previous(list, lambda) {
   marker("1");
+  return each_reverse(list, lambda);
 }
