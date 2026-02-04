2401c694352aad608a9114ea275985814eb866c8
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:28:14 -0500

    function_auto_path ..\love\public\src\each_reverse_previous.mjs

--- public/src/each_reverse_previous.mjs
+++ public/src/each_reverse_previous.mjs
@@ -1,7 +1,7 @@
+import { each_index_reverse } from "../../../love/public/src/each_index_reverse.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
-import { each_reverse } from "../../../love/public/src/each_reverse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function each_reverse_previous(list, lambda) {
   marker("1");
@@ -9,9 +9,9 @@ export function each_reverse_previous(list, lambda) {
     let fi = list_first_is(list, item);
     let previous = null;
     if (not(fi)) {
-      let index = list_index_of(list2, item2);
+      let index = list_index_of(list, item);
     }
   }
-  let v = each_reverse(list, lambda2);
+  let v = each_index_reverse(list, lambda2);
   return v;
 }
