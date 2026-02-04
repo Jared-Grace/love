5c0e2df2c4804c4a488e3d8d63603dd2058953a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 18:39:07 -0500

    function_auto_path ..\love\public\src\ebible_version_download_zip_delete.mjs

--- public/src/ebible_version_download_zip_delete.mjs
+++ public/src/ebible_version_download_zip_delete.mjs
@@ -2,5 +2,6 @@ import { ebible_version_download } from "../../../love/public/src/ebible_version
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_download_zip_delete(bible_folder) {
   marker("1");
-  return await ebible_version_download(bible_folder);
+  let v = await ebible_version_download(bible_folder);
+  return v;
 }
