3d839d9015617b7df3c9df14e194807254248f2b
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:27:29 -0500

    fws each_index reverse

--- public/src/each_index_reverse.mjs
+++ public/src/each_index_reverse.mjs
@@ -1,4 +1,6 @@
+import { each_index } from "../../../love/public/src/each_index.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function each_index_reverse() {
+export function each_index_reverse(list, lambda$item$index) {
   marker("1");
+  return each_index(list, lambda$item$index);
 }
