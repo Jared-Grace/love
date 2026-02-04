2601f2e8e4e2bdeda342b52f3d0d38c4b007c838
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:07:44 -0500

    pb function_name_parts_remove_comma

--- web/public/src/function_name_parts_remove_comma.mjs
+++ web/public/src/function_name_parts_remove_comma.mjs
@@ -1,6 +1,6 @@
 import { function_name_parts_remove } from "../../../love/public/src/function_name_parts_remove.mjs";
 import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
-export function function_name_parts_remove_comma(deleted, f_name_old) {
+export function function_name_parts_remove_comma(f_name_old, deleted) {
   let split = string_split_comma(deleted);
   let f_name_new = function_name_parts_remove(f_name_old, split);
   return f_name_new;
