7569ae45ac73e719c8c50d377b1d7ecfd2659533
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:41:56 -0500

    fws list_reduce_index list_index_of_all

--- public/src/list_reduce_index_list_index_of_all.mjs
+++ public/src/list_reduce_index_list_index_of_all.mjs
@@ -1,4 +1,6 @@
+import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function list_reduce_index_list_index_of_all() {
+export function list_reduce_index_list_index_of_all(list, reducer, inital) {
   marker("1");
+  return list_reduce_index(list, reducer, inital);
 }
