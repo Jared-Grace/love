98bf09959424a134f6c5d468ef5a9c836d328664
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:25:53 -0500

    function_auto_path ..\love\public\src\each_reverse_previous.mjs

--- public/src/each_reverse_previous.mjs
+++ public/src/each_reverse_previous.mjs
@@ -1,4 +1,3 @@
-import { not } from "../../../love/public/src/not.mjs";
 import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
 import { each_reverse } from "../../../love/public/src/each_reverse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -6,7 +5,8 @@ export function each_reverse_previous(list, lambda) {
   marker("1");
   function lambda2(item) {
     let fi = list_first_is(list, item);
-    if (not(b)) {
+    if (fi) {
+      let previous = null;
     }
   }
   let v = each_reverse(list, lambda2);
