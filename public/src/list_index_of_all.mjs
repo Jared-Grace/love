f1796cbebdc088c443073733701a4514c1758657
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:42:14 -0500

    function_auto_path ..\love\public\src\list_index_of_all.mjs

---
+++ public/src/list_index_of_all.mjs
@@ -0,0 +1,7 @@
+import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+export function list_index_of_all(list, reducer, inital) {
+  marker("1");
+  let value = list_reduce_index(list, reducer, inital);
+  return value;
+}
