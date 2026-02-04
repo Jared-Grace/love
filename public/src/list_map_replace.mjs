f2c1af6bbd51103374acc24a249033ea126600bb
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 09:44:03 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

---
+++ public/src/list_map_replace.mjs
@@ -0,0 +1,10 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { string_replace } from "../../../love/public/src/string_replace.mjs";
+export function list_map_replace(from, to, mapped) {
+  function lambda(item) {
+    let replaced = string_replace(item, from, to);
+    return replaced;
+  }
+  let mapped2 = list_map(mapped, lambda);
+  return mapped2;
+}
