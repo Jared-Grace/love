da98c008d809860e52e1c48328f66c7eed7f22c5
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 09:44:20 -0500

    pf list_map_replace mapped

--- web/public/src/list_map_replace.mjs
+++ web/public/src/list_map_replace.mjs
@@ -1,6 +1,6 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
-export function list_map_replace(from, to, mapped) {
+export function list_map_replace(mapped, from, to) {
   function lambda(item) {
     let replaced = string_replace(item, from, to);
     return replaced;
