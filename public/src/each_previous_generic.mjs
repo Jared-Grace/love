eb5fcb6a02744b2b131c657f03fb323934a1df17
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:08:29 -0500

    function_auto_path ..\love\public\src\each_previous_generic.mjs

---
+++ public/src/each_previous_generic.mjs
@@ -0,0 +1,21 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { each_index } from "../../../love/public/src/each_index.mjs";
+import { list_get } from "../../../love/public/src/list_get.mjs";
+export function each_previous_generic(
+  index_valid_is,
+  index_other_get,
+  list,
+  lambda$item$other,
+) {
+  marker("1");
+  function lambda2(item, index) {
+    let other = null;
+    if (index_valid_is(index)) {
+      let index2 = index_other_get(index);
+      other = list_get(list, index2);
+    }
+    lambda$item$other(item, other);
+  }
+  let v = each_index(list, lambda2);
+  return v;
+}
