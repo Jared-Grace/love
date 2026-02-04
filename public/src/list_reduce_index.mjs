f3e2edb854ac5689ce5af123992e07e6206f7dba
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:38:47 -0500

    function_auto_path ..\love\public\src\list_reduce_index.mjs

---
+++ public/src/list_reduce_index.mjs
@@ -0,0 +1,11 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+export function list_reduce_index(list, reducer, inital) {
+  marker("1");
+  let value = inital;
+  function lambda2(item) {
+    value = reducer(item, value);
+  }
+  each(list, lambda2);
+  return value;
+}
