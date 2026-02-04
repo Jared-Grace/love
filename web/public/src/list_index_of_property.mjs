74ced0859cf9794295f925166f4ff81d75c7fe65
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:50:13 -0500

    function_auto_path ..\love\public\src\list_index_of_property.mjs

---
+++ web/public/src/list_index_of_property.mjs
@@ -0,0 +1,9 @@
+import { marker } from "../../../love/public/src/marker.mjs";
+import { list_index_of_next } from "../../../love/public/src/list_index_of_next.mjs";
+import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
+export function list_index_of_property(list, property, value) {
+  marker("1");
+  let item = list_find_property(list, property, value);
+  let index_next = list_index_of(list, item);
+  return index_next;
+}
