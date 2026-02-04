745a79374bf2a9fed51566cf8cd1b940bc77581a
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:09:17 -0500

    function_auto_path ..\love\public\src\each_reverse_next.mjs

--- web/public/src/each_reverse_next.mjs
+++ web/public/src/each_reverse_next.mjs
@@ -1,13 +1,20 @@
+import { list_copy_reverse } from "../../../love/public/src/list_copy_reverse.mjs";
 import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { list_last_not_is } from "../../../love/public/src/list_last_not_is.mjs";
 import { each_previous_generic } from "../../../love/public/src/each_previous_generic.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function each_reverse_next(list, lambda$item$next) {
   marker("1");
-  let v4 = each_previous_generic(index_valid_is, add_1, list, lambda$item$next);
+  let reversed = list_copy_reverse(list);
+  let v4 = each_previous_generic(
+    index_valid_is,
+    add_1,
+    reversed,
+    lambda$item$next,
+  );
   return v4;
   function index_valid_is(index) {
-    let n = list_last_not_is(list, index);
+    let n = list_last_not_is(reversed, index);
     return n;
   }
 }
