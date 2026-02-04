53d7b2104633950c836f40e9c0cbf657a079ed1c
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 18:35:51 -0500

    function_auto_path ..\love\public\src\ebible_verses_upload.mjs

--- web/public/src/ebible_verses_upload.mjs
+++ web/public/src/ebible_verses_upload.mjs
@@ -1,5 +1,5 @@
+import { retry_standard } from "../../../love/public/src/retry_standard.mjs";
 import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
-import { retry } from "../../../love/public/src/retry.mjs";
 import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { ebible_firebase_upload_verse } from "../../../love/public/src/ebible_firebase_upload_verse.mjs";
 import { ebible_chapters_each_verses_check_with } from "../../../love/public/src/ebible_chapters_each_verses_check_with.mjs";
@@ -16,7 +16,7 @@ export async function ebible_verses_upload(bible_folder) {
       async function lambda3() {
         await ebible_firebase_upload_verse(v, chapter_code, bible_folder);
       }
-      await retry(5, lambda3);
+      await retry_standard(lambda3);
     }
     await each_unordered_async(verses, lambda);
   }
