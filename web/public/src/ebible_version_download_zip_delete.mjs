976daaa45f6a8eab98dc04f7f7db281055caec4e
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 18:42:08 -0500

    function_auto_path ..\love\public\src\ebible_version_download_zip_delete.mjs

--- web/public/src/ebible_version_download_zip_delete.mjs
+++ web/public/src/ebible_version_download_zip_delete.mjs
@@ -1,3 +1,4 @@
+import { file_delete } from "../../../love/public/src/file_delete.mjs";
 import { http_local_file_name } from "../../../love/public/src/http_local_file_name.mjs";
 import { firebase_delete } from "../../../love/public/src/firebase_delete.mjs";
 import { http_firebase_file_path } from "../../../love/public/src/http_firebase_file_path.mjs";
@@ -10,6 +11,7 @@ export async function ebible_version_download_zip_delete(bible_folder) {
   let f = http_firebase_file_path(url);
   await firebase_delete(f);
   let joined = http_local_file_name(url);
+  await file_delete(file_path);
   let v = await ebible_version_download(bible_folder);
   return v;
 }
