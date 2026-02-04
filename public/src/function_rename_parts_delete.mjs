05616fc1ba5be93ff90adf4505c7d1dcd11aa4e5
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:05:05 -0500

    fws r parts_delete

--- public/src/function_rename_parts_delete.mjs
+++ public/src/function_rename_parts_delete.mjs
@@ -1,4 +1,9 @@
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function function_rename_parts_delete() {
+export async function function_rename_parts_delete(
+  f_name_before,
+  f_name_after,
+) {
   marker("1");
+  return await function_rename(f_name_before, f_name_after);
 }
