2fd611dd16698f6121ffdb5e3a30caa5058ee29c
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 09:40:58 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

--- public/src/ebible_verses_readaloud.mjs
+++ public/src/ebible_verses_readaloud.mjs
@@ -35,7 +35,8 @@ export async function ebible_verses_readaloud(bible_folder, chapter_code) {
   let contents = await file_read(only);
   let lines = string_split_newline(contents);
   let skipped = list_skip(lines, 2);
-  let mapped = list_map(skipped, string_trim);lm
+  let mapped = list_map(skipped, string_trim);
+  let mapped2 = list_map(list2, function lambda(item) {});
   let filtered = list_filter_empty_not_is(mapped);
   let list = list_map_pairs(filtered, verse_numbers, ebible_verse_new_text);
   return list;
