70c8a2eb192427e55f6c7edabbfad7941e265364
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:22:11 -0500

    function_auto_path ..\love\public\src\list_index_of_last.mjs

--- web/public/src/list_index_of_last.mjs
+++ web/public/src/list_index_of_last.mjs
@@ -1,8 +1,9 @@
+import { list_index_of_last_try } from "../../../love/public/src/list_index_of_last_try.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 export function list_index_of_last(list, item) {
   marker("1");
-  let index = list.lastIndexOf(item);
+  let index = list_index_of_last_try(list, item);
   if (index <= -1) {
     error_json({
       list,
