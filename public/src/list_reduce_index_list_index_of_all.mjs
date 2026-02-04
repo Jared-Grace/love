c71da4108d41c7e91e9b56c6f9ac481e9d70acfc
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:42:00 -0500

    function_auto_path ..\love\public\src\list_reduce_index_list_index_of_all.mjs

--- public/src/list_reduce_index_list_index_of_all.mjs
+++ public/src/list_reduce_index_list_index_of_all.mjs
@@ -2,5 +2,6 @@ import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mj
 import { marker } from "../../../love/public/src/marker.mjs";
 export function list_reduce_index_list_index_of_all(list, reducer, inital) {
   marker("1");
-  return list_reduce_index(list, reducer, inital);
+  let value = list_reduce_index(list, reducer, inital);
+  return value;
 }
