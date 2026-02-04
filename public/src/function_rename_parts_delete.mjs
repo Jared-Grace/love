b05058e5863cac17c54e4dee1998d4895dfac0ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:07:24 -0500

    function_auto_path ..\love\public\src\function_rename_parts_delete.mjs

--- public/src/function_rename_parts_delete.mjs
+++ public/src/function_rename_parts_delete.mjs
@@ -1,11 +1,9 @@
-import { function_name_parts_remove } from "../../../love/public/src/function_name_parts_remove.mjs";
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { function_name_parts_remove_comma } from "../../../love/public/src/function_name_parts_remove_comma.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_parts_delete(f_name_old, deleted) {
   marker("1");
-  let split = string_split_comma(deleted);
-  let f_name_new = function_name_parts_remove(f_name_old, split);
+  let f_name_new = function_name_parts_remove_comma(deleted, f_name_old);
   let v = await function_rename(f_name_old, f_name_new);
   return v;
 }
