fb5b9d28fffe85ac3c156cd2fec40ab4b27e9b68
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:24:08 -0500

    function_auto_path ..\love\public\src\function_rename_parts_end_swap.mjs

--- web/public/src/function_rename_parts_end_swap.mjs
+++ web/public/src/function_rename_parts_end_swap.mjs
@@ -1,3 +1,4 @@
+import { list_index_last_second } from "../../../love/public/src/list_index_last_second.mjs";
 import { list_index_last } from "../../../love/public/src/list_index_last.mjs";
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
@@ -9,7 +10,8 @@ export async function function_rename_parts_end_swap(
 ) {
   marker("1");
   let parts = function_name_to_parts(f_name_before);
-  let index_last = list_index_last(list2);
+  let index_last = list_index_last(list);
+  let index_last_second = list_index_last_second(list);
   list_swap_at(list, ai, bi);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
