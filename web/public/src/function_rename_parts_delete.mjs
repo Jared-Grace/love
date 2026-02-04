20cce2e8f117d9dac291f4221176abfb95151240
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:07:44 -0500

    pb function_name_parts_remove_comma

--- web/public/src/function_rename_parts_delete.mjs
+++ web/public/src/function_rename_parts_delete.mjs
@@ -3,7 +3,7 @@ import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_parts_delete(f_name_old, deleted) {
   marker("1");
-  let f_name_new = function_name_parts_remove_comma(deleted, f_name_old);
+  let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
   let v = await function_rename(f_name_old, f_name_new);
   return v;
 }
