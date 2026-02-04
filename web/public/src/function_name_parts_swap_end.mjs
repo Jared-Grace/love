ded73bdda2912013f6db059e7dea78039dc6abe1
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:32:11 -0500

    function_auto_path ..\love\public\src\function_rename_parts_end_swap.mjs

---
+++ web/public/src/function_name_parts_swap_end.mjs
@@ -0,0 +1,9 @@
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
+import { list_swap_end } from "../../../love/public/src/list_swap_end.mjs";
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+export function function_name_parts_swap_end(f_name_before) {
+  let list = function_name_to_parts(f_name_before);
+  list_swap_end(list);
+  let f_name_after = function_name_combine_multiple(list);
+  return f_name_after;
+}
