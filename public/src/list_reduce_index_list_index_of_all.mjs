438224055354a7215aee2e497ce23a345653519e
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:42:18 -0500

    r list_reduce_index_list_index_of_all list_index_of_all

--- public/src/list_reduce_index_list_index_of_all.mjs
+++
@@ -1,7 +0,0 @@
-import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-export function list_reduce_index_list_index_of_all(list, reducer, inital) {
-  marker("1");
-  let value = list_reduce_index(list, reducer, inital);
-  return value;
-}
