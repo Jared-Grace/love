28e1b6e2f194401e39cbbd2a2c4c786077eb7e5d
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:27:33 -0500

    function_auto_path ..\love\public\src\each_index_reverse.mjs

--- public/src/each_index_reverse.mjs
+++ public/src/each_index_reverse.mjs
@@ -2,5 +2,6 @@ import { each_index } from "../../../love/public/src/each_index.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function each_index_reverse(list, lambda$item$index) {
   marker("1");
-  return each_index(list, lambda$item$index);
+  let v = each_index(list, lambda$item$index);
+  return v;
 }
