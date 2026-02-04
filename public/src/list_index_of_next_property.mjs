f78c094a8a4f1207ab230ec49dee0b357fb3bff6
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 17:48:52 -0500

    function_auto_path ..\love\public\src\list_index_of_next_property.mjs

--- public/src/list_index_of_next_property.mjs
+++ public/src/list_index_of_next_property.mjs
@@ -1,7 +1,7 @@
 import { list_index_of_next } from "../../../love/public/src/list_index_of_next.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
-export function list_index_of_next_property(languages, property, value) {
-  let item = list_find_property(languages, property, value);
-  let index_next = list_index_of_next(languages, item);
+export function list_index_of_next_property(list, property, value) {
+  let item = list_find_property(list, property, value);
+  let index_next = list_index_of_next(list, item);
   return index_next;
 }
