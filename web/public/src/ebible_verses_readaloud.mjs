262f7ebcf3de4a06c4493cd44519bfacafc05f1d
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 09:43:21 -0500

    function_auto_path ..\love\public\src\ebible_verses_readaloud.mjs

--- web/public/src/ebible_verses_readaloud.mjs
+++ web/public/src/ebible_verses_readaloud.mjs
@@ -41,8 +41,8 @@ export async function ebible_verses_readaloud(bible_folder, chapter_code) {
     let replaced = string_replace(item, "[]", "");
     return replaced;
   }
-  let mapped2 = list_map(list2, lambda);
-  let filtered = list_filter_empty_not_is(mapped);
+  let mapped2 = list_map(mapped, lambda);
+  let filtered = list_filter_empty_not_is(mapped2);
   let list = list_map_pairs(filtered, verse_numbers, ebible_verse_new_text);
   return list;
 }
