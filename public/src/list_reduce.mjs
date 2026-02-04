4c8a1bf17a71934fd801ebcac736d0880783c557
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:41:05 -0500

    function_auto_path ..\love\public\src\list_reduce.mjs

--- public/src/list_reduce.mjs
+++ public/src/list_reduce.mjs
@@ -1,17 +1,9 @@
 import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 export function list_reduce(list, reducer, inital) {
-  marker("1");
-  let value = inital;
-  function lambda2(item) {
-    value = reducer(item, value);
-  }
-  each(list, lambda2);
-  return value;
   function lambda(item, value, index) {
     let v = reducer(item, value);
     return v;
   }
-  let value2 = list_reduce_index(list, lambda, inital);
+  let value = list_reduce_index(list, lambda, inital);
+  return value;
 }
