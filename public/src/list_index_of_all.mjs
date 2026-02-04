69fb0d2728e88b3dff0f3300fbd61bb5617470c5
Grace-Jared <christrosetolife@gmail.com>
2026-02-04 16:49:48 -0500

    function_auto_path ..\love\public\src\list_index_of_all.mjs

--- public/src/list_index_of_all.mjs
+++ public/src/list_index_of_all.mjs
@@ -5,10 +5,10 @@ import { marker } from "../../../love/public/src/marker.mjs";
 export function list_index_of_all(list, search) {
   marker("1");
   function lambda(item, value, index) {
-    if (value === search) {
-      list_add(item, index);
+    if (item === search) {
+      list_add(value, index);
     }
-    return item;
+    return value;
   }
   let indices = list_reduce_index(list, lambda, []);
   log({
