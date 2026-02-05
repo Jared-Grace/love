372cc8442406dfa7fb443414bfcddd334edc1281
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:01:35 -0500

    function_auto_path ..\love\public\src\file_read.mjs

--- web/public/src/file_read_buffer.mjs
+++ web/public/src/file_read_buffer.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function file_read_buffer(file_path) {
   let fs = await import("fs");
   let v = await fs.promises.readFile(file_path);
