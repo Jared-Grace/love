ce2bdc5e1f92d778b808f59d73934ba1c74f0bd0
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:40 -0500

    function_auto_path ..\love\public\src\whitespace_normalize.mjs

--- public/src/js_visit_match_first.mjs
+++ public/src/js_visit_match_first.mjs
@@ -1,5 +1,4 @@
 import { list_first } from "../../../love/public/src/list_first.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { js_visit_matches } from "../../../love/public/src/js_visit_matches.mjs";
 export function js_visit_match_first(ast, node_search) {
   let matches = js_visit_matches(ast, node_search);
--- public/src/window_open_app.mjs
+++ public/src/window_open_app.mjs
@@ -1,6 +1,5 @@
 import { app_prefix_without } from "../../../love/public/src/app_prefix_without.mjs";
 import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { window_open } from "../../../love/public/src/window_open.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 export function window_open_app(app_fn, hash) {
