67ce2f19e48c6b7ecb9fc870a6065d3f9db6e9a6
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:00 -0500

    function_auto_path ..\love\public\src\marker_top.mjs

--- public/src/marker_remove_marker_top.mjs
+++ public/src/marker_remove_marker_top.mjs
@@ -1,6 +1,5 @@
 import { marker_top } from "../../../love/public/src/marker_top.mjs";
 import { marker_remove } from "../../../love/public/src/marker_remove.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function marker_remove_marker_top() {
   await marker_remove();
   let v = await marker_top();
