eb2a338a5ffe9ae496a87caa5650a09f6a269f7f
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:09:48 -0500

    function_auto_path ..\love\public\src\love_repo_initialize.mjs

---
+++ public/src/love_repo_initialize.mjs
@@ -0,0 +1,19 @@
+import { add } from "../../../love/public/src/add.mjs";
+import { data_folder } from "../../../love/public/src/data_folder.mjs";
+import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
+import { folder_vscode } from "../../../love/public/src/folder_vscode.mjs";
+import { path_join } from "../../../love/public/src/path_join.mjs";
+import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
+import { marker } from "../../../love/public/src/marker.mjs";
+import { file_name_json_folder } from "./file_name_json_folder.mjs";
+export async function love_repo_initialize() {
+  marker("1");
+  let vc = folder_vscode();
+  let f_path = file_name_json_folder(vc, "tasks");
+  let d_path = data_folder();
+  let file_path_old = path_join([d_path, f_path]);
+  let previous = folder_previous();
+  let file_path_new = path_join([previous, f_path]);
+  await file_copy_overwrite(file_path_old, file_path_new);
+  let sum = add(left, right);
+}
