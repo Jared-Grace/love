2e7bc60b43d0c57e9b262bed841ce3cc83fb173b
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:51:30 -0500

    function_auto_path ..\love\public\src\marker_up.mjs

--- public/src/object_properties_delete_if_exists.mjs
+++ public/src/object_properties_delete_if_exists.mjs
@@ -1,5 +1,4 @@
 import { object_property_delete_if_exists } from "../../../love/public/src/object_property_delete_if_exists.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function object_properties_delete_if_exists(data, properties) {
   function lambda2(p) {
