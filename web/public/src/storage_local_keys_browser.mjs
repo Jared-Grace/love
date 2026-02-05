d73eedda76b11eceea2bab3cdedf4c1dbb6b2397
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:57 -0500

    function_auto_path ..\love\public\src\storage_local_keys_browser.mjs

--- web/public/src/storage_local_keys_browser.mjs
+++ web/public/src/storage_local_keys_browser.mjs
@@ -2,7 +2,6 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_range_index } from "../../../karate_code/public/src/each_range_index.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_keys_browser() {
   function lambda(la) {
     function lambda2(i) {
