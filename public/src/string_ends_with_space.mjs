ec21004ef92a34a47bebdf67315cbd50689efe43
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:10 -0500

    function_auto_path ..\love\public\src\string_regex_match.mjs

--- public/src/string_ends_with_space.mjs
+++ public/src/string_ends_with_space.mjs
@@ -1,5 +1,4 @@
 import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function string_ends_with_space(s) {
   let sw2 = string_ends_with(s, " ");
   return sw2;
