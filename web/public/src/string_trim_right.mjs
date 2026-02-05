66dcc118684839f0f076ed919b0159b9efaffa78
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:15 -0500

    function_auto_path ..\love\public\src\string_split_semicolon.mjs

--- web/public/src/string_trim_right.mjs
+++ web/public/src/string_trim_right.mjs
@@ -1,6 +1,5 @@
 import { string_size } from "../../../love/public/src/string_size.mjs";
 import { string_take } from "../../../love/public/src/string_take.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 export function string_trim_right(lambda$s, s) {
   let trimmed = s;
