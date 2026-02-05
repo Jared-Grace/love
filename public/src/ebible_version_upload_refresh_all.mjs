c78c35a4c2d2ae6bc043e6f2d8f6dc7a60b6eb5f
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 15:17:24 -0500

    function_auto_path ..\love\public\src\ebible_version_upload_refresh_all.mjs

--- public/src/ebible_version_upload_refresh_all.mjs
+++ public/src/ebible_version_upload_refresh_all.mjs
@@ -1,13 +1,11 @@
+import { ebible_version_upload_refresh_all_languages } from "../../../love/public/src/ebible_version_upload_refresh_all_languages.mjs";
 import { ebible_versions_english_choices_each } from "../../../love/public/src/ebible_versions_english_choices_each.mjs";
-import { ebible_languages_without_original_english_bible_folders_each } from "../../../love/public/src/ebible_languages_without_original_english_bible_folders_each.mjs";
 import { ebible_languages_chapters_cache_refresh } from "../../../love/public/src/ebible_languages_chapters_cache_refresh.mjs";
 import { ebible_version_upload_refresh } from "../../../love/public/src/ebible_version_upload_refresh.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_upload_refresh_all() {
   marker("1");
-  await ebible_languages_without_original_english_bible_folders_each(
-    ebible_version_upload_refresh,
-  );
+  await ebible_version_upload_refresh_all_languages();
   return;
   await ebible_versions_english_choices_each(ebible_version_upload_refresh);
   return;
