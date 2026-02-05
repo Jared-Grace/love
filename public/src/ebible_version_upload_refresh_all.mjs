013589146d16a5fc8d731e31bf3ebe6126366eb5
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:55:18 -0500

    function_auto_path ..\love\public\src\ebible_verses_upload_compare.mjs

--- public/src/ebible_version_upload_refresh_all.mjs
+++ public/src/ebible_version_upload_refresh_all.mjs
@@ -2,7 +2,6 @@ import { ebible_version_upload_refresh_all_languages } from "../../../love/publi
 import { ebible_versions_english_choices_each } from "../../../love/public/src/ebible_versions_english_choices_each.mjs";
 import { ebible_languages_chapters_cache_refresh } from "../../../love/public/src/ebible_languages_chapters_cache_refresh.mjs";
 import { ebible_version_upload_refresh } from "../../../love/public/src/ebible_version_upload_refresh.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_version_upload_refresh_all() {
   await ebible_version_upload_refresh_all_languages();
   return;
