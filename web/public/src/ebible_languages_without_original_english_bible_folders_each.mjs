cdc3f001197cc987e24a82337cdd8091ca681b00
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 15:17:05 -0500

    function_auto_path ..\love\public\src\ebible_languages_without_original_english_bible_folders_each.mjs

--- web/public/src/ebible_languages_without_original_english_bible_folders_each.mjs
+++ web/public/src/ebible_languages_without_original_english_bible_folders_each.mjs
@@ -7,7 +7,7 @@ export async function ebible_languages_without_original_english_bible_folders_ea
 ) {
   let languages = ebible_languages_without_original_english();
   const property = "bible_folder";
-  const including_and_onward = "frasbl";
+  const including_and_onward = "porbrbsl";
   let index_next = list_index_of_property(
     languages,
     property,
