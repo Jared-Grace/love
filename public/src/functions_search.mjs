a2d550811394220c6a6e189dcd499b9f5f56f6f7
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:02:42 -0500

    function_auto_path ..\love\public\src\function_cache_refresh.mjs

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -1,7 +1,6 @@
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { functions_search_generic } from "../../../love/public/src/functions_search_generic.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_search(search) {
   assert_arguments(arguments, 1);
   let fn = string_includes;
