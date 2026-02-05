6e4cb0b51b872e8d8917f47e177f1d08d32d3263
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:41 -0500

    function_auto_path ..\love\public\src\string_index_of.mjs

--- public/src/string_get.mjs
+++ public/src/string_get.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
 export function string_get(s, index) {
