abc029bb503125c84b7c8e357349359ab14a4d14
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:20:30 -0500

    function_auto_path ..\love\public\src\js_log_remove.mjs

--- public/src/js_log_remove.mjs
+++ public/src/js_log_remove.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { list_get_end_2 } from "../../../love/public/src/list_get_end_2.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -7,14 +8,16 @@ import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_log_remove(ast) {
   marker("1");
+  let fn = log;
   function lambda(v) {
-    let { stack, node } = v;
+    let node = object_property_get(v, "node");
+    let stack = object_property_get(v, "stack");
     let e1 = list_get_end_1(stack);
     let e2 = list_get_end_2(stack);
     function lambda3() {
-      let { callee } = node;
-      let { name } = callee;
-      if (name === log.name) {
+      let callee = object_property_get(node, "callee");
+      let name = object_property_get(callee, "name");
+      if (name === fn.name) {
         list_remove(e2, e1);
       }
     }
