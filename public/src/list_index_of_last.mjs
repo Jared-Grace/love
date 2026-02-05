2d7ee638b52ac83be4d8127041cd089ce234d806
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:12 -0500

    function_auto_path ..\love\public\src\list_map_property_exists.mjs

--- public/src/list_index_of_last.mjs
+++ public/src/list_index_of_last.mjs
@@ -1,5 +1,4 @@
 import { list_index_of_last_try } from "../../../love/public/src/list_index_of_last_try.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 export function list_index_of_last(list, item) {
   let index = list_index_of_last_try(list, item);
--- public/src/marker_new.mjs
+++ public/src/marker_new.mjs
@@ -1,6 +1,6 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { marker_current_set } from "../../../love/public/src/marker_current_set.mjs";
 import { js_marker_insert } from "../../../love/public/src/js_marker_insert.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { marker_next_index } from "../../../love/public/src/marker_next_index.mjs";
 import { function_transform_marker } from "../../../love/public/src/function_transform_marker.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
@@ -9,7 +9,9 @@ export async function marker_new() {
   let v = await function_transform_marker(f_name, lambda);
   return v;
   async function lambda(a) {
-    let { index, stack2 } = marker_next_index(a);
+    let v2 = marker_next_index(a);
+    let stack2 = object_property_get(v2, "stack2");
+    let index = object_property_get(v2, "index");
     let name = "2";
     await js_marker_insert(name, stack2, index);
     await marker_current_set(name);
