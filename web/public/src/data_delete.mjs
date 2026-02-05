056c16f5b988b983691597b920b60f907cc691cf
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:10:55 -0500

    function_auto_path ..\love\public\src\data_delete.mjs

--- web/public/src/data_delete.mjs
+++ web/public/src/data_delete.mjs
@@ -2,5 +2,6 @@ import { data_path } from "../../../love/public/src/data_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function data_delete() {
   marker("1");
-  return data_path();
+  let d_path = data_path();
+  return d_path;
 }
