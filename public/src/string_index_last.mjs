fb9a9c648cf0d48e2700710ceec5209e90cf3e3b
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:36 -0500

    function_auto_path ..\love\public\src\string_index_last.mjs

--- public/src/string_index_last.mjs
+++ public/src/string_index_last.mjs
@@ -1,5 +1,4 @@
 import { string_size } from "../../../love/public/src/string_size.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function string_index_last(s) {
   const index_last = string_size(s) - 1;
   return index_last;
