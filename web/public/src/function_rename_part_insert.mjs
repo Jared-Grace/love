38e49b0a62c07c2e1cb237ec43c88f38c30bbf0f
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:02:14 -0500

    function_auto_path ..\love\public\src\function_rename_part_insert.mjs

--- web/public/src/function_rename_part_insert.mjs
+++ web/public/src/function_rename_part_insert.mjs
@@ -1,7 +1,6 @@
-import { null_not_is_assert } from "../../../love/public/src/null_not_is_assert.mjs";
+import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
-import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
@@ -11,8 +10,7 @@ export async function function_rename_part_insert(
   index_string,
 ) {
   marker("1");
-  let index = integer_to_try(index_string);
-  null_not_is_assert(a);
+  let index = integer_to(index_string);
   let parts = function_name_to_parts(f_name_before);
   list_insert(parts, index, part);
   let f_name_after = function_name_combine_multiple(parts);
