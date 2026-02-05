f57b99ec3b83798e4c374c3b1a82954272a91c42
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:26 -0500

    function_auto_path ..\love\public\src\undefined_not_is_assert.mjs

--- web/public/src/function_unalias_exists.mjs
+++ web/public/src/function_unalias_exists.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 export async function function_unalias_exists(f_name) {
   let u = await function_name_to_path_unalias(f_name);
--- web/public/src/undefined_not_is_assert.mjs
+++ web/public/src/undefined_not_is_assert.mjs
@@ -1,5 +1,4 @@
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function undefined_not_is_assert(value) {
   function lambda2() {
     let v2 = {};
