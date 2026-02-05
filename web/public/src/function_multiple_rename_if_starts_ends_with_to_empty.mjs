b5691dee5c48efee67e25a84b410f68e51e7ce78
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:03:16 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- web/public/src/function_multiple_rename_if_starts_ends_with_to_empty.mjs
+++ web/public/src/function_multiple_rename_if_starts_ends_with_to_empty.mjs
@@ -1,5 +1,4 @@
 import { function_multiple_rename_if_starts_ends_with } from "../../../love/public/src/function_multiple_rename_if_starts_ends_with.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_multiple_rename_if_starts_ends_with_to_empty(
   f_name_prefix,
   f_name_suffix_before,
