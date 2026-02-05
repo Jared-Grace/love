20b49e4e0bf4df3563377e1540e7ef2f1da4fdeb
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:34 -0500

    function_auto_path ..\love\public\src\mod.mjs

--- public/src/openai_key_folder.mjs
+++ public/src/openai_key_folder.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
 export function openai_key_folder() {
   let v2 = folder_user_path() + "ChristGPT\\";
