4a4a377f3b399c2264eb52ac7b25d5d6764f577c
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:25 -0500

    function_auto_path ..\love\public\src\storage_local_initialize.mjs

--- public/src/server_url_data_ending.mjs
+++ public/src/server_url_data_ending.mjs
@@ -1,5 +1,4 @@
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function server_url_data_ending() {
   let d_path = data_path();
   let v = "/" + d_path;
