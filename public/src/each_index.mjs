4a2a62bbd228b1e4dbf72dfc157a0c0e14c512b1
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 09:37:30 -0500

    function_auto_path ..\love\public\src\each_index.mjs

--- public/src/each_index.mjs
+++ public/src/each_index.mjs
@@ -1,9 +1,9 @@
 import { each } from "../../../love/public/src/each.mjs";
 export function each_index(list, lambda$item$index) {
   let index = 0;
-  function lambda2(item) {
+  function lambda_each_index(item) {
     lambda$item$index(item, index);
     index++;
   }
-  each(list, lambda2);
+  each(list, lambda_each_index);
 }
