3910f542900ac922bdc973d0429f62f1ec97ede7
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:06 -0500

    function_auto_path ..\love\public\src\marker_screen_add.mjs

--- web/public/src/list_last_remaining.mjs
+++ web/public/src/list_last_remaining.mjs
@@ -1,7 +1,6 @@
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_one_other } from "../../../love/public/src/list_one_other.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function list_last_remaining(list) {
   const property_name = "remaining";
   let value_get = list_take_less_1;
