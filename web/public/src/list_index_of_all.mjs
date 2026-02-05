2c04c2f7bcf2d7de7839aa90bacdabe74a5713b1
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:55:22 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_main.mjs

--- web/public/src/list_index_of_all.mjs
+++ web/public/src/list_index_of_all.mjs
@@ -1,6 +1,5 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function list_index_of_all(list, search) {
   function lambda(item, value, index) {
     if (item === search) {
