46f5b514feb606e5556b8f34cc6d997438224ac4
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:15 -0500

    function_auto_path ..\love\public\src\string_split_semicolon.mjs

--- public/src/string_split_semicolon.mjs
+++ public/src/string_split_semicolon.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
 export function string_split_semicolon(s) {
   let split3 = string_split(s, ";");
