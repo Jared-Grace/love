41271802d3ebcd9139d781ae306d5e77e91bf32f
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:55:49 -0500

    function_auto_path ..\love\public\src\functions_marker_remove.mjs

--- public/src/functions_marker_remove.mjs
+++ public/src/functions_marker_remove.mjs
@@ -1,6 +1,5 @@
 import { js_marker_remove } from "../../../love/public/src/js_marker_remove.mjs";
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function functions_marker_remove() {
   let lambda = js_marker_remove;
   let v = await functions_transform(lambda);
