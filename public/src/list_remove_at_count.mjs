af5abae66a9f0719e8afd781229cdb633bbd83d4
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:49:34 -0500

    function_auto_path ..\love\public\src\list_slice_from.mjs

--- public/src/list_remove_at_count.mjs
+++ public/src/list_remove_at_count.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 export function list_remove_at_count(list, index, count) {
   let e = list.splice(index, count);
   return e;
--- public/src/list_slice_from.mjs
+++ public/src/list_slice_from.mjs
@@ -1,7 +1,6 @@
 import { list_slice_from_indices } from "../../../love/public/src/list_slice_from_indices.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function list_slice_from(list, item_from, item_to) {
   function lambda(item) {
     let index = list_index_of(list, item);
