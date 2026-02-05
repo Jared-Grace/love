b3d4581109e3aa3eff193f61acaa4f693c9dbcd9
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:13:58 -0500

    function_auto_path ..\love\public\src\app_ceb_bible.mjs

--- public/src/app_ceb_bible.mjs
+++ public/src/app_ceb_bible.mjs
@@ -1,4 +1,4 @@
-import { marker } from "../../../love/public/src/marker.mjs";
-export function app_ceb_bible() {
-  marker("1");
+import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
+export async function app_ceb_bible() {
+  await app_main_production("app_ceb_bible_main", "jared-grace");
 }
