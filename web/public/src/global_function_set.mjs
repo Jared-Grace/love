f3a295a3076542ec4245d640bd8ac3697818d7fd
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:05:11 -0500

    function_auto_path ..\love\public\src\g_objection_generate_upload_path_generic.mjs

--- web/public/src/global_function_set.mjs
+++ web/public/src/global_function_set.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 export function global_function_set(fn, value) {
