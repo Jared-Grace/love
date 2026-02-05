30d409d793aa1a125079c3d08df8e4d27359f754
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:25 -0500

    function_auto_path ..\love\public\src\marker_move.mjs

--- web/public/src/list_intersect_multiple.mjs
+++ web/public/src/list_intersect_multiple.mjs
@@ -6,7 +6,6 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { set_includes } from "../../../love/public/src/set_includes.mjs";
 import { list_unique_set } from "../../../love/public/src/list_unique_set.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_intersect_multiple(list) {
---
+++ web/public/src/marker_down_1.mjs
@@ -0,0 +1,5 @@
+import { marker_down } from "../../../love/public/src/marker_down.mjs";
+export async function marker_down_1() {
+  let v = await marker_down(1);
+  return v;
+}
