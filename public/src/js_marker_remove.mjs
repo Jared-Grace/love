9d913556e73e44960c331c9dd93dec272a2249ac
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:22:07 -0500

    function_auto_path ..\love\public\src\js_marker_remove.mjs

--- public/src/js_marker_remove.mjs
+++ public/src/js_marker_remove.mjs
@@ -1,8 +1,7 @@
 import { js_statement_call_remove } from "../../../love/public/src/js_statement_call_remove.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function js_marker_remove(ast) {
   marker("1");
-  let fn = log;
+  let fn = marker;
   js_statement_call_remove(fn, ast);
 }
