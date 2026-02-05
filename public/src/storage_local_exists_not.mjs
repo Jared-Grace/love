dc56d1f57c998db8461904f126c2e0f3214b956e
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:50 -0500

    function_auto_path ..\love\public\src\string_split_newline.mjs

--- public/src/storage_local_exists_not.mjs
+++ public/src/storage_local_exists_not.mjs
@@ -1,5 +1,4 @@
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_exists_not(app_fn, key) {
   let exists = storage_local_exists(app_fn, key);
   return exists;
