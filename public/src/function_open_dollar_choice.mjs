04e4c8b0381a541b4d3d71b9c62cdfa68d56f2cb
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:03:16 -0500

    function_auto_path ..\love\public\src\function_open_app.mjs

--- public/src/function_open_dollar_choice.mjs
+++ public/src/function_open_dollar_choice.mjs
@@ -1,6 +1,5 @@
 import { js_dollar_new_name } from "../../../love/public/src/js_dollar_new_name.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_open_dollar_choice(code) {
   let combined = js_dollar_new_name(code);
   let v = await function_open(combined);
