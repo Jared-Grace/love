bd3270910e963e15770cf2e4b1771d2f7f2b8cf0
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:05:24 -0500

    function_auto_path ..\love\public\src\html_document_root.mjs

--- public/src/html_on_enter_lambda.mjs
+++ public/src/html_on_enter_lambda.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function html_on_enter_lambda(lambda) {
