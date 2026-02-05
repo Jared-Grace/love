37e2e47bc16921ac2dd6090ef197295f626958d0
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:25 -0500

    function_auto_path ..\love\public\src\storage_local_initialize.mjs

--- public/src/server_data_update_test.mjs
+++ public/src/server_data_update_test.mjs
@@ -3,7 +3,6 @@ import { server_data_get } from "../../../love/public/src/server_data_get.mjs";
 import { performance_next } from "../../../love/public/src/performance_next.mjs";
 import { performance_end } from "../../../love/public/src/performance_end.mjs";
 import { performance_start } from "../../../love/public/src/performance_start.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { data_generate } from "./data_generate.mjs";
 export async function server_data_update_test() {
   let p = performance_start("http_json");
--- public/src/string_combine.mjs
+++ public/src/string_combine.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 export function string_combine(left, right) {
   const together = left + right;
   return together;
