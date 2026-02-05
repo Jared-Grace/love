1c6d1afa8ca14c8f28c8b9d594c63cb63c648d56
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:10:51 -0500

    fw data_path data_delete

--- public/src/data_delete.mjs
+++ public/src/data_delete.mjs
@@ -1,4 +1,6 @@
+import { data_path } from "../../../love/public/src/data_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export function data_delete() {
   marker("1");
+  return data_path();
 }
