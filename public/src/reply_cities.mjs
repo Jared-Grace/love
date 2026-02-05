e99c9eb079394a26576765d0f3665ff5503ef37d
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:55 -0500

    function_auto_path ..\love\public\src\restart.mjs

--- public/src/reply_cities.mjs
+++ public/src/reply_cities.mjs
@@ -1,5 +1,4 @@
 import { reply_choice } from "../../../love/public/src/reply_choice.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function reply_cities() {
   let v = ["bungoma", "lahore"];
   let fn20 = reply_choice(v);
