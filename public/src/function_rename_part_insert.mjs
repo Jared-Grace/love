2dc65d633e6badf876ea852a34d7391abad7cda6
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 15:35:36 -0500

    function_auto_path ..\love\public\src\function_rename_part_insert.mjs

--- public/src/function_rename_part_insert.mjs
+++ public/src/function_rename_part_insert.mjs
@@ -1,7 +1,9 @@
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function function_rename_part_insert(f_name_before, f_name_after) {
   marker("1");
+  let parts = function_name_to_parts(f_name);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
