0080528f3150c5b107f67427a4b9097b104c5138
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:40 -0500

    function_auto_path ..\love\public\src\performance_next.mjs

--- public/src/performance_next.mjs
+++ public/src/performance_next.mjs
@@ -3,7 +3,6 @@ import { object_property_set } from "../../../love/public/src/object_property_se
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { performance_now } from "../../../love/public/src/performance_now.mjs";
 export function performance_next(p, category) {
