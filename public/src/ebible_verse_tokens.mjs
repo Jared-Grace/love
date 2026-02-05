513025c2a88f977e0d38049042a8dd25e4690ba2
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 16:20:01 -0500

    fws ebible_verse tokens

--- public/src/ebible_verse_tokens.mjs
+++ public/src/ebible_verse_tokens.mjs
@@ -1,4 +1,10 @@
+import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function ebible_verse_tokens() {
+export async function ebible_verse_tokens(
+  bible_folder,
+  chapter_code,
+  verse_number,
+) {
   marker("1");
+  return await ebible_verse(bible_folder, chapter_code, verse_number);
 }
