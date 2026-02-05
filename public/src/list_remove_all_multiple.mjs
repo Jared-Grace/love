740c6c3fc3eebcb388e17c72dfc85e5878f9d703
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:49:39 -0500

    function_auto_path ..\love\public\src\list_slice_from_indices.mjs

--- public/src/list_remove_all_multiple.mjs
+++ public/src/list_remove_all_multiple.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { list_remove_all } from "../../../love/public/src/list_remove_all.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function list_remove_all_multiple(removals, list) {
