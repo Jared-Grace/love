6a9557420b86240f0d8f538dcba4a74103bc2192
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:59 -0500

    function_auto_path ..\love\public\src\sandbox_4_a.mjs

--- web/public/src/sandbox_4_a.mjs
+++ web/public/src/sandbox_4_a.mjs
@@ -12,7 +12,6 @@ import { list_map_split_comma } from "../../../love/public/src/list_map_split_co
 import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function sandbox_4_a() {
   let file_path = folder_user_docs_path("nations_mentioned.carm.org.txt");
   let contents = await file_read(file_path);
