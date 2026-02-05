a4056c9002f1ab968c3d2c61143b2b21b526708d
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:40 -0500

    function_auto_path ..\love\public\src\whitespace_normalize.mjs

--- web/public/src/user_repo_get.mjs
+++ web/public/src/user_repo_get.mjs
@@ -1,10 +1,11 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { repo_exists_assert } from "../../../love/public/src/repo_exists_assert.mjs";
 import { data_get } from "../../../love/public/src/data_get.mjs";
 import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function user_repo_get() {
   let f_path = user_repo_path();
-  let { value: repo_name } = await data_get("repo_current", null, f_path);
+  let v = await data_get("repo_current", null, f_path);
+  let repo_name = object_property_get(v, "value");
   await repo_exists_assert(repo_name);
   return repo_name;
 }
