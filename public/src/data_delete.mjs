2a6fc20d3041c0692f02d7d4b755c2f3238217bb
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:11:20 -0500

    function_auto_path ..\love\public\src\data_delete.mjs

--- public/src/data_delete.mjs
+++ public/src/data_delete.mjs
@@ -1,7 +1,8 @@
+import { file_delete } from "../../../love/public/src/file_delete.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-export function data_delete() {
+export async function data_delete() {
   marker("1");
   let d_path = data_path();
-  return d_path;
+  await file_delete(file_path);
 }
