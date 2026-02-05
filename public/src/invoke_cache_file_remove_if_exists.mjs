822a271f84dc674075f9a6ab8b6b407c12cfaa70
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:55:09 -0500

    function_auto_path ..\love\public\src\invoke_cache_file_remove_if_exists.mjs

--- public/src/invoke_cache_file_remove_if_exists.mjs
+++ public/src/invoke_cache_file_remove_if_exists.mjs
@@ -1,7 +1,6 @@
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { invoke_cache_file_key_get } from "../../../love/public/src/invoke_cache_file_key_get.mjs";
 import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_file_remove.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function invoke_cache_file_remove_if_exists(fn, args) {
   let key_get = invoke_cache_file_key_get(fn, args);
   let cached_exists = file_exists;
