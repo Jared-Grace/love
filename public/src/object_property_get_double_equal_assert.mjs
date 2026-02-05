47a21f829572c4e73bd1b70f23006d18d4fc216a
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:31 -0500

    function_auto_path ..\love\public\src\marker_above_generic.mjs

--- public/src/object_property_get_double_equal_assert.mjs
+++ public/src/object_property_get_double_equal_assert.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function object_property_get_double_equal_assert(o1, o2, property_name) {
