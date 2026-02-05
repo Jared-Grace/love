0c3e1ce07a1bc33de46a73e44b83ea4fff4df48f
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 18:05:11 -0500

    function_auto_path ..\love\public\src\g_objection_generate_upload_path_generic.mjs

--- public/src/g_sermon_generate_upload.mjs
+++ public/src/g_sermon_generate_upload.mjs
@@ -2,7 +2,6 @@ import { error } from "../../../love/public/src/error.mjs";
 import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
 import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { app_g_bible } from "./app_g_bible.mjs";
 export async function g_sermon_generate_upload() {
   error(
