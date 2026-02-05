0b1e44f40102b283d86810b52fb0290b95c920c8
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 16:20:16 -0500

    function_auto_path ..\love\public\src\ebible_verse_tokens.mjs

--- public/src/ebible_verse_tokens.mjs
+++ public/src/ebible_verse_tokens.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
 export async function ebible_verse_tokens(
@@ -6,6 +7,7 @@ export async function ebible_verse_tokens(
   verse_number,
 ) {
   marker("1");
-  let v = await ebible_verse(bible_folder, chapter_code, verse_number);st
+  let v = await ebible_verse(bible_folder, chapter_code, verse_number);
+  object_property_set(object, property_name, value);
   return v;
 }
