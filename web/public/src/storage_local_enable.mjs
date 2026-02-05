4ac0187eadcd7116553676988c7bcce82c084746
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:23 -0500

    function_auto_path ..\love\public\src\storage_local_set_exists_not.mjs

--- web/public/src/storage_local_enable.mjs
+++ web/public/src/storage_local_enable.mjs
@@ -1,5 +1,4 @@
 import { storage_local_enable_set } from "../../../love/public/src/storage_local_enable_set.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_enable(context) {
   const v = true;
   let dictionary = storage_local_enable_set(v, context);
