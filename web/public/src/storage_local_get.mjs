5c6693cf716219721d12a125ea0adb6bbd3b1807
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:28 -0500

    function_auto_path ..\love\public\src\repo_path.mjs

--- web/public/src/storage_local_get.mjs
+++ web/public/src/storage_local_get.mjs
@@ -1,7 +1,6 @@
 import { storage_local_get_global } from "../../../love/public/src/storage_local_get_global.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 import { storage_local_specify_get } from "../../../love/public/src/storage_local_specify_get.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
 export function storage_local_get(app_fn, key) {
   let storage_local_key = storage_local_key_get(app_fn, key);
