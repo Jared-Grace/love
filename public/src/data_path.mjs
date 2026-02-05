735c08c2c3c798045d305da5f9bd6fafcbed20c3
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:06:48 -0500

    function_auto_path ..\love\public\src\data_path.mjs

--- public/src/data_path.mjs
+++ public/src/data_path.mjs
@@ -1,6 +1,8 @@
+import { data_folder } from "../../../love/public/src/data_folder.mjs";
 import { data_path_generic } from "../../../love/public/src/data_path_generic.mjs";
 export function data_path() {
   let inner = "";
   let d_path = data_path_generic(inner, "data");
   return d_path;
+  const folder = data_folder();
 }
