728214612277c3da8138e290f0a72cddf02c997c
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:50:52 -0500

    function_auto_path ..\love\public\src\list_to_dictionary_async.mjs

--- public/src/list_to_dictionary_async.mjs
+++ public/src/list_to_dictionary_async.mjs
@@ -1,7 +1,6 @@
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_to_dictionary_generic_async } from "../../../love/public/src/list_to_dictionary_generic_async.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function list_to_dictionary_async(list, lambda$item) {
   let v = list_to_dictionary_generic_async(lambda$item);
   let dictionary = object_property_get(v, "dictionary");
