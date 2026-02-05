f7748c250b0e91845f1a537bb962287d184e2bfe
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:59:30 -0500

    function_auto_path ..\love\public\src\data_files_update.mjs

--- public/src/data_prompts_path.mjs
+++ public/src/data_prompts_path.mjs
@@ -1,5 +1,4 @@
 import { file_name_json_folder_gitignore } from "../../../karate_code/public/src/file_name_json_folder_gitignore.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function data_prompts_path() {
   const name = "prompts";
   let f_path = file_name_json_folder_gitignore(name);
