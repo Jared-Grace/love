3c75002e0b33b84c75bd6385551755466357fa64
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:09 -0500

    function_auto_path ..\love\public\src\object_property_exists_not_assert.mjs

--- public/src/list_map_async.mjs
+++ public/src/list_map_async.mjs
@@ -1,6 +1,5 @@
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 export async function list_map_async(list, lambda$item) {
   list_is_assert(list);
