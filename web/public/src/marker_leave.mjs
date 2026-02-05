2ae6c19bfaf0825ab4052ea0b9163c52f4f7eaf4
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:31 -0500

    function_auto_path ..\love\public\src\marker_above_generic.mjs

--- web/public/src/marker_leave.mjs
+++ web/public/src/marker_leave.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { list_get_end_2 } from "../../../love/public/src/list_get_end_2.mjs";
@@ -6,10 +7,9 @@ import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 import { list_get_end } from "../../../love/public/src/list_get_end.mjs";
 import { function_transform_marker_current } from "../../../love/public/src/function_transform_marker_current.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export async function marker_leave() {
   async function lambda(a) {
-    let { stack } = a;
+    let stack = object_property_get(a, "stack");
     let stack4 = list_get_end(stack, 4);
     let stack5 = list_get_end(stack, 5);
     list_is_assert(stack5);
