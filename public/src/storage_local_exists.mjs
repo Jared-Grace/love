e43f10e7fa29a22838306f924b349191d6902be2
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:50 -0500

    function_auto_path ..\love\public\src\string_split_newline.mjs

--- public/src/storage_local_exists.mjs
+++ public/src/storage_local_exists.mjs
@@ -2,7 +2,6 @@ import { storage_local_specify_get_json } from "../../../love/public/src/storage
 import { storage_local_exists_global } from "../../../love/public/src/storage_local_exists_global.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 import { storage_local_key_get } from "../../../love/public/src/storage_local_key_get.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_exists(app_fn, key) {
   let storage_local_key = storage_local_key_get(app_fn, key);
   if (storage_local_enabled()) {
--- public/src/string_replace.mjs
+++ public/src/string_replace.mjs
@@ -1,6 +1,5 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function string_replace(s, from, to) {
   let split = string_split(s, from);
   let replaced = list_join(split, to);
