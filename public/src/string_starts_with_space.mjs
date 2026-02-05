3148abc089ffeacc9e86e99f87717584ac9b23de
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:53:54 -0500

    function_auto_path ..\love\public\src\string_starts_with_space.mjs

--- public/src/string_starts_with_space.mjs
+++ public/src/string_starts_with_space.mjs
@@ -1,4 +1,3 @@
-import { marker } from "../../../love/public/src/marker.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 export function string_starts_with_space(s) {
   let sw2 = string_starts_with(s, " ");
