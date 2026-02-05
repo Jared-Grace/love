37fee72f9655f57907616900eb8d54ebe72b5faa
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:24 -0500

    function_auto_path ..\love\public\src\marker_down.mjs

---
+++ web/public/src/object_property_equals_json_lambda.mjs
@@ -0,0 +1,11 @@
+import { object_property_equals_json } from "../../../love/public/src/object_property_equals_json.mjs";
+export function object_property_equals_json_lambda(
+  property_name,
+  property_value,
+) {
+  let v2 = function lambda(item) {
+    let v = object_property_equals_json(item, property_name, property_value);
+    return v;
+  };
+  return v2;
+}
