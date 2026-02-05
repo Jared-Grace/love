c4aede1f5eb3ab3565a76e7a03178a78312a68d9
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:52:21 -0500

    function_auto_path ..\love\public\src\list_get_or.mjs

--- web/public/src/list_first_last.mjs
+++ web/public/src/list_first_last.mjs
@@ -1,7 +1,6 @@
 import { list_multiple_is_assert } from "../../../love/public/src/list_multiple_is_assert.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function list_first_last(list) {
   list_multiple_is_assert(list);
   let first = list_first(list);
