fd0a6a3dcf1bc2799d0deb49021eeb0063d6a93d
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 17:54:04 -0500

    function_auto_path ..\love\public\src\string_transform_lookup.mjs

--- web/public/src/storage_local_remove_context.mjs
+++ web/public/src/storage_local_remove_context.mjs
@@ -1,6 +1,5 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { storage_local_remove } from "../../../love/public/src/storage_local_remove.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function storage_local_remove_context(context, key) {
   let app_fn = object_property_get(context, "app_fn");
   let v = storage_local_remove(app_fn, key);
--- web/public/src/string_includes_lambda.mjs
+++ web/public/src/string_includes_lambda.mjs
@@ -1,5 +1,4 @@
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
-import { marker } from "../../../love/public/src/marker.mjs";
 export function string_includes_lambda(item) {
   let l = function string_includes_lambda_inner(input) {
     let si = string_includes(input, item);
