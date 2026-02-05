d7b36177661d8fecf7bc71c3949c9d9afde082f8
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 15:58:37 -0500

    rs integer_to try

--- public/src/ebible_chapter_code_to_name.mjs
+++ public/src/ebible_chapter_code_to_name.mjs
@@ -1,9 +1,9 @@
 import { ebible_chapter_code_to_name_code } from "../../../love/public/src/ebible_chapter_code_to_name_code.mjs";
 import { string_to } from "../../../love/public/src/string_to.mjs";
-import { integer_to } from "../../../love/public/src/integer_to.mjs";
+import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
 export function ebible_chapter_code_to_name(chapter_code) {
   let s = ebible_chapter_code_to_name_code(chapter_code);
-  let i = integer_to(s);
+  let i = integer_to_try(s);
   let chapter_name = string_to(i);
   return chapter_name;
 }
