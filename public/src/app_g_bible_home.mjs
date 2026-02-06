c58f309f09261cb1af9a42ae82020769764323b0
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:26:55 -0500

    function_auto_path ..\love\public\src\app_g_bible_home.mjs

--- public/src/app_g_bible_home.mjs
+++ public/src/app_g_bible_home.mjs
@@ -26,7 +26,7 @@ import { g_sermon_generate_download } from "../../../love/public/src/g_sermon_ge
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function app_g_bible_home(context) {
-  let value = null;
+  let downloaded = null;
   let chapter_code = null;
   let r = null;
   async function lambda5(la) {
@@ -34,8 +34,8 @@ export async function app_g_bible_home(context) {
       let p = object_property_get(a, "p");
       let verse_number = object_property_get(a, "verse_number");
       chapter_code = object_property_get(a, "chapter_code");
-      value = await g_sermon_generate_download(chapter_code);
-      let passages = object_property_get(value, "passages");
+      downloaded = await g_sermon_generate_download(chapter_code);
+      let passages = object_property_get(downloaded, "passages");
       function lambda2(passage) {
         let verse_numbers = object_property_get(passage, "verse_numbers");
         let mapped = list_map(verse_numbers, integer_to_try);
@@ -70,7 +70,7 @@ export async function app_g_bible_home(context) {
   async function lambda4() {
     invoke_multiple(updates);
     let destination = g_sermon_generate_upload_path(chapter_code);
-    await firebase_upload_object_compressed(destination, value);
+    await firebase_upload_object_compressed(destination, downloaded);
   }
   let component2 = html_button(bar, "Update", lambda4);
 }
