fb5397b907971c4a84fb9c379823556256b1752a
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:30:55 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

--- web/public/src/app_ceb_bible_home.mjs
+++ web/public/src/app_ceb_bible_home.mjs
@@ -1,3 +1,16 @@
+import { string_replace } from "../../../love/public/src/string_replace.mjs";
+import { newline_windows } from "../../../love/public/src/newline_windows.mjs";
+import { newline } from "../../../love/public/src/newline.mjs";
+import { html_value_get } from "../../../love/public/src/html_value_get.mjs";
+import { html_value_set } from "../../../love/public/src/html_value_set.mjs";
+import { html_rows_set } from "../../../love/public/src/html_rows_set.mjs";
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
+import { html_mobile_default_font_size } from "../../../love/public/src/html_mobile_default_font_size.mjs";
+import { html_textarea } from "../../../love/public/src/html_textarea.mjs";
+import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
+import { list_size } from "../../../love/public/src/list_size.mjs";
+import { app_g_openai_split } from "../../../love/public/src/app_g_openai_split.mjs";
+import { app_g_bible_home_inner } from "../../../love/public/src/app_g_bible_home_inner.mjs";
 import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
@@ -11,6 +24,32 @@ import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function app_ceb_bible_home(context) {
+  await app_g_bible_home_inner(
+    chapter_code,
+    downloaded,
+    on_passage,
+    r,
+    context,
+  );
+  function on_passage(passage, p) {
+    let sermon = object_property_get(passage, "sermon");
+    let mapped2 = app_g_openai_split(sermon);
+    let size = list_size(mapped2);
+    let joined = list_join_newline(mapped2);
+    let ta = html_textarea(p);
+    html_mobile_default_font_size(ta);
+    html_width_full(ta);
+    html_rows_set(ta, size);
+    html_value_set(ta, joined);
+    let update = function lambda3() {
+      let value2 = html_value_get(ta);
+      let from = newline();
+      let to = newline_windows();
+      let replaced = string_replace(value2, from, to);
+      object_property_set(passage, "sermon", replaced);
+    };
+    la(update);
+  }
   return;
   let chapter_code = null;
   let r = null;
