ffbbb867dd3144e59cbb8d32fe837d5e125237c0
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:12:56 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

--- public/src/app_ceb_bible_home.mjs
+++ public/src/app_ceb_bible_home.mjs
@@ -11,13 +11,13 @@ export async function app_ceb_bible_home(context) {
   let chapter_code = null;
   let r = null;
   async function lambda(a) {
+    let p = object_property_get(a, "p");
+    let verse_number = object_property_get(a, "verse_number");
+    chapter_code = object_property_get(a, "chapter_code");
     let value = await app_ceb_bible_gloss_generate_download(chapter_code);
     log({
       value,
     });
-    let p = object_property_get(a, "p");
-    let verse_number = object_property_get(a, "verse_number");
-    chapter_code = object_property_get(a, "chapter_code");
     let verses = await ebible_verses("cebulb", chapter_code);
     let item = list_find_property(verses, "verse_number", verse_number);
     let text = object_property_get(item, "text");
