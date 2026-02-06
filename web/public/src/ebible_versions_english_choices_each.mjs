7fb1412543862d867d39d80c881ec6e1d066ba8d
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 22:27:03 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_choices_each.mjs

--- web/public/src/ebible_versions_english_choices_each.mjs
+++ web/public/src/ebible_versions_english_choices_each.mjs
@@ -7,8 +7,7 @@ export async function ebible_versions_english_choices_each(
 ) {
   let english_choices = await ebible_versions_english_choices();
   let index = list_index_of(english_choices, "eng-t4t");
-  let skipped = null;
-  skipped = list_skip(english_choices, index);
+  let skipped = list_skip(english_choices, index);
   skipped = english_choices;
   await each_async(skipped, lambda$bible_folder);
 }
