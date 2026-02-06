6d6f7a56cabcc82b91e071036283c9a165b3d475
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:18:03 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book.mjs

--- web/public/src/g_sermon_generate_book.mjs
+++ web/public/src/g_sermon_generate_book.mjs
@@ -1,7 +1,8 @@
 import { newline_windows_code } from "../../../love/public/src/newline_windows_code.mjs";
 import { g_sermon_generate_book_generic } from "../../../love/public/src/g_sermon_generate_book_generic.mjs";
 import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
-export async function g_sermon_generate_book(bible_folder, book_code) {'dont want to accidentally overwrite sermons'
+export async function g_sermon_generate_book(bible_folder, book_code) {
+  "dont want to accidentally overwrite sermons";
   return;
   const prompt_system =
     `You are a Christian preacher. You will be given a passage and its context. Rewrite the passage as follows:
