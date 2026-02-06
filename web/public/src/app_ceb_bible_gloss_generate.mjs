9e206e557f026d6ba0defcc058da5ea3c2a2d57a
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:33:43 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate.mjs

--- web/public/src/app_ceb_bible_gloss_generate.mjs
+++ web/public/src/app_ceb_bible_gloss_generate.mjs
@@ -4,7 +4,8 @@ export async function app_ceb_bible_gloss_generate() {
   let bible_folder = "engbsb";
   let book_code = "JAS";
   const prompt_system = `You will be given a Bible passage and its context in Cebuano. For each Cebuano word, output an English gloss. Each gloss should be a string in a JSON array.`;
-  const prompt_user_middle = "";
+  const prompt_user_middle =
+    "Here is the passage to output English glosses for:";
   let fn = app_ceb_bible_gloss_generate;
   let property_name = g_objection_generate_property();
   await g_sermon_generate_book_generic(
