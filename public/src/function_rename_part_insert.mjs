4935e3a5afc93f53c200550231ff13531053b2dc
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:50:46 -0500

    function_wrap_part_last_remove function_rename_part_insert_at

---
+++ public/src/function_rename_part_insert.mjs
@@ -0,0 +1,12 @@
+import { function_rename_part_insert_at } from "../../../love/public/src/function_rename_part_insert_at.mjs";
+export async function function_rename_part_insert(
+  f_name_before,
+  part,
+  index_string,
+) {
+  return await function_rename_part_insert_at(
+    f_name_before,
+    part,
+    index_string,
+  );
+}
