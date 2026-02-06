13942103dfd7772f0683819b664f6dbe2aba2d2a
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 14:26:37 -0500

    function_auto_path ..\love\public\src\list_join_colon_2.mjs

--- public/src/list_join_colon_2.mjs
+++ public/src/list_join_colon_2.mjs
@@ -1,5 +1,5 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
 export function list_join_colon_2(texts) {
-  let joined2 = list_join(texts, "::");
+  let joined2 = list_join(texts, " :: ");
   return joined2;
 }
