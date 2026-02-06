27c796ac5cdaf8d08165ccf510273b78feb7d8c2
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:36:04 -0500

    pn app_g_bible_home_inner download g_sermon_generate_download

--- public/src/app_ceb_bible_home.mjs
+++ public/src/app_ceb_bible_home.mjs
@@ -25,7 +25,7 @@ import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function app_ceb_bible_home(context) {
-  await app_g_bible_home_inner(context, on_passage);
+  await app_g_bible_home_inner(context, on_passage, g_sermon_generate_download);
   function on_passage(passage, p) {
     log({
       passage,
