7d2f2aec0019b8b25c27480deca6a6251955657e
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 14:26:07 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,7 +1,8 @@
-import { log } from "../../../love/public/src/log.mjs";
+import { list_join_colon_2 } from "../../../love/public/src/list_join_colon_2.mjs";
 export async function sandbox_5() {
-  log(1);
-  log(4);
-  let v = 5 + 9;
-  return v;
+  let l = [
+    "Si Santiago, usa ka sulugoon sa Dios ug kang Ginoong Jesu-Cristo, ngadto sa 12 ka mga tribo nga Nagkatibulaag, mga pangamusta.",
+    "James, a servant of God and of the Lord Jesus Christ, To the twelve tribes of the Dispersion: Greetings.",
+  ];
+  let joined2 = list_join_colon_2(texts);
 }
