cc8f7a4a2b446db6bc0232110b920c2f501e5ddb
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:50:39 -0500

    function_auto_path ..\love\public\src\each_multiple_async.mjs

--- public/src/each_multiple_async.mjs
+++ public/src/each_multiple_async.mjs
@@ -1,13 +1,13 @@
-import { lists_sizes_equal_assert } from "../../../love/public/src/lists_sizes_equal_assert.mjs";
-import { each_index_async } from "../../../love/public/src/each_index_async.mjs";
+import { each_range_async } from "../../../love/public/src/each_range_async.mjs";
+import { lists_size_max } from "../../../love/public/src/lists_size_max.mjs";
 import { lists_get } from "../../../love/public/src/lists_get.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export async function each_multiple_async(lists, lambda) {
-  lists_sizes_equal_assert(lists);
+  let max = lists_size_max(lists);
   let first = list_first(lists);
   async function lambda2(item, index) {
     let items = lists_get(lists, index);
     await lambda(items);
   }
-  await each_index_async(first, lambda2);
+  await each_range_async(first, lambda2);
 }
