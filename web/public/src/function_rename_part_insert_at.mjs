3f637da86766edd65300690e3c1046249075da2d
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:36:32 -0500

    rr rpi_at rpi function_rename_part_insert

--- web/public/src/function_rename_part_insert_at.mjs
+++ web/public/src/function_rename_part_insert_at.mjs
@@ -3,7 +3,11 @@ import { function_name_combine_multiple } from "../../../love/public/src/functio
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
-export async function rpi_at(f_name_before, part, index_string) {
+export async function function_rename_part_insert_at(
+  f_name_before,
+  part,
+  index_string,
+) {
   let index = integer_to(index_string);
   let parts = function_name_to_parts(f_name_before);
   list_insert(parts, index, part);
