7ba1152ce7c33091876e0d23f9063fdf4ac54380
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:00:11 -0500

    function_auto_path ..\love\public\src\openai_chat_completions.mjs

--- web/public/src/openai_chat_completions.mjs
+++ web/public/src/openai_chat_completions.mjs
@@ -1,15 +1,12 @@
-import { command_line } from "../../../love/public/src/command_line.mjs";
-import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
-import { openai_key_folder } from "../../../love/public/src/openai_key_folder.mjs";
-import { py_script_run_cmd } from "../../../love/public/src/py_script_run_cmd.mjs";
+import { openai_chat_completions_generic } from "../../../love/public/src/openai_chat_completions_generic.mjs";
 export async function openai_chat_completions(
   input_file_path,
   output_file_path,
 ) {
   const openai_script_name = "chat_completions";
-  let v = py_script_run_cmd("openai_" + openai_script_name);
-  let k = openai_key_folder();
-  let concated = [v, k + "openai.txt", input_file_path, output_file_path];
-  let joined = list_join_space(concated);
-  let stdout = await command_line(joined);
+  await openai_chat_completions_generic(
+    openai_script_name,
+    input_file_path,
+    output_file_path,
+  );
 }
