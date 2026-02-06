d49cdbf3ac20de69d6d573c92a8910a03e3111fb
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 20:17:19 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_main.mjs

--- public/src/app_ceb_bible_main.mjs
+++ public/src/app_ceb_bible_main.mjs
@@ -2,7 +2,7 @@ import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs"
 import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { app_g_bible_screens } from "../../../love/public/src/app_g_bible_screens.mjs";
 import { app_g_bible } from "../../../love/public/src/app_g_bible.mjs";
-export function app_ceb_bible_main() {
+export function app_ceb_bible_main(context) {
   let app_fn = app_g_bible;
   let screens = app_g_bible_screens();
   app_bible_main_generic_before(context, app_fn, screens, app_g_bible_home);
