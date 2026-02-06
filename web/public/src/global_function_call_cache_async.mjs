ff2cc77bef8558606e24a038a406d2fbadd0bcce
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 12:45:35 -0500

    function_auto_path ..\love\public\src\global_function_call_cache_async.mjs

--- web/public/src/global_function_call_cache_async.mjs
+++ web/public/src/global_function_call_cache_async.mjs
@@ -1,9 +1,9 @@
 import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { list_to } from "../../../love/public/src/list_to.mjs";
-export async function global_function_call_cache_async(args1, fn, get) {
-  let args = list_to(args1);
-  let json = json_to(args);
+export async function global_function_call_cache_async(args, fn, get) {
+  let args_list = list_to(args);
+  let json = json_to(args_list);
   let value = await global_function_property_initialize_async(fn, json, get);
   return value;
 }
