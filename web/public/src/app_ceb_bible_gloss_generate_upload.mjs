17d5ba0200150cd49e4bd177eabe453c1c7492ec
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:00:01 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_upload.mjs

--- web/public/src/app_ceb_bible_gloss_generate_upload.mjs
+++ web/public/src/app_ceb_bible_gloss_generate_upload.mjs
@@ -2,7 +2,7 @@ import { app_ceb_bible_gloss_generate_upload_path } from "../../../love/public/s
 import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 export async function app_ceb_bible_gloss_generate_upload() {
-  let path_get = app_ceb_bible_gloss_generate_upload_path;
   let fn = app_ceb_bible_gloss_generate;
+  let path_get = app_ceb_bible_gloss_generate_upload_path;
   await g_generate_upload_generic(path_get, fn);
 }
