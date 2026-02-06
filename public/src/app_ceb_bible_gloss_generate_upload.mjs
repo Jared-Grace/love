dbdf0ccf986d6fd1f2270816e7d05c324aedfb3e
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 17:55:13 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_upload.mjs

--- public/src/app_ceb_bible_gloss_generate_upload.mjs
+++ public/src/app_ceb_bible_gloss_generate_upload.mjs
@@ -1,6 +1,5 @@
 import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
 import { error } from "../../../love/public/src/error.mjs";
-import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 import { app_g_bible } from "./app_g_bible.mjs";
 export async function app_ceb_bible_gloss_generate_upload() {
@@ -8,7 +7,7 @@ export async function app_ceb_bible_gloss_generate_upload() {
     "this maybe should not be ran because it would overwrite edits from " +
       app_g_bible,
   );
-  let path_get = g_sermon_generate_upload_path;
+  let path_get = () => {};
   let fn = app_ceb_bible_gloss_generate;
   await g_generate_upload_generic(path_get, fn);
 }
