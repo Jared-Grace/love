f7cdcc79a0475a71f712e02dc8facb79d0afa306
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:45:27 -0500

    function_auto_path ..\love\public\src\function_wrap_part_last_remove.mjs

--- public/src/function_wrap_part_last_remove.mjs
+++ public/src/function_wrap_part_last_remove.mjs
@@ -1,11 +1,11 @@
-import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
-export async function function_wrap_part_last_remove(f_name, suffix) {
-  assert_arguments(arguments, 2);
+export async function function_wrap_part_last_remove(f_name) {
+  assert_arguments(arguments, 1);
   let unaliased = await function_name_unalias_only(f_name);
-  let f_path = function_name_to_path(f_name2);
+  let f_path = function_name_to_parts(unaliased);
   let v = await function_wrap(unaliased, f_name_wrapped);
   return v;
 }
