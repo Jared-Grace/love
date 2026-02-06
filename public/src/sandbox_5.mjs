66610984fbc104e0e58cb934714cd9c9b05dc2c4
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 14:26:16 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -4,5 +4,6 @@ export async function sandbox_5() {
     "Si Santiago, usa ka sulugoon sa Dios ug kang Ginoong Jesu-Cristo, ngadto sa 12 ka mga tribo nga Nagkatibulaag, mga pangamusta.",
     "James, a servant of God and of the Lord Jesus Christ, To the twelve tribes of the Dispersion: Greetings.",
   ];
-  let joined2 = list_join_colon_2(texts);
+  let joined2 = list_join_colon_2(l);
+  return joined2;
 }
