86cebc8329b659e7ea173f2a2995b6ef4e00c97f
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 17:54:40 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_upload.mjs

--- web/public/src/app_ceb_bible_gloss_generate_upload.mjs
+++ web/public/src/app_ceb_bible_gloss_generate_upload.mjs
@@ -1,6 +1,6 @@
+import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
-import { g_sermon_generate } from "../../../love/public/src/g_sermon_generate.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 import { app_g_bible } from "./app_g_bible.mjs";
 export async function app_ceb_bible_gloss_generate_upload() {
@@ -9,6 +9,6 @@ export async function app_ceb_bible_gloss_generate_upload() {
       app_g_bible,
   );
   let path_get = g_sermon_generate_upload_path;
-  let fn = g_sermon_generate;
+  let fn = app_ceb_bible_gloss_generate;
   await g_generate_upload_generic(path_get, fn);
 }
