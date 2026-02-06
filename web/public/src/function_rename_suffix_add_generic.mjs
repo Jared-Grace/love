485fbdc85b4efa2dc7c7858e2ef98119c0a83c49
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:36:05 -0500

    function_auto_path ..\love\public\src\function_rename_suffix_add_generic.mjs

--- web/public/src/function_rename_suffix_add_generic.mjs
+++ web/public/src/function_rename_suffix_add_generic.mjs
@@ -1,3 +1,4 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { function_name_separator_padded_not_is_assert } from "../../../love/public/src/function_name_separator_padded_not_is_assert.mjs";
 export async function function_rename_suffix_add_generic(
@@ -6,6 +7,7 @@ export async function function_rename_suffix_add_generic(
   f_name_before,
 ) {
   function_name_separator_padded_not_is_assert(other);
+  let unaliased = await function_name_unalias_only(name);
   let f_name_after = fn(f_name_before, other);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
