043b0a739946f32e492d94d92d1c88b29206e373
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 21:59:18 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

--- public/src/app_ceb_bible_home.mjs
+++ public/src/app_ceb_bible_home.mjs
@@ -2,7 +2,6 @@ import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
@@ -20,9 +19,8 @@ export async function app_ceb_bible_home(context) {
       chapter_code = object_property_get(a, "chapter_code");
       let verses = await ebible_verses("cebulb", chapter_code);
       let item = list_find_property(verses, "verse_number", verse_number);
-      let text2 = object_property_get(item, "text");
-      let div = html_div_text(root, text);
-      each(verses, lambda2);
+      let text = object_property_get(item, "text");
+      let div = html_div_text(p, text);
     }
     r = await app_bible_home_generic(context, lambda);
   }
