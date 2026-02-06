3638fb6d01a1f70350a89793019c655810d225f9
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:51:30 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic.mjs

--- public/src/g_sermon_generate_book_generic.mjs
+++ public/src/g_sermon_generate_book_generic.mjs
@@ -65,7 +65,7 @@ export async function g_sermon_generate_book_generic(
     lambda3,
   );
   async function lambda4(la) {
-    async function each_chapter(verses_chapter) {
+    async function each_chapter(verses_chapter_folders) {
       let first = list_first(verses_chapter);
       let chapter_code = object_property_get(first, "chapter_code");
       let interlinear = object_property_get(chapters_interlinear, chapter_code);
