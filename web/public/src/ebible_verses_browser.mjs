2bfd886cabbf23c5edbc0fd010990dc56c72328e
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 12:45:17 -0500

    function_auto_path ..\love\public\src\ebible_verses_browser.mjs

--- web/public/src/ebible_verses_browser.mjs
+++ web/public/src/ebible_verses_browser.mjs
@@ -1,6 +1,4 @@
-import { json_to } from "../../../love/public/src/json_to.mjs";
-import { list_to } from "../../../love/public/src/list_to.mjs";
-import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
+import { global_function_call_cache_async } from "../../../love/public/src/global_function_call_cache_async.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { firebase_storage_download_json_decompress } from "../../../love/public/src/firebase_storage_download_json_decompress.mjs";
 import { ebible_firebase_upload_path } from "../../../love/public/src/ebible_firebase_upload_path.mjs";
@@ -13,8 +11,6 @@ export async function ebible_verses_browser(bible_folder, chapter_code) {
   }
   let fn = ebible_verses_browser;
   let args1 = arguments;
-  let args = list_to(args1);
-  let json = json_to(args);
-  let value = await global_function_property_initialize_async(fn, json, get);
+  let value = await global_function_call_cache_async(args1, fn, get);
   return value;
 }
