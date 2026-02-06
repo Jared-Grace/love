a5fcf1ec9d0d5a48c1549dfb6a6018b372d4d559
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 17:55:24 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_upload.mjs

--- public/src/app_ceb_bible_gloss_generate_upload.mjs
+++ public/src/app_ceb_bible_gloss_generate_upload.mjs
@@ -1,13 +1,15 @@
+import { g_objection_generate_upload_path_generic } from "../../../love/public/src/g_objection_generate_upload_path_generic.mjs";
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
-import { error } from "../../../love/public/src/error.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
-import { app_g_bible } from "./app_g_bible.mjs";
 export async function app_ceb_bible_gloss_generate_upload() {
-  error(
-    "this maybe should not be ran because it would overwrite edits from " +
-      app_g_bible,
-  );
-  let path_get = function lambda() {};
+  let path_get = function lambda() {
+    let f_name = fn_name("g_sermon_generate_upload");
+    let destination = g_objection_generate_upload_path_generic(
+      f_name,
+      chapter_code,
+    );
+  };
   let fn = app_ceb_bible_gloss_generate;
   await g_generate_upload_generic(path_get, fn);
 }
