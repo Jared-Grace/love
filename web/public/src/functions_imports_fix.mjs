d7f1364d3c1f181363e03dc7a2fd377257c5b463
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:58:02 -0500

    function_auto_path ..\love\public\src\functions_imports_fix.mjs

--- web/public/src/functions_imports_fix.mjs
+++ web/public/src/functions_imports_fix.mjs
@@ -1,7 +1,7 @@
+import { js_imports_fix } from "../../../love/public/src/js_imports_fix.mjs";
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
-import { js_log_remove } from "../../../love/public/src/js_log_remove.mjs";
 export async function functions_imports_fix() {
-  let lambda = js_log_remove;
+  let lambda = js_imports_fix;
   let v = await functions_transform(lambda);
   return v;
 }
