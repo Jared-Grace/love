e00ab6436f3dd270953e5f1326d706ac565896df
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 17:36:15 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- web/public/src/sandbox_5.mjs
+++ web/public/src/sandbox_5.mjs
@@ -1,4 +1,6 @@
 import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
 export async function sandbox_5() {
-  await file_overwrite_json(file_path, object);
+  await file_overwrite_json("a/b/c.json", {
+    JESUS: "LORD",
+  });
 }
