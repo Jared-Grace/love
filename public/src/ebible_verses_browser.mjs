adce60495fe2ada73f459ebcd9f66c26a7cfd049
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 12:41:07 -0500

    function_auto_path ..\love\public\src\ebible_verses_browser.mjs

--- public/src/ebible_verses_browser.mjs
+++ public/src/ebible_verses_browser.mjs
@@ -3,6 +3,7 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { firebase_storage_download_json_decompress } from "../../../love/public/src/firebase_storage_download_json_decompress.mjs";
 import { ebible_firebase_upload_path } from "../../../love/public/src/ebible_firebase_upload_path.mjs";
 export async function ebible_verses_browser(bible_folder, chapter_code) {
+  let fn = ebible_verses_browser;
   function get() {}
   let value = await global_function_property_initialize_async(
     fn,
