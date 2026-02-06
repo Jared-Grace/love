b700883185115385ca3fdbf0ef9076d5140466e2
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:30:47 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

--- public/src/app_ceb_bible_home.mjs
+++ public/src/app_ceb_bible_home.mjs
@@ -11,6 +11,7 @@ import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export async function app_ceb_bible_home(context) {
+  return;
   let chapter_code = null;
   let r = null;
   async function lambda(a) {
