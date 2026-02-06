ed37e9e3aa1a077d0cf2bc4990f59ca40730ae09
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:02:36 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_download.mjs

--- public/src/g_sermon_generate_download.mjs
+++ public/src/g_sermon_generate_download.mjs
@@ -2,6 +2,7 @@ import { firebase_storage_download_json_decompress } from "../../../love/public/
 import { global_function_property_initialize_async } from "../../../love/public/src/global_function_property_initialize_async.mjs";
 import { g_sermon_generate_upload_path } from "../../../love/public/src/g_sermon_generate_upload_path.mjs";
 export async function g_sermon_generate_download(chapter_code) {
+  let path_get = g_sermon_generate_upload_path;
   async function get() {
     let destination = g_sermon_generate_upload_path(chapter_code);
     let o = await firebase_storage_download_json_decompress(destination);
