9e458ee98ef62b268f44250cced28351e482e08d
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:47:46 -0500

    function_auto_path ..\love\public\src\each_multiple_async.mjs

--- web/public/src/each_multiple_async.mjs
+++ web/public/src/each_multiple_async.mjs
@@ -1,3 +1,4 @@
+import { assert } from "../../../love/public/src/assert.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { each_index_async } from "../../../love/public/src/each_index_async.mjs";
@@ -5,11 +6,13 @@ import { lists_get } from "../../../love/public/src/lists_get.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export async function each_multiple_async(lists, lambda) {
   let first = list_first(lists);
-  let size2 = list_size(list2);
-  function lambda3(item2) {
+  let fs = list_size(first);
+  function lambda3(list) {
     let size = list_size(list);
+    return size === fs;
   }
   let a = list_all(lists, lambda3);
+  assert(b);
   async function lambda2(item, index) {
     let items = lists_get(lists, index);
     await lambda(items);
