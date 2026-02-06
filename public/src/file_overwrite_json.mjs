11f5136d330d43bb0b96c5f2b530d513add4a0e6
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 17:31:12 -0500

    function_auto_path ..\love\public\src\file_overwrite_json.mjs

--- public/src/file_overwrite_json.mjs
+++ public/src/file_overwrite_json.mjs
@@ -11,7 +11,7 @@ export async function file_overwrite_json(file_path, object) {
     await file_overwrite(file_path, json);
     return;
   }
-  let result = await file_parent_exists_ensure(file_path2);
+  let result = await file_parent_exists_ensure(file_path);
   "Using " +
     json_to.name +
     " did not work on sufficiently large object, whereas this did:";
