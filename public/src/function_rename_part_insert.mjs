70890ab1721c1ffc5d6ca936cb0fc1386d776327
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:40:33 -0500

    function_auto_path ..\love\public\src\function_rename_part_insert.mjs

--- public/src/function_rename_part_insert.mjs
+++ public/src/function_rename_part_insert.mjs
@@ -1,7 +1,16 @@
+import { integer_to } from "../../../love/public/src/integer_to.mjs";
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_part_insert(
   f_name_before,
   part,
   index_string,
 ) {
-  await function_rename_part_insert_at(f_name_before, part, index_string);
+  let index = integer_to(index_string);
+  let parts = function_name_to_parts(f_name_before);
+  list_insert(parts, index, part);
+  let f_name_after = function_name_combine_multiple(parts);
+  await function_rename(f_name_before, f_name_after);
 }
