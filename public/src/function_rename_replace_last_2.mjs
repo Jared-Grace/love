dcb3fc56d28fe4f276af235f5448ef3237ad42da
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:07:47 -0500

    function_auto_path ..\love\public\src\function_rename_replace_last_2.mjs

--- public/src/function_rename_replace_last_2.mjs
+++ public/src/function_rename_replace_last_2.mjs
@@ -1,11 +1,7 @@
-import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
-import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
-import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { function_name_to_parts_last_2 } from "../../../love/public/src/function_name_to_parts_last_2.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 export async function function_rename_replace_last_2(f_name_before, to) {
-  let parts = function_name_to_parts(f_name_before);
-  let result = list_slice_end(parts, 2);
-  let from = function_name_combine_multiple(result);
+  let from = function_name_to_parts_last_2(f_name_before);
   let v = await function_rename_replace(f_name_before, from, to);
   return v;
 }
