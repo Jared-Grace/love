f220387ea26875a859d4e1f4c2e4a4cee84f418b
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:54:48 -0500

    function_auto_path ..\love\public\src\functions_imports_paths_fix_list.mjs

--- public/src/functions_imports_paths_fix.mjs
+++ public/src/functions_imports_paths_fix.mjs
@@ -1,7 +1,7 @@
-import { functions_imports_fix_list } from "../../../love/public/src/functions_imports_fix_list.mjs";
+import { functions_imports_paths_fix_list } from "../../../love/public/src/functions_imports_paths_fix_list.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function functions_imports_paths_fix() {
   const list = await functions_names();
-  let v = await functions_imports_fix_list(list);
+  let v = await functions_imports_paths_fix_list(list);
   return v;
 }
