6d7856e808eb07c258ae38f8b463672580c15173
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 18:38:51 -0500

    function_auto_path ..\love\public\src\list_join_colon_2.mjs

--- public/src/list_join_colon_2.mjs
+++ public/src/list_join_colon_2.mjs
@@ -1,5 +1,7 @@
+import { string_colon_2 } from "../../../love/public/src/string_colon_2.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
 export function list_join_colon_2(texts) {
-  let joined2 = list_join(texts, " :: ");
+  let separator = string_colon_2();
+  let joined2 = list_join(texts, separator);
   return joined2;
 }
