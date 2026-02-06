2490960bf5a0fef7b42a30c81e929fe69de5c967
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:38:17 -0500

    function_auto_path ..\love\public\src\function_rename_parts_delete.mjs

--- public/src/function_rename_parts_delete.mjs
+++ public/src/function_rename_parts_delete.mjs
@@ -1,6 +1,8 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_name_parts_remove_comma } from "../../../love/public/src/function_name_parts_remove_comma.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_parts_delete(f_name_old, deleted) {
+  let unaliased = await function_name_unalias_only(name);
   let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
   let v = await function_rename(f_name_old, f_name_new);
   return v;
