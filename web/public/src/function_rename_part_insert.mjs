244b95e166d1859521d7564b639b72889a36a1e7
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:51:29 -0500

    function_auto_path ..\love\public\src\function_rename_part_insert.mjs

--- web/public/src/function_rename_part_insert.mjs
+++ web/public/src/function_rename_part_insert.mjs
@@ -1,12 +1,14 @@
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_rename_part_insert_at } from "../../../love/public/src/function_rename_part_insert_at.mjs";
 export async function function_rename_part_insert(
   f_name_before,
-  part,
-  index_string,
+  part_new,
+  part_insert,
 ) {
+  let parts = function_name_to_parts(f_name);
   let v = await function_rename_part_insert_at(
     f_name_before,
-    part,
+    part_new,
     index_string,
   );
   return v;
