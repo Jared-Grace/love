f1dccf0a6ea7399ead11704a7ac2af38dd3a4496
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 22:25:49 -0500

    function_auto_path ..\love\public\src\ebible_languages_without_original_english_bible_folders_each.mjs

--- public/src/ebible_languages_without_original_english_bible_folders_each.mjs
+++ public/src/ebible_languages_without_original_english_bible_folders_each.mjs
@@ -8,7 +8,8 @@ export async function ebible_languages_without_original_english_bible_folders_ea
   let languages = ebible_languages_without_original_english();
   const property = "bible_folder";
   const including_and_onward = "porbrbsl";
-  let index_next = list_index_of_property(
+  let index_next = null;
+  index_next = list_index_of_property(
     languages,
     property,
     including_and_onward,
