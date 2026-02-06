e788e7736e18fa745929349754135cd3652c4814
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:08:54 -0500

    function_auto_path ..\love\public\src\openai_responses.mjs

--- public/src/openai_responses.mjs
+++ public/src/openai_responses.mjs
@@ -1,6 +1,6 @@
 import { openai_chat_completions_generic } from "../../../love/public/src/openai_chat_completions_generic.mjs";
 export async function openai_responses(input_file_path, output_file_path) {
-  const openai_script_name = "chat_completions";
+  const openai_script_name = "responses";
   await openai_chat_completions_generic(
     openai_script_name,
     input_file_path,
