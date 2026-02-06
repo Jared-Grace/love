e635fe9abbefd3a3d95e6b644f0f1d79936b4358
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 13:11:50 -0500

    crl2 g_generate_openai_chat_completions responses

---
+++ web/public/src/g_generate_openai_responses.mjs
@@ -0,0 +1,7 @@
+import { g_generate_openai_generic } from "../../../love/public/src/g_generate_openai_generic.mjs";
+import { openai_chat_completions } from "../../../love/public/src/openai_chat_completions.mjs";
+export async function g_generate_openai_responses(system, user) {
+  let fn = openai_chat_completions;
+  let data = await g_generate_openai_generic(fn, system, user);
+  return data;
+}
