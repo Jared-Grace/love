44dfd97aacee1199e6347ce1ebd7782504ebc2ee
Grace-Jared <christrosetolife@gmail.com>
2026-02-05 19:49:09 -0500

    function_auto_path ..\love\public\src\function_wrap_part_last_remove.mjs

--- public/src/function_wrap_part_last_remove.mjs
+++ public/src/function_wrap_part_last_remove.mjs
@@ -1,6 +1,5 @@
-import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
+import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
 import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
-import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
@@ -8,9 +7,7 @@ export async function function_wrap_part_last_remove(f_name) {
   assert_arguments(arguments, 1);
   let unaliased = await function_name_unalias_only(f_name);
   let fn = list_remove_last;
-  let parts = function_name_to_parts(unaliased);
-  list_remove_last(parts);
-  let combined = function_name_combine_multiple(parts);
+  let combined = function_name_parts_transform(unaliased, fn);
   let v = await function_wrap(unaliased, combined);
   return v;
 }
