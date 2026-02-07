8f61f1a9622a0164603bb56fb7c357b471c0ba7e
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:19:14 -0500

    function_auto_path ..\love\public\src\html_span_text_nbsp_replace.mjs

--- web/public/src/html_span_text_nbsp_replace.mjs
+++ web/public/src/html_span_text_nbsp_replace.mjs
@@ -1,8 +1,9 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { html_nbsp_replace } from "../../../love/public/src/html_nbsp_replace.mjs";
 export function html_span_text_nbsp_replace(parent, text) {
   let replaced = html_nbsp_replace(text);
   let span = html_span_text(parent, replaced);
-  html_style_set(span, 'white-space', 'nowrap', );
+  html_style_set(span, "white-space", "nowrap");
   return span;
 }
