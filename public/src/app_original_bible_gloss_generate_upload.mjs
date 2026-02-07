49ada154b110c05c19bc4978d3b1f02375f65f1c
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:09:29 -0500

    function_auto_path ..\love\public\src\app_original_bible_gloss_generate_upload.mjs

--- public/src/app_original_bible_gloss_generate_upload.mjs
+++ public/src/app_original_bible_gloss_generate_upload.mjs
@@ -1,8 +1,8 @@
-import { app_ceb_bible_gloss_generate_upload_path } from "../../../love/public/src/app_ceb_bible_gloss_generate_upload_path.mjs";
-import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
+import { app_original_bible_gloss_generate_upload_path } from "../../../love/public/src/app_original_bible_gloss_generate_upload_path.mjs";
+import { app_original_bible_gloss_generate } from "../../../love/public/src/app_original_bible_gloss_generate.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 export async function app_original_bible_gloss_generate_upload() {
-  let fn = app_ceb_bible_gloss_generate;
-  let path_get = app_ceb_bible_gloss_generate_upload_path;
+  let fn = app_original_bible_gloss_generate;
+  let path_get = app_original_bible_gloss_generate_upload_path;
   await g_generate_upload_generic(fn, path_get);
 }
