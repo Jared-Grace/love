d51bc2f00827039ae8cf8e7cec4be363204937a8
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:20:00 -0500

    function_auto_path ..\love\public\src\function_rename_replace.mjs

--- public/src/function_rename_replace.mjs
+++ public/src/function_rename_replace.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_replace(f_name_before, from, to) {
   let f_name_after = string_replace(f_name_before, from, to);
+  log({
+    f_name_after,
+  });
   let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
