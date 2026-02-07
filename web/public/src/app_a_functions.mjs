d1a1b9dc6bd6ba9c26f4233e1ac034d28ad6cc8f
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:04:31 -0500

    function_auto_path ..\love\public\src\app_a_functions.mjs

--- web/public/src/app_a_functions.mjs
+++ web/public/src/app_a_functions.mjs
@@ -1,10 +1,8 @@
+import { app_a_function_select_curry } from "../../../love/public/src/app_a_function_select_curry.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
 import { app_a_button_function_refresh } from "../../../love/public/src/app_a_button_function_refresh.mjs";
-import { app_a_function_select } from "../../../love/public/src/app_a_function_select.mjs";
 export async function app_a_functions(context) {
   app_a_button_function_refresh(context);
-  let on_select = function lambda(f_name) {
-    app_a_function_select(context, f_name);
-  };
+  let on_select = app_a_function_select_curry(context);
   await app_a_functions_generic(context, on_select);
 }
