37c10916d6306f31e8068c591e4ab4f19eae78ee
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 09:42:20 -0500

    function_auto_path ..\love\public\src\html_scroll_center_generic.mjs

--- public/src/html_scroll_center_generic.mjs
+++ public/src/html_scroll_center_generic.mjs
@@ -1,7 +1,7 @@
-import { html_scroll_center_generic_wait } from "../../../love/public/src/html_scroll_center_generic_wait.mjs";
+import { html_scroll_generic_wait } from "../../../love/public/src/html_scroll_generic_wait.mjs";
 export async function html_scroll_center_generic(component, behavior) {
   const block = "center";
-  let el = await html_scroll_center_generic_wait(component);
+  let el = await html_scroll_generic_wait(component);
   el.scrollIntoView({
     behavior,
     block: block,
