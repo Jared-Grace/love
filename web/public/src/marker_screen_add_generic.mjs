7bdf7a8c5ebb1ac101646a99714294eaaf17ce98
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:08:40 -0500

    function_auto_path ..\love\public\src\marker_screen_add_generic.mjs

--- web/public/src/marker_screen_add_generic.mjs
+++ web/public/src/marker_screen_add_generic.mjs
@@ -16,7 +16,6 @@ export async function marker_screen_add_generic(a_name, lambda) {
       only,
     });
     exit();
-    await lambda(properties, prefixed);
   }
   let result = await function_transform(combined, lambda_ftms);
   return result;
