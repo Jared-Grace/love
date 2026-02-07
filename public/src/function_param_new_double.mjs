9015a102e80c411e951f43288b6400e30f284b52
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 13:48:19 -0500

    pne function_param_new_double f_name

--- public/src/function_param_new_double.mjs
+++ public/src/function_param_new_double.mjs
@@ -1,4 +1,4 @@
 import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
-export async function function_param_new_double(param_name) {
+export async function function_param_new_double(param_name, f_name) {
   await function_params_new(f_name, param_name, param_name);
 }
--- public/src/function_param_new_double_first.mjs
+++ public/src/function_param_new_double_first.mjs
@@ -1,5 +1,5 @@
 import { function_param_new_double } from "../../../love/public/src/function_param_new_double.mjs";
 export async function function_param_new_double_first(param_name) {
-  let v = await function_param_new_double(param_name);
+  let v = await function_param_new_double(param_name, error());
   return v;
 }
