a0ba5a75c1a837b3ebc5ad4763d9fefce6a9820d
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:01:07 -0500

    function_auto_path ..\love\public\src\app_a_function_select.mjs

--- web/public/src/app_a_function_select.mjs
+++ web/public/src/app_a_function_select.mjs
@@ -1,9 +1,8 @@
+import { list_add_unique } from "../../../love/public/src/list_add_unique.mjs";
 import { app_a_function_name_selected_history_key } from "../../../love/public/src/app_a_function_name_selected_history_key.mjs";
 import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
-import { list_remove_all } from "../../../love/public/src/list_remove_all.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { storage_local_transform_context } from "../../../love/public/src/storage_local_transform_context.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
@@ -11,8 +10,7 @@ export function app_a_function_select(context, f_name) {
   let key = app_a_function_name_selected_key();
   storage_local_set_context(context, key, f_name);
   function lambda(h) {
-    list_remove_all(h, f_name);
-    list_add(h, f_name);
+    list_add_unique(h, f_name);
     let max = 100;
     let result = list_slice_end(h, max);
     return result;
