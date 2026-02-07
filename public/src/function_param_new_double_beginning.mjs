25ece97d1b7cd022c79993e5a83866a0d8b9c830
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 13:48:30 -0500

    pf function_param_new_double f_name

--- public/src/function_param_new_double_beginning.mjs
+++ public/src/function_param_new_double_beginning.mjs
@@ -3,7 +3,7 @@ import { function_param_move_first } from "../../../love/public/src/function_par
 import { function_param_new_double } from "../../../love/public/src/function_param_new_double.mjs";
 export async function function_param_new_double_beginning(param_name) {
   let f_name2 = error();
-  let v = await function_param_new_double(param_name, f_name2);
+  let v = await function_param_new_double(f_name2, param_name);
   let f_name = error();
   await function_param_move_first(f_name, param_name);
   return v;
