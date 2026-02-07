b8c3e67b5100fa484c984b121f95785d205b6008
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:09:59 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

--- web/public/src/app_ceb_bible_home.mjs
+++ web/public/src/app_ceb_bible_home.mjs
@@ -46,6 +46,7 @@ export async function app_ceb_bible_home(context) {
       let explain = object_property_get(e, "explain");
       let span = html_span_text(div2, ceb);
       html_bold_mild(span);
+      html_font_color_set_green(span);
       html_span_space(div2);
       let span2 = html_span_text(div2, gloss);
       html_span_space(div2);
