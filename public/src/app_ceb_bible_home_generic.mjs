8e4b546d3c617f990eb488e42ee3bf2452e5e0b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:00:35 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home_generic.mjs

--- public/src/app_ceb_bible_home_generic.mjs
+++ public/src/app_ceb_bible_home_generic.mjs
@@ -52,12 +52,12 @@ export async function app_ceb_bible_home_generic(
       texts = [original];
     }
     if (false) {
+      function lambda5(t) {
+        let div = html_div_text(p, t);
+        html_font_color_set_green(div);
+      }
+      each(texts, lambda5);
     }
-    function lambda5(t) {
-      let div = html_div_text(p, t);
-      html_font_color_set_green(div);
-    }
-    each(texts, lambda5);
     let explains_json = object_property_get(passage, "explains");
     let explains = json_from(explains_json);
     function lambda2(item) {}
