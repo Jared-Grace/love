74d7404aed7e7ada775c0cdf5299e533d5d9d064
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:15:30 -0500

    cs html_span_colon 2

---
+++ public/src/html_span_colon_2.mjs
@@ -0,0 +1,5 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+export function html_span_colon_2(div2) {
+  let span = html_span_text(div2, " : ");
+  return span;
+}
