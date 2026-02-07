c998b82c659748594b6e3afe20a3922a0c5f10a0
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:15:30 -0500

    function_auto_path ..\love\public\src\app_original_bible_screens.mjs

--- web/public/src/app_original_bible_screens.mjs
+++ web/public/src/app_original_bible_screens.mjs
@@ -1,3 +1,4 @@
+import { app_original_bible_home } from "../../../love/public/src/app_original_bible_home.mjs";
 import { app_bible_languages } from "../../../love/public/src/app_bible_languages.mjs";
 import { app_bible_chapters } from "../../../love/public/src/app_bible_chapters.mjs";
 import { app_bible_books } from "../../../love/public/src/app_bible_books.mjs";
