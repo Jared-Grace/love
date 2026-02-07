0041a5b100fb0ac0cb03e1021b9ad44c5a92cd91
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:42:57 -0500

    function_auto_path ..\love\public\src\app_a_function_select.mjs

--- public/src/app_a_function_select.mjs
+++ public/src/app_a_function_select.mjs
@@ -5,11 +5,12 @@ import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 export function app_a_function_select(context, f_name) {
   storage_local_set_context(context, "f_name_selected", f_name);
+  function lambda() {}
   let h = storage_local_transform_context(
     context,
     "f_name_selected",
     [],
-    () => {},
+    lambda,
   );
   list_add(h, f_name);
   app_shared_screen_set(context, app_a_function);
