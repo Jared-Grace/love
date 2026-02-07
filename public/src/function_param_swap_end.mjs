da0d2fd4b937b36df46e0a09649f60bcde61d0e5
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:01:41 -0500

    function_auto_path ..\love\public\src\function_param_swap_end.mjs

--- public/src/function_param_swap_end.mjs
+++ public/src/function_param_swap_end.mjs
@@ -3,17 +3,19 @@ import { js_declaration_params_ast_get } from "../../../love/public/src/js_decla
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 export async function function_param_swap_end(f_name) {
-  let index_a = 0;
-  let index_b = 1;
   await function_params_new_generic(
     function_transform_current_lambda,
     on_call,
     f_name,
   );
   function on_call(args) {
+    let index_a = 0;
+    let index_b = 1;
     list_swap_at(args, index_a, index_b);
   }
   function function_transform_current_lambda(ast) {
+    let index_a = 0;
+    let index_b = 1;
     let v2 = js_declaration_params_ast_get(ast);
     let params = object_property_get(v2, "params");
     list_swap_at(params, index_a, index_b);
