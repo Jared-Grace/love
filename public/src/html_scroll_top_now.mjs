9fb3af0c122492a82210507ab02c26b06a2a333f
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 10:17:19 -0500

    function_auto_path ..\love\public\src\html_scroll_top_now.mjs

--- public/src/html_scroll_top_now.mjs
+++ public/src/html_scroll_top_now.mjs
@@ -1,6 +1,6 @@
 import { html_scroll_generic } from "../../../love/public/src/html_scroll_generic.mjs";
 export async function html_scroll_top_now(component) {
-  const block = "top";
+  const block = "start";
   const behavior = "auto";
   await html_scroll_generic(component, behavior, block);
 }
