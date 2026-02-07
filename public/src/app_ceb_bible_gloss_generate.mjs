0c0943a540034af21cd829ef58be302ecb0af5d9
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:13:46 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate.mjs

--- public/src/app_ceb_bible_gloss_generate.mjs
+++ public/src/app_ceb_bible_gloss_generate.mjs
@@ -1,3 +1,4 @@
+import { app_ceb_bible_gloss_generate_code } from "../../../love/public/src/app_ceb_bible_gloss_generate_code.mjs";
 import { app_ceb_bible_gloss_generate_generic } from "../../../love/public/src/app_ceb_bible_gloss_generate_generic.mjs";
 import { ebible_folder_cebuano } from "../../../love/public/src/ebible_folder_cebuano.mjs";
 import { app_ceb_bible_gloss_generate_property } from "../../../love/public/src/app_ceb_bible_gloss_generate_property.mjs";
@@ -10,6 +11,7 @@ export async function app_ceb_bible_gloss_generate() {
   let c = ebible_folder_cebuano();
   const bible_folders = [c, bible_folder];
   let last = "original language and English are";
+  let language_code = app_ceb_bible_gloss_generate_code();
   await app_ceb_bible_gloss_generate_generic(
     language,
     last,
@@ -17,6 +19,6 @@ export async function app_ceb_bible_gloss_generate() {
     book_code,
     fn,
     property_name,
-    "ceb",
+    language_code,
   );
 }
