36c2441b9060d0b0f39b3f4ee9b704dfe72e1a21
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:07:32 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home_generic.mjs

---
+++ public/src/html_span_nbsp.mjs
@@ -0,0 +1,6 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+import { html_nbsp_replace } from "../../../love/public/src/html_nbsp_replace.mjs";
+export function html_span_nbsp(div3) {
+  let text2 = html_nbsp_replace(" ");
+  let span3 = html_span_text(div3, text2);
+}
