e420c72b3d6d64c36d15f6581a38840a8aa908c7
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:04:17 -0500

    function_auto_path ..\love\public\src\js_list_type.mjs

--- public/src/js_list_type.mjs
+++ public/src/js_list_type.mjs
@@ -1,6 +1,6 @@
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-export function js_type(ast, node_type) {
+export function js_list_type(ast, node_type) {
   function lambda(la) {
     js_visit_type(ast, node_type, la);
   }
