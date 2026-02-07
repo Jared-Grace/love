f1f7db80839d3f35bf98565b2b4ecba22a5451e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:18:46 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

---
+++ web/public/src/html_font_color_set_blue.mjs
@@ -0,0 +1,4 @@
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+export function html_font_color_set_blue(span2) {
+  html_font_color_set(span2, "rgba(0, 0, 213, 1)");
+}
