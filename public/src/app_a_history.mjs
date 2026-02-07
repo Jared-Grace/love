26caaa7dc98271888225a1eb353a549f185fbde7
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:02:02 -0500

    function_auto_path ..\love\public\src\app_a_history.mjs

--- public/src/app_a_history.mjs
+++ public/src/app_a_history.mjs
@@ -1,10 +1,10 @@
+import { app_a_list_chooser } from "../../../love/public/src/app_a_list_chooser.mjs";
 import { app_a_function_name_selected_history_key } from "../../../love/public/src/app_a_function_name_selected_history_key.mjs";
-import { html_div_text_json } from "../../../love/public/src/html_div_text_json.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 export function app_a_history(context) {
   let root = html_clear_context(context);
   let key = app_a_function_name_selected_history_key();
   let h = storage_local_get_context(context, key);
-  html_div_text_json(root, h);
+  let r = app_a_list_chooser(context, "function", h, lambda$f_name);
 }
