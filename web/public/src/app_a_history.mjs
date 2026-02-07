76e002b0ecc6cd572a99eb5c1fa62d3427151cc7
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:49:52 -0500

    function_auto_path ..\love\public\src\app_a_history.mjs

--- web/public/src/app_a_history.mjs
+++ web/public/src/app_a_history.mjs
@@ -1,10 +1,8 @@
-import { json_to } from "../../../love/public/src/json_to.mjs";
-import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { html_div_text_json } from "../../../love/public/src/html_div_text_json.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 export function app_a_history(context) {
   let root = html_clear_context(context);
   let h = storage_local_get_context(context, "f_name_selected_history");
-  let json = json_to(h);
-  let div = html_div_text(root, json);
+  html_div_text_json(h, root);
 }
