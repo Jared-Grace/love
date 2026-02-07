0211b8001a8ccfad028feedd209ce4779290cf62
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:32 -0500

    function_auto_path ..\love\public\src\app_shared_name_prefixed.mjs

--- public/src/app_new_fn.mjs
+++ public/src/app_new_fn.mjs
@@ -1,8 +1,8 @@
 import { string_and_empty_not_is_assert } from "../../../love/public/src/string_and_empty_not_is_assert.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
-import { app_generic_name_prefixed } from "../../../love/public/src/app_generic_name_prefixed.mjs";
+import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
 export async function app_new_fn(name) {
   string_and_empty_not_is_assert(name);
-  let a_name = app_generic_name_prefixed(name);
+  let a_name = app_shared_name_prefixed(name);
   await function_new(a_name);
 }
