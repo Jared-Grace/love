6bcda6246aaa87c21cc67ebfa19cd4f0373fd613
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:07:45 -0500

    function_auto_path ..\love\public\src\app_a_button_function_if_exists.mjs

--- web/public/src/app_a_apps.mjs
+++ web/public/src/app_a_apps.mjs
@@ -1,12 +1,12 @@
 import { app_a_app } from "../../../love/public/src/app_a_app.mjs";
 import { app_a_app_selected_key } from "../../../love/public/src/app_a_app_selected_key.mjs";
-import { app_a_button_function_refresh } from "../../../love/public/src/app_a_button_function_refresh.mjs";
+import { app_a_button_function_if_exists } from "../../../love/public/src/app_a_button_function_if_exists.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { app_a_list_chooser } from "../../../love/public/src/app_a_list_chooser.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 export async function app_a_apps(context) {
-  app_a_button_function_refresh(context);
+  app_a_button_function_if_exists(context);
   let mapped = await apps_names();
   async function on_select(a_name) {
     let key = app_a_app_selected_key();
