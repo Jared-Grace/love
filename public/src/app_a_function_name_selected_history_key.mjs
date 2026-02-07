f7a487f61730e13e51c95f2c76b310585e3a7f3a
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:59:12 -0500

    function_auto_path ..\love\public\src\app_a_function_select.mjs

---
+++ public/src/app_a_function_name_selected_history_key.mjs
@@ -0,0 +1,5 @@
+import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
+export function app_a_function_name_selected_history_key() {
+  let v = app_a_function_name_selected_key() + "_history";
+  return v;
+}
