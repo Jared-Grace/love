707ef374606167ecb95028f127ab476d8e488baa
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:19:12 -0500

    function_auto_path ..\love\public\src\html_span_text_nbsp_replace_property_from.mjs

--- web/public/src/html_span_text_nbsp_replace.mjs
+++ web/public/src/html_span_text_nbsp_replace.mjs
@@ -3,5 +3,6 @@ import { html_nbsp_replace } from "../../../love/public/src/html_nbsp_replace.mj
 export function html_span_text_nbsp_replace(parent, text) {
   let replaced = html_nbsp_replace(text);
   let span = html_span_text(parent, replaced);
+  html_style_set(span, 'white-space', 'nowrap', );
   return span;
 }
