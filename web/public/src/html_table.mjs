1e761acfc8d01c38cee91ee5a892f2623bc99505
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:02:55 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home.mjs

---
+++ web/public/src/html_table.mjs
@@ -0,0 +1,22 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { html_element } from "../../../love/public/src/html_element.mjs";
+import { object_properties } from "../../../love/public/src/object_properties.mjs";
+import { list_first } from "../../../love/public/src/list_first.mjs";
+export function html_table(explains, p) {
+  let first = list_first(explains);
+  let properties = object_properties(first);
+  let component = html_element(p, "table");
+  let component2 = html_element(component, "tbody");
+  function lambda(e) {
+    let component3 = html_element(component2, "tr");
+    function lambda2(property) {
+      let component4 = html_element(component3, "td");
+      let value = object_property_get(e, property);
+      html_text_set(component4, value);
+    }
+    each(properties, lambda2);
+  }
+  each(explains, lambda);
+}
