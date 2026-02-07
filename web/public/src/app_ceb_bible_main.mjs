0db5a837473248b2511cd0a9c100090f061ff10d
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:11:30 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_main.mjs

--- web/public/src/app_ceb_bible_main.mjs
+++ web/public/src/app_ceb_bible_main.mjs
@@ -1,12 +1,10 @@
-import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
+import { app_ceb_bible_main_generic } from "../../../love/public/src/app_ceb_bible_main_generic.mjs";
 import { app_ceb_bible_screens } from "../../../love/public/src/app_ceb_bible_screens.mjs";
 import { app_ceb_bible_home } from "../../../love/public/src/app_ceb_bible_home.mjs";
 import { app_ceb_bible } from "../../../love/public/src/app_ceb_bible.mjs";
-import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 export function app_ceb_bible_main(context) {
   let app_fn = app_ceb_bible;
   let screens = app_ceb_bible_screens();
   let screen_home = app_ceb_bible_home;
-  app_bible_main_generic_before(context, app_fn, screens, screen_home);
-  app_generic_refresh(context);
+  app_ceb_bible_main_generic(context, app_fn, screens, screen_home);
 }
