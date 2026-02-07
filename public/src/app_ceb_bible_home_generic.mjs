cd491f97cf936a12780faf3322e68d07cef92bea
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:38:54 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home_generic.mjs

--- public/src/app_ceb_bible_home_generic.mjs
+++ public/src/app_ceb_bible_home_generic.mjs
@@ -48,6 +48,8 @@ export async function app_ceb_bible_home_generic(
       }
       texts = list_map(split3, lambda4);
     } else {
+      let original = object_property_get(passage, "original");
+      texts = [];
     }
     function lambda5(t) {
       let div = html_div_text(p, t);
