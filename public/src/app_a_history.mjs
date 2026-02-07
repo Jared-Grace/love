fac839e860f3e3ea78c1fab570a032ccae905b97
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:07:45 -0500

    rrl app_a_button_function_refresh if_exists

--- public/src/app_a_history.mjs
+++ public/src/app_a_history.mjs
@@ -1,10 +1,10 @@
 import { app_a_function_select_curry } from "../../../love/public/src/app_a_function_select_curry.mjs";
-import { app_a_button_function_refresh } from "../../../love/public/src/app_a_button_function_refresh.mjs";
+import { app_a_button_function_if_exists } from "../../../love/public/src/app_a_button_function_if_exists.mjs";
 import { app_a_list_chooser } from "../../../love/public/src/app_a_list_chooser.mjs";
 import { app_a_function_name_selected_history_key } from "../../../love/public/src/app_a_function_name_selected_history_key.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 export function app_a_history(context) {
-  app_a_button_function_refresh(context);
+  app_a_button_function_if_exists(context);
   let key = app_a_function_name_selected_history_key();
   let h = storage_local_get_context(context, key);
   let on_select = app_a_function_select_curry(context);
