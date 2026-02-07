a15516a963b571ce5ad95b9ac4ce809208c69691
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:49:52 -0500

    function_auto_path ..\love\public\src\app_a_history.mjs

---
+++ web/public/src/html_div_text_json.mjs
@@ -0,0 +1,6 @@
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { json_to } from "../../../love/public/src/json_to.mjs";
+export function html_div_text_json(h, root) {
+  let json = json_to(h);
+  let div = html_div_text(root, json);
+}
