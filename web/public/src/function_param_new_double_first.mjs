838a4e6d39e5bea58c1f67324c98188165e1a90f
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 13:49:15 -0500

    function_auto_path ..\love\public\src\function_param_new_double_first.mjs

--- web/public/src/function_param_new_double_first.mjs
+++ web/public/src/function_param_new_double_first.mjs
@@ -1,7 +1,6 @@
 import { function_param_move_first } from "../../../love/public/src/function_param_move_first.mjs";
 import { function_param_new_double } from "../../../love/public/src/function_param_new_double.mjs";
 export async function function_param_new_double_first(f_name, param_name) {
-  let v = await function_param_new_double(f_name, param_name);
+  await function_param_new_double(f_name, param_name);
   await function_param_move_first(f_name, param_name);
-  return v;
 }
