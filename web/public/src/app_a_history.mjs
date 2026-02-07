1901fe611b4de44e75b6f9b081cab8171bd267a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:03:50 -0500

    function_auto_path ..\love\public\src\app_a_history.mjs

--- web/public/src/app_a_history.mjs
+++ web/public/src/app_a_history.mjs
@@ -6,5 +6,5 @@ export function app_a_history(context) {
   app_a_button_function_refresh(context);
   let key = app_a_function_name_selected_history_key();
   let h = storage_local_get_context(context, key);
-  let r = app_a_list_chooser(context, "function", h, lambda$f_name);
+  let r = app_a_list_chooser(context, "function", h, () => {});
 }
