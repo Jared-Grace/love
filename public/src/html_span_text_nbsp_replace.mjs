b8036712099c9e1d60cc7c629a5d667610938454
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:13:44 -0500

    function_auto_path ..\love\public\src\html_span_text_nbsp_replace.mjs

--- public/src/html_span_text_nbsp_replace.mjs
+++ public/src/html_span_text_nbsp_replace.mjs
@@ -2,6 +2,6 @@ import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { html_nbsp_replace } from "../../../love/public/src/html_nbsp_replace.mjs";
 export function html_span_text_nbsp_replace(parent, text) {
   let replaced = html_nbsp_replace(text);
-  let span = html_span_text(replaced, parent);
+  let span = html_span_text(parent, replaced);
   return span;
 }
