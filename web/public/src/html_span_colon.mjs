76eb46d94e64b2cf2fac8f7a755ff49028db14e0
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:14:51 -0500

    function_auto_path ..\love\public\src\html_span_colon.mjs

--- web/public/src/html_span_colon.mjs
+++ web/public/src/html_span_colon.mjs
@@ -1,4 +1,4 @@
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function html_span_colon(div2) {
-  html_span_text(div2, " : ");
+  return html_span_text(div2, " : ");
 }
