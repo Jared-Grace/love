65e9c380d269cf1ee8e0b0d6e8c1ab1439d3c3fd
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 09:50:53 -0500

    function_auto_path ..\love\public\src\html_hr.mjs

--- public/src/html_hr.mjs
+++ public/src/html_hr.mjs
@@ -1,5 +1,5 @@
 import { html_element } from "../../../love/public/src/html_element.mjs";
 export function html_hr(root) {
-  let component2 = html_element(root, "hr");
-  return component2;
+  let h = html_element(root, "hr");
+  return h;
 }
