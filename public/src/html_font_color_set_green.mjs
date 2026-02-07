6b4c46e2d0215b3a1df52a473a03aed298589b5a
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:20:57 -0500

    function_auto_path ..\love\public\src\html_font_color_set_green.mjs

--- public/src/html_font_color_set_green.mjs
+++ public/src/html_font_color_set_green.mjs
@@ -2,5 +2,5 @@ import { html_font_color_set } from "../../../love/public/src/html_font_color_se
 import { app_karate_button_background } from "../../../karate_code/public/src/app_karate_button_background.mjs";
 export function html_font_color_set_green(div) {
   let c = app_karate_button_background();
-  html_font_color_set(div, c);
+  html_font_color_set(div, "#159834");
 }
