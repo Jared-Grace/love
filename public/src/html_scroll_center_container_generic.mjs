8b4c768b697390558b17423b200375c0ba20528e
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 09:42:16 -0500

    function_auto_path ..\love\public\src\html_scroll_generic_wait.mjs

--- public/src/html_scroll_center_container_generic.mjs
+++ public/src/html_scroll_center_container_generic.mjs
@@ -1,12 +1,12 @@
 import { html_request_animation_frame } from "../../../love/public/src/html_request_animation_frame.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-import { html_scroll_center_generic_wait } from "../../../love/public/src/html_scroll_center_generic_wait.mjs";
+import { html_scroll_generic_wait } from "../../../love/public/src/html_scroll_generic_wait.mjs";
 export async function html_scroll_center_container_generic(
   component,
   behavior,
   container,
 ) {
-  let e = await html_scroll_center_generic_wait(component);
+  let e = await html_scroll_generic_wait(component);
   let container_e = html_component_element_get(container);
   await html_request_animation_frame();
   const containerRect = container_e.getBoundingClientRect();
