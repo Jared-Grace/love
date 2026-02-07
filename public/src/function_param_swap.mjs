1f40721964a12b157da31fc77328e0687cb383a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 14:58:29 -0500

    function_auto_path ..\love\public\src\function_param_swap.mjs

--- public/src/function_param_swap.mjs
+++ public/src/function_param_swap.mjs
@@ -1,11 +1,9 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 import { function_param_index } from "../../../love/public/src/function_param_index.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 export async function function_param_swap(param_name_a, param_name_b) {
   let index_a = null;
   let index_b = null;
-  let f_name = error();
   await function_params_new_generic(
     function_transform_current_lambda,
     on_call,
