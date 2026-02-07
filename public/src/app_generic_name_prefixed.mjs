79f59d382bad2ca9e177629741161f813696402c
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:30 -0500

    function_auto_path ..\love\public\src\app_prefix_without.mjs

--- public/src/app_generic_name_prefixed.mjs
+++
@@ -1,7 +0,0 @@
-import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
-import { app_generic_name_prefix } from "../../../love/public/src/app_generic_name_prefix.mjs";
-export function app_generic_name_prefixed(name) {
-  const p = app_generic_name_prefix();
-  let a_name = function_name_combine(p, name);
-  return a_name;
-}
