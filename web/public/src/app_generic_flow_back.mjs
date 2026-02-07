3fadce2101360beb4a5a043763b899f141fe9f72
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:24:18 -0500

    function_auto_path ..\love\public\src\app_shared_flow.mjs

--- web/public/src/app_generic_flow_back.mjs
+++ web/public/src/app_generic_flow_back.mjs
@@ -1,7 +1,7 @@
 import { list_take } from "../../../love/public/src/list_take.mjs";
-import { app_generic_flow } from "../../../love/public/src/app_generic_flow.mjs";
+import { app_shared_flow } from "../../../love/public/src/app_shared_flow.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 export function app_generic_flow_back(context, screens) {
   let find = list_last;
-  app_generic_flow(context, screens, list_take, find);
+  app_shared_flow(context, screens, list_take, find);
 }
