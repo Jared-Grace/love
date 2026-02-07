88aaef6f767fb4fb7d1a71162bb42b43d21e5124
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:39:37 -0500

    function_auto_path ..\love\public\src\app_shared_screen_add_generic.mjs

--- public/src/app_shared_screen_add_generic.mjs
+++ public/src/app_shared_screen_add_generic.mjs
@@ -4,7 +4,7 @@ import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
-export async function marker_screen_add_generic(a_name, lambda) {
+export async function app_shared_screen_add_generic(a_name, lambda) {
   let prefixed = app_shared_name_prefixed(a_name);
   let combined = function_name_combine(prefixed, "screens");
   async function lambda_ftms(ast) {
