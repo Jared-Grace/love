dc898b8fb5e8ae9d08fc4a57b23a2a18b2d5321f
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:08:37 -0500

    function_auto_path ..\love\public\src\app_original_bible_gloss_generate_download.mjs

--- public/src/app_original_bible_gloss_generate_download.mjs
+++ public/src/app_original_bible_gloss_generate_download.mjs
@@ -1,7 +1,7 @@
-import { app_ceb_bible_gloss_generate_upload_path } from "../../../love/public/src/app_ceb_bible_gloss_generate_upload_path.mjs";
+import { app_original_bible_gloss_generate_upload_path } from "../../../love/public/src/app_original_bible_gloss_generate_upload_path.mjs";
 import { global_firebase_storage_download_json_decompress } from "../../../love/public/src/global_firebase_storage_download_json_decompress.mjs";
 export async function app_original_bible_gloss_generate_download(chapter_code) {
-  let destination_get = app_ceb_bible_gloss_generate_upload_path;
+  let destination_get = app_original_bible_gloss_generate_upload_path;
   let fn = app_original_bible_gloss_generate_download;
   let value = await global_firebase_storage_download_json_decompress(
     fn,
