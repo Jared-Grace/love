919dcdf6dcac5eb1212e8abac5bbd7ef541853e4
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:49:56 -0500

    pb html_div_text_json

--- public/src/app_a_history.mjs
+++ public/src/app_a_history.mjs
@@ -4,5 +4,5 @@ import { html_clear_context } from "../../../love/public/src/html_clear_context.
 export function app_a_history(context) {
   let root = html_clear_context(context);
   let h = storage_local_get_context(context, "f_name_selected_history");
-  html_div_text_json(h, root);
+  html_div_text_json(root, h);
 }
