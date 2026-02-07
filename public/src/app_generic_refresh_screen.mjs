e39ca80dee60ce4cf18824b1b74d9e063a9a19e4
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:26:04 -0500

    function_auto_path ..\love\public\src\app_bible_main_generic.mjs

--- public/src/app_generic_refresh_screen.mjs
+++
@@ -1,6 +0,0 @@
-import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
-import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
-export function app_generic_refresh_screen(context, without) {
-  storage_local_set_context(context, "screen", without);
-  app_shared_refresh(context);
-}
