79cf87e0fca016d81fe0ec897ccd6d86d70d1c6e
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:21:13 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_home_generic.mjs

--- web/public/src/app_ceb_bible_home_generic.mjs
+++ web/public/src/app_ceb_bible_home_generic.mjs
@@ -60,7 +60,10 @@ export async function app_ceb_bible_home_generic(
       let c2 = html_span_colon_2(div2);
       html_font_color_set(c2, "#aaa");
       let span3 = html_span_text(div2, explain);
-      html_font_color_set(span3, "#7b3f97ff");
+      if (green_show) {
+        html_font_color_set(span3, "#7b3f97ff");
+      } else {
+      }
     }
     each(explains, lambda);
     log(passage);
