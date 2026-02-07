41c12163b4154185be53ab2bff0c8653dda9f151
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:07:45 -0500

    function_auto_path ..\love\public\src\app_a_button_function_if_exists.mjs

--- public/src/app_a_button_function_if_exists.mjs
+++ public/src/app_a_button_function_if_exists.mjs
@@ -4,7 +4,7 @@ import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { storage_local_exists_context } from "../../../love/public/src/storage_local_exists_context.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function app_a_button_function_refresh(context) {
+export function app_a_button_function_if_exists(context) {
   "if there is a selected function, then this creates button for it";
   const key = app_a_function_name_selected_key();
   let exists = storage_local_exists_context(context, key);
