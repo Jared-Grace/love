35d054238373135133d77b607e6f7e8c65a1f87e
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:03 -0500

    function_auto_path ..\love\public\src\app_generic_name_main_get.mjs

--- web/public/src/function_dependencies_code_copy_app_main.mjs
+++ web/public/src/function_dependencies_code_copy_app_main.mjs
@@ -1,7 +1,7 @@
-import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
+import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
 import { function_dependencies_code_copy } from "../../../love/public/src/function_dependencies_code_copy.mjs";
 export async function function_dependencies_code_copy_app_main(a) {
-  let combined = app_generic_name_main(a);
+  let combined = app_shared_name_main(a);
   let v = await function_dependencies_code_copy(combined);
   return v;
 }
