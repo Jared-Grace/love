eed73e50780c178ea5a8152c89c66aa0ec5a54fb
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:13:54 -0500

    crl function_rename_replace_last first

---
+++ public/src/function_rename_replace_first.mjs
@@ -0,0 +1,7 @@
+import { function_name_to_part_last } from "../../../love/public/src/function_name_to_part_last.mjs";
+import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
+export async function function_rename_replace_first(f_name_before, to) {
+  let from = function_name_to_part_last(f_name_before);
+  let v = await function_rename_replace(f_name_before, from, to);
+  return v;
+}
