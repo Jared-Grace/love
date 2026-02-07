6f437aead1991d018aecf0d38c491a309d41d7d2
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:54:10 -0500

    function_auto_path ..\love\public\src\app_original_bible_latest.mjs

--- web/public/src/app_original_bible.mjs
+++ web/public/src/app_original_bible.mjs
@@ -1,3 +1,4 @@
+import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_original_bible() {
   await app_main_production("app_original_bible_main", "jared-grace");
 }
--- web/public/src/app_original_bible_latest.mjs
+++ web/public/src/app_original_bible_latest.mjs
@@ -1,3 +1,4 @@
+import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
 export async function app_original_bible_latest() {
   await app_main_latest("app_original_bible_main", "jared-grace");
 }
