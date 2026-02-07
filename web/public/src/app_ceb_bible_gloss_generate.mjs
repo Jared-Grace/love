9bf72756718337d7b95c6f774e4171b39e895a82
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:56:03 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate.mjs

--- web/public/src/app_ceb_bible_gloss_generate.mjs
+++ web/public/src/app_ceb_bible_gloss_generate.mjs
@@ -1,7 +1,6 @@
-import { string_upper_to } from "../../../love/public/src/string_upper_to.mjs";
+import { app_ceb_bible_gloss_generate_generic } from "../../../love/public/src/app_ceb_bible_gloss_generate_generic.mjs";
 import { ebible_folder_cebuano } from "../../../love/public/src/ebible_folder_cebuano.mjs";
 import { app_ceb_bible_gloss_generate_property } from "../../../love/public/src/app_ceb_bible_gloss_generate_property.mjs";
-import { g_sermon_generate_book_generic } from "../../../love/public/src/g_sermon_generate_book_generic.mjs";
 export async function app_ceb_bible_gloss_generate() {
   let bible_folder = "engbsb";
   let book_code = "JAS";
@@ -11,25 +10,12 @@ export async function app_ceb_bible_gloss_generate() {
   let c = ebible_folder_cebuano();
   const bible_folders = [c, bible_folder];
   let last = "original language and English are";
-  let language_upper = string_upper_to(language);
-  const prompt_system = `You will be given a Bible passage and its context in ${language}.
-For each ${language} word, output an English gloss.
-Also output a full explanation of each ${language} word, explaining its meaning and grammar (including prefixes and suffixes), written for an English speaker with no background in grammar. 
-Explanations should be easy to understand. Explain as simply as possible.
-
-Output format:
-[{"ceb":"${language_upper}_WORD","gloss":"ENGLISH_GLOSS","explain":"FULL_EXPLANATION"}, ...]
-
-"${language_upper}_WORD" should include any punctuation before or after a word.
-The ${last} provided as a reference.`;
-  const prompt_user_middle =
-    "Here is the passage to output English glosses for: ";
-  await g_sermon_generate_book_generic(
+  await app_ceb_bible_gloss_generate_generic(
+    language,
+    last,
     bible_folders,
     book_code,
     fn,
-    prompt_user_middle,
-    prompt_system,
     property_name,
   );
 }
