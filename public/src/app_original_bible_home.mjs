f2c5feb6aa0e3820d07c6d217659a95c5f70a997
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:14:24 -0500

    function_auto_path ..\love\public\src\app_original_bible_home.mjs

--- public/src/app_original_bible_home.mjs
+++ public/src/app_original_bible_home.mjs
@@ -1,7 +1,8 @@
+import { app_ceb_bible_gloss_generate_code } from "../../../love/public/src/app_ceb_bible_gloss_generate_code.mjs";
 import { app_original_bible_gloss_generate_download } from "../../../love/public/src/app_original_bible_gloss_generate_download.mjs";
 import { app_ceb_bible_home_generic } from "../../../love/public/src/app_ceb_bible_home_generic.mjs";
 export async function app_original_bible_home() {
   let download = app_original_bible_gloss_generate_download;
-  const language = "ceb";
+  let language_code = app_ceb_bible_gloss_generate_code();
   await app_ceb_bible_home_generic(context, download, language);
 }
