85340ab50402ec39b42289e090430e68f41c3c4f
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:18:46 -0500

    function_auto_path ..\love\public\src\html_span_text_nbsp_replace_property_from.mjs

--- public/src/html_span_text_nbsp_replace_property_from.mjs
+++ public/src/html_span_text_nbsp_replace_property_from.mjs
@@ -1,7 +1,9 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { html_span_text_nbsp_replace } from "../../../love/public/src/html_span_text_nbsp_replace.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function html_span_text_nbsp_replace_property_from(parent, obj, prop) {
   let word = object_property_get(obj, prop);
   let span = html_span_text_nbsp_replace(parent, word);
+  html_style_set(b, style_key, style_value);
   return span;
 }
