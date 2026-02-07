b2eea12a9567aa5fb2161ff3898107417de22bfe
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:42:59 -0500

    function_auto_path ..\love\public\src\app_bible_home_generic.mjs

--- web/public/src/app_bible_home_generic.mjs
+++ web/public/src/app_bible_home_generic.mjs
@@ -137,7 +137,7 @@ export async function app_bible_home_generic(context, lambda$a) {
     each(split, lambda2);
     let p = html_p(content);
     await lambda$a({
-      p,
+      a,
       chapter_code,
       verse_number,
     });
