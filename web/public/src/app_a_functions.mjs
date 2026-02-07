9663c2015e188ba50fced1b234ba4bfd23a72fa5
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:04:16 -0500

    function_auto_path ..\love\public\src\app_a_functions.mjs

--- web/public/src/app_a_functions.mjs
+++ web/public/src/app_a_functions.mjs
@@ -3,8 +3,8 @@ import { app_a_button_function_refresh } from "../../../love/public/src/app_a_bu
 import { app_a_function_select } from "../../../love/public/src/app_a_function_select.mjs";
 export async function app_a_functions(context) {
   app_a_button_function_refresh(context);
-  await app_a_functions_generic(context, on_select);
-  function on_select(f_name) {
+  let on_select = function lambda(f_name) {
     app_a_function_select(context, f_name);
-  }
+  };
+  await app_a_functions_generic(context, on_select);
 }
