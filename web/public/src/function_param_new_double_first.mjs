f42ec3afcc2c16171bd422dbe090b86e4e071dc2
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 13:47:35 -0500

    fws pnd first

---
+++ web/public/src/function_param_new_double_first.mjs
@@ -0,0 +1,4 @@
+import { function_param_new_double } from "../../../love/public/src/function_param_new_double.mjs";
+export async function function_param_new_double_first(param_name) {
+  return await function_param_new_double(param_name);
+}
