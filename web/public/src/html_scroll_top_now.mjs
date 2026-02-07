30b811c5030fff05275a489466e7a30808e614f7
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 09:43:04 -0500

    crl2 html_scroll_center_generic top_now

---
+++ web/public/src/html_scroll_top_now.mjs
@@ -0,0 +1,5 @@
+import { html_scroll_generic } from "../../../love/public/src/html_scroll_generic.mjs";
+export async function html_scroll_top_now(component, behavior) {
+  const block = "center";
+  await html_scroll_generic(component, behavior, block);
+}
