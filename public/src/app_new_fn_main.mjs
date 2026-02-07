3db13b261ed65cc05465fd892bb72207e4ae64a8
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:03 -0500

    function_auto_path ..\love\public\src\app_generic_name_main_get.mjs

--- public/src/app_new_fn_main.mjs
+++ public/src/app_new_fn_main.mjs
@@ -1,8 +1,8 @@
 import { string_and_empty_not_is_assert } from "../../../love/public/src/string_and_empty_not_is_assert.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
-import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
+import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
 export async function app_new_fn_main(name) {
   string_and_empty_not_is_assert(name);
-  let combined = app_generic_name_main(name);
+  let combined = app_shared_name_main(name);
   await function_new(combined);
 }
