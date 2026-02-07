e99835a2a3334fc01e4bb567781613fb210e7d32
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 14:58:33 -0500

    function_auto_path ..\love\public\src\function_param_swap_task_simple.mjs

--- web/public/src/function_param_swap_task_simple.mjs
+++ web/public/src/function_param_swap_task_simple.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 import { function_param_swap } from "../../../love/public/src/function_param_swap.mjs";
 import { todo } from "../../../love/public/src/todo.mjs";
@@ -5,6 +6,7 @@ export async function function_param_swap_task_simple() {
   "todo a simple test case of " + function_param_swap.name;
   let f_name = function_param_swap_task_simple;
   await function_new(f_name2);
-  await function_param_swap(param_name_a, param_name_b, error());
+  let f_name3 = error();
+  await function_param_swap(param_name_a, param_name_b, f_name3);
   todo();
 }
