5b0182c50df13b96a8a7cd240b8668c8f71e25c4
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 19:56:38 -0500

    cr app_ceb_bible_gloss_generate ceb original

---
+++ web/public/src/app_original_bible_gloss_generate.mjs
@@ -0,0 +1,21 @@
+import { app_ceb_bible_gloss_generate_generic } from "../../../love/public/src/app_ceb_bible_gloss_generate_generic.mjs";
+import { ebible_folder_cebuano } from "../../../love/public/src/ebible_folder_cebuano.mjs";
+import { app_ceb_bible_gloss_generate_property } from "../../../love/public/src/app_ceb_bible_gloss_generate_property.mjs";
+export async function app_original_bible_gloss_generate() {
+  let bible_folder = "engbsb";
+  let book_code = "JAS";
+  let language = "Cebuano";
+  let fn = app_original_bible_gloss_generate;
+  let property_name = app_ceb_bible_gloss_generate_property();
+  let c = ebible_folder_cebuano();
+  const bible_folders = [c, bible_folder];
+  let last = "original language and English are";
+  await app_ceb_bible_gloss_generate_generic(
+    language,
+    last,
+    bible_folders,
+    book_code,
+    fn,
+    property_name,
+  );
+}
