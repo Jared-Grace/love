1e28ea45ff24f9862d91998294e773e693935a98
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:26:07 -0500

    function_auto_path ..\love\public\src\app_shared_refresh_screen.mjs

--- web/public/src/app_generic_screen_set.mjs
+++ web/public/src/app_generic_screen_set.mjs
@@ -1,7 +1,7 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
-import { app_generic_refresh_screen } from "../../../love/public/src/app_generic_refresh_screen.mjs";
+import { app_shared_refresh_screen } from "../../../love/public/src/app_shared_refresh_screen.mjs";
 export function app_generic_screen_set(context, fn) {
   let fn_name = object_property_get(fn, "name");
-  app_generic_refresh_screen(context, fn_name);
+  app_shared_refresh_screen(context, fn_name);
 }
