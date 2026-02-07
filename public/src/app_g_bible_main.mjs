dbdf3ee4419e19bead3f418403e5bf81c18bd28b
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:53 -0500

    function_auto_path ..\love\public\src\app_shared_refresh.mjs

--- public/src/app_g_bible_main.mjs
+++ public/src/app_g_bible_main.mjs
@@ -1,5 +1,5 @@
 import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs";
-import { app_generic_refresh } from "../../../love/public/src/app_generic_refresh.mjs";
+import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
 import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { firebase_login } from "../../../love/public/src/firebase_login.mjs";
 import { app_g_bible_screens } from "../../../love/public/src/app_g_bible_screens.mjs";
@@ -9,7 +9,7 @@ export async function app_g_bible_main(context) {
   let screens = app_g_bible_screens();
   app_bible_main_generic_before(context, app_fn, screens, app_g_bible_home);
   function lambda() {
-    app_generic_refresh(context);
+    app_shared_refresh(context);
   }
   await firebase_login(context, lambda);
 }
