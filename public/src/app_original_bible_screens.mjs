10fc1b6e384b4d98c0305ef5405f6301afd7b656
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:11:54 -0500

    cr app_ceb_bible_screens ceb original

---
+++ public/src/app_original_bible_screens.mjs
@@ -0,0 +1,13 @@
+import { app_ceb_bible_home } from "../../../love/public/src/app_ceb_bible_home.mjs";
+import { app_bible_languages } from "../../../love/public/src/app_bible_languages.mjs";
+import { app_bible_chapters } from "../../../love/public/src/app_bible_chapters.mjs";
+import { app_bible_books } from "../../../love/public/src/app_bible_books.mjs";
+export function app_original_bible_screens() {
+  let screens = [
+    app_ceb_bible_home,
+    app_bible_books,
+    app_bible_chapters,
+    app_bible_languages,
+  ];
+  return screens;
+}
