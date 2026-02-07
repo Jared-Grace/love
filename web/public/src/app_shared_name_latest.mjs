6a9fe2170d799286ba90a801aeb2d102873777bd
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:24:34 -0500

    function_auto_path ..\karate_code\public\src\app_karate_flow_sign_up_next.mjs

---
+++ web/public/src/app_shared_name_latest.mjs
@@ -0,0 +1,7 @@
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { app_generic_name_prefixed } from "../../../love/public/src/app_generic_name_prefixed.mjs";
+export function app_shared_name_latest(name) {
+  let a_name = app_generic_name_prefixed(name);
+  let combined = function_name_combine(a_name, "latest");
+  return combined;
+}
