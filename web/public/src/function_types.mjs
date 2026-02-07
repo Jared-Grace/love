0eaeb1055d4e68d9af133c4926b0e7712a7f3a2d
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 14:42:13 -0500

    function_auto_path ..\love\public\src\function_types.mjs

--- web/public/src/function_types.mjs
+++ web/public/src/function_types.mjs
@@ -1,10 +1,8 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
 import { js_node_types } from "../../../love/public/src/js_node_types.mjs";
-import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 export async function function_types() {
-  let f_name = await function_current_get();
   let v = await function_parse_unaliased(f_name);
   let ast = object_property_get(v, "ast");
   const f_types = js_node_types(ast);
