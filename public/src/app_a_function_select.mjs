8f259a5bbd7af0d049d232ce5fe7a7a4bc86eef5
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:44:37 -0500

    function_auto_path ..\love\public\src\app_a_function_select.mjs

--- public/src/app_a_function_select.mjs
+++ public/src/app_a_function_select.mjs
@@ -5,7 +5,9 @@ import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 export function app_a_function_select(context, f_name) {
   storage_local_set_context(context, "f_name_selected", f_name);
-  function lambda() {}
+  function lambda(value) {
+    return value;
+  }
   let h = storage_local_transform_context(
     context,
     "f_name_selected",
