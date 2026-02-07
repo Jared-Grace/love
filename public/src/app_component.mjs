4e779077066911406dc4b38ddd76d85f63786a80
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:03 -0500

    function_auto_path ..\love\public\src\app_generic_name_main_get.mjs

--- public/src/app_component.mjs
+++ public/src/app_component.mjs
@@ -7,10 +7,10 @@ import { html_document_body } from "../../../love/public/src/html_document_body.
 import { app_context_initialize_root } from "../../../love/public/src/app_context_initialize_root.mjs";
 import { function_dependencies_code_unaliased } from "../../../love/public/src/function_dependencies_code_unaliased.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
+import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
 import { html_button_width_full } from "../../../love/public/src/html_button_width_full.mjs";
 export async function app_component(a_name, on_click, button_text) {
-  let combined = app_generic_name_main(a_name);
+  let combined = app_shared_name_main(a_name);
   let v = await function_dependencies_code_unaliased(combined);
   let d = object_property_get(v, "d");
   let code = object_property_get(d, "code");
--- public/src/app_new_update_generic.mjs
+++ public/src/app_new_update_generic.mjs
@@ -6,7 +6,7 @@ import { list_empty } from "../../../love/public/src/list_empty.mjs";
 import { js_declaration_single_block_body } from "../../../love/public/src/js_declaration_single_block_body.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
-import { app_generic_name_main } from "../../../love/public/src/app_generic_name_main.mjs";
+import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 import { firebase_name_repo } from "../../../love/public/src/firebase_name_repo.mjs";
@@ -31,7 +31,7 @@ export async function app_new_update_generic(
   async function lambda(ast) {
     let declaration = js_declaration_single(ast);
     declaration.async = true;
-    let value_string = app_generic_name_main(name);
+    let value_string = app_shared_name_main(name);
     let main_name = js_code_string(value_string);
     let code = js_code_call_args_await_maybe(
       unaliased,
