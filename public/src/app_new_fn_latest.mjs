eeb64c7b0dd5e8ef55baf0b16be7a2f3b112189a
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:24:46 -0500

    function_auto_path ..\love\public\src\app_new_fn_latest.mjs

--- public/src/app_new_fn_latest.mjs
+++ public/src/app_new_fn_latest.mjs
@@ -1,6 +1,6 @@
-import { app_generic_name_latest } from "../../../love/public/src/app_generic_name_latest.mjs";
+import { app_shared_name_latest } from "../../../love/public/src/app_shared_name_latest.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function app_new_fn_latest(name) {
-  let combined = app_generic_name_latest(name);
+  let combined = app_shared_name_latest(name);
   await function_new(combined);
 }
--- public/src/app_new_latest_update.mjs
+++ public/src/app_new_latest_update.mjs
@@ -1,7 +1,7 @@
 import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
-import { app_generic_name_latest } from "../../../love/public/src/app_generic_name_latest.mjs";
+import { app_shared_name_latest } from "../../../love/public/src/app_shared_name_latest.mjs";
 import { app_new_update_generic } from "../../../love/public/src/app_new_update_generic.mjs";
 export async function app_new_latest_update(name) {
-  let combined = app_generic_name_latest(name);
+  let combined = app_shared_name_latest(name);
   await app_new_update_generic(name, app_main_latest, combined);
 }
