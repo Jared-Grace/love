3cc421fe67531f9a54a4e12786978c650f378f81
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:25:19 -0500

    function_auto_path ..\love\public\src\app_promote.mjs

--- web/public/src/function_open_app.mjs
+++ web/public/src/function_open_app.mjs
@@ -1,9 +1,9 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
+import { app_shared_name_main_get } from "../../../love/public/src/app_shared_name_main_get.mjs";
 import { data_app_current_set } from "../../../love/public/src/data_app_current_set.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 export async function function_open_app(a_name) {
-  let v2 = await app_generic_name_main_get(a_name);
+  let v2 = await app_shared_name_main_get(a_name);
   let a_name_final = object_property_get(v2, "a_name");
   let f_name = object_property_get(v2, "f_name");
   let v = await function_open(f_name);
--- web/public/src/functions_search_app_prefix.mjs
+++ web/public/src/functions_search_app_prefix.mjs
@@ -3,10 +3,10 @@ import { function_name_to_path } from "../../../love/public/src/function_name_to
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 import { app_generic_name_prefixed } from "../../../love/public/src/app_generic_name_prefixed.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
+import { app_shared_name_main_get } from "../../../love/public/src/app_shared_name_main_get.mjs";
 import { functions_search } from "../../../love/public/src/functions_search.mjs";
 export async function functions_search_app_prefix(s) {
-  let v2 = await app_generic_name_main_get(s);
+  let v2 = await app_shared_name_main_get(s);
   s = object_property_get(v2, "a_name");
   let a_name = app_generic_name_prefixed(s);
   let separator = function_name_separator();
