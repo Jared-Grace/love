e16bd95690bf2bbc42d6d8fddf6e26c984bc2343
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:24:46 -0500

    function_auto_path ..\love\public\src\app_new_fn_latest.mjs

--- web/public/src/html_update_latest.mjs
+++ web/public/src/html_update_latest.mjs
@@ -1,13 +1,13 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_generic_name_main_get } from "../../../love/public/src/app_generic_name_main_get.mjs";
-import { app_generic_name_latest } from "../../../love/public/src/app_generic_name_latest.mjs";
+import { app_shared_name_latest } from "../../../love/public/src/app_shared_name_latest.mjs";
 import { html_update_public_generic } from "../../../love/public/src/html_update_public_generic.mjs";
 import { html_name_to_path_latest } from "../../../love/public/src/html_name_to_path_latest.mjs";
 export async function html_update_latest(name) {
   "todo: maybe rename staging everywhere to latest to be consistent with naming, one less name (staging not needed) to remember";
   let v2 = await app_generic_name_main_get(name);
   name = object_property_get(v2, "a_name");
-  let f_name = app_generic_name_latest(name);
+  let f_name = app_shared_name_latest(name);
   let file_path = html_name_to_path_latest(name);
   await html_update_public_generic(f_name, file_path, name);
 }
