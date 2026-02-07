3fc0d8149163797b9fd1b6fe4ba7860bbb9e3725
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 18:07:45 -0500

    rrl app_a_button_function_refresh if_exists

--- web/public/src/app_a_functions.mjs
+++ web/public/src/app_a_functions.mjs
@@ -1,8 +1,8 @@
 import { app_a_function_select_curry } from "../../../love/public/src/app_a_function_select_curry.mjs";
 import { app_a_functions_generic } from "../../../love/public/src/app_a_functions_generic.mjs";
-import { app_a_button_function_refresh } from "../../../love/public/src/app_a_button_function_refresh.mjs";
+import { app_a_button_function_if_exists } from "../../../love/public/src/app_a_button_function_if_exists.mjs";
 export async function app_a_functions(context) {
-  app_a_button_function_refresh(context);
+  app_a_button_function_if_exists(context);
   let on_select = app_a_function_select_curry(context);
   await app_a_functions_generic(context, on_select);
 }
