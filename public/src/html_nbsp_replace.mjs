b876fe928453fc54e5ad29111689881ffc3c3c0a
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 08:06:46 -0500

    function_auto_path ..\love\public\src\html_nbsp_replace.mjs

--- public/src/html_nbsp_replace.mjs
+++ public/src/html_nbsp_replace.mjs
@@ -1,4 +1,5 @@
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
 export function html_nbsp_replace(s, from, to) {
-  return string_replace(s, from, to);
+  let replaced = string_replace(s, from, to);
+  return replaced;
 }
