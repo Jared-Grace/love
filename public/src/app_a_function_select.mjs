438cae54d6121c211792a724e3d63ef3fc874d08
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:57:29 -0500

    function_auto_path ..\love\public\src\app_a_function_select.mjs

--- public/src/app_a_function_select.mjs
+++ public/src/app_a_function_select.mjs
@@ -1,3 +1,4 @@
+import { app_a_function_name_selected_key } from "../../../love/public/src/app_a_function_name_selected_key.mjs";
 import { list_remove_all } from "../../../love/public/src/list_remove_all.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { storage_local_transform_context } from "../../../love/public/src/storage_local_transform_context.mjs";
@@ -6,7 +7,8 @@ import { storage_local_set_context } from "../../../love/public/src/storage_loca
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 export function app_a_function_select(context, f_name) {
-  storage_local_set_context(context, "f_name_selected", f_name);
+  let key = app_a_function_name_selected_key();
+  storage_local_set_context(context, key, f_name);
   function lambda(h) {
     list_remove_all(h, f_name);
     list_add(h, f_name);
