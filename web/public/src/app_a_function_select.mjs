2f2114fc5e6b7bd06fe054e4bbf012e90ba437d8
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 17:41:27 -0500

    function_auto_path ..\love\public\src\app_a_function_select.mjs

--- web/public/src/app_a_function_select.mjs
+++ web/public/src/app_a_function_select.mjs
@@ -1,9 +1,7 @@
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { app_a_function } from "../../../love/public/src/app_a_function.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
-import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 export function app_a_function_select(context, f_name) {
-  let app_fn = object_property_get(context, "app_fn");
-  storage_local_set(app_fn, "f_name_selected", f_name);
+  storage_local_set_context(context, "f_name_selected", f_name);
   app_shared_screen_set(context, app_a_function);
 }
