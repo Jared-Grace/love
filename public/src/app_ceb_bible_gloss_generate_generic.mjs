9b066433091969e0b6729bb0827b775de854ca40
Grace-Jared <christrosetolife@gmail.com>
2026-02-06 20:44:01 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_generic.mjs

--- public/src/app_ceb_bible_gloss_generate_generic.mjs
+++ public/src/app_ceb_bible_gloss_generate_generic.mjs
@@ -13,6 +13,7 @@ export async function app_ceb_bible_gloss_generate_generic(
   const prompt_system = `You will be given a Bible passage and its context in ${language}.
 For each ${language} word, output an English gloss.
 Also output a full explanation of each ${language} word, explaining its meaning and grammar (including prefixes and suffixes), written for an English speaker with no background in grammar. 
+Explanations should include any etymology.
 Explanations should be easy to understand. Explain as simply as possible.
 
 Output format:
