e0da5ea91c7a3f4a468a90a9d3c5273f1e187237
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 15:04:18 -0500

    rpi js_type 1 list

--- web/public/src/js_return_atomize.mjs
+++ web/public/src/js_return_atomize.mjs
@@ -1,13 +1,13 @@
 import { js_return_on_async } from "../../../love/public/src/js_return_on_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { js_type } from "../../../love/public/src/js_type.mjs";
+import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
 import { js_node_atomize } from "../../../love/public/src/js_node_atomize.mjs";
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
 export async function js_return_atomize(ast) {
   let existing = js_identifiers_names(ast);
-  let rs = js_type(ast, "ReturnStatement");
+  let rs = js_list_type(ast, "ReturnStatement");
   async function lambda(v) {
     let { node } = v;
     await js_return_on_async(node, noop, identifier_not);
