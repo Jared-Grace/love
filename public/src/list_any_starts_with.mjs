ea13db6eda91347f14d3387c3a821565e6bd280d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:01 -0500

    function_auto_path ..\love\public\src\text_starts_with.mjs

--- public/src/list_any_starts_with.mjs
+++ public/src/list_any_starts_with.mjs
@@ -1,8 +1,8 @@
 import { list_any } from "../../../love/public/src/list_any.mjs";
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
+import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 export function list_any_starts_with(item, prefixes) {
   function lambda2(item2) {
-    let sw = string_starts_with(item, item2);
+    let sw = text_starts_with(item, item2);
     return sw;
   }
   let any = list_any(prefixes, lambda2);
