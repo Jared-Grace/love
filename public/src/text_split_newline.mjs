25ff0c85cfbe7639e9363d65c72762f9407ffa95
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:57 -0500

    function_auto_path ..\love\public\src\text_split_newline.mjs

--- public/src/text_split_newline.mjs
+++ public/src/text_split_newline.mjs
@@ -1,6 +1,6 @@
 import { newline } from "../../../love/public/src/newline.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
-export function string_split_newline(s) {
+export function text_split_newline(s) {
   let separator = newline();
   let lines = text_split(s, separator);
   return lines;
