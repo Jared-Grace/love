7b5f1e76257ecb4e78239f5e86b654738d0ec751
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 20:14:37 -0500

    pne js_statement_return_insert index

--- public/src/js_statement_return_insert.mjs
+++ public/src/js_statement_return_insert.mjs
@@ -1,6 +1,6 @@
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { js_statement_return_argument } from "../../../love/public/src/js_statement_return_argument.mjs";
-export function js_statement_return_insert(list, argument) {
+export function js_statement_return_insert(list, argument, index) {
   let r = js_statement_return_argument(argument);
   list_insert(list, r);
 }
