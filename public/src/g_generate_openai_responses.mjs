99776e0743e204528da2b592a09d57c0f4da1ad4
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:44:21 -0500

    rs openai_responses file

--- public/src/g_generate_openai_responses.mjs
+++ public/src/g_generate_openai_responses.mjs
@@ -1,7 +1,7 @@
-import { openai_responses } from "../../../love/public/src/openai_responses.mjs";
+import { openai_responses_file } from "../../../love/public/src/openai_responses_file.mjs";
 import { openai_generic } from "../../../love/public/src/openai_generic.mjs";
 export async function g_generate_openai_responses(system, user) {
-  let fn = openai_responses;
+  let fn = openai_responses_file;
   let data = await openai_generic(fn, system, user);
   return data;
 }
