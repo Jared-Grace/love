4d710523ca0e687aaaa3af5a29b07ea948fbd8d9
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:07:19 -0500

    function_auto_path ..\love\public\src\file_json_transform_initialize.mjs

--- public/src/file_json_transform_initialize.mjs
+++ public/src/file_json_transform_initialize.mjs
@@ -1,15 +1,10 @@
+import { file_write_json_if_exists_not } from "../../../love/public/src/file_write_json_if_exists_not.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
-import { file_write_json } from "../../../love/public/src/file_write_json.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { file_exists } from "../../../love/public/src/file_exists.mjs";
 export async function file_json_transform_initialize(
   f_path,
   initial,
   lambda$data,
 ) {
-  let exists = await file_exists(f_path);
-  if (not(exists)) {
-    await file_write_json(f_path, initial);
-  }
+  await file_write_json_if_exists_not(f_path, initial);
   await file_json_transform(f_path, lambda$data);
 }
