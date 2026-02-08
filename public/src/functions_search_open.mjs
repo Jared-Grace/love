eb5763320fc927b30a50de3b72b46fe1d0d5628d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:49:13 -0500

    r list_nearby functions_search

--- public/src/functions_search_open.mjs
+++ public/src/functions_search_open.mjs
@@ -1,9 +1,9 @@
-import { list_nearby } from "../../../love/public/src/list_nearby.mjs";
+import { functions_search } from "../../../love/public/src/functions_search.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
 export async function functions_search_open(search) {
-  let result = await list_nearby(search);
+  let result = await functions_search(search);
   let properties = object_properties(result);
   let f_name = list_single(properties);
   await function_open(f_name);
