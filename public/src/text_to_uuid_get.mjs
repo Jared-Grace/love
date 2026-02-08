2c53185c2f15f09d34cdfcef13fa60ad9334a84c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:05:02 -0500

    function_auto_path ..\love\public\src\text_to_uuid_get.mjs

--- public/src/text_to_uuid_get.mjs
+++ public/src/text_to_uuid_get.mjs
@@ -3,10 +3,9 @@ import { object_property_initialize } from "../../../love/public/src/object_prop
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { uuid } from "../../../love/public/src/uuid.mjs";
 import { object_property_exists_not_assert } from "../../../love/public/src/object_property_exists_not_assert.mjs";
-import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
 export async function text_to_uuid_get(text) {
   let joined = text_to_uuid_path();
-  await file_json_transform_initialize(joined, {}, lambda);
+  await file_json_read(joined, {}, lambda);
   async function lambda(data) {
     let set = object_property_initialize(data, "set", {});
     object_property_exists_not_assert(set, text);
