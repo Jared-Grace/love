4c605d041406f44341d9b4a1161da51fa5734a3f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:15:53 -0500

    function_auto_path ..\love\public\src\text_split_comma.mjs

--- web/public/src/function_multiple_param_delete_generic.mjs
+++ web/public/src/function_multiple_param_delete_generic.mjs
@@ -1,12 +1,12 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 export async function function_multiple_param_delete_generic(
   f_names_comma,
   param_name,
   fn,
 ) {
-  let f_names = string_split_comma(f_names_comma);
+  let f_names = text_split_comma(f_names_comma);
   async function lambda(f_name) {
     let v2 = await function_current_set(f_name);
     let v = await fn(param_name);
