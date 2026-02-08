eaff8c370ea0d9ef0badd3ac4d85539b5d40f4d5
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:00 -0500

    function_auto_path ..\love\public\src\function_alias_replace.mjs

--- public/src/string_is_assert_multiple.mjs
+++
@@ -1,8 +0,0 @@
-import { each } from "../../../love/public/src/each.mjs";
-import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
-export function string_is_assert_multiple(items) {
-  function lambda2(item) {
-    text_is_assert(item);
-  }
-  each(items, lambda2);
-}
--- public/src/text_last.mjs
+++ public/src/text_last.mjs
@@ -1,6 +1,6 @@
 import { text_get } from "../../../love/public/src/text_get.mjs";
 import { text_index_last } from "../../../love/public/src/text_index_last.mjs";
-export function string_last(s) {
+export function text_last(s) {
   let index_last = text_index_last(s);
   let item = text_get(s, index_last);
   return item;
