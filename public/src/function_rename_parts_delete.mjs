b43fbb7b481ea3daaa43fdc62d0980c3a7d2d1a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:34:05 -0500

    function_auto_path ..\love\public\src\function_rename_parts_delete.mjs

--- public/src/function_rename_parts_delete.mjs
+++ public/src/function_rename_parts_delete.mjs
@@ -1,7 +1,9 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_name_parts_remove_comma } from "../../../love/public/src/function_name_parts_remove_comma.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_parts_delete(f_name_old, deleted) {
+  assert_arguments(arguments, 2);
   f_name_old = await function_name_unalias_only(f_name_old);
   let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
   let v = await function_rename(f_name_old, f_name_new);
