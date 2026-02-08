bd3d5988ea314460050391f930f3386efceb97ed
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:27 -0500

    function_auto_path ..\love\public\src\string_is_assert.mjs

--- web/public/src/reply_wrap_invoke.mjs
+++ web/public/src/reply_wrap_invoke.mjs
@@ -2,7 +2,7 @@ import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mj
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
-import { string_is } from "../../../love/public/src/string_is.mjs";
+import { text_is } from "../../../love/public/src/text_is.mjs";
 import { json_equal } from "../../../love/public/src/json_equal.mjs";
 import { list_is_assert_json } from "../../../love/public/src/list_is_assert_json.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
@@ -11,14 +11,14 @@ import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_is } from "../../../love/public/src/function_is.mjs";
-import { reply_sequence } from "./reply_sequence.mjs";
+import { reply_sequence } from "../../../love/public/src/reply_sequence.mjs";
 export async function reply_wrap_invoke(item, possibilities) {
   let fi = function_is(item);
   let wrapped = null;
   if (fi) {
     wrapped = item;
   } else {
-    let si2 = string_is(item);
+    let si2 = text_is(item);
     let size = null;
     if (si2) {
       size = string_size(item);
