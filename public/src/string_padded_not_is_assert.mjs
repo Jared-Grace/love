bb97ef464500f47523ad18571161f1114d4cf4fc
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:49 -0500

    function_auto_path ..\love\public\src\text_padded_is.mjs

--- public/src/string_padded_not_is_assert.mjs
+++ public/src/string_padded_not_is_assert.mjs
@@ -1,6 +1,6 @@
 import { assert_not } from "../../../love/public/src/assert_not.mjs";
-import { string_padded_is } from "../../../love/public/src/string_padded_is.mjs";
+import { text_padded_is } from "../../../love/public/src/text_padded_is.mjs";
 export function string_padded_not_is_assert(suffix, padding) {
-  let p = string_padded_is(suffix, padding);
+  let p = text_padded_is(suffix, padding);
   assert_not(p);
 }
