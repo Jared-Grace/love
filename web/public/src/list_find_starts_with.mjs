05aec7aa2061cf09edda9ab479061e37546420f9
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:10 -0500

    function_auto_path ..\love\public\src\text_padded_is.mjs

--- web/public/src/list_find_starts_with.mjs
+++ web/public/src/list_find_starts_with.mjs
@@ -1,8 +1,8 @@
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
+import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 export function list_find_starts_with(list, prefix) {
   function lambda2(item2) {
-    let sw = string_starts_with(item2, prefix);
+    let sw = text_starts_with(item2, prefix);
     return sw;
   }
   let found = list_find(list, lambda2);
--- web/public/src/text_replace_if_starts_with.mjs
+++ web/public/src/text_replace_if_starts_with.mjs
@@ -1,7 +1,7 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
+import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 export function text_replace_if_starts_with(prefix, item, replacement) {
-  let sw = string_starts_with(item, prefix);
+  let sw = text_starts_with(item, prefix);
   if (sw) {
     item = text_replace(item, prefix, replacement);
   }
