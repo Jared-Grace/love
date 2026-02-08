13ebfec6ac8ad166d2f8671c35d6410e2a50d53b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:28 -0500

    function_auto_path ..\love\public\src\string_is_assert_message.mjs

--- public/src/string_is_assert.mjs
+++
@@ -1,6 +0,0 @@
-import { assert } from "../../../love/public/src/assert.mjs";
-import { text_is } from "../../../love/public/src/text_is.mjs";
-export function string_is_assert(value) {
-  let b = text_is(value);
-  assert(b);
-}
