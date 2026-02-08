6ab1158188e1bfe9e71476cc027ccadd52302698
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:15 -0500

    function_auto_path ..\love\public\src\g_characters_unzip.mjs

--- web/public/src/string_includes_not.mjs
+++ web/public/src/string_includes_not.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
-import { string_includes } from "../../../love/public/src/string_includes.mjs";
+import { text_includes } from "../../../love/public/src/text_includes.mjs";
 export function string_includes_not(item, part) {
-  let v = string_includes(item, part);
+  let v = text_includes(item, part);
   let n = not(v);
   return n;
 }
