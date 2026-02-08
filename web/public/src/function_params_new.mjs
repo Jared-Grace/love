35ab7e1c52328d5a93d12a0ece7405163039fbaf
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:15:33 -0500

    function_auto_path ..\love\public\src\text_split.mjs

--- web/public/src/function_params_new.mjs
+++ web/public/src/function_params_new.mjs
@@ -6,15 +6,15 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { js_declaration_single_params_add } from "../../../love/public/src/js_declaration_single_params_add.mjs";
-import { string_split } from "../../../love/public/src/string_split.mjs";
+import { text_split } from "../../../love/public/src/text_split.mjs";
 export async function function_params_new(
   f_name,
   param_names_comma,
   values_default_comma,
 ) {
   assert_arguments(arguments, 3);
-  let param_names = string_split(param_names_comma, ",");
-  let values_default = string_split(values_default_comma, ",");
+  let param_names = text_split(param_names_comma, ",");
+  let values_default = text_split(values_default_comma, ",");
   await function_params_new_generic(
     function_transform_current_lambda,
     on_call,
