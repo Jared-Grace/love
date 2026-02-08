bf8d23284417e2c5c2df559535425d00a80b8cae
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:15:23 -0500

    function_auto_path ..\love\public\src\text_slice.mjs

--- public/src/string_take.mjs
+++ public/src/string_take.mjs
@@ -1,5 +1,5 @@
-import { string_slice } from "../../../love/public/src/string_slice.mjs";
+import { text_slice } from "../../../love/public/src/text_slice.mjs";
 export function string_take(s, count) {
-  let taken = string_slice(s, 0, count);
+  let taken = text_slice(s, 0, count);
   return taken;
 }
