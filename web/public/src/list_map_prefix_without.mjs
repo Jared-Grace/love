778789b8d85591239e2602e72bb8f0f2c24ce34e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:13:28 -0500

    function_auto_path ..\love\public\src\list_map_prefix_without_any.mjs

--- web/public/src/list_map_prefix_without.mjs
+++ web/public/src/list_map_prefix_without.mjs
@@ -1,8 +1,8 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
+import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 export function list_map_prefix_without(list, prefix) {
   function lambda(item) {
-    let result = string_prefix_without(item, prefix);
+    let result = text_prefix_without(item, prefix);
     return result;
   }
   let mapped = list_map(list, lambda);
