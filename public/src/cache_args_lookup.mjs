c82358e445b6a5013b67f26518385ef4560e0d83
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:50:20 -0500

    function_auto_path ..\love\public\src\cache_args_lookup.mjs

--- public/src/cache_args_lookup.mjs
+++ public/src/cache_args_lookup.mjs
@@ -1,4 +1,4 @@
 import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
 export function cache_args_lookup() {
-  let joined = folder_user_storage_function_path(fn);
+  let joined = folder_user_storage_function_path(cache_args_lookup);
 }
