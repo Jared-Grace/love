f59739736c3a18cf7001c2871cc431359ac6033e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:33 -0500

    function_auto_path ..\love\public\src\text_includes_lambda.mjs

--- web/public/src/text_includes_lambda.mjs
+++ web/public/src/text_includes_lambda.mjs
@@ -1,5 +1,5 @@
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
-export function string_includes_lambda(item) {
+export function text_includes_lambda(item) {
   let l = function string_includes_lambda_inner(input) {
     let si = text_includes(input, item);
     return si;
