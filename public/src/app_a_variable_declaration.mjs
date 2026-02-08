e643a3ca3639c6ca5cd61a144fa10861dda0347a
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 19:25:43 -0500

    function_auto_path ..\love\public\src\app_a_variable_declaration.mjs

--- public/src/app_a_variable_declaration.mjs
+++ public/src/app_a_variable_declaration.mjs
@@ -1,3 +1,6 @@
+import { app_a_function_on_change } from "../../../love/public/src/app_a_function_on_change.mjs";
+import { js_call_new_insert } from "../../../love/public/src/js_call_new_insert.mjs";
+import { app_a_functions_overlay } from "../../../love/public/src/app_a_functions_overlay.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_a_call_above } from "../../../love/public/src/app_a_call_above.mjs";
 import { app_a_node_index } from "../../../love/public/src/app_a_node_index.mjs";
@@ -15,7 +18,33 @@ export function app_a_variable_declaration(a) {
   let k = app_a_keyword_blue_space(parent, kind);
   let keyword = object_property_get(k, "keyword");
   function lambda19(o2) {
-    let choices = [];
+    let choices = [
+      {
+        shortcut: "a",
+        text: "Add above",
+        fn: async function lambda2() {
+          let overlay_close = object_property_get(o, "overlay_close");
+          overlay_close();
+          let v2 = await app_a_functions_overlay(a, on_select);
+          let overlay_result = object_property_get(v2, "overlay_result");
+          async function on_select(f_name_call) {
+            let v = app_a_node_index(a);
+            let stack = object_property_get(v, "stack");
+            let index = object_property_get(v, "index");
+            let list = object_property_get(v, "list");
+            let ast = object_property_get(a, "ast");
+            let parsed = await js_call_new_insert(
+              f_name_call,
+              ast,
+              list,
+              index,
+              stack,
+            );
+            await app_a_function_on_change(overlay_result, a);
+          }
+        },
+      },
+    ];
     let v5 = app_a_call_above(o2, a);
     list_add(choices, v5);
     app_a_functionize_choices_add(choices, a, o2);
