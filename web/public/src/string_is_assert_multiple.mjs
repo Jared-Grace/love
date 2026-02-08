3c073107694f27519bccd8e2a25e199e29d0e16f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:39 -0500

    function_auto_path ..\love\public\src\js_identifier_unique.mjs

--- web/public/src/string_is_assert_multiple.mjs
+++ web/public/src/string_is_assert_multiple.mjs
@@ -1,8 +1,8 @@
 import { each } from "../../../love/public/src/each.mjs";
-import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 export function string_is_assert_multiple(items) {
   function lambda2(item) {
-    string_is_assert(item);
+    text_is_assert(item);
   }
   each(items, lambda2);
 }
