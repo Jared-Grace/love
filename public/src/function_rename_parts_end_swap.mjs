8adbeaef07b1d7c372a0bb56fee3035ad8f04f8c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:18:35 -0500

    function_auto_path ..\love\public\src\function_rename_parts_end_swap.mjs

--- public/src/function_rename_parts_end_swap.mjs
+++ public/src/function_rename_parts_end_swap.mjs
@@ -2,6 +2,5 @@ import { function_name_parts_swap_end } from "../../../love/public/src/function_
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_parts_end_swap(f_name_before) {
   let f_name_after = function_name_parts_swap_end(f_name_before);
-  let v = await function_rename(f_name_before, f_name_after);
-  return v;
+  await function_rename(f_name_before, f_name_after);
 }
