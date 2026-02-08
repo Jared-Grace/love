f229b9ee8d4f1bbfae28b06ccf8fa87ebd56b267
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 08:30:09 -0500

    de app_ceb_bible_gloss_generate

--- public/src/app_ceb_bible_gloss_generate.mjs
+++
@@ -1,5 +0,0 @@
-import { app_ceb_bible_gloss_generate_chapter } from "../../../love/public/src/app_ceb_bible_gloss_generate_chapter.mjs";
-export async function app_ceb_bible_gloss_generate() {
-  let chapter_code_specified = "JAS03";
-  await app_ceb_bible_gloss_generate_chapter(chapter_code_specified);
-}
