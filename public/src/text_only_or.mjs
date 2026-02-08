f9769398b957fa3c683d80cf9d4e09ec36394ade
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:20:39 -0500

    function_auto_path ..\love\public\src\text_transform.mjs

--- public/src/text_only_or.mjs
+++ public/src/text_only_or.mjs
@@ -1,7 +1,7 @@
-import { string_transform } from "../../../love/public/src/string_transform.mjs";
+import { text_transform } from "../../../love/public/src/text_transform.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
 export function text_only_or(s, valid, replacement) {
-  let joined = string_transform(s, lambda);
+  let joined = text_transform(s, lambda);
   function lambda(item) {
     let i = text_includes(valid, item);
     if (i) {
--- public/src/text_transform.mjs
+++ public/src/text_transform.mjs
@@ -1,7 +1,7 @@
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_to } from "../../../love/public/src/list_to.mjs";
-export function string_transform(s, lambda$c) {
+export function text_transform(s, lambda$c) {
   let l = list_to(s);
   let mapped = list_map(l, lambda$c);
   let joined = list_join_empty(mapped);
