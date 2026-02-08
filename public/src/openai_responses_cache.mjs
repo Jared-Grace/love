56b8eda6f0c30cf3916eb02aa9efcfd833bd323c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:48:07 -0500

    function_auto_path ..\love\public\src\openai_responses_cache.mjs

--- public/src/openai_responses_cache.mjs
+++ public/src/openai_responses_cache.mjs
@@ -1,4 +1,5 @@
 import { openai_responses } from "../../../love/public/src/openai_responses.mjs";
 export async function openai_responses_cache(system, user) {
-  return await openai_responses(system, user);
+  let r = await openai_responses(system, user);
+  return r;
 }
