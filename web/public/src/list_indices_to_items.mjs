3071e8676d4cce71ed136ae8f31810a361e2875d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:47:37 -0500

    function_auto_path ..\love\public\src\list_indices_to_items.mjs

--- web/public/src/list_indices_to_items.mjs
+++ web/public/src/list_indices_to_items.mjs
@@ -1,10 +1,7 @@
+import { list_get_curry } from "../../../love/public/src/list_get_curry.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 export function list_indices_to_items(list, indices) {
-  let mapper = function lambda(index) {
-    let item = list_get(list, index);
-    return item;
-  };
+  let mapper = list_get_curry(list);
   let nearby = list_map(indices, mapper);
   return nearby;
 }
