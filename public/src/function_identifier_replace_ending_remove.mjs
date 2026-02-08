3e4183880c39740884c0829c67ec358f5c11471f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:54 -0500

    function_auto_path ..\love\public\src\text_suffix_without.mjs

--- public/src/function_identifier_replace_ending_remove.mjs
+++ public/src/function_identifier_replace_ending_remove.mjs
@@ -1,10 +1,10 @@
-import { string_suffix_without } from "../../../love/public/src/string_suffix_without.mjs";
+import { text_suffix_without } from "../../../love/public/src/text_suffix_without.mjs";
 import { function_identifier_replace } from "../../../love/public/src/function_identifier_replace.mjs";
 export async function function_identifier_replace_ending_remove(
   identifier_name,
   ending_to_remove,
 ) {
-  let removed = string_suffix_without(identifier_name, ending_to_remove);
+  let removed = text_suffix_without(identifier_name, ending_to_remove);
   let v = await function_identifier_replace(identifier_name, removed);
   return v;
 }
