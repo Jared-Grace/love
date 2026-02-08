e9f000b1f54fdb6fe53116fa137a7688c70a5f99
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:27:58 -0500

    function_auto_path ..\love\public\src\list_nearby.mjs

--- public/src/list_nearby.mjs
+++ public/src/list_nearby.mjs
@@ -1,6 +1,5 @@
+import { list_indices_to_items } from "../../../love/public/src/list_indices_to_items.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { range_from } from "../../../love/public/src/range_from.mjs";
@@ -12,11 +11,7 @@ export function list_nearby(list, nearness) {
       return ii;
     }
     let filtered = list_filter(r, lambda6);
-    function lambda2(index2) {
-      let item2 = list_get(list, index2);
-      return item2;
-    }
-    let nearby = list_map(filtered, lambda2);
+    let nearby = list_indices_to_items(list, filtered);
     let m = {
       nearby,
       item,
