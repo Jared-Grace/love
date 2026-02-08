e010bdadfed1187cfec4a25c677a4ab3c163f199
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 16:13:43 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic.mjs

--- public/src/g_sermon_generate_book_generic.mjs
+++ public/src/g_sermon_generate_book_generic.mjs
@@ -15,7 +15,6 @@ import { each_multiple_async } from "../../../love/public/src/each_multiple_asyn
 import { log } from "../../../love/public/src/log.mjs";
 import { g_generate_openai_responses } from "../../../love/public/src/g_generate_openai_responses.mjs";
 import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
-import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { list_map_property_join_space } from "../../../love/public/src/list_map_property_join_space.mjs";
 import { g_generate_openai_chat_completions } from "../../../love/public/src/g_generate_openai_chat_completions.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
@@ -179,7 +178,8 @@ export async function g_sermon_generate_book_generic(
         };
         return v2;
       }
-    }await each_async(groups_match_chapter, lambda5);
+    }
+    await each_async(groups_match_chapter, lambda5);
     await file_overwrite_json(path, {
       chapter_code,
       passages,
