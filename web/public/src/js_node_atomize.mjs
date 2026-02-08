11c22069dd37484ba2f00689b0baf92eac92a942
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:04:54 -0500

    function_auto_path ..\love\public\src\js_node_atomize.mjs

--- web/public/src/js_node_atomize.mjs
+++ web/public/src/js_node_atomize.mjs
@@ -1,4 +1,3 @@
-import { js_node_atomize_name } from "../../../love/public/src/js_node_atomize_name.mjs";
 import { function_exists_strict } from "../../../love/public/src/function_exists_strict.mjs";
 import { function_parse_strict_declaration } from "../../../love/public/src/function_parse_strict_declaration.mjs";
 import { js_call_function_if } from "../../../love/public/src/js_call_function_if.mjs";
@@ -20,7 +19,6 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { assert_json } from "../../../love/public/src/assert_json.mjs";
 export async function js_node_atomize(existing, v) {
-  let variable_name = js_node_atomize_name();
   let node = object_property_get(v, "node");
   let stack = object_property_get(v, "stack");
   if (js_node_type_is(node, "CallExpression")) {
@@ -30,7 +28,7 @@ export async function js_node_atomize(existing, v) {
       let ast_callee = object_property_get(v3, "ast");
       let return_name = js_return_name(ast_callee);
       if (return_name !== null) {
-        variable_name = return_name;
+        let variable_name = return_name;
       }
     }
   }
@@ -54,7 +52,7 @@ export async function js_node_atomize(existing, v) {
             name,
             message: "param counts must match",
           });
-          variable_name = object_property_get(param, "name");
+          let variable_name = object_property_get(param, "name");
         }
       }
     }
