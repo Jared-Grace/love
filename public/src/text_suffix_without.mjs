be7799d8ee9f5ea6034ae68e5cf6e613b87ad132
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:52 -0500

    function_auto_path ..\love\public\src\function_watch_ending_change.mjs

--- public/src/text_suffix_without.mjs
+++ public/src/text_suffix_without.mjs
@@ -3,7 +3,7 @@ import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { text_slice } from "../../../love/public/src/text_slice.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
-export function string_suffix_without(s, suffix) {
+export function text_suffix_without(s, suffix) {
   let a = text_ends_with(s, suffix);
   if (not(a)) {
     error();
