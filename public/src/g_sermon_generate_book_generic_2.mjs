3fc8aef6a923a80332fef8bd1c94a0b2a5a568a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 08:42:33 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_2.mjs

--- public/src/g_sermon_generate_book_generic_2.mjs
+++ public/src/g_sermon_generate_book_generic_2.mjs
@@ -1,1 +1,9 @@
-export function g_sermon_generate_book_generic_2() {}
+export function g_sermon_generate_book_generic_2(
+  bible_folders,
+  book_code,
+  fn,
+  prompt_user_middle,
+  prompt_system,
+  property_name,
+  chapter_code_specified,
+) {}
