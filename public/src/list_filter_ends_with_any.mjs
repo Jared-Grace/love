29bbbf45f6c9684f7873f58b4dd501bcd90a4e92
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:07 -0500

    function_auto_path ..\love\public\src\string_ends_with_space.mjs

--- public/src/list_filter_ends_with_any.mjs
+++ public/src/list_filter_ends_with_any.mjs
@@ -1,10 +1,10 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_any } from "../../../love/public/src/list_any.mjs";
-import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
+import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 export function list_filter_ends_with_any(list, suffixes) {
   function lambda(item) {
     function lambda3(suffix) {
-      let ew = string_ends_with(item, suffix);
+      let ew = text_ends_with(item, suffix);
       return ew;
     }
     let any = list_any(suffixes, lambda3);
--- public/src/string_padded_is.mjs
+++ public/src/string_padded_is.mjs
@@ -1,8 +1,8 @@
-import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
+import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 export function string_padded_is(s, padding) {
   let sw = string_starts_with(s, padding);
-  let ew = string_ends_with(s, padding);
+  let ew = text_ends_with(s, padding);
   let p = sw || ew;
   return p;
 }
