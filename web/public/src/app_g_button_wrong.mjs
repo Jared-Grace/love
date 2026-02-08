c0fe6f6978bed721145f6c643f3468fb63f25728
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:19:28 -0500

    function_auto_path ..\love\public\src\text_take.mjs

--- web/public/src/app_g_button_wrong.mjs
+++ web/public/src/app_g_button_wrong.mjs
@@ -1,8 +1,8 @@
 import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
-import { string_take } from "../../../love/public/src/string_take.mjs";
+import { text_take } from "../../../love/public/src/text_take.mjs";
 import { html_border_invalid_color } from "../../../love/public/src/html_border_invalid_color.mjs";
 export function app_g_button_wrong(b) {
   let v = html_border_invalid_color();
-  let t = string_take(v, 7) + "cd";
+  let t = text_take(v, 7) + "cd";
   html_style_background_color(b, t);
 }
