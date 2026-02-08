cbeb8bae214331bdbaa28335b3e09ae0b2692a7e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:12 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_with_replace.mjs

--- web/public/src/function_multiple_rename_if_starts_with_replace.mjs
+++ web/public/src/function_multiple_rename_if_starts_with_replace.mjs
@@ -1,5 +1,5 @@
 import { function_multiple_rename_generic_starts_with } from "../../../love/public/src/function_multiple_rename_generic_starts_with.mjs";
-import { string_replace } from "../../../love/public/src/string_replace.mjs";
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export async function function_multiple_rename_if_starts_with_replace(
   f_name_prefix,
@@ -12,7 +12,7 @@ export async function function_multiple_rename_if_starts_with_replace(
     f_name_prefix,
   );
   function name_change(f_name_before) {
-    let f_name_after = string_replace(f_name_before, from, to);
+    let f_name_after = text_replace(f_name_before, from, to);
     return f_name_after;
   }
 }
--- web/public/src/git_repo_rename_normalize.mjs
+++ web/public/src/git_repo_rename_normalize.mjs
@@ -1,7 +1,7 @@
-import { string_replace } from "../../../love/public/src/string_replace.mjs";
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { git_repo_rename } from "../../../love/public/src/git_repo_rename.mjs";
 export async function git_repo_rename_normalize(owner, name_old) {
-  let name = string_replace(name_old, "-", "_");
+  let name = text_replace(name_old, "-", "_");
   let stdout = await git_repo_rename(owner, name_old, name);
   let result = {
     name,
