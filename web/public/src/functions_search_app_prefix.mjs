da91ea6fba7b5de728f271cfa4d2beae4fbb04e9
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:49:13 -0500

    function_auto_path ..\love\public\src\functions_search.mjs

--- web/public/src/functions_search_app_prefix.mjs
+++ web/public/src/functions_search_app_prefix.mjs
@@ -5,13 +5,13 @@ import { function_name_separator } from "../../../love/public/src/function_name_
 import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { app_shared_name_main_get } from "../../../love/public/src/app_shared_name_main_get.mjs";
-import { list_nearby } from "../../../love/public/src/list_nearby.mjs";
+import { functions_search } from "../../../love/public/src/functions_search.mjs";
 export async function functions_search_app_prefix(s) {
   let v2 = await app_shared_name_main_get(s);
   s = object_property_get(v2, "a_name");
   let a_name = app_shared_name_prefixed(s);
   let separator = function_name_separator();
-  let results_search = await list_nearby(a_name + separator + "");
+  let results_search = await functions_search(a_name + separator + "");
   let value = function_name_to_path(a_name);
   let results = object_merge(
     {
