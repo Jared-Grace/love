ab2b208f94dde0df50c7b1aa3f414a55baaf72ba
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:44 -0500

    function_auto_path ..\love\public\src\text_size_greater_than_1.mjs

--- public/src/bible_names_text_parse.mjs
+++ public/src/bible_names_text_parse.mjs
@@ -2,12 +2,12 @@ import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_split_space } from "../../../love/public/src/list_map_split_space.mjs";
 import { list_map_first } from "../../../love/public/src/list_map_first.mjs";
 import { list_map_split_comma } from "../../../love/public/src/list_map_split_comma.mjs";
-import { string_size_greater_than_1 } from "../../../love/public/src/string_size_greater_than_1.mjs";
+import { text_size_greater_than_1 } from "../../../love/public/src/text_size_greater_than_1.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
 export function bible_names_text_parse(text) {
   let split = string_split_newline(text);
-  let filtered2 = list_filter(split, string_size_greater_than_1);
+  let filtered2 = list_filter(split, text_size_greater_than_1);
   let mapped3 = list_map_split_comma(filtered2);
   let mapped6 = list_map_first(mapped3);
   let mapped = list_map_split_space(mapped6);
