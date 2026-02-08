772ea11e679f8646cddfd7a83f3710caca19591a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:43:12 -0500

    rrl3 openai_chat_completions_generic cmd_py_api

--- public/src/openai_responses.mjs
+++ public/src/openai_responses.mjs
@@ -1,7 +1,7 @@
-import { openai_chat_completions_generic } from "../../../love/public/src/openai_chat_completions_generic.mjs";
+import { openai_cmd_py_api } from "../../../love/public/src/openai_cmd_py_api.mjs";
 export async function openai_responses(input_file_path, output_file_path) {
   const openai_script_name = "responses";
-  await openai_chat_completions_generic(
+  await openai_cmd_py_api(
     openai_script_name,
     input_file_path,
     output_file_path,
