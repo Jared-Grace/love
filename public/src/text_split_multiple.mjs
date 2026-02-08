bcd52d6715bfa4e966065cc2ba06ae418b7aa082
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:49 -0500

    function_auto_path ..\love\public\src\folder_read_browser.mjs

--- public/src/text_split_multiple.mjs
+++ public/src/text_split_multiple.mjs
@@ -3,7 +3,7 @@ import { list_join } from "../../../love/public/src/list_join.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function string_split_multiple(str, delimiters) {
+export function text_split_multiple(str, delimiters) {
   let v = list_first_remaining(delimiters);
   let remaining = object_property_get(v, "remaining");
   let first = object_property_get(v, "first");
