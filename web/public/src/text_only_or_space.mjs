99ab3ea4448731d1f39140f4442eda3f444eabb7
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:30 -0500

    function_auto_path ..\love\public\src\string_only_or_space.mjs

--- web/public/src/text_only_or_space.mjs
+++ web/public/src/text_only_or_space.mjs
@@ -1,5 +1,5 @@
 import { text_only_or } from "../../../love/public/src/text_only_or.mjs";
-export function string_only_or_space(s, symbols_split_non) {
+export function text_only_or_space(s, symbols_split_non) {
   let replaced = text_only_or(s, symbols_split_non, " ");
   return replaced;
 }
