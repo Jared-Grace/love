3b28fe794f4d093b291af9e52ba5bacb81675c99
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:27 -0500

    function_auto_path ..\love\public\src\string_is_assert.mjs

--- web/public/src/js_visit_children_get.mjs
+++ web/public/src/js_visit_children_get.mjs
@@ -1,13 +1,13 @@
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
-import { string_is } from "../../../love/public/src/string_is.mjs";
+import { text_is } from "../../../love/public/src/text_is.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function js_visit_children_get(n) {
   if (list_is(n)) {
     return n;
   }
-  if (string_is(n)) {
+  if (text_is(n)) {
     let v = [];
     return v;
   }
