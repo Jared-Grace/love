5fa60b6f3b319c30c55871a0ebc6321ee68d36f0
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:43:44 -0500

    function_auto_path ..\love\public\src\function_rename_append.mjs

--- public/src/function_rename_append.mjs
+++ public/src/function_rename_append.mjs
@@ -1,12 +1,9 @@
+import { text_remove_end } from "../../../love/public/src/text_remove_end.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
-import { function_name_separator_padded_not_is_assert } from "../../../love/public/src/function_name_separator_padded_not_is_assert.mjs";
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
-export async function function_rename_append(f_name_before, suffix) {
-  let fn = text_combine;
-  function_name_separator_padded_not_is_assert(suffix);
+export async function function_rename_append(f_name_before, count) {
   f_name_before = await function_name_unalias_only(f_name_before);
-  let f_name_after = fn(f_name_before, suffix);
+  let f_name_after = text_remove_end(f_name_before, count);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
