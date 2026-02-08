0b57804aa7b62e8c587dfb27d95b96cc6cd62fae
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:19 -0500

    function_auto_path ..\love\public\src\bible_verses_encouragement_generate.mjs

--- public/src/text_starts_with_dot.mjs
+++ public/src/text_starts_with_dot.mjs
@@ -1,5 +1,5 @@
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-export function string_starts_with_dot(s) {
+export function text_starts_with_dot(s) {
   let sw2 = text_starts_with(s, ".");
   return sw2;
 }
