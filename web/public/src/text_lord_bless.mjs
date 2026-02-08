ba9818a6e0165956e1213874af5bdd5516ba8e46
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:17 -0500

    function_auto_path ..\love\public\src\app_reply_pray_response.mjs

--- web/public/src/text_lord_bless.mjs
+++ web/public/src/text_lord_bless.mjs
@@ -1,5 +1,5 @@
-import { string_may_the_lord } from "../../../love/public/src/string_may_the_lord.mjs";
+import { text_may_the_lord } from "../../../love/public/src/text_may_the_lord.mjs";
 export function text_lord_bless() {
-  let v = string_may_the_lord() + "bless ";
+  let v = text_may_the_lord() + "bless ";
   return v;
 }
