27bfdd87469abeacabd265d4ea3f064878fa778f
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 19:50:24 -0500

    function_auto_path ..\love\public\src\app_a_call_above.mjs

--- public/src/app_a_call_above.mjs
+++ public/src/app_a_call_above.mjs
@@ -1,11 +1,12 @@
-import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_a_cut } from "../../../love/public/src/app_a_cut.mjs";
 import { app_a_function_on_change } from "../../../love/public/src/app_a_function_on_change.mjs";
 import { js_call_new_insert } from "../../../love/public/src/js_call_new_insert.mjs";
 import { app_a_node_index } from "../../../love/public/src/app_a_node_index.mjs";
 import { app_a_functions_overlay } from "../../../love/public/src/app_a_functions_overlay.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { list_add_multiple } from "./list_add_multiple.mjs";
 export function app_a_call_above(choices, o, a) {
-  let v5 = {
+  let ab = {
     shortcut: "a",
     text: "Add above",
     fn: async function lambda2() {
@@ -30,5 +31,6 @@ export function app_a_call_above(choices, o, a) {
       }
     },
   };
-  list_add(choices, v5);
+  let c = app_a_cut(o, ab);
+  list_add_multiple(choices, [a, c]);
 }
