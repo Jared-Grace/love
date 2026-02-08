d99cb42fbba9dc1c1266f2149c309dd6c51306ea
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 16:20:20 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic.mjs

--- public/src/g_sermon_generate_book_generic.mjs
+++ public/src/g_sermon_generate_book_generic.mjs
@@ -19,6 +19,8 @@ import { list_map_property_join_space } from "../../../love/public/src/list_map_
 import { g_generate_openai_chat_completions } from "../../../love/public/src/g_generate_openai_chat_completions.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
+import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -114,6 +116,21 @@ export async function g_sermon_generate_book_generic(
     if (exists) {
     } else {
     }
+    function filter_group(group) {
+      log({
+        group,
+      });
+      exit();
+      let items = object_property_get(group, "item");
+      let match_chapter = list_filter_property(
+        items,
+        "chapter_code",
+        chapter_code,
+      );
+      let ne = list_empty_not_is(match_chapter);
+      return ne;
+    }
+    let groups_match_chapter = list_filter_property(nearbys, filter_group);
     async function lambda5(g) {
       log(g);
       let passage = object_property_get(g, "item");
@@ -158,7 +175,7 @@ export async function g_sermon_generate_book_generic(
         return user_prompt;
       }
     }
-    await each_async(groups, lambda5);
+    await each_async(groups_match_chapter, lambda5);
     await file_overwrite_json(path, {
       chapter_code,
       passages,
