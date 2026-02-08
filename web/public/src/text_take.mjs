6f1171068f4397fdb5002d8a7a4a76a730b2896b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:19:26 -0500

    function_auto_path ..\love\public\src\ebible_versions_english_downloadable_symbols_unique.mjs

--- web/public/src/text_take.mjs
+++ web/public/src/text_take.mjs
@@ -1,5 +1,5 @@
 import { text_slice } from "../../../love/public/src/text_slice.mjs";
-export function string_take(s, count) {
+export function text_take(s, count) {
   let taken = text_slice(s, 0, count);
   return taken;
 }
