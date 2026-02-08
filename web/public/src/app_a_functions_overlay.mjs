7510d61fbe95dc5af44ba944c4bee72cc773ae2b
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 20:21:44 -0500

    function_auto_path ..\love\public\src\app_a_list_overlay_generic.mjs

--- web/public/src/app_a_functions_overlay.mjs
+++ web/public/src/app_a_functions_overlay.mjs
@@ -1,12 +1,7 @@
-import { app_a_functions_overlay_generic } from "../../../love/public/src/app_a_functions_overlay_generic.mjs";
+import { app_a_list_overlay_generic } from "../../../love/public/src/app_a_list_overlay_generic.mjs";
 import { app_a_functions_generic_f_names } from "../../../love/public/src/app_a_functions_generic_f_names.mjs";
 export async function app_a_functions_overlay(a, lambda$f_name) {
   let f_names = await app_a_functions_generic_f_names();
-  let r = app_a_functions_overlay_generic(
-    a,
-    f_names,
-    "function",
-    lambda$f_name,
-  );
+  let r = app_a_list_overlay_generic(a, f_names, "function", lambda$f_name);
   return r;
 }
