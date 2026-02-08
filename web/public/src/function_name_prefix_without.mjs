027b5cc32adda8f6e88e1cbdc587f0060faa6243
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:13:19 -0500

    function_auto_path ..\love\public\src\text_prefix_without.mjs

--- web/public/src/function_name_prefix_without.mjs
+++ web/public/src/function_name_prefix_without.mjs
@@ -1,11 +1,11 @@
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
-import { string_prefix_without } from "../../../love/public/src/string_prefix_without.mjs";
+import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 export function function_name_prefix_without(fn, fn_prefix) {
   let fn_name = object_property_get(fn, "name");
   let fn_prefix_name = object_property_get(fn_prefix, "name");
-  let without = string_prefix_without(
+  let without = text_prefix_without(
     fn_name,
     "" + fn_prefix_name + function_name_separator(),
   );
