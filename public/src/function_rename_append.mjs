33ccf1f01307909a2cb56125c426de9d9d162a3c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:56:28 -0500

    function_auto_path ..\love\public\src\function_rename_append.mjs

--- public/src/function_rename_append.mjs
+++ public/src/function_rename_append.mjs
@@ -1,7 +1,7 @@
+import { string_combine } from "../../../love/public/src/string_combine.mjs";
 import { function_rename_suffix_add_generic } from "../../../love/public/src/function_rename_suffix_add_generic.mjs";
-import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 export async function function_rename_append(f_name_before, suffix) {
-  let fn = function_name_combine;
+  let fn = string_combine;
   let v = await function_rename_suffix_add_generic(f_name_before, suffix, fn);
   return v;
 }
