0afb0ad341c960a3b1c2bf6b3d136372aa85dcc1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:53:10 -0500

    function_auto_path ..\love\public\src\cache_args_lookup.mjs

--- public/src/cache_args_lookup.mjs
+++ public/src/cache_args_lookup.mjs
@@ -6,6 +6,8 @@ export async function cache_args_lookup() {
   let file_name = file_name_json("data");
   let f_path = folder_user_storage_function_path(cache_args_lookup);
   let joined = path_join([f_path, file_name]);
+  if (false) {
+  }
   async function lambda(data) {}
   await file_json_transform(f_path2, lambda);
 }
