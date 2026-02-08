203aa3353afb85771140a0dd409a025268c79dbb
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:00:18 -0500

    rrl2 g_generate_upload_filter_generic single

--- public/src/g_generate_upload_generic.mjs
+++ public/src/g_generate_upload_generic.mjs
@@ -1,8 +1,8 @@
-import { g_generate_upload_filter_generic } from "../../../love/public/src/g_generate_upload_filter_generic.mjs";
+import { g_generate_upload_single } from "../../../love/public/src/g_generate_upload_single.mjs";
 import { folder_user_storage_function_each } from "../../../love/public/src/folder_user_storage_function_each.mjs";
 export async function g_generate_upload_generic(fn, path_get) {
   await folder_user_storage_function_each(fn, file_each);
   async function file_each(file) {
-    await g_generate_upload_filter_generic(fn, path_get, file);
+    await g_generate_upload_single(fn, path_get, file);
   }
 }
