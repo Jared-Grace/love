b6007d9b365bd56f8d6627abffa36e4166c5af19
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:06:17 -0500

    function_auto_path ..\love\public\src\text_base64_to.mjs

--- public/src/http_firebase_file_name.mjs
+++ public/src/http_firebase_file_name.mjs
@@ -1,5 +1,5 @@
-import { string_base64_to } from "../../../love/public/src/string_base64_to.mjs";
+import { text_base64_to } from "../../../love/public/src/text_base64_to.mjs";
 export function http_firebase_file_name(url) {
-  let safe = string_base64_to(url);
+  let safe = text_base64_to(url);
   return safe;
 }
