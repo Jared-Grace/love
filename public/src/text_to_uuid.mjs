e050ca553fd7b63deeb5e621bbfcbeabc4b1b183
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:59:52 -0500

    function_auto_path ..\love\public\src\text_to_uuid.mjs

--- public/src/text_to_uuid.mjs
+++ public/src/text_to_uuid.mjs
@@ -1,3 +1,4 @@
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { uuid } from "../../../love/public/src/uuid.mjs";
 import { object_property_exists_not_assert } from "../../../love/public/src/object_property_exists_not_assert.mjs";
 import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
@@ -12,5 +13,6 @@ export async function text_to_uuid(text) {
   async function lambda(data) {
     object_property_exists_not_assert(data, text);
     let u = await uuid();
+    object_property_set(object, property_name, value);
   }
 }
