e8db23a8e58306fe9d0e53f6c192a4cbc59815a1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:13:56 -0500

    function_auto_path ..\karate_code\public\src\app_karate_name_sign_up.mjs

--- public/src/text_replace.mjs
+++ public/src/text_replace.mjs
@@ -1,6 +1,6 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
 import { string_split } from "../../../love/public/src/string_split.mjs";
-export function string_replace(s, from, to) {
+export function text_replace(s, from, to) {
   let split = string_split(s, from);
   let replaced = list_join(split, to);
   return replaced;
