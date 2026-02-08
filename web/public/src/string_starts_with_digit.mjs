a84e74ffdb114fe2b7d0c04db4dcae7b17467e09
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:11 -0500

    function_auto_path ..\love\public\src\text_prefix_without.mjs

--- web/public/src/string_starts_with_digit.mjs
+++
@@ -1,7 +0,0 @@
-import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
-import { digits } from "../../../love/public/src/digits.mjs";
-export function string_starts_with_digit(item2) {
-  let d = digits();
-  let any = list_any_starts_with(item2, d);
-  return any;
-}
