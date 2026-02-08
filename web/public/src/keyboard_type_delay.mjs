0edb754ab8359afa8d7b982c927840486c9d0942
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:29 -0500

    function_auto_path ..\love\public\src\app_bible_home_generic.mjs

--- web/public/src/keyboard_type_delay.mjs
+++ web/public/src/keyboard_type_delay.mjs
@@ -1,9 +1,9 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 import { keyboard_typing_delay } from "../../../love/public/src/keyboard_typing_delay.mjs";
-import { string_list_to } from "../../../love/public/src/string_list_to.mjs";
+import { text_list_to } from "../../../love/public/src/text_list_to.mjs";
 export async function keyboard_type_delay(message, fn) {
-  let list = string_list_to(message);
+  let list = text_list_to(message);
   async function lambda(item) {
     let delay_ms = keyboard_typing_delay();
     await sleep(delay_ms);
