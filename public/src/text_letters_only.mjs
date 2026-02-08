c67bd0212a8ddeafaebb850a3d30ae2d19b71549
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:16 -0500

    function_auto_path ..\love\public\src\reply_messages_inner_transform.mjs

--- public/src/text_letters_only.mjs
+++ public/src/text_letters_only.mjs
@@ -1,5 +1,5 @@
 import { regex_letters_not } from "../../../love/public/src/regex_letters_not.mjs";
-export function string_letters_only(str) {
+export function text_letters_only(str) {
   let v = regex_letters_not();
   let letters = str.replace(v, "");
   return letters;
