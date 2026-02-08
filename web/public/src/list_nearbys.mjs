7e2723ca16b778775134a0d7e485f24d86cdd80b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:59:01 -0500

    rr list_items_nearby items get

--- web/public/src/list_nearbys.mjs
+++ web/public/src/list_nearbys.mjs
@@ -1,10 +1,10 @@
-import { list_items_nearby } from "../../../love/public/src/list_items_nearby.mjs";
+import { list_get_nearby } from "../../../love/public/src/list_get_nearby.mjs";
 import { positive_is_assert } from "../../../love/public/src/positive_is_assert.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
 export function list_nearbys(list, nearness) {
   positive_is_assert(nearness);
   function mapper(item, index) {
-    let nearby = list_items_nearby(index, nearness, list);
+    let nearby = list_get_nearby(index, nearness, list);
     let m = {
       nearby,
       item,
