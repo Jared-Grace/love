f8a08e8d85304a6ebe5c283f63f70a2281951224
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:57:33 -0500

    function_auto_path ..\love\public\src\text_to_uuid.mjs

--- public/src/text_to_uuid.mjs
+++ public/src/text_to_uuid.mjs
@@ -2,9 +2,9 @@ import { file_json_transform_initialize } from "../../../love/public/src/file_js
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
-export async function cache_args_lookup(text) {
+export async function text_to_uuid(text) {
   let file_name = file_name_json("data");
-  let f_path = folder_user_storage_function_path(cache_args_lookup);
+  let f_path = folder_user_storage_function_path(text_to_uuid);
   let joined = path_join([f_path, file_name]);
   await file_json_transform_initialize(joined, {}, lambda);
   async function lambda(data) {}
