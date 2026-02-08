717a5892102404b24dac4e60c490025aa9f405cb
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:51:14 -0500

    function_auto_path ..\love\public\src\list_nearby.mjs

--- public/src/list_nearby.mjs
+++ public/src/list_nearby.mjs
@@ -1,17 +1,14 @@
+import { list_index_is_curry } from "../../../love/public/src/list_index_is_curry.mjs";
 import { positive_is_assert } from "../../../love/public/src/positive_is_assert.mjs";
 import { list_indices_to_items } from "../../../love/public/src/list_indices_to_items.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { range_from } from "../../../love/public/src/range_from.mjs";
 export function list_nearby(list, nearness) {
   positive_is_assert(nearness);
   function mapper(item, index) {
     let r = range_from(index - nearness, index + nearness);
-    let filter = function lambda(index) {
-      let ii = list_index_is(list, index);
-      return ii;
-    };
+    let filter = list_index_is_curry(list);
     let filtered = list_filter(r, filter);
     let nearby = list_indices_to_items(list, filtered);
     let m = {
