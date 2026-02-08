ff9167b12f254a02ac74b6fa00f77f3140ec3a65
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:16:01 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_with_replace.mjs

--- web/public/src/function_multiple_rename_if_starts_with_replace.mjs
+++ web/public/src/function_multiple_rename_if_starts_with_replace.mjs
@@ -1,18 +1,16 @@
-import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
-import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
+import { function_multiple_rename_generic_starts_with } from "../../../love/public/src/function_multiple_rename_generic_starts_with.mjs";
 import { string_replace } from "../../../love/public/src/string_replace.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export async function function_multiple_rename_if_starts_with_replace(
-  f_name_prefix_before,
+  f_name_prefix,
   from,
   to,
 ) {
   assert_arguments(arguments, 3);
-  await function_multiple_rename_generic(filter, name_change);
-  function filter(f_names) {
-    let filtered2 = list_filter_starts_with(f_names, f_name_prefix_before);
-    return filtered2;
-  }
+  await function_multiple_rename_generic_starts_with(
+    name_change,
+    f_name_prefix,
+  );
   function name_change(f_name_before) {
     let f_name_after = string_replace(f_name_before, from, to);
     return f_name_after;
