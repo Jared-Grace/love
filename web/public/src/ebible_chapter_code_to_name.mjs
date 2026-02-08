bfdb246e4c72c5e5880c3f3ad7d7dc9c9099a000
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:20:15 -0500

    function_auto_path ..\love\public\src\marker_first.mjs

--- web/public/src/ebible_chapter_code_to_name.mjs
+++ web/public/src/ebible_chapter_code_to_name.mjs
@@ -1,9 +1,9 @@
 import { ebible_chapter_code_to_name_code } from "../../../love/public/src/ebible_chapter_code_to_name_code.mjs";
-import { string_to } from "../../../love/public/src/string_to.mjs";
+import { text_to } from "../../../love/public/src/text_to.mjs";
 import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
 export function ebible_chapter_code_to_name(chapter_code) {
   let s = ebible_chapter_code_to_name_code(chapter_code);
   let i = integer_to_try(s);
-  let chapter_name = string_to(i);
+  let chapter_name = text_to(i);
   return chapter_name;
 }
