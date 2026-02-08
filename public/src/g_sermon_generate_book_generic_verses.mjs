d6cd3c4d122a48d339b98629c6cb3cd0b5df7354
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 09:03:06 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_verses.mjs

--- public/src/g_sermon_generate_book_generic_verses.mjs
+++ public/src/g_sermon_generate_book_generic_verses.mjs
@@ -1,8 +1,6 @@
+import { ebible_chapters_codes_to_verses } from "../../../love/public/src/ebible_chapters_codes_to_verses.mjs";
 import { ebible_chapters_codes_or_specified } from "../../../love/public/src/ebible_chapters_codes_or_specified.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function g_sermon_generate_book_generic_verses(
   book_code,
   chapter_code_specified,
@@ -14,19 +12,9 @@ export async function g_sermon_generate_book_generic_verses(
       book_code,
       chapter_code_specified,
     );
-    async function map_chapter_code(chapter_code) {
-      let verses = await ebible_verses(bible_folder, chapter_code);
-      function each_verse(v) {
-        object_merge(v, {
-          chapter_code,
-        });
-      }
-      each(verses, each_verse);
-      return verses;
-    }
-    let verses_book = await list_map_unordered_async(
+    let verses_book = await ebible_chapters_codes_to_verses(
+      bible_folder,
       chapters_codes,
-      map_chapter_code,
     );
     let v5 = {
       verses_book,
