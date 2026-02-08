d236c3620236b3a7682d82dc7e07c8b627b7dba0
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:05 -0500

    function_auto_path ..\love\public\src\function_run_output_file_temp_args.mjs

--- web/public/src/function_name_parts_remove_comma.mjs
+++ web/public/src/function_name_parts_remove_comma.mjs
@@ -1,7 +1,7 @@
 import { function_name_parts_remove } from "../../../love/public/src/function_name_parts_remove.mjs";
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 export function function_name_parts_remove_comma(f_name_old, deleteds_comma) {
-  let split = string_split_comma(deleteds_comma);
+  let split = text_split_comma(deleteds_comma);
   let f_name_new = function_name_parts_remove(f_name_old, split);
   return f_name_new;
 }
--- web/public/src/list_map_split_comma.mjs
+++ web/public/src/list_map_split_comma.mjs
@@ -1,6 +1,6 @@
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_split_comma(mapped2) {
-  let mapped = list_map(mapped2, string_split_comma);
+  let mapped = list_map(mapped2, text_split_comma);
   return mapped;
 }
