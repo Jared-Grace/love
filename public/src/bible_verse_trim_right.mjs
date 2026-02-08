d263a7da16eafef72c9ab72f684b9bdbbd374097
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:18 -0500

    function_auto_path ..\love\public\src\text_split_empty.mjs

--- public/src/bible_verse_trim_right.mjs
+++ public/src/bible_verse_trim_right.mjs
@@ -1,9 +1,9 @@
 import { string_trim_right } from "../../../love/public/src/string_trim_right.mjs";
 import { text_ends_with_any } from "../../../love/public/src/text_ends_with_any.mjs";
-import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
+import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 export function bible_verse_trim_right(text) {
   let suffixes = '"”) ’';
-  let split = string_split_empty(suffixes);
+  let split = text_split_empty(suffixes);
   function lambda3(s2) {
     let ew = text_ends_with_any(s2, split);
     return ew;
