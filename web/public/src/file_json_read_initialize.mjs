de885b4ec2051021cdd6abea332ce481971478ab
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:10:19 -0500

    function_auto_path ..\love\public\src\file_json_read_initialize.mjs

--- web/public/src/file_json_read_initialize.mjs
+++ web/public/src/file_json_read_initialize.mjs
@@ -1,7 +1,7 @@
+import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { file_write_json_if_exists_not } from "../../../love/public/src/file_write_json_if_exists_not.mjs";
-import { file_json_read } from "../../../love/public/src/file_json_read.mjs";
 export async function file_json_read_initialize(initial, joined) {
   await file_write_json_if_exists_not(f_path, initial);
-  let data = await file_json_read(joined);
+  let data = await file_read_json(joined);
   return data;
 }
