45d7734b3eae5a562fae894e78335ffa0d256743
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:42 -0500

    function_auto_path ..\love\public\src\string_trim_right.mjs

--- web/public/src/list_sort_string_size.mjs
+++ web/public/src/list_sort_string_size.mjs
@@ -1,5 +1,5 @@
-import { string_size } from "../../../love/public/src/string_size.mjs";
+import { text_size } from "../../../love/public/src/text_size.mjs";
 import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
 export function list_sort_string_size(list) {
-  list_sort_number_mapper(list, string_size);
+  list_sort_number_mapper(list, text_size);
 }
--- web/public/src/string_size_less_1.mjs
+++ web/public/src/string_size_less_1.mjs
@@ -1,7 +1,7 @@
 import { greater_than_or_equal_assert } from "../../../love/public/src/greater_than_or_equal_assert.mjs";
-import { string_size } from "../../../love/public/src/string_size.mjs";
+import { text_size } from "../../../love/public/src/text_size.mjs";
 export function string_size_less_1(s) {
-  let sz = string_size(s);
+  let sz = text_size(s);
   greater_than_or_equal_assert(sz, 1);
   const sz1 = sz - 1;
   return sz1;
