88010ca6e56bce3b1018c1a6fac81446fccacbd7
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:14:32 -0500

    frd function_rename_parts_end_swap end

--- public/src/function_rename_parts_end_swap_multiple.mjs
+++ public/src/function_rename_parts_end_swap_multiple.mjs
@@ -1,10 +1,10 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
-import { function_rename_parts_end_swap } from "../../../love/public/src/function_rename_parts_end_swap.mjs";
+import { function_rename_parts_swap } from "../../../love/public/src/function_rename_parts_swap.mjs";
 export async function function_rename_parts_end_swap_multiple(f_name_befores) {
   let split = text_split_comma(f_name_befores);
   async function lambda(f_name_before) {
-    await function_rename_parts_end_swap(f_name_before);
+    await function_rename_parts_swap(f_name_before);
   }
   await each_async(split, lambda);
 }
