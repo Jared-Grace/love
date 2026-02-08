0f625fbe22b6aed0906305ce6713f9e7c89fef3c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:55:24 -0500

    function_auto_path ..\love\public\src\cache_args_lookup.mjs

--- public/src/cache_args_lookup.mjs
+++ public/src/cache_args_lookup.mjs
@@ -1,7 +1,4 @@
-import { file_write_json } from "../../../love/public/src/file_write_json.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { file_exists } from "../../../love/public/src/file_exists.mjs";
-import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
+import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
@@ -9,11 +6,7 @@ export async function cache_args_lookup() {
   let file_name = file_name_json("data");
   let f_path = folder_user_storage_function_path(cache_args_lookup);
   let joined = path_join([f_path, file_name]);
-  let exists = await file_exists(joined);
   let initial = {};
-  if (not(exists)) {
-    await file_write_json(joined, initial);
-  }
-  await file_json_transform(joined, lambda);
+  await file_json_transform_initialize(exists, joined, initial, lambda);
   async function lambda(data) {}
 }
