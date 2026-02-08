3d2c4181358aac17efa5f7f5a6dd6491e732e61f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:07:49 -0500

    function_auto_path ..\love\public\src\list_filter_empty_not_is.mjs

--- public/src/string_empty_not_is_assert.mjs
+++
@@ -1,6 +0,0 @@
-import { assert } from "../../../love/public/src/assert.mjs";
-import { text_empty_not_is } from "../../../love/public/src/text_empty_not_is.mjs";
-export function string_empty_not_is_assert(s) {
-  let ne = text_empty_not_is(s);
-  assert(ne);
-}
