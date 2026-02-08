cacda088c20a17631d8ee295ddce31f33eb6483b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:38:18 -0500

    function_auto_path ..\love\public\src\function_name_to_parts_last_2.mjs

---
+++ web/public/src/function_name_to_parts_last_generic.mjs
@@ -0,0 +1,11 @@
+import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
+import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
+import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
+export function function_name_to_parts_last_generic(count, f_name_before) {
+  function lambda(parts) {
+    let result = list_slice_end(parts, count);
+    list_replace_all(parts, result);
+  }
+  let f_name_after = function_name_parts_transform(f_name_before, lambda);
+  return f_name_after;
+}
