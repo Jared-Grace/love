0348590b214f06211a17e476c00db53314df5c28
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:47 -0500

    function_auto_path ..\love\public\src\app_a_functions_generic_f_names.mjs

--- web/public/src/text_padded_is.mjs
+++ web/public/src/text_padded_is.mjs
@@ -1,6 +1,6 @@
 import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
-export function string_padded_is(s, padding) {
+export function text_padded_is(s, padding) {
   let sw = string_starts_with(s, padding);
   let ew = text_ends_with(s, padding);
   let p = sw || ew;
