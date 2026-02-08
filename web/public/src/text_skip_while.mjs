bd3ec815ef4b05d882038d3d612f14bdeeb73190
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:15:06 -0500

    function_auto_path ..\love\public\src\string_skip_while.mjs

---
+++ web/public/src/text_skip_while.mjs
@@ -0,0 +1,12 @@
+import { text_skip } from "../../../love/public/src/text_skip.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+export function text_skip_while(lambda$s, s) {
+  while (true) {
+    let any = lambda$s(s);
+    if (not(any)) {
+      break;
+    }
+    s = text_skip(s, 1);
+  }
+  return s;
+}
