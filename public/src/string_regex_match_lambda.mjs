84fbf84b4ab3146b4dce050e32872fbc30898d34
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:13:49 -0500

    function_auto_path ..\love\public\src\text_regex_match_lambda.mjs

--- public/src/string_regex_match_lambda.mjs
+++
@@ -1,8 +0,0 @@
-import { text_regex_match } from "../../../love/public/src/text_regex_match.mjs";
-export function string_regex_match_lambda(regex) {
-  let l = function string_regex_match_lambda_inner(input) {
-    let m = text_regex_match(input, regex);
-    return m;
-  };
-  return l;
-}
