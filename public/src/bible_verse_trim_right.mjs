d0286efa8af3ace90a686c87d02fb9f3e429d60c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:15 -0500

    function_auto_path ..\love\public\src\bible_verse_trim_right.mjs

--- public/src/bible_verse_trim_right.mjs
+++ public/src/bible_verse_trim_right.mjs
@@ -1,11 +1,11 @@
 import { string_trim_right } from "../../../love/public/src/string_trim_right.mjs";
-import { string_ends_with_any } from "../../../love/public/src/string_ends_with_any.mjs";
+import { text_ends_with_any } from "../../../love/public/src/text_ends_with_any.mjs";
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 export function bible_verse_trim_right(text) {
   let suffixes = '"”) ’';
   let split = string_split_empty(suffixes);
   function lambda3(s2) {
-    let ew = string_ends_with_any(s2, split);
+    let ew = text_ends_with_any(s2, split);
     return ew;
   }
   let trimmed = string_trim_right(lambda3, text);
--- public/src/text_ends_with_not.mjs
+++ public/src/text_ends_with_not.mjs
@@ -1,6 +1,6 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
-export function string_ends_with_not(item, suffix) {
+export function text_ends_with_not(item, suffix) {
   let a = text_ends_with(item, suffix);
   let ewn = not(a);
   return ewn;
