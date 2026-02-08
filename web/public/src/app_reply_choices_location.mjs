c78867e9562b2685f3693ae7a4aceb40e258cb43
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:20:03 -0500

    function_auto_path ..\love\public\src\app_reply_choices_location.mjs

--- web/public/src/app_reply_choices_location.mjs
+++ web/public/src/app_reply_choices_location.mjs
@@ -1,11 +1,11 @@
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
-import { string_the_servant_of_god_is } from "../../../love/public/src/string_the_servant_of_god_is.mjs";
+import { text_the_servant_of_god_is } from "../../../love/public/src/text_the_servant_of_god_is.mjs";
 import { emoji_globe_americas } from "../../../love/public/src/emoji_globe_americas.mjs";
 export function app_reply_choices_location() {
   let v2 =
     emoji_globe_americas() +
     " " +
-    string_the_servant_of_god_is() +
+    text_the_servant_of_god_is() +
     " from 🇺🇸 the United States of America, state of Florida, city of Jacksonville. " +
     emoji_pray();
   return v2;
