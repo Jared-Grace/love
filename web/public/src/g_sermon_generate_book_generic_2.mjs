36cca41344188e0716e4e8d38fdeb189a993edf1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:55:15 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_2.mjs

--- web/public/src/g_sermon_generate_book_generic_2.mjs
+++ web/public/src/g_sermon_generate_book_generic_2.mjs
@@ -1,3 +1,4 @@
+import { bible_interlinear_chapters } from "../../../love/public/src/bible_interlinear_chapters.mjs";
 import { ebible_chapters_codes_or_specified } from "../../../love/public/src/ebible_chapters_codes_or_specified.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export async function g_sermon_generate_book_generic_2(
@@ -19,4 +20,5 @@ export async function g_sermon_generate_book_generic_2(
     chapters_codes,
     bible_folders,
   );
+  let chapters_interlinear = await bible_interlinear_chapters();
 }
