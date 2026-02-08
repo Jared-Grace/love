1de74df2650cbeabf83e75b8fb7fea21e5b37d6f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:56 -0500

    function_auto_path ..\love\public\src\text_padded_not_is_assert.mjs

--- public/src/function_name_separator_padded_not_is_assert.mjs
+++ public/src/function_name_separator_padded_not_is_assert.mjs
@@ -1,6 +1,6 @@
-import { string_padded_not_is_assert } from "../../../love/public/src/string_padded_not_is_assert.mjs";
+import { text_padded_not_is_assert } from "../../../love/public/src/text_padded_not_is_assert.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 export function function_name_separator_padded_not_is_assert(suffix) {
   let padding = function_name_separator();
-  string_padded_not_is_assert(suffix, padding);
+  text_padded_not_is_assert(suffix, padding);
 }
