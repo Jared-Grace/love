fc893ff8afd4db5bb271d16fc26fa73ef69b680c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:39:48 -0500

    function_auto_path ..\love\public\src\function_copy_replace_first.mjs

--- public/src/function_copy_replace_first.mjs
+++ public/src/function_copy_replace_first.mjs
@@ -2,7 +2,7 @@ import { function_name_unalias_only } from "../../../love/public/src/function_na
 import { function_name_to_part_first } from "../../../love/public/src/function_name_to_part_first.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_copy_replace } from "../../../love/public/src/function_copy_replace.mjs";
-export async function first(f_name_old, to) {
+export async function function_copy_replace_first(f_name_old, to) {
   assert_arguments(arguments, 2);
   let unaliased = await function_name_unalias_only(f_name_old);
   let from = function_name_to_part_first(unaliased);
