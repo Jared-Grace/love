c317d6ef3f4dd5e191318499d2d6d7577f9b1e4d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 16:55:42 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic.mjs

--- web/public/src/g_sermon_generate_book_generic.mjs
+++ web/public/src/g_sermon_generate_book_generic.mjs
@@ -174,6 +174,7 @@ export async function g_sermon_generate_book_generic(
         }
         let passages_folders = list_map(groups, each_group);
         list_add_pair(r, passages_folders);
+        let mapped = list_map(list, function lambda(item2) {});
         return r;
         let user_prompt = list_join(passages_folders, " :: ");
       }
