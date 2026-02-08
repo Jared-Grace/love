ebf51c617f17d14b3777ec94d224787f7d1ebfec
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:19:04 -0500

    function_auto_path ..\love\public\src\text_symbols_unique.mjs

--- public/src/string_symbols_unique_multiple.mjs
+++ public/src/string_symbols_unique_multiple.mjs
@@ -1,7 +1,7 @@
-import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
+import { text_symbols_unique } from "../../../love/public/src/text_symbols_unique.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 export function string_symbols_unique_multiple(mapped) {
   let joined = list_join_empty(mapped);
-  let unique = string_symbols_unique(joined);
+  let unique = text_symbols_unique(joined);
   return unique;
 }
