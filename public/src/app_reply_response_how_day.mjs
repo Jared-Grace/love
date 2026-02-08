8a14baecfb71889cfba0881db6018b6400cceea3
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:32 -0500

    function_auto_path ..\love\public\src\text_first_lower_to.mjs

--- public/src/app_reply_response_how_day.mjs
+++ public/src/app_reply_response_how_day.mjs
@@ -1,12 +1,12 @@
 import { app_reply_response_how_r_u_skip_before } from "../../../love/public/src/app_reply_response_how_r_u_skip_before.mjs";
-import { string_first_lower_to } from "../../../love/public/src/string_first_lower_to.mjs";
+import { text_first_lower_to } from "../../../love/public/src/text_first_lower_to.mjs";
 import { emoji_ok } from "../../../love/public/src/emoji_ok.mjs";
 export function app_reply_response_how_day() {
   let s = app_reply_response_how_r_u_skip_before();
   let v2 =
     emoji_ok() +
     " The day of the " +
-    string_first_lower_to(s) +
+    text_first_lower_to(s) +
     "today is one day closer to the return of Jesus than yesterday!";
   return v2;
 }
