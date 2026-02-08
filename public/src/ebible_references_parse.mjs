433774a1a325899029ed44444ca75f432db3d943
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:17:08 -0500

    function_auto_path ..\love\public\src\bible_verses_encouragement_generate.mjs

--- public/src/ebible_references_parse.mjs
+++ public/src/ebible_references_parse.mjs
@@ -1,10 +1,10 @@
 import { ebible_references_parse_lines } from "../../../love/public/src/ebible_references_parse_lines.mjs";
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
-import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
+import { text_split_newline } from "../../../love/public/src/text_split_newline.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 export async function ebible_references_parse(bible_folders, file_path) {
   let contents = await file_read(file_path);
-  let split = string_split_newline(contents);
+  let split = text_split_newline(contents);
   let list = await ebible_references_parse_lines(bible_folders, split);
   let joined = list_join_newline(list);
   return list;
