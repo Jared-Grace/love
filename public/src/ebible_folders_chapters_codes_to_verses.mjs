7e1a4a705e19684f2c7cf33d58dec839399b83c6
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:53:22 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_verses.mjs

---
+++ public/src/ebible_folders_chapters_codes_to_verses.mjs
@@ -0,0 +1,16 @@
+import { ebible_chapters_codes_to_verses } from "../../../love/public/src/ebible_chapters_codes_to_verses.mjs";
+import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
+export async function ebible_folders_chapters_codes_to_verses(
+  chapters_codes,
+  bible_folders,
+) {
+  async function map_folder(bible_folder) {
+    let verses_book = await ebible_chapters_codes_to_verses(
+      bible_folder,
+      chapters_codes,
+    );
+    return verses_book;
+  }
+  let r = await list_map_unordered_async(bible_folders, map_folder);
+  return r;
+}
