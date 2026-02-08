1dde44eea9b471dea654cb0414dff147afaa2e25
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:21:25 -0500

    ri string text

--- public/src/js_code_wrap_parenthesis.mjs
+++ public/src/js_code_wrap_parenthesis.mjs
@@ -1,6 +1,6 @@
-import { string_wrap_parenthesis } from "../../../love/public/src/string_wrap_parenthesis.mjs";
+import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
 export function js_code_wrap_parenthesis(inside) {
   const inside_larger = " " + inside + " ";
-  let v = string_wrap_parenthesis(inside_larger);
+  let v = text_wrap_parenthesis(inside_larger);
   return v;
 }
