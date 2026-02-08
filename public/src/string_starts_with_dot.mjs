5b12742f2f838073a9b66134d137ba444bc1fd63
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:10 -0500

    function_auto_path ..\love\public\src\text_padded_is.mjs

--- public/src/string_starts_with_dot.mjs
+++ public/src/string_starts_with_dot.mjs
@@ -1,5 +1,5 @@
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
+import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 export function string_starts_with_dot(s) {
-  let sw2 = string_starts_with(s, ".");
+  let sw2 = text_starts_with(s, ".");
   return sw2;
 }
