01070816bb199b9c3d4bc482b05ca4e1bc8d25d3
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:03:54 -0500

    function_auto_path ..\love\public\src\text_to_uuid_set.mjs

---
+++ public/src/text_to_uuid_path.mjs
@@ -0,0 +1,10 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
+import { text_to_uuid_set } from "../../../love/public/src/text_to_uuid_set.mjs";
+import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
+import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
+export function text_to_uuid_path() {
+  let file_name = file_name_json("data");
+  let f_path = folder_user_storage_function_path(text_to_uuid_set);
+  let joined = path_join([f_path, file_name]);
+  return joined;
+}
