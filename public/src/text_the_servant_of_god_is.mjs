bf8e46e4067b9c248f15fbc38687ecb0708068b3
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:19:53 -0500

    function_auto_path ..\love\public\src\string_the_servant_of_god_is.mjs

--- public/src/text_the_servant_of_god_is.mjs
+++ public/src/text_the_servant_of_god_is.mjs
@@ -1,5 +1,5 @@
 import { text_the_servant_of_god } from "../../../love/public/src/text_the_servant_of_god.mjs";
-export function string_the_servant_of_god_is() {
+export function text_the_servant_of_god_is() {
   let v = text_the_servant_of_god() + " is";
   return v;
 }
