eaf44e56633e54f771888774a97f4d95686c118d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:49 -0500

    function_auto_path ..\love\public\src\function_replace_rename_multiple.mjs

--- public/src/text_includes_or_starts_with.mjs
+++ public/src/text_includes_or_starts_with.mjs
@@ -1,6 +1,6 @@
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
-export function string_includes_or_starts_with(s, item) {
+export function text_includes_or_starts_with(s, item) {
   let i = text_includes(s, item);
   let sw = string_starts_with(s, item);
   let v = sw || i;
