a981293bf2313768fb1403fda7300ad92f64b544
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:17:40 -0500

    function_auto_path ..\love\public\src\functions_firebase_to_root.mjs

--- public/src/text_split_space.mjs
+++ public/src/text_split_space.mjs
@@ -1,5 +1,5 @@
 import { text_split } from "../../../love/public/src/text_split.mjs";
-export function string_split_space(s) {
+export function text_split_space(s) {
   let split = text_split(s, " ");
   return split;
 }
