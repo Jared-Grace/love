ab8f1ef2a050e21df8790f4fbcf2c7424f35b21c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:01:11 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_chapter_upload.mjs

--- public/src/app_ceb_bible_gloss_generate_chapter_upload.mjs
+++ public/src/app_ceb_bible_gloss_generate_chapter_upload.mjs
@@ -2,6 +2,5 @@ import { app_ceb_bible_gloss_generate_chapter } from "../../../love/public/src/a
 export async function app_ceb_bible_gloss_generate_chapter_upload(
   chapter_code_specified,
 ) {
-  let v = await app_ceb_bible_gloss_generate_chapter(chapter_code_specified);
-  return v;
+  await app_ceb_bible_gloss_generate_chapter(chapter_code_specified);
 }
