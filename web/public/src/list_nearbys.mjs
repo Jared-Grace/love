c4998b1001ee8c65843d62a47eef1ff666f0b17d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:58:37 -0500

    function_auto_path ..\love\public\src\list_nearbys.mjs

--- web/public/src/list_nearbys.mjs
+++ web/public/src/list_nearbys.mjs
@@ -1,12 +1,10 @@
-import { list_indices_to_items_try } from "../../../love/public/src/list_indices_to_items_try.mjs";
+import { list_items_nearby } from "../../../love/public/src/list_items_nearby.mjs";
 import { positive_is_assert } from "../../../love/public/src/positive_is_assert.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
-import { range_from } from "../../../love/public/src/range_from.mjs";
 export function list_nearbys(list, nearness) {
   positive_is_assert(nearness);
   function mapper(item, index) {
-    let indices = range_from(index - nearness, index + nearness);
-    let nearby = list_indices_to_items_try(list, indices);
+    let nearby = list_items_nearby(index, nearness, list);
     let m = {
       nearby,
       item,
