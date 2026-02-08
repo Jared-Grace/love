ec6b392cf580d158ad442c45026707740b90dd9a
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 19:20:21 -0500

    function_auto_path ..\love\public\src\app_a_if_statement.mjs

--- web/public/src/app_a_if_statement.mjs
+++ web/public/src/app_a_if_statement.mjs
@@ -1,8 +1,5 @@
+import { app_a_call_above } from "../../../love/public/src/app_a_call_above.mjs";
 import { app_a_cut } from "../../../love/public/src/app_a_cut.mjs";
-import { app_a_functions_overlay } from "../../../love/public/src/app_a_functions_overlay.mjs";
-import { app_a_node_index } from "../../../love/public/src/app_a_node_index.mjs";
-import { app_a_function_on_change } from "../../../love/public/src/app_a_function_on_change.mjs";
-import { js_call_new_insert } from "../../../love/public/src/js_call_new_insert.mjs";
 import { app_a_overlay_choices } from "../../../love/public/src/app_a_overlay_choices.mjs";
 import { js_keyword_else } from "../../../love/public/src/js_keyword_else.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
@@ -20,34 +17,8 @@ export function app_a_if_statement(a) {
   let keyword = object_property_get(v4, "keyword");
   async function lambda(o) {
     let v3 = app_a_cut(o, a);
-    let choices = [
-      {
-        shortcut: "a",
-        text: "Add above",
-        fn: async function lambda2() {
-          let overlay_close = object_property_get(o, "overlay_close");
-          overlay_close();
-          let v2 = await app_a_functions_overlay(a, on_select);
-          let overlay_result = object_property_get(v2, "overlay_result");
-          async function on_select(f_name_call) {
-            let v = app_a_node_index(a);
-            let stack = object_property_get(v, "stack");
-            let index = object_property_get(v, "index");
-            let list = object_property_get(v, "list");
-            let ast = object_property_get(a, "ast");
-            let parsed = await js_call_new_insert(
-              f_name_call,
-              ast,
-              list,
-              index,
-              stack,
-            );
-            await app_a_function_on_change(overlay_result, a);
-          }
-        },
-      },
-      v3,
-    ];
+    let v5 = app_a_call_above(o, a);
+    let choices = [v5, v3];
     return choices;
   }
   app_a_overlay_choices(a, keyword, lambda);
