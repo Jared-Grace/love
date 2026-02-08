71629d807f3a0cf5e2490ea6debe7dc2e1a138ed
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:51:48 -0500

    function_auto_path ..\love\public\src\cache_args_lookup.mjs

--- public/src/cache_args_lookup.mjs
+++ public/src/cache_args_lookup.mjs
@@ -1,8 +1,10 @@
+import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
 import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
-export function cache_args_lookup() {
+export async function cache_args_lookup() {
   let file_name = file_name_json("data");
   let f_path = folder_user_storage_function_path(cache_args_lookup);
   let joined = path_join([f_path, file_name]);
+  await file_json_transform(f_path2, async function lambda(data) {});
 }
