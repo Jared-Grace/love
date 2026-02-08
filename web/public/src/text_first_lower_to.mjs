fa05e14b2e5f3d8cd660f110b4f95f2b6c42313a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:39 -0500

    function_auto_path ..\love\public\src\text_first_transform.mjs

--- web/public/src/text_first_lower_to.mjs
+++ web/public/src/text_first_lower_to.mjs
@@ -1,7 +1,7 @@
-import { string_first_transform } from "../../../love/public/src/string_first_transform.mjs";
+import { text_first_transform } from "../../../love/public/src/text_first_transform.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
 export function text_first_lower_to(s) {
   let transform = string_lower_to;
-  let v = string_first_transform(s, transform);
+  let v = text_first_transform(s, transform);
   return v;
 }
