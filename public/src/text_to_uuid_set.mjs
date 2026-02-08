7160cc9845f72a2a311deef4d09c4b41a198cd42
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:08:26 -0500

    function_auto_path ..\love\public\src\text_to_uuid_set.mjs

--- public/src/text_to_uuid_set.mjs
+++ public/src/text_to_uuid_set.mjs
@@ -1,3 +1,4 @@
+import { text_to_uuid_initial } from "../../../love/public/src/text_to_uuid_initial.mjs";
 import { text_to_uuid_get_initialize } from "../../../love/public/src/text_to_uuid_get_initialize.mjs";
 import { text_to_uuid_path } from "../../../love/public/src/text_to_uuid_path.mjs";
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
@@ -7,7 +8,8 @@ import { object_property_exists_not_assert } from "../../../love/public/src/obje
 import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
 export async function text_to_uuid_set(text) {
   let joined = text_to_uuid_path();
-  await file_json_transform_initialize(joined, {}, lambda);
+  let initial = text_to_uuid_initial();
+  await file_json_transform_initialize(joined, initial, lambda);
   async function lambda(data) {
     let set = object_property_initialize(data, "set", {});
     object_property_exists_not_assert(set, text);
