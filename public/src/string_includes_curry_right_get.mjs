37c22e26b80e7566eb0d1854efdb1e41a0fd1959
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:23 -0500

    function_auto_path ..\love\public\src\string_includes_not.mjs

--- public/src/string_includes_curry_right_get.mjs
+++
@@ -1,8 +0,0 @@
-import { text_includes } from "../../../love/public/src/text_includes.mjs";
-export function string_includes_curry_right_get(input) {
-  function string_includes_curry_right(part) {
-    let i = text_includes(input, part);
-    return i;
-  }
-  return string_includes_curry_right;
-}
