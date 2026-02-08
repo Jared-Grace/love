8587dfc8a1b3d2d49450bb1809f0e8fcb4cd46cc
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:37:57 -0500

    function_auto_path ..\love\public\src\function_name_to_parts_last_2.mjs

--- public/src/function_name_to_parts_last_2.mjs
+++ public/src/function_name_to_parts_last_2.mjs
@@ -2,6 +2,7 @@ import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs"
 import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 export function function_name_to_parts_last_2(f_name_before) {
+  let count = 2;
   function lambda(parts) {
     let result = list_slice_end(parts, 2);
     list_replace_all(parts, result);
