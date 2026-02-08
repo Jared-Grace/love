37663298acd39cf690eab7ad8f2c9c5a970e6b7a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:59:10 -0500

    pf list_get_nearby list

--- public/src/list_get_nearby.mjs
+++ public/src/list_get_nearby.mjs
@@ -1,6 +1,6 @@
 import { list_indices_to_items_try } from "../../../love/public/src/list_indices_to_items_try.mjs";
 import { range_from } from "../../../love/public/src/range_from.mjs";
-export function list_get_nearby(index, nearness, list) {
+export function list_get_nearby(list, index, nearness) {
   let indices = range_from(index - nearness, index + nearness);
   let nearby = list_indices_to_items_try(list, indices);
   return nearby;
