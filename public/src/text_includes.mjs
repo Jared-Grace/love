bd2ff568f00e1745ef9f1f3c51a6e6da71b46b17
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:48 -0500

    function_auto_path ..\love\public\src\text_is_assert_message.mjs

--- public/src/text_includes.mjs
+++ public/src/text_includes.mjs
@@ -1,11 +1,11 @@
-import { string_is_assert_json } from "../../../love/public/src/string_is_assert_json.mjs";
+import { text_is_assert_json } from "../../../love/public/src/text_is_assert_json.mjs";
 export function text_includes(input, part) {
   const o = {
     input,
     part,
   };
-  string_is_assert_json(input, o);
-  string_is_assert_json(part, o);
+  text_is_assert_json(input, o);
+  text_is_assert_json(part, o);
   let i = input.includes(part);
   return i;
 }
--- public/src/text_is_assert_json.mjs
+++ public/src/text_is_assert_json.mjs
@@ -1,6 +1,6 @@
-import { string_is_assert_message } from "../../../love/public/src/string_is_assert_message.mjs";
+import { text_is_assert_message } from "../../../love/public/src/text_is_assert_message.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export function text_is_assert_json(value, o) {
   let message = json_to(o);
-  string_is_assert_message(value, message);
+  text_is_assert_message(value, message);
 }
