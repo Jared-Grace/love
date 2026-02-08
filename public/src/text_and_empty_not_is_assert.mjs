aab6952b375cc6c6b3ba54bea050fd91660f9c79
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:05:58 -0500

    function_auto_path ..\karate_code\public\src\app_karate_email_verify.mjs

--- public/src/text_and_empty_not_is_assert.mjs
+++ public/src/text_and_empty_not_is_assert.mjs
@@ -1,6 +1,6 @@
 import { string_empty_not_is_assert } from "../../../love/public/src/string_empty_not_is_assert.mjs";
 import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
-export function string_and_empty_not_is_assert(name) {
+export function text_and_empty_not_is_assert(name) {
   string_is_assert(name);
   string_empty_not_is_assert(name);
 }
