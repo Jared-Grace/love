012415c847c38aa8befea88e46dd3a3ebc4fd785
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:17:34 -0500

    function_auto_path ..\love\public\src\text_split_slash_forward.mjs

--- public/src/text_split_slash_forward.mjs
+++ public/src/text_split_slash_forward.mjs
@@ -1,6 +1,6 @@
 import { text_slash_forward } from "../../../love/public/src/text_slash_forward.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
-export function string_split_slash_forward(f_path) {
+export function text_split_slash_forward(f_path) {
   let separator = text_slash_forward();
   let split2 = text_split(f_path, separator);
   return split2;
