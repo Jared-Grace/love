e8809f419cd72cea872769b4be1cf743f0f94a04
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:54:58 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_2.mjs

--- public/src/g_sermon_generate_book_generic_2.mjs
+++ public/src/g_sermon_generate_book_generic_2.mjs
@@ -1,4 +1,6 @@
-export function g_sermon_generate_book_generic_2(
+import { ebible_chapters_codes_or_specified } from "../../../love/public/src/ebible_chapters_codes_or_specified.mjs";
+import { list_first } from "../../../love/public/src/list_first.mjs";
+export async function g_sermon_generate_book_generic_2(
   bible_folders,
   book_code,
   fn,
@@ -6,4 +8,15 @@ export function g_sermon_generate_book_generic_2(
   prompt_system,
   property_name,
   chapter_code_specified,
-) {}
+) {
+  let bible_folder_first = list_first(bible_folders);
+  let chapters_codes = await ebible_chapters_codes_or_specified(
+    bible_folder_first,
+    book_code,
+    chapter_code_specified,
+  );
+  let verses_book_folders = await g_sermon_generate_book_generic_verses(
+    chapters_codes,
+    bible_folders,
+  );
+}
