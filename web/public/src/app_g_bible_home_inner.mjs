09bc280974f21edc284da0ab5e8ee6c6044273ac
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:20:05 -0500

    function_auto_path ..\love\public\src\text_to.mjs

--- web/public/src/app_g_bible_home_inner.mjs
+++ web/public/src/app_g_bible_home_inner.mjs
@@ -5,7 +5,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_bible_home_generic } from "../../../love/public/src/app_bible_home_generic.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
-import { string_to } from "../../../love/public/src/string_to.mjs";
+import { text_to } from "../../../love/public/src/text_to.mjs";
 import { list_max } from "../../../love/public/src/list_max.mjs";
 import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -26,7 +26,7 @@ export async function app_g_bible_home_inner(context, download) {
         let verse_numbers = object_property_get(passage, "verse_numbers");
         let mapped = list_map(verse_numbers, integer_to_try);
         let max = list_max(mapped);
-        let s = string_to(max);
+        let s = text_to(max);
         if (equal(s, verse_number)) {
           let copy = list_copy(verses);
           la({
