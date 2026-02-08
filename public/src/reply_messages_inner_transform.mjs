f63b7555633a54e91d81c67ca84828d6e115329e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:29 -0500

    function_auto_path ..\love\public\src\bible_verse_trim_right.mjs

--- public/src/reply_messages_inner_transform.mjs
+++ public/src/reply_messages_inner_transform.mjs
@@ -2,12 +2,12 @@ import { text_includes_curry_right_get } from "../../../love/public/src/text_inc
 import { list_any } from "../../../love/public/src/list_any.mjs";
 import { text_letters_is } from "../../../love/public/src/text_letters_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
+import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 import { text_lower_to } from "../../../love/public/src/text_lower_to.mjs";
 import { digits_string } from "../../../love/public/src/digits_string.mjs";
 export function reply_messages_inner_transform(message) {
   let lower = text_lower_to(message);
-  let tokens = string_split_empty(lower);
+  let tokens = text_split_empty(lower);
   let string_includes_curry_right = text_includes_curry_right_get(
     "'" + digits_string(),
   );
