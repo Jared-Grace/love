fcea67abde64c402613e0ec92f58880198d50caa
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:11 -0500

    function_auto_path ..\love\public\src\text_index_of_try.mjs

--- public/src/function_rename_part_insert.mjs
+++ public/src/function_rename_part_insert.mjs
@@ -1,4 +1,4 @@
-import { string_index_of } from "../../../love/public/src/string_index_of.mjs";
+import { text_index_of } from "../../../love/public/src/text_index_of.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { function_rename_part_insert_at } from "../../../love/public/src/function_rename_part_insert_at.mjs";
 export async function function_rename_part_insert(
@@ -7,7 +7,7 @@ export async function function_rename_part_insert(
   part_insert,
 ) {
   let parts = function_name_to_parts(f_name_before);
-  let index = string_index_of(parts, part_insert);
+  let index = text_index_of(parts, part_insert);
   let v = await function_rename_part_insert_at(f_name_before, index, part_new);
   return v;
 }
--- public/src/string_index_of_try.mjs
+++
@@ -1,4 +0,0 @@
-export function string_index_of_try(s, item) {
-  let v = s.indexOf(item);
-  return v;
-}
