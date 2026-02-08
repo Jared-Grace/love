180d08b0baa8651170fe686bcd9e15b286584ae8
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:42 -0500

    function_auto_path ..\love\public\src\text_includes_not.mjs

--- public/src/list_filter_includes_not.mjs
+++ public/src/list_filter_includes_not.mjs
@@ -1,8 +1,8 @@
-import { string_includes_not } from "../../../love/public/src/string_includes_not.mjs";
+import { text_includes_not } from "../../../love/public/src/text_includes_not.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_includes_not(mapped, part) {
   function lambda(item) {
-    let n = string_includes_not(item, part);
+    let n = text_includes_not(item, part);
     return n;
   }
   let filtered = list_filter(mapped, lambda);
