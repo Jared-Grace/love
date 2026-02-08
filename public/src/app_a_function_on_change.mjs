044a23d9a5ed3597383325a7de6c4cad334727c1
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 21:12:15 -0500

    pb app_a_function_on_change

--- public/src/app_a_function_on_change.mjs
+++ public/src/app_a_function_on_change.mjs
@@ -1,7 +1,7 @@
 import { app_a_function_overlay_refresh } from "../../../love/public/src/app_a_function_overlay_refresh.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export async function app_a_function_on_change(o, a) {
+export async function app_a_function_on_change(a, o) {
   let parsed = object_property_get(a, "parsed");
   await file_js_unparse(parsed);
   await app_a_function_overlay_refresh(a, o);
