27f2873fa4af7f442c2b2fb11739131505ec3ebb
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:34 -0500

    function_auto_path ..\love\public\src\keyboard_type_delay.mjs

--- public/src/text_lord_bless.mjs
+++ public/src/text_lord_bless.mjs
@@ -1,5 +1,5 @@
 import { string_may_the_lord } from "../../../love/public/src/string_may_the_lord.mjs";
-export function string_lord_bless() {
+export function text_lord_bless() {
   let v = string_may_the_lord() + "bless ";
   return v;
 }
