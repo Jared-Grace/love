2f1ddc2b6deb73ab4fb8fcf96665894786a9eecf
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:03:58 -0500

    function_auto_path ..\love\public\src\function_parse_strict_declaration.mjs

--- public/src/function_parse_strict_declaration.mjs
+++ public/src/function_parse_strict_declaration.mjs
@@ -2,6 +2,6 @@ import { function_parse_strict } from "../../../love/public/src/function_parse_s
 import { function_parse_declaration_inner } from "../../../love/public/src/function_parse_declaration_inner.mjs";
 export async function function_parse_strict_declaration(f_name) {
   let parsed = await function_parse_strict(f_name);
-  let to = function_parse_declaration_inner(parsed);
-  return to;
+  let r = function_parse_declaration_inner(parsed);
+  return r;
 }
