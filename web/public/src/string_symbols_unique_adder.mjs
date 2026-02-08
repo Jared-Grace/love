3e33c9166ad00734bca72e653657bf22ade58cfa
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:19:04 -0500

    function_auto_path ..\love\public\src\text_symbols_unique.mjs

--- web/public/src/string_symbols_unique_adder.mjs
+++ web/public/src/string_symbols_unique_adder.mjs
@@ -1,4 +1,4 @@
-import { string_symbols_unique } from "../../../love/public/src/string_symbols_unique.mjs";
+import { text_symbols_unique } from "../../../love/public/src/text_symbols_unique.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 export async function string_symbols_unique_adder(lambda$la) {
@@ -7,6 +7,6 @@ export async function string_symbols_unique_adder(lambda$la) {
   }
   let list = await list_adder_async(lambda3);
   let joined = list_join_empty(list);
-  let unique = string_symbols_unique(joined);
+  let unique = text_symbols_unique(joined);
   return unique;
 }
