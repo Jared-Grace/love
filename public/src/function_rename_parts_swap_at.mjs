a7a286361a43218134a1c812bc7bbaee3134276a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:22:58 -0500

    function_auto_path ..\love\public\src\function_rename_parts_swap_at.mjs

--- public/src/function_rename_parts_swap_at.mjs
+++ public/src/function_rename_parts_swap_at.mjs
@@ -3,11 +3,14 @@ import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs"
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_parts_swap_at(
   f_name_before,
-  part_a,
-  part_b,
+  index_a,
+  index_b,
 ) {
   assert_arguments(arguments, 3);
-  let f_name_after = function_name_parts_swap_at(f_name_before, part_a, part_b);
-  let v = await function_rename(f_name_before, f_name_after);
-  return v;
+  let f_name_after = function_name_parts_swap_at(
+    f_name_before,
+    index_a,
+    index_b,
+  );
+  await function_rename(f_name_before, f_name_after);
 }
