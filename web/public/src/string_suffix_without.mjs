2ee2bfc0921b1b973682ff03ca85b9aa70c02d02
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:07 -0500

    function_auto_path ..\love\public\src\string_ends_with_space.mjs

--- web/public/src/string_suffix_without.mjs
+++ web/public/src/string_suffix_without.mjs
@@ -1,10 +1,10 @@
 import { not } from "../../../love/public/src/not.mjs";
-import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
+import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { string_slice } from "../../../love/public/src/string_slice.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
 export function string_suffix_without(s, suffix) {
-  let a = string_ends_with(s, suffix);
+  let a = text_ends_with(s, suffix);
   if (not(a)) {
     error();
   }
--- web/public/src/text_ends_with_any.mjs
+++ web/public/src/text_ends_with_any.mjs
@@ -1,6 +1,6 @@
 import { list_any } from "../../../love/public/src/list_any.mjs";
 import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
-export function string_ends_with_any(s, suffixes) {
+export function text_ends_with_any(s, suffixes) {
   function lambda6(suffix) {
     let ew = text_ends_with(s, suffix);
     return ew;
