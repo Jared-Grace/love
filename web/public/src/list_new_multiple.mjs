5164d541e9f8c7f24086e203f88719f70102b70d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 16:52:34 -0500

    function_auto_path ..\love\public\src\list_new_multiple.mjs

--- web/public/src/list_new_multiple.mjs
+++ web/public/src/list_new_multiple.mjs
@@ -1,11 +1,11 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
-export function list_new_multiple(a12) {
+export function list_new_multiple(count) {
   function lambda(la2) {
     function lambda2(i) {
       la2([]);
     }
-    each_range(a12, lambda2);
+    each_range(count, lambda2);
   }
   let list = list_adder(lambda);
   return list;
