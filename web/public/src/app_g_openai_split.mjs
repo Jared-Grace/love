4a001affa216c558870b3f922e8749240b5f75da
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:51 -0500

    function_auto_path ..\love\public\src\text_split_multiple.mjs

--- web/public/src/app_g_openai_split.mjs
+++ web/public/src/app_g_openai_split.mjs
@@ -2,12 +2,12 @@ import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_e
 import { string_trim } from "../../../love/public/src/string_trim.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { newline_windows } from "../../../love/public/src/newline_windows.mjs";
-import { string_split_multiple } from "../../../love/public/src/string_split_multiple.mjs";
+import { text_split_multiple } from "../../../love/public/src/text_split_multiple.mjs";
 import { newline_windows_escaped } from "../../../love/public/src/newline_windows_escaped.mjs";
 export function app_g_openai_split(objections) {
   let separator = newline_windows_escaped();
   let n = newline_windows();
-  let split = string_split_multiple(objections, [separator, n]);
+  let split = text_split_multiple(objections, [separator, n]);
   let mapped = list_map(split, string_trim);
   let filtered = list_filter_empty_not_is(mapped);
   return filtered;
