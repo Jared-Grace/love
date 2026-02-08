f3b0525c45d14dc75ecef85cb07e4d784bb7ddbd
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:07 -0500

    function_auto_path ..\love\public\src\string_ends_with_space.mjs

--- public/src/string_ends_with_space.mjs
+++ public/src/string_ends_with_space.mjs
@@ -1,5 +1,5 @@
-import { string_ends_with } from "../../../love/public/src/string_ends_with.mjs";
+import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 export function string_ends_with_space(s) {
-  let sw2 = string_ends_with(s, " ");
+  let sw2 = text_ends_with(s, " ");
   return sw2;
 }
