888593a6fb952b98f228e2243ccf95cb14e17eff
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:07:18 -0500

    function_auto_path ..\love\public\src\text_empty.mjs

--- public/src/list_join_empty.mjs
+++ public/src/list_join_empty.mjs
@@ -1,7 +1,7 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
-import { string_empty } from "../../../love/public/src/string_empty.mjs";
+import { text_empty } from "../../../love/public/src/text_empty.mjs";
 export function list_join_empty(list) {
-  const e = string_empty();
+  const e = text_empty();
   let joined = list_join(list, e);
   return joined;
 }
