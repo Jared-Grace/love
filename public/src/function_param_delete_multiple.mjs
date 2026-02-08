879b0d61325b7c95b6d9850cb87ef6a543edd5f1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:05 -0500

    function_auto_path ..\love\public\src\function_run_output_file_temp_args.mjs

--- public/src/function_param_delete_multiple.mjs
+++ public/src/function_param_delete_multiple.mjs
@@ -3,7 +3,7 @@ import { list_reverse } from "../../../love/public/src/list_reverse.mjs";
 import { list_remove_at_multiple } from "../../../love/public/src/list_remove_at_multiple.mjs";
 import { list_sort_number } from "../../../love/public/src/list_sort_number.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
 import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
@@ -12,7 +12,7 @@ import { function_params_new_generic } from "../../../love/public/src/function_p
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export async function function_param_delete_multiple(param_names_comma) {
   assert_arguments(arguments, 1);
-  let param_names = string_split_comma(param_names_comma);
+  let param_names = text_split_comma(param_names_comma);
   let indices = null;
   let f_name = error();
   await function_params_new_generic(
--- public/src/function_transform_result.mjs
+++ public/src/function_transform_result.mjs
@@ -1,13 +1,13 @@
-import { string_split_comma } from "../../../love/public/src/string_split_comma.mjs";
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 import { data_all_initialize } from "../../../love/public/src/data_all_initialize.mjs";
 import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
-import { each_async } from "./each_async.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function function_transform_result(f_names, lambda$ast) {
-  let split = string_split_comma(f_names);
+  let split = text_split_comma(f_names);
   async function lambda(f_name) {
     async function lambda2() {
       let parsed = await function_parse_unaliased(f_name);
