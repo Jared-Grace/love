854cabca57fc099c024188bd68dc6bedec65a907
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 20:25:22 -0500

    function_auto_path ..\love\public\src\js_statement_return_insert.mjs

--- public/src/js_statement_return_insert.mjs
+++ public/src/js_statement_return_insert.mjs
@@ -1,6 +1,8 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { js_statement_return_argument } from "../../../love/public/src/js_statement_return_argument.mjs";
 export function js_statement_return_insert(list, index, argument) {
+  assert_arguments(arguments, 3);
   let r = js_statement_return_argument(argument);
   list_insert(list, index, r);
 }
