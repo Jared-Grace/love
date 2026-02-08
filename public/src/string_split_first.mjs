745b41b27e7821ffc3ec57726f870e6b34e9059c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:11 -0500

    function_auto_path ..\love\public\src\text_index_of_try.mjs

--- public/src/string_split_first.mjs
+++ public/src/string_split_first.mjs
@@ -1,7 +1,7 @@
 import { string_take } from "../../../love/public/src/string_take.mjs";
-import { string_index_of_try } from "./string_index_of_try.mjs";
+import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 export function string_split_first(item, s) {
-  let index = string_index_of_try(item, s);
+  let index = text_index_of_try(item, s);
   if (index < 0) {
     return item;
   }
---
+++ public/src/text_index_of_try.mjs
@@ -0,0 +1,4 @@
+export function text_index_of_try(s, item) {
+  let v = s.indexOf(item);
+  return v;
+}
