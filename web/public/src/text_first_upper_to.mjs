38dd12e59518ef4826679caad3efc06371be100e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:47 -0500

    function_auto_path ..\love\public\src\text_first_lower_to.mjs

--- web/public/src/text_first_upper_to.mjs
+++ web/public/src/text_first_upper_to.mjs
@@ -1,6 +1,6 @@
 import { string_upper_to } from "../../../love/public/src/string_upper_to.mjs";
 import { text_first_transform } from "../../../love/public/src/text_first_transform.mjs";
-export function string_first_upper_to(s) {
+export function text_first_upper_to(s) {
   let transform = string_upper_to;
   let v = text_first_transform(s, transform);
   return v;
