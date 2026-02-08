23faf21a2f28845da950847a8d475193881cf875
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:37 -0500

    function_auto_path ..\love\public\src\text_split_equal.mjs

--- web/public/src/html_hash_object_get.mjs
+++ web/public/src/html_hash_object_get.mjs
@@ -4,7 +4,7 @@ import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
-import { string_split_equal } from "../../../love/public/src/string_split_equal.mjs";
+import { text_split_equal } from "../../../love/public/src/text_split_equal.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 import { html_hash_get } from "../../../love/public/src/html_hash_get.mjs";
@@ -19,7 +19,7 @@ export function html_hash_object_get() {
     let without = text_prefix_without(hash_url, prefix);
     let split = text_split_comma(without);
     function lambda(s) {
-      let split2 = string_split_equal(s);
+      let split2 = text_split_equal(s);
       let v = list_first_second(split2);
       let second = object_property_get(v, "second");
       let first = object_property_get(v, "first");
