8da00fdc235ab3d1a5d68292c27d79d8de24eb22
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:17:19 -0500

    function_auto_path ..\love\public\src\app_next_hash_to_languages_chosen.mjs

--- public/src/text_split_semicolon.mjs
+++ public/src/text_split_semicolon.mjs
@@ -1,5 +1,5 @@
 import { text_split } from "../../../love/public/src/text_split.mjs";
-export function string_split_semicolon(s) {
+export function text_split_semicolon(s) {
   let split3 = text_split(s, ";");
   return split3;
 }
