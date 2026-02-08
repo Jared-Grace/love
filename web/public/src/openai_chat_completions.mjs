88d3d129cc5ca127b1783f50b62012433f81cd8b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:43:12 -0500

    rrl3 openai_chat_completions_generic cmd_py_api

--- web/public/src/openai_chat_completions.mjs
+++ web/public/src/openai_chat_completions.mjs
@@ -1,10 +1,10 @@
-import { openai_chat_completions_generic } from "../../../love/public/src/openai_chat_completions_generic.mjs";
+import { openai_cmd_py_api } from "../../../love/public/src/openai_cmd_py_api.mjs";
 export async function openai_chat_completions(
   input_file_path,
   output_file_path,
 ) {
   const openai_script_name = "chat_completions";
-  await openai_chat_completions_generic(
+  await openai_cmd_py_api(
     openai_script_name,
     input_file_path,
     output_file_path,
--- web/public/src/openai_cmd_py_api.mjs
+++ web/public/src/openai_cmd_py_api.mjs
@@ -2,7 +2,7 @@ import { command_line } from "../../../love/public/src/command_line.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { openai_key_folder } from "../../../love/public/src/openai_key_folder.mjs";
 import { py_script_run_cmd } from "../../../love/public/src/py_script_run_cmd.mjs";
-export async function openai_chat_completions_generic(
+export async function openai_cmd_py_api(
   openai_script_name,
   input_file_path,
   output_file_path,
