da0d4761c3978c8278cda509cc5645e8f46cc5b2
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:20:15 -0500

    function_auto_path ..\love\public\src\marker_first.mjs

--- public/src/marker_first.mjs
+++ public/src/marker_first.mjs
@@ -1,7 +1,7 @@
 import { marker_first_index } from "../../../love/public/src/marker_first_index.mjs";
-import { string_to } from "../../../love/public/src/string_to.mjs";
+import { text_to } from "../../../love/public/src/text_to.mjs";
 export function marker_first() {
   let index_value = marker_first_index();
-  let index = string_to(index_value);
+  let index = text_to(index_value);
   return index;
 }
