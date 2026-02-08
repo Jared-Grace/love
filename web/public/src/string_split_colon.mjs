2d92f6bd541a64993fb9d3988aba2a81da50e6d5
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:15:43 -0500

    function_auto_path ..\love\public\src\text_replace.mjs

--- web/public/src/string_split_colon.mjs
+++
@@ -1,5 +0,0 @@
-import { text_split } from "../../../love/public/src/text_split.mjs";
-export function string_split_colon(chapter_verses) {
-  let split3 = text_split(chapter_verses, ":");
-  return split3;
-}
