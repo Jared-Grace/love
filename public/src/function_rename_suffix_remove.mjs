595ae611fb9060ce7287ff49009bc6ff1af445e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:19:02 -0500

    function_auto_path ..\love\public\src\function_rename_suffix_remove.mjs

--- public/src/function_rename_suffix_remove.mjs
+++ public/src/function_rename_suffix_remove.mjs
@@ -1,10 +1,10 @@
-import { string_suffix_without } from "../../../love/public/src/string_suffix_without.mjs";
+import { text_suffix_without } from "../../../love/public/src/text_suffix_without.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 import { function_rename_suffix_add_generic } from "../../../love/public/src/function_rename_suffix_add_generic.mjs";
 export async function function_rename_suffix_remove(f_name_before, suffix) {
   function lambda(f_name_before, suffix) {
     let separator = function_name_separator();
-    let sw = string_suffix_without(f_name_before, "" + separator + suffix);
+    let sw = text_suffix_without(f_name_before, "" + separator + suffix);
     return sw;
   }
   let v = await function_rename_suffix_add_generic(
