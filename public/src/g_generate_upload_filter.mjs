961da2de03552317528666fc6c920ca0aae0d9a3
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 12:59:56 -0500

    function_auto_path ..\love\public\src\g_generate_upload_filter.mjs

--- public/src/g_generate_upload_filter.mjs
+++ public/src/g_generate_upload_filter.mjs
@@ -2,7 +2,7 @@ import { g_generate_upload_single_generic } from "../../../love/public/src/g_gen
 import { not } from "../../../love/public/src/not.mjs";
 import { string_includes } from "../../../love/public/src/string_includes.mjs";
 import { folder_user_storage_function_each } from "../../../love/public/src/folder_user_storage_function_each.mjs";
-export async function g_generate_upload_single(fn, path_get, search) {
+export async function g_generate_upload_filter(fn, path_get, search) {
   await folder_user_storage_function_each(fn, file_each);
   async function file_each(file) {
     let i = string_includes(file, search);
