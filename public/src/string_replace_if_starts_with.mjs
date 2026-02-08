98e4e9b09b7237c2c2663e5605410c0e68eb3cea
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:17 -0500

    function_auto_path ..\love\public\src\string_replace_space_underscore.mjs

--- public/src/string_replace_if_starts_with.mjs
+++
@@ -1,9 +0,0 @@
-import { text_replace } from "../../../love/public/src/text_replace.mjs";
-import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
-export function string_replace_if_starts_with(prefix, item, replacement) {
-  let sw = string_starts_with(item, prefix);
-  if (sw) {
-    item = text_replace(item, prefix, replacement);
-  }
-  return item;
-}
