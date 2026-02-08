c8c999b77d28e2b6efd83a8c1b4dff9e03ff514f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:17 -0500

    function_auto_path ..\love\public\src\text_ends_with_not.mjs

--- public/src/list_filter_ends_with_not_any.mjs
+++ public/src/list_filter_ends_with_not_any.mjs
@@ -1,10 +1,10 @@
-import { string_ends_with_not } from "../../../love/public/src/string_ends_with_not.mjs";
+import { text_ends_with_not } from "../../../love/public/src/text_ends_with_not.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_any } from "../../../love/public/src/list_any.mjs";
 export function list_filter_ends_with_not_any(suffixes, list) {
   function lambda(item) {
     function lambda3(suffix) {
-      let ewn = string_ends_with_not(item, suffix);
+      let ewn = text_ends_with_not(item, suffix);
       return ewn;
     }
     let any = list_any(suffixes, lambda3);
