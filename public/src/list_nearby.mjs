b53919076dca01fc7adf29fcd58147607eda3a49
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:49:27 -0500

    function_auto_path ..\love\public\src\list_nearby.mjs

--- public/src/list_nearby.mjs
+++ public/src/list_nearby.mjs
@@ -1,9 +1,11 @@
+import { positive_is_assert } from "../../../love/public/src/positive_is_assert.mjs";
 import { list_indices_to_items } from "../../../love/public/src/list_indices_to_items.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { range_from } from "../../../love/public/src/range_from.mjs";
 export function list_nearby(list, nearness) {
+  positive_is_assert(chunk_size);
   function mapper(item, index) {
     let r = range_from(index - nearness, index + nearness);
     function lambda6(index3) {
