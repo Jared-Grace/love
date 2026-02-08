ff50dca75b7573d5316db851a9778518cc673e5d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:25 -0500

    function_auto_path ..\love\public\src\text_replace_space_underscore.mjs

--- web/public/src/folder_files_names_underscore.mjs
+++ web/public/src/folder_files_names_underscore.mjs
@@ -1,9 +1,9 @@
-import { string_replace_space_underscore } from "../../../love/public/src/string_replace_space_underscore.mjs";
+import { text_replace_space_underscore } from "../../../love/public/src/text_replace_space_underscore.mjs";
 import { folder_files_names_transform } from "../../../love/public/src/folder_files_names_transform.mjs";
 export async function folder_files_names_underscore(path) {
   let list = await folder_files_names_transform(
     path,
-    string_replace_space_underscore,
+    text_replace_space_underscore,
   );
   return list;
 }
