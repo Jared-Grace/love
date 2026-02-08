aa8ddd2952f059c460d763e41e49b58f3ce7f58b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:40 -0500

    function_auto_path ..\love\public\src\text_pad.mjs

--- public/src/tasks_run.mjs
+++ public/src/tasks_run.mjs
@@ -1,4 +1,4 @@
-import { string_pad } from "../../../love/public/src/string_pad.mjs";
+import { text_pad } from "../../../love/public/src/text_pad.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -8,7 +8,7 @@ import { functions_search } from "../../../love/public/src/functions_search.mjs"
 export async function tasks_run() {
   let result2 = task_function_name_part();
   let separator = function_name_separator();
-  let search = string_pad(result2, separator);
+  let search = text_pad(result2, separator);
   let result = await functions_search(search);
   let properties = object_properties(result);
   async function lambda(item) {
