01b103bf3a5359adcd0268f4840e82b247fecfba
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:56 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- public/src/string_last.mjs
+++ public/src/string_last.mjs
@@ -1,7 +1,7 @@
-import { string_get } from "../../../love/public/src/string_get.mjs";
+import { text_get } from "../../../love/public/src/text_get.mjs";
 import { string_index_last } from "../../../love/public/src/string_index_last.mjs";
 export function string_last(s) {
   let index_last = string_index_last(s);
-  let item = string_get(s, index_last);
+  let item = text_get(s, index_last);
   return item;
 }
