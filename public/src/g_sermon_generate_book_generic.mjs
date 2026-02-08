c760560e19f82a5065eac92b9d8576441eff4cd8
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 16:23:37 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic.mjs

--- public/src/g_sermon_generate_book_generic.mjs
+++ public/src/g_sermon_generate_book_generic.mjs
@@ -1,3 +1,4 @@
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { list_add_pair } from "../../../love/public/src/list_add_pair.mjs";
@@ -20,7 +21,6 @@ import { list_map_property_join_space } from "../../../love/public/src/list_map_
 import { g_generate_openai_chat_completions } from "../../../love/public/src/g_generate_openai_chat_completions.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -125,7 +125,7 @@ export async function g_sermon_generate_book_generic(
       );
       return match_chapter;
     }
-    let groups_match_chapter = list_filter_property(nearbys, filter_group);
+    let groups_match_chapter = list_filter(nearbys, filter_group);
     async function lambda5(g) {
       log(g);
       let passage = object_property_get(g, "item");
