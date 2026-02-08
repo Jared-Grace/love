9d9fa81b8111ee5a649e8a853cf5edc60037af35
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:57 -0500

    function_auto_path ..\love\public\src\string_take_less_1.mjs

--- public/src/text_skip.mjs
+++ public/src/text_skip.mjs
@@ -1,6 +1,6 @@
 import { string_slice } from "../../../love/public/src/string_slice.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
-export function string_skip(s, skip_count) {
+export function text_skip(s, skip_count) {
   let b = text_size(s);
   const skipped = string_slice(s, skip_count, b);
   return skipped;
