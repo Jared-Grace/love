12af0d0e2f79b1d40f7eb95f3bdc68ea25e8bd7c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:20:46 -0500

    function_auto_path ..\love\public\src\list_swap_at.mjs

--- web/public/src/list_swap_at.mjs
+++ web/public/src/list_swap_at.mjs
@@ -1,7 +1,7 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_swap_inner } from "../../../love/public/src/list_swap_inner.mjs";
-export function list_swap_at(list, ai, bi) {
-  let a = list_get(list, ai);
-  let b = list_get(list, bi);
-  list_swap_inner(list, ai, b, bi, a);
+export function list_swap_at(list, index_a, index_b) {
+  let a = list_get(list, index_a);
+  let b = list_get(list, index_b);
+  list_swap_inner(list, index_a, b, index_b, a);
 }
