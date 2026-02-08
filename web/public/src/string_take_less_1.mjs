6ed304fe4d85679bfd37550e43827d2a6da45534
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:52 -0500

    function_auto_path ..\love\public\src\text_size_less_1.mjs

--- web/public/src/string_take_less_1.mjs
+++ web/public/src/string_take_less_1.mjs
@@ -1,7 +1,7 @@
 import { string_take } from "../../../love/public/src/string_take.mjs";
-import { string_size_less_1 } from "../../../love/public/src/string_size_less_1.mjs";
+import { text_size_less_1 } from "../../../love/public/src/text_size_less_1.mjs";
 export function string_take_less_1(s) {
-  const sz1 = string_size_less_1(s);
+  const sz1 = text_size_less_1(s);
   s = string_take(s, sz1);
   return s;
 }
