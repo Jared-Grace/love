fd2575d43bf7c9d12cbe8e1f195580a348a81262
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:18 -0500

    function_auto_path ..\love\public\src\autopray.mjs

--- public/src/string_multiply.mjs
+++
@@ -1,9 +0,0 @@
-import { each_range } from "../../../love/public/src/each_range.mjs";
-export function string_multiply(value, times) {
-  let result = "";
-  function lambda4(i) {
-    result += value;
-  }
-  each_range(times, lambda4);
-  return result;
-}
