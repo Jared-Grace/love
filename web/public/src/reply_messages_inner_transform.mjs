fa021dc09c783b150c20a9eb5f86115e091a51a7
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:26 -0500

    function_auto_path ..\love\public\src\string_only_or.mjs

--- web/public/src/reply_messages_inner_transform.mjs
+++ web/public/src/reply_messages_inner_transform.mjs
@@ -1,14 +1,14 @@
-import { string_includes_curry_right_get } from "../../../love/public/src/string_includes_curry_right_get.mjs";
+import { text_includes_curry_right_get } from "../../../love/public/src/text_includes_curry_right_get.mjs";
 import { list_any } from "../../../love/public/src/list_any.mjs";
 import { string_letters_is } from "../../../love/public/src/string_letters_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
-import { digits_string } from "./digits_string.mjs";
+import { digits_string } from "../../../love/public/src/digits_string.mjs";
 export function reply_messages_inner_transform(message) {
   let lower = string_lower_to(message);
   let tokens = string_split_empty(lower);
-  let string_includes_curry_right = string_includes_curry_right_get(
+  let string_includes_curry_right = text_includes_curry_right_get(
     "'" + digits_string(),
   );
   const choices = [string_letters_is, string_includes_curry_right];
