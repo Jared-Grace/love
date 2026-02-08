e12483cb5bd5b7fe3af3386a104262a3478311ad
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:36:00 -0500

    function_auto_path ..\love\public\src\function_name_to_parts_last_2.mjs

--- public/src/function_name_to_parts_last_2.mjs
+++ public/src/function_name_to_parts_last_2.mjs
@@ -3,10 +3,8 @@ import { function_name_combine_multiple } from "../../../love/public/src/functio
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_to_parts_last_2(f_name_before) {
-  let transformed = function_name_parts_transform(
-    before,
-    function lambda(parts2) {},
-  );
+  function lambda(parts2) {}
+  let transformed = function_name_parts_transform(before, lambda);
   let parts = function_name_to_parts(f_name_before);
   let result = list_slice_end(parts, 2);
   let from = function_name_combine_multiple(result);
