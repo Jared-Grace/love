3980aeb10a7f6f00dd71f4ff477afc1763daf075
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 20:22:18 -0500

    function_auto_path ..\love\public\src\app_a_functions_overlay.mjs

---
+++ web/public/src/app_a_functions_overlay_generic.mjs
@@ -0,0 +1,5 @@
+import { app_a_list_overlay_generic } from "../../../love/public/src/app_a_list_overlay_generic.mjs";
+export function app_a_functions_overlay_generic(a, f_names, lambda$f_name) {
+  let v = app_a_list_overlay_generic(a, f_names, "function", lambda$f_name);
+  return v;
+}
