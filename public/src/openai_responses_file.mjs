d4199b30a651131613b4d4d1a944181fae53d987
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:44:21 -0500

    function_auto_path ..\love\public\src\openai_responses_file.mjs

--- public/src/openai_responses_file.mjs
+++ public/src/openai_responses_file.mjs
@@ -1,5 +1,5 @@
 import { openai_cmd_py_api } from "../../../love/public/src/openai_cmd_py_api.mjs";
-export async function openai_responses(input_file_path, output_file_path) {
+export async function openai_responses_file(input_file_path, output_file_path) {
   const openai_script_name = "responses";
   await openai_cmd_py_api(
     openai_script_name,
