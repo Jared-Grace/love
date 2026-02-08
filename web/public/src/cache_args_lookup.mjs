4913fab2a2acc81e400878e6a96968be44064199
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:53:41 -0500

    function_auto_path ..\love\public\src\cache_args_lookup.mjs

--- web/public/src/cache_args_lookup.mjs
+++ web/public/src/cache_args_lookup.mjs
@@ -1,3 +1,4 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
@@ -7,8 +8,8 @@ export async function cache_args_lookup() {
   let file_name = file_name_json("data");
   let f_path = folder_user_storage_function_path(cache_args_lookup);
   let joined = path_join([f_path, file_name]);
-  let exists = await file_exists(file_path);
-  if (false) {
+  let exists = await file_exists(joined);
+  if (not(exists)) {
   }
   async function lambda(data) {}
   await file_json_transform(joined, lambda);
