5763a99f514c518455fe37349f903bd5de784b5b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:27 -0500

    function_auto_path ..\love\public\src\text_starts_with_not.mjs

--- web/public/src/file_path_normalize.mjs
+++ web/public/src/file_path_normalize.mjs
@@ -1,9 +1,9 @@
-import { string_starts_with_not } from "../../../love/public/src/string_starts_with_not.mjs";
+import { text_starts_with_not } from "../../../love/public/src/text_starts_with_not.mjs";
 import { path_normalize } from "../../../love/public/src/path_normalize.mjs";
 export function file_path_normalize(file_path) {
   file_path = path_normalize(file_path);
   const prefix = "../";
-  let n = string_starts_with_not(file_path, prefix);
+  let n = text_starts_with_not(file_path, prefix);
   if (n) {
     file_path = prefix + "love/" + file_path;
   }
--- web/public/src/list_filter_starts_with_not_multiple.mjs
+++ web/public/src/list_filter_starts_with_not_multiple.mjs
@@ -1,10 +1,10 @@
 import { list_all } from "../../../love/public/src/list_all.mjs";
-import { string_starts_with_not } from "../../../love/public/src/string_starts_with_not.mjs";
+import { text_starts_with_not } from "../../../love/public/src/text_starts_with_not.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_starts_with_not_multiple(mapped, prefixes) {
   function lambda(item) {
     function lambda2(prefix) {
-      let sw = string_starts_with_not(item, prefix);
+      let sw = text_starts_with_not(item, prefix);
       return sw;
     }
     let v = list_all(prefixes, lambda2);
