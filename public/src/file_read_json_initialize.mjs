d9c3522dcbd662327047946c5f54c148e6274245
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:25:06 -0500

    pb file_read_json_initialize

--- public/src/file_read_json_initialize.mjs
+++ public/src/file_read_json_initialize.mjs
@@ -1,6 +1,6 @@
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { file_write_json_if_exists_not } from "../../../love/public/src/file_write_json_if_exists_not.mjs";
-export async function file_read_json_initialize(initial, f_path) {
+export async function file_read_json_initialize(f_path, initial) {
   await file_write_json_if_exists_not(f_path, initial);
   let data = await file_read_json(f_path);
   return data;
--- public/src/text_to_uuid_get.mjs
+++ public/src/text_to_uuid_get.mjs
@@ -6,7 +6,7 @@ import { text_to_uuid_path } from "../../../love/public/src/text_to_uuid_path.mj
 export async function text_to_uuid_get(id) {
   let joined = text_to_uuid_path();
   let initial = text_to_uuid_initial();
-  let data = await file_read_json_initialize(initial, joined);
+  let data = await file_read_json_initialize(joined, initial);
   let get = text_to_uuid_get_initialize(data);
   let text = object_property_get(get, id);
   return text;
