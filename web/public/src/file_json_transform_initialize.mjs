51dd7ff0d15ed0f5d38d49d8b0e96e9d58567376
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:55:24 -0500

    function_auto_path ..\love\public\src\cache_args_lookup.mjs

---
+++ web/public/src/file_json_transform_initialize.mjs
@@ -0,0 +1,16 @@
+import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
+import { file_write_json } from "../../../love/public/src/file_write_json.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { file_exists } from "../../../love/public/src/file_exists.mjs";
+export async function file_json_transform_initialize(
+  exists2,
+  joined,
+  initial,
+  lambda,
+) {
+  let exists = await file_exists(joined);
+  if (not(exists)) {
+    await file_write_json(joined, initial);
+  }
+  await file_json_transform(joined, lambda);
+}
