b4822e6f1c481675d344180a499a163ecd589c9f
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:11:36 -0500

    function_auto_path ..\love\public\src\function_name_parts_transform.mjs

--- web/public/src/function_name_parts_transform.mjs
+++ web/public/src/function_name_parts_transform.mjs
@@ -1,8 +1,8 @@
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
-export function function_name_parts_transform(before, fn) {
+export function function_name_parts_transform(before, lambda$parts) {
   let parts = function_name_to_parts(before);
-  fn(parts);
+  lambda$parts(parts);
   let transformed = function_name_combine_multiple(parts);
   return transformed;
 }
