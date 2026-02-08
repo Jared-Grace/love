e9475d805d1dfe86f74e2605a8001a611df8df2d
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:40:00 -0500

    crl function_rename_replace_last_2 3

---
+++ web/public/src/function_rename_replace_last_3.mjs
@@ -0,0 +1,7 @@
+import { function_name_to_parts_last_2 } from "../../../love/public/src/function_name_to_parts_last_2.mjs";
+import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
+export async function function_rename_replace_last_3(f_name_before, to) {
+  let from = function_name_to_parts_last_2(f_name_before);
+  let v = await function_rename_replace(f_name_before, from, to);
+  return v;
+}
