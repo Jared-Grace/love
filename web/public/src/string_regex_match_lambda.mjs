9aa0b49a1ece2ab862a40142c1fab46e9f713f00
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:13:41 -0500

    function_auto_path ..\love\public\src\app_g_conversation.mjs

--- web/public/src/string_regex_match_lambda.mjs
+++ web/public/src/string_regex_match_lambda.mjs
@@ -1,7 +1,7 @@
-import { string_regex_match } from "../../../love/public/src/string_regex_match.mjs";
+import { text_regex_match } from "../../../love/public/src/text_regex_match.mjs";
 export function string_regex_match_lambda(regex) {
   let l = function string_regex_match_lambda_inner(input) {
-    let m = string_regex_match(input, regex);
+    let m = text_regex_match(input, regex);
     return m;
   };
   return l;
