c427ccf4dcb2fc35a0e6c6761be9b610ab3ec523
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:05:59 -0500

    function_auto_path ..\love\public\src\text_and_empty_not_is_assert.mjs

--- public/src/app_new.mjs
+++ public/src/app_new.mjs
@@ -1,12 +1,12 @@
 import { app_update } from "../../../love/public/src/app_update.mjs";
 import { app_new_fn_latest } from "../../../love/public/src/app_new_fn_latest.mjs";
 import { app_new_fn_main } from "../../../love/public/src/app_new_fn_main.mjs";
-import { string_and_empty_not_is_assert } from "../../../love/public/src/string_and_empty_not_is_assert.mjs";
+import { text_and_empty_not_is_assert } from "../../../love/public/src/text_and_empty_not_is_assert.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { app_new_fn } from "../../../love/public/src/app_new_fn.mjs";
 export async function app_new(name) {
   assert_arguments(arguments, 1);
-  string_and_empty_not_is_assert(name);
+  text_and_empty_not_is_assert(name);
   await app_new_fn(name);
   await app_new_fn_main(name);
   await app_new_fn_latest(name);
