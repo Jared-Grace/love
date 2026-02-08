d389f161a1c1ad838eab2a83f6942db2c322a435
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:11:36 -0500

    function_auto_path ..\love\public\src\function_name_parts_transform.mjs

--- public/src/function_name_parts_swap_end.mjs
+++ public/src/function_name_parts_swap_end.mjs
@@ -1,9 +1,7 @@
-import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { list_swap_end } from "../../../love/public/src/list_swap_end.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
-export function function_name_parts_swap_end(f_name_before) {
-  let transformed = function_name_parts_transform(before, fn);
+export function function_name_parts_swap_end(f_name_before) {function_name_parts_transform
   let list = function_name_to_parts(f_name_before);
   list_swap_end(list);
   let f_name_after = function_name_combine_multiple(list);
