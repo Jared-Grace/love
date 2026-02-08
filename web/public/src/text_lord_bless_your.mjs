270efe923bb9fa1c67d200d0dcb024745c878ba8
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:42 -0500

    function_auto_path ..\love\public\src\prayer_blessing.mjs

---
+++ web/public/src/text_lord_bless_your.mjs
@@ -0,0 +1,5 @@
+import { text_lord_bless } from "../../../love/public/src/text_lord_bless.mjs";
+export function text_lord_bless_your() {
+  let blessing = text_lord_bless() + "your ";
+  return blessing;
+}
