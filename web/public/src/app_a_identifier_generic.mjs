b408d02527df977d7b58b0f371a2fdd724f0675a
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 20:59:23 -0500

    function_auto_path ..\love\public\src\app_a_identifier_generic.mjs

--- web/public/src/app_a_identifier_generic.mjs
+++ web/public/src/app_a_identifier_generic.mjs
@@ -184,7 +184,7 @@ export function app_a_identifier_generic(
         let return_name = null;
         return_name = js_return_name(ast_call);
         let assign = js_declare(return_name, init);
-      } else {
+      } else {js_identifier_unique_ast
       }
     }
     js_node_type_is_if(e2, "ExpressionStatement", lambda6);
