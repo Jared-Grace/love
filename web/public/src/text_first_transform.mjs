9adb2dc2e7bc340c63f49caebb66bfbaaa6cb6d3
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:40:17 -0500

    function_auto_path ..\love\public\src\text_first_transform.mjs

--- web/public/src/text_first_transform.mjs
+++ web/public/src/text_first_transform.mjs
@@ -1,3 +1,4 @@
+import { function_copy_replace_first } from "../../../love/public/src/function_copy_replace_first.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
