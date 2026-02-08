e7c56a949b65fe4559d6e9ae8d9fcd5fe61bc543
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:18:26 -0500

    function_auto_path ..\love\public\src\bible_verses_encouragement_generate.mjs

--- web/public/src/text_starts_with_not.mjs
+++ web/public/src/text_starts_with_not.mjs
@@ -1,6 +1,6 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-export function string_starts_with_not(item, book_code) {
+export function text_starts_with_not(item, book_code) {
   let a = text_starts_with(item, book_code);
   let sw = not(a);
   return sw;
