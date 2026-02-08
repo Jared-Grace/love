48ca2d74d806e2a37da2630e7a0fcfffac5268ed
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:42 -0500

    function_auto_path ..\love\public\src\html_hash_object_get.mjs

--- web/public/src/text_split_first.mjs
+++ web/public/src/text_split_first.mjs
@@ -1,6 +1,6 @@
 import { string_take } from "../../../love/public/src/string_take.mjs";
 import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
-export function string_split_first(item, s) {
+export function text_split_first(item, s) {
   let index = text_index_of_try(item, s);
   if (index < 0) {
     return item;
