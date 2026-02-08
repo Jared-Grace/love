d9c27ede68385f8b7ece2261360d2e695865728b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:00 -0500

    function_auto_path ..\love\public\src\function_alias_replace.mjs

--- web/public/src/function_alias_replace.mjs
+++ web/public/src/function_alias_replace.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { string_is_assert_multiple } from "../../../love/public/src/string_is_assert_multiple.mjs";
+import { text_is_assert_multiple } from "../../../love/public/src/text_is_assert_multiple.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { function_alias_generic } from "../../../love/public/src/function_alias_generic.mjs";
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
@@ -7,7 +7,7 @@ import { error } from "../../../love/public/src/error.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 export async function function_alias_replace(alias_old, f_name) {
   const items = [alias_old, f_name];
-  string_is_assert_multiple(items);
+  text_is_assert_multiple(items);
   function lambda(a) {
     let aliases = object_property_get(a, "aliases");
     let exists = object_property_get(a, "exists");
