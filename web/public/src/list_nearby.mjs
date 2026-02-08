9bd7e020f66fe3acb66bc8884af86e39dc1c24ab
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:50:19 -0500

    function_auto_path ..\love\public\src\list_nearby.mjs

--- web/public/src/list_nearby.mjs
+++ web/public/src/list_nearby.mjs
@@ -8,10 +8,10 @@ export function list_nearby(list, nearness) {
   positive_is_assert(nearness);
   function mapper(item, index) {
     let r = range_from(index - nearness, index + nearness);
-    function filter(index) {
+    let filter = function lambda(index) {
       let ii = list_index_is(list, index);
       return ii;
-    }
+    };
     let filtered = list_filter(r, filter);
     let nearby = list_indices_to_items(list, filtered);
     let m = {
