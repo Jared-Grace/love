57cf8b721a3ea036a3ad73583d86888d5af88d8b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:12:30 -0500

    function_auto_path ..\love\public\src\string_only_or_space.mjs

--- public/src/text_only_or.mjs
+++ public/src/text_only_or.mjs
@@ -1,6 +1,6 @@
 import { string_transform } from "../../../love/public/src/string_transform.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
-export function string_only_or(s, valid, replacement) {
+export function text_only_or(s, valid, replacement) {
   let joined = string_transform(s, lambda);
   function lambda(item) {
     let i = text_includes(valid, item);
