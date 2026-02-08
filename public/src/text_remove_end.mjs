a3573d53a6c3026b5d5027acd10b12f857bc2fa0
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:41:04 -0500

    function_auto_path ..\love\public\src\text_remove_end.mjs

--- public/src/text_remove_end.mjs
+++ public/src/text_remove_end.mjs
@@ -1,4 +1,4 @@
-export function list_remove_end(list, count) {
+export function text_remove_end(list, count) {
   const removed = list.slice(-count);
   list.length = Math.max(0, list.length - count);
   return removed;
