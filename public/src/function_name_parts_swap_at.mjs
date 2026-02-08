8af0351063ba44fa720e53441c78cdd6e79a82e6
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:19:36 -0500

    cs function_name_parts_swap at

---
+++ public/src/function_name_parts_swap_at.mjs
@@ -0,0 +1,9 @@
+import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
+import { list_swap } from "../../../love/public/src/list_swap.mjs";
+export function function_name_parts_swap_at(f_name_before, part_a, part_b) {
+  function lambda(parts) {
+    list_swap(parts, part_a, part_b);
+  }
+  let f_name_after = function_name_parts_transform(f_name_before, lambda);
+  return f_name_after;
+}
