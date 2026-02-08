cc926b473a496f87df85b1d7a2fa183cf45e262b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:48:51 -0500

    function_auto_path ..\love\public\src\list_get_curry.mjs

--- public/src/list_get_curry.mjs
+++ public/src/list_get_curry.mjs
@@ -1,8 +1,8 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
 export function list_get_curry(list) {
-  let r = function lambda(index) {
+  let getter = function list_get_curry_getter(index) {
     let item = list_get(list, index);
     return item;
   };
-  return r;
+  return getter;
 }
