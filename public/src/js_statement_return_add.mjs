1ef42515076e7827bbc8ad537e82f9c105c9d7e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-07 20:13:32 -0500

    function_auto_path ..\love\public\src\js_statement_return_add.mjs

--- public/src/js_statement_return_add.mjs
+++ public/src/js_statement_return_add.mjs
@@ -1,8 +1,6 @@
+import { js_statement_return_argument } from "../../../love/public/src/js_statement_return_argument.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { js_statement_return_empty } from "../../../love/public/src/js_statement_return_empty.mjs";
 export function js_statement_return_add(list, argument) {
-  let r = js_statement_return_empty();
-  object_property_set(r, "argument", argument);
+  let r = js_statement_return_argument(argument);
   list_add(list, r);
 }
