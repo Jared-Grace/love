a1213b233f77f2f60ba46642dfa64aa01e9566ee
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:24:19 -0500

    function_auto_path ..\love\public\src\function_name_parts_swap_at.mjs

--- web/public/src/function_name_parts_swap_at.mjs
+++ web/public/src/function_name_parts_swap_at.mjs
@@ -1,6 +1,12 @@
+import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
-export function function_name_parts_swap_at(f_name_before, index_a, index_b) {
+export function function_name_parts_swap_at(
+  f_name_before,
+  index_a_text,
+  index_b_text,
+) {
+  let index = integer_to(index_string);
   function lambda(parts) {
     list_swap_at(parts, index_a, index_b);
   }
