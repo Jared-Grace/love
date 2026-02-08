a421da19b8c94b010d62d82f0206695a254339e7
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:40 -0500

    function_auto_path ..\karate_code\public\src\app_karate_name_sign_up.mjs

--- web/public/src/text_includes_not.mjs
+++ web/public/src/text_includes_not.mjs
@@ -1,6 +1,6 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
-export function string_includes_not(item, part) {
+export function text_includes_not(item, part) {
   let v = text_includes(item, part);
   let n = not(v);
   return n;
