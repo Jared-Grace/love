ca3e7e207ddd46d6426628dbb256bc5c0880d419
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:08:30 -0500

    function_auto_path ..\love\public\src\bible_verses_encouragement_generate.mjs

--- public/src/text_first_lower_to.mjs
+++ public/src/text_first_lower_to.mjs
@@ -1,6 +1,6 @@
 import { string_first_transform } from "../../../love/public/src/string_first_transform.mjs";
 import { string_lower_to } from "../../../love/public/src/string_lower_to.mjs";
-export function string_first_lower_to(s) {
+export function text_first_lower_to(s) {
   let transform = string_lower_to;
   let v = string_first_transform(s, transform);
   return v;
