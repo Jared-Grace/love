6a0a6387b4821c05658d0dffe07143fb9aa400d2
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:11:42 -0500

    function_auto_path ..\love\public\src\prayer_blessing.mjs

--- public/src/prayer_blessing.mjs
+++ public/src/prayer_blessing.mjs
@@ -1,10 +1,10 @@
 import { prayer_end } from "../../../love/public/src/prayer_end.mjs";
-import { string_lord_bless } from "../../../love/public/src/string_lord_bless.mjs";
+import { text_lord_bless } from "../../../love/public/src/text_lord_bless.mjs";
 import { prayer_start } from "../../../love/public/src/prayer_start.mjs";
 export function prayer_blessing(blessed) {
   let v2 =
     prayer_start() +
-    string_lord_bless() +
+    text_lord_bless() +
     blessed +
     " exceedingly abundantly more than anyone can ask or think " +
     prayer_end();
--- public/src/string_lord_bless_your.mjs
+++
@@ -1,5 +0,0 @@
-import { string_lord_bless } from "../../../love/public/src/string_lord_bless.mjs";
-export function string_lord_bless_your() {
-  let blessing = string_lord_bless() + "your ";
-  return blessing;
-}
