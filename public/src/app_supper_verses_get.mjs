bceda90c2e54e6d71e7401de39510bd9539c5ad3
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:16:57 -0500

    function_auto_path ..\love\public\src\text_split_newline.mjs

--- public/src/app_supper_verses_get.mjs
+++ public/src/app_supper_verses_get.mjs
@@ -6,7 +6,7 @@ import { ebible_chapter_codes } from "../../../love/public/src/ebible_chapter_co
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { ebible_references_parse_lines } from "../../../love/public/src/ebible_references_parse_lines.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
-import { string_split_newline } from "../../../love/public/src/string_split_newline.mjs";
+import { text_split_newline } from "../../../love/public/src/text_split_newline.mjs";
 export async function app_supper_verses_get() {
   let e = ebible_folder_english();
   let b = browser_is();
@@ -33,7 +33,7 @@ Acts 2:42
 Acts 20:7
 1 Corinthians 10:16-22
 1 Corinthians 11:17-34`;
-  let split = string_split_newline(references);
+  let split = text_split_newline(references);
   let list = await ebible_references_parse_lines([e], split);
   return list;
 }
