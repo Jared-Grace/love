5c7652e8320b119c714392df5030a132d429007b
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:20:15 -0500

    function_auto_path ..\love\public\src\marker_first.mjs

--- public/src/ebible_references_parse_lines.mjs
+++ public/src/ebible_references_parse_lines.mjs
@@ -7,14 +7,14 @@ import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { each_pair_async } from "../../../love/public/src/each_pair_async.mjs";
 import { each_range_from_async } from "../../../love/public/src/each_range_from_async.mjs";
-import { string_to } from "../../../love/public/src/string_to.mjs";
+import { text_to } from "../../../love/public/src/text_to.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
-import { catch_ignore_async } from "./catch_ignore_async.mjs";
-import { object_assign } from "./object_assign.mjs";
+import { catch_ignore_async } from "../../../love/public/src/catch_ignore_async.mjs";
+import { object_assign } from "../../../love/public/src/object_assign.mjs";
 export async function ebible_references_parse_lines(bible_folders, lines) {
   let bible_folder = ebible_folder_english();
   let books_all = await list_map_unordered_async(
@@ -38,7 +38,7 @@ export async function ebible_references_parse_lines(bible_folders, lines) {
         async function lambda4(verse_number) {
           await catch_ignore_async(verse_get);
           async function verse_get() {
-            verse_number = string_to(verse_number);
+            verse_number = text_to(verse_number);
             let result = await ebible_verse(
               bible_folder,
               chapter_code,
