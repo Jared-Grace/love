f2c50eab18f4032ea45d5e73a383d942206cef51
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:10:39 -0500

    function_auto_path ..\love\public\src\js_identifier_unique.mjs

--- public/src/text_and_empty_not_is_assert.mjs
+++ public/src/text_and_empty_not_is_assert.mjs
@@ -1,6 +1,6 @@
 import { text_empty_not_is_assert } from "../../../love/public/src/text_empty_not_is_assert.mjs";
-import { string_is_assert } from "../../../love/public/src/string_is_assert.mjs";
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 export function text_and_empty_not_is_assert(name) {
-  string_is_assert(name);
+  text_is_assert(name);
   text_empty_not_is_assert(name);
 }
