2bbdb67128077360e2dcf77fc693ed8c2570146a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:06:27 -0500

    function_auto_path ..\love\public\src\text_to_uuid_get.mjs

--- public/src/text_to_uuid_get.mjs
+++ public/src/text_to_uuid_get.mjs
@@ -1,9 +1,9 @@
+import { text_to_uuid_get_initialize } from "../../../love/public/src/text_to_uuid_get_initialize.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { text_to_uuid_path } from "../../../love/public/src/text_to_uuid_path.mjs";
-import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
 export async function text_to_uuid_get(id) {
   let joined = text_to_uuid_path();
   let data = await file_json_read(joined);
-    let get = text_to_uuid_get_initialize(data);
+  let get = text_to_uuid_get_initialize(data);
   let text = object_property_get(get, id);
 }
