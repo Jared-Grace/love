abc48f318324bcba4b1838aaeabeab87c22059d1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 08:45:38 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic.mjs

---
+++ public/src/bible_verse_end_is.mjs
@@ -0,0 +1,10 @@
+import { string_ends_with_any } from "../../../love/public/src/string_ends_with_any.mjs";
+import { string_split_empty } from "../../../love/public/src/string_split_empty.mjs";
+import { bible_verse_trim_right } from "../../../love/public/src/bible_verse_trim_right.mjs";
+export function bible_verse_end_is(text) {
+  let trimmed = bible_verse_trim_right(text);
+  let suffixes = ".?!";
+  let split = string_split_empty(suffixes);
+  let end = string_ends_with_any(trimmed, split);
+  return end;
+}
