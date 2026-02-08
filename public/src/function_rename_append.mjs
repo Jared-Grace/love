20ae84528319724af95224d03cba1befac675ce8
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:43:16 -0500

    function_auto_path ..\love\public\src\function_rename_append.mjs

--- public/src/function_rename_append.mjs
+++ public/src/function_rename_append.mjs
@@ -1,7 +1,12 @@
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
+import { function_name_separator_padded_not_is_assert } from "../../../love/public/src/function_name_separator_padded_not_is_assert.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
-import { function_rename_suffix_add_generic } from "../../../love/public/src/function_rename_suffix_add_generic.mjs";
 export async function function_rename_append(f_name_before, suffix) {
   let fn = text_combine;
-  let v = await function_rename_suffix_add_generic(f_name_before, suffix, fn);
+  function_name_separator_padded_not_is_assert(suffix);
+  f_name_before = await function_name_unalias_only(f_name_before);
+  let f_name_after = fn(f_name_before, suffix);
+  let v = await function_rename(f_name_before, f_name_after);
   return v;
 }
