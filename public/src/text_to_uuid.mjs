de290154746edb26139b2cc67a780d091ed096f4
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 17:58:02 -0500

    function_auto_path ..\love\public\src\text_to_uuid.mjs

--- public/src/text_to_uuid.mjs
+++ public/src/text_to_uuid.mjs
@@ -1,3 +1,4 @@
+import { object_property_exists_not_assert } from "../../../love/public/src/object_property_exists_not_assert.mjs";
 import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
@@ -7,5 +8,7 @@ export async function text_to_uuid(text) {
   let f_path = folder_user_storage_function_path(text_to_uuid);
   let joined = path_join([f_path, file_name]);
   await file_json_transform_initialize(joined, {}, lambda);
-  async function lambda(data) {}
+  async function lambda(data) {
+    object_property_exists_not_assert(object, property_name);
+  }
 }
