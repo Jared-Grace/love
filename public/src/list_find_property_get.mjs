5356ae27e898ff30fc0d18f4042073fcd40de8fd
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 15:40:53 -0500

    function_auto_path ..\love\public\src\list_find_property_get.mjs

--- public/src/list_find_property_get.mjs
+++ public/src/list_find_property_get.mjs
@@ -1,16 +1,12 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 export function list_find_property_get(
-  verses_chapter_folder,
+  list,
   property_find,
-  verse_number,
+  value,
   property_get,
 ) {
-  let v = list_find_property(
-    verses_chapter_folder,
-    property_find,
-    verse_number,
-  );
+  let v = list_find_property(list, property_find, value);
   let text = object_property_get(v, property_get);
   return text;
 }
