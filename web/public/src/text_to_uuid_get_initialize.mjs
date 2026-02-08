1f00687bc2362c491aa418902f8b1855de31d7e6
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:06:20 -0500

    function_auto_path ..\love\public\src\text_to_uuid_set.mjs

---
+++ web/public/src/text_to_uuid_get_initialize.mjs
@@ -0,0 +1,5 @@
+import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
+export function text_to_uuid_get_initialize(data) {
+  let value = object_property_initialize(data, "get", {});
+  return value;
+}
--- web/public/src/text_to_uuid_set.mjs
+++ web/public/src/text_to_uuid_set.mjs
@@ -1,3 +1,4 @@
+import { text_to_uuid_get_initialize } from "../../../love/public/src/text_to_uuid_get_initialize.mjs";
 import { text_to_uuid_path } from "../../../love/public/src/text_to_uuid_path.mjs";
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
@@ -10,7 +11,7 @@ export async function text_to_uuid_set(text) {
   async function lambda(data) {
     let set = object_property_initialize(data, "set", {});
     object_property_exists_not_assert(set, text);
-    let get = object_property_initialize(data, "get", {});
+    let get = text_to_uuid_get_initialize(data);
     let u = await uuid();
     object_property_set(set, text, u);
     object_property_set(get, u, text);
