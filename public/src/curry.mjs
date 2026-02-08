a9e732b21b5848d9cb6dfb3b03e3f71514c2a358
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:44:34 -0500

    function_auto_path ..\love\public\src\curry.mjs

--- public/src/curry.mjs
+++ public/src/curry.mjs
@@ -4,7 +4,7 @@ export function curry(fn) {
       let r = fn(...args);
       return r;
     }
-    let r3 = function lambda(...next) {
+    let r3 = function curried_result(...next) {
       let r2 = curried(...args, ...next);
       return r2;
     };
