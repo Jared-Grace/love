f12637208db50a116b10be90956bc8f18ce585b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:41:05 -0500

    rrf list_remove_end text

--- public/src/list_remove_last.mjs
+++ public/src/list_remove_last.mjs
@@ -1,5 +1,5 @@
-import { list_remove_end } from "../../../love/public/src/list_remove_end.mjs";
+import { text_remove_end } from "../../../love/public/src/text_remove_end.mjs";
 export function list_remove_last(list) {
-  let removed = list_remove_end(list, 1);
+  let removed = text_remove_end(list, 1);
   return removed;
 }
