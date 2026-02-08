72d3c083477a0beef1cc4a07bed9f46321528434
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:18:00 -0500

    pf function_name_parts_swap f_name_before

--- public/src/function_name_parts_swap.mjs
+++ public/src/function_name_parts_swap.mjs
@@ -1,6 +1,6 @@
 import { function_name_parts_transform } from "../../../love/public/src/function_name_parts_transform.mjs";
 import { list_swap } from "../../../love/public/src/list_swap.mjs";
-export function function_name_parts_swap(part_a, part_b, f_name_before) {
+export function function_name_parts_swap(f_name_before, part_a, part_b) {
   function lambda(parts) {
     list_swap(parts, part_a, part_b);
   }
