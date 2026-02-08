f8bd403fccc7265bfa158f62fa269ba10ac3544a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:04:30 -0500

    cr text_to_uuid_set s g

---
+++ public/src/text_to_uuid_get.mjs
@@ -0,0 +1,18 @@
+import { text_to_uuid_path } from "../../../love/public/src/text_to_uuid_path.mjs";
+import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { uuid } from "../../../love/public/src/uuid.mjs";
+import { object_property_exists_not_assert } from "../../../love/public/src/object_property_exists_not_assert.mjs";
+import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
+export async function text_to_uuid_get(text) {
+  let joined = text_to_uuid_path();
+  await file_json_transform_initialize(joined, {}, lambda);
+  async function lambda(data) {
+    let set = object_property_initialize(data, "set", {});
+    object_property_exists_not_assert(set, text);
+    let get = object_property_initialize(data, "get", {});
+    let u = await uuid();
+    object_property_set(set, text, u);
+    object_property_set(get, u, text);
+  }
+}
