ac1e394ddecb0f50595a28bcaf0871ab3802f774
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:14:50 -0500

    function_auto_path ..\love\public\src\bible_names_text_parse.mjs

--- web/public/src/text_size_less_1.mjs
+++ web/public/src/text_size_less_1.mjs
@@ -1,6 +1,6 @@
 import { greater_than_or_equal_assert } from "../../../love/public/src/greater_than_or_equal_assert.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
-export function string_size_less_1(s) {
+export function text_size_less_1(s) {
   let sz = text_size(s);
   greater_than_or_equal_assert(sz, 1);
   const sz1 = sz - 1;
