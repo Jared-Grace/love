3952bb52f6e226bbdb6dfcdf0595aabb2e24dbb9
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:48:36 -0500

    r s list_nearby

--- public/src/functions_search_open.mjs
+++ public/src/functions_search_open.mjs
@@ -1,9 +1,9 @@
-import { functions_search } from "../../../love/public/src/functions_search.mjs";
+import { list_nearby } from "../../../love/public/src/list_nearby.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { object_properties } from "../../../love/public/src/object_properties.mjs";
 export async function functions_search_open(search) {
-  let result = await functions_search(search);
+  let result = await list_nearby(search);
   let properties = object_properties(result);
   let f_name = list_single(properties);
   await function_open(f_name);
--- public/src/list_nearby.mjs
+++ public/src/list_nearby.mjs
@@ -1,7 +1,7 @@
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { functions_search_generic } from "../../../love/public/src/functions_search_generic.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
-export async function functions_search(search) {
+export async function list_nearby(search) {
   assert_arguments(arguments, 1);
   let fn = text_includes;
   let result = await functions_search_generic(search, fn);
