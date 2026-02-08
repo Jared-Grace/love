38a088721af122bbf9529dd298068cc5f1e62c08
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 13:02:04 -0500

    function_auto_path ..\love\public\src\js_statement_return.mjs

--- web/public/src/js_statement_return.mjs
+++ web/public/src/js_statement_return.mjs
@@ -1,7 +1,7 @@
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_return } from "../../../love/public/src/js_code_return.mjs";
 export function js_statement_return(return_argument_code) {
-  let code2 = js_code_return(return_argument_code);
-  let item = js_parse_statement(code2);
+  let code = js_code_return(return_argument_code);
+  let item = js_parse_statement(code);
   return item;
 }
