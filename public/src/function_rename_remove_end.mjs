a075d8659f0d812f6859fc13c55a1b662a0698c5
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:44:49 -0500

    crl function_rename_append remove_end

---
+++ public/src/function_rename_remove_end.mjs
@@ -0,0 +1,9 @@
+import { text_remove_end } from "../../../love/public/src/text_remove_end.mjs";
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
+export async function function_rename_remove_end(f_name_before, count) {
+  f_name_before = await function_name_unalias_only(f_name_before);
+  let f_name_after = text_remove_end(f_name_before, count);
+  let v = await function_rename(f_name_before, f_name_after);
+  return v;
+}
