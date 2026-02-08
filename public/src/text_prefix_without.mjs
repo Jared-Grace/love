4e2aa500aebb3107429ed3ad2cd939b4bd85a45c
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:13:19 -0500

    function_auto_path ..\love\public\src\text_prefix_without.mjs

--- public/src/text_prefix_without.mjs
+++ public/src/text_prefix_without.mjs
@@ -3,7 +3,7 @@ import { string_skip } from "../../../love/public/src/string_skip.mjs";
 import { string_starts_with } from "../../../love/public/src/string_starts_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { string_size } from "../../../love/public/src/string_size.mjs";
-export function string_prefix_without(s, prefix) {
+export function text_prefix_without(s, prefix) {
   let a = string_starts_with(s, prefix);
   if (not(a)) {
     error();
