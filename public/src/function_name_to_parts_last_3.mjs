34b666c420e42bf9e572b7b2b6878d94c5a30482
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 18:39:35 -0500

    crl function_name_to_parts_last_2 3

---
+++ public/src/function_name_to_parts_last_3.mjs
@@ -0,0 +1,6 @@
+import { function_name_to_parts_last_generic } from "../../../love/public/src/function_name_to_parts_last_generic.mjs";
+export function function_name_to_parts_last_3(f_name_before) {
+  let count = 2;
+  let f_name_after = function_name_to_parts_last_generic(f_name_before, count);
+  return f_name_after;
+}
