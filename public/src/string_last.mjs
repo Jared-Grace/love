1e55413f98b6eb0538c1af0dedc2ce5465984fab
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 14:09:57 -0500

    function_auto_path ..\love\public\src\text_index_last.mjs

--- public/src/string_last.mjs
+++ public/src/string_last.mjs
@@ -1,7 +1,7 @@
 import { text_get } from "../../../love/public/src/text_get.mjs";
-import { string_index_last } from "../../../love/public/src/string_index_last.mjs";
+import { text_index_last } from "../../../love/public/src/text_index_last.mjs";
 export function string_last(s) {
-  let index_last = string_index_last(s);
+  let index_last = text_index_last(s);
   let item = text_get(s, index_last);
   return item;
 }
