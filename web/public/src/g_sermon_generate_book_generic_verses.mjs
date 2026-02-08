27f3c02612e4cf2f1a15db92d730f44096f70dc9
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 08:59:25 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_verses.mjs

--- web/public/src/g_sermon_generate_book_generic_verses.mjs
+++ web/public/src/g_sermon_generate_book_generic_verses.mjs
@@ -1,19 +1,19 @@
+import { ebible_chapters_or_specified } from "../../../love/public/src/ebible_chapters_or_specified.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { ebible_chapters } from "../../../love/public/src/ebible_chapters.mjs";
 export async function g_sermon_generate_book_generic_verses(
   book_code,
   chapter_code_specified,
   bible_folders,
 ) {
   async function map_folder(bible_folder) {
-    let chapters = await ebible_chapters(bible_folder, book_code);
-    if (null_not_is(chapter_code_specified)) {
-      chapters = [chapter_code_specified];
-    }
+    let chapters = await ebible_chapters_or_specified(
+      bible_folder,
+      book_code,
+      chapter_code_specified,
+    );
     async function map_chapter(chapter_code) {
       let verses = await ebible_verses(bible_folder, chapter_code);
       function each_verse(v) {
