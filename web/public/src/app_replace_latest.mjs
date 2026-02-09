1f1567d281fbbdb25c44efc74bccb5199d42be43
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 18:52:51 -0500

    function_auto_path ..\love\public\src\app_replace_latest.mjs

--- web/public/src/app_replace_latest.mjs
+++ web/public/src/app_replace_latest.mjs
@@ -1,1 +1,4 @@
-export function app_replace_latest() {}
+import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
+export async function app_replace_latest() {
+  await app_main_latest("app_replace_main", "jared-grace");
+}
