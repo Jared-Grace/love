27cbaee59c60a86440e312212dcd4d7901104e66
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:36:27 -0500

    function_auto_path ..\love\public\src\app_a_function_node.mjs

--- public/src/data_strings_get.mjs
+++
@@ -1,5 +0,0 @@
-import { data_property_get } from "../../../love/public/src/data_property_get.mjs";
-export async function data_strings_get() {
-  let v = await data_property_get("strings");
-  return v;
-}
