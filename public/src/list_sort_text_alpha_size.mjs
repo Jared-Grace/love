0fbdc18ae12073d73cfe0b284112c851ca2c58e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:38:42 -0500

    function_auto_path ..\love\public\src\list_sort_text_alpha_size.mjs

--- public/src/list_sort_text_alpha_size.mjs
+++ public/src/list_sort_text_alpha_size.mjs
@@ -1,6 +1,6 @@
-import { list_sort_string_size } from "../../../love/public/src/list_sort_string_size.mjs";
+import { list_sort_text_size } from "../../../love/public/src/list_sort_text_size.mjs";
 import { list_sort_text } from "../../../love/public/src/list_sort_text.mjs";
 export function list_sort_text_alpha_size(f_names) {
   list_sort_text(f_names);
-  list_sort_string_size(f_names);
+  list_sort_text_size(f_names);
 }
