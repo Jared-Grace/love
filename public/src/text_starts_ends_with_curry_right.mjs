2212c036345ecd4982bb02f73e971ec87dab6db0
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:17:55 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_ends_with_to_empty.mjs

--- public/src/text_starts_ends_with_curry_right.mjs
+++ public/src/text_starts_ends_with_curry_right.mjs
@@ -1,4 +1,4 @@
-import { function_multiple_rename_if_starts_ends_with } from "../../../love/public/src/function_multiple_rename_if_starts_ends_with.mjs";
+import { functions_rename_if_starts_ends_with } from "../../../love/public/src/functions_rename_if_starts_ends_with.mjs";
 import { text_is_assert_multiple } from "../../../love/public/src/text_is_assert_multiple.mjs";
 import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { text_starts_ends_with } from "../../../love/public/src/text_starts_ends_with.mjs";
@@ -7,7 +7,7 @@ export function text_starts_ends_with_curry_right(
   f_name_suffix_before,
 ) {
   "asserts in this fn are to make sure " +
-    function_multiple_rename_if_starts_ends_with +
+    functions_rename_if_starts_ends_with +
     " is ran correctly";
   text_is_assert_multiple([f_name_prefix, f_name_suffix_before]);
   let r2 = function text_starts_ends_with_curry_right_result(f_name) {
