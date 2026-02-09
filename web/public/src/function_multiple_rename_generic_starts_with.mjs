7d5e0af6b9d1c76e24ebf0f0d32dff542aad2ca4
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:17:39 -0500

    function_auto_path ..\love\public\src\functions_rename_generic_starts_with.mjs

--- web/public/src/function_multiple_rename_generic_starts_with.mjs
+++
@@ -1,9 +0,0 @@
-import { text_starts_with_curry_right } from "../../../love/public/src/text_starts_with_curry_right.mjs";
-import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
-export async function function_multiple_rename_generic_starts_with(
-  name_change,
-  f_name_prefix,
-) {
-  let filter = text_starts_with_curry_right(f_name_prefix);
-  await functions_rename_generic(filter, name_change);
-}
