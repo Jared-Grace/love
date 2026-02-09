37e13d8f71a6ed1836c288143ff5287fc0407df4
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 12:55:51 -0500

    function_auto_path ..\love\public\src\text_starts_ends_with.mjs

--- public/src/text_starts_ends_with.mjs
+++ public/src/text_starts_ends_with.mjs
@@ -1,4 +1,7 @@
+import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-export function text_starts_ends_with(t) {
+export function text_starts_ends_with(t, prefix, suffix) {
   let sw = text_starts_with(t, prefix);
+  let ew = text_ends_with(item, suffix);
+  return sw && ew;
 }
