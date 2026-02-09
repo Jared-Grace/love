785d5fec6ef58285a41f9d801b49ed101df86f8e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:31:02 -0500

    function_auto_path ..\love\public\src\text_to_uuid.mjs

--- web/public/src/text_to_uuid.mjs
+++ web/public/src/text_to_uuid.mjs
@@ -1,3 +1,4 @@
+import { text_to_uuid_get } from "../../../love/public/src/text_to_uuid_get.mjs";
 import { text_to_uuid_set } from "../../../love/public/src/text_to_uuid_set.mjs";
 import { text_to_uuid_set_exists } from "../../../love/public/src/text_to_uuid_set_exists.mjs";
 import { null_get } from "../../../love/public/src/null_get.mjs";
@@ -7,7 +8,7 @@ export async function text_to_uuid(text) {
   let key_get = lambda_get(text);
   let value_get = null_get();
   let cached_exists = text_to_uuid_set_exists;
-  let cached_get = text_to_uuid_set;
+  let cached_get = text_to_uuid_get;
   let cache_save = async function lambda4(key, value) {
     await text_to_uuid_set(key);
   };
