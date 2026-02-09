5dcae4d75ddc04ad484a560a9ec76b79dccb00e9
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:17:27 -0500

    function_auto_path ..\love\public\src\functions_rename_generic.mjs

--- web/public/src/function_multiple_rename_generic_starts_with.mjs
+++ web/public/src/function_multiple_rename_generic_starts_with.mjs
@@ -1,9 +1,9 @@
 import { text_starts_with_curry_right } from "../../../love/public/src/text_starts_with_curry_right.mjs";
-import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
+import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
 export async function function_multiple_rename_generic_starts_with(
   name_change,
   f_name_prefix,
 ) {
   let filter = text_starts_with_curry_right(f_name_prefix);
-  await function_multiple_rename_generic(filter, name_change);
+  await functions_rename_generic(filter, name_change);
 }
--- web/public/src/functions_rename_generic.mjs
+++ web/public/src/functions_rename_generic.mjs
@@ -4,7 +4,7 @@ import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_empty_not_is_assert } from "../../../love/public/src/list_empty_not_is_assert.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
-export async function function_multiple_rename_generic(filter, name_change) {
+export async function functions_rename_generic(filter, name_change) {
   let f_names = await functions_names();
   let filtered = list_filter(f_names, filter);
   list_empty_not_is_assert(filtered);
