ef37deb72f618a177816d34d9853be7dbff9a033
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:10:35 -0500

    function_auto_path ..\love\public\src\text_first_transform.mjs

--- public/src/text_first_transform.mjs
+++ public/src/text_first_transform.mjs
@@ -1,4 +1,3 @@
-import { function_copy_replace_first } from "../../../love/public/src/function_copy_replace_first.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
@@ -7,8 +6,8 @@ export function text_first_transform(s, transform) {
   let split = text_split_empty(s);
   let r = list_first_remaining(split);
   let remaining = object_property_get(r, "remaining");
-  let function_copy_replace_first = object_property_get(r, "first");
-  let lower = transform(function_copy_replace_first);
+  let first = object_property_get(r, "first");
+  let lower = transform(first);
   let joined = list_join_empty(remaining);
   let v = "" + lower + joined;
   return v;
