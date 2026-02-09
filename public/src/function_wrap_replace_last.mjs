1856cc88d14c288de81de73e165ab14e94f44b4d
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:04:20 -0500

    function_auto_path ..\love\public\src\function_wrap_replace_last.mjs

--- public/src/function_wrap_replace_last.mjs
+++ public/src/function_wrap_replace_last.mjs
@@ -1,4 +1,5 @@
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_replace_last(f_name, f_name_wrapped) {
-  return await function_wrap(f_name, f_name_wrapped);
+  let r = await function_wrap(f_name, f_name_wrapped);
+  return r;
 }
