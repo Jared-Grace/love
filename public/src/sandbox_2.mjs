d985d24718b8e9c8b3d45fddbf3a933c94a5998b
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 12:41:20 -0500

    function_auto_path ..\love\public\src\sandbox_2.mjs

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,6 +1,1 @@
-import { ebible_text_to_speech_chapter } from "../../../love/public/src/ebible_text_to_speech_chapter.mjs";
-export async function sandbox_2() {
-  const bible_folder = "engwebu";
-  const chapter_code = "SIR01";
-  await ebible_text_to_speech_chapter(bible_folder, chapter_code);
-}
+export async function sandbox_2() {}
