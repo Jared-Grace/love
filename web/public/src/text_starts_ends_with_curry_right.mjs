cce2f589e5b20ddc2bd616752258c6ff097803cf
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 12:59:02 -0500

    function_auto_path ..\love\public\src\text_starts_ends_with_curry_right.mjs

--- web/public/src/text_starts_ends_with_curry_right.mjs
+++ web/public/src/text_starts_ends_with_curry_right.mjs
@@ -4,7 +4,7 @@ export function text_starts_ends_with_curry_right(
   f_name_suffix_before,
 ) {
   let r2 = function text_starts_ends_with_curry_right_result(f_name) {
-    sia;
+    tia;
     let sew = text_starts_ends_with(
       f_name,
       f_name_prefix,
