da6c5f2d552791dd35bb38ae240d9e335778e0f7
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:22:52 -0500

    function_auto_path ..\love\public\src\text_to_uuid_save_initialize.mjs

--- web/public/src/text_to_uuid_save_initialize.mjs
+++ web/public/src/text_to_uuid_save_initialize.mjs
@@ -1,5 +1,5 @@
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
 export function text_to_uuid_save_initialize(data) {
-  let value = object_property_initialize(data, "set", {});
+  let value = object_property_initialize(data, "save", {});
   return value;
 }
