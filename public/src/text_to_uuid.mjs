b7f794f1ae2ad833851b0aaf29442f7f1402ba13
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:22:19 -0500

    function_auto_path ..\love\public\src\text_to_uuid.mjs

--- public/src/text_to_uuid.mjs
+++ public/src/text_to_uuid.mjs
@@ -1,13 +1,13 @@
 import { text_to_uuid_set_get } from "../../../love/public/src/text_to_uuid_set_get.mjs";
 import { text_to_uuid_save } from "../../../love/public/src/text_to_uuid_save.mjs";
-import { text_to_uuid_set_exists } from "../../../love/public/src/text_to_uuid_set_exists.mjs";
+import { text_to_uuid_save_exists } from "../../../love/public/src/text_to_uuid_save_exists.mjs";
 import { null_get } from "../../../love/public/src/null_get.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 export async function text_to_uuid(text) {
   let key_get = lambda_get(text);
   let value_get = null_get();
-  let cached_exists = text_to_uuid_set_exists;
+  let cached_exists = text_to_uuid_save_exists;
   let cached_get = text_to_uuid_set_get;
   let cache_save = async function lambda4(key, value) {
     await text_to_uuid_save(key);
