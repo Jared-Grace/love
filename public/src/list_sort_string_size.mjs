bd7e2b0f9d505070a31f127e37261bba7abaa601
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:38:42 -0500

    function_auto_path ..\love\public\src\list_sort_text_alpha_size.mjs

--- public/src/list_sort_string_size.mjs
+++
@@ -1,5 +0,0 @@
-import { text_size } from "../../../love/public/src/text_size.mjs";
-import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
-export function list_sort_string_size(list) {
-  list_sort_number_mapper(list, text_size);
-}
