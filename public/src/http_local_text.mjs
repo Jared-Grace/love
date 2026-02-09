f826191fc1b2e36c4aeace028d4b8d425a27d88c
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:36:22 -0500

    function_auto_path ..\love\public\src\buffer_to_json.mjs

--- public/src/http_local_text.mjs
+++ public/src/http_local_text.mjs
@@ -1,7 +1,7 @@
-import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
+import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
 export async function http_local_text(url) {
   let buffer = await http_local(url);
-  const text = buffer_string_to(buffer);
+  const text = buffer_text_to(buffer);
   return text;
 }
