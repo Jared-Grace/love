62bfb30089fe234dd4214ac4a37bb8219095eef1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:37:19 -0500

    function_auto_path ..\love\public\src\invoke_cache_text_to_uuid.mjs

--- public/src/invoke_cache_text_to_uuid.mjs
+++ public/src/invoke_cache_text_to_uuid.mjs
@@ -1,5 +1,5 @@
+import { text_to_uuid_set_exists } from "../../../love/public/src/text_to_uuid_set_exists.mjs";
 import { text_to_uuid_get } from "../../../love/public/src/text_to_uuid_get.mjs";
-import { text_to_uuid_get_exists } from "../../../love/public/src/text_to_uuid_get_exists.mjs";
 import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
 import { invoke_cache_value_get } from "../../../love/public/src/invoke_cache_value_get.mjs";
@@ -8,7 +8,7 @@ import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache_text_to_uuid(fn, args, db_get, store) {
   let key_get = invoke_cache_key_get(fn, args);
   let value_get = invoke_cache_value_get(fn, args);
-  let cached_exists = text_to_uuid_get_exists;
+  let cached_exists = text_to_uuid_set_exists;
   let cached_get = async function lambda2(key) {
     let item = await text_to_uuid_get(key);
     return item;
