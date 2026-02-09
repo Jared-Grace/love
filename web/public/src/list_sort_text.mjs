da5969bd336455bd0f85c2a7e8063788ef9c405a
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:38:27 -0500

    function_auto_path ..\love\public\src\list_sort_string_property.mjs

--- web/public/src/list_sort_text.mjs
+++ web/public/src/list_sort_text.mjs
@@ -1,5 +1,5 @@
 import { identity } from "../../../love/public/src/identity.mjs";
-import { list_sort_string_mapper } from "../../../love/public/src/list_sort_string_mapper.mjs";
+import { list_sort_text_mapper } from "../../../love/public/src/list_sort_text_mapper.mjs";
 export function list_sort_text(list) {
-  list_sort_string_mapper(list, identity);
+  list_sort_text_mapper(list, identity);
 }
