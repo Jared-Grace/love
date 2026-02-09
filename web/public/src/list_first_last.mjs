7df462a406080cbe9e2f5e7d657d143ee5e97696
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:09:14 -0500

    function_auto_path ..\love\public\src\list_first_last.mjs

--- web/public/src/list_first_last.mjs
+++ web/public/src/list_first_last.mjs
@@ -3,8 +3,8 @@ import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function list_first_last(list) {
   list_multiple_is_assert(list);
-  let function_copy_replace_first = list_first(list);
+  let first = list_first(list);
   let last = list_last(list);
-  let fl = [function_copy_replace_first, last];
+  let fl = [first, last];
   return fl;
 }
