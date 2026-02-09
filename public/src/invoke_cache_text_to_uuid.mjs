1972ac4edcef5273bfb9cfcd5919817757bc2e75
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:58:45 -0500

    function_auto_path ..\love\public\src\invoke_cache_text_to_uuid.mjs

--- public/src/invoke_cache_text_to_uuid.mjs
+++ public/src/invoke_cache_text_to_uuid.mjs
@@ -1,19 +1,19 @@
+import { invoke_cache_generic } from "../../../love/public/src/invoke_cache_generic.mjs";
 import { text_to_uuid_set } from "../../../love/public/src/text_to_uuid_set.mjs";
 import { text_to_uuid_set_exists } from "../../../love/public/src/text_to_uuid_set_exists.mjs";
 import { text_to_uuid_get } from "../../../love/public/src/text_to_uuid_get.mjs";
-import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache_text_to_uuid(fn, args) {
   let cached_exists = text_to_uuid_set_exists;
   let cached_get = text_to_uuid_get;
   let cache_save = async function lambda4(key, text) {
     await text_to_uuid_set(text);
   };
-    let r = await invoke_cache_generic(
-      fn,
-      args,
-      cached_exists,
-      cached_get,
-      cache_save,
-    );
-    return r;
+  let r = await invoke_cache_generic(
+    fn,
+    args,
+    cached_exists,
+    cached_get,
+    cache_save,
+  );
+  return r;
 }
