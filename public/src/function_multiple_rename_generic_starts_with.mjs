b994cc96b1ff669b71cc9bb9c08c18114be2b009
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 12:50:45 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_generic_starts_with.mjs

--- public/src/function_multiple_rename_generic_starts_with.mjs
+++ public/src/function_multiple_rename_generic_starts_with.mjs
@@ -4,8 +4,8 @@ export async function function_multiple_rename_generic_starts_with(
   name_change,
   f_name_prefix,
 ) {
-  await function_multiple_rename_generic(filter, name_change);
   function filter(f_name) {
     text_starts_with(f_name, f_name_prefix);
   }
+  await function_multiple_rename_generic(filter, name_change);
 }
