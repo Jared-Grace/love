b59c288cd059a1295d8adeed72eadd1dc0168b97
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:50:32 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,4 +1,4 @@
-import { kjv } from "../../../love/public/src/kjv.mjs";
+import { ebible_verses } from "../../../love/public/src/ebible_verses.mjs";
 export async function sandbox_5() {
-  let verses = kjv();
+  let verses = await ebible_verses(bible_folder, chapter_code);
 }
