679f87c4e0d05ef84e69cb7c1608fa676e57a17a
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:38:06 -0500

    function_auto_path ..\love\public\src\list_sort_text.mjs

--- public/src/function_types.mjs
+++ public/src/function_types.mjs
@@ -1,11 +1,11 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
+import { list_sort_text } from "../../../love/public/src/list_sort_text.mjs";
 import { js_node_types } from "../../../love/public/src/js_node_types.mjs";
 import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 export async function function_types(f_name) {
   let v = await function_parse_unaliased(f_name);
   let ast = object_property_get(v, "ast");
   const f_types = js_node_types(ast);
-  list_sort_string(f_types);
+  list_sort_text(f_types);
   return f_types;
 }
--- public/src/list_sort_string.mjs
+++
@@ -1,5 +0,0 @@
-import { identity } from "../../../love/public/src/identity.mjs";
-import { list_sort_string_mapper } from "../../../love/public/src/list_sort_string_mapper.mjs";
-export function list_sort_string(list) {
-  list_sort_string_mapper(list, identity);
-}
