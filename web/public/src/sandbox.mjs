6d85c1d95532837c8dccffb09371cd620c8fc973
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 16:54:06 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 export async function sandbox() {
   let p = folder_user_docs_path(chapter_code_specified);
-  let data = await file_read_json(file_path);
+  let data = await file_read_json(p);
+  log({
+    data,
+  });
 }
