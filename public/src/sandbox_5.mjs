0091184a34bb5ca8a7c47d509f1da31dfb05b957
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:35:03 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,1 +1,47 @@
-export async function sandbox_5() {}
+import { marker_assign_replace_string } from "../../../love/public/src/marker_assign_replace_string.mjs";
+import { list_sort_string_size } from "../../../love/public/src/list_sort_string_size.mjs";
+import { list_sort_string_property } from "../../../love/public/src/list_sort_string_property.mjs";
+import { list_sort_string_mapper } from "../../../love/public/src/list_sort_string_mapper.mjs";
+import { list_sort_string_alpha_size } from "../../../love/public/src/list_sort_string_alpha_size.mjs";
+import { list_sort_string } from "../../../love/public/src/list_sort_string.mjs";
+import { list_map_filter_string_empty_not_is } from "../../../love/public/src/list_map_filter_string_empty_not_is.mjs";
+import { js_symbol_string_template } from "../../../love/public/src/js_symbol_string_template.mjs";
+import { js_strings } from "../../../love/public/src/js_strings.mjs";
+import { js_string } from "../../../love/public/src/js_string.mjs";
+import { js_expression_string } from "../../../love/public/src/js_expression_string.mjs";
+import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
+import { firebase_upload_string_generic } from "../../../love/public/src/firebase_upload_string_generic.mjs";
+import { firebase_upload_string } from "../../../love/public/src/firebase_upload_string.mjs";
+import { firebase_storage_download_string } from "../../../love/public/src/firebase_storage_download_string.mjs";
+import { digits_string } from "../../../love/public/src/digits_string.mjs";
+import { data_strings_search } from "../../../love/public/src/data_strings_search.mjs";
+import { data_strings_get } from "../../../love/public/src/data_strings_get.mjs";
+import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
+import { app_a_symbol_string_template } from "../../../love/public/src/app_a_symbol_string_template.mjs";
+export async function sandbox_5() {
+  let l = {
+    app_a_symbol_string_template: "public/src/app_a_symbol_string_template.mjs",
+    buffer_string_to: "public/src/buffer_string_to.mjs",
+    data_strings_get: "public/src/data_strings_get.mjs",
+    data_strings_search: "public/src/data_strings_search.mjs",
+    digits_string: "public/src/digits_string.mjs",
+    firebase_storage_download_string:
+      "public/src/firebase_storage_download_string.mjs",
+    firebase_upload_string: "public/src/firebase_upload_string.mjs",
+    firebase_upload_string_generic:
+      "public/src/firebase_upload_string_generic.mjs",
+    js_code_string: "public/src/js_code_string.mjs",
+    js_expression_string: "public/src/js_expression_string.mjs",
+    js_string: "public/src/js_string.mjs",
+    js_strings: "public/src/js_strings.mjs",
+    js_symbol_string_template: "public/src/js_symbol_string_template.mjs",
+    list_map_filter_string_empty_not_is:
+      "public/src/list_map_filter_string_empty_not_is.mjs",
+    list_sort_string: "public/src/list_sort_string.mjs",
+    list_sort_string_alpha_size: "public/src/list_sort_string_alpha_size.mjs",
+    list_sort_string_mapper: "public/src/list_sort_string_mapper.mjs",
+    list_sort_string_property: "public/src/list_sort_string_property.mjs",
+    list_sort_string_size: "public/src/list_sort_string_size.mjs",
+    marker_assign_replace_string: "public/src/marker_assign_replace_string.mjs",
+  };
+}
