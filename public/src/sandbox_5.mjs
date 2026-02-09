fbe45e35391876fa2b53f1399b4a89a0b538028a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:51:10 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -2,5 +2,5 @@ import { openai_responses_cache } from "../../../love/public/src/openai_response
 import { ebible_chapter_text } from "./ebible_chapter_text.mjs";
 export async function sandbox_5() {
   let t = await ebible_chapter_text("engbsb", "GEN01");
-  let r = await openai_responses_cache(system, user);
+  let r = await openai_responses_cache("", t);
 }
