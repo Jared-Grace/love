0806eea2e7d7b1910313273093e92ae5a4499ff2
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:09:39 -0500

    function_auto_path ..\love\public\src\list_first.mjs

--- public/src/list_first.mjs
+++ public/src/list_first.mjs
@@ -1,7 +1,6 @@
-import { function_copy_replace_first } from "../../../love/public/src/function_copy_replace_first.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 export function list_first(list) {
   const index = 0;
-  const function_copy_replace_first = list_get(list, index);
-  return function_copy_replace_first;
+  const first = list_get(list, index);
+  return first;
 }
