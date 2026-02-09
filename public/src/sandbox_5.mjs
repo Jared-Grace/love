ed763be42787ac8716441640b4c521709f2b1905
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:13:05 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,8 +1,1 @@
-import { curry } from "../../../love/public/src/curry.mjs";
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
-export async function sandbox_5() {
-  let r = curry(text_combine);
-  let r2 = r("a");
-  let r3 = r2("b");
-  return r3;
-}
+export async function sandbox_5() {}
