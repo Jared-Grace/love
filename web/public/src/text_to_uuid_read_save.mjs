bb315840edf15bba2b8a68063f771b18729ae986
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:22:29 -0500

    function_auto_path ..\love\public\src\text_to_uuid_save_initialize.mjs

--- web/public/src/text_to_uuid_read_save.mjs
+++ web/public/src/text_to_uuid_read_save.mjs
@@ -1,7 +1,7 @@
-import { text_to_uuid_set_initialize } from "../../../love/public/src/text_to_uuid_set_initialize.mjs";
+import { text_to_uuid_save_initialize } from "../../../love/public/src/text_to_uuid_save_initialize.mjs";
 import { text_to_uuid_read } from "../../../love/public/src/text_to_uuid_read.mjs";
 export async function text_to_uuid_read_save() {
   let data = await text_to_uuid_read();
-  let set = text_to_uuid_set_initialize(data);
+  let set = text_to_uuid_save_initialize(data);
   return set;
 }
