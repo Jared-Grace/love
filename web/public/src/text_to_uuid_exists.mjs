bdf84d132cab7acc1f8623ce8351d5fad86c3556
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:09:06 -0500

    crl text_to_uuid_get exists

---
+++ web/public/src/text_to_uuid_exists.mjs
@@ -0,0 +1,9 @@
+import { text_to_uuid_read } from "../../../love/public/src/text_to_uuid_read.mjs";
+import { text_to_uuid_get_initialize } from "../../../love/public/src/text_to_uuid_get_initialize.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export async function text_to_uuid_exists(id) {
+  let data = await text_to_uuid_read();
+  let get = text_to_uuid_get_initialize(data);
+  let text = object_property_get(get, id);
+  return text;
+}
