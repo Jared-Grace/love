fcf37dcd6c3ac499b05efdc2fe0bca2a54772643
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:17:46 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_if_starts_with.mjs

--- public/src/function_multiple_rename_if_starts_with_replace.mjs
+++ public/src/function_multiple_rename_if_starts_with_replace.mjs
@@ -1,4 +1,4 @@
-import { function_multiple_rename_generic_starts_with } from "../../../love/public/src/function_multiple_rename_generic_starts_with.mjs";
+import { functions_rename_generic_starts_with } from "../../../love/public/src/functions_rename_generic_starts_with.mjs";
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export async function function_multiple_rename_if_starts_with_replace(
@@ -7,10 +7,7 @@ export async function function_multiple_rename_if_starts_with_replace(
   to,
 ) {
   assert_arguments(arguments, 3);
-  await function_multiple_rename_generic_starts_with(
-    name_change,
-    f_name_prefix,
-  );
+  await functions_rename_generic_starts_with(name_change, f_name_prefix);
   function name_change(f_name_before) {
     let f_name_after = text_replace(f_name_before, from, to);
     return f_name_after;
