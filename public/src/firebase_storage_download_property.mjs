cc9e2c1cd86dd65f7fb711caf54ce94a6f199bd8
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:36:11 -0500

    function_auto_path ..\love\public\src\buffer_text_to.mjs

--- public/src/firebase_storage_download_property.mjs
+++ public/src/firebase_storage_download_property.mjs
@@ -1,5 +1,5 @@
 import { json_from } from "../../../love/public/src/json_from.mjs";
-import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
+import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { firebase_storage_download } from "../../../love/public/src/firebase_storage_download.mjs";
 export async function firebase_storage_download_property(
@@ -7,7 +7,7 @@ export async function firebase_storage_download_property(
   property_name,
 ) {
   let buffer = await firebase_storage_download(storage_path);
-  const json = buffer_string_to(buffer);
+  const json = buffer_text_to(buffer);
   let o = json_from(json);
   let value = object_property_get(o, property_name);
   return value;
