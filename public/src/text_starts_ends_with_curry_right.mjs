8dfd1c52b2d425b19ed630a1225fa43815f3ded4
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:00:44 -0500

    function_auto_path ..\love\public\src\text_starts_ends_with_curry_right.mjs

--- public/src/text_starts_ends_with_curry_right.mjs
+++ public/src/text_starts_ends_with_curry_right.mjs
@@ -1,3 +1,4 @@
+import { function_multiple_rename_if_starts_ends_with } from "../../../love/public/src/function_multiple_rename_if_starts_ends_with.mjs";
 import { text_is_assert_multiple } from "../../../love/public/src/text_is_assert_multiple.mjs";
 import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { text_starts_ends_with } from "../../../love/public/src/text_starts_ends_with.mjs";
@@ -5,7 +6,9 @@ export function text_starts_ends_with_curry_right(
   f_name_prefix,
   f_name_suffix_before,
 ) {
-  "asserts here are to make sure ";
+  "asserts here are to make sure " +
+    function_multiple_rename_if_starts_ends_with +
+    " is ran correctly";
   text_is_assert_multiple([f_name_prefix, f_name_suffix_before]);
   let r2 = function text_starts_ends_with_curry_right_result(f_name) {
     text_is_assert(f_name);
