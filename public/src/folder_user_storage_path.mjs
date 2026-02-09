b1cf4c4f88bfc3f4bff40534c5b5951a9379bf04
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:14:19 -0500

    function_auto_path ..\love\public\src\folder_user_storage_path.mjs

--- public/src/folder_user_storage_path.mjs
+++ public/src/folder_user_storage_path.mjs
@@ -1,6 +1,6 @@
 import { folder_user_combine } from "../../../love/public/src/folder_user_combine.mjs";
-export function folder_user_storage_path(destination) {
+export function folder_user_storage_path(path) {
   const folder = "storage";
-  let joined = folder_user_combine(folder, destination);
+  let joined = folder_user_combine(folder, path);
   return joined;
 }
