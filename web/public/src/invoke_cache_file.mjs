03d60ad82a666f5181e481ee91d2635970555e84
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:06:18 -0500

    function_auto_path ..\love\public\src\invoke_cache_file.mjs

--- web/public/src/invoke_cache_file.mjs
+++ web/public/src/invoke_cache_file.mjs
@@ -1,9 +1,13 @@
-import { invoke_cache_generic } from "../../../love/public/src/invoke_cache_generic.mjs";
+import { invoke_cache_file_key_get } from "../../../love/public/src/invoke_cache_file_key_get.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
+import { invoke_cache_value_get } from "../../../love/public/src/invoke_cache_value_get.mjs";
+import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache_file(fn, args) {
+  let key_get = invoke_cache_file_key_get(fn, args);
+  let value_get = invoke_cache_value_get(fn, args);
   let cached_exists = file_exists;
   let cached_get = async function lambda2(key) {
     let data = await file_read_json(key);
@@ -15,12 +19,12 @@ export async function invoke_cache_file(fn, args) {
       result,
     });
   };
-  let r = await invoke_cache_generic(
-    fn,
-    args,
+  let v = await cache_generic(
+    key_get,
     cached_exists,
     cached_get,
+    value_get,
     cache_save,
   );
-  return r;
+  return v;
 }
