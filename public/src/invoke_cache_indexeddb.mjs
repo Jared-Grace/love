a6dfc8a2c2982507544340e20548005bf0518915
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:58:26 -0500

    function_auto_path ..\love\public\src\invoke_cache_indexeddb.mjs

--- public/src/invoke_cache_indexeddb.mjs
+++ public/src/invoke_cache_indexeddb.mjs
@@ -1,10 +1,8 @@
+import { invoke_cache_generic } from "../../../love/public/src/invoke_cache_generic.mjs";
 import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 import { indexeddb_put } from "../../../love/public/src/indexeddb_put.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { indexeddb_get } from "../../../love/public/src/indexeddb_get.mjs";
-import { invoke_cache_value_get } from "../../../love/public/src/invoke_cache_value_get.mjs";
-import { invoke_cache_key_get } from "../../../love/public/src/invoke_cache_key_get.mjs";
-import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache_indexeddb(fn, args, db_get, store) {
   let cached_exists = async function lambda3(key) {
     let item = await indexeddb_get(db_get, store, key);
@@ -19,12 +17,12 @@ export async function invoke_cache_indexeddb(fn, args, db_get, store) {
     let value_get2 = lambda_get(value);
     await indexeddb_put(db_get, store, key, value_get2);
   };
-    let r = await invoke_cache_generic(
-      fn,
-      args,
-      cached_exists,
-      cached_get,
-      cache_save,
-    );
-    return r;
+  let r = await invoke_cache_generic(
+    fn,
+    args,
+    cached_exists,
+    cached_get,
+    cache_save,
+  );
+  return r;
 }
