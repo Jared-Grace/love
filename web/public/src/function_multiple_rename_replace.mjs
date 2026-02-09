4c4c9b4a03a40964d40d2e9398b1cf339f56a266
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:09:40 -0500

    p

---
+++ web/public/src/function_multiple_rename_replace.mjs
@@ -0,0 +1,4 @@
+import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
+export async function function_multiple_rename_replace(filter, name_change) {
+  return await function_multiple_rename_generic(filter, name_change);
+}
