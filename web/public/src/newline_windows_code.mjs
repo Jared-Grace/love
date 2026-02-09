a077adb4482ae90475199e8391da25ce6afcb251
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:37:22 -0500

    function_auto_path ..\love\public\src\function_name_to_path_import.mjs

--- web/public/src/newline_windows_code.mjs
+++ web/public/src/newline_windows_code.mjs
@@ -1,7 +1,7 @@
-import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
+import { js_code_text } from "../../../love/public/src/js_code_text.mjs";
 import { newline_windows } from "../../../love/public/src/newline_windows.mjs";
 export function newline_windows_code() {
   let n = newline_windows();
-  let code_string = js_code_string(n);
+  let code_string = js_code_text(n);
   return code_string;
 }
