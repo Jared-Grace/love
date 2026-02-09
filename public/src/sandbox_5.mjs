647f2b4c003a9e0c0c0c7d12079b99d9130c1a1e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:13:11 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,1 +1,4 @@
-export async function sandbox_5() {}
+import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
+export async function sandbox_5() {
+  let joined = folder_user_storage_function_path(fn);
+}
