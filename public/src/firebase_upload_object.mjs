607caefb0354bcfd44e6e76de94e32a507e1501f
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:37:11 -0500

    function_auto_path ..\love\public\src\firebase_upload_object.mjs

--- public/src/firebase_upload_object.mjs
+++ public/src/firebase_upload_object.mjs
@@ -1,7 +1,7 @@
-import { firebase_upload_string_generic } from "../../../love/public/src/firebase_upload_string_generic.mjs";
+import { firebase_upload_text_generic } from "../../../love/public/src/firebase_upload_text_generic.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export async function firebase_upload_object(destination, object) {
   let content = json_to(object);
   const content_type = "application/json";
-  await firebase_upload_string_generic(content, destination, content_type);
+  await firebase_upload_text_generic(content, destination, content_type);
 }
