878a7a3a852da7a65176419054ff92c05460228e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:30:57 -0500

    function_auto_path ..\love\public\src\text_to_uuid_cache.mjs

--- public/src/text_to_uuid_cache.mjs
+++ public/src/text_to_uuid_cache.mjs
@@ -4,7 +4,7 @@ import { text_to_uuid_save_exists } from "../../../love/public/src/text_to_uuid_
 import { null_get } from "../../../love/public/src/null_get.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
-export async function text_to_uuid(text) {
+export async function text_to_uuid_cache(text) {
   let key_get = lambda_get(text);
   let value_get = null_get();
   let cached_exists = text_to_uuid_save_exists;
