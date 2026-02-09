292ca54ae32234efc0b3f74d2e444e67b371e19a
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:38:42 -0500

    function_auto_path ..\love\public\src\list_sort_text_alpha_size.mjs

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -2,13 +2,13 @@ import { object_property_get } from "../../../love/public/src/object_property_ge
 import { js_identifier_is_assert } from "../../../love/public/src/js_identifier_is_assert.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { undefined_not_is_assert } from "../../../love/public/src/undefined_not_is_assert.mjs";
-import { list_sort_string_size } from "../../../love/public/src/list_sort_string_size.mjs";
+import { list_sort_text_size } from "../../../love/public/src/list_sort_text_size.mjs";
 import { function_alias_generic } from "../../../love/public/src/function_alias_generic.mjs";
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 export async function function_alias_add(first, second) {
   let list = [first, second];
-  list_sort_string_size(list);
+  list_sort_text_size(list);
   let [alias, f_name] = list;
   let expression = js_parse_expression(f_name);
   js_identifier_is_assert(expression);
