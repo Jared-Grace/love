9ee61fbb569d62948bab220ec9c745b9a4783c2d
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 16:54:59 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,8 +6,8 @@ import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs
 export async function sandbox() {
   let chapter_code_specified =
     app_original_bible_gloss_generate_chapter_code_specified();
-  let file_name = file_name_json(name);
-  let p = folder_user_docs_path(chapter_code_specified);
+  let file_name = file_name_json(chapter_code_specified);
+  let p = folder_user_docs_path(file_name);
   let data = await file_read_json(p);
   log({
     data,
