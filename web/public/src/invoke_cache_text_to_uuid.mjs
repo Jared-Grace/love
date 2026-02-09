34f55acc1d3ea6f1392ff776abf1f0c377d07111
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:43:50 -0500

    function_auto_path ..\love\public\src\invoke_cache_text_to_uuid.mjs

--- web/public/src/invoke_cache_text_to_uuid.mjs
+++ web/public/src/invoke_cache_text_to_uuid.mjs
@@ -1,5 +1,4 @@
 import { null_get } from "../../../love/public/src/null_get.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { text_to_uuid_set } from "../../../love/public/src/text_to_uuid_set.mjs";
 import { text_to_uuid_set_exists } from "../../../love/public/src/text_to_uuid_set_exists.mjs";
 import { text_to_uuid_get } from "../../../love/public/src/text_to_uuid_get.mjs";
@@ -8,17 +7,9 @@ export async function invoke_cache_text_to_uuid(text) {
   let key_get = value_get(text);
   let value_get = null_get(value_get);
   let cached_exists = text_to_uuid_set_exists;
-  let cached_get = async function lambda2(key) {
-    let item = await text_to_uuid_get(key);
-    return item;
-  };
+  let cached_get = text_to_uuid_get;
   let cache_save = async function lambda4(key, value) {
-    let o = {
-      key,
-      value,
-    };
-    let json = json_to(o);
-    await text_to_uuid_set(json);
+    await text_to_uuid_set(key);
   };
   let result = await cache_generic(
     key_get,
