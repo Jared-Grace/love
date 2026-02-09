6e936a767dda9f6bf0c89587f92413d1164bc391
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:35:25 -0500

    function_auto_path ..\love\public\src\text_to_uuid_get_exists.mjs

--- public/src/text_to_uuid_get_exists.mjs
+++ public/src/text_to_uuid_get_exists.mjs
@@ -1,7 +1,7 @@
-import { text_to_uuid_read_set } from "../../../love/public/src/text_to_uuid_read_set.mjs";
+import { text_to_uuid_read_get } from "../../../love/public/src/text_to_uuid_read_get.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
 export async function text_to_uuid_get_exists(id) {
-  let get = await text_to_uuid_read_set();
+  let get = await text_to_uuid_read_get();
   let text = object_property_exists(get, id);
   return text;
 }
