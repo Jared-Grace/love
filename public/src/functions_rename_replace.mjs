89dea6c99dfa6361c11ebd4d8bfb79a1c99eb7ab
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:18:40 -0500

    function_auto_path ..\love\public\src\functions_rename_replace.mjs

--- public/src/functions_rename_replace.mjs
+++ public/src/functions_rename_replace.mjs
@@ -2,7 +2,7 @@ import { text_replace_curry_right } from "../../../love/public/src/text_replace_
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { tautology } from "../../../love/public/src/tautology.mjs";
 import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
-export async function function_multiple_rename_replace(from, to) {
+export async function functions_rename_replace(from, to) {
   assert_arguments(arguments, 2);
   let mapper = text_replace_curry_right(from, to);
   let r = await functions_rename_generic(tautology, mapper);
