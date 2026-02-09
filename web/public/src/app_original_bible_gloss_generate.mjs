449ddcbf098ff1371719c2be67057caed3022ebe
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 16:51:30 -0500

    function_auto_path ..\love\public\src\app_original_bible_gloss_generate.mjs

--- web/public/src/app_original_bible_gloss_generate.mjs
+++ web/public/src/app_original_bible_gloss_generate.mjs
@@ -1,7 +1,7 @@
 import { app_ceb_bible_gloss_generate_generic } from "../../../love/public/src/app_ceb_bible_gloss_generate_generic.mjs";
 export async function app_original_bible_gloss_generate() {
-  let bible_folder = "engbsb";
   let chapter_code_specified = "1PE04";
+  let bible_folder = "engbsb";
   let language = "Greek";
   let fn = app_original_bible_gloss_generate;
   const bible_folders = [bible_folder];
