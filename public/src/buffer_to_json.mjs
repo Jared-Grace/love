08769ae92fbfb1f3761a055222e4c9073a199734
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:36:11 -0500

    function_auto_path ..\love\public\src\buffer_text_to.mjs

--- public/src/buffer_to_json.mjs
+++ public/src/buffer_to_json.mjs
@@ -1,7 +1,7 @@
 import { json_from } from "../../../love/public/src/json_from.mjs";
-import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
+import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 export function buffer_to_json(buffer) {
-  let text = buffer_string_to(buffer);
+  let text = buffer_text_to(buffer);
   let parsed = json_from(text);
   return parsed;
 }
