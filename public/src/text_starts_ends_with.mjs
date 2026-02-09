88b43451446634581fb7b38bb6d715e0d8573864
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 12:55:54 -0500

    function_auto_path ..\love\public\src\text_starts_ends_with.mjs

--- public/src/text_starts_ends_with.mjs
+++ public/src/text_starts_ends_with.mjs
@@ -3,5 +3,6 @@ import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs"
 export function text_starts_ends_with(t, prefix, suffix) {
   let sw = text_starts_with(t, prefix);
   let ew = text_ends_with(item, suffix);
-  return sw && ew;
+  let r = sw && ew;
+  return r;
 }
