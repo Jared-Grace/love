2754e6be59ae1cfdc6086077bb5ef45871175e9b
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:37:15 -0500

    function_auto_path ..\love\public\src\js_code_text.mjs

--- web/public/src/folder_current_join_code.mjs
+++ web/public/src/folder_current_join_code.mjs
@@ -1,7 +1,7 @@
-import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
+import { js_code_text } from "../../../love/public/src/js_code_text.mjs";
 import { folder_current_join } from "../../../love/public/src/folder_current_join.mjs";
 export function folder_current_join_code(f_name_ext) {
   let value_string = folder_current_join(f_name_ext);
-  const from = js_code_string(value_string);
+  const from = js_code_text(value_string);
   return from;
 }
