51f7911b2bcf38a7965f2942e3ce95b188e01875
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:35:45 -0500

    s uuid

--- web/public/src/text_to_uuid_save_exists.mjs
+++ web/public/src/text_to_uuid_save_exists.mjs
@@ -1,7 +1,7 @@
 import { text_to_uuid_read_save } from "../../../love/public/src/text_to_uuid_read_save.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-export async function text_to_uuid_save_exists(id) {
+export async function text_to_uuid_save_exists(text) {
   let set = await text_to_uuid_read_save();
-  let text = object_property_exists(set, id);
+  let text = object_property_exists(set, text);
   return text;
 }
