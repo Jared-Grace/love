ddf4a50e470d3741b14005f805136724c8680153
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:49:46 -0500

    function_auto_path ..\love\public\src\function_name_to_part_first_2.mjs

--- public/src/function_name_to_part_first_2.mjs
+++ public/src/function_name_to_part_first_2.mjs
@@ -1,7 +1,7 @@
-import { list_first } from "../../../love/public/src/list_first.mjs";
+import { list_take } from "../../../love/public/src/list_take.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_to_part_first_2(f_name_before) {
   let parts = function_name_to_parts(f_name_before);
-  let last = list_first(parts);
+  let last = list_take(parts, 2);
   return last;
 }
