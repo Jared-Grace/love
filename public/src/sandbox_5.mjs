56d510f8207b2934b5ecd0b2708db05ea54c1cd1
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:16:18 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,7 +1,7 @@
-import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
+import { folder_read_recursive_paths_async } from "../../../love/public/src/folder_read_recursive_paths_async.mjs";
 import { folder_user_storage_function_path_function } from "./folder_user_storage_function_path_function.mjs";
 export async function sandbox_5() {
   let f = folder_user_storage_function_path_function();
-  let files = await folder_read_files(f);
-  return f;
+  let files = await folder_read_recursive_paths_async(f);
+  return files;
 }
