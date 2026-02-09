d35998a771053f8fd8de0a32818906cc82c67b4e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:09:43 -0500

    function_auto_path ..\love\public\src\list_first.mjs

--- web/public/src/messenger_reply.mjs
+++ web/public/src/messenger_reply.mjs
@@ -33,10 +33,10 @@ export async function messenger_reply() {
     }
     let mapped = list_map(skips, lambda4);
     let filtered = list_filter_ends_with_not_any(mapped, properties);
-    let function_copy_replace_first = list_first(filtered);
+    let first = list_first(filtered);
     let messages = await messenger_reply_messages(
       page,
-      function_copy_replace_first,
+      first,
     );
     let v2 = await messenger_reply_unreplied(messages);
     let unreplied = object_property_get(v2, "unreplied");
