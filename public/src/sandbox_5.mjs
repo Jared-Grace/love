f4d45c4f39a6c1a308cb948f78fd11135bce719a
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:17:24 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,7 +1,9 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { folder_read_recursive_paths_async } from "../../../love/public/src/folder_read_recursive_paths_async.mjs";
 import { folder_user_storage_function_path_function } from "./folder_user_storage_function_path_function.mjs";
 export async function sandbox_5() {
   let f = folder_user_storage_function_path_function();
   let files = await folder_read_recursive_paths_async(f);
+  let mapped = list_map(list, function lambda(item) {});
   return files;
 }
