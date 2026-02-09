b6c63f79acad78b0e0c7bda584a80bd454782dea
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:18:10 -0500

    function_auto_path ..\love\public\src\functions_rename_if_starts_with.mjs

--- public/src/functions_rename_if_starts_with.mjs
+++ public/src/functions_rename_if_starts_with.mjs
@@ -1,7 +1,7 @@
 import { functions_rename_generic_starts_with } from "../../../love/public/src/functions_rename_generic_starts_with.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { text_prefix_change } from "../../../love/public/src/text_prefix_change.mjs";
-export async function function_multiple_rename_if_starts_with(
+export async function functions_rename_if_starts_with(
   f_name_prefix_before,
   f_name_prefix_after,
 ) {
