e0c2aefe4dcc38ca0e68eea63614505407fd9d76
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:14:36 -0500

    rrl folder_user_storage_function_path_fn function

--- public/src/folder_user_storage_function_path_function.mjs
+++ public/src/folder_user_storage_function_path_function.mjs
@@ -1,5 +1,5 @@
 import { folder_user_storage_path } from "../../../love/public/src/folder_user_storage_path.mjs";
-export function folder_user_storage_function_path_fn() {
+export function folder_user_storage_function_path_function() {
   let p = folder_user_storage_path("function\\");
   return p;
 }
