e1fa9cede572512a608e67481b3e7c41d11cb219
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:07:31 -0500

    function_auto_path ..\love\public\src\text_split_multiple.mjs

--- web/public/src/text_split_multiple.mjs
+++ web/public/src/text_split_multiple.mjs
@@ -1,4 +1,3 @@
-import { function_copy_replace_first } from "../../../love/public/src/function_copy_replace_first.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
@@ -7,12 +6,12 @@ import { each } from "../../../love/public/src/each.mjs";
 export function text_split_multiple(str, delimiters) {
   let v = list_first_remaining(delimiters);
   let remaining = object_property_get(v, "remaining");
-  let function_copy_replace_first = object_property_get(v, "first");
+  let first = object_property_get(v, "first");
   function lambda(delimiter) {
     let split = text_split(str, delimiter);
-    str = list_join(split, function_copy_replace_first);
+    str = list_join(split, first);
   }
   each(remaining, lambda);
-  let parts = text_split(str, function_copy_replace_first);
+  let parts = text_split(str, first);
   return parts;
 }
