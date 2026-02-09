7960420e09318c6f1c50a56670af776de0bec655
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:17:27 -0500

    function_auto_path ..\love\public\src\functions_rename_generic.mjs

--- public/src/function_multiple_rename_if_starts_ends_with.mjs
+++ public/src/function_multiple_rename_if_starts_ends_with.mjs
@@ -1,6 +1,6 @@
 import { text_starts_ends_with_curry_right } from "../../../love/public/src/text_starts_ends_with_curry_right.mjs";
 import { text_suffix_change } from "../../../love/public/src/text_suffix_change.mjs";
-import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
+import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 export async function function_multiple_rename_if_starts_ends_with(
   f_name_prefix,
@@ -12,7 +12,7 @@ export async function function_multiple_rename_if_starts_ends_with(
     f_name_prefix,
     f_name_suffix_before,
   );
-  await function_multiple_rename_generic(filter, name_change);
+  await functions_rename_generic(filter, name_change);
   function name_change(f_name_before) {
     let together2 = text_suffix_change(
       f_name_before,
--- public/src/function_multiple_rename_replace.mjs
+++ public/src/function_multiple_rename_replace.mjs
@@ -1,10 +1,10 @@
 import { text_replace_curry_right } from "../../../love/public/src/text_replace_curry_right.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { tautology } from "../../../love/public/src/tautology.mjs";
-import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
+import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
 export async function function_multiple_rename_replace(from, to) {
   assert_arguments(arguments, 2);
   let mapper = text_replace_curry_right(from, to);
-  let r = await function_multiple_rename_generic(tautology, mapper);
+  let r = await functions_rename_generic(tautology, mapper);
   return r;
 }
