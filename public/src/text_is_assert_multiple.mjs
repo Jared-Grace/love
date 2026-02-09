310783b9896dacbb44b4e287601212bd249ba7d0
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:00:10 -0500

    function_auto_path ..\love\public\src\text_is_assert_multiple.mjs

--- public/src/text_is_assert_multiple.mjs
+++ public/src/text_is_assert_multiple.mjs
@@ -2,7 +2,9 @@ import { each } from "../../../love/public/src/each.mjs";
 import { text_is_assert_json } from "./text_is_assert_json.mjs";
 export function text_is_assert_multiple(items) {
   function lambda2(item) {
-    text_is_assert_json(item, item);
+    text_is_assert_json(item, {
+      item,
+    });
   }
   each(items, lambda2);
 }
