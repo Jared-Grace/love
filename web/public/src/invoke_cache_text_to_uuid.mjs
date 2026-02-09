061765e22b7eaf46e5cc19b5cd6f45ce255eaa94
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 20:26:43 -0500

    function_auto_path ..\love\public\src\invoke_cache_text_to_uuid.mjs

--- web/public/src/invoke_cache_text_to_uuid.mjs
+++ web/public/src/invoke_cache_text_to_uuid.mjs
@@ -1,17 +1,15 @@
-import { invoke_cache_file_key_get } from "../../../love/public/src/invoke_cache_file_key_get.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { text_to_uuid_get } from "../../../love/public/src/text_to_uuid_get.mjs";
+import { text_to_uuid_set_exists } from "../../../love/public/src/text_to_uuid_set_exists.mjs";
+import { null_get } from "../../../love/public/src/null_get.mjs";
 import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
-import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
-import { file_exists } from "../../../love/public/src/file_exists.mjs";
-import { invoke_cache_value_get } from "../../../love/public/src/invoke_cache_value_get.mjs";
 import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
-export async function invoke_cache_text_to_uuid(fn, args) {
-  let key_get = invoke_cache_file_key_get(fn, args);
-  let value_get = invoke_cache_value_get(fn, args);
-  let cached_exists = file_exists;
+import { lambda_get } from "./lambda_get.mjs";
+export async function invoke_cache_text_to_uuid(text) {
+  let key_get = lambda_get(text);
+  let value_get = null_get();
+  let cached_exists = text_to_uuid_set_exists;
   let cached_get = async function lambda2(key) {
-    let data = await file_read_json(key);
-    let result2 = object_property_get(data, "result");
+    let result2 = await text_to_uuid_get(key);
     return result2;
   };
   let cache_save = async function lambda4(key, result) {
