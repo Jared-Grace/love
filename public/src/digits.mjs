74eeac16521ae0c9ac6a389e2f61524a0f2497db
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:36:53 -0500

    function_auto_path ..\love\public\src\digits.mjs

--- public/src/digits.mjs
+++ public/src/digits.mjs
@@ -1,7 +1,7 @@
-import { digits_string } from "../../../love/public/src/digits_string.mjs";
+import { digits_text } from "../../../love/public/src/digits_text.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 export function digits() {
-  let d = digits_string();
+  let d = digits_text();
   let split = text_split_empty(d);
   return split;
 }
