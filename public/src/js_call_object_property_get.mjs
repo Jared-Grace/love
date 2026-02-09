af4c582cc6bb3ea253a707230d5ff680acbe5bdb
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:59:17 -0500

    function_auto_path ..\love\public\src\js_call_object_property_get.mjs

--- public/src/js_call_object_property_get.mjs
+++ public/src/js_call_object_property_get.mjs
@@ -1,8 +1,10 @@
+import { text_is_assert_multiple } from "../../../love/public/src/text_is_assert_multiple.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
 import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 export function js_call_object_property_get(property_name, object_name) {
+  text_is_assert_multiple([property_name, object_name]);
   let code_string = js_code_string(property_name);
   let code = js_code_call_args(object_property_get.name, [
     object_name,
