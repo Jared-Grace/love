305dc3409df69c22b14319c31b83813889668d99
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:16:58 -0500

    function_auto_path ..\love\public\src\functions_param_delete.mjs

--- public/src/functions_param_delete.mjs
+++ public/src/functions_param_delete.mjs
@@ -1,6 +1,6 @@
-import { function_multiple_param_delete_generic } from "../../../love/public/src/function_multiple_param_delete_generic.mjs";
+import { functions_param_delete_generic } from "../../../love/public/src/functions_param_delete_generic.mjs";
 import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
 export async function functions_param_delete(f_names_comma, param_name) {
   let fn = function_param_delete;
-  await function_multiple_param_delete_generic(f_names_comma, param_name, fn);
+  await functions_param_delete_generic(f_names_comma, param_name, fn);
 }
