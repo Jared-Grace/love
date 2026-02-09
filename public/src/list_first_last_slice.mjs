80c970b61e41cfe5e54cd0a75613d851cd214c07
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:08:51 -0500

    function_auto_path ..\love\public\src\list_first_last_slice.mjs

--- public/src/list_first_last_slice.mjs
+++ public/src/list_first_last_slice.mjs
@@ -1,15 +1,16 @@
+import { function_copy_replace_first } from "../../../love/public/src/function_copy_replace_first.mjs";
 import { list_slice_from } from "../../../love/public/src/list_slice_from.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_first_last } from "../../../love/public/src/list_first_last.mjs";
 export function list_first_last_slice(list_rg, list) {
   let v = list_first_last(list_rg);
-  let function_copy_replace_first = list_first(v);
+  let first = list_first(v);
   let last = list_last(v);
-  let sliced = list_slice_from(list, function_copy_replace_first, last);
+  let sliced = list_slice_from(list, first, last);
   let v2 = {
     sliced,
-    function_copy_replace_first,
+    function_copy_replace_first: first,
     last,
   };
   return v2;
