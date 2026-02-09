79beb7ec91f1b1397e13381ecc27949888328064
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:11:07 -0500

    function_auto_path ..\love\public\src\function_multiple_rename_replace.mjs

--- web/public/src/function_multiple_rename_replace.mjs
+++ web/public/src/function_multiple_rename_replace.mjs
@@ -1,7 +1,12 @@
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { tautology } from "../../../love/public/src/tautology.mjs";
 import { function_multiple_rename_generic } from "../../../love/public/src/function_multiple_rename_generic.mjs";
-export async function function_multiple_rename_replace(filter, name_change) {
-  function lambda() {}
+export async function function_multiple_rename_replace(filter, from, to) {
+  assert_arguments(arguments, 3);
+  function lambda(f_name_before) {
+    let f_name_wrapped = text_replace(f_name_before, from, to);
+  }
   let r = await function_multiple_rename_generic(tautology, lambda);
   return r;
 }
