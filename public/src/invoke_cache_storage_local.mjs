c7b0d93d6d88102c49c406a92cc133ea0027e0aa
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 19:59:01 -0500

    function_auto_path ..\love\public\src\invoke_cache_storage_local.mjs

--- public/src/invoke_cache_storage_local.mjs
+++ public/src/invoke_cache_storage_local.mjs
@@ -1,7 +1,7 @@
+import { invoke_cache_generic } from "../../../love/public/src/invoke_cache_generic.mjs";
 import { storage_local_set_exists_not } from "../../../love/public/src/storage_local_set_exists_not.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
 import { storage_local_exists } from "../../../love/public/src/storage_local_exists.mjs";
-import { cache_generic } from "../../../love/public/src/cache_generic.mjs";
 export async function invoke_cache_storage_local(fn, args) {
   let cached_exists = function lambda3(key) {
     let exists = storage_local_exists(invoke_cache_storage_local, key);
@@ -19,12 +19,12 @@ export async function invoke_cache_storage_local(fn, args) {
     );
     return v2;
   };
-    let r = await invoke_cache_generic(
-      fn,
-      args,
-      cached_exists,
-      cached_get,
-      cache_save,
-    );
-    return r;
+  let r = await invoke_cache_generic(
+    fn,
+    args,
+    cached_exists,
+    cached_get,
+    cache_save,
+  );
+  return r;
 }
