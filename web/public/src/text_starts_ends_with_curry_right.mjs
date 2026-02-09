8e59e5af0102a694bee363f1c88f87a2027c81b3
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:00:00 -0500

    function_auto_path ..\love\public\src\text_starts_ends_with_curry_right.mjs

--- web/public/src/text_starts_ends_with_curry_right.mjs
+++ web/public/src/text_starts_ends_with_curry_right.mjs
@@ -1,11 +1,13 @@
+import { text_is_assert_multiple } from "../../../love/public/src/text_is_assert_multiple.mjs";
 import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { text_starts_ends_with } from "../../../love/public/src/text_starts_ends_with.mjs";
 export function text_starts_ends_with_curry_right(
   f_name_prefix,
   f_name_suffix_before,
 ) {
+  text_is_assert_multiple(items);
   let r2 = function text_starts_ends_with_curry_right_result(f_name) {
-    text_is_assert(value);
+    text_is_assert(f_name);
     let sew = text_starts_ends_with(
       f_name,
       f_name_prefix,
