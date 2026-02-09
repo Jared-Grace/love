b642163e88f5648c3ea2ba86b1a70edb13740ac9
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:36:11 -0500

    function_auto_path ..\love\public\src\buffer_text_to.mjs

--- public/src/app_message_download.mjs
+++ public/src/app_message_download.mjs
@@ -1,6 +1,6 @@
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
-import { buffer_string_to } from "../../../love/public/src/buffer_string_to.mjs";
+import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { firebase_storage_download } from "../../../love/public/src/firebase_storage_download.mjs";
 import { app_message_firebase_path } from "../../../love/public/src/app_message_firebase_path.mjs";
@@ -13,7 +13,7 @@ export async function app_message_download() {
   async function lambda(item) {
     let name2 = object_property_get(item, "name");
     let buffer = await firebase_storage_download(name2);
-    let s = buffer_string_to(buffer);
+    let s = buffer_text_to(buffer);
     let o = json_from(s);
     return o;
   }
