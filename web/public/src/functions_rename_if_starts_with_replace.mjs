8a48dcdd0e7c8e219d56326b749dacde8176503a
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 13:18:29 -0500

    function_auto_path ..\love\public\src\functions_rename_if_starts_with_replace.mjs

--- web/public/src/functions_rename_if_starts_with_replace.mjs
+++ web/public/src/functions_rename_if_starts_with_replace.mjs
@@ -1,7 +1,7 @@
 import { functions_rename_generic_starts_with } from "../../../love/public/src/functions_rename_generic_starts_with.mjs";
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-export async function function_multiple_rename_if_starts_with_replace(
+export async function functions_rename_if_starts_with_replace(
   f_name_prefix,
   from,
   to,
