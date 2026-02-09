64e7355fce8ee6018f37b73fcf95810a36b3b7ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:10:46 -0500

    function_auto_path ..\love\public\src\lists_sizes_equal_assert.mjs

--- public/src/lists_sizes_equal_assert.mjs
+++ public/src/lists_sizes_equal_assert.mjs
@@ -1,11 +1,10 @@
-import { function_copy_replace_first } from "../../../love/public/src/function_copy_replace_first.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function lists_sizes_equal_assert(lists) {
-  let function_copy_replace_first = list_first(lists);
-  let fs = list_size(function_copy_replace_first);
+  let first = list_first(lists);
+  let fs = list_size(first);
   function lambda3(list) {
     let size = list_size(list);
     let v = size === fs;
