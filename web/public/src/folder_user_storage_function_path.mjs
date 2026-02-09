e2f9ce35b9ad59c535afc3875b8d34794f36fe6d
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 17:32:09 -0500

    function_auto_path ..\love\public\src\folder_user_storage_function_path.mjs

--- web/public/src/folder_user_storage_function_path.mjs
+++ web/public/src/folder_user_storage_function_path.mjs
@@ -1,5 +1,7 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_user_storage_function_path_function } from "../../../love/public/src/folder_user_storage_function_path_function.mjs";
 export function folder_user_storage_function_path(fn) {
   let joined = folder_user_storage_function_path_function() + fn.name;
+  let joined2 = path_join(segments);
   return joined;
 }
