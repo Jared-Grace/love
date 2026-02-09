c56db86478caaed06374c07169faaca2574e4a23
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 12:41:56 -0500

    function_auto_path ..\love\public\src\sandbox_4.mjs

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -1,5 +1,1 @@
-import { js_interpreter } from "../../../karate_code/public/src/js_interpreter.mjs";
-export function sandbox_4() {
-  let i = js_interpreter()('console.log("test")');
-  i.run();
-}
+export function sandbox_4() {}
