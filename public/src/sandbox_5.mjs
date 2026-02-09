2dd65ff9962d063c6112dee6090fa17e56ae5e7e
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 22:01:13 -0500

    function_auto_path ..\love\public\src\text_is_assert_multiple.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,7 +1,8 @@
 import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
 import { ebible_chapter_text } from "./ebible_chapter_text.mjs";
 export async function sandbox_5() {
-  let t = await ebible_chapter_text("engbsb", "GEN01");$g$t$text 
+  let t = await ebible_chapter_text("engbsb", "GEN01");
+  $g$t$text 
   let r = await openai_responses_cache("", t);
   return r;
 }
