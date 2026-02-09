fbaa1c939d723118cc4376dc3ce61699d8ca9750
Grace-Jared <christrosetolife@gmail.com>
2026-02-08 21:22:27 -0500

    function_auto_path ..\love\public\src\text_to_uuid.mjs

--- public/src/text_to_uuid_save_initialize.mjs
+++ public/src/text_to_uuid_save_initialize.mjs
@@ -1,5 +1,5 @@
 import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
-export function text_to_uuid_set_initialize(data) {
+export function text_to_uuid_save_initialize(data) {
   let value = object_property_initialize(data, "set", {});
   return value;
 }
