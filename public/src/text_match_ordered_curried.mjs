83df05f00850d2ee88c23afd056dde3e86886421
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:41:02 -0500

    sb

--- public/src/text_match_ordered_curried.mjs
+++ public/src/text_match_ordered_curried.mjs
@@ -1,8 +1,6 @@
 import { text_match_ordered } from "../../../love/public/src/text_match_ordered.mjs";
 export function text_match_ordered_curried() {
-  let r = function text_match_ordered_curried_result() {
-    let matches = text_match_ordered(t, target);
-    return matches;
+  return function text_match_ordered_curried_result(target, t) {
+    return text_match_ordered(t, target);
   };
-  return r;
 }
