e6c5b4bee2bce349220681b88064990ad085dae9
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:43:16 -0500

    function_auto_path ..\love\public\src\object_filter.mjs

--- web/public/src/object_filter.mjs
+++ web/public/src/object_filter.mjs
@@ -1,10 +1,10 @@
 import { object_adder } from "../../../love/public/src/object_adder.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
-export function object_filter(object, filter) {
+export function object_filter(object, lambda$value$key) {
   function lambda2(oad2) {
-    function lambda3(value, property) {
-      if (filter(value, property)) {
-        oad2(property, value);
+    function lambda3(value, key) {
+      if (lambda$value$key(value, key)) {
+        oad2(key, value);
       }
     }
     each_object(object, lambda3);
