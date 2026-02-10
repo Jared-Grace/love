9c14724938e9cd30ae2c808df8d454713814b3bc
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:58:08 -0500

    c

--- web/public/src/app_api_fn.mjs
+++ web/public/src/app_api_fn.mjs
@@ -1,8 +1,8 @@
 import { app_api } from "../../../love/public/src/app_api.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function app_api_fn(a) {
-  let fn = object_property_get(a, "fn");
+  let fn = property_get(a, "fn");
   let f_name = fn.name;
   object_property_set_exists_not(a, "f_name", f_name);
   let r = await app_api(a);
