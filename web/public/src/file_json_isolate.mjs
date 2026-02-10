9a1db9a0be2dab203b7f7d62e632755a71eac921
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:47:23 -0500

    function_auto_path ..\love\public\src\object_replace.mjs

--- web/public/src/file_json_isolate.mjs
+++ web/public/src/file_json_isolate.mjs
@@ -1,11 +1,11 @@
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { object_properties_delete_all } from "../../../love/public/src/object_properties_delete_all.mjs";
+import { properties_delete_all } from "../../../love/public/src/properties_delete_all.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 export async function file_json_isolate(f_path, property_name) {
   async function lambda(data) {
     let value = object_property_get(data, property_name);
-    object_properties_delete_all(data);
+    properties_delete_all(data);
     object_property_set(data, property_name, value);
   }
   await file_json_transform(f_path, lambda);
