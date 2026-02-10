097d02d49a1d20fcc6325333d5fafd84aa23f358
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:20:15 -0500

    function_auto_path ..\love\public\src\list_first_remaining.mjs

--- web/public/src/list_first_remaining.mjs
+++ web/public/src/list_first_remaining.mjs
@@ -3,6 +3,6 @@ import { list_skip_1 } from "../../../love/public/src/list_skip_1.mjs";
 export function list_first_remaining(list) {
   const property_name = "remaining";
   let value_get = list_skip_1;
-  let result = list_first_second_generic(list, value_get, property_name);
-  return result;
+  let r = list_first_second_generic(list, value_get, property_name);
+  return r;
 }
