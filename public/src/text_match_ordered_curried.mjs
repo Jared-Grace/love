739a874baa6ca13e3e03eaae54c27a516d56a61a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:33:14 -0500

    sb

--- public/src/text_match_ordered_curried.mjs
+++ public/src/text_match_ordered_curried.mjs
@@ -1,1 +1,5 @@
-export function text_match_ordered_curried() {}
+export function text_match_ordered_curried() {
+  function text_match_ordered_curried_result() {
+    return text_match_ordered(target);
+  }
+}
