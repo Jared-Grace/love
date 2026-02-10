cca55bbfba5880a466f07c1af58c23c8ad245714
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:09:13 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -142,7 +142,7 @@ export async function app_replace_rule_set(context) {
       let lambda7 = app_replace_rule_valid_curried(rule2, current_list);
       let enabled = list_any(r, lambda7);
       html_enable_if(rb, enabled);
-      if (false) {
+      if (enabled) {
       }
       html_style_set_or_remove(
         index2 === index_selected,
