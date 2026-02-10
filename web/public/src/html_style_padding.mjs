3400005e7ef246097599c2b797276a32439b4ad7
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 13:29:16 -0500

    function_auto_path ..\love\public\src\html_style_padding.mjs

--- web/public/src/html_style_padding.mjs
+++ web/public/src/html_style_padding.mjs
@@ -1,7 +1,6 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function html_style_padding(component, value) {
   html_style_assign(component, {
-    "padding-left": value,
-    "padding-right": value,
+    padding: value,
   });
 }
