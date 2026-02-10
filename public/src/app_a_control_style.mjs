1fe82ba6db93bf9d29c8a2117140d8954123215b
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:48:01 -0500

    function_auto_path ..\love\public\src\app_a_control_style.mjs

--- public/src/app_a_control_style.mjs
+++ public/src/app_a_control_style.mjs
@@ -1,9 +1,9 @@
+import { html_border_radius } from "../../../love/public/src/html_border_radius.mjs";
 import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_a_control_style(component) {
-  html_style_assign(component, {
-    "border-radius": "0.8em",
-  });
+  const border_radius = "0.8em";
+  html_border_radius(component, border_radius);
   html_style_assign(component, {
     "border-width": "0px",
   });
