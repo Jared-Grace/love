68258580d79609d1f2962ad8ad846b7002cb380f
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:13 -0500

    function_auto_path ..\love\public\src\property_delete.mjs

---
+++ web/public/src/property_exists_equals.mjs
@@ -0,0 +1,8 @@
+import { property_equals } from "../../../love/public/src/property_equals.mjs";
+import { property_exists } from "../../../love/public/src/property_exists.mjs";
+export function property_exists_equals(o, property_name, property_value) {
+  let v =
+    property_exists(property_name) &&
+    property_equals(o, property_name, property_value);
+  return v;
+}
