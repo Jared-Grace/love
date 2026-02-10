c98be5a914cc3bea239414156f36ee4bb030f7f4
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:29:38 -0500

    function_auto_path ..\love\public\src\function_rename.mjs

--- web/public/src/function_rename.mjs
+++ web/public/src/function_rename.mjs
@@ -8,6 +8,7 @@ import { function_rename_fn_names_check } from "../../../love/public/src/functio
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { function_move } from "../../../love/public/src/function_move.mjs";
 export async function function_rename(f_name_before, f_name_after) {
+  ena;
   await function_rename_check(f_name_after);
   const v = await function_name_to_path_unalias(f_name_before);
   let unaliased = property_get(v, "unaliased");
