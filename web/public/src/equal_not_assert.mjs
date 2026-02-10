9e3a6af621c9b2005e246693573314eb83d87f80
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:31:31 -0500

    function_auto_path ..\love\public\src\equal_not_assert.mjs

--- web/public/src/equal_not_assert.mjs
+++ web/public/src/equal_not_assert.mjs
@@ -3,10 +3,11 @@ import { assert_json_get } from "./assert_json_get.mjs";
 export function equal_not_assert(left, right) {
   let ne = equal_not(left, right);
   function lambda() {
-    return {
+    let r = {
       left,
       right,
     };
+    return r;
   }
   assert_json_get(ne, lambda);
 }
