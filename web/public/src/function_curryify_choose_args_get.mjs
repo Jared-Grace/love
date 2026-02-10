80442649875f6e67a1b771cd488c15c8f40016b4
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:38:04 -0500

    function_auto_path ..\love\public\src\function_curryify_choose_args_get.mjs

--- web/public/src/function_curryify_choose_args_get.mjs
+++ web/public/src/function_curryify_choose_args_get.mjs
@@ -1,7 +1,9 @@
+import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
 import { list_copy } from "../../../love/public/src/list_copy.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
-export function function_curryify_choose_args_get(index) {
+export function function_curryify_choose_args_get(index_text) {
+  let index = integer_to(index_text);
   let r2 = function lambda(arg_names) {
     let item = list_get(arg_names, index);
     let fn_new_result_args = [item];
