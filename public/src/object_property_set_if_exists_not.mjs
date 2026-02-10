e772b93a72e4468590bef93fa8b97f62e7e8b6ce
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:11:40 -0500

    function_auto_path ..\love\public\src\app_search_results.mjs

--- public/src/object_property_set_if_exists_not.mjs
+++
@@ -1,8 +0,0 @@
-import { property_set } from "../../../love/public/src/property_set.mjs";
-import { property_exists_not } from "../../../love/public/src/property_exists_not.mjs";
-export function object_property_set_if_exists_not(a, p, value) {
-  let n = property_exists_not(a, p);
-  if (n) {
-    property_set(a, p, value);
-  }
-}
