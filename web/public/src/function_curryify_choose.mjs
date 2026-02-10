56b94dab1ee36023814c0544f8f36ffe9222b19e
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:36:31 -0500

    function_auto_path ..\love\public\src\function_curryify_choose.mjs

--- web/public/src/function_curryify_choose.mjs
+++ web/public/src/function_curryify_choose.mjs
@@ -1,22 +1,10 @@
+import { function_curryify_choose_args_get } from "../../../love/public/src/function_curryify_choose_args_get.mjs";
 import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
-import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
-import { list_copy } from "../../../love/public/src/list_copy.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
 export async function function_curryify_choose(f_name, index_text) {
   let index = integer_to(index_text);
-  let args_get = function lambda(arg_names) {
-    let item = list_get(arg_names, index);
-    let fn_new_result_args = [item];
-    let fn_new_args = list_copy(arg_names);
-    list_remove_at(fn_new_args, index);
-    let r = {
-      fn_new_result_args,
-      fn_new_args,
-    };
-    return r;
-  };
+  let args_get = function_curryify_choose_args_get(index);
   let output = await function_curryify_generic(
     f_name,
     args_get,
