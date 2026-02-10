e29c2047880b3c6ac7ec243d519c5b9fa010f3a3
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:40:23 -0500

    function_auto_path ..\love\public\src\functions_rename_generic.mjs

--- web/public/src/functions_rename_generic.mjs
+++ web/public/src/functions_rename_generic.mjs
@@ -1,3 +1,4 @@
+import { each_object_async } from "../../../love/public/src/each_object_async.mjs";
 import { object_property_exists_not_assert } from "../../../love/public/src/object_property_exists_not_assert.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
@@ -18,6 +19,7 @@ export async function functions_rename_generic(filter, name_change) {
     object_property_exists_not_assert(identifiers, f_name_after);
   }
   each_object_values(object, lambda2);
+  await each_object_async(object2, async function lambda3(value, key) {});
   await list_map_async(filtered, lambda);
   async function lambda(f_name_before) {
     let f_name_after = name_change(f_name_before);
