6eea0e2e4024ffea63779b8350449379c9ab097a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 15:54:15 -0500

    function_auto_path ..\love\public\src\app_replace_rule_valid_curried.mjs

--- web/public/src/app_replace_rule_valid_curried.mjs
+++ web/public/src/app_replace_rule_valid_curried.mjs
@@ -1,6 +1,8 @@
 import { app_replace_rule_valid } from "../../../love/public/src/app_replace_rule_valid.mjs";
 export function app_replace_rule_valid_curried(rule, current_list) {
-  return function app_replace_rule_valid_curried_result(index_selected) {
-    return app_replace_rule_valid(rule, index_selected, current_list);
+  let r = function app_replace_rule_valid_curried_result(index_selected) {
+    let eq = app_replace_rule_valid(rule, index_selected, current_list);
+    return eq;
   };
+  return r;
 }
