33112dcf998e5cfe7f0c54146eee65d228cc568e
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:13:00 -0500

    rr app_ceb_bible_home_generic ceb gloss

--- web/public/src/app_ceb_bible_home.mjs
+++ web/public/src/app_ceb_bible_home.mjs
@@ -1,6 +1,6 @@
-import { app_ceb_bible_home_generic } from "../../../love/public/src/app_ceb_bible_home_generic.mjs";
+import { app_gloss_bible_home_generic } from "../../../love/public/src/app_gloss_bible_home_generic.mjs";
 import { app_ceb_bible_gloss_generate_download } from "../../../love/public/src/app_ceb_bible_gloss_generate_download.mjs";
 export async function app_ceb_bible_home(context) {
   let download = app_ceb_bible_gloss_generate_download;
-  await app_ceb_bible_home_generic(context, download, true);
+  await app_gloss_bible_home_generic(context, download, true);
 }
