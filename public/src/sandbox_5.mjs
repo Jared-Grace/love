ab256498b3e6fc83fe48a3ab861362fbcea9becc
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:11:47 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,5 +1,5 @@
 import { object_property_swap } from "../../../love/public/src/object_property_swap.mjs";
-import { object_property_subtract_1 } from "../../../love/public/src/object_property_subtract_1.mjs";
+import { property_subtract_1 } from "../../../love/public/src/property_subtract_1.mjs";
 import { property_set_if_exists_not } from "../../../love/public/src/property_set_if_exists_not.mjs";
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
@@ -101,7 +101,7 @@ export async function sandbox_5() {
     property_set_exists_not: "public/src/object_property_set_exists_not.mjs",
     property_set_if_exists_not:
       "public/src/object_property_set_if_exists_not.mjs",
-    object_property_subtract_1: "public/src/object_property_subtract_1.mjs",
+    property_subtract_1: "public/src/object_property_subtract_1.mjs",
     object_property_swap: "public/src/object_property_swap.mjs",
   };
 }
