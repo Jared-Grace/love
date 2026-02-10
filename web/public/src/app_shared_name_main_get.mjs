42c9a7fa95a8438ffd1e8920209140ccda8c89ce
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:19:21 -0500

    function_auto_path ..\love\public\src\app_shared_name_main_get.mjs

--- web/public/src/app_shared_name_main_get.mjs
+++ web/public/src/app_shared_name_main_get.mjs
@@ -1,5 +1,4 @@
-import { list_single } from "../../../love/public/src/list_single.mjs";
-import { list_filter_text_match_ordered } from "../../../love/public/src/list_filter_text_match_ordered.mjs";
+import { list_find_text_match_ordered } from "../../../love/public/src/list_find_text_match_ordered.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -14,8 +13,7 @@ export async function app_shared_name_main_get(search) {
     app_name = search;
   } else {
     let mapped = await apps_names();
-    let app_names = list_filter_text_match_ordered(mapped, search);
-    app_name = list_single(app_names);
+    app_name = list_find_text_match_ordered(mapped, search, app_name);
   }
   f_name = app_shared_name_main(app_name);
   log_keep({
