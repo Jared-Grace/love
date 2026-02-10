eba869baceddd747b7ba1ff1788682cb71958085
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:30:57 -0500

    function_auto_path ..\love\public\src\equal_not_assert.mjs

--- web/public/src/equal_not_assert.mjs
+++ web/public/src/equal_not_assert.mjs
@@ -1,6 +1,6 @@
 import { assert } from "../../../love/public/src/assert.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-export function equal_not_assert(f_name_before, f_name_after) {
-  let ne = equal_not(f_name_before, f_name_after);
+export function equal_not_assert(left, right) {
+  let ne = equal_not(left, right);
   assert(ne);
 }
