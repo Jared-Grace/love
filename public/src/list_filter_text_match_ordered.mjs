aa123b1aba2272b61bd34e857f01e21dd61a7603
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:17:28 -0500

    function_auto_path ..\love\public\src\list_filter_text_match_ordered.mjs

--- public/src/list_filter_text_match_ordered.mjs
+++ public/src/list_filter_text_match_ordered.mjs
@@ -1,7 +1,7 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { text_match_ordered_curried } from "../../../love/public/src/text_match_ordered_curried.mjs";
-export function list_filter_text_match_ordered(input, searches) {
-  let lambda = text_match_ordered_curried(input);
+export function list_filter_text_match_ordered(query, searches) {
+  let lambda = text_match_ordered_curried(query);
   let filtered = list_filter(searches, lambda);
   return filtered;
 }
