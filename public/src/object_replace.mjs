23a9fb6c375c15fe42765d775794d5a21f1e3453
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:47:23 -0500

    function_auto_path ..\love\public\src\object_replace.mjs

--- public/src/object_replace.mjs
+++ public/src/object_replace.mjs
@@ -1,9 +1,9 @@
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { object_properties_delete_all } from "../../../love/public/src/object_properties_delete_all.mjs";
+import { properties_delete_all } from "../../../love/public/src/properties_delete_all.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function object_replace(to, from) {
   if (equal_not(to, from)) {
-    object_properties_delete_all(to);
+    properties_delete_all(to);
     object_merge(to, from);
   }
 }
---
+++ public/src/properties_delete_if_exists.mjs
@@ -0,0 +1,8 @@
+import { object_property_delete_if_exists } from "../../../love/public/src/object_property_delete_if_exists.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+export function properties_delete_if_exists(data, properties) {
+  function lambda2(p) {
+    object_property_delete_if_exists(data, p);
+  }
+  each(properties, lambda2);
+}
