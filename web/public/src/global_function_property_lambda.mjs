19b41f446b001fd0cb6725b86e72d41609b17721
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:10:44 -0500

    function_auto_path ..\love\public\src\global_function_initialize_lambda.mjs

--- web/public/src/global_function_property_lambda.mjs
+++ web/public/src/global_function_property_lambda.mjs
@@ -1,12 +1,8 @@
 import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
-import { object_property_initialize_lambda } from "../../../love/public/src/object_property_initialize_lambda.mjs";
+import { property_initialize_lambda } from "../../../love/public/src/property_initialize_lambda.mjs";
 export function global_function_property_lambda(fn, property_name, lambda) {
   const initial = {};
   let fn_object = global_function_initialize(fn, initial);
-  let value = object_property_initialize_lambda(
-    fn_object,
-    property_name,
-    lambda,
-  );
+  let value = property_initialize_lambda(fn_object, property_name, lambda);
   return value;
 }
