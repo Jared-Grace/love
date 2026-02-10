48cb2ae5a03de99e4e55eac92dbece4ef04f95fc
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 13:33:16 -0500

    function_auto_path ..\love\public\src\app_replace_main.mjs

--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -14,6 +14,6 @@ export async function app_replace_main(context) {
     screens,
   });
   app_shared_refresh(context);
-  app_replace_font_size_refresh(context);
   let root = html_mobile_default(context);
+  app_replace_font_size_refresh(context);
 }
