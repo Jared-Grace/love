d3383edff83964dac37a6d36c1b4bac69e06907f
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:32 -0500

    function_auto_path ..\love\public\src\property_exists_not.mjs

--- public/src/object_property_exists_not.mjs
+++
@@ -1,7 +0,0 @@
-import { not } from "../../../love/public/src/not.mjs";
-import { property_exists } from "../../../love/public/src/property_exists.mjs";
-export function object_property_exists_not(object, property_name) {
-  let e = property_exists(object, property_name);
-  let ne = not(e);
-  return ne;
-}
