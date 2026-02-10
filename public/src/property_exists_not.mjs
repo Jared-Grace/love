ae2f74943b7ee7351c1d1c7d454c736960623a22
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:28 -0500

    function_auto_path ..\love\public\src\function_rename_fn_names_check.mjs

---
+++ public/src/property_exists_not.mjs
@@ -0,0 +1,7 @@
+import { not } from "../../../love/public/src/not.mjs";
+import { property_exists } from "../../../love/public/src/property_exists.mjs";
+export function property_exists_not(object, property_name) {
+  let e = property_exists(object, property_name);
+  let ne = not(e);
+  return ne;
+}
