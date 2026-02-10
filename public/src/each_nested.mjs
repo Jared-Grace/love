be85a528f60dfb92208ab2393e6db844089303e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:03:42 -0500

    function_auto_path ..\love\public\src\each_nested.mjs

--- public/src/each_nested.mjs
+++ public/src/each_nested.mjs
@@ -1,7 +1,9 @@
+import { each_curried_right } from "../../../love/public/src/each_curried_right.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function each_nested(list, lambda$item) {
   function each_nested_lambda(item) {
     each(item, lambda$item);
   }
+  let r2 = each_curried_right(function lambda(item2) {});
   each(list, each_nested_lambda);
 }
