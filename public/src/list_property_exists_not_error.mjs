e422c29cc58b6efc23ceab27415854a35fc98a53
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:32 -0500

    function_auto_path ..\love\public\src\property_exists_not.mjs

--- public/src/list_property_exists_not_error.mjs
+++ public/src/list_property_exists_not_error.mjs
@@ -1,9 +1,9 @@
 import { error } from "../../../love/public/src/error.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
-import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
+import { property_exists_not } from "../../../love/public/src/property_exists_not.mjs";
 export function list_property_exists_not_error(all, property) {
   function lambda(item, index) {
-    let n = object_property_exists_not(item, property);
+    let n = property_exists_not(item, property);
     if (n) {
       error({
         index,
