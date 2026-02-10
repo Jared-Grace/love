3122de447239c27680cf877949d93ce33441690f
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:31:31 -0500

    function_auto_path ..\love\public\src\object_property_exists_not_assert.mjs

--- public/src/object_property_exists_not_assert.mjs
+++ public/src/object_property_exists_not_assert.mjs
@@ -1,13 +1,7 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
-import { not } from "../../../love/public/src/not.mjs";
 import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
-import { object_property_exists_not } from "../../../love/public/src/object_property_exists_not.mjs";
-import { assert } from "../../../love/public/src/assert.mjs";
 export function object_property_exists_not_assert(object, property_name) {
-  let result = object_property_exists_not(object, property_name);
-  assert(result);
-  return;
   let e = object_property_exists(object, property_name);
   if (e) {
     let value = object_property_get(object, property_name);
@@ -17,6 +11,4 @@ export function object_property_exists_not_assert(object, property_name) {
       value,
     });
   }
-  let result2 = not(result3);
-  assert(result);
 }
