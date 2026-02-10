bedc5d91cc3c904aea0c377b88844737b57d9627
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:32:58 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/js_declaration_params_add.mjs
+++ public/src/js_declaration_params_add.mjs
@@ -1,6 +1,6 @@
-import { js_declaration_param_add_curried } from "../../../love/public/src/js_declaration_param_add_curried.mjs";
+import { js_declaration_param_add_curry } from "../../../love/public/src/js_declaration_param_add_curry.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function js_declaration_params_add(declaration, param_names) {
-  let a = js_declaration_param_add_curried(declaration);
+  let a = js_declaration_param_add_curry(declaration);
   each(param_names, a);
 }
