580a45eab7742fbd66b144193b6a80b494b48c4c
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:23:06 -0500

    function_auto_path ..\love\public\src\function_new_transform.mjs

--- public/src/function_new_transform.mjs
+++ public/src/function_new_transform.mjs
@@ -2,6 +2,6 @@ import { function_transform } from "../../../love/public/src/function_transform.
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function function_new_transform(f_name, lambda$ast) {
   await function_new(f_name);
-  let v = await function_transform(f_name, lambda$ast);
-  return v;
+  let output = await function_transform(f_name, lambda$ast);
+  return output;
 }
