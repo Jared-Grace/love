11ef54ffcd6a36fe65a082ec68c35d4e578001a3
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:59:54 -0500

    function_auto_path ..\love\public\src\function_curryify_generic.mjs

--- public/src/function_curryify_generic.mjs
+++ public/src/function_curryify_generic.mjs
@@ -13,14 +13,11 @@ import { function_new_transform } from "../../../love/public/src/function_new_tr
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
-export async function function_curryify_generic(
-  f_name,
-  f_name_curried,
-  args_get,
-) {
+export async function function_curryify_generic(f_name, name_get, args_get) {
   let u = await function_parse_declaration_unaliased(f_name);
   let unaliased = property_get(u, "unaliased");
   let declaration_call = property_get(u, "declaration");
+  let f_name_curried = name_get(unaliased);
   let output = await function_new_transform(f_name_curried, lambda);
   return output;
   async function lambda(ast) {
