e1f421d83073472e2a4e467b37c795479061490b
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:36:58 -0500

    function_auto_path ..\love\public\src\js_call_args_await_maybe_declaration_return_add.mjs

--- web/public/src/js_call_args_await_maybe_declaration_return_add.mjs
+++ web/public/src/js_call_args_await_maybe_declaration_return_add.mjs
@@ -1,6 +1,6 @@
 import { js_code_call_args_await_maybe_return } from "../../../love/public/src/js_code_call_args_await_maybe_return.mjs";
 import { js_declaration_single_block_body_add } from "../../../love/public/src/js_declaration_single_block_body_add.mjs";
-export function js_code_call_args_await_maybe_declaration_return_add(
+export function js_call_args_await_maybe_declaration_return_add(
   unaliased,
   arg_names,
   declaration_call,
