a76973fb218c3dd29f7737e792ab75502f7419aa
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:10:33 -0500

    function_auto_path ..\love\public\src\property_initialize_empty.mjs

--- web/public/src/object_property_initialize_empty.mjs
+++
@@ -1,5 +0,0 @@
-import { property_initialize } from "../../../love/public/src/property_initialize.mjs";
-export function object_property_initialize_empty(object, property_name) {
-  let value2 = property_initialize(object, property_name, {});
-  return value2;
-}
