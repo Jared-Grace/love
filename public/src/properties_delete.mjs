c229f3c698d26fc099ac4fd57a301599964125c2
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:47:14 -0500

    function_auto_path ..\love\public\src\properties_delete.mjs

--- public/src/properties_delete.mjs
+++ public/src/properties_delete.mjs
@@ -1,6 +1,6 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { object_property_delete } from "../../../love/public/src/object_property_delete.mjs";
-export function object_properties_delete(data, properties) {
+export function properties_delete(data, properties) {
   function lambda2(p) {
     object_property_delete(data, p);
   }
