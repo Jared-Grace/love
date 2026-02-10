3b632901e3c4dc3b1b4bd24fe35e2458f01edd9c
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:48:16 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- web/public/src/sandbox_5.mjs
+++ web/public/src/sandbox_5.mjs
@@ -33,7 +33,7 @@ import { object_property_change_async } from "../../../love/public/src/object_pr
 import { object_property_change } from "../../../love/public/src/object_property_change.mjs";
 import { object_property_add_subtract_1 } from "../../../love/public/src/object_property_add_subtract_1.mjs";
 import { object_property_add_1 } from "../../../love/public/src/object_property_add_1.mjs";
-import { object_properties_size } from "../../../love/public/src/object_properties_size.mjs";
+import { properties_size } from "../../../love/public/src/properties_size.mjs";
 import { properties_from_empty } from "../../../love/public/src/properties_from_empty.mjs";
 import { properties_from } from "../../../love/public/src/properties_from.mjs";
 import { properties_each_async } from "../../../love/public/src/properties_each_async.mjs";
@@ -59,7 +59,7 @@ export async function sandbox_5() {
     properties_each_async: "public/src/object_properties_each_async.mjs",
     properties_from: "public/src/object_properties_from.mjs",
     properties_from_empty: "public/src/object_properties_from_empty.mjs",
-    object_properties_size: "public/src/object_properties_size.mjs",
+    properties_size: "public/src/object_properties_size.mjs",
     object_property_add_1: "public/src/object_property_add_1.mjs",
     object_property_add_subtract_1:
       "public/src/object_property_add_subtract_1.mjs",
