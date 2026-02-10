3e2d8705957e9d97320d4ff046fc0a3f44c3766a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:12:53 -0500

    function_auto_path ..\love\public\src\text_match_ordered.mjs

--- public/src/text_match_ordered.mjs
+++ public/src/text_match_ordered.mjs
@@ -1,5 +1,5 @@
-export function text_match_ordered(t, search) {
-  const escaped = t.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+export function text_match_ordered(input, search) {
+  const escaped = input.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
   let regex_text = escaped.split("").join(".*");
   const regex = new RegExp(regex_text);
   let matches = regex.test(search);
