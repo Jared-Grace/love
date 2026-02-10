930db1a9ebf0031132161da2934eb022501a14da
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:03:31 -0500

    function_auto_path ..\love\public\src\each_curried_right.mjs

--- public/src/each_curried_right.mjs
+++ public/src/each_curried_right.mjs
@@ -1,6 +1,8 @@
 import { each } from "../../../love/public/src/each.mjs";
 export function each_curried_right(lambda$item) {
-  return function each_curried_right_result(list) {
-    return each(list, lambda$item);
+  let r2 = function each_curried_right_result(list) {
+    let r = each(list, lambda$item);
+    return r;
   };
+  return r2;
 }
