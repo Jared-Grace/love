a27028d85df980621d76954aec6cdbe206a2ed97
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:32:53 -0500

    function_auto_path ..\love\public\src\app_replace_rule_valid_curry.mjs

--- public/src/app_replace_rule_valid_curry.mjs
+++ public/src/app_replace_rule_valid_curry.mjs
@@ -1,5 +1,5 @@
 import { app_replace_rule_valid } from "../../../love/public/src/app_replace_rule_valid.mjs";
-export function app_replace_rule_valid_curried(rule, current_list) {
+export function app_replace_rule_valid_curry(rule, current_list) {
   let r = function app_replace_rule_valid_curried_result(index_selected) {
     let eq = app_replace_rule_valid(rule, index_selected, current_list);
     return eq;
