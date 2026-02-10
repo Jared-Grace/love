576438fc25850648266f28ef686dcbb97270d8e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:10:09 -0500

    function_auto_path ..\love\public\src\html_style_background_color.mjs

--- web/public/src/html_style_background_color.mjs
+++ web/public/src/html_style_background_color.mjs
@@ -1,6 +1,6 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export function html_style_background_color(div, background) {
-  html_style_assign(div, {
+export function html_style_background_color(component, background) {
+  html_style_assign(component, {
     "background-color": background,
   });
 }
