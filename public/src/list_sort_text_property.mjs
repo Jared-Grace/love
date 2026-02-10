d66d28fecd4dfec6f2a265b7f6d31ef694a764e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:10:00 -0500

    function_auto_path ..\love\public\src\property_get_curry_right.mjs

--- public/src/list_sort_text_property.mjs
+++ public/src/list_sort_text_property.mjs
@@ -1,6 +1,6 @@
-import { object_property_get_curry_right } from "../../../love/public/src/object_property_get_curry_right.mjs";
+import { property_get_curry_right } from "../../../love/public/src/property_get_curry_right.mjs";
 import { list_sort_text_mapper } from "../../../love/public/src/list_sort_text_mapper.mjs";
 export function list_sort_text_property(languages, property_name) {
-  let f = object_property_get_curry_right(property_name);
+  let f = property_get_curry_right(property_name);
   list_sort_text_mapper(languages, f);
 }
