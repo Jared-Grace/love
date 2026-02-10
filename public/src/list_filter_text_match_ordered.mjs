721e1bcc3a13bb7f742f7304b879eb9827376e44
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:33:02 -0500

    p

--- public/src/list_filter_text_match_ordered.mjs
+++ public/src/list_filter_text_match_ordered.mjs
@@ -1,7 +1,7 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { text_match_ordered_curried } from "../../../love/public/src/text_match_ordered_curried.mjs";
+import { text_match_ordered_curry } from "../../../love/public/src/text_match_ordered_curry.mjs";
 export function list_filter_text_match_ordered(values, query) {
-  let lambda = text_match_ordered_curried(query);
+  let lambda = text_match_ordered_curry(query);
   let filtered = list_filter(values, lambda);
   return filtered;
 }
