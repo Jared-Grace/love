8b89f772c4c3292d30d77045687bedfc4ded6d5e
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:04 -0500

    function_auto_path ..\love\public\src\text_transform_lookup.mjs

---
+++ web/public/src/property_exists_assert.mjs
@@ -0,0 +1,13 @@
+import { property_exists } from "../../../love/public/src/property_exists.mjs";
+import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
+export function property_exists_assert(object, property_name) {
+  let result = property_exists(object, property_name);
+  function lambda() {
+    let v = {
+      object,
+      property_name,
+    };
+    return v;
+  }
+  assert_json_get(result, lambda);
+}
