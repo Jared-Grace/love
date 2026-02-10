09910e541854e1ea8de44a09d5d711eb159e03e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:17:11 -0500

    function_auto_path ..\love\public\src\text_match_ordered.mjs

--- public/src/text_match_ordered.mjs
+++ public/src/text_match_ordered.mjs
@@ -1,7 +1,7 @@
-export function text_match_ordered(query, input) {
+export function text_match_ordered(query, value) {
   const escaped = query.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
   let regex_text = escaped.split("").join(".*");
   const regex = new RegExp(regex_text);
-  let matches = regex.test(input);
+  let matches = regex.test(value);
   return matches;
 }
