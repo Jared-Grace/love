797cd7f5dd297520aa7a58e8c12c152bab77201c
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:49:11 -0500

    function_auto_path ..\love\public\src\property_delete_multiple.mjs

--- public/src/object_property_delete_multiple.mjs
+++
@@ -1,8 +0,0 @@
-import { each } from "../../../love/public/src/each.mjs";
-import { property_delete } from "../../../love/public/src/property_delete.mjs";
-export function object_property_delete_multiple(o, list) {
-  function lambda(item) {
-    property_delete(o, item);
-  }
-  each(list, lambda);
-}
