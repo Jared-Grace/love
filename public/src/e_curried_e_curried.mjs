f1b9bc3b99b53560ea54d4d04ee3a5417f73c98a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:01:07 -0500

    de e_curried_e_curried

--- public/src/e_curried_e_curried.mjs
+++
@@ -1,8 +0,0 @@
-import { each } from "../../../love/public/src/each.mjs";
-export function e_curried_e_curried(lambda$item) {
-  let r2 = function e_curried_e_curried_result(list) {
-    let r = each(list, lambda$item);
-    return r;
-  };
-  return r2;
-}
