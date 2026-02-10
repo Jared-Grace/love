6f87da4cbce5e25c56496b11f497cb3ad8b757fd
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 15:54:14 -0500

    function_curryify_choose app_replace_rule_valid 1

--- public/src/app_replace_rule_valid_curried.mjs
+++ public/src/app_replace_rule_valid_curried.mjs
@@ -1,1 +1,6 @@
-export function app_replace_rule_valid_curried() {}
+import { app_replace_rule_valid } from "../../../love/public/src/app_replace_rule_valid.mjs";
+export function app_replace_rule_valid_curried(rule, current_list) {
+  return function app_replace_rule_valid_curried_result(index_selected) {
+    return app_replace_rule_valid(rule, index_selected, current_list);
+  };
+}
