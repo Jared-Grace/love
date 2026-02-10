cad4f985821aac4dfde88cc1b7fef70379ef6e8b
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 13:31:16 -0500

    function_auto_path ..\love\public\src\html_button_notext.mjs

--- public/src/html_button_notext.mjs
+++ public/src/html_button_notext.mjs
@@ -1,9 +1,7 @@
-import { html_style_font_size_inherit } from "../../../love/public/src/html_style_font_size_inherit.mjs";
 import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
 import { html_button_element } from "../../../love/public/src/html_button_element.mjs";
 export function html_button_notext(parent, lambda) {
   let component = html_button_element(parent);
   html_on_click(component, lambda);
-  html_style_font_size_inherit(component);
   return component;
 }
