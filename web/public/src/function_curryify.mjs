bbf69791a7f8344d18d9b2e0040a2c1ac45f8eb9
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:24:08 -0500

    function_auto_path ..\love\public\src\function_curryify.mjs

--- web/public/src/function_curryify.mjs
+++ web/public/src/function_curryify.mjs
@@ -11,7 +11,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_declaration_params_names } from "../../../love/public/src/js_declaration_params_names.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 export async function function_curryify(f_name) {
-  let combined = function_name_combine(left, right);
+  let f_name_curried = function_name_combine(f_name, "curried");
   let u = await function_parse_declaration_unaliased(f_name);
   let unaliased = property_get(u, "unaliased");
   let declaration_call = property_get(u, "declaration");
