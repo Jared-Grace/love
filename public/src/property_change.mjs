9a06469ff1a22cc0b8db58c7d27a2bdebe028a2c
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:48:29 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

---
+++ public/src/property_change.mjs
@@ -0,0 +1,8 @@
+import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
+import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+export function property_change(o, property, lambda$value) {
+  let value = object_property_get(o, property);
+  value = lambda$value(value);
+  object_property_set(o, property, value);
+  return value;
+}
