90fff87f3edddaa0ea0517c0df84a37ccf506486
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 15:49:49 -0500

    function_auto_path ..\love\public\src\function_curryify_choose.mjs

--- public/src/function_curryify_choose.mjs
+++ public/src/function_curryify_choose.mjs
@@ -10,7 +10,8 @@ export async function function_curryify_choose(f_name, index_text) {
   return output;
   function args_get(arg_names) {
     let item = list_get(arg_names, index);
-    let copy = list_copy(original);
+    let copy = list_copy(arg_names);
+    lra;
     let fr = list_first_remaining(arg_names);
     let first = property_get(fr, "first");
     let fn_new_result_args = property_get(fr, "remaining");
