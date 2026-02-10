0313451a875239953e8bca5526290e12b97ac123
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:32:55 -0500

    function_auto_path ..\love\public\src\function_wrap_replace_last.mjs

--- web/public/src/function_wrap_replace_last.mjs
+++ web/public/src/function_wrap_replace_last.mjs
@@ -1,9 +1,7 @@
-import { text_replace } from "../../../love/public/src/text_replace.mjs";
+import { function_wrap_replace } from "../../../love/public/src/function_wrap_replace.mjs";
 import { function_name_to_part_last } from "../../../love/public/src/function_name_to_part_last.mjs";
-import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_replace_last(f_name_before, suffix) {
   let from = function_name_to_part_last(f_name_before);
-  let f_name_wrapped = text_replace(f_name_before, from, suffix);
-  let r = await function_wrap(f_name_before, f_name_wrapped);
+  let r = await function_wrap_replace(f_name_before, from, suffix);
   return r;
 }
