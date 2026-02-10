b73182af685ff5ee707a379cfdd416dabdcffb67
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:56:23 -0500

    cur e

--- public/src/e_curried_e_curried.mjs
+++ public/src/e_curried_e_curried.mjs
@@ -1,1 +1,6 @@
-export function e_curried_e_curried() {}
+import { each } from "../../../love/public/src/each.mjs";
+export function e_curried_e_curried(lambda$item) {
+  return function e_curried_e_curried_result(list) {
+    return each(list, lambda$item);
+  };
+}
