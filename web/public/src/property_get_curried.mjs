9f83ec16e61e046356dba12081ae946e2dd4ec7a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:52:02 -0500

    function_auto_path ..\love\public\src\property_get_curried.mjs

--- web/public/src/property_get_curried.mjs
+++ web/public/src/property_get_curried.mjs
@@ -1,6 +1,8 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function property_get_curried(object) {
-  return function property_get_curried_result(property_name) {
-    return property_get(object, property_name);
+  let r = function property_get_curried_result(property_name) {
+    let value = property_get(object, property_name);
+    return value;
   };
+  return r;
 }
