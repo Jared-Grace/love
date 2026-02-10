7875a7c9491d370334bb760a5de0ea828694d1a6
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:47:55 -0500

    function_auto_path ..\love\public\src\app_a_function.mjs

--- web/public/src/each_object.mjs
+++ web/public/src/each_object.mjs
@@ -1,11 +1,11 @@
 import { each } from "../../../love/public/src/each.mjs";
-import { object_properties_get } from "../../../love/public/src/object_properties_get.mjs";
+import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 export function each_object(object, lambda$value$property) {
   function lambda2(property) {
     let value = object_property_get(object, property);
     lambda$value$property(value, property);
   }
-  let properties = object_properties_get(object);
+  let properties = properties_get(object);
   each(properties, lambda2);
 }
--- web/public/src/functions_search_open.mjs
+++ web/public/src/functions_search_open.mjs
@@ -1,10 +1,10 @@
 import { functions_search } from "../../../love/public/src/functions_search.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
-import { object_properties_get } from "../../../love/public/src/object_properties_get.mjs";
+import { properties_get } from "../../../love/public/src/properties_get.mjs";
 export async function functions_search_open(search) {
   let result = await functions_search(search);
-  let properties = object_properties_get(result);
+  let properties = properties_get(result);
   let f_name = list_single(properties);
   await function_open(f_name);
 }
