ef63a2b0907ba7517a817b48ecf9fd31654d3064
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:41:31 -0500

    rs object_properties get

--- web/public/src/function_parse_declaration_js_unparse.mjs
+++ web/public/src/function_parse_declaration_js_unparse.mjs
@@ -1,10 +1,10 @@
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { function_parse_declaration } from "../../../love/public/src/function_parse_declaration.mjs";
-import { object_properties } from "../../../love/public/src/object_properties.mjs";
+import { object_properties_get } from "../../../love/public/src/object_properties_get.mjs";
 export async function function_parse_declaration_js_unparse(f_name) {
   "todo rename remove js";
   let { declaration } = await function_parse_declaration(f_name);
-  let properties = object_properties(declaration);
+  let properties = object_properties_get(declaration);
   let output = js_unparse(declaration);
   return output;
 }
--- web/public/src/object_properties_size.mjs
+++ web/public/src/object_properties_size.mjs
@@ -1,7 +1,7 @@
 import { list_size } from "../../../love/public/src/list_size.mjs";
-import { object_properties } from "../../../love/public/src/object_properties.mjs";
+import { object_properties_get } from "../../../love/public/src/object_properties_get.mjs";
 export function object_properties_size(s) {
-  let properties = object_properties(s);
+  let properties = object_properties_get(s);
   let size = list_size(properties);
   return size;
 }
