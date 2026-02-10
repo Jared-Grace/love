ec6ff167ab28ddbd0ae66c7faa77bf2d2c419863
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:36:40 -0500

    function_auto_path ..\love\public\src\function_curryify_choose.mjs

--- web/public/src/function_curryify_choose.mjs
+++ web/public/src/function_curryify_choose.mjs
@@ -1,10 +1,8 @@
 import { function_curryify_choose_args_get } from "../../../love/public/src/function_curryify_choose_args_get.mjs";
 import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
-import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
 export async function function_curryify_choose(f_name, index_text) {
-  let index = integer_to(index_text);
-  let args_get = function_curryify_choose_args_get(index);
+  let args_get = function_curryify_choose_args_get(index_text);
   let output = await function_curryify_generic(
     f_name,
     args_get,
