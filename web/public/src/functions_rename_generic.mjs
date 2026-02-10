78b0015955e83172dbfe98255fd57415268ebc33
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:37:49 -0500

    function_auto_path ..\love\public\src\functions_rename_generic.mjs

--- web/public/src/functions_rename_generic.mjs
+++ web/public/src/functions_rename_generic.mjs
@@ -1,14 +1,14 @@
+import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_empty_not_is_assert } from "../../../love/public/src/list_empty_not_is_assert.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function functions_rename_generic(filter, name_change) {
   let f_names = await functions_names();
   let filtered = list_filter(f_names, filter);
   list_empty_not_is_assert(filtered);
-  await each_async(filtered, lambda);
+  await list_map_async(filtered, lambda);
   async function lambda(f_name_before) {
     let f_name_after = name_change(f_name_before);
     if (equal(f_name_before, f_name_after)) {
