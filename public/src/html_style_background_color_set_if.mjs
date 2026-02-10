6c728e5459a576a6fb4cfd9f84fb9c4f8db81223
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:27:33 -0500

    o ternary

--- public/src/html_style_background_color_set_if.mjs
+++ public/src/html_style_background_color_set_if.mjs
@@ -5,11 +5,15 @@ export function html_style_background_color_set_if(
   color_if,
   color_else,
 ) {
+  ternary(condition, color_if, color_else);
+  html_style_background_color(component, color_else);
+}
+function ternary(condition, color_if, color_else) {
   let c = null;
   if (condition) {
     c = color_if;
   } else {
     c = color_else;
   }
-  html_style_background_color(component, color_else);
 }
+
