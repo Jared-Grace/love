cd381ba7227415e8baab967c33b73ec7048c79ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:34:22 -0500

    function_auto_path ..\love\public\src\functions_rename_parts_delete.mjs

--- public/src/functions_rename_parts_delete.mjs
+++ public/src/functions_rename_parts_delete.mjs
@@ -1,10 +1,12 @@
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_rename_parts_delete } from "../../../love/public/src/function_rename_parts_delete.mjs";
+import { list_map_async } from "./list_map_async.mjs";
 export async function functions_rename_parts_delete(f_names_comma, deleted) {
-  let split = text_split_comma(f_names_comma);
-  async function lambda(item) {}
-  await each_async(list, lambda);
-  let r = await function_rename_parts_delete(f_name_old, deleted);
+  let f_names = text_split_comma(f_names_comma);
+  async function lambda(f_name) {
+    let r = await function_rename_parts_delete(f_name, deleted);
+    return r;
+  }
+  await list_map_async(f_names, lambda);
   return r;
 }
