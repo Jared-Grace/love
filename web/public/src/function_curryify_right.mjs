1032c28f1c4ce2fd7dc2cc1432b5cf7911f2ad1f
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:25:35 -0500

    fwrl function_curryify_choose right

---
+++ web/public/src/function_curryify_right.mjs
@@ -0,0 +1,4 @@
+import { function_curryify_choose } from "../../../love/public/src/function_curryify_choose.mjs";
+export async function function_curryify_right(f_name, index_text) {
+  return await function_curryify_choose(f_name, index_text);
+}
