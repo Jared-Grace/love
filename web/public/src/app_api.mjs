3c9bf595b1dfd8a58f3950836f21041d622947f9
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:58:08 -0500

    c

--- web/public/src/app_api.mjs
+++ web/public/src/app_api.mjs
@@ -1,12 +1,12 @@
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { object_property_set_exists_not } from "../../../love/public/src/object_property_set_exists_not.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { app_api_generic } from "../../../love/public/src/app_api_generic.mjs";
 import { http_post_json } from "../../../love/public/src/http_post_json.mjs";
 export async function app_api(a) {
   assert_arguments(arguments, 1);
-  let f_name = object_property_get(a, "f_name");
+  let f_name = property_get(a, "f_name");
   text_is_assert(f_name);
   let fn_http = http_post_json;
   object_property_set_exists_not(a, "fn_http", fn_http);
