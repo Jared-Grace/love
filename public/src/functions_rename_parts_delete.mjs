55e2af29370bbf66cb462891b9771334fbbc6a2a
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:33:37 -0500

    function_auto_path ..\love\public\src\functions_rename_parts_delete.mjs

--- public/src/functions_rename_parts_delete.mjs
+++ public/src/functions_rename_parts_delete.mjs
@@ -1,5 +1,7 @@
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_rename_parts_delete } from "../../../love/public/src/function_rename_parts_delete.mjs";
-export async function functions_rename_parts_delete(f_name_old, deleted) {
+export async function functions_rename_parts_delete(f_names_comma, deleted) {
+  let split = text_split_comma(f_names);
   let r = await function_rename_parts_delete(f_name_old, deleted);
   return r;
 }
