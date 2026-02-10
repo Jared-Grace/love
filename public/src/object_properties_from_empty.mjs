3dbd97caa6c4a424ed25f8a76c8a0bb6e204e918
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:47:45 -0500

    function_auto_path ..\love\public\src\properties_from_empty.mjs

--- public/src/object_properties_from_empty.mjs
+++
@@ -1,5 +0,0 @@
-import { properties_from } from "../../../love/public/src/properties_from.mjs";
-export function object_properties_from_empty(o, properties) {
-  let r = properties_from({}, properties, o);
-  return r;
-}
