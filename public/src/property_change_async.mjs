779fa34cfa046701c442772f4cfd954b90888dd3
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:48:39 -0500

    function_auto_path ..\love\public\src\ebible_verses_upload_compare.mjs

--- public/src/property_change_async.mjs
+++ public/src/property_change_async.mjs
@@ -1,8 +1,8 @@
 import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export function object_property_change(o, property, lambda$value) {
+export async function property_change_async(o, property, lambda$value) {
   let value = object_property_get(o, property);
-  value = lambda$value(value);
+  value = await lambda$value(value);
   object_property_set(o, property, value);
   return value;
 }
