a9184c6bce212fdd5fabb7085e5130b1846de7ab
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:37:08 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -127,7 +127,7 @@ export async function app_replace_rule_set(context) {
         "lightgreen",
         "#d7d7d7",
       );
-      html_style_font_color_set_if(enabled, rb, "black", "#898989ff");
+      html_style_font_color_set_if(enabled, rb, "black", "#6c6c6cff");
     }
     each_index(rules_buttons, each_rule_button);
     html_clear(div);
