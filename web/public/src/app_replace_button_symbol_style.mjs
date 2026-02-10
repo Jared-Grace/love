465d2255d58452574d37e527751356de1c312e5e
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 13:11:12 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

---
+++ web/public/src/app_replace_button_symbol_style.mjs
@@ -0,0 +1,8 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+import { html_margin_em } from "../../../love/public/src/html_margin_em.mjs";
+import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
+export function app_replace_button_symbol_style(b) {
+  html_border_radius_em(b, 0.5);
+  html_margin_em(b, 0.09);
+  html_style_set(b, "border", 0);
+}
