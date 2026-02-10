82de74e7f4a8445639c5511477e9eb6c6c509c56
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:11:08 -0500

    function_auto_path ..\love\public\src\property_rename.mjs

--- web/public/src/g_objection_generate_migrate.mjs
+++ web/public/src/g_objection_generate_migrate.mjs
@@ -1,5 +1,5 @@
 import { g_objection_generate_property } from "../../../love/public/src/g_objection_generate_property.mjs";
-import { object_property_rename } from "../../../love/public/src/object_property_rename.mjs";
+import { property_rename } from "../../../love/public/src/property_rename.mjs";
 import { file_json_transform } from "../../../love/public/src/file_json_transform.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -11,7 +11,7 @@ export async function g_objection_generate_migrate() {
       function lambda3(item) {
         const property_name_before = "objection";
         let property_name_after = g_objection_generate_property();
-        object_property_rename(item, property_name_before, property_name_after);
+        property_rename(item, property_name_before, property_name_after);
       }
       each(passages, lambda3);
     }
