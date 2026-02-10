aa666703787641503150ec2704e58126a0e9b687
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:31:44 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,6 @@
 import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
 import { function_curryify } from "../../../love/public/src/function_curryify.mjs";
 export async function sandbox() {
-  await function_delete_if_exists("text_match_ordered_result");
+  await function_delete_if_exists("text_match_ordered_curried");
   await function_curryify("text_match_ordered");
 }
