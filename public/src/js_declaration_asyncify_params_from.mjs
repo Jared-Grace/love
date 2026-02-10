4f5ffcd70a9a7d96cdf79f4790a424126572cb0b
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:55 -0500

    function_auto_path ..\love\public\src\sandbox_5.mjs

--- public/src/js_declaration_asyncify_params_from.mjs
+++ public/src/js_declaration_asyncify_params_from.mjs
@@ -1,5 +1,5 @@
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
-import { object_property_from } from "../../../love/public/src/object_property_from.mjs";
+import { property_from } from "../../../love/public/src/property_from.mjs";
 import { js_declaration_asyncify } from "../../../love/public/src/js_declaration_asyncify.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export async function js_declaration_asyncify_params_from(
@@ -8,6 +8,6 @@ export async function js_declaration_asyncify_params_from(
 ) {
   let declaration = js_declaration_single(ast);
   js_declaration_asyncify(declaration, declaration_call);
-  object_property_from(declaration, "params", declaration_call);
+  property_from(declaration, "params", declaration_call);
   await js_imports_missing_add(ast);
 }
