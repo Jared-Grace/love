e206e2a61cb4f0659c1ba62acf63dfb37facd8a9
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:19:09 -0500

    function_auto_path ..\love\public\src\function_curryify.mjs

--- public/src/function_curryify.mjs
+++ public/src/function_curryify.mjs
@@ -1,8 +1,11 @@
+import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
+import { property_from } from "../../../love/public/src/property_from.mjs";
+import { js_declaration_asyncify } from "../../../love/public/src/js_declaration_asyncify.mjs";
+import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 import { js_declaration_single_block_body_add } from "../../../love/public/src/js_declaration_single_block_body_add.mjs";
 import { js_call_args_await_maybe_return } from "../../../love/public/src/js_call_args_await_maybe_return.mjs";
 import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_declaration_asyncify_params_from } from "../../../love/public/src/js_declaration_asyncify_params_from.mjs";
 import { js_declaration_params_names } from "../../../love/public/src/js_declaration_params_names.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 export async function function_curryify(f_name) {
@@ -19,6 +22,9 @@ export async function function_curryify(f_name) {
       declaration_call,
     );
     js_declaration_single_block_body_add(ast, item);
-    await js_declaration_asyncify_params_from(ast, declaration_call);
+    let declaration = js_declaration_single(ast);
+    js_declaration_asyncify(declaration, declaration_call);
+    property_from(declaration, "params", declaration_call);
+    await js_imports_missing_add(ast);
   }
 }
