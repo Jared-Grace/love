a74239de30a63d0ab4b15259966d0f30dc194e07
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:48:39 -0500

    function_auto_path ..\love\public\src\ebible_verses_upload_compare.mjs

--- public/src/property_add_1.mjs
+++ public/src/property_add_1.mjs
@@ -1,5 +1,5 @@
 import { add_1 } from "../../../love/public/src/add_1.mjs";
-import { object_property_change } from "../../../love/public/src/object_property_change.mjs";
+import { property_change } from "../../../love/public/src/property_change.mjs";
 export function property_add_1(a, property) {
-  object_property_change(a, property, add_1);
+  property_change(a, property, add_1);
 }
