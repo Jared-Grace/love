95abc917835ec5aa30e5340ec6eed747873440cd
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 13:31:21 -0500

    function_auto_path ..\love\public\src\html_button_element.mjs

--- public/src/html_button_element.mjs
+++ public/src/html_button_element.mjs
@@ -1,6 +1,8 @@
+import { html_style_font_size_inherit } from "../../../love/public/src/html_style_font_size_inherit.mjs";
 import { html_element } from "../../../love/public/src/html_element.mjs";
 export function html_button_element(parent) {
   const tag_name = "button";
   let b = html_element(parent, tag_name);
+  html_style_font_size_inherit(b);
   return b;
 }
