1afd03940c368939824638ebfd88d6d08655a4d3
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:49:18 -0500

    function_auto_path ..\love\public\src\property_equals.mjs

--- public/src/object_property_equals_lambda.mjs
+++ public/src/object_property_equals_lambda.mjs
@@ -1,7 +1,7 @@
-import { object_property_equals } from "../../../love/public/src/object_property_equals.mjs";
+import { property_equals } from "../../../love/public/src/property_equals.mjs";
 export function object_property_equals_lambda(property_name, property_value) {
   let v2 = function object_property_equals_lambda_result(item) {
-    let v = object_property_equals(item, property_name, property_value);
+    let v = property_equals(item, property_name, property_value);
     return v;
   };
   return v2;
