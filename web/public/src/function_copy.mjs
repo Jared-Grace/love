b43303033b28fd0dbb4a52ddfd63302c521c149a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:17:52 -0500

    function_auto_path ..\love\public\src\function_copy.mjs

--- web/public/src/function_copy.mjs
+++ web/public/src/function_copy.mjs
@@ -1,3 +1,4 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { user_repo_path_combine } from "../../../love/public/src/user_repo_path_combine.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
@@ -8,9 +9,11 @@ import { file_copy } from "../../../love/public/src/file_copy.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { file_open } from "../../../love/public/src/file_open.mjs";
 export async function function_copy(f_name_old, f_name_new) {
-  const { f_path: f_path_old, unaliased: unaliased_old } =
-    await function_name_to_path_unalias(f_name_old);
-  let { unaliased: unaliased_new } = await function_name_unalias(f_name_new);
+  const r = await function_name_to_path_unalias(f_name_old);
+  let unaliased_old = property_get(r, "unaliased");
+  let f_path_old = property_get(r, "f_path");
+  let r2 = await function_name_unalias(f_name_new);
+  let unaliased_new = property_get(r2, "unaliased");
   let f_path = function_name_to_path(f_name_new);
   let f_path_new = await user_repo_path_combine(f_path);
   await file_copy(f_path_old, f_path_new);
@@ -20,4 +23,5 @@ export async function function_copy(f_name_old, f_name_new) {
   await function_transform(unaliased_new, lambda);
   await file_open(f_path_new);
   await function_current_set(unaliased_new);
+  return unaliased_new;
 }
