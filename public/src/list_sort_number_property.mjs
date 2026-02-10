18e8cec7103a3bbb8b9498bd066beefe2ed05b5d
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:10:00 -0500

    function_auto_path ..\love\public\src\property_get_curry_right.mjs

--- public/src/list_sort_number_property.mjs
+++ public/src/list_sort_number_property.mjs
@@ -1,6 +1,6 @@
 import { list_sort_number_mapper } from "../../../love/public/src/list_sort_number_mapper.mjs";
-import { object_property_get_curry_right } from "../../../love/public/src/object_property_get_curry_right.mjs";
+import { property_get_curry_right } from "../../../love/public/src/property_get_curry_right.mjs";
 export function list_sort_number_property(list) {
-  let f = object_property_get_curry_right("delta");
+  let f = property_get_curry_right("delta");
   list_sort_number_mapper(list, f);
 }
