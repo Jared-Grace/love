495cf645ccb368508e293da3513005dbe27c2ac1
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:38:04 -0500

    function_auto_path ..\love\public\src\function_curryify_choose_args_get.mjs

--- public/src/function_curryify_right.mjs
+++ public/src/function_curryify_right.mjs
@@ -1,5 +1,10 @@
 import { function_curryify_choose } from "../../../love/public/src/function_curryify_choose.mjs";
 export async function function_curryify_right(f_name) {
-  let r = await function_curryify_choose(f_name, 0);
-  return r;
+    let args_get = function_curryify_choose_args_get(0);
+    let output = await function_curryify_generic(
+      f_name,
+      args_get,
+      function_curryify_generic_name,
+    );
+    return output;
 }
