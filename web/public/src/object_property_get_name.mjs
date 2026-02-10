96ed9fa91f8fa77ab6190de8d1a668599820e0a7
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:10:12 -0500

    function_auto_path ..\love\public\src\property_get_name.mjs

--- web/public/src/object_property_get_name.mjs
+++
@@ -1,5 +0,0 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-export function object_property_get_name(item) {
-  let name = property_get(item, "name");
-  return name;
-}
