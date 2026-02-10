55835533c2a98443087d35242f050a579273d039
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:30:01 -0500

    function_auto_path ..\love\public\src\function_curryify.mjs

--- web/public/src/function_curryify.mjs
+++ web/public/src/function_curryify.mjs
@@ -4,7 +4,7 @@ import { js_declaration_asyncify_params_from } from "../../../love/public/src/js
 import { js_code_call_args_await_maybe_declaration_return_add } from "../../../love/public/src/js_code_call_args_await_maybe_declaration_return_add.mjs";
 import { js_declaration_params_names } from "../../../love/public/src/js_declaration_params_names.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
-export async function function_curryify(f_name, count) {
+export async function function_curryify(f_name) {
   let r = await function_parse_declaration_unaliased(f_name);
   let unaliased = property_get(r, "unaliased");
   let declaration_call = property_get(r, "declaration");
