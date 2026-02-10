917f45f9fde9baab43b0e6e4a08f5ab5bf7d3f66
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:49:32 -0500

    function_auto_path ..\love\public\src\object_property_equals_json_lambda.mjs

---
+++ public/src/property_equals_json_lambda.mjs
@@ -0,0 +1,8 @@
+import { property_equals_json } from "../../../love/public/src/property_equals_json.mjs";
+export function property_equals_json_lambda(property_name, property_value) {
+  let v2 = function lambda(item) {
+    let v = property_equals_json(item, property_name, property_value);
+    return v;
+  };
+  return v2;
+}
