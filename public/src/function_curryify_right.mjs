c29c04883230c653783cac97b78231dee451164e
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:59:22 -0500

    function_auto_path ..\love\public\src\function_curryify_right.mjs

--- public/src/function_curryify_right.mjs
+++ public/src/function_curryify_right.mjs
@@ -1,10 +1,8 @@
-import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
 import { function_curryify_choose_args_get } from "../../../love/public/src/function_curryify_choose_args_get.mjs";
 export async function function_curryify_right(f_name) {
-  let unaliased = await function_name_unalias_only(f_name);
   let args_get = function_curryify_choose_args_get(0);
   let combined = function_curryify_generic_name(unaliased);
   let combined2 = function_name_combine(combined, "right");
