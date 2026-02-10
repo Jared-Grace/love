6af32d7fa6de5c4cdd772b2ecf848e69d91479c0
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:25:30 -0500

    function_auto_path ..\love\public\src\text_starts_with.mjs

--- web/public/src/text_starts_with.mjs
+++ web/public/src/text_starts_with.mjs
@@ -1,10 +1,4 @@
-import { log } from "../../../love/public/src/log.mjs";
 export function text_starts_with(t, prefix) {
   let sw = t.startsWith(prefix);
-  log({
-    t,
-    prefix,
-    sw,
-  });
   return sw;
 }
