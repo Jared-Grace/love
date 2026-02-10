fc9e90557d161b49a35ae5c9e0a4ffd7ecc305f0
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:26:19 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid_curried.mjs

--- public/src/app_replace_button_symbol_style_valid_curried.mjs
+++ public/src/app_replace_button_symbol_style_valid_curried.mjs
@@ -1,6 +1,8 @@
 import { app_replace_button_symbol_style_valid } from "../../../love/public/src/app_replace_button_symbol_style_valid.mjs";
 export function app_replace_button_symbol_style_valid_curried(valid) {
-  return function app_replace_button_symbol_style_valid_curried_result(sb) {
-    return app_replace_button_symbol_style_valid(sb, valid);
+  let r2 = function app_replace_button_symbol_style_valid_curried_result(sb) {
+    let r = app_replace_button_symbol_style_valid(sb, valid);
+    return r;
   };
+  return r2;
 }
