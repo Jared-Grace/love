b0a4941a785cebccdb05f9a1f046a1448d0cbf47
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:49:45 -0500

    function_auto_path ..\love\public\src\js_declaration_param_add_curried.mjs

--- web/public/src/js_declaration_param_add_curried.mjs
+++ web/public/src/js_declaration_param_add_curried.mjs
@@ -1,7 +1,8 @@
 import { js_declaration_param_add } from "../../../love/public/src/js_declaration_param_add.mjs";
 export function js_declaration_param_add_curried(declaration) {
   function js_declaration_param_add_curried_result(param_name) {
-    return js_declaration_param_add(declaration, param_name);
+    let r = js_declaration_param_add(declaration, param_name);
+    return r;
   }
   return js_declaration_param_add_curried_result;
 }
