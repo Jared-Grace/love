9309f66b092a3b440d397ee3cc6bc6a1c119f193
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:50:03 -0500

    function_auto_path ..\love\public\src\object_property_initialize_lambda_async.mjs

--- public/src/list_filter_property_exclude_if_exists.mjs
+++ public/src/list_filter_property_exclude_if_exists.mjs
@@ -1,13 +1,13 @@
 import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { object_property_exists } from "../../../love/public/src/object_property_exists.mjs";
+import { property_exists } from "../../../love/public/src/property_exists.mjs";
 export function list_filter_property_exclude_if_exists(
   list,
   property_name,
   value,
 ) {
   function lambda(item) {
-    let exists = object_property_exists(item, property_name);
+    let exists = property_exists(item, property_name);
     if (exists) {
       let ne = object_property_get(item, property_name) !== value;
       return ne;
