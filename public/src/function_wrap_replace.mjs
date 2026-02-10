a15d29554f72256a40f8572bcd329fc493053f35
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:32:44 -0500

    function_auto_path ..\love\public\src\function_wrap_replace.mjs

--- public/src/function_wrap_replace.mjs
+++ public/src/function_wrap_replace.mjs
@@ -1,5 +1,7 @@
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
-export async function function_wrap_replace(f_name, f_name_wrapped) {
-  let r = await function_wrap(f_name, f_name_wrapped);
+export async function function_wrap_replace(f_name_before, from, suffix) {
+  let f_name_wrapped = text_replace(f_name_before, from, suffix);
+  let r = await function_wrap(f_name_before, f_name_wrapped);
   return r;
 }
