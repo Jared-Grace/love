a9edce944bb0c60434505d289bed18b9f1db0c8b
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:11:44 -0500

    function_auto_path ..\love\public\src\property_subtract_1.mjs

--- public/src/object_property_subtract_1.mjs
+++
@@ -1,5 +0,0 @@
-import { subtract_1 } from "../../../love/public/src/subtract_1.mjs";
-import { property_change } from "../../../love/public/src/property_change.mjs";
-export function object_property_subtract_1(a, property) {
-  property_change(a, property, subtract_1);
-}
