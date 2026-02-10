186685f7c14a9b9a33560c173305b815a61807ce
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 20:10:47 -0500

    function_auto_path ..\love\public\src\property_initialize_lambda_async.mjs

--- web/public/src/global_function_property_initialize_async.mjs
+++ web/public/src/global_function_property_initialize_async.mjs
@@ -1,4 +1,4 @@
-import { object_property_initialize_lambda_async } from "../../../love/public/src/object_property_initialize_lambda_async.mjs";
+import { property_initialize_lambda_async } from "../../../love/public/src/property_initialize_lambda_async.mjs";
 import { property_initialize } from "../../../love/public/src/property_initialize.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 export async function global_function_property_initialize_async(
@@ -8,7 +8,7 @@ export async function global_function_property_initialize_async(
 ) {
   let global = global_get();
   let fn_object = property_initialize(global, fn.name, {});
-  let value = await object_property_initialize_lambda_async(
+  let value = await property_initialize_lambda_async(
     fn_object,
     property_name,
     lambda,
