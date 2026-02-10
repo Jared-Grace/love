59fcec4f086f18713165d12ee1fe0822a74bb436
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:58:08 -0500

    c

--- public/src/app_api_generic.mjs
+++ public/src/app_api_generic.mjs
@@ -1,16 +1,16 @@
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { app_api_generic_url_body } from "../../../love/public/src/app_api_generic_url_body.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-import { object_property_initialize } from "./object_property_initialize.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { object_property_initialize } from "../../../love/public/src/object_property_initialize.mjs";
 export async function app_api_generic(a) {
   assert_arguments(arguments, 1);
-  let f_name = object_property_get(a, "f_name");
+  let f_name = property_get(a, "f_name");
   let args = object_property_initialize(a, "args", []);
-  let fn_http = object_property_get(a, "fn_http");
+  let fn_http = property_get(a, "fn_http");
   var v = app_api_generic_url_body(f_name, args);
-  let body = object_property_get(v, "body");
-  let url = object_property_get(v, "url");
+  let body = property_get(v, "body");
+  let url = property_get(v, "url");
   let o = await fn_http(url, body);
-  let result = object_property_get(o, "result");
+  let result = property_get(o, "result");
   return result;
 }
