b5059bda959d44681f2e183ddc7c479441a0dc59
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:52:23 -0500

    function_auto_path ..\love\public\src\html_margin_em.mjs

--- public/src/html_margin_em.mjs
+++ public/src/html_margin_em.mjs
@@ -1,5 +1,5 @@
 import { html_margin } from "../../../love/public/src/html_margin.mjs";
-export function html_margin_em(b, margin) {
+export function html_margin_em(component, margin) {
   let value_em = margin + "em";
-  html_margin(b, value_em);
+  html_margin(component, value_em);
 }
