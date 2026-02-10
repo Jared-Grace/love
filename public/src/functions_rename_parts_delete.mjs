73e3f0de2e3563673b85d8e748e0bdceff4f5929
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 22:33:21 -0500

    fwr function_rename_parts_delete ion ions

--- public/src/functions_rename_parts_delete.mjs
+++ public/src/functions_rename_parts_delete.mjs
@@ -1,1 +1,4 @@
-export function functions_rename_parts_delete() {}
+import { function_rename_parts_delete } from "../../../love/public/src/function_rename_parts_delete.mjs";
+export async function functions_rename_parts_delete(f_name_old, deleted) {
+  return await function_rename_parts_delete(f_name_old, deleted);
+}
