dec0b1f7f2706d3b48a34cfa09e10e384af9a261
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 13:22:45 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -1,3 +1,4 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { html_button_notext } from "../../../love/public/src/html_button_notext.mjs";
 import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
@@ -70,6 +71,7 @@ export async function app_replace_rule_set(context) {
       return;
     }
     let b = html_button_notext(root, lambda3);
+    let span = html_span_text(parent, text2);
     html_disable(b);
     object_merge(b, {
       rule,
