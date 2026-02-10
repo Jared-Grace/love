e097d22627da0a49df7f17f28618bf26f087a486
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:29:24 -0500

    function_auto_path ..\love\public\src\html_style_font_color_set_if.mjs

--- web/public/src/html_style_font_color_set_if.mjs
+++ web/public/src/html_style_font_color_set_if.mjs
@@ -1,5 +1,4 @@
 import { ternary } from "../../../love/public/src/ternary.mjs";
-import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
 export function html_style_font_color_set_if(
   condition,
   component,
@@ -7,5 +6,5 @@ export function html_style_font_color_set_if(
   color_else,
 ) {
   let c = ternary(condition, color_if, color_else);
-  html_style_background_color(component, c);
+  html_style_font_color(component, c);
 }
