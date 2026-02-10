75365f3dc3e592ec3bb5e02bc9c6f2caaa99e79e
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:36:31 -0500

    function_auto_path ..\love\public\src\function_curryify_choose.mjs

---
+++ public/src/function_curryify_choose_args_get.mjs
@@ -0,0 +1,17 @@
+import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
+import { list_copy } from "../../../love/public/src/list_copy.mjs";
+import { list_get } from "../../../love/public/src/list_get.mjs";
+export function function_curryify_choose_args_get(index) {
+  let r2 = function lambda(arg_names) {
+    let item = list_get(arg_names, index);
+    let fn_new_result_args = [item];
+    let fn_new_args = list_copy(arg_names);
+    list_remove_at(fn_new_args, index);
+    let r = {
+      fn_new_result_args,
+      fn_new_args,
+    };
+    return r;
+  };
+  return r2;
+}
