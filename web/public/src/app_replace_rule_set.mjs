977677ceb854baac7a7b89079363bcf066edd168
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:49:27 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -101,7 +101,7 @@ export async function app_replace_rule_set(context) {
       }
       let b = html_button(div, letter, symbol_on_click);
       html_style_background_color(b, "darkgreen");
-      html_border_radius_em(b, 0.95);
+      html_border_radius_em(b, 0.3);
       property_set_exists_not(b, "index", index);
       return b;
     }
