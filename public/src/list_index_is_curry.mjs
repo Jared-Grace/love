3688cc68d0ec725d1436aed766eb1aaa5354849a
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:28:00 -0500

    function_auto_path ..\love\public\src\list_index_is_curry.mjs

--- public/src/list_index_is_curry.mjs
+++ public/src/list_index_is_curry.mjs
@@ -1,6 +1,6 @@
 import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 export function list_index_is_curry(list) {
-  let r2 = function lambda(index) {
+  let r2 = function list_index_is_curry_result(index) {
     let ii = list_index_is(list, index);
     return ii;
   };
