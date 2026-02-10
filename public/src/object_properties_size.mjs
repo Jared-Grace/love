709ad77fa0cb95ffc468f2154b674f04ce6fadd5
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:48:11 -0500

    function_auto_path ..\love\public\src\tasks_run.mjs

--- public/src/object_properties_size.mjs
+++
@@ -1,7 +0,0 @@
-import { list_size } from "../../../love/public/src/list_size.mjs";
-import { properties_get } from "../../../love/public/src/properties_get.mjs";
-export function object_properties_size(s) {
-  let properties = properties_get(s);
-  let size = list_size(properties);
-  return size;
-}
