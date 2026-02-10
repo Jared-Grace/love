c3351bb46c301c599a5433189acdf9b247b3eb9b
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 17:15:37 -0500

    function_auto_path ..\love\public\src\html_style_background_color_set_if.mjs

--- web/public/src/html_style_background_color_set_if.mjs
+++ web/public/src/html_style_background_color_set_if.mjs
@@ -1,9 +1,13 @@
-import { html_style_remove } from "../../../love/public/src/html_style_remove.mjs";
 import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
-export function html_style_background_color_set_if(chosen, component, color) {
+export function html_style_background_color_set_if(
+  chosen,
+  component,
+  color_if,
+  color_else,
+) {
   if (chosen) {
-    html_style_background_color(component, color);
+    html_style_background_color(component, color_if);
   } else {
-    html_style_remove(component, "background-color");
+    html_style_background_color(component, color_else);
   }
 }
