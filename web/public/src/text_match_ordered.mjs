05c1d1d3fa167e9eeefcc60f27d15b5de3a10fc9
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 12:12:48 -0500

    function_auto_path ..\love\public\src\text_match_ordered.mjs

--- web/public/src/text_match_ordered.mjs
+++ web/public/src/text_match_ordered.mjs
@@ -1,7 +1,7 @@
-export function text_match_ordered(t, target) {
+export function text_match_ordered(t, search) {
   const escaped = t.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
   let regex_text = escaped.split("").join(".*");
   const regex = new RegExp(regex_text);
-  let matches = regex.test(target);
+  let matches = regex.test(search);
   return matches;
 }
