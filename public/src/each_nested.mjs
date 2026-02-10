633882e1de6de1ef393dd8269372b17b92cc9e15
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 16:48:13 -0500

    function_auto_path ..\love\public\src\each_nested.mjs

--- public/src/each_nested.mjs
+++ public/src/each_nested.mjs
@@ -1,7 +1,7 @@
 import { each } from "../../../love/public/src/each.mjs";
-export function each_nested(list, lambda4) {
+export function each_nested(list, lambda) {
   function lambda2(lr) {
-    each(lr, lambda4);
+    each(lr, lambda);
   }
   each(list, lambda2);
 }
