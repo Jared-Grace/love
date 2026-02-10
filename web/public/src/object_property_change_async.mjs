b9e22c0a18a7239bf8c612a9720a7c64b1140156
Grace-Jared <christrosetolife@gmail.com>
2026-02-09 19:48:41 -0500

    function_auto_path ..\love\public\src\app_g_gospel.mjs

--- web/public/src/object_property_change_async.mjs
+++
@@ -1,8 +0,0 @@
-import { object_property_set } from "../../../love/public/src/object_property_set.mjs";
-import { object_property_get } from "../../../love/public/src/object_property_get.mjs";
-export async function object_property_change_async(o, property, lambda$value) {
-  let value = object_property_get(o, property);
-  value = await lambda$value(value);
-  object_property_set(o, property, value);
-  return value;
-}
