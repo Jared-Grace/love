fcd46668ce85ccec70357fa8485db2816edd38d0
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 11:49:33 -0500

    de js_declaration_param_add_curried

--- public/src/js_declaration_param_add_curried.mjs
+++
@@ -1,8 +0,0 @@
-import { js_declaration_param_add } from "../../../love/public/src/js_declaration_param_add.mjs";
-export function js_declaration_param_add_curried(declaration) {
-  let r2 = function js_declaration_param_add_curried_result(param_name) {
-    let r = js_declaration_param_add(declaration, param_name);
-    return r;
-  };
-  return r2;
-}
