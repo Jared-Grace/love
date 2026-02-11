8f6611e3dd7049526550758c17fde7a60c1acdcf
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:58:10 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,1 @@
-import { function_curryify_overwrite } from "../../../love/public/src/function_curryify_overwrite.mjs";
-export async function sandbox() {
-  let f_name = "text_match_ordered";
-  await function_curryify_overwrite(f_name);
-}
+export async function sandbox() {}
