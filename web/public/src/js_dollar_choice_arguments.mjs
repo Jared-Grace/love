65a8bbbb858c87a3d354fceaf183123fda815fed
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:07:33 -0500

    function_auto_path ..\love\public\src\js_dollar_choice_arguments.mjs

--- web/public/src/js_dollar_choice_arguments.mjs
+++ web/public/src/js_dollar_choice_arguments.mjs
@@ -4,7 +4,8 @@ import { function_transform_marker_specified } from "../../../love/public/src/fu
 import { marker_next_declare_single_init } from "../../../love/public/src/marker_next_declare_single_init.mjs";
 export async function js_dollar_choice_arguments() {
   let result = null;
-  let output = await function_transform(f_name, async function lambda2(ast) {});
+  async function lambda2(ast) {}
+  let output = await function_transform(f_name, lambda2);
   async function lambda(a) {
     result = marker_next_declare_single_init(a);
   }
