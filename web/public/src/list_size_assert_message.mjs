aea2b338282a06b2d79153124d1ee4819e199fb4
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:37:11 -0500

    pne list_size_assert_message size

--- web/public/src/list_size_assert_message.mjs
+++ web/public/src/list_size_assert_message.mjs
@@ -1,7 +1,7 @@
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { error_json } from "./error_json.mjs";
-export function list_size_assert_message(list, message) {
+export function list_size_assert_message(list, message, size) {
   let a = list_size(list) === 1;
   if (not(a)) {
     error_json({
