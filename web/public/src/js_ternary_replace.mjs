2f1da7514e37661b55be96e9fd33f8c350441e25
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 18:47:09 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- web/public/src/js_ternary_replace.mjs
+++ web/public/src/js_ternary_replace.mjs
@@ -22,9 +22,6 @@ export function js_ternary_replace(ast) {
       return false;
     }
     let mapped = list_map(list, js_block_to_body);
-    log({
-      mapped2,
-    });
     let a = list_all(mapped, list_size_1);
     if (not(a)) {
       return false;
