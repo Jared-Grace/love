d394d6e1f580e7495d4531d3762fa27b3405a67b
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 15:52:49 -0500

    function_auto_path ..\love\public\src\sandbox_2.mjs

--- web/public/src/sandbox_2.mjs
+++ web/public/src/sandbox_2.mjs
@@ -1,10 +1,10 @@
+import { ebible_languages_without_original } from "../../../love/public/src/ebible_languages_without_original.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { ebible_languages_add_item_info } from "../../../love/public/src/ebible_languages_add_item_info.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 export async function sandbox_2() {
-  let languages = ebible_languages();
+  let languages = ebible_languages_without_original();
   async function lambda(l) {
     let bible_folder = property_get(l, "bible_folder");
     let r = await ebible_languages_add_item_info(bible_folder);
