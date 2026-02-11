fde77a159b314b10581d55f294506e94c6bb6ae6
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:56:37 -0500

    function_auto_path ..\love\public\src\each_pair_reverse.mjs

--- public/src/each_pair_reverse.mjs
+++ public/src/each_pair_reverse.mjs
@@ -1,4 +1,5 @@
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 export function each_pair_reverse(list_a, list_b, lambda$a$b) {
-  return each_pair(list_a, list_b, lambda$a$b);
+  let r = each_pair(list_a, list_b, lambda$a$b);
+  return r;
 }
