c42879985ffbb9eca33f2668eadeb070190bb758
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:50:38 -0500

    rrl js_code_text string

--- public/src/js_marker_insert.mjs
+++ public/src/js_marker_insert.mjs
@@ -1,8 +1,8 @@
 import { js_call_insert } from "../../../love/public/src/js_call_insert.mjs";
 import { marker } from "../../../love/public/src/marker.mjs";
-import { js_code_text } from "../../../love/public/src/js_code_text.mjs";
+import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 export async function js_marker_insert(name, list, index) {
-  let code_string = await js_code_text(name);
+  let code_string = await js_code_string(name);
   const f_name = marker.name;
   const args_code = [code_string];
   js_call_insert(f_name, args_code, list, index);
