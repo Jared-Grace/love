881ad83c754718e2dcbf36f2e7977f34cd8a0fe5
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:00:11 -0500

    function_auto_path ..\love\public\src\js_object_expression_named.mjs

--- public/src/js_object_expression_named.mjs
+++ public/src/js_object_expression_named.mjs
@@ -3,7 +3,6 @@ import { js_identifier_name } from "../../../love/public/src/js_identifier_name.
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_object_expression_named(ast) {
   function lambda2(la) {
@@ -18,10 +17,6 @@ export function js_object_expression_named(ast) {
           la(name);
         }
         js_node_type_is_if(id, "Identifier", lambda4);
-        log({
-          node,
-          e1,
-        });
       }
       js_node_type_is_if(e1, "VariableDeclarator", lambda3);
     }
