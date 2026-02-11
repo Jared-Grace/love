37ab975714c854b663d250766740dd4030eb2b38
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:05:19 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid.mjs

--- web/public/src/app_replace_button_symbol_style_valid.mjs
+++ web/public/src/app_replace_button_symbol_style_valid.mjs
@@ -5,7 +5,7 @@ import { html_style_font_color_set_if } from "../../../love/public/src/html_styl
 import { html_enable_if } from "../../../love/public/src/html_enable_if.mjs";
 export function app_replace_button_symbol_style_valid(sb, valid) {
   let h = app_replace_rule_set_highlight();
-  html_margin(b, "2px solid " + h);
+  html_margin(sb, "2px solid " + h);
   html_enable_if(sb, valid);
   html_style_background_color_set_if(sb, valid, "#00b400ff", "#1e6c1eff");
   html_style_font_color_set_if(sb, valid, "white", "#b9fcb9ff");
