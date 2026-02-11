782408c01d4ad60415b396f0df61ed0b0533ec83
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:51:11 -0500

    pd js_dollar_new_update_list lambda2

--- public/src/js_dollar_new_update_list.mjs
+++ public/src/js_dollar_new_update_list.mjs
@@ -6,7 +6,7 @@ import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-export async function js_dollar_new_update_list(lambda2, code) {
+export async function js_dollar_new_update_list(code) {
   async function lambda(ast) {
     let elements = js_array_expression_single_elements(ast);
     let oe = {
