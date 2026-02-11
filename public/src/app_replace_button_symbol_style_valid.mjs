644f7c32cc166da31a03a000c29d3a378c6c1db3
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:02:25 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid.mjs

--- public/src/app_replace_button_symbol_style_valid.mjs
+++ public/src/app_replace_button_symbol_style_valid.mjs
@@ -5,6 +5,6 @@ export function app_replace_button_symbol_style_valid(sb, valid) {
   html_enable_if(sb, valid);
   let on_true = "#00b400ff";
   let on_false = "#1e6c1eff";
-  html_style_background_color_set_if(valid, on_true, on_false, sb);
+  html_style_background_color_set_if(sb, valid, on_true, on_false);
   html_style_font_color_set_if(sb, valid, "white", "#b9fcb9ff");
 }
