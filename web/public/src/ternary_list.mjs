5418d2a04ac55c6587163d742495f7188b3ad339
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:42:58 -0500

    function_auto_path ..\love\public\src\ternary_list.mjs

--- web/public/src/ternary_list.mjs
+++ web/public/src/ternary_list.mjs
@@ -1,6 +1,6 @@
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 export function ternary_list(conditions, on_matches, on_else) {
-  let last = on_else;
+  let previous = on_else;
   function lambda(a, b) {}
   each_pair(list_a, list_b, lambda);
 }
