c997bd9d2539acab7112a8c9494196a0eb57cc3d
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:28:39 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- public/src/js_ternary_replace.mjs
+++ public/src/js_ternary_replace.mjs
@@ -1,5 +1,8 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 export function js_ternary_replace(ast) {
-  function lambda(v) {}
-  js_visit_type(ast2, type, lambda);
+  function lambda(v) {
+    let node = property_get(v, "node");
+  }
+  js_visit_type(ast, "IfStatement", lambda);
 }
