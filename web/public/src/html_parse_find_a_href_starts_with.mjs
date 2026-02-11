f20765504671ac96e81076ffec2909ac7df0f6fb
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:55:11 -0500

    function_auto_path ..\love\public\src\html_parse_find_a_href_starts_with.mjs

--- web/public/src/html_parse_find_a_href_starts_with.mjs
+++ web/public/src/html_parse_find_a_href_starts_with.mjs
@@ -1,9 +1,11 @@
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { html_parse_find_a_href_text } from "../../../love/public/src/html_parse_find_a_href_text.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export function html_parse_find_a_href_starts_with(root, d, prefix) {
   let mapped = html_parse_find_a_href_text(root, d);
   let mapped2 = list_map_property(mapped, "href");
+  let filtered2 = list_filter(list, function lambda(item) {});
   let filtered = list_filter_starts_with(mapped2, prefix);
   return filtered;
 }
