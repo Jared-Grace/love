34b1450fb6815af0b8b03128350470efeff7c42d
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:57:23 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,8 @@
+import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
 import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
 import { function_curryify } from "../../../love/public/src/function_curryify.mjs";
 export async function sandbox() {
+  let combined = function_curryify_generic_name(f_name);
   await function_delete_if_exists("text_match_ordered_curried");
   await function_curryify("text_match_ordered");
 }
