0524324c4931c19f2cf4c996171ca95085d8d590
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:33:47 -0500

    function_auto_path ..\love\public\src\ebible_languages_add_item.mjs

--- web/public/src/ebible_languages_add_item.mjs
+++ web/public/src/ebible_languages_add_item.mjs
@@ -26,6 +26,7 @@ export async function ebible_languages_add_item(bible_folder) {
     url_language_prefix,
   );
   let language_code = list_single(unique);
+  let url_language = url_language_prefix + language_code + "";
   let mapped = html_parse_find_a_href_text(root, d);
   return unique;
   let r2 = await openai_responses_cache(
