52568b88b5af495c0734ce69434672777f9a88f2
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:21:24 -0500

    function_auto_path ..\love\public\src\ebible_versions.mjs

--- web/public/src/ebible_versions.mjs
+++ web/public/src/ebible_versions.mjs
@@ -1,3 +1,4 @@
+import { ebible_url_details } from "../../../love/public/src/ebible_url_details.mjs";
 import { http_local_html_parse } from "../../../love/public/src/http_local_html_parse.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_sort_text } from "../../../love/public/src/list_sort_text.mjs";
@@ -9,7 +10,8 @@ export async function ebible_versions() {
   let r = await http_local_html_parse(url);
   let root = property_get(r, "root");
   let d = property_get(r, "d");
-  let list = html_parse_find_a_href_starts_with(root, d, "details.php?id=");
+  let book_code = ebible_url_details();
+  let list = html_parse_find_a_href_starts_with(root, d, book_code);
   let mapped = list_map_prefix_without(list, "details.php?id=");
   let unique = list_unique(mapped);
   list_sort_text(unique);
