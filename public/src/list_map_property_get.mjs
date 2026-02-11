967147e0d42ddbafd94a4e131af41d8dbbfc6c12
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:51:21 -0500

    pb list_map_property_get

--- public/src/list_map_property_get.mjs
+++ public/src/list_map_property_get.mjs
@@ -1,6 +1,6 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
-export function list_map_property_get(object, properties) {
+export function list_map_property_get(properties, object) {
   let c = property_get_curried(object);
   let list = list_map(properties, c);
   return list;
