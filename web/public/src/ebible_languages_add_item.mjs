03a43b1739aede360ec0ada3ab9fc977aa916466
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:39:01 -0500

    function_auto_path ..\love\public\src\list_size_1_assert_message.mjs

--- web/public/src/ebible_languages_add_item.mjs
+++ web/public/src/ebible_languages_add_item.mjs
@@ -29,7 +29,8 @@ export async function ebible_languages_add_item(bible_folder) {
   let language_code = list_single(unique);
   let url_language = url_language_prefix + language_code + "";
   let mapped = html_parse_find_a_href_text(root, d);
-  let filtered = list_filter_property(mapped, "href", url_language);list_size_assert_message(filtered, 3, message)
+  let filtered = list_filter_property(mapped, "href", url_language);
+  list_size_assert_message(filtered, 3, message)
   lza;
   return filtered;
   let r2 = await openai_responses_cache(
