cbf144ba0ef58cb37d194fc5c58de088defdc5bd
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:48:26 -0500

    function_auto_path ..\love\public\src\html_style_background_color_set.mjs

--- web/public/src/html_style_background_color_set.mjs
+++ web/public/src/html_style_background_color_set.mjs
@@ -1,5 +1,5 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export function html_style_background_color(component, background) {
+export function html_style_background_color_set(component, background) {
   html_style_assign(component, {
     "background-color": background,
   });
