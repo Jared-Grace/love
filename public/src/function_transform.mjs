09a0035efd3a5b2f54506f697d55254116edd1de
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:35:23 -0500

    function_auto_path ..\love\public\src\js_dollar.mjs

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,7 +1,8 @@
 import { function_transform_result } from "../../../love/public/src/function_transform_result.mjs";
 import { function_parse_declaration_js_unparse } from "../../../love/public/src/function_parse_declaration_js_unparse.mjs";
-export async function function_transform(f_name, lambda$ast) {$aa
+export async function function_transform(f_name, lambda$ast) {
   await function_transform_result(f_name, lambda$ast);
   let output = await function_parse_declaration_js_unparse(f_name);
   return output;
+  $aa
 }
