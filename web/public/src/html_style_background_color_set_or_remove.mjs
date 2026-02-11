5562739dfcdd04b2f8b28a0b52052c31f9f9ceb5
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:48:29 -0500

    rs html_style_background_color set

--- web/public/src/html_style_background_color_set_or_remove.mjs
+++ web/public/src/html_style_background_color_set_or_remove.mjs
@@ -1,12 +1,12 @@
 import { html_style_remove } from "../../../love/public/src/html_style_remove.mjs";
-import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 export function html_style_background_color_set_or_remove(
   chosen,
   component,
   color,
 ) {
   if (chosen) {
-    html_style_background_color(component, color);
+    html_style_background_color_set(component, color);
   } else {
     html_style_remove(component, "background-color");
   }
