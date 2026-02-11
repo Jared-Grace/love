9d10e0fbcba665f631dfd1d3f2fae11ae93b6551
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:46:50 -0500

    function_auto_path ..\love\public\src\ebible_languages_add_item.mjs

--- web/public/src/ebible_languages_add_item.mjs
+++ web/public/src/ebible_languages_add_item.mjs
@@ -1,5 +1,7 @@
+import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function ebible_languages_add_item() {
+  let languages = ebible_languages();
   async function lambda(ast) {}
   let output = await function_transform(f_name, lambda);
 }
