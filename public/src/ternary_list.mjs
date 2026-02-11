f347bfadfbc357f2a6ac125394a226dcbbf8f2d8
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:45:09 -0500

    function_auto_path ..\love\public\src\ternary_list.mjs

--- public/src/ternary_list.mjs
+++ public/src/ternary_list.mjs
@@ -1,10 +1,10 @@
 import { ternary } from "../../../love/public/src/ternary.mjs";
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 export function ternary_list(conditions, on_matches, on_else) {
-  let previous = on_else;
+  let else_previous = on_else;
   function lambda(c, m) {
-    previous = ternary(c, m, previous);
+    else_previous = ternary(c, m, else_previous);
   }
   each_pair(conditions, on_matches, lambda);
-  return previous;
+  return else_previous;
 }
