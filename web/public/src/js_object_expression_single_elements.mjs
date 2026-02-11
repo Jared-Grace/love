6b6213eef1f32c93ef52af23a9b9f27bd72a5b8a
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:35:50 -0500

    function_auto_path ..\love\public\src\js_object_expression_single_elements.mjs

--- web/public/src/js_object_expression_single_elements.mjs
+++ web/public/src/js_object_expression_single_elements.mjs
@@ -2,7 +2,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
 export function js_object_expression_single_elements(ast) {
-  let node_type = "ArrayExpression";
+  let node_type = "ObjectExpression";
   let vs = js_list_type(ast, node_type);
   let only = list_single(vs);
   let node = property_get(only, "node");
