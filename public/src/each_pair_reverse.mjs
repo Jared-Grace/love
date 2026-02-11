a9ec901e264ff991a1219a7092f5812709700fc9
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:56:35 -0500

    fws ep reverse

--- public/src/each_pair_reverse.mjs
+++ public/src/each_pair_reverse.mjs
@@ -1,1 +1,4 @@
-export function each_pair_reverse() {}
+import { each_pair } from "../../../love/public/src/each_pair.mjs";
+export function each_pair_reverse(list_a, list_b, lambda$a$b) {
+  return each_pair(list_a, list_b, lambda$a$b);
+}
