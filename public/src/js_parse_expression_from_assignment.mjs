3abee61c57bac5c238f8be5cf65bb83f657fcac2
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:10:52 -0500

    function_auto_path ..\love\public\src\js_parse_expression_from_assignment.mjs

--- public/src/js_parse_expression_from_assignment.mjs
+++ public/src/js_parse_expression_from_assignment.mjs
@@ -2,8 +2,8 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_declare_single } from "../../../love/public/src/js_declare_single.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_let_assign } from "../../../love/public/src/js_code_let_assign.mjs";
-export function js_parse_expression_from_assignment(json) {
-  let code_assign = js_code_let_assign("a", json);
+export function js_parse_expression_from_assignment(code) {
+  let code_assign = js_code_let_assign("a", code);
   let statement = js_parse_statement(code_assign);
   let d = js_declare_single(statement);
   let init = property_get(d, "init");
