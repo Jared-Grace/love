c5b253067af108f883e4ad79a6599f189da7086a
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:23:41 -0500

    function_auto_path ..\love\public\src\ebible_versions.mjs

--- web/public/src/ebible_versions.mjs
+++ web/public/src/ebible_versions.mjs
@@ -1,20 +1,18 @@
+import { html_parse_find_a_href_starts_with_without_unique } from "../../../love/public/src/html_parse_find_a_href_starts_with_without_unique.mjs";
 import { ebible_url } from "../../../love/public/src/ebible_url.mjs";
 import { ebible_url_details } from "../../../love/public/src/ebible_url_details.mjs";
 import { http_local_html_parse } from "../../../love/public/src/http_local_html_parse.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_sort_text } from "../../../love/public/src/list_sort_text.mjs";
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
-import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
-import { html_parse_find_a_href_starts_with } from "../../../love/public/src/html_parse_find_a_href_starts_with.mjs";
 export async function ebible_versions() {
   let url = ebible_url() + "download.php";
   let r = await http_local_html_parse(url);
   let root = property_get(r, "root");
   let d = property_get(r, "d");
   let prefix = ebible_url_details();
-  let list = html_parse_find_a_href_starts_with(root, d, prefix);
-  let mapped = list_map_prefix_without(list, prefix);
-  let unique = list_unique(mapped);
-  list_sort_text(unique);
+  let unique = html_parse_find_a_href_starts_with_without_unique(
+    root,
+    d,
+    prefix,
+  );
   return unique;
 }
