13b244b31eab5c0ee97349f0997f4321267e1d2b
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:28:21 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- public/src/js_ternary_replace.mjs
+++ public/src/js_ternary_replace.mjs
@@ -1,1 +1,4 @@
-export function js_ternary_replace(ast) {}
+import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+export function js_ternary_replace(ast) {
+  js_visit_type(ast2, type, function lambda(v) {});
+}
