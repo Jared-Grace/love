2c83571ebd7fc7e0bc1a47f2d9095117d46b2c3e
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:59:29 -0500

    function_auto_path ..\love\public\src\js_object_expression_named.mjs

--- public/src/js_object_expression_named.mjs
+++ public/src/js_object_expression_named.mjs
@@ -1,3 +1,4 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
@@ -5,6 +6,7 @@ import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_object_expression_named(ast) {
+  let list = list_adder(function lambda2(la) {});
   let node_type = "ObjectExpression";
   function lambda(v) {
     let node = property_get(v, "node");
