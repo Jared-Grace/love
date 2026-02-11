b1fc91c3cc5990270e4d5a120b7a85ab25f0ecb6
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 18:27:11 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- public/src/js_ternary_replace.mjs
+++ public/src/js_ternary_replace.mjs
@@ -21,7 +21,8 @@ export function js_ternary_replace(ast) {
     if (not(a)) {
       return false;
     }
-    let mapped2 = list_map(list2, function lambda3(item2) {});
+    function lambda3(item2) {}
+    let mapped2 = list_map(list2, lambda3);
     function lambda2(item) {
       let type_is = js_node_type_is(item, "BlockStatement");
       if (not(type_is)) {
