e388f198a58e541f027c889271617773b313f755
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:10:29 -0500

    function_auto_path ..\love\public\src\js_parse_expression.mjs

---
+++ public/src/js_parse_expression_from_statement.mjs
@@ -0,0 +1,7 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
+export function js_parse_expression_from_statement(code_expression) {
+  let statement = js_parse_statement(code_expression);
+  let expression = property_get(statement, "expression");
+  return expression;
+}
