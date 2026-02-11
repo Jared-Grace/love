3c2fc0972b13792e4f24dce7e09eb2f29cf331a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:16:29 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid.mjs

--- public/src/app_replace_button_symbol_style_valid.mjs
+++ public/src/app_replace_button_symbol_style_valid.mjs
@@ -5,7 +5,7 @@ import { html_style_font_color_set_if } from "../../../love/public/src/html_styl
 import { html_enable_if } from "../../../love/public/src/html_enable_if.mjs";
 export function app_replace_button_symbol_style_valid(sb, valid) {
   let h = app_replace_rule_set_highlight();
-  html_style_set(sb, "box-shadow", " 0 0 0 " + "0.5em" + " " + h);
+  html_style_set(sb, "box-shadow", " 0 0 0 " + "0.1em" + " " + h);
   html_enable_if(sb, valid);
   html_style_background_color_set_if(valid, sb, "#00b400ff", "#1e6c1eff");
   html_style_font_color_set_if(valid, sb, "white", "#b9fcb9ff");
