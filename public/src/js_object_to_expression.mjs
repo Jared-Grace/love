6f5426d7eedf661a9501eeeab3b15422217498af
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:05:44 -0500

    function_auto_path ..\love\public\src\js_object_to_expression.mjs

--- public/src/js_object_to_expression.mjs
+++ public/src/js_object_to_expression.mjs
@@ -1,4 +1,4 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_declare_single } from "../../../love/public/src/js_declare_single.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_let_assign } from "../../../love/public/src/js_code_let_assign.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
@@ -6,6 +6,6 @@ export function js_object_to_expression(object) {
   let json = json_to(object);
   let code_assign = js_code_let_assign("a", json);
   let statement = js_parse_statement(code_assign);
-  let expression = property_get(statement, "expression");
+  let expression = js_declare_single(statement);
   return expression;
 }
