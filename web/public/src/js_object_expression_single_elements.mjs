6cf1c931fac06fba8ae1e8b95b94d43e2c7e8c07
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:35:42 -0500

    cr js_array_expression_single_elements array object

---
+++ web/public/src/js_object_expression_single_elements.mjs
@@ -0,0 +1,11 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_single } from "../../../love/public/src/list_single.mjs";
+import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
+export function js_object_expression_single_elements(ast) {
+  let node_type = "ArrayExpression";
+  let vs = js_list_type(ast, node_type);
+  let only = list_single(vs);
+  let node = property_get(only, "node");
+  let elements = property_get(node, "elements");
+  return elements;
+}
