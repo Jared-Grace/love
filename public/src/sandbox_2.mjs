63b27ea0aaab9f05fb4dde13a59dccbc0ca852e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:48:36 -0500

    function_auto_path ..\love\public\src\sandbox_2.mjs

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,1 +1,4 @@
-export async function sandbox_2() {}
+import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
+export async function sandbox_2() {
+  let languages = ebible_languages();
+}
