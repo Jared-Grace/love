17a68685238056a08ed8118d9fc11ce146dc8b2e
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:39:01 -0500

    function_auto_path ..\love\public\src\list_size_1_assert_message.mjs

--- public/src/list_size_1_assert_message.mjs
+++ public/src/list_size_1_assert_message.mjs
@@ -1,6 +1,15 @@
+import { list_size_assert_message } from "../../../love/public/src/list_size_assert_message.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
 import { error_json } from "./error_json.mjs";
 export function list_size_1_assert_message(list, message) {
-  list_size_assert_message(list, 1, message)
+  let a = list_size_1(list);
+  if (not(a)) {
+    error_json({
+      message,
+      list,
+    });
+  }
+  return;
+  list_size_assert_message(list, 1, message);
 }
