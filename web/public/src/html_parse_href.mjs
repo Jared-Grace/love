94ef73ee014c22d3dec1e4231d8a92a0dd9ad168
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:50:57 -0500

    function_auto_path ..\love\public\src\html_parse_href.mjs

--- web/public/src/html_parse_href.mjs
+++ web/public/src/html_parse_href.mjs
@@ -2,5 +2,6 @@ import { html_parse_attr } from "../../../love/public/src/html_parse_attr.mjs";
 export function html_parse_href(d, item) {
   const name = "href";
   let href = html_parse_attr(d, item, name);
+  ti;
   return href;
 }
