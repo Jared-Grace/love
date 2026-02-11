92ecf206dfcb5d823857c5436a86a5682cbafd18
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:48:43 -0500

    function_auto_path ..\love\public\src\sandbox_2.mjs

--- web/public/src/sandbox_2.mjs
+++ web/public/src/sandbox_2.mjs
@@ -2,5 +2,6 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 export async function sandbox_2() {
   let languages = ebible_languages();
-  await each_async(list, async function lambda(item) {});
+  async function lambda(item) {}
+  await each_async(list, lambda);
 }
