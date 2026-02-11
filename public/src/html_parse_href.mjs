7fa3eaff408a7a6973fb058079bc6b1708348abd
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:52:30 -0500

    function_auto_path ..\love\public\src\html_parse_href.mjs

--- public/src/html_parse_href.mjs
+++ public/src/html_parse_href.mjs
@@ -1,10 +1,8 @@
-import { not } from "../../../love/public/src/not.mjs";
-import { text_is } from "../../../love/public/src/text_is.mjs";
+import { text_not_is } from "../../../love/public/src/text_not_is.mjs";
 import { html_parse_attr } from "../../../love/public/src/html_parse_attr.mjs";
 export function html_parse_href(d, item) {
   const name = "href";
   let href = html_parse_attr(d, item, name);
-  let si = text_is(href);
-  let n = not(b);
-  return href;
+  let n = text_not_is(href);
+  return n;
 }
