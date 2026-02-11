4b0d7513f4abbe6e553c9c158ee3c9cd481d9969
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:50:38 -0500

    rrl js_code_text string

--- web/public/src/function_name_to_path_import.mjs
+++ web/public/src/function_name_to_path_import.mjs
@@ -1,4 +1,4 @@
-import { js_code_text } from "../../../love/public/src/js_code_text.mjs";
+import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
@@ -7,6 +7,6 @@ export function function_name_to_path_import(import_, dictionary) {
   let previous = folder_previous();
   let previous2 = folder_previous();
   let joined = path_join([previous, previous2, value]);
-  const c = js_code_text(joined);
+  const c = js_code_string(joined);
   return c;
 }
--- web/public/src/newline_windows_code.mjs
+++ web/public/src/newline_windows_code.mjs
@@ -1,7 +1,7 @@
-import { js_code_text } from "../../../love/public/src/js_code_text.mjs";
+import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 import { newline_windows } from "../../../love/public/src/newline_windows.mjs";
 export function newline_windows_code() {
   let n = newline_windows();
-  let code_string = js_code_text(n);
+  let code_string = js_code_string(n);
   return code_string;
 }
