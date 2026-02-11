f41ae9e061885d797821a64e74b0bf3d3cb988e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:51:36 -0500

    function_auto_path ..\love\public\src\js_dollar_new_update_list.mjs

--- web/public/src/js_dollar_new_update_list.mjs
+++ web/public/src/js_dollar_new_update_list.mjs
@@ -1,3 +1,4 @@
+import { js_dollar_new_name } from "../../../love/public/src/js_dollar_new_name.mjs";
 import { js_dollar_choices } from "../../../love/public/src/js_dollar_choices.mjs";
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -7,6 +8,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function js_dollar_new_update_list(code) {
+  let combined = js_dollar_new_name(code);
   async function lambda(ast) {
     let elements = js_array_expression_single_elements(ast);
     let oe = {
