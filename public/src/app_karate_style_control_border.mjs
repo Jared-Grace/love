31d03fdd63247536ce498f4a86e9bdde564c41e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:07:48 -0500

    function_auto_path ..\love\public\src\app_karate_style_control_border.mjs

--- public/src/app_karate_style_control_border.mjs
+++ public/src/app_karate_style_control_border.mjs
@@ -1,7 +1,8 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_karate_style_control_border(input, border_color) {
+  const border_width = "4px";
   html_style_assign(input, {
-    "border-width": "4px",
+    "border-width": border_width,
     "border-color": border_color,
   });
 }
