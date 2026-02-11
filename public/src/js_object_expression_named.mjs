398b59c27dba52f94b99502518a9b543e503b497
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:59:18 -0500

    rrl2 js_object_expression_single_elements named

--- public/src/js_object_expression_named.mjs
+++ public/src/js_object_expression_named.mjs
@@ -4,7 +4,7 @@ import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_object_expression_single_elements(ast) {
+export function js_object_expression_named(ast) {
   let node_type = "ObjectExpression";
   function lambda(v) {
     let node = property_get(v, "node");
