782f70a9f071a653d53e4fb218e1c85cdb1bc881
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:59:21 -0500

    function_auto_path ..\love\public\src\ebible_languages_add_item.mjs

---
+++ web/public/src/js_object_to_expression.mjs
@@ -0,0 +1,6 @@
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
+import { json_to } from "../../../love/public/src/json_to.mjs";
+export function js_object_to_expression(object) {
+  let json = json_to(object);
+  let expression = js_parse_expression(json);
+}
