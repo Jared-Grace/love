23cee4885633781a4946ac90794bafad74fbc7e1
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 15:59:35 -0500

    function_auto_path ..\love\public\src\ebible_languages.mjs

--- public/src/ebible_languages.mjs
+++ public/src/ebible_languages.mjs
@@ -1,3 +1,4 @@
+import { ebible_languages_add } from "../../../love/public/src/ebible_languages_add.mjs";
 import { ebible_folder_cebuano } from "../../../love/public/src/ebible_folder_cebuano.mjs";
 import { list_sort_text_property } from "../../../love/public/src/list_sort_text_property.mjs";
 import { ebible_language_original } from "../../../love/public/src/ebible_language_original.mjs";
@@ -6,6 +7,7 @@ import { ebible_folder_swahili } from "../../../love/public/src/ebible_folder_sw
 import { ebible_languages_chapters_cache_refresh } from "../../../love/public/src/ebible_languages_chapters_cache_refresh.mjs";
 import { ebible_folder_urdu } from "../../../love/public/src/ebible_folder_urdu.mjs";
 export function ebible_languages() {
+  "to add an entry, run: " + ebible_languages_add.name;
   let en = ebible_language_en();
   let o = ebible_language_original();
   let languages = [
