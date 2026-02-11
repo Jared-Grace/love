35091ddb02211bfc960b40ba8d86d4df9452f2ef
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:57:52 -0500

    function_auto_path ..\love\public\src\ternary_list.mjs

--- web/public/src/ternary_list.mjs
+++ web/public/src/ternary_list.mjs
@@ -1,3 +1,4 @@
+import { each_pair_reverse } from "../../../love/public/src/each_pair_reverse.mjs";
 import { ternary } from "../../../love/public/src/ternary.mjs";
 export function ternary_list(conditions, on_matches, on_else) {
   let result = on_else;
