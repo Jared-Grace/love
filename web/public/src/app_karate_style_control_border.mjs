e283b8111022b57a156d3a5d922c7b307a0d5bdb
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:08:14 -0500

    function_auto_path ..\love\public\src\app_karate_style_control_border.mjs

--- web/public/src/app_karate_style_control_border.mjs
+++ web/public/src/app_karate_style_control_border.mjs
@@ -1,5 +1,4 @@
 import { html_border } from "../../../love/public/src/html_border.mjs";
-export function app_karate_style_control_border(input, border_color) {$a
-  const border_width = "4px";
-  html_border(input, border_width, border_color);
+export function app_karate_style_control_border(input, border_color) {
+  html_border(input, "4px", border_color);
 }
