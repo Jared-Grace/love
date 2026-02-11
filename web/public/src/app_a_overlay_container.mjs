71e2a330ec1508ef6ecbba3590348bb4042147bb
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:48:29 -0500

    rs html_style_background_color set

--- web/public/src/app_a_overlay_container.mjs
+++ web/public/src/app_a_overlay_container.mjs
@@ -1,9 +1,9 @@
-import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_a_overlay_container(overlay) {
   let div = html_div(overlay);
   app_a_control_style(div);
-  html_style_background_color(div, "white");
+  html_style_background_color_set(div, "white");
   return div;
 }
--- web/public/src/html_style_background_color_yellow.mjs
+++ web/public/src/html_style_background_color_yellow.mjs
@@ -1,4 +1,4 @@
-import { html_style_background_color } from "../../../love/public/src/html_style_background_color.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 export function html_style_background_color_yellow(parent_new) {
-  html_style_background_color(parent_new, "yellow");
+  html_style_background_color_set(parent_new, "yellow");
 }
