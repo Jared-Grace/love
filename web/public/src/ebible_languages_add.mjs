6f290664c2d758b8c3d54e346b277fb6d9d9ee09
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 12:03:04 -0500

    fw ebible_version_upload ebible_languages_add

--- web/public/src/ebible_languages_add.mjs
+++ web/public/src/ebible_languages_add.mjs
@@ -1,1 +1,4 @@
-export function ebible_languages_add() {}
+import { ebible_version_upload } from "../../../love/public/src/ebible_version_upload.mjs";
+export async function ebible_languages_add(bible_folder) {
+  return await ebible_version_upload(bible_folder);
+}
