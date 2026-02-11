2d5cb75e5aea47a4ac9e5dc3f0cd9e2d53530969
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:33:09 -0500

    function_auto_path ..\love\public\src\ternary_nested.mjs

--- web/public/src/ternary_nested.mjs
+++ web/public/src/ternary_nested.mjs
@@ -1,5 +1,5 @@
 import { ternary } from "../../../love/public/src/ternary.mjs";
-export function ternary_nested(condition_a, on_a, condition_b, on_false) {
-  let result = ternary(condition_a, on_a, on_false);
+export function ternary_nested(condition_a, on_a, condition_b, on_b, on_false) {
+  let result = ternary(condition_a, on_a, null);
   return result;
 }
