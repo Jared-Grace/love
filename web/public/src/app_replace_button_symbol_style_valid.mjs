2a91c083b8658508bdbeb25ce972f431c83ff4a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:02:16 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid.mjs

--- web/public/src/app_replace_button_symbol_style_valid.mjs
+++ web/public/src/app_replace_button_symbol_style_valid.mjs
@@ -1,12 +1,10 @@
+import { html_style_background_color_set_if } from "../../../love/public/src/html_style_background_color_set_if.mjs";
 import { html_style_font_color_set_if } from "../../../love/public/src/html_style_font_color_set_if.mjs";
-import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { html_enable_if } from "../../../love/public/src/html_enable_if.mjs";
-import { ternary } from "./ternary.mjs";
 export function app_replace_button_symbol_style_valid(sb, valid) {
   html_enable_if(sb, valid);
   let on_true = "#00b400ff";
   let on_false = "#1e6c1eff";
-  let color_bg = ternary(valid, on_true, on_false);
-  html_style_background_color_set(sb, color_bg);
+  html_style_background_color_set_if(valid, on_true, on_false, sb);
   html_style_font_color_set_if(sb, valid, "white", "#b9fcb9ff");
 }
