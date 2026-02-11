3226ec6b645ee52d06b277f697cf4e9cd7df8d08
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:01:50 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,7 @@
 import { js_object_expression_named } from "../../../love/public/src/js_object_expression_named.mjs";
 export async function sandbox(ast) {
-  js_object_expression_named(ast, "js_dollar_arguments");
-  return;
+  let elements = js_object_expression_named(ast, "js_dollar_arguments");
+  return elements;
   const js_dollar_arguments = {
     remaining,
     node,
