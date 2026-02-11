5cc7388494b05b793ecf1f8849d488b30d19b870
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:50:36 -0500

    function_auto_path ..\love\public\src\js_code_string.mjs

--- public/src/js_code_string.mjs
+++ public/src/js_code_string.mjs
@@ -1,6 +1,6 @@
 import { js_string } from "../../../love/public/src/js_string.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
-export function js_code_text(value_string) {
+export function js_code_string(value_string) {
   let parsed_string = js_string(value_string);
   let code_string = js_unparse(parsed_string);
   return code_string;
