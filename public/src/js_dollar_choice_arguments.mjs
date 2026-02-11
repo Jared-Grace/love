04c919ad259b532de495efbfde53a9e13cbae019
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:19:45 -0500

    function_auto_path ..\love\public\src\js_dollar_choice_arguments.mjs

--- public/src/js_dollar_choice_arguments.mjs
+++ public/src/js_dollar_choice_arguments.mjs
@@ -1,3 +1,4 @@
+import { js_dollar_choice_argument } from "../../../love/public/src/js_dollar_choice_argument.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 import { js_dollar } from "../../../love/public/src/js_dollar.mjs";
@@ -7,6 +8,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_object_expression_named } from "../../../love/public/src/js_object_expression_named.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function js_dollar_choice_arguments() {
+  let result2 = await js_dollar_choice_argument();
   let result = null;
   async function lambda2(ast) {
     let oes = js_object_expression_named(ast, "js_dollar_arguments");
