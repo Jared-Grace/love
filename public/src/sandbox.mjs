f667f1b592f94295fcba2aef1359effadb3bc3b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:58:03 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,5 @@
-import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
-import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
-import { function_curryify } from "../../../love/public/src/function_curryify.mjs";
+import { function_curryify_overwrite } from "../../../love/public/src/function_curryify_overwrite.mjs";
 export async function sandbox() {
   let f_name = "text_match_ordered";
-  let combined = function_curryify_generic_name(f_name);
-  await function_delete_if_exists(combined);
-  await function_curryify(f_name);
+  await function_curryify_overwrite(f_name);
 }
