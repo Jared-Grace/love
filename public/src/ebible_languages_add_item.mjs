ae39768d5bd3ad3d83b5199b493c0b12b4c1eba2
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:48:24 -0500

    function_auto_path ..\love\public\src\ebible_languages_add_item.mjs

--- public/src/ebible_languages_add_item.mjs
+++ public/src/ebible_languages_add_item.mjs
@@ -1,12 +1,5 @@
-import { list_size_assert_message } from "../../../love/public/src/list_size_assert_message.mjs";
-import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
-import { list_single } from "../../../love/public/src/list_single.mjs";
-import { html_parse_find_a_href_text } from "../../../love/public/src/html_parse_find_a_href_text.mjs";
-import { html_parse_find_a_href_starts_with_without_unique } from "../../../love/public/src/html_parse_find_a_href_starts_with_without_unique.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { ebible_url } from "../../../love/public/src/ebible_url.mjs";
-import { ebible_url_details } from "../../../love/public/src/ebible_url_details.mjs";
-import { http_local_html_parse } from "../../../love/public/src/http_local_html_parse.mjs";
+import { ebible_languages_add_item_info } from "../../../love/public/src/ebible_languages_add_item_info.mjs";
 import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_object_to_expression } from "../../../love/public/src/js_object_to_expression.mjs";
@@ -14,31 +7,12 @@ import { sandbox } from "../../../love/public/src/sandbox.mjs";
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 import { ebible_languages } from "../../../love/public/src/ebible_languages.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { list_last } from "./list_last.mjs";
 export async function ebible_languages_add_item(bible_folder) {
   let f_name = ebible_languages.name;
   f_name = sandbox.name;
-  let prefix = ebible_url_details();
-  let r = await http_local_html_parse(ebible_url() + prefix + bible_folder);
-  let root = property_get(r, "root");
-  let d = property_get(r, "d");
-  const url_language_prefix = "http://www.ethnologue.com/language/";
-  let unique = html_parse_find_a_href_starts_with_without_unique(
-    root,
-    d,
-    url_language_prefix,
-  );
-  let language_code = list_single(unique);
-  let url_language = url_language_prefix + language_code + "";
-  let mapped = html_parse_find_a_href_text(root, d);
-  let filtered = list_filter_property(mapped, "href", url_language);
-  list_size_assert_message(
-    filtered,
-    3,
-    "Should be 3 of these, if not then investigate?",
-  );
-  let i = list_last(filtered);
-  let name = property_get(i, "text");
+  let r = await ebible_languages_add_item_info(bible_folder);
+  let language_code = property_get(r, "language_code");
+  let name = property_get(r, "name");
   return name;
   let r2 = await openai_responses_cache(
     "",
