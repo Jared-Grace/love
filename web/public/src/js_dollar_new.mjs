2bde42116f5f8579c38c0418ed44faa01171ff4b
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:50:30 -0500

    function_auto_path ..\love\public\src\js_dollar_new.mjs

--- web/public/src/js_dollar_new.mjs
+++ web/public/src/js_dollar_new.mjs
@@ -1,15 +1,8 @@
-import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_dollar_new_update_list } from "../../../love/public/src/js_dollar_new_update_list.mjs";
 import { js_declaration_single_block_body_add_return } from "../../../love/public/src/js_declaration_single_block_body_add_return.mjs";
 import { js_dollar_new_args_inner } from "../../../love/public/src/js_dollar_new_args_inner.mjs";
 import { js_dollar_new_name } from "../../../love/public/src/js_dollar_new_name.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
-import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
-import { js_property_parse_expression_add } from "../../../love/public/src/js_property_parse_expression_add.mjs";
-import { js_dollar_choices } from "../../../love/public/src/js_dollar_choices.mjs";
 export async function js_dollar_new(code) {
   let combined = js_dollar_new_name(code);
   await function_new(combined);
@@ -17,20 +10,6 @@ export async function js_dollar_new(code) {
     js_declaration_single_block_body_add_return(ast);
     await js_dollar_new_args_inner(ast);
   }
-  await function_transform(combined, lambda2);
-  async function lambda(ast) {
-    let elements = js_array_expression_single_elements(ast);
-    let oe = {
-      type: "ObjectExpression",
-      properties: [],
-    };
-    let properties = property_get(oe, "properties");
-    let code_string = js_code_string(code);
-    js_property_parse_expression_add("name", code_string, properties);
-    js_property_parse_expression_add("fn", combined, properties);
-    list_add(elements, oe);
-    await js_imports_missing_add(ast);
-  }
-  let code2 = await function_transform(js_dollar_choices.name, lambda);
+  let code2 = await js_dollar_new_update_list(combined, lambda2, code);
   return code2;
 }
