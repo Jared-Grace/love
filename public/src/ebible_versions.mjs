ad0d954b36549d3cddbd3d66a2512158e68bb0cf
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:20:31 -0500

    function_auto_path ..\love\public\src\ebible_versions.mjs

--- public/src/ebible_versions.mjs
+++ public/src/ebible_versions.mjs
@@ -1,14 +1,12 @@
+import { http_local_html_parse } from "../../../love/public/src/http_local_html_parse.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_sort_text } from "../../../love/public/src/list_sort_text.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
 import { html_parse_find_a_href_starts_with } from "../../../love/public/src/html_parse_find_a_href_starts_with.mjs";
-import { html_parse } from "../../../love/public/src/html_parse.mjs";
-import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
 export async function ebible_versions() {
   let url = "https://ebible.org/download.php";
-  let text = await http_local_text(url);
-  let r = await html_parse(text);
+  let r = await http_local_html_parse(url);
   let root = property_get(r, "root");
   let d = property_get(r, "d");
   let list = html_parse_find_a_href_starts_with(root, d, "details.php?id=");
