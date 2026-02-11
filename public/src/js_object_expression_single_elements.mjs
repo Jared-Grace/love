d327b09bfa824a8ddd4a753db4396adc598b86c0
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 16:42:18 -0500

    function_auto_path ..\love\public\src\js_object_expression_single_elements.mjs

--- public/src/js_object_expression_single_elements.mjs
+++ public/src/js_object_expression_single_elements.mjs
@@ -5,7 +5,8 @@ export function js_object_expression_single_elements(ast) {
   let node_type = "ObjectExpression";
   function lambda(v) {
     let node = property_get(v, "node");
-    let parent = property_get(v, "parent");
+    let stack = property_get(v, "stack");
+    le1;
     log({
       node,
       stack,
