34f2b1aa2e079e9a2e22710f6b71d92cacabab71
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:09:04 -0500

    function_auto_path ..\love\public\src\js_dollar_choice_arguments.mjs

--- public/src/js_dollar_choice_arguments.mjs
+++ public/src/js_dollar_choice_arguments.mjs
@@ -1,3 +1,4 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_object_expression_named } from "../../../love/public/src/js_object_expression_named.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
@@ -8,6 +9,7 @@ export async function js_dollar_choice_arguments() {
   let result = null;
   async function lambda2(ast) {
     let elements = js_object_expression_named(ast, search);
+    let only = list_single(list);
     let properties = property_get(elements, "properties");
   }
   let output = await function_transform(f_name, lambda2);
