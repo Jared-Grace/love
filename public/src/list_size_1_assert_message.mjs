d003374aa923efb9b994dc8c79f6deb6e7519599
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:39:06 -0500

    function_auto_path ..\love\public\src\list_size_1_assert_message.mjs

--- public/src/list_size_1_assert_message.mjs
+++ public/src/list_size_1_assert_message.mjs
@@ -1,8 +1,4 @@
 import { list_size_assert_message } from "../../../love/public/src/list_size_assert_message.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
-import { error_json } from "./error_json.mjs";
 export function list_size_1_assert_message(list, message) {
-
   list_size_assert_message(list, 1, message);
 }
