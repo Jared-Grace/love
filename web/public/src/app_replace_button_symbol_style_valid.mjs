1010cb7e5a5ce1a40659527278c816e340711533
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:04:43 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid.mjs

--- web/public/src/app_replace_button_symbol_style_valid.mjs
+++ web/public/src/app_replace_button_symbol_style_valid.mjs
@@ -1,8 +1,10 @@
+import { app_replace_rule_set_highlight } from "../../../love/public/src/app_replace_rule_set_highlight.mjs";
 import { html_margin } from "../../../love/public/src/html_margin.mjs";
 import { html_style_background_color_set_if } from "../../../love/public/src/html_style_background_color_set_if.mjs";
 import { html_style_font_color_set_if } from "../../../love/public/src/html_style_font_color_set_if.mjs";
 import { html_enable_if } from "../../../love/public/src/html_enable_if.mjs";
 export function app_replace_button_symbol_style_valid(sb, valid) {
+  let on_a = app_replace_rule_set_highlight();
   html_margin(b, value);
   html_enable_if(sb, valid);
   html_style_background_color_set_if(sb, valid, "#00b400ff", "#1e6c1eff");
