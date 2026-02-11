ebae920c1556a11c783efee9f9f1ab5bddab5679
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:10:01 -0500

    function_auto_path ..\love\public\src\js_dollar_choice_arguments.mjs

--- web/public/src/js_dollar_choice_arguments.mjs
+++ web/public/src/js_dollar_choice_arguments.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_object_expression_named } from "../../../love/public/src/js_object_expression_named.mjs";
@@ -11,6 +12,7 @@ export async function js_dollar_choice_arguments() {
     let oes = js_object_expression_named(ast, search);
     let only = list_single(oes);
     let properties = property_get(only, "properties");
+    let mapped = list_map(list, function lambda3(item) {});
   }
   let output = await function_transform(f_name, lambda2);
   async function lambda(a) {
