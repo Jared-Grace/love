16c4942a8024761a4b3cb5ff6c5535d6984e6caa
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 13:51:05 -0500

    function_auto_path ..\love\public\src\js_expression_string.mjs

--- web/public/src/js_expression_string.mjs
+++ web/public/src/js_expression_string.mjs
@@ -1,6 +1,6 @@
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
-export function js_expression_text(screen_name) {
+export function js_expression_string(screen_name) {
   let code_string = js_code_string(screen_name);
   let key = js_parse_expression(code_string);
   return key;
