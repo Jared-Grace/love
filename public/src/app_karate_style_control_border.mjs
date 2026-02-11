dba6021ba2c0adcc2549f5b9780fcf6764b2e73a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:08:12 -0500

    function_auto_path ..\love\public\src\html_border.mjs

--- public/src/app_karate_style_control_border.mjs
+++ public/src/app_karate_style_control_border.mjs
@@ -1,5 +1,5 @@
 import { html_border } from "../../../love/public/src/html_border.mjs";
-export function app_karate_style_control_border(input, border_color) {
+export function app_karate_style_control_border(input, border_color) {$a
   const border_width = "4px";
   html_border(input, border_width, border_color);
 }
