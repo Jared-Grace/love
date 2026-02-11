7e0d336562d09ae33885a697876a5c4a6f3a803a
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 19:42:15 -0500

    function_auto_path ..\love\public\src\ternary_list.mjs

--- public/src/ternary_list.mjs
+++ public/src/ternary_list.mjs
@@ -1,1 +1,4 @@
-export function ternary_list() {}
+import { each_pair } from "../../../love/public/src/each_pair.mjs";
+export function ternary_list() {
+  each_pair(list_a, list_b, function lambda(a, b) {});
+}
