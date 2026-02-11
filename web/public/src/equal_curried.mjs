0cd7ad3e302d84380eae29111c82e69dd7619405
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 18:56:42 -0500

    function_auto_path ..\love\public\src\equal_curried.mjs

--- web/public/src/equal_curried.mjs
+++ web/public/src/equal_curried.mjs
@@ -1,6 +1,8 @@
 import { equal } from "../../../love/public/src/equal.mjs";
 export function equal_curried(left) {
-  return function equal_curried_result(right) {
-    return equal(left, right);
+  let r = function equal_curried_result(right) {
+    let eq = equal(left, right);
+    return eq;
   };
+  return r;
 }
