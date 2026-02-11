c3b8d5d7e826042326fa8229a295466f5699dd45
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 14:29:07 -0500

    rr html_parse_find_list_to_a list_to_a a_list_to

--- public/src/html_parse_find_a_href_starts_with.mjs
+++ public/src/html_parse_find_a_href_starts_with.mjs
@@ -1,9 +1,9 @@
-import { html_parse_find_list_to_a } from "../../../love/public/src/html_parse_find_list_to_a.mjs";
+import { html_parse_find_a_list_to } from "../../../love/public/src/html_parse_find_a_list_to.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { html_parse_href_text_map } from "../../../love/public/src/html_parse_href_text_map.mjs";
 export function html_parse_find_a_href_starts_with(root, d, prefix) {
-  let list = html_parse_find_list_to_a(root);
+  let list = html_parse_find_a_list_to(root);
   let mapped = html_parse_href_text_map(d, list);
   let mapped2 = list_map_property(mapped, "href");
   let filtered = list_filter_starts_with(mapped2, prefix);
