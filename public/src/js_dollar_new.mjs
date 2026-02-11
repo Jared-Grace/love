9767820375a105a611a8840a3804840d6fb84aec
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:34:46 -0500

    function_auto_path ..\love\public\src\js_dollar_new.mjs

--- public/src/js_dollar_new.mjs
+++ public/src/js_dollar_new.mjs
@@ -1,6 +1,6 @@
+import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_declaration_single_block_body_add_return } from "../../../love/public/src/js_declaration_single_block_body_add_return.mjs";
-import { marker_next_declare_single_init_elements } from "../../../love/public/src/marker_next_declare_single_init_elements.mjs";
 import { js_dollar_new_args_inner } from "../../../love/public/src/js_dollar_new_args_inner.mjs";
 import { js_dollar_new_name } from "../../../love/public/src/js_dollar_new_name.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
@@ -10,7 +10,6 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 import { js_property_parse_expression_add } from "../../../love/public/src/js_property_parse_expression_add.mjs";
 import { js_dollar_choices } from "../../../love/public/src/js_dollar_choices.mjs";
-import { function_transform_marker_specified } from "../../../love/public/src/function_transform_marker_specified.mjs";
 export async function js_dollar_new(code) {
   let combined = js_dollar_new_name(code);
   await function_new(combined);
@@ -20,7 +19,7 @@ export async function js_dollar_new(code) {
   }
   await function_transform(combined, lambda2);
   async function lambda(ast) {
-      let elements = js_array_expression_single_elements(ast);
+    let elements = js_array_expression_single_elements(ast);
     let oe = {
       type: "ObjectExpression",
       properties: [],
@@ -32,10 +31,6 @@ export async function js_dollar_new(code) {
     list_add(elements, oe);
     await js_imports_missing_add(ast);
   }
-  let code2 = await function_transform(
-    js_dollar_choices.name,
-    "choices",
-    lambda,
-  );
+  let code2 = await function_transform(js_dollar_choices.name, lambda);
   return code2;
 }
