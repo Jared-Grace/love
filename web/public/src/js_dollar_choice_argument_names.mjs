c329e70a0d3f9934d040245f5ac9b0ab1e829973
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 17:23:22 -0500

    r js_dollar_choice_arguments_names js_dollar_choice_argument_names

--- web/public/src/js_dollar_choice_argument_names.mjs
+++ web/public/src/js_dollar_choice_argument_names.mjs
@@ -2,7 +2,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_dollar_choice_arguments } from "../../../love/public/src/js_dollar_choice_arguments.mjs";
 import { js_identifiers_to_names } from "../../../love/public/src/js_identifiers_to_names.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-export async function js_dollar_choice_arguments_names() {
+export async function js_dollar_choice_argument_names() {
   let v = await js_dollar_choice_arguments();
   let properties = property_get(v, "properties");
   let mapped = list_map_property(properties, "key");
