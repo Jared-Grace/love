6aad40e045b9b28938e55d49bed2560917cafe8a
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 18:56:40 -0500

    cu equal

--- web/public/src/equal_curried.mjs
+++ web/public/src/equal_curried.mjs
@@ -1,1 +1,6 @@
-export function equal_curried() {}
+import { equal } from "../../../love/public/src/equal.mjs";
+export function equal_curried(left) {
+  return function equal_curried_result(right) {
+    return equal(left, right);
+  };
+}
