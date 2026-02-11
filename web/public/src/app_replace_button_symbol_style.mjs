de2e7669370790fefa910da194054bd76807dcec
Grace-Jared <christrosetolife@gmail.com>
2026-02-10 20:09:28 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style.mjs

--- web/public/src/app_replace_button_symbol_style.mjs
+++ web/public/src/app_replace_button_symbol_style.mjs
@@ -1,4 +1,3 @@
-import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_style_padding_y } from "../../../love/public/src/html_style_padding_y.mjs";
 import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { html_margin_em } from "../../../love/public/src/html_margin_em.mjs";
@@ -6,7 +5,6 @@ import { html_border_radius_em } from "../../../love/public/src/html_border_radi
 export function app_replace_button_symbol_style(b) {
   html_border_radius_em(b, 0.5);
   html_margin_em(b, 0.09);
-  html_border_none(b);
   html_style_padding_x(b, "0.37em");
   html_style_padding_y(b, "0.1em");
 }
