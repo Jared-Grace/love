927067c0ae68d2438e69ce036ece73ea9aa1d783
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:35:41 -0500

    function_auto_path ..\love\public\src\json_ends_find_index.mjs

--- public/src/json_ends_find_index.mjs
+++ public/src/json_ends_find_index.mjs
@@ -1,9 +1,8 @@
 import { json_ends } from "../../../love/public/src/json_ends.mjs";
 import { list_map_min_try } from "../../../love/public/src/list_map_min_try.mjs";
-import { text_index_of_try_curried } from "../../../love/public/src/text_index_of_try_curried.mjs";
 export function json_ends_find_index(json) {
   let list = json_ends();
-  let r = text_index_of_try_curried(json);
+  let r = text_index_of_last_try_curried(json);
   let left = list_map_min_try(list, r);
   return left;
 }
