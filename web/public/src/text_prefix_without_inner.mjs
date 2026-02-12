97a6564fd10189e68956b473794724c08624a45c
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:54:06 -0500

    pb text_prefix_without_inner

--- web/public/src/text_prefix_without_inner.mjs
+++ web/public/src/text_prefix_without_inner.mjs
@@ -1,6 +1,6 @@
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
-export function text_prefix_without_inner(prefix, s) {
+export function text_prefix_without_inner(s, prefix) {
   let skipped = text_size(prefix);
   let without = text_skip(s, skipped);
   return without;
