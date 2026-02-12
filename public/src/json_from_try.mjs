7e1dcd9cb7eed6271937981fd81f48cb5eaabcdb
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:57:43 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- public/src/json_from_try.mjs
+++ public/src/json_from_try.mjs
@@ -1,8 +1,7 @@
+import { text_last } from "../../../love/public/src/text_last.mjs";
 import { js_code_bracket_close } from "../../../love/public/src/js_code_bracket_close.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { list_last } from "../../../love/public/src/list_last.mjs";
 import { js_code_bracket_open } from "../../../love/public/src/js_code_bracket_open.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 import { json_ends_find_index } from "../../../love/public/src/json_ends_find_index.mjs";
 import { json_starts_find_index } from "../../../love/public/src/json_starts_find_index.mjs";
 import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
@@ -13,9 +12,9 @@ export function json_from_try(json) {
   let skipped = text_skip(json, left);
   let right = json_ends_find_index(skipped);
   const without = text_slice_0(skipped, right + 1);
-  let first = list_first(without);
+  let first = text_first(without);
   if (first === js_code_bracket_open()) {
-    let last = list_last(without);
+    let last = text_last(without);
     let end = js_code_bracket_close();
     if (equal_not(last, end)) {
       without += end + "";
