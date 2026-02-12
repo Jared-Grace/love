45f55eba656f56241cddb16554b3adf8caad5ed1
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:42:34 -0500

    de text_index_of_last_curried

--- public/src/text_index_of_last_curried.mjs
+++
@@ -1,8 +0,0 @@
-import { text_index_of_last } from "../../../love/public/src/text_index_of_last.mjs";
-export function text_index_of_last_curried(s) {
-  let r = function text_index_of_last_curried_result(search) {
-    let i = text_index_of_last(s, search);
-    return i;
-  };
-  return r;
-}
