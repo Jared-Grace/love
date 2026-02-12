7bfcdce20eb6718f8b388fd84add509685f5a13c
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:03:13 -0500

    function_auto_path ..\love\public\src\function_transform_result_inner.mjs

--- public/src/function_transform_result_inner.mjs
+++ public/src/function_transform_result_inner.mjs
@@ -1,8 +1,10 @@
+import { function_parse_unaliased_second } from "../../../love/public/src/function_parse_unaliased_second.mjs";
+import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 import { undefined_is } from "./undefined_is.mjs";
 export async function function_transform_result_inner(f_name, lambda$ast) {
-  let parsed = await function_parse_unaliased(f_name);
+  const u = await function_name_to_path_unalias(f_name);
+  let parsed = await function_parse_unaliased_second(u);
   let ast = property_get(parsed, "ast");
   let result = await lambda$ast(ast);
   if (undefined_is(result)) {
