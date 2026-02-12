ffe49e0bc84dde77be2eb40a1a2c4f4e11d80894
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:34:30 -0500

    function_auto_path ..\love\public\src\text_index_of_last_curried.mjs

--- public/src/text_index_of_last_curried.mjs
+++ public/src/text_index_of_last_curried.mjs
@@ -1,6 +1,8 @@
 import { text_index_of_last } from "../../../love/public/src/text_index_of_last.mjs";
 export function text_index_of_last_curried(s) {
-  return function text_index_of_last_curried_result(search) {
-    return text_index_of_last(s, search);
+  let r = function text_index_of_last_curried_result(search) {
+    let i = text_index_of_last(s, search);
+    return i;
   };
+  return r;
 }
