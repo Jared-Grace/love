ec52c4772d4761923b3e4c1144012a541fec4641
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 20:12:37 -0500

    function_auto_path ..\love\public\src\js_list_type_each_async.mjs

--- web/public/src/js_list_type_each_async.mjs
+++ web/public/src/js_list_type_each_async.mjs
@@ -1,6 +1,6 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
-export async function js_list_type_each_async(ast, type, lambda2) {
+export async function js_list_type_each_async(ast, type, lambda$item) {
   let vs = js_list_type(ast, type);
-  await each_async(vs, lambda2);
+  await each_async(vs, lambda$item);
 }
