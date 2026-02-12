7c23e6efd10769d8200603a42908b0f3ab59ef10
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:00:55 -0500

    function_auto_path ..\love\public\src\text_suffix_without.mjs

---
+++ public/src/text_slice_0.mjs
@@ -0,0 +1,5 @@
+import { text_slice } from "../../../love/public/src/text_slice.mjs";
+export function text_slice_0(s, from) {
+  let sliced = text_slice(s, 0, from);
+  return sliced;
+}
--- public/src/text_suffix_without.mjs
+++ public/src/text_suffix_without.mjs
@@ -1,7 +1,7 @@
+import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
-import { text_slice } from "../../../love/public/src/text_slice.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
 export function text_suffix_without(s, suffix) {
   let a = text_ends_with(s, suffix);
@@ -9,6 +9,6 @@ export function text_suffix_without(s, suffix) {
     error();
   }
   const from = text_size(s) - text_size(suffix);
-  const without = text_slice(s, 0, from);
+  const without = text_slice_0(s, from);
   return without;
 }
