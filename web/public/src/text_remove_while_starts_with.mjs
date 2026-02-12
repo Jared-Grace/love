de6ec73daebcd1367cb48154cd29a367f159c3ff
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:39:16 -0500

    function_auto_path ..\love\public\src\text_remove_while_starts_with.mjs

--- web/public/src/text_remove_while_starts_with.mjs
+++ web/public/src/text_remove_while_starts_with.mjs
@@ -2,13 +2,12 @@ import { equal } from "../../../love/public/src/equal.mjs";
 import { text_remove_if_starts_with } from "../../../love/public/src/text_remove_if_starts_with.mjs";
 export function text_remove_while_starts_with(item, prefix) {
   while (true) {
-    let result = null;
-    result = text_remove_if_starts_with(item, prefix);
+    let removed = text_remove_if_starts_with(item, prefix);
     ("was there no change?");
-    if (equal(result, item)) {
+    if (equal(removed, item)) {
       break;
     }
-    item = result;
+    item = removed;
   }
   return item;
 }
