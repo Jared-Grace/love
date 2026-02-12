c26b8a37735cf0416310b663b23ba0aa1bc5d4a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:05:51 -0500

    function_auto_path ..\love\public\src\function_transform_result_inner.mjs

--- public/src/function_transform_result_inner.mjs
+++ public/src/function_transform_result_inner.mjs
@@ -1,6 +1,6 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_parse } from "../../../love/public/src/function_parse.mjs";
-export async function function_transform_result_inner(f_name, lambda$ast) {
+export async function function_transform_result_inner(f_name) {
   let parsed = await function_parse(f_name);
   let ast = property_get(parsed, "ast");
   return ast;
