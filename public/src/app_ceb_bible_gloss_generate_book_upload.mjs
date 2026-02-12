c0e65ae83ed16badd4c4e139f3ae8ccab375c8a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 14:54:37 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_book_upload.mjs

--- public/src/app_ceb_bible_gloss_generate_book_upload.mjs
+++ public/src/app_ceb_bible_gloss_generate_book_upload.mjs
@@ -1,9 +1,7 @@
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { ebible_chapters } from "../../../love/public/src/ebible_chapters.mjs";
+import { ebible_chapters_each } from "../../../love/public/src/ebible_chapters_each.mjs";
 import { app_ceb_bible_gloss_generate_chapter_upload } from "../../../love/public/src/app_ceb_bible_gloss_generate_chapter_upload.mjs";
 export async function app_ceb_bible_gloss_generate_book_upload(book_code) {
   const bible_folder = "engbsb";
   let lambda$chapter_code = app_ceb_bible_gloss_generate_chapter_upload;
-  let mapped = await ebible_chapters(bible_folder, book_code);
-  await each_async(mapped, chapter_code);
+  await ebible_chapters_each(bible_folder, book_code, lambda$chapter_code);
 }
