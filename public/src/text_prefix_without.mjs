d90cc54bcff7eb8f0ce7b86ba8aac1c1e7491bd3
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:46:27 -0500

    function_auto_path ..\love\public\src\text_prefix_without.mjs

--- public/src/text_prefix_without.mjs
+++ public/src/text_prefix_without.mjs
@@ -5,7 +5,7 @@ import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs"
 import { error } from "../../../love/public/src/error.mjs";
 export function text_prefix_without(s, prefix) {
   let a = text_starts_with(s, prefix);
-  assert_json_get(b, o);
+  assert_json_get(b, () => {});
   if (not(a)) {
     error();
   }
