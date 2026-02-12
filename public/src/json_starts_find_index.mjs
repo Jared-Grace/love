91f7292989c772d3b0232bd687b6633b6920cd71
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:36:12 -0500

    function_auto_path ..\love\public\src\json_starts_find_index.mjs

--- public/src/json_starts_find_index.mjs
+++ public/src/json_starts_find_index.mjs
@@ -2,7 +2,8 @@ import { list_map_min_try } from "../../../love/public/src/list_map_min_try.mjs"
 import { text_index_of_try_curried } from "../../../love/public/src/text_index_of_try_curried.mjs";
 import { json_starts } from "../../../love/public/src/json_starts.mjs";
 export function json_starts_find_index(json) {
-  let list = json_starts();
+  let get = json_starts;
+  let list = get();
   let r = text_index_of_try_curried(json);
   let left = list_map_min_try(list, r);
   return left;
