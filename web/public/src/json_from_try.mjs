a4e58cba73b964908dad2a26a519c255ed62d8bf
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:15:07 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- web/public/src/json_from_try.mjs
+++ web/public/src/json_from_try.mjs
@@ -1,7 +1,6 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { js_code_brace_left } from "../../../love/public/src/js_code_brace_left.mjs";
 import { js_code_bracket_open } from "../../../love/public/src/js_code_bracket_open.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
@@ -12,8 +11,6 @@ export function json_from_try(json) {
   let b = js_code_bracket_open();
   let v2 = js_code_brace_left();
   let list = [v2, b];
-  function lambda(item) {}
-  each(list, lambda);
   function lambda2(item2) {}
   let mapped = list_map(list2, lambda2);
   let skipped = text_skip(json, left);
