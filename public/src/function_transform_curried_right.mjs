0eb3df5b6678263d36121f7fcc827639b419fd7c
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:46:38 -0500

    function_auto_path ..\love\public\src\function_transform_curried_right.mjs

--- public/src/function_transform_curried_right.mjs
+++ public/src/function_transform_curried_right.mjs
@@ -1,6 +1,8 @@
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function function_transform_curried_right(lambda$ast) {
-  return async function function_transform_curried_right_result(f_name) {
-    return await function_transform(f_name, lambda$ast);
+  let r2 = async function function_transform_curried_right_result(f_name) {
+    let r = await function_transform(f_name, lambda$ast);
+    return r;
   };
+  return r2;
 }
