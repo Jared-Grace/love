f0e8540e3f94ab5c510164a596fa95575131a85d
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 19:42:46 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- public/src/js_ternary_replace.mjs
+++ public/src/js_ternary_replace.mjs
@@ -63,6 +63,9 @@ export function js_ternary_replace(ast) {
     let e = js_parse_expression(code_expression);
     let arguments2 = property_get(e, "arguments");
     js_left_right_set(a, expression, arguments2);
+    log({
+      a,
+    });
     let code = js_unparse(a);
     log({
       code,
