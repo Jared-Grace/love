e510e266444787ae30300810ae67070cacab6458
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:18:57 -0500

    function_auto_path ..\love\public\src\app_gloss_bible_home_generic.mjs

--- public/src/app_gloss_bible_home_generic.mjs
+++ public/src/app_gloss_bible_home_generic.mjs
@@ -54,7 +54,8 @@ export async function app_gloss_bible_home_generic(
     let texts = null;
     if (text_use) {
       let passage_texts = property_get(passage, "texts");
-      let first = list_first(list);
+      let first = list_first(passage_texts);
+      texts = [first];
     } else {
       texts = property_get(passage, "originals");
     }
