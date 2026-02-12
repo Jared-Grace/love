c47735d96a9cba064d1e431a366b561e937cad78
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:43:12 -0500

    function_auto_path ..\love\public\src\text_index_of_last.mjs

--- public/src/text_index_of_last.mjs
+++ public/src/text_index_of_last.mjs
@@ -1,7 +1,9 @@
+import { text_index_of_assert } from "../../../love/public/src/text_index_of_assert.mjs";
 import { text_index_of_last_try } from "../../../love/public/src/text_index_of_last_try.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-export function text_index_of_last(s, search) {
+export function text_index_of_last(t, search) {
   assert_arguments(arguments, 2);
-  let i = text_index_of_last_try(s, search);
+  let i = text_index_of_last_try(t, search);
+  text_index_of_assert(t, index, item);
   return i;
 }
