d6451ace9792e1d6f479e3dd1ca81428f6a775f6
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 19:08:44 -0500

    function_auto_path ..\love\public\src\js_assign_default.mjs

--- public/src/js_assign_default.mjs
+++ public/src/js_assign_default.mjs
@@ -1,9 +1,7 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_assign } from "../../../love/public/src/js_code_assign.mjs";
+import { js_parse_expression } from "./js_parse_expression.mjs";
 export function js_assign_default() {
   let code = js_code_assign("a", "a");
-  let s = js_parse_statement(code);
-  let expression = property_get(s, "expression");
+  let s = js_parse_expression(code);
   return expression;
 }
