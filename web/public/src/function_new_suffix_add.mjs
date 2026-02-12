565b5d80f0e54b24e6b8efd3a4643c5969b10936
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:33:20 -0500

    function_auto_path ..\love\public\src\function_new_suffix_add.mjs

--- web/public/src/function_new_suffix_add.mjs
+++ web/public/src/function_new_suffix_add.mjs
@@ -1,9 +1,7 @@
-import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
-import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
+import { function_name_unalias_combine } from "../../../love/public/src/function_name_unalias_combine.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function function_new_suffix_add(f_name_existing, suffix) {
-  let unaliased = await function_name_unalias_only(f_name_existing);
-  let combined = function_name_combine(unaliased, suffix);
+  let combined = await function_name_unalias_combine(f_name_existing, suffix);
   let r = await function_new(combined);
   return r;
 }
