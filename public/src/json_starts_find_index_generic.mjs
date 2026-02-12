a7e4a558aa560de722b98a8b93b2717a4a6078f6
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:48:35 -0500

    rpi list_map_min_try min negative_not

--- public/src/json_starts_find_index_generic.mjs
+++ public/src/json_starts_find_index_generic.mjs
@@ -1,7 +1,7 @@
-import { list_map_min_try } from "../../../love/public/src/list_map_min_try.mjs";
+import { list_map_negative_not_min_try } from "../../../love/public/src/list_map_negative_not_min_try.mjs";
 export function json_starts_find_index_generic(get, c, json) {
   let list = get();
   let r = c(json);
-  let left = list_map_min_try(list, r);
+  let left = list_map_negative_not_min_try(list, r);
   return left;
 }
