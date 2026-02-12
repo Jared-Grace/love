af498c8fbcfe2b505126a5d8f6a59aa20921d866
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:04:09 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- web/public/src/json_from_try.mjs
+++ web/public/src/json_from_try.mjs
@@ -1,4 +1,4 @@
-import { string_skip_end } from "../../../love/public/src/string_skip_end.mjs";
+import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
 import { text_index_of_last } from "../../../love/public/src/text_index_of_last.mjs";
 import { text_index_of } from "../../../love/public/src/text_index_of.mjs";
@@ -7,7 +7,7 @@ export function json_from_try(json) {
   let left = text_index_of(json, "{");
   let skipped = text_skip(json, left);
   let right = text_index_of_last(skipped, "}");
-  const without = string_skip_end(skipped, right);
+  const without = text_slice_0(skipped, right);
   let result = json_from(without);
   return result;
 }
