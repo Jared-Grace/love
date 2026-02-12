7fdd9128156e47c6f275337cc360191efb4e1b95
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:22:01 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- public/src/json_from_try.mjs
+++ public/src/json_from_try.mjs
@@ -1,16 +1,13 @@
+import { json_starts } from "../../../love/public/src/json_starts.mjs";
 import { list_min_try } from "../../../love/public/src/list_min_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { js_code_brace_left } from "../../../love/public/src/js_code_brace_left.mjs";
-import { js_code_bracket_open } from "../../../love/public/src/js_code_bracket_open.mjs";
 import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
 import { text_index_of_last } from "../../../love/public/src/text_index_of_last.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export function json_from_try(json) {
-  let b = js_code_bracket_open();
-  let v2 = js_code_brace_left();
-  let list = [v2, b];
+  let list = json_starts();
   let mapped = list_map(list, text_index_of_try);
   let left = list_min_try(mapped);
   let skipped = text_skip(json, left);
---
+++ public/src/json_starts.mjs
@@ -0,0 +1,8 @@
+import { js_code_brace_left } from "../../../love/public/src/js_code_brace_left.mjs";
+import { js_code_bracket_open } from "../../../love/public/src/js_code_bracket_open.mjs";
+export function json_starts() {
+  let b = js_code_bracket_open();
+  let v2 = js_code_brace_left();
+  let list = [v2, b];
+  return list;
+}
