12b92c034712c39ca7090402b5cc07553842345e
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:39:18 -0500

    function_auto_path ..\love\public\src\text_index_of_assert.mjs

--- web/public/src/text_index_of_assert.mjs
+++ web/public/src/text_index_of_assert.mjs
@@ -2,7 +2,7 @@ import { error_json } from "../../../love/public/src/error_json.mjs";
 export function text_index_of_assert(t, index, item) {
   if (index <= -1) {
     error_json({
-      s: t,
+      t,
       item,
       index,
     });
