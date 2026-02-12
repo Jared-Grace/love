d296edd408696e20583138f87bf1fc4ebce6bacc
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:11:17 -0500

    function_auto_path ..\love\public\src\functions_asts.mjs

--- web/public/src/functions_asts.mjs
+++ web/public/src/functions_asts.mjs
@@ -1,8 +1,8 @@
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
+import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 export async function functions_asts() {
   let f_names = await functions_names();
-  let asts = list_map(f_names, function_ast);
+  let asts = await list_map_unordered_async(f_names, function_ast);
   return asts;
 }
