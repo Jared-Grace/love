793fa2237bf0d1583793700f8ed3e0445cc12d2b
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:35:11 -0500

    pb text_replace_if_starts_with

--- public/src/text_replace_if_starts_with.mjs
+++ public/src/text_replace_if_starts_with.mjs
@@ -1,6 +1,6 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-export function text_replace_if_starts_with(prefix, item, replacement) {
+export function text_replace_if_starts_with(item, prefix, replacement) {
   let sw = text_starts_with(item, prefix);
   if (sw) {
     item = text_replace(item, prefix, replacement);
