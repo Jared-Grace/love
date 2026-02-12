3f0ce66629719f672bad44cb53a136713f9c22ee
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 19:21:51 -0500

    cur property_get

--- public/src/property_get_curried_right.mjs
+++ public/src/property_get_curried_right.mjs
@@ -1,1 +1,6 @@
-export function property_get_curried_right() {}
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export function property_get_curried_right(property_name) {
+  return function property_get_curried_right_result(object) {
+    return property_get(object, property_name);
+  };
+}
