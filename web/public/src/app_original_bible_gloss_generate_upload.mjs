a3a79bfe5b1037423b144646e9e176451bbb57d6
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:07:52 -0500

    p

--- web/public/src/app_original_bible_gloss_generate_upload.mjs
+++ web/public/src/app_original_bible_gloss_generate_upload.mjs
@@ -1,8 +1,8 @@
 import { app_original_bible_gloss_generate_upload_path } from "../../../love/public/src/app_original_bible_gloss_generate_upload_path.mjs";
-import { app_original_bible_gloss_generate } from "../../../love/public/src/app_original_bible_gloss_generate.mjs";
+import { app_original_bible_gloss_generate_chapter } from "../../../love/public/src/app_original_bible_gloss_generate_chapter.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 export async function app_original_bible_gloss_generate_upload() {
-  let fn = app_original_bible_gloss_generate;
+  let fn = app_original_bible_gloss_generate_chapter;
   let path_get = app_original_bible_gloss_generate_upload_path;
   await g_generate_upload_generic(fn, path_get);
 }
