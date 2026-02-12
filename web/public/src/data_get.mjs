df007e2778c6d7d39cd0ac3aa89e248579d791f8
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:42:45 -0500

    function_auto_path ..\love\public\src\data_get.mjs

--- web/public/src/data_get.mjs
+++ web/public/src/data_get.mjs
@@ -2,14 +2,14 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { data_all } from "../../../love/public/src/data_all.mjs";
 import { property_initialize } from "../../../love/public/src/property_initialize.mjs";
 export async function data_get(property_name, value_initial, d_path) {
-  var v2 = await data_all(d_path);
-  let file_path = property_get(v2, "file_path");
-  let data = property_get(v2, "data");
+  var d = await data_all(d_path);
+  let file_path = property_get(d, "file_path");
+  let data = property_get(d, "data");
   let value = property_initialize(data, property_name, value_initial);
-  let v = {
+  let r = {
     value,
     file_path,
     data,
   };
-  return v;
+  return r;
 }
