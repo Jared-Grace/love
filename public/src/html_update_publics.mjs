2bf817e36bc1b6be6cc68febf7f51e93fc4e2350
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:55:05 -0500

    function_auto_path ..\love\public\src\html_update_publics.mjs

--- public/src/html_update_publics.mjs
+++ public/src/html_update_publics.mjs
@@ -1,5 +1,5 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
-export async function html_update_publics(searches) {
-  await each_async(searches, html_update_public);
+export async function html_update_publics(searches_comma) {
+  await each_async(searches_comma, html_update_public);
 }
