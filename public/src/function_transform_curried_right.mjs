b5c55fef8f77258f4135323424f7740879af0cbd
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:46:36 -0500

    cur function_transform

--- public/src/function_transform_curried_right.mjs
+++ public/src/function_transform_curried_right.mjs
@@ -1,1 +1,6 @@
-export function function_transform_curried_right() {}
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+export async function function_transform_curried_right(lambda$ast) {
+  return async function function_transform_curried_right_result(f_name) {
+    return await function_transform(f_name, lambda$ast);
+  };
+}
