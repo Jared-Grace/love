54936ef1453962ae59966ccf5e19c35649ad1105
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:41:39 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,4 @@
-export async function sandbox(ast) {}
+import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
+export async function sandbox(ast) {
+  let result = await data_identifiers_search(s);
+}
