eb409091a1955887ca9ec2765a6251835c1c970f
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:32:41 -0500

    function_auto_path ..\love\public\src\function_new_suffix_add.mjs

--- public/src/function_new_suffix_add.mjs
+++ public/src/function_new_suffix_add.mjs
@@ -1,7 +1,9 @@
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function function_new_suffix_add(f_name_existing) {
   let unaliased = await function_name_unalias_only(f_name_existing);
+  let combined = function_name_combine(left, right);
   let r = await function_new(f_name_existing);
   return r;
 }
