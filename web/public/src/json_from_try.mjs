74f6ad1f2ad40d017fcff3161473a3f53e07e0b4
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:31:34 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- web/public/src/json_from_try.mjs
+++ web/public/src/json_from_try.mjs
@@ -2,15 +2,14 @@ import { text_index_of_try_curried } from "../../../love/public/src/text_index_o
 import { json_starts } from "../../../love/public/src/json_starts.mjs";
 import { list_min_try } from "../../../love/public/src/list_min_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
 import { text_index_of_last } from "../../../love/public/src/text_index_of_last.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export function json_from_try(json) {
   let list = json_starts();
-  let r = text_index_of_try_curried(t);
-  let mapped = list_map(list, text_index_of_try);
+  let r = text_index_of_try_curried(json);
+  let mapped = list_map(list, r);
   let left = list_min_try(mapped);
   let skipped = text_skip(json, left);
   let right = text_index_of_last(skipped, "}");
