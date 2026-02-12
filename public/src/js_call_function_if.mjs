5a8e407c0a161996ddff12416a5b0dd106ce58fd
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 14:08:12 -0500

    function_auto_path ..\love\public\src\js_call_function_if.mjs

--- public/src/js_call_function_if.mjs
+++ public/src/js_call_function_if.mjs
@@ -1,9 +1,10 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { functions_names_includes } from "../../../love/public/src/functions_names_includes.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 export async function js_call_function_if(node, lambda$name) {
-  let { callee } = node;
+  let callee = property_get(node, "callee");
   if (js_node_type_is(callee, "Identifier")) {
-    let { name } = callee;
+    let name = property_get(callee, "name");
     const valid = await functions_names_includes(name);
     if (valid) {
       await lambda$name(name);
