7d8f5354dab306c5e0818283d4b300e5ffdcf681
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 15:36:14 -0500

    function_auto_path ..\love\public\src\log_unparse.mjs

--- web/public/src/log_unparse.mjs
+++ web/public/src/log_unparse.mjs
@@ -1,4 +1,8 @@
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
+import { log } from "./log.mjs";
 export function log_unparse(node) {
   let code = js_unparse(node);
+  log({
+    code,
+  });
 }
