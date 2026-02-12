3433f85875d7276bc5d40498ac6b160bdbe3e5b7
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:32:48 -0500

    function_auto_path ..\love\public\src\function_rename_part_insert_at.mjs

---
+++ public/src/function_name_part_insert_at.mjs
@@ -0,0 +1,15 @@
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { integer_to } from "../../../love/public/src/integer_to.mjs";
+export function function_name_part_insert_at(
+  index_string,
+  f_name_before,
+  part,
+) {
+  let index = integer_to(index_string);
+  let parts = function_name_to_parts(f_name_before);
+  list_insert(parts, index, part);
+  let f_name_after = function_name_combine_multiple(parts);
+  return f_name_after;
+}
--- public/src/function_rename_part_insert_at.mjs
+++ public/src/function_rename_part_insert_at.mjs
@@ -1,16 +1,14 @@
-import { integer_to } from "../../../love/public/src/integer_to.mjs";
-import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
-import { list_insert } from "../../../love/public/src/list_insert.mjs";
-import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { function_name_part_insert_at } from "../../../love/public/src/function_name_part_insert_at.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_part_insert_at(
   f_name_before,
   index_string,
   part,
 ) {
-  let index = integer_to(index_string);
-  let parts = function_name_to_parts(f_name_before);
-  list_insert(parts, index, part);
-  let f_name_after = function_name_combine_multiple(parts);
+  let f_name_after = function_name_part_insert_at(
+    index_string,
+    f_name_before,
+    part,
+  );
   await function_rename(f_name_before, f_name_after);
 }
