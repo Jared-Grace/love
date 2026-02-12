d94732376433ca7f9d4b0e259ba009e5f3072680
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 22:24:12 -0500

    function_auto_path ..\love\public\src\functions_transform_command.mjs

--- public/src/functions_transform_command.mjs
+++ public/src/functions_transform_command.mjs
@@ -1,4 +1,5 @@
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
 export async function functions_transform_command(lambda) {
-  return await functions_transform(lambda);
+  let r = await functions_transform(lambda);
+  return r;
 }
