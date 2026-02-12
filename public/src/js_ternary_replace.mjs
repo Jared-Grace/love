9a879c7ececede936f2af309370334a43b3e6315
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 20:49:11 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- public/src/js_ternary_replace.mjs
+++ public/src/js_ternary_replace.mjs
@@ -1,4 +1,3 @@
-import { ternary } from "../../../love/public/src/ternary.mjs";
 import { function_imports_add } from "../../../love/public/src/function_imports_add.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { js_list_type_each } from "../../../love/public/src/js_list_type_each.mjs";
@@ -86,5 +85,9 @@ export async function js_ternary_replace(ast) {
   }
   return;
   let a = null;
-  a = ternary(b === 1, 3, 2);
+  if (b === 1) {
+    a = 2;
+  } else {
+    a = 3;
+  }
 }
