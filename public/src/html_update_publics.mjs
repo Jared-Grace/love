daca571ec55f9efd6074da634f3716228bb6fdd5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:53:29 -0500

    function_auto_path ..\love\public\src\html_update_publics.mjs

--- public/src/html_update_publics.mjs
+++ public/src/html_update_publics.mjs
@@ -1,4 +1,5 @@
 import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
 export async function html_update_publics(search) {
-  return await html_update_public(search);
+  let r = await html_update_public(search);
+  return r;
 }
