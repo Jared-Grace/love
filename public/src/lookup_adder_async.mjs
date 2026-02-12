319c3d37349be159e37c416f48103ec84431eb47
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:19:01 -0500

    function_auto_path ..\love\public\src\lookup_adder_async.mjs

--- public/src/lookup_adder_async.mjs
+++ public/src/lookup_adder_async.mjs
@@ -1,11 +1,11 @@
 import { property_initialize_list } from "../../../love/public/src/property_initialize_list.mjs";
 import { list_add } from "./list_add.mjs";
-export function lookup_adder_async(lambda$la) {
+export async function lookup_adder_async(lambda$la) {
   let result = {};
   function lambda(key, value) {
     let list = property_initialize_list(result, key);
     list_add(list, value);
   }
-  lambda$la(lambda);
+  await lambda$la(lambda);
   return result;
 }
