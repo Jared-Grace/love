11107464899b4f990b839492b6d33e86e1e50102
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:36:23 -0500

    function_auto_path ..\love\public\src\text_remove_if_starts_with.mjs

--- web/public/src/text_remove_if_starts_with.mjs
+++ web/public/src/text_remove_if_starts_with.mjs
@@ -1,5 +1,5 @@
 import { text_replace_if_starts_with } from "../../../love/public/src/text_replace_if_starts_with.mjs";
-export function text_remove_if_starts_with(item, prefix, replacement) {
-  let item2 = text_replace_if_starts_with(item, prefix, replacement);
+export function text_remove_if_starts_with(item, prefix) {
+  let item2 = text_replace_if_starts_with(item, prefix, "");
   return item2;
 }
