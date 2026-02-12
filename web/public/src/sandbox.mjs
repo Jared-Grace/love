7ae60d3cf35f464206c4c43fb871d893466ffdd4
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:07:59 -0500

    rrl3 function_transform_result_inner ast

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,4 +1,4 @@
-import { function_transform_result_inner } from "../../../love/public/src/function_transform_result_inner.mjs";
+import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
@@ -7,7 +7,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_declaration_single_name } from "../../../love/public/src/js_declaration_single_name.mjs";
 import { js_visit_calls_named } from "../../../love/public/src/js_visit_calls_named.mjs";
-import { ternary } from "./ternary.mjs";
+import { ternary } from "../../../love/public/src/ternary.mjs";
 export async function sandbox() {
   async function lambda(ast) {
     function lambda3(a) {
@@ -23,6 +23,6 @@ export async function sandbox() {
     js_visit_calls_named(ast, ternary.name, lambda3);
   }
   let f_names = await functions_names();
-  let asts = list_map(f_names, function_transform_result_inner);
+  let asts = list_map(f_names, function_ast);
   await each_async(asts, lambda);
 }
