2ef0e2880a3547fbadc8b37618c7685be0a5a806
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:34:49 -0500

    function_auto_path ..\love\public\src\js_statement_call_get.mjs

--- public/src/js_statement_call_get.mjs
+++ public/src/js_statement_call_get.mjs
@@ -12,7 +12,9 @@ export function js_statement_call_get(node) {
     expression = expression_next;
     if (js_node_type_is(node, "AssignmentExpression")) {
       assignment = expression_next;
+      let right = property_get(assignment, "right");
     } else {
+      expression = expression_next;
     }
   } else if (js_node_type_is(node, "VariableDeclaration")) {
     let declarations = property_get(node, "declarations");
