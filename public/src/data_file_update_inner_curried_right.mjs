c075325b6cad2626fb06c115e6cfb52e3d6b5501
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:43:25 -0500

    function_auto_path ..\love\public\src\data_file_update_inner_curried_right.mjs

--- public/src/data_file_update_inner_curried_right.mjs
+++ public/src/data_file_update_inner_curried_right.mjs
@@ -1,1 +1,8 @@
-export function data_file_update_inner_curried_right() {}
+import { data_file_update_inner } from "../../../love/public/src/data_file_update_inner.mjs";
+export function data_file_update_inner_curried_right(data) {
+  let r2 = function data_file_update_inner_curried_right_result(parsed) {
+    let r = data_file_update_inner(parsed, data);
+    return r;
+  };
+  return r2;
+}
