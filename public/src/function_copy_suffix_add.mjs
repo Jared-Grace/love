edc311c414fe7ac3845b4e624fcf1279d14a673b
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:18:24 -0500

    function_auto_path ..\love\public\src\function_copy_suffix_add.mjs

--- public/src/function_copy_suffix_add.mjs
+++ public/src/function_copy_suffix_add.mjs
@@ -1,8 +1,12 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_copy } from "../../../love/public/src/function_copy.mjs";
 export async function function_copy_suffix_add(f_name_old, suffix) {
-  let { unaliased } = await function_name_unalias(f_name_old);
+  assert_arguments(arguments, 2);
+  let r = await function_name_unalias(f_name_old);
+  let unaliased = property_get(r, "unaliased");
   let combined = function_name_combine(unaliased, suffix);
   let v = await function_copy(unaliased, combined);
   return v;
