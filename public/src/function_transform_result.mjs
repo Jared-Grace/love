d3e1638cecbc8add2d3ddd3133a21ec8c92553b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:53:39 -0500

    function_auto_path ..\love\public\src\function_transform_result.mjs

--- public/src/function_transform_result.mjs
+++ public/src/function_transform_result.mjs
@@ -1,18 +1,18 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { function_transform_result_inner } from "../../../love/public/src/function_transform_result_inner.mjs";
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
 import { data_all_initialize } from "../../../love/public/src/data_all_initialize.mjs";
 import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 export async function function_transform_result(f_names, lambda$ast) {
   let split = text_split_comma(f_names);
   async function lambda(f_name) {
     async function lambda2() {
-      let parsed = await function_parse_unaliased(f_name);
-      let ast = property_get(parsed, "ast");
-      let result = await lambda$ast(ast);
+      let r4 = await function_transform_result_inner(f_name, lambda$ast);
+      let result = property_get(r4, "result");
+      let parsed = property_get(r4, "parsed");
       await file_js_unparse(parsed);
       return result;
     }
