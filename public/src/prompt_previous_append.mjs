a48c520e73ef466e8796c54949facb961950226a
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:47:05 -0500

    function_auto_path ..\love\public\src\prompt_previous_append.mjs

--- public/src/prompt_previous_append.mjs
+++ public/src/prompt_previous_append.mjs
@@ -1,1 +1,10 @@
-export function prompt_previous_append() {}
+import { function_run_line } from "../../../love/public/src/function_run_line.mjs";
+import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
+import { data_prompts } from "../../../love/public/src/data_prompts.mjs";
+export async function prompt_previous_append() {
+  let result = await data_prompts();
+  let e1 = list_get_end_1(result);
+  e1 += suffix + "";
+  let r = await function_run_line(e1);
+  return r;
+}
