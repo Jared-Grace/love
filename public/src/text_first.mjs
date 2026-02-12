cae8286e3dc59726e4ef8bd1581e7b44babd4b5b
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:58:55 -0500

    function_auto_path ..\love\public\src\text_first.mjs

--- public/src/text_first.mjs
+++ public/src/text_first.mjs
@@ -1,5 +1,5 @@
 import { text_get } from "../../../love/public/src/text_get.mjs";
-export function text_first(s) {
-  let item = text_get(s, 0);
+export function text_first(t) {
+  let item = text_get(t, 0);
   return item;
 }
