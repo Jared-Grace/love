f4a3ea37a2aaa3c8a59ace84d69298b65bfe17aa
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:03:33 -0500

    function_auto_path ..\love\public\src\function_transform_result_inner.mjs

--- public/src/function_transform_result_inner.mjs
+++ public/src/function_transform_result_inner.mjs
@@ -1,7 +1,7 @@
+import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
-import { function_parse_unaliased_second } from "../../../love/public/src/function_parse_unaliased_second.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { undefined_is } from "./undefined_is.mjs";
 export async function function_transform_result_inner(f_name, lambda$ast) {
@@ -9,7 +9,15 @@ export async function function_transform_result_inner(f_name, lambda$ast) {
   let unaliased = property_get(u2, "unaliased");
   let result2 = await function_name_to_path_search(unaliased);
   let u = object_merge(u2, result2);
-  let parsed = await function_parse_unaliased_second(u);
+  let unaliased2 = property_get(u, "unaliased");
+  let f_path = property_get(u, "f_path");
+  let parsed_before = await file_js_parse(f_path);
+  let parsed = object_merge(
+    {
+      unaliased2,
+    },
+    parsed_before,
+  );
   let ast = property_get(parsed, "ast");
   let result = await lambda$ast(ast);
   if (undefined_is(result)) {
