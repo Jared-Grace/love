ef88e6f2651f51bc1ddfe400de5ff2f1b28e489f
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:44:58 -0500

    function_auto_path ..\love\public\src\text_prefix_without.mjs

--- web/public/src/text_prefix_without.mjs
+++ web/public/src/text_prefix_without.mjs
@@ -1,14 +1,12 @@
+import { text_prefix_without_inner } from "../../../love/public/src/text_prefix_without_inner.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { text_skip } from "../../../love/public/src/text_skip.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
-import { text_size } from "../../../love/public/src/text_size.mjs";
 export function text_prefix_without(s, prefix) {
   let a = text_starts_with(s, prefix);
   if (not(a)) {
     error();
   }
-  let skipped = text_size(prefix);
-  let without = text_skip(s, skipped);
+  let without = text_prefix_without_inner(prefix, s);
   return without;
 }
