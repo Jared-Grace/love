6e3eb75c4777abad13bc801b438bfa81dde88acc
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:45:05 -0500

    function_auto_path ..\love\public\src\list_map_min_try.mjs

--- public/src/list_map_min_try.mjs
+++ public/src/list_map_min_try.mjs
@@ -3,7 +3,6 @@ import { list_min_try } from "../../../love/public/src/list_min_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_min_try(list, r) {
   let mapped = list_map(list, r);
-  function lambda(item) {}
   let filtered = list_filter(list2, lambda);
   let left = list_min_try(mapped);
   return left;
