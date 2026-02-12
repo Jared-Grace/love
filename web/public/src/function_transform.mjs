65399686da9f8041c75aaeccce094e84d42cd00e
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:50:16 -0500

    function_auto_path ..\love\public\src\function_transform.mjs

--- web/public/src/function_transform.mjs
+++ web/public/src/function_transform.mjs
@@ -1,8 +1,9 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_transform_result } from "../../../love/public/src/function_transform_result.mjs";
 import { function_parse_declaration_js_unparse } from "../../../love/public/src/function_parse_declaration_js_unparse.mjs";
 export async function function_transform(f_name, lambda$ast) {
+  assert_arguments(arguments, 2);
   await function_transform_result(f_name, lambda$ast);
   let output = await function_parse_declaration_js_unparse(f_name);
   return output;
-  $aa;
 }
