eed5682d1a596bc504dbb6dcaeb90aac00f4b4ad
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:37:44 -0500

    function_auto_path ..\love\public\src\text_remove_while_starts_with.mjs

--- public/src/text_remove_while_starts_with.mjs
+++ public/src/text_remove_while_starts_with.mjs
@@ -1,5 +1,6 @@
 import { text_remove_if_starts_with } from "../../../love/public/src/text_remove_if_starts_with.mjs";
 export function text_remove_while_starts_with(item, prefix) {
+  while (true) {}
   let item2 = text_remove_if_starts_with(item, prefix);
   return item2;
 }
