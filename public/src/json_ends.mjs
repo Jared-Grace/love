e276d5976647f40ce4ab92c1e0bb43853ff8955e
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:33:09 -0500

    function_auto_path ..\love\public\src\json_ends.mjs

--- public/src/json_ends.mjs
+++ public/src/json_ends.mjs
@@ -1,8 +1,8 @@
+import { js_code_brace_right } from "../../../love/public/src/js_code_brace_right.mjs";
 import { js_code_bracket_close } from "../../../love/public/src/js_code_bracket_close.mjs";
-import { js_code_brace_left } from "../../../love/public/src/js_code_brace_left.mjs";
 export function json_ends() {
   let b = js_code_bracket_close();
-  let v2 = js_code_brace_left();
+  let v2 = js_code_brace_right();
   let list = [v2, b];
   return list;
 }
