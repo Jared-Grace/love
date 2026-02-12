79d4533bcc4eb738912053c7efa6a0b9146e301f
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:44:00 -0500

    function_auto_path ..\love\public\src\data_generate.mjs

--- public/src/data_generate.mjs
+++ public/src/data_generate.mjs
@@ -1,16 +1,12 @@
 import { data_file_update_inner_curried_right } from "../../../love/public/src/data_file_update_inner_curried_right.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { data_file_update_inner } from "../../../love/public/src/data_file_update_inner.mjs";
 import { file_js_parse } from "../../../love/public/src/file_js_parse.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { functions_paths } from "../../../love/public/src/functions_paths.mjs";
 export async function data_generate(data) {
   let f_paths = await functions_paths();
   let parseds = await list_map_unordered_async(f_paths, file_js_parse);
-  function lambda(parsed) {
-    data_file_update_inner(parsed, data);
-  }
+  let lambda = data_file_update_inner_curried_right(data);
   each(parseds, lambda);
   return;
-  let r2 = data_file_update_inner_curried_right(data2);
 }
