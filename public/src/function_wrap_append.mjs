e7cf969605d8c66354cc676f31fceab438294cf9
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:51:29 -0500

    fws fw append

---
+++ public/src/function_wrap_append.mjs
@@ -0,0 +1,4 @@
+import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
+export async function function_wrap_append(f_name, f_name_wrapped) {
+  return await function_wrap(f_name, f_name_wrapped);
+}
