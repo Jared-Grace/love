867da8dbdf76e807d86e03443683e79709ce2445
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:44:58 -0500

    function_auto_path ..\love\public\src\text_prefix_without.mjs

---
+++ public/src/text_prefix_without_inner.mjs
@@ -0,0 +1,7 @@
+import { text_skip } from "../../../love/public/src/text_skip.mjs";
+import { text_size } from "../../../love/public/src/text_size.mjs";
+export function text_prefix_without_inner(prefix, s) {
+  let skipped = text_size(prefix);
+  let without = text_skip(s, skipped);
+  return without;
+}
