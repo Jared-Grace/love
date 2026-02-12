822e9fe39c1eab3a0e29131cadbf048a7778c92c
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:44:51 -0500

    function_auto_path ..\love\public\src\list_map_min_try.mjs

--- web/public/src/list_map_min_try.mjs
+++ web/public/src/list_map_min_try.mjs
@@ -1,7 +1,9 @@
+import { firebase_login } from "../../../love/public/src/firebase_login.mjs";
 import { list_min_try } from "../../../love/public/src/list_min_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function list_map_min_try(list, r) {
+export async function list_map_min_try(list, r) {
   let mapped = list_map(list, r);
+  await firebase_login(context, on_logged_in);
   let left = list_min_try(mapped);
   return left;
 }
