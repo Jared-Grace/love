7b71dc4fa201997f06ce5d04a74dc443bfc3c2ad
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:43:52 -0500

    function_auto_path ..\love\public\src\text_index_of_last_try_curried.mjs

--- public/src/text_index_of_last_try_curried.mjs
+++ public/src/text_index_of_last_try_curried.mjs
@@ -1,6 +1,8 @@
 import { text_index_of_last_try } from "../../../love/public/src/text_index_of_last_try.mjs";
 export function text_index_of_last_try_curried(s) {
-  return function text_index_of_last_try_curried_result(search) {
-    return text_index_of_last_try(s, search);
+  let r2 = function text_index_of_last_try_curried_result(search) {
+    let r = text_index_of_last_try(s, search);
+    return r;
   };
+  return r2;
 }
