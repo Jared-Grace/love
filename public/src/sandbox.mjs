524e14ce932c0e5ea089fd12c31e56d79b82f6e5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:48:04 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
-export async function sandbox(ast) {
-  async function lambda2() {}
+export async function sandbox() {
+  async function lambda2(ast) {}
   let waited = await functions_transform(lambda2);
 }
