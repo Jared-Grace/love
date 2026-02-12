1a748b656b912cda97ec98aebf208d66bbc47736
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 22:24:45 -0500

    function_auto_path ..\love\public\src\functions_transform_command.mjs

--- public/src/functions_transform_command.mjs
+++ public/src/functions_transform_command.mjs
@@ -1,5 +1,7 @@
+import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
-export async function functions_transform_command(lambda) {
-  let r = await functions_transform(lambda);
+export async function functions_transform_command(transformer_name) {
+  const transformer = await function_import_unalias(transformer_name);
+  let r = await functions_transform(transformer);
   return r;
 }
