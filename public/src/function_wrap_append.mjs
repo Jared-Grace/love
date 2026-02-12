8867533e8a4a0782649710f4c43526328b4e1fa9
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:52:59 -0500

    function_auto_path ..\love\public\src\function_wrap_append.mjs

--- public/src/function_wrap_append.mjs
+++ public/src/function_wrap_append.mjs
@@ -1,5 +1,7 @@
-import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
-export async function function_wrap_append(f_name, f_name_wrapped) {
-  let r = await function_wrap(f_name, f_name_wrapped);
+import { function_wrap_suffix_add_generic } from "../../../love/public/src/function_wrap_suffix_add_generic.mjs";
+import { text_combine } from "./text_combine.mjs";
+export async function function_wrap_append(f_name, ending) {
+  let fn = text_combine;
+  let r = await function_wrap_suffix_add_generic(f_name, fn, ending);
   return r;
 }
