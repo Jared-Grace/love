196676341e5635359f35b22d58d04c465f8b8146
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:38:47 -0500

    pb text_index_of_assert

--- web/public/src/text_index_of_assert.mjs
+++ web/public/src/text_index_of_assert.mjs
@@ -1,5 +1,5 @@
 import { error_json } from "../../../love/public/src/error_json.mjs";
-export function text_index_of_assert(index, s, item) {
+export function text_index_of_assert(s, index, item) {
   if (index <= -1) {
     error_json({
       s,
