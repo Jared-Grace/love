68e12d98bbe530c67154dae7c47c1f9e78d38be7
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 20:26:32 -0500

    function_auto_path ..\love\public\src\js_ternary_replace.mjs

--- public/src/js_ternary_replace.mjs
+++ public/src/js_ternary_replace.mjs
@@ -81,7 +81,7 @@ export async function js_ternary_replace(ast) {
   }
   js_list_type_each(ast, "IfStatement", lambda);
   if (replaced) {
-    let result = await function_imports_add(ast2, imports);
+    await function_imports_add(ast, ternary);
   }
   return;
   let index_selected = null;
