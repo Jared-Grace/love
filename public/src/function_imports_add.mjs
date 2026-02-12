b6770419d245f68fc72c388507357302479942ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 20:27:40 -0500

    function_auto_path ..\love\public\src\function_imports_add.mjs

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -1,3 +1,5 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { functions_names_to_paths } from "../../../love/public/src/functions_names_to_paths.mjs";
 import { function_name_to_path_import } from "../../../love/public/src/function_name_to_path_import.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -6,7 +8,7 @@ import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
 import { js_parse_statement_module } from "../../../love/public/src/js_parse_statement_module.mjs";
 export async function function_imports_add(ast, imports) {
   let dictionary = await functions_names_to_paths();
-  let { body } = ast;
+  let body = property_get(ast, "body");
   async function lambda(import_) {
     const from = function_name_to_path_import(import_, dictionary);
     let code = js_code_import_single(import_, from);
@@ -15,4 +17,5 @@ export async function function_imports_add(ast, imports) {
   }
   await each_async(imports, lambda);
   return;
+  text_is_assert(value);
 }
