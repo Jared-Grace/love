d5e55eccd8e0ab7646677fe58d860edf9cfb343e
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 15:46:23 -0500

    function_auto_path ..\love\public\src\file_open.mjs

--- web/public/src/file_open.mjs
+++ web/public/src/file_open.mjs
@@ -1,4 +1,3 @@
-import { ternary } from "../../../love/public/src/ternary.mjs";
 import { function_path_declaration_unparse } from "../../../love/public/src/function_path_declaration_unparse.mjs";
 import { function_name_extension } from "../../../love/public/src/function_name_extension.mjs";
 import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
@@ -14,7 +13,13 @@ export async function file_open(f_path) {
     let output = null;
     let on_true = await function_path_declaration_unparse(f_path);
     let on_false = await file_read(f_path);
-    output = ternary(ew, on_true, on_false);
+    let result = null;
+    if (ew) {
+      result = on_true;
+    } else {
+      result = on_false;
+    }
+    output = result;
     log_keep(output);
   } else {
     await file_open_editor(f_path);
