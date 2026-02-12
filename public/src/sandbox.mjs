4ab07e8d6ea16cc002d6cf4333f4a19ce4b05d24
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:52:25 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_declaration_single_name } from "../../../love/public/src/js_declaration_single_name.mjs";
@@ -9,6 +10,7 @@ export async function sandbox() {
     function lambda3(a) {
       let v = property_get(a, "v");
       let node = property_get(v, "node");
+      let code = js_unparse(ast2);
       let n = js_declaration_single_name(ast);
       log({
         n,
