9d8883784a59196844738b3a3fa0522ac2d401a8
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 19:00:44 -0500

    function_auto_path ..\love\public\src\js_declare_assign_null.mjs

--- web/public/src/js_declare_assign_null.mjs
+++ web/public/src/js_declare_assign_null.mjs
@@ -1,9 +1,11 @@
+import { lambda_get } from "../../../love/public/src/lambda_get.mjs";
 import { js_null } from "../../../love/public/src/js_null.mjs";
 import { js_declare_assign_init_set } from "../../../love/public/src/js_declare_assign_init_set.mjs";
-export function js_declare_assign_null(ast) {lg
+export function js_declare_assign_null(ast) {
   js_declare_assign_init_set(ast, lambda);
   function lambda() {
     let value = js_null();
     return value;
+    let value_get = lambda_get(value2);
   }
 }
