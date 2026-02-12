aa33cb79c04a84881720b6ea50ed7b0beb9ceaad
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:47:25 -0500

    function_auto_path ..\love\public\src\text_remove_while_starts_with.mjs

--- public/src/text_remove_while_starts_with.mjs
+++ public/src/text_remove_while_starts_with.mjs
@@ -1,15 +1,9 @@
 import { text_prefix_without_inner } from "../../../love/public/src/text_prefix_without_inner.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
 export function text_remove_while_starts_with(item, prefix) {
-  while (text_starts_with(item, prefix)) {
-    let removed = text_prefix_without_inner(item, prefix);
-    ("was there no change?");
-    if (equal(removed, item)) {
-      break;
-    }
-    item = removed;
-  }
   let result = item;
+  while (text_starts_with(result, prefix)) {
+    result = text_prefix_without_inner(item, prefix);
+  }
   return result;
 }
