b07591b64792b35b533dae78b5b218fc1990cf9f
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:48:56 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,7 +2,8 @@ import { js_visit_calls_named } from "../../../love/public/src/js_visit_calls_na
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
 export async function sandbox() {
   async function lambda2(ast) {
-    js_visit_calls_named(ast2, f_name, function lambda3() {});
+    function lambda3() {}
+    js_visit_calls_named(ast2, f_name, lambda3);
   }
   let waited = await functions_transform(lambda2);
 }
