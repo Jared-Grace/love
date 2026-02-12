5e55f75a07c7545d4095c04ea4a2adadc45cf032
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:01:17 -0500

    function_auto_path ..\love\public\src\function_parse_unaliased.mjs

--- public/src/function_parse_unaliased.mjs
+++ public/src/function_parse_unaliased.mjs
@@ -1,7 +1,7 @@
 import { function_parse_unaliased_second } from "../../../love/public/src/function_parse_unaliased_second.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 export async function function_parse_unaliased(f_name) {
-  const v = await function_name_to_path_unalias(f_name);
-  let to = await function_parse_unaliased_second(v);
+  const u = await function_name_to_path_unalias(f_name);
+  let to = await function_parse_unaliased_second(u);
   return to;
 }
