bd0d1892ec5fc903d81d38738657ec602d184664
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 22:24:11 -0500

    fws functions_transform command

--- public/src/functions_transform_command.mjs
+++ public/src/functions_transform_command.mjs
@@ -1,1 +1,4 @@
-export function functions_transform_command() {}
+import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
+export async function functions_transform_command(lambda) {
+  return await functions_transform(lambda);
+}
