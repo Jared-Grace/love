6d4f33086011394991ae5e7ba0ca73c04a2fcf35
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:45:26 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,4 +1,1 @@
-import { functions_names } from "../../../love/public/src/functions_names.mjs";
-export async function sandbox(ast) {
-  let f_names = await functions_names();
-}
+export async function sandbox(ast) {}
