8290f3681fa7fc200f800b428b8182c5cd6761ce
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:39:11 -0500

    function_auto_path ..\love\public\src\text_index_of.mjs

--- web/public/src/text_index_of.mjs
+++ web/public/src/text_index_of.mjs
@@ -1,7 +1,7 @@
 import { text_index_of_assert } from "../../../love/public/src/text_index_of_assert.mjs";
 import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
-export function text_index_of(s, item) {
-  let index = text_index_of_try(s, item);
-  text_index_of_assert(s, index, item);
+export function text_index_of(t, item) {
+  let index = text_index_of_try(t, item);
+  text_index_of_assert(t, index, item);
   return index;
 }
