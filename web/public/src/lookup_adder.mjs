b28a68774d65b9a7a9c3f4219074b9917c456b20
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:17:37 -0500

    function_auto_path ..\love\public\src\lookup_adder.mjs

--- web/public/src/lookup_adder.mjs
+++ web/public/src/lookup_adder.mjs
@@ -1,14 +1,11 @@
 import { property_initialize_list } from "../../../love/public/src/property_initialize_list.mjs";
 import { list_add } from "./list_add.mjs";
-export function lookup_adder(fn_set) {
+export function lookup_adder(lambda$la) {
   let result = {};
-  let la = function lambda(key, value) {
+  function lambda(key, value) {
     let list = property_initialize_list(result, key);
     list_add(list, value);
-  };
-  let r = {
-    la,
-    result,
-  };
-  return r;
+  }
+  lambda$la(lambda);
+  return result;
 }
