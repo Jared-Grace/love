0d7e4fdfdf02edb66e1b3825f08eca7c2b2856c5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:48:08 -0500

    function_auto_path ..\love\public\src\list_map_min_try.mjs

--- public/src/list_map_min_try.mjs
+++ public/src/list_map_min_try.mjs
@@ -1,9 +1,10 @@
+import { negative_not_is } from "../../../love/public/src/negative_not_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_min_try } from "../../../love/public/src/list_min_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function list_map_min_try(list, r) {
-  let mapped = list_map(list, r);
-  let filtered = list_filter(list2, lambda);
-  let left = list_min_try(mapped);
-  return left;
+export function list_map_min_try(list, mapper) {
+  let mapped = list_map(list, mapper);
+  let filtered = list_filter(mapped, negative_not_is);
+  let r = list_min_try(filtered);
+  return mapper;
 }
