7a64a23f9e6eba75feef431b44c60d6b4afd2039
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 14:45:03 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_upload.mjs

--- web/public/src/app_ceb_bible_gloss_generate_upload.mjs
+++ web/public/src/app_ceb_bible_gloss_generate_upload.mjs
@@ -2,6 +2,7 @@ import { app_ceb_bible_gloss_generate_upload_path } from "../../../love/public/s
 import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
 import { g_generate_upload_generic } from "../../../love/public/src/g_generate_upload_generic.mjs";
 export async function app_ceb_bible_gloss_generate_upload() {
+  "this fn name is used as a path in firebase, so be careful renaming";
   "uploads stored files";
   let fn = app_ceb_bible_gloss_generate;
   let path_get = app_ceb_bible_gloss_generate_upload_path;
