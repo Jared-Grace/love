ff0988a36fd0af1ae8f3028aec1cdae8ba4e94fe
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 09:28:54 -0500

    function_auto_path ..\love\public\src\js_destructure_functionize.mjs

--- public/src/js_parse_statement.mjs
+++ public/src/js_parse_statement.mjs
@@ -2,8 +2,8 @@ import { js_code_declaration } from "../../../love/public/src/js_code_declaratio
 import { js_parse } from "../../../love/public/src/js_parse.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function js_parse_statement(code) {
-  let code2 = js_code_declaration("a", code, true);
-  let { body } = js_parse(code2);
+  let code_d = js_code_declaration("a", code, true);
+  let { body } = js_parse(code_d);
   let { body: block } = list_first(body);
   let { body: statements } = block;
   const statement = list_first(statements);
