0bc2ad8b91173738f588ab1747989e2a62aa2aa5
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 19:22:34 -0500

    function_auto_path ..\love\public\src\list_map_property.mjs

--- public/src/list_map_property.mjs
+++ public/src/list_map_property.mjs
@@ -1,3 +1,4 @@
+import { property_get_curried_right } from "../../../love/public/src/property_get_curried_right.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_property(list, property_name) {
@@ -7,4 +8,5 @@ export function list_map_property(list, property_name) {
   }
   let mapped = list_map(list, list_map_property_lambda);
   return mapped;
+  let r = property_get_curried_right(property_name);
 }
