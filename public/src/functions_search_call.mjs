68dcc2fb4db1c01a6a1208393213fc0e168e12fc
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 13:13:38 -0500

    function_auto_path ..\love\public\src\functions_search_call.mjs

--- public/src/functions_search_call.mjs
+++ public/src/functions_search_call.mjs
@@ -6,7 +6,8 @@ import { js_declaration_single_name } from "../../../love/public/src/js_declarat
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function functions_search_call(search) {
-  let list = await list_adder_async(async function lambda2(la) {});
+  async function lambda2(la) {}
+  let list = await list_adder_async(lambda2);
   async function lambda(ast) {
     function lambda3(a) {
       let v = property_get(a, "v");
