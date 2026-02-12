22e177430d82211714349e5794e7643751d43814
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:55:37 -0500

    function_auto_path ..\love\public\src\text_split_comma.mjs

--- public/src/text_split_comma.mjs
+++ public/src/text_split_comma.mjs
@@ -1,5 +1,5 @@
 import { text_split } from "../../../love/public/src/text_split.mjs";
-export function text_split_comma(f_names) {
-  let split = text_split(f_names, ",");
+export function text_split_comma(t) {
+  let split = text_split(t, ",");
   return split;
 }
