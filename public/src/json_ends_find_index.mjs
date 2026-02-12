7c58dd17054cfd2b487f211eb511a7c86e441c8f
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:37:00 -0500

    function_auto_path ..\love\public\src\json_ends_find_index.mjs

--- public/src/json_ends_find_index.mjs
+++ public/src/json_ends_find_index.mjs
@@ -3,6 +3,6 @@ import { json_ends } from "../../../love/public/src/json_ends.mjs";
 export function json_ends_find_index(json) {
   let get = json_ends;
   let c = text_index_of_last_try_curried;
-  let left = json_starts_find_index_generic(get, c, json);
-  return left;
+  let right = json_starts_find_index_generic(get, c, json);
+  return right;
 }
