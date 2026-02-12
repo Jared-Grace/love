cf58a97eb1609114c04e586f0e5fb9812ef7f869
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 16:52:24 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- public/src/json_from_try.mjs
+++ public/src/json_from_try.mjs
@@ -1,8 +1,8 @@
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { text_remove_while_starts_with } from "../../../love/public/src/text_remove_while_starts_with.mjs";
 import { text_remove_if_starts_with } from "../../../love/public/src/text_remove_if_starts_with.mjs";
-export function json_from_try(explains_json) {
-  let removed = text_remove_if_starts_with(explains_json, "json");
+export function json_from_try(json) {
+  let removed = text_remove_if_starts_with(json, "json");
   let removed2 = text_remove_while_starts_with(removed, "`");
   let result = json_from(removed2);
   return result;
