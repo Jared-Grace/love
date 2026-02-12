b8b7dca59f15f6592a1d1a71d64e4405d2839039
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 15:34:47 -0500

    function_auto_path ..\love\public\src\js_node_atomize.mjs

--- public/src/js_node_atomize.mjs
+++ public/src/js_node_atomize.mjs
@@ -1,3 +1,4 @@
+import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { list_get_end } from "../../../love/public/src/list_get_end.mjs";
 import { js_node_atomize_variable_name_get } from "../../../love/public/src/js_node_atomize_variable_name_get.mjs";
 import { js_block_insert } from "../../../love/public/src/js_block_insert.mjs";
@@ -22,5 +23,6 @@ export async function js_node_atomize(existing_ids, v, variable_name, offset) {
   js_block_insert(stack, assign);
   let v2 = js_parse_expression(unique);
   const replaced = list_get_end(stack, offset);
+  let code = js_unparse(ast);
   object_replace(replaced, v2);
 }
