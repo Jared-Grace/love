5e23a459375975e86fa53c1d1b952f2e30500bd1
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:46:27 -0500

    function_auto_path ..\love\public\src\negative_not_is.mjs

--- public/src/negative_not_is.mjs
+++ public/src/negative_not_is.mjs
@@ -1,5 +1,7 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { negative_is } from "../../../love/public/src/negative_is.mjs";
 export function negative_not_is(i) {
   let p = negative_is(i);
+  let n = not(b);
   return p;
 }
