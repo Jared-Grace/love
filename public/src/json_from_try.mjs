89b412b2850f7712e2421a42b8106720b89f5818
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:13:54 -0500

    function_auto_path ..\love\public\src\json_from_try.mjs

--- public/src/json_from_try.mjs
+++ public/src/json_from_try.mjs
@@ -1,10 +1,12 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 import { text_slice_0 } from "../../../love/public/src/text_slice_0.mjs";
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
 import { text_index_of_last } from "../../../love/public/src/text_index_of_last.mjs";
-import { text_index_of } from "../../../love/public/src/text_index_of.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export function json_from_try(json) {
-  let left = text_index_of(json, "{");
+  let left = text_index_of_try(json, "{");
+  each(list, function lambda(item) {});
   let skipped = text_skip(json, left);
   let right = text_index_of_last(skipped, "}");
   const without = text_slice_0(skipped, right + 1);
