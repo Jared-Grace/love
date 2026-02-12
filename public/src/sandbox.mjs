ff00250c2c3d38b5811dd7322ce2c50bbd988dbc
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:58:56 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { function_new_declaration } from "../../../love/public/src/function_new_declaration.mjs";
 import { function_transform_result_inner } from "../../../love/public/src/function_transform_result_inner.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
@@ -22,7 +23,8 @@ export async function sandbox() {
     js_visit_calls_named(ast, ternary.name, lambda3);
   }
   let waited = await functions_transform(lambda2);
-  async function lambda(item) {
+  await function_new_declaration(declaration);
+  async function lambda(f_name) {
     await function_transform_result_inner(f_name, lambda$ast);
   }
   await each_async(list, lambda);
