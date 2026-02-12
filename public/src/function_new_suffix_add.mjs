b59196232806b1d0ffb96465f41dfb8cc32d345e
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:32:32 -0500

    function_auto_path ..\love\public\src\function_new_suffix_add.mjs

--- public/src/function_new_suffix_add.mjs
+++ public/src/function_new_suffix_add.mjs
@@ -1,5 +1,7 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function function_new_suffix_add(f_name_existing) {
+  let unaliased = await function_name_unalias_only(name);
   let r = await function_new(f_name_existing);
   return r;
 }
