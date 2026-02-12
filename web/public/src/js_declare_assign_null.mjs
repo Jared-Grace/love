a0cd1bcf950b6b77aa06d2ab2076995e5fad61b4
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 19:00:25 -0500

    s lambda

--- web/public/src/js_declare_assign_null.mjs
+++ web/public/src/js_declare_assign_null.mjs
@@ -1,6 +1,6 @@
 import { js_null } from "../../../love/public/src/js_null.mjs";
 import { js_declare_assign_init_set } from "../../../love/public/src/js_declare_assign_init_set.mjs";
-export function js_declare_assign_null(ast) {
+export function js_declare_assign_null(ast) {lg
   js_declare_assign_init_set(ast, lambda);
   function lambda() {
     let value = js_null();
