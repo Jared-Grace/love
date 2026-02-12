c0bb31e39a6c8a1c9ceee28b9b5eaa32e53ca1a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 12:47:23 -0500

    function_auto_path ..\love\public\src\functions_transform_list.mjs

--- web/public/src/functions_transform_list.mjs
+++ web/public/src/functions_transform_list.mjs
@@ -1,7 +1,7 @@
 import { function_transform_curried_right } from "../../../love/public/src/function_transform_curried_right.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 export async function functions_transform_list(lambda$ast, list) {
-  let lambda2 = await function_transform_curried_right(lambda$ast);
-  let waited = await list_map_unordered_async(list, lambda2);
+  let lambda = await function_transform_curried_right(lambda$ast);
+  let waited = await list_map_unordered_async(list, lambda);
   return waited;
 }
