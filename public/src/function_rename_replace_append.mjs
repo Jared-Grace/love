8177a498c0eca149afffd39b4beed58944db11ac
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:48:35 -0500

    function_auto_path ..\love\public\src\function_rename_replace_append.mjs

--- public/src/function_rename_replace_append.mjs
+++ public/src/function_rename_replace_append.mjs
@@ -1,4 +1,5 @@
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 export async function function_rename_replace_append(f_name_before, from, to) {
-  return await function_rename_replace(f_name_before, from, to);
+  let r = await function_rename_replace(f_name_before, from, to);
+  return r;
 }
