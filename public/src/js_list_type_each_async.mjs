fbced2a0cb4b6e3cbd12114608dc78e6216b9d03
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 20:12:21 -0500

    cs js_list_type_each async

---
+++ public/src/js_list_type_each_async.mjs
@@ -0,0 +1,6 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
+export function js_list_type_each_async(ast, type, lambda2) {
+  let vs = js_list_type(ast, type);
+  each(vs, lambda2);
+}
