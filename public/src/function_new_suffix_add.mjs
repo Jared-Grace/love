d9f837401e8d70ce0d4bfad8370450f52e11f146
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:32:06 -0500

    function_auto_path ..\love\public\src\function_new_suffix_add.mjs

--- public/src/function_new_suffix_add.mjs
+++ public/src/function_new_suffix_add.mjs
@@ -1,4 +1,5 @@
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function function_new_suffix_add(f_name) {
-  return await function_new(f_name);
+  let r = await function_new(f_name);
+  return r;
 }
