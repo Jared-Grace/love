48dc3294cce3f3134a2737219eba0597c18c8203
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 15:11:42 -0500

    function_auto_path ..\love\public\src\js_atomize.mjs

--- public/src/js_atomize.mjs
+++ public/src/js_atomize.mjs
@@ -11,22 +11,29 @@ export async function js_atomize(ast) {
   let existing_ids = js_identifiers_names(ast);
   let ces = js_list_type(ast, "CallExpression");
   async function lambda(v) {
-    await lambda_each(v, 0);
+    let v2 = await lambda_each(v, 0);
+    v2;
   }
-  await each_async(ces, lambda);
+  let v3 = await each_async(ces, lambda);
+  v3;
   let aes = js_list_type(ast, "AwaitExpression");
-  return;
   async function lambda_each(v, offset) {
     let stack = property_get(v, "stack");
     let stack1 = list_get_end(stack, 1 + offset);
     if (list_is(stack1)) {
       ("this list could be a block body or an argument list of a fn call");
       let variable_name = js_node_atomize_name();
-      await js_node_atomize(existing_ids, v, variable_name, offset);
+      let v4 = await js_node_atomize(existing_ids, v, variable_name, offset);
+      v4;
     }
   }
-  function lambda2() {}
-  await each_async(ces, lambda2);
+  async function lambda2(v) {
+    let v5 = await lambda_each(v, 1);
+    v5;
+  }
+  let v6 = await each_async(aes, lambda2);
+  v6;
+  return;
   log({
     aes,
   });
