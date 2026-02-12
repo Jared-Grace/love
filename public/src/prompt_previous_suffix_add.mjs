fa541e4ceb185d07d9c04e90fadf7c386a93fb26
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:38:56 -0500

    function_auto_path ..\love\public\src\prompt_previous_suffix_add.mjs

--- public/src/prompt_previous_suffix_add.mjs
+++ public/src/prompt_previous_suffix_add.mjs
@@ -1,10 +1,10 @@
+import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { function_run_line } from "../../../love/public/src/function_run_line.mjs";
-import { list_last } from "../../../love/public/src/list_last.mjs";
 import { data_prompts } from "../../../love/public/src/data_prompts.mjs";
 export async function prompt_previous_suffix_add(suffix) {
   let result = await data_prompts();
-  let last = list_last(result);
+  let last = list_get_end_1(result);
   last += suffix + "";
   log({
     last,
