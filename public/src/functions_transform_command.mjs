1db9aea3250dc9996e41f4e2950ade59d2f23388
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 21:00:30 -0500

    de functions_transform_command

--- public/src/functions_transform_command.mjs
+++
@@ -1,7 +0,0 @@
-import { functions_transform_list } from "../../../love/public/src/functions_transform_list.mjs";
-import { functions_names } from "../../../love/public/src/functions_names.mjs";
-export async function functions_transform_command(lambda) {
-  const list = await functions_names();
-  let waited = await functions_transform_list(lambda, list);
-  return waited;
-}
