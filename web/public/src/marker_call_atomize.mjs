e9898efeeb88ac92c5f06205d8f069e6d5ea5543
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 15:09:06 -0500

    function_auto_path ..\love\public\src\marker_call_atomize.mjs

--- web/public/src/marker_call_atomize.mjs
+++ web/public/src/marker_call_atomize.mjs
@@ -1,3 +1,5 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_node_atomize_name } from "../../../love/public/src/js_node_atomize_name.mjs";
 import { js_node_atomize } from "../../../love/public/src/js_node_atomize.mjs";
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
 import { js_identifiers } from "../../../love/public/src/js_identifiers.mjs";
@@ -6,9 +8,11 @@ export async function marker_call_atomize(input) {
   let v = await marker_call_replace_generic(input, lambda);
   return v;
   async function lambda(a) {
-    let { replaced, ast } = a;
+    let ast = property_get(a, "ast");
+    let replaced = property_get(a, "replaced");
     let existing = js_identifiers(ast);
     let v_match = js_visit_match(ast, replaced);
-    await js_node_atomize(existing, v_match, js_node_atomize_name());
+    let variable_name = js_node_atomize_name();
+    await js_node_atomize(existing, v_match, variable_name);
   }
 }
