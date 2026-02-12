6cae16f8da16f204577591ac039bf6c03f954fcc
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 20:06:33 -0500

    function_auto_path ..\love\public\src\js_list_type_each.mjs

--- web/public/src/js_list_type_each.mjs
+++ web/public/src/js_list_type_each.mjs
@@ -1,6 +1,6 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
 export function js_list_type_each(ast, type, lambda2) {
-  let list = js_list_type(ast, type);
-  each(list, lambda2);
+  let vs = js_list_type(ast, type);
+  each(vs, lambda2);
 }
