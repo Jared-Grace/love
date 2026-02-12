5a2923834a9ec34159079eb8399f7d02eb43d834
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 18:46:14 -0500

    function_auto_path ..\love\public\src\function_rename_replace_last_2.mjs

--- public/src/function_rename_replace_last_2.mjs
+++ public/src/function_rename_replace_last_2.mjs
@@ -1,6 +1,8 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_name_to_parts_last_2 } from "../../../love/public/src/function_name_to_parts_last_2.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 export async function function_rename_replace_last_2(f_name_before, to) {
+  let unaliased = await function_name_unalias_only(name);
   let from = function_name_to_parts_last_2(f_name_before);
   let v = await function_rename_replace(f_name_before, from, to);
   return v;
