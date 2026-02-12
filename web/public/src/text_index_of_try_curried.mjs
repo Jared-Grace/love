95f68a3ab5298f6711ac5c512e38574382fdef1d
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 17:22:57 -0500

    function_auto_path ..\love\public\src\text_index_of_try_curried.mjs

--- web/public/src/text_index_of_try_curried.mjs
+++ web/public/src/text_index_of_try_curried.mjs
@@ -1,6 +1,8 @@
 import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 export function text_index_of_try_curried(t) {
-  return function text_index_of_try_curried_result(item) {
-    return text_index_of_try(t, item);
+  let r = function text_index_of_try_curried_result(item) {
+    let v = text_index_of_try(t, item);
+    return v;
   };
+  return r;
 }
