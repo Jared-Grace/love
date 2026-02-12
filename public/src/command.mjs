dedb956f2cfff98b759dc18cec17f2bf419cbf5f
Grace-Jared <christrosetolife@gmail.com>
2026-02-11 22:23:51 -0500

    de command

--- public/src/command.mjs
+++
@@ -1,5 +0,0 @@
-import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
-export async function command(lambda) {
-  let r = await functions_transform(lambda);
-  return r;
-}
