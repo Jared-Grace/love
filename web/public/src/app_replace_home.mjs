d579d83064320a1b228c9253e4c8fdf3aafe1571
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 14:54:12 -0500

    function_auto_path ..\love\public\src\app_replace_home.mjs

--- web/public/src/app_replace_home.mjs
+++ web/public/src/app_replace_home.mjs
@@ -3,7 +3,6 @@ import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_
 import { app_replace_settings } from "../../../love/public/src/app_replace_settings.mjs";
 import { emoji_gear } from "../../../love/public/src/emoji_gear.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
-import { html_button } from "../../../love/public/src/html_button.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
@@ -13,7 +12,7 @@ export function app_replace_home(context) {
   function lambda4() {
     app_shared_screen_set(context, app_replace_settings);
   }
-  html_button(root, emoji_gear() + " Settings", lambda4);
+  app_replace_button(root, emoji_gear() + " Settings", lambda4);
   let rule_sets = app_replace_rule_sets();
   function lambda2(item, index) {
     let name2 = property_get(item, "name");
