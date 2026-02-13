4e61239c307306dc1f3c419baa16204d86c4dfa7
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:18:39 -0500

    function_auto_path ..\love\public\src\app_replace_font_size_refresh.mjs

--- web/public/src/app_replace_font_size_refresh.mjs
+++ web/public/src/app_replace_font_size_refresh.mjs
@@ -1,8 +1,8 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_font_size } from "../../../love/public/src/app_replace_font_size.mjs";
 import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
-import { html_document_root } from "../../../love/public/src/html_document_root.mjs";
 export function app_replace_font_size_refresh(context) {
-  let html = html_document_root();
+  let root = property_get(context, "root");
   let value = app_replace_font_size(context);
-  html_style_font_size(html, value + "px");
+  html_style_font_size(root, value + "px");
 }
