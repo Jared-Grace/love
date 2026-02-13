3be67aa656a267f345589b6ee133dec834c324b7
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:12:35 -0500

    function_auto_path ..\love\public\src\html_update_publics.mjs

--- web/public/src/html_update_publics.mjs
+++ web/public/src/html_update_publics.mjs
@@ -2,6 +2,7 @@ import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs"
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
 export async function html_update_publics(searches_comma) {
+  let fn = html_update_public;
   let searches = text_split_comma(searches_comma);
   await each_async(searches, html_update_public);
 }
