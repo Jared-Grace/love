d79f62dca0a91a002b11b509e28b5911b1906402
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:32:43 -0500

    function_auto_path ..\love\public\src\list_next_wrap.mjs

--- web/public/src/list_next_wrap.mjs
+++ web/public/src/list_next_wrap.mjs
@@ -5,6 +5,8 @@ import { list_last_is } from "../../../love/public/src/list_last_is.mjs";
 export function list_next_wrap(list, item) {
   let next = null;
   let li = list_last_is(list, item);
-  next = ternary(li, list_first(list), list_next(list, item));
+  let on_true = list_first(list);
+  let on_false = list_next(list, item);
+  next = ternary(li, on_true, on_false);
   return next;
 }
