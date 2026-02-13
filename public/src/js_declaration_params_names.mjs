c34cf7c37a341e0cd4739a53c0e99b6c3c8219e1
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:32:18 -0500

    function_auto_path ..\love\public\src\js_declaration_params_names.mjs

--- public/src/js_declaration_params_names.mjs
+++ public/src/js_declaration_params_names.mjs
@@ -1,4 +1,3 @@
-import { ternary } from "../../../love/public/src/ternary.mjs";
 import { list_map_squash } from "../../../love/public/src/list_map_squash.mjs";
 import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
 import { js_identifiers_to_names } from "../../../love/public/src/js_identifiers_to_names.mjs";
@@ -6,16 +5,10 @@ import { js_declaration_params_names_node } from "../../../love/public/src/js_de
 export function js_declaration_params_names(declaration) {
   let params = js_declaration_params_get(declaration);
   let params_names = null;
-  let on_true = js_identifiers_to_names(params);
-  let on_false = list_map_squash(params, js_declaration_params_names_node);
-  let b = false;
-  params_names = ternary(b, on_true, on_false);
-  return params_names;
-  let result = null;
-  if (b) {
-    result = on_true;
+  if (false) {
+    params_names = js_identifiers_to_names(params);
   } else {
-    result = on_false;
+    params_names = list_map_squash(params, js_declaration_params_names_node);
   }
-  params_names = result;
+  return params_names;
 }
