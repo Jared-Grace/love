3d0eb4ad87c22fc5728394094f37a70b4da09d6c
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:00:25 -0500

    function_auto_path ..\love\public\src\text_decompress.mjs

--- public/src/text_decompress.mjs
+++ public/src/text_decompress.mjs
@@ -1,11 +1,16 @@
-import { ternary } from "../../../love/public/src/ternary.mjs";
 import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import LZString from "lz-string";
 export async function text_decompress(compressed) {
   let lz = null;
   let condition = browser_is();
   let l = (await import("lz-string")).default;
-  lz = ternary(condition, l, LZString);
+  let result = null;
+  if (condition) {
+    result = l;
+  } else {
+    result = LZString;
+  }
+  lz = result;
   let v = lz.decompressFromUTF16(compressed);
   return v;
 }
