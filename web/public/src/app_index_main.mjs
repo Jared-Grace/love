6ffacf15b777e5097f1748376bd5b48f4e9b3fe4
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 20:39:13 -0500

    function_auto_path ..\love\public\src\app_index_main.mjs

--- web/public/src/app_index_main.mjs
+++ web/public/src/app_index_main.mjs
@@ -25,7 +25,8 @@ export function app_index_main(context) {
   let lambda = window_open_app_curried_right(hash);
   function lambda3(fn) {
     let without = app_prefix_without(fn);
-    let component = html_button_wide(root, without, () => {});
+    function lambda2() {}
+    let component = html_button_wide(root, without, lambda2);
   }
   each(fns, lambda3);
 }
