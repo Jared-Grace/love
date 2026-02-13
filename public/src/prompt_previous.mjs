62821d0525e4680c75e4ab00cdad952126b213b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:03:09 -0500

    function_auto_path ..\love\public\src\prompt_previous_at.mjs

--- public/src/prompt_previous.mjs
+++ public/src/prompt_previous.mjs
@@ -4,6 +4,6 @@ import { function_run_line } from "../../../love/public/src/function_run_line.mj
 export async function prompt_previous() {
   assert_arguments(arguments, 0);
   let previous = await prompt_previous_get();
-  let v = await function_run_line(previous);
-  return v;
+  let r = await function_run_line(previous);
+  return r;
 }
