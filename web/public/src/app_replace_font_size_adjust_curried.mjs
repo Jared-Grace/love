2952c9ccd24da0095f809c721fd01d5f78680b69
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:02:52 -0500

    function_auto_path ..\love\public\src\app_replace_font_size_adjust_curried.mjs

--- web/public/src/app_replace_font_size_adjust_curried.mjs
+++ web/public/src/app_replace_font_size_adjust_curried.mjs
@@ -1,5 +1,5 @@
 import { app_replace_font_size_adjust } from "../../../love/public/src/app_replace_font_size_adjust.mjs";
-export async function app_replace_font_size_adjust_curried(context) {
+export function app_replace_font_size_adjust_curried(context) {
   let r2 = async function app_replace_font_size_adjust_curried_result(
     value_get,
   ) {
