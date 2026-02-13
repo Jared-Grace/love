289c63cc3f490bce501bdbbfe0907916843ef630
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:08:24 -0500

    function_auto_path ..\love\public\src\app_replace_font_size_adjust.mjs

--- web/public/src/app_replace_font_size_adjust.mjs
+++ web/public/src/app_replace_font_size_adjust.mjs
@@ -1,9 +1,16 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_font_size_refresh } from "../../../love/public/src/app_replace_font_size_refresh.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { app_replace_font_size } from "../../../love/public/src/app_replace_font_size.mjs";
 export async function app_replace_font_size_adjust(context, value_get) {
   let value = await app_replace_font_size(context);
+  log({
+    value,
+  });
   value = value_get(value);
+  log({
+    value,
+  });
   storage_local_set_context(context, "font_size", value);
   app_replace_font_size_refresh(context);
 }
