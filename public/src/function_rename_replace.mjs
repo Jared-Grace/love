b0b98770540dcea7d8e44c8d079e6ae15952a314
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:11:21 -0500

    function_auto_path ..\love\public\src\function_rename_replace.mjs

--- public/src/function_rename_replace.mjs
+++ public/src/function_rename_replace.mjs
@@ -2,7 +2,7 @@ import { function_name_unalias_only } from "../../../love/public/src/function_na
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function function_rename_replace(f_name_before, from, to) {
-  let unaliased = await function_name_unalias_only(name);
+  f_name_before = await function_name_unalias_only(f_name_before);
   let f_name_after = text_replace(f_name_before, from, to);
   let v = await function_rename(f_name_before, f_name_after);
   return v;
