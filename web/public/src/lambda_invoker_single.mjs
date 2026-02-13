22474a2b2ab976b3bef6eb8c3737df391ea90d20
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:04:51 -0500

    function_auto_path ..\love\public\src\app_index_main.mjs

--- web/public/src/lambda_invoker_single.mjs
+++ web/public/src/lambda_invoker_single.mjs
@@ -1,5 +1,5 @@
 import { lambda_invoker } from "../../../love/public/src/lambda_invoker.mjs";
-export function lambda_invoker_single(lambda, fn) {
-  let r = lambda_invoker(lambda, [fn]);
+export function lambda_invoker_single(lambda, arg) {
+  let r = lambda_invoker(lambda, [arg]);
   return r;
 }
