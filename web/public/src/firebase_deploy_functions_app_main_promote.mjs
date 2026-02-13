66e4ff2fbe0c8bdf10fa273f324b226c49c31968
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:13:17 -0500

    function_auto_path ..\love\public\src\firebase_deploy_functions_app_main_promote.mjs

--- web/public/src/firebase_deploy_functions_app_main_promote.mjs
+++ web/public/src/firebase_deploy_functions_app_main_promote.mjs
@@ -1,6 +1,9 @@
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
-export async function firebase_deploy_functions_app_main_promote(a) {
-  let searches = text_split_comma(searches_comma);
-  await each_async(searches, firebase_deploy_function_app_main_promote);
+import { text_split_comma_each_async } from "../../../love/public/src/text_split_comma_each_async.mjs";
+export async function firebase_deploy_functions_app_main_promote(
+  searches_comma,
+) {
+  await text_split_comma_each_async(
+    searches_comma,
+    firebase_deploy_function_app_main_promote,
+  );
 }
