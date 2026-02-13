a0e7ec629aa0ed60834bba122f1dcd020a9c0dac
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:02:21 -0500

    function_auto_path ..\love\public\src\app_replace_settings.mjs

--- public/src/app_replace_settings.mjs
+++ public/src/app_replace_settings.mjs
@@ -1,3 +1,4 @@
+import { app_replace_font_size_adjust_curried } from "../../../love/public/src/app_replace_font_size_adjust_curried.mjs";
 import { multiply_curried_right } from "../../../love/public/src/multiply_curried_right.mjs";
 import { divide_curried_right } from "../../../love/public/src/divide_curried_right.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -9,12 +10,13 @@ import { app_replace_font_size_factor } from "../../../love/public/src/app_repla
 import { emoji_font_smaller } from "../../../love/public/src/emoji_font_smaller.mjs";
 import { emoji_font_larger } from "../../../love/public/src/emoji_font_larger.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-export function app_replace_settings(context) {
+export async function app_replace_settings(context) {
   let root = property_get(context, "root");
   html_button_screen(root, emoji_home() + "Home", context, app_replace_home);
   const factor = app_replace_font_size_factor();
   let value_get_multiply = multiply_curried_right(factor);
   let value_get_divide = divide_curried_right(factor);
+  let r2 = await app_replace_font_size_adjust_curried(context2);
   async function lambda2() {
     await app_replace_font_size_adjust(context, value_get_multiply);
   }
