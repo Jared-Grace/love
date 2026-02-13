e9b52853399247d82f7674f544240c93882dc67e
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 14:53:56 -0500

    function_auto_path ..\love\public\src\app_replace_button.mjs

--- public/src/app_replace_button.mjs
+++ public/src/app_replace_button.mjs
@@ -1,8 +1,8 @@
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { app_replace_button_rule_background_color } from "../../../love/public/src/app_replace_button_rule_background_color.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-export function app_replace_button(root, name2, lambda) {
-  let b = html_button(root, name2, lambda);
+export function app_replace_button(parent, text, lambda) {
+  let b = html_button(parent, text, lambda);
   let c = app_replace_button_rule_background_color();
   html_style_background_color_set(b, c);
   return r;
