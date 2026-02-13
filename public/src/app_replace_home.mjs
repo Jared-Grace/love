605cb6dce4e9be3974c9505e20ae3458e3ddbcc4
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 15:03:44 -0500

    function_auto_path ..\love\public\src\app_replace_home.mjs

--- public/src/app_replace_home.mjs
+++ public/src/app_replace_home.mjs
@@ -1,3 +1,4 @@
+import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
 import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { app_replace_settings } from "../../../love/public/src/app_replace_settings.mjs";
@@ -16,6 +17,7 @@ export function app_replace_home(context) {
   let rule_sets = app_replace_rule_sets();
   function lambda2(item, index) {
     let name = property_get(item, "name");
+    let a = add_1(left);
     app_replace_button_wide(root, name, lambda);
     function lambda() {
       storage_local_set_context(context, "rule_set_index", index);
