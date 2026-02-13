159bf619172e8feda55b86a60cabbaeb19b5c8d5
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:01:52 -0500

    cu app_replace_font_size_adjust

---
+++ web/public/src/app_replace_font_size_adjust_curried.mjs
@@ -0,0 +1,6 @@
+import { app_replace_font_size_adjust } from "../../../love/public/src/app_replace_font_size_adjust.mjs";
+export async function app_replace_font_size_adjust_curried(context) {
+  return async function app_replace_font_size_adjust_curried_result(value_get) {
+    return await app_replace_font_size_adjust(context, value_get);
+  };
+}
