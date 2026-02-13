8ce2dcbac3f962847c88fdc63856a8110cebb0f3
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:13:06 -0500

    function_auto_path ..\love\public\src\html_update_latests.mjs

--- public/src/html_update_latests.mjs
+++ public/src/html_update_latests.mjs
@@ -1,7 +1,5 @@
+import { text_split_comma_each_async } from "../../../love/public/src/text_split_comma_each_async.mjs";
 import { html_update_latest } from "../../../love/public/src/html_update_latest.mjs";
-import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function html_update_latests(searches_comma) {
-  let searches = text_split_comma(searches_comma);
-  await each_async(searches, html_update_latest);
+  await text_split_comma_each_async(searches_comma, html_update_latest);
 }
