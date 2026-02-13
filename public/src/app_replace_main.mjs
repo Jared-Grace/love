dde0cb68e38189b6deb824f37be49565286267b3
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 13:19:08 -0500

    function_auto_path ..\love\public\src\app_replace_main.mjs

--- public/src/app_replace_main.mjs
+++ public/src/app_replace_main.mjs
@@ -1,4 +1,4 @@
-import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
+import { html_font_sans_serif_set_html } from "../../../love/public/src/html_font_sans_serif_set_html.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_font_size_refresh } from "../../../love/public/src/app_replace_font_size_refresh.mjs";
 import { html_meta_viewport } from "../../../love/public/src/html_meta_viewport.mjs";
@@ -9,11 +9,11 @@ export async function app_replace_main(context) {
   let app_fn = app_replace;
   let screens = app_replace_screens();
   html_meta_viewport();
+  html_font_sans_serif_set_html();
   object_merge(context, {
     app_fn,
     screens,
   });
   app_shared_refresh(context);
-  let root = html_mobile_default(context);
   app_replace_font_size_refresh(context);
 }
