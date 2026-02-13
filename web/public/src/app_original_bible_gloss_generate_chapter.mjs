cc8b5a118b14fce69067302b3266783b073f4c49
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:44:30 -0500

    function_auto_path ..\love\public\src\app_original_bible_gloss_generate_chapter.mjs

--- web/public/src/app_original_bible_gloss_generate_chapter.mjs
+++ web/public/src/app_original_bible_gloss_generate_chapter.mjs
@@ -1,3 +1,4 @@
+import { app_original_bible_gloss_generate } from "../../../love/public/src/app_original_bible_gloss_generate.mjs";
 import { ebible_chapter_code_to_book } from "../../../love/public/src/ebible_chapter_code_to_book.mjs";
 import { app_gloss_bible_generate_generic } from "../../../love/public/src/app_gloss_bible_generate_generic.mjs";
 export async function app_original_bible_gloss_generate_chapter(
@@ -6,7 +7,7 @@ export async function app_original_bible_gloss_generate_chapter(
   let book_code = ebible_chapter_code_to_book(chapter_code_specified);
   let bible_folder = "engbsb";
   let language = "Greek";
-  let fn = app_original_bible_gloss_generate_chapter;
+  let fn = app_original_bible_gloss_generate;
   const bible_folders = [bible_folder];
   let last = "English is";
   await app_gloss_bible_generate_generic(
