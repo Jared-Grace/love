78b40b10e9d5cddf7b409cf20876f3146f096d6b
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:35:29 -0500

    function_auto_path ..\love\public\src\app_index_main.mjs

--- public/src/app_index_main.mjs
+++ public/src/app_index_main.mjs
@@ -1,1 +1,4 @@
-export function app_index_main(context) {}
+import { html_button } from "../../../love/public/src/html_button.mjs";
+export function app_index_main(context) {
+  let component = html_button(parent, text, function lambda2() {});
+}
