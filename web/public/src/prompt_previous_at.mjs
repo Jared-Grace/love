381aa84447cd5328c367048fe8c2bfaf0fe20bb2
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:03:06 -0500

    function_auto_path ..\love\public\src\prompt_previous_at.mjs

--- web/public/src/prompt_previous_at.mjs
+++ web/public/src/prompt_previous_at.mjs
@@ -1,3 +1,4 @@
+import { function_run_line } from "../../../love/public/src/function_run_line.mjs";
 import { list_get_end } from "../../../love/public/src/list_get_end.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { data_prompts } from "../../../love/public/src/data_prompts.mjs";
@@ -5,5 +6,6 @@ export async function prompt_previous_at(index) {
   assert_arguments(arguments, 1);
   let result = await data_prompts();
   let item = list_get_end(list, index - 1);
+  let r = await function_run_line(previous);
   return r;
 }
