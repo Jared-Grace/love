d9918057241623367acf64b4e27dad1553148c43
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:06:03 -0500

    function_auto_path ..\love\public\src\function_run_log.mjs

---
+++ web/public/src/function_run_unalias_log_before.mjs
@@ -0,0 +1,9 @@
+import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
+import { json_to } from "../../../love/public/src/json_to.mjs";
+import { emoji_run } from "../../../love/public/src/emoji_run.mjs";
+import { log_keep } from "../../../love/public/src/log_keep.mjs";
+export async function function_run_unalias_log_before(unaliased, args, f_name) {
+  log_keep(emoji_run() + " " + unaliased + " " + json_to(args));
+  let result = await function_run_unalias(f_name, args);
+  return result;
+}
