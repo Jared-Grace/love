8e5e009eac97233153ebd0848bfc339b009d1afd
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:08:45 -0500

    crl html_update_publics latests

---
+++ public/src/html_update_latests.mjs
@@ -0,0 +1,7 @@
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
+export async function html_update_latests(searches_comma) {
+  let searches = text_split_comma(searches_comma);
+  await each_async(searches, html_update_public);
+}
