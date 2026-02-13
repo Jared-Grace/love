2fc21186b0028a5271c294dbae8d45ac57381168
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:29:13 -0500

    function_auto_path ..\love\public\src\app_index_latest.mjs

--- web/public/src/app_index_latest.mjs
+++ web/public/src/app_index_latest.mjs
@@ -1,3 +1,4 @@
+import { app_main_latest } from "../../../love/public/src/app_main_latest.mjs";
 export async function app_index_latest() {
   await app_main_latest("app_index_main", "jared-grace");
 }
