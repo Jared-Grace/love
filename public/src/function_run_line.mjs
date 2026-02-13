b07dc27398b420b1e237accd1acdbbbd75c2f229
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:06:42 -0500

    function_auto_path ..\love\public\src\function_run_line.mjs

--- public/src/function_run_line.mjs
+++ public/src/function_run_line.mjs
@@ -1,7 +1,10 @@
-import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { function_run_unalias_log_before } from "../../../love/public/src/function_run_unalias_log_before.mjs";
 import { function_run_line_parse } from "../../../love/public/src/function_run_line_parse.mjs";
 export async function function_run_line(line) {
-  let { f_name, args } = await function_run_line_parse(line);
-  let v = await function_run_unalias(f_name, args);
+  let r = await function_run_line_parse(line);
+  let args = property_get(r, "args");
+  let f_name = property_get(r, "f_name");
+  let v = await function_run_unalias_log_before(f_name, args);
   return v;
 }
