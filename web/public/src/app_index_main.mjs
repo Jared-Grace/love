54af8c65d3af183ebac6601050a7b235641d49e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 20:02:22 -0500

    function_auto_path ..\love\public\src\app_index_main.mjs

--- web/public/src/app_index_main.mjs
+++ web/public/src/app_index_main.mjs
@@ -8,7 +8,6 @@ import { app_supper } from "../../../love/public/src/app_supper.mjs";
 import { app_ceb_bible } from "../../../love/public/src/app_ceb_bible.mjs";
 import { app_original_bible } from "../../../love/public/src/app_original_bible.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { window_open_app } from "../../../love/public/src/window_open_app.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_index_main(context) {
   let root = property_get(context, "root");
@@ -21,12 +20,9 @@ export function app_index_main(context) {
     app_replace,
     app_designs_universal,
   ];
-  let r2 = window_open_app_curried_right(hash);
-  function lambda(fn) {
-    async function lambda2(fn) {
-      window_open_app(fn, {});
-    }
-    let component = html_button(parent, text, lambda2);
+  let lambda = window_open_app_curried_right(hash);
+  function lambda3(fn) {
+    let component = html_button(parent, text, lambda);
   }
-  each(fns, lambda);
+  each(fns, lambda3);
 }
