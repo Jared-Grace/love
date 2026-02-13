b2d1ea834e8a4895f6c3f6e34eae2c5142c2d047
Grace-Jared <christrosetolife@gmail.com>
2026-02-12 19:51:35 -0500

    function_auto_path ..\love\public\src\app_index_main.mjs

--- web/public/src/app_index_main.mjs
+++ web/public/src/app_index_main.mjs
@@ -1,8 +1,8 @@
-import { window_open } from "../../../love/public/src/window_open.mjs";
+import { window_open_app } from "../../../love/public/src/window_open_app.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_index_main(context) {
   async function lambda2() {
-    window_open(url);
+    window_open_app(url);
   }
   let component = html_button(parent, text, lambda2);
 }
