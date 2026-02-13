049623c9159240a8737eefb6a06238b20935f2ba
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 15:02:20 -0500

    function_auto_path ..\love\public\src\app_replace_button_wide.mjs

--- web/public/src/app_replace_button_wide.mjs
+++ web/public/src/app_replace_button_wide.mjs
@@ -1,6 +1,6 @@
 import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
 export function app_replace_button_wide(root, name2, lambda) {
-  app_replace_button(root, name2, lambda);
-  html_width_full(component);
+  let b = app_replace_button(root, name2, lambda);
+  html_width_full(b);
 }
