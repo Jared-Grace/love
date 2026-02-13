c742c7b337a415fca0112cd7998a77986bbcefbe
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 12:59:39 -0500

    function_auto_path ..\love\public\src\multiply_curried_right.mjs

--- public/src/multiply_curried_right.mjs
+++ public/src/multiply_curried_right.mjs
@@ -1,6 +1,8 @@
 import { multiply } from "../../../love/public/src/multiply.mjs";
 export function multiply_curried_right(right) {
-  return function multiply_curried_right_result(left) {
-    return multiply(left, right);
+  let r = function multiply_curried_right_result(left) {
+    let p = multiply(left, right);
+    return p;
   };
+  return r;
 }
