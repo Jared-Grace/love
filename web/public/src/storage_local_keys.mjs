b0aba2b39f76af72a5c98f732243f41b304ab6ed
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:56:15 -0500

    function_auto_path ..\love\public\src\storage_local_keys.mjs

--- web/public/src/storage_local_keys.mjs
+++ web/public/src/storage_local_keys.mjs
@@ -1,4 +1,3 @@
-import { ternary } from "../../../love/public/src/ternary.mjs";
 import { storage_local_keys_global } from "../../../karate_code/public/src/storage_local_keys_global.mjs";
 import { storage_local_keys_browser } from "../../../love/public/src/storage_local_keys_browser.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
@@ -7,6 +6,12 @@ export function storage_local_keys() {
   let keys = null;
   let on_true = storage_local_keys_browser();
   let on_false = storage_local_keys_global();
-  keys = ternary(enabled2, on_true, on_false);
+  let result = null;
+  if (enabled2) {
+    result = on_true;
+  } else {
+    result = on_false;
+  }
+  keys = result;
   return keys;
 }
