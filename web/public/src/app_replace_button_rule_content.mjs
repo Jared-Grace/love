8c63c2860878ad24c247bd6bf158d545b737beaa
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:48:28 -0500

    function_auto_path ..\love\public\src\app_replace_button_rule_content.mjs

--- web/public/src/app_replace_button_rule_content.mjs
+++ web/public/src/app_replace_button_rule_content.mjs
@@ -3,13 +3,13 @@ import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { text_pad_space } from "../../../love/public/src/text_pad_space.mjs";
 import { text_arrow } from "../../../love/public/src/text_arrow.mjs";
 import { app_replace_button_side } from "../../../love/public/src/app_replace_button_side.mjs";
-export function app_replace_button_rule_content(b, left, right) {
-  let lefts = app_replace_button_side(b, left);
+export function app_replace_button_rule_content(parent, left, right) {
+  let lefts = app_replace_button_side(parent, left);
   let s = text_arrow();
   let text = text_pad_space(s);
-  let arrow = html_span_text(b, text);
+  let arrow = html_span_text(parent, text);
   html_bold(arrow);
-  let rights = app_replace_button_side(b, right);
+  let rights = app_replace_button_side(parent, right);
   let r2 = {
     lefts,
     rights,
