9063127e4a0e46d9994574ed2a9609ba91385abc
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:12:07 -0500

    function_auto_path ..\love\public\src\app_replace_button.mjs

--- web/public/src/app_replace_button.mjs
+++ web/public/src/app_replace_button.mjs
@@ -1,11 +1,7 @@
-import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
-import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
-import { app_replace_button_rule_background_color } from "../../../love/public/src/app_replace_button_rule_background_color.mjs";
+import { app_replace_button_style } from "../../../love/public/src/app_replace_button_style.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_replace_button(parent, text, lambda) {
   let b = html_button(parent, text, lambda);
-  app_replace_button_symbol_style(b);
-  let c = app_replace_button_rule_background_color();
-  html_style_background_color_set(b, c);
+  app_replace_button_style(b);
   return b;
 }
---
+++ web/public/src/app_replace_button_style.mjs
@@ -0,0 +1,8 @@
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
+import { app_replace_button_rule_background_color } from "../../../love/public/src/app_replace_button_rule_background_color.mjs";
+import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
+export function app_replace_button_style(b) {
+  app_replace_button_symbol_style(b);
+  let c = app_replace_button_rule_background_color();
+  html_style_background_color_set(b, c);
+}
