0441f787dbe73143be3ce84ad0e7d443fc375795
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:53:30 -0500

    function_auto_path ..\love\public\src\functions_search_app_prefix.mjs

--- web/public/src/functions_search_app_prefix.mjs
+++ web/public/src/functions_search_app_prefix.mjs
@@ -1,13 +1,13 @@
+import { app_shared_name_main_both } from "../../../love/public/src/app_shared_name_main_both.mjs";
 import { s } from "../../../love/public/src/s.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
 import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { app_shared_name_main_get } from "../../../love/public/src/app_shared_name_main_get.mjs";
 import { functions_search } from "../../../love/public/src/functions_search.mjs";
 export async function functions_search_app_prefix(s) {
-  let v2 = await app_shared_name_main_get(s);
+  let v2 = await app_shared_name_main_both(s);
   s = property_get(v2, "a_name");
   let a_name = app_shared_name_prefixed(s);
   let separator = function_name_separator();
