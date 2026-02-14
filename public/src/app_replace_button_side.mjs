93e99e17b1ae11ac7826afb30fdae91cd0e3c750
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 18:19:35 -0500

    function_auto_path ..\love\public\src\app_replace_button_side.mjs

--- public/src/app_replace_button_side.mjs
+++ public/src/app_replace_button_side.mjs
@@ -1,12 +1,12 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-export function app_replace_button_side(parent, left) {
+export function app_replace_button_side(parent, list_symbols) {
   function symbol_each(symbol) {
     let span = html_span_text(parent, symbol);
     app_replace_button_symbol_style(span);
     return span;
   }
-  let mapped = list_map(left, symbol_each);
+  let mapped = list_map(list_symbols, symbol_each);
   return mapped;
 }
