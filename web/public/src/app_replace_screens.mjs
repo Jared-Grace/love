2db95064e390c727a05a3e63ff011eadac333c1e
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:11:01 -0500

    function_auto_path ..\love\public\src\app_replace_screens.mjs

--- web/public/src/app_replace_screens.mjs
+++ web/public/src/app_replace_screens.mjs
@@ -1,13 +1,7 @@
-import { app_replace_goal } from "../../../love/public/src/app_replace_goal.mjs";
 import { app_replace_settings } from "../../../love/public/src/app_replace_settings.mjs";
 import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { app_replace_home } from "../../../love/public/src/app_replace_home.mjs";
 export function app_replace_screens() {
-  let s = [
-    app_replace_home,
-    app_replace_rule_set,
-    app_replace_settings,
-    app_replace_goal,
-  ];
+  let s = [app_replace_home, app_replace_rule_set, app_replace_settings];
   return s;
 }
