b0fddcf715361b92642de8701586e1af82b54b24
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:12:17 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -51,7 +51,12 @@ import { list_take } from "../../../love/public/src/list_take.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 export async function app_replace_rule_set(context) {
   let root = property_get(context, "root");
-  b=html_button_screen(root, emoji_home() + " Home", context, app_replace_home);
+  let b = html_button_screen(
+    root,
+    emoji_home() + " Home",
+    context,
+    app_replace_home,
+  );
   let index = await storage_local_get_context(context, "rule_set_index");
   let rule_sets = app_replace_rule_sets();
   let item = list_get(rule_sets, index);
