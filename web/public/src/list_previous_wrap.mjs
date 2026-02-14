2d641bc6e2306ade08f19f8a1bc3a7f4f6707cb6
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:54:32 -0500

    function_auto_path ..\love\public\src\list_previous_wrap.mjs

--- web/public/src/list_previous_wrap.mjs
+++ web/public/src/list_previous_wrap.mjs
@@ -1,4 +1,3 @@
-import { ternary } from "../../../love/public/src/ternary.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_previous } from "../../../love/public/src/list_previous.mjs";
 import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
@@ -7,6 +6,12 @@ export function list_previous_wrap(list, item) {
   let li = list_first_is(list, item);
   let on_true = list_last(list);
   let on_false = list_previous(list, item);
-  previous = ternary(li, on_true, on_false);
+  let result = null;
+  if (li) {
+    result = on_true;
+  } else {
+    result = on_false;
+  }
+  previous = result;
   return previous;
 }
