57d364f4e5d45b8d6403126bc41b1706f2066cc7
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:35:39 -0500

    function_auto_path ..\love\public\src\app_replace_goals.mjs

--- web/public/src/app_replace_goals.mjs
+++ web/public/src/app_replace_goals.mjs
@@ -1,7 +1,10 @@
+import { each_index } from "../../../love/public/src/each_index.mjs";
+import { html_style_text_left_centered } from "../../../love/public/src/html_style_text_left_centered.mjs";
+import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
+import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { text_get } from "../../../love/public/src/text_get.mjs";
-import { app_replace_buttons_numbered } from "../../../love/public/src/app_replace_buttons_numbered.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_set_get } from "../../../love/public/src/app_replace_rule_set_get.mjs";
@@ -11,7 +14,16 @@ export function app_replace_goals(context) {
   let name = property_get(item, "name");
   html_p_text(root, "Rule set: " + name);
   let goals = property_get(item, "goals");
-  app_replace_buttons_numbered(root, goals, text_get, on_click);
+  function each_item(item2, index2) {
+    let name2 = text_get(item2);
+    let a = add_1(index2) + ".";
+    let b = app_replace_button_wide(root, "", lambda);
+    html_style_text_left_centered(b, a, name2);
+    function lambda() {
+      on_click(index2);
+    }
+  }
+  each_index(goals, each_item);
   function on_click(index) {
     storage_local_set_context(context, "rule_set_index", index);
     app_shared_screen_set(context, app_replace_goals);
