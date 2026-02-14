199aa55aebcf3bb90744c4da954663ada9ec7079
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 20:02:39 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- web/public/src/function_rename_identifiers.mjs
+++ web/public/src/function_rename_identifiers.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_identifier_rename_imports_fix } from "../../../love/public/src/js_identifier_rename_imports_fix.mjs";
 import { property_exists_if_async } from "../../../love/public/src/property_exists_if_async.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
@@ -10,6 +11,9 @@ export async function function_rename_identifiers(f_name_before, f_name_after) {
   async function on_exist(f_names) {
     list_remove_if_exists(f_names, f_name_before);
     async function lambda(f_name) {
+      log({
+        f_name,
+      });
       async function lambda2(ast) {
         await js_identifier_rename_imports_fix(
           ast,
