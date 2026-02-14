cc4c94301ec1c78dc52b5d0e52ce33fe9ed976e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:58:13 -0500

    function_auto_path ..\love\public\src\app_shared_name_search_main_both.mjs

--- web/public/src/app_shared_name_search_main_both.mjs
+++ web/public/src/app_shared_name_search_main_both.mjs
@@ -1,7 +1,7 @@
 import { app_shared_name_search } from "../../../love/public/src/app_shared_name_search.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
-export async function app_shared_name_main_both(search) {
+export async function app_shared_name_search_main_both(search) {
   let a_name = await app_shared_name_search(search);
   let f_name = app_shared_name_main(a_name);
   log_keep({
