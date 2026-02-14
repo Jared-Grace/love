fc1b15e947a0ce5518872b91543125fc773094f2
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:57:37 -0500

    function_auto_path ..\love\public\src\app_shared_name_main_both.mjs

--- public/src/app_shared_name_main_both.mjs
+++ public/src/app_shared_name_main_both.mjs
@@ -1,17 +1,8 @@
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
-import { list_find_text_match_ordered } from "../../../love/public/src/list_find_text_match_ordered.mjs";
+import { app_shared_name_search } from "../../../love/public/src/app_shared_name_search.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { apps_names } from "../../../love/public/src/apps_names.mjs";
 import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
 export async function app_shared_name_main_both(search) {
-  let mapped = await apps_names();
-  let includes = list_includes(mapped, search);
-  let a_name = null;
-  if (includes) {
-    a_name = search;
-  } else {
-    a_name = list_find_text_match_ordered(mapped, search);
-  }
+  let a_name = await app_shared_name_search(search);
   let f_name = app_shared_name_main(a_name);
   log_keep({
     search,
