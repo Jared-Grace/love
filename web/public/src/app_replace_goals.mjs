8db9174482c12b555a6ba9b9a44f4a4f267e4354
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:18:39 -0500

    function_auto_path ..\love\public\src\app_replace_goals.mjs

--- web/public/src/app_replace_goals.mjs
+++ web/public/src/app_replace_goals.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_set_get } from "../../../love/public/src/app_replace_rule_set_get.mjs";
@@ -7,4 +8,5 @@ export function app_replace_goals(context) {
   let name = property_get(item, "name");
   html_p_text(root, "Rule set: " + name);
   let goals = property_get(item, "goals");
+  each(list, function lambda(item2) {});
 }
