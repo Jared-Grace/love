6e9a09f18a3c548a0808de0836d3c404fde2f928
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:51:35 -0500

    function_auto_path ..\love\public\src\app_shared_name_main_get.mjs

--- public/src/app_shared_name_main_get.mjs
+++ public/src/app_shared_name_main_get.mjs
@@ -2,22 +2,16 @@ import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { list_find_text_match_ordered } from "../../../love/public/src/list_find_text_match_ordered.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 import { app_shared_name_main } from "../../../love/public/src/app_shared_name_main.mjs";
 export async function app_shared_name_main_get(search) {
   let mapped = await apps_names();
-  let includes = list_includes(list, item);
-  let f_name = app_shared_name_main(search);
-  let e = await function_unalias_exists(f_name);
-  let exists = property_get(e, "exists");
-  let a_name = null;
-  if (exists) {
-    a_name = search;
+  let includes = list_includes(mapped, search);
+  if (includes) {
+    let a_name = search;
   } else {
-    a_name = list_find_text_match_ordered(mapped, search);
+    let a_name = list_find_text_match_ordered(mapped, search);
   }
-  f_name = app_shared_name_main(a_name);
+  let f_name = app_shared_name_main(a_name);
   log_keep({
     search,
     a_name,
