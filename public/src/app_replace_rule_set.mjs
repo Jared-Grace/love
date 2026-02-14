b0edbf4a9e7d2a59064bf012f1f7d7820b8cd775
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 18:37:26 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -109,7 +109,8 @@ export async function app_replace_rule_set(context) {
   let split = text_split_empty(end);
   let p = html_p(root);
   let mapped = app_replace_button_side(p, split);
-  let mapped2 = list_map(list, function lambda(item) {});
+  function lambda(item) {}
+  let mapped2 = list_map(list, lambda);
   app_replace_button_symbol_style_valid(p, true);
   refresh();
   function refresh() {
