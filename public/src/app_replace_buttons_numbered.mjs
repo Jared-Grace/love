47f1f1147446dd4286c5263c0f549bb8d8ec3515
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:24:42 -0500

    function_auto_path ..\love\public\src\html_style_text_left_centered.mjs

--- public/src/app_replace_buttons_numbered.mjs
+++ public/src/app_replace_buttons_numbered.mjs
@@ -1,4 +1,4 @@
-import { html_style_left_centered } from "../../../love/public/src/html_style_left_centered.mjs";
+import { html_style_text_left_centered } from "../../../love/public/src/html_style_text_left_centered.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
 import { add_1 } from "../../../love/public/src/add_1.mjs";
@@ -13,7 +13,7 @@ export function app_replace_buttons_numbered(
     let name = text_get(item);
     let a = add_1(index) + ".";
     let b = app_replace_button_wide(root, "", lambda);
-    html_style_left_centered(b, a, name);
+    html_style_text_left_centered(b, a, name);
     function lambda() {
       on_click(index);
     }
