2015ff5d9fc124903146f5566c93a9c9ee653f42
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:20:51 -0500

    function_auto_path ..\love\public\src\app_replace_home.mjs

--- public/src/app_replace_home.mjs
+++ public/src/app_replace_home.mjs
@@ -1,14 +1,11 @@
+import { app_replace_buttons_numbered } from "../../../love/public/src/app_replace_buttons_numbered.mjs";
 import { text_get } from "../../../love/public/src/text_get.mjs";
 import { app_replace_goals } from "../../../love/public/src/app_replace_goals.mjs";
-import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
 import { app_replace_settings } from "../../../love/public/src/app_replace_settings.mjs";
 import { emoji_gear } from "../../../love/public/src/emoji_gear.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
-import { each_index } from "../../../love/public/src/each_index.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_replace_home(context) {
@@ -18,26 +15,7 @@ export function app_replace_home(context) {
   }
   app_replace_button_wide(root, emoji_gear() + " Settings", lambda4);
   let rule_sets = app_replace_rule_sets();
-  function each_item(item, index) {
-    let name = text_get(item);
-    let a = add_1(index) + ".";
-    let b = app_replace_button_wide(root, "", lambda);
-    html_style_assign(b, {
-      display: "flex",
-      "align-items": "center",
-    });
-    let n = html_span_text(b, a);
-    html_style_assign(n, {});
-    let title = html_span_text(b, name);
-    html_style_assign(title, {
-      "flex-grow": "1",
-      "text-align": "center",
-    });
-    function lambda() {
-      on_click(index);
-    }
-  }
-  each_index(rule_sets, each_item);
+  app_replace_buttons_numbered(text_get, root, on_click, rule_sets);
   function on_click(index) {
     storage_local_set_context(context, "rule_set_index", index);
     app_shared_screen_set(context, app_replace_goals);
