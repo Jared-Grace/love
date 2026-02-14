60ecc1758c11fa9be92d54d1dcff82c7b0deac9a
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:07:50 -0500

    function_auto_path ..\love\public\src\app_promote.mjs

--- public/src/app_shared_name_search_main.mjs
+++ public/src/app_shared_name_search_main.mjs
@@ -2,6 +2,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_main_both } from "../../../love/public/src/app_shared_name_search_main_both.mjs";
 export async function app_shared_name_search_main(name) {
   let a = await app_shared_name_search_main_both(name);
-  let combined = property_get(a, "f_name");
-  return combined;
+  let f_name = property_get(a, "f_name");
+  return f_name;
 }
