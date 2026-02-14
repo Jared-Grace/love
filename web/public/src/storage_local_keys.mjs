be77700a135ca02dd2d30bdca014f205bc4dff4b
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:56:18 -0500

    function_auto_path ..\love\public\src\storage_local_keys.mjs

--- web/public/src/storage_local_keys.mjs
+++ web/public/src/storage_local_keys.mjs
@@ -2,12 +2,12 @@ import { storage_local_keys_global } from "../../../karate_code/public/src/stora
 import { storage_local_keys_browser } from "../../../love/public/src/storage_local_keys_browser.mjs";
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 export function storage_local_keys() {
-  let enabled2 = storage_local_enabled();
+  let enabled = storage_local_enabled();
   let keys = null;
   let on_true = storage_local_keys_browser();
   let on_false = storage_local_keys_global();
   let result = null;
-  if (enabled2) {
+  if (enabled) {
     result = on_true;
   } else {
     result = on_false;
