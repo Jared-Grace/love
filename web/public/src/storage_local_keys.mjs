b2e4dc4a918b67914f536b3d3547e121c6966d3b
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:56:06 -0500

    function_auto_path ..\love\public\src\storage_local_keys.mjs

--- web/public/src/storage_local_keys.mjs
+++ web/public/src/storage_local_keys.mjs
@@ -4,9 +4,9 @@ import { storage_local_keys_browser } from "../../../love/public/src/storage_loc
 import { storage_local_enabled } from "../../../love/public/src/storage_local_enabled.mjs";
 export function storage_local_keys() {
   let enabled2 = storage_local_enabled();
-  let keys3 = null;
+  let keys = null;
   let on_true = storage_local_keys_browser();
   let on_false = storage_local_keys_global();
-  keys3 = ternary(enabled2, on_true, on_false);
-  return keys3;
+  keys = ternary(enabled2, on_true, on_false);
+  return keys;
 }
