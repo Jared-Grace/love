236b1e94a5c7ce85f79dc5649918b8725e49873d
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 18:19:48 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -1,3 +1,4 @@
+import { app_replace_button_side } from "../../../love/public/src/app_replace_button_side.mjs";
 import { app_replace_goals } from "../../../love/public/src/app_replace_goals.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
@@ -104,6 +105,7 @@ export async function app_replace_rule_set(context) {
   let content_refreshable = html_div(root);
   let current_list = text_split_empty(start);
   let label_goal = html_p_text(root, "Goals:");
+  let mapped = app_replace_button_side(parent, list_symbols);
   refresh();
   function refresh() {
     function each_button_rule_refresh(rb, index2) {
