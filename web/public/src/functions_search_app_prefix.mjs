f409d0fd6ad537de48a15aebcbe544929406b4fe
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 20:04:05 -0500

    function_auto_path ..\love\public\src\functions_search_app_prefix.mjs

--- web/public/src/functions_search_app_prefix.mjs
+++ web/public/src/functions_search_app_prefix.mjs
@@ -1,5 +1,4 @@
 import { app_shared_name_main_both } from "../../../love/public/src/app_shared_name_main_both.mjs";
-import { s } from "../../../love/public/src/s.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
