220fd3a28f7321148807a7cef0e7944e10609c8a
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:49:47 -0500

    function_auto_path ..\love\public\src\app_promote.mjs

--- public/src/app_promote.mjs
+++ public/src/app_promote.mjs
@@ -4,8 +4,8 @@ import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs"
 import { firebase_promote_function } from "../../../love/public/src/firebase_promote_function.mjs";
 export async function app_promote(name) {
   assert_arguments(arguments, 1);
-  let v2 = await app_shared_name_main_get(name);
-  let combined = property_get(v2, "f_name");
+  let a = await app_shared_name_main_get(name);
+  let combined = property_get(a, "f_name");
   let v = await firebase_promote_function(combined);
   return v;
 }
