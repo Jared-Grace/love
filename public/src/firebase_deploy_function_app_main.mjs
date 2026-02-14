6050f3acc338dec22186f2f728e4b0eff1747a76
Grace-Jared <christrosetolife@gmail.com>
2026-02-13 19:58:17 -0500

    function_auto_path ..\love\public\src\app_promote.mjs

--- public/src/firebase_deploy_function_app_main.mjs
+++ public/src/firebase_deploy_function_app_main.mjs
@@ -1,8 +1,8 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { app_shared_name_main_both } from "../../../love/public/src/app_shared_name_main_both.mjs";
+import { app_shared_name_search_main_both } from "../../../love/public/src/app_shared_name_search_main_both.mjs";
 import { firebase_deploy_function } from "../../../love/public/src/firebase_deploy_function.mjs";
 export async function firebase_deploy_function_app_main(a_name) {
-  let v2 = await app_shared_name_main_both(a_name);
+  let v2 = await app_shared_name_search_main_both(a_name);
   let combined = property_get(v2, "f_name");
   let v = await firebase_deploy_function(combined);
   return v;
