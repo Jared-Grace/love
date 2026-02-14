e90a26f71db5faf11108fc8558357386ec4402a9
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:23:35 -0500

    function_auto_path ..\love\public\src\app_replace_buttons_numbered.mjs

--- public/src/app_replace_buttons_numbered.mjs
+++ public/src/app_replace_buttons_numbered.mjs
@@ -1,6 +1,5 @@
+import { html_style_left_centered } from "../../../love/public/src/html_style_left_centered.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
-import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
 import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { text_get } from "../../../love/public/src/text_get.mjs";
@@ -14,16 +13,7 @@ export function app_replace_buttons_numbered(
     let name = text_get(item);
     let a = add_1(index) + ".";
     let b = app_replace_button_wide(root, "", lambda);
-    html_style_assign(b, {
-      display: "flex",
-      "align-items": "center",
-    });
-    let n = html_span_text(b, a);
-    let title = html_span_text(b, name);
-    html_style_assign(title, {
-      "flex-grow": "1",
-      "text-align": "center",
-    });
+    html_style_left_centered(b, a, name);
     function lambda() {
       on_click(index);
     }
