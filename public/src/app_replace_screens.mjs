ffeb800891de83226625fca7fe1cdc88f8dd3995
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:09:15 -0500

    asa rce goal

--- public/src/app_replace_screens.mjs
+++ public/src/app_replace_screens.mjs
@@ -2,6 +2,11 @@ import { app_replace_settings } from "../../../love/public/src/app_replace_setti
 import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { app_replace_home } from "../../../love/public/src/app_replace_home.mjs";
 export function app_replace_screens() {
-  let s = [app_replace_home, app_replace_rule_set, app_replace_settings];
+  let s = [
+    app_replace_home,
+    app_replace_rule_set,
+    app_replace_settings,
+    app_replace_goal,
+  ];
   return s;
 }
