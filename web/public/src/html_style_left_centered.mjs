475e97943318acadf011d9f784d9f0e4ff075370
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 17:24:17 -0500

    function_auto_path ..\love\public\src\html_style_left_centered.mjs

--- web/public/src/html_style_left_centered.mjs
+++ web/public/src/html_style_left_centered.mjs
@@ -1,12 +1,12 @@
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export function html_style_left_centered(b, a, name) {
-  html_style_assign(b, {
+export function html_style_left_centered(component, text_left, text_centered) {
+  html_style_assign(component, {
     display: "flex",
     "align-items": "center",
   });
-  let n = html_span_text(b, a);
-  let title = html_span_text(b, name);
+  let n = html_span_text(component, text_left);
+  let title = html_span_text(component, text_centered);
   html_style_assign(title, {
     "flex-grow": "1",
     "text-align": "center",
