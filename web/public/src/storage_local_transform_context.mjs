205e5865d02ab27412ebaef93d0ca6cff1fc12b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-15 16:16:36 -0500

    function_auto_path ..\love\public\src\storage_local_transform_context.mjs

--- web/public/src/storage_local_transform_context.mjs
+++ web/public/src/storage_local_transform_context.mjs
@@ -1,3 +1,4 @@
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { undefined_not_is_assert } from "../../../love/public/src/undefined_not_is_assert.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
@@ -7,6 +8,7 @@ export function storage_local_transform_context(
   value_initial,
   lambda$value,
 ) {
+  assert_arguments(arguments, 4);
   let value = storage_local_initialize_context(context, key, value_initial);
   let value_new = lambda$value(value);
   undefined_not_is_assert(value_new);
