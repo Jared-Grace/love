95ca33c2c3ee6ec8d3cf6789310c4d6b12b1a873
Grace-Jared <christrosetolife@gmail.com>
2026-02-15 16:29:50 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets.mjs

--- web/public/src/app_replace_rule_sets.mjs
+++ web/public/src/app_replace_rule_sets.mjs
@@ -37,6 +37,42 @@ export function app_replace_rule_sets() {
       ],
     },
     {
+      name: "Grow right",
+      rules: ["a > a b"],
+      goals: [
+        {
+          start: "abb",
+          end: "bba",
+        },
+        {
+          start: "abbbb",
+          end: "bbbba",
+        },
+        {
+          start: "abbabb",
+          end: "bbbbaa",
+        },
+      ],
+    },
+    {
+      name: "Grow left",
+      rules: ["a > b a"],
+      goals: [
+        {
+          start: "abb",
+          end: "bba",
+        },
+        {
+          start: "abbbb",
+          end: "bbbba",
+        },
+        {
+          start: "abbabb",
+          end: "bbbbaa",
+        },
+      ],
+    },
+    {
       name: "Swap",
       rules: ["a b > b a"],
       goals: [
