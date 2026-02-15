02cf4cbef185685d1c9191edd90b2f19e4f36983
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 20:24:31 -0500

    function_auto_path ..\love\public\src\storage_local_transform_context.mjs

--- public/src/storage_local_transform_context.mjs
+++ public/src/storage_local_transform_context.mjs
@@ -4,9 +4,9 @@ export function storage_local_transform_context(
   context,
   key,
   value_initial,
-  transform,
+  lambda$value,
 ) {
   let value = storage_local_initialize_context(context, key, value_initial);
-  let value_new = transform(value);
+  let value_new = lambda$value(value);
   storage_local_set_context(context, key, value_new);
 }
