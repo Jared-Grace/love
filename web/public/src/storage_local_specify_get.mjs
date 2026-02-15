ac0f46e0f94fab71084cee5ecf249496890f09b5
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 20:38:35 -0500

    function_auto_path ..\love\public\src\storage_local_specify_get.mjs

--- web/public/src/storage_local_specify_get.mjs
+++ web/public/src/storage_local_specify_get.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { storage_local_specify_get_json } from "../../../love/public/src/storage_local_specify_get_json.mjs";
@@ -10,5 +11,8 @@ export function storage_local_specify_get(storage_local_key) {
     let r = json_from(json);
     result == property_get(r, "value");
   }
+  log({
+    result,
+  });
   return result;
 }
