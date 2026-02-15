2c6231420c19f18dcfbb3428dfd14e8958485abc
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 20:34:04 -0500

    function_auto_path ..\love\public\src\app_replace_button_rule_content_styled.mjs

--- public/src/app_replace_button_rule_content_styled.mjs
+++ public/src/app_replace_button_rule_content_styled.mjs
@@ -3,4 +3,5 @@ import { app_replace_button_rule_content } from "../../../love/public/src/app_re
 export function app_replace_button_rule_content_styled(title, left, right) {
   let rb = app_replace_button_rule_content(title, left, right);
   app_replace_lefts_rights_style(rb, false);
+  return rb;
 }
