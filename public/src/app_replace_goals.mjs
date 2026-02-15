20ccd64e6af32b0eef0c0416d765200a7e54bb8d
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 23:31:25 -0500

    function_auto_path ..\love\public\src\app_replace_goals.mjs

--- public/src/app_replace_goals.mjs
+++ public/src/app_replace_goals.mjs
@@ -27,7 +27,7 @@ export function app_replace_goals(context) {
     let g = app_replace_rule_sets_data_goal(d, rule_name, goal);
     let completed = property_get_or(g, "completed", false);
     let a = add_1(index) + ".";
-    if (false) {
+    if (false) {emoji_check
     }
     let start = property_get(goal, "start");
     let left = text_split_empty(start);
