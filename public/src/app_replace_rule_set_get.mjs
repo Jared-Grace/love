f0fb343c9d77f0e925cafc5da6e61665389a83e6
Grace-Jared <christrosetolife@gmail.com>
2026-02-15 15:57:59 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set_get.mjs

--- public/src/app_replace_rule_set_get.mjs
+++ public/src/app_replace_rule_set_get.mjs
@@ -2,8 +2,8 @@ import { list_get } from "../../../love/public/src/list_get.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { storage_local_get_context } from "../../../love/public/src/storage_local_get_context.mjs";
 export function app_replace_rule_set_get(context) {
-  let index = storage_local_get_context(context, "rule_set_index");
+  let rule_set_index = storage_local_get_context(context, "rule_set_index");
   let rule_sets = app_replace_rule_sets();
-  let item = list_get(rule_sets, index);
+  let item = list_get(rule_sets, rule_set_index);
   return item;
 }
