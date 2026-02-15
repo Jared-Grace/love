4a8adc2d344271950fc5d2be28e2d4556ea468a9
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 20:19:39 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -1,5 +1,6 @@
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
 import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
 import { html_centered } from "../../../love/public/src/html_centered.mjs";
 import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
@@ -217,7 +218,7 @@ export async function app_replace_rule_set(context) {
           "Good job",
           "Well done",
         ];
-        let encouragements = list_shuffle_take(encouragements_choices, 2);
+        let encouragements = list_shuffle_take(encouragements_choices, 2);e
         let p4 = html_div_text(p3, encouragement + "!");
         html_bold(p4);
         let p2 = html_p(div_below);
