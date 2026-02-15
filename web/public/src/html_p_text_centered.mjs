b3edbdb54c61105ab8f19565297bdfe705a45d10
Grace-Jared <christrosetolife@gmail.com>
2026-02-15 16:22:12 -0500

    function_auto_path ..\love\public\src\html_p_text_centered.mjs

--- web/public/src/html_p_text_centered.mjs
+++ web/public/src/html_p_text_centered.mjs
@@ -2,5 +2,6 @@ import { html_centered } from "../../../love/public/src/html_centered.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 export function html_p_text_centered(p_next, text) {
   let p = html_p_text(p_next, text);
-  html_centered(p);$r,p
+  html_centered(p);
+  return p;
 }
