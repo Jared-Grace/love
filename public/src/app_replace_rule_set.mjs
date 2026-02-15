47d19ad8980597a94ba00b6826185e064690a7a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 20:18:31 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -188,6 +188,7 @@ export async function app_replace_rule_set(context) {
     if (not(success)) {
       let eq2 = json_equal(current_list, goal_list);
       if (eq2) {
+        success = true;
         let lambda4 = app_replace_button_symbol_style_valid_curry_right(true);
         each_nested([goal_list_symbols, sbs], lambda4);
         let choices = [
