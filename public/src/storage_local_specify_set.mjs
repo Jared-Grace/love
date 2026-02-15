8d039af63679bc6125a8322daa7923412c7a443b
Grace-Jared <christrosetolife@gmail.com>
2026-02-14 20:45:18 -0500

    function_auto_path ..\love\public\src\storage_local_specify_set.mjs

--- public/src/storage_local_specify_set.mjs
+++ public/src/storage_local_specify_set.mjs
@@ -1,12 +1,7 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { json_to } from "./json_to.mjs";
 export function storage_local_specify_set(value, storage_local_key) {
   let j = json_to({
     value,
   });
   localStorage.setItem(storage_local_key, j);
-  log({
-    j,
-    storage_local_key,
-  });
 }
