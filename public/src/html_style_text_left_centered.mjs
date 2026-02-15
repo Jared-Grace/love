f497b84a19b8bd08699d01030504add2fef45e78
Grace-Jared <christrosetolife@gmail.com>
2026-02-15 15:35:08 -0500

    function_auto_path ..\love\public\src\html_style_text_left_centered.mjs

--- public/src/html_style_text_left_centered.mjs
+++ public/src/html_style_text_left_centered.mjs
@@ -14,7 +14,7 @@ export function html_style_text_left_centered(
   html_style_assign(title, {
     "flex-grow": "1",
     "text-align": "center",
-    "min-width": 0,
+    "flex-wrap": "wrap",
   });
   let r = {
     title,
