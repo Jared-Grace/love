a331b39698a38805b093fc1c315ec12566663439
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:35:50 -0500

    function_auto_path ..\love\public\src\html_insert.mjs

--- public/src/html_insert.mjs
+++ public/src/html_insert.mjs
@@ -1,11 +1,11 @@
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_insert() {
-  let element = html_component_element_get(component);
-  let element2 = html_component_element_get(component2);
+  let parent_c = html_component_element_get(component);
+  let child_c = html_component_element_get(component2);
   const i = 2;
-  if (i >= parent.children.length) {
-    parent.appendChild(newChild);
+  if (i >= parent_c.children.length) {
+    parent_c.appendChild(child_c);
   } else {
-    parent.insertBefore(newChild, parent.children[i]);
+    parent_c.insertBefore(child_c, parent_c.children[i]);
   }
 }
