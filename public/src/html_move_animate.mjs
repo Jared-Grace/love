7f28829b469e0cda858a3343ab46236dd935273d
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 17:30:22 -0500

    function_auto_path ..\love\public\src\html_move_animate.mjs

--- public/src/html_move_animate.mjs
+++ public/src/html_move_animate.mjs
@@ -1,16 +1,24 @@
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+import { html_scroll_generic_wait } from "../../../love/public/src/html_scroll_generic_wait.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
-export async function html_move_animate(movingBtn, targetBtn, duration = 500) {
-  const movingRect = movingBtn.getBoundingClientRect();
-  const targetRect = targetBtn.getBoundingClientRect();
+export async function html_move_animate(
+  component_from,
+  component_to,
+  duration = 500,
+) {
+  let e = await html_scroll_generic_wait(component);
+  let container_e = html_component_element_get(container);
+  const movingRect = component_from.getBoundingClientRect();
+  const targetRect = component_to.getBoundingClientRect();
   const offsetX = targetRect.left - movingRect.left;
   const offsetY = targetRect.top - movingRect.top;
-  movingBtn.style.transition = `transform ${duration}ms`;
-  movingBtn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  component_from.style.transition = `transform ${duration}ms`;
+  component_from.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
   await sleep(duration);
   function lambda() {
-    movingBtn.style.transition = "";
-    movingBtn.style.transform = "";
-    targetBtn.parentNode.insertBefore(movingBtn, targetBtn);
+    component_from.style.transition = "";
+    component_from.style.transform = "";
+    component_to.parentNode.insertBefore(component_from, component_to);
   }
   setTimeout(lambda, duration);
 }
