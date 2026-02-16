d0729d5afdda7c2b52cd41a272b2e476d7d6aa53
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:38:43 -0500

    function_auto_path ..\love\public\src\html_insert.mjs

--- web/public/src/html_insert.mjs
+++ web/public/src/html_insert.mjs
@@ -1,12 +1,10 @@
-import { assert } from "../../../love/public/src/assert.mjs";
-import { less_than_equal } from "../../../love/public/src/less_than_equal.mjs";
+import { less_than_equal_assert } from "../../../love/public/src/less_than_equal_assert.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_insert(parent, child, index) {
   let parent_c = html_component_element_get(parent);
   let child_c = html_component_element_get(child);
   let pcl = parent_c.children.length;
-  let le = less_than_equal(index, pcl);
-  assert(le);
+  less_than_equal_assert(index, pcl);
   if (index > pcl) {
     parent_c.appendChild(child_c);
   } else {
