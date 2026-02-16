bc02ab2ca876e4261c1230627dcdae3498db5334
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:57:43 -0500

    function_auto_path ..\love\public\src\app_a_list_overlay_generic.mjs

---
+++ web/public/src/html_style_overflow_hidden.mjs
@@ -0,0 +1,4 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+export function html_style_overflow_hidden(overlay) {
+  html_style_set(overlay, "overflow", "hidden");
+}
