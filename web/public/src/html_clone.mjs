67b37287da48d84f0152dd48545eccfc221d7ac5
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:29:18 -0500

    function_auto_path ..\love\public\src\html_clone.mjs

--- web/public/src/html_clone.mjs
+++ web/public/src/html_clone.mjs
@@ -1,5 +1,7 @@
+import { html_component_wrap } from "../../../love/public/src/html_component_wrap.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_clone(c) {
   let element = html_component_element_get(c);
   element.cloneNode(true);
+  let v = html_component_wrap(element2);
 }
