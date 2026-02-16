396621084a1453a4a718e3b88eb4b85852360c77
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 16:37:04 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -186,6 +186,7 @@ export async function app_replace_rule_set(context) {
       return sb;
     }
     let sbs = list_map_index(current_list, symbols_mapper);
+    ("no success before?");
     if (not(success)) {
       let eq2 = json_equal(current_list, goal_list);
       if (eq2) {
