ac1103954a4ca02a03bed25dd7950dfc2fb1da94
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:36:23 -0500

    function_auto_path ..\love\public\src\html_insert.mjs

--- web/public/src/html_insert.mjs
+++ web/public/src/html_insert.mjs
@@ -2,7 +2,6 @@ import { html_component_element_get } from "../../../love/public/src/html_compon
 export function html_insert(parent, child) {
   let parent_c = html_component_element_get(parent);
   let child_c = html_component_element_get(child);
-  const i = 2;
   if (i >= parent_c.children.length) {
     parent_c.appendChild(child_c);
   } else {
