31c053addfb6abecb3e175e217cb9ead5706433f
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:57:01 -0500

    function_auto_path ..\love\public\src\html_hide_loadable.mjs

---
+++ web/public/src/html_visibility_hidden.mjs
@@ -0,0 +1,6 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function html_visibility_hidden(i) {
+  html_style_assign(i, {
+    visibility: "hidden",
+  });
+}
