9ce9436089eaf11d14712e4cb3009e1b0b5dcb89
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 17:29:17 -0500

    function_auto_path ..\love\public\src\html_move_animate.mjs

--- public/src/html_move_animate.mjs
+++ public/src/html_move_animate.mjs
@@ -1,1 +1,14 @@
-export function html_move_animate() {}
+export function html_move_animate(movingBtn, targetBtn, duration = 500) {
+  const movingRect = movingBtn.getBoundingClientRect();
+  const targetRect = targetBtn.getBoundingClientRect();
+  const offsetX = targetRect.left - movingRect.left;
+  const offsetY = targetRect.top - movingRect.top;
+  movingBtn.style.transition = `transform ${duration}ms`;
+  movingBtn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  function lambda() {
+    movingBtn.style.transition = "";
+    movingBtn.style.transform = "";
+    targetBtn.parentNode.insertBefore(movingBtn, targetBtn);
+  }
+  setTimeout(lambda, duration);
+}
