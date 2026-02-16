dd9ca4fd192a86280413159fda017915a20be664
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:57:43 -0500

    function_auto_path ..\love\public\src\app_a_list_overlay_generic.mjs

--- web/public/src/app_a_list_overlay_generic.mjs
+++ web/public/src/app_a_list_overlay_generic.mjs
@@ -1,13 +1,13 @@
+import { html_style_overflow_hidden } from "../../../love/public/src/html_style_overflow_hidden.mjs";
 import { app_a_list_chooser } from "../../../love/public/src/app_a_list_chooser.mjs";
 import { app_a_button_function } from "../../../love/public/src/app_a_button_function.mjs";
 import { object_copy_assign } from "../../../love/public/src/object_copy_assign.mjs";
-import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_a_overlay } from "../../../love/public/src/app_a_overlay.mjs";
 export function app_a_list_overlay_generic(a, texts, noun, lambda$text) {
   let overlay_result = app_a_overlay(a);
   let overlay = property_get(overlay_result, "overlay");
-  html_style_set(overlay, "overflow", "hidden");
+  html_style_overflow_hidden(overlay);
   const replacement = {
     root: overlay,
   };
