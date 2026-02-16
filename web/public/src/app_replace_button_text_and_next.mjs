2dd86137db8dcbecbdd22bade1c07887d4fc4245
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 15:57:28 -0500

    function_auto_path ..\love\public\src\app_replace_goals.mjs

---
+++ web/public/src/app_replace_button_text_and_next.mjs
@@ -0,0 +1,27 @@
+import { emoji_point_right } from "../../../love/public/src/emoji_point_right.mjs";
+import { string_pad_left_space } from "../../../love/public/src/string_pad_left_space.mjs";
+import { emoji_check } from "../../../love/public/src/emoji_check.mjs";
+import { add_1 } from "../../../love/public/src/add_1.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+export function app_replace_button_text_and_next(
+  completed,
+  completed_previous,
+  index,
+) {
+  const choose_this_next = not(completed) && completed_previous;
+  let a = add_1(index) + ".";
+  if (completed) {
+    let e = emoji_check();
+    a += string_pad_left_space(e);
+  } else {
+    if (choose_this_next) {
+      let e = emoji_point_right();
+      a += string_pad_left_space(e);
+    }
+  }
+  let r3 = {
+    a,
+    choose_this_next,
+  };
+  return r3;
+}
