6fa63a0a9d780861a527a8530bf4df086bcd6fd7
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 15:50:45 -0500

    function_auto_path ..\love\public\src\app_replace_home.mjs

--- public/src/app_replace_home.mjs
+++ public/src/app_replace_home.mjs
@@ -17,6 +17,7 @@ export function app_replace_home(context) {
   app_replace_button_wide(root, emoji_gear() + " Settings", lambda4);
   let rule_sets = app_replace_rule_sets();
   function each_item(item, index2) {
+    let goals = property_get(item, "goals");
     let name = property_get(item, "name");
     let a = add_1(index2) + ".";
     let b = app_replace_button_wide(root, "", lambda);
