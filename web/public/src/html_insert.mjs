70754c6ec3e7e886a1a96991ec641c4e1ed7416b
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:35:30 -0500

    function_auto_path ..\love\public\src\html_insert.mjs

--- web/public/src/html_insert.mjs
+++ web/public/src/html_insert.mjs
@@ -1,5 +1,7 @@
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_insert() {
-  hecp;
+  let element = html_component_element_get(component);
+  let element2 = html_component_element_get(component2);
   const i = 2;
   if (i >= parent.children.length) {
     parent.appendChild(newChild);
