58709ab5aa46b55f46107e4ec17e221bea6ede58
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 18:30:50 -0500

    s clo

--- web/public/src/html_clone.mjs
+++ web/public/src/html_clone.mjs
@@ -2,6 +2,6 @@ import { html_component_wrap } from "../../../love/public/src/html_component_wra
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_clone(c) {
   let element = html_component_element_get(c);
-  element.cloneNode(true);
-  let v = html_component_wrap(element2);
+  cloned=element.cloneNode(true);
+  let c = html_component_wrap(cloned);$r,c
 }
