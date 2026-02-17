59620c9391580aaa630ba906d4c72d2dcb554d6d
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:55:35 -0500

    function_auto_path ..\love\public\src\html_move_animate_multiple_parent_remove.mjs

--- web/public/src/html_move_animate_multiple_parent_remove.mjs
+++ web/public/src/html_move_animate_multiple_parent_remove.mjs
@@ -1,3 +1,4 @@
+import { html_move_animate_curried_right } from "../../../love/public/src/html_move_animate_curried_right.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_wait } from "../../../love/public/src/list_wait.mjs";
 import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
@@ -8,6 +9,7 @@ export async function html_move_animate_multiple_parent_remove(
   tos,
   duration,
 ) {
+  let r2 = await html_move_animate_curried_right(duration2);
   async function lambda6(a, b) {
     await html_move_animate(a, b, duration);
   }
