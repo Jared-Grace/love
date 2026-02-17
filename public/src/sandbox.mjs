717c0028b6209ff68c710f4c4c66c497c2745127
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:36:27 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 export async function sandbox() {
-  let combined = text_combine_multiple(["a"]);
+  let combined = text_combine_multiple(["a", "b"]);
   return combined;
 }
