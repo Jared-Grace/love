44ae1824c919efc8c16793ad682b5b0d5fd06399
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:57:47 -0500

    function_auto_path ..\love\public\src\list_map_pairs_wait.mjs

--- public/src/list_map_pairs_wait.mjs
+++ public/src/list_map_pairs_wait.mjs
@@ -1,6 +1,7 @@
 import { list_wait } from "../../../love/public/src/list_wait.mjs";
 import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
-export async function list_map_pairs_wait(froms, tos, r2) {
-  let mapped = list_map_pairs(froms, tos, r2);
-  let v = await list_wait(mapped);
+export async function list_map_pairs_wait(lefts, rights, lambda$left$right) {
+  let mapped = list_map_pairs(lefts, rights, lambda$left$right);
+  let waited = await list_wait(mapped);
+  return waited;
 }
