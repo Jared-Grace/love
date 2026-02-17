e21b4134cce541ff6d244d3cdfeeb142296db28f
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 15:14:45 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style.mjs

--- public/src/app_replace_button_symbol_style.mjs
+++ public/src/app_replace_button_symbol_style.mjs
@@ -1,18 +1,6 @@
+import { app_replace_button_symbol_style_inner } from "../../../love/public/src/app_replace_button_symbol_style_inner.mjs";
 import { html_display_inline_block } from "../../../love/public/src/html_display_inline_block.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
-import { html_style_padding_y } from "../../../love/public/src/html_style_padding_y.mjs";
-import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
-import { html_margin_em } from "../../../love/public/src/html_margin_em.mjs";
-import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 export function app_replace_button_symbol_style(b) {
-  html_border_radius_em(b, 0.5);
-  html_margin_em(b, 0.09);
-  html_border_none(b);
-  html_style_padding_x(b, "0.37em");
-  html_style_padding_y(b, "0.1em");
-  html_style_assign(b, {
-    "line-height": 1,
-  });
+  app_replace_button_symbol_style_inner(b);
   html_display_inline_block(b);
 }
