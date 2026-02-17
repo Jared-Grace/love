c09c68254c03074383181f2be74cef35698327e2
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:47:11 -0500

    function_auto_path ..\love\public\src\function_copy_suffix_remove.mjs

--- web/public/src/function_copy_suffix_remove.mjs
+++ web/public/src/function_copy_suffix_remove.mjs
@@ -1,7 +1,6 @@
 import { function_name_suffix_remove } from "../../../love/public/src/function_name_suffix_remove.mjs";
-import { function_rename_suffix_add_generic } from "../../../love/public/src/function_rename_suffix_add_generic.mjs";
 export async function function_copy_suffix_remove(f_name_before, suffix) {
-  let v = await function_rename_suffix_add_generic(
+  let v = await function_copy_suffix_add_generic(
     f_name_before,
     suffix,
     function_name_suffix_remove,
