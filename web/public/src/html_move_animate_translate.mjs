baf425c4d1c552e97ea872da6040532bf69c5106
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 22:53:58 -0500

    pf html_move_animate_translate component

--- web/public/src/html_move_animate_translate.mjs
+++ web/public/src/html_move_animate_translate.mjs
@@ -1,9 +1,9 @@
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 export async function html_move_animate_translate(
+  component,
   offsetX,
   offsetY,
-  component,
   duration,
 ) {
   let e = html_component_element_get(component);
