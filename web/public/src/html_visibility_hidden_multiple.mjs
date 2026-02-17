df658b3bbf51c8a030b4be3509203cf54ea54fe1
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:23:47 -0500

    fm html_visibility_hidden

--- web/public/src/html_visibility_hidden_multiple.mjs
+++ web/public/src/html_visibility_hidden_multiple.mjs
@@ -1,1 +1,5 @@
-export function html_visibility_hidden_multiple() {}
+import { html_visibility_hidden } from "../../../love/public/src/html_visibility_hidden.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+export function html_visibility_hidden_multiple(list) {
+  each(list, html_visibility_hidden);
+}
