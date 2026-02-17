5f23f5062c062565f1a6357e1971802477bf8bee
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:43:58 -0500

    rs app_replace_button_symbol_style_valid_multiple nested

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -1,4 +1,4 @@
-import { app_replace_button_symbol_style_valid_multiple } from "../../../love/public/src/app_replace_button_symbol_style_valid_multiple.mjs";
+import { app_replace_button_symbol_style_valid_multiple_nested } from "../../../love/public/src/app_replace_button_symbol_style_valid_multiple_nested.mjs";
 import { app_replace_button_symbol_style_invalid } from "../../../love/public/src/app_replace_button_symbol_style_invalid.mjs";
 import { html_style_padding_em } from "../../../love/public/src/html_style_padding_em.mjs";
 import { app_replace_button_screen } from "../../../love/public/src/app_replace_button_screen.mjs";
@@ -191,7 +191,7 @@ export async function app_replace_rule_set(context) {
         let eq = app_replace_rule_valid(rule2, index, current_list);
         if (eq) {
           symbols_invalid_chosen = {};
-          app_replace_button_symbol_style_valid_multiple(sbs, true);
+          app_replace_button_symbol_style_valid_multiple_nested(sbs, true);
           let right = property_get(rule2, "right");
           let left = property_get(rule2, "left");
           let before = list_take(current_list, index);
@@ -310,7 +310,7 @@ export async function app_replace_rule_set(context) {
           lambda5,
         );
         const list = [goal_list_symbols, sbs];
-        app_replace_button_symbol_style_valid_multiple(list);
+        app_replace_button_symbol_style_valid_multiple_nested(list);
         await html_move_animate_multiple(sbs, goal_list_symbols, duration);
         symbols_hide_on_success();
         let choices = [
