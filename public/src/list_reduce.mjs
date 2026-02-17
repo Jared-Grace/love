dcbe5609957d8fa72f431147d34d6d7f850d484e
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:39:52 -0500

    function_auto_path ..\love\public\src\list_reduce.mjs

--- public/src/list_reduce.mjs
+++ public/src/list_reduce.mjs
@@ -1,7 +1,7 @@
 import { list_reduce_index } from "../../../love/public/src/list_reduce_index.mjs";
-export function list_reduce(list, lambda$item$value, inital) {
-  function lambda(item, value, index) {
-    let v = lambda$item$value(item, value);
+export function list_reduce(list, lambda$value$item, inital) {
+  function lambda(value, item, index) {
+    let v = lambda$value$item(value, item);
     return v;
   }
   let value = list_reduce_index(list, lambda, inital);
