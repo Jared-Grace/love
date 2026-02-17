1717f9e8bbfb37b22f366557d3ba25670f0a8166
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:35:34 -0500

    function_auto_path ..\love\public\src\function_multiplize.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -1,4 +1,4 @@
-import { js_declaration_single_params_add } from "../../../love/public/src/js_declaration_single_params_add.mjs";
+import { js_declaration_single_param_add } from "../../../love/public/src/js_declaration_single_param_add.mjs";
 import { js_call } from "../../../love/public/src/js_call.mjs";
 import { js_declaration_single_block_body_add } from "../../../love/public/src/js_declaration_single_block_body_add.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -10,7 +10,7 @@ export async function function_multiplize(f_name) {
     const list = "list";
     let call = js_call(each.name, [list, f_name]);
     js_declaration_single_block_body_add(ast, call);
-    js_declaration_single_params_add(ast, [list]);
+    js_declaration_single_param_add(ast, list);
   }
   let output = await function_new_transform(combined, lambda);
 }
---
+++ web/public/src/js_declaration_single_param_add.mjs
@@ -0,0 +1,4 @@
+import { js_declaration_single_params_add } from "../../../love/public/src/js_declaration_single_params_add.mjs";
+export function js_declaration_single_param_add(ast, list) {
+  js_declaration_single_params_add(ast, [list]);
+}
