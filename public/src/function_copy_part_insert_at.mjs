2721c0dbd52e9be100c249b36ad5243c548a4518
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:18:45 -0500

    cr function_rename_part_insert_at rename copy

---
+++ public/src/function_copy_part_insert_at.mjs
@@ -0,0 +1,14 @@
+import { function_name_part_insert_at } from "../../../love/public/src/function_name_part_insert_at.mjs";
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
+export async function function_copy_part_insert_at(
+  f_name_before,
+  index_string,
+  part,
+) {
+  let f_name_after = function_name_part_insert_at(
+    f_name_before,
+    index_string,
+    part,
+  );
+  await function_rename(f_name_before, f_name_after);
+}
