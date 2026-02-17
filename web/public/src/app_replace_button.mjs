cad3ecf022e4f4a085d93d92006ccda91edf7f9e
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:20:28 -0500

    function_auto_path ..\love\public\src\app_replace_button.mjs

--- web/public/src/app_replace_button.mjs
+++ web/public/src/app_replace_button.mjs
@@ -1,7 +1,9 @@
+import { html_style_padding_em } from "../../../love/public/src/html_style_padding_em.mjs";
 import { app_replace_button_style } from "../../../love/public/src/app_replace_button_style.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_replace_button(parent, text, lambda) {
   let b = html_button(parent, text, lambda);
   app_replace_button_style(b);
+  html_style_padding_em(b, "0.1");
   return b;
 }
