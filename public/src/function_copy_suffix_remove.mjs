f2c1c65899c85e46f89c1502fe05c00d0d0a9cbc
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:48:08 -0500

    function_auto_path ..\love\public\src\function_copy_suffix_remove.mjs

--- public/src/function_copy_suffix_remove.mjs
+++ public/src/function_copy_suffix_remove.mjs
@@ -2,7 +2,7 @@ import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_name_suffix_remove } from "../../../love/public/src/function_name_suffix_remove.mjs";
 export async function function_copy_suffix_remove(f_name_before, suffix) {
-  let unaliased = await function_name_unalias_only(f_name_old);
+  let unaliased = await function_name_unalias_only(f_name_before);
   let combined = function_name_suffix_remove(unaliased, suffix);
   let v = await function_copy(unaliased, combined);
   return v;
