a8eff3d2ab2f4504ae607e80994e66601098345b
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:54:29 -0500

    function_auto_path ..\love\public\src\html_move_animate_rect.mjs

--- public/src/html_move_animate_rect.mjs
+++ public/src/html_move_animate_rect.mjs
@@ -1,6 +1,11 @@
 import { html_move_animate_translate } from "../../../love/public/src/html_move_animate_translate.mjs";
-export async function html_move_animate_rect(component, from, to, duration) {
-  const offsetX = from.left - to.left;
-  const offsetY = from.top - to.top;
+export async function html_move_animate_rect(
+  component,
+  from,
+  rect_from,
+  duration,
+) {
+  const offsetX = from.left - rect_from.left;
+  const offsetY = from.top - rect_from.top;
   await html_move_animate_translate(component, offsetX, offsetY, duration);
 }
