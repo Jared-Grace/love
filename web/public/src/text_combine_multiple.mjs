4eb5f06456b6ba548d9348998229162a68ddf51e
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:33:09 -0500

    function_auto_path ..\love\public\src\text_combine_multiple.mjs

--- web/public/src/text_combine_multiple.mjs
+++ web/public/src/text_combine_multiple.mjs
@@ -1,4 +1,5 @@
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export function text_combine_multiple(left, right) {
-  return text_combine(left, right);
+  let combined = text_combine(left, right);
+  return combined;
 }
