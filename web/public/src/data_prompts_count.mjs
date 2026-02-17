4dddbf0a416bc88b5d474746f888f33ea5ef94ad
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:42:33 -0500

    function_auto_path ..\love\public\src\data_prompts_count.mjs

--- web/public/src/data_prompts_count.mjs
+++ web/public/src/data_prompts_count.mjs
@@ -1,15 +1,15 @@
+import { prompt_previous_at } from "../../../love/public/src/prompt_previous_at.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { prompt_previous } from "../../../love/public/src/prompt_previous.mjs";
 import { list_map_index_countdown } from "../../../love/public/src/list_map_index_countdown.mjs";
 import { function_aliases_inverted } from "../../../love/public/src/function_aliases_inverted.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
 export async function data_prompts_count(offset) {
-  let fn = prompt_previous;
+  let fn = prompt_previous_at;
   let inverted = await function_aliases_inverted();
   let value = property_get(inverted, fn.name);
   log({
