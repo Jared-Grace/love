2433b32d4239b6a428c1358a4735c894aa6745cc
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:44:29 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid_if_multiple_nested.mjs

--- public/src/app_replace_button_symbol_style_valid_if_multiple_nested.mjs
+++ public/src/app_replace_button_symbol_style_valid_if_multiple_nested.mjs
@@ -1,9 +1,9 @@
 import { each_nested } from "../../../love/public/src/each_nested.mjs";
 import { app_replace_button_symbol_style_valid_if_curry_right } from "../../../love/public/src/app_replace_button_symbol_style_valid_if_curry_right.mjs";
 export function app_replace_button_symbol_style_valid_if_multiple_nested(
-  list,
+  lists,
   valid,
 ) {
   let lambda4 = app_replace_button_symbol_style_valid_if_curry_right(valid);
-  each_nested(list, lambda4);
+  each_nested(lists, lambda4);
 }
