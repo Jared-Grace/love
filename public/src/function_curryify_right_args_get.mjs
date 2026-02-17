37474eea878468fd3a5c7a48ba2bbd6abd91a470
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:54:08 -0500

    function_auto_path ..\love\public\src\function_curryify_right_args_get.mjs

--- public/src/function_curryify_right_args_get.mjs
+++ public/src/function_curryify_right_args_get.mjs
@@ -1,9 +1,8 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_last_remaining } from "../../../love/public/src/list_last_remaining.mjs";
+import { list_last_remaining_get } from "../../../love/public/src/list_last_remaining_get.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
 export function function_curryify_right_args_get(arg_names) {
-  let result = list_last_remaining(arg_names);
-  let fn_new_result_args = property_get(fr, "remaining");
+  let fn_new_result_args = list_last_remaining_get(arg_names);
+  fn_new_result_args;
   let r = function_curryify_args_get_generic(fn, arg_names, "last");
   return r;
 }
