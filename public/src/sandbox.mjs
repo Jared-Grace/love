c5e9dd32ca063e186cfe638e91e12d00bbcbc5ec
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:59:59 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,8 @@
+import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 export async function sandbox() {
-  let combined = text_combine_multiple(["a", "b", "c"]);
+  const a = ["a", "b", "c"];
+  let result = list_slice_end(list, offset);
+  let combined = text_combine_multiple(a);
   return combined;
 }
