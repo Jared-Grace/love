d807d9ec5c5bcd18befd20b3d4ff73dc42f9106f
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:27:39 -0500

    function_auto_path ..\love\public\src\function_multiplize.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -1,4 +1,9 @@
+import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
-export function function_multiplize(f_name) {
+export async function function_multiplize(f_name) {
   let combined = function_name_combine(f_name, "multiple");
+  let output = await function_new_transform(
+    f_name2,
+    async function lambda(ast) {},
+  );
 }
