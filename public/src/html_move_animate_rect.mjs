773f82a18f76cd06787bef2c8efb7225b283eb7b
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 22:53:58 -0500

    pf html_move_animate_translate component

--- public/src/html_move_animate_rect.mjs
+++ public/src/html_move_animate_rect.mjs
@@ -4,5 +4,5 @@ export async function html_move_animate_rect(component, from, to, duration) {
   html_style_set(component, "transition", `transform ${duration}ms`);
   const offsetX = from.left - to.left;
   const offsetY = from.top - to.top;
-  await html_move_animate_translate(offsetX, offsetY, component, duration);
+  await html_move_animate_translate(component, offsetX, offsetY, duration);
 }
