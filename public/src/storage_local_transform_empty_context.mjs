c2cac520a8f96a2277471ae791b0c3d33ad690b6
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 15:47:03 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

---
+++ public/src/storage_local_transform_empty_context.mjs
@@ -0,0 +1,4 @@
+import { storage_local_transform_context } from "../../../love/public/src/storage_local_transform_context.mjs";
+export function storage_local_transform_empty_context(key, lambda5) {
+  storage_local_transform_context(key, newLocal, {}, lambda5);
+}
