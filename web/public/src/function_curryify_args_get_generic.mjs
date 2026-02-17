e8702b1bf778dfb36e51b923d5a0ebafe80094bf
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:55:41 -0500

    function_auto_path ..\love\public\src\function_curryify_args_get_generic.mjs

--- web/public/src/function_curryify_args_get_generic.mjs
+++ web/public/src/function_curryify_args_get_generic.mjs
@@ -1,5 +1,7 @@
+import { list_slice_from } from "../../../love/public/src/list_slice_from.mjs";
 export function function_curryify_args_get_generic(fn, arg_names) {
   let fn_new_result_args = fn(arg_names);
+  let v2 = list_slice_from(list, item_from, item_to);
   let fn_new_args = [first];
   let r = {
     fn_new_result_args,
