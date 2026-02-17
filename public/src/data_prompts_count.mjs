c7c7104b80c2244691804f0255731c2b321f1d46
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:28:31 -0500

    function_auto_path ..\love\public\src\data_prompts_count.mjs

--- public/src/data_prompts_count.mjs
+++ public/src/data_prompts_count.mjs
@@ -1,3 +1,4 @@
+import { function_aliases_inverted } from "../../../love/public/src/function_aliases_inverted.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { subtract } from "../../../love/public/src/subtract.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
@@ -7,6 +8,7 @@ import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
 export async function data_prompts_count(offset) {
+  let inverted = await function_aliases_inverted();
   let d_path = data_prompts_path();
   let prompts = await data_value("prompts", d_path);
   let sliced = list_slice_end(prompts, offset);
