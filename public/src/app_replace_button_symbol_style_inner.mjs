218d14d62b2541f7987d39cd23617db8e0e18a7a
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 15:15:41 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_inner.mjs

--- public/src/app_replace_button_symbol_style_inner.mjs
+++ public/src/app_replace_button_symbol_style_inner.mjs
@@ -1,3 +1,4 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_padding_y } from "../../../love/public/src/html_style_padding_y.mjs";
 import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
@@ -9,4 +10,7 @@ export function app_replace_button_symbol_style_inner(b) {
   html_border_none(b);
   html_style_padding_x(b, "0.37em");
   html_style_padding_y(b, "0.1em");
+  html_style_assign(b, {
+    "line-height": 1,
+  });
 }
