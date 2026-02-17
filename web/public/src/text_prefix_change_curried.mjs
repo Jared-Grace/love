a8d3ae6e77374d3a9398ca266ff52a7a5589081c
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:46:12 -0500

    function_auto_path ..\love\public\src\text_prefix_change_curried.mjs

--- web/public/src/text_prefix_change_curried.mjs
+++ web/public/src/text_prefix_change_curried.mjs
@@ -1,6 +1,8 @@
 import { text_prefix_change } from "../../../love/public/src/text_prefix_change.mjs";
 export function text_prefix_change_curried(s) {
-  return function text_prefix_change_curried_result(before, after) {
-    return text_prefix_change(s, before, after);
+  let r = function text_prefix_change_curried_result(before, after) {
+    let together = text_prefix_change(s, before, after);
+    return together;
   };
+  return r;
 }
