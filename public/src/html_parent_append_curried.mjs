a0283f8582bb1521821f59ac6bddb664c50ab12b
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 11:33:07 -0500

    cu html_parent_append

--- public/src/html_parent_append_curried.mjs
+++ public/src/html_parent_append_curried.mjs
@@ -1,1 +1,6 @@
-export function html_parent_append_curried() {}
+import { html_parent_append } from "../../../love/public/src/html_parent_append.mjs";
+export function html_parent_append_curried(parent) {
+  return function html_parent_append_curried_result(e) {
+    return html_parent_append(parent, e);
+  };
+}
