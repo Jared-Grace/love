c479dbacd5e3a4bd550b136037fc271bb577061f
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:55:46 -0500

    function_auto_path ..\love\public\src\function_curryify_args_get_generic.mjs

--- public/src/function_curryify_args_get_generic.mjs
+++ public/src/function_curryify_args_get_generic.mjs
@@ -1,7 +1,7 @@
-import { list_slice_from } from "../../../love/public/src/list_slice_from.mjs";
+import { list_difference } from "../../../love/public/src/list_difference.mjs";
 export function function_curryify_args_get_generic(fn, arg_names) {
   let fn_new_result_args = fn(arg_names);
-  let v2 = list_slice_from(list, item_from, item_to);
+  let difference = list_difference(list, other);
   let fn_new_args = [first];
   let r = {
     fn_new_result_args,
