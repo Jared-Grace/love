7178c68e7ad862e82f2206f96c4890a88339dd43
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 11:38:46 -0500

    cur html_move_animate_multiple_parent_remove

---
+++ web/public/src/html_move_animate_multiple_parent_remove_curried_right.mjs
@@ -0,0 +1,11 @@
+import { html_move_animate_multiple_parent_remove } from "../../../love/public/src/html_move_animate_multiple_parent_remove.mjs";
+export async function html_move_animate_multiple_parent_remove_curried_right(
+  tos,
+  duration,
+) {
+  return async function html_move_animate_multiple_parent_remove_curried_right_result(
+    froms,
+  ) {
+    return await html_move_animate_multiple_parent_remove(froms, tos, duration);
+  };
+}
