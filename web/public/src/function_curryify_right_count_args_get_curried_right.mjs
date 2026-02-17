5705434d3e0f908ba11ba2f13161d8ee5d018907
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 17:02:12 -0500

    function_auto_path ..\love\public\src\function_curryify_right_count_args_get_curried_right.mjs

--- web/public/src/function_curryify_right_count_args_get_curried_right.mjs
+++ web/public/src/function_curryify_right_count_args_get_curried_right.mjs
@@ -1,8 +1,10 @@
 import { function_curryify_right_count_args_get } from "../../../love/public/src/function_curryify_right_count_args_get.mjs";
 export function function_curryify_right_count_args_get_curried_right(count) {
-  return function function_curryify_right_count_args_get_curried_right_result(
+  let r2 = function function_curryify_right_count_args_get_curried_right_result(
     arg_names,
   ) {
-    return function_curryify_right_count_args_get(arg_names, count);
+    let r = function_curryify_right_count_args_get(arg_names, count);
+    return r;
   };
+  return r2;
 }
