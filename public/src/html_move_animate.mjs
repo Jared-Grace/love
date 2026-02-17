c45e1669a847babe214329685659859bbad863e6
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:05:46 -0500

    function_auto_path ..\love\public\src\html_move_animate.mjs

--- public/src/html_move_animate.mjs
+++ public/src/html_move_animate.mjs
@@ -6,10 +6,10 @@ export async function html_move_animate(
   component_to,
   duration,
 ) {
-  let to_e = html_component_element_get(component_to);
   const targetRect = html_bounding_client_rect(component_to);
-  let from_e = html_component_element_get(component_from);
   const movingRect = html_bounding_client_rect(component_from);
+  let to_e = html_component_element_get(component_to);
+  let from_e = html_component_element_get(component_from);
   const offsetX = targetRect.left - movingRect.left;
   const offsetY = targetRect.top - movingRect.top;
   from_e.style.transition = `transform ${duration}ms`;
