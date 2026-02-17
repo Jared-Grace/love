48db1a9863a5a90b86f62fb539759cf2cbc5783d
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 17:02:10 -0500

    cur function_curryify_right_count_args_get

--- web/public/src/function_curryify_right_count_args_get_curried_right.mjs
+++ web/public/src/function_curryify_right_count_args_get_curried_right.mjs
@@ -1,1 +1,8 @@
-export function function_curryify_right_count_args_get_curried_right() {}
+import { function_curryify_right_count_args_get } from "../../../love/public/src/function_curryify_right_count_args_get.mjs";
+export function function_curryify_right_count_args_get_curried_right(count) {
+  return function function_curryify_right_count_args_get_curried_right_result(
+    arg_names,
+  ) {
+    return function_curryify_right_count_args_get(arg_names, count);
+  };
+}
