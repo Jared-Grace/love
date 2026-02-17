5b540a31d82076688c7118c4d681656ce388e9c2
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:32:15 -0500

    function_auto_path ..\love\public\src\js_call_insert.mjs

--- public/src/js_call_insert.mjs
+++ public/src/js_call_insert.mjs
@@ -1,8 +1,6 @@
-import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
+import { js_call } from "../../../love/public/src/js_call.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
-import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 export function js_call_insert(f_name, args_code, list, index) {
-  let code = js_code_call_args(f_name, args_code);
-  let parsed = js_parse_statement(code);
+  let parsed = js_call(f_name, args_code);
   list_insert(list, index, parsed);
 }
