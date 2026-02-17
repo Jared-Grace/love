8b28fc3e3f873230b2044f0bbed24604b391dd85
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:24:39 -0500

    function_auto_path ..\love\public\src\list_slice_end.mjs

--- public/src/list_slice_end.mjs
+++ public/src/list_slice_end.mjs
@@ -1,8 +1,10 @@
+import { negative_not_is } from "../../../love/public/src/negative_not_is.mjs";
 import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_slice_end(list, count) {
   let end = list_size(list);
   let start = end - count;
+  let nn = negative_not_is(i);
   if (start < 0) {
     start = 0;
   }
