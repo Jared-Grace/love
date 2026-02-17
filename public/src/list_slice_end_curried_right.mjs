b6abccbb678b66e258119972c492bd6863aa7931
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 17:00:24 -0500

    function_auto_path ..\love\public\src\list_slice_end_curried_right.mjs

--- public/src/list_slice_end_curried_right.mjs
+++ public/src/list_slice_end_curried_right.mjs
@@ -1,6 +1,8 @@
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 export function list_slice_end_curried_right(offset) {
-  return function list_slice_end_curried_right_result(list) {
-    return list_slice_end(list, offset);
+  let r = function list_slice_end_curried_right_result(list) {
+    let result = list_slice_end(list, offset);
+    return result;
   };
+  return r;
 }
