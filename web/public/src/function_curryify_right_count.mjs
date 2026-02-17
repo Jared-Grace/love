ad6a010d315992b6118593fcde33e75f7936b909
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:49:49 -0500

    function_auto_path ..\love\public\src\function_curryify_right_count.mjs

--- web/public/src/function_curryify_right_count.mjs
+++ web/public/src/function_curryify_right_count.mjs
@@ -1,9 +1,10 @@
 import { function_curryify_right_args_get } from "../../../love/public/src/function_curryify_right_args_get.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
 export async function function_curryify_right_count(f_name) {
+  function lambda() {}
   let output = await function_curryify_generic(
     f_name,
-    () => {},
+    lambda,
     function_curryify_right_args_get,
   );
   return output;
