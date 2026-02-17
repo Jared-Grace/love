ae909f8fa8a5252ed9bdf5dc162cfa8345c6ab36
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 12:59:15 -0500

    cpi function_curryify_args_get args right

---
+++ public/src/function_curryify_right_args_get.mjs
@@ -0,0 +1,7 @@
+import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
+import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
+export function function_curryify_right_args_get(arg_names) {
+  let fn = list_first_remaining;
+  let r = function_curryify_args_get_generic(fn, arg_names);
+  return r;
+}
