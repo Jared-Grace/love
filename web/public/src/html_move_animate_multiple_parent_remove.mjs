c971178a7f76c55abf2d85d55ddadb611d00fbb9
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 11:37:31 -0500

    function_auto_path ..\love\public\src\html_move_animate_multiple_parent_remove.mjs

--- web/public/src/html_move_animate_multiple_parent_remove.mjs
+++ web/public/src/html_move_animate_multiple_parent_remove.mjs
@@ -10,7 +10,6 @@ export async function html_move_animate_multiple_parent_remove(
 ) {
   async function lambda6(a, b) {
     await html_move_animate(a, b, duration);
-    html_parent_remove(a);
   }
   let mapped = list_map_pairs(froms, tos, lambda6);
   let v = await list_wait(mapped);
