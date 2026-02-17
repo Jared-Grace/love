9dafe2705e3be4507409686a8be4d38874b54146
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:38:37 -0500

    function_auto_path ..\love\public\src\function_multiplize.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -1,3 +1,4 @@
+import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 import { js_declaration_single_param_add } from "../../../love/public/src/js_declaration_single_param_add.mjs";
 import { js_call } from "../../../love/public/src/js_call.mjs";
 import { js_declaration_single_block_body_add } from "../../../love/public/src/js_declaration_single_block_body_add.mjs";
@@ -11,6 +12,7 @@ export async function function_multiplize(f_name) {
     let call = js_call(each.name, [list, f_name]);
     js_declaration_single_block_body_add(ast, call);
     js_declaration_single_param_add(ast, list);
+    await js_imports_missing_add(ast2);
   }
   let output = await function_new_transform(combined, lambda);
 }
