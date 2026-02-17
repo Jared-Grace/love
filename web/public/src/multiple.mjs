a714b972bc33004007e5f867cfe1b65b89f86768
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:32:30 -0500

    function_auto_path ..\love\public\src\multiple.mjs

--- web/public/src/multiple.mjs
+++ web/public/src/multiple.mjs
@@ -1,1 +1,5 @@
-export function multiple() {}
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
+export function multiple(left, right) {
+  let combined = text_combine(left, right);
+  return combined;
+}
