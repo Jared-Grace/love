063a0c16140cfbb566d7aa724f2bce0378b135b4
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:32:44 -0500

    function_auto_path ..\love\public\src\multiple.mjs

--- public/src/multiple.mjs
+++ public/src/multiple.mjs
@@ -2,6 +2,7 @@ import { list_reduce } from "../../../love/public/src/list_reduce.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export function multiple(left, right) {
   let combined = text_combine(left, right);
-  let value2 = list_reduce(list, function lambda(item, value) {}, inital);
+  function lambda(item, value) {}
+  let value2 = list_reduce(list, lambda, inital);
   return combined;
 }
