ecb52cb59075beda71d663e009ffa6b5db4845d6
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:40:17 -0500

    de sandbox_curried_right_2

--- public/src/sandbox_curried_right_2.mjs
+++
@@ -1,8 +0,0 @@
-import { sandbox } from "../../../love/public/src/sandbox.mjs";
-export async function sandbox_curried_right_2(b, c) {
-  let r2 = async function sandbox_curried_right_2_result(d) {
-    let r = await sandbox(d, b, c);
-    return r;
-  };
-  return r2;
-}
