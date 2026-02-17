28c5ba41b1c178575aa0ff8823af9313fcd9cd0c
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:49:58 -0500

    function_auto_path ..\love\public\src\function_curryify_right_count.mjs

--- public/src/function_curryify_right_count.mjs
+++ public/src/function_curryify_right_count.mjs
@@ -1,7 +1,10 @@
+import { function_curryify_right_name } from "../../../love/public/src/function_curryify_right_name.mjs";
 import { function_curryify_right_args_get } from "../../../love/public/src/function_curryify_right_args_get.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
 export async function function_curryify_right_count(f_name) {
-  function lambda() {}
+  function lambda() {
+    let combined = function_curryify_right_name(unaliased);
+  }
   let output = await function_curryify_generic(
     f_name,
     lambda,
