90b45d9ab0c0c58a827e4a31770435000488d58e
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:30:12 -0500

    function_auto_path ..\love\public\src\list_map_index_countdown.mjs

--- public/src/list_map_index_countdown.mjs
+++ public/src/list_map_index_countdown.mjs
@@ -3,9 +3,9 @@ import { subtract } from "../../../love/public/src/subtract.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_map_index_countdown(sliced, lambda_inner) {
   let size = list_size(sliced);
-  function lambda(item, index) {
-    let difference = subtract(size, index);
-    let r2 = lambda_inner(item, difference);
+  function lambda(item, index_forwards) {
+    let index = subtract(size, index_forwards);
+    let r2 = lambda_inner(item, index);
     return r2;
   }
   let mapped = list_map_index(sliced, lambda);
