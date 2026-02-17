6f25a0eeb4ccc5c1057913ebe818cf69db1789ad
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 22:53:03 -0500

    function_auto_path ..\love\public\src\html_move_animate_translate.mjs

--- public/src/html_move_animate_translate.mjs
+++ public/src/html_move_animate_translate.mjs
@@ -1,10 +1,12 @@
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 export async function html_move_animate_translate(
   offsetX,
   offsetY,
-  e,
+  component,
   duration,
 ) {
+  let e = html_component_element_get(component);
   const u = `translate(${offsetX}px, ${offsetY}px)`;
   e.style.transform = u;
   await sleep(duration);
