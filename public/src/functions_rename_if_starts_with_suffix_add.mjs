b4db3873148269e9f250f8b1fd257b9d1686fce5
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:37:24 -0500

    function_auto_path ..\love\public\src\functions_rename_if_starts_with_suffix_add.mjs

--- public/src/functions_rename_if_starts_with_suffix_add.mjs
+++ public/src/functions_rename_if_starts_with_suffix_add.mjs
@@ -2,21 +2,15 @@ import { text_prefix_change_curried_right_2 } from "../../../love/public/src/tex
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { functions_rename_generic_starts_with } from "../../../love/public/src/functions_rename_generic_starts_with.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-import { text_prefix_change } from "../../../love/public/src/text_prefix_change.mjs";
 export async function functions_rename_if_starts_with_suffix_add(
   f_name_prefix_before,
   suffix,
 ) {
   assert_arguments(arguments, 2);
   let f_name_prefix_after = function_name_combine(f_name_prefix_before, suffix);
-  let r = text_prefix_change_curried_right_2(before, after);
+  let name_change = text_prefix_change_curried_right_2(
+    f_name_prefix_before,
+    f_name_prefix_after,
+  );
   await functions_rename_generic_starts_with(name_change, f_name_prefix_before);
-  function name_change(f_name_before) {
-    let together = text_prefix_change(
-      f_name_before,
-      f_name_prefix_before,
-      f_name_prefix_after,
-    );
-    return together;
-  }
 }
