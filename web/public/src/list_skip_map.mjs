0bc97b9f637cc31db04a5856b299718b7e8f09c3
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:22:51 -0500

    function_auto_path ..\love\public\src\list_skip_map.mjs

--- web/public/src/list_skip_map.mjs
+++ web/public/src/list_skip_map.mjs
@@ -1,8 +1,7 @@
-import { html_bounding_client_rect } from "../../../love/public/src/html_bounding_client_rect.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_skip } from "../../../love/public/src/list_skip.mjs";
 export function list_skip_map(sbs, sum, mapper) {
   let skipped = list_skip(sbs, sum);
-  let mapped2 = list_map(skipped, html_bounding_client_rect);
+  let mapped2 = list_map(skipped, mapper);
   return mapped2;
 }
