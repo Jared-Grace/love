dfb4027dcc7d78cf75c24d1a267f92a001384045
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 21:03:05 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -218,7 +218,6 @@ export async function app_replace_rule_set(context) {
             html_style_set(el, "transition", "transform 0ms");
             html_style_set(el, "transform", `translate(${dx}px, ${dy}px)`);
             el.offsetWidth;
-            html_style_set(el, "transition", `transform ${duration}ms`);
             log({
               duration,
             });
