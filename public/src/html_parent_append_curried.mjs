78e1481d3cd350095afe988916e4ba6078c8a7ed
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 11:33:09 -0500

    function_auto_path ..\love\public\src\html_parent_append_curried.mjs

--- public/src/html_parent_append_curried.mjs
+++ public/src/html_parent_append_curried.mjs
@@ -1,6 +1,8 @@
 import { html_parent_append } from "../../../love/public/src/html_parent_append.mjs";
 export function html_parent_append_curried(parent) {
-  return function html_parent_append_curried_result(e) {
-    return html_parent_append(parent, e);
+  let r2 = function html_parent_append_curried_result(e) {
+    let r = html_parent_append(parent, e);
+    return r;
   };
+  return r2;
 }
