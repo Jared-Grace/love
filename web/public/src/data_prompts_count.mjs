da5a82aab456e78605c8ceeb86d02d21acce8ede
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:29:49 -0500

    function_auto_path ..\love\public\src\data_prompts_count.mjs

--- web/public/src/data_prompts_count.mjs
+++ web/public/src/data_prompts_count.mjs
@@ -1,9 +1,7 @@
+import { list_map_index_countdown } from "../../../love/public/src/list_map_index_countdown.mjs";
 import { function_aliases_inverted } from "../../../love/public/src/function_aliases_inverted.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
-import { subtract } from "../../../love/public/src/subtract.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
 import { text_between_space } from "../../../love/public/src/text_between_space.mjs";
-import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
@@ -12,13 +10,7 @@ export async function data_prompts_count(offset) {
   let d_path = data_prompts_path();
   let prompts = await data_value("prompts", d_path);
   let sliced = list_slice_end(prompts, offset);
-  let size = list_size(sliced);
-  function lambda(item, index) {
-    let difference = subtract(size, index);
-    let r2 = lambda_inner(item, difference);
-    return r2;
-  }
-  let mapped = list_map_index(sliced, lambda);
+  let mapped = list_map_index_countdown(sliced, lambda_inner);
   return mapped;
   function lambda_inner(item, difference) {
     let together = text_combine(difference, " " + " ");
