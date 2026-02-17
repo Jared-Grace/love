d003449d85ad9693c5947453e8c2d48bc8cc7f90
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:18:50 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style.mjs

--- public/src/app_replace_button_symbol_style.mjs
+++ public/src/app_replace_button_symbol_style.mjs
@@ -1,10 +1,10 @@
-import { html_p } from "../../../love/public/src/html_p.mjs";
+import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { app_replace_button_symbol_style_inner } from "../../../love/public/src/app_replace_button_symbol_style_inner.mjs";
 import { html_display_inline_block } from "../../../love/public/src/html_display_inline_block.mjs";
 export function app_replace_button_symbol_style(b) {
   app_replace_button_symbol_style_inner(b);
-  let p = html_p(root);
+  html_style_padding(component, value);
   html_display_inline_block(b);
   html_style_assign(b, {
     "line-height": 1,
