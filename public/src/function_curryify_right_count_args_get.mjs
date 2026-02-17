1985dee54446d32c1d5f09750661315e17f1c8e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:59:13 -0500

    pne function_curryify_right_count_args_get count

--- public/src/function_curryify_right_count_args_get.mjs
+++ public/src/function_curryify_right_count_args_get.mjs
@@ -1,6 +1,6 @@
 import { list_last_remaining_get } from "../../../love/public/src/list_last_remaining_get.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
-export function function_curryify_right_count_args_get(arg_names) {
+export function function_curryify_right_count_args_get(arg_names, count) {
   let r = function_curryify_args_get_generic(
     list_last_remaining_get,
     arg_names,
