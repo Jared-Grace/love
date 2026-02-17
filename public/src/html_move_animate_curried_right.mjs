d222ddaf5755d51c4bd268261c413745e1374002
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:55:07 -0500

    pp 3

--- public/src/html_move_animate_curried_right.mjs
+++ public/src/html_move_animate_curried_right.mjs
@@ -1,1 +1,9 @@
-export function html_move_animate_curried_right() {}
+import { html_move_animate } from "../../../love/public/src/html_move_animate.mjs";
+export async function html_move_animate_curried_right(duration) {
+  return async function html_move_animate_curried_right_result(
+    component_from,
+    component_to,
+  ) {
+    return await html_move_animate(component_from, component_to, duration);
+  };
+}
