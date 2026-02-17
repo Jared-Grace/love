ca564711481643e91e7905f65da7ef62e90dea21
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:18:55 -0500

    function_auto_path ..\love\public\src\function_copy_part_insert_at.mjs

--- public/src/function_copy_part_insert_at.mjs
+++ public/src/function_copy_part_insert_at.mjs
@@ -1,5 +1,5 @@
 import { function_name_part_insert_at } from "../../../love/public/src/function_name_part_insert_at.mjs";
-import { function_rename } from "../../../love/public/src/function_rename.mjs";
+import { function_copy } from "./function_copy.mjs";
 export async function function_copy_part_insert_at(
   f_name_before,
   index_string,
@@ -10,5 +10,5 @@ export async function function_copy_part_insert_at(
     index_string,
     part,
   );
-  await function_rename(f_name_before, f_name_after);
+  await function_copy(f_name_before, f_name_after);
 }
