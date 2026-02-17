00790db81e701e5e0ec819855bb0aa8329411cd2
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 22:38:14 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -224,7 +224,6 @@ export async function app_replace_rule_set(context) {
               duration,
             });
             await sleep(duration);
-            return;
             html_style_set(el, "transform", `translate(0px, 0px)`);
             html_style_set(el, "transition", "");
             html_style_set(el, "transform", "");
