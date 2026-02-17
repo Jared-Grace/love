3a01b2a278a93135473994cd6df5c5b49eb21a38
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:15:43 -0500

    function_auto_path ..\love\public\src\html_move_animate.mjs

--- public/src/html_move_animate.mjs
+++ public/src/html_move_animate.mjs
@@ -1,6 +1,5 @@
+import { html_move_animate_rect } from "../../../love/public/src/html_move_animate_rect.mjs";
 import { html_bounding_client_rect } from "../../../love/public/src/html_bounding_client_rect.mjs";
-import { sleep } from "../../../love/public/src/sleep.mjs";
-import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export async function html_move_animate(
   component_from,
   component_to,
@@ -8,12 +7,10 @@ export async function html_move_animate(
 ) {
   const movingRect = html_bounding_client_rect(component_from);
   const targetRect = html_bounding_client_rect(component_to);
-  let from_e = html_component_element_get(component_from);
-  const offsetX = targetRect.left - movingRect.left;
-  const offsetY = targetRect.top - movingRect.top;
-  from_e.style.transition = `transform ${duration}ms`;
-  from_e.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-  await sleep(duration);
-  from_e.style.transition = "";
-  from_e.style.transform = "";
+  await html_move_animate_rect(
+    component_from,
+    targetRect,
+    movingRect,
+    duration,
+  );
 }
