57e1c1734c5df5fc00d4fbf7ca1ea51e720288b4
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:26:15 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/html_display_none_multiple.mjs
+++ public/src/html_display_none_multiple.mjs
@@ -1,5 +1,5 @@
 import { html_display_none } from "../../../love/public/src/html_display_none.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function html_display_none_multiple(rights_cloned2) {
-  each(rights_cloned2, html_display_none);
+export function html_display_none_multiple(list) {
+  each(list, html_display_none);
 }
