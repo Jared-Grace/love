ae9262d29cc07048bd2a18ca240e8e465ce0221b
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:30:22 -0500

    function_auto_path ..\love\public\src\data_prompts_count.mjs

--- public/src/data_prompts_count.mjs
+++ public/src/data_prompts_count.mjs
@@ -10,8 +10,8 @@ export async function data_prompts_count(offset) {
   let d_path = data_prompts_path();
   let prompts = await data_value("prompts", d_path);
   let sliced = list_slice_end(prompts, offset);
-  function lambda_inner(item, difference) {
-    let together = text_combine(difference, " " + " ");
+  function lambda_inner(item, index) {
+    let together = text_combine(index, " " + " ");
     let r = text_between_space(together, item);
     return r;
   }
