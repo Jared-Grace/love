d6ad3348abd18d7b6ee71b50d278b309162b318f
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 20:13:41 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -204,6 +204,7 @@ export async function app_replace_rule_set(context) {
           let mapped = list_map_pairs(sliced2, lefts2, lambda6);
           let v = await list_wait(mapped);
           await html_request_animation_frame();
+          let rects_middle = list_map(skipped, html_bounding_client_rect);
           let rights_cloned = list_map(rights2, html_clone);
           function lambda8(item, index5) {
             html_visibility_hidden(item);
