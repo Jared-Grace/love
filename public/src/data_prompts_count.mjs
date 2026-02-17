4f6c59c65cda7b95d1af14d51ac5ce2bda69d47b
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:16:35 -0500

    function_auto_path ..\love\public\src\data_prompts_count.mjs

--- public/src/data_prompts_count.mjs
+++ public/src/data_prompts_count.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
@@ -5,5 +6,6 @@ export async function data_prompts_count(offset) {
   let d_path = data_prompts_path();
   let prompts = await data_value("prompts", d_path);
   let result = list_slice_end(prompts, offset);
+  let mapped = list_map(list, function lambda(item) {});
   return result;
 }
