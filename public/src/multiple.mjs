64b2c9b5b77b40beaca2f81d28ab1cf3b1c669dc
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:33:12 -0500

    de multiple

--- public/src/multiple.mjs
+++
@@ -1,5 +0,0 @@
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
-export function multiple(left, right) {
-  let combined = text_combine(left, right);
-  return combined;
-}
