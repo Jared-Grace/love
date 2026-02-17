788ee6697ea5b0cebb370b96efd0e26436c2a772
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:09:14 -0500

    function_auto_path ..\love\public\src\app_replace_button_screen.mjs

--- web/public/src/app_replace_button_screen.mjs
+++ web/public/src/app_replace_button_screen.mjs
@@ -1,8 +1,8 @@
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
-export function app_replace_button_screen(context, screen_fn, root, text) {
+export function app_replace_button_screen(context, screen_fn, parent, text) {
   function lambda2() {
     app_shared_screen_set(context, screen_fn);
   }
-  let b2 = app_replace_button(root, text, lambda2);
+  let b2 = app_replace_button(parent, text, lambda2);
 }
