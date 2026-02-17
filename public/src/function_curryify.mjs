b50ccc7f470c7e1be104f547b5049f081a09828c
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 12:55:19 -0500

    function_auto_path ..\love\public\src\function_curryify.mjs

--- public/src/function_curryify.mjs
+++ public/src/function_curryify.mjs
@@ -1,7 +1,6 @@
+import { function_curryify_args_get } from "../../../love/public/src/function_curryify_args_get.mjs";
 import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
-import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function function_curryify(f_name) {
   let output = await function_curryify_generic(
     f_name,
@@ -10,14 +9,7 @@ export async function function_curryify(f_name) {
   );
   return output;
   function args_get(arg_names) {
-    let fr = list_first_remaining(arg_names);
-    let first = property_get(fr, "first");
-    let fn_new_result_args = property_get(fr, "remaining");
-    let fn_new_args = [first];
-    let r = {
-      fn_new_result_args,
-      fn_new_args,
-    };
-    return r;
+    let r2 = function_curryify_args_get(arg_names);
+    return r2;
   }
 }
---
+++ public/src/function_curryify_args_get.mjs
@@ -0,0 +1,13 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
+export function function_curryify_args_get(arg_names) {
+  let fr = list_first_remaining(arg_names);
+  let first = property_get(fr, "first");
+  let fn_new_result_args = property_get(fr, "remaining");
+  let fn_new_args = [first];
+  let r = {
+    fn_new_result_args,
+    fn_new_args,
+  };
+  return r;
+}
