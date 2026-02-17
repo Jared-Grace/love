9710b13635126bf0d7e9db00f784f9e5a5d0a2a5
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 20:32:35 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/html_move_animate_rect.mjs
+++ public/src/html_move_animate_rect.mjs
@@ -3,7 +3,7 @@ import { sleep } from "../../../love/public/src/sleep.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export async function html_move_animate_rect(component, from, to, duration) {
   let e = html_component_element_get(component);
-  log({});
+  log({from, to});
   const offsetX = from.left - to.left;
   const offsetY = from.top - to.top;
   e.style.transition = `transform ${duration}ms`;
