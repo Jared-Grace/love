1bf07f4c2be7015732352efcf9fb3f31b4f15d63
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:56:24 -0500

    function_auto_path ..\love\public\src\list_first_remaining_get.mjs

--- web/public/src/list_first_remaining_get.mjs
+++ web/public/src/list_first_remaining_get.mjs
@@ -1,4 +1,5 @@
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 export function list_first_remaining_get(list) {
-  return list_first_remaining(list);
+  let r = list_first_remaining(list);
+  return r;
 }
