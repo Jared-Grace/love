fe41ef07e1de327b4b42f52e8fcf81b7d89a74f3
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 12:58:38 -0500

    function_auto_path ..\love\public\src\function_copy_part_insert.mjs

--- public/src/function_copy_part_insert.mjs
+++ public/src/function_copy_part_insert.mjs
@@ -1,11 +1,11 @@
+import { function_copy_part_insert_at } from "../../../love/public/src/function_copy_part_insert_at.mjs";
 import { function_name_part_index } from "../../../love/public/src/function_name_part_index.mjs";
-import { function_rename_part_insert_at } from "../../../love/public/src/function_rename_part_insert_at.mjs";
 export async function function_copy_part_insert(
   f_name_before,
   part_insert,
   part_new,
 ) {
   let index = function_name_part_index(f_name_before, part_insert);
-  let v = await function_rename_part_insert_at(f_name_before, index, part_new);
+  let v = await function_copy_part_insert_at(f_name_before, index, part_new);
   return v;
 }
