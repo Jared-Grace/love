cdaf7bd54700fea2d7e0a9c1ff8058ad88a188ea
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:55:09 -0500

    function_auto_path ..\love\public\src\html_move_animate_curried_right.mjs

--- public/src/html_move_animate_curried_right.mjs
+++ public/src/html_move_animate_curried_right.mjs
@@ -1,9 +1,11 @@
 import { html_move_animate } from "../../../love/public/src/html_move_animate.mjs";
 export async function html_move_animate_curried_right(duration) {
-  return async function html_move_animate_curried_right_result(
+  let r2 = async function html_move_animate_curried_right_result(
     component_from,
     component_to,
   ) {
-    return await html_move_animate(component_from, component_to, duration);
+    let r = await html_move_animate(component_from, component_to, duration);
+    return r;
   };
+  return r2;
 }
