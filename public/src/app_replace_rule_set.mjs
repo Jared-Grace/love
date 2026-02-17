5eb69314c80afe6ac1f5ea6ec822b9710c0434bc
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 20:17:31 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -218,7 +218,7 @@ export async function app_replace_rule_set(context) {
             const dy = rect_after.top - rect_before.top;
             html_style_set(c, "transform", `translate(${dx}px, ${dy}px)`);
             c.offsetWidth;
-            await html_move_animate_rect(c, rects_middle, rect_after, duration);
+            await html_move_animate_rect(c, rect_after, rects_middle, duration);
           }
           await each_multiple_async(
             [skipped, rects_middle, rects_after],
