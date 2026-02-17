eb6e620dfbf9ce8be32226013b094360d6a5983b
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:52:46 -0500

    p

---
+++ public/src/html_translation_transition_clear_multiple.mjs
@@ -0,0 +1,5 @@
+import { html_translation_transition_clear } from "../../../love/public/src/html_translation_transition_clear.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+export function html_translation_transition_clear_multiple(list) {
+  each(list, html_translation_transition_clear);
+}
