d6715aca9d7bed205c10abaaf608e94ef820a8f5
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:34:24 -0500

    function_auto_path ..\love\public\src\js_declaration_single_params_add.mjs

--- public/src/js_declaration_single_params_add.mjs
+++ public/src/js_declaration_single_params_add.mjs
@@ -1,11 +1,7 @@
-import { js_declaration_param_add } from "../../../love/public/src/js_declaration_param_add.mjs";
-import { each } from "../../../love/public/src/each.mjs";
+import { js_declaration_params_add } from "../../../love/public/src/js_declaration_params_add.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export function js_declaration_single_params_add(ast, param_names) {
   let declaration = js_declaration_single(ast);
-  function lambda(param_name) {
-    let v = js_declaration_param_add(declaration, param_name);
-    return v;
-  }
-  each(param_names, lambda);
+  js_declaration_params_add(declaration, fn_new_args);
+  return declaration;
 }
