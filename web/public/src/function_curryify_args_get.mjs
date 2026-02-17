5c909c589a502315c40f126906b166f42fac1371
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 12:55:49 -0500

    function_auto_path ..\love\public\src\function_curryify_args_get.mjs

--- web/public/src/function_curryify_args_get.mjs
+++ web/public/src/function_curryify_args_get.mjs
@@ -2,7 +2,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 export function function_curryify_args_get(arg_names) {
   let fn = list_first_remaining;
-  let fr = list_first_remaining(arg_names);
+  let fr = fn(arg_names);
   let first = property_get(fr, "first");
   let fn_new_result_args = property_get(fr, "remaining");
   let fn_new_args = [first];
