89f3efca524a09ce76c8285d650edef79033b54a
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:55:21 -0500

    function_auto_path ..\love\public\src\function_curryify_args_get_generic.mjs

--- web/public/src/function_curryify_args_get_generic.mjs
+++ web/public/src/function_curryify_args_get_generic.mjs
@@ -1,8 +1,5 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
 export function function_curryify_args_get_generic(fn, arg_names) {
-  let fr = fn(arg_names);
-  let first = property_get(fr, property_name);
-  let fn_new_result_args = property_get(fr, "remaining");
+  let fn_new_result_args = fn(arg_names);
   let fn_new_args = [first];
   let r = {
     fn_new_result_args,
