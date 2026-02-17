9a6abf7e6604073d70e2d14c11c624182e591ab4
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:56:01 -0500

    function_auto_path ..\love\public\src\function_curryify_args_get_generic.mjs

--- web/public/src/function_curryify_args_get_generic.mjs
+++ web/public/src/function_curryify_args_get_generic.mjs
@@ -1,8 +1,7 @@
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 export function function_curryify_args_get_generic(fn, arg_names) {
   let fn_new_result_args = fn(arg_names);
-  let difference = list_difference(list, other);
-  let fn_new_args = [first];
+  let fn_new_args = list_difference(arg_names, fn_new_result_args);
   let r = {
     fn_new_result_args,
     fn_new_args,
