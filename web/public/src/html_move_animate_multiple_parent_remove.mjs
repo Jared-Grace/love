ddbda7ddda32f2fab9d086ebb1253f4170d70e11
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:56:31 -0500

    function_auto_path ..\love\public\src\html_move_animate_multiple_parent_remove.mjs

--- web/public/src/html_move_animate_multiple_parent_remove.mjs
+++ web/public/src/html_move_animate_multiple_parent_remove.mjs
@@ -1,7 +1,6 @@
+import { list_map_pairs_wait } from "../../../love/public/src/list_map_pairs_wait.mjs";
 import { html_move_animate_curried_right } from "../../../love/public/src/html_move_animate_curried_right.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { list_wait } from "../../../love/public/src/list_wait.mjs";
-import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
 import { html_parent_remove } from "../../../love/public/src/html_parent_remove.mjs";
 export async function html_move_animate_multiple_parent_remove(
   froms,
@@ -9,7 +8,6 @@ export async function html_move_animate_multiple_parent_remove(
   duration,
 ) {
   let r2 = await html_move_animate_curried_right(duration);
-  let mapped = list_map_pairs(froms, tos, r2);
-  let v = await list_wait(mapped);
+  await list_map_pairs_wait(froms, tos, r2);
   each(froms, html_parent_remove);
 }
