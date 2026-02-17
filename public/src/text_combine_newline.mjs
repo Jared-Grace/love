7b7e9e9731d721f24011bc70ccda2ca81f2a50f8
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:24:02 -0500

    function_auto_path ..\love\public\src\text_combine_newline.mjs

--- public/src/text_combine_newline.mjs
+++ public/src/text_combine_newline.mjs
@@ -1,7 +1,7 @@
 import { text_combine_3 } from "../../../love/public/src/text_combine_3.mjs";
 import { newline } from "../../../love/public/src/newline.mjs";
-export function text_combine_newline(before, f_path) {
+export function text_combine_newline(a, b) {
   const combine = newline();
-  let r = text_combine_3(index, combine, item);
+  let r = text_combine_3(a, combine, b);
   return r;
 }
