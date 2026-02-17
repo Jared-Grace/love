138c41784b7c471b920811ee68e14753da69a1c5
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:25:42 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -233,7 +233,8 @@ export async function app_replace_rule_set(context) {
           html_display_none_multiple(rights_cloned2);
           html_parent_append_multiple(div_refresh, rights_cloned2);
           await html_move_animate_multiple(rights_cloned2, rights2, 0);
-          each(list, function lambda6(item2) {});
+          function lambda6(item2) {}
+          each(list, lambda6);
           index_selected = null;
         } else {
           property_set(symbols_invalid_chosen, index, true);
