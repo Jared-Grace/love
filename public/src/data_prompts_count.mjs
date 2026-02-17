f2414c21d1faf30b3b9e0a7677c3d7c82599a4ef
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:40:39 -0500

    function_auto_path ..\love\public\src\data_prompts_count.mjs

--- public/src/data_prompts_count.mjs
+++ public/src/data_prompts_count.mjs
@@ -5,7 +5,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { prompt_previous } from "../../../love/public/src/prompt_previous.mjs";
 import { list_map_index_countdown } from "../../../love/public/src/list_map_index_countdown.mjs";
 import { function_aliases_inverted } from "../../../love/public/src/function_aliases_inverted.mjs";
-import { text_between_space } from "../../../love/public/src/text_between_space.mjs";
 import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
@@ -21,8 +20,7 @@ export async function data_prompts_count(offset) {
   let prompts = await data_value("prompts", d_path);
   let sliced = list_slice_end(prompts, offset);
   function lambda_inner(item, index) {
-    let together = text_combine_multiple([first, " ", index, " : "]);
-    let r = text_between_space(together, item);
+    let together = text_combine_multiple([first, " ", index, " : ", item]);
     return r;
   }
   let mapped = list_map_index_countdown(sliced, lambda_inner);
