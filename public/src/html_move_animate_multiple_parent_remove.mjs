7ec7b0119b045f941784f5c8d35ed6203c2c9f74
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:58:33 -0500

    function_auto_path ..\love\public\src\html_move_animate_multiple_parent_remove.mjs

--- public/src/html_move_animate_multiple_parent_remove.mjs
+++ public/src/html_move_animate_multiple_parent_remove.mjs
@@ -1,12 +1,10 @@
+import { html_move_animate_multiple } from "../../../love/public/src/html_move_animate_multiple.mjs";
 import { html_parent_remove_multiple } from "../../../love/public/src/html_parent_remove_multiple.mjs";
-import { list_map_pairs_wait } from "../../../love/public/src/list_map_pairs_wait.mjs";
-import { html_move_animate_curried_right } from "../../../love/public/src/html_move_animate_curried_right.mjs";
 export async function html_move_animate_multiple_parent_remove(
   froms,
   tos,
   duration,
 ) {
-  let r2 = await html_move_animate_curried_right(duration);
-  await list_map_pairs_wait(froms, tos, r2);
+  await html_move_animate_multiple(duration, froms, tos);
   html_parent_remove_multiple(froms);
 }
