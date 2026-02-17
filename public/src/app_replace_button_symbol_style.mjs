906f1ef50e522714821c37acd8980332b51bbc71
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 15:15:55 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style.mjs

--- public/src/app_replace_button_symbol_style.mjs
+++ public/src/app_replace_button_symbol_style.mjs
@@ -1,9 +1,10 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { app_replace_button_symbol_style_inner } from "../../../love/public/src/app_replace_button_symbol_style_inner.mjs";
 import { html_display_inline_block } from "../../../love/public/src/html_display_inline_block.mjs";
 export function app_replace_button_symbol_style(b) {
   app_replace_button_symbol_style_inner(b);
   html_display_inline_block(b);
-    html_style_assign(b, {
-      "line-height": 1,
-    });
+  html_style_assign(b, {
+    "line-height": 1,
+  });
 }
