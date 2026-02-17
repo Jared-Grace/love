3dc3e02604217e26f26eba7963acaa214e8a2192
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:43:36 -0500

    function_auto_path ..\love\public\src\functions_rename_if_starts_with_suffix_add.mjs

--- web/public/src/functions_rename_if_starts_with_suffix_add.mjs
+++ web/public/src/functions_rename_if_starts_with_suffix_add.mjs
@@ -2,15 +2,15 @@ import { functions_rename_generic_starts_with } from "../../../love/public/src/f
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { text_prefix_change } from "../../../love/public/src/text_prefix_change.mjs";
 export async function functions_rename_if_starts_with_suffix_add(
-  f_name_prefix_before,
+  f_name_prefix,
   f_name_prefix_after,
 ) {
   assert_arguments(arguments, 2);
-  await functions_rename_generic_starts_with(name_change, f_name_prefix_before);
+  await functions_rename_generic_starts_with(name_change, f_name_prefix);
   function name_change(f_name_before) {
     let together = text_prefix_change(
       f_name_before,
-      f_name_prefix_before,
+      f_name_prefix,
       f_name_prefix_after,
     );
     return together;
