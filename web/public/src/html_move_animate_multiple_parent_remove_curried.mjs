6a3d9d91dae007d90033b44f2cc860385777ce97
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 11:38:26 -0500

    function_auto_path ..\love\public\src\html_move_animate_multiple_parent_remove_curried.mjs

--- web/public/src/html_move_animate_multiple_parent_remove_curried.mjs
+++ web/public/src/html_move_animate_multiple_parent_remove_curried.mjs
@@ -1,9 +1,16 @@
 import { html_move_animate_multiple_parent_remove } from "../../../love/public/src/html_move_animate_multiple_parent_remove.mjs";
 export async function html_move_animate_multiple_parent_remove_curried(froms) {
-  return async function html_move_animate_multiple_parent_remove_curried_result(
-    tos,
-    duration,
-  ) {
-    return await html_move_animate_multiple_parent_remove(froms, tos, duration);
-  };
+  let r2 =
+    async function html_move_animate_multiple_parent_remove_curried_result(
+      tos,
+      duration,
+    ) {
+      let r = await html_move_animate_multiple_parent_remove(
+        froms,
+        tos,
+        duration,
+      );
+      return r;
+    };
+  return r2;
 }
