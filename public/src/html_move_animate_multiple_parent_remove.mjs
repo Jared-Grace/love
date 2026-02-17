795b55a5ffbf8d79bfbd40a797147566b6a88712
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:55:48 -0500

    function_auto_path ..\love\public\src\html_move_animate_multiple_parent_remove.mjs

--- public/src/html_move_animate_multiple_parent_remove.mjs
+++ public/src/html_move_animate_multiple_parent_remove.mjs
@@ -3,17 +3,13 @@ import { each } from "../../../love/public/src/each.mjs";
 import { list_wait } from "../../../love/public/src/list_wait.mjs";
 import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
 import { html_parent_remove } from "../../../love/public/src/html_parent_remove.mjs";
-import { html_move_animate } from "../../../love/public/src/html_move_animate.mjs";
 export async function html_move_animate_multiple_parent_remove(
   froms,
   tos,
   duration,
 ) {
-  let r2 = await html_move_animate_curried_right(duration2);
-  async function lambda6(a, b) {
-    await html_move_animate(a, b, duration);
-  }
-  let mapped = list_map_pairs(froms, tos, lambda6);
+  let r2 = await html_move_animate_curried_right(duration);
+  let mapped = list_map_pairs(froms, tos, r2);
   let v = await list_wait(mapped);
   each(froms, html_parent_remove);
 }
