27ce903ba0c1d1eaa600507cb924f733c6f19d04
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 15:54:18 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -312,7 +312,7 @@ export async function app_replace_rule_set(context) {
         );
         const list = [goal_list_symbols, sbs];
         app_replace_button_symbol_style_valid_multiple(list, true);
-        await html_move_animate_multiple(froms, tos, duration2);
+        await html_move_animate_multiple(sbs, goal_list_symbols, duration);
         let choices = [
           emoji_trophy,
           emoji_100,
