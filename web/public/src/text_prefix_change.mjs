55caf368a037d0d4c5d028ccfbedf91f1c9bbd11
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:46:21 -0500

    function_auto_path ..\love\public\src\text_prefix_change.mjs

--- web/public/src/text_prefix_change.mjs
+++ web/public/src/text_prefix_change.mjs
@@ -1,7 +1,7 @@
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
-export function text_prefix_change(s, before, after) {
-  let without = text_prefix_without(s, before);
+export function text_prefix_change(t, before, after) {
+  let without = text_prefix_without(t, before);
   let together = text_combine(after, without);
   return together;
 }
