8d2e9976d205f80c808bf68903e72ca76b16e89d
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:15:43 -0500

    function_auto_path ..\love\public\src\html_move_animate.mjs

---
+++ web/public/src/html_move_animate_rect.mjs
@@ -0,0 +1,12 @@
+import { sleep } from "../../../love/public/src/sleep.mjs";
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+export async function html_move_animate_rect(component, from, to, duration) {
+  let from_e = html_component_element_get(component);
+  const offsetX = from.left - to.left;
+  const offsetY = from.top - to.top;
+  from_e.style.transition = `transform ${duration}ms`;
+  from_e.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  await sleep(duration);
+  from_e.style.transition = "";
+  from_e.style.transform = "";
+}
