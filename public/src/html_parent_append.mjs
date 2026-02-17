11036db42735be4d2dde60211590fae76a2451ed
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:14:08 -0500

    function_auto_path ..\love\public\src\html_parent_append.mjs

--- public/src/html_parent_append.mjs
+++ public/src/html_parent_append.mjs
@@ -1,5 +1,6 @@
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
-export function html_parent_append(parent, e) {
+export function html_parent_append(parent, child) {
   let parent_element = html_component_element_get(parent);
-  parent_element.appendChild(e);
+  let child_element = html_component_element_get(child);
+  parent_element.appendChild(child_element);
 }
