14030ff67bb2603f1ecb93b2a7bfbc5cbb1c9941
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:33:15 -0500

    de list_slice_end_curried_right

--- public/src/list_slice_end_curried_right.mjs
+++
@@ -1,8 +0,0 @@
-import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
-export function list_slice_end_curried_right(count) {
-  let r = function list_slice_end_curried_right_result(list) {
-    let result = list_slice_end(list, count);
-    return result;
-  };
-  return r;
-}
