5a0128cb056c4c25118449a0cbd7700709bf14ab
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 17:00:55 -0500

    function_auto_path ..\love\public\src\function_curryify_right_count_args_get.mjs

--- public/src/function_curryify_right_count_args_get.mjs
+++ public/src/function_curryify_right_count_args_get.mjs
@@ -1,11 +1,7 @@
 import { list_slice_end_curried_right } from "../../../love/public/src/list_slice_end_curried_right.mjs";
-import { list_last_remaining_get } from "../../../love/public/src/list_last_remaining_get.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
 export function function_curryify_right_count_args_get(arg_names, count) {
-  let r2 = list_slice_end_curried_right(count2);
-  let r = function_curryify_args_get_generic(
-    list_last_remaining_get,
-    arg_names,
-  );
+  let r2 = list_slice_end_curried_right(count);
+  let r = function_curryify_args_get_generic(r2, arg_names);
   return r;
 }
