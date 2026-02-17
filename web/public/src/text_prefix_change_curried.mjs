0cfe63dc02a7952714c19d7e714c8ecc73f23377
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:46:46 -0500

    de text_prefix_change_curried

--- web/public/src/text_prefix_change_curried.mjs
+++
@@ -1,8 +0,0 @@
-import { text_prefix_change } from "../../../love/public/src/text_prefix_change.mjs";
-export function text_prefix_change_curried(t) {
-  let r = function text_prefix_change_curried_result(before, after) {
-    let together = text_prefix_change(t, before, after);
-    return together;
-  };
-  return r;
-}
