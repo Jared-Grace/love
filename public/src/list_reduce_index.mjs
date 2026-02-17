ab977ecd450560379a823520df426dcfcffb2f7a
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:39:11 -0500

    function_auto_path ..\love\public\src\list_reduce_index.mjs

--- public/src/list_reduce_index.mjs
+++ public/src/list_reduce_index.mjs
@@ -2,7 +2,7 @@ import { each_index } from "./each_index.mjs";
 export function list_reduce_index(list, lambda$item$value$index, inital) {
   let value = inital;
   function lambda2(item, index) {
-    value = lambda$item$value$index(item, value, index);
+    value = lambda$item$value$index(value, item, index);
   }
   each_index(list, lambda2);
   return value;
