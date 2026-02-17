db33201c250a565dd7085847eb3185c39262517d
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 17:00:32 -0500

    function_auto_path ..\love\public\src\list_slice_end.mjs

--- web/public/src/list_slice_end.mjs
+++ web/public/src/list_slice_end.mjs
@@ -1,8 +1,8 @@
 import { list_slice } from "../../../love/public/src/list_slice.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-export function list_slice_end(list, offset) {
+export function list_slice_end(list, count) {
   let end = list_size(list);
-  let start = end - offset;
+  let start = end - count;
   if (start < 0) {
     start = 0;
   }
