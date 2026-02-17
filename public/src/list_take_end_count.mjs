2f24fd40bbfc8fd7de4d8b8a3ab7470347824866
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:23:47 -0500

    crl2 list_slice_end take_end_count

---
+++ public/src/list_take_end_count.mjs
@@ -0,0 +1,11 @@
+import { list_slice } from "../../../love/public/src/list_slice.mjs";
+import { list_size } from "../../../love/public/src/list_size.mjs";
+export function list_take_end_count(list, count) {
+  let end = list_size(list);
+  let start = end - count;
+  if (start < 0) {
+    start = 0;
+  }
+  let result = list_slice(list, start, end);
+  return result;
+}
