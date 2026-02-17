386ef2348d0147be92ba47936c0e456c6ba7717d
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:33:43 -0500

    function_auto_path ..\love\public\src\list_take_end_count_curried_right.mjs

--- public/src/list_take_end_count_curried_right.mjs
+++ public/src/list_take_end_count_curried_right.mjs
@@ -1,6 +1,8 @@
 import { list_take_end_count } from "../../../love/public/src/list_take_end_count.mjs";
 export function list_take_end_count_curried_right(count) {
-  return function list_take_end_count_curried_right_result(list) {
-    return list_take_end_count(list, count);
+  let r = function list_take_end_count_curried_right_result(list) {
+    let result = list_take_end_count(list, count);
+    return result;
   };
+  return r;
 }
