204abcdf7b1cd109096a234de38456a02409b690
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:47:18 -0500

    function_auto_path ..\love\public\src\data_prompts_count_display.mjs

--- public/src/data_prompts_count_display.mjs
+++ public/src/data_prompts_count_display.mjs
@@ -1,18 +1,14 @@
+import { data_prompts_count } from "../../../love/public/src/data_prompts_count.mjs";
 import { function_aliases_for } from "../../../love/public/src/function_aliases_for.mjs";
 import { prompt_previous_at } from "../../../love/public/src/prompt_previous_at.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_map_index_countdown } from "../../../love/public/src/list_map_index_countdown.mjs";
-import { list_slice_end } from "../../../love/public/src/list_slice_end.mjs";
-import { data_value } from "../../../love/public/src/data_value.mjs";
-import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
 export async function data_prompts_count_display(offset) {
   let fn = prompt_previous_at;
   let value = await function_aliases_for(fn);
   let first = list_first(value);
-  let d_path = data_prompts_path();
-  let prompts = await data_value("prompts", d_path);
-  let sliced = list_slice_end(prompts, offset);
+  let sliced = await data_prompts_count(offset);
   function lambda_inner(item, index) {
     let together = text_combine_multiple([first, " ", index, " : ", item]);
     return together;
