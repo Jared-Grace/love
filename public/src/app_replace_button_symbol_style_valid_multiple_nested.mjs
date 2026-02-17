b82b38fc024da7a23699e27366cec4a6386eeb09
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 18:43:57 -0500

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid_multiple_nested.mjs

--- public/src/app_replace_button_symbol_style_valid_multiple_nested.mjs
+++ public/src/app_replace_button_symbol_style_valid_multiple_nested.mjs
@@ -1,4 +1,4 @@
 import { app_replace_button_symbol_style_valid_if_multiple } from "../../../love/public/src/app_replace_button_symbol_style_valid_if_multiple.mjs";
-export function app_replace_button_symbol_style_valid_multiple(list) {
+export function app_replace_button_symbol_style_valid_multiple_nested(list) {
   app_replace_button_symbol_style_valid_if_multiple(list, true);
 }
