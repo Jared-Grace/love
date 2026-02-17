e884fcdfb10dacedaf9d02fa782352327c895dbb
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:43:58 -0500

    function_auto_path ..\love\public\src\functions_rename_if_starts_with_suffix_add.mjs

--- public/src/functions_rename_if_starts_with_suffix_add.mjs
+++ public/src/functions_rename_if_starts_with_suffix_add.mjs
@@ -1,18 +1,14 @@
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { functions_rename_generic_starts_with } from "../../../love/public/src/functions_rename_generic_starts_with.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
-import { text_prefix_change } from "../../../love/public/src/text_prefix_change.mjs";
 export async function functions_rename_if_starts_with_suffix_add(
   f_name_prefix,
-  f_name_prefix_after,
+  suffix,
 ) {
   assert_arguments(arguments, 2);
   await functions_rename_generic_starts_with(name_change, f_name_prefix);
   function name_change(f_name_before) {
-    let together = text_prefix_change(
-      f_name_before,
-      f_name_prefix,
-      f_name_prefix_after,
-    );
+    let together = function_name_combine(f_name_before, suffix);
     return together;
   }
 }
