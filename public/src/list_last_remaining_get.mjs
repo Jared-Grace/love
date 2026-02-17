6294a0750f2802e5030d48875cf2614eacdca798
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:56:53 -0500

    function_auto_path ..\love\public\src\list_last_remaining_get.mjs

--- public/src/list_last_remaining_get.mjs
+++ public/src/list_last_remaining_get.mjs
@@ -2,6 +2,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_last_remaining } from "../../../love/public/src/list_last_remaining.mjs";
 export function list_last_remaining_get(arg_names) {
   let result = list_last_remaining(arg_names);
-  let fn_new_result_args = property_get(result, "remaining");
-  return fn_new_result_args;
+  let remaining = property_get(result, "remaining");
+  return remaining;
 }
