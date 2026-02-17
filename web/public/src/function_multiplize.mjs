1d91fa4504cfc9457eb0e2b71d688a2a713cd02e
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 14:27:28 -0500

    function_auto_path ..\love\public\src\function_multiplize.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -1,1 +1,4 @@
-export function function_multiplize(f_name) {}
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+export function function_multiplize(f_name) {
+  let combined = function_name_combine(f_name, "multiple");
+}
