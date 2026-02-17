cb58db776261b6a72dcc5826e2e5bfcd94166873
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:03:22 -0500

    function_auto_path ..\love\public\src\html_parent_remove.mjs

--- public/src/html_parent_remove.mjs
+++ public/src/html_parent_remove.mjs
@@ -1,4 +1,6 @@
-export function html_parent_remove(from_e) {
+import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
+export function html_parent_remove(component) {
+  let from_e = html_component_element_get(component);
   const parent = from_e.parentNode;
   parent.removeChild(from_e);
 }
