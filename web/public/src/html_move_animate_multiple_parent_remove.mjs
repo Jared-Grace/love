52807c5aee641174764435e3dc6c1fa5c6f368ff
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 23:01:05 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

---
+++ web/public/src/html_move_animate_multiple_parent_remove.mjs
@@ -0,0 +1,16 @@
+import { list_wait } from "../../../love/public/src/list_wait.mjs";
+import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
+import { html_parent_remove } from "../../../love/public/src/html_parent_remove.mjs";
+import { html_move_animate } from "../../../love/public/src/html_move_animate.mjs";
+export async function html_move_animate_multiple_parent_remove(
+  duration,
+  sliced2,
+  lefts2,
+) {
+  async function lambda6(a, b) {
+    await html_move_animate(a, b, duration);
+    html_parent_remove(a);
+  }
+  let mapped = list_map_pairs(sliced2, lefts2, lambda6);
+  let v = await list_wait(mapped);
+}
