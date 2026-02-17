65ddd496ce4b179384d4e6fde6d132ed63ab34c2
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 13:49:43 -0500

    cs data_prompts display

---
+++ public/src/data_prompts_display.mjs
@@ -0,0 +1,8 @@
+import { data_prompts_count_display } from "../../../love/public/src/data_prompts_count_display.mjs";
+import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
+export async function data_prompts_display() {
+  assert_arguments(arguments, 0);
+  let offset = 25;
+  let result = await data_prompts_count_display(offset);
+  return result;
+}
