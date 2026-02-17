da5aff38cc705184fa1d3807a1108099430d65cb
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 16:22:02 -0500

    function_auto_path ..\love\public\src\app_replace_button_home.mjs

--- public/src/app_replace_button_home.mjs
+++ public/src/app_replace_button_home.mjs
@@ -3,9 +3,9 @@ import { emoji_home } from "../../../love/public/src/emoji_home.mjs";
 import { app_replace_button_screen } from "./app_replace_button_screen.mjs";
 export function app_replace_button_home(root, context) {
   let b = app_replace_button_screen(
-    root,
-    emoji_home() + " Home",
     context,
     app_replace_home,
+    root,
+    emoji_home() + " Home",
   );
 }
