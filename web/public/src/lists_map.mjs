b3ed5c096c335f3b07992f27bafe0cf19467b60c
Grace-Jared <christrosetolife@gmail.com>
2026-02-16 19:20:44 -0500

    function_auto_path ..\love\public\src\lists_map.mjs

--- web/public/src/lists_map.mjs
+++ web/public/src/lists_map.mjs
@@ -2,11 +2,11 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_multiple } from "./each_multiple.mjs";
 export function lists_map(lists, mapper) {
   function lambda_list_adder(la) {
-    function lambda_each_pair(a, b) {
+    function lambda_each(elements) {
       const m = mapper(a, b);
       la(m);
     }
-    each_multiple(lists, lambda_each_pair);
+    each_multiple(lists, lambda_each);
   }
   let mapped = list_adder(lambda_list_adder);
   return mapped;
