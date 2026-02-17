8d587f4e097995f3ecf4ef0075ad59f88e9a9c4f
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 11:31:56 -0500

    function_auto_path ..\love\public\src\html_element.mjs

--- web/public/src/html_element.mjs
+++ web/public/src/html_element.mjs
@@ -2,8 +2,8 @@ import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { html_component_wrap } from "../../../love/public/src/html_component_wrap.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_element(parent, tag_name) {
-  let parent_element = html_component_element_get(parent);
   const e = document.createElement(tag_name);
+  let parent_element = html_component_element_get(parent);
   parent_element.appendChild(e);
   let component = html_component_wrap(e);
   html_style_set(component, "box-sizing", "border-box");
