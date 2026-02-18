3c5be34b2666b71c21bc2cd4b8495659ae1eb1da
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:54:23 -0500

    fwr function_rename_suffix_add function functions

---
+++ web/public/src/functions_rename_suffix_add.mjs
@@ -0,0 +1,4 @@
+import { function_rename_suffix_add } from "../../../love/public/src/function_rename_suffix_add.mjs";
+export async function functions_rename_suffix_add(f_name_before, suffix) {
+  return await function_rename_suffix_add(f_name_before, suffix);
+}
