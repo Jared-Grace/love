d37573d47fac3f5ed284de474036842646900cd4
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:38:22 -0500

    function_auto_path ..\love\public\src\js_object_expression_named.mjs

--- web/public/src/js_object_expression_named.mjs
+++ web/public/src/js_object_expression_named.mjs
@@ -1,6 +1,9 @@
 import { js_object_expression_named_generic } from "../../../love/public/src/js_object_expression_named_generic.mjs";
 export function js_object_expression_named(ast, search) {
-  let node_type = "ObjectExpression";
-  let list = js_object_expression_named_generic(ast, node_type, search);
+  let list = js_object_expression_named_generic(
+    ast,
+    "ObjectExpression",
+    search,
+  );
   return list;
 }
