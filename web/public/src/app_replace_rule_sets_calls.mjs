d2ca83611a51ff2d90fcc67e40e512fe0678daf8
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:47:09 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_calls.mjs

--- web/public/src/app_replace_rule_sets_calls.mjs
+++ web/public/src/app_replace_rule_sets_calls.mjs
@@ -1,5 +1,5 @@
+import { js_function_declaration_single_block_body_add_return } from "../../../love/public/src/js_function_declaration_single_block_body_add_return.mjs";
 import { list_empty } from "../../../love/public/src/list_empty.mjs";
-import { js_function_declaration_single_block_body } from "../../../love/public/src/js_function_declaration_single_block_body.mjs";
 import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
 import { function_ast_fn } from "../../../love/public/src/function_ast_fn.mjs";
 import { js_list_calls_names } from "../../../love/public/src/js_list_calls_names.mjs";
@@ -8,7 +8,7 @@ export async function app_replace_rule_sets_calls() {
   let ast2 = await function_ast_fn(app_replace_rule_sets_v_1);
   let names = js_list_calls_names(ast2);
   async function lambda(ast) {
-    let body_block = js_function_declaration_single_block_body(ast);
+    let body_block = js_function_declaration_single_block_body_add_return(ast);
     list_empty(body_block);
   }
   let output = await function_transform_fn(lambda);
