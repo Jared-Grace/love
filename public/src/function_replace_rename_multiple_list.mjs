2797ad37188c623a6db608f53f87e67b08d6978d
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:00:34 -0500

    function_auto_path ..\love\public\src\function_replace_rename_multiple_list.mjs

--- public/src/function_replace_rename_multiple_list.mjs
+++ public/src/function_replace_rename_multiple_list.mjs
@@ -1,4 +1,5 @@
 import { function_replace_rename_multiple_generic } from "../../../love/public/src/function_replace_rename_multiple_generic.mjs";
 export async function function_replace_rename_multiple_list(f_names, from, to) {
-  return await function_replace_rename_multiple_generic(f_names, from, to);
+  let r = await function_replace_rename_multiple_generic(f_names, from, to);
+  return r;
 }
