5fdc1f2b23627227997595ea4a2509aca61c4dce
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:34:21 -0500

    function_auto_path ..\love\public\src\json_format_to.mjs

--- public/src/json_format_to.mjs
+++ public/src/json_format_to.mjs
@@ -1,6 +1,6 @@
 import { text_take } from "../../../love/public/src/text_take.mjs";
 export function json_format_to(object) {
   let j = JSON.stringify(object, null, 1);
-  let taken = text_take(s, count);
-  return j;
+  let taken = text_take(j, 10000);
+  return taken;
 }
