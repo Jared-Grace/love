6955c28bf6307aab24081edfa5732146474a2c9c
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:00:32 -0500

    fwrl function_replace_rename_multiple_generic list

---
+++ public/src/function_replace_rename_multiple_list.mjs
@@ -0,0 +1,4 @@
+import { function_replace_rename_multiple_generic } from "../../../love/public/src/function_replace_rename_multiple_generic.mjs";
+export async function function_replace_rename_multiple_list(f_names, from, to) {
+  return await function_replace_rename_multiple_generic(f_names, from, to);
+}
