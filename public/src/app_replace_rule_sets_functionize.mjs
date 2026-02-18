b9d92ffa1a6349c474a9e0c0cce769e249b21467
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:34:34 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- public/src/app_replace_rule_sets_functionize.mjs
+++ public/src/app_replace_rule_sets_functionize.mjs
@@ -12,6 +12,7 @@ import { function_transform_marker_specified } from "../../../love/public/src/fu
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function app_replace_rule_sets_functionize() {
+  "this took a list of objects and moved each object to its own file";
   let f_name = app_replace_rule_sets.name;
   let code = await function_transform_marker_specified(f_name, "rules", lambda);
   async function lambda(a) {
