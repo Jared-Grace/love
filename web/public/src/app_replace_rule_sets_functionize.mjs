d081f672490985d368e1f091de061c9ba121de2c
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:37:17 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -1,3 +1,4 @@
+import { log_unparse } from "../../../love/public/src/log_unparse.mjs";
 import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
 import { js_function_declaration_to_block_body } from "../../../love/public/src/js_function_declaration_to_block_body.mjs";
 import { function_new_declaration_to } from "../../../love/public/src/function_new_declaration_to.mjs";
@@ -5,7 +6,6 @@ import { js_object_expression_properties_find_key_named } from "../../../love/pu
 import { js_property_value_get } from "../../../love/public/src/js_property_value_get.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { marker_next_declare_single_init_elements } from "../../../love/public/src/marker_next_declare_single_init_elements.mjs";
 import { function_transform_marker_specified } from "../../../love/public/src/function_transform_marker_specified.mjs";
@@ -21,10 +21,11 @@ export async function app_replace_rule_sets_functionize() {
       let p = js_property_value_get(found);
       let l = js_literal_value_get(p);
       let replaced = text_replace_space_underscore_lower(l);
-      let declaration = function_new_declaration_to(f_name);
-      let r = js_statement_return_empty_add(body_block2);
+      let declaration = function_new_declaration_to(replaced);
       let body_block = js_function_declaration_to_block_body(declaration);
-      log(e);
+      let r = js_statement_return_empty_add(body_block);
+      log_unparse(node);
+      log_unparse(node2);
     }
     each(elements, lambda2);
   }
