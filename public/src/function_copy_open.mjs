e02319b6481aaeb93ca6b3cf44e18577b3ee5594
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:23:45 -0500

    s copy

--- public/src/function_copy_open.mjs
+++ public/src/function_copy_open.mjs
@@ -9,19 +9,24 @@ import { file_copy } from "../../../love/public/src/file_copy.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { file_open } from "../../../love/public/src/file_open.mjs";
 export async function function_copy_open(f_name_old, f_name_new) {
+  let { f_path_new, name } = await function_copy(f_name_old, f_name_new);
+  await file_open(f_path_new);
+  return name;
+}
+async function function_copy(f_name_old, f_name_new) {
   const r = await function_name_to_path_unalias(f_name_old);
   let unaliased_old = property_get(r, "unaliased");
   let f_path_old = property_get(r, "f_path");
   let r2 = await function_name_unalias(f_name_new);
-  let unaliased_new = property_get(r2, "unaliased");
+  let name = property_get(r2, "unaliased");
   let f_path = function_name_to_path(f_name_new);
   let f_path_new = await user_repo_path_combine(f_path);
   await file_copy(f_path_old, f_path_new);
   function lambda(ast) {
-    js_identifier_rename(ast, unaliased_old, unaliased_new);
+    js_identifier_rename(ast, unaliased_old, name);
   }
-  await function_transform(unaliased_new, lambda);
-  await function_current_set(unaliased_new);
-  await file_open(f_path_new);
-  return unaliased_new;
+  await function_transform(name, lambda);
+  await function_current_set(name);
+  return { f_path_new,  name };
 }
+
