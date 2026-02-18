5d0c3a11fa825eb1488228c737bb356ea2a6bae3
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:09:32 -0500

    function_auto_path ..\love\public\src\js_function_declaration_single_param_add.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -3,7 +3,7 @@ import { js_function_declaration_params_names } from "../../../love/public/src/j
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
-import { js_declaration_single_param_add } from "../../../love/public/src/js_declaration_single_param_add.mjs";
+import { js_function_declaration_single_param_add } from "../../../love/public/src/js_function_declaration_single_param_add.mjs";
 import { js_call } from "../../../love/public/src/js_call.mjs";
 import { js_function_declaration_single_block_body_add } from "../../../love/public/src/js_function_declaration_single_block_body_add.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -19,7 +19,7 @@ export async function function_multiplize(f_name) {
     const list = "list";
     let call = js_call(each.name, [list, f_name]);
     js_function_declaration_single_block_body_add(ast, call);
-    js_declaration_single_param_add(ast, list);
+    js_function_declaration_single_param_add(ast, list);
     await js_imports_missing_add(ast);
   }
   let output = await function_new_transform(combined, lambda);
