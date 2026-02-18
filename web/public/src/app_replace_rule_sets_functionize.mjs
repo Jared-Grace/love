b42e9ef6bf876effa47a4b048e99f915b0739e11
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:37:33 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -1,10 +1,8 @@
-import { js_object_expression_properties } from "../../../love/public/src/js_object_expression_properties.mjs";
-import { js_property_key_named_curried_right } from "../../../love/public/src/js_property_key_named_curried_right.mjs";
+import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { js_property_value_get } from "../../../love/public/src/js_property_value_get.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { list_find } from "../../../love/public/src/list_find.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { marker_next_declare_single_init_elements } from "../../../love/public/src/marker_next_declare_single_init_elements.mjs";
 import { function_transform_marker_specified } from "../../../love/public/src/function_transform_marker_specified.mjs";
@@ -17,9 +15,7 @@ export async function app_replace_rule_sets_functionize() {
     let elements = marker_next_declare_single_init_elements(a);
     function lambda2(e) {
       let search = "name";
-      let properties = js_object_expression_properties(e);
-      let r = js_property_key_named_curried_right(search);
-      let found = list_find(properties, r);
+      let found = js_object_expression_properties_find_key_named(e, search);
       let value = js_property_value_get(found);
       let value2 = js_literal_value_get(value);
       let replaced = text_replace_space_underscore_lower(value2);
