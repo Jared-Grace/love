9375da490337205d5199ab93afc8b9834fba543d
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:05:46 -0500

    function_auto_path ..\love\public\src\js_outside_move.mjs

--- web/public/src/js_outside_move.mjs
+++ web/public/src/js_outside_move.mjs
@@ -1,3 +1,4 @@
+import { js_declaration_name_to_path } from "../../../love/public/src/js_declaration_name_to_path.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_imports_fix } from "../../../love/public/src/js_imports_fix.mjs";
@@ -7,14 +8,11 @@ import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { function_new_declaration } from "../../../love/public/src/function_new_declaration.mjs";
 import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { js_declaration_name } from "../../../love/public/src/js_declaration_name.mjs";
-import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 export async function js_outside_move(ast) {
   let body = property_get(ast, "body");
   let fds = list_filter_property(body, "type", "FunctionDeclaration");
   async function lambda(fd) {
-    let f_name = js_declaration_name(fd);
-    let f_path = function_name_to_path(f_name);
+    let f_path = js_declaration_name_to_path(fd);
     await assert_file_exists_not(f_path);
   }
   await each_async(fds, lambda);
