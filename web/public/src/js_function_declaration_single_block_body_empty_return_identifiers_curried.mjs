e30c1896acb5bcbb4cd7f3a9f3553e7f46b065ff
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:56:46 -0500

    pp 6

--- web/public/src/js_function_declaration_single_block_body_empty_return_identifiers_curried.mjs
+++ web/public/src/js_function_declaration_single_block_body_empty_return_identifiers_curried.mjs
@@ -1,1 +1,13 @@
-export function js_function_declaration_single_block_body_empty_return_identifiers_curried() {}
+import { js_function_declaration_single_block_body_empty_return_identifiers } from "../../../love/public/src/js_function_declaration_single_block_body_empty_return_identifiers.mjs";
+export function js_function_declaration_single_block_body_empty_return_identifiers_curried(
+  ast,
+) {
+  return function js_function_declaration_single_block_body_empty_return_identifiers_curried_result(
+    names,
+  ) {
+    return js_function_declaration_single_block_body_empty_return_identifiers(
+      ast,
+      names,
+    );
+  };
+}
