fe050c7dd0d3f046b718c6e909e35a6973b3be75
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:22:49 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set_replace.mjs

---
+++ web/public/src/app_replace_rule_set_shrink_both_same.mjs
@@ -0,0 +1,20 @@
+export function app_replace_rule_set_shrink_both_same() {
+  return {
+    name: "Shrink Both Same",
+    rules: ["a b a > b"],
+    goals: [
+      {
+        end: "cabac",
+        start: "cbc",
+      },
+      {
+        end: "aaabaaa",
+        start: "aba",
+      },
+      {
+        end: "aaaaabaaaaa",
+        start: "aba",
+      },
+    ],
+  };
+}
