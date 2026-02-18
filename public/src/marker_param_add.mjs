21a01904d48af6df451e888031eedc77222f8fb9
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:08:07 -0500

    function_auto_path ..\love\public\src\js_declaration_param_add_curry.mjs

--- public/src/marker_param_add.mjs
+++ public/src/marker_param_add.mjs
@@ -1,4 +1,4 @@
-import { js_declaration_param_add } from "../../../love/public/src/js_declaration_param_add.mjs";
+import { js_function_declaration_param_add } from "../../../love/public/src/js_function_declaration_param_add.mjs";
 import { js_stack_last_function } from "../../../love/public/src/js_stack_last_function.mjs";
 import { function_transform_marker } from "../../../love/public/src/function_transform_marker.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
@@ -10,7 +10,7 @@ export async function marker_param_add(param_name) {
     async function lambda(a) {
       let { stack } = a;
       let f = js_stack_last_function(stack);
-      js_declaration_param_add(f, param_name);
+      js_function_declaration_param_add(f, param_name);
     }
   }
   let list = await list_adder_async(lambda2);
