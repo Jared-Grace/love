0a08f39050b760b73847d8e7597f72899aebe275
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:02:12 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- public/src/js_call_statement_insert.mjs
+++ public/src/js_call_statement_insert.mjs
@@ -1,6 +1,6 @@
 import { js_call_statement } from "../../../love/public/src/js_call_statement.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
-export function js_call_insert(f_name, args_code, list, index) {
+export function js_call_statement_insert(f_name, args_code, list, index) {
   let parsed = js_call_statement(f_name, args_code);
   list_insert(list, index, parsed);
 }
