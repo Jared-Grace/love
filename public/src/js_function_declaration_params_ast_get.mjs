819928e7f204fcc51fbea2521b2eccbf4ec8c8b4
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:07:19 -0500

    function_auto_path ..\love\public\src\function_curryify_generic.mjs

--- public/src/js_function_declaration_params_ast_get.mjs
+++ public/src/js_function_declaration_params_ast_get.mjs
@@ -1,6 +1,6 @@
 import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
-export function js_declaration_params_ast_get(ast) {
+export function js_function_declaration_params_ast_get(ast) {
   let declaration = js_declaration_single(ast);
   let params = js_declaration_params_get(declaration);
   let r = {
