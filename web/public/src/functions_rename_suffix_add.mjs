b08a99a2e26cc0b904e298202a1a1a21a9aae132
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:54:25 -0500

    function_auto_path ..\love\public\src\functions_rename_suffix_add.mjs

--- web/public/src/functions_rename_suffix_add.mjs
+++ web/public/src/functions_rename_suffix_add.mjs
@@ -1,4 +1,5 @@
 import { function_rename_suffix_add } from "../../../love/public/src/function_rename_suffix_add.mjs";
 export async function functions_rename_suffix_add(f_name_before, suffix) {
-  return await function_rename_suffix_add(f_name_before, suffix);
+  let r = await function_rename_suffix_add(f_name_before, suffix);
+  return r;
 }
