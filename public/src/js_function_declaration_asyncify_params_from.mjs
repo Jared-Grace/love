572569f8fba5100e80b376989b8d57ffbcccd104
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:08:42 -0500

    function_auto_path ..\love\public\src\js_dollar_new_args_inner.mjs

--- public/src/js_function_declaration_asyncify_params_from.mjs
+++ public/src/js_function_declaration_asyncify_params_from.mjs
@@ -1,12 +1,12 @@
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 import { property_from } from "../../../love/public/src/property_from.mjs";
 import { js_function_declaration_asyncify } from "../../../love/public/src/js_function_declaration_asyncify.mjs";
-import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
+import { js_function_declaration_single } from "../../../love/public/src/js_function_declaration_single.mjs";
 export async function js_function_declaration_asyncify_params_from(
   ast,
   declaration_call,
 ) {
-  let declaration = js_declaration_single(ast);
+  let declaration = js_function_declaration_single(ast);
   js_function_declaration_asyncify(declaration, declaration_call);
   property_from(declaration, "params", declaration_call);
   await js_imports_missing_add(ast);
