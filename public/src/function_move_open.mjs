53ffab5fd90f3765b5a0114c4f27f31c7d70ab1d
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:25:14 -0500

    function_auto_path ..\love\public\src\function_move_open.mjs

--- public/src/function_move_open.mjs
+++ public/src/function_move_open.mjs
@@ -1,6 +1,11 @@
+import { file_open } from "../../../love/public/src/file_open.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
-import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 export async function function_move_open(f_name_before, f_name_after) {
-  await function_copy_open(f_name_before, f_name_after);
+  let r = await function_copy(f_name_before, f_name_after);
+  let name = property_get(r, "name");
+  let f_path_new = property_get(r, "f_path_new");
+  await file_open(f_path_new);
   await function_delete(f_name_before);
 }
