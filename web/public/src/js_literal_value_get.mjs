d640a93079e43e369ab6626281aeaf5d2f2ce826
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:26:32 -0500

    function_auto_path ..\love\public\src\js_literal_value_get.mjs

--- web/public/src/js_literal_value_get.mjs
+++ web/public/src/js_literal_value_get.mjs
@@ -1,7 +1,7 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_literal_is_assert } from "../../../love/public/src/js_literal_is_assert.mjs";
-export function js_literal_value_get(value) {
-  js_literal_is_assert(value);
-  let value2 = property_get(value, "value");
+export function js_literal_value_get(literal) {
+  js_literal_is_assert(literal);
+  let value2 = property_get(literal, "value");
   return value2;
 }
