b08abbfd34a4544ccaa11729a4cf019d4a87644a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:07:39 -0500

    function_auto_path ..\love\public\src\js_declaration_param_add_node.mjs

--- web/public/src/function_param_delete_multiple.mjs
+++ web/public/src/function_param_delete_multiple.mjs
@@ -6,7 +6,7 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_declaration_param_named } from "../../../love/public/src/js_declaration_param_named.mjs";
-import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
+import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 import { function_params_new_generic } from "../../../love/public/src/function_params_new_generic.mjs";
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
@@ -25,7 +25,7 @@ export async function function_param_delete_multiple(param_names_comma) {
   }
   function function_transform_current_lambda(ast) {
     let declaration = js_declaration_single(ast);
-    let params = js_declaration_params_get(declaration);
+    let params = js_function_declaration_params_get(declaration);
     function lambda(param_name) {
       let p = js_declaration_param_named(declaration, param_name);
       let index = list_index_of(params, p);
