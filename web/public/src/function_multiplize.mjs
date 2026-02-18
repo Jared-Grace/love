f4a6fbf622628c3127f16e859ba37dd352843132
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:02:09 -0500

    function_auto_path ..\love\public\src\js_call_statement.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -4,7 +4,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 import { js_function_declaration_single_param_add } from "../../../love/public/src/js_function_declaration_single_param_add.mjs";
-import { js_call } from "../../../love/public/src/js_call.mjs";
+import { js_call_statement } from "../../../love/public/src/js_call_statement.mjs";
 import { js_function_declaration_single_block_body_add } from "../../../love/public/src/js_function_declaration_single_block_body_add.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
@@ -17,7 +17,7 @@ export async function function_multiplize(f_name) {
   let combined = function_name_combine(f_name, "multiple");
   async function lambda(ast) {
     const list = "list";
-    let call = js_call(each.name, [list, f_name]);
+    let call = js_call_statement(each.name, [list, f_name]);
     js_function_declaration_single_block_body_add(ast, call);
     js_function_declaration_single_param_add(ast, list);
     await js_imports_missing_add(ast);
