0aabffe2dc984193c2f584a2a591e35d6d32f7a0
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:24:31 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set_binary_counting.mjs

--- public/src/app_replace_rule_set_grow_same_three_different.mjs
+++ public/src/app_replace_rule_set_grow_same_three_different.mjs
@@ -1,5 +1,5 @@
 export function app_replace_rule_set_grow_same_three_different() {
-  return {
+  let r = {
     name: "Grow Same Three Different",
     rules: ["c e b c > e b"],
     goals: [
@@ -13,4 +13,5 @@ export function app_replace_rule_set_grow_same_three_different() {
       },
     ],
   };
+  return r;
 }
