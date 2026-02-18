d013e95fcaba5d8a0a7e535d3393f59255fbebe8
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:18:06 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

---
+++ web/public/src/js_identifier_named_try.mjs
@@ -0,0 +1,7 @@
+import { equal } from "../../../love/public/src/equal.mjs";
+import { js_identifier_name_try } from "../../../love/public/src/js_identifier_name_try.mjs";
+export function js_identifier_named_try(key, search) {
+  let name = js_identifier_name_try(key);
+  let eq2 = equal(name, search);
+  return eq2;
+}
