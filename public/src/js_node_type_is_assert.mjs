752d3f764fb75920520c69edb3fc9bea866a51b7
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:09:08 -0500

    function_auto_path ..\love\public\src\js_node_type_is_assert.mjs

--- public/src/js_node_type_is_assert.mjs
+++ public/src/js_node_type_is_assert.mjs
@@ -1,9 +1,7 @@
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
-import { assert } from "../../../love/public/src/assert.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 export function js_node_type_is_assert(node, type) {
   let type_is = js_node_type_is(node, type);
-  assert(type_is);
   function lambda2() {
     let r = {
       node,
@@ -11,5 +9,5 @@ export function js_node_type_is_assert(node, type) {
     };
     return r;
   }
-  assert_json_get(b, lambda2);
+  assert_json_get(type_is, lambda2);
 }
