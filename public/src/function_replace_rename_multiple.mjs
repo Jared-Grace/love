9321e2c1df4da8acfdde7226bde4ba88e0060d96
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:59:36 -0500

    function_auto_path ..\love\public\src\function_replace_rename_multiple.mjs

--- public/src/function_replace_rename_multiple.mjs
+++ public/src/function_replace_rename_multiple.mjs
@@ -1,15 +1,6 @@
-import { text_includes_not } from "../../../love/public/src/text_includes_not.mjs";
+import { function_replace_rename_multiple_generic } from "../../../love/public/src/function_replace_rename_multiple_generic.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 export async function function_replace_rename_multiple(from, to) {
   let f_names = await functions_names();
-  async function lambda(f_name) {
-    let n = text_includes_not(f_name, from);
-    if (n) {
-      return;
-    }
-    let v = await function_rename_replace(f_name, from, to);
-  }
-  await each_async(f_names, lambda);
+  await function_replace_rename_multiple_generic(from, to, f_names);
 }
