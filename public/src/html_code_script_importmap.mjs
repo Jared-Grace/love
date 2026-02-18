9128b01c32abbe56584d97848ef31b0d1899a9ec
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:34:35 -0500

    rs json_format_to truncated

--- public/src/html_code_script_importmap.mjs
+++ public/src/html_code_script_importmap.mjs
@@ -1,9 +1,9 @@
 import { function_dependencies_externals_to_urls } from "../../../love/public/src/function_dependencies_externals_to_urls.mjs";
 import { html_code_script_module_generic } from "../../../love/public/src/html_code_script_module_generic.mjs";
-import { json_format_to } from "./json_format_to.mjs";
+import { json_format_to_truncated } from "../../../love/public/src/json_format_to_truncated.mjs";
 export function html_code_script_importmap(externals) {
   let imports = function_dependencies_externals_to_urls(externals);
-  let j = json_format_to({
+  let j = json_format_to_truncated({
     imports,
   });
   const script_type = "importmap";
