545fc4e4321350833a6ab56468ae09b782941e89
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:30:50 -0500

    c js_visit_calls_named app_replace_rule_sets_calls

---
+++ public/src/app_replace_rule_sets_calls.mjs
@@ -0,0 +1,19 @@
+import { js_visit_calls } from "../../../love/public/src/js_visit_calls.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { equal_not } from "../../../love/public/src/equal_not.mjs";
+import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
+export function app_replace_rule_sets_calls(ast, f_name, lambda) {
+  function lambda_inner(v) {
+    let node = property_get(v, "node");
+    let name = js_call_callee_name(node);
+    if (equal_not(name, f_name)) {
+      return;
+    }
+    let args = property_get(node, "arguments");
+    lambda({
+      v,
+      args,
+    });
+  }
+  js_visit_calls(ast, lambda_inner);
+}
