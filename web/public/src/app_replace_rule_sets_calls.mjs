329fef90f570035e84ba02b2f68d819fbd5d2a53
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:35:31 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_calls.mjs

--- web/public/src/app_replace_rule_sets_calls.mjs
+++ web/public/src/app_replace_rule_sets_calls.mjs
@@ -4,7 +4,6 @@ import { list_map_property } from "../../../love/public/src/list_map_property.mj
 import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_rule_sets_v_1 } from "../../../love/public/src/app_replace_rule_sets_v_1.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
-import { js_visit_calls } from "../../../love/public/src/js_visit_calls.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
 export async function app_replace_rule_sets_calls() {
   let ast = await function_ast(app_replace_rule_sets_v_1.name);
