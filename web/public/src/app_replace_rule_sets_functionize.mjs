222faf0ecc82dac9b315f81f414aa5b8f23f22a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:54:40 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -1,3 +1,4 @@
+import { function_transform_marker_specified } from "../../../love/public/src/function_transform_marker_specified.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_array_expression_named } from "../../../love/public/src/js_array_expression_named.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
@@ -8,7 +9,12 @@ import { function_transform } from "../../../love/public/src/function_transform.
 export async function app_replace_rule_sets_functionize() {
   let f_name = app_replace_rule_sets.name;
   async function lambda(ast) {
-    let search = "rs";
+    let search = "rules";
+    let code = await function_transform_marker_specified(
+      f_name2,
+      marker_name,
+      async function lambda2(a) {},
+    );
     let list = js_array_expression_named(ast, search);
     let only = list_single(list);
     let elements = js_array_expression_single_elements(ast);
