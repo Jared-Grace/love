07f79042855ba8c9541dc8a194315cf51b5da21a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:07:07 -0500

    function_auto_path ..\love\public\src\js_function_declaration_name_to_path.mjs

--- public/src/js_declaration_name_to_path.mjs
+++
@@ -1,7 +0,0 @@
-import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
-import { js_function_declaration_name } from "../../../love/public/src/js_function_declaration_name.mjs";
-export function js_declaration_name_to_path(fd) {
-  let f_name = js_function_declaration_name(fd);
-  let f_path = function_name_to_path(f_name);
-  return f_path;
-}
