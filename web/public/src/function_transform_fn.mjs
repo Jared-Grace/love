ec0e9af04df5c1b0f4a050053f4dfafd7649148b
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:59:21 -0500

    function_auto_path ..\love\public\src\function_transform_fn.mjs

--- web/public/src/function_transform_fn.mjs
+++ web/public/src/function_transform_fn.mjs
@@ -1,6 +1,5 @@
-import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function function_transform_fn(fn, lambda) {
-  let r = await function_transform(app_replace_rule_sets.name, lambda);
+  let r = await function_transform(fn.name, lambda);
   return r;
 }
