0d4cee1902ea39e52616b1b7f378b153baaa5bf1
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:06:57 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- public/src/app_replace_rule_sets_functionize.mjs
+++ public/src/app_replace_rule_sets_functionize.mjs
@@ -23,14 +23,13 @@ export async function app_replace_rule_sets_functionize() {
       let r = js_statement_return_empty_add(body_block);
       js_return_argument_set(r, e);
       log_unparse(declaration);
-      return declaration;
       await function_new_declaration_from(declaration);
     }
     await each_async(elements, lambda2);
     async function lambda2(e) {
       let f_name_new = app_replace_rule_sets_functionize_name(e);
       let parsed = js_call_empty(f_name_new);
-      object_replace(to, from);
+      object_replace(e, parsed);
     }
     await each_async(elements, lambda2);
   }
