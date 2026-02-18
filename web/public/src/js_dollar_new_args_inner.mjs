eb9dd06c0e107896b8d0a06ee81a8e656bdcadbe
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:08:20 -0500

    function_auto_path ..\love\public\src\js_function_declaration_param_add.mjs

--- web/public/src/js_dollar_new_args_inner.mjs
+++ web/public/src/js_dollar_new_args_inner.mjs
@@ -1,8 +1,8 @@
 import { js_dollar_choice_argument } from "../../../love/public/src/js_dollar_choice_argument.mjs";
-import { js_declaration_param_add_node } from "../../../love/public/src/js_declaration_param_add_node.mjs";
+import { js_function_declaration_param_add_node } from "../../../love/public/src/js_function_declaration_param_add_node.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export async function js_dollar_new_args_inner(ast) {
   let a = await js_dollar_choice_argument();
   let declaration = js_declaration_single(ast);
-  js_declaration_param_add_node(declaration, a);
+  js_function_declaration_param_add_node(declaration, a);
 }
