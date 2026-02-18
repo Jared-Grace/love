ee38444c93c1bb269ef6250a2ff196dab8035882
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:06:50 -0500

    function_auto_path ..\love\public\src\js_declaration_asyncify_params_from.mjs

--- web/public/src/js_function_declaration_asyncify_params_from.mjs
+++ web/public/src/js_function_declaration_asyncify_params_from.mjs
@@ -2,7 +2,7 @@ import { js_imports_missing_add } from "../../../love/public/src/js_imports_miss
 import { property_from } from "../../../love/public/src/property_from.mjs";
 import { js_function_declaration_asyncify } from "../../../love/public/src/js_function_declaration_asyncify.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
-export async function js_declaration_asyncify_params_from(
+export async function js_function_declaration_asyncify_params_from(
   ast,
   declaration_call,
 ) {
