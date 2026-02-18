4479697fedf557052b98632d30602593f98e60d0
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:59:51 -0500

    pf function_replace_rename_multiple_generic f_names

--- public/src/function_replace_rename_multiple_generic.mjs
+++ public/src/function_replace_rename_multiple_generic.mjs
@@ -2,9 +2,9 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 import { text_includes_not } from "../../../love/public/src/text_includes_not.mjs";
 export async function function_replace_rename_multiple_generic(
+  f_names,
   from,
   to,
-  f_names,
 ) {
   async function lambda(f_name) {
     let n = text_includes_not(f_name, from);
