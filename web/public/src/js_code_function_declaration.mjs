20e7c727bb850d1e73b3ab0fc45ab4e5811866c5
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:52:07 -0500

    function_auto_path ..\love\public\src\js_code_function_declaration.mjs

--- web/public/src/js_code_function_declaration.mjs
+++ web/public/src/js_code_function_declaration.mjs
@@ -1,5 +1,5 @@
 import { js_code_declaration_args } from "../../../love/public/src/js_code_declaration_args.mjs";
-export function js_code_declaration(f_name, inside, async_is) {
+export function js_code_function_declaration(f_name, inside, async_is) {
   let args = [];
   let code = js_code_declaration_args(async_is, f_name, args, inside);
   return code;
