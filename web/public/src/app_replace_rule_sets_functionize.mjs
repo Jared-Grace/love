d1ea999986a760e0827b5942494c3dfc6158ced2
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:05:23 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -16,6 +16,7 @@ export async function app_replace_rule_sets_functionize() {
     function lambda2(e) {
       let properties = property_get(e, "properties");
       function lambda3(p) {
+        let key = property_get(p, "key");
         let ii = js_identifier_is(p);
         let name2 = js_identifier_name(i);
       }
