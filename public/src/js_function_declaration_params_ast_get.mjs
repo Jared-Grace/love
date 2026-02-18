1c23ce1a0b41e83dc403843e6596ec837a941a5f
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:07:39 -0500

    function_auto_path ..\love\public\src\js_declaration_param_add_node.mjs

--- public/src/js_function_declaration_params_ast_get.mjs
+++ public/src/js_function_declaration_params_ast_get.mjs
@@ -1,8 +1,8 @@
-import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
+import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 export function js_function_declaration_params_ast_get(ast) {
   let declaration = js_declaration_single(ast);
-  let params = js_declaration_params_get(declaration);
+  let params = js_function_declaration_params_get(declaration);
   let r = {
     declaration,
     params,
