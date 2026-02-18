58733e9171fc7aa495ba4da5fa63177c44d10eed
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:54:55 -0500

    function_auto_path ..\love\public\src\functions_rename_suffix_add.mjs

--- web/public/src/functions_rename_suffix_add.mjs
+++ web/public/src/functions_rename_suffix_add.mjs
@@ -1,5 +1,10 @@
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_rename_suffix_add } from "../../../love/public/src/function_rename_suffix_add.mjs";
-export async function functions_rename_suffix_add(f_name_before, suffix) {
+export async function functions_rename_suffix_add(
+  f_name_befores_comma,
+  suffix,
+) {
+  let split = text_split_comma(t);
   let r = await function_rename_suffix_add(f_name_before, suffix);
   return r;
 }
