62e61c8db6dcfe87acb7a5cc3da9627c09568a4d
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:26:20 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -1,4 +1,4 @@
-import { js_literal_is_assert } from "../../../love/public/src/js_literal_is_assert.mjs";
+import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 import { js_identifier_named_try } from "../../../love/public/src/js_identifier_named_try.mjs";
 import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -24,8 +24,7 @@ export async function app_replace_rule_sets_functionize() {
       }
       let found = list_find(properties, lambda3);
       let value = property_get(found, "value");
-      js_literal_is_assert(value);
-      let value2 = property_get(value, "value");
+      let value2 = js_literal_value_get(value);
       let replaced = text_replace_space_underscore_lower(value2);
       log(replaced);
     }
