524c9a4a3f7f2e72bd5f0faabc2cc7c7eb2f3bd1
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:13:22 -0500

    function_auto_path ..\love\public\src\app_bible_home_generic.mjs

---
+++ public/src/text_replace_space_underscore_lower.mjs
@@ -0,0 +1,7 @@
+import { text_replace_space_underscore } from "../../../love/public/src/text_replace_space_underscore.mjs";
+import { text_lower_to } from "../../../love/public/src/text_lower_to.mjs";
+export function text_replace_space_underscore_lower(book_name) {
+  let lower = text_lower_to(book_name);
+  let replaced = text_replace_space_underscore(lower);
+  return replaced;
+}
