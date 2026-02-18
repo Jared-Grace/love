655a50df52646e04f5b661c6d32172f453be00a0
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:58:57 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- public/src/app_replace_rule_sets_functionize.mjs
+++ public/src/app_replace_rule_sets_functionize.mjs
@@ -1,5 +1,5 @@
+import { marker_next_declare_single_init_elements } from "../../../love/public/src/marker_next_declare_single_init_elements.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { marker_next_get } from "../../../love/public/src/marker_next_get.mjs";
 import { function_transform_marker_specified } from "../../../love/public/src/function_transform_marker_specified.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
@@ -7,7 +7,7 @@ export async function app_replace_rule_sets_functionize() {
   let f_name = app_replace_rule_sets.name;
   let code = await function_transform_marker_specified(f_name, "rules", lambda);
   async function lambda(a) {
-    let r = marker_next_get(a);
+    let r = marker_next_declare_single_init_elements(a);
     log({
       r,
     });
