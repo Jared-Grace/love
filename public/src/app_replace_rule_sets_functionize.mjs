267e831ba403698e918a101ead2bca0c89cb8246
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:48:30 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- public/src/app_replace_rule_sets_functionize.mjs
+++ public/src/app_replace_rule_sets_functionize.mjs
@@ -1,10 +1,9 @@
+import { js_statement_return_empty_add_argument_set } from "../../../love/public/src/js_statement_return_empty_add_argument_set.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
 import { js_call_empty } from "../../../love/public/src/js_call_empty.mjs";
 import { app_replace_rule_sets_functionize_name } from "../../../love/public/src/app_replace_rule_sets_functionize_name.mjs";
 import { function_new_declaration_from } from "../../../love/public/src/function_new_declaration_from.mjs";
-import { js_return_argument_set } from "../../../love/public/src/js_return_argument_set.mjs";
 import { log_unparse } from "../../../love/public/src/log_unparse.mjs";
-import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
 import { js_function_declaration_to_block_body } from "../../../love/public/src/js_function_declaration_to_block_body.mjs";
 import { function_new_declaration_to } from "../../../love/public/src/function_new_declaration_to.mjs";
 import { marker_next_declare_single_init_elements } from "../../../love/public/src/marker_next_declare_single_init_elements.mjs";
@@ -21,8 +20,7 @@ export async function app_replace_rule_sets_functionize() {
       let f_name_new = app_replace_rule_sets_functionize_name(e);
       let declaration = function_new_declaration_to(f_name_new);
       let body_block = js_function_declaration_to_block_body(declaration);
-      let r = js_statement_return_empty_add(body_block);
-      js_return_argument_set(r, e);
+      js_statement_return_empty_add_argument_set(body_block, e);
       log_unparse(declaration);
       await function_new_declaration_from(declaration);
     }
