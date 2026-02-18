f45958ead73a547bd89bb91475c19cfafaed7000
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:24:54 -0500

    rs function_move open

--- public/src/function_move_open.mjs
+++ public/src/function_move_open.mjs
@@ -1,6 +1,6 @@
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
-export async function function_move(f_name_before, f_name_after) {
+export async function function_move_open(f_name_before, f_name_after) {
   await function_copy_open(f_name_before, f_name_after);
   await function_delete(f_name_before);
 }
