1fad1c0d46390e687bbb0d4ed3e02ebb29517a55
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:31:00 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_calls.mjs

--- public/src/app_replace_rule_sets_calls.mjs
+++ public/src/app_replace_rule_sets_calls.mjs
@@ -1,8 +1,10 @@
+import { firebase_deploy_function_app_main } from "../../../love/public/src/firebase_deploy_function_app_main.mjs";
 import { js_visit_calls } from "../../../love/public/src/js_visit_calls.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
-export function app_replace_rule_sets_calls() {
+export async function app_replace_rule_sets_calls() {
+  let v2 = await firebase_deploy_function_app_main(a_name);
   function lambda_inner(v) {
     let node = property_get(v, "node");
     let name = js_call_callee_name(node);
