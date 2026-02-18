51082a4598a3b0b266351f717081ad930e6797a8
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:45:04 -0500

    function_auto_path ..\love\public\src\function_alias_add.mjs

--- web/public/src/function_alias_add.mjs
+++ web/public/src/function_alias_add.mjs
@@ -14,7 +14,7 @@ export async function function_alias_add(first, second) {
   let expression = js_parse_expression(f_name);
   js_identifier_is_assert(expression);
   undefined_not_is_assert(f_name);
-  await function_exists_assert(f_name2);
+  await function_exists_assert(f_name);
   function lambda(a) {
     let unaliased = property_get(a, "unaliased");
     let aliases = property_get(a, "aliases");
