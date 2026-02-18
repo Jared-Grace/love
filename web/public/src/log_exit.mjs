6e72e91191186cfd5d41d0a80c3e714a7f32a07a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:46:08 -0500

    function_auto_path ..\love\public\src\log_exit.mjs

--- web/public/src/log_exit.mjs
+++ web/public/src/log_exit.mjs
@@ -1,4 +1,6 @@
+import { list_skip } from "../../../love/public/src/list_skip.mjs";
 import { exit } from "../../../love/public/src/exit.mjs";
 export function log_exit(v) {
+  let skipped = list_skip(list, skip_count);
   exit();
 }
