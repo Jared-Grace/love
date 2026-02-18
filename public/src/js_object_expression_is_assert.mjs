b28276ab45cb2ffd83bd2a52610fdac37f6cf514
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:35:49 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

---
+++ public/src/js_object_expression_is_assert.mjs
@@ -0,0 +1,4 @@
+import { js_node_type_is_assert } from "../../../love/public/src/js_node_type_is_assert.mjs";
+export function js_object_expression_is_assert(e) {
+  js_node_type_is_assert(e, "ObjectExpression");
+}
