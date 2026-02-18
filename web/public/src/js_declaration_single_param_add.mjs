4a37df13bb4681699de836a22cae7e22f9750e06
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:09:32 -0500

    function_auto_path ..\love\public\src\js_function_declaration_single_param_add.mjs

--- web/public/src/js_declaration_single_param_add.mjs
+++
@@ -1,4 +0,0 @@
-import { js_function_declaration_single_params_add } from "../../../love/public/src/js_function_declaration_single_params_add.mjs";
-export function js_declaration_single_param_add(ast, list) {
-  js_function_declaration_single_params_add(ast, [list]);
-}
