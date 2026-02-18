2e0d0c73b10d0eedb3e9eaeafc2bb2d4f2f2f866
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:19:53 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize_name.mjs

--- web/public/src/app_replace_rule_sets_functionize_name.mjs
+++ web/public/src/app_replace_rule_sets_functionize_name.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
@@ -9,6 +10,7 @@ export function app_replace_rule_sets_functionize_name(e) {
   let p = js_property_value_get(found);
   let l = js_literal_value_get(p);
   let f_name_new = text_replace_space_underscore_lower(l);
+  app_replace_rule_sets.name;
   let combined = function_name_combine(left, f_name_new);
   return f_name_new;
 }
