b89adb6644dec4187910071740e3841e2b9f4739
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:23:41 -0500

    function_auto_path ..\love\public\src\list_size_1_assert.mjs

--- web/public/src/list_size_1_assert.mjs
+++ web/public/src/list_size_1_assert.mjs
@@ -1,10 +1,6 @@
 import { list_size_1_assert_message } from "../../../love/public/src/list_size_1_assert_message.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-import { json_to } from "./json_to.mjs";
 export function list_size_1_assert(list) {
   let size = list_size(list);
-  let json = json_to({
-    size,
-  });
-  list_size_1_assert_message(list, json);
+  list_size_1_assert_message(list, {});
 }
