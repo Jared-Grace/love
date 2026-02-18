b015dc9515d3fffca02e9bc0043ef0b47795f3e9
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:57:16 -0500

    function_auto_path ..\love\public\src\marker_next.mjs

--- public/src/marker_next.mjs
+++ public/src/marker_next.mjs
@@ -1,3 +1,4 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { marker_next_get } from "../../../love/public/src/marker_next_get.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
@@ -8,8 +9,10 @@ export async function marker_next() {
   async function lambda2(la) {
     await function_transform_marker(f_name, lambda);
     async function lambda(a) {
-      let { next } = marker_next_get(a);
-      la(await js_unparse(next));
+      let r = marker_next_get(a);
+      let next = property_get(r, "next");
+      let code = js_unparse(next);
+      la(code);
     }
   }
   let list = await list_adder_async(lambda2);
