c944436c2936ee9239a91074576eca21218383c1
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:57:43 -0500

    cur js_function_declaration_single_block_body_empty_return_identifiers

---
+++ public/src/js_function_declaration_single_block_body_empty_return_identifiers_curried_right.mjs
@@ -0,0 +1,13 @@
+import { js_function_declaration_single_block_body_empty_return_identifiers } from "../../../love/public/src/js_function_declaration_single_block_body_empty_return_identifiers.mjs";
+export function js_function_declaration_single_block_body_empty_return_identifiers_curried_right(
+  names,
+) {
+  return function js_function_declaration_single_block_body_empty_return_identifiers_curried_right_result(
+    ast,
+  ) {
+    return js_function_declaration_single_block_body_empty_return_identifiers(
+      ast,
+      names,
+    );
+  };
+}
