89f7dd51a320b21d0a8c48a791c5c542885be919
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:24:19 -0500

    rs function_copy open

--- web/public/src/function_copy_part_insert_at.mjs
+++ web/public/src/function_copy_part_insert_at.mjs
@@ -1,5 +1,5 @@
 import { function_name_part_insert_at } from "../../../love/public/src/function_name_part_insert_at.mjs";
-import { function_copy } from "./function_copy.mjs";
+import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 export async function function_copy_part_insert_at(
   f_name_before,
   index_string,
@@ -10,5 +10,5 @@ export async function function_copy_part_insert_at(
     index_string,
     part,
   );
-  await function_copy(f_name_before, f_name_after);
+  await function_copy_open(f_name_before, f_name_after);
 }
