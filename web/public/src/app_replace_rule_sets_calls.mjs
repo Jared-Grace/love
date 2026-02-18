cd5190c239ecc445716464464e389c57a9048d63
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:35:27 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_calls.mjs

--- web/public/src/app_replace_rule_sets_calls.mjs
+++ web/public/src/app_replace_rule_sets_calls.mjs
@@ -5,14 +5,9 @@ import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_rule_sets_v_1 } from "../../../love/public/src/app_replace_rule_sets_v_1.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { js_visit_calls } from "../../../love/public/src/js_visit_calls.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
 export async function app_replace_rule_sets_calls() {
   let ast = await function_ast(app_replace_rule_sets_v_1.name);
-  function lambda_inner(v) {
-    let node = property_get(v, "node");
-    let name = js_call_callee_name(node);
-  }
   js_visit_calls(ast, lambda_inner);
   let vs = js_list_calls(ast);
   let mapped = list_map_property(vs, "node");
