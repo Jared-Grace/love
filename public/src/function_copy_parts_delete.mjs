1d9147d82f46c85f420a7df21f10fdb3c4fa052a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:24:18 -0500

    function_auto_path ..\love\public\src\function_copy_open.mjs

--- public/src/function_copy_parts_delete.mjs
+++ public/src/function_copy_parts_delete.mjs
@@ -1,7 +1,7 @@
 import { function_name_parts_remove_comma } from "../../../love/public/src/function_name_parts_remove_comma.mjs";
-import { function_copy } from "../../../love/public/src/function_copy.mjs";
+import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 export async function function_copy_parts_delete(f_name_old, deleted) {
   let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
-  let v = await function_copy(f_name_old, f_name_new);
+  let v = await function_copy_open(f_name_old, f_name_new);
   return v;
 }
