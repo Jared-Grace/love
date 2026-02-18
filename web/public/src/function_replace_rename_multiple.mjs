9c68f0fa6b9fd82db942f92901ca56f65496c268
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:59:51 -0500

    pf function_replace_rename_multiple_generic f_names

--- web/public/src/function_replace_rename_multiple.mjs
+++ web/public/src/function_replace_rename_multiple.mjs
@@ -2,5 +2,5 @@ import { function_replace_rename_multiple_generic } from "../../../love/public/s
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function function_replace_rename_multiple(from, to) {
   let f_names = await functions_names();
-  await function_replace_rename_multiple_generic(from, to, f_names);
+  await function_replace_rename_multiple_generic(f_names, from, to);
 }
