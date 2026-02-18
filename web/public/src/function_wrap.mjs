d919fcee7311e10e75beb71daec0dd96c71719cf
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:06:56 -0500

    function_auto_path ..\love\public\src\function_wrap.mjs

--- web/public/src/function_wrap.mjs
+++ web/public/src/function_wrap.mjs
@@ -1,7 +1,7 @@
 import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs";
 import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_declaration_asyncify_params_from } from "../../../love/public/src/js_declaration_asyncify_params_from.mjs";
+import { js_function_declaration_asyncify_params_from } from "../../../love/public/src/js_function_declaration_asyncify_params_from.mjs";
 import { js_call_args_await_maybe_declaration_return_add } from "../../../love/public/src/js_call_args_await_maybe_declaration_return_add.mjs";
 import { js_declaration_params_names } from "../../../love/public/src/js_declaration_params_names.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
@@ -20,6 +20,6 @@ export async function function_wrap(f_name, f_name_wrapped) {
       declaration_call,
       ast,
     );
-    await js_declaration_asyncify_params_from(ast, declaration_call);
+    await js_function_declaration_asyncify_params_from(ast, declaration_call);
   }
 }
