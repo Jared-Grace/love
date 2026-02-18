35690d73293ddb8d264d78e7661780a43c115920
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:07:39 -0500

    function_auto_path ..\love\public\src\js_declaration_param_add_node.mjs

--- public/src/js_assert_arguments_args.mjs
+++ public/src/js_assert_arguments_args.mjs
@@ -1,6 +1,6 @@
 import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-import { js_declaration_params_get } from "../../../love/public/src/js_declaration_params_get.mjs";
+import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
 import { js_declaration_single } from "../../../love/public/src/js_declaration_single.mjs";
 import { js_special_arguments } from "../../../love/public/src/js_special_arguments.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
@@ -8,7 +8,7 @@ import { assert_arguments } from "../../../love/public/src/assert_arguments.mjs"
 import { js_visit_calls_named } from "../../../love/public/src/js_visit_calls_named.mjs";
 export function js_assert_arguments_args(ast) {
   let declaration = js_declaration_single(ast);
-  let params = js_declaration_params_get(declaration);
+  let params = js_function_declaration_params_get(declaration);
   let size = list_size(params);
   function lambda2({ args }) {
     let code_expression = js_special_arguments();
