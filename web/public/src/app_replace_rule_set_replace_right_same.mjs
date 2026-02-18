2998dedb8967e39773654546a6fb9c79d4b998b8
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:23:14 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set_replace_right_same.mjs

--- web/public/src/app_replace_rule_set_replace_right_same.mjs
+++ web/public/src/app_replace_rule_set_replace_right_same.mjs
@@ -1,5 +1,5 @@
 export function app_replace_rule_set_replace_right_same() {
-  return {
+  let r = {
     name: "Replace Right Same",
     rules: ["a b > a a"],
     goals: [
@@ -17,4 +17,5 @@ export function app_replace_rule_set_replace_right_same() {
       },
     ],
   };
+  return r;
 }
