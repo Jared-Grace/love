bbac6c920169b47bdccbe66a247dde30ddc9dad2
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:12:54 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- public/src/app_replace_rule_sets_functionize.mjs
+++ public/src/app_replace_rule_sets_functionize.mjs
@@ -26,7 +26,7 @@ export async function app_replace_rule_sets_functionize() {
       let found = list_find(properties, lambda3);
       let value = property_get(found, "value");
       let value2 = property_get(value, "value");
-      let replaced2 = text_replace_space_underscore(name_old);
+      let replaced2 = text_replace_space_underscore(value2);
       log(value2);
     }
     each(elements, lambda2);
