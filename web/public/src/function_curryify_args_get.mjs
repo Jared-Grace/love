bd2ca95cff926540a12d42ce55fb5af414109aff
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:56:22 -0500

    function_auto_path ..\love\public\src\function_curryify_args_get.mjs

--- web/public/src/function_curryify_args_get.mjs
+++ web/public/src/function_curryify_args_get.mjs
@@ -1,7 +1,7 @@
+import { list_first_remaining_get } from "../../../love/public/src/list_first_remaining_get.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
-import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 export function function_curryify_args_get(arg_names) {
-  let fn = list_first_remaining;
+  let fn = list_first_remaining_get;
   let r = function_curryify_args_get_generic(fn, arg_names);
   return r;
 }
