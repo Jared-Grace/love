853cdd6c0720108779df1f2ef21f0efd3c7026d7
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:09:35 -0500

    function_auto_path ..\love\public\src\function_multiplize.mjs

---
+++ web/public/src/js_function_declaration_single_path.mjs
@@ -0,0 +1,7 @@
+import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
+import { js_function_declaration_single_name } from "../../../love/public/src/js_function_declaration_single_name.mjs";
+export function js_function_declaration_single_path(ast) {
+  let f_name = js_function_declaration_single_name(ast);
+  let f_path = function_name_to_path(f_name);
+  return f_path;
+}
