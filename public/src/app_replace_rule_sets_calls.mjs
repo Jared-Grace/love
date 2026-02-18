a7b7a75384dbcbb80787ca9bf034bd2f0b62148e
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:34:53 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_calls.mjs

--- public/src/app_replace_rule_sets_calls.mjs
+++ public/src/app_replace_rule_sets_calls.mjs
@@ -19,6 +19,5 @@ export async function app_replace_rule_sets_calls() {
   js_visit_calls(ast, lambda_inner);
   let vs = js_list_calls(ast);
   let mapped = list_map_property(vs, "node");
-  function lambda(item) {}
-  let mapped2 = list_map(list, lambda);
+  let mapped2 = list_map(list, js_call_callee_name);
 }
