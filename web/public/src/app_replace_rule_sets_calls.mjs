a883cf87707dc297f075b9b3212143819f431266
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:35:58 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_calls.mjs

--- web/public/src/app_replace_rule_sets_calls.mjs
+++ web/public/src/app_replace_rule_sets_calls.mjs
@@ -1,6 +1,5 @@
-import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
+import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_rule_sets_v_1 } from "../../../love/public/src/app_replace_rule_sets_v_1.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
@@ -8,8 +7,7 @@ import { js_call_callee_name } from "../../../love/public/src/js_call_callee_nam
 export async function app_replace_rule_sets_calls() {
   let ast = await function_ast(app_replace_rule_sets_v_1.name);
   const node_type = "CallExpression";
-  let vs = js_list_type(ast, node_type);
-  let mapped = list_map_property(vs, "node");
+  let mapped = js_list_type_nodes(ast, node_type);
   let names = list_map(mapped, js_call_callee_name);
   log({
     names,
