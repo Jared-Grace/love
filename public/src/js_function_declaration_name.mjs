53793032e00482015d13ea4065160d28d57b001e
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:06:56 -0500

    function_auto_path ..\love\public\src\function_wrap.mjs

---
+++ public/src/js_function_declaration_name.mjs
@@ -0,0 +1,6 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export function js_function_declaration_name(declaration) {
+  let declaration_id = property_get(declaration, "id");
+  let name = property_get(declaration_id, "name");
+  return name;
+}
