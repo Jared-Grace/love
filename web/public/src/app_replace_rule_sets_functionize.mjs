30e1b5523af64c14c068661c91cb80229c15bae7
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 13:04:35 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -1,3 +1,4 @@
+import { js_identifier_is } from "../../../love/public/src/js_identifier_is.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -13,7 +14,9 @@ export async function app_replace_rule_sets_functionize() {
     let elements = marker_next_declare_single_init_elements(a);
     function lambda2(e) {
       let properties = property_get(e, "properties");
-      function lambda3(p) {}
+      function lambda3(p) {
+        let ii = js_identifier_is(node);
+      }
       let found = list_find(properties, lambda3);
       log({
         properties,
