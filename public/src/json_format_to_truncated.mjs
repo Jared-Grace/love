8ca30d177832df2e5a71167481fa5a5f1f337691
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:34:54 -0500

    function_auto_path ..\love\public\src\json_format_to_truncated.mjs

--- public/src/json_format_to_truncated.mjs
+++ public/src/json_format_to_truncated.mjs
@@ -1,6 +1,7 @@
+import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
 import { text_take } from "../../../love/public/src/text_take.mjs";
 export function json_format_to_truncated(object) {
-  let j = JSON.stringify(object, null, 1);
+  let j = json_format_to(object);
   let taken = text_take(j, 10000);
   return taken;
 }
