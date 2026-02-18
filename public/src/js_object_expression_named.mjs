d55a7822846ea9aa0a8ae382409b22c3a1e87a31
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 12:38:11 -0500

    pf js_object_expression_named_generic ast

--- public/src/js_object_expression_named.mjs
+++ public/src/js_object_expression_named.mjs
@@ -1,6 +1,6 @@
 import { js_object_expression_named_generic } from "../../../love/public/src/js_object_expression_named_generic.mjs";
 export function js_object_expression_named(ast, search) {
   let node_type = "ObjectExpression";
-  let list = js_object_expression_named_generic(search, ast, node_type);
+  let list = js_object_expression_named_generic(ast, search, node_type);
   return list;
 }
