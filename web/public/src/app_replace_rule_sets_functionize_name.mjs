8512441fdd8a4c93c239e322f7eb6b5c02f65bf8
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:19:32 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize_name.mjs

--- web/public/src/app_replace_rule_sets_functionize_name.mjs
+++ web/public/src/app_replace_rule_sets_functionize_name.mjs
@@ -9,6 +9,6 @@ export function app_replace_rule_sets_functionize_name(e) {
   let p = js_property_value_get(found);
   let l = js_literal_value_get(p);
   let f_name_new = text_replace_space_underscore_lower(l);
-  let combined = function_name_combine(left, right);
+  let combined = function_name_combine(left, f_name_new);
   return f_name_new;
 }
