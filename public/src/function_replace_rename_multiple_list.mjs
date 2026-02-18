7d38b86a07a10063195d1c2ef2b67f92aebe1757
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 15:00:47 -0500

    function_auto_path ..\love\public\src\function_replace_rename_multiple_list.mjs

--- public/src/function_replace_rename_multiple_list.mjs
+++ public/src/function_replace_rename_multiple_list.mjs
@@ -1,5 +1,11 @@
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_replace_rename_multiple_generic } from "../../../love/public/src/function_replace_rename_multiple_generic.mjs";
-export async function function_replace_rename_multiple_list(f_names, from, to) {
+export async function function_replace_rename_multiple_list(
+  f_names_comma,
+  from,
+  to,
+) {
+  let split = text_split_comma(t);
   let r = await function_replace_rename_multiple_generic(f_names, from, to);
   return r;
 }
