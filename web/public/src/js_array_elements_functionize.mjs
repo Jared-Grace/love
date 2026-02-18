c0e49dbbd38bd50f21cf3a95fb80e3c8d930ffb3
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 11:57:21 -0500

    function_auto_path ..\love\public\src\js_array_elements_functionize.mjs

--- web/public/src/js_array_elements_functionize.mjs
+++ web/public/src/js_array_elements_functionize.mjs
@@ -1,4 +1,4 @@
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 export function js_array_elements_functionize(ast) {
-  let elements = js_array_expression_single_elements(ast2);
+  let elements = js_array_expression_single_elements(ast);
 }
