717bd0e86b27bb83393ed9fc6b2691931e25b5ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 20:24:04 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets.mjs

--- public/src/app_replace_rule_sets.mjs
+++ public/src/app_replace_rule_sets.mjs
@@ -643,32 +643,12 @@ export function app_replace_rule_sets() {
       rules: ["1 a > a 0", "1 > 1 1"],
       goals: [
         {
-          start: "b0c",
-          end: "b0ac",
+          start: "1a",
+          end: "1a0",
         },
         {
-          start: "b0c",
-          end: "b1c",
-        },
-        {
-          start: "b0c",
-          end: "b10c",
-        },
-        {
-          start: "b0c",
-          end: "b11c",
-        },
-        {
-          start: "b0c",
-          end: "b101c",
-        },
-        {
-          start: "b0c",
-          end: "b111c",
-        },
-        {
-          start: "b0c",
-          end: "b1010c",
+          start: "1a",
+          end: "a000",
         },
       ],
     },
