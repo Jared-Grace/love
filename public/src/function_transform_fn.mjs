ffb69e8299f9a183c28a84fafd6160682425e7b3
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 18:58:46 -0500

    pnef function_transform_fn fn

--- public/src/function_transform_fn.mjs
+++ public/src/function_transform_fn.mjs
@@ -1,6 +1,6 @@
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-export async function function_transform_fn(lambda) {
+export async function function_transform_fn(fn, lambda) {
   let r = await function_transform(app_replace_rule_sets.name, lambda);
   return r;
 }
