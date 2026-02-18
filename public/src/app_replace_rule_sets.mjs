fd60889cdb696994f82a97a78919df8029be7554
Grace-Jared <christrosetolife@gmail.com>
2026-02-17 20:07:31 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets.mjs

--- public/src/app_replace_rule_sets.mjs
+++ public/src/app_replace_rule_sets.mjs
@@ -533,6 +533,36 @@ export function app_replace_rule_sets() {
       ],
     },
     {
+      name: "Grow Left Pair Change Right",
+      rules: ["= > 1 =", "1 1 > 1 +"],
+      goals: [
+        {
+          start: "1=1",
+          end: "11=11",
+        },
+        {
+          start: "1=1",
+          end: "11=1+1",
+        },
+        {
+          start: "1=1",
+          end: "1+1=11",
+        },
+        {
+          start: "1=1",
+          end: "1+11=111",
+        },
+        {
+          start: "1=1",
+          end: "1+11=11+1",
+        },
+        {
+          start: "1=1",
+          end: "1+1+111=11111",
+        },
+      ],
+    },
+    {
       name: "Unary Equations Adding",
       rules: ["= > 1 = 1", "1 1 > 1 + 1"],
       goals: [
