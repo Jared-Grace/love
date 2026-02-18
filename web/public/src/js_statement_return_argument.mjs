e172500b543ab04cc6bb0484619208b99cedbc9a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:39:56 -0500

    function_auto_path ..\love\public\src\js_statement_return_argument.mjs

--- web/public/src/js_statement_return_argument.mjs
+++ web/public/src/js_statement_return_argument.mjs
@@ -1,7 +1,7 @@
-import { property_set } from "../../../love/public/src/property_set.mjs";
+import { js_return_argument_set } from "../../../love/public/src/js_return_argument_set.mjs";
 import { js_statement_return_empty } from "../../../love/public/src/js_statement_return_empty.mjs";
 export function js_statement_return_argument(argument) {
   let r = js_statement_return_empty();
-  property_set(r, "argument", argument);
+  js_return_argument_set(r, argument);
   return r;
 }
