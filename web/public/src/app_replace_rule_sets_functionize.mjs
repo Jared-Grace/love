d3c57fba84ecdf2652dfcb76f7d2857cdcb41801
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 14:33:10 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets_functionize.mjs

--- web/public/src/app_replace_rule_sets_functionize.mjs
+++ web/public/src/app_replace_rule_sets_functionize.mjs
@@ -1,3 +1,4 @@
+import { js_code_function_declaration } from "../../../love/public/src/js_code_function_declaration.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { js_property_value_get } from "../../../love/public/src/js_property_value_get.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
@@ -18,6 +19,7 @@ export async function app_replace_rule_sets_functionize() {
       let p = js_property_value_get(found);
       let l = js_literal_value_get(p);
       let replaced = text_replace_space_underscore_lower(l);
+      let code2 = js_code_function_declaration(f_name2, inside, async_is);
       log(e);
     }
     each(elements, lambda2);
