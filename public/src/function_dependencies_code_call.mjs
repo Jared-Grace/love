a59d7ea3807d8c8f611ccc6b35eab21d4cdf484e
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:47:24 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,5 +1,4 @@
 import { property_change } from "../../../love/public/src/property_change.mjs";
-import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
 import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
@@ -8,12 +7,13 @@ export async function function_dependencies_code_call(f_name) {
   let global_init = js_code_global_init();
   let r = await function_dependencies_code_multiple([f_name]);
   let dependencies = property_get(r, "code");
-  function lambda(value) {}
-  let value2 = property_change(o, property, lambda);
-  let call = js_code_call_statement(f_name);
-  const code = `${global_init}
+  function lambda(dependencies) {
+    let call = js_code_call_statement(f_name);
+    const code = `${global_init}
     ${dependencies}
     ${call}`;
-  property_set(r, "code", code);
+    return code;
+  }
+  let value2 = property_change(o, "code", lambda);
   return r;
 }
