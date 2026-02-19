3e0b33a011b92b1f2b60cc7a97b00da5c1749156
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:12 -0500

    function_auto_path ..\love\public\src\js_return_name.mjs

--- public/src/js_function_declaration_single_block_body_add.mjs
+++
@@ -1,6 +0,0 @@
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { js_flo_block_body } from "../../../love/public/src/js_flo_block_body.mjs";
-export function js_function_declaration_single_block_body_add(ast, item) {
-  let body_block = js_flo_block_body(ast);
-  list_add(body_block, item);
-}
