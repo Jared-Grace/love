0dc0294f65a28860736c699f222aa17807e0efa2
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:26:48 -0500

    ri ternary_equal change_if_equal

--- public/src/function_name_part_replace.mjs
+++ public/src/function_name_part_replace.mjs
@@ -1,11 +1,11 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
-import { ternary_equal_curried_right } from "../../../love/public/src/ternary_equal_curried_right.mjs";
+import { change_if_equal_curried_right } from "../../../love/public/src/change_if_equal_curried_right.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_part_replace(f_name, from, to) {
   let parts = function_name_to_parts(f_name);
-  let r = ternary_equal_curried_right(from, to);
+  let r = change_if_equal_curried_right(from, to);
   let mapped = list_map(parts, r);
   let combined = function_name_combine_multiple(mapped);
   log({
