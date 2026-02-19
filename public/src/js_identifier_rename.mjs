bc13b2e183f0ae89113cdd5c00bf7dfe77f2fa75
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:39:47 -0500

    function_auto_path ..\love\public\src\js_identifier_rename.mjs

--- public/src/js_identifier_rename.mjs
+++ public/src/js_identifier_rename.mjs
@@ -1,20 +1,12 @@
 import { property_set_if_equals_curried_right_3 } from "../../../love/public/src/property_set_if_equals_curried_right_3.mjs";
 import { js_visit_nodes_lambda } from "../../../love/public/src/js_visit_nodes_lambda.mjs";
-import { property_set_if_equals } from "../../../love/public/src/property_set_if_equals.mjs";
 import { js_visit_identifiers } from "../../../love/public/src/js_visit_identifiers.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 export function js_identifier_rename(ast, name_from, name_to) {
   if (equal(name_from, name_to)) {
     return;
   }
-  let r2 = property_set_if_equals_curried_right_3(
-    property_name,
-    name_from2,
-    name_to2,
-  );
-  function lambda2(node) {
-    property_set_if_equals(node, "name", name_from, name_to);
-  }
-  let lambda = js_visit_nodes_lambda(lambda2);
+  let r2 = property_set_if_equals_curried_right_3("name", name_from, name_to);
+  let lambda = js_visit_nodes_lambda(r2);
   js_visit_identifiers(ast, lambda);
 }
