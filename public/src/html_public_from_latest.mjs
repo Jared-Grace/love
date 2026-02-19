6d010f7cf80b2a43645bb31b07e8c5d6fef4a9c1
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:25:12 -0500

    function_auto_path ..\love\public\src\html_public_from_latest.mjs

--- public/src/html_public_from_latest.mjs
+++ public/src/html_public_from_latest.mjs
@@ -1,7 +1,7 @@
 import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
-export async function html_public_promote(search) {
+export async function html_public_from_latest(search) {
   let info = await app_shared_name_search_info(search);
   let f_path_latest = property_get(info, "f_path_latest");
   let f_path = property_get(info, "f_path");
