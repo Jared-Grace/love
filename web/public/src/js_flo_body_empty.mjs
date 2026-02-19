d615cf42ee424a3154208433bf23385e6f6eb377
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:06:26 -0500

    function_auto_path ..\love\public\src\js_flo_body_add_return.mjs

--- web/public/src/js_flo_body_empty.mjs
+++ web/public/src/js_flo_body_empty.mjs
@@ -1,6 +1,6 @@
 import { list_empty } from "../../../love/public/src/list_empty.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
-export function js_flo_block_body_empty(ast) {
+export function js_flo_body_empty(ast) {
   let body_block = js_flo_body(ast);
   list_empty(body_block);
   return body_block;
