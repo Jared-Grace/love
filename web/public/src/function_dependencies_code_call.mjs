a871a0349cdb0c259f28620809a08f6cce13572f
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:08:41 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- web/public/src/function_dependencies_code_call.mjs
+++ web/public/src/function_dependencies_code_call.mjs
@@ -2,7 +2,8 @@ import { app_shared_code_run } from "../../../love/public/src/app_shared_code_ru
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { function_dependencies_code_multiple } from "./function_dependencies_code_multiple.mjs";
 export async function function_dependencies_code_call(f_name) {
-  let r = await function_dependencies_code_multiple([f_name]);
+  const f_names = [f_name];
+  let r = await function_dependencies_code_multiple(f_names);
   function lambda(dependencies) {
     let code = app_shared_code_run(f_name, dependencies);
     return code;
