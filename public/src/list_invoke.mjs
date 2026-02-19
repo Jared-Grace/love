732b5a0b2895720722b05a1da3696cdb63315eb0
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:22:12 -0500

    function_auto_path ..\love\public\src\list_invoke.mjs

--- public/src/list_invoke.mjs
+++ public/src/list_invoke.mjs
@@ -1,6 +1,6 @@
 import { invoke } from "../../../love/public/src/invoke.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_invoke(taken) {
-  let mapped4 = list_map(taken, invoke);
-  return mapped4;
+  let mapped = list_map(taken, invoke);
+  return mapped;
 }
