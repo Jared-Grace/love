20b19ba3d656e5245660ef8a02049559527ac1f8
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:02:12 -0500

    function_auto_path ..\love\public\src\functions_rename_part.mjs

--- public/src/functions_rename_part.mjs
+++ public/src/functions_rename_part.mjs
@@ -1,4 +1,4 @@
 import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
 export async function functions_rename_part(from, to) {
-  let r = await functions_rename_generic(filter, name_change);
+  let r = await functions_rename_generic(() => {}, name_change);
 }
