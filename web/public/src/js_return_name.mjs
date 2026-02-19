2f10f5003ee10cce0af0c4f33826b8e976088261
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:11 -0500

    function_auto_path ..\love\public\src\js_function_declaration_single_block_body_add_return.mjs

--- web/public/src/js_return_name.mjs
+++ web/public/src/js_return_name.mjs
@@ -2,9 +2,9 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_return_on } from "../../../love/public/src/js_return_on.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
-import { js_function_declaration_single_block_body } from "../../../love/public/src/js_function_declaration_single_block_body.mjs";
+import { js_flo_block_body } from "../../../love/public/src/js_flo_block_body.mjs";
 export function js_return_name(ast) {
-  let body_block = js_function_declaration_single_block_body(ast);
+  let body_block = js_flo_block_body(ast);
   let return_name = null;
   if (list_empty_not_is(body_block)) {
     let last = list_last(body_block);
