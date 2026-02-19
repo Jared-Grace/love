4c3b4c760f3738e8074d72755dee77b322aa6003
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:37:09 -0500

    function_auto_path ..\love\public\src\js_identifier_rename.mjs

--- public/src/js_identifier_rename.mjs
+++ public/src/js_identifier_rename.mjs
@@ -2,13 +2,11 @@ import { js_visit_nodes_lambda } from "../../../love/public/src/js_visit_nodes_l
 import { property_set_if_equals } from "../../../love/public/src/property_set_if_equals.mjs";
 import { js_visit_identifiers } from "../../../love/public/src/js_visit_identifiers.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_identifier_rename(ast, name_from, name_to) {
   if (equal(name_from, name_to)) {
     return;
   }
   function lambda2(v) {
-    let node = property_get(v, "node");
     property_set_if_equals(node, "name", name_from, name_to);
   }
   let lambda = js_visit_nodes_lambda(lambda2);
