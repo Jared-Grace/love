db1ff874f3ebcbdc344c63e3ce82cbb04547f5e6
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:24:52 -0500

    function_auto_path ..\love\public\src\ternary_equal.mjs

--- public/src/ternary_equal.mjs
+++ public/src/ternary_equal.mjs
@@ -2,6 +2,6 @@ import { ternary } from "../../../love/public/src/ternary.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 export function ternary_equal(item, from, to) {
   let eq = equal(item, from);
-  let result = ternary(eq, item, to);
+  let result = ternary(eq, to, item);
   return result;
 }
