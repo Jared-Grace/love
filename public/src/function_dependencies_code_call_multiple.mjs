b204cf034bd2eaf3700468476d7286c5f252f88f
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 17:45:18 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call_multiple.mjs

--- public/src/function_dependencies_code_call_multiple.mjs
+++ public/src/function_dependencies_code_call_multiple.mjs
@@ -7,7 +7,7 @@ export async function function_dependencies_code_call_multiple(
   f_names_dependencies,
   call_code_get,
 ) {
-  list_add_if_not_includes(list, item);
+  list_add_if_not_includes(f_names_dependencies, f_name_call);
   let r = await function_dependencies_code_multiple(f_names_dependencies);
   function lambda(dependencies) {
     let code = app_shared_code_run(f_name_call, dependencies, call_code_get);
