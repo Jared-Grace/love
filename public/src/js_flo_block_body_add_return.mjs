fcbf9e8cefc8c758ade141ae60712d055b96fda7
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:18 -0500

    function_auto_path ..\love\public\src\app_shared_screen_add.mjs

--- public/src/js_flo_block_body_add_return.mjs
+++ public/src/js_flo_block_body_add_return.mjs
@@ -1,6 +1,6 @@
 import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
 import { js_flo_block_body } from "../../../love/public/src/js_flo_block_body.mjs";
-export function js_function_declaration_single_block_body_add_return(ast) {
+export function js_flo_block_body_add_return(ast) {
   let body_block = js_flo_block_body(ast);
   let r = js_statement_return_empty_add(body_block);
   return r;
