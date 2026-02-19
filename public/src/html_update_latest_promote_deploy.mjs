bc5f24f7664d71b27f0a1f87d8c7f03d10570998
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:40:26 -0500

    function_auto_path ..\love\public\src\html_update_latest_promote_deploy.mjs

--- public/src/html_update_latest_promote_deploy.mjs
+++ public/src/html_update_latest_promote_deploy.mjs
@@ -1,4 +1,5 @@
 import { html_update_latest_promote } from "../../../love/public/src/html_update_latest_promote.mjs";
 export async function html_update_latest_promote_deploy(search) {
-  return await html_update_latest_promote(search);
+  let r = await html_update_latest_promote(search);
+  return r;
 }
