2f5f719e8bca367311f14d88e23d3bec19249dd9
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:09:41 -0500

    pb function_dependencies_code_call_multiple

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,6 +1,6 @@
 import { function_dependencies_code_call_multiple } from "../../../love/public/src/function_dependencies_code_call_multiple.mjs";
 export async function function_dependencies_code_call(f_name) {
   const f_names = [f_name];
-  let r = await function_dependencies_code_call_multiple(f_names, f_name);
+  let r = await function_dependencies_code_call_multiple(f_name, f_names);
   return r;
 }
