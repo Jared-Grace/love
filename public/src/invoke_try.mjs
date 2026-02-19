c55299d8731a110513c592f83352c57fb7112508
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:19:58 -0500

    rrl2 invoke_if_function try

--- public/src/invoke_try.mjs
+++ public/src/invoke_try.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { function_is } from "../../../love/public/src/function_is.mjs";
 import { invoke } from "../../../love/public/src/invoke.mjs";
-export function invoke_if_function(fn) {
+export function invoke_try(fn) {
   let fi = function_is(fn);
   if (not(fi)) {
     return;
