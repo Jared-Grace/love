f886b0a7fd04139ca312e6c30c742e058aa24b97
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:35:31 -0500

    function_auto_path ..\love\public\src\function_dependencies_code.mjs

--- web/public/src/function_dependencies_code.mjs
+++ web/public/src/function_dependencies_code.mjs
@@ -1,5 +1,5 @@
 import { function_dependencies_code_multiple } from "./function_dependencies_code_multiple.mjs";
 export async function function_dependencies_code(f_name) {
-  let v = await function_dependencies_code_multiple([f_name]);
-  return v;
+  let r = await function_dependencies_code_multiple([f_name]);
+  return r;
 }
