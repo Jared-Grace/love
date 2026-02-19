c8f874cb92e2058a6de3c54198eb8016631e7011
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:11:35 -0500

    function_auto_path ..\love\public\src\function_name_part_replace_curried_right.mjs

--- web/public/src/function_name_part_replace_curried_right.mjs
+++ web/public/src/function_name_part_replace_curried_right.mjs
@@ -1,9 +1,11 @@
 import { function_name_part_replace } from "../../../love/public/src/function_name_part_replace.mjs";
 export function function_name_part_replace_curried_right(to) {
-  return function function_name_part_replace_curried_right_result(
+  let r = function function_name_part_replace_curried_right_result(
     f_name,
     from,
   ) {
-    return function_name_part_replace(f_name, from, to);
+    let mapped = function_name_part_replace(f_name, from, to);
+    return mapped;
   };
+  return r;
 }
