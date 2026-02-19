9d24af7958180579b397c10037020fc6c180a58a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:06:24 -0500

    function_auto_path ..\love\public\src\js_call_args_await_maybe_declaration_return_add.mjs

--- web/public/src/js_flo_body_add_return.mjs
+++ web/public/src/js_flo_body_add_return.mjs
@@ -1,6 +1,6 @@
 import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
-export function js_flo_block_body_add_return(ast) {
+export function js_flo_body_add_return(ast) {
   let body_block = js_flo_body(ast);
   let r = js_statement_return_empty_add(body_block);
   return r;
