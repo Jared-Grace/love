a566e9ba6eddf67b52d34cf33ff49bf2108d0a69
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:03:00 -0500

    function_auto_path ..\love\public\src\functions_rename_part.mjs

--- web/public/src/functions_rename_part.mjs
+++ web/public/src/functions_rename_part.mjs
@@ -1,7 +1,7 @@
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
 export async function functions_rename_part(from, to) {
-  function lambda(name) {
+  function lambda(f_name) {
     let parts = function_name_to_parts(f_name);
   }
   let r = await functions_rename_generic(lambda, name_change);
