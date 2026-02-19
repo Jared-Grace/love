42e912e24a08c750cf504056babc88dc178b2af4
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 17:45:10 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call_multiple.mjs

--- public/src/function_dependencies_code_call_multiple.mjs
+++ public/src/function_dependencies_code_call_multiple.mjs
@@ -1,3 +1,4 @@
+import { list_add_if_not_includes } from "../../../love/public/src/list_add_if_not_includes.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { app_shared_code_run } from "../../../love/public/src/app_shared_code_run.mjs";
 import { function_dependencies_code_multiple } from "../../../love/public/src/function_dependencies_code_multiple.mjs";
@@ -6,6 +7,7 @@ export async function function_dependencies_code_call_multiple(
   f_names_dependencies,
   call_code_get,
 ) {
+  list_add_if_not_includes(list, item);
   let r = await function_dependencies_code_multiple(f_names_dependencies);
   function lambda(dependencies) {
     let code = app_shared_code_run(f_name_call, dependencies, call_code_get);
