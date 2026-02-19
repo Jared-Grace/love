14aee20754e7bcd80366b439c96721c2c5b6e650
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:46:49 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,3 +1,4 @@
+import { property_change } from "../../../love/public/src/property_change.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
@@ -7,6 +8,7 @@ export async function function_dependencies_code_call(f_name) {
   let global_init = js_code_global_init();
   let r = await function_dependencies_code_multiple([f_name]);
   let dependencies = property_get(r, "code");
+  let value2 = property_change(o, property, function lambda(value) {});
   let call = js_code_call_statement(f_name);
   const code = `${global_init}
     ${dependencies}
