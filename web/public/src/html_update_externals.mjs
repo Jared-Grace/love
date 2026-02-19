053a831546eb530206636e4dde490e581391d50d
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:21:57 -0500

    function_auto_path ..\love\public\src\html_update_externals.mjs

--- web/public/src/html_update_externals.mjs
+++ web/public/src/html_update_externals.mjs
@@ -1,3 +1,4 @@
+import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { html_update_externals_dependencies } from "../../../love/public/src/html_update_externals_dependencies.mjs";
 export async function html_update_externals(f_name) {
   const f_names_dependencies = [f_name];
