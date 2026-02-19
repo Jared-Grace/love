c31e3a09affd13ecb98555dafe7ffb827c9abfc4
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:47:58 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -3,9 +3,9 @@ import { js_code_global_init } from "../../../karate_code/public/src/js_code_glo
 import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { function_dependencies_code_multiple } from "./function_dependencies_code_multiple.mjs";
 export async function function_dependencies_code_call(f_name) {
-  let global_init = js_code_global_init();
   let r = await function_dependencies_code_multiple([f_name]);
   function lambda(dependencies) {
+    let global_init = js_code_global_init();
     let call = js_code_call_statement(f_name);
     const code = `${global_init}
     ${dependencies}
