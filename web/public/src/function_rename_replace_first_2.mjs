d292e07396613d3e421b68e617e2001d1269372c
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:42:23 -0500

    function_auto_path ..\love\public\src\function_rename_replace_first_2.mjs

--- web/public/src/function_rename_replace_first_2.mjs
+++ web/public/src/function_rename_replace_first_2.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { function_name_to_part_first_2 } from "../../../love/public/src/function_name_to_part_first_2.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 export async function function_rename_replace_first_2(f_name_before, to) {
   let from = function_name_to_part_first_2(f_name_before);
+  log({
+    from,
+  });
   let v = await function_rename_replace(f_name_before, from, to);
   return v;
 }
