3d5a5e643726fcbe27691a5817dd5833b0f61825
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 17:54:55 -0500

    function_auto_path ..\love\public\src\html_update_public_new.mjs

--- public/src/html_update_public_new.mjs
+++ public/src/html_update_public_new.mjs
@@ -11,6 +11,7 @@ import { app_shared_name_search_info } from "../../../love/public/src/app_shared
 export async function html_update_public_new() {
   let search = "rce";
   let r = await app_shared_name_search_info(search);
+  let f_name = property_get(r, "f_name");
   let app_name = property_get(r, "app_name");
   let f_path = property_get(r, "f_path");
   let a_name = property_get(r, "a_name");
