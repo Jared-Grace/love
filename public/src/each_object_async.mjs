e5533c5a1cd5a5f59d12770b1998c6ea1ed07c27
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:56:03 -0500

    function_rename_inner object_map_async each_object_async

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,6 +1,6 @@
 import { object_map_generic_async } from "../../../love/public/src/object_map_generic_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-export async function object_map_async(object, lambda$value$key) {
+export async function each_object_async(object, lambda$value$key) {
   let each_lambda = each_async;
   await object_map_generic_async(object, lambda$value$key, each_lambda);
 }
