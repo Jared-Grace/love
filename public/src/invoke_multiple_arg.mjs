3d41dda1bdbdbab3d6c7fe7ac6799467c51f8397
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:21:44 -0500

    function_auto_path ..\love\public\src\invoke_multiple.mjs

--- public/src/invoke_multiple_arg.mjs
+++ public/src/invoke_multiple_arg.mjs
@@ -1,4 +1,4 @@
 import { invoke_multiple_args } from "../../../love/public/src/invoke_multiple_args.mjs";
-export function invoke_multiple_arg(on_keydowns, k) {
-  invoke_multiple_args(on_keydowns, [k]);
+export function invoke_multiple_arg(list, argument) {
+  invoke_multiple_args(list, [argument]);
 }
--- public/src/invoke_multiple_async.mjs
+++ public/src/invoke_multiple_async.mjs
@@ -1,6 +1,7 @@
 import { invoke } from "../../../love/public/src/invoke.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
+import { list_map_async } from "./list_map_async.mjs";
 export async function invoke_multiple_async(afters) {
-  let r = await each_async(afters, invoke);
+  let r = await list_map_async(afters, invoke);
   return r;
 }
