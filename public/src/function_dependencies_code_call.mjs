c87c762ca72551e7b6358afff7a99695f262d142
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:09:34 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,13 +1,6 @@
-import { app_shared_code_run } from "../../../love/public/src/app_shared_code_run.mjs";
-import { property_change } from "../../../love/public/src/property_change.mjs";
-import { function_dependencies_code_multiple } from "./function_dependencies_code_multiple.mjs";
+import { function_dependencies_code_call_multiple } from "../../../love/public/src/function_dependencies_code_call_multiple.mjs";
 export async function function_dependencies_code_call(f_name) {
   const f_names = [f_name];
-  let r = await function_dependencies_code_multiple(f_names);
-  function lambda(dependencies) {
-    let code = app_shared_code_run(f_name, dependencies);
-    return code;
-  }
-  let value2 = property_change(r, "code", lambda);
+  let r = await function_dependencies_code_call_multiple(f_names, f_name);
   return r;
 }
---
+++ public/src/function_dependencies_code_call_multiple.mjs
@@ -0,0 +1,15 @@
+import { property_change } from "../../../love/public/src/property_change.mjs";
+import { app_shared_code_run } from "../../../love/public/src/app_shared_code_run.mjs";
+import { function_dependencies_code_multiple } from "../../../love/public/src/function_dependencies_code_multiple.mjs";
+export async function function_dependencies_code_call_multiple(
+  f_names_dependencies,
+  f_name_call,
+) {
+  let r = await function_dependencies_code_multiple(f_names_dependencies);
+  function lambda(dependencies) {
+    let code = app_shared_code_run(f_name_call, dependencies);
+    return code;
+  }
+  let value2 = property_change(r, "code", lambda);
+  return r;
+}
