33d68cd8b5e720df37042f55cdec5ea5e79a3360
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:12:29 -0500

    function_auto_path ..\love\public\src\html_update_externals.mjs

--- public/src/html_update_externals.mjs
+++ public/src/html_update_externals.mjs
@@ -1,24 +1,9 @@
-import { function_dependencies_code_call_multiple } from "../../../love/public/src/function_dependencies_code_call_multiple.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { html_code_script_importmap } from "../../../love/public/src/html_code_script_importmap.mjs";
-import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { html_update_externals_dependencies } from "../../../love/public/src/html_update_externals_dependencies.mjs";
 export async function html_update_externals(f_name) {
   const f_names_dependencies = [f_name];
-  let scripts = [];
-  let dependencies = await function_dependencies_code_call_multiple(
+  let r = await html_update_externals_dependencies(
     f_name,
     f_names_dependencies,
   );
-  let externals = property_get(dependencies, "externals");
-  let ne = list_empty_not_is(externals);
-  if (ne) {
-    let importmap = html_code_script_importmap(externals);
-    list_add(scripts, importmap);
-  }
-  let r = {
-    dependencies,
-    scripts,
-  };
   return r;
 }
---
+++ public/src/html_update_externals_dependencies.mjs
@@ -0,0 +1,26 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { html_code_script_importmap } from "../../../love/public/src/html_code_script_importmap.mjs";
+import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { function_dependencies_code_call_multiple } from "../../../love/public/src/function_dependencies_code_call_multiple.mjs";
+export async function html_update_externals_dependencies(
+  f_name,
+  f_names_dependencies,
+) {
+  let scripts = [];
+  let dependencies = await function_dependencies_code_call_multiple(
+    f_name,
+    f_names_dependencies,
+  );
+  let externals = property_get(dependencies, "externals");
+  let ne = list_empty_not_is(externals);
+  if (ne) {
+    let importmap = html_code_script_importmap(externals);
+    list_add(scripts, importmap);
+  }
+  let r = {
+    dependencies,
+    scripts,
+  };
+  return r;
+}
