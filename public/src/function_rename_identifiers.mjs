ac81131703f5172510fbcb41b7b7711cc9015e9f
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:44:50 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- public/src/function_rename_identifiers.mjs
+++ public/src/function_rename_identifiers.mjs
@@ -2,17 +2,13 @@ import { functions_transform_list } from "../../../love/public/src/functions_tra
 import { js_identifier_rename_imports_fix } from "../../../love/public/src/js_identifier_rename_imports_fix.mjs";
 import { property_exists_if_async } from "../../../love/public/src/property_exists_if_async.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
   let identifiers = await data_identifiers_get();
   await property_exists_if_async(identifiers, f_name_before, on_exist);
   async function on_exist(f_names) {
     list_remove_if_exists(f_names, f_name_before);
-    async function lambda(f_name) {
-      let output = await function_transform(f_name, lambda2);
-    }
-    await functions_transform_list(f_names, lambda);
+    await functions_transform_list(f_names, lambda2);
   }
   async function lambda2(ast) {
     await js_identifier_rename_imports_fix(ast, f_name_before, f_name_after);
