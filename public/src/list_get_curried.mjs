296364c04647f7b495f3e0df4ca129351e5650cb
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:46:09 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/list_get_curried.mjs
+++ public/src/list_get_curried.mjs
@@ -1,5 +1,5 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
-export function list_get_curry(list) {
+export function list_get_curried(list) {
   let getter = function list_get_curry_result(index) {
     let item = list_get(list, index);
     return item;
