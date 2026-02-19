d79f62761c16c43ab08799bb28f154915fd75974
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:49:59 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,14 +1,10 @@
-import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
+import { app_shared_code_run } from "../../../love/public/src/app_shared_code_run.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
-import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
-import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { function_dependencies_code_multiple } from "./function_dependencies_code_multiple.mjs";
 export async function function_dependencies_code_call(f_name) {
   let r = await function_dependencies_code_multiple([f_name]);
   function lambda(dependencies) {
-    let global_init = js_code_global_init();
-    let call = js_code_call_statement(f_name);
-    let code = list_join_newline([global_init, dependencies, call]);
+    let code = app_shared_code_run(f_name, dependencies);
     return code;
   }
   let value2 = property_change(r, "code", lambda);
