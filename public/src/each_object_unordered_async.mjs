b30e2bc719d63d6a2a7730c9867da96b37052cc5
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:38:04 -0500

    function_auto_path ..\love\public\src\each_object_unordered_async.mjs

--- public/src/each_object_unordered_async.mjs
+++ public/src/each_object_unordered_async.mjs
@@ -1,6 +1,6 @@
-import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
+import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { each_object_generic_async } from "../../../love/public/src/each_object_generic_async.mjs";
 export async function each_object_unordered_async(object, lambda$value$key) {
-  let each_lambda = each_unordered_async;
+  let each_lambda = list_map_unordered_async;
   await each_object_generic_async(object, lambda$value$key, each_lambda);
 }
