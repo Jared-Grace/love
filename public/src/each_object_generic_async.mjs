9fae4e86925e6d4f7c894d85132f94a8642a3c45
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:38:34 -0500

    function_auto_path ..\love\public\src\each_object_generic_async.mjs

--- public/src/each_object_generic_async.mjs
+++ public/src/each_object_generic_async.mjs
@@ -1,11 +1,11 @@
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export async function each_object_generic_async(object, lambda, each_lambda) {
+export async function each_object_generic_async(object, lambda, mapper_lambda) {
   async function lambda2(property) {
     let value = property_get(object, property);
     await lambda(value, property);
   }
   let properties = properties_get(object);
-  let mapped = await each_lambda(properties, lambda2);
+  let mapped = await mapper_lambda(properties, lambda2);
   return mapped;
 }
