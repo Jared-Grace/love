7231b145663148dc10b4fc0df00fe449cf8c1df9
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:48 -0500

    function_auto_path ..\love\public\src\js_flo_params_add.mjs

--- web/public/src/js_function_declaration_single_param_add.mjs
+++ web/public/src/js_function_declaration_single_param_add.mjs
@@ -1,4 +1,4 @@
-import { js_function_declaration_single_params_add } from "../../../love/public/src/js_function_declaration_single_params_add.mjs";
+import { js_flo_params_add } from "../../../love/public/src/js_flo_params_add.mjs";
 export function js_function_declaration_single_param_add(ast, list) {
-  js_function_declaration_single_params_add(ast, [list]);
+  js_flo_params_add(ast, [list]);
 }
