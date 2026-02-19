d02219305f4ae7edac42d5a201842daa0d3b4c35
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:04:22 -0500

    function_auto_path ..\love\public\src\functions_rename_part.mjs

--- public/src/functions_rename_part.mjs
+++ public/src/functions_rename_part.mjs
@@ -1,7 +1,7 @@
 import { function_name_to_parts_includes } from "../../../love/public/src/function_name_to_parts_includes.mjs";
 import { functions_rename_generic } from "../../../love/public/src/functions_rename_generic.mjs";
 export async function functions_rename_part(from, to) {
-  function_name_to_parts_includes;
+  let includes = function_name_to_parts_includes(f_name, part);
   let r = await functions_rename_generic(
     function_name_to_parts_includes,
     name_change,
