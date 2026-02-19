e470c18e1cb8af2cefae09c6c5efb32631f1d2b8
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:27:04 -0500

    function_auto_path ..\love\public\src\app_replace_rule_sets.mjs

--- public/src/app_replace_rule_sets.mjs
+++ public/src/app_replace_rule_sets.mjs
@@ -1,5 +1,7 @@
+import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 export function app_replace_rule_sets() {
   let r = app_replace_rule_sets_fns();
+  let mapped = invoke_multiple(list_fns);
   return r;
 }
