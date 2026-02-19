e2318f0b5651a47da82204eb97b8b1064dea43d6
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:45:56 -0500

    function_auto_path ..\love\public\src\app_a_functions.mjs

--- web/public/src/app_replace_button_symbol_style_valid_if_curried_right.mjs
+++ web/public/src/app_replace_button_symbol_style_valid_if_curried_right.mjs
@@ -1,5 +1,5 @@
 import { app_replace_button_symbol_style_valid_if } from "../../../love/public/src/app_replace_button_symbol_style_valid_if.mjs";
-export function app_replace_button_symbol_style_valid_if_curry_right(valid) {
+export function app_replace_button_symbol_style_valid_if_curried_right(valid) {
   let lambda = function app_replace_button_symbol_style_valid_curried_result(
     sb,
   ) {
