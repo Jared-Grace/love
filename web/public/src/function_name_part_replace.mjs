89179c76dc0fe4424c4f08092a445ed694e49cb7
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:29:55 -0500

    function_auto_path ..\love\public\src\function_name_part_replace.mjs

--- web/public/src/function_name_part_replace.mjs
+++ web/public/src/function_name_part_replace.mjs
@@ -1,11 +1,11 @@
+import { change_if_equal_curried_right_2 } from "../../../love/public/src/change_if_equal_curried_right_2.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
-import { change_if_equal_curried_right } from "../../../love/public/src/change_if_equal_curried_right.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_part_replace(f_name, from, to) {
   let parts = function_name_to_parts(f_name);
-  let r = change_if_equal_curried_right(from, to);
+  let r = change_if_equal_curried_right_2(from, to);
   let mapped = list_map(parts, r);
   let combined = function_name_combine_multiple(mapped);
   log({
