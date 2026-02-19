7755531ab246c2167fc69acc94d0dcfc621d57e7
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:38 -0500

    function_auto_path ..\love\public\src\js_flo_block_body_empty_return_identifiers_curried_right.mjs

--- web/public/src/app_replace_rule_sets_calls.mjs
+++ web/public/src/app_replace_rule_sets_calls.mjs
@@ -1,5 +1,5 @@
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
-import { js_function_declaration_single_block_body_empty_return_identifiers_curried_right } from "../../../love/public/src/js_function_declaration_single_block_body_empty_return_identifiers_curried_right.mjs";
+import { js_flo_block_body_empty_return_identifiers_curried_right } from "../../../love/public/src/js_flo_block_body_empty_return_identifiers_curried_right.mjs";
 import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
 import { function_ast_fn } from "../../../love/public/src/function_ast_fn.mjs";
 import { js_list_calls_names } from "../../../love/public/src/js_list_calls_names.mjs";
@@ -7,9 +7,6 @@ import { app_replace_rule_sets_v_1 } from "../../../love/public/src/app_replace_
 export async function app_replace_rule_sets_calls() {
   let ast2 = await function_ast_fn(app_replace_rule_sets_v_1);
   let names = js_list_calls_names(ast2);
-  let r22 =
-    js_function_declaration_single_block_body_empty_return_identifiers_curried_right(
-      names,
-    );
+  let r22 = js_flo_block_body_empty_return_identifiers_curried_right(names);
   let output = await function_transform_fn(app_replace_rule_sets, r2);
 }
