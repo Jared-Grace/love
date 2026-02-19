15300fff538f456ee30be64db4be6dd7a2cf4ac3
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:45:56 -0500

    function_auto_path ..\love\public\src\function_rename_identifiers.mjs

--- public/src/function_rename_identifiers.mjs
+++ public/src/function_rename_identifiers.mjs
@@ -1,11 +1,9 @@
+import { data_identifier_exists_if } from "../../../love/public/src/data_identifier_exists_if.mjs";
 import { functions_transform_list } from "../../../love/public/src/functions_transform_list.mjs";
 import { js_identifier_rename_imports_fix } from "../../../love/public/src/js_identifier_rename_imports_fix.mjs";
-import { property_exists_if_async } from "../../../love/public/src/property_exists_if_async.mjs";
 import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
-import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
 export async function function_rename_identifiers(f_name_before, f_name_after) {
-  let identifiers = await data_identifiers_get();
-  await property_exists_if_async(identifiers, f_name_before, on_exist);
+  await data_identifier_exists_if(f_name_before, on_exist);
   async function on_exist(f_names) {
     list_remove_if_exists(f_names, f_name_before);
     await functions_transform_list(f_names, lambda2);
