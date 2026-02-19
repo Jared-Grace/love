6b8773b717abb6b325a7b1abb9c40b5fd6969cc6
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:17:22 -0500

    function_auto_path ..\love\public\src\html_update_public_new.mjs

--- web/public/src/html_update_public_new.mjs
+++ web/public/src/html_update_public_new.mjs
@@ -1,4 +1,4 @@
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 export async function html_update_public_new(search) {
-  let info = await app_shared_name_search_info(search2);
+  let info = await app_shared_name_search_info(search);
 }
