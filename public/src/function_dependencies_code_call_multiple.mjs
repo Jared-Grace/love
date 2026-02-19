f325239f05ac8c569472bb891bab19c82af460d0
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:17:06 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call_multiple.mjs

--- public/src/function_dependencies_code_call_multiple.mjs
+++ public/src/function_dependencies_code_call_multiple.mjs
@@ -1,3 +1,4 @@
+import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { app_shared_code_run } from "../../../love/public/src/app_shared_code_run.mjs";
 import { function_dependencies_code_multiple } from "../../../love/public/src/function_dependencies_code_multiple.mjs";
