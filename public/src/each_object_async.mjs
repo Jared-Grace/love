e70fae6c03879abd084f130dfa1165db7972126f
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:43:51 -0500

    pp 5

--- public/src/each_object_async.mjs
+++ public/src/each_object_async.mjs
@@ -1,6 +1,6 @@
-import { each_object_generic_async } from "../../../love/public/src/each_object_generic_async.mjs";
-import { each_async } from "./each_async.mjs";
+import { object_map_generic_async } from "../../../love/public/src/object_map_generic_async.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function each_object_async(object, lambda$value$key) {
   let each_lambda = each_async;
-  await each_object_generic_async(object, lambda$value$key, each_lambda);
+  await object_map_generic_async(object, lambda$value$key, each_lambda);
 }
--- public/src/each_object_unordered_async.mjs
+++ public/src/each_object_unordered_async.mjs
@@ -1,6 +1,6 @@
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
-import { each_object_generic_async } from "../../../love/public/src/each_object_generic_async.mjs";
+import { object_map_generic_async } from "../../../love/public/src/object_map_generic_async.mjs";
 export async function each_object_unordered_async(object, lambda$value$key) {
   let each_lambda = list_map_unordered_async;
-  await each_object_generic_async(object, lambda$value$key, each_lambda);
+  await object_map_generic_async(object, lambda$value$key, each_lambda);
 }
