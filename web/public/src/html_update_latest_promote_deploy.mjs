64f2f296bb670b067aa183c1896556c1dc2b27cd
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:40:31 -0500

    function_auto_path ..\love\public\src\html_update_latest_promote_deploy.mjs

--- web/public/src/html_update_latest_promote_deploy.mjs
+++ web/public/src/html_update_latest_promote_deploy.mjs
@@ -1,5 +1,7 @@
+import { firebase_deploy } from "../../../love/public/src/firebase_deploy.mjs";
 import { html_update_latest_promote } from "../../../love/public/src/html_update_latest_promote.mjs";
 export async function html_update_latest_promote_deploy(search) {
-  let r = await html_update_latest_promote(search);fb
+  let r = await html_update_latest_promote(search);
+  let stdout = await firebase_deploy();
   return r;
 }
