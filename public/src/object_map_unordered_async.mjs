2a3495b89cb7a25018ff9210004ab1436968ff62
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:44:22 -0500

    rrf2 each_object_unordered_async object_map

--- public/src/object_map_unordered_async.mjs
+++ public/src/object_map_unordered_async.mjs
@@ -1,6 +1,6 @@
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { object_map_generic_async } from "../../../love/public/src/object_map_generic_async.mjs";
-export async function each_object_unordered_async(object, lambda$value$key) {
+export async function object_map_unordered_async(object, lambda$value$key) {
   let each_lambda = list_map_unordered_async;
   await object_map_generic_async(object, lambda$value$key, each_lambda);
 }
