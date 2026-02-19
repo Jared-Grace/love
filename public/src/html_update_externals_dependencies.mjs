3fd62dacc128450e0a4b92b6abc036a48e2fc8fb
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:18:57 -0500

    function_auto_path ..\love\public\src\html_update_externals_dependencies.mjs

--- public/src/html_update_externals_dependencies.mjs
+++ public/src/html_update_externals_dependencies.mjs
@@ -1,3 +1,4 @@
+import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { html_code_script_importmap } from "../../../love/public/src/html_code_script_importmap.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
