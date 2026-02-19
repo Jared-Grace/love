0a9ef6dcfd675ec32305f3632308e5e36c18168d
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:06:19 -0500

    function_auto_path ..\love\public\src\js_dollar_aa.mjs

--- web/public/src/js_flo_block_body_empty.mjs
+++ web/public/src/js_flo_block_body_empty.mjs
@@ -1,7 +1,7 @@
 import { list_empty } from "../../../love/public/src/list_empty.mjs";
-import { js_flo_block_body } from "../../../love/public/src/js_flo_block_body.mjs";
+import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
 export function js_flo_block_body_empty(ast) {
-  let body_block = js_flo_block_body(ast);
+  let body_block = js_flo_body(ast);
   list_empty(body_block);
   return body_block;
 }
