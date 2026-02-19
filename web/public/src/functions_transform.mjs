c9af68c7e1234b602b0877c0835dd11fce122e43
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:43:25 -0500

    pb functions_transform_list

--- web/public/src/functions_transform.mjs
+++ web/public/src/functions_transform.mjs
@@ -2,6 +2,6 @@ import { functions_transform_list } from "../../../love/public/src/functions_tra
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function functions_transform(lambda$ast) {
   const list = await functions_names();
-  let waited = await functions_transform_list(lambda$ast, list);
+  let waited = await functions_transform_list(list, lambda$ast);
   return waited;
 }
