bb9d4c4703fa0c8072e77d6f3829f1f90858e8ff
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:20:09 -0500

    function_auto_path ..\love\public\src\curry_curry_curry_curry_curry.mjs

---
+++ web/public/src/curry_curry_curry_curry_curry_curry_curry_curry_curry.mjs
@@ -0,0 +1,10 @@
+import { app_replace_button_symbol_style_valid_if } from "../../../love/public/src/app_replace_button_symbol_style_valid_if.mjs";
+export function curry_curry_curry_curry_curry_curry_curry_curry_curry(valid) {
+  let lambda = function app_replace_button_symbol_style_valid_curried_result(
+    sb,
+  ) {
+    let r = app_replace_button_symbol_style_valid_if(sb, valid);
+    return r;
+  };
+  return lambda;
+}
