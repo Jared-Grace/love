c368ff0478745620314e1f1ea3eb40f2758d83c3
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:21:39 -0500

    function_auto_path ..\love\public\src\app_a_function_select_curry.mjs

--- public/src/app_a_history.mjs
+++ public/src/app_a_history.mjs
@@ -1,4 +1,4 @@
-import { curry_curry_curry_curry_curry } from "../../../love/public/src/curry_curry_curry_curry_curry.mjs";
+import { app_a_function_select_curry } from "../../../love/public/src/app_a_function_select_curry.mjs";
 import { app_a_button_function_if_exists } from "../../../love/public/src/app_a_button_function_if_exists.mjs";
 import { app_a_list_chooser } from "../../../love/public/src/app_a_list_chooser.mjs";
 import { app_a_function_name_selected_history_key } from "../../../love/public/src/app_a_function_name_selected_history_key.mjs";
@@ -7,6 +7,6 @@ export function app_a_history(context) {
   app_a_button_function_if_exists(context);
   let key = app_a_function_name_selected_history_key();
   let h = storage_local_get_context(context, key);
-  let on_select = curry_curry_curry_curry_curry(context);
+  let on_select = app_a_function_select_curry(context);
   let r = app_a_list_chooser(context, "function", h, on_select);
 }
