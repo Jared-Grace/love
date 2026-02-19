310fe869e2ef920440ca14ed0e993f7b622fd2c1
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:06:39 -0500

    function_auto_path ..\love\public\src\function_name_part_replace.mjs

--- public/src/function_name_part_replace.mjs
+++ public/src/function_name_part_replace.mjs
@@ -1,4 +1,6 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_part_replace(f_name, from, to) {
   let parts = function_name_to_parts(f_name);
+  each(list, function lambda(item) {});
 }
