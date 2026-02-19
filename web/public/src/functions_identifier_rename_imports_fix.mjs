e6444cd46962d724996d3c6014cd0f82d764da20
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:48:10 -0500

    pf functions_identifier_rename_imports_fix f_names

--- web/public/src/functions_identifier_rename_imports_fix.mjs
+++ web/public/src/functions_identifier_rename_imports_fix.mjs
@@ -1,9 +1,9 @@
 import { functions_transform_list } from "../../../love/public/src/functions_transform_list.mjs";
 import { js_identifier_rename_imports_fix_curried_right_2 } from "../../../love/public/src/js_identifier_rename_imports_fix_curried_right_2.mjs";
 export async function functions_identifier_rename_imports_fix(
+  f_names,
   f_name_before,
   f_name_after,
-  f_names,
 ) {
   let r2 = await js_identifier_rename_imports_fix_curried_right_2(
     f_name_before,
