5fdcdbd402dcee1eb6586462567da9f172ed7966
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:44:13 -0500

    function_auto_path ..\love\public\src\function_replace.mjs

--- web/public/src/function_replace.mjs
+++ web/public/src/function_replace.mjs
@@ -1,11 +1,9 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { functions_identifiers_rename_alias } from "../../../love/public/src/functions_identifiers_rename_alias.mjs";
 import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
-import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function function_replace(f_name_before, f_name_after) {
-  const v = await function_name_to_path_unalias(f_name_before);
-  let unaliased = property_get(v, "unaliased");
+  const unaliased = await function_name_unalias_only(f_name_before);
   f_name_before = unaliased;
   await function_rename_fn_names_check(f_name_before);
   await function_delete(f_name_before);
