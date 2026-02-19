284149fbbcf72ed8835649c3a0281c97458f255c
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:40:02 -0500

    function_auto_path ..\love\public\src\app_shared_refresh.mjs

--- web/public/src/app_shared_refresh.mjs
+++ web/public/src/app_shared_refresh.mjs
@@ -1,5 +1,3 @@
-import { html_pre_text } from "../../../love/public/src/html_pre_text.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -7,23 +5,17 @@ import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_document_body } from "../../../love/public/src/html_document_body.mjs";
 import { list_find_property } from "./list_find_property.mjs";
 export function app_shared_refresh(context) {
-  try {
-    let screens = property_get(context, "screens");
-    let app_fn = property_get(context, "app_fn");
-    let body = html_document_body();
-    html_clear(body);
-    let value = property_get(app_fn, "name");
-    let combined = function_name_combine(value, "home");
-    let screen_name = storage_local_initialize_context(
-      context,
-      "screen",
-      combined,
-    );
-    let screen = list_find_property(screens, "name", screen_name);
-    screen(context);
-  } catch (e) {
-    let body = html_document_body();
-    let json = json_to(e);
-    let p = html_pre_text(body, json);
-  }
+  let screens = property_get(context, "screens");
+  let app_fn = property_get(context, "app_fn");
+  let body = html_document_body();
+  html_clear(body);
+  let value = property_get(app_fn, "name");
+  let combined = function_name_combine(value, "home");
+  let screen_name = storage_local_initialize_context(
+    context,
+    "screen",
+    combined,
+  );
+  let screen = list_find_property(screens, "name", screen_name);
+  screen(context);
 }
