66bfb16372c8185afa89ee2f3c0dfc4320a5d0c6
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:00:24 -0500

    function_auto_path ..\love\public\src\html_update_public.mjs

--- web/public/src/html_update_public.mjs
+++ web/public/src/html_update_public.mjs
@@ -1,11 +1,9 @@
-import { app_shared_name_search_main_both } from "../../../love/public/src/app_shared_name_search_main_both.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_shared_name_search } from "../../../love/public/src/app_shared_name_search.mjs";
 import { html_update_public_generic } from "../../../love/public/src/html_update_public_generic.mjs";
 import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
 export async function html_update_public(search) {
-  let v2 = await app_shared_name_search_main_both(search);
-  let name = property_get(v2, "a_name");
+  let name = await app_shared_name_search(search);
   let a_name = app_shared_name_prefixed(name);
   let file_path = html_name_to_path(name);
   await html_update_public_generic(a_name, file_path, name);
