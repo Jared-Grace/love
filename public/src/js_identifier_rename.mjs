49b8b301f8d91079b8e638e02893dbc359abf526
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:28:09 -0500

    function_auto_path ..\love\public\src\js_identifier_rename.mjs

--- public/src/js_identifier_rename.mjs
+++ public/src/js_identifier_rename.mjs
@@ -1,7 +1,6 @@
-import { property_equals } from "../../../love/public/src/property_equals.mjs";
+import { property_set_if_equals } from "../../../love/public/src/property_set_if_equals.mjs";
 import { js_visit_identifiers } from "../../../love/public/src/js_visit_identifiers.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
-import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_identifier_rename(ast, name_from, name_to) {
   if (equal(name_from, name_to)) {
@@ -10,9 +9,7 @@ export function js_identifier_rename(ast, name_from, name_to) {
   function lambda(v) {
     let node = property_get(v, "node");
     const property_name = "name";
-    if (property_equals(node, property_name, name_from)) {
-      property_set(node, property_name, name_to);
-    }
+    property_set_if_equals(node, property_name, name_from, name_to);
   }
   js_visit_identifiers(ast, lambda);
 }
