9ed769cfe1c60a8803723e0f72735583691edac2
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:26:15 -0500

    fw html_update_latest_new html_update_latest_promote

---
+++ public/src/html_update_latest_promote.mjs
@@ -0,0 +1,4 @@
+import { html_update_latest_new } from "../../../love/public/src/html_update_latest_new.mjs";
+export async function html_update_latest_promote(search) {
+  return await html_update_latest_new(search);
+}
