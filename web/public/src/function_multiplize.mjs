934c03bd08baf671e96b4042b8d5a5d7b3ee1761
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:06:22 -0500

    function_auto_path ..\love\public\src\js_flo_body_add.mjs

--- web/public/src/function_multiplize.mjs
+++ web/public/src/function_multiplize.mjs
@@ -5,7 +5,7 @@ import { function_parse_declaration_unaliased } from "../../../love/public/src/f
 import { js_imports_missing_add } from "../../../love/public/src/js_imports_missing_add.mjs";
 import { js_flo_param_add } from "../../../love/public/src/js_flo_param_add.mjs";
 import { js_call_statement } from "../../../love/public/src/js_call_statement.mjs";
-import { js_flo_block_body_add } from "../../../love/public/src/js_flo_block_body_add.mjs";
+import { js_flo_body_add } from "../../../love/public/src/js_flo_body_add.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
@@ -18,7 +18,7 @@ export async function function_multiplize(f_name) {
   async function lambda(ast) {
     const list = "list";
     let call = js_call_statement(each.name, [list, f_name]);
-    js_flo_block_body_add(ast, call);
+    js_flo_body_add(ast, call);
     js_flo_param_add(ast, list);
     await js_imports_missing_add(ast);
   }
