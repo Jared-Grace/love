f463f8eb4260fbe8ed5267f3861e9e5a948d829b
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:43:51 -0500

    pp 5

--- web/public/src/object_map_generic_async.mjs
+++ web/public/src/object_map_generic_async.mjs
@@ -1,6 +1,6 @@
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export async function each_object_generic_async(object, lambda, mapper_lambda) {
+export async function object_map_generic_async(object, lambda, mapper_lambda) {
   async function lambda2(property) {
     let value = property_get(object, property);
     await lambda(value, property);
