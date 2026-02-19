0ccfe6aa3b56b0616b112653ba0dee103c6d297d
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:08:08 -0500

    function_auto_path ..\love\public\src\function_name_part_replace.mjs

--- public/src/function_name_part_replace.mjs
+++ public/src/function_name_part_replace.mjs
@@ -1,9 +1,11 @@
+import { equal } from "../../../love/public/src/equal.mjs";
 import { ternary } from "../../../love/public/src/ternary.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_part_replace(f_name, from, to) {
   let parts = function_name_to_parts(f_name);
-  function lambda(item) {eq
+  function lambda(item) {
+    let eq2 = equal(left, right);
     let result = ternary(condition, on_true, on_false);
   }
   let mapped = list_map(parts, lambda);
