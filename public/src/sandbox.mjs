d46754ffb62b9997e3a5a64e32092563b30b3d9d
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:23:35 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,30 @@
-export async function sandbox(d, b, c) {
-  throw new Error("aa");
+import { text_starts_with_curry_right } from "../../../love/public/src/text_starts_with_curry_right.mjs";
+import { text_starts_ends_with_curry_right } from "../../../love/public/src/text_starts_ends_with_curry_right.mjs";
+import { text_replace_curry_right } from "../../../love/public/src/text_replace_curry_right.mjs";
+import { text_match_ordered_curry } from "../../../love/public/src/text_match_ordered_curry.mjs";
+import { text_includes_curry_right_get } from "../../../love/public/src/text_includes_curry_right_get.mjs";
+import { property_get_curry_right } from "../../../love/public/src/property_get_curry_right.mjs";
+import { list_index_is_curry } from "../../../love/public/src/list_index_is_curry.mjs";
+import { list_get_curry } from "../../../love/public/src/list_get_curry.mjs";
+import { js_function_declaration_param_add_curry } from "../../../love/public/src/js_function_declaration_param_add_curry.mjs";
+import { app_replace_rule_valid_curry } from "../../../love/public/src/app_replace_rule_valid_curry.mjs";
+import { app_replace_button_symbol_style_valid_if_curry_right } from "../../../love/public/src/app_replace_button_symbol_style_valid_if_curry_right.mjs";
+import { app_a_function_select_curry } from "../../../love/public/src/app_a_function_select_curry.mjs";
+export async function sandbox() {
+  let a = {
+    app_a_function_select_curry: "curry_curry_curry_curry_curry",
+    app_replace_button_symbol_style_valid_if_curry_right:
+      "curry_curry_curry_curry_curry_curry_curry_curry_curry",
+    app_replace_rule_valid_curry: "curry_curry_curry_curry_curry",
+    js_function_declaration_param_add_curry:
+      "curry_curry_curry_curry_curry_curry",
+    list_get_curry: "curry_curry_curry",
+    list_index_is_curry: "curry_curry_curry_curry",
+    property_get_curry_right: "curry_curry_curry_curry",
+    text_includes_curry_right_get: "curry_curry_curry_curry_curry",
+    text_match_ordered_curry: "curry_curry_curry_curry",
+    text_replace_curry_right: "curry_curry_curry_curry",
+    text_starts_ends_with_curry_right: "curry_curry_curry_curry_curry_curry",
+    text_starts_with_curry_right: "curry_curry_curry_curry_curry",
+  };
 }
