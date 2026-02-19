efa09fde5d41cca23a3fd40be311aafed42ab664
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:52 -0500

    function_auto_path ..\love\public\src\function_params_new.mjs

--- public/src/js_flo_param_add.mjs
+++ public/src/js_flo_param_add.mjs
@@ -1,4 +1,4 @@
 import { js_flo_params_add } from "../../../love/public/src/js_flo_params_add.mjs";
-export function js_function_declaration_single_param_add(ast, list) {
+export function js_flo_param_add(ast, list) {
   js_flo_params_add(ast, [list]);
 }
