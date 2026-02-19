91c0e750b4580a4560c8f860b6dbc0ab83701481
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:13:57 -0500

    function_auto_path ..\love\public\src\property_exists_not_assert.mjs

--- public/src/property_exists_not_assert.mjs
+++ public/src/property_exists_not_assert.mjs
@@ -1,3 +1,4 @@
+import { list_skip } from "../../../love/public/src/list_skip.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 import { property_exists } from "../../../love/public/src/property_exists.mjs";
@@ -5,6 +6,7 @@ export function property_exists_not_assert(object, property_name) {
   let e = property_exists(object, property_name);
   if (e) {
     let value = property_get(object, property_name);
+    let skipped = list_skip(list, skip_count);
     error_json({
       object,
       property_name,
