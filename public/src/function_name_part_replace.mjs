5b1828f6f2ac3083b7b54e6ffe3b1351fae1b36a
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:19:22 -0500

    function_auto_path ..\love\public\src\function_name_part_replace.mjs

--- public/src/function_name_part_replace.mjs
+++ public/src/function_name_part_replace.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { ternary_equal_curried_right } from "../../../love/public/src/ternary_equal_curried_right.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -7,5 +8,8 @@ export function function_name_part_replace(f_name, from, to) {
   let r = ternary_equal_curried_right(from, to);
   let mapped = list_map(parts, r);
   let combined = function_name_combine_multiple(mapped);
+  log({
+    combined,
+  });
   return combined;
 }
