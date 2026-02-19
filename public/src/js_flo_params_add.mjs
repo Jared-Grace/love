b9f2f7b5456b90212ee200da849feceb928b3394
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:45 -0500

    function_auto_path ..\love\public\src\js_function_declaration_single_path.mjs

--- public/src/js_flo_params_add.mjs
+++ public/src/js_flo_params_add.mjs
@@ -1,6 +1,6 @@
 import { js_function_declaration_params_add } from "../../../love/public/src/js_function_declaration_params_add.mjs";
 import { js_flo } from "../../../love/public/src/js_flo.mjs";
-export function js_function_declaration_single_params_add(ast, param_names) {
+export function js_flo_params_add(ast, param_names) {
   let declaration = js_flo(ast);
   js_function_declaration_params_add(declaration, param_names);
   return declaration;
