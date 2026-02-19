c7d9b1ff8c09e23eefe9042a3c37d71b5288559a
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:12:37 -0500

    rrl app_replace_rule_sets_calls generate

--- public/src/app_replace_rule_sets_generate.mjs
+++ public/src/app_replace_rule_sets_generate.mjs
@@ -2,7 +2,7 @@ import { app_replace_rule_sets_v_1_names } from "../../../love/public/src/app_re
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { js_flo_body_empty_return_identifiers_curried_right } from "../../../love/public/src/js_flo_body_empty_return_identifiers_curried_right.mjs";
 import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
-export async function app_replace_rule_sets_calls() {
+export async function app_replace_rule_sets_generate() {
   let names = await app_replace_rule_sets_v_1_names();
   let r2 = js_flo_body_empty_return_identifiers_curried_right(names);
   let output = await function_transform_fn(app_replace_rule_sets, r2);
