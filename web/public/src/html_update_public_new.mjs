38015f5cc268de1f031686b02f6ab8e581f6aee1
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:01:21 -0500

    function_auto_path ..\love\public\src\html_update_public_new.mjs

--- web/public/src/html_update_public_new.mjs
+++ web/public/src/html_update_public_new.mjs
@@ -1,3 +1,9 @@
-export function html_update_public_new() {
-  let a_name = "rce";
+import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
+import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
+import { app_shared_name_search } from "../../../love/public/src/app_shared_name_search.mjs";
+export async function html_update_public_new() {
+  let search = "rce";
+  let name = await app_shared_name_search(search);
+  let a_name = app_shared_name_prefixed(name);
+  let file_path = html_name_to_path(name);
 }
