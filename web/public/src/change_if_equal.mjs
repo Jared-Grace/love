2c14de04c055e3b0d93794476470ab8a2bba8aae
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:26:47 -0500

    function_auto_path ..\love\public\src\change_if_equal.mjs

--- web/public/src/change_if_equal.mjs
+++ web/public/src/change_if_equal.mjs
@@ -1,6 +1,6 @@
 import { ternary } from "../../../love/public/src/ternary.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
-export function ternary_equal(item, from, to) {
+export function change_if_equal(item, from, to) {
   let eq = equal(item, from);
   let result = ternary(eq, to, item);
   return result;
