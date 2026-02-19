994609ba9e8d38293dc6050d28a0c5d66a15ff25
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:46:09 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -6,7 +6,7 @@ import { text_includes_curry_right_get } from "../../../love/public/src/text_inc
 import { property_get_curried_right } from "../../../love/public/src/property_get_curried_right.mjs";
 import { list_index_is_curry } from "../../../love/public/src/list_index_is_curry.mjs";
 import { list_get_curry } from "../../../love/public/src/list_get_curry.mjs";
-import { js_function_declaration_param_add_curry } from "../../../love/public/src/js_function_declaration_param_add_curry.mjs";
+import { js_function_declaration_param_add_curried } from "../../../love/public/src/js_function_declaration_param_add_curried.mjs";
 import { app_replace_rule_valid_curried } from "../../../love/public/src/app_replace_rule_valid_curried.mjs";
 import { app_replace_button_symbol_style_valid_if_curried_right } from "../../../love/public/src/app_replace_button_symbol_style_valid_if_curried_right.mjs";
 import { app_a_function_select_curried } from "../../../love/public/src/app_a_function_select_curried.mjs";
@@ -16,7 +16,7 @@ export async function sandbox() {
     app_replace_button_symbol_style_valid_if_curried_right:
       "curry_curry_curry_curry_curry_curry_curry_curry_curry",
     app_replace_rule_valid_curried: "curry_curry_curry_curry_curry",
-    js_function_declaration_param_add_curry:
+    js_function_declaration_param_add_curried:
       "curry_curry_curry_curry_curry_curry",
     list_get_curry: "curry_curry_curry",
     list_index_is_curry: "curry_curry_curry_curry",
