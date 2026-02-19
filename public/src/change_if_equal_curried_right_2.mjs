a85f591508b9ad14c1c948e827aec632ad804374
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:29:47 -0500

    function_auto_path ..\love\public\src\change_if_equal_curried_right_2.mjs

--- public/src/change_if_equal_curried_right_2.mjs
+++ public/src/change_if_equal_curried_right_2.mjs
@@ -1,6 +1,8 @@
 import { change_if_equal } from "../../../love/public/src/change_if_equal.mjs";
 export function change_if_equal_curried_right_2(from, to) {
-  return function change_if_equal_curried_right_2_result(item) {
-    return change_if_equal(item, from, to);
+  let r = function change_if_equal_curried_right_2_result(item) {
+    let result = change_if_equal(item, from, to);
+    return result;
   };
+  return r;
 }
