9b93f31f0ab3bc4cd086a5e8f86b90e225a1b982
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:04:39 -0500

    function_auto_path ..\love\public\src\function_name_to_parts_includes_curried_right.mjs

--- web/public/src/function_name_to_parts_includes_curried_right.mjs
+++ web/public/src/function_name_to_parts_includes_curried_right.mjs
@@ -1,6 +1,10 @@
 import { function_name_to_parts_includes } from "../../../love/public/src/function_name_to_parts_includes.mjs";
 export function function_name_to_parts_includes_curried_right(part) {
-  return function function_name_to_parts_includes_curried_right_result(f_name) {
-    return function_name_to_parts_includes(f_name, part);
+  let r = function function_name_to_parts_includes_curried_right_result(
+    f_name,
+  ) {
+    let includes = function_name_to_parts_includes(f_name, part);
+    return includes;
   };
+  return r;
 }
