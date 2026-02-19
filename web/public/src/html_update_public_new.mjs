e94ba49d34400cde38624443c6720ff814d31085
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 18:18:33 -0500

    function_auto_path ..\love\public\src\html_update_public_new.mjs

--- web/public/src/html_update_public_new.mjs
+++ web/public/src/html_update_public_new.mjs
@@ -1,7 +1,9 @@
+import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 export async function html_update_public_new(search) {
   let info = await app_shared_name_search_info(search);
   let f_path_latest = property_get(info, "f_path_latest");
   let f_path = property_get(info, "f_path");
+  await file_copy_overwrite(file_path_old, file_path_new);
 }
