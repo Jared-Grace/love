e10abcf4f9be67e8cd277e217cf52f32adfd0192
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:21:53 -0500

    function_auto_path ..\love\public\src\invoke_multiple.mjs

--- web/public/src/invoke_multiple.mjs
+++ web/public/src/invoke_multiple.mjs
@@ -1,9 +1,6 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { invoke } from "../../../love/public/src/invoke.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 export function invoke_multiple(list_fns) {
-  each(list_fns, invoke);
-  return;
   let mapped = list_map(list_fns, invoke);
   return mapped;
 }
