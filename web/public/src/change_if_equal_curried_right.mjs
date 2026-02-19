d7fec27790fe7276bf62385124f13c1315c81a0d
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:26:48 -0500

    ri ternary_equal change_if_equal

--- web/public/src/change_if_equal_curried_right.mjs
+++ web/public/src/change_if_equal_curried_right.mjs
@@ -1,5 +1,5 @@
 import { change_if_equal } from "../../../love/public/src/change_if_equal.mjs";
-export function ternary_equal_curried_right(to) {
+export function change_if_equal_curried_right(to) {
   let r = function ternary_equal_curried_right_result(item, from) {
     let result = change_if_equal(item, from, to);
     return result;
