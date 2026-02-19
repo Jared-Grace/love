6d756151b0badd62bad5c074de7a23cdfdca67f2
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:46:15 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/list_filter_index_is.mjs
+++ public/src/list_filter_index_is.mjs
@@ -1,7 +1,7 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { list_index_is_curry } from "../../../love/public/src/list_index_is_curry.mjs";
+import { list_index_is_curried } from "../../../love/public/src/list_index_is_curried.mjs";
 export function list_filter_index_is(list, indices) {
-  let filter = list_index_is_curry(list);
+  let filter = list_index_is_curried(list);
   let filtered = list_filter(indices, filter);
   return filtered;
 }
