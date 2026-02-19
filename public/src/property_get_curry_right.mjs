a819eb67ac8a64873e1a039ca73191a2820a59d3
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:45:31 -0500

    function_replace property_get_curry_right property_get_curried_right

--- public/src/property_get_curry_right.mjs
+++
@@ -1,8 +0,0 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-export function property_get_curry_right(property_name) {
-  let f = function object_property_get_curry_right_result(item) {
-    let value = property_get(item, property_name);
-    return value;
-  };
-  return f;
-}
