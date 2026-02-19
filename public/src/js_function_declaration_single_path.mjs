9b792f5cc3235407d58f60f3403d5e2a01e5966f
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:02:45 -0500

    function_auto_path ..\love\public\src\js_function_declaration_single_path.mjs

--- public/src/js_function_declaration_single_path.mjs
+++ public/src/js_function_declaration_single_path.mjs
@@ -1,7 +1,7 @@
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
-import { js_function_declaration_single_name } from "../../../love/public/src/js_function_declaration_single_name.mjs";
+import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
 export function js_function_declaration_single_path(ast) {
-  let f_name = js_function_declaration_single_name(ast);
+  let f_name = js_flo_name(ast);
   let f_path = function_name_to_path(f_name);
   return f_path;
 }
