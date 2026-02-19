5caf7c9bd7fcef663287179d48dd4ac7c7802b63
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:48:18 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- public/src/function_dependencies_code_call.mjs
+++ public/src/function_dependencies_code_call.mjs
@@ -1,3 +1,4 @@
+import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
 import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
@@ -7,6 +8,7 @@ export async function function_dependencies_code_call(f_name) {
   function lambda(dependencies) {
     let global_init = js_code_global_init();
     let call = js_code_call_statement(f_name);
+    let joined = list_join_newline(list);
     const code = `${global_init}
     ${dependencies}
     ${call}`;
