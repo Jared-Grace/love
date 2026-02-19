9ebac8e76976d072e1a985ad6ee71ab630efa7e7
Grace-Jared <christrosetolife@gmail.com>
2026-02-18 19:14:31 -0500

    function_auto_path ..\love\public\src\function_rename_open.mjs

--- public/src/function_rename_open.mjs
+++ public/src/function_rename_open.mjs
@@ -1,30 +1,7 @@
+import { function_rename } from "../../../love/public/src/function_rename.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
-import { function_move } from "../../../love/public/src/function_move.mjs";
-import { equal_not_assert } from "../../../love/public/src/equal_not_assert.mjs";
-import { function_rename_check } from "../../../love/public/src/function_rename_check.mjs";
-import { list_empty_is_assert_json } from "../../../love/public/src/list_empty_is_assert_json.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_identifiers_named } from "../../../love/public/src/js_identifiers_named.mjs";
-import { function_parse_unaliased } from "../../../love/public/src/function_parse_unaliased.mjs";
-import { function_rename_identifiers_alias } from "../../../love/public/src/function_rename_identifiers_alias.mjs";
-import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
-import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
 export async function function_rename_open(f_name_before, f_name_after) {
-  equal_not_assert(f_name_before, f_name_after);
-  await function_rename_check(f_name_after);
-  const v = await function_name_to_path_unalias(f_name_before);
-  let unaliased = property_get(v, "unaliased");
-  f_name_before = unaliased;
-  let v2 = await function_parse_unaliased(f_name_before);
-  let ast = property_get(v2, "ast");
-  let identifiers_named = js_identifiers_named(ast, f_name_after);
-  list_empty_is_assert_json(identifiers_named, {
-    f_name_after,
-    msg: "already exists in file as identifier",
-  });
-  await function_rename_fn_names_check(f_name_before);
-  await function_move(f_name_before, f_name_after);
-  await function_rename_identifiers_alias(f_name_before, f_name_after);
+  f_name_before = await function_rename(f_name_before, f_name_after);
   await function_open(f_name_after);
   return f_name_after;
 }
