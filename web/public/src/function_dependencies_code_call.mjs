08be37a0d074f391fa58c3872450f1941d4e3e95
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 12:35:25 -0500

    function_auto_path ..\love\public\src\function_dependencies_code_call.mjs

--- web/public/src/function_dependencies_code_call.mjs
+++ web/public/src/function_dependencies_code_call.mjs
@@ -4,9 +4,9 @@ import { js_code_call_statement } from "../../../love/public/src/js_code_call_st
 import { function_dependencies_code } from "../../../love/public/src/function_dependencies_code.mjs";
 export async function function_dependencies_code_call(f_name) {
   let global_init = js_code_global_init();
-  let v = await function_dependencies_code(f_name);
-  let dependencies = property_get(v, "code");
-  let externals = property_get(v, "externals");
+  let r = await function_dependencies_code(f_name);
+  let dependencies = property_get(r, "code");
+  let externals = property_get(r, "externals");
   let call = js_code_call_statement(f_name);
   const code = `${global_init}
     ${dependencies}
