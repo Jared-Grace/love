7c228f7586e9e007a75757884b3d52268a69b79e
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:18:02 -0500

    function_auto_path ..\love\public\src\app_shared_code_run.mjs

--- public/src/app_shared_code_run.mjs
+++ public/src/app_shared_code_run.mjs
@@ -1,9 +1,8 @@
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
-import { js_code_call_statement } from "../../../love/public/src/js_code_call_statement.mjs";
 import { js_code_global_init } from "../../../karate_code/public/src/js_code_global_init.mjs";
 export function app_shared_code_run(f_name, dependencies, code_call_get) {
   let global_init = js_code_global_init();
-  let call = js_code_call_statement(f_name);
+  let call = code_call_get(f_name);
   let code = list_join_newline([global_init, dependencies, call]);
   return code;
 }
