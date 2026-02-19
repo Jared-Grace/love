878ff804a10e68a25c867f4807474afaab152c58
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:28:46 -0500

    function_auto_path ..\love\public\src\js_visit_nodes.mjs

--- public/src/js_visit_nodes.mjs
+++ public/src/js_visit_nodes.mjs
@@ -1,9 +1,10 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { js_node_is } from "../../../love/public/src/js_node_is.mjs";
 export function js_visit_nodes(parsed, on_each) {
   function lambda(v) {
-    let { node } = v;
+    let node = property_get(v, "node");
     let a = js_node_is(node);
     if (not(a)) {
       return;
