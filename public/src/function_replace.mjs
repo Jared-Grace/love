2b10bfe79d4f5c5bcf52a2f66d8a2f6f4ea7d950
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 13:50:35 -0500

    ri function_rename_identifiers functions_identifiers_rename

--- public/src/function_replace.mjs
+++ public/src/function_replace.mjs
@@ -1,13 +1,13 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { function_rename_identifiers_alias } from "../../../love/public/src/function_rename_identifiers_alias.mjs";
+import { functions_identifiers_rename_alias } from "../../../love/public/src/functions_identifiers_rename_alias.mjs";
 import { function_rename_fn_names_check } from "../../../love/public/src/function_rename_fn_names_check.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
-import { function_delete } from "./function_delete.mjs";
+import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function function_replace(f_name_before, f_name_after) {
   const v = await function_name_to_path_unalias(f_name_before);
   let unaliased = property_get(v, "unaliased");
   f_name_before = unaliased;
   await function_rename_fn_names_check(f_name_before);
   await function_delete(f_name_before);
-  await function_rename_identifiers_alias(f_name_before, f_name_after);
+  await functions_identifiers_rename_alias(f_name_before, f_name_after);
 }
