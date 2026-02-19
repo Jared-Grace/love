75d41d900ba46dcf24399554f46cdd372a57fa84
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 16:10:22 -0500

    function_auto_path ..\love\public\src\ternary_equal_curried_right.mjs

--- public/src/ternary_equal_curried_right.mjs
+++ public/src/ternary_equal_curried_right.mjs
@@ -1,6 +1,8 @@
 import { ternary_equal } from "../../../love/public/src/ternary_equal.mjs";
 export function ternary_equal_curried_right(to) {
-  return function ternary_equal_curried_right_result(item, from) {
-    return ternary_equal(item, from, to);
+  let r = function ternary_equal_curried_right_result(item, from) {
+    let result = ternary_equal(item, from, to);
+    return result;
   };
+  return r;
 }
