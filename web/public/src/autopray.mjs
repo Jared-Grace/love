3fe8799182e72e8b99e8ab0ed9b1d0e078844f9d
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 15:46:57 -0500

    function_auto_path ..\love\public\src\autopray.mjs

--- web/public/src/autopray.mjs
+++ web/public/src/autopray.mjs
@@ -2,11 +2,9 @@ import { each_object } from "../../../love/public/src/each_object.mjs";
 import { text_may_the_lord } from "../../../love/public/src/text_may_the_lord.mjs";
 import { prayer_end } from "../../../love/public/src/prayer_end.mjs";
 import { prayer_start } from "../../../love/public/src/prayer_start.mjs";
-import { kjv } from "../../../love/public/src/kjv.mjs";
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export async function autopray() {
-  let v = kjv();
   function lambda2(verse_text, verse_reference) {
     let v2 = prayer_start();
     let v3 = prayer_end();
