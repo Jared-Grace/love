6bdf8e1a65d0eabc17441709ff004d657151b1ec
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 19:01:18 -0500

    function_auto_path ..\love\public\src\text_decompress.mjs

--- public/src/text_decompress.mjs
+++ public/src/text_decompress.mjs
@@ -1,12 +1,5 @@
-import { browser_is } from "../../../love/public/src/browser_is.mjs";
 export async function text_decompress(compressed) {
-  let LZModule = null;
-  if (browser_is()) {
-    LZModule = await import("lz-string");
-  } else {
-    const LZ = await import("lz-string");
-    LZModule = LZ;
-  }
+  let LZModule = await import("lz-string");
   const LZString = LZModule.default ?? LZModule;
   const result = LZString.decompressFromUTF16(compressed);
   return result;
