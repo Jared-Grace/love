df42ecc347380fc2f645a78e0a3ae26b7be5a965
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:12:24 -0500

    function_auto_path ..\love\public\src\vite_run.mjs

---
+++ web/public/src/vite_run_command.mjs
@@ -0,0 +1,8 @@
+import { file_exists_assert } from "../../../love/public/src/file_exists_assert.mjs";
+import { vite_config_path } from "../../../love/public/src/vite_config_path.mjs";
+export async function vite_run_command() {
+  let path = vite_config_path();
+  const command = "vite --config " + path;
+  await file_exists_assert(path);
+  return command;
+}
