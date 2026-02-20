9507fd1ac6eb1b776b04ad2593369781faafdd04
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:38:45 -0500

    fws function_name_to_path fn

---
+++ public/src/function_name_to_path_fn.mjs
@@ -0,0 +1,4 @@
+import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
+export function function_name_to_path_fn(f_name) {
+  return function_name_to_path(f_name);
+}
