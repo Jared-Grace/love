2789c8b2dcc158a14abde6a233f79ff7d7680dd4
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:01:03 -0500

    r assert_file_exists_not file_exists_not_assert

--- web/public/src/file_write_generic.mjs
+++ web/public/src/file_write_generic.mjs
@@ -1,5 +1,5 @@
-import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
+import { file_exists_not_assert } from "../../../love/public/src/file_exists_not_assert.mjs";
 export async function file_write_generic(f_path, overwrite, contents) {
-  await assert_file_exists_not(f_path);
+  await file_exists_not_assert(f_path);
   await overwrite(f_path, contents);
 }
