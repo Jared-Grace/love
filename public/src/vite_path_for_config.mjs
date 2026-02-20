888c608d6d73cf2fbccc360642bfeeb1dc01fa04
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:13:59 -0500

    function_auto_path ..\love\public\src\vite_path_for_config.mjs

--- public/src/vite_path_for_config.mjs
+++ public/src/vite_path_for_config.mjs
@@ -1,1 +1,4 @@
-export function vite_path_for_config() {}
+import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
+export function vite_path_for_config() {
+  folder_previous();
+}
