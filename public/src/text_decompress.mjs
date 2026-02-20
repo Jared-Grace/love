7887fdf4af772bcbadbbfe9ca4213ef492bbcda7
Grace-Jared <christrosetolife@gmail.com>
2026-02-19 19:01:51 -0500

    function_auto_path ..\love\public\src\text_decompress.mjs

--- public/src/text_decompress.mjs
+++ public/src/text_decompress.mjs
@@ -1,6 +1,6 @@
 export async function text_decompress(compressed) {
   let imported = await import("lz-string");
   const l = imported.default ?? imported;
-  const result = l.decompressFromUTF16(compressed);
-  return result;
+  const decompressed = l.decompressFromUTF16(compressed);
+  return decompressed;
 }
