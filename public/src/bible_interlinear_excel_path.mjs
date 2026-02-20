94995907459cf35ee5896f1a5b611d7a1edf4f6f
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:19:03 -0500

    function_auto_path ..\love\public\src\html_name_to_path.mjs

--- public/src/bible_interlinear_excel_path.mjs
+++ public/src/bible_interlinear_excel_path.mjs
@@ -1,5 +1,5 @@
-import { folder_user_combine } from "../../../love/public/src/folder_user_combine.mjs";
+import { folder_user_join } from "../../../love/public/src/folder_user_join.mjs";
 export function bible_interlinear_excel_path() {
-  let joined = folder_user_combine("downloads", "bsb_tables.xlsx");
+  let joined = folder_user_join("downloads", "bsb_tables.xlsx");
   return joined;
 }
