7fdab93bfa276aa94ba7206fda827ed63d462ad9
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:23:42 -0500

    function_auto_path ..\love\public\src\vite_config_json.mjs

--- public/src/vite_config_json.mjs
+++ public/src/vite_config_json.mjs
@@ -2,6 +2,6 @@ import { json_to } from "../../../love/public/src/json_to.mjs";
 import { vite_config } from "../../../love/public/src/vite_config.mjs";
 export async function vite_config_json() {
   let r = await vite_config();
-  let json = json_to(object);
-  return r;
+  let json = json_to(r);
+  return json;
 }
