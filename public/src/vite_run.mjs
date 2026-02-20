5dc5412cfb693e329c551b572f3c1c55907547ee
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:30:32 -0500

    pne vite_run_command lib_entry

--- public/src/vite_run.mjs
+++ public/src/vite_run.mjs
@@ -1,7 +1,7 @@
 import { vite_run_command } from "../../../love/public/src/vite_run_command.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function vite_run() {
-  const command = await vite_run_command();
+  const command = await vite_run_command(error());
   let stdout = await command_line(command);
   return stdout;
 }
