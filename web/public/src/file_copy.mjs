be0405936299c11042492591b3520947387d6626
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:01:03 -0500

    r assert_file_exists_not file_exists_not_assert

--- web/public/src/file_copy.mjs
+++ web/public/src/file_copy.mjs
@@ -1,6 +1,6 @@
 import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
-import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
+import { file_exists_not_assert } from "../../../love/public/src/file_exists_not_assert.mjs";
 export async function file_copy(file_path_old, file_path_new) {
-  await assert_file_exists_not(file_path_new);
+  await file_exists_not_assert(file_path_new);
   await file_copy_overwrite(file_path_old, file_path_new);
 }
--- web/public/src/html_new.mjs
+++ web/public/src/html_new.mjs
@@ -1,8 +1,8 @@
 import { app_new_html_update } from "../../../love/public/src/app_new_html_update.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
-import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
+import { file_exists_not_assert } from "../../../love/public/src/file_exists_not_assert.mjs";
 export async function html_new(name) {
   let file_path = html_name_to_path(name);
-  await assert_file_exists_not(file_path);
+  await file_exists_not_assert(file_path);
   await app_new_html_update(name);
 }
