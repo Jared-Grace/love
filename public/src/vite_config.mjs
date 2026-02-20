1b3675b3385091a12a1e1d88c2f1fb50214d5315
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:29:18 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- public/src/vite_config.mjs
+++ public/src/vite_config.mjs
@@ -2,15 +2,14 @@ import { vite_path_for_config } from "../../../love/public/src/vite_path_for_con
 import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
 import { defineConfig } from "vite";
 export async function vite_config() {
+  const entry = {
+    [app_context_initialize.name]: vite_path_for_config(app_context_initialize),
+    decompress: "src/text_decompress.js",
+  };
   let r = defineConfig({
     build: {
       lib: {
-        entry: {
-          [app_context_initialize.name]: vite_path_for_config(
-            app_context_initialize,
-          ),
-          decompress: "src/text_decompress.js",
-        },
+        entry: entry,
         name: "TextCompress",
         fileName: "text-compress",
         formats: ["iife"],
