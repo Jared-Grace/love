acfac38b600587024c1ab27f2811f287ba41fe2c
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:09:31 -0500

    o app_index

--- scripts/vite.config.mjs
+++ scripts/vite.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
-
 import { function_name_to_path } from '../public/src/function_name_to_path.mjs';
+function_name_to_path()
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/compress.js",
+      entry: {
+        compress: "src/text_compress.js",
+        decompress: "src/text_decompress.js"
+      },
       name: "TextCompress",
       fileName: "text-compress",
       formats: ["iife"]
