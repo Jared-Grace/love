1b2ff80cb0aa23b2b754c4003afd354902485231
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:48:13 -0500

    function_auto_path ..\love\public\src\vite_run.mjs

--- public/src/vite_run.mjs
+++ public/src/vite_run.mjs
@@ -1,6 +1,6 @@
-import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
+import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { vite_config_path } from "../../../love/public/src/vite_config_path.mjs";
 export async function vite_run() {
   let path = vite_config_path();
-  let u = await function_unalias_exists(f_name);
+  let exists = await file_exists(file_path);
 }
