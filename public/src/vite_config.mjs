f107ac13201d813299eb83cb58a352f0d01128df
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:31:35 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- public/src/vite_config.mjs
+++ public/src/vite_config.mjs
@@ -2,9 +2,8 @@ import { vite_config_entry_add } from "../../../love/public/src/vite_config_entr
 import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
 import { defineConfig } from "vite";
 export async function vite_config() {
-  let fn = app_context_initialize;
   const entry = {};
-  vite_config_entry_add(entry, fn);
+  vite_config_entry_add(entry, app_context_initialize);
   let r = defineConfig({
     build: {
       lib: {
