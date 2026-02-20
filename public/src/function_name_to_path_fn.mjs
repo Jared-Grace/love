3f639df8e6a7f3eaa174b34460805aac42c6bc58
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:42:34 -0500

    function_auto_path ..\love\public\src\function_name_to_path_fn.mjs

--- public/src/function_name_to_path_fn.mjs
+++ public/src/function_name_to_path_fn.mjs
@@ -1,5 +1,5 @@
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
-export function function_name_to_path_fn(f_name) {
-  let f_path = function_name_to_path(f_name);
+export function function_name_to_path_fn(fn) {
+  let f_path = function_name_to_path(fn.name);
   return f_path;
 }
