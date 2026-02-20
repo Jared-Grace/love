7e122dc89d4f777dfb2680bd2821936424ddc43e
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:59:36 -0500

    function_auto_path ..\love\public\src\npm_install.mjs

--- public/src/npm_install.mjs
+++ public/src/npm_install.mjs
@@ -1,6 +1,6 @@
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function npm_install(package_name) {
   let before = "";
-  const command = "npm install " + package_name + "@latest";
+  const command = "npm install " + before + package_name + "@latest";
   await command_line(command);
 }
