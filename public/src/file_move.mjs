59318169a9862456bd4e83b6a0e7ef77eb8861c7
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 16:01:03 -0500

    r assert_file_exists_not file_exists_not_assert

--- public/src/file_move.mjs
+++ public/src/file_move.mjs
@@ -1,14 +1,14 @@
 import { equal_by_lower } from "../../../love/public/src/equal_by_lower.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
-import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
+import { file_exists_not_assert } from "../../../love/public/src/file_exists_not_assert.mjs";
 export async function file_move(old_path, new_path) {
   if (equal(old_path, new_path)) {
     return;
   }
   let a = equal_by_lower(old_path, new_path);
   if (not(a)) {
-    await assert_file_exists_not(new_path);
+    await file_exists_not_assert(new_path);
   }
   const fs = await import("fs");
   await fs.promises.rename(old_path, new_path);
--- public/src/js_outside_move.mjs
+++ public/src/js_outside_move.mjs
@@ -6,14 +6,14 @@ import { each } from "../../../love/public/src/each.mjs";
 import { list_filter_property } from "../../../love/public/src/list_filter_property.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { function_new_declaration_from } from "../../../love/public/src/function_new_declaration_from.mjs";
-import { assert_file_exists_not } from "../../../love/public/src/assert_file_exists_not.mjs";
+import { file_exists_not_assert } from "../../../love/public/src/file_exists_not_assert.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function js_outside_move(ast) {
   let body = property_get(ast, "body");
   let fds = list_filter_property(body, "type", "FunctionDeclaration");
   async function lambda(fd) {
     let f_path = js_function_declaration_name_to_path(fd);
-    await assert_file_exists_not(f_path);
+    await file_exists_not_assert(f_path);
   }
   await each_async(fds, lambda);
   async function lambda2(fd) {
