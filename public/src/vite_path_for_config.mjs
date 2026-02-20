465093a9c64c012a5927685c3b93465c6a0ce302
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:24:07 -0500

    function_auto_path ..\love\public\src\vite_path_for_config.mjs

--- public/src/vite_path_for_config.mjs
+++ public/src/vite_path_for_config.mjs
@@ -1,7 +1,7 @@
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 export function vite_path_for_config(fn) {
-  let path = function_name_to_path(fn.name);
-  let joined = folder_previous_join(path);
-  return joined;
+  let f_path = function_name_to_path(fn.name);
+  let path = folder_previous_join(f_path);
+  return path;
 }
