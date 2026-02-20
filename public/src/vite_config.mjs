8f552a39920bda2d4c18d04a1edd7004c161e536
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 15:15:08 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- public/src/vite_config.mjs
+++ public/src/vite_config.mjs
@@ -1,1 +1,16 @@
-export function vite_config() {}
+import { defineConfig } from "vite";
+export function vite_config() {
+  return defineConfig({
+    build: {
+      lib: {
+        entry: {
+          compress: "src/text_compress.js",
+          decompress: "src/text_decompress.js",
+        },
+        name: "TextCompress",
+        fileName: "text-compress",
+        formats: ["iife"],
+      },
+    },
+  });
+}
