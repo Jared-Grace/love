6ef09dedf354697e9cebcc4ea250f965f7429de4
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 11:59:11 -0500

    function_auto_path ..\love\public\src\vite_run_fn.mjs

--- web/public/src/vite_run_fn.mjs
+++ web/public/src/vite_run_fn.mjs
@@ -1,10 +1,12 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { vite_run_command } from "../../../love/public/src/vite_run_command.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 export async function vite_run_fn(f_name) {
   let path = function_name_to_path(f_name);
-  const command = await vite_run_command(path, error());
+  let name = error();
+  const command = await vite_run_command(path, name);
   log_keep(command);
   let stdout = await command_line(command);
   return stdout;
