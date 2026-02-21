c4dc9583cabaf1e3e5159f6fc4ae9eee002a1daf
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:10:20 -0500

    rs vite_config_out_dir value

--- public/src/vite_config_out_dir_value.mjs
+++ public/src/vite_config_out_dir_value.mjs
@@ -1,7 +1,7 @@
 import { app_shared_name_latest_text } from "../../../love/public/src/app_shared_name_latest_text.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
-export function vite_config_out_dir() {
+export function vite_config_out_dir_value() {
   let p = folder_public();
   const folder = app_shared_name_latest_text();
   let path = path_join([p, folder]);
