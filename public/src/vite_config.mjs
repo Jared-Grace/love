e693c454023f794a2a450757db26bbf88eae69e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 19:54:10 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- public/src/vite_config.mjs
+++ public/src/vite_config.mjs
@@ -1,8 +1,12 @@
+import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
+import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
 import { defineConfig } from "vite";
 export async function vite_config() {
+  let path = function_name_to_path_fn(app_index_main);
   let c = defineConfig({
     build: {
       lib: {
+        entry: path,
         name: "ViteLib",
         formats: ["iife"],
         fileName: function lambda(format, entryName) {
