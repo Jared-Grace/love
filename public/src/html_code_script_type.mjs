afc307b282900135e536d5ce4a19398b54667739
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:18:01 -0500

    function_auto_path ..\love\public\src\html_code_script_type.mjs

--- public/src/html_code_script_type.mjs
+++ public/src/html_code_script_type.mjs
@@ -1,8 +1,10 @@
+import { text_between_equal } from "../../../love/public/src/text_between_equal.mjs";
 import { html_code_script_attributes } from "../../../love/public/src/html_code_script_attributes.mjs";
 export function html_code_script_type(script_type, middle) {
   const attributes = {
     type: script_type,
   };
+  let combined2 = text_between_equal(key, value);
   let c = html_code_script_attributes(attributes, middle);
   return c;
 }
