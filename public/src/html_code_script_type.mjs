a6acc5c1f5f33173a205fa221c7e401d51668acb
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:11:42 -0500

    function_auto_path ..\love\public\src\html_code_script_type.mjs

--- public/src/html_code_script_type.mjs
+++ public/src/html_code_script_type.mjs
@@ -1,4 +1,4 @@
-export function html_code_script_module_type(script_type, middle) {
+export function html_code_script_type(script_type, middle) {
   let v = `<script type="${script_type}"> 
     ${middle}
   </script>`;
