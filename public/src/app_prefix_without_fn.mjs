d7b9a8fe86bfe27dc4971ef7a9bfeaf1631df3fc
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:59:09 -0500

    function_auto_path ..\love\public\src\app_prefix_without_fn.mjs

--- public/src/app_prefix_without_fn.mjs
+++ public/src/app_prefix_without_fn.mjs
@@ -1,9 +1,6 @@
-import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
-import { app_shared_name_prefix } from "../../../love/public/src/app_shared_name_prefix.mjs";
-import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
+import { app_prefix_without } from "../../../love/public/src/app_prefix_without.mjs";
 export function app_prefix_without_fn(app_fn) {
   const name = app_fn.name;
-  let prefix = app_shared_name_prefix() + function_name_separator() + "";
-  let without = text_prefix_without(name, prefix);
+  let without = app_prefix_without(name);
   return without;
 }
