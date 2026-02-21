f4aae2197a4e75a55bea9bdf5199c018e3197dd7
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:49:38 -0500

    function_auto_path ..\love\public\src\app_shared_name_latest.mjs

--- public/src/app_shared_name_latest.mjs
+++ public/src/app_shared_name_latest.mjs
@@ -1,7 +1,9 @@
+import { app_shared_name_latest_text } from "../../../love/public/src/app_shared_name_latest_text.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
 export function app_shared_name_latest(name) {
   let a_name = app_shared_name_prefixed(name);
-  let combined = function_name_combine(a_name, "latest");
+  let right = app_shared_name_latest_text();
+  let combined = function_name_combine(a_name, right);
   return combined;
 }
