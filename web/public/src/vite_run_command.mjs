e27293ef50ad6a7a71c68c59fec2349c86c746ad
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:31:17 -0500

    function_auto_path ..\love\public\src\vite_run_command.mjs

--- web/public/src/vite_run_command.mjs
+++ web/public/src/vite_run_command.mjs
@@ -2,6 +2,7 @@ import { text_combine_multiple } from "../../../love/public/src/text_combine_mul
 import { file_exists_assert } from "../../../love/public/src/file_exists_assert.mjs";
 import { vite_config_path } from "../../../love/public/src/vite_config_path.mjs";
 export async function vite_run_command(lib_entry) {
+  let env_vars = {};
   let path = vite_config_path();
   let command = text_combine_multiple([
     "set LIB_ENTRY=public/src/app_index_main.mjs && set LIB_NAME=AppIndex && vite build --config ",
