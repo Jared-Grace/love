0351a7a1fbd1458cf0afcefa0e18efed28562370
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:59:06 -0500

    function_auto_path ..\love\public\src\vite_config_out_dir.mjs

--- public/src/vite_config_out_dir.mjs
+++ public/src/vite_config_out_dir.mjs
@@ -1,5 +1,7 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 export function vite_config_out_dir() {
-  let v = folder_public();
-  return v;
+  let p = folder_public();
+  let path = path_join([p]);
+  return p;
 }
