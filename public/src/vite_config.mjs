1aa1a282426c204104d48eafe5e3be1ed2b18adf
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:11:03 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- public/src/vite_config.mjs
+++ public/src/vite_config.mjs
@@ -1,6 +1,5 @@
 import { vite_config_out_dir } from "../../../love/public/src/vite_config_out_dir.mjs";
 import { process_env_trim } from "../../../love/public/src/process_env_trim.mjs";
-import { vite_config_out_dir_value } from "../../../love/public/src/vite_config_out_dir_value.mjs";
 import { vite_config_name } from "../../../love/public/src/vite_config_name.mjs";
 import { vite_config_lib_entry } from "../../../love/public/src/vite_config_lib_entry.mjs";
 import { vite_config_file_name_get } from "../../../love/public/src/vite_config_file_name_get.mjs";
@@ -10,12 +9,13 @@ export async function vite_config() {
   let entry = process_env_trim(ev_lib_entry);
   let ev_name = vite_config_name();
   let name = process_env_trim(ev_name);
-  vite_config_out_dir();
+  let ev_lib_entry2 = vite_config_out_dir();
+  let outDir = process_env_trim(ev_lib_entry2);
   let c = defineConfig({
     publicDir: false,
     build: {
       emptyOutDir: false,
-      outDir: vite_config_out_dir_value(),
+      outDir,
       lib: {
         entry,
         name: name,
