859a9241260627368751bc3a8ff52b37917dc914
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:15:32 -0500

    function_auto_path ..\love\public\src\html_name_to_path_latest.mjs

--- web/public/src/html_name_to_path_latest.mjs
+++ web/public/src/html_name_to_path_latest.mjs
@@ -1,3 +1,4 @@
+import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 import { app_shared_name_latest_text } from "../../../love/public/src/app_shared_name_latest_text.mjs";
 import { html_name_to_path_folder } from "../../../love/public/src/html_name_to_path_folder.mjs";
 export function html_name_to_path_latest(name) {
