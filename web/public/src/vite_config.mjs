ab16951b72928c65a2a23c262e1e6391e5d52203
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:01:28 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- web/public/src/vite_config.mjs
+++ web/public/src/vite_config.mjs
@@ -1,3 +1,4 @@
+import { folder_public } from "../../../love/public/src/folder_public.mjs";
 import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
 import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
 import { defineConfig } from "vite";
@@ -14,7 +15,7 @@ export async function vite_config() {
           let r = `${entryName}.js`;
           return r;
         },
-        outDir: "public",
+        outDir: folder_public(),
       },
     },
   });
