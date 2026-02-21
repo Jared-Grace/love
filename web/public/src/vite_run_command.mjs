ba4914b50537f6c49a416cb28d1de3ec8a350d5a
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:11:56 -0500

    function_auto_path ..\love\public\src\vite_run_command.mjs

--- web/public/src/vite_run_command.mjs
+++ web/public/src/vite_run_command.mjs
@@ -1,3 +1,4 @@
+import { vite_config_out_dir } from "../../../love/public/src/vite_config_out_dir.mjs";
 import { vite_config_name } from "../../../love/public/src/vite_config_name.mjs";
 import { vite_config_lib_entry } from "../../../love/public/src/vite_config_lib_entry.mjs";
 import { command_line_text_env_vars } from "../../../love/public/src/command_line_text_env_vars.mjs";
@@ -11,6 +12,7 @@ export async function vite_run_command(lib_entry, name) {
   let env_vars = {
     [vite_config_lib_entry()]: lib_entry,
     [vite_config_name()]: name,
+    [vite_config_out_dir()]: name,
   };
   let c = command_line_text_env_vars(env_vars, command);
   return c;
