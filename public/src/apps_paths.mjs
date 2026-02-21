b56d14f76eb7881c6c5a160a29c384418dc7d9f5
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:37:27 -0500

    function_auto_path ..\love\public\src\apps_paths.mjs

--- public/src/apps_paths.mjs
+++ public/src/apps_paths.mjs
@@ -1,9 +1,11 @@
+import { html_extension } from "../../../love/public/src/html_extension.mjs";
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
 export async function apps_paths() {
   let fop = folder_public();
-  let aps = await folder_read_files(fop);
-  let filtered = list_filter_ends_with(list, sufix);
+  let files = await folder_read_files(fop);
+  let sufix = html_extension();
+  let aps = list_filter_ends_with(files, sufix);
   return aps;
 }
