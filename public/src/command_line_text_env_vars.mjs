6805c093d700f9be9d1fade7f0cc4adc96f8a009
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:18:02 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

--- public/src/command_line_text_env_vars.mjs
+++ public/src/command_line_text_env_vars.mjs
@@ -1,1 +1,4 @@
-export function command_line_text_env_vars(dictionary) {}
+import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
+export function command_line_text_env_vars(dictionary) {
+  let list = object_to_list(o);
+}
