27a6d4c966432c448af32c840314002d5e2a6718
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:16:34 -0500

    function_auto_path ..\love\public\src\html_update_latest_new.mjs

--- web/public/src/html_update_latest_new.mjs
+++ web/public/src/html_update_latest_new.mjs
@@ -2,11 +2,13 @@ import { html_code_script_src } from "../../../love/public/src/html_code_script_
 import { html_overwrite } from "../../../love/public/src/html_overwrite.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
+import { file_name_js } from "./file_name_js.mjs";
 export async function html_update_latest_new(search) {
   let r = await app_shared_name_search_info(search);
   let f_path_latest = property_get(r, "f_path_latest");
   let a_name = property_get(r, "a_name");
   let f_name = property_get(r, "a_name");
-  let r2 = html_code_script_src(f_name, "");
+  let src = file_name_js(f_name);
+  let r2 = html_code_script_src(src, "");
   await html_overwrite(a_name, f_path_latest, joined);
 }
