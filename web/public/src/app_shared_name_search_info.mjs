6c15d605445d8bd13f74ead95c140cd4e22d1670
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:17:15 -0500

    function_auto_path ..\love\public\src\app_shared_name_search_info.mjs

--- web/public/src/app_shared_name_search_info.mjs
+++ web/public/src/app_shared_name_search_info.mjs
@@ -1,3 +1,5 @@
+import { file_name_js } from "../../../love/public/src/file_name_js.mjs";
+import { html_name_to_path_latest_generic } from "../../../love/public/src/html_name_to_path_latest_generic.mjs";
 import { html_name_to_path_latest } from "../../../love/public/src/html_name_to_path_latest.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
 import { app_shared_name_prefixed } from "../../../love/public/src/app_shared_name_prefixed.mjs";
@@ -10,6 +12,7 @@ export async function app_shared_name_search_info(search) {
   let app_name = app_shared_name_prefixed(a_name);
   let f_path = html_name_to_path(a_name);
   let f_path_latest = html_name_to_path_latest(a_name);
+  let src_path_latest = html_name_to_path_latest_generic(a_name, file_name_js);
   log_keep({
     search,
     a_name,
