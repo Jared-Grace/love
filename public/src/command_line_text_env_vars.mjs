73127e2d73b653268cae156f980f0ffad6d04888
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:20:27 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

--- public/src/command_line_text_env_vars.mjs
+++ public/src/command_line_text_env_vars.mjs
@@ -1,16 +1,13 @@
-import { text_between_equal } from "../../../love/public/src/text_between_equal.mjs";
+import { property_key_value_text_between_equal } from "../../../love/public/src/property_key_value_text_between_equal.mjs";
 import { list_join } from "../../../love/public/src/list_join.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 export function command_line_text_env_vars(env_vars, command) {
   let list = object_to_list(env_vars);
   function lambda(item) {
-    let key = property_get(item, "key");
-    let value = property_get(item, "value");
-    let combined2 = text_between_equal(key, value);
+    let combined2 = property_key_value_text_between_equal(item);
     let combined = text_combine_multiple(["set ", combined2]);
     return combined;
   }
