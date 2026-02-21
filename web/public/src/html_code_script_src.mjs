983a593c74d8029ecb4168cebd575d8745da17b7
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:44:16 -0500

    pd html_code_script_src middle

--- web/public/src/html_code_script_src.mjs
+++ web/public/src/html_code_script_src.mjs
@@ -1,5 +1,5 @@
 import { html_code_script_attributes } from "../../../love/public/src/html_code_script_attributes.mjs";
-export function html_code_script_src(src, middle) {
+export function html_code_script_src(src) {
   let c = html_code_script_attributes(
     {
       src,
