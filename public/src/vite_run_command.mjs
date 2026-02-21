4db8d8c4453e313d802a1fb6665554a8a3a54081
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 11:59:52 -0500

    function_auto_path ..\love\public\src\vite_run_command.mjs

--- public/src/vite_run_command.mjs
+++ public/src/vite_run_command.mjs
@@ -8,9 +8,8 @@ export async function vite_run_command(lib_entry, name) {
   let path = vite_config_path();
   await file_exists_assert(path);
   let command = text_combine_multiple(["vite build --config ", path]);
-  let env_var_name = vite_config_lib_entry();
   let env_vars = {
-    [env_var_name]: lib_entry,
+    [vite_config_lib_entry()]: lib_entry,
     [vite_config_name()]: name,
   };
   let c = command_line_text_env_vars(env_vars, command);
