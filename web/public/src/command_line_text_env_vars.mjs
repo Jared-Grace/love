950a5541ba280d1ef3e3263a7c33d7e5ef3abf91
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:31:23 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

--- web/public/src/command_line_text_env_vars.mjs
+++ web/public/src/command_line_text_env_vars.mjs
@@ -4,8 +4,8 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
-export function command_line_text_env_vars(dictionary, command) {
-  let list = object_to_list(dictionary);
+export function command_line_text_env_vars(env_vars, command) {
+  let list = object_to_list(env_vars);
   function lambda(item) {
     let key = property_get(item, "key");
     let value = property_get(item, "value");
