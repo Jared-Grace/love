ab4f343574c8823a5a44420e4f6f9295cd639085
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 11:25:55 -0500

    function_auto_path ..\love\public\src\vite_run.mjs

--- public/src/vite_run.mjs
+++ public/src/vite_run.mjs
@@ -1,16 +1,11 @@
+import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { app_shared_name_search_main } from "../../../love/public/src/app_shared_name_search_main.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
-import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
 import { vite_run_command } from "../../../love/public/src/vite_run_command.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function vite_run(search) {
-  let path = function_name_to_path_fn(app_index_main);
   let f_name = await app_shared_name_search_main(search);
+  let path = function_name_to_path(f_name);
   const command = await vite_run_command(path);
-  log({
-    command,
-  });
   let stdout = await command_line(command);
   return stdout;
 }
