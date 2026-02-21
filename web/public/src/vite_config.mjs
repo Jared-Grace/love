13e4824d8e53380e89025501009e44d709ca76c0
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:53:38 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- web/public/src/vite_config.mjs
+++ web/public/src/vite_config.mjs
@@ -1,10 +1,9 @@
+import { process_env } from "../../../love/public/src/process_env.mjs";
 import { vite_config_file_name_get } from "../../../love/public/src/vite_config_file_name_get.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
-import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
-import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
 import { defineConfig } from "vite";
 export async function vite_config() {
-  let path = function_name_to_path_fn(app_index_main);
+  let value = process_env(env_var_name);
   let c = defineConfig({
     publicDir: false,
     build: {
