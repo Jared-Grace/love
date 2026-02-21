a775cd550e481d611f34b8e45de2cdd1a7d89adf
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:25:11 -0500

    function_auto_path ..\love\public\src\html_code_script_attributes.mjs

--- public/src/html_code_script_attributes.mjs
+++ public/src/html_code_script_attributes.mjs
@@ -3,7 +3,7 @@ import { text_pad } from "../../../love/public/src/text_pad.mjs";
 import { text_between_equal } from "../../../love/public/src/text_between_equal.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 export function html_code_script_attributes(attributes, middle) {
-  let list = object_to_list(attributes);
+  let list = object_to_list(attributes);$x
   let mapped2 = list_map_property_key_value_text_between_equal(list);
   let padded = text_pad(s, '"');
   let combined2 = text_between_equal(key, padded);
