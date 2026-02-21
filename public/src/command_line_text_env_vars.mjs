3d63008d822acc0a9051c8b75a60659189e97a0c
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:19:57 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

--- public/src/command_line_text_env_vars.mjs
+++ public/src/command_line_text_env_vars.mjs
@@ -2,6 +2,8 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 export function command_line_text_env_vars(dictionary, command) {
   let list = object_to_list(dictionary);
-  function lambda(item) {}
+  function lambda(item) {
+    "set LIB_ENTRY=public/src/app_index_main.mjs";
+  }
   let mapped = list_map(list, lambda);
 }
