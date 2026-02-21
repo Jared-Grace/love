ef50568dbf7885e79047c7c6952f498ad3e2bc3c
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 15:01:20 -0500

    function_auto_path ..\love\public\src\app_index_main_fns_migrate.mjs

--- public/src/app_index_main_fns_migrate.mjs
+++ public/src/app_index_main_fns_migrate.mjs
@@ -1,1 +1,4 @@
-export function app_index_main_fns_migrate() {}
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+export async function app_index_main_fns_migrate() {
+  let output = await function_transform(f_name, async function lambda(ast) {});
+}
