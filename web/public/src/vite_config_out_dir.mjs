601d462d53638ccd0a69998ecfad62b11703b1f2
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:59:35 -0500

    function_auto_path ..\love\public\src\vite_config_out_dir.mjs

--- web/public/src/vite_config_out_dir.mjs
+++ web/public/src/vite_config_out_dir.mjs
@@ -1,7 +1,9 @@
+import { app_shared_name_latest_text } from "../../../love/public/src/app_shared_name_latest_text.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 export function vite_config_out_dir() {
   let p = folder_public();
-  let path = path_join([p]);
+  const folder = app_shared_name_latest_text();
+  let path = path_join([p, folder]);
   return path;
 }
