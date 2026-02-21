95e98a110107c7ef2f74b78956fbf01a9a9ac979
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:48:16 -0500

    function_auto_path ..\love\public\src\vite_run_fn_curried_right.mjs

--- public/src/vite_run_fn_curried_right.mjs
+++ public/src/vite_run_fn_curried_right.mjs
@@ -1,5 +1,5 @@
 import { vite_run_fn } from "../../../love/public/src/vite_run_fn.mjs";
-export async function vite_run_fn_curried_right(path_prefix) {
+export function vite_run_fn_curried_right(path_prefix) {
   let r2 = async function vite_run_fn_curried_right_result(f_name) {
     let r = await vite_run_fn(f_name, path_prefix);
     return r;
