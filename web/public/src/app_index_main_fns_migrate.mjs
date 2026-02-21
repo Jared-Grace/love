37b1722a2a0534d2306876b485aca67dcfb98eeb
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 15:44:58 -0500

    function_auto_path ..\love\public\src\app_index_main_fns_migrate.mjs

--- web/public/src/app_index_main_fns_migrate.mjs
+++ web/public/src/app_index_main_fns_migrate.mjs
@@ -1,11 +1,14 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { js_object_expression_properties } from "../../../love/public/src/js_object_expression_properties.mjs";
 import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
 import { app_index_main_fns } from "../../../love/public/src/app_index_main_fns.mjs";
 export async function app_index_main_fns_migrate() {
   async function lambda(ast) {
-    let properties = js_object_expression_properties(e);
-    function lambda2(v) {}
+    function lambda2(v) {
+      let node = property_get(v, "node");
+      let properties = js_object_expression_properties(e);
+    }
     js_visit_type(ast, "ObjectExpression", lambda2);
   }
   let output = await function_transform_fn(app_index_main_fns, lambda);
