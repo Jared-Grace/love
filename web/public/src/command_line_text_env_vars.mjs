3e45efff9cd57a6298a116a440483b703148abba
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:19:39 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

--- web/public/src/command_line_text_env_vars.mjs
+++ web/public/src/command_line_text_env_vars.mjs
@@ -3,5 +3,5 @@ import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 export function command_line_text_env_vars(dictionary, command) {
   let list = object_to_list(dictionary);
   function lambda(item) {}
-  let mapped = list_map(list2, lambda);
+  let mapped = list_map(list, lambda);
 }
