89e39f9dda259037543797758641fad85faeadd7
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 15:01:58 -0500

    is function_transform_fn

--- public/src/app_index_main_fns_migrate.mjs
+++ public/src/app_index_main_fns_migrate.mjs
@@ -2,5 +2,9 @@ import { app_index_main_fns } from "../../../love/public/src/app_index_main_fns.
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function app_index_main_fns_migrate() {
   async function lambda(ast) {}
-  let output = await function_transform(app_index_main_fns.name, lambda);
+  let output = await function_transform_fn(lambda);
 }
+function function_transform_fn(lambda) {
+    return function_transform(app_index_main_fns.name, lambda);
+}
+
