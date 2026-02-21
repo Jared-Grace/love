667243e8d166bd13658246d9e4b9a0f1c713b5d4
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 15:05:40 -0500

    function_auto_path ..\love\public\src\app_index_main_fns_migrate.mjs

--- public/src/app_index_main_fns_migrate.mjs
+++ public/src/app_index_main_fns_migrate.mjs
@@ -1,3 +1,4 @@
+import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { js_object_expression_properties } from "../../../love/public/src/js_object_expression_properties.mjs";
 import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
 import { app_index_main_fns } from "../../../love/public/src/app_index_main_fns.mjs";
@@ -5,6 +6,7 @@ export async function app_index_main_fns_migrate() {
   async function lambda(ast) {
     let properties = js_object_expression_properties(e);
     ("ObjectExpression");
+    js_visit_type(ast2, type, function lambda2(v) {});
   }
   let output = await function_transform_fn(app_index_main_fns, lambda);
 }
