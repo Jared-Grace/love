44f523c6480a9086c27daf1bb5407ee908644bff
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:38:04 -0500

    function_auto_path ..\love\public\src\html_code_script_src.mjs

--- public/src/html_code_script_src.mjs
+++ public/src/html_code_script_src.mjs
@@ -1,5 +1,5 @@
 import { html_code_script_attributes } from "../../../love/public/src/html_code_script_attributes.mjs";
-export function html_code_script_src(src) {
+export function html_code_script_src(src, middle) {
   let r = html_code_script_attributes(
     {
       src,
