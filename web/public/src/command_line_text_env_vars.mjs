88edeae924160e6a3f44a7263f8db47d8e9ea9fa
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:21:00 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

--- web/public/src/command_line_text_env_vars.mjs
+++ web/public/src/command_line_text_env_vars.mjs
@@ -6,13 +6,12 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 export function command_line_text_env_vars(env_vars, command) {
   let list = object_to_list(env_vars);
-  let mapped2 = list_map(list2, property_key_value_text_between_equal);
+  let mapped2 = list_map(list, property_key_value_text_between_equal);
   function lambda(item) {
-    let combined2 = property_key_value_text_between_equal(item);
     let combined = text_combine_multiple(["set ", combined2]);
     return combined;
   }
-  let mapped = list_map(list, lambda);
+  let mapped = list_map(mapped2, lambda);
   list_add(mapped, command);
   let c = list_join(mapped, " && ");
   return c;
