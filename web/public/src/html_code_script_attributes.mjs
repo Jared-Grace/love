6707a3bc5e40a1377cc1587db15349bbede6202e
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:16:05 -0500

    function_auto_path ..\love\public\src\html_code_script_attributes.mjs

--- web/public/src/html_code_script_attributes.mjs
+++ web/public/src/html_code_script_attributes.mjs
@@ -1,3 +1,4 @@
+import { object_to_list } from "../../../love/public/src/object_to_list.mjs";
 export function html_code_script_attributes(attributes, middle) {
   let list = object_to_list(env_vars);
   let c = `<script${attributes}> 
