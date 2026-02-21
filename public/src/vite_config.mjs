c155aba3e26568c64e6208ca87cb5c6939b9a85a
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:09:12 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- public/src/vite_config.mjs
+++ public/src/vite_config.mjs
@@ -1,14 +1,13 @@
+import { process_env_trim } from "../../../love/public/src/process_env_trim.mjs";
 import { vite_config_out_dir } from "../../../love/public/src/vite_config_out_dir.mjs";
 import { vite_config_name } from "../../../love/public/src/vite_config_name.mjs";
-import { text_trim } from "../../../love/public/src/text_trim.mjs";
 import { vite_config_lib_entry } from "../../../love/public/src/vite_config_lib_entry.mjs";
 import { process_env } from "../../../love/public/src/process_env.mjs";
 import { vite_config_file_name_get } from "../../../love/public/src/vite_config_file_name_get.mjs";
 import { defineConfig } from "vite";
 export async function vite_config() {
   let ev_lib_entry = vite_config_lib_entry();
-  let entry = process_env(ev_lib_entry);
-  entry = text_trim(entry);
+  let entry = process_env_trim(ev_lib_entry);
   let ev_name = vite_config_name();
   let name = process_env(ev_name);
   let c = defineConfig({
