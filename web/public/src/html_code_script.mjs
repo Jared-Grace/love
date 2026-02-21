90447bea949aade1c2bb165753dc7da246033c79
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:14:26 -0500

    fwlr html_code_script_attributes

---
+++ web/public/src/html_code_script.mjs
@@ -0,0 +1,4 @@
+import { html_code_script_attributes } from "../../../love/public/src/html_code_script_attributes.mjs";
+export function html_code_script(attributes, middle) {
+  return html_code_script_attributes(attributes, middle);
+}
