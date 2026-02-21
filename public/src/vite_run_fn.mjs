ae10a31381abeff82da6800db07cca8e7ff6c2af
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:45:40 -0500

    pne vite_run_fn path_prefix

--- public/src/vite_run_fn.mjs
+++ public/src/vite_run_fn.mjs
@@ -3,7 +3,7 @@ import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { vite_run_command } from "../../../love/public/src/vite_run_command.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
-export async function vite_run_fn(f_name) {
+export async function vite_run_fn(f_name, path_prefix) {
   let path = function_name_to_path(f_name);
   let name = path_name(path);
   const command = await vite_run_command(path, name);
