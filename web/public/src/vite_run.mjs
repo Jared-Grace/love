71e86515113e8203d67af9417947e660f95f03d0
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 11:20:10 -0500

    function_auto_path ..\love\public\src\vite_run.mjs

--- web/public/src/vite_run.mjs
+++ web/public/src/vite_run.mjs
@@ -3,7 +3,7 @@ import { function_name_to_path_fn } from "../../../love/public/src/function_name
 import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
 import { vite_run_command } from "../../../love/public/src/vite_run_command.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
-export async function vite_run() {
+export async function vite_run(search) {
   let path = function_name_to_path_fn(app_index_main);
   const command = await vite_run_command(path);
   log({
