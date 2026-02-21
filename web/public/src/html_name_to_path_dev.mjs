4d291db2724171e310d206836771a64b2ad68598
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:15:32 -0500

    function_auto_path ..\love\public\src\html_name_to_path_dev.mjs

--- web/public/src/html_name_to_path_dev.mjs
+++ web/public/src/html_name_to_path_dev.mjs
@@ -1,3 +1,4 @@
+import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 import { html_name_to_path_folder } from "../../../love/public/src/html_name_to_path_folder.mjs";
 export function html_name_to_path_dev(name) {
   const folder = "dev";
