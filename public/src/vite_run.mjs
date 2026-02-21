5fb99dd65255bbaf2d4176ad553e593913709aee
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 11:29:11 -0500

    function_auto_path ..\love\public\src\vite_run.mjs

--- public/src/vite_run.mjs
+++ public/src/vite_run.mjs
@@ -1,7 +1,9 @@
+import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
 import { vite_run_fn } from "../../../love/public/src/vite_run_fn.mjs";
 import { app_shared_name_search_main } from "../../../love/public/src/app_shared_name_search_main.mjs";
 export async function vite_run(search) {
   let f_name = await app_shared_name_search_main(search);
+  let f_names = [f_name, app_context_initialize.name];
   let stdout = await vite_run_fn(f_name);
   return stdout;
 }
