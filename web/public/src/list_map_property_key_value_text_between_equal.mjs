d3c17b459301841a42f056b63c927f6eccdd1a11
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:21:32 -0500

    function_auto_path ..\love\public\src\command_line_text_env_vars.mjs

---
+++ web/public/src/list_map_property_key_value_text_between_equal.mjs
@@ -0,0 +1,6 @@
+import { property_key_value_text_between_equal } from "../../../love/public/src/property_key_value_text_between_equal.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+export function list_map_property_key_value_text_between_equal(list) {
+  let mapped3 = list_map(list, property_key_value_text_between_equal);
+  return mapped3;
+}
