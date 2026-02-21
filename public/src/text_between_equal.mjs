287e35134f90bb2d4c4f36fcfa9548d6fcce8d36
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:17:34 -0500

    function_auto_path ..\love\public\src\text_between_equal.mjs

--- public/src/text_between_equal.mjs
+++ public/src/text_between_equal.mjs
@@ -1,5 +1,5 @@
 import { text_combine_3 } from "../../../love/public/src/text_combine_3.mjs";
 export function text_between_equal(key, value) {
-  let combined3 = text_combine_3(key, "=", value);
-  return combined3;
+  let combined = text_combine_3(key, "=", value);
+  return combined;
 }
