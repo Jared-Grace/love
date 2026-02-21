2e24daeb954afd8cd3d8e0ebad1545e50179a6bb
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 13:52:26 -0500

    function_auto_path ..\love\public\src\vite_run.mjs

--- public/src/vite_run.mjs
+++ public/src/vite_run.mjs
@@ -1,3 +1,4 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_shared_name_latest_text } from "../../../love/public/src/app_shared_name_latest_text.mjs";
 import { vite_run_fn_curried_right } from "../../../love/public/src/vite_run_fn_curried_right.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
@@ -6,7 +7,8 @@ import { app_shared_name_search_main } from "../../../love/public/src/app_shared
 export async function vite_run(search) {
   let f_name = await app_shared_name_search_main(search);
   let f_names = [f_name, app_context_initialize.name];
-  let latest = app_shared_name_latest_text();tc
+  let latest = app_shared_name_latest_text();
+  let combined = text_combine(left, right);
   let r2 = vite_run_fn_curried_right(path_prefix);
   let waited = await list_map_unordered_async(f_names, r2);
   return waited;
