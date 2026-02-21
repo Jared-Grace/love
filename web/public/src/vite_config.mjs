f7a8ad3d6e8ec2e0404814ade2d599e5f07bd8f9
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:13:11 -0500

    function_auto_path ..\love\public\src\vite_config.mjs

--- web/public/src/vite_config.mjs
+++ web/public/src/vite_config.mjs
@@ -1,4 +1,4 @@
-import { file_extension_js } from "../../../love/public/src/file_extension_js.mjs";
+import { file_name_js } from "../../../love/public/src/file_name_js.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
 import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
@@ -13,8 +13,7 @@ export async function vite_config() {
         name: "ViteLib",
         formats: ["iife"],
         fileName: function lambda(format, entryName) {
-          let r2 = file_extension_js();
-          let r = `${entryName}${r2}`;
+          let r = file_name_js(entryName);
           return r;
         },
         outDir: folder_public(),
