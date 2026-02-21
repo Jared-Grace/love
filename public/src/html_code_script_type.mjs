b8c914b4a64c2ff57f4a271bc5f2181c44b41d71
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 12:16:00 -0500

    function_auto_path ..\love\public\src\html_code_script_type.mjs

--- public/src/html_code_script_type.mjs
+++ public/src/html_code_script_type.mjs
@@ -1,6 +1,8 @@
 import { html_code_script_attributes } from "../../../love/public/src/html_code_script_attributes.mjs";
 export function html_code_script_type(script_type, middle) {
-  const attributes = ` type="${script_type}"`;
+  const attributes = {
+    type: script_type,
+  };
   let c = html_code_script_attributes(attributes, middle);
   return c;
 }
