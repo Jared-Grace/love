4e89909158c410a3fd076f470d35e104702e6b2d
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 14:58:56 -0500

    function_auto_path ..\love\public\src\app_prefix_without_fn.mjs

--- public/src/app_prefix_without_fn.mjs
+++ public/src/app_prefix_without_fn.mjs
@@ -1,7 +1,7 @@
 import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 import { app_shared_name_prefix } from "../../../love/public/src/app_shared_name_prefix.mjs";
 import { function_name_separator } from "../../../love/public/src/function_name_separator.mjs";
-export function app_prefix_without(app_fn) {
+export function app_prefix_without_fn(app_fn) {
   const name = app_fn.name;
   let prefix = app_shared_name_prefix() + function_name_separator() + "";
   let without = text_prefix_without(name, prefix);
