fa4bdc80b1ff1e64df4d0b08ac4d3ac1faf46527
Grace-Jared <christrosetolife@gmail.com>
2026-02-20 20:52:46 -0500

    function_auto_path ..\love\public\src\process_env.mjs

--- web/public/src/process_env.mjs
+++ web/public/src/process_env.mjs
@@ -1,1 +1,4 @@
-export function process_env() {}
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export function process_env() {
+  let value = property_get(object, property_name);
+}
