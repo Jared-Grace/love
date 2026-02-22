0855bba17e96c1313053e6ecff50b5b33e1ba6c8
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:12:08 -0500

    rs js_call args

--- web/public/src/js_call_empty.mjs
+++ web/public/src/js_call_empty.mjs
@@ -1,5 +1,5 @@
-import { js_call } from "../../../love/public/src/js_call.mjs";
+import { js_call_args } from "../../../love/public/src/js_call_args.mjs";
 export function js_call_empty(f_name_new) {
-  let parsed2 = js_call(f_name_new, []);
+  let parsed2 = js_call_args(f_name_new, []);
   return parsed2;
 }
