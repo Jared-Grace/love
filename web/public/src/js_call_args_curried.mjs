97ae6f8a218eb7e4bbd8fc65c09956de50c62e7e
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:17:32 -0500

    function_auto_path ..\love\public\src\js_call_args_curried.mjs

--- web/public/src/js_call_args_curried.mjs
+++ web/public/src/js_call_args_curried.mjs
@@ -1,6 +1,8 @@
 import { js_call_args } from "../../../love/public/src/js_call_args.mjs";
 export function js_call_args_curried(f_name) {
-  return function js_call_args_curried_result(args_code) {
-    return js_call_args(f_name, args_code);
+  let r = function js_call_args_curried_result(args_code) {
+    let parsed = js_call_args(f_name, args_code);
+    return parsed;
   };
+  return r;
 }
