6e2d4b29de85db9773b1da20d56afa322fd8c12a
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:36:15 -0500

    curc property_get_exists 2

---
+++ public/src/property_get_exists_curried_right_2.mjs
@@ -0,0 +1,9 @@
+import { property_get_exists } from "../../../love/public/src/property_get_exists.mjs";
+export function property_get_exists_curried_right_2(
+  property_name_a,
+  property_name_b,
+) {
+  return function property_get_exists_curried_right_2_result(item3) {
+    return property_get_exists(item3, property_name_a, property_name_b);
+  };
+}
