263b32bbe5eb4a876d4934abfd3f210a3fb1a746
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:40:09 -0500

    function_auto_path ..\love\public\src\js_identifier_named_curried_right.mjs

--- web/public/src/js_identifier_named_curried_right.mjs
+++ web/public/src/js_identifier_named_curried_right.mjs
@@ -1,6 +1,8 @@
 import { js_identifier_named } from "../../../love/public/src/js_identifier_named.mjs";
 export function js_identifier_named_curried_right(identifier_name) {
-  return function js_identifier_named_curried_right_result(i) {
-    return js_identifier_named(i, identifier_name);
+  let r2 = function js_identifier_named_curried_right_result(i) {
+    let r = js_identifier_named(i, identifier_name);
+    return r;
   };
+  return r2;
 }
