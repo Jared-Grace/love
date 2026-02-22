8e01294eb38293dabee0b42b2d8b94e4641d67f3
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 12:57:26 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_old.mjs

--- web/public/src/firebase_storage_download_old.mjs
+++ web/public/src/firebase_storage_download_old.mjs
@@ -1,22 +1,14 @@
 import { html_loading } from "../../../love/public/src/html_loading.mjs";
 import { http } from "../../../love/public/src/http.mjs";
-import { firebase_bucket } from "../../../love/public/src/firebase_bucket.mjs";
-import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { firebase_path_fix } from "../../../love/public/src/firebase_path_fix.mjs";
 import { firebase_storage_url } from "../../../love/public/src/firebase_storage_url.mjs";
 export async function firebase_storage_download_old(destination) {
   destination = firebase_path_fix(destination);
-  let b = browser_is();
-  if (b) {
-    async function lambda2() {
-      let url = await firebase_storage_url(destination);
-      let buffer = await http(url);
-      return buffer;
-    }
-    let result = await html_loading(lambda2);
-    return result;
+  async function lambda2() {
+    let url = await firebase_storage_url(destination);
+    let buffer = await http(url);
+    return buffer;
   }
-  const bucket = await firebase_bucket();
-  let [buffer] = await bucket.file(destination).download();
-  return buffer;
+  let result = await html_loading(lambda2);
+  return result;
 }
