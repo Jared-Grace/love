9939ccdb9137ddd73ac2630c48dc902db0076e1b
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 20:32:29 -0500

    rs vite_config build

--- public/src/vite_config_json.mjs
+++ public/src/vite_config_json.mjs
@@ -1,7 +1,7 @@
 import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
-import { vite_config } from "../../../love/public/src/vite_config.mjs";
+import { vite_config_build } from "../../../love/public/src/vite_config_build.mjs";
 export async function vite_config_json() {
-  let r = await vite_config();
+  let r = await vite_config_build();
   let json = json_format_to(r);
   return json;
 }
