f39b138fb2da60781f0a8ac7adbe3c8a31d8c1fa
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 20:25:41 -0500

    functions_rename_if_starts_with_prefix_suffix_add vite_run build

--- web/public/src/vite_run_build.mjs
+++ web/public/src/vite_run_build.mjs
@@ -1,7 +1,7 @@
-import { vite_run_fn } from "../../../love/public/src/vite_run_fn.mjs";
+import { vite_run_build_fn } from "../../../love/public/src/vite_run_build_fn.mjs";
 import { app_shared_name_search_main } from "../../../love/public/src/app_shared_name_search_main.mjs";
-export async function vite_run(search) {
+export async function vite_run_build(search) {
   let f_name = await app_shared_name_search_main(search);
-  let stdout = await vite_run_fn(f_name);
+  let stdout = await vite_run_build_fn(f_name);
   return stdout;
 }
