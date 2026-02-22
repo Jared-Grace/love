9751722a6dab758bd97699972bb9f5618a90cab9
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 13:42:56 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -3,18 +3,13 @@ import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitign
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { git_add } from "../../../love/public/src/git_add.mjs";
 import { file_transform } from "../../../love/public/src/file_transform.mjs";
-import { text_between_newline } from "../../../love/public/src/text_between_newline.mjs";
 import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_push } from "../../../love/public/src/git_push.mjs";
 export async function git_remove(f_path) {
   let g_name = git_ignore_name();
   await command_line_git("rm -r --cached " + f_path);
-  let r2 = text_between_newline_curried_right(b);
-  function lambda(before) {
-    let after = text_between_newline(before, f_path);
-    return after;
-  }
+  let lambda = text_between_newline_curried_right(f_path);
   await file_transform(g_name, lambda);
   let added = g_name;
   await git_add(added);
