71b75ae00c1f1dfb5fa8be585dc49908794afb90
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:31:31 -0500

    rrl property_equals_lambda curry_right_2

--- web/public/src/list_filter_property.mjs
+++ web/public/src/list_filter_property.mjs
@@ -1,7 +1,7 @@
-import { property_equals_lambda } from "../../../love/public/src/property_equals_lambda.mjs";
+import { property_equals_curry_right_2 } from "../../../love/public/src/property_equals_curry_right_2.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_property(list, property_name, property_value) {
-  let filter = property_equals_lambda(property_name, property_value);
+  let filter = property_equals_curry_right_2(property_name, property_value);
   let filtered = list_filter(list, filter);
   return filtered;
 }
