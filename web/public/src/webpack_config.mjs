ef7043e02dfeac116b6de1c73408d619884eac33
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 15:18:07 -0500

    function_auto_path ..\love\public\src\webpack_config.mjs

--- web/public/src/webpack_config.mjs
+++ web/public/src/webpack_config.mjs
@@ -3,6 +3,7 @@ import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
 import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
 import path from "path";
 import TerserPlugin from "terser-webpack-plugin";
+import webpack from "webpack";
 export function webpack_config() {
   let result = function_name_to_path_fn(app_reply_main);
   let r = {
