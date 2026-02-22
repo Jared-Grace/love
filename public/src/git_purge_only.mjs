063c93d0ad3015bd6677771d78a5076cc8ac0208
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 13:20:44 -0500

    function_auto_path ..\love\public\src\git_purge_only.mjs

--- public/src/git_purge_only.mjs
+++ public/src/git_purge_only.mjs
@@ -3,7 +3,7 @@ import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function git_purge_only(f_path) {
   await command_line("pip install git-filter-repo");
   let stdout = await command_line(
-    "filter-repo  --force --path " + f_path + " --invert-paths",
+    "python -m git_filter_repo --force --path " + f_path + " --invert-paths",
   );
   await git_purge_only_after();
 }
