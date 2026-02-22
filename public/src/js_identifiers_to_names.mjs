5a142c1dfae89c438c7eae81fd787cd12d334f10
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:15:33 -0500

    function_auto_path ..\love\public\src\js_identifiers_to_names.mjs

--- public/src/js_identifiers_to_names.mjs
+++ public/src/js_identifiers_to_names.mjs
@@ -1,11 +1,9 @@
+import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_all_assert } from "../../../love/public/src/list_all_assert.mjs";
 import { js_identifier_is } from "../../../love/public/src/js_identifier_is.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export function js_identifiers_to_names(identifiers) {
   list_all_assert(identifiers, js_identifier_is);
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
-  const names = list_map_property(identifiers, "name");
+  let names = list_map(list, js_identifier_name);
   return names;
 }
