ecd0c4990c2523269fc756d3b06d55a72a82d6ba
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 20:32:29 -0500

    rs vite_config build

--- public/src/vite_config_build.mjs
+++ public/src/vite_config_build.mjs
@@ -4,7 +4,7 @@ import { process_env_trim } from "../../../love/public/src/process_env_trim.mjs"
 import { vite_config_name } from "../../../love/public/src/vite_config_name.mjs";
 import { vite_config_lib_entry } from "../../../love/public/src/vite_config_lib_entry.mjs";
 import { vite_config_file_name_get } from "../../../love/public/src/vite_config_file_name_get.mjs";
-export async function vite_config() {
+export async function vite_config_build() {
   let ev_lib_entry = vite_config_lib_entry();
   let entry = process_env_trim(ev_lib_entry);
   let ev_name = vite_config_name();
