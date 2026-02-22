c1bea915af10f67c17926e372edef95b7fc10e90
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:40:24 -0500

    function_auto_path ..\love\public\src\js_identifiers_named.mjs

--- public/src/js_identifiers_named.mjs
+++ public/src/js_identifiers_named.mjs
@@ -1,14 +1,9 @@
 import { js_identifier_named_curried_right } from "../../../love/public/src/js_identifier_named_curried_right.mjs";
-import { js_identifier_named } from "../../../love/public/src/js_identifier_named.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { js_identifiers } from "../../../love/public/src/js_identifiers.mjs";
 export function js_identifiers_named(ast, identifier_name) {
   let identifiers = js_identifiers(ast);
-  let r2 = js_identifier_named_curried_right(identifier_name2);
-  function lambda(i) {
-    let v = js_identifier_named(i, identifier_name);
-    return v;
-  }
-  let identifiers_named = list_filter(identifiers, lambda);
+  let r2 = js_identifier_named_curried_right(identifier_name);
+  let identifiers_named = list_filter(identifiers, lamr2da);
   return identifiers_named;
 }
