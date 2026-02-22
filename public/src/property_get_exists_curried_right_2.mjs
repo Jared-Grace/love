af77a746c94980849dae217824a1fee7fda713f6
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:36:17 -0500

    function_auto_path ..\love\public\src\property_get_exists_curried_right_2.mjs

--- public/src/property_get_exists_curried_right_2.mjs
+++ public/src/property_get_exists_curried_right_2.mjs
@@ -3,7 +3,9 @@ export function property_get_exists_curried_right_2(
   property_name_a,
   property_name_b,
 ) {
-  return function property_get_exists_curried_right_2_result(item3) {
-    return property_get_exists(item3, property_name_a, property_name_b);
+  let r = function property_get_exists_curried_right_2_result(item3) {
+    let e = property_get_exists(item3, property_name_a, property_name_b);
+    return e;
   };
+  return r;
 }
