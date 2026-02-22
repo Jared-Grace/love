c3302e22002ffea5cd6a5110edf7161b0d600a64
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 14:36:40 -0500

    function_auto_path ..\love\public\src\webpack_config.mjs

--- web/public/src/webpack_config.mjs
+++ web/public/src/webpack_config.mjs
@@ -1,10 +1,11 @@
+import { app_reply_main } from "../../../love/public/src/app_reply_main.mjs";
 import { function_name_to_path_fn } from "../../../love/public/src/function_name_to_path_fn.mjs";
 import path from "path";
 import TerserPlugin from "terser-webpack-plugin";
 export function webpack_config() {
   let r = {
     mode: "production",
-    entry: function_name_to_path_fn,
+    entry: function_name_to_path_fn(app_reply_main),
     output: {
       filename: "bundle.js",
       path: path.resolve("./dist"),
