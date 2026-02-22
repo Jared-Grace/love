fbb1a871b801da9faca8028b0437388de5624718
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:35:53 -0500

    function_auto_path ..\love\public\src\property_get_exists.mjs

--- public/src/property_get_exists.mjs
+++ public/src/property_get_exists.mjs
@@ -1,7 +1,7 @@
 import { property_exists } from "../../../love/public/src/property_exists.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function property_get_exists(item3, property_name2, property_name) {
-  let value = property_get(item3, property_name2);
-  let r2 = property_exists(value, property_name);
+export function property_get_exists(item3, property_name_a, property_name_b) {
+  let value = property_get(item3, property_name_a);
+  let r2 = property_exists(value, property_name_b);
   return r2;
 }
