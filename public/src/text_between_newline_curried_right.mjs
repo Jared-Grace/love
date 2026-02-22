faa5fa78dc0b6c8b7b85196c137736298b2e45d7
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 13:42:39 -0500

    function_auto_path ..\love\public\src\text_between_newline_curried_right.mjs

--- public/src/text_between_newline_curried_right.mjs
+++ public/src/text_between_newline_curried_right.mjs
@@ -1,6 +1,8 @@
 import { text_between_newline } from "../../../love/public/src/text_between_newline.mjs";
 export function text_between_newline_curried_right(b) {
-  return function text_between_newline_curried_right_result(a) {
-    return text_between_newline(a, b);
+  let r2 = function text_between_newline_curried_right_result(a) {
+    let r = text_between_newline(a, b);
+    return r;
   };
+  return r2;
 }
