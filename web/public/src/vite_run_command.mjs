b7375ec5a40cc2926d429033000ad6f07dad124e
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 20:29:15 -0500

    function_auto_path ..\love\public\src\vite_run_command.mjs

--- web/public/src/vite_run_command.mjs
+++ web/public/src/vite_run_command.mjs
@@ -1,6 +1,6 @@
 import { noop } from "../../../love/public/src/noop.mjs";
 import { vite_run_build_generic } from "../../../love/public/src/vite_run_build_generic.mjs";
-export async function vite_run_command(lib_entry, name) {
-  let c = await vite_run_build_generic(noop, lib_entry, name);
+export async function vite_run_command() {
+  let c = await vite_run_build_generic(noop, "", "");
   return c;
 }
