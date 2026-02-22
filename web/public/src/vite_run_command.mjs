5182d57738f976f387dcb8f201c4b126b03d90c8
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 20:27:29 -0500

    cpd vite_run_build_command build

---
+++ web/public/src/vite_run_command.mjs
@@ -0,0 +1,9 @@
+import { vite_run_build_generic } from "../../../love/public/src/vite_run_build_generic.mjs";
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+export async function vite_run_command(lib_entry, name) {
+  let c = await vite_run_build_generic(command_transform, lib_entry, name);
+  return c;
+  function command_transform(command_parts) {
+    list_insert(command_parts, 1, "build");
+  }
+}
