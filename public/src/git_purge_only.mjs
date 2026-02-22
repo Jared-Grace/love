7a23a685e2f128732e88b57845f03a4a747332bc
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 13:21:11 -0500

    function_auto_path ..\love\public\src\git_purge_only.mjs

--- public/src/git_purge_only.mjs
+++ public/src/git_purge_only.mjs
@@ -1,9 +1,11 @@
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { git_purge_only_after } from "../../../love/public/src/git_purge_only_after.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function git_purge_only(f_path) {
   await command_line("pip install git-filter-repo");
+  let combined = text_combine_multiple(list);
   let stdout = await command_line(
-    "python -m git_filter_repo --force --path " + f_path + " --invert-paths",
+    " -m git_filter_repo --force --path " + f_path + " --invert-paths",
   );
   await git_purge_only_after();
 }
