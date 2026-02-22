6a5723c7df2f29a6b195087f9f24544f07b51ace
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 14:56:01 -0500

    function_auto_path ..\love\public\src\path_normalize.mjs

--- public/src/path_normalize.mjs
+++ public/src/path_normalize.mjs
@@ -1,12 +1,10 @@
 import { text_slash_forward } from "../../../love/public/src/text_slash_forward.mjs";
 export function path_normalize(path) {
   let s = text_slash_forward();
-  let s2 = text_slash_forward();
-  let s3 = text_slash_forward();
   let n = path
     .replace(/\\/g, s)
-    .replace(/\/+/g, s2)
-    .replace(/\/\.\//g, s3)
+    .replace(/\/+/g, s)
+    .replace(/\/\.\//g, s)
     .replace(/\/$/, "")
     .replace(/^\.\/+/, "");
   return n;
