76caeb8915a1c2239510dba0ca60087cf89abe7e
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 18:04:00 -0500

    function_auto_path ..\love\public\src\app_index_main_sb.mjs

--- public/src/app_index_main_sb.mjs
+++ public/src/app_index_main_sb.mjs
@@ -1,4 +1,6 @@
+import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
 import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
-export function app_index_main_sb() {
+export async function app_index_main_sb() {
+  await app_context_initialize(fn);
   app_index_main(context);
 }
