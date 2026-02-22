fb9c4771a42929dc439373cb105e076548f58b1d
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 13:09:20 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_old.mjs

--- web/public/src/firebase_storage_download.mjs
+++ web/public/src/firebase_storage_download.mjs
@@ -6,17 +6,11 @@ import { firebase_path_fix } from "../../../love/public/src/firebase_path_fix.mj
 import { firebase_storage_url } from "../../../love/public/src/firebase_storage_url.mjs";
 export async function firebase_storage_download(destination) {
   destination = firebase_path_fix(destination);
-  let b = browser_is();
-  if (b) {
-    async function lambda2() {
-      let url = await firebase_storage_url(destination);
-      let buffer = await http(url);
-      return buffer;
-    }
-    let result = await html_loading(lambda2);
-    return result;
+  async function lambda2() {
+    let url = await firebase_storage_url(destination);
+    let buffer = await http(url);
+    return buffer;
   }
-  const bucket = await firebase_bucket();
-  let [buffer] = await bucket.file(destination).download();
-  return buffer;
+  let result = await html_loading(lambda2);
+  return result;
 }
