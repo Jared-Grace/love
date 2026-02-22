fd64a1296cac6ebf2363a26f1b49c43082bd0d58
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 14:57:29 -0500

    function_auto_path ..\love\public\src\folder_current_join.mjs

--- web/public/src/folder_current_join.mjs
+++ web/public/src/folder_current_join.mjs
@@ -1,10 +1,8 @@
-import { text_pad } from "../../../love/public/src/text_pad.mjs";
 import { text_slash_forward } from "../../../love/public/src/text_slash_forward.mjs";
 import { folder_current } from "../../../love/public/src/folder_current.mjs";
 export function folder_current_join(result) {
   let s = text_slash_forward();
   let current = folder_current();
-  let padded = text_pad(s2, padding);
   let value_string = current + s + result;
   return value_string;
 }
