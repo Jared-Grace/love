9fb2e19474a68ecf1d6a30bc2c5b49ad1ca4f0e2
Grace-Jared <christrosetolife@gmail.com>
2026-02-22 17:06:57 -0500

    function_auto_path ..\love\public\src\list_map_async.mjs

--- web/public/src/list_map_async.mjs
+++ web/public/src/list_map_async.mjs
@@ -3,13 +3,13 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 export async function list_map_async(list, lambda$item) {
   list_is_assert(list);
-  async function lambda2(la) {
+  async function list_adder_lambda(la) {
     async function lambda(item) {
       let m = await lambda$item(item);
       la(m);
     }
     await each_async(list, lambda);
   }
-  let mapped = await list_adder_async(lambda2);
+  let mapped = await list_adder_async(list_adder_lambda);
   return mapped;
 }
