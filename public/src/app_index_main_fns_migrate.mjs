197c5b32c13ded8415ada3900f9dcb0ed90ebeaf
Grace-Jared <christrosetolife@gmail.com>
2026-02-21 19:29:31 -0500

    function_auto_path ..\love\public\src\app_index_main_fns_migrate.mjs

--- public/src/app_index_main_fns_migrate.mjs
+++ public/src/app_index_main_fns_migrate.mjs
@@ -18,7 +18,7 @@ export async function app_index_main_fns_migrate() {
     function lambda2(v) {
       let node = property_get(v, "node");
       let properties = js_object_expression_properties(node);
-      lfp;
+      lfdp;
       let mapped = list_map_property(properties, "key");
       let filtered = list_filter(mapped, js_identifier_is);
       let names = js_identifiers_to_names(filtered);
