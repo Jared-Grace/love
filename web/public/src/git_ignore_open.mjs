05dcab99c01da302be8da3b4d58eed28d211491a
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:32:01 -0500

    function_auto_path ..\love\public\src\git_ignore_open.mjs

--- web/public/src/git_ignore_open.mjs
+++ web/public/src/git_ignore_open.mjs
@@ -1,7 +1,7 @@
-import { file_read } from "../../../love/public/src/file_read.mjs";
+import { file_open } from "../../../love/public/src/file_open.mjs";
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 export async function git_ignore_open() {
   let g_name = git_ignore_name();
-  let r = await file_read(g_name);
+  let r = await file_open(g_name);
   return r;
 }
