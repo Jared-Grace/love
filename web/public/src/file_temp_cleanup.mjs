3ec1a6902081bf58932e7ea5062bf05a187f9699
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 13:54:14 -0500

    function_auto_path ..\love\public\src\file_temp.mjs

---
+++ web/public/src/file_temp_cleanup.mjs
@@ -0,0 +1,10 @@
+import { file_delete_if_exists } from "../../../love/public/src/file_delete_if_exists.mjs";
+export async function file_temp_cleanup(lambda$temp_path, temp_path) {
+  let result = null;
+  try {
+    result = await lambda$temp_path(temp_path);
+  } finally {
+    await file_delete_if_exists(temp_path);
+  }
+  return result;
+}
