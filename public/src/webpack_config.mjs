a4975d6f0d61554fd6f047a8637991cfdcba9111
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 18:14:40 -0500

    function_auto_path ..\love\public\src\webpack_config.mjs

--- public/src/webpack_config.mjs
+++ public/src/webpack_config.mjs
@@ -1,3 +1,4 @@
+import { webpack_config_path } from "../../../love/public/src/webpack_config_path.mjs";
 import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
 import { webpack_config_filename } from "../../../love/public/src/webpack_config_filename.mjs";
 import { process_env_args_get } from "../../../love/public/src/process_env_args_get.mjs";
@@ -9,7 +10,8 @@ export async function webpack_config() {
   let process_env_get = process_env_args_get();
   let entry = process_env_get(webpack_config_entry_path);
   let filename = process_env_get(webpack_config_filename);
-  let path = await path_resolve("./dist");
+  let path_env = process_env_get(webpack_config_path);
+  let path = await path_resolve(path_env);
   let r = {
     mode: "production",
     entry,
