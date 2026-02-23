857165efcef82882438bcf4285af116901304571
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:41:26 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

--- web/public/src/webpack_build.mjs
+++ web/public/src/webpack_build.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { folder_previous_2_join } from "../../../love/public/src/folder_previous_2_join.mjs";
 import { js_code_import_single } from "../../../love/public/src/js_code_import_single.mjs";
@@ -16,6 +17,9 @@ export async function webpack_build(search) {
   async function lambda(temp_path) {
     let joined = folder_previous_2_join(f_path);
     let i = js_code_import_single(f_name, joined);
+    log({
+      i,
+    });
     await file_overwrite(file_path, contents);
   }
   let result = await file_delete_after(f_name_ext, lambda);
