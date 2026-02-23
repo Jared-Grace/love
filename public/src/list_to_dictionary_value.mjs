f8ee19620816e8cf70f783e5ebfe791f663045cd
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 16:50:52 -0500

    function_auto_path ..\love\public\src\list_to_dictionary_value.mjs

--- public/src/list_to_dictionary_value.mjs
+++ public/src/list_to_dictionary_value.mjs
@@ -1,7 +1,7 @@
 import { list_to_dictionary } from "../../../love/public/src/list_to_dictionary.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
-export function list_to_dictionary_value(list, lambda$item) {
+export function list_to_dictionary_value(list, lambda$item2v) {
   let key_get = identity;
-  let dictionary = list_to_dictionary(list, key_get, lambda$item);
+  let dictionary = list_to_dictionary(list, key_get, lambda$item2v);
   return dictionary;
 }
