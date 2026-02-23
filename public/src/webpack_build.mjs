39868d5cbbbf76174f4f0f16b3ba886acc6c4c47
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:41:42 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

--- public/src/webpack_build.mjs
+++ public/src/webpack_build.mjs
@@ -1,3 +1,5 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
 import { folder_previous_2_join } from "../../../love/public/src/folder_previous_2_join.mjs";
@@ -6,10 +8,10 @@ import { path_join } from "../../../love/public/src/path_join.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_name_to_base } from "../../../love/public/src/function_name_to_base.mjs";
 import { folder_scripts_join } from "../../../love/public/src/folder_scripts_join.mjs";
-import { app_shared_name_search_main } from "../../../love/public/src/app_shared_name_search_main.mjs";
 import { file_delete_after } from "../../../love/public/src/file_delete_after.mjs";
 export async function webpack_build(search) {
-  let f_name = await app_shared_name_search_main(search);
+  let a = await app_shared_name_search_info(search);
+  let f_name = property_get(a, "f_name");
   let combined = function_name_combine(f_name, "run");
   let path2 = path_join(["temp", combined]);
   let path = folder_scripts_join(path2);
