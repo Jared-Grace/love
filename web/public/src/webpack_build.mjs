ca67fe81f45d17e07b83c975d9e6d03bff6454a6
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 16:14:30 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

--- web/public/src/webpack_build.mjs
+++ web/public/src/webpack_build.mjs
@@ -1,3 +1,4 @@
+import { log_exit } from "../../../love/public/src/log_exit.mjs";
 import { webpack_config } from "../../../love/public/src/webpack_config.mjs";
 import { global_function_property_initialize } from "../../../love/public/src/global_function_property_initialize.mjs";
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
@@ -27,6 +28,7 @@ export async function webpack_build(search) {
     let i = js_code_import_single(f_name, code_string);
     let call = js_code_call_app_context_initialize(f_name);
     let joined2 = list_join_newline([i, call]);
+    log_exit(message);
     await file_overwrite(entry_path, joined2);
     let value = global_function_property_initialize(
       webpack_config,
