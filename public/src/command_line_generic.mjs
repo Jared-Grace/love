bc9a4132d65cd0b6bccb666f94d7c2a64cdaaa8a
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 18:22:12 -0500

    function_auto_path ..\love\public\src\command_line_generic.mjs

--- public/src/command_line_generic.mjs
+++ public/src/command_line_generic.mjs
@@ -1,7 +1,12 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export async function command_line_generic(command, extra) {
-  const { exec } = await import("child_process");
-  const { promisify } = await import("util");
+  text_is_assert(command);
+  const r = await import("child_process");
+  let exec = property_get(r, "exec");
+  const r2 = await import("util");
+  let promisify = property_get(r2, "promisify");
   const execAsync = promisify(exec);
   const options = {
     encoding: "utf8",
