a85270e5e660819c65cfd2ba32d524e44b1df5fd
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 18:34:54 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function sandbox() {
   let r = await command_line("echo %PATH%");
   let stdout = await command_line("npx webpack");
+  log({
+    stdout,
+  });
   return r;
 }
