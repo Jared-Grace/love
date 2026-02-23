6a4c3782ff0dfe854a6efa20491f4b11bb94d7c8
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:01:29 -0500

    function_auto_path ..\love\public\src\vite_config_path.mjs

--- public/src/vite_config_path.mjs
+++ public/src/vite_config_path.mjs
@@ -1,7 +1,6 @@
-import { path_join } from "../../../love/public/src/path_join.mjs";
+import { folder_scripts_join } from "../../../love/public/src/folder_scripts_join.mjs";
 export function vite_config_path() {
   const f_name = "vite.config.mjs";
-  let s = "scripts";
-  let path = path_join([s, f_name]);
+  let path = folder_scripts_join(f_name);
   return path;
 }
