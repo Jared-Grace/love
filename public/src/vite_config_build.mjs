6c21c8774fe6e44a3f55a18d281134b7b508866b
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:28:39 -0500

    function_auto_path ..\love\public\src\vite_config_build.mjs

--- public/src/vite_config_build.mjs
+++ public/src/vite_config_build.mjs
@@ -1,16 +1,17 @@
+import { webpack_config_entry_path } from "../../../love/public/src/webpack_config_entry_path.mjs";
+import { property_invoke_get } from "../../../love/public/src/property_invoke_get.mjs";
+import { process_env_args_get } from "../../../love/public/src/process_env_args_get.mjs";
 import { vite_config_define } from "../../../love/public/src/vite_config_define.mjs";
 import { vite_config_out_dir } from "../../../love/public/src/vite_config_out_dir.mjs";
-import { process_env_trim } from "../../../love/public/src/process_env_trim.mjs";
 import { vite_config_name } from "../../../love/public/src/vite_config_name.mjs";
 import { vite_config_lib_entry } from "../../../love/public/src/vite_config_lib_entry.mjs";
 import { vite_config_file_name_get } from "../../../love/public/src/vite_config_file_name_get.mjs";
 export async function vite_config_build() {
-  let ev_lib_entry = vite_config_lib_entry();
-  let entry = process_env_trim(ev_lib_entry);
-  let ev_name = vite_config_name();
-  let name = process_env_trim(ev_name);
-  let ev_out_dir = vite_config_out_dir();
-  let outDir = process_env_trim(ev_out_dir);
+  let props = [vite_config_lib_entry, vite_config_name, vite_config_out_dir];
+  let dictionary = process_env_args_get(props);
+  let entry = property_invoke_get(dictionary, webpack_config_entry_path);
+  let name = property_invoke_get(dictionary, vite_config_name);
+  let outDir = property_invoke_get(dictionary, vite_config_out_dir);
   let c = vite_config_define({
     publicDir: false,
     define: {
