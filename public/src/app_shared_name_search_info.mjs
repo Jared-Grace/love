c8997c2b21b3449981a5ef0422aca9c5e944abff
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:45:20 -0500

    function_auto_path ..\love\public\src\app_shared_name_search_info.mjs

--- public/src/app_shared_name_search_info.mjs
+++ public/src/app_shared_name_search_info.mjs
@@ -16,7 +16,7 @@ export async function app_shared_name_search_info(search) {
   let src_path_latest = html_name_to_path_latest_generic(a_name, file_name_js);
   let file_name = file_name_js(a_name);
   let src_path = folder_public_join(file_name);
-  let parts = function_name_to_parth(f_name);
+  let parts = function_name_to_path(f_name);
   log_keep({
     search,
     a_name,
