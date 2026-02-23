0be5f390d6db7b36ed166e9f16d19be45543df3d
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:58:19 -0500

    function_auto_path ..\love\public\src\vite_config_build.mjs

--- web/public/src/vite_config_build.mjs
+++ web/public/src/vite_config_build.mjs
@@ -1,5 +1,4 @@
 import { webpack_config_entry_path } from "../../../love/public/src/webpack_config_entry_path.mjs";
-import { property_invoke_get } from "../../../love/public/src/property_invoke_get.mjs";
 import { process_env_args_get } from "../../../love/public/src/process_env_args_get.mjs";
 import { vite_config_define } from "../../../love/public/src/vite_config_define.mjs";
 import { vite_config_out_dir } from "../../../love/public/src/vite_config_out_dir.mjs";
@@ -8,10 +7,10 @@ import { vite_config_lib_entry } from "../../../love/public/src/vite_config_lib_
 import { vite_config_file_name_get } from "../../../love/public/src/vite_config_file_name_get.mjs";
 export async function vite_config_build() {
   let props = [vite_config_lib_entry, vite_config_name, vite_config_out_dir];
-  let dictionary = process_env_args_get(props);
-  let entry = property_invoke_get(dictionary, webpack_config_entry_path);
-  let name = property_invoke_get(dictionary, vite_config_name);
-  let outDir = property_invoke_get(dictionary, vite_config_out_dir);
+  let process_env_get = process_env_args_get();
+  let entry = process_env_get(webpack_config_entry_path);
+  let name = process_env_get(vite_config_name);
+  let outDir = process_env_get(vite_config_out_dir);
   let c = vite_config_define({
     publicDir: false,
     define: {
