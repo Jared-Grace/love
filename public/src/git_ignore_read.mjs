45fff9bf29d0bec8e744633643c85df00fbc2a8d
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:31:53 -0500

    crl gio read

---
+++ public/src/git_ignore_read.mjs
@@ -0,0 +1,7 @@
+import { file_read } from "../../../love/public/src/file_read.mjs";
+import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
+export async function git_ignore_read() {
+  let g_name = git_ignore_name();
+  let r = await file_read(g_name);
+  return r;
+}
