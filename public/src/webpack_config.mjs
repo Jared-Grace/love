aac60da8f61860cb593317563dae167389fc339a
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:57:24 -0500

    function_auto_path ..\love\public\src\webpack_config.mjs

--- public/src/webpack_config.mjs
+++ public/src/webpack_config.mjs
@@ -1,15 +1,14 @@
 import { webpack_config_filename } from "../../../love/public/src/webpack_config_filename.mjs";
 import { process_env_args_get } from "../../../love/public/src/process_env_args_get.mjs";
-import { property_invoke_get } from "../../../love/public/src/property_invoke_get.mjs";
 import { webpack_config_entry_path } from "../../../love/public/src/webpack_config_entry_path.mjs";
 import path from "path";
 import TerserPlugin from "terser-webpack-plugin";
 import webpack from "webpack";
 export function webpack_config() {
   let props = [webpack_config_entry_path, webpack_config_filename];
-  let dictionary = process_env_args_get(props);
-  let entry = property_invoke_get(dictionary, webpack_config_entry_path);
-  let filename = property_invoke_get(dictionary, webpack_config_filename);
+  let process_env_get = process_env_args_get();
+  let entry = process_env_get(webpack_config_entry_path);
+  let filename = process_env_get(webpack_config_filename);
   let r = {
     mode: "production",
     entry,
