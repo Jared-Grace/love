995fa74fb160687d4184ba6b8ff0e4525dce761c
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:42:55 -0500

    function_auto_path ..\love\public\src\webpack_config.mjs

--- web/public/src/webpack_config.mjs
+++ web/public/src/webpack_config.mjs
@@ -14,7 +14,7 @@ export function webpack_config() {
     mode: "production",
     entry,
     output: {
-      filename: filename,
+      filename,
       path: path.resolve("./dist"),
     },
     optimization: {
