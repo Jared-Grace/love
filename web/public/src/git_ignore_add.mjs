b0e0de9302274704ecee2607100b432e416e9d51
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:25:26 -0500

    function_auto_path ..\love\public\src\git_ignore_add.mjs

--- web/public/src/git_ignore_add.mjs
+++ web/public/src/git_ignore_add.mjs
@@ -4,6 +4,6 @@ import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 export async function git_ignore_add(f_path) {
   let g_name = git_ignore_name();
   let lambda = text_between_newline_curried_right(f_path);
-  await file_transform(g_name, lambda);
+  return await file_transform(g_name, lambda);
   return g_name;
 }
