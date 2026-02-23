ba8fd130c46696d185e18344484d8e1aae0c4dbd
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:04:21 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

--- web/public/src/webpack_build.mjs
+++ web/public/src/webpack_build.mjs
@@ -4,6 +4,7 @@ import { file_delete_after } from "../../../love/public/src/file_delete_after.mj
 export async function webpack_build(search) {
   let f_name = await app_shared_name_search_main(search);
   let path = folder_scripts_join(f_name);
+  return path;
   async function lambda(temp_path) {}
   let result = await file_delete_after(lambda, temp_path);
 }
