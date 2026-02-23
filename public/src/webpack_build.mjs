45a0ee790a147371feb7a26ad75fac0348c477ff
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 13:50:31 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

--- public/src/webpack_build.mjs
+++ public/src/webpack_build.mjs
@@ -1,1 +1,4 @@
-export function webpack_build() {}
+import { file_temp } from "../../../love/public/src/file_temp.mjs";
+export async function webpack_build() {
+  let result = await file_temp(async function lambda(temp_path) {});
+}
