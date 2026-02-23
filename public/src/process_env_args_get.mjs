259f96895b6e498257974c22c4a21d56dad83153
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:56:37 -0500

    function_auto_path ..\love\public\src\process_env_args_get.mjs

--- public/src/process_env_args_get.mjs
+++ public/src/process_env_args_get.mjs
@@ -1,6 +1,6 @@
 import { process_env_trim } from "../../../love/public/src/process_env_trim.mjs";
-export function process_env_args_get(props) {
-  let r = function lambda(property_fn) {
+export function process_env_args_get() {
+  let r = function process_env_args_getter(property_fn) {
     let property_name = property_fn();
     let entry = process_env_trim(property_name);
   };
