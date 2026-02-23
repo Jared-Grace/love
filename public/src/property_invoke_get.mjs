5341da07fb1ecf1af129737aca1a39b1194766e7
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:19:58 -0500

    function_auto_path ..\love\public\src\property_invoke_get.mjs

--- public/src/property_invoke_get.mjs
+++ public/src/property_invoke_get.mjs
@@ -1,6 +1,6 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function property_invoke_get(dictionary, p2) {
-  let property_name = p2();
+export function property_invoke_get(dictionary, property_name_get) {
+  let property_name = property_name_get();
   let entry = property_get(dictionary, property_name);
   return entry;
 }
