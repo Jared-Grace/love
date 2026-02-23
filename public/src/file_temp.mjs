2f619e3d4df7dce6a1a361b853b4218b613db764
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 14:01:11 -0500

    function_auto_path ..\love\public\src\file_delete_after.mjs

--- public/src/file_temp.mjs
+++ public/src/file_temp.mjs
@@ -1,9 +1,9 @@
-import { file_temp_cleanup } from "../../../love/public/src/file_temp_cleanup.mjs";
+import { file_delete_after } from "../../../love/public/src/file_delete_after.mjs";
 import { folder_user_join } from "../../../love/public/src/folder_user_join.mjs";
 import { uuid } from "../../../love/public/src/uuid.mjs";
 export async function file_temp(lambda$temp_path) {
   let u = await uuid();
   let temp_path = folder_user_join("temp", u);
-  let result = await file_temp_cleanup(lambda$temp_path, temp_path);
+  let result = await file_delete_after(lambda$temp_path, temp_path);
   return result;
 }
