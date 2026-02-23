1e97c23d17d2ecf6bce71747864e9eb6dd26de73
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 15:11:19 -0500

    function_auto_path ..\love\public\src\git_remove.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -7,8 +7,8 @@ import { command_line_git } from "../../../love/public/src/command_line_git.mjs"
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_push } from "../../../love/public/src/git_push.mjs";
 export async function git_remove(f_path) {
-  let g_name = git_ignore_name();
   await command_line_git("rm -r --cached " + f_path);
+  let g_name = git_ignore_name();
   let lambda = text_between_newline_curried_right(f_path);
   await file_transform(g_name, lambda);
   let added = g_name;
