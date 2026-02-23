230ece7ece9474d2391027f52b04c0a75035cea2
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 17:51:02 -0500

    function_auto_path ..\love\public\src\process_env_args_get.mjs

--- web/public/src/process_env_args_get.mjs
+++ web/public/src/process_env_args_get.mjs
@@ -2,7 +2,8 @@ import { process_env_trim } from "../../../love/public/src/process_env_trim.mjs"
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 import { list_invoke } from "../../../love/public/src/list_invoke.mjs";
 export function process_env_args_get(props) {
-  return () => {};
+  let r = function lambda() {};
+  return r;
   let mapped = list_invoke(props);
   let dictionary = list_to_dictionary_value(mapped, process_env_trim);
   return dictionary;
