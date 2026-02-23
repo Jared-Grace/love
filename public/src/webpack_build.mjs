97f4ba67ed8f622836d48db15f8217f09907a568
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 18:41:35 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

--- public/src/webpack_build.mjs
+++ public/src/webpack_build.mjs
@@ -49,6 +49,10 @@ export async function webpack_build(search) {
     let f_name_ext = folder_scripts_join_mjs("webpack.config");
     let combined2 = text_combine("npx webpack --config ", f_name_ext);
     let command = await command_line_text_env_vars(env_vars, combined2);
+    let stdout2 = await command_line("npx webpack");
+    log({
+      stdout2,
+    });
     let stdout = await command_line(command);
     return stdout;
   }
