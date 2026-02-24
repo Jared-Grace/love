0b55671d3d437ec1e9e95c2f859539237fba757d
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 21:16:54 -0500

    function_auto_path ..\love\public\src\html_update_latest_webpack.mjs

--- web/public/src/html_update_latest_webpack.mjs
+++ web/public/src/html_update_latest_webpack.mjs
@@ -4,6 +4,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 export async function html_update_latest_webpack(search) {
   let r = await app_shared_name_search_info(search);
+  let f_path_latest = property_get(r, "f_path_latest");
   let a_name = property_get(r, "a_name");
   let c = html_code_script_src_js(a_name);
   await html_overwrite(a_name, f_path_latest, joined);
