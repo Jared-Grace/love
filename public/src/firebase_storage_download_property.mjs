2ad9324f784a8ab154866a1f74036ec17f8ce204
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:08:24 -0500

    function_auto_path ..\love\public\src\firebase_storage_download.mjs

--- public/src/firebase_storage_download_property.mjs
+++ public/src/firebase_storage_download_property.mjs
@@ -6,7 +6,7 @@ export async function firebase_storage_download_property(
   storage_path,
   property_name,
 ) {
-  let buffer = await firebase_storage_download(storage_path);
+  let buffer = await firebase_storage_download(storage_path, error());
   const json = buffer_text_to(buffer);
   let o = json_from(json);
   let value = property_get(o, property_name);
