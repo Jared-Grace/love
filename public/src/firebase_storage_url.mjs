02b8a59b6c5a31a1dcebd034e54722c7709dfcfd
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:00:02 -0500

    pne firebase_storage_url project_url

--- public/src/firebase_storage_url.mjs
+++ public/src/firebase_storage_url.mjs
@@ -1,6 +1,6 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { firebase_storage_url_project } from "../../../love/public/src/firebase_storage_url_project.mjs";
-export async function firebase_storage_url(storage_path) {
+export async function firebase_storage_url(storage_path, project_url) {
   let replaced = text_replace(storage_path, "/", "%2F");
   let url =
     "https://firebasestorage.googleapis.com/v0/b/" +
