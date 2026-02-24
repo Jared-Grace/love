e15e5fccef7fb36d49ed5e344dcd3c62986d3907
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 21:24:11 -0500

    cr html_update_latest_webpack latest public

---
+++ public/src/html_update_public_webpack.mjs
@@ -0,0 +1,5 @@
+import { html_update_latest_webpack_generic } from "../../../love/public/src/html_update_latest_webpack_generic.mjs";
+export async function html_update_public_webpack(search) {
+  const path = "f_path_latest";
+  await html_update_latest_webpack_generic(search, path);
+}
