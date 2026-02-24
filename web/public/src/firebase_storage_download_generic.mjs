0e96c4ac8ba852e96170074874ab342ce04e2441
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 13:00:02 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_generic.mjs

--- web/public/src/firebase_storage_download_generic.mjs
+++ web/public/src/firebase_storage_download_generic.mjs
@@ -1,11 +1,11 @@
 import { html_loading } from "../../../love/public/src/html_loading.mjs";
 import { firebase_storage_url } from "../../../love/public/src/firebase_storage_url.mjs";
 import { firebase_path_fix } from "../../../love/public/src/firebase_path_fix.mjs";
-export async function firebase_storage_download_generic(destination, fn) {
+export async function firebase_storage_download_generic(destination, http_fn) {
   destination = firebase_path_fix(destination);
   async function lambda2() {
     let url = await firebase_storage_url(destination);
-    let buffer = await fn(url);
+    let buffer = await http_fn(url);
     return buffer;
   }
   let result = await html_loading(lambda2);
