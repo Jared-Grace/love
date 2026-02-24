f379b1f367c59faebb88d7625fd25bdb62f8ea11
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 13:00:36 -0500

    cs http browser

---
+++ public/src/http_browser.mjs
@@ -0,0 +1,7 @@
+import { http_get_options } from "../../../love/public/src/http_get_options.mjs";
+import { http_generic } from "../../../love/public/src/http_generic.mjs";
+export async function http_browser(url) {
+  let options = http_get_options();
+  let v = await http_generic(url, options);
+  return v;
+}
