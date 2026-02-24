9bfa8199f55133882d72ba6433f060bc6989792c
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 06:29:33 -0500

    function_auto_path ..\love\public\src\app_replace.mjs

--- web/public/src/app_replace.mjs
+++ web/public/src/app_replace.mjs
@@ -1,4 +1,1 @@
-import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
-export async function app_replace() {
-  await app_main_production("app_replace_main", "jared-grace");
-}
+export async function app_replace() {}
