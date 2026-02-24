27cb48ad55d42feddc84aecad15d9b75ba3639c4
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 06:25:17 -0500

    function_auto_path ..\love\public\src\app_shared_latest_build.mjs

--- web/public/src/app_shared_latest_build.mjs
+++ web/public/src/app_shared_latest_build.mjs
@@ -1,4 +1,6 @@
+import { webpack_build } from "../../../love/public/src/webpack_build.mjs";
 import { html_update_latest_webpack } from "../../../love/public/src/html_update_latest_webpack.mjs";
 export async function app_shared_latest_build() {
+  let result = await webpack_build(search2);
   await html_update_latest_webpack(search);
 }
