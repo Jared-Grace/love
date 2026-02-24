39b8ffb4f2b5562c0d6c4b8d1573c2e6862e777c
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 17:42:14 -0500

    function_auto_path ..\love\public\src\js_return_above_combine.mjs

--- public/src/js_return_above_combine.mjs
+++ public/src/js_return_above_combine.mjs
@@ -1,5 +1,5 @@
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 export function js_return_above_combine(ast) {
   function lambda(v) {}
-  js_visit_type(ast2, type, lambda);
+  js_visit_type(ast, "ReturnStatement", lambda);
 }
