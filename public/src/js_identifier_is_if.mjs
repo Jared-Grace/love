3aa4de4c13f59ca61fbeb974c6210bae0769ae97
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 17:59:14 -0500

    function_auto_path ..\love\public\src\js_return_above_combine.mjs

---
+++ public/src/js_identifier_is_if.mjs
@@ -0,0 +1,4 @@
+import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
+export function js_identifier_is_if(only, lambda4) {
+  js_node_type_is_if(only, "Identifier", lambda4);
+}
