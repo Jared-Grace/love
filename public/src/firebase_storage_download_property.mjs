5182615139a9c797019ce7ab87ba89c0182725e3
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:22:03 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_property.mjs

--- public/src/firebase_storage_download_property.mjs
+++ public/src/firebase_storage_download_property.mjs
@@ -1,4 +1,3 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -7,7 +6,6 @@ export async function firebase_storage_download_property(
   storage_path,
   property_name,
 ) {
-  let project_url = error();
   let buffer = await firebase_storage_download(project_url, storage_path);
   const json = buffer_text_to(buffer);
   let o = json_from(json);
