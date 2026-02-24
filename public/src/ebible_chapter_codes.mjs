3d3073cf8d6d9e6aae3bb1794059e4cf7d0e0473
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 11:40:42 -0500

    function_auto_path ..\love\public\src\ebible_chapter_codes.mjs

--- public/src/ebible_chapter_codes.mjs
+++ public/src/ebible_chapter_codes.mjs
@@ -1,9 +1,9 @@
-import { ebible_version_books_browser } from "../../../love/public/src/ebible_version_books_browser.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { ebible_books_to_chapter_codes } from "../../../love/public/src/ebible_books_to_chapter_codes.mjs";
+import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function ebible_chapter_codes(bible_folder) {
   arguments_assert(arguments, 1);
-  let books = await ebible_version_books_browser(bible_folder);
+  let books = await ebible_version_books(bible_folder);
   let chapter_codes = await ebible_books_to_chapter_codes(books, bible_folder);
   return chapter_codes;
 }
