bdec1f8e4fd2517c20c97a83119cdf6f78614dc2
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 06:30:34 -0500

    function_auto_path ..\love\public\src\html_update_latest_webpack_generic.mjs

--- public/src/html_update_latest_webpack_generic.mjs
+++ public/src/html_update_latest_webpack_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { html_overwrite } from "../../../love/public/src/html_overwrite.mjs";
 import { html_code_script_src_js } from "../../../love/public/src/html_code_script_src_js.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -7,5 +8,8 @@ export async function html_update_latest_webpack_generic(search, path) {
   let f_path_latest = property_get(r, path);
   let a_name = property_get(r, "a_name");
   let c = html_code_script_src_js(a_name);
+  log({
+    f_path_latest,
+  });
   await html_overwrite(a_name, f_path_latest, c);
 }
