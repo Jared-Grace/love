83f5ea0c068a0bf497a996f9ceb77e07965924e0
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:34:30 -0500

    function_auto_path ..\love\public\src\js_return_on_async.mjs

--- public/src/js_return_on_async.mjs
+++ public/src/js_return_on_async.mjs
@@ -1,7 +1,9 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_node_is_return } from "../../../love/public/src/js_node_is_return.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 export async function js_return_on_async(node, identifier_if, identifier_not) {
-  if (js_node_type_is(node, "ReturnStatement")) {
-    let { argument } = node;
+  if (js_node_is_return(node)) {
+    let argument = property_get(node, "argument");
     if (js_node_type_is(argument, "Identifier")) {
       await identifier_if(argument);
     } else {
