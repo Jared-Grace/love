fd4fd8b939076db626e523506d70fce763b521bb
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 12:00:34 -0500

    function_auto_path ..\love\public\src\firebase_upload_object_browser.mjs

--- public/src/firebase_upload_object_browser.mjs
+++ public/src/firebase_upload_object_browser.mjs
@@ -3,11 +3,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { firebase_upload_object_arg } from "../../../love/public/src/firebase_upload_object_arg.mjs";
 export async function firebase_upload_object_browser(destination, object) {
   var r = firebase_upload_object_arg(object);
-  let content_type = property_get(r, "content_type");
   let content = property_get(r, "content");
-  await firebase_upload_text_generic_browser(
-    content,
-    destination,
-    content_type,
-  );
+  await firebase_upload_text_generic_browser(content, destination);
 }
