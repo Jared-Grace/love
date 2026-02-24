b570ab0e99a1aff7dd3130c4118f9c6706540836
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 13:00:14 -0500

    cs firebase_storage_download browser

---
+++ public/src/firebase_storage_download_browser.mjs
@@ -0,0 +1,7 @@
+import { firebase_storage_download_generic } from "../../../love/public/src/firebase_storage_download_generic.mjs";
+import { http } from "../../../love/public/src/http.mjs";
+export async function firebase_storage_download_browser(destination) {
+  let fn = http;
+  let result = await firebase_storage_download_generic(destination, fn);
+  return result;
+}
