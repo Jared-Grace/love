b1193f5a97d24ee7856c2891b8c0ceaf1f035cb0
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:23:46 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_json.mjs

--- web/public/src/firebase_storage_download_json.mjs
+++ web/public/src/firebase_storage_download_json.mjs
@@ -1,7 +1,9 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { firebase_storage_download_text } from "../../../love/public/src/firebase_storage_download_text.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export async function firebase_storage_download_json(destination) {
-  let s = await firebase_storage_download_text(destination, error());
+  let project_url = error();
+  let s = await firebase_storage_download_text(destination, project_url);
   let o = json_from(s);
   return o;
 }
