5b77bfbf0ef972b14c7930412795a11e63391ab8
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 17:57:01 -0500

    function_auto_path ..\love\public\src\list_single_if.mjs

--- web/public/src/list_single_if.mjs
+++ web/public/src/list_single_if.mjs
@@ -1,7 +1,7 @@
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
-export function list_single_if(declarations, lambda$only) {
-  let s1 = list_size_1(declarations);
+export function list_single_if(list, lambda$only) {
+  let s1 = list_size_1(list);
   if (s1) {
     let only = list_single(list);
     lambda$only(only);
