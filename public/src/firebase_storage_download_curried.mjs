8260a99b4f509e648df33cfc2d7b9a23d7f3e5eb
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:20:07 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_curried.mjs

--- public/src/firebase_storage_download_curried.mjs
+++ public/src/firebase_storage_download_curried.mjs
@@ -1,6 +1,10 @@
 import { firebase_storage_download } from "../../../love/public/src/firebase_storage_download.mjs";
 export async function firebase_storage_download_curried(project_url) {
-  return async function firebase_storage_download_curried_result(destination) {
-    return await firebase_storage_download(project_url, destination);
+  let r2 = async function firebase_storage_download_curried_result(
+    destination,
+  ) {
+    let r = await firebase_storage_download(project_url, destination);
+    return r;
   };
+  return r2;
 }
