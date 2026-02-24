399779227c679e60bab91f4f694677634b918b1d
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:17:01 -0500

    function_auto_path ..\love\public\src\functions_param_new_multiple.mjs

--- public/src/functions_param_new_multiple.mjs
+++ public/src/functions_param_new_multiple.mjs
@@ -1,3 +1,4 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { functions_param_new } from "../../../love/public/src/functions_param_new.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -6,6 +7,7 @@ export async function functions_param_new_multiple(
   param_names,
   default_value,
 ) {
+  arguments_assert(arguments, 3);
   let split = text_split_comma(param_names);
   async function lambda(param_name) {
     let v = await functions_param_new(f_names_comma, param_name, default_value);
