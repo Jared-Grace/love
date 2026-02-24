b02d681a8819fc9e2f92facf6c82eee2b9b9ea27
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 21:17:40 -0500

    np

--- public/src/app_index_main_sb.mjs
+++
@@ -1,9 +0,0 @@
-import { browser_is } from "../../../love/public/src/browser_is.mjs";
-import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
-import { app_index_main } from "../../../love/public/src/app_index_main.mjs";
-  if (browser_is()) {
-app_index_main_sb()
-  }
-export async function app_index_main_sb() {
-  await app_context_initialize(app_index_main);
-}
