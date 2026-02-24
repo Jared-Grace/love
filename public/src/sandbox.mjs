88d761bda438ae69d606d91c5d7b354b34e33b73
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:17:52 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,14 +1,1 @@
-import { each } from "../../../love/public/src/each.mjs";
-import { function_param_new } from "../../../love/public/src/function_param_new.mjs";
-import { firebase_storage_download_text } from "../../../love/public/src/firebase_storage_download_text.mjs";
-import { firebase_storage_download_property } from "../../../love/public/src/firebase_storage_download_property.mjs";
-export async function sandbox() {
-  let fns = [
-    firebase_storage_download_property,
-    firebase_storage_download_text,
-  ];
-  async function lambda(item) {
-    await function_param_new(f_name, param_name, default_value);
-  }
-  each(fns, lambda);
-}
+export async function sandbox() {}
