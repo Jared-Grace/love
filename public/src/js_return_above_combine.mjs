81f6357b18c12e912fe7d3178e64d8cc916a3d9a
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 18:03:31 -0500

    function_auto_path ..\love\public\src\js_return_above_combine.mjs

--- public/src/js_return_above_combine.mjs
+++ public/src/js_return_above_combine.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 import { equal_by } from "../../../love/public/src/equal_by.mjs";
 import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_if.mjs";
@@ -33,11 +32,8 @@ export function js_return_above_combine(ast) {
           let id = property_get(only, "id");
           function lambda4() {
             let eq = equal_by(id, argument, js_identifier_name);
-            log({
-              eq,
-              id,
-              argument,
-            });
+            if (not(b)) {
+            }
           }
           js_identifier_is_if(id, lambda4);
         }
