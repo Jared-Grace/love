0747a1416c481f591af99dfd40424533a4597a17
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 17:55:14 -0500

    function_auto_path ..\love\public\src\js_return_above_combine.mjs

---
+++ public/src/list_single_if.mjs
@@ -0,0 +1,9 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
+import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
+export function list_single_if(declarations, lambda) {
+  let s1 = list_size_1(declarations);
+  if (s1) {
+    let only = list_single(list);
+    lambda(only);
+  }
+}
