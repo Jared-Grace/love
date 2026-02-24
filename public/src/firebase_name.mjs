ee70a6fe68d531a70284654e64d51b458eca695e
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 12:04:48 -0500

    function_auto_path ..\love\public\src\firebase_name.mjs

--- public/src/firebase_name.mjs
+++ public/src/firebase_name.mjs
@@ -1,12 +1,6 @@
-import { firebase_name_browser } from "../../../love/public/src/firebase_name_browser.mjs";
 import { firebase_name_repo } from "../../../love/public/src/firebase_name_repo.mjs";
-import { browser_is } from "./browser_is.mjs";
 import { user_repo_get } from "./user_repo_get.mjs";
 export async function firebase_name() {
-  if (browser_is()) {
-    let r = firebase_name_browser();
-    return r;
-  }
   let repo_name = await user_repo_get();
   let default2 = await firebase_name_repo(repo_name);
   return default2;
