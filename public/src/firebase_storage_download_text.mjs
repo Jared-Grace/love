473b0a346d15193361e00ba0a309dbb8c4376b1b
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:23:42 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_text.mjs

--- public/src/firebase_storage_download_text.mjs
+++ public/src/firebase_storage_download_text.mjs
@@ -1,8 +1,6 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { firebase_storage_download } from "../../../love/public/src/firebase_storage_download.mjs";
-export async function firebase_storage_download_text(destination) {
-  let project_url = error();
+export async function firebase_storage_download_text(destination, project_url) {
   let buffer = await firebase_storage_download(project_url, destination);
   let s = buffer_text_to(buffer);
   return s;
