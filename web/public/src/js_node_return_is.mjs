5dd93b1bd9fda770c2c74716b02f0262ac2d662d
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:35:17 -0500

    function_auto_path ..\love\public\src\js_node_return_is.mjs

--- web/public/src/js_node_return_is.mjs
+++ web/public/src/js_node_return_is.mjs
@@ -1,5 +1,5 @@
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
-export function js_node_is_return(node) {
+export function js_node_return_is(node) {
   let type_is = js_node_type_is(node, "ReturnStatement");
   return type_is;
 }
