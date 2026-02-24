b60a43e690c8e65f489678acfd8a8cafe87f4dd6
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 12:05:09 -0500

    function_auto_path ..\love\public\src\firebase_name_jg.mjs

--- public/src/firebase_name_jg.mjs
+++ public/src/firebase_name_jg.mjs
@@ -1,5 +1,5 @@
-import { firebase_name } from "../../../love/public/src/firebase_name.mjs";
 import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
+import { firebase_name_browser } from "./firebase_name_browser.mjs";
 export function firebase_name_jg() {
-  global_function_initialize(firebase_name, "jared-grace");
+  global_function_initialize(firebase_name_browser, "jared-grace");
 }
