010f80265d80e1e15091d96410bfb928f2197f17
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:10:30 -0500

    function_auto_path ..\love\public\src\firebase_admin_get.mjs

--- public/src/firebase_admin_get.mjs
+++ public/src/firebase_admin_get.mjs
@@ -1,9 +1,11 @@
+import { firebase_name } from "../../../love/public/src/firebase_name.mjs";
 import { firebase_storage_url_project } from "../../../love/public/src/firebase_storage_url_project.mjs";
 import { firebase_service_account } from "../../../love/public/src/firebase_service_account.mjs";
 export async function firebase_admin_get() {
   const admin = (await import("firebase-admin")).default;
   let service_account = await firebase_service_account();
-  const sb = await firebase_storage_url_project(await firebase_name());
+  let project_name = await firebase_name();
+  const sb = await firebase_storage_url_project(project_name);
   admin.initializeApp({
     credential: admin.credential.cert(service_account),
     storageBucket: sb,
