5cd99858f4a82f67fcdd352232583d915dfd4e23
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 16:33:01 -0500

    function_auto_path ..\love\public\src\js_marker_name_get.mjs

--- web/public/src/js_marker_name_get.mjs
+++ web/public/src/js_marker_name_get.mjs
@@ -1,5 +1,6 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { assert } from "../../../love/public/src/assert.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { error } from "../../../love/public/src/error.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
@@ -8,7 +9,7 @@ import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 import { js_node_is } from "../../../love/public/src/js_node_is.mjs";
 import { list_get_end } from "../../../love/public/src/list_get_end.mjs";
 export function js_marker_name_get(v) {
-  let { stack } = v;
+  let stack = property_get(v, "stack");
   let stack1 = list_get_end(stack, 1);
   let a = js_node_is(stack1);
   if (not(a)) {
@@ -20,19 +21,19 @@ export function js_marker_name_get(v) {
     let v3 = null;
     return v3;
   }
-  let { node } = v;
-  let { callee } = node;
+  let node = property_get(v, "node");
+  let callee = property_get(node, "callee");
   let a3 = js_node_type_is(callee, "Identifier");
   if (not(a3)) {
     let v4 = null;
     return v4;
   }
-  let { name } = callee;
+  let name = property_get(callee, "name");
   if (name !== marker.name) {
     let v5 = null;
     return v5;
   }
-  let { arguments: arguments2 } = node;
+  let arguments2 = property_get(node, "arguments");
   if (list_empty_is(arguments2)) {
     let v6 = null;
     return v6;
@@ -45,9 +46,7 @@ export function js_marker_name_get(v) {
   }
   let stack2 = list_get_end(stack, 2);
   let a5 = list_is(stack2);
-  if (not(a5)) {
-    error();
-  }
-  let { value } = a_first;
+  assert(a5);
+  let value = property_get(a_first, "value");
   return value;
 }
