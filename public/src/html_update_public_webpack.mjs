8902373b605a0d6ee0a365e51cb85ae88d245328
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 21:36:13 -0500

    de html_update_public_webpack

--- public/src/html_update_public_webpack.mjs
+++
@@ -1,5 +0,0 @@
-import { html_update_latest_webpack_generic } from "../../../love/public/src/html_update_latest_webpack_generic.mjs";
-export async function html_update_public_webpack(search) {
-  const path = "f_path";
-  await html_update_latest_webpack_generic(search, path);
-}
