b9436c1f3736382001282111b642168417abfa4a
Grace-Jared <christrosetolife@gmail.com>
2026-02-23 21:21:23 -0500

    function_auto_path ..\love\public\src\html_public_from_latest.mjs

--- public/src/html_public_from_latest.mjs
+++ public/src/html_public_from_latest.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
@@ -5,5 +6,6 @@ export async function html_public_from_latest(search) {
   let info = await app_shared_name_search_info(search);
   let src_path_latest = property_get(info, "src_path_latest");
   let src_path = property_get(info, "src_path");
+  log({});
   await file_copy_overwrite(src_path_latest, src_path);
 }
