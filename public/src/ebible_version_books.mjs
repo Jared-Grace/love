23b4cb6e7256b81faa2f262ed9d0724319634ba5
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 11:25:11 -0500

    function_auto_path ..\love\public\src\ebible_version_books.mjs

--- public/src/ebible_version_books.mjs
+++ public/src/ebible_version_books.mjs
@@ -1,17 +1,14 @@
-import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
-import { ebible_language_original_code } from "../../../love/public/src/ebible_language_original_code.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
+import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
+import { ebible_version_books_original_check } from "../../../love/public/src/ebible_version_books_original_check.mjs";
 import { ebible_class_apocrypha } from "../../../love/public/src/ebible_class_apocrypha.mjs";
 import { ebible_version_books_testament } from "../../../love/public/src/ebible_version_books_testament.mjs";
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 import { ebible_class_old } from "../../../love/public/src/ebible_class_old.mjs";
 import { ebible_class_new } from "../../../love/public/src/ebible_class_new.mjs";
 export async function ebible_version_books(bible_folder) {
-  let right = ebible_language_original_code();
-  if (equal(bible_folder, right)) {
-    let bible_folder2 = ebible_folder_english();
-    let r = await ebible_version_books(bible_folder2);
-    return r;
+  let result = await ebible_version_books_original_check(bible_folder);
+  if (null_not_is(result)) {
+    return result;
   }
   const n = ebible_class_new();
   let o = ebible_class_old();
