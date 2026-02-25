535dfcbbbc5ac5c6da0ff233bed2b3badf6c874f
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 20:01:25 -0500

    function_auto_path ..\love\public\src\app_a_file_system_initialize.mjs

--- web/public/src/app_a_file_system_initialize.mjs
+++ web/public/src/app_a_file_system_initialize.mjs
@@ -10,6 +10,7 @@ export async function app_a_file_system_initialize() {
   async function lambda2() {
     await app_a_file_system_initialize_download();
     let v = true;
+    return v;
   }
   let value2 = await global_function_initialize_lambda_async(
     app_a_file_system_initialize,
