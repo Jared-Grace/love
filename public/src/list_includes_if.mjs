b759d5ea205457dfe995b9d83b2799c56dfbcd80
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 20:16:56 -0500

    function_auto_path ..\love\public\src\list_includes_if.mjs

--- public/src/list_includes_if.mjs
+++ public/src/list_includes_if.mjs
@@ -1,7 +1,7 @@
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-export function list_includes_if(values, value, lambda_if) {
+export function list_includes_if(values, value, lambda) {
   let includes = list_includes(values, value);
   if (includes) {
-    lambda_if();
+    lambda();
   }
 }
