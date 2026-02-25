fd393e0656f3a7527c788765f9dde50bcc81ef46
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 19:51:05 -0500

    function_auto_path ..\love\public\src\functions_return_above_find.mjs

--- public/src/functions_return_above_find.mjs
+++ public/src/functions_return_above_find.mjs
@@ -1,5 +1,8 @@
+import { js_visit_returns_identifiers } from "../../../love/public/src/js_visit_returns_identifiers.mjs";
 import { functions_names_each } from "../../../love/public/src/functions_names_each.mjs";
 export async function functions_return_above_find() {
-  async function lambda2() {}
+  async function lambda2(f_name) {
+    js_visit_returns_identifiers(ast, lambda2);
+  }
   await functions_names_each(lambda2);
 }
