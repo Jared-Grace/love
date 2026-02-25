f10aacb09eaaeab395384c474e0ff8dafe00e486
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 20:17:56 -0500

    function_auto_path ..\love\public\src\js_return_above_combine.mjs

---
+++ public/src/js_literal_is_if.mjs
@@ -0,0 +1,4 @@
+import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
+export function js_literal_is_if(init, lambda6) {
+  js_node_type_is_if(init, "Literal", lambda6);
+}
