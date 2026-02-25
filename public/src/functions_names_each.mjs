e56ee445433292f1fd8fb4fb743c0bce852fa4b0
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 19:51:14 -0500

    function_auto_path ..\love\public\src\functions_names_each.mjs

--- public/src/functions_names_each.mjs
+++ public/src/functions_names_each.mjs
@@ -1,6 +1,6 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
-export async function functions_names_each(lambda) {
+export async function functions_names_each(lambda$f_name) {
   let f_names = await functions_names();
-  await each_async(f_names, lambda);
+  await each_async(f_names, lambda$f_name);
 }
