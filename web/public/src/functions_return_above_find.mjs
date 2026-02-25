68110fe8088a296e9666f9d22f2e582033868c6e
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 19:49:54 -0500

    function_auto_path ..\love\public\src\functions_return_above_find.mjs

--- web/public/src/functions_return_above_find.mjs
+++ web/public/src/functions_return_above_find.mjs
@@ -1,1 +1,4 @@
-export function functions_return_above_find() {}
+import { functions_names } from "../../../love/public/src/functions_names.mjs";
+export async function functions_return_above_find() {
+  let f_names = await functions_names();
+}
