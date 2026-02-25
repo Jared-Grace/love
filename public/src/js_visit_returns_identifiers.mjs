e1431d2593b2ff893a5e4f6684e698cdfd174977
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 19:50:55 -0500

    pb js_visit_returns_identifiers

--- public/src/js_visit_returns_identifiers.mjs
+++ public/src/js_visit_returns_identifiers.mjs
@@ -1,7 +1,7 @@
 import { js_visit_returns } from "../../../love/public/src/js_visit_returns.mjs";
 import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_if.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_returns_identifiers(lambda2, ast) {
+export function js_visit_returns_identifiers(ast, lambda2) {
   function lambda(v) {
     let node = property_get(v, "node");
     let argument = property_get(node, "argument");
