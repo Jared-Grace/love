82286230c24155b49c90a248c1c82d0501c799bb
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 20:02:54 -0500

    function_auto_path ..\love\public\src\functions_return_above_find.mjs

--- web/public/src/functions_return_above_find.mjs
+++ web/public/src/functions_return_above_find.mjs
@@ -7,11 +7,10 @@ export async function functions_return_above_find() {
   async function lambda2(f_name) {
     let ast = await function_ast(f_name);
     js_return_above_combine(ast);
-    let output = await function_transform(
-      f_name2,
-      async function lambda(ast2) {},
-    );
+    async function lambda(ast2) {}
+    let output = await function_transform(f_name2, lambda);
   }
   await functions_names_each(lambda2);
-  let waited = await functions_transform(async function lambda3(ast3) {});
+  async function lambda3(ast3) {}
+  let waited = await functions_transform(lambda3);
 }
