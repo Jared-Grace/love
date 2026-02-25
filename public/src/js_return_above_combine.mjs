63b2e6a1e1064ee05fbec015775f1aca532b3739
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 19:50:55 -0500

    pb js_visit_returns_identifiers

--- public/src/js_return_above_combine.mjs
+++ public/src/js_return_above_combine.mjs
@@ -11,7 +11,7 @@ import { list_is } from "../../../love/public/src/list_is.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_return_above_combine(ast) {
-  js_visit_returns_identifiers(lambda2, ast);
+  js_visit_returns_identifiers(ast, lambda2);
   let a = null;
   return a;
   function lambda2({ v, node, argument }) {
