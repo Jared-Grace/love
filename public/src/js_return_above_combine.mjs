74a6ecbdc6b6fd9ca08d2b26650f8ee7c8081870
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 19:54:24 -0500

    function_auto_path ..\love\public\src\js_return_above_combine.mjs

--- public/src/js_return_above_combine.mjs
+++ public/src/js_return_above_combine.mjs
@@ -38,9 +38,9 @@ export function js_return_above_combine(ast) {
             return;
           }
           function lambda6() {}
-          js_node_type_is_if(node2, "Literal", lambda6);
           let name = js_flo_name(ast);
           log_exit(name);
+          js_node_type_is_if(node2, "Literal", lambda6);
         }
         js_identifier_is_if(id, lambda4);
       }
