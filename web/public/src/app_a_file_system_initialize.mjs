339877be7bd90f75dbf02cf1b3a565e83efbf3f8
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 20:01:15 -0500

    functions_return_above_find

--- web/public/src/app_a_file_system_initialize.mjs
+++ web/public/src/app_a_file_system_initialize.mjs
@@ -10,7 +10,6 @@ export async function app_a_file_system_initialize() {
   async function lambda2() {
     await app_a_file_system_initialize_download();
     let v = true;
-    return v;
   }
   let value2 = await global_function_initialize_lambda_async(
     app_a_file_system_initialize,
