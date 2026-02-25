4e4b0fdad650c5ed052d6c0cd85c05b17947efde
Grace-Jared <christrosetolife@gmail.com>
2026-02-24 20:00:03 -0500

    function_auto_path ..\love\public\src\functions_return_above_find.mjs

--- public/src/functions_return_above_find.mjs
+++ public/src/functions_return_above_find.mjs
@@ -1,13 +1,9 @@
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_return_above_combine } from "../../../love/public/src/js_return_above_combine.mjs";
-import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { functions_names_each } from "../../../love/public/src/functions_names_each.mjs";
 export async function functions_return_above_find() {
   async function lambda2(f_name) {
-    let ast = await function_ast(f_name);
-    js_return_above_combine(ast);
-    async function lambda(ast2) {}
-    let output = await function_transform(f_name2, lambda);
+    let output = await function_transform(f_name, js_return_above_combine);
   }
   await functions_names_each(lambda2);
 }
