4f1a7b993d61e6bbf918b589606a41e4f5fca6b1
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 19:13:09 -0500

    de firebase_project_name_jg

--- web/public/src/firebase_project_name_jg.mjs
+++
@@ -1,5 +0,0 @@
-import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
-import { firebase_name_browser } from "./firebase_name_browser.mjs";
-export function firebase_project_name_jg() {
-  global_function_initialize(firebase_name_browser, "jared-grace");
-}
