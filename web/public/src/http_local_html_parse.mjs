d3c2e2ade314dcfa34795c26615ca27adeea933d
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:03:10 -0500

    function_auto_path ..\love\public\src\http_local_html_parse.mjs

--- web/public/src/http_local_html_parse.mjs
+++ web/public/src/http_local_html_parse.mjs
@@ -1,8 +1,6 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { html_parse } from "../../../love/public/src/html_parse.mjs";
 import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
 export async function http_local_html_parse(url) {
-  let project_url = error();
   let text = await http_local_text(url, project_url);
   let r = await html_parse(text);
   return r;
