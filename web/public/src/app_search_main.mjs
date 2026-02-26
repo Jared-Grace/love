a0a34e3e8888329cfa8411c9e8a09c697808cdca
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:11:15 -0500

    function_auto_path ..\love\public\src\app_reply_generic.mjs

--- web/public/src/app_search_main.mjs
+++ web/public/src/app_search_main.mjs
@@ -1,6 +1,5 @@
 import { app_search_home } from "../../../love/public/src/app_search_home.mjs";
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
-import { firebase_project_name_jg } from "../../../love/public/src/firebase_project_name_jg.mjs";
 import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
 export async function app_search_main(context) {
   html_mobile_default(context);
