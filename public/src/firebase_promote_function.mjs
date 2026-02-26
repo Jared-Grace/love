dad8c05e9cb7ad4d1dd1dc07df895c1de204fd3f
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 19:08:22 -0500

    function_auto_path ..\love\public\src\firebase_promote_function.mjs

--- public/src/firebase_promote_function.mjs
+++ public/src/firebase_promote_function.mjs
@@ -1,10 +1,15 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { firebase_deploy_function_destination_latest } from "../../../love/public/src/firebase_deploy_function_destination_latest.mjs";
 import { firebase_storage_download_json } from "../../../love/public/src/firebase_storage_download_json.mjs";
 import { firebase_upload_object } from "../../../love/public/src/firebase_upload_object.mjs";
 import { firebase_deploy_function_destination_production } from "../../../love/public/src/firebase_deploy_function_destination_production.mjs";
 export async function firebase_promote_function(f_name) {
   let destination_latest = firebase_deploy_function_destination_latest(f_name);
-  let value = await firebase_storage_download_json(error(), destination_latest);
+  let project_url = error();
+  let value = await firebase_storage_download_json(
+    project_url,
+    destination_latest,
+  );
   let destination_production =
     firebase_deploy_function_destination_production(f_name);
   await firebase_upload_object(destination_production, value);
