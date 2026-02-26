1c23bc3b1551158585007461884d6613fb67c9a3
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:03:39 -0500

    function_auto_path ..\love\public\src\ebible_versions.mjs

--- public/src/ebible_versions.mjs
+++ public/src/ebible_versions.mjs
@@ -1,4 +1,4 @@
-import { error } from "../../../love/public/src/error.mjs";
+import { firebase_storage_url_project_jg } from "../../../love/public/src/firebase_storage_url_project_jg.mjs";
 import { html_parse_find_a_href_starts_with_without_unique } from "../../../love/public/src/html_parse_find_a_href_starts_with_without_unique.mjs";
 import { ebible_url } from "../../../love/public/src/ebible_url.mjs";
 import { ebible_url_details } from "../../../love/public/src/ebible_url_details.mjs";
@@ -6,7 +6,7 @@ import { http_local_html_parse } from "../../../love/public/src/http_local_html_
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function ebible_versions() {
   let url = ebible_url() + "download.php";
-  let project_url = error();
+  let project_url = firebase_storage_url_project_jg();
   let r = await http_local_html_parse(url, project_url);
   let root = property_get(r, "root");
   let d = property_get(r, "d");
