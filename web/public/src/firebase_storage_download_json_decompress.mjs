173f7e8834b41d17baec9e86ecf6d139346e986a
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 19:08:13 -0500

    p

--- web/public/src/firebase_storage_download_json_decompress.mjs
+++ web/public/src/firebase_storage_download_json_decompress.mjs
@@ -1,7 +1,9 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { json_decompress_object } from "../../../love/public/src/json_decompress_object.mjs";
 import { firebase_storage_download_json } from "../../../love/public/src/firebase_storage_download_json.mjs";
 export async function firebase_storage_download_json_decompress(destination) {
-  let c = await firebase_storage_download_json(destination, error());
+  let destination2 = error();
+  let c = await firebase_storage_download_json(destination, destination2);
   let o = await json_decompress_object(c);
   return o;
 }
