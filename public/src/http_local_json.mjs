bfe3421c14ab0364850b2ee1cb1b4bc3d699295c
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:02:57 -0500

    pne http_local_json project_url

--- public/src/http_local_json.mjs
+++ public/src/http_local_json.mjs
@@ -1,6 +1,6 @@
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
-export async function http_local_json(url) {
+export async function http_local_json(url, project_url) {
   let text = await http_local_text(url, project_url);
   let dictionary = json_from(text);
   return dictionary;
