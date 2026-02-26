494890da6cebf25164c3f518e070c1086d5c8311
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:01:00 -0500

    pne http_local_text project_url

--- public/src/http_local_text.mjs
+++ public/src/http_local_text.mjs
@@ -1,6 +1,6 @@
 import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
-export async function http_local_text(url) {
+export async function http_local_text(url, project_url) {
   let buffer = await http_local(url, project_url);
   const text = buffer_text_to(buffer);
   return text;
