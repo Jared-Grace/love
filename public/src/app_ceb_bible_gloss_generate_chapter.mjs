59a3eb8ac0c76b33923e3b068dff0aa8cf6b8eac
Grace-Jared <christrosetolife@gmail.com>
2026-02-26 17:55:13 -0500

    function_auto_path ..\love\public\src\app_ceb_bible_gloss_generate_chapter.mjs

--- public/src/app_ceb_bible_gloss_generate_chapter.mjs
+++ public/src/app_ceb_bible_gloss_generate_chapter.mjs
@@ -1,6 +1,6 @@
+import { app_ceb_bible_gloss_generate_chapter_bible_folders } from "../../../love/public/src/app_ceb_bible_gloss_generate_chapter_bible_folders.mjs";
 import { ebible_chapter_code_to_book } from "../../../love/public/src/ebible_chapter_code_to_book.mjs";
 import { app_gloss_bible_generate_generic } from "../../../love/public/src/app_gloss_bible_generate_generic.mjs";
-import { ebible_folder_cebuano } from "../../../love/public/src/ebible_folder_cebuano.mjs";
 import { app_ceb_bible_gloss_generate } from "../../../love/public/src/app_ceb_bible_gloss_generate.mjs";
 export async function app_ceb_bible_gloss_generate_chapter(
   chapter_code_specified,
@@ -8,9 +8,7 @@ export async function app_ceb_bible_gloss_generate_chapter(
   let book_code = ebible_chapter_code_to_book(chapter_code_specified);
   let language = "Cebuano";
   let fn = app_ceb_bible_gloss_generate;
-  let c = ebible_folder_cebuano();
-  let bible_folder = "engbsb";
-  const bible_folders = [c, bible_folder];
+  const bible_folders = app_ceb_bible_gloss_generate_chapter_bible_folders();
   let last = "original language and English are";
   await app_gloss_bible_generate_generic(
     language,
