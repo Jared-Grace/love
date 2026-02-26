35db101846fae169efe765a7609d5640302042a1
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:00:27 -0500

    pnef http_local project_url

--- web/public/src/ebible_version_readaloud_download.mjs
+++ web/public/src/ebible_version_readaloud_download.mjs
@@ -4,7 +4,7 @@ import { unzip } from "../../../love/public/src/unzip.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
 export async function ebible_version_readaloud_download(bible_folder) {
   let url = ebible_version_download_url(bible_folder, "readaloud");
-  let buffer = await http_local(url, error());
+  let buffer = await http_local(error(), url);
   let file_path = ebible_version_readaloud_download_path(bible_folder);
   await unzip(file_path, buffer);
   return file_path;
