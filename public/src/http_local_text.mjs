37e76f67ec0902ff7aef9421567c9f7188022076
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:00:30 -0500

    function_auto_path ..\love\public\src\ebible_version_readaloud_download.mjs

--- public/src/http_local_text.mjs
+++ public/src/http_local_text.mjs
@@ -1,7 +1,9 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { buffer_text_to } from "../../../love/public/src/buffer_text_to.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
 export async function http_local_text(url) {
-  let buffer = await http_local(error(), url);
+  let url2 = error();
+  let buffer = await http_local(url, url2);
   const text = buffer_text_to(buffer);
   return text;
 }
