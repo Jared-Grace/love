42cc49aa22b82edbbd5a9b0392e78f2278287c60
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:22:39 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_open.mjs

--- public/src/g_sermon_generate_book_generic_open.mjs
+++ public/src/g_sermon_generate_book_generic_open.mjs
@@ -1,4 +1,6 @@
+import { function_import } from "../../../love/public/src/function_import.mjs";
 import { local_function_path_json } from "../../../love/public/src/local_function_path_json.mjs";
-export function g_sermon_generate_book_generic_open() {
+export async function g_sermon_generate_book_generic_open() {
+  let imported_fn = await function_import(f_name);
   let path = local_function_path_json(chapter_code, fn);
 }
