cb608b388bd21a0d93f20775f5efdaf36e9e08c7
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:04:05 -0500

    function_auto_path ..\love\public\src\reply_dictionary.mjs

--- web/public/src/reply_dictionary.mjs
+++ web/public/src/reply_dictionary.mjs
@@ -1,10 +1,10 @@
-import { error } from "../../../love/public/src/error.mjs";
+import { firebase_storage_url_project_jg } from "../../../love/public/src/firebase_storage_url_project_jg.mjs";
 import { http_local_json } from "../../../love/public/src/http_local_json.mjs";
 import { property_delete_multiple } from "../../../love/public/src/property_delete_multiple.mjs";
 export async function reply_dictionary() {
   const url =
     "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json";
-  let project_url = error();
+  let project_url = firebase_storage_url_project_jg();
   let dictionary = await http_local_json(url, project_url);
   let excludes_letters = ["h", "e", "d", "l", "t", "w"];
   property_delete_multiple(dictionary, excludes_letters);
