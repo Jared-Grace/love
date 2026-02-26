1419d3250e4d2bcb4ebb7f6ca6b95036a64528ec
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:44:56 -0500

    function_auto_path ..\love\public\src\function_run_output_file_temp_generic.mjs

--- public/src/function_run_output_file_temp_generic.mjs
+++ public/src/function_run_output_file_temp_generic.mjs
@@ -1,12 +1,12 @@
+import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
 import { file_temp_keep } from "../../../love/public/src/file_temp_keep.mjs";
 import { file_open } from "../../../love/public/src/file_open.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
-import { json_format_to_truncated } from "../../../love/public/src/json_format_to_truncated.mjs";
 import { function_run_unalias } from "../../../love/public/src/function_run_unalias.mjs";
 export async function function_run_output_file_temp_generic(f_name, args) {
   let result = await function_run_unalias(f_name, args);
   let temp_path = await file_temp_keep();
-  let json = json_format_to_truncated(result);
+  let json = json_format_to(result);
   await file_overwrite(temp_path, json);
   await file_open(temp_path);
 }
