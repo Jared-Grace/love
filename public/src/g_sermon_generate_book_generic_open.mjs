98b8dd544bd22bb2f7209ca4fdd02135536c9c5c
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:52:23 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_open.mjs

--- public/src/g_sermon_generate_book_generic_open.mjs
+++ public/src/g_sermon_generate_book_generic_open.mjs
@@ -1,3 +1,5 @@
+import { app_gloss_bible_generate_generic_prompt_user_middle } from "../../../love/public/src/app_gloss_bible_generate_generic_prompt_user_middle.mjs";
+import { app_gloss_bible_generate_generic_prompt_system } from "../../../love/public/src/app_gloss_bible_generate_generic_prompt_system.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { file_open } from "../../../love/public/src/file_open.mjs";
@@ -7,6 +9,13 @@ export async function g_sermon_generate_book_generic_open(
   f_name,
   chapter_code,
 ) {
+  const prompt_system = app_gloss_bible_generate_generic_prompt_system(
+    language,
+    word,
+    last,
+  );
+  const prompt_user_middle =
+    app_gloss_bible_generate_generic_prompt_user_middle(language);
   let imported_fn = await function_import(f_name);
   let path = local_function_path_json(chapter_code, imported_fn);
   let data = await file_read_json(path);
