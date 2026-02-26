e49523cc05fd04a4bf6410a9847f5bd2b06b6477
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:00:32 -0500

    function_auto_path ..\love\public\src\ebible_version_download.mjs

--- public/src/ebible_version_download.mjs
+++ public/src/ebible_version_download.mjs
@@ -5,8 +5,8 @@ import { unzip } from "../../../love/public/src/unzip.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
 export async function ebible_version_download(bible_folder) {
   let url = ebible_version_download_url_html(bible_folder);
-  let url2 = error();
-  let buffer = await http_local(url, url2);
+  let project_url = error();
+  let buffer = await http_local(project_url, url);
   let file_path = ebible_version_download_path(bible_folder);
   await unzip(file_path, buffer);
   return file_path;
