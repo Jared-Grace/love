3edd1bc6916e30e8e019cd1fc48e53a5875c0a98
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:53:32 -0500

    function_auto_path ..\love\public\src\invoke_cache_value_get.mjs

--- public/src/invoke_cache_value_get.mjs
+++ public/src/invoke_cache_value_get.mjs
@@ -1,7 +1,7 @@
 export function invoke_cache_value_get(fn, args) {
   let l = async function lambda() {
-    let v = await fn(...args);
-    return v;
+    let r = await fn(...args);
+    return r;
   };
   return l;
 }
