cc4a2fd2993f39172629550200812fb58540b6e8
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:00:30 -0500

    function_auto_path ..\love\public\src\ebible_version_readaloud_download.mjs

--- web/public/src/ebible_version_readaloud_download.mjs
+++ web/public/src/ebible_version_readaloud_download.mjs
@@ -1,10 +1,12 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { ebible_version_readaloud_download_path } from "../../../love/public/src/ebible_version_readaloud_download_path.mjs";
 import { ebible_version_download_url } from "../../../love/public/src/ebible_version_download_url.mjs";
 import { unzip } from "../../../love/public/src/unzip.mjs";
 import { http_local } from "../../../love/public/src/http_local.mjs";
 export async function ebible_version_readaloud_download(bible_folder) {
   let url = ebible_version_download_url(bible_folder, "readaloud");
-  let buffer = await http_local(error(), url);
+  let url2 = error();
+  let buffer = await http_local(url, url2);
   let file_path = ebible_version_readaloud_download_path(bible_folder);
   await unzip(file_path, buffer);
   return file_path;
