3b2ebb4f4fec916ff9c0f716d00d6db27e964a74
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 19:13:28 -0500

    function_auto_path ..\love\public\src\firebase_project_name_jg.mjs

--- public/src/app_next_main.mjs
+++ public/src/app_next_main.mjs
@@ -13,11 +13,11 @@ import { ebible_parts_chapter_code_to_reference } from "../../../love/public/src
 import { list_join_newline_2_copy } from "../../../love/public/src/list_join_newline_2_copy.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
-import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
+import { firebase_project_name_jg } from "../../../love/public/src/firebase_project_name_jg.mjs";
 import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function app_next_main(context) {
-  firebase_name_jg();
+  firebase_project_name_jg();
   let hash = html_hash_object_get();
   let chapter_code = property_get(hash, "c");
   let verse_number = property_get(hash, "v");
--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -11,9 +11,9 @@ import { html_mobile_default } from "../../../love/public/src/html_mobile_defaul
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
-import { firebase_name_jg } from "../../../love/public/src/firebase_name_jg.mjs";
+import { firebase_project_name_jg } from "../../../love/public/src/firebase_project_name_jg.mjs";
 export async function app_supper_main(context) {
-  firebase_name_jg();
+  firebase_project_name_jg();
   let root = html_mobile_default(context);
   html_bar_content_padding(root);
   let verses = await app_supper_verses_get();
