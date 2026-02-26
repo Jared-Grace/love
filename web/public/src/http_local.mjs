ea9545d9a374d28438fe0cd10642dedfa6099cbe
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 19:57:49 -0500

    function_auto_path ..\love\public\src\http_local.mjs

--- web/public/src/http_local.mjs
+++ web/public/src/http_local.mjs
@@ -1,4 +1,4 @@
-import { error } from "../../../love/public/src/error.mjs";
+import { firebase_storage_url_project_jg } from "../../../love/public/src/firebase_storage_url_project_jg.mjs";
 import { file_write_buffer } from "../../../love/public/src/file_write_buffer.mjs";
 import { file_read_buffer } from "../../../love/public/src/file_read_buffer.mjs";
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
@@ -13,7 +13,7 @@ export async function http_local(url) {
   let cached_exists = file_exists;
   let cached_get = file_read_buffer;
   let value_get = async function lambda2() {
-    let project_url = error();
+    let project_url = firebase_storage_url_project_jg();
     let v = await http_firebase(url, project_url);
     return v;
   };
