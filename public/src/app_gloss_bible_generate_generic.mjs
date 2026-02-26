8c8334570e6d50abba66594cb685b03beaa59239
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:51:48 -0500

    function_auto_path ..\love\public\src\app_gloss_bible_generate_generic.mjs

--- public/src/app_gloss_bible_generate_generic.mjs
+++ public/src/app_gloss_bible_generate_generic.mjs
@@ -1,3 +1,4 @@
+import { app_gloss_bible_generate_generic_prompt_system } from "../../../love/public/src/app_gloss_bible_generate_generic_prompt_system.mjs";
 import { app_gloss_bible_generate_generic_word } from "../../../love/public/src/app_gloss_bible_generate_generic_word.mjs";
 import { g_sermon_generate_book_generic } from "../../../love/public/src/g_sermon_generate_book_generic.mjs";
 export async function app_gloss_bible_generate_generic(
@@ -9,16 +10,11 @@ export async function app_gloss_bible_generate_generic(
   chapter_code_specified,
 ) {
   let word = app_gloss_bible_generate_generic_word();
-  const prompt_system = `You will be given a Bible passage and its context in ${language}.
-For each ${language} word, output an English gloss.
-Also output a full explanation of each ${language} word, explaining its meaning and grammar (including prefixes and suffixes), written for an English speaker with no background in grammar. 
-Explanations should include any etymology.
-Explanations should be easy to understand. Explain as simply as possible.
-
-Output format:
-[{"${word}":"${language} word","gloss":"English gloss","explain":"full explanation"}, ...]
-
-The ${last} provided as a reference.`;
+  const prompt_system = app_gloss_bible_generate_generic_prompt_system(
+    language,
+    word,
+    last,
+  );
   const prompt_user_middle = `Here is the ${language} passage to output English glosses for: `;
   await g_sermon_generate_book_generic(
     bible_folders,
