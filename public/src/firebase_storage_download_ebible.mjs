46775775c8f7a37bd9838f0c9dc6b610d0a2d0ef
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 20:07:32 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_ebible.mjs

--- public/src/firebase_storage_download_ebible.mjs
+++ public/src/firebase_storage_download_ebible.mjs
@@ -3,7 +3,6 @@ import { firebase_storage_download_json_jg } from "../../../love/public/src/fire
 import { ebible_firebase_upload_path } from "../../../love/public/src/ebible_firebase_upload_path.mjs";
 export async function firebase_storage_download_ebible(en, file_name) {
   let destination = ebible_firebase_upload_path(en, file_name);
-  let project_url = firebase_storage_url_project_jg();
-  let o = await firebase_storage_download_json_jg(destination, destination2);
+  let o = await firebase_storage_download_json_jg(destination, );
   return o;
 }
