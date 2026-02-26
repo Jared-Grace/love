63296160522774c3061b36936a9e3467ed43d97b
Grace-Jared <christrosetolife@gmail.com>
2026-02-25 19:56:33 -0500

    function_auto_path ..\love\public\src\firebase_storage_download_json_decompress.mjs

--- public/src/firebase_storage_download_json_decompress.mjs
+++ public/src/firebase_storage_download_json_decompress.mjs
@@ -1,6 +1,9 @@
 import { json_decompress_object } from "../../../love/public/src/json_decompress_object.mjs";
 import { firebase_storage_download_json } from "../../../love/public/src/firebase_storage_download_json.mjs";
-export async function firebase_storage_download_json_decompress(destination) {
+export async function firebase_storage_download_json_decompress(
+  project_url,
+  destination,
+) {
   let c = await firebase_storage_download_json(project_url, destination);
   let o = await json_decompress_object(c);
   return o;
