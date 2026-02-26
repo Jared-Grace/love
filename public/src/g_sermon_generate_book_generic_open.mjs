81256fe4ae5e29d8fd01bf0f7a3f138e6ca190ae
Grace-Jared <christrosetolife@gmail.com>
2026-02-26 17:54:31 -0500

    function_auto_path ..\love\public\src\g_sermon_generate_book_generic_open.mjs

--- public/src/g_sermon_generate_book_generic_open.mjs
+++ public/src/g_sermon_generate_book_generic_open.mjs
@@ -10,14 +10,6 @@ export async function g_sermon_generate_book_generic_open(
   f_name,
   chapter_code,
 ) {
-  let chapters = await g_sermon_generate_book_generic_prompts(
-    bible_folders,
-    book_code,
-    chapter_code_specified,
-    fn,
-    prompt_user_middle2,
-    prompt_system2,
-  );
   const prompt_system = app_gloss_bible_generate_generic_prompt_system(
     language,
     word,
@@ -25,6 +17,14 @@ export async function g_sermon_generate_book_generic_open(
   );
   const prompt_user_middle =
     app_gloss_bible_generate_generic_prompt_user_middle(language);
+  let chapters = await g_sermon_generate_book_generic_prompts(
+    bible_folders,
+    book_code,
+    chapter_code_specified,
+    fn,
+    prompt_user_middle,
+    prompt_system,
+  );
   let imported_fn = await function_import(f_name);
   let path = local_function_path_json(chapter_code, imported_fn);
   let data = await file_read_json(path);
