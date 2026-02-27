beb448a3822bbd4895c14d8e0f0c48c00466581c
Grace-Jared <christrosetolife@gmail.com>
2026-02-27 15:44:13 -0500

    p

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,9 +1,9 @@
 import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
 import { ebible_chapters_upload_refresh } from "../../../love/public/src/ebible_chapters_upload_refresh.mjs";
 export async function sandbox() {
+  let bible_folder = "engbsb";
   let chapters = await ebible_version_chapters_cache(bible_folder);
   return chapters;
   return;
-  let bible_folder = "engbsb";
   await ebible_chapters_upload_refresh(bible_folder);
 }
