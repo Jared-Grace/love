2ca4bcf26c103840b6f6966c4ac5072545d374b6
Grace-Jared <christrosetolife@gmail.com>
2026-02-27 15:46:09 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,9 @@
+import { ebible_verses_readaloud } from "../../../love/public/src/ebible_verses_readaloud.mjs";
 import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
-import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
 import { ebible_chapters_upload_refresh } from "../../../love/public/src/ebible_chapters_upload_refresh.mjs";
 export async function sandbox() {
   let bible_folder = "engbsb";
-  let chapters = await ebible_version_chapters_cache(bible_folder);
+  let chapters = await ebible_verses_readaloud(bible_folder);
   let item = list_find_property(chapters, "chapter_code", "AMO09");
   ebible_verses_get;
   return item;
