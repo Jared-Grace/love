3b252b8cc3c19b17664383315ff00cc9041ef946
Grace-Jared <christrosetolife@gmail.com>
2026-02-27 15:43:58 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,8 @@
+import { ebible_version_chapters_cache } from "../../../love/public/src/ebible_version_chapters_cache.mjs";
 import { ebible_chapters_upload_refresh } from "../../../love/public/src/ebible_chapters_upload_refresh.mjs";
 export async function sandbox() {
+  let chapters = await ebible_version_chapters_cache(bible_folder);
+  return;
   let bible_folder = "engbsb";
   await ebible_chapters_upload_refresh(bible_folder);
 }
