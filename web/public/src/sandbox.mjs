37c5315b373a8695a144d2920d7def03d9f6bd0f
Grace-Jared <christrosetolife@gmail.com>
2026-02-27 15:14:58 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { ebible_version_chapters } from "../../../love/public/src/ebible_version_chapters.mjs";
 import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_file_remove.mjs";
 export async function sandbox() {
-  await invoke_cache_file_remove(fn, args);
+  let bible_folder = "engbsb";
+  await invoke_cache_file_remove(ebible_version_chapters, [bible_folder]);
 }
