7893ebbf6719d89e6dd7e29d7f154c53e4612dfd
Grace-Jared <christrosetolife@gmail.com>
2026-02-27 15:14:39 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,4 @@
-export async function sandbox() {}
+import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_file_remove.mjs";
+export async function sandbox() {
+  await invoke_cache_file_remove(fn, args);
+}
