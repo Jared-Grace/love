53578d93d6216eb9a33bb68416c765ff6d24ca04
Grace-Jared <christrosetolife@gmail.com>
2026-02-27 11:13:33 -0500

    function_auto_path ..\love\public\src\app_shared_name_search.mjs

--- public/src/app_shared_name_search.mjs
+++ public/src/app_shared_name_search.mjs
@@ -2,6 +2,7 @@ import { list_find_text_match_ordered } from "../../../love/public/src/list_find
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { apps_names } from "../../../love/public/src/apps_names.mjs";
 export async function app_shared_name_search(search) {
+  ta;
   let mapped = await apps_names();
   let includes = list_includes(mapped, search);
   let a_name = null;
