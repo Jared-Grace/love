661670ab5ce577fec401bdcdae14a9584161f4f2
Grace-Jared <christrosetolife@gmail.com>
2026-02-26 21:32:08 -0500

    function_auto_path ..\love\public\src\ebible_references_parse_lines_browser.mjs

--- public/src/ebible_references_parse_lines_browser.mjs
+++ public/src/ebible_references_parse_lines_browser.mjs
@@ -1,10 +1,12 @@
 import { ebible_version_books_browser } from "../../../love/public/src/ebible_version_books_browser.mjs";
 import { ebible_references_parse_lines_generic } from "../../../love/public/src/ebible_references_parse_lines_generic.mjs";
+import { ebible_verse } from "../../../love/public/src/ebible_verse.mjs";
 export async function ebible_references_parse_lines_browser(
   bible_folders,
   lines,
 ) {
   let books_get = ebible_version_books_browser;
+  let verse_get = ebible_verse;
   let waited = await ebible_references_parse_lines_generic(
     books_get,
     verse_get,
