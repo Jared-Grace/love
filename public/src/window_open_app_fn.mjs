9167d29c235e51216ffdf5ea0feb0cde5306f12d
Grace-Jared <christrosetolife@gmail.com>
2026-02-28 14:59:26 -0500

    function_auto_path ..\love\public\src\window_open_app_fn.mjs

--- public/src/window_open_app_fn.mjs
+++ public/src/window_open_app_fn.mjs
@@ -1,6 +1,6 @@
 import { window_open_app_generic } from "../../../love/public/src/window_open_app_generic.mjs";
 import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
-export function window_open_app_fn(app_fn_name, hash) {
+export function window_open_app_fn(app_fn, hash) {
   let fn = app_prefix_without_fn;
-  window_open_app_generic(fn, app_fn_name, hash);
+  window_open_app_generic(fn, app_fn, hash);
 }
