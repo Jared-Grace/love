fafadf066ac38a40282c751835b312795cd46658
Grace-Jared <christrosetolife@gmail.com>
2026-02-28 14:58:40 -0500

    function_auto_path ..\love\public\src\window_open_app_fn.mjs

--- web/public/src/window_open_app_fn.mjs
+++ web/public/src/window_open_app_fn.mjs
@@ -4,7 +4,7 @@ import { window_open } from "../../../love/public/src/window_open.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 export function window_open_app_fn(app_fn_name, hash) {
   let fn = app_prefix_without_fn;
-  let without = app_prefix_without_fn(app_fn_name);
+  let without = fn(app_fn_name);
   let file_name = file_name_html(without);
   let h2 = hash_to_url(hash);
   window_open(file_name + h2);
