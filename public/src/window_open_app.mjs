76ab031488bcd2e03833e24257860c834756c6a4
Grace-Jared <christrosetolife@gmail.com>
2026-02-28 14:59:06 -0500

    crml window_open_app_fn

---
+++ public/src/window_open_app.mjs
@@ -0,0 +1,6 @@
+import { window_open_app_generic } from "../../../love/public/src/window_open_app_generic.mjs";
+import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
+export function window_open_app(app_fn_name, hash) {
+  let fn = app_prefix_without_fn;
+  window_open_app_generic(fn, app_fn_name, hash);
+}
