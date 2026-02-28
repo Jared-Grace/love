e69b1e5317df428e9a847355c3710270fd616a01
Grace-Jared <christrosetolife@gmail.com>
2026-02-28 14:47:28 -0500

    function_auto_path ..\love\public\src\firebase_app_initialize.mjs

--- public/src/firebase_app_initialize.mjs
+++ public/src/firebase_app_initialize.mjs
@@ -1,12 +1,13 @@
 import { global_function_async } from "../../../love/public/src/global_function_async.mjs";
 import { firebase_config_get } from "../../../love/public/src/firebase_config_get.mjs";
+import { initializeApp } from "firebase/app";
 export async function firebase_app_initialize() {
   async function lambda2() {
-    const firebase = await import(
+    `const firebase = await import(
       "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"
-    );
+    );`;
     let firebase_config = firebase_config_get();
-    const app = firebase.initializeApp(firebase_config);
+    const app = initializeApp(firebase_config);
     return app;
   }
   let awaited = await global_function_async(firebase_app_initialize, lambda2);
