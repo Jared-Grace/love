0fd25afd4294e964f8e116b8ce23975dd4be167a
Grace-Jared <christrosetolife@gmail.com>
2026-02-28 14:49:59 -0500

    function_auto_path ..\love\public\src\firebase_login.mjs

--- web/public/src/firebase_login.mjs
+++ web/public/src/firebase_login.mjs
@@ -1,19 +1,20 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { firebase_login_email } from "../../../love/public/src/firebase_login_email.mjs";
 import { firebase_app_initialize } from "../../../love/public/src/firebase_app_initialize.mjs";
 export async function firebase_login(context, on_logged_in) {
   const app = await firebase_app_initialize();
-  const firebase_auth = await import(
+  `const firebase_auth = await import(
     "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js"
-  );
-  const auth = firebase_auth.getAuth(app);
+  );`;
+  const auth = getAuth(app);
   function lambda(user) {
     if (user) {
       on_logged_in({
         user,
       });
     } else {
-      firebase_login_email(context, firebase_auth, auth);
+      firebase_login_email(context, auth);
     }
   }
-  firebase_auth.onAuthStateChanged(auth, lambda);
+  onAuthStateChanged(auth, lambda);
 }
