2f9dec54673063a5b248ea3339f5cf77f45ec9bf
Grace-Jared <christrosetolife@gmail.com>
2026-02-28 14:59:45 -0500

    function_auto_path ..\love\public\src\window_open_app_curried_right.mjs

--- public/src/window_open_app_curried_right.mjs
+++ public/src/window_open_app_curried_right.mjs
@@ -1,7 +1,7 @@
 import { window_open_app } from "../../../love/public/src/window_open_app.mjs";
 export function window_open_app_curried_right(hash) {
-  let r2 = function window_open_app_curried_right_result(app_fn) {
-    let r = window_open_app(app_fn, hash);
+  let r2 = function window_open_app_curried_right_result(app_fn_name) {
+    let r = window_open_app(app_fn_name, hash);
     return r;
   };
   return r2;
