35912db0defb17e1755598c56938fa2319272398
Grace-Jared <christrosetolife@gmail.com>
2026-03-02 07:33:19 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set_grow_same_three_different.mjs

--- web/public/src/app_replace_rule_set_grow_same_three_different.mjs
+++ web/public/src/app_replace_rule_set_grow_same_three_different.mjs
@@ -1,15 +1,15 @@
 export function app_replace_rule_set_grow_same_three_different() {
   let r = {
     name: "Grow Same Three Different",
-    rules: ["c e b c > e b"],
+    rules: ["e = b > c e = b c"],
     goals: [
       {
-        start: "cce=bcc",
-        end: "e=b",
+        start: "e=b",
+        end: "cce=bcc",
       },
       {
-        start: "cccce=bcccc",
-        end: "e=b",
+        start: "e=b",
+        end: "cccce=bcccc",
       },
     ],
   };
