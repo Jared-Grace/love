19505bbe15c818c5abeb403edd5ac61e1047d3ee
Grace-Jared <christrosetolife@gmail.com>
2026-03-03 11:15:50 -0500

    function_auto_path ..\love\public\src\app_replace_rule_set_unary_to_binary_equations.mjs

--- web/public/src/app_replace_rule_set_unary_to_binary_equations.mjs
+++ web/public/src/app_replace_rule_set_unary_to_binary_equations.mjs
@@ -8,9 +8,9 @@ export function app_replace_rule_set_unary_to_binary_equations() {
       "d c > a d",
       "d e = > = d",
       "d + > + d",
-      "0 a > 1",
-      "1 a > a 0",
       "b a > b 1",
+      "1 a > a 0",
+      "0 a > 1",
     ],
     goals: [
       {
