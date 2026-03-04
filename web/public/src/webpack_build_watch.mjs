eb355775edccddf08fa9ef34bbbce08c21cc5206
Grace-Jared <christrosetolife@gmail.com>
2026-03-04 18:14:25 -0500

    function_auto_path ..\love\public\src\webpack_build_watch.mjs

--- web/public/src/webpack_build_watch.mjs
+++ web/public/src/webpack_build_watch.mjs
@@ -1,8 +1,8 @@
-import { app_shared_name_dev_text } from "../../../love/public/src/app_shared_name_dev_text.mjs";
 import { webpack_build_generic } from "../../../love/public/src/webpack_build_generic.mjs";
+import { app_shared_name_latest_text } from "../../../love/public/src/app_shared_name_latest_text.mjs";
 import { folder_public_join } from "./folder_public_join.mjs";
 export async function webpack_build_watch(search) {
-  const command_text_after = "";
+  const command_text_after = "--watch";
   const folder = app_shared_name_dev_text();
   let config_folder = folder_public_join(folder);
   let result = await webpack_build_generic(
