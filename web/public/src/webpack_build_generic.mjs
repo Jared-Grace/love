ebc5e4658837106623da9febe500cc7b3f35751f
Grace-Jared <christrosetolife@gmail.com>
2026-03-04 18:12:30 -0500

    function_auto_path ..\love\public\src\webpack_build.mjs

---
+++ web/public/src/webpack_build_generic.mjs
@@ -0,0 +1,59 @@
+import { file_delete_after } from "../../../love/public/src/file_delete_after.mjs";
+import { command_line } from "../../../love/public/src/command_line.mjs";
+import { command_line_text_env_vars } from "../../../love/public/src/command_line_text_env_vars.mjs";
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
+import { webpack_config_folder } from "../../../love/public/src/webpack_config_folder.mjs";
+import { webpack_config_filename } from "../../../love/public/src/webpack_config_filename.mjs";
+import { webpack_config_entry_path } from "../../../love/public/src/webpack_config_entry_path.mjs";
+import { folder_current_join } from "../../../love/public/src/folder_current_join.mjs";
+import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
+import { file_name_js } from "../../../love/public/src/file_name_js.mjs";
+import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { js_code_call_app_context_initialize } from "../../../love/public/src/js_code_call_app_context_initialize.mjs";
+import { webpack_build_code_import } from "../../../love/public/src/webpack_build_code_import.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
+import { folder_scripts_join_mjs } from "../../../love/public/src/folder_scripts_join_mjs.mjs";
+import { path_join } from "../../../love/public/src/path_join.mjs";
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
+export async function webpack_build_generic(
+  search,
+  config_folder,
+  command_text_after,
+) {
+  let a = await app_shared_name_search_info(search);
+  let a_name = property_get(a, "a_name");
+  let f_name = property_get(a, "f_name");
+  let combined = function_name_combine(f_name, "run");
+  let path2 = path_join(["temp", combined]);
+  let f_name_ext = folder_scripts_join_mjs(path2);
+  let fns = [f_name, app_context_initialize.name];
+  let mapped = list_map(fns, webpack_build_code_import);
+  let call = js_code_call_app_context_initialize(f_name);
+  list_add(mapped, call);
+  let joined = list_join_newline(mapped);
+  let r = file_name_js(a_name);
+  async function lambda(entry) {
+    await file_overwrite(entry, joined);
+    const entry_path = folder_current_join(entry);
+    let env_vars = {
+      [webpack_config_entry_path()]: entry_path,
+      [webpack_config_filename()]: r,
+      [webpack_config_folder()]: config_folder,
+    };
+    let f_name_ext = folder_scripts_join_mjs("webpack.config");
+    let combined2 = text_combine_multiple([
+      "npx webpack --config ",
+      f_name_ext,
+      command_text_after,
+    ]);
+    let command = await command_line_text_env_vars(env_vars, combined2);
+    let stdout = await command_line(command);
+    return stdout;
+  }
+  let result = await file_delete_after(f_name_ext, lambda);
+  return result;
+}
