027fb8dae2fe2e989eb57056079c59846441a507
Grace-Jared <christrosetolife@gmail.com>
2026-03-04 18:16:15 -0500

    function_auto_path ..\love\public\src\webpack_build_generic.mjs

--- public/src/webpack_build_generic.mjs
+++ public/src/webpack_build_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { file_delete_after } from "../../../love/public/src/file_delete_after.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { command_line_text_env_vars } from "../../../love/public/src/command_line_text_env_vars.mjs";
@@ -52,6 +53,9 @@ export async function webpack_build_generic(
       command_text_after,
     ]);
     let command = await command_line_text_env_vars(env_vars, combined2);
+    log({
+      command,
+    });
     let stdout = await command_line(command);
     return stdout;
   }
