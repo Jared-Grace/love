9f71ecf9a4d0005ec840a17907ab08b5e7919ae2
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:35:58 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,8 +1,10 @@
+import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { file_read_folder_user_split_normalize } from "../../../love/public/src/file_read_folder_user_split_normalize.mjs";
 export async function sandbox() {
   const path = "David.txt";
   let r = await file_read_folder_user_split_normalize(path);
   let normalized = property_get(r, "normalized");
+  let filtered = list_filter_empty_not_is(item);
   return normalized;
 }
