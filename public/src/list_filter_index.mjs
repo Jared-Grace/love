6d1c5c7f39c09b761e413673c742d89c176c141e
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:57:03 -0500

    function_auto_path ..\love\public\src\list_filter_index.mjs

--- public/src/list_filter_index.mjs
+++ public/src/list_filter_index.mjs
@@ -1,8 +1,8 @@
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
-export function list_filter_index(list, lambda$item) {
+export function list_filter_index(list, lambda$item$index) {
   list_is_assert(list);
-  function list_filter_lambda(item) {
-    let match = lambda$item(item);
+  function list_filter_lambda(item, index) {
+    let match = lambda$item$index(item, index);
     return match;
   }
   let filtered = list.filter(list_filter_lambda);
