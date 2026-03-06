8e4ba89960c6ccc38432affd6aab03727a7f0948
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:39:10 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,5 +6,6 @@ export async function sandbox() {
   let r = await file_read_folder_user_split_normalize(path);
   let normalized = property_get(r, "normalized");
   let filtered = list_filter_empty_not_is(normalized);
+  let separator = "---";
   return filtered;
 }
