dd7e89dc1bd947a4f3d3cbb7cbd56f573046430f
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 14:07:53 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -20,7 +20,7 @@ export async function sandbox() {
   let list = list_filter_index(groups, lambda);
   let language = "Urdu";
   let r3 = await openai_responses_cache(
-    "Translate the text inside the JSON object to " + language,
+    "Translate the JSON object to " + language,
     user,
   );
   return list;
