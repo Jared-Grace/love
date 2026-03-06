70e2703683e5a9ee45de52ac540be22f388fd250
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:34:13 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,8 @@
-export async function sandbox() {}
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { file_read_folder_user_split_normalize } from "../../../love/public/src/file_read_folder_user_split_normalize.mjs";
+export async function sandbox() {
+  const path = "David.txt";
+  let r = await file_read_folder_user_split_normalize(path);
+  let split = property_get(r, "split");
+  let normalized = property_get(r, "normalized");
+}
