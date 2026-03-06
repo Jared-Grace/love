2e91d1f947e2cca679da83d06199a3a0225f5420
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:34:19 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -4,5 +4,5 @@ export async function sandbox() {
   const path = "David.txt";
   let r = await file_read_folder_user_split_normalize(path);
   let split = property_get(r, "split");
-  let normalized = property_get(r, "normalized");
+  let normalized = property_get(r, "normalized");$r,split
 }
