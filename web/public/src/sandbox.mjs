54a9a4bd98361c971e3d20a6aefd08e8b2c7b8c9
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:49:42 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { list_adder_group } from "../../../love/public/src/list_adder_group.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { file_read_folder_user_split_normalize } from "../../../love/public/src/file_read_folder_user_split_normalize.mjs";
@@ -6,6 +7,7 @@ export async function sandbox() {
   let r = await file_read_folder_user_split_normalize(path);
   let normalized = property_get(r, "normalized");
   let filtered = list_filter_empty_not_is(normalized);
-  let separator = "---";list_adder_group
+  let separator = "---";
+  let groups = list_adder_group(function lambda2() {});
   return filtered;
 }
