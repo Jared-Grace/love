7f3ee45de8aa3cf8283b858f8da2160baed0f7d9
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 13:54:20 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ public/src/list_split.mjs
@@ -0,0 +1,19 @@
+import { list_adder_group } from "../../../love/public/src/list_adder_group.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+import { add } from "../../../love/public/src/add.mjs";
+export function list_split(separator, filtered) {
+  function lambda2(g) {
+    g.start();
+    function lambda(item) {
+      if (item === separator) {
+        g.end();
+      } else {
+        g.add(item);
+      }
+    }
+    each(filtered, lambda);
+    g.end();
+  }
+  let groups = list_adder_group(lambda2);
+  return groups;
+}
--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,4 @@
-import { add } from "../../../love/public/src/add.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { list_adder_group } from "../../../love/public/src/list_adder_group.mjs";
+import { list_split } from "../../../love/public/src/list_split.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { file_read_folder_user_split_normalize } from "../../../love/public/src/file_read_folder_user_split_normalize.mjs";
@@ -10,18 +8,6 @@ export async function sandbox() {
   let normalized = property_get(r, "normalized");
   let filtered = list_filter_empty_not_is(normalized);
   let separator = "---";
-  function lambda2(g) {
-    g.start();
-    function lambda(item) {
-      if (item === separator) {
-        g.end();
-      } else {
-        g.add(item);
-      }
-    }
-    each(filtered, lambda);
-    g.end();
-  }
-  let groups = list_adder_group(lambda2);
+  let groups = list_split(separator, filtered);
   return groups;
 }
