13ad500b90d6139e476e1ddacdd15f55e97c822a
Grace-Jared <christrosetolife@gmail.com>
2026-03-06 14:33:01 -0500

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -38,7 +38,7 @@ export async function sandbox() {
     let change = lambda(item2, index2);
     if (change) {
       let i = floor(index2 / 2);
-      let item3 = list_get(list2, index3);
+      let item3 = list_get(list2, value2);
     }
   }
   let mapped = list_map_index(list, lambda2);
