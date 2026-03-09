a3521d5efee0daca0e5605581229aeeddb00378b
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 12:13:22 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new.mjs

--- web/public/src/app_new_rule_set_new.mjs
+++ web/public/src/app_new_rule_set_new.mjs
@@ -7,22 +7,6 @@ export function app_new_rule_set_new() {
         start: "a",
         end: "b",
       },
-      {
-        start: "aa",
-        end: "bb",
-      },
-      {
-        start: "aaa",
-        end: "bbb",
-      },
-      {
-        start: "aaaa",
-        end: "abba",
-      },
-      {
-        start: "aaaaaaa",
-        end: "abababa",
-      },
     ],
   };
   return r;
