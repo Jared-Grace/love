c6b1f28d3bffd5cf846f154d40d0807d9200e623
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:32:01 -0400

    function_auto_path ..\love\public\src\property_equals_try.mjs

--- web/public/src/property_equals_try.mjs
+++ web/public/src/property_equals_try.mjs
@@ -1,8 +1,6 @@
-import { function_param_swap_end } from "../../../love/public/src/function_param_swap_end.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { equal } from "./equal.mjs";
-export async function property_equals_try(item, property_name, property_value) {
-  await function_param_swap_end(f_name);
+export function property_equals_try(item, property_name, property_value) {
   let left = property_get(item, property_name);
   let eq = equal(left, property_value);
   return eq;
