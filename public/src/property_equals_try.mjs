24a2dcea10b780888b044a27a2f471b5f6ae8a61
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:32:39 -0400

    function_auto_path ..\love\public\src\property_equals_try.mjs

--- public/src/property_equals_try.mjs
+++ public/src/property_equals_try.mjs
@@ -3,6 +3,8 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { equal } from "./equal.mjs";
 export function property_equals_try(item, property_name, property_value) {
   let exists = property_exists(object, property_name2);
+  if (false) {
+  }
   let left = property_get(item, property_name);
   let eq = equal(left, property_value);
   return eq;
