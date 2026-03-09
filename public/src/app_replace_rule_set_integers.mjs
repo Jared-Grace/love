f149f9a38509db42d17465f06af01db3403729e1
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 16:07:55 -0400

    arr app_replace_rule_set_integer_digits_2

--- public/src/app_replace_rule_set_integers.mjs
+++ public/src/app_replace_rule_set_integers.mjs
@@ -1,7 +1,7 @@
 import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_map_combine_left } from "../../../love/public/src/list_map_combine_left.mjs";
-export function app_replace_rule_set_integer_digits_2() {
+export function app_replace_rule_set_integers() {
   let r2 = digits_positive();
   let combineds = list_map_combine_left(r2, "c > ");
   const rules = ["a > c b", "a > d", "b > d b", "d > 0", "d > c", "b > d"];
