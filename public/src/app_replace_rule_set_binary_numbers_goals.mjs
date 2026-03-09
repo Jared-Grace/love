1636ec2b25e175f95a38cfca3b2566cbe796cd5e
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:20:28 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_binary_numbers.mjs

---
+++ public/src/app_replace_rule_set_binary_numbers_goals.mjs
@@ -0,0 +1,29 @@
+export function app_replace_rule_set_binary_numbers_goals() {
+  let r2 = [
+    {
+      start: "a",
+      end: "0",
+    },
+    {
+      start: "a",
+      end: "1",
+    },
+    {
+      start: "a",
+      end: "10",
+    },
+    {
+      start: "a",
+      end: "11",
+    },
+    {
+      start: "a",
+      end: "101",
+    },
+    {
+      start: "a",
+      end: "1010",
+    },
+  ];
+  return r2;
+}
