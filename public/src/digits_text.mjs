8e4f6ef9c5bfefb8ad59dbcdff7b42811fd0181c
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:43:12 -0400

    function_auto_path ..\love\public\src\digits_text.mjs

--- public/src/digits_text.mjs
+++ public/src/digits_text.mjs
@@ -1,4 +1,5 @@
+import { digits_positive_text } from "../../../love/public/src/digits_positive_text.mjs";
 export function digits_text() {
-  let d = "0" + "123456789";
+  let d = "0" + digits_positive_text();
   return d;
 }
