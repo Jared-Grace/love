ebdecc5e342c68b84414f1b40bdb191079b23cc9
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:39:17 -0400

    function_auto_path ..\love\public\src\list_find_property_or_null.mjs

--- public/src/list_find_property_or_null.mjs
+++ public/src/list_find_property_or_null.mjs
@@ -1,5 +1,5 @@
+import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
-import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { property_equals_curry_right_2 } from "../../../love/public/src/property_equals_curry_right_2.mjs";
 export function list_find_property_or_null(
@@ -10,10 +10,9 @@ export function list_find_property_or_null(
   let filter_get = property_equals_curry_right_2;
   let filter = filter_get(property_name, property_value);
   let filtered = list_filter(list, filter);
-  let e = list_empty_is(filtered);
+  let n = list_empty_not_is(filtered);
   let only = null;
-  if (e) {
-  } else {
+  if (n) {
     only = list_single(filtered);
   }
   return only;
