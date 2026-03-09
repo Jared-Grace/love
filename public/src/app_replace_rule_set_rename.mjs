3e6746acbac562cc7a460eb8ed5f5cd5a7f25310
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 19:59:04 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_rename.mjs

--- public/src/app_replace_rule_set_rename.mjs
+++ public/src/app_replace_rule_set_rename.mjs
@@ -1,14 +1,12 @@
+import { function_ast_list_type_nodes } from "../../../love/public/src/function_ast_list_type_nodes.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_replace_rule_sets_name_expression } from "../../../love/public/src/app_replace_rule_sets_name_expression.mjs";
-import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
-import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { function_rename_open } from "./function_rename_open.mjs";
 export async function app_replace_rule_set_rename(f_name) {
   const type = "ObjectExpression";
-  let ast = await function_ast(f_name);
-  let list = js_list_type_nodes(ast, type);
+  let list = await function_ast_list_type_nodes(f_name, type);
   let mapped = list_map(list, app_replace_rule_sets_name_expression);
   let filtered = list_filter_null_not_is(mapped);
   let f_name_after = list_single(filtered);
