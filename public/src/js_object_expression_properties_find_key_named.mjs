7230124a4bff0020f3944a0ead9c774a06f6aaaf
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:52:46 -0400

    function_auto_path ..\love\public\src\js_object_expression_properties_find_key_named.mjs

--- public/src/js_object_expression_properties_find_key_named.mjs
+++ public/src/js_object_expression_properties_find_key_named.mjs
@@ -1,9 +1,11 @@
+import { log_unparse } from "../../../love/public/src/log_unparse.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_find_property_try_or_null } from "../../../love/public/src/list_find_property_try_or_null.mjs";
 import { js_property_key_named_curried_right } from "../../../love/public/src/js_property_key_named_curried_right.mjs";
 import { js_object_expression_properties } from "../../../love/public/src/js_object_expression_properties.mjs";
 export function js_object_expression_properties_find_key_named(e, search) {
   let properties = js_object_expression_properties(e);
+  log_unparse(node);
   log({
     properties,
   });
