6e6a1359a38af90a096633b9ffac568d787ea46f
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:55:23 -0400

    function_auto_path ..\love\public\src\property_equals_try.mjs

--- web/public/src/property_equals_try.mjs
+++ web/public/src/property_equals_try.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { property_equals } from "../../../love/public/src/property_equals.mjs";
 import { property_exists_not } from "../../../love/public/src/property_exists_not.mjs";
 export function property_equals_try(object, property_name, property_value) {
@@ -6,5 +7,10 @@ export function property_equals_try(object, property_name, property_value) {
     return false;
   }
   let eq = property_equals(object, property_name, property_value);
+  log({
+    eq,
+    property_name,
+    property_value,
+  });
   return eq;
 }
