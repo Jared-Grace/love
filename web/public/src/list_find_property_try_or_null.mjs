4e43258a1e1b7da0b6bafbb3c4ad8f87d795c925
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:47:35 -0400

    function_auto_path ..\love\public\src\list_find_property_try_or_null.mjs

--- web/public/src/list_find_property_try_or_null.mjs
+++ web/public/src/list_find_property_try_or_null.mjs
@@ -1,5 +1,5 @@
+import { property_equals_try_curried_right_2 } from "../../../love/public/src/property_equals_try_curried_right_2.mjs";
 import { list_find_property_or_null_generic } from "../../../love/public/src/list_find_property_or_null_generic.mjs";
-import { property_equals_curry_right_2 } from "../../../love/public/src/property_equals_curry_right_2.mjs";
 export function list_find_property_try_or_null(
   list,
   property_name,
