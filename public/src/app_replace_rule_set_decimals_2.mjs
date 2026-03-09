638cc82d7b67946cfe18032569e3b1b125711cf4
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 18:22:10 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_2.mjs

--- public/src/app_replace_rule_set_decimals_2.mjs
+++ public/src/app_replace_rule_set_decimals_2.mjs
@@ -2,7 +2,14 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
 export function app_replace_rule_set_decimals_2() {
   const rules = app_replace_rule_set_integers_rules();
-  list_add_multiple(rules, ["h > a j", "h > g j", "j > i k", "k > "]);
+  list_add_multiple(rules, [
+    "h > a j",
+    "h > g j",
+    "j > i k",
+    "i > e",
+    "i > E",
+    "k > ",
+  ]);
   let r = {
     name: "Numbers",
     rules,
