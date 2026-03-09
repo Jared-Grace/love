1972bd5b2cfc06a6136d930cb5a59611dc4b3044
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:23:28 -0400

    function_auto_path ..\love\public\src\js_list_type_nodes.mjs

--- public/src/js_list_type_nodes.mjs
+++ public/src/js_list_type_nodes.mjs
@@ -2,6 +2,6 @@ import { list_map_property } from "../../../love/public/src/list_map_property.mj
 import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
 export function js_list_type_nodes(ast, node_type) {
   let vs = js_list_type(ast, node_type);
-  let mapped = list_map_property(vs, "node");
-  return mapped;
+  let nodes = list_map_property(vs, "node");
+  return nodes;
 }
