49e2fea81f1895b08c1ea9db0511a154883b4784
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 12:28:57 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,7 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Binary numbers",
-    rules: ["a > a b", "b > 0", "b > 1"],
+    rules: ["a > a b", , "a > b", "b > 0", "b > 1"],
     goals: [
       {
         start: "a",
