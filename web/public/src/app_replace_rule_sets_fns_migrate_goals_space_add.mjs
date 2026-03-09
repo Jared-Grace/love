394eaa9ade472963c47972b91d9fcd3088253164
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 19:56:58 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -1,4 +1,4 @@
-import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
+import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 export function app_replace_rule_sets_fns_migrate_goals_space_add() {
-  let f_name = app_replace_rule_sets.name;
+  let f_name = app_replace_rule_sets_fns.name;
 }
