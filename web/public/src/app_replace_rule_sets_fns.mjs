2b710cebb23996bb1ee204c6660ef2025148c57f
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:19:48 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns.mjs

--- web/public/src/app_replace_rule_sets_fns.mjs
+++ web/public/src/app_replace_rule_sets_fns.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_set_binary_numbers_simple } from "../../../love/public/src/app_replace_rule_set_binary_numbers_simple.mjs";
 import { app_replace_rule_set_binary_numbers } from "../../../love/public/src/app_replace_rule_set_binary_numbers.mjs";
 import { app_replace_rule_set_swap_3 } from "../../../love/public/src/app_replace_rule_set_swap_3.mjs";
 import { app_replace_rule_set_swap_2 } from "../../../love/public/src/app_replace_rule_set_swap_2.mjs";
@@ -86,6 +87,7 @@ export function app_replace_rule_sets_fns() {
     app_replace_rule_set_unary_to_binary_equations_preparation,
     app_replace_rule_set_unary_to_binary_equations,
     app_replace_rule_set_binary_numbers,
+    app_replace_rule_set_binary_numbers_simple,
   ];
   return r2;
 }
