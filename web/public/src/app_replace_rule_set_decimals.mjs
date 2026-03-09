5cd9b7c58e7e00a5678fa04814bc6259f82e4657
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 17:00:51 -0400

    arr app_replace_rule_set_integers_2

--- web/public/src/app_replace_rule_set_decimals.mjs
+++ web/public/src/app_replace_rule_set_decimals.mjs
@@ -1,6 +1,6 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
-export function app_replace_rule_set_integers_2() {
+export function app_replace_rule_set_decimals() {
   const rules = app_replace_rule_set_integers_rules();
   list_add_multiple(rules, [
     "e > f . b",
