1503caf1a98732d638c8d04cd75ecb240c2c84c2
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 18:44:29 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_numbers.mjs

--- web/public/src/app_replace_rule_set_numbers.mjs
+++ web/public/src/app_replace_rule_set_numbers.mjs
@@ -1,6 +1,6 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
-export function app_replace_rule_set_decimals_2() {
+export function app_replace_rule_set_numbers() {
   const rules = app_replace_rule_set_integers_rules();
   list_add_multiple(rules, [
     "h > a j",
