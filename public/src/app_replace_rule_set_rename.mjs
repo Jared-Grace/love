a4f7fed41b43b04f18c2f034ae4da5a768ce522b
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:17:06 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_rename.mjs

--- public/src/app_replace_rule_set_rename.mjs
+++ public/src/app_replace_rule_set_rename.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_sets_name_expression } from "../../../love/public/src/app_replace_rule_sets_name_expression.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
@@ -5,5 +6,6 @@ export async function app_replace_rule_set_rename(f_name) {
   let ast = await function_ast(f_name);
   const type = "ObjectExpression";
   let vs = js_list_type_nodes(ast, type);
-  let first = list_first(vs);
+  let e = list_first(vs);
+  app_replace_rule_sets_name_expression(e);
 }
