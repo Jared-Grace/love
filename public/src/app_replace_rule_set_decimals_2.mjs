0e739297f7e5cb90a795f2be7abc6890f43b4714
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 18:18:33 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_2.mjs

--- public/src/app_replace_rule_set_decimals_2.mjs
+++ public/src/app_replace_rule_set_decimals_2.mjs
@@ -3,7 +3,7 @@ import { app_replace_rule_set_integers_rules } from "../../../love/public/src/ap
 export function app_replace_rule_set_decimals_2() {
   const rules = app_replace_rule_set_integers_rules();
   list_add_multiple(rules, [
-    "g > f . b",
+    "h > a i a",
     "g > f .",
     "f > a",
     "f > 0",
