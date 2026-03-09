c818c5696aaa79ec84d18be0d94bee371eaa290d
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:43:34 -0400

    function_auto_path ..\love\public\src\digits_positive.mjs

--- web/public/src/digits_positive.mjs
+++ web/public/src/digits_positive.mjs
@@ -1,7 +1,7 @@
-import { digits_text } from "../../../love/public/src/digits_text.mjs";
+import { digits_positive_text } from "../../../love/public/src/digits_positive_text.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 export function digits_positive() {
-  let d = digits_text();
+  let d = digits_positive_text();
   let split = text_split_empty(d);
   return split;
 }
