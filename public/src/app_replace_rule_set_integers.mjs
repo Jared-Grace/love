1fc1877cd111a19b9637834437a4c2990a6e6a01
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:28:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers.mjs

--- public/src/app_replace_rule_set_integers.mjs
+++ public/src/app_replace_rule_set_integers.mjs
@@ -1,7 +1,6 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_map_combine_left } from "../../../love/public/src/list_map_combine_left.mjs";
 import { range } from "../../../love/public/src/range.mjs";
-import { app_replace_rule_set_binary_numbers_goals } from "../../../love/public/src/app_replace_rule_set_binary_numbers_goals.mjs";
 export function app_replace_rule_set_integers() {
   let r2 = range(10);
   let combineds = list_map_combine_left(r2, "b > ");
@@ -10,7 +9,32 @@ export function app_replace_rule_set_integers() {
   let r = {
     name: "Integers",
     rules,
-    goals: app_replace_rule_set_binary_numbers_goals(),
+    goals: [
+      {
+        start: "a",
+        end: "2",
+      },
+      {
+        start: "a",
+        end: "3",
+      },
+      {
+        start: "a",
+        end: "54",
+      },
+      {
+        start: "a",
+        end: "76",
+      },
+      {
+        start: "a",
+        end: "890",
+      },
+      {
+        start: "a",
+        end: "1234",
+      },
+    ],
   };
   return r;
 }
