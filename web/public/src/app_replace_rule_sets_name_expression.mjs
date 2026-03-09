004b74cfeff6df4425bd9ba9c0fdd6ffe99122b7
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:19:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_name_expression.mjs

--- web/public/src/app_replace_rule_sets_name_expression.mjs
+++ web/public/src/app_replace_rule_sets_name_expression.mjs
@@ -4,7 +4,8 @@ import { js_object_expression_properties_find_key_named } from "../../../love/pu
 export function app_replace_rule_sets_name_expression(e) {
   let search = "name";
   let found = js_object_expression_properties_find_key_named(e, search);
-  if (null_is(value)) {
+  if (null_is(found)) {
+    return found;
   }
   let f_name_new = app_replace_rule_sets_name(found);
   return f_name_new;
