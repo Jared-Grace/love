fd44609ae449a8dd3a7aa533ea06bce34140c054
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:24:16 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_binary_numbers.mjs

--- public/src/app_replace_rule_set_binary_numbers.mjs
+++ public/src/app_replace_rule_set_binary_numbers.mjs
@@ -2,7 +2,7 @@ import { app_replace_rule_set_binary_numbers_goals } from "../../../love/public/
 export function app_replace_rule_set_binary_numbers() {
   let r = {
     name: "Binary numbers",
-    rules: ["a > a b", "a > b", "b > 0", "b > 1"],
+    rules: ["a > b a", "a > b", "b > 0", "b > 1"],
     goals: app_replace_rule_set_binary_numbers_goals(),
   };
   return r;
