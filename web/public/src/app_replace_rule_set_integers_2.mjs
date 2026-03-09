fc2e5aae8a7448af9682e6513f39bd7732175621
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 16:53:55 -0400

    cs app_replace_rule_set_integers 2

---
+++ web/public/src/app_replace_rule_set_integers_2.mjs
@@ -0,0 +1,39 @@
+import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
+export function app_replace_rule_set_integers_2() {
+  const rules = app_replace_rule_set_integers_rules();
+  let r = {
+    name: "Integers",
+    rules,
+    goals: [
+      {
+        start: "a",
+        end: "0",
+      },
+      {
+        start: "a",
+        end: "2",
+      },
+      {
+        start: "a",
+        end: "13",
+      },
+      {
+        start: "a",
+        end: "77",
+      },
+      {
+        start: "a",
+        end: "40",
+      },
+      {
+        start: "a",
+        end: "969",
+      },
+      {
+        start: "a",
+        end: "5678",
+      },
+    ],
+  };
+  return r;
+}
