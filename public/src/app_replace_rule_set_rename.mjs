486b72ac56a0ea7f4fb5bc502a9150b22ed3c2e0
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:25:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_rename.mjs

--- public/src/app_replace_rule_set_rename.mjs
+++ public/src/app_replace_rule_set_rename.mjs
@@ -1,5 +1,4 @@
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_replace_rule_sets_name_expression } from "../../../love/public/src/app_replace_rule_sets_name_expression.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
@@ -10,7 +9,7 @@ export async function app_replace_rule_set_rename(f_name) {
   const type = "ObjectExpression";
   let list = js_list_type_nodes(ast, type);
   let mapped = list_map(list, app_replace_rule_sets_name_expression);
-  let filtered = list_filter(mapped, null_not_is);
+  let filtered = list_filter_null_not_is(mapped);
   let e = list_first(filtered);
   let f_name_new = app_replace_rule_sets_name_expression(e);
   return f_name_new;
