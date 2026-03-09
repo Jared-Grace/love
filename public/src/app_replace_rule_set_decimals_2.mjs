de4628653cc5e2292382ea0ce444f18d2b0a1a8a
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 18:17:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_2.mjs

--- public/src/app_replace_rule_set_decimals_2.mjs
+++ public/src/app_replace_rule_set_decimals_2.mjs
@@ -3,34 +3,34 @@ import { app_replace_rule_set_integers_rules } from "../../../love/public/src/ap
 export function app_replace_rule_set_decimals_2() {
   const rules = app_replace_rule_set_integers_rules();
   list_add_multiple(rules, [
-    "e > f . b",
-    "e > f .",
+    "g > f . b",
+    "g > f .",
     "f > a",
     "f > 0",
-    "e > . b",
+    "g > . b",
   ]);
   let r = {
     name: "Numbers",
     rules,
     goals: [
       {
-        start: "e",
+        start: "g",
         end: "0.",
       },
       {
-        start: "e",
+        start: "g",
         end: ".5",
       },
       {
-        start: "e",
+        start: "g",
         end: "2.",
       },
       {
-        start: "e",
+        start: "g",
         end: "3.14",
       },
       {
-        start: "e",
+        start: "g",
         end: "77.4607",
       },
     ],
