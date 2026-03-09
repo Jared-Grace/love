999f3cfad87afd7d4b04939bf5bbf2efe0dbfe24
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 16:16:23 -0400

    rrl app_replace_rule_set_add_left rights

--- web/public/src/app_replace_rule_set_integers.mjs
+++ web/public/src/app_replace_rule_set_integers.mjs
@@ -1,9 +1,9 @@
-import { app_replace_rule_set_add_left } from "../../../love/public/src/app_replace_rule_set_add_left.mjs";
+import { app_replace_rule_set_add_rights } from "../../../love/public/src/app_replace_rule_set_add_rights.mjs";
 import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
 export function app_replace_rule_set_integers() {
   const rules = ["a > c b", "a > d", "b > d b", "d > 0", "d > c", "b > d"];
   let r2 = digits_positive();
-  app_replace_rule_set_add_left(rules, "c", r2);
+  app_replace_rule_set_add_rights(rules, "c", r2);
   let r = {
     name: "Integers",
     rules,
