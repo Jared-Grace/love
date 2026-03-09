bd6ba9a752c660dbcd65e16f9b79a4065de53087
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 16:54:17 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_2.mjs

--- public/src/app_replace_rule_set_integers_2.mjs
+++ public/src/app_replace_rule_set_integers_2.mjs
@@ -1,6 +1,8 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
 export function app_replace_rule_set_integers_2() {
   const rules = app_replace_rule_set_integers_rules();
+  list_add_multiple(list, items);
   let r = {
     name: "Decimals",
     rules,
