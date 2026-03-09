ef0fad3ed812dd54e043cf80dd981eabff78d6cf
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 18:13:58 -0400

    cs app_replace_rule_set_decimals 2

---
+++ public/src/app_replace_rule_set_decimals_2.mjs
@@ -0,0 +1,39 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
+export function app_replace_rule_set_decimals_2() {
+  const rules = app_replace_rule_set_integers_rules();
+  list_add_multiple(rules, [
+    "e > f . b",
+    "e > f .",
+    "f > a",
+    "f > 0",
+    "e > . b",
+  ]);
+  let r = {
+    name: "Decimals",
+    rules,
+    goals: [
+      {
+        start: "e",
+        end: "0.",
+      },
+      {
+        start: "e",
+        end: ".5",
+      },
+      {
+        start: "e",
+        end: "2.",
+      },
+      {
+        start: "e",
+        end: "3.14",
+      },
+      {
+        start: "e",
+        end: "77.4607",
+      },
+    ],
+  };
+  return r;
+}
