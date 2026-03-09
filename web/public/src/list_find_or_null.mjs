d8c05705eec6cda36ee8a913f3202216d4bf6dc3
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 14:04:19 -0400

    function_auto_path ..\love\public\src\list_find_or_null.mjs

--- web/public/src/list_find_or_null.mjs
+++ web/public/src/list_find_or_null.mjs
@@ -3,7 +3,7 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export function list_find_or_null(list, lambda$item) {
   let filtered = list_filter(list, lambda$item);
-  let e = list_empty_is(list2);
+  let e = list_empty_is(list);
   if (e) {
     return null;
   }
