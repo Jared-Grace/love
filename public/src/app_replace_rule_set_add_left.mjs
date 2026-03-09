7bb1e29c4d56dc88826d6064b5b7aa73f862d410
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 16:15:39 -0400

    pe app_replace_rule_set_add_left

--- public/src/app_replace_rule_set_add_left.mjs
+++ public/src/app_replace_rule_set_add_left.mjs
@@ -1,6 +1,6 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_map_combine_left } from "../../../love/public/src/list_map_combine_left.mjs";
-export function app_replace_rule_set_add_left(rules, rights, symbol) {
+export function app_replace_rule_set_add_left(rules, symbol, rights) {
   let combineds = list_map_combine_left(rights, symbol + " > ");
   list_add_multiple(rules, combineds);
 }
