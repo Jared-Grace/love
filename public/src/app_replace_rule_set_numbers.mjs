69d52b691371b86aaf181852ee74d06a9de8e4a2
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 18:47:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_numbers.mjs

--- public/src/app_replace_rule_set_numbers.mjs
+++ public/src/app_replace_rule_set_numbers.mjs
@@ -22,15 +22,15 @@ export function app_replace_rule_set_numbers() {
       },
       {
         start: "h",
-        end: "2.e+39",
+        end: "2.E+39",
       },
       {
         start: "h",
-        end: ".5E-18",
+        end: ".5e-18",
       },
       {
         start: "h",
-        end: "3.14e0",
+        end: "3.14E0",
       },
       {
         start: "h",
