e34d1a678fe99a910d61d1952ea9dd5a47855b6e
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 15:27:06 -0400

    app_replace_rule_set_rename app_replace_rule_set_binary_numbers_2

--- web/public/src/app_replace_rule_set_integers.mjs
+++ web/public/src/app_replace_rule_set_integers.mjs
@@ -2,7 +2,7 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { list_map_combine_left } from "../../../love/public/src/list_map_combine_left.mjs";
 import { range } from "../../../love/public/src/range.mjs";
 import { app_replace_rule_set_binary_numbers_goals } from "../../../love/public/src/app_replace_rule_set_binary_numbers_goals.mjs";
-export function app_replace_rule_set_binary_numbers_2() {
+export function app_replace_rule_set_integers() {
   let r2 = range(10);
   let combineds = list_map_combine_left(r2, "b > ");
   const rules = ["a > b a", "a > b"];
