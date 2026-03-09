1d7f7066017ad4fa8603157a28e0e1983e248e38
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 13:06:43 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_rename.mjs

--- public/src/app_replace_rule_set_rename.mjs
+++ public/src/app_replace_rule_set_rename.mjs
@@ -1,6 +1,6 @@
-import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
+import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
 export async function app_replace_rule_set_rename(f_name) {
   let ast = await function_ast(f_name);
-  let vs = js_list_type(ast2, node_type);
+  let vs = js_list_type_nodes(ast2, node_type);
 }
