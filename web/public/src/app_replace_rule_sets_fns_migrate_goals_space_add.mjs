94aadfbd21bcd03bd86042f56d2ac1776b6772f3
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:52:34 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -41,8 +41,8 @@ export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
     function lambda$previous(item) {
       let s = js_object_expression_properties_find_key_named(item, "start");
       let literal = js_property_value_get(s);
-      let s2 = lambda$previous(literal);
-      js_property_value_set(s, s2);
+      let value_new = lambda$previous(literal);
+      js_property_value_set(s, value_new);
       let e = js_object_expression_properties_find_key_named(item, "end");
       let value2 = js_property_value_get(e);
       log(value2);
