3c7dce796487b96dc19f536aba3060042d41f2c0
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:42:04 -0400

    function_auto_path ..\love\public\src\js_literal_map_curried_right.mjs

--- public/src/js_literal_map_curried_right.mjs
+++ public/src/js_literal_map_curried_right.mjs
@@ -1,6 +1,8 @@
 import { js_literal_map } from "../../../love/public/src/js_literal_map.mjs";
 export function js_literal_map_curried_right(lambda$value) {
-  return function js_literal_map_curried_right_result(literal) {
-    return js_literal_map(literal, lambda$value);
+  let r = function js_literal_map_curried_right_result(literal) {
+    let value_after = js_literal_map(literal, lambda$value);
+    return value_after;
   };
+  return r;
 }
