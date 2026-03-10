3cfd2d674db6df18dff7f1873723b658739bccc3
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 15:20:06 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_rules.mjs

--- public/src/app_replace_rule_set_integers_rules.mjs
+++ public/src/app_replace_rule_set_integers_rules.mjs
@@ -1,7 +1,14 @@
 import { app_replace_rule_set_add_rights } from "../../../love/public/src/app_replace_rule_set_add_rights.mjs";
 import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
 export function app_replace_rule_set_integers_rules(digit_name) {
-  const rules = ["i > p g", "i > d", "g > d g", "d > 0", "d > p", "g > d"];
+  const rules = [
+    "i > p g",
+    "i > " + digit_name,
+    "g > d g",
+    digit_name + " > 0",
+    digit_name + " > p",
+    "g > " + digit_name,
+  ];
   let p = digits_positive();
   app_replace_rule_set_add_rights(rules, "p", p);
   return rules;
