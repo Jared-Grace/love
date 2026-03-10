b00b4e4c2bd743ce0de5400b76173f0178925f16
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 15:05:16 -0400

    function_auto_path ..\love\public\src\function_string_split_space_change.mjs

--- web/public/src/app_replace_rule_set_integers_rules.mjs
+++ web/public/src/app_replace_rule_set_integers_rules.mjs
@@ -1,7 +1,14 @@
 import { app_replace_rule_set_add_rights } from "../../../love/public/src/app_replace_rule_set_add_rights.mjs";
 import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
 export function app_replace_rule_set_integers_rules() {
-  const rules = ["a > dp b", "a > d", "b > d b", "d > 0", "d > dp", "b > d"];
+  const rules = [
+    "i > dp dg",
+    "i > d",
+    "dg > d dg",
+    "d > 0",
+    "d > dp",
+    "dg > d",
+  ];
   let p = digits_positive();
   app_replace_rule_set_add_rights(rules, "dp", p);
   return rules;
