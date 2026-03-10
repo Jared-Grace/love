403f141f62b04dcc1bd42c4f04b5f9b604296932
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:57:01 -0400

    function_auto_path ..\love\public\src\function_dependencies.mjs

--- web/public/src/function_dependencies.mjs
+++ web/public/src/function_dependencies.mjs
@@ -1,5 +1,4 @@
 import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_exists_assert } from "../../../love/public/src/function_exists_assert.mjs";
 import { list_adder_unique_async } from "../../../love/public/src/list_adder_unique_async.mjs";
@@ -18,7 +17,7 @@ export async function function_dependencies(f_names) {
       }
       await visit_unique_async(item, function_imports, lambda);
     }
-    await each_async(f_names, lambda3);
+    await each_unordered_async(f_names, lambda3);
   }
   let v2 = await list_adder_unique_async(lambda2);
   return v2;
