f318f44a400d790b7aa3b0e7f01419ca62286d98
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:05:39 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_names.mjs

--- public/src/app_replace_rule_sets_names.mjs
+++ public/src/app_replace_rule_sets_names.mjs
@@ -1,7 +1,7 @@
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 export function app_replace_rule_sets_names() {
-  let mapped = app_replace_rule_sets();
-  let mapped2 = list_map_property(mapped, "name");
-  return mapped2;
+  let rs = app_replace_rule_sets();
+  let names = list_map_property(rs, "name");
+  return names;
 }
