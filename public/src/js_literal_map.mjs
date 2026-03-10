7510a92b61108e1421458b82cd6918b2d5fedad9
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:41:42 -0400

    function_auto_path ..\love\public\src\js_literal_map.mjs

--- public/src/js_literal_map.mjs
+++ public/src/js_literal_map.mjs
@@ -2,7 +2,7 @@ import { js_string } from "../../../love/public/src/js_string.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 export function js_literal_map(literal, lambda$value) {
   let value = js_literal_value_get(literal);
-  let joined = lambda$value(value);
-  let mapped = js_string(joined);
-  return mapped;
+  let mapped = lambda$value(value);
+  let value_after = js_string(mapped);
+  return value_after;
 }
