f100eb66cb97761384eda42db067395b2b6d0c8f
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:09:50 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,11 +1,11 @@
+import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { function_name_separator_trail } from "../../../love/public/src/function_name_separator_trail.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
-import { app_replace_rule_sets_names } from "../../../love/public/src/app_replace_rule_sets_names.mjs";
 export async function sandbox() {
-  let names = app_replace_rule_sets_names();
+  let names = app_replace_rule_sets_fns();
   let f_names = await functions_names();
   let difference = list_difference(f_names, names);
   let prefix = function_name_separator_trail(app_replace_rule_set.name);
