7862b8449236d03624c2c16e1253113d58a974db
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:33:42 -0400

    function_auto_path ..\love\public\src\function_string_split_space_change.mjs

--- web/public/src/function_string_split_space_change.mjs
+++ web/public/src/function_string_split_space_change.mjs
@@ -1,1 +1,4 @@
-export function function_string_split_space_change(f_name, from, to) {}
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+export async function function_string_split_space_change(f_name, from, to) {
+  let output = await function_transform(f_name2, async function lambda(ast) {});
+}
