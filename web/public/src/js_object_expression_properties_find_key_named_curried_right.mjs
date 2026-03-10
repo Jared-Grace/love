98be5abaa490484d476e97c994c5abd3bf18443f
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:32:13 -0400

    cur js_object_expression_properties_find_key_named

---
+++ web/public/src/js_object_expression_properties_find_key_named_curried_right.mjs
@@ -0,0 +1,10 @@
+import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
+export function js_object_expression_properties_find_key_named_curried_right(
+  search,
+) {
+  return function js_object_expression_properties_find_key_named_curried_right_result(
+    e,
+  ) {
+    return js_object_expression_properties_find_key_named(e, search);
+  };
+}
