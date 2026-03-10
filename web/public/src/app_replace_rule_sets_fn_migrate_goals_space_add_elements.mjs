73553f653cc7b984ebb68dc9707f668ac015b4c2
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:39:25 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fn_migrate_goals_space_add_elements.mjs

--- web/public/src/app_replace_rule_sets_fn_migrate_goals_space_add_elements.mjs
+++ web/public/src/app_replace_rule_sets_fn_migrate_goals_space_add_elements.mjs
@@ -1,6 +1,8 @@
-import { js_literal_value_change } from "../../../love/public/src/js_literal_value_change.mjs";
 import { each } from "../../../love/public/src/each.mjs";
+import { js_property_value_change } from "../../../love/public/src/js_property_value_change.mjs";
+import { js_string } from "../../../love/public/src/js_string.mjs";
 import { text_between_space } from "../../../love/public/src/text_between_space.mjs";
+import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 export function app_replace_rule_sets_fn_migrate_goals_space_add_elements(
   elements,
@@ -9,7 +11,13 @@ export function app_replace_rule_sets_fn_migrate_goals_space_add_elements(
     let ps = ["start", "end"];
     function lambda2(p) {
       let s = js_object_expression_properties_find_key_named(item, p);
-      js_literal_value_change(s, text_between_space);
+      function lambda$previous(literal) {
+        let value3 = js_literal_value_get(literal);
+        let joined = text_between_space(value3);
+        let s2 = js_string(joined);
+        return s2;
+      }
+      js_property_value_change(s, lambda$previous);
     }
     each(ps, lambda2);
   }
