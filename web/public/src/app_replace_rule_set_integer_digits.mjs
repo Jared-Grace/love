c6263f0f23094ab162e061c9c117fd39a374221b
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:49:33 -0400

    p

--- web/public/src/app_replace_rule_set_integer_digits.mjs
+++ web/public/src/app_replace_rule_set_integer_digits.mjs
@@ -2,8 +2,8 @@ import { app_replace_rule_set_add_rights } from "../../../love/public/src/app_re
 import { digits } from "../../../love/public/src/digits.mjs";
 export function app_replace_rule_set_integer_digits() {
   let d = digits();
-  const rules = ["a > b a", "a > b"];
-  app_replace_rule_set_add_rights(rules, "b", d);
+  const rules = ["a > id a", "a > id"];
+  app_replace_rule_set_add_rights(rules, "id", d);
   let r = {
     name: "Integer digits",
     rules,
