d028916d1e0f435597fe6924a76634d3407b58d7
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 15:16:09 -0400

    function_string_split_space_change app_replace_rule_set_grow_right,app_replace_rule_set_grow_left a,g,b,s

--- web/public/src/app_replace_rule_set_grow_left.mjs
+++ web/public/src/app_replace_rule_set_grow_left.mjs
@@ -1,23 +1,23 @@
 export function app_replace_rule_set_grow_left() {
   let r = {
     name: "Grow left",
-    rules: ["a > b a"],
+    rules: ["g > s g"],
     goals: [
       {
-        start: "a",
-        end: "b b a",
+        start: "g",
+        end: "s s g",
       },
       {
-        start: "a a",
-        end: "b a b b a",
+        start: "g g",
+        end: "s g s s g",
       },
       {
-        start: "a a",
-        end: "b b b a b b a",
+        start: "g g",
+        end: "s s s g s s g",
       },
       {
-        start: "a a a",
-        end: "b b b a b b a b a",
+        start: "g g g",
+        end: "s s s g s s g s g",
       },
     ],
   };
