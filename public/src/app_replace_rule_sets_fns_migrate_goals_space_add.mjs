568a83653f6b224151ff69a9d8677a0f769274eb
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:32:34 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -1,6 +1,5 @@
+import { js_object_expression_properties_find_key_named_curried_right } from "../../../love/public/src/js_object_expression_properties_find_key_named_curried_right.mjs";
 import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
-import { app_replace_rule_sets_name_expression } from "../../../love/public/src/app_replace_rule_sets_name_expression.mjs";
-import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
@@ -15,8 +14,8 @@ export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
   let name = "app_replace_rule_set_numbers";
   let list = await function_ast_list_type_nodes_object_expression(name);
   let search = "goals";
-  let found = js_object_expression_properties_find_key_named(e, search);
-  let mapped2 = list_map(list, app_replace_rule_sets_name_expression);
+  let m = js_object_expression_properties_find_key_named_curried_right(search);
+  let mapped2 = list_map(list, m);
   let filtered = list_filter_null_not_is(mapped2);
   return filtered;
 }
