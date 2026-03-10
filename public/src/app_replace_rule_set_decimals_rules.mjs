06b48515a95314cdfc96dd499586b02b39ccb499
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 15:26:54 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_rules.mjs

--- public/src/app_replace_rule_set_decimals_rules.mjs
+++ public/src/app_replace_rule_set_decimals_rules.mjs
@@ -2,6 +2,6 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
 export function app_replace_rule_set_decimals_rules() {
   const rules = app_replace_rule_set_integers_rules("di");
-  list_add_multiple(rules, ["de > f . g", "de > f .", "f > i", "de > . g"]);
+  list_add_multiple(rules, ["de > i . g", "de > i .", "de > . g"]);
   return rules;
 }
