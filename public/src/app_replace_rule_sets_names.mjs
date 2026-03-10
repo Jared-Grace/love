daebb1d8308b57059568a25671233b425f4a38d7
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 23:19:42 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_names.mjs

--- public/src/app_replace_rule_sets_names.mjs
+++ public/src/app_replace_rule_sets_names.mjs
@@ -1,4 +1,5 @@
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 export function app_replace_rule_sets_names() {
-  return app_replace_rule_sets();
+  let mapped = app_replace_rule_sets();
+  return mapped;
 }
