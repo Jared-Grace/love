8cd5036ff931ea402bed580d82732f4397b1b71a
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:59:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -1,4 +1,4 @@
-import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
+import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { js_property_value_change } from "../../../love/public/src/js_property_value_change.mjs";
 import { js_string } from "../../../love/public/src/js_string.mjs";
 import { text_between_space } from "../../../love/public/src/text_between_space.mjs";
@@ -25,7 +25,7 @@ export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
   let elements = js_array_expression_single_elements(ast);
   let mapped = list_map(elements, js_identifier_name);
   let name = "app_replace_rule_set_numbers";
-  let waited = await list_map_unordered_async(
+  let waited = await each_unordered_async(
     mapped,
     function_ast_list_type_nodes_object_expression,
   );
