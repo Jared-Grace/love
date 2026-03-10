5143085037a32f994e6721dcc0283cb5f1c9337a
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 21:01:28 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -49,10 +49,10 @@ export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
           let value3 = js_literal_value_get(literal);
           let joined = text_between_space(value3);
           let s2 = js_string(joined);
+          return s2;
         }
       }
       each(elements, lambda);
-      log_unparse(only);
     }
   }
   let waited = await each_unordered_async(mapped, lambda2);
