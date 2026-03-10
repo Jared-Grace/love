8fc2f6fec98deb8a172b8c1ef11085064ae29301
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:06:11 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,6 @@
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { app_replace_rule_sets_names } from "../../../love/public/src/app_replace_rule_sets_names.mjs";
 export async function sandbox() {
-  let names = app_replace_rule_sets_names();
+  let names = app_replace_rule_sets_names();fns
   let filtered = list_filter_starts_with(list, prefix);
 }
