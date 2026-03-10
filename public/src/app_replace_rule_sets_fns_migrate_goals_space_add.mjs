dfe242ae442201b9b0d7a0bd4c917813c5c67b9c
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:28:14 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -1,3 +1,4 @@
+import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
@@ -7,8 +8,7 @@ export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
   let f_name = app_replace_rule_sets_fns.name;
   let ast = await function_ast(f_name);
   let elements = js_array_expression_single_elements(ast);
-  function lambda(item) {}
-  let mapped = list_map(list2, lambda);
-  return elements;
+  let mapped = list_map(elements, js_identifier_name);
+  return mapped;
   let list = await function_ast_list_type_nodes_object_expression(f_name);
 }
