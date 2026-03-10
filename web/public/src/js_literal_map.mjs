eaac2b56779817e6f3f6f1dc3135f6ef32bba21a
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:41:08 -0400

    function_auto_path ..\love\public\src\js_literal_map.mjs

--- web/public/src/js_literal_map.mjs
+++ web/public/src/js_literal_map.mjs
@@ -1,8 +1,8 @@
 import { js_string } from "../../../love/public/src/js_string.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
-export function js_literal_map(literal, fn) {
-  let value3 = js_literal_value_get(literal);
-  let joined = fn(value3);
+export function js_literal_map(literal, lambda$value) {
+  let value = js_literal_value_get(literal);
+  let joined = lambda$value(value);
   let s2 = js_string(joined);
   return s2;
 }
