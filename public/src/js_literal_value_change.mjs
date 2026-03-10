14bc22661440d416ed1a8d28d7286cd404078f64
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:38:29 -0400

    pb js_literal_value_change

--- public/src/js_literal_value_change.mjs
+++ public/src/js_literal_value_change.mjs
@@ -1,7 +1,7 @@
 import { js_property_value_change } from "../../../love/public/src/js_property_value_change.mjs";
 import { js_string } from "../../../love/public/src/js_string.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
-export function js_literal_value_change(lambda, s) {
+export function js_literal_value_change(s, lambda) {
   function lambda$previous(literal) {
     let value3 = js_literal_value_get(literal);
     let joined = lambda(value3);
