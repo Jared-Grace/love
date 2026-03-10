4d898551397c21310d212c5c650199996ec8d0d3
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:07:39 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,10 @@
+import { function_name_separator_trail } from "../../../love/public/src/function_name_separator_trail.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { app_replace_rule_sets_names } from "../../../love/public/src/app_replace_rule_sets_names.mjs";
 export async function sandbox() {
   let names = app_replace_rule_sets_names();
-  let f_names = await functions_names();function_name_separator_trail
+  let f_names = await functions_names();
+  let c = function_name_separator_trail(a_name);
   let filtered = list_filter_starts_with(list, prefix);
 }
