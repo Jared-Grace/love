f42fa6bd1a2490b3f01f5b31a1e219e06e10a6fd
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:04:19 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,1 @@
-import { sermon_translate_david } from "../../../love/public/src/sermon_translate_david.mjs";
-export async function sandbox() {
-  let joined = await sermon_translate_david();
-  return joined;
-}
+export async function sandbox() {}
