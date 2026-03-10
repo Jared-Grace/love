674721def8f53a7d767d2d7f9328a6a69391ebbb
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:07:49 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { function_name_separator_trail } from "../../../love/public/src/function_name_separator_trail.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
@@ -5,6 +6,6 @@ import { app_replace_rule_sets_names } from "../../../love/public/src/app_replac
 export async function sandbox() {
   let names = app_replace_rule_sets_names();
   let f_names = await functions_names();
-  let c = function_name_separator_trail(a_name);
+  let c = function_name_separator_trail(app_replace_rule_set.name);
   let filtered = list_filter_starts_with(list, prefix);
 }
