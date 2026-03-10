f76b1dc60288ade412a5723c3afaa76a1fa01e75
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 21:56:10 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

---
+++ public/src/app_replace_rule_sets_fn_migrate_goals_space_add.mjs
@@ -0,0 +1,54 @@
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+import { js_string } from "../../../love/public/src/js_string.mjs";
+import { text_between_space } from "../../../love/public/src/text_between_space.mjs";
+import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
+import { js_property_value_change } from "../../../love/public/src/js_property_value_change.mjs";
+import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
+import { js_array_expression_elements } from "../../../love/public/src/js_array_expression_elements.mjs";
+import { js_property_value_get } from "../../../love/public/src/js_property_value_get.mjs";
+import { list_single } from "../../../love/public/src/list_single.mjs";
+import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
+import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { js_object_expression_properties_find_key_named_curried_right } from "../../../love/public/src/js_object_expression_properties_find_key_named_curried_right.mjs";
+import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
+import { log } from "../../../love/public/src/log.mjs";
+export async function app_replace_rule_sets_fn_migrate_goals_space_add(name) {
+  async function lambda3(ast) {
+    log({
+      name,
+    });
+    const type = "ObjectExpression";
+    let list = js_list_type_nodes(ast, type);
+    let search = "goals";
+    let m =
+      js_object_expression_properties_find_key_named_curried_right(search);
+    let mapped2 = list_map(list, m);
+    let filtered = list_filter_null_not_is(mapped2);
+    let e = list_empty_is(filtered);
+    if (e) {
+      log({
+        name,
+      });
+    } else {
+      let only = list_single(filtered);
+      let value = js_property_value_get(only);
+      let elements = js_array_expression_elements(value);
+      function lambda(item) {
+        let s = js_object_expression_properties_find_key_named(item, "start");
+        js_property_value_change(s, lambda$previous);
+        let e = js_object_expression_properties_find_key_named(item, "end");
+        js_property_value_change(e, lambda$previous);
+        function lambda$previous(literal) {
+          let value3 = js_literal_value_get(literal);
+          let joined = text_between_space(value3);
+          let s2 = js_string(joined);
+          return s2;
+        }
+      }
+      each(elements, lambda);
+    }
+  }
+  let output = await function_transform(name, lambda3);
+}
