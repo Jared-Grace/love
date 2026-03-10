b4d188fe195c5bd36e5c13080f73640ca9f0981e
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:27:47 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -4,7 +4,8 @@ import { function_ast_list_type_nodes_object_expression } from "../../../love/pu
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
   let f_name = app_replace_rule_sets_fns.name;
-  let ast2 = await function_ast(f_name2);
+  let ast = await function_ast(f_name);
   let elements = js_array_expression_single_elements(ast);
+  return elements;
   let list = await function_ast_list_type_nodes_object_expression(f_name);
 }
