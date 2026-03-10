954e6190f6cc1495d58002b57196b04263f300a1
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:51:27 -0400

    function_auto_path ..\love\public\src\js_property_value_set.mjs

--- public/src/js_property_value_set.mjs
+++ public/src/js_property_value_set.mjs
@@ -1,7 +1,6 @@
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { js_property_is_assert } from "../../../love/public/src/js_property_is_assert.mjs";
-export function js_property_value_set(found) {
+export function js_property_value_set(found, value) {
   js_property_is_assert(found);
-  let value = property_set(found, "value");
-  return value;
+  property_set(found, "value", value);
 }
