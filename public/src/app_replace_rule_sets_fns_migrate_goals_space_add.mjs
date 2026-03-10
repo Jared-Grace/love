654884cc2e2b5d8710d4290cae704ba1aee71904
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 21:01:20 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_goals_space_add.mjs

--- public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -24,7 +24,6 @@ export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
   let ast = await function_ast(f_name);
   let elements = js_array_expression_single_elements(ast);
   let mapped = list_map(elements, js_identifier_name);
-  let name = "app_replace_rule_set_numbers";
   async function lambda2(name) {
     let list = await function_ast_list_type_nodes_object_expression(name);
     let search = "goals";
