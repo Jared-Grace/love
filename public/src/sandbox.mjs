f38cec3ad60e0b8dd241a3e9ea3f43bb866bedde
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 17:05:15 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,3 @@
-import { app_replace_rule_sets_names } from "../../../love/public/src/app_replace_rule_sets_names.mjs";
 export async function sandbox() {
-  let mapped2 = app_replace_rule_sets_names();
+  let mapped2 = app_replace_rule_sets_naames();
 }
