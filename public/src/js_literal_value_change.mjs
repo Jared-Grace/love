84a8540bb0b851e1cd789651d580e07220b16446
Grace-Jared <christrosetolife@gmail.com>
2026-03-10 14:38:40 -0400

    function_auto_path ..\love\public\src\js_literal_value_change.mjs

--- public/src/js_literal_value_change.mjs
+++ public/src/js_literal_value_change.mjs
@@ -3,8 +3,8 @@ import { js_string } from "../../../love/public/src/js_string.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 export function js_literal_value_change(s, lambda) {
   function lambda$previous(literal) {
-    let value3 = js_literal_value_get(literal);
-    let joined = lambda(value3);
+    let value = js_literal_value_get(literal);
+    let joined = lambda(value);
     let s2 = js_string(joined);
     return s2;
   }
