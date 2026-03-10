2e31e282d140b9635efc962df1e54e1ccd4c766b
Grace-Jared <christrosetolife@gmail.com>
2026-03-09 20:48:12 -0400

    rr text_between_space between combine

--- web/public/src/text_combine_space.mjs
+++ web/public/src/text_combine_space.mjs
@@ -1,5 +1,5 @@
 import { text_combine_3 } from "../../../love/public/src/text_combine_3.mjs";
-export function text_between_space(index, item) {
+export function text_combine_space(index, item) {
   const combine = " ";
   let r = text_combine_3(index, combine, item);
   return r;
