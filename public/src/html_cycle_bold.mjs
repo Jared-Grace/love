11b8493518c990c5e67d789c746a8b78cb59db2e
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:03:57 -0400

    function_auto_path ..\love\public\src\html_cycle_bold.mjs

--- public/src/html_cycle_bold.mjs
+++ public/src/html_cycle_bold.mjs
@@ -1,1 +1,5 @@
-export function html_cycle_bold(parent, parts) {}
+import { noop } from "../../../love/public/src/noop.mjs";
+import { html_bold } from "./html_bold.mjs";
+export function html_cycle_bold(parent, parts) {
+  let cycle = [noop, html_bold];
+}
