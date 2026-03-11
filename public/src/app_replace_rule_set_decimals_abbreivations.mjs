421a34d8daf56ae823aa886e840ef121dc0b550d
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:59:57 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_abbreivations.mjs

--- public/src/app_replace_rule_set_decimals_abbreivations.mjs
+++ public/src/app_replace_rule_set_decimals_abbreivations.mjs
@@ -1,10 +1,12 @@
 export function app_replace_rule_set_decimals_abbreivations() {
-  let a = {
-    de: ["", "de", "cimal number"],
+  let i = {
     di: ["", "di", "git (numbers 0-9)"],
     g: ["integer ", "g", "rower"],
     i: ["", "i", "nteger (non-negative whole number)"],
     p: ["", "p", "ositive digit (numbers 1-9)"],
+  };
+  let a = {
+    de: ["", "de", "cimal number"],
   };
   return a;
 }
