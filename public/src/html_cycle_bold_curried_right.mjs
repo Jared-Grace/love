d276504a66e8ed7b59b40ab95290d9d1561d2bc7
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:07:25 -0400

    cur html_cycle_bold

---
+++ public/src/html_cycle_bold_curried_right.mjs
@@ -0,0 +1,6 @@
+import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
+export function html_cycle_bold_curried_right(parts) {
+  return function html_cycle_bold_curried_right_result(parent) {
+    return html_cycle_bold(parent, parts);
+  };
+}
