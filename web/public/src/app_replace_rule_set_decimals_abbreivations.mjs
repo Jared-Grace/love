003c9173ec0f1e22201d794c94a0a9b4daf1e774
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:50:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_scientific_notation_numbers.mjs

---
+++ web/public/src/app_replace_rule_set_decimals_abbreivations.mjs
@@ -0,0 +1,10 @@
+export function app_replace_rule_set_decimals_abbreivations() {
+  let r2 = {
+    de: ["", "de", "cimal number"],
+    di: ["", "di", "git (numbers 0-9)"],
+    g: ["integer ", "g", "rower"],
+    i: ["", "i", "nteger (non-negative whole number)"],
+    p: ["", "p", "ositive digit (numbers 1-9)"],
+  };
+  return r2;
+}
