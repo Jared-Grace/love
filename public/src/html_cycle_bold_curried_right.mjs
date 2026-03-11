23ade59757748283c1a40eed9d4aa11f1b4ab127
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:07:30 -0400

    function_auto_path ..\love\public\src\html_cycle_bold_curried_right.mjs

--- public/src/html_cycle_bold_curried_right.mjs
+++ public/src/html_cycle_bold_curried_right.mjs
@@ -1,6 +1,8 @@
 import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
 export function html_cycle_bold_curried_right(parts) {
-  return function html_cycle_bold_curried_right_result(parent) {
-    return html_cycle_bold(parent, parts);
+  let r2 = function html_cycle_bold_curried_right_result(parent) {
+    let r = html_cycle_bold(parent, parts);
+    return r;
   };
+  return r2;
 }
