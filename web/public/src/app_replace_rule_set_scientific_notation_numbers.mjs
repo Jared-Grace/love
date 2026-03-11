d986c4cf5597f3edd65636c9aa0ae16d8d9530f6
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:11:22 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_scientific_notation_numbers.mjs

--- web/public/src/app_replace_rule_set_scientific_notation_numbers.mjs
+++ web/public/src/app_replace_rule_set_scientific_notation_numbers.mjs
@@ -15,6 +15,7 @@ export function app_replace_rule_set_scientific_notation_numbers() {
   let a = {
     eE: ['lowercase or uppercase letter "', "e", '" for "', "e", 'xponent"'],
     i: ["", "i", "nteger (non-negative whole number)"],
+    p: ["", "p", "ositive digit"],
     se: ["", "s", "cientific notation number", "e", "nding"],
     sn: ["", "s", "cientific ", "n", "otation number"],
   };
