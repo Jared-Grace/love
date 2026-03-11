0d9ea45dac50c707f0972e9fb82950282e402223
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:05:06 -0400

    function_auto_path ..\love\public\src\html_cycle_bold.mjs

--- public/src/html_cycle_bold.mjs
+++ public/src/html_cycle_bold.mjs
@@ -1,3 +1,4 @@
+import { mod } from "../../../love/public/src/mod.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
@@ -5,6 +6,8 @@ import { html_bold } from "./html_bold.mjs";
 export function html_cycle_bold(parent, parts) {
   let cycles = [noop, html_bold];
   let size = list_size(cycles);
-  function lambda(part) {}
+  function lambda(part) {
+    let r = mod(n, m);
+  }
   each(parts, lambda);
 }
