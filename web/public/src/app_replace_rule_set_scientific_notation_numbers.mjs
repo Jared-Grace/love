1fedc3caa34b0ea1395a69ca1bfc83bc45760477
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 19:12:09 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_scientific_notation_numbers.mjs

--- web/public/src/app_replace_rule_set_scientific_notation_numbers.mjs
+++ web/public/src/app_replace_rule_set_scientific_notation_numbers.mjs
@@ -13,7 +13,7 @@ export function app_replace_rule_set_scientific_notation_numbers() {
     "ex > i",
   ]);
   let a = {
-    di: ["", "di", "git (0-9)"],
+    di: ["", "di", "git (numbers 0-9)"],
     eE: ['lowercase or uppercase letter "', "e", '" for "', "e", 'xponent"'],
     i: ["", "i", "nteger (non-negative whole number)"],
     p: ["", "p", "ositive digit"],
