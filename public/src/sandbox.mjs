665265ba5f410e2bb156dfacc4a67c5c97fe922d
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:14:55 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,4 @@
-import { app_replace_rule_sets_fns_names_others } from "../../../love/public/src/app_replace_rule_sets_fns_names_others.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
-  let filtered = await app_replace_rule_sets_fns_names_others();
-  return filtered;
   let joined = folder_previous_join(path);
 }
