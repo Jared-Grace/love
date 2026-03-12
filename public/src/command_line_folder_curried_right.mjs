3029ffb8289ff4b0dca66e74522ec4502a5f2709
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:57:21 -0400

    function_auto_path ..\love\public\src\command_line_folder_curried_right.mjs

--- public/src/command_line_folder_curried_right.mjs
+++ public/src/command_line_folder_curried_right.mjs
@@ -1,6 +1,8 @@
 import { command_line_folder } from "../../../love/public/src/command_line_folder.mjs";
 export async function command_line_folder_curried_right(folder) {
-  return async function command_line_folder_curried_right_result(cmd) {
-    return await command_line_folder(cmd, folder);
+  let r2 = async function command_line_folder_curried_right_result(cmd) {
+    let r = await command_line_folder(cmd, folder);
+    return r;
   };
+  return r2;
 }
