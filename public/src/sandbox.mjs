a5b96720ad4af7f834b0ff44d7e4123d58de78da
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:24:07 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,8 +3,9 @@ import { command_line } from "../../../love/public/src/command_line.mjs";
 import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
+  const dir = "data";
   let stdout2 = await command_line_generic("dir", {
-    cwd: "data",
+    cwd: dir,
   });
   return stdout2;
   let stdout = await command_line("cd data && dir");
