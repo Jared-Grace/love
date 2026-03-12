ca023b6177a440a00c7c958b2db4231fef9547db
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 20:01:05 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers.mjs

--- public/src/app_replace_rule_set_integers.mjs
+++ public/src/app_replace_rule_set_integers.mjs
@@ -1,8 +1,11 @@
+import { app_replace_rule_set_integers_abbreivations } from "../../../love/public/src/app_replace_rule_set_integers_abbreivations.mjs";
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
 export function app_replace_rule_set_integers() {
   const rules = app_replace_rule_set_integers_rules("d");
+  let abbreviations = app_replace_rule_set_integers_abbreivations();
   let r = {
     name: "Integers",
+    abbreviations,
     rules,
     goals: [
       {
