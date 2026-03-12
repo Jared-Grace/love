58f7adc896457bf2de14547c582c23a21f432086
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 12:16:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviations.mjs

--- public/src/app_replace_rule_set_integers_abbreviations.mjs
+++ public/src/app_replace_rule_set_integers_abbreviations.mjs
@@ -5,6 +5,7 @@ export function app_replace_rule_set_integers_abbreviations() {
   let r = {
     di: ["", "di", "git (numbers 0-9)"],
     i: ["", "i", "nteger (non-negative whole number)"],
+    p: ["", "p", "ositive digit (numbers 1-9)"],
   };
   let to2 = object_merge(r, i);
   return r;
