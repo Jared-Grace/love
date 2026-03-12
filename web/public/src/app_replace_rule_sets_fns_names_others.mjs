dec0e96938325beb67aeb0beaad85f4f62258778
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:13:15 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ web/public/src/app_replace_rule_sets_fns_names_others.mjs
@@ -0,0 +1,14 @@
+import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
+import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
+import { function_name_separator_trail } from "../../../love/public/src/function_name_separator_trail.mjs";
+import { list_difference } from "../../../love/public/src/list_difference.mjs";
+import { functions_names } from "../../../love/public/src/functions_names.mjs";
+import { app_replace_rule_sets_fns_names } from "../../../love/public/src/app_replace_rule_sets_fns_names.mjs";
+export async function app_replace_rule_sets_fns_names_others() {
+  let mapped = app_replace_rule_sets_fns_names();
+  let f_names = await functions_names();
+  let difference = list_difference(f_names, mapped);
+  let prefix = function_name_separator_trail(app_replace_rule_set.name);
+  let filtered = list_filter_starts_with(difference, prefix);
+  return filtered;
+}
