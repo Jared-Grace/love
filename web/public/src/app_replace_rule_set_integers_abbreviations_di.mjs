1f06f73023a13ff8746eb128f960444265106c64
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 12:18:42 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviations_di.mjs

--- web/public/src/app_replace_rule_set_integers_abbreviations_di.mjs
+++ web/public/src/app_replace_rule_set_integers_abbreviations_di.mjs
@@ -1,9 +1,6 @@
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function app_replace_rule_set_integers_abbreviations_di(i) {
-  object_merge(
-    {
-      di: ["", "di", "git (numbers 0-9)"],
-    },
-    i,
-  );
+  object_merge(i, {
+    di: ["", "di", "git (numbers 0-9)"],
+  });
 }
