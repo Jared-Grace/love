b27cdfc552a42fd75bc26024d068194d1ebb7364
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:13:15 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,16 +1,7 @@
+import { app_replace_rule_sets_fns_names_others } from "../../../love/public/src/app_replace_rule_sets_fns_names_others.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
-import { app_replace_rule_sets_fns_names } from "../../../love/public/src/app_replace_rule_sets_fns_names.mjs";
-import { list_difference } from "../../../love/public/src/list_difference.mjs";
-import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
-import { function_name_separator_trail } from "../../../love/public/src/function_name_separator_trail.mjs";
-import { functions_names } from "../../../love/public/src/functions_names.mjs";
-import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 export async function sandbox() {
-  let mapped = app_replace_rule_sets_fns_names();
-  let f_names = await functions_names();
-  let difference = list_difference(f_names, mapped);
-  let prefix = function_name_separator_trail(app_replace_rule_set.name);
-  let filtered = list_filter_starts_with(difference, prefix);
+  let filtered = await app_replace_rule_sets_fns_names_others();
   return filtered;
   let joined = folder_previous_join(path);
 }
