9219d22140198d1464d012132a57c6feee41fe7d
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 20:03:53 -0400

    pd app_replace_rule_set_integers_rules digit_name

--- public/src/app_replace_rule_set_decimals_rules.mjs
+++ public/src/app_replace_rule_set_decimals_rules.mjs
@@ -1,7 +1,7 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_integers_rules } from "../../../love/public/src/app_replace_rule_set_integers_rules.mjs";
 export function app_replace_rule_set_decimals_rules() {
-  const rules = app_replace_rule_set_integers_rules("di");
+  const rules = app_replace_rule_set_integers_rules();
   list_add_multiple(rules, ["de > i . g", "de > i .", "de > . g"]);
   return rules;
 }
