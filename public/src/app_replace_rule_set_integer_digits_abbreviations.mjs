4d3d9b679872d02c2214f87d8f5407a760a63590
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 12:16:23 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integer_digits_abbreviations.mjs

--- public/src/app_replace_rule_set_integer_digits_abbreviations.mjs
+++ public/src/app_replace_rule_set_integer_digits_abbreviations.mjs
@@ -1,7 +1,6 @@
 export function app_replace_rule_set_integer_digits_abbreviations() {
   let r2 = {
     g: ["integer ", "g", "rower"],
-    p: ["", "p", "ositive digit (numbers 1-9)"],
   };
   return r2;
 }
