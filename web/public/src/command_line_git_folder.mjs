0bd0018ab5d4b86b47ecd80b83d9c0716d699dad
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 17:23:31 -0400

    function_auto_path ..\love\public\src\command_line_git_folder.mjs

--- web/public/src/command_line_git_folder.mjs
+++ web/public/src/command_line_git_folder.mjs
@@ -1,7 +1,6 @@
-import { command_line_git_prefix } from "../../../love/public/src/command_line_git_prefix.mjs";
+import { command_line_git_folder_text } from "../../../love/public/src/command_line_git_folder_text.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function command_line_git_folder(folder, command_git) {
-  await command_line(
-    command_line_git_prefix() + "-C " + folder + " " + command_git,
-  );
+  let command = command_line_git_folder_text(folder, command_git);
+  await command_line(command);
 }
