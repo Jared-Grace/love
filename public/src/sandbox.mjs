3e7ccef652d9a12c74846a20e993d66f0eeede3a
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:21:38 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,9 @@
+import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
+  let stdout2 = await command_line_generic(command, extra);
   let stdout = await command_line("cd data && dir");
   return stdout;
   let repo_name = "portfolio_qa";
