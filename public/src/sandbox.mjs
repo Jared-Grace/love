1feb113789e1962fe54b9f6f071ac7817bba3173
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 17:03:40 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,12 +1,10 @@
 import { repos_gitignore_overwrite_list } from "../../../love/public/src/repos_gitignore_overwrite_list.mjs";
 import { command_line_folder_curried_right } from "../../../love/public/src/command_line_folder_curried_right.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
   let repo_name = "portfolio_qa";
   let joined = folder_previous_join(repo_name);
-  await folder_exists_ensure(joined);
   await repos_gitignore_overwrite_list([repo_name]);
   let cmds = [
     "git init",
