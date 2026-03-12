1755282265bea07de7eea6e0ee13eac43a82c7e0
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 19:03:23 -0400

    function_auto_path ..\love\public\src\playwright_browsers_install.mjs

--- public/src/playwright_browsers_install.mjs
+++ public/src/playwright_browsers_install.mjs
@@ -1,5 +1,6 @@
 import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function playwright_browsers_install() {
   let c = "npx playwright install";
-  let stdout = await command_line(command);
+  let stdout = await command_line(c);
+  return stdout;
 }
