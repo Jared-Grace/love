e3d082461326ec66d17caf9b399cb5c5ae562c14
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:31:20 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,5 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { command_line_folder } from "../../../love/public/src/command_line_folder.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
 import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
@@ -9,10 +9,9 @@ export async function sandbox() {
     "git init",
     "gh repo create my-project --public --source=. --remote=origin --push",
   ];
+  await each_async(list, async function lambda(item) {});
   let stdout2 = await command_line_folder(cmd, folder);
   return stdout2;
-  let stdout = await command_line("cd data && dir");
-  return stdout;
   let repo_name = "portfolio_qa";
   let joined = folder_previous_join(repo_name);
   let path = await folder_exists_ensure(joined);
