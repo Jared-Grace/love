4b3de363e3963a0a3546d8f54ec626802cc56245
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 17:25:25 -0400

    function_auto_path ..\love\public\src\git_init_folder.mjs

--- public/src/git_init_folder.mjs
+++ public/src/git_init_folder.mjs
@@ -1,5 +1,5 @@
 import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
-export async function git_init(folder) {
+export async function git_init_folder(folder) {
   let r = await command_line_git_folder(folder, "init");
   return r;
 }
