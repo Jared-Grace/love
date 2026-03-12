95cea9b5f5f53125349771d5812d859927cb9475
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:32:26 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,7 +5,7 @@ import { folder_previous_join } from "../../../love/public/src/folder_previous_j
 export async function sandbox() {
   let repo_name = "portfolio_qa";
   let joined = folder_previous_join(repo_name);
-  let path = await folder_exists_ensure(joined);
+  await folder_exists_ensure(joined);
   const folder = "data";
   const cmd = "dir";
   let cmds = [
