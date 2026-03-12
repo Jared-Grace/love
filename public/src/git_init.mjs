18af464b87a7640f8032aae325e768a647a824ba
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 17:24:13 -0400

    fw command_line_git_folder git_init

--- public/src/git_init.mjs
+++ public/src/git_init.mjs
@@ -1,1 +1,4 @@
-export function git_init() {}
+import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
+export async function git_init(folder, command_git) {
+  return await command_line_git_folder(folder, command_git);
+}
