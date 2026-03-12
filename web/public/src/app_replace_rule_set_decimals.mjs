5fa668e5267e36e49b924749f285ae1e2d9c3648
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 20:02:08 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_abbreviations.mjs

--- web/public/src/app_replace_rule_set_decimals.mjs
+++ web/public/src/app_replace_rule_set_decimals.mjs
@@ -1,8 +1,8 @@
-import { app_replace_rule_set_decimals_abbreivations } from "../../../love/public/src/app_replace_rule_set_decimals_abbreivations.mjs";
+import { app_replace_rule_set_decimals_abbreviations } from "../../../love/public/src/app_replace_rule_set_decimals_abbreviations.mjs";
 import { app_replace_rule_set_decimals_rules } from "../../../love/public/src/app_replace_rule_set_decimals_rules.mjs";
 export function app_replace_rule_set_decimals() {
   const rules = app_replace_rule_set_decimals_rules();
-  let abbreviations = app_replace_rule_set_decimals_abbreivations();
+  let abbreviations = app_replace_rule_set_decimals_abbreviations();
   let r = {
     name: "Decimals",
     abbreviations,
--- web/public/src/app_replace_rule_set_decimals_abbreviations.mjs
+++ web/public/src/app_replace_rule_set_decimals_abbreviations.mjs
@@ -1,6 +1,6 @@
 import { app_replace_rule_set_integers_abbreivations } from "../../../love/public/src/app_replace_rule_set_integers_abbreivations.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-export function app_replace_rule_set_decimals_abbreivations() {
+export function app_replace_rule_set_decimals_abbreviations() {
   let i = app_replace_rule_set_integers_abbreivations();
   let a = {
     de: ["", "de", "cimal number"],
