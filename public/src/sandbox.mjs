8d566c73668db8e29fa0e26c74d6c00d07e504b2
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:25:22 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,13 +1,11 @@
-import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
+import { command_line_folder } from "../../../love/public/src/command_line_folder.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
   const folder = "data";
   const cmd = "dir";
-  let stdout2 = await command_line_generic(cmd, {
-    cwd: folder,
-  });
+  let stdout2 = await command_line_folder(cmd, folder);
   return stdout2;
   let stdout = await command_line("cd data && dir");
   return stdout;
