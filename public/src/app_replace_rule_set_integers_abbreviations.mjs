b62009ab7bc97d737d4500e51014454d7eddb330
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 12:17:22 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviations.mjs

--- public/src/app_replace_rule_set_integers_abbreviations.mjs
+++ public/src/app_replace_rule_set_integers_abbreviations.mjs
@@ -7,6 +7,6 @@ export function app_replace_rule_set_integers_abbreviations() {
     i: ["", "i", "nteger (non-negative whole number)"],
     p: ["", "p", "ositive digit (numbers 1-9)"],
   };
-  let to2 = object_merge(r, i);
+  object_merge(r, i);
   return r;
 }
