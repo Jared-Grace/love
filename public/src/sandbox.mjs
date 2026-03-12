9571fb825fef50d2bfe93d4ed6f4a7c30f09b2b6
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 16:15:58 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,8 @@
+import { folder_exists_ensure } from "../../../love/public/src/folder_exists_ensure.mjs";
 import { folder_previous_join } from "../../../love/public/src/folder_previous_join.mjs";
 export async function sandbox() {
   let repo_name = "portfolio_qa";
   let joined = folder_previous_join(repo_name);
+  let path = await folder_exists_ensure(dir);
   return joined;
 }
