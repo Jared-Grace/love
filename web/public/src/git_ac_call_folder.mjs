3c79d1f45ebe5689908541f95e4000ab8cf1c97f
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 17:52:02 -0400

    function_auto_path ..\love\public\src\git_ac_call_folder.mjs

--- web/public/src/git_ac_call_folder.mjs
+++ web/public/src/git_ac_call_folder.mjs
@@ -1,6 +1,6 @@
+import { git_ac_folder } from "../../../love/public/src/git_ac_folder.mjs";
 import { git_call_message } from "../../../love/public/src/git_call_message.mjs";
-import { git_acp_folder } from "../../../love/public/src/git_acp_folder.mjs";
 export async function git_ac_call_folder(f_name, args, folder) {
   let message = git_call_message(f_name, args);
-  await git_acp_folder(folder, message);
+  await git_ac_folder(folder, message);
 }
