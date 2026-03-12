1083b8ce768a1ccadc6e1a3568113f4b15f94391
Grace-Jared <christrosetolife@gmail.com>
2026-03-11 20:05:34 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviations.mjs

--- web/public/src/app_replace_rule_set_integers_abbreviations.mjs
+++ web/public/src/app_replace_rule_set_integers_abbreviations.mjs
@@ -1,4 +1,8 @@
 export function app_replace_rule_set_integers_abbreviations() {
+  let i = {
+    g: ["integer ", "g", "rower"],
+    p: ["", "p", "ositive digit (numbers 1-9)"],
+  };
   let r = {
     di: ["", "di", "git (numbers 0-9)"],
     g: ["integer ", "g", "rower"],
