81d752fc4a622f4e54b722214057f27616017fc9
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 15:05:33 -0400

    o log_keep

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -5,8 +5,7 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
 import { js_parse_statement_module } from "../../../love/public/src/js_parse_statement_module.mjs";
 export async function function_imports_add(ast, imports) {
-  let r = await function_name_to_path_import_code_multiple();
-  let mapped = list_map(imports, r);
+  let mapped = await function_name_to_path_import_code_multiple_map(imports);
   let body = property_get(ast, "body");
   async function lambda(code) {
     let statement = js_parse_statement_module(code);
