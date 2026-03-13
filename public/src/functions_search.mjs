e3a93f159dd3b0beb9123083f19347cf80587612
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 13:55:41 -0400

    function_auto_path ..\love\public\src\functions_search.mjs

--- public/src/functions_search.mjs
+++ public/src/functions_search.mjs
@@ -1,8 +1,10 @@
+import { repos_paths_map_unordered_combine_squash } from "../../../love/public/src/repos_paths_map_unordered_combine_squash.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { functions_search_generic } from "../../../love/public/src/functions_search_generic.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
 export async function functions_search(search) {
   arguments_assert(arguments, 1);
+  let squashed = await repos_paths_map_unordered_combine_squash(mapper);
   let fn = text_includes;
   let result = await functions_search_generic(search, fn);
   return result;
