e0c7b04d0700c82ed2f980b31913b19a19494059
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 16:06:55 -0400

    function_auto_path ..\love\public\src\ebible_text_to_speech_book.mjs

--- public/src/ebible_version_symbols_unique.mjs
+++ public/src/ebible_version_symbols_unique.mjs
@@ -7,10 +7,13 @@ export async function ebible_version_symbols_unique(bible_folder) {
   let unique = await text_symbols_unique_adder(lambda$la);
   async function lambda$la(la) {
     async function lambda2(chapter_code, verses) {
-      log({
-        bible_folder,
-        chapter_code,
-      });
+      log(
+        {
+          bible_folder,
+          chapter_code,
+        },
+        "TODO",
+      );
       let mapped = list_map_property(verses, "text");
       let unique_chapter = text_symbols_unique_multiple(mapped);
       la(unique_chapter);
