e8aa6bfdf9789c43a9cc19d22ee9418ab116e160
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 15:10:14 -0400

    function_auto_path ..\love\public\src\js_parse_generic.mjs

--- public/src/git_rebase.mjs
+++ public/src/git_rebase.mjs
@@ -4,11 +4,11 @@ export async function git_rebase() {
   try {
     await command_line_git("rebase --no-stat --no-verify origin/main");
   } catch (e) {
-    log_keep("Rebase failed, aborting rebase", "TODO");
+    log_keep("TODO", "Rebase failed, aborting rebase");
     try {
       await command_line_git("rebase --abort");
     } catch (abortErr) {
-      log_keep("No rebase in progress, nothing to abort.", "TODO");
+      log_keep("TODO", "No rebase in progress, nothing to abort.");
     }
     throw e;
   }
--- public/src/log.mjs
+++ public/src/log.mjs
@@ -1,5 +1,5 @@
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export function log(message) {
-  log_keep(message, "TODO");
+  log_keep("TODO", message);
   let a = 1;
 }
