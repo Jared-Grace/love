9d2f1875b7ee5b3a196cd57c4a709a59506dc039
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 14:00:17 -0400

    function_auto_path ..\love\public\src\apps_paths.mjs

--- public/src/apps_paths.mjs
+++ public/src/apps_paths.mjs
@@ -1,8 +1,13 @@
+import { repos_paths_map_unordered_combine } from "../../../love/public/src/repos_paths_map_unordered_combine.mjs";
 import { html_extension } from "../../../love/public/src/html_extension.mjs";
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
 export async function apps_paths() {
+  let result = await repos_paths_map_unordered_combine(
+    path,
+    async function lambda(path2) {},
+  );
   let fop = folder_public();
   let files = await folder_read_files(fop);
   let sufix = html_extension();
