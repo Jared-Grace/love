d6c260c0776e47baa94cc55c86835965e40bf252
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 18:11:41 -0400

    pb html_name_to_path_folder

--- public/src/html_name_to_path_dev.mjs
+++ public/src/html_name_to_path_dev.mjs
@@ -3,6 +3,6 @@ import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 import { html_name_to_path_folder } from "../../../love/public/src/html_name_to_path_folder.mjs";
 export function html_name_to_path_dev(name) {
   const folder = app_shared_name_dev_text();
-  let file_path = html_name_to_path_folder(name, folder, file_name_html);
+  let file_path = html_name_to_path_folder(folder, name, file_name_html);
   return file_path;
 }
