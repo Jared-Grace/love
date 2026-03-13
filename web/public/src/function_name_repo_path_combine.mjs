b59407f09a5e4ccdeb2569585b9237596f0c6d59
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 15:10:03 -0400

    function_auto_path ..\love\public\src\command_line_log.mjs

--- web/public/src/function_name_repo_path_combine.mjs
+++ web/public/src/function_name_repo_path_combine.mjs
@@ -4,9 +4,12 @@ import { function_name_to_path_search } from "./function_name_to_path_search.mjs
 import { log_keep } from "./log_keep.mjs";
 export async function function_name_repo_path_combine(f_name, file_path) {
   let search = await function_name_to_path_search(f_name);
-  log_keep({
-    search,
-  });
+  log_keep(
+    {
+      search,
+    },
+    "TODO",
+  );
   let repo_name = property_get(search, "repo_name");
   let joined = repo_path_combine(repo_name, file_path);
   return joined;
--- web/public/src/log_unparse.mjs
+++ web/public/src/log_unparse.mjs
@@ -2,7 +2,10 @@ import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { log_keep } from "./log_keep.mjs";
 export function log_unparse(node) {
   let code = js_unparse(node);
-  log_keep({
-    [log_unparse.name]: code,
-  });
+  log_keep(
+    {
+      [log_unparse.name]: code,
+    },
+    "TODO",
+  );
 }
