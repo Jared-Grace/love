e92650377b0a9a864a6dabd736496da32527ced7
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 16:06:50 -0400

    function_auto_path ..\love\public\src\log.mjs

--- public/src/log.mjs
+++ public/src/log.mjs
@@ -1,5 +1,5 @@
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-export function log(message) {
+export function log(message, f_name) {
   log_keep(log.name, message);
   let a = 1;
 }
