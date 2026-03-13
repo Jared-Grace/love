220cf537216556d47e37913be94ad80d8bf4382d
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 16:07:27 -0400

    function_auto_path ..\love\public\src\file_copy_overwrite_from_properties.mjs

--- public/src/invoke.mjs
+++ public/src/invoke.mjs
@@ -1,11 +1,8 @@
 import { log } from "../../../love/public/src/log.mjs";
 export function invoke(lambda) {
   let r = lambda();
-  log(
-    {
-      lambda,
-    },
-    "TODO",
-  );
+  log("TODO", {
+    lambda,
+  });
   return r;
 }
