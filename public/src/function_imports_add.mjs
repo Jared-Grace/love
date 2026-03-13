65461b0d986786ae6402215ca75873bf6e8494ac
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 14:51:13 -0400

    function_auto_path ..\love\public\src\function_imports_add.mjs

--- public/src/function_imports_add.mjs
+++ public/src/function_imports_add.mjs
@@ -1,5 +1,4 @@
 import { function_name_to_path_import_code_curried_right } from "../../../karate_code/public/src/function_name_to_path_import_code_curried_right.mjs";
-import { function_name_to_path_import_code } from "../../../karate_code/public/src/function_name_to_path_import_code.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { functions_names_to_paths } from "../../../love/public/src/functions_names_to_paths.mjs";
@@ -8,11 +7,8 @@ import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
 import { js_parse_statement_module } from "../../../love/public/src/js_parse_statement_module.mjs";
 export async function function_imports_add(ast, imports) {
   let dictionary = await functions_names_to_paths();
-  function lambda2(import_) {
-    let code = function_name_to_path_import_code(import_, dictionary);
-    return code;
-  }
-  let mapped = list_map(imports, lambda2);
+  let r = function_name_to_path_import_code_curried_right(dictionary);
+  let mapped = list_map(imports, r);
   let body = property_get(ast, "body");
   async function lambda(code) {
     let statement = js_parse_statement_module(code);
@@ -20,5 +16,4 @@ export async function function_imports_add(ast, imports) {
   }
   await each_async(mapped, lambda);
   return;
-  let r = function_name_to_path_import_code_curried_right(dictionary2);
 }
