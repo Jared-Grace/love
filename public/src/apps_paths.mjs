d28810cf1a9a7298b4988c76999c4a9d38725691
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 14:02:52 -0400

    function_auto_path ..\karate_code\public\src\repos_paths_map_unordered_combine_squash_functions.mjs

--- public/src/apps_paths.mjs
+++ public/src/apps_paths.mjs
@@ -2,10 +2,10 @@ import { html_extension } from "../../../love/public/src/html_extension.mjs";
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
-import { repos_paths_map_unordered_combine_squash_functions } from "../../../karate_code/public/src/repos_paths_map_unordered_combine_squash_functions.mjs";
+import { repos_paths_map_unordered_combine_squash } from "../../../karate_code/public/src/repos_paths_map_unordered_combine_squash.mjs";
 export async function apps_paths() {
   let fop = folder_public();
-  let result = await repos_paths_map_unordered_combine_squash_functions(
+  let result = await repos_paths_map_unordered_combine_squash(
     fop,
     folder_read_files,
   );
