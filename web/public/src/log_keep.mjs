4709b6027b3947f28ce1bd40c1f4fe9181ca8d5a
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 15:11:16 -0400

    function_auto_path ..\love\public\src\log_keep.mjs

--- web/public/src/log_keep.mjs
+++ web/public/src/log_keep.mjs
@@ -1,8 +1,5 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export function log_keep(f_name, message) {
-  console.log(message);
-  return;
-  function lambda(item) {}
-  each(list, lambda);
+  each([f_name, message], console.log);
 }
