e97e401d0eeff5862fc66eceec1cb87654146e31
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 14:54:23 -0400

    function_auto_path ..\love\public\src\html_update_dev.mjs

--- public/src/html_update_dev.mjs
+++ public/src/html_update_dev.mjs
@@ -1,5 +1,5 @@
+import { function_name_to_path_import_code_multiple } from "../../../karate_code/public/src/function_name_to_path_import_code_multiple.mjs";
 import { function_name_to_path_import_code } from "../../../karate_code/public/src/function_name_to_path_import_code.mjs";
-import { functions_names_to_paths } from "../../../love/public/src/functions_names_to_paths.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 import { js_code_call_app_context_initialize } from "../../../love/public/src/js_code_call_app_context_initialize.mjs";
 import { list_join_newline } from "../../../love/public/src/list_join_newline.mjs";
@@ -24,7 +24,7 @@ export async function html_update_dev(name) {
   const name_prefixed = app_shared_name_main(name);
   let call = js_code_call_app_context_initialize(name_prefixed);
   let code = function_code_import_dev(name_prefixed);
-  let dictionary = await functions_names_to_paths();
+  let r = await function_name_to_path_import_code_multiple();
   let code2 = function_name_to_path_import_code(app_context_initialize.name);
   let middle = list_join_newline([code, code2, call]);
   let body = html_code_script_module(middle);
