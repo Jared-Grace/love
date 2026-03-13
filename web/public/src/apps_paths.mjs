ae4cff39cb4bc74be6d9c59c59ec44df70cfd7de
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 14:00:47 -0400

    function_auto_path ..\love\public\src\apps_paths.mjs

--- web/public/src/apps_paths.mjs
+++ web/public/src/apps_paths.mjs
@@ -1,12 +1,15 @@
-import { repos_paths_map_unordered_combine } from "../../../love/public/src/repos_paths_map_unordered_combine.mjs";
 import { html_extension } from "../../../love/public/src/html_extension.mjs";
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
 import { folder_public } from "../../../love/public/src/folder_public.mjs";
 import { folder_read_files } from "../../../love/public/src/folder_read_files.mjs";
+import { repos_paths_map_unordered_combine_squash } from "./repos_paths_map_unordered_combine_squash.mjs";
 export async function apps_paths() {
   async function lambda(path2) {}
-  let result = await repos_paths_map_unordered_combine(path, lambda);
   let fop = folder_public();
+  let result = await repos_paths_map_unordered_combine_squash(
+    fop,
+    folder_read_files,
+  );
   let files = await folder_read_files(fop);
   let sufix = html_extension();
   let aps = list_filter_ends_with(files, sufix);
