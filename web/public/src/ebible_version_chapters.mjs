298a6959ab437d8c7b14bf04095ef84e99698a52
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 16:06:55 -0400

    function_auto_path ..\love\public\src\ebible_text_to_speech_book.mjs

--- web/public/src/ebible_version_chapters.mjs
+++ web/public/src/ebible_version_chapters.mjs
@@ -3,10 +3,13 @@ import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs"
 import { ebible_chapters_each_verses } from "../../../love/public/src/ebible_chapters_each_verses.mjs";
 import { ebible_version_download } from "../../../love/public/src/ebible_version_download.mjs";
 export async function ebible_version_chapters(bible_folder) {
-  log({
-    t: ebible_version_chapters,
-    bible_folder,
-  });
+  log(
+    {
+      t: ebible_version_chapters,
+      bible_folder,
+    },
+    "TODO",
+  );
   await ebible_version_download(bible_folder);
   async function lambda(la) {
     await ebible_chapters_each_verses(bible_folder, each_chapter);
