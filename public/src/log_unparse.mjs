c241c268a493cdee13279173616424a24c602e09
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 15:10:17 -0400

    function_auto_path ..\love\public\src\git_rebase.mjs

--- public/src/log_unparse.mjs
+++ public/src/log_unparse.mjs
@@ -2,10 +2,7 @@ import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { log_keep } from "./log_keep.mjs";
 export function log_unparse(node) {
   let code = js_unparse(node);
-  log_keep(
-    {
-      [log_unparse.name]: code,
-    },
-    "TODO",
-  );
+  log_keep("TODO", {
+    [log_unparse.name]: code,
+  });
 }
