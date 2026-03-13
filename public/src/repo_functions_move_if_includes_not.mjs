23c1e384d78f18170cd1ff17b039b0773fbef14a
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 13:20:56 -0400

    repo_functions_move_all q l

--- public/src/repo_functions_move_if_includes_not.mjs
+++ public/src/repo_functions_move_if_includes_not.mjs
@@ -1,4 +1,4 @@
-import { repo_functions_move_acronym } from "../../../portfolio_qa/public/src/repo_functions_move_acronym.mjs";
+import { repo_functions_move_acronym } from "../../../love/public/src/repo_functions_move_acronym.mjs";
 import { repo_functions_move_if_includes_not_filtered } from "../../../love/public/src/repo_functions_move_if_includes_not_filtered.mjs";
 export async function repo_functions_move_if_includes_not(to, query) {
   let filtered = await repo_functions_move_if_includes_not_filtered(query);
