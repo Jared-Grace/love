e67e75af96636b214146bb8568c47c2537b8c1aa
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 15:47:58 -0400

    function_auto_path ..\love\public\src\catch_call.mjs

--- public/src/catch_call.mjs
+++ public/src/catch_call.mjs
@@ -3,6 +3,9 @@ export function catch_call(lambda$e, lambda) {
   let i = function inner() {
     let result = null;
     try {
+      log(catch_call.name, {
+        a: "a",
+      });
       result = lambda(...arguments);
     } catch (e) {
       log(catch_call.name, {
