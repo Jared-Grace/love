389aa6650294966263825e9c47218a05f952cd41
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 14:29:46 -0400

    function_auto_path ..\love\public\src\functions_search_generic.mjs

--- public/src/functions_search_generic.mjs
+++ public/src/functions_search_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
 import { functions_names_paths } from "../../../karate_code/public/src/functions_names_paths.mjs";
@@ -6,6 +7,7 @@ export async function functions_search_generic(search, fn) {
   let r = await functions_names_paths();
   let r2 = property_get_curried(r);
   let properties = properties_get(r);
+  log(functions_search_generic.name, message);
   let result2 = search_generic(search, properties, r2, fn);
   return result2;
   ("$L$properties");
