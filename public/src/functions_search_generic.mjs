7689dbfcdd99f572f839649efba1146d7e14c1d8
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 14:22:52 -0400

    function_auto_path ..\love\public\src\functions_search_generic.mjs

--- public/src/functions_search_generic.mjs
+++ public/src/functions_search_generic.mjs
@@ -1,3 +1,4 @@
+import { functions_names_paths } from "../../../karate_code/public/src/functions_names_paths.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { search_generic } from "../../../love/public/src/search_generic.mjs";
@@ -5,4 +6,5 @@ export async function functions_search_generic(search, fn) {
   let f_names = await functions_names();
   let result2 = search_generic(search, f_names, function_name_to_path, fn);
   return result2;
+  let r = await functions_names_paths();
 }
