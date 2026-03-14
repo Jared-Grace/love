4818c10228133a4421bc3681b19e1d6499512194
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 17:31:51 -0400

    function_auto_path ..\love\public\src\function_rename.mjs

--- public/src/function_rename.mjs
+++ public/src/function_rename.mjs
@@ -1,7 +1,9 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_rename_inner } from "../../../love/public/src/function_rename_inner.mjs";
 import { function_rename_check } from "../../../love/public/src/function_rename_check.mjs";
 import { equal_not_assert } from "../../../love/public/src/equal_not_assert.mjs";
 export async function function_rename(f_name_before, f_name_after) {
+  arguments_assert(arguments, 2);
   equal_not_assert(f_name_before, f_name_after);
   await function_rename_check(f_name_after);
   f_name_before = await function_rename_inner(f_name_before, f_name_after);
