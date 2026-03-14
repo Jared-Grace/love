280f6053515e642c96c200892db18a700a272bc4
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 14:19:47 -0400

    function_auto_path ..\love\public\src\functions_names.mjs

--- public/src/functions_names.mjs
+++ public/src/functions_names.mjs
@@ -1,3 +1,4 @@
+import { repo_path } from "../../../love/public/src/repo_path.mjs";
 import { repos_paths_map_unordered_combine_squash_functions } from "../../../karate_code/public/src/repos_paths_map_unordered_combine_squash_functions.mjs";
 import { functions_names_from_path } from "../../../love/public/src/functions_names_from_path.mjs";
 export async function functions_names() {
@@ -5,5 +6,5 @@ export async function functions_names() {
     functions_names_from_path,
   );
   return f_names;
-  (function lambda() {});
+  (function lambda(repo_path) {});
 }
