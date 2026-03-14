8c4a5324ae07a9489f04d5db8e06cf64808c4900
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 14:45:43 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,11 +1,7 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { repos_paths_map_unordered_combine_squash_functions } from "../../../karate_code/public/src/repos_paths_map_unordered_combine_squash_functions.mjs";
 import { identity } from "./identity.mjs";
 export async function sandbox() {
   let f_names =
     await repos_paths_map_unordered_combine_squash_functions(identity);
   return f_names;
-  log(sandbox.name, {
-    f_names,
-  });
 }
