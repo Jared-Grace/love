28f0276cd1ec1cea81dce38557672d9558d40324
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 14:14:21 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,7 @@
-export async function sandbox() {}
+import { repos_paths_map_unordered_combine_squash_functions } from "../../../karate_code/public/src/repos_paths_map_unordered_combine_squash_functions.mjs";
+import { identity } from "./identity.mjs";
+export async function sandbox() {
+  let f_names =
+    await repos_paths_map_unordered_combine_squash_functions(identity);
+  return f_names;
+}
