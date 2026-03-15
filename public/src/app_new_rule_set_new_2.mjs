bb1a0ded73e0113eb44419ccab4c34a7f4b4a6c6
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:57:20 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -9,10 +9,16 @@ export function app_new_rule_set_new_2() {
       "idg > idp idg",
       "idf > A",
       "idf > B",
-      "idf > B",
+      "idf > l",
+      "idf > u",
+      "idf > v",
       "idp > 1",
     ],
     goals: [
+      {
+        start: "id",
+        end: "luv",
+      },
       {
         start: "id",
         end: "ABBA",
