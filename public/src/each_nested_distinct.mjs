c86216da858ffc88907477fc1ff0e8ae7f4e49b0
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 19:50:11 -0400

    function_auto_path ..\love\public\src\each_nested_distinct.mjs

--- public/src/each_nested_distinct.mjs
+++ public/src/each_nested_distinct.mjs
@@ -1,10 +1,10 @@
 import { each } from "../../../love/public/src/each.mjs";
-export function each_nested_distinct(lambda3, r, rules) {
+export function each_nested_distinct(lambda3, list_b, list_a) {
   function lambda(rule) {
     function lambda2(index) {
       lambda3(rule, index);
     }
-    each(r, lambda2);
+    each(list_b, lambda2);
   }
-  each(rules, lambda);
+  each(list_a, lambda);
 }
