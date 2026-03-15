1ca1594b51a61416dedc83a684a3cde5e87f674d
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 18:20:51 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- web/public/src/app_replace_rule_set_verify.mjs
+++ web/public/src/app_replace_rule_set_verify.mjs
@@ -7,7 +7,7 @@ export function app_replace_rule_set_verify() {
   let rs = app_replace_rule_set_identifiers_simple;
   let rules = property_get(rs, "rules");
   let goals = property_get(rs, "goals");
-  let first = list_first(list);
+  let first = list_first(goals);
   let eq = app_replace_rule_valid(rule, index, start);
   let start3 = app_replace_rule_apply(rule, start, index);
 }
