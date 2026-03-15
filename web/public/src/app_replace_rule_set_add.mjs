b103d5c789126d47a147b2f0e20650fe386f935e
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:14:24 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_add.mjs

--- web/public/src/app_replace_rule_set_add.mjs
+++ web/public/src/app_replace_rule_set_add.mjs
@@ -1,10 +1,11 @@
+import { app_new_rule_set_new } from "../../../love/public/src/app_new_rule_set_new.mjs";
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 export async function app_replace_rule_set_add(name) {
   let prefix = "app_replace_rule_set_";
   let combined = text_combine(prefix, name);
-  let name2 = await function_copy_open(f_name_old, f_name_new);
+  let name2 = await function_copy_open(app_new_rule_set_new.name, combined);
   async function lambda(ast) {}
   let output = await function_new_transform(combined, lambda);
 }
