a1f748cac5c561e26c8ff22f7766a33fc30083cb
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:47:43 -0400

    function_auto_path ..\love\public\src\text_alphabet.mjs

--- web/public/src/text_alphabet.mjs
+++ web/public/src/text_alphabet.mjs
@@ -1,6 +1,8 @@
+import { text_alphabet_lower } from "../../../love/public/src/text_alphabet_lower.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { text_alphabet_upper } from "../../../love/public/src/text_alphabet_upper.mjs";
 export function text_alphabet() {
+  let r = text_alphabet_lower();
   let u = text_alphabet_upper();
   let combined = text_combine(left, right);
   return u;
