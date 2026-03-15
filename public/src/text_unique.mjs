a9e24a54c2944a15c52e9c2ae355dba45b8790fa
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:35:57 -0400

    pn text_unique prefix "

--- public/src/text_unique.mjs
+++ public/src/text_unique.mjs
@@ -3,7 +3,7 @@ import { text_empty_is } from "../../../love/public/src/text_empty_is.mjs";
 import { text_empty_not_is } from "../../../love/public/src/text_empty_not_is.mjs";
 import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { list_all } from "../../../love/public/src/list_all.mjs";
-export function text_unique(used, name) {
+export function text_unique(used, name, prefix) {
   list_all(used, text_is_assert);
   let unique = null;
   let attempt = 1;
