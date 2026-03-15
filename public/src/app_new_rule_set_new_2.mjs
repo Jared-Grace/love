2661be5ed0b6e803674cb2c71975a9b6c7035c1e
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:53:35 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -3,7 +3,14 @@ export function app_new_rule_set_new_2() {
   let combined = text_alphabet();
   let r = {
     name: "Identifier",
-    rules: ["id > idf", "id > idf idp", "idp > a", "idp > b", "idp > c"],
+    rules: [
+      "id > idf",
+      "id > idf idg",
+      "idg > idp, idg",
+      "idf > a",
+      "idf > b",
+      "idf > c",
+    ],
     goals: [
       {
         start: "id",
