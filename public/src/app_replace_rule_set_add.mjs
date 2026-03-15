cdeedad01bde21b0d37fae44f8b47b8c7237891e
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:11:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_add.mjs

--- public/src/app_replace_rule_set_add.mjs
+++ public/src/app_replace_rule_set_add.mjs
@@ -1,5 +1,8 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 export async function app_replace_rule_set_add(name) {
-  let prefix = "app_replace_rule_set_";tc
+  let prefix = "app_replace_rule_set_";
+  let combined = text_combine(left, right);
   async function lambda(ast) {}
   let output = await function_new_transform(f_name, lambda);
+}
