2db6ae161da311433738ebc0bd289bcaa19531e6
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 18:34:14 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -15,8 +15,8 @@ export function app_replace_rule_set_verify() {
   let se = app_replace_start_end_get(g);
   let start = property_get(se, "start");
   let end = property_get(se, "end");
-  let size = list_size(list);
-  let r = range(count);
+  let size = list_size(start);
+  let r = range(size);
   function lambda(rule) {
     function lambda2(item2) {}
     each(list2, lambda2);
