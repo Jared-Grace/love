692b4e875f7d598982ab6695c79b442e039e8e29
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:35:07 -0400

    s playwright_by_attribute_exists

--- public/src/playwright_by_attribute.mjs
+++ public/src/playwright_by_attribute.mjs
@@ -1,5 +1,9 @@
 export function playwright_by_attribute(page, name, value) {
   const inside = name + '="' + value + '"';
-  let r = page.locator("[" + inside + "]");
+  let r = playwright_by_attribute_exists(page, inside);
   return r;
 }
+function playwright_by_attribute_exists(page, inside) {
+  return page.locator("[" + inside + "]");
+}
+
