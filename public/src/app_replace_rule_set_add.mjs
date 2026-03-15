11decf142891b0c9a183523e288d494fea8270f1
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:11:50 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_add.mjs

--- public/src/app_replace_rule_set_add.mjs
+++ public/src/app_replace_rule_set_add.mjs
@@ -1,5 +1,6 @@
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 export async function app_replace_rule_set_add(name) {
+  let prefix = "app_replace_rule_set_";
   async function lambda(ast) {}
   let output = await function_new_transform(f_name, lambda);
 }
