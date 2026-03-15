c8572b2609ca0ffce1eacd1e6344d2205254a64a
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:46:53 -0400

    ps

--- public/src/text_alphabet_upper.mjs
+++ public/src/text_alphabet_upper.mjs
@@ -2,6 +2,6 @@ import { text_upper_to } from "../../../love/public/src/text_upper_to.mjs";
 import { text_alphabet_lower } from "../../../love/public/src/text_alphabet_lower.mjs";
 export function text_alphabet_upper() {
   let r = text_alphabet_lower();
-  let u = text_upper_to(s);
-  return r;
+  let u = text_upper_to(r);
+  return u;
 }
