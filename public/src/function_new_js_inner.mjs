e4b2ac83af469934a1a725338959b45f8da208dd
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:36:58 -0400

    function_auto_path ..\love\public\src\function_new_js_inner.mjs

--- public/src/function_new_js_inner.mjs
+++ public/src/function_new_js_inner.mjs
@@ -3,8 +3,8 @@ import { js_flo_params_add } from "../../../love/public/src/js_flo_params_add.mj
 import { function_new } from "../../../love/public/src/function_new.mjs";
 export async function function_new_js_inner(combined) {
   await function_new(combined);
+  let result = await function_transform(combined, lambda);
   async function lambda(ast) {
     js_flo_params_add(ast, ["ast"]);
   }
-  let result = await function_transform(combined, lambda);
 }
