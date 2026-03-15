9226e3e1d3e29fd0609bdd049a67b216a81ca8a6
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:26:30 -0400

    function_auto_path ..\love\public\src\js_identifier_unique.mjs

---
+++ web/public/src/text_unique.mjs
@@ -0,0 +1,16 @@
+import { list_includes } from "../../../love/public/src/list_includes.mjs";
+import { text_empty_is } from "../../../love/public/src/text_empty_is.mjs";
+import { text_empty_not_is } from "../../../love/public/src/text_empty_not_is.mjs";
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
+import { list_all } from "../../../love/public/src/list_all.mjs";
+export function text_unique(used, name) {
+  list_all(used, text_is_assert);
+  let unique = null;
+  let attempt = 1;
+  do {
+    const suffix = attempt === 1 && text_empty_not_is(name) ? "" : attempt;
+    unique = name + suffix;
+    attempt++;
+  } while (text_empty_is(unique) || list_includes(used, unique));
+  return unique;
+}
