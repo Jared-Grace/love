66fbc386cab0ff19b1f96fa5a20026c7dc9058a6
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 17:27:16 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_add.mjs

--- web/public/src/app_replace_rule_set_add.mjs
+++ web/public/src/app_replace_rule_set_add.mjs
@@ -1,3 +1,4 @@
+import { text_unique } from "../../../love/public/src/text_unique.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
@@ -5,11 +6,10 @@ import { js_array_expression_single_elements } from "../../../love/public/src/js
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { app_new_rule_set_new } from "../../../love/public/src/app_new_rule_set_new.mjs";
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export async function app_replace_rule_set_add(name) {
-  let prefix = "app_replace_rule_set_";
-  let combined = text_combine(prefix, name);
-  await function_copy_open(app_new_rule_set_new.name, combined);
+  const f = app_new_rule_set_new.name;
+  let unique = text_unique(used, f);
+  await function_copy_open(f, combined);
   let expression = js_parse_expression(combined);
   async function lambda(ast) {
     let elements = js_array_expression_single_elements(ast);
