3aa873379ace3a05e3154902d6f080503ff1bdc9
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 22:30:49 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,10 +1,7 @@
 import { log } from "../../../love/public/src/log.mjs";
 import nearley from "nearley";
 export async function sandbox() {
-  let v2 = `sn -> i se
-i -> di g
-di -> "0"
-di -> "1"`.split("\n");
+  let v2 = ["sn -> i se", "i -> di g", 'di -> "0"', 'di -> "1"'];
   let v = nearley.Grammar.fromCompiled(v2);
   const parser = new nearley.Parser(v);
   parser.feed("0.e1");
