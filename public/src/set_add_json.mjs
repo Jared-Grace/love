1fe280dfafe786fbd7bc405f018223be1b09d3fb
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 20:10:45 -0400

    function_auto_path ..\love\public\src\set_add_json.mjs

--- public/src/set_add_json.mjs
+++ public/src/set_add_json.mjs
@@ -1,4 +1,5 @@
 import { set_add } from "../../../love/public/src/set_add.mjs";
 export function set_add_json(found, item) {
-  return set_add(found, item);
+  let r = set_add(found, item);
+  return r;
 }
