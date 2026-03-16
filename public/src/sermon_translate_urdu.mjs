ba3bf0807085a232b3a634180fcf2d7743686ee4
Grace-Jared <christrosetolife@gmail.com>
2026-03-16 18:25:37 -0400

    function_auto_path ..\love\public\src\sermon_translate_urdu.mjs

--- public/src/sermon_translate_urdu.mjs
+++ public/src/sermon_translate_urdu.mjs
@@ -46,7 +46,7 @@ export async function sermon_translate_urdu(file_name) {
     if (change) {
       let i = floor(index2 / 2);
       let item3 = list_get(value2, i);
-      let mapped2 = list_map_pairs(item3, item2, $f);
+      let mapped2 = list_map_pairs(item3, item2, () => {});
       let r5 = [item3, item2];
       return r5;
     }
