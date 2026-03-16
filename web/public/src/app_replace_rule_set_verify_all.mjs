37779c33db8f9baa486c0645d10790281986b792
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 20:02:48 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_all.mjs

--- web/public/src/app_replace_rule_set_verify_all.mjs
+++ web/public/src/app_replace_rule_set_verify_all.mjs
@@ -1,4 +1,5 @@
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
 export function app_replace_rule_set_verify_all(rule_set_get) {
-  return app_replace_rule_set_verify(rule_set_get);
+  let r = app_replace_rule_set_verify(rule_set_get);
+  return r;
 }
