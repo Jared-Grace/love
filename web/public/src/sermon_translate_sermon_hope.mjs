f508563666e541e5e45b20fabbe7a2cb21322d42
Grace-Jared <christrosetolife@gmail.com>
2026-03-16 18:18:18 -0400

    function_auto_path ..\love\public\src\sermon_translate_sermon_hope.mjs

--- web/public/src/sermon_translate_sermon_hope.mjs
+++ web/public/src/sermon_translate_sermon_hope.mjs
@@ -1,6 +1,6 @@
 import { sermon_translate_urdu } from "../../../love/public/src/sermon_translate_urdu.mjs";
 export async function sermon_translate_sermon_hope() {
-  const file_name = "David";
+  const file_name = "sermon_hope";
   let joined = await sermon_translate_urdu(file_name);
   return joined;
 }
