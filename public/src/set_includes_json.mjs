3408dcd541f71c87af2b4a0f537930ed753a49a4
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 20:12:12 -0400

    function_auto_path ..\love\public\src\set_includes_json.mjs

--- public/src/set_includes_json.mjs
+++ public/src/set_includes_json.mjs
@@ -1,5 +1,7 @@
+import { json_to } from "../../../love/public/src/json_to.mjs";
 import { set_includes } from "../../../love/public/src/set_includes.mjs";
 export function set_includes_json(set, item) {
-  let si = set_includes(set, item);
+  let json = json_to(item);
+  let si = set_includes(set, json);
   return si;
 }
