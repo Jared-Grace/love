b8d092663c8447623ac5549e6b5655bb71040a29
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 20:02:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- web/public/src/app_replace_rule_set_verify.mjs
+++ web/public/src/app_replace_rule_set_verify.mjs
@@ -8,10 +8,9 @@ import { list_first } from "../../../love/public/src/list_first.mjs";
 import { app_replace_rule_apply } from "../../../love/public/src/app_replace_rule_apply.mjs";
 import { app_replace_rule_valid } from "../../../love/public/src/app_replace_rule_valid.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { app_replace_rule_set_identifiers_simple } from "../../../love/public/src/app_replace_rule_set_identifiers_simple.mjs";
 import { json_equal } from "./json_equal.mjs";
-export function app_replace_rule_set_verify() {
-  let rs = app_replace_rule_set_identifiers_simple();
+export function app_replace_rule_set_verify(fn) {
+  let rs = fn();
   let rules = app_replace_rule_set_rules_get(rs);
   let goals = property_get(rs, "goals");
   let g = list_first(goals);
