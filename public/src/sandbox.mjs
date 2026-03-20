5615d68a0064434e3777293aa43f952322d00a2c
Grace-Jared <christrosetolife@gmail.com>
2026-03-20 17:07:01 -0400

    p

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,6 +3,7 @@ import compile from "nearley/lib/compile.js";
 import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 import { log_json } from "../../../love/public/src/log_json.mjs";
+
 export async function sandbox() {
   const grammarText = `
 main -> bits {% d => d[0] %}
@@ -20,34 +21,34 @@ bits -> bits di {% ([b,d]) => ({
   steps: [d]
 }) %}
 
-di -> "0" {% (d, location) => ({
-  rule: "di",
-  value: d.flat(Infinity)[0],
-  start: location - 1,
-  end: location
-}) %}
-   | "1" {% (d, location) => ({
-  rule: "di",
-  value: d.flat(Infinity)[0],
-  start: location - 1,
-  end: location
-}) %}
+di -> "0" {% (d, location) => {
+  const val = d.flat(Infinity)[0]; // unwrap nested array
+  return { rule: "di", value: val, start: location - 1, end: location };
+} %}
+   | "1" {% (d, location) => {
+  const val = d.flat(Infinity)[0];
+  return { rule: "di", value: val, start: location - 1, end: location };
+} %}
 `;
-  let v = nearley.Grammar.fromCompiled(grammarParser);
-  const parserGrammar = new nearley.Parser(v);
+
+  // Parse grammar string
+  const parserGrammar = new nearley.Parser(
+    nearley.Grammar.fromCompiled(grammarParser)
+  );
   parserGrammar.feed(grammarText);
   const grammarAst = parserGrammar.results[0];
+
+  // Compile and generate JS module
   const compiled = compile(grammarAst, {});
-  const jsModule = generate(compiled, "grammar", {
-    output: "commonjs",
-  });
-  const module = {
-    exports: {},
-  };
+  const jsModule = generate(compiled, "grammar", { output: "commonjs" });
+  const module = { exports: {} };
   eval(jsModule);
   const grammar = module.exports;
-  let v2 = nearley.Grammar.fromCompiled(grammar);
-  const parser = new nearley.Parser(v2);
+
+  // Parse input
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
   parser.feed("001");
+
+  // Log full annotated parse
   log_json(parser.results);
 }
