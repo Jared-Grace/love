63595b8f79cc23be5743755f8689d5f3b2a8eef6
Grace-Jared <christrosetolife@gmail.com>
2026-03-20 17:08:03 -0400

    p

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -44,6 +44,6 @@ di -> "0" {% (d, location) => {
   const grammar = module.exports;
   let v2 = nearley.Grammar.fromCompiled(grammar);
   const parser = new nearley.Parser(v2);
-  parser.feed("001");
+  parser.feed("111");
   log_json(parser.results);
 }
