00a6e84ece7d58d64b7c198e16ae8e46d084cee1
Grace-Jared <christrosetolife@gmail.com>
2026-03-20 17:06:29 -0400

    p

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,11 +3,12 @@ import compile from "nearley/lib/compile.js";
 import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 import { log_json } from "../../../love/public/src/log_json.mjs";
+
 export async function sandbox() {
   const grammarText = `
 main -> bits {% d => d[0] %}
 
-bits -> bits di {% ([b,d], location) => ({
+bits -> bits di {% ([b,d]) => ({
   value: b.value + d.value,
   start: b.start,
   end: d.end,
@@ -22,32 +23,36 @@ bits -> bits di {% ([b,d], location) => ({
 
 di -> "0" {% (d, location) => ({
   rule: "di",
-  value: Array.isArray(d[0]) ? d[0][0] : d[0],
+  value: d.flat(Infinity)[0],
   start: location - 1,
   end: location
 }) %}
    | "1" {% (d, location) => ({
   rule: "di",
-  value: Array.isArray(d[0]) ? d[0][0] : d[0],
+  value: d.flat(Infinity)[0],
   start: location - 1,
   end: location
 }) %}
 `;
-  let v = nearley.Grammar.fromCompiled(grammarParser);
-  const parserGrammar = new nearley.Parser(v);
+
+  // Parse grammar text
+  const parserGrammar = new nearley.Parser(
+    nearley.Grammar.fromCompiled(grammarParser)
+  );
   parserGrammar.feed(grammarText);
   const grammarAst = parserGrammar.results[0];
+
+  // Compile and generate JS
   const compiled = compile(grammarAst, {});
-  const jsModule = generate(compiled, "grammar", {
-    output: "commonjs",
-  });
-  const module = {
-    exports: {},
-  };
+  const jsModule = generate(compiled, "grammar", { output: "commonjs" });
+  const module = { exports: {} };
   eval(jsModule);
   const grammar = module.exports;
-  let v2 = nearley.Grammar.fromCompiled(grammar);
-  const parser = new nearley.Parser(v2);
+
+  // Parse input
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
   parser.feed("001");
+
+  // Log fully annotated result
   log_json(parser.results);
 }
