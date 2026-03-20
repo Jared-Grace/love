4142ad3fb17e030cbfc3a4df360e1f5a73c368f9
Grace-Jared <christrosetolife@gmail.com>
2026-03-20 16:59:56 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,14 +5,24 @@ import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 export async function sandbox() {
   const grammarText = `
-    main -> bits
+main -> bits {% (d, location, reject) => d[0] %}
 
-    bits -> bits di {% ([b,d]) => b + d %}
-         | di {% id %}
+bits -> bits di {% ([b,d], location) => ({
+  value: b.value + d.value,
+  start: b.start,
+  end: location,
+  steps: [...b.steps, d]
+}) %}
+     | di {% (d, location) => ({
+  value: d.value,
+  start: location - 1,
+  end: location,
+  steps: [{ rule: "di", value: d, start: location-1, end: location }]
+}) %}
 
-    di -> "0"
-        | "1"
-  `;
+di -> "0" {% (d, location) => ({ rule: "di", value: "0", start: location-1, end: location }) %}
+   | "1" {% (d, location) => ({ rule: "di", value: "1", start: location-1, end: location }) %}
+`;
   let v = nearley.Grammar.fromCompiled(grammarParser);
   const parserGrammar = new nearley.Parser(v);
   parserGrammar.feed(grammarText);
