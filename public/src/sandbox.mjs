90ade29e1eb51a1c84c9f2ae13d6cbe221d36620
Grace-Jared <christrosetolife@gmail.com>
2026-03-20 17:07:32 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -8,7 +8,7 @@ export async function sandbox() {
 main -> bits {% d => d[0] %}
 
 bits -> bits di {% ([b,d]) => ({
-  value: b.value,
+  value: d.value,
   start: b.start,
   end: d.end,
   steps: [...b.steps, d]
