5e4736c615f3ef4f53adf6fae6f6fba3a5b86e26
Grace-Jared <christrosetolife@gmail.com>
2026-03-20 17:30:45 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,18 +1,34 @@
-import { log } from "../../../love/public/src/log.mjs";
 import nearley from "nearley";
 import compile from "nearley/lib/compile.js";
 import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
+import { log_json } from "../../../love/public/src/log_json.mjs";
 export async function sandbox() {
   const grammarText = `
-    main -> bits
+main -> bits {% d => d[0] %}
 
-    bits -> bits di {% ([b,d]) => b + d %}
-         | di {% id %}
+bits -> bits di {% ([b,d]) => ({
+  value: b.value + d.value,
+  start: b.start,
+  end: d.end,
+  steps: [...b.steps, d]
+}) %}
+     | di {% (d) => ({
+  value: d.value,
+  start: d.start,
+  end: d.end,
+  steps: [d]
+}) %}
 
-    di -> "0"
-        | "1"
-  `;
+di -> "0" {% (d, location) => {
+  const val = d.flat(Infinity)[0]; // unwrap nested array
+  return { rule: "di", value: val, start: location - 1, end: location };
+} %}
+   | "1" {% (d, location) => {
+  const val = d.flat(Infinity)[0];
+  return { rule: "di", value: val, start: location - 1, end: location };
+} %}
+`;
   let v = nearley.Grammar.fromCompiled(grammarParser);
   const parserGrammar = new nearley.Parser(v);
   parserGrammar.feed(grammarText);
@@ -29,5 +45,5 @@ export async function sandbox() {
   let v2 = nearley.Grammar.fromCompiled(grammar);
   const parser = new nearley.Parser(v2);
   parser.feed("001");
-  console.log(parser.results);
+  log_json(parser.results);
 }
