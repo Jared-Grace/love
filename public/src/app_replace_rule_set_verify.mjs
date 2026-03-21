06a7e7a0edefa66443626f86a5986db62661c16e
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 16:44:09 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { not } from "../../../love/public/src/not.mjs";
@@ -14,6 +15,9 @@ import { json_to } from "./json_to.mjs";
 export function app_replace_rule_set_verify(rule_set_get) {
   let rs = rule_set_get();
   let rules = app_replace_rule_set_rules_get(rs);
+  log(app_replace_rule_set_verify.name, {
+    rules,
+  });
   let goals = property_get(rs, "goals");
   function lambda2(goal) {
     let se = app_replace_start_end_get(goal);
