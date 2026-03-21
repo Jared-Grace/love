ff0dbf3c523a5d01e8d1704f719aaf4b528a17dd
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 14:03:12 -0400

    function_auto_path ..\love\public\src\list_join_comma_space.mjs

--- web/public/src/list_join_comma_space.mjs
+++ web/public/src/list_join_comma_space.mjs
@@ -1,5 +1,5 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
 export function list_join_comma_space(args) {
-  let v = list_join(args, ", ");
-  return v;
+  let joined = list_join(args, ", ");
+  return joined;
 }
