a60eaf64b7044da0d6c036c2291c57a30a4e2bc4
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 19:36:43 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,9 +1,3 @@
-import { app_replace_rule_set_rules_get } from "../../../love/public/src/app_replace_rule_set_rules_get.mjs";
-import { app_replace_rule_set_scientific_notation_numbers } from "../../../love/public/src/app_replace_rule_set_scientific_notation_numbers.mjs";
-import { app_replace_rule_set_verify_nearley } from "../../../love/public/src/app_replace_rule_set_verify_nearley.mjs";
 export async function sandbox() {
-  let rs = app_replace_rule_set_scientific_notation_numbers();
-  let rules = app_replace_rule_set_rules_get(rs);
-  let r = app_replace_rule_set_verify_nearley(rules);
   return r;
 }
