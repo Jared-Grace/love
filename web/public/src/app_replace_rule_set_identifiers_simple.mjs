282b51c885bea62594908bf45fa03977884ae63b
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 19:44:11 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

--- web/public/src/app_replace_rule_set_identifiers_simple.mjs
+++ web/public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -59,7 +59,7 @@ export function app_replace_rule_set_identifiers_simple() {
         end: "idf idf idf idf",
       },
       {
-        start: "idf ida ida ida",
+        start: "idf idf idf idf",
         end: "A B B A",
       },
       {
