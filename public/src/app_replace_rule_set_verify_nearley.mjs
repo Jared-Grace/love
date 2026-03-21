6f948156589df1a472192557e2e5292ae36cf8a3
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:58:04 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_nearley.mjs

--- public/src/app_replace_rule_set_verify_nearley.mjs
+++ public/src/app_replace_rule_set_verify_nearley.mjs
@@ -22,6 +22,8 @@ export function app_replace_rule_set_verify_nearley(rules) {
       right: identifier,
     };
     let list = object_to_list(object);
+    function lambda2(item) {}
+    let mapped2 = list_map(list2, lambda2);
     let w = js_code_wrap_braces(inside2);
     let w2 = js_code_wrap_parenthesis(w);
     let code = js_code_arrow_args_body_expression(identifier, w2);
