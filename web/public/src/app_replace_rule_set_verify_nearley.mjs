d6aee630cc7fe0f446bc2bfb6862475b8c5eecb9
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:40:41 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_nearley.mjs

--- web/public/src/app_replace_rule_set_verify_nearley.mjs
+++ web/public/src/app_replace_rule_set_verify_nearley.mjs
@@ -1,5 +1,5 @@
 import { log_json } from "../../../love/public/src/log_json.mjs";
-export function app_replace_rule_set_verify_nearley(fn) {
+export function app_replace_rule_set_verify_nearley(rs) {
   const grammarText = `
 bits -> bits di {% (d) => ({
   left: 'bits',
