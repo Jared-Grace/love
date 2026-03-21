6993901091ddd81165aca33ee097ac31421eff3f
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 18:08:24 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals.mjs

--- public/src/app_replace_rule_set_decimals.mjs
+++ public/src/app_replace_rule_set_decimals.mjs
@@ -22,14 +22,14 @@ export function app_replace_rule_set_decimals() {
       },
       {
         start: "de",
-        end: "p . di di",
+        end: "p . di g",
       },
       {
-        start: "p . di di",
+        start: "p . di g",
         end: "3 . 1 4",
       },
       {
-        start: "de",
+        start: "p . di g",
         end: "p . di di di di di",
       },
       {
