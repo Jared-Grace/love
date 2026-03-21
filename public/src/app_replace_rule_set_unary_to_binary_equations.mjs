7f8fb184274437684910f22be02ec8fe6a66c7d7
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 16:53:05 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_unary_to_binary_equations.mjs

--- public/src/app_replace_rule_set_unary_to_binary_equations.mjs
+++ public/src/app_replace_rule_set_unary_to_binary_equations.mjs
@@ -62,6 +62,10 @@ export function app_replace_rule_set_unary_to_binary_equations() {
         end: "d b c + b c e = b c c",
       },
       {
+        start: "d b c c c c e = b c c c c",
+        end: "d b c c + b c c e = b c c c c",
+      },
+      {
         start: "d b c c c c c e = b c c c c c",
         end: "d b c + b c c c + b c e = b c c c + b c c",
       },
@@ -106,7 +110,7 @@ export function app_replace_rule_set_unary_to_binary_equations() {
         end: "b 1 0 + b 1 0 = b 1 0 0 d",
       },
       {
-        start: "d b c c c c c e = b c c c c c",
+        start: "d b c c c c c ce = b c c c c c",
         end: "b 1 0 0 + b 1 1 = b 1 1 1 d",
       },
     ],
