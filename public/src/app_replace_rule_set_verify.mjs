abf575b5388ea40d6419d761e5f1b4869965183d
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:37:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -17,7 +17,7 @@ export function app_replace_rule_set_verify(rule_set_get) {
   let se = app_replace_start_end_get(g);
   let end = property_get(se, "end");
   let start = property_get(se, "start");
-  let r2 = graph_search_depth_first(start, neighbors_get, json_to, 8, end);
+  let r2 = graph_search_depth_first(start, neighbors_get, json_to, 7, end);
   return r2;
   function neighbors_get(start) {
     let r = list_size_range(start);
