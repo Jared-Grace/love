d8b70bcbeb56ec841d06dc3a89daac063f2365b9
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 17:29:48 -0400

    function_auto_path ..\love\public\src\app_replace_rule_parse.mjs

--- public/src/app_replace_rule_parse.mjs
+++ public/src/app_replace_rule_parse.mjs
@@ -1,3 +1,4 @@
+import { lists_sizes_equal_assert } from "../../../love/public/src/lists_sizes_equal_assert.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { list_skip } from "../../../love/public/src/list_skip.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
@@ -9,6 +10,7 @@ export function app_replace_rule_parse(rule) {
   let left = list_take(split, middle);
   let right = list_skip(split, middle + 1);
   let filtered = list_filter_empty_not_is(left);
+  lists_sizes_equal_assert(lists);
   let v = {
     left,
     right,
