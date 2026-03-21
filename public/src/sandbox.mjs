c8d482a036cb7c22f0a7c01a2195df1c03b200c2
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:41:20 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_set_rules_get } from "../../../love/public/src/app_replace_rule_set_rules_get.mjs";
 import { app_replace_rule_set_scientific_notation_numbers } from "../../../love/public/src/app_replace_rule_set_scientific_notation_numbers.mjs";
 import { app_replace_rule_set_verify_nearley } from "../../../love/public/src/app_replace_rule_set_verify_nearley.mjs";
 import nearley from "nearley";
@@ -6,5 +7,6 @@ import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 export async function sandbox() {
   let rs = app_replace_rule_set_scientific_notation_numbers();
-  app_replace_rule_set_verify_nearley(rs);
+  let rules = app_replace_rule_set_rules_get(rs);
+  app_replace_rule_set_verify_nearley(rules);
 }
