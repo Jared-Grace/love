67606e00d9153c6be5736cbd272ced793c550a92
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:54:25 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_nearley.mjs

--- web/public/src/js_code_arrow_args_body_expression.mjs
+++ web/public/src/js_code_arrow_args_body_expression.mjs
@@ -1,6 +1,6 @@
 import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
-export function js_code_arrow_args_body_expression(args, b) {
+export function js_code_arrow_args_body_expression(args, expression) {
   let oc = js_code_wrap_parenthesis(args);
-  const code = oc + "=>" + b;
+  const code = oc + "=>" + expression;
   return code;
 }
