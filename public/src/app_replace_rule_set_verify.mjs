1328d87af688f71dcb866f8be6fade44890ba1f0
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 15:10:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { app_replace_rule_set_rules_get } from "../../../love/public/src/app_replace_rule_set_rules_get.mjs";
 import { graph_search_depth_first } from "../../../love/public/src/graph_search_depth_first.mjs";
@@ -14,6 +15,8 @@ export function app_replace_rule_set_verify(rule_set_get) {
   let rs = rule_set_get();
   let rules = app_replace_rule_set_rules_get(rs);
   let goals = property_get(rs, "goals");
+  function lambda2(item) {}
+  each(list, lambda2);
   let g = list_first(goals);
   let se = app_replace_start_end_get(g);
   let end = property_get(se, "end");
