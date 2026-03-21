6783f02687e46fed9bbbacb46dccb7c3538d1408
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:40:13 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -5,6 +5,6 @@ import compile from "nearley/lib/compile.js";
 import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 export async function sandbox() {
-  let r = app_replace_rule_set_scientific_notation_numbers();
+  let fn = app_replace_rule_set_scientific_notation_numbers;
   app_replace_rule_set_verify_nearley();
 }
