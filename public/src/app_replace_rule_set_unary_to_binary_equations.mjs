b93398a741b5487b41cc3d7f88b8105246950c9d
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 17:27:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_unary_to_binary_equations.mjs

--- public/src/app_replace_rule_set_unary_to_binary_equations.mjs
+++ public/src/app_replace_rule_set_unary_to_binary_equations.mjs
@@ -138,11 +138,11 @@ export function app_replace_rule_set_unary_to_binary_equations() {
         end: "b 1 1 + b 1 0 0 = b 1 0 0 a a a d",
       },
       {
-        start: "b 1 1 + b 1 0 0 = b 1 0 0 a a a a d",
-        end: "b 1 1 + b 1 0 0 = b 1 0 1 a a a d",
+        start: "b 1 1 + b 1 0 0 = b 1 0 0 a a a d",
+        end: "b 1 1 + b 1 0 0 = b 1 0 1 a a d",
       },
       {
-        start: "b 1 1 + b 1 0 0 = b 1 0 1 a a a d",
+        start: "b 1 1 + b 1 0 0 = b 1 0 1 a a d",
         end: "b 1 1 + b 1 0 0 = b 1 1 1 d",
       },
     ],
