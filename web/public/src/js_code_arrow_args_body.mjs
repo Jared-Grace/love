fa2153345a9894a42e621154acbee4e589343e03
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:52:39 -0400

    function_auto_path ..\love\public\src\js_code_arrow_args_body.mjs

--- web/public/src/js_code_arrow_args_body.mjs
+++ web/public/src/js_code_arrow_args_body.mjs
@@ -1,8 +1,7 @@
+import { js_code_arrow_args_body_expression } from "../../../love/public/src/js_code_arrow_args_body_expression.mjs";
 import { js_code_wrap_braces } from "../../../love/public/src/js_code_wrap_braces.mjs";
-import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
 export function js_code_arrow_args_body(args, body) {
   let b = js_code_wrap_braces(body);
-  let oc = js_code_wrap_parenthesis(args);
-  const code = oc + "=>" + b;
+  const code = js_code_arrow_args_body_expression(args, b);
   return code;
 }
