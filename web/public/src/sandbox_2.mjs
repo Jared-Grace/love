9e1a45ac68f13360c5535292277cf3577c9ce52b
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:28:12 -0400

    function_auto_path ..\love\public\src\sandbox_2.mjs

--- web/public/src/sandbox_2.mjs
+++ web/public/src/sandbox_2.mjs
@@ -1,16 +1,45 @@
-import { ebible_languages_without_original } from "../../../love/public/src/ebible_languages_without_original.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { ebible_languages_add_item_info } from "../../../love/public/src/ebible_languages_add_item_info.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
+import nearley from "nearley";
+import compile from "nearley/lib/compile.js";
+import generate from "nearley/lib/generate.js";
+import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
+import { log_json } from "../../../love/public/src/log_json.mjs";
 export async function sandbox_2() {
-  let languages = ebible_languages_without_original();
-  async function lambda(l) {
-    let bible_folder = property_get(l, "bible_folder");
-    let r = await ebible_languages_add_item_info(bible_folder);
-    log(sandbox_2.name, {
-      r,
-    });
-  }
-  await each_async(languages, lambda);
+  const grammarText = `
+bits -> bits di {% (d) => ({
+  left: 'bits',
+  right: d
+}) %}
+
+bits -> di {% (d) => ({
+  left: 'bits',
+  right: d
+}) %}
+
+di -> "0" {% (d) => {
+  const val = d.flat(Infinity)[0];
+  return { left: "di", right: [val] };
+} %}
+
+di -> "1" {% (d) => {
+  const val = d.flat(Infinity)[0];
+  return { left: "di", right: [val] };
+} %}
+`;
+  let v = nearley.Grammar.fromCompiled(grammarParser);
+  const parserGrammar = new nearley.Parser(v);
+  parserGrammar.feed(grammarText);
+  const grammarAst = parserGrammar.results[0];
+  const compiled = compile(grammarAst, {});
+  const jsModule = generate(compiled, "grammar", {
+    output: "commonjs",
+  });
+  const module = {
+    exports: {},
+  };
+  eval(jsModule);
+  const grammar = module.exports;
+  let v2 = nearley.Grammar.fromCompiled(grammar);
+  const parser = new nearley.Parser(v2);
+  parser.feed("001");
+  log_json(parser.results);
 }
