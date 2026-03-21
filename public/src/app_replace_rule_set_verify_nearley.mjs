f4a515a1efe034b017662d6a199d81f06a8a4cec
Grace-Jared <christrosetolife@gmail.com>
2026-03-21 13:38:59 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

---
+++ public/src/app_replace_rule_set_verify_nearley.mjs
@@ -0,0 +1,41 @@
+import { log_json } from "../../../love/public/src/log_json.mjs";
+export function app_replace_rule_set_verify_nearley() {
+  const grammarText = `
+bits -> bits di {% (d) => ({
+  left: 'bits',
+  right: d
+}) %}
+
+bits -> di {% (d) => ({
+  left: 'bits',
+  right: d
+}) %}
+
+di -> "0" {% (d) => {
+  const val = d.flat(Infinity)[0];
+  return { left: "di", right: [val] };
+} %}
+
+di -> "1" {% (d) => {
+  const val = d.flat(Infinity)[0];
+  return { left: "di", right: [val] };
+} %}
+`;
+  let v = nearley.Grammar.fromCompiled(grammarParser);
+  const parserGrammar = new nearley.Parser(v);
+  parserGrammar.feed(grammarText);
+  const grammarAst = parserGrammar.results[0];
+  const compiled = compile(grammarAst, {});
+  const jsModule = generate(compiled, "grammar", {
+    output: "commonjs",
+  });
+  const module = {
+    exports: {},
+  };
+  eval(jsModule);
+  const grammar = module.exports;
+  let v2 = nearley.Grammar.fromCompiled(grammar);
+  const parser = new nearley.Parser(v2);
+  parser.feed("001");
+  log_json(parser.results);
+}
