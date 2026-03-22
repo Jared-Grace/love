5e370a9acd176876ebb0dc0c26795f1e966bf7ab
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 19:13:13 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_goal_path.mjs

--- web/public/src/app_replace_rule_set_verify_goal_path.mjs
+++ web/public/src/app_replace_rule_set_verify_goal_path.mjs
@@ -7,8 +7,8 @@ export function app_replace_rule_set_verify_goal_path(goal, rules_parsed) {
   let dfs = app_replace_rule_set_verify_goal(goal, rules_parsed);
   let max_depth = app_replace_rule_set_verify_goal_depth_max();
   const property_name = "previous";
-  let list3 = list_linked_to_list(dfs, property_name, max_depth);
-  let mapped3 = list_map_property(list3, "data");
+  let list = list_linked_to_list(dfs, property_name, max_depth);
+  let mapped3 = list_map_property(list, "data");
   list_reverse(mapped3);
   return mapped3;
 }
