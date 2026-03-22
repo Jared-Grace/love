bc8bb998b82c0aa660ee2679b9da00d687dbbcfc
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 00:56:50 -0400

    function_auto_path ..\love\public\src\ebible_version_readaloud_download_refresh.mjs

--- public/src/ebible_version_readaloud_download_refresh.mjs
+++ public/src/ebible_version_readaloud_download_refresh.mjs
@@ -1,3 +1,4 @@
+import { invoke_multiple_arg_async } from "../../../love/public/src/invoke_multiple_arg_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { ebible_version_readaloud_download_zip_delete } from "../../../love/public/src/ebible_version_readaloud_download_zip_delete.mjs";
 import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
@@ -10,6 +11,7 @@ export async function ebible_version_readaloud_download_refresh(bible_folder) {
     await fn(bible_folder);
   }
   await each_async(fns, lambda);
+  let r = await invoke_multiple_arg_async(list_fns, arg);
   await ebible_version_readaloud_download_zip_delete(bible_folder);
   await ebible_version_readaloud_download(bible_folder);
 }
