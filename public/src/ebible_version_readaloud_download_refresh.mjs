e316a162d8749d17e885bc130cd688fe3dc8c211
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 00:51:18 -0400

    fws ebible_version_readaloud_download refresh

---
+++ public/src/ebible_version_readaloud_download_refresh.mjs
@@ -0,0 +1,4 @@
+import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
+export async function ebible_version_readaloud_download_refresh(bible_folder) {
+  return await ebible_version_readaloud_download(bible_folder);
+}
