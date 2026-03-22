e20462bf53fb7cc00ffbc2b7cbfcdc14daf9ed34
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 00:56:40 -0400

    function_auto_path ..\love\public\src\invoke_multiple_arg_async.mjs

--- public/src/invoke_multiple_arg_async.mjs
+++ public/src/invoke_multiple_arg_async.mjs
@@ -1,5 +1,5 @@
 import { invoke_multiple_args_async } from "../../../love/public/src/invoke_multiple_args_async.mjs";
-export async function invoke_multiple_arg_async(list_fns, args) {
-  let r = await invoke_multiple_args_async(list_fns, args);
+export async function invoke_multiple_arg_async(list_fns, arg) {
+  let r = await invoke_multiple_args_async(list_fns, [arg]);
   return r;
 }
