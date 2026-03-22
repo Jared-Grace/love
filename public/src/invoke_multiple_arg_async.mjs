fd5f475f4ace29dac0c03447e048a3e0ceca85d6
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 00:56:30 -0400

    fwr invoke_multiple_args_async gs g

---
+++ public/src/invoke_multiple_arg_async.mjs
@@ -0,0 +1,4 @@
+import { invoke_multiple_args_async } from "../../../love/public/src/invoke_multiple_args_async.mjs";
+export async function invoke_multiple_arg_async(list_fns, args) {
+  return await invoke_multiple_args_async(list_fns, args);
+}
