f2dd00d4ebc59f4ffe40beca756362e6f32e9bf2
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 00:47:30 -0400

    function_auto_path ..\love\public\src\ebible_version_readaloud_download_zip_delete.mjs

--- web/public/src/ebible_version_readaloud_download_zip_delete.mjs
+++ web/public/src/ebible_version_readaloud_download_zip_delete.mjs
@@ -1,8 +1,8 @@
+import { ebible_version_readaloud_download_url } from "../../../love/public/src/ebible_version_readaloud_download_url.mjs";
 import { ebible_version_download_zip_delete_generic } from "../../../love/public/src/ebible_version_download_zip_delete_generic.mjs";
-import { ebible_version_download_url_html } from "../../../love/public/src/ebible_version_download_url_html.mjs";
 export async function ebible_version_readaloud_download_zip_delete(
   bible_folder,
 ) {
-  let url_get = ebible_version_download_url_html;
+  let url_get = ebible_version_readaloud_download_url;
   await ebible_version_download_zip_delete_generic(url_get, bible_folder);
 }
