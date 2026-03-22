02d35c1a6ebb068f93924ea9f18f1cb4fd85473d
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 00:51:20 -0400

    function_auto_path ..\love\public\src\ebible_version_readaloud_download_refresh.mjs

--- public/src/ebible_version_readaloud_download_refresh.mjs
+++ public/src/ebible_version_readaloud_download_refresh.mjs
@@ -1,4 +1,5 @@
 import { ebible_version_readaloud_download } from "../../../love/public/src/ebible_version_readaloud_download.mjs";
 export async function ebible_version_readaloud_download_refresh(bible_folder) {
-  return await ebible_version_readaloud_download(bible_folder);
+  let r = await ebible_version_readaloud_download(bible_folder);
+  return r;
 }
