896f5ff514a7751765c3d324f5e159419858028c
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 18:29:41 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- web/public/src/app_replace_rule_set_verify.mjs
+++ web/public/src/app_replace_rule_set_verify.mjs
@@ -4,10 +4,10 @@ import { app_replace_rule_set_rules_get } from "../../../love/public/src/app_rep
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_replace_rule_set_verify(rule_set_get) {
   let rs = rule_set_get();
-  let rules = app_replace_rule_set_rules_get(rs);
+  let rules_parsed = app_replace_rule_set_rules_get(rs);
   let goals = property_get(rs, "goals");
   function lambda2(goal) {
-    let dfs = app_replace_rule_set_verify_goal(goal, rules);
+    let dfs = app_replace_rule_set_verify_goal(goal, rules_parsed);
     return dfs;
   }
   each(goals, lambda2);
