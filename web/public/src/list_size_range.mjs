cc8c228f8199c3def004730f6537c1bf8db9db3d
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 19:41:39 -0400

    function_auto_path ..\love\public\src\list_size_range.mjs

--- web/public/src/list_size_range.mjs
+++ web/public/src/list_size_range.mjs
@@ -1,7 +1,7 @@
 import { range } from "../../../love/public/src/range.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-export function list_size_range(start) {
-  let size = list_size(start);
+export function list_size_range(list) {
+  let size = list_size(list);
   let r = range(size);
   return r;
 }
