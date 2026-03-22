f30c7bfeb51a4a1d6c2c3e646c640e3cee45c7c8
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 19:11:28 -0400

    function_auto_path ..\love\public\src\list_linked_to_list.mjs

--- public/src/list_linked_to_list.mjs
+++ public/src/list_linked_to_list.mjs
@@ -2,15 +2,15 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function list_linked_to_list(dfs, property_name, max_depth) {
+export function list_linked_to_list(node_first, property_name_next, max_depth) {
   function lambda12(la) {
-    la(dfs);
+    la(node_first);
     function lambda10(i) {
-      dfs = property_get(dfs, property_name);
-      if (null_is(dfs)) {
+      node_first = property_get(node_first, property_name_next);
+      if (null_is(node_first)) {
         return true;
       }
-      la(dfs);
+      la(node_first);
     }
     each_range(max_depth, lambda10);
   }
