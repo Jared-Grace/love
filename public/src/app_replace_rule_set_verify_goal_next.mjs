fd58f7977b201ae812cc7fe104d906a5a571a668
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 12:21:20 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_goal_next.mjs

--- public/src/app_replace_rule_set_verify_goal_next.mjs
+++ public/src/app_replace_rule_set_verify_goal_next.mjs
@@ -1,5 +1,4 @@
 import { list_second } from "../../../love/public/src/list_second.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_rule_set_verify_goal_path } from "../../../love/public/src/app_replace_rule_set_verify_goal_path.mjs";
 export function app_replace_rule_set_verify_goal_next(
   rules_parsed,
@@ -7,9 +6,6 @@ export function app_replace_rule_set_verify_goal_next(
   end,
 ) {
   let path = app_replace_rule_set_verify_goal_path(rules_parsed, start, end);
-  log(app_replace_rule_set_verify_goal_next.name, {
-    path,
-  });
   let second = list_second(path);
   return second;
 }
