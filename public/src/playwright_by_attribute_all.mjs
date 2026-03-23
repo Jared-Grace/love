aa96c912cc458f178fce5694dd28a9475dce327d
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 18:39:43 -0400

    fws playwright_by_attribute all

---
+++ public/src/playwright_by_attribute_all.mjs
@@ -0,0 +1,4 @@
+import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
+export function playwright_by_attribute_all(page, name, value) {
+  return playwright_by_attribute(page, name, value);
+}
