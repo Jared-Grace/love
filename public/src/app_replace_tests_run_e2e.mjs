42ddc51d7fe5ab4d8e3fb97e4f221c203486fcd6
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 18:15:22 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -88,6 +88,7 @@ export async function app_replace_tests_run_e2e() {
                 break;
               }
             }
+          } else {
           }
           refresh_count = await app_replace_rule_set_attribute_refresh_click(
             page,
