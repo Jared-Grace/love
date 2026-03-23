e202b771eb40fb92e72b51f1eed1125626cc1ae7
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 12:14:24 -0400

    function_auto_path ..\love\public\src\git_acp_call_folder_try_curried_right_2.mjs

--- public/src/git_acp_call_folder_try_curried_right_2.mjs
+++ public/src/git_acp_call_folder_try_curried_right_2.mjs
@@ -1,6 +1,10 @@
 import { git_acp_call_folder_try } from "../../../love/public/src/git_acp_call_folder_try.mjs";
 export async function git_acp_call_folder_try_curried_right_2(f_name, args) {
-  return async function git_acp_call_folder_try_curried_right_2_result(folder) {
-    return await git_acp_call_folder_try(folder, f_name, args);
+  let r2 = async function git_acp_call_folder_try_curried_right_2_result(
+    folder,
+  ) {
+    let r = await git_acp_call_folder_try(folder, f_name, args);
+    return r;
   };
+  return r2;
 }
