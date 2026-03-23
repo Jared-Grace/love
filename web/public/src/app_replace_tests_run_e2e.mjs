24fb3d6d8f2397678eb88348346e999f1eb4adcc
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 18:46:37 -0400

    pd app_replace_tests_run_e2e_generic normal

--- web/public/src/app_replace_tests_run_e2e.mjs
+++ web/public/src/app_replace_tests_run_e2e.mjs
@@ -16,7 +16,7 @@ import { app_replace_rule_set_attribute_refresh_click } from "../../../love/publ
 import { app_replace_rule_set_attribute_symbol } from "../../../love/public/src/app_replace_rule_set_attribute_symbol.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function app_replace_tests_run_e2e() {
-  await app_replace_tests_run_e2e_generic(hinted, normal);
+  await app_replace_tests_run_e2e_generic(hinted);
   async function normal(p, refresh_count, page, symbol_id) {
     let rule = property_get(p, "rule");
     let original = property_get(rule, "original");
