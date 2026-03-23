8babdd90de1131dc2fe006ab2c2134a2dcd34f22
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:33:51 -0400

    function_auto_path ..\love\public\src\html_data_set_test.mjs

--- public/src/html_data_set_test.mjs
+++ public/src/html_data_set_test.mjs
@@ -2,6 +2,6 @@ import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
 export function html_data_set_test(component, value) {
   let suffix = "";
-  let combined = text_combine(left, right);
-  html_data_set(component, "test", value);
+  let combined = text_combine("test", suffix);
+  html_data_set(component, combined, value);
 }
