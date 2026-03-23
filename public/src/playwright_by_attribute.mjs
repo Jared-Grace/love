a685715bd183fe81fad37763921e082ef2d10c59
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:20:30 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute.mjs

--- public/src/playwright_by_attribute.mjs
+++ public/src/playwright_by_attribute.mjs
@@ -1,7 +1,6 @@
-import { tasks_run } from "../../../love/public/src/tasks_run.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
-export async function playwright_by_attribute(page, name, value) {
-  let result = await tasks_run();
+export function playwright_by_attribute(page, name, value) {
+  '\\"';
   const inside = name + '="' + value + '"';
   let r = playwright_by_attribute_named(page, inside);
   return r;
