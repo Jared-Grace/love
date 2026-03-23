67b7b330f0c656e624c769acf9498024c0b3a671
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:53:53 -0400

    function_auto_path ..\love\public\src\retry_fast.mjs

--- public/src/retry_fast.mjs
+++ public/src/retry_fast.mjs
@@ -2,7 +2,8 @@ import { retry_generic } from "../../../love/public/src/retry_generic.mjs";
 export async function retry_fast(lambda) {
   let wait = 100;
   let timeout = 5000;
-  let result = await retry_generic(lambda, wait, wait_get, 50);
+  let count = timeout / wait;
+  let result = await retry_generic(lambda, wait, wait_get, count);
   return result;
   function wait_get(wait) {
     wait += 100;
