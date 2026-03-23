3c8fc722794419310077b3d962a8fa2c23c40ee7
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 22:45:08 -0400

    function_auto_path ..\love\public\src\karate_tests.mjs

--- public/src/karate_tests.mjs
+++ public/src/karate_tests.mjs
@@ -1,12 +1,7 @@
+import { server_url_app_dev } from "../../../love/public/src/server_url_app_dev.mjs";
 import { portfolio_qa_attribute_test_data } from "../../../portfolio_qa/public/src/portfolio_qa_attribute_test_data.mjs";
 import { app_karate_main_title } from "../../../karate_code/public/src/app_karate_main_title.mjs";
-import { server_url } from "../../../love/public/src/server_url.mjs";
-import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
-import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_karate } from "../../../karate_code/public/src/app_karate.mjs";
-import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
-import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 import { playwright_by_attribute_type } from "../../../love/public/src/playwright_by_attribute_type.mjs";
@@ -14,12 +9,7 @@ import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { playwright_test_url } from "../../../love/public/src/playwright_test_url.mjs";
 export async function karate_tests() {
   let app_fn = app_karate;
-  let without = app_prefix_without_fn(app_fn);
-  let r = await app_shared_name_search_info(without);
-  let f_path_dev = property_get(r, "f_path_dev");
-  let previous = folder_previous();
-  let together = text_prefix_without(f_path_dev, previous);
-  const url = server_url() + together + "";
+  const url = await server_url_app_dev(app_fn);
   await playwright_test_url(url, lambda);
   async function lambda(page) {
     const title_actual = await page.title();
---
+++ public/src/server_url_app_dev.mjs
@@ -0,0 +1,15 @@
+import { server_url } from "../../../love/public/src/server_url.mjs";
+import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
+import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
+import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
+export async function server_url_app_dev(app_fn) {
+  let without = app_prefix_without_fn(app_fn);
+  let r = await app_shared_name_search_info(without);
+  let f_path_dev = property_get(r, "f_path_dev");
+  let previous = folder_previous();
+  let together = text_prefix_without(f_path_dev, previous);
+  const url = server_url() + together + "";
+  return url;
+}
