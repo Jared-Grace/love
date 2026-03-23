ec921d396a0176c101995611b7658e5ba9b3d8ff
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:58:25 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_test_exists_assert.mjs

--- web/public/src/playwright_by_attribute_test_exists_assert.mjs
+++ web/public/src/playwright_by_attribute_test_exists_assert.mjs
@@ -1,8 +1,6 @@
+import { playwright_by_attribute_exists_assert } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert.mjs";
 import { qa_attribute_test_data } from "../../../love/public/src/qa_attribute_test_data.mjs";
-import { playwright_by_attribute_exists_assert_assertion } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert_assertion.mjs";
-import { playwright_by_attribute_exists } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists.mjs";
 export async function playwright_by_attribute_test_exists_assert(page, value) {
   const name = qa_attribute_test_data();
-  let e = await playwright_by_attribute_exists(page, name, value);
-  playwright_by_attribute_exists_assert_assertion(e, name, value);
+  let e = await playwright_by_attribute_exists_assert(page, name, value);
 }
