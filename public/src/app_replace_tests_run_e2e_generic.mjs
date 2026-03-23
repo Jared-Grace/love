906dd9a5d4f1a246be59c5461824a5109c84526e
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 18:45:53 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_generic.mjs

--- public/src/app_replace_tests_run_e2e_generic.mjs
+++ public/src/app_replace_tests_run_e2e_generic.mjs
@@ -42,21 +42,12 @@ export async function app_replace_tests_run_e2e_generic(hinted, normal) {
         );
         async function lambda3(p) {
           let symbol_id = null;
-          if (true) {
-            ({ refresh_count, symbol_id } = await hinted(
-              p,
-              refresh_count,
-              page,
-              symbol_id,
-            ));
-          } else {
-            ({ refresh_count, symbol_id } = await normal(
-              p,
-              refresh_count,
-              page,
-              symbol_id,
-            ));
-          }
+          ({ refresh_count, symbol_id } = await hinted(
+            p,
+            refresh_count,
+            page,
+            symbol_id,
+          ));
           refresh_count = await app_replace_rule_set_attribute_refresh_click(
             page,
             symbol_id,
