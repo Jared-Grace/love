24662d3423a62107fe08cc5a0833c2a4e31b46cf
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:06:27 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run.mjs

--- public/src/app_replace_tests_run.mjs
+++ public/src/app_replace_tests_run.mjs
@@ -1,6 +1,6 @@
+import { qa_attribute_test_data } from "../../../love/public/src/qa_attribute_test_data.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { playwright_by_attribute_click } from "../../../love/public/src/playwright_by_attribute_click.mjs";
-import { portfolio_qa_attribute_test_data } from "../../../portfolio_qa/public/src/portfolio_qa_attribute_test_data.mjs";
 import { sleep_long } from "../../../portfolio_qa/public/src/sleep_long.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
@@ -12,9 +12,8 @@ export async function app_replace_tests_run() {
     await sleep_long();
     async function lambda_each(rule_set) {
       let name2 = property_get(rule_set, "name");
-      const name = portfolio_qa_attribute_test_data();
-      const value = "login-button";
-      await playwright_by_attribute_click(page, name, value);
+      const name = qa_attribute_test_data();
+      await playwright_by_attribute_click(page, name, name2);
     }
     await each_async(rule_sets, lambda_each);
   }
