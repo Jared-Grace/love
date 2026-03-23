644b742bdbb9ef812cfc8fb7ded3adc6b8986497
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 11:51:58 -0400

    rs repos_gitignore_overwrite all

--- web/public/src/repos_gitignore_overwrite_all.mjs
+++ web/public/src/repos_gitignore_overwrite_all.mjs
@@ -1,6 +1,6 @@
 import { repos_gitignore_overwrite_list } from "../../../love/public/src/repos_gitignore_overwrite_list.mjs";
 import { repos_names } from "../../../love/public/src/repos_names.mjs";
-export async function repos_gitignore_overwrite() {
+export async function repos_gitignore_overwrite_all() {
   let repos = await repos_names();
   await repos_gitignore_overwrite_list(repos);
 }
