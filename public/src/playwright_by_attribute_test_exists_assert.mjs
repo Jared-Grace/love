c963710ffa689212d64ded38010baf265ee270b9
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:26:41 -0400

    cpi playwright_by_attribute_exists_assert exists test

---
+++ public/src/playwright_by_attribute_test_exists_assert.mjs
@@ -0,0 +1,10 @@
+import { playwright_by_attribute_exists_assert_assertion } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert_assertion.mjs";
+import { playwright_by_attribute_exists } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists.mjs";
+export async function playwright_by_attribute_test_exists_assert(
+  page,
+  name,
+  value,
+) {
+  let e = await playwright_by_attribute_exists(page, name, value);
+  playwright_by_attribute_exists_assert_assertion(e, name, value);
+}
