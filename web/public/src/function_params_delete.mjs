83253a74ef287c707c522ba95c6e9e85187036b6
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:41:46 -0400

    function_auto_path ..\love\public\src\function_params_delete.mjs

--- web/public/src/function_params_delete.mjs
+++ web/public/src/function_params_delete.mjs
@@ -3,8 +3,8 @@ import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs"
 import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
 export async function function_params_delete(f_name, param_names_comma) {
   let split = text_split_comma(param_names_comma);
-  async function lambda(item) {}
-  await each_async(list, lambda);
-  let r = await function_param_delete(f_name, param_name);
-  return r;
+  async function lambda(item) {
+    await function_param_delete(f_name, param_name);
+  }
+  await each_async(split, lambda);
 }
