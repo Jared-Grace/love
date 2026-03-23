d0f745b77f876c9169e427eeeb1a42185a9f58f0
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 16:02:18 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- public/src/playwright_by_attribute_named_all.mjs
+++ public/src/playwright_by_attribute_named_all.mjs
@@ -1,27 +1,20 @@
-import { text_to } from "../../../love/public/src/text_to.mjs";
-import { playwright_element_attribute_equals } from "../../../love/public/src/playwright_element_attribute_equals.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
-import { list_filter } from "./list_filter.mjs";
-export async function playwright_by_attribute_named_all(page, name, value) {
-  value = text_to(value);
+export async function playwright_by_attribute_named_all(page, name) {
   name = text_combine("data-", name);
   log(playwright_by_attribute_named_all.name, {
     name,
   });
   let locator = playwright_by_attribute_named(page, name);
-  function lambda2(a) {
-    return a;
+  function lambda5(elements, name) {
+    function lambda4(el) {
+      let r = el.getAttribute(name);
+      return r;
+    }
+    let r2 = elements.map(lambda4);
+    return r2;
   }
-  const elements = await locator.evaluateAll(lambda2, name);
-  log(playwright_by_attribute_named_all.name, {
-    elements,
-  });
-  function lambda(element) {
-    let r3 = playwright_element_attribute_equals(element, name, value);
-    return r3;
-  }
-  let mapped = list_filter(elements, lambda);
-  return mapped;
+  const values = await locator.evaluateAll(lambda5, name);
+  return values;
 }
