9bd899fab407202a6eb590bb3d7abfa97e67962c
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 11:51:58 -0400

    rs repos_gitignore_overwrite all

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -1,6 +1,6 @@
 import { git_ignore_name } from "../../../love/public/src/git_ignore_name.mjs";
 import { git_ignore_add } from "../../../love/public/src/git_ignore_add.mjs";
-import { repos_gitignore_overwrite } from "../../../love/public/src/repos_gitignore_overwrite.mjs";
+import { repos_gitignore_overwrite_all } from "../../../love/public/src/repos_gitignore_overwrite_all.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { git_add } from "../../../love/public/src/git_add.mjs";
 import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
@@ -12,5 +12,5 @@ export async function git_remove(f_path) {
   await git_add(added);
   await git_commit("Remove " + f_path + " and add to " + g_name);
   await git_push();
-  await repos_gitignore_overwrite();
+  await repos_gitignore_overwrite_all();
 }
