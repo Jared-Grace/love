7ef61845a508c1e5fd89aee3d6635bc8b417c0bf
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:07:04 -0400

    function_auto_path ..\love\public\src\add_curried.mjs

--- web/public/src/retry_fast.mjs
+++ web/public/src/retry_fast.mjs
@@ -4,7 +4,7 @@ export async function retry_fast(lambda) {
   let interval = 100;
   let timeout = 5000;
   let count = timeout / interval;
-  let r2 = add_curried_right(right);
+  let r2 = add_curried(right);
   let result = await retry_generic(lambda, interval, wait_get, count);
   return result;
   function wait_get(wait) {
