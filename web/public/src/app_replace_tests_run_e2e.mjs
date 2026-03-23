f459702ffe38de3705bb020cb89f422c871bf2b9
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:34:46 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- web/public/src/app_replace_tests_run_e2e.mjs
+++ web/public/src/app_replace_tests_run_e2e.mjs
@@ -41,7 +41,7 @@ export async function app_replace_tests_run_e2e() {
         async function lambda3(p) {
           let value3 =
             app_replace_rule_set_attribute_refresh_count(refresh_count);
-          await sleep(ms);
+          await sleep(3000);
           await playwright_by_attribute_test_exists_assert(page, value3);
           let rule = property_get(p, "rule");
           let index = property_get(p, "index");
