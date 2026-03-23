5e60c2c59fffeb6251083de8370921033e37b4b3
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:48:49 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- public/src/playwright_by_attribute_named_all.mjs
+++ public/src/playwright_by_attribute_named_all.mjs
@@ -1,6 +1,8 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
 export async function playwright_by_attribute_named_all(page, name) {
+  let combined = text_combine(left, right);
   log(playwright_by_attribute_named_all.name, {
     name,
   });
