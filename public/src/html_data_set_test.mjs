fdc29fb614245b858fd0f1f3a000de3089ab4cf9
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:33:41 -0400

    function_auto_path ..\love\public\src\html_data_set_test.mjs

--- public/src/html_data_set_test.mjs
+++ public/src/html_data_set_test.mjs
@@ -1,5 +1,7 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
 export function html_data_set_test(component, value) {
-  let suffix = "";tc
+  let suffix = "";
+  let combined = text_combine(left, right);
   html_data_set(component, "test", value);
 }
