55aa2fcec6f25b48e77496a880fa99b793b17973
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:43:45 -0400

    function_auto_path ..\love\public\src\text_split_dot.mjs

--- public/src/text_split_dot.mjs
+++ public/src/text_split_dot.mjs
@@ -1,5 +1,7 @@
+import { text_split_multiple } from "../../../love/public/src/text_split_multiple.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
 export function text_split_dot(t) {
+  let parts = text_split_multiple(str, delimiters);
   let split = text_split(t, ",");
   return split;
 }
