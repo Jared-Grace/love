ffca926a4d8fe4cc355abdcc4249d3295777bbe9
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 22:23:17 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_all.mjs

--- web/public/src/app_replace_rule_set_verify_all.mjs
+++ web/public/src/app_replace_rule_set_verify_all.mjs
@@ -1,9 +1,11 @@
+import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
 export function app_replace_rule_set_verify_all() {
   let rule_set_gets = app_replace_rule_sets_fns();
+  let mapped = invoke_multiple(list_fns);
   function lambda(rule_set_get) {
     log(app_replace_rule_set_verify_all.name, {
       rule_set_get,
