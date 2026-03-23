1c1a5d2af308770e53574be8104714b55cce7d7d
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:39:17 -0400

    function_auto_path ..\love\public\src\function_params_delete.mjs

--- web/public/src/function_params_delete.mjs
+++ web/public/src/function_params_delete.mjs
@@ -1,4 +1,5 @@
 import { function_param_delete } from "../../../love/public/src/function_param_delete.mjs";
 export async function function_params_delete(f_name, param_name) {
-  return await function_param_delete(f_name, param_name);
+  let r = await function_param_delete(f_name, param_name);
+  return r;
 }
