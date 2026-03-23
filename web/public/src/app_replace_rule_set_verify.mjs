2064f70868bc434476ac9c3504091bce96893969
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 20:46:08 -0400

    rrl app_replace_rule_set_verify_goal from

--- web/public/src/app_replace_rule_set_verify.mjs
+++ web/public/src/app_replace_rule_set_verify.mjs
@@ -1,4 +1,4 @@
-import { app_replace_rule_set_verify_goal } from "../../../love/public/src/app_replace_rule_set_verify_goal.mjs";
+import { app_replace_rule_set_verify_from } from "../../../love/public/src/app_replace_rule_set_verify_from.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_replace_rule_set_rules_get } from "../../../love/public/src/app_replace_rule_set_rules_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -7,7 +7,7 @@ export function app_replace_rule_set_verify(rule_set_get) {
   let rules_parsed = app_replace_rule_set_rules_get(rs);
   let goals = property_get(rs, "goals");
   function lambda2(goal) {
-    let dfs = app_replace_rule_set_verify_goal(goal, rules_parsed);
+    let dfs = app_replace_rule_set_verify_from(goal, rules_parsed);
     return dfs;
   }
   each(goals, lambda2);
--- web/public/src/app_replace_rule_set_verify_goal_path.mjs
+++ web/public/src/app_replace_rule_set_verify_goal_path.mjs
@@ -2,9 +2,9 @@ import { list_reverse } from "../../../love/public/src/list_reverse.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { list_linked_to_list } from "../../../love/public/src/list_linked_to_list.mjs";
 import { app_replace_rule_set_verify_goal_depth_max } from "../../../love/public/src/app_replace_rule_set_verify_goal_depth_max.mjs";
-import { app_replace_rule_set_verify_goal } from "../../../love/public/src/app_replace_rule_set_verify_goal.mjs";
+import { app_replace_rule_set_verify_from } from "../../../love/public/src/app_replace_rule_set_verify_from.mjs";
 export function app_replace_rule_set_verify_goal_path(goal, rules_parsed) {
-  let dfs = app_replace_rule_set_verify_goal(goal, rules_parsed);
+  let dfs = app_replace_rule_set_verify_from(goal, rules_parsed);
   let max_depth = app_replace_rule_set_verify_goal_depth_max();
   const property_name = "previous";
   let list = list_linked_to_list(dfs, property_name, max_depth);
