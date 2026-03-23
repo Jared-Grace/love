9c50b322e8c6b7c1b78c40349016dee674b82795
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 16:25:12 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- public/src/playwright_by_attribute_named_all.mjs
+++ public/src/playwright_by_attribute_named_all.mjs
@@ -1,20 +1,15 @@
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
-export async function playwright_by_attribute_named_all(page, name) {
+export async function playwright_by_attribute_named_all_handles(page, name) {
   name = text_combine("data-", name);
-  log(playwright_by_attribute_named_all.name, {
+  log(playwright_by_attribute_named_all_handles.name, {
     name,
   });
-  let locator = playwright_by_attribute_named(page, name);
-  function lambda5(elements, name) {
-    function lambda4(el) {
-      let r = el.getAttribute(name);
-      return r;
-    }
-    let r2 = elements.filter(lambda4);
-    return r2;
-  }
-  const values = await locator.evaluateAll(lambda5, name);
-  return values;
+  const locator = playwright_by_attribute_named(page, name);
+  await locator.first().waitFor({
+    state: "attached",
+  });
+  const handles = await locator.elementHandles();
+  return handles;
 }
