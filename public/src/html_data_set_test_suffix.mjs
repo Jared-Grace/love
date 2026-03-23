bdc35a62270999036e6772d21f41e699ac2b969e
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:45:25 -0400

    pndf html_data_set_test_suffix component

--- public/src/html_data_set_test_suffix.mjs
+++ public/src/html_data_set_test_suffix.mjs
@@ -1,4 +1,4 @@
 import { html_data_set_test_suffixes } from "../../../love/public/src/html_data_set_test_suffixes.mjs";
-export function html_data_set_test_suffix(suffix, valid) {
+export function html_data_set_test_suffix(component, suffix, valid) {
   html_data_set_test_suffixes(component, [suffix], valid);
 }
