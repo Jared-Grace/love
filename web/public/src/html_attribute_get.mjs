a19f89913a498a81c8a8fb6ac4ce4f56b74ffad0
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 17:26:45 -0400

    function_auto_path ..\love\public\src\html_attribute_get.mjs

--- web/public/src/html_attribute_get.mjs
+++ web/public/src/html_attribute_get.mjs
@@ -1,6 +1,7 @@
+import { html_attribute_get_unwrapped } from "../../../love/public/src/html_attribute_get_unwrapped.mjs";
 import { html_component_element_get } from "../../../love/public/src/html_component_element_get.mjs";
 export function html_attribute_get(component, key) {
   let element = html_component_element_get(component);
-  let value = element.getAttribute(key);
+  let value = html_attribute_get_unwrapped(element, key);
   return value;
 }
---
+++ web/public/src/html_attribute_get_unwrapped.mjs
@@ -0,0 +1,4 @@
+export function html_attribute_get_unwrapped(element, key) {
+  let r = element.getAttribute(key);
+  return r;
+}
