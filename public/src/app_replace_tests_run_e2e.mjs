991bee1eb3714df8aa518dd9aa690a066bbfb37b
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:11:01 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -39,7 +39,7 @@ export async function app_replace_tests_run_e2e() {
           end,
         );
         async function lambda3(p) {
-          let r2 = playwright_by_attribute_named(page2, inside);
+          let r2 = playwright_by_attribute_named(page, "data-test");
           let value3 =
             app_replace_rule_set_attribute_refresh_count(refresh_count);
           await playwright_by_attribute_test_exists_assert(page, value3);
