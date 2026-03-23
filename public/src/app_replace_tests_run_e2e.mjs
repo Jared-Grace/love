b8f2162051860efad59136a7a9e79444127320de
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:25:38 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -40,7 +40,7 @@ export async function app_replace_tests_run_e2e() {
           let value3 =
             app_replace_rule_set_attribute_refresh_count(refresh_count);
           const name = qa_attribute_test_data();
-          let e = await playwright_by_attribute_exists(page2, name, value3);
+          let e = await playwright_by_attribute_exists(page, name, value3);
           let rule = property_get(p, "rule");
           let index = property_get(p, "index");
           let original = property_get(rule, "original");
