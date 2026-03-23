1e8f60d5dede7a809de585e338d61db9a3445b80
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:50:25 -0400

    function_auto_path ..\love\public\src\retry_generic.mjs

--- web/public/src/retry_generic.mjs
+++ web/public/src/retry_generic.mjs
@@ -3,7 +3,6 @@ import { not } from "../../../love/public/src/not.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { each_range_async } from "../../../love/public/src/each_range_async.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
-import { retry } from "../../../love/public/src/retry.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export async function retry_generic(lambda, wait, wait_get, count) {
   let result = null;
