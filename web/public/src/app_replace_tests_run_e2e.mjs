e456cb495560fd32824d015d7b3b68ab4febf9a9
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 18:49:28 -0400

    rpi app_replace_tests_run_hinted hinted e2e

--- web/public/src/app_replace_tests_run_e2e.mjs
+++ web/public/src/app_replace_tests_run_e2e.mjs
@@ -1,6 +1,6 @@
-import { app_replace_tests_run_hinted } from "../../../love/public/src/app_replace_tests_run_hinted.mjs";
+import { app_replace_tests_run_e2e_hinted } from "../../../love/public/src/app_replace_tests_run_e2e_hinted.mjs";
 import { app_replace_tests_run_e2e_normal } from "../../../love/public/src/app_replace_tests_run_e2e_normal.mjs";
 export async function app_replace_tests_run_e2e() {
   await app_replace_tests_run_e2e_normal();
-  await app_replace_tests_run_hinted();
+  await app_replace_tests_run_e2e_hinted();
 }
