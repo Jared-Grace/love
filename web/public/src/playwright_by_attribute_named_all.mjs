aea3cb23a5a54e3441e0b955a7326244bb43b289
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:50:39 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- web/public/src/playwright_by_attribute_named_all.mjs
+++ web/public/src/playwright_by_attribute_named_all.mjs
@@ -1,3 +1,4 @@
+import { identity } from "../../../love/public/src/identity.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
@@ -7,6 +8,7 @@ export async function playwright_by_attribute_named_all(page, name) {
     name,
   });
   let locator = playwright_by_attribute_named(page, name);
+  const values = await locator.evaluateAll(identity, name);
   function lambda5(elements, name) {
     function lambda4(el) {
       let r = el.getAttribute(name);
@@ -15,6 +17,5 @@ export async function playwright_by_attribute_named_all(page, name) {
     let r2 = elements.map(lambda4);
     return r2;
   }
-  const values = await locator.evaluateAll(lambda5, name);
   return values;
 }
