97d85f8b49214b78a1ab3954871178880c8d3829
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 20:40:33 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_symbol_on_click.mjs

--- public/src/app_replace_rule_set_symbol_on_click.mjs
+++ public/src/app_replace_rule_set_symbol_on_click.mjs
@@ -83,7 +83,6 @@ export async function app_replace_rule_set_symbol_on_click(
   } else {
     property_set(symbols_invalid_chosen, index, true);
   }
-  refresh();
   let r5 = {
     index_selected,
     start,
