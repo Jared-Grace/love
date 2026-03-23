16aed64062c7099e99ae940200484b671e7a593d
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 11:54:42 -0400

    function_auto_path ..\love\public\src\repo_new.mjs

--- public/src/repo_new.mjs
+++ public/src/repo_new.mjs
@@ -1,9 +1,10 @@
+import { git_init_folder } from "../../../love/public/src/git_init_folder.mjs";
 import { repo_new_local } from "../../../love/public/src/repo_new_local.mjs";
 import { git_ac_call_folder } from "../../../love/public/src/git_ac_call_folder.mjs";
 import { command_line_folder } from "../../../love/public/src/command_line_folder.mjs";
 export async function repo_new(repo_name) {
   let folder = await repo_new_local(repo_name);
-    let r3 = await git_init_folder(folder);
+  let r3 = await git_init_folder(folder);
   await git_ac_call_folder(repo_new.name, [repo_name], folder);
   let r = await command_line_folder(
     "gh repo create " +
