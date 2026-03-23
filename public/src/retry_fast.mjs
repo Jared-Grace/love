c690b553ec69f3dbf4d43fda7aa09ac65ac3beec
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:53:13 -0400

    s div

--- public/src/retry_fast.mjs
+++ public/src/retry_fast.mjs
@@ -1,7 +1,8 @@
 import { retry_generic } from "../../../love/public/src/retry_generic.mjs";
-export async function retry_fast(count, lambda) {
+export async function retry_fast(lambda) {
   let wait = 100;
-  let result = await retry_generic(lambda, wait, wait_get, count);
+  let timeout = 5000;
+  let result = await retry_generic(lambda, wait, wait_get, 50);
   return result;
   function wait_get(wait) {
     wait += 100;
