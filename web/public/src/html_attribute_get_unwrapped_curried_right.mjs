d8468028daeeb6c4ef7e210d0f8a00cde4be799b
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 17:34:24 -0400

    function_auto_path ..\love\public\src\html_attribute_get_unwrapped_curried_right.mjs

--- web/public/src/html_attribute_get_unwrapped_curried_right.mjs
+++ web/public/src/html_attribute_get_unwrapped_curried_right.mjs
@@ -1,6 +1,8 @@
 import { html_attribute_get_unwrapped } from "../../../love/public/src/html_attribute_get_unwrapped.mjs";
 export function html_attribute_get_unwrapped_curried_right(key) {
-  return function html_attribute_get_unwrapped_curried_right_result(element) {
-    return html_attribute_get_unwrapped(element, key);
+  let r2 = function html_attribute_get_unwrapped_curried_right_result(element) {
+    let r = html_attribute_get_unwrapped(element, key);
+    return r;
   };
+  return r2;
 }
