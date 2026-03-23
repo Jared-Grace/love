09090268fa95d31a81a95354469206d200ae0b61
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:53:03 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- public/src/playwright_by_attribute_named_all.mjs
+++ public/src/playwright_by_attribute_named_all.mjs
@@ -2,6 +2,7 @@ import { identity } from "../../../love/public/src/identity.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
+import { list_filter } from "./list_filter.mjs";
 export async function playwright_by_attribute_named_all(page, name, value) {
   name = text_combine("data-", name);
   log(playwright_by_attribute_named_all.name, {
@@ -11,8 +12,4 @@ export async function playwright_by_attribute_named_all(page, name, value) {
   const elements = await locator.evaluateAll(identity, name);
   function lambda(element) {
     let actual = element.getAttribute(name);
-    let r3 = actual === value;
-    return r3;
-  }
-  return mapped;
-}
+    let r3 = actual 
