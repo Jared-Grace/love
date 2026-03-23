10a35e5a3fc4d28acef1221bcf363eafb70a5b5d
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 16:27:06 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- public/src/playwright_by_attribute_named_all.mjs
+++ public/src/playwright_by_attribute_named_all.mjs
@@ -10,5 +10,8 @@ export async function playwright_by_attribute_named_all_handles(page, name) {
   const locator = playwright_by_attribute_named(page, name);
   await playwright_locator_wait(locator);
   const handles = await locator.elementHandles();
+  log(playwright_by_attribute_named_all_handles.name, {
+    handles,
+  });
   return handles;
 }
