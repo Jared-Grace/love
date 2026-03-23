cf5b7c54a6df4924287153632f72ca6dcda3878a
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 14:51:43 -0400

    cs retry fast

---
+++ public/src/retry_fast.mjs
@@ -0,0 +1,9 @@
+import { retry_generic } from "../../../love/public/src/retry_generic.mjs";
+export async function retry_fast(count, lambda) {
+  let wait = 1000;
+  let result = await retry_generic(lambda, wait, wait_get, count);
+  return result;
+  function wait_get(wait) {
+    wait *= 2;
+  }
+}
