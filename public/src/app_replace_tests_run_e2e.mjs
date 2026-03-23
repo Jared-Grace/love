bcbc060a2feb75c62320fc097becc3fe3548ff98
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 18:19:11 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -94,8 +94,9 @@ export async function app_replace_tests_run_e2e() {
               original,
               refresh_count,
             );
-          }$s
-          let symbol_id = app_replace_rule_set_attribute_symbol(index);
+          }
+          let symbol_id = null;
+          symbol_id = app_replace_rule_set_attribute_symbol(index);
           refresh_count = await app_replace_rule_set_attribute_refresh_click(
             page,
             symbol_id,
