d59d58f3f07854d712672ba07c8573630fd14142
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:37:42 -0400

    function_auto_path ..\love\public\src\html_data_set_test.mjs

--- public/src/html_data_set_test.mjs
+++ public/src/html_data_set_test.mjs
@@ -1,4 +1,4 @@
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
-export function html_data_set_test(title, value) {
-  html_data_set(title, "test", value);
+export function html_data_set_test(component, value) {
+  html_data_set(component, "test", value);
 }
