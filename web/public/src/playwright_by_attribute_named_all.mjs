2b1c3a1a9347625eac7c2a0f7ceb8b4bb795c135
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:55:41 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- web/public/src/playwright_by_attribute_named_all.mjs
+++ web/public/src/playwright_by_attribute_named_all.mjs
@@ -1,3 +1,4 @@
+import { text_to } from "../../../love/public/src/text_to.mjs";
 import { playwright_element_attribute_equals } from "../../../love/public/src/playwright_element_attribute_equals.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
@@ -5,6 +6,7 @@ import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
 import { list_filter } from "./list_filter.mjs";
 export async function playwright_by_attribute_named_all(page, name, value) {
+  let s = text_to(input);
   name = text_combine("data-", name);
   log(playwright_by_attribute_named_all.name, {
     name,
