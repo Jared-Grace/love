0be3f7985b209418373f8d486f49e1c35bcc19a8
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:36:47 -0400

    function_auto_path ..\love\public\src\html_data_set_test_suffixes.mjs

--- public/src/html_data_set_test.mjs
+++ public/src/html_data_set_test.mjs
@@ -1,5 +1,5 @@
-import { html_data_set_test_suffix } from "../../../love/public/src/html_data_set_test_suffix.mjs";
+import { html_data_set_test_suffixes } from "../../../love/public/src/html_data_set_test_suffixes.mjs";
 export function html_data_set_test(component, value) {
   let suffix = "";
-  html_data_set_test_suffix(suffix, component, value);
+  html_data_set_test_suffixes(suffix, component, value);
 }
