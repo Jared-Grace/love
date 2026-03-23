448f7bce3478cb30ea0323f89d75936936aacf52
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:37:28 -0400

    function_auto_path ..\love\public\src\app_replace_button_rule.mjs

--- public/src/app_replace_button_rule.mjs
+++ public/src/app_replace_button_rule.mjs
@@ -1,9 +1,11 @@
+import { html_data_set_test } from "../../../love/public/src/html_data_set_test.mjs";
 import { app_replace_button_rule_style } from "../../../love/public/src/app_replace_button_rule_style.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_button_rule_content } from "../../../love/public/src/app_replace_button_rule_content.mjs";
 import { html_button_notext } from "../../../love/public/src/html_button_notext.mjs";
 export function app_replace_button_rule(root, left, right, on_click) {
   let b = html_button_notext(root, on_click);
+  html_data_set_test(title, value);
   app_replace_button_rule_style(b);
   let r2 = app_replace_button_rule_content(b, left, right);
   let arrow = property_get(r2, "arrow");
