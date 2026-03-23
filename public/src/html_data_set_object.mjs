f5924286799ee9132171e160a0ce59bebbd25f5a
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:00:01 -0400

    function_auto_path ..\love\public\src\html_data_set_object.mjs

--- public/src/html_data_set_object.mjs
+++ public/src/html_data_set_object.mjs
@@ -1,8 +1,8 @@
 import { each_object } from "../../../love/public/src/each_object.mjs";
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
-export function html_data_set_object(title, r4) {
+export function html_data_set_object(component, object) {
   function lambda2(value, property) {
-    html_data_set(title, property, value);
+    html_data_set(component, property, value);
   }
-  each_object(r4, lambda2);
+  each_object(object, lambda2);
 }
