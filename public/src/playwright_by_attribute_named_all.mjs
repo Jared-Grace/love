6bba5b2509395aee60b049484edc481f691c24a2
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:57:27 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_named_all.mjs

--- public/src/playwright_by_attribute_named_all.mjs
+++ public/src/playwright_by_attribute_named_all.mjs
@@ -1,6 +1,5 @@
 import { text_to } from "../../../love/public/src/text_to.mjs";
 import { playwright_element_attribute_equals } from "../../../love/public/src/playwright_element_attribute_equals.mjs";
-import { identity } from "../../../love/public/src/identity.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
@@ -12,7 +11,7 @@ export async function playwright_by_attribute_named_all(page, name, value) {
     name,
   });
   let locator = playwright_by_attribute_named(page, name);
-  const elements = await locator.evaluateAll(identity, name);
+  const elements = await locator.evaluateAll(() => {}, name);
   log(playwright_by_attribute_named_all.name, {
     elements,
   });
