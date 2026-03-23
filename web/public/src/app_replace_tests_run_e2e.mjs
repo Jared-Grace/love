e836c0130d3b128d331a2b7b3b69f9a9adb1d14c
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:08:23 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run.mjs

---
+++ web/public/src/app_replace_tests_run_e2e.mjs
@@ -0,0 +1,20 @@
+import { app_replace } from "../../../love/public/src/app_replace.mjs";
+import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { sleep_long } from "../../../portfolio_qa/public/src/sleep_long.mjs";
+import { playwright_by_attribute_click } from "../../../love/public/src/playwright_by_attribute_click.mjs";
+import { qa_attribute_test_data } from "../../../love/public/src/qa_attribute_test_data.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export async function app_replace_tests_run_e2e(rule_sets) {
+  async function lambda(page) {
+    async function lambda_each(rule_set) {
+      let name2 = property_get(rule_set, "name");
+      const name = qa_attribute_test_data();
+      await playwright_by_attribute_click(page, name, name2);
+      await sleep_long();
+      return true;
+    }
+    await each_async(rule_sets, lambda_each);
+  }
+  await playwright_test_app_dev(app_replace, lambda);
+}
