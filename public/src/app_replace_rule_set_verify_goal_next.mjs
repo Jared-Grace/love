a9b478d941067d626da3fd33a0726d2c8b9f5f8a
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:26:54 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_goal_next.mjs

--- public/src/app_replace_rule_set_verify_goal_next.mjs
+++ public/src/app_replace_rule_set_verify_goal_next.mjs
@@ -1,5 +1,4 @@
 import { list_second } from "../../../love/public/src/list_second.mjs";
-import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_rule_set_verify_goal_path } from "../../../love/public/src/app_replace_rule_set_verify_goal_path.mjs";
 export function app_replace_rule_set_verify_goal_next(
