9628fc38f5d690e1e52e87d41ab6245aa850c8b9
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:06:53 -0400

    de add_curried_right

--- web/public/src/add_curried_right.mjs
+++
@@ -1,8 +0,0 @@
-import { add } from "../../../love/public/src/add.mjs";
-export function add_curried_right(right) {
-  let r = function add_curried_right_result(left) {
-    let sum = add(left, right);
-    return sum;
-  };
-  return r;
-}
