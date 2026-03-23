ef483a7f3e03d1be4be65b3abe786d069447ad27
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 22:49:15 -0400

    function_auto_path ..\love\public\src\app_replace_home.mjs

--- public/src/app_replace_home.mjs
+++ public/src/app_replace_home.mjs
@@ -39,7 +39,7 @@ export function app_replace_home(context) {
       index2,
       lambda,
     );
-    html_data_set(title, "t", "123");
+    html_data_set(title, "test", rule_name);
     html_text_set(title, rule_name);
     function lambda() {
       on_click(index2);
