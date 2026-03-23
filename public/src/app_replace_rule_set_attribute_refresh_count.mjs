dc599ef6951d45527d73209d69c76b72eb6e883b
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:09:50 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_attribute_refresh_count.mjs

--- public/src/app_replace_rule_set_attribute_refresh_count.mjs
+++ public/src/app_replace_rule_set_attribute_refresh_count.mjs
@@ -1,4 +1,4 @@
 export function app_replace_rule_set_attribute_refresh_count(refresh_count) {
-  let r3 = "refresh_count_" + refresh_count;
-  return r3;
+  let a = "refresh_count_" + refresh_count;
+  return a;
 }
