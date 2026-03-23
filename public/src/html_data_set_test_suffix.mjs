4bd786179dfd31f2349a55fd9db4c4705f25c311
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:38:39 -0400

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid_if.mjs

---
+++ public/src/html_data_set_test_suffix.mjs
@@ -0,0 +1,4 @@
+import { html_data_set_test_suffixes } from "../../../love/public/src/html_data_set_test_suffixes.mjs";
+export function html_data_set_test_suffix(suffix, valid) {
+  html_data_set_test_suffixes(component, [suffix], valid);
+}
