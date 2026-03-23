22f607756a048e1e3bd882138f93f890a32da133
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:19:36 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

---
+++ web/public/src/app_replace_rule_set_attribute_refresh_count_assert.mjs
@@ -0,0 +1,9 @@
+import { playwright_by_attribute_test_exists_assert } from "../../../love/public/src/playwright_by_attribute_test_exists_assert.mjs";
+import { app_replace_rule_set_attribute_refresh_count } from "../../../love/public/src/app_replace_rule_set_attribute_refresh_count.mjs";
+export async function app_replace_rule_set_attribute_refresh_count_assert(
+  refresh_count,
+  page,
+) {
+  let value3 = app_replace_rule_set_attribute_refresh_count(refresh_count);
+  await playwright_by_attribute_test_exists_assert(page, value3);
+}
