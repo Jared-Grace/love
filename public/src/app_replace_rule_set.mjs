8255c8b5b89f20877f962788d580809606f301cd
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 20:40:45 -0400

    pd app_replace_rule_set_symbol_on_click refresh

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -198,7 +198,6 @@ export async function app_replace_rule_set(context) {
             rules_buttons,
             duration,
             div_symbols,
-            refresh,
           ));
         refresh();
       }
