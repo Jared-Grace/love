117c6abd79775ff941aec43901e31f9bd25bcfbc
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:35:43 -0400

    function_auto_path ..\love\public\src\html_data_set_test_suffix.mjs

--- web/public/src/html_data_set_test_suffix.mjs
+++ web/public/src/html_data_set_test_suffix.mjs
@@ -1,9 +1,9 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
-export function html_data_set_test_suffix(suffix, component, value) {
+export function html_data_set_test_suffix(suffixes, component, value) {
   const list = ["test"];
-  list_add_multiple(list2, items);
+  list_add_multiple(list2, suffixes);
   let combined = function_name_combine_multiple(list);
   html_data_set(component, combined, value);
 }
