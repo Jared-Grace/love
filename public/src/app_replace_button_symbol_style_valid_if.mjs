7f683e13cd8ec03cbf66ef2ca9ae74a63ac613f6
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:32:59 -0400

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid_if.mjs

--- public/src/app_replace_button_symbol_style_valid_if.mjs
+++ public/src/app_replace_button_symbol_style_valid_if.mjs
@@ -1,4 +1,4 @@
-import { html_data_set_test } from "../../../love/public/src/html_data_set_test.mjs";
+import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
 import { app_replace_button_symbol_style_box_shadow } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow.mjs";
 import { app_replace_rule_set_highlight } from "../../../love/public/src/app_replace_rule_set_highlight.mjs";
 import { html_style_background_color_set_if_else } from "../../../love/public/src/html_style_background_color_set_if_else.mjs";
@@ -8,7 +8,7 @@ export function app_replace_button_symbol_style_valid_if(sb, valid) {
   html_enable_if(sb, valid);
   html_style_background_color_set_if_else(valid, sb, "#00b400ff", "#1e6c1eff");
   html_style_font_color_set_if(valid, sb, "white", "#b9fcb9ff");
-  html_data_set_test(sb, value);
+  html_data_set(sb, "test", value);
   let h = app_replace_rule_set_highlight();
   app_replace_button_symbol_style_box_shadow(valid, sb, h);
 }
