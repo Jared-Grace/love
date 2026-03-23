bf0feab1dccb4fb2076ed53ad4d59ccbf71e3243
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:42:21 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -1,3 +1,4 @@
+import { html_data_set_test_suffixes_attribute } from "../../../love/public/src/html_data_set_test_suffixes_attribute.mjs";
 import { playwright_by_attribute_named_all } from "../../../love/public/src/playwright_by_attribute_named_all.mjs";
 import { app_replace_rule_set_attribute_refresh_click } from "../../../love/public/src/app_replace_rule_set_attribute_refresh_click.mjs";
 import { app_replace_rule_set_attribute_refresh_count_assert } from "../../../love/public/src/app_replace_rule_set_attribute_refresh_count_assert.mjs";
@@ -51,6 +52,7 @@ export async function app_replace_tests_run_e2e() {
             original,
             refresh_count,
           );
+          let combined = html_data_set_test_suffixes_attribute(suffixes);
           let values = await playwright_by_attribute_named_all(page2, name3);
           let value = app_replace_rule_set_attribute_symbol(index);
           refresh_count = await app_replace_rule_set_attribute_refresh_click(
