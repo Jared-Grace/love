8e8c1e6f948650856f5ffc9af1d21e1b6cf0da20
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 17:53:06 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -85,7 +85,7 @@ export async function app_replace_tests_run_e2e() {
                 filtered2,
               });
               let size = list_size(filtered2);
-              let s = list_size_1(filtered2);
+              let s = list_size_1(filtered2);$i
               let ceiling = list_size_half_ceil(start_indices);
               refresh_count =
                 await app_replace_rule_set_attribute_refresh_click(
