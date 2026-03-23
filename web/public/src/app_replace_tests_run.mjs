9ddb112e484feaf3084d4c974b5e4b6831c604f6
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 22:27:25 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run.mjs

--- web/public/src/app_replace_tests_run.mjs
+++ web/public/src/app_replace_tests_run.mjs
@@ -2,6 +2,6 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_rule_set_verify_all } from "../../../love/public/src/app_replace_rule_set_verify_all.mjs";
 export async function app_replace_tests_run() {
   let rule_sets = app_replace_rule_set_verify_all();
-  async function lambda(item) {}
-  await each_async(list, lambda);
+  async function lambda(rule_set) {}
+  await each_async(rule_sets, lambda);
 }
