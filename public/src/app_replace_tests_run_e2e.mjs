fe8e065dee50385f866f92737c8892f77b69778a
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 17:43:09 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -61,6 +61,11 @@ export async function app_replace_tests_run_e2e() {
           let index = property_get(p, "index");
           let original = property_get(rule, "original");
           if (true) {
+            refresh_count = await app_replace_rule_set_attribute_refresh_click(
+              page,
+              hint,
+              refresh_count,
+            );
             let suffix = app_replace_button_symbol_style_valid_if_attribute();
             let combined = html_data_set_test_suffixes_attribute([suffix]);
             let values = await playwright_by_attribute_named_all(
