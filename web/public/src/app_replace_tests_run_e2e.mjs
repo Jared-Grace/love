b5492a2f84f5b6b86c9647ecf8ded3347f48ce91
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:28:18 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- web/public/src/app_replace_tests_run_e2e.mjs
+++ web/public/src/app_replace_tests_run_e2e.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { app_replace_rule_set_verify_goal_next } from "../../../love/public/src/app_replace_rule_set_verify_goal_next.mjs";
 import { playwright_by_attribute_test_click } from "../../../love/public/src/playwright_by_attribute_test_click.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
@@ -19,7 +20,8 @@ export async function app_replace_tests_run_e2e() {
     await playwright_by_attribute_test_click(page, json);
     async function lambda_each(rule_set) {
       let goals = property_get(rule_set, "goals");
-      let end2 = property_get(start, "end");
+      function lambda2(item) {}
+      each(list, lambda2);
       let second = app_replace_rule_set_verify_goal_next(
         rules_parsed,
         start,
