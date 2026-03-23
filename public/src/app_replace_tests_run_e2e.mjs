e9d463d403ac029a420fa0de704d5884e885e0d0
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:43:07 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -1,3 +1,4 @@
+import { app_replace_button_symbol_style_valid_if_attribute } from "../../../love/public/src/app_replace_button_symbol_style_valid_if_attribute.mjs";
 import { html_data_set_test_suffixes_attribute } from "../../../love/public/src/html_data_set_test_suffixes_attribute.mjs";
 import { playwright_by_attribute_named_all } from "../../../love/public/src/playwright_by_attribute_named_all.mjs";
 import { app_replace_rule_set_attribute_refresh_click } from "../../../love/public/src/app_replace_rule_set_attribute_refresh_click.mjs";
@@ -52,7 +53,8 @@ export async function app_replace_tests_run_e2e() {
             original,
             refresh_count,
           );
-          let combined = html_data_set_test_suffixes_attribute(suffixes);
+          let suffix = app_replace_button_symbol_style_valid_if_attribute();
+          let combined = html_data_set_test_suffixes_attribute([suffix]);
           let values = await playwright_by_attribute_named_all(page2, name3);
           let value = app_replace_rule_set_attribute_symbol(index);
           refresh_count = await app_replace_rule_set_attribute_refresh_click(
