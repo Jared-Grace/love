75abbfbed9c876a667f9ee497ebc49780a847677
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 15:39:19 -0400

    function_auto_path ..\love\public\src\app_replace_button_symbol_style_valid_if.mjs

--- web/public/src/app_replace_button_symbol_style_valid_if.mjs
+++ web/public/src/app_replace_button_symbol_style_valid_if.mjs
@@ -1,5 +1,4 @@
 import { html_data_set_test_suffix } from "../../../love/public/src/html_data_set_test_suffix.mjs";
-import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
 import { app_replace_button_symbol_style_box_shadow } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow.mjs";
 import { app_replace_rule_set_highlight } from "../../../love/public/src/app_replace_rule_set_highlight.mjs";
 import { html_style_background_color_set_if_else } from "../../../love/public/src/html_style_background_color_set_if_else.mjs";
@@ -9,9 +8,7 @@ export function app_replace_button_symbol_style_valid_if(sb, valid) {
   html_enable_if(sb, valid);
   html_style_background_color_set_if_else(valid, sb, "#00b400ff", "#1e6c1eff");
   html_style_font_color_set_if(valid, sb, "white", "#b9fcb9ff");
-  html_data_set(sb, "test", valid);
-  let suffix = "valid";
-  html_data_set_test_suffix(suffix, valid);
+  html_data_set_test_suffix("valid", valid);
   let h = app_replace_rule_set_highlight();
   app_replace_button_symbol_style_box_shadow(valid, sb, h);
 }
