0f2df0e9a856933e619679381aff8ed0c58efda4
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:37:15 -0400

    function_auto_path ..\love\public\src\property_set_curried_right_2.mjs

--- public/src/property_set_curried_right_2.mjs
+++ public/src/property_set_curried_right_2.mjs
@@ -1,6 +1,8 @@
 import { property_set } from "../../../love/public/src/property_set.mjs";
 export function property_set_curried_right_2(property_name, value) {
-  return function property_set_curried_right_2_result(object) {
-    return property_set(object, property_name, value);
+  let r2 = function property_set_curried_right_2_result(object) {
+    let r = property_set(object, property_name, value);
+    return r;
   };
+  return r2;
 }
