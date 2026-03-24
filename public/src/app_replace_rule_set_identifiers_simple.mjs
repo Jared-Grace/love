7daae38e81aa0d49a6afd93e87bb39d7407c5471
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 17:34:33 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

--- public/src/app_replace_rule_set_identifiers_simple.mjs
+++ public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -1,6 +1,10 @@
 import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_goals.mjs";
 import { app_replace_rule_set_identifiers_simple_rules } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_rules.mjs";
 export function app_replace_rule_set_identifiers_simple() {
+  let abbreviations = {
+    bo: ["", "bo", "oolean literal"],
+    pe: ["", "p", "rimary ", "e", "xpression"],
+  };
   let r = {
     name: "Identifiers simple",
     rules: app_replace_rule_set_identifiers_simple_rules(),
