39f2794e9222e65cba34a3983fa085bb18e65351
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:34:44 -0400

    function_auto_path ..\love\public\src\string_size_1.mjs

--- web/public/src/string_size_1.mjs
+++ web/public/src/string_size_1.mjs
@@ -1,4 +1,5 @@
+import { text_size } from "../../../love/public/src/text_size.mjs";
 export function string_size_1(list) {
-  const s1 = string_size(list) === 1;
+  const s1 = text_size(list) === 1;
   return s1;
 }
