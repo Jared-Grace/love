81a42ed8ba489be4e28ce8123b43fbb6f1e21aa8
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:24:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple.mjs

---
+++ public/src/app_replace_rule_set_strings_simple_rules_base.mjs
@@ -0,0 +1,20 @@
+export function app_replace_rule_set_strings_simple_rules_base() {
+  let r2 = [
+    "ida > idf",
+    "ida > di",
+    "idf > A",
+    "idf > B",
+    "idf > h",
+    "idf > J",
+    "idf > l",
+    "idf > t",
+    "idf > u",
+    "idf > v",
+    "idf > $",
+    "idf > _",
+    "di > 0",
+    "di > 1",
+    "di > 3",
+  ];
+  return r2;
+}
