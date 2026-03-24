c3b733276ec8da0375087a672463e1d5996f330b
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 15:40:20 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_nearley.mjs

--- public/src/app_replace_rule_set_verify_nearley.mjs
+++ public/src/app_replace_rule_set_verify_nearley.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_set_strings_simple } from "../../../love/public/src/app_replace_rule_set_strings_simple.mjs";
 import { js_code_object } from "../../../love/public/src/js_code_object.mjs";
 import { js_code_string } from "../../../love/public/src/js_code_string.mjs";
 import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
@@ -12,6 +13,7 @@ import compile from "nearley/lib/compile.js";
 import generate from "nearley/lib/generate.js";
 import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 export function app_replace_rule_set_verify_nearley(rules) {
+  let r2 = app_replace_rule_set_strings_simple();
   const grammarText = `
 bits -> bits di {% (d) => ({
   left: 'bits',
