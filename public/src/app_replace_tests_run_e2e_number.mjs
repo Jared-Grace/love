1626290b96293a1b8e542779418bc1616f45c925
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:57:39 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_number.mjs

--- public/src/app_replace_tests_run_e2e_number.mjs
+++ public/src/app_replace_tests_run_e2e_number.mjs
@@ -1,3 +1,4 @@
+import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
 import { app_replace_tests_run_e2e_generic } from "../../../love/public/src/app_replace_tests_run_e2e_generic.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
@@ -5,6 +6,10 @@ import { fn_name } from "../../../love/public/src/fn_name.mjs";
 export async function app_replace_tests_run_e2e_number(fn_name) {
   let rs = await function_run_args_none(fn_name);
   let name = property_get(rs, "name");
-  await app_replace_tests_run_e2e_generic([rs], rs, inner);
+  await app_replace_tests_run_e2e_generic(
+    [rs],
+    rs,
+    app_replace_tests_run_e2e_normal_fn,
+  );
   return name;
 }
