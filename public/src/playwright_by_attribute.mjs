2265c2adcae705cf9f40ab49420f882463906dc2
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 21:44:04 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute.mjs

--- public/src/playwright_by_attribute.mjs
+++ public/src/playwright_by_attribute.mjs
@@ -1,11 +1,6 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
 export function playwright_by_attribute(page, name, value) {
-  log(playwright_by_attribute.name, {
-    name,
-    value,
-  });
   let replaced = text_replace(value, '"', '\\"');
   const inside = name + '="' + replaced + '"';
   let r = playwright_by_attribute_named(page, inside);
