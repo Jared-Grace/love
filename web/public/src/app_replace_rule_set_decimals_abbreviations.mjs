43583a5e106ea8a76b2e6e6fa895431138d430b5
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 18:55:05 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_decimals_abbreviations.mjs

--- web/public/src/app_replace_rule_set_decimals_abbreviations.mjs
+++ web/public/src/app_replace_rule_set_decimals_abbreviations.mjs
@@ -1,10 +1,7 @@
+import { app_replace_rule_set_decimals_abbreviations_only } from "../../../love/public/src/app_replace_rule_set_decimals_abbreviations_only.mjs";
 import { app_replace_rule_set_integers_abbreviations } from "../../../love/public/src/app_replace_rule_set_integers_abbreviations.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function app_replace_rule_set_decimals_abbreviations() {
   let i = app_replace_rule_set_integers_abbreviations();
-  let a = {
-    de: ["", "de", "cimal number"],
-  };
-  let ab = object_merge(a, i);
+  let ab = app_replace_rule_set_decimals_abbreviations_only(i);
   return ab;
 }
