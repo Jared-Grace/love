fa4f738bb9d2bb9cdfe3b326eba2533fcb95b013
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:52:28 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_number.mjs

--- public/src/app_replace_tests_run_e2e_number.mjs
+++ public/src/app_replace_tests_run_e2e_number.mjs
@@ -1,8 +1,11 @@
+import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 export async function app_replace_tests_run_e2e_number(fn_name) {
   let result = await function_run_args_none(fn_name);
   let name = property_get(result, "name");
+  async function lambda2() {}
+  await playwright_test_app_dev(app_fn, lambda2);
   return name;
 }
