13141f761980f447c5c99027012ad4958b0683e9
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 14:16:02 -0400

    p

--- web/public/src/app_replace_rule_set_strings_simple.mjs
+++ web/public/src/app_replace_rule_set_strings_simple.mjs
@@ -44,10 +44,10 @@ export function app_replace_rule_set_strings_simple() {
   each(filtered, lambda2);
   let mapped = list_map(filtered, app_replace_end_get);
   let mapped2 = list_map(mapped, list_size);
-  let unique = list_unique(mapped);
+  let unique = list_unique(mapped2);
   list_sort_number(unique);
   log(app_replace_rule_set_strings_simple.name, {
-    filtered,
+    unique,
   });
   let r = {
     name: "Strings simple",
