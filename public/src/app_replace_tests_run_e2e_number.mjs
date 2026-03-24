91f150320e5c343fecb30a9ef36a85f971b9c10e
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:51:01 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_number.mjs

--- public/src/app_replace_tests_run_e2e_number.mjs
+++ public/src/app_replace_tests_run_e2e_number.mjs
@@ -2,4 +2,5 @@ import { function_run_args_none } from "../../../love/public/src/function_run_ar
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 export async function app_replace_tests_run_e2e_number(fn_name) {
   let result = await function_run_args_none(fn_name);
+  return result;
 }
