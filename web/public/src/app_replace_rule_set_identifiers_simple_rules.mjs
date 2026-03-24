e44b25dcf4ece00063ad22b968361b30a0efb721
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 00:25:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple_rules.mjs

--- web/public/src/app_replace_rule_set_identifiers_simple_rules.mjs
+++ web/public/src/app_replace_rule_set_identifiers_simple_rules.mjs
@@ -2,7 +2,14 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_strings_simple_rules_base } from "../../../love/public/src/app_replace_rule_set_strings_simple_rules_base.mjs";
 export function app_replace_rule_set_identifiers_simple_rules() {
   const rules = app_replace_rule_set_strings_simple_rules_base();
-  let r2 = ["id > idf", "id > idf idg", "idg > ida", "idg > ida idg"];
+  let r2 = [
+    "id > idf",
+    "id > idf idg",
+    "idg > ida",
+    "idg > ida idg",
+    "ida > idf",
+    "ida > di",
+  ];
   list_add_multiple(r2, rules);
   return r2;
 }
