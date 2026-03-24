123841679e9e74ad142d62fc83053bdaae18cc0c
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:26:32 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_member_and_access.mjs

--- web/public/src/app_replace_rule_set_expressions_member_and_access.mjs
+++ web/public/src/app_replace_rule_set_expressions_member_and_access.mjs
@@ -2,8 +2,7 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_expressions_primary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_primary_rules.mjs";
 export function app_replace_rule_set_expressions_member_and_access() {
   const rules = [];
-  list_add_multiple(list, items);
-  ["me > pe", "me > me . id", "me > [ e ]"];
+  list_add_multiple(rules, ["me > pe", "me > me . id", "me > [ e ]"]);
   app_replace_rule_set_expressions_primary_rules(rules);
   let r = {
     name: "Expressions member and access",
