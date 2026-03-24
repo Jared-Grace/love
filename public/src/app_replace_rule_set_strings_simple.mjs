3ff617027f3dd753ed79fa712f808f900723785f
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 14:09:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple.mjs

--- public/src/app_replace_rule_set_strings_simple.mjs
+++ public/src/app_replace_rule_set_strings_simple.mjs
@@ -1,3 +1,4 @@
+import { list_sort_number } from "../../../love/public/src/list_sort_number.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -43,6 +44,7 @@ export function app_replace_rule_set_strings_simple() {
   let mapped2 = list_map(goals, app_replace_end_get);
   let mapped = list_map(mapped2, list_size);
   let unique = list_unique(mapped);
+  list_sort_number(list);
   let r = {
     name: "Strings simple",
     rules,
