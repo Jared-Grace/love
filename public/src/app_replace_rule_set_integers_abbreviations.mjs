f183278c85f31e363aab6be162c1dca33494d5c3
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 18:56:41 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviations.mjs

--- public/src/app_replace_rule_set_integers_abbreviations.mjs
+++ public/src/app_replace_rule_set_integers_abbreviations.mjs
@@ -3,11 +3,13 @@ import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_integer_digits_abbreviations } from "../../../love/public/src/app_replace_rule_set_integer_digits_abbreviations.mjs";
 export function app_replace_rule_set_integers_abbreviations() {
   let i = app_replace_rule_set_integer_digits_abbreviations();
-  let r = {
+  app_replace_rule_set_integers_abbreviations_di(i);
+  object_merge(i, {
+    i: ["", "i", "nteger (non-negative whole number)"],
+    p: ["", "p", "ositive digit (numbers 1-9)"],
+  });
+  return {
     i: ["", "i", "nteger (non-negative whole number)"],
     p: ["", "p", "ositive digit (numbers 1-9)"],
   };
-  app_replace_rule_set_integers_abbreviations_di(i);
-  object_merge(i, r);
-  return r;
 }
