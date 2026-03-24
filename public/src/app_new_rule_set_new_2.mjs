3fb3d6601432fa9c4afbe17c5569ffac6a46c524
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:20:57 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,27 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Strings simple",
-    rules: ["a > b"],
+    rules: [
+      "id > idf",
+      "id > idf idg",
+      "idg > ida",
+      "idg > ida idg",
+      "ida > idf",
+      "ida > di",
+      "idf > A",
+      "idf > B",
+      "idf > h",
+      "idf > J",
+      "idf > l",
+      "idf > t",
+      "idf > u",
+      "idf > v",
+      "idf > $",
+      "idf > _",
+      "di > 0",
+      "di > 1",
+      "di > 3",
+    ],
     goals: [
       {
         start: "a",
