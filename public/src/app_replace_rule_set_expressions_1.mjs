a1555623f1cb64496839ad9eef2a2e7ffc81eed2
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 17:27:19 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_1.mjs

--- public/src/app_replace_rule_set_expressions_1.mjs
+++ public/src/app_replace_rule_set_expressions_1.mjs
@@ -1,7 +1,7 @@
 export function app_replace_rule_set_expressions_1() {
   let r = {
     name: "Expressions 1",
-    rules: ["pe > id", "pe > li", "pe > ( e )"],
+    rules: ["pe > id", "pe > li", "pe > ( e )", "li > n", "n > de", "n > i"],
     goals: [
       {
         start: "a",
