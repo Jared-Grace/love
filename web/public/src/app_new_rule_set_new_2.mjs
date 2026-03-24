676c1b54f4df1466c62a53ceae11ce38129e1d1c
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:25:47 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,6 +1,7 @@
 import { app_replace_rule_set_expressions_primary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_primary_rules.mjs";
 export function app_new_rule_set_new_2() {
-  const rules = ["me > pe", "me > me . id", "me > [ e ]"];
+  const rules = [];
+  ["me > pe", "me > me . id", "me > [ e ]"];
   app_replace_rule_set_expressions_primary_rules(rules2);
   let r = {
     name: "Expressions member and access",
