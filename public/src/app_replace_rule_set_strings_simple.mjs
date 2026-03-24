0c3692e7166a211b35f06092ab01582d3a56f7d5
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:23:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple.mjs

--- public/src/app_replace_rule_set_strings_simple.mjs
+++ public/src/app_replace_rule_set_strings_simple.mjs
@@ -1,6 +1,8 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_concat } from "../../../love/public/src/list_concat.mjs";
 export function app_replace_rule_set_strings_simple() {
   const rules = ["st > ida"];
+  list_add_multiple(list, items);
   let concated = list_concat(rules, ["st > ida"]);
   let r = {
     name: "Strings simple",
