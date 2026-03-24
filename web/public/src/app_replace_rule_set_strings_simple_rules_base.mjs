7c494b689c5af33962ccff63bb63bd94b993c279
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 00:26:00 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple_rules_base.mjs

--- web/public/src/app_replace_rule_set_strings_simple_rules_base.mjs
+++ web/public/src/app_replace_rule_set_strings_simple_rules_base.mjs
@@ -1,9 +1,5 @@
 export function app_replace_rule_set_strings_simple_rules_base() {
   let r2 = [
-    "idg > ida",
-    "idg > ida idg",
-    "ida > idf",
-    "ida > di",
     "idf > A",
     "idf > B",
     "idf > h",
