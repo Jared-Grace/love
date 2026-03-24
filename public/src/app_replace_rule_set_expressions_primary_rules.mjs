2001c8a6af7080754179574569e1abe7db13ef96
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:25:11 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_primary_rules.mjs

--- public/src/app_replace_rule_set_expressions_primary_rules.mjs
+++ public/src/app_replace_rule_set_expressions_primary_rules.mjs
@@ -1,3 +1,4 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_replace_rule_set_expressions_primary_rules(rules) {
   let r2 = [
     "pe > id",
@@ -9,5 +10,6 @@ export function app_replace_rule_set_expressions_primary_rules(rules) {
     "li > st",
     "li > null",
   ];
+  list_add_multiple(list, items);
   return r2;
 }
