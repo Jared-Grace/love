f04672f16d190e659ad13ffc094544fa38cf41a8
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:20:04 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

--- public/src/app_replace_rule_set_identifiers_simple.mjs
+++ public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -1,58 +1,10 @@
+import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_goals.mjs";
 import { app_replace_rule_set_identifiers_simple_rules } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_rules.mjs";
 export function app_replace_rule_set_identifiers_simple() {
   let r = {
     name: "Identifiers simple",
     rules: app_replace_rule_set_identifiers_simple_rules(),
-    goals: [
-      {
-        start: "id",
-        end: "_",
-      },
-      {
-        start: "id",
-        end: "idf ida",
-      },
-      {
-        start: "idf ida",
-        end: "$ 1",
-      },
-      {
-        start: "id",
-        end: "t 3",
-      },
-      {
-        start: "id",
-        end: "idf ida ida",
-      },
-      {
-        start: "idf ida ida",
-        end: "l u v",
-      },
-      {
-        start: "id",
-        end: "l u v",
-      },
-      {
-        start: "id",
-        end: "idf ida ida ida",
-      },
-      {
-        start: "idf ida ida ida",
-        end: "idf idf idf idf",
-      },
-      {
-        start: "idf idf idf idf",
-        end: "A B B A",
-      },
-      {
-        start: "idf ida ida ida",
-        end: "idf di idf idf",
-      },
-      {
-        start: "idf di idf idf",
-        end: "J 0 $ h",
-      },
-    ],
+    goals: app_replace_rule_set_identifiers_simple_goals(),
   };
   return r;
 }
