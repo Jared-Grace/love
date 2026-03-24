9e7150db6c14bc8dab937bf7d5083cb2cbe87c0c
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:31:48 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple.mjs

--- public/src/app_replace_rule_set_strings_simple.mjs
+++ public/src/app_replace_rule_set_strings_simple.mjs
@@ -1,6 +1,5 @@
+import { app_replace_end_get } from "../../../love/public/src/app_replace_end_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { app_replace_rule_parse } from "../../../love/public/src/app_replace_rule_parse.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_goals.mjs";
 import { app_replace_rule_set_strings_simple_rules_base } from "../../../love/public/src/app_replace_rule_set_strings_simple_rules_base.mjs";
@@ -10,10 +9,9 @@ export function app_replace_rule_set_strings_simple() {
   let concated = list_concat(["st > ida"], rules);
   let goals = app_replace_rule_set_identifiers_simple_goals();
   function lambda(g) {
-    let end2 = property_get(g, "end");
-    let v = app_replace_rule_parse(end2);
+    let end = app_replace_end_get(goal);
     log(app_replace_rule_set_strings_simple.name, {
-      v,
+      end,
     });
   }
   let filtered = list_filter(goals, lambda);
