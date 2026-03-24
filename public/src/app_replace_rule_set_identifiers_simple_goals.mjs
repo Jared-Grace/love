2137600c0ca99533185d7f90f9782337b8867eb1
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:20:04 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

---
+++ public/src/app_replace_rule_set_identifiers_simple_goals.mjs
@@ -0,0 +1,53 @@
+export function app_replace_rule_set_identifiers_simple_goals() {
+  let r2 = [
+    {
+      start: "id",
+      end: "_",
+    },
+    {
+      start: "id",
+      end: "idf ida",
+    },
+    {
+      start: "idf ida",
+      end: "$ 1",
+    },
+    {
+      start: "id",
+      end: "t 3",
+    },
+    {
+      start: "id",
+      end: "idf ida ida",
+    },
+    {
+      start: "idf ida ida",
+      end: "l u v",
+    },
+    {
+      start: "id",
+      end: "l u v",
+    },
+    {
+      start: "id",
+      end: "idf ida ida ida",
+    },
+    {
+      start: "idf ida ida ida",
+      end: "idf idf idf idf",
+    },
+    {
+      start: "idf idf idf idf",
+      end: "A B B A",
+    },
+    {
+      start: "idf ida ida ida",
+      end: "idf di idf idf",
+    },
+    {
+      start: "idf di idf idf",
+      end: "J 0 $ h",
+    },
+  ];
+  return r2;
+}
