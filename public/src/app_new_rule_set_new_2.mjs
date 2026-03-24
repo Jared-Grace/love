785de226a184195c05c39578a07880a2ff40390b
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:19:45 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,7 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Expressions member and access",
-    rules: ["me > pe", "me > . id", "me > [ e ]"],
+    rules: ["me > pe", "me > me . id", "me > [ e ]"],
     goals: [
       {
         start: "a",
