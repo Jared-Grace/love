81d4054161a5bd03219d19d2918e933006c8ac89
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:20:42 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_primary.mjs

---
+++ public/src/app_replace_rule_set_expressions_primary_rules.mjs
@@ -0,0 +1,13 @@
+export function app_replace_rule_set_expressions_primary_rules() {
+  let r2 = [
+    "pe > id",
+    "pe > li",
+    "pe > ( e )",
+    "li > n",
+    "n > de",
+    "n > i",
+    "li > st",
+    "li > null",
+  ];
+  return r2;
+}
