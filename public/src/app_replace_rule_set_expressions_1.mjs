5b42cf112724aef5ef15088a3353fa534cfb3b03
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 18:27:10 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_1.mjs

--- public/src/app_replace_rule_set_expressions_1.mjs
+++ public/src/app_replace_rule_set_expressions_1.mjs
@@ -1,3 +1,5 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { app_replace_rule_set_boolean_literal_abbreviations } from "../../../love/public/src/app_replace_rule_set_boolean_literal_abbreviations.mjs";
 import { app_replace_rule_set_boolean_literal_rules } from "../../../love/public/src/app_replace_rule_set_boolean_literal_rules.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_replace_rule_set_expressions_1() {
@@ -10,11 +12,13 @@ export function app_replace_rule_set_expressions_1() {
     "n > i",
     "li > st",
   ];
+  let b = app_replace_rule_set_boolean_literal_abbreviations();
   let abbreviations = {
     bo: ["", "bo", "oolean literal"],
     pe: ["", "p", "rimary ", "e", "xpression"],
     id: ["", "id", "entifier"],
   };
+  let to2 = object_merge(to, from);
   let items = app_replace_rule_set_boolean_literal_rules();
   list_add_multiple(rules, items);
   let r = {
