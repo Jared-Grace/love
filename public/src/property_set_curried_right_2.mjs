dee22f86243541ef2a8f7d7d731c13eda964bc02
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:45:18 -0400

    de property_set_curried_right_2

--- public/src/property_set_curried_right_2.mjs
+++
@@ -1,8 +0,0 @@
-import { property_set } from "../../../love/public/src/property_set.mjs";
-export function property_set_curried_right_2(property_name, value) {
-  let r2 = function property_set_curried_right_2_result(object) {
-    let r = property_set(object, property_name, value);
-    return r;
-  };
-  return r2;
-}
