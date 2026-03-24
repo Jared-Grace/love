817ebcce84600b870ab188dd231972758902bdf6
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:01:17 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

---
+++ public/src/app_replace_rule_set_identifiers_simple_abbreviations.mjs
@@ -0,0 +1,23 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+export function app_replace_rule_set_identifiers_simple_abbreviations(
+  abbreviations,
+) {
+  let abbreviations2 = {
+    idf: [
+      "",
+      "id",
+      "entifier ",
+      "f",
+      "irst: symbol that can be used as the first symbol of an identifier",
+    ],
+    ida: [
+      "",
+      "id",
+      "entifier ",
+      "a",
+      "ny: symbol that can be used as any symbol of an identifier",
+    ],
+    di: ["", "di", "git sample: a few choices of digits"],
+  };
+  let to2 = object_merge(abbreviations, abbreviations2);
+}
