a0d3c1039f68956aad4d9d5192fc62dd78814aae
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:27:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_member_and_access.mjs

---
+++ web/public/src/app_replace_rule_set_expressions_member_and_access_rules.mjs
@@ -0,0 +1,8 @@
+import { app_replace_rule_set_expressions_primary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_primary_rules.mjs";
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+export function app_replace_rule_set_expressions_member_and_access_rules(
+  rules,
+) {
+  list_add_multiple(rules, ["me > pe", "me > me . id", "me > [ e ]"]);
+  app_replace_rule_set_expressions_primary_rules(rules);
+}
