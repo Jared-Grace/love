0319aadb8275a2512b1d1de1758c3c99eaf8bbbb
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 14:33:35 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple.mjs

--- public/src/app_replace_rule_set_strings_simple.mjs
+++ public/src/app_replace_rule_set_strings_simple.mjs
@@ -23,9 +23,9 @@ export function app_replace_rule_set_strings_simple() {
   const root = "st";
   let delimeter = '"';
   let character = "ida";
-  let padded22 = text_pad_nested(s, padding, padding2);
+  let padded22 = text_pad_nested("stg", " ", delimeter);
   const rules = [
-    root + " > " + delimeter + " stg " + delimeter,
+    root + " > " + delimeter + padded22 + delimeter,
     "stg > " + character,
     "stg > " + character + " stg",
   ];
