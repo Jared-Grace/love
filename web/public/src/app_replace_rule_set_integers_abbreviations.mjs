d565b5dabed542d01ee31b118f569aa5942ddc43
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 18:57:12 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviations.mjs

--- web/public/src/app_replace_rule_set_integers_abbreviations.mjs
+++ web/public/src/app_replace_rule_set_integers_abbreviations.mjs
@@ -1,12 +1,11 @@
+import { app_replace_rule_set_integers_abbreviation_i } from "../../../love/public/src/app_replace_rule_set_integers_abbreviation_i.mjs";
 import { app_replace_rule_set_integers_abbreviations_di } from "../../../love/public/src/app_replace_rule_set_integers_abbreviations_di.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_integer_digits_abbreviations } from "../../../love/public/src/app_replace_rule_set_integer_digits_abbreviations.mjs";
 export function app_replace_rule_set_integers_abbreviations() {
   let i = app_replace_rule_set_integer_digits_abbreviations();
   app_replace_rule_set_integers_abbreviations_di(i);
-  object_merge(i, {
-    i: ["", "i", "nteger (non-negative whole number)"],
-  });
+  app_replace_rule_set_integers_abbreviation_i(i);
   object_merge(i, {
     p: ["", "p", "ositive digit (numbers 1-9)"],
   });
