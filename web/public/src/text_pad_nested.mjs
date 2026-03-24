049ce7125dab434b184371c5f41766f3a8ac3114
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 14:33:01 -0400

    function_auto_path ..\love\public\src\text_pad_nested.mjs

--- web/public/src/text_pad_nested.mjs
+++ web/public/src/text_pad_nested.mjs
@@ -1,6 +1,6 @@
 import { text_pad } from "../../../love/public/src/text_pad.mjs";
 export function text_pad_nested(s, padding, padding2) {
-  let padded = text_pad(s, padding);
-  let padded2 = text_pad(padded, padding2);
-  return padded2;
+  let padded_inner = text_pad(s, padding);
+  let padded = text_pad(padded_inner, padding2);
+  return padded;
 }
