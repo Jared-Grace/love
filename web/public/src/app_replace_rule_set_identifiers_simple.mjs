d1432d7187fb7fd1849e7a0d07d4f83c2126eef7
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:01:17 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

--- web/public/src/app_replace_rule_set_identifiers_simple.mjs
+++ web/public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -1,4 +1,4 @@
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { app_replace_rule_set_identifiers_simple_abbreviations } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_abbreviations.mjs";
 import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_goals.mjs";
 import { app_replace_rule_set_identifiers_simple_rules } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_rules.mjs";
 export function app_replace_rule_set_identifiers_simple() {
@@ -6,24 +6,7 @@ export function app_replace_rule_set_identifiers_simple() {
     id: ["", "id", "entifier"],
     idg: ["", "id", "entifier ", "g", "rower"],
   };
-  let abbreviations2 = {
-    idf: [
-      "",
-      "id",
-      "entifier ",
-      "f",
-      "irst: symbol that can be used as the first symbol of an identifier",
-    ],
-    ida: [
-      "",
-      "id",
-      "entifier ",
-      "a",
-      "ny: symbol that can be used as any symbol of an identifier",
-    ],
-    di: ["", "di", "git sample: a few choices of digits"],
-  };
-  let to2 = object_merge(abbreviations, abbreviations2);
+  app_replace_rule_set_identifiers_simple_abbreviations(abbreviations);
   let r = {
     name: "Identifiers simple",
     rules: app_replace_rule_set_identifiers_simple_rules(),
