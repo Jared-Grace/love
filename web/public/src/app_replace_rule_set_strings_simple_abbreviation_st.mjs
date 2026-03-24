2ca23fb45975131a9b1a88ad0ab19c33a9e874e5
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 19:04:13 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple_abbreviation_st.mjs

--- web/public/src/app_replace_rule_set_strings_simple_abbreviation_st.mjs
+++ web/public/src/app_replace_rule_set_strings_simple_abbreviation_st.mjs
@@ -1,5 +1,7 @@
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-export function app_replace_rule_set_strings_simple_st(abbreviations) {
+export function app_replace_rule_set_strings_simple_abbreviation_st(
+  abbreviations,
+) {
   object_merge(abbreviations, {
     st: ["", "st", "ring"],
   });
