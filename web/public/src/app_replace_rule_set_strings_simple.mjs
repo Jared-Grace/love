b2b43612e976084a29f7253784d33bc3de285abf
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:48:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_strings_simple.mjs

--- web/public/src/app_replace_rule_set_strings_simple.mjs
+++ web/public/src/app_replace_rule_set_strings_simple.mjs
@@ -1,3 +1,4 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { text_pad } from "../../../love/public/src/text_pad.mjs";
 import { text_pad_space } from "../../../love/public/src/text_pad_space.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
@@ -11,13 +12,12 @@ import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/pub
 import { app_replace_rule_set_strings_simple_rules_base } from "../../../love/public/src/app_replace_rule_set_strings_simple_rules_base.mjs";
 import { list_concat } from "../../../love/public/src/list_concat.mjs";
 export function app_replace_rule_set_strings_simple() {
-  const rules = app_replace_rule_set_strings_simple_rules_base();
+  const extra = app_replace_rule_set_strings_simple_rules_base();
   const root = "st";
   let delimeter = '"';
-  let concated = list_concat(
-    [root + " > idg", root + " > " + delimeter],
-    rules,
-  );
+  const rules = [root + " > idg", root + " > " + delimeter];
+  list_add_multiple(list, items);
+  let concated = list_concat(rules, extra);
   let goals = app_replace_rule_set_identifiers_simple_goals();
   function lambda(g) {
     let end = app_replace_end_get(g);
