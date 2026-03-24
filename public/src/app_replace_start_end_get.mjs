497213d98204b4d9e8921cd2c62e4669e61c0e96
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:31:38 -0400

    function_auto_path ..\love\public\src\app_replace_start_end_get.mjs

--- public/src/app_replace_start_end_get.mjs
+++ public/src/app_replace_start_end_get.mjs
@@ -1,10 +1,10 @@
+import { app_replace_end_get } from "../../../love/public/src/app_replace_end_get.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_replace_start_end_get(goal) {
   let start_value = property_get(goal, "start");
   let start = text_split_space(start_value);
-  let end_value = property_get(goal, "end");
-  let end = text_split_space(end_value);
+  let end = app_replace_end_get(goal);
   let r4 = {
     end,
     start,
