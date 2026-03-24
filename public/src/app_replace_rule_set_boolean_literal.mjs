fa3b2b7b9419bf8856539cddc3e1e988c3301cc3
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 18:26:41 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_boolean_literal.mjs

--- public/src/app_replace_rule_set_boolean_literal.mjs
+++ public/src/app_replace_rule_set_boolean_literal.mjs
@@ -1,7 +1,11 @@
 import { app_replace_rule_set_boolean_literal_rules } from "../../../love/public/src/app_replace_rule_set_boolean_literal_rules.mjs";
 export function app_replace_rule_set_boolean_literal() {
+  let abbreviations = {
+    bo: ["", "bo", "oolean literal"],
+  };
   let r = {
     name: "Boolean Literal",
+    abbreviations,
     rules: app_replace_rule_set_boolean_literal_rules(),
     goals: [
       {
