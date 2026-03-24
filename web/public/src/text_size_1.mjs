463cd8a2dfb3b357ec38b8b9a37bd577c73ba279
Grace-Jared <christrosetolife@gmail.com>
2026-03-23 22:34:56 -0400

    rr string_size_1 string text

--- web/public/src/text_size_1.mjs
+++ web/public/src/text_size_1.mjs
@@ -1,5 +1,5 @@
 import { text_size } from "../../../love/public/src/text_size.mjs";
-export function string_size_1(list) {
+export function text_size_1(list) {
   const s1 = text_size(list) === 1;
   return s1;
 }
