cec31501e28ce9b964885f6a3cd11c87783f92eb
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 18:01:36 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_generic.mjs

--- web/public/src/app_replace_tests_run_e2e_generic.mjs
+++ web/public/src/app_replace_tests_run_e2e_generic.mjs
@@ -1,6 +1,5 @@
 import { playwright_by_attribute_test_exists_assert } from "../../../love/public/src/playwright_by_attribute_test_exists_assert.mjs";
 import { app_replace_rule_set_success_attribute_completed } from "../../../love/public/src/app_replace_rule_set_success_attribute_completed.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
 import { json_equal } from "../../../love/public/src/json_equal.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
@@ -63,7 +62,7 @@ export async function app_replace_tests_run_e2e_generic(
       let last_goal = false;
       let eq2 = json_equal(rule_set, last_rs);
       if (eq2) {
-        if (equal(goal, goal_last)) {
+        if (json_equal(goal, goal_last)) {
           last_goal = true;
         }
       }
