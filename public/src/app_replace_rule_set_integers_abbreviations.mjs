c0316e10c39287277ac1b66f10e3fadb870b9c50
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 00:48:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviation_in.mjs

--- public/src/app_replace_rule_set_integers_abbreviations.mjs
+++ public/src/app_replace_rule_set_integers_abbreviations.mjs
@@ -1,11 +1,11 @@
-import { app_replace_rule_set_integers_abbreviation_i } from "../../../love/public/src/app_replace_rule_set_integers_abbreviation_i.mjs";
+import { app_replace_rule_set_integers_abbreviation_in } from "../../../love/public/src/app_replace_rule_set_integers_abbreviation_in.mjs";
 import { app_replace_rule_set_integers_abbreviations_di } from "../../../love/public/src/app_replace_rule_set_integers_abbreviations_di.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_integer_digits_abbreviations } from "../../../love/public/src/app_replace_rule_set_integer_digits_abbreviations.mjs";
 export function app_replace_rule_set_integers_abbreviations() {
   let i = app_replace_rule_set_integer_digits_abbreviations();
   app_replace_rule_set_integers_abbreviations_di(i);
-  app_replace_rule_set_integers_abbreviation_i(i);
+  app_replace_rule_set_integers_abbreviation_in(i);
   object_merge(i, {
     p: ["", "p", "ositive ", "i", "nteger digit (numbers 1-9)"],
   });
