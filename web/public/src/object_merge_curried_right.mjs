464a15a29eab2efe92fd301f83652fb87289a3dc
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:49:55 -0400

    function_auto_path ..\love\public\src\object_merge_curried_right.mjs

--- web/public/src/object_merge_curried_right.mjs
+++ web/public/src/object_merge_curried_right.mjs
@@ -1,6 +1,8 @@
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function object_merge_curried_right(from) {
-  return function object_merge_curried_right_result(to) {
-    return object_merge(to, from);
+  let r = function object_merge_curried_right_result(to) {
+    let to2 = object_merge(to, from);
+    return to2;
   };
+  return r;
 }
