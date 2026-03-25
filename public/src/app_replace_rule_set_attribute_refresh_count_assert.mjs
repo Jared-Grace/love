23185df17260a857fa42952f6dacd916be0e9785
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:40:58 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_attribute_refresh_count_assert.mjs

--- public/src/app_replace_rule_set_attribute_refresh_count_assert.mjs
+++ public/src/app_replace_rule_set_attribute_refresh_count_assert.mjs
@@ -1,10 +1,14 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { playwright_by_attribute_test_exists_assert } from "../../../love/public/src/playwright_by_attribute_test_exists_assert.mjs";
 import { app_replace_rule_set_attribute_refresh_count } from "../../../love/public/src/app_replace_rule_set_attribute_refresh_count.mjs";
 export async function app_replace_rule_set_attribute_refresh_count_assert(
   refresh_count,
   page,
 ) {
-  refresh_count++;$L$refresh_count
+  refresh_count++;
+  log(app_replace_rule_set_attribute_refresh_count_assert.name, {
+    refresh_count,
+  });
   let a = app_replace_rule_set_attribute_refresh_count(refresh_count);
   await playwright_by_attribute_test_exists_assert(page, a);
   return refresh_count;
