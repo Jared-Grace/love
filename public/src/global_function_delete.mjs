90cec951873a3df9d068f412680a0526dbf6ea7c
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:14:15 -0400

    ao property_get

--- public/src/global_function_delete.mjs
+++ public/src/global_function_delete.mjs
@@ -1,6 +1,8 @@
 import { property_delete } from "../../../love/public/src/property_delete.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 export function global_function_delete(fn) {
+  console.log(global_function_delete)
+  process.exit()
   let global = global_get();
   property_delete(global, fn.name);
 }
