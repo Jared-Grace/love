686549110c5b58357eeb43caa2656a8e10537a8b
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:30:41 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -6,7 +6,7 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 export function app_new_rule_set_new_2() {
   const rules = [];
   app_replace_rule_set_expressions_multiplicative_rules(rules);
-  list_add_multiple(rules, ["ae > mue", "ae > ae mo mue", "ao > +", "ao > -"]);
+  list_add_multiple(rules, ["ae > mue", "ae > ae ao mue", "ao > +", "ao > -"]);
   list_add(rules, "ex > ae");
   let abbreviations = {};
   app_replace_rule_set_expressions_multiplicative_abbreviations(abbreviations);
