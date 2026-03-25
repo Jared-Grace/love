d586f5c9c935bc036e0a30aa61d8ad2daea21d3c
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:05:09 -0400

    p

--- web/public/src/app_replace_rule_set_expressions_multiplicative.mjs
+++ web/public/src/app_replace_rule_set_expressions_multiplicative.mjs
@@ -18,7 +18,7 @@ export function app_replace_rule_set_expressions_multiplicative() {
     goals: [
       {
         start: "mue",
-        end: "li mo li",
+        end: "mue mo ue",
       },
     ],
   };
