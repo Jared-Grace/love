e6c60e608417e00a5ec58d54abefb9a43621c9f5
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:01:09 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_unary.mjs

---
+++ web/public/src/app_replace_rule_set_expressions_unary_rules.mjs
@@ -0,0 +1,13 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+import { app_replace_rule_set_expressions_function_calls_rules } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_rules.mjs";
+export function app_replace_rule_set_expressions_unary_rules(rules) {
+  app_replace_rule_set_expressions_function_calls_rules(rules);
+  list_add_multiple(rules, [
+    "ue > ce",
+    "ue > uo ue",
+    "uo > !",
+    "uo > -",
+    "uo > +",
+    "uo > typeof",
+  ]);
+}
