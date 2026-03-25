a456905bdf2bf059061d64f530056182a0fbedea
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:47:18 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal.mjs

--- web/public/src/list_map_squash.mjs
+++ web/public/src/list_map_squash.mjs
@@ -1,7 +1,7 @@
 import { list_squash } from "../../../love/public/src/list_squash.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function list_map_squash(properties, fn) {
-  let mapped = list_map(properties, fn);
+export function list_map_squash(properties, lambda$item) {
+  let mapped = list_map(properties, lambda$item);
   let squashed = list_squash(mapped);
   return squashed;
 }
