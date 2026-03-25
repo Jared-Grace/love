390f9887656d0305eaaa7573b5f68f0a44fafe76
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 00:48:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviation_in.mjs

--- public/src/app_replace_rule_set_integers_abbreviation_in.mjs
+++ public/src/app_replace_rule_set_integers_abbreviation_in.mjs
@@ -1,5 +1,5 @@
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-export function app_replace_rule_set_integers_abbreviation_i(i) {
+export function app_replace_rule_set_integers_abbreviation_in(i) {
   object_merge(i, {
     i: ["", "i", "nteger (non-negative whole number)"],
   });
