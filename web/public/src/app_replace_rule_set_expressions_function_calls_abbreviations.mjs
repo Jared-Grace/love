73a508cd2f1bef5ac5643dd1e4410706175752d8
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:44:21 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_member_and_access_abbreviations.mjs

--- web/public/src/app_replace_rule_set_expressions_function_calls_abbreviations.mjs
+++ web/public/src/app_replace_rule_set_expressions_function_calls_abbreviations.mjs
@@ -3,7 +3,7 @@ import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function app_replace_rule_set_expressions_function_calls_abbreviations(
   abbreviations,
 ) {
-  app_replace_rule_set_expressions_primary_abbreviations(abbreviations);
+  app_replace_rule_set_expressions_member_and_access_abbreviations(abbreviations);
   let to2 = object_merge(abbreviations, {
     ce: ["", "c", "all ", "e", "xpression"],
     arg: ["", "ar", "arguments ", "g", "rower"],
