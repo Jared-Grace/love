17069fc16d0b12b91f5dc0751379b63c803b4e8c
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:59:40 -0400

    function_auto_path ..\love\public\src\object_wrap_curried_right.mjs

--- public/src/object_wrap_curried_right.mjs
+++ public/src/object_wrap_curried_right.mjs
@@ -1,6 +1,8 @@
 import { object_wrap } from "../../../love/public/src/object_wrap.mjs";
 export function object_wrap_curried_right(property_name) {
-  return function object_wrap_curried_right_result(goal) {
-    return object_wrap(goal, property_name);
+  let r = function object_wrap_curried_right_result(goal) {
+    let r2 = object_wrap(goal, property_name);
+    return r2;
   };
+  return r;
 }
