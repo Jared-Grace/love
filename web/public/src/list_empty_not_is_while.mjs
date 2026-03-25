820479a777269b4c0dfc991032a109e76c449a14
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 18:14:15 -0400

    function_auto_path ..\love\public\src\list_empty_not_is_while.mjs

--- web/public/src/list_empty_not_is_while.mjs
+++ web/public/src/list_empty_not_is_while.mjs
@@ -1,1 +1,4 @@
-export function list_empty_not_is_while() {}
+import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
+export function list_empty_not_is_while() {
+  while (list_empty_not_is(queue)) {}
+}
