5ed291780630ebeba702ecd92712f9ceccef5a4a
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:35:33 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_function_calls.mjs

--- web/public/src/app_replace_rule_set_expressions_function_calls.mjs
+++ web/public/src/app_replace_rule_set_expressions_function_calls.mjs
@@ -1,3 +1,5 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { app_replace_rule_set_expressions_primary_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_expressions_member_and_access_rules } from "../../../love/public/src/app_replace_rule_set_expressions_member_and_access_rules.mjs";
@@ -12,6 +14,9 @@ export function app_replace_rule_set_expressions_function_calls() {
     "arg > e",
   ]);
   list_add(rules, "e > ce");
+  let abbreviations = {};
+  app_replace_rule_set_expressions_primary_abbreviations(abbreviations);
+  let to2 = object_merge(to, from);
   let r = {
     name: "Expressions function calls",
     rules,
