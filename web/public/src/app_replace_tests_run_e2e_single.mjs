f0c12e5c4747c91ee7d05b94ef8f6f4fb0272cb3
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 18:08:39 -0400

    rrl app_replace_tests_run_e2e_generic browser

--- web/public/src/app_replace_tests_run_e2e_single.mjs
+++ web/public/src/app_replace_tests_run_e2e_single.mjs
@@ -1,12 +1,12 @@
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
-import { app_replace_tests_run_e2e_generic } from "../../../love/public/src/app_replace_tests_run_e2e_generic.mjs";
+import { app_replace_tests_run_e2e_browser } from "../../../love/public/src/app_replace_tests_run_e2e_browser.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 export async function app_replace_tests_run_e2e_single(fn_name) {
   let rs = await function_run_args_none(fn_name);
   let name = property_get(rs, "name");
-  await app_replace_tests_run_e2e_generic(
+  await app_replace_tests_run_e2e_browser(
     rs,
     goal,
     app_replace_tests_run_e2e_normal_fn,
