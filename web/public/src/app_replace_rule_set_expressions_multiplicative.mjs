b2f1139426d49c16104640cc85ba9a0a96af67f3
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:24:28 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_multiplicative.mjs

--- web/public/src/app_replace_rule_set_expressions_multiplicative.mjs
+++ web/public/src/app_replace_rule_set_expressions_multiplicative.mjs
@@ -1,12 +1,10 @@
-import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+import { app_replace_rule_set_expressions_multiplicative_rules } from "../../../love/public/src/app_replace_rule_set_expressions_multiplicative_rules.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expressions_unary_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_unary_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-import { app_replace_rule_set_expressions_unary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_unary_rules.mjs";
 export function app_replace_rule_set_expressions_multiplicative() {
   const rules = [];
-  app_replace_rule_set_expressions_unary_rules(rules);
-  list_add_multiple(rules, ["mue > ue", "mue > mue mo ue", "mo > *", "mo > /"]);
+  app_replace_rule_set_expressions_multiplicative_rules(rules);
   list_add(rules, "ex > mue");
   let abbreviations = {};
   app_replace_rule_set_expressions_unary_abbreviations(abbreviations);
