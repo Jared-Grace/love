5efb43a7c83f3ef3d08fdae37cfcf91605f8d28c
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:02:03 -0400

    fws app_replace_rule_set_verify named

---
+++ web/public/src/app_replace_rule_set_verify_named.mjs
@@ -0,0 +1,4 @@
+import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
+export function app_replace_rule_set_verify_named(rule_set) {
+  return app_replace_rule_set_verify(rule_set);
+}
