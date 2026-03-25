8913a3d4d1dd07b535396fa8baa549a0269549bc
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 16:50:51 -0400

    function_name_to_path_search sb

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,2 +1,5 @@
+import { server_url } from "../../../love/public/src/server_url.mjs";
+import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export async function sandbox() {
-    log_keep(sandbox.name, "Static server running at: " + server_url());}
+  log_keep(sandbox.name, "Static server running at: " + server_url());
+}
