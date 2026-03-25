f2c181ffc0a9e67654ed31890d6ef745197904e9
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 13:27:31 -0400

    rrl app_replace_tests_run_e2e_number single

--- web/public/src/app_replace_tests_run_e2e_single.mjs
+++ web/public/src/app_replace_tests_run_e2e_single.mjs
@@ -3,7 +3,7 @@ import { app_replace_tests_run_e2e_generic } from "../../../love/public/src/app_
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
-export async function app_replace_tests_run_e2e_number(fn_name) {
+export async function app_replace_tests_run_e2e_single(fn_name) {
   let rs = await function_run_args_none(fn_name);
   let name = property_get(rs, "name");
   await app_replace_tests_run_e2e_generic(
