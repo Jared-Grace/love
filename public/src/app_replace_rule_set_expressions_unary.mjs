6c2a99f85310e02f7f71b1bc80b6cc02763d26ef
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:01:38 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_unary.mjs

--- public/src/app_replace_rule_set_expressions_unary.mjs
+++ public/src/app_replace_rule_set_expressions_unary.mjs
@@ -1,17 +1,12 @@
+import { app_replace_rule_set_expressions_unary_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_unary_abbreviations.mjs";
 import { app_replace_rule_set_expressions_unary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_unary_rules.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { app_replace_rule_set_expressions_function_calls_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_replace_rule_set_expressions_unary() {
   const rules = [];
   app_replace_rule_set_expressions_unary_rules(rules);
   list_add(rules, "ex > ue");
   let abbreviations = {};
-  app_replace_rule_set_expressions_function_calls_abbreviations(abbreviations);
-  object_merge(abbreviations, {
-    ue: ["", "u", "nary ", "e", "xpression"],
-    uo: ["", "u", "nary ", "o", "perator"],
-  });
+  app_replace_rule_set_expressions_unary_abbreviations(abbreviations);
   let r = {
     name: "Expressions unary",
     rules,
