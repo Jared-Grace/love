07bfe8b1fc782dec0bbd6c97871250f5759c1bf5
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:21:42 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,7 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Expressions function calls",
-    rules: ["a > b"],
+    rules: ["ce > me", "ce > ce ( )"],
     goals: [
       {
         start: "a",
