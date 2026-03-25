acb46b6c1f88f7fe2e1fb914596757c823e88cdc
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 22:46:13 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_unary.mjs

--- web/public/src/app_replace_rule_set_expressions_unary.mjs
+++ web/public/src/app_replace_rule_set_expressions_unary.mjs
@@ -1,9 +1,12 @@
+import { app_replace_rule_set_expressions_function_calls_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_expressions_function_calls_rules } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_rules.mjs";
 export function app_replace_rule_set_expressions_unary() {
   const rules = [];
   app_replace_rule_set_expressions_function_calls_rules(rules);
   list_add(rules, "e > ue");
+  let abbreviations = {};
+  app_replace_rule_set_expressions_function_calls_abbreviations(abbreviations);
   let r = {
     name: "Expressions unary",
     rules: [
