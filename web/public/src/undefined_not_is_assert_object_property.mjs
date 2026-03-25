912ddf9e4d0d3642b3fe6ed44b6d7dd5a41721d7
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:14:33 -0400

    rpd undefined_not_is_assert_lambda_object_property lambda

--- web/public/src/undefined_not_is_assert_object_property.mjs
+++ web/public/src/undefined_not_is_assert_object_property.mjs
@@ -1,5 +1,5 @@
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
-export function undefined_not_is_assert_lambda_object_property(
+export function undefined_not_is_assert_object_property(
   value,
   object,
   property_name,
