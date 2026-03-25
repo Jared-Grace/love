a6ee767ae38b83a32a1090ffd2aeb05a283dde0e
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:29:28 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_function_calls.mjs

--- public/src/app_replace_rule_set_expressions_function_calls.mjs
+++ public/src/app_replace_rule_set_expressions_function_calls.mjs
@@ -38,7 +38,7 @@ export function app_replace_rule_set_expressions_function_calls() {
       },
       {
         start: "id ( e )",
-        end: "id ( arg )",
+        end: "id ( e, e )",
       },
     ],
   };
