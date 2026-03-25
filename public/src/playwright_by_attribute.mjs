dedf793bf4c3720d95c5a25be01d058a71062660
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:51:31 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute.mjs

--- public/src/playwright_by_attribute.mjs
+++ public/src/playwright_by_attribute.mjs
@@ -5,16 +5,18 @@ export async function playwright_by_attribute(page, name, value) {
     name,
     value,
   });
-  async function lambda2() {}
-  let result = await retry_fast(lambda2);
-  await page.waitForSelector(`[${name}]`);
-  const elements = await page.locator(`[${name}]`).elementHandles();
-  const filtered = [];
-  for (const el of elements) {
-    const a = await el.getAttribute(name);
-    if (a === value) {
-      filtered.push(el);
+  async function lambda2() {
+    await page.waitForSelector(`[${name}]`);
+    const elements = await page.locator(`[${name}]`).elementHandles();
+    const filtered = [];
+    for (const el of elements) {
+      const a = await el.getAttribute(name);
+      if (a === value) {
+        filtered.push(el);
+      }
+      return filtered;
     }
   }
-  return filtered;
+  let result = await retry_fast(lambda2);
+  return result;
 }
