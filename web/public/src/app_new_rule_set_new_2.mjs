942f46dc2865777d8b07eb9cf9cdf80f22d75643
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:01:51 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,4 +1,12 @@
+import { app_replace_rule_set_expressions_unary_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_unary_abbreviations.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_replace_rule_set_expressions_unary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_unary_rules.mjs";
 export function app_new_rule_set_new_2() {
+  const rules = [];
+  app_replace_rule_set_expressions_unary_rules(rules);
+  list_add(rules, "ex > mue");
+  let abbreviations = {};
+  app_replace_rule_set_expressions_unary_abbreviations(abbreviations);
   let r = {
     name: "Expressions multiplicative",
     rules: ["mue > ue", "mue > mue mo ue", "mo > *", "mo > /"],
