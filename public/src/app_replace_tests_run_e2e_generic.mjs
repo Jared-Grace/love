d41d7260a0d85db8d67ac00e1da5a89e5de115f9
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 13:39:26 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_generic.mjs

--- public/src/app_replace_tests_run_e2e_generic.mjs
+++ public/src/app_replace_tests_run_e2e_generic.mjs
@@ -67,7 +67,8 @@ export async function app_replace_tests_run_e2e_generic(
         await each_async(path, each_step);
         let eq2 = json_equal(rule_set, last_rs);
         if (not(eq2)) {
-          if (equal(goal, goal_last)) {$s
+          if (equal(goal, goal_last)) {
+            $s;
             last = true;
             let name = app_replace_rule_set_success_attribute_next();
             await playwright_by_attribute_test_click(page, name);
