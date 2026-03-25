30735e3d2a91922dc06e24673ecbffcbda980daf
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 16:58:20 -0400

    o au

--- public/src/property_get.mjs
+++ public/src/property_get.mjs
@@ -5,6 +5,7 @@ export function property_get(object, property_name) {
   undefined_not_is_assert_lambda(value, object_get);
   function object_get() {
     let v = {
+      object,
       property_name,
     };
     return v;
