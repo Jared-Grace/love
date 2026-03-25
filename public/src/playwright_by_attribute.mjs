448433a9f22f63d234719cf3837c542286df0db5
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:32:24 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute.mjs

--- public/src/playwright_by_attribute.mjs
+++ public/src/playwright_by_attribute.mjs
@@ -1,16 +1,17 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export async function playwright_by_attribute(page, name, value) {
-  log(playwright_by_attribute.name, {
-    name,
-    value,
-  });
   const elements = await page
     .locator("*")
     .filter({
       has: page.locator(`[${name}]`),
     })
     .elementHandles();
+  log(playwright_by_attribute.name, {
+    name,
+    value,
+    elements,
+  });
   const filtered = [];
   for (const el of elements) {
     if ((await el.getAttribute(name)) === value) {
