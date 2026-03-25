0241ae5e2ede91d79479b0bb605f462c155028ea
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 22:44:48 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,14 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Expressions unary",
-    rules: ["ue > ce", "ue > uo ue", "uo > ue"],
+    rules: [
+      "ue > ce",
+      "ue > uo ue",
+      "uo > !",
+      "uo > -",
+      "uo > +",
+      "uo > typeof",
+    ],
     goals: [
       {
         start: "a",
