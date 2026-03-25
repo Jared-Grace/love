d4eee665cfafa12ac7d2e69409624fc099780bea
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 16:52:33 -0400

    function_name_unalias_only sb

--- public/src/function_auto.mjs
+++ public/src/function_auto.mjs
@@ -1,6 +1,8 @@
 import { js_auto } from "../../../love/public/src/js_auto.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function function_auto(f_name) {
-  await function_transform(f_name, js_auto);
+  let unaliased=function_name_unalias_only(f_name)
+  await function_transform(unaliased, js_auto);
   return;
 }
