1a07998757caab3eeb87c430ffec416d8cf968c4
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 18:08:39 -0400

    rrl app_replace_tests_run_e2e_generic browser

--- web/public/src/app_replace_tests_run_e2e_all.mjs
+++ web/public/src/app_replace_tests_run_e2e_all.mjs
@@ -1,8 +1,8 @@
-import { app_replace_tests_run_e2e_generic } from "../../../love/public/src/app_replace_tests_run_e2e_generic.mjs";
+import { app_replace_tests_run_e2e_browser } from "../../../love/public/src/app_replace_tests_run_e2e_browser.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 export async function app_replace_tests_run_e2e_all(inner) {
   let rule_sets = app_replace_rule_sets();
   let first = list_first(rule_sets);
-  await app_replace_tests_run_e2e_generic(first, goal, inner);
+  await app_replace_tests_run_e2e_browser(first, goal, inner);
 }
