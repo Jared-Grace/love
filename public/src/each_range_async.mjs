bd6a0cbb3825b33942eb6056cd4eaa490a785614
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:36:02 -0400

    function_auto_path ..\love\public\src\each_range_async.mjs

--- public/src/each_range_async.mjs
+++ public/src/each_range_async.mjs
@@ -1,6 +1,6 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { range } from "../../../love/public/src/range.mjs";
-export async function each_range_async(count, lambda) {
+export async function each_range_async(count, lambda$index) {
   let r = range(count);
-  await each_async(r, lambda);
+  await each_async(r, lambda$index);
 }
