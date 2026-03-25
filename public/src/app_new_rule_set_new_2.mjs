ae028bbaf6940d0dbc6323e88293f2f4108fa63b
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:24:13 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -2,7 +2,7 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 export function app_new_rule_set_new_2() {
   list_add_multiple(rules, ["ae > mue", "ae > ae mo mue", "ao > +", "ao > -"]);
   let r = {
-    name: "TODO",
+    name: "Expresions additive",
     rules: ["a > b"],
     goals: [
       {
