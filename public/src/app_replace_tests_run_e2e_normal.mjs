0af1b3f58b91b807e4552fb05f735824a4ea110d
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:53:42 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal.mjs

--- public/src/app_replace_tests_run_e2e_normal.mjs
+++ public/src/app_replace_tests_run_e2e_normal.mjs
@@ -11,7 +11,7 @@ import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/ap
 export async function app_replace_tests_run_e2e_normal() {
   let rule_sets = app_replace_rule_sets();
   function lambda2(rule_set) {
-    let goals = property_get(rs, "goals");
+    let goals = property_get(rule_set, "goals");
     let merged = {
       rule_set,
     };
