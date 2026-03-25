0c3adc0f661f01716edd02f4a7b4e20231ec7576
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:24:41 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_multiplicative.mjs

--- web/public/src/app_replace_rule_set_expressions_multiplicative.mjs
+++ web/public/src/app_replace_rule_set_expressions_multiplicative.mjs
@@ -1,17 +1,12 @@
+import { app_replace_rule_set_expressions_multiplicative_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_multiplicative_abbreviations.mjs";
 import { app_replace_rule_set_expressions_multiplicative_rules } from "../../../love/public/src/app_replace_rule_set_expressions_multiplicative_rules.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { app_replace_rule_set_expressions_unary_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_unary_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_replace_rule_set_expressions_multiplicative() {
   const rules = [];
   app_replace_rule_set_expressions_multiplicative_rules(rules);
   list_add(rules, "ex > mue");
   let abbreviations = {};
-  app_replace_rule_set_expressions_unary_abbreviations(abbreviations);
-  object_merge(abbreviations, {
-    mue: ["", "mu", "ltiplicative ", "e", "xpression"],
-    mo: ["", "m", "ultiplicative ", "o", "perator"],
-  });
+  app_replace_rule_set_expressions_multiplicative_abbreviations(abbreviations);
   let r = {
     name: "Expressions multiplicative",
     rules,
