c5dd0e6f65c83ea91bf69db8aac6b9e01726b892
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 13:53:45 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_generic.mjs

--- web/public/src/app_replace_tests_run_e2e_generic.mjs
+++ web/public/src/app_replace_tests_run_e2e_generic.mjs
@@ -77,7 +77,7 @@ export async function app_replace_tests_run_e2e_generic(
         if (not(last_goal)) {
           let name = app_replace_rule_set_success_attribute_next();
           await playwright_by_attribute_test_click(page, name);
-        }
+        }$e
         await playwright_by_attribute_exists_assert(page2, name3, value);
         let a = app_replace_rule_set_success_attribute_completed();
       }
