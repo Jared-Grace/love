f48de7a452674526395c345a73372d34b817588b
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:30:42 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute_type.mjs

--- public/src/playwright_by_attribute_type.mjs
+++ public/src/playwright_by_attribute_type.mjs
@@ -1,4 +1,4 @@
 import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 export async function playwright_by_attribute_type(page, name, value, typed) {
-  await playwright_by_attribute(page, name, value).type(typed);
+  await (await playwright_by_attribute(page, name, value)).type(typed);
 }
