c1bc821f65899496d98bc428f06eca6f868efb19
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:03:32 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_named.mjs

--- public/src/app_replace_rule_set_verify_named.mjs
+++ public/src/app_replace_rule_set_verify_named.mjs
@@ -1,5 +1,8 @@
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
+import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
-export function app_replace_rule_set_verify_named(rule_set) {
+export async function app_replace_rule_set_verify_named(rule_set_name) {
+  let rs = await function_run_args_none(fn_name);
   let r = app_replace_rule_set_verify(rule_set);
   return r;
 }
