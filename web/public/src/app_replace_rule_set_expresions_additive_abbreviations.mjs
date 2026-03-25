9e74e7059878c0d4bea334440abe3b47a0c3f191
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:42:17 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expresions_additive.mjs

---
+++ web/public/src/app_replace_rule_set_expresions_additive_abbreviations.mjs
@@ -0,0 +1,11 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { app_replace_rule_set_expressions_multiplicative_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_multiplicative_abbreviations.mjs";
+export function app_replace_rule_set_expresions_additive_abbreviations(
+  abbreviations,
+) {
+  app_replace_rule_set_expressions_multiplicative_abbreviations(abbreviations);
+  object_merge(abbreviations, {
+    ae: ["", "a", "dditive ", "e", "xpression"],
+    ao: ["", "a", "dditive ", "o", "perator"],
+  });
+}
