b8909c85e45e99b8d723a1e4b21e491ac420fc98
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 22:45:24 -0400

    arr app_new_rule_set_new_2

--- web/public/src/app_replace_rule_set_expressions_unary.mjs
+++ web/public/src/app_replace_rule_set_expressions_unary.mjs
@@ -1,6 +1,6 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_expressions_function_calls_rules } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_rules.mjs";
-export function app_new_rule_set_new_2() {
+export function app_replace_rule_set_expressions_unary() {
   const rules = [];
   app_replace_rule_set_expressions_function_calls_rules(rules);
   list_add(rules, "e > ce");
--- web/public/src/app_replace_rule_sets_fns.mjs
+++ web/public/src/app_replace_rule_sets_fns.mjs
@@ -1,4 +1,4 @@
-import { app_new_rule_set_new_2 } from "../../../love/public/src/app_new_rule_set_new_2.mjs";
+import { app_replace_rule_set_expressions_unary } from "../../../love/public/src/app_replace_rule_set_expressions_unary.mjs";
 import { app_replace_rule_set_expressions_function_calls } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls.mjs";
 import { app_replace_rule_set_expressions_member_and_access } from "../../../love/public/src/app_replace_rule_set_expressions_member_and_access.mjs";
 import { app_replace_rule_set_expressions_primary } from "../../../love/public/src/app_replace_rule_set_expressions_primary.mjs";
@@ -109,7 +109,7 @@ export function app_replace_rule_sets_fns() {
     app_replace_rule_set_expressions_primary,
     app_replace_rule_set_expressions_member_and_access,
     app_replace_rule_set_expressions_function_calls,
-    app_new_rule_set_new_2,
+    app_replace_rule_set_expressions_unary,
   ];
   return r2;
 }
