f76fd6e45419a6805baaea2a3923176e2eba4d32
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:21:34 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal.mjs

--- web/public/src/app_replace_tests_run_e2e_normal.mjs
+++ web/public/src/app_replace_tests_run_e2e_normal.mjs
@@ -6,11 +6,12 @@ import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/ap
 export async function app_replace_tests_run_e2e_normal() {
   let rule_sets = app_replace_rule_sets();
   let first = list_first(rule_sets);
-  async function lambda(rule_set) {}
+  async function lambda(rule_set) {
+    await app_replace_tests_run_e2e_generic(
+      [rule_set],
+      first,
+      app_replace_tests_run_e2e_normal_fn,
+    );
+  }
   await each_unordered_async(rule_sets, lambda);
-  await app_replace_tests_run_e2e_generic(
-    rule_sets,
-    first,
-    app_replace_tests_run_e2e_normal_fn,
-  );
 }
