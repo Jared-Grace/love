f471401ed195f2189b675686ad0a1bde33671292
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:42:06 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expresions_additive.mjs

---
+++ public/src/app_replace_rule_set_expresions_additive_rules.mjs
@@ -0,0 +1,6 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+import { app_replace_rule_set_expressions_multiplicative_rules } from "../../../love/public/src/app_replace_rule_set_expressions_multiplicative_rules.mjs";
+export function app_replace_rule_set_expresions_additive_rules(rules) {
+  app_replace_rule_set_expressions_multiplicative_rules(rules);
+  list_add_multiple(rules, ["ae > mue", "ae > ae ao mue", "ao > +", "ao > -"]);
+}
