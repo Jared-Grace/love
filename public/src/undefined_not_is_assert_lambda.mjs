332b2bbedbcfb686f6fcb5fd4e331ae96b69a456
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 16:44:06 -0400

    dts log_inner

--- public/src/undefined_not_is_assert_lambda.mjs
+++ public/src/undefined_not_is_assert_lambda.mjs
@@ -3,8 +3,9 @@ import { json_to } from "../../../love/public/src/json_to.mjs";
 import { undefined_is } from "../../../love/public/src/undefined_is.mjs";
 export function undefined_not_is_assert_lambda(value, lambda) {
   if (undefined_is(value)) {
-    const object = lambda();
-    let message = json_to(object);
+    
+    const message_object = lambda();
+    let message = json_to(message_object);
     error(message);
   }
 }
