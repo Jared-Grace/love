cc7e8611917c047b9ea7d95d5374f86e45b32cc8
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:01:09 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_unary.mjs

--- public/src/app_replace_rule_set_expressions_unary.mjs
+++ public/src/app_replace_rule_set_expressions_unary.mjs
@@ -1,19 +1,10 @@
-import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+import { app_replace_rule_set_expressions_unary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_unary_rules.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expressions_function_calls_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-import { app_replace_rule_set_expressions_function_calls_rules } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_rules.mjs";
 export function app_replace_rule_set_expressions_unary() {
   const rules = [];
-  app_replace_rule_set_expressions_function_calls_rules(rules);
-  list_add_multiple(rules, [
-    "ue > ce",
-    "ue > uo ue",
-    "uo > !",
-    "uo > -",
-    "uo > +",
-    "uo > typeof",
-  ]);
+  app_replace_rule_set_expressions_unary_rules(rules);
   list_add(rules, "ex > ue");
   let abbreviations = {};
   app_replace_rule_set_expressions_function_calls_abbreviations(abbreviations);
