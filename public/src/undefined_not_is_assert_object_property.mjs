182acff53ae565837d6ba80ec2cc7e868fc7b812
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:53:56 -0400

    function_auto_path ..\love\public\src\undefined_not_is_assert_object_property.mjs

--- public/src/undefined_not_is_assert_object_property.mjs
+++ public/src/undefined_not_is_assert_object_property.mjs
@@ -1,3 +1,4 @@
+import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
 export function undefined_not_is_assert_object_property(
   value,
@@ -6,6 +7,7 @@ export function undefined_not_is_assert_object_property(
 ) {
   undefined_not_is_assert_lambda(value, object_get);
   function object_get() {
+    let properties = properties_get(obj);
     let v = {
       object,
       property_name,
