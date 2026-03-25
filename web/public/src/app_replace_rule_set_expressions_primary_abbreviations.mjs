46b4183031056bb3f010104e86055d8127db627d
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 00:48:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_abbreviation_in.mjs

--- web/public/src/app_replace_rule_set_expressions_primary_abbreviations.mjs
+++ web/public/src/app_replace_rule_set_expressions_primary_abbreviations.mjs
@@ -1,7 +1,7 @@
 import { app_replace_rule_set_decimals_abbreviation_de } from "../../../love/public/src/app_replace_rule_set_decimals_abbreviation_de.mjs";
 import { app_replace_rule_set_boolean_literal_abbreviations } from "../../../love/public/src/app_replace_rule_set_boolean_literal_abbreviations.mjs";
 import { app_replace_rule_set_strings_simple_abbreviation_st } from "../../../love/public/src/app_replace_rule_set_strings_simple_abbreviation_st.mjs";
-import { app_replace_rule_set_integers_abbreviation_i } from "../../../love/public/src/app_replace_rule_set_integers_abbreviation_i.mjs";
+import { app_replace_rule_set_integers_abbreviation_in } from "../../../love/public/src/app_replace_rule_set_integers_abbreviation_in.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function app_replace_rule_set_expressions_primary_abbreviations(
   abbreviations,
@@ -13,7 +13,7 @@ export function app_replace_rule_set_expressions_primary_abbreviations(
     ex: ["", "ex", "pression"],
     nu: ["", "nu", "mber"],
   });
-  app_replace_rule_set_integers_abbreviation_i(abbreviations);
+  app_replace_rule_set_integers_abbreviation_in(abbreviations);
   app_replace_rule_set_strings_simple_abbreviation_st(abbreviations);
   let b = app_replace_rule_set_boolean_literal_abbreviations();
   let to2 = object_merge(abbreviations, b);
