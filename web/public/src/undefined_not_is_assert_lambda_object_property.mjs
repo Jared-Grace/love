28f31a872139fd03242682dc1b5835eed63e8c0b
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:14:15 -0400

    ao property_get

---
+++ web/public/src/undefined_not_is_assert_lambda_object_property.mjs
@@ -0,0 +1,15 @@
+import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
+export function undefined_not_is_assert_lambda_object_property(
+  value,
+  object,
+  property_name,
+) {
+  undefined_not_is_assert_lambda(value, object_get);
+  function object_get() {
+    let v = {
+      object,
+      property_name,
+    };
+    return v;
+  }
+}
