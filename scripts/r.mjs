3211e7010cbbad248473c323b6abdedc3edb53d8
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:50:12 -0400

    function_auto_path ..\love\public\src\server.mjs

--- scripts/r.mjs
+++ scripts/r.mjs
@@ -1,8 +1,8 @@
 import { function_run } from '../public/src/function_run.mjs';
-import { log } from '../public/src/log.mjs';
+import { log_keep } from '../public/src/log_keep.mjs';
 Error.stackTraceLimit = Infinity;
 (async () => {
   const [, , funcName, ...args] = process.argv;
  let result=await function_run(funcName, args)
-  log(result)
+  log_keep('r.mjs result:', result)
 })();
