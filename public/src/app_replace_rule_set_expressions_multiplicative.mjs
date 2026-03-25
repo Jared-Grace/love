d29a1806276c9eb64025999e46289d17238e8c83
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:09:47 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_multiplicative.mjs

--- public/src/app_replace_rule_set_expressions_multiplicative.mjs
+++ public/src/app_replace_rule_set_expressions_multiplicative.mjs
@@ -30,6 +30,10 @@ export function app_replace_rule_set_expressions_multiplicative() {
         start: "nu mo nu",
         end: "1 / 2",
       },
+      {
+        start: "nu mo nu",
+        end: "3 . 1 4 * 2",
+      },
     ],
   };
   return r;
