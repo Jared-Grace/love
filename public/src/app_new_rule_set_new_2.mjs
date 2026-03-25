50805f1791083f0188ca91b647fa114449868881
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 22:45:16 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,4 +1,9 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_replace_rule_set_expressions_function_calls_rules } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls_rules.mjs";
 export function app_new_rule_set_new_2() {
+  const rules = [];
+  app_replace_rule_set_expressions_function_calls_rules(rules);
+  list_add(rules, "e > ce");
   let r = {
     name: "Expressions unary",
     rules: [
