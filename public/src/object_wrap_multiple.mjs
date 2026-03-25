d12273a938471b2e8db1a07362e9a06a1b9cb0ac
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 18:00:13 -0400

    pb object_wrap_multiple

--- public/src/object_wrap_multiple.mjs
+++ public/src/object_wrap_multiple.mjs
@@ -1,6 +1,6 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_wrap_curried_right } from "../../../love/public/src/object_wrap_curried_right.mjs";
-export function object_wrap_multiple(property_name, goals) {
+export function object_wrap_multiple(goals, property_name) {
   let r = object_wrap_curried_right(property_name);
   let mapped = list_map(goals, r);
   return mapped;
