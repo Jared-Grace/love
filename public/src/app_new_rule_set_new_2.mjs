dc3875a6bcfd387002949c00ff0bac053064077b
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 00:42:30 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,7 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Expressions multiplicative",
-    rules: ["a > b"],
+    rules: ["mu"],
     goals: [
       {
         start: "a",
