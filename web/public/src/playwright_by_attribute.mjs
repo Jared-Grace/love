fbc24940ad14826938406d2156915d8026ee3eeb
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:30:18 -0400

    function_auto_path ..\love\public\src\playwright_by_attribute.mjs

--- web/public/src/playwright_by_attribute.mjs
+++ web/public/src/playwright_by_attribute.mjs
@@ -1,8 +1,15 @@
-import { text_replace } from "../../../love/public/src/text_replace.mjs";
-import { playwright_by_attribute_named } from "../../../portfolio_qa/public/src/playwright_by_attribute_named.mjs";
-export function playwright_by_attribute(page, name, value) {
-  let replaced = text_replace(value, '"', '\\"');
-  const inside = name + '="' + replaced + '"';
-  let r = playwright_by_attribute_named(page, inside);
-  return r;
+export async function playwright_by_attribute(page, name, value) {
+  const elements = await page
+    .locator("*")
+    .filter({
+      has: page.locator(`[${name}]`),
+    })
+    .elementHandles();
+  const filtered = [];
+  for (const el of elements) {
+    if ((await el.getAttribute(name)) === value) {
+      filtered.push(el);
+    }
+  }
+  return filtered;
 }
