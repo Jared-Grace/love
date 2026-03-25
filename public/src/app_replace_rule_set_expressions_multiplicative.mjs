cf530df55b06ababb3c0c774d000599e1c333075
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 01:04:37 -0400

    arr app_new_rule_set_new_2

--- public/src/app_replace_rule_set_expressions_multiplicative.mjs
+++ public/src/app_replace_rule_set_expressions_multiplicative.mjs
@@ -2,7 +2,7 @@ import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expressions_unary_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_unary_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_expressions_unary_rules } from "../../../love/public/src/app_replace_rule_set_expressions_unary_rules.mjs";
-export function app_new_rule_set_new_2() {
+export function app_replace_rule_set_expressions_multiplicative() {
   const rules = [];
   app_replace_rule_set_expressions_unary_rules(rules);
   list_add(rules, "ex > mue");
