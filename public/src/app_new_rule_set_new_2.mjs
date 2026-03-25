efd287a1445e684a457b7f4fe84b455c1e38d21f
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 22:43:30 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,7 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Expressions unary",
-    rules: ["ue > ce"],
+    rules: ["ue > ce", "ue > ce"],
     goals: [
       {
         start: "a",
