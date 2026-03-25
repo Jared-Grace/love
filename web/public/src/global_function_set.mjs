57a5b3b39aa8df62d71ad3b6dc77533583feaecd
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 17:53:23 -0400

    function_auto_path ..\love\public\src\global_function_set.mjs

--- web/public/src/global_function_set.mjs
+++ web/public/src/global_function_set.mjs
@@ -1,6 +1,9 @@
+import { undefined_not_is_assert } from "../../../love/public/src/undefined_not_is_assert.mjs";
 import { global_get } from "../../../love/public/src/global_get.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 export function global_function_set(fn, value) {
   let global = global_get();
   property_set(global, fn.name, value);
+  return;
+  undefined_not_is_assert(value);
 }
