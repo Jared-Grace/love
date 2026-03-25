d39b2f8884075b35bde10af2266c3ef78ec61837
Grace-Jared <christrosetolife@gmail.com>
2026-03-24 20:35:16 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_primary.mjs

---
+++ public/src/app_replace_rule_set_expressions_primary_abbreviations.mjs
@@ -0,0 +1,21 @@
+import { app_replace_rule_set_decimals_abbreviation_de } from "../../../love/public/src/app_replace_rule_set_decimals_abbreviation_de.mjs";
+import { app_replace_rule_set_boolean_literal_abbreviations } from "../../../love/public/src/app_replace_rule_set_boolean_literal_abbreviations.mjs";
+import { app_replace_rule_set_strings_simple_abbreviation_st } from "../../../love/public/src/app_replace_rule_set_strings_simple_abbreviation_st.mjs";
+import { app_replace_rule_set_integers_abbreviation_i } from "../../../love/public/src/app_replace_rule_set_integers_abbreviation_i.mjs";
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+export function app_replace_rule_set_expressions_primary_abbreviations(
+  abbreviations,
+) {
+  object_merge(abbreviations, {
+    pe: ["", "p", "rimary ", "e", "xpression"],
+    id: ["", "id", "entifier"],
+    li: ["", "li", "teral"],
+    e: ["", "e", "xpression"],
+    n: ["", "n", "umber"],
+  });
+  app_replace_rule_set_integers_abbreviation_i(abbreviations);
+  app_replace_rule_set_strings_simple_abbreviation_st(abbreviations);
+  let b = app_replace_rule_set_boolean_literal_abbreviations();
+  let to2 = object_merge(abbreviations, b);
+  let ab = app_replace_rule_set_decimals_abbreviation_de(abbreviations);
+}
