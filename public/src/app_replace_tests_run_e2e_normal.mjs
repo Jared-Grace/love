3761a8b454c95e7d5279c4ccbb79920bb49297a8
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:30:34 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal.mjs

--- public/src/app_replace_tests_run_e2e_normal.mjs
+++ public/src/app_replace_tests_run_e2e_normal.mjs
@@ -1,3 +1,4 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { app_replace_tests_run_e2e_generic } from "../../../love/public/src/app_replace_tests_run_e2e_generic.mjs";
@@ -7,13 +8,15 @@ import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/ap
 export async function app_replace_tests_run_e2e_normal() {
   let rule_sets = app_replace_rule_sets();
   let first = list_first(rule_sets);
+  let chunks = list_chunk(rule_sets, 2);
   await app_replace_tests_run_e2e_generic(
     [first],
     first,
     app_replace_tests_run_e2e_normal_fn,
   );
   return;
-  let chunks = list_chunk(rule_sets, 2);
+  async function lambda2(item) {}
+  await each_async(list, lambda2);
   async function lambda(rule_set) {
     await app_replace_tests_run_e2e_generic(
       [rule_set],
