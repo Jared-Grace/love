414cc4a1f385f31c528329a468d291a117cf8963
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 15:51:37 -0400

    pe object_merge_multiple

--- web/public/src/object_merge_multiple.mjs
+++ web/public/src/object_merge_multiple.mjs
@@ -1,6 +1,6 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { object_merge_curried_right } from "../../../love/public/src/object_merge_curried_right.mjs";
-export function object_merge_multiple(merged, goals) {
+export function object_merge_multiple(goals, merged) {
   let r = object_merge_curried_right(merged);
   each(goals, r);
 }
