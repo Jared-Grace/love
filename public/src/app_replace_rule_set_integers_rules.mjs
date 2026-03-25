4045e158ff17fd8fd2aa1c846c8f5459ea785d0f
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 00:52:34 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_integers_rules.mjs

--- public/src/app_replace_rule_set_integers_rules.mjs
+++ public/src/app_replace_rule_set_integers_rules.mjs
@@ -1,8 +1,15 @@
 import { app_replace_rule_set_add_rights } from "../../../love/public/src/app_replace_rule_set_add_rights.mjs";
 import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
 export function app_replace_rule_set_integers_rules() {
-  const rules = ["i > p g", "i > di", "g > di g", "di > 0", "di > p", "g > di"];
+  const rules = [
+    "in > pi ig",
+    "in > di",
+    "ig > di ig",
+    "di > 0",
+    "di > pi",
+    "ig > di",
+  ];
   let p = digits_positive();
-  app_replace_rule_set_add_rights(rules, "p", p);
+  app_replace_rule_set_add_rights(rules, "pi", p);
   return rules;
 }
