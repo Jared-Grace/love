4b9684501cefbb95c9b7a5bf8cfe873ad542753e
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 20:47:38 -0400

    de playwright_by_attribute_all

--- public/src/playwright_by_attribute_all.mjs
+++
@@ -1,7 +0,0 @@
-import { playwright_locator_handles } from "../../../love/public/src/playwright_locator_handles.mjs";
-import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
-export async function playwright_by_attribute_all(page, name, value) {
-  let locator = await playwright_by_attribute(page, name, value);
-  let handles = await playwright_locator_handles(locator);
-  return handles;
-}
