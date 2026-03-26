1435800df494a80b66cb12211e91d3d9ae64ec01
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:03:58 -0400

    cu list_map_property

--- web/public/src/list_map_property_curried.mjs
+++ web/public/src/list_map_property_curried.mjs
@@ -1,1 +1,6 @@
-export function list_map_property_curried() {}
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+export function list_map_property_curried(list) {
+  return function list_map_property_curried_result(property_name) {
+    return list_map_property(list, property_name);
+  };
+}
