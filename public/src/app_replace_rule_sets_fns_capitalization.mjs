24d8907fa51bf88a5ef52985c0408be212a60020
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:29:39 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_capitalization.mjs

--- public/src/app_replace_rule_sets_fns_capitalization.mjs
+++ public/src/app_replace_rule_sets_fns_capitalization.mjs
@@ -1,7 +1,6 @@
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { app_replace_rule_sets_fn_migrate_goals_space_add_elements } from "../../../love/public/src/app_replace_rule_sets_fn_migrate_goals_space_add_elements.mjs";
 import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
-import { app_replace_rule_sets_fn_migrate_space_add } from "../../../love/public/src/app_replace_rule_sets_fn_migrate_space_add.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function app_replace_rule_sets_fns_capitalization() {
   async function lambda3(ast) {
