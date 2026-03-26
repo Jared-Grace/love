4dfa7551c340e954fca7d598ac3c55ae70fa0585
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:02:31 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal_fn.mjs

--- web/public/src/app_replace_tests_run_e2e_normal_fn.mjs
+++ web/public/src/app_replace_tests_run_e2e_normal_fn.mjs
@@ -18,7 +18,8 @@ export async function app_replace_tests_run_e2e_normal_fn(
   let symbol_id = app_replace_rule_set_attribute_symbol(index);
   let r3 = {
     refresh_count,
-    symbol_id,rule_original_previous
+    symbol_id,
+    rule_original_previous,
   };
   return r3;
 }
