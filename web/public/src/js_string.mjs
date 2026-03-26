008fe6ca07aa3bd5d15764410b71405cceb31112
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 19:06:01 -0400

    function_auto_path ..\love\public\src\js_string.mjs

--- web/public/src/js_string.mjs
+++ web/public/src/js_string.mjs
@@ -1,11 +1,11 @@
-import { assert } from "../../../love/public/src/assert.mjs";
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { property_delete } from "../../../love/public/src/property_delete.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 export function js_string(value_string) {
-  text_is_assert(value_string);
   let s = js_parse_expression('""');
   property_set(s, "value", value_string);
   property_delete(s, "raw");
   return s;
+  text_is_assert(value_string);
 }
