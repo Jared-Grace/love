48924a081a85828f34ed00365cff770138012003
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 13:45:44 -0400

    function_auto_path ..\love\public\src\property_set.mjs

--- public/src/property_set.mjs
+++ public/src/property_set.mjs
@@ -1,3 +1,6 @@
+import { undefined_not_is_assert_object_property } from "../../../love/public/src/undefined_not_is_assert_object_property.mjs";
 export function property_set(object, property_name, value) {
   object[property_name] = value;
+  return;
+  undefined_not_is_assert_object_property(value, object, property_name);
 }
