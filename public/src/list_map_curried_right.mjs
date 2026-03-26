e83e93f0a8346d204f161d25d36236bfbff4ad6d
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:38:58 -0400

    function_auto_path ..\love\public\src\list_map_curried_right.mjs

--- public/src/list_map_curried_right.mjs
+++ public/src/list_map_curried_right.mjs
@@ -1,6 +1,8 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_curried_right(lambda$item) {
-  return function list_map_curried_right_result(list) {
-    return list_map(list, lambda$item);
+  let r = function list_map_curried_right_result(list) {
+    let mapped = list_map(list, lambda$item);
+    return mapped;
   };
+  return r;
 }
