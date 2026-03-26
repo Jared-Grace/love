dd36b2b259be0fd414fbbe1dd25cb7933eb8ae4b
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 07:49:59 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_goal.mjs

--- web/public/src/app_replace_tests_run_e2e_goal.mjs
+++ web/public/src/app_replace_tests_run_e2e_goal.mjs
@@ -1,4 +1,3 @@
-import { app_replace_tests_run_e2e_all } from "../../../love/public/src/app_replace_tests_run_e2e_all.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { playwright_refresh } from "../../../love/public/src/playwright_refresh.mjs";
 import { app_replace_rule_set_success_attribute_next } from "../../../love/public/src/app_replace_rule_set_success_attribute_next.mjs";
