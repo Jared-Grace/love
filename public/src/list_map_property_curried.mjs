ce5b9dfdf1b9245a9b8d92c83501e3e6e5e7f31f
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:04:00 -0400

    function_auto_path ..\love\public\src\list_map_property_curried.mjs

--- public/src/list_map_property_curried.mjs
+++ public/src/list_map_property_curried.mjs
@@ -1,6 +1,8 @@
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export function list_map_property_curried(list) {
-  return function list_map_property_curried_result(property_name) {
-    return list_map_property(list, property_name);
+  let r = function list_map_property_curried_result(property_name) {
+    let mapped = list_map_property(list, property_name);
+    return mapped;
   };
+  return r;
 }
