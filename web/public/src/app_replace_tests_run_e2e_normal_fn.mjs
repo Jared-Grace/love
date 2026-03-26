a6c96a6bfd7822147285b6bd252b48542a417940
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:03:33 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal_fn.mjs

--- web/public/src/app_replace_tests_run_e2e_normal_fn.mjs
+++ web/public/src/app_replace_tests_run_e2e_normal_fn.mjs
@@ -1,4 +1,4 @@
-import { equal } from "../../../love/public/src/equal.mjs";
+import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { app_replace_rule_set_attribute_symbol } from "../../../love/public/src/app_replace_rule_set_attribute_symbol.mjs";
 import { app_replace_rule_set_attribute_refresh_click } from "../../../love/public/src/app_replace_rule_set_attribute_refresh_click.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -10,7 +10,7 @@ export async function app_replace_tests_run_e2e_normal_fn(
 ) {
   let rule = property_get(p, "rule");
   let original = property_get(rule, "original");
-  if (equal(left, right)) {
+  if (equal_not(left, right)) {
   }
   refresh_count = await app_replace_rule_set_attribute_refresh_click(
     page,
