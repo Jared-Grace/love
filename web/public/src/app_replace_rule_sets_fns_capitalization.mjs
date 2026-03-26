f0423c8c4949847b4d560616147e330497887968
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:37:11 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_capitalization.mjs

--- web/public/src/app_replace_rule_sets_fns_capitalization.mjs
+++ web/public/src/app_replace_rule_sets_fns_capitalization.mjs
@@ -1,10 +1,10 @@
+import { each_unordered_async } from "../../../love/public/src/each_unordered_async.mjs";
 import { exit } from "../../../love/public/src/exit.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
-import { each_async } from "../../../love/public/src/each_async.mjs";
 import { list_map_name } from "../../../love/public/src/list_map_name.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -43,6 +43,6 @@ export async function app_replace_rule_sets_fns_capitalization() {
     let output = await function_transform(name, lambda3);
     exit();
   }
-  await each_async(result, lambda);
+  await each_unordered_async(result, lambda);
   return result;
 }
