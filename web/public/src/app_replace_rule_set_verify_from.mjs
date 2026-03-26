db0dc6618a819c893bfee627c54674a22f3a1b47
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 21:51:39 -0400

    pf app_replace_rule_set_verify_from rules_parsed

--- web/public/src/app_replace_rule_set_verify_from.mjs
+++ web/public/src/app_replace_rule_set_verify_from.mjs
@@ -1,7 +1,7 @@
 import { app_replace_rule_set_verify_from_try } from "../../../love/public/src/app_replace_rule_set_verify_from_try.mjs";
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function app_replace_rule_set_verify_from(start, end, rules_parsed) {
+export function app_replace_rule_set_verify_from(rules_parsed, start, end) {
   let dfs = app_replace_rule_set_verify_from_try(rules_parsed, start, end);
   let found = property_get(dfs, "found");
   function lambda3() {
