3ff56758f48cc9413fe5c597170b0bcf5f67af68
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:30:25 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_capitalization.mjs

--- public/src/app_replace_rule_sets_fns_capitalization.mjs
+++ public/src/app_replace_rule_sets_fns_capitalization.mjs
@@ -1,6 +1,10 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { js_property_value_change } from "../../../love/public/src/js_property_value_change.mjs";
+import { text_between_space } from "../../../love/public/src/text_between_space.mjs";
+import { js_literal_map_curried_right } from "../../../love/public/src/js_literal_map_curried_right.mjs";
+import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { js_list_nodes_object_expression } from "../../../love/public/src/js_list_nodes_object_expression.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { app_replace_rule_sets_fn_migrate_goals_space_add_elements } from "../../../love/public/src/app_replace_rule_sets_fn_migrate_goals_space_add_elements.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function app_replace_rule_sets_fns_capitalization() {
   async function lambda3(ast) {
@@ -8,7 +12,16 @@ export async function app_replace_rule_sets_fns_capitalization() {
       name,
     });
     let list = js_list_nodes_object_expression(ast);
-    app_replace_rule_sets_fn_migrate_goals_space_add_elements(list);
+    function lambda_each(item) {
+      let ps = ["start", "end"];
+      function lambda2(p) {
+        let s = js_object_expression_properties_find_key_named(item, p);
+        let r = js_literal_map_curried_right(text_between_space);
+        js_property_value_change(s, r);
+      }
+      each(ps, lambda2);
+    }
+    each(list, lambda_each);
   }
   let output = await function_transform(name, lambda3);
 }
