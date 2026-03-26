e251360bfca4ba97b6547add280ca85313d74d7e
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 18:35:48 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- public/src/app_replace_rule_sets_why_generate.mjs
+++ public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,7 +1,6 @@
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { app_replace_rule_sets_why_generate_single } from "../../../love/public/src/app_replace_rule_sets_why_generate_single.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -16,7 +15,6 @@ export async function app_replace_rule_sets_why_generate() {
   }
   let mapped = list_map(r2, lambda2);
   let mapped2 = list_map_property(list, property_name);
-  let rule_sets = invoke_multiple(r2);
   let taken = list_take(rule_sets, 2);
   await each_async(rule_sets, app_replace_rule_sets_why_generate_single);
   function lambda() {}
