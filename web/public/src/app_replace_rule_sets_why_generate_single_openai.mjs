234d29f4821398df7ceb076ea6d5612afe6bd06d
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 18:46:05 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate_single_openai.mjs

--- web/public/src/app_replace_rule_sets_why_generate_single_openai.mjs
+++ web/public/src/app_replace_rule_sets_why_generate_single_openai.mjs
@@ -7,14 +7,14 @@ export async function app_replace_rule_sets_why_generate_single_openai(
   rule_set,
 ) {
   let json = json_to(rule_set);
-  log(app_replace_rule_sets_why_generate_single.name, {
+  log(app_replace_rule_sets_why_generate_single_openai.name, {
     json,
   });
   let r = await openai_responses_cache(
     'You will receive the JSON of the replacement rules of a grammar. Please provide a sentence or two describing what the replacement rules demonstrate, and why. Answer using JSON: { "result": "description" }',
     json,
   );
-  log(app_replace_rule_sets_why_generate_single.name, {
+  log(app_replace_rule_sets_why_generate_single_openai.name, {
     r,
   });
   let parsed = json_from(r);
