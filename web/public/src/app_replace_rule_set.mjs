486b3ebc9f9cbb256e63dc59b628ff7d6600c820
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 14:31:34 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- web/public/src/app_replace_rule_set.mjs
+++ web/public/src/app_replace_rule_set.mjs
@@ -131,7 +131,6 @@ export async function app_replace_rule_set(context) {
     "border-radius": "9999px",
     "background-color": highlight,
     padding: "0",
-    "font-size": "0.8em",
   });
   html_text_set(progress_container, "0");
   let rule_name = property_get(rs, "name");
