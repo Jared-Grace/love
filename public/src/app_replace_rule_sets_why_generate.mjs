4278a08a4ff42b2ad8a697124e45cea4f24419fc
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:24:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- public/src/app_replace_rule_sets_why_generate.mjs
+++ public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,9 +1,12 @@
+import { exit } from "../../../love/public/src/exit.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
 export async function app_replace_rule_sets_why_generate() {
   let rule_sets = app_replace_rule_sets();
-  async function lambda(item) {}
-  await each_async(list, lambda);
+  async function lambda(item) {
+    exit();
+  }
+  await each_async(rule_sets, lambda);
   let r = await openai_responses_cache(system, user);
 }
