2e4ea2a2b67be93f12f8a5d9b64adeb8cf247692
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:24:35 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal.mjs

--- public/src/app_replace_tests_run_e2e_normal.mjs
+++ public/src/app_replace_tests_run_e2e_normal.mjs
@@ -2,5 +2,5 @@ import { app_replace_tests_run_e2e_all } from "../../../love/public/src/app_repl
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
 export async function app_replace_tests_run_e2e_normal() {
   let e2e_inner = app_replace_tests_run_e2e_normal_fn;
-  await app_replace_tests_run_e2e_all(e2e_inner);
+  await app_replace_tests_run_e2e_all([e2e_inner]);
 }
