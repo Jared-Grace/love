516ae0f4071e8e97dba500be3e2f68516fc20501
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:23:50 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- web/public/src/app_replace_rule_sets_why_generate.mjs
+++ web/public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,4 +1,6 @@
+import { app_replace_rule_set } from "../../../love/public/src/app_replace_rule_set.mjs";
 import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
-export async function app_replace_rule_sets_why_generate() {arrs
+export async function app_replace_rule_sets_why_generate() {
+  await app_replace_rule_set(context);
   let r = await openai_responses_cache(system, user);
 }
