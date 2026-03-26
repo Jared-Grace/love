452c800acf6139773f9cac63697db9677dbac870
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:29:50 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- web/public/src/app_replace_rule_sets_why_generate.mjs
+++ web/public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rules_parse } from "../../../love/public/src/app_replace_rules_parse.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
@@ -8,8 +9,8 @@ import { openai_responses_cache } from "../../../love/public/src/openai_response
 export async function app_replace_rule_sets_why_generate() {
   let rule_sets = app_replace_rule_sets();
   async function lambda(rule_set) {
-    function lambda2(value) {}
-    let value2 = property_change(o, property, lambda2);
+    let value2 = property_change(rule_set, "rules", app_replace_rules_parse);
+    return value2;
     let json = json_to(rule_set);
     let r = await openai_responses_cache(
       "You will receive the JSON of an unrestricted grammar (although it could be more restrictive like a context-free grammar). Please provide a sentence or two describing what the grammar demonstrates, and why.",
