bd5d26b452dc6b253fe3babf7af219add71b59fa
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 19:09:44 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_same_replace_left.mjs

--- public/src/app_replace_rule_set_shrink_triple.mjs
+++ public/src/app_replace_rule_set_shrink_triple.mjs
@@ -16,6 +16,7 @@ export function app_replace_rule_set_shrink_triple() {
         end: "a",
       },
     ],
+    why: "The replacement rules demonstrate a grammar that reduces any sequence of three consecutive 'a's to a single 'a'. This process can be repeatedly applied to shrink longer sequences of 'a's down to a single 'a', as shown in the goals where sequences of 3, 5, or 7 'a's are all reduced to just one 'a'.",
   };
   return r;
 }
