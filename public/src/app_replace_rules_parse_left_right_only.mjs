78e4578c6e44b386705a78c4b7aa292fe7451cde
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:33:39 -0400

    function_auto_path ..\love\public\src\app_replace_rules_parse_left_right_only.mjs

--- public/src/app_replace_rules_parse_left_right_only.mjs
+++ public/src/app_replace_rules_parse_left_right_only.mjs
@@ -1,6 +1,6 @@
 import { app_replace_rule_parse_left_right_only } from "../../../love/public/src/app_replace_rule_parse_left_right_only.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function app_replace_rules_parse_2(rules) {
+export function app_replace_rules_parse_left_right_only(rules) {
   let mapped = list_map(rules, app_replace_rule_parse_left_right_only);
   return mapped;
 }
