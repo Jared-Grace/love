7d67a9d2ce353c8485bf6a95de18b57e500767ef
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:56:11 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -1,6 +1,5 @@
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
-import { list_empty_not_is_while_async } from "../../../love/public/src/list_empty_not_is_while_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_tests_run_e2e_goal } from "../../../love/public/src/app_replace_tests_run_e2e_goal.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -10,21 +9,13 @@ import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/ap
 import { app_replace_tests_run_e2e_all } from "../../../love/public/src/app_replace_tests_run_e2e_all.mjs";
 export async function app_replace_tests_run_e2e() {
   async function lambda(page) {
-    async function lambda4() {
-      let next = list_pop_first(remaining);
-      let rule_set = property_get(next, "rule_set");
-      let goal = property_get(next, "goal");
-      async function lambda3(e2e_inner_fn) {
-        await app_replace_tests_run_e2e_goal(
-          page,
-          goal,
-          rule_set,
-          e2e_inner_fn,
-        );
-      }
-      await each_async(e2e_inner_fns, lambda3);
+    let next = list_pop_first(remaining);
+    let rule_set = property_get(next, "rule_set");
+    let goal = property_get(next, "goal");
+    async function lambda3(e2e_inner_fn) {
+      await app_replace_tests_run_e2e_goal(page, goal, rule_set, e2e_inner_fn);
     }
-    await list_empty_not_is_while_async(remaining, lambda4);
+    await each_async(e2e_inner_fns, lambda3);
   }
   await playwright_test_app_dev(app_replace, lambda);
   let e2e_inner_fns = [
