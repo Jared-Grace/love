0367b8ec89bb106b14abf8412bf70e8ab74f87ed
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:21:18 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_binary_numbers_simple.mjs

--- public/src/app_replace_rule_set_binary_numbers_simple.mjs
+++ public/src/app_replace_rule_set_binary_numbers_simple.mjs
@@ -3,7 +3,7 @@ export function app_replace_rule_set_binary_numbers_simple() {
   let r = {
     name: "Binary numbers simple 2",
     rules: ["0 > 0 0", "0 > 1"],
-    goals: app_replace_rule_set_binary_numbers_goals("b"),
+    goals: app_replace_rule_set_binary_numbers_goals("0"),
   };
   return r;
 }
