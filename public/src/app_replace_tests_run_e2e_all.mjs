554bb25ece13a358a9819e2599e6829d6f8f6abd
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 08:55:51 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_all.mjs

--- public/src/app_replace_tests_run_e2e_all.mjs
+++ public/src/app_replace_tests_run_e2e_all.mjs
@@ -15,7 +15,6 @@ import { log_keep } from "./log_keep.mjs";
 export async function app_replace_tests_run_e2e_all(e2e_inner_fns) {
   let rule_sets = app_replace_rule_sets();
   let r = app_replace_rule_set_binary_counting();
-  rule_sets = [r];
   function lambda2(rule_set) {
     let goals = property_get(rule_set, "goals");
     let mapped = object_wrap_multiple(goals, "goal");
