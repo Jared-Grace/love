11a740924b6777ff419aa2f6729c493e5807f6a5
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 19:08:37 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate_single.mjs

--- public/src/app_replace_rule_sets_why_generate_single.mjs
+++ public/src/app_replace_rule_sets_why_generate_single.mjs
@@ -1,3 +1,4 @@
+import { property_delete_if_exists } from "../../../love/public/src/property_delete_if_exists.mjs";
 import { app_replace_rule_sets_why_generate_single_openai } from "../../../love/public/src/app_replace_rule_sets_why_generate_single_openai.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -7,6 +8,7 @@ import { app_replace_rules_parse_left_right_only } from "../../../love/public/sr
 import { property_change } from "../../../love/public/src/property_change.mjs";
 export async function app_replace_rule_sets_why_generate_single(rs) {
   let rule_set = property_get(rs, "rule_set");
+  property_delete_if_exists(data, p);
   console.log(rule_set.rules);
   let value2 = property_change(
     rule_set,
