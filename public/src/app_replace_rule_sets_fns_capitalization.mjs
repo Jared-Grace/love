a32b0dd8bd5c23367582474176484e5767cc6fc6
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:35:52 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_capitalization.mjs

--- public/src/app_replace_rule_sets_fns_capitalization.mjs
+++ public/src/app_replace_rule_sets_fns_capitalization.mjs
@@ -1,3 +1,5 @@
+import { log_json } from "../../../love/public/src/log_json.mjs";
+import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
@@ -28,8 +30,8 @@ export async function app_replace_rule_sets_fns_capitalization() {
         }
         function lambda2(t) {
           let split = text_split_space(t);
-          function lambda4(item2) {}
-          let mapped = list_map(list2, lambda4);
+          let mapped = list_map(split, text_first_upper_to);
+          log_json(right);
         }
         let r = js_literal_map_curried_right(lambda2);
         log(app_replace_rule_sets_fns_capitalization.name, {
