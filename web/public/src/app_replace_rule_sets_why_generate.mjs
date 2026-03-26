c620ef385a5e2ecc6481fe55250a6acc49f30b6e
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:34:01 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- web/public/src/app_replace_rule_sets_why_generate.mjs
+++ web/public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,4 +1,4 @@
-import { app_replace_rules_parse } from "../../../love/public/src/app_replace_rules_parse.mjs";
+import { app_replace_rules_parse_left_right_only } from "../../../love/public/src/app_replace_rules_parse_left_right_only.mjs";
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
@@ -9,7 +9,11 @@ import { openai_responses_cache } from "../../../love/public/src/openai_response
 export async function app_replace_rule_sets_why_generate() {
   let rule_sets = app_replace_rule_sets();
   async function lambda(rule_set) {
-    let value2 = property_change(rule_set, "rules", app_replace_rules_parse);
+    let value2 = property_change(
+      rule_set,
+      "rules",
+      app_replace_rules_parse_left_right_only,
+    );
     let json = json_to(rule_set);
     let r = await openai_responses_cache(
       "You will receive the JSON of an unrestricted grammar (although it could be more restrictive like a context-free grammar). Please provide a sentence or two describing what the grammar demonstrates, and why.",
