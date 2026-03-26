4a452bdb61778b8823c38d9f286620a7cc0b440f
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:01:24 -0400

    pd app_replace_tests_run_e2e_hinted_fn symbol_id

--- public/src/app_replace_tests_run_e2e_hinted_fn.mjs
+++ public/src/app_replace_tests_run_e2e_hinted_fn.mjs
@@ -17,7 +17,6 @@ export async function app_replace_tests_run_e2e_hinted_fn(
   p,
   refresh_count,
   page,
-  symbol_id,
 ) {
   let max = app_replace_rule_set_verify_goal_depth_max();
   async function lambda(item) {
