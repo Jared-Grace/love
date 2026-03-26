f7f87d6dae2fe0b99569b031fc406cd6985db5fc
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:31:09 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- public/src/app_replace_rule_sets_why_generate.mjs
+++ public/src/app_replace_rule_sets_why_generate.mjs
@@ -10,10 +10,6 @@ export async function app_replace_rule_sets_why_generate() {
   let rule_sets = app_replace_rule_sets();
   async function lambda(rule_set) {
     let value2 = property_change(rule_set, "rules", app_replace_rules_parse);
-    log(app_replace_rule_sets_why_generate.name, {
-      rule_set,
-    });
-    exit();
     let json = json_to(rule_set);
     let r = await openai_responses_cache(
       "You will receive the JSON of an unrestricted grammar (although it could be more restrictive like a context-free grammar). Please provide a sentence or two describing what the grammar demonstrates, and why.",
