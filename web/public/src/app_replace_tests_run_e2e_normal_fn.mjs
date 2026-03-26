f8d840b1c9cbfb107e4d4dcd7865ba67ce2ac2a9
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:03:21 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_normal_fn.mjs

--- web/public/src/app_replace_tests_run_e2e_normal_fn.mjs
+++ web/public/src/app_replace_tests_run_e2e_normal_fn.mjs
@@ -8,7 +8,7 @@ export async function app_replace_tests_run_e2e_normal_fn(
   rule_original_previous,
 ) {
   let rule = property_get(p, "rule");
-  let original = property_get(rule, "original");
+  let original = property_get(rule, "original");$ie
   refresh_count = await app_replace_rule_set_attribute_refresh_click(
     page,
     original,
