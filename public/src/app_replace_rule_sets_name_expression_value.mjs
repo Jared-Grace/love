30c86ab9f464aca55a7f3575a20ddff1dcaa5f13
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 18:41:15 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_name_expression_value.mjs

--- public/src/app_replace_rule_sets_name_expression_value.mjs
+++ public/src/app_replace_rule_sets_name_expression_value.mjs
@@ -1,12 +1,11 @@
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
+import { app_replace_rule_sets_name_expression } from "../../../love/public/src/app_replace_rule_sets_name_expression.mjs";
+import { null_is } from "../../../love/public/src/null_is.mjs";
 import { app_replace_rule_sets_name } from "../../../love/public/src/app_replace_rule_sets_name.mjs";
-import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 export function app_replace_rule_sets_name_expression_value(e) {
-  let search = "name";
-  let found = js_object_expression_properties_find_key_named(e, search);
-  let result = null;
-  if (null_not_is(found)) {
-    result = app_replace_rule_sets_name(found);
+  let found = app_replace_rule_sets_name_expression(e);
+  if (null_is(found)) {
+    return found;
   }
-  return result;
+  let f_name_new = app_replace_rule_sets_name(found);
+  return f_name_new;
 }
