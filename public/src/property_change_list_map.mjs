4c277bd9aad0a7ef13abbb79b466ed7749c702f0
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:40:27 -0400

    pb property_change_list_map

--- public/src/property_change_list_map.mjs
+++ public/src/property_change_list_map.mjs
@@ -1,6 +1,6 @@
 import { property_change } from "../../../love/public/src/property_change.mjs";
 import { list_map_curried_right } from "../../../love/public/src/list_map_curried_right.mjs";
-export function property_change_list_map(lambda$item, property_name) {
+export function property_change_list_map(property_name, lambda$item) {
   let c = list_map_curried_right(lambda$item);
   let value_changed = property_change(o, property_name, c);
   return value_changed;
