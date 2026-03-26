237d52d7ea68918e90c539609be3dba80cc40023
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:56:22 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

--- public/src/app_replace_tests_run_e2e.mjs
+++ public/src/app_replace_tests_run_e2e.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -8,6 +9,8 @@ import { app_replace_tests_run_e2e_hinted_fn } from "../../../love/public/src/ap
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
 import { app_replace_tests_run_e2e_all } from "../../../love/public/src/app_replace_tests_run_e2e_all.mjs";
 export async function app_replace_tests_run_e2e() {
+  let rule_sets = app_replace_rule_sets();
+  ltk;
   async function lambda(page) {
     let next = list_pop_first(remaining);
     let rule_set = property_get(next, "rule_set");
