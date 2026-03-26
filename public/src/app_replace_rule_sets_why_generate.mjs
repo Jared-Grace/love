3b3e2eae9cf1c63f12722a74667ac092acb9e72d
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 16:27:20 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- public/src/app_replace_rule_sets_why_generate.mjs
+++ public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,10 +1,12 @@
+import { json_to } from "../../../love/public/src/json_to.mjs";
 import { exit } from "../../../love/public/src/exit.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
 export async function app_replace_rule_sets_why_generate() {
   let rule_sets = app_replace_rule_sets();
-  async function lambda(item) {
+  async function lambda(rule_set) {
+    let json = json_to(object);
     let r = await openai_responses_cache(
       "You will receive the JSON of an unrestricted grammar (although it could be more restrictive like a context-free grammar). Please provide a sentence or two describing what the grammar demonstrates, and why.",
       user,
