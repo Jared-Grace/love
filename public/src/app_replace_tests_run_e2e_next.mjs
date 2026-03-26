673491eadb04f0dbd46d38c2614d086c79fb8a2c
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:59:09 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e.mjs

---
+++ public/src/app_replace_tests_run_e2e_next.mjs
@@ -0,0 +1,28 @@
+import { app_replace } from "../../../love/public/src/app_replace.mjs";
+import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
+import { each_async } from "../../../love/public/src/each_async.mjs";
+import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
+import { app_replace_tests_run_e2e_goal } from "../../../love/public/src/app_replace_tests_run_e2e_goal.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_take } from "../../../love/public/src/list_take.mjs";
+import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
+export async function app_replace_tests_run_e2e_next() {
+  let rule_sets = app_replace_rule_sets();
+  let taken = list_take(rule_sets, 2);
+  async function lambda(page) {
+    async function lambda3(rule_set) {
+      let goals = property_get(rule_set, "goals");
+      async function lambda2(goal) {
+        await app_replace_tests_run_e2e_goal(
+          page,
+          goal,
+          rule_set,
+          app_replace_tests_run_e2e_normal_fn,
+        );
+      }
+      await each_async(goals, lambda2);
+    }
+    await each_async(taken, lambda3);
+  }
+  await playwright_test_app_dev(app_replace, lambda);
+}
