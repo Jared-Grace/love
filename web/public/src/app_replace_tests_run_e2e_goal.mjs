dc661713bbe6f253733fbf629e0b601ca95afca0
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 07:50:30 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_goal.mjs

--- web/public/src/app_replace_tests_run_e2e_goal.mjs
+++ web/public/src/app_replace_tests_run_e2e_goal.mjs
@@ -80,6 +80,7 @@ export async function app_replace_tests_run_e2e_goal(
     await each_goal(goal);
   } catch (e) {
     log_keep(app_replace_tests_run_e2e_goal.name, {
+      e,
       goal,
       completed: true,
       e2e_inner_fn_name: e2e_inner_fn.name,
