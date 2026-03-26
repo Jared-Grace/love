d15d22eab5d4c2d6ba40a1b38aca5e728027cc03
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 22:30:19 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_capitalization.mjs

--- public/src/app_replace_rule_sets_fns_capitalization.mjs
+++ public/src/app_replace_rule_sets_fns_capitalization.mjs
@@ -1,14 +1,13 @@
+import { js_list_nodes_object_expression } from "../../../love/public/src/js_list_nodes_object_expression.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { app_replace_rule_sets_fn_migrate_goals_space_add_elements } from "../../../love/public/src/app_replace_rule_sets_fn_migrate_goals_space_add_elements.mjs";
-import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function app_replace_rule_sets_fns_capitalization() {
   async function lambda3(ast) {
     log(app_replace_rule_sets_fns_capitalization.name, {
       name,
     });
-    const type = "ObjectExpression";
-    let list = js_list_type_nodes(ast, type);
+    let list = js_list_nodes_object_expression(ast);
     app_replace_rule_sets_fn_migrate_goals_space_add_elements(list);
   }
   let output = await function_transform(name, lambda3);
