7fc51c3454a858b6150f5c8d429f8097aae7b7f9
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 07:50:06 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_goal.mjs

--- web/public/src/app_replace_tests_run_e2e_goal.mjs
+++ web/public/src/app_replace_tests_run_e2e_goal.mjs
@@ -22,6 +22,62 @@ export async function app_replace_tests_run_e2e_goal(
   e2e_inner_fn,
 ) {
   try {
+    await playwright_refresh(page);
+    let json = json_to(goal);
+    let fns = app_replace_rule_sets_fns();
+    let last = list_last(fns);
+    let last_rs = last();
+    let goals = property_get(last_rs, "goals");
+    let goal_last = list_last(goals);
+    let rule_set_name = property_get(rule_set, "name");
+    await playwright_by_attribute_test_click(page, rule_set_name);
+    await playwright_by_attribute_test_click(page, json);
+    let rules_parsed = app_replace_rule_set_rules_get(rule_set);
+    async function each_goal(goal) {
+      let refresh_count = 0;
+      refresh_count = await app_replace_rule_set_attribute_refresh_count_assert(
+        refresh_count,
+        page,
+      );
+      let se = app_replace_start_end_get(goal);
+      let start = property_get(se, "start");
+      let end = property_get(se, "end");
+      let path = app_replace_rule_set_verify_goal_path(
+        rules_parsed,
+        start,
+        end,
+      );
+      async function each_step(p) {
+        let symbol_id = null;
+        ({ refresh_count, symbol_id } = await e2e_inner_fn(
+          p,
+          refresh_count,
+          page,
+          symbol_id,
+        ));
+        refresh_count = await app_replace_rule_set_attribute_refresh_click(
+          page,
+          symbol_id,
+          refresh_count,
+        );
+      }
+      await each_async(path, each_step);
+      let last_goal = false;
+      let eq2 = json_equal(rule_set, last_rs);
+      if (eq2) {
+        if (json_equal(goal, goal_last)) {
+          last_goal = true;
+        }
+      }
+      if (last_goal) {
+        let a = app_replace_rule_set_success_attribute_completed();
+        await playwright_by_attribute_test_exists_assert(page, a);
+      } else {
+        let name = app_replace_rule_set_success_attribute_next();
+        await playwright_by_attribute_test_click(page, name);
+      }
+    }
+    await each_goal(goal);
   } catch (e) {
     log_keep(app_replace_tests_run_e2e_goal.name, {
       goal,
@@ -29,56 +85,4 @@ export async function app_replace_tests_run_e2e_goal(
       e2e_inner_fn_name: e2e_inner_fn.name,
     });
   }
-  await playwright_refresh(page);
-  let json = json_to(goal);
-  let fns = app_replace_rule_sets_fns();
-  let last = list_last(fns);
-  let last_rs = last();
-  let goals = property_get(last_rs, "goals");
-  let goal_last = list_last(goals);
-  let rule_set_name = property_get(rule_set, "name");
-  await playwright_by_attribute_test_click(page, rule_set_name);
-  await playwright_by_attribute_test_click(page, json);
-  let rules_parsed = app_replace_rule_set_rules_get(rule_set);
-  async function each_goal(goal) {
-    let refresh_count = 0;
-    refresh_count = await app_replace_rule_set_attribute_refresh_count_assert(
-      refresh_count,
-      page,
-    );
-    let se = app_replace_start_end_get(goal);
-    let start = property_get(se, "start");
-    let end = property_get(se, "end");
-    let path = app_replace_rule_set_verify_goal_path(rules_parsed, start, end);
-    async function each_step(p) {
-      let symbol_id = null;
-      ({ refresh_count, symbol_id } = await e2e_inner_fn(
-        p,
-        refresh_count,
-        page,
-        symbol_id,
-      ));
-      refresh_count = await app_replace_rule_set_attribute_refresh_click(
-        page,
-        symbol_id,
-        refresh_count,
-      );
-    }
-    await each_async(path, each_step);
-    let last_goal = false;
-    let eq2 = json_equal(rule_set, last_rs);
-    if (eq2) {
-      if (json_equal(goal, goal_last)) {
-        last_goal = true;
-      }
-    }
-    if (last_goal) {
-      let a = app_replace_rule_set_success_attribute_completed();
-      await playwright_by_attribute_test_exists_assert(page, a);
-    } else {
-      let name = app_replace_rule_set_success_attribute_next();
-      await playwright_by_attribute_test_click(page, name);
-    }
-  }
-  await each_goal(goal);
 }
