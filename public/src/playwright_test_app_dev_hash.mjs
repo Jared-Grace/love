b31bb32aa6decc527a837c8a89d0b42d8ac9ae1c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 11:01:28 -0400

    function_auto_path ..\love\public\src\playwright_test_app_dev_hash.mjs

--- public/src/playwright_test_app_dev_hash.mjs
+++ public/src/playwright_test_app_dev_hash.mjs
@@ -1,7 +1,6 @@
 import { server_url_app_dev_hash } from "../../../love/public/src/server_url_app_dev_hash.mjs";
 import { playwright_test_url } from "../../../love/public/src/playwright_test_url.mjs";
 export async function playwright_test_app_dev_hash(app_fn, lambda) {
-  const hash = {};
   let url = await server_url_app_dev_hash(app_fn, hash);
   await playwright_test_url(url, lambda);
 }
