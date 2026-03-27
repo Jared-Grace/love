51da2f535878e4207a1b295cf14723756c83a7a6
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 19:33:57 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -4,7 +4,7 @@ import { app_replace_rule_set_logical_expressions_rules } from "../../../love/pu
 export function app_new_rule_set_new_2() {
   const rules = [];
   app_replace_rule_set_logical_expressions_rules(rules);
-  list_add(rules, "ex > le");
+  list_add(rules, "ase > le");
   let abbreviations = {};
   app_replace_rule_set_logical_expressions_abbreviations(abbreviations);
   let r = {
