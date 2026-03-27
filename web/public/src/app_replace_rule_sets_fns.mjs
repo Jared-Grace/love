8e06cbc5d7b7afaf5162a491ecab50df6b2222e0
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 19:32:33 -0400

    ara

--- web/public/src/app_replace_rule_sets_fns.mjs
+++ web/public/src/app_replace_rule_sets_fns.mjs
@@ -122,6 +122,7 @@ export function app_replace_rule_sets_fns() {
     app_replace_rule_set_expressions_relational,
     app_replace_rule_set_expressions_equality,
     app_replace_rule_set_logical_expressions,
+    app_new_rule_set_new_2,
   ];
   return fns;
 }
