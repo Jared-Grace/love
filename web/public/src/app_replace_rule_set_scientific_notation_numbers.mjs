2313dbdf53e323bc71da108071c13239ac5b19c4
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 17:06:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_scientific_notation_numbers.mjs

--- web/public/src/app_replace_rule_set_scientific_notation_numbers.mjs
+++ web/public/src/app_replace_rule_set_scientific_notation_numbers.mjs
@@ -6,7 +6,6 @@ export function app_replace_rule_set_scientific_notation_numbers() {
   const rules = app_replace_rule_set_decimals_rules();
   list_add_multiple(rules, [
     "sn > de se",
-    "sn > ig se",
     "se > eE ex",
     "eE > e",
     "eE > E",
