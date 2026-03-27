aed2e720f9cfef5c5060736036fcf8f534a35c24
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:25:34 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_next.mjs

--- public/src/app_replace_tests_run_e2e_next.mjs
+++ public/src/app_replace_tests_run_e2e_next.mjs
@@ -1,3 +1,4 @@
+import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { app_replace_test_e2e } from "../../../love/public/src/app_replace_test_e2e.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
@@ -7,7 +8,8 @@ import { list_take } from "../../../love/public/src/list_take.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 export async function app_replace_tests_run_e2e_next() {
   "it takes a long time to run all tests in browser in a row - instead, run all tests in parallel, and this function tests the first two rule sets so that next-ing is tested";
-  let rule_sets = app_replace_rule_sets();
+  "run dynamically in case modification in previous step of deploy";
+  let rule_sets = await function_run_args_none(app_replace_rule_sets);
   let taken = list_take(rule_sets, 2);
   async function on_page(page) {
     async function lambda3(rule_set) {
