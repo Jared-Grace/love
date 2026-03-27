9c12448d379916620f37e429ffa5a440e7aa0eb1
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:48:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_title.mjs

--- public/src/app_replace_rule_set_title.mjs
+++ public/src/app_replace_rule_set_title.mjs
@@ -10,7 +10,7 @@ export function app_replace_rule_set_title(context) {
   let rule_set = app_replace_rule_set_get(context);
   let rule_set_name = property_get(rule_set, "name");
   let title = html_p_text(root, "Rule set: " + rule_set_name);
-  html_style_margin_y(title, "0.5em");
+  html_style_margin_y(title, "0.1em");
   let why = property_get(rule_set, "why");
   let p = html_p_text(root, why);
   html_style_font_size(p, "0.9em");
