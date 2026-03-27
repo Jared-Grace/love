0375ceb1b92e9480463482ccc3cb37e1458ea0f0
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:46:35 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_title.mjs

--- web/public/src/app_replace_rule_set_title.mjs
+++ web/public/src/app_replace_rule_set_title.mjs
@@ -1,3 +1,4 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
 import { app_replace_rule_sets_progress_bar } from "../../../love/public/src/app_replace_rule_sets_progress_bar.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -10,6 +11,7 @@ export function app_replace_rule_set_title(context) {
   html_p_text(root, "Rule set: " + rule_set_name);
   let why = property_get(rule_set, "why");
   let p = html_p_text(root, why);
+  html_style_set(b, style_key, style_value);
   let r = {
     rule_set,
     rule_set_name,
