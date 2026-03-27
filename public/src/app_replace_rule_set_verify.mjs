9d19ac77fcaa9e662114b8a3de9d76091e77e4f6
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:22:48 -0400

    p

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -13,8 +13,8 @@ export function app_replace_rule_set_verify(rule_set) {
   let r = app_replace_rule_set_verify_goal_curried_right(rules_parsed);
   let dfss = list_map(goals, r);
   let paths = list_map(dfss, app_replace_rule_set_verify_path_get);
-  log(app_replace_rule_set_verify.name, {
+  log(app_replace_rule_set_verify.name, 
     paths,
-  });
+  );
   return dfss;
 }
