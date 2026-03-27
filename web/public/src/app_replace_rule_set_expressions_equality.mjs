5cc871ee1800433c2fb3530bdb9fede0ea79077c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 15:12:11 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_equality.mjs

--- web/public/src/app_replace_rule_set_expressions_equality.mjs
+++ web/public/src/app_replace_rule_set_expressions_equality.mjs
@@ -6,7 +6,7 @@ import { app_replace_rule_set_expressions_relational_rules } from "../../../love
 export function app_replace_rule_set_expressions_equality() {
   const rules = [];
   app_replace_rule_set_expressions_relational_rules(rules);
-  list_add_multiple(rules, ["re > ae", "re > re ro ae", "ro > <", "ro > >>"]);
+  list_add_multiple(rules, ["re > ae", "re > re === ae"]);
   list_add(rules, "ex > re");
   let abbreviations = {};
   app_replace_rule_set_expressions_relational_abbreviations(abbreviations);
