bf96ed4bff300a5f65358e26c4c3fe90276e7c1b
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:19:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_all.mjs

--- web/public/src/app_replace_rule_set_verify_all.mjs
+++ web/public/src/app_replace_rule_set_verify_all.mjs
@@ -1,7 +1,7 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
 export function app_replace_rule_set_verify_all() {
   let rule_sets = app_replace_rule_sets();
-  each(rule_sets, app_replace_rule_set_verify);
+  list_map(rule_sets, app_replace_rule_set_verify);
 }
