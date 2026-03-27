6a41bb20810afe8e8893f7b3c7d593f6c256d5cb
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:29:31 -0400

    function_auto_path ..\love\public\src\function_run_args_none_fn.mjs

--- web/public/src/function_run_args_none_fn.mjs
+++ web/public/src/function_run_args_none_fn.mjs
@@ -1,6 +1,5 @@
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
-import { fn_name } from "../../../love/public/src/fn_name.mjs";
-export async function function_run_args_none_fn(fn_name) {
-  let r = await function_run_args_none(fn_name);
+export async function function_run_args_none_fn(fn) {
+  let r = await function_run_args_none(fn.name);
   return r;
 }
