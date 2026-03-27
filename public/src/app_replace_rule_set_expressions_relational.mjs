c40eac053b6370b4f72a6bb5510fa3c1e786fc03
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 10:46:39 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_relational.mjs

--- public/src/app_replace_rule_set_expressions_relational.mjs
+++ public/src/app_replace_rule_set_expressions_relational.mjs
@@ -1,15 +1,10 @@
+import { app_replace_rule_set_expressions_relational_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_relational_abbreviations.mjs";
 import { app_replace_rule_set_expressions_relational_rules } from "../../../love/public/src/app_replace_rule_set_expressions_relational_rules.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { app_replace_rule_set_expresions_additive_abbreviations } from "../../../love/public/src/app_replace_rule_set_expresions_additive_abbreviations.mjs";
 export function app_replace_rule_set_expressions_relational() {
   const rules = [];
   app_replace_rule_set_expressions_relational_rules(rules);
   let abbreviations = {};
-  app_replace_rule_set_expresions_additive_abbreviations(abbreviations);
-  object_merge(abbreviations, {
-    re: ["", "r", "elational ", "e", "xpression"],
-    ro: ["", "r", "elational ", "o", "perator"],
-  });
+  app_replace_rule_set_expressions_relational_abbreviations(abbreviations);
   let r = {
     name: "Expressions relational",
     abbreviations,
