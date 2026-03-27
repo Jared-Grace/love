c51a614d0879fed7b063ee921828c4a1701486df
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 19:41:07 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,9 +1,16 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_logical_expressions_abbreviations } from "../../../love/public/src/app_replace_rule_set_logical_expressions_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_logical_expressions_rules } from "../../../love/public/src/app_replace_rule_set_logical_expressions_rules.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
   app_replace_rule_set_logical_expressions_rules(rules);
+  list_add_multiple(rules, [
+    "ase > le",
+    "ase > ase aso le",
+    "aso > =",
+    "aso > >>",
+  ]);
   list_add(rules, "ase > le");
   let abbreviations = {};
   app_replace_rule_set_logical_expressions_abbreviations(abbreviations);
