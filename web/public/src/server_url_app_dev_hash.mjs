cc0d008060eca84a3d2b2a4a857f85cd63c3da59
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 11:01:02 -0400

    pb server_url_app_dev_hash

--- web/public/src/server_url_app_dev_hash.mjs
+++ web/public/src/server_url_app_dev_hash.mjs
@@ -5,7 +5,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
 import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
-export async function server_url_app_dev_hash(hash, app_fn) {
+export async function server_url_app_dev_hash(app_fn, hash) {
   let url2 = hash_to_url(hash);
   let without = app_prefix_without_fn(app_fn);
   let r = await app_shared_name_search_info(without);
