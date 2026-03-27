801f2669de32ccd0b59f184e991ca5cac040ce34
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:27:45 -0400

    function_auto_path ..\love\public\src\list_map_properties.mjs

--- web/public/src/list_map_properties.mjs
+++ web/public/src/list_map_properties.mjs
@@ -1,9 +1,10 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 export function list_map_properties(list, properties) {
+  let mapped = list;
   function lambda(p) {
-    list = list_map_property(list, p);
+    mapped = list_map_property(mapped, p);
   }
   each(properties, lambda);
-  return list;
+  return mapped;
 }
