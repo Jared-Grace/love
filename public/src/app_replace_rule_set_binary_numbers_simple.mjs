7196c2cf6dda133a3eca5b9e6b92c495f6fb52fd
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:28:50 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_two_different_grow_same.mjs

--- public/src/app_replace_rule_set_binary_numbers_simple.mjs
+++ public/src/app_replace_rule_set_binary_numbers_simple.mjs
@@ -4,7 +4,7 @@ export function app_replace_rule_set_binary_numbers_simple() {
     name: "Binary Numbers Simple",
     rules: ["0 > 0 0", "0 > 1"],
     goals: app_replace_rule_set_binary_numbers_goals("0"),
-    why: "These replacement rules demonstrate a simple grammar for generating binary numbers. Starting from '0', the rules allow either doubling the current sequence by appending another '0' (representing binary left-shift) or changing the last '0' to a '1'. This process can generate all binary numbers by repeated application, illustrating how binary counting can be modeled with simple replacement rules.",
+    why: "The rules generate all binary numbers starting with '1' by repeatedly doubling the length of zeros and then replacing the last zero with a one, demonstrating a simple binary counting system.",
   };
   return r;
 }
