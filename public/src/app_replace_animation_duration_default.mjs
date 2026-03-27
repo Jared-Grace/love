227241bb082fb4c6fc75bb45148518dbf4f85d0a
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 11:03:20 -0400

    function_auto_path ..\love\public\src\app_replace_animation_duration_default.mjs

--- public/src/app_replace_animation_duration_default.mjs
+++ public/src/app_replace_animation_duration_default.mjs
@@ -1,5 +1,7 @@
+import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
 import { app_replace_animation_duration_max } from "../../../love/public/src/app_replace_animation_duration_max.mjs";
 export function app_replace_animation_duration_default() {
+  let hash = html_hash_object_get();
   let r = app_replace_animation_duration_max();
   return r;
 }
