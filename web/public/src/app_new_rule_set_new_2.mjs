3e2cf2d26f88de47f3ab6c765bd2f1d00b794858
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 09:32:04 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,3 +1,4 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expresions_additive_abbreviations } from "../../../love/public/src/app_replace_rule_set_expresions_additive_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_expresions_additive_rules } from "../../../love/public/src/app_replace_rule_set_expresions_additive_rules.mjs";
@@ -7,6 +8,10 @@ export function app_new_rule_set_new_2() {
   list_add(rules, "ex > coe");
   let abbreviations = {};
   app_replace_rule_set_expresions_additive_abbreviations(abbreviations);
+  object_merge(abbreviations, {
+    coe: ["", "co", "mparison ", "e", "xpression"],
+    coo: ["", "co", "mparison ", "o", "perator"],
+  });
   let r = {
     name: "Expressions comparison",
     abbreviations,
