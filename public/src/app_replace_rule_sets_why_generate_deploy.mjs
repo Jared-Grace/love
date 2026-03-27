291c65c11e40719b16ff7dcacbd10c8c7b9b2b04
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 15:32:29 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate_deploy.mjs

--- public/src/app_replace_rule_sets_why_generate_deploy.mjs
+++ public/src/app_replace_rule_sets_why_generate_deploy.mjs
@@ -1,9 +1,5 @@
-import { app_replace_rule_sets_fns_migrate_capitalization_upper } from "../../../love/public/src/app_replace_rule_sets_fns_migrate_capitalization_upper.mjs";
-import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app_replace_rule_sets_why_generate.mjs";
 import { app_replace_deploy } from "../../../love/public/src/app_replace_deploy.mjs";
 export async function app_replace_rule_sets_why_generate_deploy() {
-  let result = await app_replace_rule_sets_fns_migrate_capitalization_upper();
-  await app_replace_rule_sets_why_generate();
   let r = await app_replace_deploy();
   return r;
 }
