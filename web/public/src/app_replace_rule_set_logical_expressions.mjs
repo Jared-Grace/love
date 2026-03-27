ed758e4eeb1d9213c5d6a5133b1975cb2c04fa3c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:16:58 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_logical_expressions.mjs

--- web/public/src/app_replace_rule_set_logical_expressions.mjs
+++ web/public/src/app_replace_rule_set_logical_expressions.mjs
@@ -3,7 +3,7 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_expressions_equality_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_equality_abbreviations.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_expressions_equality_rules } from "../../../love/public/src/app_replace_rule_set_expressions_equality_rules.mjs";
-export function app_new_rule_set_new_2() {
+export function app_replace_rule_set_logical_expressions() {
   const rules = [];
   app_replace_rule_set_expressions_equality_rules(rules);
   list_add_multiple(rules, ["le > ee", "le > le lo ee", "lo > &&", "lo > ||"]);
