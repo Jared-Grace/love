95dbb49914e486f0219804504f71290417bd3d43
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 11:03:49 -0400

    function_auto_path ..\love\public\src\app_replace_animation_duration_default.mjs

--- web/public/src/app_replace_animation_duration_default.mjs
+++ web/public/src/app_replace_animation_duration_default.mjs
@@ -1,9 +1,9 @@
-import { function_param_swap_end } from "../../../love/public/src/function_param_swap_end.mjs";
+import { property_exists } from "../../../love/public/src/property_exists.mjs";
 import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
 import { app_replace_animation_duration_max } from "../../../love/public/src/app_replace_animation_duration_max.mjs";
-export async function app_replace_animation_duration_default() {
+export function app_replace_animation_duration_default() {
   let hash = html_hash_object_get();
-  await function_param_swap_end(f_name);
+  let exists = property_exists(object, property_name);
   let r = app_replace_animation_duration_max();
   return r;
 }
