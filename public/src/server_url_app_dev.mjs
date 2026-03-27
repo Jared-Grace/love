a2e115f74a460ac99cd0feecfe629a3a93b3662f
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 10:59:49 -0400

    function_auto_path ..\love\public\src\server_url_app_dev.mjs

--- public/src/server_url_app_dev.mjs
+++ public/src/server_url_app_dev.mjs
@@ -1,3 +1,4 @@
+import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
 import { server_url } from "../../../love/public/src/server_url.mjs";
 import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
@@ -5,6 +6,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
 export async function server_url_app_dev(app_fn) {
+  let url2 = hash_to_url(hash);
   let without = app_prefix_without_fn(app_fn);
   let r = await app_shared_name_search_info(without);
   let f_path_dev = property_get(r, "f_path_dev");
