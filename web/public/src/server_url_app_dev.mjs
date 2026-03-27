5f7cf787ea2172978ba8201c603b674d64a3d046
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 11:00:44 -0400

    function_auto_path ..\love\public\src\server_url_app_dev.mjs

--- web/public/src/server_url_app_dev.mjs
+++ web/public/src/server_url_app_dev.mjs
@@ -6,13 +6,13 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
 export async function server_url_app_dev(app_fn) {
-  let url2 = hash_to_url({});
-  return url2;
+  let url2 = hash_to_url(hash);
+  const hash = {};
   let without = app_prefix_without_fn(app_fn);
   let r = await app_shared_name_search_info(without);
   let f_path_dev = property_get(r, "f_path_dev");
   let previous = folder_previous();
   let together = text_prefix_without(f_path_dev, previous);
-  const url = server_url() + together + "";
+  const url = server_url() + together + "" + url2;
   return url;
 }
