cff384ba9313010519f62566634a690264698b98
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:52:25 -0400

    function_auto_path ..\love\public\src\html_style_margin_y.mjs

--- web/public/src/html_style_margin_y.mjs
+++ web/public/src/html_style_margin_y.mjs
@@ -1,9 +1,8 @@
+import { html_style_margin_bottom } from "../../../love/public/src/html_style_margin_bottom.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function html_style_margin_y(component, value) {
   html_style_assign(component, {
     "margin-top": value,
   });
-  html_style_assign(component, {
-    "margin-bottom": value,
-  });
+  html_style_margin_bottom(component, value);
 }
