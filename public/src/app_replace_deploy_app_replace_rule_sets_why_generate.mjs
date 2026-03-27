4cc2630a5dd5d5913739753296d0318963fdad0f
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 15:27:25 -0400

    function_auto_path ..\love\public\src\app_replace_deploy_app_replace_rule_sets_why_generate.mjs

--- public/src/app_replace_deploy_app_replace_rule_sets_why_generate.mjs
+++ public/src/app_replace_deploy_app_replace_rule_sets_why_generate.mjs
@@ -1,4 +1,5 @@
 import { app_replace_deploy } from "../../../love/public/src/app_replace_deploy.mjs";
 export async function app_replace_deploy_app_replace_rule_sets_why_generate() {
-  return await app_replace_deploy();
+  let r = await app_replace_deploy();
+  return r;
 }
