3b383c91f3f61b4c99e025c693e66f5d9342d474
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 14:14:06 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,3 +1,4 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expressions_relational_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_relational_abbreviations.mjs";
 import { app_replace_rule_set_expressions_relational_rules } from "../../../love/public/src/app_replace_rule_set_expressions_relational_rules.mjs";
 export function app_new_rule_set_new_2() {
@@ -5,6 +6,10 @@ export function app_new_rule_set_new_2() {
   app_replace_rule_set_expressions_relational_rules(rules);
   let abbreviations = {};
   app_replace_rule_set_expressions_relational_abbreviations(abbreviations);
+  object_merge(abbreviations, {
+    ee: ["", "e", "quality ", "e", "xpression"],
+    eo: ["", "e", "elational ", "o", "perator"],
+  });
   let r = {
     name: "Expressions equality",
     rules,
