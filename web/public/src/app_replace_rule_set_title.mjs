2a1c614d2cb813a363689dd4602af9faa8d6337a
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:47:59 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_title.mjs

--- web/public/src/app_replace_rule_set_title.mjs
+++ web/public/src/app_replace_rule_set_title.mjs
@@ -1,3 +1,4 @@
+import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
 import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
 import { app_replace_rule_sets_progress_bar } from "../../../love/public/src/app_replace_rule_sets_progress_bar.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
@@ -9,6 +10,7 @@ export function app_replace_rule_set_title(context) {
   let rule_set = app_replace_rule_set_get(context);
   let rule_set_name = property_get(rule_set, "name");
   html_p_text(root, "Rule set: " + rule_set_name);
+  html_style_margin_y(component, value);
   let why = property_get(rule_set, "why");
   let p = html_p_text(root, why);
   html_style_font_size(p, "0.9em");
