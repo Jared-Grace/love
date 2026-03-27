7d7367f8707cc43eab7a2020e5729c4f7d0c2fa2
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:29:56 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_run.mjs

--- public/src/app_replace_rule_sets_run.mjs
+++ public/src/app_replace_rule_sets_run.mjs
@@ -1,7 +1,7 @@
+import { function_run_args_none_fn } from "../../../love/public/src/function_run_args_none_fn.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
-import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 export async function app_replace_rule_sets_run() {
   "run dynamically in case modification in previous step of deploy";
-  let rule_sets = await function_run_args_none(app_replace_rule_sets);
+  let rule_sets = await function_run_args_none_fn(app_replace_rule_sets);
   return rule_sets;
 }
