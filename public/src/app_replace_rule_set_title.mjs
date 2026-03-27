065b97f87bce1e9755539c78f6d18ead25c2bd36
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:06:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_title.mjs

--- public/src/app_replace_rule_set_title.mjs
+++ public/src/app_replace_rule_set_title.mjs
@@ -2,11 +2,11 @@ import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_set_get } from "../../../love/public/src/app_replace_rule_set_get.mjs";
 export function app_replace_rule_set_title(context) {
-  let item = app_replace_rule_set_get(context);
-  let rule_name = property_get(item, "name");
+  let rule_set = app_replace_rule_set_get(context);
+  let rule_name = property_get(rule_set, "name");
   html_p_text(root, "Rule set: " + rule_name);
   let r = {
-    item,
+    item: rule_set,
     rule_name,
   };
   return r;
