b8ab0f73c79c0d2a0da787e6d5e6441a6114bc07
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 15:19:16 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_equality.mjs

--- public/src/app_replace_rule_set_expressions_equality.mjs
+++ public/src/app_replace_rule_set_expressions_equality.mjs
@@ -27,6 +27,10 @@ export function app_replace_rule_set_expressions_equality() {
         start: "mue === mue",
         end: "mae === mae",
       },
+      {
+        start: "mae === mae",
+        end: "mae === mae",
+      },
     ],
   };
   return r;
