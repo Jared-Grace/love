eff69ee90a7923e899e402970f119ace0215e82c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:26:23 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_next.mjs

--- web/public/src/app_replace_tests_run_e2e_next.mjs
+++ web/public/src/app_replace_tests_run_e2e_next.mjs
@@ -1,11 +1,10 @@
-import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
+import { app_replace_rule_sets_run } from "../../../love/public/src/app_replace_rule_sets_run.mjs";
 import { app_replace_test_e2e } from "../../../love/public/src/app_replace_test_e2e.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
 import { app_replace_tests_run_e2e_goal } from "../../../love/public/src/app_replace_tests_run_e2e_goal.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
-import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 export async function app_replace_tests_run_e2e_next() {
   "it takes a long time to run all tests in browser in a row - instead, run all tests in parallel, and this function tests the first two rule sets so that next-ing is tested";
   let rule_sets = await app_replace_rule_sets_run();
