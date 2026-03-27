a3c78a40ed792dd93c291ddf6978c1a5522969b3
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 14:13:16 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -7,7 +7,7 @@ export function app_new_rule_set_new_2() {
   app_replace_rule_set_expressions_relational_abbreviations(abbreviations);
   let r = {
     name: "TODO",
-    rules: ["a > b"],
+    rules,
     goals: [
       {
         start: "a",
