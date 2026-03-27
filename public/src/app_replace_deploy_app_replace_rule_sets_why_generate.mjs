cb5aafe613925e4a77dc317bba6e9abea6c5087c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 15:27:22 -0400

    fws rd app_replace_rule_sets_why_generate

---
+++ public/src/app_replace_deploy_app_replace_rule_sets_why_generate.mjs
@@ -0,0 +1,4 @@
+import { app_replace_deploy } from "../../../love/public/src/app_replace_deploy.mjs";
+export async function app_replace_deploy_app_replace_rule_sets_why_generate() {
+  return await app_replace_deploy();
+}
