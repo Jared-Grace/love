379ad5829d725884e8d335e7d49caede596350ac
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:43:05 -0400

    function_auto_path ..\love\public\src\list_includes_all.mjs

--- web/public/src/list_includes_all.mjs
+++ web/public/src/list_includes_all.mjs
@@ -1,7 +1,7 @@
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { list_includes_curried } from "../../../love/public/src/list_includes_curried.mjs";
-export function list_includes_all(rules, unique) {
+export function list_includes_all(list, unique) {
   let r2 = list_includes_curried(unique);
-  let all_rules_used = list_all(rules, r2);
+  let all_rules_used = list_all(list, r2);
   return all_rules_used;
 }
