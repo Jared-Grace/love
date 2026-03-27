011c8aa19003f6265025299237b404e98bb090c5
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 21:07:28 -0400

    p

--- public/src/app_replace_rule_set_expressions_additive.mjs
+++ public/src/app_replace_rule_set_expressions_additive.mjs
@@ -68,6 +68,10 @@ export function app_replace_rule_set_expressions_additive() {
         start: "ce ao ce mo ce",
         end: "nu ao ce mo ce",
       },
+      {
+        start: "nu ao ce mo ce",
+        end: "nu ao nu mo ce",
+      },
     ],
     why: "The replacement rules define a context-free grammar for parsing arithmetic and function call expressions similar to those in JavaScript, including literals, identifiers, unary and binary operators, function calls, and member access, demonstrating how complex expressions are constructed from simpler components.",
   };
