707a801c904b551e3a542c07139be563cdc6f901
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:14:56 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_goal_curried_right.mjs

--- web/public/src/app_replace_rule_set_verify_goal_curried_right.mjs
+++ web/public/src/app_replace_rule_set_verify_goal_curried_right.mjs
@@ -1,6 +1,8 @@
 import { app_replace_rule_set_verify_goal } from "../../../love/public/src/app_replace_rule_set_verify_goal.mjs";
 export function app_replace_rule_set_verify_goal_curried_right(rules_parsed) {
-  return function app_replace_rule_set_verify_goal_curried_right_result(goal) {
-    return app_replace_rule_set_verify_goal(goal, rules_parsed);
+  let r = function app_replace_rule_set_verify_goal_curried_right_result(goal) {
+    let dfs = app_replace_rule_set_verify_goal(goal, rules_parsed);
+    return dfs;
   };
+  return r;
 }
