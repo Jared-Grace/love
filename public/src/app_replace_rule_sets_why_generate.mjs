ab9395e39a3152854d294bf07439bcf4b8b33a4c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:26:44 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate.mjs

--- public/src/app_replace_rule_sets_why_generate.mjs
+++ public/src/app_replace_rule_sets_why_generate.mjs
@@ -1,4 +1,4 @@
-import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
+import { app_replace_rule_sets_fns_run } from "../../../love/public/src/app_replace_rule_sets_fns_run.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_list_type_nodes } from "../../../love/public/src/js_list_type_nodes.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
@@ -19,12 +19,10 @@ import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_sets_why_generate_single } from "../../../love/public/src/app_replace_rule_sets_why_generate_single.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function app_replace_rule_sets_why_generate() {
-  "run dynamically in case modification in previous step of deploy";
-  let r2 = await function_run_args_none(app_replace_rule_sets_fns);
+  let r2 = await app_replace_rule_sets_fns_run();
   function lambda2(fn) {
     let r3 = {
       f_name: fn.name,
