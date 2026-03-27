760586d0a4ed25e3976a277df7c2cac032d08691
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 11:01:02 -0400

    pb server_url_app_dev_hash

--- public/src/server_url_app_dev.mjs
+++ public/src/server_url_app_dev.mjs
@@ -1,6 +1,6 @@
 import { server_url_app_dev_hash } from "../../../love/public/src/server_url_app_dev_hash.mjs";
 export async function server_url_app_dev(app_fn) {
   const hash = {};
-  const url = await server_url_app_dev_hash(hash, app_fn);
+  const url = await server_url_app_dev_hash(app_fn, hash);
   return url;
 }
