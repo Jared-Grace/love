199b75b2ab360061b0649002de7c0cbd377e2612
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 14:14:17 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,9 +1,13 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expressions_relational_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_relational_abbreviations.mjs";
 import { app_replace_rule_set_expressions_relational_rules } from "../../../love/public/src/app_replace_rule_set_expressions_relational_rules.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
   app_replace_rule_set_expressions_relational_rules(rules);
+  list_add_multiple(rules, ["re > ae", "re > re ro ae", "ro > <", "ro > >>"]);
+  list_add(rules, "ex > re");
   let abbreviations = {};
   app_replace_rule_set_expressions_relational_abbreviations(abbreviations);
   object_merge(abbreviations, {
