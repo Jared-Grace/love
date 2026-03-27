bb43918358529baaf751421b59ffe1214316d4af
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:05:59 -0400

    function_auto_path ..\love\public\src\app_replace_goals.mjs

---
+++ web/public/src/app_replace_rule_set_title.mjs
@@ -0,0 +1,13 @@
+import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_replace_rule_set_get } from "../../../love/public/src/app_replace_rule_set_get.mjs";
+export function app_replace_rule_set_title(context, root) {
+  let item = app_replace_rule_set_get(context);
+  let rule_name = property_get(item, "name");
+  html_p_text(root, "Rule set: " + rule_name);
+  let r = {
+    item,
+    rule_name,
+  };
+  return r;
+}
