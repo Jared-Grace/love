8f3ca56dbaf03afbbfe3b9ac1bf9bc9880ee7d0b
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 17:20:07 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,4 +1,12 @@
+import { app_replace_rule_set_expressions_equality_abbreviations } from "../../../love/public/src/app_replace_rule_set_expressions_equality_abbreviations.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_replace_rule_set_expressions_equality_rules } from "../../../love/public/src/app_replace_rule_set_expressions_equality_rules.mjs";
 export function app_new_rule_set_new_2() {
+  const rules = [];
+  app_replace_rule_set_expressions_equality_rules(rules);
+  list_add(rules, "ex > ee");
+  let abbreviations = {};
+  app_replace_rule_set_expressions_equality_abbreviations(abbreviations);
   let r = {
     name: "Logical Expressions",
     rules: ["a > b"],
