efea0114bc0958291f04ea695e8c8d7dbd32f7e7
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:18:04 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_named.mjs

--- public/src/app_replace_rule_set_verify_named.mjs
+++ public/src/app_replace_rule_set_verify_named.mjs
@@ -1,8 +1,11 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_replace_rule_set_verify_path_get } from "../../../love/public/src/app_replace_rule_set_verify_path_get.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
 export async function app_replace_rule_set_verify_named(rule_set_name) {
   let rs = await function_run_args_none(rule_set_name);
   let dfss = app_replace_rule_set_verify(rs);
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
   let path = app_replace_rule_set_verify_path_get(dfs);
 }
