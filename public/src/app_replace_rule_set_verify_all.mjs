6395a975193c15f4e7ff6e7014b3522532498006
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:26:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_all.mjs

--- public/src/app_replace_rule_set_verify_all.mjs
+++ public/src/app_replace_rule_set_verify_all.mjs
@@ -1,9 +1,7 @@
-import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
-import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
+import { app_replace_rule_sets_run } from "../../../love/public/src/app_replace_rule_sets_run.mjs";
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
 import { each } from "./each.mjs";
 export async function app_replace_rule_set_verify_all() {
-  "run dynamically in case modification in previous step of deploy";
-  let rule_sets = await function_run_args_none(app_replace_rule_sets);
+  let rule_sets = await app_replace_rule_sets_run();
   each(rule_sets, app_replace_rule_set_verify);
 }
