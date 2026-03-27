e81996b4593bb958a68b79efd4b34b28a5767472
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 09:40:52 -0400

    function_auto_path ..\love\public\src\text_replace_curried_right_2.mjs

--- public/src/text_replace_curried_right_2.mjs
+++ public/src/text_replace_curried_right_2.mjs
@@ -1,8 +1,8 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 export function text_replace_curried_right_2(from, to) {
   let r2 = function text_replace_curry_right_result(t) {
-    let after = text_replace(t, from, to);
-    return after;
+    let replaced = text_replace(t, from, to);
+    return replaced;
   };
   return r2;
 }
