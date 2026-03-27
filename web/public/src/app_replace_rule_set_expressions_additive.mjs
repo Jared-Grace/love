31adfcf78d6c7d46a9d73af1c6490b850497f5fa
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 21:05:59 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_additive.mjs

--- web/public/src/app_replace_rule_set_expressions_additive.mjs
+++ web/public/src/app_replace_rule_set_expressions_additive.mjs
@@ -58,7 +58,7 @@ export function app_replace_rule_set_expressions_additive() {
       },
       {
         start: "ae",
-        end: "ce ao mue mo ue",
+        end: "ce ao ce mo ue",
       },
     ],
     why: "The replacement rules define a context-free grammar for parsing arithmetic and function call expressions similar to those in JavaScript, including literals, identifiers, unary and binary operators, function calls, and member access, demonstrating how complex expressions are constructed from simpler components.",
