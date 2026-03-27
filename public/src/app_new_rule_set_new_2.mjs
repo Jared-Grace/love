9b5aa881c6d4ca75267461a3a970087d03591613
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 09:30:24 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,6 +1,15 @@
+import { app_replace_rule_set_expresions_additive_abbreviations } from "../../../love/public/src/app_replace_rule_set_expresions_additive_abbreviations.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_replace_rule_set_expresions_additive_rules } from "../../../love/public/src/app_replace_rule_set_expresions_additive_rules.mjs";
 export function app_new_rule_set_new_2() {
+  const rules = [];
+  app_replace_rule_set_expresions_additive_rules(rules);
+  list_add(rules, "ex > ae");
+  let abbreviations = {};
+  app_replace_rule_set_expresions_additive_abbreviations(abbreviations);
   let r = {
     name: "Expressions comparison",
+    abbreviations,
     rules: ["a > b"],
     goals: [
       {
