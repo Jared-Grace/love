dcdbf1ef37794eeb74005f538e1ce8b8089023a7
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 17:18:30 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_scientific_notation_numbers.mjs

--- public/src/app_replace_rule_set_scientific_notation_numbers.mjs
+++ public/src/app_replace_rule_set_scientific_notation_numbers.mjs
@@ -26,7 +26,6 @@ export function app_replace_rule_set_scientific_notation_numbers() {
     name: "Scientific Notation Numbers",
     abbreviations,
     rules,
-    rules_unused: ["in > pi ig"],
     goals: [
       {
         start: "sn",
