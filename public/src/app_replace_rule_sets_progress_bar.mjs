8f23ec843794c406ef954519291af374403d2512
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:11:38 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_progress_bar.mjs

--- public/src/app_replace_rule_sets_progress_bar.mjs
+++ public/src/app_replace_rule_sets_progress_bar.mjs
@@ -1,8 +1,10 @@
+import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { html_progress_bar } from "../../../love/public/src/html_progress_bar.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_set_index_get } from "../../../love/public/src/app_replace_rule_set_index_get.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function app_replace_rule_sets_progress_bar(context) {
+  let rule_sets = app_replace_rule_sets();
   let size = list_size(rule_sets);
   let rule_set_index = app_replace_rule_set_index_get(context);
   let root2 = property_get(context, "root");
