e71d2762f21cfa4689868982ed231b4e72cee805
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 19:33:49 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,4 +1,12 @@
+import { app_replace_rule_set_logical_expressions_abbreviations } from "../../../love/public/src/app_replace_rule_set_logical_expressions_abbreviations.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { app_replace_rule_set_logical_expressions_rules } from "../../../love/public/src/app_replace_rule_set_logical_expressions_rules.mjs";
 export function app_new_rule_set_new_2() {
+  const rules = [];
+  app_replace_rule_set_logical_expressions_rules(rules);
+  list_add(rules, "ex > le");
+  let abbreviations = {};
+  app_replace_rule_set_logical_expressions_abbreviations(abbreviations);
   let r = {
     name: "Assignment Expressions",
     rules: ["a > b"],
