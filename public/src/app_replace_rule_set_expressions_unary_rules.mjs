af7ab90a35727bb758cb708a5a75f6d998bb8ce7
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 09:32:24 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_unary_rules.mjs

--- public/src/app_replace_rule_set_expressions_unary_rules.mjs
+++ public/src/app_replace_rule_set_expressions_unary_rules.mjs
@@ -3,7 +3,7 @@ import { app_replace_rule_set_expressions_function_calls_rules } from "../../../
 export function app_replace_rule_set_expressions_unary_rules(rules) {
   app_replace_rule_set_expressions_function_calls_rules(rules);
   list_add_multiple(rules, [
-    "ue > ce",
+    "ue > cae",
     "ue > uo ue",
     "uo > !",
     "uo > -",
