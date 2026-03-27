05b2fd258bfce683e284d0069150551cc31e1976
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:44:39 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -24,11 +24,11 @@ export function app_replace_rule_set_verify(rule_set) {
   let squashed2 = list_squash(paths);
   let mapped = list_map_properties(squashed2, ["rule", "original"]);
   let unique = list_unique(mapped);
-  let list = property_get(rule_set, "rules");
-  let includes_all = list_includes_all_assert(list, unique);
+  let rules = property_get(rule_set, "rules");
+  let includes_all = list_includes_all_assert(rules, unique);
   log(app_replace_rule_set_verify.name, {
     unique: unique,
-    rules: list,
+    rules: rules,
     all_rules_used: includes_all,
   });
 }
