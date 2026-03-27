c61db0b0d5f22b23cf7d42b9c2de9befc2e330dd
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:19:08 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_named.mjs

--- web/public/src/app_replace_rule_set_verify_named.mjs
+++ web/public/src/app_replace_rule_set_verify_named.mjs
@@ -6,7 +6,7 @@ import { app_replace_rule_set_verify } from "../../../love/public/src/app_replac
 export async function app_replace_rule_set_verify_named(rule_set_name) {
   let rs = await function_run_args_none(rule_set_name);
   let dfss = app_replace_rule_set_verify(rs);
-  let paths = list_map(list, app_replace_rule_set_verify_path_get);
+  let paths = list_map(dfss, app_replace_rule_set_verify_path_get);
   log(app_replace_rule_set_verify_named.name, {
     paths,
   });
