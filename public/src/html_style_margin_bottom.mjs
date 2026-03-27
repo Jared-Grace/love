ae5db11832ae6cecf4fe25ffdb815d8628fdb6fa
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:52:25 -0400

    function_auto_path ..\love\public\src\html_style_margin_y.mjs

---
+++ public/src/html_style_margin_bottom.mjs
@@ -0,0 +1,6 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function html_style_margin_bottom(component, value) {
+  html_style_assign(component, {
+    "margin-bottom": value,
+  });
+}
