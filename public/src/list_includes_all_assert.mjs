9474de546e337efa36ea5b869b28a35a51cf509d
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:44:45 -0400

    function_auto_path ..\love\public\src\list_includes_all_assert.mjs

--- public/src/list_includes_all_assert.mjs
+++ public/src/list_includes_all_assert.mjs
@@ -1,8 +1,10 @@
+import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { list_includes_all } from "../../../love/public/src/list_includes_all.mjs";
 export function list_includes_all_assert(list, items) {
   let includes_all = list_includes_all(list, items);
   function lambda2() {
+    let difference = list_difference(list2, other);
     let r3 = {};
     return r3;
   }
