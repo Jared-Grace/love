1a3761bb22081df28da268e93961e94b8acbc2e9
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:21:12 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_all.mjs

--- web/public/src/app_replace_rule_set_verify_all.mjs
+++ web/public/src/app_replace_rule_set_verify_all.mjs
@@ -1,10 +1,7 @@
-import { list_squash } from "../../../love/public/src/list_squash.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { app_replace_rule_set_verify } from "../../../love/public/src/app_replace_rule_set_verify.mjs";
 export function app_replace_rule_set_verify_all() {
   let rule_sets = app_replace_rule_sets();
-  let dfsss = list_map(rule_sets, app_replace_rule_set_verify);
-  let squashed = list_squash(dfsss);
-  return squashed;
+  list_map(rule_sets, app_replace_rule_set_verify);
 }
