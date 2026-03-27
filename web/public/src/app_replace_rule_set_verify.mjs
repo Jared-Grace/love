a934890d857670490550989fa18e041c10303b7c
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 16:14:15 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify.mjs

--- web/public/src/app_replace_rule_set_verify.mjs
+++ web/public/src/app_replace_rule_set_verify.mjs
@@ -1,6 +1,6 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { app_replace_rule_set_verify_goal } from "../../../love/public/src/app_replace_rule_set_verify_goal.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { app_replace_rule_set_rules_get } from "../../../love/public/src/app_replace_rule_set_rules_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_replace_rule_set_verify(rule_set) {
@@ -11,10 +11,7 @@ export function app_replace_rule_set_verify(rule_set) {
   let goals = property_get(rule_set, "goals");
   function lambda2(goal) {
     let dfs = app_replace_rule_set_verify_goal(goal, rules_parsed);
-    log(app_replace_rule_set_verify.name, {
-      dfs,
-    });
     return dfs;
   }
-  each(goals, lambda2);
+  return list_map(goals, lambda2);
 }
