9419c4c7eeb224aac8c8f26e2f80f2d9188e811f
Grace-Jared <christrosetolife@gmail.com>
2026-03-26 20:20:08 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate_single_openai.mjs

--- public/src/app_replace_rule_sets_why_generate_single_openai.mjs
+++ public/src/app_replace_rule_sets_why_generate_single_openai.mjs
@@ -10,7 +10,7 @@ export async function app_replace_rule_sets_why_generate_single_openai(
     json,
   });
   let r = await openai_responses_cache(
-    'You will receive the JSON of the replacement rules of a grammar. Please provide a sentence or two describing what the replacement rules demonstrate, and why. Answer using JSON: { "result": "description" }',
+    'You will receive the JSON of the replacement rules of a grammar. Please describe what the replacement rules demonstrate, and why. Answer using JSON: { "result": "description" }',
     json,
   );
   log(app_replace_rule_sets_why_generate_single_openai.name, {
