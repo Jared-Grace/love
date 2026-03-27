f74f9dd3acdeb842611d6abd927a468fd6a38881
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 12:36:23 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_next.mjs

--- public/src/app_replace_tests_run_e2e_next.mjs
+++ public/src/app_replace_tests_run_e2e_next.mjs
@@ -1,5 +1,4 @@
-import { app_replace } from "../../../love/public/src/app_replace.mjs";
-import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
+import { app_replace_test_e2e } from "../../../love/public/src/app_replace_test_e2e.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
 import { app_replace_tests_run_e2e_goal } from "../../../love/public/src/app_replace_tests_run_e2e_goal.mjs";
@@ -10,7 +9,7 @@ export async function app_replace_tests_run_e2e_next() {
   "it takes a long time to run all tests in browser in a row - instead, run all tests in parallel, and this function tests the first two rule sets so that next-ing is tested";
   let rule_sets = app_replace_rule_sets();
   let taken = list_take(rule_sets, 2);
-  async function lambda(page) {
+  async function on_page(page) {
     async function lambda3(rule_set) {
       let goals = property_get(rule_set, "goals");
       async function lambda2(goal) {
@@ -25,5 +24,5 @@ export async function app_replace_tests_run_e2e_next() {
     }
     await each_async(taken, lambda3);
   }
-  await playwright_test_app_dev(app_replace, lambda);
+  await app_replace_test_e2e(on_page);
 }
