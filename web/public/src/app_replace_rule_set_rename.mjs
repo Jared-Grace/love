5039b1be7a50ed09484c92453f72d880d53f55ca
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 18:16:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_rename.mjs

--- web/public/src/app_replace_rule_set_rename.mjs
+++ web/public/src/app_replace_rule_set_rename.mjs
@@ -1,4 +1,4 @@
-import { equal_not } from "../../../love/public/src/equal_not.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
 import { list_filter_null_not_is_single } from "../../../love/public/src/list_filter_null_not_is_single.mjs";
 import { function_ast_list_type_nodes_object_expression } from "../../../love/public/src/function_ast_list_type_nodes_object_expression.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -8,7 +8,7 @@ export async function app_replace_rule_set_rename(f_name) {
   let list = await function_ast_list_type_nodes_object_expression(f_name);
   let mapped = list_map(list, app_replace_rule_sets_name_expression_value);
   let f_name_after = list_filter_null_not_is_single(mapped);
-  if (equal_not(f_name, f_name_after)) {
+  if (equal(f_name, f_name_after)) {
     return null;
   }
   await function_rename_open(f_name, f_name_after);
