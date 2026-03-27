d032b76b5d7451bad9357ee81093f81e9389502f
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 10:46:28 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_relational.mjs

--- public/src/app_replace_rule_set_expressions_relational.mjs
+++ public/src/app_replace_rule_set_expressions_relational.mjs
@@ -1,13 +1,9 @@
-import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+import { app_replace_rule_set_expressions_relational_rules } from "../../../love/public/src/app_replace_rule_set_expressions_relational_rules.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_expresions_additive_abbreviations } from "../../../love/public/src/app_replace_rule_set_expresions_additive_abbreviations.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { app_replace_rule_set_expresions_additive_rules } from "../../../love/public/src/app_replace_rule_set_expresions_additive_rules.mjs";
 export function app_replace_rule_set_expressions_relational() {
   const rules = [];
-  app_replace_rule_set_expresions_additive_rules(rules);
-  list_add_multiple(rules, ["re > ae", "re > re ro ae", "ro > <", "ro > >>"]);
-  list_add(rules, "ex > re");
+  app_replace_rule_set_expressions_relational_rules(rules);
   let abbreviations = {};
   app_replace_rule_set_expresions_additive_abbreviations(abbreviations);
   object_merge(abbreviations, {
