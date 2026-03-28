8e28a453434d623226c1e4373301d6ffb57f9825
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:15:24 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_run.mjs

--- public/src/app_replace_rule_sets_fns_run.mjs
+++ public/src/app_replace_rule_sets_fns_run.mjs
@@ -2,6 +2,6 @@ import { function_run_args_none_fn } from "../../../love/public/src/function_run
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 export async function app_replace_rule_sets_fns_run() {
   "run dynamically in case modification in previous step of deploy";
-  let r2 = await function_run_args_none_fn(app_replace_rule_sets_fns);
-  return r2;
+  let r = await function_run_args_none_fn(app_replace_rule_sets_fns);
+  return r;
 }
