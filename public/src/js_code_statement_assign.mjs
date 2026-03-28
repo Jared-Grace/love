d99fd7ae70c26195e0745d0658a74d01e93d64a1
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 13:17:38 -0400

    function_auto_path ..\love\public\src\js_code_statement_assign.mjs

--- public/src/js_code_statement_assign.mjs
+++ public/src/js_code_statement_assign.mjs
@@ -1,6 +1,9 @@
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { js_code_semicolon } from "../../../love/public/src/js_code_semicolon.mjs";
 import { js_code_assign } from "../../../love/public/src/js_code_assign.mjs";
 export function js_code_statement_assign(kind, left, right) {
   let r = kind + " " + js_code_assign(left, right) + " " + js_code_semicolon();
   return r;
+  "this space is needed for " +
+    fn_name("app_replace_rule_set_statement_variable");
 }
