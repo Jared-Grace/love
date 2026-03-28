0960d8f48ca68a729fdf3e968bc2c8e97811d441
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:19:31 -0400

    pl command_line_node_scripts args

--- web/public/src/command_line_node_scripts.mjs
+++ web/public/src/command_line_node_scripts.mjs
@@ -2,7 +2,7 @@ import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { command_line } from "../../../love/public/src/command_line.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
-export async function command_line_node_scripts(args, script_name, f_name) {
+export async function command_line_node_scripts(script_name, f_name, args) {
   let result = list_join_space(args);
   let output = await command_line(
     "node scripts/" + script_name + ".mjs " + f_name + " " + result,
