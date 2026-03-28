36d6d6a790c8e647f256f18e551929c6d0439737
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 19:01:00 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_3.mjs

--- public/src/app_new_rule_set_new_3.mjs
+++ public/src/app_new_rule_set_new_3.mjs
@@ -1,7 +1,7 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_3() {
   const rules = [];
-  list_add_multiple(rules, ["a > b"]);
+  list_add_multiple(rules, ["fs > for ( ex ; ex ; ex ) sm"]);
   let abbreviations = {};
   let r = {
     name: "Statements for",
