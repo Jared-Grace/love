7fa662786af034bc115e873be369807504847828
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 13:17:09 -0400

    function_auto_path ..\love\public\src\js_code_statement_assign.mjs

--- web/public/src/js_code_statement_assign.mjs
+++ web/public/src/js_code_statement_assign.mjs
@@ -1,6 +1,6 @@
 import { js_code_semicolon } from "../../../love/public/src/js_code_semicolon.mjs";
 import { js_code_assign } from "../../../love/public/src/js_code_assign.mjs";
 export function js_code_statement_assign(kind, left, right) {
-  let r = kind + " " + js_code_assign(left, right) + js_code_semicolon();
+  let r = kind + " " + js_code_assign(left, right) + " " + js_code_semicolon();
   return r;
 }
