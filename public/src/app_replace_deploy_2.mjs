21932e80e43a87fb8c1d6997f1c9b62561e0f10a
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:24:57 -0400

    function_auto_path ..\love\public\src\app_replace_deploy_2.mjs

--- public/src/app_replace_deploy_2.mjs
+++ public/src/app_replace_deploy_2.mjs
@@ -3,6 +3,7 @@ import { html_update_latest_promote_deploy_app_fn } from "../../../love/public/s
 import { app_replace_tests_run } from "../../../love/public/src/app_replace_tests_run.mjs";
 import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app_replace_rule_sets_why_generate.mjs";
 export async function app_replace_deploy_2() {
+  "there is a potential rename, so these functions are ran after";
   await app_replace_rule_sets_why_generate();
   await app_replace_tests_run();
   await html_update_latest_promote_deploy_app_fn(app_replace);
