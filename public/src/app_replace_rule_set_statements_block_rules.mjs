4f6ea744f8f877d0e5940ce9db24f22dfa8b0b8a
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 18:52:54 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_block_rules.mjs

--- public/src/app_replace_rule_set_statements_block_rules.mjs
+++ public/src/app_replace_rule_set_statements_block_rules.mjs
@@ -2,5 +2,10 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_statements_simple_rules } from "../../../love/public/src/app_replace_rule_set_statements_simple_rules.mjs";
 export function app_replace_rule_set_statements_block_rules(rules) {
   app_replace_rule_set_statements_simple_rules(rules);
-  list_add_multiple(rules, ["bs > { smg }", "smg > sm", "smg > smg sm"]);
+  list_add_multiple(rules, [
+    "bs > { smg }",
+    "smg > sm",
+    "smg > smg sm",
+    "sm > bs",
+  ]);
 }
