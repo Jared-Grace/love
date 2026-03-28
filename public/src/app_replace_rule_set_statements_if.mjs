ed18f4cf1a997ba04d0d6749f5009d8c4689ea54
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 18:12:46 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_if.mjs

--- public/src/app_replace_rule_set_statements_if.mjs
+++ public/src/app_replace_rule_set_statements_if.mjs
@@ -6,7 +6,7 @@ import { app_replace_rule_set_statements_variable_abbreviations } from "../../..
 import { app_replace_rule_set_statements_variable_rules } from "../../../love/public/src/app_replace_rule_set_statements_variable_rules.mjs";
 import { js_keyword_if } from "../../../love/public/src/js_keyword_if.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
-export function app_new_rule_set_new_2() {
+export function app_replace_rule_set_statements_if() {
   const rules = [];
   app_replace_rule_set_statements_variable_rules(rules);
   app_replace_rule_set_statements_block_rules(rules);
