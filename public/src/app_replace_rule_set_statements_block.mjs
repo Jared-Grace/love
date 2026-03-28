84b24d10b2780b67517940cf06576a7b07797713
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 15:43:05 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_block.mjs

--- public/src/app_replace_rule_set_statements_block.mjs
+++ public/src/app_replace_rule_set_statements_block.mjs
@@ -2,7 +2,7 @@ import { app_replace_rule_set_statements_simple_abbreviations } from "../../../l
 import { app_replace_rule_set_statements_simple_rules } from "../../../love/public/src/app_replace_rule_set_statements_simple_rules.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
-export function app_new_rule_set_new_2() {
+export function app_replace_rule_set_statements_block() {
   const rules = [];
   app_replace_rule_set_statements_simple_rules(rules);
   list_add_multiple(rules, ["bs > { smg }", "smg > sm", "smg > smg sm"]);
