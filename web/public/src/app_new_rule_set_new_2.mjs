6cf45d240fff2fdda2663889a3d61dd4a6ce4e11
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 14:59:01 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,8 +1,10 @@
+import { app_replace_rule_set_statements_simple_abbreviation_sm } from "../../../love/public/src/app_replace_rule_set_statements_simple_abbreviation_sm.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
   list_add_multiple(rules, ["bs > { smg }", "smg > sm", "smg > smg sm"]);
   let abbreviations = {};
+  app_replace_rule_set_statements_simple_abbreviation_sm(abbreviations);
   let r = {
     name: "TODO",
     rules,
