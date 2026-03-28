054195461835cbe2d452fec24e2ae237826d39b1
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 13:14:09 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statement_variable.mjs

--- web/public/src/app_replace_rule_sets_fns.mjs
+++ web/public/src/app_replace_rule_sets_fns.mjs
@@ -1,4 +1,4 @@
-import { app_new_rule_set_new_2 } from "../../../love/public/src/app_new_rule_set_new_2.mjs";
+import { app_replace_rule_set_statement_variable } from "../../../love/public/src/app_replace_rule_set_statement_variable.mjs";
 import { app_replace_rule_set_statements_simple } from "../../../love/public/src/app_replace_rule_set_statements_simple.mjs";
 import { app_replace_rule_set_expressions_logical } from "../../../love/public/src/app_replace_rule_set_expressions_logical.mjs";
 import { app_replace_rule_set_expressions_equality } from "../../../love/public/src/app_replace_rule_set_expressions_equality.mjs";
@@ -125,7 +125,7 @@ export function app_replace_rule_sets_fns() {
     app_replace_rule_set_expressions_equality,
     app_replace_rule_set_expressions_logical,
     app_replace_rule_set_statements_simple,
-    app_new_rule_set_new_2,
+    app_replace_rule_set_statement_variable,
   ];
   return fns;
 }
