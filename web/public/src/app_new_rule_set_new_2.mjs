6145b9fff2c66a25c6d01a46d574e213c3b03d62
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 23:16:08 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,6 +1,5 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_logical_expressions_abbreviations } from "../../../love/public/src/app_replace_rule_set_logical_expressions_abbreviations.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { app_replace_rule_set_logical_expressions_rules } from "../../../love/public/src/app_replace_rule_set_logical_expressions_rules.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
@@ -13,7 +12,6 @@ export function app_new_rule_set_new_2() {
     "ex > true",
     "ex > u p d a t e ( )",
   ]);
-  list_add(rules, "ex > ase");
   let abbreviations = {};
   app_replace_rule_set_logical_expressions_abbreviations(abbreviations);
   let r = {
