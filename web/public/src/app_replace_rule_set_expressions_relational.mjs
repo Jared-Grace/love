d9bdce971c026f3688f583f0dedbfbbef9b851a3
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 17:36:04 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_relational.mjs

--- web/public/src/app_replace_rule_set_expressions_relational.mjs
+++ web/public/src/app_replace_rule_set_expressions_relational.mjs
@@ -42,6 +42,10 @@ export function app_replace_rule_set_expressions_relational() {
       },
       {
         start: "mae > mae",
+        end: "li > li",
+      },
+      {
+        start: "li > li",
         end: "nu > nu",
       },
       {
