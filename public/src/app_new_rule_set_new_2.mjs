b0fd8fa98cc0c234ed9bcd29bf22713237ce107a
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:01:17 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -2,7 +2,10 @@ import { js_keyword_if } from "../../../love/public/src/js_keyword_if.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
-  list_add_multiple(rules, [js_keyword_if() + " ( ex ) sm"]);
+  list_add_multiple(rules, [
+    js_keyword_if() + " ( ex ) sm",
+    js_keyword_if() + " ( ex ) sm else sm",
+  ]);
   let abbreviations = {};
   let r = {
     name: "Statements if",
