1838573d65e4ca2e1f352f77965fcec448367d34
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 17:57:45 -0400

    p

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -31,6 +31,10 @@ export function app_new_rule_set_new_2() {
     goals: [
       {
         start: "is",
+        end: js_keyword_if() + " ( x < 0 ) x = 1 ;",
+      },
+      {
+        start: "is",
         end: js_keyword_if() + " ( x < 0 ) { x = 1 ; }",
       },
       {
