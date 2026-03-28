51a8ca57072743f1b7eeb52a8f19e539cf031043
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 18:11:36 -0400

    p

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -61,6 +61,13 @@ export function app_new_rule_set_new_2() {
         start: "is",
         end:
           js_keyword_if() +
+          " ( x > 0 ) sm else sm",
+      },
+      {
+        start: js_keyword_if() +
+          " ( x > 0 ) sm else sm",
+        end:
+          js_keyword_if() +
           " ( x > 0 ) positive = true ; else positive = false ;",
       },
     ],
