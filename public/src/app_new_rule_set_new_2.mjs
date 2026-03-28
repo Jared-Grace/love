d6197574aa0464d2ebf5f23c98ddd657252ad082
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 18:47:41 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -2,7 +2,7 @@ import { js_keyword_let_while } from "../../../love/public/src/js_keyword_let_wh
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
-  list_add_multiple(rules, ["ws > " + js_keyword_let_while()]);
+  list_add_multiple(rules, ["ws > " + js_keyword_let_while() + " ( ex ) sm"]);
   let abbreviations = {};
   let r = {
     name: "TODO",
