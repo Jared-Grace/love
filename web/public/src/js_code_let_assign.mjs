c58e295da8d5c7aecaac8207ec71b0524e38c8af
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 13:09:59 -0400

    function_auto_path ..\love\public\src\js_code_let_assign.mjs

--- web/public/src/js_code_let_assign.mjs
+++ web/public/src/js_code_let_assign.mjs
@@ -2,7 +2,8 @@ import { js_code_semicolon } from "../../../love/public/src/js_code_semicolon.mj
 import { js_code_assign } from "../../../love/public/src/js_code_assign.mjs";
 import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
 export function js_code_let_assign(left, right) {
-  let code_assign = js_keyword_let() + " " + js_code_assign(left, right) + ";";
+  let code_assign =
+    js_keyword_let() + " " + js_code_assign(left, right) + js_code_semicolon();
   return code_assign;
   let v2 = js_code_semicolon();
 }
