5550b0c3e444900065bfab96099aa677d1ae72ab
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:02:25 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_variable_rules.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -2,6 +2,7 @@ import { js_keyword_if } from "../../../love/public/src/js_keyword_if.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
+  app_replace_rule_set_statements_variable_rules(rules);
   list_add_multiple(rules, [
     js_keyword_if() + " ( ex ) sm",
     js_keyword_if() + " ( ex ) sm else sm",
