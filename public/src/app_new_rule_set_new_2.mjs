47f6345868edc0641d5e151e2a11f12d8e2f0846
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 18:50:28 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,13 +1,17 @@
+import { app_replace_rule_set_statements_block_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_block_abbreviations.mjs";
+import { app_replace_rule_set_statements_block_rules } from "../../../love/public/src/app_replace_rule_set_statements_block_rules.mjs";
 import { js_keyword_while } from "../../../love/public/src/js_keyword_while.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
+  app_replace_rule_set_statements_block_rules(rules);
   list_add_multiple(rules, [
     "ws > " + js_keyword_while() + " ( ex ) sm",
     "ex > x < 3",
     "ex > x = x + 1",
   ]);
   let abbreviations = {};
+  app_replace_rule_set_statements_block_abbreviations(abbreviations);
   let r = {
     name: "TODO",
     rules,
