37eae6b9860d5ba3e5441b59aa46c8c6f89a4a5d
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:19:31 -0400

    pl command_line_node_scripts args

--- public/src/command_line_node_g.mjs
+++ public/src/command_line_node_g.mjs
@@ -1,5 +1,5 @@
 import { command_line_node_scripts } from "../../../love/public/src/command_line_node_scripts.mjs";
 export async function command_line_node_g(f_name, args) {
   const script_name = "g";
-  await command_line_node_scripts(args, script_name, f_name);
+  await command_line_node_scripts(script_name, f_name, args);
 }
