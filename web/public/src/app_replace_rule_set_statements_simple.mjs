a9cf4ff74b3ba8a07e50f938120b76401c35f52a
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:42:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_simple.mjs

--- web/public/src/app_replace_rule_set_statements_simple.mjs
+++ web/public/src/app_replace_rule_set_statements_simple.mjs
@@ -1,3 +1,4 @@
+import { js_keyword_return } from "../../../love/public/src/js_keyword_return.mjs";
 import { app_replace_rule_set_statements_simple_rules } from "../../../love/public/src/app_replace_rule_set_statements_simple_rules.mjs";
 import { app_replace_rule_set_expressions_primary_abbreviation_ex } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
@@ -20,7 +21,7 @@ export function app_replace_rule_set_statements_simple() {
       },
       {
         start: "sm",
-        end: "return ;",
+        end: js_keyword_return() + " ;",
       },
       {
         start: "sm",
@@ -28,7 +29,7 @@ export function app_replace_rule_set_statements_simple() {
       },
       {
         start: "sm",
-        end: "return true ;",
+        end: js_keyword_return() + " true ;",
       },
     ],
     why: "The replacement rules define a simple grammar for statements, allowing for empty statements, return statements, expression statements, and return-expression statements, with expressions limited to 'true' or a 'update()' call; this demonstrates a minimal subset of statement and expression syntax, likely for a programming language.",
