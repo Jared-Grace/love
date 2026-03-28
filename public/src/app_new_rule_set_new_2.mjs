441c14fb6d2ce5bd3e88a7c54a0b0632ba428f52
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:40:22 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,10 +1,11 @@
+import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_expressions_primary_abbreviation_ex } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
   list_add_multiple(rules, [
     "vs > vk vdg ;",
-    "vk > let",
+    "vk > " + js_keyword_let(),
     "vk > const",
     "vk > var",
     "vdg > vd",
