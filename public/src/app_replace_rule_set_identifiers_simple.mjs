8049fd7d3f21fb68730c53806ade29925c764ecf
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 14:54:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

--- public/src/app_replace_rule_set_identifiers_simple.mjs
+++ public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -1,4 +1,3 @@
-import { app_replace_rule_set_expressions_primary_abbreviation_id } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_id.mjs";
 import { app_replace_rule_set_identifiers_simple_abbreviations } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_abbreviations.mjs";
 import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_goals.mjs";
 import { app_replace_rule_set_identifiers_simple_rules } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_rules.mjs";
@@ -7,7 +6,6 @@ export function app_replace_rule_set_identifiers_simple() {
     id: ["", "id", "entifier"],
     idg: ["", "id", "entifier ", "g", "rower"],
   };
-  app_replace_rule_set_expressions_primary_abbreviation_id(abbreviations2);
   app_replace_rule_set_identifiers_simple_abbreviations(abbreviations);
   let r = {
     name: "Identifiers Simple",
