717c0e4616f141beb7b4f250a1026f78d9675d43
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:02:23 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_variable.mjs

--- web/public/src/app_replace_rule_set_statements_variable.mjs
+++ web/public/src/app_replace_rule_set_statements_variable.mjs
@@ -1,36 +1,12 @@
+import { app_replace_rule_set_statements_variable_rules } from "../../../love/public/src/app_replace_rule_set_statements_variable_rules.mjs";
 import { app_replace_rule_set_identifiers_simple_abbreviation_id } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_abbreviation_id.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { js_code_assign } from "../../../love/public/src/js_code_assign.mjs";
 import { js_code_let_assign } from "../../../love/public/src/js_code_let_assign.mjs";
-import { js_code_semicolon } from "../../../love/public/src/js_code_semicolon.mjs";
-import { js_keyword_var } from "../../../love/public/src/js_keyword_var.mjs";
-import { js_keyword_const } from "../../../love/public/src/js_keyword_const.mjs";
-import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
-import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_expressions_primary_abbreviation_ex } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs";
 export function app_replace_rule_set_statements_variable() {
   const rules = [];
-  list_add_multiple(rules, [
-    "vs > vk vdg " + js_code_semicolon(),
-    "vk > " + js_keyword_let(),
-    "vk > " + js_keyword_const(),
-    "vk > " + js_keyword_var(),
-    "vdg > vd",
-    "vdg > vdg , vd",
-    "vd > id",
-    "vd > id = ex",
-    "id > a",
-    "id > b",
-    "id > c",
-    "id > i",
-    "id > j",
-    "id > x",
-    "id > y",
-    "id > z",
-    "ex > 0",
-    "ex > 1",
-    "ex > 2",
-  ]);
+  app_replace_rule_set_statements_variable_rules(rules);
   let abbreviations = {};
   app_replace_rule_set_expressions_primary_abbreviation_ex(abbreviations);
   app_replace_rule_set_identifiers_simple_abbreviation_id(abbreviations);
---
+++ web/public/src/app_replace_rule_set_statements_variable_rules.mjs
@@ -0,0 +1,28 @@
+import { js_keyword_var } from "../../../love/public/src/js_keyword_var.mjs";
+import { js_keyword_const } from "../../../love/public/src/js_keyword_const.mjs";
+import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
+import { js_code_semicolon } from "../../../love/public/src/js_code_semicolon.mjs";
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+export function app_replace_rule_set_statements_variable_rules(rules) {
+  list_add_multiple(rules, [
+    "vs > vk vdg " + js_code_semicolon(),
+    "vk > " + js_keyword_let(),
+    "vk > " + js_keyword_const(),
+    "vk > " + js_keyword_var(),
+    "vdg > vd",
+    "vdg > vdg , vd",
+    "vd > id",
+    "vd > id = ex",
+    "id > a",
+    "id > b",
+    "id > c",
+    "id > i",
+    "id > j",
+    "id > x",
+    "id > y",
+    "id > z",
+    "ex > 0",
+    "ex > 1",
+    "ex > 2",
+  ]);
+}
