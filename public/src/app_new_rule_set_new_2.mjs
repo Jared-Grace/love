842ea9e7422a34b98f7e5c062f7c7ff1c18f5cee
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:35:08 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -2,18 +2,18 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 import { app_replace_rule_set_expressions_primary_abbreviation_ex } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
-  list_add_multiple(list, items);
+  list_add_multiple(rules, [
+    "vs > vk vdg ;",
+    "vdg > vd",
+    "vdg > vdg , vd",
+    "vd > id",
+    "vd > id = ex",
+  ]);
   let abbreviations = {};
   app_replace_rule_set_expressions_primary_abbreviation_ex(abbreviations);
   let r = {
     name: "Statement variable",
-    rules: [
-      "vs > vk vdg ;",
-      "vdg > vd",
-      "vdg > vdg , vd",
-      "vd > id",
-      "vd > id = ex",
-    ],
+    rules,
     goals: [
       {
         start: "a",
