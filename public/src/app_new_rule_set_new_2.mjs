c3475ff56f72cd455e5835273bdde279fe73c7fe
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 15:02:01 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,13 +1,13 @@
+import { app_replace_rule_set_statements_simple_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_simple_abbreviations.mjs";
 import { app_replace_rule_set_statements_simple_rules } from "../../../love/public/src/app_replace_rule_set_statements_simple_rules.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { app_replace_rule_set_statements_simple_abbreviation_sm } from "../../../love/public/src/app_replace_rule_set_statements_simple_abbreviation_sm.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
   app_replace_rule_set_statements_simple_rules(rules2);
   list_add_multiple(rules, ["bs > { smg }", "smg > sm", "smg > smg sm"]);
   let abbreviations = {};
-  app_replace_rule_set_statements_simple_abbreviation_sm(abbreviations);
+  app_replace_rule_set_statements_simple_abbreviations(abbreviations);
   object_merge(abbreviations, {
     sm: ["", "s", "tate", "m", "ent ", "g", "rower"],
   });
