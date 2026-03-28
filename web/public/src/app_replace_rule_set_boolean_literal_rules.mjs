55e4203137249e818552beb50dbf966f74e41100
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:48:45 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_boolean_literal_rules.mjs

--- web/public/src/app_replace_rule_set_boolean_literal_rules.mjs
+++ web/public/src/app_replace_rule_set_boolean_literal_rules.mjs
@@ -1,11 +1,8 @@
+import { js_keywords_true_false } from "../../../love/public/src/js_keywords_true_false.mjs";
 import { app_replace_rule_set_add_rights } from "../../../love/public/src/app_replace_rule_set_add_rights.mjs";
-import { js_keyword_false } from "../../../love/public/src/js_keyword_false.mjs";
-import { js_keyword_true } from "../../../love/public/src/js_keyword_true.mjs";
 export function app_replace_rule_set_boolean_literal_rules() {
   let rules = [];
-  let t = js_keyword_true();
-  let f = js_keyword_false();
-  let r = [t, f];
+  let r = js_keywords_true_false();
   app_replace_rule_set_add_rights(rules, "bo", r);
   return rules;
 }
