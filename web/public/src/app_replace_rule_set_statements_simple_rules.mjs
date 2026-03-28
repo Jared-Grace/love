0cea8f3db52aa846ec14c9e5951b3f543518f6f3
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:34:11 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_simple.mjs

---
+++ web/public/src/app_replace_rule_set_statements_simple_rules.mjs
@@ -0,0 +1,11 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+export function app_replace_rule_set_statements_simple_rules(rules) {
+  list_add_multiple(rules, [
+    "sm > ;",
+    "sm > return ;",
+    "sm > ex ;",
+    "sm > return ex ;",
+    "ex > true",
+    "ex > u p d a t e ( )",
+  ]);
+}
