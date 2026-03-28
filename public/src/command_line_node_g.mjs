36ee35d1af5e7006e522d8efcd42c3f046154d01
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:19:14 -0400

    function_auto_path ..\love\public\src\command_line_node_g.mjs

--- public/src/command_line_node_g.mjs
+++ public/src/command_line_node_g.mjs
@@ -1,13 +1,5 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
-import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
+import { command_line_node_scripts } from "../../../love/public/src/command_line_node_scripts.mjs";
 export async function command_line_node_g(f_name, args) {
   const script_name = "g";
-  let result = list_join_space(args);
-  let output = await command_line(
-    "node scripts/" + script_name + ".mjs " + f_name + " " + result,
-  );
-  let stdout = property_get(output, "stdout");
-  log_keep(command_line_node_g.name, stdout);
+  await command_line_node_scripts(args, script_name, f_name);
 }
