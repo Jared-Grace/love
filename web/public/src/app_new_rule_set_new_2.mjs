d0c6f721f3b9c3fb237503ce1c9df651c5413ab4
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 19:05:44 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -5,8 +5,8 @@ import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mj
 export function app_new_rule_set_new_2() {
   const rules = [];
   app_replace_rule_set_statements_if_rules(rules);
+  list_add_multiple(rules, ["ws > " + js_keyword_while() + " ( ex ) sm"]);
   list_add_multiple(rules, [
-    "ws > " + js_keyword_while() + " ( ex ) sm",
     "ex > y > 0",
     "ex > x < 3",
     "ex > x = x + 1",
