9ea00d448d19e4fa486d27297d08bd2abf0902a9
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:02:27 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rule_set_statements_variable_rules } from "../../../love/public/src/app_replace_rule_set_statements_variable_rules.mjs";
 import { js_keyword_if } from "../../../love/public/src/js_keyword_if.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
