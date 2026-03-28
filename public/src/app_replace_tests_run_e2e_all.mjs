abeb3c772b63ca52d3e84d2522422cb6f680b164
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:22:44 -0400

    function_auto_path ..\love\public\src\app_replace_tests_run_e2e_all.mjs

--- public/src/app_replace_tests_run_e2e_all.mjs
+++ public/src/app_replace_tests_run_e2e_all.mjs
@@ -1,4 +1,4 @@
-import { app_replace_rule_sets_run } from "../../../love/public/src/app_replace_rule_sets_run.mjs";
+import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
 import { app_replace_test_e2e } from "../../../love/public/src/app_replace_test_e2e.mjs";
 import { app_replace_rule_set_binary_counting } from "../../../love/public/src/app_replace_rule_set_binary_counting.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -11,7 +11,7 @@ import { object_merge_multiple } from "../../../love/public/src/object_merge_mul
 import { object_wrap_multiple } from "../../../love/public/src/object_wrap_multiple.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function app_replace_tests_run_e2e_all(e2e_inner_fns) {
-  let rule_sets = await app_replace_rule_sets_run();
+  let rule_sets = app_replace_rule_sets();
   let r = app_replace_rule_set_binary_counting();
   function lambda2(rule_set) {
     let goals = property_get(rule_set, "goals");
