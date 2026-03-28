aa633b97350944b59a60b1da8d0abfce4ea0d5f4
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:10:22 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,6 +1,6 @@
+import { app_replace_rule_set_statements_block_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_block_abbreviations.mjs";
 import { js_keyword_else } from "../../../love/public/src/js_keyword_else.mjs";
 import { app_replace_rule_set_statements_variable_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_variable_abbreviations.mjs";
-import { app_replace_rule_set_statements_simple_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_simple_abbreviations.mjs";
 import { app_replace_rule_set_statements_simple_rules } from "../../../love/public/src/app_replace_rule_set_statements_simple_rules.mjs";
 import { app_replace_rule_set_statements_variable_rules } from "../../../love/public/src/app_replace_rule_set_statements_variable_rules.mjs";
 import { js_keyword_if } from "../../../love/public/src/js_keyword_if.mjs";
@@ -16,8 +16,8 @@ export function app_new_rule_set_new_2() {
     "sm > bs",
   ]);
   let abbreviations = {};
-  app_replace_rule_set_statements_simple_abbreviations(abbreviations);
   app_replace_rule_set_statements_variable_abbreviations(abbreviations);
+  app_replace_rule_set_statements_block_abbreviations(abbreviations);
   let r = {
     name: "Statements if",
     rules,
