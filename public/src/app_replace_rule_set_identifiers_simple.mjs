0d580c804457ce7f1976eb82192929de0b3018fb
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 14:54:25 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple.mjs

--- public/src/app_replace_rule_set_identifiers_simple.mjs
+++ public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -3,7 +3,6 @@ import { app_replace_rule_set_identifiers_simple_goals } from "../../../love/pub
 import { app_replace_rule_set_identifiers_simple_rules } from "../../../love/public/src/app_replace_rule_set_identifiers_simple_rules.mjs";
 export function app_replace_rule_set_identifiers_simple() {
   let abbreviations = {
-    id: ["", "id", "entifier"],
     idg: ["", "id", "entifier ", "g", "rower"],
   };
   app_replace_rule_set_identifiers_simple_abbreviations(abbreviations);
