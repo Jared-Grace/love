6609f942845586913758bd5ad986980e4ab4756f
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:00:56 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,8 @@
+import { js_keyword_if } from "../../../love/public/src/js_keyword_if.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 export function app_new_rule_set_new_2() {
   const rules = [];
-  list_add_multiple(rules, ["a > b"]);
+  list_add_multiple(rules, [js_keyword_if() + " ( ex ) sm"]);
   let abbreviations = {};
   let r = {
     name: "Statements if",
