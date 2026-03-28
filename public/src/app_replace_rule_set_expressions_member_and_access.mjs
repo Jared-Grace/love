8d04938afc8e379d699d8e11c85f694e556656e7
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 16:45:39 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_member_and_access.mjs

--- public/src/app_replace_rule_set_expressions_member_and_access.mjs
+++ public/src/app_replace_rule_set_expressions_member_and_access.mjs
@@ -39,6 +39,10 @@ export function app_replace_rule_set_expressions_member_and_access() {
         end: "id . id . id",
       },
       {
+        start: "mae . id",
+        end: "h u m a n . b i r t h d a t e . y e a r",
+      },
+      {
         start: "mae",
         end: "id [ pe ]",
       },
