04a02802e78dc8f53dfd89be13c743ca5f0a64a3
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:43:05 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_simple.mjs

--- public/src/app_replace_rule_set_statements_simple.mjs
+++ public/src/app_replace_rule_set_statements_simple.mjs
@@ -1,3 +1,4 @@
+import { js_keyword_true } from "../../../love/public/src/js_keyword_true.mjs";
 import { js_keyword_return } from "../../../love/public/src/js_keyword_return.mjs";
 import { app_replace_rule_set_statements_simple_rules } from "../../../love/public/src/app_replace_rule_set_statements_simple_rules.mjs";
 import { app_replace_rule_set_expressions_primary_abbreviation_ex } from "../../../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs";
@@ -29,7 +30,7 @@ export function app_replace_rule_set_statements_simple() {
       },
       {
         start: "sm",
-        end: js_keyword_return() + " true ;",
+        end: js_keyword_return() + " " + js_keyword_true() + " ;",
       },
     ],
     why: "The replacement rules define a simple grammar for statements, allowing for empty statements, return statements, expression statements, and return-expression statements, with expressions limited to 'true' or a 'update()' call; this demonstrates a minimal subset of statement and expression syntax, likely for a programming language.",
