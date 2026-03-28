383ac596a2313bf09058847adffda5664ba3b6cd
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 18:05:57 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -17,7 +17,7 @@ export function app_new_rule_set_new_2() {
     "sm > bs",
     "ex > x < 0",
     "ex > x = 1",
-    "ex > null",
+    "ex > y === null",
     "ex > positive = true",
     "ex > positive = false",
   ]);
