bcf9eb5780336201481addac5f094c908e201da9
Grace-Jared <christrosetolife@gmail.com>
2026-03-27 23:16:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_simple.mjs

--- web/public/src/app_replace_rule_set_statements_simple.mjs
+++ web/public/src/app_replace_rule_set_statements_simple.mjs
@@ -1,7 +1,7 @@
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { app_replace_rule_set_logical_expressions_abbreviations } from "../../../love/public/src/app_replace_rule_set_logical_expressions_abbreviations.mjs";
 import { app_replace_rule_set_logical_expressions_rules } from "../../../love/public/src/app_replace_rule_set_logical_expressions_rules.mjs";
-export function app_new_rule_set_new_2() {
+export function app_replace_rule_set_statements_simple() {
   const rules = [];
   app_replace_rule_set_logical_expressions_rules(rules);
   list_add_multiple(rules, [
--- web/public/src/app_replace_rule_sets_fns.mjs
+++ web/public/src/app_replace_rule_sets_fns.mjs
@@ -1,4 +1,4 @@
-import { app_new_rule_set_new_2 } from "../../../love/public/src/app_new_rule_set_new_2.mjs";
+import { app_replace_rule_set_statements_simple } from "../../../love/public/src/app_replace_rule_set_statements_simple.mjs";
 import { app_replace_rule_set_logical_expressions } from "../../../love/public/src/app_replace_rule_set_logical_expressions.mjs";
 import { app_replace_rule_set_expressions_equality } from "../../../love/public/src/app_replace_rule_set_expressions_equality.mjs";
 import { app_replace_rule_set_expressions_relational } from "../../../love/public/src/app_replace_rule_set_expressions_relational.mjs";
@@ -123,7 +123,7 @@ export function app_replace_rule_sets_fns() {
     app_replace_rule_set_expressions_relational,
     app_replace_rule_set_expressions_equality,
     app_replace_rule_set_logical_expressions,
-    app_new_rule_set_new_2,
+    app_replace_rule_set_statements_simple,
   ];
   return fns;
 }
