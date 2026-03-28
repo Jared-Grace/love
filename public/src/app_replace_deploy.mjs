b5029650179d6f966adeaa574e362f1119ba8bf7
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:25:41 -0400

    function_auto_path ..\love\public\src\app_replace_deploy.mjs

--- public/src/app_replace_deploy.mjs
+++ public/src/app_replace_deploy.mjs
@@ -1,3 +1,4 @@
+import { command_line_node_r } from "../../../love/public/src/command_line_node_r.mjs";
 import { app_replace_deploy_2 } from "../../../love/public/src/app_replace_deploy_2.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_rule_sets_fns_names } from "../../../love/public/src/app_replace_rule_sets_fns_names.mjs";
@@ -7,5 +8,6 @@ export async function app_replace_deploy() {
   let result = await app_replace_rule_sets_fns_migrate_capitalization_upper();
   let mapped = app_replace_rule_sets_fns_names();
   await each_async(mapped, app_replace_rule_set_rename);
+  await command_line_node_r(f_name, args);
   await app_replace_deploy_2();
 }
