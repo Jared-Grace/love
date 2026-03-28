f3e8c11d6293bd0cf32df4b89586585e4f7caf3b
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 14:54:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_identifiers_simple_abbreviation_id.mjs

--- public/src/app_replace_rule_set_identifiers_simple_abbreviation_id.mjs
+++ public/src/app_replace_rule_set_identifiers_simple_abbreviation_id.mjs
@@ -1,5 +1,5 @@
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-export function app_replace_rule_set_expressions_primary_abbreviation_id(
+export function app_replace_rule_set_identifiers_simple_abbreviation_id(
   abbreviations,
 ) {
   object_merge(abbreviations, {
