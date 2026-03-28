014da0e19a61def3f0279c52c9b9591bfe630943
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 09:23:41 -0400

    function_auto_path ..\love\public\src\app_replace_deploy.mjs

---
+++ public/src/app_replace_deploy_2.mjs
@@ -0,0 +1,9 @@
+import { app_replace } from "../../../love/public/src/app_replace.mjs";
+import { html_update_latest_promote_deploy_app_fn } from "../../../love/public/src/html_update_latest_promote_deploy_app_fn.mjs";
+import { app_replace_tests_run } from "../../../love/public/src/app_replace_tests_run.mjs";
+import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app_replace_rule_sets_why_generate.mjs";
+export async function app_replace_deploy_2() {
+  await app_replace_rule_sets_why_generate();
+  await app_replace_tests_run();
+  await html_update_latest_promote_deploy_app_fn(app_replace);
+}
