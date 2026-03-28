d202b22faeb6612f7254e47040b55702d8310039
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 19:00:06 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns.mjs

--- web/public/src/app_replace_rule_sets_fns.mjs
+++ web/public/src/app_replace_rule_sets_fns.mjs
@@ -1,3 +1,4 @@
+import { app_new_rule_set_new_3 } from "../../../love/public/src/app_new_rule_set_new_3.mjs";
 import { app_new_rule_set_new_2 } from "../../../love/public/src/app_new_rule_set_new_2.mjs";
 import { app_replace_rule_set_expressions_assignment } from "../../../love/public/src/app_replace_rule_set_expressions_assignment.mjs";
 import { app_replace_rule_set_statements_if } from "../../../love/public/src/app_replace_rule_set_statements_if.mjs";
