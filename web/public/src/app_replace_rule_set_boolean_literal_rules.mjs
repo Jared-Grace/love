d92f0d20e7575fa3e16283b5824afd847b45acf0
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:44:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_boolean_literal_rules.mjs

--- web/public/src/app_replace_rule_set_boolean_literal_rules.mjs
+++ web/public/src/app_replace_rule_set_boolean_literal_rules.mjs
@@ -1,4 +1,6 @@
+import { js_keyword_false } from "../../../love/public/src/js_keyword_false.mjs";
+import { js_keyword_true } from "../../../love/public/src/js_keyword_true.mjs";
 export function app_replace_rule_set_boolean_literal_rules() {
-  let r = ["bo > true", "bo > false"];
+  let r = ["bo > " + js_keyword_true(), "bo > " + js_keyword_false()];
   return r;
 }
