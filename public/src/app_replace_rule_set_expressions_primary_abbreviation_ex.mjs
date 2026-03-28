7651d24cc28dca288149853a9bf7627114e5f53b
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 10:20:40 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_primary_abbreviations.mjs

---
+++ public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs
@@ -0,0 +1,8 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+export function app_replace_rule_set_expressions_primary_abbreviation_ex(
+  abbreviations,
+) {
+  object_merge(abbreviations, {
+    ex: ["", "ex", "pression"],
+  });
+}
