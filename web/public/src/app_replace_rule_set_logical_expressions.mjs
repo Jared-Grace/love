a796632d7d1dcf985cea652113ea22fa2f634749
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 10:23:20 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_expressions_function_calls.mjs

--- web/public/src/app_replace_rule_set_logical_expressions.mjs
+++ web/public/src/app_replace_rule_set_logical_expressions.mjs
@@ -61,7 +61,7 @@ export function app_replace_rule_set_logical_expressions() {
         end: "( true || false ) && true",
       },
     ],
-    why: "The replacement rules define a context-free grammar for logical expressions similar to those in JavaScript, demonstrating operator precedence, associativity, and the structure of primary, unary, multiplicative, additive, relational, equality, and logical expressions, as well as literals and function calls.",
+    why: "These replacement rules define a context-free grammar for logical expressions similar to those in JavaScript, demonstrating operator precedence, associativity, and the structure of primary expressions, literals, member access, function calls, and logical operations.",
   };
   return r;
 }
