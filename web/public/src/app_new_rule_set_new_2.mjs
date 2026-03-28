026b66515e26f1ec13faea48e1c3a623bfc65f36
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 12:29:58 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -1,7 +1,7 @@
 export function app_new_rule_set_new_2() {
   let r = {
     name: "Statement variable",
-    rules: ["a > b"],
+    rules: ["vs > vk vdg ;"],
     goals: [
       {
         start: "a",
