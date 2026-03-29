be892963b5d8179c440559d446a21a88174db0e0
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 22:47:00 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- public/src/app_new_rule_set_new_2.mjs
+++ public/src/app_new_rule_set_new_2.mjs
@@ -98,7 +98,7 @@ export function app_new_rule_set_new_2() {
         end: js_keyword_function() + " id ( id ) { sm sm sm }",
       },
       {
-        start: js_keyword_function() + " id ( fpg ) { smg }",
+        start: js_keyword_function() + " id ( id ) { sm sm sm }",
         end: js_keyword_function() + " s u m ( l i s t ) { sm sm sm }",
       },
     ],
