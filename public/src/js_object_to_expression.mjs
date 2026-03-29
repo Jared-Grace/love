e1561bf6690583b04ccc68fe1119ece455dfc647
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:32:52 -0400

    function_auto_path ..\love\public\src\js_object_to_expression.mjs

--- public/src/js_object_to_expression.mjs
+++ public/src/js_object_to_expression.mjs
@@ -2,6 +2,6 @@ import { js_parse_expression_from_assignment } from "../../../love/public/src/js
 import { json_to } from "../../../love/public/src/json_to.mjs";
 export function js_object_to_expression(object) {
   let json = json_to(object);
-  let init = js_parse_expression_from_assignment(json);
-  return init;
+  let e = js_parse_expression_from_assignment(json);
+  return e;
 }
