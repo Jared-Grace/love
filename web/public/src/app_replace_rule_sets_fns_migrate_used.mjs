142ebceb7fc9bdade07554add31a0b9adf87e2d0
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:27:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -17,9 +17,9 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     let goals = property_get(rs, "goals");
     let rules_parsed = app_replace_rules_parse_left_right_only(rules);
     function lambda2(g) {
-      let r4 = app_replace_start_end_get(goal);
-      let end = property_get(g, "end");
-      let start = property_get(g, "start");
+      let se = app_replace_start_end_get(g);
+      let end = property_get(se, "end");
+      let start = property_get(se, "start");
       let ru = app_replace_rule_set_rules_used(rules_parsed, start, end);
       return ru;
     }
