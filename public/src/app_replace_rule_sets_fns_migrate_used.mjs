f1ada47da3ad7e24555cfaa523e181c3b7fcb001
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:05:41 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
@@ -16,7 +17,10 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     if (equal_not(t, "ArrayExpression")) {
       return;
     }
-    let code = js_unparse(ast);
+    let code = js_unparse(goals);
+    log(app_replace_rule_sets_fns_migrate_used.name, {
+      code,
+    });
   }
   let result = await app_replace_rule_sets_fns_transform(lambda);
 }
