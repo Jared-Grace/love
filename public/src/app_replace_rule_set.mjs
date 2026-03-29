48934b5faea29054e5cff7169ad7ced1e53996ab
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:26:44 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -184,8 +184,8 @@ export async function app_replace_rule_set(context) {
   refresh();
   async function refresh() {
     html_clear(div_rules_buttons);
-    let t = app_replace_rule_set_verify_from_try(rules_parsed, start, end);
-    function lambda() {}
+    function lambda() {
+    let t = app_replace_rule_set_verify_from_try(rules_parsed, start, end);}
     let r32 = await lambda_timeout(lambda, ms);
     let found = property_get(t, "found");
     if (not(found)) {
