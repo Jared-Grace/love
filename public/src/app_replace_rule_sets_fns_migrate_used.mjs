00ab123fb50fb56137946fa15ab49743c74bbfab
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:03:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,3 +1,4 @@
+import { null_is } from "../../../love/public/src/null_is.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
@@ -8,7 +9,7 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     let item = property_get(a, "item");
     let p = "goals";
     let goals = js_object_expression_properties_find_key_named(item, p);
-    if (false) {
+    if (null_is(value2)) {
     }
     const t = goals.value.type;
     if (equal_not(t, "ArrayExpression")) {
