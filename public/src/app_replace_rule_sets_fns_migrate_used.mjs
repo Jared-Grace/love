cd922eca7e4babb77a00f80dd7184a5ac0327bb8
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:18:38 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -3,19 +3,12 @@ import { app_replace_rules_parse_left_right_only } from "../../../love/public/sr
 import { app_replace_rule_set_rules_used } from "../../../love/public/src/app_replace_rule_set_rules_used.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { null_is } from "../../../love/public/src/null_is.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_sets_fns_transform } from "../../../love/public/src/app_replace_rule_sets_fns_transform.mjs";
 export async function app_replace_rule_sets_fns_migrate_used() {
   async function lambda(a) {
     let item = property_get(a, "item");
-    let p = "goals";
-    let goals = js_object_expression_properties_find_key_named(item, p);
-    if (null_is(goals)) {
-      return;
-    }
     let value = property_get(goals, "value");
     const t = value.type;
     if (equal_not(t, "ArrayExpression")) {
@@ -30,10 +23,10 @@ export async function app_replace_rule_sets_fns_migrate_used() {
       rs,
     });
     let rules = property_get(rs, "rules");
-    let goals2 = property_get(rs, "goals");
+    let goals = property_get(rs, "goals");
     let rules_parsed = app_replace_rules_parse_left_right_only(rules);
     function lambda2(item2) {}
-    list_map(list, lambda2);
+    list_map(goals, lambda2);
     let rules_used = app_replace_rule_set_rules_used(rules_parsed, start, end);
   }
   let result = await app_replace_rule_sets_fns_transform(lambda);
