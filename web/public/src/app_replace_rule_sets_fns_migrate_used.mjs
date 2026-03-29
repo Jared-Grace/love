c6a02ae4b9db284da83a588f176966637df2abe5
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:05:08 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,6 +1,5 @@
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_sets_fns_transform } from "../../../love/public/src/app_replace_rule_sets_fns_transform.mjs";
@@ -14,9 +13,7 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     }
     const t = goals.value.type;
     if (equal_not(t, "ArrayExpression")) {
-      log(app_replace_rule_sets_fns_migrate_used.name, {
-        goals: t,
-      });
+      return;
     }
   }
   let result = await app_replace_rule_sets_fns_transform(lambda);
