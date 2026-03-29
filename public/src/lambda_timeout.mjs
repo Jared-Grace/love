def1613eb2428b4f8bd9af2edf4049231f4c7c6d
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:27:03 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/lambda_timeout.mjs
+++ public/src/lambda_timeout.mjs
@@ -4,8 +4,8 @@ export async function lambda_timeout(fn, ms) {
     v,
     new Promise(function lambda2(_, reject) {
       function lambda() {
-        let r = reject(new Error("Timeout"));
-        return r;
+        let r4 = reject(new Error("Timeout"));
+        return r4;
       }
       let r2 = setTimeout(lambda, ms);
       return r2;
