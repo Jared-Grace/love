80d3e7dca93fc565d587a782d42064e952042aea
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 22:40:13 -0400

    function_auto_path ..\love\public\src\app_new_rule_set_new_2.mjs

--- web/public/src/app_new_rule_set_new_2.mjs
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -83,6 +83,11 @@ export function app_new_rule_set_new_2() {
       {
         start: js_keyword_function() + " id ( x , y ) { vs return ex ; }",
         end:
+          js_keyword_function() + " id ( x , y ) { let id = ex ; return ex ; }",
+      },
+      {
+        start: js_keyword_function() + " id ( x , y ) { vs return ex ; }",
+        end:
           js_keyword_function() +
           " a v e r a g e ( x , y ) { let s u m = a d d ( x , y ) ; return s u m / 2 ; }",
       },
