9ac022fb7dea87d2d53c7f53519d59cdd2070668
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:44:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_names.mjs

--- web/public/src/app_replace_rule_sets_fns_names.mjs
+++ web/public/src/app_replace_rule_sets_fns_names.mjs
@@ -1,7 +1,7 @@
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+import { list_map_name } from "../../../love/public/src/list_map_name.mjs";
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 export function app_replace_rule_sets_fns_names() {
   let fns = app_replace_rule_sets_fns();
-  let mapped = list_map_property(fns, "name");
+  let mapped = list_map_name(fns);
   return mapped;
 }
