7aac97213046bdb670172724fbee252cbd2d4dc2
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:35:47 -0400

    function_auto_path ..\love\public\src\js_flo_body_add_return_argument_curried.mjs

--- public/src/js_flo_body_add_return_argument_curried.mjs
+++ public/src/js_flo_body_add_return_argument_curried.mjs
@@ -1,6 +1,8 @@
 import { js_flo_body_add_return_argument } from "../../../portfolio_qa/public/src/js_flo_body_add_return_argument.mjs";
 export function js_flo_body_add_return_argument_curried(ast) {
-  return function js_flo_body_add_return_argument_curried_result(e) {
-    return js_flo_body_add_return_argument(ast, e);
+  let r2 = function js_flo_body_add_return_argument_curried_result(e) {
+    let r = js_flo_body_add_return_argument(ast, e);
+    return r;
   };
+  return r2;
 }
