f45820d6b242fbd3f7c30f63d2f69dde4e61ca6d
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:29:50 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -1,4 +1,4 @@
-import { lambda_timeout } from "../../../love/public/src/lambda_timeout.mjs";
+import { lambda_timeout_null } from "../../../love/public/src/lambda_timeout_null.mjs";
 import { list_map_property_invoke } from "../../../love/public/src/list_map_property_invoke.mjs";
 import { html_style_margin_top } from "../../../love/public/src/html_style_margin_top.mjs";
 import { app_replace_animation_duration_get } from "../../../love/public/src/app_replace_animation_duration_get.mjs";
@@ -192,7 +192,9 @@ export async function app_replace_rule_set(context) {
       );
       return t_inner;
     }
-    let t = await lambda_timeout_null(lambda, 1000);$inn
+    let t = await lambda_timeout_null(lambda, 1000);
+    if (null_not_is(value5)) {
+    }
     if (false) {
     }
     let found = property_get(t, "found");
