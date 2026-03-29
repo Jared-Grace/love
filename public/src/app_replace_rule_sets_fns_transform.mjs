8296b7fa430f33b45afe31a2eecc7d07ed8092ff
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:49:17 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_transform.mjs

--- public/src/app_replace_rule_sets_fns_transform.mjs
+++ public/src/app_replace_rule_sets_fns_transform.mjs
@@ -2,7 +2,6 @@ import { each } from "../../../love/public/src/each.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { js_list_nodes_object_expression } from "../../../love/public/src/js_list_nodes_object_expression.mjs";
-import { app_replace_rule_sets_fns_migrate_capitalization_upper } from "../../../love/public/src/app_replace_rule_sets_fns_migrate_capitalization_upper.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
 import { functions_transform_list } from "../../../love/public/src/functions_transform_list.mjs";
