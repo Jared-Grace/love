9fd8484f5246a09d6242a45b3532987dbcb11d0c
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:36:14 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_rules_used_generate.mjs

--- web/public/src/app_replace_rule_sets_fns_rules_used_generate.mjs
+++ web/public/src/app_replace_rule_sets_fns_rules_used_generate.mjs
@@ -55,13 +55,10 @@ export async function app_replace_rule_sets_fns_rules_used_generate() {
   }
   let result = await object_adder_async(lambda3);
   let e = js_object_to_expression(result);
-  let r22 = js_flo_body_add_return_argument_curried_right(e2);
-  async function lambda4(ast) {
-    js_flo_body_add_return_argument(ast, e);
-  }
+  let r22 = js_flo_body_add_return_argument_curried_right(e);
   let output = await function_transform_fn(
     app_replace_rule_sets_fns_rules_used,
-    lambda4,
+    r22,
   );
   return result;
 }
