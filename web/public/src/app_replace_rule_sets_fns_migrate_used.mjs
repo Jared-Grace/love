0b62e57f26e17bbd233882122b754f53019c0d55
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:51:30 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,5 +1,8 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_rule_sets_fns_transform } from "../../../love/public/src/app_replace_rule_sets_fns_transform.mjs";
 export async function app_replace_rule_sets_fns_migrate_used() {
-  function lambda() {}
+  function lambda(a) {
+    let item = property_get(a, "item");
+  }
   let result = await app_replace_rule_sets_fns_transform(lambda);
 }
