30b8b1d1ff76a091b423e534b9307f5da3ec34fd
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:48:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_capitalization_upper.mjs

--- public/src/app_replace_rule_sets_fns_migrate_capitalization_upper.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_capitalization_upper.mjs
@@ -1,34 +1,13 @@
 import { app_replace_rule_sets_fns_transform } from "../../../love/public/src/app_replace_rule_sets_fns_transform.mjs";
-import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
-import { null_is } from "../../../love/public/src/null_is.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { js_property_value_change } from "../../../love/public/src/js_property_value_change.mjs";
 import { js_literal_map_curried_right } from "../../../love/public/src/js_literal_map_curried_right.mjs";
-import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
-import { js_list_nodes_object_expression } from "../../../love/public/src/js_list_nodes_object_expression.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function app_replace_rule_sets_fns_migrate_capitalization_upper() {
   let result = await app_replace_rule_sets_fns_transform(lambda3);
-  async function lambda3(ast) {
-    let name = js_flo_name(ast);
-    log(app_replace_rule_sets_fns_migrate_capitalization_upper.name, {
-      name,
-    });
-    let list = js_list_nodes_object_expression(ast);
-    function lambda_each(item) {
-      let p = "name";
-      let name = js_object_expression_properties_find_key_named(item, p);
-      if (null_is(name)) {
-        return;
-      }
-      on_result(name);
-    }
-    each(list, lambda_each);
-  }
   return result;
   function on_result(name) {
     function lambda2(t) {
