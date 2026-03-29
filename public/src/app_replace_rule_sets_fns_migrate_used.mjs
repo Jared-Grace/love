0e09b6a5fe690afcc38ace18073feba06d30241c
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:17:31 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { app_replace_rules_parse_left_right_only } from "../../../love/public/src/app_replace_rules_parse_left_right_only.mjs";
 import { app_replace_rule_set_rules_used } from "../../../love/public/src/app_replace_rule_set_rules_used.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
@@ -28,9 +29,11 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     log(app_replace_rule_sets_fns_migrate_used.name, {
       rs,
     });
-    let rules2 = property_get(rs, "rules");
+    let rules = property_get(rs, "rules");
     let goals2 = property_get(rs, "goals");
     let rules_parsed = app_replace_rules_parse_left_right_only(rules);
+    function lambda2(item2) {}
+    each(list, lambda2);
     let rules_used = app_replace_rule_set_rules_used(rules_parsed, start, end);
   }
   let result = await app_replace_rule_sets_fns_transform(lambda);
