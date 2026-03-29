7330485784749ab0db9becb8a210d88ba9ad3029
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:41:47 -0400

    function_auto_path ..\love\public\src\js_property_key_named_is.mjs

--- public/src/js_property_key_named_is.mjs
+++ public/src/js_property_key_named_is.mjs
@@ -1,6 +1,6 @@
 import { js_identifier_named_try } from "../../../love/public/src/js_identifier_named_try.mjs";
 import { js_property_key_get } from "../../../love/public/src/js_property_key_get.mjs";
-export function js_property_key_named(p, search) {
+export function js_property_key_named_is(p, search) {
   let key = js_property_key_get(p);
   let eq = js_identifier_named_try(key, search);
   return eq;
