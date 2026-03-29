01c4e967ad018540227be89a3ea807fc47031b16
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 20:47:43 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_while.mjs

---
+++ public/src/app_replace_rule_set_statements_while_abbreviations.mjs
@@ -0,0 +1,10 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+import { app_replace_rule_set_statements_block_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_block_abbreviations.mjs";
+export function app_replace_rule_set_statements_while_abbreviations(
+  abbreviations,
+) {
+  app_replace_rule_set_statements_block_abbreviations(abbreviations);
+  object_merge(abbreviations, {
+    smg: ["", "w", "hile", "s", "tatement"],
+  });
+}
