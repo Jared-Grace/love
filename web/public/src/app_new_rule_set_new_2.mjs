2fa19deafdf68dc8d79a258b518ecec08c769a6a
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 20:42:02 -0400

    ara

---
+++ web/public/src/app_new_rule_set_new_2.mjs
@@ -0,0 +1,18 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
+export function app_new_rule_set_new_2() {
+  const rules = [];
+  list_add_multiple(rules, ["a > b"]);
+  let abbreviations = {};
+  let r = {
+    name: "TODO",
+    rules,
+    abbreviations,
+    goals: [
+      {
+        start: "a",
+        end: "b",
+      },
+    ],
+  };
+  return r;
+}
