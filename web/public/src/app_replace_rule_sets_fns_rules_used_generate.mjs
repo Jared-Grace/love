fc8efe1517f3b736482e6e1e4fe14edff52e4592
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:34:51 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_rules_used_generate.mjs

--- web/public/src/app_replace_rule_sets_fns_rules_used_generate.mjs
+++ web/public/src/app_replace_rule_sets_fns_rules_used_generate.mjs
@@ -1,4 +1,4 @@
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
 import { app_replace_rule_sets_fns_rules_used } from "../../../love/public/src/app_replace_rule_sets_fns_rules_used.mjs";
 import { object_adder_async } from "../../../love/public/src/object_adder_async.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
@@ -53,8 +53,10 @@ export async function app_replace_rule_sets_fns_rules_used_generate() {
   }
   let result = await object_adder_async(lambda3);
   let e = js_object_to_expression(result);
-  let fn = app_replace_rule_sets_fns_rules_used;
   async function lambda4(ast) {}
-  let output = await function_transform(f_name, lambda4);
+  let output = await function_transform_fn(
+    app_replace_rule_sets_fns_rules_used,
+    lambda4,
+  );
   return result;
 }
