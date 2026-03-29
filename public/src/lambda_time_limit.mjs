01f3a8b4e92ac2625750e6f10605b50bdf119ad0
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:18:45 -0400

    function_auto_path ..\love\public\src\lambda_time_limit.mjs

--- public/src/lambda_time_limit.mjs
+++ public/src/lambda_time_limit.mjs
@@ -1,1 +1,17 @@
-export function lambda_time_limit() {}
+export function lambda_time_limit() {
+  function runWithTimeout(fn, ms) {
+    let v = Promise.resolve().then(fn);
+    let r3 = Promise.race([
+      v,
+      new Promise(function lambda2(_, reject) {
+        function lambda() {
+          let r = reject(new Error("Timeout"));
+          return r;
+        }
+        let r2 = setTimeout(lambda, ms);
+        return r2;
+      }),
+    ]);
+    return r3;
+  }
+}
