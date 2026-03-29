09d6db674839ba02c79d1258c82e6b1ad965498b
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:26:17 -0400

    function_auto_path ..\love\public\src\lambda_timeout.mjs

--- public/src/lambda_timeout.mjs
+++ public/src/lambda_timeout.mjs
@@ -1,6 +1,6 @@
 export async function lambda_timeout(fn, ms) {
   let v = Promise.resolve().then(fn);
-  let r3 = Promise.race([
+  let r3 = await Promise.race([
     v,
     new Promise(function lambda2(_, reject) {
       function lambda() {
