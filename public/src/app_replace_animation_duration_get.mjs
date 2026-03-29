f572a1f72e1f26b6ffcea875368476a8c5bce0ed
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 13:13:06 -0400

    function_auto_path ..\love\public\src\app_replace_animation_duration_get.mjs

--- public/src/app_replace_animation_duration_get.mjs
+++ public/src/app_replace_animation_duration_get.mjs
@@ -1,6 +1,6 @@
 import { app_replace_animation_duration_default } from "../../../love/public/src/app_replace_animation_duration_default.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
-export function app_replace_animation_duration_initialize(context) {
+export function app_replace_animation_duration_get(context) {
   let value_initial = app_replace_animation_duration_default();
   let value5 = storage_local_initialize_context(
     context,
