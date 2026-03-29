9a22a65f571d94975a06ccb8e14fb8f8c6fe3472
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:03:49 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -8,6 +8,8 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     let item = property_get(a, "item");
     let p = "goals";
     let goals = js_object_expression_properties_find_key_named(item, p);
+    if (false) {
+    }
     const t = goals.value.type;
     if (equal_not(t, "ArrayExpression")) {
       log(app_replace_rule_sets_fns_migrate_used.name, {
