3b13a9a9843d8fee7ade2c51665e031cb59cac83
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:18:28 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_transform.mjs

--- web/public/src/app_replace_rule_sets_fns_transform.mjs
+++ web/public/src/app_replace_rule_sets_fns_transform.mjs
@@ -1,36 +1,8 @@
-import { each_async } from "../../../love/public/src/each_async.mjs";
-import { null_is } from "../../../love/public/src/null_is.mjs";
-import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
-import { js_list_nodes_object_expression } from "../../../love/public/src/js_list_nodes_object_expression.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
+import { app_replace_rule_sets_fns_transform_lambda } from "../../../love/public/src/app_replace_rule_sets_fns_transform_lambda.mjs";
 import { functions_transform_list } from "../../../love/public/src/functions_transform_list.mjs";
 import { app_replace_rule_sets_fns_names } from "../../../love/public/src/app_replace_rule_sets_fns_names.mjs";
 export async function app_replace_rule_sets_fns_transform(lambda$a) {
   let names = app_replace_rule_sets_fns_names();
-  async function app_replace_rule_sets_fns_transform_lambda(ast) {
-    let name = js_flo_name(ast);
-    log(app_replace_rule_sets_fns_transform.name, {
-      name,
-    });
-    let list = js_list_nodes_object_expression(ast);
-    async function lambda_each(item) {
-      let p = "name";
-      let name_property = js_object_expression_properties_find_key_named(
-        item,
-        p,
-      );
-      if (null_is(name_property)) {
-        return;
-      }
-      await lambda$a({
-        name_property,
-        item,
-        name,
-      });
-    }
-    await each_async(list, lambda_each);
-  }
   await functions_transform_list(
     names,
     app_replace_rule_sets_fns_transform_lambda,
