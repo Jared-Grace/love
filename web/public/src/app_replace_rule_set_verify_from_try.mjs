e7f4837029033245a1ab734fa17e5ea0cc24febd
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:27:06 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_verify_from_try.mjs

--- web/public/src/app_replace_rule_set_verify_from_try.mjs
+++ web/public/src/app_replace_rule_set_verify_from_try.mjs
@@ -1,3 +1,4 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { app_replace_rule_apply } from "../../../love/public/src/app_replace_rule_apply.mjs";
@@ -8,6 +9,8 @@ import { json_to } from "../../../love/public/src/json_to.mjs";
 import { graph_search_depth_first } from "../../../love/public/src/graph_search_depth_first.mjs";
 import { app_replace_rule_set_verify_goal_depth_max } from "../../../love/public/src/app_replace_rule_set_verify_goal_depth_max.mjs";
 export function app_replace_rule_set_verify_from_try(rules_parsed, start, end) {
+  function lambda2(item) {}
+  each(list, lambda2);
   log(app_replace_rule_set_verify_from_try.name, {
     rules_parsed,
     start,
