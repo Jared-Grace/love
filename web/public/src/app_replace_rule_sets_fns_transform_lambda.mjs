bc9e5e54e7cce02e1540d2b5bddcd476fa28dca6
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:18:34 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_transform_lambda.mjs

--- web/public/src/app_replace_rule_sets_fns_transform_lambda.mjs
+++ web/public/src/app_replace_rule_sets_fns_transform_lambda.mjs
@@ -2,7 +2,6 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { js_list_nodes_object_expression } from "../../../love/public/src/js_list_nodes_object_expression.mjs";
-import { app_replace_rule_sets_fns_transform } from "../../../love/public/src/app_replace_rule_sets_fns_transform.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
 export async function app_replace_rule_sets_fns_transform_lambda(ast) {
