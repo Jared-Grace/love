ee76e288334ddb3d032d7fd047ef0c89cce536d1
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:02:08 -0400

    de app_replace_rule_set_identifiers_simple_goals

--- public/src/app_replace_rule_set_identifiers_simple.mjs
+++ public/src/app_replace_rule_set_identifiers_simple.mjs
@@ -9,7 +9,52 @@ export function app_replace_rule_set_identifiers_simple() {
   let r = {
     name: "Identifiers Simple",
     rules: app_replace_rule_set_identifiers_simple_rules(),
-    goals: app_replace_rule_set_identifiers_simple_goals(),
+    goals: [
+    {
+      start: "id",
+      end: "_",
+    },
+    {
+      start: "id",
+      end: "idf ida",
+    },
+    {
+      start: "idf ida",
+      end: "$ 1",
+    },
+    {
+      start: "id",
+      end: "t 3",
+    },
+    {
+      start: "id",
+      end: "idf ida ida",
+    },
+    {
+      start: "idf ida ida",
+      end: "l u v",
+    },
+    {
+      start: "id",
+      end: "idf ida ida ida",
+    },
+    {
+      start: "idf ida ida ida",
+      end: "idf idf idf idf",
+    },
+    {
+      start: "idf idf idf idf",
+      end: "A B B A",
+    },
+    {
+      start: "idf ida ida ida",
+      end: "idf di idf idf",
+    },
+    {
+      start: "idf di idf idf",
+      end: "J 0 $ h",
+    },
+  ],
     abbreviations,
     why: "The replacement rules define a context-free grammar for a simplified identifier system, where identifiers can start with certain letters or symbols and may include a limited set of digits and additional characters, demonstrating how identifiers are constructed from allowed starting and subsequent characters.",
   };
