1283e439ea49f6cdadd93556308a7fc834714cd8
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:20:40 -0400

    function_auto_path ..\love\public\src\functions_asts_each.mjs

--- public/src/functions_asts_each.mjs
+++ public/src/functions_asts_each.mjs
@@ -1,6 +1,6 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { functions_asts } from "../../../love/public/src/functions_asts.mjs";
-export async function functions_asts_each(lambda) {
+export async function functions_asts_each(lambda$ast) {
   let asts = await functions_asts();
-  await each_async(asts, lambda);
+  await each_async(asts, lambda$ast);
 }
