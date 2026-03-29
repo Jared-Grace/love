e597f7ab406ce8c6b3c3f371d352b938948f937e
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:16:54 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,3 +1,4 @@
+import { app_replace_rules_parse_left_right_only } from "../../../love/public/src/app_replace_rules_parse_left_right_only.mjs";
 import { app_replace_rule_set_rules_used } from "../../../love/public/src/app_replace_rule_set_rules_used.mjs";
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -27,6 +28,7 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     log(app_replace_rule_sets_fns_migrate_used.name, {
       rs,
     });
+    let mapped = app_replace_rules_parse_left_right_only(rules);
     let rules_used = app_replace_rule_set_rules_used(rules_parsed, start, end);
   }
   let result = await app_replace_rule_sets_fns_transform(lambda);
