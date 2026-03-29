217c2cacdfe2f5cdbf2645a96a5921e8a63f203d
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 20:44:39 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_if_abbreviations.mjs

--- public/src/app_replace_rule_set_statements_if_abbreviations.mjs
+++ public/src/app_replace_rule_set_statements_if_abbreviations.mjs
@@ -1,3 +1,4 @@
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { app_replace_rule_set_statements_block_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_block_abbreviations.mjs";
 import { app_replace_rule_set_statements_variable_abbreviations } from "../../../love/public/src/app_replace_rule_set_statements_variable_abbreviations.mjs";
 export function app_replace_rule_set_statements_if_abbreviations(
@@ -5,4 +6,7 @@ export function app_replace_rule_set_statements_if_abbreviations(
 ) {
   app_replace_rule_set_statements_variable_abbreviations(abbreviations);
   app_replace_rule_set_statements_block_abbreviations(abbreviations);
+  object_merge(abbreviations, {
+    is: ["", "i", "f ", "s", "tatement"],
+  });
 }
