600678ba22a2d422dd6c3ae3f4ee5c2db3948d51
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 17:37:35 -0400

    function_auto_path ..\love\public\src\list_index_of_json.mjs

--- public/src/list_index_of_json.mjs
+++ public/src/list_index_of_json.mjs
@@ -1,8 +1,11 @@
+import { equal } from "../../../love/public/src/equal.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
 export function list_index_of_json(list, item) {
+  let search = json_to(item);
   function lambda(item_found) {
-    let json = json_to(object);
+    let json = json_to(item_found);
+    let eq2 = equal(left, right);
   }
   let found = list_find(list, lambda);
 }
