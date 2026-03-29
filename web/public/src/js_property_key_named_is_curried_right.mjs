ffce5b8f9195a124fb7e7926c332de53a015a027
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:41:48 -0400

    functions_rename_if_starts_with_prefix_suffix_add js_property_key_named is

--- web/public/src/js_property_key_named_is_curried_right.mjs
+++ web/public/src/js_property_key_named_is_curried_right.mjs
@@ -1,5 +1,5 @@
 import { js_property_key_named_is } from "../../../love/public/src/js_property_key_named_is.mjs";
-export function js_property_key_named_curried_right(search) {
+export function js_property_key_named_is_curried_right(search) {
   let r = function js_property_key_named_curried_right_result(p) {
     let eq = js_property_key_named_is(p, search);
     return eq;
