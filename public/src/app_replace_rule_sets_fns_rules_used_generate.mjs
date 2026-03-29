41bb20805d5fbb2f61101cadbbd417157fffc71b
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:35:27 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_rules_used_generate.mjs

--- public/src/app_replace_rule_sets_fns_rules_used_generate.mjs
+++ public/src/app_replace_rule_sets_fns_rules_used_generate.mjs
@@ -1,4 +1,4 @@
-import { js_flo_body_add_return } from "../../../love/public/src/js_flo_body_add_return.mjs";
+import { js_flo_body_add_return_argument } from "../../../portfolio_qa/public/src/js_flo_body_add_return_argument.mjs";
 import { function_transform_fn } from "../../../love/public/src/function_transform_fn.mjs";
 import { app_replace_rule_sets_fns_rules_used } from "../../../love/public/src/app_replace_rule_sets_fns_rules_used.mjs";
 import { object_adder_async } from "../../../love/public/src/object_adder_async.mjs";
@@ -55,7 +55,7 @@ export async function app_replace_rule_sets_fns_rules_used_generate() {
   let result = await object_adder_async(lambda3);
   let e = js_object_to_expression(result);
   async function lambda4(ast) {
-    let r = js_flo_body_add_return(ast2);
+    let r = js_flo_body_add_return_argument(ast);
   }
   let output = await function_transform_fn(
     app_replace_rule_sets_fns_rules_used,
