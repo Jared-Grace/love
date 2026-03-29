02206ec7d7ba40f3de7f509efd31ff5b5447af2a
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:29:18 -0400

    function_auto_path ..\love\public\src\lambda_timeout_null.mjs

--- web/public/src/lambda_timeout_null.mjs
+++ web/public/src/lambda_timeout_null.mjs
@@ -1,6 +1,6 @@
 import { lambda_timeout_generic } from "../../../love/public/src/lambda_timeout_generic.mjs";
 export async function lambda_timeout_null(fn, ms) {
-  let value = new Error(lambda_timeout_null.name);
+  let value = null;
   let r = await lambda_timeout_generic(fn, ms, value);
   return r;
 }
