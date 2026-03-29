1da7fa67e05c3f99d4ef7bd4cb8ecc8d615b4d1e
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 17:36:54 -0400

    function_auto_path ..\love\public\src\list_index_of_json.mjs

--- web/public/src/list_index_of_json.mjs
+++ web/public/src/list_index_of_json.mjs
@@ -1,1 +1,5 @@
-export function list_index_of_json() {}
+import { list_find } from "../../../love/public/src/list_find.mjs";
+export function list_index_of_json() {
+  function lambda(item) {}
+  let found = list_find(list, lambda);
+}
