a0095b66eea6d216bd1e06fbac595e179e08585d
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:37:35 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_rules_used.mjs

--- public/src/app_replace_rule_sets_fns_rules_used.mjs
+++ public/src/app_replace_rule_sets_fns_rules_used.mjs
@@ -1,5 +1,74 @@
+import { app_replace_rule_set_unary_to_binary_equations } from "../../../love/public/src/app_replace_rule_set_unary_to_binary_equations.mjs";
+import { app_replace_rule_set_expressions_assignment } from "../../../love/public/src/app_replace_rule_set_expressions_assignment.mjs";
+import { app_replace_rule_set_expressions_additive } from "../../../love/public/src/app_replace_rule_set_expressions_additive.mjs";
+import { app_replace_rule_set_expressions_function_calls } from "../../../love/public/src/app_replace_rule_set_expressions_function_calls.mjs";
+import { app_replace_rule_set_expressions_relational } from "../../../love/public/src/app_replace_rule_set_expressions_relational.mjs";
+import { app_replace_rule_set_scientific_notation_numbers } from "../../../love/public/src/app_replace_rule_set_scientific_notation_numbers.mjs";
+import { app_replace_rule_set_expressions_equality } from "../../../love/public/src/app_replace_rule_set_expressions_equality.mjs";
+import { app_replace_rule_set_expressions_member_and_access } from "../../../love/public/src/app_replace_rule_set_expressions_member_and_access.mjs";
+import { app_replace_rule_set_expressions_primary } from "../../../love/public/src/app_replace_rule_set_expressions_primary.mjs";
+import { app_replace_rule_set_strings_simple } from "../../../love/public/src/app_replace_rule_set_strings_simple.mjs";
+import { app_replace_rule_set_function_declarations } from "../../../love/public/src/app_replace_rule_set_function_declarations.mjs";
+import { app_replace_rule_set_expressions_logical } from "../../../love/public/src/app_replace_rule_set_expressions_logical.mjs";
+import { app_replace_rule_set_expressions_multiplicative } from "../../../love/public/src/app_replace_rule_set_expressions_multiplicative.mjs";
+import { app_replace_rule_set_identifiers_simple } from "../../../love/public/src/app_replace_rule_set_identifiers_simple.mjs";
+import { app_replace_rule_set_integers } from "../../../love/public/src/app_replace_rule_set_integers.mjs";
+import { app_replace_rule_set_integer_digits } from "../../../love/public/src/app_replace_rule_set_integer_digits.mjs";
+import { app_replace_rule_set_decimals } from "../../../love/public/src/app_replace_rule_set_decimals.mjs";
+import { app_replace_rule_set_expressions_unary } from "../../../love/public/src/app_replace_rule_set_expressions_unary.mjs";
+import { app_replace_rule_set_statements_if } from "../../../love/public/src/app_replace_rule_set_statements_if.mjs";
+import { app_replace_rule_set_statements_variable } from "../../../love/public/src/app_replace_rule_set_statements_variable.mjs";
+import { app_replace_rule_set_unary_to_binary_equations_preparation } from "../../../love/public/src/app_replace_rule_set_unary_to_binary_equations_preparation.mjs";
+import { app_replace_rule_set_binary_counting } from "../../../love/public/src/app_replace_rule_set_binary_counting.mjs";
+import { app_replace_rule_set_unary_equations_adding } from "../../../love/public/src/app_replace_rule_set_unary_equations_adding.mjs";
+import { app_replace_rule_set_expand_collapse } from "../../../love/public/src/app_replace_rule_set_expand_collapse.mjs";
+import { app_replace_rule_set_shrink_left_pair_replace_right_same } from "../../../love/public/src/app_replace_rule_set_shrink_left_pair_replace_right_same.mjs";
+import { app_replace_rule_set_grow_left_pair_change_right } from "../../../love/public/src/app_replace_rule_set_grow_left_pair_change_right.mjs";
+import { app_replace_rule_set_statements_for } from "../../../love/public/src/app_replace_rule_set_statements_for.mjs";
+import { app_replace_rule_set_statements_while } from "../../../love/public/src/app_replace_rule_set_statements_while.mjs";
+import { app_replace_rule_set_statements_simple } from "../../../love/public/src/app_replace_rule_set_statements_simple.mjs";
+import { app_replace_rule_set_statements_block } from "../../../love/public/src/app_replace_rule_set_statements_block.mjs";
+import { app_replace_rule_set_swap_change_left } from "../../../love/public/src/app_replace_rule_set_swap_change_left.mjs";
+import { app_replace_rule_set_replace_flow } from "../../../love/public/src/app_replace_rule_set_replace_flow.mjs";
+import { app_replace_rule_set_replace } from "../../../love/public/src/app_replace_rule_set_replace.mjs";
+import { app_replace_rule_set_swap_change_right } from "../../../love/public/src/app_replace_rule_set_swap_change_right.mjs";
+import { app_replace_rule_set_swap } from "../../../love/public/src/app_replace_rule_set_swap.mjs";
+import { app_replace_rule_set_replace_2 } from "../../../love/public/src/app_replace_rule_set_replace_2.mjs";
+import { app_replace_rule_set_shrink_left } from "../../../love/public/src/app_replace_rule_set_shrink_left.mjs";
+import { app_replace_rule_set_shrink_right } from "../../../love/public/src/app_replace_rule_set_shrink_right.mjs";
+import { app_replace_rule_set_grow_left } from "../../../love/public/src/app_replace_rule_set_grow_left.mjs";
+import { app_replace_rule_set_grow_right } from "../../../love/public/src/app_replace_rule_set_grow_right.mjs";
+import { app_replace_rule_set_swap_2 } from "../../../love/public/src/app_replace_rule_set_swap_2.mjs";
+import { app_replace_rule_set_binary_counting_prepare_2 } from "../../../love/public/src/app_replace_rule_set_binary_counting_prepare_2.mjs";
+import { app_replace_rule_set_swap_3 } from "../../../love/public/src/app_replace_rule_set_swap_3.mjs";
+import { app_replace_rule_set_shrink_different_2 } from "../../../love/public/src/app_replace_rule_set_shrink_different_2.mjs";
+import { app_replace_rule_set_grow_different_2 } from "../../../love/public/src/app_replace_rule_set_grow_different_2.mjs";
+import { app_replace_rule_set_shrink_between } from "../../../love/public/src/app_replace_rule_set_shrink_between.mjs";
+import { app_replace_rule_set_grow_between } from "../../../love/public/src/app_replace_rule_set_grow_between.mjs";
+import { app_replace_rule_set_unary_equations } from "../../../love/public/src/app_replace_rule_set_unary_equations.mjs";
+import { app_replace_rule_set_grow_triple } from "../../../love/public/src/app_replace_rule_set_grow_triple.mjs";
+import { app_replace_rule_set_shrink_both_same } from "../../../love/public/src/app_replace_rule_set_shrink_both_same.mjs";
+import { app_replace_rule_set_shrink_triple } from "../../../love/public/src/app_replace_rule_set_shrink_triple.mjs";
+import { app_replace_rule_set_replace_left_same } from "../../../love/public/src/app_replace_rule_set_replace_left_same.mjs";
+import { app_replace_rule_set_right_change } from "../../../love/public/src/app_replace_rule_set_right_change.mjs";
+import { app_replace_rule_set_same_replace_left } from "../../../love/public/src/app_replace_rule_set_same_replace_left.mjs";
+import { app_replace_rule_set_half } from "../../../love/public/src/app_replace_rule_set_half.mjs";
+import { app_replace_rule_set_same_replace_right } from "../../../love/public/src/app_replace_rule_set_same_replace_right.mjs";
+import { app_replace_rule_set_replace_right_same } from "../../../love/public/src/app_replace_rule_set_replace_right_same.mjs";
+import { app_replace_rule_set_double } from "../../../love/public/src/app_replace_rule_set_double.mjs";
+import { app_replace_rule_set_shrink_different } from "../../../love/public/src/app_replace_rule_set_shrink_different.mjs";
+import { app_replace_rule_set_grow_different } from "../../../love/public/src/app_replace_rule_set_grow_different.mjs";
+import { app_replace_rule_set_boolean_literal } from "../../../love/public/src/app_replace_rule_set_boolean_literal.mjs";
+import { app_replace_rule_set_grow_same_three_different } from "../../../love/public/src/app_replace_rule_set_grow_same_three_different.mjs";
+import { app_replace_rule_set_two_different_grow_same } from "../../../love/public/src/app_replace_rule_set_two_different_grow_same.mjs";
+import { app_replace_rule_set_shrink_same_three_different } from "../../../love/public/src/app_replace_rule_set_shrink_same_three_different.mjs";
+import { app_replace_rule_set_three_different_grow_same } from "../../../love/public/src/app_replace_rule_set_three_different_grow_same.mjs";
+import { app_replace_rule_set_binary_counting_prepare_1 } from "../../../love/public/src/app_replace_rule_set_binary_counting_prepare_1.mjs";
+import { app_replace_rule_set_binary_numbers } from "../../../love/public/src/app_replace_rule_set_binary_numbers.mjs";
+import { app_replace_rule_set_binary_numbers_simple_2 } from "../../../love/public/src/app_replace_rule_set_binary_numbers_simple_2.mjs";
+import { app_replace_rule_set_binary_numbers_simple } from "../../../love/public/src/app_replace_rule_set_binary_numbers_simple.mjs";
 export function app_replace_rule_sets_fns_rules_used() {
-  return {
+  let r = {
     app_replace_rule_set_binary_numbers_simple: [
       [
         {
@@ -8189,4 +8258,5 @@ export function app_replace_rule_sets_fns_rules_used() {
       ],
     ],
   };
+  return r;
 }
