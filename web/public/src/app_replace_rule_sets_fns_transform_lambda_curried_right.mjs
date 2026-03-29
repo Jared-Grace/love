1722e7d462611b5b1d1eae28aa5799f5147743c2
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 18:19:06 -0400

    cur app_replace_rule_sets_fns_transform_lambda

--- web/public/src/app_replace_rule_sets_fns_transform_lambda_curried_right.mjs
+++ web/public/src/app_replace_rule_sets_fns_transform_lambda_curried_right.mjs
@@ -1,1 +1,10 @@
-export function app_replace_rule_sets_fns_transform_lambda_curried_right() {}
+import { app_replace_rule_sets_fns_transform_lambda } from "../../../love/public/src/app_replace_rule_sets_fns_transform_lambda.mjs";
+export async function app_replace_rule_sets_fns_transform_lambda_curried_right(
+  lambda$a,
+) {
+  return async function app_replace_rule_sets_fns_transform_lambda_curried_right_result(
+    ast,
+  ) {
+    return await app_replace_rule_sets_fns_transform_lambda(ast, lambda$a);
+  };
+}
