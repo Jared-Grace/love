18e72242ff3272b8ab905158ce21bb2b2fec1d71
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 17:18:54 -0400

    function_auto_path ..\love\public\src\app_replace_deploy_inner.mjs

--- public/src/app_replace_deploy.mjs
+++ public/src/app_replace_deploy.mjs
@@ -1,5 +1,5 @@
 import { command_line_node_r } from "../../../love/public/src/command_line_node_r.mjs";
-import { app_replace_deploy_2 } from "../../../love/public/src/app_replace_deploy_2.mjs";
+import { app_replace_deploy_inner } from "../../../love/public/src/app_replace_deploy_inner.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_replace_rule_sets_fns_names } from "../../../love/public/src/app_replace_rule_sets_fns_names.mjs";
 import { app_replace_rule_set_rename } from "../../../love/public/src/app_replace_rule_set_rename.mjs";
@@ -8,5 +8,5 @@ export async function app_replace_deploy() {
   let result = await app_replace_rule_sets_fns_migrate_capitalization_upper();
   let mapped = app_replace_rule_sets_fns_names();
   await each_async(mapped, app_replace_rule_set_rename);
-  await command_line_node_r(app_replace_deploy_2.name, []);
+  await command_line_node_r(app_replace_deploy_inner.name, []);
 }
--- public/src/app_replace_deploy_inner.mjs
+++ public/src/app_replace_deploy_inner.mjs
@@ -2,7 +2,7 @@ import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { html_update_latest_promote_deploy_app_fn } from "../../../love/public/src/html_update_latest_promote_deploy_app_fn.mjs";
 import { app_replace_tests_run } from "../../../love/public/src/app_replace_tests_run.mjs";
 import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app_replace_rule_sets_why_generate.mjs";
-export async function app_replace_deploy_2() {
+export async function app_replace_deploy_inner() {
   "there is a potential rename, so these functions are ran after";
   await app_replace_rule_sets_why_generate();
   await app_replace_tests_run();
