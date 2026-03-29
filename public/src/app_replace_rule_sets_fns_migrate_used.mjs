47b711b4829edc3d0c799107ee9fdf8571dbac32
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:53:56 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,4 +1,3 @@
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -8,9 +7,8 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     let item = property_get(a, "item");
     let p = "goals";
     let goals = js_object_expression_properties_find_key_named(item, p);
-    let mapped = list_map_property(goals, "value");
     log(app_replace_rule_sets_fns_migrate_used.name, {
-      goals,
+      goals: goals.value.type,
     });
   }
   let result = await app_replace_rule_sets_fns_transform(lambda);
