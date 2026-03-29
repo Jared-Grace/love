c63e1507a1b3a201f854d6682e71c0ad72a7444a
Grace-Jared <christrosetolife@gmail.com>
2026-03-28 22:59:55 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set_statements_for.mjs

--- public/src/app_replace_rule_set_statements_for.mjs
+++ public/src/app_replace_rule_set_statements_for.mjs
@@ -5,7 +5,7 @@ export function app_replace_rule_set_statements_for() {
   const rules = [];
   app_replace_rule_set_statements_for_rules(rules);
   list_add_multiple(rules, [
-    "ex > let i = 0",
+    "ex > i = 0",
     "ex > i < 12",
     "ex > i = i + 1",
     "ex > c o p y [ i ] = l i s t [ i ]",
@@ -22,19 +22,19 @@ export function app_replace_rule_set_statements_for() {
     goals: [
       {
         start: "fs",
-        end: "for ( let i = 0 ; i < 12 ; i = i + 1 ) sm",
+        end: "for ( i = 0 ; i < 12 ; i = i + 1 ) sm",
       },
       {
-        start: "for ( let i = 0 ; i < 12 ; i = i + 1 ) sm",
-        end: "for ( let i = 0 ; i < 12 ; i = i + 1 ) l o g ( a p o s t l e [ i ] ) ;",
+        start: "for ( i = 0 ; i < 12 ; i = i + 1 ) sm",
+        end: "for ( i = 0 ; i < 12 ; i = i + 1 ) l o g ( a p o s t l e [ i ] ) ;",
       },
       {
         start: "fs",
-        end: "for ( let i = 0 ; i < l i s t . l e n g t h ; i = i + 1 ) sm",
+        end: "for ( i = 0 ; i < l i s t . l e n g t h ; i = i + 1 ) sm",
       },
       {
-        start: "for ( let i = 0 ; i < l i s t . l e n g t h ; i = i + 1 ) sm",
-        end: "for ( let i = 0 ; i < l i s t . l e n g t h ; i = i + 1 ) { c o p y [ i ] = l i s t [ i ] ; l o g ( i ) ; }",
+        start: "for ( i = 0 ; i < l i s t . l e n g t h ; i = i + 1 ) sm",
+        end: "for ( i = 0 ; i < l i s t . l e n g t h ; i = i + 1 ) { c o p y [ i ] = l i s t [ i ] ; l o g ( i ) ; }",
       },
     ],
     why: "The replacement rules describe the grammar for JavaScript-like variable declarations, statements, and especially for-loops, demonstrating how for-loops are constructed with initialization, condition, increment, and a statement or block as the body.",
