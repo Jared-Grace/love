5bb8739d793adf28391c66b4ee4de110026752b5
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 17:37:13 -0400

    function_auto_path ..\love\public\src\list_index_of_json.mjs

--- public/src/list_index_of_json.mjs
+++ public/src/list_index_of_json.mjs
@@ -1,5 +1,8 @@
+import { json_to } from "../../../love/public/src/json_to.mjs";
 import { list_find } from "../../../love/public/src/list_find.mjs";
-export function list_index_of_json() {
-  function lambda(item) {}
+export function list_index_of_json(list, item) {
+  function lambda(item_found) {
+    let json = json_to(object);
+  }
   let found = list_find(list, lambda);
 }
