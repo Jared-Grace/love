3a4ba4354b5671548651dedec2aa39ed513e14d6
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 17:15:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_why_generate_single.mjs

--- public/src/app_replace_rule_sets_why_generate_single.mjs
+++ public/src/app_replace_rule_sets_why_generate_single.mjs
@@ -1,4 +1,4 @@
-import { property_delete_if_exists } from "../../../love/public/src/property_delete_if_exists.mjs";
+import { properties_delete_if_exists } from "../../../love/public/src/properties_delete_if_exists.mjs";
 import { app_replace_rule_sets_why_generate_single_openai } from "../../../love/public/src/app_replace_rule_sets_why_generate_single_openai.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_start_end_get } from "../../../love/public/src/app_replace_start_end_get.mjs";
@@ -7,7 +7,7 @@ import { app_replace_rules_parse_left_right_only } from "../../../love/public/sr
 import { property_change } from "../../../love/public/src/property_change.mjs";
 export async function app_replace_rule_sets_why_generate_single(rs) {
   let rule_set = property_get(rs, "rule_set");
-  properties_delete_if_exists(rule_set, ["why",'rules_used']);
+  properties_delete_if_exists(rule_set, ["why", "rules_used"]);
   let value2 = property_change(
     rule_set,
     "rules",
