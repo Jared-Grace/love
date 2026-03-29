6c556b4486026372cd35bdda6ef772f2c9433bad
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:12:53 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,7 +1,5 @@
 import { function_run_args_none } from "../../../love/public/src/function_run_args_none.mjs";
-import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { js_object_expression_properties_find_key_named } from "../../../love/public/src/js_object_expression_properties_find_key_named.mjs";
@@ -20,15 +18,12 @@ export async function app_replace_rule_sets_fns_migrate_used() {
     if (equal_not(t, "ArrayExpression")) {
       return;
     }
-    let code = js_unparse(value);
-    let expression = js_parse_expression(code);
     let name = property_get(a, "name");
     log(app_replace_rule_sets_fns_migrate_used.name, {
       name,
     });
     let rs = await function_run_args_none(name);
     log(app_replace_rule_sets_fns_migrate_used.name, {
-      expression,
       rs,
     });
   }
