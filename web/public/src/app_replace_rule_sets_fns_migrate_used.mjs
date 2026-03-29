87fdc78604e3ec8340fbf3e48c3ae66d06b717c2
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 15:46:54 -0400

    function_auto_path ..\love\public\src\app_replace_rule_sets_fns_migrate_used.mjs

--- web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
+++ web/public/src/app_replace_rule_sets_fns_migrate_used.mjs
@@ -1,1 +1,4 @@
-export function app_replace_rule_sets_fns_migrate_used() {}
+import { app_replace_rule_sets_fns_transform } from "../../../love/public/src/app_replace_rule_sets_fns_transform.mjs";
+export async function app_replace_rule_sets_fns_migrate_used() {
+  let result = await app_replace_rule_sets_fns_transform(lambda3);
+}
