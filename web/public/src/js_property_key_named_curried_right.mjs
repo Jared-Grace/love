b8694325a120c787f87a7a4f5ac04f53052643a2
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:41:47 -0400

    function_auto_path ..\love\public\src\js_property_key_named_is.mjs

--- web/public/src/js_property_key_named_curried_right.mjs
+++ web/public/src/js_property_key_named_curried_right.mjs
@@ -1,7 +1,7 @@
-import { js_property_key_named } from "../../../love/public/src/js_property_key_named.mjs";
+import { js_property_key_named_is } from "../../../love/public/src/js_property_key_named_is.mjs";
 export function js_property_key_named_curried_right(search) {
   let r = function js_property_key_named_curried_right_result(p) {
-    let eq = js_property_key_named(p, search);
+    let eq = js_property_key_named_is(p, search);
     return eq;
   };
   return r;
