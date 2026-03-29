2dfc3934da231aa39894f0ffe14bba5213bb4437
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:41:48 -0400

    functions_rename_if_starts_with_prefix_suffix_add js_property_key_named is

--- public/src/js_object_expression_property_named_or_null.mjs
+++ public/src/js_object_expression_property_named_or_null.mjs
@@ -1,9 +1,9 @@
 import { list_find_or_null } from "../../../love/public/src/list_find_or_null.mjs";
-import { js_property_key_named_curried_right } from "../../../love/public/src/js_property_key_named_curried_right.mjs";
+import { js_property_key_named_is_curried_right } from "../../../love/public/src/js_property_key_named_is_curried_right.mjs";
 import { js_object_expression_properties } from "../../../love/public/src/js_object_expression_properties.mjs";
 export function js_object_expression_property_named_or_null(e, search) {
   let properties = js_object_expression_properties(e);
-  let r = js_property_key_named_curried_right(search);
+  let r = js_property_key_named_is_curried_right(search);
   let found = list_find_or_null(properties, r);
   return found;
 }
