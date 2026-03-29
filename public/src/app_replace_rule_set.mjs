b98339f1c513d21906a6f2a5fa49e0025a015a20
Grace-Jared <christrosetolife@gmail.com>
2026-03-29 16:59:51 -0400

    function_auto_path ..\love\public\src\app_replace_rule_set.mjs

--- public/src/app_replace_rule_set.mjs
+++ public/src/app_replace_rule_set.mjs
@@ -174,7 +174,7 @@ export async function app_replace_rule_set(context) {
     let t = app_replace_rule_set_verify_from_try(rules_used, start, end);
     let found = property_get(t, "found");
     if (not(found)) {
-      html_style_background_color_set(start_over, "#ff3e3eff");
+      html_style_background_color_set(start_over, "red");
     }
     refresh_count_increase();
     function each_rule(rule, index) {
