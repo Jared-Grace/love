53d1f13212e7c23c917cf311869896a02f5fb07e
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 17:09:13 -0400

    function_auto_path ..\love\public\src\function_source_replace.mjs

--- public/src/function_source_replace.mjs
+++ public/src/function_source_replace.mjs
@@ -1,12 +1,10 @@
+import { file_transform_replace } from "../../../love/public/src/file_transform_replace.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_name_to_path_search } from "../../../love/public/src/function_name_to_path_search.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
-import { text_replace_curried_right_2 } from "../../../love/public/src/text_replace_curried_right_2.mjs";
-import { file_transform } from "../../../love/public/src/file_transform.mjs";
 export async function function_source_replace(f_name, from, to) {
   arguments_assert(arguments, 3);
   let r3 = await function_name_to_path_search(f_name);
   let f_path = property_get(r3, "f_path");
-  let r2 = text_replace_curried_right_2(from, to);
-  let r = await file_transform(f_path, r2);
+  await file_transform_replace(from, to, f_path);
 }
