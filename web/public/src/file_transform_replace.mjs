36a9c1522d15242b4ed3fc915a28e36f9907218f
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 17:09:13 -0400

    function_auto_path ..\love\public\src\function_source_replace.mjs

---
+++ web/public/src/file_transform_replace.mjs
@@ -0,0 +1,6 @@
+import { file_transform } from "../../../love/public/src/file_transform.mjs";
+import { text_replace_curried_right_2 } from "../../../love/public/src/text_replace_curried_right_2.mjs";
+export async function file_transform_replace(from, to, f_path) {
+  let r2 = text_replace_curried_right_2(from, to);
+  let r = await file_transform(f_path, r2);
+}
