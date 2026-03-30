ebfc677e0f82dc25d4399754d5f28b24eccfa119
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:38:10 -0400

    rrf2 file_text_replace function_source

--- public/src/function_source_replace.mjs
+++ public/src/function_source_replace.mjs
@@ -1,5 +1,5 @@
 import { file_transform } from "../../../love/public/src/file_transform.mjs";
-export async function file_text_replace() {
+export async function function_source_replace() {
   async function lambda2() {}
   let r = await file_transform(f_path, lambda2);
 }
