9c41cffd9167c98c21031e40757b0c10ca332419
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:47:44 -0400

    function_auto_path ..\love\public\src\text_remove.mjs

--- public/src/text_remove.mjs
+++ public/src/text_remove.mjs
@@ -1,6 +1,6 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
-export function text_remove(from, removal) {
+export function text_remove(t, removal) {
   let empty = "";
-  let replaced = text_replace(from, removal, empty);
+  let replaced = text_replace(t, removal, empty);
   return replaced;
 }
