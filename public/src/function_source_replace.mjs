f933379c79e9ee0ccd5fa06e45eb60a64bb233f0
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:38:49 -0400

    function_auto_path ..\love\public\src\function_source_replace.mjs

--- public/src/function_source_replace.mjs
+++ public/src/function_source_replace.mjs
@@ -1,7 +1,7 @@
 import { function_name_to_path } from "../../../love/public/src/function_name_to_path.mjs";
 import { file_transform } from "../../../love/public/src/file_transform.mjs";
-export async function function_source_replace() {
-  let f_path2 = function_name_to_path(f_name);
+export async function function_source_replace(f_name) {
+  let f_path = function_name_to_path(f_name);
   async function lambda2() {}
   let r = await file_transform(f_path, lambda2);
 }
