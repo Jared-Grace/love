c58e6568d074b18a81d3f6614e59d1839e505015
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:43:48 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,10 @@
-export async function sandbox() {}
+export async function sandbox() {
+  let list = [
+    "id > e m p t y",
+    "id > t a u t o l o g y",
+    "id > i d e n t i t y",
+    "id > i n v o k e",
+    "id > a v e r a g e",
+    "id > s u m",
+  ];
+}
