c70977e37193e3c11eafa8bd92121df3206a63c5
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:43:23 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,1 @@
-import { server_url } from "../../../love/public/src/server_url.mjs";
-import { log_keep } from "../../../love/public/src/log_keep.mjs";
-export async function sandbox() {
-  log_keep(sandbox.name, "Static server running at: " + server_url());
-}
+export async function sandbox() {}
