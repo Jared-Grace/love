1cc4401d4877c9d895844517b92c88c0c9cc05ba
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:39:27 -0400

    function_auto_path ..\love\public\src\function_source_replace.mjs

--- public/src/function_source_replace.mjs
+++ public/src/function_source_replace.mjs
@@ -4,6 +4,5 @@ import { file_transform } from "../../../love/public/src/file_transform.mjs";
 export async function function_source_replace(f_name) {
   let f_path = function_name_to_path(f_name);
   let r2 = text_replace_curried_right_2(from, to);
-  async function lambda2() {}
-  let r = await file_transform(f_path, lambda2);
+  let r = await file_transform(f_path, r2);
 }
