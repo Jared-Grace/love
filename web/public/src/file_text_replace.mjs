2a3ee5e5afd539f950c5c36220ffa0c19daa0051
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:34:57 -0400

    function_auto_path ..\love\public\src\file_text_replace.mjs

--- web/public/src/file_text_replace.mjs
+++ web/public/src/file_text_replace.mjs
@@ -1,1 +1,5 @@
-export function file_text_replace() {}
+import { file_transform } from "../../../love/public/src/file_transform.mjs";
+export async function file_text_replace() {
+  async function lambda2() {}
+  let r = await file_transform(f_path, lambda2);
+}
