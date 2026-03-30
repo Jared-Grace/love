e3674dfdd4e1ea1b10d2c5e2f22cad0536eee629
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 17:12:29 -0400

    function_auto_path ..\love\public\src\functions_paths.mjs

--- web/public/src/functions_paths.mjs
+++ web/public/src/functions_paths.mjs
@@ -1,8 +1,10 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { repos_paths_map_unordered_combine_squash_functions } from "../../../love/public/src/repos_paths_map_unordered_combine_squash_functions.mjs";
 import { functions_names_from_path } from "../../../love/public/src/functions_names_from_path.mjs";
 import { function_name_folder_to_path } from "../../../love/public/src/function_name_folder_to_path.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export async function functions_paths() {$aa
+export async function functions_paths() {
+  arguments_assert(arguments, 0);
   async function mapper(folder) {
     let f_names = await functions_names_from_path(folder);
     function lambda2(f_name) {
