24edae6c7d5fb87d7a16e1013f912104a2e8e8f0
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:44:07 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { function_source_replace } from "../../../love/public/src/function_source_replace.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function sandbox() {
   let list = [
@@ -8,6 +9,8 @@ export async function sandbox() {
     "id > a v e r a g e",
     "id > s u m",
   ];
-  async function lambda(item) {}
-  await each_async(list2, lambda);
+  async function lambda(item) {
+    await function_source_replace(f_name, from, to);
+  }
+  await each_async(list, lambda);
 }
