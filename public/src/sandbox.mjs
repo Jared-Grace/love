57c0fb0ae3e4f6542f4463efac70e90f347022c6
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:52:37 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,12 +1,12 @@
-import { app_replace_rule_set_statements_while } from "../../../love/public/src/app_replace_rule_set_statements_while.mjs";
+import { app_replace_rule_set_statements_if } from "../../../love/public/src/app_replace_rule_set_statements_if.mjs";
 import { text_remove } from "../../../love/public/src/text_remove.mjs";
 import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
 import { function_source_replace } from "../../../love/public/src/function_source_replace.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function sandbox() {
-  let f_name = app_replace_rule_set_statements_while.name;
-  let prefix = "ex > ";
-  let list = ["ex > f o u n d", "ex > a s k", "ex > s e e k"];
+  let f_name = app_replace_rule_set_statements_if.name;
+  let prefix = "";
+  let list = ['" r e s e t "'];
   let mapped = list_map_prefix_without(list, prefix);
   async function lambda(from) {
     const removal = " ";
