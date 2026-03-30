c0d41b076353b07e0c089e24ef543f290fc22a50
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 17:11:01 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,23 +1,23 @@
+import { functions_paths } from "../../../love/public/src/functions_paths.mjs";
+import { file_transform_replace } from "../../../love/public/src/file_transform_replace.mjs";
 import { functions_names_paths } from "../../../love/public/src/functions_names_paths.mjs";
-import { functions_names_each } from "../../../love/public/src/functions_names_each.mjs";
 import { app_replace_rule_set_statements_if } from "../../../love/public/src/app_replace_rule_set_statements_if.mjs";
 import { text_remove } from "../../../love/public/src/text_remove.mjs";
 import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
-import { function_source_replace } from "../../../love/public/src/function_source_replace.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function sandbox() {
   let f_name = app_replace_rule_set_statements_if.name;
   let prefix = "";
   let list = ['" l u v "', "3 . 1 4"];
   let mapped = list_map_prefix_without(list, prefix);
-  async function lambda2(f_name) {
+  async function lambda2(f_path) {
     async function lambda(from) {
       const removal = " ";
       let replaced = text_remove(from, removal);
-      await function_source_replace(f_name, from, replaced);
+      await file_transform_replace(f_path, from, replaced);
     }
     await each_async(mapped, lambda);
   }
   let r = await functions_names_paths();
-  await functions_names_each(lambda2);
+  await functions_paths(lambda2);
 }
