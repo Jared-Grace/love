ca71b5f278427496e6e4aff9337f50467dbbdb46
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:49:12 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,5 @@
 import { app_replace_rule_set_function_declarations } from "../../../love/public/src/app_replace_rule_set_function_declarations.mjs";
 import { text_remove } from "../../../love/public/src/text_remove.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
 import { function_source_replace } from "../../../love/public/src/function_source_replace.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -19,10 +18,6 @@ export async function sandbox() {
   async function lambda(from) {
     const removal = " ";
     let replaced = text_remove(from, removal);
-    log(sandbox.name, {
-      from,
-      replaced,
-    });
     await function_source_replace(f_name, from, replaced);
   }
   await each_async(mapped, lambda);
