ac91aa15c94ea9d24376b1c2d2eb10f85d1d7837
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:57:41 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -5,8 +5,8 @@ import { list_map_prefix_without } from "../../../love/public/src/list_map_prefi
 import { function_source_replace } from "../../../love/public/src/function_source_replace.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function sandbox() {
-  async function lambda2(f_name2) {
-    let f_name = app_replace_rule_set_statements_if.name;
+  let f_name = app_replace_rule_set_statements_if.name;
+  async function lambda2(f_name) {
     let prefix = "";
     let list = ['" l u v "'];
     let mapped = list_map_prefix_without(list, prefix);
