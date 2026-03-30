13b6d40a557003e60ee7cadb4d6cdbdc4a07880e
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 16:57:47 -0400

    function_auto_path ..\love\public\src\sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -6,10 +6,10 @@ import { function_source_replace } from "../../../love/public/src/function_sourc
 import { each_async } from "../../../love/public/src/each_async.mjs";
 export async function sandbox() {
   let f_name = app_replace_rule_set_statements_if.name;
+  let prefix = "";
+  let list = ['" l u v "'];
+  let mapped = list_map_prefix_without(list, prefix);
   async function lambda2(f_name) {
-    let prefix = "";
-    let list = ['" l u v "'];
-    let mapped = list_map_prefix_without(list, prefix);
     async function lambda(from) {
       const removal = " ";
       let replaced = text_remove(from, removal);
