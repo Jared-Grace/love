5185433d1afeb964b69c50f6abcaa87541c06ea6
Grace-Jared <christrosetolife@gmail.com>
2026-03-30 17:09:28 -0400

    pf file_transform_replace f_path

--- public/src/file_transform_replace.mjs
+++ public/src/file_transform_replace.mjs
@@ -1,6 +1,6 @@
 import { file_transform } from "../../../love/public/src/file_transform.mjs";
 import { text_replace_curried_right_2 } from "../../../love/public/src/text_replace_curried_right_2.mjs";
-export async function file_transform_replace(from, to, f_path) {
+export async function file_transform_replace(f_path, from, to) {
   let r2 = text_replace_curried_right_2(from, to);
   let r = await file_transform(f_path, r2);
 }
