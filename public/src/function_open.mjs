057ffbf3b3ed28059569b0aaa6428d34f2b4c786
JESUS rose to life <username@users.noreply.github.com>
2026-03-31 21:15:57 -0400

    "p"

--- public/src/function_open.mjs
+++ public/src/function_open.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
 import { function_name_to_path_unalias } from "../../../love/public/src/function_name_to_path_unalias.mjs";
@@ -5,6 +6,9 @@ import { file_open } from "../../../love/public/src/file_open.mjs";
 export async function function_open(f_name) {
   const v = await function_name_to_path_unalias(f_name);
   let f_path = property_get(v, "f_path");
+  log(function_open.name, {
+    f_path,
+  });
   await file_open(f_path);
   await function_current_set(f_name);
 }
