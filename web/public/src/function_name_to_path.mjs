044e83f30110a590a9ba55e1d9097e97e28da07d
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:54:53 -0400

    function_auto_path ../love/public/src/function_name_to_path.mjs

--- web/public/src/function_name_to_path.mjs
+++ web/public/src/function_name_to_path.mjs
@@ -1,12 +1,8 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { function_name_folder_to_path } from "../../../love/public/src/function_name_folder_to_path.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
 import { functions_path } from "../../../love/public/src/functions_path.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 export function function_name_to_path(f_name) {
-  log(function_name_to_path.name, {
-    f_name,
-  });
   if (text_includes(f_name, ".")) {
     error_json({
       f_name,
