ddc8124bcff8273bedf1f71688b9f72832fa57ca
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:48:43 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,6 +2,6 @@ import { file_read } from "../../../love/public/src/file_read.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 export async function sandbox() {
   let p = folder_user_docs_path("monothesism_wiki.txt");
-  let contents = await file_read(file_path);
-  return p;
+  let contents = await file_read(p);
+  return contents;
 }
