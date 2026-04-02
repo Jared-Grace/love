a12453a73c2fe4a00f73e8a26ccc1104359aa7d9
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:44:40 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
 import { firebase_storage_url_project_jg } from "../../../love/public/src/firebase_storage_url_project_jg.mjs";
 export async function sandbox() {
   let project_url = firebase_storage_url_project_jg();
+  let text = await http_local_text(url, project_url2);
 }
