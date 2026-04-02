1e9ae070f50701fa4dffc7f0ad86ae7d9d229f8b
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:45:03 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -2,5 +2,8 @@ import { http_local_text } from "../../../love/public/src/http_local_text.mjs";
 import { firebase_storage_url_project_jg } from "../../../love/public/src/firebase_storage_url_project_jg.mjs";
 export async function sandbox() {
   let project_url = firebase_storage_url_project_jg();
-  let text = await http_local_text(url, project_url2);
+  let text = await http_local_text(
+    "https://en.wikipedia.org/wiki/Monotheism",
+    project_url,
+  );
 }
