d7cef5fa14f544a26920dd55f0613e40de918506
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:54:37 -0400

    pb list_find_indices

--- public/src/list_find_indices.mjs
+++ public/src/list_find_indices.mjs
@@ -1,6 +1,6 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
-export function list_find_indices(predicate, split) {
+export function list_find_indices(split, predicate) {
   function lambda(la) {
     function lambda2(item, index) {
       let sw = predicate(item);
