5e43f7969ec73eec2b93b561340c03c39f04c06e
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:54:31 -0400

    function_auto_path ../love/public/src/sandbox.mjs

---
+++ web/public/src/list_find_indices.mjs
@@ -0,0 +1,15 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
+import { each_index } from "../../../love/public/src/each_index.mjs";
+export function list_find_indices(predicate, split) {
+  function lambda(la) {
+    function lambda2(item, index) {
+      let sw = predicate(item);
+      if (sw) {
+        la(index);
+      }
+    }
+    each_index(split, lambda2);
+  }
+  let list = list_adder(lambda);
+  return list;
+}
