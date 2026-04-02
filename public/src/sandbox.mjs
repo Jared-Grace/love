986c9ef80d5c225edeaee332188ab4793ef03533
JESUS rose to life <username@users.noreply.github.com>
2026-04-02 15:42:42 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,1 +1,4 @@
-export async function sandbox() {}
+import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
+export async function sandbox() {
+  let p = folder_user_docs_path(file_name);
+}
