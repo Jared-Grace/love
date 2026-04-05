f071b19d0a2262c7ea7c9a67fcb3c1585c5ac588
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 19:39:16 -0400

    function_auto_path ../love/public/src/list_take_end_count.mjs

--- web/public/src/list_take_end_count.mjs
+++ web/public/src/list_take_end_count.mjs
@@ -1,10 +1,7 @@
-import { negative_not_is_assert } from "../../../love/public/src/negative_not_is_assert.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
+import { list_take_end_count_generic } from "../../../karate_code/public/src/list_take_end_count_generic.mjs";
 import { list_take } from "./list_take.mjs";
 export function list_take_end_count(list, count) {
-  let end = list_size(list);
-  let c = end - count;
-  negative_not_is_assert(c);
+  let c = list_take_end_count_generic(list, count);
   let result = list_take(list, c);
   return result;
 }
