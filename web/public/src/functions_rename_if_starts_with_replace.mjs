07d87fe096d4e4bbe7b9a4dada1c2e6abbb7a427
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:43:49 -0400

    function_auto_path ../love/public/src/functions_rename_if_starts_with_replace.mjs

--- web/public/src/functions_rename_if_starts_with_replace.mjs
+++ web/public/src/functions_rename_if_starts_with_replace.mjs
@@ -1,3 +1,4 @@
+import { text_replace_curried_right_2 } from "../../../love/public/src/text_replace_curried_right_2.mjs";
 import { functions_rename_generic_starts_with } from "../../../love/public/src/functions_rename_generic_starts_with.mjs";
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
@@ -7,6 +8,7 @@ export async function functions_rename_if_starts_with_replace(
   to,
 ) {
   arguments_assert(arguments, 3);
+  let r2 = text_replace_curried_right_2(from2, to2);
   await functions_rename_generic_starts_with(name_change, f_name_prefix);
   function name_change(f_name_before) {
     let f_name_after = text_replace(f_name_before, from, to);
