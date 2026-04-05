98b43b1415cc6bd1fffb3af5186c8e2100c97634
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:49:51 -0400

    function_auto_path ../love/public/src/app_shared_button_uncolored_background_color.mjs

---
+++ public/src/app_shared_button_uncolored_background_color.mjs
@@ -0,0 +1,4 @@
+export function app_shared_button_uncolored_background_color() {
+  let v = "#d5d5d5";
+  return v;
+}
--- public/src/app_shared_button_uncolored_style_assign.mjs
+++ public/src/app_shared_button_uncolored_style_assign.mjs
@@ -1,8 +1,8 @@
-import { app_karate_button_uncolored_background_color } from "../../../karate_code/public/src/app_karate_button_uncolored_background_color.mjs";
+import { app_shared_button_uncolored_background_color } from "../../../love/public/src/app_shared_button_uncolored_background_color.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_shared_button_uncolored_style_assign(component) {
   html_style_assign(component, {
     color: "black",
-    "background-color": app_karate_button_uncolored_background_color(),
+    "background-color": app_shared_button_uncolored_background_color(),
   });
 }
