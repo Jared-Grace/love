fff6f176faa146cdd91a1c3110f4636773557394
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:46:17 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { function_rename_replace_curried_right_2 } from "../../../love/public/src/function_rename_replace_curried_right_2.mjs";
 import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { app_karate } from "../../../karate_code/public/src/app_karate.mjs";
@@ -7,6 +8,7 @@ import { function_dependencies_single } from "../../../love/public/src/function_
 export async function sandbox() {
   let r = await function_dependencies_single(app_g_main.name);
   let filtered = list_filter_starts_with(r, app_karate.name);
+  await function_rename_replace_curried_right_2(from, to);
   async function lambda(item) {
     let v = await function_rename_replace(item, "karate", "shared");
   }
