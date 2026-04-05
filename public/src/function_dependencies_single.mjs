898840006d3b539380a52b4908528a6112337c8f
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:28:19 -0400

    fws function_dependencies single

---
+++ public/src/function_dependencies_single.mjs
@@ -0,0 +1,4 @@
+import { function_dependencies } from "../../../love/public/src/function_dependencies.mjs";
+export async function function_dependencies_single(f_names) {
+  return await function_dependencies(f_names);
+}
