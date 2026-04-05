92ea56989d9193f32abd8d9970c65c33bdc6784c
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:28:36 -0400

    function_auto_path ../love/public/src/function_dependencies_single.mjs

--- web/public/src/function_dependencies_single.mjs
+++ web/public/src/function_dependencies_single.mjs
@@ -1,5 +1,5 @@
 import { function_dependencies } from "../../../love/public/src/function_dependencies.mjs";
-export async function function_dependencies_single(f_names) {
-  let r = await function_dependencies(f_names);
+export async function function_dependencies_single(f_name) {
+  let r = await function_dependencies([f_name]);
   return r;
 }
