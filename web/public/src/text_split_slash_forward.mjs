723fbe945494c60598f90d465f5efe37231c0910
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 19:35:48 -0400

    p

--- web/public/src/text_split_slash_forward.mjs
+++ web/public/src/text_split_slash_forward.mjs
@@ -2,6 +2,6 @@ import { text_slash_forward } from "../../../love/public/src/text_slash_forward.
 import { text_split } from "../../../love/public/src/text_split.mjs";
 export function text_split_slash_forward(f_path) {
   let separator = text_slash_forward();
-  let split2 = text_split(f_path, separator);
-  return split2;
+  let split = text_split(f_path, separator);
+  return split;
 }
