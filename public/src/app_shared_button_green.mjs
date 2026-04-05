2509de57e39bda98f72a9c574a2c304f9570a7c2
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:49:41 -0400

    function_auto_path ../love/public/src/app_shared_button_uncolored.mjs

--- public/src/app_shared_button_green.mjs
+++ public/src/app_shared_button_green.mjs
@@ -1,7 +1,7 @@
 import { app_shared_button_screen_green_style_assign } from "../../../love/public/src/app_shared_button_screen_green_style_assign.mjs";
-import { app_karate_button_uncolored } from "../../../karate_code/public/src/app_karate_button_uncolored.mjs";
+import { app_shared_button_uncolored } from "../../../love/public/src/app_shared_button_uncolored.mjs";
 export function app_shared_button_green(div, text, lambda) {
-  let component = app_karate_button_uncolored(div, text, lambda);
+  let component = app_shared_button_uncolored(div, text, lambda);
   app_shared_button_screen_green_style_assign(component);
   return component;
 }
