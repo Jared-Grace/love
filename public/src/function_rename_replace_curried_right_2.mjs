10716a4044931cd30251c4c0c8c2f337a8188439
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:46:05 -0400

    curc rr 2

--- public/src/function_rename_replace_curried_right_2.mjs
+++ public/src/function_rename_replace_curried_right_2.mjs
@@ -1,1 +1,8 @@
-export function function_rename_replace_curried_right_2() {}
+import { function_rename_replace } from "../../../love/public/src/function_rename_replace.mjs";
+export async function function_rename_replace_curried_right_2(from, to) {
+  return async function function_rename_replace_curried_right_2_result(
+    f_name_before,
+  ) {
+    return await function_rename_replace(f_name_before, from, to);
+  };
+}
