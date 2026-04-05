e7af6bf5f4c63d8f5367345e992ecd84205e765a
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:49:57 -0400

    function_auto_path ../love/public/src/app_shared_style_control.mjs

---
+++ web/public/src/app_shared_style_control.mjs
@@ -0,0 +1,13 @@
+import { app_karate_style_control_font_size } from "../../../karate_code/public/src/app_karate_style_control_font_size.mjs";
+import { app_karate_margin_y_set } from "../../../karate_code/public/src/app_karate_margin_y_set.mjs";
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function app_shared_style_control(component) {
+  html_style_assign(component, {
+    "border-radius": "0.8em",
+    padding: "0.55em",
+    width: "100%",
+    "border-width": "0px",
+    "font-size": app_karate_style_control_font_size(),
+  });
+  app_karate_margin_y_set(component);
+}
