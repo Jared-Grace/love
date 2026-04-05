b34c0d85d01248c2a270fb89a16155c10d6651e5
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 17:48:11 -0400

    function_auto_path ../love/public/src/git_push_repos.mjs

--- web/public/src/git_push_repos.mjs
+++ web/public/src/git_push_repos.mjs
@@ -1,8 +1,12 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { git_push_folder } from "../../../love/public/src/git_push_folder.mjs";
 import { repos_paths_map_unordered } from "../../../love/public/src/repos_paths_map_unordered.mjs";
 export async function git_push_repos() {
   await repos_paths_map_unordered(each_folder);
   async function each_folder(folder) {
+    log(git_push_repos.name, {
+      folder,
+    });
     await git_push_folder(folder);
   }
 }
