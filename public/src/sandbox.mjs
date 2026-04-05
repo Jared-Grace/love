a16c53bfc33e662ab13e204d8d0b62ae843124d9
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:38:17 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,1 @@
-import { wiki_monotheism_dates } from "../../../love/public/src/wiki_monotheism_dates.mjs";
-export async function sandbox() {
-  let r = await wiki_monotheism_dates();
-  return r;
-}
+export async function sandbox() {}
