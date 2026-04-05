c17b29de03d96c0b0c42b512ab3aca4d975fdc06
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 19:41:45 -0400

    function_auto_path ../love/public/src/integer_is_assert.mjs

--- public/src/integer_is_assert.mjs
+++ public/src/integer_is_assert.mjs
@@ -1,8 +1,8 @@
 import { assert_json } from "../../../love/public/src/assert_json.mjs";
 import { integer_is } from "../../../love/public/src/integer_is.mjs";
-export function integer_is_assert(index) {
-  let ii = integer_is(index);
+export function integer_is_assert(i) {
+  let ii = integer_is(i);
   assert_json(ii, {
-    index,
+    index: i,
   });
 }
