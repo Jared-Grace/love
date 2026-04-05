9e177e001586712032707289fb4c836c0a8466c7
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:49:26 -0400

    function_auto_path ../love/public/src/app_shared_button_green.mjs

---
+++ web/public/src/app_shared_button_green.mjs
@@ -0,0 +1,7 @@
+import { app_karate_button_screen_green_style_assign } from "../../../karate_code/public/src/app_karate_button_screen_green_style_assign.mjs";
+import { app_karate_button_uncolored } from "../../../karate_code/public/src/app_karate_button_uncolored.mjs";
+export function app_shared_button_green(div, text, lambda) {
+  let component = app_karate_button_uncolored(div, text, lambda);
+  app_karate_button_screen_green_style_assign(component);
+  return component;
+}
