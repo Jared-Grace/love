1ac4219743789a8c7d63f38a62bc579b8103f263
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:49:45 -0400

    function_auto_path ../love/public/src/app_shared_button_uncolored_style_assign.mjs

--- web/public/src/app_shared_button_uncolored.mjs
+++ web/public/src/app_shared_button_uncolored.mjs
@@ -1,4 +1,4 @@
-import { app_karate_button_uncolored_style_assign } from "../../../karate_code/public/src/app_karate_button_uncolored_style_assign.mjs";
+import { app_shared_button_uncolored_style_assign } from "../../../love/public/src/app_shared_button_uncolored_style_assign.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { app_karate_style_control } from "../../../karate_code/public/src/app_karate_style_control.mjs";
@@ -6,6 +6,6 @@ export function app_shared_button_uncolored(div, text, lambda) {
   arguments_assert(arguments, 3);
   let component = html_button(div, text, lambda);
   app_karate_style_control(component);
-  app_karate_button_uncolored_style_assign(component);
+  app_shared_button_uncolored_style_assign(component);
   return component;
 }
