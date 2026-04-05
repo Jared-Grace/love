575ed8e896530d3445cd36c76f1a877c19c59cdf
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:46:49 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -8,7 +8,7 @@ import { function_dependencies_single } from "../../../love/public/src/function_
 export async function sandbox() {
   let r = await function_dependencies_single(app_g_main.name);
   let filtered = list_filter_starts_with(r, app_karate.name);
-  await function_rename_replace_curried_right_2(from, to);
+  function_rename_replace_curried_right_2("karate", "shared");
   async function lambda(item) {
     let v = await function_rename_replace(item, "karate", "shared");
   }
