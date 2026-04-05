5b4667ea29f95ff56d7b3e226539d72b61443c73
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:50:04 -0400

    function_auto_path ../love/public/src/app_shared_style_control_font_size.mjs

--- public/src/app_shared_style_control.mjs
+++ public/src/app_shared_style_control.mjs
@@ -1,4 +1,4 @@
-import { app_karate_style_control_font_size } from "../../../karate_code/public/src/app_karate_style_control_font_size.mjs";
+import { app_shared_style_control_font_size } from "../../../love/public/src/app_shared_style_control_font_size.mjs";
 import { app_karate_margin_y_set } from "../../../karate_code/public/src/app_karate_margin_y_set.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_shared_style_control(component) {
@@ -7,7 +7,7 @@ export function app_shared_style_control(component) {
     padding: "0.55em",
     width: "100%",
     "border-width": "0px",
-    "font-size": app_karate_style_control_font_size(),
+    "font-size": app_shared_style_control_font_size(),
   });
   app_karate_margin_y_set(component);
 }
