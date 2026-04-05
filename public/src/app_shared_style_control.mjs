ed85c501e5389e815af96fe208b8f9298ec7d1a7
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:50:09 -0400

    function_auto_path ../love/public/src/app_shared_margin_y_set.mjs

--- public/src/app_shared_style_control.mjs
+++ public/src/app_shared_style_control.mjs
@@ -1,5 +1,5 @@
 import { app_shared_style_control_font_size } from "../../../love/public/src/app_shared_style_control_font_size.mjs";
-import { app_karate_margin_y_set } from "../../../karate_code/public/src/app_karate_margin_y_set.mjs";
+import { app_shared_margin_y_set } from "../../../love/public/src/app_shared_margin_y_set.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function app_shared_style_control(component) {
   html_style_assign(component, {
@@ -9,5 +9,5 @@ export function app_shared_style_control(component) {
     "border-width": "0px",
     "font-size": app_shared_style_control_font_size(),
   });
-  app_karate_margin_y_set(component);
+  app_shared_margin_y_set(component);
 }
