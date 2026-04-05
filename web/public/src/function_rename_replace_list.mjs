e1b77b9bed528ad3ee159ad7c9d9087d3c13b07f
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:48:06 -0400

    function_auto_path ../love/public/src/function_rename_replace_list.mjs

--- web/public/src/function_rename_replace_list.mjs
+++ web/public/src/function_rename_replace_list.mjs
@@ -1,6 +1,6 @@
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_rename_replace_curried_right_2 } from "../../../love/public/src/function_rename_replace_curried_right_2.mjs";
-export async function function_rename_replace_list(filtered, from, to) {
+export async function function_rename_replace_list(list, from, to) {
   let c = function_rename_replace_curried_right_2(from, to);
-  await each_async(filtered, c);
+  await each_async(list, c);
 }
