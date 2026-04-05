86e414b5fd0557690d13526ad40187b0dc09a953
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:50:43 -0400

    sb

--- web/public/src/app_shared_button_back_text.mjs
+++ web/public/src/app_shared_button_back_text.mjs
@@ -1,5 +1,5 @@
 import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs";
-export function app_karate_button_back_text() {
+export function app_shared_button_back_text() {
   let text = emoji_arrow_left() + " Back";
   return text;
 }
