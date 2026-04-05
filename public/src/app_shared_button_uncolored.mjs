ee96630f5f51f39507a98880573837690aa0bcd4
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:49:57 -0400

    function_auto_path ../love/public/src/app_shared_style_control.mjs

--- public/src/app_shared_button_uncolored.mjs
+++ public/src/app_shared_button_uncolored.mjs
@@ -1,11 +1,11 @@
 import { app_shared_button_uncolored_style_assign } from "../../../love/public/src/app_shared_button_uncolored_style_assign.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-import { app_karate_style_control } from "../../../karate_code/public/src/app_karate_style_control.mjs";
+import { app_shared_style_control } from "../../../love/public/src/app_shared_style_control.mjs";
 export function app_shared_button_uncolored(div, text, lambda) {
   arguments_assert(arguments, 3);
   let component = html_button(div, text, lambda);
-  app_karate_style_control(component);
+  app_shared_style_control(component);
   app_shared_button_uncolored_style_assign(component);
   return component;
 }
