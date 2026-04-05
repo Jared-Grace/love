9b658a93c9365ba1f27d30c47704463ce0fa83f2
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 19:01:16 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,6 +1,8 @@
 import { function_curryify_specify } from "../../../karate_code/public/src/function_curryify_specify.mjs";
 import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
 export async function sandbox() {
-  await function_delete_if_exists(f_name);
+  await function_delete_if_exists(
+    "list_random_item_invoke_count_curried_specify_1_3",
+  );
   let r = await function_curryify_specify(f_name2, positions_comma);
 }
