69b48f55701db98124245b26fef1a32131ca05a3
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 15:38:47 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,5 @@
+import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
 import { function_dependencies_single } from "../../../love/public/src/function_dependencies_single.mjs";
 export async function sandbox() {
-  let r = await function_dependencies_single(f_name);
+  let r = await function_dependencies_single(app_g_main.name);
 }
