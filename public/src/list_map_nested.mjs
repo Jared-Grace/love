341d988ecf4d1881e2b9646f8cbee0f0e19f2a6e
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 19:23:22 -0400

    pb list_map_nested

--- public/src/list_map_nested.mjs
+++ public/src/list_map_nested.mjs
@@ -1,5 +1,5 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function list_map_nested(mapper, clauses) {
+export function list_map_nested(clauses, mapper) {
   function lambda(item) {
     let mapped2 = list_map(item, mapper);
     return mapped2;
