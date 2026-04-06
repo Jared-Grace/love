845ae5bf424faff9f470c54fc9200996b3253d69
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 19:04:09 -0400

    function_auto_path ../love/public/src/list_permute.mjs

--- public/src/list_permute.mjs
+++ public/src/list_permute.mjs
@@ -1,7 +1,9 @@
+import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function list_permute(r, fns) {
+export function list_permute(list, fns) {
+  let r = list_first_remaining(list);
   let first = property_get(r, "first");
   let result = [];
   let remaining = property_get(r, "remaining");
