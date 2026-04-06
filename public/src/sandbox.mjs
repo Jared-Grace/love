14cc141da25417ba6d5de7867e81a1567eff610a
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:49:05 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,6 @@
+import { equal_not_curried_right } from "../../../karate_code/public/src/equal_not_curried_right.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function sandbox() {
+  let s = equal_not_curried_right.name;
   let result = await data_identifiers_search(s);
 }
