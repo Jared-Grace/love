5e30f1d84debe9cc48e0d90b4924d48635d55c0a
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 19:10:14 -0400

    function_auto_path ../***REMOVED***/public/src/***REMOVED***.mjs

---
+++ web/public/src/list_permute.mjs
@@ -0,0 +1,7 @@
+import { list_permute_inner } from "../../../love/public/src/list_permute_inner.mjs";
+export function list_permute(list, fns) {
+  let result = [];
+  let candidate = [];
+  list_permute_inner(list, fns, result, candidate);
+  return result;
+}
