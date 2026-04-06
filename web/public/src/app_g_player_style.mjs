69b6a82ae84668fa52e7d5c9a884e7d75eef887b
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 18:15:31 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

---
+++ web/public/src/app_g_player_style.mjs
@@ -0,0 +1,4 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+export function app_g_player_style(player_img_c) {
+  html_style_set(player_img_c, "animation", "pulseGlow 2s infinite alternate");
+}
