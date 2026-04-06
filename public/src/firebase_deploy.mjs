ec1e23f1c7ef2100db2418ea05cc0f084cfca657
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 14:53:13 -0400

    function_auto_path ../love/public/src/firebase_deploy.mjs

--- public/src/firebase_deploy.mjs
+++ public/src/firebase_deploy.mjs
@@ -2,7 +2,7 @@ import { user_repo_path_combine } from "../../../love/public/src/user_repo_path_
 import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
 export async function firebase_deploy() {
   let combined = await user_repo_path_combine(".");
-  let stdout = await command_line_generic("firebase deploy", {
+  let stdout = await command_line_generic("npx firebase deploy", {
     cwd: combined,
   });
   return stdout;
