e90ae869fb7a2651bca87b9baa2dde4f6214ff58
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 20:16:17 -0400

    function_auto_path ../love/public/src/g_distance_1_any_filter.mjs

--- public/src/g_distance_1_any_filter.mjs
+++ public/src/g_distance_1_any_filter.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { g_distance_1_any_curried_right } from "../../../love/public/src/g_distance_1_any_curried_right.mjs";
 export function g_distance_1_any_filter(waters, candidates) {
   let c = g_distance_1_any_curried_right(waters);
   let filtered = list_filter(candidates, c);
+  log(g_distance_1_any_filter.name, {
+    filtered,
+  });
   return filtered;
 }
