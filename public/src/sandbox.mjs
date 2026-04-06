c9b40d40e7b893808e18d0fa8eafc7f10b413538
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:42:45 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,8 +1,10 @@
+import { function_curryify_right_count } from "../../../love/public/src/function_curryify_right_count.mjs";
 import { list_random_item_invoke_count } from "../../../love/public/src/list_random_item_invoke_count.mjs";
 import { function_curryify_specify } from "../../../love/public/src/function_curryify_specify.mjs";
 import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
 export async function sandbox() {
   await function_delete_if_exists("property_equals_not_curried_right_2");
+  let output = await function_curryify_right_count(f_name, count);
   let r = await function_curryify_specify(
     list_random_item_invoke_count.name,
     "1,3",
