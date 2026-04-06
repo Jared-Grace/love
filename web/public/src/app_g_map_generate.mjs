c10f38da009244b14bcdfa22e8d09634a93504e6
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 20:12:38 -0400

    function_auto_path ../love/public/src/app_g_map_generate.mjs

--- web/public/src/app_g_map_generate.mjs
+++ web/public/src/app_g_map_generate.mjs
@@ -1,7 +1,6 @@
 import { app_g_map_generate_waters } from "../../../karate_code/public/src/app_g_map_generate_waters.mjs";
 import { list_random_item_count_nested } from "../../../love/public/src/list_random_item_count_nested.mjs";
 import { g_tiles_grasses_choices_weighted } from "../../../love/public/src/g_tiles_grasses_choices_weighted.mjs";
-import { g_coordinates } from "../../../love/public/src/g_coordinates.mjs";
 export function app_g_map_generate() {
   let tiles_choices = g_tiles_grasses_choices_weighted();
   let row_count = 15;
@@ -11,7 +10,6 @@ export function app_g_map_generate() {
     row_count,
     column_count,
   );
-  let coordinates = g_coordinates(rows);
   app_g_map_generate_waters(coordinates);
   return rows;
 }
