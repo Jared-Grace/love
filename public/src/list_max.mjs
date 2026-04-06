72e73fbaddbadd792670c0439fae3832a24f3fa6
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 20:09:24 -0400

    function_replace reduce list_reduce

--- public/src/list_max.mjs
+++ public/src/list_max.mjs
@@ -1,9 +1,9 @@
-import { reduce } from "../../../love/public/src/reduce.mjs";
+import { list_reduce } from "../../../love/public/src/list_reduce.mjs";
 export function list_max(list) {
   function lambda(a, b) {
     let v = a > b ? a : b;
     return v;
   }
-  const max = list.reduce(lambda, -Infinity);
+  const max = list.list_reduce(lambda, -Infinity);
   return max;
 }
