1764c2f1c400ddbc34a2f560522c517237afb412
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:46:29 -0400

    function_auto_path ../love/public/src/integer_to.mjs

--- public/src/integer_to.mjs
+++ public/src/integer_to.mjs
@@ -1,7 +1,7 @@
 import { null_not_is_assert } from "../../../love/public/src/null_not_is_assert.mjs";
 import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
 export function integer_to(integer_text) {
-  let index = integer_to_try(integer_text);
-  null_not_is_assert(index);
-  return index;
+  let i = integer_to_try(integer_text);
+  null_not_is_assert(i);
+  return i;
 }
