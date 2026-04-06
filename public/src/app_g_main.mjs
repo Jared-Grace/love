7c34314920a9a9e8601c673ff2c21b5d9f66aa8a
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 22:59:26 -0400

    function_auto_path ../love/public/src/app_g_main.mjs

--- public/src/app_g_main.mjs
+++ public/src/app_g_main.mjs
@@ -1,3 +1,4 @@
+import { equal_not_curried_right } from "../../../karate_code/public/src/equal_not_curried_right.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -100,6 +101,7 @@ export async function app_g_main(context) {
   let names_men = bible_names_men();
   let right = player_img;
   let coordinates = g_coordinates(rows);
+  let r = equal_not_curried_right(right2);
   function lambda2(row) {
     function lambda3(item2) {}
     let filtered = list_filter(list, lambda3);
