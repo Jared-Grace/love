41e00f71e79253a0048057a95d9c6bcba9aca660
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 14:48:40 -0400

    function_auto_path ../love/public/src/html_scroll_none.mjs

--- web/public/src/html_scroll_none.mjs
+++ web/public/src/html_scroll_none.mjs
@@ -2,12 +2,12 @@ import { uuid_browser } from "../../../karate_code/public/src/uuid_browser.mjs";
 import { html_data_set } from "../../../love/public/src/html_data_set.mjs";
 import { html_style_head } from "../../../love/public/src/html_style_head.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export async function html_scroll_none(component) {
+export function html_scroll_none(component) {
   html_style_assign(component, {
     scrollbarWidth: "none",
     msOverflowStyle: "none",
   });
-  let u = await uuid_browser();
+  uuid_browser();
   html_data_set(component, html_scroll_none.name, u);
   html_style_head(`
   [data-${html_scroll_none.name}="${u}"]::-webkit-scrollbar {
