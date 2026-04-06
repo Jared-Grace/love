cca51bab580ff1ad89afd15d290dbfceedca35a3
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 16:20:28 -0400

    function_auto_path ../love/public/src/list_map_property_get.mjs

--- web/public/src/list_map_property_get.mjs
+++ web/public/src/list_map_property_get.mjs
@@ -1,7 +1,7 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
-export function list_map_property_get(lilst, object) {
+export function list_map_property_get(list, object) {
   let c = property_get_curried(object);
-  let mapped = list_map(lilst, c);
+  let mapped = list_map(list, c);
   return mapped;
 }
