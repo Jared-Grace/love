d2b73f23afa3b31d07f4f7005d625d5a33c040b3
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 16:54:17 -0400

    function_auto_path ../love/public/src/app_g_game_save.mjs

--- public/src/app_g_game_save.mjs
+++ public/src/app_g_game_save.mjs
@@ -1,12 +1,10 @@
-import { repo_love_path } from "../../../love/public/src/repo_love_path.mjs";
+import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_path.mjs";
 import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
-import { file_name_json_folder_gitignore } from "../../../love/public/src/file_name_json_folder_gitignore.mjs";
 import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 export async function app_g_game_save(g) {
   storage_local_set(app_g, "game", g);
-  let f_path = file_name_json_folder_gitignore("1");
-  let joined = repo_love_path(f_path);
+  let joined = app_g_game_save_path();
   await file_overwrite(joined, contents);
   return joined;
 }
