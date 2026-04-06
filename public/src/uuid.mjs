2d1187d9ced9fe8ec15d1ea215552bd611ef9bf0
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 14:49:14 -0400

    function_auto_path ../love/public/src/uuid.mjs

--- public/src/uuid.mjs
+++ public/src/uuid.mjs
@@ -1,12 +1,6 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { uuid_browser } from "../../../karate_code/public/src/uuid_browser.mjs";
-import { browser_is } from "../../../love/public/src/browser_is.mjs";
 import { import_install } from "../../../love/public/src/import_install.mjs";
 export async function uuid() {
-  if (browser_is()) {
-    let r = uuid_browser();
-    return r;
-  }
   const r2 = await import_install("uuid");
   let uuidv4 = property_get(r2, "v4");
   let u = uuidv4();
