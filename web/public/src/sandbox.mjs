cf6c9b6ec418c83610b010c3a01f180e1d9755be
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:52:34 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,11 +1,13 @@
+import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
+import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { properties_size_1 } from "../../../karate_code/public/src/properties_size_1.mjs";
 import { equal_not_curried_right } from "../../../karate_code/public/src/equal_not_curried_right.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function sandbox() {
   let s = equal_not_curried_right.name;
   let result = await data_identifiers_search(s);
-  let s1 = properties_size_1(result);
+  let properties = properties_get(result);
+  let s1 = list_size_1(properties);
   if (s1) {
   } else {
     log_keep(sandbox.name, "Used in multiple places. Not deleting.");
