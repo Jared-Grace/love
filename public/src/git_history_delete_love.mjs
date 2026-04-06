e0edad296ec99c8684b45a8f12e93034753e3068
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 16:53:47 -0400

    function_auto_path ../love/public/src/repo_love_name.mjs

--- public/src/git_history_delete_love.mjs
+++ public/src/git_history_delete_love.mjs
@@ -1,7 +1,7 @@
-import { repo_name_love } from "../../../love/public/src/repo_name_love.mjs";
+import { repo_love_name } from "../../../love/public/src/repo_love_name.mjs";
 import { git_history_delete } from "../../../love/public/src/git_history_delete.mjs";
 export async function git_history_delete_love(f_path) {
-  let repo = repo_name_love();
+  let repo = repo_love_name();
   let v = await git_history_delete("Jared-Grace", repo, f_path, ".");
   return v;
 }
