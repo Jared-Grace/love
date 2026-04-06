31b82927712815fbd263d6a5469fdd1950833e9e
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:54:34 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,7 +1,6 @@
 import { json_equal } from "../../../love/public/src/json_equal.mjs";
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
-import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { equal_not_curried_right } from "../../../karate_code/public/src/equal_not_curried_right.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function sandbox() {
@@ -10,9 +9,12 @@ export async function sandbox() {
   let properties = properties_get(result);
   let eq2 = json_equal(properties, [s]);
   if (eq2) {
-    await function_delete(s);
+    return await function_delete(s);
   } else {
-    log_keep(sandbox.name, "Used in multiple places. Not deleting.");
-    return properties;
+    let r = {
+      message: "Used in multiple places. Not deleting.",
+      properties,
+    };
+    return r;
   }
 }
