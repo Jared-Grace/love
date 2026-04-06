16c0aab8b5d58dfd72aca6d0f0bc873a8b5484d0
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 16:49:46 -0400

    function_auto_path ../love/public/src/app_g_game_save.mjs

--- public/src/app_g_game_save.mjs
+++ public/src/app_g_game_save.mjs
@@ -1,7 +1,9 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
 import { file_name_json_folder_gitignore } from "../../../love/public/src/file_name_json_folder_gitignore.mjs";
 import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 export function app_g_game_save(g) {
   let f_path = file_name_json_folder_gitignore("1");
+  let result = path_join(segments);
   storage_local_set(app_g, "game", f_path);
 }
