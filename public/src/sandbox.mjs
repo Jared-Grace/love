22859937aa724449c0a39fca89565f7929f2aefa
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:42:55 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,12 +1,9 @@
+import { property_equals_not } from "../../../karate_code/public/src/property_equals_not.mjs";
 import { function_curryify_right_count } from "../../../love/public/src/function_curryify_right_count.mjs";
-import { list_random_item_invoke_count } from "../../../love/public/src/list_random_item_invoke_count.mjs";
 import { function_curryify_specify } from "../../../love/public/src/function_curryify_specify.mjs";
 import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
 export async function sandbox() {
   await function_delete_if_exists("property_equals_not_curried_right_2");
-  let output = await function_curryify_right_count(f_name, count);
-  let r = await function_curryify_specify(
-    list_random_item_invoke_count.name,
-    "1,3",
-  );
+  let output = await function_curryify_right_count(f_name, 2);
+  let r = await function_curryify_specify(property_equals_not.name, "1,3");
 }
