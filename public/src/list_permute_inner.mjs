b6a17196df6ba94a02dae30dae9a50fb7c8ad442
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 19:12:51 -0400

    function_auto_path ../love/public/src/list_permute_inner.mjs

--- public/src/list_permute_inner.mjs
+++ public/src/list_permute_inner.mjs
@@ -3,10 +3,8 @@ import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_copy } from "../../../love/public/src/list_copy.mjs";
 import { list_pop } from "../../../love/public/src/list_pop.mjs";
-import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 export function list_permute_inner(list, index, fns, result, candidate) {
   let size = list_size(list);
   let g = greater_than_or_equal(index, size);
@@ -16,11 +14,8 @@ export function list_permute_inner(list, index, fns, result, candidate) {
     return;
   }
   let item = list_get(list, index);
-  let r = list_first_remaining(list);
-  let first = property_get(r, "first");
-  let remaining = property_get(r, "remaining");
   function lambda(fn) {
-    let v = fn(first);
+    let v = fn(item);
     list_add(candidate, v);
     list_permute_inner(remaining, index + 1, fns, result, candidate);
     list_pop(candidate);
