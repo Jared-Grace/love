9f17839f01e9cf9b4ea4df32946570c95cfc02bc
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 16:54:27 -0400

    function_auto_path ../love/public/src/app_g_game_save.mjs

--- public/src/app_g_game_save.mjs
+++ public/src/app_g_game_save.mjs
@@ -4,7 +4,7 @@ import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_set } from "../../../love/public/src/storage_local_set.mjs";
 export async function app_g_game_save(g) {
   storage_local_set(app_g, "game", g);
-  let joined = app_g_game_save_path();
-  await file_overwrite(joined, contents);
-  return joined;
+  let p = app_g_game_save_path();
+  await file_overwrite(p, contents);
+  return p;
 }
