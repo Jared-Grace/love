206e5346c1c1ed394368d809000d3704a3bbcf54
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:51:55 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,4 @@
-import { list_skip } from "../../../love/public/src/list_skip.mjs";
+import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { properties_size_1 } from "../../../karate_code/public/src/properties_size_1.mjs";
 import { equal_not_curried_right } from "../../../karate_code/public/src/equal_not_curried_right.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
@@ -7,7 +7,7 @@ export async function sandbox() {
   let result = await data_identifiers_search(s);
   let s1 = properties_size_1(result);
   if (s1) {
-    let skipped = list_skip(list, skip_count);
+    log_keep(sandbox.name, message);
   } else {
   }
   return s1;
