6cdf5d0eb9adefddc58f89d70930a4f9119b97e4
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 13:56:19 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,7 +1,1 @@
-import { function_delete_unused } from "../../../karate_code/public/src/function_delete_unused.mjs";
-import { equal_not_curried_right } from "../../../karate_code/public/src/equal_not_curried_right.mjs";
-export async function sandbox() {
-  let s = equal_not_curried_right.name;
-  let result = await function_delete_unused(s);
-  return result;
-}
+export async function sandbox() {}
