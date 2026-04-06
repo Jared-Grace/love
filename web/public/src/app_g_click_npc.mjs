e1a2a335f3788665ccaadf6491c1beb82dd6406b
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 18:04:48 -0400

    pd app_g_click_npc game_prefix

--- web/public/src/app_g_click_npc.mjs
+++ web/public/src/app_g_click_npc.mjs
@@ -13,12 +13,7 @@ import { html_remove } from "../../../love/public/src/html_remove.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_overlay } from "../../../love/public/src/app_g_overlay.mjs";
-export async function app_g_click_npc(
-  div_map,
-  npcs_matched,
-  game_prefix,
-  refresh,
-) {
+export async function app_g_click_npc(div_map, npcs_matched, refresh) {
   let g = await app_g_game_save_get();
   let player = property_get(g, "player");
   let overlay = app_g_overlay(div_map);
