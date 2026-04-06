4d86c025201492f174a0d896be4de827bfebf3d4
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 19:12:08 -0400

    function_auto_path ../love/public/src/list_permute.mjs

--- web/public/src/list_permute.mjs
+++ web/public/src/list_permute.mjs
@@ -2,6 +2,7 @@ import { list_permute_inner } from "../../../love/public/src/list_permute_inner.
 export function list_permute(list, fns) {
   let result = [];
   let candidate = [];
+  let index = 0;
   list_permute_inner(list, index, fns, result, candidate);
   return result;
 }
