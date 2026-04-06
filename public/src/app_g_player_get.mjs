8c6c4ef2e5c8653825fd09445f77aba9dbf56fcf
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 17:03:02 -0400

    function_auto_path ../love/public/src/app_g_player_get.mjs

--- public/src/app_g_player_get.mjs
+++ public/src/app_g_player_get.mjs
@@ -1,7 +1,9 @@
+import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g } from "../../../love/public/src/app_g.mjs";
 import { storage_local_get } from "../../../love/public/src/storage_local_get.mjs";
-export function app_g_player_get() {
+export async function app_g_player_get() {
+  let data = await file_read_json(file_path);
   let game = storage_local_get(app_g, "game");
   let player = property_get(game, "player");
   return player;
