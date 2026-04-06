84268ccd1d2b179350b9eedcef3e53e2e5a9b652
JESUS rose to life <username@users.noreply.github.com>
2026-04-05 20:02:29 -0400

    function_auto_path ../love/public/src/app_g_map_generate.mjs

--- web/public/src/app_g_map_generate.mjs
+++ web/public/src/app_g_map_generate.mjs
@@ -1,3 +1,4 @@
+import { list_set_nested } from "../../../karate_code/public/src/list_set_nested.mjs";
 import { app_g_map_generate_waters_next } from "../../../love/public/src/app_g_map_generate_waters_next.mjs";
 import { list_random_item_count_nested } from "../../../love/public/src/list_random_item_count_nested.mjs";
 import { g_tiles_grasses_choices_weighted } from "../../../love/public/src/g_tiles_grasses_choices_weighted.mjs";
@@ -7,8 +8,6 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { g_coordinates } from "../../../love/public/src/g_coordinates.mjs";
-import { list_set } from "../../../love/public/src/list_set.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
 export function app_g_map_generate() {
   let tiles_choices = g_tiles_grasses_choices_weighted();
@@ -28,8 +27,7 @@ export function app_g_map_generate() {
     let x = property_get(r, "x");
     let y = property_get(r, "y");
     let value = app_a_water();
-    let water_row = list_get(rows, y);
-    list_set(water_row, x, value);
+    list_set_nested(rows, y, x, value);
     list_remove(coordinates, r);
     list_add(waters, r);
   }
