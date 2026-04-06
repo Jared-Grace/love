5fb914dda70b385314e5060fff986b89ba01545d
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 17:34:25 -0400

    pd app_g_tutorial_study player

--- public/src/app_g_tutorial_study.mjs
+++ public/src/app_g_tutorial_study.mjs
@@ -1,6 +1,6 @@
 import { app_g_tutorial } from "../../../love/public/src/app_g_tutorial.mjs";
 import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
-export async function app_g_tutorial_study(player, div_map) {
+export async function app_g_tutorial_study(div_map) {
   let text = emoji_book_open();
   const player_property = "studied";
   const tutorial_property = "tutorial_study";
