fd49d9806b045353c84debe349f59a1114d706ce
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 16:36:24 -0400

    function_auto_path ../love/public/src/indexeddb_exists.mjs

--- public/src/indexeddb_exists.mjs
+++ public/src/indexeddb_exists.mjs
@@ -6,8 +6,8 @@ export async function indexeddb_exists(db_get, store, key) {
   const property = "key";
   let all = await indexeddb_get_all(db_get, store);
   list_property_exists_not_error(all, property);
-  let s1 = list_find_property_exists(all, property, key);
-  return s1;
+  let e = list_find_property_exists(all, property, key);
+  return e;
   let exists = await indexeddb_exists_backend(db_get, store, key);
   return exists;
 }
