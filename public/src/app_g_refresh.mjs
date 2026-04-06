350d3ef56003182406a894bcd67202e85149a18d
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 17:30:29 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -1,3 +1,4 @@
+import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { html_on_load_wait } from "../../../love/public/src/html_on_load_wait.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
@@ -6,7 +7,6 @@ import { g_icon_cross } from "../../../love/public/src/g_icon_cross.mjs";
 import { html_style_head } from "../../../love/public/src/html_style_head.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_scroll_center_container_now } from "../../../love/public/src/html_scroll_center_container_now.mjs";
-import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
 import { html_data_set_json } from "../../../love/public/src/html_data_set_json.mjs";
 import { html_class_add } from "../../../love/public/src/html_class_add.mjs";
@@ -36,7 +36,8 @@ export async function app_g_refresh(
     await app_g_refresh(div_map_container, game_prefix, tiles_path, rows);
   }
   let npcs = property_get(map, "npcs");
-  let player = await app_g_player_get();
+  let g = await app_g_game_save_get();
+  let player = property_get(g, "player");
   let player_img_c = g_character_img(game_prefix, div_map, player);
   const style_text = `@keyframes pulseGlow {
   0%,100% { 
