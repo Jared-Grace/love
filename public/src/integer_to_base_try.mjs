596fc2a19948e09643f563d1d139a5737d416ebe
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:16:09 -0400

    function_auto_path ../love/public/src/integer_to_try.mjs

---
+++ public/src/integer_to_base_try.mjs
@@ -0,0 +1,8 @@
+import { number_nan_is } from "../../../love/public/src/number_nan_is.mjs";
+export function integer_to_base_try(input, base) {
+  let i = parseInt(input, base);
+  if (number_nan_is(i)) {
+    i = null;
+  }
+  return i;
+}
