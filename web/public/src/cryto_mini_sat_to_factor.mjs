2166230bc556b016b2642ceb230f455a27737791
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:55:08 -0400

    function_auto_path ../love/public/src/cryto_mini_sat_to_factor.mjs

--- web/public/src/cryto_mini_sat_to_factor.mjs
+++ web/public/src/cryto_mini_sat_to_factor.mjs
@@ -4,8 +4,8 @@ import { boolean_to_binary_text } from "../../../love/public/src/boolean_to_bina
 import { positive_is } from "../../../love/public/src/positive_is.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_remove_last_multiple } from "../../../love/public/src/list_remove_last_multiple.mjs";
-export function cryto_mini_sat_to_factor(filtered, bits) {
-  let e = list_remove_last_multiple(filtered, bits);
+export function cryto_mini_sat_to_factor(reversed, bits) {
+  let e = list_remove_last_multiple(reversed, bits);
   let mapped2 = list_map(e, positive_is);
   let mapped3 = list_map(mapped2, boolean_to_binary_text);
   let joined2 = list_join_empty(mapped3);
