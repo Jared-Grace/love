b1198e6ddcce704ca93f3b9352c5a0f00cf21f3a
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:12:50 -0400

    pd list_remove_first_multiple index

--- public/src/list_remove_first_multiple.mjs
+++ public/src/list_remove_first_multiple.mjs
@@ -1,5 +1,5 @@
 import { list_remove_at_count } from "../../../love/public/src/list_remove_at_count.mjs";
-export function list_remove_first_multiple(list, index, count) {
+export function list_remove_first_multiple(list, count) {
   let e = list_remove_at_count(list, 0, count);
   return e;
 }
