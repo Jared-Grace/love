ca788d04907182c26c11db8c610361b74d8c8d58
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:00:22 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

--- public/src/cryto_mini_sat.mjs
+++ public/src/cryto_mini_sat.mjs
@@ -1,3 +1,4 @@
+import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
 import { property_set_new_fn } from "../../../love/public/src/property_set_new_fn.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { command_line_generic_code_ignore } from "../../../love/public/src/command_line_generic_code_ignore.mjs";
@@ -12,5 +13,6 @@ sudo apt install cryptominisat`;
     "cryptominisat5 /media/j/JPM/user/temp/3addf5dd-c638-4b30-b164-d47670db6f54";
   let r = await command_line_generic(command, object);
   let stdout = property_get(r, "stdout");
+  let filtered = list_filter_starts_with(list, prefix);
   return stdout;
 }
