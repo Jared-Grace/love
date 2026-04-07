172839fe92945ac7874716f18c7eb9fbde825845
JESUS rose to life <username@users.noreply.github.com>
2026-04-06 22:41:05 -0400

    function_auto_path ../love/public/src/text_integer_base_2_to.mjs

--- public/src/text_integer_base_2_to.mjs
+++ public/src/text_integer_base_2_to.mjs
@@ -1,4 +1,5 @@
 import { integer_base_2_to } from "../../../love/public/src/integer_base_2_to.mjs";
 export function text_integer_base_2_to(i) {
-  return integer_base_2_to(i);
+  let r = integer_base_2_to(i);
+  return r;
 }
