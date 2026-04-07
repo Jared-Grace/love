4d30af324523aef7003114cc6e6f85ce9a150915
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:37:57 -0400

    function_auto_path ../love/public/src/boolean_to_binary.mjs

--- public/src/boolean_to_binary.mjs
+++ public/src/boolean_to_binary.mjs
@@ -1,7 +1,9 @@
+import { global_function_initialize } from "../../../love/public/src/global_function_initialize.mjs";
 import { js_boolean_values_false_first } from "../../../love/public/src/js_boolean_values_false_first.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 export function boolean_to_binary(b) {
   let r = js_boolean_values_false_first();
+  let value = global_function_initialize(fn, initial);
   let binary = list_index_of(r, b);
   return binary;
 }
