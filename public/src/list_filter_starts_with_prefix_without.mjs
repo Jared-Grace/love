e27a4c2cfbd339f31e33775c0ce730ae6a061dae
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:22:18 -0400

    function_auto_path ../love/public/src/list_filter_starts_with_prefix_without.mjs

--- public/src/list_filter_starts_with_prefix_without.mjs
+++ public/src/list_filter_starts_with_prefix_without.mjs
@@ -1,10 +1,9 @@
 import { text_prefix_without_curried_right } from "../../../love/public/src/text_prefix_without_curried_right.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_filter_starts_with } from "../../../love/public/src/list_filter_starts_with.mjs";
-import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 export function list_filter_starts_with_prefix_without(lines, prefix) {
   let filtered = list_filter_starts_with(lines, prefix);
-  let r = text_prefix_without_curried_right(prefix2);
-  let without = list_map(filtered, text_prefix_without);
+  let r = text_prefix_without_curried_right(prefix);
+  let without = list_map(filtered, r);
   return without;
 }
