dce3c724818fe168148435b0d95b0cd9707aee3f
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:07:45 -0400

    crl chalk_red white

---
+++ public/src/chalk_white.mjs
@@ -0,0 +1,6 @@
+import { chalk_color } from "../../../love/public/src/chalk_color.mjs";
+export async function chalk_white(prompt) {
+  const color = "red";
+  let colored = await chalk_color(color, prompt);
+  return colored;
+}
