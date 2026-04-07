66e8efcfd71543d20bcd4fa09b6ec1079e1adfb1
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:45:04 -0400

    function_auto_path ../love/public/src/list_remove_last_multiple.mjs

--- web/public/src/list_remove_last_multiple.mjs
+++ web/public/src/list_remove_last_multiple.mjs
@@ -1,5 +1,7 @@
+import { list_index_last } from "../../../love/public/src/list_index_last.mjs";
 import { list_remove_at_count } from "../../../love/public/src/list_remove_at_count.mjs";
 export function list_remove_last_multiple(list, count) {
+  let index_last = list_index_last(list2);
   let e = list_remove_at_count(list, 0, count);
   return e;
 }
