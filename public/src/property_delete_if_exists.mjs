3027c4282781f91e5f40248126d8c368d2f271bb
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:07:25 -0400

    function_auto_path ../love/public/src/property_delete_if_exists.mjs

--- public/src/property_delete_if_exists.mjs
+++ public/src/property_delete_if_exists.mjs
@@ -8,5 +8,9 @@ export function property_delete_if_exists(object, property_name) {
     value = property_get(object, property_name);
     property_delete(object, property_name);
   }
-  return exists;
+  let r = {
+    exists,
+    value,
+  };
+  return r;
 }
