d0bc54428c3cf00ea1efd891a4536a5393620f15
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:15:54 -0400

    function_auto_path ../love/public/src/integer_to_try.mjs

--- public/src/integer_to_try.mjs
+++ public/src/integer_to_try.mjs
@@ -1,8 +1,7 @@
 import { number_nan_is } from "../../../love/public/src/number_nan_is.mjs";
 export function integer_to_try(input) {
   const base = 10;
-  let i = null;
-  i = parseInt(input, base);
+  let i = parseInt(input, base);
   if (number_nan_is(i)) {
     i = null;
   }
