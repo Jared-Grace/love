b8f0800d42ac988da7951d6f27a3c442d05a2052
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:04:54 -0400

    function_auto_path ../love/public/src/property_delete_if_exists_fn.mjs

--- public/src/property_delete_if_exists_fn.mjs
+++ public/src/property_delete_if_exists_fn.mjs
@@ -1,6 +1,6 @@
 import { property_delete_if_exists } from "../../../love/public/src/property_delete_if_exists.mjs";
-export function property_delete_if_exists_fn(extra, fn) {
-  let combined = fn();
-  let code_ignore = property_delete_if_exists(extra, combined);
+export function property_delete_if_exists_fn(object, fn_property) {
+  let combined = fn_property();
+  let code_ignore = property_delete_if_exists(object, combined);
   return code_ignore;
 }
