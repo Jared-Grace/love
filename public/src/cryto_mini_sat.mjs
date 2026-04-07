6705818064d4e37febdce24afa29e13ff1d98b84
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:23:28 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

--- public/src/cryto_mini_sat.mjs
+++ public/src/cryto_mini_sat.mjs
@@ -17,7 +17,7 @@ sudo apt install cryptominisat`;
   let stdout = property_get(r, "stdout");
   let lines = text_split_newline(stdout);
   let without = list_find_starts_with_prefix_without(lines, "s ");
-  aeq;
+  eqa;
   let without2 = list_filter_starts_with_prefix_without(lines, "v ");
   return without;
 }
