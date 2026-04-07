96695d2dd27ad9aaa825dfc9826ae29b044ae64b
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:51:51 -0400

    rr integer_to_base_try to from

--- public/src/integer_to_try.mjs
+++ public/src/integer_to_try.mjs
@@ -1,5 +1,5 @@
-import { integer_to_base_try } from "../../../love/public/src/integer_to_base_try.mjs";
+import { integer_from_base_try } from "../../../love/public/src/integer_from_base_try.mjs";
 export function integer_to_try(input) {
-  let i = integer_to_base_try(input, 10);
+  let i = integer_from_base_try(input, 10);
   return i;
 }
