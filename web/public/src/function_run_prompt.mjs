ffe659f4d0af6f63cba7e7221d4d4a849e29752f
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:52:54 -0400

    function_auto_path ../love/public/src/function_run_prompt.mjs

--- web/public/src/function_run_prompt.mjs
+++ web/public/src/function_run_prompt.mjs
@@ -19,6 +19,7 @@ export async function function_run_prompt() {
     }
   }
   let prompt_colored = await chalk_green("✟ ");
+  let safe = ["***REMOVED***"];
   let line = await command_line_read(prompt_colored);
   await function_run_line_git(line);
 }
