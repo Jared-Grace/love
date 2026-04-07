6c839551ef72c7a552be5ce4a47e450afa65d9c8
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:09:51 -0400

    function_auto_path ../love/public/src/command_line_code_ignore.mjs

--- web/public/src/command_line_code_ignore.mjs
+++ web/public/src/command_line_code_ignore.mjs
@@ -3,6 +3,7 @@ import { property_set_new_fn } from "../../../love/public/src/property_set_new_f
 import { command_line_generic_code_ignore } from "../../../love/public/src/command_line_generic_code_ignore.mjs";
 export async function command_line_code_ignore(command) {
   let fn = command_line_generic_code_ignore;
+  let value = true;
   let object = property_set_new_fn(fn, value);
   let r = await command_line_generic(command, object);
   return r;
