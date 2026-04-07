3a94fac8b4b672c3186a30bdb8c18a4c710500a4
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:57:06 -0400

    function_auto_path ../love/public/src/function_run_prompt.mjs

--- public/src/function_run_prompt.mjs
+++ public/src/function_run_prompt.mjs
@@ -1,8 +1,6 @@
+import { function_run_prompt_repo_name_colored } from "../../../love/public/src/function_run_prompt_repo_name_colored.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
-import { chalk_color } from "../../../love/public/src/chalk_color.mjs";
-import { chalk_red } from "../../../love/public/src/chalk_red.mjs";
 import { user_repo_get } from "../../../love/public/src/user_repo_get.mjs";
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { log_error } from "../../../love/public/src/log_error.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { data_property_get_generic } from "../../../love/public/src/data_property_get_generic.mjs";
@@ -11,7 +9,6 @@ import { function_run_line_git } from "../../../love/public/src/function_run_lin
 import { command_line_read } from "../../../love/public/src/command_line_read.mjs";
 import { json_format_to_truncated } from "../../../love/public/src/json_format_to_truncated.mjs";
 import { data_path } from "../../../love/public/src/data_path.mjs";
-import { ternary } from "./ternary.mjs";
 export async function function_run_prompt() {
   if (false) {
     const property_name = "error_attention";
@@ -26,10 +23,7 @@ export async function function_run_prompt() {
   }
   let repo_name = await user_repo_get();
   let prompt_colored = await chalk_green("✟ ");
-  let safe = ["p_np"];
-  let includes = list_includes(safe, repo_name);
-  let chalk_color = ternary(includes, chalk_green, chalk_red);
-  let colored = await chalk_color(repo_name);
+  let colored = await function_run_prompt_repo_name_colored(repo_name);
   let combined = text_combine_multiple([prompt_colored, colored, " "]);
   let line = await command_line_read(combined);
   await function_run_line_git(line);
