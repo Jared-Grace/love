daa3f4bd5c4a0cde9f30ad7f0e5ad952d531be45
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:41:07 -0400

    function_auto_path ../love/public/src/boolean_to_binary_text.mjs

--- web/public/src/boolean_to_binary_text.mjs
+++ web/public/src/boolean_to_binary_text.mjs
@@ -1,6 +1,7 @@
+import { text_to } from "../../../love/public/src/text_to.mjs";
 import { boolean_to_binary } from "../../../love/public/src/boolean_to_binary.mjs";
 export function boolean_to_binary_text(b) {
   let binary = boolean_to_binary(b);
-  tt;
+  let s = text_to(input);
   return binary;
 }
