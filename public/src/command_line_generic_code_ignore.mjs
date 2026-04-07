59cbec379ab36bc020fb55d71cb91c941adb22ae
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:03:33 -0400

    function_auto_path ../love/public/src/command_line_generic.mjs

---
+++ public/src/command_line_generic_code_ignore.mjs
@@ -0,0 +1,9 @@
+import { command_line_generic } from "../../../love/public/src/command_line_generic.mjs";
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+export function command_line_generic_code_ignore() {
+  let combined2 = function_name_combine(
+    command_line_generic.name,
+    "code_ignore",
+  );
+  return combined2;
+}
