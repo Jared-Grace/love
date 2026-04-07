b5700446059ed346dd3c1c12055f624e54072619
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:51:41 -0400

    pb repo_functions_move_acronym

--- public/src/repo_function_move_acronym.mjs
+++ public/src/repo_function_move_acronym.mjs
@@ -1,5 +1,5 @@
 import { repo_functions_move_acronym } from "../../../love/public/src/repo_functions_move_acronym.mjs";
 export async function repo_function_move_acronym(f_name, repo_name_to) {
-  let r = await repo_functions_move_acronym([f_name], repo_name_to);
+  let r = await repo_functions_move_acronym(repo_name_to, [f_name]);
   return r;
 }
--- public/src/repo_functions_move_if_includes_not.mjs
+++ public/src/repo_functions_move_if_includes_not.mjs
@@ -2,5 +2,5 @@ import { repo_functions_move_acronym } from "../../../love/public/src/repo_funct
 import { repo_functions_move_if_includes_not_filtered } from "../../../love/public/src/repo_functions_move_if_includes_not_filtered.mjs";
 export async function repo_functions_move_if_includes_not(to, query) {
   let filtered = await repo_functions_move_if_includes_not_filtered(query);
-  await repo_functions_move_acronym(to, filtered);
+  await repo_functions_move_acronym(filtered, to);
 }
