1c0fb030bf1dc1862d447e8f4ee834d465e097ee
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:12:09 -0400

    function_auto_path ../love/public/src/function_run_prompt_repo_name_colored.mjs

--- public/src/function_run_prompt_repo_name_colored.mjs
+++ public/src/function_run_prompt_repo_name_colored.mjs
@@ -2,6 +2,7 @@ import { chalk_green_or_red_bg } from "../../../love/public/src/chalk_green_or_r
 import { chalk_color } from "../../../love/public/src/chalk_color.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export async function function_run_prompt_repo_name_colored(repo_name) {
+  "this will make repos not in safe stand out in prompt visually";
   let safe = ["p_np"];
   let includes = list_includes(safe, repo_name);
   let chalk_color = chalk_green_or_red_bg(includes);
