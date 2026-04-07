86f987a1a805ceebdf6c6ce6f422bf95df0ef629
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:07:11 -0400

    function_auto_path ../love/public/src/chalk_red_bg.mjs

--- web/public/src/chalk_red_bg.mjs
+++ web/public/src/chalk_red_bg.mjs
@@ -1,6 +1,6 @@
 import { chalk_color } from "../../../love/public/src/chalk_color.mjs";
 export async function chalk_red_bg(prompt) {
-  const color = "red";
+  const color = "bgRed";
   let colored = await chalk_color(color, prompt);
   return colored;
 }
