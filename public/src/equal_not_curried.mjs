ec987461909089750d29b0002e12bc3bd80c114d
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:42:57 -0400

    cu equal_not

--- public/src/equal_not_curried.mjs
+++ public/src/equal_not_curried.mjs
@@ -1,1 +1,6 @@
-export function equal_not_curried() {}
+import { equal_not } from "../../../love/public/src/equal_not.mjs";
+export function equal_not_curried(left) {
+  return function equal_not_curried_result(right) {
+    return equal_not(left, right);
+  };
+}
