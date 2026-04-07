5ae458ace10989884ce157abc29a98feda7910b1
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:21:59 -0400

    cur text_prefix_without

--- public/src/text_prefix_without_curried_right.mjs
+++ public/src/text_prefix_without_curried_right.mjs
@@ -1,1 +1,6 @@
-export function text_prefix_without_curried_right() {}
+import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
+export function text_prefix_without_curried_right(prefix) {
+  return function text_prefix_without_curried_right_result(t) {
+    return text_prefix_without(t, prefix);
+  };
+}
