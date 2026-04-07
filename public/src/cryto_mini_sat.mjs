afb2397c67dbf362d559dcce1bb292474f374f6c
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:46:57 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

--- public/src/cryto_mini_sat.mjs
+++ public/src/cryto_mini_sat.mjs
@@ -1,3 +1,4 @@
+import { text_remove_while_starts_with } from "../../../love/public/src/text_remove_while_starts_with.mjs";
 import { list_remove_last_multiple } from "../../../love/public/src/list_remove_last_multiple.mjs";
 import { list_sort_number_abs_reverse } from "../../../love/public/src/list_sort_number_abs_reverse.mjs";
 import { boolean_to_binary_text } from "../../../love/public/src/boolean_to_binary_text.mjs";
@@ -36,6 +37,7 @@ sudo apt install cryptominisat`;
   let mapped3 = list_map(mapped2, boolean_to_binary_text);
   let e = list_remove_last_multiple(mapped3, bits);
   let e2 = list_remove_last_multiple(mapped3, bits);
+  let result = text_remove_while_starts_with(item, prefix);
   let r = {
     e,
     e2,
