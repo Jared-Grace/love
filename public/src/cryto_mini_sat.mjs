f99dd39c4e3058351e2038627e5d6437297f140b
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:35:43 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

--- public/src/cryto_mini_sat.mjs
+++ public/src/cryto_mini_sat.mjs
@@ -1,3 +1,4 @@
+import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
@@ -25,7 +26,6 @@ sudo apt install cryptominisat`;
   let without2 = list_filter_starts_with_prefix_without(lines, "v ");
   let joined = list_join_space(without2);
   let split = text_split_space(joined);
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
+  let mapped = list_map(list, integer_to_try);
   return split;
 }
