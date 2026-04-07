c0f2496901dbca90bc1b4bf1995313c2431cfea2
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:27:09 -0400

    function_auto_path ../love/public/src/integer_base_2_to_try.mjs

--- public/src/integer_base_2_to_try.mjs
+++ public/src/integer_base_2_to_try.mjs
@@ -1,5 +1,5 @@
 import { integer_to_base_try } from "../../../love/public/src/integer_to_base_try.mjs";
 export function integer_base_2_to_try(input) {
-  let i = integer_to_base_try(input, 10);
+  let i = integer_to_base_try(input, 2);
   return i;
 }
