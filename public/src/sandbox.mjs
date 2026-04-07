2707db32f3cd48cef2e4dff7ae24a337051ef166
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 15:50:55 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,5 +1,7 @@
 import { repo_function_move_acronym } from "../../../love/public/src/repo_function_move_acronym.mjs";
 export async function sandbox() {
-  let v = await repo_function_move_acronym(f_names, repo_name_to);
-  ("emoji_no", "text_alphabet_includes", "l");
+  let v = await repo_function_move_acronym(
+    ["emoji_no", "text_alphabet_includes"],
+    "l",
+  );
 }
