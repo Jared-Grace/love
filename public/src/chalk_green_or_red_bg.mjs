aa2ddb7f073a584eb2f8ffb71abf14568335efc2
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:08:52 -0400

    function_auto_path ../love/public/src/chalk_green_or_red_bg.mjs

--- public/src/chalk_green_or_red_bg.mjs
+++ public/src/chalk_green_or_red_bg.mjs
@@ -1,7 +1,7 @@
 import { chalk_white_red_bg } from "../../../love/public/src/chalk_white_red_bg.mjs";
 import { chalk_green } from "../../../love/public/src/chalk_green.mjs";
 import { ternary } from "../../../love/public/src/ternary.mjs";
-export function chalk_green_or_red(includes) {
+export function chalk_green_or_red_bg(includes) {
   let result = ternary(includes, chalk_green, chalk_white_red_bg);
   return result;
 }
