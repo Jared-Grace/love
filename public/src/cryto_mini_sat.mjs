0a45c64983a9ac9a9c50f41b8a601094173a1acf
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 14:25:47 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

--- public/src/cryto_mini_sat.mjs
+++ public/src/cryto_mini_sat.mjs
@@ -2,5 +2,7 @@ import { command_line } from "../../../love/public/src/command_line.mjs";
 export async function cryto_mini_sat() {
   `sudo apt update
 sudo apt install cryptominisat`;
-  let stdout = await command_line("cryptominisat5 ");
+  let stdout = await command_line(
+    "cryptominisat5 /media/j/JPM/user/temp/3addf5dd-c638-4b30-b164-d47670db6f54",
+  );
 }
