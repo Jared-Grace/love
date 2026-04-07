e5fe0c95d4b757a7f1306c616227ae0e774108ea
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:08:12 -0400

    fws chalk_white red_bg

--- public/src/chalk_white_red_bg.mjs
+++ public/src/chalk_white_red_bg.mjs
@@ -1,1 +1,4 @@
-export function chalk_white_red_bg() {}
+import { chalk_white } from "../../../love/public/src/chalk_white.mjs";
+export async function chalk_white_red_bg(prompt) {
+  return await chalk_white(prompt);
+}
