1f7247e620f8e2d21754f6f03cf361f0b0d5452f
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:51:51 -0400

    rr integer_to_base_try to from

--- web/public/src/integer_from_base_try.mjs
+++ web/public/src/integer_from_base_try.mjs
@@ -1,5 +1,5 @@
 import { number_nan_is } from "../../../love/public/src/number_nan_is.mjs";
-export function integer_to_base_try(input, base) {
+export function integer_from_base_try(input, base) {
   let i = parseInt(input, base);
   if (number_nan_is(i)) {
     i = null;
