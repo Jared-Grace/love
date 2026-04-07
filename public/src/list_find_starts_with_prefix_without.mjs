7d3e39b23ea451e589f75827d4de48d0ae178367
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:19:10 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

---
+++ public/src/list_find_starts_with_prefix_without.mjs
@@ -0,0 +1,7 @@
+import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
+import { list_find_starts_with } from "../../../love/public/src/list_find_starts_with.mjs";
+export function list_find_starts_with_prefix_without(lines, combined) {
+  let found = list_find_starts_with(lines, combined);
+  let without = text_prefix_without(found, combined);
+  return without;
+}
