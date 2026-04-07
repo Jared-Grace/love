f78ffe8fb074738372b2f3d540166a812c44cc67
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 14:24:52 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

--- public/src/cryto_mini_sat.mjs
+++ public/src/cryto_mini_sat.mjs
@@ -1,1 +1,4 @@
-export function cryto_mini_sat() {}
+import { command_line } from "../../../love/public/src/command_line.mjs";
+export async function cryto_mini_sat() {
+  let stdout = await command_line(command);
+}
