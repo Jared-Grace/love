9bdf355129131291331337061c664cac9e247b67
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:43:57 -0400

    function_auto_path ../love/public/src/cryto_mini_sat.mjs

---
+++ public/src/list_filter_equal_not.mjs
@@ -0,0 +1,7 @@
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
+import { equal_not_curried } from "../../../love/public/src/equal_not_curried.mjs";
+export function list_filter_equal_not(en, mapped) {
+  let r3 = equal_not_curried(en);
+  let filtered = list_filter(mapped, r3);
+  return filtered;
+}
