47c979273d318c4dd76023be3e1b3289b227b0e4
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:22:01 -0400

    function_auto_path ../love/public/src/text_prefix_without_curried_right.mjs

--- public/src/text_prefix_without_curried_right.mjs
+++ public/src/text_prefix_without_curried_right.mjs
@@ -1,6 +1,8 @@
 import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 export function text_prefix_without_curried_right(prefix) {
-  return function text_prefix_without_curried_right_result(t) {
-    return text_prefix_without(t, prefix);
+  let r = function text_prefix_without_curried_right_result(t) {
+    let without = text_prefix_without(t, prefix);
+    return without;
   };
+  return r;
 }
