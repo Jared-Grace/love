6113c4696cdbd53fb95dd91cb0a5805cc7b6fb2b
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 16:42:59 -0400

    function_auto_path ../love/public/src/equal_not_curried.mjs

--- public/src/equal_not_curried.mjs
+++ public/src/equal_not_curried.mjs
@@ -1,6 +1,8 @@
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 export function equal_not_curried(left) {
-  return function equal_not_curried_result(right) {
-    return equal_not(left, right);
+  let r = function equal_not_curried_result(right) {
+    let ne = equal_not(left, right);
+    return ne;
   };
+  return r;
 }
