7601cd3deb0b539495c4e0ba33c3c094627fa137
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 18:51:38 -0400

    function_auto_path ../love/public/src/integer_base_2_from_try.mjs

--- web/public/src/integer_base_2_from.mjs
+++ web/public/src/integer_base_2_from.mjs
@@ -1,7 +1,7 @@
-import { integer_base_2_to_try } from "../../../love/public/src/integer_base_2_to_try.mjs";
+import { integer_base_2_from_try } from "../../../love/public/src/integer_base_2_from_try.mjs";
 import { null_not_is_assert } from "../../../love/public/src/null_not_is_assert.mjs";
 export function integer_base_2_from(integer_text) {
-  let i = integer_base_2_to_try(integer_text);
+  let i = integer_base_2_from_try(integer_text);
   null_not_is_assert(i);
   return i;
 }
