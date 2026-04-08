4bbc084b40cd5f791f336850b145a66da6dceb8f
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:56:58 -0400

    function_auto_path ../love/public/src/function_wrap_generic.mjs

--- public/src/function_wrap_generic.mjs
+++ public/src/function_wrap_generic.mjs
@@ -1,9 +1,11 @@
+import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_read } from "../../../love/public/src/function_read.mjs";
 import { function_name_parts_remove_comma } from "../../../love/public/src/function_name_parts_remove_comma.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_generic(plugin_fn, f_name_old, deleted) {
   f_name_old = await function_name_unalias_only(f_name_old);
+  let result = await function_run(f_name3, args);
   let contents = await function_read(f_name2);
   let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
   let r = await function_wrap(f_name, f_name_wrapped);
