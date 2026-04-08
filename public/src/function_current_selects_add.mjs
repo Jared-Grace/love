f14c647db426dcc0076b5560b4ffc2358e96d54c
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:44:49 -0400

    function_auto_path ../love/public/src/function_current_selects_add.mjs

--- public/src/function_current_selects_add.mjs
+++ public/src/function_current_selects_add.mjs
@@ -1,5 +1,6 @@
-import { function_ast } from "../../../love/public/src/function_ast.mjs";
+import { exit } from "../../../love/public/src/exit.mjs";
 import { log } from "../../../love/public/src/log.mjs";
+import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { js_unparse_or_self_multiple } from "../../../love/public/src/js_unparse_or_self_multiple.mjs";
 import { js_visit_id_to_node_or_id_multiple } from "../../../love/public/src/js_visit_id_to_node_or_id_multiple.mjs";
@@ -7,8 +8,8 @@ import { data_transform } from "../../../love/public/src/data_transform.mjs";
 import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export async function function_current_selects_add(item_to_add, on_previous) {
-  let f_name_current2 = await function_current_get();
-  let ast = await function_ast(f_name_current2);
+  exit();
+  let ast = await function_current_ast();
   async function lambda(previous) {
     log(function_current_selects_add.name, {
       previous1: previous,
