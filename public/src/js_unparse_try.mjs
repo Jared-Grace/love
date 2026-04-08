513690401c15c9eec1dce80f0d882e3c8bfede3f
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:44:39 -0400

    function_auto_path ../love/public/src/js_unparse_try.mjs

--- public/src/js_unparse_try.mjs
+++ public/src/js_unparse_try.mjs
@@ -2,8 +2,9 @@ import { catch_ignore } from "../../../love/public/src/catch_ignore.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 export function js_unparse_try(ast) {
   let code = null;
-  function lambda2() {}
+  function lambda2() {
+    code = js_unparse(ast);
+  }
   catch_ignore(lambda2);
-  code = js_unparse(ast);
   return code;
 }
