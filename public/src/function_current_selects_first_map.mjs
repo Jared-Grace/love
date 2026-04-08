bb1de9a519180a83a04948273fc2b69ba960bdfa
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:24:50 -0400

    wa nc function_current_selects_first map

--- public/src/function_current_selects_first_map.mjs
+++ public/src/function_current_selects_first_map.mjs
@@ -1,1 +1,4 @@
-export function function_current_selects_first_map() {}
+import { function_current_selects_first } from "../../../love/public/src/function_current_selects_first.mjs";
+export async function function_current_selects_first_map() {
+  return await function_current_selects_first();
+}
