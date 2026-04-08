cdafa33c15703a821a0940c82f2ae34896d0638b
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:27:49 -0400

    function_auto_path ../love/public/src/function_name_to_part_last_replace.mjs

--- public/src/function_name_to_part_last_replace.mjs
+++ public/src/function_name_to_part_last_replace.mjs
@@ -1,4 +1,5 @@
 import { function_name_to_part_last } from "../../../love/public/src/function_name_to_part_last.mjs";
 export function function_name_to_part_last_replace(f_name_before) {
-  return function_name_to_part_last(f_name_before);
+  let last = function_name_to_part_last(f_name_before);
+  return last;
 }
