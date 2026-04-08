eaecb2df51e67fc6be7b285eb61de762e2b4d369
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:29:46 -0400

    function_auto_path ../love/public/src/function_wrap_part_insert.mjs

--- public/src/function_wrap_part_insert.mjs
+++ public/src/function_wrap_part_insert.mjs
@@ -4,5 +4,6 @@ export async function function_wrap_part_insert(
   index_string,
   part,
 ) {
-  return await function_wrap_part_insert_at(f_name_before, index_string, part);
+  let r = await function_wrap_part_insert_at(f_name_before, index_string, part);
+  return r;
 }
