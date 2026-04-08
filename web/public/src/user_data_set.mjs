1e74d6389b70d3859b50f9f0ee4de50980a22afb
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:38:51 -0400

    function_auto_path ../love/public/src/user_repo_set.mjs

---
+++ web/public/src/user_data_set.mjs
@@ -0,0 +1,7 @@
+import { user_data_set_fn } from "../../../love/public/src/user_data_set_fn.mjs";
+export async function user_data_set(repo_name, property_name) {
+  async function lambda(previous) {
+    return repo_name;
+  }
+  await user_data_set_fn(property_name, lambda);
+}
