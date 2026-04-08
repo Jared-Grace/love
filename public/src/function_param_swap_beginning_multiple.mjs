3856e8e04462e4f20733c67008eaebf2bb563dc0
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:38:55 -0400

    function_auto_path ../love/public/src/function_param_swap_beginning_multiple.mjs

--- public/src/function_param_swap_beginning_multiple.mjs
+++ public/src/function_param_swap_beginning_multiple.mjs
@@ -1,4 +1,5 @@
 import { function_param_swap_beginning } from "../../../love/public/src/function_param_swap_beginning.mjs";
 export async function function_param_swap_beginning_multiple(f_name) {
-  return await function_param_swap_beginning(f_name);
+  let r = await function_param_swap_beginning(f_name);
+  return r;
 }
