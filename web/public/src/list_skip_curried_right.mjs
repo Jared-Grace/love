dc6fad00dc204ec692a1b34d4f40789ed5d0b7d5
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:17:36 -0400

    cur list_skip

--- web/public/src/list_skip_curried_right.mjs
+++ web/public/src/list_skip_curried_right.mjs
@@ -1,1 +1,6 @@
-export function list_skip_curried_right() {}
+import { list_skip } from "../../../love/public/src/list_skip.mjs";
+export function list_skip_curried_right(skip_count) {
+  return function list_skip_curried_right_result(list) {
+    return list_skip(list, skip_count);
+  };
+}
