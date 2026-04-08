3bc6a335ca096f5653f24daff7f0bdda8a7b93d6
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:41:06 -0400

    function_auto_path ../love/public/src/list_remove.mjs

--- public/src/list_remove.mjs
+++ public/src/list_remove.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_remove_at } from "../../../love/public/src/list_remove_at.mjs";
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 export function list_remove(list, item) {
+  log(list_remove.name, {
+    list,
+  });
   const index = list_index_of(list, item);
   list_remove_at(list, index);
 }
