332b5760e52da8979c7158145be8101594ae3d9a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:52:41 -0400

    function_auto_path ../love/public/src/js_unparse_or_self.mjs

--- web/public/src/js_unparse_or_self.mjs
+++ web/public/src/js_unparse_or_self.mjs
@@ -2,7 +2,7 @@ import { null_is } from "../../../love/public/src/null_is.mjs";
 import { js_unparse_try } from "../../../love/public/src/js_unparse_try.mjs";
 export function js_unparse_or_self(ast) {
   let code = js_unparse_try(ast);
-  if (null_is(value)) {
+  if (null_is(code)) {
     code = ast;
   }
   return code;
