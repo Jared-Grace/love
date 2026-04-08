bb64c2cebf95db4be6b4019ff27701fa5e25029b
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:56:23 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- web/public/src/function_node_select_inner.mjs
+++ web/public/src/function_node_select_inner.mjs
@@ -1,8 +1,7 @@
-import { null_is_if } from "../../../love/public/src/null_is_if.mjs";
+import { js_visit_id_or_node } from "../../../love/public/src/js_visit_id_or_node.mjs";
 import { js_visit_id_to_node_or_id_multiple } from "../../../love/public/src/js_visit_id_to_node_or_id_multiple.mjs";
 import { js_unparse_try_multiple } from "../../../love/public/src/js_unparse_try_multiple.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
-import { js_visit_id_try } from "../../../love/public/src/js_visit_id_try.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { data_transform } from "../../../love/public/src/data_transform.mjs";
 import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
@@ -16,8 +15,7 @@ export async function function_node_select_inner(
 ) {
   let node = js_visit_id_to_node(ast, node_id);
   let n = await function_run(select_fn_name, [node]);
-  let item_to_add = js_visit_id_try(ast, n);
-  item_to_add = null_is_if(item_to_add, n);
+  let item_to_add = js_visit_id_or_node(ast, n);
   async function lambda(previous) {
     list_add(previous, item_to_add);
     on_previous(previous);
