7d61c9fcb38b29b8ea429e9d8ac0f92201845828
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:13:19 -0400

    function_auto_path ../love/public/src/repo_about_get.mjs

--- public/src/repo_about_get.mjs
+++ public/src/repo_about_get.mjs
@@ -1,8 +1,7 @@
-import { data_get } from "../../../love/public/src/data_get.mjs";
+import { data_get_value } from "../../../love/public/src/data_get_value.mjs";
 import { path_repo_about } from "../../../love/public/src/path_repo_about.mjs";
 export async function repo_about_get(repo, key) {
   let a_path = path_repo_about(repo);
-  let { value } = await data_get(key, null, a_path);
-  let v = value;
-  return v;
+  let value = await data_get_value(property_name, null, a_path);
+  return value;
 }
