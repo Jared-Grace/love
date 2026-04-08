bfdde108ab6797743f6ece5ad534fd7a057fcd30
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:02:47 -0400

    function_auto_path ../love/public/src/search_generic.mjs

--- public/src/search_generic.mjs
+++ public/src/search_generic.mjs
@@ -1,9 +1,9 @@
+import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { text_split } from "../../../love/public/src/text_split.mjs";
 export function search_generic(search, list, value_get, include) {
-  let terms = text_split(search, ",");
+  let terms = text_split_dot_comma(search);
   function filter(item) {
     function lambda(term) {
       let v = include(item, term);
