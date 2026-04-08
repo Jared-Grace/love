03328cdcee77196117a0177861c8fef39e772344
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:33:33 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- public/src/function_node_select_inner.mjs
+++ public/src/function_node_select_inner.mjs
@@ -4,6 +4,7 @@ import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_nod
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { js_visit_id_try } from "../../../love/public/src/js_visit_id_try.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
+import { js_unparse_multiple } from "../../../love/public/src/js_unparse_multiple.mjs";
 import { data_transform } from "../../../love/public/src/data_transform.mjs";
 import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -33,10 +34,12 @@ export async function function_node_select_inner(
     d_path,
   );
   let selected = list_map(value, js_visit_id_to_node_or_id);
+  let selected_code = js_unparse_multiple(selected);
   let f_name_current = await function_current_get();
   let r = {
     f_name_current,
     selected_code,
+    selected,
   };
   return r;
 }
