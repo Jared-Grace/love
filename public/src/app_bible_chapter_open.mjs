47f7a7ea5f1b5642603aafbf303484db9d920661
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 20:38:53 -0400

    function_auto_path ../love/public/src/app_bible_chapter_open.mjs

--- public/src/app_bible_chapter_open.mjs
+++ public/src/app_bible_chapter_open.mjs
@@ -2,14 +2,9 @@ import { app_bible_verse_set } from "../../../love/public/src/app_bible_verse_se
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { app_bible_chapter_set } from "../../../love/public/src/app_bible_chapter_set.mjs";
-export function app_bible_chapter_open(
-  context,
-  hash,
-  chapter_code,
-  verse_number,
-) {
+export function app_bible_chapter_open(context, hash, chapter_code) {
   app_bible_verse_set(hash, chapter_code);
-  app_bible_chapter_set(hash, chapter_code);
+  app_bible_chapter_set(hash, verse_number);
   let screen_home = property_get(context, "screen_home");
   app_shared_screen_set(context, screen_home);
 }
