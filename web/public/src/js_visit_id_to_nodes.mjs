fd7772096035458e5b74e5e40b486c0c61cafb7f
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:24:06 -0400

    s visit,node

--- web/public/src/js_visit_id_to_nodes.mjs
+++ web/public/src/js_visit_id_to_nodes.mjs
@@ -1,14 +1,15 @@
+import { equal } from "../../../love/public/src/equal.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_id_to_nodes(ast, ids) {
+import { equal } from "assert";
+export function js_visit_id_to_nodes(ast, id) {
   let result = [];
   let i = 0;
   function lambda(v) {
     let node = property_get(v, "node");
-    let includes = list_includes(ids, i);
-    if (includes) {
+    let e = equal(id, i);
+    if (e) {
       list_add(result, node);
     }
     i++;
