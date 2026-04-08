4878ded955d5221a0798b323ec3436d10fb4a0d4
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 23:03:09 -0400

    function_auto_path ../love/public/src/function_curryify_count.mjs

--- public/src/function_curryify_count.mjs
+++ public/src/function_curryify_count.mjs
@@ -1,6 +1,6 @@
+import { function_curryify_count_args_get_curried_right } from "../../../love/public/src/function_curryify_count_args_get_curried_right.mjs";
 import { function_curryify_generic_name } from "../../../love/public/src/function_curryify_generic_name.mjs";
 import { integer_to } from "../../../love/public/src/integer_to.mjs";
-import { function_curryify_right_count_args_get_curried_right } from "../../../love/public/src/function_curryify_right_count_args_get_curried_right.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
@@ -12,7 +12,7 @@ export async function function_curryify_count(f_name, count) {
     let combined = function_name_combine(n, count);
     return combined;
   }
-  let r2 = function_curryify_right_count_args_get_curried_right(count);
+  let r2 = function_curryify_count_args_get_curried_right(count);
   let output = await function_curryify_generic(f_name, lambda, r2);
   return output;
 }
