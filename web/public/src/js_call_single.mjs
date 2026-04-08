86b288adb60b4a37ccf5b8730fa592abe3b1049a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:00:42 -0400

    function_auto_path ../love/public/src/js_call_single.mjs

--- web/public/src/js_call_single.mjs
+++ web/public/src/js_call_single.mjs
@@ -1,1 +1,4 @@
-export function js_call_single(ast) {}
+import { js_list_calls } from "../../../love/public/src/js_list_calls.mjs";
+export function js_call_single(ast) {
+  let vs = js_list_calls(ast2);
+}
