b26e6e6ba982dc2d8c45d6573ba2184c9bb20197
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:26:55 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- web/public/src/function_wrap_generic_args.mjs
+++ web/public/src/function_wrap_generic_args.mjs
@@ -1,8 +1,8 @@
-import { function_name_new_get } from "../../../love/public/src/function_name_new_get.mjs";
+import { function_name_new_get_args } from "../../../love/public/src/function_name_new_get_args.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_generic_args(plugin_fn, f_name_old, args) {
   let f_name_new = null;
-  ({ f_name_new, f_name_old } = await function_name_new_get(
+  ({ f_name_new, f_name_old } = await function_name_new_get_args(
     plugin_fn,
     f_name_old,
     args,
