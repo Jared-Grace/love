5dda580d2837e9c5b637144acf942158b2333035
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:07:31 -0400

    ra tr user_repo_path repo,data

--- public/src/user_data_set_fn.mjs
+++ public/src/user_data_set_fn.mjs
@@ -1,6 +1,6 @@
 import { data_set } from "../../../love/public/src/data_set.mjs";
-import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
+import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
 export async function user_data_set_fn(property_name, lambda) {
-  let f_path = user_repo_path();
+  let f_path = user_data_path();
   await data_set(lambda, property_name, f_path);
 }
