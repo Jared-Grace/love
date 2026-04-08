45ddeb51bd9cd93742d08165354f430a0ee9fdcd
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:59:37 -0400

    function_auto_path ../love/public/src/js_visit_id_to.mjs

--- public/src/function_node_select.mjs
+++ public/src/function_node_select.mjs
@@ -2,12 +2,12 @@ import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { data_transform } from "../../../love/public/src/data_transform.mjs";
 import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
-import { js_visit_id_get } from "../../../love/public/src/js_visit_id_get.mjs";
+import { js_visit_id_to } from "../../../love/public/src/js_visit_id_to.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 export async function function_node_select(select_fn_name) {
   let ast = await function_current_ast();
   let result = await function_run(select_fn_name, [ast]);
-  let id = js_visit_id_get(ast, result);
+  let id = js_visit_id_to(ast, result);
   async function lambda(previous) {
     list_add(previous, id);
     return previous;
