1d6b1d548e19b9e5f26fc48b29c82ba5c1f397b7
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:55:21 -0400

    function_auto_path ../love/public/src/function_rename_open_generic.mjs

--- public/src/function_rename_open_generic.mjs
+++ public/src/function_rename_open_generic.mjs
@@ -1,8 +1,18 @@
+import { function_name_new_get_args } from "../../../love/public/src/function_name_new_get_args.mjs";
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_rename_open } from "../../../love/public/src/function_rename_open.mjs";
 export async function function_rename_open_generic(
-  f_name_before,
-  f_name_after,
+  plugin_fn,
+  f_name_old,
+  args,
 ) {
+  arguments_assert(arguments, 3);
+  let f_name_new = null;
+  ({ f_name_new, f_name_old } = await function_name_new_get_args(
+    plugin_fn,
+    f_name_old,
+    args,
+  ));
   let r = await function_rename_open(f_name_before, f_name_after);
   return r;
 }
