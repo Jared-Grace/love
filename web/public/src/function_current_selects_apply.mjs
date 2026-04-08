c2bbacb57b29f9cd99289da5f7e2764ebf81f800
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:33:27 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- web/public/src/function_current_selects_apply.mjs
+++ web/public/src/function_current_selects_apply.mjs
@@ -1,7 +1,9 @@
+import { function_current_selects_add } from "../../../love/public/src/function_current_selects_add.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_current_selects_first } from "../../../love/public/src/function_current_selects_first.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
   let r = await function_current_selects_first();
   let n = await function_run(apply_fn_name, [r]);
+  let value = await function_current_selects_add(item_to_add, on_previous);
   return n;
 }
