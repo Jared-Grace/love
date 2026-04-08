18e0092e1a6b7f868190624fc674c5f932d440fc
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:46:08 -0400

    fwa nc file_open fn

--- public/src/file_open_fn.mjs
+++ public/src/file_open_fn.mjs
@@ -1,1 +1,4 @@
-export function file_open_fn() {}
+import { file_open } from "../../../love/public/src/file_open.mjs";
+export async function file_open_fn(f_path) {
+  return await file_open(f_path);
+}
