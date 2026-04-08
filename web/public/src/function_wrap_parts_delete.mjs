86ac887298d74d911f6d2546c3847e7144f93605
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:25:58 -0400

    function_auto_path ../love/public/src/function_wrap_parts_delete.mjs

--- web/public/src/function_wrap_parts_delete.mjs
+++ web/public/src/function_wrap_parts_delete.mjs
@@ -1,5 +1,9 @@
+import { function_name_parts_remove_comma } from "../../../love/public/src/function_name_parts_remove_comma.mjs";
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
-export async function function_wrap_parts_delete(f_name, f_name_wrapped) {
-  let r = await function_wrap(f_name, f_name_wrapped);
+export async function function_wrap_parts_delete(f_name, deleted) {
+  let f_name_old = await function_name_unalias_only(f_name_old);
+  let f_name_new = function_name_parts_remove_comma(f_name_old, deleted);
+  let r = await function_wrap(f_name, f_name_new);
   return r;
 }
