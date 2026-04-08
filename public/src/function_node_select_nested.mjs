387760fd18fc29846137fce8a17802f97bcaafd7
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:34:19 -0400

    function_auto_path ../love/public/src/function_node_select_nested.mjs

--- public/src/function_node_select_nested.mjs
+++ public/src/function_node_select_nested.mjs
@@ -1,20 +1,11 @@
 import { list_remove_curried_right } from "../../../love/public/src/list_remove_curried_right.mjs";
 import { function_current_selects_first } from "../../../love/public/src/function_current_selects_first.mjs";
 import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
-import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { function_node_select_inner } from "../../../love/public/src/function_node_select_inner.mjs";
 export async function function_node_select_nested(select_fn_name) {
   let ast = await function_current_ast();
   let first_id = await function_current_selects_first();
-  let r2 = list_remove_curried_right(item);
-  function lambda(previous) {
-    list_remove(previous, first_id);
-  }
-  let r = await function_node_select_inner(
-    select_fn_name,
-    first_id,
-    lambda,
-    ast,
-  );
+  let r2 = list_remove_curried_right(first_id);
+  let r = await function_node_select_inner(select_fn_name, first_id, r2, ast);
   return r;
 }
