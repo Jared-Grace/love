6bdf8452ede6bc616bd4a9d50826547e0608a036
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:41:27 -0400

    function_param_swap_beginning_multiple app_bible_chapter_open_curried_2,app_bible_chapter_open

--- web/public/src/app_bible_chapter_open.mjs
+++ web/public/src/app_bible_chapter_open.mjs
@@ -1,7 +1,7 @@
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_bible_chapter_set } from "../../../love/public/src/app_bible_chapter_set.mjs";
-export function app_bible_chapter_open(hash, context, chapter_code) {
+export function app_bible_chapter_open(context, hash, chapter_code) {
   app_bible_chapter_set(hash, chapter_code);
   let screen_home = property_get(context, "screen_home");
   app_shared_screen_set(context, screen_home);
--- web/public/src/app_bible_verse_open.mjs
+++ web/public/src/app_bible_verse_open.mjs
@@ -4,7 +4,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_bible_verse_set } from "../../../love/public/src/app_bible_verse_set.mjs";
 export function app_bible_verse_open(context, hash, chapter_code) {
   app_bible_verse_set(hash, verse_number);
-  app_bible_chapter_open(hash, context, chapter_code);
+  app_bible_chapter_open(context, hash, chapter_code);
   let screen_home = property_get(context, "screen_home");
   app_shared_screen_set(context, screen_home);
 }
