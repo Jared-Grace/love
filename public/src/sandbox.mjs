2499a7fa0261def97305d20b0b23ce2020981c34
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:41:54 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,3 @@
-import { function_rename_open_generic } from "../../../love/public/src/function_rename_open_generic.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { function_current_selects_apply } from "../../../love/public/src/function_current_selects_apply.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
@@ -15,7 +14,7 @@ export async function sandbox() {
   let r = await function_node_select(js_statement_first.name);
   let r2 = await function_node_select_nested(js_call_single.name);
   let r4 = await function_node_select_nested(js_call_callee_name.name);
-  let r5 = await function_rename_open_generic(plugin_fn, f_name_old);
+  return;
   let r32 = await function_current_selects_apply(function_open.name);
   let r3 = {
     r,
