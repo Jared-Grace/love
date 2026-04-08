baee9a0e9821f0b49e87c4b4b958c2a98d4273fa
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:46:14 -0400

    function_auto_path ../love/public/src/js_visit_id_to_node_or_id_multiple.mjs

--- web/public/src/js_visit_id_to_node_or_id_multiple.mjs
+++ web/public/src/js_visit_id_to_node_or_id_multiple.mjs
@@ -1,7 +1,7 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { js_visit_id_to_node_or_id_curried } from "../../../love/public/src/js_visit_id_to_node_or_id_curried.mjs";
-export function js_visit_id_to_node_or_id_multiple(ast, value) {
+export function js_visit_id_to_node_or_id_multiple(ast, list) {
   let r2 = js_visit_id_to_node_or_id_curried(ast);
-  let selected = list_map(value, r2);
+  let selected = list_map(list, r2);
   return selected;
 }
