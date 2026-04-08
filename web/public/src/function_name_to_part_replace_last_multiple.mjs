7f215f4e5db4bd9a7d8106364a936bee8bbee1b7
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 16:04:03 -0400

    function_auto_path ../love/public/src/function_name_to_part_replace_last.mjs

---
+++ web/public/src/function_name_to_part_replace_last_multiple.mjs
@@ -0,0 +1,13 @@
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { function_name_to_parts_remove_last_multiple } from "../../../love/public/src/function_name_to_parts_remove_last_multiple.mjs";
+export function function_name_to_part_replace_last_multiple(
+  f_name_old,
+  count,
+  replacement,
+) {
+  let parts = function_name_to_parts_remove_last_multiple(f_name_old, count);
+  list_add(parts, replacement);
+  let f_name_new = function_name_combine_multiple(parts);
+  return f_name_new;
+}
