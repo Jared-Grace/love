0184f2089af533989b73c993a2f77acb31a35d90
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 16:33:15 -0400

    function_auto_path ../love/public/src/js_visit_id.mjs

--- public/src/js_visit_id.mjs
+++ public/src/js_visit_id.mjs
@@ -4,14 +4,17 @@ import { js_visit_property_node_index } from "../../../love/public/src/js_visit_
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 export function js_visit_id(ast, target) {
+  let c = js_unparse(target);
   let id = null;
   js_visit_property_node_index(ast, inner);
   function inner(node, i) {
     try {
       let code = js_unparse(node);
-      log(js_visit_id.name, {
-        code,
-      });
+      if (equal(c, code)) {
+        log(js_visit_id.name, {
+          code,
+        });
+      }
     } catch (E) {}
     if (equal(node, target)) {
       id = i;
