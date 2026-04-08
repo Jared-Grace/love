fd49896e78072ccf139ea4372c41953b5dec6628
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:51:35 -0400

    wa lrt js_unparse_try js_unparse_or_self

--- public/src/js_unparse_or_self.mjs
+++ public/src/js_unparse_or_self.mjs
@@ -1,1 +1,4 @@
-export function js_unparse_or_self() {}
+import { js_unparse_try } from "../../../love/public/src/js_unparse_try.mjs";
+export function js_unparse_or_self(ast) {
+  return js_unparse_try(ast);
+}
