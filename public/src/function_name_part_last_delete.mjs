e675e00559ab867a96bfeeb784d4dcf080a4dc0a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:15:57 -0400

    function_auto_path ../love/public/src/function_name_part_last_delete.mjs

--- public/src/function_name_part_last_delete.mjs
+++ public/src/function_name_part_last_delete.mjs
@@ -1,9 +1,9 @@
+import { list_pop } from "../../../love/public/src/list_pop.mjs";
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
-import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
 export function function_name_part_last_delete(f_name_old, removals) {
   let parts = function_name_to_parts(f_name_old);
-  let difference = list_difference(parts, removals);
+  let popped = list_pop(list);
   let f_name_new = function_name_combine_multiple(difference);
   return f_name_new;
 }
