84a92f660b2c6bd76c13015672006f26245728b1
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:33:20 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- web/public/src/function_node_select_inner.mjs
+++ web/public/src/function_node_select_inner.mjs
@@ -1,11 +1,9 @@
+import { function_current_selects_add } from "../../../love/public/src/function_current_selects_add.mjs";
 import { js_unparse_or_self_multiple } from "../../../love/public/src/js_unparse_or_self_multiple.mjs";
 import { js_visit_id_or_node } from "../../../love/public/src/js_visit_id_or_node.mjs";
 import { js_visit_id_to_node_or_id_multiple } from "../../../love/public/src/js_visit_id_to_node_or_id_multiple.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
-import { data_transform } from "../../../love/public/src/data_transform.mjs";
-import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 export async function function_node_select_inner(
   select_fn_name,
@@ -16,18 +14,7 @@ export async function function_node_select_inner(
   let node = js_visit_id_to_node(ast, node_id);
   let n = await function_run(select_fn_name, [node]);
   let item_to_add = js_visit_id_or_node(ast, n);
-  async function lambda(previous) {
-    list_add(previous, item_to_add);
-    on_previous(previous);
-    return previous;
-  }
-  let d_path = user_data_path();
-  let value = await data_transform(
-    "function_current_selects",
-    [],
-    lambda,
-    d_path,
-  );
+  let value = await function_current_selects_add(item_to_add, on_previous);
   let selected = js_visit_id_to_node_or_id_multiple(value, ast);
   let selected_code = js_unparse_or_self_multiple(selected);
   let f_name_current = await function_current_get();
