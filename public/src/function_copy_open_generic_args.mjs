56324809c88435ce20ac93f2cbeb05ff5699ddd4
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:33:20 -0400

    fwa nc c generic_args

--- public/src/function_copy_open_generic_args.mjs
+++ public/src/function_copy_open_generic_args.mjs
@@ -1,1 +1,4 @@
-export function function_copy_open_generic_args() {}
+import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
+export async function function_copy_open_generic_args(f_name_old, f_name_new) {
+  return await function_copy_open(f_name_old, f_name_new);
+}
