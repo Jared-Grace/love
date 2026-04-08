08d16d3dd3ce76ba27f738649509705789e73ee2
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:17:13 -0400

    function_auto_path ../love/public/src/function_curryify_count_args_get.mjs

--- public/src/function_curryify_count_args_get.mjs
+++ public/src/function_curryify_count_args_get.mjs
@@ -1,7 +1,5 @@
-import { list_skip_end_count_curried_right } from "../../../love/public/src/list_skip_end_count_curried_right.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
 export function function_curryify_count_args_get(arg_names, count) {
-  let r2 = list_skip_end_count_curried_right(count);
   let r = function_curryify_args_get_generic(r2, arg_names);
   return r;
 }
