25df700323f7377ff0eb61863c04d63565617d26
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:26:40 -0400

    pf function_name_new_get plugin_fn

--- web/public/src/function_name_new_get.mjs
+++ web/public/src/function_name_new_get.mjs
@@ -2,7 +2,7 @@ import { function_run } from "../../../love/public/src/function_run.mjs";
 import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
-export async function function_name_new_get(f_name_old, args, plugin_fn) {
+export async function function_name_new_get(plugin_fn, f_name_old, args) {
   f_name_old = await function_name_unalias_only(f_name_old);
   let split = text_split_comma(args);
   let args_run = list_concat_single(f_name_old, split);
