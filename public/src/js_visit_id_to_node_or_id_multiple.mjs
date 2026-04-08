aa70ab1e0ae9bd03d1618d351ef4c12a44958c43
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:46:17 -0400

    pb js_visit_id_to_node_or_id_multiple

--- public/src/js_visit_id_to_node_or_id_multiple.mjs
+++ public/src/js_visit_id_to_node_or_id_multiple.mjs
@@ -1,6 +1,6 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { js_visit_id_to_node_or_id_curried } from "../../../love/public/src/js_visit_id_to_node_or_id_curried.mjs";
-export function js_visit_id_to_node_or_id_multiple(ast, list) {
+export function js_visit_id_to_node_or_id_multiple(list, ast) {
   let r2 = js_visit_id_to_node_or_id_curried(ast);
   let selected = list_map(list, r2);
   return selected;
