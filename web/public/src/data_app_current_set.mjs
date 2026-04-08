ad5286a6755f3e103c3079de37f00d3f742e2589
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:40:28 -0400

    function_auto_path ../love/public/src/data_app_current_set.mjs

--- web/public/src/data_app_current_set.mjs
+++ web/public/src/data_app_current_set.mjs
@@ -1,6 +1,4 @@
 import { user_data_set } from "../../../love/public/src/user_data_set.mjs";
-import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
 export async function data_app_current_set(f_name) {
-  let d_path = user_repo_path();
-  await user_data_set("app_current", d_path);
+  await user_data_set("app_current", f_name);
 }
