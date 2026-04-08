45f43126f6d8e586183e0281aace11061784cd25
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:52:44 -0400

    function_auto_path ../love/public/src/function_transform_command.mjs

--- web/public/src/function_transform_command.mjs
+++ web/public/src/function_transform_command.mjs
@@ -2,6 +2,6 @@ import { function_transform_result } from "../../../love/public/src/function_tra
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 export async function function_transform_command(transformer_name, f_name) {
   const transformer = await function_import_unalias(transformer_name);
-  let v = await function_transform_result(f_name, transformer);
-  return v;
+  let r = await function_transform_result(f_name, transformer);
+  return r;
 }
