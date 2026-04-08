7e343e3cc0485de4f54f2852c54a96c86f48e32f
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 22:36:52 -0400

    function_auto_path ../love/public/src/app_shared_screen_set_fn.mjs

--- public/src/app_shared_screen_set_fn.mjs
+++ public/src/app_shared_screen_set_fn.mjs
@@ -1,6 +1,6 @@
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 export function app_shared_screen_set_fn(context, fn) {
-  let r = function lambda11() {
+  let r = function app_shared_screen_set_fn_inner() {
     app_shared_screen_set(context, fn);
   };
   return r;
