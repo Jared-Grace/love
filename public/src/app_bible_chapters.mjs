ef64ac5e2c531a5de9baa5820d7b45c5fdab9e15
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 20:36:55 -0400

    function_auto_path ../love/public/src/app_bible_chapters.mjs

--- public/src/app_bible_chapters.mjs
+++ public/src/app_bible_chapters.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { ebible_book_code_to_chapter_codes_browser } from "../../../love/public/src/ebible_book_code_to_chapter_codes_browser.mjs";
 import { ebible_version_books_browser } from "../../../love/public/src/ebible_version_books_browser.mjs";
 import { html_centered } from "../../../love/public/src/html_centered.mjs";
@@ -37,7 +38,8 @@ export async function app_bible_chapters(context) {
   function lambda(chapter_code) {
     let chapter_name = ebible_chapter_code_to_name(chapter_code);
     function lambda3() {
-      app_bible_chapter_open(context, hash, chapter_code, error());
+      let verse_number = error();
+      app_bible_chapter_open(context, hash, chapter_code, verse_number);
     }
     let component = html_button(div, chapter_name, lambda3);
   }
