a6b3d7bf0f5874a393403af30716590f9546885c
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:48:33 -0400

    function_auto_path ../love/public/src/js_statement_first.mjs

--- public/src/js_statement_first.mjs
+++ public/src/js_statement_first.mjs
@@ -7,6 +7,7 @@ export function js_statement_first(ast) {
   let body_block = js_flo_body(ast);
   let first = list_first(body_block);
   let code = js_unparse(first);
+  let result = null;
   let i = -1;
   function lambda(v) {
     let node = property_get(v, "node");
