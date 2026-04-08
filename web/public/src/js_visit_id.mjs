541931514905ed86b357dfa284d9dc1800986c1e
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 16:30:06 -0400

    function_auto_path ../love/public/src/js_visit_id.mjs

--- web/public/src/js_visit_id.mjs
+++ web/public/src/js_visit_id.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_visit_property_node_index } from "../../../love/public/src/js_visit_property_node_index.mjs";
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
@@ -5,6 +6,9 @@ export function js_visit_id(ast, target) {
   let id = null;
   js_visit_property_node_index(ast, inner);
   function inner(node, i) {
+    log(js_visit_id.name, {
+      node,
+    });
     if (equal(node, target)) {
       id = i;
     }
