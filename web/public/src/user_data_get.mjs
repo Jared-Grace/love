5eb3a3a72abb0efd3f7c8c7af39f2a630cd66bf3
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:12:37 -0400

    function_auto_path ../love/public/src/user_data_get.mjs

--- web/public/src/user_data_get.mjs
+++ web/public/src/user_data_get.mjs
@@ -2,7 +2,6 @@ import { data_get_value } from "../../../love/public/src/data_get_value.mjs";
 import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
 export async function user_data_get(property_name) {
   let f_path = user_data_path();
-  const initial = null;
-  let value = await data_get_value(property_name, initial, f_path);
+  let value = await data_get_value(property_name, null, f_path);
   return value;
 }
