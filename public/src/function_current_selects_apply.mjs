c5298226fdac6171473623fe1e69ca02dfda6758
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:34:39 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -1,9 +1,11 @@
+import { list_remove_curried_right } from "../../../love/public/src/list_remove_curried_right.mjs";
 import { function_current_selects_add } from "../../../love/public/src/function_current_selects_add.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_current_selects_first } from "../../../love/public/src/function_current_selects_first.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
   let r = await function_current_selects_first();
   let n = await function_run(apply_fn_name, [r]);
-  let value = await function_current_selects_add(item_to_add, on_previous);
+  let r2 = list_remove_curried_right(r);
+  let value = await function_current_selects_add(item_to_add, r2);
   return n;
 }
