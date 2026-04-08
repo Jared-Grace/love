89716022f4644326a1bcee34a1d270293341766f
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:38:30 -0400

    function_auto_path ../love/public/src/user_data_set_fn.mjs

--- web/public/src/user_repo_set.mjs
+++ web/public/src/user_repo_set.mjs
@@ -1,4 +1,4 @@
-import { user_data_set } from "../../../love/public/src/user_data_set.mjs";
+import { user_data_set_fn } from "../../../love/public/src/user_data_set_fn.mjs";
 import { user_repo_get } from "../../../love/public/src/user_repo_get.mjs";
 import { repo_exists_assert } from "../../../love/public/src/repo_exists_assert.mjs";
 import { repo_acronym_to_name } from "../../../love/public/src/repo_acronym_to_name.mjs";
@@ -9,7 +9,7 @@ export async function user_repo_set(value) {
   async function lambda(previous) {
     return repo_name;
   }
-  await user_data_set(property_name, lambda);
+  await user_data_set_fn(property_name, lambda);
   let repo_name2 = await user_repo_get();
   return repo_name2;
 }
