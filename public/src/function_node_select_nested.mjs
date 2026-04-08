7bd3847dc26501338fa640bf56f728caf3f14f8a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:19:28 -0400

    function_auto_path ../love/public/src/function_node_select_nested.mjs

--- public/src/function_node_select_nested.mjs
+++ public/src/function_node_select_nested.mjs
@@ -1,10 +1,13 @@
+import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { user_data_get } from "../../../love/public/src/user_data_get.mjs";
 import { function_node_select_inner } from "../../../love/public/src/function_node_select_inner.mjs";
 export async function function_node_select_nested(select_fn_name) {
   let selects = await user_data_get("function_current_selects");
   let first = list_first(selects);
-  function lambda(previous) {}
+  function lambda(previous) {
+    list_remove(list, item);
+  }
   let r = await function_node_select_inner(select_fn_name, first, lambda);
   return r;
 }
