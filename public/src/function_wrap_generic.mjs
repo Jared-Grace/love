596f01894a1025be070452ccd0f0f4ba91e06e7f
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:59:21 -0400

    function_auto_path ../love/public/src/function_wrap_generic.mjs

--- public/src/function_wrap_generic.mjs
+++ public/src/function_wrap_generic.mjs
@@ -4,7 +4,8 @@ import { function_name_unalias_only } from "../../../love/public/src/function_na
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_generic(plugin_fn, f_name_old, args) {
   f_name_old = await function_name_unalias_only(f_name_old);
-  let split = text_split_comma(t);
+  let split = text_split_comma(args);
+  let args_run = [f_name_old, args];
   let f_name_new = await function_run(plugin_fn, [f_name_old, args]);
   let r = await function_wrap(f_name_old, f_name_new);
   return r;
