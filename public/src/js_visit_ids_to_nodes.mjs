b423d8e1c72ff2d41cb7d8a27506fb35926c5f2c
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 16:24:41 -0400

    function_auto_path ../love/public/src/js_visit_ids_to_nodes.mjs

--- public/src/js_visit_ids_to_nodes.mjs
+++ public/src/js_visit_ids_to_nodes.mjs
@@ -3,7 +3,7 @@ import { list_includes } from "../../../love/public/src/list_includes.mjs";
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_id_ids_to_nodes(ast, ids) {
+export function js_visit_ids_to_nodes(ast, ids) {
   let result = [];
   let id = null;
   let i = 0;
