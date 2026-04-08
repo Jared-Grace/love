de1c1e26a9343a60b57f95fbb212313f51a1d64a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:49:49 -0400

    function_auto_path ../love/public/src/js_statement_first.mjs

--- public/src/js_statement_first.mjs
+++ public/src/js_statement_first.mjs
@@ -1,6 +1,4 @@
-import { equal } from "../../../love/public/src/equal.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_visit } from "../../../love/public/src/js_visit.mjs";
+import { js_visit_id } from "../../../love/public/src/js_visit_id.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
@@ -8,15 +6,6 @@ export function js_statement_first(ast) {
   let body_block = js_flo_body(ast);
   let first = list_first(body_block);
   let code = js_unparse(first);
-  let result = null;
-  let i = 0;
-  function lambda(v) {
-    let node = property_get(v, "node");
-    if (equal(node, first)) {
-      result = i;
-    }
-    i++;
-  }
-  js_visit(ast, lambda);
+  js_visit_id(first, ast);
   return code;
 }
