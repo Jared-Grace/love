4a8f9f10aebcb279efbfe43356df4c4bfbe468bf
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:34:03 -0400

    cur list_remove

--- web/public/src/list_remove_curried_right.mjs
+++ web/public/src/list_remove_curried_right.mjs
@@ -1,1 +1,6 @@
-export function list_remove_curried_right() {}
+import { list_remove } from "../../../love/public/src/list_remove.mjs";
+export function list_remove_curried_right(item) {
+  return function list_remove_curried_right_result(list) {
+    return list_remove(list, item);
+  };
+}
