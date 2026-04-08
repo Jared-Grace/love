8e8245cd5f2e7659171591828e5e62184b6b9b1a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:27:47 -0400

    fwa nc function_name_to_part_last replace

--- public/src/function_name_to_part_last_replace.mjs
+++ public/src/function_name_to_part_last_replace.mjs
@@ -1,1 +1,4 @@
-export function function_name_to_part_last_replace() {}
+import { function_name_to_part_last } from "../../../love/public/src/function_name_to_part_last.mjs";
+export function function_name_to_part_last_replace(f_name_before) {
+  return function_name_to_part_last(f_name_before);
+}
