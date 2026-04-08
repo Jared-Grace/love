36e8c9ebd35f7fa56b183801ffdfdb2eca30813e
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:36:29 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { function_open } from "../../../love/public/src/function_open.mjs";
 import { function_current_selects_apply } from "../../../love/public/src/function_current_selects_apply.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
@@ -12,7 +13,8 @@ export async function sandbox() {
   await function_current_set(app_bible_verses.name);
   let r = await function_node_select(js_statement_first.name);
   let r2 = await function_node_select_nested(js_call_single.name);
-  let r4 = await function_node_select_nested(js_call_callee_name.name);o
+  let r4 = await function_node_select_nested(js_call_callee_name.name);
+  await function_open(f_name);
   let r32 = await function_current_selects_apply(apply_fn_name);
   let r3 = {
     r,
