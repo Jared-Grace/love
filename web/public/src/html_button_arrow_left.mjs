38b466a22069342c541f662e155a8c6a1ad2c38e
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 22:23:06 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

---
+++ web/public/src/html_button_arrow_left.mjs
@@ -0,0 +1,6 @@
+import { html_button } from "../../../love/public/src/html_button.mjs";
+import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs";
+export function html_button_arrow_left(bar, lambda6) {
+  let text3 = emoji_arrow_left();
+  let component4 = html_button(bar, text3, lambda6);
+}
