d97ee51decbd9fca1a9f930552c04fdb4bbd27d5
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 22:44:38 -0400

    function_auto_path ../love/public/src/app_bible_chapters.mjs

--- public/src/app_bible_chapters.mjs
+++ public/src/app_bible_chapters.mjs
@@ -5,13 +5,10 @@ import { html_centered } from "../../../love/public/src/html_centered.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_bible_verse_open } from "../../../love/public/src/app_bible_verse_open.mjs";
 import { ebible_chapter_code_to_name } from "../../../love/public/src/ebible_chapter_code_to_name.mjs";
-import { list_map_prefix_without } from "../../../love/public/src/list_map_prefix_without.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { html_div_text_centered } from "../../../love/public/src/html_div_text_centered.mjs";
 import { ebible_book_code_to_name } from "../../../love/public/src/ebible_book_code_to_name.mjs";
-import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { ebible_chapter_code_to_book } from "../../../love/public/src/ebible_chapter_code_to_book.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
@@ -27,8 +24,6 @@ export async function app_bible_chapters(context) {
     e,
     book_code,
   );
-  let mapped = list_map_prefix_without(chapter_codes, book_code);
-  let chapter_numbers = list_map(mapped, integer_to_try);
   let books = await ebible_version_books_browser(e);
   let book_name = ebible_book_code_to_name(books, book_code);
   html_div_text_centered(root, book_name);
