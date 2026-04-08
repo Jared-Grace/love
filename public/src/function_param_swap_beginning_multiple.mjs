3d055549fd5725ca2a6668054d2e7e1a15e15029
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:39:30 -0400

    function_auto_path ../love/public/src/function_param_swap_beginning_multiple.mjs

--- public/src/function_param_swap_beginning_multiple.mjs
+++ public/src/function_param_swap_beginning_multiple.mjs
@@ -3,8 +3,8 @@ import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs"
 import { function_param_swap_beginning } from "../../../love/public/src/function_param_swap_beginning.mjs";
 export async function function_param_swap_beginning_multiple(f_name) {
   let split = text_split_comma(f_name);
-  async function lambda(item) {}
+  async function lambda(item) {
+    let r = await function_param_swap_beginning(f_name);
+  }
   await each_async(list, lambda);
-  let r = await function_param_swap_beginning(f_name);
-  return r;
 }
