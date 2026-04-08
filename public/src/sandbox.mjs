9ff01b53026d1de1e044b033586b65d3eca1c079
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:21:51 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,3 +1,4 @@
+import { function_node_select_nested } from "../../../love/public/src/function_node_select_nested.mjs";
 import { js_statement_first } from "../../../love/public/src/js_statement_first.mjs";
 import { function_node_select } from "../../../love/public/src/function_node_select.mjs";
 import { app_bible_verses } from "../../../love/public/src/app_bible_verses.mjs";
@@ -5,4 +6,5 @@ import { function_open } from "../../../love/public/src/function_open.mjs";
 export async function sandbox() {
   await function_open(app_bible_verses.name);
   let r = await function_node_select(js_statement_first.name);
+  let r2 = await function_node_select_nested(select_fn_name);
 }
