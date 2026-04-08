8f10df208f56b767d60fbc94ac895d6275b89553
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:05:28 -0400

    function_auto_path ../love/public/src/app_bible_chapters.mjs

--- public/src/app_bible_chapters.mjs
+++ public/src/app_bible_chapters.mjs
@@ -1,3 +1,4 @@
+import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_bible_chapters_before } from "../../../love/public/src/app_bible_chapters_before.mjs";
 import { html_button_list_centered } from "../../../love/public/src/html_button_list_centered.mjs";
@@ -8,6 +9,7 @@ export async function app_bible_chapters(context) {
   let r = await app_bible_chapters_before(context);
   let root = property_get(r, "root");
   let book_code = property_get(r, "book_code");
+  let e = ebible_folder_english();
   let items = await ebible_book_code_to_chapter_codes_browser(e, book_code);
   let code_to_button_text = ebible_chapter_code_to_name;
   let oc = app_bible_chapter_open_curried(context);
