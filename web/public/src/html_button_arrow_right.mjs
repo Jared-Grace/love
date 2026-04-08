6a5c68bf82d7ced4f805437e0995c4f6bd406837
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 22:23:37 -0400

    function_auto_path ../love/public/src/html_button_arrow_right.mjs

--- web/public/src/html_button_arrow_right.mjs
+++ web/public/src/html_button_arrow_right.mjs
@@ -1,6 +1,6 @@
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { emoji_arrow_right } from "../../../love/public/src/emoji_arrow_right.mjs";
-export function html_button_arrow_right(bar, lambda3) {
+export function html_button_arrow_right(parent, lambda3) {
   let text2 = emoji_arrow_right();
-  let component3 = html_button(bar, text2, lambda3);
+  let component3 = html_button(parent, text2, lambda3);
 }
