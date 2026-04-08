dd64fe749f8a45e94742914b7460638e6de381a3
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:26:40 -0400

    pf function_name_new_get plugin_fn

--- public/src/function_wrap_generic.mjs
+++ public/src/function_wrap_generic.mjs
@@ -3,9 +3,9 @@ import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_generic(plugin_fn, f_name_old, args) {
   let f_name_new = null;
   ({ f_name_new, f_name_old } = await function_name_new_get(
+    plugin_fn,
     f_name_old,
     args,
-    plugin_fn,
   ));
   let r = await function_wrap(f_name_old, f_name_new);
   return r;
