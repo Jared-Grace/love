c111f7a5edd599ae5a198c86be1adaa0cb48b7c0
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:43:29 -0400

    function_auto_path ../love/public/src/user_data_open.mjs

--- web/public/src/user_data_open.mjs
+++ web/public/src/user_data_open.mjs
@@ -1,4 +1,5 @@
 import { user_data_path } from "../../../love/public/src/user_data_path.mjs";
 export function user_data_open() {
-  return user_data_path();
+  let f_path = user_data_path();
+  return f_path;
 }
