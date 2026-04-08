a924d7081543d19698e1cac273758ad784c81c41
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:14:34 -0400

    function_auto_path ../love/public/src/function_wrap_generic.mjs

--- public/src/function_wrap_generic.mjs
+++ public/src/function_wrap_generic.mjs
@@ -1,13 +1,12 @@
-import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
-import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
-import { function_run } from "../../../love/public/src/function_run.mjs";
-import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
+import { function_name_new_get } from "../../../love/public/src/function_name_new_get.mjs";
 import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_generic(plugin_fn, f_name_old, args) {
-  f_name_old = await function_name_unalias_only(f_name_old);
-  let split = text_split_comma(args);
-  let args_run = list_concat_single(f_name_old, split);
-  let f_name_new = await function_run(plugin_fn, args_run);
+  let f_name_new = null;
+  ({ f_name_new, f_name_old } = await function_name_new_get(
+    f_name_old,
+    args,
+    plugin_fn,
+  ));
   let r = await function_wrap(f_name_old, f_name_new);
   return r;
 }
