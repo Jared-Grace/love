35c63eeb1ef6912ddfd4c1993e1c61915b5bdab1
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:54:56 -0400

    function_auto_path ../love/public/src/function_node_select.mjs

--- public/src/function_node_select.mjs
+++ public/src/function_node_select.mjs
@@ -1,3 +1,4 @@
+import { data_transform } from "../../../love/public/src/data_transform.mjs";
 import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
 import { js_visit_id } from "../../../love/public/src/js_visit_id.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
@@ -5,4 +6,11 @@ export async function function_node_select(select_fn_name) {
   let ast = await function_current_ast();
   let result = await function_run(select_fn_name, [ast]);
   let id = js_visit_id(ast, result);
+  async function lambda(previous) {}
+  let value = await data_transform(
+    property_name,
+    value_initial,
+    lambda,
+    d_path,
+  );
 }
