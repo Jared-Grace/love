0a24cb64c75255a0c9b1f5fe88bc1bfcd78b388e
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 20:45:06 -0400

    function_auto_path ../love/public/src/property_set_if_exists_not_get.mjs

--- public/src/property_set_if_exists_not_get.mjs
+++ public/src/property_set_if_exists_not_get.mjs
@@ -1,1 +1,5 @@
-export function property_set_if_exists_not_get() {}
+import { property_set_if_exists_not } from "../../../love/public/src/property_set_if_exists_not.mjs";
+export function property_set_if_exists_not_get(a, p, value) {
+  let r = property_set_if_exists_not(a, p, value);
+  return r;
+}
