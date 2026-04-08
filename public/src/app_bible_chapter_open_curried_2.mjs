f2ccd45eed6bd7e534c9d5976185a30fdd387f2d
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:18:45 -0400

    pp 13

--- public/src/app_bible_chapter_open_curried_2.mjs
+++ public/src/app_bible_chapter_open_curried_2.mjs
@@ -1,1 +1,6 @@
-export function app_bible_chapter_open_curried_2() {}
+import { app_bible_chapter_open } from "../../../love/public/src/app_bible_chapter_open.mjs";
+export function app_bible_chapter_open_curried_2(hash, context) {
+  return function app_bible_chapter_open_curried_2_result(chapter_code) {
+    return app_bible_chapter_open(hash, context, chapter_code);
+  };
+}
