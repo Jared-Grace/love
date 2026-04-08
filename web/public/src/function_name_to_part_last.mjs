a2c6df842cb70d654941c399fbb6cddfb1a61aa9
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:30:24 -0400

    function_auto_path ../love/public/src/function_name_to_part_last.mjs

--- web/public/src/function_name_to_part_last.mjs
+++ web/public/src/function_name_to_part_last.mjs
@@ -1,7 +1,7 @@
-import { list_last } from "../../../love/public/src/list_last.mjs";
-import { function_name_to_parts } from "../../../love/public/src/function_name_to_parts.mjs";
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
+import { function_name_to_parts_last_delete } from "../../../love/public/src/function_name_to_parts_last_delete.mjs";
 export function function_name_to_part_last(f_name_before) {
-  let parts = function_name_to_parts(f_name_before);
-  let last = list_last(parts);
-  return last;
+  let parts = function_name_to_parts_last_delete(f_name_old);
+  let f_name_new = function_name_combine_multiple(parts);
+  return f_name_new;
 }
