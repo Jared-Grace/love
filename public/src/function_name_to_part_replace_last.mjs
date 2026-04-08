deb38cee45c9bcc9bcb9c4bde9cfe8041e1b3f13
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 16:02:54 -0400

    function_auto_path ../love/public/src/function_name_to_part_replace_last.mjs

--- public/src/function_name_to_part_replace_last.mjs
+++ public/src/function_name_to_part_replace_last.mjs
@@ -5,10 +5,9 @@ import { function_name_combine_multiple } from "../../../love/public/src/functio
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function function_name_to_part_replace_last(f_name_old, replacement) {
   let count = 1;
-  let parts2 = function_name_to_parts(f_name_old);
+  let parts = function_name_to_parts(f_name_old);
   let e = list_remove_last_multiple(list, count);
-  list_pop(parts2);
-  let parts = parts2;
+  list_pop(parts);
   list_add(parts, replacement);
   let f_name_new = function_name_combine_multiple(parts);
   return f_name_new;
