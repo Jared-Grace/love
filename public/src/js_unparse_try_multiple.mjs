e6dd6e24670a7f134faef1a9bd44acef84255ab8
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:46:09 -0400

    function_auto_path ../love/public/src/js_unparse_try_multiple.mjs

--- public/src/js_unparse_try_multiple.mjs
+++ public/src/js_unparse_try_multiple.mjs
@@ -1,6 +1,6 @@
 import { js_unparse_try } from "../../../love/public/src/js_unparse_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function js_unparse_try_multiple(selected) {
-  let mapped = list_map(selected, js_unparse_try);
+export function js_unparse_try_multiple(list) {
+  let mapped = list_map(list, js_unparse_try);
   return mapped;
 }
