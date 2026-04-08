e9bf11e8fd4b76c418395d07d3e4262439842a1b
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:17:02 -0400

    de list_skip_end_count_curried_right

--- web/public/src/list_skip_end_count_curried_right.mjs
+++
@@ -1,8 +0,0 @@
-import { list_skip_end_count } from "../../../love/public/src/list_skip_end_count.mjs";
-export function list_skip_end_count_curried_right(count) {
-  let r = function list_skip_end_count_curried_right_result(list) {
-    let result = list_skip_end_count(list, count);
-    return result;
-  };
-  return r;
-}
