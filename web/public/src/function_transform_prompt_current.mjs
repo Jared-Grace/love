55631f24bfed56462954902d4e6cfb1d85d1d2dd
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:01:59 -0400

    fwa nc ftp current

---
+++ web/public/src/function_transform_prompt_current.mjs
@@ -0,0 +1,7 @@
+import { function_transform_prompt } from "../../../love/public/src/function_transform_prompt.mjs";
+export async function function_transform_prompt_current(
+  transformer_name,
+  f_name,
+) {
+  return await function_transform_prompt(transformer_name, f_name);
+}
