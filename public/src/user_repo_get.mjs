ab1ec2477d3db26804cc0e939705d4101cf2c73e
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:11:58 -0400

    function_auto_path ../love/public/src/user_repo_get.mjs

--- public/src/user_repo_get.mjs
+++ public/src/user_repo_get.mjs
@@ -1,10 +1,8 @@
 import { user_data_get } from "../../../love/public/src/user_data_get.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { repo_exists_assert } from "../../../love/public/src/repo_exists_assert.mjs";
 export async function user_repo_get() {
   const property_name = "repo_current";
-  let v = await user_data_get(property_name);
-  let repo_name = property_get(v, "value");
+  let repo_name = await user_data_get(property_name);
   await repo_exists_assert(repo_name);
   return repo_name;
 }
