3ef4e4f4f1ef7b64f38fb9d0363db23b98c9c2b5
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:46:57 -0400

    function_auto_path ../love/public/src/app_bible_chapters.mjs

---
+++ web/public/src/html_button_list.mjs
@@ -0,0 +1,14 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { html_button } from "../../../love/public/src/html_button.mjs";
+export function html_button_list(item_to_text, on_click, parent, items) {
+  function lambda(item) {
+    let text = item_to_text(item);
+    function lambda2() {
+      on_click(item);
+    }
+    let component = html_button(parent, text, lambda2);
+    return component;
+  }
+  let mapped = list_map(items, lambda);
+  return mapped;
+}
