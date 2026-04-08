9d3bafac485cf864b011f7e428607c2a90a690d2
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:14:43 -0400

    function_auto_path ../love/public/src/list_skip_end_count_sandbox.mjs

--- public/src/list_skip_end_count_sandbox.mjs
+++ public/src/list_skip_end_count_sandbox.mjs
@@ -1,1 +1,4 @@
-export function list_skip_end_count_sandbox() {}
+import { range_1 } from "../../../love/public/src/range_1.mjs";
+export function list_skip_end_count_sandbox() {
+  let r = range_1(count);
+}
