b31f48bf0c0c3153e38b8a9e3d756dc496c47496
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 22:38:03 -0400

    function_auto_path ../love/public/src/app_shared_screen_set_button.mjs

--- web/public/src/app_shared_screen_set_button.mjs
+++ web/public/src/app_shared_screen_set_button.mjs
@@ -2,5 +2,6 @@ import { html_button } from "../../../love/public/src/html_button.mjs";
 import { app_shared_screen_set_fn } from "../../../love/public/src/app_shared_screen_set_fn.mjs";
 export function app_shared_screen_set_button(context, screen_fn, bar, text) {
   let f = app_shared_screen_set_fn(context, screen_fn);
-  let component2 = html_button(bar, text, f);
+  let b = html_button(bar, text, f);
+  return b;
 }
