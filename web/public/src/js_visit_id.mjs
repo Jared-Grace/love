f3fd4098e7fa45eafac8db5cb837ded47235af00
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:49:49 -0400

    function_auto_path ../love/public/src/js_statement_first.mjs

---
+++ web/public/src/js_visit_id.mjs
@@ -0,0 +1,15 @@
+import { js_visit } from "../../../love/public/src/js_visit.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export function js_visit_id(first, ast) {
+  let result = null;
+  let i = 0;
+  function lambda(v) {
+    let node = property_get(v, "node");
+    if (equal(node, first)) {
+      result = i;
+    }
+    i++;
+  }
+  js_visit(ast, lambda);
+}
