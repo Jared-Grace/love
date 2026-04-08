5d2a67b3cdb84b017af7b1f628d5fd448c56c8f5
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:21:33 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,8 @@
+import { js_statement_first } from "../../../love/public/src/js_statement_first.mjs";
 import { function_node_select } from "../../../love/public/src/function_node_select.mjs";
 import { app_bible_verses } from "../../../love/public/src/app_bible_verses.mjs";
 import { function_open } from "../../../love/public/src/function_open.mjs";
 export async function sandbox() {
   await function_open(app_bible_verses.name);
-  let r = await function_node_select(select_fn_name);
+  let r = await function_node_select(js_statement_first.name);
 }
