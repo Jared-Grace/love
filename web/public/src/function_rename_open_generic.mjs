8a43660b58bde52b09b4d27993c474f71abdd81a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:01:34 -0400

    fwa nc rn generic

---
+++ web/public/src/function_rename_open_generic.mjs
@@ -0,0 +1,7 @@
+import { function_rename_open } from "../../../love/public/src/function_rename_open.mjs";
+export async function function_rename_open_generic(
+  f_name_before,
+  f_name_after,
+) {
+  return await function_rename_open(f_name_before, f_name_after);
+}
