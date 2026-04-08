e163e020cec05a627a9016c5b2f5cd476381d942
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:19:50 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -1,4 +1,5 @@
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 export async function sandbox() {
-  let split = text_split_comma(t);
+  let split = text_split_comma("");
+  return split;
 }
