153ad49e0ae2f292abeab25e514452303326df96
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:38:53 -0400

    fwa nc function_param_swap_beginning multiple

--- public/src/function_param_swap_beginning_multiple.mjs
+++ public/src/function_param_swap_beginning_multiple.mjs
@@ -1,1 +1,4 @@
-export function function_param_swap_beginning_multiple() {}
+import { function_param_swap_beginning } from "../../../love/public/src/function_param_swap_beginning.mjs";
+export async function function_param_swap_beginning_multiple(f_name) {
+  return await function_param_swap_beginning(f_name);
+}
