416324894929aab33abcb99c71c214fbd00917c2
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:38:19 -0400

    function_auto_path ../love/public/src/js_statement_first.mjs

--- web/public/src/js_statement_first.mjs
+++ web/public/src/js_statement_first.mjs
@@ -1,3 +1,4 @@
+import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
@@ -5,5 +6,7 @@ export function js_statement_first(ast) {
   let body_block = js_flo_body(ast);
   let first = list_first(body_block);
   let code = js_unparse(first);
+  function lambda(v) {}
+  js_visit(ast2, lambda);
   return code;
 }
