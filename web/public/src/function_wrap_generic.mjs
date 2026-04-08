d0c6ffb344ebb7a07a35618b44cd6d4d09518619
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:59:27 -0400

    function_auto_path ../love/public/src/function_wrap_generic.mjs

--- web/public/src/function_wrap_generic.mjs
+++ web/public/src/function_wrap_generic.mjs
@@ -1,3 +1,4 @@
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
@@ -5,7 +6,8 @@ import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
 export async function function_wrap_generic(plugin_fn, f_name_old, args) {
   f_name_old = await function_name_unalias_only(f_name_old);
   let split = text_split_comma(args);
-  let args_run = [f_name_old, args];
+  let args_run = [f_name_old];
+  list_add_multiple(list, items);
   let f_name_new = await function_run(plugin_fn, [f_name_old, args]);
   let r = await function_wrap(f_name_old, f_name_new);
   return r;
