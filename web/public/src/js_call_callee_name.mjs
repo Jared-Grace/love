4e60839cf15f7dda3ba9cf11b41dd252b407fe77
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 18:15:17 -0400

    function_auto_path ../love/public/src/js_call_callee_name.mjs

--- web/public/src/js_call_callee_name.mjs
+++ web/public/src/js_call_callee_name.mjs
@@ -1,12 +1,8 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { js_node_type_not_is } from "../../../love/public/src/js_node_type_not_is.mjs";
 import { js_identifier_not_is } from "../../../love/public/src/js_identifier_not_is.mjs";
 import { property_get_name } from "../../../love/public/src/property_get_name.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_call_callee_name(expression) {
-  log(js_call_callee_name.name, {
-    expression,
-  });
   let name = null;
   let jin2 = js_node_type_not_is(expression, "CallExpression");
   if (jin2) {
