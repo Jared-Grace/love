8f8f619f02c2e9a5ac5e28bf8a8a7f6c5d63f5a4
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 15:56:11 -0400

    function_auto_path ../love/public/src/function_node_select.mjs

--- public/src/function_node_select.mjs
+++ public/src/function_node_select.mjs
@@ -1,3 +1,4 @@
+import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { data_transform } from "../../../love/public/src/data_transform.mjs";
 import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
@@ -11,6 +12,7 @@ export async function function_node_select(select_fn_name) {
     list_add(previous, id);
     return previous;
   }
+  let d_path = user_repo_path();
   let value = await data_transform(
     "function_current_selects",
     [],
