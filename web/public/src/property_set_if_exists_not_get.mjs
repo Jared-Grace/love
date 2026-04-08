bf3be95cbe9895c1878929b015db0792c8a32b2e
JESUS rose to life <username@users.noreply.github.com>
2026-04-07 20:48:19 -0400

    de property_set_if_exists_not_get

--- web/public/src/property_set_if_exists_not_get.mjs
+++
@@ -1,7 +0,0 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { property_set_if_exists_not } from "../../../love/public/src/property_set_if_exists_not.mjs";
-export function property_set_if_exists_not_get(a, p, value) {
-  property_set_if_exists_not(a, p, value);
-  let value = property_get(hash, p);
-  return value;
-}
