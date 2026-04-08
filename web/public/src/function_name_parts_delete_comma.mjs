ae692bbd340d95f8b683804b5e9937645794bf0a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 13:01:34 -0400

    function_auto_path ../love/public/src/function_name_parts_delete_comma.mjs

--- web/public/src/function_name_parts_delete_comma.mjs
+++ web/public/src/function_name_parts_delete_comma.mjs
@@ -1,6 +1,6 @@
 import { function_name_parts_delete } from "../../../love/public/src/function_name_parts_delete.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
-export function function_name_parts_remove_comma(f_name_old, deleteds_comma) {
+export function function_name_parts_delete_comma(f_name_old, deleteds_comma) {
   let split = text_split_comma(deleteds_comma);
   let f_name_new = function_name_parts_delete(f_name_old, split);
   return f_name_new;
