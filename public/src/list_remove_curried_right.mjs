b045683a5bfdf646034bca3ef4c13740ea518e0e
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 19:34:05 -0400

    function_auto_path ../love/public/src/list_remove_curried_right.mjs

--- public/src/list_remove_curried_right.mjs
+++ public/src/list_remove_curried_right.mjs
@@ -1,6 +1,8 @@
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 export function list_remove_curried_right(item) {
-  return function list_remove_curried_right_result(list) {
-    return list_remove(list, item);
+  let r2 = function list_remove_curried_right_result(list) {
+    let r = list_remove(list, item);
+    return r;
   };
+  return r2;
 }
