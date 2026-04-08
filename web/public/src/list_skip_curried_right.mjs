10c35e24ae668f77c8299c709776a00319e6f264
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:17:38 -0400

    function_auto_path ../love/public/src/list_skip_curried_right.mjs

--- web/public/src/list_skip_curried_right.mjs
+++ web/public/src/list_skip_curried_right.mjs
@@ -1,6 +1,8 @@
 import { list_skip } from "../../../love/public/src/list_skip.mjs";
 export function list_skip_curried_right(skip_count) {
-  return function list_skip_curried_right_result(list) {
-    return list_skip(list, skip_count);
+  let r = function list_skip_curried_right_result(list) {
+    let skipped = list_skip(list, skip_count);
+    return skipped;
   };
+  return r;
 }
