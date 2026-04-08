1c559b2a42a017f6abf293621fe7163537fee9ad
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:33:22 -0400

    function_auto_path ../love/public/src/function_copy_open_generic_args.mjs

--- public/src/function_copy_open_generic_args.mjs
+++ public/src/function_copy_open_generic_args.mjs
@@ -1,4 +1,5 @@
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 export async function function_copy_open_generic_args(f_name_old, f_name_new) {
-  return await function_copy_open(f_name_old, f_name_new);
+  let r = await function_copy_open(f_name_old, f_name_new);
+  return r;
 }
