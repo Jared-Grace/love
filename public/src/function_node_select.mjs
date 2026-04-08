5fc3a3c92656c209a271fcb5c52d8d3639907e12
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 17:02:35 -0400

    function_auto_path ../love/public/src/function_node_select.mjs

--- public/src/function_node_select.mjs
+++ public/src/function_node_select.mjs
@@ -1,33 +1,7 @@
-import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
-import { js_unparse_multiple } from "../../../love/public/src/js_unparse_multiple.mjs";
-import { js_visit_ids_to_nodes } from "../../../love/public/src/js_visit_ids_to_nodes.mjs";
-import { user_repo_path } from "../../../love/public/src/user_repo_path.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { data_transform } from "../../../love/public/src/data_transform.mjs";
+import { function_node_select_inner } from "../../../love/public/src/function_node_select_inner.mjs";
 import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
-import { js_visit_id } from "../../../love/public/src/js_visit_id.mjs";
-import { function_run } from "../../../love/public/src/function_run.mjs";
 export async function function_node_select(select_fn_name) {
   let ast = await function_current_ast();
-  let node = await function_run(select_fn_name, [ast]);
-  let id = js_visit_id(ast, node);
-  async function lambda(previous) {
-    list_add(previous, id);
-    return previous;
-  }
-  let d_path = user_repo_path();
-  let value = await data_transform(
-    "function_current_selects",
-    [],
-    lambda,
-    d_path,
-  );
-  let selected = js_visit_ids_to_nodes(ast, value);
-  let selected_code = js_unparse_multiple(selected);
-  let f_name_current = await function_current_get();
-  let r = {
-    f_name_current,
-    selected_code,
-  };
+  let r = await function_node_select_inner(select_fn_name, ast);
   return r;
 }
