8973521e4439592918c4f7755f588841d4937eb5
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 12:22:10 -0400

    fw fw part_insert_at

--- public/src/part_insert_at.mjs
+++ public/src/part_insert_at.mjs
@@ -1,1 +1,4 @@
-export function part_insert_at() {}
+import { function_wrap } from "../../../love/public/src/function_wrap.mjs";
+export async function part_insert_at(f_name, f_name_wrapped) {
+  return await function_wrap(f_name, f_name_wrapped);
+}
