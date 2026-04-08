221480346f66a7c7dc54356bb7d734175b62b400
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:55:59 -0400

    function_auto_path ../love/public/src/js_statement_first.mjs

--- web/public/src/js_statement_first.mjs
+++ web/public/src/js_statement_first.mjs
@@ -1,1 +1,4 @@
-export function js_statement_first(ast) {}
+import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
+export function js_statement_first(ast) {
+  let body_block = js_flo_body(ast2);
+}
