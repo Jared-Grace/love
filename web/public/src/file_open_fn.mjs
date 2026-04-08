b6f5ef910976c8e6895a6ba032e4d7ed83f4355a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 14:46:10 -0400

    function_auto_path ../love/public/src/file_open_fn.mjs

--- web/public/src/file_open_fn.mjs
+++ web/public/src/file_open_fn.mjs
@@ -1,4 +1,5 @@
 import { file_open } from "../../../love/public/src/file_open.mjs";
 export async function file_open_fn(f_path) {
-  return await file_open(f_path);
+  let r = await file_open(f_path);
+  return r;
 }
