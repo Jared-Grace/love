30a82d883a8f897d2ca72cb5b0e20c12bb422f8c
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:07:58 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

---
+++ public/src/html_button_biblehub_open.mjs
@@ -0,0 +1,31 @@
+import { html_button } from "../../../love/public/src/html_button.mjs";
+import { window_open } from "../../../love/public/src/window_open.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
+import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
+export function html_button_biblehub_open(
+  folder,
+  ending,
+  verse_number_v,
+  bottom,
+  button_text,
+) {
+  function lambda10() {
+    let replaced = text_replace_space_underscore_lower(book_name);
+    if (equal(replaced, "song")) {
+      replaced = "songs";
+    }
+    window_open(
+      "https://biblehub.com/" +
+        folder +
+        replaced +
+        "/" +
+        chapter_name +
+        "-" +
+        verse_number_v +
+        ".htm" +
+        ending,
+    );
+  }
+  let component5 = html_button(bottom, button_text, lambda10);
+  return component5;
+}
