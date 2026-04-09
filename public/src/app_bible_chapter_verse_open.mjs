1d18f3d3fc40a7a88350ab3a96120f4ff2ac3150
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 17:31:15 -0400

    pf app_bible_chapter_verse_open context

--- public/src/app_bible_chapter_verse_open.mjs
+++ public/src/app_bible_chapter_verse_open.mjs
@@ -1,6 +1,6 @@
 import { app_bible_verse_open } from "../../../love/public/src/app_bible_verse_open.mjs";
 import { app_bible_chapter_set } from "../../../love/public/src/app_bible_chapter_set.mjs";
-export function app_bible_chapter_verse_open(chapter, context, verse_number) {
+export function app_bible_chapter_verse_open(context, chapter, verse_number) {
   app_bible_chapter_set(chapter);
   app_bible_verse_open(context, verse_number);
 }
