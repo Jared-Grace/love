edc21f6639607f2452968f528a70c2ff9eae8fa6
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:06:59 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- public/src/js_statement_duplicate.mjs
+++ public/src/js_statement_duplicate.mjs
@@ -1,4 +1,6 @@
+import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
 export function js_statement_duplicate(id) {
   let node = js_visit_id_to_node(ast, id2);
+  let v_match = js_node_to_visitor(ast2, node_search);
 }
