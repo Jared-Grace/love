36378263ca2b492fe574d5929c664b9acda1efda
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 14:30:06 -0400

    function_auto_path ../love/public/src/app_bible_screens.mjs

--- public/src/app_bible_screens.mjs
+++ public/src/app_bible_screens.mjs
@@ -1,7 +1,6 @@
 import { app_bible_screens_base } from "../../../love/public/src/app_bible_screens_base.mjs";
 import { app_bible_home } from "../../../love/public/src/app_bible_home.mjs";
-export function app_bible_screens() {$a
-  let a = [app_bible_home];
-  let screens = app_bible_screens_base(a);
+export function app_bible_screens() {
+  let screens = app_bible_screens_base([app_bible_home]);
   return screens;
 }
