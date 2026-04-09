3796fa78117bff4b19052e0e188c5117d3677c32
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:52:06 -0400

    function_auto_path ../love/public/src/function_node_select.mjs

--- web/public/src/function_node_select.mjs
+++ web/public/src/function_node_select.mjs
@@ -1,5 +1,7 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_node_select_args } from "../../../love/public/src/function_node_select_args.mjs";
 export async function function_node_select(select_fn_name) {
+  arguments_assert(arguments, 1);
   let r5 = await function_node_select_args(select_fn_name, null);
   return r5;
 }
