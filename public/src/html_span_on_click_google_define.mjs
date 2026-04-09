13e582c97cee9e0c99c110b1c6aa572b561bddbc
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:29:46 -0400

    pne html_span_on_click_google_define search_term

--- public/src/html_span_on_click_google_define.mjs
+++ public/src/html_span_on_click_google_define.mjs
@@ -1,7 +1,7 @@
 import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
 import { window_open_google_define_later } from "../../../love/public/src/window_open_google_define_later.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-export function html_span_on_click_google_define(parent, item) {
+export function html_span_on_click_google_define(parent, item, search_term) {
   let item_span = html_span_text(parent, item);
   let lambda9 = window_open_google_define_later(item);
   html_on_click(item_span, lambda9);
