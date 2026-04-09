cd80d8870f0b6201d47283da2f3c7fb43860654e
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:49:54 -0400

    function_auto_path ../love/public/src/list_adder_single.mjs

--- public/src/list_adder_single.mjs
+++ public/src/list_adder_single.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_adder_single(lambda) {
   let list = list_adder(lambda);
+  log(list_adder_single.name, {
+    list,
+  });
   let only = list_single(list);
   return only;
 }
