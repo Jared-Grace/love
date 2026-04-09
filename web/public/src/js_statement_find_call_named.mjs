12d6153245e33b55a3c38cf51e88baa2fe896508
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:57:25 -0400

    function_auto_path ../love/public/src/js_call_named.mjs

--- web/public/src/js_statement_find_call_named.mjs
+++ web/public/src/js_statement_find_call_named.mjs
@@ -1,7 +1,7 @@
-import { js_visit_call_named } from "../../../love/public/src/js_visit_call_named.mjs";
+import { js_call_named } from "../../../love/public/src/js_call_named.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function js_statement_find_call_named(ast, f_name) {
   let unaliased = await function_name_unalias_only(f_name);
-  let only = js_visit_call_named(ast, unaliased);
+  let only = js_call_named(ast, unaliased);
   return only;
 }
