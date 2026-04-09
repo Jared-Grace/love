e101b5419b373d7664767f8e0a91226fc858b9af
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 14:30:02 -0400

    function_auto_path ../love/public/src/app_bible_screens.mjs

--- web/public/src/app_bible_screens.mjs
+++ web/public/src/app_bible_screens.mjs
@@ -1,16 +1,7 @@
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
-import { app_bible_verses } from "../../../love/public/src/app_bible_verses.mjs";
-import { app_bible_languages } from "../../../love/public/src/app_bible_languages.mjs";
-import { app_bible_chapters } from "../../../love/public/src/app_bible_chapters.mjs";
-import { app_bible_books } from "../../../love/public/src/app_bible_books.mjs";
+import { app_bible_screens_base } from "../../../love/public/src/app_bible_screens_base.mjs";
 import { app_bible_home } from "../../../love/public/src/app_bible_home.mjs";
 export function app_bible_screens() {
   let a = [app_bible_home];
-  let screens = list_concat(a, [
-    app_bible_books,
-    app_bible_chapters,
-    app_bible_verses,
-    app_bible_languages,
-  ]);
+  let screens = app_bible_screens_base(a);
   return screens;
 }
