42e8fe6c4c4ce3a4dcee7459ac6a84445ec1af3d
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:50:27 -0400

    frp js_statement_return_empty_add_argument_set js_statement_return_add

--- public/src/js_statement_return_empty_add_argument_set.mjs
+++
@@ -1,6 +0,0 @@
-import { js_return_argument_set } from "../../../love/public/src/js_return_argument_set.mjs";
-import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
-export function js_statement_return_empty_add_argument_set(body_block, e) {
-  let r = js_statement_return_empty_add(body_block);
-  js_return_argument_set(r, e);
-}
