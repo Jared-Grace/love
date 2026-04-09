88991080f2cad984990b7ae705e3e553f4ec8fef
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:49:06 -0400

    function_auto_path ../love/public/src/function_param_move_first_curried.mjs

--- web/public/src/function_param_move_first_curried.mjs
+++ web/public/src/function_param_move_first_curried.mjs
@@ -1,6 +1,8 @@
 import { function_param_move_first } from "../../../love/public/src/function_param_move_first.mjs";
 export async function function_param_move_first_curried(f_name) {
-  return async function function_param_move_first_curried_result(param_name) {
-    return await function_param_move_first(f_name, param_name);
+  let r2 = async function function_param_move_first_curried_result(param_name) {
+    let r = await function_param_move_first(f_name, param_name);
+    return r;
   };
+  return r2;
 }
