e50fabffda12b71b5fdd9aa3a5eeb3223d13dd73
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:02:36 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- web/public/src/function_node_select_inner.mjs
+++ web/public/src/function_node_select_inner.mjs
@@ -14,7 +14,7 @@ export async function function_node_select_inner(
   let args = null;
   if (null_is(args_comma)) {
     args = [];
-  } else {
+  } else {text_split_dot_comma
   }
   let node = js_visit_id_to_node(ast, node_id);
   let concated = list_concat_single(node, args);
