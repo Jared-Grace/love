511c34388d998fc4a720fac3355821f1b04832e0
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:46:15 -0400

    function_auto_path ../love/public/src/dictionary_functions_to_names.mjs

---
+++ public/src/object_values_map_self.mjs
@@ -0,0 +1,9 @@
+import { each_object } from "../../../love/public/src/each_object.mjs";
+import { property_set } from "../../../love/public/src/property_set.mjs";
+export function object_values_map_self(r, overrides) {
+  function lambda(value, property) {
+    let value_new = r(value);
+    property_set(overrides, property, value_new);
+  }
+  each_object(overrides, lambda);
+}
