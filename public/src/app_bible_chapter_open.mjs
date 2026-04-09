d35d19f83dd1fa9bb7308b8edb3c2ed031fdee9f
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:34:05 -0400

    function_auto_path ../love/public/src/app_bible_chapter_open.mjs

--- public/src/app_bible_chapter_open.mjs
+++ public/src/app_bible_chapter_open.mjs
@@ -1,7 +1,9 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { app_bible_verses } from "../../../love/public/src/app_bible_verses.mjs";
 import { app_bible_chapter_set } from "../../../love/public/src/app_bible_chapter_set.mjs";
 export function app_bible_chapter_open(context, chapter_code) {
+  arguments_assert(arguments, 2);
   app_bible_chapter_set(chapter_code);
   app_shared_screen_set(context, app_bible_verses);
 }
