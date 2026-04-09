97038e5d60c4af59cf2cb84bff8a4bd2360ea1a8
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:28:56 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

---
+++ public/src/html_button_biblehub_open_commentary.mjs
@@ -0,0 +1,17 @@
+import { html_button_biblehub_open } from "../../../love/public/src/html_button_biblehub_open.mjs";
+export function html_button_biblehub_open_commentary(
+  verse_number,
+  bottom,
+  book_name,
+  chapter_name,
+) {
+  html_button_biblehub_open(
+    "",
+    "#commentary",
+    verse_number,
+    bottom,
+    "Commentary",
+    book_name,
+    chapter_name,
+  );
+}
---
+++ public/src/html_button_biblehub_open_parallel.mjs
@@ -0,0 +1,17 @@
+import { html_button_biblehub_open } from "../../../love/public/src/html_button_biblehub_open.mjs";
+export function html_button_biblehub_open_parallel(
+  verse_number,
+  bottom,
+  book_name,
+  chapter_name,
+) {
+  html_button_biblehub_open(
+    "",
+    "",
+    verse_number,
+    bottom,
+    "Parallel",
+    book_name,
+    chapter_name,
+  );
+}
