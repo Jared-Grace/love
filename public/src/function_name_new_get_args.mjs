22fecfa11d35ca1e4bbd39ef2502d47f50b2ac21
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:38:59 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- public/src/function_name_new_get_args.mjs
+++ public/src/function_name_new_get_args.mjs
@@ -1,5 +1,4 @@
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { property_get_or_null } from "../../../love/public/src/property_get_or_null.mjs";
+import { property_exists_if_get } from "../../../love/public/src/property_exists_if_get.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
@@ -12,10 +11,7 @@ export async function function_name_new_get_args(plugin_fn, f_name_old, args) {
   let overrides = {
     c: function_name_combine,
   };
-  let v = property_get_or_null(overrides, plugin_fn);
-  if (null_not_is(v)) {
-    plugin_fn = v;
-  }
+  plugin_fn = property_exists_if_get(overrides, plugin_fn);
   let f_name_new = await function_run(plugin_fn, args_run);
   let r2 = {
     f_name_new,
