93d93fbbd72ebf1492c173c1307ae493f9bc19b5
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 13:34:22 -0400

    function_auto_path ../love/public/src/function_copy_replace.mjs

--- public/src/function_copy_replace.mjs
+++ public/src/function_copy_replace.mjs
@@ -2,7 +2,7 @@ import { function_name_unalias_only } from "../../../love/public/src/function_na
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 export async function function_copy_replace(f_name_old, from, to) {
-  f_name_old= await function_name_unalias_only(f_name_old);
+  f_name_old = await function_name_unalias_only(f_name_old);
   let f_name_new = text_replace(f_name_old, from, to);
   let v = await function_copy_open(f_name_old, f_name_new);
   return v;
