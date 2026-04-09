f7c2c309b67e59f02130ce31a8e936e649803f5d
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 17:04:35 -0400

    function_auto_path ../love/public/src/list_previous_try.mjs

---
+++ public/src/list_previous_try.mjs
@@ -0,0 +1,7 @@
+import { list_next_try_generic } from "../../../love/public/src/list_next_try_generic.mjs";
+import { list_index_of_next } from "../../../love/public/src/list_index_of_next.mjs";
+export function list_previous_try(list, item) {
+  let fn = list_index_of_next;
+  let next = list_next_try_generic(fn, list, item);
+  return next;
+}
