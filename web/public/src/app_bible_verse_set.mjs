32f55c0598d870c78a9812c565b1540cbc384e8b
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 13:44:57 -0400

    pd app_bible_verse_set hash

--- web/public/src/app_bible_verse_set.mjs
+++ web/public/src/app_bible_verse_set.mjs
@@ -1,4 +1,4 @@
 import { html_hash_property_set } from "../../../love/public/src/html_hash_property_set.mjs";
-export function app_bible_verse_set(hash, verse_number) {
+export function app_bible_verse_set(verse_number) {
   html_hash_property_set("v", verse_number);
 }
