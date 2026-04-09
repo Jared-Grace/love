5738c8c1a4226832aadc52dc228c5f069d39317a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:11:30 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- public/src/js_statement_duplicate.mjs
+++ public/src/js_statement_duplicate.mjs
@@ -1,4 +1,5 @@
-import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
+import { function_ast } from "../../../love/public/src/function_ast.mjs";
+import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -6,7 +7,8 @@ import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
 import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
 export async function js_statement_duplicate(id) {
-  let ast = await function_current_ast();
+  let f_name_current = await function_current_get();
+  let ast = await function_ast(f_name_current);
   let node = js_visit_id_to_node(ast, id);
   let v = js_node_to_visitor(ast, node);
   let stack = property_get(v, "stack");
