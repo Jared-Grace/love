44559ad8203fe6023b2e26ceb0b4e092e128397c
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:00:11 -0400

    pnd function_node_select_inner args

--- public/src/function_node_select_nested.mjs
+++ public/src/function_node_select_nested.mjs
@@ -6,6 +6,12 @@ export async function function_node_select_nested(select_fn_name) {
   let ast = await function_current_ast();
   let first_id = await function_current_selects_first();
   let r2 = list_remove_curried_right(first_id);
-  let r = await function_node_select_inner(select_fn_name, first_id, r2, ast);
+  let r = await function_node_select_inner(
+    select_fn_name,
+    first_id,
+    r2,
+    ast,
+    args,
+  );
   return r;
 }
