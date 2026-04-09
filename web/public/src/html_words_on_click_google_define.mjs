18aae35cad1834ef54ff964ff9f3b72399595613
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:32:20 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

---
+++ web/public/src/html_words_on_click_google_define.mjs
@@ -0,0 +1,12 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { html_span_on_click_google_define } from "../../../love/public/src/html_span_on_click_google_define.mjs";
+import { html_span_space } from "../../../love/public/src/html_span_space.mjs";
+import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
+export function html_words_on_click_google_define(text, top) {
+  let split = text_split_space(text);
+  function lambda2(item) {
+    html_span_space(top);
+    html_span_on_click_google_define(top, item);
+  }
+  each(split, lambda2);
+}
