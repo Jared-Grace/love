26eccf67eed2fb1e3c9d1247d64bb0063e597da5
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:04:53 -0400

    function_auto_path ../love/public/src/js_node_to_visitor.mjs

--- public/src/js_node_to_visitor.mjs
+++ public/src/js_node_to_visitor.mjs
@@ -1,6 +1,7 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_visit_matches } from "../../../love/public/src/js_visit_matches.mjs";
 export function js_node_to_visitor(ast, node_search) {
   let matches = js_visit_matches(ast, node_search);
-  let v_match = list_fsingle(matches);
+  let v_match = list_single(matches);
   return v_match;
 }
