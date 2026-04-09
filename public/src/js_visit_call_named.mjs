5989e732ec2d9a61210d573bddf567c48b95cfb9
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:55:21 -0400

    function_auto_path ../love/public/src/js_statement_find_call_named.mjs

---
+++ public/src/js_visit_call_named.mjs
@@ -0,0 +1,9 @@
+import { list_adder_single } from "../../../love/public/src/list_adder_single.mjs";
+import { js_visit_calls_named_nodes } from "../../../love/public/src/js_visit_calls_named_nodes.mjs";
+export function js_visit_call_named(ast, unaliased) {
+  function lambda(la) {
+    js_visit_calls_named_nodes(ast, unaliased, la);
+  }
+  let only = list_adder_single(lambda);
+  return only;
+}
