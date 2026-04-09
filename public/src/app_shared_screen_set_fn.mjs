4184443b4a2659abf94174036416065790ec730d
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 14:09:19 -0400

    function_auto_path ../love/public/src/app_shared_screen_set_fn.mjs

--- public/src/app_shared_screen_set_fn.mjs
+++ public/src/app_shared_screen_set_fn.mjs
@@ -1,5 +1,7 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 export function app_shared_screen_set_fn(context, fn) {
+  arguments_assert(arguments, 2);
   let r = function app_shared_screen_set_fn_inner() {
     app_shared_screen_set(context, fn);
   };
