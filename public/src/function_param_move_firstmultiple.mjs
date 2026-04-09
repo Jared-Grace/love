cf60b9b25e73eeb159fd36d026b671e03a124c46
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:34:58 -0400

    wa tc pf multiple

--- public/src/function_param_move_firstmultiple.mjs
+++ public/src/function_param_move_firstmultiple.mjs
@@ -1,1 +1,4 @@
-export function function_param_move_firstmultiple() {}
+import { function_param_move_first } from "../../../love/public/src/function_param_move_first.mjs";
+export async function function_param_move_firstmultiple(f_name, param_name) {
+  return await function_param_move_first(f_name, param_name);
+}
