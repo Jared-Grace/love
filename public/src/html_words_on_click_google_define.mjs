3a8b78836348ff38f2f21cbd8415d888d7b1829e
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:29:48 -0400

    function_auto_path ../love/public/src/html_words_on_click_google_define.mjs

--- public/src/html_words_on_click_google_define.mjs
+++ public/src/html_words_on_click_google_define.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { html_span_on_click_google_define } from "../../../love/public/src/html_span_on_click_google_define.mjs";
 import { html_span_space } from "../../../love/public/src/html_span_space.mjs";
@@ -6,7 +7,8 @@ export function html_words_on_click_google_define(top, text) {
   let split = text_split_space(text);
   function lambda2(item) {
     html_span_space(top);
-    html_span_on_click_google_define(top, item, error());
+    let search_term = error();
+    html_span_on_click_google_define(top, item, search_term);
   }
   each(split, lambda2);
 }
