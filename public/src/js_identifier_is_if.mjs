e099551c15f784b7fed5bb86921017245b192fe3
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:19:17 -0400

    function_auto_path ../love/public/src/js_identifier_is_if.mjs

--- public/src/js_identifier_is_if.mjs
+++ public/src/js_identifier_is_if.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
-export function js_identifier_is_if(only, lambda) {
-  js_node_type_is_if(only, "Identifier", lambda);
+export function js_identifier_is_if(node, lambda) {
+  js_node_type_is_if(node, "Identifier", lambda);
 }
