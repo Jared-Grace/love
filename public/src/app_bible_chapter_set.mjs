dc487f2068b5f1f63618a69790f51329e8fd23a0
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 13:42:28 -0400

    function_auto_path ../love/public/src/app_bible_chapter_set.mjs

--- public/src/app_bible_chapter_set.mjs
+++ public/src/app_bible_chapter_set.mjs
@@ -1,3 +1,4 @@
+import { html_hash_object_get } from "../../../love/public/src/html_hash_object_get.mjs";
 import { html_hash_set_object } from "../../../love/public/src/html_hash_set_object.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 export function app_bible_chapter_set(chapter_code) {
