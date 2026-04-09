6f3516ac88250e29ce459981108328926b0ee21b
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:46:15 -0400

    function_auto_path ../love/public/src/dictionary_functions_to_names.mjs

--- web/public/src/dictionary_functions_to_names.mjs
+++ web/public/src/dictionary_functions_to_names.mjs
@@ -1,14 +1,9 @@
-import { property_set } from "../../../love/public/src/property_set.mjs";
-import { each_object } from "../../../love/public/src/each_object.mjs";
+import { object_values_map_self } from "../../../love/public/src/object_values_map_self.mjs";
 import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
 import { property_get_curried_right } from "../../../love/public/src/property_get_curried_right.mjs";
 export function dictionary_functions_to_names(overrides_fns) {
   let r = property_get_curried_right("name");
   let overrides = object_values_map(overrides_fns, r);
-  function lambda(value, property) {
-    let value_new = r(value);
-    property_set(overrides, property, value_new);
-  }
-  each_object(overrides, lambda);
+  object_values_map_self(r, overrides);
   return overrides;
 }
