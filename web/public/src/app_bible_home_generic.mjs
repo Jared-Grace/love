a75ebc50a14dd5336f977f6a05bc996b45e39dfc
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:23:09 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

--- web/public/src/app_bible_home_generic.mjs
+++ web/public/src/app_bible_home_generic.mjs
@@ -111,10 +111,10 @@ export async function app_bible_home_generic(context, lambda$a) {
     verse_number,
   );
   html_button_biblehub_open_commentary(
+    chapter_name,
     book_name,
     verse_number,
     bottom,
-    chapter_name,
   );
   function lambda8() {}
   let text4 = html_button_copy_text();
