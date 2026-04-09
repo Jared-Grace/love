faac655597786a3a9d7f385e0532b14a239f6d5d
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:01:20 -0400

    function_auto_path ../love/public/src/null_is_if.mjs

--- public/src/null_is_if.mjs
+++ public/src/null_is_if.mjs
@@ -1,7 +1,7 @@
 import { null_is } from "../../../love/public/src/null_is.mjs";
-export function null_is_if(code, ast) {
-  if (null_is(code)) {
-    code = ast;
+export function null_is_if(value, ast) {
+  if (null_is(value)) {
+    value = ast;
   }
-  return code;
+  return value;
 }
