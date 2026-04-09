57997e3eeea52e55f365fc38c09435131d6f50ce
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 23:12:12 -0400

    function_auto_path ../love/public/src/function_transform_current_fn_args.mjs

--- public/src/function_transform_current_fn_args.mjs
+++ public/src/function_transform_current_fn_args.mjs
@@ -1,5 +1,5 @@
 import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
 export async function function_transform_current_fn_args(lambda, args) {
-  let r = await function_transform_current_fn(lambda);
+  let r = await function_transform_current_fn(() => {});
   return r;
 }
