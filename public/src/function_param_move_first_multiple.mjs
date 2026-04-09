8ce1514da3dffa645264855246294361f2138ab0
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:49:37 -0400

    function_auto_path ../love/public/src/function_param_move_first_multiple.mjs

--- public/src/function_param_move_first_multiple.mjs
+++ public/src/function_param_move_first_multiple.mjs
@@ -1,15 +1,11 @@
 import { function_param_move_first_curried } from "../../../love/public/src/function_param_move_first_curried.mjs";
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-import { function_param_move_first } from "../../../love/public/src/function_param_move_first.mjs";
 export async function function_param_move_first_multiple(
   f_name,
   param_names_comma,
 ) {
   let param_names = text_split_dot_comma(param_names_comma);
-  let r2 = await function_param_move_first_curried(f_name2);
-  async function lambda(param_name) {
-    await function_param_move_first(f_name, param_name);
-  }
-  await each_async(param_names, lambda);
+  let r2 = await function_param_move_first_curried(f_name);
+  await each_async(param_names, r2);
 }
