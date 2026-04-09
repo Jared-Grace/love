d62bfb873e1e1f78096be30c37944146c857aa16
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:36:01 -0400

    pd app_bible_chapter_set_default hash

--- public/src/app_bible_chapter_set_default.mjs
+++ public/src/app_bible_chapter_set_default.mjs
@@ -1,6 +1,6 @@
 import { app_bible_chapter_verse_open } from "../../../love/public/src/app_bible_chapter_verse_open.mjs";
 import { property_exists_not } from "../../../love/public/src/property_exists_not.mjs";
-export function app_bible_chapter_set_default(hash, context) {
+export function app_bible_chapter_set_default(context) {
   let n = property_exists_not(hash, "c");
   if (n) {
     app_bible_chapter_verse_open(context, "JHN01", "1");
