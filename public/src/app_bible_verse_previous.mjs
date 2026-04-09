ca9bed7175928d0639a5c52d71271e4bb8006bea
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:17:41 -0400

    function_auto_path ../love/public/src/app_bible_verse_next.mjs

--- public/src/app_bible_verse_previous.mjs
+++ public/src/app_bible_verse_previous.mjs
@@ -2,9 +2,9 @@ import { app_bible_verse_change } from "../../../love/public/src/app_bible_verse
 import { app_bible_chapter_previous } from "../../../love/public/src/app_bible_chapter_previous.mjs";
 import { list_previous_try } from "../../../love/public/src/list_previous_try.mjs";
 export async function app_bible_verse_previous(
+  context,
   chapter_code,
   verse_current,
-  context,
 ) {
   let verse_get = list_previous_try;
   let chapter_change = app_bible_chapter_previous;
