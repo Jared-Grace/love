c9be86aeb01c48a48f3c4ce709c8c0e656a8e14f
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 23:09:14 -0400

    wa nc function_transform_current_fn args

--- web/public/src/function_transform_current_fn_args.mjs
+++ web/public/src/function_transform_current_fn_args.mjs
@@ -1,1 +1,4 @@
-export function function_transform_current_fn_args() {}
+import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
+export async function function_transform_current_fn_args(lambda) {
+  return await function_transform_current_fn(lambda);
+}
