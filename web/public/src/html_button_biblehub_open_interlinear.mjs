1eb388b5270780e72ba9a44e4c2c48b0a9396010
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:23:05 -0400

    function_auto_path ../love/public/src/html_button_biblehub_open_interlinear.mjs

--- web/public/src/html_button_biblehub_open_interlinear.mjs
+++ web/public/src/html_button_biblehub_open_interlinear.mjs
@@ -1,9 +1,9 @@
 import { html_button_biblehub_open } from "../../../love/public/src/html_button_biblehub_open.mjs";
 export function html_button_biblehub_open_interlinear(
-  verse_number,
   bottom,
-  book_name,
   chapter_name,
+  book_name,
+  verse_number,
 ) {
   html_button_biblehub_open(
     bottom,
