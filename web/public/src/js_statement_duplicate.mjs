d9fd6e5ed80198318b5b7c990ea4993780bafe5b
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:01:13 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- web/public/src/js_statement_duplicate.mjs
+++ web/public/src/js_statement_duplicate.mjs
@@ -1,1 +1,4 @@
-export function js_statement_duplicate(id) {}
+import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
+export function js_statement_duplicate(id) {
+  let node = js_visit_id_to_node(ast, id2);
+}
