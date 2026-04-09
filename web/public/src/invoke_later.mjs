15083cc6ec87bc2b01d3df620fd1a3569e264076
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:51:36 -0400

    function_auto_path ../love/public/src/invoke_later.mjs

--- web/public/src/invoke_later.mjs
+++ web/public/src/invoke_later.mjs
@@ -1,6 +1,6 @@
 export function invoke_later(lambda, args) {
   let later = function lambda3() {
-    let result = lambda(args);
+    let result = lambda(...args);
     return result;
   };
   return later;
