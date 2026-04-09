d6a5aceb771b671bb97459cf1f133771309f791c
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 20:59:50 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- web/public/src/function_node_select_inner.mjs
+++ web/public/src/function_node_select_inner.mjs
@@ -1,3 +1,4 @@
+import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
 import { function_current_selects_add } from "../../../love/public/src/function_current_selects_add.mjs";
 import { js_visit_id_or_node } from "../../../love/public/src/js_visit_id_or_node.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
@@ -9,6 +10,7 @@ export async function function_node_select_inner(
   ast,
 ) {
   let node = js_visit_id_to_node(ast, node_id);
+  let concated = list_concat_single(single, list);
   let n = await function_run(select_fn_name, [node]);
   let item_to_add = js_visit_id_or_node(ast, n);
   let r = await function_current_selects_add(item_to_add, on_previous);
