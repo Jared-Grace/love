cf03d6952fcaebeadddcf5543c4d5f5c4787c0c3
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 17:31:15 -0400

    pf app_bible_chapter_verse_open context

--- web/public/src/app_bible_home_generic.mjs
+++ web/public/src/app_bible_home_generic.mjs
@@ -66,7 +66,7 @@ export async function app_bible_home_generic(context, lambda$a) {
   let hash = html_hash_object_get();
   let n = property_exists_not(hash, "c");
   if (n) {
-    app_bible_chapter_verse_open("JHN01", context, verse_number);
+    app_bible_chapter_verse_open(context, "JHN01", verse_number);
     return;
   }
   let verse_number_hash = app_bible_hash_v_get(hash);
