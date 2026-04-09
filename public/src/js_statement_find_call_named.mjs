d1155f6d1a8170389df8ada81c4e99f8f58b61f6
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 21:55:21 -0400

    function_auto_path ../love/public/src/js_statement_find_call_named.mjs

--- public/src/js_statement_find_call_named.mjs
+++ public/src/js_statement_find_call_named.mjs
@@ -1,15 +1,11 @@
-import { js_visit_calls_named_nodes } from "../../../love/public/src/js_visit_calls_named_nodes.mjs";
+import { js_visit_call_named } from "../../../love/public/src/js_visit_call_named.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { list_adder_single } from "../../../love/public/src/list_adder_single.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function js_statement_find_call_named(ast, f_name) {
   let unaliased = await function_name_unalias_only(f_name);
   log(js_statement_find_call_named.name, {
     unaliased,
   });
-  function lambda(la) {
-    js_visit_calls_named_nodes(ast, unaliased, la);
-  }
-  let only = list_adder_single(lambda);
+  let only = js_visit_call_named(ast, unaliased);
   return only;
 }
