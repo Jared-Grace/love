9ddaf0055f217776858caea2e8e9f572430ea647
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:07:58 -0400

    function_auto_path ../love/public/src/js_node_to_visitor.mjs

--- public/src/js_node_to_visitor.mjs
+++ public/src/js_node_to_visitor.mjs
@@ -2,6 +2,6 @@ import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_node_to_visitors } from "../../../love/public/src/js_node_to_visitors.mjs";
 export function js_node_to_visitor(ast, node_search) {
   let matches = js_node_to_visitors(ast, node_search);
-  let v_match = list_single(matches);
-  return v_match;
+  let v = list_single(matches);
+  return v;
 }
