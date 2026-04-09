9d2b63db4f6db78f3c4bfa6cde0ef34544984dd3
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:11:01 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- public/src/js_statement_duplicate.mjs
+++ public/src/js_statement_duplicate.mjs
@@ -1,10 +1,12 @@
+import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
 import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
-export function js_statement_duplicate(id) {
+export async function js_statement_duplicate(id) {
+  let ast2 = await function_current_ast();
   let node = js_visit_id_to_node(ast, id);
   let v = js_node_to_visitor(ast, node);
   let stack = property_get(v, "stack");
