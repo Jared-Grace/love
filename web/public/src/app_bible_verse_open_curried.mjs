3cc8e9b9a4b27609e9a24b3e973ce77a59bda5df
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 13:55:10 -0400

    function_auto_path ../love/public/src/app_bible_verse_open_curried.mjs

--- web/public/src/app_bible_verse_open_curried.mjs
+++ web/public/src/app_bible_verse_open_curried.mjs
@@ -1,6 +1,8 @@
 import { app_bible_verse_open } from "../../../love/public/src/app_bible_verse_open.mjs";
 export function app_bible_verse_open_curried(context) {
-  return function app_bible_verse_open_curried_result(verse_number) {
-    return app_bible_verse_open(context, verse_number);
+  let r2 = function app_bible_verse_open_curried_result(verse_number) {
+    let r = app_bible_verse_open(context, verse_number);
+    return r;
   };
+  return r2;
 }
