8b373b5171d6997de1e46f63145a347ae71c6e39
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 17:02:50 -0400

    function_auto_path ../love/public/src/list_next_try.mjs

--- public/src/list_next_try.mjs
+++ public/src/list_next_try.mjs
@@ -1,12 +1,7 @@
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { list_get } from "../../../love/public/src/list_get.mjs";
+import { list_next_try_generic } from "../../../love/public/src/list_next_try_generic.mjs";
 import { list_index_of_next } from "../../../love/public/src/list_index_of_next.mjs";
 export function list_next_try(list, item) {
   let fn = list_index_of_next;
-  let index_next = fn(list, item);
-  let next = null;
-  if (null_not_is(index_next)) {
-    next = list_get(list, index_next);
-  }
+  let next = list_next_try_generic(fn, list, item);
   return next;
 }
