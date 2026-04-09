73a75aa9d67c4abda7bf24a912fd34746d515a37
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:23:09 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

--- public/src/html_button_biblehub_open_commentary.mjs
+++ public/src/html_button_biblehub_open_commentary.mjs
@@ -1,9 +1,9 @@
 import { html_button_biblehub_open } from "../../../love/public/src/html_button_biblehub_open.mjs";
 export function html_button_biblehub_open_commentary(
+  bottom,
   chapter_name,
   book_name,
   verse_number,
-  bottom,
 ) {
   html_button_biblehub_open(
     bottom,
