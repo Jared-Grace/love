fe65e3892acee164a97858a88b0f9cfd565ef1d3
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:10:07 -0400

    function_auto_path ../love/public/src/app_bible_verse_next.mjs

--- public/src/app_bible_verse_next.mjs
+++ public/src/app_bible_verse_next.mjs
@@ -1,7 +1,5 @@
-import { app_bible_verse_open } from "../../../love/public/src/app_bible_verse_open.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_bible_verse_change } from "../../../love/public/src/app_bible_verse_change.mjs";
 import { app_bible_chapter_next } from "../../../love/public/src/app_bible_chapter_next.mjs";
-import { null_is } from "../../../love/public/src/null_is.mjs";
 import { list_next_try } from "../../../love/public/src/list_next_try.mjs";
 export async function app_bible_verse_next(
   verses,
@@ -11,12 +9,12 @@ export async function app_bible_verse_next(
 ) {
   let verse_get = list_next_try;
   let chapter_change = app_bible_chapter_next;
-  let next = verse_get(verses, verse_current);
-  let ni = null_is(next);
-  if (ni) {
-    await chapter_change(context, chapter_code);
-  } else {
-    let verse_number = property_get(next, "verse_number");
-    app_bible_verse_open(context, verse_number);
-  }
+  await app_bible_verse_change(
+    verse_get,
+    verses,
+    verse_current,
+    chapter_change,
+    context,
+    chapter_code,
+  );
 }
