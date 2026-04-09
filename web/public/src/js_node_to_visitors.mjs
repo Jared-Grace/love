1e2c4b72884ebe08af759093d50290019eb6226b
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:05:36 -0400

    ra lrt js_visit_matches js_node_to_visitors

--- web/public/src/js_node_to_visitors.mjs
+++ web/public/src/js_node_to_visitors.mjs
@@ -1,7 +1,7 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_matches(ast, node_search) {
+export function js_node_to_visitors(ast, node_search) {
   function lambda2(la) {
     function lambda(v) {
       let node = property_get(v, "node");
