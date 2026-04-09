46bbc3adb8924cc4f0ca563067fdd4ad6b55270a
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:10:29 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,7 +1,7 @@
+import { js_statement_duplicate } from "../../../love/public/src/js_statement_duplicate.mjs";
 import { function_node_select_args } from "../../../love/public/src/function_node_select_args.mjs";
 import { js_statement_find_call_named } from "../../../love/public/src/js_statement_find_call_named.mjs";
 import { app_bible_chapters_before } from "../../../love/public/src/app_bible_chapters_before.mjs";
-import { function_open } from "../../../love/public/src/function_open.mjs";
 import { function_current_selects_apply } from "../../../love/public/src/function_current_selects_apply.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
@@ -15,10 +15,10 @@ export async function sandbox() {
     js_statement_find_call_named.name,
     "pg",
   );
+  let r32 = await function_current_selects_apply(js_statement_duplicate.name);
   return r;
   let r2 = await function_node_select_nested(js_call_single.name);
   let r4 = await function_node_select_nested(js_call_callee_name.name);
-  let r32 = await function_current_selects_apply(function_open.name);
   let r3 = {
     r,
     r2,
