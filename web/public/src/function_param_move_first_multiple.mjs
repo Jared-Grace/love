4a117605cee035bc745bf03b72c5c9c59beca3cb
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 18:43:49 -0400

    pp 4

---
+++ web/public/src/function_param_move_first_multiple.mjs
@@ -0,0 +1,4 @@
+import { function_param_move_first } from "../../../love/public/src/function_param_move_first.mjs";
+export async function function_param_move_first_multiple(f_name, param_name) {
+  return await function_param_move_first(f_name, param_name);
+}
