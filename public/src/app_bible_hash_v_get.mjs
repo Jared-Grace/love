ce23775f36bd98ec456963413b466ebb1548770b
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 17:20:58 -0400

    function_auto_path ../love/public/src/app_bible_hash_v_get.mjs

--- public/src/app_bible_hash_v_get.mjs
+++ public/src/app_bible_hash_v_get.mjs
@@ -1,5 +1,9 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { property_initialize } from "../../../love/public/src/property_initialize.mjs";
 export function app_bible_hash_v_get(hash) {
   let value2 = property_initialize(hash, "v", "1");
+  log(app_bible_hash_v_get.name, {
+    value2,
+  });
   return value2;
 }
