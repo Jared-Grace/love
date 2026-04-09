56843fbcd60777e24a1d14fd766498c48eed72ba
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 19:12:51 -0400

    pd app_bible_verse_next verses

--- public/src/app_bible_verse_next.mjs
+++ public/src/app_bible_verse_next.mjs
@@ -2,7 +2,6 @@ import { app_bible_verse_change } from "../../../love/public/src/app_bible_verse
 import { app_bible_chapter_next } from "../../../love/public/src/app_bible_chapter_next.mjs";
 import { list_next_try } from "../../../love/public/src/list_next_try.mjs";
 export async function app_bible_verse_next(
-  verses,
   verse_current,
   context,
   chapter_code,
