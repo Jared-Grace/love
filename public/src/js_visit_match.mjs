3d44bcbeed234dcf4cb20b272d7d62d7b207bcff
JESUS rose to life <username@users.noreply.github.com>
2026-04-08 22:05:36 -0400

    ra lrt js_visit_matches js_node_to_visitors

--- public/src/js_visit_match.mjs
+++ public/src/js_visit_match.mjs
@@ -1,7 +1,7 @@
-import { js_visit_matches } from "../../../love/public/src/js_visit_matches.mjs";
+import { js_node_to_visitors } from "../../../love/public/src/js_node_to_visitors.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_visit_match(ast, node_search) {
-  let matches = js_visit_matches(ast, node_search);
+  let matches = js_node_to_visitors(ast, node_search);
   let v_match = list_single(matches);
   return v_match;
 }
