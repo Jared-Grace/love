af2fcfd92db70d8427d7d37dc7fd4eaa35a2d9dc
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:47:09 -0400

    frp object_intersect_multiple objects_merge

--- public/src/data_identifiers_search_multiple.mjs
+++ public/src/data_identifiers_search_multiple.mjs
@@ -1,10 +1,10 @@
-import { object_intersect_multiple } from "../../../love/public/src/object_intersect_multiple.mjs";
+import { objects_merge } from "../../../love/public/src/objects_merge.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function data_identifiers_search_multiple(ids_comma) {
   let ids = text_split_dot_comma(ids_comma);
   let waited = await list_map_unordered_async(ids, data_identifiers_search);
-  let dictionary = object_intersect_multiple(waited);
+  let dictionary = objects_merge(waited);
   return dictionary;
 }
