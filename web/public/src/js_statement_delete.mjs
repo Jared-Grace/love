9fc4eef55765b5f26c6b1856bfcfb180f6884691
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 12:10:11 -0400

    function_auto_path ../love/public/src/js_statement_delete.mjs

--- web/public/src/js_statement_delete.mjs
+++ web/public/src/js_statement_delete.mjs
@@ -1,4 +1,6 @@
+import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function js_statement_delete(ast, nodes) {
   let first = list_first(nodes);
+  let v = js_node_to_visitor(ast2, node_search);
 }
