6b91a38eea566ec3a76d4e165797a44b85c248a5
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:11:05 -0400

    function_auto_path ../love/public/src/js_function_node_find_named.mjs

--- web/public/src/js_function_node_find_named.mjs
+++ web/public/src/js_function_node_find_named.mjs
@@ -1,6 +1,6 @@
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_visit_types_function_named_list } from "../../../love/public/src/js_visit_types_function_named_list.mjs";
-export function js_visit_types_function_named_single(ast, name) {
+export function js_function_node_find_named(ast, name) {
   let list = js_visit_types_function_named_list(ast, name);
   let only = list_single(list);
   return only;
