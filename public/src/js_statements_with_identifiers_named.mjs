c773e5fcf78dc951e7b2c005c0996b2a0dd4d0ab
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:57:19 -0400

    function_auto_path ../love/public/src/js_node_to_block_curried.mjs

--- public/src/js_statements_with_identifiers_named.mjs
+++ public/src/js_statements_with_identifiers_named.mjs
@@ -2,5 +2,5 @@ import { js_node_to_block } from "../../../love/public/src/js_node_to_block.mjs"
 import { js_identifiers_named } from "../../../love/public/src/js_identifiers_named.mjs";
 export function js_statements_with_identifiers_named(ast, identifier_name) {
   let identifiers_named = js_identifiers_named(ast, identifier_name);
-  let f = js_node_to_block(ast, node);
+  let f = js_node_to_block(ast, node);js_node_to_block_curried
 }
