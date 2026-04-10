a7f6829f76edc1e81804251a6fff712e70b9cb3f
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:55:44 -0400

    function_auto_path ../love/public/src/js_node_to_block_item.mjs

--- public/src/js_node_to_block_item.mjs
+++ public/src/js_node_to_block_item.mjs
@@ -1,7 +1,7 @@
 import { js_node_to_block } from "../../../love/public/src/js_node_to_block.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_node_to_block_item(ast, node) {
-  let r = js_node_to_block(ast, node);
-  let item = property_get(r, "item");
+  let f = js_node_to_block(ast, node);
+  let item = property_get(f, "item");
   return item;
 }
