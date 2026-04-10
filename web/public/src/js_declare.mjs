0c9928cd4cb7b949b92dc9851bb7386d8c88e411
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:04:52 -0400

    function_auto_path ../love/public/src/js_declare.mjs

--- web/public/src/js_declare.mjs
+++ web/public/src/js_declare.mjs
@@ -3,7 +3,7 @@ import { js_parse_statement } from "../../../love/public/src/js_parse_statement.
 import { js_code_let_assign } from "../../../love/public/src/js_code_let_assign.mjs";
 export function js_declare(name, init) {
   let assign_code = js_code_let_assign(name, "a");
-  let assign = js_parse_statement(assign_code);
-  js_declare_init_set(assign, init);
-  return assign;
+  let declare = js_parse_statement(assign_code);
+  js_declare_init_set(declare, init);
+  return declare;
 }
