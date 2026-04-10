4afbce22dd41a79d802393d7f9d05da594b3942a
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:45:09 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- web/public/src/js_statement_duplicate.mjs
+++ web/public/src/js_statement_duplicate.mjs
@@ -1,12 +1,10 @@
-import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
+import { js_block_find_from_nodes_single } from "../../../love/public/src/js_block_find_from_nodes_single.mjs";
 import { js_visit_declarators_uniqueify } from "../../../love/public/src/js_visit_declarators_uniqueify.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_statement_duplicate(ast, nodes) {
-  let node = list_single(nodes);
-  let r = js_block_find_from_node(ast, node);
+  let r = js_block_find_from_nodes_single(nodes, ast);
   let item = property_get(r, "item");
   let index = property_get(r, "index");
   let body = property_get(r, "body");
