1dfda55029eec9039ee39b56c07ca78aa77a8a1b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:11:50 -0400

    function_auto_path ../love/public/src/data_identifiers_search_multiple.mjs

--- public/src/data_identifiers_search_multiple.mjs
+++ public/src/data_identifiers_search_multiple.mjs
@@ -1,8 +1,10 @@
+import { list_intersect_multiple } from "../../../love/public/src/list_intersect_multiple.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function data_identifiers_search_multiple(ids_comma) {
   let ids = text_split_dot_comma(ids_comma);
   let waited = await list_map_unordered_async(ids, data_identifiers_search);
+  let i = list_intersect_multiple(list);
   return r;
 }
