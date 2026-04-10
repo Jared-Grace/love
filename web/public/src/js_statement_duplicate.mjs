7f742dca38bee47ae1a933ac3d82742696dbfac9
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:44:49 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- web/public/src/js_statement_duplicate.mjs
+++ web/public/src/js_statement_duplicate.mjs
@@ -3,7 +3,9 @@ import { js_visit_declarators_uniqueify } from "../../../love/public/src/js_visi
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_statement_duplicate(ast, nodes) {
+  let node = list_single(nodes);
   let r = js_block_find_from_node(ast, node);
   let item = property_get(r, "item");
   let index = property_get(r, "index");
