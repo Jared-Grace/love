04663cfb6522a2c30f79a1c2fbbbfd40daaac224
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 17:12:11 -0400

    function_auto_path ../love/public/src/function_rename_open_generic_args.mjs

--- public/src/function_rename_open_generic_args.mjs
+++ public/src/function_rename_open_generic_args.mjs
@@ -3,15 +3,14 @@ import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs"
 import { function_rename_open } from "../../../love/public/src/function_rename_open.mjs";
 export async function function_rename_open_generic_args(
   plugin_fn,
-  f_name_old,
-  args,
+  f_name_old_args_comma,
 ) {
-  arguments_assert(arguments, 3);
+  arguments_assert(arguments, 2);
   let f_name_new = null;
+  let f_name_old = null;
   ({ f_name_new, f_name_old } = await function_name_new_get_args(
     plugin_fn,
-    f_name_old,
-    args,
+    f_name_old_args_comma,
   ));
   let r = await function_rename_open(f_name_old, f_name_new);
   return r;
