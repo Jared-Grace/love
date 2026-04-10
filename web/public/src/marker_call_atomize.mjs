8ebc6ad5b0f911c9e1e0640059e73a5542eff4a8
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:03:00 -0400

    function_auto_path ../love/public/src/marker_call_atomize.mjs

--- web/public/src/marker_call_atomize.mjs
+++ web/public/src/marker_call_atomize.mjs
@@ -2,7 +2,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_node_atomize_name } from "../../../love/public/src/js_node_atomize_name.mjs";
 import { js_node_atomize } from "../../../love/public/src/js_node_atomize.mjs";
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
-import { js_identifiers } from "../../../love/public/src/js_identifiers.mjs";
 import { marker_call_replace_generic } from "../../../love/public/src/marker_call_replace_generic.mjs";
 export async function marker_call_atomize(input) {
   let v = await marker_call_replace_generic(input, lambda);
