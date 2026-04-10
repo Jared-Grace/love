0adb7617320c246b02e62344059ce0ea41b6d54b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:22:22 -0400

    function_auto_path ../love/public/src/list_map.mjs

--- web/public/src/list_map.mjs
+++ web/public/src/list_map.mjs
@@ -1,10 +1,10 @@
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
 export function list_map(list, lambda$item) {
   list_is_assert(list);
-  let mapped = list.map(lambda$item);
-  return mapped;
-  (function lambda(item) {
+  function lambda(item) {
     let r = lambda$item(item);
     return r;
-  });
+  }
+  let mapped = list.map(lambda);
+  return mapped;
 }
