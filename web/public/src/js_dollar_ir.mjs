87f9de0ee7f9dd890076603ba3f7aee62a7c667a
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:47:36 -0400

    function_auto_path ../love/public/src/js_dollar_ir.mjs

--- web/public/src/js_dollar_ir.mjs
+++ web/public/src/js_dollar_ir.mjs
@@ -1,3 +1,4 @@
+import { js_statement_if_consequent_get } from "../../../love/public/src/js_statement_if_consequent_get.mjs";
 import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_dollar_i } from "../../../love/public/src/js_dollar_i.mjs";
@@ -13,7 +14,7 @@ export function js_dollar_ir({
   js_dollar_i({
     stack1,
   });
-  let consequent = property_get(stack1, "consequent");
+  let consequent = js_statement_if_consequent_get(stack1);
   let body_block = property_get(consequent, "body");
   js_statement_return_empty_add(body_block);
   return;
