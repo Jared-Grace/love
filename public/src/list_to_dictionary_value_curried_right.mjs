5df0712afdb2dfdad95be9f01e44714b85afe142
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:27:58 -0400

    function_auto_path ../love/public/src/list_to_dictionary_value_curried_right.mjs

--- public/src/list_to_dictionary_value_curried_right.mjs
+++ public/src/list_to_dictionary_value_curried_right.mjs
@@ -1,6 +1,8 @@
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 export function list_to_dictionary_value_curried_right(lambda$item2v) {
-  return function list_to_dictionary_value_curried_right_result(list) {
-    return list_to_dictionary_value(list, lambda$item2v);
+  let r = function list_to_dictionary_value_curried_right_result(list) {
+    let dictionary = list_to_dictionary_value(list, lambda$item2v);
+    return dictionary;
   };
+  return r;
 }
