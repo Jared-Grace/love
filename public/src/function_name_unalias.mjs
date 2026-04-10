ea489ef2524b9f8f727652c06d6179bcd0377b97
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:10:08 -0400

    function_auto_path ../love/public/src/function_name_unalias.mjs

--- public/src/function_name_unalias.mjs
+++ public/src/function_name_unalias.mjs
@@ -7,12 +7,12 @@ export async function function_name_unalias(f_name) {
   arguments_assert(arguments, 1);
   var v2 = await function_alias_add_generic(f_name);
   let unaliased_actual = property_get(v2, "unaliased");
-  let exists = property_get(v2, "exists");
+  let exists_alias = property_get(v2, "exists");
   let r = await function_acronym_to_name(f_name);
   let expandeds = property_get(r, "expandeds");
   let expanded = property_get(r, "expanded");
   let expanded_paths = await functions_names_to_paths_list(expandeds);
-  const unaliased = exists
+  const unaliased = exists_alias
     ? unaliased_actual
     : expanded !== null
       ? expanded
@@ -20,6 +20,7 @@ export async function function_name_unalias(f_name) {
   let v = {
     unaliased,
     expandeds: expanded_paths,
+    exists_alias,
   };
   return v;
 }
