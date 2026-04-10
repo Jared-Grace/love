9e8c3bc0afef3aee1c92d5171e99a49c4f824d93
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 18:58:43 -0400

    function_auto_path ../love/public/src/js_visit_types_function_named.mjs

--- web/public/src/js_visit_types_function_named.mjs
+++ web/public/src/js_visit_types_function_named.mjs
@@ -1,12 +1,15 @@
+import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_if.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_visit_types_function } from "../../../love/public/src/js_visit_types_function.mjs";
-export function js_visit_types_function_named(ast, lambda$v) {
+export function js_visit_types_function_named(ast, lambda$v, name) {
   function lambda(v) {
     let node = property_get(v, "node");
     let id = property_get(node, "id");
-    let eq2 = equal(left, right);
-    if (id) {
+    let eq2 = equal(left, name);
+    function lambda3() {}
+    js_identifier_is_if(node2, lambda3);
+    if (ideq2) {
     }
   }
   let r = js_visit_types_function(ast, lambda);
