b782eea85fa3ca41b1574bb1a8a48be5bca5c85d
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 17:06:20 -0400

    function_auto_path ../love/public/src/function_transform_current_args.mjs

--- public/src/function_transform_current_args.mjs
+++ public/src/function_transform_current_args.mjs
@@ -1,5 +1,5 @@
+import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
@@ -7,8 +7,7 @@ import { function_import_unalias } from "../../../love/public/src/function_impor
 export async function function_transform_current_args(
   f_name_transformer_args_comma,
 ) {
-  let split = text_split_comma_dot(f_name_transformer_args_comma);
-  let fr = list_first_remaining(split);
+  let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
   let remaining = property_get(fr, "remaining");
   let f_name_transformer = property_get(fr, "first");
   let args = text_split_comma_dot(args_comma);
