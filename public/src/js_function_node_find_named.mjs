0aee4e5fb4f3517b447749de32f42cd9330545ea
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:07:11 -0400

    de js_function_node_find_named

--- public/src/js_function_node_find_named.mjs
+++
@@ -1,4 +0,0 @@
-import { js_visit_types_function_named_single } from "../../../love/public/src/js_visit_types_function_named_single.mjs";
-export function js_function_node_find_named(ast, f_name) {
-  let only = js_visit_types_function_named_single(ast, f_name);
-}
