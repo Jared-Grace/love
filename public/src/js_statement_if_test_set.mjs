4eb66d59f5b021cfc39e5888a332f250b640c72c
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:56:47 -0400

    function_auto_path ../love/public/src/js_statement_if_test_set.mjs

--- public/src/js_statement_if_test_set.mjs
+++ public/src/js_statement_if_test_set.mjs
@@ -1,6 +1,6 @@
 import { js_node_type_is_assert } from "../../../love/public/src/js_node_type_is_assert.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 export function js_statement_if_test_set(statement_if, expression) {
-  js_node_type_is_assert(statement_if, "ExpressionStatement");
+  js_node_type_is_assert(statement_if, "IfStatement");
   property_set(statement_if, "test", expression);
 }
