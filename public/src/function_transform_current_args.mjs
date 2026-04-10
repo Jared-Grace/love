f222d7240c74ba77fa5e953d5cc5e776c6cbed1f
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:19:03 -0400

    function_auto_path ../love/public/src/function_transform_current_args.mjs

--- public/src/function_transform_current_args.mjs
+++ public/src/function_transform_current_args.mjs
@@ -1,10 +1,12 @@
+import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 export async function function_transform_current_args(
   f_name_transformer,
   args_comma,
-) {text_split_dot_comma
+) {
+  let split = text_split_dot_comma(t);
   let imported_fn = await function_import_unalias(f_name_transformer);
   let f_name = await function_current_get();
   async function lambda(ast) {
