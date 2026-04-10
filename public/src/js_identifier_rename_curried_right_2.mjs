c42950c826fc75aa78d3d873cb07d1bd05179b82
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 13:43:49 -0400

    curc js_identifier_rename 2

--- public/src/js_identifier_rename_curried_right_2.mjs
+++ public/src/js_identifier_rename_curried_right_2.mjs
@@ -1,1 +1,6 @@
-export function js_identifier_rename_curried_right_2() {}
+import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
+export function js_identifier_rename_curried_right_2(name_from, name_to) {
+  return function js_identifier_rename_curried_right_2_result(ast) {
+    return js_identifier_rename(ast, name_from, name_to);
+  };
+}
