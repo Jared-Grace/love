518e4f68214e0ed42fa7913dff33e04bc4df13df
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:39:28 -0400

    is functions_names_to_paths

--- public/src/data_identifiers_search_generic.mjs
+++ public/src/data_identifiers_search_generic.mjs
@@ -10,7 +10,12 @@ export async function data_identifiers_search_generic(fn, ids_comma) {
   let r3 = property_get_curried(identifiers);
   let mapped = list_map(ids, r3);
   let i = list_intersect_multiple(mapped);
+  let result = await functions_names_to_paths(i);
+  return result;
+}
+async function functions_names_to_paths(i) {
   let r2 = await functions_names_paths_getter();
   let result = list_to_dictionary_value(i, r2);
   return result;
 }
+
