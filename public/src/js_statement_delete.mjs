e9be0f0aed79eb416d5f77466f5ac1c22b48dc50
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 12:16:18 -0400

    function_auto_path ../love/public/src/js_statement_delete.mjs

--- public/src/js_statement_delete.mjs
+++ public/src/js_statement_delete.mjs
@@ -1,6 +1,10 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function js_statement_delete(ast, nodes) {
   let first = list_first(nodes);
-  let b = js_block_find_from_node(ast, first);
+  let r = js_block_find_from_node(ast, first);
+  let item = property_get(r, "item");
+  let index = property_get(r, "index");
+  let body = property_get(r, "body");
 }
