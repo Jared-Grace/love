87e7106df7c4b46b823bec24b8af3b7b4bd16109
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:32:53 -0400

    function_auto_path ../love/public/src/data_identifiers_search_generic.mjs

--- public/src/data_identifiers_search_generic.mjs
+++ public/src/data_identifiers_search_generic.mjs
@@ -3,11 +3,11 @@ import { property_get_curried } from "../../../love/public/src/property_get_curr
 import { functions_names_paths } from "../../../love/public/src/functions_names_paths.mjs";
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export async function data_identifiers_search_generic(fn, s) {
+export async function data_identifiers_search_generic(fn, ids_comma) {
   let identifiers = await fn();
   let r = await functions_names_paths();
   let r2 = property_get_curried(r);
-  let list = property_get(identifiers, s);
+  let list = property_get(identifiers, ids_comma);
   function lambda(item) {}
   let mapped = list_map(list2, lambda);
   let result = list_to_dictionary_value(list, r2);
