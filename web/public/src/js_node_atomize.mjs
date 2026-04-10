1b9f915bf877395ace38ac3c3244335bff49cfe2
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:02:39 -0400

    function_auto_path ../love/public/src/js_node_atomize.mjs

--- web/public/src/js_node_atomize.mjs
+++ web/public/src/js_node_atomize.mjs
@@ -1,3 +1,4 @@
+import { js_identifier_unique_ast } from "../../../love/public/src/js_identifier_unique_ast.mjs";
 import { list_get_end } from "../../../love/public/src/list_get_end.mjs";
 import { js_node_atomize_variable_name_get } from "../../../love/public/src/js_node_atomize_variable_name_get.mjs";
 import { js_block_insert } from "../../../love/public/src/js_block_insert.mjs";
@@ -5,7 +6,6 @@ import { js_declare } from "../../../love/public/src/js_declare.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { object_copy } from "../../../love/public/src/object_copy.mjs";
-import { js_identifier_unique } from "../../../love/public/src/js_identifier_unique.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function js_node_atomize(
   ast,
@@ -25,7 +25,7 @@ export async function js_node_atomize(
   ("this is in case there is an await");
   const copied = list_get_end(stack, offset);
   let copy = object_copy(copied);
-  let unique = js_identifier_unique(existing_ids, variable_name);
+  let unique = js_identifier_unique_ast(ast, variable_name);
   let assign = js_declare(unique, copy);
   js_block_insert(stack, assign);
   let v2 = js_parse_expression(unique);
