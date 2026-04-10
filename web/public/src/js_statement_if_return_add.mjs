917ecae921e41ccc6aa8238563a09962e4dfa8b4
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:27:03 -0400

    function_auto_path ../love/public/src/js_statement_if_return_add.mjs

--- web/public/src/js_statement_if_return_add.mjs
+++ web/public/src/js_statement_if_return_add.mjs
@@ -1,9 +1,9 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_statement_return_empty_add } from "../../../love/public/src/js_statement_return_empty_add.mjs";
 import { js_block_body_get } from "../../../love/public/src/js_block_body_get.mjs";
 import { js_statement_if_consequent_get } from "../../../love/public/src/js_statement_if_consequent_get.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 export function js_statement_if_return_add(ast, selects) {
-  let statement_if = list_first(selects);
+  let statement_if = list_single(selects);
   let c = js_statement_if_consequent_get(statement_if);
   let value = js_block_body_get(c);
   let r = js_statement_return_empty_add(value);
