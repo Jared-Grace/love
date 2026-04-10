d0a3d79a6f14adf9e3da61233909aa5ba5ff4adf
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:40:09 -0400

    function_auto_path ../love/public/src/data_identifiers_search_generic.mjs

--- web/public/src/data_identifiers_search_generic.mjs
+++ web/public/src/data_identifiers_search_generic.mjs
@@ -1,21 +1,14 @@
-import { functions_names_paths_getter } from "../../../love/public/src/functions_names_paths_getter.mjs";
+import { function_list_name_to_paths } from "../../../love/public/src/function_list_name_to_paths.mjs";
 import { list_intersect_multiple } from "../../../love/public/src/list_intersect_multiple.mjs";
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
-import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 export async function data_identifiers_search_generic(fn, ids_comma) {
   let identifiers = await fn();
   let ids = text_split_dot_comma(ids_comma);
   let r3 = property_get_curried(identifiers);
   let mapped = list_map(ids, r3);
   let i = list_intersect_multiple(mapped);
-  let result = await functions_names_to_paths(i);
+  let result = await function_list_name_to_paths(i);
   return result;
 }
-async function functions_names_to_paths(i) {
-  let r2 = await functions_names_paths_getter();
-  let result = list_to_dictionary_value(i, r2);
-  return result;
-}
-
