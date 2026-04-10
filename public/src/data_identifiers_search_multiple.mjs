e35135da15f1f872c146c963cb0111f10e25b02b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:16:40 -0400

    function_auto_path ../love/public/src/data_identifiers_search_multiple.mjs

--- public/src/data_identifiers_search_multiple.mjs
+++ public/src/data_identifiers_search_multiple.mjs
@@ -1,4 +1,4 @@
-import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
+import { list_to_dictionary_from_object } from "../../../love/public/src/list_to_dictionary_from_object.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -6,14 +6,12 @@ import { list_intersect_multiple } from "../../../love/public/src/list_intersect
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
-import { list_to_dictionary_value } from "./list_to_dictionary_value.mjs";
 export async function data_identifiers_search_multiple(ids_comma) {
   let ids = text_split_dot_comma(ids_comma);
   let waited = await list_map_unordered_async(ids, data_identifiers_search);
   let mapped = list_map(waited, properties_get);
   let i = list_intersect_multiple(mapped);
   let first = list_first(waited);
-  let r2 = property_get_curried(first);
-  let dictionary = list_to_dictionary_value(i, r2);
+  let dictionary = list_to_dictionary_from_object(first, i);
   return dictionary;
 }
