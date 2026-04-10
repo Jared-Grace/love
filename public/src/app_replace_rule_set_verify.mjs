1a71ec950a3d21e8d4434861e55944153a2d9e6d
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:59:05 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_verify.mjs

--- public/src/app_replace_rule_set_verify.mjs
+++ public/src/app_replace_rule_set_verify.mjs
@@ -1,9 +1,8 @@
+import { list_map_properties_unique } from "../../../love/public/src/list_map_properties_unique.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { property_get_or_null } from "../../../love/public/src/property_get_or_null.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { list_includes_all_assert } from "../../../love/public/src/list_includes_all_assert.mjs";
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
-import { list_map_properties } from "../../../love/public/src/list_map_properties.mjs";
 import { app_replace_rule_set_verify_path_get } from "../../../love/public/src/app_replace_rule_set_verify_path_get.mjs";
 import { list_squash } from "../../../love/public/src/list_squash.mjs";
 import { app_replace_rule_set_verify_goal_curried_right } from "../../../love/public/src/app_replace_rule_set_verify_goal_curried_right.mjs";
@@ -25,8 +24,7 @@ export function app_replace_rule_set_verify(rule_set) {
   let paths = list_map(squashed, app_replace_rule_set_verify_path_get);
   let squashed2 = list_squash(paths);
   const properties = ["rule", "original"];
-  let mapped = list_map_properties(squashed2, properties);
-  let unique = list_unique(mapped);
+  let unique = list_map_properties_unique(squashed2, properties);
   let rules = property_get(rule_set, "rules");
   let value = property_get_or_null(rule_set, "rules_unused");
   if (null_not_is(value)) {
