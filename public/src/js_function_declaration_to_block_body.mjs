6d87daab1b03a584c9d23f6e297ae3613fabc67a
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:55:38 -0400

    r 1 js_block_body_to get

--- public/src/js_function_declaration_to_block_body.mjs
+++ public/src/js_function_declaration_to_block_body.mjs
@@ -1,7 +1,7 @@
-import { js_block_body_to } from "../../../love/public/src/js_block_body_to.mjs";
+import { js_block_body_get } from "../../../love/public/src/js_block_body_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_function_declaration_to_block_body(declaration) {
   let block = property_get(declaration, "body");
-  let body_block = js_block_body_to(block);
+  let body_block = js_block_body_get(block);
   return body_block;
 }
