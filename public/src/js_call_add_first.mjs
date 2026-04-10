e9c6e5bc931a28010f06b444e47b6ab080a877d3
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:21:44 -0400

    function_auto_path ../love/public/src/js_call_add_first.mjs

--- public/src/js_call_add_first.mjs
+++ public/src/js_call_add_first.mjs
@@ -3,13 +3,11 @@ import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.
 import { js_flo_body_add_first } from "../../../love/public/src/js_flo_body_add_first.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { data_functions_get } from "../../../love/public/src/data_functions_get.mjs";
-export async function js_call_add_first(ast) {
+export async function js_call_add_first(ast, f_name) {
   let functions = await data_functions_get();
   let visited = [];
-  async function lambda(ast) {
-    let statement = js_parse_statement(f_name);
-    js_flo_body_add_first(ast, statement);
-    let visitor = js_node_to_visitor(ast, statement);
-    await js_call_fill_inner(ast, visitor, functions, visited);
-  }
+  let statement = js_parse_statement(f_name);
+  js_flo_body_add_first(ast, statement);
+  let visitor = js_node_to_visitor(ast, statement);
+  await js_call_fill_inner(ast, visitor, functions, visited);
 }
