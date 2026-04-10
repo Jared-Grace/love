87f7aaab84ec19a63209b9be860c8cd831274c2c
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 13:45:37 -0400

    function_auto_path ../love/public/src/function_transform_current_identifier_rename.mjs

--- public/src/function_transform_current_identifier_rename.mjs
+++ public/src/function_transform_current_identifier_rename.mjs
@@ -1,20 +1,10 @@
 import { js_identifier_rename_curried_right_2 } from "../../../love/public/src/js_identifier_rename_curried_right_2.mjs";
-import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
-import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
-import { js_call_fill_inner } from "../../../love/public/src/js_call_fill_inner.mjs";
-import { data_functions_get } from "../../../love/public/src/data_functions_get.mjs";
-import { js_flo_body_add_first } from "../../../love/public/src/js_flo_body_add_first.mjs";
 import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
-export async function function_transform_current_identifier_rename(f_name) {
+export async function function_transform_current_identifier_rename(
+  name_from,
+  name_to,
+) {
   let r2 = js_identifier_rename_curried_right_2(name_from, name_to);
-  let functions = await data_functions_get();
-  let visited = [];
-  async function lambda(ast) {
-    let statement = js_parse_statement(f_name);
-    js_flo_body_add_first(ast, statement);
-    let visitor = js_node_to_visitor(ast, statement);
-    await js_call_fill_inner(ast, visitor, functions, visited);
-  }
-  let r = await function_transform_current_fn(lambda);
+  let r = await function_transform_current_fn(r2);
   return r;
 }
