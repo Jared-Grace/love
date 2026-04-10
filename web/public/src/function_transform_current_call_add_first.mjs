87335b379d0907e788a33e277437c4ec17c6fada
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 13:29:51 -0400

    function_auto_path ../love/public/src/function_transform_current_call_add_first.mjs

--- web/public/src/function_transform_current_call_add_first.mjs
+++ web/public/src/function_transform_current_call_add_first.mjs
@@ -1,7 +1,14 @@
+import { js_call_fill_inner } from "../../../love/public/src/js_call_fill_inner.mjs";
+import { data_functions_get } from "../../../love/public/src/data_functions_get.mjs";
 import { js_flo_body_add_first } from "../../../love/public/src/js_flo_body_add_first.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
 export async function function_transform_current_call_add_first(code_argument) {
+  let functions = await data_functions_get();
+  let visited = [];
+  async function lambda(v) {
+    await js_call_fill_inner(ast, v, functions, visited);
+  }
   function lambda(ast) {
     let expression = js_parse_expression(code_argument);
     js_flo_body_add_first(ast, expression);
