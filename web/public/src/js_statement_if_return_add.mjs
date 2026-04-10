ab0a535c16e66c64d9896b225480bd85ba7a4415
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:37:48 -0400

    function_auto_path ../love/public/src/js_statement_if_return_add.mjs

--- web/public/src/js_statement_if_return_add.mjs
+++ web/public/src/js_statement_if_return_add.mjs
@@ -1,3 +1,5 @@
-export function js_statement_if_return_add(ast) {
+import { list_first } from "../../../love/public/src/list_first.mjs";
+export function js_statement_if_return_add(ast, selects) {
+  let first = list_first(list);
   let p = "consequent";
 }
