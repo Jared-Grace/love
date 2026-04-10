ed3af461d660bd1029c0819a88dc02a41d35192b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 07:59:20 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -3,6 +3,7 @@ import { function_transform_current_return_add_last } from "../../../love/public
 import { app_bible_chapter_set_default } from "../../../love/public/src/app_bible_chapter_set_default.mjs";
 import { js_statement_duplicate } from "../../../love/public/src/js_statement_duplicate.mjs";
 import { function_node_select_args } from "../../../love/public/src/function_node_select_args.mjs";
+import { js_statement_find_call_named } from "../../../love/public/src/js_statement_find_call_named.mjs";
 import { function_current_selects_apply } from "../../../love/public/src/function_current_selects_apply.mjs";
 import { function_current_set } from "../../../love/public/src/function_current_set.mjs";
 import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
@@ -12,8 +13,8 @@ export async function sandbox() {
   await function_current_selects_empty();
   await function_current_set(app_bible_home_generic.name);
   let r = await function_node_select_args(
+    js_statement_find_call_named.name,
     app_bible_chapter_set_default.name,
-    "pg",
   );
   return;
   let r32 = await function_current_selects_apply(js_statement_duplicate.name);
