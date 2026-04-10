34951aeab095db618d6b8aa489de768a47ba9cd9
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 12:03:28 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- web/public/src/sandbox.mjs
+++ web/public/src/sandbox.mjs
@@ -18,9 +18,9 @@ export async function sandbox() {
     js_statement_find_call_named.name,
     app_bible_chapter_set_default.name,
   );
-  await function_current_selects_apply(js_statement_if_return_add.name);
   return r;
   ("below is functionality that has been used in the past");
+  await function_current_selects_apply(js_statement_if_return_add.name);
   await function_current_selects_apply(js_statement_wrap_if.name);
   await function_current_selects_apply(js_statement_duplicate.name);
   let r4 = await function_node_select_nested(js_call_callee_name.name);
