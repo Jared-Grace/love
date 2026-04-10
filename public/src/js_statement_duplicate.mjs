ff04b0f006840c37bae23ba7023981549c710db2
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:45:13 -0400

    pb js_block_find_from_nodes_single

--- public/src/js_statement_duplicate.mjs
+++ public/src/js_statement_duplicate.mjs
@@ -4,7 +4,7 @@ import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_statement_duplicate(ast, nodes) {
-  let r = js_block_find_from_nodes_single(nodes, ast);
+  let r = js_block_find_from_nodes_single(ast, nodes);
   let item = property_get(r, "item");
   let index = property_get(r, "index");
   let body = property_get(r, "body");
