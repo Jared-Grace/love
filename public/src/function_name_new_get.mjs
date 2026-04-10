5f9c4bb48d8b0e8e1a9613077bb70880b91063e7
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 22:39:40 -0400

    function_auto_path ../love/public/src/function_name_new_get.mjs

--- public/src/function_name_new_get.mjs
+++ public/src/function_name_new_get.mjs
@@ -1,7 +1,10 @@
+import { function_name_new_get_generic } from "../../../love/public/src/function_name_new_get_generic.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
-import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function function_name_new_get(plugin_fn, f_name_old) {
-  f_name_old = await function_name_unalias_only(f_name_old);
+  ({ f_name_old, plugin_fn } = await function_name_new_get_generic(
+    f_name_old,
+    plugin_fn,
+  ));
   let f_name_new = await function_run(plugin_fn, [f_name_old]);
   let r2 = {
     f_name_new,
