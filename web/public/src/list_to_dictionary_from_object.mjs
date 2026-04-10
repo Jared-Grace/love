86a48980ac63384f08ea99d7414a03654e722170
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:17:00 -0400

    pb list_to_dictionary_from_object

--- web/public/src/list_to_dictionary_from_object.mjs
+++ web/public/src/list_to_dictionary_from_object.mjs
@@ -1,6 +1,6 @@
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
-export function list_to_dictionary_from_object(object, list) {
+export function list_to_dictionary_from_object(list, object) {
   let r2 = property_get_curried(object);
   let dictionary = list_to_dictionary_value(list, r2);
   return dictionary;
