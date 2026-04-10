84ab29dd1ccebbe5e4e72cfd0aef88c6753f41c7
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 13:01:57 -0400

    function_auto_path ../love/public/src/js_flo_body_add_first_curried_right.mjs

---
+++ public/src/js_flo_body_add_first_curried_right.mjs
@@ -0,0 +1,8 @@
+import { js_flo_body_add_first } from "../../../love/public/src/js_flo_body_add_first.mjs";
+export function js_flo_body_add_first_curried_right(item) {
+  let r2 = function js_flo_body_add_first_curried_right_result(ast) {
+    let r = js_flo_body_add_first(ast, item);
+    return r;
+  };
+  return r2;
+}
