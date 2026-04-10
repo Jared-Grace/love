18bc949358ed51672fd5fa3190d820a4de0d0b19
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 17:16:12 -0400

    de function_transform_prompt_current

--- public/src/function_transform_prompt_current.mjs
+++
@@ -1,7 +0,0 @@
-import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
-import { function_transform_prompt } from "../../../love/public/src/function_transform_prompt.mjs";
-export async function function_transform_prompt_current(transformer_name) {
-  let f_name_current = await function_current_get();
-  let r = await function_transform_prompt(transformer_name, f_name_current);
-  return r;
-}
