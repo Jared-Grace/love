b38e61fc3eed8ce207462d4cd74a445bc13613b4
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:27:14 -0400

    function_auto_path ../love/public/src/js_statement_wrap_if.mjs

--- web/public/src/js_statement_wrap_if.mjs
+++ web/public/src/js_statement_wrap_if.mjs
@@ -1,10 +1,10 @@
-import { list_first } from "../../../love/public/src/list_first.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
 import { js_statement_if_test_set } from "../../../love/public/src/js_statement_if_test_set.mjs";
 import { js_statement_expression_get } from "../../../love/public/src/js_statement_expression_get.mjs";
 import { js_statement_if } from "../../../love/public/src/js_statement_if.mjs";
+import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_statement_wrap_if(ast, selects) {
-  let node = list_first(selects);
+  let node = list_single(selects);
   let expression = js_statement_expression_get(node);
   let statement_if = js_statement_if();
   js_statement_if_test_set(statement_if, expression);
