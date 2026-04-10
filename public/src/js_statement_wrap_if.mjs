540553f0779f0124e26c12f7aa76e83dfe4540dc
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 10:23:06 -0400

    function_auto_path ../love/public/src/js_statement_wrap_if.mjs

--- public/src/js_statement_wrap_if.mjs
+++ public/src/js_statement_wrap_if.mjs
@@ -9,7 +9,7 @@ export function js_statement_wrap_if(ast, selects) {
   let expression = js_statement_expression_get(node);
   let statement_if = js_statement_if();
   js_statement_if_test_set(stack1, expression);
-  object_replace(to, from);
+  object_replace(expression, stack1);
   log(js_statement_wrap_if.name, {
     node,
     statement_if,
