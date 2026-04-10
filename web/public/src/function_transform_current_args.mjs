8820ee7f7f5a5c0861875149b997da3c4f9bb114
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:18:48 -0400

    function_auto_path ../love/public/src/function_transform_current_args.mjs

--- web/public/src/function_transform_current_args.mjs
+++ web/public/src/function_transform_current_args.mjs
@@ -1,3 +1,4 @@
+import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
@@ -5,6 +6,7 @@ export async function function_transform_current_args(
   f_name_transformer,
   args,
 ) {
+  js_identifier_rename(ast2, name_from, name_to);
   let imported_fn = await function_import_unalias(f_name_transformer);
   let f_name = await function_current_get();
   async function lambda(ast) {
