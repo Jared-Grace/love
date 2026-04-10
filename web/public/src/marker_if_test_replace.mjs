58518487d1861a12e2a40d003f4ce92446846b18
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 10:21:18 -0400

    function_auto_path ../love/public/src/marker_if_test_replace.mjs

--- web/public/src/marker_if_test_replace.mjs
+++ web/public/src/marker_if_test_replace.mjs
@@ -1,5 +1,5 @@
+import { js_statement_if_test_set } from "../../../love/public/src/js_statement_if_test_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { property_set } from "../../../love/public/src/property_set.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { js_stack_last } from "../../../love/public/src/js_stack_last.mjs";
 import { function_transform_marker_current } from "../../../love/public/src/function_transform_marker_current.mjs";
@@ -8,7 +8,7 @@ export async function marker_if_test_replace(replacement) {
     let stack = property_get(a, "stack");
     let last = js_stack_last(stack, "IfStatement");
     let expression = js_parse_expression(replacement);
-    property_set(last, "test", expression);
+    js_statement_if_test_set(last, expression);
   }
   let v = await function_transform_marker_current(lambda);
   return v;
