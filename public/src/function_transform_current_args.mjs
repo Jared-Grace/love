7f2d097ad3e35c69144f830c27f333b709cc3cdd
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:13:20 -0400

    function_auto_path ../love/public/src/function_transform_current_args.mjs

--- public/src/function_transform_current_args.mjs
+++ public/src/function_transform_current_args.mjs
@@ -1,6 +1,9 @@
 import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
-export async function function_transform_current_args(f_name_transformer) {
+export async function function_transform_current_args(
+  f_name_transformer,
+  args,
+) {
   let imported_fn = await function_import_unalias(f_name_transformer);
   let r = await function_transform_current_fn(imported_fn);
   return r;
