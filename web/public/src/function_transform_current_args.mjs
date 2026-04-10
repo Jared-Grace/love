4b86efb83e416c59753c14b6d1dabf50ce705b27
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 17:06:35 -0400

    function_auto_path ../love/public/src/function_transform_current_args.mjs

--- web/public/src/function_transform_current_args.mjs
+++ web/public/src/function_transform_current_args.mjs
@@ -1,6 +1,5 @@
 import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
@@ -10,7 +9,6 @@ export async function function_transform_current_args(
   let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
   let remaining = property_get(fr, "remaining");
   let f_name_transformer = property_get(fr, "first");
-  let args = text_split_comma_dot(args_comma);
   let imported_fn = await function_import_unalias(f_name_transformer);
   let f_name = await function_current_get();
   async function lambda(ast) {
