a009444e2978c474cdbf47a8e5f93fdd5007e11c
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 13:01:03 -0400

    function_auto_path ../love/public/src/function_name_new_get_generic.mjs

--- public/src/function_name_new_get_generic.mjs
+++ public/src/function_name_new_get_generic.mjs
@@ -19,7 +19,7 @@ export async function function_name_new_get_generic(f_name_old, plugin_fn) {
     t: text_replace,
     1: function_name_to_part_replace_last,
     2: function_name_to_part_replace_last_2,
-    2: function_name_to_part_replace_last_3,
+    3: function_name_to_part_replace_last_3,
   };
   dictionary_functions_to_names(overrides);
   plugin_fn = property_exists_if_get(overrides, plugin_fn);
