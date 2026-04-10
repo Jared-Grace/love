54b26f8f1d15b3d0d1e969ce52fe91598a1af408
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:04:49 -0400

    w 1 js_visit_types_function_named_list,single

--- web/public/src/js_visit_types_function_named_single.mjs
+++ web/public/src/js_visit_types_function_named_single.mjs
@@ -1,1 +1,4 @@
-export function js_visit_types_function_named_single() {}
+import { js_visit_types_function_named_list } from "../../../love/public/src/js_visit_types_function_named_list.mjs";
+export function js_visit_types_function_named_single(ast, name) {
+  return js_visit_types_function_named_list(ast, name);
+}
