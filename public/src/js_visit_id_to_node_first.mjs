e613efeaa78a615f2b4cb0b5967b87a7f340adde
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:27:36 -0400

    function_auto_path ../love/public/src/js_visit_id_to_node_first.mjs

--- public/src/js_visit_id_to_node_first.mjs
+++ public/src/js_visit_id_to_node_first.mjs
@@ -1,7 +1,7 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 export function js_visit_id_to_node_first(ast, selects) {
-  let id = list_first(selects);
+  let id = list_single(selects);
   let node = js_visit_id_to_node(ast, id);
   return node;
 }
