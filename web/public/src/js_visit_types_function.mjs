12eb05cf9c32368a1a1002f2b0446819b80d1853
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 18:48:04 -0400

    function_auto_path ../love/public/src/js_visit_types_function.mjs

--- web/public/src/js_visit_types_function.mjs
+++ web/public/src/js_visit_types_function.mjs
@@ -1,6 +1,6 @@
 import { js_types_function } from "../../../love/public/src/js_types_function.mjs";
 import { js_visit_types } from "../../../love/public/src/js_visit_types.mjs";
-export function js_visit_types_function(ast, types, lambda$v) {
+export function js_visit_types_function(ast, lambda$v) {
   let types_function = js_types_function();
   let r = js_visit_types(ast, types_function, lambda$v);
   return r;
