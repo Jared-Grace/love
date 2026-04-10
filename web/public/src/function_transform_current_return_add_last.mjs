49e9407667fbce4b5709f439cf23dd43117b39ff
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 22:53:24 -0400

    function_auto_path ../love/public/src/function_transform_current_return_add_last.mjs

--- web/public/src/function_transform_current_return_add_last.mjs
+++ web/public/src/function_transform_current_return_add_last.mjs
@@ -1,8 +1,10 @@
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { js_flo_body_add_return_argument_curried_right } from "../../../love/public/src/js_flo_body_add_return_argument_curried_right.mjs";
 import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
 export async function function_transform_current_return_add_last(
   code_argument,
 ) {
+  let expression = js_parse_expression(code_expression);
   let r2 = js_flo_body_add_return_argument_curried_right(e);
   let r = await function_transform_current_fn(r2);
   return r;
