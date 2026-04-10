4f42208d4eae972e704cb3f57958fc35a5727ca9
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:37:41 -0400

    de text_split_dot_comma

--- public/src/text_split_dot_comma.mjs
+++
@@ -1,5 +0,0 @@
-import { text_split_multiple } from "../../../love/public/src/text_split_multiple.mjs";
-export function text_split_dot_comma(t) {
-  let split = text_split_multiple(t, [",", "."]);
-  return split;
-}
