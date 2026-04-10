1b061a40a5be7caef4dd4cab63b2f5970b1cf29f
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:36:18 -0400

    function_auto_path ../love/public/src/function_delete.mjs

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -3,8 +3,8 @@ import { assert } from "../../../love/public/src/assert.mjs";
 import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 import { data_files_update } from "../../../love/public/src/data_files_update.mjs";
 import { file_delete } from "../../../love/public/src/file_delete.mjs";
-export async function function_delete(f_name) {
-  const v = await function_unalias_exists(f_name);
+export async function function_delete(f_names_comma) {
+  const v = await function_unalias_exists(f_names_comma);
   let exists = property_get(v, "exists");
   let f_path = property_get(v, "f_path");
   assert(exists);
