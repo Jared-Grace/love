8972845bd3c3d8e7227ad8c12d674f8babae131b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:45:22 -0400

    function_auto_path ../love/public/src/function_list_name_to_paths.mjs

--- web/public/src/function_list_name_to_paths.mjs
+++ web/public/src/function_list_name_to_paths.mjs
@@ -1,7 +1,9 @@
+import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
+import { functions_names_paths } from "../../../love/public/src/functions_names_paths.mjs";
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
-import { functions_names_paths_getter } from "../../../love/public/src/functions_names_paths_getter.mjs";
 export async function function_list_name_to_paths(i) {
-  let r2 = await functions_names_paths_getter();
+  let r = await functions_names_paths();
+  let r2 = property_get_curried(r);
   let result = list_to_dictionary_value(i, r2);
   return result;
 }
