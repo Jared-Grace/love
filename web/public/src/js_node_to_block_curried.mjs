ff98304f207293a9e5f1378962ee158636bcb653
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:57:14 -0400

    cu js_node_to_block

--- web/public/src/js_node_to_block_curried.mjs
+++ web/public/src/js_node_to_block_curried.mjs
@@ -1,1 +1,6 @@
-export function js_node_to_block_curried() {}
+import { js_node_to_block } from "../../../love/public/src/js_node_to_block.mjs";
+export function js_node_to_block_curried(ast) {
+  return function js_node_to_block_curried_result(node) {
+    return js_node_to_block(ast, node);
+  };
+}
