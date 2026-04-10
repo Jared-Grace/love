dea31ff276afc642d69c17140bc8ad9d031c1d35
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:31:38 -0400

    function_auto_path ../love/public/src/function_current_selects_add.mjs

--- public/src/function_current_selects_add.mjs
+++ public/src/function_current_selects_add.mjs
@@ -1,11 +1,11 @@
 import { function_current_selects_on_previous } from "../../../love/public/src/function_current_selects_on_previous.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export async function function_current_selects_add(item_to_add, on_previous) {
-  async function lambda(previous) {
+  async function on_previous_inner(previous) {
     list_add(previous, item_to_add);
     on_previous(previous);
     return previous;
   }
-  let r = await function_current_selects_on_previous(lambda);
+  let r = await function_current_selects_on_previous(on_previous_inner);
   return r;
 }
