271212e151cb8a723b16293a89638a8c441f28e9
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 13:00:51 -0400

    p

---
+++ web/public/src/function_name_to_part_replace_last_3.mjs
@@ -0,0 +1,10 @@
+import { function_name_to_part_replace_last_multiple } from "../../../love/public/src/function_name_to_part_replace_last_multiple.mjs";
+export function function_name_to_part_replace_last_3(f_name_old, replacement) {
+  let count = 2;
+  let f_name_new = function_name_to_part_replace_last_multiple(
+    f_name_old,
+    count,
+    replacement,
+  );
+  return f_name_new;
+}
