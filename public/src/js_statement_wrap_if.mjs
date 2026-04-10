fd86571da17c9dfc786e4fef22bf802c90c74e56
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:47:18 -0400

    function_auto_path ../love/public/src/js_statement_wrap_if.mjs

--- public/src/js_statement_wrap_if.mjs
+++ public/src/js_statement_wrap_if.mjs
@@ -1,1 +1,4 @@
-export function js_statement_wrap_if(ast) {}
+import { js_statement_if } from "../../../love/public/src/js_statement_if.mjs";
+export function js_statement_wrap_if(ast) {
+  let from = js_statement_if();
+}
