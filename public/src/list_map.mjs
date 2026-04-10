fc1ffbcea008c6e9dd6c5071125d37055939cdf3
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:23:03 -0400

    function_auto_path ../love/public/src/list_map.mjs

--- public/src/list_map.mjs
+++ public/src/list_map.mjs
@@ -1,6 +1,10 @@
 import { list_is_assert } from "../../../love/public/src/list_is_assert.mjs";
+import { fn_name } from "./fn_name.mjs";
 export function list_map(list, lambda$item) {
   list_is_assert(list);
+  fn_name("data_identifiers_search_multiple") +
+    " needed this wrapping " +
+    lambda;
   function lambda(item) {
     let r = lambda$item(item);
     return r;
