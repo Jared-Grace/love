313a656b8021ae0a01027794e58cdfb54da00adc
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:54:50 -0400

    function_auto_path ../love/public/src/js_statements_with_identifiers_named.mjs

--- public/src/js_statements_with_identifiers_named.mjs
+++ public/src/js_statements_with_identifiers_named.mjs
@@ -1,4 +1,4 @@
 import { js_identifiers_named } from "../../../love/public/src/js_identifiers_named.mjs";
-export function js_statements_with_identifiers_named(ast) {
+export function js_statements_with_identifiers_named(ast, identifier_name) {
   let identifiers_named = js_identifiers_named(ast, identifier_name);
 }
