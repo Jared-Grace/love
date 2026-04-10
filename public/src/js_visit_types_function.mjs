446a557d7cd199c64a8a8dfccbca22f024930fc3
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 18:47:41 -0400

    w c js_visit_types,function

--- public/src/js_visit_types_function.mjs
+++ public/src/js_visit_types_function.mjs
@@ -1,1 +1,4 @@
-export function js_visit_types_function() {}
+import { js_visit_types } from "../../../love/public/src/js_visit_types.mjs";
+export function js_visit_types_function(ast, types, lambda$v) {
+  return js_visit_types(ast, types, lambda$v);
+}
