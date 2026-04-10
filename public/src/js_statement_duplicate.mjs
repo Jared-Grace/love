a1b50142f94264a76758430a0c2695c633cb8447
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:38:28 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- public/src/js_statement_duplicate.mjs
+++ public/src/js_statement_duplicate.mjs
@@ -7,7 +7,7 @@ import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
 import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
 export async function js_statement_duplicate(ast, selects) {
-  let first = list_first(list);
+  let id = list_first(selects);
   let node = js_visit_id_to_node(ast, id);
   let v = js_node_to_visitor(ast, node);
   let stack = property_get(v, "stack");
