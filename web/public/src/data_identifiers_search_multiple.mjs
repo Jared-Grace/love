23fa1749b72544431c079baab1b8a84570492652
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:11:10 -0400

    function_auto_path ../love/public/src/data_identifiers_search_multiple.mjs

--- web/public/src/data_identifiers_search_multiple.mjs
+++ web/public/src/data_identifiers_search_multiple.mjs
@@ -2,9 +2,8 @@ import { list_map_unordered_async } from "../../../love/public/src/list_map_unor
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function data_identifiers_search_multiple(ids_comma) {
-  let split = text_split_dot_comma(ids_comma);
-  async function lambda(item) {}
-  let waited = await list_map_unordered_async(list, lambda);
+  let ids = text_split_dot_comma(ids_comma);
+  let waited = await list_map_unordered_async(ids, data_identifiers_search);
   let r = await data_identifiers_search(ids_comma);
   return r;
 }
