6703359fe966a2879f298c781f6f192f385d3b5f
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:11:15 -0400

    function_auto_path ../love/public/src/data_identifiers_search.mjs

--- web/public/src/data_identifiers_search.mjs
+++ web/public/src/data_identifiers_search.mjs
@@ -1,9 +1,9 @@
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { data_identifiers_search_generic } from "../../../love/public/src/data_identifiers_search_generic.mjs";
 import { data_identifiers_get } from "../../../love/public/src/data_identifiers_get.mjs";
-export async function data_identifiers_search(s) {
+export async function data_identifiers_search(id) {
   arguments_assert(arguments, 1);
   let fn = data_identifiers_get;
-  let result = await data_identifiers_search_generic(fn, s);
+  let result = await data_identifiers_search_generic(fn, id);
   return result;
 }
