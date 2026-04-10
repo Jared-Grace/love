e5ee12f9bc19039b1779fcfd8e9108cd3fa0c35d
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:36:44 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -1,11 +1,9 @@
-import { js_visit_id_to_node_or_id_multiple } from "../../../love/public/src/js_visit_id_to_node_or_id_multiple.mjs";
-import { function_current_selects } from "../../../love/public/src/function_current_selects.mjs";
+import { function_current_selects_nodes } from "../../../love/public/src/function_current_selects_nodes.mjs";
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
-  let selects = await function_current_selects();
-  selects = js_visit_id_to_node_or_id_multiple(selects, ast);
+  let selects = await function_current_selects_nodes();
   let f_name_current = await function_current_get();
   let imported_fn = await function_import(apply_fn_name);
   function lambda(ast) {
