79ccab80004be90ee72e0561eba439f855bc223c
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:36:06 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -5,10 +5,10 @@ import { function_transform } from "../../../love/public/src/function_transform.
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
   let selects = await function_current_selects();
+  selects = js_visit_id_to_node_or_id_multiple(selects, ast);
   let f_name_current = await function_current_get();
   let imported_fn = await function_import(apply_fn_name);
   function lambda(ast) {
-    selects = js_visit_id_to_node_or_id_multiple(selects, ast);
     imported_fn(ast, selects);
   }
   let output = await function_transform(f_name_current, lambda);
