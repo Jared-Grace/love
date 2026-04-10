d372b337a43360b7bf27af0925227568095ca0a4
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:45:12 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- web/public/src/js_block_find_from_nodes_single.mjs
+++ web/public/src/js_block_find_from_nodes_single.mjs
@@ -1,6 +1,6 @@
 import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
-export function js_block_find_from_nodes_single(nodes, ast) {
+export function js_block_find_from_nodes_single(ast, nodes) {
   let node = list_single(nodes);
   let r = js_block_find_from_node(ast, node);
   return r;
