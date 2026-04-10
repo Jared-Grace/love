afb79a8e866ccdd52e4adf75e2962fd7ef359c61
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:46:37 -0400

    function_auto_path ../love/public/src/data_identifiers_search_generic.mjs

--- web/public/src/data_identifiers_search_generic.mjs
+++ web/public/src/data_identifiers_search_generic.mjs
@@ -1,9 +1,11 @@
+import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { function_list_names_to_paths } from "../../../love/public/src/function_list_names_to_paths.mjs";
 import { list_intersect_multiple } from "../../../love/public/src/list_intersect_multiple.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
 export async function data_identifiers_search_generic(fn, ids_comma) {
+  text_is_assert(value);
   let identifiers = await fn();
   let ids = text_split_comma_dot(ids_comma);
   let r3 = property_get_curried(identifiers);
