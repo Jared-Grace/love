bb1cb69cc372ac490dc74a1d11bbc1a588f17062
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:27:55 -0400

    cur list_to_dictionary_value

--- public/src/list_to_dictionary_value_curried_right.mjs
+++ public/src/list_to_dictionary_value_curried_right.mjs
@@ -1,1 +1,6 @@
-export function list_to_dictionary_value_curried_right() {}
+import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
+export function list_to_dictionary_value_curried_right(lambda$item2v) {
+  return function list_to_dictionary_value_curried_right_result(list) {
+    return list_to_dictionary_value(list, lambda$item2v);
+  };
+}
