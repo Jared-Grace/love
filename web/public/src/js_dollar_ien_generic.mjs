1cd30a4035ccd85e2441b163255709e843c8ba36
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:57:27 -0400

    function_auto_path ../love/public/src/js_dollar_ien_generic.mjs

--- web/public/src/js_dollar_ien_generic.mjs
+++ web/public/src/js_dollar_ien_generic.mjs
@@ -1,4 +1,4 @@
-import { property_set } from "../../../love/public/src/property_set.mjs";
+import { js_statement_if_test_set } from "../../../love/public/src/js_statement_if_test_set.mjs";
 import { js_call_new_expression } from "../../../love/public/src/js_call_new_expression.mjs";
 import { js_dollar_i } from "../../../love/public/src/js_dollar_i.mjs";
 export async function js_dollar_ien_generic(stack1, fn, ast) {
@@ -6,5 +6,5 @@ export async function js_dollar_ien_generic(stack1, fn, ast) {
     stack1,
   });
   let expression = await js_call_new_expression(fn.name, ast);
-  property_set(stack1, "test", expression);
+  js_statement_if_test_set(stack1, expression);
 }
