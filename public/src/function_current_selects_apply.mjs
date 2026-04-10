128abd2cb605e351735e06371ef69aef9a47f623
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:32:17 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -1,6 +1,6 @@
+import { function_current_selects_on_previous } from "../../../love/public/src/function_current_selects_on_previous.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
-import { function_current_selects_replace } from "../../../love/public/src/function_current_selects_replace.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_current_selects_first } from "../../../love/public/src/function_current_selects_first.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
@@ -8,6 +8,6 @@ export async function function_current_selects_apply(apply_fn_name) {
   let f_name_current = await function_current_get();
   let output = await function_transform(f_name_current, lambda);
   let result = await function_run(apply_fn_name, [r]);
-  let r3 = await function_current_selects_replace(r, result);
+  let r3 = await function_current_selects_on_previous(r, result);
   return r3;
 }
