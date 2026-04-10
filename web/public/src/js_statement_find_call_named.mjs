02a862bee6669b095acea4ee931e91de59b50058
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:04:52 -0400

    function_auto_path ../love/public/src/js_statement_find_call_named.mjs

--- web/public/src/js_statement_find_call_named.mjs
+++ web/public/src/js_statement_find_call_named.mjs
@@ -7,6 +7,7 @@ export async function js_statement_find_call_named(ast, f_name) {
   let unaliased = await function_name_unalias_only(f_name);
   let only = js_call_named(ast, unaliased);
   let v = js_node_to_visitor(ast, only);
+  let stack = property_get(v, "stack");
   let r = js_block_find(v);
   let item = property_get(r, "item");
   return item;
