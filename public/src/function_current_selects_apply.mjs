05c80d6c61707ff6d1a901941764b9fc8047a5e7
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:27:44 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -4,8 +4,8 @@ import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_current_selects_first } from "../../../love/public/src/function_current_selects_first.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
   let r = await function_current_selects_first();
-  let n = await function_run(apply_fn_name, [r]);
+  let result = await function_run(apply_fn_name, [r]);
   let r2 = list_remove_curried_right(r);
-  let r3 = await function_current_selects_add(n, r2);
+  let r3 = await function_current_selects_add(result, r2);
   return r3;
 }
