566aa8b94b1f634b33dc7594ef09f6b989b68640
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 10:24:49 -0400

    p

--- public/src/app_bible_home_generic.mjs
+++ public/src/app_bible_home_generic.mjs
@@ -47,7 +47,8 @@ export async function app_bible_home_generic(context, lambda$a) {
   html_centered(bar);
   let e = ebible_folder_english();
   let hash = html_hash_object_get();
-  app_bible_chapter_set_default(context);
+  if (app_bible_chapter_set_default(context)) {
+  }
   let verse_number_hash = app_bible_hash_v_get(hash);
   let chapter_code = property_get(hash, "c");
   let v2 = ebible_chapter_code_parse(chapter_code);
