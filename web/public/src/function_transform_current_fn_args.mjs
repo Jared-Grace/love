8dbf18c2eb98a44124d60681cec370aede99a044
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 22:41:50 -0400

    de function_transform_current_fn_args

--- web/public/src/function_transform_current_fn_args.mjs
+++
@@ -1,7 +0,0 @@
-import { invoke_later } from "../../../love/public/src/invoke_later.mjs";
-import { function_transform_current_fn } from "../../../love/public/src/function_transform_current_fn.mjs";
-export async function function_transform_current_fn_args(lambda, args) {
-  let later = invoke_later(lambda, args);
-  let r = await function_transform_current_fn(later);
-  return r;
-}
