ffcfcc80403ba8d53857ecfc670e1b7e671b4672
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:38:20 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- public/src/js_statement_duplicate.mjs
+++ public/src/js_statement_duplicate.mjs
@@ -1,3 +1,4 @@
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_visit_declarators_uniqueify } from "../../../love/public/src/js_visit_declarators_uniqueify.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
@@ -5,17 +6,16 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
 import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
-export async function js_statement_duplicate(id) {
-  async function lambda(ast) {
-    let node = js_visit_id_to_node(ast, id);
-    let v = js_node_to_visitor(ast, node);
-    let stack = property_get(v, "stack");
-    let r = js_block_find(stack);
-    let item = property_get(r, "item");
-    let index = property_get(r, "index");
-    let body = property_get(r, "body");
-    let copy = json_copy(item);
-    js_visit_declarators_uniqueify(ast, copy);
-    list_insert(body, index + 1, copy);
-  }
+export async function js_statement_duplicate(ast, selects) {
+  let first = list_first(list);
+  let node = js_visit_id_to_node(ast, id);
+  let v = js_node_to_visitor(ast, node);
+  let stack = property_get(v, "stack");
+  let r = js_block_find(stack);
+  let item = property_get(r, "item");
+  let index = property_get(r, "index");
+  let body = property_get(r, "body");
+  let copy = json_copy(item);
+  js_visit_declarators_uniqueify(ast, copy);
+  list_insert(body, index + 1, copy);
 }
