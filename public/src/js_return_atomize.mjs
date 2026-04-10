472e8f0eb14f144d8ca41a6c442e5b184bfb1e1b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:03:03 -0400

    function_auto_path ../love/public/src/js_return_atomize.mjs

--- public/src/js_return_atomize.mjs
+++ public/src/js_return_atomize.mjs
@@ -8,7 +8,6 @@ import { each_async } from "../../../love/public/src/each_async.mjs";
 import { js_list_type } from "../../../love/public/src/js_list_type.mjs";
 import { js_node_atomize } from "../../../love/public/src/js_node_atomize.mjs";
 import { js_visit_match } from "../../../love/public/src/js_visit_match.mjs";
-import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
 export async function js_return_atomize(ast) {
   let rs = js_list_type(ast, "ReturnStatement");
   async function lambda(v) {
