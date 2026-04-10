673553814c09c904b94fa6344c6655d4a87db9c8
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:34:36 -0400

    function_auto_path ../love/public/src/function_current_selects_first.mjs

--- public/src/function_current_selects_first.mjs
+++ public/src/function_current_selects_first.mjs
@@ -1,7 +1,7 @@
+import { function_current_selects } from "../../../love/public/src/function_current_selects.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
-import { user_data_get } from "../../../love/public/src/user_data_get.mjs";
 export async function function_current_selects_first() {
-  let ids = await user_data_get("function_current_selects");
+  let ids = await function_current_selects();
   let first_id = list_first(ids);
   return first_id;
 }
