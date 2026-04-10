095500d01a748c994beb6b8e0eb670cba4af5fc1
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:49:34 -0400

    frp functions_names_paths functions_names_to_paths

--- public/src/functions_search_generic.mjs
+++ public/src/functions_search_generic.mjs
@@ -1,9 +1,9 @@
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
-import { functions_names_paths } from "../../../love/public/src/functions_names_paths.mjs";
+import { functions_names_to_paths } from "../../../love/public/src/functions_names_to_paths.mjs";
 import { search_generic } from "../../../love/public/src/search_generic.mjs";
 export async function functions_search_generic(search, fn) {
-  let r = await functions_names_paths();
+  let r = await functions_names_to_paths();
   let r2 = property_get_curried(r);
   let properties = properties_get(r);
   let result2 = search_generic(search, properties, r2, fn);
