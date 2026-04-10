cbbc76f349348d5706f67efc1763d7f3e232f822
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:52:00 -0400

    function_auto_path ../love/public/src/js_block_to_body.mjs

--- web/public/src/js_block_to_body.mjs
+++ web/public/src/js_block_to_body.mjs
@@ -1,7 +1,7 @@
-import { js_node_type_is_assert } from "../../../love/public/src/js_node_type_is_assert.mjs";
+import { js_statement_block_is_assert } from "../../../love/public/src/js_statement_block_is_assert.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_block_to_body(block) {
-  js_node_type_is_assert(block, "BlockStatement");
+  js_statement_block_is_assert(block);
   let value = property_get(block, "body");
   return value;
 }
