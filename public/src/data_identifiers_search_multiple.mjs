b05a8e40109b9e14c4d3c738a28ad0d7f649d83c
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:12:16 -0400

    function_auto_path ../love/public/src/data_identifiers_search_multiple.mjs

--- public/src/data_identifiers_search_multiple.mjs
+++ public/src/data_identifiers_search_multiple.mjs
@@ -8,8 +8,7 @@ export async function data_identifiers_search_multiple(ids_comma) {
   let ids = text_split_dot_comma(ids_comma);
   let waited = await list_map_unordered_async(ids, data_identifiers_search);
   let properties = properties_get(obj);
-  function lambda(item) {}
-  let mapped = list_map(list2, lambda);
+  let mapped = list_map(list2, properties_get);
   let i = list_intersect_multiple(list);
   return r;
 }
