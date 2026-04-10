a5d99640976d704827de12eba80004ddb2e30be0
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:30:20 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- public/src/function_name_new_get_args.mjs
+++ public/src/function_name_new_get_args.mjs
@@ -4,11 +4,11 @@ import { list_concat_single } from "../../../love/public/src/list_concat_single.
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 export async function function_name_new_get_args(plugin_fn, f_name_old, args) {
   let split = text_split_comma(args);
-  let args_run = list_concat_single(f_name_old, split);
   ({ f_name_old, plugin_fn } = await function_name_new_get_generic(
     f_name_old,
     plugin_fn,
   ));
+  let args_run = list_concat_single(f_name_old, split);
   let f_name_new = await function_run(plugin_fn, args_run);
   let r2 = {
     f_name_new,
