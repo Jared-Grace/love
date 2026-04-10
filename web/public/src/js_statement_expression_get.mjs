cb7015be071fdbf5510aedc7aaf89fbc84d06deb
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:54:26 -0400

    function_auto_path ../love/public/src/js_statement_expression_get.mjs

--- web/public/src/js_statement_expression_get.mjs
+++ web/public/src/js_statement_expression_get.mjs
@@ -2,6 +2,6 @@ import { js_statement_expression_is_assert } from "../../../love/public/src/js_s
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_statement_expression_get(node) {
   js_statement_expression_is_assert(node);
-  let value = property_get(node, "expression");
-  return value;
+  let expression = property_get(node, "expression");
+  return expression;
 }
