8174d1fdc4cd2b25a7cd728b7ee4bb9b22934c75
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:30:00 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -5,6 +5,7 @@ import { function_current_selects_first } from "../../../love/public/src/functio
 export async function function_current_selects_apply(apply_fn_name) {
   let r = await function_current_selects_first();
   let f_name_current = await function_current_get();
+  let output = await function_transform(f_name_current, lambda);
   let result = await function_run(apply_fn_name, [r]);
   let r3 = await function_current_selects_replace(r, result);
   return r3;
