8b56363581d3a5fb0c869bc1aae9c117e3ce52f4
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 08:15:17 -0400

    function_auto_path ../love/public/src/data_identifiers_search_multiple.mjs

--- public/src/data_identifiers_search_multiple.mjs
+++ public/src/data_identifiers_search_multiple.mjs
@@ -4,10 +4,13 @@ import { list_intersect_multiple } from "../../../love/public/src/list_intersect
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 import { text_split_dot_comma } from "../../../love/public/src/text_split_dot_comma.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
+import { list_to_dictionary_value } from "./list_to_dictionary_value.mjs";
 export async function data_identifiers_search_multiple(ids_comma) {
   let ids = text_split_dot_comma(ids_comma);
   let waited = await list_map_unordered_async(ids, data_identifiers_search);
   let mapped = list_map(waited, properties_get);
   let i = list_intersect_multiple(mapped);
+  function lambda(item2v) {}
+  let dictionary = list_to_dictionary_value(list, lambda);
   return r;
 }
