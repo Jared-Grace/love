8e301dc7f579431e9464261da3837ecf7a0f5f54
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 16:44:42 -0400

    function_auto_path ../love/public/src/data_identifiers_search_unalias.mjs

--- web/public/src/data_identifiers_search_unalias.mjs
+++ web/public/src/data_identifiers_search_unalias.mjs
@@ -1,7 +1,10 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function data_identifiers_search_unalias(ids_comma) {
   let split = text_split_comma_dot(ids_comma);
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
   let r = await data_identifiers_search(ids_comma);
   return r;
 }
