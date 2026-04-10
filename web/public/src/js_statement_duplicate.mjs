e97e8f11c07c7b29c006b71a5fc4c317aff3e600
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 12:14:19 -0400

    function_auto_path ../love/public/src/js_statement_duplicate.mjs

--- web/public/src/js_statement_duplicate.mjs
+++ web/public/src/js_statement_duplicate.mjs
@@ -1,15 +1,12 @@
+import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
 import { js_visit_id_to_node_first } from "../../../love/public/src/js_visit_id_to_node_first.mjs";
 import { js_visit_declarators_uniqueify } from "../../../love/public/src/js_visit_declarators_uniqueify.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_block_find } from "../../../love/public/src/js_block_find.mjs";
-import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 export function js_statement_duplicate(ast, selects) {
   let node = js_visit_id_to_node_first(ast, selects);
-  let v = js_node_to_visitor(ast, node);
-  let stack = property_get(v, "stack");
-  let r = js_block_find(stack);
+  let r = js_block_find_from_node(ast, node);
   let item = property_get(r, "item");
   let index = property_get(r, "index");
   let body = property_get(r, "body");
