67c3ae064ba8e0c8889a8f2640eb979e21657e2e
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 22:39:51 -0400

    function_auto_path ../love/public/src/function_name_new_get_generic.mjs

--- public/src/function_name_new_get_generic.mjs
+++ public/src/function_name_new_get_generic.mjs
@@ -1,3 +1,4 @@
+import { lambda_right } from "../../../love/public/src/lambda_right.mjs";
 import { property_exists_if_get } from "../../../love/public/src/property_exists_if_get.mjs";
 import { dictionary_functions_to_names } from "../../../love/public/src/dictionary_functions_to_names.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
@@ -6,6 +7,7 @@ export async function function_name_new_get_generic(f_name_old, plugin_fn) {
   f_name_old = await function_name_unalias_only(f_name_old);
   let overrides = {
     c: function_name_combine,
+    r: lambda_right,
   };
   dictionary_functions_to_names(overrides);
   plugin_fn = property_exists_if_get(overrides, plugin_fn);
