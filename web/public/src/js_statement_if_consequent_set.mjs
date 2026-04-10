4671a949164c941aa3bbef3f58da176b40623b00
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:50:05 -0400

    c t js_statement_if_consequent_get g,s

---
+++ web/public/src/js_statement_if_consequent_set.mjs
@@ -0,0 +1,7 @@
+import { js_statement_if_is_assert } from "../../../love/public/src/js_statement_if_is_assert.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export function js_statement_if_consequent_set(statement_if) {
+  js_statement_if_is_assert(statement_if);
+  let consequent = property_get(statement_if, "consequent");
+  return consequent;
+}
