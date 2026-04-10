a3cba59e5c56c2419f63de26b000f881068df897
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:57:00 -0400

    function_auto_path ../love/public/src/js_statement_if_test_set.mjs

---
+++ public/src/js_statement_if_is_assert.mjs
@@ -0,0 +1,4 @@
+import { js_node_type_is_assert } from "../../../love/public/src/js_node_type_is_assert.mjs";
+export function js_statement_if_is_assert(statement_if) {
+  js_node_type_is_assert(statement_if, "IfStatement");
+}
