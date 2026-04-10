0ec8d640609e39c26ad154a79244d65ac89bba4d
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 09:52:42 -0400

    function_auto_path ../love/public/src/js_statement_wrap_if.mjs

--- web/public/src/js_statement_wrap_if.mjs
+++ web/public/src/js_statement_wrap_if.mjs
@@ -1,8 +1,10 @@
+import { js_statement_expression_get } from "../../../love/public/src/js_statement_expression_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_visit_id_to_node_first } from "../../../love/public/src/js_visit_id_to_node_first.mjs";
 import { js_statement_if } from "../../../love/public/src/js_statement_if.mjs";
 export function js_statement_wrap_if(ast, selects) {
   let node = js_visit_id_to_node_first(ast, selects);
+  let value = js_statement_expression_get(node2);
   let statement_if = js_statement_if();
   log(js_statement_wrap_if.name, {
     node,
