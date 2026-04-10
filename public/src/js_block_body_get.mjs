9fc4013fdaea1d9f1819b1ddb6e8f8907cc6deb9
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 11:55:38 -0400

    r 1 js_block_body_to get

--- public/src/js_block_body_get.mjs
+++ public/src/js_block_body_get.mjs
@@ -1,6 +1,6 @@
 import { js_statement_block_is_assert } from "../../../love/public/src/js_statement_block_is_assert.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_block_body_to(block) {
+export function js_block_body_get(block) {
   js_statement_block_is_assert(block);
   let value = property_get(block, "body");
   return value;
