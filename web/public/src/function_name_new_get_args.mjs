7907a32ef8e8d5f5d9dee372d27ac48d6bb901bb
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 17:14:20 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- web/public/src/function_name_new_get_args.mjs
+++ web/public/src/function_name_new_get_args.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
 import { function_name_new_get_generic } from "../../../love/public/src/function_name_new_get_generic.mjs";
@@ -15,9 +14,6 @@ export async function function_name_new_get_args(
     f_name_old,
     plugin_fn,
   ));
-  log(function_name_new_get_args.name, {
-    split,
-  });
   let args_run = list_concat_single(f_name_old, split);
   let f_name_new = await function_run(plugin_fn, args_run);
   let r2 = {
