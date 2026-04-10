0d5a23704c2238ba27fd45b413ff882e09489bb0
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 17:01:01 -0400

    function_auto_path ../love/public/src/js_flo_body_add_return_argument_from_code.mjs

--- web/public/src/js_flo_body_add_return_argument_from_code.mjs
+++ web/public/src/js_flo_body_add_return_argument_from_code.mjs
@@ -1,5 +1,7 @@
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { js_flo_body_add_return_argument } from "../../../portfolio_qa/public/src/js_flo_body_add_return_argument.mjs";
-export function js_flo_body_add_return_argument_from_code(ast, e) {
-  let r = js_flo_body_add_return_argument(ast, e);
+export function js_flo_body_add_return_argument_from_code(ast, code_argument) {
+  let expression = js_parse_expression(code_argument);
+  let r = js_flo_body_add_return_argument(ast, expression);
   return r;
 }
