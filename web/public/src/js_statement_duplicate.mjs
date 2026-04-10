75d1c301ec9a79a74634e0cde7d19e2450e019a7
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:29:06 -0400

    function_auto_path ../love/public/src/js_visit_id_to_block_single.mjs

--- web/public/src/js_statement_duplicate.mjs
+++ web/public/src/js_statement_duplicate.mjs
@@ -1,10 +1,10 @@
-import { js_visit_id_to_block } from "../../../love/public/src/js_visit_id_to_block.mjs";
+import { js_visit_id_to_block_single } from "../../../love/public/src/js_visit_id_to_block_single.mjs";
 import { js_visit_declarators_uniqueify } from "../../../love/public/src/js_visit_declarators_uniqueify.mjs";
 import { list_insert } from "../../../love/public/src/list_insert.mjs";
 import { json_copy } from "../../../love/public/src/json_copy.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_statement_duplicate(ast, selects) {
-  let r = js_visit_id_to_block(ast, selects);
+  let r = js_visit_id_to_block_single(ast, selects);
   let item = property_get(r, "item");
   let index = property_get(r, "index");
   let body = property_get(r, "body");
