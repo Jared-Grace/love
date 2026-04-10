b36de4fb66262cd33cfaf6df60d25a3cb5dec9c7
JESUS rose to life <username@users.noreply.github.com>
2026-04-09 22:38:59 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- public/src/function_name_new_get_args.mjs
+++ public/src/function_name_new_get_args.mjs
@@ -6,9 +6,9 @@ import { list_concat_single } from "../../../love/public/src/list_concat_single.
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function function_name_new_get_args(plugin_fn, f_name_old, args) {
-  f_name_old = await function_name_unalias_only(f_name_old);
   let split = text_split_comma(args);
   let args_run = list_concat_single(f_name_old, split);
+  f_name_old = await function_name_unalias_only(f_name_old);
   let overrides = {
     c: function_name_combine,
   };
