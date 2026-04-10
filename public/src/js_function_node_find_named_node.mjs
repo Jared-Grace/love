4c99b7f090a4911064f9e9c120bada5e3d0ea027
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 19:20:42 -0400

    w c js_function_node_find_named,node

--- public/src/js_function_node_find_named_node.mjs
+++ public/src/js_function_node_find_named_node.mjs
@@ -1,1 +1,4 @@
-export function js_function_node_find_named_node() {}
+import { js_function_node_find_named } from "../../../love/public/src/js_function_node_find_named.mjs";
+export function js_function_node_find_named_node(ast, name) {
+  return js_function_node_find_named(ast, name);
+}
