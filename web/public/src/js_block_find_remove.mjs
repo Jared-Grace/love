1c3d4c9790bd2e0b81aad5f7d791e2444f9c59f1
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 12:17:34 -0400

    function_auto_path ../love/public/src/js_block_find_remove.mjs

--- web/public/src/js_block_find_remove.mjs
+++ web/public/src/js_block_find_remove.mjs
@@ -1,7 +1,7 @@
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_block_find_remove(r) {
-  let item = property_get(r, "item");
-  let body = property_get(r, "body");
+export function js_block_find_remove(f) {
+  let item = property_get(f, "item");
+  let body = property_get(f, "body");
   list_remove(body, item);
 }
