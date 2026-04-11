bbd1f630b9901a03b2ec236ac39bf8eacfaf8cad
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:21:35 -0400

    function_auto_path ../love/public/src/js_node_to_visitors.mjs

--- web/public/src/js_node_to_visitors.mjs
+++ web/public/src/js_node_to_visitors.mjs
@@ -1,3 +1,4 @@
+import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -6,6 +7,7 @@ export function js_node_to_visitors(ast, node_search) {
     function lambda(v) {
       let node = property_get(v, "node");
       if (node === node_search) {
+        let e1 = list_get_end_1(stack);
         la(v);
       }
     }
