26fe80351d3d809269be3936f50cb9f8dc840bfb
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 20:10:29 -0400

    cu js_visit_id_or_node

--- public/src/js_visit_id_or_node_curried.mjs
+++ public/src/js_visit_id_or_node_curried.mjs
@@ -1,1 +1,6 @@
-export function js_visit_id_or_node_curried() {}
+import { js_visit_id_or_node } from "../../../love/public/src/js_visit_id_or_node.mjs";
+export function js_visit_id_or_node_curried(ast) {
+  return function js_visit_id_or_node_curried_result(n) {
+    return js_visit_id_or_node(ast, n);
+  };
+}
