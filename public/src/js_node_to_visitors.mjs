b498ce1637b96ede9f6879c502f9922a8fa10fcb
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:30:29 -0400

    function_auto_path ../love/public/src/js_node_to_visitors.mjs

--- public/src/js_node_to_visitors.mjs
+++ public/src/js_node_to_visitors.mjs
@@ -1,3 +1,4 @@
+import { set_new } from "../../../love/public/src/set_new.mjs";
 import { object_properties_equal } from "../../../love/public/src/object_properties_equal.mjs";
 import { js_import_specifier_is } from "../../../love/public/src/js_import_specifier_is.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
@@ -5,6 +6,7 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_node_to_visitors(ast, node_search) {
+  let v2 = set_new();
   function lambda2(la) {
     function lambda(v) {
       let node = property_get(v, "node");
