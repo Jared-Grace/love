26133dd0f2d50b8134a013d44ad669c40b442ad6
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:35:38 -0400

    function_auto_path ../love/public/src/set_add_try_curried.mjs

--- web/public/src/set_add_try_curried.mjs
+++ web/public/src/set_add_try_curried.mjs
@@ -1,6 +1,8 @@
 import { set_add_try } from "../../../love/public/src/set_add_try.mjs";
 export function set_add_try_curried(set) {
-  return function set_add_try_curried_result(item) {
-    return set_add_try(set, item);
+  let r = function set_add_try_curried_result(item) {
+    let n = set_add_try(set, item);
+    return n;
   };
+  return r;
 }
