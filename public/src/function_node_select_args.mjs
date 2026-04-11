9793894833d0c8e2ceee8b9188bfc7fd7ad05bac
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:43:59 -0400

    function_auto_path ../love/public/src/function_node_select_args.mjs

--- public/src/function_node_select_args.mjs
+++ public/src/function_node_select_args.mjs
@@ -5,8 +5,8 @@ import { function_node_select_inner } from "../../../love/public/src/function_no
 import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
 export async function function_node_select_args(select_fn_name, args_comma) {
   let ast = await function_current_ast();
-  let id = js_visit_id(ast, ast);
-  let declaration = js_flo(ast2);
+  let declaration = js_flo(ast);
+  let id = js_visit_id(ast, declaration);
   let r = await function_node_select_inner(
     select_fn_name,
     id,
