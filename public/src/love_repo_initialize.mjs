9081e466779e9449988fe9abda32b899c20d78a6
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 13:48:01 -0400

    ao love_repo_initialize

--- public/src/love_repo_initialize.mjs
+++ public/src/love_repo_initialize.mjs
@@ -1,17 +1,9 @@
-import { add } from "../../../love/public/src/add.mjs";
-import { data_folder } from "../../../love/public/src/data_folder.mjs";
-import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
-import { folder_vscode } from "../../../love/public/src/folder_vscode.mjs";
-import { path_join } from "../../../love/public/src/path_join.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { love_repo_initialize_paths } from "../../../love/public/src/love_repo_initialize_paths.mjs";
 import { file_copy_overwrite } from "../../../love/public/src/file_copy_overwrite.mjs";
-import { file_name_json_folder } from "./file_name_json_folder.mjs";
 export async function love_repo_initialize() {
-  let vc = folder_vscode();
-  let f_path = file_name_json_folder(vc, "tasks");
-  let d_path = data_folder();
-  let file_path_old = path_join([d_path, f_path]);
-  let previous = folder_previous();
-  let file_path_new = path_join([previous, f_path]);
+  let r = love_repo_initialize_paths();
+  let file_path_new = property_get(r, "file_path_new");
+  let file_path_old = property_get(r, "file_path_old");
   await file_copy_overwrite(file_path_old, file_path_new);
-  let sum = add(left, right);
 }
