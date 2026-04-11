6d8ba11aed171e75f6d7c2a2aa14781646810072
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 17:14:51 -0400

    function_auto_path ../love/public/src/js_call_arguments_get.mjs

--- public/src/js_call_arguments_get.mjs
+++ public/src/js_call_arguments_get.mjs
@@ -1,7 +1,9 @@
+import { js_call_is } from "../../../love/public/src/js_call_is.mjs";
 import { js_special_arguments } from "../../../love/public/src/js_special_arguments.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_call_arguments_get(e) {
   let property_name = js_special_arguments();
   let value = property_get(e, property_name);
   return value;
+  js_call_is(expression);
 }
