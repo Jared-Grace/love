5885502155f9df39eb6667a73f0f436e04eb174f
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:57:31 -0400

    function_auto_path ../love/public/src/function_transform_multiple.mjs

--- public/src/function_transform_multiple.mjs
+++ public/src/function_transform_multiple.mjs
@@ -1,8 +1,11 @@
+import { each_async } from "../../../love/public/src/each_async.mjs";
 import { function_transform_single } from "../../../love/public/src/function_transform_single.mjs";
 export async function function_transform_multiple(
   f_name_transformer_args_comma,
   f_name,
-) {ea
+) {
+  async function lambda(item) {}
+  await each_async(list, lambda);
   let r = await function_transform_single(
     f_name_transformer_args_comma,
     f_name,
