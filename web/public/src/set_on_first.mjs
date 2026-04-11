d57ec81474e92ad2a05afe498991940d91b2c7f7
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:32:50 -0400

    function_auto_path ../love/public/src/set_on_first.mjs

--- web/public/src/set_on_first.mjs
+++ web/public/src/set_on_first.mjs
@@ -1,6 +1,9 @@
+import { set_includes } from "../../../love/public/src/set_includes.mjs";
 import { set_new } from "../../../love/public/src/set_new.mjs";
 export function set_on_first(sof) {
   let s = set_new();
-  function lambda() {}
-  sof(lambda);
+  function on_first_check(item) {
+    let si = set_includes(set, item2);
+  }
+  sof(on_first_check);
 }
