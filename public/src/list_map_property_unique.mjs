3fc27bda8c0431071b71838c80dd90830a59da4a
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 20:15:34 -0400

    function_auto_path ../love/public/src/list_map_property_unique.mjs

--- public/src/list_map_property_unique.mjs
+++ public/src/list_map_property_unique.mjs
@@ -1,7 +1,7 @@
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-export function list_map_property_unique(fs, property_name) {
-  let statements = list_map_property(fs, property_name);
-  let unique = list_unique(list);
-  return statements;
+export function list_map_property_unique(list, property_name) {
+  let mapped = list_map_property(list, property_name);
+  let unique = list_unique(mapped);
+  return unique;
 }
