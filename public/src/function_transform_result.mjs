983dd592c1be51e798865e85247e277ba1d0b053
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:51:36 -0400

    function_auto_path ../love/public/src/function_transform_result.mjs

--- public/src/function_transform_result.mjs
+++ public/src/function_transform_result.mjs
@@ -3,9 +3,6 @@ import { function_parse_unaliased } from "../../../love/public/src/function_pars
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
-import { data_path } from "../../../love/public/src/data_path.mjs";
-import { data_all_initialize } from "../../../love/public/src/data_all_initialize.mjs";
-import { file_transform_cached } from "../../../love/public/src/file_transform_cached.mjs";
 import { file_js_unparse } from "../../../love/public/src/file_js_unparse.mjs";
 export async function function_transform_result(f_names, lambda$ast) {
   let split = text_split_comma(f_names);
@@ -19,9 +16,4 @@ export async function function_transform_result(f_names, lambda$ast) {
   }
   let r3 = await list_map_async(split, lambda_each_function);
   return r3;
-  return;
-  let d_path = data_path();
-  await data_all_initialize(d_path);
-  let r = await file_transform_cached(d_path, lambda2);
-  return r;
 }
