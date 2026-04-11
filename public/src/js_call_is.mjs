c8510a78658ff67c872da1076b8d94d3ba0d01b2
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 17:03:46 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_call_is.mjs
+++ public/src/js_call_is.mjs
@@ -1,5 +1,5 @@
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 export function js_call_is(node) {
-  let type_is2 = js_node_type_is(node, "CallExpression");
-  return type_is2;
+  let ci = js_node_type_is(node, "CallExpression");
+  return ci;
 }
