7d35b744b6a6f94718a1c49d72cd3cc185f6668e
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 20:14:23 -0400

    function_auto_path ../love/public/src/function_current_selects_replace.mjs

--- public/src/function_current_selects_replace.mjs
+++ public/src/function_current_selects_replace.mjs
@@ -1,7 +1,10 @@
 import { function_current_selects_add } from "../../../love/public/src/function_current_selects_add.mjs";
 import { list_remove_curried_right } from "../../../love/public/src/list_remove_curried_right.mjs";
-export async function function_current_selects_replace(item, result) {
-  let r2 = list_remove_curried_right(item);
-  let r3 = await function_current_selects_add(result, r2);
+export async function function_current_selects_replace(
+  item_to_remove,
+  item_to_add,
+) {
+  let r2 = list_remove_curried_right(item_to_remove);
+  let r3 = await function_current_selects_add(item_to_add, r2);
   return r3;
 }
