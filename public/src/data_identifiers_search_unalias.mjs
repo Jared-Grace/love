ad7374f36168d1674c12960e003619e623286c11
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 15:29:23 -0400

    function_auto_path ../love/public/src/data_identifiers_search_unalias.mjs

--- public/src/data_identifiers_search_unalias.mjs
+++ public/src/data_identifiers_search_unalias.mjs
@@ -1,7 +1,7 @@
 import { function_aliases_to_names } from "../../../love/public/src/function_aliases_to_names.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
-export async function data_identifiers_search_unalias(ids_comma) {
-  let names = await function_aliases_to_names(ids_comma);
+export async function data_identifiers_search_unalias(aliases_comma) {
+  let names = await function_aliases_to_names(aliases_comma);
   let r = await data_identifiers_search(names);
   return r;
 }
