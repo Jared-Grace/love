81a1df64df1bacad6c6cf381c2db6f0979df2a34
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 20:11:56 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- public/src/function_node_select_inner.mjs
+++ public/src/function_node_select_inner.mjs
@@ -5,7 +5,6 @@ import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
 import { function_current_selects_add } from "../../../love/public/src/function_current_selects_add.mjs";
-import { js_visit_id_or_node } from "../../../love/public/src/js_visit_id_or_node.mjs";
 import { js_visit_id_to_node } from "../../../love/public/src/js_visit_id_to_node.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 export async function function_node_select_inner(
@@ -26,8 +25,7 @@ export async function function_node_select_inner(
   let n = await function_run(select_fn_name, concated);
   n = list_ensure(n);
   let r2 = js_visit_id_or_node_curried(ast);
-  let mapped = list_map(n, r2);
-  let item_to_add = js_visit_id_or_node(ast, n);
-  let r = await function_current_selects_add(item_to_add, on_previous);
+  let items_to_add = list_map(n, r2);
+  let r = await function_current_selects_add(items_to_add, on_previous);
   return r;
 }
