00f60c697ccf54490dcc9c486b930a50351dff0a
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:59:06 -0400

    function_auto_path ../love/public/src/function_delete.mjs

--- public/src/function_delete.mjs
+++ public/src/function_delete.mjs
@@ -1,13 +1,13 @@
+import { text_split_comma_dot_map_unordered } from "../../../love/public/src/text_split_comma_dot_map_unordered.mjs";
 import { function_aliases_delete } from "../../../love/public/src/function_aliases_delete.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
-import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { assert } from "../../../love/public/src/assert.mjs";
 import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
 import { data_files_update } from "../../../love/public/src/data_files_update.mjs";
 import { file_delete } from "../../../love/public/src/file_delete.mjs";
 export async function function_delete(f_names_comma) {
-  let f_names = text_split_comma_dot(f_names_comma);
+  let f_names = text_split_comma_dot_map_unordered(f_names_comma);
   async function lambda(f_name) {
     const u = await function_unalias_exists(f_name);
     let exists = property_get(u, "exists");
