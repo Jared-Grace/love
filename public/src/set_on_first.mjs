30a1a40c8335aa3180369fa86308330d097054af
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:35:42 -0400

    function_auto_path ../love/public/src/set_on_first.mjs

--- public/src/set_on_first.mjs
+++ public/src/set_on_first.mjs
@@ -1,5 +1,7 @@
+import { set_add_try_curried } from "../../../love/public/src/set_add_try_curried.mjs";
 import { set_new } from "../../../love/public/src/set_new.mjs";
 export function set_on_first(sof) {
-  let s = set_new();set_add_try_curried
+  let s = set_new();
+  let r = set_add_try_curried(set);
   sof(on_first_check);
 }
