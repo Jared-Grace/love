2a6aaa54c98759b08a96445599ffa129db5acdec
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:45:02 -0400

    pf app_bible_button_chapter_previous,app_bible_button_chapter_next bar

--- web/public/src/app_bible_button_chapter_previous.mjs
+++ web/public/src/app_bible_button_chapter_previous.mjs
@@ -1,6 +1,6 @@
 import { html_button_arrow_left } from "../../../love/public/src/html_button_arrow_left.mjs";
 import { app_bible_chapter_previous } from "../../../love/public/src/app_bible_chapter_previous.mjs";
-export function app_bible_button_chapter_previous(context, chapter_code, bar) {
+export function app_bible_button_chapter_previous(bar, context, chapter_code) {
   async function chapter_previous() {
     await app_bible_chapter_previous(context, chapter_code);
   }
