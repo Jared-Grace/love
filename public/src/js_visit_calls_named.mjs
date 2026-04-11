341582a5d34439206bc291005a02be3db23b2ab9
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 17:06:27 -0400

    r c js_call_callee_name,try

--- public/src/js_visit_calls_named.mjs
+++ public/src/js_visit_calls_named.mjs
@@ -1,11 +1,11 @@
 import { js_visit_calls } from "../../../love/public/src/js_visit_calls.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { js_call_callee_name } from "../../../love/public/src/js_call_callee_name.mjs";
+import { js_call_callee_name_try } from "../../../love/public/src/js_call_callee_name_try.mjs";
 export function js_visit_calls_named(ast, f_name, lambda) {
   function lambda_inner(v) {
     let node = property_get(v, "node");
-    let name = js_call_callee_name(node);
+    let name = js_call_callee_name_try(node);
     if (equal_not(name, f_name)) {
       return;
     }
