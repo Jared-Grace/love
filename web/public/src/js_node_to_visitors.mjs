aa00489585d18c6d897d5af17e0cfab422a8286b
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:38:47 -0400

    function_auto_path ../love/public/src/js_node_to_visitors.mjs

--- web/public/src/js_node_to_visitors.mjs
+++ web/public/src/js_node_to_visitors.mjs
@@ -7,25 +7,26 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_visit } from "../../../love/public/src/js_visit.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_node_to_visitors(ast, node_search) {
-  function lambda3(sa) {}
-  set_on_first(lambda3);
   function lambda2(la) {
-    function lambda(v) {
-      let node = property_get(v, "node");
-      if (node === node_search) {
-        let stack = property_get(v, "stack");
-        let e1 = list_get_end_1(stack);
-        let type_is = js_import_specifier_is(e1);
-        if (type_is) {
-          let eq = object_properties_equal(e1, ["imported", "imported"]);
-          if (eq) {
-            set_add(s, node);
+    function lambda3(sa) {
+      function lambda(v) {
+        let node = property_get(v, "node");
+        if (node === node_search) {
+          let stack = property_get(v, "stack");
+          let e1 = list_get_end_1(stack);
+          let type_is = js_import_specifier_is(e1);
+          if (type_is) {
+            let eq = object_properties_equal(e1, ["imported", "imported"]);
+            if (eq) {
+              set_add(s, node);
+            }
           }
+          la(v);
         }
-        la(v);
       }
+      js_visit(ast, lambda);
     }
-    js_visit(ast, lambda);
+    set_on_first(lambda3);
   }
   let matches = list_adder(lambda2);
   return matches;
