ec4580c5062271f3782d4b0609ae9306f25278a2
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:12:11 -0400

    function_auto_path ../love/public/src/js_function_node_expand.mjs

--- public/src/js_function_node_expand.mjs
+++ public/src/js_function_node_expand.mjs
@@ -1,3 +1,4 @@
+import { list_insert_at_multiple } from "../../../love/public/src/list_insert_at_multiple.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
@@ -8,5 +9,6 @@ export function js_function_node_expand(ast, function_node) {
   let item = property_get(r, "item");
   let index = property_get(r, "index");
   let body = property_get(r, "body");
-  list_remove(list, item);
+  list_remove(body, item);
+  list_insert_at_multiple(list, index2, inserteds);
 }
