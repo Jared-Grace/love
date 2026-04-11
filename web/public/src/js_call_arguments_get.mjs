34ef579ab82bc9c62579deed168a5b4ae51823c7
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 17:16:18 -0400

    function_auto_path ../love/public/src/js_call_arguments_get.mjs

--- web/public/src/js_call_arguments_get.mjs
+++ web/public/src/js_call_arguments_get.mjs
@@ -2,11 +2,16 @@ import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { js_call_is } from "../../../love/public/src/js_call_is.mjs";
 import { js_special_arguments } from "../../../love/public/src/js_special_arguments.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_call_arguments_get(e) {
+export function js_call_arguments_get(node) {
   let property_name = js_special_arguments();
-  let value = property_get(e, property_name);
+  let value = property_get(node, property_name);
   return value;
-  js_call_is(e);
-  function lambda2() {}
+  js_call_is(node);
+  function lambda2() {
+    let r = {
+      e: node,
+    };
+    return r;
+  }
   assert_json_get(b, lambda2);
 }
