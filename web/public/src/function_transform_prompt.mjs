4a7813d540e3e4572ebd6aec484ee6180effe78b
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:47:58 -0400

    pb function_transform_prompt

--- web/public/src/function_transform_prompt.mjs
+++ web/public/src/function_transform_prompt.mjs
@@ -1,6 +1,6 @@
 import { function_transform_result } from "../../../love/public/src/function_transform_result.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
-export async function function_transform_prompt(transformer_name, f_name) {
+export async function function_transform_prompt(f_name, transformer_name) {
   const transformer = await function_import_unalias(transformer_name);
   let r = await function_transform_result(f_name, transformer);
   return r;
