ae44c85e9b10d61fae97013aaaf4f4bf21f3b602
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 15:04:17 -0400

    function_auto_path ../love/public/src/function_param_move_first_multiple.mjs

--- web/public/src/function_param_move_first_multiple.mjs
+++ web/public/src/function_param_move_first_multiple.mjs
@@ -1,5 +1,4 @@
 import { function_params_move_first_curried_right } from "../../../love/public/src/function_params_move_first_curried_right.mjs";
-import { function_params_move_first } from "../../../love/public/src/function_params_move_first.mjs";
 import { text_split_comma_dot_each_async } from "../../../love/public/src/text_split_comma_dot_each_async.mjs";
 import { text_split_comma_dot_reverse } from "../../../love/public/src/text_split_comma_dot_reverse.mjs";
 export async function function_param_move_first_multiple(
@@ -7,9 +6,6 @@ export async function function_param_move_first_multiple(
   param_names_comma,
 ) {
   let param_names = text_split_comma_dot_reverse(param_names_comma);
-  let r2 = await function_params_move_first_curried_right(param_names2);
+  let lambda = await function_params_move_first_curried_right(param_names);
   await text_split_comma_dot_each_async(f_names_comma, lambda);
-  async function lambda(f_name) {
-    await function_params_move_first(f_name, param_names);
-  }
 }
