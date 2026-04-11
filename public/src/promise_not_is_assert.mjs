e61439fcfe3999618fca9c0e774a838f1080e5f4
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:19:38 -0400

    w c promise_not_is,assert

--- public/src/promise_not_is_assert.mjs
+++ public/src/promise_not_is_assert.mjs
@@ -1,1 +1,4 @@
-export function promise_not_is_assert() {}
+import { promise_not_is } from "../../../love/public/src/promise_not_is.mjs";
+export function promise_not_is_assert(ast) {
+  return promise_not_is(ast);
+}
