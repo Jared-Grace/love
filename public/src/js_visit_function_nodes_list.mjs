496ec7814fd586b664c1380e85413fdc530cf16a
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 15:43:11 -0400

    function_auto_path ../love/public/src/js_visit_function_nodes_list.mjs

--- public/src/js_visit_function_nodes_list.mjs
+++ public/src/js_visit_function_nodes_list.mjs
@@ -1,10 +1,9 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { list_to } from "../../../love/public/src/list_to.mjs";
 import { js_visit_function_nodes } from "../../../love/public/src/js_visit_function_nodes.mjs";
 export function js_visit_function_nodes_list(ast, lambda$v) {
-  function lambda(la) {}
+  function lambda(la) {
+    js_visit_function_nodes(ast, la);
+  }
   let list = list_adder(lambda);
-  let r = js_visit_function_nodes(ast, lambda$v);
-  let l = list_to(r);
-  return l;
+  return list;
 }
