dcc6975b97ef8c94c0d7b05a8e1f3b266f53cc15
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:19:41 -0400

    function_auto_path ../love/public/src/promise_not_is_assert.mjs

--- public/src/promise_not_is_assert.mjs
+++ public/src/promise_not_is_assert.mjs
@@ -1,4 +1,5 @@
 import { promise_not_is } from "../../../love/public/src/promise_not_is.mjs";
 export function promise_not_is_assert(ast) {
-  return promise_not_is(ast);
+  let n = promise_not_is(ast);
+  return n;
 }
