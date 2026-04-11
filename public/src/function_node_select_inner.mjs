f7f96a9f8129c974fb43cc80afb7532d0df28fd9
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 20:12:18 -0400

    function_auto_path ../love/public/src/function_node_select_inner.mjs

--- public/src/function_node_select_inner.mjs
+++ public/src/function_node_select_inner.mjs
@@ -1,5 +1,4 @@
-import { list_map } from "../../../love/public/src/list_map.mjs";
-import { js_visit_id_or_node_curried } from "../../../love/public/src/js_visit_id_or_node_curried.mjs";
+import { js_visit_id_or_node_multiple } from "../../../love/public/src/js_visit_id_or_node_multiple.mjs";
 import { list_ensure } from "../../../love/public/src/list_ensure.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
@@ -24,8 +23,7 @@ export async function function_node_select_inner(
   let concated = list_concat_single(node, args);
   let n = await function_run(select_fn_name, concated);
   n = list_ensure(n);
-  let r2 = js_visit_id_or_node_curried(ast);
-  let items_to_add = list_map(n, r2);
+  let items_to_add = js_visit_id_or_node_multiple(ast, n);
   let r = await function_current_selects_add(items_to_add, on_previous);
   return r;
 }
