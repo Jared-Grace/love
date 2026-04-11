8b853620ba1460f4cc3e3171887dc21001488cbe
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 17:18:02 -0400

    function_auto_path ../love/public/src/js_call_arguments_get.mjs

--- public/src/js_call_arguments_get.mjs
+++ public/src/js_call_arguments_get.mjs
@@ -4,6 +4,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_call_arguments_get(node_call) {
   js_call_is_assert(node_call);
   let property_name = js_special_arguments();
-  let value = property_get(node_call, property_name);
-  return value;
+  let args = property_get(node_call, property_name);
+  return args;
 }
