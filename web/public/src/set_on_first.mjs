24b8b08bc4c5bcbde340021393a6a823c81cc38e
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 22:32:22 -0400

    function_auto_path ../love/public/src/set_on_first.mjs

--- web/public/src/set_on_first.mjs
+++ web/public/src/set_on_first.mjs
@@ -1,6 +1,6 @@
 import { set_new } from "../../../love/public/src/set_new.mjs";
-export function set_on_first(first_is) {
+export function set_on_first(of) {
   let s = set_new();
   function lambda() {}
-  first_is(lambda);
+  of(lambda);
 }
