fa118d566ee3ecdae2c1e1207673c58d5cf8d917
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:57:24 -0400

    w 1 function_transform_single,multiple

--- public/src/function_transform_multiple.mjs
+++ public/src/function_transform_multiple.mjs
@@ -1,1 +1,7 @@
-export function function_transform_multiple() {}
+import { function_transform_single } from "../../../love/public/src/function_transform_single.mjs";
+export async function function_transform_multiple(
+  f_name_transformer_args_comma,
+  f_name,
+) {
+  return await function_transform_single(f_name_transformer_args_comma, f_name);
+}
