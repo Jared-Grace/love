c9556a752cce6448aa9e20c4888873f8e2248976
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 13:55:07 -0400

    function_auto_path ../love/public/src/js_function_node_expand.mjs

--- public/src/js_function_node_expand.mjs
+++ public/src/js_function_node_expand.mjs
@@ -2,5 +2,5 @@ import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.
 import { js_function_declaration_to_block_body } from "../../../love/public/src/js_function_declaration_to_block_body.mjs";
 export function js_function_node_expand(ast, function_node) {
   let body_block = js_function_declaration_to_block_body(function_node);
-  let v = js_node_to_visitor(ast2, node_search);
+  let v = js_node_to_visitor(ast, body_block);
 }
