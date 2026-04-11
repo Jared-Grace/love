3cdd17b857745f788076092098a93380063833ed
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 15:43:47 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -3,13 +3,12 @@ import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_visit_function_nodes_list } from "../../../love/public/src/js_visit_function_nodes_list.mjs";
 export function js_curry_replace(ast) {
+  let l = js_visit_function_nodes_list(ast);
   function lambda(v) {
     let node = property_get(v, "node");
     log(js_curry_replace.name, {
       node,
     });
   }
-  let l = js_visit_function_nodes_list(ast);
-  function lambda2(item) {}
-  each(list, lambda2);
+  each(list, lambda);
 }
