f7a0246eef6cfe2955ce0fa1934dedd97c6f50d0
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 13:56:47 -0400

    function_auto_path ../love/public/src/js_function_node_expand.mjs

--- public/src/js_function_node_expand.mjs
+++ public/src/js_function_node_expand.mjs
@@ -1,8 +1,12 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
 import { js_function_declaration_to_block_body } from "../../../love/public/src/js_function_declaration_to_block_body.mjs";
 export function js_function_node_expand(ast, function_node) {
   let body_block = js_function_declaration_to_block_body(function_node);
-  let f = js_block_find_from_node(ast, function_node);
+  let r = js_block_find_from_node(ast, function_node);
+  let item = property_get(r, "item");
+  let index = property_get(r, "index");
+  let body = property_get(r, "body");
   list_remove(list, item);
 }
