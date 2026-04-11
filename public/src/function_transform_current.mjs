072118e0f86f2b6f823c344dde118b84d018ff82
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:56:59 -0400

    function_auto_path ../love/public/src/function_transform_current.mjs

--- public/src/function_transform_current.mjs
+++ public/src/function_transform_current.mjs
@@ -1,20 +1,9 @@
-import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { function_transform_single } from "../../../love/public/src/function_transform_single.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
-import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 export async function function_transform_current(
   f_name_transformer_args_comma,
 ) {
   let f_name = await function_current_get();
-  let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
-  let remaining = property_get(fr, "remaining");
-  let f_name_transformer = property_get(fr, "first");
-  let imported_fn = await function_import_unalias(f_name_transformer);
-  async function lambda(ast) {
-    let result = await imported_fn(ast, ...remaining);
-    return result;
-  }
-  await function_transform(f_name, lambda);
+  await function_transform_single(f_name_transformer_args_comma, f_name);
   return r;
 }
---
+++ public/src/function_transform_single.mjs
@@ -0,0 +1,18 @@
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
+export async function function_transform_single(
+  f_name_transformer_args_comma,
+  f_name,
+) {
+  let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
+  let remaining = property_get(fr, "remaining");
+  let f_name_transformer = property_get(fr, "first");
+  let imported_fn = await function_import_unalias(f_name_transformer);
+  async function lambda(ast) {
+    let result = await imported_fn(ast, ...remaining);
+    return result;
+  }
+  await function_transform(f_name, lambda);
+}
