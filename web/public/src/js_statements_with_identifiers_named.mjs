860152728413b00ecc6c6fa914a1694ffa93ee5c
JESUS rose to life <username@users.noreply.github.com>
2026-04-10 20:00:53 -0400

    function_auto_path ../love/public/src/js_statements_with_identifiers_named.mjs

--- web/public/src/js_statements_with_identifiers_named.mjs
+++ web/public/src/js_statements_with_identifiers_named.mjs
@@ -1,11 +1,10 @@
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { js_node_to_block_curried } from "../../../love/public/src/js_node_to_block_curried.mjs";
 import { js_node_to_block } from "../../../love/public/src/js_node_to_block.mjs";
 import { js_identifiers_named } from "../../../love/public/src/js_identifiers_named.mjs";
+import { list_map_unique } from "../../../love/public/src/list_map_unique.mjs";
 export function js_statements_with_identifiers_named(ast, identifier_name) {
   let identifiers_named = js_identifiers_named(ast, identifier_name);
   let f = js_node_to_block(ast, node);
   let r = js_node_to_block_curried(ast);
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
+  let mapped = list_map_unique(identifiers_named, r);
 }
