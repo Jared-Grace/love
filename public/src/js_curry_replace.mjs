66d1a6f09f04f5c3fa1723567c341eca33f41196
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 15:41:47 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -1,5 +1,8 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_visit_function_nodes_list } from "../../../love/public/src/js_visit_function_nodes_list.mjs";
 export function js_curry_replace(ast) {
-  function lambda(v) {}
+  function lambda(v) {
+    let node = property_get(v, "node");
+  }
   let l = js_visit_function_nodes_list(ast, lambda);
 }
