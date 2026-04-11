255a65d0ecbff7fc9a22db6046f3b27e899d1cb0
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:21:56 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -24,14 +24,14 @@ export async function sandbox() {
   await function_current_selects_empty();
   const f_name = app_gloss_bible_home_generic.name;
   await function_current_set(f_name);
-  let r = await function_node_select_args(
-    js_function_node_find_named_node.name,
-    "on_passage",
-  );
   await function_current_selects_apply(js_function_node_unwrap.name);
   return r;
   await function_node_select_args(
     js_statements_with_identifiers_named.name,
+    "on_passage",
+  );
+  let r = await function_node_select_args(
+    js_function_node_find_named_node.name,
     "on_passage",
   );
   await function_current_selects_apply(js_statement_delete.name);
