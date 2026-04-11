40e59d205f0eb94e90a6fe60a5d0ba15e62fd868
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:29:31 -0400

    function_auto_path ../love/public/src/app_g_bible_home_inner.mjs

--- public/src/app_g_bible_home_inner.mjs
+++ public/src/app_g_bible_home_inner.mjs
@@ -1,3 +1,4 @@
+import { integer_to_try_multiple } from "../../../love/public/src/integer_to_try_multiple.mjs";
 import { list_adder_async } from "../../../love/public/src/list_adder_async.mjs";
 import { list_copy } from "../../../love/public/src/list_copy.mjs";
 import { list_empty } from "../../../love/public/src/list_empty.mjs";
@@ -7,8 +8,6 @@ import { each } from "../../../love/public/src/each.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { text_to } from "../../../love/public/src/text_to.mjs";
 import { list_max } from "../../../love/public/src/list_max.mjs";
-import { integer_to_try } from "../../../love/public/src/integer_to_try.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function app_g_bible_home_inner(context, download) {
   let downloaded = null;
@@ -24,7 +23,7 @@ export async function app_g_bible_home_inner(context, download) {
       let passages = property_get(downloaded, "passages");
       function lambda2(passage) {
         let verse_numbers = property_get(passage, "verse_numbers");
-        let mapped = list_map(verse_numbers, integer_to_try);
+        let mapped = integer_to_try_multiple(verse_numbers);
         let max = list_max(mapped);
         let s = text_to(max);
         if (equal(s, verse_number)) {
