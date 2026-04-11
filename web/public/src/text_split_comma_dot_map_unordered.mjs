00bf300504c7a2987e3bab18891c27a3684a4dad
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 14:59:06 -0400

    function_auto_path ../love/public/src/function_delete.mjs

---
+++ web/public/src/text_split_comma_dot_map_unordered.mjs
@@ -0,0 +1,5 @@
+import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
+export function text_split_comma_dot_map_unordered(f_names_comma) {
+  let split = text_split_comma_dot(f_names_comma);
+  return split;
+}
