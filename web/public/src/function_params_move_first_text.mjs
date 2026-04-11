a6743feedc11a6b14055b34ce665aa0f346d9269
JESUS rose to life <username@users.noreply.github.com>
2026-04-11 15:27:24 -0400

    pb function_params_move_first_text

--- web/public/src/function_params_move_first_text.mjs
+++ web/public/src/function_params_move_first_text.mjs
@@ -1,8 +1,8 @@
 import { text_split_comma_dot_each_async } from "../../../love/public/src/text_split_comma_dot_each_async.mjs";
 import { function_params_move_first_curried_right } from "../../../love/public/src/function_params_move_first_curried_right.mjs";
 export async function function_params_move_first_text(
-  param_names,
   f_names_comma,
+  param_names,
 ) {
   let lambda = await function_params_move_first_curried_right(param_names);
   await text_split_comma_dot_each_async(f_names_comma, lambda);
