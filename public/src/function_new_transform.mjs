f99c2aa57453f2ced3744d804cf1a8c09e774249
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:50:20 -0400

    function_auto_path ../love/public/src/function_new_transform.mjs

--- public/src/function_new_transform.mjs
+++ public/src/function_new_transform.mjs
@@ -1,7 +1,7 @@
+import { function_new } from "../../../love/public/src/function_new.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { function_new_open } from "../../../love/public/src/function_new_open.mjs";
 export async function function_new_transform(f_name, lambda$ast) {
-  await function_new_open(f_name);
+  await function_new(f_name);
   let output = await function_transform(f_name, lambda$ast);
   return output;
 }
