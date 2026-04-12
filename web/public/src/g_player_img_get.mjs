84eeea977f95a53326f184c896292d4f67e7f503
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 19:06:55 -0400

    function_auto_path ../love/public/src/g_player_img_get.mjs

--- web/public/src/g_player_img_get.mjs
+++ web/public/src/g_player_img_get.mjs
@@ -1,7 +1,7 @@
 import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
 import { g_male_img_names } from "../../../love/public/src/g_male_img_names.mjs";
 export function g_player_img_get() {
-  let imgs_men1 = g_male_img_names();
-  const player_img = list_random_item(imgs_men1);
+  let imgs_men = g_male_img_names();
+  const player_img = list_random_item(imgs_men);
   return player_img;
 }
