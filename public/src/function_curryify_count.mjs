595e729846730495dee94e91dd7a60e20e5bab0c
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:52:17 -0400

    r c function_curryify_generic,open

--- public/src/function_curryify_count.mjs
+++ public/src/function_curryify_count.mjs
@@ -3,7 +3,7 @@ import { function_curryify_generic_name } from "../../../love/public/src/functio
 import { integer_to } from "../../../love/public/src/integer_to.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
-import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
+import { function_curryify_generic_open } from "../../../love/public/src/function_curryify_generic_open.mjs";
 export async function function_curryify_count(f_name, count) {
   arguments_assert(arguments, 2);
   count = integer_to(count);
@@ -13,6 +13,6 @@ export async function function_curryify_count(f_name, count) {
     return combined;
   }
   let r2 = function_curryify_count_args_get_curried_right(count);
-  let output = await function_curryify_generic(f_name, lambda, r2);
+  let output = await function_curryify_generic_open(f_name, lambda, r2);
   return output;
 }
