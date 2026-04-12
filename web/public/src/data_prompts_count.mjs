b6836d69d01e3bf5fa206ede789d863e923d1580
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:17:55 -0400

    function_auto_path ../love/public/src/data_prompts_count.mjs

--- web/public/src/data_prompts_count.mjs
+++ web/public/src/data_prompts_count.mjs
@@ -1,13 +1,9 @@
 import { list_slice_end_try } from "../../../love/public/src/list_slice_end_try.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { data_value } from "../../../love/public/src/data_value.mjs";
 import { data_prompts_path } from "../../../love/public/src/data_prompts_path.mjs";
 export async function data_prompts_count(offset) {
   let d_path = data_prompts_path();
   let prompts = await data_value("prompts", d_path);
-  log(data_prompts_count.name, {
-    prompts,
-  });
   let sliced = list_slice_end_try(prompts, offset);
   return sliced;
 }
