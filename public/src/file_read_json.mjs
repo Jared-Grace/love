e16bc9bacd5280e5d07e3e2ff019db1f96905149
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:59:05 -0400

    function_auto_path ../love/public/src/file_read_json.mjs

--- public/src/file_read_json.mjs
+++ public/src/file_read_json.mjs
@@ -3,9 +3,9 @@ import { file_read } from "../../../love/public/src/file_read.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 export async function file_read_json(file_path) {
   let json = await file_read(file_path);
-  let data = json_from(json);
-  return data;
   log(file_read_json.name, {
     json,
   });
+  let data = json_from(json);
+  return data;
 }
