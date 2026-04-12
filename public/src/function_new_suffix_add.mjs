2266916835493b70683cb6e37dbf9944a132047c
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:48:32 -0400

    function_auto_path ../love/public/src/function_new_open.mjs

--- public/src/function_new_suffix_add.mjs
+++ public/src/function_new_suffix_add.mjs
@@ -1,7 +1,7 @@
 import { function_name_unalias_combine } from "../../../love/public/src/function_name_unalias_combine.mjs";
-import { function_new } from "../../../love/public/src/function_new.mjs";
+import { function_new_open } from "../../../love/public/src/function_new_open.mjs";
 export async function function_new_suffix_add(f_name_existing, suffix) {
   let combined = await function_name_unalias_combine(f_name_existing, suffix);
-  let r = await function_new(combined);
+  let r = await function_new_open(combined);
   return r;
 }
