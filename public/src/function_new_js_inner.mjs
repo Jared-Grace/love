72fc8b1abd98f8ac7a8bd75ece521796c58d6c77
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:50:46 -0400

    function_auto_path ../love/public/src/function_new_open_transform.mjs

--- public/src/function_new_js_inner.mjs
+++ public/src/function_new_js_inner.mjs
@@ -1,7 +1,7 @@
-import { function_new_transform_open } from "../../../love/public/src/function_new_transform_open.mjs";
+import { function_new_open_transform } from "../../../love/public/src/function_new_open_transform.mjs";
 import { js_flo_params_add } from "../../../love/public/src/js_flo_params_add.mjs";
 export async function function_new_js_inner(combined) {
-  let result = await function_new_transform_open(combined, lambda);
+  let result = await function_new_open_transform(combined, lambda);
   async function lambda(ast) {
     js_flo_params_add(ast, ["ast"]);
   }
