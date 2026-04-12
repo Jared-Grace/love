2157f9715819ce6e4f3242cd235c017a1d3f8ffa
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:29:11 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -20,8 +20,7 @@ export function js_curry_replace(ast) {
     let body_block = js_function_declaration_to_block_body(node);
     let s1 = list_size_1(body_block);
     if (s1) {
-      let only = null;
-      only = list_single(body_block);
+      let only = list_single(body_block);
       let esi = js_expression_statement_is(only);
       if (esi) {
         let expression = js_statement_expression_get(only);
