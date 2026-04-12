5dc843bb816f43c1d857ee4a3f2b9dfbda9588d8
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:19:07 -0400

    function_auto_path ../love/public/src/js_imports_missing_add_specified.mjs

--- web/public/src/js_imports_missing_add_specified.mjs
+++ web/public/src/js_imports_missing_add_specified.mjs
@@ -1,6 +1,8 @@
+import { js_imports_missing_specify } from "../../../love/public/src/js_imports_missing_specify.mjs";
+import { functions_names } from "../../../love/public/src/functions_names.mjs";
 import { function_imports_add } from "../../../love/public/src/function_imports_add.mjs";
-import { js_imports_missing_all } from "../../../love/public/src/js_imports_missing_all.mjs";
 export async function js_imports_missing_add_specified(ast) {
-  let imports_missing = await js_imports_missing_all(ast);
+  let f_names = await functions_names();
+  let imports_missing = js_imports_missing_specify(ast, f_names);
   await function_imports_add(ast, imports_missing);
 }
