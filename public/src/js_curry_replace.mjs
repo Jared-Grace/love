818a81c28d0a624adde4feca9ac046de854052a1
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:22:38 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -86,5 +86,5 @@ export async function js_curry_replace(ast) {
     each(list, lambda);
   }
   let list2 = list_adder(lambda2);
-  await js_imports_missing_add_specified(ast2, f_names2);
+  await js_imports_missing_add_specified(ast, f_names);
 }
