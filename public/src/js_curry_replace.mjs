c77a2325f9e6e6564967c4da2f63b0d0f06910e9
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:41:21 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -57,9 +57,7 @@ export async function js_curry_replace(ast) {
                   let fi = list_first_is(args, only);
                   if (fi) {
                     let combined = function_curryify_generic_name(f_name);
-                    log(js_curry_replace.name, {
-                      combined,
-                    });
+                    let includes2 = list_includes(list2, item);
                   }
                 }
               }
