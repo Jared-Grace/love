97e5b7a6ef9483db477b4c8b5eadc9b0109f81d1
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 19:11:04 -0400

    function_auto_path ../love/public/src/app_g_game_initialize.mjs

--- web/public/src/app_g_game_initialize.mjs
+++ web/public/src/app_g_game_initialize.mjs
@@ -36,8 +36,8 @@ export async function app_g_game_initialize() {
     img: player_img,
   });
   let imgs_women_rg = range_1(21);
-  let imgs_men = g_male_img_names();
   let imgs_women = list_map_combine_left(imgs_women_rg, "woman_");
+  let imgs_men = g_male_img_names();
   list_shuffle(coordinates_land);
   let names_women = bible_names_women();
   let female = {
