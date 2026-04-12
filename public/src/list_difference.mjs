c9d60ca4c4630a29f1189fb067a85342449a6318
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:24:01 -0400

    r 1 list_difference_mapped,mapper

--- public/src/list_difference.mjs
+++ public/src/list_difference.mjs
@@ -1,7 +1,7 @@
-import { list_difference_mapped } from "../../../love/public/src/list_difference_mapped.mjs";
+import { list_difference_mapper } from "../../../love/public/src/list_difference_mapper.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 export function list_difference(list, list_other) {
   let mapper = identity;
-  let difference = list_difference_mapped(list, list_other, mapper);
+  let difference = list_difference_mapper(list, list_other, mapper);
   return difference;
 }
