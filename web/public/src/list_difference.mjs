1ad9ccad3431afb0c96348eb0afb1614e7f8540a
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:21:55 -0400

    function_auto_path ../love/public/src/list_difference.mjs

--- web/public/src/list_difference.mjs
+++ web/public/src/list_difference.mjs
@@ -4,6 +4,7 @@ import { each } from "../../../love/public/src/each.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export function list_difference(list, other) {
+  let mapper = identity;
   function lambda2(la) {
     function lambda(l) {
       let a = list_includes(other, l);
@@ -15,5 +16,4 @@ export function list_difference(list, other) {
   }
   let difference = list_adder(lambda2);
   return difference;
-  let mapper = identity;
 }
