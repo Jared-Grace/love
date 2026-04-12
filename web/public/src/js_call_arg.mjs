95eb59ab15a50d32f263cd9ee75f14400f0efe4e
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:12:40 -0400

    function_auto_path ../love/public/src/js_call_arg.mjs

--- web/public/src/js_call_arg.mjs
+++ web/public/src/js_call_arg.mjs
@@ -1,7 +1,7 @@
 import { js_call_args } from "../../../love/public/src/js_call_args.mjs";
 import { list_single_item } from "../../../love/public/src/list_single_item.mjs";
-export function js_call_arg(callee_code, cod_arg) {
-  let r3 = list_single_item(cod_arg);
-  let parsed = js_call_args(callee_code, r3);
+export function js_call_arg(callee_code, code_arg) {
+  let arg_list = list_single_item(code_arg);
+  let parsed = js_call_args(callee_code, arg_list);
   return parsed;
 }
