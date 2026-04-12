37d3e860bbb14c744e51ee63e80d3cef4f309f36
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:13:02 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- web/public/src/js_curry_replace.mjs
+++ web/public/src/js_curry_replace.mjs
@@ -67,7 +67,7 @@ export async function js_curry_replace(ast) {
                     }
                     let name_function = js_function_declaration_name(node);
                     let arg_name = js_identifier_name(only);
-                    let c = js_call_arg(arg_name, name_curried);
+                    let c = js_call_arg(name_curried, arg_name);
                     let declare = js_declare(name_function, c);
                     log_unparse(declare);
                   }
