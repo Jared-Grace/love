6d58285b796ade9ef33e9b1560419036f2ee411b
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:56:58 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -38,7 +38,7 @@ export async function app_g_refresh(div_map_container, rows) {
     await app_g_refresh(div_map_container, rows);
   }
   let g = await catch_null_async(app_g_game_save_get);
-  if (null_is(value)) {
+  if (null_is(g)) {
   }
   let npcs = property_get(g, "npcs");
   let player = property_get(g, "player");
