17d53bfe168d49829311c07ee77f80b60117397a
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:52:18 -0400

    function_auto_path ../love/public/src/http_generic.mjs

--- web/public/src/catch_call_later_async.mjs
+++ web/public/src/catch_call_later_async.mjs
@@ -1,11 +1,11 @@
 import { log } from "../../../love/public/src/log.mjs";
-export function catch_call_async(lambda$e, lambda) {
+export function catch_call_later_async(lambda$e, lambda) {
   let i = async function inner() {
     let result = null;
     try {
       result = await lambda(...arguments);
     } catch (e) {
-      log(catch_call_async.name, {
+      log(catch_call_later_async.name, {
         e,
       });
       lambda$e(e);
