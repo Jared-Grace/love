7ab6603a8027a4910d8a0098f7284628d2ac21c5
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:52:19 -0400

    functions_rename_if_starts_with catch_call catch_call_later

--- web/public/src/qa_tests_run.mjs
+++ web/public/src/qa_tests_run.mjs
@@ -1,7 +1,7 @@
 import { portfolio_qa_tests_run_result } from "../../../portfolio_qa/public/src/portfolio_qa_tests_run_result.mjs";
 import { invoke_multiple_unordered_async } from "../../../love/public/src/invoke_multiple_unordered_async.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { catch_call_async } from "../../../love/public/src/catch_call_async.mjs";
+import { catch_call_later_async } from "../../../love/public/src/catch_call_later_async.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 export async function qa_tests_run(fns, name_get) {
@@ -13,7 +13,7 @@ export async function qa_tests_run(fns, name_get) {
         error,
       });
     }
-    let i = catch_call_async(on_error, item);
+    let i = catch_call_later_async(on_error, item);
     return i;
   }
   let mapped2 = list_map(fns, lambda);
