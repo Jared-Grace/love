79def76f22f5165c22a38a109146d57afb623d77
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:50:46 -0400

    function_auto_path ../love/public/src/function_new_open_transform.mjs

--- public/src/function_wrap.mjs
+++ public/src/function_wrap.mjs
@@ -4,13 +4,13 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_function_declaration_asyncify_params_from } from "../../../love/public/src/js_function_declaration_asyncify_params_from.mjs";
 import { js_call_args_await_maybe_declaration_return_add } from "../../../love/public/src/js_call_args_await_maybe_declaration_return_add.mjs";
 import { js_function_declaration_params_names } from "../../../love/public/src/js_function_declaration_params_names.mjs";
-import { function_new_transform_open } from "../../../love/public/src/function_new_transform_open.mjs";
+import { function_new_open_transform } from "../../../love/public/src/function_new_open_transform.mjs";
 export async function function_wrap(f_name, f_name_wrapped) {
   arguments_assert(arguments, 2);
   let r = await function_parse_declaration_unaliased(f_name);
   let unaliased = property_get(r, "unaliased");
   let declaration_call = property_get(r, "declaration");
-  let v = await function_new_transform_open(f_name_wrapped, lambda);
+  let v = await function_new_open_transform(f_name_wrapped, lambda);
   return v;
   async function lambda(ast) {
     let arg_names = js_function_declaration_params_names(declaration_call);
