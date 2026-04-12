06e9914f12be1567cfb71f09fa8e9f12a33096f0
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:23:09 -0400

    function_auto_path ../love/public/src/list_difference.mjs

--- web/public/src/list_difference.mjs
+++ web/public/src/list_difference.mjs
@@ -1,21 +1,7 @@
+import { list_difference_mapped } from "../../../love/public/src/list_difference_mapped.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export function list_difference(list, list_other) {
   let mapper = identity;
-  let list_other_mapped = mapper(list_other);
-  function lambda2(la) {
-    function lambda(l) {
-      let l_mapped = mapper(l);
-      let a = list_includes(list_other_mapped, l_mapped);
-      if (not(a)) {
-        la(l);
-      }
-    }
-    each(list, lambda);
-  }
-  let difference = list_adder(lambda2);
+  let difference = list_difference_mapped(mapper, list_other, list);
   return difference;
 }
