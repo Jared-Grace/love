0b60cbdf7acdbd65403f9b98833dffe3f32e0f62
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 15:16:46 -0400

    function_auto_path ../love/public/src/js_imports_missing_all.mjs

---
+++ public/src/js_imports_missing_specify.mjs
@@ -0,0 +1,17 @@
+import { list_intersect } from "../../../love/public/src/list_intersect.mjs";
+import { list_difference } from "../../../love/public/src/list_difference.mjs";
+import { list_concat } from "../../../love/public/src/list_concat.mjs";
+import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
+import { js_imports } from "../../../love/public/src/js_imports.mjs";
+import { js_flo } from "../../../love/public/src/js_flo.mjs";
+import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
+export function js_imports_missing_specify(ast, f_names) {
+  let name = js_flo_name(ast);
+  let declaration = js_flo(ast);
+  let imports = js_imports(ast);
+  let identifiers = js_identifiers_names(declaration);
+  let imports_self = list_concat(imports, [name]);
+  let missing = list_difference(identifiers, imports_self);
+  let imports_missing = list_intersect(missing, f_names);
+  return imports_missing;
+}
