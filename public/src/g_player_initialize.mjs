facf3ce6de2c409110c4e1caf879076012d20fbd
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 19:15:30 -0400

    function_auto_path ../love/public/src/app_g_game_initialize.mjs

---
+++ public/src/g_player_initialize.mjs
@@ -0,0 +1,27 @@
+import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
+import { bible_names_men } from "../../../love/public/src/bible_names_men.mjs";
+import { list_single } from "../../../love/public/src/list_single.mjs";
+import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
+export function g_player_initialize(player_img, coordinates_land) {
+  let player = {};
+  object_merge(player, {
+    img: player_img,
+  });
+  let player_list = list_remove_last(coordinates_land);
+  let player_coordinates = list_single(player_list);
+  object_merge(player, player_coordinates);
+  let names_men = bible_names_men();
+  object_merge(player, {
+    img: player_img,
+    prayer: {
+      conversation: false,
+      study: false,
+    },
+    name: list_random_item(names_men),
+    conversed: false,
+    studied: false,
+    review: [],
+  });
+  return player;
+}
