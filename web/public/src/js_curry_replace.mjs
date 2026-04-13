1e38f3840d6c07beaa46a4fe71838804c300cc78
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:56:34 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- web/public/src/js_curry_replace.mjs
+++ web/public/src/js_curry_replace.mjs
@@ -88,10 +88,7 @@ export async function js_curry_replace(ast) {
                       difference,
                     );
                     let positions_1 = list_map_index_of_1(difference, args);
-                    let combined = function_curryify_specify_name_get(
-                      f_name,
-                      positions_1,
-                    );
+                    function_curryify_specify_name_get(f_name, positions_1);
                     let name_curried = function_curryify_generic_name(f_name);
                     let n = list_includes_not(f_names, name_curried);
                     if (n) {
