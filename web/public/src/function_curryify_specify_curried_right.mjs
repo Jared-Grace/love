6dd5af063278cba463b6ec2d3f848c0369e990ad
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 14:00:21 -0400

    cur function_curryify_specify

---
+++ web/public/src/function_curryify_specify_curried_right.mjs
@@ -0,0 +1,10 @@
+import { function_curryify_specify } from "../../../love/public/src/function_curryify_specify.mjs";
+export async function function_curryify_specify_curried_right(positions_comma) {
+  let r2 = async function function_curryify_specify_curried_right_result(
+    f_name,
+  ) {
+    let r = await function_curryify_specify(f_name, positions_comma);
+    return r;
+  };
+  return r2;
+}
