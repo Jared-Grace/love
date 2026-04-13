9629a7ba27cb731060b824ee4e1d428f70e16fab
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:39:43 -0400

    p

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -50,10 +50,10 @@ export async function js_curry_replace(ast) {
             if (ci) {
               let f_name = js_call_callee_name_try(expression);
               let includes = list_includes(f_names, f_name);
+                log(js_curry_replace.name, {});
               if (includes) {
                 let args = js_call_arguments_get(expression);
                 let ii_expression = js_identifier_list_is(args);
-                log(js_curry_replace.name, {});
                 if (ii_expression) {
                   let difference = js_identifiers_names_difference(
                     args,
