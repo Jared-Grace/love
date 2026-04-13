dd7fe97bc16842a29a86b87434cdfbf100bc1183
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:32:57 -0400

    function_auto_path ../love/public/src/list_index_of_curried.mjs

--- public/src/list_index_of_curried.mjs
+++ public/src/list_index_of_curried.mjs
@@ -1,6 +1,8 @@
 import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 export function list_index_of_curried(list) {
-  return function list_index_of_curried_result(item) {
-    return list_index_of(list, item);
+  let r = function list_index_of_curried_result(item) {
+    let index = list_index_of(list, item);
+    return index;
   };
+  return r;
 }
