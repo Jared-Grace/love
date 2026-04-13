ceccd5e600545058c64c98d74a0a8dfe784cc8d2
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:54:34 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get_curried_right.mjs

---
+++ public/src/function_curryify_specify_name_get_curried_right.mjs
@@ -0,0 +1,10 @@
+import { function_curryify_specify_name_get } from "../../../love/public/src/function_curryify_specify_name_get.mjs";
+export function function_curryify_specify_name_get_curried_right(positions_1) {
+  let r = function function_curryify_specify_name_get_curried_right_result(
+    unaliased,
+  ) {
+    let combined = function_curryify_specify_name_get(unaliased, positions_1);
+    return combined;
+  };
+  return r;
+}
