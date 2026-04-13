c5e5152af79250b86b72a20c7a50237d464b8d3a
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 14:02:33 -0400

    function_auto_path ../love/public/src/list_add_if_not_includes.mjs

--- public/src/list_add_if_not_includes.mjs
+++ public/src/list_add_if_not_includes.mjs
@@ -3,10 +3,11 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
 export function list_add_if_not_includes(list, item) {
   "rename includes to exists todo";
+  let added = false;
   let exists = list_includes(list, item);
   if (not(exists)) {
     list_add(list, item);
   }
-  return;
-  let added = false;
+  return added;
+  added = true;
 }
