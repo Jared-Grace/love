a316ba300d0d3bb5498bc05081cd7dc5ab891357
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 14:51:48 -0400

    function_auto_path ../love/public/src/js_declare_replace.mjs

--- public/src/js_declare_replace.mjs
+++ public/src/js_declare_replace.mjs
@@ -1,6 +1,6 @@
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
 import { js_declare } from "../../../love/public/src/js_declare.mjs";
-export function js_declare_replace(name_function, c, node) {
-  let declare = js_declare(name_function, c);
+export function js_declare_replace(declare_variable_name, c, node) {
+  let declare = js_declare(declare_variable_name, c);
   object_replace(node, declare);
 }
