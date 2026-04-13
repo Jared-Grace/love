c708120fdbd991ec8ed01e0014bcc85213432939
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:51:10 -0400

    function_auto_path ../love/public/src/function_curryify_specify_args_get.mjs

--- public/src/function_curryify_specify_args_get.mjs
+++ public/src/function_curryify_specify_args_get.mjs
@@ -1,12 +1,11 @@
+import { list_map_subtract_1 } from "../../../love/public/src/list_map_subtract_1.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { list_indices_to_items } from "../../../love/public/src/list_indices_to_items.mjs";
-import { subtract_1 } from "../../../love/public/src/subtract_1.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 export function function_curryify_specify_args_get(arg_names, positions) {
   function lambda() {
-    let mapped = list_map(positions, subtract_1);
+    let mapped = list_map_subtract_1(positions);
     log(function_curryify_specify_args_get.name, {
       mapped,
       positions,
