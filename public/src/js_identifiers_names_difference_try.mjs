44c4070c9d2ec268e01dc8af357be4f42da72ce3
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:44:29 -0400

    function_auto_path ../love/public/src/js_identifiers_names_difference_try.mjs

--- public/src/js_identifiers_names_difference_try.mjs
+++ public/src/js_identifiers_names_difference_try.mjs
@@ -1,6 +1,6 @@
 import { js_identifier_name_try } from "../../../love/public/src/js_identifier_name_try.mjs";
 import { list_difference_mapper } from "../../../love/public/src/list_difference_mapper.mjs";
-export function js_identifiers_names_difference(args, params) {
+export function js_identifiers_names_difference_try(args, params) {
   let r = list_difference_mapper(args, params, js_identifier_name_try);
   return r;
 }
