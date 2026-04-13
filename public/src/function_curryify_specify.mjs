c26966b0479d0720c9ce65519577872e1d765767
JESUS rose to life <username@users.noreply.github.com>
2026-04-13 13:53:31 -0400

    function_auto_path ../love/public/src/function_curryify_specify.mjs

--- public/src/function_curryify_specify.mjs
+++ public/src/function_curryify_specify.mjs
@@ -4,12 +4,12 @@ import { function_curryify_specify_name } from "../../../love/public/src/functio
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { function_curryify_generic_open } from "../../../love/public/src/function_curryify_generic_open.mjs";
 export async function function_curryify_specify(f_name, positions_comma) {
-  let split = text_split_comma(positions_comma);
-  let args_get = function_curryify_specify_args_get_curried_right(split);
+  let positions = text_split_comma(positions_comma);
+  let args_get = function_curryify_specify_args_get_curried_right(positions);
   function name_get(unaliased) {
     let n = function_curryify_specify_name(unaliased);
     const parts = [n];
-    let combined = function_name_combine_multiple_concat(parts, split);
+    let combined = function_name_combine_multiple_concat(parts, positions);
     return combined;
   }
   let r = await function_curryify_generic_open(f_name, name_get, args_get);
