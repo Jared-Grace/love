cb08eb629ba13f48d77a9ed12ec46af58487e986
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 18:39:42 -0400

    r c file_read_cached,initialize

--- public/src/file_read_cached_initialize.mjs
+++ public/src/file_read_cached_initialize.mjs
@@ -2,13 +2,16 @@ import { not_assert } from "../../../love/public/src/not_assert.mjs";
 import { global_function_property_exists } from "../../../love/public/src/global_function_property_exists.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
-export async function file_read_cached(f_path) {
-  let exists = global_function_property_exists(file_read_cached, f_path);
+export async function file_read_cached_initialize(f_path) {
+  let exists = global_function_property_exists(
+    file_read_cached_initialize,
+    f_path,
+  );
   if (exists) {
     return false;
   }
   not_assert(exists);
   let contents = await file_read(f_path);
-  global_function_property_set(file_read_cached, f_path, contents);
+  global_function_property_set(file_read_cached_initialize, f_path, contents);
   return true;
 }
--- public/src/file_transform_cached.mjs
+++ public/src/file_transform_cached.mjs
@@ -1,7 +1,7 @@
 import { file_overwrite_cached } from "../../../love/public/src/file_overwrite_cached.mjs";
-import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
+import { file_read_cached_initialize } from "../../../love/public/src/file_read_cached_initialize.mjs";
 export async function file_transform_cached(f_path, lambda) {
-  let overwrite = await file_read_cached(f_path);
+  let overwrite = await file_read_cached_initialize(f_path);
   let r = await lambda();
   if (overwrite) {
     await file_overwrite_cached(f_path);
