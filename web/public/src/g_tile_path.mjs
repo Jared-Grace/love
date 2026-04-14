6cc2e10d9867a5fce5e4799acbdaccc33ee6662e
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:49:49 -0400

    function_auto_path ../love/public/src/g_tile_path.mjs

--- web/public/src/g_tile_path.mjs
+++ web/public/src/g_tile_path.mjs
@@ -1,6 +1,6 @@
 import { g_folder_tiles } from "../../../love/public/src/g_folder_tiles.mjs";
-export function g_tile_path(r) {
+export function g_tile_path(tile_name) {
   const tiles_path = g_folder_tiles();
-  const src = tiles_path + r + ".png";
+  const src = tiles_path + tile_name + ".png";
   return src;
 }
