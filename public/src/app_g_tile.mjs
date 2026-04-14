35f59395e0e402d97b9e2f4534de4ef653883177
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:49:46 -0400

    function_auto_path ../love/public/src/app_g_tile.mjs

--- public/src/app_g_tile.mjs
+++ public/src/app_g_tile.mjs
@@ -2,8 +2,8 @@ import { g_tile_path } from "../../../love/public/src/g_tile_path.mjs";
 import { app_g_tile_style } from "../../../love/public/src/app_g_tile_style.mjs";
 import { html_data_set_json } from "../../../love/public/src/html_data_set_json.mjs";
 import { html_img } from "../../../love/public/src/html_img.mjs";
-export function app_g_tile(parent, r, x, y) {
-  const src = g_tile_path(r);
+export function app_g_tile(parent, tile_name, x, y) {
+  const src = g_tile_path(tile_name);
   let tile = html_img(parent, src);
   app_g_tile_style(tile);
   const coordinates_tile = {
