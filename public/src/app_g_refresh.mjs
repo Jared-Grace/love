0111ff3605be1286c82a779ce6faf3bb7a91e5de
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:41:27 -0400

    pd app_g_player_img player

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -14,7 +14,7 @@ export async function app_g_refresh(context, div_map_container) {
   html_clear(div_map_container);
   let div_map = await app_g_div_map_new(div_map_container);
   let player = property_get(g, "player");
-  let player_img_c = app_g_player_img(div_map, player);
+  let player_img_c = app_g_player_img(div_map);
   app_g_div_map_on_click(context, div_map, player_img_c);
   await app_g_player_scroll_center(div_map, player_img_c);
 }
