6b5cae8c696431ac5e6114c485f273a056ea3914
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:47:10 -0400

    function_auto_path ../love/public/src/g_folder_tiles.mjs

--- public/src/g_folder_tiles.mjs
+++ public/src/g_folder_tiles.mjs
@@ -1,5 +1,7 @@
+import { app_g_path_prefix } from "../../../love/public/src/app_g_path_prefix.mjs";
 import { g_folder_img } from "../../../love/public/src/g_folder_img.mjs";
-export function g_folder_tiles(path_prefix) {
+export function g_folder_tiles() {
+  let path_prefix = app_g_path_prefix();
   const game_prefix1 = g_folder_img(path_prefix);
   const tiles_path = game_prefix1 + "tiles\\seamless\\";
   return tiles_path;
