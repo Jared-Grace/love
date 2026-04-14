a15dea166c83aa0fe787f0a1312421e530da7101
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:04:44 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

---
+++ public/src/app_g_div_map_new.mjs
@@ -0,0 +1,34 @@
+import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
+import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
+import { app_g_refresh } from "../../../love/public/src/app_g_refresh.mjs";
+import { app_g_div_map_tiles_add } from "../../../love/public/src/app_g_div_map_tiles_add.mjs";
+import { app_g_div_map_style } from "../../../love/public/src/app_g_div_map_style.mjs";
+import { app_g_div_map_npcs_add } from "../../../love/public/src/app_g_div_map_npcs_add.mjs";
+import { app_g_player_img } from "../../../love/public/src/app_g_player_img.mjs";
+import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+export async function app_g_div_map_new(
+  div_map_container,
+  player,
+  npcs,
+  context,
+) {
+  let div_map = html_div(div_map_container);
+  property_set_exists_not(div_map, "container", div_map_container);
+  let player_img_c = app_g_player_img(div_map, player);
+  app_g_div_map_npcs_add(div_map, npcs);
+  await app_g_div_map_style(div_map);
+  await app_g_div_map_tiles_add(div_map);
+  async function on_click(e) {
+    async function refresh() {
+      await app_g_refresh(context, div_map_container);
+    }
+    await app_g_click(e, div_map, player_img_c, refresh);
+  }
+  html_on_click(div_map, on_click);
+  let r = {
+    div_map,
+    player_img_c,
+  };
+  return r;
+}
--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -1,19 +1,12 @@
-import { app_g_div_map_tiles_add } from "../../../love/public/src/app_g_div_map_tiles_add.mjs";
-import { app_g_div_map_npcs_add } from "../../../love/public/src/app_g_div_map_npcs_add.mjs";
-import { app_g_div_map_style } from "../../../love/public/src/app_g_div_map_style.mjs";
+import { app_g_div_map_new } from "../../../love/public/src/app_g_div_map_new.mjs";
 import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { catch_null_async } from "../../../love/public/src/catch_null_async.mjs";
-import { app_g_player_img } from "../../../love/public/src/app_g_player_img.mjs";
 import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { html_on_load_wait } from "../../../love/public/src/html_on_load_wait.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
-import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
-import { html_div } from "../../../love/public/src/html_div.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_scroll_center_container_now } from "../../../love/public/src/html_scroll_center_container_now.mjs";
-import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
-import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
 export async function app_g_refresh(context, div_map_container) {
   let g = await catch_null_async(app_g_game_save_get);
   if (null_is(g)) {
@@ -23,19 +16,9 @@ export async function app_g_refresh(context, div_map_container) {
   let npcs = property_get(g, "npcs");
   let player = property_get(g, "player");
   html_clear(div_map_container);
-  let div_map = html_div(div_map_container);
-  property_set_exists_not(div_map, "container", div_map_container);
-  let player_img_c = app_g_player_img(div_map, player);
-  app_g_div_map_npcs_add(div_map, npcs);
-  await app_g_div_map_style(div_map);
-  await app_g_div_map_tiles_add(div_map);
-  async function on_click(e) {
-    async function refresh() {
-      await app_g_refresh(context, div_map_container);
-    }
-    await app_g_click(e, div_map, player_img_c, refresh);
-  }
-  html_on_click(div_map, on_click);
+  let r = await app_g_div_map_new(div_map_container, player, npcs, context);
+  let player_img_c = property_get(r, "player_img_c");
+  let div_map = property_get(r, "div_map");
   await html_on_load_wait();
   let container = property_get(div_map, "container");
   await html_scroll_center_container_now(player_img_c, container);
