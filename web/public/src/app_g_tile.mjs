2cd7f07f830d440e0f3af46e62971bb066d9a813
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:48:58 -0400

    function_auto_path ../love/public/src/app_g_tile.mjs

--- web/public/src/app_g_tile.mjs
+++ web/public/src/app_g_tile.mjs
@@ -1,16 +1,14 @@
+import { app_g_class_tile_add } from "../../../love/public/src/app_g_class_tile_add.mjs";
 import { html_data_set_json } from "../../../love/public/src/html_data_set_json.mjs";
-import { html_class_add } from "../../../love/public/src/html_class_add.mjs";
 import { g_img_square_style } from "../../../love/public/src/g_img_square_style.mjs";
 import { html_img } from "../../../love/public/src/html_img.mjs";
-import { app_g_class_tile } from "../../../love/public/src/app_g_class_tile.mjs";
 import { g_folder_tiles } from "../../../love/public/src/g_folder_tiles.mjs";
 export function app_g_tile(parent, r, x, y) {
   const tiles_path = g_folder_tiles();
   const src = tiles_path + r + ".png";
   let tile = html_img(parent, src);
   g_img_square_style(tile);
-  const tile_class = app_g_class_tile();
-  html_class_add(tile, tile_class);
+  app_g_class_tile_add(tile);
   const coordinates_tile = {
     x,
     y,
