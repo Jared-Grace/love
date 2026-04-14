aab8077ca13abe4445d1290495f45bb54953ab27
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:36:57 -0400

    function_auto_path ../love/public/src/app_g_div_map_new.mjs

--- web/public/src/app_g_div_map_new.mjs
+++ web/public/src/app_g_div_map_new.mjs
@@ -17,7 +17,6 @@ export async function app_g_div_map_new(context, div_map_container) {
   app_g_div_map_npcs_add(div_map, npcs);
   await app_g_div_map_style(div_map);
   await app_g_div_map_tiles_add(div_map);
-  app_g_div_map_on_click(context, div_map_container, div_map, player_img_c);
   let r = {
     div_map,
     player_img_c,
