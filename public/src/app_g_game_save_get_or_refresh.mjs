a476d2776c5129fa1822b9eb338c64b24ca40fbc
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:38:28 -0400

    function_auto_path ../love/public/src/app_g_game_save_get_or_refresh.mjs

--- public/src/app_g_game_save_get_or_refresh.mjs
+++ public/src/app_g_game_save_get_or_refresh.mjs
@@ -3,11 +3,9 @@ import { null_is } from "../../../love/public/src/null_is.mjs";
 import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { catch_null_async } from "../../../love/public/src/catch_null_async.mjs";
 export async function app_g_game_save_get_or_refresh(context) {
-  let refresh = false;
   let g = await catch_null_async(app_g_game_save_get);
   if (null_is(g)) {
     await app_g_main(context);
-    refresh = true;
   }
-  return refresh;
+  return g;
 }
