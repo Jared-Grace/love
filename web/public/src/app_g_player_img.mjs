df97c04040acf63234c1c058c8bf6e4a6efac6ac
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:54:35 -0400

    pf g_character_img parent

--- web/public/src/app_g_player_img.mjs
+++ web/public/src/app_g_player_img.mjs
@@ -1,7 +1,7 @@
 import { app_g_player_style } from "../../../love/public/src/app_g_player_style.mjs";
 import { g_character_img } from "../../../love/public/src/g_character_img.mjs";
 export function app_g_player_img(game_prefix, parent, player) {
-  let player_img_c = g_character_img(game_prefix, parent, player);
+  let player_img_c = g_character_img(parent, game_prefix, player);
   app_g_player_style(player_img_c);
   return player_img_c;
 }
