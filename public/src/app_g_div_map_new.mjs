50e97e246dea5fa2b0c8fb74d919523b0bf2e7e0
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:05:24 -0400

    function_auto_path ../love/public/src/app_g_div_map_new.mjs

--- public/src/app_g_div_map_new.mjs
+++ public/src/app_g_div_map_new.mjs
@@ -1,3 +1,4 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
 import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
@@ -10,6 +11,8 @@ import { property_set_exists_not } from "../../../love/public/src/property_set_e
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export async function app_g_div_map_new(div_map_container, context) {
   let g = await app_g_game_save_get();
+  let npcs = property_get(g, "npcs");
+  let player = property_get(g, "player");
   let div_map = html_div(div_map_container);
   property_set_exists_not(div_map, "container", div_map_container);
   let player_img_c = app_g_player_img(div_map, player);
