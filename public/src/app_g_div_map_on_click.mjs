0d3d2e9feb0a0f693a2e0842bd7ebafe7515b335
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:40:07 -0400

    pd app_g_div_map_on_click div_map_container

--- public/src/app_g_div_map_on_click.mjs
+++ public/src/app_g_div_map_on_click.mjs
@@ -1,12 +1,7 @@
 import { html_on_click } from "../../../love/public/src/html_on_click.mjs";
 import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
 import { app_g_refresh } from "../../../love/public/src/app_g_refresh.mjs";
-export function app_g_div_map_on_click(
-  context,
-  div_map_container,
-  div_map,
-  player_img_c,
-) {
+export function app_g_div_map_on_click(context, div_map, player_img_c) {
   async function on_click(e) {
     async function refresh() {
       await app_g_refresh(context, div_map_container);
