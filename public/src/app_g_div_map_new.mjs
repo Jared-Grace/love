426cefde9fc89b683915a5827d33511d03588627
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:37:30 -0400

    function_auto_path ../love/public/src/app_g_div_map_new.mjs

--- public/src/app_g_div_map_new.mjs
+++ public/src/app_g_div_map_new.mjs
@@ -3,7 +3,6 @@ import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_ge
 import { app_g_div_map_tiles_add } from "../../../love/public/src/app_g_div_map_tiles_add.mjs";
 import { app_g_div_map_style } from "../../../love/public/src/app_g_div_map_style.mjs";
 import { app_g_div_map_npcs_add } from "../../../love/public/src/app_g_div_map_npcs_add.mjs";
-import { app_g_player_img } from "../../../love/public/src/app_g_player_img.mjs";
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export async function app_g_div_map_new(div_map_container) {
@@ -12,7 +11,6 @@ export async function app_g_div_map_new(div_map_container) {
   let player = property_get(g, "player");
   let div_map = html_div(div_map_container);
   property_set_exists_not(div_map, "container", div_map_container);
-  let player_img_c = app_g_player_img(div_map, player);
   app_g_div_map_npcs_add(div_map, npcs);
   await app_g_div_map_style(div_map);
   await app_g_div_map_tiles_add(div_map);
