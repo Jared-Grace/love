853313579c80f8dd0c80eb4ebbfa9662080bb7fa
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:59:17 -0400

    function_auto_path ../love/public/src/app_g_div_map_npcs_add.mjs

--- web/public/src/app_g_div_map_npcs_add.mjs
+++ web/public/src/app_g_div_map_npcs_add.mjs
@@ -1,8 +1,10 @@
+import { g_game_prefix } from "../../../love/public/src/g_game_prefix.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { g_icon_cross } from "../../../love/public/src/g_icon_cross.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { g_character_img } from "../../../love/public/src/g_character_img.mjs";
 export function app_g_div_map_npcs_add(div_map, npcs) {
+  const game_prefix = g_game_prefix();
   function lambda12(npc) {
     let ci = g_character_img(div_map, game_prefix, npc);
     let christian = property_get(npc, "christian");
