16a495bc4f1016f71ffbb2edfa1b4ede7ad73e83
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:01:14 -0400

    function_auto_path ../love/public/src/app_g_div_map_tiles_add.mjs

--- public/src/app_g_div_map_tiles_add.mjs
+++ public/src/app_g_div_map_tiles_add.mjs
@@ -1,6 +1,8 @@
+import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { each_index } from "../../../love/public/src/each_index.mjs";
 import { app_g_tile } from "../../../love/public/src/app_g_tile.mjs";
-export function app_g_div_map_tiles_add(div_map) {
+export async function app_g_div_map_tiles_add(div_map) {
+  let g = await app_g_game_save_get();
   function lambda2(columns, y) {
     function lambda(r, x) {
       app_g_tile(div_map, r, x, y);
