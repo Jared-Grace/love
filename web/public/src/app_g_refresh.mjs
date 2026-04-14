9641c4e505e8696f8db48eb4c00f0213a95655a3
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:01:27 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

--- web/public/src/app_g_refresh.mjs
+++ web/public/src/app_g_refresh.mjs
@@ -30,11 +30,10 @@ export async function app_g_refresh(context, div_map_container) {
   }
   let npcs = property_get(g, "npcs");
   let player = property_get(g, "player");
-  let rows = property_get(g, "rows");
   let player_img_c = app_g_player_img(div_map, player);
   app_g_div_map_npcs_add(div_map, npcs);
   await app_g_div_map_style(div_map);
-  app_g_div_map_tiles_add(div_map);
+  await app_g_div_map_tiles_add(div_map);
   async function on_click(e) {
     await app_g_click(e, div_map, player_img_c, refresh);
   }
