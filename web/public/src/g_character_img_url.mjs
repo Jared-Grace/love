92c5e6ee655d9827b94dc6d2d0564bcdbcba1a29
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:00:04 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

--- web/public/src/g_character_img_url.mjs
+++ web/public/src/g_character_img_url.mjs
@@ -1,5 +1,6 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function g_character_img_url(c) {
+  const game_prefix = g_game_prefix();
   let img = property_get(c, "img");
   const c_src = game_prefix + "characters\\" + img + "\\rotations\\south.png";
   return c_src;
