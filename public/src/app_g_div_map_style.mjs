04c25e3955599d5f21ff6c62e0d98315731f569c
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:57:26 -0400

    function_auto_path ../love/public/src/app_g_div_map_style.mjs

--- public/src/app_g_div_map_style.mjs
+++ public/src/app_g_div_map_style.mjs
@@ -1,9 +1,11 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export async function app_g_div_map_style(div_map) {
   let g = await app_g_game_save_get();
+  let rows = property_get(g, "rows");
   html_style_assign(div_map, {
     position: "relative",
     display: "grid",
