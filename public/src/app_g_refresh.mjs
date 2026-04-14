f46a3bf3465445f5997531df179d53fb18d6245e
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:37:44 -0400

    function_auto_path ../love/public/src/app_g_div_map_new.mjs

--- public/src/app_g_refresh.mjs
+++ public/src/app_g_refresh.mjs
@@ -10,7 +10,7 @@ export async function app_g_refresh(context, div_map_container) {
     return;
   }
   html_clear(div_map_container);
-  let r = await app_g_div_map_new(div_map_container);
+  let div_map = await app_g_div_map_new(div_map_container);
   let player_img_c = app_g_player_img(div_map, player);
   app_g_div_map_on_click(context, div_map_container, div_map, player_img_c);
   await app_g_player_scroll_center(div_map, player_img_c);
