4b767c19858b5690889d681476e8bfa75fcd979c
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:45:22 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

---
+++ public/src/app_g_tile.mjs
@@ -0,0 +1,19 @@
+import { html_data_set_json } from "../../../love/public/src/html_data_set_json.mjs";
+import { html_class_add } from "../../../love/public/src/html_class_add.mjs";
+import { g_img_square_style } from "../../../love/public/src/g_img_square_style.mjs";
+import { html_img } from "../../../love/public/src/html_img.mjs";
+import { app_g_class_tile } from "../../../love/public/src/app_g_class_tile.mjs";
+import { g_folder_tiles } from "../../../love/public/src/g_folder_tiles.mjs";
+export function app_g_tile(path_prefix, r, div_map, x, y) {
+  const tiles_path = g_folder_tiles(path_prefix);
+  const tile_class = app_g_class_tile();
+  const src = tiles_path + r + ".png";
+  let tile = html_img(div_map, src);
+  g_img_square_style(tile);
+  html_class_add(tile, tile_class);
+  const coordinates_tile = {
+    x,
+    y,
+  };
+  html_data_set_json(tile, "coordinates", coordinates_tile);
+}
