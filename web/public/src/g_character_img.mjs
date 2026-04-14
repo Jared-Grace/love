a5ef3a3e186a4b4e81569983df71ba5431e380eb
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 15:59:53 -0400

    pd g_character_img_url game_prefix

--- web/public/src/g_character_img.mjs
+++ web/public/src/g_character_img.mjs
@@ -3,7 +3,7 @@ import { html_click_none } from "../../../love/public/src/html_click_none.mjs";
 import { g_img_square_style_position_object } from "../../../love/public/src/g_img_square_style_position_object.mjs";
 import { g_img_square } from "../../../love/public/src/g_img_square.mjs";
 export function g_character_img(parent, c) {
-  const c_src = g_character_img_url(c, game_prefix);
+  const c_src = g_character_img_url(c);
   let ci = g_img_square(parent, c_src, -1, -1, "character");
   g_img_square_style_position_object(c, ci);
   html_click_none(ci);
