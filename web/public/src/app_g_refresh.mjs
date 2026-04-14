627a4981f638ed1be2c580d19fbbfbe75ec0650c
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 16:41:34 -0400

    function_auto_path ../love/public/src/app_g_refresh.mjs

--- web/public/src/app_g_refresh.mjs
+++ web/public/src/app_g_refresh.mjs
@@ -1,4 +1,3 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_player_img } from "../../../love/public/src/app_g_player_img.mjs";
 import { app_g_div_map_on_click } from "../../../love/public/src/app_g_div_map_on_click.mjs";
 import { app_g_game_save_get_or_refresh } from "../../../love/public/src/app_g_game_save_get_or_refresh.mjs";
@@ -13,7 +12,7 @@ export async function app_g_refresh(context, div_map_container) {
   }
   html_clear(div_map_container);
   let div_map = await app_g_div_map_new(div_map_container);
-  let player_img_c = app_g_player_img(div_map);
+  let player_img_c = await app_g_player_img(div_map);
   app_g_div_map_on_click(context, div_map, player_img_c);
   await app_g_player_scroll_center(div_map, player_img_c);
 }
