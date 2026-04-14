027e68de8cde88f9b697b99402cef9b25edb576e
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 18:38:24 -0400

    function_auto_path ../love/public/src/file_read_cached_json.mjs

--- public/src/file_read_cached_json.mjs
+++ public/src/file_read_cached_json.mjs
@@ -1,9 +1,9 @@
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
-export async function file_read_cached_json(p) {
-  await file_read_cached(p);
+export async function file_read_cached_json(file_path) {
+  await file_read_cached(file_path);
   let contents = await file_read(file_path);
-  let g = json_from(json);
+  let g = json_from(contents);
   return g;
 }
