40fe38a7e3f726e38a9b6513737e1c4d7796f584
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:47:21 -0400

    function_auto_path ../love/public/src/function_name_combine_curried_right.mjs

--- public/src/function_name_combine_curried_right.mjs
+++ public/src/function_name_combine_curried_right.mjs
@@ -1,6 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 export function function_name_combine_curried_right(right) {
   let r = function function_name_combine_curried_right_result(left) {
+    log(function_name_combine_curried_right.name, {
+      left,
+      right,
+    });
     let combined = function_name_combine(left, right);
     return combined;
   };
