3610fb2f13c0f53c3ae61bbd2b25573ebaf3ca91
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:52:51 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get.mjs

--- web/public/src/function_curryify_specify_name_get.mjs
+++ web/public/src/function_curryify_specify_name_get.mjs
@@ -1,4 +1,5 @@
-import { js_function_declaration_param_add_node } from "../../../love/public/src/js_function_declaration_param_add_node.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { js_flo } from "../../../love/public/src/js_flo.mjs";
@@ -8,7 +9,8 @@ export function function_curryify_specify_name_get(unaliased, positions_1) {
   let declaration2 = js_flo(ast);
   let a = function js_declaration_param_add_curried_result(param_name) {
     let item = js_parse_expression(param_name);
-    js_function_declaration_param_add_node(declaration2, item);
+    let params = js_function_declaration_params_get(declaration2);
+    list_add(params, item);
   };
   each(param_names, a);
   let declaration = declaration2;
