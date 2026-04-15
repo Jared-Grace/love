e9073b9c009fc087e419c3edff0fd1d52ea95dc8
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:06:33 -0400

    function_auto_path ../love/public/src/js_return_argument_get.mjs

--- web/public/src/js_return_argument_get.mjs
+++ web/public/src/js_return_argument_get.mjs
@@ -1,5 +1,5 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_return_argument_get(node) {
-  let value2 = property_get(node, "argument");
-  return value2;
+  let value = property_get(node, "argument");
+  return value;
 }
