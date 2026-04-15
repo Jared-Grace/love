edc937fb31a67cafc7c71dd0e04a5165f372b16c
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:51:24 -0400

    function_auto_path ../love/public/src/function_name_new_get_generic.mjs

--- public/src/function_name_new_get_generic.mjs
+++ public/src/function_name_new_get_generic.mjs
@@ -1,12 +1,10 @@
+import { override_get } from "../../../love/public/src/override_get.mjs";
 import { function_name_new_get_generic_overrides } from "../../../love/public/src/function_name_new_get_generic_overrides.mjs";
-import { property_exists_if_get } from "../../../love/public/src/property_exists_if_get.mjs";
-import { dictionary_functions_to_names } from "../../../love/public/src/dictionary_functions_to_names.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function function_name_new_get_generic(f_name_old, plugin_fn) {
   f_name_old = await function_name_unalias_only(f_name_old);
   let overrides = function_name_new_get_generic_overrides();
-  dictionary_functions_to_names(overrides);
-  plugin_fn = property_exists_if_get(overrides, plugin_fn);
+  plugin_fn = override_get(overrides, plugin_fn);
   let r = {
     f_name_old,
     plugin_fn,
---
+++ public/src/override_get.mjs
@@ -0,0 +1,7 @@
+import { property_exists_if_get } from "../../../love/public/src/property_exists_if_get.mjs";
+import { dictionary_functions_to_names } from "../../../love/public/src/dictionary_functions_to_names.mjs";
+export function override_get(overrides, value) {
+  dictionary_functions_to_names(overrides);
+  value = property_exists_if_get(overrides, value);
+  return value;
+}
