6fc17395b90b0e8ad2c233dc52136e11b756bd45
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 18:50:26 -0400

    function_auto_path ../love/public/src/data_prompts.mjs

--- public/src/data_prompts.mjs
+++ public/src/data_prompts.mjs
@@ -2,7 +2,7 @@ import { data_prompts_count } from "../../../love/public/src/data_prompts_count.
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 export async function data_prompts() {
   arguments_assert(arguments, 0);
-  let offset = 25;
+  let offset = await data_prompts_count();
   let result = await data_prompts_count(offset);
   return result;
 }
--- public/src/data_prompts_display.mjs
+++ public/src/data_prompts_display.mjs
@@ -2,7 +2,11 @@ import { data_prompts_count_display } from "../../../love/public/src/data_prompt
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 export async function data_prompts_display() {
   arguments_assert(arguments, 0);
-  let offset = 50;
+  let offset = data_prompts_count();
   let result = await data_prompts_count_display(offset);
   return result;
 }
+function data_prompts_count() {
+  return 50;
+}
+
