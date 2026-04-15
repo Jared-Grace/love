f51459380f4edfd3d10a6db2300f796e47ef3df8
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 17:22:15 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

---
+++ web/public/src/g_img_square_style_position_object_later.mjs
@@ -0,0 +1,6 @@
+import { g_img_square_style_position_object } from "../../../love/public/src/g_img_square_style_position_object.mjs";
+export function g_img_square_style_position_object_later(c, ci) {
+  return function g_img_square_style_position_object_later_result() {
+    return g_img_square_style_position_object(c, ci);
+  };
+}
