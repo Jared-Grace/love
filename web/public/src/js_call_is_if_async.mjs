efd746667e3330a1e1b0c9cb9efd3fbd824e20ca
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:20:41 -0400

    function_auto_path ../love/public/src/js_call_is_if_async.mjs

--- web/public/src/js_call_is_if_async.mjs
+++ web/public/src/js_call_is_if_async.mjs
@@ -1,7 +1,7 @@
 import { js_call_is } from "../../../love/public/src/js_call_is.mjs";
-export function js_call_is_if_async(expression, on_call_is) {
+export async function js_call_is_if_async(expression, on_call_is) {
   let ci = js_call_is(expression);
   if (ci) {
-    on_call_is();
+    await on_call_is();
   }
 }
