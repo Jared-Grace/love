b1478ed79146226bd44495468cc8b4788536c7b7
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:42:19 -0400

    de g_img_square_style_position_object_curried_specify_1_2

--- public/src/g_img_square_style_position_object_curried_specify_1_2.mjs
+++
@@ -1,9 +0,0 @@
-import { g_img_square_style_position_object } from "../../../love/public/src/g_img_square_style_position_object.mjs";
-export function g_img_square_style_position_object_curried_specify_1_2(c, ci) {
-  let r2 =
-    function g_img_square_style_position_object_curried_specify_1_2_result() {
-      let r = g_img_square_style_position_object(c, ci);
-      return r;
-    };
-  return r2;
-}
