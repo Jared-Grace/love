36936f6b266271ed93ed34b2d580ba9eed9c9f40
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:54:40 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get.mjs

--- public/src/function_curryify_specify_name_get.mjs
+++ public/src/function_curryify_specify_name_get.mjs
@@ -1,3 +1,4 @@
+import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
 import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
 import { js_flo } from "../../../love/public/src/js_flo.mjs";
 import { function_name_combine_multiple_concat } from "../../../love/public/src/function_name_combine_multiple_concat.mjs";
@@ -5,6 +6,10 @@ import { function_curryify_specify_name } from "../../../love/public/src/functio
 export function function_curryify_specify_name_get(unaliased, positions_1) {
   let declaration2 = js_flo(ast);
   let params = js_function_declaration_params_get(declaration2);
+  let e = lists_sizes_equal([params, positions_1]);
+  if (e) {
+  } else {
+  }
   let n = function_curryify_specify_name(unaliased);
   const parts = [n];
   let combined = function_name_combine_multiple_concat(parts, positions_1);
