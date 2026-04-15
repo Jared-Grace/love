467c28689886d813e62b4f5d39febfb5516cc021
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 17:16:04 -0400

    function_auto_path ../love/public/src/list_adder_generic.mjs

--- web/public/src/list_adder_generic.mjs
+++ web/public/src/list_adder_generic.mjs
@@ -1,8 +1,10 @@
+import { promise_not_is_assert } from "../../../love/public/src/promise_not_is_assert.mjs";
 export function list_adder_generic(lambda, fn) {
   let list = [];
   function list_adder_inner(item) {
     fn(list, item);
   }
-  lambda(list_adder_inner);
+  let result = lambda(list_adder_inner);
   return list;
+  promise_not_is_assert(ast);
 }
