e5048b8810a6f45a18d189e42923db6cfc82c647
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:02:44 -0400

    function_auto_path ../love/public/src/js_return_above_combine.mjs

--- public/src/js_return_above_combine.mjs
+++ public/src/js_return_above_combine.mjs
@@ -1,5 +1,4 @@
-import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
-import { js_declare_is_if } from "../../../love/public/src/js_declare_is_if.mjs";
+import { js_declare_single_is_if } from "../../../love/public/src/js_declare_single_is_if.mjs";
 import { js_identifiers_names_equal_not } from "../../../love/public/src/js_identifiers_names_equal_not.mjs";
 import { js_literal_is_if } from "../../../love/public/src/js_literal_is_if.mjs";
 import { list_includes_if } from "../../../love/public/src/list_includes_if.mjs";
@@ -7,7 +6,6 @@ import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { js_return_argument_set } from "../../../love/public/src/js_return_argument_set.mjs";
 import { js_visit_returns_identifiers } from "../../../love/public/src/js_visit_returns_identifiers.mjs";
 import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_if.mjs";
-import { list_single_if } from "../../../love/public/src/list_single_if.mjs";
 import { list_first_is } from "../../../love/public/src/list_first_is.mjs";
 import { list_previous } from "../../../love/public/src/list_previous.mjs";
 import { not } from "../../../love/public/src/not.mjs";
@@ -50,10 +48,6 @@ export function js_return_above_combine(ast) {
       }
       js_identifier_is_if(id, lambda4);
     }
-    function lambda3() {
-      let declarations = js_declare_declarations_get(previous);
-      list_single_if(declarations, lambda);
-    }
-    js_declare_is_if(previous, lambda3);
+    js_declare_single_is_if(previous, lambda);
   }
 }
