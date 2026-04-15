c541a4df98fea1aa93e102c11a60aaf8ce823fed
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:39:27 -0400

    function_auto_path ../love/public/src/g_distance_1.mjs

--- public/src/g_distance_1.mjs
+++ public/src/g_distance_1.mjs
@@ -1,6 +1,6 @@
 import { g_distance_equal } from "../../../love/public/src/g_distance_equal.mjs";
-export function g_distance_1(c, w) {
+export function g_distance_1(left, right) {
   const d = 1;
-  const zi = g_distance_equal(player, coordinates, d);
+  const zi = g_distance_equal(left, right, d);
   return zi;
 }
