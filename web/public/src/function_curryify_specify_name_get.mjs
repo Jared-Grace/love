a02019560f47cc8d6c73fa3a346c606cd3726e01
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 17:20:15 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get.mjs

--- web/public/src/function_curryify_specify_name_get.mjs
+++ web/public/src/function_curryify_specify_name_get.mjs
@@ -1,8 +1,8 @@
+import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { js_flo_params_get } from "../../../love/public/src/js_flo_params_get.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
-import { function_name_combine_multiple_concat } from "../../../love/public/src/function_name_combine_multiple_concat.mjs";
 import { function_curryify_specify_name } from "../../../love/public/src/function_curryify_specify_name.mjs";
 export async function function_curryify_specify_name_get(
   unaliased,
@@ -20,6 +20,6 @@ export async function function_curryify_specify_name_get(
     let concated = list_concat(a, b);
   }
   const parts = [n];
-  let combined = function_name_combine_multiple_concat(parts, ending);
+  let combined = function_name_combine_multiple(parts, ending);
   return combined;
 }
