9833573b735a0c5aa47bbb4d091af0034a50f9a1
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:53:30 -0400

    c r function_name_new_get_generic_overrides,function_transform_single_overrides

---
+++ public/src/function_transform_single_overrides.mjs
@@ -0,0 +1,23 @@
+import { function_name_to_part_replace_last_3 } from "../../../love/public/src/function_name_to_part_replace_last_3.mjs";
+import { function_name_to_part_replace_last_2 } from "../../../love/public/src/function_name_to_part_replace_last_2.mjs";
+import { function_name_to_part_replace_last } from "../../../love/public/src/function_name_to_part_replace_last.mjs";
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
+import { function_name_parts_swap_end } from "../../../love/public/src/function_name_parts_swap_end.mjs";
+import { lambda_right } from "../../../love/public/src/lambda_right.mjs";
+import { function_name_part_last_delete } from "../../../love/public/src/function_name_part_last_delete.mjs";
+import { function_name_parts_delete } from "../../../love/public/src/function_name_parts_delete.mjs";
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+export function function_transform_single_overrides() {
+  let r2 = {
+    c: function_name_combine,
+    d: function_name_parts_delete,
+    l: function_name_part_last_delete,
+    r: lambda_right,
+    s: function_name_parts_swap_end,
+    t: text_replace,
+    1: function_name_to_part_replace_last,
+    2: function_name_to_part_replace_last_2,
+    3: function_name_to_part_replace_last_3,
+  };
+  return r2;
+}
