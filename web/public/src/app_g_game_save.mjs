854c3679d17233c54c595570d00530d83ef6ea54
JESUS rose to life <username@users.noreply.github.com>
2026-04-14 23:19:37 -0400

    function_auto_path ../love/public/src/app_g_game_save.mjs

--- web/public/src/app_g_game_save.mjs
+++ web/public/src/app_g_game_save.mjs
@@ -1,8 +1,6 @@
 import { file_overwrite_cached } from "../../../love/public/src/file_overwrite_cached.mjs";
-import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
 import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_path.mjs";
 export async function app_g_game_save(g) {
   let p = app_g_game_save_path();
   await file_overwrite_cached(p);
-  await file_overwrite_json(p, g);
 }
