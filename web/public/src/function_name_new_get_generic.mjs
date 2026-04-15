e03d0a4f1576ac825c5e380d815c49c5d1894781
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:47:56 -0400

    function_auto_path ../love/public/src/function_name_new_get_generic.mjs

--- web/public/src/function_name_new_get_generic.mjs
+++ web/public/src/function_name_new_get_generic.mjs
@@ -1,28 +1,10 @@
-import { function_name_part_last_delete } from "../../../love/public/src/function_name_part_last_delete.mjs";
-import { function_name_to_part_replace_last_3 } from "../../../love/public/src/function_name_to_part_replace_last_3.mjs";
-import { function_name_to_part_replace_last } from "../../../love/public/src/function_name_to_part_replace_last.mjs";
-import { function_name_parts_swap_end } from "../../../love/public/src/function_name_parts_swap_end.mjs";
-import { function_name_to_part_replace_last_2 } from "../../../love/public/src/function_name_to_part_replace_last_2.mjs";
-import { function_name_parts_delete } from "../../../love/public/src/function_name_parts_delete.mjs";
-import { lambda_right } from "../../../love/public/src/lambda_right.mjs";
+import { function_name_new_get_generic_overrides } from "../../../love/public/src/function_name_new_get_generic_overrides.mjs";
 import { property_exists_if_get } from "../../../love/public/src/property_exists_if_get.mjs";
 import { dictionary_functions_to_names } from "../../../love/public/src/dictionary_functions_to_names.mjs";
-import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
-import { text_replace } from "../../../love/public/src/text_replace.mjs";
 export async function function_name_new_get_generic(f_name_old, plugin_fn) {
   f_name_old = await function_name_unalias_only(f_name_old);
-  let overrides = {
-    c: function_name_combine,
-    d: function_name_parts_delete,
-    l: function_name_part_last_delete,
-    r: lambda_right,
-    s: function_name_parts_swap_end,
-    t: text_replace,
-    1: function_name_to_part_replace_last,
-    2: function_name_to_part_replace_last_2,
-    3: function_name_to_part_replace_last_3,
-  };
+  let overrides = function_name_new_get_generic_overrides();
   dictionary_functions_to_names(overrides);
   plugin_fn = property_exists_if_get(overrides, plugin_fn);
   let r = {
