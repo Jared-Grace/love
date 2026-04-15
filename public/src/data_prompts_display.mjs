576e6a62fc2136f46356858be22548fe7e6e6c0e
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 18:50:44 -0400

    function_auto_path ../love/public/src/data_prompts_display.mjs

--- public/src/data_prompts_display.mjs
+++ public/src/data_prompts_display.mjs
@@ -1,12 +1,9 @@
+import { data_prompts_offset } from "../../../love/public/src/data_prompts_offset.mjs";
 import { data_prompts_count_display } from "../../../love/public/src/data_prompts_count_display.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 export async function data_prompts_display() {
   arguments_assert(arguments, 0);
-  let offset = data_prompts_count();
+  let offset = data_prompts_offset();
   let result = await data_prompts_count_display(offset);
   return result;
 }
-function data_prompts_count() {
-  return 50;
-}
-
