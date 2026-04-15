59740d10671a656c1202939aa368032ed71656ac
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:45:55 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,1 +1,4 @@
-export async function sandbox_3() {function_delete_if_exists}
+import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
+export async function sandbox_3() {
+  await function_delete_if_exists(f_name);
+}
