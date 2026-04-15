4c4f5acafca178518fde6e5d02e8f0171cf87176
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:25:23 -0400

    function_auto_path ../love/public/src/js_parse_expression_from_assignment.mjs

--- public/src/js_parse_expression_from_assignment.mjs
+++ public/src/js_parse_expression_from_assignment.mjs
@@ -1,5 +1,4 @@
 import { js_declare_init_get } from "../../../love/public/src/js_declare_init_get.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_declare_single } from "../../../love/public/src/js_declare_single.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { js_code_let_assign } from "../../../love/public/src/js_code_let_assign.mjs";
@@ -7,7 +6,6 @@ export function js_parse_expression_from_assignment(code) {
   let code_assign = js_code_let_assign("a", code);
   let statement = js_parse_statement(code_assign);
   let d = js_declare_single(statement);
-  let init = property_get(d, "init");
+  let init = js_declare_init_get(d, "init");
   return init;
-  let r = js_declare_init_get(node);
 }
