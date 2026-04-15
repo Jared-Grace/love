a9d0a2c350d9201ed10819f2f06e4a905b22b5a2
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:14:04 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- web/public/src/js_curry_replace.mjs
+++ web/public/src/js_curry_replace.mjs
@@ -1,5 +1,5 @@
+import { js_declare_single_identifier_is_if } from "../../../love/public/src/js_declare_single_identifier_is_if.mjs";
 import { js_return_argument_identifier_is_if } from "../../../love/public/src/js_return_argument_identifier_is_if.mjs";
-import { js_declare_single_is_if } from "../../../love/public/src/js_declare_single_is_if.mjs";
 import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
 import { list_adder_unique_async } from "../../../love/public/src/list_adder_unique_async.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -118,7 +118,7 @@ export async function js_curry_replace(ast) {
           }
           js_return_argument_identifier_is_if(second, lambda5);
         }
-        js_declare_single_is_if(first, lambda4);
+        js_declare_single_identifier_is_if(first, lambda4);
       }
     }
     await each_async(list, lambda);
