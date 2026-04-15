9b56eb3de8eee8b24cef92d0a0864a567401375c
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:57:00 -0400

    function_auto_path ../love/public/src/override_get.mjs

--- public/src/override_get.mjs
+++ public/src/override_get.mjs
@@ -1,7 +1,7 @@
 import { property_exists_if_get } from "../../../love/public/src/property_exists_if_get.mjs";
 import { dictionary_functions_to_names } from "../../../love/public/src/dictionary_functions_to_names.mjs";
-export function override_get(overrides, value) {
-  dictionary_functions_to_names(overrides);
-  value = property_exists_if_get(overrides, value);
+export function override_get(overrides_get, value) {
+  dictionary_functions_to_names(overrides_get);
+  value = property_exists_if_get(overrides_get, value);
   return value;
 }
