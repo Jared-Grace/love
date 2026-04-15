5f736e0b034a1855be79f250c04693e9ea58a33d
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:08:05 -0400

    function_auto_path ../love/public/src/js_return_on_async.mjs

--- public/src/js_return_on_async.mjs
+++ public/src/js_return_on_async.mjs
@@ -1,9 +1,9 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_return_argument_get } from "../../../love/public/src/js_return_argument_get.mjs";
 import { js_node_return_is } from "../../../love/public/src/js_node_return_is.mjs";
 import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 export async function js_return_on_async(node, identifier_if, identifier_not) {
   if (js_node_return_is(node)) {
-    let argument = property_get(node, "argument");
+    let argument = js_return_argument_get(node, "argument");
     if (js_node_type_is(argument, "Identifier")) {
       await identifier_if(argument);
     } else {
