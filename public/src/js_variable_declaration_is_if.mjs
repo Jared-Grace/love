f85393cad581a160e3aad3f9bd5edee5940f49d5
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:01:49 -0400

    frp js_variable_declaration_is_if js_declare_is_if

--- public/src/js_variable_declaration_is_if.mjs
+++
@@ -1,4 +0,0 @@
-import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
-export function js_variable_declaration_is_if(previous, lambda3) {
-  js_node_type_is_if(previous, "VariableDeclaration", lambda3);
-}
