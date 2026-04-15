c1bde95699a4804bcd14c619d0a1448dd8683383
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:33:58 -0400

    function_auto_path ../love/public/src/html_event_target_closest.mjs

--- public/src/html_event_target_closest.mjs
+++ public/src/html_event_target_closest.mjs
@@ -1,6 +1,6 @@
 import { html_component_wrap } from "../../../love/public/src/html_component_wrap.mjs";
-export function html_event_target_closest(e, c) {
-  const tile_e = e.target.closest(c);
+export function html_event_target_closest(e, selector) {
+  const tile_e = e.target.closest(selector);
   let tile = html_component_wrap(tile_e);
   return tile;
 }
