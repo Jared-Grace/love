a063d5c09d13db583f201da5ee52f842d577dec3
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 18:38:08 -0400

    function_auto_path ../love/public/src/list_shuffle_sort_number_mapper_first.mjs

--- public/src/list_shuffle_sort_number_mapper_first.mjs
+++ public/src/list_shuffle_sort_number_mapper_first.mjs
@@ -1,7 +1,7 @@
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { list_shuffle_sort_number_mapper } from "../../../love/public/src/list_shuffle_sort_number_mapper.mjs";
-export function list_shuffle_sort_number_mapper_first(list, lambda19) {
-  list_shuffle_sort_number_mapper(list, lambda19);
+export function list_shuffle_sort_number_mapper_first(list, lambda$item) {
+  list_shuffle_sort_number_mapper(list, lambda$item);
   let coordinates_move_to = list_first(list);
   return coordinates_move_to;
 }
