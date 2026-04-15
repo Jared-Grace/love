fa90fdd322f946e5b6275b37e1455dee897bcc47
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 15:52:42 -0400

    function_auto_path ../love/public/src/function_transform_single.mjs

--- public/src/function_transform_single.mjs
+++ public/src/function_transform_single.mjs
@@ -1,3 +1,5 @@
+import { override_get } from "../../../love/public/src/override_get.mjs";
+import { function_name_new_get_generic_overrides } from "../../../love/public/src/function_name_new_get_generic_overrides.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_import_unalias } from "../../../love/public/src/function_import_unalias.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -9,6 +11,8 @@ export async function function_transform_single(
   let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
   let remaining = property_get(fr, "remaining");
   let f_name_transformer = property_get(fr, "first");
+  let overrides = function_name_new_get_generic_overrides();
+  f_name_transformer = override_get(overrides, f_name_transformer);
   let imported_fn = await function_import_unalias(f_name_transformer);
   async function lambda(ast) {
     let result = await imported_fn(ast, ...remaining);
