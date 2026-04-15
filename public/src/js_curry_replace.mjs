f9957ecd745b9a88c7595f5839865276bfa75dab
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 18:56:39 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -107,9 +107,9 @@ export async function js_curry_replace(ast) {
       let s2 = list_size_2(body_block);
       if (s2) {
         let r = list_first_second(body_block);
-        let second = property_get(r, "second");
         let first = property_get(r, "first");
         function lambda4() {
+          let second = property_get(r, "second");
           function lambda5() {
             log(js_curry_replace.name, {
               first,
