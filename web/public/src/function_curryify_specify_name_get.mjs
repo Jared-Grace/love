0c64ec4d5ca95c27b867576e917630c4df2e0b12
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:56:05 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get.mjs

--- web/public/src/function_curryify_specify_name_get.mjs
+++ web/public/src/function_curryify_specify_name_get.mjs
@@ -1,16 +1,14 @@
+import { js_flo_params_get } from "../../../love/public/src/js_flo_params_get.mjs";
 import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
-import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
-import { js_flo } from "../../../love/public/src/js_flo.mjs";
 import { function_name_combine_multiple_concat } from "../../../love/public/src/function_name_combine_multiple_concat.mjs";
 import { function_curryify_specify_name } from "../../../love/public/src/function_curryify_specify_name.mjs";
 export async function function_curryify_specify_name_get(
   unaliased,
   positions_1,
 ) {
-  let ast2 = await function_ast(f_name);
-  let declaration2 = js_flo(ast);
-  let params = js_function_declaration_params_get(declaration2);
+  let ast = await function_ast(unaliased);
+  let params = js_flo_params_get(ast);
   let ending = null;
   let e = lists_sizes_equal([params, positions_1]);
   if (e) {
