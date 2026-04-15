7cff7eedb917c38a0e5f98dc64520f00d83292bf
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 19:00:00 -0400

    function_auto_path ../love/public/src/js_declare_single.mjs

--- public/src/js_declare_single.mjs
+++ public/src/js_declare_single.mjs
@@ -1,5 +1,4 @@
 import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
@@ -10,7 +9,7 @@ export function js_declare_single(vd) {
   if (not(b)) {
     return declaration;
   }
-  let declarations = property_get(vd, "declarations");
+  let declarations = js_declare_declarations_get(vd);
   let s1 = list_size_1(declarations);
   if (not(s1)) {
     return declaration;
