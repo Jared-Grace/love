8c21a4ec969419852c176cfb609cae400a8e6efd
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 16:55:50 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get.mjs

--- public/src/function_curryify_specify_name_get.mjs
+++ public/src/function_curryify_specify_name_get.mjs
@@ -1,9 +1,14 @@
+import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
 import { js_function_declaration_params_get } from "../../../love/public/src/js_function_declaration_params_get.mjs";
 import { js_flo } from "../../../love/public/src/js_flo.mjs";
 import { function_name_combine_multiple_concat } from "../../../love/public/src/function_name_combine_multiple_concat.mjs";
 import { function_curryify_specify_name } from "../../../love/public/src/function_curryify_specify_name.mjs";
-export function function_curryify_specify_name_get(unaliased, positions_1) {
+export async function function_curryify_specify_name_get(
+  unaliased,
+  positions_1,
+) {
+  let ast2 = await function_ast(f_name);
   let declaration2 = js_flo(ast);
   let params = js_function_declaration_params_get(declaration2);
   let ending = null;
