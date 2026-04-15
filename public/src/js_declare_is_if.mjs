d27980d0fc4390665e226fa23942d06c8df1a116
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 18:58:06 -0400

    function_auto_path ../love/public/src/js_declare_is_if.mjs

--- public/src/js_declare_is_if.mjs
+++ public/src/js_declare_is_if.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
-export function js_declare_is_if(first, lambda4) {
-  js_node_type_is_if(first, "VariableDeclaration", lambda4);
+export function js_declare_is_if(node, lambda) {
+  js_node_type_is_if(node, "VariableDeclaration", lambda);
 }
