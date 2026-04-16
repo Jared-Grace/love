7aefd15060f250e3be5389d8512be564723b9e61
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:27:55 -0400

    function_auto_path ../love/public/src/object_includes.mjs

--- public/src/object_includes.mjs
+++ public/src/object_includes.mjs
@@ -1,11 +1,9 @@
+import { property_equals_not } from "../../../love/public/src/property_equals_not.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
-import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 export function object_includes(object, item) {
   let e = true;
   function lambda18(value, property) {
-    let value2 = property_get(object, property);
-    if (equal_not(value, value2)) {
+    if (property_equals_not(object, property, value)) {
       e = false;
     }
   }
