dbf46314206b4bf7a13deffa38bacd972f5d2552
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:30:14 -0400

    t c list_filter_includes

--- public/src/list_filter_includes.mjs
+++ public/src/list_filter_includes.mjs
@@ -1,10 +1,8 @@
+import { text_includes_curried_right } from "../../../love/public/src/text_includes_curried_right.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_includes(mapped, part) {
-  function lambda(item) {
-    let n = text_includes(item, part);
-    return n;
-  }
+  let lambda = text_includes_curried_right(part);
   let filtered = list_filter(mapped, lambda);
   return filtered;
 }
