c3b839e8db1deb6d70573bf859c7534ba9caa754
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:27:31 -0400

    function_auto_path ../love/public/src/js_return_argument_identifier_is_if_async.mjs

--- web/public/src/js_return_argument_identifier_is_if_async.mjs
+++ web/public/src/js_return_argument_identifier_is_if_async.mjs
@@ -2,8 +2,8 @@ import { js_identifier_is_if_async } from "../../../love/public/src/js_identifie
 import { js_return_argument_get } from "../../../love/public/src/js_return_argument_get.mjs";
 export async function js_return_argument_identifier_is_if_async(node, lambda) {
   let argument = js_return_argument_get(node);
-  function lambda2() {
-    lambda(argument);
+  async function lambda2() {
+    await lambda(argument);
   }
   await js_identifier_is_if_async(argument, lambda2);
   return argument;
