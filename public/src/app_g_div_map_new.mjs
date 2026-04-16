9b97cda3496a6ffa919c9898dc130ec6b0339a18
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:40:15 -0400

    function_auto_path ../love/public/src/app_g_div_map_new.mjs

--- public/src/app_g_div_map_new.mjs
+++ public/src/app_g_div_map_new.mjs
@@ -1,13 +1,11 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
+import { app_g_npcs_get } from "../../../love/public/src/app_g_npcs_get.mjs";
 import { app_g_div_map_tiles_add } from "../../../love/public/src/app_g_div_map_tiles_add.mjs";
 import { app_g_div_map_style } from "../../../love/public/src/app_g_div_map_style.mjs";
 import { app_g_div_map_npcs_add } from "../../../love/public/src/app_g_div_map_npcs_add.mjs";
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export async function app_g_div_map_new(div_map_container) {
-  let g = await app_g_game_save_get();
-  let npcs = property_get(g, "npcs");
+  let npcs = await app_g_npcs_get();
   let div_map = html_div(div_map_container);
   property_set_exists_not(div_map, "container", div_map_container);
   app_g_div_map_npcs_add(div_map, npcs);
