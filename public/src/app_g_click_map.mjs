db26e770821b25f312ad9033fc1668576f7ce79d
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 18:02:28 -0400

    function_auto_path ../love/public/src/app_g_click_map.mjs

--- public/src/app_g_click_map.mjs
+++ public/src/app_g_click_map.mjs
@@ -3,15 +3,13 @@ import { app_g_click_npc_if } from "../../../love/public/src/app_g_click_npc_if.
 import { app_g_player_coordinates_update_move } from "../../../love/public/src/app_g_player_coordinates_update_move.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { list_filter_object_includes } from "../../../love/public/src/list_filter_object_includes.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 export async function app_g_click_map(
   clicked_coordinates,
   player_img_c,
   div_map,
   refresh,
 ) {
-  let npcs2 = await app_g_npcs_get();
-  let npcs = property_get(g, "npcs");
+  let npcs = await app_g_npcs_get();
   let npcs_matched = list_filter_object_includes(npcs, clicked_coordinates);
   let npc_clicked = list_empty_not_is(npcs_matched);
   await app_g_player_coordinates_update_move(
