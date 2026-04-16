98f3abf8b8c464bc91b03ece181fa707ad0db141
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:05:24 -0400

    function_auto_path ../love/public/src/function_curryify_right.mjs

--- public/src/function_curryify_right.mjs
+++ public/src/function_curryify_right.mjs
@@ -1,10 +1,10 @@
+import { function_curryify_generic } from "../../../love/public/src/function_curryify_generic.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_curryify_right_name } from "../../../love/public/src/function_curryify_right_name.mjs";
 import { function_curryify_right_args_get } from "../../../love/public/src/function_curryify_right_args_get.mjs";
-import { function_curryify_generic_open } from "../../../love/public/src/function_curryify_generic_open.mjs";
 export async function function_curryify_right(f_name) {
   arguments_assert(arguments, 1);
-  let output = await function_curryify_generic_open(
+  let output = await function_curryify_generic(
     f_name,
     function_curryify_right_name,
     function_curryify_right_args_get,
