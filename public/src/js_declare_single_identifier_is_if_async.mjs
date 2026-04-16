90fe20d9be900344465602a6f859c8d2b70f8778
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:09:52 -0400

    function_auto_path ../love/public/src/js_declare_single_identifier_is_if_async.mjs

--- public/src/js_declare_single_identifier_is_if_async.mjs
+++ public/src/js_declare_single_identifier_is_if_async.mjs
@@ -1,15 +1,15 @@
+import { js_identifier_is_if_async } from "../../../love/public/src/js_identifier_is_if_async.mjs";
 import { js_declare_init_get } from "../../../love/public/src/js_declare_init_get.mjs";
 import { js_declare_single_is_if } from "../../../love/public/src/js_declare_single_is_if.mjs";
-import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_if.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_declare_single_identifier_is_if_async(node, lambda$d) {
-  function lambda(d) {
+  async function lambda(d) {
     let id = property_get(d, "id");
     function lambda3() {
       let init = js_declare_init_get(d);
       lambda$d(init, id);
     }
-    js_identifier_is_if(id, lambda3);
+    await js_identifier_is_if_async(id, lambda3);
   }
   js_declare_single_is_if(node, lambda);
 }
