a3f370d8c303ee7dcb60770a4e3b078c9e077b14
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:13:31 -0400

    function_auto_path ../love/public/src/function_copy_generic_args_async.mjs

--- web/public/src/function_copy_generic_args_async.mjs
+++ web/public/src/function_copy_generic_args_async.mjs
@@ -1,4 +1,4 @@
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
@@ -9,7 +9,7 @@ export async function function_copy_generic_args_async(
 ) {
   arguments_assert(arguments, 2);
   plugin_fn = function_name_combine.name;
-  let combined = text_combine(left, right);
+  let combined = text_combine_multiple([f_name_old_args_comma, ",", "async"]);
   let f_name_new = null;
   let f_name_old = null;
   ({ f_name_new, f_name_old } = await function_name_new_get_args(
