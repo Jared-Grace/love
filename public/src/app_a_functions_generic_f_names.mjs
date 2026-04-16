761b8c753ba49773b569015cbfab263e49f75cc3
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:18:29 -0400

    function_auto_path ../love/public/src/list_filter_text_includes.mjs

--- public/src/app_a_functions_generic_f_names.mjs
+++ public/src/app_a_functions_generic_f_names.mjs
@@ -1,6 +1,6 @@
 import { function_path_to_name } from "../../../love/public/src/function_path_to_name.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_filter_includes } from "../../../love/public/src/list_filter_includes.mjs";
+import { list_filter_text_includes } from "../../../love/public/src/list_filter_text_includes.mjs";
 import { text_pad } from "../../../love/public/src/text_pad.mjs";
 import { functions_path } from "../../../love/public/src/functions_path.mjs";
 import { list_filter_ends_with } from "../../../love/public/src/list_filter_ends_with.mjs";
@@ -12,7 +12,7 @@ export async function app_a_functions_generic_f_names() {
   let filtered3 = list_filter_ends_with(mapped, suffix);
   let joined = functions_path();
   let padded = text_pad(joined, "/");
-  let filtered4 = list_filter_includes(filtered3, padded);
+  let filtered4 = list_filter_text_includes(filtered3, padded);
   let f_names = list_map(filtered4, function_path_to_name);
   return f_names;
 }
