18901f62a8f657dbf6e79dfd0685d4715105f01a
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:51:22 -0400

    function_auto_path ../love/public/src/at_least.mjs

--- public/src/greater_than_or_equal_assert.mjs
+++ public/src/greater_than_or_equal_assert.mjs
@@ -1,7 +1,7 @@
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
-import { greater_than_or_equal } from "../../../love/public/src/greater_than_or_equal.mjs";
+import { at_least } from "../../../love/public/src/at_least.mjs";
 export function greater_than_or_equal_assert(left, right) {
-  let l = greater_than_or_equal(left, right);
+  let l = at_least(left, right);
   function lambda() {
     let r = {
       left,
