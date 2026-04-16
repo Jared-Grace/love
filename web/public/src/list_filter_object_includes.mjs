1b355128429f04f4b81c5849ac1aa189128a4429
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:28:14 -0400

    function_auto_path ../love/public/src/list_filter_object_includes.mjs

--- web/public/src/list_filter_object_includes.mjs
+++ web/public/src/list_filter_object_includes.mjs
@@ -1,7 +1,7 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { object_includes_curried_right } from "../../../love/public/src/object_includes_curried_right.mjs";
 export function list_filter_object_includes(list, object) {
-  let lambda17 = object_includes_curried_right(object);
-  let npcs_matched = list_filter(list, lambda17);
+  let lambda = object_includes_curried_right(object);
+  let npcs_matched = list_filter(list, lambda);
   return npcs_matched;
 }
