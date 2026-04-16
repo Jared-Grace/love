74a5e5b72a6174b1899d510c55426d359bfd1bff
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:20:30 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,8 +1,10 @@
+import { function_auto } from "../../../love/public/src/function_auto.mjs";
 import { function_delete_if_exists_multiple } from "../../../love/public/src/function_delete_if_exists_multiple.mjs";
 import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
 import { function_transform_multiple } from "../../../love/public/src/function_transform_multiple.mjs";
 export async function sandbox_3() {
   let fns = ["list_filter_object_includes"];
   await function_delete_if_exists_multiple(fns);
+  let result = await function_auto(f_name);
   await function_transform_multiple("c", app_g_click.name);
 }
