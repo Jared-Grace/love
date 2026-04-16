4f5128131c7a591125c2303c82d7ccbf198c2ddf
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:35:00 -0400

    function_auto_path ../love/public/src/g_coordinates_adjascent.mjs

--- public/src/g_coordinates_adjascent.mjs
+++ public/src/g_coordinates_adjascent.mjs
@@ -3,7 +3,7 @@ import { g_distance_1_curried } from "../../../love/public/src/g_distance_1_curr
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function g_coordinates_adjascent(g, clicked_coordinates) {
   let coordinates_map_all = property_get(g, "coordinates");
-  let lambda18 = g_distance_1_curried(clicked_coordinates);
-  let adjascent = list_filter(coordinates_map_all, lambda18);
+  let lambda = g_distance_1_curried(clicked_coordinates);
+  let adjascent = list_filter(coordinates_map_all, lambda);
   return adjascent;
 }
