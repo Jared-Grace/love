8580c5cdd40c667ad317ced5724f3fd50d156f16
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 18:02:00 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

--- public/src/app_g_click.mjs
+++ public/src/app_g_click.mjs
@@ -1,14 +1,11 @@
+import { app_g_click_map } from "../../../love/public/src/app_g_click_map.mjs";
 import { app_g_menu_new } from "../../../love/public/src/app_g_menu_new.mjs";
-import { app_g_player_coordinates_update_move } from "../../../love/public/src/app_g_player_coordinates_update_move.mjs";
-import { list_filter_object_includes } from "../../../love/public/src/list_filter_object_includes.mjs";
-import { app_g_click_npc_if } from "../../../love/public/src/app_g_click_npc_if.mjs";
 import { g_distance_0 } from "../../../love/public/src/g_distance_0.mjs";
 import { app_g_event_target_closest_tile_coordinates } from "../../../love/public/src/app_g_event_target_closest_tile_coordinates.mjs";
 import { g_tutorials_each_remove_try } from "../../../love/public/src/g_tutorials_each_remove_try.mjs";
 import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
-import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 export async function app_g_click(e, div_map, player_img_c, refresh) {
   let clicked_coordinates = app_g_event_target_closest_tile_coordinates(e);
   g_tutorials_each_remove_try();
@@ -16,18 +13,15 @@ export async function app_g_click(e, div_map, player_img_c, refresh) {
   let player = property_get(g, "player");
   const clicked_on_player = g_distance_0(player, clicked_coordinates);
   if (clicked_on_player) {
-    app_g_menu_new(div_map);
+    await app_g_menu_new(div_map);
   } else {
-    let npcs = property_get(g, "npcs");
-    let npcs_matched = list_filter_object_includes(npcs, clicked_coordinates);
-    let npc_clicked = list_empty_not_is(npcs_matched);
-    await app_g_player_coordinates_update_move(
-      npc_clicked,
+    await app_g_click_map(
+      g,
       clicked_coordinates,
       player_img_c,
       div_map,
+      refresh,
     );
-    await app_g_click_npc_if(npc_clicked, div_map, npcs_matched, refresh);
   }
   await app_g_player_save(player);
 }
