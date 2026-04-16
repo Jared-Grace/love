bb9466c533b66a6f407a3a83613f9f9642bde4ba
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:39:09 -0400

    frp app_g_game_save_get_player app_g_player_get

--- public/src/app_g_player_img.mjs
+++ public/src/app_g_player_img.mjs
@@ -1,8 +1,8 @@
-import { app_g_game_save_get_player } from "../../../love/public/src/app_g_game_save_get_player.mjs";
+import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { app_g_player_style } from "../../../love/public/src/app_g_player_style.mjs";
 import { g_character_img } from "../../../love/public/src/g_character_img.mjs";
 export async function app_g_player_img(parent) {
-  let player = await app_g_game_save_get_player();
+  let player = await app_g_player_get();
   let player_img_c = g_character_img(parent, player);
   app_g_player_style(player_img_c);
   return player_img_c;
