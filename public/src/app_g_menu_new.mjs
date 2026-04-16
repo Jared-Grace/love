b52715c467cd1b22a44ae713431d650cb69a6229
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 18:00:09 -0400

    pd app_g_menu_new player

--- public/src/app_g_menu_new.mjs
+++ public/src/app_g_menu_new.mjs
@@ -1,6 +1,6 @@
 import { app_g_menu } from "../../../love/public/src/app_g_menu.mjs";
 import { app_g_overlay } from "../../../love/public/src/app_g_overlay.mjs";
-export function app_g_menu_new(div_map, player) {
+export function app_g_menu_new(div_map) {
   let overlay = app_g_overlay(div_map);
   app_g_menu(overlay, player);
 }
