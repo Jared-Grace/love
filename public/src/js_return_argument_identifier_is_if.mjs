f8f3c424a468a67f8e711632bb829f201834a753
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 22:40:32 -0400

    function_auto_path ../love/public/src/js_return_argument_identifier_is_if.mjs

--- public/src/js_return_argument_identifier_is_if.mjs
+++ public/src/js_return_argument_identifier_is_if.mjs
@@ -2,6 +2,7 @@ import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_i
 import { js_return_argument_get } from "../../../love/public/src/js_return_argument_get.mjs";
 export function js_return_argument_identifier_is_if(node, lambda) {
   let argument = js_return_argument_get(node);
-  js_identifier_is_if(argument, lambda);
+  function lambda2() {}
+  js_identifier_is_if(argument, lambda2);
   return argument;
 }
