59e951c792ba8b9f6917740509c11f7fc91c9187
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 13:57:44 -0400

    function_auto_path ../love/public/src/function_run_git.mjs

--- web/public/src/function_run_git.mjs
+++ web/public/src/function_run_git.mjs
@@ -1,8 +1,11 @@
+import { promise_later } from "../../../love/public/src/promise_later.mjs";
 import { git_acp_call_repos } from "../../../love/public/src/git_acp_call_repos.mjs";
 import { function_run_log } from "../../../love/public/src/function_run_log.mjs";
 Error.stackTraceLimit = Infinity;
 export async function function_run_git(f_name, args) {
   let result = await function_run_log(f_name, args);
+  function lambda2() {}
+  promise_later(lambda2);
   await git_acp_call_repos(f_name, args);
   return result;
 }
