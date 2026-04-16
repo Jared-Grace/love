ff6d3dfb05d1760a24413b5859f1d86dca57da91
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:15:00 -0400

    function_auto_path ../love/public/src/function_copy_generic_args_async.mjs

--- public/src/function_copy_generic_args_async.mjs
+++ public/src/function_copy_generic_args_async.mjs
@@ -1,3 +1,4 @@
+import { function_copy_generic_args } from "../../../love/public/src/function_copy_generic_args.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
@@ -14,6 +15,7 @@ export async function function_copy_generic_args_async(
     ",",
     "async",
   ]);
+  let r2 = await function_copy_generic_args(plugin_fn2, f_name_old_args_comma2);
   let f_name_new = null;
   let f_name_old = null;
   ({ f_name_new, f_name_old } = await function_name_new_get_args(
