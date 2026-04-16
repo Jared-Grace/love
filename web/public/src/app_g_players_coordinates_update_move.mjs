8c6dcdd642c465b5d3444a9721460d7ebb081ef9
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:59:07 -0400

    function_auto_path ../love/public/src/app_g_player_coordinates_update.mjs

--- web/public/src/app_g_players_coordinates_update_move.mjs
+++ web/public/src/app_g_players_coordinates_update_move.mjs
@@ -1,12 +1,12 @@
 import { app_g_player_move } from "../../../love/public/src/app_g_player_move.mjs";
-import { app_g_players_coordinates_update } from "../../../love/public/src/app_g_players_coordinates_update.mjs";
+import { app_g_player_coordinates_update } from "../../../love/public/src/app_g_player_coordinates_update.mjs";
 export async function app_g_players_coordinates_update_move(
   npc_clicked,
   clicked_coordinates,
   player_img_c,
   div_map,
 ) {
-  let coordinates_move_to = await app_g_players_coordinates_update(
+  let coordinates_move_to = await app_g_player_coordinates_update(
     npc_clicked,
     clicked_coordinates,
   );
