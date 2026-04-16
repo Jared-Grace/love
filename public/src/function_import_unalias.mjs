6b933cb201a69cd22246b0bb13a26f6e1ea6e6df
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:25:32 -0400

    function_auto_path ../love/public/src/function_import_unalias.mjs

--- public/src/function_import_unalias.mjs
+++ public/src/function_import_unalias.mjs
@@ -1,11 +1,7 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
 export async function function_import_unalias(f_name) {
-  log(function_import_unalias.name, {
-    f_name,
-  });
   let v = await function_name_unalias(f_name);
   let unaliased = property_get(v, "unaliased");
   const imported_fn = await function_import(unaliased);
