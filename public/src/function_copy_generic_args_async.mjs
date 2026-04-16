78cac62a50fe399996bf151f4c6f279792bef519
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:17:04 -0400

    function_auto_path ../love/public/src/function_copy_generic_args_async.mjs

--- public/src/function_copy_generic_args_async.mjs
+++ public/src/function_copy_generic_args_async.mjs
@@ -2,12 +2,9 @@ import { function_copy_generic_args } from "../../../love/public/src/function_co
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
-export async function function_copy_generic_args_async(
-  plugin_fn,
-  f_name_old_args_comma,
-) {
-  arguments_assert(arguments, 2);
-  plugin_fn = function_name_combine.name;
+export async function function_copy_generic_args_async(f_name_old_args_comma) {
+  arguments_assert(arguments, 1);
+  let plugin_fn = function_name_combine.name;
   f_name_old_args_comma = text_combine_multiple([
     f_name_old_args_comma,
     ",",
