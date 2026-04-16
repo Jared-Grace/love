dbd8556fedc653d3707ca3af27cee5bb9bba88e7
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:02:41 -0400

    function_auto_path ../love/public/src/function_alias_replace.mjs

--- web/public/src/function_alias_replace.mjs
+++ web/public/src/function_alias_replace.mjs
@@ -6,9 +6,9 @@ import { function_alias_generic } from "../../../love/public/src/function_alias_
 import { property_delete } from "../../../love/public/src/property_delete.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 export async function function_alias_replace(alias_old, f_name) {
-  f_name = await function_name_unalias_only(f_name);
   const items = [alias_old, f_name];
   text_is_assert_multiple(items);
+  f_name = await function_name_unalias_only(f_name);
   function lambda(a) {
     let aliases = property_get(a, "aliases");
     let exists = property_get(a, "exists");
