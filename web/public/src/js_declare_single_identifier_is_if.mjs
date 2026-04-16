a65471583a2aba27f1f9cdf062a67e78b3d30908
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 22:42:30 -0400

    function_auto_path ../love/public/src/js_declare_single_identifier_is_if.mjs

--- web/public/src/js_declare_single_identifier_is_if.mjs
+++ web/public/src/js_declare_single_identifier_is_if.mjs
@@ -7,7 +7,7 @@ export function js_declare_single_identifier_is_if(node, lambda$d) {
     let id = property_get(d, "id");
     function lambda3() {
       let init = js_declare_init_get(d);
-      lambda$d(init.id);
+      lambda$d(init, id);
     }
     js_identifier_is_if(id, lambda3);
   }
