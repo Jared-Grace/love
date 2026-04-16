5b89603f1c75bbdd32714edc75ad8556aae5d390
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:37:21 -0400

    function_auto_path ../love/public/src/g_coordinates_clicked_adjascent_nearest_player.mjs

--- public/src/g_coordinates_clicked_adjascent_nearest_player.mjs
+++ public/src/g_coordinates_clicked_adjascent_nearest_player.mjs
@@ -1,10 +1,14 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { list_shuffle_sort_number_mapper_first } from "../../../love/public/src/list_shuffle_sort_number_mapper_first.mjs";
 import { g_distance_curried } from "../../../love/public/src/g_distance_curried.mjs";
 import { g_coordinates_adjascent } from "../../../love/public/src/g_coordinates_adjascent.mjs";
-export function g_coordinates_clicked_adjascent_nearest_player(
+export async function g_coordinates_clicked_adjascent_nearest_player(
   clicked_coordinates,
   coordinates_move_to,
 ) {
+  let g = await app_g_game_save_get();
+  let player = property_get(g, "player");
   let nearby = g_coordinates_adjascent(g, clicked_coordinates);
   let lambda19 = g_distance_curried(player);
   coordinates_move_to = list_shuffle_sort_number_mapper_first(nearby, lambda19);
