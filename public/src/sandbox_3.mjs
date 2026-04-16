81e00c49f950deba1b87138b7d2defbf6cac9c14
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 14:26:45 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -2,7 +2,7 @@ import { app_g_click } from "../../../love/public/src/app_g_click.mjs";
 import { function_transform_multiple } from "../../../love/public/src/function_transform_multiple.mjs";
 import { function_delete_if_exists } from "../../../love/public/src/function_delete_if_exists.mjs";
 export async function sandbox_3() {
-  let fns = ["g_distance_curried"];
+  let fns = ["g_distance_curried", "object_includes_curried_right"];
   await function_delete_if_exists();
   await function_transform_multiple("c", app_g_click.name);
 }
