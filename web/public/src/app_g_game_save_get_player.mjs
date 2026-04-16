f703cb7ab8651fd5ead9195bf77ff5880cf64ec2
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:39:09 -0400

    frp app_g_game_save_get_player app_g_player_get

--- web/public/src/app_g_game_save_get_player.mjs
+++
@@ -1,7 +0,0 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
-export async function app_g_game_save_get_player() {
-  let g = await app_g_game_save_get();
-  let player = property_get(g, "player");
-  return player;
-}
