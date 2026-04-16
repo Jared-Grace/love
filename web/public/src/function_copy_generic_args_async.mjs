dd54f66538c51ed37bafc633a9dcd40187b787fc
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:11:39 -0400

    c c c,async

---
+++ web/public/src/function_copy_generic_args_async.mjs
@@ -0,0 +1,17 @@
+import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
+import { function_name_new_get_args } from "../../../love/public/src/function_name_new_get_args.mjs";
+export async function function_copy_generic_args_async(
+  plugin_fn,
+  f_name_old_args_comma,
+) {
+  arguments_assert(arguments, 2);
+  let f_name_new = null;
+  let f_name_old = null;
+  ({ f_name_new, f_name_old } = await function_name_new_get_args(
+    plugin_fn,
+    f_name_old_args_comma,
+  ));
+  let r = await function_copy_open(f_name_old, f_name_new);
+  return r;
+}
