c74d5cce3587464163a1ffc592580c86dd285858
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:14:14 -0400

    function_auto_path ../love/public/src/js_declare_single_is_if_async.mjs

--- public/src/js_declare_single_is_if_async.mjs
+++ public/src/js_declare_single_is_if_async.mjs
@@ -1,10 +1,9 @@
 import { js_declare_is_if } from "../../../love/public/src/js_declare_is_if.mjs";
-import { list_single_if } from "../../../love/public/src/list_single_if.mjs";
 import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
 export function js_declare_single_is_if_async(previous, lambda) {
   function lambda3() {
     let declarations = js_declare_declarations_get(previous);
-    list_single_if(declarations, lambda);
+    list_single_if_async(declarations, lambda);
   }
   js_declare_is_if(previous, lambda3);
 }
