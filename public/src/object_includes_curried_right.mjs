b6a1113743d6fb212130817887767e0776e6ac7e
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:24:45 -0400

    p

--- public/src/object_includes_curried_right.mjs
+++
@@ -1,8 +0,0 @@
-import { object_includes } from "../../../love/public/src/object_includes.mjs";
-export function object_includes_curried_right(clicked_coordinates) {
-  let r = function object_includes_curried_right_result(npc) {
-    let e = object_includes(npc, clicked_coordinates);
-    return e;
-  };
-  return r;
-}
