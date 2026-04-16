d473090a2728e4f53f8eb580f17f86b0ebe0faac
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:18:23 -0400

    function_auto_path ../love/public/src/js_declare_single_identifier_is_if_async.mjs

--- public/src/js_declare_single_identifier_is_if_async.mjs
+++ public/src/js_declare_single_identifier_is_if_async.mjs
@@ -1,8 +1,8 @@
+import { js_declare_single_is_if_async } from "../../../love/public/src/js_declare_single_is_if_async.mjs";
 import { js_identifier_is_if_async } from "../../../love/public/src/js_identifier_is_if_async.mjs";
 import { js_declare_init_get } from "../../../love/public/src/js_declare_init_get.mjs";
-import { js_declare_single_is_if } from "../../../love/public/src/js_declare_single_is_if.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_declare_single_identifier_is_if_async(node, lambda$d) {
+export async function js_declare_single_identifier_is_if_async(node, lambda$d) {
   async function lambda(d) {
     let id = property_get(d, "id");
     function lambda3() {
@@ -11,5 +11,5 @@ export function js_declare_single_identifier_is_if_async(node, lambda$d) {
     }
     await js_identifier_is_if_async(id, lambda3);
   }
-  js_declare_single_is_if(node, lambda);
+  await js_declare_single_is_if_async(node, lambda);
 }
