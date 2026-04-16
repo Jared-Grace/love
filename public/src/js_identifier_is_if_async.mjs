d9d11c8bc205d2219092abe0a04c09f693456ae1
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:00:08 -0400

    function_auto_path ../love/public/src/js_identifier_is_if_async.mjs

--- public/src/js_identifier_is_if_async.mjs
+++ public/src/js_identifier_is_if_async.mjs
@@ -1,4 +1,6 @@
 import { js_node_type_is_if_async } from "../../../love/public/src/js_node_type_is_if_async.mjs";
-export function js_identifier_is_if_async(node, lambda) {
+export async function js_identifier_is_if_async(node, lambda) {
+  async function lambda3() {}
+  await js_node_type_is_if_async(node2, type, lambda3);
   js_node_type_is_if_async(node, "Identifier", lambda);
 }
