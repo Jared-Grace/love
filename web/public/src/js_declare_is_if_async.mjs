cbcb6736cf198763cadd714b293fcd2749bc80bb
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:18:00 -0400

    function_auto_path ../love/public/src/js_declare_is_if_async.mjs

--- web/public/src/js_declare_is_if_async.mjs
+++ web/public/src/js_declare_is_if_async.mjs
@@ -1,4 +1,4 @@
-import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
-export function js_declare_is_if_async(node, lambda) {
-  js_node_type_is_if(node, "VariableDeclaration", lambda);
+import { js_node_type_is_if_async } from "../../../love/public/src/js_node_type_is_if_async.mjs";
+export async function js_declare_is_if_async(node, lambda) {
+  await js_node_type_is_if_async(node, "VariableDeclaration", lambda);
 }
