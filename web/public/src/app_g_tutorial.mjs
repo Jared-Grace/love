580bc1f403e95180f188bb90cc26c5f4fef814eb
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 18:03:05 -0400

    function_auto_path ../love/public/src/app_g_tutorial.mjs

--- web/public/src/app_g_tutorial.mjs
+++ web/public/src/app_g_tutorial.mjs
@@ -1,7 +1,6 @@
+import { app_g_tutorial_style } from "../../../love/public/src/app_g_tutorial_style.mjs";
 import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { g_icon } from "../../../love/public/src/g_icon.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-import { g_img_square_size_css } from "../../../love/public/src/g_img_square_size_css.mjs";
 import { app_g_main } from "../../../love/public/src/app_g_main.mjs";
 import { global_function_property_set } from "../../../love/public/src/global_function_property_set.mjs";
 import { not } from "../../../love/public/src/not.mjs";
@@ -17,14 +16,6 @@ export async function app_g_tutorial(
   if (not(already)) {
     let tutorial = g_icon(div_map, player, emoji);
     global_function_property_set(app_g_main, tutorial_property, tutorial);
-    const square_size = `calc(` + g_img_square_size_css() + `*.4)`;
-    html_style_assign(tutorial, {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      textAlign: "center",
-      fontSize: square_size,
-      animation: "upDown 1.25s infinite ease-in-out alternate",
-    });
+    app_g_tutorial_style(tutorial);
   }
 }
