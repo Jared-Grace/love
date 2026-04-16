ce4ab5afb6371eaf08df176caca1e33b6285749f
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 22:47:17 -0400

    function_auto_path ../love/public/src/js_identifiers_names_equal.mjs

--- public/src/js_identifiers_names_equal.mjs
+++ public/src/js_identifiers_names_equal.mjs
@@ -1,6 +1,6 @@
 import { equal_by } from "../../../love/public/src/equal_by.mjs";
 import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 export function js_identifiers_names_equal(a, b) {
-  let ne = equal_by(a, b, js_identifier_name);
-  return ne;
+  let eq = equal_by(a, b, js_identifier_name);
+  return eq;
 }
