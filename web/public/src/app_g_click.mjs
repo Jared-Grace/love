a86e0f12076306b1439be6a500f76af6118848dd
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:47:39 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

--- web/public/src/app_g_click.mjs
+++ web/public/src/app_g_click.mjs
@@ -1,8 +1,8 @@
+import { app_g_player_move_animate } from "../../../love/public/src/app_g_player_move_animate.mjs";
 import { g_coordinates_clicked_adjascent_nearest_player } from "../../../love/public/src/g_coordinates_clicked_adjascent_nearest_player.mjs";
 import { list_filter_object_includes } from "../../../love/public/src/list_filter_object_includes.mjs";
 import { greater_than_or_equal_1 } from "../../../love/public/src/greater_than_or_equal_1.mjs";
 import { app_g_click_npc_if } from "../../../love/public/src/app_g_click_npc_if.mjs";
-import { g_img_square_style_position_object_later } from "../../../love/public/src/g_img_square_style_position_object_later.mjs";
 import { g_distance_0 } from "../../../love/public/src/g_distance_0.mjs";
 import { app_g_event_target_closest_tile_coordinates } from "../../../love/public/src/app_g_event_target_closest_tile_coordinates.mjs";
 import { g_tutorials_each_remove_try } from "../../../love/public/src/g_tutorials_each_remove_try.mjs";
@@ -11,7 +11,6 @@ import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_ge
 import { html_scroll_center_container } from "../../../love/public/src/html_scroll_center_container.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
-import { html_on_transitionend } from "../../../love/public/src/html_on_transitionend.mjs";
 import { object_assign } from "../../../love/public/src/object_assign.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { app_g_menu } from "../../../love/public/src/app_g_menu.mjs";
@@ -44,16 +43,7 @@ export async function app_g_click(e, div_map, player_img_c, refresh) {
     object_assign(player, coordinates_move_to);
     const away = greater_than_or_equal_1(distance);
     if (away) {
-      let properties = ["left", "top"];
-      let on_transition_begin = g_img_square_style_position_object_later(
-        player,
-        player_img_c,
-      );
-      await html_on_transitionend(
-        properties,
-        player_img_c,
-        on_transition_begin,
-      );
+      await app_g_player_move_animate(player, player_img_c);
     }
     let container = app_g_div_map_container_get(div_map);
     await html_scroll_center_container(player_img_c, container);
