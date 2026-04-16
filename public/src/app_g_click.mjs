a3437239ce5279d3074b92df2a67a5e07d0ab2a9
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:58:00 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

--- public/src/app_g_click.mjs
+++ public/src/app_g_click.mjs
@@ -1,3 +1,4 @@
+import { app_g_menu_new } from "../../../love/public/src/app_g_menu_new.mjs";
 import { app_g_players_coordinates_update_move } from "../../../love/public/src/app_g_players_coordinates_update_move.mjs";
 import { list_filter_object_includes } from "../../../love/public/src/list_filter_object_includes.mjs";
 import { app_g_click_npc_if } from "../../../love/public/src/app_g_click_npc_if.mjs";
@@ -8,8 +9,6 @@ import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_ge
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
-import { app_g_menu } from "../../../love/public/src/app_g_menu.mjs";
-import { app_g_overlay } from "../../../love/public/src/app_g_overlay.mjs";
 export async function app_g_click(e, div_map, player_img_c, refresh) {
   let clicked_coordinates = app_g_event_target_closest_tile_coordinates(e);
   g_tutorials_each_remove_try();
@@ -17,8 +16,7 @@ export async function app_g_click(e, div_map, player_img_c, refresh) {
   let player = property_get(g, "player");
   const clicked_on_player = g_distance_0(player, clicked_coordinates);
   if (clicked_on_player) {
-    let overlay = app_g_overlay(div_map);
-    app_g_menu(overlay, player);
+    app_g_menu_new(div_map, player);
   } else {
     let npcs = property_get(g, "npcs");
     let npcs_matched = list_filter_object_includes(npcs, clicked_coordinates);
