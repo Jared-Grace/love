873ac4c742120065e284d03bae9bb961ee538413
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:18:29 -0400

    function_auto_path ../love/public/src/list_filter_text_includes.mjs

--- web/public/src/list_filter_text_includes.mjs
+++ web/public/src/list_filter_text_includes.mjs
@@ -1,6 +1,6 @@
 import { text_includes_curried_right } from "../../../love/public/src/text_includes_curried_right.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-export function list_filter_includes(mapped, part) {
+export function list_filter_text_includes(mapped, part) {
   let lambda = text_includes_curried_right(part);
   let filtered = list_filter(mapped, lambda);
   return filtered;
