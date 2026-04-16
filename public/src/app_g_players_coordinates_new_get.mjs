14306e14e5c08e41bcc09ce71bec93bc86a7bb0b
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:55:41 -0400

    function_auto_path ../love/public/src/app_g_players_coordinates_new_get.mjs

--- public/src/app_g_players_coordinates_new_get.mjs
+++ public/src/app_g_players_coordinates_new_get.mjs
@@ -3,14 +3,12 @@ export async function app_g_players_coordinates_new_get(
   npc_clicked,
   clicked_coordinates,
 ) {
-  let coordinates_move_to = null;
+  let coordinates_move_to = clicked_coordinates;
   if (npc_clicked) {
     coordinates_move_to = await g_coordinates_clicked_adjascent_nearest_player(
       clicked_coordinates,
       coordinates_move_to,
     );
-  } else {
-    coordinates_move_to = clicked_coordinates;
   }
   return coordinates_move_to;
 }
