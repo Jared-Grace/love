9228c467b7277bdaf64da3d6c71b3e1656c11fad
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:23:00 -0400

    function_auto_path ../love/public/src/function_import_unalias.mjs

--- web/public/src/function_import_unalias.mjs
+++ web/public/src/function_import_unalias.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { function_name_unalias } from "../../../love/public/src/function_name_unalias.mjs";
@@ -6,4 +7,5 @@ export async function function_import_unalias(f_name) {
   let unaliased = property_get(v, "unaliased");
   const imported_fn = await function_import(unaliased);
   return imported_fn;
+  log(function_import_unalias.name, {});
 }
