5aaa998691dee0a1035f514906d7e069549c1876
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:08:33 -0400

    function_auto_path ../love/public/src/js_call_function_if.mjs

--- public/src/js_call_function_if.mjs
+++ public/src/js_call_function_if.mjs
@@ -1,16 +1,14 @@
 import { js_identifier_is } from "../../../love/public/src/js_identifier_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { functions_names_includes } from "../../../love/public/src/functions_names_includes.mjs";
-import { js_node_type_is } from "../../../love/public/src/js_node_type_is.mjs";
 export async function js_call_function_if(node, lambda$name) {
   "calls a lambda if the function name is a valid function";
   let callee = property_get(node, "callee");
-  if (js_node_type_is(callee)) {
+  if (js_identifier_is(callee)) {
     let name = property_get(callee, "name");
     const valid = await functions_names_includes(name);
     if (valid) {
       await lambda$name(name);
-      let ii = js_identifier_is(node2);
     }
   }
 }
