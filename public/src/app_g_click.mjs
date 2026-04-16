de97053f9d0734b48656f706151fe4802bff5ad3
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:52:28 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

--- public/src/app_g_click.mjs
+++ public/src/app_g_click.mjs
@@ -1,6 +1,4 @@
-import { g_distance_at_least_1 } from "../../../love/public/src/g_distance_at_least_1.mjs";
-import { app_g_player_center } from "../../../love/public/src/app_g_player_center.mjs";
-import { app_g_player_move_animate } from "../../../love/public/src/app_g_player_move_animate.mjs";
+import { app_g_player_move } from "../../../love/public/src/app_g_player_move.mjs";
 import { g_coordinates_clicked_adjascent_nearest_player } from "../../../love/public/src/g_coordinates_clicked_adjascent_nearest_player.mjs";
 import { list_filter_object_includes } from "../../../love/public/src/list_filter_object_includes.mjs";
 import { app_g_click_npc_if } from "../../../love/public/src/app_g_click_npc_if.mjs";
@@ -38,11 +36,7 @@ export async function app_g_click(e, div_map, player_img_c, refresh) {
       coordinates_move_to = clicked_coordinates;
     }
     object_assign(player, coordinates_move_to);
-    const away = g_distance_at_least_1(player, coordinates_move_to);
-    if (away) {
-      await app_g_player_move_animate(player, player_img_c);
-    }
-    await app_g_player_center(div_map, player_img_c);
+    await app_g_player_move(player, coordinates_move_to, player_img_c, div_map);
     await app_g_click_npc_if(npc_clicked, div_map, npcs_matched, refresh);
   }
   await app_g_player_save(player);
