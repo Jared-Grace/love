d8bc699a45bc56e50011f2eace88a609795a0436
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:22:37 -0400

    function_auto_path ../love/public/src/g_distance_1_curried.mjs

--- public/src/g_distance_1_curried.mjs
+++ public/src/g_distance_1_curried.mjs
@@ -1,6 +1,8 @@
 import { g_distance_1 } from "../../../love/public/src/g_distance_1.mjs";
 export function g_distance_1_curried(left) {
-  return function g_distance_1_curried_result(right) {
-    return g_distance_1(left, right);
+  let r = function g_distance_1_curried_result(right) {
+    let zi = g_distance_1(left, right);
+    return zi;
   };
+  return r;
 }
