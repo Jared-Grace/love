051721176d3aa88911be1d345fbb332d4e6d829f
JESUS rose to life <username@users.noreply.github.com>
2026-04-15 23:10:57 -0400

    pp 4

---
+++ public/src/js_declare_single_is_if_async.mjs
@@ -0,0 +1,10 @@
+import { js_declare_is_if } from "../../../love/public/src/js_declare_is_if.mjs";
+import { list_single_if } from "../../../love/public/src/list_single_if.mjs";
+import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
+export function js_declare_single_is_if_async(previous, lambda) {
+  function lambda3() {
+    let declarations = js_declare_declarations_get(previous);
+    list_single_if(declarations, lambda);
+  }
+  js_declare_is_if(previous, lambda3);
+}
