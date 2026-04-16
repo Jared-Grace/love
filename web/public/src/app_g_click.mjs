262a854ee36bde5c3d008d894e0089d7035fd056
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:30:36 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

--- web/public/src/app_g_click.mjs
+++ web/public/src/app_g_click.mjs
@@ -36,8 +36,8 @@ export async function app_g_click(e, div_map, player_img_c, refresh) {
     let coordinates_move_to = null;
     if (npc_clicked) {
       ("find the coordinates next to the npc that is nearest to the player");
-      let lambda18 = g_distance_1_curried(clicked_coordinates);
       let coordinates = property_get(g, "coordinates");
+      let lambda18 = g_distance_1_curried(clicked_coordinates);
       let filtered3 = list_filter(coordinates, lambda18);
       let lambda19 = g_distance_curried(player);
       coordinates_move_to = list_shuffle_sort_number_mapper_first(
