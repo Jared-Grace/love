8f2758ba09160c413d4a403b04ea5b99daac39a8
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 15:30:17 -0400

    function_auto_path ../love/public/src/text_includes_curried_right.mjs

--- public/src/text_includes_curried_right.mjs
+++ public/src/text_includes_curried_right.mjs
@@ -1,6 +1,8 @@
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
 export function text_includes_curried_right(part) {
-  return function text_includes_curried_right_result(input) {
-    return text_includes(input, part);
+  let r = function text_includes_curried_right_result(input) {
+    let i = text_includes(input, part);
+    return i;
   };
+  return r;
 }
