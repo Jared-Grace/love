497a892bf4f5abf19544c3690ae41415b2e9d181
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:26:55 -0400

    function_auto_path ../love/public/src/object_includes_curried_right.mjs

--- public/src/object_includes_curried_right.mjs
+++ public/src/object_includes_curried_right.mjs
@@ -1,7 +1,7 @@
 import { object_includes } from "../../../love/public/src/object_includes.mjs";
-export function object_includes_curried_right(clicked_coordinates) {
-  let r = function object_includes_curried_right_result(npc) {
-    let e = object_includes(npc, clicked_coordinates);
+export function object_includes_curried_right(item) {
+  let r = function object_includes_curried_right_result(object) {
+    let e = object_includes(object, item);
     return e;
   };
   return r;
