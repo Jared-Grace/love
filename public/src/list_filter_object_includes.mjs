7f91ec0fe76dce2b67d4aee61dd82afa87884e43
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:26:30 -0400

    pb list_filter_object_includes

--- public/src/list_filter_object_includes.mjs
+++ public/src/list_filter_object_includes.mjs
@@ -1,6 +1,6 @@
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { object_includes_curried_right } from "../../../love/public/src/object_includes_curried_right.mjs";
-export function list_filter_object_includes(object, list) {
+export function list_filter_object_includes(list, object) {
   let lambda17 = object_includes_curried_right(object);
   let npcs_matched = list_filter(list, lambda17);
   return npcs_matched;
