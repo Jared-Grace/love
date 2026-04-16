4ff49e4b1a40ecb677011197681ae4dcf2c5131d
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 18:00:29 -0400

    function_auto_path ../love/public/src/app_g_menu_new.mjs

--- public/src/app_g_menu_new.mjs
+++ public/src/app_g_menu_new.mjs
@@ -2,7 +2,7 @@ import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs"
 import { app_g_menu } from "../../../love/public/src/app_g_menu.mjs";
 import { app_g_overlay } from "../../../love/public/src/app_g_overlay.mjs";
 export async function app_g_menu_new(div_map) {
-  let player2 = await app_g_player_get();
+  let player = await app_g_player_get();
   let overlay = app_g_overlay(div_map);
   app_g_menu(overlay, player);
 }
