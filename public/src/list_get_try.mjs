5d67823904cd547c9f2520f44fd65e58a7e51018
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 14:59:01 -0400

    pb list_get_try

--- public/src/list_get_try.mjs
+++ public/src/list_get_try.mjs
@@ -1,5 +1,5 @@
 import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
-export function list_get_try(index, list) {
+export function list_get_try(list, index) {
   integer_is_assert(index);
   let item = list[index];
   return item;
