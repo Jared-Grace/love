9b903a1bad9f0d1562fd2e81a8a07edaeccafe26
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 14:59:33 -0400

    function_auto_path ../love/public/src/list_first_try.mjs

--- web/public/src/list_first_try.mjs
+++ web/public/src/list_first_try.mjs
@@ -1,6 +1,6 @@
-import { list_get } from "../../../love/public/src/list_get.mjs";
+import { list_get_try } from "../../../love/public/src/list_get_try.mjs";
 export function list_first_try(list) {
   const index = 0;
-  const first = list_get(list, index);
+  const first = list_get_try(list, index);
   return first;
 }
