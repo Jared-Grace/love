acd92e63162a395711cc1becf87fa30f5993f4c2
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 14:56:36 -0400

    function_auto_path ../love/public/src/integer_odd_is.mjs

--- public/src/integer_odd_is.mjs
+++ public/src/integer_odd_is.mjs
@@ -1,6 +1,6 @@
 import { integer_evenness } from "../../../love/public/src/integer_evenness.mjs";
-export function integer_odd_is(index) {
-  let r2 = integer_evenness(index);
-  let r4 = r2 === 1;
-  return r4;
+export function integer_odd_is(n) {
+  let e = integer_evenness(n);
+  let o = e === 1;
+  return o;
 }
