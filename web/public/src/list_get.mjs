69fecb591b227c8cd98177470f38143da450cd92
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 14:59:01 -0400

    pb list_get_try

--- web/public/src/list_get.mjs
+++ web/public/src/list_get.mjs
@@ -1,7 +1,7 @@
 import { list_get_try } from "../../../love/public/src/list_get_try.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
 export function list_get(list, index) {
-  let item = list_get_try(index, list);
+  let item = list_get_try(list, index);
   undefined_not_is_assert_lambda(item, object_get);
   function object_get() {
     let v = {
