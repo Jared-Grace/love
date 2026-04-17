7e34e762ea44264578268527b0c2af57771c0fcc
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 10:56:48 -0400

    function_auto_path ../love/public/src/openai_key_folder.mjs

--- public/src/openai_key_folder.mjs
+++ public/src/openai_key_folder.mjs
@@ -1,5 +1,7 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_user_path } from "../../../love/public/src/folder_user_path.mjs";
 export function openai_key_folder() {
+  let result = path_join(segments);
   let v2 = folder_user_path() + "ChristGPT\\";
   return v2;
 }
