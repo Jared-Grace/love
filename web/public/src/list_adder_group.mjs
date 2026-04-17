59d9d0345891780b57c2d0c20e8b950743b0d5de
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 10:41:44 -0400

    function_auto_path ../love/public/src/list_adder_group.mjs

--- web/public/src/list_adder_group.mjs
+++ web/public/src/list_adder_group.mjs
@@ -2,7 +2,7 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { add } from "../../../love/public/src/add.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function list_adder_group(lambda$g) {
-  async function adder_groups(la) {
+  function adder_groups(la) {
     let group = null;
     lambda$g({
       start,
