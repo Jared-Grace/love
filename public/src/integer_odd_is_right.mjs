f7c1e7baccd9543d8e02acdc2c8b4c5c3f40332f
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 15:06:58 -0400

    function_auto_path ../love/public/src/integer_odd_is_right.mjs

--- public/src/integer_odd_is_right.mjs
+++ public/src/integer_odd_is_right.mjs
@@ -1,1 +1,8 @@
-export function integer_odd_is_right() {}
+import { integer_odd_is } from "../../../love/public/src/integer_odd_is.mjs";
+export function integer_odd_is_right() {
+  let r = function lambda(item, index) {
+    let r4 = integer_odd_is(index);
+    return r4;
+  };
+  return r;
+}
