978a520e68ee47907385b4ef60517a152d57d4ba
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 20:32:52 -0400

    function_auto_path ../love/public/src/app_g_click.mjs

--- public/src/app_g_click.mjs
+++ public/src/app_g_click.mjs
@@ -1,16 +1,14 @@
+import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { app_g_click_map } from "../../../love/public/src/app_g_click_map.mjs";
 import { app_g_menu_new } from "../../../love/public/src/app_g_menu_new.mjs";
 import { g_distance_0 } from "../../../love/public/src/g_distance_0.mjs";
 import { app_g_event_target_closest_tile_coordinates } from "../../../love/public/src/app_g_event_target_closest_tile_coordinates.mjs";
 import { g_tutorials_each_remove_try } from "../../../love/public/src/g_tutorials_each_remove_try.mjs";
-import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_g_player_save } from "../../../love/public/src/app_g_player_save.mjs";
 export async function app_g_click(e, div_map, player_img_c, refresh) {
   let clicked_coordinates = app_g_event_target_closest_tile_coordinates(e);
   g_tutorials_each_remove_try();
-  let g = await app_g_game_save_get();
-  let player = property_get(g, "player");
+  let player = await app_g_player_get();
   const clicked_on_player = g_distance_0(player, clicked_coordinates);
   if (clicked_on_player) {
     await app_g_menu_new(div_map);
