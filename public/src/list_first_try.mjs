e0cd8697d7ddb377cc911ba7b39f187adfb317b2
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 15:00:32 -0400

    function_auto_path ../love/public/src/list_first_try.mjs

--- public/src/list_first_try.mjs
+++ public/src/list_first_try.mjs
@@ -1,6 +1,8 @@
+import { undefined_is_if_null } from "../../../love/public/src/undefined_is_if_null.mjs";
 import { list_get_try } from "../../../love/public/src/list_get_try.mjs";
 export function list_first_try(list) {
   const index = 0;
-  const first = list_get_try(list, index);
+  let first = list_get_try(list, index);
+  first = undefined_is_if_null(first);
   return first;
 }
