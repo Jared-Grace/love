8655d77aa7f8443d6fe3f8d04a0e4e44551b8fb2
JESUS rose to life <username@users.noreply.github.com>
2026-04-17 11:01:28 -0400

    function_auto_path ../love/public/src/openai_responses.mjs

--- public/src/openai_responses.mjs
+++ public/src/openai_responses.mjs
@@ -1,8 +1,6 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { openai_responses_file } from "../../../love/public/src/openai_responses_file.mjs";
 import { openai_generic } from "../../../love/public/src/openai_generic.mjs";
 export async function openai_responses(system, user) {
-  log(openai_responses.name, {});
   let fn = openai_responses_file;
   let data = await openai_generic(fn, system, user);
   return data;
