1f87712dee1b738583597acd811748a4e493dbfe
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 19:53:53 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- web/public/src/js_curry_replace.mjs
+++ web/public/src/js_curry_replace.mjs
@@ -77,7 +77,7 @@ export async function js_curry_replace(ast) {
       }
       async function on_expression(expression) {
         log(js_curry_replace.name, {
-          a: 3,
+          a: 3,expression
         });
         await js_call_is_if_async(expression, on_call_is);
         async function on_call_is() {
