f3040d51c3ac8017a6dad44b9f264678d9bd96b4
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 14:58:42 -0400

    function_auto_path ../love/public/src/global_function_property_lambda.mjs

--- web/public/src/global_function_property_lambda.mjs
+++ web/public/src/global_function_property_lambda.mjs
@@ -1,7 +1,7 @@
 import { global_function_initialize_object } from "../../../love/public/src/global_function_initialize_object.mjs";
 import { property_initialize_lambda } from "../../../love/public/src/property_initialize_lambda.mjs";
 export function global_function_property_lambda(fn, property_name, lambda) {
-  let fn_object = global_function_initialize_object(fn, initial);
+  let fn_object = global_function_initialize_object(fn);
   let value = property_initialize_lambda(fn_object, property_name, lambda);
   return value;
 }
