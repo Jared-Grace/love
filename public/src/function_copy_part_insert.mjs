4cf352c5fc0189ce46438199f069ed093ec410d4
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 14:50:27 -0400

    function_auto_path ../love/public/src/function_copy_part_insert.mjs

--- public/src/function_copy_part_insert.mjs
+++ public/src/function_copy_part_insert.mjs
@@ -1,11 +1,15 @@
+import { function_name_part_insert } from "../../../love/public/src/function_name_part_insert.mjs";
 import { function_copy_part_insert_at } from "../../../love/public/src/function_copy_part_insert_at.mjs";
-import { function_name_part_index } from "../../../love/public/src/function_name_part_index.mjs";
 export async function function_copy_part_insert(
   f_name_before,
   part_insert,
   part_new,
 ) {
-  let index = function_name_part_index(f_name_before, part_insert);
+  let f_name_after = function_name_part_insert(
+    f_name_before,
+    part_insert,
+    part_new,
+  );
   let v = await function_copy_part_insert_at(f_name_before, index, part_new);
   return v;
 }
