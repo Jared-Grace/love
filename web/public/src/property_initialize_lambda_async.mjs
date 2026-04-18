8d1808a596b7f23de6dbea8e93035b82931e6876
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 14:53:00 -0400

    function_auto_path ../love/public/src/property_initialize_lambda_async.mjs

--- web/public/src/property_initialize_lambda_async.mjs
+++ web/public/src/property_initialize_lambda_async.mjs
@@ -1,24 +1,15 @@
 import { property_initialize_lambda_info_async } from "../../../love/public/src/property_initialize_lambda_info_async.mjs";
-import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { property_exists_not } from "../../../love/public/src/property_exists_not.mjs";
 export async function property_initialize_lambda_async(
   object,
   property_name,
   lambda,
 ) {
-  const exists_not = property_exists_not(object, property_name);
-  if (exists_not) {
-    let value_set = lambda();
-    property_set(object, property_name, value_set);
-  }
-  let value2 = await property_get(object, property_name);
-  return value2;
-  async function lambda3() {}
   let r = await property_initialize_lambda_info_async(
-    object2,
-    property_name2,
-    lambda3,
+    object,
+    property_name,
+    lambda,
   );
   let value = property_get(r, "value");
+  return value;
 }
