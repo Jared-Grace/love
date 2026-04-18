691ab5d1659ac2b0e32a4bea2b53762caaa171cc
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 14:48:58 -0400

    w l function_name_part_insert_at

--- web/public/src/function_name_part_insert.mjs
+++ web/public/src/function_name_part_insert.mjs
@@ -1,1 +1,4 @@
-export function function_name_part_insert() {}
+import { function_name_part_insert_at } from "../../../love/public/src/function_name_part_insert_at.mjs";
+export function function_name_part_insert(f_name_before, index_string, part) {
+  return function_name_part_insert_at(f_name_before, index_string, part);
+}
