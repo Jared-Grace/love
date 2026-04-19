daa7cb6a5e7aed53927d0d14d701358b919d1f4d
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 20:53:13 -0400

    function_auto_path ../love/public/src/file_read_cached_initialize.mjs

--- public/src/file_read_cached_initialize.mjs
+++ public/src/file_read_cached_initialize.mjs
@@ -2,7 +2,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { global_function_property_lambda_info_async } from "../../../love/public/src/global_function_property_lambda_info_async.mjs";
 export async function file_read_cached_initialize(f_path) {
   "this function was refactored, but not manually tested";
-  let lambda2 = file_read_curried(f_path);
   let r = await global_function_property_lambda_info_async(
     file_read_cached_initialize,
     f_path,
