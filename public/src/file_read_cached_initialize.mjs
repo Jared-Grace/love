ad269ecf56cdd9a63e0101fdd21381a2d35e8e85
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 20:53:08 -0400

    function_auto_path ../love/public/src/file_read_cached_initialize.mjs

--- public/src/file_read_cached_initialize.mjs
+++ public/src/file_read_cached_initialize.mjs
@@ -1,4 +1,3 @@
-import { file_read_curried } from "../../../love/public/src/file_read_curried.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { global_function_property_lambda_info_async } from "../../../love/public/src/global_function_property_lambda_info_async.mjs";
 export async function file_read_cached_initialize(f_path) {
