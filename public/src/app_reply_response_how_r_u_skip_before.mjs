d3b94826ab0ea12665817b6933b0b1b4c7382d34
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:25:30 -0400

    function_auto_path ../love/public/src/app_reply_response_how_r_u_skip_before.mjs

--- public/src/app_reply_response_how_r_u_skip_before.mjs
+++ public/src/app_reply_response_how_r_u_skip_before.mjs
@@ -1,8 +1,7 @@
 import { emoji_100 } from "../../../love/public/src/emoji_100.mjs";
-import { text_the_servant_of_god_is_first_upper } from "../../../love/public/src/text_the_servant_of_god_is_first_upper.mjs";
 export function app_reply_response_how_r_u_skip_before() {
   let r =
-    text_the_servant_of_god_is_first_upper() +
+    text_the_servant_of_god_is() +
     " doing good " +
     emoji_100() +
     " through the grace of God, because ";
