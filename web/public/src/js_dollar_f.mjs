1373b7d82d0ebf25b7a48ec3876ff2b72bb91146
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:42:46 -0400

    function_auto_path ../love/public/src/js_dollar_f.mjs

--- web/public/src/js_dollar_f.mjs
+++ web/public/src/js_dollar_f.mjs
@@ -1,4 +1,4 @@
-import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
+import { js_expression_statement_is_if } from "../../../love/public/src/js_expression_statement_is_if.mjs";
 import { js_arrow_to_function_node } from "../../../love/public/src/js_arrow_to_function_node.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_code_arrow } from "../../../love/public/src/js_code_arrow.mjs";
@@ -9,7 +9,7 @@ export function js_dollar_f({ node, afters, stack1 }) {
   let parsed = js_parse_expression(code);
   js_arrow_to_function_node(parsed);
   function lambda3() {}
-  js_node_type_is_if(stack1, "ExpressionStatement", lambda3);
+  js_expression_statement_is_if(stack1, lambda3);
   list_add(afters, lambda);
   function lambda() {
     object_replace(node, parsed);
