7cb1299970f3f2d1e5c57fec1d2e6332129352c0
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:31:19 -0400

    function_auto_path ../love/public/src/app_g_game_save_get.mjs

--- public/src/app_g_game_save_get.mjs
+++ public/src/app_g_game_save_get.mjs
@@ -1,10 +1,20 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { global_function_property_lambda_info_async } from "../../../love/public/src/global_function_property_lambda_info_async.mjs";
+import { file_read_curried } from "../../../love/public/src/file_read_curried.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { file_read_cached_initialize } from "../../../love/public/src/file_read_cached_initialize.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_path.mjs";
 export async function app_g_game_save_get() {
   let p = app_g_game_save_path();
-  await file_read_cached_initialize(p);
+  ("this function was refactored, but not manually tested");
+  let lambda2 = file_read_curried(p);
+  let r = await global_function_property_lambda_info_async(
+    file_read_cached_initialize,
+    p,
+    lambda2,
+  );
+  let exists = property_get(r, "exists");
   let json = await file_read(p);
   let g = json_from(json);
   return g;
