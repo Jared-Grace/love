69041e1d1ee171158c068fadaac2467e74ca1b71
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:00:38 -0400

    function_auto_path ../love/public/src/function_curryify_generic.mjs

--- web/public/src/function_curryify_generic.mjs
+++ web/public/src/function_curryify_generic.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_return_atomize_node } from "../../../love/public/src/js_return_atomize_node.mjs";
 import { js_imports_missing_add_all } from "../../../love/public/src/js_imports_missing_add_all.mjs";
 import { js_flo_params_add } from "../../../love/public/src/js_flo_params_add.mjs";
@@ -29,10 +30,16 @@ export async function function_curryify_generic(f_name, name_get, args_get) {
       declaration_call,
       name_result,
     );
+    log(function_curryify_generic.name, {
+      declaration_result,
+    });
     js_function_declaration_params_add(declaration_result, fn_new_result_args);
     let ret = js_statement_return_argument(declaration_result);
     js_flo_body_add(ast, ret);
     await js_return_atomize_node(ast, ret, "c");
+    log(function_curryify_generic.name, {
+      declaration_result,
+    });
     let body_block = js_function_declaration_to_block_body(declaration_result);
     let item = js_call_args_await_maybe_return(
       unaliased,
