340c9f14bae5d01e1b30cb41af4829368dfd6509
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:27:21 -0400

    function_auto_path ../love/public/src/app_reply_response_how_r_u_skip_first_upper.mjs

--- public/src/app_reply_response_how_r_u_skip_first_upper.mjs
+++ public/src/app_reply_response_how_r_u_skip_first_upper.mjs
@@ -1,14 +1,7 @@
-import { app_reply_response_how_r_u_skip_before } from "../../../love/public/src/app_reply_response_how_r_u_skip_before.mjs";
+import { app_reply_response_how_r_u_skip } from "../../../love/public/src/app_reply_response_how_r_u_skip.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
-import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
-import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
 export function app_reply_response_how_r_u_skip_first_upper() {
-  const before = app_reply_response_how_r_u_skip_before();
-  let t =
-    before +
-    emoji_book_open() +
-    " all things work together for good to them that love God and are called according to the purpose of God! (Romans 8:28) However, please pray that the servant of God will be perfectly loving and healed" +
-    emoji_pray();
+  let t = app_reply_response_how_r_u_skip();
   let u = text_first_upper_to(t);
   return u;
 }
