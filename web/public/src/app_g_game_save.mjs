15bfb54cfbb4dc943a86dd35064e7f185cad361f
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 23:30:33 -0400

    function_auto_path ../love/public/src/app_g_game_save.mjs

--- web/public/src/app_g_game_save.mjs
+++ web/public/src/app_g_game_save.mjs
@@ -1,11 +1,10 @@
 import { file_overwrite_json } from "../../../love/public/src/file_overwrite_json.mjs";
 import { app_g_game_save_get } from "../../../love/public/src/app_g_game_save_get.mjs";
 import { global_function_property_delete } from "../../../love/public/src/global_function_property_delete.mjs";
-import { file_read_cached_initialize } from "../../../love/public/src/file_read_cached_initialize.mjs";
 import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_path.mjs";
-export async function app_g_game_save(g) {
+export async function app_g_game_save() {
   let p = app_g_game_save_path();
-  let g2 = await app_g_game_save_get();
-  await file_overwrite_json(p, contents);
-  global_function_property_delete(file_read_cached_initialize, p);
+  let g = await app_g_game_save_get();
+  await file_overwrite_json(p, g);
+  global_function_property_delete(app_g_game_save_get, p);
 }
