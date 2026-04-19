51a85ec478fc2f73be12d1e3a4083da91efa9251
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:35:15 -0400

    function_auto_path ../love/public/src/js_dollar_f.mjs

--- web/public/src/js_dollar_f.mjs
+++ web/public/src/js_dollar_f.mjs
@@ -1,3 +1,5 @@
+import { js_statement_return_add } from "../../../love/public/src/js_statement_return_add.mjs";
+import { js_if_blockify_generic_node } from "../../../love/public/src/js_if_blockify_generic_node.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_code_arrow } from "../../../love/public/src/js_code_arrow.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
@@ -5,6 +7,7 @@ import { js_parse_expression } from "../../../love/public/src/js_parse_expressio
 export function js_dollar_f({ node, afters }) {
   const code = js_code_arrow();
   let parsed = js_parse_expression(code);
+  js_if_blockify_generic_node(parsed, "body", js_statement_return_add);
   list_add(afters, lambda);
   function lambda() {
     object_replace(node, parsed);
