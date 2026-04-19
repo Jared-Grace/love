30656d3f026d47f215836c43ec259fd36ef4d5fd
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:40:42 -0400

    function_auto_path ../love/public/src/js_dollar_f.mjs

--- public/src/js_dollar_f.mjs
+++ public/src/js_dollar_f.mjs
@@ -4,7 +4,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_code_arrow } from "../../../love/public/src/js_code_arrow.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
-export function js_dollar_f({ node, afters }) {
+export function js_dollar_f({ node, afters, stack1 }) {
   const code = js_code_arrow();
   let parsed = js_parse_expression(code);
   js_arrow_to_function_node(parsed);
