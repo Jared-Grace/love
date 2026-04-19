f79e962f9f73fe71504cca4036e8311aa2222aae
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:26:31 -0400

    function_auto_path ../love/public/src/app_reply_response_how_r_u_skip_first_upper.mjs

--- public/src/app_reply_response_how_r_u.mjs
+++ public/src/app_reply_response_how_r_u.mjs
@@ -1,6 +1,6 @@
-import { app_reply_response_how_r_u_skip } from "../../../love/public/src/app_reply_response_how_r_u_skip.mjs";
+import { app_reply_response_how_r_u_skip_first_upper } from "../../../love/public/src/app_reply_response_how_r_u_skip_first_upper.mjs";
 import { emoji_ok } from "../../../love/public/src/emoji_ok.mjs";
 export function app_reply_response_how_r_u() {
-  let hru = emoji_ok() + " " + app_reply_response_how_r_u_skip();
+  let hru = emoji_ok() + " " + app_reply_response_how_r_u_skip_first_upper();
   return hru;
 }
