ec0c8c9d175794da4a22bb9186d823a42dd2e14a
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:45:04 -0400

    function_auto_path ../love/public/src/js_arrow_to_function_node.mjs

--- web/public/src/js_arrow_to_function_node.mjs
+++ web/public/src/js_arrow_to_function_node.mjs
@@ -1,5 +1,5 @@
-import { property_set } from "../../../love/public/src/property_set.mjs";
+import { js_node_type_set } from "../../../love/public/src/js_node_type_set.mjs";
 export function js_arrow_to_function_node(node) {
   const type = "FunctionExpression";
-  property_set(node, "type", type);
+  js_node_type_set(node, type);
 }
