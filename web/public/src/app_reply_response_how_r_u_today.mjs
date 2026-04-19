64551052e6104d609d58c1a1be73c3e1b8ddaac9
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:35:36 -0400

    function_auto_path ../love/public/src/app_reply_response_how_r_u_today.mjs

--- web/public/src/app_reply_response_how_r_u_today.mjs
+++ web/public/src/app_reply_response_how_r_u_today.mjs
@@ -1,8 +1,7 @@
+import { app_reply_response_how_r_u_skip } from "../../../love/public/src/app_reply_response_how_r_u_skip.mjs";
 import { app_reply_response_how_r_u_before } from "../../../love/public/src/app_reply_response_how_r_u_before.mjs";
-import { app_reply_response_how_r_u_skip_first_upper } from "../../../love/public/src/app_reply_response_how_r_u_skip_first_upper.mjs";
 export function app_reply_response_how_r_u_today() {
-  let hru =
-    app_reply_response_how_r_u_before() +
-    app_reply_response_how_r_u_skip_first_upper();
+  let hru = app_reply_response_how_r_u_before() + "Today, ";
+  app_reply_response_how_r_u_skip();
   return hru;
 }
