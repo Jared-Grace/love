0606e7e54464416ae3f9a2badc916d03cdc96597
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 20:51:02 -0400

    function_auto_path ../love/public/src/js_curry_replace.mjs

--- public/src/js_curry_replace.mjs
+++ public/src/js_curry_replace.mjs
@@ -1,4 +1,3 @@
-import { js_await_if_unwrap_argument } from "../../../love/public/src/js_await_if_unwrap_argument.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { function_curryify_right_name } from "../../../love/public/src/function_curryify_right_name.mjs";
 import { function_curryify_right } from "../../../love/public/src/function_curryify_right.mjs";
@@ -88,11 +87,6 @@ export async function js_curry_replace(ast) {
           if (includes) {
             let args = js_call_arguments_get(expression);
             let difference = js_identifiers_names_difference_try(args, params);
-            log(js_curry_replace.name, {
-              args,
-              params,
-            });
-            let call3 = js_await_if_unwrap_argument(call2);
             let difference_sz_1 = list_size_1(difference);
             let first = list_first(difference);
             let fi = list_first_is(args, first);
