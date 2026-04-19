54bf007fef22ec5e0c55669ea8e1acc093015532
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:35:44 -0400

    function_auto_path ../love/public/src/js_dollar_f.mjs

--- web/public/src/js_dollar_f.mjs
+++ web/public/src/js_dollar_f.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_statement_return_add } from "../../../love/public/src/js_statement_return_add.mjs";
 import { js_if_blockify_generic_node } from "../../../love/public/src/js_if_blockify_generic_node.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
@@ -8,6 +9,9 @@ export function js_dollar_f({ node, afters }) {
   const code = js_code_arrow();
   let parsed = js_parse_expression(code);
   js_if_blockify_generic_node(parsed, "body", js_statement_return_add);
+  log(js_dollar_f.name, {
+    parsed,
+  });
   list_add(afters, lambda);
   function lambda() {
     object_replace(node, parsed);
