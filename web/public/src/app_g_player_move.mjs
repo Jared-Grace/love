757b2796ca46e060f0a7b2067f62bd0620b7baa3
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 23:49:18 -0400

    function_auto_path ../love/public/src/app_g_player_move.mjs

--- web/public/src/app_g_player_move.mjs
+++ web/public/src/app_g_player_move.mjs
@@ -1,3 +1,5 @@
+import { app_g_player_coordinates_update_move } from "../../../love/public/src/app_g_player_coordinates_update_move.mjs";
+import { log } from "../../../love/public/src/log.mjs";
 import { app_g_player_get } from "../../../love/public/src/app_g_player_get.mjs";
 import { app_g_player_center } from "../../../love/public/src/app_g_player_center.mjs";
 import { app_g_player_move_animate } from "../../../love/public/src/app_g_player_move_animate.mjs";
@@ -9,6 +11,9 @@ export async function app_g_player_move(
 ) {
   let player = await app_g_player_get();
   const away = g_distance_at_least_1(player, coordinates_move_to);
+  log(app_g_player_move.name, {
+    away,
+  });
   if (away) {
     await app_g_player_move_animate(player, player_img_c);
   }
