d71e408476889e30b3ab386eb7c786ae9d32a692
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:29:19 -0400

    function_auto_path ../love/public/src/text_the_servant_of_god_is_first_upper.mjs

--- public/src/text_the_servant_of_god_is_first_upper.mjs
+++ public/src/text_the_servant_of_god_is_first_upper.mjs
@@ -1,5 +1,7 @@
+import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 import { text_the_servant_of_god } from "../../../love/public/src/text_the_servant_of_god.mjs";
 export function text_the_servant_of_god_is_first_upper() {
-  let v = text_the_servant_of_god() + " is";
-  return v;
+  let t = text_the_servant_of_god() + " is";
+  let u = text_first_upper_to(t);
+  return t;
 }
