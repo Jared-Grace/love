dc5465e5c3ef4325fea08e7e49b7a987ca3df18c
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 20:51:38 -0400

    function_auto_path ../love/public/src/file_read_curried.mjs

--- public/src/file_read_curried.mjs
+++ public/src/file_read_curried.mjs
@@ -1,5 +1,5 @@
 import { file_read } from "../../../love/public/src/file_read.mjs";
-export async function file_read_curried(file_path) {
+export function file_read_curried(file_path) {
   let r2 = async function file_read_curried_result() {
     let r = await file_read(file_path);
     return r;
