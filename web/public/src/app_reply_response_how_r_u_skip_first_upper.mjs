9690307b63fb1aec23f885c14323d7205e9a95dd
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:26:46 -0400

    function_auto_path ../love/public/src/app_reply_response_how_r_u_skip_first_upper.mjs

--- web/public/src/app_reply_response_how_r_u_skip_first_upper.mjs
+++ web/public/src/app_reply_response_how_r_u_skip_first_upper.mjs
@@ -1,3 +1,4 @@
+import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 import { app_reply_response_how_r_u_skip_before_first_upper } from "../../../love/public/src/app_reply_response_how_r_u_skip_before_first_upper.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
@@ -8,5 +9,6 @@ export function app_reply_response_how_r_u_skip_first_upper() {
     emoji_book_open() +
     " all things work together for good to them that love God and are called according to the purpose of God! (Romans 8:28) However, please pray that the servant of God will be perfectly loving and healed" +
     emoji_pray();
+  let u = text_first_upper_to(t);
   return t;
 }
