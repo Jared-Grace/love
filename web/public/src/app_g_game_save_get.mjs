7f88f421df518918b027ac7d02a7f5b2dbbe0ff1
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:31:11 -0400

    function_auto_path ../love/public/src/app_g_game_save_get.mjs

--- web/public/src/app_g_game_save_get.mjs
+++ web/public/src/app_g_game_save_get.mjs
@@ -1,9 +1,11 @@
+import { file_read } from "../../../love/public/src/file_read.mjs";
+import { file_read_cached_initialize } from "../../../love/public/src/file_read_cached_initialize.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
-import { file_read_cached } from "../../../love/public/src/file_read_cached.mjs";
 import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_path.mjs";
 export async function app_g_game_save_get() {
   let p = app_g_game_save_path();
-  let json = await file_read_cached(p);
+  await file_read_cached_initialize(p);
+  let json = await file_read(p);
   let g = json_from(json);
   return g;
 }
