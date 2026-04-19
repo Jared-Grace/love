faaa51162762de3102382bdffa10c10289aa35b9
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 23:27:46 -0400

    function_auto_path ../love/public/src/app_g_game_save_get.mjs

--- web/public/src/app_g_game_save_get.mjs
+++ web/public/src/app_g_game_save_get.mjs
@@ -2,14 +2,13 @@ import { file_read_json_curried } from "../../../love/public/src/file_read_json_
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { global_function_property_lambda_info_async } from "../../../love/public/src/global_function_property_lambda_info_async.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
-import { file_read_cached_initialize } from "../../../love/public/src/file_read_cached_initialize.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
 import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_path.mjs";
 export async function app_g_game_save_get() {
   let p = app_g_game_save_path();
   let lambda2 = file_read_json_curried(file_path);
   let r = await global_function_property_lambda_info_async(
-    file_read_cached_initialize,
+    app_g_game_save_get,
     p,
     lambda2,
   );
