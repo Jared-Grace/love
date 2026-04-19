91cedf40d52caa5688a1322ce372bee0a2c370d1
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:31:30 -0400

    function_auto_path ../love/public/src/app_g_game_save_get.mjs

--- public/src/app_g_game_save_get.mjs
+++ public/src/app_g_game_save_get.mjs
@@ -1,6 +1,5 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { global_function_property_lambda_info_async } from "../../../love/public/src/global_function_property_lambda_info_async.mjs";
-import { file_read_curried } from "../../../love/public/src/file_read_curried.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { file_read_cached_initialize } from "../../../love/public/src/file_read_cached_initialize.mjs";
 import { json_from } from "../../../love/public/src/json_from.mjs";
@@ -8,7 +7,7 @@ import { app_g_game_save_path } from "../../../love/public/src/app_g_game_save_p
 export async function app_g_game_save_get() {
   let p = app_g_game_save_path();
   ("this function was refactored, but not manually tested");
-  let lambda2 = file_read_curried(p);
+  (function lambda() {});
   let r = await global_function_property_lambda_info_async(
     file_read_cached_initialize,
     p,
