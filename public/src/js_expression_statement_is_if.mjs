6ccadbb84c349dc9be0a4a5e02f2ee27c8956e31
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:42:46 -0400

    function_auto_path ../love/public/src/js_dollar_f.mjs

---
+++ public/src/js_expression_statement_is_if.mjs
@@ -0,0 +1,4 @@
+import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
+export function js_expression_statement_is_if(stack1, lambda3) {
+  js_node_type_is_if(stack1, "ExpressionStatement", lambda3);
+}
