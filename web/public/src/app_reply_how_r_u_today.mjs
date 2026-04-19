382f9eec5eb4a66c91f1e75578da2f77ee4b336d
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:32:23 -0400

    function_auto_path ../love/public/src/app_reply_how_r_u_today.mjs

--- web/public/src/app_reply_how_r_u_today.mjs
+++ web/public/src/app_reply_how_r_u_today.mjs
@@ -2,6 +2,6 @@ import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_reply_how_r_u } from "../../../love/public/src/app_reply_how_r_u.mjs";
 export function app_reply_how_r_u_today() {
   let t = app_reply_how_r_u();
-  let combined = text_combine(t, ' today');
+  let combined = text_combine(t, " today");
   return t;
 }
