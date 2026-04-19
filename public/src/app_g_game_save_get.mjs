d68bf1c85dcf9d80d07b2d2866f6bf86596c361b
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 21:31:52 -0400

    function_auto_path ../love/public/src/app_g_game_save_get.mjs

--- public/src/app_g_game_save_get.mjs
+++ public/src/app_g_game_save_get.mjs
@@ -8,7 +8,7 @@ export async function app_g_game_save_get() {
   let p = app_g_game_save_path();
   (function lambda() {
     return;
-  })
+  });
   let r = await global_function_property_lambda_info_async(
     file_read_cached_initialize,
     p,
