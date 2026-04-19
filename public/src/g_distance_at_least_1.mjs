07fb1e206b6fb98fba133942b409096f73e214b2
JESUS rose to life <username@users.noreply.github.com>
2026-04-18 23:50:05 -0400

    function_auto_path ../love/public/src/g_distance_at_least_1.mjs

--- public/src/g_distance_at_least_1.mjs
+++ public/src/g_distance_at_least_1.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { at_least_1 } from "../../../love/public/src/at_least_1.mjs";
 import { g_distance } from "../../../love/public/src/g_distance.mjs";
 export function g_distance_at_least_1(player, coordinates_move_to) {
   let distance = g_distance(player, coordinates_move_to);
+  log(g_distance_at_least_1.name, {
+    distance,
+  });
   const away = at_least_1(distance);
   return away;
 }
