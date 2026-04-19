cf9f11503c08492335971666e81dde9ca77d4c17
JESUS rose to life <username@users.noreply.github.com>
2026-04-19 15:26:55 -0400

    function_auto_path ../love/public/src/app_reply_response_how_r_u_skip_first_upper.mjs

--- public/src/app_reply_response_how_r_u_skip_first_upper.mjs
+++ public/src/app_reply_response_how_r_u_skip_first_upper.mjs
@@ -1,9 +1,9 @@
+import { app_reply_response_how_r_u_skip_before } from "../../../love/public/src/app_reply_response_how_r_u_skip_before.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
-import { app_reply_response_how_r_u_skip_before_first_upper } from "../../../love/public/src/app_reply_response_how_r_u_skip_before_first_upper.mjs";
 import { emoji_pray } from "../../../love/public/src/emoji_pray.mjs";
 import { emoji_book_open } from "../../../love/public/src/emoji_book_open.mjs";
 export function app_reply_response_how_r_u_skip_first_upper() {
-  const before = app_reply_response_how_r_u_skip_before_first_upper();
+  const before = app_reply_response_how_r_u_skip_before();
   let t =
     before +
     emoji_book_open() +
