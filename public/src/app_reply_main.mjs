85bef5e8856aa3a318477b2f2115edafd109aaf8
JESUS rose to life <username@users.noreply.github.com>
2026-04-20 14:53:44 -0400

    function_auto_path ../love/public/src/app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -121,6 +121,7 @@ export async function app_reply_main(context) {
   html_p_text(root, "3. (Optional) Or, instead of 1 or 2, choose shortcuts:");
   let buttons = null;
   let shortcuts = [];
+  list_add(list, item3);
   async function lambda5() {
     languages_chosen_reset();
     await update(1);
