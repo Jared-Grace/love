34ca5d6757bd8820b3c86a0103f6f05475e133af
JESUS rose to life <username@users.noreply.github.com>
2026-04-20 15:41:17 -0400

    ri property_change property_transform

--- web/public/src/property_transform_list_map.mjs
+++ web/public/src/property_transform_list_map.mjs
@@ -1,6 +1,10 @@
 import { property_transform } from "../../../love/public/src/property_transform.mjs";
 import { list_map_curried_right } from "../../../love/public/src/list_map_curried_right.mjs";
-export function property_change_list_map(object, property_name, lambda$item) {
+export function property_transform_list_map(
+  object,
+  property_name,
+  lambda$item,
+) {
   let c = list_map_curried_right(lambda$item);
   let value_changed = property_transform(object, property_name, c);
   return value_changed;
