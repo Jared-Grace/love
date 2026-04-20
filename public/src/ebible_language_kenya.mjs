eb3e9b029185a0376f47c62542c3f133bb382b45
JESUS rose to life <username@users.noreply.github.com>
2026-04-20 17:06:32 -0400

    function_auto_path ../love/public/src/ebible_languages.mjs

---
+++ public/src/ebible_language_kenya.mjs
@@ -0,0 +1,9 @@
+import { ebible_folder_swahili } from "../../../love/public/src/ebible_folder_swahili.mjs";
+export function ebible_language_kenya() {
+  let r = {
+    name: "Swahili",
+    bible_folder: ebible_folder_swahili(),
+    language_code: "swh",
+  };
+  return r;
+}
