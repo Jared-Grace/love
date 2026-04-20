88ac91db06f1575e01a1505abdcfa421ab64434c
JESUS rose to life <username@users.noreply.github.com>
2026-04-20 14:58:48 -0400

    function_auto_path ../love/public/src/app_reply_main.mjs

--- public/src/app_reply_main.mjs
+++ public/src/app_reply_main.mjs
@@ -136,11 +136,12 @@ export async function app_reply_main(context) {
     async function lambda5() {
       app_reply_languages_chosen_reset(languages_chosen, languages2, languages);
       await update(count2);
-      function lambda7(item3) {}
+      function lambda7(item3) {
+        let property_value = app_reply_greetings_live();
+        let b = list_find_property(buttons, "text", property_value);
+        property_get_invoke(b, "click");
+      }
       each(list, lambda7);
-      let property_value = app_reply_greetings_live();
-      let b = list_find_property(buttons, "text", property_value);
-      property_get_invoke(b, "click");
     }
     let component2 = html_button(root, name2, lambda5);
   }
