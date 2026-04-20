cb9c6be87931cd51a1580dc21a16926a7c9b7673
JESUS rose to life <username@users.noreply.github.com>
2026-04-20 15:41:13 -0400

    function_auto_path ../love/public/src/js_visit_declarators_uniqueify.mjs

--- web/public/src/property_transform_async.mjs
+++ web/public/src/property_transform_async.mjs
@@ -1,6 +1,6 @@
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export async function property_change_async(o, property, lambda$value) {
+export async function property_transform_async(o, property, lambda$value) {
   let value = property_get(o, property);
   value = await lambda$value(value);
   property_set(o, property, value);
