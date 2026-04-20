fdc9f3c9715eea5629a664817c609fda6c8a045d
JESUS rose to life <username@users.noreply.github.com>
2026-04-20 16:37:35 -0400

    function_auto_path ../love/public/src/app_reply_buttons_languages.mjs

--- public/src/app_reply_buttons_languages.mjs
+++ public/src/app_reply_buttons_languages.mjs
@@ -2,10 +2,11 @@ import { app_reply_buttons_languages_on_toggle } from "../../../love/public/src/
 import { noop } from "../../../love/public/src/noop.mjs";
 export function app_reply_buttons_languages(languages_chosen, root, languages) {
   let on_toggle = noop;
-  app_reply_buttons_languages_on_toggle(
+  let mapped = app_reply_buttons_languages_on_toggle(
     languages_chosen,
     on_toggle,
     root,
     languages,
   );
+  return mapped;
 }
