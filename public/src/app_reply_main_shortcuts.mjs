7e8729dc8af46c625a75b8e69f5601c7eaaeabb4
JESUS rose to life <username@users.noreply.github.com>
2026-04-21 16:56:48 -0400

    function_auto_path ../love/public/src/app_reply_main_shortcuts.mjs

--- public/src/app_reply_main_shortcuts.mjs
+++ public/src/app_reply_main_shortcuts.mjs
@@ -52,7 +52,7 @@ export function app_reply_main_shortcuts(
   let r_pk_10_gh = shortcut_extend_response(r_pk_10_g, "h", h);
   let r_pk_10_h = shortcut_extend_response(r_pk_10, "h", h);
   let c = app_reply_called_why();
-  let r_pk_10_c = shortcut_extend_response(r_pk_10, "c", v2);
+  let r_pk_10_c = shortcut_extend_response(r_pk_10, "c", c);
   let r_pk_20 = shortcut_extend_count(r_pk_base, 20);
   let r_pk_40 = shortcut_extend_count(r_pk_base, 40);
   let languages_ug = languages_default_concat_single(lug);
