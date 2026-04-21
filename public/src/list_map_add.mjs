6d7d9f6b7ac48ce4c6ad6f678c4c584e0dcab68a
JESUS rose to life <username@users.noreply.github.com>
2026-04-21 11:29:32 -0400

    function_auto_path ../love/public/src/list_map_add.mjs

--- public/src/list_map_add.mjs
+++ public/src/list_map_add.mjs
@@ -1,6 +1,6 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-export function list_map_add(list, lambda9, buttons_responses) {
-  let buttons_responses_result = list_map(list, lambda9);
-  list_add(buttons_responses, buttons_responses_result);
+export function list_map_add(list, mapper, existing) {
+  let buttons_responses_result = list_map(list, mapper);
+  list_add(existing, buttons_responses_result);
 }
