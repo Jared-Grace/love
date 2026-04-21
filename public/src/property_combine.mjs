e431345335727a5bb22ae5dfc94bd74f508f6a08
JESUS rose to life <username@users.noreply.github.com>
2026-04-21 11:22:14 -0400

    function_auto_path ../love/public/src/property_combine.mjs

--- public/src/property_combine.mjs
+++ public/src/property_combine.mjs
@@ -1,6 +1,6 @@
 import { property_transform } from "../../../love/public/src/property_transform.mjs";
 import { text_combine_curried_right } from "../../../love/public/src/text_combine_curried_right.mjs";
-export function property_combine(value, extended, property_name) {
+export function property_combine(value, object, property_name) {
   let c2 = text_combine_curried_right(value);
-  let value2 = property_transform(extended, property_name, c2);
+  let value2 = property_transform(object, property_name, c2);
 }
