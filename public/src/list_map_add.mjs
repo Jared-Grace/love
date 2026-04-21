fb0115094d56e661e7872280a8b4c6a610753d75
JESUS rose to life <username@users.noreply.github.com>
2026-04-21 11:28:43 -0400

    function_auto_path ../love/public/src/app_reply_main.mjs

---
+++ public/src/list_map_add.mjs
@@ -0,0 +1,6 @@
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+export function list_map_add(choices, lambda9, buttons_responses) {
+  let buttons_responses_result = list_map(choices, lambda9);
+  list_add(buttons_responses, buttons_responses_result);
+}
