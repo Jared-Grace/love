b81b434b1343c1794734ef6b5487b05a8017460e
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:47:06 -0400

    function_auto_path ../love/public/src/file_exists_ensure_json.mjs

--- web/public/src/file_exists_ensure_json.mjs
+++ web/public/src/file_exists_ensure_json.mjs
@@ -1,10 +1,10 @@
-import { json_to } from "../../../love/public/src/json_to.mjs";
+import { json_empty } from "../../../love/public/src/json_empty.mjs";
 import { file_write } from "../../../love/public/src/file_write.mjs";
 import { file_exists_not } from "../../../love/public/src/file_exists_not.mjs";
 export async function file_exists_ensure_json(file_path) {
   let n = await file_exists_not(file_path);
   if (n) {
-    let json = json_to({});
+    let json = json_empty();
     let r = await file_write(file_path, json);
   }
 }
