bc2ad5a901d8644cfd95c4fba3a9e23c12fea634
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 14:31:09 -0400

    function_auto_path ../love/public/src/app_reply_called_why.mjs

--- web/public/src/app_reply_called_why.mjs
+++ web/public/src/app_reply_called_why.mjs
@@ -1,5 +1,5 @@
 import { emoji_phone } from "../../../love/public/src/emoji_phone.mjs";
 export function app_reply_called_why() {
-  let v2 = emoji_phone() + " Called why?";
-  return v2;
+  let t = emoji_phone() + " Called why?";
+  return t;
 }
