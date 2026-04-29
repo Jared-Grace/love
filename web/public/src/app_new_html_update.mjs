034184fca2596a209790aeb68a1817b8c3a60708
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 17:52:20 -0400

    function_auto_path ../love/public/src/app_new_html_update.mjs

--- web/public/src/app_new_html_update.mjs
+++ web/public/src/app_new_html_update.mjs
@@ -1,3 +1,4 @@
+import { html_update_latest_promote_deploy } from "../../../love/public/src/html_update_latest_promote_deploy.mjs";
 import { html_public_exists_ensure } from "../../../love/public/src/html_public_exists_ensure.mjs";
 import { html_update_latest } from "../../../love/public/src/html_update_latest.mjs";
 import { html_update_dev } from "../../../love/public/src/html_update_dev.mjs";
@@ -6,6 +7,8 @@ export async function app_new_html_update(name) {
   await html_public_exists_ensure(name);
   await html_update_dev(name);
   return;
+  ("before, app html files worked differently, so these may not be needed anymore");
+  "now use " + html_update_latest_promote_deploy;
   await html_update_public(name);
   await html_update_latest(name);
 }
