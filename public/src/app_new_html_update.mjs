581ba13790d2e48ea438083f17b13971648c1a8d
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 17:49:24 -0400

    function_auto_path ../love/public/src/app_new_html_update.mjs

--- public/src/app_new_html_update.mjs
+++ public/src/app_new_html_update.mjs
@@ -1,15 +1,11 @@
-import { file_overwrite_empty } from "../../../love/public/src/file_overwrite_empty.mjs";
-import { file_exists_not } from "../../../love/public/src/file_exists_not.mjs";
+import { file_exists_ensure } from "../../../love/public/src/file_exists_ensure.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
 import { html_update_latest } from "../../../love/public/src/html_update_latest.mjs";
 import { html_update_dev } from "../../../love/public/src/html_update_dev.mjs";
 import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
 export async function app_new_html_update(name) {
   let file_path = html_name_to_path(name);
-  let n = await file_exists_not(file_path);
-  if (n) {
-    await file_overwrite_empty(file_path);
-  }
+  await file_exists_ensure(file_path);
   await html_update_public(name);
   await html_update_dev(name);
   await html_update_latest(name);
