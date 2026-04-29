00e8e3352f4fa343e1c0a76acb27191f05942c03
JESUS rose to life <username@users.noreply.github.com>
2026-04-28 23:12:52 -0400

    function_auto_path ../love/public/src/git_as.mjs

--- web/public/src/git_as.mjs
+++ web/public/src/git_as.mjs
@@ -1,1 +1,4 @@
-export function git_as() {}
+import { git_acp_folder } from "../../../love/public/src/git_acp_folder.mjs";
+export async function git_as() {
+  await git_acp_folder(folder, message);
+}
