dde30da95b3dd1cf59a79513d89ff6f9976fabf0
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:31:26 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- public/src/app_calendar_main.mjs
+++ public/src/app_calendar_main.mjs
@@ -1,4 +1,5 @@
-import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
+import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
+import { html_p } from "../../../love/public/src/html_p.mjs";
 import { clipboard_paste } from "../../../love/public/src/clipboard_paste.mjs";
 import { html_input_wide } from "../../../love/public/src/html_input_wide.mjs";
 import { html_button_wide } from "../../../love/public/src/html_button_wide.mjs";
@@ -10,9 +11,10 @@ export function app_calendar_main(context) {
     "Paste Facebook Messages URL or WhatsApp number",
     lambda2,
   );
-  let p = html_p_text(root2, text);
+  let p = html_p(root);
   html_input_wide(root);
   async function lambda2() {
     let paste = await clipboard_paste();
+    html_text_set(component, text);
   }
 }
