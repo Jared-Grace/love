de28a1b2a5a709f25db12871834526886db75400
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 17:48:49 -0400

    function_auto_path ../love/public/src/app_new_html_update.mjs

--- public/src/app_new_html_update.mjs
+++ public/src/app_new_html_update.mjs
@@ -1,4 +1,4 @@
-import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
+import { file_overwrite_empty } from "../../../love/public/src/file_overwrite_empty.mjs";
 import { file_exists_not } from "../../../love/public/src/file_exists_not.mjs";
 import { html_name_to_path } from "../../../love/public/src/html_name_to_path.mjs";
 import { html_update_latest } from "../../../love/public/src/html_update_latest.mjs";
@@ -8,7 +8,7 @@ export async function app_new_html_update(name) {
   let file_path = html_name_to_path(name);
   let n = await file_exists_not(file_path);
   if (n) {
-    await file_overwrite(file_path, "");
+    await file_overwrite_empty(file_path);
   }
   await html_update_public(name);
   await html_update_dev(name);
