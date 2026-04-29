57560b5b2f51497e1264c0bbd8aec11811443d39
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:32:05 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- web/public/src/app_calendar_main.mjs
+++ web/public/src/app_calendar_main.mjs
@@ -1,7 +1,6 @@
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { html_p } from "../../../love/public/src/html_p.mjs";
 import { clipboard_paste } from "../../../love/public/src/clipboard_paste.mjs";
-import { html_input_wide } from "../../../love/public/src/html_input_wide.mjs";
 import { html_button_wide } from "../../../love/public/src/html_button_wide.mjs";
 import { html_mobile_default_font_size_context } from "../../../love/public/src/html_mobile_default_font_size_context.mjs";
 export function app_calendar_main(context) {
@@ -12,7 +11,6 @@ export function app_calendar_main(context) {
     lambda2,
   );
   let p = html_p(root);
-  html_input_wide(root);
   async function lambda2() {
     let paste = await clipboard_paste();
     html_text_set(p, paste);
