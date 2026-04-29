0489c27fbafba70d8a734676bd0cc71796c84b49
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:28:22 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- web/public/src/app_calendar_main.mjs
+++ web/public/src/app_calendar_main.mjs
@@ -1,3 +1,4 @@
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { html_input } from "../../../love/public/src/html_input.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -7,4 +8,5 @@ export function app_calendar_main(context) {
   });
   let root = property_get(context, "root");
   let component = html_input(root);
+  html_width_full(component2);
 }
