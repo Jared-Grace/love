a93efeace0eddafd4f463ddbf724948ff2070592
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:28:58 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- public/src/app_calendar_main.mjs
+++ public/src/app_calendar_main.mjs
@@ -1,14 +1,12 @@
 import { html_mobile_default_font_size_context } from "../../../love/public/src/html_mobile_default_font_size_context.mjs";
 import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { html_input } from "../../../love/public/src/html_input.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export function app_calendar_main(context) {
-  let root2 = html_mobile_default_font_size_context(context2);
+  let root = html_mobile_default_font_size_context(context);
   log(app_calendar_main.name, {
     context,
   });
-  let root = property_get(context, "root");
   let component = html_input(root);
   html_width_full(component);
 }
