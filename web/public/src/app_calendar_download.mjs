555dfb663d4032179ebc18e82260f1c961d580bc
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:44:21 -0400

    function_auto_path ../love/public/src/app_calendar_download.mjs

--- web/public/src/app_calendar_download.mjs
+++ web/public/src/app_calendar_download.mjs
@@ -1,1 +1,4 @@
-export function app_calendar_download() {}
+import { file_read_json } from "../../../love/public/src/file_read_json.mjs";
+export async function app_calendar_download() {
+  let data = await file_read_json(file_path);
+}
