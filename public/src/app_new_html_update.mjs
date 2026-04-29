e2fd3a0f6a366c0b753fb34b0345b8689d6a24fb
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 17:48:40 -0400

    function_auto_path ../love/public/src/app_new_html_update.mjs

--- public/src/app_new_html_update.mjs
+++ public/src/app_new_html_update.mjs
@@ -8,7 +8,7 @@ export async function app_new_html_update(name) {
   let file_path = html_name_to_path(name);
   let n = await file_exists_not(file_path);
   if (n) {
-    await file_overwrite(file_path2, contents);
+    await file_overwrite(file_path, "");
   }
   await html_update_public(name);
   await html_update_dev(name);
