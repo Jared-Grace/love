07792ec74ead6d0724033662fcf67fb996522970
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 17:51:48 -0400

    function_auto_path ../love/public/src/app_new_html_update.mjs

--- web/public/src/app_new_html_update.mjs
+++ web/public/src/app_new_html_update.mjs
@@ -4,7 +4,8 @@ import { html_update_dev } from "../../../love/public/src/html_update_dev.mjs";
 import { html_update_public } from "../../../love/public/src/html_update_public.mjs";
 export async function app_new_html_update(name) {
   await html_public_exists_ensure(name);
-  await html_update_public(name);
   await html_update_dev(name);
+  return;
+  await html_update_public(name);
   await html_update_latest(name);
 }
