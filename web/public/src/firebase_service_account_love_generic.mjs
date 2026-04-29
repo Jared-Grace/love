014ff130f3d732051bde73249de11422ef95e151
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:36:06 -0400

    function_auto_path ../love/public/src/firebase_service_account_love_generic.mjs

--- web/public/src/firebase_service_account_love_generic.mjs
+++ web/public/src/firebase_service_account_love_generic.mjs
@@ -3,8 +3,8 @@ import { folder_find_starts_with } from "../../../love/public/src/folder_find_st
 import { path_join } from "../../../love/public/src/path_join.mjs";
 export async function firebase_service_account_love_generic(name) {
   let prefix = name + "-firebase-adminsdk-";
-  let path_folder = folder_previous_2_join("secret");
   let only = await folder_find_starts_with(path_folder, prefix);
+  let path_folder = folder_previous_2_join("secret");
   let file_path = path_join([path_folder, only]);
   return file_path;
 }
