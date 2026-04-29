f7acb19f83859d3df43cf80264032dd2790382b2
JESUS rose to life <username@users.noreply.github.com>
2026-04-28 23:14:35 -0400

    function_auto_path ../love/public/src/git_as.mjs

--- public/src/git_as.mjs
+++ public/src/git_as.mjs
@@ -1,4 +1,6 @@
+import { date_now_file } from "../../../love/public/src/date_now_file.mjs";
 import { git_acp_folder } from "../../../love/public/src/git_acp_folder.mjs";
 export async function git_as() {
+  let now_file = date_now_file();
   await git_acp_folder("/home/j/AndroidStudioProjects/AlarmSync/", message);
 }
