2be57fa41b50f43c56784d003f1e4e362adba100
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 19:45:18 -0400

    function_auto_path ../love/public/src/file_exists_ensure.mjs

--- public/src/file_exists_ensure.mjs
+++ public/src/file_exists_ensure.mjs
@@ -1,8 +1,8 @@
-import { file_overwrite } from "../../../love/public/src/file_overwrite.mjs";
+import { file_write } from "../../../love/public/src/file_write.mjs";
 import { file_exists_not } from "../../../love/public/src/file_exists_not.mjs";
 export async function file_exists_ensure(file_path) {
   let n = await file_exists_not(file_path);
   if (n) {
-    let r = await file_overwrite(file_path, "");
+    let r = await file_write(file_path, "");
   }
 }
