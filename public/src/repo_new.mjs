642fa5ea994e619033c78e7ce788805b1be95431
JESUS rose to life <username@users.noreply.github.com>
2026-04-28 21:26:40 -0400

    function_auto_path ../love/public/src/repo_new.mjs

--- public/src/repo_new.mjs
+++ public/src/repo_new.mjs
@@ -1,16 +1,7 @@
-import { git_init_folder } from "../../../love/public/src/git_init_folder.mjs";
+import { git_repo_new } from "../../../love/public/src/git_repo_new.mjs";
 import { repo_new_local } from "../../../love/public/src/repo_new_local.mjs";
-import { git_ac_call_folder } from "../../../love/public/src/git_ac_call_folder.mjs";
-import { command_line_folder } from "../../../love/public/src/command_line_folder.mjs";
 export async function repo_new(repo_name) {
   let folder = await repo_new_local(repo_name);
-  let r3 = await git_init_folder(folder);
-  await git_ac_call_folder(repo_new.name, [repo_name], folder);
-  let r = await command_line_folder(
-    "gh repo create " +
-      repo_name +
-      " --public --source=. --remote=origin --push",
-    folder,
-  );
+  await git_repo_new(folder, repo_name);
   return folder;
 }
