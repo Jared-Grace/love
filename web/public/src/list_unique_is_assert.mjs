365e287494438ec2922869857918ed794ed0d69a
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 20:22:39 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

---
+++ web/public/src/list_unique_is_assert.mjs
@@ -0,0 +1,7 @@
+import { assert } from "../../../love/public/src/assert.mjs";
+import { list_unique_is } from "../../../love/public/src/list_unique_is.mjs";
+export function list_unique_is_assert(mapped) {
+  let a = list_unique_is(mapped);
+  assert(a);
+  return a;
+}
