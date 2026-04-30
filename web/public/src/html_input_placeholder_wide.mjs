16d606a6d3aabc40d6b89c325810f8c33d01e447
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 20:14:12 -0400

    function_auto_path ../love/public/src/html_input_placeholder_wide.mjs

--- web/public/src/html_input_placeholder_wide.mjs
+++ web/public/src/html_input_placeholder_wide.mjs
@@ -3,4 +3,5 @@ import { html_input_wide } from "../../../love/public/src/html_input_wide.mjs";
 export function html_input_placeholder_wide(root, placeholder) {
   let input = html_input_wide(root);
   html_placeholder(input, placeholder);
+  return input;
 }
