2847cdcce5d9e2d97f78e2173f3ffc1e32e849c9
JESUS rose to life <username@users.noreply.github.com>
2026-04-29 20:22:45 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- public/src/app_calendar_main.mjs
+++ public/src/app_calendar_main.mjs
@@ -1,5 +1,4 @@
 import { list_unique_is_assert } from "../../../love/public/src/list_unique_is_assert.mjs";
-import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { object_values } from "../../../love/public/src/object_values.mjs";
 import { app_calendar_upload } from "../../../love/public/src/app_calendar_upload.mjs";
 import { html_value_get } from "../../../love/public/src/html_value_get.mjs";
@@ -53,7 +52,6 @@ export async function app_calendar_main(context) {
     list_add(contacts, result);
     let mapped = list_map(contacts, object_values);
     let a = list_unique_is_assert(mapped);
-    assert_json_get(a, lambda6);
     let r2 = await app_api_fn({
       fn: app_calendar_upload,
       args: [data],
