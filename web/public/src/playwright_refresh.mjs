163c207fe73682e9b418a79599ad4119d4e5883c
JESUS rose to life <username@users.noreply.github.com>
2026-05-01 19:18:09 -0400

    function_auto_path ../love/public/src/playwright_refresh.mjs

--- web/public/src/playwright_refresh.mjs
+++ web/public/src/playwright_refresh.mjs
@@ -2,8 +2,10 @@ export async function playwright_refresh(page) {
   const context = page.context();
   await context.clearCookies();
   function lambda() {
-    localStorage.clear();
-    sessionStorage.clear();
+    try {
+      localStorage.clear();
+      sessionStorage.clear();
+    } catch {}
   }
   await page.evaluate(lambda);
   await page.reload();
