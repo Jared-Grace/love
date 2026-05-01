7d9d160042928595a8028ffba6eeda29959a2707
JESUS rose to life <username@users.noreply.github.com>
2026-05-01 19:22:39 -0400

    function_auto_path ../love/public/src/playwright_test_url.mjs

--- public/src/playwright_test_url.mjs
+++ public/src/playwright_test_url.mjs
@@ -1,15 +1,10 @@
-import { global_function_async } from "../../../love/public/src/global_function_async.mjs";
 import { chromium } from "playwright";
 export async function playwright_test_url(url, lambda) {
-  async function lambda3() {
-    const browser = await chromium.launch({
-      headless: false,
-    });
-    return browser;
-  }
-  let awaited = await global_function_async(playwright_test_url, lambda3);
+  const browser = await chromium.launch({
+    headless: false,
+  });
   try {
-    const page = await awaited.newPage();
+    const page = await browser.newPage();
     await page.goto(url);
     await lambda(page);
   } finally {
