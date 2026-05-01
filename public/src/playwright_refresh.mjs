044b52e864bd5f054bc38569f1615b01d1009c1e
JESUS rose to life <username@users.noreply.github.com>
2026-05-01 19:30:38 -0400

    function_auto_path ../love/public/src/playwright_refresh.mjs

--- public/src/playwright_refresh.mjs
+++ public/src/playwright_refresh.mjs
@@ -1,13 +1,17 @@
 export async function playwright_refresh(page) {
   const context = page.context();
   await context.clearCookies();
-  function lambda() {
-    localStorage.clear();
-    sessionStorage.clear();
-  }
-  await page.evaluate(lambda);
-  await page.reload({
+  let v = page.url();
+  await page.goto(v, {
     waitUntil: "domcontentloaded",
     timeout: 15000,
   });
+  function lambda() {
+    try {
+      localStorage.clear();
+      sessionStorage.clear();
+    } catch {}
+  }
+  await page.evaluate(lambda);
+  await page.waitForTimeout(300);
 }
