24fdd060db7d40c8f1a134851808ae139c71ba0e
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 00:11:27 -0400

    function_auto_path ../love/public/src/function_delete_if_exists.mjs

--- public/src/function_delete_if_exists.mjs
+++ public/src/function_delete_if_exists.mjs
@@ -1,9 +1,7 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { function_unalias_exists } from "../../../love/public/src/function_unalias_exists.mjs";
+import { function_unalias_exists_only } from "../../../love/public/src/function_unalias_exists_only.mjs";
 import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function function_delete_if_exists(f_name) {
-  const v = await function_unalias_exists(f_name);
-  let exists = property_get(v, "exists");
+  let exists = await function_unalias_exists_only(f_name);
   if (exists) {
     await function_delete(f_name);
   }
