6f8d25aff9acb92ca46573e3a34195f3ad346294
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:54:15 -0400

    pnef app_replace_tests_run url_prefix

--- public/src/app_replace_deploy_inner.mjs
+++ public/src/app_replace_deploy_inner.mjs
@@ -5,6 +5,6 @@ import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app
 export async function app_replace_deploy_inner() {
   "there is a potential rename, so these functions are ran after";
   await app_replace_rule_sets_why_generate();
-  await app_replace_tests_run();
+  await app_replace_tests_run(error());
   await html_update_latest_promote_deploy_app_fn(app_replace);
 }
