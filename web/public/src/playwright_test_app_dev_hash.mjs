d044b08fdd11a7d9c633112d2f1b83f56ece162b
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:41:08 -0400

    function_auto_path ../love/public/src/playwright_test_app_dev_hash.mjs

--- web/public/src/playwright_test_app_dev_hash.mjs
+++ web/public/src/playwright_test_app_dev_hash.mjs
@@ -1,10 +1,8 @@
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_shared_url_dev_hash } from "../../../love/public/src/app_shared_url_dev_hash.mjs";
 export async function playwright_test_app_dev_hash() {
   let url = await app_shared_url_dev_hash(app_replace, {
     d: 111,
   });
-  let combined = text_combine(url_prefix, url);
-  return combined;
+  return url;
 }
