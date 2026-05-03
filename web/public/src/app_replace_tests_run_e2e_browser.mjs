0ad64211d8379b9d0e6ca8df5c21ae1ab442aa84
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:38:04 -0400

    pne playwright_test_app_dev url_prefix

--- web/public/src/app_replace_tests_run_e2e_browser.mjs
+++ web/public/src/app_replace_tests_run_e2e_browser.mjs
@@ -5,5 +5,5 @@ export async function app_replace_tests_run_e2e_browser(rule_set, goal, inner) {
   async function lambda(page) {
     await app_replace_tests_run_e2e_goal(page, goal, rule_set, inner);
   }
-  await playwright_test_app_dev(app_replace, lambda);
+  await playwright_test_app_dev(app_replace, lambda, error());
 }
