7292e4e91b8d5986e91c062c9ca608069e580793
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:45:27 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_browser.mjs

--- web/public/src/app_replace_tests_run_e2e_browser.mjs
+++ web/public/src/app_replace_tests_run_e2e_browser.mjs
@@ -6,6 +6,6 @@ export async function app_replace_tests_run_e2e_browser(rule_set, goal, inner) {
   async function lambda(page) {
     await app_replace_tests_run_e2e_goal(page, goal, rule_set, inner);
   }
-  let url_prefix = await app_replace_url_dev();
-  await playwright_test_app_dev(url_prefix, app_replace, lambda);
+  let url = await app_replace_url_dev();
+  await playwright_test_app_dev(url, app_replace, lambda);
 }
