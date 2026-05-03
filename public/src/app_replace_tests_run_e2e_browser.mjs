8ac3f219cb5fffa7f500e6115cbfd0af5b4a246a
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:39:58 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_browser.mjs

--- public/src/app_replace_tests_run_e2e_browser.mjs
+++ public/src/app_replace_tests_run_e2e_browser.mjs
@@ -1,4 +1,4 @@
-import { error } from "../../../love/public/src/error.mjs";
+import { server_url } from "../../../love/public/src/server_url.mjs";
 import { app_replace_tests_run_e2e_goal } from "../../../love/public/src/app_replace_tests_run_e2e_goal.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
@@ -6,6 +6,6 @@ export async function app_replace_tests_run_e2e_browser(rule_set, goal, inner) {
   async function lambda(page) {
     await app_replace_tests_run_e2e_goal(page, goal, rule_set, inner);
   }
-  let url_prefix = error();
+  let url_prefix = server_url();
   await playwright_test_app_dev(url_prefix, app_replace, lambda);
 }
