51740731dde54aa147ab6102e31adce77f7dc973
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:44:22 -0400

    function_auto_path ../love/public/src/app_replace_deploy_inner.mjs

--- web/public/src/app_replace_deploy_inner.mjs
+++ web/public/src/app_replace_deploy_inner.mjs
@@ -1,3 +1,4 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_replace_url_suffix_dev_hash } from "../../../love/public/src/app_replace_url_suffix_dev_hash.mjs";
 import { app_replace_tests_run_e2e_deployed } from "../../../love/public/src/app_replace_tests_run_e2e_deployed.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
@@ -10,6 +11,7 @@ export async function app_replace_deploy_inner() {
   await app_replace_rule_sets_why_generate();
   let url_prefix = server_url();
   let url_suffix = await app_replace_url_suffix_dev_hash();
+  let combined = text_combine(left, right);
   await app_replace_tests_run(url_prefix);
   await html_update_latest_promote_deploy_app_fn(app_replace);
   await app_replace_tests_run_e2e_deployed();
