ae874ffe67584de46136b40b756ed8d2c521e0c1
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:43:33 -0400

    function_auto_path ../love/public/src/app_shared_url_suffix_dev_hash.mjs

--- public/src/app_replace_url_suffix_dev_hash.mjs
+++ public/src/app_replace_url_suffix_dev_hash.mjs
@@ -1,7 +1,7 @@
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
-import { app_shared_url_dev_hash } from "../../../love/public/src/app_shared_url_dev_hash.mjs";
+import { app_shared_url_suffix_dev_hash } from "../../../love/public/src/app_shared_url_suffix_dev_hash.mjs";
 export async function app_replace_url_suffix_dev_hash() {
-  let url = await app_shared_url_dev_hash(app_replace, {
+  let url = await app_shared_url_suffix_dev_hash(app_replace, {
     d: 111,
   });
   return url;
