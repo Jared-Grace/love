6dacf19947ee8a38fe8da2a64540c1d4f466f1c1
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:54:17 -0400

    function_auto_path ../love/public/src/app_replace_deploy_inner.mjs

--- public/src/app_replace_deploy_inner.mjs
+++ public/src/app_replace_deploy_inner.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { html_update_latest_promote_deploy_app_fn } from "../../../love/public/src/html_update_latest_promote_deploy_app_fn.mjs";
 import { app_replace_tests_run } from "../../../love/public/src/app_replace_tests_run.mjs";
@@ -5,6 +6,7 @@ import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app
 export async function app_replace_deploy_inner() {
   "there is a potential rename, so these functions are ran after";
   await app_replace_rule_sets_why_generate();
-  await app_replace_tests_run(error());
+  let url_prefix = error();
+  await app_replace_tests_run(url_prefix);
   await html_update_latest_promote_deploy_app_fn(app_replace);
 }
