0345525ea0ee80312ca8ab5c9fbebed3f0529e5f
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:49:38 -0400

    function_auto_path ../love/public/src/app_shared_url_suffix_hash.mjs

--- public/src/app_shared_url_suffix_hash.mjs
+++ public/src/app_shared_url_suffix_hash.mjs
@@ -1,3 +1,4 @@
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { text_slash_forward } from "../../../love/public/src/text_slash_forward.mjs";
 import { file_name_html } from "../../../love/public/src/file_name_html.mjs";
 import { app_shared_name_search_info_fn } from "../../../love/public/src/app_shared_name_search_info_fn.mjs";
@@ -9,5 +10,6 @@ export async function app_shared_url_suffix_hash(app_fn, hash) {
   let a_name = property_get(r, "a_name");
   let file_name = file_name_html(a_name);
   let s = text_slash_forward();
+  let combined = text_combine_multiple(list);
   return suffix;
 }
