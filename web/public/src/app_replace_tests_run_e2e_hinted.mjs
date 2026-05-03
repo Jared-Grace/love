73a2afb810682e5d0401096fa5a08485b71f537f
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:50:14 -0400

    functions_param_new_multiple_error_first app_replace_tests_run_e2e_next,app_replace_tests_run_e2e_all url_prefix

--- web/public/src/app_replace_tests_run_e2e_hinted.mjs
+++ web/public/src/app_replace_tests_run_e2e_hinted.mjs
@@ -1,8 +1,7 @@
 import { app_replace_tests_run_e2e_hinted_fn } from "../../../love/public/src/app_replace_tests_run_e2e_hinted_fn.mjs";
 import { app_replace_tests_run_e2e_all } from "../../../love/public/src/app_replace_tests_run_e2e_all.mjs";
 export async function app_replace_tests_run_e2e_hinted() {
-  await app_replace_tests_run_e2e_all(
-    [app_replace_tests_run_e2e_hinted_fn],
-    error(),
-  );
+  await app_replace_tests_run_e2e_all(error(), [
+    app_replace_tests_run_e2e_hinted_fn,
+  ]);
 }
