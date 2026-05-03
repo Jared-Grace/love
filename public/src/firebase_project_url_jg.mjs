e97b721bf2c8f08a3c30c11d4db6f9955a3e74f7
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:07:36 -0400

    function_auto_path ../love/public/src/firebase_project_url_jg.mjs

--- public/src/firebase_project_url_jg.mjs
+++ public/src/firebase_project_url_jg.mjs
@@ -1,6 +1,6 @@
 import { firebase_project_url } from "../../../love/public/src/firebase_project_url.mjs";
 import { firebase_project_name_jg } from "../../../love/public/src/firebase_project_name_jg.mjs";
-export function firebase_url_jg() {
+export function firebase_project_url_jg() {
   let project_id = firebase_project_name_jg();
   let combined = firebase_project_url(project_id);
   return combined;
