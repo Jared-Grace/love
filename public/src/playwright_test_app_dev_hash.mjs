9fe96bb740515594ce6b5e6abb40b6660418cc11
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:40:27 -0400

    pnef playwright_test_app_dev_hash url_prefix

--- public/src/playwright_test_app_dev_hash.mjs
+++ public/src/playwright_test_app_dev_hash.mjs
@@ -1,6 +1,11 @@
 import { app_shared_url_dev_hash } from "../../../love/public/src/app_shared_url_dev_hash.mjs";
 import { playwright_test_url } from "../../../love/public/src/playwright_test_url.mjs";
-export async function playwright_test_app_dev_hash(app_fn, hash, lambda) {
+export async function playwright_test_app_dev_hash(
+  url_prefix,
+  app_fn,
+  hash,
+  lambda,
+) {
   let url = await app_shared_url_dev_hash(app_fn, hash);
   await playwright_test_url(url, lambda);
 }
