d50cea3bd10733e07ce4653af7aa215cd87d9395
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:43:29 -0400

    function_auto_path ../love/public/src/function_param_new_error.mjs

--- web/public/src/function_param_new_error.mjs
+++ web/public/src/function_param_new_error.mjs
@@ -1,7 +1,6 @@
-import { js_code_call } from "../../../love/public/src/js_code_call.mjs";
+import { function_param_new_error_value_default } from "../../../love/public/src/function_param_new_error_value_default.mjs";
 import { function_params_new } from "../../../love/public/src/function_params_new.mjs";
-import { error } from "./error.mjs";
 export async function function_param_new_error(f_name, param_name) {
-  let values_default_comma = js_code_call(error.name);
+  let values_default_comma = function_param_new_error_value_default();
   await function_params_new(f_name, param_name, values_default_comma);
 }
