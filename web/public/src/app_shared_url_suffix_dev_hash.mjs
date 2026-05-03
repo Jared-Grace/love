34d610004794aaed2da77107f2d2262a5dade33d
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:47:32 -0400

    function_auto_path ../love/public/src/app_shared_url_suffix_dev_hash.mjs

--- web/public/src/app_shared_url_suffix_dev_hash.mjs
+++ web/public/src/app_shared_url_suffix_dev_hash.mjs
@@ -1,13 +1,11 @@
+import { app_shared_name_search_info_fn } from "../../../love/public/src/app_shared_name_search_info_fn.mjs";
 import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
-import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
 import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
 export async function app_shared_url_suffix_dev_hash(app_fn, hash) {
   let url2 = hash_to_url(hash);
-  let without = app_prefix_without_fn(app_fn);
-  let r = await app_shared_name_search_info(without);
+  let r = await app_shared_name_search_info_fn(app_fn);
   let f_path_dev = property_get(r, "f_path_dev");
   let previous = folder_previous();
   let together = text_prefix_without(f_path_dev, previous);
