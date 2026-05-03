0441db3e1d29edc6a1fd6bed4d9951f6f1ecfe7e
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:46:16 -0400

    w c functions_param_new_multiple_error,first

---
+++ public/src/functions_param_new_multiple_error_first.mjs
@@ -0,0 +1,7 @@
+import { functions_param_new_multiple_error } from "../../../love/public/src/functions_param_new_multiple_error.mjs";
+export async function functions_param_new_multiple_error_first(
+  f_names_comma,
+  param_names,
+) {
+  return await functions_param_new_multiple_error(f_names_comma, param_names);
+}
