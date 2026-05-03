7a7973e987cdedced47a0d2d9063be32f325129f
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:43:58 -0400

    function_auto_path ../love/public/src/app_replace_deploy_inner.mjs

--- public/src/app_replace_deploy_inner.mjs
+++ public/src/app_replace_deploy_inner.mjs
@@ -1,3 +1,4 @@
+import { app_replace_url_suffix_dev_hash } from "../../../love/public/src/app_replace_url_suffix_dev_hash.mjs";
 import { app_replace_tests_run_e2e_deployed } from "../../../love/public/src/app_replace_tests_run_e2e_deployed.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { html_update_latest_promote_deploy_app_fn } from "../../../love/public/src/html_update_latest_promote_deploy_app_fn.mjs";
@@ -8,6 +9,7 @@ export async function app_replace_deploy_inner() {
   "there is a potential rename, so these functions are ran after";
   await app_replace_rule_sets_why_generate();
   let url_prefix = server_url();
+  let url = await app_replace_url_suffix_dev_hash();
   await app_replace_tests_run(url_prefix);
   await html_update_latest_promote_deploy_app_fn(app_replace);
   await app_replace_tests_run_e2e_deployed();
