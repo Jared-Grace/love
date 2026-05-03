5b958afc7ba6f4603280ca65d10e1014253b16ef
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:47:05 -0400

    function_auto_path ../love/public/src/functions_param_new_multiple_error_first.mjs

--- web/public/src/functions_param_new_multiple_error_first.mjs
+++ web/public/src/functions_param_new_multiple_error_first.mjs
@@ -1,12 +1,10 @@
-import { list_size_1_assert } from "../../../love/public/src/list_size_1_assert.mjs";
-import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
+import { text_split_comma_size_1_assert } from "../../../love/public/src/text_split_comma_size_1_assert.mjs";
 import { functions_param_new_multiple_error } from "../../../love/public/src/functions_param_new_multiple_error.mjs";
 export async function functions_param_new_multiple_error_first(
   f_names_comma,
   param_names,
 ) {
-  let split = text_split_comma(param_names);
-  list_size_1_assert(list);
+  text_split_comma_size_1_assert(param_names);
   let r = await functions_param_new_multiple_error(f_names_comma, param_names);
   return r;
 }
