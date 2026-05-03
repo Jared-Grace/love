3c44b6e3fddddbf11eab3a1f5a4b8d706d5b3313
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:49:05 -0400

    function_auto_path ../love/public/src/app_shared_url_suffix_hash.mjs

--- public/src/app_shared_url_suffix_hash.mjs
+++ public/src/app_shared_url_suffix_hash.mjs
@@ -1,14 +1,9 @@
 import { app_shared_name_search_info_fn } from "../../../love/public/src/app_shared_name_search_info_fn.mjs";
-import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
-import { folder_previous } from "../../../love/public/src/folder_previous.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { hash_to_url } from "../../../love/public/src/hash_to_url.mjs";
 export async function app_shared_url_suffix_hash(app_fn, hash) {
   let hash_url = hash_to_url(hash);
   let r = await app_shared_name_search_info_fn(app_fn);
   let a_name = property_get(r, "a_name");
-  let previous = folder_previous();
-  let together = text_prefix_without(f_path_dev, previous);
-  const suffix = together + "" + hash_url;
   return suffix;
 }
