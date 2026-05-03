42387a18b66654dcbd67fd726c175c8909f6fafd
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:50:10 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_next.mjs

--- web/public/src/app_replace_tests_run_e2e_next.mjs
+++ web/public/src/app_replace_tests_run_e2e_next.mjs
@@ -5,7 +5,7 @@ import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/ap
 import { app_replace_tests_run_e2e_goal } from "../../../love/public/src/app_replace_tests_run_e2e_goal.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_take } from "../../../love/public/src/list_take.mjs";
-export async function app_replace_tests_run_e2e_next() {
+export async function app_replace_tests_run_e2e_next(url_prefix) {
   "it took a long time to run all tests in browser in a row - instead, run all tests in parallel, and this function tests the first two rule sets so that next-ing is tested";
   let rule_sets = app_replace_rule_sets();
   let taken = list_take(rule_sets, 2);
