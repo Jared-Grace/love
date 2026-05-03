35ffe3cebf37c405f992b103ae08b136b93f0364
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:51:36 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_deployed.mjs

--- public/src/app_replace_tests_run_e2e_deployed.mjs
+++ public/src/app_replace_tests_run_e2e_deployed.mjs
@@ -5,9 +5,9 @@ import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_replace_tests_run_e2e } from "../../../love/public/src/app_replace_tests_run_e2e.mjs";
 import { firebase_project_url_jg } from "../../../love/public/src/firebase_project_url_jg.mjs";
 export async function app_replace_tests_run_e2e_deployed() {
-  let combined = firebase_project_url_jg();
+  let url_prefix = firebase_project_url_jg();
   let hash = app_replace_test_url_hash_default();
   let url_suffix = await app_shared_url_suffix_hash(app_replace, hash);
-  let url_prefix = text_combine(combined, url_suffix);
-  await app_replace_tests_run_e2e(combined);
+  let url = text_combine(url_prefix, url_suffix);
+  await app_replace_tests_run_e2e(url);
 }
