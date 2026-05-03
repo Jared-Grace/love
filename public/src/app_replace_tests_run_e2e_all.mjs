be2a788a7e1f8f3164f4bfccd22d9c0f58d18049
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:36:52 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_all.mjs

--- public/src/app_replace_tests_run_e2e_all.mjs
+++ public/src/app_replace_tests_run_e2e_all.mjs
@@ -1,5 +1,5 @@
+import { playwright_test_url } from "../../../love/public/src/playwright_test_url.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
-import { app_replace_test_e2e } from "../../../love/public/src/app_replace_test_e2e.mjs";
 import { app_replace_rule_set_binary_counting } from "../../../love/public/src/app_replace_rule_set_binary_counting.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
 import { each_range_unordered_async } from "../../../love/public/src/each_range_unordered_async.mjs";
@@ -42,7 +42,7 @@ export async function app_replace_tests_run_e2e_all(url_prefix, e2e_inner_fns) {
       }
       await list_empty_not_is_while_async(remaining, while_non_empty);
     }
-    await app_replace_test_e2e(url_prefix, on_page);
+    await playwright_test_url(url_prefix, on_page);
   }
   await each_range_unordered_async(parallel_count, lambda);
 }
