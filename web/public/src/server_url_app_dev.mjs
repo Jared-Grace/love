44f1b379b4c4371f175570febc257dccd27cffcc
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:34:08 -0400

    function_auto_path ../love/public/src/app_shared_url_dev_hash.mjs

--- web/public/src/server_url_app_dev.mjs
+++ web/public/src/server_url_app_dev.mjs
@@ -1,6 +1,6 @@
-import { server_url_app_dev_hash } from "../../../love/public/src/server_url_app_dev_hash.mjs";
+import { app_shared_url_dev_hash } from "../../../love/public/src/app_shared_url_dev_hash.mjs";
 export async function server_url_app_dev(app_fn) {
   const hash = {};
-  const url = await server_url_app_dev_hash(app_fn, hash);
+  const url = await app_shared_url_dev_hash(app_fn, hash);
   return url;
 }
