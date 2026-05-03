b3d41db77fbc47ad38db8858257969d435400bc4
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:39:34 -0400

    function_auto_path ../love/public/src/playwright_test_app_dev_hash.mjs

--- web/public/src/playwright_test_app_dev_hash.mjs
+++ web/public/src/playwright_test_app_dev_hash.mjs
@@ -1,12 +1,7 @@
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_shared_url_dev_hash } from "../../../love/public/src/app_shared_url_dev_hash.mjs";
-export async function playwright_test_app_dev_hash(
-  url_prefix,
-  app_fn,
-  hash,
-  lambda,
-) {
+export async function playwright_test_app_dev_hash() {
   let url = await app_shared_url_dev_hash(app_replace, {
     d: 111,
   });
