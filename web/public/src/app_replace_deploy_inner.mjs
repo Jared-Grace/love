ed32fcf439cc66a5ca1cc5df9cb975a88c13ae70
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:44:54 -0400

    function_auto_path ../love/public/src/app_replace_deploy_inner.mjs

--- web/public/src/app_replace_deploy_inner.mjs
+++ web/public/src/app_replace_deploy_inner.mjs
@@ -1,17 +1,13 @@
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
-import { app_replace_url_suffix_dev_hash } from "../../../love/public/src/app_replace_url_suffix_dev_hash.mjs";
+import { app_replace_url_dev } from "../../../love/public/src/app_replace_url_dev.mjs";
 import { app_replace_tests_run_e2e_deployed } from "../../../love/public/src/app_replace_tests_run_e2e_deployed.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { html_update_latest_promote_deploy_app_fn } from "../../../love/public/src/html_update_latest_promote_deploy_app_fn.mjs";
 import { app_replace_tests_run } from "../../../love/public/src/app_replace_tests_run.mjs";
 import { app_replace_rule_sets_why_generate } from "../../../love/public/src/app_replace_rule_sets_why_generate.mjs";
-import { server_url } from "../../../love/public/src/server_url.mjs";
 export async function app_replace_deploy_inner() {
   "there is a potential rename, so these functions are ran after";
   await app_replace_rule_sets_why_generate();
-  let url_prefix = server_url();
-  let url_suffix = await app_replace_url_suffix_dev_hash();
-  let combined = text_combine(url_prefix, url_suffix);
+  let combined = await app_replace_url_dev();
   await app_replace_tests_run(combined);
   await html_update_latest_promote_deploy_app_fn(app_replace);
   await app_replace_tests_run_e2e_deployed();
