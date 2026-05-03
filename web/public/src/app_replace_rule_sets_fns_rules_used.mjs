4092e06487271ebaaf0097b3673dd892a727cefb
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 00:13:36 -0400

    function_auto_path ../love/public/src/app_replace_rule_sets_fns_rules_used.mjs

--- web/public/src/app_replace_rule_sets_fns_rules_used.mjs
+++ web/public/src/app_replace_rule_sets_fns_rules_used.mjs
@@ -1,5 +1,5 @@
 export function app_replace_rule_sets_fns_rules_used() {
-  return {
+  let r = {
     "Binary Counting Prepare 1": [
       [
         {
@@ -8143,4 +8143,5 @@ export function app_replace_rule_sets_fns_rules_used() {
       ],
     ],
   };
+  return r;
 }
