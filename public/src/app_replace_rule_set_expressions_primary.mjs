efd863ff6d95f604e456bd96497bb777357af06e
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 00:17:07 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_expressions_primary.mjs

--- public/src/app_replace_rule_set_expressions_primary.mjs
+++ public/src/app_replace_rule_set_expressions_primary.mjs
@@ -41,7 +41,7 @@ export function app_replace_rule_set_expressions_primary() {
       },
       {
         start: "pe",
-        end: '" luv "',
+        end: '"luv"',
       },
       {
         start: "pe",
