85c1fe59cc48d6e752cc772978451282bd0e1862
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:03:41 -0400

    function_auto_path ../love/public/src/firebase_url_jg.mjs

--- web/public/src/firebase_url_jg.mjs
+++ web/public/src/firebase_url_jg.mjs
@@ -3,7 +3,7 @@ import { https_prefix } from "../../../love/public/src/https_prefix.mjs";
 import { firebase_project_name_jg } from "../../../love/public/src/firebase_project_name_jg.mjs";
 export function firebase_url_jg() {
   let project_id = firebase_project_name_jg();
-  let r2 = https_prefix();
-  let combined = text_combine_multiple([r2, project_id, ".web.app"]);
+  let prefix = https_prefix();
+  let combined = text_combine_multiple([prefix, project_id, ".web.app"]);
   return combined;
 }
