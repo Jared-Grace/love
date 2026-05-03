45ebc902f14e7b2ffefe21d4e12757a5017db39f
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 16:46:18 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_deployed.mjs

--- public/src/app_replace_tests_run_e2e_deployed.mjs
+++ public/src/app_replace_tests_run_e2e_deployed.mjs
@@ -1,6 +1,8 @@
+import { app_replace_url_dev } from "../../../love/public/src/app_replace_url_dev.mjs";
 import { app_replace_tests_run_e2e } from "../../../love/public/src/app_replace_tests_run_e2e.mjs";
 import { firebase_project_url_jg } from "../../../love/public/src/firebase_project_url_jg.mjs";
 export async function app_replace_tests_run_e2e_deployed() {
   let combined = firebase_project_url_jg();
+  let url_prefix = await app_replace_url_dev();
   await app_replace_tests_run_e2e(combined);
 }
