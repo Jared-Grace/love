3575733b1b3dddd209da2888b566f674777d6033
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:40:29 -0400

    function_auto_path ../love/public/src/app_replace_test_e2e.mjs

--- public/src/app_replace_test_e2e.mjs
+++ public/src/app_replace_test_e2e.mjs
@@ -1,12 +1,14 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 import { playwright_test_app_dev_hash } from "../../../love/public/src/playwright_test_app_dev_hash.mjs";
 export async function app_replace_test_e2e(on_page) {
+  let lambda = error();
   await playwright_test_app_dev_hash(
-    error(),
     app_replace,
     {
       d: 111,
     },
     on_page,
+    lambda,
   );
 }
