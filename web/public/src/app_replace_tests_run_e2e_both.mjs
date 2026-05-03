2410c9aa7995be83088cdfc1a4316372b320a287
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 15:51:23 -0400

    function_auto_path ../love/public/src/app_replace_tests_run_e2e_both.mjs

--- web/public/src/app_replace_tests_run_e2e_both.mjs
+++ web/public/src/app_replace_tests_run_e2e_both.mjs
@@ -1,4 +1,3 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { app_replace_tests_run_e2e_all } from "../../../love/public/src/app_replace_tests_run_e2e_all.mjs";
 import { app_replace_tests_run_e2e_hinted_fn } from "../../../love/public/src/app_replace_tests_run_e2e_hinted_fn.mjs";
 import { app_replace_tests_run_e2e_normal_fn } from "../../../love/public/src/app_replace_tests_run_e2e_normal_fn.mjs";
@@ -7,6 +6,5 @@ export async function app_replace_tests_run_e2e_both() {
     app_replace_tests_run_e2e_normal_fn,
     app_replace_tests_run_e2e_hinted_fn,
   ];
-  let url_prefix = error();
   await app_replace_tests_run_e2e_all(url_prefix, e2e_inner_fns);
 }
