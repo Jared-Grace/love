f5ce88ad5554e705c36ee5f8323c2996065701f0
JESUS rose to life <username@users.noreply.github.com>
2026-05-03 00:12:06 -0400

    function_auto_path ../love/public/src/function_new_if_exists.mjs

--- web/public/src/function_new_if_exists.mjs
+++ web/public/src/function_new_if_exists.mjs
@@ -1,8 +1,9 @@
+import { function_new } from "../../../love/public/src/function_new.mjs";
+import { not } from "../../../love/public/src/not.mjs";
 import { function_unalias_exists_only } from "../../../love/public/src/function_unalias_exists_only.mjs";
-import { function_delete } from "../../../love/public/src/function_delete.mjs";
 export async function function_new_if_exists(f_name) {
   let exists = await function_unalias_exists_only(f_name);
-  if (exists) {
-    await function_delete(f_name);
+  if (not(exists)) {
+    await function_new(f_name);
   }
 }
