5ff929ab6d7c8314d08193dcb9a6bac38f16f290
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:38:34 -0400

    function_auto_path ../love/public/src/folder_user_docs_read.mjs

--- public/src/folder_user_docs_read.mjs
+++ public/src/folder_user_docs_read.mjs
@@ -1,7 +1,7 @@
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
-export async function folder_user_docs_read(input) {
-  let file_path = folder_user_docs_path(input);
+export async function folder_user_docs_read(file_name) {
+  let file_path = folder_user_docs_path(file_name);
   let contents = await file_read(file_path);
   return contents;
 }
