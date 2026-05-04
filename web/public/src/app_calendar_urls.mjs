ae1338e38baac41bfc20f4c2d9e732301c05dc50
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:48:58 -0400

    function_auto_path ../love/public/src/app_calendar_urls.mjs

--- web/public/src/app_calendar_urls.mjs
+++ web/public/src/app_calendar_urls.mjs
@@ -6,6 +6,9 @@ export async function app_calendar_urls() {
   let split = await folder_user_docs_read_lines("preaching_ask.txt");
   let filtered = list_filter_empty_not_is(split);
   let filtered2 = list_filter_starts_with_https_prefix(filtered);
-  let mapped = list_map_prefix_without_try(filtered2, 'https://www.facebook.com/profile.php?id=');
+  let mapped = list_map_prefix_without_try(
+    filtered2,
+    "https://www.facebook.com/profile.php?id=",
+  );
   return mapped;
 }
