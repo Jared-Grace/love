43828e188d8417a04058634c6878fc45834567ef
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:46:30 -0400

    function_auto_path ../love/public/src/text_prefix_without_try.mjs

--- web/public/src/text_prefix_without_try.mjs
+++ web/public/src/text_prefix_without_try.mjs
@@ -1,11 +1,10 @@
 import { text_prefix_without_inner } from "../../../love/public/src/text_prefix_without_inner.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-import { error } from "../../../love/public/src/error.mjs";
 export function text_prefix_without_try(t, prefix) {
   let a = text_starts_with(t, prefix);
   if (not(a)) {
-    error();
+    return t;
   }
   let without = text_prefix_without_inner(t, prefix);
   return without;
