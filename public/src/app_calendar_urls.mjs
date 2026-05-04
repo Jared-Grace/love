d7d6fe65bab4edc0b45e1bd336d57e76fb9608fc
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:39:12 -0400

    function_auto_path ../love/public/src/app_calendar_urls.mjs

--- public/src/app_calendar_urls.mjs
+++ public/src/app_calendar_urls.mjs
@@ -1,4 +1,5 @@
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 export async function app_calendar_urls() {
   let split = await folder_user_docs_read_lines("preaching_ask.txt");
+  return split;
 }
