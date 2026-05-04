60df9de9279229db4b5a74cc0e501766b1e24c89
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:47:04 -0400

    function_auto_path ../love/public/src/text_starts_with_not.mjs

--- public/src/text_starts_with_not.mjs
+++ public/src/text_starts_with_not.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
-export function text_starts_with_not(item, book_code) {
-  let a = text_starts_with(item, book_code);
+export function text_starts_with_not(t, prefix) {
+  let a = text_starts_with(t, prefix);
   let sw = not(a);
   return sw;
 }
