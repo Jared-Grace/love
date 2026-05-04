7a2277b0976c99cbfba7240fb17d754929de5ad4
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:38:58 -0400

    function_auto_path ../love/public/src/app_calendar_urls.mjs

--- public/src/app_calendar_urls.mjs
+++ public/src/app_calendar_urls.mjs
@@ -1,4 +1,8 @@
+import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
-export function app_calendar_urls() {
+export async function app_calendar_urls() {
   let result = folder_user_docs_path(file_name);
+  let split = await folder_user_docs_read_lines(
+    "bible_references.hopenation.org.txt",
+  );
 }
