75c117f523add90f42285c24ff19bbcbfeb069ca
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:46:13 -0400

    c c text_prefix_without,try

---
+++ public/src/text_prefix_without_try.mjs
@@ -0,0 +1,21 @@
+import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
+import { text_prefix_without_inner } from "../../../love/public/src/text_prefix_without_inner.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
+import { error } from "../../../love/public/src/error.mjs";
+export function text_prefix_without_try(t, prefix) {
+  let a = text_starts_with(t, prefix);
+  function lambda() {
+    let r = {
+      s: t,
+      prefix,
+    };
+    return r;
+  }
+  assert_json_get(a, lambda);
+  if (not(a)) {
+    error();
+  }
+  let without = text_prefix_without_inner(t, prefix);
+  return without;
+}
