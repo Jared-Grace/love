19f8da5532772da9d64b71cbc3c39df4b247784f
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:48:07 -0400

    function_auto_path ../love/public/src/list_map_prefix_without_try.mjs

--- public/src/list_map_prefix_without_try.mjs
+++ public/src/list_map_prefix_without_try.mjs
@@ -1,8 +1,8 @@
+import { text_prefix_without_try } from "../../../love/public/src/text_prefix_without_try.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { text_prefix_without } from "../../../love/public/src/text_prefix_without.mjs";
 export function list_map_prefix_without_try(list, prefix) {
   function lambda(item) {
-    let result = text_prefix_without(item, prefix);
+    let result = text_prefix_without_try(item, prefix);
     return result;
   }
   let mapped = list_map(list, lambda);
