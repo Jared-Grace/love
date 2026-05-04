2e679d43bcbc908ebe74c5dcdc2ca78080d62eab
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:48:34 -0400

    function_auto_path ../love/public/src/app_calendar_urls.mjs

--- public/src/app_calendar_urls.mjs
+++ public/src/app_calendar_urls.mjs
@@ -1,3 +1,4 @@
+import { list_map_prefix_without_try } from "../../../love/public/src/list_map_prefix_without_try.mjs";
 import { list_filter_starts_with_https_prefix } from "../../../love/public/src/list_filter_starts_with_https_prefix.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
@@ -5,5 +6,6 @@ export async function app_calendar_urls() {
   let split = await folder_user_docs_read_lines("preaching_ask.txt");
   let filtered = list_filter_empty_not_is(split);
   let filtered2 = list_filter_starts_with_https_prefix(filtered);
+  let mapped = list_map_prefix_without_try(list, prefix);
   return filtered2;
 }
