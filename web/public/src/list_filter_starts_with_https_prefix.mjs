67b0120ce0d3906ea66f81712469dfb83ee8743c
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:43:42 -0400

    function_auto_path ../love/public/src/list_filter_starts_with_https_prefix.mjs

--- web/public/src/list_filter_starts_with_https_prefix.mjs
+++ web/public/src/list_filter_starts_with_https_prefix.mjs
@@ -1,6 +1,6 @@
 import { text_starts_with_https_prefix } from "../../../love/public/src/text_starts_with_https_prefix.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-export function list_filter_starts_with_https_prefix(filtered) {
-  let filtered3 = list_filter(filtered, text_starts_with_https_prefix);
-  return filtered3;
+export function list_filter_starts_with_https_prefix(list) {
+  let filtered = list_filter(list, text_starts_with_https_prefix);
+  return filtered;
 }
