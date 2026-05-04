f418e21edfff1ad6679d7c614749624143177fb3
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:39:42 -0400

    function_auto_path ../love/public/src/app_calendar_urls.mjs

--- web/public/src/app_calendar_urls.mjs
+++ web/public/src/app_calendar_urls.mjs
@@ -1,5 +1,7 @@
+import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 export async function app_calendar_urls() {
   let split = await folder_user_docs_read_lines("preaching_ask.txt");
+  let filtered = list_filter_empty_not_is(list);
   return split;
 }
