4465506f686ad125e4bc067f19893cd5c8c7acd7
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 19:37:15 -0400

    function_auto_path ../love/public/src/app_calendar_urls.mjs

--- web/public/src/app_calendar_urls.mjs
+++ web/public/src/app_calendar_urls.mjs
@@ -1,1 +1,4 @@
-export function app_calendar_urls() {}
+import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
+export function app_calendar_urls() {
+  let result = folder_user_docs_path(file_name);
+}
