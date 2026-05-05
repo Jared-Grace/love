7f0e8a3c86f51c31d56924ff09cdf932150ad068
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:24:41 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook.mjs

--- web/public/src/playwright_session_load_facebook.mjs
+++ web/public/src/playwright_session_load_facebook.mjs
@@ -1,3 +1,4 @@
+import { playwright_session_save_prompt } from "../../../love/public/src/playwright_session_save_prompt.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { app_calendar_url_ids } from "../../../love/public/src/app_calendar_url_ids.mjs";
 import { playwright_session_load } from "../../../love/public/src/playwright_session_load.mjs";
@@ -8,4 +9,5 @@ export async function playwright_session_load_facebook() {
   const session_name = playwright_session_save_facebook_name();
   const page = await playwright_session_load(session_name);
   await page.goto("https://www.m.me/" + first);
+  await playwright_session_save_prompt(session_name, context, browser);
 }
