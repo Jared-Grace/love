5ab9fc2376dc426d2ddb6be7c53dd126728bd9a8
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 20:53:04 -0400

    function_auto_path ../love/public/src/text_suffix_without.mjs

--- web/public/src/text_suffix_without.mjs
+++ web/public/src/text_suffix_without.mjs
@@ -1,10 +1,9 @@
+import { text_ends_with_not } from "../../../love/public/src/text_ends_with_not.mjs";
 import { text_suffix_without_inner } from "../../../love/public/src/text_suffix_without_inner.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 export function text_suffix_without(t, suffix) {
-  let a = text_ends_with(t, suffix);
-  if (not(a)) {
+  let n = text_ends_with_not(t, suffix);
+  if (n) {
     error();
   }
   const without = text_suffix_without_inner(t, suffix);
