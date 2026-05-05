8301aa8aef5f1d19c682d8ab164737a9b3c1f7e3
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 21:01:19 -0400

    function_auto_path ../love/public/src/playwright_session_save.mjs

--- public/src/playwright_session_save.mjs
+++ public/src/playwright_session_save.mjs
@@ -1,4 +1,5 @@
-import { folder_secret_join } from "../../../love/public/src/folder_secret_join.mjs";
+import { app_calendar } from "../../../love/public/src/app_calendar.mjs";
+import { folder_secret_join_json } from "../../../love/public/src/folder_secret_join_json.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 export async function playwright_session_save() {
   const browser = await chromium.launch({
@@ -12,7 +13,7 @@ export async function playwright_session_save() {
     let r = process.stdin.once("data", resolve);
     return r;
   });
-  let file_path = folder_secret_join(only);
+  let file_path = folder_secret_join_json(app_calendar.name);
   await context.storageState({
     path: "fb-session.json",
   });
