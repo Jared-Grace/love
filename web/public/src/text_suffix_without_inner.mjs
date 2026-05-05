3ab2fca9fabdf058a8e7627ddb59c69fe19352c6
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 20:52:08 -0400

    function_auto_path ../love/public/src/text_suffix_without.mjs

--- web/public/src/text_suffix_without_inner.mjs
+++ web/public/src/text_suffix_without_inner.mjs
@@ -1,6 +1,6 @@
 import { string_skip_end } from "../../../love/public/src/string_skip_end.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
-export function text_suffix_without_inner(suffix, t) {
+export function text_suffix_without_inner(t, suffix) {
   const missing = text_size(suffix);
   const without = string_skip_end(t, missing);
   return without;
