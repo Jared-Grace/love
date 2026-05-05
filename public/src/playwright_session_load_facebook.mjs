d2e7919c60de9b7e89168bf9b71072d5620d29bf
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:42:13 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- public/src/playwright_session_load_facebook.mjs
+++ public/src/playwright_session_load_facebook.mjs
@@ -9,6 +9,7 @@ import { playwright_session_load } from "../../../love/public/src/playwright_ses
 import { playwright_session_save_facebook_name } from "../../../love/public/src/playwright_session_save_facebook_name.mjs";
 export async function playwright_session_load_facebook() {
   let url_ids = await app_calendar_url_ids();
+    let file_path = folder_user_docs_path("preaching_ask.lookup.json");
   const session_name = playwright_session_save_facebook_name();
   const r = await playwright_session_load(session_name);
   let browser = property_get(r, "browser");
