8df69f881532786552d0d39cbb26589998f9ad00
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 19:19:34 -0400

    function_auto_path ../love/public/src/file_backup.mjs

--- web/public/src/file_backup.mjs
+++ web/public/src/file_backup.mjs
@@ -5,9 +5,9 @@ import { path_extension } from "../../../love/public/src/path_extension.mjs";
 import { path_without_extension } from "../../../love/public/src/path_without_extension.mjs";
 export async function file_backup(file_path) {
   let now_file = date_now_file();
-  let joined2 = list_join_dot(["", "backup", now_file]);
+  let joined = list_join_dot(["", "backup", now_file]);
   let p = path_without_extension(file_path);
   let extension = path_extension(file_path);
-  let combined = text_combine_multuple([p, joined2, extension]);
+  let combined = text_combine_multuple([p, joined, extension]);
   await file_copy(file_path, combined);
 }
