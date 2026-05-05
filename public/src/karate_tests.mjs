5b7237e56a401f27147233a378e91d5bd367a64c
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:36:14 -0400

    function_auto_path ../love/public/src/karate_tests.mjs

--- public/src/karate_tests.mjs
+++ public/src/karate_tests.mjs
@@ -1,9 +1,9 @@
+import { sleep_seconds } from "../../../love/public/src/sleep_seconds.mjs";
 import { app_replace_url_dev } from "../../../love/public/src/app_replace_url_dev.mjs";
 import { playwright_test_app_dev } from "../../../love/public/src/playwright_test_app_dev.mjs";
 import { portfolio_qa_attribute_test_data } from "../../../portfolio_qa/public/src/portfolio_qa_attribute_test_data.mjs";
 import { app_karate_main_title } from "../../../karate_code/public/src/app_karate_main_title.mjs";
 import { app_karate } from "../../../karate_code/public/src/app_karate.mjs";
-import { sleep } from "../../../love/public/src/sleep.mjs";
 import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 import { playwright_by_attribute_type } from "../../../love/public/src/playwright_by_attribute_type.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
@@ -14,7 +14,7 @@ export async function karate_tests() {
     const title_actual = await page.title();
     const title = app_karate_main_title();
     equal_assert(title_actual, title);
-    await sleep(10000);
+    await sleep_seconds(10);
     return;
     ("this assert is a smoke test");
     let name = portfolio_qa_attribute_test_data();
