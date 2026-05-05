a35ebc96556cbf96f7b42d26a237f21e04b9c1cd
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:20:39 -0400

    function_auto_path ../love/public/src/playwright_session_load.mjs

--- web/public/src/playwright_session_load.mjs
+++ web/public/src/playwright_session_load.mjs
@@ -1,10 +1,12 @@
+import { folder_secret_join_json } from "../../../love/public/src/folder_secret_join_json.mjs";
 import { playwright_session_save_facebook_name } from "../../../love/public/src/playwright_session_save_facebook_name.mjs";
 import { playwright_chromium_visible } from "../../../love/public/src/playwright_chromium_visible.mjs";
 export async function playwright_session_load() {
   const browser = await playwright_chromium_visible();
   const session_name = playwright_session_save_facebook_name();
+  let path = folder_secret_join_json(session_name);
   const context = await browser.newContext({
-    storageState: session_name,
+    storageState: path,
   });
   const page = await context.newPage();
   await page.goto("https://www.facebook.com/");
