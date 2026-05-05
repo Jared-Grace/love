73c72906b47660283d4aa6e61b4eefe4f2fe8b4e
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 19:21:29 -0400

    function_auto_path ../love/public/src/file_backup.mjs

--- public/src/file_backup.mjs
+++ public/src/file_backup.mjs
@@ -1,10 +1,8 @@
+import { file_backup_path } from "../../../love/public/src/file_backup_path.mjs";
 import { path_ending_add } from "../../../love/public/src/path_ending_add.mjs";
 import { file_copy } from "../../../love/public/src/file_copy.mjs";
-import { list_join_dot } from "../../../love/public/src/list_join_dot.mjs";
-import { date_now_file } from "../../../love/public/src/date_now_file.mjs";
 export async function file_backup(file_path) {
-  let now_file = date_now_file();
-  let ending = list_join_dot(["", "backup", now_file]);
+  let ending = file_backup_path();
   let combined = path_ending_add(file_path, ending);
   await file_copy(file_path, combined);
 }
