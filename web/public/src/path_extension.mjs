a9e568dc9d78f56b109d7de60fa951cf9de0571b
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 19:12:14 -0400

    function_auto_path ../love/public/src/path_extension.mjs

--- web/public/src/path_extension.mjs
+++ web/public/src/path_extension.mjs
@@ -1,1 +1,12 @@
-export function path_extension() {}
+export function path_extension(path) {
+  const lastDot = path.lastIndexOf(".");
+  let v = path.lastIndexOf("/");
+  let v2 = path.lastIndexOf("\\");
+  const lastSlash = Math.max(v, v2);
+  if (lastDot === -1 || lastDot < lastSlash) {
+    let r = "";
+    return r;
+  }
+  let r2 = path.slice(lastDot);
+  return r2;
+}
