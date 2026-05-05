f5e5b0d1cc3449a84a363b68a8991b22959114a9
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:44:14 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook.mjs

--- web/public/src/playwright_session_load_facebook.mjs
+++ web/public/src/playwright_session_load_facebook.mjs
@@ -25,7 +25,9 @@ export async function playwright_session_load_facebook() {
     const url = page.url();
     let prefix = "https://www.messenger.com/e2ee/t/";
     let without = text_prefix_without(url, prefix);
-    property_set_exists_not(object, property_name, value);
+    property_set_exists_not(lookup, url_id, {
+      conversation_id: without,
+    });
     console.log(url);
   }
   await each_async(url_ids, lambda);
