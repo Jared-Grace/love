8766a174c8801feda3e53a8fd001dc7b2c66941d
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 20:52:24 -0400

    function_auto_path ../love/public/src/list_map_suffix_without_try.mjs

--- public/src/list_map_suffix_without_try.mjs
+++ public/src/list_map_suffix_without_try.mjs
@@ -1,7 +1,7 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_suffix_without_try(list, prefix) {
   function lambda(item) {
-    let result = text_suffix_without_try(item, prefix);
+    let result = text_suffix_without_try1(item, prefix);
     return result;
   }
   let mapped = list_map(list, lambda);
