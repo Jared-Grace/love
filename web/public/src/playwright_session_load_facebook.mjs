6b54a82978a0e81ae6e13e963815661f1e8dd68b
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:34:08 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook.mjs

--- web/public/src/playwright_session_load_facebook.mjs
+++ web/public/src/playwright_session_load_facebook.mjs
@@ -1,13 +1,10 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { playwright_session_save_prompt } from "../../../love/public/src/playwright_session_save_prompt.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 import { app_calendar_url_ids } from "../../../love/public/src/app_calendar_url_ids.mjs";
 import { playwright_session_load } from "../../../love/public/src/playwright_session_load.mjs";
 import { playwright_session_save_facebook_name } from "../../../love/public/src/playwright_session_save_facebook_name.mjs";
 export async function playwright_session_load_facebook() {
   let url_ids = await app_calendar_url_ids();
-  let first = list_first(url_ids);
-  return first;
   const session_name = playwright_session_save_facebook_name();
   const r = await playwright_session_load(session_name);
   let browser = property_get(r, "browser");
