563a3b2e047397d14a74ea59865c29c444c41074
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:21:58 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook.mjs

--- web/public/src/playwright_session_load_facebook.mjs
+++ web/public/src/playwright_session_load_facebook.mjs
@@ -3,5 +3,5 @@ import { playwright_session_save_facebook_name } from "../../../love/public/src/
 export async function playwright_session_load_facebook() {
   const session_name = playwright_session_save_facebook_name();
   const page = await playwright_session_load(session_name);
-  await page.goto("https://www.facebook.com/");
+  await page.goto("https://www.m.me/");
 }
