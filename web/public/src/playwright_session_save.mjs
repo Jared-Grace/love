aadaf4c1985867bc6dda9151d9662e16eb16beaa
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:24:21 -0400

    function_auto_path ../love/public/src/playwright_session_save.mjs

--- web/public/src/playwright_session_save.mjs
+++ web/public/src/playwright_session_save.mjs
@@ -1,17 +1,9 @@
+import { playwright_session_save_prompt } from "../../../love/public/src/playwright_session_save_prompt.mjs";
 import { playwright_chromium_visible } from "../../../love/public/src/playwright_chromium_visible.mjs";
-import { command_line_read } from "../../../love/public/src/command_line_read.mjs";
-import { folder_secret_join_json } from "../../../love/public/src/folder_secret_join_json.mjs";
 export async function playwright_session_save(url, session_name) {
   const browser = await playwright_chromium_visible();
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto(url);
-  let answer = await command_line_read(
-    "Press enter to save sesssion and close the browser",
-  );
-  let path = folder_secret_join_json(session_name);
-  await context.storageState({
-    path,
-  });
-  await browser.close();
+  await playwright_session_save_prompt(session_name, context, browser);
 }
