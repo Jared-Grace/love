d3fa24f303a3ed6827d1f35ef73ccf4ec7816f0a
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:18:59 -0400

    function_auto_path ../love/public/src/playwright_session_load.mjs

--- public/src/playwright_session_load.mjs
+++ public/src/playwright_session_load.mjs
@@ -1,1 +1,6 @@
-export function playwright_session_load() {}
+import { playwright_chromium_visible } from "../../../love/public/src/playwright_chromium_visible.mjs";
+export async function playwright_session_load() {
+  const browser = await playwright_chromium_visible();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+}
