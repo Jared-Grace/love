926689398a5d08f66c3f8a4f710121c579cc6d74
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 20:52:27 -0400

    p

---
+++ web/public/src/text_suffix_without_try.mjs
@@ -0,0 +1,12 @@
+import { text_suffix_without_inner } from "../../../love/public/src/text_suffix_without_inner.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { text_ends_with } from "../../../love/public/src/text_ends_with.mjs";
+import { error } from "../../../love/public/src/error.mjs";
+export function text_suffix_without_try(t, suffix) {
+  let a = text_ends_with(t, suffix);
+  if (not(a)) {
+    error();
+  }
+  const without = text_suffix_without_inner(t, suffix);
+  return without;
+}
