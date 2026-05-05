f7c6acd6e19d55d9720fdc7d8debb59318d060e7
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 21:01:08 -0400

    function_auto_path ../love/public/src/app_calendar_secret_path.mjs

--- web/public/src/app_calendar_secret_path.mjs
+++ web/public/src/app_calendar_secret_path.mjs
@@ -1,6 +1,6 @@
 import { folder_secret_join_json } from "../../../love/public/src/folder_secret_join_json.mjs";
 import { app_calendar } from "../../../love/public/src/app_calendar.mjs";
-export function app_calendar_secret_path() {
+export function app_calendar_secret_path() {$a
   const name = app_calendar.name;
   let file_path = folder_secret_join_json(name);
   return file_path;
