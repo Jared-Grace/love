72e7f77e5e4201fd87a720881c6d74245241d41f
JESUS rose to life <username@users.noreply.github.com>
2026-05-04 22:42:11 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- public/src/app_calendar_url_ids.mjs
+++ public/src/app_calendar_url_ids.mjs
@@ -1,4 +1,3 @@
-import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
 import { list_map_suffix_without_try } from "../../../love/public/src/list_map_suffix_without_try.mjs";
 import { list_filter_starts_with_https_prefix } from "../../../love/public/src/list_filter_starts_with_https_prefix.mjs";
@@ -6,7 +5,6 @@ import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_e
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 export async function app_calendar_url_ids() {
   let split = await folder_user_docs_read_lines("preaching_ask.txt");
-  let file_path = folder_user_docs_path("preaching_ask.lookup.json");
   let filtered = list_filter_empty_not_is(split);
   let filtered2 = list_filter_starts_with_https_prefix(filtered);
   const p1 = "https://www.facebook.com/profile.php?id=";
