61e512ad38d3155f799c42ed6ee0a0093dfa9d1f
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:45:53 -0400

    function_auto_path ../love/public/src/sandbox_2.mjs

---
+++ web/public/src/file_json_transform_initialize_default.mjs
@@ -0,0 +1,7 @@
+import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
+export async function file_json_transform_initialize_default(
+  file_path_calendar,
+  lambda2,
+) {
+  await file_json_transform_initialize(file_path_calendar, {}, lambda2);
+}
