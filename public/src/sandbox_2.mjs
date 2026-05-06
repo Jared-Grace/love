0d5a5f10dd2c9eebb778076ccb79549771ba23f5
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:33:59 -0400

    function_auto_path ../love/public/src/sandbox_2.mjs

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,3 +1,4 @@
+import { app_calendar_secret_path } from "../../../love/public/src/app_calendar_secret_path.mjs";
 import { text_between } from "../../../love/public/src/text_between.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
@@ -7,6 +8,7 @@ import { object_values } from "../../../love/public/src/object_values.mjs";
 import { file_read_json_exists_ensure } from "../../../love/public/src/file_read_json_exists_ensure.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 export async function sandbox_2() {
+  let file_path_calendar = app_calendar_secret_path();
   const file_name = "preaching_ask.lookup.json";
   let file_path = folder_user_docs_path(file_name);
   let lookup = await file_read_json_exists_ensure(file_path);
