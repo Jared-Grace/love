ad283dcb58f72cae874cfc8137a923bf59e9f387
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:32:30 -0400

    function_auto_path ../love/public/src/text_between.mjs

--- web/public/src/text_between.mjs
+++ web/public/src/text_between.mjs
@@ -2,6 +2,6 @@ import { text_index_of_take } from "../../../love/public/src/text_index_of_take.
 import { text_index_of_skip } from "../../../love/public/src/text_index_of_skip.mjs";
 export function text_between(value, left, right) {
   let skipped = text_index_of_skip(value, left);
-  let taken = text_index_of_take(skipped, right);
-  return taken;
+  let between = text_index_of_take(skipped, right);
+  return between;
 }
