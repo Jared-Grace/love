f0d637c8dc44758ffec1d67d8320cc91db9ca3ce
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:46:11 -0400

    function_auto_path ../love/public/src/file_json_transform_initialize_default.mjs

--- public/src/file_json_transform_initialize_default.mjs
+++ public/src/file_json_transform_initialize_default.mjs
@@ -1,7 +1,7 @@
 import { file_json_transform_initialize } from "../../../love/public/src/file_json_transform_initialize.mjs";
 export async function file_json_transform_initialize_default(
   file_path,
-  labda2,
+  lambda$data,
 ) {
-  await file_json_transform_initialize(file_path, {}, labda2);
+  await file_json_transform_initialize(file_path, {}, lambda$data);
 }
