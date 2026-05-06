bfa701c0f249833bc4146ef50b402ef413181474
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:12:26 -0400

    function_auto_path ../love/public/src/sandbox_2.mjs

--- public/src/sandbox_2.mjs
+++ public/src/sandbox_2.mjs
@@ -1,9 +1,11 @@
+import { object_values } from "../../../love/public/src/object_values.mjs";
 import { file_read_json_exists_ensure } from "../../../love/public/src/file_read_json_exists_ensure.mjs";
 import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 export async function sandbox_2() {
   const file_name = "preaching_ask.lookup.json";
   let file_path = folder_user_docs_path(file_name);
   let lookup = await file_read_json_exists_ensure(file_path);
-  ovs("t/");
+  let v = object_values(o);
+  ("t/");
   return lookup;
 }
