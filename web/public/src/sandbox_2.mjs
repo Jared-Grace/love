f5c5813c3ca0282983f66b592527657831e28db7
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:13:32 -0400

    function_auto_path ../love/public/src/sandbox_2.mjs

--- web/public/src/sandbox_2.mjs
+++ web/public/src/sandbox_2.mjs
@@ -1,3 +1,4 @@
+import { json_equal_assert } from "../../../love/public/src/json_equal_assert.mjs";
 import { list_filter_text_includes } from "../../../love/public/src/list_filter_text_includes.mjs";
 import { object_values } from "../../../love/public/src/object_values.mjs";
 import { file_read_json_exists_ensure } from "../../../love/public/src/file_read_json_exists_ensure.mjs";
@@ -8,5 +9,6 @@ export async function sandbox_2() {
   let lookup = await file_read_json_exists_ensure(file_path);
   let v = object_values(lookup);
   let filtered = list_filter_text_includes(v, "/t/");
+  json_equal_assert(left, right);
   return lookup;
 }
