2c89d7941bdd2b36c5c0c7128c82de17be92db5e
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:10:35 -0400

    function_auto_path ../love/public/src/sandbox_2.mjs

--- web/public/src/sandbox_2.mjs
+++ web/public/src/sandbox_2.mjs
@@ -1,8 +1,4 @@
 import { sandbox_nearley } from "../../../love/public/src/sandbox_nearley.mjs";
-import nearley from "nearley";
-import compile from "nearley/lib/compile.js";
-import generate from "nearley/lib/generate.js";
-import grammarParser from "nearley/lib/nearley-language-bootstrapped.js";
 export async function sandbox_2() {
   sandbox_nearley();
 }
