65cfcc9c6573a0672f65e5dd2fd0215492e1900c
JESUS rose to life <username@users.noreply.github.com>
2026-05-05 20:34:51 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

---
+++ public/src/app_calendar_contacts_initialize.mjs
@@ -0,0 +1,5 @@
+import { property_initialize } from "../../../love/public/src/property_initialize.mjs";
+export function app_calendar_contacts_initialize(data) {
+  let value2 = property_initialize(data, "contacts", []);
+  return value2;
+}
