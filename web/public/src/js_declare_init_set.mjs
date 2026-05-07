249864c736ec3e67e00b0f5f6fc00d1f8cb76b30
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:44:40 -0400

    function_auto_path ../love/public/src/js_declare_init_set.mjs

--- web/public/src/js_declare_init_set.mjs
+++ web/public/src/js_declare_init_set.mjs
@@ -1,6 +1,6 @@
+import { js_declarator_init_set } from "../../../love/public/src/js_declarator_init_set.mjs";
 import { js_declare_single } from "../../../love/public/src/js_declare_single.mjs";
-import { property_set } from "../../../love/public/src/property_set.mjs";
 export function js_declare_init_set(vd, init) {
   let declarator = js_declare_single(vd);
-  property_set(declarator, "init", init);
+  js_declarator_init_set(declarator, init);
 }
