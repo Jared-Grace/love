1592787a0abd818d54c06f7e850138f4b2ce6c91
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 16:32:49 -0400

    function_auto_path ../love/public/src/app_calendar_preaching_ask_lookup_migrate.mjs

--- public/src/app_calendar_preaching_ask_lookup_migrate.mjs
+++ public/src/app_calendar_preaching_ask_lookup_migrate.mjs
@@ -1,1 +1,4 @@
-export function app_calendar_preaching_ask_lookup_migrate() {}
+import { app_calendar_preaching_ask_lookup_get } from "../../../love/public/src/app_calendar_preaching_ask_lookup_get.mjs";
+export async function app_calendar_preaching_ask_lookup_migrate() {
+  let lookup = await app_calendar_preaching_ask_lookup_get();
+}
