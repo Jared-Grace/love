80ad2179d9211104567112f8a3dd3f4c4a38d4c2
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 17:23:49 -0400

    function_auto_path ../love/public/src/playwright_by_attribute.mjs

--- web/public/src/playwright_by_attribute.mjs
+++ web/public/src/playwright_by_attribute.mjs
@@ -1,6 +1,7 @@
+import { equal } from "../../../love/public/src/equal.mjs";
 import { playwright_by_attribute_generic } from "../../../love/public/src/playwright_by_attribute_generic.mjs";
 export async function playwright_by_attribute(page, name, value) {
-  let r2 = await playwright_by_attribute_generic(page, name, compare, value);
+  let r2 = await playwright_by_attribute_generic(page, name, equal, value);
   return r2;
   function compare(a, value) {
     let r = a === value;
