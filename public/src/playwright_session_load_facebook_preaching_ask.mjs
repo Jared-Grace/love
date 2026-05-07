dd219d204675100581eb847406df0a078d667c01
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 18:18:48 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook_preaching_ask.mjs

--- public/src/playwright_session_load_facebook_preaching_ask.mjs
+++ public/src/playwright_session_load_facebook_preaching_ask.mjs
@@ -31,7 +31,10 @@ export async function playwright_session_load_facebook_preaching_ask() {
       const url_goto1 = "https://www.facebook.com/" + url_id;
       await playwright_sleep_goto(page, url_goto1);
       let h2 = await playwright_by_tag_name_text_contents(page, "h2");
-      let includes = list_includes(list, item);
+      let includes = list_includes(
+        list,
+        "This content isn't available right now",
+      );
       let h1 = await playwright_by_tag_name_text_contents(page, "h1");
       list_remove_if_exists(waited, "Chats");
       let t = list_single(waited);
