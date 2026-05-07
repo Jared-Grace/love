6a8b48d96f53654252ad1533e7637f1239c84321
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 17:25:34 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook_preaching_ask.mjs

--- public/src/playwright_session_load_facebook_preaching_ask.mjs
+++ public/src/playwright_session_load_facebook_preaching_ask.mjs
@@ -1,4 +1,4 @@
-import { playwright_by_attribute_text } from "../../../portfolio_qa/public/src/playwright_by_attribute_text.mjs";
+import { playwright_by_attribute_starts_with } from "../../../love/public/src/playwright_by_attribute_starts_with.mjs";
 import { app_calendar_facebook_conversation_url } from "../../../love/public/src/app_calendar_facebook_conversation_url.mjs";
 import { property_initialize_empty } from "../../../love/public/src/property_initialize_empty.mjs";
 import { app_calendar_preaching_ask_lookup_overwrite } from "../../../love/public/src/app_calendar_preaching_ask_lookup_overwrite.mjs";
@@ -29,7 +29,7 @@ export async function playwright_session_load_facebook_preaching_ask() {
     let v = property_initialize_empty(lookup, url_id);
     let property = app_calendar_facebook_conversation_url();
     property_set_exists_not(v, property, url);
-    let actual = await playwright_by_attribute_text(page2, name, value);
+    let actual = await playwright_by_attribute_starts_with(page, name, value);
     return;
     await app_calendar_preaching_ask_lookup_overwrite(lookup);
   }
