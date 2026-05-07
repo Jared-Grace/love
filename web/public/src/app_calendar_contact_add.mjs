a01df68066361f0abb7827bc368205167a43d6c7
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:41:22 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- web/public/src/app_calendar_contact_add.mjs
+++ web/public/src/app_calendar_contact_add.mjs
@@ -1,7 +1,6 @@
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
+import { object_values_map_unique } from "../../../love/public/src/object_values_map_unique.mjs";
 import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
 import { list_find_property_or_null_curried } from "../../../love/public/src/list_find_property_or_null_curried.mjs";
-import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_calendar_contact_add(data, item) {
@@ -11,8 +10,7 @@ export function app_calendar_contact_add(data, item) {
     let existing = c(key, value);
     return existing;
   }
-  let mapped = object_values_map(item, lambda);
-  let unique = list_unique(mapped);
+  let unique = object_values_map_unique(item, lambda);
   let filtered2 = list_filter_null_not_is(unique);
   list_add(contacts, {
     facebook_conversation_id,
