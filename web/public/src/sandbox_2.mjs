be83625b8bf7e7c4d0e9195f5a052777054727f9
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 16:30:06 -0400

    function_auto_path ../love/public/src/sandbox_2.mjs

--- web/public/src/sandbox_2.mjs
+++ web/public/src/sandbox_2.mjs
@@ -1,13 +1,12 @@
+import { folder_user_docs_path_preaching_ask_lookup } from "../../../love/public/src/folder_user_docs_path_preaching_ask_lookup.mjs";
 import { app_calendar_contact_add } from "../../../love/public/src/app_calendar_contact_add.mjs";
 import { file_json_transform_initialize_default } from "../../../love/public/src/file_json_transform_initialize_default.mjs";
 import { app_calendar_secret_path } from "../../../love/public/src/app_calendar_secret_path.mjs";
 import { text_between } from "../../../love/public/src/text_between.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
 import { file_read_json_exists_ensure } from "../../../love/public/src/file_read_json_exists_ensure.mjs";
-import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
 export async function sandbox_2() {
-  const file_name = "preaching_ask.lookup.json";
-  let file_path = folder_user_docs_path(file_name);
+  let file_path = folder_user_docs_path_preaching_ask_lookup();
   let lookup = await file_read_json_exists_ensure(file_path);
   let file_path_calendar = app_calendar_secret_path();
   async function lambda2(data) {
