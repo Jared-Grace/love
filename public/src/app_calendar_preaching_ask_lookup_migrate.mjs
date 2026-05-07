09e500c5ac9dee9594abf3b6e6bc18733048e760
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 16:36:32 -0400

    function_auto_path ../love/public/src/app_calendar_preaching_ask_lookup_migrate.mjs

--- public/src/app_calendar_preaching_ask_lookup_migrate.mjs
+++ public/src/app_calendar_preaching_ask_lookup_migrate.mjs
@@ -1,15 +1,9 @@
+import { object_map_self_value_to_property } from "../../../love/public/src/object_map_self_value_to_property.mjs";
 import { app_calendar_preaching_ask_lookup_overwrite } from "../../../love/public/src/app_calendar_preaching_ask_lookup_overwrite.mjs";
-import { object_values_map_self } from "../../../love/public/src/object_values_map_self.mjs";
 import { app_calendar_preaching_ask_lookup_get } from "../../../love/public/src/app_calendar_preaching_ask_lookup_get.mjs";
 export async function app_calendar_preaching_ask_lookup_migrate() {
   let lookup = await app_calendar_preaching_ask_lookup_get();
   const property = "facebook_conversation_url";
-  function lambda(value) {
-    let r = {
-      [property]: value,
-    };
-    return r;
-  }
-  object_values_map_self(lookup, lambda);
+  object_map_self_value_to_property(property, lookup);
   await app_calendar_preaching_ask_lookup_overwrite(lookup);
 }
