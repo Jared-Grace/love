38db11d29e2f1b3bd86cec3073f421206b7b3984
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 16:29:54 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook_preaching_ask.mjs

---
+++ web/public/src/folder_user_docs_path_preaching_ask_lookup.mjs
@@ -0,0 +1,6 @@
+import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
+export function folder_user_docs_path_preaching_ask_lookup() {
+  const file_name = "preaching_ask.lookup.json";
+  let file_path = folder_user_docs_path(file_name);
+  return file_path;
+}
