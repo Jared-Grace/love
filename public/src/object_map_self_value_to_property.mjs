b01ebaa3d16679262af6f520fccc0f4044ce3c6a
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 16:36:43 -0400

    function_auto_path ../love/public/src/object_map_self_value_to_property.mjs

--- public/src/object_map_self_value_to_property.mjs
+++ public/src/object_map_self_value_to_property.mjs
@@ -1,10 +1,10 @@
 import { object_values_map_self } from "../../../love/public/src/object_values_map_self.mjs";
-export function object_map_self_value_to_property(lookup, property) {
+export function object_map_self_value_to_property(object, property) {
   function lambda(value) {
     let r = {
       [property]: value,
     };
     return r;
   }
-  object_values_map_self(lookup, lambda);
+  object_values_map_self(object, lambda);
 }
