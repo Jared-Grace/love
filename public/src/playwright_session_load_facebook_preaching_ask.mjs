7dc0e82df03bb6ec855137fd09571e025fae05e4
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 17:26:54 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook_preaching_ask.mjs

--- public/src/playwright_session_load_facebook_preaching_ask.mjs
+++ public/src/playwright_session_load_facebook_preaching_ask.mjs
@@ -4,8 +4,6 @@ import { app_calendar_facebook_conversation_url } from "../../../love/public/src
 import { property_initialize_empty } from "../../../love/public/src/property_initialize_empty.mjs";
 import { app_calendar_preaching_ask_lookup_overwrite } from "../../../love/public/src/app_calendar_preaching_ask_lookup_overwrite.mjs";
 import { app_calendar_preaching_ask_lookup_get } from "../../../love/public/src/app_calendar_preaching_ask_lookup_get.mjs";
-import { list_difference } from "../../../love/public/src/list_difference.mjs";
-import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
 import { http_sleep } from "../../../love/public/src/http_sleep.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
@@ -16,8 +14,6 @@ import { playwright_session_save_facebook_name } from "../../../love/public/src/
 export async function playwright_session_load_facebook_preaching_ask() {
   let url_ids_all = await app_calendar_url_ids();
   let lookup = await app_calendar_preaching_ask_lookup_get();
-  let properties = properties_get(lookup);
-  let url_ids_missing = list_difference(url_ids_all, properties);
   const session_name = playwright_session_save_facebook_name();
   const r = await playwright_session_load(session_name);
   let browser = property_get(r, "browser");
