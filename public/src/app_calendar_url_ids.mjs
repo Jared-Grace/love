d1a2dcdb35b383a45f7a0291ceacbdf9b35b526f
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 13:02:25 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- public/src/app_calendar_url_ids.mjs
+++ public/src/app_calendar_url_ids.mjs
@@ -14,9 +14,9 @@ export async function app_calendar_url_ids() {
   let prefixes = [p1, p2];
   filtered2 = list_map_prefix_without_try_multiple(filtered2, prefixes);
   function lambda(item) {
-    let url_ids_with_duplicates = list_map_suffix_without_try(filtered2, "#");
+    let url_ids_with_duplicates = list_map_suffix_without_try(filtered2);
   }
-  each(list, lambda);
+  each(["#", "?"], lambda);
   let url_ids = list_unique(url_ids_with_duplicates);
   return url_ids;
 }
