a9ec3e708d93a2d05938f4f4dc9e612c8051cc93
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:31:28 -0400

    function_auto_path ../love/public/src/object_merge_generic.mjs

--- public/src/object_merge_generic.mjs
+++ public/src/object_merge_generic.mjs
@@ -7,7 +7,7 @@ import { property_exists } from "../../../love/public/src/property_exists.mjs";
 import { error_json } from "../../../love/public/src/error_json.mjs";
 export function object_merge_generic(mode, to, from) {
   function lambda(property_name) {
-    let value = property_get(from, property_name);
+    let value_new = property_get(from, property_name);
     if (property_exists(to, property_name)) {
       if (mode === "strict") {
         error_json({
@@ -18,17 +18,17 @@ export function object_merge_generic(mode, to, from) {
       } else {
         if (mode === "match") {
           let existing = property_get(to, property_name);
-          if (equal_not(left, right)) {
+          if (equal_not(existing, value_new)) {
+            error_json({
+              to,
+              from,
+              property_name,
+            });
           }
-          error_json({
-            to,
-            from,
-            property_name,
-          });
         }
       }
     }
-    property_set(to, property_name, value);
+    property_set(to, property_name, value_new);
   }
   let list = properties_get(from);
   each(list, lambda);
