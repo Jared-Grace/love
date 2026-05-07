1c1dd964c0b7951aebe26affbc16c24fc4271be6
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 17:38:48 -0400

    function_auto_path ../love/public/src/list_sort_text_mapper.mjs

--- public/src/list_sort_text_mapper.mjs
+++ public/src/list_sort_text_mapper.mjs
@@ -1,11 +1,12 @@
+import { undefined } from "../../../love/public/src/undefined.mjs";
 export function list_sort_text_mapper(list, lambda$item) {
   function lambda(a, b) {
-    const va = lambda$item(a);
-    const vb = lambda$item(b);
-    const [na, ia] = va.split(/[_\.]/);
-    const [nb, ib] = vb.split(/[_\.]/);
-    let v = na.localeCompare(nb) || Number(ia) - Number(ib);
-    return v;
+    let v = lambda$item(b);
+    let r = lambda$item(a).localeCompare(v, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    });
+    return r;
   }
   list.sort(lambda);
 }
