4cdf9f4ffefbf3884f05bb9174d020753e267aba
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:55:38 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

---
+++ public/src/text_includes_assert.mjs
@@ -0,0 +1,13 @@
+import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
+import { text_includes } from "../../../love/public/src/text_includes.mjs";
+export function text_includes_assert(input, part) {
+  let i = text_includes(input, part);
+  function lambda3() {
+    let r = {
+      input,
+      part,
+    };
+    return r;
+  }
+  assert_json_get(i, lambda3);
+}
