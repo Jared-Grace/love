f4baf3053eb19b6d0ae0d96d313159003231fddc
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 16:33:30 -0400

    function_auto_path ../love/public/src/app_calendar_preaching_ask_lookup_migrate.mjs

--- public/src/app_calendar_preaching_ask_lookup_migrate.mjs
+++ public/src/app_calendar_preaching_ask_lookup_migrate.mjs
@@ -1,7 +1,7 @@
-import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
+import { object_values_map_self } from "../../../love/public/src/object_values_map_self.mjs";
 import { app_calendar_preaching_ask_lookup_get } from "../../../love/public/src/app_calendar_preaching_ask_lookup_get.mjs";
 export async function app_calendar_preaching_ask_lookup_migrate() {
   let lookup = await app_calendar_preaching_ask_lookup_get();
   function lambda(value, property) {}
-  object_values_map(lookup, lambda);
+  object_values_map_self(lookup, lambda);
 }
