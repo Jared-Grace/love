075658c8bf92554745e41e65c8f9782285b04b5d
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:46:22 -0400

    s3

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -34,4 +34,18 @@ export async function sandbox_3() {
     b: 2,
     a: 1,
   });
+  to = {
+    c: 3,
+    b: 2,
+    a: 1,
+  };
+  object_merge_match(to, {
+    b: 2,
+    a: 1,
+  });
+  json_equal_assert(to, {
+    c: 3,
+    b: 2,
+    a: 1,
+  });
 }
