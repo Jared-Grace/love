2973c71fb28d3403be0af21df026354caac65041
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:39:38 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- public/src/app_calendar_contact_add.mjs
+++ public/src/app_calendar_contact_add.mjs
@@ -1,13 +1,12 @@
 import { list_find_property_or_null_curried } from "../../../love/public/src/list_find_property_or_null_curried.mjs";
 import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
-import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_calendar_contact_add(data, item) {
   let contacts = app_calendar_contacts_initialize(data);
   let c = list_find_property_or_null_curried(list);
   function lambda(value, key) {
-    let existing = list_find_property_or_null(contacts, key, value);
+    let existing = c(key, value);
     return existing;
   }
   object_values_map(properties, lambda);
