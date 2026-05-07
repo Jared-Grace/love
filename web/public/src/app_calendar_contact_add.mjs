198b47dd75e66b521de460cf81e0b342b742b344
JESUS rose to life <username@users.noreply.github.com>
2026-05-06 20:40:36 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- web/public/src/app_calendar_contact_add.mjs
+++ web/public/src/app_calendar_contact_add.mjs
@@ -1,3 +1,4 @@
+import { list_find } from "../../../love/public/src/list_find.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_calendar_contact_add(
@@ -5,6 +6,8 @@ export function app_calendar_contact_add(
   facebook_conversation_id,
   facebook_url_id,
 ) {
+  function lambda(item) {}
+  let found = list_find(list, lambda);
   let contacts = app_calendar_contacts_initialize(data);
   list_add(contacts, {
     facebook_conversation_id,
