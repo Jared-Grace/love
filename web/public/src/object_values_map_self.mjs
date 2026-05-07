461022739a569c990e0f40b16a791b9d7feee1c4
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:48:13 -0400

    function_auto_path ../love/public/src/object_values_map_self.mjs

--- web/public/src/object_values_map_self.mjs
+++ web/public/src/object_values_map_self.mjs
@@ -1,9 +1,9 @@
 import { each_object } from "../../../love/public/src/each_object.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
-export function object_values_map_self(overrides, lambda$value) {
+export function object_values_map_self(object, lambda$value) {
   function lambda(value, property) {
     let value_new = lambda$value(value);
-    property_set(overrides, property, value_new);
+    property_set(object, property, value_new);
   }
-  each_object(overrides, lambda);
+  each_object(object, lambda);
 }
