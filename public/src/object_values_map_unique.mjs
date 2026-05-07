2afe298cff521aedee687b823e2449d090d9a64f
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:41:22 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

---
+++ public/src/object_values_map_unique.mjs
@@ -0,0 +1,7 @@
+import { list_unique } from "../../../love/public/src/list_unique.mjs";
+import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
+export function object_values_map_unique(item, lambda) {
+  let mapped = object_values_map(item, lambda);
+  let unique = list_unique(mapped);
+  return unique;
+}
