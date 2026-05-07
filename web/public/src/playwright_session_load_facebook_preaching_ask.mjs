d1e209ca7699b066fae97bf2052d0e80259cc784
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 18:04:14 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook_preaching_ask.mjs

--- web/public/src/playwright_session_load_facebook_preaching_ask.mjs
+++ web/public/src/playwright_session_load_facebook_preaching_ask.mjs
@@ -27,7 +27,9 @@ export async function playwright_session_load_facebook_preaching_ask() {
     let property_name = app_calendar_facebook_name();
     let ne = property_exists_not(v, property_name);
     if (ne) {
+      await lambda();
     }
+    function lambda2() {}
     const url_goto1 = "https://www.facebook.com/" + url_id;
     await playwright_sleep_goto(page, url_goto1);
     let es = await playwright_by_tag_name(page, "h1");
