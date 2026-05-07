753fb3cc3419ecee07611bb1b4c794e9169275be
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:36:47 -0400

    function_auto_path ../love/public/src/js_declare_init_set.mjs

--- web/public/src/js_declare_init_set.mjs
+++ web/public/src/js_declare_init_set.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_declare_single } from "../../../love/public/src/js_declare_single.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 export function js_declare_init_set(vd, init) {
+  log(js_declare_init_set.name, {
+    vd,
+  });
   let declarators = js_declare_single(vd);
   property_set(declarators, "init", init);
 }
--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,7 +1,7 @@
 import { json_equal_assert } from "../../../love/public/src/json_equal_assert.mjs";
 import { object_merge_match } from "../../../love/public/src/object_merge_match.mjs";
 export async function sandbox_3() {
-  let to = null;
+  let to;
   to = {};
   object_merge_match(to, {});
   json_equal_assert(to, {});
