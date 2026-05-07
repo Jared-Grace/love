f87a670f82a32963cc26ab1f71dc85ff1dfb6812
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:42:02 -0400

    function_auto_path ../love/public/src/js_visit_declarations.mjs

--- web/public/src/js_visit_declarations.mjs
+++ web/public/src/js_visit_declarations.mjs
@@ -1,4 +1,4 @@
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 export function js_visit_declarations(ast, lambda2) {
-  js_visit_type(ast, "VariableDeclarator", lambda2);
+  js_visit_type(ast, "VariableDeclaration", lambda2);
 }
