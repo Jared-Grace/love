f337c7059a163f6d269d67df1e38b5cacc640e43
JESUS rose to life <username@users.noreply.github.com>
2026-05-06 20:41:58 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- public/src/app_calendar_contact_add.mjs
+++ public/src/app_calendar_contact_add.mjs
@@ -1,9 +1,12 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_calendar_contact_add(data, item) {
   let contacts = app_calendar_contacts_initialize(data);
   let properties = ["facebook_conversation_id", "facebook_url_id"];
+  function lambda(item2) {}
+  each(list, lambda);
   let r = list_find_property_or_null(
     contacts,
     "facebook_conversation_id",
