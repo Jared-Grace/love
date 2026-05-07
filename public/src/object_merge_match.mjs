ccc912b5b2ec7dc58833f6673eec9cef9defaa0e
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:32:18 -0400

    function_auto_path ../love/public/src/object_merge_match.mjs

--- public/src/object_merge_match.mjs
+++ public/src/object_merge_match.mjs
@@ -1,5 +1,4 @@
 import { object_merge_generic } from "../../../love/public/src/object_merge_generic.mjs";
 export function object_merge_match(to, from) {
   object_merge_generic("match", to, from);
-  return to;
 }
