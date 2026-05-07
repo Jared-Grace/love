a51283df2ed19fdce4a0c28d6290b6f063ff9f42
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 13:03:14 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- public/src/app_calendar_url_ids.mjs
+++ public/src/app_calendar_url_ids.mjs
@@ -1,7 +1,6 @@
-import { each } from "../../../love/public/src/each.mjs";
+import { list_map_suffix_without_try_multiple } from "../../../love/public/src/list_map_suffix_without_try_multiple.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
-import { list_map_suffix_without_try } from "../../../love/public/src/list_map_suffix_without_try.mjs";
 import { list_filter_starts_with_https_prefix } from "../../../love/public/src/list_filter_starts_with_https_prefix.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
@@ -14,10 +13,7 @@ export async function app_calendar_url_ids() {
   let prefixes = [p1, p2];
   filtered2 = list_map_prefix_without_try_multiple(filtered2, prefixes);
   const suffixes = ["#", "?"];
-  function lambda(item) {
-    filtered2 = list_map_suffix_without_try(filtered2, item);
-  }
-  each(suffixes, lambda);
+  filtered2 = list_map_suffix_without_try_multiple(filtered2, suffixes);
   let url_ids = list_unique(url_ids_with_duplicates);
   return url_ids;
 }
