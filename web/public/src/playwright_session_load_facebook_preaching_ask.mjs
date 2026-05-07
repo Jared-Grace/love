0d2fe93692936d55dd517c0ebaef51ce3d9c9993
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 18:21:15 -0400

    function_auto_path ../love/public/src/playwright_session_load_facebook_preaching_ask.mjs

--- web/public/src/playwright_session_load_facebook_preaching_ask.mjs
+++ web/public/src/playwright_session_load_facebook_preaching_ask.mjs
@@ -37,9 +37,9 @@ export async function playwright_session_load_facebook_preaching_ask() {
       if (includes) {
         property_set_exists_not(v, "blocked", true);
       }
+      let name = null;
       let h1 = await playwright_by_tag_name_text_contents(page, "h1");
       list_remove_if_exists(waited, "Chats");
-      let name = null;
       name = list_single(waited);
       property_set_exists_not(v, property_name, name);
       await app_calendar_preaching_ask_lookup_overwrite(lookup);
