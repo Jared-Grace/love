5a59c9cef75681dcf90af1efe178872ceba5ff84
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 13:03:41 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- public/src/app_calendar_url_ids.mjs
+++ public/src/app_calendar_url_ids.mjs
@@ -7,13 +7,13 @@ import { folder_user_docs_read_lines } from "../../../love/public/src/folder_use
 export async function app_calendar_url_ids() {
   let split = await folder_user_docs_read_lines("preaching_ask.txt");
   let filtered = list_filter_empty_not_is(split);
-  let filtered2 = list_filter_starts_with_https_prefix(filtered);
+  let urls = list_filter_starts_with_https_prefix(filtered);
   const p1 = "https://www.facebook.com/profile.php?id=";
   const p2 = "https://www.facebook.com/";
   let prefixes = [p1, p2];
-  filtered2 = list_map_prefix_without_try_multiple(filtered2, prefixes);
+  urls = list_map_prefix_without_try_multiple(urls, prefixes);
   const suffixes = ["#", "?"];
-  filtered2 = list_map_suffix_without_try_multiple(filtered2, suffixes);
+  urls = list_map_suffix_without_try_multiple(urls, suffixes);
   let url_ids = list_unique(url_ids_with_duplicates);
   return url_ids;
 }
