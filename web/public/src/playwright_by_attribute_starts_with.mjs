c43005d2a8bce58483b34d33dda554c7da9fafa6
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 17:25:24 -0400

    function_auto_path ../love/public/src/playwright_by_attribute_starts_with.mjs

--- web/public/src/playwright_by_attribute_starts_with.mjs
+++ web/public/src/playwright_by_attribute_starts_with.mjs
@@ -1,6 +1,11 @@
-import { equal } from "../../../love/public/src/equal.mjs";
+import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 import { playwright_by_attribute_generic } from "../../../love/public/src/playwright_by_attribute_generic.mjs";
 export async function playwright_by_attribute_starts_with(page, name, value) {
-  let r2 = await playwright_by_attribute_generic(page, name, value, equal);
+  let r2 = await playwright_by_attribute_generic(
+    page,
+    name,
+    value,
+    text_starts_with,
+  );
   return r2;
 }
