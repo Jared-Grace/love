046c684aec41b58f61fe80b3f4e5253645ec57b2
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:35:22 -0400

    function_auto_path ../love/public/src/throws_assert_text.mjs

--- web/public/src/throws_assert_text.mjs
+++ web/public/src/throws_assert_text.mjs
@@ -1,7 +1,7 @@
 import { text_to } from "../../../love/public/src/text_to.mjs";
 import { throws_assert } from "../../../love/public/src/throws_assert.mjs";
-export function throws_assert_text(lambda2) {
-  let e = throws_assert(lambda2);
+export function throws_assert_text(lambda) {
+  let e = throws_assert(lambda);
   let input = text_to(e);
   return input;
 }
