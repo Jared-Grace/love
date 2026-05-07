e015d05a008980c8f301b1b79ba2f9e6e2a22ccb
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:48:50 -0400

    function_auto_path ../love/public/src/object_values_map_unique.mjs

--- public/src/object_values_map_unique.mjs
+++ public/src/object_values_map_unique.mjs
@@ -1,7 +1,9 @@
+import { object_values } from "../../../love/public/src/object_values.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { object_values_map } from "../../../love/public/src/object_values_map.mjs";
 export function object_values_map_unique(item, lambda) {
   let mapped = object_values_map(item, lambda);
+  let v = object_values(o);
   let unique = list_unique(mapped);
   return unique;
 }
