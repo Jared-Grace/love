73028a9e7a7ab2b6a3a5a310480f1bbd5cf7d72c
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 12:36:44 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- public/src/app_calendar_contact_add.mjs
+++ public/src/app_calendar_contact_add.mjs
@@ -1,5 +1,3 @@
-import { object_merge_match } from "../../../love/public/src/object_merge_match.mjs";
-import { property_exists } from "../../../love/public/src/property_exists.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
@@ -8,15 +6,12 @@ export function app_calendar_contact_add(data, item) {
   let contacts = app_calendar_contacts_initialize(data);
   let properties = ["facebook_conversation_id", "facebook_url_id"];
   function lambda(property) {
-    let exists = property_exists(item, property);
-    if (exists) {
-      let existing = list_find_property_or_null(
-        contacts,
-        property,
-        facebook_conversation_id,
-      );
-      object_merge_match(existing, item);
-    }
+    let existing = list_find_property_or_null(
+      contacts,
+      property,
+      facebook_conversation_id,
+    );
+    return existing;
   }
   each(properties, lambda);
   list_add(contacts, {
