1079ff72b3eeddaaa1701424dbadf5f45ed85e1f
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 13:03:01 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- web/public/src/app_calendar_url_ids.mjs
+++ web/public/src/app_calendar_url_ids.mjs
@@ -13,10 +13,11 @@ export async function app_calendar_url_ids() {
   const p2 = "https://www.facebook.com/";
   let prefixes = [p1, p2];
   filtered2 = list_map_prefix_without_try_multiple(filtered2, prefixes);
+  const suffixes = ["#", "?"];
   function lambda(item) {
     filtered2 = list_map_suffix_without_try(filtered2, item);
   }
-  each(["#", "?"], lambda);
+  each(suffixes, lambda);
   let url_ids = list_unique(url_ids_with_duplicates);
   return url_ids;
 }
