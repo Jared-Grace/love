baa596bb0413ee145214375c50afbe59e9c35c83
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 17:41:32 -0400

    function_auto_path ../love/public/src/playwright_by_tag_name.mjs

--- web/public/src/playwright_by_tag_name.mjs
+++ web/public/src/playwright_by_tag_name.mjs
@@ -1,1 +1,16 @@
-export function playwright_by_tag_name() {}
+import { equal } from "../../../love/public/src/equal.mjs";
+export async function playwright_by_tag_name(page, value) {
+  const elements = await page.locator("*").elementHandles();
+  const filtered = [];
+  for (const el of elements) {
+    function lambda(e) {
+      let r = e.tagName.toLowerCase();
+      return r;
+    }
+    const name = await el.evaluate(lambda);
+    if (equal(name, value)) {
+      filtered.push(el);
+    }
+  }
+  return filtered;
+}
