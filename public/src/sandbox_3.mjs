7c71b9dde5cc485ba5dd5f2502dc26912274728e
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 11:34:50 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,7 +1,9 @@
+import { json_equal_assert } from "../../../love/public/src/json_equal_assert.mjs";
 import { object_merge_match } from "../../../love/public/src/object_merge_match.mjs";
 export async function sandbox_3() {
   let to = {};
   object_merge_match(to, {
     a: 1,
   });
+  json_equal_assert(left, right);
 }
