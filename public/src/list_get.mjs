bd616f84b078988092c6983a8a69ba5ca49f091b
JESUS rose to life <username@users.noreply.github.com>
2026-05-08 17:05:54 -0400

    function_auto_path ../love/public/src/list_get.mjs

--- public/src/list_get.mjs
+++ public/src/list_get.mjs
@@ -1,8 +1,9 @@
+import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mjs";
 import { list_get_try } from "../../../love/public/src/list_get_try.mjs";
 import { undefined_not_is_assert_lambda } from "../../../love/public/src/undefined_not_is_assert_lambda.mjs";
 export function list_get(list, index) {
-  let item = list_get_try(list, index);
-  undefined_not_is_assert_lambda(item, object_get);
+  let item2 = list_get_try(list, index);
+  undefined_not_is_assert_lambda(item2, object_get);
   function object_get() {
     let v = {
       list,
@@ -10,5 +11,7 @@ export function list_get(list, index) {
     };
     return v;
   }
-  return item;
+  return item2;
+  integer_is_assert(index);
+  let item = list[index];
 }
