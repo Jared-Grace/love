30d65179edcab034fb4089e5317680f177b86abf
JESUS rose to life <username@users.noreply.github.com>
2026-05-08 16:51:54 -0400

    function_auto_path ../love/public/src/folder_user_downloads_path.mjs

--- public/src/folder_user_downloads_path.mjs
+++ public/src/folder_user_downloads_path.mjs
@@ -1,4 +1,5 @@
+import { folder_user_path_join } from "../../../love/public/src/folder_user_path_join.mjs";
 export function folder_user_downloads_path() {
-  let r = "/home/j/Downloads/";
+  let result = folder_user_path_join("Downloads/", file_name);
   return r;
 }
