4f6351732fc7844a5b0c202ca18d5a35a3365c59
JESUS rose to life <username@users.noreply.github.com>
2026-05-08 16:54:38 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_split_newline } from "../../../love/public/src/text_split_newline.mjs";
 import { file_read } from "../../../love/public/src/file_read.mjs";
 import { folder_user_downloads_path } from "../../../love/public/src/folder_user_downloads_path.mjs";
@@ -5,5 +6,7 @@ export async function sandbox_3() {
   let r = folder_user_downloads_path("contacts.csv");
   let contents = await file_read(r);
   let lines = text_split_newline(contents);
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
   return lines;
 }
