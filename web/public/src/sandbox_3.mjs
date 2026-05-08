d9ed89fae39fd8acc024031d2517a74b063707f5
JESUS rose to life <username@users.noreply.github.com>
2026-05-08 16:52:54 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,4 +1,6 @@
+import { file_open } from "../../../love/public/src/file_open.mjs";
 import { folder_user_downloads_path } from "../../../love/public/src/folder_user_downloads_path.mjs";
 export async function sandbox_3() {
   let r = folder_user_downloads_path("contacts.csv");
+  await file_open(f_path);
 }
