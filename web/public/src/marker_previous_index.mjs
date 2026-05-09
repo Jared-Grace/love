24da1caaacb7dc996ce9c6ee497d8688364c9abd
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:38:07 -0400

    function_auto_path ../love/public/src/marker_previous_index.mjs

--- web/public/src/marker_previous_index.mjs
+++ web/public/src/marker_previous_index.mjs
@@ -1,7 +1,9 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_index_of_previous } from "../../../love/public/src/list_index_of_previous.mjs";
 import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs";
 export function marker_previous_index(a) {
-  let { stack2, stack1 } = a;
+  let stack1 = property_get(a, "stack1");
+  let stack2 = property_get(a, "stack2");
   let index = list_index_of_previous(stack2, stack1);
   const p = object_merge_set(
     {
