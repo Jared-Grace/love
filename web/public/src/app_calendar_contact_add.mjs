89e6ba9ba5ee213d51e39b975e0348a2da63b09b
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:52:50 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

---
+++ web/public/src/app_calendar_contact_add.mjs
@@ -0,0 +1,36 @@
+import { log_keep } from "../../../love/public/src/log_keep.mjs";
+import { object_pick } from "../../../love/public/src/object_pick.mjs";
+import { list_find_property_try_or_null_curried } from "../../../love/public/src/list_find_property_try_or_null_curried.mjs";
+import { list_single } from "../../../love/public/src/list_single.mjs";
+import { object_merge_match } from "../../../love/public/src/object_merge_match.mjs";
+import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
+import { list_multiple_not_is_assert } from "../../../love/public/src/list_multiple_not_is_assert.mjs";
+import { object_values_map_list_unique } from "../../../love/public/src/object_values_map_list_unique.mjs";
+import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
+import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+export function app_calendar_contact_add(data, item) {
+  const properties_unique_across_all = [
+    "facebook_conversation_url",
+    "facebook_conversation_id",
+    "facebook_url_id",
+  ];
+  let picked = object_pick(item, properties_unique_across_all);
+  let contacts = app_calendar_contacts_initialize(data);
+  let c = list_find_property_try_or_null_curried(contacts);
+  function lambda(value, key) {
+    let existing = c(key, value);
+    return existing;
+  }
+  let unique = object_values_map_list_unique(picked, lambda);
+  let list = list_filter_null_not_is(unique);
+  list_multiple_not_is_assert(list);
+  let s1 = list_size_1(list);
+  if (s1) {
+    let only = list_single(list);
+    object_merge_match(only, item);
+  } else {
+    list_add(contacts, item);
+    log_keep(app_calendar_contact_add.name, message);
+  }
+}
