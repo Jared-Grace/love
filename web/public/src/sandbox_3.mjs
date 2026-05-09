19a561b6ce12ebffa45dee4f1ddf36607b26cfa9
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:17:57 -0400

    s curry

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -32,9 +32,9 @@ export async function sandbox_3() {
   }
   let mapped = list_map(lines, lambda);
   async function lambda2(data) {
-    function lambda3(item) {}
-    each(list, lambda3);
-    app_calendar_contact_add(data, value);
+    function lambda3(value) {
+    app_calendar_contact_add(data, value);}
+    each(mapped, lambda3);
   }
   await app_calendar_secret_transform(lambda2);
   return mapped;
