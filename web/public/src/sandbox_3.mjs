4cb8903eac3264d8cc83861a1a4967439defa01f
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 19:24:47 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,4 +1,3 @@
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_filter_property_exists_map_fn } from "../../../love/public/src/list_filter_property_exists_map_fn.mjs";
 import { app_calendar_download_contacts } from "../../../love/public/src/app_calendar_download_contacts.mjs";
 import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calendar_whatsapp_phone.mjs";
@@ -9,9 +8,7 @@ export async function sandbox_3() {
   let phones_stored = list_filter_property_exists_map_fn(
     list,
     app_calendar_whatsapp_phone,
-  );
-  function lambda(la) {}
-  let list2 = list_adder(lambda);
+  );ldf
   let r = {
     phones: phones_asked,
     mapped: phones_stored,
