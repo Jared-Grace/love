c244be13f9235fd2db117909a18349307d6f10de
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 19:16:15 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- public/src/app_calendar_contact_add.mjs
+++ public/src/app_calendar_contact_add.mjs
@@ -1,3 +1,4 @@
+import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calendar_whatsapp_phone.mjs";
 import { object_pick_try } from "../../../love/public/src/object_pick_try.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 import { list_find_property_try_or_null_curried } from "../../../love/public/src/list_find_property_try_or_null_curried.mjs";
@@ -10,11 +11,12 @@ import { list_filter_null_not_is } from "../../../love/public/src/list_filter_nu
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_calendar_contact_add(data, item) {
+  let r = app_calendar_whatsapp_phone();
   const properties_unique_across_all = [
     "facebook_conversation_url",
     "facebook_conversation_id",
     "facebook_url_id",
-    "whatsapp_phone",
+    r,
   ];
   let picked = object_pick_try(item, properties_unique_across_all);
   let contacts = app_calendar_contacts_initialize(data);
