8b74377e55179e7718b9502f19fa3535d48a3b56
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:38:13 -0400

    function_auto_path ../love/public/src/ebible_chapters_codes_to_verses.mjs

--- public/src/app_sandbox_main.mjs
+++ public/src/app_sandbox_main.mjs
@@ -6,7 +6,7 @@ import { app_message } from "../../../love/public/src/app_message.mjs";
 import { app_message_provide_travel } from "../../../love/public/src/app_message_provide_travel.mjs";
 import { app_message_provide_food } from "../../../love/public/src/app_message_provide_food.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
-export function app_sandbox_main(context) {
+export async function app_sandbox_main(context) {
   let root = property_get(context, "root");
   html_clear(root);
   let p = html_p_text(root, "text");
@@ -19,5 +19,5 @@ export function app_sandbox_main(context) {
       provide_food: app_message_provide_food,
     },
   });
-  app_shared_refresh(context);
+  await app_shared_refresh(context);
 }
