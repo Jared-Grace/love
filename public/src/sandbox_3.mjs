5bab6d2ba13bdd24f09bd7169537491e2f997cfb
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 19:24:49 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { list_filter_property_exists_map_fn } from "../../../love/public/src/list_filter_property_exists_map_fn.mjs";
 import { app_calendar_download_contacts } from "../../../love/public/src/app_calendar_download_contacts.mjs";
 import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calendar_whatsapp_phone.mjs";
@@ -8,7 +9,8 @@ export async function sandbox_3() {
   let phones_stored = list_filter_property_exists_map_fn(
     list,
     app_calendar_whatsapp_phone,
-  );ldf
+  );
+  let difference = list_difference(list2, list_other);
   let r = {
     phones: phones_asked,
     mapped: phones_stored,
