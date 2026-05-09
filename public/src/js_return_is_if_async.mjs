0bc29c1ad88fbbac8d2efe3fcc7fb72edaeb7805
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:22:53 -0400

    function_auto_path ../love/public/src/js_return_is_if_async.mjs

--- public/src/js_return_is_if_async.mjs
+++ public/src/js_return_is_if_async.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
-export function js_return_is_if_async(node, lambda) {
+export async function js_return_is_if_async(node, lambda) {
   js_node_type_is_if(node, "ReturnStatement", lambda);
 }
