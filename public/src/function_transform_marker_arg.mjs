97cedb1e808c67ae779ca920e9a6e74b248df5a2
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:38:10 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_expressions_primary_abbreviation_ex.mjs

--- public/src/function_transform_marker_arg.mjs
+++ public/src/function_transform_marker_arg.mjs
@@ -1,7 +1,8 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs";
 import { list_get_end } from "../../../love/public/src/list_get_end.mjs";
 export function function_transform_marker_arg(v, ast) {
-  let { stack } = v;
+  let stack = property_get(v, "stack");
   let stack1 = list_get_end(stack, 1);
   let stack2 = list_get_end(stack, 2);
   let a = object_merge_set(
