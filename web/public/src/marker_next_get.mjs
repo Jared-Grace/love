a7a0aae75b1da369b659b35e5013d1fa59a631ef
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:38:04 -0400

    function_auto_path ../love/public/src/marker_next_get.mjs

--- web/public/src/marker_next_get.mjs
+++ web/public/src/marker_next_get.mjs
@@ -1,9 +1,11 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs";
 import { marker_next_index } from "../../../love/public/src/marker_next_index.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 export function marker_next_get(a) {
   let ni = marker_next_index(a);
-  let { index, stack2 } = ni;
+  let stack2 = property_get(ni, "stack2");
+  let index = property_get(ni, "index");
   let next = list_get(stack2, index);
   let n = {
     next,
