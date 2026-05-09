f3b4bdee5dfb40fa6180f6cd5b5ba22add27209d
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:53:46 -0400

    function_auto_path ../love/public/src/app_calendar_url_ids.mjs

--- public/src/app_calendar_url_ids.mjs
+++ public/src/app_calendar_url_ids.mjs
@@ -1,12 +1,10 @@
+import { app_calendar_preaching_ask_entries } from "../../../love/public/src/app_calendar_preaching_ask_entries.mjs";
 import { list_map_suffix_without_try_multiple } from "../../../love/public/src/list_map_suffix_without_try_multiple.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
 import { list_filter_starts_with_https_prefix } from "../../../love/public/src/list_filter_starts_with_https_prefix.mjs";
-import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
-import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 export async function app_calendar_url_ids() {
-  let lines = await folder_user_docs_read_lines("preaching_ask.txt");
-  let filtered = list_filter_empty_not_is(lines);
+  let filtered = await app_calendar_preaching_ask_entries();
   let urls = list_filter_starts_with_https_prefix(filtered);
   const p1 = "https://www.facebook.com/profile.php?id=";
   const p2 = "https://www.facebook.com/";
