7ffb831d8472a18feccb0fbfb8b42dd8537ad789
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:59:33 -0400

    s map,replace

--- public/src/app_calendar_secret_whatsapp_add.mjs
+++ public/src/app_calendar_secret_whatsapp_add.mjs
@@ -17,7 +17,8 @@ import { file_read } from "../../../love/public/src/file_read.mjs";
 import { folder_user_downloads_path } from "../../../love/public/src/folder_user_downloads_path.mjs";
 export async function app_calendar_secret_whatsapp_add() {
   let filtered = await app_calendar_preaching_ask_entries();
-  let filtered3 = list_filter_starts_with(list, prefix);
+  let phones = list_filter_starts_with(filtered, '+');
+  
   let r = folder_user_downloads_path("contacts.csv");
   let contents = await file_read(r);
   let lines = text_split_newline(contents);
