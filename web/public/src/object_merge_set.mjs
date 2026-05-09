3f6eb143c1ff1a56fb2c20586e666292403da6e5
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:38:52 -0400

    function_auto_path ../love/public/src/object_merge_set.mjs

--- web/public/src/object_merge_set.mjs
+++ web/public/src/object_merge_set.mjs
@@ -1,9 +1,5 @@
-import { object_merge_generic } from "../../../love/public/src/object_merge_generic.mjs";
+import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function object_merge_set(to, from) {
   object_merge(to, from);
   return to;
-}
-
-function object_merge(to, from) {
-  object_merge_generic("strict", to, from);
 }
