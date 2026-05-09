3cd29182c84d3cfa9f401770d4a9d72a5e2d3ee8
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 19:25:54 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,15 +1,5 @@
-import { list_difference } from "../../../love/public/src/list_difference.mjs";
-import { list_filter_property_exists_map_fn } from "../../../love/public/src/list_filter_property_exists_map_fn.mjs";
-import { app_calendar_download_contacts } from "../../../love/public/src/app_calendar_download_contacts.mjs";
-import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calendar_whatsapp_phone.mjs";
-import { app_calendar_preaching_ask_phones } from "../../../love/public/src/app_calendar_preaching_ask_phones.mjs";
+import { app_calendar_contacts_phones_missing } from "../../../love/public/src/app_calendar_contacts_phones_missing.mjs";
 export async function sandbox_3() {
-  let phones_asked = await app_calendar_preaching_ask_phones();
-  let list = await app_calendar_download_contacts();
-  let phones_stored = list_filter_property_exists_map_fn(
-    list,
-    app_calendar_whatsapp_phone,
-  );
-  let add_to_contacts = list_difference(phones_asked, phones_stored);
+  let add_to_contacts = await app_calendar_contacts_phones_missing();
   return add_to_contacts;
 }
