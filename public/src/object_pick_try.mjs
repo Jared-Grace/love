3dccacd0be65db9d624f22ddc93566bdef113b86
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:26:58 -0400

    function_auto_path ../love/public/src/object_pick_try.mjs

--- public/src/object_pick_try.mjs
+++ public/src/object_pick_try.mjs
@@ -1,7 +1,6 @@
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
-import { property_get_curried } from "../../../love/public/src/property_get_curried.mjs";
 export function object_pick_try(object, property_names) {
-  let c = property_get_curried(object);
+  let c = property_get_try_curried(object);
   let picked = list_to_dictionary_value(property_names, c);
   return picked;
 }
