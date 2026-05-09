03ae368dc81af65753b19b6d1d6ac6e08a997d2c
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:40:32 -0400

    function_auto_path ../love/public/src/file_backup_fn.mjs

--- public/src/file_backup_fn.mjs
+++ public/src/file_backup_fn.mjs
@@ -1,5 +1,7 @@
+import { function_is } from "../../../love/public/src/function_is.mjs";
 import { file_backup } from "../../../love/public/src/file_backup.mjs";
 export async function file_backup_fn(f_name) {
+  let fi2 = function_is(f);
   let r = await file_backup(file_path);
   return r;
 }
