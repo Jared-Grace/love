026fc314f9ce6a448a9d96fa0a8a4e5c59af3cee
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:22:23 -0400

    function_auto_path ../love/public/src/js_return_is_if.mjs

--- web/public/src/js_return_is_if.mjs
+++ web/public/src/js_return_is_if.mjs
@@ -1,4 +1,4 @@
 import { js_node_type_is_if } from "../../../love/public/src/js_node_type_is_if.mjs";
-export function js_return_is_if(second, lambda5) {
-  js_node_type_is_if(second, "ReturnStatement", lambda5);
+export function js_return_is_if(node, lambda) {
+  js_node_type_is_if(node, "ReturnStatement", lambda);
 }
