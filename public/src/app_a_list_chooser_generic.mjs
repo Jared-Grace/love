4f045bdb7510020dc3f85a8030f2dd56c02c5c57
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 17:34:09 -0400

    function_auto_path ../love/public/src/app_a_list_chooser.mjs

---
+++ public/src/app_a_list_chooser_generic.mjs
@@ -0,0 +1,61 @@
+import { html_value_set } from "../../../love/public/src/html_value_set.mjs";
+import { list_remove } from "../../../love/public/src/list_remove.mjs";
+import { html_focus } from "../../../love/public/src/html_focus.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+import { app_a_button_wide } from "../../../love/public/src/app_a_button_wide.mjs";
+import { list_sort_text_alpha_size } from "../../../love/public/src/list_sort_text_alpha_size.mjs";
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
+import { html_on_input } from "../../../love/public/src/html_on_input.mjs";
+import { list_filter_text_match_ordered } from "../../../love/public/src/list_filter_text_match_ordered.mjs";
+import { html_value_get } from "../../../love/public/src/html_value_get.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+import { app_a_input } from "../../../love/public/src/app_a_input.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
+import { app_a_control_style } from "../../../love/public/src/app_a_control_style.mjs";
+import { html_div_text_centered } from "../../../love/public/src/html_div_text_centered.mjs";
+import { text_articled } from "../../../love/public/src/text_articled.mjs";
+export function app_a_list_chooser_generic(root, noun, texts, lambda$text) {
+  const articled = text_articled(noun);
+  const text = "Choose " + articled + ":";
+  let d = html_div_text_centered(root, text);
+  app_a_control_style(d);
+  html_style_background_color_set(d, "white");
+  let filtered = null;
+  let input = app_a_input(root);
+  let f_names_div = html_div(root);
+  function on_input() {
+    let value = html_value_get(input);
+    filtered = list_filter_text_match_ordered(texts, value);
+    refresh();
+  }
+  html_on_input(input, on_input);
+  filtered = texts;
+  refresh();
+  function refresh() {
+    html_clear(f_names_div);
+    list_sort_text_alpha_size(filtered);
+    function lambda(text) {
+      async function on_click() {
+        await f_name_select(text);
+      }
+      app_a_button_wide(f_names_div, text, on_click);
+    }
+    each(filtered, lambda);
+  }
+  html_focus(input);
+  async function f_name_select(text) {
+    list_remove(on_keydowns, on_keydown);
+    await lambda$text(text);
+  }
+  function input_set(value) {
+    html_value_set(input, value);
+    on_input();
+  }
+  function filtered_get() {
+    return filtered;
+  }
+  let v4 = {
+    input_set,
+    filtered_get,
+  };
+}
