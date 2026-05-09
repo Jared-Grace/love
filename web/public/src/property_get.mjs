46d08130848f09da3441c314ec8cae2c53d6eaf4
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 15:27:55 -0400

    function_auto_path ../love/public/src/property_get.mjs

--- web/public/src/property_get.mjs
+++ web/public/src/property_get.mjs
@@ -1,8 +1,6 @@
 import { undefined_not_is_assert_object_property } from "../../../love/public/src/undefined_not_is_assert_object_property.mjs";
-import { property_get_try } from "../../../love/public/src/property_get_try.mjs";
 export function property_get(object, property_name) {
-  let value = property_get_try(object, property_name);
+  let value = object[property_name];
   undefined_not_is_assert_object_property(value, object, property_name);
   return value;
-  let value2 = object[property_name];
 }
