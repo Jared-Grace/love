2936127fa0d43e94f9f199e7a05a3731bdcbe040
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 19:23:52 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -4,7 +4,7 @@ import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calend
 import { app_calendar_preaching_ask_phones } from "../../../love/public/src/app_calendar_preaching_ask_phones.mjs";
 export async function sandbox_3() {
   let phones = await app_calendar_preaching_ask_phones();
-  let list = await app_calendar_download_contacts();
+  let list = await app_calendar_download_contacts();$a
   let fn = app_calendar_whatsapp_phone;
   let mapped = list_filter_property_exists_map_fn(list, fn);
   let r = {
