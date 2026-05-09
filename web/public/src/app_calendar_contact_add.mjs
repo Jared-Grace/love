7ef26cb8d19b0f65b74c03b5110fc3eb0f4b07a9
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:46:14 -0400

    function_auto_path ../love/public/src/app_calendar_contact_add.mjs

--- web/public/src/app_calendar_contact_add.mjs
+++ web/public/src/app_calendar_contact_add.mjs
@@ -1,10 +1,10 @@
+import { list_find_property_try_or_null_curried } from "../../../love/public/src/list_find_property_try_or_null_curried.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { object_merge_match } from "../../../love/public/src/object_merge_match.mjs";
 import { list_size_1 } from "../../../love/public/src/list_size_1.mjs";
 import { list_multiple_not_is_assert } from "../../../love/public/src/list_multiple_not_is_assert.mjs";
 import { object_values_map_list_unique } from "../../../love/public/src/object_values_map_list_unique.mjs";
 import { list_filter_null_not_is } from "../../../love/public/src/list_filter_null_not_is.mjs";
-import { list_find_property_or_null_curried } from "../../../love/public/src/list_find_property_or_null_curried.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function app_calendar_contact_add(data, item) {
