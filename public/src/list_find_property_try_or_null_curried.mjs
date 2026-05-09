e9816bd731d173646b646384c3c1c1ae4d1d78b6
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:45:52 -0400

    cu list_find_property_try_or_null

---
+++ public/src/list_find_property_try_or_null_curried.mjs
@@ -0,0 +1,10 @@
+import { list_find_property_try_or_null } from "../../../love/public/src/list_find_property_try_or_null.mjs";
+export function list_find_property_try_or_null_curried(list) {
+  let c = function list_find_property_try_or_null_curried_result(
+    property_name,
+    property_value,
+  ) {
+    return list_find_property_try_or_null(list, property_name, property_value);
+  };
+  return c;
+}
