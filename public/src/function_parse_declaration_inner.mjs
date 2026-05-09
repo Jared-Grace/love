8d803a881dfd3249fc4d4ce763d2f03896082d08
JESUS rose to life <username@users.noreply.github.com>
2026-05-09 14:38:04 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_expressions_primary_abbreviations.mjs

--- public/src/function_parse_declaration_inner.mjs
+++ public/src/function_parse_declaration_inner.mjs
@@ -1,7 +1,8 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs";
 import { js_flo } from "../../../love/public/src/js_flo.mjs";
 export function function_parse_declaration_inner(parsed) {
-  let { ast } = parsed;
+  let ast = property_get(parsed, "ast");
   let declaration = js_flo(ast);
   let to = object_merge_set(
     {
