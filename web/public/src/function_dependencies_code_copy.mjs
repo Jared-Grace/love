cbabf3dfe7d07ee686b8d0a232fec4d018f7c8ab
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:42:59 -0400

    function_auto_path ../love/public/src/function_dependencies_code_copy.mjs

--- web/public/src/function_dependencies_code_copy.mjs
+++ web/public/src/function_dependencies_code_copy.mjs
@@ -1,3 +1,4 @@
+import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { function_dependencies_code_multiple } from "../../../love/public/src/function_dependencies_code_multiple.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -6,6 +7,7 @@ import { function_name_unalias_only } from "../../../love/public/src/function_na
 export async function function_dependencies_code_copy(f_name) {
   arguments_assert(arguments, 1);
   let unaliased = await function_name_unalias_only(f_name);
+  let split = text_split_comma_dot(t);
   let v = await function_dependencies_code_multiple(unaliased);
   let code = property_get(v, "code");
   await clipboard_copy(code);
