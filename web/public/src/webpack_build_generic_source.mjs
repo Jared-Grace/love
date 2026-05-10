ffa0d5d1b2a04c57f60003142422311c6abcd2ea
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:49:02 -0400

    function_auto_path ../love/public/src/webpack_build_generic_source.mjs

--- web/public/src/webpack_build_generic_source.mjs
+++ web/public/src/webpack_build_generic_source.mjs
@@ -6,8 +6,8 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_context_initialize } from "../../../love/public/src/app_context_initialize.mjs";
 export function webpack_build_generic_source(f_name) {
   let fns = [f_name, app_context_initialize.name];
-  let mapped = list_map(fns, webpack_build_code_import);
   let call = js_code_call_app_context_initialize(f_name);
+  let mapped = list_map(fns, webpack_build_code_import);
   list_add(mapped, call);
   let joined = list_join_newline(mapped);
   return joined;
