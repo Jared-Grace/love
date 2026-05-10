5f2821082983eb04fe9d8fa9b5ef19b345c84aff
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:44:05 -0400

    function_auto_path ../love/public/src/functions_dependencies_code_copy.mjs

--- public/src/functions_dependencies_code_copy.mjs
+++ public/src/functions_dependencies_code_copy.mjs
@@ -5,7 +5,7 @@ import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs"
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
-export async function function_dependencies_code_copy(f_names) {
+export async function functions_dependencies_code_copy(f_names) {
   arguments_assert(arguments, 1);
   let split = text_split_comma_dot(f_names);
   let waited = await list_map_unordered_async(
