652034d15b4957eb9e58f0fcc4a2e9fd299b4af6
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 19:44:37 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,7 +1,9 @@
+import { app_calendar_download } from "../../../love/public/src/app_calendar_download.mjs";
 import { app_calendar_url_ids } from "../../../love/public/src/app_calendar_url_ids.mjs";
 import { app_calendar_contacts_phones_missing_text } from "../../../love/public/src/app_calendar_contacts_phones_missing_text.mjs";
 export async function sandbox_3() {
   let url_ids = await app_calendar_url_ids();
+  let data = await app_calendar_download();
   return;
   let joined = await app_calendar_contacts_phones_missing_text();
 }
