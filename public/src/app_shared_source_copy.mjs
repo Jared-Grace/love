d067565dd91fec5c6aa7990e5a46d5c9354907e9
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:55:55 -0400

    function_auto_path ../love/public/src/app_shared_source_copy.mjs

--- public/src/app_shared_source_copy.mjs
+++ public/src/app_shared_source_copy.mjs
@@ -6,7 +6,7 @@ import { webpack_build_generic_source_fn_names } from "../../../love/public/src/
 import { js_code_call_app_context_initialize } from "../../../love/public/src/js_code_call_app_context_initialize.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
-export async function app_shared_source(search) {
+export async function app_shared_source_copy(search) {
   let a = await app_shared_name_search_info(search);
   let f_name = property_get(a, "f_name");
   let a_name = property_get(a, "a_name");
