2197eb22c52c62ab991f9c4229ef1bfeb9824ed2
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 19:48:49 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -7,14 +7,14 @@ import { app_calendar_url_ids } from "../../../love/public/src/app_calendar_url_
 import { app_calendar_contacts_phones_missing_text } from "../../../love/public/src/app_calendar_contacts_phones_missing_text.mjs";
 export async function sandbox_3() {
   let url_ids = await app_calendar_url_ids();
-  let data = await app_calendar_download_contacts();
+  let contacts = await app_calendar_download_contacts();
   let property = app_calendar_facebook_url_id();
   function lambda(item) {
     let value = property_get_or_null(item, property);
     let includes = list_includes(url_ids, value);
     return includes;
   }
-  let filtered = list_filter(data, lambda);
+  let filtered = list_filter(contacts, lambda);
   return filtered;
   let joined = await app_calendar_contacts_phones_missing_text();
 }
