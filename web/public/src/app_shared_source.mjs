c63326996b20616db34fe5161516d067b0252737
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:55:32 -0400

    function_auto_path ../love/public/src/app_shared_source.mjs

--- web/public/src/app_shared_source.mjs
+++ web/public/src/app_shared_source.mjs
@@ -14,6 +14,6 @@ export async function app_shared_source(search) {
   let fns = webpack_build_generic_source_fn_names(f_name);
   let code = await functions_dependencies_code_split(fns);
   let body = list_join_newline([call, code]);
-  let contents = html_code(name, body);
+  let contents = html_code(a_name, body);
   await clipboard_copy(contents);
 }
