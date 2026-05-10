e18cbfe39f4d4d86984af6f673efb28ea7e8086c
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:47:59 -0400

    function_auto_path ../love/public/src/app_shared_source.mjs

--- public/src/app_shared_source.mjs
+++ public/src/app_shared_source.mjs
@@ -1,8 +1,8 @@
 import { webpack_build_generic_source } from "../../../love/public/src/webpack_build_generic_source.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
-export async function app_shared_source() {
-  let s = await app_shared_name_search_info(search);
+export async function app_shared_source(search) {
+  let a = await app_shared_name_search_info(search);
   let f_name = property_get(a, "f_name");
   let joined = webpack_build_generic_source(f_name);
   return joined;
