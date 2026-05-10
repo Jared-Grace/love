b35eb6187f147aaaaa089a63136cd86c6fe8ae75
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:51:38 -0400

    function_auto_path ../love/public/src/functions_dependencies_code_copy.mjs

--- web/public/src/functions_dependencies_code_copy.mjs
+++ web/public/src/functions_dependencies_code_copy.mjs
@@ -1,8 +1,10 @@
+import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
 import { functions_dependencies_code_copy_split } from "../../../love/public/src/functions_dependencies_code_copy_split.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 export async function functions_dependencies_code_copy(f_names) {
   arguments_assert(arguments, 1);
   let split = text_split_comma_dot(f_names);
-  await functions_dependencies_code_copy_split(split);
+  let code = await functions_dependencies_code_copy_split(split);
+  await clipboard_copy(code);
 }
