f9331c057e5111943db1114f01cdb58e07073db0
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:47:13 -0400

    function_auto_path ../love/public/src/app_shared_source.mjs

--- public/src/app_shared_source.mjs
+++ public/src/app_shared_source.mjs
@@ -1,1 +1,4 @@
-export function app_shared_source() {}
+import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
+export async function app_shared_source() {
+  let info = await app_shared_name_search_info(search);
+}
