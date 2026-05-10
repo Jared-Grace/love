681499d531692930751c1937eb0f6ff0742cf8e6
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:50:50 -0400

    function_auto_path ../love/public/src/functions_dependencies_code_copy.mjs

--- public/src/functions_dependencies_code_copy.mjs
+++ public/src/functions_dependencies_code_copy.mjs
@@ -1,18 +1,8 @@
-import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
+import { functions_dependencies_code_copy_split } from "../../../love/public/src/functions_dependencies_code_copy_split.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
-import { function_dependencies_code_multiple } from "../../../love/public/src/function_dependencies_code_multiple.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
-import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function functions_dependencies_code_copy(f_names) {
   arguments_assert(arguments, 1);
   let split = text_split_comma_dot(f_names);
-  let waited = await list_map_unordered_async(
-    split,
-    function_name_unalias_only,
-  );
-  let v = await function_dependencies_code_multiple(waited);
-  let code = property_get(v, "code");
-  await clipboard_copy(code);
+  await functions_dependencies_code_copy_split(split);
 }
