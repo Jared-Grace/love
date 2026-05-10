b5a35884ee5097c6247cbd9d819545d2832f625e
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 15:51:04 -0400

    function_auto_path ../love/public/src/app_shared_source.mjs

--- public/src/app_shared_source.mjs
+++ public/src/app_shared_source.mjs
@@ -1,6 +1,4 @@
 import { functions_dependencies_code_copy_split } from "../../../love/public/src/functions_dependencies_code_copy_split.mjs";
-import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
-import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { webpack_build_generic_source_fn_names } from "../../../love/public/src/webpack_build_generic_source_fn_names.mjs";
 import { js_code_call_app_context_initialize } from "../../../love/public/src/js_code_call_app_context_initialize.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -10,8 +8,6 @@ export async function app_shared_source(search) {
   let f_name = property_get(a, "f_name");
   let call = js_code_call_app_context_initialize(f_name);
   let fns = webpack_build_generic_source_fn_names(f_name);
-  arguments_assert(arguments, 1);
-  let split = text_split_comma_dot(f_names);
-  await functions_dependencies_code_copy_split(split);
+  await functions_dependencies_code_copy_split(fns);
   return joined;
 }
