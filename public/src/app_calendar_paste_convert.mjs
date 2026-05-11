b3bfc34ae8c6e6d6781347f8da838e1c3c9d7172
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:29:16 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- public/src/app_calendar_paste_convert.mjs
+++ public/src/app_calendar_paste_convert.mjs
@@ -1,7 +1,6 @@
 import { list_first_second_only } from "../../../love/public/src/list_first_second_only.mjs";
 import { text_trim } from "../../../love/public/src/text_trim.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -11,7 +10,7 @@ export async function app_calendar_paste_convert(input) {
   let split = text_split(input, "⋅");
   let r2 = list_first_second_only(split);
   let time_range = property_get(r2, "second");
-  let split2 = text_split_comma(time_range);
+  let split2 = text_split(time_range, ",");
   let mapped = list_map(split2, text_trim);
   let r3 = list_first_second_only(mapped);
   let second = property_get(r3, "second");
