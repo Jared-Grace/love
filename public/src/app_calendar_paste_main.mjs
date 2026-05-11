d7d4fb8c0c9bff4064bd67c61d2b666d3b07eae7
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 19:23:06 -0400

    function_auto_path ../love/public/src/app_calendar_paste_main.mjs

--- public/src/app_calendar_paste_main.mjs
+++ public/src/app_calendar_paste_main.mjs
@@ -1,12 +1,12 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
 import { clipboard_transform } from "../../../love/public/src/clipboard_transform.mjs";
-import { app_calendar_paste_convert } from "../../../love/public/src/app_calendar_paste_convert.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_calendar_paste_main(context) {
   let root = property_get(context, "root");
   async function lambda2() {
-    await clipboard_transform(app_calendar_paste_convert);
+    function lambda() {}
+    await clipboard_transform(lambda);
   }
   let p = html_p_text(
     root,
