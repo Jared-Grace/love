8932b8ec80fb2028fc3c24dd9a3e09b4d6616730
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:45:25 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- web/public/src/app_calendar_main.mjs
+++ web/public/src/app_calendar_main.mjs
@@ -1,3 +1,4 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_filter_property_exclude_if_exists } from "../../../love/public/src/list_filter_property_exclude_if_exists.mjs";
 import { json_to } from "../../../love/public/src/json_to.mjs";
@@ -52,7 +53,8 @@ export async function app_calendar_main(context) {
   const properties_name = ["whatsapp_name", "facebook_name"];
   function lambda7(item) {
     let picked = object_pick_try(o, properties_name);
-    let properties = properties_get(picked);lsg
+    let properties = properties_get(picked);
+    let only = list_single(list2);
   }
   let mapped2 = list_map(list, lambda7);
   function lambda6(o) {
