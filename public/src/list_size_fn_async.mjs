7056a3a0c0273b9ad24b58b60fe9fafa2cd3de68
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:36:43 -0400

    function_auto_path ../love/public/src/list_size_fn_async.mjs

--- public/src/list_size_fn_async.mjs
+++ public/src/list_size_fn_async.mjs
@@ -1,5 +1,7 @@
+import { app_calendar_download_contacts } from "../../../love/public/src/app_calendar_download_contacts.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-export function list_size_fn_async(list) {
+export async function list_size_fn_async(list) {
+  let list2 = await app_calendar_download_contacts();
   let size = list_size(list);
   return size;
 }
