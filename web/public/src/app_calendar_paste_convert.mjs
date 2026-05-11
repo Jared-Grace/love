2bf7da2df3d8eb09fd52215449eb47c6933ca545
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:35:43 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- web/public/src/app_calendar_paste_convert.mjs
+++ web/public/src/app_calendar_paste_convert.mjs
@@ -1,4 +1,4 @@
-import { date_time_zone_past_is } from "../../../love/public/src/date_time_zone_past_is.mjs";
+import { date_time_zone_future_is } from "../../../love/public/src/date_time_zone_future_is.mjs";
 import { date_time_zone_parse } from "../../../love/public/src/date_time_zone_parse.mjs";
 import { text_split_dash_en } from "../../../love/public/src/text_split_dash_en.mjs";
 import { list_first_second_only } from "../../../love/public/src/list_first_second_only.mjs";
@@ -25,7 +25,8 @@ export async function app_calendar_paste_convert(input) {
   const zone = "America/New_York";
   const format = "cccc, LLL dd yyyy h:mma";
   const dt = date_time_zone_parse(input_luxon, format, zone);
-  if (date_time_zone_past_is(dt, zone)) {
+  let f = date_time_zone_future_is(dt2, zone2);
+  if ((dt, zone)) {
     throw new Error("DateTime is not in the future");
   }
   return dt;
