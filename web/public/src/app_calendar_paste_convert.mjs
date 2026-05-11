49c9c03674345c03a37c691872dfd956b85c88ff
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:39:27 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- web/public/src/app_calendar_paste_convert.mjs
+++ web/public/src/app_calendar_paste_convert.mjs
@@ -8,8 +8,7 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { import_install } from "../../../love/public/src/import_install.mjs";
-export  function app_calendar_paste_convert(input) {
+export async function app_calendar_paste_convert(input) {
   input = "Monday, May 11⋅11:00am – 12:00pm";
   let split = text_split(input, "⋅");
   let r2 = list_first_second_only(split);
