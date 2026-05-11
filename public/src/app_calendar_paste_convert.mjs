6211189370c3a122c22932fb801072736377cb7a
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:07:03 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- public/src/app_calendar_paste_convert.mjs
+++ public/src/app_calendar_paste_convert.mjs
@@ -1,3 +1,4 @@
+import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
@@ -8,6 +9,7 @@ export async function app_calendar_paste_convert(input) {
   let split = text_split(input, "⋅");
   let r2 = list_first_second(list);
   let second = property_get(r2, "second");
+  let split2 = text_split_comma(t);
   let first = property_get(r2, "first");
   let r = await import_install("luxon");
   let DateTime = property_get(r, "DateTime");
