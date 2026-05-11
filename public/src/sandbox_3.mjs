fb48e748eee0c221c3a456924ded8dcac3f8ef65
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:34:45 -0400

    pf list_filter_includes_not list

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -7,7 +7,7 @@ export async function sandbox_3() {
   let url_ids = await app_calendar_url_ids();
   let contacts = await app_calendar_download_contacts();
   let property = app_calendar_facebook_url_id();
-  let filtered = list_filter_includes_not(property, url_ids, contacts);
+  let filtered = list_filter_includes_not(contacts, property, url_ids);
   return filtered;
   let joined = await app_calendar_contacts_phones_missing_text();
 }
