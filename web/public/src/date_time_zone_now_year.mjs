c5f8f19b5372f3f056ffdc2f53657f7700f04c23
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:38:46 -0400

    function_auto_path ../love/public/src/date_time_zone_now_year.mjs

--- web/public/src/date_time_zone_now_year.mjs
+++ web/public/src/date_time_zone_now_year.mjs
@@ -1,4 +1,5 @@
 import { date_time_zone_now } from "../../../love/public/src/date_time_zone_now.mjs";
 export function date_time_zone_now_year() {
-  return date_time_zone_now();
+  let r = date_time_zone_now();
+  return r;
 }
