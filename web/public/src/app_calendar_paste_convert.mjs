75e1661fde97ae43fb5d0b35b6292433d072c2d7
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:29:51 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- web/public/src/app_calendar_paste_convert.mjs
+++ web/public/src/app_calendar_paste_convert.mjs
@@ -1,3 +1,4 @@
+import { text_split_dash_en } from "../../../love/public/src/text_split_dash_en.mjs";
 import { list_first_second_only } from "../../../love/public/src/list_first_second_only.mjs";
 import { text_trim } from "../../../love/public/src/text_trim.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -10,7 +11,7 @@ export async function app_calendar_paste_convert(input) {
   let split = text_split(input, "⋅");
   let r2 = list_first_second_only(split);
   let time_range = property_get(r2, "second");
-  let split2 = text_split(time_range, "–");
+  let split2 = text_split_dash_en(time_range);
   let mapped = list_map(split2, text_trim);
   let r3 = list_first_second_only(mapped);
   let second = property_get(r3, "second");
