0ba5a4459dda6cf87e4f20899d7550e614ecc794
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:34:41 -0400

    function_auto_path ../love/public/src/list_filter_includes_not.mjs

--- public/src/list_filter_includes_not.mjs
+++ public/src/list_filter_includes_not.mjs
@@ -2,15 +2,15 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_includes_not } from "../../../love/public/src/list_includes_not.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { property_get_or_null } from "../../../love/public/src/property_get_or_null.mjs";
-export function list_filter_includes_not(property, list, contacts) {
+export function list_filter_includes_not(property, list_other, list) {
   function lambda(item) {
     let value = property_get_or_null(item, property);
     if (null_is(value)) {
       return false;
     }
-    let includes = list_includes_not(list, value);
+    let includes = list_includes_not(list_other, value);
     return includes;
   }
-  let filtered = list_filter(contacts, lambda);
+  let filtered = list_filter(list, lambda);
   return filtered;
 }
