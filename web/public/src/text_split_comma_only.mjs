0519c28e961e1b05ef47a8ade6396a18c0d25ded
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:25:58 -0400

    function_auto_path ../love/public/src/text_split_comma_only.mjs

--- web/public/src/text_split_comma_only.mjs
+++ web/public/src/text_split_comma_only.mjs
@@ -1,5 +1,7 @@
+import { list_size_1_assert } from "../../../love/public/src/list_size_1_assert.mjs";
 import { text_split_comma } from "../../../love/public/src/text_split_comma.mjs";
 export function text_split_comma_only(t) {
   let split = text_split_comma(t);
+  list_size_1_assert(list);
   return split;
 }
