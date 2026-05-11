1ba4118b53ca60527505d2f67c7de78e88a25813
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:39:11 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- public/src/date_time_zone_now_year.mjs
+++ public/src/date_time_zone_now_year.mjs
@@ -1,6 +1,6 @@
 import { date_time_zone_now } from "../../../love/public/src/date_time_zone_now.mjs";
 export function date_time_zone_now_year() {
   let r = date_time_zone_now();
-  let r2 = r.year;
-  return r2;
+  let y = r.year;
+  return y;
 }
