0da3bdaa24ad451ef640650d6542571504fdb36a
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:55:10 -0400

    function_auto_path ../love/public/src/date_time_zone_format_to_standard.mjs

--- public/src/date_time_zone_format_to_standard.mjs
+++ public/src/date_time_zone_format_to_standard.mjs
@@ -1,7 +1,6 @@
 import { date_time_zone_format } from "../../../love/public/src/date_time_zone_format.mjs";
-export function date_time_zone_format_to_standard(pakistan) {
+export function date_time_zone_format_to_standard(dt) {
   let format = date_time_zone_format();
-  let formatted = pakistdt;
-  an.toFormat(format);
+  let formatted = dt.toFormat(format);
   return formatted;
 }
