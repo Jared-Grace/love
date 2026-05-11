52250f2a514e1c37cd1bad074c18252812bd1e6c
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:37:14 -0400

    function_auto_path ../love/public/src/function_import_invoke.mjs

--- web/public/src/function_import_invoke.mjs
+++ web/public/src/function_import_invoke.mjs
@@ -1,6 +1,6 @@
 import { function_import } from "../../../love/public/src/function_import.mjs";
 export async function function_import_invoke(f_name) {
   let imported_fn = await function_import(f_name);
-  let file_path = imported_fn();
-  return file_path;
+  let result = imported_fn();
+  return result;
 }
