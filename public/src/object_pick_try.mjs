37f4b7dd70e44e5992160f7aa7e5b470b9a3ce60
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 21:27:30 -0400

    function_auto_path ../love/public/src/object_pick_try.mjs

--- public/src/object_pick_try.mjs
+++ public/src/object_pick_try.mjs
@@ -3,7 +3,7 @@ import { list_intersect } from "../../../love/public/src/list_intersect.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 export function object_pick_try(object, property_names) {
   let properties = properties_get(object);
-  let intersection = list_intersect(properties, property_names);
+  let intersection = list_intersect(property_names, properties);
   let picked = object_pick(object, intersection);
   return picked;
 }
