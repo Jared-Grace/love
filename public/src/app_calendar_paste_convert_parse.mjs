d63cc197c35a0d489b38c0763f52567d2c65803c
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:43:50 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert_parse.mjs

--- public/src/app_calendar_paste_convert_parse.mjs
+++ public/src/app_calendar_paste_convert_parse.mjs
@@ -1,3 +1,4 @@
+import { assert } from "../../../love/public/src/assert.mjs";
 import { date_time_zone_parse } from "../../../love/public/src/date_time_zone_parse.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { date_time_zone_now_year } from "../../../love/public/src/date_time_zone_now_year.mjs";
@@ -5,5 +6,6 @@ export function app_calendar_paste_convert_parse(date, hour, zone) {
   let y = date_time_zone_now_year();
   let input_luxon = text_combine_multiple([date, " ", y, " ", hour]);
   const dt = date_time_zone_parse(input_luxon, "cccc, LLL dd yyyy h:mma", zone);
+  assert(b);
   return dt;
 }
