69bc3a0c2f231bd3fff52574ca1ac00aac43635b
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:33:38 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- web/public/src/date_time_zone_parse.mjs
+++ web/public/src/date_time_zone_parse.mjs
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
-export function date_time_zone_parse(input_luxon, format, zone) {
-  let r4 = DateTime.fromFormat(input_luxon, format, {
+export function date_time_zone_parse(input, format, zone) {
+  let r4 = DateTime.fromFormat(input, format, {
     zone,
   });
   return r4;
