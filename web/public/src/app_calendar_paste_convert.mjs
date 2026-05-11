99ac24e63763bf311edbe12ef6aca884db8ad74d
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:02:47 -0400

    function_auto_path ../love/public/src/app_calendar_paste_convert.mjs

--- web/public/src/app_calendar_paste_convert.mjs
+++ web/public/src/app_calendar_paste_convert.mjs
@@ -1,9 +1,9 @@
-import { data_texts_search } from "../../../love/public/src/data_texts_search.mjs";
+import { text_split } from "../../../love/public/src/text_split.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { import_install } from "../../../love/public/src/import_install.mjs";
 export async function app_calendar_paste_convert(input) {
   input = "Monday, May 11⋅11:00am – 12:00pm";
-  let result = await data_texts_search(s);
+  let split = text_split(s, separator);
   let r = await import_install("luxon");
   let DateTime = property_get(r, "DateTime");
   const dt = DateTime.fromFormat(
