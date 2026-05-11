34aa72d6948a9943b9e8d5cbf7161d099ee24969
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:35:15 -0400

    function_auto_path ../love/public/src/date_time_zone_past_is.mjs

--- web/public/src/date_time_zone_past_is.mjs
+++ web/public/src/date_time_zone_past_is.mjs
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
 export function date_time_zone_past_is(dt, zone) {
-  let p = dt <= DateTime.now().setZone(zone);
+  let p = dt < DateTime.now().setZone(zone);
   return p;
 }
