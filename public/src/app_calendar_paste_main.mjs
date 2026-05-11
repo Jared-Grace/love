246035d4d3bdd210cd894af77950c7da8296fc2a
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 19:10:08 -0400

    s paste

--- public/src/app_calendar_paste_main.mjs
+++ public/src/app_calendar_paste_main.mjs
@@ -1,9 +1,8 @@
 import { app_calendar_paste_convert } from "../../../love/public/src/app_calendar_paste_convert.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-export async function app_calendar_paste_main() {
+export  function app_calendar_paste_main() {
   let input = "Monday, May 11⋅11:00am – 12:00pm";
-  await app_calendar_paste_convert(input);
-  function lambda2() {}
+  function lambda2() {app_calendar_paste_convert}
   let component = html_button(
     parent,
     "Click to paste the date/time from Google Calendar to copy for Pakistan",
