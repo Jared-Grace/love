09b5ddeedb43c89dce4c5aa08fcbc18cd5c864a2
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:26:53 -0400

    function_auto_path ../love/public/src/list_first_second_only.mjs

--- public/src/list_first_second_only.mjs
+++ public/src/list_first_second_only.mjs
@@ -2,6 +2,7 @@ import { list_size_1_assert_message } from "../../../love/public/src/list_size_1
 import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
 export function list_first_second_only(list) {
   let result = list_first_second(list);
-  list_size_1_assert_message(result, {});
+  const message = {};
+  list_size_1_assert_message(result, message);
   return result;
 }
