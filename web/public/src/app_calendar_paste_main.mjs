067aaa54b993d75c394e9d2877b4b8742c009a7b
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 18:00:38 -0400

    function_auto_path ../love/public/src/app_calendar_paste_main.mjs

--- web/public/src/app_calendar_paste_main.mjs
+++ web/public/src/app_calendar_paste_main.mjs
@@ -1,7 +1,9 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { import_install } from "../../../love/public/src/import_install.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export async function app_calendar_paste_main() {
-  let DateTime = await import_install("luxon");
+  let r = await import_install("luxon");
+  let DateTime = property_get(r, "DateTime");
   let example = "Monday, May 11⋅11:00am – 12:00pm";
   function lambda2() {}
   let component = html_button(
