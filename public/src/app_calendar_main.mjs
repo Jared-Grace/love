bbc497d6a3404cb789e1e43badd40074465083bd
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:42:25 -0400

    function_auto_path ../love/public/src/app_calendar_main.mjs

--- public/src/app_calendar_main.mjs
+++ public/src/app_calendar_main.mjs
@@ -1,7 +1,6 @@
+import { object_pick_try_json } from "../../../love/public/src/object_pick_try_json.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_filter_property_exclude_if_exists } from "../../../love/public/src/list_filter_property_exclude_if_exists.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
-import { object_pick_try } from "../../../love/public/src/object_pick_try.mjs";
 import { list_to_dictionary_key } from "../../../love/public/src/list_to_dictionary_key.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
@@ -51,8 +50,7 @@ export async function app_calendar_main(context) {
   }
   const properties_name = ["whatsapp_name", "facebook_name"];
   function lambda6(o) {
-    let picked = object_pick_try(o, properties_name);
-    let json2 = json_to(picked);
+    let json2 = object_pick_try_json(o, properties_name);
     return json2;
   }
   let dictionary = list_to_dictionary_key(filtered, lambda6);
