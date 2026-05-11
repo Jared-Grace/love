14e14495157bc542474f68ed37c5c2bbec5f3b4f
JESUS rose to life <username@users.noreply.github.com>
2026-05-10 20:34:45 -0400

    pf list_filter_includes_not list

--- public/src/list_filter_includes_not.mjs
+++ public/src/list_filter_includes_not.mjs
@@ -2,7 +2,7 @@ import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { list_includes_not } from "../../../love/public/src/list_includes_not.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 import { property_get_or_null } from "../../../love/public/src/property_get_or_null.mjs";
-export function list_filter_includes_not(property, list_other, list) {
+export function list_filter_includes_not(list, property, list_other) {
   function lambda(item) {
     let value = property_get_or_null(item, property);
     if (null_is(value)) {
