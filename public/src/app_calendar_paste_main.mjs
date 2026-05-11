f8ed121cac7547c44c9fd5f25ebf92c9ef77d96f
JESUS rose to life <username@users.noreply.github.com>
2026-05-11 17:57:13 -0400

    function_auto_path ../love/public/src/app_calendar_paste_main.mjs

--- public/src/app_calendar_paste_main.mjs
+++ public/src/app_calendar_paste_main.mjs
@@ -1,5 +1,9 @@
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_calendar_paste_main() {
   function lambda2() {}
-  let component = html_button(parent, text, lambda2);
+  let component = html_button(
+    parent,
+    "Click to paste the date/time from Google Calendar to copy for Pakistan",
+    lambda2,
+  );
 }
