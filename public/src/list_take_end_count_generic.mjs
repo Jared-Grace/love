58e66632187a2543d9368b861db8fc0ebd2e7fc0
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 12:07:52 -0400

    function_auto_path ../love/public/src/list_take_end_count_generic.mjs

--- public/src/list_take_end_count_generic.mjs
+++ public/src/list_take_end_count_generic.mjs
@@ -2,9 +2,9 @@ import { integer_is_assert } from "../../../love/public/src/integer_is_assert.mj
 import { negative_not_is_assert } from "../../../love/public/src/negative_not_is_assert.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_take_end_count_generic(list, count) {
-  let fn = list_size;
+  let size_get = list_size;
   integer_is_assert(count);
-  let end = list_size(list);
+  let end = size_get(list);
   let c = end - count;
   negative_not_is_assert(c);
   return c;
