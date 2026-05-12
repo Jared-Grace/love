613f1c9cc758c294835ec97d40ab9603d9bf1fd1
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 12:08:19 -0400

    pf take_end_count_generic list

--- public/src/list_take_end_count_generic.mjs
+++ public/src/list_take_end_count_generic.mjs
@@ -2,6 +2,6 @@ import { take_end_count_generic } from "../../../love/public/src/take_end_count_
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_take_end_count_generic(list, count) {
   let size_get = list_size;
-  let c = take_end_count_generic(count, size_get, list);
+  let c = take_end_count_generic(list, count, size_get);
   return c;
 }
