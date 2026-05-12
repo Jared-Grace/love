5ecdff4e558ce711ab79f2467396db26164cb174
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 12:36:11 -0400

    function_auto_path ../love/public/src/date_time_zone_format_google_calendar.mjs

--- web/public/src/app_calendar_paste_convert_parse.mjs
+++ web/public/src/app_calendar_paste_convert_parse.mjs
@@ -1,4 +1,4 @@
-import { date_time_zone_format } from "../../../love/public/src/date_time_zone_format.mjs";
+import { date_time_zone_format_google_calendar } from "../../../love/public/src/date_time_zone_format_google_calendar.mjs";
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { date_time_zone_parse } from "../../../love/public/src/date_time_zone_parse.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
@@ -6,7 +6,7 @@ import { date_time_zone_now_year } from "../../../love/public/src/date_time_zone
 export function app_calendar_paste_convert_parse(date, hour, zone) {
   let y = date_time_zone_now_year();
   let input_luxon = text_combine_multiple([date, " ", y, " ", hour]);
-  let format = date_time_zone_format();
+  let format = date_time_zone_format_google_calendar();
   const dt = date_time_zone_parse(input_luxon, format, zone);
   function lambda2() {
     let r = [dt.invalidReason, dt.invalidExplanation];
