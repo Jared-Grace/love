1d624505e27910fda8139825665e7e7fd581bddf
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 12:43:48 -0400

    function_auto_path ../love/public/src/app_calendar_paste_main.mjs

--- public/src/app_calendar_paste_main.mjs
+++ public/src/app_calendar_paste_main.mjs
@@ -8,7 +8,7 @@ import { clipboard_transform } from "../../../love/public/src/clipboard_transfor
 import { html_button } from "../../../love/public/src/html_button.mjs";
 export function app_calendar_paste_main(context) {
   let root = property_get(context, "root");
-  let p2 = null;
+  let output = null;
   async function lambda2() {
     function lambda(input) {
       let date_time_zones = app_calendar_paste_convert(input);
@@ -16,7 +16,7 @@ export function app_calendar_paste_main(context) {
         date_time_zones,
         "Do you want a meeting on this day and time?",
       ]);
-      html_text_set(component2, text);
+      html_text_set(output, joined);
       return joined;
     }
     await clipboard_transform(lambda);
@@ -26,5 +26,5 @@ export function app_calendar_paste_main(context) {
     "Click to paste the date/time from Google Calendar to copy for Pakistan",
   );
   let component = html_button(root, "Request date/time", lambda2);
-  p2 = html_p(root);
+  output = html_p(root);
 }
