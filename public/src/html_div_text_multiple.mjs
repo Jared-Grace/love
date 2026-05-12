b739691c791a5025b92b815b67f5997fe298f3a7
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 13:59:31 -0400

    function_auto_path ../love/public/src/html_div_text_multiple.mjs

--- public/src/html_div_text_multiple.mjs
+++ public/src/html_div_text_multiple.mjs
@@ -2,7 +2,8 @@ import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function html_div_text_multiple(parent, list) {
   function lambda(item) {
-    html_div_text(parent, item);
+    let div = html_div_text(parent, item);
+    return div;
   }
   let mapped = list_map(list, lambda);
   return mapped;
