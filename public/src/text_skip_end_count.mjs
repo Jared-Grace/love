1387d9ce5caa1bd4fd70b69a3f1e170f4917c25f
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 12:11:54 -0400

    function_auto_path ../love/public/src/text_skip_end_count.mjs

--- public/src/text_skip_end_count.mjs
+++ public/src/text_skip_end_count.mjs
@@ -1,9 +1,7 @@
+import { text_take_end_count_generic } from "../../../love/public/src/text_take_end_count_generic.mjs";
 import { text_skip } from "../../../love/public/src/text_skip.mjs";
-import { text_size } from "../../../love/public/src/text_size.mjs";
-import { take_end_count_generic } from "../../../love/public/src/take_end_count_generic.mjs";
 export function text_skip_end_count(t, count) {
-  let size_get = text_size;
-  let c = take_end_count_generic(t, count, size_get);
+  let c = text_take_end_count_generic(t, count);
   let result = text_skip(t, c);
   return result;
 }
