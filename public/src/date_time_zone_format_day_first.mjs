f58c2b5c03c1abc8f7ed69080cbb6b42463c4048
JESUS rose to life <username@users.noreply.github.com>
2026-05-12 12:29:23 -0400

    function_auto_path ../love/public/src/date_time_zone_format_day_first.mjs

--- public/src/date_time_zone_format_day_first.mjs
+++ public/src/date_time_zone_format_day_first.mjs
@@ -1,5 +1,7 @@
+import { date_time_zone_format_date_day_first } from "../../../love/public/src/date_time_zone_format_date_day_first.mjs";
 import { date_time_zone_format_time } from "../../../love/public/src/date_time_zone_format_time.mjs";
 export function date_time_zone_format_day_first() {
-  let f = "cccc, dd LLL yyyy" + " " + date_time_zone_format_time();
+  let f =
+    date_time_zone_format_date_day_first() + " " + date_time_zone_format_time();
   return f;
 }
