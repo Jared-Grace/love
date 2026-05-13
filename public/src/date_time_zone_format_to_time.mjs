f25efbf6b58fd7cef98c79738a6eae3969d4044c
JESUS rose to life <username@users.noreply.github.com>
2026-05-13 19:43:34 -0400

    function_auto_path ../love/public/src/date_time_zone_format_to_time.mjs

--- public/src/date_time_zone_format_to_time.mjs
+++ public/src/date_time_zone_format_to_time.mjs
@@ -1,6 +1,6 @@
 import { date_time_zone_format_to } from "../../../love/public/src/date_time_zone_format_to.mjs";
 export function date_time_zone_format_to_time(dt) {
-  let format = "h:mma";
+  let format = "h:mm a";
   let formatted = date_time_zone_format_to(dt, format);
   return formatted;
 }
