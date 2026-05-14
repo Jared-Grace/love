74674ba56cc7fa7dd709ccb951d3b24cd202b932
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 18:41:53 -0400

    function_auto_path ../love/public/src/app_calendar_download_browser_contacts.mjs

--- public/src/app_calendar_download_browser_contacts.mjs
+++ public/src/app_calendar_download_browser_contacts.mjs
@@ -6,10 +6,11 @@ import { list_to_dictionary_key } from "../../../love/public/src/list_to_diction
 import { list_filter_property_exclude_if_exists } from "../../../love/public/src/list_filter_property_exclude_if_exists.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
-import { app_calendar_download_browser } from "../../../love/public/src/app_calendar_download_browser.mjs";
 import { global_function_self_async } from "../../../love/public/src/global_function_self_async.mjs";
 export async function app_calendar_download_browser_contacts() {
-  let data = await global_function_self_async(app_calendar_download_browser);
+  let data = await global_function_self_async(
+    app_calendar_download_browser_contacts,
+  );
   let contacts = app_calendar_contacts_initialize(data);
   log(app_calendar_download_browser_contacts.name, {
     contacts,
