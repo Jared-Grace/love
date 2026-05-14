85102b1591d1df14d70c7a6858431409e22bf1ca
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:52:32 -0400

    function_auto_path ../love/public/src/app_shared_screens_generate.mjs

--- web/public/src/app_shared_screens_generate.mjs
+++ web/public/src/app_shared_screens_generate.mjs
@@ -1,1 +1,4 @@
-export function app_shared_screens_generate() {}
+import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
+export async function app_shared_screens_generate() {
+  let info = await app_shared_name_search_info(search);
+}
