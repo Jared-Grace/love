c4f112877e60699851b356ff1cad452b836f257a
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:20:53 -0400

    function_auto_path ../love/public/src/app_replace_main.mjs

--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -7,9 +7,9 @@ import { app_replace_screens } from "../../../love/public/src/app_replace_screen
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 export async function app_replace_main(context) {
   let app_fn = app_replace;
-  let screens = app_replace_screens();
   html_meta_viewport();
   html_font_sans_serif_set_html();
+  let screens = app_replace_screens();
   object_merge_set(context, {
     app_fn,
     screens,
