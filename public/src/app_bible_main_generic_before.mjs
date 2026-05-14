9396ab0dc668fa1cee27428d3fa64357989492fb
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:45:27 -0400

    function_auto_path ../love/public/src/app_bible_main_generic_before.mjs

--- public/src/app_bible_main_generic_before.mjs
+++ public/src/app_bible_main_generic_before.mjs
@@ -1,4 +1,5 @@
-import { app_shared_initialize_refresh } from "../../../love/public/src/app_shared_initialize_refresh.mjs";
+import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
+import { app_shared_initialize } from "../../../love/public/src/app_shared_initialize.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 export async function app_bible_main_generic_before(
@@ -11,5 +12,6 @@ export async function app_bible_main_generic_before(
     screen_home,
   });
   html_margin_0(root);
-  await app_shared_initialize_refresh(context, app_fn, screens);
+  app_shared_initialize(context, app_fn, screens);
+  await app_shared_refresh(context);
 }
