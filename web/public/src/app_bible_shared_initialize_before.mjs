b7ea46143672a6bb11a08f40ebcd7894ce8525bf
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:48:33 -0400

    pl app_bible_shared_initialize_before screen_home

--- web/public/src/app_bible_shared_initialize_before.mjs
+++ web/public/src/app_bible_shared_initialize_before.mjs
@@ -3,9 +3,9 @@ import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export function app_bible_shared_initialize_before(
   context,
-  screen_home,
   app_fn,
   screens,
+  screen_home,
 ) {
   object_merge(context, {
     screen_home,
