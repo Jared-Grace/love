4ff3e65681183110071196edccdf6372b6293029
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 18:43:36 -0400

    function_auto_path ../love/public/src/app_calendar_download_browser.mjs

--- web/public/src/app_calendar_download_browser.mjs
+++ web/public/src/app_calendar_download_browser.mjs
@@ -1,8 +1,8 @@
-import { app_calendar_download } from "../../../love/public/src/app_calendar_download.mjs";
-import { app_api_fn } from "../../../love/public/src/app_api_fn.mjs";
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
+import { app_api } from "../../../love/public/src/app_api.mjs";
 export async function app_calendar_download_browser() {
-  let r5 = await app_api_fn({
-    fn: app_calendar_download,
+  let r5 = await app_api({
+    f_name: fn_name("app_calendar_download"),
   });
   return r5;
 }
