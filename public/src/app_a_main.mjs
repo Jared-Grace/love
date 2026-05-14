394221bc72cbc679312246c40aaa7a642c8b800e
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:27:46 -0400

    function_auto_path ../love/public/src/app_a_main.mjs

--- public/src/app_a_main.mjs
+++ public/src/app_a_main.mjs
@@ -22,10 +22,10 @@ export async function app_a_main(context) {
   let root = property_get(context, "root");
   html_margin_0(root);
   html_on_keydown(root, lambda);
+  html_font_sans_serif_set_html();
   object_merge(context, {
     app_fn,
     screens,
   });
-  html_font_sans_serif_set_html();
   await app_shared_refresh(context);
 }
