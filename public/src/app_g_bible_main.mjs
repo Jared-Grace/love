189aa8dae74818dbdeacc4ca75079a40e2719054
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:45:55 -0400

    r r app_bible_main_generic_before,app_bible_shared_initialize

--- public/src/app_g_bible_main.mjs
+++ public/src/app_g_bible_main.mjs
@@ -1,13 +1,13 @@
 import { app_g_bible_home } from "../../../love/public/src/app_g_bible_home.mjs";
 import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
-import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
+import { app_bible_shared_initialize } from "../../../love/public/src/app_bible_shared_initialize.mjs";
 import { firebase_login } from "../../../love/public/src/firebase_login.mjs";
 import { app_g_bible_screens } from "../../../love/public/src/app_g_bible_screens.mjs";
 import { app_g_bible } from "../../../love/public/src/app_g_bible.mjs";
 export async function app_g_bible_main(context) {
   let app_fn = app_g_bible;
   let screens = app_g_bible_screens();
-  app_bible_main_generic_before(context, app_fn, screens, app_g_bible_home);
+  app_bible_shared_initialize(context, app_fn, screens, app_g_bible_home);
   async function lambda() {
     await app_shared_refresh(context);
   }
