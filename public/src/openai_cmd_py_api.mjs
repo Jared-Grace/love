87945a6926954c6db63a62d0007d4ad4ce66fd91
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 19:06:24 -0400

    function_auto_path ../love/public/src/openai_cmd_py_api.mjs

--- public/src/openai_cmd_py_api.mjs
+++ public/src/openai_cmd_py_api.mjs
@@ -1,10 +1,6 @@
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
-import { log } from "../../../love/public/src/log.mjs";
+import { py_script_run } from "../../../love/public/src/py_script_run.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
-import { command_line } from "../../../love/public/src/command_line.mjs";
-import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 import { openai_key_folder } from "../../../love/public/src/openai_key_folder.mjs";
-import { py_script_run_cmd } from "../../../love/public/src/py_script_run_cmd.mjs";
 export async function openai_cmd_py_api(
   openai_script_name,
   input_file_path,
@@ -13,12 +9,6 @@ export async function openai_cmd_py_api(
   let k = openai_key_folder();
   let result = path_join([k, "openai.txt"]);
   const script_name = "openai_" + openai_script_name;
-  let v = py_script_run_cmd(script_name);
   let concated = [result, input_file_path, output_file_path];
-  let concated2 = list_concat([v], concated);
-  let joined = list_join_space(concated2);
-  log(openai_cmd_py_api.name, {
-    joined,
-  });
-  let stdout = await command_line(joined);
+  await py_script_run(script_name, concated);
 }
