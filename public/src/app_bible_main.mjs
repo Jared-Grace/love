487e96e32404b6c40dd915777a2878d3f8bc1b86
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:48:04 -0400

    psw app_bible_shared_initialize screen_home app_fn

--- public/src/app_bible_main.mjs
+++ public/src/app_bible_main.mjs
@@ -5,5 +5,5 @@ import { app_bible_screens } from "../../../love/public/src/app_bible_screens.mj
 export async function app_bible_main(context) {
   let app_fn = app_bible;
   let screens = app_bible_screens();
-  await app_bible_shared_initialize(context, app_fn, screens, app_bible_home);
+  await app_bible_shared_initialize(context, app_bible_home, screens, app_fn);
 }
