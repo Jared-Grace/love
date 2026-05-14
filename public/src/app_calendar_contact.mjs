f2ebf46f462d500e9c7d396e7b8670c3c841414f
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 17:21:26 -0400

    asa calendar contact

--- public/src/app_calendar_contact.mjs
+++ public/src/app_calendar_contact.mjs
@@ -1,1 +1,3 @@
-export function app_calendar_contact() {}
+export function app_calendar_contact(context) {
+  let root = html_clear_context(context);
+}
--- public/src/app_calendar_screens.mjs
+++ public/src/app_calendar_screens.mjs
@@ -1,5 +1,5 @@
 import { app_calendar_home } from "../../../love/public/src/app_calendar_home.mjs";
 export function app_calendar_screens() {
-  let r = [app_calendar_home];
+  let r = [app_calendar_home, app_calendar_contact];
   return r;
 }
