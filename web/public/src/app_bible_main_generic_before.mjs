4e0d1032e28b19d62052a5901657c0dc1df0faf3
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:44:24 -0400

    function_auto_path ../love/public/src/app_shared_initialize_refresh.mjs

--- web/public/src/app_bible_main_generic_before.mjs
+++ web/public/src/app_bible_main_generic_before.mjs
@@ -1,4 +1,4 @@
-import { app_shared_initialize } from "../../../love/public/src/app_shared_initialize.mjs";
+import { app_shared_initialize_refresh } from "../../../love/public/src/app_shared_initialize_refresh.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 export async function app_bible_main_generic_before(
@@ -11,5 +11,5 @@ export async function app_bible_main_generic_before(
     screen_home,
   });
   html_margin_0(root);
-  await app_shared_initialize(context, app_fn, screens);
+  await app_shared_initialize_refresh(context, app_fn, screens);
 }
--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -1,8 +1,8 @@
-import { app_shared_initialize } from "../../../love/public/src/app_shared_initialize.mjs";
+import { app_shared_initialize_refresh } from "../../../love/public/src/app_shared_initialize_refresh.mjs";
 import { app_replace_screens } from "../../../love/public/src/app_replace_screens.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 export async function app_replace_main(context) {
   let app_fn = app_replace;
   let screens = app_replace_screens();
-  await app_shared_initialize(context, app_fn, screens);
+  await app_shared_initialize_refresh(context, app_fn, screens);
 }
