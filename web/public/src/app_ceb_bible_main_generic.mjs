6d15f0f6465e2d62e4cdc9d76c31c2e67d20875c
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:50:05 -0400

    function_replace app_ceb_bible_main_generic app_bible_shared_initialize

--- web/public/src/app_ceb_bible_main_generic.mjs
+++
@@ -1,9 +0,0 @@
-import { app_bible_shared_initialize } from "../../../love/public/src/app_bible_shared_initialize.mjs";
-export async function app_ceb_bible_main_generic(
-  context,
-  app_fn,
-  screens,
-  screen_home,
-) {
-  await app_bible_shared_initialize(context, app_fn, screens, screen_home);
-}
