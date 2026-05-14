b18da68d89a88eee0284a7ad643e9e73dadbe2e2
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 18:42:43 -0400

    function_auto_path ../love/public/src/app_calendar_download_browser_contacts.mjs

--- public/src/app_calendar_download_browser_contacts.mjs
+++ public/src/app_calendar_download_browser_contacts.mjs
@@ -1,3 +1,4 @@
+import { app_calendar_download_browser } from "../../../love/public/src/app_calendar_download_browser.mjs";
 import { object_pick_try_values_join_space } from "../../../love/public/src/object_pick_try_values_join_space.mjs";
 import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calendar_whatsapp_phone.mjs";
 import { app_calendar_facebook_conversation_id } from "../../../love/public/src/app_calendar_facebook_conversation_id.mjs";
@@ -8,9 +9,7 @@ import { log } from "../../../love/public/src/log.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { global_function_self_async } from "../../../love/public/src/global_function_self_async.mjs";
 export async function app_calendar_download_browser_contacts() {
-  let data = await global_function_self_async(
-    app_calendar_download_browser_contacts,
-  );
+  let data = await global_function_self_async(app_calendar_download_browser);
   let contacts = app_calendar_contacts_initialize(data);
   log(app_calendar_download_browser_contacts.name, {
     contacts,
