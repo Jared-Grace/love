7ad5a2ee2ee5391dbb71e4813e20e446f775cfd4
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:48:11 -0400

    p

--- public/src/app_bible_shared_initialize.mjs
+++ public/src/app_bible_shared_initialize.mjs
@@ -2,9 +2,9 @@ import { app_bible_shared_initialize_before } from "../../../love/public/src/app
 import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
 export async function app_bible_shared_initialize(
   context,
-  screen_home,
-  screens,
   app_fn,
+  screens,
+  screen_home,
 ) {
   app_bible_shared_initialize_before(context, screen_home, app_fn, screens);
   await app_shared_refresh(context);
