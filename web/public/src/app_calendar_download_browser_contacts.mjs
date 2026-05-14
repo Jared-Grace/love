aa1f7e96ebd75647cab29da7848528e99574310d
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 17:56:49 -0400

    function_auto_path ../love/public/src/app_calendar_download_browser_contacts.mjs

--- web/public/src/app_calendar_download_browser_contacts.mjs
+++ web/public/src/app_calendar_download_browser_contacts.mjs
@@ -1,3 +1,7 @@
+import { object_pick_try_values_join_space } from "../../../love/public/src/object_pick_try_values_join_space.mjs";
+import { app_calendar_whatsapp_phone } from "../../../love/public/src/app_calendar_whatsapp_phone.mjs";
+import { app_calendar_facebook_conversation_id } from "../../../love/public/src/app_calendar_facebook_conversation_id.mjs";
+import { app_calendar_facebook_url_id } from "../../../love/public/src/app_calendar_facebook_url_id.mjs";
 import { list_to_dictionary_key } from "../../../love/public/src/list_to_dictionary_key.mjs";
 import { list_filter_property_exclude_if_exists } from "../../../love/public/src/list_filter_property_exclude_if_exists.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -15,14 +19,14 @@ export async function app_calendar_download_browser_contacts(lambda6) {
     "unavailable",
     true,
   );
-    let r2 = app_calendar_facebook_url_id();
-    let r3 = app_calendar_facebook_conversation_id();
-    let r4 = app_calendar_whatsapp_phone();
-    const properties = ["whatsapp_name", "facebook_name", r4, r2, r3];
-    function lambda6(o) {
-      let joined = object_pick_try_values_join_space(o, properties);
-      return joined;
-    }
+  let r2 = app_calendar_facebook_url_id();
+  let r3 = app_calendar_facebook_conversation_id();
+  let r4 = app_calendar_whatsapp_phone();
+  const properties = ["whatsapp_name", "facebook_name", r4, r2, r3];
+  function lambda6(o) {
+    let joined = object_pick_try_values_join_space(o, properties);
+    return joined;
+  }
   let dictionary = list_to_dictionary_key(filtered, lambda6);
   return dictionary;
 }
