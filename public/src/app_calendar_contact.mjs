316edf648fc798ebe030202643988e01863b24b9
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 18:09:13 -0400

    function_auto_path ../love/public/src/app_calendar_contact.mjs

--- public/src/app_calendar_contact.mjs
+++ public/src/app_calendar_contact.mjs
@@ -4,7 +4,6 @@ import { app_shared_screen_set } from "../../../love/public/src/app_shared_scree
 import { app_replace_button_home_text } from "../../../love/public/src/app_replace_button_home_text.mjs";
 import { app_a_button_wide } from "../../../love/public/src/app_a_button_wide.mjs";
 import { html_p_text } from "../../../love/public/src/html_p_text.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { app_calendar_download_browser_contacts } from "../../../love/public/src/app_calendar_download_browser_contacts.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 import { app_calendar_home } from "../../../love/public/src/app_calendar_home.mjs";
@@ -16,8 +15,7 @@ export async function app_calendar_contact(context) {
   let text = app_replace_button_home_text();
   let component = app_a_button_wide(root, text, lambda2);
   let dictionary = await app_calendar_download_browser_contacts();
-  let json = json_to(dictionary);
   let contact_selected = storage_local_get_context(context, "contact_selected");
-  let value = property_get(object, contact_selected);
+  let value = property_get(dictionary, contact_selected);
   let p = html_p_text(root, value);
 }
