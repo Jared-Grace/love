4fa2e6641c8803ca0c16fc3a307fd252bc4af754
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 18:25:05 -0400

    function_auto_path ../love/public/src/object_property_single_value.mjs

--- web/public/src/object_property_single_value.mjs
+++ web/public/src/object_property_single_value.mjs
@@ -1,9 +1,9 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
-export function object_property_single_value(picked) {
-  let properties = properties_get(picked);
+export function object_property_single_value(object) {
+  let properties = properties_get(object);
   let only = list_single(properties);
-  let value3 = property_get(picked, only);
+  let value3 = property_get(object, only);
   return value3;
 }
