048c17e10863636f70c13b7ffdfdc6d6eb310791
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 17:48:01 -0400

    function_auto_path ../love/public/src/global_function_async_self.mjs

--- web/public/src/global_function_async_self.mjs
+++ web/public/src/global_function_async_self.mjs
@@ -1,4 +1,5 @@
 import { global_function_async } from "../../../love/public/src/global_function_async.mjs";
 export async function global_function_async_self(fn, lambda) {
-  return await global_function_async(fn, lambda);
+  let r = await global_function_async(fn, lambda);
+  return r;
 }
