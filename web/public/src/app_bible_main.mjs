274ad2eb0fab8a6c7edf6c38d214381239e01be8
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:29:24 -0400

    function_auto_path ../love/public/src/app_bible_main.mjs

--- web/public/src/app_bible_main.mjs
+++ web/public/src/app_bible_main.mjs
@@ -4,5 +4,5 @@ import { app_bible_screens } from "../../../love/public/src/app_bible_screens.mj
 export async function app_bible_main(context) {
   let app_fn = app_bible;
   let screens = app_bible_screens();
-  app_bible_main_generic(context, app_fn, screens);
+  await app_bible_main_generic(context, app_fn, screens);
 }
