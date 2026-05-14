75d3454160635574af6e5874a46e4ade1ea9f6ee
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:44:56 -0400

    function_auto_path ../love/public/src/app_shared_initialize_refresh.mjs

--- public/src/app_shared_initialize_refresh.mjs
+++ public/src/app_shared_initialize_refresh.mjs
@@ -1,13 +1,5 @@
 import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
-import { app_replace_font_size_refresh } from "../../../love/public/src/app_replace_font_size_refresh.mjs";
-import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
 export async function app_shared_initialize_refresh(context, app_fn, screens) {
-  object_merge(context, {
-    app_fn,
-    screens,
-  });
-  html_mobile_default(context);
-  app_replace_font_size_refresh(context);
+  app_shared_initialize;
   await app_shared_refresh(context);
 }
