99638f48f5bc714b083c6e2ec1c7bdd4b133d08a
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:28:18 -0400

    function_auto_path ../love/public/src/app_a_main.mjs

--- web/public/src/app_a_main.mjs
+++ web/public/src/app_a_main.mjs
@@ -1,7 +1,6 @@
+import { html_margin_0_context_root } from "../../../love/public/src/html_margin_0_context_root.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 import { app_a_file_system_initialize } from "../../../love/public/src/app_a_file_system_initialize.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_on_keydown } from "../../../love/public/src/html_on_keydown.mjs";
 import { html_font_sans_serif_set_html } from "../../../love/public/src/html_font_sans_serif_set_html.mjs";
 import { invoke_multiple_arg } from "../../../love/public/src/invoke_multiple_arg.mjs";
@@ -16,8 +15,7 @@ export async function app_a_main(context) {
   object_merge(context, {
     on_keydowns,
   });
-  let root = property_get(context, "root");
-  html_margin_0(root);
+  let root = html_margin_0_context_root(context);
   function lambda(e) {
     invoke_multiple_arg(on_keydowns, e);
   }
