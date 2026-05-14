bbd2397fa596dc515af136bd65f6ea27da06b544
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:15:34 -0400

    function_auto_path ../love/public/src/app_a_list_chooser.mjs

--- public/src/app_a_list_chooser.mjs
+++ public/src/app_a_list_chooser.mjs
@@ -1,3 +1,4 @@
+import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_a_list_chooser_generic } from "../../../love/public/src/app_a_list_chooser_generic.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -6,7 +7,6 @@ import { html_on_enter_lambda } from "../../../love/public/src/html_on_enter_lam
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function app_a_list_chooser(context, noun, texts, lambda$text) {
   let root = property_get(context, "root");
-  function lambda() {}
   let r = app_a_list_chooser_generic(root, noun, texts, lambda, noop);
   let filtered_get = property_get(r, "filtered_get");
   async function on_enter() {
@@ -16,5 +16,9 @@ export function app_a_list_chooser(context, noun, texts, lambda$text) {
   }
   let on_keydown = html_on_enter_lambda(on_enter);
   let on_keydowns = app_a_on_keydown_add(context, on_keydown);
+  function lambda(text) {
+    list_remove(on_keydowns, on_keydown);
+    lambda$text(text);
+  }
   return r;
 }
