f6c2bf77b14a381bdc930bba9d2738444b73aff1
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 18:03:42 -0400

    function_auto_path ../love/public/src/app_calendar_contact.mjs

--- public/src/app_calendar_contact.mjs
+++ public/src/app_calendar_contact.mjs
@@ -8,7 +8,7 @@ export async function app_calendar_contact(context) {
   let root = html_clear_context(context);
   function lambda2() {}
   let text = app_replace_button_home_text();
-  let component = app_a_button_wide(parent, text, lambda2);
+  let component = app_a_button_wide(root, text, lambda2);
   let dictionary = await app_calendar_download_browser_contacts();
   let json = json_to(dictionary);
   let p = html_p_text(root, json);
