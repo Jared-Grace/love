cbe0586ad7dcf9e539505b46568240b298f4e238
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:40:27 -0400

    function_auto_path ../love/public/src/app_bible_main_generic_before.mjs

--- public/src/app_bible_main_generic_before.mjs
+++ public/src/app_bible_main_generic_before.mjs
@@ -1,7 +1,8 @@
+import { app_shared_initialize } from "../../../love/public/src/app_shared_initialize.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
 import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
-export function app_bible_main_generic_before(
+export async function app_bible_main_generic_before(
   context,
   app_fn,
   screens,
@@ -16,4 +17,5 @@ export function app_bible_main_generic_before(
   });
   let root = html_mobile_default(context);
   html_margin_0(root);
+  await app_shared_initialize(context, app_fn, screens);
 }
