0300a13a35797d31352cbc898f6de9f24b82ce53
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 17:21:29 -0400

    function_auto_path ../love/public/src/app_calendar_screens.mjs

--- web/public/src/app_calendar_screens.mjs
+++ web/public/src/app_calendar_screens.mjs
@@ -1,3 +1,4 @@
+import { app_calendar_contact } from "../../../love/public/src/app_calendar_contact.mjs";
 import { app_calendar_home } from "../../../love/public/src/app_calendar_home.mjs";
 export function app_calendar_screens() {
   let r = [app_calendar_home, app_calendar_contact];
