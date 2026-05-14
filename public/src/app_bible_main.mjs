7c62b7a7df817c3790ebefa374fbd33749e569cc
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:30:14 -0400

    function_auto_path ../love/public/src/app_bible_main.mjs

--- public/src/app_bible_main.mjs
+++ public/src/app_bible_main.mjs
@@ -1,8 +1,11 @@
-import { app_bible_main_generic } from "../../../love/public/src/app_bible_main_generic.mjs";
+import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
+import { app_bible_home } from "../../../love/public/src/app_bible_home.mjs";
+import { app_bible_main_generic_before } from "../../../love/public/src/app_bible_main_generic_before.mjs";
 import { app_bible } from "../../../love/public/src/app_bible.mjs";
 import { app_bible_screens } from "../../../love/public/src/app_bible_screens.mjs";
 export async function app_bible_main(context) {
   let app_fn = app_bible;
   let screens = app_bible_screens();
-  await app_bible_main_generic(context, app_fn, screens);
+  app_bible_main_generic_before(context, app_fn, screens, app_bible_home);
+  await app_shared_refresh(context);
 }
