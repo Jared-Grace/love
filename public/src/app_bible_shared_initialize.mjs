bdbc1e9788b3b120424d2e6bb4631908e2360c31
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:45:55 -0400

    r r app_bible_main_generic_before,app_bible_shared_initialize

--- public/src/app_bible_shared_initialize.mjs
+++ public/src/app_bible_shared_initialize.mjs
@@ -2,7 +2,7 @@ import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.
 import { app_shared_initialize } from "../../../love/public/src/app_shared_initialize.mjs";
 import { object_merge } from "../../../love/public/src/object_merge.mjs";
 import { html_margin_0 } from "../../../love/public/src/html_margin_0.mjs";
-export async function app_bible_main_generic_before(
+export async function app_bible_shared_initialize(
   context,
   app_fn,
   screens,
