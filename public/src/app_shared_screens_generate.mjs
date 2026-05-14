c82745d63574ac44cef4a0d23fe95cf2a16535ca
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:58:58 -0400

    function_auto_path ../love/public/src/app_shared_screens_generate.mjs

--- public/src/app_shared_screens_generate.mjs
+++ public/src/app_shared_screens_generate.mjs
@@ -1,6 +1,8 @@
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_shared_name_search_info } from "../../../love/public/src/app_shared_name_search_info.mjs";
 export async function app_shared_screens_generate(search) {
   let r = await app_shared_name_search_info(search);
   let app_name = property_get(r, "app_name");
+  let combined = function_name_combine(left, right);
 }
