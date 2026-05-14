3d0c7fc857d9e8f852f98012ec19fe29931bb451
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 14:38:53 -0400

    function_auto_path ../love/public/src/app_replace_main.mjs

--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -1,17 +1,8 @@
-import { html_mobile_default } from "../../../love/public/src/html_mobile_default.mjs";
-import { object_merge } from "../../../love/public/src/object_merge.mjs";
-import { app_replace_font_size_refresh } from "../../../love/public/src/app_replace_font_size_refresh.mjs";
-import { app_shared_refresh } from "../../../love/public/src/app_shared_refresh.mjs";
+import { app_shared_initialize } from "../../../love/public/src/app_shared_initialize.mjs";
 import { app_replace_screens } from "../../../love/public/src/app_replace_screens.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 export async function app_replace_main(context) {
   let app_fn = app_replace;
   let screens = app_replace_screens();
-  object_merge(context, {
-    app_fn,
-    screens,
-  });
-  let root = html_mobile_default(context);
-  app_replace_font_size_refresh(context);
-  await app_shared_refresh(context);
+  await app_shared_initialize(context, app_fn, screens);
 }
