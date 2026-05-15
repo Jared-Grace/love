82a89d36f3e91bbd33b2859b9844993097d67f97
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 14:21:30 -0400

    function_auto_path ../love/public/src/file_name_json_folder_gitignore.mjs

--- public/src/file_name_json_folder_gitignore.mjs
+++ public/src/file_name_json_folder_gitignore.mjs
@@ -1,9 +1,7 @@
+import { folder_gitignore_join } from "../../../love/public/src/folder_gitignore_join.mjs";
 import { file_name_json } from "../../../love/public/src/file_name_json.mjs";
-import { path_join } from "../../../love/public/src/path_join.mjs";
-import { folder_gitignore_name } from "../../../love/public/src/folder_gitignore_name.mjs";
 export function file_name_json_folder_gitignore(name) {
-  let folder = folder_gitignore_name();
-  let joined = path_join([folder, name]);
+  let joined = folder_gitignore_join(name);
   let f_path = file_name_json(joined);
   return f_path;
 }
