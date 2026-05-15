33a9ee7759be2325f4043329516284d144fd055b
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:18:08 -0400

    function_auto_path ../love/public/src/text_includes_not_multiple.mjs

--- web/public/src/text_includes_not_multiple.mjs
+++ web/public/src/text_includes_not_multiple.mjs
@@ -1,7 +1,7 @@
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { text_includes_not } from "../../../love/public/src/text_includes_not.mjs";
-export function text_includes_not_multiple(item, parts) {
-  let n = text_includes_not(item, part);
+export function text_includes_not_multiple(t, parts) {
+  let n = text_includes_not(t, part);
   function lambda(item2) {}
   let a = list_all(list, lambda);
 }
