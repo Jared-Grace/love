5503461971cf13c2adadf1a96b24f6fbee9d0d12
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 22:52:18 -0400

    function_auto_path ../love/public/src/sermon_translate_urdu.mjs

--- public/src/sermon_translate_urdu.mjs
+++ public/src/sermon_translate_urdu.mjs
@@ -1,3 +1,4 @@
+import { list_translate_openai } from "../../../love/public/src/list_translate_openai.mjs";
 import { integer_odd_is_right } from "../../../love/public/src/integer_odd_is_right.mjs";
 import { pair_to_list } from "../../../love/public/src/pair_to_list.mjs";
 import { list_map_pairs } from "../../../love/public/src/list_map_pairs.mjs";
@@ -7,9 +8,6 @@ import { list_squash } from "../../../love/public/src/list_squash.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
-import { json_from } from "../../../love/public/src/json_from.mjs";
-import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
-import { json_to } from "../../../love/public/src/json_to.mjs";
 import { list_filter_index } from "../../../love/public/src/list_filter_index.mjs";
 import { list_split } from "../../../love/public/src/list_split.mjs";
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
@@ -26,17 +24,7 @@ export async function sermon_translate_urdu(file_name) {
   let groups = list_split(filtered, separator);
   let lambda = integer_odd_is_right();
   let value = list_filter_index(groups, lambda);
-  let json = json_to({
-    value,
-  });
-  let r3 = await openai_responses_cache(
-    "Translate the text inside the JSON object to " +
-      language +
-      ". Do not change any object keys.",
-    json,
-  );
-  let v = json_from(r3);
-  let value2 = property_get(v, "value");
+  let value2 = await list_translate_openai(value, language);
   function lambda2(item2, index2) {
     let change = lambda(item2, index2);
     if (change) {
