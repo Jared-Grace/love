026a0fd3a6cd58859dfa8991b5a96c156ea23333
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 22:59:14 -0400

    function_auto_path ../love/public/src/sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,6 +1,4 @@
-import { each } from "../../../love/public/src/each.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { each_pair } from "../../../love/public/src/each_pair.mjs";
+import { list_pair_weave } from "../../../love/public/src/list_pair_weave.mjs";
 import { file_read_folder_user_txt_split_normalize } from "../../../love/public/src/file_read_folder_user_txt_split_normalize.mjs";
 import { list_translate_openai } from "../../../love/public/src/list_translate_openai.mjs";
 export async function sandbox_5() {
@@ -8,12 +6,6 @@ export async function sandbox_5() {
   let language = "Urdu";
   let filtered = await file_read_folder_user_txt_split_normalize(file_name);
   let translated = await list_translate_openai(filtered, language);
-  function lambda2(la) {
-    function lambda(a, b) {
-      each([a, b], la);
-    }
-    each_pair(filtered, translated, lambda);
-  }
-  let list = list_adder(lambda2);
+  let list = list_pair_weave(filtered, translated);
   return list;
 }
