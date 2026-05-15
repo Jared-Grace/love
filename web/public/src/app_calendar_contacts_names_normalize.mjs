3d0434b829bdc562dd326281c3251274d929a508
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:11:47 -0400

    function_auto_path ../love/public/src/app_calendar_contacts_names_normalize.mjs

--- web/public/src/app_calendar_contacts_names_normalize.mjs
+++ web/public/src/app_calendar_contacts_names_normalize.mjs
@@ -1,5 +1,6 @@
 import { app_calendar_secret_transform } from "../../../love/public/src/app_calendar_secret_transform.mjs";
-export async function app_calendar_contacts_names_normalize(lambda$data) {
-  let r = await app_calendar_secret_transform(lambda$data);
+export async function app_calendar_contacts_names_normalize() {
+  function lambda() {}
+  let r = await app_calendar_secret_transform(lambda);
   return r;
 }
