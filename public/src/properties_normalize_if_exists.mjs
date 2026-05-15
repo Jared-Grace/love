0e91177e3d6c4bbeee3a00b32386e1a9af23049d
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:29:30 -0400

    function_auto_path ../love/public/src/properties_normalize_if_exists.mjs

--- public/src/properties_normalize_if_exists.mjs
+++ public/src/properties_normalize_if_exists.mjs
@@ -1,5 +1,4 @@
 import { property_normalize_if_exists_curried } from "../../../love/public/src/property_normalize_if_exists_curried.mjs";
-import { property_normalize_if_exists } from "../../../love/public/src/property_normalize_if_exists.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function properties_normalize_if_exists(object, properties) {
   let lambda = property_normalize_if_exists_curried(object);
