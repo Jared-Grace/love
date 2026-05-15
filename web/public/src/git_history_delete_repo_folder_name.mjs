381e89dc8b4cafe61de315bc0581b018f65a26b8
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:54:57 -0400

    function_auto_path ../love/public/src/git_history_delete_repo_folder_name.mjs

--- web/public/src/git_history_delete_repo_folder_name.mjs
+++ web/public/src/git_history_delete_repo_folder_name.mjs
@@ -1,5 +1,5 @@
-import { uuid } from "../../../love/public/src/uuid.mjs";
+import { date_now_file } from "../../../love/public/src/date_now_file.mjs";
 export async function git_history_delete_repo_folder_name(repo) {
-  let r = repo + "-clean-" + (await uuid());
+  let r = repo + "-clean-" + date_now_file();
   return r;
 }
