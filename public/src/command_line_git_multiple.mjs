608c27eb5c133e808b0f92979c412be21a0ffd22
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 14:24:23 -0400

    function_auto_path ../love/public/src/command_line_git_love.mjs

--- public/src/command_line_git_multiple.mjs
+++ public/src/command_line_git_multiple.mjs
@@ -1,6 +1,6 @@
-import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
+import { command_line_git_love } from "../../../love/public/src/command_line_git_love.mjs";
 import { list_map_async } from "../../../love/public/src/list_map_async.mjs";
 export async function command_line_git_multiple(commands) {
-  let results = await list_map_async(commands, command_line_git);
+  let results = await list_map_async(commands, command_line_git_love);
   return results;
 }
--- public/src/git_push_upstream_set.mjs
+++ public/src/git_push_upstream_set.mjs
@@ -1,7 +1,7 @@
-import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
+import { command_line_git_love } from "../../../love/public/src/command_line_git_love.mjs";
 import { git_push_upstream_set_text } from "../../../love/public/src/git_push_upstream_set_text.mjs";
 export async function git_push_upstream_set() {
   let c = git_push_upstream_set_text();
-  let v = await command_line_git(c);
+  let v = await command_line_git_love(c);
   return v;
 }
