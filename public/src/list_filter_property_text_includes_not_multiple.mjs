133838338a6d09ffe8f9f429c995b45ae52d214c
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:21:49 -0400

    c i list_filter_text_includes_not_multiple,text,property

---
+++ public/src/list_filter_property_text_includes_not_multiple.mjs
@@ -0,0 +1,10 @@
+import { text_includes_not_multiple } from "../../../love/public/src/text_includes_not_multiple.mjs";
+import { list_filter } from "../../../love/public/src/list_filter.mjs";
+export function list_filter_property_text_includes_not_multiple(list, parts) {
+  function lambda(item) {
+    let n = text_includes_not_multiple(item, parts);
+    return n;
+  }
+  let filtered = list_filter(list, lambda);
+  return filtered;
+}
