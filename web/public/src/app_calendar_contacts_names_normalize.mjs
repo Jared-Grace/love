e90ce56f1c116113f41cf663118efcd2d797b50b
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:30:35 -0400

    function_auto_path ../love/public/src/app_calendar_contacts_names_normalize.mjs

--- web/public/src/app_calendar_contacts_names_normalize.mjs
+++ web/public/src/app_calendar_contacts_names_normalize.mjs
@@ -1,8 +1,6 @@
 import { properties_normalize_if_exists } from "../../../love/public/src/properties_normalize_if_exists.mjs";
 import { app_calendar_name_properties } from "../../../love/public/src/app_calendar_name_properties.mjs";
-import { object_pick_try_single_value } from "../../../love/public/src/object_pick_try_single_value.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
 import { app_calendar_secret_transform } from "../../../love/public/src/app_calendar_secret_transform.mjs";
 export async function app_calendar_contacts_names_normalize() {
@@ -11,10 +9,6 @@ export async function app_calendar_contacts_names_normalize() {
     let contacts = app_calendar_contacts_initialize(data);
     function lambda2(c) {
       properties_normalize_if_exists(c, name_properties);
-      let value = object_pick_try_single_value(c, name_properties);
-      log(app_calendar_contacts_names_normalize.name, {
-        value,
-      });
     }
     each(contacts, lambda2);
   }
