e960ecf7e045c6754557123253a36152b8d60f09
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:27:17 -0400

    function_auto_path ../love/public/src/app_calendar_contacts_names_normalize.mjs

--- web/public/src/app_calendar_contacts_names_normalize.mjs
+++ web/public/src/app_calendar_contacts_names_normalize.mjs
@@ -1,4 +1,4 @@
-import { property_normalize_if_exists } from "../../../love/public/src/property_normalize_if_exists.mjs";
+import { properties_normalize_if_exists } from "../../../love/public/src/properties_normalize_if_exists.mjs";
 import { app_calendar_name_properties } from "../../../love/public/src/app_calendar_name_properties.mjs";
 import { object_pick_try_single_value } from "../../../love/public/src/object_pick_try_single_value.mjs";
 import { each } from "../../../love/public/src/each.mjs";
@@ -10,7 +10,7 @@ export async function app_calendar_contacts_names_normalize() {
   function lambda(data) {
     let contacts = app_calendar_contacts_initialize(data);
     function lambda2(c) {
-      each(name_properties, property_normalize_if_exists);
+      properties_normalize_if_exists(name_properties);
       let value = object_pick_try_single_value(c, name_properties);
       log(app_calendar_contacts_names_normalize.name, {
         value,
