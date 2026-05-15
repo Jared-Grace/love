90d62fd697e577865b8a03452b7610b54740cb43
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 23:00:31 -0400

    function_auto_path ../love/public/src/sandbox_5.mjs

--- public/src/sandbox_5.mjs
+++ public/src/sandbox_5.mjs
@@ -1,15 +1,5 @@
-import { clipboard_copy } from "../../../love/public/src/clipboard_copy.mjs";
-import { list_join_newline_2 } from "../../../love/public/src/list_join_newline_2.mjs";
-import { list_pair_weave } from "../../../love/public/src/list_pair_weave.mjs";
-import { file_read_folder_user_txt_split_normalize } from "../../../love/public/src/file_read_folder_user_txt_split_normalize.mjs";
-import { list_translate_openai } from "../../../love/public/src/list_translate_openai.mjs";
+import { sermon_translate_1CO03_second_half } from "../../../love/public/src/sermon_translate_1CO03_second_half.mjs";
 export async function sandbox_5() {
-  let file_name = "1CO03_second_half";
-  let language = "Urdu";
-  let filtered = await file_read_folder_user_txt_split_normalize(file_name);
-  let translated = await list_translate_openai(filtered, language);
-  let list = list_pair_weave(filtered, translated);
-  let joined = list_join_newline_2(list);
-  await clipboard_copy(joined);
+  let joined = await sermon_translate_1CO03_second_half();
   return joined;
 }
