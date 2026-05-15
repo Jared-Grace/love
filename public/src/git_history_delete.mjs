cbf1bcb6cb7f4235a1b5a13294a70e53e1b32df2
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 14:21:42 -0400

    function_auto_path ../love/public/src/git_history_delete.mjs

--- public/src/git_history_delete.mjs
+++ public/src/git_history_delete.mjs
@@ -1,9 +1,9 @@
+import { folder_gitignore_join } from "../../../love/public/src/folder_gitignore_join.mjs";
 import { catch_ignore_async } from "../../../love/public/src/catch_ignore_async.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
 import { git_push_folder_now } from "../../../love/public/src/git_push_folder_now.mjs";
 import { git_history_delete_repo_folder_name } from "../../../love/public/src/git_history_delete_repo_folder_name.mjs";
-import { file_name_json_folder_gitignore } from "../../../love/public/src/file_name_json_folder_gitignore.mjs";
 import { repo_path } from "../../../love/public/src/repo_path.mjs";
 import { git_repo_url } from "../../../love/public/src/git_repo_url.mjs";
 import { folder_delete } from "../../../love/public/src/folder_delete.mjs";
@@ -14,7 +14,7 @@ export async function git_history_delete(user, repo, f_path, repo_path) {
   ("make sure this is ran from the correct directory");
   const url = git_repo_url(user, repo);
   const repo_folder_name = await git_history_delete_repo_folder_name(repo);
-  let repo_folder = file_name_json_folder_gitignore(repo_folder_name);
+  let repo_folder = folder_gitignore_join(repo_folder_name);
   let repo_folder_resolved = await path_resolve(repo_folder);
   let stdout = await command_line_git(
     "clone --mirror " + url + " " + repo_folder,
