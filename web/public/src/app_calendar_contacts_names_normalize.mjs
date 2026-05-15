7c388f2dbf9e51bbcc359b1ce64bca438b84d04d
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:25:39 -0400

    function_auto_path ../love/public/src/app_calendar_contacts_names_normalize.mjs

--- web/public/src/app_calendar_contacts_names_normalize.mjs
+++ web/public/src/app_calendar_contacts_names_normalize.mjs
@@ -1,4 +1,4 @@
-import { text_trim } from "../../../love/public/src/text_trim.mjs";
+import { whitespace_normalize } from "../../../love/public/src/whitespace_normalize.mjs";
 import { property_transform_if_exists } from "../../../love/public/src/property_transform_if_exists.mjs";
 import { app_calendar_name_properties } from "../../../love/public/src/app_calendar_name_properties.mjs";
 import { object_pick_try_single_value } from "../../../love/public/src/object_pick_try_single_value.mjs";
@@ -12,11 +12,7 @@ export async function app_calendar_contacts_names_normalize() {
     let contacts = app_calendar_contacts_initialize(data);
     function lambda2(c) {
       function lambda3(property_name) {
-        function lambda4(value) {
-          let trimmed = text_trim(value);
-          return value;
-        }
-        property_transform_if_exists(property_name, lambda4);
+        property_transform_if_exists(property_name, whitespace_normalize);
       }
       each(name_properties, lambda3);
       let value = object_pick_try_single_value(c, name_properties);
