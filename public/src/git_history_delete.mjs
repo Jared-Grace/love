075012f5e53dec529709e7114d31e599c31a6509
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:46:55 -0400

    function_auto_path ../love/public/src/git_history_delete.mjs

--- public/src/git_history_delete.mjs
+++ public/src/git_history_delete.mjs
@@ -1,3 +1,4 @@
+import { path_resolve } from "../../../love/public/src/path_resolve.mjs";
 import { git_push_folder_now } from "../../../love/public/src/git_push_folder_now.mjs";
 import { git_history_delete_repo_folder_name } from "../../../love/public/src/git_history_delete_repo_folder_name.mjs";
 import { file_name_json_folder_gitignore } from "../../../love/public/src/file_name_json_folder_gitignore.mjs";
@@ -13,6 +14,7 @@ export async function git_history_delete(user, repo, f_path, repo_path) {
   const url = git_repo_url(user, repo);
   const repo_folder_name = await git_history_delete_repo_folder_name(repo);
   let repo_folder = file_name_json_folder_gitignore(repo_folder_name);
+  let v = await path_resolve(paths);
   let stdout = await command_line_git(
     "clone --mirror " + url + " " + repo_folder,
   );
