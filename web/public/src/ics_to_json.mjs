af13d98b5d6588c1f2c003631bf16520b0151c63
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 12:32:00 -0400

    function_auto_path ../love/public/src/ics_to_json.mjs

--- web/public/src/ics_to_json.mjs
+++ web/public/src/ics_to_json.mjs
@@ -3,7 +3,7 @@ import { file_temp } from "../../../love/public/src/file_temp.mjs";
 import { py_script_run } from "../../../love/public/src/py_script_run.mjs";
 export async function ics_to_json(path_calendar) {
   async function lambda(temp_path) {
-    let r = await py_script_run("ics_to_json.py", [path_calendar, temp_path]);
+    let r = await py_script_run("ics_to_json", [path_calendar, temp_path]);
     let data = await file_read_json(file_path);
     return data;
   }
