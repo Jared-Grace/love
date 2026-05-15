da817372949d21d2df5547e1bd9ea9231f0912cf
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 22:53:04 -0400

    function_auto_path ../love/public/src/sandbox_5.mjs

--- web/public/src/sandbox_5.mjs
+++ web/public/src/sandbox_5.mjs
@@ -1,1 +1,4 @@
-export async function sandbox_5() {}
+import { list_translate_openai } from "../../../love/public/src/list_translate_openai.mjs";
+export async function sandbox_5() {
+  let translated = await list_translate_openai(list, language);
+}
