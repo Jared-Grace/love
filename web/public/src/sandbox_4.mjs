318644bfda6091f58755c65da91522449323f453
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 15:55:29 -0400

    function_auto_path ../love/public/src/sandbox_4.mjs

---
+++ web/public/src/sandbox_4.mjs
@@ -0,0 +1,21 @@
+import { property_transform_multiple_trim } from "../../../love/public/src/property_transform_multiple_trim.mjs";
+import { list_filter_property_text_includes_not_multiple } from "../../../love/public/src/list_filter_property_text_includes_not_multiple.mjs";
+import { properties_delete_multiple } from "../../../love/public/src/properties_delete_multiple.mjs";
+import { me_calendar } from "../../../love/public/src/me_calendar.mjs";
+import { app_calendar_paste_convert } from "../../../love/public/src/app_calendar_paste_convert.mjs";
+export async function sandbox_4() {
+  let list = await me_calendar();
+  property_transform_multiple_trim(list, "name");
+  let properties = ["description", "location"];
+  properties_delete_multiple(list, properties);
+  let remove_if_contains = ["Extended Stay America"];
+  let filtered = list_filter_property_text_includes_not_multiple(
+    list,
+    "name",
+    remove_if_contains,
+  );
+  return filtered;
+  let input = "Thursday, May 14⋅10:00 – 11:00am";
+  let date_time_zones = app_calendar_paste_convert(input);
+  return date_time_zones;
+}
