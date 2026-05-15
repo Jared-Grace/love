22aed2478571147b01d1613b12679f6968f7e930
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:09:36 -0400

    function_auto_path ../love/public/src/app_calendar_download_contacts.mjs

--- public/src/app_calendar_download_contacts.mjs
+++ public/src/app_calendar_download_contacts.mjs
@@ -2,6 +2,6 @@ import { app_calendar_contacts_initialize } from "../../../love/public/src/app_c
 import { app_calendar_download } from "../../../love/public/src/app_calendar_download.mjs";
 export async function app_calendar_download_contacts() {
   let data = await app_calendar_download();
-  let list = app_calendar_contacts_initialize(data);
-  return list;
+  let contacts = app_calendar_contacts_initialize(data);
+  return contacts;
 }
