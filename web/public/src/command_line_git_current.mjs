8cc25b41ef7a7e4ccbf9628a14d440a533c3f4cf
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 14:24:47 -0400

    function_auto_path ../love/public/src/command_line_git_current.mjs

--- web/public/src/command_line_git_current.mjs
+++ web/public/src/command_line_git_current.mjs
@@ -1,5 +1,5 @@
 import { command_line_git_folder } from "../../../love/public/src/command_line_git_folder.mjs";
-export async function command_line_git_love(command_git) {
+export async function command_line_git_current(command_git) {
   let folder = ".";
   let r = await command_line_git_folder(folder, command_git);
   return r;
--- web/public/src/git_repo_clone.mjs
+++ web/public/src/git_repo_clone.mjs
@@ -1,8 +1,8 @@
 import { repo_path } from "../../../love/public/src/repo_path.mjs";
-import { command_line_git_love } from "../../../love/public/src/command_line_git_love.mjs";
+import { command_line_git_current } from "../../../love/public/src/command_line_git_current.mjs";
 import { git_repo_url } from "../../../love/public/src/git_repo_url.mjs";
 export async function git_repo_clone(user, repo) {
   const url = git_repo_url(user, repo);
   let folder_name = repo_path(repo);
-  await command_line_git_love("clone " + url + " " + folder_name);
+  await command_line_git_current("clone " + url + " " + folder_name);
 }
