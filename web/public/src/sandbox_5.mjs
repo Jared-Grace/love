ef3420f3f5cebe9e31917db6864ff53bb1f79ebd
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 22:57:08 -0400

    function_auto_path ../love/public/src/sandbox_5.mjs

--- web/public/src/sandbox_5.mjs
+++ web/public/src/sandbox_5.mjs
@@ -1,6 +1,7 @@
 import { file_read_folder_user_txt_split_normalize } from "../../../love/public/src/file_read_folder_user_txt_split_normalize.mjs";
 import { list_translate_openai } from "../../../love/public/src/list_translate_openai.mjs";
 export async function sandbox_5() {
+  let file_name = "1CO03_second_half";
   let language = "Urdu";
   let filtered = await file_read_folder_user_txt_split_normalize(file_name);
   let translated = await list_translate_openai(filtered, language);
