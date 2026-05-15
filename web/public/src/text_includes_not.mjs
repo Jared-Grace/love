2e345ef54c12e54e2fafce489ce341a40b695d58
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:20:55 -0400

    function_auto_path ../love/public/src/text_includes_not.mjs

--- web/public/src/text_includes_not.mjs
+++ web/public/src/text_includes_not.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { text_includes } from "../../../love/public/src/text_includes.mjs";
 export function text_includes_not(item, part) {
+  log(text_includes_not.name, {
+    part,
+  });
   let v = text_includes(item, part);
   let n = not(v);
   return n;
