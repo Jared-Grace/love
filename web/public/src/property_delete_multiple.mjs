d864748bb2bb88677cc40fc263f72de7592630d1
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:09:37 -0400

    pp 3

--- web/public/src/property_delete_multiple.mjs
+++
@@ -1,6 +0,0 @@
-import { property_delete_curried } from "../../../love/public/src/property_delete_curried.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-export function property_delete_multiple(object, properties) {
-  let lambda = property_delete_curried(object);
-  each(properties, lambda);
-}
--- web/public/src/reply_dictionary.mjs
+++ web/public/src/reply_dictionary.mjs
@@ -1,17 +1,17 @@
 import { firebase_storage_url_project_jg } from "../../../love/public/src/firebase_storage_url_project_jg.mjs";
 import { http_local_json } from "../../../love/public/src/http_local_json.mjs";
-import { property_delete_multiple } from "../../../love/public/src/property_delete_multiple.mjs";
+import { properties_delete } from "../../../love/public/src/properties_delete.mjs";
 export async function reply_dictionary() {
   const url =
     "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json";
   let project_url = firebase_storage_url_project_jg();
   let dictionary = await http_local_json(url, project_url);
   let excludes_letters = ["h", "e", "d", "l", "t", "w"];
-  property_delete_multiple(dictionary, excludes_letters);
+  properties_delete(dictionary, excludes_letters);
   let excludes_pairs = ["ar", "ii", "ey", "ot", "pr", "re", "wa"];
-  property_delete_multiple(dictionary, excludes_pairs);
+  properties_delete(dictionary, excludes_pairs);
   let excludes = ["ware"];
-  property_delete_multiple(dictionary, excludes);
+  properties_delete(dictionary, excludes);
   let includes = [];
   return dictionary;
 }
