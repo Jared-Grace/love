d6ca70f75dff14b478f39bf0f90b4a553e6558ea
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 22:53:46 -0400

    function_auto_path ../love/public/src/sermon_translate_urdu.mjs

---
+++ web/public/src/file_read_folder_user_txt_split_normalize.mjs
@@ -0,0 +1,7 @@
+import { file_read_folder_user_split_normalize } from "../../../love/public/src/file_read_folder_user_split_normalize.mjs";
+import { file_name_txt } from "../../../love/public/src/file_name_txt.mjs";
+export async function file_read_folder_user_txt_split_normalize(file_name) {
+  const path = file_name_txt(file_name);
+  let r = await file_read_folder_user_split_normalize(path);
+  return r;
+}
