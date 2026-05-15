db9299b5e845fa78c9d8a3e3bef61b05721efc89
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:22:33 -0400

    function_auto_path ../love/public/src/list_filter_property_text_includes_not_multiple.mjs

--- web/public/src/list_filter_property_text_includes_not_multiple.mjs
+++ web/public/src/list_filter_property_text_includes_not_multiple.mjs
@@ -1,5 +1,4 @@
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { text_includes_not_multiple } from "../../../love/public/src/text_includes_not_multiple.mjs";
+import { property_text_includes_not } from "../../../love/public/src/property_text_includes_not.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_property_text_includes_not_multiple(
   list,
@@ -7,8 +6,7 @@ export function list_filter_property_text_includes_not_multiple(
   parts,
 ) {
   function lambda(item) {
-    let value = property_get(item, property_name);
-    let n = text_includes_not_multiple(value, parts);
+    let n = property_text_includes_not(item, property_name, parts);
     return n;
   }
   let filtered = list_filter(list, lambda);
