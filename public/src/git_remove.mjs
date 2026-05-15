375a9b4108bc3a6272dc37d301ba4504060dbcda
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 14:24:47 -0400

    function_auto_path ../love/public/src/command_line_git_current.mjs

--- public/src/git_remove.mjs
+++ public/src/git_remove.mjs
@@ -3,10 +3,10 @@ import { git_ignore_add } from "../../../love/public/src/git_ignore_add.mjs";
 import { repos_gitignore_overwrite_all } from "../../../love/public/src/repos_gitignore_overwrite_all.mjs";
 import { git_commit } from "../../../love/public/src/git_commit.mjs";
 import { git_add } from "../../../love/public/src/git_add.mjs";
-import { command_line_git_love } from "../../../love/public/src/command_line_git_love.mjs";
+import { command_line_git_current } from "../../../love/public/src/command_line_git_current.mjs";
 import { git_push } from "../../../love/public/src/git_push.mjs";
 export async function git_remove(f_path) {
-  await command_line_git_love("rm -r --cached " + f_path);
+  await command_line_git_current("rm -r --cached " + f_path);
   await git_ignore_add(f_path);
   let added = git_ignore_name();
   await git_add(added);
