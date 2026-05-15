572dc8c933439ce6e001f843715100b7bb18e74f
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:23:29 -0400

    function_auto_path ../love/public/src/property_transform_if_exists.mjs

--- web/public/src/property_transform_if_exists.mjs
+++ web/public/src/property_transform_if_exists.mjs
@@ -1,8 +1,8 @@
 import { property_transform } from "../../../love/public/src/property_transform.mjs";
 import { property_exists } from "../../../love/public/src/property_exists.mjs";
-export function property_transform_if_exists(property_name, lambda4) {
+export function property_transform_if_exists(property_name, lambda$value) {
   let exists = property_exists(object, property_name);
   if (exists) {
-    property_transform(o, property_name, lambda4);
+    property_transform(o, property_name, lambda$value);
   }
 }
