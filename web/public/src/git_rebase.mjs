b874c4c3eb73dd341c1e9510b3d2d4d1fdee2dcb
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 14:24:23 -0400

    function_auto_path ../love/public/src/command_line_git_love.mjs

--- web/public/src/git_rebase.mjs
+++ web/public/src/git_rebase.mjs
@@ -1,12 +1,12 @@
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-import { command_line_git } from "../../../love/public/src/command_line_git.mjs";
+import { command_line_git_love } from "../../../love/public/src/command_line_git_love.mjs";
 export async function git_rebase() {
   try {
-    await command_line_git("rebase --no-stat --no-verify origin/main");
+    await command_line_git_love("rebase --no-stat --no-verify origin/main");
   } catch (e) {
     log_keep(git_rebase.name, "Rebase failed, aborting rebase");
     try {
-      await command_line_git("rebase --abort");
+      await command_line_git_love("rebase --abort");
     } catch (abortErr) {
       log_keep(git_rebase.name, "No rebase in progress, nothing to abort.");
     }
