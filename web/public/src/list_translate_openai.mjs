9d09974e86e62e7c07be55010c37dee7d7b8c0dc
JESUS rose to life <username@users.noreply.github.com>
2026-05-14 22:52:18 -0400

    function_auto_path ../love/public/src/sermon_translate_urdu.mjs

---
+++ web/public/src/list_translate_openai.mjs
@@ -0,0 +1,18 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { json_from } from "../../../love/public/src/json_from.mjs";
+import { openai_responses_cache } from "../../../love/public/src/openai_responses_cache.mjs";
+import { json_to } from "../../../love/public/src/json_to.mjs";
+export async function list_translate_openai(value, language) {
+  let json = json_to({
+    value,
+  });
+  let r3 = await openai_responses_cache(
+    "Translate the text inside the JSON object to " +
+      language +
+      ". Do not change any object keys.",
+    json,
+  );
+  let v = json_from(r3);
+  let value2 = property_get(v, "value");
+  return value2;
+}
