9c359911d9a529a6abba552ffce19d7d2654f3bc
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 17:30:48 -0400

    function_auto_path ../love/public/src/app_calendar_contacts_names_normalize.mjs

--- web/public/src/app_calendar_contacts_names_normalize.mjs
+++ web/public/src/app_calendar_contacts_names_normalize.mjs
@@ -1,5 +1,4 @@
 import { properties_normalize_if_exists_curried_right } from "../../../love/public/src/properties_normalize_if_exists_curried_right.mjs";
-import { properties_normalize_if_exists } from "../../../love/public/src/properties_normalize_if_exists.mjs";
 import { app_calendar_name_properties } from "../../../love/public/src/app_calendar_name_properties.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_calendar_contacts_initialize } from "../../../love/public/src/app_calendar_contacts_initialize.mjs";
--- web/public/src/properties_normalize_if_exists_curried_right.mjs
+++ web/public/src/properties_normalize_if_exists_curried_right.mjs
@@ -1,7 +1,8 @@
 import { properties_normalize_if_exists } from "../../../love/public/src/properties_normalize_if_exists.mjs";
 export function properties_normalize_if_exists_curried_right(properties) {
   let c = function properties_normalize_if_exists_curried_right_result(object) {
-    return properties_normalize_if_exists(object, properties);
+    let r = properties_normalize_if_exists(object, properties);
+    return r;
   };
   return c;
 }
