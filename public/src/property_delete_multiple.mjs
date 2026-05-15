0dd7c06290a728183850470b87a70f782568bc95
JESUS rose to life <username@users.noreply.github.com>
2026-05-15 13:06:55 -0400

    function_auto_path ../love/public/src/property_delete_multiple.mjs

--- public/src/property_delete_multiple.mjs
+++ public/src/property_delete_multiple.mjs
@@ -1,8 +1,8 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { property_delete } from "../../../love/public/src/property_delete.mjs";
-export function property_delete_multiple(o, list) {
+export function property_delete_multiple(object, list) {
   function lambda(item) {
-    property_delete(o, item);
+    property_delete(object, item);
   }
   each(list, lambda);
 }
