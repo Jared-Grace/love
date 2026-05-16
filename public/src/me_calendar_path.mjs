4d49ce82b4b98fb906461ca487c1fc5b7e27f38c
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 15:52:57 -0400

    function_auto_path ../love/public/src/me_calendar_path.mjs

--- public/src/me_calendar_path.mjs
+++ public/src/me_calendar_path.mjs
@@ -1,9 +1,11 @@
+import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_user_downloads_path } from "../../../love/public/src/folder_user_downloads_path.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { me_email } from "../../../love/public/src/me_email.mjs";
 export function me_calendar_path() {
   const email_address = me_email();
-  let combined = text_combine(email_address, ".ics");pj
+  let combined = text_combine(email_address, ".ics");
+  let result = path_join(segments);
   let r = folder_user_downloads_path(combined);
   return r;
 }
