dda31c44c31d52469ffc3d683ee015d3aa32630d
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 15:46:44 -0400

    function_auto_path ../love/public/src/me_calendar_google_unzip.mjs

--- public/src/me_calendar_google_unzip.mjs
+++ public/src/me_calendar_google_unzip.mjs
@@ -1,4 +1,5 @@
 import { unzip } from "../../../love/public/src/unzip.mjs";
 export async function me_calendar_google_unzip(file_path, buffer) {
-  return await unzip(file_path, buffer);
+  let r = await unzip(file_path, buffer);
+  return r;
 }
