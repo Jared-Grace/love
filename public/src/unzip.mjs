3a5af578435dc0eb1d7bb3ed5517282acac9a64c
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 15:51:13 -0400

    function_auto_path ../love/public/src/unzip.mjs

--- public/src/unzip.mjs
+++ public/src/unzip.mjs
@@ -1,6 +1,6 @@
 import { import_install } from "../../../love/public/src/import_install.mjs";
-export async function unzip(file_path_to, buffer) {
+export async function unzip(file_path_to, file_path_from) {
   const AdmZip = (await import_install("adm-zip")).default;
-  const zip = new AdmZip(buffer);
+  const zip = new AdmZip(file_path_from);
   zip.extractAllTo(file_path_to, true);
 }
