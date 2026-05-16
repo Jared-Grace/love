b9cf151063e94b0a88abaf464ed0bdb797afb129
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 16:19:14 -0400

    w r file_backup_fn_delete,app_calendar_secret_calendar_backup

---
+++ public/src/app_calendar_secret_calendar_backup.mjs
@@ -0,0 +1,4 @@
+import { file_backup_fn_delete } from "../../../love/public/src/file_backup_fn_delete.mjs";
+export async function app_calendar_secret_calendar_backup(f_name) {
+  return await file_backup_fn_delete(f_name);
+}
