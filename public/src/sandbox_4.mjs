192e99629d7ca9d73f627c364f20fc26a06d66da
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 16:31:17 -0400

    function_auto_path ../love/public/src/sandbox_4.mjs

--- public/src/sandbox_4.mjs
+++ public/src/sandbox_4.mjs
@@ -23,8 +23,8 @@ export async function sandbox_4() {
   let contacts = await app_calendar_download_contacts();
   let name_properties = app_calendar_name_properties();
   let mapped = object_pick_try_single_value_multiple(contacts, name_properties);
-  let difference = list_difference(list2, list_other);
-  return mapped;
+  let difference = list_difference(calendar_names, mapped);
+  return difference;
   let input = "Thursday, May 14⋅10:00 – 11:00am";
   let date_time_zones = app_calendar_paste_convert(input);
   return date_time_zones;
