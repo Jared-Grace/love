f73f726b1721676dc4562ed85828c64356ccb325
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 16:30:34 -0400

    function_auto_path ../love/public/src/object_pick_try_single_value_multiple.mjs

--- web/public/src/object_pick_try_single_value_multiple.mjs
+++ web/public/src/object_pick_try_single_value_multiple.mjs
@@ -1,11 +1,8 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_pick_try_single_value } from "../../../love/public/src/object_pick_try_single_value.mjs";
-export function object_pick_try_single_value_multiple(
-  contacts,
-  name_properties,
-) {
+export function object_pick_try_single_value_multiple(contacts, properties) {
   function lambda(item) {
-    let value = object_pick_try_single_value(item, name_properties);
+    let value = object_pick_try_single_value(item, properties);
     return value;
   }
   let mapped = list_map(contacts, lambda);
