5928d264245c445760280fe9eaa1f9c24b8a0a20
JESUS rose to life <username@users.noreply.github.com>
2026-05-16 15:52:51 -0400

    function_auto_path ../love/public/src/me_calendar_zip_path.mjs

--- web/public/src/me_calendar_zip_path.mjs
+++ web/public/src/me_calendar_zip_path.mjs
@@ -1,9 +1,8 @@
+import { me_calendar_zip_folder_path } from "../../../love/public/src/me_calendar_zip_folder_path.mjs";
 import { folder_user_downloads_path } from "../../../love/public/src/folder_user_downloads_path.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
-import { me_email } from "../../../love/public/src/me_email.mjs";
 export function me_calendar_zip_path() {
-  const email_address = me_email();
-  let combined = text_combine(email_address, ".ical");
+  let combined = me_calendar_zip_folder_path();
   let combined2 = text_combine(combined, ".zip");
   let r = folder_user_downloads_path(combined2);
   return r;
