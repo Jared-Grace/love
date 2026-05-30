673101140b6bdf4201ed023fbe4dbe3bee92683a
JESUS rose to life <username@users.noreply.github.com>
2026-05-30 15:46:41 -0400

    function_auto_path ../love/public/src/date_time_zone_future_is_assert.mjs

--- public/src/date_time_zone_future_is_assert.mjs
+++ public/src/date_time_zone_future_is_assert.mjs
@@ -1,6 +1,6 @@
-import { assert } from "../../../love/public/src/assert.mjs";
 import { date_time_zone_future_is } from "../../../love/public/src/date_time_zone_future_is.mjs";
+import { assert_message } from "../../../love/public/src/assert_message.mjs";
 export function date_time_zone_future_is_assert(dt, zone) {
   let f = date_time_zone_future_is(dt, zone);
-  assert(f);
+  assert_message(f, "not future");
 }
