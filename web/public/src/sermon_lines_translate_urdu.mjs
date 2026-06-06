316a7a8bff311b391b1ce39550ac09c19f6d2537
JESUS rose to life <username@users.noreply.github.com>
2026-06-05 22:29:05 -0400

    ps

--- web/public/src/sermon_lines_translate_urdu.mjs
+++ web/public/src/sermon_lines_translate_urdu.mjs
@@ -8,7 +8,7 @@ export async function sermon_lines_translate_urdu(file_name) {
   let filtered = await file_read_folder_user_txt_split_normalize(file_name);
   let translated = await list_translate_openai(filtered, language);
   let list = list_pair_weave(filtered, translated);
-  if (0) {
+  if (10) {
     list = translated;
   }
   let joined = list_join_newline_2(list);
