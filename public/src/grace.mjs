a030647853b74c505c82f330b4d817a994cfb829
JESUS rose to life <username@users.noreply.github.com>
2026-06-22 22:57:30 -0400

    function_auto_path ../love/public/src/grace.mjs

--- public/src/grace.mjs
+++ public/src/grace.mjs
@@ -1,1 +1,4 @@
-export function grace() {}
+import { function_new_open } from "../../../love/public/src/function_new_open.mjs";
+export async function grace() {
+  await function_new_open(f_name);
+}
