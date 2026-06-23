fc30385e6b787798a6167bfdbec0662ca5c38fde
JESUS rose to life <username@users.noreply.github.com>
2026-06-22 23:05:28 -0400

    function_auto_path ../love/public/src/grace.mjs

--- web/public/src/grace.mjs
+++ web/public/src/grace.mjs
@@ -2,5 +2,5 @@ import { functions_search } from "../../../love/public/src/functions_search.mjs"
 import { function_new_open } from "../../../love/public/src/function_new_open.mjs";
 export async function grace() {
   await function_new_open(f_name);
-  let result = await functions_search(search);
+  let result = await functions_search("grace");
 }
