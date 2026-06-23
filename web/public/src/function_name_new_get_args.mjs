5a3735652b736dbac5a6506150a6d09a4055830f
JESUS rose to life <username@users.noreply.github.com>
2026-06-23 01:35:45 -0400

    ps

--- web/public/src/function_name_new_get_args.mjs
+++ web/public/src/function_name_new_get_args.mjs
@@ -7,7 +7,7 @@ export async function function_name_new_get_args(
   plugin_fn,
   f_name_old_args_comma,
 ) {
-  let fr = list_first_remaining_from_comma_dot(f_name_old_args_comma);
+  let fr = list_first_remaining_from_comma_dot(f_name_old_args_comma);$L$fr
   let split = property_get(fr, "remaining");
   let f_name_old = property_get(fr, "first");
   ({ f_name_old, plugin_fn } = await function_name_new_get_generic(
