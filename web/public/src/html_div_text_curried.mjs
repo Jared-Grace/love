f61abfc15fe9fb500a8d2e09ff8ab630c1070e31
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:34:50 -0400

    function_auto_path ../love/public/src/html_div_text_curried.mjs

--- web/public/src/html_div_text_curried.mjs
+++ web/public/src/html_div_text_curried.mjs
@@ -1,7 +1,8 @@
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 export function html_div_text_curried(root) {
   let c = function html_div_text_curried_result(text) {
-    return html_div_text(root, text);
+    let div = html_div_text(root, text);
+    return div;
   };
   return c;
 }
