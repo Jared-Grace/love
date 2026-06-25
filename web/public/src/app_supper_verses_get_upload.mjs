804c35b6a5fc0be7f0025d821a241659121f4b28
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:11:11 -0400

    function_auto_path ../love/public/src/app_supper_verses_get_upload.mjs

--- web/public/src/app_supper_verses_get_upload.mjs
+++ web/public/src/app_supper_verses_get_upload.mjs
@@ -1,10 +1,8 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { app_supper_verses_get_upload_destination } from "../../../love/public/src/app_supper_verses_get_upload_destination.mjs";
 import { firebase_upload_object_compressed } from "../../../love/public/src/firebase_upload_object_compressed.mjs";
 import { app_supper_verses_get } from "../../../love/public/src/app_supper_verses_get.mjs";
 export async function app_supper_verses_get_upload(ebible_folder) {
-  let ebible_folder2 = error();
-  let verses = await app_supper_verses_get(ebible_folder2);
+  let verses = await app_supper_verses_get(ebible_folder);
   let destination = app_supper_verses_get_upload_destination(ebible_folder);
   await firebase_upload_object_compressed(destination, {
     verses,
