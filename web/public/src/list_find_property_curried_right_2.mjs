12ebf062cc19f1482e48149e1015131633aae147
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:26:13 -0400

    function_auto_path ../love/public/src/list_find_property_curried_right_2.mjs

--- web/public/src/list_find_property_curried_right_2.mjs
+++ web/public/src/list_find_property_curried_right_2.mjs
@@ -4,7 +4,8 @@ export function list_find_property_curried_right_2(
   property_value,
 ) {
   let c = function list_find_property_curried_right_2_result(list) {
-    return list_find_property(list, property_name, property_value);
+    let item = list_find_property(list, property_name, property_value);
+    return item;
   };
   return c;
 }
