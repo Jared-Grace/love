74ade354c66c6ec0b68dd1a6a6fbc76d3dda0f94
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:27:45 -0400

    function_auto_path ../love/public/src/app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -47,8 +47,7 @@ export async function app_supper_main(context) {
     let text = property_get(v, "text");
     let reference = property_get(v, "reference");
     let c = list_find_property_curried_right_2("reference", reference);
-    function lambda4(item3) {}
-    let mapped = list_map(list2, lambda4);
+    let mapped = list_map(remaining, c);
     let item2 = list_find_property(list, "reference", reference);
     let p = html_p(root);
     let d = html_div_text_centered(p, reference);
