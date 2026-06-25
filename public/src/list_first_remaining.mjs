1825e4f28af1ccb2481bc75ec9c1b3006b0d208e
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:24:46 -0400

    function_auto_path ../love/public/src/list_first_remaining.mjs

--- public/src/list_first_remaining.mjs
+++ public/src/list_first_remaining.mjs
@@ -4,7 +4,11 @@ export function list_first_remaining(list) {
   const property_name = "remaining";
   let value_get = list_skip_1;
   let r = list_first_second_generic(list, value_get, property_name);
-  return r;
   if (false) {
+    ("this is for copy and pasting");
+    {
+      (first, remaining);
+    }
   }
+  return r;
 }
