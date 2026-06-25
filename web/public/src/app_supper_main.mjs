c19d094c628cb6af17c85e04d2919743ef1468a8
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:26:39 -0400

    function_auto_path ../love/public/src/app_supper_main.mjs

--- web/public/src/app_supper_main.mjs
+++ web/public/src/app_supper_main.mjs
@@ -45,7 +45,7 @@ export async function app_supper_main(context) {
     previous_chapter_code = chapter_code;
     let text = property_get(v, "text");
     let reference = property_get(v, "reference");
-    let c = list_find_property_curried_right_2(property_name, property_value);
+    let c = list_find_property_curried_right_2("reference", reference);
     let item2 = list_find_property(list, "reference", reference);
     let p = html_p(root);
     let d = html_div_text_centered(p, reference);
