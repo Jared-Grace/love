c60166e422a1a5ccdf80d65fdeb571228f2b5b7c
JESUS rose to life <username@users.noreply.github.com>
2026-06-24 22:29:54 -0400

    function_auto_path ../love/public/src/sat_contradiction_3.mjs

--- public/src/sat_contradiction_3.mjs
+++ public/src/sat_contradiction_3.mjs
@@ -1,11 +1,6 @@
-import { list_cartesian_functions } from "../../../love/public/src/list_cartesian_functions.mjs";
-import { range_1 } from "../../../love/public/src/range_1.mjs";
-import { negative } from "../../../love/public/src/negative.mjs";
-import { identity } from "../../../love/public/src/identity.mjs";
+import { sat_contradiction_n } from "../../../love/public/src/sat_contradiction_n.mjs";
 export function sat_contradiction_3() {
   let variables = 3;
-  let fns = [identity, negative];
-  let list = range_1(variables);
-  let clauses = list_cartesian_functions(list, fns);
+  let clauses = sat_contradiction_n(variables);
   return clauses;
 }
