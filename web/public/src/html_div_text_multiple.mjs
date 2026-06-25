3b40a0429b51e4efe12b15acec0b008cc40b4f7a
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:35:21 -0400

    t c html_div_text_multiple

--- web/public/src/html_div_text_multiple.mjs
+++ web/public/src/html_div_text_multiple.mjs
@@ -1,10 +1,8 @@
+import { html_div_text_curried } from "../../../love/public/src/html_div_text_curried.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function html_div_text_multiple(parent, list) {
-  function lambda(item) {
-    let div = html_div_text(parent, item);
-    return div;
-  }
+  let lambda = html_div_text_curried(parent);
   let mapped = list_map(list, lambda);
   return mapped;
 }
