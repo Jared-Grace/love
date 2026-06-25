89828ba793d5444368fd245d51609f724d1b84b4
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 02:44:34 -0400

    function_auto_path ../love/public/src/app_supper_main.mjs

--- public/src/app_supper_main.mjs
+++ public/src/app_supper_main.mjs
@@ -1,10 +1,7 @@
 import { app_supper_main_generic } from "../../../love/public/src/app_supper_main_generic.mjs";
-import { ebible_folder_tagalog } from "../../../love/public/src/ebible_folder_tagalog.mjs";
-import { ebible_folder_cebuano } from "../../../love/public/src/ebible_folder_cebuano.mjs";
 import { ebible_folder_english } from "../../../love/public/src/ebible_folder_english.mjs";
 export async function app_supper_main(context) {
-  let ebible_folder = ebible_folder_cebuano;
+  let ebible_folder = ebible_folder_english;
   let folder_gets = [ebible_folder];
-  folder_gets = [ebible_folder_tagalog, ebible_folder_english];
   await app_supper_main_generic(folder_gets, context);
 }
