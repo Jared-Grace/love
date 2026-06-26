15b1a40c94066238fd557dc68913078b21d7b8c6
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 22:39:17 -0400

    function_auto_path ../love/public/src/function_dependencies_externals_to_urls.mjs

--- public/src/function_dependencies_externals_to_urls.mjs
+++ public/src/function_dependencies_externals_to_urls.mjs
@@ -19,6 +19,7 @@ export function function_dependencies_externals_to_urls(externals) {
     acorn: "https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs",
     astring: "https://cdn.jsdelivr.net/npm/astring/dist/astring.mjs",
     "lz-string": "https://cdn.jsdelivr.net/npm/lz-string@1.5.0/+esm",
+    luxon: "https://cdn.jsdelivr.net/npm/luxon@3/build/global/luxon.min.js",
   };
   let r = properties_from(o, externals, lookup);
   return r;
