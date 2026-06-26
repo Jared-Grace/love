ca789162caecbbfc5ba01508721fc57da0bf952f
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 22:46:57 -0400

    function_auto_path ../love/public/src/list_unique_is.mjs

--- web/public/src/list_unique_is.mjs
+++ web/public/src/list_unique_is.mjs
@@ -1,7 +1,7 @@
 import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
-export function list_unique_is(mapped) {
-  let unique = list_unique(mapped);
-  let a = lists_sizes_equal([mapped, unique]);
+export function list_unique_is(list) {
+  let unique = list_unique(list);
+  let a = lists_sizes_equal([list, unique]);
   return a;
 }
