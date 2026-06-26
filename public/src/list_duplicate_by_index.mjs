a8dbcce78fbfd5576af45ddac47bb283a7ae820d
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 22:47:03 -0400

    function_auto_path ../love/public/src/list_duplicate_by_index.mjs

--- public/src/list_duplicate_by_index.mjs
+++ public/src/list_duplicate_by_index.mjs
@@ -4,8 +4,8 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_duplicate_by_index(list) {
   'for example: [a,b,c] becomes [a,b,b,c,c,c] because a is in position 1 so there\'s 1 a; and b is in position 2 so there are 2 b\'s, etc.';
   function lambda5(la) {
-    let r2 = invoke_count_arg_curried(la);
-    each_index_1(list, r2);
+    let c = invoke_count_arg_curried(la);
+    each_index_1(list, c);
   }
   let result = list_adder(lambda5);
   return result;
