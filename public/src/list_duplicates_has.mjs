4f7f8fcd72517f3457272de75cd38f30d4f7bc5f
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 22:46:52 -0400

    function_auto_path ../love/public/src/list_duplicates_has.mjs

--- public/src/list_duplicates_has.mjs
+++ public/src/list_duplicates_has.mjs
@@ -1,7 +1,7 @@
 import { not } from "../../../love/public/src/not.mjs";
 import { list_unique_is } from "../../../love/public/src/list_unique_is.mjs";
-export function list_duplicates_has(mapped) {
-  let b = list_unique_is(mapped);
+export function list_duplicates_has(list) {
+  let b = list_unique_is(list);
   let a = not(b);
   return a;
 }
