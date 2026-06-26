fa97f23932d1466e88060c322a67e80b99c52c33
JESUS rose to life <username@users.noreply.github.com>
2026-06-25 22:48:22 -0400

    function_auto_path ../love/public/src/list_unique_is_assert.mjs

--- web/public/src/list_unique_is_assert.mjs
+++ web/public/src/list_unique_is_assert.mjs
@@ -1,6 +1,6 @@
 import { assert } from "../../../love/public/src/assert.mjs";
 import { list_unique_is } from "../../../love/public/src/list_unique_is.mjs";
 export function list_unique_is_assert(list) {
-  let a = list_unique_is(list);
-  assert(a);
+  let u = list_unique_is(list);
+  assert(u);
 }
