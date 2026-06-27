9c9e8ef014770103ef3ebb09eb22c6e1d28f216b
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 06:46:54 -0400

    function_auto_path ../love/public/src/js_identifiers_to_statements.mjs

--- web/public/src/js_identifiers_to_statements.mjs
+++ web/public/src/js_identifiers_to_statements.mjs
@@ -2,8 +2,8 @@ import { list_map_property_unique } from "../../../love/public/src/list_map_prop
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { js_node_to_block_curried } from "../../../love/public/src/js_node_to_block_curried.mjs";
 export function js_identifiers_to_statements(ast, identifiers_named) {
-  let r = js_node_to_block_curried(ast);
-  let fs = list_map(identifiers_named, r);
+  let c = js_node_to_block_curried(ast);
+  let fs = list_map(identifiers_named, c);
   let statements = list_map_property_unique(fs, "item");
   return statements;
 }
