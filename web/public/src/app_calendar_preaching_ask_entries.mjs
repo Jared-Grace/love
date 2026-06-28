5451535544346b51683815ca4a0c4c7b0729b49e
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:24:31 -0400

    sb

--- web/public/src/app_calendar_preaching_ask_entries.mjs
+++ web/public/src/app_calendar_preaching_ask_entries.mjs
@@ -3,7 +3,9 @@ import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_e
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 export async function app_calendar_preaching_ask_entries() {
   let file_name = app_calendar_preaching_ask_file_name();
-  let lines = await folder_user_docs_read_lines(file_name);
+  let contents = await folder_user_docs_read(file_name);
+  let lines2 = text_split_newline(contents);
+  let lines = lines2;
   let filtered = list_filter_empty_not_is(lines);
   return filtered;
 }
