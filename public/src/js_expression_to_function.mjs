06d8a89580dd05f94a30910108d131b4c98f2d31
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:31:36 -0400

    function_auto_path ../love/public/src/js_expression_to_function.mjs

--- public/src/js_expression_to_function.mjs
+++ public/src/js_expression_to_function.mjs
@@ -1,7 +1,9 @@
+import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
 import { function_name_new_get_args_list } from "../../../love/public/src/function_name_new_get_args_list.mjs";
 import { function_new_expression } from "../../../love/public/src/function_new_expression.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export async function js_expression_to_function(ast, selects, args) {
+  let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
   let r2 = await function_name_new_get_args_list(plugin_fn, list);
   let node = list_single(selects);
   await function_new_expression(f_name, node);
