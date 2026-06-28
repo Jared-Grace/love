693ea6cf140df03a8608b5fc25df55daa98a7f00
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:34:39 -0400

    function_auto_path ../love/public/src/js_expression_to_function.mjs

--- public/src/js_expression_to_function.mjs
+++ public/src/js_expression_to_function.mjs
@@ -1,9 +1,11 @@
+import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
 import { function_name_new_get_args_list } from "../../../love/public/src/function_name_new_get_args_list.mjs";
 import { function_new_expression } from "../../../love/public/src/function_new_expression.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export async function js_expression_to_function(ast, selects, args) {
+  let name = js_flo_name(ast2);
   let fr = list_first_remaining_from_comma_dot(f_name_transformer_args_comma);
   let list = property_get(fr, "remaining");
   let plugin_fn = property_get(fr, "first");
