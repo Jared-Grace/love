e6e97d775e6ffa67b5605fed77dff3cf9692b917
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:49:36 -0400

    function_auto_path ../love/public/src/js_expression_to_function.mjs

--- web/public/src/js_expression_to_function.mjs
+++ web/public/src/js_expression_to_function.mjs
@@ -1,4 +1,6 @@
+import { function_new_expression } from "../../../love/public/src/function_new_expression.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
-export function js_expression_to_function(ast, selects) {
+export async function js_expression_to_function(ast, selects) {
   let node = list_single(selects);
+  await function_new_expression(f_name, expression);
 }
