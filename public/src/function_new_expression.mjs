b543283afbc0c290ffda40803343aa5b995a132d
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:43:18 -0400

    function_auto_path ../love/public/src/function_new_expression.mjs

--- public/src/function_new_expression.mjs
+++ public/src/function_new_expression.mjs
@@ -1,6 +1,6 @@
 import { js_flo_body_add_return_argument_curried_right } from "../../../love/public/src/js_flo_body_add_return_argument_curried_right.mjs";
 import { function_new_open_transform } from "../../../love/public/src/function_new_open_transform.mjs";
-export async function function_new_expression(e, f_name) {
-  let lambda = js_flo_body_add_return_argument_curried_right(e);
+export async function function_new_expression(expression, f_name) {
+  let lambda = js_flo_body_add_return_argument_curried_right(expression);
   let output = await function_new_open_transform(f_name, lambda);
 }
