706fc6d7fdddeb7fd2cbbc853979ce7289f565a0
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:23:41 -0400

    function_auto_path ../love/public/src/js_expand_selects.mjs

--- public/src/js_expand_selects.mjs
+++ public/src/js_expand_selects.mjs
@@ -1,11 +1,9 @@
-import { list_get_end_3 } from "../../../love/public/src/list_get_end_3.mjs";
+import { js_expand_generic } from "../../../love/public/src/js_expand_generic.mjs";
 import { js_node_to_block } from "../../../love/public/src/js_node_to_block.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { list_next_index } from "../../../love/public/src/list_next_index.mjs";
 import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
-import { list_get_end_2 } from "../../../love/public/src/list_get_end_2.mjs";
+import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { js_expand } from "../../../love/public/src/js_expand.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export async function js_expand_selects(ast, selects) {
   let first = list_single(selects);
@@ -13,12 +11,9 @@ export async function js_expand_selects(ast, selects) {
   let item = property_get(block, "item");
   let v = js_node_to_visitor(ast, item);
   let stack = property_get(v, "stack");
-  let stack1 = list_get_end_2(stack);
-  let stack2 = list_get_end_3(stack);
+  let stack2 = list_get_end_1(stack);
   log(js_expand_selects.name, {
-    stack1,
     stack2,
   });
-  let index = list_next_index(stack2, stack1);
-  await js_expand(stack2, stack1, item, ast, index);
+  let inserted = await js_expand_generic(item, stack2, index, ast);
 }
