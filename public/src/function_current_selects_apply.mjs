b098b51c41bee1de3f9466198d339eaf4dc641dd
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:01:20 -0400

    function_auto_path ../love/public/src/function_current_selects_apply.mjs

--- public/src/function_current_selects_apply.mjs
+++ public/src/function_current_selects_apply.mjs
@@ -1,18 +1,5 @@
-import { log } from "../../../love/public/src/log.mjs";
-import { function_current_selects_nodes } from "../../../love/public/src/function_current_selects_nodes.mjs";
-import { function_import } from "../../../love/public/src/function_import.mjs";
-import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
+import { function_current_selects_apply_generic } from "../../../love/public/src/function_current_selects_apply_generic.mjs";
 export async function function_current_selects_apply(apply_fn_name) {
   let args = [];
-  let f_name_current = await function_current_get();
-  let imported_fn = await function_import(apply_fn_name);
-  async function lambda(ast) {
-    let selects = await function_current_selects_nodes(ast);
-    log(function_current_selects_apply.name, {
-      selects,
-    });
-    imported_fn(ast, selects, args);
-  }
-  let output = await function_transform(f_name_current, lambda);
+  await function_current_selects_apply_generic(apply_fn_name, args);
 }
