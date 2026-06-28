da39b72c3624968743aff7465673599cb89fd539
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:41:48 -0400

    function_auto_path ../love/public/src/function_new.mjs

--- public/src/function_new.mjs
+++ public/src/function_new.mjs
@@ -3,4 +3,5 @@ import { function_new_declaration_to } from "../../../love/public/src/function_n
 export async function function_new(f_name) {
   let declaration = function_new_declaration_to(f_name);
   await function_new_declaration_from(declaration);
+  return declaration;
 }
