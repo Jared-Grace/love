063d57e35ed819688fbd909dbcb2ae4ce5c1004c
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:29:17 -0400

    function_auto_path ../love/public/src/list_filter_starts_with.mjs

--- public/src/list_filter_starts_with.mjs
+++ public/src/list_filter_starts_with.mjs
@@ -1,6 +1,5 @@
 import { text_starts_with_curried_right } from "../../../love/public/src/text_starts_with_curried_right.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 export function list_filter_starts_with(list, prefix) {
   let lambda = text_starts_with_curried_right(prefix);
   let filtered = list_filter(list, lambda);
