6ad80cb5b26a07359cc1ea8f6311ad8d0121c24b
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:00:12 -0400

    function_auto_path ../love/public/src/js_statement_find_call_named.mjs

--- web/public/src/js_statement_find_call_named.mjs
+++ web/public/src/js_statement_find_call_named.mjs
@@ -1,10 +1,8 @@
+import { js_call_named_find_alias } from "../../../love/public/src/js_call_named_find_alias.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_node_to_block_item } from "../../../love/public/src/js_node_to_block_item.mjs";
-import { js_call_named_find } from "../../../love/public/src/js_call_named_find.mjs";
-import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 export async function js_statement_find_call_named(ast, f_name) {
-  let unaliased = await function_name_unalias_only(f_name);
-  let only = js_call_named_find(ast, unaliased);
+  let only = await js_call_named_find_alias(f_name, ast);
   log(js_statement_find_call_named.name, {
     only,
   });
