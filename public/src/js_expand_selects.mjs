c742799689c46971c7b9289a82c33f358ae046c8
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 03:51:47 -0400

    function_auto_path ../love/public/src/js_expand_selects.mjs

--- public/src/js_expand_selects.mjs
+++ public/src/js_expand_selects.mjs
@@ -2,5 +2,5 @@ import { js_expand } from "../../../love/public/src/js_expand.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export async function js_expand_selects(ast, selects) {
   let first = list_single(selects);
-  let index = await js_expand(stack2, stack1, next, ast);
+  let index = await js_expand(stack2, stack1, first, ast);
 }
