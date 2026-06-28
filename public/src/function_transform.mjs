87614c0a42b0c29b2da75f2905191787715865b9
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:59:09 -0400

    function_auto_path ../love/public/src/function_transform.mjs

--- public/src/function_transform.mjs
+++ public/src/function_transform.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_transform_result } from "../../../love/public/src/function_transform_result.mjs";
 import { function_parse_declaration_js_unparse } from "../../../love/public/src/function_parse_declaration_js_unparse.mjs";
@@ -6,4 +7,5 @@ export async function function_transform(f_name, lambda$ast) {
   await function_transform_result(f_name, lambda$ast);
   let output = await function_parse_declaration_js_unparse(f_name);
   return output;
+  log(function_transform.name, {});
 }
