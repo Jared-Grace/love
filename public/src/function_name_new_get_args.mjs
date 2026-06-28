7c53abc25ea0ca42d4818c5c424fa829bd7fec19
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:23:39 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- public/src/function_name_new_get_args.mjs
+++ public/src/function_name_new_get_args.mjs
@@ -1,5 +1,6 @@
+import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
+import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_first_remaining_from_comma_dot } from "../../../love/public/src/list_first_remaining_from_comma_dot.mjs";
 import { function_name_new_get_generic } from "../../../love/public/src/function_name_new_get_generic.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
@@ -7,7 +8,8 @@ export async function function_name_new_get_args(
   plugin_fn,
   f_name_old_args_comma,
 ) {
-  let fr = list_first_remaining_from_comma_dot(f_name_old_args_comma);
+  let split2 = text_split_comma_dot(f_name_old_args_comma);
+  let fr = list_first_remaining(split2);
   let split = property_get(fr, "remaining");
   let f_name_old = property_get(fr, "first");
   ({ f_name_old, plugin_fn } = await function_name_new_get_generic(
