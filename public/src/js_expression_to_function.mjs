8368de8910225d81771d3d905a9d1257e4405dfe
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:22:10 -0400

    function_auto_path ../love/public/src/js_expression_to_function.mjs

--- public/src/js_expression_to_function.mjs
+++ public/src/js_expression_to_function.mjs
@@ -1,6 +1,8 @@
+import { function_name_new_get_args } from "../../../love/public/src/function_name_new_get_args.mjs";
 import { function_new_expression } from "../../../love/public/src/function_new_expression.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 export async function js_expression_to_function(ast, selects) {
+  let r2 = await function_name_new_get_args(plugin_fn, f_name_old_args_comma);
   let node = list_single(selects);
   await function_new_expression(f_name, node);
 }
