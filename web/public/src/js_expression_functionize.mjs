071eff50a40c4686286d8fef270637e11f0f0b3e
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:53:36 -0400

    function_auto_path ../love/public/src/js_expression_functionize.mjs

--- web/public/src/js_expression_functionize.mjs
+++ web/public/src/js_expression_functionize.mjs
@@ -25,8 +25,9 @@ export async function js_expression_functionize(ast, selects, args) {
   let r2 = await function_new_expression(f_name_new, node);
   let declaration = property_get(r2, "declaration");
   let parsed = js_code_call_args_await_maybe_parse(f_name_new, [], declaration);
+  object_replace(node, parsed);
   log(js_expression_functionize.name, {
     parsed,
+    node,
   });
-  object_replace(node, parsed);
 }
