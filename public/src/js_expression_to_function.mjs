e848be01befde1d43b65e3a0964b72a42bedbbc4
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:48:29 -0400

    function_auto_path ../love/public/src/js_expression_to_function.mjs

--- public/src/js_expression_to_function.mjs
+++ public/src/js_expression_to_function.mjs
@@ -1,2 +1,4 @@
+import { list_single } from "../../../love/public/src/list_single.mjs";
 export function js_expression_to_function(ast, selects) {
-  let node = list_single(selects);}
+  let node = list_single(selects);
+}
