cfe8c52868f4d0188a5b7d084d178a086a2ed1ba
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:25:21 -0400

    function_auto_path ../love/public/src/js_dollar_x.mjs

--- public/src/js_dollar_x.mjs
+++ public/src/js_dollar_x.mjs
@@ -1,8 +1,6 @@
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { js_expand_generic } from "../../../love/public/src/js_expand_generic.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { list_next_index } from "../../../love/public/src/list_next_index.mjs";
-import { js_expand } from "../../../love/public/src/js_expand.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_is } from "../../../love/public/src/list_is.mjs";
 export async function js_dollar_x({
@@ -18,14 +16,8 @@ export async function js_dollar_x({
   if (l) {
     let index = list_next_index(stack2, stack1);
     let next = list_get(stack2, index);
-    await js_expand(stack2, stack1, next, ast, index);
+    let inserted = await js_expand_generic(next, stack2, index, ast);
+    list_remove(stack2, stack1);
   }
   return;
-  log(js_dollar_x.name, {
-    stack2,
-    stack1,
-    next,
-  });
-  let inserted = await js_expand_generic(next, stack2, index, ast);
-  list_remove(stack2, stack1);
 }
