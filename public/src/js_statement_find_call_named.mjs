a27398af883a3bc18001a8e1119b2e72dd8afd3b
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:00:50 -0400

    pb js_call_named_find_alias

--- public/src/js_statement_find_call_named.mjs
+++ public/src/js_statement_find_call_named.mjs
@@ -2,7 +2,7 @@ import { js_call_named_find_alias } from "../../../love/public/src/js_call_named
 import { log } from "../../../love/public/src/log.mjs";
 import { js_node_to_block_item } from "../../../love/public/src/js_node_to_block_item.mjs";
 export async function js_statement_find_call_named(ast, f_name) {
-  let only = await js_call_named_find_alias(f_name, ast);
+  let only = await js_call_named_find_alias(ast, f_name);
   log(js_statement_find_call_named.name, {
     only,
   });
