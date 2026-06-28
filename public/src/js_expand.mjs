8635c213501ef0527a95ac0f49d7f97055ae17a5
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:04:16 -0400

    function_auto_path ../love/public/src/js_expand.mjs

--- public/src/js_expand.mjs
+++ public/src/js_expand.mjs
@@ -1,14 +1,12 @@
 import { log } from "../../../love/public/src/log.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { js_expand_generic } from "../../../love/public/src/js_expand_generic.mjs";
-import { list_next_index } from "../../../love/public/src/list_next_index.mjs";
 export async function js_expand(stack2, stack1, next, ast) {
   log(js_expand.name, {
     stack2,
     stack1,
     next,
   });
-  let index = list_next_index(stack2, stack1);
   let inserted = await js_expand_generic(next, stack2, index, ast);
   list_remove(stack2, stack1);
   return index;
--- public/src/sandbox_3_a.mjs
+++ public/src/sandbox_3_a.mjs
@@ -1,5 +1,5 @@
 import { sleep } from "../../../love/public/src/sleep.mjs";
 export async function sandbox_3_a() {
-  let ms = null;
+  let ms = null;$x
   await sleep(ms);
 }
