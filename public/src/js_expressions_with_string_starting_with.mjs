1a6898219e88ca423f03de3f2f8287b76ee9f0e5
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:37:13 -0400

    function_auto_path ../love/public/src/js_expressions_with_string_starting_with.mjs

--- public/src/js_expressions_with_string_starting_with.mjs
+++ public/src/js_expressions_with_string_starting_with.mjs
@@ -1,7 +1,7 @@
+import { js_strings_generic } from "../../../love/public/src/js_strings_generic.mjs";
 import { list_filter_property_starts_with } from "../../../love/public/src/list_filter_property_starts_with.mjs";
-import { js_strings_nodes } from "../../../love/public/src/js_strings_nodes.mjs";
 export function js_expressions_with_string_starting_with(ast, prefix) {
-  let strings = js_strings_nodes(ast);
-  let filtered = list_filter_property_starts_with(strings, prefix);
+  let strings = js_strings_generic(ast);
+  let filtered = list_filter_property_starts_with(strings, "value", prefix);
   return filtered;
 }
