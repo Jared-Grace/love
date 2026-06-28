e1773233f412f377642bdb23f8730bdb1f63b8f9
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:19:36 -0400

    function_auto_path ../love/public/src/function_name_new_get.mjs

--- web/public/src/function_name_new_get.mjs
+++ web/public/src/function_name_new_get.mjs
@@ -6,9 +6,9 @@ export async function function_name_new_get(plugin_fn, f_name_old) {
     plugin_fn,
   ));
   let f_name_new = await function_run(plugin_fn, [f_name_old]);
-  let r2 = {
+  let r = {
     f_name_new,
     f_name_old,
   };
-  return r2;
+  return r;
 }
