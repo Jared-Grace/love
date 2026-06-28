0cb79fe3c72d8c8c9b534f1b67fba5f8aa01fdc3
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:29:14 -0400

    t c list_filter_starts_with

--- public/src/list_filter_starts_with.mjs
+++ public/src/list_filter_starts_with.mjs
@@ -1,10 +1,8 @@
+import { text_starts_with_curried_right } from "../../../love/public/src/text_starts_with_curried_right.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 export function list_filter_starts_with(list, prefix) {
-  function lambda(item) {
-    let sw = text_starts_with(item, prefix);
-    return sw;
-  }
+  let lambda = text_starts_with_curried_right(prefix);
   let filtered = list_filter(list, lambda);
   return filtered;
 }
