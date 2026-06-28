be8d5585b68176df63b91ca4c3b5b5f85b4247f3
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:39:55 -0400

    function_auto_path ../love/public/src/js_expression_to_function.mjs

--- web/public/src/js_expression_to_function.mjs
+++ web/public/src/js_expression_to_function.mjs
@@ -16,9 +16,5 @@ export async function js_expression_to_function(ast, selects, args) {
   let f_name_new = property_get(r, "f_name_new");
   let node = list_single(selects);
   await function_new_expression(f_name_new, node);
-  let parsed = js_code_call_args_await_maybe_parse(
-    f_name_new,
-    missing,
-    declaration,
-  );
+  let parsed = js_code_call_args_await_maybe_parse(f_name_new, [], declaration);
 }
