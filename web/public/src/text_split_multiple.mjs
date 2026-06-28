6a5ecd8348cc5b24eb4492492ce954dcdd142a04
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:50:07 -0400

    function_auto_path ../love/public/src/text_split_multiple.mjs

--- web/public/src/text_split_multiple.mjs
+++ web/public/src/text_split_multiple.mjs
@@ -3,15 +3,15 @@ import { list_join } from "../../../love/public/src/list_join.mjs";
 import { text_split } from "../../../love/public/src/text_split.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function text_split_multiple(str, delimiters) {
+export function text_split_multiple(t, delimiters) {
   let v = list_first_remaining(delimiters);
   let remaining = property_get(v, "remaining");
   let first = property_get(v, "first");
   function lambda(delimiter) {
-    let split = text_split(str, delimiter);
-    str = list_join(split, first);
+    let split = text_split(t, delimiter);
+    t = list_join(split, first);
   }
   each(remaining, lambda);
-  let parts = text_split(str, first);
+  let parts = text_split(t, first);
   return parts;
 }
