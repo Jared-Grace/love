5b29ea08651d9c7215771b3ce52a900f33e87875
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:49:24 -0400

    function_auto_path ../love/public/src/list_first_remaining_from_comma_dot.mjs

--- web/public/src/list_first_remaining_from_comma_dot.mjs
+++ web/public/src/list_first_remaining_from_comma_dot.mjs
@@ -1,9 +1,7 @@
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
-export function list_first_remaining_from_comma_dot(
-  f_name_transformer_args_comma,
-) {
-  let split = text_split_comma_dot(f_name_transformer_args_comma);
+export function list_first_remaining_from_comma_dot(args_comma_dot) {
+  let split = text_split_comma_dot(args_comma_dot);
   let fr = list_first_remaining(split);
   return fr;
 }
