a5afbc6699f869a7b6cf204750b1f57868d88007
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:23:48 -0400

    function_auto_path ../love/public/src/function_name_new_get_args.mjs

--- public/src/function_name_new_get_args.mjs
+++ public/src/function_name_new_get_args.mjs
@@ -8,8 +8,8 @@ export async function function_name_new_get_args(
   plugin_fn,
   f_name_old_args_comma,
 ) {
-  let split2 = text_split_comma_dot(f_name_old_args_comma);
-  let fr = list_first_remaining(split2);
+  let list = text_split_comma_dot(f_name_old_args_comma);
+  let fr = list_first_remaining(list);
   let split = property_get(fr, "remaining");
   let f_name_old = property_get(fr, "first");
   ({ f_name_old, plugin_fn } = await function_name_new_get_generic(
