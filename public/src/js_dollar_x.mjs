0346485a4c4575113410c1f9a30b725b5cc43293
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 04:25:04 -0400

    function_auto_path ../love/public/src/js_dollar_x.mjs

--- public/src/js_dollar_x.mjs
+++ public/src/js_dollar_x.mjs
@@ -1,3 +1,6 @@
+import { list_remove } from "../../../love/public/src/list_remove.mjs";
+import { js_expand_generic } from "../../../love/public/src/js_expand_generic.mjs";
+import { log } from "../../../love/public/src/log.mjs";
 import { list_next_index } from "../../../love/public/src/list_next_index.mjs";
 import { js_expand } from "../../../love/public/src/js_expand.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
@@ -17,4 +20,12 @@ export async function js_dollar_x({
     let next = list_get(stack2, index);
     await js_expand(stack2, stack1, next, ast, index);
   }
+  return;
+  log(js_dollar_x.name, {
+    stack2,
+    stack1,
+    next,
+  });
+  let inserted = await js_expand_generic(next, stack2, index, ast);
+  list_remove(stack2, stack1);
 }
