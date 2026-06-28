b46fbaf036aebfb4677b2d8c56c616a90cc55094
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 00:03:14 -0400

    sb

--- web/public/src/app_calendar_preaching_ask_entries.mjs
+++ web/public/src/app_calendar_preaching_ask_entries.mjs
@@ -1,7 +1,9 @@
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 import { folder_user_docs_read_lines } from "../../../love/public/src/folder_user_docs_read_lines.mjs";
 export async function app_calendar_preaching_ask_entries() {
-  let lines = await folder_user_docs_read_lines("preaching_ask.txt");
+  let lines = await folder_user_docs_read_lines(
+    app_calendar_preaching_ask_file_name(),
+  );
   let filtered = list_filter_empty_not_is(lines);
   return filtered;
 }
