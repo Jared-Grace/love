d61e3eeecb75ee2ba318a924d75ce2f2e5fd52ca
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:02:22 -0400

    function_auto_path ../love/public/src/function_current_selects_apply_args.mjs

--- web/public/src/function_current_selects_apply_args.mjs
+++ web/public/src/function_current_selects_apply_args.mjs
@@ -1,5 +1,7 @@
+import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { function_current_selects_apply_generic } from "../../../love/public/src/function_current_selects_apply_generic.mjs";
 export async function function_current_selects_apply_args(apply_fn_name) {
+  let split = text_split_comma_dot(t);
   let args = [];
   await function_current_selects_apply_generic(apply_fn_name, args);
 }
