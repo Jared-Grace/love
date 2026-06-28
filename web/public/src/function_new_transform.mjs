07e66b5e62a0a3c80ed25689a8cdc9af876adc75
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 23:41:59 -0400

    function_auto_path ../love/public/src/function_new_transform.mjs

--- web/public/src/function_new_transform.mjs
+++ web/public/src/function_new_transform.mjs
@@ -2,8 +2,12 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_new } from "../../../love/public/src/function_new.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 export async function function_new_transform(f_name, lambda$ast) {
-  await function_new(f_name);
+  let declaration = await function_new(f_name);
   let r = await function_transform(f_name, lambda$ast);
   let output = property_get(r, "output");
-  return output;
+  let r2 = {
+    output,
+    declaration,
+  };
+  return r2;
 }
