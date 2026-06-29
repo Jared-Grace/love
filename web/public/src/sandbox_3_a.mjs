988c73ed41265b38ecbc02071272033d1912da65
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 05:49:47 -0400

    function_auto_path ../love/public/src/sandbox_3_a.mjs

--- web/public/src/sandbox_3_a.mjs
+++ web/public/src/sandbox_3_a.mjs
@@ -1,3 +1,4 @@
+import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { app_calendar_preaching_ask_entries_previous } from "../../../love/public/src/app_calendar_preaching_ask_entries_previous.mjs";
 import { app_calendar_preaching_ask_entries } from "../../../love/public/src/app_calendar_preaching_ask_entries.mjs";
 export async function sandbox_3_a() {
