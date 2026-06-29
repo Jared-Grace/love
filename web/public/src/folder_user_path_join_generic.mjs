77afd7e5b2a26ee69aa78d326fc2b4ffe89bbcc2
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 22:28:37 -0400

    function_auto_path ../love/public/src/folder_user_path_join_generic.mjs

--- web/public/src/folder_user_path_join_generic.mjs
+++ web/public/src/folder_user_path_join_generic.mjs
@@ -1,5 +1,5 @@
 import { path_join } from "../../../love/public/src/path_join.mjs";
-export function folder_user_path_join_generic(p, folder, file_name) {
-  let result2 = path_join([p, folder + "\\", file_name]);
+export function folder_user_path_join_generic(path_root, folder, file_name) {
+  let result2 = path_join([path_root, folder + "\\", file_name]);
   return result2;
 }
