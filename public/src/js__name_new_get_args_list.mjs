45b00833058a795c05a454b3b381effc1dafe0f5
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:06:36 -0400

    function_auto_path ../love/public/src/function_name_new_get_args_list.mjs

---
+++ public/src/js__name_new_get_args_list.mjs
@@ -0,0 +1,24 @@
+import { function_run } from "../../../love/public/src/function_run.mjs";
+import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
+import { function_name_new_get_generic_overrides } from "../../../love/public/src/function_name_new_get_generic_overrides.mjs";
+import { override_get } from "../../../love/public/src/override_get.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
+export async function js__name_new_get_args_list(
+  list,
+  name_old_transform,
+  plugin_fn,
+) {
+  let fr = list_first_remaining(list);
+  let split = property_get(fr, "remaining");
+  let f_name_old = property_get(fr, "first");
+  f_name_old = await name_old_transform(f_name_old);
+  plugin_fn = override_get(function_name_new_get_generic_overrides, plugin_fn);
+  let args_run = list_concat_single(f_name_old, split);
+  let f_name_new = await function_run(plugin_fn, args_run);
+  let r = {
+    f_name_new,
+    f_name_old,
+  };
+  return r;
+}
