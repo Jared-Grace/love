973d5d100910ea0a7ee643e1698c82e5cedf5e1e
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:06:28 -0400

    function_auto_path ../love/public/src/js_call_add_before_return.mjs

--- web/public/src/js_call_add_before_return.mjs
+++ web/public/src/js_call_add_before_return.mjs
@@ -1,13 +1,11 @@
-import { list_index_of_previous } from "../../../love/public/src/list_index_of_previous.mjs";
+import { list_insert } from "../../../love/public/src/list_insert.mjs";
+import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { js_call_add_generic } from "../../../love/public/src/js_call_add_generic.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_visit_returns } from "../../../love/public/src/js_visit_returns.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
 export async function js_call_add_before_return(ast, f_name) {
   let lambda_add = function lambda2(ast, statement) {
     function lambda(la) {
@@ -16,14 +14,10 @@ export async function js_call_add_before_return(ast, f_name) {
     let list = list_adder(lambda);
     let only = list_single(list);
     let stack = property_get(only, "stack");
+    let node = property_get(only, "node");
     let e1 = list_get_end_1(stack);
-    let index_previous = list_index_of_previous(list2, item2);
-    log(js_call_add_before_return.name, {
-      only,
-    });
-    return;
-    let body_block = js_flo_body(ast);
-    list_add(body_block, item);
+    let index = list_index_of(e1, node);
+    list_insert(body_block, index, item);
   };
   await js_call_add_generic(ast, f_name, lambda_add);
 }
