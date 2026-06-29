ab09ead0d35273a6f02c93e1eb0e1fd7cbf12bd0
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 05:48:54 -0400

    function_auto_path ../love/public/src/js_call_add_generic.mjs

--- public/src/js_call_add_generic.mjs
+++ public/src/js_call_add_generic.mjs
@@ -1,3 +1,4 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { js_call_fill_inner } from "../../../love/public/src/js_call_fill_inner.mjs";
 import { js_node_to_visitor } from "../../../love/public/src/js_node_to_visitor.mjs";
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
@@ -5,6 +6,7 @@ import { data_functions_get } from "../../../love/public/src/data_functions_get.
 export async function js_call_add_generic(ast, f_name, lambda_add) {
   let functions = await data_functions_get();
   let visited = [];
+  let unaliased = await function_name_unalias_only(name);
   let statement = js_parse_statement(f_name);
   lambda_add(ast, statement);
   let visitor = js_node_to_visitor(ast, statement);
