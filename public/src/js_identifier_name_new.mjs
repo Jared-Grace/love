ad983e70af48ebaa6ea356a288392a08c4197c2b
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:10:26 -0400

    function_auto_path ../love/public/src/js_identifier_name_new.mjs

--- public/src/js_identifier_name_new.mjs
+++ public/src/js_identifier_name_new.mjs
@@ -1,4 +1,5 @@
 import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 export function js_identifier_name_new(ast, name_from, name_to) {
-  return js_identifier_rename(ast, name_from, name_to);
+  let r = js_identifier_rename(ast, name_from, name_to);
+  return r;
 }
