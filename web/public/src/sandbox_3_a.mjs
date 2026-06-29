478708ea017760e17ca3dbc9be0d5e393c357047
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 00:33:31 -0400

    p

--- web/public/src/sandbox_3_a.mjs
+++ web/public/src/sandbox_3_a.mjs
@@ -1,4 +1,5 @@
 import { app_calendar_preaching_ask_entries } from "../../../love/public/src/app_calendar_preaching_ask_entries.mjs";
 export async function sandbox_3_a() {
   let filtered = await app_calendar_preaching_ask_entries();
+  let filtered2 = await app_calendar_preaching_ask_entries_previous();
 }
