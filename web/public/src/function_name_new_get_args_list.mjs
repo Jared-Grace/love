80f9ace8bc2b9f9ef6c523bef992136684ed29e1
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:12:51 -0400

    function_auto_path ../love/public/src/function_name_new_get_args_list.mjs

--- web/public/src/function_name_new_get_args_list.mjs
+++ web/public/src/function_name_new_get_args_list.mjs
@@ -4,7 +4,7 @@ import { function_name_unalias_only } from "../../../love/public/src/function_na
 export async function function_name_new_get_args_list(plugin_fn, list) {
   let name_old_transform = function_name_unalias_only;
   let r2 = await js_name_new_get_args_list(plugin_fn, list, name_old_transform);
-  let name_old = property_get(r2, "name_old");
-  let name_new = property_get(r2, "name_new");
+  let f_name_old = property_get(r2, "name_old");
+  let f_name_new = property_get(r2, "name_new");
   return r;
 }
