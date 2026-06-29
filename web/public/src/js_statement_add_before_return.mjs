dc006e7d409a80a04530d993fdabc9ce8921cb50
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 05:57:28 -0400

    function_auto_path ../love/public/src/js_statement_add_before_return.mjs

--- web/public/src/js_statement_add_before_return.mjs
+++ web/public/src/js_statement_add_before_return.mjs
@@ -1,6 +1,9 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
 export function js_statement_add_before_return(ast, item) {
+  function lambda(la) {}
+  let list = list_adder(lambda);
   let body_block = js_flo_body(ast);
   list_add(body_block, item);
 }
