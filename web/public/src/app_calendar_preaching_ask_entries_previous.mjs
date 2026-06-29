5b1b02c724a91553da5a21d214986f868d574b01
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 00:05:52 -0400

    sb

--- web/public/src/app_calendar_preaching_ask_entries_previous.mjs
+++ web/public/src/app_calendar_preaching_ask_entries_previous.mjs
@@ -1,7 +1,7 @@
 import { app_calendar_preaching_ask_entries_generic } from "../../../love/public/src/app_calendar_preaching_ask_entries_generic.mjs";
-import { folder_user_docs_path } from "../../../love/public/src/folder_user_docs_path.mjs";
+import { folder_user_docs_path_previous } from "../../../love/public/src/folder_user_docs_path.mjs";
 export async function app_calendar_preaching_ask_entries_previous() {
-  let file_name_to_path = folder_user_docs_path;
+  let file_name_to_path = folder_user_docs_path_previous;
   let filtered =
     await app_calendar_preaching_ask_entries_generic(file_name_to_path);
   return filtered;
