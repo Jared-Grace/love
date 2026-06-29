480d44d2d33805e9d9acfae1a845b0d4fd7ec5db
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:25:34 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -34,7 +34,9 @@ import { function_node_select_nested } from "../../../love/public/src/function_n
 export async function sandbox() {
   const f_name = sandbox_3_a.name;
   await function_current_set(f_name);
-  let r2 = await function_transform_current("");
+  let r2 = await function_transform_current(
+    "js_identifier_rename,list,difference",
+  );
   return r2;
   await function_current_selects_empty();
   let r = await function_node_select_args(
