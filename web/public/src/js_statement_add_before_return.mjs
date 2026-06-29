8f9f5c6426a465ae597828facc0b469ac1c7c759
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 05:58:06 -0400

    function_auto_path ../love/public/src/js_statement_add_before_return.mjs

--- web/public/src/js_statement_add_before_return.mjs
+++ web/public/src/js_statement_add_before_return.mjs
@@ -5,7 +5,7 @@ import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
 export function js_statement_add_before_return(ast, item) {
   function lambda(la) {
     function lambda3() {}
-    js_visit_returns(ast2, lambda3);
+    js_visit_returns(ast, lambda3);
   }
   let list = list_adder(lambda);
   let body_block = js_flo_body(ast);
