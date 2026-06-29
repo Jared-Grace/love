09cf69e0ddb6de0e833b1f71d3ff7c0304c42b15
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 22:23:33 -0400

    sb

--- public/src/app_calendar_preaching_ask_entries.mjs
+++ public/src/app_calendar_preaching_ask_entries.mjs
@@ -4,7 +4,9 @@ import { app_calendar_preaching_ask_file_name } from "../../../love/public/src/a
 import { list_filter_empty_not_is } from "../../../love/public/src/list_filter_empty_not_is.mjs";
 export async function app_calendar_preaching_ask_entries() {
   let file_name = app_calendar_preaching_ask_file_name();
-  let contents = await folder_user_docs_read(file_name);
+  let file_path = folder_user_docs_path(file_name);
+  let contents2 = await file_read(file_path);
+  let contents = contents2;
   let lines = text_split_newline(contents);
   let filtered = list_filter_empty_not_is(lines);
   return filtered;
