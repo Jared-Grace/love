9718304fbbcbba24f5f5870f6d67e48c22da4dea
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:04:16 -0400

    function_auto_path ../love/public/src/js_call_add_before_return.mjs

--- public/src/js_call_add_before_return.mjs
+++ public/src/js_call_add_before_return.mjs
@@ -1,5 +1,4 @@
 import { js_call_add_generic } from "../../../love/public/src/js_call_add_generic.mjs";
-import { js_flo_body_add } from "../../../love/public/src/js_flo_body_add.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { log } from "../../../love/public/src/log.mjs";
@@ -9,7 +8,7 @@ import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
 export async function js_call_add_before_return(ast, f_name) {
-  let lambda_add = js_flo_body_add;
+  let lambda_add = function lambda2() {};
   await js_call_add_generic(ast, f_name, lambda_add);
   function lambda(la) {
     js_visit_returns(ast, la);
