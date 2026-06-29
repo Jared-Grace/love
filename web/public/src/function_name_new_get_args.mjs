b17ca05fc33503b3ff5482ad89a6ab15bc0c8ab2
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:21:21 -0400

    function_auto_path ../love/public/src/js_identifier_name_new.mjs

--- web/public/src/function_name_new_get_args.mjs
+++ web/public/src/function_name_new_get_args.mjs
@@ -2,9 +2,9 @@ import { function_name_new_get_args_list } from "../../../love/public/src/functi
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 export async function function_name_new_get_args(
   plugin_fn,
-  f_name_old_args_comma,
+  args_comma,
 ) {
-  let list = text_split_comma_dot(f_name_old_args_comma);
+  let list = text_split_comma_dot(args_comma);
   let r = await function_name_new_get_args_list(plugin_fn, list);
   return r;
 }
