9085d9a8baab570370bd041791b6485b11ad1105
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:57:24 -0400

    w c c,replace

--- public/src/function_copy_generic_args_replace.mjs
+++ public/src/function_copy_generic_args_replace.mjs
@@ -1,1 +1,7 @@
-export function function_copy_generic_args_replace() {}
+import { function_copy_generic_args } from "../../../love/public/src/function_copy_generic_args.mjs";
+export async function function_copy_generic_args_replace(
+  plugin_fn,
+  args_comma,
+) {
+  return await function_copy_generic_args(plugin_fn, args_comma);
+}
