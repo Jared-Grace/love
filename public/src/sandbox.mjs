570b15c6566d3d984de90665250ecee6abd8c920
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:38:10 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -1,4 +1,4 @@
-import { folder_user_docs_path_previous } from "../../../love/public/src/folder_user_docs_path_previous.mjs";
+import { app_calendar_preaching_ask_entries } from "../../../love/public/src/app_calendar_preaching_ask_entries.mjs";
 import { js_call_named_find_alias } from "../../../love/public/src/js_call_named_find_alias.mjs";
 import { js_expand_selects } from "../../../love/public/src/js_expand_selects.mjs";
 import { function_current_selects_apply_args } from "../../../love/public/src/function_current_selects_apply_args.mjs";
@@ -26,10 +26,10 @@ import { js_call_callee_name_try } from "../../../love/public/src/js_call_callee
 import { function_current_selects_empty } from "../../../love/public/src/function_current_selects_empty.mjs";
 import { function_node_select_nested } from "../../../love/public/src/function_node_select_nested.mjs";
 export async function sandbox() {
-  const f_name = folder_user_docs_path_previous.name;
+  const f_name = app_calendar_preaching_ask_entries.name;
   await function_current_set(f_name);
   let r2 = await function_transform_current(
-    "js_identifier_name_new,c,folder_user_path,previous",
+    "js_identifier_name_new,l,folder_user_docs_path_previous",
   );
   return r2;
   await function_current_selects_empty();
