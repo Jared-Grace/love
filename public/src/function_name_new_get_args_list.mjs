9a18ad6859165baacf833d8abc09d5c92fbce769
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:03:10 -0400

    function_auto_path ../love/public/src/function_name_new_get_args_list.mjs

--- public/src/function_name_new_get_args_list.mjs
+++ public/src/function_name_new_get_args_list.mjs
@@ -1,18 +1,16 @@
+import { function_name_new_get_generic_overrides } from "../../../love/public/src/function_name_new_get_generic_overrides.mjs";
+import { override_get } from "../../../love/public/src/override_get.mjs";
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { function_run } from "../../../love/public/src/function_run.mjs";
 import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
-import { function_name_new_get_generic } from "../../../love/public/src/function_name_new_get_generic.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 export async function function_name_new_get_args_list(plugin_fn, list) {
   let fr = list_first_remaining(list);
   let split = property_get(fr, "remaining");
   let f_name_old = property_get(fr, "first");
-  $x(
-    ({ f_name_old, plugin_fn } = await function_name_new_get_generic(
-      f_name_old,
-      plugin_fn,
-    )),
-  );
+  f_name_old = await function_name_unalias_only(f_name_old);
+  plugin_fn = override_get(function_name_new_get_generic_overrides, plugin_fn);
   let args_run = list_concat_single(f_name_old, split);
   let f_name_new = await function_run(plugin_fn, args_run);
   let r = {
