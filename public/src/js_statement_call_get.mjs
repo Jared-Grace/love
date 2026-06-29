0fe3ee52b1aad214baa32fa4ad15af0c81557552
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 22:23:50 -0400

    function_auto_path ../love/public/src/js_statement_call_get.mjs

--- public/src/js_statement_call_get.mjs
+++ public/src/js_statement_call_get.mjs
@@ -1,6 +1,5 @@
 import { js_await_if_unwrap_argument } from "../../../love/public/src/js_await_if_unwrap_argument.mjs";
 import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
@@ -11,9 +10,6 @@ export function js_statement_call_get(node) {
   let call = null;
   let declaration = null;
   let assignment = null;
-  log(js_statement_call_get.name, {
-    node,
-  });
   if (js_node_type_is(node, "ExpressionStatement")) {
     let expression_next = property_get(node, "expression");
     call = expression_next;
