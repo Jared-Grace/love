ceff4772564e3ab0d86664bef2b4db9fcf9b549b
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:04:13 -0400

    function_auto_path ../love/public/src/js_call_add_before_return.mjs

--- web/public/src/js_call_add_before_return.mjs
+++ web/public/src/js_call_add_before_return.mjs
@@ -8,7 +8,7 @@ import { js_visit_returns } from "../../../love/public/src/js_visit_returns.mjs"
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_flo_body } from "../../../love/public/src/js_flo_body.mjs";
-export async function js_call_add_before_return(ast, item) {
+export async function js_call_add_before_return(ast, f_name) {
   let lambda_add = js_flo_body_add;
   await js_call_add_generic(ast, f_name, lambda_add);
   function lambda(la) {
