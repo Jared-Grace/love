38e9aeab0edfa69d0c6366dfff9ff5848276bdab
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:25:35 -0400

    sb

--- web/public/src/sandbox_3_a.mjs
+++ web/public/src/sandbox_3_a.mjs
@@ -6,6 +6,6 @@ export async function sandbox_3_a() {
   let filtered = await app_calendar_preaching_ask_entries();
   let filtered2 = await app_calendar_preaching_ask_entries_previous();
   let difference = list_difference(filtered2, filtered);
-  let joined = list_join_newline(list);
+  let joined = list_join_newline(difference);
   return joined;
 }
