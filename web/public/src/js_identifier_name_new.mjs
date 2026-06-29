ee3077a24072cbdaa196a4d50403f888d100dbb5
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:28:08 -0400

    function_auto_path ../love/public/src/js_identifier_name_new.mjs

--- web/public/src/js_identifier_name_new.mjs
+++ web/public/src/js_identifier_name_new.mjs
@@ -1,11 +1,11 @@
-import { list_take } from "../../../love/public/src/list_take.mjs";
+import { list_to } from "../../../love/public/src/list_to.mjs";
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_name_new_get_args_list } from "../../../love/public/src/js_name_new_get_args_list.mjs";
 import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 export async function js_identifier_name_new(ast, plugin_fn) {
-  let taken = list_take(list2, count);
+  let taken = list_to(arguments, count);
   let list = text_split_comma_dot(args_comma);
   let r2 = await js_name_new_get_args_list(plugin_fn, list, identity);
   let name_old = property_get(r2, "name_old");
