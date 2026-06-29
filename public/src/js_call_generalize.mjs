0fc00cac45d75d383aaa985b27c9d1ce5c02acca
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:49:43 -0400

    function_auto_path ../love/public/src/js_call_generalize.mjs

--- public/src/js_call_generalize.mjs
+++ public/src/js_call_generalize.mjs
@@ -1,1 +1,4 @@
-export function js_call_generalize(ast, name_identifier, name_generalized) {}
+import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
+export function js_call_generalize(ast, name_identifier, name_generalized) {
+  js_identifier_rename(ast2, name_from, name_to);
+}
