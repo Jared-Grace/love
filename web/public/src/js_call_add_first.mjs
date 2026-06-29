c281ac6162dbd8e8c1a7862722e83f25e9196958
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 00:27:38 -0400

    sb

--- web/public/src/js_call_add_first.mjs
+++ web/public/src/js_call_add_first.mjs
@@ -4,10 +4,11 @@ import { js_flo_body_add_first } from "../../../love/public/src/js_flo_body_add_
 import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
 import { data_functions_get } from "../../../love/public/src/data_functions_get.mjs";
 export async function js_call_add_first(ast, f_name) {
+  let lambda_add = js_flo_body_add_first;
   let functions = await data_functions_get();
   let visited = [];
   let statement = js_parse_statement(f_name);
-  js_flo_body_add_first(ast, statement);
+  lambda_add(ast, statement);
   let visitor = js_node_to_visitor(ast, statement);
   await js_call_fill_inner(ast, visitor, functions, visited);
 }
