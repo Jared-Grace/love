ec693c9dd6c11128092c175fa6e4f6a6b5549a89
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:51:15 -0400

    function_auto_path ../love/public/src/js_call_generalize.mjs

--- web/public/src/js_call_generalize.mjs
+++ web/public/src/js_call_generalize.mjs
@@ -1,7 +1,9 @@
+import { js_flo_body_add_first } from "../../../love/public/src/js_flo_body_add_first.mjs";
 import { js_flo } from "../../../love/public/src/js_flo.mjs";
 import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 export function js_call_generalize(ast, name_identifier, name_generalized) {
   "we do not want to rename the import";
   let declaration = js_flo(ast);
   js_identifier_rename(declaration, name_identifier, name_generalized);
+  js_flo_body_add_first(ast2, item);
 }
