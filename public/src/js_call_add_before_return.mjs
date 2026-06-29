41066ff56a2dc9234564ace3bbb4241553545a43
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:13:50 -0400

    function_auto_path ../love/public/src/js_call_add_before_return.mjs

--- public/src/js_call_add_before_return.mjs
+++ public/src/js_call_add_before_return.mjs
@@ -1,5 +1,6 @@
 import { js_find_return_insert_statement } from "../../../love/public/src/js_find_return_insert_statement.mjs";
 import { js_call_add_generic } from "../../../love/public/src/js_call_add_generic.mjs";
 export async function js_call_add_before_return(ast, f_name) {
-  await js_call_add_generic(ast, f_name, js_find_return_insert_statement);
+  let lambda_add = js_find_return_insert_statement;
+  await js_call_add_generic(ast, f_name, lambda_add);
 }
