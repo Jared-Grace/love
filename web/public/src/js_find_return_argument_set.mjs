38ab69d1cc073e5b9aff16c689ae80a59d5d4856
JESUS rose to life <username@users.noreply.github.com>
2026-06-29 06:23:57 -0400

    function_auto_path ../love/public/src/js_find_return_argument_set.mjs

--- web/public/src/js_find_return_argument_set.mjs
+++ web/public/src/js_find_return_argument_set.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { js_find_return } from "../../../love/public/src/js_find_return.mjs";
 import { js_return_argument_set } from "../../../love/public/src/js_return_argument_set.mjs";
@@ -5,4 +6,5 @@ export function js_find_return_argument_set(ast, code) {
   let only = js_find_return(ast);
   let expression = js_parse_expression(code);
   js_return_argument_set(only, expression);
+  log(js_find_return_argument_set.name, {});
 }
