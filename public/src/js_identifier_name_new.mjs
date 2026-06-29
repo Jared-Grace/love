ea4bb66062f091c0c9083d093cce453c14ffb93d
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:10:38 -0400

    function_auto_path ../love/public/src/js_identifier_name_new.mjs

--- public/src/js_identifier_name_new.mjs
+++ public/src/js_identifier_name_new.mjs
@@ -1,5 +1,7 @@
+import { js_name_new_get_args_list } from "../../../love/public/src/js_name_new_get_args_list.mjs";
 import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
-export function js_identifier_name_new(ast, name_from, name_to) {
+export async function js_identifier_name_new(ast, name_from, name_to) {
+  let r2 = await js_name_new_get_args_list(plugin_fn, list, name_old_transform);
   let r = js_identifier_rename(ast, name_from, name_to);
   return r;
 }
