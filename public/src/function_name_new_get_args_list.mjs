953397f9a70d7ee69ba33a244dc2111b67a14ca3
JESUS rose to life <username@users.noreply.github.com>
2026-06-28 23:13:00 -0400

    function_auto_path ../love/public/src/function_name_new_get_args_list.mjs

--- public/src/function_name_new_get_args_list.mjs
+++ public/src/function_name_new_get_args_list.mjs
@@ -6,5 +6,9 @@ export async function function_name_new_get_args_list(plugin_fn, list) {
   let r2 = await js_name_new_get_args_list(plugin_fn, list, name_old_transform);
   let f_name_old = property_get(r2, "name_old");
   let f_name_new = property_get(r2, "name_new");
+  let r = {
+    f_name_old,
+    f_name_new,
+  };
   return r;
 }
