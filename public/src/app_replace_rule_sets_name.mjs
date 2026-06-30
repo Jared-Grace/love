f43083f19b82f8e5c98d100ae6071d53dcc64837
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 07:40:07 -0400

    pb app_shared_name_text_to_idenitifier

--- public/src/app_replace_rule_sets_name.mjs
+++ public/src/app_replace_rule_sets_name.mjs
@@ -4,6 +4,6 @@ import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule
 export function app_replace_rule_sets_name(found) {
   let n = app_replace_rule_sets.name;
   let prefix = text_suffix_without(n, "s");
-  let f_name_new = app_shared_name_text_to_idenitifier(found, prefix);
+  let f_name_new = app_shared_name_text_to_idenitifier(prefix, found);
   return f_name_new;
 }
