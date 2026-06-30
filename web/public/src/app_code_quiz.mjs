7a788266c47087447d3c7f1d6ffe853ff816a2e2
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 08:33:50 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_current } from "../../../love/public/src/app_code_lesson_current.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 export function app_code_quiz(context) {
   let root = html_clear_context(context);
