5270d9e165131db89c376160e3eb303365576da4
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:49:56 -0400

    function_auto_path ../love/public/src/list_map_index_of.mjs

--- public/src/list_map_index_of.mjs
+++ public/src/list_map_index_of.mjs
@@ -1,7 +1,7 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_index_of_curried } from "../../../love/public/src/list_index_of_curried.mjs";
-export function list_map_index_of(list, list_indices) {
-  let r = list_index_of_curried(list_indices);
+export function list_map_index_of(list, items) {
+  let r = list_index_of_curried(items);
   let indices = list_map(list, r);
   return indices;
 }
