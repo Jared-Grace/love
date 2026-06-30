c6f50eb47b5660190fb5ad385bc743c67c9b18a0
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:40:38 -0400

    function_auto_path ../love/public/src/html_font_color_set_white.mjs

--- web/public/src/html_font_color_set_white.mjs
+++ web/public/src/html_font_color_set_white.mjs
@@ -1,4 +1,4 @@
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
-export function html_font_color_set_white(span2) {
-  html_font_color_set(span2, "white");
+export function html_font_color_set_white(parent) {
+  html_font_color_set(parent, "white");
 }
