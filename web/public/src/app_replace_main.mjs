bed96a6c0d24864e290d06b1401a8c2adce32534
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:46:03 -0400

    function_auto_path ../love/public/src/app_replace_main.mjs

--- web/public/src/app_replace_main.mjs
+++ web/public/src/app_replace_main.mjs
@@ -2,7 +2,7 @@ import { app_shared_initialize_refresh } from "../../../love/public/src/app_shar
 import { app_replace_screens } from "../../../love/public/src/app_replace_screens.mjs";
 import { app_replace } from "../../../love/public/src/app_replace.mjs";
 export async function app_replace_main(context) {
-  let app_fn = app_replace;
   let screens = app_replace_screens();
+  let app_fn = app_replace;
   await app_shared_initialize_refresh(context, app_fn, screens);
 }
