d245871392652c3488126a20116f59a32c6a7a43
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:43:38 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- web/public/src/app_code_main.mjs
+++ web/public/src/app_code_main.mjs
@@ -1,7 +1,9 @@
+import { digits } from "../../../love/public/src/digits.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 export function app_code_main(context) {
   let root = property_get(context, "root");
+  let split = digits();
   html_p_text_multiple(root, [
     "In computer programming",
     "There are symbols",
