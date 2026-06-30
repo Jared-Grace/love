74d815f94ee12309cfab47e44b153a24a0c40f1d
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:05:49 -0400

    ps

--- web/public/src/app_code_container_dark.mjs
+++ web/public/src/app_code_container_dark.mjs
@@ -1,11 +1,9 @@
-import { undefined } from "../../../love/public/src/undefined.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
 import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_code_container_dark(parent) {
-  let undefined = undefined;
   let div3 = html_div(parent);
   html_style_background_color_black(div3);
   html_style_padding(div3, "0.2em");
