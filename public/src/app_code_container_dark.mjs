adfa4ea4ee7f397c86b1120930fc291c7e6f5178
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:07:09 -0400

    function_auto_path ../love/public/src/app_code_container_dark.mjs

--- public/src/app_code_container_dark.mjs
+++ public/src/app_code_container_dark.mjs
@@ -1,14 +1,7 @@
-import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
-import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
-import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
+import { app_code_container_generic } from "../../../love/public/src/app_code_container_generic.mjs";
 import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
-import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_code_container_dark(parent) {
   let color_background_set = html_style_background_color_black;
-  let div3 = html_div(parent);
-  color_background_set(div3);
-  html_style_padding(div3, "0.2em");
-  html_border_radius_em(div3, 0.75);
-  html_border_none(div3);
+  let div3 = app_code_container_generic(parent, color_background_set);
   return div3;
 }
