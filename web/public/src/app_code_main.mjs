dbe2d21cccecf423da2b98d1fd019a0ec4e02fb7
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:38:46 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- web/public/src/app_code_main.mjs
+++ web/public/src/app_code_main.mjs
@@ -2,5 +2,10 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 export function app_code_main(context) {
   let root = property_get(context, "root");
-  html_p_text_multiple(parent, list);
+  html_p_text_multiple(root, [
+    "In English:",
+    "There are letters",
+    "Letters are inside words",
+    "Words are inside sentences",
+  ]);
 }
