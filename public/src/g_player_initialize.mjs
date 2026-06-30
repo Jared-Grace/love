086146397a542459523e4b873f0a573517662c1d
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 02:30:20 -0400

    function_auto_path ../love/public/src/g_player_initialize.mjs

--- public/src/g_player_initialize.mjs
+++ public/src/g_player_initialize.mjs
@@ -1,12 +1,10 @@
+import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
 import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
 import { bible_names_men } from "../../../love/public/src/bible_names_men.mjs";
-import { list_single } from "../../../love/public/src/list_single.mjs";
-import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
 import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs";
 export function g_player_initialize(player_img, coordinates_land) {
   let player = {};
-  let player_list = list_remove_last(coordinates_land);
-  let player_coordinates = list_single(player_list);
+  let player_coordinates = list_remove_last_single(coordinates_land);
   object_merge_set(player, player_coordinates);
   let names_men = bible_names_men();
   object_merge_set(player, {
