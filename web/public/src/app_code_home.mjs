a679e08c87992de8852293df98ccb8addd30ee9d
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:39:06 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/app_code_home.mjs
+++ web/public/src/app_code_home.mjs
@@ -71,7 +71,6 @@ export function app_code_home(context) {
           answer: function lambda3(parent_button) {
             html_text_set(parent_button, "Number of symbols: ");
             let div3 = app_code_container_dark(parent_button);
-            html_text_set(div3, digit_count);
             let span22 = app_code_symbol(div3, digit_count);
           },
         },
