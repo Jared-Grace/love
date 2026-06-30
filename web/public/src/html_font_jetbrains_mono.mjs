387acc2fd303fa62eefe5458192fea2805254dc7
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:12:29 -0400

    function_auto_path ../love/public/src/html_font_jetbrains_mono.mjs

--- web/public/src/html_font_jetbrains_mono.mjs
+++ web/public/src/html_font_jetbrains_mono.mjs
@@ -1,6 +1,6 @@
 import { html_jetbrains_mono_include } from "../../../love/public/src/html_jetbrains_mono_include.mjs";
 import { html_font_set } from "../../../love/public/src/html_font_set.mjs";
-export function html_font_jetbrains_mono(div) {
+export function html_font_jetbrains_mono(component) {
   html_jetbrains_mono_include();
-  html_font_set(div, '"JetBrains Mono", monospace');
+  html_font_set(component, '"JetBrains Mono", monospace');
 }
