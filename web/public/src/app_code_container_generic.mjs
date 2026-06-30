33d8d635bfc6718cc88d2f8ee9a89cac2a51f729
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:07:09 -0400

    function_auto_path ../love/public/src/app_code_container_dark.mjs

---
+++ web/public/src/app_code_container_generic.mjs
@@ -0,0 +1,12 @@
+import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
+import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
+import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+export function app_code_container_generic(parent, color_background_set) {
+  let div3 = html_div(parent);
+  color_background_set(div3);
+  html_style_padding(div3, "0.2em");
+  html_border_radius_em(div3, 0.75);
+  html_border_none(div3);
+  return div3;
+}
