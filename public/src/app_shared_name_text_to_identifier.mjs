46ad7b3af852ba0a481976b1e1f6ff438750c095
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 07:42:25 -0400

    function_auto_path ../love/public/src/app_shared_name_text_to_identifier.mjs

--- public/src/app_shared_name_text_to_identifier.mjs
+++ public/src/app_shared_name_text_to_identifier.mjs
@@ -2,7 +2,7 @@ import { function_name_combine } from "../../../love/public/src/function_name_co
 import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
 import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
 import { js_property_value_get } from "../../../love/public/src/js_property_value_get.mjs";
-export function app_shared_name_text_to_idenitifier(prefix, name) {
+export function app_shared_name_text_to_identifier(prefix, name) {
   let p = js_property_value_get(name);
   let l = js_literal_value_get(p);
   let rl = text_replace_space_underscore_lower(l);
