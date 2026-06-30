3791d9b1cee3911324044e4710fd06d3a34b083f
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:25:57 -0400

    function_auto_path ../love/public/src/app_code_container_light.mjs

--- web/public/src/app_code_container_light.mjs
+++ web/public/src/app_code_container_light.mjs
@@ -1,11 +1,15 @@
+import { app_replace_button_symbol_style_box_shadow_value_width_set } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_width_set.mjs";
 import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
-import { app_replace_button_symbol_style_box_shadow_value_set } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs";
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { app_code_container_generic } from "../../../love/public/src/app_code_container_generic.mjs";
 export function app_code_container_light(parent) {
   let container = app_code_container_generic(parent);
   html_style_background_color_set(container, "#eee");
-  app_replace_button_symbol_style_box_shadow_value_set(container, "#ccc");
+  app_replace_button_symbol_style_box_shadow_value_width_set(
+    container,
+    "#ccc",
+    "0.05",
+  );
   html_style_margin_y(container, 0.3 + "em");
   return container;
 }
