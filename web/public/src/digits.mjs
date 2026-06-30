77e05e42fb7e94eafed222793a7a6dfff8997d23
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:43:46 -0400

    function_auto_path ../love/public/src/digits.mjs

--- web/public/src/digits.mjs
+++ web/public/src/digits.mjs
@@ -2,6 +2,6 @@ import { digits_text } from "../../../love/public/src/digits_text.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 export function digits() {
   let d = digits_text();
-  let split = text_split_empty(d);
-  return split;
+  let ds = text_split_empty(d);
+  return ds;
 }
