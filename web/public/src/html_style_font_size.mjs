47915c52d01021ed40d66fc1fa7991a1b7753459
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:53:49 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/html_style_font_size.mjs
+++ web/public/src/html_style_font_size.mjs
@@ -1,4 +1,4 @@
 import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
-export function html_style_font_size(html, value) {
-  html_style_set(html, "font-size", value);
+export function html_style_font_size(parent, value) {
+  html_style_set(parent, "font-size", value);
 }
