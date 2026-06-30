9a31743198570cbdfc09fe55f8ccb07d0828b877
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:17:53 -0400

    pd app_code_container_generic color_background_set

--- web/public/src/app_code_container_generic.mjs
+++ web/public/src/app_code_container_generic.mjs
@@ -2,7 +2,7 @@ import { html_border_none } from "../../../love/public/src/html_border_none.mjs"
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
-export function app_code_container_generic(parent, color_background_set) {
+export function app_code_container_generic(parent) {
   let container = html_div(parent);
   color_background_set(container);
   html_style_padding(container, "0.2em");
