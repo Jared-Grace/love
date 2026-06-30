951c8b0e5d08371eec0ad8f98601e7f17c4dcc2a
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:24:17 -0400

    function_auto_path ../love/public/src/app_replace_button_symbol_style_box_shadow_value.mjs

--- web/public/src/app_replace_button_symbol_style_box_shadow_value.mjs
+++ web/public/src/app_replace_button_symbol_style_box_shadow_value.mjs
@@ -1,5 +1,6 @@
+import { app_replace_button_symbol_style_box_shadow_value_width } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_width.mjs";
 export function app_replace_button_symbol_style_box_shadow_value(color) {
   const width = "0.1";
-  let r = " 0 0 0 " + width + "em" + " " + color;
+  let r = app_replace_button_symbol_style_box_shadow_value_width(width, color);
   return r;
 }
