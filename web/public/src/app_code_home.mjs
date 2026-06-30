52af0ee5484bce5bf8c2aca138cf647f54099513
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 09:34:07 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/app_code_home.mjs
+++ web/public/src/app_code_home.mjs
@@ -1,4 +1,4 @@
-import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
+import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_empty } from "../../../love/public/src/list_empty.mjs";
 import { app_code_example_text } from "../../../love/public/src/app_code_example_text.mjs";
 import { app_code_quiz } from "../../../love/public/src/app_code_quiz.mjs";
@@ -31,7 +31,6 @@ export function app_code_home(context) {
   let refresh = function lambda() {
     let e = list_empty_is(b);
     if (e) {
-      list_empty(b);
       list_replace_all(list, items);
       b = batch();
       list_shuffle(b);
