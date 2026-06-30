44f8081c78729523faeb3b76c183037b5de51e08
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:40:13 -0400

    function_auto_path ../love/public/src/html_font_color_set_white.mjs

--- public/src/app_code_home.mjs
+++ public/src/app_code_home.mjs
@@ -1,4 +1,4 @@
-import { html_font_color_set_blue } from "../../../love/public/src/html_font_color_set_blue.mjs";
+import { html_font_color_set_white } from "../../../love/public/src/html_font_color_set_white.mjs";
 import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 import { app_code_container_dark } from "../../../love/public/src/app_code_container_dark.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
@@ -73,7 +73,7 @@ export function app_code_home(context) {
             html_text_set(parent_button, "Number of symbols: ");
             let div3 = app_code_container_dark(parent_button);
             html_text_set(div3, digit_count);
-            html_font_color_set_blue(span22);
+            html_font_color_set_white(span22);
           },
         },
         answer: digit_count,
