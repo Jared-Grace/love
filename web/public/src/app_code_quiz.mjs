9b611cb18bb927c9d8c2a8263ffbfcc010ba4412
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 08:34:18 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -1,6 +1,8 @@
+import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
 import { app_code_lesson_current } from "../../../love/public/src/app_code_lesson_current.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
+  let lesson_id = storage_local_initialize_context(context, "quiz_index", 0);
 }
