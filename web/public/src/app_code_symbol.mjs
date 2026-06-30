1b180a4e27d2ef0a2bb20574d9c2343735166d49
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:45:36 -0400

    function_auto_path ../love/public/src/app_code_symbol.mjs

--- web/public/src/app_code_symbol.mjs
+++ web/public/src/app_code_symbol.mjs
@@ -5,11 +5,11 @@ import { html_style_background_color_set } from "../../../love/public/src/html_s
 import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
 import { app_replace_button_symbol } from "../../../love/public/src/app_replace_button_symbol.mjs";
 export function app_code_symbol(parent, d) {
-  let span2 = app_replace_button_symbol(parent, d);
-  app_replace_button_symbol_style(span2);
-  html_style_background_color_set(span2, "#444");
-  html_font_color_set(span2, "white");
+  let s = app_replace_button_symbol(parent, d);
+  app_replace_button_symbol_style(s);
+  html_style_background_color_set(s, "#444");
+  html_font_color_set(s, "white");
   let style_value = app_replace_button_symbol_style_box_shadow_value("#777");
-  html_style_set(span2, "box-shadow", style_value);
-  return span2;
+  html_style_set(s, "box-shadow", style_value);
+  return s;
 }
