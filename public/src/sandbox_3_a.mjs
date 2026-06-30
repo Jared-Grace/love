590e35d32f3ecc5affe365756951dc03f3957c49
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:12:31 -0400

    function_auto_path ../love/public/src/sandbox_3_a.mjs

--- public/src/sandbox_3_a.mjs
+++ public/src/sandbox_3_a.mjs
@@ -1,11 +1,5 @@
-import { list_join_newline_2 } from "../../../love/public/src/list_join_newline_2.mjs";
-import { list_difference } from "../../../love/public/src/list_difference.mjs";
-import { app_calendar_preaching_ask_entries_previous } from "../../../love/public/src/app_calendar_preaching_ask_entries_previous.mjs";
-import { app_calendar_preaching_ask_entries } from "../../../love/public/src/app_calendar_preaching_ask_entries.mjs";
+import { app_calendar_preaching_ask_entries_previous_difference } from "../../../love/public/src/app_calendar_preaching_ask_entries_previous_difference.mjs";
 export async function sandbox_3_a() {
-  let filtered = await app_calendar_preaching_ask_entries();
-  let filtered2 = await app_calendar_preaching_ask_entries_previous();
-  let difference = list_difference(filtered2, filtered);
-  let joined = list_join_newline_2(difference);
+  let joined = await app_calendar_preaching_ask_entries_previous_difference();
   return joined;
 }
