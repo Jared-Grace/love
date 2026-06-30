832f098f9b9780eaebd02105f1ab4ab638c975d7
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:46:32 -0400

    function_auto_path ../love/public/src/app_code_symbol.mjs

---
+++ public/src/app_code_symbol_generic.mjs
@@ -0,0 +1,21 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+import { app_replace_button_symbol_style_box_shadow_value } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value.mjs";
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
+import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
+import { app_replace_button_symbol } from "../../../love/public/src/app_replace_button_symbol.mjs";
+export function app_code_symbol_generic(
+  parent,
+  d,
+  color_background,
+  color_box_shadow,
+) {
+  let s = app_replace_button_symbol(parent, d);
+  app_replace_button_symbol_style(s);
+  html_style_background_color_set(s, color_background);
+  html_font_color_set(s, "white");
+  let style_value =
+    app_replace_button_symbol_style_box_shadow_value(color_box_shadow);
+  html_style_set(s, "box-shadow", style_value);
+  return s;
+}
