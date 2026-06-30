f51cf4fb75f9f1ef64ca878f156f92bb00b04146
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 02:24:03 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- public/src/app_code_home.mjs
+++ public/src/app_code_home.mjs
@@ -1,10 +1,10 @@
+import { range_1 } from "../../../love/public/src/range_1.mjs";
 import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 import { integer_positive_random_digits_text } from "../../../love/public/src/integer_positive_random_digits_text.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { integer_random_1 } from "../../../love/public/src/integer_random_1.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
@@ -42,7 +42,7 @@ export function app_code_home(context) {
     "Symbols are inside ",
   ]);
   function digit_batch() {
-    let digit_counts = integer_random_1(5);
+    let digit_counts = range_1(5);
     function lambda(digit_count) {
       let digits = integer_positive_random_digits_text(digit_count);
       let r2 = {
