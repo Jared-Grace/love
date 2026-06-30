a79bb4bac06dbb4991ecd6ed8b758949f295f2c7
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:07:22 -0400

    c 1 app_code_container_dark,light

---
+++ public/src/app_code_container_light.mjs
@@ -0,0 +1,7 @@
+import { app_code_container_generic } from "../../../love/public/src/app_code_container_generic.mjs";
+import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
+export function app_code_container_light(parent) {
+  let color_background_set = html_style_background_color_black;
+  let div3 = app_code_container_generic(parent, color_background_set);
+  return div3;
+}
