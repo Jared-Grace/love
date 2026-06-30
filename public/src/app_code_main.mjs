327a64724cbfc345cf050dec6b265e22bb763a9e
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:49:54 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- public/src/app_code_main.mjs
+++ public/src/app_code_main.mjs
@@ -9,23 +9,4 @@ export async function app_code_main(context) {
   let app_fn = app_code;
   let screens = app_code_screens();
   await app_shared_initialize_refresh(context, app_fn, screens);
-  let root = property_get(context, "root");
-  let split = digits();
-  function lambda2() {}
-  let b = app_replace_button(root, "Show me another example", lambda2);
-  return;
-  html_p_text_multiple(root, [
-    "In computer programming",
-    "There are symbols",
-    "All 10 of these numbers are different symbols: ",
-  ]);
-  html_p_text_multiple(root, [
-    "In English:",
-    "There are letters",
-    "Letters are inside words",
-    "Words are inside sentences",
-    "Compture programs have a similar structure",
-    "In a computer program, there are symbols",
-    "Symbols are inside ",
-  ]);
 }
