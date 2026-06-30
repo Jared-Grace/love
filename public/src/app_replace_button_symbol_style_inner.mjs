4baaf3a66827d16befcef7a6dcdd6c6b3323a5e6
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:19:24 -0400

    function_auto_path ../love/public/src/app_replace_button_symbol_style_inner.mjs

--- public/src/app_replace_button_symbol_style_inner.mjs
+++ public/src/app_replace_button_symbol_style_inner.mjs
@@ -5,8 +5,8 @@ import { html_margin_em } from "../../../love/public/src/html_margin_em.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 export function app_replace_button_symbol_style_inner(b) {
   html_border_radius_em(b, 0.5);
-  html_margin_em(b, 0.09);
   html_border_none(b);
+  html_margin_em(b, 0.09);
   html_style_padding_x(b, "0.37em");
   html_style_padding_y(b, "0.1em");
 }
