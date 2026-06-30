c2450cc2c23619df2274248e85a8abbea0b216ad
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 09:35:09 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/app_code_home.mjs
+++ web/public/src/app_code_home.mjs
@@ -1,4 +1,4 @@
-import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
+import { app_code_batch_item_get } from "../../../love/public/src/app_code_batch_item_get.mjs";
 import { app_code_example_text } from "../../../love/public/src/app_code_example_text.mjs";
 import { app_code_quiz } from "../../../love/public/src/app_code_quiz.mjs";
 import { app_code_lesson_current } from "../../../love/public/src/app_code_lesson_current.mjs";
@@ -8,13 +8,9 @@ import { html_hr } from "../../../love/public/src/html_hr.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
-import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
-import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
-import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 export function app_code_home(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
@@ -27,17 +23,7 @@ export function app_code_home(context) {
   let b = [];
   html_div_text(root, "Do you want another example?");
   let combined = app_code_example_text();
-  let refresh = function lambda() {
-    let e = list_empty_is(b);
-    if (e) {
-      list_replace_all(list, items);
-      list_shuffle(b);
-    }
-    html_clear(example_div);
-    let r = list_remove_last_single(b);
-    on_batch_item(r);
-  };
-  refresh();
+  let refresh = app_code_batch_item_get(b, example_div, on_batch_item);
   app_replace_button(root, combined, refresh);
   html_hr(root);
   html_div_text(root, "Do you want to be quizzed now?");
