bb1eb9b4c1d487c3fd2ecd092177a15a79c08a03
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:25:53 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/app_code_home.mjs
+++ web/public/src/app_code_home.mjs
@@ -1,13 +1,11 @@
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
 import { app_replace_button_symbol_style_box_shadow } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow.mjs";
 import { app_replace_rule_set_highlight } from "../../../love/public/src/app_replace_rule_set_highlight.mjs";
 import { html_data_set_test_suffix } from "../../../love/public/src/html_data_set_test_suffix.mjs";
 import { app_replace_button_symbol_style_valid_if_attribute } from "../../../love/public/src/app_replace_button_symbol_style_valid_if_attribute.mjs";
 import { html_style_font_color_set_if } from "../../../love/public/src/html_style_font_color_set_if.mjs";
-import { html_style_background_color_set_if_else } from "../../../love/public/src/html_style_background_color_set_if_else.mjs";
 import { app_replace_button_symbol_style_background_color_valid } from "../../../love/public/src/app_replace_button_symbol_style_background_color_valid.mjs";
-import { app_replace_button_symbol_style_background_color_invalid } from "../../../love/public/src/app_replace_button_symbol_style_background_color_invalid.mjs";
-import { html_enable_if } from "../../../love/public/src/html_enable_if.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
@@ -84,18 +82,9 @@ export function app_code_home(context) {
               let div4 = html_div(div3);
               let span2 = app_replace_button_symbol(div4, d);
               app_replace_button_style(span2);
-              let t = true;
-              html_enable_if(span2, t);
-              let color_else =
-                app_replace_button_symbol_style_background_color_invalid();
               let color_if =
                 app_replace_button_symbol_style_background_color_valid();
-              html_style_background_color_set_if_else(
-                t,
-                span2,
-                color_if,
-                color_else,
-              );
+              html_style_background_color_set(span2, color_if);
               html_style_font_color_set_if(t, span2, "white", "#b9fcb9ff");
               let suffix = app_replace_button_symbol_style_valid_if_attribute();
               html_data_set_test_suffix(span2, suffix, t);
