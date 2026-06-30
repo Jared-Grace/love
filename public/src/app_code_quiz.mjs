5797bdde79f8315823a7963e5e58b6c18ab0bde1
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 09:16:03 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- public/src/app_code_quiz.mjs
+++ public/src/app_code_quiz.mjs
@@ -1,3 +1,4 @@
+import { app_replace_button_screen } from "../../../love/public/src/app_replace_button_screen.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
 import { app_code_lesson_current } from "../../../love/public/src/app_code_lesson_current.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
@@ -5,4 +6,5 @@ export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
+  app_replace_button_screen(context, app_code_quiz, root, combined2);
 }
