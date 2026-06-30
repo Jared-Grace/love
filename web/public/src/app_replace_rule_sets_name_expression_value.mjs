6095aefed56fb1617236bfc9df4bb2bdb1979b15
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 07:42:03 -0400

    function_auto_path ../love/public/src/app_replace_rule_sets_name_expression_value.mjs

--- web/public/src/app_replace_rule_sets_name_expression_value.mjs
+++ web/public/src/app_replace_rule_sets_name_expression_value.mjs
@@ -4,12 +4,12 @@ import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule
 import { app_replace_rule_sets_name_expression } from "../../../love/public/src/app_replace_rule_sets_name_expression.mjs";
 import { null_is } from "../../../love/public/src/null_is.mjs";
 export function app_replace_rule_sets_name_expression_value(e) {
-  let found = app_replace_rule_sets_name_expression(e);
-  if (null_is(found)) {
-    return found;
-  }
   let n = app_replace_rule_sets.name;
   let prefix = text_suffix_without(n, "s");
-  let f_name_new = app_shared_name_text_to_idenitifier(prefix, found);
+  let name = app_replace_rule_sets_name_expression(e);
+  if (null_is(name)) {
+    return name;
+  }
+  let f_name_new = app_shared_name_text_to_idenitifier(prefix, name);
   return f_name_new;
 }
