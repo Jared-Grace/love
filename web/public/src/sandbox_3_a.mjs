19a7f841e4778fa4fd0b2bcbadf2208ef387dbfb
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:12:35 -0400

    function_auto_path ../love/public/src/app_calendar_preaching_ask_entries_previous_difference.mjs

--- web/public/src/sandbox_3_a.mjs
+++ web/public/src/sandbox_3_a.mjs
@@ -1,5 +1,3 @@
 import { app_calendar_preaching_ask_entries_previous_difference } from "../../../love/public/src/app_calendar_preaching_ask_entries_previous_difference.mjs";
 export async function sandbox_3_a() {
-  let joined = await app_calendar_preaching_ask_entries_previous_difference();
-  return joined;
 }
