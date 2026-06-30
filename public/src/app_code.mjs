ab3b110c0ad61a52ed6896e5994cd270b95a766e
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:36:38 -0400

    function_auto_path ../love/public/src/app_code.mjs

--- public/src/app_code.mjs
+++ public/src/app_code.mjs
@@ -1,3 +1,4 @@
+import { app_main_production } from "../../../love/public/src/app_main_production.mjs";
 export async function app_code() {
   await app_main_production("app_code_main", "jared-grace");
 }
