aa9955b1523e923295a59c16e485e675798f7d51
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:37:59 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- public/src/app_code_main.mjs
+++ public/src/app_code_main.mjs
@@ -1,5 +1,6 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 export function app_code_main(context) {
-  let {root} = context;
+  let root = property_get(context, "root");
   html_p_text_multiple(parent, list);
 }
