aadfc5e1825627a988527459d72c82de9b324588
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:49:51 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- public/src/app_code_main.mjs
+++ public/src/app_code_main.mjs
@@ -1,3 +1,4 @@
+import { app_code_screens } from "../../../love/public/src/app_code_screens.mjs";
 import { app_shared_initialize_refresh } from "../../../love/public/src/app_shared_initialize_refresh.mjs";
 import { app_code } from "../../../love/public/src/app_code.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
@@ -6,7 +7,7 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 export async function app_code_main(context) {
   let app_fn = app_code;
-  let screens = [];
+  let screens = app_code_screens();
   await app_shared_initialize_refresh(context, app_fn, screens);
   let root = property_get(context, "root");
   let split = digits();
