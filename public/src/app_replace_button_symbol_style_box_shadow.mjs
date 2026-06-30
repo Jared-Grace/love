ae3cecdf2f932e1a13f367e0455b16451b31ccc1
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:31:38 -0400

    function_auto_path ../love/public/src/app_replace_button_symbol_style_box_shadow.mjs

--- public/src/app_replace_button_symbol_style_box_shadow.mjs
+++ public/src/app_replace_button_symbol_style_box_shadow.mjs
@@ -1,9 +1,6 @@
+import { app_replace_button_symbol_style_box_shadow_value } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value.mjs";
 import { html_style_set_or_remove } from "../../../love/public/src/html_style_set_or_remove.mjs";
 export function app_replace_button_symbol_style_box_shadow(valid, sb, h) {
-  html_style_set_or_remove(
-    valid,
-    sb,
-    "box-shadow",
-    " 0 0 0 " + "0.1em" + " " + h,
-  );
+  let style_value = app_replace_button_symbol_style_box_shadow_value(h);
+  html_style_set_or_remove(valid, sb, "box-shadow", style_value);
 }
