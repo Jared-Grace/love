9825d3e24bd9b5641eba5652aecf1a4945cef84d
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:37:38 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- public/src/app_code_main.mjs
+++ public/src/app_code_main.mjs
@@ -1,1 +1,4 @@
-export function app_code_main() {}
+import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
+export function app_code_main() {
+  html_p_text_multiple(parent, list);
+}
