894df623378c16328a4e3cff1e753e0a044d9ae4
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 09:35:09 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

---
+++ web/public/src/app_code_batch_item_get.mjs
@@ -0,0 +1,19 @@
+import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
+import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
+import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
+import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
+export function app_code_batch_item_get(batches, container, on_batch_item) {
+  let refresh = function lambda() {
+    let e = list_empty_is(batches);
+    if (e) {
+      list_replace_all(list, items);
+      list_shuffle(batches);
+    }
+    html_clear(container);
+    let r = list_remove_last_single(batches);
+    on_batch_item(r);
+  };
+  refresh();
+  return refresh;
+}
