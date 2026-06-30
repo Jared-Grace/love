c49b0b942f756f3cd3eaefe49798e90d8416e0bd
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 07:39:57 -0400

    function_auto_path ../love/public/src/app_replace_rule_sets_name.mjs

--- public/src/app_replace_rule_sets_name.mjs
+++ public/src/app_replace_rule_sets_name.mjs
@@ -1,20 +1,9 @@
-import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { app_shared_name_text_to_idenitifier } from "../../../love/public/src/app_shared_name_text_to_idenitifier.mjs";
 import { text_suffix_without } from "../../../love/public/src/text_suffix_without.mjs";
 import { app_replace_rule_sets } from "../../../love/public/src/app_replace_rule_sets.mjs";
-import { text_replace_space_underscore_lower } from "../../../love/public/src/text_replace_space_underscore_lower.mjs";
-import { js_literal_value_get } from "../../../love/public/src/js_literal_value_get.mjs";
-import { js_property_value_get } from "../../../love/public/src/js_property_value_get.mjs";
 export function app_replace_rule_sets_name(found) {
   let n = app_replace_rule_sets.name;
   let prefix = text_suffix_without(n, "s");
-  let f_name_new = app_shared_name_prefixed(found, prefix);
+  let f_name_new = app_shared_name_text_to_idenitifier(found, prefix);
   return f_name_new;
 }
-function app_shared_name_prefixed(found, prefix) {
-  let p = js_property_value_get(found);
-  let l = js_literal_value_get(p);
-  let rl = text_replace_space_underscore_lower(l);
-  let f_name_new = function_name_combine(prefix, rl);
-  return f_name_new;
-}
-
