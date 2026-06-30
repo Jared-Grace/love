1352f5d3f418cf7c2741db07652fd0117930b673
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:45:19 -0400

    function_auto_path ../love/public/src/app_code_main.mjs

--- web/public/src/app_code_main.mjs
+++ web/public/src/app_code_main.mjs
@@ -1,9 +1,12 @@
+import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_p_text_multiple } from "../../../love/public/src/html_p_text_multiple.mjs";
 export function app_code_main(context) {
   let root = property_get(context, "root");
   let split = digits();
+  function lambda2() {}
+  let b = app_replace_button(parent, text, lambda2);
   hbt;
   return;
   html_p_text_multiple(root, [
