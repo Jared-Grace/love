3f62a0ea253ce2319a15fc59c72139f927120cbd
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 07:39:05 -0400

    o app_shared_name_prefixed

--- web/public/src/app_replace_rule_sets_name.mjs
+++ web/public/src/app_replace_rule_sets_name.mjs
@@ -7,9 +7,14 @@ import { js_property_value_get } from "../../../love/public/src/js_property_valu
 export function app_replace_rule_sets_name(found) {
   let n = app_replace_rule_sets.name;
   let prefix = text_suffix_without(n, "s");
+  let f_name_new = app_shared_name_prefixed(found, prefix);
+  return f_name_new;
+}
+function app_shared_name_prefixed(found, prefix) {
   let p = js_property_value_get(found);
   let l = js_literal_value_get(p);
   let rl = text_replace_space_underscore_lower(l);
   let f_name_new = function_name_combine(prefix, rl);
   return f_name_new;
 }
+
