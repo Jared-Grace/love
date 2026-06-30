df5ff5ee08504f866a9cbec7910659e2c3d4e58b
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 03:30:45 -0400

    function_auto_path ../love/public/src/html_font_color_set.mjs

--- web/public/src/html_font_color_set.mjs
+++ web/public/src/html_font_color_set.mjs
@@ -1,4 +1,6 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
-export function html_font_color_set(component, color) {$aa
+export function html_font_color_set(component, color) {
+  arguments_assert(arguments, 2);
   html_style_set(component, "color", color);
 }
