f0945a9591b62c89ebdbf82a8050e44f1906c272
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:15:11 -0400

    function_auto_path ../love/public/src/app_code_container_generic.mjs

--- web/public/src/app_code_container_generic.mjs
+++ web/public/src/app_code_container_generic.mjs
@@ -3,10 +3,10 @@ import { html_border_radius_em } from "../../../love/public/src/html_border_radi
 import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_code_container_generic(parent, color_background_set) {
-  let div3 = html_div(parent);
-  color_background_set(div3);
-  html_style_padding(div3, "0.2em");
-  html_border_radius_em(div3, 0.75);
-  html_border_none(div3);
-  return div3;
+  let container = html_div(parent);
+  color_background_set(container);
+  html_style_padding(container, "0.2em");
+  html_border_radius_em(container, 0.75);
+  html_border_none(container);
+  return container;
 }
