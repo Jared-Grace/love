4335d97494c89bcc20e3af47ba3fa63b4d773042
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:56:27 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- public/src/app_code_home.mjs
+++ public/src/app_code_home.mjs
@@ -29,6 +29,7 @@ import { html_clear_context } from "../../../love/public/src/html_clear_context.
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 export function app_code_home(context) {
   let root = html_clear_context(context);
+  let rule_sets = app_code_lessons();
   let value = storage_local_get_context(context, "lesson_index");
   let p3 = html_p(root);
   let ds = digits();
