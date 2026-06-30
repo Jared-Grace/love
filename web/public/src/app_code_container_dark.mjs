e3a1a376a32e6126fe7b3916431a7799b86d3aa0
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 04:18:20 -0400

    function_auto_path ../love/public/src/app_code_container_dark.mjs

--- web/public/src/app_code_container_dark.mjs
+++ web/public/src/app_code_container_dark.mjs
@@ -1,8 +1,7 @@
 import { app_code_container_generic } from "../../../love/public/src/app_code_container_generic.mjs";
 import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
 export function app_code_container_dark(parent) {
-  let color_background_set = html_style_background_color_black;
   let container = app_code_container_generic(parent);
-  html_style_background_color_black(parent_new);
+  html_style_background_color_black(container);
   return container;
 }
