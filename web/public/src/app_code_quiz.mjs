ab506a1c4652e45324934bfba2c267c42582124c
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 09:18:59 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -1,4 +1,5 @@
-import { app_code_example_text } from "../../../love/public/src/app_code_example_text.mjs";
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
+import { emoji_repeat_1 } from "../../../love/public/src/emoji_repeat_1.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
 import { app_replace_button_screen } from "../../../love/public/src/app_replace_button_screen.mjs";
@@ -10,6 +11,7 @@ export function app_code_quiz(context) {
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
   html_div_text(root, "Do you want go back and see another example?");
-  let combined = app_code_example_text();
+  let left = emoji_repeat_1();
+  let combined = text_combine(left, "Yes, show me another example");
   app_replace_button_screen(context, app_code_home, root, combined);
 }
