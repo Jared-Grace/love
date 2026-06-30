153f8a58b49850d66121e74dcd9496d70ee66a51
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 01:10:25 -0400

    function_auto_path ../love/public/src/text_starts_with_curried_right.mjs

--- web/public/src/text_starts_with_curried_right.mjs
+++ web/public/src/text_starts_with_curried_right.mjs
@@ -1,8 +1,9 @@
+import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { text_is_assert } from "../../../love/public/src/text_is_assert.mjs";
 import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs";
 export function text_starts_with_curried_right(f_name_prefix) {
   "asserts in this fn are to make sure " +
-    function_name("functions_rename_generic_starts_with") +
+    fn_name("functions_rename_generic_starts_with") +
     " is ran correctly";
   text_is_assert(f_name_prefix);
   let r = function text_starts_with_curry_right_result(f_name) {
