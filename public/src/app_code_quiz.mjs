ff1c31854ac10038f098176cdc970e8b64cdb361
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 09:28:50 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- public/src/app_code_quiz.mjs
+++ public/src/app_code_quiz.mjs
@@ -14,6 +14,7 @@ export function app_code_quiz(context) {
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
   let quizzes = property_get(lesson, "quizzes");
   let item = list_get(quizzes, quiz_index);
+  item(root);
   html_div_text(root, "Do you want go back and see another example?");
   let left = emoji_arrow_left();
   let combined = text_combine(
