5a92c044ba96f00c738d2faf07314bef831a9d7e
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 06:38:17 -0400

    function_auto_path ../love/public/src/list_beetween.mjs

--- web/public/src/list_beetween.mjs
+++ web/public/src/list_beetween.mjs
@@ -2,14 +2,14 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
-export function list_beetween(ds, between) {
-  let taken = list_take_less_1(ds);
+export function list_beetween(list, item_between) {
+  let taken = list_take_less_1(list);
   function lambda(d) {
-    let r = [d, between];
+    let r = [d, item_between];
     return r;
   }
   let mapped = list_map(taken, lambda);
-  let last = list_last(ds);
+  let last = list_last(list);
   list_add(mapped, last);
   return mapped;
 }
