fd83b2673bcbb26541f00d0dfa1b75cd7b6e548f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 02:16:39 -0400

    function_auto_path ../love/public/src/html_border_top.mjs

--- web/public/src/html_border_top.mjs
+++ web/public/src/html_border_top.mjs
@@ -3,5 +3,6 @@ export function html_border_top(component, border_width, border_color) {
   html_style_assign(component, {
     "border-top-width": border_width,
     "border-top-color": border_color,
+    "border-style": "solid",
   });
 }
