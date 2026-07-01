38256937dc4f6789500e68b79af2c0a5d019197c
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:47:09 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- public/src/app_code_lesson_symbols_digits.mjs
+++ public/src/app_code_lesson_symbols_digits.mjs
@@ -103,7 +103,9 @@ export function app_code_lesson_symbols_digits() {
               storage_local_set_context(context, "quiz_index", index_new);
               let li = list_index_last_is(quizzes, index);
               if (li) {
-                function lambda7() {}
+                function lambda7(lesson_id) {
+                  let a2 = add_1(n);
+                }
                 storage_local_transform_context(context, "lesson_id", lambda7);
                 await app_shared_screen_set(context, app_code_home);
               } else {
