04ffd1486147d2b393f3bc88b624f6a114790abe
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:08:29 -0400

    function_auto_path ../love/public/src/html_visibility_visible_multiple.mjs

--- public/src/html_visibility_visible_multiple.mjs
+++ public/src/html_visibility_visible_multiple.mjs
@@ -1,5 +1,8 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { html_visibility_visible } from "../../../love/public/src/html_visibility_visible.mjs";
 export function html_visibility_visible_multiple(on_success) {
+  function lambda(item) {}
+  each(list, lambda);
   let r = html_visibility_visible(on_success);
   return r;
 }
