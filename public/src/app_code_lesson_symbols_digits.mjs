439e3c925047bbd0a74dd6d8a791835e795a818e
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 01:43:26 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- public/src/app_code_lesson_symbols_digits.mjs
+++ public/src/app_code_lesson_symbols_digits.mjs
@@ -1,7 +1,5 @@
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { html_display_block } from "../../../love/public/src/html_display_block.mjs";
-import { html_display_none } from "../../../love/public/src/html_display_none.mjs";
-import { app_replace_success_message } from "../../../love/public/src/app_replace_success_message.mjs";
 import { app_g_button_wrong_generic } from "../../../love/public/src/app_g_button_wrong_generic.mjs";
 import { list_sort_text_to } from "../../../love/public/src/list_sort_text_to.mjs";
 import { app_shared_button_screen_green_style_assign } from "../../../love/public/src/app_shared_button_screen_green_style_assign.mjs";
@@ -88,8 +86,6 @@ export function app_code_lesson_symbols_digits() {
               let taken = list_take(answers, answer_count_max - 1);
               let concated = list_concat(taken, [digit_count]);
               list_sort_text_to(concated);
-              let success = app_replace_success_message(parent);
-              html_display_none(success);
               let buttons = list_map(concated, lambda3);
               function lambda3(answer) {
                 let b2 = app_replace_button_wide(
