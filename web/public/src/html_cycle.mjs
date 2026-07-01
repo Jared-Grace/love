4a917c06d38a39a3a915fa847eefdf4586513807
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 06:10:19 -0400

    function_auto_path ../love/public/src/html_cycle_bold.mjs

---
+++ web/public/src/html_cycle.mjs
@@ -0,0 +1,11 @@
+import { each_index } from "../../../love/public/src/each_index.mjs";
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+import { list_get_wrap } from "../../../love/public/src/list_get_wrap.mjs";
+export function html_cycle(cycles, parent, parts) {
+  function lambda(part, index) {
+    let item = list_get_wrap(cycles, index);
+    let span = html_span_text(parent, part);
+    item(span);
+  }
+  each_index(parts, lambda);
+}
