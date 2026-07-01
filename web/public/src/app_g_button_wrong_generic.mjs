cff15402e459b44eaafd4f9d2a96bf6ccb128e28
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 00:19:50 -0400

    function_auto_path ../love/public/src/app_g_button_wrong_generic.mjs

--- web/public/src/app_g_button_wrong_generic.mjs
+++ web/public/src/app_g_button_wrong_generic.mjs
@@ -1,8 +1,8 @@
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { text_take } from "../../../love/public/src/text_take.mjs";
 import { html_border_invalid_color } from "../../../love/public/src/html_border_invalid_color.mjs";
-export function app_g_button_wrong_generic(b, transparency_alpha_channel_hex) {
+export function app_g_button_wrong_generic(b, alpha_channel) {
   let v = html_border_invalid_color();
-  let t = text_take(v, 7) + transparency_alpha_channel_hex;
+  let t = text_take(v, 7) + alpha_channel;
   html_style_background_color_set(b, t);
 }
