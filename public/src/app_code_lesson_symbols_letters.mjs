cb002405125a7bd2c31c2721cfa20b0da556b5e3
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 07:45:16 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- public/src/app_code_lesson_symbols_letters.mjs
+++ public/src/app_code_lesson_symbols_letters.mjs
@@ -1,7 +1,6 @@
-import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
+import { list_alphabet_lower } from "../../../love/public/src/list_alphabet_lower.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { html_cycle_mono_list_between_comma_space_before_after } from "../../../love/public/src/html_cycle_mono_list_between_comma_space_before_after.mjs";
-import { text_alphabet_lower } from "../../../love/public/src/text_alphabet_lower.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
@@ -9,8 +8,7 @@ export function app_code_lesson_symbols_letters() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
     let div = html_div(c);
-    let alphabet_lower_unsplit = text_alphabet_lower();
-    let alphabet_lower = text_split_empty(alphabet_lower_unsplit);
+    let alphabet_lower = list_alphabet_lower();
     html_cycle_mono_list_between_comma_space_before_after(
       div,
       "In English, there are 26 letters of the alphabet: ",
