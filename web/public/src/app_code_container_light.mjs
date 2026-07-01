fe6fa31257b091775218fa87df7f3aacb9ff84b7
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 00:02:35 -0400

    function_auto_path ../love/public/src/app_code_container_light.mjs

--- web/public/src/app_code_container_light.mjs
+++ web/public/src/app_code_container_light.mjs
@@ -5,11 +5,11 @@ import { app_code_container_generic } from "../../../love/public/src/app_code_co
 export function app_code_container_light(parent) {
   let container = app_code_container_generic(parent);
   html_style_background_color_set(container, "#eee");
+  html_style_margin_y(container, 0.3 + "em");
   app_replace_button_symbol_style_box_shadow_value_width_set(
     container,
     "#ddd",
     "0.05",
   );
-  html_style_margin_y(container, 0.3 + "em");
   return container;
 }
