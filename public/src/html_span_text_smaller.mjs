73ec4f0009e21d18d3da539d7f3029392d61a56f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 08:04:28 -0400

    function_auto_path ../love/public/src/html_span_text_smaller.mjs

--- public/src/html_span_text_smaller.mjs
+++ public/src/html_span_text_smaller.mjs
@@ -1,7 +1,7 @@
 import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function html_span_text_smaller(parent, text) {
-  let div4 = html_span_text(parent, text);
-  html_style_font_size(div4, "0.75em");
-  return div4;
+  let s = html_span_text(parent, text);
+  html_style_font_size(s, "0.75em");
+  return s;
 }
