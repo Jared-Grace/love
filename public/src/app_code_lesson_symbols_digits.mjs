1bcd5d1eaf7c54731630decbd957fe9ca1858c8e
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 01:57:23 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- public/src/app_code_lesson_symbols_digits.mjs
+++ public/src/app_code_lesson_symbols_digits.mjs
@@ -78,7 +78,7 @@ export function app_code_lesson_symbols_digits() {
           },
           answer: digit_count,
           quizzes: [
-            function lambda2(context, parent) {
+            function lambda2(parent) {
               let a = example_above(parent, digits);
               let container_answer2 = property_get(a, "container_answer");
               html_text_set(container_answer2, "How many symbols are there? ");
@@ -108,7 +108,7 @@ export function app_code_lesson_symbols_digits() {
                     }
                     app_code_next(
                       context,
-                      parent,
+                      root,
                       "practice more with another quiz",
                       "take another quiz",
                       lambda5,
