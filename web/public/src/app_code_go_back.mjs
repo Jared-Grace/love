e2f4c8f067d31f14278942b2996bf2e85e3be8cc
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 04:09:29 -0400

    function_auto_path ../love/public/src/app_code_go_back.mjs

--- web/public/src/app_code_go_back.mjs
+++ web/public/src/app_code_go_back.mjs
@@ -1,5 +1,6 @@
+import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
+import { app_replace_button_screen } from "../../../love/public/src/app_replace_button_screen.mjs";
 import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
-import { app_replace_button_screen_wide } from "../../../love/public/src/app_replace_button_screen_wide.mjs";
 import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
@@ -14,5 +15,11 @@ export function app_code_go_back(context, root, question_text, button_text) {
   html_div_text(container3, combined2);
   let left = emoji_arrow_left();
   let combined = text_combine_multiple([left, "Yes, please ", button_text]);
-  app_replace_button_screen_wide(context, app_code_home, container3, combined);
+  let b = app_replace_button_screen(
+    context,
+    app_code_home,
+    container3,
+    combined,
+  );
+  html_width_full(b);
 }
