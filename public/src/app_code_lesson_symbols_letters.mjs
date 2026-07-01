0c453b5c4001dcb2804cc311eeaf0b65ce5d438f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 08:16:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- public/src/app_code_lesson_symbols_letters.mjs
+++ public/src/app_code_lesson_symbols_letters.mjs
@@ -1,13 +1,6 @@
+import { html_text_characters_numbered } from "../../../love/public/src/html_text_characters_numbered.mjs";
 import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
-import { list_squash } from "../../../love/public/src/list_squash.mjs";
-import { list_between_comma_space } from "../../../love/public/src/list_between_comma_space.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
-import { html_display_set } from "../../../love/public/src/html_display_set.mjs";
-import { incrementer } from "../../../love/public/src/incrementer.mjs";
-import { html_span_text_smaller } from "../../../love/public/src/html_span_text_smaller.mjs";
-import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
-import { html_flex_column_center } from "../../../love/public/src/html_flex_column_center.mjs";
 import { list_alphabet_lower } from "../../../love/public/src/list_alphabet_lower.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
@@ -23,20 +16,7 @@ export function app_code_lesson_symbols_letters() {
     );
     let div2 = html_div(c);
     let alphabet_lower = list_alphabet_lower();
-    let betweened = list_between_comma_space(alphabet_lower);
-    let squashed = list_squash(betweened);
-    let i = incrementer();
-    let cycles = [
-      function lambda2(span) {
-        html_flex_column_center(span);
-        html_display_set(span, "inline-flex");
-        let text = i();
-        let s = html_span_text_smaller(span, text);
-        html_font_color_set(s, "rgb(0, 110, 221)");
-      },
-      noop,
-    ];
-    html_cycle(div2, cycles, squashed);
+    html_text_characters_numbered(alphabet_lower, div2);
     let div3 = html_div(c);
     html_cycle_bold(div3, [
       "Those 26 letters are written in ",
