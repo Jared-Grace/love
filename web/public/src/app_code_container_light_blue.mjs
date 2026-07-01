97ffd500fc7e3814c335e6e10d202e4a03372b50
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 02:18:32 -0400

    function_auto_path ../love/public/src/app_code_container_light_blue.mjs

--- web/public/src/app_code_container_light_blue.mjs
+++ web/public/src/app_code_container_light_blue.mjs
@@ -1,6 +1,5 @@
-import { html_border_bottom } from "../../../love/public/src/html_border_bottom.mjs";
+import { html_border_y } from "../../../love/public/src/html_border_y.mjs";
 import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
-import { html_border_top } from "../../../love/public/src/html_border_top.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_style_padding } from "../../../love/public/src/html_style_padding.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
@@ -11,8 +10,9 @@ export function app_code_container_light_blue(parent) {
   html_border_none(container2);
   let container = container2;
   html_style_background_color_set(container, "rgb(209, 231, 253)");
-  html_border_top(container, "0.15em", "black");
-  html_border_bottom(component, border_width, border_color);
+  const border_width = "0.15em";
+  const border_color = "black";
+  html_border_y(container, border_width, border_color);
   html_style_margin_y(container, "0.5em");
   return container;
 }
---
+++ web/public/src/html_border_y.mjs
@@ -0,0 +1,6 @@
+import { html_border_bottom } from "../../../love/public/src/html_border_bottom.mjs";
+import { html_border_top } from "../../../love/public/src/html_border_top.mjs";
+export function html_border_y(container, border_width, border_color) {
+  html_border_top(container, border_width, border_color);
+  html_border_bottom(component, border_width, border_color);
+}
