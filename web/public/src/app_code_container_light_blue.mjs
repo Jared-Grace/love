885f902602002e23332dd6d2469258ad510cefcc
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 02:09:25 -0400

    function_auto_path ../love/public/src/app_code_container_light_blue.mjs

--- web/public/src/app_code_container_light_blue.mjs
+++ web/public/src/app_code_container_light_blue.mjs
@@ -2,6 +2,6 @@ import { html_style_background_color_set } from "../../../love/public/src/html_s
 import { app_code_container_generic } from "../../../love/public/src/app_code_container_generic.mjs";
 export function app_code_container_light_blue(parent) {
   let container = app_code_container_generic(parent);
-  html_style_background_color_set(container, "rgb(203, 213, 255)");
+  html_style_background_color_set(container, "rgb(209, 211, 253)");
   return container;
 }
