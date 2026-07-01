21c11b469a59e9a7f2f8677c157c42b4efadeb32
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 07:37:55 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- web/public/src/app_code_lesson_symbols_letters.mjs
+++ web/public/src/app_code_lesson_symbols_letters.mjs
@@ -1,3 +1,4 @@
+import { text_alphabet_lower } from "../../../love/public/src/text_alphabet_lower.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
@@ -5,6 +6,7 @@ import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src
 export function app_code_lesson_symbols_letters() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
+    let r = text_alphabet_lower();
     let div = html_div_text(
       c,
       "In English, there are 26 letters of the alphabet: A through Z",
