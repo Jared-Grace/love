b3b194043fda8969ce007bdf30533c06d0bceeca
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:57:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered.mjs

--- web/public/src/app_code_lesson_symbols_digits_numbered.mjs
+++ web/public/src/app_code_lesson_symbols_digits_numbered.mjs
@@ -6,9 +6,4 @@ import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function app_code_lesson_symbols_digits_numbered() {
   let r5 = app_code_lesson_symbols_digits_generic(noop);
   return r5;
-  function on_symbol(parent, index_1) {
-    let div4 = html_span_text(parent, index_1);
-    html_style_font_size(div4, "0.75em");
-    html_font_color_set(div4, "#bbb");
-  }
 }
