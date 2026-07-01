74c5deac73a57fc988d112789bd5959d6f8d95d8
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 06:39:01 -0400

    function_auto_path ../love/public/src/list_between.mjs

--- web/public/src/list_between.mjs
+++ web/public/src/list_between.mjs
@@ -2,7 +2,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
-export function list_beetween(list, item_between) {
+export function list_between(list, item_between) {
   let taken = list_take_less_1(list);
   function lambda(d) {
     let r = [d, item_between];
