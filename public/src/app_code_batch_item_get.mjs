a61035afe76582ce8a5d726643ac3870a11361d0
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:02:59 -0400

    function_auto_path ../love/public/src/app_code_batch_item_get.mjs

--- public/src/app_code_batch_item_get.mjs
+++ public/src/app_code_batch_item_get.mjs
@@ -1,6 +1,6 @@
-import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
 import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
@@ -8,7 +8,7 @@ import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 export function app_code_batch_item_get(parent, lesson, on_batch_item) {
   let batch = property_get(lesson, "batch");
   let remaining = [];
-  let container = app_code_container_light_blue(parent);
+  let container = html_div(parent);
   let refresh = function lambda() {
     let e = list_empty_is(remaining);
     if (e) {
