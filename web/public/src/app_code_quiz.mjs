5fe3fda41c778c6f7103a6e006b1990cdd44650d
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 02:56:35 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -14,7 +14,8 @@ export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
-  let refresh = app_code_batch_item_get(container2, lesson, on_batch_item);
+  let container2 = app_code_container_light_blue(root);
+  let refresh = app_code_batch_item_get(root, lesson, on_batch_item);
   function on_batch_item(container, b, refresh) {
     let qs = property_get(b, "quizzes");
     let q = list_get(qs, quiz_index);
