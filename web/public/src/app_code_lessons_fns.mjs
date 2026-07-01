6f6006f0f2cfcf89a1db17ff6b486a0800907b7e
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:56:44 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered.mjs

--- web/public/src/app_code_lessons_fns.mjs
+++ web/public/src/app_code_lessons_fns.mjs
@@ -1,5 +1,5 @@
-import { app_code_lesson_symbols_digits } from "../../../love/public/src/app_code_lesson_symbols_digits.mjs";
+import { app_code_lesson_symbols_digits_numbered } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered.mjs";
 export function app_code_lessons_fns() {
-  let r = [app_code_lesson_symbols_digits];
+  let r = [app_code_lesson_symbols_digits_numbered];
   return r;
 }
