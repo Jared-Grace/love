0ba1418087807b14eeddb2cddfa2b99b3dd4566b
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 07:38:29 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- public/src/app_code_lesson_symbols_letters.mjs
+++ public/src/app_code_lesson_symbols_letters.mjs
@@ -1,3 +1,4 @@
+import { html_cycle_mono_list_between_comma_space_before_after } from "../../../love/public/src/html_cycle_mono_list_between_comma_space_before_after.mjs";
 import { text_alphabet_lower } from "../../../love/public/src/text_alphabet_lower.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
@@ -7,13 +8,12 @@ export function app_code_lesson_symbols_letters() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
     let r = text_alphabet_lower();
-
-        html_cycle_mono_list_between_comma_space_before_after(
-          p3,
-          "The numbers ",
-          ds,
-          " are called ",
-        );
+    html_cycle_mono_list_between_comma_space_before_after(
+      p3,
+      "The numbers ",
+      ds,
+      " are called ",
+    );
     let div = html_div_text(
       c,
       "In English, there are 26 letters of the alphabet: A through Z",
