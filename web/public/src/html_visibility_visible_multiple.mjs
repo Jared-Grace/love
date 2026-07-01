19cbb2d2e1757d63b4d3a76c376e9629f1741f67
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:08:17 -0400

    w c html_visibility_visible,multiple

---
+++ web/public/src/html_visibility_visible_multiple.mjs
@@ -0,0 +1,4 @@
+import { html_visibility_visible } from "../../../love/public/src/html_visibility_visible.mjs";
+export function html_visibility_visible_multiple(on_success) {
+  return html_visibility_visible(on_success);
+}
