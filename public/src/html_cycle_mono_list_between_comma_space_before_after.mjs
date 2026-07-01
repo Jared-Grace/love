1f6d50aee9fccd19985fee9ee252868b6040b293
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 07:31:58 -0400

    pf html_cycle_mono_list_between_comma_space_before_after parent

--- public/src/html_cycle_mono_list_between_comma_space_before_after.mjs
+++ public/src/html_cycle_mono_list_between_comma_space_before_after.mjs
@@ -1,10 +1,10 @@
 import { html_cycle_mono } from "../../../love/public/src/html_cycle_mono.mjs";
 import { list_between_comma_space_before_after } from "../../../love/public/src/list_between_comma_space_before_after.mjs";
 export function html_cycle_mono_list_between_comma_space_before_after(
+  parent,
   ds,
   before,
   after,
-  parent,
 ) {
   let squashed = list_between_comma_space_before_after(ds, before, after);
   html_cycle_mono(parent, squashed);
