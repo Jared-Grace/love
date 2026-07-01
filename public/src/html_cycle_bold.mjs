1b01e7d484b3c5f7d85d96c48bd9bdf23850b6ab
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 06:10:30 -0400

    pf html_cycle parent

--- public/src/html_cycle_bold.mjs
+++ public/src/html_cycle_bold.mjs
@@ -3,5 +3,5 @@ import { noop } from "../../../love/public/src/noop.mjs";
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
 export function html_cycle_bold(parent, parts) {
   let cycles = [noop, html_bold];
-  html_cycle(cycles, parent, parts);
+  html_cycle(parent, cycles, parts);
 }
