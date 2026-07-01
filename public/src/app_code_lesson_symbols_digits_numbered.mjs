ddff50c9f9a614ef1b756892849416a0c7730ba3
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 06:30:17 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered.mjs

--- public/src/app_code_lesson_symbols_digits_numbered.mjs
+++ public/src/app_code_lesson_symbols_digits_numbered.mjs
@@ -1,3 +1,4 @@
+import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
@@ -38,7 +39,8 @@ export function app_code_lesson_symbols_digits_numbered() {
       return r;
     }
     let mapped = list_map(taken, lambda);
-    list_add(list, item);
+    let last = list_last(list);
+    list_add(mapped, item);
     let joined = list_join_comma_space(ds);
     let combined2 = text_combine_multiple([
       "The numbers ",
