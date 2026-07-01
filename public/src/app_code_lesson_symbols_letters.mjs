6400e42e06bbd83357228274bfcbf7b5d3488901
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 07:36:03 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- public/src/app_code_lesson_symbols_letters.mjs
+++ public/src/app_code_lesson_symbols_letters.mjs
@@ -1,15 +1,7 @@
-import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
-import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
 export function app_code_lesson_symbols_letters() {
-  function lambda(root) {
-    let c = app_code_container_light_blue(root);
-    let div = html_div_text(
-      c,
-      "This lesson is the same as the previous lesson, except there are not numbers underneath",
-    );
-  }
+  function lambda(root) {}
   let r5 = app_code_lesson_symbols_digits_generic(
     "Symbols (Letters)",
     "symbols_letters",
