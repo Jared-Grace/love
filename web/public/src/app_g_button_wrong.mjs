de00f9f6d8843f5c00b6e708510b3c3c698628d9
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 23:58:06 -0400

    function_auto_path ../love/public/src/app_g_button_wrong.mjs

--- web/public/src/app_g_button_wrong.mjs
+++ web/public/src/app_g_button_wrong.mjs
@@ -1,14 +1,5 @@
-import { app_shared_button_font_color } from "../../../love/public/src/app_shared_button_font_color.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
-import { text_take } from "../../../love/public/src/text_take.mjs";
-import { html_border_invalid_color } from "../../../love/public/src/html_border_invalid_color.mjs";
+import { app_g_button_wrong_generic } from "../../../love/public/src/app_g_button_wrong_generic.mjs";
 export function app_g_button_wrong(b) {
   const transparency_alpha_channel_hex = "cd";
-  let v = html_border_invalid_color();
-  let t = text_take(v, 7) + transparency_alpha_channel_hex;
-  html_style_background_color_set(b, t);
-  html_style_assign(b, {
-    color: app_shared_button_font_color(),
-  });
+  app_g_button_wrong_generic(transparency_alpha_channel_hex, b);
 }
