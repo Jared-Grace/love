f273eef7eda20fc70c92d426580b9e7d2d7822df
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 04:02:58 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered.mjs

--- web/public/src/app_code_lesson_symbols_digits_numbered.mjs
+++ web/public/src/app_code_lesson_symbols_digits_numbered.mjs
@@ -1,3 +1,11 @@
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
+import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
+import { digits } from "../../../love/public/src/digits.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
@@ -7,8 +15,35 @@ export function app_code_lesson_symbols_digits_numbered() {
     "Symbols (Digits, numbered)",
     "symbols_digits_numbered",
     on_symbol,
+    above,
   );
   return r5;
+  function above(root) {
+    let c = app_code_container_light_blue(root);
+    html_style_assign(c, {
+      display: "flex",
+      flexDirection: "column",
+      gap: "0.8em",
+    });
+    let p3 = html_div(c);
+    let ds = digits();
+    let joined = list_join_comma_space(ds);
+    let combined2 = text_combine_multiple([
+      "The numbers ",
+      joined,
+      " are called ",
+    ]);
+    html_cycle_bold(p3, [combined2, "digits"]);
+    let p2 = html_div(c);
+    html_cycle_bold(p2, [
+      "In a number, the digits (0, 1, 2, ..., 9) are examples of ",
+      "symbols",
+    ]);
+    let p = html_div_text(
+      c,
+      "When we write computer programs, we use symbols, including numbers",
+    );
+  }
   function on_symbol(parent, index_1) {
     let div4 = html_span_text(parent, index_1);
     html_style_font_size(div4, "0.75em");
