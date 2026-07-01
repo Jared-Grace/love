d4fe7c440426b89262740447b427a3fd08e49e5f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 06:10:19 -0400

    function_auto_path ../love/public/src/html_cycle_bold.mjs

--- web/public/src/html_cycle_bold.mjs
+++ web/public/src/html_cycle_bold.mjs
@@ -1,14 +1,7 @@
-import { list_get_wrap } from "../../../love/public/src/list_get_wrap.mjs";
-import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { each_index } from "../../../love/public/src/each_index.mjs";
+import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
-import { html_bold } from "./html_bold.mjs";
+import { html_bold } from "../../../love/public/src/html_bold.mjs";
 export function html_cycle_bold(parent, parts) {
   let cycles = [noop, html_bold];
-  function lambda(part, index) {
-    let item = list_get_wrap(cycles, index);
-    let span = html_span_text(parent, part);
-    item(span);
-  }
-  each_index(parts, lambda);
+  html_cycle(cycles, parent, parts);
 }
