d96e1299240fcc2286d4a1cea5045ba912ade287
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:58:02 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- web/public/src/app_code_lesson_symbols_digits.mjs
+++ web/public/src/app_code_lesson_symbols_digits.mjs
@@ -1,13 +1,6 @@
+import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
-import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
-import { html_style_font_size } from "../../../love/public/src/html_style_font_size.mjs";
-import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function app_code_lesson_symbols_digits() {
-  let r5 = app_code_lesson_symbols_digits_generic(on_symbol);
+  let r5 = app_code_lesson_symbols_digits_generic(noop);
   return r5;
-  function on_symbol(parent, index_1) {
-    let div4 = html_span_text(parent, index_1);
-    html_style_font_size(div4, "0.75em");
-    html_font_color_set(div4, "#bbb");
-  }
 }
