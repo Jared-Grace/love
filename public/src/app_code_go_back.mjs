aaacc2f813e59bd8732e250a92d6e4d98c33dbb1
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 04:07:24 -0400

    pf app_code_go_back context

--- public/src/app_code_go_back.mjs
+++ public/src/app_code_go_back.mjs
@@ -4,7 +4,7 @@ import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs"
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
-export function app_code_go_back(root, question_text, button_text, context) {
+export function app_code_go_back(context, root, question_text, button_text) {
   let container3 = app_code_container_light_blue(root);
   let combined2 = text_combine_multiple([
     "Do you want to go back and ",
