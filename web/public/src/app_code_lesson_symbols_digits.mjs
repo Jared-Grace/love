b279109b0e1e592da47c2bcc0be6ffeedacfe125
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:59:57 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- web/public/src/app_code_lesson_symbols_digits.mjs
+++ web/public/src/app_code_lesson_symbols_digits.mjs
@@ -1,6 +1,10 @@
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
 export function app_code_lesson_symbols_digits() {
-  let r5 = app_code_lesson_symbols_digits_generic(noop);
+  let r5 = app_code_lesson_symbols_digits_generic(
+    "Symbols (Digits)",
+    "symbols_digits",
+    noop,
+  );
   return r5;
 }
