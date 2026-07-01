45395be6ad0f4909d2d6f62870d0d5b6c5a418ce
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 23:25:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- public/src/app_code_lesson_symbols_digits.mjs
+++ public/src/app_code_lesson_symbols_digits.mjs
@@ -28,6 +28,7 @@ import { text_combine_multiple } from "../../../love/public/src/text_combine_mul
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
 import { html_p } from "../../../love/public/src/html_p.mjs";
+import { ternary } from "../../../love/public/src/ternary.mjs";
 export function app_code_lesson_symbols_digits() {
   const answer_count_max = 4;
   let r3 = {
@@ -85,8 +86,7 @@ export function app_code_lesson_symbols_digits() {
               function lambda3(answer) {
                 function lambda8() {
                   let eq2 = equal(answer, digit_count);
-                  if (false) {
-                  }
+                  let result = ternary(condition, on_true, on_false);
                 }
                 let b2 = app_replace_button_wide(
                   container_answer2,
