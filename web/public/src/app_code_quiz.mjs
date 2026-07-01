c74d39301be0afa157ae9526431a0c7f7fcda94b
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 22:07:24 -0400

    pndf app_code_batch_item_get parent

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -14,7 +14,7 @@ export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
-  let refresh = app_code_batch_item_get(lesson, on_batch_item);
+  let refresh = app_code_batch_item_get(parent, lesson, on_batch_item);
   function on_batch_item(container, b) {
     let qs = property_get(b, "quizzes");
     let q = list_get(qs, quiz_index);
