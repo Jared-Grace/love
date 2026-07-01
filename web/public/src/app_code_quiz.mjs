50c1d987485270df8be5e3ee38e8962e6857170c
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 01:45:01 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -1,6 +1,5 @@
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { html_display_none } from "../../../love/public/src/html_display_none.mjs";
-import { app_replace_success_message } from "../../../love/public/src/app_replace_success_message.mjs";
 import { app_code_batch_item_get } from "../../../love/public/src/app_code_batch_item_get.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -17,16 +16,14 @@ export function app_code_quiz(context) {
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
   let refresh = app_code_batch_item_get(root, lesson, on_batch_item);
-  let div = html_div(parent);
-  let success = null;
+  let success = html_div(parent);
+  html_display_none(success);
   function on_batch_item(container, b, refresh) {
     let qs = property_get(b, "quizzes");
     let q = list_get(qs, quiz_index);
     q(container, refresh, success);
   }
   refresh();
-  success = app_replace_success_message(root);
-  html_display_none(success);
   html_div_text(root, "Do you want to go back and see another example?");
   let left = emoji_arrow_left();
   let combined = text_combine(
