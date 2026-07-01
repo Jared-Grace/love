e61670011633651b6d4edaaee9587c550e6a9a4e
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 04:09:49 -0400

    function_auto_path ../love/public/src/app_code_go_back.mjs

--- public/src/app_code_go_back.mjs
+++ public/src/app_code_go_back.mjs
@@ -1,7 +1,5 @@
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
-import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
-import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
 import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
@@ -16,9 +14,6 @@ export function app_code_go_back(context, root, question_text, button_text) {
   html_div_text(container3, combined2);
   let left = emoji_arrow_left();
   let combined = text_combine_multiple([left, "Yes, please ", button_text]);
-  async function lambda() {
-    await app_shared_screen_set(context, app_code_home);
-  }
   let b = app_replace_button(container3, combined, lambda);
   html_width_full(b);
 }
