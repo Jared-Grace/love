8af5d0f25bff7c11d95e7eb1634d78146fe26738
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 00:01:44 -0400

    function_auto_path ../love/public/src/app_code_container_generic.mjs

--- public/src/app_code_container_generic.mjs
+++ public/src/app_code_container_generic.mjs
@@ -8,6 +8,6 @@ export function app_code_container_generic(parent) {
   html_style_padding(container, "0.2em");
   html_border_radius_em(container, 0.75);
   html_border_none(container);
-  html_style_margin_y(container, 0.3 + "em");
+  html_style_margin_y(container, 0.5 + "em");
   return container;
 }
