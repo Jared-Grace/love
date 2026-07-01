143f3fd9e0005cfa0f5b9e1a6b88b54b1080c987
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:20:18 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- public/src/app_code_lesson_symbols_digits.mjs
+++ public/src/app_code_lesson_symbols_digits.mjs
@@ -86,7 +86,10 @@ export function app_code_lesson_symbols_digits() {
               let container = app_code_container_light_blue(parent);
               let a = example_above(container, digits);
               let container_above = property_get(a, "container_above");
-              html_text_set(container_above, "How many symbols are there? ");
+              html_text_set(
+                container_above,
+                "Please answer the following quiz question:",
+              );
               let container_answer2 = property_get(a, "container_answer");
               html_text_set(container_answer2, "How many symbols are there? ");
               let b = digit_batch();
