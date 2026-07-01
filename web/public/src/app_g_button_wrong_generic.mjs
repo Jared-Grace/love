b7fad70031f0132b2a6a4e6b821ccf21ee8d7758
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 23:58:21 -0400

    pb app_g_button_wrong_generic

--- web/public/src/app_g_button_wrong_generic.mjs
+++ web/public/src/app_g_button_wrong_generic.mjs
@@ -3,7 +3,7 @@ import { html_style_assign } from "../../../love/public/src/html_style_assign.mj
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { text_take } from "../../../love/public/src/text_take.mjs";
 import { html_border_invalid_color } from "../../../love/public/src/html_border_invalid_color.mjs";
-export function app_g_button_wrong_generic(transparency_alpha_channel_hex, b) {
+export function app_g_button_wrong_generic(b, transparency_alpha_channel_hex) {
   let v = html_border_invalid_color();
   let t = text_take(v, 7) + transparency_alpha_channel_hex;
   html_style_background_color_set(b, t);
