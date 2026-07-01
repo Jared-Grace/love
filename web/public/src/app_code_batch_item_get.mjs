1b3bef1a4deeb54716821d4dde43b804bf8da053
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 22:01:37 -0400

    pd app_code_batch_item_get container2

--- web/public/src/app_code_batch_item_get.mjs
+++ web/public/src/app_code_batch_item_get.mjs
@@ -4,7 +4,7 @@ import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_replace_all } from "../../../love/public/src/list_replace_all.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
-export function app_code_batch_item_get(batches, container2, on_batch_item) {
+export function app_code_batch_item_get(batches, on_batch_item) {
   let container = html_div(root);
   let refresh = function lambda() {
     let e = list_empty_is(batches);
