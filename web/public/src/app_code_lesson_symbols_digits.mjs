cc9281cc104851a5b1c7b7c0c59a7f8407441a3d
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 00:00:11 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- web/public/src/app_code_lesson_symbols_digits.mjs
+++ web/public/src/app_code_lesson_symbols_digits.mjs
@@ -95,7 +95,7 @@ export function app_code_lesson_symbols_digits() {
                   if (eq2) {
                     app_shared_button_screen_green_style_assign(b2);
                   } else {
-                    const transparency_alpha_channel_hex = "44";
+                    const transparency_alpha_channel_hex = "55";
                     app_g_button_wrong_generic(
                       b2,
                       transparency_alpha_channel_hex,
