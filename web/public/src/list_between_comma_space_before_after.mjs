e5d9bb6fcde76139a7c5c94d7ccbb9ca3d38459a
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 08:00:11 -0400

    pb list_between_comma_space_before_after

--- web/public/src/list_between_comma_space_before_after.mjs
+++ web/public/src/list_between_comma_space_before_after.mjs
@@ -1,6 +1,6 @@
 import { list_concat_multiple_squash } from "../../../love/public/src/list_concat_multiple_squash.mjs";
 import { list_between_comma_space } from "../../../love/public/src/list_between_comma_space.mjs";
-export function list_between_comma_space_before_after(ds, before, after) {
+export function list_between_comma_space_before_after(before, ds, after) {
   let mapped = list_between_comma_space(ds);
   const lists = [before, mapped, after];
   let squashed = list_concat_multiple_squash(lists);
