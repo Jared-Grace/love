5dd83abbc235910b31752c9a6e96f18caf8234fb
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 23:49:13 -0400

    function_auto_path ../love/public/src/list_sort_text_to.mjs

--- web/public/src/list_sort_text_to.mjs
+++ web/public/src/list_sort_text_to.mjs
@@ -1,5 +1,5 @@
 import { text_to } from "../../../love/public/src/text_to.mjs";
 import { list_sort_text_mapper } from "../../../love/public/src/list_sort_text_mapper.mjs";
-export function list_sort_text_to(concated) {
-  list_sort_text_mapper(concated, text_to);
+export function list_sort_text_to(list) {
+  list_sort_text_mapper(list, text_to);
 }
