90c7e1d8c49363c5392d26cdc335e852031ab6c2
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 03:01:06 -0400

    function_auto_path ../love/public/src/html_visibility_visible.mjs

--- public/src/html_visibility_visible.mjs
+++ public/src/html_visibility_visible.mjs
@@ -1,6 +1,6 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 export function html_visibility_visible(on_success) {
   html_style_assign(on_success, {
-    visibility: "visibile",
+    visibility: "visible",
   });
 }
