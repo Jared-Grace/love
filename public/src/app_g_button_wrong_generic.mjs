3d0447fbc6361a8a309ebbd479cdb2d3204abb56
JESUS rose to life <username@users.noreply.github.com>
2026-06-30 23:59:41 -0400

    function_auto_path ../love/public/src/app_g_button_wrong_generic.mjs

--- public/src/app_g_button_wrong_generic.mjs
+++ public/src/app_g_button_wrong_generic.mjs
@@ -1,5 +1,3 @@
-import { app_shared_button_font_color } from "../../../love/public/src/app_shared_button_font_color.mjs";
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { text_take } from "../../../love/public/src/text_take.mjs";
 import { html_border_invalid_color } from "../../../love/public/src/html_border_invalid_color.mjs";
@@ -7,7 +5,4 @@ export function app_g_button_wrong_generic(b, transparency_alpha_channel_hex) {
   let v = html_border_invalid_color();
   let t = text_take(v, 7) + transparency_alpha_channel_hex;
   html_style_background_color_set(b, t);
-  html_style_assign(b, {
-    color: app_shared_button_font_color(),
-  });
 }
