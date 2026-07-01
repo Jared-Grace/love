818ab9b1eb1a7c63239c2873d53ef6ee6a7141cf
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 04:06:08 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

---
+++ public/src/app_code_go_back.mjs
@@ -0,0 +1,22 @@
+import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
+import { app_replace_button_screen_wide } from "../../../love/public/src/app_replace_button_screen_wide.mjs";
+import { emoji_arrow_left } from "../../../love/public/src/emoji_arrow_left.mjs";
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
+import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
+export function app_code_go_back(root, question_text, button_text, context) {
+  let container3 = app_code_container_light_blue(root);
+  let combined2 = text_combine_multiple([
+    "Do you want to go back and ",
+    question_text,
+    "?",
+  ]);
+  html_div_text(container3, combined2);
+  let left = emoji_arrow_left();
+  let combined = text_combine_multiple([
+    left,
+    "Yes, please go back and ",
+    button_text,
+  ]);
+  app_replace_button_screen_wide(context, app_code_home, container3, combined);
+}
