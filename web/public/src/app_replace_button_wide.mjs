15fd3b94482eaf9efc7b986a54c1229103280e99
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 02:21:33 -0400

    function_auto_path ../love/public/src/app_replace_button_wide.mjs

--- web/public/src/app_replace_button_wide.mjs
+++ web/public/src/app_replace_button_wide.mjs
@@ -1,7 +1,7 @@
 import { html_width_full } from "../../../love/public/src/html_width_full.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
-export function app_replace_button_wide(root, name, lambda) {
-  let b = app_replace_button(root, name, lambda);
+export function app_replace_button_wide(parent, text, lambda) {
+  let b = app_replace_button(parent, text, lambda);
   html_width_full(b);
   return b;
 }
