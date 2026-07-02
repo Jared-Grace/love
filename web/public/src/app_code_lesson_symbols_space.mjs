9a2be7796009fcd55156e0706b44c9dabe54d3f9
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 01:37:41 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

--- web/public/src/app_code_lesson_symbols_space.mjs
+++ web/public/src/app_code_lesson_symbols_space.mjs
@@ -6,12 +6,7 @@ import { list_map_multiple } from "../../../love/public/src/list_map_multiple.mj
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
-import { list_alphabet_upper } from "../../../love/public/src/list_alphabet_upper.mjs";
-import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
-import { html_text_characters_numbered } from "../../../love/public/src/html_text_characters_numbered.mjs";
-import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { list_alphabet_lower } from "../../../love/public/src/list_alphabet_lower.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
@@ -27,39 +22,6 @@ export function app_code_lesson_symbols_space() {
       "In English, when writing, we use spaces to separate words",
     );
     html_span_text(div, "For a computer, a space is considered a symbol");
-    let div2 = html_div(c);
-    let alphabet_lower = list_alphabet_lower();
-    html_text_characters_numbered(div2, alphabet_lower);
-    let div3 = html_div(c);
-    html_cycle_bold(div3, [
-      "Those 26 letters are written in ",
-      "lowercase",
-      ".",
-    ]);
-    let container = app_code_container_light_blue(root);
-    let div4 = html_div(container);
-    html_cycle_bold(div4, [
-      "Here are the 26 English letters written in ",
-      "uppercase",
-      ":",
-    ]);
-    let div5 = html_div(container);
-    let alphabet_upper = list_alphabet_upper();
-    html_text_characters_numbered(div5, alphabet_upper);
-    let container2 = app_code_container_light_blue(root);
-    html_div_text(container2, "Remember, numbers are examples of symbols");
-    html_div_text(
-      container2,
-      "Also, both uppercase and lowercase letters are examples of symbols",
-    );
-    html_div_text(
-      container2,
-      "When we write computer programs, we use symbols, including letters and numbers",
-    );
-    html_div_text(
-      container2,
-      "For a computer, lowercase symbols may be considered different than uppercase symbols",
-    );
   }
   function lambda2(word) {
     function lambda3(c2) {
