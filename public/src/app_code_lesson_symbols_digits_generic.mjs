5c2dc4ebaa82e92a6f7f29725f5638343fe265ef
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:14:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_generic.mjs

--- public/src/app_code_lesson_symbols_digits_generic.mjs
+++ public/src/app_code_lesson_symbols_digits_generic.mjs
@@ -57,7 +57,8 @@ export function app_code_lesson_symbols_digits_generic(
         return digits;
       }
       let mapped2 = list_map(digit_counts, lambda);
-      function lambda2(item) {
+      function lambda2(digits) {
+        let size2 = list_size(list);
         const answer_count_max = app_code_answer_count_max();
         const quizzes = [
           function on_quiz1(context, parent, refresh) {
