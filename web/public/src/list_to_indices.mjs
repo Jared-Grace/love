dd82e03992c7d0f95b10a80842898cd1ba4c1748
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 07:36:31 -0400

    function_auto_path ../love/public/src/list_to_indices.mjs

--- web/public/src/list_to_indices.mjs
+++ web/public/src/list_to_indices.mjs
@@ -2,6 +2,6 @@ import { range } from "../../../love/public/src/range.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_to_indices(item3) {
   let size = list_size(item3);
-  let r = range(size);
-  return r;
+  let indices = range(size);
+  return indices;
 }
