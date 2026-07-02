b5640f0ccf87a20d21fa4febe6dd90c71c442bb4
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:30:49 -0400

    function_auto_path ../love/public/src/list_map_multiple.mjs

--- web/public/src/list_map_multiple.mjs
+++ web/public/src/list_map_multiple.mjs
@@ -2,9 +2,9 @@ import { each } from "../../../love/public/src/each.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 export function list_map_multiple(list, mappers) {
   let mapped = list;
-  function lambda3(mapper) {
+  function lambda(mapper) {
     mapped = list_map(mapped, mapper);
   }
-  each(mappers, lambda3);
+  each(mappers, lambda);
   return mapped;
 }
