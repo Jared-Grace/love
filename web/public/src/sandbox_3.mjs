3ae297174afd41af9639588e53fd130e721ea116
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:17:27 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -4,17 +4,12 @@ import { each } from "../../../love/public/src/each.mjs";
 import { lookup_adder } from "../../../love/public/src/lookup_adder.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function sandbox_3() {
   let books = await ebible_version_books("engbsb");
   let mapped2 = list_map_property(books, "text");
-  function lambda(book_name) {
-    let prefixes = ["1 ", "2 "];
-    let item3 = list_map_prefix_without_try_multiple(book_name, prefixes);
-    return item3;
-  }
-  let mapped = list_map(mapped2, lambda);
+  let prefixes = ["1 ", "2 "];
+  let mapped = list_map_prefix_without_try_multiple(mapped2, prefixes);
   let unique = list_unique(mapped);
   function lambda2(la) {
     function lambda3(book_name) {
