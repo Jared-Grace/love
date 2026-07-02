bd7b3a95513032b779671fa754e5dc8c73dd1ef9
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 04:26:39 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_add.mjs

--- public/src/app_replace_rule_set_add.mjs
+++ public/src/app_replace_rule_set_add.mjs
@@ -2,7 +2,7 @@ import { app_replace_rule_set_add_generic } from "../../../love/public/src/app_r
 import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
 import { app_new_rule_set_new } from "../../../love/public/src/app_new_rule_set_new.mjs";
 export async function app_replace_rule_set_add() {
-  let base = app_new_rule_set_new;
+  let base = app_new_rule_set_new.name;
   let fns_list = app_replace_rule_sets_fns;
   await app_replace_rule_set_add_generic(base, fns_list);
 }
