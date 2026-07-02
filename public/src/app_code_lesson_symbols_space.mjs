5537b3e4400dd7c73b97dde10bf0fbc035a6b4a3
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 01:39:39 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

--- public/src/app_code_lesson_symbols_space.mjs
+++ public/src/app_code_lesson_symbols_space.mjs
@@ -1,8 +1,5 @@
 import { each_next } from "../../../love/public/src/each_next.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { boolean_random } from "../../../love/public/src/boolean_random.mjs";
-import { text_transform } from "../../../love/public/src/text_transform.mjs";
 import { list_map_multiple } from "../../../love/public/src/list_map_multiple.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
@@ -12,8 +9,6 @@ import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
-import { ternary } from "../../../love/public/src/ternary.mjs";
-import { text_upper_to } from "../../../love/public/src/text_upper_to.mjs";
 export function app_code_lesson_symbols_space() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
@@ -24,24 +19,11 @@ export function app_code_lesson_symbols_space() {
     );
     html_span_text(div, "For a computer, a space is considered a symbol");
   }
-  function lambda2(word) {
-    function lambda3(c2) {
-      let u = boolean_random();
-      let mapper = ternary(u, text_upper_to, identity);
-      let r = mapper(c2);
-      return r;
-    }
-    let joined = text_transform(word, lambda3);
-    log(app_code_lesson_symbols_space.name, {
-      joined,
-    });
-    return joined;
-  }
   function batch_get() {
     let verse =
       "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
     let split = text_split_space(verse);
-    let mappers = [text_letters_only, lambda2, text_split_empty];
+    let mappers = [text_letters_only, identity, text_split_empty];
     let mapped = list_map_multiple(split, mappers);
     function lambda4(item, next) {}
     each_next(list, lambda4);
