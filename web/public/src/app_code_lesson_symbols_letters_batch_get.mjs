aad92d5a530a9f9f86bc9f52034c06d344adbe3e
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 03:03:10 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- web/public/src/app_code_lesson_symbols_letters_batch_get.mjs
+++ web/public/src/app_code_lesson_symbols_letters_batch_get.mjs
@@ -3,8 +3,8 @@ import { list_map_multiple } from "../../../love/public/src/list_map_multiple.mj
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
 export function app_code_lesson_symbols_letters_batch_get(mapper) {
-  let mappers = [text_letters_only, mapper, text_split_empty];
   let split = app_code_verse_words();
+  let mappers = [text_letters_only, mapper, text_split_empty];
   let mapped = list_map_multiple(split, mappers);
   return mapped;
 }
