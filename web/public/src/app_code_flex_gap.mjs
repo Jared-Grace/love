ce602528a2e69b99908cc3ba0402aae29003868e
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 00:53:53 -0400

    function_auto_path ../love/public/src/app_code_container_light_blue.mjs

---
+++ web/public/src/app_code_flex_gap.mjs
@@ -0,0 +1,6 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function app_code_flex_gap(container) {
+  html_style_assign(container, {
+    gap: "0.8em",
+  });
+}
