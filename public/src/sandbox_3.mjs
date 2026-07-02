76e8c7a81e3844de6b27d00c651fc12feaf84a51
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:53:22 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,5 +1,8 @@
+import { each_object } from "../../../love/public/src/each_object.mjs";
 import { bible_books_by_first_letter } from "../../../love/public/src/bible_books_by_first_letter.mjs";
 export async function sandbox_3() {
   let dictionary = await bible_books_by_first_letter();
+  function lambda(value, property) {}
+  each_object(object, lambda);
   return dictionary;
 }
