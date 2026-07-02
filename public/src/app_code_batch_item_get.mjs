2bcabcacc6d3541afe28b04e23aae22eb70095d4
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 06:50:34 -0400

    function_auto_path ../love/public/src/app_code_batch_item_get.mjs

--- public/src/app_code_batch_item_get.mjs
+++ public/src/app_code_batch_item_get.mjs
@@ -3,7 +3,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
 import { html_clear } from "../../../love/public/src/html_clear.mjs";
-import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
 export function app_code_batch_item_get(
   parent,
