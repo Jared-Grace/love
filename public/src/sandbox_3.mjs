29b8e7276cb4e6315b053b65bfae2dbd4b6a26cb
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:54:22 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -6,9 +6,9 @@ import { list_join_comma_space } from "../../../love/public/src/list_join_comma_
 export async function sandbox_3() {
   let dictionary = await bible_books_by_first_letter();
   function lambda(value, property) {
-    let joined = list_join_comma_space(list);
-    let combined = text_combine_multiple([property, ": "]);
-    log(sandbox_3.name, {});
+    let joined = list_join_comma_space(value);
+    let combined = text_combine_multiple([property, ": ", joined]);
+    log(sandbox_3.name, combined);
   }
   each_object(object, lambda);
   return dictionary;
