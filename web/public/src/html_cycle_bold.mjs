1d9f765ccf11b20c94bbecb77303f7b81acfd536
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 04:39:16 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_letters_spaces.mjs

--- web/public/src/html_cycle_bold.mjs
+++ web/public/src/html_cycle_bold.mjs
@@ -1,7 +1,7 @@
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
-export function html_cycle_bold(parent, parts) {
+export function html_cycle_bold(parent, parts) {$aa
   let cycles = [noop, html_bold];
   html_cycle(parent, cycles, parts);
 }
