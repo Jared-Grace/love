0e40e79691f3bc2fffcb64fad2825fba47a699be
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:54:47 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters_upper.mjs

--- public/src/app_code_lesson_symbols_letters_upper.mjs
+++ public/src/app_code_lesson_symbols_letters_upper.mjs
@@ -1,3 +1,5 @@
+import { text_upper_to } from "../../../love/public/src/text_upper_to.mjs";
+import { app_code_lesson_symbols_letters_batch } from "../../../love/public/src/app_code_lesson_symbols_letters_batch.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { list_alphabet_upper } from "../../../love/public/src/list_alphabet_upper.mjs";
 import { html_text_characters_numbered } from "../../../love/public/src/html_text_characters_numbered.mjs";
@@ -46,12 +48,13 @@ export function app_code_lesson_symbols_letters_upper() {
       "For a computer, lowercase symbols may be considered different symbols than uppercase",
     );
   }
+  let batch_symbols = app_code_lesson_symbols_letters_batch(text_upper_to);
   let r5 = app_code_lesson_symbols_digits_generic(
     "Symbols (Letters)",
     "symbols_letters",
     lambda,
     noop,
-    lambda2,
+    batch_symbols,
   );
   return r5;
 }
