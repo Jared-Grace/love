bde404dcfee983ad8c83f270cb8137386a4697d3
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:53:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters_lower.mjs

--- web/public/src/app_code_lesson_symbols_letters_lower.mjs
+++ web/public/src/app_code_lesson_symbols_letters_lower.mjs
@@ -1,8 +1,3 @@
-import { list_map_multiple } from "../../../love/public/src/list_map_multiple.mjs";
-import { text_lower_to } from "../../../love/public/src/text_lower_to.mjs";
-import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
-import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
-import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { html_text_characters_numbered } from "../../../love/public/src/html_text_characters_numbered.mjs";
 import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
@@ -35,14 +30,6 @@ export function app_code_lesson_symbols_letters_lower() {
       container2,
       "Lowercase letters are also examples of symbols",
     );
-  }
-  function lambda2() {
-    let verse =
-      "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
-    let split = text_split_space(verse);
-    let mappers = [text_letters_only, text_lower_to, text_split_empty];
-    let mapped = list_map_multiple(split, mappers);
-    return mapped;
   }
   let r5 = app_code_lesson_symbols_digits_generic(
     "Symbols (Letters)",
