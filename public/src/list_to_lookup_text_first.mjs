2dada320cbb59b474bd4cea908f46dce3af61219
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:44:15 -0400

    function_auto_path ../love/public/src/list_to_lookup_text_first.mjs

--- public/src/list_to_lookup_text_first.mjs
+++ public/src/list_to_lookup_text_first.mjs
@@ -1,9 +1,11 @@
+import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
 import { lookup_adder } from "../../../love/public/src/lookup_adder.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { text_first } from "../../../love/public/src/text_first.mjs";
 export function list_to_lookup_text_first(unique) {
   function lambda2(la) {
     function lambda3(book_name) {
+      let letters = text_letters_only(t);
       let key = text_first(book_name);
       la(key, book_name);
     }
