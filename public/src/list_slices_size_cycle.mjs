1a571ade2d0be209e3575695f18fe57c3d8d5c51
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 06:42:54 -0400

    function_auto_path ../love/public/src/list_slices_size_cycle.mjs

--- public/src/list_slices_size_cycle.mjs
+++ public/src/list_slices_size_cycle.mjs
@@ -2,6 +2,7 @@ import { range_from } from "../../../love/public/src/range_from.mjs";
 import { list_slices_size_generic } from "../../../love/public/src/list_slices_size_generic.mjs";
 export function list_slices_size_cycle(list, min, max) {
   let mapped = range_from(min, max);
+  let i = 0;
   let count_get = function lambda() {};
   let slices = list_slices_size_generic(count_get, min, max, list);
   return slices;
