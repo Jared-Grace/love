95a7a98406e62dc79578366b11acb1b408f03e70
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 06:35:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_identifiers_valid.mjs

---
+++ public/src/js_parse_expression_try.mjs
@@ -0,0 +1,10 @@
+import { catch_ignore } from "../../../love/public/src/catch_ignore.mjs";
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
+export function js_parse_expression_try(joined) {
+  let expression = null;
+  function lambda3() {
+    expression = js_parse_expression(joined);
+  }
+  catch_ignore(lambda3);
+  return expression;
+}
