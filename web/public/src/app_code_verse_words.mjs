ec07537d65e6c61b59533bc78b870b4c76aba02d
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 02:57:37 -0400

    function_auto_path ../love/public/src/app_code_verse_words.mjs

--- web/public/src/app_code_verse_words.mjs
+++ web/public/src/app_code_verse_words.mjs
@@ -2,6 +2,6 @@ import { text_split_space } from "../../../love/public/src/text_split_space.mjs"
 export function app_code_verse_words() {
   let verse =
     "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
-  let split = text_split_space(verse);
-  return split;
+  let words = text_split_space(verse);
+  return words;
 }
