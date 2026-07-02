42704a70929f013eb726df2482784c947f673bdd
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:18:17 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- public/src/app_code_lesson_symbols_letters.mjs
+++ public/src/app_code_lesson_symbols_letters.mjs
@@ -1,3 +1,7 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { integer_positive_random_digits_text } from "../../../love/public/src/integer_positive_random_digits_text.mjs";
+import { digits } from "../../../love/public/src/digits.mjs";
+import { range_1 } from "../../../love/public/src/range_1.mjs";
 import { error } from "../../../love/public/src/error.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { list_alphabet_upper } from "../../../love/public/src/list_alphabet_upper.mjs";
@@ -48,13 +52,15 @@ export function app_code_lesson_symbols_letters() {
     );
   }
   let batch_symbols = error();
-  function lambda2() {  let digit_counts = range_1(5);
+  function lambda2() {
+    let digit_counts = range_1(5);
     function lambda(digit_count) {
       let digits = integer_positive_random_digits_text(digit_count);
       return digits;
     }
     let batch_digits = list_map(digit_counts, lambda);
-    return batch_digits;}
+    return batch_digits;
+  }
   let r5 = app_code_lesson_symbols_digits_generic(
     "Symbols (Letters)",
     "symbols_letters",
