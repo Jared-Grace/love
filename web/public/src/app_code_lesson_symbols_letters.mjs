b19f1b8908eac3f5dac9cd035a2a51442bf07bc4
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:29:51 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- web/public/src/app_code_lesson_symbols_letters.mjs
+++ web/public/src/app_code_lesson_symbols_letters.mjs
@@ -1,3 +1,4 @@
+import { text_lower_to } from "../../../love/public/src/text_lower_to.mjs";
 import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
@@ -55,7 +56,7 @@ export function app_code_lesson_symbols_letters() {
     "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
   let split = text_split_space(verse);
   function lambda2() {
-    let mappers = [text_split_empty, text_letters_only];
+    let mappers = [text_split_empty, text_letters_only, text_lower_to];
     function lambda3(mapper) {
       split = list_map(split, mapper);
     }
