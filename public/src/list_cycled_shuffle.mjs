283b5cee7cb484632f27af4c2d8a38c903073240
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 07:36:22 -0400

    function_auto_path ../love/public/src/list_cycled_shuffle.mjs

--- public/src/list_cycled_shuffle.mjs
+++ public/src/list_cycled_shuffle.mjs
@@ -1,8 +1,10 @@
+import { list_to_indices } from "../../../love/public/src/list_to_indices.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { range } from "../../../love/public/src/range.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 export function list_cycled_shuffle(list, cycle_size) {
   let mods = range(cycle_size);
+  let r2 = list_to_indices(item3);
   function lambda(m) {}
   each(mods, lambda);
   let r = list_shuffle(list);
