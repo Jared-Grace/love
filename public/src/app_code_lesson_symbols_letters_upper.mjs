719fbe775d0f3e0b189f0dff198181ec6d77ef53
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 23:25:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters_lower.mjs

--- public/src/app_code_lesson_symbols_letters_upper.mjs
+++ public/src/app_code_lesson_symbols_letters_upper.mjs
@@ -6,18 +6,6 @@ import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src
 export function app_code_lesson_symbols_letters_upper() {
   function lambda(root) {
     return;
-    html_div_text(
-      container2,
-      "Also, both uppercase and lowercase letters are examples of symbols",
-    );
-    html_div_text(
-      container2,
-      "When we write computer programs, we use symbols, including letters and numbers",
-    );
-    html_div_text(
-      container2,
-      "For a computer, lowercase symbols may be considered different symbols than uppercase",
-    );
   }
   let batch_symbols = app_code_lesson_symbols_letters_batch(text_upper_to);
   let r5 = app_code_lesson_symbols_digits_generic(
