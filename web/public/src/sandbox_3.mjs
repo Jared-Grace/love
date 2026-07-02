aa704de1ea539e74b90ff316b659fa83ea3a3d13
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:02:40 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,5 +1,8 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function sandbox_3() {
   let books = await ebible_version_books("engbsb");
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
   return books;
 }
