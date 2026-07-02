937d112e9365e3e99e15e1d7fd408ab861107174
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 04:25:18 -0400

    c r app_replace_rule_set_add,app_code_lesson_add

---
+++ web/public/src/app_code_lesson_add.mjs
@@ -0,0 +1,8 @@
+import { app_replace_rule_set_add_generic } from "../../../love/public/src/app_replace_rule_set_add_generic.mjs";
+import { app_replace_rule_sets_fns } from "../../../love/public/src/app_replace_rule_sets_fns.mjs";
+import { app_new_rule_set_new } from "../../../love/public/src/app_new_rule_set_new.mjs";
+export async function app_code_lesson_add() {
+  let base = app_new_rule_set_new;
+  let fns_list = app_replace_rule_sets_fns;
+  await app_replace_rule_set_add_generic(base, fns_list);
+}
