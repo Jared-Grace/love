c135bda70b6361773d71a9e65d9861ff7c8ff089
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 07:37:55 -0400

    function_auto_path ../love/public/src/list_cycled_shuffle.mjs

--- public/src/list_cycled_shuffle.mjs
+++ public/src/list_cycled_shuffle.mjs
@@ -11,7 +11,8 @@ export function list_cycled_shuffle(list, cycle_size) {
   function lambda(m) {
     function lambda2(index) {
       let m2 = mod(index, cycle_size);
-      let eq2 = equal(left, right);
+      let eq2 = equal(m2, m);
+      return eq2;
     }
     let filtered = list_filter(indices, lambda2);
   }
