4f0e01d2771dbbb32fed50200cd277721c0790dd
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 07:38:36 -0400

    c c list_shuffle,indice

---
+++ web/public/src/list_shuffle_indice.mjs
@@ -0,0 +1,9 @@
+import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
+import { random } from "../../../love/public/src/random.mjs";
+import { floor } from "../../../love/public/src/floor.mjs";
+export function list_shuffle_indice(list) {
+  for (let i = list.length - 1; i > 0; i--) {
+    const j = floor(random() * (i + 1));
+    list_swap_at(list, i, j);
+  }
+}
