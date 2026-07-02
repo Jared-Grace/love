2344a8abe4281a6074e3d171f0bbf4df20a4009f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:55:13 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters_upper.mjs

--- web/public/src/app_code_lesson_symbols_letters_upper.mjs
+++ web/public/src/app_code_lesson_symbols_letters_upper.mjs
@@ -4,32 +4,15 @@ import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { list_alphabet_upper } from "../../../love/public/src/list_alphabet_upper.mjs";
 import { html_text_characters_numbered } from "../../../love/public/src/html_text_characters_numbered.mjs";
 import { html_cycle_bold } from "../../../love/public/src/html_cycle_bold.mjs";
-import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { list_alphabet_lower } from "../../../love/public/src/list_alphabet_lower.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
 export function app_code_lesson_symbols_letters_upper() {
   function lambda(root) {
-    let c = app_code_container_light_blue(root);
-    let div = html_div(c);
-    let span2 = html_span_text(
-      div,
-      "In English, there are 26 letters of the alphabet: ",
-    );
-    let div2 = html_div(c);
-    let alphabet_lower = list_alphabet_lower();
-    html_text_characters_numbered(div2, alphabet_lower);
-    let div3 = html_div(c);
-    html_cycle_bold(div3, [
-      "Those 26 letters are written in ",
-      "lowercase",
-      ".",
-    ]);
     let container = app_code_container_light_blue(root);
     let div4 = html_div(container);
-    html_cycle_bold(div4, ["Below are letters written in ", "uppercase", ":"]);
+    html_cycle_bold(div4, ["Here are letters written in ", "uppercase", ":"]);
     let div5 = html_div(container);
     let alphabet_upper = list_alphabet_upper();
     html_text_characters_numbered(div5, alphabet_upper);
