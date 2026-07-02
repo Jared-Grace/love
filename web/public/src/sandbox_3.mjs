a9c780337ec60875164e8ca00a8430e1795e2f66
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:57:08 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -4,11 +4,13 @@ import { log } from "../../../love/public/src/log.mjs";
 import { each_object } from "../../../love/public/src/each_object.mjs";
 import { bible_books_by_first_letter } from "../../../love/public/src/bible_books_by_first_letter.mjs";
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
+import { ternary } from "../../../love/public/src/ternary.mjs";
 export async function sandbox_3() {
   let dictionary = await bible_books_by_first_letter();
   function lambda(value, property) {
     let joined = list_join_comma_space(value);
     let size = list_size(value);
+    let result = ternary(condition, on_true, on_false);
     let combined = text_combine_multiple([
       property,
       ": ",
