766bed1501a2614d2ed86a41083fc624dcd5ab75
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:27:22 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,7 +1,5 @@
+import { list_to_lookup_text_first } from "../../../love/public/src/list_to_lookup_text_first.mjs";
 import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
-import { text_first } from "../../../love/public/src/text_first.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { lookup_adder } from "../../../love/public/src/lookup_adder.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
@@ -11,13 +9,6 @@ export async function sandbox_3() {
   let prefixes = ["1 ", "2 ", "3 "];
   let mapped = list_map_prefix_without_try_multiple(mapped2, prefixes);
   let unique = list_unique(mapped);
-  function lambda2(la) {
-    function lambda3(book_name) {
-      let key = text_first(book_name);
-      la(key, book_name);
-    }
-    each(unique, lambda3);
-  }
-  let result = lookup_adder(lambda2);
+  let result = list_to_lookup_text_first(unique);
   return result;
 }
