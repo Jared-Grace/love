32ef6d6cb4b2a48d13e259aa08e0ee86e4aa5953
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 00:58:06 -0400

    function_auto_path ../love/public/src/app_code_quiz.mjs

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -1,3 +1,5 @@
+import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
+import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { app_code_go_back } from "../../../love/public/src/app_code_go_back.mjs";
@@ -11,7 +13,9 @@ export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
-  let refresh = app_code_batch_item_get(root, lesson, on_batch_item);
+  let c = app_code_container_light_blue(root);
+  html_div_text(c, "Please answer the following quiz question:");
+  let refresh = app_code_batch_item_get(c, lesson, on_batch_item);
   function on_batch_item(container, b, refresh) {
     let qs = property_get(b, "quizzes");
     let q = list_get(qs, quiz_index);
