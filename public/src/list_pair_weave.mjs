7549fd7d20f6209854a45e42bcc4a65590e61b08
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 01:38:21 -0400

    function_auto_path ../love/public/src/list_pair_weave.mjs

--- public/src/list_pair_weave.mjs
+++ public/src/list_pair_weave.mjs
@@ -1,12 +1,12 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function list_pair_weave(filtered, translated) {
+export function list_pair_weave(list_a, list_b) {
   function lambda2(la) {
     function lambda(a, b) {
       each([a, b], la);
     }
-    each_pair(filtered, translated, lambda);
+    each_pair(list_a, list_b, lambda);
   }
   let list = list_adder(lambda2);
   return list;
