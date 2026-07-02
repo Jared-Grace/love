f84b0d2b355f4aebe40ca33550c5aedccb16e04a
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:44:46 -0400

    function_auto_path ../love/public/src/list_to_lookup_letter_first.mjs

--- web/public/src/list_to_lookup_text_first_unique.mjs
+++ web/public/src/list_to_lookup_text_first_unique.mjs
@@ -1,7 +1,7 @@
-import { list_to_lookup_text_first } from "../../../love/public/src/list_to_lookup_text_first.mjs";
+import { list_to_lookup_letter_first } from "../../../love/public/src/list_to_lookup_letter_first.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 export function list_to_lookup_text_first_unique(mapped) {
   let unique = list_unique(mapped);
-  let result = list_to_lookup_text_first(unique);
+  let result = list_to_lookup_letter_first(unique);
   return result;
 }
