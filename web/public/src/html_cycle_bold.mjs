c4824960895bebf857793fc0b672b609d98820a6
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 04:39:16 -0400

    function_auto_path ../love/public/src/html_cycle_bold.mjs

--- web/public/src/html_cycle_bold.mjs
+++ web/public/src/html_cycle_bold.mjs
@@ -1,7 +1,9 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
-export function html_cycle_bold(parent, parts) {$aa
+export function html_cycle_bold(parent, parts) {
+  arguments_assert(arguments, 2);
   let cycles = [noop, html_bold];
   html_cycle(parent, cycles, parts);
 }
