15197b1dfb7622823888ece06f690a29f6fdf1bd
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:43:55 -0400

    function_auto_path ../love/public/src/text_letters_only.mjs

--- public/src/text_letters_only.mjs
+++ public/src/text_letters_only.mjs
@@ -1,6 +1,6 @@
 import { regex_letters_not } from "../../../love/public/src/regex_letters_not.mjs";
 export function text_letters_only(t) {
-  let v = regex_letters_not();
-  let letters = t.replace(v, "");
+  let r = regex_letters_not();
+  let letters = t.replace(r, "");
   return letters;
 }
