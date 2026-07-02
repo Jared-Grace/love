a52cffe8e1e22c62771f5af83dd8a183fb057257
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 01:40:56 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters.mjs

--- web/public/src/app_code_lesson_symbols_letters.mjs
+++ web/public/src/app_code_lesson_symbols_letters.mjs
@@ -73,15 +73,14 @@ export function app_code_lesson_symbols_letters() {
     });
     return joined;
   }
-  function batch_get() {
+  let batch_symbols = function lambda4() {
     let mappers = [text_letters_only, lambda2, text_split_empty];
     let verse =
       "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
     let split = text_split_space(verse);
     let mapped = list_map_multiple(split, mappers);
     return mapped;
-  }
-  let batch_symbols = batch_get;
+  };
   let r5 = app_code_lesson_symbols_digits_generic(
     "Symbols (Letters)",
     "symbols_letters",
