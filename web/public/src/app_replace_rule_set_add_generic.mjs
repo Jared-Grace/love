3d47bf9e68d8d2b86858cf8d5a0d0e785e74e715
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 04:25:01 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_add.mjs

---
+++ web/public/src/app_replace_rule_set_add_generic.mjs
@@ -0,0 +1,19 @@
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
+import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
+import { text_unique } from "../../../love/public/src/text_unique.mjs";
+import { functions_names } from "../../../love/public/src/functions_names.mjs";
+export async function app_replace_rule_set_add_generic(base, fns_list) {
+  let f_names = await functions_names();
+  const f = base.name;
+  let unique = text_unique(f_names, f, "_");
+  await function_copy_open(f, unique);
+  let expression = js_parse_expression(unique);
+  async function lambda(ast) {
+    let elements = js_array_expression_single_elements(ast);
+    list_add(elements, expression);
+  }
+  let output = await function_transform(fns_list.name, lambda);
+}
