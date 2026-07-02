24140a2b29cb367779ed297ff98c32dee4148613
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 03:05:03 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered.mjs

--- public/src/app_code_lesson_symbols_digits_numbered.mjs
+++ public/src/app_code_lesson_symbols_digits_numbered.mjs
@@ -1,5 +1,5 @@
+import { app_code_lesson_symbols_digits_numbered_on_symbol } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
-import { html_span_text_smaller } from "../../../love/public/src/html_span_text_smaller.mjs";
 import { html_cycle_mono_list_between_comma_space_before_after } from "../../../love/public/src/html_cycle_mono_list_between_comma_space_before_after.mjs";
 import { html_span_text_bold } from "../../../love/public/src/html_span_text_bold.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
@@ -7,13 +7,12 @@ import { digits } from "../../../love/public/src/digits.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
-import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 export function app_code_lesson_symbols_digits_numbered() {
   let r5 = app_code_lesson_symbols_digits_generic(
     "Symbols (Digits, numbered)",
     "symbols_digits_numbered",
     above,
-    on_symbol,
+    app_code_lesson_symbols_digits_numbered_on_symbol,
     app_code_lesson_symbols_batch_digits,
   );
   return r5;
@@ -40,9 +39,5 @@ export function app_code_lesson_symbols_digits_numbered() {
       c,
       "When we write computer programs, we use symbols, including numbers",
     );
-  }
-  function on_symbol(parent, index_1) {
-    let div4 = html_span_text_smaller(parent, index_1);
-    html_font_color_set(div4, "#bbb");
   }
 }
---
+++ public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs
@@ -0,0 +1,9 @@
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+import { html_span_text_smaller } from "../../../love/public/src/html_span_text_smaller.mjs";
+export function app_code_lesson_symbols_digits_numbered_on_symbol(
+  parent,
+  index_1,
+) {
+  let div4 = html_span_text_smaller(parent, index_1);
+  html_font_color_set(div4, "#bbb");
+}
