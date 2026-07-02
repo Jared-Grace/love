91fea7c302556463f67067b65700cc9b65700a28
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 02:20:29 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

--- web/public/src/app_code_lesson_symbols_space.mjs
+++ web/public/src/app_code_lesson_symbols_space.mjs
@@ -7,7 +7,6 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_to_indices_skip_1 } from "../../../love/public/src/list_to_indices_skip_1.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
 import { integer_random } from "../../../love/public/src/integer_random.mjs";
 import { list_random_item_count } from "../../../love/public/src/list_random_item_count.mjs";
 import { app_code_lesson_symbols_letters_batch_get } from "../../../love/public/src/app_code_lesson_symbols_letters_batch_get.mjs";
@@ -17,6 +16,7 @@ import { app_code_container_light_blue } from "../../../love/public/src/app_code
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_digits_generic } from "../../../love/public/src/app_code_lesson_symbols_digits_generic.mjs";
 import { list_map_index } from "../../../love/public/src/list_map_index.mjs";
+import { list_size_less_1 } from "../../../love/public/src/list_size_less_1.mjs";
 export function app_code_lesson_symbols_space() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
@@ -31,7 +31,7 @@ export function app_code_lesson_symbols_space() {
     let mapped = app_code_lesson_symbols_letters_batch_get(identity);
     function lambda5(item3) {
       let skipped = list_to_indices_skip_1(item3);
-      let max = list_size(skipped);
+      let max = list_size_less_1(skipped);
       let count = integer_random(1, max);
       let items = list_random_item_count(skipped, count);
       function lambda2(item, index) {
