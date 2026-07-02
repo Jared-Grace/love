0d8bd430113ed70ad2ad2ec4f78c1e4acc51c76d
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 06:34:43 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_identifiers_valid.mjs

--- web/public/src/app_code_lesson_symbols_identifiers_valid.mjs
+++ web/public/src/app_code_lesson_symbols_identifiers_valid.mjs
@@ -14,8 +14,8 @@ export function app_code_lesson_symbols_identifiers_valid(
   const quiz_label = example_label;
   let symbols_to_answer = function lambda(symbols) {
     let joined = list_join_empty(symbols);
+    let expression = null;
     try {
-      let expression = null;
       expression = js_parse_expression_from_assignment(joined);
     } catch (e) {}
     let ii = js_identifier_is(expression);
