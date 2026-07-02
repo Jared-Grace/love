abc550332f3f299a61624fa9c0816786d53f981f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:36:43 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_letters_lower.mjs

--- web/public/src/app_code_lesson_symbols_letters_lower.mjs
+++ web/public/src/app_code_lesson_symbols_letters_lower.mjs
@@ -36,10 +36,10 @@ export function app_code_lesson_symbols_letters_lower() {
       "Lowercase letters are also examples of symbols",
     );
   }
-  let verse =
-    "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
-  let split = text_split_space(verse);
   function lambda2() {
+    let verse =
+      "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life";
+    let split = text_split_space(verse);
     let mappers = [text_letters_only, text_lower_to, text_split_empty];
     let mapped = list_map_multiple(split, mappers);
     return mapped;
