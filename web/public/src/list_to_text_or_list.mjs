564fa907af17f0a5615f3f51c15744efe1a1a210
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 04:31:12 -0400

    function_auto_path ../love/public/src/list_to_text_or_list.mjs

--- web/public/src/list_to_text_or_list.mjs
+++ web/public/src/list_to_text_or_list.mjs
@@ -1,4 +1,6 @@
+import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
 export function list_to_text_or_list(list) {
-  let taken = list_take_less_1(list);ll
+  let taken = list_take_less_1(list);
+  let last = list_last(list2);
 }
