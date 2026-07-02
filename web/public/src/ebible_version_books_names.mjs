4b33c89fd8b253a1694aef9852a1df53ce483560
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:49:18 -0400

    function_auto_path ../love/public/src/ebible_version_books_names.mjs

--- web/public/src/ebible_version_books_names.mjs
+++ web/public/src/ebible_version_books_names.mjs
@@ -2,6 +2,6 @@ import { list_map_property } from "../../../love/public/src/list_map_property.mj
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function ebible_version_books_names(bible_folder) {
   let books = await ebible_version_books(bible_folder);
-  let mapped2 = list_map_property(books, "text");
-  return mapped2;
+  let book_names = list_map_property(books, "text");
+  return book_names;
 }
