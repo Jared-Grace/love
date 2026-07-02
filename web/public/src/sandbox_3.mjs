dc42ce51d3c9409285644068b9ef9c3b25b9f036
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:42:53 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,3 +1,4 @@
+import { ebible_version_books_names } from "../../../love/public/src/ebible_version_books_names.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { property_exists } from "../../../love/public/src/property_exists.mjs";
@@ -6,12 +7,9 @@ import { text_lower_to } from "../../../love/public/src/text_lower_to.mjs";
 import { list_alphabet_upper } from "../../../love/public/src/list_alphabet_upper.mjs";
 import { list_to_lookup_text_first_unique } from "../../../love/public/src/list_to_lookup_text_first_unique.mjs";
 import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 import { list_to_dictionary_value } from "../../../love/public/src/list_to_dictionary_value.mjs";
 export async function sandbox_3() {
-  let books = await ebible_version_books("engbsb");
-  let mapped2 = list_map_property(books, "text");
+  let mapped2 = await ebible_version_books_names("engbsb");
   let prefixes = ["1 ", "2 ", "3 "];
   let mapped = list_map_prefix_without_try_multiple(mapped2, prefixes);
   let result = list_to_lookup_text_first_unique(mapped);
