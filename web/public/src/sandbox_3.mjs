09acc4e23c10b0eb23be8358039d852e9fb7bf1f
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:27:37 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,6 +1,5 @@
-import { list_to_lookup_text_first } from "../../../love/public/src/list_to_lookup_text_first.mjs";
+import { list_to_lookup_text_first_unique } from "../../../love/public/src/list_to_lookup_text_first_unique.mjs";
 import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function sandbox_3() {
@@ -8,7 +7,6 @@ export async function sandbox_3() {
   let mapped2 = list_map_property(books, "text");
   let prefixes = ["1 ", "2 ", "3 "];
   let mapped = list_map_prefix_without_try_multiple(mapped2, prefixes);
-  let unique = list_unique(mapped);
-  let result = list_to_lookup_text_first(unique);
+  let result = list_to_lookup_text_first_unique(mapped);
   return result;
 }
