f9042c4c07eb93a4648b756d39ea2fc83e35aafa
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 06:50:23 -0400

    pne app_code_batch_item_get on_batch list_shuffle

--- web/public/src/app_code_quiz.mjs
+++ web/public/src/app_code_quiz.mjs
@@ -13,7 +13,7 @@ export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
   let quiz_index = storage_local_initialize_context(context, "quiz_index", 0);
-  let refresh = app_code_batch_item_get(root, lesson, on_batch_item);
+  let refresh = app_code_batch_item_get(root, lesson, on_batch_item, error());
   function on_batch_item(container, b, refresh) {
     let c = app_code_container_light_blue(container);
     html_div_text(c, "Please answer the following quiz question:");
