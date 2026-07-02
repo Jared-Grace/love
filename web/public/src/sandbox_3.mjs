02be587cb91e45979eda4e79ab845a11e2c3222e
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 21:14:36 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,18 +1,17 @@
+import { list_map_prefix_without_try_multiple } from "../../../love/public/src/list_map_prefix_without_try_multiple.mjs";
 import { text_first } from "../../../love/public/src/text_first.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { lookup_adder } from "../../../love/public/src/lookup_adder.mjs";
 import { list_unique } from "../../../love/public/src/list_unique.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { text_replace_if_starts_with } from "../../../love/public/src/text_replace_if_starts_with.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { ebible_version_books } from "../../../love/public/src/ebible_version_books.mjs";
 export async function sandbox_3() {
   let books = await ebible_version_books("engbsb");
   let mapped2 = list_map_property(books, "text");
   function lambda(book_name) {
-    let item3 = text_replace_if_starts_with(book_name, "1 ", "");
-    let item4 = text_replace_if_starts_with(item3, "2 ", "");
-    return item4;
+    let item3 = list_map_prefix_without_try_multiple(book_name, ["1 ", "2 "]);
+    return item3;
   }
   let mapped = list_map(mapped2, lambda);
   let unique = list_unique(mapped);
