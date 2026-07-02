5acf83eb554a968bb4c3a8abdcd65b83779af788
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 07:33:42 -0400

    function_auto_path ../love/public/src/list_cycled_shuffle.mjs

--- public/src/list_cycled_shuffle.mjs
+++ public/src/list_cycled_shuffle.mjs
@@ -1,4 +1,5 @@
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 export function list_cycled_shuffle(list) {
-  return list_shuffle(list);
+  let r = list_shuffle(list);
+  return r;
 }
