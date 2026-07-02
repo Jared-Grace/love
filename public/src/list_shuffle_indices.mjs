eec60969b7b8a6f5d441816fbf339eec248e6f8b
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 07:42:02 -0400

    function_auto_path ../love/public/src/list_shuffle_indices.mjs

--- public/src/list_shuffle_indices.mjs
+++ public/src/list_shuffle_indices.mjs
@@ -1,12 +1,15 @@
+import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 import { random } from "../../../love/public/src/random.mjs";
 import { floor } from "../../../love/public/src/floor.mjs";
 export function list_shuffle_indices(list, indices) {
-  function lambda(item) {}
-  each(list2, lambda);
+  function lambda(item) {
+    let r = list_random_item(list2);
+    list_swap_at(list, i, j);
+  }
+  each(indices, lambda);
   for (let i = list.length - 1; i > 0; i--) {
     const j = floor(random() * (i + 1));
-    list_swap_at(list, i, j);
   }
 }
