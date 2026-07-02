174072e8707a3b0abcf4faf2f786c69a28a09b59
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 06:42:47 -0400

    function_auto_path ../love/public/src/list_slices_size_cycle.mjs

--- public/src/list_slices_size_cycle.mjs
+++ public/src/list_slices_size_cycle.mjs
@@ -1,9 +1,8 @@
 import { range_from } from "../../../love/public/src/range_from.mjs";
 import { list_slices_size_generic } from "../../../love/public/src/list_slices_size_generic.mjs";
-import { integer_random } from "../../../love/public/src/integer_random.mjs";
 export function list_slices_size_cycle(list, min, max) {
   let mapped = range_from(min, max);
-  let count_get = integer_random;
+  let count_get = function lambda() {};
   let slices = list_slices_size_generic(count_get, min, max, list);
   return slices;
 }
