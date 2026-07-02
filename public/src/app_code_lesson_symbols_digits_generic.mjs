3f267eed7d39384f3a39aa7c0e9b31ddbbede9ed
JESUS rose to life <username@users.noreply.github.com>
2026-07-01 22:08:26 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_generic.mjs

--- public/src/app_code_lesson_symbols_digits_generic.mjs
+++ public/src/app_code_lesson_symbols_digits_generic.mjs
@@ -63,6 +63,8 @@ export function app_code_lesson_symbols_digits_generic(
         return r5;
       }
       let mapped2 = list_map(digit_counts, lambda);
+      function lambda2(item) {}
+      let mapped = list_map(list, lambda2);
       return mapped2;
     },
   };
