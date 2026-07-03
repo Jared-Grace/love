709c1da991a2e8f5c7718b3ce32a71af668a56d4
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 09:01:21 -0400

    function_auto_path ../love/public/src/boolean_random_n.mjs

--- web/public/src/boolean_random_n.mjs
+++ web/public/src/boolean_random_n.mjs
@@ -1,4 +1,5 @@
 import { integer_random_range } from "../../../love/public/src/integer_random_range.mjs";
 export function boolean_random_n(n) {
-  return integer_random_range(n);
+  let r = integer_random_range(n);
+  return r;
 }
