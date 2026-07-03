6c3841cccfa3326accabd49ccbcbb2181dac7b2d
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:57:02 -0400

    function_auto_path ../love/public/src/html_style_code.mjs

--- public/src/html_style_code.mjs
+++ public/src/html_style_code.mjs
@@ -1,3 +1,4 @@
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
@@ -11,6 +12,7 @@ export function html_style_code(component) {
     component,
     "rgb(210, 232, 255)",
   );
+  html_font_color_set(s, "white");
   html_border_radius_em(component, 0.5);
   html_border_none(component);
   html_style_padding_x(component, "0.37em");
