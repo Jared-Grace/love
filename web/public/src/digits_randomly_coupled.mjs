dd7b9eea10687852ed8fdaf44583c41ab5e005f5
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 09:19:52 -0400

    function_auto_path ../love/public/src/digits_randomly_coupled.mjs

--- web/public/src/digits_randomly_coupled.mjs
+++ web/public/src/digits_randomly_coupled.mjs
@@ -1,14 +1,14 @@
+import { boolean_random } from "../../../love/public/src/boolean_random.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { boolean_random_3 } from "../../../love/public/src/boolean_random_3.mjs";
 import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 export function digits_randomly_coupled() {
   let ds = digits();
   function lambda(la) {
     let chunks = list_chunk(ds, 2);
     function lambda3(c) {
-      let rb2 = boolean_random_3();
+      let rb2 = boolean_random();
       if (rb2) {
         la(c);
       } else {
