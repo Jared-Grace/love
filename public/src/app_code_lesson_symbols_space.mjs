1eb82f872a8551c20a7eaaaa06ef3e05f5e27aa8
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:20:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

--- public/src/app_code_lesson_symbols_space.mjs
+++ public/src/app_code_lesson_symbols_space.mjs
@@ -1,5 +1,5 @@
+import { app_replace_button_symbol_style_box_shadow_value_width } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_width.mjs";
 import { html_box_shadow_set } from "../../../love/public/src/html_box_shadow_set.mjs";
-import { app_replace_button_symbol_style_box_shadow_value } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value.mjs";
 import { html_style_code_generic_unshadowed } from "../../../love/public/src/html_style_code_generic_unshadowed.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { text_space_nb } from "../../../love/public/src/text_space_nb.mjs";
@@ -30,15 +30,18 @@ export function app_code_lesson_symbols_space() {
         function lambda2(component) {
           const color_background = "white";
           const color_font = "transparent";
-          color_box_shadow='black'
+          let color_box_shadow = "black";
           html_style_code_generic_unshadowed(
             component,
             color_background,
             color_font,
           );
-            $x
+          const width = "0.1";
           let style_value =
-            app_replace_button_symbol_style_box_shadow_value(color_box_shadow);
+            app_replace_button_symbol_style_box_shadow_value_width(
+              color_box_shadow,
+              width,
+            );
           html_box_shadow_set(component, style_value);
         },
       ],
