24a91539adb2d016831e7c83655f4001ff505bf5
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:52:18 -0400

    function_auto_path ../love/public/src/html_div_cycle_mono_multiple.mjs

--- public/src/html_div_cycle_mono_multiple.mjs
+++ public/src/html_div_cycle_mono_multiple.mjs
@@ -1,4 +1,5 @@
 import { html_cycle_mono_multiple } from "../../../love/public/src/html_cycle_mono_multiple.mjs";
 export function html_div_cycle_mono_multiple(parent, list) {
-  return html_cycle_mono_multiple(parent, list);
+  let mapped = html_cycle_mono_multiple(parent, list);
+  return mapped;
 }
