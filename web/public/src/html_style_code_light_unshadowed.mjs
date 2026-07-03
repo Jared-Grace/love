97c86ada5103486f61e6daca2193ce32210a72ec
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:14:55 -0400

    c c html_style_code_light,unshadowed

---
+++ web/public/src/html_style_code_light_unshadowed.mjs
@@ -0,0 +1,5 @@
+import { html_style_code_light_generic } from "../../../love/public/src/html_style_code_light_generic.mjs";
+export function html_style_code_light_unshadowed(component) {
+  const color_box_shadow = "white";
+  html_style_code_light_generic(component, color_box_shadow);
+}
