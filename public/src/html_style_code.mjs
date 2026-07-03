9a7bdef592bdba45ee8d23a1e827c2c69498d51e
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:59:43 -0400

    function_auto_path ../love/public/src/html_style_code.mjs

--- public/src/html_style_code.mjs
+++ public/src/html_style_code.mjs
@@ -7,7 +7,7 @@ import { html_style_background_color_set } from "../../../love/public/src/html_s
 import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 export function html_style_code(component) {
   html_font_jetbrains_mono(component);
-  html_style_background_color_set(component, "rgb(68, 68, 68)");
+  html_style_background_color_set(component, "black");
   app_replace_button_symbol_style_box_shadow_value_set(component, "black");
   html_font_color_set(component, "white");
   html_border_radius_em(component, 0.5);
