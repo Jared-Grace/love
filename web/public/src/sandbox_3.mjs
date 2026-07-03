ffb4ea3985bb5430a4a904a183667cf1eb272593
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 07:06:02 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,5 +1,5 @@
-import { list_multiply } from "../../../love/public/src/list_multiply.mjs";
+import { object_multiply } from "../../../love/public/src/object_multiply.mjs";
 export async function sandbox_3() {
-  let multiplied = list_multiply([1, 2], 3);
+  let multiplied = object_multiply([1, 2], 3);
   return multiplied;
 }
