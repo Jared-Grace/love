80af9002688c81174985e58ba7ab740f03ffd555
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 09:18:41 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first.mjs

---
+++ web/public/src/digits_randomly_coupled.mjs
@@ -0,0 +1,23 @@
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+import { boolean_random_3 } from "../../../love/public/src/boolean_random_3.mjs";
+import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
+export function digits_randomly_coupled() {
+  function lambda(la) {
+    let chunks = list_chunk(ds, 2);
+    function lambda3(c) {
+      let rb2 = boolean_random_3();
+      if (rb2) {
+        la(c);
+      } else {
+        function lambda4(d) {
+          la([d]);
+        }
+        each(c, lambda4);
+      }
+    }
+    each(chunks, lambda3);
+  }
+  let list2 = list_adder(lambda);
+  return list2;
+}
