86c41f466726f791e3ab8a2bf55de711ea0cd205
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:23:23 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

--- web/public/src/app_code_lesson_symbols_space.mjs
+++ web/public/src/app_code_lesson_symbols_space.mjs
@@ -1,3 +1,4 @@
+import { app_code_container_light_blue_border_color } from "../../../love/public/src/app_code_container_light_blue_border_color.mjs";
 import { app_replace_button_symbol_style_box_shadow_value_width } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_width.mjs";
 import { html_box_shadow_set } from "../../../love/public/src/html_box_shadow_set.mjs";
 import { html_style_code_generic_unshadowed } from "../../../love/public/src/html_style_code_generic_unshadowed.mjs";
@@ -30,7 +31,7 @@ export function app_code_lesson_symbols_space() {
         function lambda2(component) {
           const color_background = "white";
           const color_font = "transparent";
-          let color_box_shadow = "rgb(0, 123, 255)";
+          let color_box_shadow = app_code_container_light_blue_border_color();
           html_style_code_generic_unshadowed(
             component,
             color_background,
