d138e9a450cfbe0a285a1f4dd256400986d9f796
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 07:01:54 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores_generic.mjs

--- web/public/src/property_transform.mjs
+++ web/public/src/property_transform.mjs
@@ -1,8 +1,8 @@
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function property_transform(o, property, lambda$value) {
-  let value = property_get(o, property);
-  value = lambda$value(value);
-  property_set(o, property, value);
-  return value;
+  let transformed = property_get(o, property);
+  transformed = lambda$value(transformed);
+  property_set(o, property, transformed);
+  return transformed;
 }
