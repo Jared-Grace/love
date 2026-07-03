4cd8a932153182af6b99a748224a02fffb12f2cd
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 22:22:41 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_letters_spaces_2.mjs

--- public/src/app_code_lesson_identifiers_letters_spaces_2.mjs
+++ public/src/app_code_lesson_identifiers_letters_spaces_2.mjs
@@ -1,10 +1,10 @@
+import { html_cycle_mono } from "../../../love/public/src/html_cycle_mono.mjs";
 import { js_identifier_words_invalid } from "../../../love/public/src/js_identifier_words_invalid.mjs";
 import { list_difference } from "../../../love/public/src/list_difference.mjs";
 import { list_slices_size_cycles_shuffled } from "../../../love/public/src/list_slices_size_cycles_shuffled.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_identifiers_valid } from "../../../love/public/src/app_code_lesson_symbols_identifiers_valid.mjs";
 import { html_div_text_multiple } from "../../../love/public/src/html_div_text_multiple.mjs";
-import { html_div_cycle_bold } from "../../../love/public/src/html_div_cycle_bold.mjs";
 import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_code_verse_words } from "../../../love/public/src/app_code_verse_words.mjs";
@@ -15,7 +15,7 @@ export function app_code_lesson_identifiers_letters_spaces_2() {
     html_div_text_multiple(c, [
       "Remember, identifiers can have different kinds of symbols including letter symbols",
     ]);
-    html_div_cycle_bold(c, [
+    html_cycle_mono(c, [
       "Also, identifiers can have a ",
       "$",
       "(dollar sign) or an ",
