0fee9dfad60299e97190f494e1595862c98dbc47
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 07:12:52 -0400

    function_auto_path ../love/public/src/list_swap_first.mjs

--- public/src/list_swap_first.mjs
+++ public/src/list_swap_first.mjs
@@ -1,6 +1,6 @@
 import { list_swap } from "../../../love/public/src/list_swap.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
-export function list_swap_first(list, index_symbol) {
+export function list_swap_first(list, item) {
   let first = list_first(list);
-  list_swap(list, first, index_symbol);
+  list_swap(list, first, item);
 }
