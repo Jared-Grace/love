3d2a869a8594cfd4c73ca98cf58c0111659cba92
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 08:45:52 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first.mjs

---
+++ web/public/src/list_transform_at.mjs
@@ -0,0 +1,4 @@
+import { property_transform } from "../../../love/public/src/property_transform.mjs";
+export function list_transform_at(multiplied, multiplied_index, lambda) {
+  property_transform(multiplied, multiplied_index, lambda);
+}
