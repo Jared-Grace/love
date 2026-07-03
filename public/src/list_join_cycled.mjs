fa9fd6abc9ae1f7d56a079796dfeb69e5e05bb6f
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:35:40 -0400

    function_auto_path ../love/public/src/list_join_cycled.mjs

--- public/src/list_join_cycled.mjs
+++ public/src/list_join_cycled.mjs
@@ -1,4 +1,5 @@
 import { list_join } from "../../../love/public/src/list_join.mjs";
 export function list_join_cycled(list, separator) {
-  return list_join(list, separator);
+  let joined = list_join(list, separator);
+  return joined;
 }
