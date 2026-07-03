121f54fe3343da031d37b1cc4ae593fc05485845
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:51:41 -0400

    function_auto_path ../love/public/src/list_to_text_and_list.mjs

--- public/src/list_to_text_and_list.mjs
+++ public/src/list_to_text_and_list.mjs
@@ -1,6 +1,6 @@
 import { list_to_text_or_list_generic } from "../../../love/public/src/list_to_text_or_list_generic.mjs";
 export function list_to_text_and_list(list) {
-  const word_relationship = "or";
+  const word_relationship = "and";
   let combined = list_to_text_or_list_generic(list, word_relationship);
   return combined;
 }
