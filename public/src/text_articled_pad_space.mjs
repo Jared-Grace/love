2ae4cd0e9a7b0b48ac35cace0da83aa0b7788ef4
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:49:06 -0400

    function_auto_path ../love/public/src/text_articled_pad_space.mjs

--- public/src/text_articled_pad_space.mjs
+++ public/src/text_articled_pad_space.mjs
@@ -1,6 +1,7 @@
+import { text_pad_space } from "../../../love/public/src/text_pad_space.mjs";
 import { text_articled } from "../../../love/public/src/text_articled.mjs";
 export function text_articled_pad_space(noun) {
   let articled = text_articled(noun);
-  pad_space;
+  let padded = text_pad_space(s);
   return articled;
 }
