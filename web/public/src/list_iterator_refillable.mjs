37007b2394e044b222b0404f4f653dc7ba90f480
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 09:26:02 -0400

    function_auto_path ../love/public/src/list_iterator_refillable.mjs

--- web/public/src/list_iterator_refillable.mjs
+++ web/public/src/list_iterator_refillable.mjs
@@ -1,14 +1,14 @@
 import { list_remove_first } from "../../../love/public/src/list_remove_first.mjs";
 import { list_add_multiple } from "../../../love/public/src/list_add_multiple.mjs";
 import { list_empty_is } from "../../../love/public/src/list_empty_is.mjs";
-export function list_iterator_refillable(on_batch_item, on_batch) {
+export function list_iterator_refillable(refill_get, on_refill) {
   let remaining = [];
   let refresh = function lambda() {
     let e = list_empty_is(remaining);
     if (e) {
-      let items = batch();
+      let items = refill_get();
       list_add_multiple(remaining, items);
-      on_batch(remaining);
+      on_refill(remaining);
     }
     let b = list_remove_first(remaining);
     return b;
