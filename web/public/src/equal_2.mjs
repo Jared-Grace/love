79595778ee921fee98b4adbd18891ea9190cf048
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:44:22 -0400

    function_auto_path ../love/public/src/equal_2.mjs

--- web/public/src/equal_2.mjs
+++ web/public/src/equal_2.mjs
@@ -1,5 +1,5 @@
 import { equal } from "../../../love/public/src/equal.mjs";
-export function equal_2(size) {
-  let eq = equal(size, 2);
+export function equal_2(value) {
+  let eq = equal(value, 2);
   return eq;
 }
