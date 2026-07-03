db35dcb3bae8f01733752d74a0556d8d15cdc5ed
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 06:42:45 -0400

    function_auto_path ../love/public/src/text_multiply.mjs

--- public/src/text_multiply.mjs
+++ public/src/text_multiply.mjs
@@ -1,7 +1,8 @@
-import { list_add } from "../../../love/public/src/list_add.mjs";
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
 export function text_multiply(value, times) {
-  list_add(list, item);
+  function lambda(la) {}
+  let list = list_adder(lambda);
   let result = "";
   function lambda4(i) {
     result += value;
