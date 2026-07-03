2d9e26947dd4e785b1a288d35123db6cd4ea605c
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:22:29 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores.mjs

---
+++ web/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
@@ -0,0 +1,25 @@
+import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
+import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
+import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
+import { html_bold } from "../../../love/public/src/html_bold.mjs";
+import { noop } from "../../../love/public/src/noop.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
+export function app_code_lesson_identifiers_underscores_define_symbol(
+  c,
+  separator_valid_name,
+  separator_valid,
+) {
+  let div3 = html_div(c);
+  let cycles = [
+    noop,
+    html_bold,
+    html_font_jetbrains_mono,
+    html_style_code_dark,
+  ];
+  html_cycle(div3, cycles, [
+    " This is an ",
+    separator_valid_name,
+    ": ",
+    separator_valid,
+  ]);
+}
