edb9faaa8f8853c855245eab8a52284250db5f6c
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:30:33 -0400

    function_auto_path ../love/public/src/text_articled.mjs

--- public/src/text_articled.mjs
+++ public/src/text_articled.mjs
@@ -1,3 +1,4 @@
+import { text_vowels } from "../../../love/public/src/text_vowels.mjs";
 import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { text_upper_to } from "../../../love/public/src/text_upper_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
@@ -5,7 +6,8 @@ import { list_any_starts_with } from "../../../love/public/src/list_any_starts_w
 import { list_to } from "../../../love/public/src/list_to.mjs";
 export function text_articled(noun) {
   let article = "a";
-  let vowels_lower = list_to("aeiou");
+  let a = text_vowels();
+  let vowels_lower = list_to(a);
   let vowels_upper = list_map(list, text_upper_to);
   let vowels = list_concat(vowels_lower, vowels_upper);
   let any = list_any_starts_with(noun, vowels);
