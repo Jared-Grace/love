f29099c3d4a0fbf63afa9c93a9d9c66bcdb5ba2e
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 06:15:05 -0400

    function_auto_path ../love/public/src/app_code_next.mjs

--- public/src/app_code_next.mjs
+++ public/src/app_code_next.mjs
@@ -1,9 +1,9 @@
+import { text_pad_space_quote_double } from "../../../love/public/src/text_pad_space_quote_double.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { emoji_repeat_1 } from "../../../love/public/src/emoji_repeat_1.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
-import { text_pad_nested_space_quote_double } from "../../../love/public/src/text_pad_nested_space_quote_double.mjs";
 import { app_karate_button_next_text } from "../../../love/public/src/app_karate_button_next_text.mjs";
 export function app_code_next(
   context,
@@ -15,7 +15,7 @@ export function app_code_next(
 ) {
   let container = app_code_container_light_blue(parent);
   let nt = app_karate_button_next_text();
-  let padded3 = text_pad_nested_space_quote_double(nt);
+  let padded3 = text_pad_space_quote_double(nt);
   let combined3 = text_combine_multiple([
     "Do you want to ",
     do_you_want_to_text,
