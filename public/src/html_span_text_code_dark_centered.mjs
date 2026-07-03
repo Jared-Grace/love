df243a1ef10fc853b68280731e6df03738648c86
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:23:50 -0400

    function_auto_path ../love/public/src/html_span_text_code_dark_centered.mjs

--- public/src/html_span_text_code_dark_centered.mjs
+++ public/src/html_span_text_code_dark_centered.mjs
@@ -1,7 +1,7 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { html_span_text_code_dark } from "../../../love/public/src/html_span_text_code_dark.mjs";
-export function html_span_text_code_dark_centered(div3, separator_invalid) {
-  let d2 = html_span_text_code_dark(div3, separator_invalid);
+export function html_span_text_code_dark_centered(parent, text) {
+  let d2 = html_span_text_code_dark(parent, text);
   html_style_assign(d2, {
     "align-self": "center",
   });
