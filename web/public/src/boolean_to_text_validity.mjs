4186ad1ee3e9ddec00bb313527cee7a3123df36f
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 04:58:13 -0400

    function_auto_path ../love/public/src/boolean_to_text_validity.mjs

--- web/public/src/boolean_to_text_validity.mjs
+++ web/public/src/boolean_to_text_validity.mjs
@@ -1,5 +1,6 @@
+import { emoji_check } from "../../../love/public/src/emoji_check.mjs";
 import { ternary } from "../../../love/public/src/ternary.mjs";
 export function boolean_to_text_validity(b) {
-  let v = ternary(b, "valid", "invalid");
+  let v = ternary(b, emoji_check() + " valid", "invalid");
   return v;
 }
