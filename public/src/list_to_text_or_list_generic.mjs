a1a29a1d6e867f4eaf13a747230fe3b77f403944
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 06:17:22 -0400

    function_auto_path ../love/public/src/list_to_text_or_list_generic.mjs

--- public/src/list_to_text_or_list_generic.mjs
+++ public/src/list_to_text_or_list_generic.mjs
@@ -1,3 +1,4 @@
+import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
@@ -6,6 +7,7 @@ export function list_to_text_or_list_generic(list, word_relationship) {
   let taken = list_take_less_1(list);
   let last = list_last(list);
   let joined = list_join_comma_space(taken);
+  let ne = list_empty_not_is(list2);
   let combined = text_combine_multiple([
     joined,
     " ",
