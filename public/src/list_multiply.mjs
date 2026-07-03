adb85f76e13a864c8cf92f3e51c13371eb4e1780
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 06:48:32 -0400

    function_auto_path ../love/public/src/list_multiply.mjs

--- public/src/list_multiply.mjs
+++ public/src/list_multiply.mjs
@@ -1,7 +1,7 @@
+import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { object_multiply } from "../../../love/public/src/object_multiply.mjs";
-import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 export function list_multiply(value, times) {
   let list = object_multiply(value, times);
-  let joined = list_join_empty(list);
+  let joined = list_concat(list);
   return joined;
 }
