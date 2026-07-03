55dbba2607687fe8a7cd74da04d0f09187068563
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:36:58 -0400

    function_auto_path ../love/public/src/list_join_cycled.mjs

--- public/src/list_join_cycled.mjs
+++ public/src/list_join_cycled.mjs
@@ -1,7 +1,10 @@
+import { list_get_wrap } from "../../../love/public/src/list_get_wrap.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function list_join_cycled(list, list_separators) {
-  function lambda(la) {}
+  function lambda(la) {
+    let item = list_get_wrap(cycles, index);
+  }
   let list2 = list_adder(lambda);
   let joined = list_join_empty(list2);
   return joined;
