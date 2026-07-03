84ea5b16341a7f1a77fb5e4034e7765b885e5e38
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:58:42 -0400

    function_auto_path ../love/public/src/html_style_code.mjs

--- web/public/src/html_style_code.mjs
+++ web/public/src/html_style_code.mjs
@@ -2,11 +2,13 @@ import { html_font_color_set } from "../../../love/public/src/html_font_color_se
 import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
+import { app_replace_button_symbol_style_box_shadow_value_set } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs";
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 export function html_style_code(component) {
   html_font_jetbrains_mono(component);
-  html_style_background_color_set(component, "black");
+  html_style_background_color_set(component, "rgb(68, 68, 68)");
+  app_replace_button_symbol_style_box_shadow_value_set(component, "black");
   html_font_color_set(component, "white");
   html_border_radius_em(component, 0.5);
   html_border_none(component);
