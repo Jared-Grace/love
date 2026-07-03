9af2832cc6544ed24a95e36826e7c5cbe6dff317
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:40:03 -0400

    function_auto_path ../love/public/src/app_code_lessons_fns.mjs

--- web/public/src/app_code_lesson_identifiers_underscores_2.mjs
+++ web/public/src/app_code_lesson_identifiers_underscores_2.mjs
@@ -25,29 +25,5 @@ export function app_code_lesson_identifiers_underscores_2() {
     separator_valid,
     defines_after,
   );
-  function defines_after(root) {
-    let c4 = app_code_container_light_blue(root);
-    html_div_text(c4, "How they look different:");
-    let div3 = html_div(c4);
-    html_style_assign(div3, {
-      display: "grid",
-      "grid-template-columns": "auto 1fr",
-    });
-    app_code_flex_gap(div3);
-    html_span_text_code_dark_centered(div3, separator_invalid);
-    html_div_text(
-      div3,
-      "The line of" +
-        text_articled_pad_space(separator_invalid_name) +
-        "is shorter and is near the middle vertically, not the bottom or the top",
-    );
-    let d = html_span_text_code_dark_centered(div3, separator_valid);
-    html_div_text(
-      div3,
-      "The line of" +
-        text_articled_pad_space(separator_valid_name) +
-        "is longer and is near the bottom",
-    );
-  }
   return r;
 }
