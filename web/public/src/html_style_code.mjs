698ebbd6b73450d0864ed350e2b3bd702107f0cb
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:52:57 -0400

    function_auto_path ../love/public/src/html_style_code.mjs

--- web/public/src/html_style_code.mjs
+++ web/public/src/html_style_code.mjs
@@ -1,4 +1,4 @@
-import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 import { app_replace_button_symbol_style_box_shadow_value_set } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs";
@@ -10,7 +10,5 @@ export function html_style_code(component) {
   app_replace_button_symbol_style_box_shadow_value_set(component, "black");
   html_border_radius_em(component, 0.5);
   html_border_none(component);
-  html_style_assign(component, {
-    "min-width": "1ch",
-  });
+  html_style_padding_x(component, "0.37em");
 }
