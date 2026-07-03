435640412e50e659cf0c82e344447a9a79bfb36d
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 04:57:49 -0400

    function_auto_path ../love/public/src/boolean_to_text_validity.mjs

--- web/public/src/boolean_to_text_validity.mjs
+++ web/public/src/boolean_to_text_validity.mjs
@@ -1,5 +1,5 @@
 import { ternary } from "../../../love/public/src/ternary.mjs";
 export function boolean_to_text_validity(b) {
-  let result2 = ternary(b, "valid", "invalid");
-  return result2;
+  let v = ternary(b, "valid", "invalid");
+  return v;
 }
