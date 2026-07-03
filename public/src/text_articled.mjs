3e3b66838ce8dc9c7051e712b90cf4a649c8f2b3
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:31:18 -0400

    function_auto_path ../love/public/src/text_articled.mjs

--- public/src/text_articled.mjs
+++ public/src/text_articled.mjs
@@ -1,15 +1,8 @@
-import { text_vowels_lower } from "../../../love/public/src/text_vowels_lower.mjs";
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
-import { text_upper_to } from "../../../love/public/src/text_upper_to.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
+import { list_vowels } from "../../../love/public/src/list_vowels.mjs";
 import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
-import { list_to } from "../../../love/public/src/list_to.mjs";
 export function text_articled(noun) {
   let article = "a";
-  let a = text_vowels_lower();
-  let vowels_lower = list_to(a);
-  let vowels_upper = list_map(list, text_upper_to);
-  let vowels = list_concat(vowels_lower, vowels_upper);
+  let vowels = list_vowels();
   let any = list_any_starts_with(noun, vowels);
   if (any) {
     article += "n";
