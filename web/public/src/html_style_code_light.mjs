2a5b8ea5e62eb86fc2445a284fadfae1a73b3d33
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:11:08 -0400

    function_auto_path ../love/public/src/html_style_code_light.mjs

--- web/public/src/html_style_code_light.mjs
+++ web/public/src/html_style_code_light.mjs
@@ -1,8 +1,8 @@
 import { html_style_code_generic } from "../../../love/public/src/html_style_code_generic.mjs";
 export function html_style_code_light(component) {
-  const color_background = "black";
-  const color_box_shadow = "black";
-  const color_font = "white";
+  const color_background = "white";
+  const color_box_shadow = "white";
+  const color_font = "black";
   html_style_code_generic(
     component,
     color_background,
