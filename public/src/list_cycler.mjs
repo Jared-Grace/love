3f1cd3a4886f098b47eb2aef4d6627ba3c8a4bc2
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:48:13 -0400

    function_auto_path ../love/public/src/list_cycler.mjs

--- public/src/list_cycler.mjs
+++ public/src/list_cycler.mjs
@@ -1,3 +1,11 @@
+import { list_get } from "../../../love/public/src/list_get.mjs";
+import { list_get_wrap_index } from "../../../love/public/src/list_get_wrap_index.mjs";
 export function list_cycler(list) {
   let i = 0;
+  let next = function lambda() {
+    i = list_get_wrap_index(list, i);
+    let item = list_get(list, i);
+    i++;
+    return item;
+  };
 }
