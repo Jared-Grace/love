8ac86809dd874c49175289a0a95a588474d25cf2
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:29:35 -0400

    function_auto_path ../love/public/src/text_articled.mjs

--- public/src/text_articled.mjs
+++ public/src/text_articled.mjs
@@ -1,10 +1,11 @@
-import { log_unparse_try } from "../../../love/public/src/log_unparse_try.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
 import { list_to } from "../../../love/public/src/list_to.mjs";
 export function text_articled(noun) {
   let article = "a";
   let vowels_lower = list_to("aeiou");
-  log_unparse_try(right);
+  function lambda(item) {}
+  let mapped = list_map(list, lambda);
   let any = list_any_starts_with(noun, vowels_lower);
   if (any) {
     article += "n";
