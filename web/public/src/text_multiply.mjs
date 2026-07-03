b72e1fea2e444f9b486b39d2dec5fb67243b8906
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 06:43:13 -0400

    function_auto_path ../love/public/src/text_multiply.mjs

--- web/public/src/text_multiply.mjs
+++ web/public/src/text_multiply.mjs
@@ -1,12 +1,14 @@
+import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
 export function text_multiply(value, times) {
-  function lambda(la) {}
-  let list = list_adder(lambda);
-  let result = "";
-  function lambda4(i) {
-    result += value;
+  function lambda(la) {
+    function lambda4(i) {
+      la(value);
+    }
+    each_range(times, lambda4);
   }
-  each_range(times, lambda4);
+  let list = list_adder(lambda);
+  let joined = list_join_empty(list2);
   return result;
 }
