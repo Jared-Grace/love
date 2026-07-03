dea56ffc86b609354b38daa950d4c3c6899e7e84
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:54:38 -0400

    function_auto_path ../love/public/src/html_div_cycle_mono_multiple.mjs

--- public/src/html_div_cycle_mono_multiple.mjs
+++ public/src/html_div_cycle_mono_multiple.mjs
@@ -1,7 +1,10 @@
-import { html_div } from "../../../love/public/src/html_div.mjs";
-import { html_cycle_mono_multiple } from "../../../love/public/src/html_cycle_mono_multiple.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { html_div_cycle_mono } from "../../../love/public/src/html_div_cycle_mono.mjs";
 export function html_div_cycle_mono_multiple(parent, list) {
-  let div = html_div(parent);
-  let mapped = html_cycle_mono_multiple(div, list);
+  function lambda(parts) {
+    let r = html_div_cycle_mono(parent, parts);
+    return r;
+  }
+  let mapped = list_map(list, lambda);
   return mapped;
 }
