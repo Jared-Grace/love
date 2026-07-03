dc5d502907a3483a60c36f83e697a80ac2d88eb3
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:18:47 -0400

    function_auto_path ../love/public/src/html_style_code_generic.mjs

---
+++ public/src/html_style_code_generic_unshadowed.mjs
@@ -0,0 +1,18 @@
+import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
+import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
+import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
+import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
+export function html_style_code_generic_unshadowed(
+  component,
+  color_background,
+  color_font,
+) {
+  html_font_jetbrains_mono(component);
+  html_style_background_color_set(component, color_background);
+  html_font_color_set(component, color_font);
+  html_border_radius_em(component, 0.5);
+  html_border_none(component);
+  html_style_padding_x(component, "0.37em");
+}
