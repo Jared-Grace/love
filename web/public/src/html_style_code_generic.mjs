cb83fbd6205b525fa1b781292a86d03a8c9a646b
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:18:39 -0400

    function_auto_path ../love/public/src/html_style_code_generic.mjs

--- web/public/src/html_style_code_generic.mjs
+++ web/public/src/html_style_code_generic.mjs
@@ -13,12 +13,12 @@ export function html_style_code_generic(
 ) {
   html_font_jetbrains_mono(component);
   html_style_background_color_set(component, color_background);
-  app_replace_button_symbol_style_box_shadow_value_set(
-    component,
-    color_box_shadow,
-  );
   html_font_color_set(component, color_font);
   html_border_radius_em(component, 0.5);
   html_border_none(component);
   html_style_padding_x(component, "0.37em");
+  app_replace_button_symbol_style_box_shadow_value_set(
+    component,
+    color_box_shadow,
+  );
 }
