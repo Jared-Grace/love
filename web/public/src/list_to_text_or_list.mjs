c390c7d917cec5f7cd26e66199761e0f615a454b
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:51:25 -0400

    function_auto_path ../love/public/src/list_to_text_or_list.mjs

--- web/public/src/list_to_text_or_list.mjs
+++ web/public/src/list_to_text_or_list.mjs
@@ -1,18 +1,6 @@
-import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
-import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
-import { list_last } from "../../../love/public/src/list_last.mjs";
-import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
+import { list_to_text_or_list_generic } from "../../../love/public/src/list_to_text_or_list_generic.mjs";
 export function list_to_text_or_list(list) {
   const word_relationship = "or";
-  let taken = list_take_less_1(list);
-  let last = list_last(list);
-  let joined = list_join_comma_space(taken);
-  let combined = text_combine_multiple([
-    joined,
-    " ",
-    word_relationship,
-    " ",
-    last,
-  ]);
+  let combined = list_to_text_or_list_generic(list, word_relationship);
   return combined;
 }
---
+++ web/public/src/list_to_text_or_list_generic.mjs
@@ -0,0 +1,17 @@
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
+import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
+import { list_last } from "../../../love/public/src/list_last.mjs";
+import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
+export function list_to_text_or_list_generic(list, word_relationship) {
+  let taken = list_take_less_1(list);
+  let last = list_last(list);
+  let joined = list_join_comma_space(taken);
+  let combined = text_combine_multiple([
+    joined,
+    " ",
+    word_relationship,
+    " ",
+    last,
+  ]);
+  return combined;
+}
