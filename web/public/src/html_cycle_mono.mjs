d5f3ad88a2a16b5a3346c4606db499f566c91020
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:44:33 -0400

    function_auto_path ../love/public/src/html_cycle_mono.mjs

--- web/public/src/html_cycle_mono.mjs
+++ web/public/src/html_cycle_mono.mjs
@@ -1,7 +1,7 @@
-import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
+import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
 export function html_cycle_mono(parent, parts) {
-  let cycles = [noop, html_font_jetbrains_mono];
+  let cycles = [noop, html_style_code_dark];
   html_cycle(parent, cycles, parts);
 }
