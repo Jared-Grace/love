9de5ca1e853749d2f84e4bef0bf515a7447cc5ba
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:28:21 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs

--- public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
+++ public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
@@ -1,7 +1,6 @@
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
-import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
@@ -12,13 +11,7 @@ export function app_code_lesson_identifiers_underscores_define_symbol(
   text_after,
 ) {
   let div3 = html_div(c);
-  let cycles = [
-    noop,
-    html_bold,
-    noop,
-    html_font_jetbrains_mono,
-    html_style_code_dark,
-  ];
+  let cycles = [noop, html_bold, noop, html_style_code_dark];
   let combined = text_combine(" ", text_after);
   html_cycle(div3, cycles, [
     " This is an ",
