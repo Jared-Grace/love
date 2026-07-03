7861521aad6e21a5ca5465a6cd1656cf59e4097d
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:59:42 -0400

    function_auto_path ../love/public/src/boolean_random.mjs

--- web/public/src/boolean_random.mjs
+++ web/public/src/boolean_random.mjs
@@ -1,6 +1,6 @@
 import { random } from "../../../love/public/src/random.mjs";
 export function boolean_random() {
   let rnd = random();
-  let r = rnd > 1 / 2;
-  return r;
+  let rb = rnd > 1 / 2;
+  return rb;
 }
