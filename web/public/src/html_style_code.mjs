235ec8d97f0f7c2727b0a1367acf72a907afc562
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:55:56 -0400

    function_auto_path ../love/public/src/html_style_code.mjs

--- web/public/src/html_style_code.mjs
+++ web/public/src/html_style_code.mjs
@@ -1,17 +1,16 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+import { app_replace_button_symbol_style_box_shadow_value } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value.mjs";
 import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { html_border_none } from "../../../love/public/src/html_border_none.mjs";
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
-import { app_replace_button_symbol_style_box_shadow_value_set } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs";
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 export function html_style_code(component) {
   html_font_jetbrains_mono(component);
   html_style_background_color_set(component, "white");
-  const color = "rgb(167, 211, 255)";$x
-  app_replace_button_symbol_style_box_shadow_value_set(
-    component,
-    color,
-  );
+  const color = "rgb(167, 211, 255)";
+  let style_value = app_replace_button_symbol_style_box_shadow_value(color);
+  html_style_set(component, "box-shadow", style_value);
   html_border_radius_em(component, 0.5);
   html_border_none(component);
   html_style_padding_x(component, "0.37em");
