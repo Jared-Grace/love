43f7ce414de5e9818949c0acc8c0dc8e79acb599
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 06:57:08 -0400

    function_auto_path ../love/public/src/text_multiply.mjs

--- public/src/list_multiply.mjs
+++ public/src/list_multiply.mjs
@@ -2,6 +2,6 @@ import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { object_multiply } from "../../../love/public/src/object_multiply.mjs";
 export function list_multiply(value, times) {
   let list = object_multiply(value, times);
-  let joined = list_concat(list);
-  return joined;
+  let multiplied = list_concat(list);
+  return multiplied;
 }
