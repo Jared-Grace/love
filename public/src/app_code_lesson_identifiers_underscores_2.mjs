051dd9bf2e96d41b35be369c6f42d44ffb639bb1
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:43:22 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores_2.mjs

--- public/src/app_code_lesson_identifiers_underscores_2.mjs
+++ public/src/app_code_lesson_identifiers_underscores_2.mjs
@@ -1,17 +1,10 @@
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_identifiers_underscores_generic } from "../../../love/public/src/app_code_lesson_identifiers_underscores_generic.mjs";
-import { html_div_cycle_mono_multiple } from "../../../love/public/src/html_div_cycle_mono_multiple.mjs";
 export function app_code_lesson_identifiers_underscores_2() {
-  if (false) {
-    html_div_cycle_mono_multiple(c, [
-      ["Identifiers can also have a ", "$", " (dollar sign)"],
-      ["But identifiers cannot have a ", ".", " (period) symbol"],
-    ]);
-  }
-  const separator_valid = "_";
-  let separator_valid_name = "underscore";
-  const separator_invalid = "-";
-  let separator_invalid_name = "hyphen";
+  const separator_valid = "$";
+  let separator_valid_name = "dollar sign";
+  const separator_invalid = ".";
+  let separator_invalid_name = "period";
   let r = app_code_lesson_identifiers_underscores_generic(
     separator_invalid_name,
     separator_invalid,
