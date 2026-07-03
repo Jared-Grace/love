fa76e679f86593ae41954041cc293d2a9165ccf2
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 07:13:50 -0400

    function_auto_path ../love/public/src/list_swap_beginning.mjs

--- public/src/list_swap_beginning.mjs
+++ public/src/list_swap_beginning.mjs
@@ -1,6 +1,6 @@
 import { list_swap_at } from "../../../love/public/src/list_swap_at.mjs";
 export function list_swap_beginning(list) {
-  let index_last = 0;
-  let index_last_second = 1;
-  list_swap_at(list, index_last, index_last_second);
+  let index_first = 0;
+  let index_second = 1;
+  list_swap_at(list, index_first, index_second);
 }
