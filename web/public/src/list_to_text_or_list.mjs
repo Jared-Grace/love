26c117777da9822dd7a0b244f260b87a0e69e91b
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:51:06 -0400

    function_auto_path ../love/public/src/list_to_text_or_list.mjs

--- web/public/src/list_to_text_or_list.mjs
+++ web/public/src/list_to_text_or_list.mjs
@@ -3,9 +3,16 @@ import { list_join_comma_space } from "../../../love/public/src/list_join_comma_
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
 export function list_to_text_or_list(list) {
+  const word_relationship = "or";
   let taken = list_take_less_1(list);
   let last = list_last(list);
   let joined = list_join_comma_space(taken);
-  let combined = text_combine_multiple([joined, " ", "or", " ", last]);
+  let combined = text_combine_multiple([
+    joined,
+    " ",
+    word_relationship,
+    " ",
+    last,
+  ]);
   return combined;
 }
