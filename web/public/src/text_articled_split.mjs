3485bac4018ff6044d972c059c28ed53359be8d7
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 05:45:39 -0400

    function_auto_path ../love/public/src/text_articled_split.mjs

--- web/public/src/text_articled_split.mjs
+++ web/public/src/text_articled_split.mjs
@@ -1,9 +1,9 @@
-import { list_first_second_only } from "../../../love/public/src/list_first_second_only.mjs";
+import { list_first_second } from "../../../love/public/src/list_first_second.mjs";
 import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { text_articled } from "../../../love/public/src/text_articled.mjs";
 export function text_articled_split(separator_valid_name) {
   let articled = text_articled(separator_valid_name);
   let split = text_split_space(articled);
-  let r = list_first_second_only(split);
+  let r = list_first_second(split);
   return r;
 }
