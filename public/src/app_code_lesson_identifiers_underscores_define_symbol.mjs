fa3a31e474be4e28c0b6fd245547431a272281cf
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 04:21:15 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs

--- public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
+++ public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
@@ -1,7 +1,5 @@
 import { text_articled_split } from "../../../love/public/src/text_articled_split.mjs";
-import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
@@ -24,6 +22,4 @@ export function app_code_lesson_identifiers_underscores_define_symbol(
     ": ",
     separator_valid,
   ]);
-  let combined = text_combine(" ", text_after);
-  let div = html_div_text(c, combined);
 }
