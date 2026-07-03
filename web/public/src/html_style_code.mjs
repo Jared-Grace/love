1dede0e6e706308f2d6965bb6432575c644032c5
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:44:45 -0400

    function_auto_path ../love/public/src/html_style_code.mjs

--- web/public/src/html_style_code.mjs
+++ web/public/src/html_style_code.mjs
@@ -1,5 +1,4 @@
 import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 export function html_style_code(component) {
-  let r = html_font_jetbrains_mono(component);
-  return r;
+  html_font_jetbrains_mono(component);
 }
