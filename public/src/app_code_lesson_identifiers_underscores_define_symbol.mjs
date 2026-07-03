9ba8951fb5447a3bd4795fb3c99fce58a197fc1c
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 03:34:20 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs

--- public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
+++ public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
@@ -1,3 +1,4 @@
+import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { text_articled } from "../../../love/public/src/text_articled.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
@@ -15,6 +16,7 @@ export function app_code_lesson_identifiers_underscores_define_symbol(
   let cycles = [noop, html_bold, noop, html_style_code_dark];
   let combined = text_combine(" ", text_after);
   let articled = text_articled(separator_valid_name);
+  let split = text_split_space(s);
   html_cycle(div3, cycles, [
     " This is ",
     articled,
