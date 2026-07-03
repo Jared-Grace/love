e7f5f81471576b884cc76556f68bde6a9c3112a3
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 01:44:26 -0400

    w r html_font_jetbrains_mono,html_style_code

---
+++ web/public/src/html_style_code.mjs
@@ -0,0 +1,4 @@
+import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
+export function html_style_code(component) {
+  return html_font_jetbrains_mono(component);
+}
