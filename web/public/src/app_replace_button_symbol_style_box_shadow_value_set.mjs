d7e7721972a8a379b9fb9dedab3f8094ad9209cd
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:19:41 -0400

    function_auto_path ../love/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs

--- web/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs
+++ web/public/src/app_replace_button_symbol_style_box_shadow_value_set.mjs
@@ -1,4 +1,4 @@
-import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+import { html_box_shadow_set } from "../../../love/public/src/html_box_shadow_set.mjs";
 import { app_replace_button_symbol_style_box_shadow_value } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value.mjs";
 export function app_replace_button_symbol_style_box_shadow_value_set(
   component,
@@ -6,5 +6,5 @@ export function app_replace_button_symbol_style_box_shadow_value_set(
 ) {
   let style_value =
     app_replace_button_symbol_style_box_shadow_value(color_box_shadow);
-  html_style_set(component, "box-shadow", style_value);
+  html_box_shadow_set(component, style_value);
 }
