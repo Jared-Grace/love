c2c5d77073b6f18074a60bcc91bea8ae2c9a631f
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 22:28:41 -0400

    function_auto_path ../love/public/src/html_cycle_mono_multiple.mjs

--- web/public/src/html_cycle_mono_multiple.mjs
+++ web/public/src/html_cycle_mono_multiple.mjs
@@ -1,5 +1,8 @@
+import { each } from "../../../love/public/src/each.mjs";
 import { html_cycle_mono } from "../../../love/public/src/html_cycle_mono.mjs";
 export function html_cycle_mono_multiple(parent, parts) {
-  let r = html_cycle_mono(parent, parts);e
+  let r = html_cycle_mono(parent, parts);
+  function lambda(item) {}
+  each(list, lambda);
   return r;
 }
