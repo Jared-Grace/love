0a8e9f4abad9957d8baa9522a048a7eefdad5301
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 08:54:10 -0400

    function_auto_path ../love/public/src/list_between_space_nb.mjs

--- web/public/src/list_between_space_nb.mjs
+++ web/public/src/list_between_space_nb.mjs
@@ -1,7 +1,7 @@
 import { list_between } from "../../../love/public/src/list_between.mjs";
 import { text_space_nb } from "../../../love/public/src/text_space_nb.mjs";
 export function list_between_space_nb(list) {
-  let nb1 = text_space_nb();
-  let parts = list_between(list, nb1);
+  let nb = text_space_nb();
+  let parts = list_between(list, nb);
   return parts;
 }
