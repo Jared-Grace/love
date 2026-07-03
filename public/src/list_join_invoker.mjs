e5ef476f2e3ddb092de9453e696ab9baa0a47422
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 19:45:45 -0400

    function_auto_path ../love/public/src/list_join_invoker.mjs

--- public/src/list_join_invoker.mjs
+++ public/src/list_join_invoker.mjs
@@ -1,9 +1,9 @@
 import { list_join_cycled } from "../../../love/public/src/list_join_cycled.mjs";
 import { range_map } from "../../../love/public/src/range_map.mjs";
 import { list_size_less_1 } from "../../../love/public/src/list_size_less_1.mjs";
-export function list_join_invoker(list, next_get) {
+export function list_join_invoker(list, mapper) {
   let size = list_size_less_1(list);
-  let mapped2 = range_map(size, next_get);
+  let mapped2 = range_map(size, mapper);
   let joined = list_join_cycled(list, mapped2);
   return joined;
 }
