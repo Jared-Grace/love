54994503a01a73cb45451f8800990678bcdeb631
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 09:00:13 -0400

    function_auto_path ../love/public/src/text_double.mjs

--- public/src/text_double.mjs
+++ public/src/text_double.mjs
@@ -1,4 +1,5 @@
 import { text_multiply } from "../../../love/public/src/text_multiply.mjs";
 export function text_double(value, times) {
-  return text_multiply(value, times);
+  let multiplied = text_multiply(value, times);
+  return multiplied;
 }
