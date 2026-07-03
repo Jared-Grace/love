877e57f4ec39737fd4deecfb8e0b08182db0c8d4
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 00:59:10 -0400

    function_auto_path ../love/public/src/list_slices_size_cycles_shuffled.mjs

--- public/src/list_slices_size_cycles_shuffled.mjs
+++ public/src/list_slices_size_cycles_shuffled.mjs
@@ -1,8 +1,8 @@
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { list_slices_size_cycle } from "../../../love/public/src/list_slices_size_cycle.mjs";
 export function list_slices_size_cycles_shuffled(mapped, min, max) {
-  let choices_count = max - min + 1;
   let list = list_slices_size_cycle(mapped, min, max);
+  let choices_count = max - min + 1;
   list_shuffle_cycled(list, choices_count);
   return list;
 }
