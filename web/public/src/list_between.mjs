cad3966dbb24caa58a6b0823e528e3295484b637
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:27:55 -0400

    function_auto_path ../love/public/src/list_between.mjs

--- web/public/src/list_between.mjs
+++ web/public/src/list_between.mjs
@@ -2,6 +2,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
+import { list_concat_multiple } from "../../../love/public/src/list_concat_multiple.mjs";
 export function list_between(list, item_between) {
   let taken = list_take_less_1(list);
   function lambda(t) {
@@ -10,6 +11,7 @@ export function list_between(list, item_between) {
   }
   let mapped = list_map(taken, lambda);
   let last = list_last(list);
-  list_add(mapped, last);
+  list_add(mapped, [last]);
+  let combined = list_concat_multiple(lists);
   return mapped;
 }
