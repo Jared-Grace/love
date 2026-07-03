f5efb3cdd85cbbe473f03aef68b0aef3ea695a3a
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 19:44:50 -0400

    function_auto_path ../love/public/src/invoke_map.mjs

--- public/src/invoke_map.mjs
+++ public/src/invoke_map.mjs
@@ -1,5 +1,5 @@
 export function invoke_map(next_get_list, mapper) {
-  let r3 = function lambda3() {
+  let r3 = function invoke_map_result() {
     let result = next_get_list();
     let r = mapper(result);
     return r;
