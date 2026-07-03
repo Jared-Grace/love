f7cb8378c7227a48d3f896c7b1fde1f748f85ede
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 04:01:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores.mjs

--- web/public/src/app_code_lesson_identifiers_underscores.mjs
+++ web/public/src/app_code_lesson_identifiers_underscores.mjs
@@ -94,8 +94,8 @@ export function app_code_lesson_identifiers_underscores() {
       function lambda3(fn_list_add) {
         let r = boolean_random();
         if (r) {
-          let r2 = list_random_item(item, separators);
-          fn_list_add(r2);
+          let r2 = list_random_item(separators);
+          fn_list_add(item, r2);
         }
       }
       each(fns, lambda3);
