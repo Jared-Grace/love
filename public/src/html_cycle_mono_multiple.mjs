eb9c20db430216eb558762c9701e8af9f5ee4d2d
JESUS rose to life <username@users.noreply.github.com>
2026-07-02 22:28:33 -0400

    function_auto_path ../love/public/src/html_cycle_mono_multiple.mjs

---
+++ public/src/html_cycle_mono_multiple.mjs
@@ -0,0 +1,4 @@
+import { html_cycle_mono } from "../../../love/public/src/html_cycle_mono.mjs";
+export function html_cycle_mono_multiple(parent, parts) {
+  return html_cycle_mono(parent, parts);
+}
