9acd1e0b2c3fd76398ffbc87a963d4996438de13
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 02:37:05 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

--- web/public/src/app_code_lesson_symbols_space.mjs
+++ web/public/src/app_code_lesson_symbols_space.mjs
@@ -1,8 +1,5 @@
+import { app_code_lesson_symbols_space_style } from "../../../love/public/src/app_code_lesson_symbols_space_style.mjs";
 import { list_between } from "../../../love/public/src/list_between.mjs";
-import { app_code_container_light_blue_border_color } from "../../../love/public/src/app_code_container_light_blue_border_color.mjs";
-import { app_replace_button_symbol_style_box_shadow_value_width } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_width.mjs";
-import { html_box_shadow_set } from "../../../love/public/src/html_box_shadow_set.mjs";
-import { html_style_code_generic_unshadowed } from "../../../love/public/src/html_style_code_generic_unshadowed.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { text_space_nb } from "../../../love/public/src/text_space_nb.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
@@ -34,21 +31,7 @@ export function app_code_lesson_symbols_space() {
       [
         noop,
         function lambda2(component) {
-          const color_background = "white";
-          const color_font = "transparent";
-          let color_box_shadow = app_code_container_light_blue_border_color();
-          html_style_code_generic_unshadowed(
-            component,
-            color_background,
-            color_font,
-          );
-          const width = "0.03";
-          let style_value =
-            app_replace_button_symbol_style_box_shadow_value_width(
-              color_box_shadow,
-              width,
-            );
-          html_box_shadow_set(component, style_value);
+          app_code_lesson_symbols_space_style(component);
         },
       ],
       parts,
