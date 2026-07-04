b57268d45b83d31c192dd458827f543f062391c6
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:23:40 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/app_code_home.mjs
+++ web/public/src/app_code_home.mjs
@@ -1,3 +1,5 @@
+import { app_code_examples } from "../../../love/public/src/app_code_examples.mjs";
+import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
@@ -10,8 +12,9 @@ export function app_code_home(context) {
   function lambda(item) {
     let name = property_get(item, "name");
     let id = property_get(item, "id");
-    function lambda3() {
+    async function lambda3() {
       storage_local_set_context(context, "lesson_id", id);
+      await app_shared_screen_set(context, app_code_examples);
     }
     let b = app_replace_button_wide(root, name, lambda3);
   }
