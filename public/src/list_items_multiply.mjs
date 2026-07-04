a4d2d8b767e04375aa720b31c3e442d56f146cdc
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:33:56 -0400

    function_auto_path ../love/public/src/list_items_multiply.mjs

--- public/src/list_items_multiply.mjs
+++ public/src/list_items_multiply.mjs
@@ -1,12 +1,12 @@
 import { list_adder_each } from "../../../love/public/src/list_adder_each.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
-export function list_items_multiply(counts, count) {
+export function list_items_multiply(list, count) {
   function lambda(la, item) {
     function lambda3(i) {
       la(item);
     }
     each_range(count, lambda3);
   }
-  let doubled = list_adder_each(counts, lambda);
+  let doubled = list_adder_each(list, lambda);
   return doubled;
 }
