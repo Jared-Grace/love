c6522d84014dc8b5b1c12aabd2053e00fd2980dd
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 08:46:21 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic.mjs

--- public/src/app_code_lesson_operators_subtraction.mjs
+++ public/src/app_code_lesson_operators_subtraction.mjs
@@ -2,7 +2,7 @@ import { add } from "../../../love/public/src/add.mjs";
 import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
 export function app_code_lesson_operators_subtraction() {
   const operator = "-";
-  const operator_name = "minus";
+  const operator_name = "minus sign";
   let math_name = "subtraction";
   let verb = "subtract";
   let r = app_code_lesson_operators_generic(
