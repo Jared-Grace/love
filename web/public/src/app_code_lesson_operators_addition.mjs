0b63b1ee15a57beec8a7a59af9bdc14e62b935fc
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:52:07 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_addition.mjs

--- web/public/src/app_code_lesson_operators_addition.mjs
+++ web/public/src/app_code_lesson_operators_addition.mjs
@@ -17,7 +17,7 @@ export function app_code_lesson_operators_addition() {
   }
   const example_label = "Code: ";
   const quiz_label = "What is the value of this code? ";
-  let max = 15;
+  let max = 10;
   let symbols_to_answer = js_parse_expression;
   function lambda2() {
     let lefts = range_1(max);
