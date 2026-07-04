657c4027717bc99d27f98745d61bcd070733b03f
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:10:17 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- public/src/app_code_home.mjs
+++ public/src/app_code_home.mjs
@@ -1,9 +1,13 @@
+import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_code_lessons } from "../../../love/public/src/app_code_lessons.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 export function app_code_home(context) {
   let root = html_clear_context(context);
   let lessons = app_code_lessons();
-  function lambda(item) {}
+  function lambda(item) {
+    function lambda3() {}
+    let b = app_replace_button_wide(parent, text, lambda3);
+  }
   each(lessons, lambda);
 }
