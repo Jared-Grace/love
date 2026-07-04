a6dd17b28106db6111dcd0802b8afdf4384e0666
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 21:39:04 -0400

    function_auto_path ../love/public/src/html_span_text_padded_space.mjs

--- web/public/src/html_span_text_padded_space.mjs
+++ web/public/src/html_span_text_padded_space.mjs
@@ -1,4 +1,5 @@
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function html_span_text_padded_space(parent, text) {
-  return html_span_text(parent, text);
+  let span = html_span_text(parent, text);
+  return span;
 }
