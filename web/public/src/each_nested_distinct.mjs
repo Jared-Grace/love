f08dcea8e1c6a7d42fdaa81d0dda3d58eb547ff1
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:57:05 -0400

    function_auto_path ../love/public/src/each_nested_distinct.mjs

--- web/public/src/each_nested_distinct.mjs
+++ web/public/src/each_nested_distinct.mjs
@@ -1,6 +1,7 @@
 import { each } from "../../../love/public/src/each.mjs";
 export function each_nested_distinct(list_a, list_b, lambda$a$b) {
   'not sure if this function is doing the "distinct" part - maybe this is coded wrong?';
+  "or distinct could refer to a and b both being passed into the lambda?";
   function lambda_a(a) {
     function lambda_b(b) {
       lambda$a$b(a, b);
