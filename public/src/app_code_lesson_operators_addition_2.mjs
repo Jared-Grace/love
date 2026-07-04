555ac7e50bfaa3ff3add6e63e36ddecb25a83542
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 06:35:13 -0400

    aca app_code_lesson_operators_addition

---
+++ public/src/app_code_lesson_operators_addition_2.mjs
@@ -0,0 +1,16 @@
+import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
+export function app_code_lesson_operators_addition_2() {
+  const operator = "+";
+  const operator_name = "plus";
+  let math_name = "addition";
+  let verb = "add";
+  let ending = "together";
+  let r = app_code_lesson_operators_generic(
+    operator_name,
+    operator,
+    verb,
+    ending,
+    math_name,
+  );
+  return r;
+}
