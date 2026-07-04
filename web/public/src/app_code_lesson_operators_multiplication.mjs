f5d92e95fda61f85683166a8d847dc51195cc882
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 07:50:24 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_multiplication.mjs

--- web/public/src/app_code_lesson_operators_multiplication.mjs
+++ web/public/src/app_code_lesson_operators_multiplication.mjs
@@ -1,4 +1,3 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
 export function app_code_lesson_operators_multiplication() {
