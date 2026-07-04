b0011878c4dbd5a1578098ef480d08b762ae8325
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:04:50 -0400

    asa code example

--- public/src/app_code_screens.mjs
+++ public/src/app_code_screens.mjs
@@ -1,6 +1,6 @@
 import { app_code_quiz } from "../../../love/public/src/app_code_quiz.mjs";
 import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
 export function app_code_screens() {
-  let s = [app_code_home, app_code_quiz];
+  let s = [app_code_home, app_code_quiz, app_code_example];
   return s;
 }
