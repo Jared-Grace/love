b98a2363374c728542d1c2d82bd7e73c5e146ee4
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:03:38 -0400

    function_auto_path ../love/public/src/app_replace_button_home_text.mjs

--- web/public/src/app_replace_button_home_text.mjs
+++ web/public/src/app_replace_button_home_text.mjs
@@ -1,5 +1,5 @@
 import { emoji_home } from "../../../love/public/src/emoji_home.mjs";
 export function app_replace_button_home_text() {
-  let r = emoji_home() + "Home";
+  let r = emoji_home() + " Home";
   return r;
 }
