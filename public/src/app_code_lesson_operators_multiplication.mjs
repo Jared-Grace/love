a24a96383b6b9f6203973f1e3e0f2807c4cb2bce
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 07:53:27 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_multiplication.mjs

--- public/src/app_code_lesson_operators_multiplication.mjs
+++ public/src/app_code_lesson_operators_multiplication.mjs
@@ -1,12 +1,12 @@
 import { identity } from "../../../love/public/src/identity.mjs";
 import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
 export function app_code_lesson_operators_multiplication() {
-  const operatsor_js = "*";
+  const operator_js = "*";
   const operator_name = "multiplication";
   let math_name = operator_name;
   let verb = "multiply";
   let r = app_code_lesson_operators_generic(
-    operator,
+    operator_js,
     "×",
     operator_name,
     operator_name,
