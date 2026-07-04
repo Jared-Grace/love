9d0b07931074d32ea55e77b5fce034e542c48139
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 23:33:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

---
+++ web/public/src/app_code_example_answer_gap.mjs
@@ -0,0 +1,6 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+export function app_code_example_answer_gap(parent) {
+  html_style_assign(parent, {
+    gap: "0.2em",
+  });
+}
