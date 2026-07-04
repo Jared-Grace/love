3964989c4b18db3c8f2ec560c554fa62802d5de5
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 00:45:46 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first_2.mjs

--- web/public/src/app_code_lesson_identifiers_symbol_first_2.mjs
+++ web/public/src/app_code_lesson_identifiers_symbol_first_2.mjs
@@ -1,7 +1,7 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 import { app_code_lesson_identifiers_symbol_first_generic } from "../../../love/public/src/app_code_lesson_identifiers_symbol_first_generic.mjs";
-import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 export function app_code_lesson_identifiers_symbol_first_2() {
-  let symbol_create = app_code_symbol;
+  let symbol_create = html_span_text;
   let r = app_code_lesson_identifiers_symbol_first_generic(symbol_create);
   return r;
 }
