1fed42dd72bb52646bbbf33fbe57282840d31c21
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 01:25:42 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs

--- web/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
+++ web/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
@@ -35,13 +35,13 @@ export function app_code_lesson_identifiers_symbol_first_unseparated() {
     let first = list_first(b);
     let question = property_get(first, "question");
     let split = text_split_empty(question);
-    let span = html_span_text(parent, text2);
+    let span = html_span_text(div, "Instead of: ");
     let before = app_code_container_dark_inline(div);
     let lambda$item2 = app_code_symbol_separated_curried(before);
     each(split, lambda$item2);
     let s = emoji_arrow_right();
     let padded = text_pad_space("➡");
-    html_span_text(div, padded);
+    html_span_text(div, " , you wil now see: ");
     let after = app_code_container_dark_inline(div);
     let lambda$item = app_code_symbol_curried(after);
     each(split, lambda$item);
