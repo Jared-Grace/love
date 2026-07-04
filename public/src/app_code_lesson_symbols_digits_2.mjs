7d4e52622fba922d37d1e904e2a646b9d29ede59
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:47:41 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_2.mjs

--- public/src/app_code_lesson_symbols_digits_2.mjs
+++ public/src/app_code_lesson_symbols_digits_2.mjs
@@ -1,8 +1,10 @@
+import { app_code_symbol_separated } from "../../../love/public/src/app_code_symbol_separated.mjs";
+import { app_code_lesson_symbols_batches_generic } from "../../../love/public/src/app_code_lesson_symbols_batches_generic.mjs";
+import { text_size } from "../../../love/public/src/text_size.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_lesson_underscores_define_symbol } from "../../../love/public/src/app_code_lesson_underscores_define_symbol.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
-import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
 import { html_div_text_multiple } from "../../../love/public/src/html_div_text_multiple.mjs";
 export function app_code_lesson_symbols_digits_2() {
   function lambda(root) {
@@ -12,13 +14,23 @@ export function app_code_lesson_symbols_digits_2() {
       "In math, we use plus signs to add numbers together",
       "In JavaScript, a plus sign can add two numbers together",
     ]);
-  }$x
-  let r5 = app_code_lesson_symbols_counting(
-    "Operators (Addition)",
-    "operators_addition",
+  }
+  const example_label = "Number of symbols: ";
+  const quiz_label = "How many symbols are there? ";
+  let symbols_to_answer = text_size;
+  let r5 = app_code_lesson_symbols_batches_generic(
+    name,
+    id,
     lambda,
     noop,
     app_code_lesson_symbols_batch_digits,
+    example_label,
+    quiz_label,
+    symbols_to_answer,
+    1,
+    app_code_symbol_separated,
   );
+  let name = "Operators (Addition)";
+  let id = "operators_addition";
   return r5;
 }
