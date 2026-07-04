87dc4c34d54544f0f6d99058927d2ac0744345e6
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:06:45 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first.mjs

--- public/src/app_code_lesson_identifiers_symbol_first.mjs
+++ public/src/app_code_lesson_identifiers_symbol_first.mjs
@@ -65,9 +65,7 @@ export function app_code_lesson_identifiers_symbol_first() {
     let mapped = list_map(words, text_letters_only);
     const min = 1;
     const max = 3;
-    let counts = range_from(min, max);
-    function lambda(item) {}
-    let mapped2 = list_map(list3, lambda);
+    let counts = range_from(min, max);la
     let list2 = list_slices_size_cycler(mapped, counts, min);
     list_shuffle_cycled_range(list2, min, max);
     let list = list2;
