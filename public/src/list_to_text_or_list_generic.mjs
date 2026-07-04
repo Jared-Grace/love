fff72a33724b9ae9997be0b032f25fc555700b61
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 21:05:32 -0400

    function_auto_path ../love/public/src/list_to_text_or_list_generic.mjs

--- public/src/list_to_text_or_list_generic.mjs
+++ public/src/list_to_text_or_list_generic.mjs
@@ -1,13 +1,15 @@
+import { list_join } from "../../../love/public/src/list_join.mjs";
+import { text_comma_space } from "../../../love/public/src/text_comma_space.mjs";
 import { list_empty_not_is } from "../../../love/public/src/list_empty_not_is.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
-import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 import { list_last } from "../../../love/public/src/list_last.mjs";
 import { list_take_less_1 } from "../../../love/public/src/list_take_less_1.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export function list_to_text_or_list_generic(list, word_relationship) {
   let taken = list_take_less_1(list);
   let last = list_last(list);
-  let joined = list_join_comma_space(taken);
+  let separator = text_comma_space();
+  let joined = list_join(taken, separator);
   let prefix = "";
   let ne = list_empty_not_is(taken);
   if (ne) {
