2e8f14ee8a6f3a085726b12736b05c49d45239f1
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 06:46:24 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic.mjs

--- web/public/src/app_code_lesson_operators_generic.mjs
+++ web/public/src/app_code_lesson_operators_generic.mjs
@@ -44,8 +44,8 @@ export function app_code_lesson_operators_generic(
     let rights = range_1(max);
     function lambda4(la) {
       function lambda3(left, right) {
-        let transformedc = left_transform(left, right);
-        let combined = text_combine_multiple([transformedc, operator, right]);
+        let transformed = left_transform(left, right);
+        let combined = text_combine_multiple([transformed, operator, right]);
         la(combined);
       }
       each_nested_distinct(lefts, rights, lambda3);
