719450b5a82beb2ccb7633c35f13505b06650215
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:34:17 -0400

    function_auto_path ../love/public/src/list_items_multiply.mjs

--- web/public/src/list_items_multiply.mjs
+++ web/public/src/list_items_multiply.mjs
@@ -2,10 +2,10 @@ import { list_adder_each } from "../../../love/public/src/list_adder_each.mjs";
 import { each_range } from "../../../love/public/src/each_range.mjs";
 export function list_items_multiply(list, count) {
   function lambda(la, item) {
-    function lambda3(i) {
+    function lambda2(i) {
       la(item);
     }
-    each_range(count, lambda3);
+    each_range(count, lambda2);
   }
   let doubled = list_adder_each(list, lambda);
   return doubled;
