a205db25e6ea5219afa23f15d98cc5bb4c4fcf8a
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:36:01 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_2.mjs

--- public/src/app_code_lesson_symbols_digits_2.mjs
+++ public/src/app_code_lesson_symbols_digits_2.mjs
@@ -1,12 +1,14 @@
-import { app_code_container_light_blue_text } from "../../../love/public/src/app_code_container_light_blue_text.mjs";
-import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
+import { app_code_lesson_identifiers_underscores_define_symbol } from "../../../love/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
 export function app_code_lesson_symbols_digits_2() {
   function lambda(root) {
-    let text = app_code_lesson_same_message("there are not numbers underneath");
-    app_code_container_light_blue_text(root, text);
+    app_code_lesson_identifiers_underscores_define_symbol(
+      c,
+      separator_valid_name,
+      separator_valid,
+    );
   }
   let r5 = app_code_lesson_symbols_counting(
     "Symbols (Digits)",
