144179fdc5551a4c7f32587701bba79239ff3de8
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:28:51 -0400

    function_auto_path ../love/public/src/list_adder_each.mjs

--- public/src/list_adder_each.mjs
+++ public/src/list_adder_each.mjs
@@ -1,10 +1,12 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-export function list_adder_each(lambda$la$e) {
+export function list_adder_each(list, lambda$la$e) {
   function lambda(la) {
-    function lambda2(item) {}
-    each(list2, lambda2);
+    function lambda2(item) {
+      lambda$la$e(la, item);
+    }
+    each(list, lambda2);
   }
-  let list = list_adder(lambda);
-  return list;
+  let r = list_adder(lambda);
+  return r;
 }
