273b96fd33fc3e69169c99e960d6a2700db44d19
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 00:59:55 -0400

    function_auto_path ../love/public/src/app_code_symbol.mjs

--- web/public/src/app_code_symbol.mjs
+++ web/public/src/app_code_symbol.mjs
@@ -4,6 +4,6 @@ import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function app_code_symbol(parent, text) {
   let span = html_span_text(parent, text);
   html_font_jetbrains_mono(span);
-  html_font_color_set_white(parent2);
+  html_font_color_set_white(span);
   return span;
 }
