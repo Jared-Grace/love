c31add7bcb66cbefb6c8ef015beac6326918b867
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:01:55 -0400

    aca app_code_lesson_symbols_digits

---
+++ web/public/src/app_code_lesson_symbols_digits_2.mjs
@@ -0,0 +1,19 @@
+import { app_code_container_light_blue_text } from "../../../love/public/src/app_code_container_light_blue_text.mjs";
+import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
+import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
+import { noop } from "../../../love/public/src/noop.mjs";
+import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
+export function app_code_lesson_symbols_digits_2() {
+  function lambda(root) {
+    let text = app_code_lesson_same_message("there are not numbers underneath");
+    app_code_container_light_blue_text(root, text);
+  }
+  let r5 = app_code_lesson_symbols_counting(
+    "Symbols (Digits)",
+    "symbols_digits",
+    lambda,
+    noop,
+    app_code_lesson_symbols_batch_digits,
+  );
+  return r5;
+}
