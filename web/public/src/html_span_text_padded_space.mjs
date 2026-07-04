f764130f89f25c8535a324ed3b2f03d2dfff1c3f
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 21:39:01 -0400

    w c html_span_text,padded_space

--- web/public/src/html_span_text_padded_space.mjs
+++ web/public/src/html_span_text_padded_space.mjs
@@ -1,1 +1,4 @@
-export function html_span_text_padded_space() {}
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+export function html_span_text_padded_space(parent, text) {
+  return html_span_text(parent, text);
+}
