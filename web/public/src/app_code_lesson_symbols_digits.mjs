283d8c8dfbfcd0d04465417e53bc40442ccf4b33
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 00:55:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- web/public/src/app_code_lesson_symbols_digits.mjs
+++ web/public/src/app_code_lesson_symbols_digits.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
@@ -6,11 +7,8 @@ import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_c
 export function app_code_lesson_symbols_digits() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
-        let text = app_code_lesson_same_message("there are not numbers underneath");
-    let div = html_div_text(
-      c,
-      text,
-    );
+    let text = app_code_lesson_same_message("there are not numbers underneath");
+    let div = html_div_text(c, text);
   }
   let r5 = app_code_lesson_symbols_counting(
     "Symbols (Digits)",
