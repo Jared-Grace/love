71f518dfcb313fda1806302087f526b5a88e6132
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:05:53 -0400

    pf list_shuffle_cycled_range list,min

--- web/public/src/list_shuffle_cycled_range.mjs
+++ web/public/src/list_shuffle_cycled_range.mjs
@@ -1,5 +1,5 @@
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
-export function list_shuffle_cycled_range(max, min, list) {
+export function list_shuffle_cycled_range(list, min, max) {
   let choices_count = max - min + 1;
   list_shuffle_cycled(list, choices_count);
 }
