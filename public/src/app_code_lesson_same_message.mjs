7136b65eb15eca35992d2c064be16307f7b8401e
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 00:54:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_same_message.mjs

--- public/src/app_code_lesson_same_message.mjs
+++ public/src/app_code_lesson_same_message.mjs
@@ -1,4 +1,6 @@
-export function app_code_lesson_same_message() {tc
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
+export function app_code_lesson_same_message() {
+  let combined = text_combine(left, right);
   let r =
     "This lesson is the same as the previous lesson, except there are not numbers underneath";
   return r;
