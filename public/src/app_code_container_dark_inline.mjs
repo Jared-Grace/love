8c1b7b72b169b98529dac33ebac4cd28cd582056
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 01:22:31 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs

---
+++ public/src/app_code_container_dark_inline.mjs
@@ -0,0 +1,6 @@
+import { html_display_inline } from "../../../love/public/src/html_display_inline.mjs";
+import { app_code_container_dark } from "../../../love/public/src/app_code_container_dark.mjs";
+export function app_code_container_dark_inline() {
+  let container = app_code_container_dark(parent);
+  html_display_inline(container);
+}
