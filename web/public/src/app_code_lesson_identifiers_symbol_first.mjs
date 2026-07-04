efcb14cdd5aa8124764b31e69abce3064588a6bb
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 21:18:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first.mjs

--- web/public/src/app_code_lesson_identifiers_symbol_first.mjs
+++ web/public/src/app_code_lesson_identifiers_symbol_first.mjs
@@ -82,8 +82,11 @@ export function app_code_lesson_identifiers_symbol_first() {
       "However identifiers cannot have a digit as their first symbol (Identifiers cannot begin with a digit)",
       "After the first symbol an identifier can always be a digit",
     ]);
-    function lambda3(item) {}
-    let mapped2 = list_map(list4, lambda3);
+    function lambda3(t) {
+      let plural = property_get(t, "plural");
+      let examples2 = property_get(t, "examples");
+    }
+    let mapped2 = list_map(identifier_symbols_types, lambda3);
     let concated = list_to_or_list_generic(list3, word_relationship);
     let div2 = html_div(c);
     html_cycle_code(div2, [
