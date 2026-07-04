738f9ca046f10e2b64877672bc9587bf962c4d2f
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 21:25:07 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first.mjs

---
+++ public/src/text_combine_space_right.mjs
@@ -0,0 +1,5 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
+export function text_combine_space_right(plural) {
+  let combined3 = text_combine(plural, " ");
+  return combined3;
+}
