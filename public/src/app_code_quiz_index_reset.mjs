074b1558784ae12229f97c8850b17ba0f88472af
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:25:55 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

---
+++ public/src/app_code_quiz_index_reset.mjs
@@ -0,0 +1,4 @@
+import { app_code_quiz_index_set } from "../../../love/public/src/app_code_quiz_index_set.mjs";
+export function app_code_quiz_index_reset(context) {
+  app_code_quiz_index_set(context, 0);
+}
