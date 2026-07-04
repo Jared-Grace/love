a6e02ce158de9f11cb1d979f7d273575350df3c4
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 01:18:23 -0400

    de app_code_symbol_curried_right

--- web/public/src/app_code_symbol_curried_right.mjs
+++
@@ -1,8 +0,0 @@
-import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
-export function app_code_symbol_curried_right(text) {
-  let c = function app_code_symbol_curried_right_result(parent) {
-    let span = app_code_symbol(parent, text);
-    return span;
-  };
-  return c;
-}
