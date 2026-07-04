c1b14863b891db5fd7086ca8d538cc589d3517fb
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:36:30 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs

--- web/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
+++ web/public/src/app_code_lesson_identifiers_underscores_define_symbol.mjs
@@ -8,19 +8,14 @@ import { noop } from "../../../love/public/src/noop.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function app_code_lesson_identifiers_underscores_define_symbol(
   c,
-  separator_valid_name,
-  separator_valid,
+  symbol_name,
+  symbol,
 ) {
   arguments_assert(arguments, 3);
   let div3 = html_div(c);
-  let r = text_articled_split(separator_valid_name);
+  let r = text_articled_split(symbol_name);
   let text = property_get(r, "text");
   let article = property_get(r, "article");
   let cycles = [noop, html_bold, noop, html_style_code_dark];
-  html_cycle(div3, cycles, [
-    " This is " + article + " ",
-    text,
-    ": ",
-    separator_valid,
-  ]);
+  html_cycle(div3, cycles, [" This is " + article + " ", text, ": ", symbol]);
 }
