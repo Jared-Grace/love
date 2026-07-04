9be4e501ebb19e583f43161829714a29be5f6fe5
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:04:52 -0400

    function_auto_path ../love/public/src/app_code_screens.mjs

--- web/public/src/app_code_screens.mjs
+++ web/public/src/app_code_screens.mjs
@@ -1,3 +1,4 @@
+import { app_code_example } from "../../../love/public/src/app_code_example.mjs";
 import { app_code_quiz } from "../../../love/public/src/app_code_quiz.mjs";
 import { app_code_home } from "../../../love/public/src/app_code_home.mjs";
 export function app_code_screens() {
