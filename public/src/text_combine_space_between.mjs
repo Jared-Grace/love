db56cbe479d9dffeb8597dce3510f2152515867b
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 21:24:26 -0400

    function_auto_path ../love/public/src/text_combine_space_between.mjs

--- public/src/text_combine_space_between.mjs
+++ public/src/text_combine_space_between.mjs
@@ -1,6 +1,6 @@
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { text_combine_3 } from "../../../love/public/src/text_combine_3.mjs";
-export function text_combine_space(index, item) {
+export function text_combine_space_between(index, item) {
   arguments_assert(arguments, 2);
   const combine = " ";
   let r = text_combine_3(index, combine, item);
