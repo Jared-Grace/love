0a634cf90bb099d04ea7fa056abd68b4ba0abaeb
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 23:45:24 -0400

    function_auto_path ../love/public/src/app_code_lesson_text_example_another.mjs

--- web/public/src/app_code_lesson_text_example_another.mjs
+++ web/public/src/app_code_lesson_text_example_another.mjs
@@ -1,3 +1,4 @@
+import { word_plural } from "../../../love/public/src/word_plural.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_code_lesson_text_example_another(lesson) {
   let example_count = property_get(lesson, "example_count");
@@ -5,7 +6,7 @@ export function app_code_lesson_text_example_another(lesson) {
   const root_word = "example";
   let another = null;
   if (plural) {
-    another = "some more " + root_word + "s";
+    another = "some more " + word_plural(root_word);
   } else {
     another = "another " + root_word;
   }
