fe422feaa5726ceb43a140720a8fa5826fd9550f
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:36:17 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_2.mjs

--- web/public/src/app_code_lesson_symbols_digits_2.mjs
+++ web/public/src/app_code_lesson_symbols_digits_2.mjs
@@ -4,11 +4,7 @@ import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
 export function app_code_lesson_symbols_digits_2() {
   function lambda(root) {
-    app_code_lesson_identifiers_underscores_define_symbol(
-      c,
-      separator_valid_name,
-      separator_valid,
-    );
+    app_code_lesson_identifiers_underscores_define_symbol(c, "plus sign", "+");
   }
   let r5 = app_code_lesson_symbols_counting(
     "Symbols (Digits)",
