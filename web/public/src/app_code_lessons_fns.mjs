b32e5ebd6b973e8113860562bf8f845ad1f28c85
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 06:35:29 -0400

    r 2 app_code_lesson_operators_addition_2,subtraction

--- web/public/src/app_code_lessons_fns.mjs
+++ web/public/src/app_code_lessons_fns.mjs
@@ -1,4 +1,4 @@
-import { app_code_lesson_operators_addition_2 } from "../../../love/public/src/app_code_lesson_operators_addition_2.mjs";
+import { app_code_lesson_operators_subtraction } from "../../../love/public/src/app_code_lesson_operators_subtraction.mjs";
 import { app_code_lesson_operators_addition } from "../../../love/public/src/app_code_lesson_operators_addition.mjs";
 import { app_code_lesson_identifiers_symbol_first_unseparated } from "../../../love/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs";
 import { app_code_lesson_identifiers_symbol_first } from "../../../love/public/src/app_code_lesson_identifiers_symbol_first.mjs";
@@ -21,7 +21,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_identifiers_symbol_first,
     app_code_lesson_identifiers_symbol_first_unseparated,
     app_code_lesson_operators_addition,
-    app_code_lesson_operators_addition_2,
+    app_code_lesson_operators_subtraction,
   ];
   return r;
 }
