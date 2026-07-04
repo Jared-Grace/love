014c7b25df53f1bb6df725245b6c62338d355742
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 01:06:32 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs

--- web/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
+++ web/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
@@ -1,4 +1,3 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 import { app_code_container_light_blue_text } from "../../../love/public/src/app_code_container_light_blue_text.mjs";
 import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
@@ -7,18 +6,15 @@ export function app_code_lesson_identifiers_symbol_first_unseparated() {
   let symbol_create = app_code_symbol;
   let name = "Identifiers (first symbol, unseparated)";
   let id = "identifiers_symbol_first_unseparated";
-  function above(root) {
-    let text = app_code_lesson_same_message("the symbols are not separated");
-    app_code_container_light_blue_text(root, text);
-    log(app_code_lesson_identifiers_symbol_first_unseparated.name, {
-      r,
-    });
-  }
   let r = app_code_lesson_identifiers_symbol_first_generic(
     symbol_create,
     name,
     id,
     above,
   );
+  function above(root) {
+    let text = app_code_lesson_same_message("the symbols are not separated");
+    app_code_container_light_blue_text(root, text);
+  }
   return r;
 }
