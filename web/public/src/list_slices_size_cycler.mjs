2990bb20d72ba8c4d921f3928013e4bf36a5448e
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:03:18 -0400

    function_auto_path ../love/public/src/list_slices_size_cycler.mjs

--- web/public/src/list_slices_size_cycler.mjs
+++ web/public/src/list_slices_size_cycler.mjs
@@ -1,7 +1,7 @@
 import { list_slices_size_generic } from "../../../love/public/src/list_slices_size_generic.mjs";
 import { list_cycler } from "../../../love/public/src/list_cycler.mjs";
-export function list_slices_size_cycler(mapped, min, list) {
-  let count_get = list_cycler(mapped);
+export function list_slices_size_cycler(counts, min, list) {
+  let count_get = list_cycler(counts);
   let slices = list_slices_size_generic(count_get, min, list);
   return slices;
 }
