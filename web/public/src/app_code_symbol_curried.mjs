302d99d0355a244003159125b0879991025cb576
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 01:18:32 -0400

    function_auto_path ../love/public/src/app_code_symbol_curried.mjs

--- web/public/src/app_code_symbol_curried.mjs
+++ web/public/src/app_code_symbol_curried.mjs
@@ -1,7 +1,8 @@
 import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 export function app_code_symbol_curried(parent) {
   let c = function app_code_symbol_curried_result(text) {
-    return app_code_symbol(parent, text);
+    let span = app_code_symbol(parent, text);
+    return span;
   };
   return c;
 }
