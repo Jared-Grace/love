9f55accd2f36c54024fdbab04c0d0b762e867280
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:44:27 -0400

    function_auto_path ../love/public/src/js_expand_generic.mjs

--- web/public/src/js_expand_generic.mjs
+++ web/public/src/js_expand_generic.mjs
@@ -39,8 +39,7 @@ export async function js_expand_generic(next, stack2, index, ast) {
     let callee = property_get(call, "callee");
     let arguments2 = js_call_arguments_get(call);
     async function lambda5(item2, index2) {
-      let jin = js_identifier_not_is(item2);
-      await js_node_atomize(ast3, visitor2, variable_name2, offset2);
+      let jin = js_identifier_not_is(item2);$i
       await js_node_atomize(ast2, visitor, variable_name, offset);
     }
     each_index(arguments2, lambda5);
