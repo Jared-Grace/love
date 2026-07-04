fa12846e4b609dd7776aa86f2cfd25b4b75e90cf
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 01:24:37 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs

--- public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
+++ public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
@@ -1,4 +1,4 @@
-import { app_code_container_dark } from "../../../love/public/src/app_code_container_dark.mjs";
+import { app_code_container_dark_inline } from "../../../love/public/src/app_code_container_dark_inline.mjs";
 import { app_code_symbol_separated_curried } from "../../../love/public/src/app_code_symbol_separated_curried.mjs";
 import { app_code_symbol_curried } from "../../../love/public/src/app_code_symbol_curried.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
@@ -35,13 +35,13 @@ export function app_code_lesson_identifiers_symbol_first_unseparated() {
     let first = list_first(b);
     let question = property_get(first, "question");
     let split = text_split_empty(question);
-    let before = app_code_container_dark(div);
+    let before = app_code_container_dark_inline(div);
     let lambda$item2 = app_code_symbol_separated_curried(before);
     each(split, lambda$item2);
     let s = emoji_arrow_right();
     let padded = text_pad_space("➡");
     html_span_text(div, padded);
-    let after = app_code_container_dark(div);
+    let after = app_code_container_dark_inline(div);
     let lambda$item = app_code_symbol_curried(after);
     each(split, lambda$item);
     log(app_code_lesson_identifiers_symbol_first_unseparated.name, {
