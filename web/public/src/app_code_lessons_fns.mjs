36e3e3b9ba79c5aff12c64ffcaa904e2f4293cdb
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 04:48:46 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_addition.mjs

--- web/public/src/app_code_lessons_fns.mjs
+++ web/public/src/app_code_lessons_fns.mjs
@@ -1,4 +1,4 @@
-import { app_code_lesson_symbols_digits_2 } from "../../../love/public/src/app_code_lesson_symbols_digits_2.mjs";
+import { app_code_lesson_operators_addition } from "../../../love/public/src/app_code_lesson_operators_addition.mjs";
 import { app_code_lesson_identifiers_symbol_first_unseparated } from "../../../love/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs";
 import { app_code_lesson_identifiers_symbol_first } from "../../../love/public/src/app_code_lesson_identifiers_symbol_first.mjs";
 import { app_code_lesson_identifiers_dollar_signs } from "../../../love/public/src/app_code_lesson_identifiers_dollar_signs.mjs";
@@ -19,7 +19,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_identifiers_dollar_signs,
     app_code_lesson_identifiers_symbol_first,
     app_code_lesson_identifiers_symbol_first_unseparated,
-    app_code_lesson_symbols_digits_2,
+    app_code_lesson_operators_addition,
   ];
   return r;
 }
