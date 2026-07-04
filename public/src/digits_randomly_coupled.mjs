02828169131d8c3d121ebca424a6f5132d477ffb
JESUS rose to life <username@users.noreply.github.com>
2026-07-03 20:29:26 -0400

    function_auto_path ../love/public/src/digits_randomly_coupled.mjs

--- public/src/digits_randomly_coupled.mjs
+++ public/src/digits_randomly_coupled.mjs
@@ -1,25 +1,22 @@
+import { list_adder_each } from "../../../love/public/src/list_adder_each.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { boolean_random_3 } from "../../../love/public/src/boolean_random_3.mjs";
 import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
 export function digits_randomly_coupled() {
   let ds = digits();
   let chunks = list_chunk(ds, 2);
-  function lambda(la) {
-    function lambda3(c) {
-      let rb2 = boolean_random_3();
-      if (rb2) {
-        la(c);
-      } else {
-        function lambda4(d) {
-          la([d]);
-        }
-        each(c, lambda4);
+  function lambda(la, c) {
+    let rb2 = boolean_random_3();
+    if (rb2) {
+      la(c);
+    } else {
+      function lambda4(d) {
+        la([d]);
       }
+      each(c, lambda4);
     }
-    each(chunks, lambda3);
   }
-  let list = list_adder(lambda);
+  let list = list_adder_each(chunks, lambda);
   return list;
 }
