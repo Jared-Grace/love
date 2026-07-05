e9fceeee03c19ee893297027dbd2d88bed270242
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 21:23:23 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_addition_2.mjs

--- web/public/src/app_code_lesson_operators_addition_2.mjs
+++ web/public/src/app_code_lesson_operators_addition_2.mjs
@@ -1,14 +1,9 @@
+import { app_code_lesson_operators_generic_batch_get } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get.mjs";
 import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_batches_generic } from "../../../love/public/src/app_code_lesson_symbols_batches_generic.mjs";
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
-import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
-import { each_nested_distinct } from "../../../love/public/src/each_nested_distinct.mjs";
-import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
-import { list_between_space_nb } from "../../../love/public/src/list_between_space_nb.mjs";
-import { range_1 } from "../../../love/public/src/range_1.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { text_articled_pad_space } from "../../../love/public/src/text_articled_pad_space.mjs";
@@ -16,7 +11,6 @@ import { text_replace } from "../../../love/public/src/text_replace.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { app_code_lesson_underscores_define_symbol } from "../../../love/public/src/app_code_lesson_underscores_define_symbol.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
-import { add } from "../../../love/public/src/add.mjs";
 export function app_code_lesson_operators_addition_2() {
   const operator = "-";
   const operator_name = "minus sign";
@@ -25,6 +19,10 @@ export function app_code_lesson_operators_addition_2() {
   function above(root) {
     let c = app_code_container_light_blue(root);
     app_code_lesson_underscores_define_symbol(c, operator_name, operator);
+    let batch = app_code_lesson_operators_generic_batch_get(
+      left_transform,
+      operator_js,
+    );
     let list2 = batch();
     let first = list_first(list2);
     let replaced = text_replace(first, operator, operator);
@@ -63,22 +61,6 @@ export function app_code_lesson_operators_addition_2() {
   const quiz_label = "What is the value of this code? ";
   let max = 7;
   let symbols_to_answer = eval;
-  function batch() {
-    let lefts = range_1(max);
-    let rights = range_1(max);
-    function lambda4(la) {
-      function lambda3(left, right) {
-        let transformed = add(left, right);
-        let list3 = list_between_space_nb([transformed, operator, right]);
-        let combined = text_combine_multiple(list3);
-        la(combined);
-      }
-      each_nested_distinct(lefts, rights, lambda3);
-    }
-    let list = list_adder(lambda4);
-    list_shuffle(list);
-    return list;
-  }
   let inside = text_first_upper_to(math_name);
   let name = "Operators " + text_wrap_parenthesis(inside);
   let id = "operators_" + math_name;
