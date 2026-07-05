1965508a6fe6d82c4b40bd3053229c7f065172ce
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:40:08 -0400

    function_auto_path ../love/public/src/text_combine_right_fn.mjs

--- web/public/src/text_combine_right_fn.mjs
+++ web/public/src/text_combine_right_fn.mjs
@@ -1,5 +1,5 @@
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
-export function text_combine_right_fn(right_random_get, operator) {
+export function text_combine_right_fn(operator, right_random_get) {
   let right2 = right_random_get();
   let combined = text_combine(operator, right2);
   return combined;
