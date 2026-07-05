3b89a08c3f0f6beebac98ddf27a85dbaed8e2a55
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 06:46:55 -0400

    np

--- public/src/file_write_json.mjs
+++ public/src/file_write_json.mjs
@@ -1,6 +1,6 @@
 import { file_write } from "../../../love/public/src/file_write.mjs";
-import { json_format_to_truncated } from "../../../love/public/src/json_format_to_truncated.mjs";
+import { json_format_to } from "../../../love/public/src/json_format_to.mjs";
 export async function file_write_json(file_path, object) {
-  let json = json_format_to_truncated(object);
+  let json = json_format_to(object);
   await file_write(file_path, json);
 }
