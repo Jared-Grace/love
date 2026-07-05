472507e8c076fe0bff7a688dd948e631733b6311
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 05:39:22 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_asterisk.mjs

--- web/public/src/app_code_lesson_operators_asterisk.mjs
+++ web/public/src/app_code_lesson_operators_asterisk.mjs
@@ -1,12 +1,12 @@
 import { app_code_lesson_operators_asterisk_generic } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic.mjs";
 import { js_operator_multiplication_verb } from "../../../love/public/src/js_operator_multiplication_verb.mjs";
 import { js_operator_multiplication } from "../../../love/public/src/js_operator_multiplication.mjs";
-import { add } from "../../../love/public/src/add.mjs";
+import { multiply } from "../../../love/public/src/multiply.mjs";
 export function app_code_lesson_operators_asterisk() {
   const operator = js_operator_multiplication();
   let lesson_name = "asterisk";
   let verb = js_operator_multiplication_verb();
-  let left_transform = add;
+  let left_transform = multiply;
   let r = app_code_lesson_operators_asterisk_generic(
     operator,
     left_transform,
