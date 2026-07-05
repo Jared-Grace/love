d89a326f67858503a3a39d257bbf8c6eba282d64
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:53:44 -0400

    function_auto_path ../love/public/src/app_code_lessons_fns.mjs

--- public/src/app_code_lessons_fns.mjs
+++ public/src/app_code_lessons_fns.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_operators_minus_2 } from "../../../love/public/src/app_code_lesson_operators_minus_2.mjs";
 import { app_code_lesson_operators_plus } from "../../../love/public/src/app_code_lesson_operators_plus.mjs";
 import { app_code_lesson_operators_minus } from "../../../love/public/src/app_code_lesson_operators_minus.mjs";
 import { app_code_lesson_operators_division } from "../../../love/public/src/app_code_lesson_operators_division.mjs";
