3ffffc72195682dc65f908c0f738baa8457c3e01
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 21:27:51 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic.mjs

--- web/public/src/app_code_lesson_operators_generic.mjs
+++ web/public/src/app_code_lesson_operators_generic.mjs
@@ -37,7 +37,7 @@ export function app_code_lesson_operators_generic(
     const operator_name_math_articled =
       text_articled_pad_space(operator_name_math);
     html_div_cycle_code(c, [
-      "In math, " +
+      "In math," +
         operator_name_math_articled +
         "can be used to " +
         verb +
