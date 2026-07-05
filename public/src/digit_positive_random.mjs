d36a5bc55345f3aeabacba184be36825c5708287
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 21:36:17 -0400

    function_auto_path ../love/public/src/digit_positive_random.mjs

--- public/src/digit_positive_random.mjs
+++ public/src/digit_positive_random.mjs
@@ -1,5 +1,5 @@
-import { integer_random_range } from "../../../love/public/src/integer_random_range.mjs";
+import { integer_random_1 } from "../../../love/public/src/integer_random_1.mjs";
 export function digit_positive_random() {
-  let r = integer_random_range(10);
+  let r = integer_random_1(9);
   return r;
 }
