00d5ad80f98a10576d22149e07976814462f6c57
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:49:57 -0400

    function_auto_path ../love/public/src/js_code_binary_spaced_nb.mjs

---
+++ public/src/js_code_binary_spaced_nb_generic.mjs
@@ -0,0 +1,11 @@
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
+export function js_code_binary_spaced_nb_generic(
+  mapper,
+  left,
+  operator_js,
+  right,
+) {
+  let parts = mapper([left, operator_js, right]);
+  let combined = text_combine_multiple(parts);
+  return combined;
+}
