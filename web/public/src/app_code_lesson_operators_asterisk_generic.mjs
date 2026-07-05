501dd16d676e84f2fb1e00b5f6a36bf13b142e43
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:43:40 -0400

    pnd app_code_lesson_operators_minus_generic_container_both_sides noun

--- web/public/src/app_code_lesson_operators_asterisk_generic.mjs
+++ web/public/src/app_code_lesson_operators_asterisk_generic.mjs
@@ -27,6 +27,7 @@ export function app_code_lesson_operators_asterisk_generic(
       operator,
       left_transform,
       text_when,
+      noun,
     );
     app_code_lesson_operators_asterisk_generic_minus(root, operator);
     let c4 = app_code_container_light_blue(root);
