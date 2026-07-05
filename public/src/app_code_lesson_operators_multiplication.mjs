f58facb5d830b1711a83850633517ff0f956caf2
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 06:38:48 -0400

    function_auto_path ../love/public/src/js_operator_asterisk.mjs

--- public/src/app_code_lesson_operators_multiplication.mjs
+++ public/src/app_code_lesson_operators_multiplication.mjs
@@ -1,10 +1,10 @@
 import { js_operator_asterisk_name } from "../../../love/public/src/js_operator_asterisk_name.mjs";
 import { js_operator_asterisk_verb } from "../../../love/public/src/js_operator_asterisk_verb.mjs";
-import { js_operator_multiplication } from "../../../love/public/src/js_operator_multiplication.mjs";
+import { js_operator_asterisk } from "../../../love/public/src/js_operator_asterisk.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
 export function app_code_lesson_operators_multiplication() {
-  const operator_js = js_operator_multiplication();
+  const operator_js = js_operator_asterisk();
   const operator_name_js = js_operator_asterisk_name();
   const operator_math = "×";
   let math_name = "multiplication";
--- public/src/js_operator_multiplication.mjs
+++
@@ -1,4 +0,0 @@
-export function js_operator_multiplication() {
-  let r2 = "*";
-  return r2;
-}
