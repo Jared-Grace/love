108b6ee3bdfd52567555079910cafc419d57fa3d
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:56:01 -0400

    pf text_left_right_middle_random left

--- public/src/text_left_right_middle_random.mjs
+++ public/src/text_left_right_middle_random.mjs
@@ -1,6 +1,6 @@
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { text_random_or_empty } from "../../../love/public/src/text_random_or_empty.mjs";
-export function text_left_right_middle_random(t, left, right) {
+export function text_left_right_middle_random(left, t, right) {
   let r2 = text_random_or_empty(t);
   let combined2 = text_combine_multiple([left, r2, right]);
   return combined2;
--- public/src/text_left_right_middle_random_space_nb.mjs
+++ public/src/text_left_right_middle_random_space_nb.mjs
@@ -2,6 +2,6 @@ import { text_left_right_middle_random } from "../../../love/public/src/text_lef
 import { text_space_nb } from "../../../love/public/src/text_space_nb.mjs";
 export function text_left_right_middle_random_space_nb(left, right) {
   let middle = text_space_nb();
-  let combined = text_left_right_middle_random(middle, left, right);
+  let combined = text_left_right_middle_random(left, middle, right);
   return combined;
 }
