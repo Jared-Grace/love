1684d890fdf729554955bbb94fb620ecb9b84a32
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:50:10 -0400

    np

--- web/public/src/list_remove_last_single.mjs
+++ web/public/src/list_remove_last_single.mjs
@@ -1,5 +1,5 @@
 import { list_single } from "../../../love/public/src/list_single.mjs";
-import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
+import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
 export function list_remove_last_single(coordinates_land) {
   let player_list = list_remove_last(coordinates_land);
   let player_coordinates = list_single(player_list);
