7c739feef9682110a7dd2612f6075796763f810e
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:11:08 -0400

    function_auto_path ../love/public/src/html_div_cycle_code_multiple.mjs

--- public/src/html_div_cycle_code_multiple.mjs
+++ public/src/html_div_cycle_code_multiple.mjs
@@ -1,8 +1,10 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 export function html_div_cycle_code_multiple(parent, list_parts) {
-  function lambda(item) {}
-  let mapped = list_map(list, lambda);
-  let r = html_div_cycle_code(parent, list_parts);
-  return r;
+  function lambda(parts) {
+    let r = html_div_cycle_code(parent, parts);
+    return r;
+  }
+  let mapped = list_map(list_parts, lambda);
+  return mapped;
 }
