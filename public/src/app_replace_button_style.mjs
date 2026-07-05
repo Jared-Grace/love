4904ab5c9f6501ab513ee4d5e8b39a6cd9143bc6
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 03:53:01 -0400

    function_auto_path ../love/public/src/app_replace_button_style.mjs

--- public/src/app_replace_button_style.mjs
+++ public/src/app_replace_button_style.mjs
@@ -1,4 +1,3 @@
-import { app_replace_button_symbol_style_box_shadow_value_width_set } from "../../../love/public/src/app_replace_button_symbol_style_box_shadow_value_width_set.mjs";
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { app_replace_button_rule_background_color } from "../../../love/public/src/app_replace_button_rule_background_color.mjs";
 import { app_replace_button_symbol_style } from "../../../love/public/src/app_replace_button_symbol_style.mjs";
@@ -6,9 +5,4 @@ export function app_replace_button_style(b) {
   app_replace_button_symbol_style(b);
   let c = app_replace_button_rule_background_color();
   html_style_background_color_set(b, c);
-  app_replace_button_symbol_style_box_shadow_value_width_set(
-    b,
-    "#9a9a9a",
-    "0.04",
-  );
 }
