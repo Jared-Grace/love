6d1a53ef3424d78b7dd7525854774cd297c45dcc
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:55:44 -0400

    function_auto_path ../love/public/src/text_left_right_middle_random_space_nb.mjs

---
+++ public/src/text_left_right_middle_random.mjs
@@ -0,0 +1,7 @@
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
+import { text_random_or_empty } from "../../../love/public/src/text_random_or_empty.mjs";
+export function text_left_right_middle_random(t, left, right) {
+  let r2 = text_random_or_empty(t);
+  let combined2 = text_combine_multiple([left, r2, right]);
+  return combined2;
+}
