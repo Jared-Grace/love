b880359914bf716835e06ce69752383103782f22
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 02:31:39 -0400

    function_auto_path ../love/public/src/js_code_dot.mjs

--- web/public/src/js_code_dot.mjs
+++ web/public/src/js_code_dot.mjs
@@ -1,5 +1,6 @@
+import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
 import { js_operator_dot } from "../../../love/public/src/js_operator_dot.mjs";
 export function js_code_dot(left, right) {
-  let code = "(" + left + js_operator_dot() + right + ")";
+  let code = js_code_wrap_parenthesis("" + left + js_operator_dot() + right);
   return code;
 }
