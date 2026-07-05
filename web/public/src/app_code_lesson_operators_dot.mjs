daa6d2cd3d6c18011c95941ee860ed84b9fe4ceb
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:51:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot.mjs

--- web/public/src/app_code_lesson_operators_dot.mjs
+++ web/public/src/app_code_lesson_operators_dot.mjs
@@ -58,7 +58,7 @@ export function app_code_lesson_operators_dot() {
       left_transform,
       "using the dot",
       "something",
-      js_code_binary_spaced_nb,
+      js_code_binary,
     );
     let c3 = app_code_container_light_blue(root);
     html_div_cycle_code(c3, [
