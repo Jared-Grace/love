487b67d9a89b16165c9fbf67aca26b490a5d4195
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 05:54:48 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_slash_forward.mjs

--- web/public/src/app_code_lesson_operators_slash_forward.mjs
+++ web/public/src/app_code_lesson_operators_slash_forward.mjs
@@ -1,12 +1,12 @@
+import { js_operator_division_verb } from "../../../love/public/src/js_operator_division_verb.mjs";
 import { js_operator_division_name } from "../../../love/public/src/js_operator_division_name.mjs";
 import { js_operator_division } from "../../../love/public/src/js_operator_division.mjs";
 import { app_code_lesson_operators_asterisk_generic } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic.mjs";
-import { js_operator_multiplication_verb } from "../../../love/public/src/js_operator_multiplication_verb.mjs";
 import { multiply } from "../../../love/public/src/multiply.mjs";
 export function app_code_lesson_operators_slash_forward() {
   const operator = js_operator_division();
   const lesson_name = js_operator_division_name();
-  let verb = js_operator_multiplication_verb();
+  let verb = js_operator_division_verb();
   let left_transform = multiply;
   let r = app_code_lesson_operators_asterisk_generic(
     operator,
