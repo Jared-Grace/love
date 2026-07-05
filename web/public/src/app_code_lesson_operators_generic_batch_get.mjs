3e79c1f9c14f356f875584df29f8c670187c3012
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 21:46:44 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic_batch_get.mjs

--- web/public/src/app_code_lesson_operators_generic_batch_get.mjs
+++ web/public/src/app_code_lesson_operators_generic_batch_get.mjs
@@ -2,7 +2,6 @@ import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binar
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_nested_distinct } from "../../../love/public/src/each_nested_distinct.mjs";
-import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { range_1 } from "../../../love/public/src/range_1.mjs";
 export function app_code_lesson_operators_generic_batch_get(
   operator_js,
@@ -15,8 +14,11 @@ export function app_code_lesson_operators_generic_batch_get(
     function lambda4(la) {
       function lambda3(left, right) {
         let transformed = left_transform(left, right);
-        let list3 = js_code_binary_spaced_nb(transformed, operator_js, right);
-        let combined = text_combine_multiple(list3);
+        let combined = js_code_binary_spaced_nb(
+          transformed,
+          operator_js,
+          right,
+        );
         la(combined);
       }
       each_nested_distinct(lefts, rights, lambda3);
