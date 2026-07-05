6eec1f24e01813657acb66957bc0fab87b5af38e
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 01:06:52 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_plus_unary.mjs

--- web/public/src/app_code_lesson_operators_plus_unary.mjs
+++ web/public/src/app_code_lesson_operators_plus_unary.mjs
@@ -1,9 +1,7 @@
+import { list_adder_each } from "../../../love/public/src/list_adder_each.mjs";
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
-import { text_left_right_middle_random_space_nb } from "../../../love/public/src/text_left_right_middle_random_space_nb.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
-import { each_nested_args_both_range_1_list_adder } from "../../../love/public/src/each_nested_args_both_range_1_list_adder.mjs";
 import { app_code_lesson_operators_generic_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get_max.mjs";
 import { js_operator_plus_name } from "../../../love/public/src/js_operator_plus_name.mjs";
 import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
@@ -26,21 +24,12 @@ export function app_code_lesson_operators_plus_unary() {
   let left_transform = add;
   let max = app_code_lesson_operators_generic_batch_get_max();
   let batch = function batch_get() {
-    let list = each_nested_args_both_range_1_list_adder(
-      max,
-      lambda$left$right$la,
-    );
-    function lambda$left$right$la(left, right, la) {
-      "there are 4 added at once";
-      "valid, invalid, valid, invalid";
-      let combined5 = text_combine(operator, right);
-      let combined2 = text_left_right_middle_random_space_nb(right, operator);
-      let transformed = left_transform(left, right);
-      let combined3 = js_code_binary_spaced_nb(transformed, operator, right);
-      let combined4 = text_left_right_middle_random_space_nb(left, operator);
-      each([combined5, combined2, combined3, combined4], la);
+    let list = list_adder_each(max, on_each);
+    function on_each(item, la) {
+      let combined5 = text_combine(operator, item);
+      let combined2 = text_combine(operator, item);
+      each([combined5, combined2], la);
     }
-    ("hence cycle of 2: (valid, invalid)");
     list_shuffle_cycled(list, 2);
     return list;
   };
