025f82af01218f0ffd4effc96012e499285b494e
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 03:46:09 -0400

    function_auto_path ../love/public/src/app_code_container_light.mjs

--- public/src/app_code_container_light.mjs
+++ public/src/app_code_container_light.mjs
@@ -8,7 +8,7 @@ export function app_code_container_light(parent) {
   html_style_margin_y(container, 0.3 + "em");
   app_replace_button_symbol_style_box_shadow_value_width_set(
     container,
-    "hsl(210, 75%, 32%)",
+    "hsl(210, 50%, 50%)",
     "0.05",
   );
   return container;
