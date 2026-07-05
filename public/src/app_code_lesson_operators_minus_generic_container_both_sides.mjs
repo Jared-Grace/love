66883ec688220c84b60bbaea3eca94e5f052ba13
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:46:37 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs

--- public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs
+++ public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs
@@ -1,3 +1,5 @@
+import { list_last } from "../../../love/public/src/list_last.mjs";
+import { list_first } from "../../../love/public/src/list_first.mjs";
 import { text_articled_pad_space } from "../../../love/public/src/text_articled_pad_space.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 import { html_div_code_multiple } from "../../../love/public/src/html_div_code_multiple.mjs";
@@ -12,13 +14,15 @@ export function app_code_lesson_operators_minus_generic_container_both_sides(
   example_get,
 ) {
   let c = app_code_container_light_blue(root);
-  let u = text_first_upper_to(
-    "when " +
-      text_when +
-      " there is" +
+  let t = text_first_upper_to("when ");
+  let start = [t];
+  let end = [
+    " there is" +
       text_articled_pad_space(noun) +
       "on both the left and right sides of the ",
-  );
+  ];
+  let first = list_first(list);
+  let last = list_last(list2);
   html_div_cycle_code(c, [u, operator, " : "]);
   const right = "right";
   let left = "left";
