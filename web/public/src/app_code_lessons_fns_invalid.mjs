a3efab216f9a8cca2f2feb249c2984bd85c828fb
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 02:27:06 -0400

    function_auto_path ../love/public/src/app_code_lessons_fns.mjs

---
+++ web/public/src/app_code_lessons_fns_invalid.mjs
@@ -0,0 +1,5 @@
+import { app_code_lesson_operators_plus_unary } from "../../../love/public/src/app_code_lesson_operators_plus_unary.mjs";
+export function app_code_lessons_fns_invalid() {
+  let r2 = [app_code_lesson_operators_plus_unary];
+  return r2;
+}
