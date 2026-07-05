33bf1f0dc7f3f6a8163a784c124016b78b351828
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:43:11 -0400

    function_auto_path ../love/public/src/each_nested_args_range_1_list_adder.mjs

--- web/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs
+++ web/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs
@@ -2,7 +2,7 @@ import { text_left_right_middle_random_space_nb } from "../../../love/public/src
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { app_code_lesson_operators_generic_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get_max.mjs";
-import { each_nested_args_both_range_1_list_adder } from "../../../love/public/src/each_nested_args_both_range_1_list_adder.mjs";
+import { each_nested_args_range_1_list_adder } from "../../../love/public/src/each_nested_args_range_1_list_adder.mjs";
 import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
 export function app_code_lesson_operators_generic_batch_get_binary(
   operator_js,
@@ -10,10 +10,7 @@ export function app_code_lesson_operators_generic_batch_get_binary(
 ) {
   let max = app_code_lesson_operators_generic_batch_get_max();
   let r = function batch_get() {
-    let list = each_nested_args_both_range_1_list_adder(
-      max,
-      lambda$left$right$la,
-    );
+    let list = each_nested_args_range_1_list_adder(max, lambda$left$right$la);
     function lambda$left$right$la(left, right, la) {
       let transformed = left_transform(left, right);
       let combined = js_code_binary_spaced_nb(transformed, operator_js, right);
--- web/public/src/each_nested_args_range_1_list_adder.mjs
+++ web/public/src/each_nested_args_range_1_list_adder.mjs
@@ -1,9 +1,6 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { each_nested_args_both_range_1 } from "../../../love/public/src/each_nested_args_both_range_1.mjs";
-export function each_nested_args_both_range_1_list_adder(
-  max,
-  lambda$left$right$la,
-) {
+export function each_nested_args_range_1_list_adder(max, lambda$left$right$la) {
   function lambda(la) {
     each_nested_args_both_range_1(max, lambda$left$right);
     function lambda$left$right(left, right) {
