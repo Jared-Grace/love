08f91f7a675ab463928e68d2239faa247a63f414
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:53:54 -0400

    function_auto_path ../love/public/src/text_random_or_empty.mjs

--- public/src/text_random_or_empty.mjs
+++ public/src/text_random_or_empty.mjs
@@ -1,5 +1,5 @@
 import { list_random_item } from "../../../love/public/src/list_random_item.mjs";
-export function text_random_or_empty(a) {
-  let r = list_random_item(["", a]);
+export function text_random_or_empty(t) {
+  let r = list_random_item(["", t]);
   return r;
 }
