a370e9f5b869545b0b4d09fd5205cc1f1eda43c8
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:37:52 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_plus.mjs

--- public/src/app_code_lesson_operators_plus.mjs
+++ public/src/app_code_lesson_operators_plus.mjs
@@ -1,11 +1,11 @@
+import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
 import { digit_negative_random } from "../../../love/public/src/digit_negative_random.mjs";
 import { js_operator_plus_name } from "../../../love/public/src/js_operator_plus_name.mjs";
 import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
 import { app_code_lesson_operators_minus_generic } from "../../../love/public/src/app_code_lesson_operators_minus_generic.mjs";
-import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
 import { add } from "../../../love/public/src/add.mjs";
 export function app_code_lesson_operators_plus() {
-  const operator = js_operator_minus();
+  const operator = js_operator_plus();
   const operator_name = js_operator_plus_name();
   let math_name = operator_name;
   let verb = "subtract";
