0e96aefc79bd1d1491673085ad1b73941df08e50
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:51:03 -0400

    function_auto_path ../love/public/src/app_code_lesson_named.mjs

--- public/src/app_code_lesson_named.mjs
+++ public/src/app_code_lesson_named.mjs
@@ -1,3 +1,4 @@
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { text_combine_middle_comma } from "../../../love/public/src/text_combine_middle_comma.mjs";
 import { function_rename_open_generic_args } from "../../../love/public/src/function_rename_open_generic_args.mjs";
@@ -7,6 +8,7 @@ export async function app_code_lesson_named(
   plugin_fn,
   args_comma,
 ) {
+  arguments_assert(arguments, 3);
   let name_new = await app_code_lesson_add(fn_base_name);
   let combined = text_combine_middle_comma(name_new, args_comma);
   log(app_code_lesson_named.name, {
