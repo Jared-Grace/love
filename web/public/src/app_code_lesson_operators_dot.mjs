928be8462b46b1e33d8c836759c6428e79292944
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 02:33:23 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot.mjs

--- web/public/src/app_code_lesson_operators_dot.mjs
+++ web/public/src/app_code_lesson_operators_dot.mjs
@@ -1,12 +1,11 @@
 import { js_operator_dot } from "../../../love/public/src/js_operator_dot.mjs";
 import { js_operator_minus_verb } from "../../../love/public/src/js_operator_minus_verb.mjs";
-import { js_operator_minus_name } from "../../../love/public/src/js_operator_minus_name.mjs";
 import { digit_positive_random } from "../../../love/public/src/digit_positive_random.mjs";
 import { app_code_lesson_operators_minus_generic } from "../../../love/public/src/app_code_lesson_operators_minus_generic.mjs";
 import { add } from "../../../love/public/src/add.mjs";
 export function app_code_lesson_operators_dot() {
   const operator = js_operator_dot();
-  const operator_name = js_operator_minus_name();
+  const operator_name = "period";
   let math_name = "property access";
   let verb = js_operator_minus_verb();
   let sign = "negative";
