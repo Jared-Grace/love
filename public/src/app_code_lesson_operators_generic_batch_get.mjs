859ce62abb7ef4f8bfc77ef1c35ef759a95f4e04
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:45:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic_batch_get.mjs

--- public/src/app_code_lesson_operators_generic_batch_get.mjs
+++ public/src/app_code_lesson_operators_generic_batch_get.mjs
@@ -1,20 +1,16 @@
-import { each_nested_args_both_range_1 } from "../../../love/public/src/each_nested_args_both_range_1.mjs";
+import { each_nested_args_both_range_1_list_adder } from "../../../love/public/src/each_nested_args_both_range_1_list_adder.mjs";
 import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
 export function app_code_lesson_operators_generic_batch_get(
   operator_js,
   left_transform,
 ) {
   let max = 7;
   let r = function lambda2() {
-    function lambda4(la) {
-      each_nested_args_both_range_1(max, lambda$left$right);
-      function lambda$left$right(left, right) {
-        lambda$left$right$la(left, right, la);
-      }
-    }
-    let list = list_adder(lambda4);
+    let list = each_nested_args_both_range_1_list_adder(
+      max,
+      lambda$left$right$la,
+    );
     list_shuffle(list);
     return list;
     function lambda$left$right$la(left, right, la) {
