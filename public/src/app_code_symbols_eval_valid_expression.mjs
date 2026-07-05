de53f913493890e9e7d5625fe94dd69e04673fa8
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:44:42 -0400

    function_auto_path ../love/public/src/app_code_symbols_eval_valid_expression.mjs

--- public/src/app_code_symbols_eval_valid_expression.mjs
+++ public/src/app_code_symbols_eval_valid_expression.mjs
@@ -1,3 +1,4 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { boolean_to_text_validity } from "../../../love/public/src/boolean_to_text_validity.mjs";
 import { js_identifier_is } from "../../../love/public/src/js_identifier_is.mjs";
 import { js_parse_expression_try } from "../../../love/public/src/js_parse_expression_try.mjs";
@@ -5,5 +6,9 @@ export function app_code_symbols_eval_valid_expression(symbols) {
   let expression = js_parse_expression_try(symbols);
   let ii = js_identifier_is(expression);
   let validity = boolean_to_text_validity(ii);
+  log(app_code_symbols_eval_valid_expression.name, {
+    symbols,
+    validity,
+  });
   return validity;
 }
