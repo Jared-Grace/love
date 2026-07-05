ecfbcf53f052312f186d1bd878e058e1b93e2ed6
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:27:04 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_numbers.mjs

--- web/public/src/app_code_lesson_operators_dot_numbers.mjs
+++ web/public/src/app_code_lesson_operators_dot_numbers.mjs
@@ -49,6 +49,7 @@ export function app_code_lesson_operators_dot_numbers() {
       " with identifiers:",
     ]);
     html_div_code(c2, "person.name");
+    return;
     let combined = js_code_binary("person", operator, "age");
     let example_get = lambda_value(combined);
     app_code_lesson_operators_minus_generic_container_both_sides(
