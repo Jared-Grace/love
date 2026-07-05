63bd57c086370e200422a195c0716059f9d2fce7
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:53:19 -0400

    function_auto_path ../love/public/src/app_code_lesson_named.mjs

--- public/src/app_code_lesson_named.mjs
+++ public/src/app_code_lesson_named.mjs
@@ -12,11 +12,12 @@ export async function app_code_lesson_named(
   arguments_assert(arguments, 3);
   let name_new = await app_code_lesson_add(fn_base_name);
   let combined = text_combine_middle_comma(fn_base_name, args_comma);
-  log(app_code_lesson_named.name, {
-    combined,
-  });
   let f_name_new = null;
   ({ f_name_new } = await function_name_new_get_args(plugin_fn, combined));
+  log(app_code_lesson_named.name, {
+    combined,
+    f_name_new,
+  });
   let r = await function_rename_open(name_new, f_name_new);
   return r;
 }
