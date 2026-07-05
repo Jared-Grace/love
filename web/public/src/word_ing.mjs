fb858a41bf9d7fa1ff125cad57d928120263c41c
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 06:02:00 -0400

    function_auto_path ../love/public/src/word_ing.mjs

--- web/public/src/word_ing.mjs
+++ web/public/src/word_ing.mjs
@@ -1,4 +1,6 @@
+import { text_suffix_without_try } from "../../../love/public/src/text_suffix_without_try.mjs";
 export function word_ing(verb) {
+  let without = text_suffix_without_try(t, suffix);
   let r = verb + "ing";
   return r;
 }
