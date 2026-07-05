cf05d186e766067238b268a56e7d3b338a038650
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:42:48 -0400

    function_auto_path ../love/public/src/each_nested_args_both_range_1.mjs

--- public/src/each_nested_args_both_range_1.mjs
+++ public/src/each_nested_args_both_range_1.mjs
@@ -2,7 +2,7 @@ import { each_nested_args_both } from "../../../love/public/src/each_nested_args
 import { range_1 } from "../../../love/public/src/range_1.mjs";
 export function each_nested_args_both_range_1(max, lambda$left$right) {
   let lefts = range_1(max);
-  let rights = range_1(max);
+  let rights = lefts;
   function lambda(left, right) {
     lambda$left$right(left, right);
   }
