e85b541412b3f526c8a0e30b18d12a036ec2f24c
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 01:01:02 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_plus.mjs

--- web/public/src/app_code_lesson_operators_plus.mjs
+++ web/public/src/app_code_lesson_operators_plus.mjs
@@ -1,8 +1,7 @@
+import { digit_negative_random_parenthesis_wrapped } from "../../../love/public/src/digit_negative_random_parenthesis_wrapped.mjs";
 import { js_operator_plus_verb } from "../../../love/public/src/js_operator_plus_verb.mjs";
 import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
-import { digit_negative_random } from "../../../love/public/src/digit_negative_random.mjs";
 import { js_operator_plus_name } from "../../../love/public/src/js_operator_plus_name.mjs";
-import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
 import { app_code_lesson_operators_minus_generic } from "../../../love/public/src/app_code_lesson_operators_minus_generic.mjs";
 import { add } from "../../../love/public/src/add.mjs";
 export function app_code_lesson_operators_plus() {
@@ -13,8 +12,7 @@ export function app_code_lesson_operators_plus() {
   let sign = "positive";
   let left_transform = add;
   function lambda() {
-    let combined = digit_negative_random();
-    let wrapped = js_code_wrap_parenthesis(combined);
+    let wrapped = digit_negative_random_parenthesis_wrapped();
     return wrapped;
   }
   let r = app_code_lesson_operators_minus_generic(
---
+++ web/public/src/digit_negative_random_parenthesis_wrapped.mjs
@@ -0,0 +1,7 @@
+import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
+import { digit_negative_random } from "../../../love/public/src/digit_negative_random.mjs";
+export function digit_negative_random_parenthesis_wrapped() {
+  let combined = digit_negative_random();
+  let wrapped = js_code_wrap_parenthesis(combined);
+  return wrapped;
+}
