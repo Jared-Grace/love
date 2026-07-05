5ce305d4ac2575dc2e27953ff2fc30d636e4b658
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 06:51:08 -0400

    function_auto_path ../love/public/src/command_line_generic.mjs

--- web/public/src/command_line_generic.mjs
+++ web/public/src/command_line_generic.mjs
@@ -48,7 +48,7 @@ export async function command_line_generic(command, extra) {
             `Command exited with code ${code}\n\nSTDOUT:\n${stdout}\n\nSTDERR:\n${stderr}`,
           ),
         );
-        if (true) {
+        if (false) {
           await file_temp_json_open({
             code,
             stdout,
