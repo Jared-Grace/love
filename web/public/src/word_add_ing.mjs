010787b4707189a264b806e512048d80a89636c4
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 06:15:00 -0400

    function_auto_path ../love/public/src/word_add_ing.mjs

--- web/public/src/word_add_ing.mjs
+++ web/public/src/word_add_ing.mjs
@@ -1,5 +1,5 @@
 import { text_suffix_without_try } from "../../../love/public/src/text_suffix_without_try.mjs";
-export function word_ing(w) {
+export function word_add_ing(w) {
   w = text_suffix_without_try(w, "e");
   let r = w + "ing";
   return r;
