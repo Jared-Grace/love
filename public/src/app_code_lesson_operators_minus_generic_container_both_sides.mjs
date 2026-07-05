1e945d2351fe9ce918cde75a33d6975a89402863
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:54:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs

--- public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs
+++ public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs
@@ -1,9 +1,7 @@
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
-import { app_code_lesson_operators_generic_batch_get } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get.mjs";
 import { html_div_code_multiple } from "../../../love/public/src/html_div_code_multiple.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 export function app_code_lesson_operators_minus_generic_container_both_sides(
   root,
   operator,
@@ -27,13 +25,4 @@ export function app_code_lesson_operators_minus_generic_container_both_sides(
   );
   html_div_cycle_code(c2, [u, operator, " : "]);
   html_div_code_multiple(c2, [first, combined3]);
-  function example_get() {
-    let batch_binary = app_code_lesson_operators_generic_batch_get(
-      operator,
-      left_transform,
-    );
-    let list2 = batch_binary();
-    let first = list_first(list2);
-    return first;
-  }
 }
