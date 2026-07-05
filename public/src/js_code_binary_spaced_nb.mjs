c2a774438e593a80d14c11731b4cabaf8d88b7ea
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:49:41 -0400

    function_auto_path ../love/public/src/js_code_binary_spaced_nb.mjs

--- public/src/js_code_binary_spaced_nb.mjs
+++ public/src/js_code_binary_spaced_nb.mjs
@@ -1,7 +1,8 @@
-import { js_code_binary_spaced_nb_generic } from "../../../love/public/src/js_code_binary_spaced_nb_generic.mjs";
+import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { list_between_space_nb } from "../../../love/public/src/list_between_space_nb.mjs";
 export function js_code_binary_spaced_nb(left, operator_js, right) {
   let mapper = list_between_space_nb;
-  let combined = js_code_binary_spaced_nb_generic(left, operator_js, right);
+  let parts = mapper([left, operator_js, right]);
+  let combined = text_combine_multiple(parts);
   return combined;
 }
