bb1a00af3298e22b8b2205d78f8362c51a0806a9
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:28:43 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_plus.mjs

--- web/public/src/app_code_lesson_operators_plus.mjs
+++ web/public/src/app_code_lesson_operators_plus.mjs
@@ -1,4 +1,3 @@
-import { digit_positive_random } from "../../../love/public/src/digit_positive_random.mjs";
 import { app_code_lesson_operators_minus_generic } from "../../../love/public/src/app_code_lesson_operators_minus_generic.mjs";
 import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
 import { add } from "../../../love/public/src/add.mjs";
@@ -9,6 +8,7 @@ export function app_code_lesson_operators_plus() {
   let verb = "subtract";
   let sign = "negative";
   let left_transform = add;
+  function lambda() {}
   let r = app_code_lesson_operators_minus_generic(
     operator,
     left_transform,
@@ -16,7 +16,7 @@ export function app_code_lesson_operators_plus() {
     sign,
     verb,
     math_name,
-    digit_positive_random,
+    lambda,
   );
   return r;
 }
