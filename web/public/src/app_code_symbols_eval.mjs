59ffab94ca183bc924d616d3a55632080ea57d5c
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:32:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_numbers.mjs

---
+++ web/public/src/app_code_symbols_eval.mjs
@@ -0,0 +1,10 @@
+import { boolean_to_text_validity } from "../../../love/public/src/boolean_to_text_validity.mjs";
+import { throws_not } from "../../../love/public/src/throws_not.mjs";
+export function app_code_symbols_eval(symbols) {
+  function lambda2() {
+    eval(symbols);
+  }
+  let valid = throws_not(lambda2);
+  let result = boolean_to_text_validity(valid);
+  return result;
+}
