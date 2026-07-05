65a354183073898190a42b2a431029beb421ac0a
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:10:01 -0400

    function_auto_path ../love/public/src/html_div_cycle_code_multiple.mjs

--- web/public/src/html_div_cycle_code_multiple.mjs
+++ web/public/src/html_div_cycle_code_multiple.mjs
@@ -1,4 +1,5 @@
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 export function html_div_cycle_code_multiple(c2, parts) {
-  return html_div_cycle_code(c2, parts);
+  let r = html_div_cycle_code(c2, parts);
+  return r;
 }
