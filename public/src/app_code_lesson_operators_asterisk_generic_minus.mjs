1b22a79896c27ed7a8843308661053b9308ef7fd
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:39:29 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_asterisk_generic.mjs

---
+++ public/src/app_code_lesson_operators_asterisk_generic_minus.mjs
@@ -0,0 +1,27 @@
+import { html_div_code_multiple } from "../../../love/public/src/html_div_code_multiple.mjs";
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
+import { digit_positive_random } from "../../../love/public/src/digit_positive_random.mjs";
+import { text_combine_right_fn } from "../../../love/public/src/text_combine_right_fn.mjs";
+import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
+import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
+import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
+export function app_code_lesson_operators_asterisk_generic_minus(
+  root,
+  operator,
+) {
+  let c3 = app_code_container_light_blue(root);
+  let minus = js_operator_minus();
+  html_div_cycle_code(c3, [
+    "Remember, for ",
+    minus,
+    " it's possible to have a number only on the right and not on the left: ",
+  ]);
+  let combined2 = text_combine_right_fn(minus, digit_positive_random);
+  const right2 = "right";
+  let combined4 = text_combine(minus, right2);
+  html_div_code_multiple(c3, [combined2, combined4]);
+  html_div_cycle_code(c3, [
+    "However JavaScript does not have something like this for ",
+    operator,
+  ]);
+}
