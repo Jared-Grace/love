1226b72014020a1c522cbfd4e09864ddb7d126cb
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:44:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs

--- public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs
+++ public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs
@@ -3,7 +3,6 @@ import { text_first_upper_to } from "../../../love/public/src/text_first_upper_t
 import { html_div_code_multiple } from "../../../love/public/src/html_div_code_multiple.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
 export function app_code_lesson_operators_minus_generic_container_both_sides(
   root,
   operator,
@@ -17,15 +16,12 @@ export function app_code_lesson_operators_minus_generic_container_both_sides(
   const right = "right";
   let left = "left";
   let combined3 = code_to(left, operator, right);
-  let t = text_first_upper_to("when ");
-  let u = list_concat(
-    [t] +
-      [text_when] +
-      [
-        " there is" +
-          text_articled_pad_space(noun) +
-          "on both the left and right sides of the ",
-      ],
+  let u = text_first_upper_to(
+    "when " +
+      text_when +
+      " there is" +
+      text_articled_pad_space(noun) +
+      "on both the left and right sides of the ",
   );
   html_div_cycle_code(c, [u, operator, " : "]);
   html_div_code_multiple(c, [first, combined3]);
