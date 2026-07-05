37cb42ca90f6e21f99a42632484a922f58728c93
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:56:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic_batch_get_unary.mjs

--- web/public/src/app_code_lesson_operators_generic_batch_get_unary.mjs
+++ web/public/src/app_code_lesson_operators_generic_batch_get_unary.mjs
@@ -25,7 +25,7 @@ export function app_code_lesson_operators_generic_batch_get_unary(
       );
       let transformed = left_transform(left, right);
       let combined3 = js_code_binary_spaced_nb(transformed, operator_js, right);
-      let combined4 = text_combine(left, operator_js);
+      let combined4 = text_left_right_middle_random_space_nb(left, operator_js);
       each([combined, combined2, combined3, combined4], la);
     }
   };
