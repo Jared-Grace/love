55bb2f3b082db0e981d4d5f5fb98cc78b5895fc2
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 02:41:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_division.mjs

--- public/src/app_code_lesson_operators_division.mjs
+++ public/src/app_code_lesson_operators_division.mjs
@@ -1,7 +1,8 @@
+import { js_operator_division } from "../../../love/public/src/js_operator_division.mjs";
 import { multiply } from "../../../love/public/src/multiply.mjs";
 import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
 export function app_code_lesson_operators_division() {
-  const operator = "/";
+  const operator = js_operator_division();
   const operator_name = "forward slash";
   let math_name = "division";
   let verb = "divide";
