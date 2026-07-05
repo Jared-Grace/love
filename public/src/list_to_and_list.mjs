f1f040e6c545e53008957505b85050682b493044
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 02:43:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_symbol_first.mjs

---
+++ public/src/list_to_and_list.mjs
@@ -0,0 +1,5 @@
+import { list_to_or_list_generic } from "../../../love/public/src/list_to_or_list_generic.mjs";
+export function list_to_and_list(mapped2) {
+  let concated3 = list_to_or_list_generic(mapped2, "and");
+  return concated3;
+}
