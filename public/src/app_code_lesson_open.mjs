59f81879b3b11c30a1636bf6657bdc5b246a47bc
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:56:08 -0400

    function_auto_path ../love/public/src/app_code_lesson_open.mjs

--- public/src/app_code_lesson_open.mjs
+++ public/src/app_code_lesson_open.mjs
@@ -1,7 +1,7 @@
 import { function_open } from "../../../love/public/src/function_open.mjs";
 import { app_code_lesson_add } from "../../../love/public/src/app_code_lesson_add.mjs";
 export async function app_code_lesson_open(fn_base_name) {
-  let r = await app_code_lesson_add(fn_base_name);
-  await function_open(f_name);
-  return r;
+  let name_new = await app_code_lesson_add(fn_base_name);
+  await function_open(name_new);
+  return name_new;
 }
