da0f206447c95a7356a5df6cc2a8569ad5c1ebd3
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 23:47:53 -0400

    function_auto_path ../love/public/src/app_code_lesson_add.mjs

--- web/public/src/app_code_lesson_add.mjs
+++ web/public/src/app_code_lesson_add.mjs
@@ -2,5 +2,6 @@ import { app_code_lessons_fns } from "../../../love/public/src/app_code_lessons_
 import { app_replace_rule_set_add_generic } from "../../../love/public/src/app_replace_rule_set_add_generic.mjs";
 export async function app_code_lesson_add(fn_base_name) {
   let fns_list = app_code_lessons_fns;
-  $r,await app_replace_rule_set_add_generic(fn_base_name, fns_list);
+  let r = await app_replace_rule_set_add_generic(fn_base_name, fns_list);
+  return r;
 }
