b4a1c203c5e5643da75d5033910143579cd48a67
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 21:38:58 -0400

    function_auto_path ../love/public/src/digit_random_operator.mjs

--- web/public/src/digit_random_operator.mjs
+++ web/public/src/digit_random_operator.mjs
@@ -1,7 +1,7 @@
+import { digit_positive_random } from "../../../love/public/src/digit_positive_random.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
-import { digit_random } from "../../../love/public/src/digit_random.mjs";
 export function digit_random_operator(operator) {
-  let right = digit_random();
+  let right = digit_positive_random();
   let combined = text_combine(operator, right);
   return combined;
 }
