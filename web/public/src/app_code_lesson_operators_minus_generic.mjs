efa0ed14084e5883ee0e59b46fc414e91c2647b4
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:40:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus_generic.mjs

--- web/public/src/app_code_lesson_operators_minus_generic.mjs
+++ web/public/src/app_code_lesson_operators_minus_generic.mjs
@@ -14,7 +14,6 @@ import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binar
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { text_articled_pad_space } from "../../../love/public/src/text_articled_pad_space.mjs";
-import { digit_positive_random_operator } from "../../../love/public/src/digit_positive_random_operator.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_lesson_operators_generic_batch_get_unary } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get_unary.mjs";
 import { app_code_lesson_operators_generic_batch_get } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get.mjs";
@@ -69,7 +68,7 @@ export function app_code_lesson_operators_minus_generic(
       operator,
       " : ",
     ]);
-    let combined2 = digit_positive_random_operator(operator);
+    let combined2 = text_combine_right_fn(operator, right_random_get);
     let combined4 = text_combine(operator, right);
     html_div_code_multiple(c3, [combined2, combined4]);
     let c4 = app_code_container_light_blue(root);
