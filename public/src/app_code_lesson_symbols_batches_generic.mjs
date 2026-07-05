97fe6caf8fd8a65f8784db6c7f45d8307e88631a
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 03:55:12 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

--- public/src/app_code_lesson_symbols_batches_generic.mjs
+++ public/src/app_code_lesson_symbols_batches_generic.mjs
@@ -1,3 +1,4 @@
+import { html_style_margin_top } from "../../../love/public/src/html_style_margin_top.mjs";
 import { text_replace_space_underscore } from "../../../love/public/src/text_replace_space_underscore.mjs";
 import { app_code_quiz_index_reset } from "../../../love/public/src/app_code_quiz_index_reset.mjs";
 import { app_code_quiz_index_set } from "../../../love/public/src/app_code_quiz_index_set.mjs";
@@ -164,6 +165,7 @@ export function app_code_lesson_symbols_batches_generic(
                 quiz_answer,
                 on_click,
               );
+              html_style_margin_top(component, value2);
               async function on_click() {
                 let eq2 = equal(quiz_answer, answer);
                 if (eq2) {
