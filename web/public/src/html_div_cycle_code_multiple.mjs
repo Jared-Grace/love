8874b2afe09f203773b838a5dab9e6c2efb7f675
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:09:58 -0400

    w c html_div_cycle_code,multiple

--- web/public/src/html_div_cycle_code_multiple.mjs
+++ web/public/src/html_div_cycle_code_multiple.mjs
@@ -1,1 +1,4 @@
-export function html_div_cycle_code_multiple() {}
+import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
+export function html_div_cycle_code_multiple(c2, parts) {
+  return html_div_cycle_code(c2, parts);
+}
