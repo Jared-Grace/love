9a10d7620b458642d98023b186fcf243b87de863
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:54:47 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_add_generic.mjs

--- public/src/app_replace_rule_set_add_generic.mjs
+++ public/src/app_replace_rule_set_add_generic.mjs
@@ -1,9 +1,9 @@
+import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
-import { function_copy_open } from "../../../love/public/src/function_copy_open.mjs";
 import { text_unique } from "../../../love/public/src/text_unique.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function app_replace_rule_set_add_generic(fn_base_name, fns_list) {
@@ -13,7 +13,7 @@ export async function app_replace_rule_set_add_generic(fn_base_name, fns_list) {
   log(app_replace_rule_set_add_generic.name, {
     unique,
   });
-  await function_copy_open(f, unique);
+  await function_copy(f, unique);
   let expression = js_parse_expression(unique);
   async function lambda(ast) {
     let elements = js_array_expression_single_elements(ast);
