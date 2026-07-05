f92b9ae4c6e854a54946b7fa390dffc79dc5429f
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 23:50:36 -0400

    r 2 app_code_lesson_operators_minus_2,plus

--- web/public/src/app_code_lesson_operators_plus.mjs
+++ web/public/src/app_code_lesson_operators_plus.mjs
@@ -1,7 +1,7 @@
 import { app_code_lesson_operators_minus_generic } from "../../../love/public/src/app_code_lesson_operators_minus_generic.mjs";
 import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
 import { add } from "../../../love/public/src/add.mjs";
-export function app_code_lesson_operators_minus_2() {
+export function app_code_lesson_operators_plus() {
   const operator = js_operator_minus();
   const operator_name = "minus sign";
   let math_name = "negation";
--- web/public/src/app_code_lessons_fns.mjs
+++ web/public/src/app_code_lessons_fns.mjs
@@ -1,4 +1,4 @@
-import { app_code_lesson_operators_minus_2 } from "../../../love/public/src/app_code_lesson_operators_minus_2.mjs";
+import { app_code_lesson_operators_plus } from "../../../love/public/src/app_code_lesson_operators_plus.mjs";
 import { app_code_lesson_operators_minus } from "../../../love/public/src/app_code_lesson_operators_minus.mjs";
 import { app_code_lesson_operators_division } from "../../../love/public/src/app_code_lesson_operators_division.mjs";
 import { app_code_lesson_operators_multiplication } from "../../../love/public/src/app_code_lesson_operators_multiplication.mjs";
@@ -29,7 +29,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_operators_multiplication,
     app_code_lesson_operators_division,
     app_code_lesson_operators_minus,
-    app_code_lesson_operators_minus_2,
+    app_code_lesson_operators_plus,
   ];
   return r;
 }
