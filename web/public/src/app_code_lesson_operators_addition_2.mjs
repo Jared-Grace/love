bd0eb4db054d9899babf06c5b767834a4423a68e
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:16:53 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_addition_2.mjs

--- web/public/src/app_code_lesson_operators_addition_2.mjs
+++ web/public/src/app_code_lesson_operators_addition_2.mjs
@@ -75,7 +75,7 @@ export function app_code_lesson_operators_addition_2() {
     function lambda2() {}
     let r2 = lambda_throws(lambda2);
     let throws = property_get(r2, "throws");
-    let valid = not(throws);
+    let valid = not(throws);$r,valid
   };
   let inside = text_first_upper_to(math_name);
   let name = "Operators " + text_wrap_parenthesis(inside);
