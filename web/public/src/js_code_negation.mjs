35b7154ee3fcba4606d0e18692cd7a1c3dc40324
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:30:54 -0400

    function_auto_path ../love/public/src/js_code_negation.mjs

--- web/public/src/js_code_negation.mjs
+++ web/public/src/js_code_negation.mjs
@@ -1,6 +1,7 @@
 import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export function js_code_negation(d) {
-  let combined2 = text_combine(js_operator_minus(), d);
+  let left = js_operator_minus();
+  let combined2 = text_combine(left, d);
   return combined2;
 }
