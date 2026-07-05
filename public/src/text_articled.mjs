5663ac9543ce4024df2a0e5403edafe8e70f988b
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:37:43 -0400

    function_auto_path ../love/public/src/text_articled.mjs

--- public/src/text_articled.mjs
+++ public/src/text_articled.mjs
@@ -3,7 +3,7 @@ import { list_vowels } from "../../../love/public/src/list_vowels.mjs";
 import { list_any_starts_with } from "../../../love/public/src/list_any_starts_with.mjs";
 export function text_articled(noun) {
   let exceptions = ["something"];
-  let includes = list_includes(list, item);
+  let includes = list_includes(list, item);$i
   let article = "a";
   let vowels = list_vowels();
   let any = list_any_starts_with(noun, vowels);
