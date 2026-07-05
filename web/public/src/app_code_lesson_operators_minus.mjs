636813934b3e6bfc5a0a3021587a25655b214069
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 03:05:55 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus.mjs

--- web/public/src/app_code_lesson_operators_minus.mjs
+++ web/public/src/app_code_lesson_operators_minus.mjs
@@ -1,3 +1,4 @@
+import { js_operator_minus_name_root } from "../../../love/public/src/js_operator_minus_name_root.mjs";
 import { js_operator_minus_verb } from "../../../love/public/src/js_operator_minus_verb.mjs";
 import { js_operator_minus_name } from "../../../love/public/src/js_operator_minus_name.mjs";
 import { digit_positive_random } from "../../../love/public/src/digit_positive_random.mjs";
@@ -7,7 +8,7 @@ import { add } from "../../../love/public/src/add.mjs";
 export function app_code_lesson_operators_minus() {
   const operator = js_operator_minus();
   const operator_name = js_operator_minus_name();
-  let math_name = "minus";
+  let math_name = js_operator_minus_name_root();
   let verb = js_operator_minus_verb();
   let sign = "negative";
   let left_transform = add;
