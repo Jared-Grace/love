031d9d986d5c1d4f88b42d1154b4251a6471a232
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 07:32:48 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot.mjs

--- public/src/app_code_lesson_operators_dot.mjs
+++ public/src/app_code_lesson_operators_dot.mjs
@@ -47,11 +47,12 @@ export function app_code_lesson_operators_dot() {
     let d2 = html_div_text(c2, "We can use ");
     app_code_operators_arithmetic(d2);
     html_span_text(d2, " with numbers");
-    let div = html_div_cycle_code(c2, [
+    html_div_cycle_code(c2, [
       "And we can use ",
       operator,
       " with identifiers :",
     ]);
+    html_div_code(c2, "a.b.c");
     let verb = js_operator_asterisk_verb();
     let operator2 = js_operator_asterisk();
     app_code_lesson_operators_minus_generic_container_both_sides(
