0090881c0e26bb328d3a6142b32ca96a6b49bb53
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:41:20 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic_batch_get.mjs

--- web/public/src/app_code_lesson_operators_generic_batch_get.mjs
+++ web/public/src/app_code_lesson_operators_generic_batch_get.mjs
@@ -1,3 +1,4 @@
+import { newFunction } from "../../../love/public/src/newFunction.mjs";
 import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
@@ -8,24 +9,23 @@ export function app_code_lesson_operators_generic_batch_get(
   left_transform,
 ) {
   let max = 7;
-  let r = function lambda() {
+  let r = function lambda2() {
     let lefts = range_1(max);
     let rights = range_1(max);
     function lambda4(la) {
-      function lambda3(left, right) {
-        let transformed = left_transform(left, right);
-        let combined = js_code_binary_spaced_nb(
-          transformed,
-          operator_js,
-          right,
-        );
-        la(combined);
+      async function lambda3(left, right) {
+        await newFunction(left, right, la);
       }
       each_nested_distinct(lefts, rights, lambda3);
     }
     let list = list_adder(lambda4);
     list_shuffle(list);
     return list;
+    function newFunction(left, right, la) {
+      let transformed = left_transform(left, right);
+      let combined = js_code_binary_spaced_nb(transformed, operator_js, right);
+      la(combined);
+    }
   };
   return r;
 }
