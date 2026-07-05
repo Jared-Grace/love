f18c62daf1ca70beb7be2511d7ed6d55c0465050
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:25:53 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

--- public/src/text_replace_space_underscore.mjs
+++ public/src/text_replace_space_underscore.mjs
@@ -1,5 +1,5 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
-export function text_replace_space_underscore(name_old) {
-  let replaced = text_replace(name_old, " ", "_");
+export function text_replace_space_underscore(t) {
+  let replaced = text_replace(t, " ", "_");
   return replaced;
 }
