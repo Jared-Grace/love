5f8949da7365101a3cb812d497883c7eb4f2e79c
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:53:47 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_negation.mjs

--- public/src/app_code_lessons_fns.mjs
+++ public/src/app_code_lessons_fns.mjs
@@ -1,4 +1,4 @@
-import { app_code_lesson_operators_minus_2 } from "../../../love/public/src/app_code_lesson_operators_minus_2.mjs";
+import { app_code_lesson_operators_negation } from "../../../love/public/src/app_code_lesson_operators_negation.mjs";
 import { app_code_lesson_operators_plus } from "../../../love/public/src/app_code_lesson_operators_plus.mjs";
 import { app_code_lesson_operators_minus } from "../../../love/public/src/app_code_lesson_operators_minus.mjs";
 import { app_code_lesson_operators_division } from "../../../love/public/src/app_code_lesson_operators_division.mjs";
@@ -31,7 +31,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_operators_division,
     app_code_lesson_operators_minus,
     app_code_lesson_operators_plus,
-    app_code_lesson_operators_minus_2,
+    app_code_lesson_operators_negation,
   ];
   return r;
 }
