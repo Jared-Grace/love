eb74a357b6fd6c495da85557dbefd1833e56946b
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 03:29:21 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs

--- web/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs
+++ web/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs
@@ -1,7 +1,6 @@
 import { text_left_right_middle_random_space_nb } from "../../../love/public/src/text_left_right_middle_random_space_nb.mjs";
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_code_lesson_operators_generic_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get_max.mjs";
 import { each_nested_args_both_range_1_list_adder } from "../../../love/public/src/each_nested_args_both_range_1_list_adder.mjs";
 import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
@@ -18,13 +17,18 @@ export function app_code_lesson_operators_generic_batch_get_binary(
     function lambda$left$right$la(left, right, la) {
       "there are 4 added at once";
       "valid, invalid, valid, invalid";
-      let combined = text_combine(operator_js, right);
+      let transformed = left_transform(left, right);
+      let combined = js_code_binary_spaced_nb(transformed, operator_js, right);
       let combined2 = text_left_right_middle_random_space_nb(
         right,
         operator_js,
       );
-      let transformed = left_transform(left, right);
-      let combined3 = js_code_binary_spaced_nb(transformed, operator_js, right);
+      let transformed2 = left_transform(left, right);
+      let combined3 = js_code_binary_spaced_nb(
+        transformed2,
+        operator_js,
+        right,
+      );
       let combined4 = text_left_right_middle_random_space_nb(left, operator_js);
       each([combined, combined2, combined3, combined4], la);
     }
