ca21d8e4bcf3e8e27ad138c7362b0070a9389525
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 23:55:42 -0400

    ps

--- web/public/src/app_code_lessons_fns.mjs
+++ web/public/src/app_code_lessons_fns.mjs
@@ -29,7 +29,6 @@ export function app_code_lessons_fns() {
     app_code_lesson_operators_multiplication,
     app_code_lesson_operators_division,
     app_code_lesson_operators_minus,
-    app_code_lesson_operators_plus,
   ];
   return r;
 }
