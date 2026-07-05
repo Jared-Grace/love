8a45e3eb72f87874c271b1447f3daa3803937746
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 02:43:58 -0400

    function_auto_path ../love/public/src/list_to_and_list.mjs

--- web/public/src/list_to_and_list.mjs
+++ web/public/src/list_to_and_list.mjs
@@ -1,5 +1,6 @@
 import { list_to_or_list_generic } from "../../../love/public/src/list_to_or_list_generic.mjs";
-export function list_to_and_list(mapped2) {
-  let concated3 = list_to_or_list_generic(mapped2, "and");
-  return concated3;
+export function list_to_and_list(list) {
+  const word_relationship = "and";
+  let combined = list_to_or_list_generic(list, word_relationship);
+  return combined;
 }
