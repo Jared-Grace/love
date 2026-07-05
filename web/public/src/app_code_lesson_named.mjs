ef359c253b75581280efab9bfcb639f82a67e81a
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 23:00:51 -0400

    function_auto_path ../love/public/src/app_code_lesson_named.mjs

--- web/public/src/app_code_lesson_named.mjs
+++ web/public/src/app_code_lesson_named.mjs
@@ -1,6 +1,10 @@
 import { function_rename_open_generic_args } from "../../../love/public/src/function_rename_open_generic_args.mjs";
 import { app_code_lesson_add } from "../../../love/public/src/app_code_lesson_add.mjs";
-export async function app_code_lesson_named(fn_base_name) {
+export async function app_code_lesson_named(
+  fn_base_name,
+  plugin_fn,
+  args_comma,
+) {
   let r = await app_code_lesson_add(fn_base_name);
   let r2 = await function_rename_open_generic_args(plugin_fn, args_comma);
   return r;
