73ba92409e3f414975e8f0fc3bbad1fc2e03a213
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 10:02:28 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus_generic.mjs

--- public/src/app_code_lesson_operators_minus_generic.mjs
+++ public/src/app_code_lesson_operators_minus_generic.mjs
@@ -1,13 +1,16 @@
-import { app_code_symbols_eval_valid_expression } from "../../../love/public/src/app_code_symbols_eval_valid_expression.mjs";
-import { app_code_lesson_operators_asterisk_generic_invalid } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic_invalid.mjs";
-import { app_code_lesson_operators_asterisk_generic_minus } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic_minus.mjs";
-import { app_code_lesson_operators_minus_generic_container_both_sides_number } from "../../../love/public/src/app_code_lesson_operators_minus_generic_container_both_sides_number.mjs";
+import { app_code_lesson_operators_minus_generic_container_both_sides } from "../../../love/public/src/app_code_lesson_operators_minus_generic_container_both_sides.mjs";
 import { text_combine_right_fn } from "../../../love/public/src/text_combine_right_fn.mjs";
 import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_batches_generic } from "../../../love/public/src/app_code_lesson_symbols_batches_generic.mjs";
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
 import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
+import { boolean_to_text_validity } from "../../../love/public/src/boolean_to_text_validity.mjs";
+import { throws_not } from "../../../love/public/src/throws_not.mjs";
+import { html_div_code } from "../../../love/public/src/html_div_code.mjs";
+import { html_div_cycle_code_multiple } from "../../../love/public/src/html_div_cycle_code_multiple.mjs";
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
+import { html_div_code_multiple } from "../../../love/public/src/html_div_code_multiple.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { text_articled_pad_space } from "../../../love/public/src/text_articled_pad_space.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
@@ -37,17 +40,46 @@ export function app_code_lesson_operators_minus_generic(
         ": ",
       combined,
     ]);
-    app_code_lesson_operators_minus_generic_container_both_sides_number(
+    app_code_lesson_operators_minus_generic_container_both_sides(
       root,
       operator,
-      left_transform,
       verb,
+      left_transform,
     );
-    app_code_lesson_operators_asterisk_generic_minus(root, operator);
-    app_code_lesson_operators_asterisk_generic_invalid(root, operator);
+    let c3 = app_code_container_light_blue(root);
+    html_div_cycle_code(c3, [
+      "However, when making a number " +
+        sign +
+        ", there is only a number on right side of the ",
+      operator,
+      " : ",
+    ]);
+    let combined2 = text_combine_right_fn(operator, right_random_get);
+    const right2 = "right";
+    let combined4 = text_combine(operator, right2);
+    html_div_code_multiple(c3, [combined2, combined4]);
+    let c4 = app_code_container_light_blue(root);
+    html_div_cycle_code_multiple(c4, [
+      ["Therefore, ", operator, " must have a value on the right side"],
+      [
+        "And if ",
+        operator,
+        " does not have anything on its right side, then that is invalid code: ",
+      ],
+    ]);
+    let combined5 = text_combine("invalid", operator);
+    html_div_code(c4, combined5);
   }
   const example_label = "Is this code valid? ";
   const quiz_label = example_label;
+  let symbols_to_answer = function lambda(symbols) {
+    function lambda2() {
+      eval(symbols);
+    }
+    let valid = throws_not(lambda2);
+    let result = boolean_to_text_validity(valid);
+    return result;
+  };
   let inside = text_first_upper_to(math_name);
   let name = "Operators " + text_wrap_parenthesis(inside);
   let id = "operators_" + math_name;
@@ -60,7 +92,7 @@ export function app_code_lesson_operators_minus_generic(
     batch,
     example_label,
     quiz_label,
-    app_code_symbols_eval_valid_expression,
+    symbols_to_answer,
     2,
     app_code_symbol,
     question_label,
