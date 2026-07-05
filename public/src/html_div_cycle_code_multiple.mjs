add22c3bfa7b338ed9e84562a9e3658b7590a099
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:10:29 -0400

    function_auto_path ../love/public/src/html_div_cycle_code_multiple.mjs

--- public/src/html_div_cycle_code_multiple.mjs
+++ public/src/html_div_cycle_code_multiple.mjs
@@ -1,5 +1,7 @@
+import { marker_leave } from "../../../love/public/src/marker_leave.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
-export function html_div_cycle_code_multiple(parent, list_parts) {
+export async function html_div_cycle_code_multiple(parent, list_parts) {
+  let v = await marker_leave();
   let r = html_div_cycle_code(parent, list_parts);
   return r;
 }
