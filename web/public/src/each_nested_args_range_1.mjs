d38955c32246567838e165c2f845797bd8947698
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:43:13 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs

--- web/public/src/each_nested_args_range_1.mjs
+++ web/public/src/each_nested_args_range_1.mjs
@@ -1,6 +1,6 @@
 import { each_nested_args_both } from "../../../love/public/src/each_nested_args_both.mjs";
 import { range_1 } from "../../../love/public/src/range_1.mjs";
-export function each_nested_args_both_range_1(max, lambda$left$right) {
+export function each_nested_args_range_1(max, lambda$left$right) {
   let lefts = range_1(max);
   let rights = lefts;
   function lambda(left, right) {
