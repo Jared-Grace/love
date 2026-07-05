e85fbeac3f9bd9e5eafa013f75fa40c0dd692876
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:50:10 -0400

    np

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,5 +1,5 @@
-import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
+import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
 export async function sandbox_3() {
-  let removed = list_remove_last([1, 2, 3]);
+  let removed = list_remove_last_single([1, 2, 3]);
   return removed;
 }
