f116fe523ed201b3f3210001908325537269b01f
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:47:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_asterisk_generic.mjs

--- web/public/src/app_code_lesson_operators_dot_numbers.mjs
+++ web/public/src/app_code_lesson_operators_dot_numbers.mjs
@@ -125,7 +125,7 @@ export function app_code_lesson_operators_dot_numbers() {
     batch,
     example_label,
     quiz_label,
-    app_code_symbols_eval_valid_identifier,
+    app_code_symbols_eval_valid_expression,
     2,
     app_code_symbol,
     question_label,
