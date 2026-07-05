3c5dd3efb4b373c626815964d7f38a7a45db4db5
JESUS rose to life <username@users.noreply.github.com>
2026-07-04 22:01:00 -0400

    function_auto_path ../love/public/src/js_code_wrap_parenthesis.mjs

--- web/public/src/js_code_wrap_parenthesis.mjs
+++ web/public/src/js_code_wrap_parenthesis.mjs
@@ -1,6 +1,5 @@
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
 export function js_code_wrap_parenthesis(inside) {
-  const inside_larger = " " + inside + " ";
-  let wrapped = text_wrap_parenthesis(inside_larger);
+  let wrapped = text_wrap_parenthesis(inside);
   return wrapped;
 }
