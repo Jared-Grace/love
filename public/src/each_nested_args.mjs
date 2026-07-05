147c9a4e8cea70b883690de7c8a2914c619eb342
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 09:43:17 -0400

    function_auto_path ../love/public/src/each_nested_args_range_1_list_adder.mjs

---
+++ public/src/each_nested_args.mjs
@@ -0,0 +1,10 @@
+import { each } from "../../../love/public/src/each.mjs";
+export function each_nested_args_both(list_a, list_b, lambda$a$b) {
+  function lambda_a(a) {
+    function lambda_b(b) {
+      lambda$a$b(a, b);
+    }
+    each(list_b, lambda_b);
+  }
+  each(list_a, lambda_a);
+}
