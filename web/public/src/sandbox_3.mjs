850c0a205027e80f8283caf072c12b9c4420066b
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 08:50:58 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,5 +1,5 @@
-import { list_remove_last_single } from "../../../love/public/src/list_remove_last_single.mjs";
+import { list_pop } from "../../../love/public/src/list_pop.mjs";
 export async function sandbox_3() {
-  let removed = list_remove_last_single([1, 2, 3]);
+  let removed = list_pop([1, 2, 3]);
   return removed;
 }
