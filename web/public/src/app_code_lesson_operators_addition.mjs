921eb2c78178a561c5cb42950b2df15edf376c6e
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 00:31:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_addition.mjs

--- web/public/src/app_code_lesson_operators_addition.mjs
+++ web/public/src/app_code_lesson_operators_addition.mjs
@@ -1,7 +1,8 @@
+import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 import { app_code_lesson_operators_generic } from "../../../love/public/src/app_code_lesson_operators_generic.mjs";
 export function app_code_lesson_operators_addition() {
-  const operator = "+";
+  const operator = js_operator_plus();
   const operator_name = "plus sign";
   let math_name = "addition";
   let verb = "add";
