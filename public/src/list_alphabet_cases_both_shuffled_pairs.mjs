7e071ed532db2d96850c3b5e2e94c4e130dff84c
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:45:28 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_missing_batch.mjs

---
+++ public/src/list_alphabet_cases_both_shuffled_pairs.mjs
@@ -0,0 +1,7 @@
+import { list_chunk } from "../../../love/public/src/list_chunk.mjs";
+import { list_alphabet_cases_both_shuffled } from "../../../love/public/src/list_alphabet_cases_both_shuffled.mjs";
+export function list_alphabet_cases_both_shuffled_pairs() {
+  let letters = list_alphabet_cases_both_shuffled();
+  let pairs = list_chunk(letters, 2);
+  return pairs;
+}
