6758a473d382b602541777ca36232fbbc823558d
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:50:22 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_identifiers_valid.mjs

--- public/src/app_code_lesson_symbols_identifiers_valid.mjs
+++ public/src/app_code_lesson_symbols_identifiers_valid.mjs
@@ -1,3 +1,4 @@
+import { app_code_batch_question_answer_fns } from "../../../love/public/src/app_code_batch_question_answer_fns.mjs";
 import { app_code_symbols_eval_valid_identifier } from "../../../love/public/src/app_code_symbols_eval_valid_identifier.mjs";
 import { app_code_label_symbols } from "../../../love/public/src/app_code_label_symbols.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
@@ -14,12 +15,13 @@ export function app_code_lesson_symbols_identifiers_valid(
   const example_label = "Is this a valid identifier? ";
   const quiz_label = example_label;
   let question_label = app_code_label_symbols();
+  let batch_get2 = app_code_batch_question_answer_fns(batch_get);
   let r = app_code_lesson_symbols_batches_generic(
     name,
     id,
     above,
     on_symbol,
-    batch_get,
+    batch_get2,
     example_label,
     quiz_label,
     app_code_symbols_eval_valid_identifier,
