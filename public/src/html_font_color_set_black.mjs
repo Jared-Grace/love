389b3d25c804bcd084e963c3471631df4bc17c90
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 02:15:46 -0400

    function_auto_path ../love/public/src/html_font_color_set_black.mjs

--- public/src/html_font_color_set_black.mjs
+++ public/src/html_font_color_set_black.mjs
@@ -1,4 +1,4 @@
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 export function html_font_color_set_black(parent) {
-  html_font_color_set(parent, "white");
+  html_font_color_set(parent, "black");
 }
