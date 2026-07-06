895a6b2ce109e5110aaa473d4e59b4044ba9da7a
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:18:30 -0400

    function_auto_path ../love/public/src/app_code_symbol_separated.mjs

--- public/src/app_code_symbol_separated.mjs
+++ public/src/app_code_symbol_separated.mjs
@@ -1,4 +1,4 @@
-import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
+import { html_style_margin_top } from "../../../love/public/src/html_style_margin_top.mjs";
 import { app_code_symbol_generic } from "../../../love/public/src/app_code_symbol_generic.mjs";
 export function app_code_symbol_separated(parent, d) {
   const color_background = "#444";
@@ -9,6 +9,6 @@ export function app_code_symbol_separated(parent, d) {
     color_background,
     color_box_shadow,
   );
-  html_style_margin_y(s, "0.2em");
+  html_style_margin_top(s, "0.2em");
   return s;
 }
