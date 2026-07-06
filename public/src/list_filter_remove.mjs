f5940e32d938cb2707deb8843adebe828501b3d9
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 05:24:03 -0400

    function_auto_path ../love/public/src/list_filter_remove.mjs

--- public/src/list_filter_remove.mjs
+++ public/src/list_filter_remove.mjs
@@ -1,6 +1,10 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { list_remove_multiple } from "../../../love/public/src/list_remove_multiple.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_remove(existing, lambda) {
   let filtered = list_filter(existing, lambda);
+  log(list_filter_remove.name, {
+    filtered,
+  });
   list_remove_multiple(existing, filtered);
 }
