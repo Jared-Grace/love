6f8fd32ed60adbc0df207b7ec651f28f54e262da
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:23:23 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs

--- web/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs
+++ web/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs
@@ -1,3 +1,4 @@
+import { html_style_margin_top } from "../../../love/public/src/html_style_margin_top.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_span_text_smaller } from "../../../love/public/src/html_span_text_smaller.mjs";
 export function app_code_lesson_symbols_digits_numbered_on_symbol(
@@ -6,4 +7,5 @@ export function app_code_lesson_symbols_digits_numbered_on_symbol(
 ) {
   let div4 = html_span_text_smaller(parent, index_1);
   html_font_color_set(div4, "#bbb");
+  html_style_margin_top(component, value);
 }
