5fb16cd8f364cc44e9198e183aebab26701bec68
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:27:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- web/public/src/app_code_lesson_functions_arithmetic.mjs
+++ web/public/src/app_code_lesson_functions_arithmetic.mjs
@@ -1,9 +1,5 @@
 import { app_code_lesson_symbols_digits_numbered_on_symbol } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
-import { html_cycle_mono_list_between_comma_space_before_after } from "../../../love/public/src/html_cycle_mono_list_between_comma_space_before_after.mjs";
-import { html_span_text_bold } from "../../../love/public/src/html_span_text_bold.mjs";
-import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
-import { digits } from "../../../love/public/src/digits.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
@@ -18,26 +14,6 @@ export function app_code_lesson_functions_arithmetic() {
   return r5;
   function above(root) {
     let c = app_code_container_light_blue(root);
-    let p3 = html_div(c);
-    let ds = digits();
-    html_cycle_mono_list_between_comma_space_before_after(
-      p3,
-      "The numbers ",
-      ds,
-      " are called ",
-    );
-    html_span_text_bold(p3, "digits");
-    let p2 = html_div(c);
-    html_cycle_mono_list_between_comma_space_before_after(
-      p2,
-      "In a number, its digits (",
-      [0, 1, 2],
-      [", ..., ", 9, ") are examples of "],
-    );
-    html_span_text_bold(p2, "symbols");
-    let p = html_div_text(
-      c,
-      "When we write computer programs, we use symbols, including numbers",
-    );
+    let p = html_div(c);
   }
 }
