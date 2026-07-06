08e0ffa6d40d59f56d7965ca53b22077f116eadf
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 06:02:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_base.mjs

--- public/src/app_code_lesson_base.mjs
+++ public/src/app_code_lesson_base.mjs
@@ -1,8 +1,5 @@
+import { app_code_lesson_base_quizzes } from "../../../love/public/src/app_code_lesson_base_quizzes.mjs";
 import { app_code_lesson_above } from "../../../love/public/src/app_code_lesson_above.mjs";
-import { app_code_lesson_quiz } from "../../../love/public/src/app_code_lesson_quiz.mjs";
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { log } from "../../../love/public/src/log.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { text_replace_space_underscore } from "../../../love/public/src/text_replace_space_underscore.mjs";
@@ -51,55 +48,18 @@ export function app_code_lesson_base(
           ["app_code_symbol_generic", "transparent", "transparent"];
           html_text_set(div3, answer);
         }
-        const quizzes = [
-          function quiz_forwards(context, parent, container, refresh) {
-            let quiz_question = question;
-            let quiz_answer = answer;
-            const answer_property = "answer";
-            app_code_lesson_quiz(
-              container,
-              quiz_question,
-              answer_property,
-              quiz_answer,
-              parent,
-              quiz_forwards,
-              context,
-              refresh,
-              quiz_label,
-              on_quiz_answer_button_forwards,
-              on_question,
-              question_label,
-              batch_get,
-              answer_count_override,
-            );
-          },
-        ];
-        let nn = null_not_is(quiz_backwards_label_answer);
-        if (nn) {
-          function quiz_backwards(context, parent, container, refresh) {
-            log(app_code_lesson_base.name, "quiz_backwards");
-            let quiz_question = answer;
-            let quiz_answer = question;
-            const answer_property = "question";
-            app_code_lesson_quiz(
-              container,
-              quiz_question,
-              answer_property,
-              quiz_answer,
-              parent,
-              quiz_backwards,
-              context,
-              refresh,
-              quiz_backwards_label_answer,
-              on_quiz_answer_button_backwards,
-              on_question,
-              question_label,
-              batch_get,
-              quiz_backwards_answer_count_override,
-            );
-          }
-          list_add(quizzes, quiz_backwards);
-        }
+        const quizzes = app_code_lesson_base_quizzes(
+          question,
+          answer,
+          quiz_label,
+          on_quiz_answer_button_forwards,
+          on_question,
+          question_label,
+          batch_get,
+          quiz_backwards_label_answer,
+          on_quiz_answer_button_backwards,
+          quiz_backwards_answer_count_override,
+        );
         let mapped = {
           question,
           answer,
