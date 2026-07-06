b8edf09da4455b66a8ec2e6b7cc197f18ddc3e8e
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 06:14:27 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_numbers.mjs

--- web/public/src/app_code_lesson_operators_dot_numbers.mjs
+++ web/public/src/app_code_lesson_operators_dot_numbers.mjs
@@ -98,7 +98,7 @@ export function app_code_lesson_operators_dot_numbers() {
   let id = "operators_" + math_name;
   let question_label = app_code_label_code_question();
   let quiz_backwards_label = app_code_label_code_quiz_backwards();
-  const quizzes = app_code_lesson_base_quizzes(
+  const quizzes_get = app_code_lesson_base_quizzes(
     question,
     answer,
     quiz_label,
