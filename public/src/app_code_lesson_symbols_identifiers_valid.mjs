e6f19b64bf199c652cdf9bd75ba8ec6d9a5c5e4b
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 08:36:18 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_identifiers_valid.mjs

--- public/src/app_code_lesson_symbols_identifiers_valid.mjs
+++ public/src/app_code_lesson_symbols_identifiers_valid.mjs
@@ -1,9 +1,10 @@
+import { app_code_lesson_base } from "../../../love/public/src/app_code_lesson_base.mjs";
+import { app_code_lesson_base_quizzes_forwards_backwards } from "../../../love/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_batch_question_answer_fns } from "../../../love/public/src/app_code_batch_question_answer_fns.mjs";
 import { app_code_symbols_eval_valid_identifier } from "../../../love/public/src/app_code_symbols_eval_valid_identifier.mjs";
 import { app_code_label_symbols } from "../../../love/public/src/app_code_label_symbols.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
-import { app_code_lesson_base_with_quizzes } from "../../../love/public/src/app_code_lesson_base_with_quizzes.mjs";
 export function app_code_lesson_symbols_identifiers_valid(
   name,
   id,
