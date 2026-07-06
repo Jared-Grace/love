8fe044a633129d31a45758d01680a20233392b5a
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:55:03 -0400

    function_auto_path ../love/public/src/list_iterator_refillable.mjs

--- public/src/list_iterator_refillable.mjs
+++ public/src/list_iterator_refillable.mjs
@@ -1,6 +1,6 @@
 import { noop } from "../../../love/public/src/noop.mjs";
 import { list_iterator_refillable_on } from "../../../love/public/src/list_iterator_refillable_on.mjs";
 export function list_iterator_refillable(refill_get) {
-  let next_get2 = list_iterator_refillable_on(refill_get, noop);
-  return next_get2;
+  let next_get = list_iterator_refillable_on(refill_get, noop);
+  return next_get;
 }
