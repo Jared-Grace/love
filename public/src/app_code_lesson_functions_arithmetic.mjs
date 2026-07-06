958fcb8a61b8cf2234543be766b15a55add1675d
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:34:55 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -1,5 +1,4 @@
-import { list_iterator_refillable_on } from "../../../love/public/src/list_iterator_refillable_on.mjs";
-import { digits_randomly_coupled } from "../../../love/public/src/digits_randomly_coupled.mjs";
+import { list_iterator_refillable } from "../../../love/public/src/list_iterator_refillable.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_symbol_separated } from "../../../love/public/src/app_code_symbol_separated.mjs";
@@ -30,8 +29,7 @@ export function app_code_lesson_functions_arithmetic() {
   let id = "functions_arithmetic";
   return r5;
   function above(root) {
-    let list = digits_randomly_coupled();
-    let next_get = list_iterator_refillable_on(refill_get, on_refill);
+    let next_get_list = list_iterator_refillable(refill_get);
     let c = app_code_container_light_blue(root);
     html_div_cycle_code(c, ["Instead of "]);
   }
