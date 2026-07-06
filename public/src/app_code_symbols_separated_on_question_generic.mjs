eda47577b1ad3cd12594767b0b47993f58a57cc1
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:32:47 -0400

    function_auto_path ../love/public/src/app_code_symbols_separated_on_question_generic.mjs

--- public/src/app_code_symbols_separated_on_question_generic.mjs
+++ public/src/app_code_symbols_separated_on_question_generic.mjs
@@ -1,3 +1,4 @@
+import { html_style_padding_x } from "../../../love/public/src/html_style_padding_x.mjs";
 import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
 import { list_map_index_1 } from "../../../love/public/src/list_map_index_1.mjs";
 import { app_code_symbol_separated } from "../../../love/public/src/app_code_symbol_separated.mjs";
@@ -14,6 +15,7 @@ export function app_code_symbols_separated_on_question_generic(
     display: "flex",
     "flex-wrap": "wrap",
   });
+  html_style_padding_x(component, value);
   function lambda4(d, index_1) {
     let row_item = html_span(parent);
     html_flex_column_center(row_item);
