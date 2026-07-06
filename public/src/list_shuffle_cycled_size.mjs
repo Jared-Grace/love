70977a455c70a9bb6441be7c19f335c84586886f
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:42:13 -0400

    function_auto_path ../love/public/src/list_shuffle_cycled_size.mjs

--- public/src/list_shuffle_cycled_size.mjs
+++ public/src/list_shuffle_cycled_size.mjs
@@ -1,6 +1,6 @@
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
-export function list_shuffle_cycled_size(counts, list2) {
+export function list_shuffle_cycled_size(list2, counts) {
   let size = list_size(counts);
   list_shuffle_cycled(list2, size);
 }
