d220ac144a7f8678252ce3dcbcf6873df7867b40
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:08:32 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits.mjs

--- web/public/src/app_code_lesson_symbols_digits.mjs
+++ web/public/src/app_code_lesson_symbols_digits.mjs
@@ -2,7 +2,6 @@ import { app_code_symbols_separated_on_question } from "../../../love/public/src
 import { app_code_container_light_blue_text } from "../../../love/public/src/app_code_container_light_blue_text.mjs";
 import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
-import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
 export function app_code_lesson_symbols_digits() {
   function lambda(root) {
