c8e4c95a5e3b794e6d56a298711132dc8d42a165
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 09:39:29 -0400

    function_auto_path ../love/public/src/text_size_text_to.mjs

--- web/public/src/text_size_text_to.mjs
+++ web/public/src/text_size_text_to.mjs
@@ -2,6 +2,6 @@ import { text_to } from "../../../love/public/src/text_to.mjs";
 import { text_size } from "../../../love/public/src/text_size.mjs";
 export function text_size_text_to(s) {
   let size = text_size(s);
-  let t = text_to(input);
+  let t = text_to(size);
   return t;
 }
