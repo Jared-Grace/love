fb1ffbefcd0304e0725dd78fbfc946dabd6c94c8
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:36:39 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -1,5 +1,4 @@
-import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
-import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
+import { digits_positive_shuffled } from "../../../love/public/src/digits_positive_shuffled.mjs";
 import { list_iterator_refillable } from "../../../love/public/src/list_iterator_refillable.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
@@ -31,14 +30,8 @@ export function app_code_lesson_functions_arithmetic() {
   let id = "functions_arithmetic";
   return r5;
   function above(root) {
-    digits_positive_shuffled();
-    let next_get_list = list_iterator_refillable(refill_get);
+    let next_get_list = list_iterator_refillable(digits_positive_shuffled);
     let c = app_code_container_light_blue(root);
     html_div_cycle_code(c, ["Instead of "]);
-    function digits_positive_shuffled() {
-      let dps = digits_positive();
-      list_shuffle(dps);
-      return dps;
-    }
   }
 }
