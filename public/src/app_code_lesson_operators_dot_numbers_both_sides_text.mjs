e46c35ada56b2fedd4f58d587e50ae8907404c25
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:06:40 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_numbers_both_sides_text.mjs

--- public/src/app_code_lesson_operators_dot_numbers_both_sides_text.mjs
+++ public/src/app_code_lesson_operators_dot_numbers_both_sides_text.mjs
@@ -1,4 +1,7 @@
+import { app_code_lesson_operators_dot_numbers_both_sides_text_common } from "../../../love/public/src/app_code_lesson_operators_dot_numbers_both_sides_text_common.mjs";
 export function app_code_lesson_operators_dot_numbers_both_sides_text() {
-  let t = "when used with identifiers," + " there must be something";
+  let t =
+    "when used with identifiers," +
+    app_code_lesson_operators_dot_numbers_both_sides_text_common();
   return t;
 }
