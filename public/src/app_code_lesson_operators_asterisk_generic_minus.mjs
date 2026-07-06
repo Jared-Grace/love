f195b1e411c75e4d3dda6f1daa41d3dd45a5ed50
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:15:39 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_asterisk_generic.mjs

--- public/src/app_code_lesson_operators_asterisk_generic_minus.mjs
+++ public/src/app_code_lesson_operators_asterisk_generic_minus.mjs
@@ -13,7 +13,7 @@ export function app_code_lesson_operators_asterisk_generic_minus(
 ) {
   let c = app_code_container_light_blue(root);
   let minus = js_operator_minus();
-  let t = text_first_upper_to("remember, for ");
+  let t = text_first_upper_to(text_before+"remember, for ");
   html_div_cycle_code(c, [
     t,
     minus,
