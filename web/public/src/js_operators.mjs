207052a7eb26e804786788cde24f0388c8a36ea5
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:53:12 -0400

    function_auto_path ../love/public/src/js_operators.mjs

--- web/public/src/js_operators.mjs
+++ web/public/src/js_operators.mjs
@@ -6,23 +6,28 @@ import { js_operator_minus_verb } from "../../../love/public/src/js_operator_min
 import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
 import { js_operator_plus_verb } from "../../../love/public/src/js_operator_plus_verb.mjs";
 import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
+import { identity } from "../../../love/public/src/identity.mjs";
 export function js_operators() {
   let r = [
     {
       operator: js_operator_plus(),
       verb: js_operator_plus_verb(),
+      left_transform: identity,
     },
     {
       operator: js_operator_minus(),
       verb: js_operator_minus_verb(),
+      left_transform: identity,
     },
     {
       operator: js_operator_asterisk(),
       verb: js_operator_asterisk_verb(),
+      left_transform: identity,
     },
     {
       operator: js_operator_division(),
       verb: js_operator_division_verb(),
+      left_transform: identity,
     },
   ];
   return r;
