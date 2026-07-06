fe4232a18e60430c315aa49dde40495e896ca617
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:19:26 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

---
+++ web/public/src/js_operator_to_expression.mjs
@@ -0,0 +1,16 @@
+import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+export function js_operator_to_expression(o, next) {
+  let operator = property_get(o, "operator");
+  let left_transform = property_get(o, "left_transform");
+  let left = next();
+  let right = next();
+  left = left_transform(left, right);
+  let expression = js_code_binary_spaced_nb(left, operator, right);
+  let r2 = {
+    left,
+    right,
+    expression,
+  };
+  return r2;
+}
