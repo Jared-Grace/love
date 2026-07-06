c522043f5ce32edff63af13f3130672b28aa391a
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 02:16:22 -0400

    cur html_font_color_set

---
+++ public/src/html_font_color_set_curried_right.mjs
@@ -0,0 +1,7 @@
+import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
+export function html_font_color_set_curried_right(color) {
+  let c = function html_font_color_set_curried_right_result(component) {
+    return html_font_color_set(component, color);
+  };
+  return c;
+}
