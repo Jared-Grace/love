1c8c05ccc5be3b4d8b884201926983edada85f73
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 00:40:43 -0400

    function_auto_path ../love/public/src/html_div_code_dark.mjs

--- public/src/app_code_container_dark_inline.mjs
+++ public/src/app_code_container_dark_inline.mjs
@@ -1,7 +1,7 @@
 import { html_display_inline } from "../../../love/public/src/html_display_inline.mjs";
-import { app_code_div_code_dark } from "../../../love/public/src/app_code_div_code_dark.mjs";
+import { html_div_code_dark } from "../../../love/public/src/html_div_code_dark.mjs";
 export function app_code_container_dark_inline(parent) {
-  let container = app_code_div_code_dark(parent);
+  let container = html_div_code_dark(parent);
   html_display_inline(container);
   return container;
 }
