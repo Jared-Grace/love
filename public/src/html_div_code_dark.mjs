2122e90f8aeab967c41545c19c5de47581d79ae1
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 00:40:43 -0400

    function_auto_path ../love/public/src/html_div_code_dark.mjs

--- public/src/html_div_code_dark.mjs
+++ public/src/html_div_code_dark.mjs
@@ -1,6 +1,6 @@
 import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
-export function app_code_div_code_dark(parent) {
+export function html_div_code_dark(parent) {
   let container = html_div(parent);
   html_style_code_dark(container);
   return container;
