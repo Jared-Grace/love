aafc8a19b89e194e90f8fac9b6ec4b2061ece9bd
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 02:01:28 -0400

    function_auto_path ../love/public/src/html_cycle_code.mjs

--- web/public/src/html_cycle_code.mjs
+++ web/public/src/html_cycle_code.mjs
@@ -1,8 +1,14 @@
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 export function html_cycle_code(parent, parts) {
   arguments_assert(arguments, 2);
-  let cycles = [noop, function lambda() {}];
+  let cycles = [
+    noop,
+    function lambda(span) {
+      html_style_assign(b, s);
+    },
+  ];
   html_cycle(parent, cycles, parts);
 }
