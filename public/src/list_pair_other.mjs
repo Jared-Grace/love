72ac9b7ebc2f31fcebe1287d4583faeb29e2f982
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 05:23:23 -0400

    function_auto_path ../love/public/src/list_pair_other.mjs

--- public/src/list_pair_other.mjs
+++ public/src/list_pair_other.mjs
@@ -2,6 +2,6 @@ import { list_single } from "../../../love/public/src/list_single.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
 export function list_pair_other(list, item) {
   list_remove(list, item);
-  let question_property = list_single(list);
-  return question_property;
+  let other = list_single(list);
+  return other;
 }
