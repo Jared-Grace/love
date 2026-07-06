157bd183239f4f6c3e9c31cea529d8fa939628ef
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 02:20:39 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

--- web/public/src/app_code_lesson_symbols_batches_generic.mjs
+++ web/public/src/app_code_lesson_symbols_batches_generic.mjs
@@ -163,9 +163,9 @@ export function app_code_lesson_symbols_batches_generic(
                   html_visibility_visible_multiple(hides);
                 } else {
                   if (not(answered)) {
-                  let color_bg = "rgb(255 168 168)";
-                  html_style_background_color_set(b, color_bg);
-                  html_font_color_set(b, "rgb(167, 51, 51)");
+                    let color_bg = "rgb(255 168 168)";
+                    html_style_background_color_set(b, color_bg);
+                    html_font_color_set(b, "rgb(167, 51, 51)");
                   }
                 }
               }
