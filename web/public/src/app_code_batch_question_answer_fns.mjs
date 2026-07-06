f763a2cc4183d37906b711b2102b5783b8fc8ac9
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:32:09 -0400

    function_auto_path ../love/public/src/app_code_batch_question_answer_fns.mjs

--- web/public/src/app_code_batch_question_answer_fns.mjs
+++ web/public/src/app_code_batch_question_answer_fns.mjs
@@ -1,11 +1,12 @@
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+import { list_map } from "../../../love/public/src/list_map.mjs";
 export function app_code_batch_question_answer_fns(
   batch_questions_get,
   question_to_answer,
 ) {
   let r = function lambda() {
     let questions = batch_questions_get();
-    let mapped = list_map_property(list, property_name);
+    function lambda2(item) {}
+    let mapped = list_map(list, lambda2);
   };
   return r;
 }
