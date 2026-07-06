cbbc9664a17a6bf02e939af9e44345d466517ec8
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:07:58 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_asterisk_generic.mjs

--- public/src/app_code_lesson_operators_asterisk_generic.mjs
+++ public/src/app_code_lesson_operators_asterisk_generic.mjs
@@ -5,7 +5,6 @@ import { app_code_symbols_eval_valid_expression } from "../../../love/public/src
 import { app_code_lesson_operators_asterisk_generic_invalid } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic_invalid.mjs";
 import { app_code_lesson_operators_minus_generic_container_both_sides_number } from "../../../love/public/src/app_code_lesson_operators_minus_generic_container_both_sides_number.mjs";
 import { app_code_lesson_operators_asterisk_generic_minus } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic_minus.mjs";
-import { app_code_symbol } from "../../../love/public/src/app_code_symbol.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_lesson_symbols_batches_generic } from "../../../love/public/src/app_code_lesson_symbols_batches_generic.mjs";
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
