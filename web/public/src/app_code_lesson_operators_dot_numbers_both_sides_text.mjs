c49b41390c15d7c9233713c76d5a70fc28ec5fdb
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:06:24 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_numbers_both_sides_text.mjs

--- web/public/src/app_code_lesson_operators_dot_numbers_both_sides_text.mjs
+++ web/public/src/app_code_lesson_operators_dot_numbers_both_sides_text.mjs
@@ -1,4 +1,4 @@
 export function app_code_lesson_operators_dot_numbers_both_sides_text() {
-  let t = "when used with identifiers, there must be something";
+  let t = "when used with identifiers, " + "there must be something";
   return t;
 }
