98afb3c4173398caed2e09f5a71378130f1c9478
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 00:53:07 -0400

    function_auto_path ../love/public/src/app_code_symbols_separated_on_question_couted.mjs

--- web/public/src/app_code_symbols_separated_on_question_couted.mjs
+++ web/public/src/app_code_symbols_separated_on_question_couted.mjs
@@ -1,5 +1,5 @@
 import { app_code_symbols_separated_on_question_generic } from "../../../love/public/src/app_code_symbols_separated_on_question_generic.mjs";
-export function app_code_symbols_separated_on_question_separated(
+export function app_code_symbols_separated_on_question_couted(
   parent,
   symbols,
   on_symbol,
