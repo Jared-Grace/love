27968e795175d5b55eaf200ff91b18b6f726b0d8
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:39:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_missing_batch.mjs

--- web/public/src/app_code_lesson_operators_dot_missing_batch.mjs
+++ web/public/src/app_code_lesson_operators_dot_missing_batch.mjs
@@ -4,24 +4,16 @@ import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 import { js_code_binary } from "../../../love/public/src/js_code_binary.mjs";
-import { each_nested_args } from "../../../love/public/src/each_nested_args.mjs";
 import { list_alphabet_cases_both } from "../../../love/public/src/list_alphabet_cases_both.mjs";
 export function app_code_lesson_operators_dot_missing_batch(operator) {
   let r2 = function batch_get() {
     let letters = list_alphabet_cases_both();
     list_shuffle(letters);
     let pairs = list_chunk(letters, 2);
-    function lambda(la2) {
-      let rights = letters;
-      function lambda2(left5, right5) {
-        lambda$left$right(left5, right5);
-      }
-      each_nested_args(letters, rights, lambda2);
-      function lambda$left$right(left3, right3) {
-        lambda$left$right$la(left3, right3, la2);
-      }
+    function lambda(la2, pair) {
+      lambda$left$right$la(left3, right3, la2);
     }
-    let list = list_adder_each(lambda);
+    let list = list_adder_each(pairs, lambda);
     function lambda$left$right$la(left2, right2, la) {
       let combined = js_code_binary(left2, operator, right2);
       let right4 = "";
