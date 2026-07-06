5fa030b416b2ae31aed0ef51ce78ab1eece2c9cd
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 00:38:30 -0400

    function_auto_path ../love/public/src/app_code_container_dark.mjs

--- web/public/src/app_code_container_dark.mjs
+++ web/public/src/app_code_container_dark.mjs
@@ -1,7 +1,10 @@
-import { app_code_container_generic } from "../../../love/public/src/app_code_container_generic.mjs";
+import { app_code_container_generic_style } from "../../../love/public/src/app_code_container_generic_style.mjs";
+import { html_div } from "../../../love/public/src/html_div.mjs";
 import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
 export function app_code_container_dark(parent) {
-  let container = app_code_container_generic(parent);
+  let container2 = html_div(parent);
+  app_code_container_generic_style(container2);
+  let container = container2;
   html_style_background_color_black(container);
   return container;
 }
