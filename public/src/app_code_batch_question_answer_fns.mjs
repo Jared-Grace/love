4e807a9292b89e0fad628477b99097e2af0f4533
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:31:48 -0400

    function_auto_path ../love/public/src/app_code_batch_question_answer_fns.mjs

--- public/src/app_code_batch_question_answer_fns.mjs
+++ public/src/app_code_batch_question_answer_fns.mjs
@@ -2,6 +2,8 @@ export function app_code_batch_question_answer_fns(
   batch_questions_get,
   question_to_answer,
 ) {
-  let r = function lambda() {};
+  let r = function lambda() {
+    let questions = batch_questions_get();
+  };
   return r;
 }
