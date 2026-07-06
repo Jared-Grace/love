575d53f136c8ef86afec09fd9e8aa798d7adad70
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 03:46:20 -0400

    pd app_code_lesson_symbols_batches_generic quiz_backwards_label

--- web/public/src/app_code_lesson_symbols_batches_generic.mjs
+++ web/public/src/app_code_lesson_symbols_batches_generic.mjs
@@ -56,7 +56,6 @@ export function app_code_lesson_symbols_batches_generic(
   question_label,
   on_quiz_answer_button,
   on_question,
-  quiz_backwards_label,
 ) {
   arguments_assert(arguments, 11);
   id = text_replace_space_underscore(id);
