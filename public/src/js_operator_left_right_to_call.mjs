44d5eab8881c927d622cc6f578ea37790f380bb1
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:27:40 -0400

    function_auto_path ../love/public/src/js_operator_left_right_to_call.mjs

--- public/src/js_operator_left_right_to_call.mjs
+++ public/src/js_operator_left_right_to_call.mjs
@@ -1,8 +1,8 @@
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_operator_left_right_to_call(r2, o) {
-  let right = property_get(r2, "right");
-  let left = property_get(r2, "left");
+export function js_operator_left_right_to_call(left_right, o) {
+  let right = property_get(left_right, "right");
+  let left = property_get(left_right, "left");
   let verb = property_get(o, "verb");
   let code = js_code_call_args(verb, [left, right]);
   return code;
