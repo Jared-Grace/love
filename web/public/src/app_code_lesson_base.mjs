50c042f4f35cee5d98763f2d83e999391811256a
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 06:11:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_base_with_quizzes.mjs

---
+++ web/public/src/app_code_lesson_base.mjs
@@ -0,0 +1,56 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
+import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
+import { html_div_code_dark } from "../../../love/public/src/html_div_code_dark.mjs";
+import { app_code_example_answer_label } from "../../../love/public/src/app_code_example_answer_label.mjs";
+import { app_code_lesson_above } from "../../../love/public/src/app_code_lesson_above.mjs";
+import { app_code_flex_gap } from "../../../love/public/src/app_code_flex_gap.mjs";
+import { html_flex_column_stretch } from "../../../love/public/src/html_flex_column_stretch.mjs";
+import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { text_replace_space_underscore } from "../../../love/public/src/text_replace_space_underscore.mjs";
+export function app_code_lesson_base(
+  id,
+  name,
+  above,
+  example_count,
+  batch_get,
+  on_question,
+  example_label,
+  quizzes,
+) {
+  id = text_replace_space_underscore(id);
+  let lesson = {
+    name,
+    id,
+    above,
+    example_count,
+    batch: function batch() {
+      let batch_items = batch_get();
+      function each_batch_item(bi) {
+        let question = property_get(bi, "question");
+        let answer = property_get(bi, "answer");
+        let symbols = text_split_empty(question);
+        function example(parent) {
+          html_flex_column_stretch(parent);
+          app_code_flex_gap(parent);
+          let a = app_code_lesson_above(parent, label, question, on_question);
+          let container = property_get(a, "container");
+          app_code_example_answer_label(container, example_label);
+          let div3 = html_div_code_dark(container);
+          ["app_code_symbol_generic", "transparent", "transparent"];
+          html_text_set(div3, answer);
+        }
+        let mapped = {
+          question,
+          answer,
+          example,
+          quizzes,
+        };
+        return mapped;
+      }
+      let mapped_items = list_map(batch_items, each_batch_item);
+      return mapped_items;
+    },
+  };
+  return lesson;
+}
