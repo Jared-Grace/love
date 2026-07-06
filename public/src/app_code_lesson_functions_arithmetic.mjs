6ac5210ccbe8645e7d1a7decf8a864a3339a4a44
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:35:17 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -1,3 +1,4 @@
+import { digits_positive } from "../../../love/public/src/digits_positive.mjs";
 import { list_iterator_refillable } from "../../../love/public/src/list_iterator_refillable.mjs";
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
@@ -29,6 +30,7 @@ export function app_code_lesson_functions_arithmetic() {
   let id = "functions_arithmetic";
   return r5;
   function above(root) {
+    let dps = digits_positive();
     let next_get_list = list_iterator_refillable(refill_get);
     let c = app_code_container_light_blue(root);
     html_div_cycle_code(c, ["Instead of "]);
