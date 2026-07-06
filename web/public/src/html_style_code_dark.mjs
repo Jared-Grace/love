7ba8cc3f146a53dab0e94ec4e411e3be7f6e5963
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 23:30:29 -0400

    function_auto_path ../love/public/src/html_style_code_dark.mjs

--- web/public/src/html_style_code_dark.mjs
+++ web/public/src/html_style_code_dark.mjs
@@ -1,7 +1,7 @@
 import { html_style_code_generic } from "../../../love/public/src/html_style_code_generic.mjs";
 export function html_style_code_dark(component) {
+  const color_box_shadow = "transparent";
   const color_background = "black";
-  const color_box_shadow = "black";
   const color_font = "white";
   html_style_code_generic(
     component,
