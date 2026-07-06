8cfb2832a9717c95bb9e4d1f964ccb4f918a8aef
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 03:33:22 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

--- web/public/src/app_code_lesson_symbols_batches_generic.mjs
+++ web/public/src/app_code_lesson_symbols_batches_generic.mjs
@@ -107,7 +107,7 @@ export function app_code_lesson_symbols_batches_generic(
               refresh,
             );
           },
-        ];
+        ];$i
         let mapped = {
           question,
           answer,
