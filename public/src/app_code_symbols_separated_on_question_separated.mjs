5f06e2243cba200d98f94a053268825e1dbf8c6c
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 00:52:51 -0400

    w 1 app_code_symbols_separated_on_question_generic,separated

---
+++ public/src/app_code_symbols_separated_on_question_separated.mjs
@@ -0,0 +1,12 @@
+import { app_code_symbols_separated_on_question_generic } from "../../../love/public/src/app_code_symbols_separated_on_question_generic.mjs";
+export function app_code_symbols_separated_on_question_separated(
+  parent,
+  symbols,
+  on_symbol,
+) {
+  return app_code_symbols_separated_on_question_generic(
+    parent,
+    symbols,
+    on_symbol,
+  );
+}
