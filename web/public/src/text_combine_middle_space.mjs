ffed2244179b256c5c4a1fdb15c2663119be8535
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:11:16 -0400

    function_auto_path ../love/public/src/text_combine_middle_space.mjs

--- web/public/src/text_combine_middle_space.mjs
+++ web/public/src/text_combine_middle_space.mjs
@@ -1,5 +1,5 @@
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 export function text_combine_middle_space(left, right) {
-  let combined2 = text_combine_multiple([left, ",", right]);
-  return combined2;
+  let combined = text_combine_multiple([left, " ", right]);
+  return combined;
 }
