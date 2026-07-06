86e2f0e39bcfb2fd7bbffd23d39e16a959cd6064
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 09:30:59 -0400

    function_auto_path ../love/public/src/app_code_lesson_base.mjs

--- public/src/app_code_lesson_base.mjs
+++ public/src/app_code_lesson_base.mjs
@@ -16,7 +16,7 @@ export function app_code_lesson_base(
   batch_get,
   on_question,
   example_label,
-  quizzes_get,
+  quizzquizzeses_get,
 ) {
   id = text_replace_space_underscore(id);
   let lesson = {
