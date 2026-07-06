4a85da8dfa8f43ec7049372bcebf317ff8467741
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 05:23:21 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

--- public/src/list_pair_other.mjs
+++ public/src/list_pair_other.mjs
@@ -1,7 +1,7 @@
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { list_remove } from "../../../love/public/src/list_remove.mjs";
-export function list_pair_other(choices, answer_property) {
-  list_remove(choices, answer_property);
-  let question_property = list_single(choices);
+export function list_pair_other(list, item) {
+  list_remove(list, item);
+  let question_property = list_single(list);
   return question_property;
 }
