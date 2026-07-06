791bbe5c75d47d030f79a42575ed390d9c836d07
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 00:59:14 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_space.mjs

---
+++ public/src/app_code_lesson_symbols_digits_numbered_fifth_on_symbol.mjs
@@ -0,0 +1,20 @@
+import { app_code_lesson_symbols_digits_numbered_on_symbol } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs";
+import { equal_not } from "../../../love/public/src/equal_not.mjs";
+import { list_size } from "../../../love/public/src/list_size.mjs";
+import { equal_0 } from "../../../love/public/src/equal_0.mjs";
+import { mod } from "../../../love/public/src/mod.mjs";
+export function app_code_lesson_symbols_digits_numbered_fifth_on_symbol(
+  index_1,
+  symbols,
+  parent,
+) {
+  let r = mod(index_1, 5);
+  let eq = equal_0(r);
+  if (eq) {
+    let size = list_size(symbols);
+    let ne = equal_not(index_1, size);
+    if (ne) {
+      app_code_lesson_symbols_digits_numbered_on_symbol(parent, index_1);
+    }
+  }
+}
--- public/src/app_code_lesson_symbols_space.mjs
+++ public/src/app_code_lesson_symbols_space.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_symbols_digits_numbered_fifth_on_symbol } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered_fifth_on_symbol.mjs";
 import { app_code_symbols_separated_on_question } from "../../../love/public/src/app_code_symbols_separated_on_question.mjs";
 import { list_between_space_nb } from "../../../love/public/src/list_between_space_nb.mjs";
 import { app_code_lesson_symbols_space_style } from "../../../love/public/src/app_code_lesson_symbols_space_style.mjs";
@@ -6,18 +7,13 @@ import { text_space_nb } from "../../../love/public/src/text_space_nb.mjs";
 import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { list_join_space_nb } from "../../../love/public/src/list_join_space_nb.mjs";
-import { equal_not } from "../../../love/public/src/equal_not.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_slices_size_random } from "../../../love/public/src/list_slices_size_random.mjs";
-import { mod } from "../../../love/public/src/mod.mjs";
-import { app_code_lesson_symbols_digits_numbered_on_symbol } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs";
 import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_code_verse_words } from "../../../love/public/src/app_code_verse_words.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
-import { equal_0 } from "../../../love/public/src/equal_0.mjs";
 export function app_code_lesson_symbols_space() {
   function lambda(root) {
     let c = app_code_container_light_blue(root);
@@ -43,15 +39,11 @@ export function app_code_lesson_symbols_space() {
     return mapped2;
   }
   function lambda4(parent, index_1, symbols) {
-    let r = mod(index_1, 5);
-    let eq = equal_0(r);
-    if (eq) {
-      let size = list_size(symbols);
-      let ne = equal_not(index_1, size);
-      if (ne) {
-        app_code_lesson_symbols_digits_numbered_on_symbol(parent, index_1);
-      }
-    }
+    app_code_lesson_symbols_digits_numbered_fifth_on_symbol(
+      index_1,
+      symbols,
+      parent,
+    );
   }
   let r5 = app_code_lesson_symbols_counting(
     "Symbols (Space)",
