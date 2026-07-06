47156636aa6363a9c1ac5017f05e0237faf06a7b
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:00:16 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_counting.mjs

--- public/src/app_code_lesson_symbols_counting.mjs
+++ public/src/app_code_lesson_symbols_counting.mjs
@@ -1,4 +1,3 @@
-import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_batch_question_answer_fns } from "../../../love/public/src/app_code_batch_question_answer_fns.mjs";
 import { app_code_label_symbols } from "../../../love/public/src/app_code_label_symbols.mjs";
@@ -33,7 +32,7 @@ export function app_code_lesson_symbols_counting(
     app_code_symbol_separated,
     question_label,
     noop,
-    html_text_set,
+    on_question,
   );
   return r;
 }
