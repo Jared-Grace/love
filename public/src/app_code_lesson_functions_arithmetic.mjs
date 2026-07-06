fd73e0c0b0c17c8324aa6e16015cf3e803e04b9e
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 21:27:47 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -1,16 +1,31 @@
+import { app_code_symbol_separated } from "../../../love/public/src/app_code_symbol_separated.mjs";
+import { app_code_lesson_symbols_batches_generic } from "../../../love/public/src/app_code_lesson_symbols_batches_generic.mjs";
+import { app_code_label_symbols } from "../../../love/public/src/app_code_label_symbols.mjs";
+import { text_size } from "../../../love/public/src/text_size.mjs";
 import { app_code_lesson_symbols_digits_numbered_on_symbol } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
-import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
 export function app_code_lesson_functions_arithmetic() {
-  let r5 = app_code_lesson_symbols_counting(
-    "Functions (Arithmetic)",
-    "functions_arithmetic",
+  const example_label = "Number of symbols: ";
+  const quiz_label = "How many symbols are there? ";
+  let symbols_to_answer = text_size;
+  let question_label = app_code_label_symbols();
+  let r5 = app_code_lesson_symbols_batches_generic(
+    name,
+    id,
     above,
     app_code_lesson_symbols_digits_numbered_on_symbol,
     app_code_lesson_symbols_batch_digits,
+    example_label,
+    quiz_label,
+    symbols_to_answer,
+    1,
+    app_code_symbol_separated,
+    question_label,
   );
+  let name = "Functions (Arithmetic)";
+  let id = "functions_arithmetic";
   return r5;
   function above(root) {
     let c = app_code_container_light_blue(root);
