9562cdfccc4b7b2c8ec74a7a07fa9228d4713d91
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 01:25:03 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs

--- public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs
+++ public/src/app_code_lesson_symbols_digits_numbered_on_symbol.mjs
@@ -1,3 +1,4 @@
+import { app_code_symbol_separated_margin_y } from "../../../love/public/src/app_code_symbol_separated_margin_y.mjs";
 import { html_style_margin_top } from "../../../love/public/src/html_style_margin_top.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_span_text_smaller } from "../../../love/public/src/html_span_text_smaller.mjs";
@@ -7,5 +8,6 @@ export function app_code_lesson_symbols_digits_numbered_on_symbol(
 ) {
   let d = html_span_text_smaller(parent, index_1);
   html_font_color_set(d, "#bbb");
-  html_style_margin_top(d, "-0.25em");
+  let value = app_code_symbol_separated_margin_y();
+  html_style_margin_top(d, "-" + value);
 }
