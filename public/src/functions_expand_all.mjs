2fd686867ebe8987fefac7cd8c73569f18f64b44
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 08:07:29 -0400

    function_auto_path ../love/public/src/functions_expand_all.mjs

--- public/src/functions_expand_all.mjs
+++ public/src/functions_expand_all.mjs
@@ -1,7 +1,7 @@
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-export async function functions_expand_all() {
-  let result = await data_identifiers_search(ids_comma);
+export async function functions_expand_all(f_name) {
+  let result = await data_identifiers_search(f_name);
   async function lambda(ast) {}
   let output = await function_transform(f_name, lambda);
 }
