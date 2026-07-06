496f74dd87bba9dd86814d331caa77b048100b9c
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 03:46:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batches_generic.mjs

--- web/public/src/app_code_lesson_symbols_batches_generic.mjs
+++ web/public/src/app_code_lesson_symbols_batches_generic.mjs
@@ -57,7 +57,7 @@ export function app_code_lesson_symbols_batches_generic(
   on_quiz_answer_button,
   on_question,
 ) {
-  arguments_assert(arguments, 11);
+  arguments_assert(arguments, 10);
   id = text_replace_space_underscore(id);
   function example_above(parent, question) {
     app_code_example_answer_gap(parent);
