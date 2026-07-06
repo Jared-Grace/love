c06a7a91fd0f7210cc5ffd3bea859775cd2ed111
JESUS rose to life <username@users.noreply.github.com>
2026-07-05 22:38:57 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_missing_batch.mjs

--- web/public/src/app_code_lesson_operators_dot_missing_batch.mjs
+++ web/public/src/app_code_lesson_operators_dot_missing_batch.mjs
@@ -8,15 +8,15 @@ import { each_nested_args } from "../../../love/public/src/each_nested_args.mjs"
 import { list_alphabet_cases_both } from "../../../love/public/src/list_alphabet_cases_both.mjs";
 export function app_code_lesson_operators_dot_missing_batch(operator) {
   let r2 = function batch_get() {
-    let lefts = list_alphabet_cases_both();
-    list_shuffle(lefts);
-    let chunks = list_chunk(lefts, 2);
+    let letters = list_alphabet_cases_both();
+    list_shuffle(letters);
+    let pairs = list_chunk(letters, 2);
     function lambda(la2) {
-      let rights = lefts;
+      let rights = letters;
       function lambda2(left5, right5) {
         lambda$left$right(left5, right5);
       }
-      each_nested_args(lefts, rights, lambda2);
+      each_nested_args(letters, rights, lambda2);
       function lambda$left$right(left3, right3) {
         lambda$left$right$la(left3, right3, la2);
       }
