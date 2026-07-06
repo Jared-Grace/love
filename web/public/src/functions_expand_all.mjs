0995dd4e8c8d2f14be7482c70c1153847447f5e7
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 08:07:11 -0400

    function_auto_path ../love/public/src/functions_expand_all.mjs

--- web/public/src/functions_expand_all.mjs
+++ web/public/src/functions_expand_all.mjs
@@ -1,1 +1,5 @@
-export function functions_expand_all() {}
+import { function_transform } from "../../../love/public/src/function_transform.mjs";
+export async function functions_expand_all() {
+  async function lambda(ast) {}
+  let output = await function_transform(f_name, lambda);
+}
