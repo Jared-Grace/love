4d4e1538fcfa61288b9763af0140c22ab06124c4
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:50:04 -0400

    function_auto_path ../love/public/src/lock_wait.mjs

---
+++ public/src/invoke_cache_file_exists_not_wait.mjs
@@ -0,0 +1,8 @@
+import { retry_wait_until_success } from "../../../love/public/src/retry_wait_until_success.mjs";
+import { invoke_cache_file_exists_throw } from "../../../love/public/src/invoke_cache_file_exists_throw.mjs";
+export async function invoke_cache_file_exists_not_wait(fn, args) {
+  async function lambda3() {
+    await invoke_cache_file_exists_throw(fn, args);
+  }
+  let r = await retry_wait_until_success(lambda3);
+}
