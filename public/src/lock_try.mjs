e39e299a8a2c9959877f75b7d74febdf6f120565
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 04:37:57 -0400

    function_auto_path ../love/public/src/lock_try.mjs

--- public/src/lock_try.mjs
+++ public/src/lock_try.mjs
@@ -1,8 +1,7 @@
 import { lock_generic } from "../../../love/public/src/lock_generic.mjs";
 export async function lock_try(lock_name, lambda) {
+  ("if the lock is already locked, then does not run ", lambda);
   let wait = false;
   let r = await lock_generic(lock_name, wait, lambda);
-
-  
   return r;
 }
