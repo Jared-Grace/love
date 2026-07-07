5ea74012354722d3ead062b5338d5c615a1def74
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 04:35:52 -0400

    pp 3

--- public/src/lock_try.mjs
+++ public/src/lock_try.mjs
@@ -2,6 +2,7 @@ import { lock_generic } from "../../../love/public/src/lock_generic.mjs";
 export async function lock_try(lock_name, lambda) {
   let wait = false;
   let r = await lock_generic(lock_name, wait, lambda);
+
   
   return r;
 }
