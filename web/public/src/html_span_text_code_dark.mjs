c2c58bc073b500da8b524f4a8656f9da25d51288
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 22:40:20 -0400

    function_auto_path ../love/public/src/html_span_text_code_dark.mjs

--- web/public/src/html_span_text_code_dark.mjs
+++ web/public/src/html_span_text_code_dark.mjs
@@ -2,9 +2,8 @@ import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
 import { html_span } from "../../../love/public/src/html_span.mjs";
 import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
 export function html_span_text_code_dark(parent, text) {
-  let span2 = html_span(parent);
-  html_text_set(span2, text);
-  let span = span2;
+  let span = html_span(parent);
   html_style_code_dark(span);
+  html_text_set(span, text);
   return span;
 }
