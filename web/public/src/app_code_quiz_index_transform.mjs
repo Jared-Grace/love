c4c6686bbc60e4291df14114c201bf332acb50ef
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 21:46:32 -0400

    pe app_code_quiz_index_transform

--- web/public/src/app_code_quiz_index_transform.mjs
+++ web/public/src/app_code_quiz_index_transform.mjs
@@ -2,7 +2,7 @@ import { app_code_quiz_index_set } from "../../../love/public/src/app_code_quiz_
 import { mod } from "../../../love/public/src/mod.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { app_code_quiz_index_get } from "../../../love/public/src/app_code_quiz_index_get.mjs";
-export function app_code_quiz_index_transform(context, transform, quizzes) {
+export function app_code_quiz_index_transform(context, quizzes, transform) {
   let quiz_index = app_code_quiz_index_get(context);
   let a1 = transform(quiz_index);
   let size = list_size(quizzes);
