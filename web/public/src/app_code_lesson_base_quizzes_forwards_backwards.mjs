ec52d3b39a203765fc13d7531be077d6dad26758
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:22:40 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_valid.mjs

--- web/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
+++ web/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
@@ -1,5 +1,6 @@
 import { app_code_lesson_quiz } from "../../../love/public/src/app_code_lesson_quiz.mjs";
 export function app_code_lesson_base_quizzes_forwards_backwards(
+  backwards_on_question,
   backwards_answer_label,
   backwards_answer_on_button,
   backwards_answer_count_override,
@@ -9,7 +10,6 @@ export function app_code_lesson_base_quizzes_forwards_backwards(
   forwards_question_label,
   batch_get,
   backwards_question_label,
-  backwards_on_question,
 ) {
   let quizzes_get = function lambda(question, answer) {
     let quizzes = null;
