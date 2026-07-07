bb415dd47bc64b0df0b9c158ee3d6b0ee7bb87a2
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 22:59:41 -0400

    function_auto_path ../love/public/src/app_code_lesson_base.mjs

--- public/src/app_code_lesson_base.mjs
+++ public/src/app_code_lesson_base.mjs
@@ -1,6 +1,7 @@
-import { html_div } from "../../../love/public/src/html_div.mjs";
+import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_text_set } from "../../../love/public/src/html_text_set.mjs";
+import { html_div_code_dark } from "../../../love/public/src/html_div_code_dark.mjs";
 import { app_code_example_answer_label } from "../../../love/public/src/app_code_example_answer_label.mjs";
 import { app_code_lesson_above } from "../../../love/public/src/app_code_lesson_above.mjs";
 import { app_code_flex_gap } from "../../../love/public/src/app_code_flex_gap.mjs";
@@ -41,7 +42,8 @@ export function app_code_lesson_base(
           );
           let container = property_get(a, "container");
           app_code_example_answer_label(container, example_label);
-          let div3 = html_div(container);
+          let div3 = html_div_code_dark(container);
+          html_style_background_color_set(div3, "white");
           ["app_code_symbol_generic", "transparent", "transparent"];
           html_text_set(div3, answer);
         }
