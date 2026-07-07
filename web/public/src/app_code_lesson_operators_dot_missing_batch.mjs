053df9a3100cff82b7b08d2d0af202510730ae54
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 00:07:19 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_missing_batch.mjs

--- web/public/src/app_code_lesson_operators_dot_missing_batch.mjs
+++ web/public/src/app_code_lesson_operators_dot_missing_batch.mjs
@@ -1,3 +1,4 @@
+import { digits_positive_shuffled_next } from "../../../love/public/src/digits_positive_shuffled_next.mjs";
 import { app_code_symbols_eval_valid_expression } from "../../../love/public/src/app_code_symbols_eval_valid_expression.mjs";
 import { list_alphabet_cases_both_shuffled_pairs } from "../../../love/public/src/list_alphabet_cases_both_shuffled_pairs.mjs";
 import { app_code_batch_question_answer_fns } from "../../../love/public/src/app_code_batch_question_answer_fns.mjs";
@@ -8,6 +9,7 @@ import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycle
 import { each } from "../../../love/public/src/each.mjs";
 import { js_code_binary } from "../../../love/public/src/js_code_binary.mjs";
 export function app_code_lesson_operators_dot_missing_batch(operator) {
+  let next_get = digits_positive_shuffled_next();
   function batch_get() {
     let pairs = list_alphabet_cases_both_shuffled_pairs();
     function lambda(la, pair) {
