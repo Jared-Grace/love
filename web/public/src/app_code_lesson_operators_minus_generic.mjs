4e6a68155d41171ee3efc7fe25fbf8f86dd92cb3
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 21:34:25 -0400

    pn app_code_lesson_base_quizzes_forwards_backwards on_question_backwards noop

--- web/public/src/app_code_lesson_operators_minus_generic.mjs
+++ web/public/src/app_code_lesson_operators_minus_generic.mjs
@@ -118,6 +118,7 @@ export function app_code_lesson_operators_minus_generic(
     on_quiz_answer_button_backwards,
     quiz_backwards_answer_count_override,
     null,
+    noop,
   );
   let lesson = app_code_lesson_base(
     id,
