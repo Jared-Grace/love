d10a8d5c5b1502645141338932ce5ca05809a216
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 23:50:24 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_minus_generic.mjs

--- web/public/src/app_code_lesson_operators_minus_generic.mjs
+++ web/public/src/app_code_lesson_operators_minus_generic.mjs
@@ -1,3 +1,4 @@
+import { app_code_style_normal_text } from "../../../love/public/src/app_code_style_normal_text.mjs";
 import { app_code_lesson_base } from "../../../love/public/src/app_code_lesson_base.mjs";
 import { app_code_lesson_base_quizzes_forwards_backwards } from "../../../love/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs";
 import { app_code_label_code_example } from "../../../love/public/src/app_code_label_code_example.mjs";
@@ -117,8 +118,8 @@ export function app_code_lesson_operators_minus_generic(
     quiz_backwards_label_answer,
     on_quiz_answer_button_backwards,
     quiz_backwards_answer_count_override,
-    null,
-    html_text_set,
+    "Identifier validity: ",
+    app_code_style_normal_text,
   );
   let lesson = app_code_lesson_base(
     id,
