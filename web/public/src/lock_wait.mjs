8921b9ba286bc087325323ffd2bb0488418d8693
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:59:17 -0400

    function_auto_path ../love/public/src/lock_wait.mjs

--- web/public/src/lock_wait.mjs
+++ web/public/src/lock_wait.mjs
@@ -1,3 +1,4 @@
+import { error } from "../../../love/public/src/error.mjs";
 import { npm_install } from "../../../love/public/src/npm_install.mjs";
 import { invoke_cache_file_exists_not_wait } from "../../../love/public/src/invoke_cache_file_exists_not_wait.mjs";
 import { lock_wait_cacher } from "../../../love/public/src/lock_wait_cacher.mjs";
@@ -5,6 +6,20 @@ import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_
 import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
 export async function lock_wait(lock_name, lambda) {
   let lockfile = await npm_install("proper-lockfile");
+  let release = null;
+  try {
+    release = await lockfile.lock("some/file");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    if (release) {
+      try {
+        await release();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
   let run = false;
   async function lambda2() {
     await invoke_cache_file_exists_not_wait(fn, args);
