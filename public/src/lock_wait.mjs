fcaddf55aa2b1c0e74edf33314255d9a678e8dd1
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 04:01:58 -0400

    function_auto_path ../love/public/src/lock_wait.mjs

--- public/src/lock_wait.mjs
+++ public/src/lock_wait.mjs
@@ -1,5 +1,7 @@
+import { list_empty } from "../../../love/public/src/list_empty.mjs";
 import { path_join } from "../../../love/public/src/path_join.mjs";
 import { folder_user_storage_function_path } from "../../../love/public/src/folder_user_storage_function_path.mjs";
+import { error } from "../../../love/public/src/error.mjs";
 import { npm_install } from "../../../love/public/src/npm_install.mjs";
 export async function lock_wait(lock_name, lambda) {
   let lockfile = await npm_install("proper-lockfile");
@@ -8,9 +10,16 @@ export async function lock_wait(lock_name, lambda) {
     let f_path = folder_user_storage_function_path(lock_wait);
     let result = path_join([f_path, lock_name]);
     release = await lockfile.lock(result);
+  } catch (e) {
+    console.error(e);
   } finally {
     if (release) {
-      await release();
+      try {
+        await release();
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
+  list_empty(list);
 }
