08923762dea147994cc9c47e13b46f05759f4d3a
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 23:30:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_asterisk_generic.mjs

--- web/public/src/app_code_lesson_operators_asterisk_generic.mjs
+++ web/public/src/app_code_lesson_operators_asterisk_generic.mjs
@@ -1,4 +1,3 @@
-import { error } from "../../../love/public/src/error.mjs";
 import { app_code_lesson_base } from "../../../love/public/src/app_code_lesson_base.mjs";
 import { app_code_lesson_base_quizzes_forwards_backwards } from "../../../love/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs";
 import { app_code_quiz_backwards_label_answer_validity } from "../../../love/public/src/app_code_quiz_backwards_label_answer_validity.mjs";
@@ -65,7 +64,6 @@ export function app_code_lesson_operators_asterisk_generic(
     null,
     html_text_set,
   );
-  let question_label2 = error();
   let lesson = app_code_lesson_base(
     id,
     name,
@@ -75,7 +73,7 @@ export function app_code_lesson_operators_asterisk_generic(
     html_text_set,
     example_label,
     quizzes,
-    question_label2,
+    question_label,
   );
   return lesson;
 }
