648c6026408c3f1faa16dc4f0420eb0e51f2dee6
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 01:05:52 -0400

    function_auto_path ../love/public/src/app_code_next.mjs

--- public/src/app_code_next.mjs
+++ public/src/app_code_next.mjs
@@ -1,3 +1,4 @@
+import { html_div } from "../../../love/public/src/html_div.mjs";
 import { text_combine_middle_space } from "../../../love/public/src/text_combine_middle_space.mjs";
 import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { app_shared_button_back_text } from "../../../love/public/src/app_shared_button_back_text.mjs";
@@ -29,6 +30,7 @@ export function app_code_next(
   let left = emoji_repeat_1();
   let answer_yes = text_combine_multiple([left, " Yes, ", yes_text]);
   app_replace_button_wide(parent, answer_yes, refresh);
+  let div = html_div(parent2);
   let bn = app_replace_button_wide(parent, nt, on_next);
   if (null_not_is(on_back)) {
     let bt = app_shared_button_back_text();
