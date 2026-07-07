67e6a00a5fe86ab749d5c32ae459eba42f8efa25
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:44:25 -0400

    function_auto_path ../love/public/src/invoke_cache_file_exists.mjs

--- public/src/invoke_cache_file_exists.mjs
+++ public/src/invoke_cache_file_exists.mjs
@@ -1,8 +1,9 @@
 import { file_exists } from "../../../love/public/src/file_exists.mjs";
 import { invoke_cache_file_key_get } from "../../../love/public/src/invoke_cache_file_key_get.mjs";
-export function invoke_cache_file_exists(fn, args) {
+export async function invoke_cache_file_exists(fn, args) {
   let key_get = invoke_cache_file_key_get(fn, args);
   let cached_exists = file_exists;
   let key = await key_get();
-  let e = await cached_exists(key);$r,e
+  let e = await cached_exists(key);
+  return e;
 }
