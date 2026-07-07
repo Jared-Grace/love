32ec3bcc44dbf5930934d2f3f27df41814cc2450
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 21:15:43 -0400

    function_auto_path ../love/public/src/app_code_symbol_separated.mjs

--- public/src/app_code_symbol_separated.mjs
+++ public/src/app_code_symbol_separated.mjs
@@ -1,12 +1,12 @@
 import { app_code_symbol_separated_margin_y } from "../../../love/public/src/app_code_symbol_separated_margin_y.mjs";
 import { app_code_symbol_generic } from "../../../love/public/src/app_code_symbol_generic.mjs";
 import { html_style_margin_y } from "../../../love/public/src/html_style_margin_y.mjs";
-export function app_code_symbol_separated(parent, d) {
+export function app_code_symbol_separated(parent, symbol) {
   const color_background = "#444";
   const color_box_shadow = "#777";
   let s = app_code_symbol_generic(
     parent,
-    d,
+    symbol,
     color_background,
     color_box_shadow,
   );
