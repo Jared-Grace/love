b0bcc192668fc03a877bffda5fc770e1c0f83ca1
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:44:30 -0400

    function_auto_path ../love/public/src/lock_wait.mjs

--- public/src/lock_wait.mjs
+++ public/src/lock_wait.mjs
@@ -1,11 +1,13 @@
+import { retry_until_success } from "../../../love/public/src/retry_until_success.mjs";
 import { invoke_cache_file_exists } from "../../../love/public/src/invoke_cache_file_exists.mjs";
 import { lock_wait_cacher } from "../../../love/public/src/lock_wait_cacher.mjs";
 import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_file_remove.mjs";
 import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
-import { sleep } from "./sleep.mjs";
 export async function lock_wait(lock_name, lambda) {
   let fn = lock_wait_cacher;
-  const args = [lock_name];retry_until_success
+  const args = [lock_name];
+  async function lambda3() {}
+  let r = await retry_until_success(lambda3);
   await invoke_cache_file_exists(fn, args);
   await invoke_cache_file(fn, args);
   await lambda();
