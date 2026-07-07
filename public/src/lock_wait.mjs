d1e7b8cec3bb7499a423dc949d8333632e477807
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 04:30:40 -0400

    function_auto_path ../love/public/src/lock_wait.mjs

--- public/src/lock_wait.mjs
+++ public/src/lock_wait.mjs
@@ -1,6 +1,6 @@
 import { lock_generic } from "../../../love/public/src/lock_generic.mjs";
 export async function lock_wait(lock_name, lambda) {
   let wait = true;
-  lock_generic(wait);
+  let r = lock_generic(lock_name, wait, lambda);
   return r;
 }
