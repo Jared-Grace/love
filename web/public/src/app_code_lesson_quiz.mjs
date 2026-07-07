9b4a0569ec30ee0fa3ba29a9fa447bcad1b95159
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 21:47:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz.mjs

--- web/public/src/app_code_lesson_quiz.mjs
+++ web/public/src/app_code_lesson_quiz.mjs
@@ -1,3 +1,4 @@
+import { app_code_quiz_index_get } from "../../../love/public/src/app_code_quiz_index_get.mjs";
 import { subtract_1 } from "../../../love/public/src/subtract_1.mjs";
 import { app_code_quiz_index_transform } from "../../../love/public/src/app_code_quiz_index_transform.mjs";
 import { app_code_answer_count_max } from "../../../love/public/src/app_code_answer_count_max.mjs";
@@ -107,6 +108,7 @@ export function app_code_lesson_quiz(
       refresh();
     }
   }
+  let quiz_index = app_code_quiz_index_get(context);
   let on_back = null;
   on_back = function lambda() {
     let quiz_index = app_code_quiz_index_transform(
