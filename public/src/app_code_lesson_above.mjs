f0304cd0805ccd9bcb331ffd61d45ace459d9d93
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 22:39:22 -0400

    function_auto_path ../love/public/src/app_code_lesson_above.mjs

--- public/src/app_code_lesson_above.mjs
+++ public/src/app_code_lesson_above.mjs
@@ -1,3 +1,4 @@
+import { html_div_code_dark } from "../../../love/public/src/html_div_code_dark.mjs";
 import { html_div_text } from "../../../love/public/src/html_div_text.mjs";
 import { app_code_container_medium_blue } from "../../../love/public/src/app_code_container_medium_blue.mjs";
 import { app_code_example_answer_gap } from "../../../love/public/src/app_code_example_answer_gap.mjs";
@@ -5,6 +6,7 @@ export function app_code_lesson_above(parent, label, question, on_question) {
   app_code_example_answer_gap(parent);
   let container = app_code_container_medium_blue(parent);
   html_div_text(container, label);
+  let container2 = html_div_code_dark(parent2);
   on_question(container, question);
   let r4 = {
     container,
