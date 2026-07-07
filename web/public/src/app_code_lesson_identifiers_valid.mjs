38b93e39a1f7d8b7cddb27420df8506c09301d01
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 22:17:51 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_valid.mjs

--- web/public/src/app_code_lesson_identifiers_valid.mjs
+++ web/public/src/app_code_lesson_identifiers_valid.mjs
@@ -6,7 +6,7 @@ import { noop } from "../../../love/public/src/noop.mjs";
 import { app_code_batch_question_answer_fns } from "../../../love/public/src/app_code_batch_question_answer_fns.mjs";
 import { app_code_symbols_eval_valid_identifier } from "../../../love/public/src/app_code_symbols_eval_valid_identifier.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
-export function app_code_lesson_symbols_identifiers_valid(
+export function app_code_lesson_identifiers_valid(
   name,
   id,
   above,
