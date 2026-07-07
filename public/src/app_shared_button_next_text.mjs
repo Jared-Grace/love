0543c6e8ca102ba230408c72c4e0f2d58ff82f35
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 01:09:54 -0400

    function_auto_path ../love/public/src/app_shared_button_next_text.mjs

--- public/src/app_shared_button_next_text.mjs
+++ public/src/app_shared_button_next_text.mjs
@@ -1,5 +1,7 @@
+import { text_combine_middle_space_nb } from "../../../love/public/src/text_combine_middle_space_nb.mjs";
 import { emoji_arrow_right } from "../../../love/public/src/emoji_arrow_right.mjs";
 export function app_shared_button_next_text() {
-  let r = emoji_arrow_right() + " Next";
+  let left = emoji_arrow_right();
+  let r = text_combine_middle_space_nb(left, "Next");
   return r;
 }
