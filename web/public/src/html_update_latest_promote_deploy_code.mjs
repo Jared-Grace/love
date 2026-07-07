42d8c2005034842dd41c36c2a4a5c51c56610866
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 05:11:18 -0400

    function_auto_path ../love/public/src/html_update_latest_promote_deploy_code.mjs

--- web/public/src/html_update_latest_promote_deploy_code.mjs
+++ web/public/src/html_update_latest_promote_deploy_code.mjs
@@ -1,7 +1,8 @@
+import { app_code } from "../../../love/public/src/app_code.mjs";
 import { app_prefix_without_fn } from "../../../love/public/src/app_prefix_without_fn.mjs";
 import { html_update_latest_promote_deploy } from "../../../love/public/src/html_update_latest_promote_deploy.mjs";
-export async function html_update_latest_promote_deploy_code(search) {
-  let without = app_prefix_without_fn(app_fn);
-  let r = await html_update_latest_promote_deploy(search);
+export async function html_update_latest_promote_deploy_code() {
+  let without = app_prefix_without_fn(app_code);
+  let r = await html_update_latest_promote_deploy(without);
   return r;
 }
