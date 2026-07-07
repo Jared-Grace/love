d7222fe0c850d07a047169c8ea0a9736c1f5cd62
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:46:33 -0400

    function_auto_path ../love/public/src/retry_wait_until_success.mjs

--- web/public/src/retry_wait_until_success.mjs
+++ web/public/src/retry_wait_until_success.mjs
@@ -1,15 +1,12 @@
-import { equal } from "../../../love/public/src/equal.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 export async function retry_wait_until_success(lambda) {
   let r = null;
   while (true) {
     try {
       r = await lambda();
+      break;
+    } catch (e) {
       await sleep(200);
-      break;
-    } catch (e) {}
-    if (equal(i, 30)) {
-      break;
     }
   }
   return r;
