41f05b4d60c1ebccb4f9cbfbe88f27c45a487e5e
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 22:20:04 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_counting.mjs

---
+++ web/public/src/app_code_lesson_symbols_counting_quiz_backwards_on_button.mjs
@@ -0,0 +1,18 @@
+import { html_style_background_color_black } from "../../../love/public/src/html_style_background_color_black.mjs";
+import { html_style_padding_y_none } from "../../../love/public/src/html_style_padding_y_none.mjs";
+import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
+export function app_code_lesson_symbols_counting_quiz_backwards_on_button(
+  on_question,
+) {
+  let r2 = function lambda(parent, text) {
+    html_clear(parent);
+    html_style_assign(parent, {
+      "justify-content": "center",
+    });
+    html_style_padding_y_none(parent);
+    html_style_background_color_black(parent);
+    let r = on_question(parent, text);
+  };
+  return r2;
+}
