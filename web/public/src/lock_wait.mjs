8b3f0a1a9cb9a3ddd0a0eb98a6f9323fee8b9f2e
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:44:25 -0400

    function_auto_path ../love/public/src/invoke_cache_file_exists.mjs

--- web/public/src/lock_wait.mjs
+++ web/public/src/lock_wait.mjs
@@ -2,10 +2,11 @@ import { invoke_cache_file_exists } from "../../../love/public/src/invoke_cache_
 import { lock_wait_cacher } from "../../../love/public/src/lock_wait_cacher.mjs";
 import { invoke_cache_file_remove } from "../../../love/public/src/invoke_cache_file_remove.mjs";
 import { invoke_cache_file } from "../../../love/public/src/invoke_cache_file.mjs";
+import { sleep } from "./sleep.mjs";
 export async function lock_wait(lock_name, lambda) {
   let fn = lock_wait_cacher;
-  const args = [lock_name];
-   invoke_cache_file_exists(fn, args);
+  const args = [lock_name];retry_until_success
+  await invoke_cache_file_exists(fn, args);
   await invoke_cache_file(fn, args);
   await lambda();
   await invoke_cache_file_remove(fn, args);
