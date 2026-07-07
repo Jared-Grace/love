6afe101d3d5d38fbb94ab11e11d78f9e88125395
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 23:05:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_identifiers_valid.mjs

--- public/src/app_code_lesson_identifiers_valid.mjs
+++ public/src/app_code_lesson_identifiers_valid.mjs
@@ -37,7 +37,7 @@ export function app_code_lesson_identifiers_valid(
     quiz_backwards_label_answer,
     on_quiz_answer_button_backwards,
     quiz_backwards_answer_count_override,
-    null,
+    "null",
     html_text_set,
   );
   let lesson = app_code_lesson_base(
