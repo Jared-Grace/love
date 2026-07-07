ab4ff184f9c14958a53ec73a1d4ace1722f25fc3
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 23:02:34 -0400

    function_auto_path ../love/public/src/html_style_code_unfonted.mjs

--- public/src/html_style_code_generic_unshadowed.mjs
+++ public/src/html_style_code_generic_unshadowed.mjs
@@ -1,4 +1,4 @@
-import { html_style_code_generic_unfonted } from "../../../love/public/src/html_style_code_generic_unfonted.mjs";
+import { html_style_code_unfonted } from "../../../love/public/src/html_style_code_unfonted.mjs";
 import { html_font_jetbrains_mono } from "../../../love/public/src/html_font_jetbrains_mono.mjs";
 export function html_style_code_generic_unshadowed(
   component,
@@ -6,5 +6,5 @@ export function html_style_code_generic_unshadowed(
   color_font,
 ) {
   html_font_jetbrains_mono(component);
-  html_style_code_generic_unfonted(component, color_background, color_font);
+  html_style_code_unfonted(component, color_background, color_font);
 }
--- public/src/html_style_code_unfonted.mjs
+++ public/src/html_style_code_unfonted.mjs
@@ -3,7 +3,7 @@ import { html_border_none } from "../../../love/public/src/html_border_none.mjs"
 import { html_border_radius_em } from "../../../love/public/src/html_border_radius_em.mjs";
 import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
 import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
-export function html_style_code_generic_unfonted(
+export function html_style_code_unfonted(
   component,
   color_background,
   color_font,
