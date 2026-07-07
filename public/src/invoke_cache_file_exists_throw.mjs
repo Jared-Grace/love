08a18a93ef741f9a72e66d3236d3994ec561aff8
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 03:48:41 -0400

    function_auto_path ../love/public/src/invoke_cache_file_exists_throw.mjs

--- public/src/invoke_cache_file_exists_throw.mjs
+++ public/src/invoke_cache_file_exists_throw.mjs
@@ -2,8 +2,7 @@ import { error_combine_multiple } from "../../../love/public/src/error_combine_m
 import { invoke_cache_file_exists } from "../../../love/public/src/invoke_cache_file_exists.mjs";
 export async function invoke_cache_file_exists_throw(fn, args) {
   let e = await invoke_cache_file_exists(fn, args);
-  if (e) {$a
-    const list = [invoke_cache_file_exists.name, " returned ", e];
-    error_combine_multiple(list);
+  if (e) {
+    error_combine_multiple([invoke_cache_file_exists.name, " returned ", e]);
   }
 }
