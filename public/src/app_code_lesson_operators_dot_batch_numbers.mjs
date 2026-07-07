9fcb6bb332c5dec02f6cdb12bcb948044653f8e8
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 00:10:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_operators_dot_batch_missing.mjs

--- public/src/app_code_lesson_operators_dot_batch_numbers.mjs
+++ public/src/app_code_lesson_operators_dot_batch_numbers.mjs
@@ -2,8 +2,7 @@ import { app_code_lesson_operators_dot_batch } from "../../../love/public/src/ap
 import { lambda_value } from "../../../love/public/src/lambda_value.mjs";
 import { digits_positive_shuffled_next } from "../../../love/public/src/digits_positive_shuffled_next.mjs";
 export function app_code_lesson_operators_dot_batch_numbers(operator) {
-  let next_get = digits_positive_shuffled_next();
-  let next = lambda_value("");
+  let next = digits_positive_shuffled_next();
   let b = app_code_lesson_operators_dot_batch(operator, next);
   return b;
 }
