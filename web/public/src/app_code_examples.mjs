1551366415959915cfc5b639d6176dea688a67b0
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 01:45:42 -0400

    function_auto_path ../love/public/src/app_code_examples.mjs

--- web/public/src/app_code_examples.mjs
+++ web/public/src/app_code_examples.mjs
@@ -1,3 +1,4 @@
+import { list_index_last } from "../../../love/public/src/list_index_last.mjs";
 import { app_code_lesson_previous_set } from "../../../love/public/src/app_code_lesson_previous_set.mjs";
 import { subtract_1 } from "../../../love/public/src/subtract_1.mjs";
 import { app_code_quiz_index_transform } from "../../../love/public/src/app_code_quiz_index_transform.mjs";
@@ -63,6 +64,7 @@ export function app_code_examples(context) {
       app_code_lesson_previous_set(context);
       let previous = app_code_lesson_current(context);
       let quizzes2 = property_get(previous, "quizzes");
+      let index_last = list_index_last(list);
       let quiz_index = app_code_quiz_index_transform(
         context,
         quizzes,
