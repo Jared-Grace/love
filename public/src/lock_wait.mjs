bcf0b94978fb0e833d8505f30c1438d3dc21e419
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 04:20:47 -0400

    function_auto_path ../love/public/src/lock_wait.mjs

--- public/src/lock_wait.mjs
+++ public/src/lock_wait.mjs
@@ -9,7 +9,7 @@ export async function lock_wait(lock_name, lambda) {
   let lockfile = await import_install("proper-lockfile");
   let f_path = folder_user_storage_function_path(lock_wait);
   let result = path_join([f_path, lock_name]);
-  await file_parent_exists_ensure(file_path);
+  await file_parent_exists_ensure(result);
   let release = null;
   let r = null;
   try {
