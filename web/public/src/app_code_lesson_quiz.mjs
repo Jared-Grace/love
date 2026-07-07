f7f0f179a39aa8411ec61ccdb3544947b9cb6d5e
JESUS rose to life <username@users.noreply.github.com>
2026-07-06 21:47:47 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz.mjs

--- web/public/src/app_code_lesson_quiz.mjs
+++ web/public/src/app_code_lesson_quiz.mjs
@@ -108,7 +108,9 @@ export function app_code_lesson_quiz(
       refresh();
     }
   }
-  let quiz_index = app_code_quiz_index_get(context);$i
+  let quiz_index = app_code_quiz_index_get(context);
+  if (false) {
+  }
   let on_back = null;
   on_back = function lambda() {
     let quiz_index = app_code_quiz_index_transform(
