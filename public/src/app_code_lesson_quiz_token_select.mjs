d3d39bba8f38df34b29fcb9e5454a43030a1f4f7
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:14:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz_token_select.mjs

--- public/src/app_code_lesson_quiz_token_select.mjs
+++ public/src/app_code_lesson_quiz_token_select.mjs
@@ -1,3 +1,5 @@
+import { app_code_lesson_quiz_qa_question } from "../../../love/public/src/app_code_lesson_quiz_qa_question.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_code_lesson_quiz_token_select(
   parent,
   info,
@@ -5,4 +7,7 @@ export function app_code_lesson_quiz_token_select(
   on_success,
   on_wrong,
   batch_get,
-) {}
+) {
+  let answer_property = property_get(info, "answer_property");
+  let quiz_question = app_code_lesson_quiz_qa_question(qa, answer_property);
+}
