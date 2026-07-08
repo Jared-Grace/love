ba3f17467152dc607114da90852f8d3e0f7c684c
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:02:14 -0400

    function_auto_path ../love/public/src/js_tokenizer.mjs

--- web/public/src/js_tokenizer.mjs
+++ web/public/src/js_tokenizer.mjs
@@ -6,6 +6,17 @@ export function js_tokenizer(acorn, code) {
       ecmaVersion: 2020,
       sourceType: "module",
     });
+    const tokenizer = acorn.tokenizer(code, {
+      ecmaVersion: "latest",
+    });
+    const tokens = [];
+    while (true) {
+      const token = tokenizer.getToken();
+      tokens.push(token);
+      if (token.type.label === "eof") {
+        break;
+      }
+    }
   } catch (e) {
     log_keep(js_tokenizer.name, code);
     throw e;
