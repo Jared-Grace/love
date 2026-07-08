a4eb715129a00f35a7b652d6d8185ecd4312e27b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 03:42:25 -0400

    function_auto_path ../love/public/src/js_visit_type_node.mjs

--- public/src/js_visit_type_node.mjs
+++ public/src/js_visit_type_node.mjs
@@ -1,9 +1,9 @@
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_type_node(ast, type, on_node) {
+export function js_visit_type_node(ast, type, lambda$node) {
   function lambda7(v) {
     let n = property_get(v, "node");
-    on_node(n);
+    lambda$node(n);
   }
   js_visit_type(ast, type, lambda7);
 }
