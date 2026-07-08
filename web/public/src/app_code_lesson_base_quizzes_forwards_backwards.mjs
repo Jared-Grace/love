14a956724974bbdb7812f70b91a3aa1e4ac1953b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:56:36 -0400

    function_auto_path ../love/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs

--- web/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
+++ web/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
@@ -13,6 +13,15 @@ export function app_code_lesson_base_quizzes_forwards_backwards(
   backwards_answer_on_button,
   backwards_answer_count_override,
 ) {
+  const backwards = {
+    answer_label: backwards_answer_label,
+    answer_on_button: backwards_answer_on_button,
+    on_question: backwards_on_question,
+    question_label: backwards_question_label,
+    answer_count_override: backwards_answer_count_override,
+    answer_property: "question",
+    on_answer: app_code_lesson_quiz_multiple_choice,
+  };
   let infos = [
     {
       answer_label: forwards_answer_label,
@@ -23,15 +32,7 @@ export function app_code_lesson_base_quizzes_forwards_backwards(
       answer_property: "answer",
       on_answer: app_code_lesson_quiz_multiple_choice,
     },
-    {
-      answer_label: backwards_answer_label,
-      answer_on_button: backwards_answer_on_button,
-      on_question: backwards_on_question,
-      question_label: backwards_question_label,
-      answer_count_override: backwards_answer_count_override,
-      answer_property: "question",
-      on_answer: app_code_lesson_quiz_multiple_choice,
-    },
+    backwards,
   ];
   let quizzes_get = function lambda(question, answer) {
     function each_info(qa) {
