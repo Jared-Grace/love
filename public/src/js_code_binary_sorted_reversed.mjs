fd852035c6958347a85837cee9a38f1bff392122
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 03:07:09 -0400

    function_auto_path ../love/public/src/js_code_binary_sorted_reversed.mjs

--- public/src/js_code_binary_sorted_reversed.mjs
+++ public/src/js_code_binary_sorted_reversed.mjs
@@ -1,6 +1,8 @@
+import { list_sort_text } from "../../../love/public/src/list_sort_text.mjs";
 import { js_code_binary } from "../../../love/public/src/js_code_binary.mjs";
 export function js_code_binary_sorted_reversed(left, operator, right) {
   let operands = [left, right];
   let combined = js_code_binary(left, operator, right);
+  list_sort_text(list);
   return combined;
 }
