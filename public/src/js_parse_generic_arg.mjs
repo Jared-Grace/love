c6c827c1296758baf613e9bc2b672408c7e51af2
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:03:24 -0400

    function_auto_path ../love/public/src/js_parse_generic_arg.mjs

--- public/src/js_parse_generic_arg.mjs
+++ public/src/js_parse_generic_arg.mjs
@@ -1,8 +1,8 @@
 import { js_parse_ecma_version } from "../../../love/public/src/js_parse_ecma_version.mjs";
 export function js_parse_generic_arg() {
-  let r = {
+  let a = {
     ecmaVersion: js_parse_ecma_version(),
     sourceType: "module",
   };
-  return r;
+  return a;
 }
