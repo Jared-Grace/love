96295b0bf7ba7031f06203ba20fb7acc51481a92
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 09:28:17 -0400

    function_auto_path ../love/public/src/list_map_upper_join.mjs

--- web/public/src/list_map_upper_join.mjs
+++ web/public/src/list_map_upper_join.mjs
@@ -1,8 +1,8 @@
+import { text_upper_to } from "../../../love/public/src/text_upper_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
 export function list_map_upper_join(list) {
-  function lambda(item) {}
-  let mapped = list_map(list2, lambda);
+  let mapped = list_map(list2, text_upper_to);
   let joined = list_join_empty(list);
   return joined;
 }
