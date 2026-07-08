19e0b650e09bc5b8af9ff20881624bbf37879340
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:19:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_current.mjs

---
+++ web/public/src/app_code_lesson_current_id.mjs
@@ -0,0 +1,11 @@
+import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
+import { app_code_lesson_first_id } from "../../../love/public/src/app_code_lesson_first_id.mjs";
+export function app_code_lesson_current_id(context) {
+  let first_id = app_code_lesson_first_id();
+  let lesson_id = storage_local_initialize_context(
+    context,
+    "lesson_id",
+    first_id,
+  );
+  return lesson_id;
+}
