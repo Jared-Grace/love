013e3cbdcb9970ab5258c2f6a7155200b34c3c65
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:24:59 -0400

    function_auto_path ../love/public/src/property_get.mjs

--- public/src/property_get.mjs
+++ public/src/property_get.mjs
@@ -1,9 +1,19 @@
+import { error_json_lamba } from "../../../love/public/src/error_json_lamba.mjs";
+import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { undefined_not_is_assert_object_property } from "../../../love/public/src/undefined_not_is_assert_object_property.mjs";
 export function property_get(object, property_name) {
   if (not(property_name in object)) {
-    let v = String(property_name);
-    throw new Error(`Property does not exist: ${v}`);
+    function object_get() {
+      let properties = properties_get(object);
+      let v = {
+        property_name,
+        properties,
+        object,
+      };
+      return v;
+    }
+    error_json_lamba(object_get);
   }
   let value = object[property_name];
   undefined_not_is_assert_object_property(value, object, property_name);
