202f1a4995b722bb2bbd4e96732e6a93c8748de3
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:18:06 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz_token_select.mjs

--- public/src/app_code_lesson_quiz_token_select.mjs
+++ public/src/app_code_lesson_quiz_token_select.mjs
@@ -1,3 +1,4 @@
+import { js_tokenizer_normalized } from "../../../love/public/src/js_tokenizer_normalized.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
@@ -13,10 +14,7 @@ import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
 import { app_replace_button } from "../../../love/public/src/app_replace_button.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { text_to } from "../../../love/public/src/text_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { js_tokenizer } from "../../../love/public/src/js_tokenizer.mjs";
 import { app_code_lesson_quiz_qa_question } from "../../../love/public/src/app_code_lesson_quiz_qa_question.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_code_lesson_quiz_token_select(
@@ -30,9 +28,7 @@ export function app_code_lesson_quiz_token_select(
   let answer_property = property_get(info, "answer_property");
   let quiz_question = app_code_lesson_quiz_qa_question(qa, answer_property);
   let code = property_get(qa, answer_property);
-  let tokens = js_tokenizer(code);
-  let mapped = list_map_property(tokens, "value");
-  let mapped2 = list_map(mapped, text_to);
+  let mapped2 = js_tokenizer_normalized(code);
   list_shuffle(mapped2);
   function lambda(token) {
     function lambda3() {}
