04cbfe4f6684bf59d928c9f228cee1d6f5918528
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 06:02:43 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz_token_select.mjs

--- web/public/src/app_code_lesson_quiz_token_select.mjs
+++ web/public/src/app_code_lesson_quiz_token_select.mjs
@@ -43,7 +43,7 @@ export function app_code_lesson_quiz_token_select(
       let concated = list_concat_single_right(chosen, token);
       let lambda2 = list_starts_with_curried_right(concated);
       variations = list_filter(variations, lambda2);
-      let e = list_empty_is(filtered);
+      let e = list_empty_is(variations);
       if (e) {
         app_code_lesson_quiz_wrong_set(b);
         on_wrong();
