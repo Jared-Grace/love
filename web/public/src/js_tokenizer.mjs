be29d914bdd4c55770193a3cd6d2730e96732ea2
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:13:03 -0400

    function_auto_path ../love/public/src/js_tokenizer.mjs

--- web/public/src/js_tokenizer.mjs
+++ web/public/src/js_tokenizer.mjs
@@ -1,5 +1,5 @@
+import { invoke_until } from "../../../love/public/src/invoke_until.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_parse_generic_arg } from "../../../love/public/src/js_parse_generic_arg.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export function js_tokenizer(acorn, code) {
@@ -7,21 +7,12 @@ export function js_tokenizer(acorn, code) {
   try {
     let a = js_parse_generic_arg();
     const tokenizer = acorn.tokenizer(code, a);
-    const next = tokenizer.getToken;
-    function end(token) {
+    const next_get = tokenizer.getToken;
+    function end_is(token) {
       let r = token.type.label === "eof";
       return r;
     }
-    function lambda(la) {
-      while (true) {
-        const token = next();
-        la(token);
-        if (end(token)) {
-          break;
-        }
-      }
-    }
-    const tokens = list_adder(lambda);
+    const tokens = invoke_until(next_get, end_is);
     log(js_tokenizer.name, {
       tokens,
     });
