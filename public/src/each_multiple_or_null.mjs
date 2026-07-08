d56419db787d82857b5b3b6e4a59544c8c14a6c8
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 05:00:57 -0400

    pl each_multiple_generic lambda

--- public/src/each_multiple_or_null.mjs
+++ public/src/each_multiple_or_null.mjs
@@ -3,5 +3,5 @@ import { lists_get_or_null } from "../../../love/public/src/lists_get_or_null.mj
 import { each_multiple_generic } from "../../../love/public/src/each_multiple_generic.mjs";
 export function each_multiple_or_null(lists, lambda) {
   let getter = lists_get_or_null;
-  each_multiple_generic(lists, getter, lambda, lists_size_max);
+  each_multiple_generic(lists, getter, lists_size_max, lambda);
 }
