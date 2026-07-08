7b44aa3e80adc4e2d7829e24868fd4555b92d139
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 09:09:38 -0400

    function_auto_path ../love/public/src/list_map_cycle.mjs

--- web/public/src/list_map_cycle.mjs
+++ web/public/src/list_map_cycle.mjs
@@ -1,3 +1,5 @@
+import { list_adder_each } from "../../../love/public/src/list_adder_each.mjs";
 export function list_map_cycle(list, mappers) {
-  lae;
+  function lambda(la, item) {}
+  let r = list_adder_each(list2, lambda);
 }
