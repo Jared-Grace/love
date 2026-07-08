6c0b2b8aa8e261a76fb63c173927291ca411722e
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 00:06:15 -0400

    function_auto_path ../love/public/src/app_code_lesson_current.mjs

--- web/public/src/app_code_lesson_current.mjs
+++ web/public/src/app_code_lesson_current.mjs
@@ -4,8 +4,12 @@ import { storage_local_initialize_context } from "../../../love/public/src/stora
 import { app_code_lessons } from "../../../love/public/src/app_code_lessons.mjs";
 export function app_code_lesson_current(context) {
   let lessons = app_code_lessons();
-  let value = app_code_lesson_first_id();
-  let lesson_id = storage_local_initialize_context(context, "lesson_id", value);
+  let first_id = app_code_lesson_first_id();
+  let lesson_id = storage_local_initialize_context(
+    context,
+    "lesson_id",
+    first_id,
+  );
   let lesson = list_find_property(lessons, "id", lesson_id);
   return lesson;
 }
