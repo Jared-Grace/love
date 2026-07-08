afc1ffda093bc48c024f36fcd57c0228534f0770
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:58:55 -0400

    function_auto_path ../love/public/src/lists_size_min.mjs

--- public/src/lists_size_min.mjs
+++ public/src/lists_size_min.mjs
@@ -2,6 +2,6 @@ import { list_min } from "../../../love/public/src/list_min.mjs";
 import { list_map_size } from "../../../love/public/src/list_map_size.mjs";
 export function lists_size_min(lists) {
   let mapped = list_map_size(lists);
-  let max = list_min(mapped);
-  return max;
+  let min = list_min(mapped);
+  return min;
 }
