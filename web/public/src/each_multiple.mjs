c280bceac07f8e479e729612d411898cc6642154
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 05:00:02 -0400

    pn each_multiple_generic size_get lists_size_max

--- web/public/src/each_multiple.mjs
+++ web/public/src/each_multiple.mjs
@@ -1,6 +1,7 @@
+import { lists_size_max } from "../../../love/public/src/lists_size_max.mjs";
 import { lists_get } from "../../../love/public/src/lists_get.mjs";
 import { each_multiple_generic } from "../../../love/public/src/each_multiple_generic.mjs";
 export function each_multiple(lists, lambda) {
   let getter = lists_get;
-  each_multiple_generic(lists, getter, lambda);
+  each_multiple_generic(lists, getter, lambda, lists_size_max);
 }
--- web/public/src/each_multiple_or_null.mjs
+++ web/public/src/each_multiple_or_null.mjs
@@ -1,6 +1,7 @@
+import { lists_size_max } from "../../../love/public/src/lists_size_max.mjs";
 import { lists_get_or_null } from "../../../love/public/src/lists_get_or_null.mjs";
 import { each_multiple_generic } from "../../../love/public/src/each_multiple_generic.mjs";
 export function each_multiple_or_null(lists, lambda) {
   let getter = lists_get_or_null;
-  each_multiple_generic(lists, getter, lambda);
+  each_multiple_generic(lists, getter, lambda, lists_size_max);
 }
