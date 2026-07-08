7cd4026b2e37a76c2a087e24302de109ff195ddc
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 05:06:07 -0400

    function_auto_path ../love/public/src/list_starts_with.mjs

--- web/public/src/list_starts_with.mjs
+++ web/public/src/list_starts_with.mjs
@@ -1,12 +1,12 @@
 import { each_pair } from "../../../love/public/src/each_pair.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-export function list_starts_with(variation, concated) {
+export function list_starts_with(list, list_prefix) {
   let starts_with = true;
-  function lambda4(left, right) {
+  function lambda(left, right) {
     if (equal_not(left, right)) {
       starts_with = false;
     }
   }
-  each_pair(variation, concated, lambda4);
+  each_pair(list, list_prefix, lambda);
   return starts_with;
 }
