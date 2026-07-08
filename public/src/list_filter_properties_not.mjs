1d29e7fbf11a13e57feba8df94d1836a686fc4c4
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:38:49 -0400

    function_auto_path ../love/public/src/list_filter_properties_not.mjs

--- public/src/list_filter_properties_not.mjs
+++ public/src/list_filter_properties_not.mjs
@@ -1,9 +1,10 @@
-import { list_filter_property_not } from "../../../love/public/src/list_filter_property_not.mjs";
+import { list_reduce } from "../../../love/public/src/list_reduce.mjs";
 import { property_equals_not } from "../../../love/public/src/property_equals_not.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 export function list_filter_properties_not(list, property_names, value) {
   let c = function property_equals_not_curried_right_2_result(object) {
-    let filtered2 = list_filter_property_not(list2, property_name2, value2);
+    function lambda(value2, item) {}
+    let value3 = list_reduce(list2, lambda, inital);
     let ne = property_equals_not(object, property_name, value);
     return ne;
   };
