f2b633543a755168f428dcd3b703722340a385c8
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:48:21 -0400

    function_auto_path ../love/public/src/property_path_get_not.mjs

--- web/public/src/property_path_get_not.mjs
+++ web/public/src/property_path_get_not.mjs
@@ -1,11 +1,7 @@
-import { log } from "../../../love/public/src/log.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
 import { property_path_get } from "../../../love/public/src/property_path_get.mjs";
 export function property_path_get_not(item, property_names, value) {
   let result = property_path_get(item, property_names);
-  log(property_path_get_not.name, {
-    result,
-  });
   let ne = equal_not(result, value);
-  return result;
+  return ne;
 }
