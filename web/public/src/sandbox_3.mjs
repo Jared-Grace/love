58119a65ca5c0d5bfedf14dd7961919fe4e95cc2
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:35:28 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,5 +1,5 @@
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 export async function sandbox_3() {
-  let expression = js_parse_expression("c=b");
+  let expression = js_parse_expression("let c=b");
   return expression;
 }
