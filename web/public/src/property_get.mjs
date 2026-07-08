7ffc62efa8812367fe774ce25a8aba694a4c0d6f
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:25:08 -0400

    function_auto_path ../love/public/src/property_get.mjs

--- web/public/src/property_get.mjs
+++ web/public/src/property_get.mjs
@@ -1,7 +1,6 @@
 import { error_json_lamba } from "../../../love/public/src/error_json_lamba.mjs";
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { undefined_not_is_assert_object_property } from "../../../love/public/src/undefined_not_is_assert_object_property.mjs";
 export function property_get(object, property_name) {
   if (not(property_name in object)) {
     function object_get() {
@@ -16,6 +15,5 @@ export function property_get(object, property_name) {
     error_json_lamba(object_get);
   }
   let value = object[property_name];
-  undefined_not_is_assert_object_property(value, object, property_name);
   return value;
 }
