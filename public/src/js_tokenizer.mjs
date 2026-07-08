08d41c9788db81662f9d2f25c9a3e8a7f238797b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:46:08 -0400

    function_auto_path ../love/public/src/js_tokenizer.mjs

--- public/src/js_tokenizer.mjs
+++ public/src/js_tokenizer.mjs
@@ -1,7 +1,11 @@
-import { list_filter_property_not } from "../../../love/public/src/list_filter_property_not.mjs";
+import { list_filter_property_path_not } from "../../../love/public/src/list_filter_property_path_not.mjs";
 import { js_tokenizer_with_eof } from "../../../love/public/src/js_tokenizer_with_eof.mjs";
 export function js_tokenizer(code) {
   let tokens = js_tokenizer_with_eof(code);
-  let filtered = list_filter_property_not(tokens, "label", "eof");
+  let filtered = list_filter_property_path_not(
+    tokens,
+    ["type", "label"],
+    "eof",
+  );
   return filtered;
 }
