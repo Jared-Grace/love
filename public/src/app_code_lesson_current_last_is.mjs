1fa68a7b280064cff43cd4a0ff0d076ec24585a1
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:25:45 -0400

    w i app_code_lesson_last_is,last,current

---
+++ public/src/app_code_lesson_current_last_is.mjs
@@ -0,0 +1,4 @@
+import { app_code_lesson_last_is } from "../../../love/public/src/app_code_lesson_last_is.mjs";
+export function app_code_lesson_current_last_is(context, lesson_id) {
+  return app_code_lesson_last_is(context, lesson_id);
+}
