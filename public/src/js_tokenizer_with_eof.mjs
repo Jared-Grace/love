8237f70a98384b31942c833c01f3b68cc5541387
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:29:12 -0400

    r c js_tokenizer,with_eof

--- public/src/js_tokenizer_with_eof.mjs
+++ public/src/js_tokenizer_with_eof.mjs
@@ -4,7 +4,7 @@ import { invoke_until } from "../../../love/public/src/invoke_until.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_parse_generic_arg } from "../../../love/public/src/js_parse_generic_arg.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-export function js_tokenizer(code) {
+export function js_tokenizer_with_eof(code) {
   let acorn = module_acorn_get();
   let tokens = null;
   try {
@@ -16,11 +16,11 @@ export function js_tokenizer(code) {
     }
     let next_get = bind(tokenizer.getToken, tokenizer);
     tokens = invoke_until(next_get, end_is);
-    log(js_tokenizer.name, {
+    log(js_tokenizer_with_eof.name, {
       tokens,
     });
   } catch (e) {
-    log_keep(js_tokenizer.name, code);
+    log_keep(js_tokenizer_with_eof.name, code);
     throw e;
   }
   return tokens;
