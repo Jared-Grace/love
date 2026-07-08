4cdba1832df63e42de0bd2629ae4f084db5df6bb
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 09:27:53 -0400

    s upper

--- web/public/src/list_map_upper_join.mjs
+++ web/public/src/list_map_upper_join.mjs
@@ -1,5 +1,5 @@
 import { list_join_empty } from "../../../love/public/src/list_join_empty.mjs";
-export function list_map_upper_join(list) {
+export function list_map_upper_join(list) {lm
   let joined = list_join_empty(list);
   return joined;
 }
