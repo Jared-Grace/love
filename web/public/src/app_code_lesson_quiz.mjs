4f35530bc95fd14f4629719df1fa1f488aa632a3
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 20:59:51 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz.mjs

--- web/public/src/app_code_lesson_quiz.mjs
+++ web/public/src/app_code_lesson_quiz.mjs
@@ -144,6 +144,7 @@ export function app_code_lesson_quiz(
         app_shared_button_screen_green_style_assign(b);
         html_visibility_visible_multiple(hides);
         await sleep_seconds(0.5);
+        refresh();
       } else {
         if (not(answered)) {
           let color_bg = app_code_lesson_quiz_wrong_background_color();
