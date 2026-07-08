f4a453d710893734d1271cb06600e8d75db4f984
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:40:33 -0400

    function_auto_path ../love/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs

--- public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
+++ public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
@@ -29,6 +29,7 @@ export function app_code_lesson_base_quizzes_forwards_backwards(
       question_label: backwards_question_label,
       answer_count_override: backwards_answer_count_override,
       answer_property: "question",
+      on_answer: app_code_lesson_quiz_multiple_choice,
     },
   ];
   let quizzes_get = function lambda(question, answer) {
@@ -47,7 +48,6 @@ export function app_code_lesson_base_quizzes_forwards_backwards(
           batch_get,
           quizzes,
           next_get,
-          app_code_lesson_quiz_multiple_choice,
         );
       };
       return r;
