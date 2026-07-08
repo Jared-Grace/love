f0f831f6c07806aaf6c19d894c61904d0dd8f107
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 03:47:06 -0400

    function_auto_path ../love/public/src/property_swap.mjs

--- web/public/src/property_swap.mjs
+++ web/public/src/property_swap.mjs
@@ -1,8 +1,8 @@
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function property_swap(n, p1, p2) {
-  let v1 = property_get(n, p1);
-  let v2 = property_get(n, p2);
-  property_set(n, p1, v2);
-  property_set(n, p2, v1);
+export function property_swap(object, property_name_a, property_name_b) {
+  let a = property_get(object, property_name_a);
+  let b = property_get(object, property_name_b);
+  property_set(object, property_name_a, b);
+  property_set(object, property_name_b, a);
 }
