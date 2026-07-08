024574df6a3f5919c09963909b658df2b260685e
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 03:05:39 -0400

    w c js_code_binary,sorted_reversed

---
+++ web/public/src/js_code_binary_sorted_reversed.mjs
@@ -0,0 +1,4 @@
+import { js_code_binary } from "../../../love/public/src/js_code_binary.mjs";
+export function js_code_binary_sorted_reversed(left, operator, right) {
+  return js_code_binary(left, operator, right);
+}
