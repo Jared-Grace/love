2ded4f476977ac30a1d71d812bafbc8f3dd0b07e
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:35:15 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,12 +1,5 @@
-import { property_path_get } from "../../../love/public/src/property_path_get.mjs";
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
 export async function sandbox_3() {
-  let removed = property_path_get(
-    {
-      a: {
-        b: "y",
-      },
-    },
-    ["a", "b"],
-  );
-  return removed;
+  let expression = js_parse_expression("c=b");
+  return expression;
 }
