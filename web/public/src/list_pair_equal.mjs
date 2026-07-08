d5b12bd568e0e380408b026f50bcd3b52a2fc5af
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 05:51:43 -0400

    function_auto_path ../love/public/src/list_pair_equal.mjs

--- web/public/src/list_pair_equal.mjs
+++ web/public/src/list_pair_equal.mjs
@@ -1,14 +1,16 @@
+import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 import { each_pair_min } from "../../../love/public/src/each_pair_min.mjs";
 import { equal_not } from "../../../love/public/src/equal_not.mjs";
-export function list_pair_equal(list, list_prefix) {
+export function list_pair_equal(list_a, list_b) {
+  let e = lists_sizes_equal(lists);
   let difference = false;
   function lambda(left, right) {
     if (equal_not(left, right)) {
       difference = true;
     }
   }
-  each_pair_min(list, list_prefix, lambda);
+  each_pair_min(list_a, list_b, lambda);
   let n = not(difference);
   return n;
 }
