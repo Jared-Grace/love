44ae7704b23f2e8d0cdd7f7f7e6301acf0c90f36
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:23:41 -0400

    function_auto_path ../love/public/src/property_get.mjs

--- public/src/property_get.mjs
+++ public/src/property_get.mjs
@@ -1,5 +1,10 @@
+import { not } from "../../../love/public/src/not.mjs";
 import { undefined_not_is_assert_object_property } from "../../../love/public/src/undefined_not_is_assert_object_property.mjs";
 export function property_get(object, property_name) {
+  if (not(property_name in object)) {
+    let v = String(property_name);
+    throw new Error(`Property does not exist: ${v}`);
+  }
   let value = object[property_name];
   undefined_not_is_assert_object_property(value, object, property_name);
   return value;
