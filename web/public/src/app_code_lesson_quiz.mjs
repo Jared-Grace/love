b6398a905d3ceaff92da872735b7afba1dc38775
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:29:11 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz.mjs

--- web/public/src/app_code_lesson_quiz.mjs
+++ web/public/src/app_code_lesson_quiz.mjs
@@ -91,7 +91,7 @@ export function app_code_lesson_quiz(
   if (lcli) {
     let div = html_div_text(
       parent_container,
-      "There are no more lessons available at this time",
+      "Next: There are no more lessons available at this time",
     );
   } else {
     app_replace_button_wide_next(parent_container, on_next);
