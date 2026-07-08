37b5d983be0f230a35550610fc76a49250fa238b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 19:36:00 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_counting_quiz_backwards_on_button.mjs

--- web/public/src/app_code_lesson_symbols_counting_quiz_backwards_on_button.mjs
+++ web/public/src/app_code_lesson_symbols_counting_quiz_backwards_on_button.mjs
@@ -1,8 +1,10 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
+import { html_clear } from "../../../love/public/src/html_clear.mjs";
 export function app_code_lesson_symbols_counting_quiz_backwards_on_button(
   on_question,
 ) {
   let r = function lambda(parent, text) {
+    html_clear(parent);
     html_style_assign(parent, {
       "justify-content": "center",
     });
