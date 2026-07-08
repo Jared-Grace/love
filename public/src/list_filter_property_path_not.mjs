7553b8857784c9e735706c845ea31d37cb6ac702
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:40:59 -0400

    r t list_filter_properties_not,ies,y_path

--- public/src/list_filter_property_path_not.mjs
+++ public/src/list_filter_property_path_not.mjs
@@ -1,6 +1,6 @@
 import { property_path_get } from "../../../love/public/src/property_path_get.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
-export function list_filter_properties_not(list, property_names, value) {
+export function list_filter_property_path_not(list, property_names, value) {
   let c = function property_equals_not_curried_right_2_result(item) {
     let result = property_path_get(property_names, item);
     return result;
