160e07abc48eeb073366cbd2c231b6db9c88efad
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:41:56 -0400

    function_auto_path ../love/public/src/property_path_get.mjs

--- public/src/property_path_get.mjs
+++ public/src/property_path_get.mjs
@@ -1,10 +1,6 @@
 import { list_reduce } from "../../../love/public/src/list_reduce.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function property_path_get(item, property_names) {
-  function lambda(value, property_name) {
-    let value_next = property_get(value, property_name);
-    return value_next;
-  }
-  let result = list_reduce(property_names, lambda, item);
+  let result = list_reduce(property_names, property_get, item);
   return result;
 }
