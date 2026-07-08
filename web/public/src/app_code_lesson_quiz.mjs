cc03357fdeceac411e65130ef6b487bf1bf77303
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 20:58:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz.mjs

--- web/public/src/app_code_lesson_quiz.mjs
+++ web/public/src/app_code_lesson_quiz.mjs
@@ -1,4 +1,5 @@
-import { app_shared_button_next_text } from "../../../love/public/src/app_shared_button_next_text.mjs";
+import { sleep } from "../../../love/public/src/sleep.mjs";
+import { app_replace_button_wide_next } from "../../../love/public/src/app_replace_button_wide_next.mjs";
 import { app_shared_button_back_text } from "../../../love/public/src/app_shared_button_back_text.mjs";
 import { app_code_lesson_quiz_wrong_background_color } from "../../../love/public/src/app_code_lesson_quiz_wrong_background_color.mjs";
 import { at_least_1 } from "../../../love/public/src/at_least_1.mjs";
@@ -142,6 +143,7 @@ export function app_code_lesson_quiz(
         answered = true;
         app_shared_button_screen_green_style_assign(b);
         html_visibility_visible_multiple(hides);
+        await sleep(ms);
       } else {
         if (not(answered)) {
           let color_bg = app_code_lesson_quiz_wrong_background_color();
@@ -157,8 +159,3 @@ export function app_code_lesson_quiz(
     return b;
   }
 }
-function app_replace_button_wide_next(parent, on_next) {
-  let nt = app_shared_button_next_text();
-  let bn = app_replace_button_wide(parent, nt, on_next);
-}
-
