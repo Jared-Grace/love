481fb2a850c8c07bbb4e7765b5c289075f57d0f3
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 20:47:59 -0400

    function_auto_path ../love/public/src/app_code_lesson_symbols_batch_digits.mjs

--- public/src/app_code_lesson_symbols_batch_digits.mjs
+++ public/src/app_code_lesson_symbols_batch_digits.mjs
@@ -1,10 +1,13 @@
+import { add_1 } from "../../../love/public/src/add_1.mjs";
+import { range } from "../../../love/public/src/range.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { integer_positive_random_digits_text } from "../../../love/public/src/integer_positive_random_digits_text.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
-import { range_1 } from "../../../love/public/src/range_1.mjs";
 export function app_code_lesson_symbols_batch_digits() {
-  let digit_counts = range_1(5);
+  let count = 5;
+  let r_before = range(count);
+  let digit_counts = list_map(r_before, add_1);
   function lambda(digit_count) {
     let digits = integer_positive_random_digits_text(digit_count);
     return digits;
