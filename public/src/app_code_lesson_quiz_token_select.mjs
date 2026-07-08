b313cb7ed7defef725471b04c0ccbf200d80ea77
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:48:50 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz_token_select.mjs

--- public/src/app_code_lesson_quiz_token_select.mjs
+++ public/src/app_code_lesson_quiz_token_select.mjs
@@ -1,3 +1,4 @@
+import { list_map } from "../../../love/public/src/list_map.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { js_tokenizer } from "../../../love/public/src/js_tokenizer.mjs";
@@ -16,6 +17,8 @@ export function app_code_lesson_quiz_token_select(
   let quiz_answer = property_get(qa, answer_property);
   let tokens = js_tokenizer(quiz_answer);
   let mapped = list_map_property(tokens, "value");
+  function lambda(item) {}
+  let mapped2 = list_map(list, lambda);
   log(app_code_lesson_quiz_token_select.name, {
     mapped,
   });
