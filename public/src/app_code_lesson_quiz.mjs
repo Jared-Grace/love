0a46fd4215c95311c1afe38bc1221790418f46dd
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 23:47:47 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz.mjs

--- public/src/app_code_lesson_quiz.mjs
+++ public/src/app_code_lesson_quiz.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_quiz_multiple_choice } from "../../../love/public/src/app_code_lesson_quiz_multiple_choice.mjs";
 import { html_visibility_hidden } from "../../../love/public/src/html_visibility_hidden.mjs";
 import { html_visibility_visible } from "../../../love/public/src/html_visibility_visible.mjs";
 import { app_shared_button_next_text } from "../../../love/public/src/app_shared_button_next_text.mjs";
@@ -7,20 +8,12 @@ import { html_clear } from "../../../love/public/src/html_clear.mjs";
 import { sleep_seconds } from "../../../love/public/src/sleep_seconds.mjs";
 import { app_replace_button_wide_next } from "../../../love/public/src/app_replace_button_wide_next.mjs";
 import { app_shared_button_back_text } from "../../../love/public/src/app_shared_button_back_text.mjs";
-import { app_code_lesson_quiz_wrong_background_color } from "../../../love/public/src/app_code_lesson_quiz_wrong_background_color.mjs";
 import { at_least_1 } from "../../../love/public/src/at_least_1.mjs";
 import { app_code_quiz_index_get } from "../../../love/public/src/app_code_quiz_index_get.mjs";
 import { subtract_1 } from "../../../love/public/src/subtract_1.mjs";
 import { app_code_quiz_index_transform } from "../../../love/public/src/app_code_quiz_index_transform.mjs";
-import { app_code_answer_count_max } from "../../../love/public/src/app_code_answer_count_max.mjs";
 import { app_code_lesson_above } from "../../../love/public/src/app_code_lesson_above.mjs";
-import { html_font_color_set } from "../../../love/public/src/html_font_color_set.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { app_shared_button_screen_green_style_assign } from "../../../love/public/src/app_shared_button_screen_green_style_assign.mjs";
-import { html_style_margin_top } from "../../../love/public/src/html_style_margin_top.mjs";
-import { html_style_background_color_set } from "../../../love/public/src/html_style_background_color_set.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_visibility_hidden_multiple } from "../../../love/public/src/html_visibility_hidden_multiple.mjs";
 import { app_code_examples } from "../../../love/public/src/app_code_examples.mjs";
 import { app_shared_screen_set } from "../../../love/public/src/app_shared_screen_set.mjs";
@@ -33,16 +26,6 @@ import { list_index_last_is } from "../../../love/public/src/list_index_last_is.
 import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { app_replace_success_message } from "../../../love/public/src/app_replace_success_message.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
-import { list_sort_text_to } from "../../../love/public/src/list_sort_text_to.mjs";
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
-import { list_shuffle_take } from "../../../love/public/src/list_shuffle_take.mjs";
-import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
-import { list_remove_if_exists } from "../../../love/public/src/list_remove_if_exists.mjs";
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
-import { list_filter_remove } from "../../../love/public/src/list_filter_remove.mjs";
-import { or } from "../../../love/public/src/or.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
 import { list_pair_other } from "../../../love/public/src/list_pair_other.mjs";
 import { app_code_example_answer_label } from "../../../love/public/src/app_code_example_answer_label.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -129,55 +112,19 @@ export function app_code_lesson_quiz(
     quiz_question = property_get(qa, question_property);
     on_question(container_question, quiz_question);
     html_clear(answers_div);
-    let quiz_batch_items = batch_get();
-    function filter(quiz_batch_item) {
-      let question_batch = property_get(quiz_batch_item, question_property);
-      let answer_batch = property_get(quiz_batch_item, answer_property);
-      let eq_a = equal(answer_batch, quiz_answer);
-      let eq_q = equal(question_batch, quiz_question);
-      let ored = or(eq_a, eq_q);
-      return ored;
-    }
-    list_filter_remove(quiz_batch_items, filter);
-    let answers = list_map_property(quiz_batch_items, answer_property);
-    let answers_unique = list_unique(answers);
-    list_remove_if_exists(answers_unique, quiz_answer);
-    let answer_count_max = app_code_answer_count_max();
-    let nn2 = null_not_is(answer_count_override);
-    if (nn2) {
-      answer_count_max = answer_count_override;
-    }
-    let taken = list_shuffle_take(answers_unique, answer_count_max - 1);
-    let choices = list_concat(taken, [quiz_answer]);
-    list_sort_text_to(choices);
-    let buttons = list_map(choices, each_button);
-    let answered = false;
-    function each_button(quiz_choice) {
-      let b = app_replace_button_wide(answers_div, quiz_choice, on_click);
-      html_style_background_color_set(b, "#ececec");
-      html_style_margin_top(b, "0.2em");
-      async function on_click() {
-        let eq2 = equal(quiz_choice, quiz_answer);
-        if (eq2) {
-          answered = true;
-          app_shared_button_screen_green_style_assign(b);
-          html_clear(container_success_message);
-          await on_success();
-        } else {
-          on_wrong();
-          if (not(answered)) {
-            let color_bg = app_code_lesson_quiz_wrong_background_color();
-            html_style_background_color_set(b, color_bg);
-            html_font_color_set(b, "rgb(255, 255, 255)");
-          }
-        }
-      }
-      let nn = null_not_is(answer_on_button);
-      if (nn) {
-        answer_on_button(b, quiz_choice);
-      }
-      return b;
-    }
+    app_code_lesson_quiz_multiple_choice(
+      batch_get,
+      question_property,
+      answer_property,
+      quiz_answer,
+      quiz_question,
+      answer_count_override,
+      answers_div,
+      container_success_message,
+      on_success,
+      on_wrong,
+      answer_on_button,
+    );
   }
   function on_wrong() {
     html_visibility_hidden(container_success_message);
