f42f4447020c73b47378dc31803a260dccc99f6e
JESUS rose to life <username@users.noreply.github.com>
2026-07-07 23:52:14 -0400

    pb app_code_lesson_quiz_qa_question

--- web/public/src/app_code_lesson_quiz_qa_question.mjs
+++ web/public/src/app_code_lesson_quiz_qa_question.mjs
@@ -1,6 +1,6 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_code_lesson_quiz_qa_property_other } from "../../../love/public/src/app_code_lesson_quiz_qa_property_other.mjs";
-export function app_code_lesson_quiz_qa_question(answer_property, qa) {
+export function app_code_lesson_quiz_qa_question(qa, answer_property) {
   let question_property =
     app_code_lesson_quiz_qa_property_other(answer_property);
   let quiz_question = property_get(qa, question_property);
