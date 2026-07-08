1c75dab7fa943a5074bd9fafaab1c73edc86355b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:26:35 -0400

    function_auto_path ../love/public/src/js_tokenizer_normalized.mjs

--- public/src/js_tokenizer_normalized.mjs
+++ public/src/js_tokenizer_normalized.mjs
@@ -1,10 +1,12 @@
+import { property_get_curried_right } from "../../../love/public/src/property_get_curried_right.mjs";
 import { text_to } from "../../../love/public/src/text_to.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { js_tokenizer } from "../../../love/public/src/js_tokenizer.mjs";
 export function js_tokenizer_normalized(code) {
   let tokens = js_tokenizer(code);
-  let mapped = list_map_property(tokens, "value");
+  let property_name = "value";
+  let r = property_get_curried_right(property_name);
+  let mapped = list_map(tokens, r);
   let normalized = list_map(mapped, text_to);
   return normalized;
 }
