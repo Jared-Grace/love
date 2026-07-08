aec8665be15bb2717a9ab4ab380252f768149fe6
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 00:08:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_current.mjs

--- web/public/src/app_code_lesson_current.mjs
+++ web/public/src/app_code_lesson_current.mjs
@@ -1,5 +1,5 @@
+import { app_code_lesson_find_by_id } from "../../../love/public/src/app_code_lesson_find_by_id.mjs";
 import { app_code_lesson_first_id } from "../../../love/public/src/app_code_lesson_first_id.mjs";
-import { list_find_property } from "../../../love/public/src/list_find_property.mjs";
 import { storage_local_initialize_context } from "../../../love/public/src/storage_local_initialize_context.mjs";
 import { app_code_lessons } from "../../../love/public/src/app_code_lessons.mjs";
 export function app_code_lesson_current(context) {
@@ -10,7 +10,6 @@ export function app_code_lesson_current(context) {
     "lesson_id",
     first_id,
   );
-  let lessons1 = app_code_lessons();
-  let lesson = list_find_property(lessons1, "id", lesson_id);
+  let lesson = app_code_lesson_find_by_id(lessons, lesson_id);
   return lesson;
 }
