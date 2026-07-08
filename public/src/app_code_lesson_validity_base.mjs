e198dc5eaf6b3754aa123aa5d9611306d3cff9ef
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 19:26:49 -0400

    function_auto_path ../love/public/src/app_code_lesson_validity_base.mjs

--- public/src/app_code_lesson_validity_base.mjs
+++ public/src/app_code_lesson_validity_base.mjs
@@ -3,7 +3,6 @@ import { app_code_label_code_example } from "../../../love/public/src/app_code_l
 import { app_code_lesson_base } from "../../../love/public/src/app_code_lesson_base.mjs";
 import { app_code_style_normal_text } from "../../../love/public/src/app_code_style_normal_text.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
-import { html_text_set_code_dark } from "../../../love/public/src/html_text_set_code_dark.mjs";
 import { app_code_quiz_backwards_label_answer_validity } from "../../../love/public/src/app_code_quiz_backwards_label_answer_validity.mjs";
 import { app_code_label_code_question } from "../../../love/public/src/app_code_label_code_question.mjs";
 export function app_code_lesson_validity_base(
@@ -20,17 +19,16 @@ export function app_code_lesson_validity_base(
   let example_count = 2;
   let quiz_backwards_label_answer =
     app_code_quiz_backwards_label_answer_validity();
-  let on_question_forwards1 = html_text_set_code_dark;
   const quizzes = app_code_lesson_quizzes(
     batch,
     question_label,
-    on_question_forwards1,
+    on_question_forwards,
     quiz_label,
     noop,
     backwards_question_label,
     app_code_style_normal_text,
     quiz_backwards_label_answer,
-    on_question_forwards1,
+    on_question_forwards,
     2,
   );
   let lesson = app_code_lesson_base(
@@ -39,7 +37,7 @@ export function app_code_lesson_validity_base(
     above,
     example_count,
     batch,
-    on_question_forwards1,
+    on_question_forwards,
     example_label,
     quizzes,
     question_label,
