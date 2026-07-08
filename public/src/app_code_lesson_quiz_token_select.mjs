817666073561fb424300dbdfbbf3b9719018b7f7
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 06:38:48 -0400

    function_auto_path ../love/public/src/app_code_lesson_quiz_token_select.mjs

--- public/src/app_code_lesson_quiz_token_select.mjs
+++ public/src/app_code_lesson_quiz_token_select.mjs
@@ -62,6 +62,11 @@ export function app_code_lesson_quiz_token_select(
         let code2 = js_tokens_to_code(variation_first);
         function lambda5(index, token) {
           let index2 = text_index_of_from(code2, token, index);
+          log(app_code_lesson_quiz_token_select.name, {
+            index,
+            token,
+            index2,
+          });
           return index2;
         }
         let reduced = list_reduce(chosen, lambda5, 0);
