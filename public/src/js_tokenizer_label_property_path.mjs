fd24ccf6205ceebd262193c4582310aa4fb86f53
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:29:12 -0400

    function_auto_path ../love/public/src/js_tokenizer_normalized.mjs

--- public/src/js_tokenizer_label_property_path.mjs
+++ public/src/js_tokenizer_label_property_path.mjs
@@ -1,4 +1,4 @@
 export function js_tokenizer_label_property_path() {
-  let property_names = ["type", "label"];
-  return property_names;
+  let type_label = ["type", "label"];
+  return type_label;
 }
--- public/src/js_tokenizer_normalized.mjs
+++ public/src/js_tokenizer_normalized.mjs
@@ -8,12 +8,12 @@ import { js_tokenizer } from "../../../love/public/src/js_tokenizer.mjs";
 export function js_tokenizer_normalized(code) {
   let tokens = js_tokenizer(code);
   let property_name = "value";
-  let r = function lambda(object) {
-    let value = property_get(object, property_name);
-    let v = undefined_is(value);
-    if (false) {
-      let r2 = js_tokenizer_label_property_path();
-      let result = property_path_get(item, property_names);
+  let r = function lambda(item) {
+    let value = property_get(item, property_name);
+    let u = undefined_is(value);
+    if (u) {
+      let type_label = js_tokenizer_label_property_path();
+      value = property_path_get(item, type_label);
     }
     return value;
   };
