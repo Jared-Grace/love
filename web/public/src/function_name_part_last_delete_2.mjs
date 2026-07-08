9f16e824f4bbb0df6ffb004121065f4d37ea5db4
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:31:58 -0400

    function_auto_path ../love/public/src/function_name_part_last_delete_2.mjs

--- web/public/src/function_name_part_last_delete_2.mjs
+++ web/public/src/function_name_part_last_delete_2.mjs
@@ -4,9 +4,8 @@ import { function_name_to_parts } from "../../../love/public/src/function_name_t
 import { function_name_combine_multiple } from "../../../love/public/src/function_name_combine_multiple.mjs";
 export function function_name_part_last_delete_2(f_name_old) {
   let size = list_size(list);
-  let parts2 = function_name_to_parts(f_name_old);
-  list_pop(parts2);
-  let parts = parts2;
+  let parts = function_name_to_parts(f_name_old);
+  list_pop(parts);
   let f_name_new = function_name_combine_multiple(parts);
   return f_name_new;
 }
