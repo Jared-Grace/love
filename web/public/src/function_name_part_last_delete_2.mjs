f841a3ca9750af48ad2ba791a39317c4f7176b9a
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:32:35 -0400

    function_auto_path ../love/public/src/function_name_part_last_delete_2.mjs

--- web/public/src/function_name_part_last_delete_2.mjs
+++ web/public/src/function_name_part_last_delete_2.mjs
@@ -7,7 +7,7 @@ export function function_name_part_last_delete_2(f_name_old) {
   let count = 2;
   let parts = function_name_to_parts(f_name_old);
   let size = list_size(parts);
-  let difference = subtract(left, right);
+  let difference = subtract(size, count);
   list_pop(parts);
   let f_name_new = function_name_combine_multiple(parts);
   return f_name_new;
