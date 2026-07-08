e9648f06c250de9d5ceaeb37b8bdf798ff380b50
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:19:02 -0400

    function_auto_path ../love/public/src/module_acorn_get.mjs

--- public/src/js_tokenizer.mjs
+++ public/src/js_tokenizer.mjs
@@ -2,10 +2,11 @@ import { invoke_until } from "../../../love/public/src/invoke_until.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_parse_generic_arg } from "../../../love/public/src/js_parse_generic_arg.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
-export function js_tokenizer(acorn, code) {
+export function js_tokenizer( code) {
+  let acorn = module_acorn_get();
   let tokens = null;
   try {
-    let a = js_parse_generic_arg();
+    let a = js_parse_generic_arg(); 
     const tokenizer = acorn.tokenizer(code, a);
     function end_is(token) {
       let r = token.type.label === "eof";
--- public/src/module_acorn_get.mjs
+++ public/src/module_acorn_get.mjs
@@ -1,5 +1,6 @@
 import { global_import_get } from "../../../love/public/src/global_import_get.mjs";
 import { global_import_exists } from "../../../love/public/src/global_import_exists.mjs";
+import * as acorn from "acorn";
 export function module_acorn_get() {
   let a = null;
   let module_name = "acorn";
