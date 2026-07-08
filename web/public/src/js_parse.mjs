c2530ad88536fe7fc0c24955a11ad9d3db4a796c
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:19:02 -0400

    function_auto_path ../love/public/src/module_acorn_get.mjs

--- web/public/src/js_parse.mjs
+++ web/public/src/js_parse.mjs
@@ -1,8 +1,7 @@
 import { module_acorn_get } from "../../../love/public/src/module_acorn_get.mjs";
 import { js_parse_generic } from "../../../love/public/src/js_parse_generic.mjs";
-import * as acorn from "acorn";
 export function js_parse(code) {
-  let a = module_acorn_get();
-  let ast = js_parse_generic(a, code);
+  let acorn = module_acorn_get();
+  let ast = js_parse_generic(acorn, code);
   return ast;
 }
--- web/public/src/js_parse_statement.mjs
+++ web/public/src/js_parse_statement.mjs
@@ -6,8 +6,8 @@ export function js_parse_statement(code) {
   let code_d = js_code_function_declaration("a", code, true);
   let r = js_parse(code_d);
   let body = property_get(r, "body");
-  let r2 = list_first(body);
-  let block = property_get(r2, "body");
+  let f = list_first(body);
+  let block = property_get(f, "body");
   let statements = property_get(block, "body");
   const statement = list_first(statements);
   return statement;
