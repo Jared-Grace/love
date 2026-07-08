3e669eebcd3ef92e81934018cdb517e5704f3350
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:12:06 -0400

    function_auto_path ../love/public/src/js_code_binary_expression_commutative.mjs

--- web/public/src/js_code_binary_expression_commutative.mjs
+++ web/public/src/js_code_binary_expression_commutative.mjs
@@ -1,6 +1,6 @@
 import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
 export function js_code_binary_expression_commutative() {
   let p = js_operator_plus();
-  let c = [p];
-  return c;
+  let commutatives = [p];
+  return commutatives;
 }
