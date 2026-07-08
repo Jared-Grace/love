3e0c1bb00411cec8eb0643c86e838fec31e2263b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 06:34:51 -0400

    function_auto_path ../love/public/src/text_index_of_from.mjs

--- web/public/src/text_index_of_from.mjs
+++ web/public/src/text_index_of_from.mjs
@@ -1,7 +1,7 @@
+import { text_index_of_from_try } from "../../../love/public/src/text_index_of_from_try.mjs";
 import { text_index_of_assert } from "../../../love/public/src/text_index_of_assert.mjs";
-import { text_index_of_try } from "../../../love/public/src/text_index_of_try.mjs";
 export function text_index_of_from(t, item) {
-  let index = text_index_of_try(t, item);
+  let index = text_index_of_from_try(t, item);
   text_index_of_assert(t, index, item);
   return index;
 }
