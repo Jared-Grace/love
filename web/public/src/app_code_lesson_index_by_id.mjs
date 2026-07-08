d4cb48ae73cd7baa7757e5f7e0ad3b139d347e81
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:22:52 -0400

    function_auto_path ../love/public/src/app_code_lesson_index_by_id.mjs

--- web/public/src/app_code_lesson_index_by_id.mjs
+++ web/public/src/app_code_lesson_index_by_id.mjs
@@ -1,7 +1,9 @@
+import { list_index_of } from "../../../love/public/src/list_index_of.mjs";
 import { app_code_lesson_find_by_id } from "../../../love/public/src/app_code_lesson_find_by_id.mjs";
 import { app_code_lessons } from "../../../love/public/src/app_code_lessons.mjs";
 export function app_code_lesson_index_by_id(context, lesson_id) {
   let lessons = app_code_lessons();
   let lesson = app_code_lesson_find_by_id(lessons, lesson_id);
+  let index = list_index_of(list, item);
   return lesson;
 }
