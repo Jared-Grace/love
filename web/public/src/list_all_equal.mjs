08e8951f7e263de83e0d7564df2b60e40e8dafe7
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 05:46:46 -0400

    pb list_all_equal_to

--- web/public/src/list_all_equal.mjs
+++ web/public/src/list_all_equal.mjs
@@ -2,6 +2,6 @@ import { list_all_equal_to } from "../../../love/public/src/list_all_equal_to.mj
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function list_all_equal(list) {
   let first = list_first(list);
-  let eq = list_all_equal_to(first, list);
+  let eq = list_all_equal_to(list, first);
   return eq;
 }
--- web/public/src/list_all_equal_to.mjs
+++ web/public/src/list_all_equal_to.mjs
@@ -1,6 +1,6 @@
 import { list_all } from "../../../love/public/src/list_all.mjs";
 import { equal_curried } from "../../../love/public/src/equal_curried.mjs";
-export function list_all_equal_to(item, list) {
+export function list_all_equal_to(list, item) {
   let r = equal_curried(item);
   let eq = list_all(list, r);
   return eq;
