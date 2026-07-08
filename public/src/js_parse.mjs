4362d70843d121aa891626c7eb24ee3beb6e1258
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:16:17 -0400

    function_auto_path ../love/public/src/js_parse.mjs

--- public/src/js_parse.mjs
+++ public/src/js_parse.mjs
@@ -1,16 +1,8 @@
+import { module_acorn_get } from "../../../love/public/src/module_acorn_get.mjs";
 import { js_parse_generic } from "../../../love/public/src/js_parse_generic.mjs";
 import * as acorn from "acorn";
-import { global_import_get } from "../../../love/public/src/global_import_get.mjs";
-import { global_import_exists } from "../../../love/public/src/global_import_exists.mjs";
 export function js_parse(code) {
-  let a = null;
-  let module_name = "acorn";
-  let e = global_import_exists(module_name);
-  if (e) {
-    a = global_import_get(module_name);
-  } else {
-    a = acorn;
-  }
+  let a = module_acorn_get();
   let ast = js_parse_generic(a, code);
   return ast;
 }
