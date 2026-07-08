8831051d14f3659bd9c2dca5c5bef0f619d318e1
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:02:34 -0400

    function_auto_path ../love/public/src/js_parse_generic.mjs

--- public/src/js_parse_generic.mjs
+++ public/src/js_parse_generic.mjs
@@ -1,9 +1,10 @@
+import { js_parse_ecma_version } from "../../../love/public/src/js_parse_ecma_version.mjs";
 import { log_keep } from "../../../love/public/src/log_keep.mjs";
 export function js_parse_generic(acorn, code) {
   let ast = null;
   try {
     ast = acorn.parse(code, {
-      ecmaVersion: 2020,
+      ecmaVersion: js_parse_ecma_version(),
       sourceType: "module",
     });
   } catch (e) {
