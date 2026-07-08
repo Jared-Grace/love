1ec3b1ad5f539acbbaf67d79f41a14899696ae02
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:13:14 -0400

    function_auto_path ../love/public/src/js_tokenizer.mjs

--- web/public/src/js_tokenizer.mjs
+++ web/public/src/js_tokenizer.mjs
@@ -7,12 +7,11 @@ export function js_tokenizer(acorn, code) {
   try {
     let a = js_parse_generic_arg();
     const tokenizer = acorn.tokenizer(code, a);
-    const next_get = tokenizer.getToken;
     function end_is(token) {
       let r = token.type.label === "eof";
       return r;
     }
-    const tokens = invoke_until(next_get, end_is);
+    const tokens = invoke_until(tokenizer.getToken, end_is);
     log(js_tokenizer.name, {
       tokens,
     });
