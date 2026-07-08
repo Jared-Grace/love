0d5d50117fff35f8ffb40750c0659d38e0bfb15e
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 05:46:43 -0400

    function_auto_path ../love/public/src/list_all_equal.mjs

--- public/src/list_all_equal.mjs
+++ public/src/list_all_equal.mjs
@@ -1,9 +1,7 @@
-import { list_all } from "../../../love/public/src/list_all.mjs";
-import { equal_curried } from "../../../love/public/src/equal_curried.mjs";
+import { list_all_equal_to } from "../../../love/public/src/list_all_equal_to.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function list_all_equal(list) {
   let first = list_first(list);
-  let r = equal_curried(first);
-  let eq = list_all(list, r);
+  let eq = list_all_equal_to(first, list);
   return eq;
 }
