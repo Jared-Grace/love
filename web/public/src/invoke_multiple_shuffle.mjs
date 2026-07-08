4c28a1faba485e71276d5f142e35c9cfd7975ae2
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 02:12:20 -0400

    function_auto_path ../love/public/src/invoke_multiple_shuffle.mjs

--- web/public/src/invoke_multiple_shuffle.mjs
+++ web/public/src/invoke_multiple_shuffle.mjs
@@ -1,6 +1,6 @@
 import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 import { list_shuffle } from "../../../love/public/src/list_shuffle.mjs";
-export function invoke_multiple_shuffle(choices) {
-  list_shuffle(choices);
-  invoke_multiple(choices);
+export function invoke_multiple_shuffle(list) {
+  list_shuffle(list);
+  invoke_multiple(list);
 }
