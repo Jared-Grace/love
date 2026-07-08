702f3fef7e65a4c4be202d7005009de186ddc142
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:54:39 -0400

    function_auto_path ../love/public/src/list_concat_single_right.mjs

--- public/src/list_concat_single_right.mjs
+++ public/src/list_concat_single_right.mjs
@@ -1,7 +1,7 @@
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_copy } from "../../../love/public/src/list_copy.mjs";
 export function list_concat_single_right(list, single) {
-  let copy = list_copy(list);
-  list_add(copy, single);
-  return copy;
+  let concated = list_copy(list);
+  list_add(concated, single);
+  return concated;
 }
