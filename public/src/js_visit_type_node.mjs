372ffeae0e37f751d4714dddbc2de4880123b5cf
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 03:42:13 -0400

    pl js_visit_type_node on_node

--- public/src/js_visit_type_node.mjs
+++ public/src/js_visit_type_node.mjs
@@ -1,6 +1,6 @@
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_type_node(on_node, ast, type) {
+export function js_visit_type_node(ast, type, on_node) {
   function lambda7(v) {
     let n = property_get(v, "node");
     on_node(n);
