96bfbfa46f00985ad8fafeace86c3e9d0cbdd3bf
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 04:58:43 -0400

    function_auto_path ../love/public/src/lists_size_max.mjs

--- public/src/lists_size_max.mjs
+++ public/src/lists_size_max.mjs
@@ -1,8 +1,7 @@
+import { list_map_size } from "../../../love/public/src/list_map_size.mjs";
 import { list_max } from "../../../love/public/src/list_max.mjs";
-import { list_size } from "../../../love/public/src/list_size.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 export function lists_size_max(lists) {
-  let mapped = list_map(lists, list_size);
+  let mapped = list_map_size(lists);
   let max = list_max(mapped);
   return max;
 }
