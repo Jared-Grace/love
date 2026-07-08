30e2b23b9e9ec13c87559a1d9cda0ed5ba0dbd09
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:36:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs

--- web/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
+++ web/public/src/app_code_lesson_base_quizzes_forwards_backwards.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_quiz_multiple_choice } from "../../../love/public/src/app_code_lesson_quiz_multiple_choice.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { app_code_lesson_quiz } from "../../../love/public/src/app_code_lesson_quiz.mjs";
