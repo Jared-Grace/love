82f57ff039091a6dcdb7e6620aa3693bbcc98425
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:35:28 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

---
+++ public/src/text_replace_to_space.mjs
@@ -0,0 +1,5 @@
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
+export function text_replace_to_space(call2, missing) {
+  let replaced2 = text_replace(call2, missing, " ");
+  return replaced2;
+}
