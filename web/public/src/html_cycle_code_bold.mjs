049b5319a872ffcfc68e4eec1a0b1ac9c0809378
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:20:12 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

---
+++ web/public/src/html_cycle_code_bold.mjs
@@ -0,0 +1,8 @@
+import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
+import { html_bold } from "../../../love/public/src/html_bold.mjs";
+import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
+import { noop } from "../../../love/public/src/noop.mjs";
+export function html_cycle_code_bold(c, parts) {
+  let cycles = [noop, html_style_code_dark, noop, html_bold];
+  html_cycle(c, cycles, parts);
+}
