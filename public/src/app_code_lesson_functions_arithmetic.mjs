8dabff854fe62af39b1974338d4f5b069ffbc3a3
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:25:11 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -1,3 +1,4 @@
+import { app_code_code_example_name } from "../../../love/public/src/app_code_code_example_name.mjs";
 import { html_div_cycle_code_bold } from "../../../love/public/src/html_div_cycle_code_bold.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { equal_0 } from "../../../love/public/src/equal_0.mjs";
@@ -86,7 +87,8 @@ export function app_code_lesson_functions_arithmetic() {
     let o_f = list_first(operators);
     let c = app_code_container_light_blue(root);
     let verb = property_get(o_f, "verb");
-    html_div_cycle_code_bold(c, ["", verb, " is an example of a ", "function"]);
+    const name = "function";
+    app_code_code_example_name(c, verb, name);
     let r = js_operator_to_code_call(o_f, next);
     let code = property_get(r, "call");
     html_div_cycle_code_bold(c, [
