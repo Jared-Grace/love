4d353bbb11ecdd0455c4e30f1cb52cb72a24c40b
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:01:49 -0400

    function_auto_path ../love/public/src/list_filter_indices_odd.mjs

--- public/src/list_filter_indices_odd.mjs
+++ public/src/list_filter_indices_odd.mjs
@@ -1,12 +1,12 @@
 import { list_adder_each_index } from "../../../love/public/src/list_adder_each_index.mjs";
 import { integer_odd_is } from "../../../love/public/src/integer_odd_is.mjs";
-export function list_filter_indices_odd(parts) {
+export function list_filter_indices_odd(list) {
   function lambda(la, item, index) {
     let eq = integer_odd_is(index);
     if (eq) {
       la(item);
     }
   }
-  let r4 = list_adder_each_index(parts, lambda);
-  return r4;
+  let filtered = list_adder_each_index(list, lambda);
+  return filtered;
 }
