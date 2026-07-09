361aca9555672e24631c8d11dacdff9f2d2f3e21
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:58:44 -0400

    w c app_code_lesson_name_id,space

---
+++ public/src/app_code_lesson_name_id_space.mjs
@@ -0,0 +1,5 @@
+import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
+export function app_code_lesson_name_id_space(left, rights) {
+  let r = app_code_lesson_name_id(left, rights);
+  return r;
+}
