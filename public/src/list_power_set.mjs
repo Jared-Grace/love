05b3a7f619a07abd03c004ff039764e109d6df81
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:53:34 -0400

    function_auto_path ../love/public/src/list_power_set.mjs

--- public/src/list_power_set.mjs
+++ public/src/list_power_set.mjs
@@ -1,6 +1,8 @@
 import { list_power_set_inner } from "../../../love/public/src/list_power_set_inner.mjs";
-export function list_power_set(list, index, result, results) {
-  results = [];
+export function list_power_set(list) {
+  let index = 0;
+  let results = [];
+  let result = [];
   let r = list_power_set_inner(list, index, result, results);
   return r;
 }
