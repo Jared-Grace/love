5010653618f9cba888ff77f1c9c3eb42aceff683
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:48:45 -0400

    function_auto_path ../love/public/src/list_power_set.mjs

--- web/public/src/list_power_set.mjs
+++ web/public/src/list_power_set.mjs
@@ -1,6 +1,6 @@
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-export function list_power_set(list, index, result) {
+export function list_power_set(list, index, result, results) {
   let item = list_get(list, index);
   list_add(result, item);
   list_power_set(list, index + 1, result);
