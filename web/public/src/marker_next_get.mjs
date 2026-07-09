ebd9c4ae42d88432437d7fe6772983a1afaf1bd3
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 05:11:49 -0400

    np

--- web/public/src/marker_next_get.mjs
+++ web/public/src/marker_next_get.mjs
@@ -4,9 +4,9 @@ import { marker_next_index } from "../../../love/public/src/marker_next_index.mj
 import { list_get } from "../../../love/public/src/list_get.mjs";
 export function marker_next_get(a) {
   let ni = marker_next_index(a);
-  let stack = property_get(ni, "stack2");
+  let stack2 = property_get(ni, "stack2");
   let index = property_get(ni, "index");
-  let next = list_get(stack, index);
+  let next = list_get(stack2, index);
   let n = {
     next,
   };
