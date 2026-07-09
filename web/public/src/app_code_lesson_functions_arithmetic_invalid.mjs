352f1eaa203536fe59549cf47c9c1c058995fd3d
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:37:38 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

--- web/public/src/app_code_lesson_functions_arithmetic_invalid.mjs
+++ web/public/src/app_code_lesson_functions_arithmetic_invalid.mjs
@@ -1,32 +1,16 @@
 import { list_between_empty } from "../../../love/public/src/list_between_empty.mjs";
-import { js_operator_to_code_call } from "../../../love/public/src/js_operator_to_code_call.mjs";
 import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 import { app_code_lesson_validity_code } from "../../../love/public/src/app_code_lesson_validity_code.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { range_1_next } from "../../../love/public/src/range_1_next.mjs";
 import { app_code_lesson_operators_generic_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get_max.mjs";
 import { js_operators } from "../../../love/public/src/js_operators.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { each } from "../../../love/public/src/each.mjs";
-import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
-import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 export function app_code_lesson_functions_arithmetic_invalid() {
   let name_id = app_code_lesson_name_id("functions", ["arithmetic", "invalid"]);
   let operators = js_operators();
   let m = app_code_lesson_operators_generic_batch_get_max();
   let next = range_1_next(m);
   function batch_get() {
-    let mapper = function lambda2(o) {
-      let r = js_operator_to_code_call(o, next);
-      let answer = property_get(r, "call");
-      let question = property_get(r, "expression");
-      let r3 = {
-        question,
-        answer,
-      };
-      return r3;
-    };
-    let mapped = list_map(operators, mapper);
+    let mapped = [];
     return mapped;
   }
   let lesson = app_code_lesson_validity_code(batch_get, name_id, above);
@@ -42,19 +26,5 @@ export function app_code_lesson_functions_arithmetic_invalid() {
     ];
     let combined = list_between_empty(parts);
     ("Function calls require all of these parts: ");
-    let next = range_1_next(m);
-    function lambda(o) {
-      let r = js_operator_to_code_call(o, next);
-      let code = property_get(r, "call");
-      let expression = property_get(r, "expression");
-      let c = app_code_container_light_blue(root);
-      html_div_cycle_code(c, [
-        "Instead of ",
-        expression,
-        " we could write: ",
-        code,
-      ]);
-    }
-    each(operators, lambda);
   }
 }
