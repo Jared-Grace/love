42aac3306fc8632ad2e4d10595ddecd608b4e545
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 02:00:08 -0400

    de app_code_lesson_name_id_space

--- web/public/src/app_code_lesson_name_id_space.mjs
+++
@@ -1,7 +0,0 @@
-import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
-import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
-export function app_code_lesson_name_id_space(left, rights) {
-  let split = text_split_space(rights);
-  let r = app_code_lesson_name_id(left, split);
-  return r;
-}
