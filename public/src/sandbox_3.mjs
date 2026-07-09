810a219d6a7f73510d2577a0efe708a5c72f0e5b
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:57:28 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,4 +1,5 @@
 import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 export async function sandbox_3() {
   let r = app_code_lesson_name_id("a", ["b"]);
+  return r;
 }
