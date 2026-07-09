9a520a5c07b4e6010d3402d7fa3b73377a2aa1c7
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:35:40 -0400

    function_auto_path ../love/public/src/text_replace_to_space.mjs

--- web/public/src/text_replace_to_space.mjs
+++ web/public/src/text_replace_to_space.mjs
@@ -1,5 +1,5 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
-export function text_replace_to_space(call2, missing) {
-  let replaced2 = text_replace(call2, missing, " ");
+export function text_replace_to_space(t, from) {
+  let replaced2 = text_replace(t, from, " ");
   return replaced2;
 }
