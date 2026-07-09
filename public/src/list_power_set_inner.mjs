08185d65e8c7c78a97e1d168c4996fc6e48c2cd4
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:52:38 -0400

    function_auto_path ../love/public/src/list_power_set_inner.mjs

--- public/src/list_power_set_inner.mjs
+++ public/src/list_power_set_inner.mjs
@@ -1,3 +1,4 @@
+import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { list_remove_last_equal } from "../../../love/public/src/list_remove_last_equal.mjs";
 import { list_index_not_is } from "../../../love/public/src/list_index_not_is.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
@@ -5,6 +6,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 export function list_power_set_inner(list, index, result, results) {
   let n = list_index_not_is(list, index);
   if (n) {
+    let concated = list_concat(a, b);
     list_add(results, result);
     return;
   }
