26797f72f7e8c5a0e76ce59afd2ee85f2f189dd7
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:51:49 -0400

    r c list_power_set,inner

--- web/public/src/list_power_set_inner.mjs
+++ web/public/src/list_power_set_inner.mjs
@@ -2,7 +2,7 @@ import { list_remove_last_equal } from "../../../love/public/src/list_remove_las
 import { list_index_not_is } from "../../../love/public/src/list_index_not_is.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
-export function list_power_set(list, index, result, results) {
+export function list_power_set_inner(list, index, result, results) {
   let n = list_index_not_is(list, index);
   if (n) {
     list_add(results, result);
@@ -10,7 +10,7 @@ export function list_power_set(list, index, result, results) {
   }
   let item = list_get(list, index);
   list_add(result, item);
-  list_power_set(list, index + 1, result);
+  list_power_set_inner(list, index + 1, result);
   list_remove_last_equal(result, item);
-  list_power_set(list, index + 1, result);
+  list_power_set_inner(list, index + 1, result);
 }
