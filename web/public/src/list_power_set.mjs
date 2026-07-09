21b62a1dc1c8bce7233708e05b9cd8e2586e471d
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:49:33 -0400

    function_auto_path ../love/public/src/list_power_set.mjs

--- web/public/src/list_power_set.mjs
+++ web/public/src/list_power_set.mjs
@@ -1,10 +1,11 @@
-import { equal } from "../../../love/public/src/equal.mjs";
+import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 import { list_get } from "../../../love/public/src/list_get.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function list_power_set(list, index, result, results) {
   let size = list_size(list);
-  if (equal(left, right)) {
+  let result2 = list_index_is(list2, index2);
+  if (false) {
   }
   let item = list_get(list, index);
   list_add(result, item);
