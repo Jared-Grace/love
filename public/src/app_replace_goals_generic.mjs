26aea506c26837230956a44ab9217eeafaa86f27
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 03:34:40 -0400

    function_auto_path ../love/public/src/app_replace_goals_generic.mjs

--- public/src/app_replace_goals_generic.mjs
+++ public/src/app_replace_goals_generic.mjs
@@ -3,7 +3,6 @@ import { app_replace_button_wide_text_left_centered } from "../../../love/public
 import { add_1_period } from "../../../love/public/src/add_1_period.mjs";
 import { html_style_background_color_set_if } from "../../../love/public/src/html_style_background_color_set_if.mjs";
 import { app_replace_rule_set_highlight } from "../../../love/public/src/app_replace_rule_set_highlight.mjs";
-import { app_replace_button_rule_style } from "../../../love/public/src/app_replace_button_rule_style.mjs";
 import { emoji_point_right } from "../../../love/public/src/emoji_point_right.mjs";
 import { string_pad_left_space } from "../../../love/public/src/string_pad_left_space.mjs";
 import { emoji_check } from "../../../love/public/src/emoji_check.mjs";
@@ -35,7 +34,6 @@ export function app_replace_goals_generic(
   );
   let title = property_get(r, "title");
   let button = property_get(r, "button");
-  app_replace_button_rule_style(button);
   let background = app_replace_rule_set_highlight();
   html_style_background_color_set_if(choose_this_next, button, background);
   return title;
