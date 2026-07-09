346477a8039a05281662882e85fcbaca0185acd2
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:50:20 -0400

    function_auto_path ../love/public/src/list_index_not_is.mjs

--- public/src/list_index_not_is.mjs
+++ public/src/list_index_not_is.mjs
@@ -3,4 +3,5 @@ import { list_index_is } from "../../../love/public/src/list_index_is.mjs";
 export function list_index_not_is(list, index) {
   let b = list_index_is(list, index);
   let n = not(b);
+  return n;
 }
