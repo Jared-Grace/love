259c9558042af158e664267641c5f653a06bd637
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:53:35 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id.mjs

--- web/public/src/app_code_lesson_name_id.mjs
+++ web/public/src/app_code_lesson_name_id.mjs
@@ -1,7 +1,7 @@
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 import { text_combine_middle_space } from "../../../love/public/src/text_combine_middle_space.mjs";
 export function app_code_lesson_name_id(left, rights) {
-  let joined = list_join_comma_space(list);
+  let joined = list_join_comma_space(rights);
   let combined = text_combine_middle_space(left, right);
   return combined;
 }
