8f26961bfc9f76613de5d17645c61d7e24932650
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 22:53:57 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- public/src/app_code_home.mjs
+++ public/src/app_code_home.mjs
@@ -6,14 +6,14 @@ import { app_shared_screen_set } from "../../../love/public/src/app_shared_scree
 import { storage_local_set_context } from "../../../love/public/src/storage_local_set_context.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
-import { each } from "../../../love/public/src/each.mjs";
 import { app_code_lessons } from "../../../love/public/src/app_code_lessons.mjs";
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
+import { each_index } from "../../../love/public/src/each_index.mjs";
 export function app_code_home(context) {
   let root = html_clear_context(context);
   let div = html_div_text(root, "Lessons:");
   let lessons = app_code_lessons();
-  function lambda(item) {
+  function lambda(item, index) {
     let name = property_get(item, "name");
     let id = property_get(item, "id");
     async function lambda3() {
@@ -24,5 +24,5 @@ export function app_code_home(context) {
     let combined = text_combine_middle_space(text, name);
     let b = app_replace_button_wide(root, combined, lambda3);
   }
-  each(lessons, lambda);
+  each_index(lessons, lambda);
 }
