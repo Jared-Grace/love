3fbaacb059b67012fd538f8928ffed712901cc0d
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:58:52 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id_space.mjs

--- public/src/app_code_lesson_name_id_space.mjs
+++ public/src/app_code_lesson_name_id_space.mjs
@@ -1,5 +1,7 @@
+import { text_split_space } from "../../../love/public/src/text_split_space.mjs";
 import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 export function app_code_lesson_name_id_space(left, rights) {
+  let split = text_split_space(s);
   let r = app_code_lesson_name_id(left, rights);
   return r;
 }
