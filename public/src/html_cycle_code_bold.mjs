9d27041af17a7231c4fe43734c99a14e76f97057
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:23:09 -0400

    function_auto_path ../love/public/src/html_div_cycle_code_bold.mjs

--- public/src/html_cycle_code_bold.mjs
+++ public/src/html_cycle_code_bold.mjs
@@ -2,7 +2,7 @@ import { html_cycle } from "../../../love/public/src/html_cycle.mjs";
 import { html_bold } from "../../../love/public/src/html_bold.mjs";
 import { html_style_code_dark } from "../../../love/public/src/html_style_code_dark.mjs";
 import { noop } from "../../../love/public/src/noop.mjs";
-export function html_cycle_code_bold(c, parts) {
+export function html_cycle_code_bold(parent, parts) {
   let cycles = [noop, html_style_code_dark, noop, html_bold];
-  html_cycle(c, cycles, parts);
+  html_cycle(parent, cycles, parts);
 }
--- public/src/html_div_cycle_code_bold.mjs
+++ public/src/html_div_cycle_code_bold.mjs
@@ -2,5 +2,5 @@ import { html_cycle_code_bold } from "../../../love/public/src/html_cycle_code_b
 import { html_div } from "../../../love/public/src/html_div.mjs";
 export function html_div_cycle_code_bold(parent, parts) {
   let div = html_div(parent);
-  html_cycle_code_bold(parent, parts);
+  html_cycle_code_bold(div, parts);
 }
