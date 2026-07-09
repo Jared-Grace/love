d3b8d3b26e57aa02e828418df343643b88bc306b
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:43:32 -0400

    function_auto_path ../love/public/src/list_between_comma_space_before_after.mjs

--- public/src/list_between_comma_space_before_after.mjs
+++ public/src/list_between_comma_space_before_after.mjs
@@ -2,6 +2,6 @@ import { list_between_before_after_generic } from "../../../love/public/src/list
 import { list_between_comma_space } from "../../../love/public/src/list_between_comma_space.mjs";
 export function list_between_comma_space_before_after(before, list, after) {
   let mapper = list_between_comma_space;
-  let squashed = list_between_before_after_generic(mapper, list, before, after);
-  return squashed;
+  let concated = list_between_before_after_generic(mapper, list, before, after);
+  return concated;
 }
