dc684c6856ce8cb6ed614d6d1232f67925dc6dcf
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:10:06 -0400

    function_auto_path ../love/public/src/js_operator_to_code_call.mjs

--- web/public/src/js_operator_to_code_call.mjs
+++ web/public/src/js_operator_to_code_call.mjs
@@ -1,3 +1,4 @@
+import { property_set_if_exists_not } from "../../../love/public/src/property_set_if_exists_not.mjs";
 import { js_operator_left_right_to_code_call } from "../../../love/public/src/js_operator_left_right_to_code_call.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_operator_to_expression } from "../../../love/public/src/js_operator_to_expression.mjs";
@@ -5,6 +6,7 @@ export function js_operator_to_code_call(o, next) {
   let e = js_operator_to_expression(o, next);
   let question = property_get(e, "expression");
   let answer = js_operator_left_right_to_code_call(o, e);
+  property_set_if_exists_not(a, p, value);
   let r = {
     question,
     answer,
