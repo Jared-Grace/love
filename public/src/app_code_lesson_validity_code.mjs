17c6ca94c8b80591133f039203d1c8ff3ee412ab
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:38:22 -0400

    function_auto_path ../love/public/src/app_code_lesson_validity_operator.mjs

---
+++ public/src/app_code_lesson_validity_code.mjs
@@ -0,0 +1,17 @@
+import { html_text_set_code_dark } from "../../../love/public/src/html_text_set_code_dark.mjs";
+import { app_code_lesson_validity_base } from "../../../love/public/src/app_code_lesson_validity_base.mjs";
+import { app_code_label_code_example } from "../../../love/public/src/app_code_label_code_example.mjs";
+export function app_code_lesson_validity_code(batch, id, name, above) {
+  const example_answer_label = app_code_label_code_example();
+  let lesson = app_code_lesson_validity_base(
+    batch,
+    id,
+    name,
+    above,
+    "Code validity: ",
+    html_text_set_code_dark,
+    example_answer_label,
+    html_text_set_code_dark,
+  );
+  return lesson;
+}
