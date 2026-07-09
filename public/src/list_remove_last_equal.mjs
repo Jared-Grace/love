092e09ae3b9b85a762f5cea1041b53a41089a765
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:51:29 -0400

    function_auto_path ../love/public/src/list_remove_last_equal.mjs

--- public/src/list_remove_last_equal.mjs
+++ public/src/list_remove_last_equal.mjs
@@ -1,6 +1,6 @@
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { list_remove_last } from "../../../love/public/src/list_remove_last.mjs";
-export function list_remove_last_equal(item, expected_last) {
-  let popped = list_remove_last(item);
+export function list_remove_last_equal(list, expected_last) {
+  let popped = list_remove_last(list);
   equal_assert(popped, expected_last);
 }
