2ff24db92f70f63976136065797b3a4905f878fe
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:37:07 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

---
+++ web/public/src/list_between_empty.mjs
@@ -0,0 +1,5 @@
+import { list_between } from "../../../love/public/src/list_between.mjs";
+export function list_between_empty(parts) {
+  let combined2 = list_between(parts, "");
+  return combined2;
+}
