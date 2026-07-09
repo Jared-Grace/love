69fa18ffb4a23d871bfe89291e329c838525b46d
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:22:27 -0400

    function_auto_path ../love/public/src/html_div_cycle_code_bold.mjs

--- public/src/html_div_cycle_code_bold.mjs
+++ public/src/html_div_cycle_code_bold.mjs
@@ -1,6 +1,6 @@
 import { html_cycle_code_bold } from "../../../love/public/src/html_cycle_code_bold.mjs";
 import { html_div } from "../../../love/public/src/html_div.mjs";
-export function html_div_cycle_code_bold(c, parts) {
+export function html_div_cycle_code_bold(parent, parts) {
   let div = html_div(parent);
-  html_cycle_code_bold(c, parts);
+  html_cycle_code_bold(parent, parts);
 }
