f8a3b92a2d47c193e46801513df5ad6eeb7207ec
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:01:10 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

--- web/public/src/app_code_lesson_functions_arithmetic_invalid.mjs
+++ web/public/src/app_code_lesson_functions_arithmetic_invalid.mjs
@@ -1,6 +1,6 @@
+import { integer_odd_is } from "../../../love/public/src/integer_odd_is.mjs";
 import { list_between_space } from "../../../love/public/src/list_between_space.mjs";
 import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
-import { integer_even_is } from "../../../love/public/src/integer_even_is.mjs";
 import { list_adder_each_index } from "../../../love/public/src/list_adder_each_index.mjs";
 import { list_between_space_before } from "../../../love/public/src/list_between_space_before.mjs";
 import { js_operator_to_expression } from "../../../love/public/src/js_operator_to_expression.mjs";
@@ -53,7 +53,7 @@ export function app_code_lesson_functions_arithmetic_invalid() {
     let combined = list_between_space_before(parts);
     html_div_cycle_code(c, combined);
     function lambda(la, item, index) {
-      let eq = integer_even_is(index);
+      let eq = integer_odd_is(index);
       if (eq) {
         la(item);
       }
