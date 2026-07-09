6b6fc954acd48320e2b28af73aff562f14cbe023
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 22:58:26 -0400

    function_auto_path ../love/public/src/app_replace_goals_generic.mjs

---
+++ web/public/src/app_replace_button_wide_text_left_centered.mjs
@@ -0,0 +1,20 @@
+import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { html_style_text_left_centered } from "../../../love/public/src/html_style_text_left_centered.mjs";
+import { app_replace_button_wide } from "../../../love/public/src/app_replace_button_wide.mjs";
+export function app_replace_button_wide_text_left_centered(
+  root,
+  lambda,
+  text_left,
+  text_centered,
+) {
+  let button = app_replace_button_wide(root, "", lambda);
+  let r = html_style_text_left_centered(button, text_left, text_centered);
+  let title = property_get(r, "title");
+  html_style_set(title, "line-height", 1.5);
+  let r2 = {
+    button,
+    title,
+  };
+  return r2;
+}
