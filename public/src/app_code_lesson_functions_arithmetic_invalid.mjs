d27cd4a109d358d49997a1b98ddb0f646e277e72
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:37:52 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

--- public/src/app_code_lesson_functions_arithmetic_invalid.mjs
+++ public/src/app_code_lesson_functions_arithmetic_invalid.mjs
@@ -1,15 +1,27 @@
 import { list_between_empty } from "../../../love/public/src/list_between_empty.mjs";
+import { js_operator_to_code_call } from "../../../love/public/src/js_operator_to_code_call.mjs";
 import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 import { app_code_lesson_validity_code } from "../../../love/public/src/app_code_lesson_validity_code.mjs";
 import { range_1_next } from "../../../love/public/src/range_1_next.mjs";
 import { app_code_lesson_operators_generic_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_generic_batch_get_max.mjs";
 import { js_operators } from "../../../love/public/src/js_operators.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_code_lesson_functions_arithmetic_invalid() {
   let name_id = app_code_lesson_name_id("functions", ["arithmetic", "invalid"]);
   let operators = js_operators();
   let m = app_code_lesson_operators_generic_batch_get_max();
   let next = range_1_next(m);
   function batch_get() {
+    let mapper = function lambda2(o) {
+      let r = js_operator_to_code_call(o, next);
+      let answer = property_get(r, "call");
+      let question = property_get(r, "expression");
+      let r3 = {
+        question,
+        answer,
+      };
+      return r3;
+    };
     let mapped = [];
     return mapped;
   }
