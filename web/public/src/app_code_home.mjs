f3acd368b9fe176516131128b14e901dbed2353b
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 22:38:10 -0400

    function_auto_path ../love/public/src/app_code_home.mjs

--- web/public/src/app_code_home.mjs
+++ web/public/src/app_code_home.mjs
@@ -9,7 +9,7 @@ import { app_code_lessons } from "../../../love/public/src/app_code_lessons.mjs"
 import { html_clear_context } from "../../../love/public/src/html_clear_context.mjs";
 export function app_code_home(context) {
   let root = html_clear_context(context);
-  let div = html_div_text(parent, text);
+  let div = html_div_text(root, "Lessons:");
   let lessons = app_code_lessons();
   function lambda(item) {
     let name = property_get(item, "name");
