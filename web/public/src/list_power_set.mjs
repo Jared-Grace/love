8aaa601c917ad2db248a1b8cb2ebb2596cd995c2
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:47:41 -0400

    function_auto_path ../love/public/src/list_power_set.mjs

--- web/public/src/list_power_set.mjs
+++ web/public/src/list_power_set.mjs
@@ -1,1 +1,4 @@
-export function list_power_set(list) {}
+import { list_add } from "../../../love/public/src/list_add.mjs";
+export function list_power_set(list, index, result) {
+  list_add(list2, item);
+}
