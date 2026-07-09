f7647d539cf46acdd572f4d8393a69d6ed942d12
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:55:18 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id.mjs

--- web/public/src/app_code_lesson_name_id.mjs
+++ web/public/src/app_code_lesson_name_id.mjs
@@ -1,3 +1,4 @@
+import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 import { text_combine_middle_space } from "../../../love/public/src/text_combine_middle_space.mjs";
@@ -5,5 +6,9 @@ export function app_code_lesson_name_id(left, rights) {
   let joined = list_join_comma_space(rights);
   let wrapped = text_wrap_parenthesis(joined);
   let name = text_combine_middle_space(left, wrapped);
-  return {name};
+  let concated = list_concat_single(single, list);
+  let r = {
+    name,
+  };
+  return r;
 }
