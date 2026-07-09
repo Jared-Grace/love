190e81aa592f208da49d804a35c9b8aae94f826a
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:43:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

--- web/public/src/app_code_lesson_functions_arithmetic_invalid.mjs
+++ web/public/src/app_code_lesson_functions_arithmetic_invalid.mjs
@@ -36,13 +36,9 @@ export function app_code_lesson_functions_arithmetic_invalid() {
     let o_f = list_first(operators);
     let verb = property_get(o_f, "verb");
     let c = app_code_container_light_blue(root);
-    let {
-    left,
-    right,
-    expression,
-  } = js_operator_to_expression(o_f, next);
-    let left = next();
-    let right = next();
+    let r2 = js_operator_to_expression(o_f, next);
+    let right = property_get(r2, "right");
+    let left = property_get(r2, "left");
     let parts = ["add", "(", left, ",", right, ")"];
     let combined = list_between_empty(parts);
     let parts2 = ["Calling the add function requires all of these parts: "];
