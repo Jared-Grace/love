8021d2d27ff0c454bcddc0c81b1a07393b59e963
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:07:35 -0400

    r i js_operator_to_call,call,code

--- web/public/src/js_operator_to_code_call.mjs
+++ web/public/src/js_operator_to_code_call.mjs
@@ -1,7 +1,7 @@
 import { js_operator_left_right_to_code_call } from "../../../love/public/src/js_operator_left_right_to_code_call.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_operator_to_expression } from "../../../love/public/src/js_operator_to_expression.mjs";
-export function js_operator_to_call(o, next) {
+export function js_operator_to_code_call(o, next) {
   let e = js_operator_to_expression(o, next);
   let question = property_get(e, "expression");
   let answer = js_operator_left_right_to_code_call(o, e);
