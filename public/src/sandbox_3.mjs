593c0cee01f82a94ee5864b9be02feaec9bb2ac2
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 02:07:20 -0400

    function_auto_path ../love/public/src/sandbox_3.mjs

--- public/src/sandbox_3.mjs
+++ public/src/sandbox_3.mjs
@@ -1,5 +1,5 @@
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function sandbox_3() {
-  let result = await data_identifiers_search(ids_comma);
+  let result = await data_identifiers_search("id,name");
   return result;
 }
