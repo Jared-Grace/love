adccfa2e9f012e139daa9b87d12088ad491febe9
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:16:13 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -80,6 +80,8 @@ export function app_code_lesson_functions_arithmetic() {
       ]);
       let eq = equal_0(index);
       if (eq) {
+        let verb = property_get(o, "verb");
+        html_div_cycle_code(c, ["", verb, " is an example of a ", "function"]);
         html_div_cycle_code(c, ["", code, " is called a ", "function call"]);
       }
     }
