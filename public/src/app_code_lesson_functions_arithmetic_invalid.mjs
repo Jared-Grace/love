1ac9f5380fa7587c89aec9938e826fe14f463d46
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 08:36:08 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

--- public/src/app_code_lesson_functions_arithmetic_invalid.mjs
+++ public/src/app_code_lesson_functions_arithmetic_invalid.mjs
@@ -31,6 +31,13 @@ export function app_code_lesson_functions_arithmetic_invalid() {
   let lesson = app_code_lesson_validity_code(batch_get, name_id, above);
   return lesson;
   function above(root) {
+    ("Function calls require all of these parts: ",
+      "function_name",
+      "(",
+      "any_arguments",
+      ",",
+      "separated_by_commas",
+      ")");
     let next = range_1_next(m);
     function lambda(o) {
       let r = js_operator_to_code_call(o, next);
