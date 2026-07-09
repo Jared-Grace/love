51c72969562a444e3aece161448812b354e97ddb
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 09:32:18 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic_invalid.mjs

--- web/public/src/js_operator_to_code_call_only.mjs
+++ web/public/src/js_operator_to_code_call_only.mjs
@@ -2,6 +2,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_operator_to_code_call } from "../../../love/public/src/js_operator_to_code_call.mjs";
 export function js_operator_to_code_call_only(o_f, next) {
   let r = js_operator_to_code_call(o_f, next);
-  let code = property_get(r, "call");
-  return code;
+  let call = property_get(r, "call");
+  return call;
 }
