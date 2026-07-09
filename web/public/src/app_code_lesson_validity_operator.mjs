f90e491c945b272be1d0279edb235856ae26737a
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 23:02:56 -0400

    function_auto_path ../love/public/src/app_code_lesson_validity_operator.mjs

--- web/public/src/app_code_lesson_validity_operator.mjs
+++ web/public/src/app_code_lesson_validity_operator.mjs
@@ -2,10 +2,8 @@ import { app_code_label_code_example } from "../../../love/public/src/app_code_l
 import { html_text_set_code_dark } from "../../../love/public/src/html_text_set_code_dark.mjs";
 import { app_code_lesson_validity_base } from "../../../love/public/src/app_code_lesson_validity_base.mjs";
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
-import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
 export function app_code_lesson_validity_operator(math_name, batch, above) {
-  let inside = text_first_upper_to(math_name);
-  let name = "Operators " + text_wrap_parenthesis(inside);
+  let name = "Operators " + text_wrap_parenthesis(math_name);
   let id = "operators_" + math_name;
   const example_answer_label = app_code_label_code_example();
   let lesson = app_code_lesson_validity_base(
