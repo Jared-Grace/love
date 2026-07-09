56fbbdc183f66b08d2bdb0a7d548376c36530130
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 10:00:47 -0400

    function_auto_path ../love/public/src/text_replace_multiple_to.mjs

--- public/src/text_replace_multiple_to.mjs
+++ public/src/text_replace_multiple_to.mjs
@@ -1,9 +1,10 @@
 import { each } from "../../../love/public/src/each.mjs";
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 export function text_replace_multiple_to(t, froms, to) {
+  let replaced = t;
   function lambda(from) {
     t = text_replace(t, from, to);
   }
   each(froms, lambda);
-  return t;
+  return replaced;
 }
