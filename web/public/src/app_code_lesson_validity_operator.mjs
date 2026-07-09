c23e3232b7e5c68430b888c2789c541f44aeae01
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 02:14:55 -0400

    o app_code_lesson_operators_minus

--- web/public/src/app_code_lesson_validity_operator.mjs
+++ web/public/src/app_code_lesson_validity_operator.mjs
@@ -1,8 +1,10 @@
 import { app_code_lesson_validity_code } from "../../../love/public/src/app_code_lesson_validity_code.mjs";
-import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
+import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_code_lesson_validity_operator(math_name, batch, above) {
-  let name = "Operators " + text_wrap_parenthesis(math_name);
-  let id = "operators_" + math_name;
+  let r = app_code_lesson_name_id("operators", [math_name]);
+  let id = property_get(r, "id");
+  let name = property_get(r, "name");
   let lesson = app_code_lesson_validity_code(batch, id, name, above);
   return lesson;
 }
--- web/public/src/sandbox_3.mjs
+++ web/public/src/sandbox_3.mjs
@@ -1,7 +1,16 @@
 import { properties_get } from "../../../love/public/src/properties_get.mjs";
 import { data_identifiers_search } from "../../../love/public/src/data_identifiers_search.mjs";
 export async function sandbox_3() {
-  let result = await data_identifiers_search("id,name");
-  let properties = properties_get(result);
-  return properties;
+  return ['app_code_lesson_identifiers_symbol_first_generic',
+  'app_code_lesson_identifiers_symbol_first_unseparated',
+  'app_code_lesson_identifiers_symbol_first',
+  'app_code_lesson_identifiers_underscores_generic',
+  'app_code_lesson_identifiers_valid',
+  'app_code_lesson_name_id',
+  'app_code_lesson_operators_generic',
+  'app_code_lesson_operators_plus_unary',
+  'app_code_lesson_symbols_counting',
+  'app_code_lesson_validity_base',
+  'app_code_lesson_validity_code',
+  'app_code_lesson_validity_operator',];
 }
