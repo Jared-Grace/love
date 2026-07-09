b9713e18ebd7f6493bfa1cb38c7881799743f71d
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 01:52:35 -0400

    de app_code_lesson_name

--- web/public/src/app_code_lesson_name.mjs
+++
@@ -1,4 +0,0 @@
-import { text_combine_middle_space } from "../../../love/public/src/text_combine_middle_space.mjs";
-export function app_code_lesson_name(left, right) {
-  return text_combine_middle_space(left, right);
-}
