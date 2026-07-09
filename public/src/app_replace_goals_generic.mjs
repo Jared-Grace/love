42303f03e6aaa03876f1d9c9749979592d8465b7
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 22:52:43 -0400

    function_auto_path ../love/public/src/app_replace_goals_generic.mjs

--- public/src/app_replace_goals_generic.mjs
+++ public/src/app_replace_goals_generic.mjs
@@ -1,3 +1,4 @@
+import { add_1_period } from "../../../love/public/src/add_1_period.mjs";
 import { html_style_background_color_set_if } from "../../../love/public/src/html_style_background_color_set_if.mjs";
 import { app_replace_rule_set_highlight } from "../../../love/public/src/app_replace_rule_set_highlight.mjs";
 import { html_style_set } from "../../../love/public/src/html_style_set.mjs";
@@ -8,7 +9,6 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { emoji_point_right } from "../../../love/public/src/emoji_point_right.mjs";
 import { string_pad_left_space } from "../../../love/public/src/string_pad_left_space.mjs";
 import { emoji_check } from "../../../love/public/src/emoji_check.mjs";
-import { add_1 } from "../../../love/public/src/add_1.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 export function app_replace_goals_generic(
   root,
@@ -18,7 +18,7 @@ export function app_replace_goals_generic(
   lambda,
 ) {
   const choose_this_next = not(completed) && completed_previous;
-  let text = add_1(index) + ".";
+  let text = add_1_period(index);
   if (completed) {
     let e = emoji_check();
     text += string_pad_left_space(e);
