883a623c10a229fc10f507d0c0d452d26aa50667
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:12:17 -0400

    function_auto_path ../love/public/src/lock_claude.mjs

--- web/public/src/lock_claude.mjs
+++ web/public/src/lock_claude.mjs
@@ -1,6 +1,7 @@
 import { lock_wait } from "../../../love/public/src/lock_wait.mjs";
 import { function_run_prompt } from "../../../love/public/src/function_run_prompt.mjs";
 export async function lock_claude(lambda) {
-  let r = await lock_wait(function_run_prompt.name, lambda);
+  function lambda2() {}
+  let r = await lock_wait(function_run_prompt.name, lambda2);
   return r;
 }
