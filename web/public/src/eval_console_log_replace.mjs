0933809fee3114f2083006db902c0e209e49b54b
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 00:12:05 -0400

    pb eval_console_log_replace

--- web/public/src/eval_console_log_replace.mjs
+++ web/public/src/eval_console_log_replace.mjs
@@ -1,5 +1,5 @@
 import { log } from "../../../love/public/src/log.mjs";
-export function eval_console_log_replace(console_log_replacement, code) {
+export function eval_console_log_replace(code, console_log_replacement) {
   const console_replacement = {
     log: console_log_replacement,
   };
