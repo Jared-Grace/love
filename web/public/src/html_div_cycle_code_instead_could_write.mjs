4029eae2a61f04a0cccbdaf5722996a41dce966e
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:07:42 -0400

    function_auto_path ../love/public/src/html_div_cycle_code_instead_could_write.mjs

--- web/public/src/html_div_cycle_code_instead_could_write.mjs
+++ web/public/src/html_div_cycle_code_instead_could_write.mjs
@@ -1,13 +1,13 @@
 import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
 export function html_div_cycle_code_instead_could_write(
-  c,
-  code2,
-  fn_name_call,
+  parent,
+  code_a,
+  code_b,
 ) {
-  html_div_cycle_code(c, [
+  html_div_cycle_code(parent, [
     "Instead of ",
-    code2,
+    code_a,
     " we could write: ",
-    fn_name_call,
+    code_b,
   ]);
 }
