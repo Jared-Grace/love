6f545f1f4cc6006c2795c6bf092fcef70ed22670
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:16:28 -0400

    function_auto_path ../love/public/src/app_code_operators_arithmetic.mjs

---
+++ public/src/app_code_operators_arithmetic_generic.mjs
@@ -0,0 +1,25 @@
+import { each } from "../../../love/public/src/each.mjs";
+import { not } from "../../../love/public/src/not.mjs";
+import { html_span_text_code_dark } from "../../../love/public/src/html_span_text_code_dark.mjs";
+import { html_span_text_padded_space } from "../../../love/public/src/html_span_text_padded_space.mjs";
+import { ternary } from "../../../love/public/src/ternary.mjs";
+import { text_is } from "../../../love/public/src/text_is.mjs";
+import { list_to_and_list } from "../../../love/public/src/list_to_and_list.mjs";
+import { js_operators } from "../../../love/public/src/js_operators.mjs";
+export function app_code_operators_arithmetic_generic(operator_map, parent) {
+  let operators = js_operators();
+  let concated = list_to_and_list(operators);
+  function lambda(item) {
+    let condition = text_is(item);
+    let result = ternary(
+      condition,
+      html_span_text_padded_space,
+      html_span_text_code_dark,
+    );
+    if (not(condition)) {
+      item = operator_map(item);
+    }
+    result(parent, item);
+  }
+  each(concated, lambda);
+}
