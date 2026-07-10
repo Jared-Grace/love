5d7af825ea9f5d643313ef77050a72bda2781e73
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 06:26:03 -0400

    function_auto_path ../love/public/src/html_display_flex.mjs

--- web/public/src/html_display_flex.mjs
+++ web/public/src/html_display_flex.mjs
@@ -1,6 +1,6 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export function html_display_flex(line) {
-  html_style_assign(line, {
+export function html_display_flex(component) {
+  html_style_assign(component, {
     display: "flex",
   });
 }
