8d16ec2a96db3af1a4520707c00b425aada73593
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 02:54:21 -0400

    function_auto_path ../love/public/src/list_concat_first_pair.mjs

--- web/public/src/list_concat_first_pair.mjs
+++ web/public/src/list_concat_first_pair.mjs
@@ -1,5 +1,7 @@
+import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 export function list_concat_first_pair(item, list) {
   let first = list_first(list2);
+  let combined = text_combine(left, right);
   return list;
 }
