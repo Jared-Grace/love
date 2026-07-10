b7b3820323c2943d8954499ea81ac3f70a542fd4
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:31:20 -0400

    c c digits,count

---
+++ public/src/digits_count.mjs
@@ -0,0 +1,7 @@
+import { digits_text } from "../../../love/public/src/digits_text.mjs";
+import { text_split_empty } from "../../../love/public/src/text_split_empty.mjs";
+export function digits_count() {
+  let d = digits_text();
+  let ds = text_split_empty(d);
+  return ds;
+}
