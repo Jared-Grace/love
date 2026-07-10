e338949a4eb29e6652cb9b168abcb8e4074a12bb
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 23:08:42 -0400

    r d app_code_lesson_operators_generic_batch_get_max,generic

---
+++ public/src/app_code_lesson_operators_batch_get_max.mjs
@@ -0,0 +1,4 @@
+export function app_code_lesson_operators_batch_get_max() {
+  let m = 7;
+  return m;
+}
--- public/src/app_code_lesson_operators_generic_batch_get_max.mjs
+++
@@ -1,4 +0,0 @@
-export function app_code_lesson_operators_generic_batch_get_max() {
-  let m = 7;
-  return m;
-}
