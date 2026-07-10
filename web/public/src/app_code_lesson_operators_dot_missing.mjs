b4aec79afb970ce86ee55431ff0875e18c9cd7af
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:19:57 -0400

    ai

--- web/public/src/app_code_lesson_operators_dot_missing.mjs
+++ web/public/src/app_code_lesson_operators_dot_missing.mjs
@@ -3,8 +3,7 @@ import { app_code_lesson_operators_dot_batch_numbers } from "../../../love/publi
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 import { app_code_lesson_operators_dot_numbers_both_sides_text_common } from "../../../love/public/src/app_code_lesson_operators_dot_numbers_both_sides_text_common.mjs";
 import { app_code_lesson_operators_dot_numbers_example } from "../../../love/public/src/app_code_lesson_operators_dot_numbers_example.mjs";
-import { html_div_cycle_code } from "../../../love/public/src/html_div_cycle_code.mjs";
-import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
+import { app_code_container_light_blue_cycle_code } from "../../../love/public/src/app_code_container_light_blue_cycle_code.mjs";
 import { app_code_lesson_operators_asterisk_generic_invalid } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic_invalid.mjs";
 import { app_code_lesson_operators_asterisk_generic_minus } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic_minus.mjs";
 import { js_code_binary } from "../../../love/public/src/js_code_binary.mjs";
@@ -15,8 +14,7 @@ export function app_code_lesson_operators_dot_missing() {
   let math_name = "property access, missing";
   let batch = app_code_lesson_operators_dot_batch_numbers(operator);
   function above(root) {
-    let c = app_code_container_light_blue(root);
-    html_div_cycle_code(c, [
+    app_code_container_light_blue_cycle_code(root, [
       "For this lesson, ",
       operator,
       " will be used with identifiers, and not as a decimal point in a number",
