52079c1596aef50b8afaf038702948491d5c70ef
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:24:03 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_console_log.mjs

--- public/src/app_code_lesson_functions_console_log.mjs
+++ public/src/app_code_lesson_functions_console_log.mjs
@@ -3,14 +3,14 @@ import { eval_console_log_to_list } from "../../../love/public/src/eval_console_
 import { app_code_batch_question_answer_fns } from "../../../love/public/src/app_code_batch_question_answer_fns.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
-import { digits_count_2_to_3_random } from "../../../love/public/src/digits_count_2_to_3_random.mjs";
 import { list_iterator_refillable } from "../../../love/public/src/list_iterator_refillable.mjs";
 import { app_code_lesson_name_id_function } from "../../../love/public/src/app_code_lesson_name_id_function.mjs";
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
 export function app_code_lesson_functions_console_log() {
   const fn_name = "console.log";
   let name_id = app_code_lesson_name_id_function("function", [fn_name]);
-  let next = list_iterator_refillable(digits_count_2_to_3_random);
+  function lambda() {}
+  let next = list_iterator_refillable(lambda);
   function batch_get() {
     let v = next();
     let code = js_code_call_args(fn_name, [v]);
