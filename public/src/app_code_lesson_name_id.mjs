147772935d6c24b3c83cf5914ba52afd52b6c0df
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:34:27 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id.mjs

--- public/src/app_code_lesson_name_id.mjs
+++ public/src/app_code_lesson_name_id.mjs
@@ -1,24 +1,7 @@
+import { app_code_lesson_name_id_generic } from "../../../love/public/src/app_code_lesson_name_id_generic.mjs";
 import { html_text_set_curried_right } from "../../../love/public/src/html_text_set_curried_right.mjs";
-import { text_first_upper_to } from "../../../love/public/src/text_first_upper_to.mjs";
-import { text_lower_to } from "../../../love/public/src/text_lower_to.mjs";
-import { list_join_underscore } from "../../../love/public/src/list_join_underscore.mjs";
-import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
-import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
-import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
-import { text_combine_middle_space } from "../../../love/public/src/text_combine_middle_space.mjs";
 export function app_code_lesson_name_id(left, rights) {
   let name_get = html_text_set_curried_right;
-  let rights_joined = list_join_comma_space(rights);
-  let rights_wrapped = text_wrap_parenthesis(rights_joined);
-  let combined = text_combine_middle_space(left, rights_wrapped);
-  let lesson_name = text_first_upper_to(combined);
-  let concated = list_concat_single(left, rights);
-  let joined_id = list_join_underscore(concated);
-  let id = text_lower_to(joined_id);
-  let lambda = name_get(lesson_name);
-  let name_id = {
-    name: lambda,
-    id,
-  };
+  let name_id = app_code_lesson_name_id_generic(rights, left, name_get);
   return name_id;
 }
