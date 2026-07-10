859951389db482acd5b465c6772e2c2905ce2339
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 00:48:20 -0400

    function_auto_path ../love/public/src/app_code_lessons_fns.mjs

--- public/src/app_code_lessons_fns.mjs
+++ public/src/app_code_lessons_fns.mjs
@@ -5,7 +5,6 @@ import { app_code_lesson_operators_dot_missing } from "../../../love/public/src/
 import { app_code_lesson_operators_dot_numbers } from "../../../love/public/src/app_code_lesson_operators_dot_numbers.mjs";
 import { app_code_lesson_operators_slash_forward } from "../../../love/public/src/app_code_lesson_operators_slash_forward.mjs";
 import { app_code_lesson_operators_asterisk } from "../../../love/public/src/app_code_lesson_operators_asterisk.mjs";
-import { app_code_lessons_fns_invalid } from "../../../love/public/src/app_code_lessons_fns_invalid.mjs";
 import { app_code_lesson_operators_minus } from "../../../love/public/src/app_code_lesson_operators_minus.mjs";
 import { app_code_lesson_operators_division } from "../../../love/public/src/app_code_lesson_operators_division.mjs";
 import { app_code_lesson_operators_multiplication } from "../../../love/public/src/app_code_lesson_operators_multiplication.mjs";
@@ -21,7 +20,6 @@ import { app_code_lesson_symbols_letters } from "../../../love/public/src/app_co
 import { app_code_lesson_symbols_digits } from "../../../love/public/src/app_code_lesson_symbols_digits.mjs";
 import { app_code_lesson_symbols_digits_numbered } from "../../../love/public/src/app_code_lesson_symbols_digits_numbered.mjs";
 export function app_code_lessons_fns() {
-  let invalid = app_code_lessons_fns_invalid();
   let fns = [
     app_code_lesson_symbols_digits_numbered,
     app_code_lesson_symbols_digits,
