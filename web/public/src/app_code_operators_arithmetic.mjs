ad55fda2992c5dd35ef0450ff80690508f327b03
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:16:28 -0400

    function_auto_path ../love/public/src/app_code_operators_arithmetic.mjs

--- web/public/src/app_code_operators_arithmetic.mjs
+++ web/public/src/app_code_operators_arithmetic.mjs
@@ -1,28 +1,7 @@
-import { list_to_and_list } from "../../../love/public/src/list_to_and_list.mjs";
-import { js_operators } from "../../../love/public/src/js_operators.mjs";
-import { each } from "../../../love/public/src/each.mjs";
+import { app_code_operators_arithmetic_generic } from "../../../love/public/src/app_code_operators_arithmetic_generic.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { not } from "../../../love/public/src/not.mjs";
-import { html_span_text_code_dark } from "../../../love/public/src/html_span_text_code_dark.mjs";
-import { html_span_text_padded_space } from "../../../love/public/src/html_span_text_padded_space.mjs";
-import { ternary } from "../../../love/public/src/ternary.mjs";
-import { text_is } from "../../../love/public/src/text_is.mjs";
 export function app_code_operators_arithmetic(parent) {
-  let operators = js_operators();
-  let concated = list_to_and_list(operators);
-  function lambda(item) {
-    let condition = text_is(item);
-    let result = ternary(
-      condition,
-      html_span_text_padded_space,
-      html_span_text_code_dark,
-    );
-    if (not(condition)) {
-      item = operator_map(item);
-    }
-    result(parent, item);
-  }
-  each(concated, lambda);
+  app_code_operators_arithmetic_generic(operator_map, parent);
   function operator_map(item) {
     item = property_get(item, "operator");
     return item;
