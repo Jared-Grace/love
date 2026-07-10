e12b96c67a8ba40c444478fa41c15858b44d43e6
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:39:55 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_console_log.mjs

--- web/public/src/app_code_lesson_functions_console_log.mjs
+++ web/public/src/app_code_lesson_functions_console_log.mjs
@@ -1,4 +1,5 @@
-import { digit_count_values_shuffled } from "../../../love/public/src/digit_count_values_shuffled.mjs";
+import { property_initialize_lambda } from "../../../love/public/src/property_initialize_lambda.mjs";
+import { digit_count_values_shuffled_next } from "../../../love/public/src/digit_count_values_shuffled_next.mjs";
 import { each_range_1 } from "../../../love/public/src/each_range_1.mjs";
 import { app_code_lesson_code_logged } from "../../../love/public/src/app_code_lesson_code_logged.mjs";
 import { eval_console_log_to_list } from "../../../love/public/src/eval_console_log_to_list.mjs";
@@ -13,8 +14,10 @@ export function app_code_lesson_functions_console_log() {
   const fn_name = "console.log";
   let name_id = app_code_lesson_name_id_function("function", [fn_name]);
   let digit_count = 1;
-  let next = digit_count_values_shuffled_next(digit_count);
+  let next1 = digit_count_values_shuffled_next(digit_count);
   function lambda() {
+    function lambda5() {}
+    let value = property_initialize_lambda(object, property_name, lambda5);
     const digit_count_max = 3;
     let digit_counts = range_1(digit_count_max);
     function lambda2(c) {
@@ -39,11 +42,3 @@ export function app_code_lesson_functions_console_log() {
     let c = app_code_container_light_blue(root);
   }
 }
-function digit_count_values_shuffled_next(digit_count) {
-  function lambda4() {
-    digit_count_values_shuffled(digit_count);
-  }
-  let next = list_iterator_refillable(lambda4);
-  return next;
-}
-
