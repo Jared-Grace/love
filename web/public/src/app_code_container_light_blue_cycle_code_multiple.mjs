940ee030159815e9d1d55bb7fb51f5faeeebc01e
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:14:41 -0400

    function_auto_path ../love/public/src/app_code_container_light_blue_cycle_code_multiple.mjs

--- web/public/src/app_code_container_light_blue_cycle_code_multiple.mjs
+++ web/public/src/app_code_container_light_blue_cycle_code_multiple.mjs
@@ -1,5 +1,8 @@
 import { app_code_container_light_blue } from "../../../love/public/src/app_code_container_light_blue.mjs";
-export function app_code_container_light_blue_cycle_code_multiple(parent) {
+export function app_code_container_light_blue_cycle_code_multiple(
+  parent,
+  list_parts,
+) {
   let c = app_code_container_light_blue(parent);
   return c;
 }
