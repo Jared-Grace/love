b3ee834b5879c257735679f209c9245123569b5c
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:29:38 -0400

    de js_code_call_arg_fn_curried_right

--- public/src/js_code_call_arg_fn_curried_right.mjs
+++
@@ -1,8 +0,0 @@
-import { js_code_call_arg_fn } from "../../../love/public/src/js_code_call_arg_fn.mjs";
-import { fn_name } from "../../../love/public/src/fn_name.mjs";
-export function js_code_call_arg_fn_curried_right(arg_get) {
-  let c = function js_code_call_arg_fn_curried_right_result(fn_name) {
-    return js_code_call_arg_fn(fn_name, arg_get);
-  };
-  return c;
-}
