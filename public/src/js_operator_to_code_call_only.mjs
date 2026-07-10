b93ec1541e487b10811a2ba12add38352c31b384
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:40:56 -0400

    function_auto_path ../love/public/src/js_operator_to_code_call_only.mjs

--- public/src/js_operator_to_code_call_only.mjs
+++ public/src/js_operator_to_code_call_only.mjs
@@ -1,7 +1,7 @@
-import { property_transform } from "../../../love/public/src/property_transform.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { js_operator_to_code_call } from "../../../love/public/src/js_operator_to_code_call.mjs";
 export function js_operator_to_code_call_only(o_f, next) {
   let r = js_operator_to_code_call(o_f, next);
-  let call = property_transform(r, "call", lambda);
+  let call = property_get(r, "call");
   return call;
 }
