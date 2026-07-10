5055d86ad4091497e992947bddf0273f4020b2be
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 04:04:06 -0400

    function_auto_path ../love/public/src/text_adjascent_duplicates_remove.mjs

--- public/src/text_adjascent_duplicates_remove.mjs
+++ public/src/text_adjascent_duplicates_remove.mjs
@@ -4,6 +4,6 @@ import { text_split } from "../../../love/public/src/text_split.mjs";
 export function text_adjascent_duplicates_remove(t, separator) {
   let split = text_split(t, separator);
   let filtered = list_filter_text_empty_not_is(split);
-  let joined = list_join(t, separator);
+  let joined = list_join(filtered, separator);
   return joined;
 }
