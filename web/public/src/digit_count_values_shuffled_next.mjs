ff05d8397d85bb4b1b8b81a561963b691346064f
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:43:47 -0400

    function_auto_path ../love/public/src/digit_count_values_shuffled_next.mjs

--- web/public/src/digit_count_values_shuffled_next.mjs
+++ web/public/src/digit_count_values_shuffled_next.mjs
@@ -2,7 +2,8 @@ import { list_iterator_refillable } from "../../../love/public/src/list_iterator
 import { digit_count_values_shuffled } from "../../../love/public/src/digit_count_values_shuffled.mjs";
 export function digit_count_values_shuffled_next(digit_count) {
   function lambda4() {
-    digit_count_values_shuffled(digit_count);
+    let r = digit_count_values_shuffled(digit_count);
+    return r;
   }
   let next = list_iterator_refillable(lambda4);
   return next;
