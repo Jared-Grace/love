6c0943a7523a6dd8330519ee50a67e32e9eea131
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 04:34:46 -0400

    function_auto_path ../love/public/src/js_tokens_to_code.mjs

--- web/public/src/js_tokens_to_code.mjs
+++ web/public/src/js_tokens_to_code.mjs
@@ -3,8 +3,7 @@ import { js_unparse } from "../../../love/public/src/js_unparse.mjs";
 import { list_join_space } from "../../../love/public/src/list_join_space.mjs";
 export function js_tokens_to_code(tokens) {
   let joined = list_join_space(tokens);
-  let parse = js_parse_choose(joined);
-  let expression = parse(joined);
+  let expression = js_parse_choose(joined);
   let code = js_unparse(expression);
   return code;
 }
