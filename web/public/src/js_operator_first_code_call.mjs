9d81ef143b7654e5adcb33e928195553aa635765
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:13:27 -0400

    function_auto_path ../love/public/src/js_operator_first_code_call.mjs

--- web/public/src/js_operator_first_code_call.mjs
+++ web/public/src/js_operator_first_code_call.mjs
@@ -3,8 +3,8 @@ import { js_operator_to_code_call_only } from "../../../love/public/src/js_opera
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_operators } from "../../../love/public/src/js_operators.mjs";
 export function js_operator_first_code_call(next) {
-  let operators1 = js_operators();
-  let first = list_first(operators1);
+  let operators = js_operators();
+  let first = list_first(operators);
   let code = js_operator_to_code_call_only(first, next);
   property_set_exists_not(first, "code", code);
   return first;
