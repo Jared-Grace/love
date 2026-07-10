e6b59767b91b1c37e2a73f09603832ef036edf1b
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 21:55:47 -0400

    ai

--- web/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
+++ web/public/src/app_code_lesson_identifiers_symbol_first_unseparated.mjs
@@ -16,11 +16,13 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { app_code_container_light_blue_text } from "../../../love/public/src/app_code_container_light_blue_text.mjs";
 import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
 import { app_code_lesson_identifiers_symbol_first_generic } from "../../../love/public/src/app_code_lesson_identifiers_symbol_first_generic.mjs";
+import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 export function app_code_lesson_identifiers_symbol_first_unseparated() {
-  let name_id = {
-    name: "Identifiers (first symbol, unseparated)",
-    id: "identifiers_symbol_first_unseparated",
-  };
+  let name_id = app_code_lesson_name_id("identifiers", [
+    "symbol",
+    "first",
+    "unseparated",
+  ]);
   let r = app_code_lesson_identifiers_symbol_first_generic(
     name_id,
     above,
