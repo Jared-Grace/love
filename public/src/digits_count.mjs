d433f7cf36d4f332f503e11054e29007e86ed1fb
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:31:49 -0400

    function_auto_path ../love/public/src/digits_count.mjs

--- public/src/digits_count.mjs
+++ public/src/digits_count.mjs
@@ -2,6 +2,6 @@ import { list_size } from "../../../love/public/src/list_size.mjs";
 import { digits } from "../../../love/public/src/digits.mjs";
 export function digits_count() {
   let ds = digits();
-  let size = list_size(list);
-  return ds;
+  let size = list_size(ds);
+  return size;
 }
