0fab34b45e60488b409719c900253d8b6407dcf6
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:38:50 -0400

    function_auto_path ../love/public/src/js_operator_to_code_call.mjs

--- web/public/src/js_operator_to_code_call.mjs
+++ web/public/src/js_operator_to_code_call.mjs
@@ -4,6 +4,7 @@ import { js_operator_to_expression } from "../../../love/public/src/js_operator_
 export function js_operator_to_code_call(o, next) {
   let e = js_operator_to_expression(o, next);
   let call = js_operator_left_right_to_code_call(o, e);
-  property_set_if_exists_not(e, "call", call);
+  let replaced = text_replace_space_nb(call);
+  property_set_if_exists_not(e, "call", replaced);
   return e;
 }
