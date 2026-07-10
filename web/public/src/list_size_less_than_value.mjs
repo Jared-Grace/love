c39577a6f75c20f49dd8eeabddb3b11cc99739db
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:16:00 -0400

    function_auto_path ../love/public/src/list_size_less_than_value.mjs

--- web/public/src/list_size_less_than_value.mjs
+++ web/public/src/list_size_less_than_value.mjs
@@ -2,6 +2,6 @@ import { less_than } from "../../../love/public/src/less_than.mjs";
 import { list_size } from "../../../love/public/src/list_size.mjs";
 export function list_size_less_than_value(list, value) {
   list_size(list);
-  let v = less_than(list, value);
-  return v;
+  let lt = less_than(list, value);
+  return lt;
 }
