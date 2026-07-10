ea8407e51492fd1133db068ae7adc42ba8f1ddbb
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:33:54 -0400

    function_auto_path ../love/public/src/subtract_1.mjs

--- web/public/src/subtract_1.mjs
+++ web/public/src/subtract_1.mjs
@@ -1,5 +1,5 @@
 import { subtract } from "../../../love/public/src/subtract.mjs";
-export function subtract_1(objection) {
-  let difference = subtract(objection, 1);
+export function subtract_1(n) {
+  let difference = subtract(n, 1);
   return difference;
 }
