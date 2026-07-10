d5f385afd80e3a2659c695f518a64e678f5bf8aa
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 06:47:47 -0400

    function_auto_path ../love/public/src/app_chapter_open_en.mjs

--- public/src/app_chapter_open_en.mjs
+++ public/src/app_chapter_open_en.mjs
@@ -1,6 +1,6 @@
+import { ebible_language_english } from "../../../love/public/src/ebible_language_english.mjs";
 import { app_chapter_open } from "../../../love/public/src/app_chapter_open.mjs";
-import { ebible_language_en_code } from "../../../love/public/src/ebible_language_en_code.mjs";
 export function app_chapter_open_en(chapter_code, verse_number) {
-  let v = ebible_language_en_code();
+  let v = ebible_language_english();
   app_chapter_open([v], chapter_code, verse_number);
 }
