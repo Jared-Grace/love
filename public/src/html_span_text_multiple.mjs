2b8732b0030bf8c9421fd3feb0df6ce05040fd78
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:48:33 -0400

    function_auto_path ../love/public/src/html_span_text_multiple.mjs

--- public/src/html_span_text_multiple.mjs
+++ public/src/html_span_text_multiple.mjs
@@ -2,7 +2,7 @@ import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
 export function html_span_text_multiple(parent, list) {
   function lambda(d) {
-    let span = html_span_text(parent, d);
+    let span = html_span_text(parent, thisd);
     return span;
   }
   let spans = list_map(list, lambda);
