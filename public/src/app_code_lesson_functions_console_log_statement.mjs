cad0ec5aa3a40c7f340955411142343f972ad590
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:53:08 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_console_log_statement.mjs

--- public/src/app_code_lesson_functions_console_log_statement.mjs
+++ public/src/app_code_lesson_functions_console_log_statement.mjs
@@ -1,9 +1,12 @@
-import { identity } from "../../../love/public/src/identity.mjs";
+import { js_code_statement } from "../../../love/public/src/js_code_statement.mjs";
 import { app_code_lesson_functions_console_log_generic } from "../../../love/public/src/app_code_lesson_functions_console_log_generic.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 export function app_code_lesson_functions_console_log_statement() {
-  var r = app_code_lesson_functions_console_log_generic(above, identity);
+  var r = app_code_lesson_functions_console_log_generic(
+    above,
+    js_code_statement,
+  );
   let next = property_get(r, "next");
   let fn_name = property_get(r, "fn_name");
   let next_operator = property_get(r, "next_operator");
