a66adbb87c108a62bc7096aad23f0100d316702a
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 23:09:13 -0400

    r t app_code_lesson_operators_batch_get_max,batch_get,value

--- public/src/app_code_lesson_functions_invalid.mjs
+++ public/src/app_code_lesson_functions_invalid.mjs
@@ -17,13 +17,13 @@ import { js_operator_to_code_call } from "../../../love/public/src/js_operator_t
 import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 import { app_code_lesson_validity_code } from "../../../love/public/src/app_code_lesson_validity_code.mjs";
 import { range_1_next } from "../../../love/public/src/range_1_next.mjs";
-import { app_code_lesson_operators_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_batch_get_max.mjs";
+import { app_code_lesson_operators_value_max } from "../../../love/public/src/app_code_lesson_operators_value_max.mjs";
 import { js_operators } from "../../../love/public/src/js_operators.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function app_code_lesson_functions_invalid() {
   let name_id = app_code_lesson_name_id("functions", ["invalid"]);
   let operators = js_operators();
-  let m = app_code_lesson_operators_batch_get_max();
+  let m = app_code_lesson_operators_value_max();
   let next = range_1_next(m);
   let symbols_required = ["(", ",", ")"];
   let operators_next = list_iterator_refillable_value(operators);
