038e60cad336740a39a1e334c09295733e94a99b
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:46:05 -0400

    function_auto_path ../love/public/src/digit_count_value_max.mjs

--- web/public/src/digit_count_value_max.mjs
+++ web/public/src/digit_count_value_max.mjs
@@ -1,11 +1,7 @@
 import { subtract_1 } from "../../../love/public/src/subtract_1.mjs";
 import { exponent } from "../../../love/public/src/exponent.mjs";
-import { digits_count } from "../../../love/public/src/digits_count.mjs";
-import { add_1 } from "../../../love/public/src/add_1.mjs";
 export function digit_count_value_max(digit_count) {
-  let a = add_1(digit_count);
-  const base = digits_count();
-  let from_1 = exponent(base, a);
+  let from_1 = exponent(base, digit_count);
   let difference = subtract_1(from_1);
   return difference;
 }
