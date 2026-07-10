e37dc0dc750c694630ffcc5b96f80c6658bc2da7
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 06:27:34 -0400

    function_auto_path ../love/public/src/html_flex_grow_1_multiple.mjs

--- public/src/html_flex_grow_1_multiple.mjs
+++ public/src/html_flex_grow_1_multiple.mjs
@@ -1,5 +1,5 @@
 import { html_flex_grow_1 } from "../../../love/public/src/html_flex_grow_1.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-export function html_flex_grow_1_multiple([l, r]) {
-  each([l, r], html_flex_grow_1);
+export function html_flex_grow_1_multiple(list) {
+  each(list, html_flex_grow_1);
 }
