fd757eba194dd43cc65039d6129cc92be08ba033
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 06:25:13 -0400

    function_auto_path ../love/public/src/app_bible_home_generic.mjs

---
+++ web/public/src/html_flex_grow_1_multiple.mjs
@@ -0,0 +1,5 @@
+import { html_flex_grow_1 } from "../../../love/public/src/html_flex_grow_1.mjs";
+import { each } from "../../../love/public/src/each.mjs";
+export function html_flex_grow_1_multiple([l, r]) {
+  each([l, r], html_flex_grow_1);
+}
