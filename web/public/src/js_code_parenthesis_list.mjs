8fdf395352a935022c7dc6aaa3b5e5edcb3a4888
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 02:42:31 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_invalid.mjs

---
+++ web/public/src/js_code_parenthesis_list.mjs
@@ -0,0 +1,6 @@
+import { js_code_parenthesis_right } from "../../../love/public/src/js_code_parenthesis_right.mjs";
+import { js_code_parenthesis_left } from "../../../love/public/src/js_code_parenthesis_left.mjs";
+export function js_code_parenthesis_list() {
+  let r = [js_code_parenthesis_left(), js_code_parenthesis_right()];
+  return r;
+}
