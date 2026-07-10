172f6092eb443fefbfb7097ed81ad081327c6c1f
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:58:37 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id_function.mjs

--- public/src/app_code_lesson_name_id_function.mjs
+++ public/src/app_code_lesson_name_id_function.mjs
@@ -1,5 +1,3 @@
-import { html_span_text_curried } from "../../../love/public/src/html_span_text_curried.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
 import { html_span_text_multiple } from "../../../love/public/src/html_span_text_multiple.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
@@ -19,9 +17,7 @@ export function app_code_lesson_name_id_function(left, rights) {
       let remaining = property_get(r, "remaining");
       let first = property_get(r, "first");
       html_span_text_code_dark(parent, first);
-      let mapped = list_map(remaining, html_span_text);
-      let c = html_span_text_curried(parent);
-      let spans = html_span_text_multiple(parent, c);
+      let spans = html_span_text_multiple(parent, remaining);
     };
     return r2;
   };
