5de45e44320101d144ed671999e54282b6a6e48e
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:16:52 -0400

    function_auto_path ../love/public/src/lock_claude.mjs

--- web/public/src/lock_claude.mjs
+++ web/public/src/lock_claude.mjs
@@ -2,21 +2,25 @@ import { promise_wrap_unawait } from "../../../love/public/src/promise_wrap_unaw
 import { lock_wait } from "../../../love/public/src/lock_wait.mjs";
 import { function_run_prompt } from "../../../love/public/src/function_run_prompt.mjs";
 export async function lock_claude() {
-  async function on_lock(resolve, reject) {
+  async function on_lock_lambda(resolve, reject) {
     let on_lock_resolve = null;
     on_lock_resolve = resolve;
     let on_lock_reject = null;
     on_lock_reject = reject;
   }
-  let p_on_lock = promise_wrap_unawait(on_lock);
-  async function on_finish(resolve, reject) {
+  let on_lock = promise_wrap_unawait(on_lock_lambda);
+  async function on_finish_lambda(resolve, reject) {
     let on_finish_resolve = null;
     on_finish_resolve = resolve;
     let on_finish_reject = null;
     on_finish_reject = reject;
   }
-  let p_on_finish = promise_wrap_unawait(on_finish);
+  let on_finish = promise_wrap_unawait(on_finish_lambda);
   async function lambda2() {}
   await lock_wait(function_run_prompt.name, lambda2);
+  let r = {
+    on_lock,
+    on_finish,
+  };
   return r;
 }
