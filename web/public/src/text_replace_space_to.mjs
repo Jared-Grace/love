79865461808d6be827b6282263306dffb088cce6
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:30:31 -0400

    function_auto_path ../love/public/src/html_nbsp_replace.mjs

---
+++ web/public/src/text_replace_space_to.mjs
@@ -0,0 +1,5 @@
+import { text_replace } from "../../../love/public/src/text_replace.mjs";
+export function text_replace_space_to(t, nbsp) {
+  let replaced2 = text_replace(t, " ", nbsp);
+  return replaced2;
+}
