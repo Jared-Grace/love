d82f81e07c1047a048792f71165bd4b6e5720cf6
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:45:25 -0400

    r 1 list_first_remaining_get,only

--- public/src/function_curryify_args_get.mjs
+++ public/src/function_curryify_args_get.mjs
@@ -1,7 +1,7 @@
-import { list_first_remaining_get } from "../../../love/public/src/list_first_remaining_get.mjs";
+import { list_first_remaining_only } from "../../../love/public/src/list_first_remaining_only.mjs";
 import { function_curryify_args_get_generic } from "../../../love/public/src/function_curryify_args_get_generic.mjs";
 export function function_curryify_args_get(arg_names) {
-  let fn = list_first_remaining_get;
+  let fn = list_first_remaining_only;
   let r = function_curryify_args_get_generic(fn, arg_names);
   return r;
 }
