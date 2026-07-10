dc383319b7c63823c46ff293f9d1a0768f9c1710
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 02:55:40 -0400

    function_auto_path ../love/public/src/list_concat_first_pair.mjs

--- web/public/src/list_concat_first_pair.mjs
+++ web/public/src/list_concat_first_pair.mjs
@@ -1,9 +1,9 @@
-import { list_transform_at } from "../../../love/public/src/list_transform_at.mjs";
+import { list_transform_first } from "../../../love/public/src/list_transform_first.mjs";
 import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export function list_concat_first_pair(item, list) {
   function lambda2(first) {
     let combined = text_combine(item, first);
   }
-  list_transform_at(list, 0, lambda2);
+  list_transform_first(list, lambda2);
   return list;
 }
---
+++ web/public/src/list_transform_first.mjs
@@ -0,0 +1,4 @@
+import { list_transform_at } from "../../../love/public/src/list_transform_at.mjs";
+export function list_transform_first(list, lambda2) {
+  list_transform_at(list, 0, lambda2);
+}
