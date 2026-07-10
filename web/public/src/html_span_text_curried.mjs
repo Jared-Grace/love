a48e067979381d857e14ce8eccd6a986c5e32b28
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:57:33 -0400

    cu html_span_text

---
+++ web/public/src/html_span_text_curried.mjs
@@ -0,0 +1,7 @@
+import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
+export function html_span_text_curried(parent) {
+  let c = function html_span_text_curried_result(text) {
+    return html_span_text(parent, text);
+  };
+  return c;
+}
