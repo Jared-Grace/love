0f44821deaf7311ea9924b2185ec045d072fb772
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:54:17 -0400

    function_auto_path ../love/public/src/js_operator_first_code_call.mjs

--- web/public/src/js_operator_first_code_call.mjs
+++ web/public/src/js_operator_first_code_call.mjs
@@ -1,17 +1,11 @@
 import { property_set_exists_not } from "../../../love/public/src/property_set_exists_not.mjs";
 import { js_operator_to_code_call_only } from "../../../love/public/src/js_operator_to_code_call_only.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_operators } from "../../../love/public/src/js_operators.mjs";
 export function js_operator_first_code_call(next) {
   let operators1 = js_operators();
-  let o_f = list_first(operators1);
-  let verb = property_get(o_f, "verb");
-  property_set_exists_not(object, property_name, value);
-  let code = js_operator_to_code_call_only(o_f, next);
-  let r2 = {
-    verb,
-    code,
-  };
-  return r2;
+  let first = list_first(operators1);
+  let code = js_operator_to_code_call_only(first, next);
+  property_set_exists_not(first, "code", code);
+  return first;
 }
