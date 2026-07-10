8b5d0a65ac8f9eac2826fab6f8fa846a080893f3
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 00:49:35 -0400

    function_auto_path ../love/public/src/functions_expand_all_delete.mjs

--- web/public/src/functions_expand_all_delete.mjs
+++ web/public/src/functions_expand_all_delete.mjs
@@ -2,6 +2,6 @@ import { function_delete } from "../../../love/public/src/function_delete.mjs";
 import { functions_expand_all } from "../../../love/public/src/functions_expand_all.mjs";
 export async function functions_expand_all_delete(f_name_expand) {
   let r = await functions_expand_all(f_name_expand);
-  let result = await function_delete(f_names_comma);
+  let result = await function_delete(f_name_expand);
   return r;
 }
