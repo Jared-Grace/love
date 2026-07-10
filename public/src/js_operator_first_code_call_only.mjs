38e4308cab5dcc10db728930adb3a6f566d524b9
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 03:25:00 -0400

    w c js_operator_first_code_call,only

---
+++ public/src/js_operator_first_code_call_only.mjs
@@ -0,0 +1,5 @@
+import { js_operator_first_code_call } from "../../../love/public/src/js_operator_first_code_call.mjs";
+export function js_operator_first_code_call_only(next) {
+  let first = js_operator_first_code_call(next);
+  return first;
+}
