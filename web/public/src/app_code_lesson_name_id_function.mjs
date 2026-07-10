44378b23b33239d666a7bb4abee2df8d74e5b5c8
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:43:32 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id_function.mjs

--- web/public/src/app_code_lesson_name_id_function.mjs
+++ web/public/src/app_code_lesson_name_id_function.mjs
@@ -5,8 +5,8 @@ import { app_code_lesson_name_id_generic } from "../../../love/public/src/app_co
 export function app_code_lesson_name_id_function(left, rights) {
   let name_get = function lambda(parent) {
     let first = list_first(rights);
-    let combined = text_combine_space_right(plural);
-    let span = html_span_text(parent2, text);
+    let combined = text_combine_space_right(left);
+    let span = html_span_text(parent, combined);
   };
   let name_id = app_code_lesson_name_id_generic(rights, left, name_get);
   return name_id;
