b5a0af9663ebfc21fb76568feaddc0ef77876623
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 21:55:47 -0400

    ai

--- public/src/app_code_lesson_symbols_digits.mjs
+++ public/src/app_code_lesson_symbols_digits.mjs
@@ -3,13 +3,15 @@ import { app_code_container_light_blue_text } from "../../../love/public/src/app
 import { app_code_lesson_same_message } from "../../../love/public/src/app_code_lesson_same_message.mjs";
 import { app_code_lesson_symbols_batch_digits } from "../../../love/public/src/app_code_lesson_symbols_batch_digits.mjs";
 import { app_code_lesson_symbols_counting } from "../../../love/public/src/app_code_lesson_symbols_counting.mjs";
+import { app_code_lesson_name_id } from "../../../love/public/src/app_code_lesson_name_id.mjs";
 export function app_code_lesson_symbols_digits() {
   function lambda(root) {
     let text = app_code_lesson_same_message("there are not numbers underneath");
     app_code_container_light_blue_text(root, text);
   }
+  let name_id = app_code_lesson_name_id("symbols", ["digits"]);
   let r = app_code_lesson_symbols_counting(
-    { name: "Symbols (digits)", id: "symbols_digits" },
+    name_id,
     lambda,
     app_code_lesson_symbols_batch_digits,
     app_code_symbols_separated_on_question,
