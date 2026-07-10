00a14e7e02f6491e661eaceb8cbb9a55c6b3b311
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 02:00:40 -0400

    function_auto_path ../love/public/src/js_code_call_args.mjs

--- public/src/js_code_call_args.mjs
+++ public/src/js_code_call_args.mjs
@@ -1,3 +1,4 @@
+import { js_code_join_comma_space } from "../../../love/public/src/js_code_join_comma_space.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { fn_name } from "../../../love/public/src/fn_name.mjs";
 import { js_code_wrap_parenthesis } from "../../../love/public/src/js_code_wrap_parenthesis.mjs";
@@ -6,6 +7,8 @@ import { text_combine } from "../../../love/public/src/text_combine.mjs";
 export function js_code_call_args(fn_name, args) {
   arguments_assert(arguments, 2);
   let joined = list_join_comma_space(args);
-  let code = text_combine(fn_name, js_code_wrap_parenthesis(joined));
+  let right = js_code_wrap_parenthesis(joined);
+  let code = text_combine(fn_name, right);
   return code;
+  let v = js_code_join_comma_space(args2);
 }
