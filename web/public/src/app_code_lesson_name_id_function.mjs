44b1501e13784b6fde1656c4ad4e16037cd03ff4
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:46:34 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id_function.mjs

--- web/public/src/app_code_lesson_name_id_function.mjs
+++ web/public/src/app_code_lesson_name_id_function.mjs
@@ -1,3 +1,4 @@
+import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { html_span_text_code_dark } from "../../../love/public/src/html_span_text_code_dark.mjs";
 import { text_combine_space_right } from "../../../love/public/src/text_combine_space_right.mjs";
@@ -7,7 +8,9 @@ export function app_code_lesson_name_id_function(left, rights) {
   let name_get = function lambda(parent) {
     let combined = text_combine_space_right(left);
     let span = html_span_text(parent, combined);
-    let first = list_first_remaining(rights);
+    let r = list_first_remaining(rights);
+    let remaining = property_get(r, "remaining");
+    let first = property_get(r, "first");
     html_span_text_code_dark(parent, first);
   };
   let name_id = app_code_lesson_name_id_generic(rights, left, name_get);
