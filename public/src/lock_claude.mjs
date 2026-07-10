4230bca929fe7717bf67267e0266fb75dc58df36
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:14:45 -0400

    function_auto_path ../love/public/src/lock_claude.mjs

--- public/src/lock_claude.mjs
+++ public/src/lock_claude.mjs
@@ -6,6 +6,10 @@ export async function lock_claude() {
     let on_lock_resolve = resolve;
     let on_lock_reject = reject;
   }
+  async function on_finish(resolve, reject) {
+    let on_finish_resolve = resolve;
+    let on_finish_reject = reject;
+  }
   let p = await promise_wrap(on_lock);
   async function lambda2() {}
   await lock_wait(function_run_prompt.name, lambda2);
