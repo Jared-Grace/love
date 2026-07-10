fa468bf35ee1069004552fd1ccb95c765fda27ba
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:53:31 -0400

    r i js_operator_first_call,call,code

--- public/src/js_operator_first_code_call.mjs
+++ public/src/js_operator_first_code_call.mjs
@@ -2,7 +2,7 @@ import { js_operator_to_code_call_only } from "../../../love/public/src/js_opera
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { js_operators } from "../../../love/public/src/js_operators.mjs";
-export function js_operator_first_call(next) {
+export function js_operator_first_code_call(next) {
   let operators1 = js_operators();
   let o_f = list_first(operators1);
   let verb = property_get(o_f, "verb");
