882e06f0d6caef830fcdc5c8a30d0d1b7355397f
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 23:09:13 -0400

    r t app_code_lesson_operators_batch_get_max,batch_get,value

--- web/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs
+++ web/public/src/app_code_lesson_operators_generic_batch_get_binary.mjs
@@ -1,14 +1,14 @@
 import { text_left_right_middle_random_space_nb } from "../../../love/public/src/text_left_right_middle_random_space_nb.mjs";
 import { list_shuffle_cycled } from "../../../love/public/src/list_shuffle_cycled.mjs";
 import { each } from "../../../love/public/src/each.mjs";
-import { app_code_lesson_operators_batch_get_max } from "../../../love/public/src/app_code_lesson_operators_batch_get_max.mjs";
+import { app_code_lesson_operators_value_max } from "../../../love/public/src/app_code_lesson_operators_value_max.mjs";
 import { each_nested_args_range_1_list_adder } from "../../../love/public/src/each_nested_args_range_1_list_adder.mjs";
 import { js_code_binary_spaced_nb } from "../../../love/public/src/js_code_binary_spaced_nb.mjs";
 export function app_code_lesson_operators_generic_batch_get_binary(
   operator_js,
   left_transform,
 ) {
-  let max = app_code_lesson_operators_batch_get_max();
+  let max = app_code_lesson_operators_value_max();
   let r = function batch_get() {
     let list = each_nested_args_range_1_list_adder(max, lambda$left$right$la);
     function lambda$left$right$la(left, right, la) {
---
+++ web/public/src/app_code_lesson_operators_value_max.mjs
@@ -0,0 +1,4 @@
+export function app_code_lesson_operators_value_max() {
+  let m = 7;
+  return m;
+}
