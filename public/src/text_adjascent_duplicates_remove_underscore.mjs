52ca9298c888282d73ab3cbcfbe1442d5d41d555
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 04:04:59 -0400

    function_auto_path ../love/public/src/text_adjascent_duplicates_remove_underscore.mjs

--- public/src/text_adjascent_duplicates_remove_underscore.mjs
+++ public/src/text_adjascent_duplicates_remove_underscore.mjs
@@ -1,4 +1,5 @@
 import { text_adjascent_duplicates_remove } from "../../../love/public/src/text_adjascent_duplicates_remove.mjs";
 export function text_adjascent_duplicates_remove_underscore(lesson_unique_id) {
-  text_adjascent_duplicates_remove(lesson_unique_id, "_");
+  let joined = text_adjascent_duplicates_remove(lesson_unique_id, "_");
+  return joined;
 }
