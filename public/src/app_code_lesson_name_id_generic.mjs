cc5290054dcd5b28b26f1e287f7bb4e84661021e
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:53:25 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id_generic.mjs

--- public/src/app_code_lesson_name_id_generic.mjs
+++ public/src/app_code_lesson_name_id_generic.mjs
@@ -6,14 +6,14 @@ import { text_combine_middle_space } from "../../../love/public/src/text_combine
 import { text_wrap_parenthesis } from "../../../love/public/src/text_wrap_parenthesis.mjs";
 import { list_join_comma_space } from "../../../love/public/src/list_join_comma_space.mjs";
 export function app_code_lesson_name_id_generic(rights, left, name_get) {
+  left = text_first_upper_to(left);
   let rights_joined = list_join_comma_space(rights);
   let rights_wrapped = text_wrap_parenthesis(rights_joined);
   let combined = text_combine_middle_space(left, rights_wrapped);
-  let lesson_name = text_first_upper_to(combined);
   let concated = list_concat_single(left, rights);
   let joined_id = list_join_underscore(concated);
   let id = text_lower_to(joined_id);
-  let lambda = name_get(lesson_name);
+  let lambda = name_get(lesson_name, left);
   let name_id = {
     name: lambda,
     id,
