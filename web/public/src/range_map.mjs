bf94822fd968589e5ed00080e5c31540566a8012
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 01:21:06 -0400

    function_auto_path ../love/public/src/range_map.mjs

--- web/public/src/range_map.mjs
+++ web/public/src/range_map.mjs
@@ -1,7 +1,7 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { range } from "../../../love/public/src/range.mjs";
-export function range_map(digit_count, lambda$item) {
-  let n_indices = range(digit_count);
+export function range_map(count, lambda$item) {
+  let n_indices = range(count);
   let mapped = list_map(n_indices, lambda$item);
   return mapped;
 }
