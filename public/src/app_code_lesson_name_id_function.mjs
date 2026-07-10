ad8f7523f315406ea965d25c128703b111d58748
JESUS rose to life <username@users.noreply.github.com>
2026-07-09 22:45:42 -0400

    function_auto_path ../love/public/src/app_code_lesson_name_id_function.mjs

--- public/src/app_code_lesson_name_id_function.mjs
+++ public/src/app_code_lesson_name_id_function.mjs
@@ -1,13 +1,13 @@
+import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { html_span_text_code_dark } from "../../../love/public/src/html_span_text_code_dark.mjs";
 import { text_combine_space_right } from "../../../love/public/src/text_combine_space_right.mjs";
 import { html_span_text } from "../../../love/public/src/html_span_text.mjs";
-import { list_first } from "../../../love/public/src/list_first.mjs";
 import { app_code_lesson_name_id_generic } from "../../../love/public/src/app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_name_id_function(left, rights) {
   let name_get = function lambda(parent) {
     let combined = text_combine_space_right(left);
     let span = html_span_text(parent, combined);
-    let first = list_first(rights);
+    let first = list_first_remaining(rights);
     html_span_text_code_dark(parent, first);
   };
   let name_id = app_code_lesson_name_id_generic(rights, left, name_get);
