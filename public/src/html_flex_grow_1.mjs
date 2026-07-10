832a220898074d56226bcecc87c0a08228b7fabd
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 06:27:57 -0400

    function_auto_path ../love/public/src/html_flex_grow_1.mjs

--- public/src/html_flex_grow_1.mjs
+++ public/src/html_flex_grow_1.mjs
@@ -1,6 +1,6 @@
 import { html_style_assign } from "../../../love/public/src/html_style_assign.mjs";
-export function html_flex_grow_1(r) {
-  html_style_assign(r, {
+export function html_flex_grow_1(component) {
+  html_style_assign(component, {
     "flex-grow": "1",
   });
 }
