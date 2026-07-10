0b8fd6d8b7b4f00d215ffa0d2c2bf37d694cfc71
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:38:02 -0400

    function_auto_path ../love/public/src/js_operator_to_code_call_only.mjs

--- web/public/src/text_replace_space_to.mjs
+++ web/public/src/text_replace_space_to.mjs
@@ -1,5 +1,5 @@
 import { text_replace } from "../../../love/public/src/text_replace.mjs";
 export function text_replace_space_to(t, to) {
-  let replaced2 = text_replace(t, " ", to);
-  return replaced2;
+  let replaced = text_replace(t, " ", to);
+  return replaced;
 }
