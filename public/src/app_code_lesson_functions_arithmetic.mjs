2594b97195eea3201d662b39af80d81139d825f4
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 05:44:12 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_arithmetic.mjs

--- public/src/app_code_lesson_functions_arithmetic.mjs
+++ public/src/app_code_lesson_functions_arithmetic.mjs
@@ -80,9 +80,9 @@ export function app_code_lesson_functions_arithmetic() {
     each_index(operators, lambda);
     let c = app_code_container_light_blue(root);
     let o_f = js_operator_first_code_call(next);
-    let code = property_get(o_f, "code");
+    let call = property_get(o_f, "call");
     let verb = property_get(o_f, "verb");
     app_code_code_example_name(c, verb, "function");
-    html_div_cycle_code_bold(c, ["", code, " is called a ", "function call"]);
+    html_div_cycle_code_bold(c, ["", call, " is called a ", "function call"]);
   }
 }
