06428b82054dc8a1aa83b9ef57a167ffb26268f1
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:52:37 -0400

    function_auto_path ../love/public/src/js_declaration_declarators_get.mjs

--- public/src/js_declaration_declarators_get.mjs
+++ public/src/js_declaration_declarators_get.mjs
@@ -1,5 +1,6 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 export function js_declaration_declarators_get(node) {
+  "yes, this is a declaration that has a declarations property containing declarators";
   let value = property_get(node, "declarations");
   return value;
 }
