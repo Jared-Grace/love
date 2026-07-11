591d40f0247ae9adb4a24e35f1a0329cb5a867f0
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:27:46 -0400

    functions_rename_swap function_current_selects_nodes_ast function_current_selects_nodes

--- public/src/function_current_selects_apply_generic.mjs
+++ public/src/function_current_selects_apply_generic.mjs
@@ -1,5 +1,5 @@
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
-import { function_current_selects_nodes } from "../../../love/public/src/function_current_selects_nodes.mjs";
+import { function_current_selects_nodes_ast } from "../../../love/public/src/function_current_selects_nodes_ast.mjs";
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { function_current_get } from "../../../love/public/src/function_current_get.mjs";
 export async function function_current_selects_apply_generic(
@@ -9,7 +9,7 @@ export async function function_current_selects_apply_generic(
   let f_name_current = await function_current_get();
   let imported_fn = await function_import(apply_fn_name);
   async function lambda(ast) {
-    let selects = await function_current_selects_nodes(ast);
+    let selects = await function_current_selects_nodes_ast(ast);
     await imported_fn(ast, selects, args);
   }
   let output = await function_transform(f_name_current, lambda);
