31c47e9bfe45adc7e3f6ad17894a10dec8403c1c
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:22:25 -0400

    function_auto_path ../love/public/src/functions_rename_swap.mjs

--- public/src/functions_rename_swap.mjs
+++ public/src/functions_rename_swap.mjs
@@ -2,6 +2,5 @@ import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs"
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function functions_rename_swap(f_name_a, f_name_b) {
   arguments_assert(arguments, 2);
-  let f_name_before = await function_rename(f_name_a, f_name_b);
-  return r;
+  await function_rename(f_name_a, f_name_b);
 }
