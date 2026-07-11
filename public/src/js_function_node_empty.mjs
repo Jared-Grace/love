b0c384f09a28719827ab685b742be800fc1c111b
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:33:28 -0400

    function_auto_path ../love/public/src/js_function_node_empty.mjs

--- public/src/js_function_node_empty.mjs
+++ public/src/js_function_node_empty.mjs
@@ -1,16 +1,8 @@
+import { list_empty } from "../../../love/public/src/list_empty.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
-import { list_insert_at_multiple } from "../../../love/public/src/list_insert_at_multiple.mjs";
-import { property_get } from "../../../love/public/src/property_get.mjs";
-import { list_remove } from "../../../love/public/src/list_remove.mjs";
-import { js_block_find_from_node } from "../../../love/public/src/js_block_find_from_node.mjs";
 import { js_function_declaration_to_block_body } from "../../../love/public/src/js_function_declaration_to_block_body.mjs";
 export function js_function_node_empty(ast, function_nodes) {
   let function_node = list_single(function_nodes);
   let body_block = js_function_declaration_to_block_body(function_node);
-  let r = js_block_find_from_node(ast, function_node);
-  let item = property_get(r, "item");
-  let index = property_get(r, "index");
-  let body = property_get(r, "body");
-  list_remove(body, item);
-  list_insert_at_multiple(body, index, body_block);
+  list_empty(body_block);
 }
