9212ad8a6fc0a048cf9b86cc4ba179a6b8da6b63
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 04:08:15 -0400

    ai

---
+++ public/src/js_triple_equal_to_equal.mjs
@@ -0,0 +1,34 @@
+import { js_call_arguments_get } from "../../../love/public/src/js_call_arguments_get.mjs";
+import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
+import { object_replace } from "../../../love/public/src/object_replace.mjs";
+import { object_copy } from "../../../love/public/src/object_copy.mjs";
+import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
+import { js_code_call } from "../../../love/public/src/js_code_call.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
+export function js_triple_equal_to_equal(ast) {
+  let name = js_flo_name(ast);
+  if (equal(name, equal.name)) {
+    return;
+  }
+  function lambda(v) {
+    let node = property_get(v, "node");
+    let operator = property_get(node, "operator");
+    if (equal(operator, "===")) {
+      let left = property_get(node, "left");
+      let right = property_get(node, "right");
+      let left_copy = object_copy(left);
+      let right_copy = object_copy(right);
+      let code = js_code_call(equal.name);
+      let expression = js_parse_expression(code);
+      let arguments2 = js_call_arguments_get(expression);
+      list_add(arguments2, left_copy);
+      list_add(arguments2, right_copy);
+      object_replace(node, expression);
+    }
+  }
+  js_visit_type(ast, "BinaryExpression", lambda);
+  return;
+}
