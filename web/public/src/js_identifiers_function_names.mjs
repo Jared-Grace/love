ae3648cee4faa4a04726f8f28eb57c8b6b646bd4
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:35:19 -0400

    ai

--- web/public/src/js_identifiers_function_names.mjs
+++ web/public/src/js_identifiers_function_names.mjs
@@ -1,10 +1,9 @@
 import { js_visit_function_nodes_names } from "../../../love/public/src/js_visit_function_nodes_names.mjs";
 import { js_visit_import_specifiers_names } from "../../../love/public/src/js_visit_import_specifiers_names.mjs";
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
-import { list_unique } from "../../../love/public/src/list_unique.mjs";
+import { list_concat_unique } from "../../../love/public/src/list_concat_unique.mjs";
 export function js_identifiers_function_names(ast) {
   const declaration_names = js_visit_function_nodes_names(ast);
   const import_names = js_visit_import_specifiers_names(ast);
-  const names = list_unique(list_concat(declaration_names, import_names));
+  const names = list_concat_unique(declaration_names, import_names);
   return names;
 }
