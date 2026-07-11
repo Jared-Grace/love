6336789349f03cd4596f566ab3489ddc0d8a704f
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:39:29 -0400

    function_auto_path ../love/public/src/function_transform_multiple_logs_remove.mjs

--- web/public/src/function_transform_multiple_logs_remove.mjs
+++ web/public/src/function_transform_multiple_logs_remove.mjs
@@ -1,6 +1,6 @@
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { js_log_remove } from "../../../love/public/src/js_log_remove.mjs";
 export async function function_transform_multiple_logs_remove(f_names_comma) {
-  let r = await function_transform(js_log_remove, f_names_comma);
+  let r = await function_transform(f_names_comma, js_log_remove);
   return r;
 }
