3e0b2e658b80981f8d2088108f150974f43107cf
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 05:58:54 -0400

    ai

--- web/public/src/js_identifiers_function_names.mjs
+++ web/public/src/js_identifiers_function_names.mjs
@@ -1,26 +1,28 @@
-import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { js_visit_types } from "../../../love/public/src/js_visit_types.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 import { js_identifier_is } from "../../../love/public/src/js_identifier_is.mjs";
+import { equal } from "../../../love/public/src/equal.mjs";
 export function js_identifiers_function_names(ast) {
   function lambda2(la) {
-    function lambda_declaration(v) {
+    function lambda(v) {
       let node = property_get(v, "node");
-      let id = property_get(node, "id");
-      let is_identifier = js_identifier_is(id);
-      if (is_identifier) {
-        let name = property_get(id, "name");
+      let type = property_get(node, "type");
+      let is_declaration = equal(type, "FunctionDeclaration");
+      if (is_declaration) {
+        let id = property_get(node, "id");
+        let is_identifier = js_identifier_is(id);
+        if (is_identifier) {
+          let name = property_get(id, "name");
+          la(name);
+        }
+      } else {
+        let local = property_get(node, "local");
+        let name = property_get(local, "name");
         la(name);
       }
     }
-    js_visit_type(ast, "FunctionDeclaration", lambda_declaration);
-    function lambda_import(v) {
-      let node = property_get(v, "node");
-      let local = property_get(node, "local");
-      let name = property_get(local, "name");
-      la(name);
-    }
-    js_visit_type(ast, "ImportSpecifier", lambda_import);
+    js_visit_types(ast, ["FunctionDeclaration", "ImportSpecifier"], lambda);
   }
   const names = list_adder_unique(lambda2);
   return names;
