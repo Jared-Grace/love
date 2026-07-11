040100dff56822672d030cb08e928e9721f35146
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:50:11 -0400

    r r js_declare_declarations_get,js_declaration_declarators_get

--- web/public/src/js_declare_single_is_if_async.mjs
+++ web/public/src/js_declare_single_is_if_async.mjs
@@ -1,9 +1,9 @@
 import { js_declare_is_if_async } from "../../../love/public/src/js_declare_is_if_async.mjs";
 import { list_single_if_async } from "../../../love/public/src/list_single_if_async.mjs";
-import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
+import { js_declaration_declarators_get } from "../../../love/public/src/js_declaration_declarators_get.mjs";
 export async function js_declare_single_is_if_async(previous, lambda) {
   async function lambda3() {
-    let declarations = js_declare_declarations_get(previous);
+    let declarations = js_declaration_declarators_get(previous);
     await list_single_if_async(declarations, lambda);
   }
   await js_declare_is_if_async(previous, lambda3);
