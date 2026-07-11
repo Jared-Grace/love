a47fe76633e73496162b51a2b04fb31c19c7df36
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:25:46 -0400

    function_auto_path ../love/public/src/js_function_node_call.mjs

--- public/src/js_function_node_call.mjs
+++ public/src/js_function_node_call.mjs
@@ -6,7 +6,7 @@ export async function js_function_node_call(ast, function_nodes, f_name) {
   let function_node = list_single(function_nodes);
   let body_block = js_function_declaration_to_block_body(function_node);
   async function lambda(ast3, statement) {
-    list_add(list, item);
+    list_add(body_block, statement);
   }
   await js_call_add_generic(ast, f_name, lambda);
 }
