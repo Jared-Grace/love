3f3021a09942e7a5790a181d5bac10fcb4271754
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 10:18:28 -0400

    ai

--- web/public/src/window_open_bible_word.mjs
+++ web/public/src/window_open_bible_word.mjs
@@ -1,9 +1,9 @@
-import { text_letters_only } from "../../../love/public/src/text_letters_only.mjs";
+import { text_word_only } from "../../../love/public/src/text_word_only.mjs";
 import { text_combine_multiple } from "../../../love/public/src/text_combine_multiple.mjs";
 import { window_open_google_search } from "../../../love/public/src/window_open_google_search.mjs";
 export function window_open_bible_word(item) {
-  let letters_only = text_letters_only(item);
+  let word_only = text_word_only(item);
   window_open_google_search(
-    text_combine_multiple([letters_only, "+meaning+bible"]),
+    text_combine_multiple([word_only, "+meaning+bible"]),
   );
 }
