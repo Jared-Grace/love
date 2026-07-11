49d296c25d60c553ee8ec01b936e0dd0832ef683
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 05:34:32 -0400

    ai

--- web/public/src/js_operators_arithmetic.mjs
+++ web/public/src/js_operators_arithmetic.mjs
@@ -1,36 +1,7 @@
-import { js_operator_asterisk_verb } from "../../../love/public/src/js_operator_asterisk_verb.mjs";
-import { js_operator_asterisk_symbol } from "../../../love/public/src/js_operator_asterisk_symbol.mjs";
-import { js_operator_division_verb } from "../../../love/public/src/js_operator_division_verb.mjs";
-import { js_operator_division_symbol } from "../../../love/public/src/js_operator_division_symbol.mjs";
-import { js_operator_minus_verb } from "../../../love/public/src/js_operator_minus_verb.mjs";
-import { js_operator_minus_symbol } from "../../../love/public/src/js_operator_minus_symbol.mjs";
-import { js_operator_plus_verb } from "../../../love/public/src/js_operator_plus_verb.mjs";
-import { js_operator_plus_symbol } from "../../../love/public/src/js_operator_plus_symbol.mjs";
-import { identity } from "../../../love/public/src/identity.mjs";
-import { add } from "../../../love/public/src/add.mjs";
-import { multiply } from "../../../love/public/src/multiply.mjs";
+import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
+import { js_operators_arithmetic_fns } from "../../../love/public/src/js_operators_arithmetic_fns.mjs";
 export function js_operators_arithmetic() {
-  let os = [
-    {
-      operator: js_operator_plus_symbol(),
-      verb: js_operator_plus_verb(),
-      left_transform: identity,
-    },
-    {
-      operator: js_operator_minus_symbol(),
-      verb: js_operator_minus_verb(),
-      left_transform: add,
-    },
-    {
-      operator: js_operator_asterisk_symbol(),
-      verb: js_operator_asterisk_verb(),
-      left_transform: identity,
-    },
-    {
-      operator: js_operator_division_symbol(),
-      verb: js_operator_division_verb(),
-      left_transform: multiply,
-    },
-  ];
+  let fns = js_operators_arithmetic_fns();
+  let os = invoke_multiple(fns);
   return os;
 }
