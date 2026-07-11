e00956aac80a64c8ffd58f9d4c9a66123222f023
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:48:52 -0400

    function_auto_path ../love/public/src/function_curryify_specify_name_get.mjs

--- public/src/function_curryify_specify_name_get.mjs
+++ public/src/function_curryify_specify_name_get.mjs
@@ -1,6 +1,5 @@
+import { function_params_get } from "../../../love/public/src/function_params_get.mjs";
 import { list_concat_single } from "../../../love/public/src/list_concat_single.mjs";
-import { js_flo_params_get } from "../../../love/public/src/js_flo_params_get.mjs";
-import { function_ast } from "../../../love/public/src/function_ast.mjs";
 import { lists_sizes_equal } from "../../../love/public/src/lists_sizes_equal.mjs";
 import { function_name_combine_multiple_concat } from "../../../love/public/src/function_name_combine_multiple_concat.mjs";
 import { function_curryify_specify_name } from "../../../love/public/src/function_curryify_specify_name.mjs";
@@ -8,8 +7,7 @@ export async function function_curryify_specify_name_get(
   unaliased,
   positions_1,
 ) {
-  let ast = await function_ast(unaliased);
-  let params = js_flo_params_get(ast);
+  let params = await function_params_get(unaliased);
   let ending = null;
   let e = lists_sizes_equal([params, positions_1]);
   if (e) {
