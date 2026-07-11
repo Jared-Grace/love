f944a9683915fb72e4c5ad4dac1ef6ec6ea143d0
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:21:56 -0400

    ai

--- web/public/src/js_expression_functionize.mjs
+++ web/public/src/js_expression_functionize.mjs
@@ -1,22 +1,17 @@
 import { js_code_call_args_await_maybe_parse_expression } from "../../../love/public/src/js_code_call_args_await_maybe_parse_expression.mjs";
-import { list_first_remaining } from "../../../love/public/src/list_first_remaining.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { object_replace } from "../../../love/public/src/object_replace.mjs";
-import { list_add_first } from "../../../love/public/src/list_add_first.mjs";
 import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { function_name_new_get_args_list } from "../../../love/public/src/function_name_new_get_args_list.mjs";
 import { function_new_expression } from "../../../love/public/src/function_new_expression.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
-export async function js_expression_functionize(ast, selects, ...args) {
+export async function js_expression_functionize(ast, selects, plugin_fn) {
   let name = js_flo_name(ast);
   log(js_expression_functionize.name, {
-    args,
+    plugin_fn,
   });
-  let fr = list_first_remaining(args);
-  let list = property_get(fr, "remaining");
-  let plugin_fn = property_get(fr, "first");
-  list_add_first(list, name);
+  let list = [name];
   let r = await function_name_new_get_args_list(plugin_fn, list);
   let f_name_new = property_get(r, "f_name_new");
   let node = list_single(selects);
