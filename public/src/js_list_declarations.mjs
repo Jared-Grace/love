4bb538ad867ac833ccd8cd123bf87c19bc3e9665
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:43:22 -0400

    function_auto_path ../love/public/src/js_visit_declarations.mjs

--- public/src/js_list_declarations.mjs
+++ public/src/js_list_declarations.mjs
@@ -1,5 +1,5 @@
 import { js_visit_declarations } from "../../../love/public/src/js_visit_declarations.mjs";
-export function js_list_declarations(ast, lambda) {
-  let r = js_visit_declarations(ast, lambda);
+export function js_list_declarations(ast, lambda$v) {
+  let r = js_visit_declarations(ast, lambda$v);
   return r;
 }
