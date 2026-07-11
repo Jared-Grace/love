9cf8fb3a411ce0ad3c0fd18fb40294fa5af7499a
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:27:18 -0400

    function_auto_path ../love/public/src/sandbox.mjs

--- public/src/sandbox.mjs
+++ public/src/sandbox.mjs
@@ -53,7 +53,7 @@ export async function sandbox() {
     );
     await function_current_selects_nodes_log();
     let r2 = js_operators();
-    await function_current_selects_apply(js_function_node_call.name);
+    await function_current_selects_apply_args(js_function_node_call.name, r2);
   }
   return;
   ("below is functionality that has been used in the past: ");
