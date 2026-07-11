99fc50c90089f2bc74fa9c7ec45877af8f50c97e
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:25:49 -0400

    function_auto_path ../love/public/src/functions_rename_swap.mjs

--- public/src/functions_rename_swap.mjs
+++ public/src/functions_rename_swap.mjs
@@ -1,6 +1,8 @@
+import { text_unique_underscore } from "../../../love/public/src/text_unique_underscore.mjs";
 import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { function_rename } from "../../../love/public/src/function_rename.mjs";
 export async function functions_rename_swap(f_name_a, f_name_b) {
   arguments_assert(arguments, 2);
+  let unique = text_unique_underscore(used, name);
   await function_rename(f_name_a, f_name_b);
 }
