f8ba9f8deebb3b7f2624ddcf0da305a3e7015030
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:38:18 -0400

    ai

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -2,13 +2,15 @@ import { property_get } from "../../../love/public/src/property_get.mjs";
 import { property_set } from "../../../love/public/src/property_set.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
 import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { js_keyword_const } from "../../../love/public/src/js_keyword_const.mjs";
+import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
 export function js_const_to_let(ast) {
   function lambda(v) {
     let node = property_get(v, "node");
     let kind = property_get(node, "kind");
-    let is_const = equal(kind, "const");
+    let is_const = equal(kind, js_keyword_const());
     if (is_const) {
-      property_set(node, "kind", "let");
+      property_set(node, "kind", js_keyword_let());
     }
   }
   js_visit_type(ast, "VariableDeclaration", lambda);
