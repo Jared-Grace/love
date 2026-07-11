496b93bba92fb5ecec87187a71f933876dd333fb
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 02:08:35 -0400

    ai

---
+++ public/src/js_identifier_name_number_suffix_base.mjs
@@ -0,0 +1,8 @@
+import { text_regex_match } from "../../../love/public/src/text_regex_match.mjs";
+export function js_identifier_name_number_suffix_base(name) {
+  let m = text_regex_match(name, /^(.*[^0-9])([1-9][0-9]*)$/);
+  if (m === null) {
+    return null;
+  }
+  return m[1];
+}
