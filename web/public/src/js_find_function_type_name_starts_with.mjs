313f4e82814c76651006137fc116a80180f35472
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:20:06 -0400

    function_auto_path ../love/public/src/js_find_function_type_name_starts_with.mjs

--- web/public/src/js_find_function_type_name_starts_with.mjs
+++ web/public/src/js_find_function_type_name_starts_with.mjs
@@ -1,7 +1,9 @@
+import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { js_list_function_nodes_visitors } from "../../../love/public/src/js_list_function_nodes_visitors.mjs";
 export function js_find_function_type_name_starts_with(ast) {
   let list = js_list_function_nodes_visitors(ast);
+  let mapped = list_map_property(list2, property_name);
   log(js_find_function_type_name_starts_with.name, {
     list,
   });
