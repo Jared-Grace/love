c25d8584668be3dc3c20d6d64c8e8ce84e2970fe
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 09:42:43 -0400

    ai

--- public/src/app_reply_buttons_languages_on_toggle.mjs
+++ public/src/app_reply_buttons_languages_on_toggle.mjs
@@ -3,7 +3,10 @@ import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs"
 import { html_button } from "../../../love/public/src/html_button.mjs";
 import { html_style_background_color_set_or_remove_list } from "../../../love/public/src/html_style_background_color_set_or_remove_list.mjs";
 import { list_toggle } from "../../../love/public/src/list_toggle.mjs";
+import { list_includes } from "../../../love/public/src/list_includes.mjs";
+import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
+import { not } from "../../../love/public/src/not.mjs";
 export function app_reply_buttons_languages_on_toggle(
   languages_chosen,
   on_toggle,
@@ -14,6 +17,12 @@ export function app_reply_buttons_languages_on_toggle(
     let name = property_get(language, "name");
     let component = null;
     function on_click() {
+      let chosen = list_includes(languages_chosen, language);
+      let m = list_multiple_is(languages_chosen);
+      let last_chosen = chosen && not(m);
+      if (last_chosen) {
+        return;
+      }
       list_toggle(languages_chosen, language);
       update();
       on_toggle();
