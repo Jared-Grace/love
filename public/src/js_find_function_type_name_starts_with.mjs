5c0d89b29982b44681acc5b87217811e43a560e9
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:07:04 -0400

    r i js_function_type_name_starts_with,function,find

--- public/src/js_find_function_type_name_starts_with.mjs
+++ public/src/js_find_function_type_name_starts_with.mjs
@@ -1,6 +1,6 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_visit_function_nodes } from "../../../love/public/src/js_visit_function_nodes.mjs";
-export function js_function_type_name_starts_with(ast) {
+export function js_find_function_type_name_starts_with(ast) {
   function lambda(la) {
     js_visit_function_nodes(ast, la);
   }
