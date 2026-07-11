47888d70af9ca0070ec952a761f05709af3724cf
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:53:43 -0400

    function_auto_path ../love/public/src/js_function_node_call.mjs

--- web/public/src/js_function_node_call.mjs
+++ web/public/src/js_function_node_call.mjs
@@ -1,6 +1,8 @@
+import { js_call_add_generic } from "../../../love/public/src/js_call_add_generic.mjs";
 import { list_single } from "../../../love/public/src/list_single.mjs";
 import { js_function_declaration_to_block_body } from "../../../love/public/src/js_function_declaration_to_block_body.mjs";
-export function js_function_node_call(ast, function_nodes) {
+export async function js_function_node_call(ast, function_nodes) {
   let function_node = list_single(function_nodes);
   let body_block = js_function_declaration_to_block_body(function_node);
+  await js_call_add_generic(ast, f_name, lambda_add);
 }
