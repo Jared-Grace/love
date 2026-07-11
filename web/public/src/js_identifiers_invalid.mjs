f7be5d65dee431e7d6f2674602cf667af2a4fe4b
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:39:01 -0400

    function_auto_path ../love/public/src/js_identifier_unique.mjs

---
+++ web/public/src/js_identifiers_invalid.mjs
@@ -0,0 +1,9 @@
+import { list_concat } from "../../../love/public/src/list_concat.mjs";
+import { js_keyword_default } from "../../../love/public/src/js_keyword_default.mjs";
+import { js_special_arguments } from "../../../love/public/src/js_special_arguments.mjs";
+export function js_identifiers_invalid(existing) {
+  let v2 = js_special_arguments();
+  let v = js_keyword_default();
+  let used = list_concat(existing, [v2, v]);
+  return used;
+}
