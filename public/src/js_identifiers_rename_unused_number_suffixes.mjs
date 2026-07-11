af697c16c9bcdf516711b618dfbccc991315ffd4
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:52:47 -0400

    pp 4

--- public/src/js_identifiers_rename_unused_number_suffixes.mjs
+++ public/src/js_identifiers_rename_unused_number_suffixes.mjs
@@ -6,7 +6,7 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function js_identifiers_rename_unused_number_suffixes(ast) {
-  let invalid = js_identifiers_invalid(existing);
+  let invalid = js_identifiers_invalid();
   let existing = js_identifiers_names(ast);
   function lambda(name) {
     let base = js_identifier_name_number_suffix_base(name);
