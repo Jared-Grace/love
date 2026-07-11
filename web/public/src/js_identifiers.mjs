08ea7026dc79f69ec0dc0107da3762c0cf47e4de
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 02:53:02 -0400

    t js_identifiers_rename_unused_number_suffixes js_identifiers

--- web/public/src/js_identifiers.mjs
+++ web/public/src/js_identifiers.mjs
@@ -1,9 +1,9 @@
 import { js_visit_identifiers_nodes } from "../../../love/public/src/js_visit_identifiers_nodes.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 export function js_identifiers(ast) {
-  function lambda2(la) {
+  function lambda(la) {
     js_visit_identifiers_nodes(ast, la);
   }
-  let result = list_adder_unique(lambda2);
+  let result = list_adder_unique(lambda);
   return result;
 }
