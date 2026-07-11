e6d1d62a2b07fde97d42fb11ef8ae436f076fb5c
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:04:12 -0400

    function_auto_path ../love/public/src/js_function_type_name_starts_with.mjs

--- public/src/js_function_type_name_starts_with.mjs
+++ public/src/js_function_type_name_starts_with.mjs
@@ -1,9 +1,9 @@
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
 import { js_visit_function_nodes } from "../../../love/public/src/js_visit_function_nodes.mjs";
 export function js_function_type_name_starts_with(ast) {
-  function lambda2(la) {
+  function lambda(la) {
     js_visit_function_nodes(ast, la);
   }
-  let list = list_adder(lambda2);
+  let list = list_adder(lambda);
   return list;
 }
