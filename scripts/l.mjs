580ab034a68914f4778123ed38310af783c96944
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:10:10 -0400

    q

--- scripts/l.mjs
+++ scripts/l.mjs
@@ -1,6 +1,6 @@
 
-import { lock_generic_force_release } from '../public/src/lock_generic_force_release.mjs';
+import { lock_force_release } from '../public/src/lock_force_release.mjs';
 import { function_run_prompt } from '../public/src/function_run_prompt.mjs';
 (async () => {
-  await lock_generic_force_release(function_run_prompt.name)
+  await lock_force_release(function_run_prompt.name)
 })();
