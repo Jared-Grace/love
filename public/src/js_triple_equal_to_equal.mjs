d8105aa9c1f50e3099a243addb2196c6d52827dd
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 07:13:57 -0400

    function_auto_path ../love/public/src/js_triple_equal_to_equal.mjs

--- public/src/js_triple_equal_to_equal.mjs
+++ public/src/js_triple_equal_to_equal.mjs
@@ -1,9 +1,7 @@
-import { js_operator_to_call } from "../../../love/public/src/js_operator_to_call.mjs";
+import { js_operator_to_call_binary } from "../../../love/public/src/js_operator_to_call_binary.mjs";
 import { js_operator_triple_equal } from "../../../love/public/src/js_operator_triple_equal.mjs";
 export async function js_triple_equal_to_equal(ast) {
   let o = js_operator_triple_equal();
-  let properties = ["left", "right"];
-  let type = "BinaryExpression";
-  let r = await js_operator_to_call(ast, o, properties, type);
+  let r = await js_operator_to_call_binary(ast, o);
   return r;
 }
