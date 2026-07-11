ed7c848ca48129e076d29da012d5568570da0269
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 05:30:39 -0400

    ai

--- public/src/app_code_lesson_operators_slash_forward.mjs
+++ public/src/app_code_lesson_operators_slash_forward.mjs
@@ -1,10 +1,10 @@
 import { js_operator_division_verb } from "../../../love/public/src/js_operator_division_verb.mjs";
 import { js_operator_division_name } from "../../../love/public/src/js_operator_division_name.mjs";
-import { js_operator_division } from "../../../love/public/src/js_operator_division.mjs";
+import { js_operator_division_symbol } from "../../../love/public/src/js_operator_division_symbol.mjs";
 import { app_code_lesson_operators_asterisk_generic } from "../../../love/public/src/app_code_lesson_operators_asterisk_generic.mjs";
 import { multiply } from "../../../love/public/src/multiply.mjs";
 export function app_code_lesson_operators_slash_forward() {
-  const operator = js_operator_division();
+  const operator = js_operator_division_symbol();
   const lesson_name = js_operator_division_name();
   let verb = js_operator_division_verb();
   let left_transform = multiply;
--- public/src/js_operators_arithmetic.mjs
+++ public/src/js_operators_arithmetic.mjs
@@ -1,33 +1,33 @@
 import { js_operator_asterisk_verb } from "../../../love/public/src/js_operator_asterisk_verb.mjs";
-import { js_operator_asterisk } from "../../../love/public/src/js_operator_asterisk.mjs";
+import { js_operator_asterisk_symbol } from "../../../love/public/src/js_operator_asterisk_symbol.mjs";
 import { js_operator_division_verb } from "../../../love/public/src/js_operator_division_verb.mjs";
-import { js_operator_division } from "../../../love/public/src/js_operator_division.mjs";
+import { js_operator_division_symbol } from "../../../love/public/src/js_operator_division_symbol.mjs";
 import { js_operator_minus_verb } from "../../../love/public/src/js_operator_minus_verb.mjs";
-import { js_operator_minus } from "../../../love/public/src/js_operator_minus.mjs";
+import { js_operator_minus_symbol } from "../../../love/public/src/js_operator_minus_symbol.mjs";
 import { js_operator_plus_verb } from "../../../love/public/src/js_operator_plus_verb.mjs";
-import { js_operator_plus } from "../../../love/public/src/js_operator_plus.mjs";
+import { js_operator_plus_symbol } from "../../../love/public/src/js_operator_plus_symbol.mjs";
 import { identity } from "../../../love/public/src/identity.mjs";
 import { add } from "../../../love/public/src/add.mjs";
 import { multiply } from "../../../love/public/src/multiply.mjs";
 export function js_operators_arithmetic() {
   let os = [
     {
-      operator: js_operator_plus(),
+      operator: js_operator_plus_symbol(),
       verb: js_operator_plus_verb(),
       left_transform: identity,
     },
     {
-      operator: js_operator_minus(),
+      operator: js_operator_minus_symbol(),
       verb: js_operator_minus_verb(),
       left_transform: add,
     },
     {
-      operator: js_operator_asterisk(),
+      operator: js_operator_asterisk_symbol(),
       verb: js_operator_asterisk_verb(),
       left_transform: identity,
     },
     {
-      operator: js_operator_division(),
+      operator: js_operator_division_symbol(),
       verb: js_operator_division_verb(),
       left_transform: multiply,
     },
