36e1feabe7c265261ee1e011cf45df455cb84033
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:23:47 -0400

    function_auto_path ../love/public/src/js_find_function_type_name_starts_with.mjs

--- web/public/src/js_find_function_type_name_starts_with.mjs
+++ web/public/src/js_find_function_type_name_starts_with.mjs
@@ -2,16 +2,12 @@ import { js_identifier_is_if } from "../../../love/public/src/js_identifier_is_i
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_filter } from "../../../love/public/src/list_filter.mjs";
 import { js_list_function_nodes } from "../../../love/public/src/js_list_function_nodes.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 export function js_find_function_type_name_starts_with(ast) {
   let mapped = js_list_function_nodes(ast);
   function lambda(n) {
     let id = property_get(n, "id");
     function lambda3() {}
     js_identifier_is_if(node, lambda3);
-    log(js_find_function_type_name_starts_with.name, {
-      id,
-    });
   }
   let filtered = list_filter(mapped, lambda);
 }
