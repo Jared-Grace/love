cdcc5308dac062c246a8ac01b1e3ecb6a909304b
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:57:08 -0400

    ai

--- web/public/src/js_const_to_let_node.mjs
+++ web/public/src/js_const_to_let_node.mjs
@@ -1,15 +1,10 @@
 import { property_transform } from "../../../love/public/src/property_transform.mjs";
 import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
 import { js_keyword_const } from "../../../love/public/src/js_keyword_const.mjs";
-import { equal } from "../../../love/public/src/equal.mjs";
+import { change_if_equal } from "../../../love/public/src/change_if_equal.mjs";
 export function js_const_to_let_node(node) {
   function lambda_kind(kind) {
-    let is_const = equal(kind, js_keyword_const());
-    if (is_const) {
-      let k = js_keyword_let();
-      return k;
-    }
-    return kind;
+    return change_if_equal(kind, js_keyword_const(), js_keyword_let());
   }
   property_transform(node, "kind", lambda_kind);
 }
