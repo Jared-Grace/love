5ed1eec30ad17ea89f7c754e7b05d85bec0ce34a
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:38:14 -0400

    function_auto_path ../love/public/src/function_transform_multiple_logs_remove.mjs

--- web/public/src/function_transform_multiple_logs_remove.mjs
+++ web/public/src/function_transform_multiple_logs_remove.mjs
@@ -1,8 +1,5 @@
 import { function_transform_multiple } from "../../../love/public/src/function_transform_multiple.mjs";
-export async function function_transform_multiple_logs_remove(
-  f_name_transformer_args_comma,
-  f_names_comma,
-) {
+export async function function_transform_multiple_logs_remove(f_names_comma) {
   let r = await function_transform_multiple(
     f_name_transformer_args_comma,
     f_names_comma,
