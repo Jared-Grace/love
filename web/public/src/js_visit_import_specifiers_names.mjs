f37f7d0054a4b4ce7ab675166357d21aec02f86d
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:34:35 -0400

    ai

--- web/public/src/js_visit_import_specifiers_names.mjs
+++ web/public/src/js_visit_import_specifiers_names.mjs
@@ -1,11 +1,16 @@
 import { js_visit_import_specifiers } from "../../../love/public/src/js_visit_import_specifiers.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
-export function js_visit_import_specifiers_names(ast, la) {
-  function lambda_import(v) {
-    let node = property_get(v, "node");
-    let local = property_get(node, "local");
-    let name = property_get(local, "name");
-    la(name);
+import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
+export function js_visit_import_specifiers_names(ast) {
+  function lambda2(la) {
+    function lambda_import(v) {
+      let node = property_get(v, "node");
+      let local = property_get(node, "local");
+      let name = property_get(local, "name");
+      la(name);
+    }
+    js_visit_import_specifiers(ast, lambda_import);
   }
-  js_visit_import_specifiers(ast, lambda_import);
+  const names = list_adder_unique(lambda2);
+  return names;
 }
