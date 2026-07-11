463753b844a387b7aec69189ed1059f27bbe3354
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:52:04 -0400

    function_auto_path ../love/public/src/function_params_delete_first.mjs

--- public/src/function_params_delete.mjs
+++ public/src/function_params_delete.mjs
@@ -1,7 +1,7 @@
 import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { function_param_delete_curried } from "../../../love/public/src/function_param_delete_curried.mjs";
 import { each_async } from "../../../love/public/src/each_async.mjs";
-export async function function_params_delete(f_name, param_names_comma) {
+export async function function_params_delete(f_name, param_names_comma) {tia
   let param_names = text_split_comma_dot(param_names_comma);
   let c = await function_param_delete_curried(f_name);
   await each_async(param_names, c);
--- public/src/function_params_delete_first.mjs
+++ public/src/function_params_delete_first.mjs
@@ -1,5 +1,4 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { function_params_get } from "../../../love/public/src/function_params_get.mjs";
@@ -9,9 +8,6 @@ export async function function_params_delete_first(f_name) {
   let params = await function_params_get(unaliased);
   let first = list_first(params);
   let name2 = property_get(first, "name");
-  log(function_params_delete_first.name, {
-    first,
-  });
   let r = await function_params_delete(f_name, name2);
   return r;
 }
