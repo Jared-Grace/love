2ccd4bb7f9ab3f3744f61e15280eba3a54504829
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:49:40 -0400

    function_auto_path ../love/public/src/function_params_delete_first.mjs

--- public/src/function_params_delete_first.mjs
+++ public/src/function_params_delete_first.mjs
@@ -2,7 +2,7 @@ import { list_first } from "../../../love/public/src/list_first.mjs";
 import { function_params_get } from "../../../love/public/src/function_params_get.mjs";
 import { function_params_delete } from "../../../love/public/src/function_params_delete.mjs";
 export async function function_params_delete_first(f_name) {
-  let params = await function_params_get(unaliased);
+  let params = await function_params_get(f_name);
   let first = list_first(params);
   let r = await function_params_delete(f_name, first);
   return r;
