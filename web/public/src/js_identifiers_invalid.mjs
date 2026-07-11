9b65bc1f50908c1d8698d582a798d46a2aa67fad
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 04:40:17 -0400

    function_auto_path ../love/public/src/js_identifiers_invalid.mjs

--- web/public/src/js_identifiers_invalid.mjs
+++ web/public/src/js_identifiers_invalid.mjs
@@ -1,8 +1,7 @@
-import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
+import { invoke_multiple_fn } from "../../../love/public/src/invoke_multiple_fn.mjs";
 import { js_identifiers_invalid_fns } from "../../../love/public/src/js_identifiers_invalid_fns.mjs";
 export function js_identifiers_invalid() {
   let fn = js_identifiers_invalid_fns;
-  let fns = fn();
-  let invalid = invoke_multiple(fns);
+  let invalid = invoke_multiple_fn(fn);
   return invalid;
 }
