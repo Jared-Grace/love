a67f8ee839ebb296f63c3529ad4ba18ab8ec3e55
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 07:01:34 -0400

    function_auto_path ../love/public/src/js_const_to_let_node.mjs

--- web/public/src/js_const_to_let_node.mjs
+++ web/public/src/js_const_to_let_node.mjs
@@ -3,9 +3,9 @@ import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
 import { js_keyword_const } from "../../../love/public/src/js_keyword_const.mjs";
 import { change_if_equal } from "../../../love/public/src/change_if_equal.mjs";
 export function js_const_to_let_node(node) {
+  let from2 = js_keyword_const();
+  let to = js_keyword_let();
   function lambda_kind(kind) {
-    let from2 = js_keyword_const();
-    let to = js_keyword_let();
     let result = change_if_equal(kind, from2, to);
     return result;
   }
