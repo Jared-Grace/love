bf3133c36cf4d00b77faca5a46de42a5a7d82528
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:40:41 -0400

    function_auto_path ../love/public/src/js_identifiers_rename_unused_number_suffixes.mjs

--- public/src/js_identifiers_rename_unused_number_suffixes.mjs
+++ public/src/js_identifiers_rename_unused_number_suffixes.mjs
@@ -1,3 +1,4 @@
+import { js_identifiers_invalid } from "../../../love/public/src/js_identifiers_invalid.mjs";
 import { js_identifiers_names } from "../../../love/public/src/js_identifiers_names.mjs";
 import { js_identifier_name_number_suffix_base } from "../../../love/public/src/js_identifier_name_number_suffix_base.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
@@ -5,14 +6,20 @@ import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_identifier_rename } from "../../../love/public/src/js_identifier_rename.mjs";
 import { each } from "../../../love/public/src/each.mjs";
 export function js_identifiers_rename_unused_number_suffixes(ast) {
+  let invalid = js_identifiers_invalid(existing);
   let names = js_identifiers_names(ast);
   function lambda(name) {
     let base = js_identifier_name_number_suffix_base(name);
     if (base === null) {
       return;
     }
+    let lists = [names, invalid];
     let exists = list_includes(names, base);
     if (exists) {
+      return;
+    }
+    let exists2 = list_includes(invalid, names);
+    if (exists2) {
       return;
     }
     js_identifier_rename(ast, name, base);
