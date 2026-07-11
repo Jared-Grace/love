c2a19fe3c8c82a066633ba998c74d05451e85609
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:46:02 -0400

    r t js_declare_is_if,declare,declaration

--- public/src/js_declare_single_is_if.mjs
+++ public/src/js_declare_single_is_if.mjs
@@ -1,4 +1,4 @@
-import { js_declare_is_if } from "../../../love/public/src/js_declare_is_if.mjs";
+import { js_declaration_is_if } from "../../../love/public/src/js_declaration_is_if.mjs";
 import { list_single_if } from "../../../love/public/src/list_single_if.mjs";
 import { js_declare_declarations_get } from "../../../love/public/src/js_declare_declarations_get.mjs";
 export function js_declare_single_is_if(node, lambda) {
@@ -6,5 +6,5 @@ export function js_declare_single_is_if(node, lambda) {
     let declarations = js_declare_declarations_get(node);
     list_single_if(declarations, lambda);
   }
-  js_declare_is_if(node, lambda3);
+  js_declaration_is_if(node, lambda3);
 }
