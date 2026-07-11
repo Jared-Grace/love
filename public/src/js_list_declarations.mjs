d2122b30e7aba26998cef388a78cdd6589712003
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:43:04 -0400

    w t js_visit_declarations,visit,list

---
+++ public/src/js_list_declarations.mjs
@@ -0,0 +1,5 @@
+import { js_visit_declarations } from "../../../love/public/src/js_visit_declarations.mjs";
+export function js_list_declarations(ast, lambda) {
+  let r = js_visit_declarations(ast, lambda);
+  return r;
+}
