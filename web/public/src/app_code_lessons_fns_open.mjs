072b0b249768102630360dc2a43532b9dfa09625
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 20:46:51 -0400

    w c app_code_lessons_fns,open

---
+++ web/public/src/app_code_lessons_fns_open.mjs
@@ -0,0 +1,5 @@
+import { app_code_lessons_fns } from "../../../love/public/src/app_code_lessons_fns.mjs";
+export function app_code_lessons_fns_open() {
+  let fns = app_code_lessons_fns();
+  return fns;
+}
