4588356099e8b14732dbea6e7b116588f22a187c
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:09:07 -0400

    np

--- scripts/l.mjs
+++ scripts/l.mjs
@@ -2,6 +2,5 @@
 import { lock_generic_force_release } from '../public/src/lock_generic_force_release.mjs';
 import { function_run_prompt } from '../public/src/function_run_prompt.mjs';
 (async () => {
-  const [, , lock_name] = process.argv;
-  await lock_generic_force_release(lock_name || function_run_prompt.name)
+  await lock_generic_force_release(function_run_prompt.name)
 })();
