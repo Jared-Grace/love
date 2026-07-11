2e6e9d683947fdc592f97a83c4702f61b211d10b
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:16:29 -0400

    function_auto_path ../love/public/src/js_find_function_type_name_starts_with.mjs

--- public/src/js_find_function_type_name_starts_with.mjs
+++ public/src/js_find_function_type_name_starts_with.mjs
@@ -1,5 +1,5 @@
 import { js_list_function_nodes } from "../../../love/public/src/js_list_function_nodes.mjs";
 export function js_find_function_type_name_starts_with(ast) {
-  let list2 = js_list_function_nodes(ast2);
+  let list2 = js_list_function_nodes(ast);
   return list;
 }
