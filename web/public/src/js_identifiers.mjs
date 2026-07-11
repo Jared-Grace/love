f278f7268f5bd6ebed2d56e6064ec7877e5a869d
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:57:31 -0400

    ai

--- web/public/src/js_identifiers.mjs
+++ web/public/src/js_identifiers.mjs
@@ -1,12 +1,8 @@
-import { js_visit_identifiers } from "../../../love/public/src/js_visit_identifiers.mjs";
+import { js_visit_identifiers_nodes } from "../../../love/public/src/js_visit_identifiers_nodes.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 export function js_identifiers(ast) {
   function lambda2(la) {
-    function lambda(v) {
-      let { node } = v;
-      la(node);
-    }
-    js_visit_identifiers(ast, lambda);
+    js_visit_identifiers_nodes(ast, la);
   }
   let result = list_adder_unique(lambda2);
   return result;
--- web/public/src/js_identifiers_names.mjs
+++ web/public/src/js_identifiers_names.mjs
@@ -1,14 +1,13 @@
-import { js_visit_identifiers } from "../../../love/public/src/js_visit_identifiers.mjs";
+import { js_visit_identifiers_nodes } from "../../../love/public/src/js_visit_identifiers_nodes.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { list_adder_unique } from "../../../love/public/src/list_adder_unique.mjs";
 export function js_identifiers_names(ast) {
   function lambda2(la) {
-    function lambda(v) {
-      let node = property_get(v, "node");
+    function lambda(node) {
       let value = property_get(node, "name");
       la(value);
     }
-    js_visit_identifiers(ast, lambda);
+    js_visit_identifiers_nodes(ast, lambda);
   }
   const names = list_adder_unique(lambda2);
   return names;
