f03204543f3aa3f3324bc6aed7514216b150406d
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:25:23 -0400

    function_auto_path ../love/public/src/app_replace_rule_set_add_generic.mjs

--- web/public/src/app_replace_rule_set_add_generic.mjs
+++ web/public/src/app_replace_rule_set_add_generic.mjs
@@ -1,15 +1,15 @@
+import { text_unique_underscore } from "../../../love/public/src/text_unique_underscore.mjs";
 import { js_imports_missing_add_specified_single } from "../../../love/public/src/js_imports_missing_add_specified_single.mjs";
 import { function_copy } from "../../../love/public/src/function_copy.mjs";
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 import { js_array_expression_single_elements } from "../../../love/public/src/js_array_expression_single_elements.mjs";
 import { js_parse_expression } from "../../../love/public/src/js_parse_expression.mjs";
-import { text_unique } from "../../../love/public/src/text_unique.mjs";
 import { functions_names } from "../../../love/public/src/functions_names.mjs";
 export async function app_replace_rule_set_add_generic(fn_base_name, fns_list) {
   let f_names = await functions_names();
   const f = fn_base_name;
-  let name_new = text_unique(f_names, f, "_");
+  let name_new = text_unique_underscore(f_names, f);
   await function_copy(f, name_new);
   let expression = js_parse_expression(name_new);
   async function lambda(ast) {
