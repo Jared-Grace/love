26a8a3c4c915fc3755d4d72860aad2cd25ee2749
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:55:20 -0400

    function_auto_path ../love/public/src/js_const_to_let.mjs

--- web/public/src/js_const_to_let.mjs
+++ web/public/src/js_const_to_let.mjs
@@ -1,9 +1,6 @@
 import { js_const_to_let_node } from "../../../love/public/src/js_const_to_let_node.mjs";
 import { js_visit_declarations_nodes } from "../../../love/public/src/js_visit_declarations_nodes.mjs";
 export function js_const_to_let(ast) {
-  function lambda(node) {
-    js_const_to_let_node(node);
-  }
-  js_visit_declarations_nodes(ast, lambda);
+  js_visit_declarations_nodes(ast, js_const_to_let_node);
   return;
 }
