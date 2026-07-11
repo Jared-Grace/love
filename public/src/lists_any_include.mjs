c5fc08f0856b4b439603d7c076069627900341e3
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:42:05 -0400

    function_auto_path ../love/public/src/js_identifiers_rename_unused_number_suffixes.mjs

---
+++ public/src/lists_any_include.mjs
@@ -0,0 +1,9 @@
+import { list_any } from "../../../love/public/src/list_any.mjs";
+import { list_includes } from "../../../love/public/src/list_includes.mjs";
+export function lists_any_include(base, lists) {
+  function lambda2(list) {
+    let i = list_includes(list, base);
+    return i;
+  }
+  let any = list_any(lists, lambda2);
+}
