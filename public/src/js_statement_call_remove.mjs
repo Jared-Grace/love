ec44f66cc6e8a7fbc2437cf22cab63285fbe7177
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:11:25 -0400

    ai

--- public/src/js_statement_call_remove.mjs
+++ public/src/js_statement_call_remove.mjs
@@ -5,19 +5,28 @@ import { list_remove } from "../../../love/public/src/list_remove.mjs";
 import { list_get_end_2 } from "../../../love/public/src/list_get_end_2.mjs";
 import { list_get_end_1 } from "../../../love/public/src/list_get_end_1.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
+import { list_adder } from "../../../love/public/src/list_adder.mjs";
+import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 export function js_statement_call_remove(ast, fn) {
-  function lambda(v) {
-    let node = property_get(v, "node");
-    let stack = property_get(v, "stack");
-    let e1 = list_get_end_1(stack);
-    let e2 = list_get_end_2(stack);
-    function lambda3() {
-      let name = js_call_callee_name_try(node);
-      if (name === fn.name) {
-        list_remove(e2, e1);
+  function lambda(la) {
+    function lambda_visit(v) {
+      let node = property_get(v, "node");
+      let stack = property_get(v, "stack");
+      let e1 = list_get_end_1(stack);
+      let e2 = list_get_end_2(stack);
+      function lambda3() {
+        let name = js_call_callee_name_try(node);
+        if (name === fn.name) {
+          function lambda_remove() {
+            list_remove(e2, e1);
+          }
+          la(lambda_remove);
+        }
       }
+      js_node_type_is_if(e1, "ExpressionStatement", lambda3);
     }
-    js_node_type_is_if(e1, "ExpressionStatement", lambda3);
+    js_visit_type(ast, "CallExpression", lambda_visit);
   }
-  js_visit_type(ast, "CallExpression", lambda);
+  let fns = list_adder(lambda);
+  invoke_multiple(fns);
 }
