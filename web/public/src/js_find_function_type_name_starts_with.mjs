e95afb65c6ab7291eb7dd6fc8b2c0d1b1aeeac5f
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:20:26 -0400

    function_auto_path ../love/public/src/js_find_function_type_name_starts_with.mjs

--- web/public/src/js_find_function_type_name_starts_with.mjs
+++ web/public/src/js_find_function_type_name_starts_with.mjs
@@ -1,9 +1,7 @@
-import { list_map_property } from "../../../love/public/src/list_map_property.mjs";
+import { js_list_function_nodes } from "../../../love/public/src/js_list_function_nodes.mjs";
 import { log } from "../../../love/public/src/log.mjs";
-import { js_list_function_nodes_visitors } from "../../../love/public/src/js_list_function_nodes_visitors.mjs";
 export function js_find_function_type_name_starts_with(ast) {
-  let list = js_list_function_nodes_visitors(ast);
-  let mapped = list_map_property(list, "node");
+  let mapped = js_list_function_nodes(ast);
   log(js_find_function_type_name_starts_with.name, {
     mapped,
   });
