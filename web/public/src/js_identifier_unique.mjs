91d8380e4ccf75c9557f65f7a60f169ef233b8cc
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:39:01 -0400

    function_auto_path ../love/public/src/js_identifier_unique.mjs

--- web/public/src/js_identifier_unique.mjs
+++ web/public/src/js_identifier_unique.mjs
@@ -1,12 +1,8 @@
+import { js_identifiers_invalid } from "../../../love/public/src/js_identifiers_invalid.mjs";
 import { text_unique } from "../../../love/public/src/text_unique.mjs";
-import { js_keyword_default } from "../../../love/public/src/js_keyword_default.mjs";
-import { js_special_arguments } from "../../../love/public/src/js_special_arguments.mjs";
-import { list_concat } from "../../../love/public/src/list_concat.mjs";
 import { list_add } from "../../../love/public/src/list_add.mjs";
 export function js_identifier_unique(existing, name) {
-  let v2 = js_special_arguments();
-  let v = js_keyword_default();
-  let used = list_concat(existing, [v2, v]);
+  let used = js_identifiers_invalid(existing);
   let unique = text_unique(used, name, "");
   list_add(existing, unique);
   return unique;
