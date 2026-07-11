481defc6c7281c31a63724c19227fabd58dfeeba
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 09:48:19 -0400

    ai

--- public/src/app_reply_buttons_languages_on_toggle.mjs
+++ public/src/app_reply_buttons_languages_on_toggle.mjs
@@ -1,11 +1,13 @@
 import { list_map } from "../../../love/public/src/list_map.mjs";
 import { object_merge_set } from "../../../love/public/src/object_merge_set.mjs";
 import { html_button } from "../../../love/public/src/html_button.mjs";
-import { html_style_background_color_set_or_remove_list } from "../../../love/public/src/html_style_background_color_set_or_remove_list.mjs";
-import { list_toggle } from "../../../love/public/src/list_toggle.mjs";
-import { list_includes } from "../../../love/public/src/list_includes.mjs";
+import { html_style_background_color_set_or_remove } from "../../../love/public/src/html_style_background_color_set_or_remove.mjs";
+import { list_find_property_or_null } from "../../../love/public/src/list_find_property_or_null.mjs";
+import { list_remove_property } from "../../../love/public/src/list_remove_property.mjs";
+import { list_add } from "../../../love/public/src/list_add.mjs";
 import { list_multiple_is } from "../../../love/public/src/list_multiple_is.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
+import { null_not_is } from "../../../love/public/src/null_not_is.mjs";
 import { not } from "../../../love/public/src/not.mjs";
 export function app_reply_buttons_languages_on_toggle(
   languages_chosen,
@@ -15,25 +17,40 @@ export function app_reply_buttons_languages_on_toggle(
 ) {
   function lambda(language) {
     let name = property_get(language, "name");
+    let language_code = property_get(language, "language_code");
     let component = null;
+    function chosen_get() {
+      let found = list_find_property_or_null(
+        languages_chosen,
+        "language_code",
+        language_code,
+      );
+      let chosen = null_not_is(found);
+      return chosen;
+    }
     function on_click() {
-      let chosen = list_includes(languages_chosen, language);
+      let chosen = chosen_get();
       let m = list_multiple_is(languages_chosen);
       let last_chosen = chosen && not(m);
       if (last_chosen) {
         return;
       }
-      list_toggle(languages_chosen, language);
+      if (chosen) {
+        list_remove_property(languages_chosen, "language_code", language_code);
+      } else {
+        list_add(languages_chosen, language);
+      }
       update();
       on_toggle();
     }
     component = html_button(root, name, on_click);
     update();
     function update() {
-      html_style_background_color_set_or_remove_list(
+      let chosen = chosen_get();
+      html_style_background_color_set_or_remove(
+        chosen,
         component,
-        languages_chosen,
-        language,
+        "lightgreen",
       );
     }
     let to = object_merge_set(component, {
--- public/src/js_operators_to_calls_generic.mjs
+++ public/src/js_operators_to_calls_generic.mjs
@@ -1,4 +1,4 @@
-import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { js_visit_type_node } from "../../../love/public/src/js_visit_type_node.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { property_get_equal } from "../../../love/public/src/property_get_equal.mjs";
 import { js_flo_name } from "../../../love/public/src/js_flo_name.mjs";
@@ -11,8 +11,7 @@ import { js_imports_missing_add_specified } from "../../../love/public/src/js_im
 export async function js_operators_to_calls_generic(ast, operators, properties, type) {
   let name = js_flo_name(ast);
   let usable = list_filter_property_path_not(operators, ["fn", "name"], name);
-  function lambda(v) {
-    let node = property_get(v, "node");
+  function lambda(node) {
     let node_operator = property_get(node, "operator");
     function lambda2(o) {
       let matched = property_get_equal(o, "operator", node_operator);
@@ -24,7 +23,7 @@ export async function js_operators_to_calls_generic(ast, operators, properties, 
     }
     each(usable, lambda2);
   }
-  js_visit_type(ast, type, lambda);
+  js_visit_type_node(ast, type, lambda);
   let names = js_operators_to_fn_name(usable);
   await js_imports_missing_add_specified(ast, names);
   return;
