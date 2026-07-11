60ad304f12fa417f36b041d585366cf535700f2f
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 07:00:17 -0400

    watch

--- public/src/js_const_to_let_node.mjs
+++ public/src/js_const_to_let_node.mjs
@@ -5,6 +5,7 @@ import { change_if_equal } from "../../../love/public/src/change_if_equal.mjs";
 export function js_const_to_let_node(node) {
   function lambda_kind(kind) {
     return change_if_equal(kind, js_keyword_const(), js_keyword_let());
+    
   }
   property_transform(node, "kind", lambda_kind);
 }
