2b5381bb87d89668f517e42acc94c1ac43406d7d
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 04:39:25 -0400

    ai

--- web/public/src/js_identifiers_invalid.mjs
+++ web/public/src/js_identifiers_invalid.mjs
@@ -1,7 +1,7 @@
 import { invoke_multiple } from "../../../love/public/src/invoke_multiple.mjs";
 import { js_identifiers_invalid_fns } from "../../../love/public/src/js_identifiers_invalid_fns.mjs";
-export function js_identifiers_invalid() {
-  let fns = js_identifiers_invalid_fns();
+export function js_identifiers_invalid() {fn=js_identifiers_invalid_fns
+  let fns = fn();
   let invalid = invoke_multiple(fns);
   return invalid;
 }
