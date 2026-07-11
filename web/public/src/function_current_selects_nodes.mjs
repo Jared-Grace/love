fe6c1b66254817adc3044f0247a712ab9c06bf4b
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 23:27:46 -0400

    functions_rename_swap function_current_selects_nodes_ast function_current_selects_nodes

--- web/public/src/function_current_selects_nodes.mjs
+++ web/public/src/function_current_selects_nodes.mjs
@@ -1,7 +1,7 @@
-import { js_visit_id_to_node_or_id_multiple } from "../../../love/public/src/js_visit_id_to_node_or_id_multiple.mjs";
-import { function_current_selects } from "../../../love/public/src/function_current_selects.mjs";
-export async function function_current_selects_nodes(ast) {
-  let selects = await function_current_selects();
-  selects = js_visit_id_to_node_or_id_multiple(selects, ast);
+import { function_current_selects_nodes_ast } from "../../../love/public/src/function_current_selects_nodes_ast.mjs";
+import { function_current_ast } from "../../../love/public/src/function_current_ast.mjs";
+export async function function_current_selects_nodes() {
+  let ast = await function_current_ast();
+  let selects = await function_current_selects_nodes_ast(ast);
   return selects;
 }
