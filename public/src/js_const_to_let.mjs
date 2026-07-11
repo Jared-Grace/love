d6869a17cbfefc2056dfe63d48afbb3365b73eea
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 06:48:14 -0400

    ai

--- public/src/js_const_to_let.mjs
+++ public/src/js_const_to_let.mjs
@@ -1,7 +1,7 @@
 import { property_get } from "../../../love/public/src/property_get.mjs";
 import { property_transform } from "../../../love/public/src/property_transform.mjs";
 import { equal } from "../../../love/public/src/equal.mjs";
-import { js_visit_type } from "../../../love/public/src/js_visit_type.mjs";
+import { js_visit_declarations } from "../../../love/public/src/js_visit_declarations.mjs";
 import { js_keyword_const } from "../../../love/public/src/js_keyword_const.mjs";
 import { js_keyword_let } from "../../../love/public/src/js_keyword_let.mjs";
 export function js_const_to_let(ast) {
@@ -16,6 +16,6 @@ export function js_const_to_let(ast) {
     }
     property_transform(node, "kind", lambda_kind);
   }
-  js_visit_type(ast, "VariableDeclaration", lambda);
+  js_visit_declarations(ast, lambda);
   return;
 }
