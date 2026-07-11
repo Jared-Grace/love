318da8f75b21636a638c809aeed1bb70df18d515
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:50:08 -0400

    function_auto_path ../love/public/src/function_params_delete_first.mjs

--- public/src/function_params_delete_first.mjs
+++ public/src/function_params_delete_first.mjs
@@ -1,7 +1,9 @@
+import { function_name_unalias_only } from "../../../love/public/src/function_name_unalias_only.mjs";
 import { list_first } from "../../../love/public/src/list_first.mjs";
 import { function_params_get } from "../../../love/public/src/function_params_get.mjs";
 import { function_params_delete } from "../../../love/public/src/function_params_delete.mjs";
 export async function function_params_delete_first(f_name) {
+  let unaliased = await function_name_unalias_only(name);
   let params = await function_params_get(f_name);
   let first = list_first(params);
   let r = await function_params_delete(f_name, first);
