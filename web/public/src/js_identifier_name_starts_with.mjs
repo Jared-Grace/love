dd7f52ccc028f24309ad1e4bd1f81121a00bf88f
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 01:34:04 -0400

    l js_identifier_name_starts_with

--- web/public/src/js_identifier_name_starts_with.mjs
+++ web/public/src/js_identifier_name_starts_with.mjs
@@ -3,9 +3,6 @@ import { text_starts_with } from "../../../love/public/src/text_starts_with.mjs"
 import { js_identifier_name } from "../../../love/public/src/js_identifier_name.mjs";
 export function js_identifier_name_starts_with(id, prefix) {
   let name = js_identifier_name(id);
-  log(js_identifier_name_starts_with.name, {
-    name,
-  });
   let starts_with = text_starts_with(name, prefix);
   return starts_with;
 }
