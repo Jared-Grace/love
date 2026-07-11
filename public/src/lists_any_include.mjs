c74b9b5a61235d8643c0aa9807b3ecee22845943
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 03:42:49 -0400

    pb lists_any_include

--- public/src/lists_any_include.mjs
+++ public/src/lists_any_include.mjs
@@ -1,6 +1,6 @@
 import { list_any } from "../../../love/public/src/list_any.mjs";
 import { list_includes } from "../../../love/public/src/list_includes.mjs";
-export function lists_any_include(base, lists) {
+export function lists_any_include(lists, base) {
   function lambda2(list) {
     let i = list_includes(list, base);
     return i;
