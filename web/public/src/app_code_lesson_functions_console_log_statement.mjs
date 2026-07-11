259f7873fc32fb8e09858d880daf90406a56a143
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 20:52:03 -0400

    function_auto_path ../love/public/src/app_code_lesson_functions_console_log_statement.mjs

--- web/public/src/app_code_lesson_functions_console_log_statement.mjs
+++ web/public/src/app_code_lesson_functions_console_log_statement.mjs
@@ -3,7 +3,6 @@ import { html_div } from "../../../love/public/src/html_div.mjs";
 import { html_cycle_bold_code } from "../../../love/public/src/html_cycle_bold_code.mjs";
 import { html_div_cycle_bold } from "../../../love/public/src/html_div_cycle_bold.mjs";
 import { app_code_container_light_blue_text } from "../../../love/public/src/app_code_container_light_blue_text.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { js_code_statement } from "../../../love/public/src/js_code_statement.mjs";
 import { app_code_lesson_functions_console_log_generic } from "../../../love/public/src/app_code_lesson_functions_console_log_generic.mjs";
 import { property_get } from "../../../love/public/src/property_get.mjs";
@@ -14,13 +13,8 @@ export function app_code_lesson_functions_console_log_statement() {
     js_code_statement,
     [" statement"],
   );
-  let next = property_get(r, "next");
   let fn_name = property_get(r, "fn_name");
-  let next_operator = property_get(r, "next_operator");
   let lesson = property_get(r, "lesson");
-  log(app_code_lesson_functions_console_log_statement.name, {
-    lesson,
-  });
   return lesson;
   function above(root) {
     let c2 = app_code_container_light_blue_text(
