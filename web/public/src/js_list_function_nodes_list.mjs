b6a2e4e5b1a3c50c38722f5fcdb891459323a5d9
JESUS rose to life <username@users.noreply.github.com>
2026-07-10 21:05:06 -0400

    r t js_visit_function_nodes_list,visit,list

--- web/public/src/js_list_function_nodes_list.mjs
+++ web/public/src/js_list_function_nodes_list.mjs
@@ -1,6 +1,6 @@
 import { js_visit_function_nodes_curried } from "../../../love/public/src/js_visit_function_nodes_curried.mjs";
 import { list_adder } from "../../../love/public/src/list_adder.mjs";
-export function js_visit_function_nodes_list(ast) {
+export function js_list_function_nodes_list(ast) {
   let lambda = js_visit_function_nodes_curried(ast);
   let list = list_adder(lambda);
   return list;
