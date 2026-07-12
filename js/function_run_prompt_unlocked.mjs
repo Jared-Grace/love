8fa9a592fa70f2cfa4d21e1251e92283d90fe99e
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 02:52:35 -0400

    s test

--- js/function_run_prompt_unlocked.mjs
+++ js/function_run_prompt_unlocked.mjs
@@ -1,8 +1,8 @@
-import { list_take } from "../../love/js/list_take.mjs";
-import { function_run_prompt_line } from "../../love/js/function_run_prompt_line.mjs";
-import { function_run_line_git } from "../../love/js/function_run_line_git.mjs";
+import { function_run_prompt_line } from "./function_run_prompt_line.mjs";
+import { function_run_line_git } from "./function_run_line_git.mjs";
 export async function function_run_prompt_unlocked() {
   let line = await function_run_prompt_line();
-  let taken = list_take(list, count);
   await function_run_line_git(line);
+  return;
+  
 }
