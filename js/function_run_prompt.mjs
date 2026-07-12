06047e31ef509103523c2331b75e686d3b2c880d
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 00:31:06 -0400

    ai

--- js/function_run_prompt.mjs
+++ js/function_run_prompt.mjs
@@ -1,11 +1,6 @@
 import { function_run_prompt_lock } from "./function_run_prompt_lock.mjs";
 import { function_run_prompt_line } from "./function_run_prompt_line.mjs";
-import { log_error } from "./log_error.mjs";
-import { null_not_is } from "./null_not_is.mjs";
-import { data_property_get_generic } from "./data_property_get_generic.mjs";
 import { function_run_line_git } from "./function_run_line_git.mjs";
-import { data_path } from "./data_path.mjs";
-import { json_format_to } from "./json_format_to.mjs";
 export async function function_run_prompt() {
   let line = await function_run_prompt_line();
   async function lambda() {
