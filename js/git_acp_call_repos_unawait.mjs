ca9da08437a011e875fa714fd6a212ef666614b4
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 00:42:32 -0400

    __marker_should_run_after_wait__

--- js/git_acp_call_repos_unawait.mjs
+++ js/git_acp_call_repos_unawait.mjs
@@ -1,14 +1,13 @@
 import { promise_later } from "./promise_later.mjs";
 import { git_acp_call_repos } from "./git_acp_call_repos.mjs";
-import { lock_generic } from "./lock_generic.mjs";
+import { lock_wait } from "./lock_wait.mjs";
 export function git_acp_call_repos_unawait(f_name, args) {
   async function wrapped() {
     await git_acp_call_repos(f_name, args);
   }
   async function lambda() {
-    await lock_generic(
+    await lock_wait(
       git_acp_call_repos.name,
-      false,
       wrapped,
       git_acp_call_repos_unawait.name,
     );
