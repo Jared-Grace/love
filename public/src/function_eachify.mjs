681b0469f44325a82e72b1361409cec9301d6d47
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 22:08:41 -0400

    ai

---
+++ public/src/function_eachify.mjs
@@ -0,0 +1,48 @@
+import { function_parse_declaration_unaliased } from "../../../love/public/src/function_parse_declaration_unaliased.mjs";
+import { property_get } from "../../../love/public/src/property_get.mjs";
+import { js_function_declaration_params_names } from "../../../love/public/src/js_function_declaration_params_names.mjs";
+import { list_size } from "../../../love/public/src/list_size.mjs";
+import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
+import { function_name_combine } from "../../../love/public/src/function_name_combine.mjs";
+import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
+import { js_flo_params_add } from "../../../love/public/src/js_flo_params_add.mjs";
+import { js_flo_body_add } from "../../../love/public/src/js_flo_body_add.mjs";
+import { js_code_call_args_await_maybe } from "../../../love/public/src/js_code_call_args_await_maybe.mjs";
+import { js_code_function_declaration_args } from "../../../love/public/src/js_code_function_declaration_args.mjs";
+import { js_code_call_args } from "../../../love/public/src/js_code_call_args.mjs";
+import { js_code_await } from "../../../love/public/src/js_code_await.mjs";
+import { js_parse_statement } from "../../../love/public/src/js_parse_statement.mjs";
+import { js_imports_missing_add_all } from "../../../love/public/src/js_imports_missing_add_all.mjs";
+export async function function_eachify(fn_name) {
+  let u = await function_parse_declaration_unaliased(fn_name);
+  let unaliased = property_get(u, "unaliased");
+  let declaration_call = property_get(u, "declaration");
+  let arg_names = js_function_declaration_params_names(declaration_call);
+  equal_assert(list_size(arg_names), 1);
+  let async_is = property_get(declaration_call, "async");
+  let each_name = async_is ? "each_async" : "each";
+  let f_name_multiple = function_name_combine(unaliased, "multiple");
+  let output = await function_new_transform(f_name_multiple, lambda);
+  async function lambda(ast) {
+    js_flo_params_add(ast, ["list"]);
+    let call_inner_code = js_code_call_args_await_maybe(
+      unaliased,
+      ["item"],
+      declaration_call,
+    );
+    let lambda_code = js_code_function_declaration_args(
+      async_is,
+      "lambda",
+      ["item"],
+      call_inner_code,
+    );
+    let each_call_code = js_code_call_args(each_name, ["list", lambda_code]);
+    if (async_is) {
+      each_call_code = js_code_await(each_call_code);
+    }
+    let statement = js_parse_statement(each_call_code);
+    js_flo_body_add(ast, statement);
+    await js_imports_missing_add_all(ast);
+  }
+  return output;
+}
