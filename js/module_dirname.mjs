7cdc3d8f6655677d11237ea1744d4a3b8953f95d
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 01:12:50 -0400

    ai

--- js/module_dirname.mjs
+++ js/module_dirname.mjs
@@ -1,7 +1,9 @@
 import { fileURLToPath } from "url";
 import { path_dirname } from "./path_dirname.mjs";
-export async function module_dirname(meta_url) {
-  let __filename = fileURLToPath(meta_url);
+import { property_get } from "./property_get.mjs";
+export async function module_dirname(meta) {
+  let url = property_get(meta, "url");
+  let __filename = fileURLToPath(url);
   let __dirname = await path_dirname(__filename);
   return __dirname;
 }
