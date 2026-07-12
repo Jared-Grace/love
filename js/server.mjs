a35f03e62daa283d99649c769ae9c09460864876
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 01:00:35 -0400

    ai

--- js/server.mjs
+++ js/server.mjs
@@ -6,12 +6,10 @@ import { server_port } from "./server_port.mjs";
 import { server_url_api } from "./server_url_api.mjs";
 import { function_run_io_file_wrapper } from "./function_run_io_file_wrapper.mjs";
 import { property_get } from "./property_get.mjs";
-import { folder_repo_root_find } from "./folder_repo_root_find.mjs";
+import { folder_repo_root_parent_resolve } from "./folder_repo_root_parent_resolve.mjs";
 import { log_keep } from "./log_keep.mjs";
 import express from "express";
 import { path_dirname } from "./path_dirname.mjs";
-import { path_resolve } from "./path_resolve.mjs";
-import { path_join } from "./path_join.mjs";
 import { fileURLToPath } from "url";
 import { text_combine } from "./text_combine.mjs";
 export async function server() {
@@ -23,8 +21,7 @@ export async function server() {
   let port = server_port();
   let __filename = fileURLToPath(import.meta.url);
   let __dirname = await path_dirname(__filename);
-  let repo_root = folder_repo_root_find(__dirname);
-  let result = await path_resolve(path_join([repo_root, ".."]));
+  let result = await folder_repo_root_parent_resolve(__dirname);
   let v = express.static(result);
   let u = server_url_api();
   async function api_generic(req, res) {
