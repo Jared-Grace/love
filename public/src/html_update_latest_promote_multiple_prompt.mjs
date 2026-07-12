79d133015b234e5082566aed17661d4217876fc0
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 22:28:28 -0400

    function_auto_path ../love/public/src/html_update_latest_promote_multiple_prompt.mjs

--- public/src/html_update_latest_promote_multiple_prompt.mjs
+++ public/src/html_update_latest_promote_multiple_prompt.mjs
@@ -1,5 +1,7 @@
+import { text_split_comma_dot } from "../../../love/public/src/text_split_comma_dot.mjs";
 import { html_update_latest_promote_multiple } from "../../../love/public/src/html_update_latest_promote_multiple.mjs";
 export async function html_update_latest_promote_multiple_prompt(list) {
+  let split = text_split_comma_dot(t);
   let r = await html_update_latest_promote_multiple(list);
   return r;
 }
