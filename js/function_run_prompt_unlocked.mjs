e5aeb555a3ebbea594682683b1832585aa362f04
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 02:52:53 -0400

    ao function_run_prompt_unlocked

--- js/function_run_prompt_unlocked.mjs
+++ js/function_run_prompt_unlocked.mjs
@@ -1,8 +1,10 @@
-import { function_run_prompt_line } from "./function_run_prompt_line.mjs";
-import { function_run_line_git } from "./function_run_line_git.mjs";
+import { lock_try } from "../../love/js/lock_try.mjs";
+import { function_run_prompt_line } from "../../love/js/function_run_prompt_line.mjs";
+import { function_run_line_git } from "../../love/js/function_run_line_git.mjs";
 export async function function_run_prompt_unlocked() {
   let line = await function_run_prompt_line();
   await function_run_line_git(line);
   return;
-  
+  async function lambda2() {}
+  let r = await lock_try(lock_name, lambda2, who);
 }
