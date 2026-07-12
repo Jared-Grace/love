384453f9c7ef55e79f592817bbe2778f94f88778
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 21:25:08 +0800

    ai

--- js/text_is_assert_message.mjs
+++
@@ -1,7 +0,0 @@
-import { text_is } from "./text_is.mjs";
-import { assert_message } from "./assert_message.mjs";
-export function text_is_assert_message(value, message) {
-  let b = text_is(value);
-  let v = assert_message(b, message);
-  return v;
-}
