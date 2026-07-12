44ed6af539a309682b586be115ad36417774b4f5
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 05:29:57 -0400

    ai

--- js/html_update_dev.mjs
+++ js/html_update_dev.mjs
@@ -1,19 +1,18 @@
-import { function_name_to_path_import_code_root_multiple_map } from "./function_name_to_path_import_code_root_multiple_map.mjs";
-import { list_add } from "./list_add.mjs";
-import { app_shared_name_search_info } from "./app_shared_name_search_info.mjs";
-import { js_code_call_app_context_initialize } from "./js_code_call_app_context_initialize.mjs";
-import { list_join_newline } from "./list_join_newline.mjs";
-import { app_context_initialize } from "./app_context_initialize.mjs";
-import { list_add_join_newline } from "./list_add_join_newline.mjs";
-import { property_get } from "./property_get.mjs";
-import { html_update_externals } from "./html_update_externals.mjs";
-import { function_name_repo_path_combine } from "./function_name_repo_path_combine.mjs";
-import { app_shared_name_prefixed } from "./app_shared_name_prefixed.mjs";
-import { file_open } from "./file_open.mjs";
-import { html_overwrite } from "./html_overwrite.mjs";
-import { html_code_script_module } from "./html_code_script_module.mjs";
-import { app_shared_name_main } from "./app_shared_name_main.mjs";
-import { html_name_to_path_dev } from "./html_name_to_path_dev.mjs";
+import { html_overwrite } from "../../love/js/html_overwrite.mjs";
+import { list_add_join_newline } from "../../love/js/list_add_join_newline.mjs";
+import { html_update_externals } from "../../love/js/html_update_externals.mjs";
+import { html_code_script_module } from "../../love/js/html_code_script_module.mjs";
+import { list_join_newline } from "../../love/js/list_join_newline.mjs";
+import { list_add } from "../../love/js/list_add.mjs";
+import { function_name_to_path_import_code_root_multiple_map } from "../../love/js/function_name_to_path_import_code_root_multiple_map.mjs";
+import { app_context_initialize } from "../../love/js/app_context_initialize.mjs";
+import { js_code_call_app_context_initialize } from "../../love/js/js_code_call_app_context_initialize.mjs";
+import { app_shared_name_main } from "../../love/js/app_shared_name_main.mjs";
+import { function_name_repo_path_combine } from "../../love/js/function_name_repo_path_combine.mjs";
+import { app_shared_name_prefixed } from "../../love/js/app_shared_name_prefixed.mjs";
+import { html_name_to_path_dev } from "../../love/js/html_name_to_path_dev.mjs";
+import { property_get } from "../../love/js/property_get.mjs";
+import { app_shared_name_search_info } from "../../love/js/app_shared_name_search_info.mjs";
 export async function html_update_dev(name) {
   let v2 = await app_shared_name_search_info(name);
   name = property_get(v2, "a_name");
@@ -23,7 +22,8 @@ export async function html_update_dev(name) {
   let name_prefixed = await app_shared_name_main(name);
   let call = js_code_call_app_context_initialize(name_prefixed);
   let imports = [name_prefixed, app_context_initialize.name];
-  let mapped = await function_name_to_path_import_code_root_multiple_map(imports);
+  let mapped =
+    await function_name_to_path_import_code_root_multiple_map(imports);
   list_add(mapped, call);
   let middle = list_join_newline(mapped);
   let body = html_code_script_module(middle);
@@ -31,5 +31,5 @@ export async function html_update_dev(name) {
   let scripts = property_get(v, "scripts");
   let joined = list_add_join_newline(scripts, body);
   await html_overwrite(name, path, joined);
-  await file_open(path);
+  return path;
 }
