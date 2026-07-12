f265509bd52c01499bb99010bb87873b028783ee
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 00:27:06 -0400

    s unlo

--- js/function_run_prompt_unlocked.mjs
+++ js/function_run_prompt_unlocked.mjs
@@ -5,10 +5,6 @@ import { chalk_green } from "./chalk_green.mjs";
 import { function_run_line_git } from "./function_run_line_git.mjs";
 import { command_line_read } from "./command_line_read.mjs";
 export async function function_run_prompt_unlocked() {
-  let repo_name = await user_repo_get();
-  let prompt_colored = await chalk_green("✟ ");
-  let colored = await function_run_prompt_repo_name_colored(repo_name);
-  let combined = text_combine_multiple([prompt_colored, colored, " "]);
-  let line = await command_line_read(combined);
+  let line = await function_run_prompt_line();
   await function_run_line_git(line);
 }
