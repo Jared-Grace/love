a0faf6c3196b1c013b1c3302a16cce0cfd67257f
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 23:53:40 -0400

    ai

--- js/firebase_upload_object_browser.mjs
+++ js/firebase_upload_object_browser.mjs
@@ -4,5 +4,5 @@ import { firebase_upload_object_arg } from "./firebase_upload_object_arg.mjs";
 export async function firebase_upload_object_browser(destination, object) {
   var r = firebase_upload_object_arg(object);
   let content = property_get(r, "content");
-  await firebase_upload_text_generic_browser(content, destination);
+  await firebase_upload_text_generic_browser(destination, content);
 }
