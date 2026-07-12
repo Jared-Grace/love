605bd3c4412acb528eb6e7fbcfcec0061b1ec129
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 23:33:39 -0400

    ai

--- js/js_dollar_choice_argument_names.mjs
+++ js/js_dollar_choice_argument_names.mjs
@@ -1,9 +1,9 @@
 import { property_get } from "./property_get.mjs";
-import { js_dollar_choice_arguments } from "../../../love/public/src/js_dollar_choice_arguments.mjs";
+import { js_dollar_choice_argument } from "./js_dollar_choice_argument.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 export async function js_dollar_choice_argument_names() {
-  let v = await js_dollar_choice_arguments();
+  let v = await js_dollar_choice_argument();
   let properties = property_get(v, "properties");
   let mapped = list_map_property(properties, "key");
   let names = js_identifiers_to_names(mapped);
