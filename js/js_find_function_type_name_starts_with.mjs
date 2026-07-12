d13ecb365b76a2be1864e71cf04af9a66d7a6cbb
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 05:59:04 -0400

    ai

--- js/js_find_function_type_name_starts_with.mjs
+++ js/js_find_function_type_name_starts_with.mjs
@@ -1,12 +1,10 @@
-import { js_identifier_name_starts_with_try } from "../../love/js/js_identifier_name_starts_with_try.mjs";
+import { js_id_name_starts_with_try } from "../../love/js/js_id_name_starts_with_try.mjs";
 import { list_find } from "../../love/js/list_find.mjs";
-import { property_get } from "../../love/js/property_get.mjs";
 import { js_list_function_nodes } from "../../love/js/js_list_function_nodes.mjs";
 export function js_find_function_type_name_starts_with(ast, prefix) {
   let mapped = js_list_function_nodes(ast);
   function lambda(n) {
-    let id = property_get(n, "id");
-    let starts_with = js_identifier_name_starts_with_try(id, prefix);
+    let starts_with = js_id_name_starts_with_try(n, prefix);
     return starts_with;
   }
   let only = list_find(mapped, lambda);
