ad42c3ee79ad191ed36f14fb7e4bbe99f6ab6171
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 05:46:58 -0400

    ai

--- js/js_find_call_name_includes.mjs
+++ js/js_find_call_name_includes.mjs
@@ -1,6 +1,5 @@
 import { js_identifier_name_includes_try } from "../../love/js/js_identifier_name_includes_try.mjs";
-import { list_single } from "../../love/js/list_single.mjs";
-import { list_filter } from "../../love/js/list_filter.mjs";
+import { list_find } from "../../love/js/list_find.mjs";
 import { property_get } from "../../love/js/property_get.mjs";
 import { js_list_calls_nodes } from "../../love/js/js_list_calls_nodes.mjs";
 export function js_find_call_name_includes(ast, part) {
@@ -10,7 +9,6 @@ export function js_find_call_name_includes(ast, part) {
     let includes = js_identifier_name_includes_try(callee, part);
     return includes;
   }
-  let filtered = list_filter(nodes, lambda);
-  let only = list_single(filtered);
+  let only = list_find(nodes, lambda);
   return only;
 }
