9a7f72a5a31d7a2451c57c0c178ed00d8e83bc41
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 22:28:14 -0400

    w c html_update_latest_promote_multiple,prompt

---
+++ web/public/src/html_update_latest_promote_multiple_prompt.mjs
@@ -0,0 +1,5 @@
+import { html_update_latest_promote_multiple } from "../../../love/public/src/html_update_latest_promote_multiple.mjs";
+export async function html_update_latest_promote_multiple_prompt(list) {
+  let r = await html_update_latest_promote_multiple(list);
+  return r;
+}
