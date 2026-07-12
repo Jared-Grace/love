83221dbf73d24260003927cf29ee2a58caeaeffc
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 06:29:44 -0400

    t c js_find_call_name_includes

--- js/js_find_call_name_includes.mjs
+++ js/js_find_call_name_includes.mjs
@@ -1,12 +1,10 @@
+import { js_callee_name_includes_try_curried_right } from "../../love/js/js_callee_name_includes_try_curried_right.mjs";
 import { js_callee_name_includes_try } from "../../love/js/js_callee_name_includes_try.mjs";
 import { list_find } from "../../love/js/list_find.mjs";
 import { js_list_calls_nodes } from "../../love/js/js_list_calls_nodes.mjs";
 export function js_find_call_name_includes(ast, part) {
   let nodes = js_list_calls_nodes(ast);
-  function lambda(n) {
-    let includes = js_callee_name_includes_try(n, part);
-    return includes;
-  }
+  let lambda = js_callee_name_includes_try_curried_right(part);
   let only = list_find(nodes, lambda);
   return only;
 }
