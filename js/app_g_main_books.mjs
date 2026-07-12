8da0633c0206f6c73482d5f829b2b76eac0dcafd
JESUS rose to life <username@users.noreply.github.com>
2026-07-12 04:17:29 -0400

    ai

--- js/app_g_main_books.mjs
+++ js/app_g_main_books.mjs
@@ -1,6 +1,6 @@
-import { app_g_main } from "./app_g_main.mjs";
-import { global_function_property_get } from "./global_function_property_get.mjs";
+import { app_g } from "../../love/js/app_g.mjs";
+import { global_function_property_get } from "../../love/js/global_function_property_get.mjs";
 export function app_g_main_books() {
-  let value = global_function_property_get(app_g_main, "books");
+  let value = global_function_property_get(app_g, "books");
   return value;
 }
--- js/firebase_promote_function_app_g_main.mjs
+++
@@ -1,6 +0,0 @@
-import { app_g_main } from "./app_g_main.mjs";
-import { firebase_promote_function } from "./firebase_promote_function.mjs";
-export async function firebase_promote_function_app_g_main() {
-  let v = await firebase_promote_function(app_g_main.name);
-  return v;
-}
