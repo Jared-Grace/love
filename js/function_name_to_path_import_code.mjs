22fce4d4dfb374c8708b58cf681684534f869d95
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 15:28:08 +0800

    ai

--- js/function_name_to_path_import_code.mjs
+++ js/function_name_to_path_import_code.mjs
@@ -1,8 +1,10 @@
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { function_name_to_path_import } from "./function_name_to_path_import.mjs";
-import { text_is_assert } from "./text_is_assert.mjs";
+import { text_is_assert_json } from "./text_is_assert_json.mjs";
 export function function_name_to_path_import_code(import_, dictionary) {
-  text_is_assert(import_);
+  text_is_assert_json(import_, {
+    import_,
+  });
   let from = function_name_to_path_import(import_, dictionary);
   let code = js_code_import_single(import_, from);
   return code;
--- js/function_name_to_path_import_code_root.mjs
+++ js/function_name_to_path_import_code_root.mjs
@@ -1,8 +1,10 @@
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { function_name_to_path_import_root } from "./function_name_to_path_import_root.mjs";
-import { text_is_assert } from "./text_is_assert.mjs";
+import { text_is_assert_json } from "./text_is_assert_json.mjs";
 export function function_name_to_path_import_code_root(import_, dictionary) {
-  text_is_assert(import_);
+  text_is_assert_json(import_, {
+    import_,
+  });
   let from = function_name_to_path_import_root(import_, dictionary);
   let code = js_code_import_single(import_, from);
   return code;
