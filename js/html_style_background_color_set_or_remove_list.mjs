efab5e883de2db57763effed9dd36b9c66dac629
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 18:05:10 +0800

    ai

--- js/html_style_background_color_set_or_remove_list.mjs
+++ js/html_style_background_color_set_or_remove_list.mjs
@@ -1,4 +1,7 @@
 import { html_style_background_color_set_or_remove } from "./html_style_background_color_set_or_remove.mjs";
+import { html_font_color_set_or_remove } from "./html_font_color_set_or_remove.mjs";
+import { app_shared_button_selected_background_color } from "./app_shared_button_selected_background_color.mjs";
+import { app_shared_button_font_color } from "./app_shared_button_font_color.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function html_style_background_color_set_or_remove_list(
   component,
@@ -6,5 +9,8 @@ export function html_style_background_color_set_or_remove_list(
   item,
 ) {
   let chosen = list_includes(list, item);
-  html_style_background_color_set_or_remove(chosen, component, "lightgreen");
+  let color = app_shared_button_selected_background_color();
+  html_style_background_color_set_or_remove(chosen, component, color);
+  let font = app_shared_button_font_color();
+  html_font_color_set_or_remove(chosen, component, font);
 }
