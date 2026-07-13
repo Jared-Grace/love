12f010416a8c65a5cc0dffdb2da9a356e702854e
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 21:58:15 +0800

    ai

--- js/app_code_flex_gap_value.mjs
+++ js/app_code_flex_gap_value.mjs
@@ -1,4 +1,5 @@
+import { app_shared_spaced_gap } from "./app_shared_spaced_gap.mjs";
 export function app_code_flex_gap_value() {
-  let r = "0.8em";
+  let r = app_shared_spaced_gap();
   return r;
 }
