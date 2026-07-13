4d22bd61a4eed547dbb81861e188e064401f9251
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 10:24:41 +0800

    ai

--- js/app_shared_refresh.mjs
+++ js/app_shared_refresh.mjs
@@ -1,5 +1,5 @@
 import { html_scroll_top_window } from "./html_scroll_top_window.mjs";
-import { function_name_combine } from "./function_name_combine.mjs";
+import { app_shared_home_name } from "./app_shared_home_name.mjs";
 import { storage_local_initialize_context } from "./storage_local_initialize_context.mjs";
 import { property_get } from "./property_get.mjs";
 import { html_clear } from "./html_clear.mjs";
@@ -7,11 +7,9 @@ import { html_document_body } from "./html_document_body.mjs";
 import { list_find_property } from "./list_find_property.mjs";
 export async function app_shared_refresh(context) {
   let screens = property_get(context, "screens");
-  let app_fn = property_get(context, "app_fn");
   let body = html_document_body();
   html_clear(body);
-  let value = property_get(app_fn, "name");
-  let combined = function_name_combine(value, "home");
+  let combined = app_shared_home_name(context);
   let screen_name = storage_local_initialize_context(
     context,
     "screen",
