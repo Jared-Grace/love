fb6b08e416764f72f344a72c3909a6b6c935937e
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 01:52:42 +0800

    ai

--- js/app_apps_all_main_fns_generate.mjs
+++ js/app_apps_all_main_fns_generate.mjs
@@ -1,10 +1,10 @@
-import { apps_names_prefixed } from "./apps_names_prefixed.mjs";
-import { data_identifiers_get_properties } from "./data_identifiers_get_properties.mjs";
-import { list_intersect_sort_text } from "./list_intersect_sort_text.mjs";
-import { function_list_generate } from "./function_list_generate.mjs";
+import { apps_names_prefixed } from "../../love/js/apps_names_prefixed.mjs";
+import { data_identifiers_get_properties } from "../../love/js/data_identifiers_get_properties.mjs";
+import { list_intersect_sort_text } from "../../love/js/list_intersect_sort_text.mjs";
+import { function_list_generate_open } from "../../love/js/function_list_generate_open.mjs";
 export async function app_apps_all_main_fns_generate() {
   let prefixed = await apps_names_prefixed();
   let identifier_names = await data_identifiers_get_properties();
   let apps = list_intersect_sort_text(prefixed, identifier_names);
-  await function_list_generate(app_apps_all_main_fns_generate, apps);
+  await function_list_generate_open(app_apps_all_main_fns_generate, apps);
 }
--- js/function_list_generate.mjs
+++ js/function_list_generate.mjs
@@ -1,14 +1,13 @@
-import { js_statement_return_insert_code } from "./js_statement_return_insert_code.mjs";
-import { property_get } from "./property_get.mjs";
-import { text_suffix_without } from "./text_suffix_without.mjs";
-import { function_open } from "./function_open.mjs";
-import { function_transform } from "./function_transform.mjs";
-import { json_to } from "./json_to.mjs";
-import { list_empty } from "./list_empty.mjs";
-import { js_flo_body } from "./js_flo_body.mjs";
-import { function_new_open } from "./function_new_open.mjs";
-import { not } from "./not.mjs";
-import { function_unalias_exists } from "./function_unalias_exists.mjs";
+import { function_transform } from "../../love/js/function_transform.mjs";
+import { js_statement_return_insert_code } from "../../love/js/js_statement_return_insert_code.mjs";
+import { json_to } from "../../love/js/json_to.mjs";
+import { list_empty } from "../../love/js/list_empty.mjs";
+import { js_flo_body } from "../../love/js/js_flo_body.mjs";
+import { function_new_open } from "../../love/js/function_new_open.mjs";
+import { not } from "../../love/js/not.mjs";
+import { property_get } from "../../love/js/property_get.mjs";
+import { function_unalias_exists } from "../../love/js/function_unalias_exists.mjs";
+import { text_suffix_without } from "../../love/js/text_suffix_without.mjs";
 export async function function_list_generate(f_generate, list) {
   let f_generate_name = f_generate.name;
   let f_name = text_suffix_without(f_generate_name, "_generate");
@@ -24,5 +23,5 @@ export async function function_list_generate(f_generate, list) {
     js_statement_return_insert_code(body_block, 0, code);
   }
   let output = await function_transform(f_name, lambda);
-  await function_open(f_name);
+  return f_name;
 }
