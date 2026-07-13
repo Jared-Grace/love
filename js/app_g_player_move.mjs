1f25df0cf7a1a43c4bf61d52568fcccb331027ac
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 16:30:23 +0800

    ai

--- js/app_g_player_move.mjs
+++ js/app_g_player_move.mjs
@@ -11,8 +11,8 @@ export async function app_g_player_move(
   let player = await app_g_player_get();
   let away = g_distance_at_least_1(player, coordinates_move_to);
   object_assign(player, coordinates_move_to);
+  app_g_player_center(coordinates_move_to, player_img_c, div_map);
   if (away) {
     await app_g_player_move_animate(coordinates_move_to, player_img_c);
   }
-  await app_g_player_center(div_map, player_img_c);
 }
