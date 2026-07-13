1e79df088c86d4dae48278a999ec2aecb9d349b2
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:49:52 +0800

    ai

--- js/list_empty_not_is_assert.mjs
+++ js/list_empty_not_is_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function list_empty_not_is_assert(list) {
   let ne = list_empty_not_is(list);
-  assert_message(
-    ne,
-    "This list was expected to have at least one item. Would you like to check what should have filled it?",
-  );
+  assert(ne);
 }
--- js/list_multiple_is_assert.mjs
+++ js/list_multiple_is_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
 export function list_multiple_is_assert(list) {
   let m = list_multiple_is(list);
-  assert_message(
-    m,
-    "This list was expected to hold more than one item. Would you like to check what filled it?",
-  );
+  assert(m);
 }
