b775555d3f5b460d76e7b306e22a8530c3b1b6b7
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 15:33:23 +0800

    ai

--- js/app_code_container_light_blue.mjs
+++ js/app_code_container_light_blue.mjs
@@ -7,11 +7,12 @@ import { html_border_none } from "./html_border_none.mjs";
 import { html_style_padding } from "./html_style_padding.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
+import { app_shared_container_plain_background_color } from "./app_shared_container_plain_background_color.mjs";
 export function app_code_container_light_blue(parent) {
   let c = html_div(parent);
   html_style_padding(c, "0.2em");
   html_border_none(c);
-  html_style_background_color_set(c, "rgb(228, 241, 255)");
+  html_style_background_color_set(c, app_shared_container_plain_background_color());
   let border_color = app_code_container_light_blue_border_color();
   html_border_y(c, "0.15em", border_color);
   html_style_margin_y(c, "0.5em");
--- js/app_shared_container_plain_border_color.mjs
+++ js/app_shared_container_plain_border_color.mjs
@@ -1,4 +1,4 @@
 export function app_shared_container_plain_border_color() {
-  let c = "#e2e2e2";
+  let c = "rgb(152, 203, 255)";
   return c;
 }
