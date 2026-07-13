281f4d100c6c78b025374aa96690ab0b18efb193
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 01:52:42 +0800

    ai

--- js/bible_verses_encouragement_generate.mjs
+++ js/bible_verses_encouragement_generate.mjs
@@ -1,17 +1,17 @@
-import { ebible_version_books_names } from "./ebible_version_books_names.mjs";
-import { folder_user_docs_read_lines } from "./folder_user_docs_read_lines.mjs";
-import { function_list_generate } from "./function_list_generate.mjs";
-import { list_filter_starts_with_any } from "./list_filter_starts_with_any.mjs";
-import { ebible_folder_english } from "./ebible_folder_english.mjs";
-import { list_map } from "./list_map.mjs";
-import { text_ends_with_space } from "./text_ends_with_space.mjs";
-import { text_trim_right } from "./text_trim_right.mjs";
-import { text_skip_while } from "./text_skip_while.mjs";
-import { each } from "./each.mjs";
-import { whitespace_normalize } from "./whitespace_normalize.mjs";
-import { text_starts_with_space } from "./text_starts_with_space.mjs";
-import { text_starts_with_dot } from "./text_starts_with_dot.mjs";
-import { text_starts_with_digit } from "./text_starts_with_digit.mjs";
+import { ebible_version_books_names } from "../../love/js/ebible_version_books_names.mjs";
+import { folder_user_docs_read_lines } from "../../love/js/folder_user_docs_read_lines.mjs";
+import { function_list_generate_open } from "../../love/js/function_list_generate_open.mjs";
+import { list_filter_starts_with_any } from "../../love/js/list_filter_starts_with_any.mjs";
+import { ebible_folder_english } from "../../love/js/ebible_folder_english.mjs";
+import { list_map } from "../../love/js/list_map.mjs";
+import { text_ends_with_space } from "../../love/js/text_ends_with_space.mjs";
+import { text_trim_right } from "../../love/js/text_trim_right.mjs";
+import { text_skip_while } from "../../love/js/text_skip_while.mjs";
+import { each } from "../../love/js/each.mjs";
+import { whitespace_normalize } from "../../love/js/whitespace_normalize.mjs";
+import { text_starts_with_space } from "../../love/js/text_starts_with_space.mjs";
+import { text_starts_with_dot } from "../../love/js/text_starts_with_dot.mjs";
+import { text_starts_with_digit } from "../../love/js/text_starts_with_digit.mjs";
 export async function bible_verses_encouragement_generate() {
   let split = await folder_user_docs_read_lines(
     "bible_references.hopenation.org.txt",
@@ -34,7 +34,7 @@ export async function bible_verses_encouragement_generate() {
   let bible_folder = ebible_folder_english();
   let mapped2 = await ebible_version_books_names(bible_folder);
   let verse_references = list_filter_starts_with_any(mapped, mapped2);
-  await function_list_generate(
+  await function_list_generate_open(
     bible_verses_encouragement_generate,
     verse_references,
   );
