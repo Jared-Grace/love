1bc22bdd5fa4ce113be290aa8778e449a4df9756
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:47:35 +0800

    ai

--- js/app_g_div_map_style.mjs
+++ js/app_g_div_map_style.mjs
@@ -2,6 +2,7 @@ import { app_g_rows_get } from "./app_g_rows_get.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_size } from "./list_size.mjs";
 import { html_style_assign } from "./html_style_assign.mjs";
+import { html_style_set } from "./html_style_set.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export async function app_g_div_map_style(div_map) {
   let rows = await app_g_rows_get();
