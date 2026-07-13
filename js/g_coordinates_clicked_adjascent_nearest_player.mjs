6af2546aeb6e0e073107cd08fdf89f8e98a63447
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 15:13:42 +0800

    ai

--- js/g_coordinates_clicked_adjascent_nearest_player.mjs
+++ js/g_coordinates_clicked_adjascent_nearest_player.mjs
@@ -1,3 +1,4 @@
+import { app_g_game_save_get } from "./app_g_game_save_get.mjs";
 import { app_g_player_get } from "./app_g_player_get.mjs";
 import { list_shuffle_sort_number_mapper_first } from "./list_shuffle_sort_number_mapper_first.mjs";
 import { g_distance_curried } from "./g_distance_curried.mjs";
@@ -7,8 +8,9 @@ export async function g_coordinates_clicked_adjascent_nearest_player(
   coordinates_move_to,
 ) {
   "find the coordinates next to the npc that is nearest to the player";
-  let g = await app_g_player_get();
+  let g = await app_g_game_save_get();
   let nearby = g_coordinates_adjascent(g, clicked_coordinates);
+  let player = await app_g_player_get();
   let lambda = g_distance_curried(player);
   coordinates_move_to = list_shuffle_sort_number_mapper_first(nearby, lambda);
   return coordinates_move_to;
