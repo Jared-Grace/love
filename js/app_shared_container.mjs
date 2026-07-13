f0668e56761ff357db0bd99d6c13e6e2e61863cb
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:26:27 +0800

    ai

--- js/app_shared_container.mjs
+++ js/app_shared_container.mjs
@@ -1,14 +1,10 @@
 import { app_shared_container_background_color } from "./app_shared_container_background_color.mjs";
-import { html_card } from "./html_card.mjs";
-import { html_style_margin_y } from "./html_style_margin_y.mjs";
+import { app_shared_container_base } from "./app_shared_container_base.mjs";
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
-import { html_div } from "./html_div.mjs";
 import { text_combine } from "./text_combine.mjs";
 export function app_shared_container(parent) {
-  let div = html_div(parent);
-  html_card(div);
+  let div = app_shared_container_base(parent);
   let color = text_combine(app_shared_container_background_color(), "ff");
   html_style_background_color_set(div, color);
-  html_style_margin_y(div, "10px");
   return div;
 }
