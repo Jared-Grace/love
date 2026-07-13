dbaa38c9bd991069e406b0f34b96a165cde98ef9
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 12:58:57 +0800

    ai

--- js/list_empty_not_is_assert.mjs
+++ js/list_empty_not_is_assert.mjs
@@ -2,5 +2,5 @@ import { assert_message } from "./assert_message.mjs";
 import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export function list_empty_not_is_assert(list) {
   let ne = list_empty_not_is(list);
-  assert(ne);
+  assert_message(ne, "This list was expected to have at least one item. Would you like to check what should have filled it?");
 }
