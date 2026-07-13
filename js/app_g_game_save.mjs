cf755f95b40a3d24167b49026b02a4e4e203a924
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 11:14:31 +0800

    ai

--- js/app_g_game_save.mjs
+++ js/app_g_game_save.mjs
@@ -1,9 +1,9 @@
 import { file_overwrite_json } from "./file_overwrite_json.mjs";
 import { app_g_game_save_get } from "./app_g_game_save_get.mjs";
-import { global_function_property_delete } from "./global_function_property_delete.mjs";
+import { global_function_property_set } from "./global_function_property_set.mjs";
 import { app_g_game_save_path } from "./app_g_game_save_path.mjs";
 export async function app_g_game_save(g) {
   let p = app_g_game_save_path();
   await file_overwrite_json(p, g);
-  global_function_property_delete(app_g_game_save_get, p);
+  global_function_property_set(app_g_game_save_get, p, g);
 }
