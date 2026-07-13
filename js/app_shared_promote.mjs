14c66e335f6246aa2f2cca373acb99b32ccffed7
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 01:54:31 +0800

    ai

--- js/app_shared_promote.mjs
+++ js/app_shared_promote.mjs
@@ -1,7 +1,7 @@
 import { app_shared_name_search_main } from "./app_shared_name_search_main.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { firebase_promote_function } from "./firebase_promote_function.mjs";
-export async function app_promote(name) {
+export async function app_shared_promote(name) {
   arguments_assert(arguments, 1);
   let combined = await app_shared_name_search_main(name);
   await firebase_promote_function(combined);
--- js/firebase_deploy_function_app_main_promote.mjs
+++ js/firebase_deploy_function_app_main_promote.mjs
@@ -1,6 +1,6 @@
-import { app_promote } from "./app_promote.mjs";
-import { firebase_deploy_function_app_main } from "./firebase_deploy_function_app_main.mjs";
+import { app_shared_promote } from "../../love/js/app_shared_promote.mjs";
+import { firebase_deploy_function_app_main } from "../../love/js/firebase_deploy_function_app_main.mjs";
 export async function firebase_deploy_function_app_main_promote(a) {
   await firebase_deploy_function_app_main(a);
-  await app_promote(a);
+  await app_shared_promote(a);
 }
