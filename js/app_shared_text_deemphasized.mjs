d610b5e31e6042f7b642fa9c2ec78580117d2041
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 02:52:46 +0800

    ai

--- js/app_shared_text_deemphasized.mjs
+++ js/app_shared_text_deemphasized.mjs
@@ -2,6 +2,6 @@ import { html_style_font_size } from "./html_style_font_size.mjs";
 import { html_font_color_set } from "./html_font_color_set.mjs";
 import { app_shared_text_deemphasized_color } from "./app_shared_text_deemphasized_color.mjs";
 export function app_shared_text_deemphasized(component) {
-  html_style_font_size(component, "0.85em");
+  html_style_font_size(component, "0.95em");
   html_font_color_set(component, app_shared_text_deemphasized_color());
 }
