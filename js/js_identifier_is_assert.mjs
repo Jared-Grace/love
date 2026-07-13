092bfbe221f031ae4abee7e1ba8eef286543987e
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:47:35 +0800

    ai

--- js/js_identifier_is_assert.mjs
+++ js/js_identifier_is_assert.mjs
@@ -1,4 +1,4 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
 export function js_identifier_is_assert(expression) {
   let ii = js_identifier_is(expression);
--- js/less_than_equal_assert.mjs
+++ js/less_than_equal_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { less_than_equal } from "./less_than_equal.mjs";
 export function less_than_equal_assert(a, b) {
   let le = less_than_equal(a, b);
-  assert_message(
-    le,
-    "The first value was expected to be less than or equal to the second. Would you like to check their order?",
-  );
+  assert(le);
 }
