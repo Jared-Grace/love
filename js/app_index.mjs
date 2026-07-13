543bf4e1319150793e504fee048beab9ce57158e
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:35:51 +0800

    ai

--- js/app_index.mjs
+++ js/app_index.mjs
@@ -2,9 +2,8 @@ import { app_index_main_fns } from "./app_index_main_fns.mjs";
 import { app_prefix_without } from "./app_prefix_without.mjs";
 import { html_mobile_default } from "./html_mobile_default.mjs";
 import { html_div_text_centered } from "./html_div_text_centered.mjs";
-import { app_shared_container } from "./app_shared_container.mjs";
+import { app_shared_container_plain } from "./app_shared_container_plain.mjs";
 import { app_replace_button_wide } from "./app_replace_button_wide.mjs";
-import { app_shared_button_screen_green_style_assign } from "./app_shared_button_screen_green_style_assign.mjs";
 import { window_open_app_curried_right } from "./window_open_app_curried_right.mjs";
 import { each } from "./each.mjs";
 import { property_get } from "./property_get.mjs";
@@ -20,9 +19,8 @@ export function app_index(context) {
     function lambda2() {
       lambda(fn);
     }
-    let card = app_shared_container(root);
+    let card = app_shared_container_plain(root);
     let component = app_replace_button_wide(card, without, lambda2);
-    app_shared_button_screen_green_style_assign(component);
     let text = property_get(a, "text");
     let div = html_div_text_centered(card, text);
   }
---
+++ js/app_shared_container_plain_border_color.mjs
@@ -0,0 +1,4 @@
+export function app_shared_container_plain_border_color() {
+  let c = "#e2e2e2";
+  return c;
+}
