3cb5e4eb05dda487b9e32dce0fc0c2bb248bb920
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 15:41:13 +0800

    ai

--- js/app_index.mjs
+++ js/app_index.mjs
@@ -1,28 +1,6 @@
 import { app_index_main_fns } from "./app_index_main_fns.mjs";
-import { app_prefix_without } from "./app_prefix_without.mjs";
-import { html_mobile_default } from "./html_mobile_default.mjs";
-import { html_div_text_centered } from "./html_div_text_centered.mjs";
-import { app_shared_container_plain } from "./app_shared_container_plain.mjs";
-import { app_replace_button_wide } from "./app_replace_button_wide.mjs";
-import { window_open_app_curried_right } from "./window_open_app_curried_right.mjs";
-import { each } from "./each.mjs";
-import { property_get } from "./property_get.mjs";
+import { app_index_generic } from "./app_index_generic.mjs";
 export function app_index(context) {
-  let root = property_get(context, "root");
-  html_mobile_default(context);
-  let fns = app_index_main_fns();
-  let hash = {};
-  let lambda = window_open_app_curried_right(hash);
-  function lambda3(a) {
-    let fn = property_get(a, "app_fn");
-    let without = app_prefix_without(fn);
-    function lambda2() {
-      lambda(fn);
-    }
-    let card = app_shared_container_plain(root);
-    let component = app_replace_button_wide(card, without, lambda2);
-    let text = property_get(a, "text");
-    let div = html_div_text_centered(card, text);
-  }
-  each(fns, lambda3);
+  let entries = app_index_main_fns();
+  app_index_generic(context, entries);
 }
