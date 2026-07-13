f856d28cb8b2319bf04280d99a5be4790b0c8f9a
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 20:23:21 +0800

    ai

--- js/app_reply_button.mjs
+++ js/app_reply_button.mjs
@@ -1,15 +1,20 @@
-import { html_style_background_color_set_or_remove_list } from "./html_style_background_color_set_or_remove_list.mjs";
 import { html_button } from "./html_button.mjs";
 import { app_shared_style_control_inline } from "./app_shared_style_control_inline.mjs";
+import { app_shared_button_toggle_style } from "./app_shared_button_toggle_style.mjs";
+import { list_includes } from "./list_includes.mjs";
 import { list_toggle } from "./list_toggle.mjs";
 export function app_reply_button(list, item, root, text) {
   let component = null;
+  function style() {
+    let chosen = list_includes(list, item);
+    app_shared_button_toggle_style(chosen, component);
+  }
   function lambda() {
     list_toggle(list, item);
-    html_style_background_color_set_or_remove_list(component, list, item);
+    style();
   }
   component = html_button(root, text, lambda);
   app_shared_style_control_inline(component);
-  html_style_background_color_set_or_remove_list(component, list, item);
+  style();
   return component;
 }
