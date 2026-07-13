155f33c83ec5433ae6b25c7d4b484007b1a66ca0
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 21:58:15 +0800

    ai

---
+++ js/app_shared_spaced.mjs
@@ -0,0 +1,5 @@
+import { html_flex_column_gap } from "./html_flex_column_gap.mjs";
+import { app_shared_spaced_gap } from "./app_shared_spaced_gap.mjs";
+export function app_shared_spaced(container) {
+  html_flex_column_gap(container, app_shared_spaced_gap());
+}
