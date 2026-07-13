50034425aefbd1007d87d625d60268421a18e64e
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 13:00:46 +0800

    ai

--- js/list_includes_assert.mjs
+++ js/list_includes_assert.mjs
@@ -1,6 +1,6 @@
-import { assert } from "./assert.mjs";
+import { assert_message } from "./assert_message.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function list_includes_assert(all, repo_name) {
   let includes = list_includes(all, repo_name);
-  assert(includes);
+  assert_message(includes, "This list was expected to include that value. Would you like to check what's inside it?");
 }
