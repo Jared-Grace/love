d54f25e5e5588efec37c5a71447f6b2023e1faa9
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:22:15 +0800

    ai

--- js/js_list_add_call_try.mjs
+++ js/js_list_add_call_try.mjs
@@ -1,14 +1,14 @@
-import { js_expression_statement_is } from "./js_expression_statement_is.mjs";
-import { not } from "./not.mjs";
-import { js_statement_expression_get } from "./js_statement_expression_get.mjs";
-import { js_call_callee_name_try } from "./js_call_callee_name_try.mjs";
-import { list_add } from "./list_add.mjs";
-import { equal } from "./equal.mjs";
-import { js_call_arguments_get } from "./js_call_arguments_get.mjs";
-import { list_size_2 } from "./list_size_2.mjs";
-import { list_first } from "./list_first.mjs";
-import { js_identifier_is } from "./js_identifier_is.mjs";
-import { list_second } from "./list_second.mjs";
+import { js_expression_statement_is } from "../../love/js/js_expression_statement_is.mjs";
+import { not } from "../../love/js/not.mjs";
+import { js_statement_expression_get } from "../../love/js/js_statement_expression_get.mjs";
+import { js_call_callee_name_try } from "../../love/js/js_call_callee_name_try.mjs";
+import { list_add } from "../../love/js/list_add.mjs";
+import { equal } from "../../love/js/equal.mjs";
+import { js_call_arguments_get } from "../../love/js/js_call_arguments_get.mjs";
+import { list_size_2 } from "../../love/js/list_size_2.mjs";
+import { list_first } from "../../love/js/list_first.mjs";
+import { js_identifier_is } from "../../love/js/js_identifier_is.mjs";
+import { list_second } from "../../love/js/list_second.mjs";
 export function js_list_add_call_try(node) {
   let esi = js_expression_statement_is(node);
   if (not(esi)) {
@@ -21,8 +21,8 @@ export function js_list_add_call_try(node) {
     return null;
   }
   let args = js_call_arguments_get(expression);
-  let s2 = list_size_2(args);
-  if (not(s2)) {
+  let s = list_size_2(args);
+  if (not(s)) {
     return null;
   }
   let list = list_first(args);
