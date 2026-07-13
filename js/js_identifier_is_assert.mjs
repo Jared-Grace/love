767258f79ec065ed9fbf28c599c8e42e66f7deb2
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 12:57:43 +0800

    ai

--- js/js_identifier_is_assert.mjs
+++ js/js_identifier_is_assert.mjs
@@ -2,5 +2,5 @@ import { assert_message } from "./assert_message.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
 export function js_identifier_is_assert(expression) {
   let ii = js_identifier_is(expression);
-  assert(ii);
+  assert_message(ii, "This expression was expected to be an identifier. Would you like to check what kind of node it is?");
 }
--- js/throws_assert.mjs
+++ js/throws_assert.mjs
@@ -1,10 +1,10 @@
 import { property_get } from "./property_get.mjs";
 import { lambda_throws } from "./lambda_throws.mjs";
-import { assert } from "./assert.mjs";
+import { assert_message } from "./assert_message.mjs";
 export function throws_assert(lambda) {
   let r = lambda_throws(lambda);
   let result = property_get(r, "result");
   let throws = property_get(r, "throws");
-  assert(throws);
+  assert_message(throws, "This code was expected to throw, but it finished peacefully. Should it be throwing here?");
   return result;
 }
