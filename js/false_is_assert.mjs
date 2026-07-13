553d8928aca6094b817a5f596798c266a31b80c4
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 12:58:57 +0800

    ai

--- js/false_is_assert.mjs
+++ js/false_is_assert.mjs
@@ -1,6 +1,6 @@
 import { false_is } from "./false_is.mjs";
-import { assert } from "./assert.mjs";
+import { assert_message } from "./assert_message.mjs";
 export function false_is_assert(b) {
   let ti = false_is(b);
-  assert(ti);
+  assert_message(ti, "This value was expected to be false. Would you like to check where it's set?");
 }
