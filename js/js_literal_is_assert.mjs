64733b874703998f5398bb2b19cdec7706a69a2c
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:50:01 +0800

    ai

--- js/js_literal_is_assert.mjs
+++ js/js_literal_is_assert.mjs
@@ -1,4 +1,4 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { js_literal_is } from "./js_literal_is.mjs";
 export function js_literal_is_assert(node) {
   let li = js_literal_is(node);
