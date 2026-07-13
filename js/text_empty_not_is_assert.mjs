7eeff42b0e3518b7628df488cd87a304f4e8a26e
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:51:00 +0800

    ai

--- js/text_empty_not_is_assert.mjs
+++ js/text_empty_not_is_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { text_empty_not_is } from "./text_empty_not_is.mjs";
 export function text_empty_not_is_assert(s) {
   let ne = text_empty_not_is(s);
-  assert_message(
-    ne,
-    "This text was expected to be non-empty. Would you like to check where it comes from?",
-  );
+  assert(ne);
 }
