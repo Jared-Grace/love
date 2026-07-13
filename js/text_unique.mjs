bafb23531c84056607d6e544f929e06179ba0dbb
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 18:40:08 +0800

    ai

--- js/text_unique.mjs
+++ js/text_unique.mjs
@@ -1,16 +1,16 @@
-import { list_includes } from "./list_includes.mjs";
-import { text_empty_is } from "./text_empty_is.mjs";
-import { text_empty_not_is } from "./text_empty_not_is.mjs";
-import { text_is_assert } from "./text_is_assert.mjs";
-import { list_all } from "./list_all.mjs";
-import { text_combine } from "./text_combine.mjs";
+import { text_is_assert_multiple } from "../../love/js/text_is_assert_multiple.mjs";
+import { equal } from "../../love/js/equal.mjs";
+import { list_includes } from "../../love/js/list_includes.mjs";
+import { text_empty_is } from "../../love/js/text_empty_is.mjs";
+import { text_empty_not_is } from "../../love/js/text_empty_not_is.mjs";
+import { text_combine } from "../../love/js/text_combine.mjs";
 export function text_unique(used, name, prefix) {
   text_is_assert_multiple(used);
   let unique = null;
   let attempt = 1;
   do {
     let suffix =
-      attempt === 1 && text_empty_not_is(name)
+      equal(attempt, 1) && text_empty_not_is(name)
         ? ""
         : text_combine(prefix, attempt);
     unique = text_combine(name, suffix);
