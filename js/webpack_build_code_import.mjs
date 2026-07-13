673524c651172ce5165bf71138f23bb52d98f3a8
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 02:20:25 +0800

    ai

--- js/webpack_build_code_import.mjs
+++ js/webpack_build_code_import.mjs
@@ -1,10 +1,16 @@
 import { function_name_to_path } from "./function_name_to_path.mjs";
+import { function_name_to_path_search } from "./function_name_to_path_search.mjs";
+import { property_get } from "./property_get.mjs";
+import { repo_path_combine } from "./repo_path_combine.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 import { folder_previous_2_join } from "./folder_previous_2_join.mjs";
-export function webpack_build_code_import(f_name) {
+export async function webpack_build_code_import(f_name) {
+  let search = await function_name_to_path_search(f_name);
+  let repo_name = property_get(search, "repo_name");
   let path = function_name_to_path(f_name);
-  let joined = folder_previous_2_join(path);
+  let with_repo = repo_path_combine(repo_name, path);
+  let joined = folder_previous_2_join(with_repo);
   let code_string = js_code_string(joined);
   let i = js_code_import_single(f_name, code_string);
   return i;
--- js/webpack_build_generic_source.mjs
+++ js/webpack_build_generic_source.mjs
@@ -3,11 +3,11 @@ import { list_join_newline } from "./list_join_newline.mjs";
 import { list_add } from "./list_add.mjs";
 import { js_code_call_app_context_initialize } from "./js_code_call_app_context_initialize.mjs";
 import { webpack_build_code_import } from "./webpack_build_code_import.mjs";
-import { list_map } from "./list_map.mjs";
-export function webpack_build_generic_source(f_name) {
+import { list_map_async } from "./list_map_async.mjs";
+export async function webpack_build_generic_source(f_name) {
   let fns = webpack_build_generic_source_fn_names(f_name);
   let call = js_code_call_app_context_initialize(f_name);
-  let mapped = list_map(fns, webpack_build_code_import);
+  let mapped = await list_map_async(fns, webpack_build_code_import);
   list_add(mapped, call);
   let joined = list_join_newline(mapped);
   return joined;
