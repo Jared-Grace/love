041e1be4cf13159f80cff5f23a3132459e5471ec
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 11:20:26 +0800

    ai

--- js/app_g_player_coordinates_update_move.mjs
+++ js/app_g_player_coordinates_update_move.mjs
@@ -1,6 +1,7 @@
 import { object_assign } from "./object_assign.mjs";
 import { app_g_player_move } from "./app_g_player_move.mjs";
 import { app_g_player_coordinates_update } from "./app_g_player_coordinates_update.mjs";
+import { app_g_player_get } from "./app_g_player_get.mjs";
 export async function app_g_player_coordinates_update_move(
   npc_clicked,
   clicked_coordinates,
@@ -12,5 +13,6 @@ export async function app_g_player_coordinates_update_move(
     clicked_coordinates,
   );
   await app_g_player_move(coordinates_move_to, player_img_c, div_map);
+  let player = await app_g_player_get();
   object_assign(player, coordinates_move_to);
 }
