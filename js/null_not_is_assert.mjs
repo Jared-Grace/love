4facd19483e681184f1e95dea1b6f46de98cee50
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:51:00 +0800

    ai

--- js/null_not_is_assert.mjs
+++ js/null_not_is_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 export function null_not_is_assert(a) {
   let nn = null_not_is(a);
-  assert_message(
-    nn,
-    "This value was expected not to be null. Would you like to check where it should have been set?",
-  );
+  assert(nn);
 }
--- js/positive_is_assert.mjs
+++ js/positive_is_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { positive_is } from "./positive_is.mjs";
 export function positive_is_assert(n) {
   let p = positive_is(n);
-  assert_message(
-    p,
-    "This value was expected to be positive. Would you like to check how it was computed?",
-  );
+  assert(p);
 }
