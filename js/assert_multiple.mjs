5cb3d234f20a0514922ed9a26dc888e457741acc
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 13:01:53 +0800

    ai

--- js/assert_multiple.mjs
+++ js/assert_multiple.mjs
@@ -1,9 +1,9 @@
 import { each } from "./each.mjs";
-import { assert } from "./assert.mjs";
+import { assert_message } from "./assert_message.mjs";
 export function assert_multiple(fn, list) {
   function lambda(l) {
     let ii = fn(l);
-    assert(ii);
+    assert_message(ii, "One of these items didn't pass the check. Would you like to look at which one fell short?");
   }
   each(list, lambda);
 }
