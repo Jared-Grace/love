bd1dec024f8d46c74ffaa3be97a85b1d16013506
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 02:42:06 +0800

    ai

--- js/html_bar_content_padded.mjs
+++ js/html_bar_content_padded.mjs
@@ -1,9 +1,9 @@
 import { property_get } from "./property_get.mjs";
 import { html_bar_content } from "./html_bar_content.mjs";
-import { html_bar_content_padding } from "./html_bar_content_padding.mjs";
+import { html_page_padding_x } from "./html_page_padding_x.mjs";
 export function html_bar_content_padded(root) {
   let bc = html_bar_content(root);
   let content = property_get(bc, "content");
-  html_bar_content_padding(content);
+  html_page_padding_x(content);
   return bc;
 }
