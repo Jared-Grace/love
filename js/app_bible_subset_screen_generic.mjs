7d7de42f760f9de8f0c741945270a94f35214ef8
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 16:08:01 +0800

    ai

--- js/app_bible_subset_screen_generic.mjs
+++ js/app_bible_subset_screen_generic.mjs
@@ -1,4 +1,5 @@
 import { html_clear_context } from "./html_clear_context.mjs";
+import { html_centered } from "./html_centered.mjs";
 import { app_replace_button_back } from "./app_replace_button_back.mjs";
 import { app_shared_screen_set_home } from "./app_shared_screen_set_home.mjs";
 import { html_subset_ordered_choose } from "./html_subset_ordered_choose.mjs";
@@ -13,6 +14,7 @@ export function app_bible_subset_screen_generic(
   order_label,
 ) {
   let root = html_clear_context(context);
+  html_centered(root);
   function lambda_back() {
     app_shared_screen_set_home(context);
   }
