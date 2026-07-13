543f9b865903b98355ec15e6fb4061ed6a53ee8c
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 10:24:41 +0800

    ai

--- js/app_bible_languages.mjs
+++ js/app_bible_languages.mjs
@@ -3,13 +3,12 @@ import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { ebible_languages } from "./ebible_languages.mjs";
 import { html_clear_context } from "./html_clear_context.mjs";
 import { app_replace_button_back } from "./app_replace_button_back.mjs";
-import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
-import { app_bible_home } from "./app_bible_home.mjs";
+import { app_shared_screen_set_home } from "./app_shared_screen_set_home.mjs";
 import { app_bible_languages_chosen_get } from "./app_bible_languages_chosen_get.mjs";
 export function app_bible_languages(context) {
   let root = html_clear_context(context);
   function lambda_back() {
-    app_shared_screen_set(context, app_bible_home);
+    app_shared_screen_set_home(context);
   }
   app_replace_button_back(root, lambda_back);
   let languages = ebible_languages();
