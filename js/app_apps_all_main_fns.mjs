ad5e9d09e61fabea628d9b6f39ec755477f4a2b4
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 15:17:25 +0800

    ai

---
+++ js/app_apps_all_main_fns.mjs
@@ -0,0 +1,30 @@
+import { fn_name } from "./fn_name.mjs";
+export function app_apps_all_main_fns() {
+  let r = [
+    fn_name("app_reply"),
+    fn_name("app_search"),
+    fn_name("app_bible"),
+    fn_name("app_original_bible"),
+    fn_name("app_ceb_bible"),
+    fn_name("app_supper"),
+    fn_name("app_g"),
+    fn_name("app_replace"),
+    fn_name("app_designs_universal"),
+    fn_name("app_g_bible"),
+    fn_name("app_a"),
+    fn_name("app_autopray"),
+    fn_name("app_calendar"),
+    fn_name("app_calendar_paste"),
+    fn_name("app_chapter"),
+    fn_name("app_code"),
+    fn_name("app_granddaddy"),
+    fn_name("app_index"),
+    fn_name("app_karate"),
+    fn_name("app_message"),
+    fn_name("app_next"),
+    fn_name("app_reply_local"),
+    fn_name("app_sandbox"),
+    fn_name("app_supper_tl"),
+  ];
+  return r;
+}
