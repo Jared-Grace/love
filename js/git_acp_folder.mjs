954fe506070d649bd8867154a70d858121055be7
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 11:34:17 +0800

    ai

--- js/git_acp_folder.mjs
+++ js/git_acp_folder.mjs
@@ -1,6 +1,10 @@
 import { git_ac_folder } from "./git_ac_folder.mjs";
 import { git_push_folder } from "./git_push_folder.mjs";
+import { promise_later } from "./promise_later.mjs";
 export async function git_acp_folder(folder, message) {
   await git_ac_folder(folder, message);
-  await git_push_folder(folder);
+  function push_later() {
+    return git_push_folder(folder);
+  }
+  promise_later(push_later);
 }
