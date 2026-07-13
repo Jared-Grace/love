7f5b1c61e54ba99fb82d6eb1b6e4709fe1ccdf67
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 02:19:42 +0800

    ai

--- js/app_apps_all_main_fns_generate.mjs
+++ js/app_apps_all_main_fns_generate.mjs
@@ -3,8 +3,9 @@ import { data_identifiers_get_properties } from "../../love/js/data_identifiers_
 import { list_intersect_sort_text } from "../../love/js/list_intersect_sort_text.mjs";
 import { function_list_generate_open } from "../../love/js/function_list_generate_open.mjs";
 export async function app_apps_all_main_fns_generate() {
+   generate_fn=function_list_generate_open
   let prefixed = await apps_names_prefixed();
   let identifier_names = await data_identifiers_get_properties();
   let apps = list_intersect_sort_text(prefixed, identifier_names);
-  await function_list_generate_open(app_apps_all_main_fns_generate, apps);
+  await generate_fn(app_apps_all_main_fns_generate, apps);
 }
