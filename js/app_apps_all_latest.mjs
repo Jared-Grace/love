b7d8c8fa6777c8792fe4ac49c7c7e66ff9f080aa
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 14:47:35 +0800

    ai

--- js/app_apps_all_latest.mjs
+++ js/app_apps_all_latest.mjs
@@ -1,3 +1,4 @@
+import { app_main_latest } from "./app_main_latest.mjs";
 export async function app_apps_all_latest() {
   await app_main_latest("app_apps_all", "jared-grace");
 }
--- js/null_is_assert.mjs
+++ js/null_is_assert.mjs
@@ -1,9 +1,6 @@
-import { assert_message } from "./assert_message.mjs";
+import { assert } from "./assert.mjs";
 import { null_is } from "./null_is.mjs";
 export function null_is_assert(value) {
   let n = null_is(value);
-  assert_message(
-    n,
-    "This value was hoping to be null, but it's holding something else. Would you like to check where it gets set?",
-  );
+  assert(n);
 }
