b2167631f9e6f592fc2a61800ddb8304f091002a
JESUS rose to life <username@users.noreply.github.com>
2026-07-13 21:23:53 +0800

    ai

--- js/js_strings_add_reference_to_fn_names_generic.mjs
+++ js/js_strings_add_reference_to_fn_names_generic.mjs
@@ -1,4 +1,4 @@
-import { js_strings_dot_name_skip_nodes } from "./js_strings_dot_name_skip_nodes.mjs";
+import { js_strings_add_reference_skip_nodes } from "./js_strings_add_reference_skip_nodes.mjs";
 import { js_statement_expression_nodes } from "./js_statement_expression_nodes.mjs";
 import { text_identifier_segments } from "./text_identifier_segments.mjs";
 import { property_get } from "./property_get.mjs";
@@ -6,11 +6,11 @@ import { list_includes } from "./list_includes.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
 import { js_code_fn_names_pieces } from "./js_code_fn_names_pieces.mjs";
-import { js_fn_names_pieces_replace_code } from "./js_fn_names_pieces_replace_code.mjs";
+import { js_add_reference_pieces_replace_code } from "./js_add_reference_pieces_replace_code.mjs";
 import { js_parse_expression_replace } from "./js_parse_expression_replace.mjs";
 import { js_strings_replace_generic } from "./js_strings_replace_generic.mjs";
 export async function js_strings_add_reference_to_fn_names_generic(ast, fn_names) {
-  let skip = js_strings_dot_name_skip_nodes(ast);
+  let skip = js_strings_add_reference_skip_nodes(ast);
   let statement_nodes = js_statement_expression_nodes(ast);
   function replace_try(value, node) {
     let segments = text_identifier_segments(value);
@@ -27,7 +27,7 @@ export async function js_strings_add_reference_to_fn_names_generic(ast, fn_names
     }
     let pieces = js_code_fn_names_pieces(segments, fn_names);
     let statement = list_includes(statement_nodes, node);
-    let code = js_fn_names_pieces_replace_code(pieces, statement);
+    let code = js_add_reference_pieces_replace_code(pieces, statement);
     js_parse_expression_replace(code, node);
     return true;
   }
