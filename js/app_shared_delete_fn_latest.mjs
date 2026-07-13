114d17d02550a92ff9a80e83dd3a5208e83f8b61
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 01:28:46 +0800

    ai

--- js/app_shared_delete_fn_latest.mjs
+++ js/app_shared_delete_fn_latest.mjs
@@ -1,6 +1,6 @@
 import { app_shared_name_latest } from "./app_shared_name_latest.mjs";
 import { function_delete } from "./function_delete.mjs";
-export async function app_delete_fn_latest(name) {
+export async function app_shared_delete_fn_latest(name) {
   let combined = app_shared_name_latest(name);
   await function_delete(combined);
 }
