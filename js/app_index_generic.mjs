3f788debd853d8139ada860ab88f97e69f5f255d
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 06:31:19 +0800

    ai

--- js/app_index_generic.mjs
+++ js/app_index_generic.mjs
@@ -3,8 +3,6 @@ import { html_mobile_default } from "./html_mobile_default.mjs";
 import { html_div_text_centered } from "./html_div_text_centered.mjs";
 import { app_shared_container_blue } from "./app_shared_container_blue.mjs";
 import { app_replace_button_wide } from "./app_replace_button_wide.mjs";
-import { html_style_font_size } from "./html_style_font_size.mjs";
-import { app_shared_font_size_small } from "./app_shared_font_size_small.mjs";
 import { window_open_app_curried_right } from "./window_open_app_curried_right.mjs";
 import { text_empty_not_is } from "./text_empty_not_is.mjs";
 import { each } from "./each.mjs";
@@ -22,7 +20,6 @@ export function app_index_generic(context, entries) {
     }
     let card = app_shared_container_blue(root);
     let component = app_replace_button_wide(card, without, lambda2);
-    html_style_font_size(component, app_shared_font_size_small());
     let text = property_get(entry, "text");
     let has_text = text_empty_not_is(text);
     if (has_text) {
