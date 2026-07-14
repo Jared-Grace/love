5946000aaa0b572f07d1d0690f9454d9face1d1d
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 06:09:54 +0800

    ai

--- js/html_mobile_default_font_size.mjs
+++ js/html_mobile_default_font_size.mjs
@@ -1,4 +1,4 @@
 import { html_style_font_size } from "./html_style_font_size.mjs";
 export function html_mobile_default_font_size(root) {
-  html_style_font_size(root, "25px");
+  html_style_font_size(root, "20px");
 }
