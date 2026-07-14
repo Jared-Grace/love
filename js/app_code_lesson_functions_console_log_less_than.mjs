4e897e2cb75a2de1cdf716eb729cb6b948464a39
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 13:22:24 +0800

    ai

---
+++ js/app_code_lesson_functions_console_log_less_than.mjs
@@ -0,0 +1,61 @@
+import { html_div_code } from "./html_div_code.mjs";
+import { each } from "./each.mjs";
+import { app_code_lesson_operators_value_max_range_1_next } from "./app_code_lesson_operators_value_max_range_1_next.mjs";
+import { identity } from "./identity.mjs";
+import { app_code_lesson_functions_console_log_generic } from "./app_code_lesson_functions_console_log_generic.mjs";
+import { js_operator_less_than } from "./js_operator_less_than.mjs";
+import { list_map } from "./list_map.mjs";
+import { js_operator_to_expression_only } from "./js_operator_to_expression_only.mjs";
+import { list_iterator_refillable } from "./list_iterator_refillable.mjs";
+import { js_code_call_arg } from "./js_code_call_arg.mjs";
+import { js_code_statement } from "./js_code_statement.mjs";
+import { html_div_cycle_code } from "./html_div_cycle_code.mjs";
+import { app_code_container_light_blue } from "./app_code_container_light_blue.mjs";
+import { app_code_container_light_blue_cycle_code } from "./app_code_container_light_blue_cycle_code.mjs";
+import { property_get } from "./property_get.mjs";
+export function app_code_lesson_functions_console_log_less_than() {
+  let operators = [js_operator_less_than()];
+  let number_next = app_code_lesson_operators_value_max_range_1_next();
+  function refill() {
+    function mapper(o) {
+      let expression = js_operator_to_expression_only(o, number_next);
+      return expression;
+    }
+    let list = list_map(operators, mapper);
+    return list;
+  }
+  let next_arg = list_iterator_refillable(refill);
+  var r = app_code_lesson_functions_console_log_generic(
+    above,
+    identity,
+    [" less than"],
+    next_arg,
+  );
+  let fn_name = property_get(r, "fn_name");
+  let lesson = property_get(r, "lesson");
+  return lesson;
+  function above(root) {
+    let c = app_code_container_light_blue(root);
+    html_div_cycle_code(c, [
+      "The symbol ",
+      "<",
+      " asks a question: is the left number smaller than the right number?",
+    ]);
+    html_div_cycle_code(c, ["For example: "]);
+    function lambda(o) {
+      let e = js_operator_to_expression_only(o, number_next);
+      let call = js_code_call_arg(fn_name, e);
+      let s = js_code_statement(call);
+      html_div_code(c, s);
+    }
+    each(operators, lambda);
+    app_code_container_light_blue_cycle_code(root, [
+      "Before, the answer was always a number. Now ",
+      fn_name,
+      " will write out ",
+      "true",
+      " or ",
+      "false",
+    ]);
+  }
+}
--- js/js_operator_less_than.mjs
+++ js/js_operator_less_than.mjs
@@ -1,10 +1,12 @@
 import { less_than } from "./less_than.mjs";
+import { identity } from "./identity.mjs";
 export function js_operator_less_than() {
   let operator = "<";
   let fn = less_than;
   let o = {
     fn,
     operator,
+    left_transform: identity,
   };
   return o;
 }
