dc126856b1823f6386f3eec0266e666c4912b640
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 21:05:38 +0800

    ai

--- js/app_replace_button_symbol_style_inner.mjs
+++ js/app_replace_button_symbol_style_inner.mjs
@@ -2,9 +2,10 @@ import { html_style_padding_y } from "./html_style_padding_y.mjs";
 import { html_style_padding_x } from "./html_style_padding_x.mjs";
 import { html_border_none } from "./html_border_none.mjs";
 import { html_margin_em } from "./html_margin_em.mjs";
-import { html_border_radius_em } from "./html_border_radius_em.mjs";
+import { html_border_radius } from "./html_border_radius.mjs";
+import { app_shared_border_radius } from "./app_shared_border_radius.mjs";
 export function app_replace_button_symbol_style_inner(b) {
-  html_border_radius_em(b, 0.5);
+  html_border_radius(b, app_shared_border_radius());
   html_border_none(b);
   html_style_padding_x(b, "0.37em");
   html_style_padding_y(b, "0.1em");
