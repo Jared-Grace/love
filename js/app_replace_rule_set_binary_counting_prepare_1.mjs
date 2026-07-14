775b782db0e75c25499573ca94b138a24a2c249d
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 21:01:48 +0800

    ai

--- js/app_replace_rule_set_binary_counting_prepare_1.mjs
+++ js/app_replace_rule_set_binary_counting_prepare_1.mjs
@@ -5,7 +5,15 @@ export function app_replace_rule_set_binary_counting_prepare_1() {
     goals: [
       {
         start: "1 a",
+        end: "1 1 a",
+      },
+      {
+        start: "1 a",
         end: "1 a 0",
+      },
+      {
+        start: "1 a",
+        end: "a 0 0",
       },
       {
         start: "1 a",
