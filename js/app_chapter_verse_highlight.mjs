13f43c0b0f4d1695e774208ebbf160c34b0bdb35
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 17:44:39 +0800

    ai

---
+++ js/app_chapter_verse_highlight.mjs
@@ -0,0 +1,30 @@
+import { list_includes } from "./list_includes.mjs";
+import { list_first } from "./list_first.mjs";
+import { list_last } from "./list_last.mjs";
+import { app_shared_verse_selected_background_color } from "./app_shared_verse_selected_background_color.mjs";
+import { html_style_background_color_set_or_remove } from "./html_style_background_color_set_or_remove.mjs";
+import { html_border_radius } from "./html_border_radius.mjs";
+import { html_style_remove } from "./html_style_remove.mjs";
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
+export function app_chapter_verse_highlight(component, sliced, verse_number) {
+  let chosen = list_includes(sliced, verse_number);
+  let color = app_shared_verse_selected_background_color();
+  html_style_background_color_set_or_remove(chosen, component, color);
+  if (chosen) {
+    let radius = "0.4em";
+    let first = list_first(sliced);
+    let last = list_last(sliced);
+    let top = "0";
+    if (first === verse_number) {
+      top = radius;
+    }
+    let bottom = "0";
+    if (last === verse_number) {
+      bottom = radius;
+    }
+    let value = text_combine_multiple([top, " ", top, " ", bottom, " ", bottom]);
+    html_border_radius(component, value);
+  } else {
+    html_style_remove(component, "border-radius");
+  }
+}
