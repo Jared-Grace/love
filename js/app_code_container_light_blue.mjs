cc9be6e34e4f45b0026eefcaac7f8e87b97b895c
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 11:21:52 +0800

    ai

--- js/app_code_container_light_blue.mjs
+++ js/app_code_container_light_blue.mjs
@@ -1,16 +1,19 @@
 import { app_code_container_light_blue_border_color } from "./app_code_container_light_blue_border_color.mjs";
 import { app_code_flex_gap } from "./app_code_flex_gap.mjs";
+import { app_shared_spaced_gap } from "./app_shared_spaced_gap.mjs";
 import { html_style_assign } from "./html_style_assign.mjs";
 import { html_border_y } from "./html_border_y.mjs";
 import { html_style_margin_y } from "./html_style_margin_y.mjs";
 import { html_border_none } from "./html_border_none.mjs";
-import { html_style_padding } from "./html_style_padding.mjs";
+import { html_style_padding_x } from "./html_style_padding_x.mjs";
+import { html_style_padding_y } from "./html_style_padding_y.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 import { app_shared_container_blue_background_color } from "./app_shared_container_blue_background_color.mjs";
 export function app_code_container_light_blue(parent) {
   let c = html_div(parent);
-  html_style_padding(c, "0.2em");
+  html_style_padding_y(c, "0.2em");
+  html_style_padding_x(c, app_shared_spaced_gap());
   html_border_none(c);
   html_style_background_color_set(c, app_shared_container_blue_background_color());
   let border_color = app_code_container_light_blue_border_color();
