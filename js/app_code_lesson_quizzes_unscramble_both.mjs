92ba8ea895856d8634ee9e857967c8593efbabf3
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 15:43:26 +0800

    ai

--- js/app_code_lesson_quizzes_unscramble_both.mjs
+++ js/app_code_lesson_quizzes_unscramble_both.mjs
@@ -12,7 +12,7 @@ export function app_code_lesson_quizzes_unscramble_both(
   backwards_answer_count_override,
 ) {
   let backwards_code = true;
-  let quizzes_get = app_code_lesson_quizzes_generic(
+  let quizzes_get = app_code_lesson_quizzes_generic({
     backwards_answer_label,
     backwards_answer_on_button,
     backwards_on_question,
@@ -24,7 +24,7 @@ export function app_code_lesson_quizzes_unscramble_both(
     forwards_question_label,
     backwards_code,
     batch_get,
-    true,
-  );
+    forwards_code: true,
+  });
   return quizzes_get;
 }
