5ec48fcd812147df2eedf5cffad1ef1cef8374af
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 21:05:52 +0800

    ai

--- js/html_style_code_unfonted.mjs
+++ js/html_style_code_unfonted.mjs
@@ -1,6 +1,7 @@
 import { html_style_padding_x } from "./html_style_padding_x.mjs";
 import { html_border_none } from "./html_border_none.mjs";
-import { html_border_radius_em } from "./html_border_radius_em.mjs";
+import { html_border_radius } from "./html_border_radius.mjs";
+import { app_shared_border_radius } from "./app_shared_border_radius.mjs";
 import { html_font_color_set } from "./html_font_color_set.mjs";
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 export function html_style_code_unfonted(
@@ -10,7 +11,7 @@ export function html_style_code_unfonted(
 ) {
   html_style_background_color_set(component, color_background);
   html_font_color_set(component, color_font);
-  html_border_radius_em(component, 0.5);
+  html_border_radius(component, app_shared_border_radius());
   html_border_none(component);
   html_style_padding_x(component, "0.37em");
 }
