b33f0d6c823228ae6e0aba7289a6ca171b596915
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 16:38:11 +0800

    ai

---
+++ js/app_chapter_change.mjs
@@ -0,0 +1,18 @@
+import { list_last } from "./list_last.mjs";
+import { ebible_language_to_bible_folder } from "./ebible_language_to_bible_folder.mjs";
+import { ebible_chapter_codes_browser } from "./ebible_chapter_codes_browser.mjs";
+import { html_hash_transform } from "./html_hash_transform.mjs";
+import { property_set } from "./property_set.mjs";
+import { window_reload } from "./window_reload.mjs";
+export async function app_chapter_change(chapter_code, languages_chosen, list_wrap) {
+  let language = list_last(languages_chosen);
+  let bible_folder = ebible_language_to_bible_folder(language);
+  let list = await ebible_chapter_codes_browser(bible_folder);
+  let next = list_wrap(list, chapter_code);
+  function transform(hash) {
+    property_set(hash, "c", next);
+    property_set(hash, "v", "");
+  }
+  html_hash_transform(transform);
+  window_reload();
+}
