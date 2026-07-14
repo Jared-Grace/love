e74036c4ceec7586f9b46a0b3024c55f5e7374e7
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 21:14:38 +0800

    ai

--- js/app_g_player_scroll_center.mjs
+++ js/app_g_player_scroll_center.mjs
@@ -1,10 +1,8 @@
-import { app_g_player_get } from "./app_g_player_get.mjs";
-import { app_g_player_center } from "./app_g_player_center.mjs";
+import { html_scroll_center_container_now } from "./html_scroll_center_container_now.mjs";
 import { html_on_load_wait } from "./html_on_load_wait.mjs";
-import { html_request_animation_frame } from "./html_request_animation_frame.mjs";
+import { property_get } from "./property_get.mjs";
 export async function app_g_player_scroll_center(div_map, player_img_c) {
+  let container = property_get(div_map, "container");
   await html_on_load_wait();
-  await html_request_animation_frame();
-  let player = await app_g_player_get();
-  app_g_player_center(player, player_img_c, div_map);
+  await html_scroll_center_container_now(player_img_c, container);
 }
