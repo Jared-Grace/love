5ba3751fa5691bc5121b2906d5bc38adabafeecf
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 10:42:24 +0800

    ai

--- js/function_run_prompt_lock.mjs
+++ js/function_run_prompt_lock.mjs
@@ -1,6 +1,5 @@
 import { lock_wait_prompt } from "../../love/js/lock_wait_prompt.mjs";
 export async function function_run_prompt_lock(lambda) {
-  $a;
   let who = function_run_prompt_lock.name;
   let r = await lock_wait_prompt(lambda, who);
   return r;
