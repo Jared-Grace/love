46ed33451a6387530da2d416030a915705d7a5b7
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 15:41:21 +0800

    ai

--- js/app_code_lesson_code_logged.mjs
+++ js/app_code_lesson_code_logged.mjs
@@ -1,10 +1,13 @@
 import { app_code_lesson_code_generic } from "./app_code_lesson_code_generic.mjs";
-export function app_code_lesson_code_logged(
-  batch_get,
-  name_id,
-  above,
-  quiz_backwards_answer_count_override,
-) {
+import { property_get } from "./property_get.mjs";
+export function app_code_lesson_code_logged(params) {
+  let batch_get = property_get(params, "batch_get");
+  let name_id = property_get(params, "name_id");
+  let above = property_get(params, "above");
+  let quiz_backwards_answer_count_override = property_get(
+    params,
+    "quiz_backwards_answer_count_override",
+  );
   let value = "logged output";
   let lesson = app_code_lesson_code_generic({
     value,
