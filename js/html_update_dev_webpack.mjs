960dde6ef60bf9dad83e1d7ae00c08fc1e0272f9
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 17:59:33 +0800

    ai

--- js/html_update_dev_webpack.mjs
+++ js/html_update_dev_webpack.mjs
@@ -1,5 +1,6 @@
 import { html_update_latest_webpack_generic } from "./html_update_latest_webpack_generic.mjs";
 export async function html_update_dev_webpack(search) {
   let path = "f_path_dev";
-  await html_update_latest_webpack_generic(search, path);
+  let cache_bust = true;
+  await html_update_latest_webpack_generic(search, path, cache_bust);
 }
