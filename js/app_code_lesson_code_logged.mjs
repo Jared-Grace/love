34fb8fc30493a0e7e5db8b5166b04573f37eea7f
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 16:24:28 +0800

    ai

--- js/app_code_lesson_code_logged.mjs
+++ js/app_code_lesson_code_logged.mjs
@@ -1,8 +1,8 @@
 import { app_code_lesson_code_generic } from "./app_code_lesson_code_generic.mjs";
-import { object_assign } from "./object_assign.mjs";
+import { object_copy_assign } from "./object_copy_assign.mjs";
 export function app_code_lesson_code_logged(params) {
   let value = "logged output";
-  object_assign(params, { value });
-  let lesson = app_code_lesson_code_generic(params);
+  let next_params = object_copy_assign(params, { value });
+  let lesson = app_code_lesson_code_generic(next_params);
   return lesson;
 }
