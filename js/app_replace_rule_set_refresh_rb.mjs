8349d316c81d3a78d12604a30ce8235b4a626c26
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 18:20:43 +0800

    ai

--- js/app_replace_rule_set_refresh_rb.mjs
+++ js/app_replace_rule_set_refresh_rb.mjs
@@ -5,15 +5,15 @@ import { app_replace_rule_set_rule_background_color } from "./app_replace_rule_s
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 import { html_font_color_set_if } from "./html_font_color_set_if.mjs";
 import { not } from "./not.mjs";
-export function app_replace_rule_set_refresh_rb(rb, index2, state) {
+export function app_replace_rule_set_refresh_rb(rule_button, rule_index, state) {
   let index_selected = property_get(state, "index_selected");
   let success = property_get(state, "success");
-  let selected = index2 === index_selected;
+  let selected = rule_index === index_selected;
   let enabled = true;
-  html_enable_if(rb, enabled);
-  app_replace_lefts_rights_style(rb, selected || success, success);
+  html_enable_if(rule_button, enabled);
+  app_replace_lefts_rights_style(rule_button, selected || success, success);
   let c = app_replace_rule_set_rule_background_color(selected, enabled, success);
-  html_style_background_color_set(rb, c);
-  let arrow2 = property_get(rb, "arrow");
-  html_font_color_set_if(selected && not(success), arrow2, "white", "black");
+  html_style_background_color_set(rule_button, c);
+  let arrow = property_get(rule_button, "arrow");
+  html_font_color_set_if(selected && not(success), arrow, "white", "black");
 }
