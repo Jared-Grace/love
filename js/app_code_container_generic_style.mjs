76534fd576c71b90f760debe5079c37694a4e1e1
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 21:06:06 +0800

    ai

--- js/app_code_container_generic_style.mjs
+++ js/app_code_container_generic_style.mjs
@@ -1,8 +1,9 @@
 import { html_border_none } from "./html_border_none.mjs";
-import { html_border_radius_em } from "./html_border_radius_em.mjs";
+import { html_border_radius } from "./html_border_radius.mjs";
+import { app_shared_border_radius_large } from "./app_shared_border_radius_large.mjs";
 import { html_style_padding } from "./html_style_padding.mjs";
 export function app_code_container_generic_style(container) {
   html_style_padding(container, "0.2em");
-  html_border_radius_em(container, 0.75);
+  html_border_radius(container, app_shared_border_radius_large());
   html_border_none(container);
 }
