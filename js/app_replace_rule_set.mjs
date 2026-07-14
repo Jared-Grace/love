14f934b03b348711d565eb211c21ee7a3f85401e
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 18:08:19 +0800

    ai

--- js/app_replace_rule_set.mjs
+++ js/app_replace_rule_set.mjs
@@ -166,7 +166,6 @@ export async function app_replace_rule_set(context) {
       });
       function refresh_rb() {
         let state = {
-          start,
           index_selected,
           success,
         };
