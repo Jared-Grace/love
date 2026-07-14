344a187f24e8421293077f4db6c16da6e3ad01fc
JESUS rose to life <username@users.noreply.github.com>
2026-07-14 13:30:32 +0800

    ai

--- js/app_replace_button_symbol_style_background_color_invalid.mjs
+++ js/app_replace_button_symbol_style_background_color_invalid.mjs
@@ -1,5 +1,4 @@
-import { app_shared_container_blue_border_color } from "./app_shared_container_blue_border_color.mjs";
 export function app_replace_button_symbol_style_background_color_invalid() {
-  let r = app_shared_container_blue_border_color();
+  let r = "white";
   return r;
 }
