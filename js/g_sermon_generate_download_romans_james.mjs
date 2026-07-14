71a533a2c771cdc42e5c6b48f45a3a89b643a151
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 07:46:26 +0800

    ai

--- js/g_sermon_generate_download_romans_james.mjs
+++ js/g_sermon_generate_download_romans_james.mjs
@@ -1,8 +1,9 @@
 import { list_map_async } from "./list_map_async.mjs";
 import { list_concat_multiple } from "./list_concat_multiple.mjs";
 import { g_sermon_generate_download_book } from "./g_sermon_generate_download_book.mjs";
+import { g_sermon_edited_book_codes } from "./g_sermon_edited_book_codes.mjs";
 export async function g_sermon_generate_download_romans_james() {
-  let book_codes = ["ROM", "JAS"];
+  let book_codes = g_sermon_edited_book_codes();
   let books_sermons = await list_map_async(
     book_codes,
     g_sermon_generate_download_book,
