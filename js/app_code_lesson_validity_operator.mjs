4a512c2c225a67f6770a0e205ad3591f2697ebd6
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 09:17:27 +0800

    ai

--- js/app_code_lesson_validity_operator.mjs
+++ js/app_code_lesson_validity_operator.mjs
@@ -1,7 +1,7 @@
 import { app_code_lesson_validity_code } from "./app_code_lesson_validity_code.mjs";
 import { app_code_lesson_name_id_symbol } from "./app_code_lesson_name_id_symbol.mjs";
-export function app_code_lesson_validity_operator(math_name, symbol, batch, above) {
-  let name_id = app_code_lesson_name_id_symbol("operators", [math_name], symbol);
+export function app_code_lesson_validity_operator(rights, symbol, batch, above) {
+  let name_id = app_code_lesson_name_id_symbol("operators", rights, symbol);
   let lesson = app_code_lesson_validity_code(batch, name_id, above);
   return lesson;
 }
