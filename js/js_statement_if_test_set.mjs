f3061d7374dc60aa98c752960f41d2911315e8bd
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 07:28:05 +0800

    ai

--- js/js_statement_if_test_set.mjs
+++ js/js_statement_if_test_set.mjs
@@ -1,8 +1,8 @@
 import { arguments_assert_each } from "./arguments_assert_each.mjs";
 import { js_statement_if_is } from "./js_statement_if_is.mjs";
-import { js_node_is } from "./js_node_is.mjs";
+import { js_expression_node_is } from "./js_expression_node_is.mjs";
 import { property_set } from "./property_set.mjs";
 export function js_statement_if_test_set(statement_if, expression) {
-  arguments_assert_each(arguments, [js_statement_if_is, js_node_is]);
+  arguments_assert_each(arguments, [js_statement_if_is, js_expression_node_is]);
   property_set(statement_if, "test", expression);
 }
