4e5d1de30fd0aa2c47dfe7539d2ae1e855df5b76
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 19:04:00 +0800

    ai

--- js/app_g_game_read_curried.mjs
+++ js/app_g_game_read_curried.mjs
@@ -1,8 +1,10 @@
 import { file_read_json } from "./file_read_json.mjs";
+import { app_g_rows_ensure } from "./app_g_rows_ensure.mjs";
 import { app_g_coordinates_ensure } from "./app_g_coordinates_ensure.mjs";
 export function app_g_game_read_curried(file_path) {
   async function app_g_game_read_curried_result() {
     let g = await file_read_json(file_path);
+    await app_g_rows_ensure(g);
     app_g_coordinates_ensure(g);
     return g;
   }
