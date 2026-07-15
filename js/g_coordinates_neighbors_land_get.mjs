3b989c51895d689950df9975ae817a235ef2a3ca
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 17:06:33 +0800

    ai

--- js/g_coordinates_neighbors_land_get.mjs
+++ js/g_coordinates_neighbors_land_get.mjs
@@ -1,4 +1,4 @@
-import { g_coordinates_land_index } from "./g_coordinates_land_index.mjs";
+import { g_coordinates_walkable_index } from "./g_coordinates_walkable_index.mjs";
 import { g_coordinates_orthogonal } from "./g_coordinates_orthogonal.mjs";
 import { g_coordinates_key } from "./g_coordinates_key.mjs";
 import { graph_neighbor_dataless } from "./graph_neighbor_dataless.mjs";
@@ -7,7 +7,8 @@ import { property_exists } from "./property_exists.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function g_coordinates_neighbors_land_get(g) {
   let coordinates = property_get(g, "coordinates");
-  let index = g_coordinates_land_index(coordinates);
+  let npcs = property_get(g, "npcs");
+  let index = g_coordinates_walkable_index(coordinates, npcs);
   function neighbors_get(node) {
     let orthogonal = g_coordinates_orthogonal(node);
     function lambda(add) {
