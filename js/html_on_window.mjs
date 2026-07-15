de121015589679d36471b19dd6a22e40412d0f81
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 20:23:55 +0800

    ai

--- js/html_on_window.mjs
+++ js/html_on_window.mjs
@@ -2,5 +2,6 @@ import { html_on } from "./html_on.mjs";
 import { html_component_wrap } from "./html_component_wrap.mjs";
 export function html_on_window(name, lambda) {
   let w = html_component_wrap(window);
-  html_on(w, name, lambda);
+  let remove = html_on(w, name, lambda);
+  return remove;
 }
