29444fff628b9afbfd491b500cb49220ebbef383
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 13:34:38 +0800

    ai

--- js/app_replace_tests_run_e2e_dev.mjs
+++ js/app_replace_tests_run_e2e_dev.mjs
@@ -1,6 +1,6 @@
 import { app_replace_url_dev } from "./app_replace_url_dev.mjs";
-import { app_replace_tests_run_e2e_both } from "./app_replace_tests_run_e2e_both.mjs";
+import { app_replace_tests_run_e2e } from "./app_replace_tests_run_e2e.mjs";
 export async function app_replace_tests_run_e2e_dev() {
   let url = await app_replace_url_dev();
-  await app_replace_tests_run_e2e_both(url);
+  await app_replace_tests_run_e2e(url);
 }
