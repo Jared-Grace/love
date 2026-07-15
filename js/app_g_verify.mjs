7bb33b6b956de728417064d2d5d8387e9ccac6ad
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 18:02:42 +0800

    ai

--- js/app_g_verify.mjs
+++ js/app_g_verify.mjs
@@ -1,3 +1,18 @@
-export async function app_g_verify() {
-  await app_main_production("app_g_verify", "jared-grace");
+import { app_shared_initialize } from "./app_shared_initialize.mjs";
+import { app_shared_refresh } from "./app_shared_refresh.mjs";
+import { app_g_verify_screens } from "./app_g_verify_screens.mjs";
+import { app_g_verify_home } from "./app_g_verify_home.mjs";
+import { object_merge } from "./object_merge.mjs";
+import { property_get } from "./property_get.mjs";
+import { html_margin_0 } from "./html_margin_0.mjs";
+export async function app_g_verify(context) {
+  let app_fn = app_g_verify;
+  let screens = app_g_verify_screens();
+  object_merge(context, {
+    screen_home: app_g_verify_home,
+  });
+  let root = property_get(context, "root");
+  html_margin_0(root);
+  app_shared_initialize(context, app_fn, screens);
+  await app_shared_refresh(context);
 }
---
+++ js/app_g_verify_screens.mjs
@@ -0,0 +1,4 @@
+import { app_g_verify_home } from "./app_g_verify_home.mjs";
+export function app_g_verify_screens() {
+  return [app_g_verify_home];
+}
