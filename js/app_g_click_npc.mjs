e0cb7b49f4c644328e244ab7f971e7ffc7b38133
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 18:39:35 +0800

    ai

--- js/app_g_click_npc.mjs
+++ js/app_g_click_npc.mjs
@@ -7,14 +7,16 @@ import { property_get } from "./property_get.mjs";
 import { g_direction } from "./g_direction.mjs";
 import { app_g_npc_img_get } from "./app_g_npc_img_get.mjs";
 import { app_g_character_face } from "./app_g_character_face.mjs";
-export async function app_g_click_npc(div_map, npcs_matched) {
+export async function app_g_click_npc(div_map, npcs_matched, player_img_c) {
   let npc = list_single(npcs_matched);
   let x = property_get(npc, "x");
   let y = property_get(npc, "y");
   let player = await app_g_player_get();
-  let direction = g_direction(npc, player);
   let npc_img = app_g_npc_img_get(npc);
-  app_g_character_face(npc, npc_img, direction);
+  let direction_npc = g_direction(npc, player);
+  app_g_character_face(npc, npc_img, direction_npc);
+  let direction_player = g_direction(player, npc);
+  app_g_character_face(player, player_img_c, direction_player);
   let phase = app_g_npc_phase_get(player);
   await app_g_view_set_render(
     {
