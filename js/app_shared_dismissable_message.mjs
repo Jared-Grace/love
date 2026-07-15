70c7f7b956e5919a01ff67b7f910d9738037f429
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 17:34:39 +0800

    ai

---
+++ js/app_shared_dismissable_message.mjs
@@ -0,0 +1,25 @@
+import { storage_local_exists_not_context } from "./storage_local_exists_not_context.mjs";
+import { storage_local_set_context } from "./storage_local_set_context.mjs";
+import { html_p } from "./html_p.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { text_combine } from "./text_combine.mjs";
+import { emoji_x_purple } from "./emoji_x_purple.mjs";
+import { app_shared_text_deemphasized } from "./app_shared_text_deemphasized.mjs";
+import { html_remove } from "./html_remove.mjs";
+import { html_on_click } from "./html_on_click.mjs";
+import { app_shared_margin_y_set } from "./app_shared_margin_y_set.mjs";
+export function app_shared_dismissable_message(context, parent, key, text) {
+  let show = storage_local_exists_not_context(context, key);
+  if (show) {
+    let p = html_p(parent);
+    app_shared_margin_y_set(p);
+    html_span_text(p, text);
+    let x = html_span_text(p, text_combine("  ", emoji_x_purple()));
+    app_shared_text_deemphasized(x);
+    function dismiss() {
+      storage_local_set_context(context, key, true);
+      html_remove(p);
+    }
+    html_on_click(x, dismiss);
+  }
+}
