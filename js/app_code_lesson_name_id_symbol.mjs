7b72d9387bb71816b204aebbd1c03372ec62b947
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 09:44:54 +0800

    ai

--- js/app_code_lesson_name_id_symbol.mjs
+++ js/app_code_lesson_name_id_symbol.mjs
@@ -1,11 +1,12 @@
 import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
-import { text_combine_space_right } from "./text_combine_space_right.mjs";
 import { list_first_remaining } from "./list_first_remaining.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_span_space } from "./html_span_space.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
+import { html_span_text_deemphasized } from "./html_span_text_deemphasized.mjs";
 import { each } from "./each.mjs";
 import { text_combine } from "./text_combine.mjs";
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { property_get } from "./property_get.mjs";
 export function app_code_lesson_name_id_symbol(left, rights, symbol) {
   let name_get = function app_code_lesson_name_id_symbol_name_get(
@@ -13,9 +14,6 @@ export function app_code_lesson_name_id_symbol(left, rights, symbol) {
     left_upper,
   ) {
     let render = function app_code_lesson_name_id_symbol_render(parent) {
-      let left_spaced = text_combine_space_right(left_upper);
-      html_span_text(parent, left_spaced);
-      html_span_text(parent, "(");
       let r = list_first_remaining(rights);
       let first = property_get(r, "first");
       let remaining = property_get(r, "remaining");
@@ -27,7 +25,9 @@ export function app_code_lesson_name_id_symbol(left, rights, symbol) {
         html_span_text(parent, combined);
       }
       each(remaining, each_remaining);
-      html_span_text(parent, ")");
+      html_span_space(parent);
+      let category = text_combine_multiple(["(", left_upper, ")"]);
+      html_span_text_deemphasized(parent, category);
     };
     return render;
   };
