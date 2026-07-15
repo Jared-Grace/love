d50892299d0ac071a5b7098646e1ed5e85b0d832
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 16:18:03 +0800

    ai

--- js/app_g_player_center.mjs
+++ js/app_g_player_center.mjs
@@ -2,5 +2,5 @@ import { html_scroll_center_coordinates } from "./html_scroll_center_coordinates
 import { app_g_div_map_container_get } from "./app_g_div_map_container_get.mjs";
 export function app_g_player_center(coordinates, player_img_c, div_map) {
   let container = app_g_div_map_container_get(div_map);
-  html_scroll_center_coordinates(coordinates, player_img_c, container);
+  return html_scroll_center_coordinates(coordinates, player_img_c, container);
 }
