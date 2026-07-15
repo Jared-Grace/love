ea845177e4d44d8971dcba7334e24993d501e2b7
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 08:16:41 +0800

    ai

--- js/firebase_login_email.mjs
+++ js/firebase_login_email.mjs
@@ -1,7 +1,9 @@
-import { html_button_wide } from "./html_button_wide.mjs";
+import { app_replace_button_wide } from "./app_replace_button_wide.mjs";
 import { html_value_get } from "./html_value_get.mjs";
-import { html_width_full } from "./html_width_full.mjs";
+import { app_shared_input_style } from "./app_shared_input_style.mjs";
 import { each } from "./each.mjs";
+import { html_on_enter } from "./html_on_enter.mjs";
+import { html_focus } from "./html_focus.mjs";
 import { html_placeholder } from "./html_placeholder.mjs";
 import { html_input_password } from "./html_input_password.mjs";
 import { html_input_email } from "./html_input_email.mjs";
@@ -11,7 +13,8 @@ export function firebase_login_email(context, firebase_auth, auth) {
   let input_username = html_input_email(root);
   let input_password = html_input_password(root);
   html_placeholder(input_password, "Password");
-  each([input_username, input_password], html_width_full);
+  each([input_username, input_password], app_shared_input_style);
+  html_focus(input_username);
   async function login() {
     let username = html_value_get(input_username);
     let password = html_value_get(input_password);
@@ -22,5 +25,6 @@ export function firebase_login_email(context, firebase_auth, auth) {
     );
     return v;
   }
-  let component = html_button_wide(root, "Login", login);
+  html_on_enter(input_password, login);
+  let component = app_replace_button_wide(root, "Login", login);
 }
