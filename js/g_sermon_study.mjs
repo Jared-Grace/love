70504f647c64513d2d06f96ba01c503862bd22b5
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 08:13:31 +0800

    ai

---
+++ js/g_sermon_study.mjs
@@ -0,0 +1,26 @@
+import { property_get } from "./property_get.mjs";
+import { list_map } from "./list_map.mjs";
+import { list_find_property } from "./list_find_property.mjs";
+import { g_sermon_generate_chapter_passages_get } from "./g_sermon_generate_chapter_passages_get.mjs";
+import { g_sermon_generate_download } from "./g_sermon_generate_download.mjs";
+import { g_sermon_study_passage } from "./g_sermon_study_passage.mjs";
+export async function g_sermon_study(chapter_code) {
+  let originals = await g_sermon_generate_chapter_passages_get(chapter_code);
+  let edited = await g_sermon_generate_download(chapter_code);
+  let edited_passages = property_get(edited, "passages");
+  function lambda(original_passage) {
+    let verse_numbers = property_get(original_passage, "verse_numbers");
+    let edited_passage = list_find_property(
+      edited_passages,
+      "verse_numbers",
+      verse_numbers,
+    );
+    let study = g_sermon_study_passage(original_passage, edited_passage);
+    return study;
+  }
+  let passages = list_map(originals, lambda);
+  return {
+    chapter_code,
+    passages,
+  };
+}
