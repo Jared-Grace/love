6334073298e256c3e581815ca14778d7bd4e6edc
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 08:24:55 +0800

    ai

---
+++ js/app_code_lesson_name_id_symbol.mjs
@@ -0,0 +1,36 @@
+import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
+import { text_combine_space_right } from "./text_combine_space_right.mjs";
+import { list_first_remaining } from "./list_first_remaining.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { html_span_space } from "./html_span_space.mjs";
+import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
+import { each } from "./each.mjs";
+import { text_combine } from "./text_combine.mjs";
+import { property_get } from "./property_get.mjs";
+export function app_code_lesson_name_id_symbol(left, rights, symbol) {
+  let name_get = function app_code_lesson_name_id_symbol_name_get(
+    lesson_name,
+    left_upper,
+  ) {
+    let render = function app_code_lesson_name_id_symbol_render(parent) {
+      let left_spaced = text_combine_space_right(left_upper);
+      html_span_text(parent, left_spaced);
+      html_span_text(parent, "(");
+      let r = list_first_remaining(rights);
+      let first = property_get(r, "first");
+      let remaining = property_get(r, "remaining");
+      html_span_text(parent, first);
+      html_span_space(parent);
+      html_span_text_code_dark(parent, symbol);
+      function each_remaining(rt) {
+        let combined = text_combine(", ", rt);
+        html_span_text(parent, combined);
+      }
+      each(remaining, each_remaining);
+      html_span_text(parent, ")");
+    };
+    return render;
+  };
+  let name_id = app_code_lesson_name_id_generic(rights, left, name_get);
+  return name_id;
+}
--- js/app_code_lesson_validity_operator.mjs
+++ js/app_code_lesson_validity_operator.mjs
@@ -1,7 +1,7 @@
 import { app_code_lesson_validity_code } from "./app_code_lesson_validity_code.mjs";
-import { app_code_lesson_name_id } from "./app_code_lesson_name_id.mjs";
-export function app_code_lesson_validity_operator(math_name, batch, above) {
-  let name_id = app_code_lesson_name_id("operators", [math_name]);
+import { app_code_lesson_name_id_symbol } from "./app_code_lesson_name_id_symbol.mjs";
+export function app_code_lesson_validity_operator(math_name, symbol, batch, above) {
+  let name_id = app_code_lesson_name_id_symbol("operators", [math_name], symbol);
   let lesson = app_code_lesson_validity_code(batch, name_id, above);
   return lesson;
 }
