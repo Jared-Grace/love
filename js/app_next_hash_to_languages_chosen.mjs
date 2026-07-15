fdfc412b11669335f132c17b590253e2ed651ff7
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 09:51:17 +0800

    ai

--- js/app_next_hash_to_languages_chosen.mjs
+++ js/app_next_hash_to_languages_chosen.mjs
@@ -1,7 +1,8 @@
 import { text_split_plus } from "./text_split_plus.mjs";
-import { property_get } from "./property_get.mjs";
+import { property_get_or } from "./property_get_or.mjs";
+import { ebible_language_en_code } from "./ebible_language_en_code.mjs";
 export function app_next_hash_to_languages_chosen(hash) {
-  let l = property_get(hash, "l");
+  let l = property_get_or(hash, "l", ebible_language_en_code());
   let languages_chosen = text_split_plus(l);
   return languages_chosen;
 }
