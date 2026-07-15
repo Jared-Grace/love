c625839a94285d5661813dc350d62dfada96f46c
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 17:07:18 +0800

    ai

--- js/g_coordinates_path_shortest.mjs
+++ js/g_coordinates_path_shortest.mjs
@@ -1,10 +1,10 @@
-import { graph_path_shortest_list } from "./graph_path_shortest_list.mjs";
-import { g_coordinates_neighbors_land_get } from "./g_coordinates_neighbors_land_get.mjs";
-import { g_coordinates_key } from "./g_coordinates_key.mjs";
-import { property_get } from "./property_get.mjs";
-import { list_size } from "./list_size.mjs";
+import { graph_path_shortest_list } from "../../love/js/graph_path_shortest_list.mjs";
+import { g_coordinates_neighbors_walkable_get } from "../../love/js/g_coordinates_neighbors_walkable_get.mjs";
+import { g_coordinates_key } from "../../love/js/g_coordinates_key.mjs";
+import { property_get } from "../../love/js/property_get.mjs";
+import { list_size } from "../../love/js/list_size.mjs";
 export function g_coordinates_path_shortest(g, start, target) {
-  let neighbors_get = g_coordinates_neighbors_land_get(g);
+  let neighbors_get = g_coordinates_neighbors_walkable_get(g);
   let coordinates = property_get(g, "coordinates");
   let max_depth = list_size(coordinates);
   let path = graph_path_shortest_list(
