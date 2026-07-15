d980bcf19d58af0f9cb0ecbb1d2298b24d33fc69
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 17:42:17 +0800

    ai

--- js/app_g_player_face.mjs
+++ js/app_g_player_face.mjs
@@ -1,6 +1,8 @@
-import { g_character_img_url_direction } from "./g_character_img_url_direction.mjs";
+import { g_character_img_url } from "./g_character_img_url.mjs";
 import { html_src_set } from "./html_src_set.mjs";
+import { property_set } from "./property_set.mjs";
 export function app_g_player_face(player, player_img_c, direction) {
-  let src = g_character_img_url_direction(player, direction);
+  property_set(player, "direction", direction);
+  let src = g_character_img_url(player);
   html_src_set(player_img_c, src);
 }
