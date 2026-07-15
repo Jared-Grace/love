283391aef51321ae0fdb9b53ca99f4bfcb0dabee
JESUS rose to life <username@users.noreply.github.com>
2026-07-15 12:57:30 +0800

    ai

--- js/g_coordinates_land_get.mjs
+++
@@ -1,7 +0,0 @@
-import { list_shuffle } from "./list_shuffle.mjs";
-import { g_coordinates_land } from "./g_coordinates_land.mjs";
-export function g_coordinates_land_get(coordinates) {
-  let coordinates_land = g_coordinates_land(coordinates);
-  list_shuffle(coordinates_land);
-  return coordinates_land;
-}
