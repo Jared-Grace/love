5220c2604b673adbbc1548c0a28f1e558f409997
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 07:27:29 +0800

    ai

---
+++ js/js_expression_node_is.mjs
@@ -0,0 +1,7 @@
+import { js_node_is } from "./js_node_is.mjs";
+import { js_unparse } from "./js_unparse.mjs";
+import { js_expression_is } from "./js_expression_is.mjs";
+export function js_expression_node_is(node) {
+  let is = js_node_is(node) && js_expression_is(js_unparse(node));
+  return is;
+}
