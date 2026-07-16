15abdd3934b4a8b8a739ea20e244881856ae28d1
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 08:49:11 +0800

    ai

--- js/js_operator_bang_double_equal.mjs
+++ js/js_operator_bang_double_equal.mjs
@@ -1,6 +1,7 @@
 import { not_equal } from "./not_equal.mjs";
+import { js_operator_bang_double_equal_symbol } from "./js_operator_bang_double_equal_symbol.mjs";
 export function js_operator_bang_double_equal() {
-  let operator = "!==";
+  let operator = js_operator_bang_double_equal_symbol();
   let fn = not_equal;
   let o = {
     fn,
