eef3560abd344a701050b66bce313a59b5cb1eb1
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 07:48:34 +0800

    ai

--- js/app_shared_name_expression_find.mjs
+++ js/app_shared_name_expression_find.mjs
@@ -1,5 +1,5 @@
 import { js_object_expression_properties_find_key_named } from "./js_object_expression_properties_find_key_named.mjs";
-export function app_replace_rule_sets_name_expression(e) {
+export function app_shared_name_expression_find(e) {
   let search = "name";
   let found = js_object_expression_properties_find_key_named(e, search);
   return found;
