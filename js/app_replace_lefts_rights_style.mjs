290573dfc3d877dbe6c10d49ba824617fa7f3063
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 23:07:55 +0800

    ai

--- js/app_replace_lefts_rights_style.mjs
+++ js/app_replace_lefts_rights_style.mjs
@@ -1,12 +1,9 @@
-import { each_nested } from "./each_nested.mjs";
-import { app_replace_button_symbol_style_valid_if_curried_right } from "./app_replace_button_symbol_style_valid_if_curried_right.mjs";
-import { list_map_property_get } from "./list_map_property_get.mjs";
+import { each_nested } from "../../love/js/each_nested.mjs";
+import { app_replace_symbol_tile_valid_if_curried_right } from "../../love/js/app_replace_symbol_tile_valid_if_curried_right.mjs";
+import { list_map_property_get } from "../../love/js/list_map_property_get.mjs";
 export function app_replace_lefts_rights_style(rb, enabled, solved) {
   let properties = ["rights", "lefts"];
   let list = list_map_property_get(properties, rb);
-  let lambda = app_replace_button_symbol_style_valid_if_curried_right(
-    enabled,
-    solved,
-  );
+  let lambda = app_replace_symbol_tile_valid_if_curried_right(enabled, solved);
   each_nested(list, lambda);
 }
--- js/app_replace_symbol_tile_valid_if_multiple_nested.mjs
+++ js/app_replace_symbol_tile_valid_if_multiple_nested.mjs
@@ -1,10 +1,10 @@
-import { each } from "../../love/js/each.mjs";
+import { each_nested } from "../../love/js/each_nested.mjs";
 import { app_replace_symbol_tile_valid_if_curried_right } from "../../love/js/app_replace_symbol_tile_valid_if_curried_right.mjs";
-export function app_replace_button_symbol_style_valid_if_multiple(
+export function app_replace_symbol_tile_valid_if_multiple_nested(
   lists,
   valid,
   solved,
 ) {
   let lambda = app_replace_symbol_tile_valid_if_curried_right(valid, solved);
-  each(lists, lambda);
+  each_nested(lists, lambda);
 }
