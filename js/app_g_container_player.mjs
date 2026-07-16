b96d5880c092dfef750964fb73067c6b4a830ef8
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 19:06:58 +0800

    ai

--- js/app_g_container_player.mjs
+++ js/app_g_container_player.mjs
@@ -1,7 +1,7 @@
 import { app_g_container_color } from "./app_g_container_color.mjs";
-import { app_shared_button_background } from "./app_shared_button_background.mjs";
+import { app_shared_container_background_color } from "./app_shared_container_background_color.mjs";
 export function app_g_container_player(overlay) {
-  let color = app_shared_button_background();
+  let color = app_shared_container_background_color();
   let container = app_g_container_color(overlay, color);
   return container;
 }
---
+++ js/app_shared_bible_books_choose.mjs
@@ -0,0 +1,18 @@
+import { html_clear } from "../../love/js/html_clear.mjs";
+import { app_replace_button_back } from "../../love/js/app_replace_button_back.mjs";
+import { window_reload } from "../../love/js/window_reload.mjs";
+import { app_replace_button_list_centered } from "../../love/js/app_replace_button_list_centered.mjs";
+import { property_get } from "../../love/js/property_get.mjs";
+import { app_shared_bible_book_open } from "../../love/js/app_shared_bible_book_open.mjs";
+export function app_shared_bible_books_choose(content, books) {
+  html_clear(content);
+  app_replace_button_back(content, window_reload);
+  function item_to_text(book) {
+    return property_get(book, "text");
+  }
+  function on_click(book) {
+    let book_code = property_get(book, "book_code");
+    app_shared_bible_book_open(book_code);
+  }
+  app_replace_button_list_centered(content, books, item_to_text, on_click);
+}
