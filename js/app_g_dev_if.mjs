19c376bb25987353215b69b2fe01540b340f5fc9
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 20:57:51 +0800

    ai

--- js/app_g_dev_if.mjs
+++ js/app_g_dev_if.mjs
@@ -1,8 +1,11 @@
 import { html_hash_get } from "./html_hash_get.mjs";
 import { app_g_view_get } from "./app_g_view_get.mjs";
 import { app_g_view_set } from "./app_g_view_set.mjs";
+import { app_g_npcs_get } from "./app_g_npcs_get.mjs";
 import { app_g_view_kind_study } from "./app_g_view_kind_study.mjs";
-import { app_g_view_kind_opener } from "./app_g_view_kind_opener.mjs";
+import { app_g_view_kind_npc } from "./app_g_view_kind_npc.mjs";
+import { app_g_view_phase_conversation } from "./app_g_view_phase_conversation.mjs";
+import { list_random_item } from "./list_random_item.mjs";
 import { null_is } from "./null_is.mjs";
 import { not } from "./not.mjs";
 import { property_get } from "./property_get.mjs";
@@ -17,8 +20,13 @@ export async function app_g_dev_if() {
     };
   }
   if (hash === "#conversation") {
+    let npcs = await app_g_npcs_get();
+    let npc = list_random_item(npcs);
     view = {
-      kind: app_g_view_kind_opener(),
+      kind: app_g_view_kind_npc(),
+      x: property_get(npc, "x"),
+      y: property_get(npc, "y"),
+      phase: app_g_view_phase_conversation(),
     };
   }
   if (null_is(view)) {
