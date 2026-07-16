8336a17c04209477bb560a61966ce81564eab198
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 19:07:24 +0800

    ai

--- js/app_shared_bible_languages_gear.mjs
+++ js/app_shared_bible_languages_gear.mjs
@@ -3,7 +3,11 @@ import { app_replace_button } from "../../love/js/app_replace_button.mjs";
 import { ebible_languages } from "../../love/js/ebible_languages.mjs";
 import { list_multiple_is } from "../../love/js/list_multiple_is.mjs";
 import { app_shared_bible_languages_choose } from "../../love/js/app_shared_bible_languages_choose.mjs";
-export function app_chapter_languages_gear(bar, content, languages_chosen) {
+export function app_shared_bible_languages_gear(
+  bar,
+  content,
+  languages_chosen,
+) {
   let languages = ebible_languages();
   ("only offer the gear when the chapter can be shown in more than one language");
   let multiple = list_multiple_is(languages);
