8003d1dbfc2f34c52469aaa4af5e960094160d26
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 20:57:51 +0800

    ai

--- js/app_g_view_render.mjs
+++ js/app_g_view_render.mjs
@@ -4,11 +4,9 @@ import { property_get } from "./property_get.mjs";
 import { app_g_view_render_menu } from "./app_g_view_render_menu.mjs";
 import { app_g_view_render_npc } from "./app_g_view_render_npc.mjs";
 import { app_g_view_render_study } from "./app_g_view_render_study.mjs";
-import { app_g_view_render_opener } from "./app_g_view_render_opener.mjs";
 import { app_g_view_kind_menu } from "./app_g_view_kind_menu.mjs";
 import { app_g_view_kind_npc } from "./app_g_view_kind_npc.mjs";
 import { app_g_view_kind_study } from "./app_g_view_kind_study.mjs";
-import { app_g_view_kind_opener } from "./app_g_view_kind_opener.mjs";
 import { error_json } from "./error_json.mjs";
 export async function app_g_view_render(div_map) {
   let view = await app_g_view_get();
@@ -26,10 +24,6 @@ export async function app_g_view_render(div_map) {
   }
   if (kind === app_g_view_kind_study()) {
     await app_g_view_render_study(div_map);
-    return;
-  }
-  if (kind === app_g_view_kind_opener()) {
-    await app_g_view_render_opener(div_map);
     return;
   }
   error_json({
