5b71c925f8ce5d711b2b2a3f1202e9deed986ab3
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 12:54:00 +0800

    ai

--- js/js_code_negation.mjs
+++ js/js_code_negation.mjs
@@ -1,7 +1,7 @@
 import { js_operator_minus_symbol } from "./js_operator_minus_symbol.mjs";
-import { text_combine } from "./text_combine.mjs";
+import { js_code_prefix } from "./js_code_prefix.mjs";
 export function js_code_negation(n) {
   let left = js_operator_minus_symbol();
-  let negated = text_combine(left, n);
+  let negated = js_code_prefix(left, n);
   return negated;
 }
