d1b994efc7238b202f2ed01b6ae82b1265ec6322
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 09:04:27 +0800

    ai

--- js/js_code_binary_expression_commutative.mjs
+++ js/js_code_binary_expression_commutative.mjs
@@ -1,10 +1,12 @@
 import { js_operator_asterisk_symbol } from "./js_operator_asterisk_symbol.mjs";
 import { js_operator_plus_symbol } from "./js_operator_plus_symbol.mjs";
 import { js_operator_triple_equal_symbol } from "./js_operator_triple_equal_symbol.mjs";
+import { js_operator_bang_double_equal_symbol } from "./js_operator_bang_double_equal_symbol.mjs";
 export function js_code_binary_expression_commutative() {
   let p = js_operator_plus_symbol();
   let o = js_operator_asterisk_symbol();
   let e = js_operator_triple_equal_symbol();
-  let commutatives = [p, o, e];
+  let n = js_operator_bang_double_equal_symbol();
+  let commutatives = [p, o, e, n];
   return commutatives;
 }
