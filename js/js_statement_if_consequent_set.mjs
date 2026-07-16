b0cf02a5024db30a376d0f7069ff44640f20b181
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 12:08:59 +0800

    ai

--- js/js_statement_if_consequent_set.mjs
+++ js/js_statement_if_consequent_set.mjs
@@ -1,7 +1,9 @@
+import { arguments_assert_each } from "./arguments_assert_each.mjs";
+import { js_statement_if_is } from "./js_statement_if_is.mjs";
+import { js_statement_node_is } from "./js_statement_node_is.mjs";
 import { property_set } from "./property_set.mjs";
-import { js_statement_if_is_assert } from "./js_statement_if_is_assert.mjs";
 export function js_statement_if_consequent_set(statement_if, value) {
-  js_statement_if_is_assert(statement_if);
+  arguments_assert_each(arguments, [js_statement_if_is, js_statement_node_is]);
   let consequent = property_set(statement_if, "consequent", value);
   return consequent;
 }
