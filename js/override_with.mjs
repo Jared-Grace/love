01232510cff3d5b5bcf0c497be07c04009ec92e5
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 22:14:12 +0800

    ai

--- js/override_with.mjs
+++ js/override_with.mjs
@@ -1,9 +1,9 @@
 import { global_override } from "./global_override.mjs";
-import { property_get } from "./property_get.mjs";
+import { property_get_or_null } from "./property_get_or_null.mjs";
 import { property_set } from "./property_set.mjs";
 export async function override_with(fn, key, value, lambda) {
   let overrides = global_override(fn);
-  let previous = property_get(overrides, key);
+  let previous = property_get_or_null(overrides, key);
   property_set(overrides, key, value);
   try {
     let result = await lambda();
