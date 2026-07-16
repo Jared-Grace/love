c901d957f3305ff94da0f235e8d376dc14fa2aa1
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 09:27:20 +0800

    ai

--- js/js_property_value_set.mjs
+++ js/js_property_value_set.mjs
@@ -1,6 +1,8 @@
+import { arguments_assert_each } from "./arguments_assert_each.mjs";
+import { js_property_is } from "./js_property_is.mjs";
+import { js_expression_node_is } from "./js_expression_node_is.mjs";
 import { property_set } from "./property_set.mjs";
-import { js_property_is_assert } from "./js_property_is_assert.mjs";
 export function js_property_value_set(p, value) {
-  js_property_is_assert(p);
+  arguments_assert_each(arguments, [js_property_is, js_expression_node_is]);
   property_set(p, "value", value);
 }
