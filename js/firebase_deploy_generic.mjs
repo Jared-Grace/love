52b1eeb5be48476018c70b99db43f4b292897c48
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 14:57:01 +0800

    ai

--- js/firebase_deploy_generic.mjs
+++ js/firebase_deploy_generic.mjs
@@ -3,7 +3,7 @@ import { text_combine } from "../../love/js/text_combine.mjs";
 import { user_repo_path_combine } from "../../love/js/user_repo_path_combine.mjs";
 export async function firebase_deploy_generic(suffix) {
   let combined = await user_repo_path_combine(".");
-  let left = "npx firebase-tools deploy";
+  let left = "npx firebase-tools deploy ";
   let c = text_combine(left, suffix);
   let stdout = await command_line_generic(c, {
     cwd: combined,
