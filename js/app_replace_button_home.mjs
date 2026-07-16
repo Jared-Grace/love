a03e0b62995c6fd8d512ee439aadff398be2055f
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 22:14:12 +0800

    ai

--- js/app_replace_button_home.mjs
+++ js/app_replace_button_home.mjs
@@ -1,7 +1,7 @@
 import { app_replace_button_home_text } from "./app_replace_button_home_text.mjs";
 import { app_replace_home } from "./app_replace_home.mjs";
-import { app_replace_button_screen } from "./app_replace_button_screen.mjs";
+import { app_shared_screen_set_button } from "./app_shared_screen_set_button.mjs";
 export function app_replace_button_home(root, context) {
   let text = app_replace_button_home_text();
-  let b = app_replace_button_screen(context, app_replace_home, root, text);
+  let b = app_shared_screen_set_button(root, context, app_replace_home, text);
 }
--- js/app_replace_button_screen_wide.mjs
+++ js/app_replace_button_screen_wide.mjs
@@ -1,11 +1,11 @@
 import { html_width_full } from "./html_width_full.mjs";
-import { app_replace_button_screen } from "./app_replace_button_screen.mjs";
+import { app_shared_screen_set_button } from "./app_shared_screen_set_button.mjs";
 export function app_replace_button_screen_wide(
   context,
   screen_fn,
   parent,
   text,
 ) {
-  let b = app_replace_button_screen(context, screen_fn, parent, text);
+  let b = app_shared_screen_set_button(parent, context, screen_fn, text);
   html_width_full(b);
 }
