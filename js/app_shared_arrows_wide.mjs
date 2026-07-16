d59772e687603648e00db5bf02b6b160e124e454
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 07:30:49 +0800

    ai

--- js/app_shared_arrows_wide.mjs
+++ js/app_shared_arrows_wide.mjs
@@ -1,12 +1,12 @@
-import { html_div } from "./html_div.mjs";
-import { html_display_flex } from "./html_display_flex.mjs";
-import { app_replace_button_arrow_left } from "./app_replace_button_arrow_left.mjs";
-import { app_replace_button_arrow_right } from "./app_replace_button_arrow_right.mjs";
-import { html_flex_grow_1_multiple } from "./html_flex_grow_1_multiple.mjs";
+import { html_div } from "../../love/js/html_div.mjs";
+import { html_display_flex } from "../../love/js/html_display_flex.mjs";
+import { app_shared_button_arrow_left } from "../../love/js/app_shared_button_arrow_left.mjs";
+import { app_replace_button_arrow_right } from "../../love/js/app_replace_button_arrow_right.mjs";
+import { html_flex_grow_1_multiple } from "../../love/js/html_flex_grow_1_multiple.mjs";
 export function app_shared_arrows_wide(parent, on_left, on_right) {
   let row = html_div(parent);
   html_display_flex(row);
-  let l = app_replace_button_arrow_left(row, on_left);
+  let l = app_shared_button_arrow_left(row, on_left);
   let r = app_replace_button_arrow_right(row, on_right);
   html_flex_grow_1_multiple([l, r]);
   return row;
