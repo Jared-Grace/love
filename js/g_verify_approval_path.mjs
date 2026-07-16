5d4978e8a084abf697dafafcd5bc242b72537d91
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 14:03:57 +0800

    ai

---
+++ js/g_verify_approval_path.mjs
@@ -0,0 +1,7 @@
+import { g_objection_generate_upload_path_generic } from "./g_objection_generate_upload_path_generic.mjs";
+import { fn_name } from "./fn_name.mjs";
+export function g_verify_approval_path(chapter_code) {
+  let f_name = fn_name("g_verify_approval");
+  let destination = g_objection_generate_upload_path_generic(f_name, chapter_code);
+  return destination;
+}
