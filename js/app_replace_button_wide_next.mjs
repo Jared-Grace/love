4d4031e55cb4ee1318d8045c1c9ded44296568fd
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 21:48:23 +0800

    ai

--- js/app_replace_button_wide_next.mjs
+++ js/app_replace_button_wide_next.mjs
@@ -3,4 +3,5 @@ import { app_shared_button_next_text } from "../../love/js/app_shared_button_nex
 export function app_replace_button_wide_next(parent, on_next) {
   let nt = app_shared_button_next_text();
   let bn = app_shared_button_wide(parent, nt, on_next);
+  return bn;
 }
