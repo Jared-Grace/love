024c960bfdb7057d54be8b0af46cda3b4d8bc420
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 07:05:37 +0800

    ai

--- js/app_replace_button_back.mjs
+++
@@ -1,7 +0,0 @@
-import { app_shared_button_wide } from "../../love/js/app_shared_button_wide.mjs";
-import { app_shared_button_back_text } from "../../love/js/app_shared_button_back_text.mjs";
-export function app_replace_button_back(parent, lambda) {
-  let text = app_shared_button_back_text();
-  let component = app_shared_button_wide(parent, text, lambda);
-  return component;
-}
