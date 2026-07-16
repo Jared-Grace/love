2dba4eae5fc022783a2e08b617c10aa32ba7c9c3
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 07:30:49 +0800

    ai

--- js/app_shared_button_arrow_left.mjs
+++ js/app_shared_button_arrow_left.mjs
@@ -1,6 +1,6 @@
 import { app_shared_button } from "../../love/js/app_shared_button.mjs";
 import { emoji_arrow_left } from "../../love/js/emoji_arrow_left.mjs";
-export function app_replace_button_arrow_left(parent, lambda) {
+export function app_shared_button_arrow_left(parent, lambda) {
   let text = emoji_arrow_left();
   let component = app_shared_button(parent, text, lambda);
   return component;
