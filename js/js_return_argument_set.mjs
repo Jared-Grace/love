ca743e5c1d50b0d15259c2465458b4fedd8373e2
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 09:26:35 +0800

    ai

--- js/js_return_argument_set.mjs
+++ js/js_return_argument_set.mjs
@@ -1,4 +1,8 @@
+import { arguments_assert_each } from "./arguments_assert_each.mjs";
+import { js_return_is } from "./js_return_is.mjs";
+import { js_expression_node_is } from "./js_expression_node_is.mjs";
 import { property_set } from "./property_set.mjs";
 export function js_return_argument_set(r, a) {
+  arguments_assert_each(arguments, [js_return_is, js_expression_node_is]);
   property_set(r, "argument", a);
 }
