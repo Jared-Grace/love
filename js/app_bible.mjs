328679a091603b6ed5998db045abd652a80f598a
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 13:12:55 +0800

    ai

--- js/app_bible.mjs
+++ js/app_bible.mjs
@@ -1,8 +1,4 @@
-import { app_bible_home } from "./app_bible_home.mjs";
-import { app_bible_shared_initialize } from "./app_bible_shared_initialize.mjs";
-import { app_bible_screens } from "./app_bible_screens.mjs";
+import { app_chapter } from "./app_chapter.mjs";
 export async function app_bible(context) {
-  let app_fn = app_bible;
-  let screens = app_bible_screens();
-  await app_bible_shared_initialize(context, app_fn, screens, app_bible_home);
+  await app_chapter(context);
 }
