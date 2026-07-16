439539ce211b13d7b1b399c0690631f553bf50d7
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 12:55:02 +0800

    ai

--- js/app_code_lessons_fns.mjs
+++ js/app_code_lessons_fns.mjs
@@ -4,6 +4,7 @@ import { app_code_lesson_functions_console_log_greater_than } from "./app_code_l
 import { app_code_lesson_functions_console_log_equals } from "./app_code_lesson_functions_console_log_equals.mjs";
 import { app_code_lesson_functions_console_log_not_equal } from "./app_code_lesson_functions_console_log_not_equal.mjs";
 import { app_code_lesson_functions_console_log_true_false } from "./app_code_lesson_functions_console_log_true_false.mjs";
+import { app_code_lesson_functions_console_log_not } from "./app_code_lesson_functions_console_log_not.mjs";
 import { app_code_lesson_functions_console_log_statement } from "./app_code_lesson_functions_console_log_statement.mjs";
 import { app_code_lesson_functions_console_log } from "./app_code_lesson_functions_console_log.mjs";
 import { app_code_lesson_functions_invalid } from "./app_code_lesson_functions_invalid.mjs";
