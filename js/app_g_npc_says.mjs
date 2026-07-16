f24da366d83a73f77e4b9e7694cb5348190b0d7c
JESUS rose to life <username@users.noreply.github.com>
2026-07-16 21:47:35 +0800

    ai

--- js/app_g_npc_says.mjs
+++ js/app_g_npc_says.mjs
@@ -6,6 +6,7 @@ import { g_gender_male } from "./g_gender_male.mjs";
 import { g_gender_female } from "./g_gender_female.mjs";
 import { app_g_container_color } from "./app_g_container_color.mjs";
 import { html_bold_mild } from "./html_bold_mild.mjs";
+import { html_style_set } from "./html_style_set.mjs";
 import { property_get } from "./property_get.mjs";
 import { text_combine } from "./text_combine.mjs";
 export function app_g_npc_says(npc, overlay, npc_says) {
