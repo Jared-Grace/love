1bb99143938a1d0bb8f47a6446c7c6dd6f34c5e5
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 16:45:01 +0800

    ai

--- js/app_code_lesson_add.mjs
+++ js/app_code_lesson_add.mjs
@@ -1,7 +1,7 @@
-import { app_code_lessons_fns } from "./app_code_lessons_fns.mjs";
-import { app_replace_rule_set_add_generic } from "./app_replace_rule_set_add_generic.mjs";
+import { app_code_lessons_fns } from "../../love/js/app_code_lessons_fns.mjs";
+import { function_copy_list_add } from "../../love/js/function_copy_list_add.mjs";
 export async function app_code_lesson_add(fn_base_name) {
   let fns_list = app_code_lessons_fns;
-  let name_new = await app_replace_rule_set_add_generic(fn_base_name, fns_list);
+  let name_new = await function_copy_list_add(fn_base_name, fns_list);
   return name_new;
 }
