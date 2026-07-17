bc33012b94cccb5826365627300173cef011fb94
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 19:30:04 +0800

    ai

--- js/ebible_languages_add_item.mjs
+++ js/ebible_languages_add_item.mjs
@@ -1,5 +1,3 @@
-import { false_is_assert_json } from "./false_is_assert_json.mjs";
-import { list_find_property_exists } from "./list_find_property_exists.mjs";
 import { property_get } from "./property_get.mjs";
 import { ebible_languages_add_item_info } from "./ebible_languages_add_item_info.mjs";
 import { list_add } from "./list_add.mjs";
@@ -9,15 +7,6 @@ import { ebible_languages } from "./ebible_languages.mjs";
 import { function_transform } from "./function_transform.mjs";
 export async function ebible_languages_add_item(bible_folder) {
   let f_name = ebible_languages.name;
-  let exists = list_find_property_exists(
-    ebible_languages(),
-    "bible_folder",
-    bible_folder,
-  );
-  false_is_assert_json(exists, {
-    hint: "this bible folder is already in the languages list -- would you like to add a different bible folder, or was this language already added?",
-    bible_folder,
-  });
   let r = await ebible_languages_add_item_info(bible_folder);
   let language_code = property_get(r, "language_code");
   let name = property_get(r, "name");
