5a6929fc1a998f0d8386b16498dd73ded2b66805
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 21:26:17 +0800

    ai

--- js/app_g_button_wrong_generic.mjs
+++ js/app_g_button_wrong_generic.mjs
@@ -1,9 +1,7 @@
-import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 import { text_take } from "./text_take.mjs";
 import { html_border_invalid_color } from "./html_border_invalid_color.mjs";
-import { text_combine } from "./text_combine.mjs";
+import { app_g_button_background_translucent } from "./app_g_button_background_translucent.mjs";
 export function app_g_button_wrong_generic(b, alpha_channel) {
   let v = html_border_invalid_color();
-  let t = text_combine(text_take(v, 7), alpha_channel);
-  html_style_background_color_set(b, t);
+  app_g_button_background_translucent(b, text_take(v, 7), alpha_channel);
 }
---
+++ js/app_shared_container_blue_dark_font_color.mjs
@@ -0,0 +1,4 @@
+export function app_shared_container_blue_dark_font_color() {
+  let c = "white";
+  return c;
+}
