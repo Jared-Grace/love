20f3b11ef3146f6c1caedbe5e113524f68e3896b
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 06:36:56 +0800

    ai

--- js/function_name_to_path_import_code_multiple_map.mjs
+++
@@ -1,7 +0,0 @@
-import { list_map } from "./list_map.mjs";
-import { function_name_to_path_import_code_multiple } from "./function_name_to_path_import_code_multiple.mjs";
-export async function function_name_to_path_import_code_multiple_map(imports) {
-  let r = await function_name_to_path_import_code_multiple();
-  let mapped = list_map(imports, r);
-  return mapped;
-}
