a82b91f7b083317d1db4799b0d5de181f7859525
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 11:49:28 +0800

    ai

---
+++ js/file_imports_repair.mjs
@@ -0,0 +1,12 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { file_js_transform } from "./file_js_transform.mjs";
+import { js_imports_missing_add_all_program_at } from "./js_imports_missing_add_all_program_at.mjs";
+import { path_dirname } from "./path_dirname.mjs";
+export async function file_imports_repair(f_path) {
+  arguments_assert(arguments, 1);
+  let base_dir = await path_dirname(f_path);
+  async function lambda(ast) {
+    await js_imports_missing_add_all_program_at(ast, base_dir);
+  }
+  await file_js_transform(f_path, lambda);
+}
