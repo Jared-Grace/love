d8f4966ee47481881903105481d697a1337e2708
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 08:07:39 +0800

    ai

--- js/g_z.mjs
+++ js/g_z.mjs
@@ -1,7 +1,7 @@
 import { list_index_of } from "./list_index_of.mjs";
 export function g_z(z) {
   let index = list_index_of(
-    ["tile", "character", "icon", "tutorial", "click", "overlay"],
+    ["tile", "character", "icon", "tutorial", "click", "overlay", "raised"],
     z,
   );
   return index;
