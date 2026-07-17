0fb35b110f3c5b22223c6b9a56769fd1b3d0b237
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 19:30:21 +0800

    ai

--- js/ebible_languages_add.mjs
+++ js/ebible_languages_add.mjs
@@ -3,6 +3,7 @@ import { ebible_languages_add_item } from "./ebible_languages_add_item.mjs";
 import { ebible_languages_chapters_cache_refresh } from "./ebible_languages_chapters_cache_refresh.mjs";
 import { ebible_version_upload } from "./ebible_version_upload.mjs";
 export async function ebible_languages_add(bible_folder) {
+  ebible_languages_bible_folder_exists_not_assert(bible_folder);
   await ebible_version_upload(bible_folder);
   let r = await ebible_languages_chapters_cache_refresh();
   await ebible_languages_add_item(bible_folder);
