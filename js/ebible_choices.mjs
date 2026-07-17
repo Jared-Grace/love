e0f884f3e63b186ab0cf31cab699b9ba416f2690
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 22:47:28 +0800

    ai

--- js/ebible_choices.mjs
+++ js/ebible_choices.mjs
@@ -3,7 +3,7 @@ import { ebible_languages } from "./ebible_languages.mjs";
 import { list_filter_includes_not } from "./list_filter_includes_not.mjs";
 import { list_concat } from "./list_concat.mjs";
 import { list_map } from "./list_map.mjs";
-export async function app_supper_choices() {
+export async function ebible_choices() {
   let english = await ebible_versions_english_choices_browser();
   function lambda(code) {
     let choice = {
