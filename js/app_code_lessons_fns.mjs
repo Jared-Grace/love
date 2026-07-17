4dd5c5038e3ad295b41bac8e03ff8618a342bd0e
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 21:55:04 +0800

    ai

--- js/app_code_lessons_fns.mjs
+++ js/app_code_lessons_fns.mjs
@@ -14,6 +14,7 @@ import { app_code_lesson_functions_console_log_remainder_3 } from "./app_code_le
 import { app_code_lesson_functions_console_log_remainder_4 } from "./app_code_lesson_functions_console_log_remainder_4.mjs";
 import { app_code_lesson_functions_console_log_remainder_any } from "./app_code_lesson_functions_console_log_remainder_any.mjs";
 import { app_code_lesson_functions_console_log_nested_add } from "./app_code_lesson_functions_console_log_nested_add.mjs";
+import { app_code_lesson_functions_console_log_nested_subtract } from "./app_code_lesson_functions_console_log_nested_subtract.mjs";
 import { app_code_lesson_functions_console_log_exponent } from "./app_code_lesson_functions_console_log_exponent.mjs";
 import { app_code_lesson_functions_console_log_statement } from "./app_code_lesson_functions_console_log_statement.mjs";
 import { app_code_lesson_functions_console_log } from "./app_code_lesson_functions_console_log.mjs";
@@ -77,6 +78,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_functions_console_log_remainder_4,
     app_code_lesson_functions_console_log_remainder_any,
     app_code_lesson_functions_console_log_nested_add,
+    app_code_lesson_functions_console_log_nested_subtract,
     app_code_lesson_functions_console_log_exponent,
   ];
   return fns;
