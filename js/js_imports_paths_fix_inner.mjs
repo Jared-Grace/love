e73a50e730fb9dac56b08bfcaef66798586e2d44
JESUS rose to life <username@users.noreply.github.com>
2026-07-17 21:21:09 +0800

    ai

--- js/js_imports_paths_fix_inner.mjs
+++ js/js_imports_paths_fix_inner.mjs
@@ -1,15 +1,17 @@
 import { each } from "./each.mjs";
 import { property_set } from "./property_set.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
-import { function_name_to_path_import } from "./function_name_to_path_import.mjs";
+import { function_name_to_path_import_relative } from "./function_name_to_path_import_relative.mjs";
+import { import_from_dir } from "./import_from_dir.mjs";
 import { property_get } from "./property_get.mjs";
 import { js_imports_declarations } from "./js_imports_declarations.mjs";
 export function js_imports_paths_fix_inner(ast, dictionary) {
+  let from_dir = import_from_dir(ast, dictionary);
   let imports_declarations = js_imports_declarations(ast);
   function lambda(i) {
     let name = property_get(i, "name");
     let declaration = property_get(i, "declaration");
-    let from = function_name_to_path_import(name, dictionary);
+    let from = function_name_to_path_import_relative(name, dictionary, from_dir);
     let expression = js_parse_expression(from);
     property_set(declaration, "source", expression);
   }
