518fdbbe0171559b07be926c4316c9e63b194b42
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 23:14:41 +0800

    ai

--- js/app_shared_bible_change.mjs
+++ js/app_shared_bible_change.mjs
@@ -1,9 +1,8 @@
 import { list_last } from "./list_last.mjs";
 import { ebible_language_to_bible_folder } from "./ebible_language_to_bible_folder.mjs";
 import { ebible_chapter_codes_browser } from "./ebible_chapter_codes_browser.mjs";
-import { html_hash_transform } from "./html_hash_transform.mjs";
+import { html_hash_transform_reload } from "./html_hash_transform_reload.mjs";
 import { property_set } from "./property_set.mjs";
-import { window_reload } from "./window_reload.mjs";
 export async function app_shared_bible_change(
   chapter_code,
   languages_chosen,
