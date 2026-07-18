42f35eeea88762b9d30d873697c8929bc57d922e
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 19:03:35 +0800

    ai

--- js/app_a_color_literal.mjs
+++ js/app_a_color_literal.mjs
@@ -1,4 +1,5 @@
 import { html_font_color_set } from "./html_font_color_set.mjs";
+import { app_shared_color_literal } from "./app_shared_color_literal.mjs";
 export function app_a_color_literal(span) {
-  html_font_color_set(span, "#d07200ff");
+  html_font_color_set(span, app_shared_color_literal());
 }
