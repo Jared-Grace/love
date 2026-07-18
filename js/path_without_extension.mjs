4a9697c539f7a9beefe844f37d035d36a9209c20
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 09:31:13 +0800

    ai

--- js/path_without_extension.mjs
+++ js/path_without_extension.mjs
@@ -1,8 +1,7 @@
+import { path_extension } from "./path_extension.mjs";
 export function path_without_extension(p) {
-  function lambda(m) {
-    let v = m.replace(/\.[^.]+$/, "");
-    return v;
-  }
-  let v2 = p.replace(/[/\\][^/\\]+$/, lambda);
-  return v2;
+  let extension = path_extension(p);
+  let length = p.length - extension.length;
+  let v = p.slice(0, length);
+  return v;
 }
