a531cb285b5ee7b697990dbb731ed068ed5168f1
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 13:58:53 +0800

    ai

--- js/import_from_dir.mjs
+++ js/import_from_dir.mjs
@@ -2,10 +2,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get_or_null } from "./property_get_or_null.mjs";
 import { js_flo_name } from "./js_flo_name.mjs";
 import { path_directory } from "./path_directory.mjs";
-import { path_join } from "./path_join.mjs";
-import { folder_previous } from "./folder_previous.mjs";
-import { repo_current_name } from "./repo_current_name.mjs";
-import { folder_src } from "./folder_src.mjs";
+import { import_from_dir_root } from "./import_from_dir_root.mjs";
 export function import_from_dir(ast, dictionary) {
   arguments_assert(arguments, 2);
   let f_name = js_flo_name(ast);
@@ -14,6 +11,6 @@ export function import_from_dir(ast, dictionary) {
     let dir = path_directory(own);
     return dir;
   }
-  let fallback = path_join([folder_previous(), repo_current_name(), folder_src()]);
+  let fallback = import_from_dir_root();
   return fallback;
 }
