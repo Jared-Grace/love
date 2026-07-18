934767b5c40fdd29e77a12c7c9e0e7862ad2db83
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 17:18:01 +0800

    ai

--- js/examples_page_write.mjs
+++ js/examples_page_write.mjs
@@ -1,11 +1,14 @@
 import { examples_corpus_read } from "./examples_corpus_read.mjs";
 import { examples_page_html } from "./examples_page_html.mjs";
 import { file_overwrite } from "./file_overwrite.mjs";
-import { folder_public_join } from "./folder_public_join.mjs";
+import { html_name_to_path } from "./html_name_to_path.mjs";
+import { html_name_to_path_dev } from "./html_name_to_path_dev.mjs";
 export async function examples_page_write() {
   let examples = await examples_corpus_read();
   let html = examples_page_html(examples);
-  let out = folder_public_join("examples.html");
+  let out = html_name_to_path("examples");
+  let out_dev = html_name_to_path_dev("examples");
   await file_overwrite(out, html);
+  await file_overwrite(out_dev, html);
   return out;
 }
