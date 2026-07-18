47be1752b75af5c647873e3a41909e0527abfcae
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 17:57:32 +0800

    ai

--- js/app_code_after_lesson.mjs
+++ js/app_code_after_lesson.mjs
@@ -1,8 +1,7 @@
 import { app_code_lesson_current_number } from "./app_code_lesson_current_number.mjs";
 import { app_code_review_scope } from "./app_code_review_scope.mjs";
 import { null_not_is } from "./null_not_is.mjs";
-import { storage_local_set_context } from "./storage_local_set_context.mjs";
-import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
+import { app_shared_screen_go } from "./app_shared_screen_go.mjs";
 import { app_code_review } from "./app_code_review.mjs";
 import { app_code_lesson_next } from "./app_code_lesson_next.mjs";
 export async function app_code_after_lesson(context) {
@@ -11,8 +10,7 @@ export async function app_code_after_lesson(context) {
   let scope = app_code_review_scope(number);
   let has_review = null_not_is(scope);
   if (has_review) {
-    storage_local_set_context(context, "review_number", number);
-    await app_shared_screen_set(context, app_code_review);
+    await app_shared_screen_go(context, "review_number", number, app_code_review);
     return;
   }
   await app_code_lesson_next(context);
