6b0cc09038536280944df1fd5fd04f7c7dc9394a
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 21:33:11 +0800

    ai

--- js/app_bible.mjs
+++ js/app_bible.mjs
@@ -1,15 +1,21 @@
 import { app_shared_bible_read } from "../../love/js/app_shared_bible_read.mjs";
-import { app_bible_home_generic } from "../../love/js/app_bible_home_generic.mjs";
+import { app_bible_shared_initialize } from "../../love/js/app_bible_shared_initialize.mjs";
+import { app_bible_screens } from "../../love/js/app_bible_screens.mjs";
+import { app_bible_home } from "../../love/js/app_bible_home.mjs";
 import { app_bible_mode_get } from "../../love/js/app_bible_mode_get.mjs";
 import { app_bible_mode_verse } from "../../love/js/app_bible_mode_verse.mjs";
 import { app_bible_mode_button } from "../../love/js/app_bible_mode_button.mjs";
-import { noop } from "../../love/js/noop.mjs";
 import { equal } from "../../love/js/equal.mjs";
 export async function app_bible(context) {
   let mode = app_bible_mode_get();
   let verse = equal(mode, app_bible_mode_verse());
   if (verse) {
-    await app_bible_home_generic(context, noop, app_bible_mode_button);
+    await app_bible_shared_initialize(
+      context,
+      app_bible,
+      app_bible_screens(),
+      app_bible_home,
+    );
     return;
   }
   await app_shared_bible_read(context, app_bible_mode_button);
