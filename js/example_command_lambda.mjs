12a7c1d0f79918e9672bd964ac6cd976030f2417
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 14:41:04 +0800

    ai

--- js/example_command_lambda.mjs
+++ js/example_command_lambda.mjs
@@ -3,9 +3,13 @@ import { js_node_type_is_new_lambda } from "./js_node_type_is_new_lambda.mjs";
 import { example_imports_lambda } from "./example_imports_lambda.mjs";
 import { example_auto_lambda } from "./example_auto_lambda.mjs";
 import { js_atomize } from "./js_atomize.mjs";
+import { js_imports_unused_remove } from "./js_imports_unused_remove.mjs";
 export function example_command_lambda(t) {
   if (t[0] === "atomize") {
     return js_atomize;
+  }
+  if (t[0] === "prune") {
+    return js_imports_unused_remove;
   }
   if (t[0] === "aea") {
     return function_arguments_assert_each_add_lambda(t[2]);
