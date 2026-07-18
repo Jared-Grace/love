acca27e4f70437c0b5db9136c96c912f742118c7
JESUS rose to life <username@users.noreply.github.com>
2026-07-18 09:21:05 +0800

    ai

---
+++ js/examples_corpus_read.mjs
@@ -0,0 +1,21 @@
+import { folder_read } from "./folder_read.mjs";
+import { list_filter } from "./list_filter.mjs";
+import { list_sort_text } from "./list_sort_text.mjs";
+import { text_ends_with } from "./text_ends_with.mjs";
+import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
+import { property_get } from "./property_get.mjs";
+export async function examples_corpus_read() {
+  let names = await folder_read("data/examples");
+  function is_mjs(name) {
+    return text_ends_with(name, ".mjs");
+  }
+  let mjs = list_filter(names, is_mjs);
+  let sorted = list_sort_text(mjs);
+  async function to_example(name) {
+    let mod = await import("../data/examples/" + name);
+    let example = property_get(mod, "example");
+    return example;
+  }
+  let examples = await list_map_unordered_async(sorted, to_example);
+  return examples;
+}
