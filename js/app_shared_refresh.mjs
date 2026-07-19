4523e50de8be350a2e7180644c9bd4a32e7fc65b
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 13:32:22 +0800

    ai

--- js/app_shared_refresh.mjs
+++ js/app_shared_refresh.mjs
@@ -5,6 +5,8 @@ import { property_get } from "./property_get.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { html_document_body } from "./html_document_body.mjs";
 import { list_find_property } from "./list_find_property.mjs";
+import { property_get_or_null } from "./property_get_or_null.mjs";
+import { null_not_is } from "./null_not_is.mjs";
 export async function app_shared_refresh(context) {
   let screens = property_get(context, "screens");
   let body = html_document_body();
@@ -17,5 +19,10 @@ export async function app_shared_refresh(context) {
   );
   let screen = list_find_property(screens, "name", screen_name);
   await screen(context);
+  let after_refresh = property_get_or_null(context, "after_refresh");
+  let has_after = null_not_is(after_refresh);
+  if (has_after) {
+    after_refresh(context);
+  }
   html_scroll_top_window();
 }
