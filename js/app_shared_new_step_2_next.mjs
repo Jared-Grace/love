733e72b259789843a2339c382995c67af8ea0c71
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 09:08:12 +0800

    ai

--- js/app_shared_new_step_2_next.mjs
+++ js/app_shared_new_step_2_next.mjs
@@ -3,7 +3,7 @@ import { app_shared_latest_update } from "./app_shared_latest_update.mjs";
 import { app_shared_new_fn_main } from "./app_shared_new_fn_main.mjs";
 import { app_shared_production_update } from "./app_shared_production_update.mjs";
 import { html_new } from "./html_new.mjs";
-export async function app_new_step_2_next(name) {
+export async function app_shared_new_step_2_next(name) {
   await app_shared_new_fn_main(name);
   await app_new_fn_latest(name);
   await app_shared_production_update(name);
