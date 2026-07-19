d07afd8f5c6b584c3359a6da683291ec8cda583f
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 11:36:16 +0800

    ai

--- js/app_main_production.mjs
+++ js/app_main_production.mjs
@@ -1,7 +1,7 @@
 import { firebase_deploy_function_destination_production } from "./firebase_deploy_function_destination_production.mjs";
-import { app_main } from "./app_main.mjs";
+import { app_shared_main } from "./app_shared_main.mjs";
 export async function app_main_production(f_name, firebase_name_value) {
-  let v = await app_main(
+  let v = await app_shared_main(
     f_name,
     firebase_name_value,
     firebase_deploy_function_destination_production,
