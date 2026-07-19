d36b3c9717bfc19bedf195cfcc72579fda313bf0
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 06:32:42 +0800

    ai

--- js/js_code_call_app_context_initialize.mjs
+++ js/js_code_call_app_context_initialize.mjs
@@ -1,7 +1,7 @@
-import { app_context_initialize } from "./app_context_initialize.mjs";
+import { app_shared_context_initialize } from "./app_shared_context_initialize.mjs";
 import { js_code_call_args_statement } from "./js_code_call_args_statement.mjs";
 export function js_code_call_app_context_initialize(name_prefixed) {
-  let result = js_code_call_args_statement(app_context_initialize.name, [
+  let result = js_code_call_args_statement(app_shared_context_initialize.name, [
     name_prefixed,
   ]);
   return result;
