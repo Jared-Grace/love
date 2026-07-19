9065cb491c1f160b61aac9e781d8457639de6d38
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 14:42:11 +0800

    ai

--- js/pwa_icon_emoji.mjs
+++ js/pwa_icon_emoji.mjs
@@ -2,7 +2,7 @@ import { emoji_pray } from "./emoji_pray.mjs";
 import { emoji_book_open } from "./emoji_book_open.mjs";
 import { equal } from "./equal.mjs";
 export function pwa_icon_emoji(app_name) {
-  if (equal(app_name, "app_verses")) {
+  if (equal(app_name, "verses")) {
     return emoji_book_open();
   }
   return emoji_pray();
