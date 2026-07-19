ef4ae80f4c198c4ac78672d857670bd24e9c5d80
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 10:01:35 +0800

    ai

--- js/js_operator_plus.mjs
+++ js/js_operator_plus.mjs
@@ -1,11 +1,13 @@
 import { js_operator_plus_symbol } from "./js_operator_plus_symbol.mjs";
 import { js_operator_plus_verb } from "./js_operator_plus_verb.mjs";
 import { identity } from "./identity.mjs";
+import { add } from "./add.mjs";
 export function js_operator_plus() {
   let o = {
     operator: js_operator_plus_symbol(),
     verb: js_operator_plus_verb(),
     left_transform: identity,
+    fn: add,
   };
   return o;
 }
