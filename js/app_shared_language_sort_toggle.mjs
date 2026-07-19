d5c4fe2b3b6c6fa590fdaf148ecb2169eddebc5b
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 21:59:49 +0800

    ai

--- js/app_shared_language_sort_toggle.mjs
+++ js/app_shared_language_sort_toggle.mjs
@@ -3,7 +3,7 @@ import { app_shared_language_sort_set } from "./app_shared_language_sort_set.mjs
 import { app_shared_language_sort_by_speakers } from "./app_shared_language_sort_by_speakers.mjs";
 import { app_shared_language_sort_by_name } from "./app_shared_language_sort_by_name.mjs";
 import { equal } from "./equal.mjs";
-export function app_language_sort_toggle() {
+export function app_shared_language_sort_toggle() {
   let mode = app_shared_language_sort_get();
   let by_name = equal(mode, app_shared_language_sort_by_name());
   let next = app_shared_language_sort_by_name();
