cdad33a164345f3b56ff4668a251b1b4097a88a5
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 11:13:25 +0800

    ai

--- js/apps_frozen.mjs
+++ js/apps_frozen.mjs
@@ -2,6 +2,6 @@ import { app_replace } from "../../love/js/app_replace.mjs";
 import { app_g } from "../../love/js/app_g.mjs";
 export function apps_frozen() {
   "these apps should not be changed in prod or deleted";
-  let r = [app_g, app_replace];
+  let r = [ app_replace];
   return r;
 }
