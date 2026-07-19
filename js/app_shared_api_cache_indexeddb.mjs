09a2760d363aab68e399bd57f7225131ab32f059
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 20:52:03 +0800

    ai

--- js/app_shared_api_cache_indexeddb.mjs
+++ js/app_shared_api_cache_indexeddb.mjs
@@ -1,6 +1,11 @@
 import { http_post_json_cache_indexeddb } from "./http_post_json_cache_indexeddb.mjs";
 import { app_api_generic } from "./app_api_generic.mjs";
-export async function app_api_cache_indexeddb(f_name, args, db_get, store) {
+export async function app_shared_api_cache_indexeddb(
+  f_name,
+  args,
+  db_get,
+  store,
+) {
   let fn = async function lambda(url, body) {
     let v = await http_post_json_cache_indexeddb(url, body, db_get, store);
     return v;
--- js/app_shared_api_cache_storage_local_fn.mjs
+++ js/app_shared_api_cache_storage_local_fn.mjs
@@ -1,5 +1,5 @@
 import { app_api_cache_storage_local } from "./app_api_cache_storage_local.mjs";
-export async function app_api_cache_storage_local_fn(fn, args) {
+export async function app_shared_api_cache_storage_local_fn(fn, args) {
   let f_name = fn.name;
   let v = await app_api_cache_storage_local(f_name, args);
   return v;
