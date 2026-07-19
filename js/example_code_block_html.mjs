563f8b88fb99fd8e201a913caa76eb5fd475027e
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 11:19:42 +0800

    ai

--- js/example_code_block_html.mjs
+++
@@ -1,8 +0,0 @@
-import { js_highlight_html } from "./js_highlight_html.mjs";
-import { html_code_element } from "./html_code_element.mjs";
-export function example_code_block_html(code, class_name) {
-  let highlighted = js_highlight_html(code);
-  let attributes = { class: class_name };
-  let r = html_code_element("pre", attributes, highlighted);
-  return r;
-}
