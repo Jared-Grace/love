9a7ff3870bbe9cb4f69fc02b13bf291817ecd16f
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 11:36:16 +0800

    ai

--- js/app_karate_latest.mjs
+++ js/app_karate_latest.mjs
@@ -1,7 +1,7 @@
 import { firebase_deploy_function_destination_latest } from "./firebase_deploy_function_destination_latest.mjs";
-import { app_main } from "./app_main.mjs";
+import { app_shared_main } from "./app_shared_main.mjs";
 export async function app_karate_latest() {
-  await app_main(
+  await app_shared_main(
     "app_karate_main",
     "karate-code",
     firebase_deploy_function_destination_latest,
