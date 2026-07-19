db4506b1be8d8f6b1a3cd623b716fc079dfa31fe
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 20:20:18 +0800

    ai

--- js/app_shared_api_cache_args.mjs
+++ js/app_shared_api_cache_args.mjs
@@ -1,6 +1,6 @@
 import { server_url_api } from "./server_url_api.mjs";
 import { app_api_generic_f_name_args } from "./app_api_generic_f_name_args.mjs";
-export function app_api_cache_args(fn, f_args) {
+export function app_shared_api_cache_args(fn, f_args) {
   let a = app_api_generic_f_name_args(fn.name, f_args);
   let a2 = server_url_api();
   let args_cache = [a2, a];
---
+++ js/app_shared_api_cache_clear.mjs
@@ -0,0 +1,7 @@
+import { app_shared_api_cache_args } from "./app_shared_api_cache_args.mjs";
+import { http_post_json } from "./http_post_json.mjs";
+import { invoke_cache_clear } from "./invoke_cache_clear.mjs";
+export function app_shared_api_cache_clear(fn, f_args) {
+  let args_cache = app_shared_api_cache_args(fn, f_args);
+  invoke_cache_clear(http_post_json, args_cache);
+}
