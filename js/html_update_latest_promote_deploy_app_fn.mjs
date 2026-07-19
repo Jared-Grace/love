457d508717d5e88565ef379133395696cb25c7b8
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 21:51:50 +0800

    ai

--- js/html_update_latest_promote_deploy_app_fn.mjs
+++ js/html_update_latest_promote_deploy_app_fn.mjs
@@ -1,6 +1,6 @@
 import { html_update_latest_promote_deploy } from "./html_update_latest_promote_deploy.mjs";
-import { app_prefix_without_fn } from "./app_prefix_without_fn.mjs";
+import { app_shared_name_prefix_without_fn } from "./app_shared_name_prefix_without_fn.mjs";
 export async function html_update_latest_promote_deploy_app_fn(app_fn) {
-  let without = app_prefix_without_fn(app_fn);
+  let without = app_shared_name_prefix_without_fn(app_fn);
   await html_update_latest_promote_deploy(without);
 }
--- js/html_update_latest_promote_deploy_code.mjs
+++ js/html_update_latest_promote_deploy_code.mjs
@@ -1,8 +1,8 @@
-import { app_code } from "../../love/js/app_code.mjs";
-import { app_prefix_without_fn } from "../../love/js/app_prefix_without_fn.mjs";
-import { html_update_latest_promote_deploy } from "../../love/js/html_update_latest_promote_deploy.mjs";
+import { app_code } from "./app_code.mjs";
+import { app_shared_name_prefix_without_fn } from "./app_shared_name_prefix_without_fn.mjs";
+import { html_update_latest_promote_deploy } from "./html_update_latest_promote_deploy.mjs";
 export async function html_update_latest_promote_deploy_code() {
-  let without = app_prefix_without_fn(app_code);
+  let without = app_shared_name_prefix_without_fn(app_code);
   let r = await html_update_latest_promote_deploy(without);
   return r;
 }
