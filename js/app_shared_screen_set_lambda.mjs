11d73ee6025b970d3350e465833fe1ad58c1e138
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 19:02:29 +0800

    ai

--- js/app_shared_screen_set_lambda.mjs
+++ js/app_shared_screen_set_lambda.mjs
@@ -1,5 +1,5 @@
 import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
-export function app_screen_set_lambda(context, screen_fn) {
+export function app_shared_screen_set_lambda(context, screen_fn) {
   let l = function lambda() {
     app_shared_screen_set(context, screen_fn);
   };
