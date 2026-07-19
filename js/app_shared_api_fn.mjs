d5122f631ea2441e6aec653b01e8b6bf2f0954cd
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 20:52:48 +0800

    ai

--- js/app_shared_api_fn.mjs
+++ js/app_shared_api_fn.mjs
@@ -1,7 +1,7 @@
 import { app_api } from "./app_api.mjs";
 import { property_set_exists_not } from "./property_set_exists_not.mjs";
 import { property_get } from "./property_get.mjs";
-export async function app_api_fn(a) {
+export async function app_shared_api_fn(a) {
   let fn = property_get(a, "fn");
   let f_name = fn.name;
   property_set_exists_not(a, "f_name", f_name);
