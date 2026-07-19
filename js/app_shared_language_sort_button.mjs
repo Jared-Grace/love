15b433de54a6dcbba63919202f0da0bec384a01f
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 21:59:49 +0800

    ai

--- js/app_shared_language_sort_button.mjs
+++ js/app_shared_language_sort_button.mjs
@@ -1,14 +1,14 @@
 import { app_shared_button } from "./app_shared_button.mjs";
 import { html_text_set } from "./html_text_set.mjs";
 import { app_shared_language_sort_get } from "./app_shared_language_sort_get.mjs";
-import { app_language_sort_toggle } from "./app_language_sort_toggle.mjs";
+import { app_shared_language_sort_toggle } from "./app_shared_language_sort_toggle.mjs";
 import { app_shared_language_sort_label } from "./app_shared_language_sort_label.mjs";
 export function app_shared_language_sort_button(parent, on_change) {
   let mode = app_shared_language_sort_get();
   let label = app_shared_language_sort_label(mode);
   let button = null;
   function on_click() {
-    app_language_sort_toggle();
+    app_shared_language_sort_toggle();
     let mode_next = app_shared_language_sort_get();
     let label_next = app_shared_language_sort_label(mode_next);
     html_text_set(button, label_next);
