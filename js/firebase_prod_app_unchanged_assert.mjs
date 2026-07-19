654db9ff8c7b3f5baa1afdcf43dc3fbd4a86adec
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 11:19:42 +0800

    ai

--- js/firebase_prod_app_unchanged_assert.mjs
+++ js/firebase_prod_app_unchanged_assert.mjs
@@ -1,7 +1,7 @@
 import { firebase_prod_app_asset_file_names } from "./firebase_prod_app_asset_file_names.mjs";
 import { firebase_prod_asset_unchanged_assert } from "./firebase_prod_asset_unchanged_assert.mjs";
-import { each_async } from "./each_async.mjs";
+import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 export async function firebase_prod_app_unchanged_assert(app_name) {
   let assets = firebase_prod_app_asset_file_names(app_name);
-  await each_async(assets, firebase_prod_asset_unchanged_assert);
+  await list_map_unordered_async(assets, firebase_prod_asset_unchanged_assert);
 }
