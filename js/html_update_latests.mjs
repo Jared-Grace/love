f50225a882d0b2f06a3fc2ea11b75aaa51483d97
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 13:46:08 +0800

    ai

--- js/html_update_latests.mjs
+++
@@ -1,5 +0,0 @@
-import { text_split_comma_each_async } from "./text_split_comma_each_async.mjs";
-import { html_update_latest } from "./html_update_latest.mjs";
-export async function html_update_latests(searches_comma) {
-  await text_split_comma_each_async(searches_comma, html_update_latest);
-}
