428061e3bb1bd47738849f1afd598bc2fcda6c86
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 18:37:51 +0800

    ai

--- js/app_shared_html_open.mjs
+++ js/app_shared_html_open.mjs
@@ -1,7 +1,7 @@
 import { file_open } from "./file_open.mjs";
 import { folder_public_join } from "./folder_public_join.mjs";
 import { file_name_html } from "./file_name_html.mjs";
-export async function app_html_open(a_name) {
+export async function app_shared_html_open(a_name) {
   let file_name = file_name_html(a_name);
   let combined = folder_public_join(file_name);
   await file_open(combined);
