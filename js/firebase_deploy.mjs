1d9b40c1159ec4a327d4125042fdbd1bad25b67f
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 09:37:31 +0800

    ai

--- js/firebase_deploy.mjs
+++ js/firebase_deploy.mjs
@@ -1,13 +1,12 @@
-import { firebase_deploy_generic } from "../../love/js/firebase_deploy_generic.mjs";
-import { apps_frozen_names } from "../../love/js/apps_frozen_names.mjs";
-import { firebase_prod_app_unchanged_assert } from "../../love/js/firebase_prod_app_unchanged_assert.mjs";
-import { firebase_prod_app_backup_auto } from "../../love/js/firebase_prod_app_backup_auto.mjs";
-import { each_async } from "../../love/js/each_async.mjs";
+import { firebase_deploy_bypass_unchaged_assert_confirm } from "./firebase_deploy_bypass_unchaged_assert_confirm.mjs";
+import { firebase_deploy_bypass_unchanged } from "./firebase_deploy_bypass_unchanged.mjs";
+import { apps_frozen_names } from "./apps_frozen_names.mjs";
+import { firebase_prod_app_unchanged_assert } from "./firebase_prod_app_unchanged_assert.mjs";
+import { each_async } from "./each_async.mjs";
 export async function firebase_deploy() {
   let app_names = apps_frozen_names();
   await each_async(app_names, firebase_prod_app_unchanged_assert);
-  await each_async(app_names, firebase_prod_app_backup_auto);
-  let suffix = "";
-  let stdout = await firebase_deploy_generic(suffix);
+  let confirm = firebase_deploy_bypass_unchaged_assert_confirm();
+  let stdout = await firebase_deploy_bypass_unchanged(confirm);
   return stdout;
 }
