afd169467b5141b8e59462a4872389cb8c714a6e
JESUS rose to life <username@users.noreply.github.com>
2026-07-19 09:12:38 +0800

    ai

--- js/app_shared_new.mjs
+++ js/app_shared_new.mjs
@@ -1,6 +1,6 @@
 import { app_apps_all_main_fns_generate } from "./app_apps_all_main_fns_generate.mjs";
 import { app_update } from "./app_update.mjs";
-import { app_new_fn_latest } from "./app_new_fn_latest.mjs";
+import { app_shared_new_fn_latest } from "./app_shared_new_fn_latest.mjs";
 import { app_shared_new_fn_main } from "./app_shared_new_fn_main.mjs";
 import { text_and_empty_not_is_assert_json } from "./text_and_empty_not_is_assert_json.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
@@ -12,7 +12,7 @@ export async function app_shared_new(name) {
   });
   await app_new_fn(name);
   await app_shared_new_fn_main(name);
-  await app_new_fn_latest(name);
+  await app_shared_new_fn_latest(name);
   await app_update(name);
   await app_apps_all_main_fns_generate();
 }
--- js/app_shared_new_step_2_next.mjs
+++ js/app_shared_new_step_2_next.mjs
@@ -1,11 +1,11 @@
-import { app_new_fn_latest } from "./app_new_fn_latest.mjs";
+import { app_shared_new_fn_latest } from "./app_shared_new_fn_latest.mjs";
 import { app_shared_latest_update } from "./app_shared_latest_update.mjs";
 import { app_shared_new_fn_main } from "./app_shared_new_fn_main.mjs";
 import { app_shared_production_update } from "./app_shared_production_update.mjs";
 import { html_new } from "./html_new.mjs";
 export async function app_shared_new_step_2_next(name) {
   await app_shared_new_fn_main(name);
-  await app_new_fn_latest(name);
+  await app_shared_new_fn_latest(name);
   await app_shared_production_update(name);
   await app_shared_latest_update(name);
   await html_new(name);
