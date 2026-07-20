d96f3dac64cf1347bc9a8026d489bd3f6d24da47
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 20:43:33 +0800

    ai

--- js/app_calendar_latest.mjs
+++ js/app_calendar_latest.mjs
@@ -1,4 +1,4 @@
 import { app_shared_main_latest } from "./app_shared_main_latest.mjs";
 export async function app_calendar_latest() {
-  await app_shared_main_latest("app_calendar", "jared-grace");
+  await app_shared_main_latest("app_calendar", firebase_project_name_jg());
 }
