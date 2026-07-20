d49739615a162fffeeba9f775aedeed0263adfdf
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 15:05:02 +0800

    ai

--- js/app_g_npc_says.mjs
+++ js/app_g_npc_says.mjs
@@ -1,6 +1,6 @@
 import { g_game_prefix } from "./g_game_prefix.mjs";
 import { app_g_p_text } from "./app_g_p_text.mjs";
-import { html_img } from "./html_img.mjs";
+import { html_img_retry } from "./html_img_retry.mjs";
 import { g_character_img_url_direction } from "./g_character_img_url_direction.mjs";
 import { g_gender_male } from "./g_gender_male.mjs";
 import { g_gender_female } from "./g_gender_female.mjs";
@@ -19,7 +19,7 @@ export function app_g_npc_says(npc, overlay, npc_says) {
   let container = app_g_container_color(overlay, color_background);
   let game_prefix = g_game_prefix();
   let c_src = g_character_img_url_direction(npc, "south");
-  let component = html_img(container, c_src);
+  let component = html_img_retry(container, c_src);
   let name_map = {
     [g_gender_female()]: "#a3006e",
     [g_gender_male()]: "#1a3aa0",
