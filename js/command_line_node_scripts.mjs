e4dfa542abff78c131c2fb3bb90bf2565a6914a2
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 11:50:31 +0800

    ai

--- js/command_line_node_scripts.mjs
+++ js/command_line_node_scripts.mjs
@@ -1,6 +1,5 @@
 import { log_keep } from "./log_keep.mjs";
-import { property_get } from "./property_get.mjs";
-import { command_line } from "./command_line.mjs";
+import { command_line_stdout } from "./command_line_stdout.mjs";
 import { list_join_space } from "./list_join_space.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export async function command_line_node_scripts(script_name, f_name, args) {
