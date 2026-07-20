65b170f556b11eb2b353f54a5c498dc2b6e04fee
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 10:49:47 +0800

    ai

--- js/firebase_deploy.mjs
+++ js/firebase_deploy.mjs
@@ -4,11 +4,18 @@ import { apps_frozen_names } from "./apps_frozen_names.mjs";
 import { firebase_prod_app_unchanged_assert } from "./firebase_prod_app_unchanged_assert.mjs";
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
 import { qa_gate_run } from "./qa_gate_run.mjs";
+import { lock_error } from "./lock_error.mjs";
+import { firebase_deploy_locked_message } from "./firebase_deploy_locked_message.mjs";
 export async function firebase_deploy() {
-  await qa_gate_run();
-  let app_names = apps_frozen_names();
-  await list_map_unordered_async(app_names, firebase_prod_app_unchanged_assert);
-  let confirm = firebase_deploy_bypass_unchaged_assert_confirm();
-  let stdout = await firebase_deploy_bypass_unchanged(confirm);
-  return stdout;
+  async function lambda() {
+    await qa_gate_run();
+    let app_names = apps_frozen_names();
+    await list_map_unordered_async(app_names, firebase_prod_app_unchanged_assert);
+    let confirm = firebase_deploy_bypass_unchaged_assert_confirm();
+    let stdout = await firebase_deploy_bypass_unchanged(confirm);
+    return stdout;
+  }
+  let message = firebase_deploy_locked_message();
+  let r = await lock_error(firebase_deploy.name, lambda, firebase_deploy.name, message);
+  return r;
 }
