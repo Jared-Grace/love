7f8dbd139fc6cb9a98789c9b653152f959a05bc1
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 20:52:37 +0800

    ai

--- js/firebase_service_account_love.mjs
+++ js/firebase_service_account_love.mjs
@@ -1,3 +1,4 @@
+import { firebase_project_name_jg } from "./firebase_project_name_jg.mjs";
 import { firebase_service_account_love_generic } from "./firebase_service_account_love_generic.mjs";
 export async function firebase_service_account_love() {
   let name = firebase_project_name_jg();
