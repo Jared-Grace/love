89858583178822c914f298e1686e314e409d4dac
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 08:17:02 +0800

    ai

--- js/firebase_deploy.mjs
+++ js/firebase_deploy.mjs
@@ -3,7 +3,9 @@ import { firebase_deploy_bypass_unchanged } from "./firebase_deploy_bypass_uncha
 import { apps_frozen_names } from "./apps_frozen_names.mjs";
 import { firebase_prod_app_unchanged_assert } from "./firebase_prod_app_unchanged_assert.mjs";
 import { list_map_unordered_async } from "./list_map_unordered_async.mjs";
+import { qa_gate_run } from "./qa_gate_run.mjs";
 export async function firebase_deploy() {
+  await qa_gate_run();
   let app_names = apps_frozen_names();
   await list_map_unordered_async(app_names, firebase_prod_app_unchanged_assert);
   let confirm = firebase_deploy_bypass_unchaged_assert_confirm();
