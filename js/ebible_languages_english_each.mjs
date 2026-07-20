d73b3daec0bc71eb64d7614f499583ccfe1b0c0f
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 22:14:12 +0800

    ai

--- js/ebible_languages_english_each.mjs
+++ js/ebible_languages_english_each.mjs
@@ -1,3 +1,4 @@
+import { ebible_folder_english } from "./ebible_folder_english.mjs";
 import { ebible_languages_without_original_english_bible_folders_each } from "./ebible_languages_without_original_english_bible_folders_each.mjs";
 import { ebible_versions_english_choices_each } from "./ebible_versions_english_choices_each.mjs";
 export async function ebible_languages_english_each(lambda$bible_folder) {
