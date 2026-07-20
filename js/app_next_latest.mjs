a596e4e370fee3cf6b4ecda178816a02418d2dcb
JESUS rose to life <username@users.noreply.github.com>
2026-07-20 20:43:50 +0800

    ai

--- js/app_next_latest.mjs
+++ js/app_next_latest.mjs
@@ -1,4 +1,4 @@
 import { app_shared_main_latest } from "./app_shared_main_latest.mjs";
 export async function app_next_latest() {
-  await app_shared_main_latest("app_next", "jared-grace");
+  await app_shared_main_latest("app_next", firebase_project_name_jg());
 }
