08424b7607ace0894d66cc3ffe8cc7f8b0c3b78d
JESUS rose to life <username@users.noreply.github.com>
2026-07-23 17:58:10 +0800

    ai

--- js/app_code_quiz_index_transform_get.mjs
+++ js/app_code_quiz_index_transform_get.mjs
@@ -4,7 +4,6 @@ import { app_code_quiz_index_get } from "./app_code_quiz_index_get.mjs";
 export function app_code_quiz_index_transform_get(context, quizzes, transform) {
   let quiz_index = app_code_quiz_index_get(context);
   let a = transform(quiz_index);
-  let size = list_size(quizzes);
-  let index_new = mod(a, size);
+  let index_new = list_get_wrap_index(quizzes, a);
   return index_new;
 }
