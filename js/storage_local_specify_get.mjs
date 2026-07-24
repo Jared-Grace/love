b5900b2d49aeede3f9fbe7e67e57ae0942d16fc5
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 17:25:12 +0800

    ai

--- js/storage_local_specify_get.mjs
+++ js/storage_local_specify_get.mjs
@@ -1,7 +1,7 @@
+import { storage_json_parse_or_throw } from "./storage_json_parse_or_throw.mjs";
 import { property_get } from "./property_get.mjs";
 import { null_not_is } from "./null_not_is.mjs";
 import { storage_local_specify_get_json } from "./storage_local_specify_get_json.mjs";
-import { json_from } from "./json_from.mjs";
 export function storage_local_specify_get(storage_local_key) {
   let json = storage_local_specify_get_json(storage_local_key);
   let nn = null_not_is(json);
