0749692d40fc8fce3a7a407d2d488d8b4830ea88
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 10:22:39 +0800

    ai

--- js/range_1.mjs
+++ js/range_1.mjs
@@ -1,8 +1,6 @@
+import { range_map } from "./range_map.mjs";
 import { add_1 } from "./add_1.mjs";
-import { list_map } from "./list_map.mjs";
-import { range } from "./range.mjs";
 export function range_1(count) {
-  let r_before = range(count);
-  let r = list_map(r_before, add_1);
+  let r = range_map(count, add_1);
   return r;
 }
