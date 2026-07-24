d97f528683113d1105a942f2b1a7ca067e35ce68
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 11:45:40 +0800

    ai

--- js/app_shared_button_style.mjs
+++ js/app_shared_button_style.mjs
@@ -2,11 +2,11 @@ import { html_style_background_color_set } from "./html_style_background_color_s
 import { app_shared_button_background_color } from "./app_shared_button_background_color.mjs";
 import { app_shared_symbol_tile_style } from "./app_shared_symbol_tile_style.mjs";
 import { html_border } from "./html_border.mjs";
-import { app_shared_color_gray } from "./app_shared_color_gray.mjs";
+import { app_shared_color_gray_light } from "./app_shared_color_gray_light.mjs";
 export function app_shared_button_style(b) {
   app_shared_symbol_tile_style(b);
   let c = app_shared_button_background_color();
   html_style_background_color_set(b, c);
-  let border_color = app_shared_color_gray();
+  let border_color = app_shared_color_gray_light();
   html_border(b, "0.05em", border_color);
 }
