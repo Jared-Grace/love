27d74049ce9ad38ff48d6fc8158024fdb8d9e983
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 15:10:51 +0800

    ai

--- js/list_remove_all_multiple.mjs
+++ js/list_remove_all_multiple.mjs
@@ -1,8 +1,8 @@
-import { list_remove_all } from "./list_remove_all.mjs";
+import { list_remove_every } from "./list_remove_every.mjs";
 import { each } from "./each.mjs";
 export function list_remove_all_multiple(removals, list) {
   function lambda(r) {
-    list_remove_all(list, r);
+    list_remove_every(list, r);
   }
   each(removals, lambda);
 }
--- js/list_remove_every.mjs
+++ js/list_remove_every.mjs
@@ -1,6 +1,6 @@
 import { list_remove } from "./list_remove.mjs";
 import { list_includes } from "./list_includes.mjs";
-export function list_remove_all(list, item) {
+export function list_remove_every(list, item) {
   while (list_includes(list, item)) {
     list_remove(list, item);
   }
