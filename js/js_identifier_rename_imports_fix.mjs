13f247f1a3defbb6f66212123af28abdcc65d7d1
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 13:20:27 +0800

    ai

--- js/js_identifier_rename_imports_fix.mjs
+++ js/js_identifier_rename_imports_fix.mjs
@@ -1,10 +1,12 @@
 import { js_imports_paths_fix } from "./js_imports_paths_fix.mjs";
 import { js_identifier_rename } from "./js_identifier_rename.mjs";
+import { js_imports_dedupe } from "./js_imports_dedupe.mjs";
 export async function js_identifier_rename_imports_fix(
   ast,
   f_name_before,
   f_name_after,
 ) {
   js_identifier_rename(ast, f_name_before, f_name_after);
+  js_imports_dedupe(ast);
   await js_imports_paths_fix(ast);
 }
