ad432aa8e33255d79fc8e4769da62628850a8d6f
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 10:05:04 +0800

    ai

--- js/app_shared_button_style.mjs
+++ js/app_shared_button_style.mjs
@@ -2,10 +2,10 @@ import { html_style_background_color_set } from "../../love/js/html_style_backgr
 import { app_shared_button_background_color } from "../../love/js/app_shared_button_background_color.mjs";
 import { app_shared_symbol_tile_style } from "../../love/js/app_shared_symbol_tile_style.mjs";
 import { html_border } from "../../love/js/html_border.mjs";
-import { app_shared_container_blue_border_color } from "../../love/js/app_shared_container_blue_border_color.mjs";
+import { app_shared_color_gray } from "../../love/js/app_shared_color_gray.mjs";
 export function app_shared_button_style(b) {
   app_shared_symbol_tile_style(b);
   let c = app_shared_button_background_color();
   html_style_background_color_set(b, c);
-  html_border(b, "0.1em", app_shared_container_blue_border_color());
+  html_border(b, "0.1em", app_shared_color_gray());
 }
