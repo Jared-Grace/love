ff850ad3cb74fd0769592ee20922c903ad425476
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 10:06:29 +0800

    ai

--- js/ebible_text_to_speech_chapter_generic.mjs
+++ js/ebible_text_to_speech_chapter_generic.mjs
@@ -1,14 +1,12 @@
+import { list_map_property_join_space } from "./list_map_property_join_space.mjs";
 import { text_to_speech } from "./text_to_speech.mjs";
 import { bible_audio_folder } from "./bible_audio_folder.mjs";
-import { list_join_space } from "./list_join_space.mjs";
-import { list_map_property } from "./list_map_property.mjs";
 export async function ebible_text_to_speech_chapter_generic(
   bible_folder,
   verses,
   chapter_code,
 ) {
-  let mapped = list_map_property(verses, "text");
-  let text = list_join_space(mapped);
+  let text = list_map_property_join_space(verses, "text");
   let f = bible_audio_folder(bible_folder, chapter_code);
   await text_to_speech({
     text: text,
