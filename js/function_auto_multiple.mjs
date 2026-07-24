696d589e840ed532558f47fce344fbf62336ccf4
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 17:25:12 +0800

    ai

--- js/function_auto_multiple.mjs
+++ js/function_auto_multiple.mjs
@@ -1,5 +1,5 @@
-import { each_unordered_async } from "./each_unordered_async.mjs";
 import { function_auto } from "./function_auto.mjs";
-export async function function_auto_multiple(list) {
-  await each_unordered_async(list, function_auto);
+import { text_split_comma_each_async } from "./text_split_comma_each_async.mjs";
+export async function function_auto_multiple(names_comma) {
+  await text_split_comma_each_async(names_comma, function_auto);
 }
