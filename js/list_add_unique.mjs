6a1d261e6bf3ef0bf3feb7b5641b33310b627903
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 15:10:51 +0800

    ai

--- js/list_add_unique.mjs
+++ js/list_add_unique.mjs
@@ -1,6 +1,6 @@
 import { list_add } from "./list_add.mjs";
-import { list_remove_all } from "./list_remove_all.mjs";
+import { list_remove_every } from "./list_remove_every.mjs";
 export function list_add_unique(list, item) {
-  list_remove_all(list, item);
+  list_remove_every(list, item);
   list_add(list, item);
 }
