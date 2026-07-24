fa3b2cd44667fe7aff0d961d7716d5c07bc6222d
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 20:52:45 +0800

    ai

--- js/function_copy_generic_async.mjs
+++ js/function_copy_generic_async.mjs
@@ -1,8 +1,8 @@
-import { function_copy_generic_args } from "./function_copy_generic_args.mjs";
+import { function_copy_generic } from "./function_copy_generic.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-export async function function_copy_generic_args_async(f_name_old_args_comma) {
+export async function function_copy_generic_async(f_name_old_args_comma) {
   arguments_assert(arguments, 1);
   let plugin_fn = function_name_combine.name;
   f_name_old_args_comma = text_combine_multiple([
@@ -10,6 +10,6 @@ export async function function_copy_generic_args_async(f_name_old_args_comma) {
     ",",
     "async",
   ]);
-  let r = await function_copy_generic_args(plugin_fn, f_name_old_args_comma);
+  let r = await function_copy_generic(plugin_fn, f_name_old_args_comma);
   return r;
 }
