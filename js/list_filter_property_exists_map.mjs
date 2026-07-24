5870cd7d6b2595e4dfbf86367ed75b02e5dcd112
JESUS rose to life <username@users.noreply.github.com>
2026-07-24 12:47:10 +0800

    ai

--- js/list_filter_property_exists_map.mjs
+++
@@ -1,7 +0,0 @@
-import { list_map_property } from "./list_map_property.mjs";
-import { list_filter_property_exists } from "./list_filter_property_exists.mjs";
-export function list_filter_property_exists_map(list, property_name) {
-  let filtered = list_filter_property_exists(list, property_name);
-  let mapped = list_map_property(filtered, property_name);
-  return mapped;
-}
--- js/list_filter_property_exists_map_fn.mjs
+++ js/list_filter_property_exists_map_fn.mjs
@@ -1,6 +1,6 @@
-import { list_filter_property_exists_map } from "./list_filter_property_exists_map.mjs";
+import { list_map_property_exists } from "./list_map_property_exists.mjs";
 export function list_filter_property_exists_map_fn(list, fn) {
   let property_name = fn();
-  let mapped = list_filter_property_exists_map(list, property_name);
+  let mapped = list_map_property_exists(list, property_name);
   return mapped;
 }
