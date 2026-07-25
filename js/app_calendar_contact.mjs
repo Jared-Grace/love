e2bd46d03f261fa729e9f8a1ea35d659089bd87b
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 21:41:53 +0800

    ai

--- js/app_calendar_contact.mjs
+++ js/app_calendar_contact.mjs
@@ -1,19 +1,19 @@
-import { error } from "../../love/js/error.mjs";
-import { log } from "../../love/js/log.mjs";
-import { app_calendar_paste_convert } from "../../love/js/app_calendar_paste_convert.mjs";
-import { clipboard_paste } from "../../love/js/clipboard_paste.mjs";
-import { app_calendar_id_properties } from "../../love/js/app_calendar_id_properties.mjs";
-import { object_pick_try_single_value } from "../../love/js/object_pick_try_single_value.mjs";
-import { storage_session_get_context } from "../../love/js/storage_session_get_context.mjs";
-import { property_get } from "../../love/js/property_get.mjs";
-import { app_shared_screen_set } from "../../love/js/app_shared_screen_set.mjs";
-import { app_shared_button_home_text } from "../../love/js/app_shared_button_home_text.mjs";
-import { app_a_button_wide } from "../../love/js/app_a_button_wide.mjs";
-import { app_shared_text_body } from "../../love/js/app_shared_text_body.mjs";
-import { json_to } from "../../love/js/json_to.mjs";
-import { app_calendar_download_browser_contacts } from "../../love/js/app_calendar_download_browser_contacts.mjs";
-import { html_clear_context } from "../../love/js/html_clear_context.mjs";
-import { app_calendar_home } from "../../love/js/app_calendar_home.mjs";
+import { error } from "./error.mjs";
+import { log } from "./log.mjs";
+import { app_calendar_paste_convert } from "./app_calendar_paste_convert.mjs";
+import { clipboard_paste } from "./clipboard_paste.mjs";
+import { app_calendar_id_properties } from "./app_calendar_id_properties.mjs";
+import { object_pick_try_single_value } from "./object_pick_try_single_value.mjs";
+import { storage_session_get_context } from "./storage_session_get_context.mjs";
+import { property_get } from "./property_get.mjs";
+import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
+import { app_shared_button_home_text } from "./app_shared_button_home_text.mjs";
+import { app_a_button_wide } from "./app_a_button_wide.mjs";
+import { app_shared_text_body } from "./app_shared_text_body.mjs";
+import { json_to } from "./json_to.mjs";
+import { app_calendar_download_browser_contacts } from "./app_calendar_download_browser_contacts.mjs";
+import { html_clear_context } from "./html_clear_context.mjs";
+import { app_calendar_home } from "./app_calendar_home.mjs";
 export async function app_calendar_contact(context) {
   let root = html_clear_context(context);
   async function lambda2() {
