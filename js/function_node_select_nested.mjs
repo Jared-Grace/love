bf2219493c23aade84fea8586f59620b5d3e5419
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 14:07:28 +0800

    ai

--- js/function_node_select_nested.mjs
+++ js/function_node_select_nested.mjs
@@ -1,17 +1,7 @@
-import { list_remove_curried_right } from "./list_remove_curried_right.mjs";
-import { function_current_selects_first } from "./function_current_selects_first.mjs";
-import { function_current_ast } from "./function_current_ast.mjs";
-import { function_node_select_inner } from "./function_node_select_inner.mjs";
+import { arguments_assert } from "./arguments_assert.mjs";
+import { function_node_select_nested_args } from "./function_node_select_nested_args.mjs";
 export async function function_node_select_nested(select_fn_name) {
-  let ast = await function_current_ast();
-  let first_id = await function_current_selects_first();
-  let r2 = list_remove_curried_right(first_id);
-  let r = await function_node_select_inner(
-    select_fn_name,
-    first_id,
-    r2,
-    ast,
-    args,
-  );
+  arguments_assert(arguments, 1);
+  let r = await function_node_select_nested_args(select_fn_name, null);
   return r;
 }
