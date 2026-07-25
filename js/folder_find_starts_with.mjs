acb73d4015a68fb31e6c5effb46fb1fac5bcd557
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 23:14:32 +0800

    ai

--- js/folder_find_starts_with.mjs
+++ js/folder_find_starts_with.mjs
@@ -1,8 +1,8 @@
-import { folder_read_async } from "./folder_read_async.mjs";
+import { folder_read_files } from "./folder_read_files.mjs";
 import { list_find } from "./list_find.mjs";
 import { text_starts_with } from "./text_starts_with.mjs";
 export async function folder_find_starts_with(path_folder, prefix) {
-  let files = await folder_read_async(path_folder);
+  let files = await folder_read_files(path_folder);
   function lambda(item) {
     let sw = text_starts_with(item, prefix);
     return sw;
