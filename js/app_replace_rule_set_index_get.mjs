525146462099f939cb9ade61ae4c59c2754ccf14
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 21:55:43 +0800

    ai

--- js/app_replace_rule_set_index_get.mjs
+++ js/app_replace_rule_set_index_get.mjs
@@ -1,5 +1,5 @@
-import { storage_local_get_context } from "./storage_local_get_context.mjs";
+import { storage_session_get_context } from "./storage_session_get_context.mjs";
 export function app_replace_rule_set_index_get(context) {
-  let value = storage_local_get_context(context, "rule_set_index");
+  let value = storage_session_get_context(context, "rule_set_index");
   return value;
 }
