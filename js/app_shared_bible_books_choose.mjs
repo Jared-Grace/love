e9c980db65bc3db6570b78c74742484f22735248
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 19:14:44 +0800

    ai

--- js/app_shared_bible_books_choose.mjs
+++ js/app_shared_bible_books_choose.mjs
@@ -1,12 +1,13 @@
-import { app_shared_bible_panel_open } from "../../love/js/app_shared_bible_panel_open.mjs";
-import { window_reload } from "../../love/js/window_reload.mjs";
-import { app_shared_button_list_centered } from "../../love/js/app_shared_button_list_centered.mjs";
-import { property_get } from "../../love/js/property_get.mjs";
-import { app_shared_bible_book_open } from "../../love/js/app_shared_bible_book_open.mjs";
+import { app_shared_bible_panel_open } from "./app_shared_bible_panel_open.mjs";
+import { window_reload } from "./window_reload.mjs";
+import { app_shared_button_list_centered } from "./app_shared_button_list_centered.mjs";
+import { property_get } from "./property_get.mjs";
+import { app_shared_bible_book_open } from "./app_shared_bible_book_open.mjs";
 export function app_shared_bible_books_choose(content, books) {
   app_shared_bible_panel_open(content, window_reload);
   function item_to_text(book) {
-    return property_get(book, "text");
+    let value = property_get(book, "text");
+    return value;
   }
   function on_click(book) {
     let book_code = property_get(book, "book_code");
