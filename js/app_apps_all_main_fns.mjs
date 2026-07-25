9aebf3193e65a182910d04c65d0b717e01b22d45
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 14:06:39 +0800

    ai

--- js/app_apps_all_main_fns.mjs
+++ js/app_apps_all_main_fns.mjs
@@ -1,29 +1,55 @@
+import { app_a } from "./app_a.mjs";
+import { app_apps_all } from "./app_apps_all.mjs";
+import { app_autopray } from "./app_autopray.mjs";
+import { app_bible } from "./app_bible.mjs";
+import { app_calendar } from "./app_calendar.mjs";
+import { app_calendar_paste } from "./app_calendar_paste.mjs";
+import { app_ceb_bible } from "./app_ceb_bible.mjs";
+import { app_code } from "./app_code.mjs";
+import { app_designs_universal } from "./app_designs_universal.mjs";
+import { app_examples } from "./app_examples.mjs";
+import { app_g } from "./app_g.mjs";
+import { app_g_bible } from "./app_g_bible.mjs";
+import { app_index } from "./app_index.mjs";
+import { app_karate } from "../../karate_code/js/app_karate.mjs";
+import { app_message } from "./app_message.mjs";
+import { app_next } from "./app_next.mjs";
+import { app_original_bible } from "./app_original_bible.mjs";
+import { app_replace } from "./app_replace.mjs";
+import { app_reply } from "./app_reply.mjs";
+import { app_reply_local } from "./app_reply_local.mjs";
+import { app_sandbox } from "./app_sandbox.mjs";
+import { app_search } from "./app_search.mjs";
+import { app_supper } from "./app_supper.mjs";
+import { app_supper_tl } from "./app_supper_tl.mjs";
+import { app_verses } from "./app_verses.mjs";
 export function app_apps_all_main_fns() {
-  return [
-    "app_a",
-    "app_apps_all",
-    "app_autopray",
-    "app_bible",
-    "app_calendar",
-    "app_calendar_paste",
-    "app_ceb_bible",
-    "app_code",
-    "app_designs_universal",
-    "app_examples",
-    "app_g",
-    "app_g_bible",
-    "app_index",
-    "app_karate",
-    "app_message",
-    "app_next",
-    "app_original_bible",
-    "app_replace",
-    "app_reply",
-    "app_reply_local",
-    "app_sandbox",
-    "app_search",
-    "app_supper",
-    "app_supper_tl",
-    "app_verses",
+  let r = [
+    app_a.name,
+    app_apps_all.name,
+    app_autopray.name,
+    app_bible.name,
+    app_calendar.name,
+    app_calendar_paste.name,
+    app_ceb_bible.name,
+    app_code.name,
+    app_designs_universal.name,
+    app_examples.name,
+    app_g.name,
+    app_g_bible.name,
+    app_index.name,
+    app_karate.name,
+    app_message.name,
+    app_next.name,
+    app_original_bible.name,
+    app_replace.name,
+    app_reply.name,
+    app_reply_local.name,
+    app_sandbox.name,
+    app_search.name,
+    app_supper.name,
+    app_supper_tl.name,
+    app_verses.name,
   ];
+  return r;
 }
