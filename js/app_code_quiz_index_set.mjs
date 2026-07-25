8b5f80a3f988a882e0a8d782d47a29359c6f1bb0
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 20:51:53 +0800

    ai

--- js/app_code_quiz_index_set.mjs
+++ js/app_code_quiz_index_set.mjs
@@ -1,4 +1,4 @@
-import { storage_local_set_context } from "./storage_local_set_context.mjs";
+import { storage_session_set_context } from "./storage_session_set_context.mjs";
 export function app_code_quiz_index_set(context, index_new) {
-  storage_local_set_context(context, "quiz_index", index_new);
+  storage_session_set_context(context, "quiz_index", index_new);
 }
