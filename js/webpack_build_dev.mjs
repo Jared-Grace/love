26499e476830cf39fb1d90ca04b15aedd99946c1
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 16:50:35 +0800

    ai

--- js/webpack_build_dev.mjs
+++ js/webpack_build_dev.mjs
@@ -4,13 +4,16 @@ import { app_shared_name_search_info } from "./app_shared_name_search_info.mjs";
 import { property_get } from "./property_get.mjs";
 import { repo_path_combine } from "./repo_path_combine.mjs";
 import { folder_public_join } from "./folder_public_join.mjs";
+import { webpack_dev_chunks_clean } from "./webpack_dev_chunks_clean.mjs";
 export async function webpack_build_dev(search) {
   let command_text_after = "";
   let info = await app_shared_name_search_info(search);
   let repo_name = property_get(info, "repo_name");
+  let a_name = property_get(info, "a_name");
   let folder = app_shared_name_dev_text();
   let public_dev = folder_public_join(folder);
   let config_folder = repo_path_combine(repo_name, public_dev);
+  await webpack_dev_chunks_clean(config_folder, a_name);
   let result = await webpack_build_generic(
     search,
     config_folder,
