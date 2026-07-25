d9ca9a7069bda576644f6b8d65b25bae9b045aa0
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 15:51:31 +0800

    ai

--- js/app_a_indexeddb_initialize.mjs
+++ js/app_a_indexeddb_initialize.mjs
@@ -1,9 +1,8 @@
-import { app_a_indexeddb_path_key } from "../../love/js/app_a_indexeddb_path_key.mjs";
-import { app_a } from "../../love/js/app_a.mjs";
-import { error } from "../../love/js/error.mjs";
-import { not } from "../../love/js/not.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { app_a_indexeddb_path_key } from "./app_a_indexeddb_path_key.mjs";
+import { not } from "./not.mjs";
 export async function app_a_indexeddb_initialize() {
-  let db_name = app_a.name;
+  let db_name = fn_name("app_a");
   let store_files = "files";
   let version = 1;
   let db = await new Promise(function lambda4(resolve, reject) {
