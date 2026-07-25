9481000839ef79f0bb21ae972f4452fda972e82a
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 20:58:26 +0800

    ai

--- js/app_code_quiz.mjs
+++ js/app_code_quiz.mjs
@@ -1,37 +1,38 @@
-import { app_code_quiz_index_reset } from "../../love/js/app_code_quiz_index_reset.mjs";
-import { app_shared_button_wide } from "../../love/js/app_shared_button_wide.mjs";
-import { html_style_margin_top } from "../../love/js/html_style_margin_top.mjs";
-import { app_shared_spaced_gap } from "../../love/js/app_shared_spaced_gap.mjs";
-import { app_code_home } from "../../love/js/app_code_home.mjs";
-import { app_shared_button_home_text } from "../../love/js/app_shared_button_home_text.mjs";
-import { app_code_examples } from "../../love/js/app_code_examples.mjs";
-import { app_code_lesson_text_example_another } from "../../love/js/app_code_lesson_text_example_another.mjs";
-import { app_code_example_answer_gap } from "../../love/js/app_code_example_answer_gap.mjs";
-import { list_single } from "../../love/js/list_single.mjs";
-import { list_shuffle } from "../../love/js/list_shuffle.mjs";
-import { app_code_batch_on_refill } from "../../love/js/app_code_batch_on_refill.mjs";
-import { html_div_text } from "../../love/js/html_div_text.mjs";
-import { app_code_container_light_blue } from "../../love/js/app_code_container_light_blue.mjs";
-import { app_code_container_padded_x } from "../../love/js/app_code_container_padded_x.mjs";
-import { app_shared_screen_set } from "../../love/js/app_shared_screen_set.mjs";
-import { app_code_go_back } from "../../love/js/app_code_go_back.mjs";
-import { app_code_batch_item_get } from "../../love/js/app_code_batch_item_get.mjs";
-import { list_get } from "../../love/js/list_get.mjs";
-import { property_get } from "../../love/js/property_get.mjs";
-import { storage_session_initialize_context } from "../../love/js/storage_session_initialize_context.mjs";
-import { app_code_lesson_current } from "../../love/js/app_code_lesson_current.mjs";
-import { app_code_lesson_title_strip } from "../../love/js/app_code_lesson_title_strip.mjs";
-import { html_clear_context } from "../../love/js/html_clear_context.mjs";
-import { text_combine } from "../../love/js/text_combine.mjs";
+import { app_code_quiz_index_reset } from "./app_code_quiz_index_reset.mjs";
+import { app_shared_button_wide } from "./app_shared_button_wide.mjs";
+import { html_style_margin_top } from "./html_style_margin_top.mjs";
+import { app_shared_spaced_gap } from "./app_shared_spaced_gap.mjs";
+import { app_code_home } from "./app_code_home.mjs";
+import { app_shared_button_home_text } from "./app_shared_button_home_text.mjs";
+import { app_code_examples } from "./app_code_examples.mjs";
+import { app_code_lesson_text_example_another } from "./app_code_lesson_text_example_another.mjs";
+import { app_code_example_answer_gap } from "./app_code_example_answer_gap.mjs";
+import { list_single } from "./list_single.mjs";
+import { list_shuffle } from "./list_shuffle.mjs";
+import { app_code_batch_on_refill } from "./app_code_batch_on_refill.mjs";
+import { html_div_text } from "./html_div_text.mjs";
+import { app_code_container_light_blue } from "./app_code_container_light_blue.mjs";
+import { app_code_container_padded_x } from "./app_code_container_padded_x.mjs";
+import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
+import { app_code_go_back } from "./app_code_go_back.mjs";
+import { app_code_batch_item_get } from "./app_code_batch_item_get.mjs";
+import { list_get } from "./list_get.mjs";
+import { property_get } from "./property_get.mjs";
+import { storage_session_initialize_context } from "./storage_session_initialize_context.mjs";
+import { app_code_lesson_current } from "./app_code_lesson_current.mjs";
+import { app_code_lesson_title_strip } from "./app_code_lesson_title_strip.mjs";
+import { html_clear_context } from "./html_clear_context.mjs";
+import { text_combine } from "./text_combine.mjs";
 export function app_code_quiz(context) {
   let root = html_clear_context(context);
   let lesson = app_code_lesson_current(context);
   app_code_lesson_title_strip(root, context, lesson);
+  let on_batch = app_code_batch_on_refill(list_shuffle);
   let refresh = app_code_batch_item_get(
     root,
     lesson,
     on_batch_item,
-    app_code_batch_on_refill(list_shuffle),
+    on_batch,
     true,
   );
   function on_batch_item(container, bs, refresh, next_get) {
@@ -69,5 +70,6 @@ export function app_code_quiz(context) {
   }
   let g = app_code_container_padded_x(root);
   let b3 = app_shared_button_wide(g, text, lambda2);
-  html_style_margin_top(b3, app_shared_spaced_gap());
+  let value = app_shared_spaced_gap();
+  html_style_margin_top(b3, value);
 }
