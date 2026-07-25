3e7b53cd3b2eb8fe9e568cd6cad26438b90811c5
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 22:50:11 +0800

    ai

--- js/watching_ensure.mjs
+++
@@ -1,9 +0,0 @@
-import { watching_start } from "./watching_start.mjs";
-import { equal } from "./equal.mjs";
-export async function watching_ensure() {
-  if (equal(watching, null)) {
-    let watching = watching_start();
-  }
-  let r = await watching;
-  return r;
-}
