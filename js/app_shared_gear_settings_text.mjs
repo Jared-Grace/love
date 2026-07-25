9ce286efeb8e8549774063041a2004d7566f0120
JESUS rose to life <username@users.noreply.github.com>
2026-07-25 19:07:53 +0800

    ai

--- js/app_shared_gear_settings_text.mjs
+++ js/app_shared_gear_settings_text.mjs
@@ -1,7 +1,5 @@
-import { emoji_gear } from "./emoji_gear.mjs";
-import { text_combine } from "./text_combine.mjs";
+import { app_shared_gear_text } from "./app_shared_gear_text.mjs";
 export function app_shared_gear_settings_text() {
-  let left = emoji_gear();
-  let text = text_combine(left, " Settings");
+  let text = app_shared_gear_text(" Settings");
   return text;
 }
