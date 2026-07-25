458c9b9c8ca17bb5fc5f2f9bd88ac4d3186d986e
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 07:37:42 +0800

    ai

--- js/function_copy_generic_open.mjs
+++ js/function_copy_generic_open.mjs
@@ -1,14 +1,9 @@
-import { function_copy_open } from "./function_copy_open.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { function_name_new_get_args } from "./function_name_new_get_args.mjs";
+import { function_copy_generic } from "./function_copy_generic.mjs";
+import { function_copy_result_open } from "./function_copy_result_open.mjs";
 export async function function_copy_generic_open(plugin_fn, args_comma) {
   arguments_assert(arguments, 2);
-  let f_name_new = null;
-  let f_name_old = null;
-  ({ f_name_new, f_name_old } = await function_name_new_get_args(
-    plugin_fn,
-    args_comma,
-  ));
-  let r = await function_copy_open(f_name_old, f_name_new);
-  return r;
+  let r = await function_copy_generic(plugin_fn, args_comma);
+  let name = await function_copy_result_open(r);
+  return name;
 }
