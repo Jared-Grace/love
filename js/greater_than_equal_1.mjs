96b8f90d27d7d6ec5cabda633e048891c0a7c801
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 20:50:51 +0800

    ai

--- js/greater_than_equal_1.mjs
+++ js/greater_than_equal_1.mjs
@@ -1,5 +1,5 @@
 import { greater_than_equal } from "./greater_than_equal.mjs";
-export function at_least_1(n) {
+export function greater_than_equal_1(n) {
   let r = greater_than_equal(n, 1);
   return r;
 }
