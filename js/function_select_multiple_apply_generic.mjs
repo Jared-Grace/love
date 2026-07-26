7c0ce0bbc091869b27c571c0db2fa4378f3da640
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 08:15:14 +0800

    ai

--- js/function_select_multiple_apply_generic.mjs
+++ js/function_select_multiple_apply_generic.mjs
@@ -1,3 +1,4 @@
+import { function_callee_seam_assert } from "./function_callee_seam_assert.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { function_import } from "./function_import.mjs";
 import { js_select_multiple_apply } from "./js_select_multiple_apply.mjs";
