fc2ccae50a32ad9345201cfce4f21ab34a56a22d
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 15:07:04 +0800

    ai

--- js/app_code_flex_gap.mjs
+++ js/app_code_flex_gap.mjs
@@ -1,5 +1,6 @@
 import { app_code_flex_gap_value } from "./app_code_flex_gap_value.mjs";
 import { html_style_set } from "./html_style_set.mjs";
 export function app_code_flex_gap(container) {
-  html_style_set(container, "gap", app_code_flex_gap_value());
+  let style_value = app_code_flex_gap_value();
+  html_style_set(container, "gap", style_value);
 }
