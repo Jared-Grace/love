3aae9a893bfb8cd769326ea0d9afe369035ef94d
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 10:24:20 +0800

    ai

--- js/shadowing_entries_difference.mjs
+++ js/shadowing_entries_difference.mjs
@@ -12,14 +12,12 @@ export function shadowing_entries_difference(entries, others) {
     function consider(entry) {
       let name = property_get(entry, "name");
       let other = shadowing_entry_or_empty(by_name, name);
-      let duplicated = list_difference(
-        property_get(entry, "duplicated"),
-        property_get(other, "duplicated"),
-      );
-      let shadowed = list_difference(
-        property_get(entry, "shadowed"),
-        property_get(other, "shadowed"),
-      );
+      let list = property_get(entry, "duplicated");
+      let list_other = property_get(other, "duplicated");
+      let duplicated = list_difference(list, list_other);
+      let list2 = property_get(entry, "shadowed");
+      let list_other2 = property_get(other, "shadowed");
+      let shadowed = list_difference(list2, list_other2);
       let count = duplicated.length + shadowed.length;
       let any = greater_than(count, 0);
       if (any) {
