8afdded74708332277833bb87c852e588eb8316c
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 19:08:42 +0800

    ai

--- js/list_slices_size_generic.mjs
+++ js/list_slices_size_generic.mjs
@@ -1,6 +1,6 @@
 import { list_adder } from "./list_adder.mjs";
 import { each_index } from "./each_index.mjs";
-import { at_least } from "./at_least.mjs";
+import { greater_than_equal } from "./greater_than_equal.mjs";
 import { list_size } from "./list_size.mjs";
 import { list_slice } from "./list_slice.mjs";
 import { text_combine } from "./text_combine.mjs";
@@ -8,9 +8,10 @@ export function list_slices_size_generic(count_get, list, min) {
   function lambda3(la) {
     function lambda2(item, index) {
       let count = count_get();
-      let slice = list_slice(list, index, text_combine(index, count));
+      let index_b = text_combine(index, count);
+      let slice = list_slice(list, index, index_b);
       let z = list_size(slice);
-      let al = at_least(z, min);
+      let al = greater_than_equal(z, min);
       if (al) {
         la(slice);
       }
