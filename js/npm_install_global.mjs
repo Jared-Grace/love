2daf8b70caa5da40b3de8de79937838c556e879d
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 21:54:05 +0800

    ai

--- js/npm_install_global.mjs
+++ js/npm_install_global.mjs
@@ -1,5 +1,5 @@
 import { npm_install_generic } from "./npm_install_generic.mjs";
 export async function npm_install_global(package_name) {
-  let before = "-g ";
-  await npm_install_generic(package_name, before);
+  let flags = ["-g"];
+  await npm_install_generic(package_name, flags);
 }
