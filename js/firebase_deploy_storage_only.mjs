87573850b2797f4fd8d7be23fd2a073ee5529cf9
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 21:32:07 +0800

    ai

--- js/firebase_deploy_storage_only.mjs
+++ js/firebase_deploy_storage_only.mjs
@@ -1,4 +1,4 @@
-import { firebase_deploy_generic } from "../../love/js/firebase_deploy_generic.mjs";
+import { firebase_deploy_generic } from "./firebase_deploy_generic.mjs";
 export async function firebase_deploy_storage_only() {
   let stdout = await firebase_deploy_generic(["--only", "storage"]);
   return stdout;
