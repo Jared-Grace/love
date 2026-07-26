2eca03b926eb3e425d987f905a745a34904011d1
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 20:51:37 +0800

    function_rename at_least_assert greater_than_equal_assert

--- js/greater_than_equal_assert.mjs
+++ js/greater_than_equal_assert.mjs
@@ -1,6 +1,6 @@
 import { assert_json_get } from "./assert_json_get.mjs";
 import { greater_than_equal } from "./greater_than_equal.mjs";
-export function at_least_assert(left, right) {
+export function greater_than_equal_assert(left, right) {
   let l = greater_than_equal(left, right);
   function lambda() {
     let r = {
