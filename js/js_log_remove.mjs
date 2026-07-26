60b9add106499ecf6deb1d2d06a131adf32aae27
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 16:58:42 +0800

    u aom

--- js/js_log_remove.mjs
+++ js/js_log_remove.mjs
@@ -1,6 +1,6 @@
 import { js_statement_call_remove } from "./js_statement_call_remove.mjs";
 import { log } from "./log.mjs";
-export function js_log_remove(ast) {
+export function js_log_remove(ast) {'see: ',js_log_remove_workflow
   let fn = log;
   js_statement_call_remove(ast, fn);
 }
