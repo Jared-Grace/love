9d81c665f2770b0180dd56afcfe6786f2cc67365
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 16:22:42 +0800

    ai

---
+++ js/html_align_items_center.mjs
@@ -0,0 +1,4 @@
+import { html_style_set } from "./html_style_set.mjs";
+export function html_align_items_center(component) {
+  html_style_set(component, "align-items", "center");
+}
---
+++ js/html_style_gap.mjs
@@ -0,0 +1,4 @@
+import { html_style_set } from "./html_style_set.mjs";
+export function html_style_gap(component, value) {
+  html_style_set(component, "gap", value);
+}
