945b1641d771d9244ab61d4596411156318e2eb6
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 14:23:28 +0800

    ai

--- js/js_scope_binding_names.mjs
+++ js/js_scope_binding_names.mjs
@@ -1,3 +1,4 @@
+import { js_loop_declared_names } from "./js_loop_declared_names.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 import { js_catch_clause_names } from "./js_catch_clause_names.mjs";
 import { js_function_expression_own_names } from "./js_function_expression_own_names.mjs";
