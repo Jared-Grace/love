f45167c685cfc2871137555b5d86f9831d394188
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 06:12:33 +0800

    function_param_plain_mark g_verify_status_set chapter_code

--- js/g_verify_status_set.mjs
+++ js/g_verify_status_set.mjs
@@ -2,7 +2,12 @@ import { local_function_path_json } from "./local_function_path_json.mjs";
 import { file_overwrite_uncached } from "./file_overwrite_uncached.mjs";
 import { json_format_to } from "./json_format_to.mjs";
 export async function g_verify_status_set(chapter_code, verse, note) {
+  "$plain chapter_code";
   let path = local_function_path_json(chapter_code, g_verify_status_set);
-  let status = { busy: true, verse, note };
+  let status = {
+    busy: true,
+    verse,
+    note,
+  };
   await file_overwrite_uncached(path, json_format_to(status));
 }
