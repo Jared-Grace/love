f58fb58ac9705949d93b67853cfe2e39add98071
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 16:58:23 +0800

    ao js_log_remove_workflow

--- js/js_log_remove_workflow.mjs
+++ js/js_log_remove_workflow.mjs
@@ -1,8 +1,10 @@
+import { log_keep } from "./log_keep.mjs";
+import { js_log_remove } from "./js_log_remove.mjs";
 import { log } from "./log.mjs";
 export function js_log_remove_workflow() {
-    log.name,' means temporary log'
-    log_keep.name,' means permanent log'
-  ("first, developer adds ", log.name, " calls");'this is meant to be automated - logs are added to every assignment , showing the contents of the line and JSON value of the assigned value'
-  'then developer runs ',js_log_remove, ' to remove temporary logs'
-
+  (log.name, " means temporary log");
+  (log_keep.name,
+    " means permanent log"("first, developer adds ", log.name, " calls"));
+  ("this is meant to be automated - logs are added to every assignment , showing the contents of the line and JSON value of the assigned value");
+  ("then developer runs ", js_log_remove, " to remove temporary logs");
 }
