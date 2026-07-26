ed2a9cd03d6d1089e41a99b4c80fd8be1e4dac2a
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 06:07:16 +0800

    ai

--- js/local_function_path.mjs
+++ js/local_function_path.mjs
@@ -1,7 +1,7 @@
-import { folder_user_storage_path } from "./folder_user_storage_path.mjs";
+import { local_function_folder } from "./local_function_folder.mjs";
 import { path_join } from "./path_join.mjs";
 export function local_function_path(fn, file_name) {
-  let joined2 = path_join(["function", fn.name, file_name]);
-  let joined = folder_user_storage_path(joined2);
+  let folder = local_function_folder(fn);
+  let joined = path_join([folder, file_name]);
   return joined;
 }
