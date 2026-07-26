9374b21706aed3bee41adc37acc9dbab7655451a
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 17:03:37 +0800

    ai

--- js/js_node_atomize_variable_name_get.mjs
+++ js/js_node_atomize_variable_name_get.mjs
@@ -1,10 +1,11 @@
+import { not_equal } from "./not_equal.mjs";
 import { assert_json } from "./assert_json.mjs";
 import { list_size } from "./list_size.mjs";
 import { equal_by } from "./equal_by.mjs";
 import { list_get } from "./list_get.mjs";
 import { list_index_of } from "./list_index_of.mjs";
 import { function_parse_strict_declaration } from "./function_parse_strict_declaration.mjs";
-import { function_exists_strict } from "./function_exists_strict.mjs";
+import { function_exists } from "./function_exists.mjs";
 import { list_is } from "./list_is.mjs";
 import { list_get_end } from "./list_get_end.mjs";
 import { js_return_name } from "./js_return_name.mjs";
@@ -23,23 +24,25 @@ export async function js_node_atomize_variable_name_get(
   if (js_node_type_is(node, "CallExpression")) {
     await js_call_function_if(node, lambda);
     async function lambda(name) {
-      let v3 = await function_parse_unaliased(name);
-      let ast_callee = property_get(v3, "ast");
+      let v = await function_parse_unaliased(name);
+      let ast_callee = property_get(v, "ast");
       let return_name = js_return_name(ast_callee);
-      if (return_name !== null) {
+      if (not_equal(return_name, null)) {
         variable_name = return_name;
       }
     }
   }
   ("use param name of containing fn");
-  let stack_2 = list_get_end(stack, text_combine(2, offset));
-  if (js_node_type_is(stack_2, "CallExpression")) {
-    let stack_1 = list_get_end(stack, text_combine(1, offset));
+  let index_from_end = text_combine(2, offset);
+  let stack_ = list_get_end(stack, index_from_end);
+  if (js_node_type_is(stack_, "CallExpression")) {
+    let index_from_end2 = text_combine(1, offset);
+    let stack_1 = list_get_end(stack, index_from_end2);
     if (list_is(stack_1)) {
-      let callee = property_get(stack_2, "callee");
+      let callee = property_get(stack_, "callee");
       if (js_node_type_is(callee, "Identifier")) {
         let name = property_get(callee, "name");
-        let v4 = await function_exists_strict(name);
+        let v4 = await function_exists(name);
         let exists = property_get(v4, "exists");
         if (exists) {
           let r = await function_parse_strict_declaration(name);
