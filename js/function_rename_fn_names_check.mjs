f26b55b30e7a900d99d7dbabeef194e153dea856
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 20:17:29 +0800

    ai

--- js/function_rename_fn_names_check.mjs
+++ js/function_rename_fn_names_check.mjs
@@ -1,11 +1,6 @@
-import { object_replace } from "./object_replace.mjs";
-import { js_string } from "./js_string.mjs";
-import { js_flo_name } from "./js_flo_name.mjs";
-import { fn_name_arg_get } from "./fn_name_arg_get.mjs";
-import { js_visit_calls_named } from "./js_visit_calls_named.mjs";
+import { js_fn_name_strings_rename } from "./js_fn_name_strings_rename.mjs";
 import { functions_transform_list } from "./functions_transform_list.mjs";
 import { property_exists_if_async } from "./property_exists_if_async.mjs";
-import { fn_name } from "./fn_name.mjs";
 import { data_identifiers_fn_names_get } from "./data_identifiers_fn_names_get.mjs";
 export async function function_rename_fn_names_check(
   f_name_before,
@@ -14,13 +9,7 @@ export async function function_rename_fn_names_check(
   let i = await data_identifiers_fn_names_get();
   async function lambda(value) {
     function lambda3(ast) {
-      js_visit_calls_named(ast, fn_name.name, lambda4);
-      function lambda4({ args }) {
-        let f_name = js_flo_name(ast);
-        let value = fn_name_arg_get(args, f_name);
-        let s = js_string(f_name_after);
-        object_replace(value, s);
-      }
+      js_fn_name_strings_rename(ast, f_name_before, f_name_after);
     }
     let waited = await functions_transform_list(value, lambda3);
   }
