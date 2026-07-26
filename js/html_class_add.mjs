af7f0128697b049a1ae6dc5a20c4d86c3792c657
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 11:55:18 +0800

    ai

--- js/html_class_add.mjs
+++ js/html_class_add.mjs
@@ -1,4 +1,3 @@
-import { add } from "./add.mjs";
 import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_class_add(tile, tile_class) {
   let element = html_component_element_get(tile);
