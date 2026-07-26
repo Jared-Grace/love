94e92a974b9194dd0b08e2641c76b48df7c901ab
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 06:12:34 +0800

    function_param_plain_mark g_verify_suggest_read chapter_code

--- js/g_verify_suggest_read.mjs
+++ js/g_verify_suggest_read.mjs
@@ -3,6 +3,7 @@ import { file_exists } from "./file_exists.mjs";
 import { file_read_json } from "./file_read_json.mjs";
 import { g_verify_suggest_set } from "./g_verify_suggest_set.mjs";
 export async function g_verify_suggest_read(chapter_code) {
+  "$plain chapter_code";
   let path = local_function_path_json(chapter_code, g_verify_suggest_set);
   let exists = await file_exists(path);
   if (exists) {
