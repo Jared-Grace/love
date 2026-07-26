2d15332dd183ef78d0b6ff673fd3531bcc0ee59f
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 11:59:11 +0800

    ai

--- js/js_code_call_arg.mjs
+++ js/js_code_call_arg.mjs
@@ -1,6 +1,5 @@
 import { js_code_call_args } from "./js_code_call_args.mjs";
-import { fn_name } from "./fn_name.mjs";
 export function js_code_call_arg(fn_name, v) {
-  let code3 = js_code_call_args(fn_name, [v]);
-  return code3;
+  let code = js_code_call_args(fn_name, [v]);
+  return code;
 }
