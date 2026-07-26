81a3de0fccd2f7ffd125f980faa16db054144629
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 20:44:47 +0800

    ai

--- js/catch_ignore_async.mjs
+++
@@ -1,7 +0,0 @@
-export async function catch_ignore_async(lambda) {
-  let r = null;
-  try {
-    r = await lambda();
-  } catch (e) {}
-  return r;
-}
--- js/ebible_versions_download.mjs
+++ js/ebible_versions_download.mjs
@@ -1,4 +1,4 @@
-import { catch_ignore_async } from "./catch_ignore_async.mjs";
+import { catch_null_async } from "./catch_null_async.mjs";
 import { each_async } from "./each_async.mjs";
 import { ebible_version_download } from "./ebible_version_download.mjs";
 import { ebible_versions } from "./ebible_versions.mjs";
@@ -8,7 +8,7 @@ export async function ebible_versions_download() {
     async function lambda3() {
       let file_path = await ebible_version_download(item);
     }
-    await catch_ignore_async(lambda3);
+    await catch_null_async(lambda3);
   }
   await each_async(list, lambda);
 }
