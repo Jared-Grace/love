c00a4d2d7c062f7571d596018f3608e8ce799bd8
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 15:48:04 +0800

    function_delete_unused playwright_by_attribute_typed

--- js/playwright_by_attribute_typed.mjs
+++
@@ -1,5 +0,0 @@
-import { playwright_by_attribute } from "./playwright_by_attribute.mjs";
-export async function playwright_by_attribute_typed(page, name, value) {
-  let r = (await playwright_by_attribute(page, name, value)).type(typed);
-  return r;
-}
