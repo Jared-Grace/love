9ec3f4815f148211e8be20a26f6a3cccfc7fd85b
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 18:55:25 +0800

    function_rename files_to_commit_probe_alpha files_to_commit_trial_alpha

--- js/files_to_commit_trial_alpha.mjs
+++ js/files_to_commit_trial_alpha.mjs
@@ -1,5 +1,5 @@
 import { text_combine } from "./text_combine.mjs";
-export function files_to_commit_probe_alpha() {
+export function files_to_commit_trial_alpha() {
   let joined = text_combine("alpha", "probe");
   return joined;
 }
