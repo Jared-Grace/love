9c540f40406109f0272a95c2392495bf02a748e0
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 16:59:01 +0800

    s auto,mult

--- js/log_keep.mjs
+++ js/log_keep.mjs
@@ -1,5 +1,4 @@
 import { log_inner } from "./log_inner.mjs";
-export function log_keep(f_name, message) {'see: ',js_log_remove_workflow
-  ''
+export function log_keep(f_name, message) {
   log_inner(f_name, message);
 }
