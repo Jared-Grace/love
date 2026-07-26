1c6809a30dd0d664dcedba3720e79742470964ea
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 12:04:14 +0800

    ai

--- js/server_data_endpoints.mjs
+++ js/server_data_endpoints.mjs
@@ -1,3 +1,4 @@
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { object_replace } from "./object_replace.mjs";
 import { property_get } from "./property_get.mjs";
 import { server_url_data } from "./server_url_data.mjs";
@@ -5,14 +6,19 @@ import { data_generate } from "./data_generate.mjs";
 import { null_is } from "./null_is.mjs";
 import { performance_end } from "./performance_end.mjs";
 import { performance_next } from "./performance_next.mjs";
-import { data_get } from "./data_get.mjs";
 import { performance_start } from "./performance_start.mjs";
 import { promise_resolved } from "./promise_resolved.mjs";
 export function server_data_endpoints(app) {
   let data_sequence = promise_resolved();
   let data = null;
   async function d_get(req, res) {
-    let p = performance_start("data_sequence = data_sequence.then(data_get)");
+    let p = performance_start(
+      text_combine_multiple([
+        "data_sequence = data_sequence.then(",
+        data_get.name,
+        ")",
+      ]),
+    );
     data_sequence = data_sequence.then(data_get);
     performance_next(p, "await data_sequence");
     await data_sequence;
