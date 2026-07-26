ce18dec0eda6fb27421d4d44030344bbf1f6a9ec
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 06:55:59 +0800

    ai

--- js/app_code_lesson_expression_round_up.mjs
+++ js/app_code_lesson_expression_round_up.mjs
@@ -112,7 +112,7 @@ export function app_code_lesson_expression_round_up() {
     let rounds = app_code_container_light_blue(root);
     app_code_between_two_wholes(rounds, low_decimal, whole_text, whole_up);
     html_div_cycle_code(rounds, [
-      "so ",
+      "So ",
       "Math.ceil",
       " chooses ",
       whole_up,
