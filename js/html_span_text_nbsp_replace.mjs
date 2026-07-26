a68dc73e855b1ca42653bd58b656ff067220d16e
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 16:45:04 +0800

    ai

--- js/html_span_text_nbsp_replace.mjs
+++ js/html_span_text_nbsp_replace.mjs
@@ -1,4 +1,4 @@
-import { html_style_set } from "./html_style_set.mjs";
+import { html_style_white_space } from "./html_style_white_space.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_nbsp_replace } from "./html_nbsp_replace.mjs";
 export function html_span_text_nbsp_replace(parent, text) {
--- js/string_skip_end.mjs
+++ js/string_skip_end.mjs
@@ -2,7 +2,8 @@ import { text_take } from "./text_take.mjs";
 import { text_size } from "./text_size.mjs";
 import { subtract } from "./subtract.mjs";
 export function string_skip_end(s, missing_count) {
-  let from = subtract(text_size(s), missing_count);
+  let left = text_size(s);
+  let from = subtract(left, missing_count);
   let without = text_take(s, from);
   return without;
 }
