8198ad6278ea66842810ecc02911ee0cc8bd7ffc
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 16:22:42 +0800

    ai

---
+++ js/html_style_justify_self.mjs
@@ -0,0 +1,4 @@
+import { html_style_set } from "./html_style_set.mjs";
+export function html_style_justify_self(component, value) {
+  html_style_set(component, "justify-self", value);
+}
---
+++ js/html_style_line_height.mjs
@@ -0,0 +1,4 @@
+import { html_style_set } from "./html_style_set.mjs";
+export function html_style_line_height(component, value) {
+  html_style_set(component, "line-height", value);
+}
