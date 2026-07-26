7693b5851f4c4cb3995ce038e273da8f14588e49
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 11:51:46 +0800

    ai

--- js/list_sort_text_property.mjs
+++ js/list_sort_text_property.mjs
@@ -3,4 +3,5 @@ import { list_sort_text_mapper } from "./list_sort_text_mapper.mjs";
 export function list_sort_text_property(languages, property_name) {
   let f = property_get_curried_right(property_name);
   list_sort_text_mapper(languages, f);
+  return languages;
 }
