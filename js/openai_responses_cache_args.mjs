a91c42ab75b55ac656fccd72a47360f1d258d709
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:13:35 +0800

    function_parallel_mark openai_responses_cache_args

--- js/openai_responses_cache_args.mjs
+++ js/openai_responses_cache_args.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function openai_responses_cache_args(system, user) {
+  function_duplicate_kind_parallel();
   let r = [system, user];
   return r;
 }
