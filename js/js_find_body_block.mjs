6c0faf5a1540f0c5b2616019b12a655555c05fe9
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 20:39:50 +0800

    ai

--- js/js_find_body_block.mjs
+++ js/js_find_body_block.mjs
@@ -1,1 +1,7 @@
-export function js_find_body_block(ast) {}
+import { js_flo } from "./js_flo.mjs";
+import { property_get } from "./property_get.mjs";
+export function js_find_body_block(ast) {
+  let declaration = js_flo(ast);
+  let value = property_get(declaration, "body");
+  return value;
+}
