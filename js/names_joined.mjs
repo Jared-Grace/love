67a780c9b593dd317da2f1610e263b9740399dfd
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 22:23:27 +0800

    ai

--- js/names_joined.mjs
+++
@@ -1,6 +0,0 @@
-import { list_sort_text } from "./list_sort_text.mjs";
-export function names_joined(names, text_join) {
-  let sorted = list_sort_text(names);
-  let joined = text_join(sorted);
-  return joined;
-}
