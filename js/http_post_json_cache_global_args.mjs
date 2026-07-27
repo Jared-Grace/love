b44cce6ce48c8db75c25a7774c4a9163fd2de02c
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:13:20 +0800

    function_parallel_mark http_post_json_cache_global_args

--- js/http_post_json_cache_global_args.mjs
+++ js/http_post_json_cache_global_args.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function http_post_json_cache_global_args(url, body) {
+  function_duplicate_kind_parallel();
   let v = [url, body];
   return v;
 }
