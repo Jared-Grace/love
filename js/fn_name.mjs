45c16817b1c8c61fa224254c5e3ee5b939734fb0
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:09:04 +0800

    function_parallel_mark fn_name

--- js/fn_name.mjs
+++ js/fn_name.mjs
@@ -1,5 +1,6 @@
 import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function fn_name(f_name) {
   function_duplicate_kind_parallel();
+  function_duplicate_kind_parallel();
   return f_name;
 }
