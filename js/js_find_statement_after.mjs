dd2c5ca57e5f51f096de9e8a93e0e6bc88ecb32e
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 20:34:34 +0800

    ai

--- js/js_find_statement_after.mjs
+++ js/js_find_statement_after.mjs
@@ -1,3 +1,6 @@
 export function js_find_statement_after(ast, name) {
   let body = js_flo_body(ast);
+  let statement = js_find_declaration_named(ast, name);
+  let after = list_index_of_next(body, statement);
+  return after;
 }
