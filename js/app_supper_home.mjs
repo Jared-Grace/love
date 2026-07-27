eb06ed4c5c9aa28da34576716353eeaa001c877f
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:10:59 +0800

    ai

--- js/app_supper_home.mjs
+++ js/app_supper_home.mjs
@@ -1,4 +1,5 @@
 import { app_supper_home_generic } from "./app_supper_home_generic.mjs";
 export async function app_supper_home(context) {
+  function_duplicate_kind_parallel();
   await app_supper_home_generic(context);
 }
