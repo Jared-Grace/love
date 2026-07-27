c628a23b2cc42618f17e459878e3b4ee3715e91a
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:10:39 +0800

    function_parallel_mark app_replace_symbol_tile_font_color_invalid

--- js/app_replace_symbol_tile_font_color_invalid.mjs
+++ js/app_replace_symbol_tile_font_color_invalid.mjs
@@ -1,3 +1,4 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 import { app_shared_container_blue_border_color } from "./app_shared_container_blue_border_color.mjs";
 export function app_replace_symbol_tile_font_color_invalid() {
   function_duplicate_kind_parallel();
