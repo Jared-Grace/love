33802b54c61b64b4dfe6057ca7ba675a74b37b82
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:13:04 +0800

    function_parallel_mark pair_to_list

--- js/pair_to_list.mjs
+++ js/pair_to_list.mjs
@@ -1,3 +1,4 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function pair_to_list(a, b) {
   function_duplicate_kind_parallel();
   let l = [a, b];
