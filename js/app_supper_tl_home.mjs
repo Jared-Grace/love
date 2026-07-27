de0fdda5942c18b6a48a8a491b71ee07fb8f15b7
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 06:11:42 +0800

    function_parallel_mark app_supper_tl_home

--- js/app_supper_tl_home.mjs
+++ js/app_supper_tl_home.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 import { app_supper_home_generic } from "./app_supper_home_generic.mjs";
 export async function app_supper_tl_home(context) {
+  function_duplicate_kind_parallel();
   await app_supper_home_generic(context);
 }
