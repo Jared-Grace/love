bf685f9221c59d3c4e107d2b00f4964679033fda
JESUS rose to life <username@users.noreply.github.com>
2026-07-27 20:39:00 +0800

    function_rename function_identifier_replace function_identifier_replace_current

--- js/function_identifier_replace_ending_remove.mjs
+++ js/function_identifier_replace_ending_remove.mjs
@@ -1,10 +1,10 @@
 import { text_suffix_without } from "./text_suffix_without.mjs";
-import { function_identifier_replace } from "./function_identifier_replace.mjs";
+import { function_identifier_replace_current } from "./function_identifier_replace_current.mjs";
 export async function function_identifier_replace_ending_remove(
   identifier_name,
   ending_to_remove,
 ) {
   let removed = text_suffix_without(identifier_name, ending_to_remove);
-  let v = await function_identifier_replace(identifier_name, removed);
+  let v = await function_identifier_replace_current(identifier_name, removed);
   return v;
 }
