f3ba856bccde2c3b32bb379569ac0bb431939922
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 09:39:27 +0800

    ai

--- js/app_shared_success_message.mjs
+++ js/app_shared_success_message.mjs
@@ -1,27 +1,28 @@
-import { app_shared_color_light_green } from "../../love/js/app_shared_color_light_green.mjs";
-import { each } from "../../love/js/each.mjs";
-import { html_bold } from "../../love/js/html_bold.mjs";
-import { html_style_font_size } from "../../love/js/html_style_font_size.mjs";
-import { html_span_text } from "../../love/js/html_span_text.mjs";
-import { html_div } from "../../love/js/html_div.mjs";
-import { html_centered } from "../../love/js/html_centered.mjs";
-import { html_style_padding_em } from "../../love/js/html_style_padding_em.mjs";
-import { app_shared_symbol_tile_style_inner } from "../../love/js/app_shared_symbol_tile_style_inner.mjs";
-import { html_style_background_color_set } from "../../love/js/html_style_background_color_set.mjs";
-import { html_p } from "../../love/js/html_p.mjs";
-import { list_join_empty } from "../../love/js/list_join_empty.mjs";
-import { invoke_multiple } from "../../love/js/invoke_multiple.mjs";
-import { emoji_check } from "../../love/js/emoji_check.mjs";
-import { list_add_first } from "../../love/js/list_add_first.mjs";
-import { list_shuffle_take } from "../../love/js/list_shuffle_take.mjs";
-import { emoji_party_face } from "../../love/js/emoji_party_face.mjs";
-import { emoji_party_popper } from "../../love/js/emoji_party_popper.mjs";
-import { emoji_medal_1 } from "../../love/js/emoji_medal_1.mjs";
-import { emoji_medal_star } from "../../love/js/emoji_medal_star.mjs";
-import { emoji_clap } from "../../love/js/emoji_clap.mjs";
-import { emoji_100 } from "../../love/js/emoji_100.mjs";
-import { emoji_trophy } from "../../love/js/emoji_trophy.mjs";
-import { text_combine } from "../../love/js/text_combine.mjs";
+import { app_shared_success_emoji_font_size } from "./app_shared_success_emoji_font_size.mjs";
+import { app_shared_color_light_green } from "./app_shared_color_light_green.mjs";
+import { each } from "./each.mjs";
+import { html_bold } from "./html_bold.mjs";
+import { html_style_font_size } from "./html_style_font_size.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { html_div } from "./html_div.mjs";
+import { html_centered } from "./html_centered.mjs";
+import { html_style_padding_em } from "./html_style_padding_em.mjs";
+import { app_shared_symbol_tile_style_inner } from "./app_shared_symbol_tile_style_inner.mjs";
+import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
+import { html_p } from "./html_p.mjs";
+import { list_join_empty } from "./list_join_empty.mjs";
+import { invoke_multiple } from "./invoke_multiple.mjs";
+import { emoji_check } from "./emoji_check.mjs";
+import { list_add_first } from "./list_add_first.mjs";
+import { list_shuffle_take } from "./list_shuffle_take.mjs";
+import { emoji_party_face } from "./emoji_party_face.mjs";
+import { emoji_party_popper } from "./emoji_party_popper.mjs";
+import { emoji_medal_1 } from "./emoji_medal_1.mjs";
+import { emoji_medal_star } from "./emoji_medal_star.mjs";
+import { emoji_clap } from "./emoji_clap.mjs";
+import { emoji_100 } from "./emoji_100.mjs";
+import { emoji_trophy } from "./emoji_trophy.mjs";
+import { text_combine } from "./text_combine.mjs";
 export function app_shared_success_message(parent) {
   let choices = [
     emoji_trophy,
@@ -46,7 +47,8 @@ export function app_shared_success_message(parent) {
   html_centered(p);
   let p_emojis = html_div(p);
   html_span_text(p_emojis, joined);
-  html_style_font_size(p_emojis, app_shared_success_emoji_font_size());
+  let value = app_shared_success_emoji_font_size();
+  html_style_font_size(p_emojis, value);
   let p_encouragement = html_div(p);
   let encouragements_choices = [
     "Congratulations",
@@ -62,7 +64,8 @@ export function app_shared_success_message(parent) {
   let encouragements = list_shuffle_take(encouragements_choices, 2);
   html_bold(p_encouragement);
   function lambda(encouragement) {
-    html_span_text(p_encouragement, text_combine(encouragement, "! "));
+    let text = text_combine(encouragement, "! ");
+    html_span_text(p_encouragement, text);
   }
   each(encouragements, lambda);
   return p;
--- js/html_progress_bar.mjs
+++ js/html_progress_bar.mjs
@@ -1,18 +1,19 @@
-import { html_style_font_size } from "../../love/js/html_style_font_size.mjs";
-import { add_1 } from "../../love/js/add_1.mjs";
-import { html_div_text_centered } from "../../love/js/html_div_text_centered.mjs";
-import { text_first_upper_to } from "../../love/js/text_first_upper_to.mjs";
-import { text_combine_multiple } from "../../love/js/text_combine_multiple.mjs";
-import { html_style_padding_y } from "../../love/js/html_style_padding_y.mjs";
-import { html_centered } from "../../love/js/html_centered.mjs";
-import { html_style_assign } from "../../love/js/html_style_assign.mjs";
-import { app_shared_color_green } from "../../love/js/app_shared_color_green.mjs";
-import { app_shared_color_light_green } from "../../love/js/app_shared_color_light_green.mjs";
-import { html_div } from "../../love/js/html_div.mjs";
-import { html_p } from "../../love/js/html_p.mjs";
-import { text_combine } from "../../love/js/text_combine.mjs";
-import { divide } from "../../love/js/divide.mjs";
-import { multiply } from "../../love/js/multiply.mjs";
+import { html_progress_caption_font_size } from "./html_progress_caption_font_size.mjs";
+import { html_style_font_size } from "./html_style_font_size.mjs";
+import { add_1 } from "./add_1.mjs";
+import { html_div_text_centered } from "./html_div_text_centered.mjs";
+import { text_first_upper_to } from "./text_first_upper_to.mjs";
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
+import { html_style_padding_y } from "./html_style_padding_y.mjs";
+import { html_centered } from "./html_centered.mjs";
+import { html_style_assign } from "./html_style_assign.mjs";
+import { app_shared_color_green } from "./app_shared_color_green.mjs";
+import { app_shared_color_light_green } from "./app_shared_color_light_green.mjs";
+import { html_div } from "./html_div.mjs";
+import { html_p } from "./html_p.mjs";
+import { text_combine } from "./text_combine.mjs";
+import { divide } from "./divide.mjs";
+import { multiply } from "./multiply.mjs";
 export function html_progress_bar(
   root,
   count_progress,
@@ -29,15 +30,14 @@ export function html_progress_bar(
     padding: "0",
   });
   let div = html_div(progress_container);
+  let top = multiply(100, count_progress);
+  let left = divide(top, count_total);
   html_style_assign(div, {
     "border-radius": "9999px",
     "background-color": color_valid,
     "padding-left": "0.6em",
     height: "100%",
-    width: text_combine(
-      divide(multiply(100, count_progress), count_total),
-      "%",
-    ),
+    width: text_combine(left, "%"),
   });
   html_centered(div);
   html_style_padding_y(div, "0.3em");
@@ -51,7 +51,8 @@ export function html_progress_bar(
   ]);
   let text = text_first_upper_to(combined);
   let div2 = html_div_text_centered(progress_container_text, text);
-  html_style_font_size(div2, html_progress_caption_font_size());
+  let value = html_progress_caption_font_size();
+  html_style_font_size(div2, value);
   let r = {
     container: progress_container_text,
   };
