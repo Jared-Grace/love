a653a2d845683331ad72f1a84c795ad2a53b9bfe
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 11:01:10 +0800

    ai

--- js/list_replace_all.mjs
+++ js/list_replace_all.mjs
@@ -1,6 +1,6 @@
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { list_clear } from "./list_clear.mjs";
-export function list_replace_all(list, list_new) {
+export function list_replace_all(list, items) {
   list_clear(list);
-  list_add_multiple(list, list_new);
+  list_add_multiple(list, items);
 }
