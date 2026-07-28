f4c928d03d147a49cecf7e2ab55f9bc2bd7b3e94
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 14:03:15 +0800

    ai

--- js/app_shared_text_body.mjs
+++ js/app_shared_text_body.mjs
@@ -2,15 +2,8 @@ import { app_shared_spaced_tiny_gap } from "./app_shared_spaced_tiny_gap.mjs";
 import { html_p_text } from "./html_p_text.mjs";
 import { html_style_margin_y } from "./html_style_margin_y.mjs";
 export function app_shared_text_body(parent, text) {
-  let { p, value } = app_shared_text_body_styled(parent, text);
-  html_style_margin_y(p, value);
-  return p;
-}
-function app_shared_text_body_styled(parent, text) {
   let p = html_p_text(parent, text);
   let value = app_shared_spaced_tiny_gap();
-  return {
-    p,
-    value,
-  };
+  html_style_margin_y(p, value);
+  return p;
 }
