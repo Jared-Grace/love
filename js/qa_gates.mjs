3c3576d7e4557d7457e691e9c420655e97cd0b56
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 22:16:31 +0800

    function_select_apply_args_auto qa_gates js_find_declaration_named gates js_array_identifier_add claude_md_size_gate_run

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { claude_md_size_gate_run } from "./claude_md_size_gate_run.mjs";
 import { app_shared_imports_gate_run } from "./app_shared_imports_gate_run.mjs";
 import { functions_shadowing_function_gate_run } from "./functions_shadowing_function_gate_run.mjs";
 import { functions_unreachable_check_gate_run } from "./functions_unreachable_check_gate_run.mjs";
@@ -128,6 +129,7 @@ export function qa_gates() {
     literals_frozen_gate_run,
     functions_unreachable_check_gate_run,
     app_shared_imports_gate_run,
+    claude_md_size_gate_run,
   ];
   return gates;
 }
