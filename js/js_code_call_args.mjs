7f12d69aae5ec34c9fee32d35ac54b594497f120
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:05:50 +0800

    ai

--- js/js_code_call_args.mjs
+++ js/js_code_call_args.mjs
@@ -2,10 +2,10 @@ import { js_code_join_comma_space } from "./js_code_join_comma_space.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
 import { text_combine } from "./text_combine.mjs";
-export function js_code_call_args(fn_name, args) {
+export function js_code_call_args(f_name, args) {
   arguments_assert(arguments, 2);
   let joined = js_code_join_comma_space(args);
   let right = js_code_wrap_parenthesis(joined);
-  let code = text_combine(fn_name, right);
+  let code = text_combine(f_name, right);
   return code;
 }
