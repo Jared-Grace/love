93543f035099f288678fd0d43a6f689efd5c998f
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 07:37:24 +0800

    function_rename object_map_async each_object_async

--- js/each_object_async.mjs
+++ js/each_object_async.mjs
@@ -1,6 +1,6 @@
 import { each_object_generic_async } from "./each_object_generic_async.mjs";
 import { each_async } from "./each_async.mjs";
-export async function object_map_async(object, lambda$value$key) {
+export async function each_object_async(object, lambda$value$key) {
   let each_lambda = each_async;
   await each_object_generic_async(object, lambda$value$key, each_lambda);
 }
