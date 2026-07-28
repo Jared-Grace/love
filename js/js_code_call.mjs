f06bcb1f6a4d8e035ea12ca32c24fc0847727657
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:05:50 +0800

    function_shadowing_function_rename js_code_call fn_name f_name

--- js/js_code_call.mjs
+++ js/js_code_call.mjs
@@ -1,6 +1,6 @@
 import { js_code_call_args } from "./js_code_call_args.mjs";
-export function js_code_call(fn_name) {
+export function js_code_call(f_name) {
   let args = [];
-  let result = js_code_call_args(fn_name, args);
+  let result = js_code_call_args(f_name, args);
   return result;
 }
