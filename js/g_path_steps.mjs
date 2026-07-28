3954eadb48ecdd0208b19ef58166f2cd188dcf13
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 18:58:50 +0800

    function_shadowing_function_rename g_path_steps each each_item

--- js/g_path_steps.mjs
+++ js/g_path_steps.mjs
@@ -5,14 +5,14 @@ import { each_index } from "./each_index.mjs";
 export function g_path_steps(path) {
   let destinations = list_skip_1(path);
   function lambda(add_step) {
-    function each(to, index) {
+    function each_item(to, index) {
       let from = list_get(path, index);
       add_step({
         from,
         to,
       });
     }
-    each_index(destinations, each);
+    each_index(destinations, each_item);
   }
   let steps = list_adder(lambda);
   return steps;
