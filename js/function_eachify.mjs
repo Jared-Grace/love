b4cc31d7106f3a44cb4efc1b20a2bac74179be1a
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:05:48 +0800

    function_shadowing_function_rename function_eachify fn_name f_name

--- js/function_eachify.mjs
+++ js/function_eachify.mjs
@@ -2,9 +2,9 @@ import { function_eachify_transform } from "./function_eachify_transform.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { each } from "./each.mjs";
 import { each_async } from "./each_async.mjs";
-export async function function_eachify(fn_name) {
+export async function function_eachify(f_name) {
   let output = await function_eachify_transform(
-    fn_name,
+    f_name,
     each_name_get,
     f_name_multiple_get,
   );
