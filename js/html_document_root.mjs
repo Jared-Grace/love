34270d4932fc5a22392145bb5b75d393480c052e
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 19:04:35 +0800

    function_shadowing_function_rename html_document_root html_element root_element

--- js/html_document_root.mjs
+++ js/html_document_root.mjs
@@ -1,6 +1,6 @@
 import { html_component_wrap } from "./html_component_wrap.mjs";
 export function html_document_root() {
-  let html_element = document.documentElement;
-  let html = html_component_wrap(html_element);
+  let root_element = document.documentElement;
+  let html = html_component_wrap(root_element);
   return html;
 }
