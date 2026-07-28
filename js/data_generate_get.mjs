a853aa47415a32df1d06abcbcd9dac48c6e05b26
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 19:01:49 +0800

    function_shadowing_function_rename data_generate_get data_get data_get_fn

--- js/data_generate_get.mjs
+++ js/data_generate_get.mjs
@@ -2,12 +2,12 @@ import { data_generate } from "./data_generate.mjs";
 import { null_is } from "./null_is.mjs";
 export function data_generate_get() {
   let data = null;
-  let data_get = async function lambda() {
+  let data_get_fn = async function lambda() {
     if (null_is(data)) {
       data = {};
       await data_generate(data);
     }
     return data;
   };
-  return data_get;
+  return data_get_fn;
 }
