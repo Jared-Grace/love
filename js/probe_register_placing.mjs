695e47a14903e25234d3208c1b701fd1931eb045
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 14:30:38 +0800

    ai

--- js/probe_register_placing.mjs
+++
@@ -1,6 +0,0 @@
-import { add } from "./add.mjs";
-import { equal } from "./equal.mjs";
-import { not } from "./not.mjs";
-export function probe_register_placing() {
-  let gates = [add, not, equal];
-}
