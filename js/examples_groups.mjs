bc10cfb2a8628d5a5f51c4a5d029b8a3a340cafd
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 16:33:13 +0800

    ai

--- js/examples_groups.mjs
+++ js/examples_groups.mjs
@@ -56,6 +56,7 @@ export function examples_groups() {
         "example_type_find_address",
         "example_call_name_includes_address",
         "example_call_named_index_address",
+        "example_call_index_argument_address",
         "example_statement_call_named_index",
         "example_function_node_named_address",
         "example_wrap_for_of",
