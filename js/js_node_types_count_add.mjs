6fe4574fae6bce170bfe742d07515e1e5dcc1d88
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 14:20:37 +0800

    ai

--- js/js_node_types_count_add.mjs
+++ js/js_node_types_count_add.mjs
@@ -1,10 +1,8 @@
+import { property_count_add } from "./property_count_add.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { js_visit } from "./js_visit.mjs";
 import { js_node_is } from "./js_node_is.mjs";
 import { property_get } from "./property_get.mjs";
-import { property_or_null } from "./property_or_null.mjs";
-import { property_set } from "./property_set.mjs";
-import { null_is } from "./null_is.mjs";
 import { not } from "./not.mjs";
 export function js_node_types_count_add(ast, counts) {
   arguments_assert(arguments, 2);
