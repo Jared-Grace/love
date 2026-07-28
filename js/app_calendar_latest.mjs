357a6132964e6253dd20707ed4d38b9284a8d8ea
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:16:28 +0800

    ai

--- js/app_calendar_latest.mjs
+++ js/app_calendar_latest.mjs
@@ -1,5 +1,7 @@
+import { firebase_project_name_jg } from "./firebase_project_name_jg.mjs";
 import { app_calendar } from "./app_calendar.mjs";
 import { app_shared_main_latest } from "./app_shared_main_latest.mjs";
 export async function app_calendar_latest() {
-  await app_shared_main_latest(app_calendar.name, firebase_project_name_jg());
+  let firebase_name_value = firebase_project_name_jg();
+  await app_shared_main_latest(app_calendar.name, firebase_name_value);
 }
--- js/pwa_manifest_object.mjs
+++ js/pwa_manifest_object.mjs
@@ -1,7 +1,9 @@
+import { html_extension } from "./html_extension.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function pwa_manifest_object(name) {
-  let start_url = text_combine_multiple(["/", name, html_extension()]);
-  let icon_192 = {
+  let ext_h = html_extension();
+  let start_url = text_combine_multiple(["/", name, ext_h]);
+  let icon_ = {
     src: text_combine_multiple(["/", name, "-192.png"]),
     sizes: "192x192",
     type: "image/png",
@@ -21,7 +23,7 @@ export function pwa_manifest_object(name) {
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
-    icons: [icon_192, icon_512],
+    icons: [icon_, icon_512],
   };
   return r;
 }
