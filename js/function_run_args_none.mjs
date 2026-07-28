6057aeae6158d0080902ce56b53debb51ffaa3a1
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:03:54 +0800

    ai

--- js/function_run_args_none.mjs
+++ js/function_run_args_none.mjs
@@ -1,5 +1,5 @@
 import { function_run } from "./function_run.mjs";
-export async function function_run_args_none(fn_name) {
-  let r = await function_run(fn_name, []);
+export async function function_run_args_none(f_name) {
+  let r = await function_run(f_name, []);
   return r;
 }
