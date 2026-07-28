dd1312b9975f97ab902aca4e03bcef4005b66620
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 19:05:17 +0800

    function_shadowing_function_rename function_run_prompt_repo_name_colored chalk_color chalk_color_fn

--- js/function_run_prompt_repo_name_colored.mjs
+++ js/function_run_prompt_repo_name_colored.mjs
@@ -4,7 +4,7 @@ export async function function_run_prompt_repo_name_colored(repo_name) {
   let safe = ["p_np"];
   ("this will make repos not in safe stand out in prompt visually");
   let includes = list_includes(safe, repo_name);
-  let chalk_color = chalk_green_or_red_bg(includes);
-  let colored = await chalk_color(repo_name);
+  let chalk_color_fn = chalk_green_or_red_bg(includes);
+  let colored = await chalk_color_fn(repo_name);
   return colored;
 }
