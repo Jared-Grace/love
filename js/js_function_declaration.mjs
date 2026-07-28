f82bd31115c9e31f658ac069545f9c7d357f8553
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:05:51 +0800

    function_shadowing_function_rename js_function_declaration fn_name f_name

--- js/js_function_declaration.mjs
+++ js/js_function_declaration.mjs
@@ -1,9 +1,9 @@
 import { js_parse_statement_module } from "./js_parse_statement_module.mjs";
 import { js_code_function_declaration } from "./js_code_function_declaration.mjs";
 import { property_get } from "./property_get.mjs";
-export function js_function_declaration(declaration, fn_name) {
+export function js_function_declaration(declaration, f_name) {
   let async_is = property_get(declaration, "async");
-  let code = js_code_function_declaration(fn_name, "", async_is);
+  let code = js_code_function_declaration(f_name, "", async_is);
   let declaration_lambda = js_parse_statement_module(code);
   return declaration_lambda;
 }
