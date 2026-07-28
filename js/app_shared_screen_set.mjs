bfb5f1c586d2d5a263968e609a975a4cbd991d3e
JESUS rose to life <username@users.noreply.github.com>
2026-07-28 13:05:44 +0800

    function_shadowing_function_rename app_shared_screen_set fn_name f_name

--- js/app_shared_screen_set.mjs
+++ js/app_shared_screen_set.mjs
@@ -1,6 +1,6 @@
 import { property_get } from "./property_get.mjs";
 import { app_shared_refresh_screen } from "./app_shared_refresh_screen.mjs";
 export async function app_shared_screen_set(context, fn) {
-  let fn_name = property_get(fn, "name");
-  await app_shared_refresh_screen(context, fn_name);
+  let f_name = property_get(fn, "name");
+  await app_shared_refresh_screen(context, f_name);
 }
