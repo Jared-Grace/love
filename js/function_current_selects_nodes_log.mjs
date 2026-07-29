a8e0f3493b1e8aa8e21b5a8a18bd4b2c4058cb52
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:42 +0800

    function_name_only_imports_spell function_current_selects_nodes_log

--- js/function_current_selects_nodes_log.mjs
+++ js/function_current_selects_nodes_log.mjs
@@ -1,9 +1,9 @@
-import { sandbox } from "./sandbox.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { log_keep } from "./log_keep.mjs";
 import { function_current_selects_nodes } from "./function_current_selects_nodes.mjs";
 export async function function_current_selects_nodes_log() {
   let selects = await function_current_selects_nodes();
-  log_keep(sandbox.name, {
+  log_keep(fn_name("sandbox"), {
     selects,
   });
 }
