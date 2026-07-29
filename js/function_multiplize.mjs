147c18047fe5b517a1ef0bd13d30eb0d14f8a623
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:46 +0800

    function_name_only_imports_spell function_multiplize

--- js/function_multiplize.mjs
+++ js/function_multiplize.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_size_1_assert } from "./list_size_1_assert.mjs";
 import { js_function_declaration_params_names } from "./js_function_declaration_params_names.mjs";
 import { property_get } from "./property_get.mjs";
@@ -6,7 +7,6 @@ import { js_imports_missing_add_all } from "./js_imports_missing_add_all.mjs";
 import { js_flo_param_add } from "./js_flo_param_add.mjs";
 import { js_call_statement } from "./js_call_statement.mjs";
 import { js_flo_body_add } from "./js_flo_body_add.mjs";
-import { each } from "./each.mjs";
 import { function_new_open_transform } from "./function_new_open_transform.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 export async function function_multiplize(f_name) {
@@ -17,7 +17,7 @@ export async function function_multiplize(f_name) {
   let combined = function_name_combine(f_name, "multiple");
   async function lambda(ast) {
     let list = "list";
-    let call = js_call_statement(each.name, [list, f_name]);
+    let call = js_call_statement(fn_name("each"), [list, f_name]);
     js_flo_body_add(ast, call);
     js_flo_param_add(ast, list);
     await js_imports_missing_add_all(ast);
