e521eccb4e0ebb8521d106c673f1c1b0013d7214
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:10 +0800

    function_name_only_imports_spell app_calendar_secret_calendar_backup

--- js/app_calendar_secret_calendar_backup.mjs
+++ js/app_calendar_secret_calendar_backup.mjs
@@ -1,6 +1,6 @@
-import { app_calendar_secret_path } from "./app_calendar_secret_path.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { file_backup_fn_delete } from "./file_backup_fn_delete.mjs";
 export async function app_calendar_secret_calendar_backup() {
-  let r = await file_backup_fn_delete(app_calendar_secret_path.name);
+  let r = await file_backup_fn_delete(fn_name("app_calendar_secret_path"));
   return r;
 }
