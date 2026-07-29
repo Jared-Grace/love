44579dbf1c4485cb5a954da8c32eb375c951b4f2
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:21 +0800

    function_name_only_imports_spell list_to_lookup

--- js/list_to_lookup.mjs
+++ js/list_to_lookup.mjs
@@ -1,14 +1,14 @@
+import { fn_name } from "./fn_name.mjs";
 import { each } from "./each.mjs";
 import { list_add } from "./list_add.mjs";
 import { property_initialize_list } from "./property_initialize_list.mjs";
 import { property_get } from "./property_get.mjs";
 import { property_exists_not } from "./property_exists_not.mjs";
-import { list_to_dictionary } from "./list_to_dictionary.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function list_to_lookup(list, property_key) {
   text_combine_multiple([
     "if each key has one value, then ",
-    list_to_dictionary.name,
+    fn_name("list_to_dictionary"),
     " but if each key corresponds to a list of values, then ",
     list_to_lookup.name,
   ]);
