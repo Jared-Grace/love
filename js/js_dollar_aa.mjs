08508257560b1d967765668fb0f6d81462fc27d1
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:08 +0800

    function_name_only_imports_spell js_dollar_aa

--- js/js_dollar_aa.mjs
+++ js/js_dollar_aa.mjs
@@ -1,6 +1,6 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { js_code_call } from "./js_code_call.mjs";
-import { arguments_assert } from "./arguments_assert.mjs";
 import { list_add_first } from "./list_add_first.mjs";
 import { js_flo_body } from "./js_flo_body.mjs";
 import { js_parse_statement } from "./js_parse_statement.mjs";
@@ -15,7 +15,7 @@ export function js_dollar_aa({
 }) {
   list_remove(stack_2, stack_1);
   let body_block = js_flo_body(ast);
-  let code = js_code_call(arguments_assert.name);
+  let code = js_code_call(fn_name("arguments_assert"));
   let expression = js_parse_statement(code);
   list_add_first(body_block, expression);
   return;
