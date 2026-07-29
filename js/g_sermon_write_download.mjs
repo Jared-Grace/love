65c7b348e19fb2081f8caca431c4e0ba6a629fe3
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 11:32:34 +0800

    ai

--- js/g_sermon_write_download.mjs
+++ js/g_sermon_write_download.mjs
@@ -1,5 +1,4 @@
-import { firebase_storage_url_project_jg } from "./firebase_storage_url_project_jg.mjs";
-import { global_firebase_storage_download_json_decompress } from "./global_firebase_storage_download_json_decompress.mjs";
+import { firebase_storage_download_json_decompress_project_jg } from "./firebase_storage_download_json_decompress_project_jg.mjs";
 import { g_sermon_write_upload_path } from "./g_sermon_write_upload_path.mjs";
 export async function g_sermon_write_download(chapter_code) {
   let destination_get = g_sermon_write_upload_path;
