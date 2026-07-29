b48bb7391fadf4671d3f7d11995ad4b5779fd936
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 11:10:29 +0800

    function_identifier_rename each_object lambda value property lambda value key

--- js/each_object.mjs
+++ js/each_object.mjs
@@ -1,10 +1,10 @@
 import { each } from "./each.mjs";
 import { properties_get } from "./properties_get.mjs";
 import { property_get } from "./property_get.mjs";
-export function each_object(object, lambda$value$property) {
+export function each_object(object, lambda$value$key) {
   function lambda(property) {
     let value = property_get(object, property);
-    lambda$value$property(value, property);
+    lambda$value$key(value, property);
   }
   let properties = properties_get(object);
   each(properties, lambda);
