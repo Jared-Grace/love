7c6398d4fdc1007e463fccbfbd70fab8d9979146
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:04 +0800

    function_name_only_imports_spell js_auto_transform_new_open

--- js/js_auto_transform_new_open.mjs
+++ js/js_auto_transform_new_open.mjs
@@ -1,5 +1,5 @@
+import { fn_name } from "./fn_name.mjs";
 import { function_open } from "./function_open.mjs";
-import { js_auto_transforms } from "./js_auto_transforms.mjs";
 import { js_auto_transform_new_inner } from "./js_auto_transform_new_inner.mjs";
 import { function_new_js_open } from "./function_new_js_open.mjs";
 export async function js_auto_transform_new_open(f_name_unprefixed) {
@@ -7,5 +7,5 @@ export async function js_auto_transform_new_open(f_name_unprefixed) {
   "The showing lives here rather than in the inner fn so that every fn able to put a window on the human's screen says so in its name";
   await function_new_js_open(f_name_unprefixed);
   await js_auto_transform_new_inner(f_name_unprefixed);
-  await function_open(js_auto_transforms.name);
+  await function_open(fn_name("js_auto_transforms"));
 }
