175cc9465957b0f5131a59b30191c7c0a24452ff
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:21 +0800

    function_name_only_imports_spell app_replace_deploy

--- js/app_replace_deploy.mjs
+++ js/app_replace_deploy.mjs
@@ -1,5 +1,5 @@
+import { fn_name } from "./fn_name.mjs";
 import { command_line_node_r } from "./command_line_node_r.mjs";
-import { app_replace_deploy_inner } from "./app_replace_deploy_inner.mjs";
 import { each_async } from "./each_async.mjs";
 import { app_replace_rule_sets_fns_names } from "./app_replace_rule_sets_fns_names.mjs";
 import { app_replace_rule_set_rename } from "./app_replace_rule_set_rename.mjs";
@@ -8,5 +8,5 @@ export async function app_replace_deploy() {
   let result = await app_replace_rule_sets_fns_migrate_capitalization_upper();
   let mapped = app_replace_rule_sets_fns_names();
   await each_async(mapped, app_replace_rule_set_rename);
-  await command_line_node_r(app_replace_deploy_inner.name, []);
+  await command_line_node_r(fn_name("app_replace_deploy_inner"), []);
 }
