5c0c19d07d8cafb5e3e27919c99c4e115069d200
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:17 +0800

    function_name_only_imports_spell js_log_remove_workflow

--- js/js_log_remove_workflow.mjs
+++ js/js_log_remove_workflow.mjs
@@ -1,10 +1,9 @@
-import { log_keep } from "./log_keep.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { js_log_remove } from "./js_log_remove.mjs";
-import { log } from "./log.mjs";
 export function js_log_remove_workflow() {
-  (log.name, " means temporary log");
-  (log_keep.name,
-    " means permanent log"("first, developer adds ", log.name, " calls"));
+  (fn_name("log"), " means temporary log");
+  (fn_name("log_keep"),
+    " means permanent log"("first, developer adds ", fn_name("log"), " calls"));
   ("this is meant to be automated - logs are added to every assignment , showing the contents of the line and JSON value of the assigned value");
   ("then developer runs ", js_log_remove, " to remove temporary logs");
 }
