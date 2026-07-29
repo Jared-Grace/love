f1d456f7bf862d87a0773aa3867bbe197b5d631b
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 18:18:36 +0800

    function_select_apply_args_auto ebible_version_chapters_all_download_fresh js_call_named_find firebase_storage_download_json_decompress_cache js_call_callee_set firebase_storage_download_json_decompress

--- js/ebible_version_chapters_all_download_fresh.mjs
+++ js/ebible_version_chapters_all_download_fresh.mjs
@@ -1,10 +1,10 @@
-import { firebase_storage_download_json_decompress_cache } from "./firebase_storage_download_json_decompress_cache.mjs";
+import { firebase_storage_download_json_decompress } from "./firebase_storage_download_json_decompress.mjs";
 import { firebase_storage_url_project_jg } from "./firebase_storage_url_project_jg.mjs";
 import { ebible_version_chapters_all_upload_path } from "./ebible_version_chapters_all_upload_path.mjs";
 export async function ebible_version_chapters_all_download_fresh(version) {
   let project_url = firebase_storage_url_project_jg();
   let destination = ebible_version_chapters_all_upload_path(version);
-  let chapters = await firebase_storage_download_json_decompress_cache(
+  let chapters = await firebase_storage_download_json_decompress(
     project_url,
     destination,
   );
