8c3f0a0346a4ca94b1cf3cb324e02ab4e35f1f6b
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:25 +0800

    function_name_only_imports_spell app_replace_rule_sets_name

--- js/app_replace_rule_sets_name.mjs
+++ js/app_replace_rule_sets_name.mjs
@@ -1,8 +1,8 @@
+import { fn_name } from "./fn_name.mjs";
 import { app_shared_name_text_to_identifier } from "./app_shared_name_text_to_identifier.mjs";
 import { text_suffix_without } from "./text_suffix_without.mjs";
-import { app_replace_rule_sets } from "./app_replace_rule_sets.mjs";
 export function app_replace_rule_sets_name(found) {
-  let n = app_replace_rule_sets.name;
+  let n = fn_name("app_replace_rule_sets");
   let prefix = text_suffix_without(n, "s");
   let f_name_new = app_shared_name_text_to_identifier(prefix, found);
   return f_name_new;
