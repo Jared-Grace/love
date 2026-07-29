9d99c5f51300289b626f008cecfb9ac676fa5254
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:09 +0800

    function_name_only_imports_spell js_dollar_l

--- js/js_dollar_l.mjs
+++ js/js_dollar_l.mjs
@@ -1,9 +1,9 @@
+import { fn_name } from "./fn_name.mjs";
 import { js_call_args_from_code } from "./js_call_args_from_code.mjs";
 import { js_keyword_null } from "./js_keyword_null.mjs";
 import { js_code_wrap_braces } from "./js_code_wrap_braces.mjs";
 import { list_join_comma } from "./list_join_comma.mjs";
 import { js_unparse } from "./js_unparse.mjs";
-import { log } from "./log.mjs";
 import { object_replace } from "./object_replace.mjs";
 export function js_dollar_l({
   remaining,
@@ -17,7 +17,7 @@ export function js_dollar_l({
   let result = list_join_comma(remaining);
   let n = js_keyword_null();
   let v = js_code_wrap_braces(result);
-  let parsed = js_call_args_from_code(log.name, [n, v]);
+  let parsed = js_call_args_from_code(fn_name("log"), [n, v]);
   let code2 = js_unparse(node);
   object_replace(node, parsed);
 }
