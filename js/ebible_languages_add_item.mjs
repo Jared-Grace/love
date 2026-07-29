5e3a67dcc0303abd45f4b4fab89bbf96cfb881c8
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:33 +0800

    function_name_only_imports_spell ebible_languages_add_item

--- js/ebible_languages_add_item.mjs
+++ js/ebible_languages_add_item.mjs
@@ -1,12 +1,12 @@
+import { fn_name } from "./fn_name.mjs";
 import { property_get } from "./property_get.mjs";
 import { ebible_languages_add_item_info } from "./ebible_languages_add_item_info.mjs";
 import { list_add } from "./list_add.mjs";
 import { js_object_to_expression } from "./js_object_to_expression.mjs";
 import { js_array_expression_single_elements } from "./js_array_expression_single_elements.mjs";
-import { ebible_languages } from "./ebible_languages.mjs";
 import { function_transform } from "./function_transform.mjs";
 export async function ebible_languages_add_item(bible_folder) {
-  let f_name = ebible_languages.name;
+  let f_name = fn_name("ebible_languages");
   let r = await ebible_languages_add_item_info(bible_folder);
   let language_code = property_get(r, "language_code");
   let name = property_get(r, "name");
