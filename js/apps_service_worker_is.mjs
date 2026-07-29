3be58c5071b7bb0d3e4cf52d88e1eef5788bb37a
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 14:21:20 +0800

    ai

--- js/apps_service_worker_is.mjs
+++ js/apps_service_worker_is.mjs
@@ -1,6 +1,7 @@
 import { apps_service_worker } from "./apps_service_worker.mjs";
 import { list_includes } from "./list_includes.mjs";
 export function apps_service_worker_is(name) {
-  let r = list_includes(apps_service_worker(), name);
+  let list = apps_service_worker();
+  let r = list_includes(list, name);
   return r;
 }
