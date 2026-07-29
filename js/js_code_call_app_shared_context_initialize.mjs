847fe2aa0746fe049580d7319a79dfbaa1a37a83
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:05 +0800

    function_name_only_imports_spell js_code_call_app_shared_context_initialize

--- js/js_code_call_app_shared_context_initialize.mjs
+++ js/js_code_call_app_shared_context_initialize.mjs
@@ -1,8 +1,9 @@
-import { app_shared_context_initialize } from "./app_shared_context_initialize.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { js_code_call_args_statement } from "./js_code_call_args_statement.mjs";
 export function js_code_call_app_shared_context_initialize(name_prefixed) {
-  let result = js_code_call_args_statement(app_shared_context_initialize.name, [
-    name_prefixed,
-  ]);
+  let result = js_code_call_args_statement(
+    fn_name("app_shared_context_initialize"),
+    [name_prefixed],
+  );
   return result;
 }
