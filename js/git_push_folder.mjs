095a49c7334493c9a1b994743e77e0af2febc118
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:58 +0800

    function_name_only_imports_spell git_push_folder

--- js/git_push_folder.mjs
+++ js/git_push_folder.mjs
@@ -1,3 +1,4 @@
+import { fn_name } from "./fn_name.mjs";
 import { git_push_folder_now } from "./git_push_folder_now.mjs";
 import { date_diff_mins } from "./date_diff_mins.mjs";
 import { path_join } from "./path_join.mjs";
@@ -6,7 +7,6 @@ import { lambda_get } from "./lambda_get.mjs";
 import { date_to } from "./date_to.mjs";
 import { data_property_get_generic } from "./data_property_get_generic.mjs";
 import { user_data_path } from "./user_data_path.mjs";
-import { git_push } from "./git_push.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 import { date_iso_to } from "./date_iso_to.mjs";
 import { date_now } from "./date_now.mjs";
@@ -16,7 +16,7 @@ export async function git_push_folder(folder) {
   async function lambda() {
     let now = date_now();
     let now_iso = date_iso_to(now);
-    let property_name = function_name_combine(git_push.name, "when");
+    let property_name = function_name_combine(fn_name("git_push"), "when");
     let d_path = user_data_path();
     let joined = path_join([folder, d_path]);
     let before_iso = await data_property_get_generic(joined, property_name);
