e9ec35f962222bd5e193bd5f43d2288da41fe674
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:17 +0800

    function_name_only_imports_spell js_marker_insert

--- js/js_marker_insert.mjs
+++ js/js_marker_insert.mjs
@@ -1,9 +1,9 @@
+import { fn_name } from "./fn_name.mjs";
 import { js_call_statement_insert } from "./js_call_statement_insert.mjs";
-import { marker } from "./marker.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 export async function js_marker_insert(name, list, index) {
   let code_string = await js_code_string(name);
-  let f_name = marker.name;
+  let f_name = fn_name("marker");
   let args_code = [code_string];
   js_call_statement_insert(f_name, args_code, list, index);
 }
