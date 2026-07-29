42c27737df8c82644c4f442e055ab197efd1b98a
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:53 +0800

    function_name_only_imports_spell functions_parallel_marks_stale

--- js/functions_parallel_marks_stale.mjs
+++ js/functions_parallel_marks_stale.mjs
@@ -1,8 +1,8 @@
+import { fn_name } from "./fn_name.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { functions_duplicates } from "./functions_duplicates.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
-import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 import { data_identifiers_search } from "./data_identifiers_search.mjs";
 import { properties_get } from "./properties_get.mjs";
 import { function_parallel_marked_is } from "./function_parallel_marked_is.mjs";
@@ -19,7 +19,7 @@ export async function functions_parallel_marks_stale() {
     let names = property_get(group, "names");
     list_add_multiple(grouped, names);
   }
-  let mark_name = function_duplicate_kind_parallel.name;
+  let mark_name = fn_name("function_duplicate_kind_parallel");
   let search = await data_identifiers_search(mark_name);
   let referrers = properties_get(search);
   let stale = [];
