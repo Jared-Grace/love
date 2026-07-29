4685ad9d7a977678d3489180bb5c9e83a424a899
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:09 +0800

    function_name_only_imports_spell js_dollar_choice_argument

--- js/js_dollar_choice_argument.mjs
+++ js/js_dollar_choice_argument.mjs
@@ -1,11 +1,11 @@
+import { fn_name } from "./fn_name.mjs";
 import { js_dollar_choice_argument_find } from "./js_dollar_choice_argument_find.mjs";
-import { js_dollar } from "./js_dollar.mjs";
 import { function_transform } from "./function_transform.mjs";
 export async function js_dollar_choice_argument() {
   let result = null;
   async function lambda(ast) {
     result = js_dollar_choice_argument_find(ast, result);
   }
-  let output = await function_transform(js_dollar.name, lambda);
+  let output = await function_transform(fn_name("js_dollar"), lambda);
   return result;
 }
