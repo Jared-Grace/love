2fff6762e8c2495a07546ad182cc858c71409ceb
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:26 +0800

    function_name_only_imports_spell app_replace_rule_sets_prefix

--- js/app_replace_rule_sets_prefix.mjs
+++ js/app_replace_rule_sets_prefix.mjs
@@ -1,7 +1,7 @@
+import { fn_name } from "./fn_name.mjs";
 import { text_suffix_without } from "./text_suffix_without.mjs";
-import { app_replace_rule_sets } from "./app_replace_rule_sets.mjs";
 export function app_replace_rule_sets_prefix() {
-  let n = app_replace_rule_sets.name;
+  let n = fn_name("app_replace_rule_sets");
   let prefix = text_suffix_without(n, "s");
   return prefix;
 }
