eeb6e33e7da19ccdc1483396bc27c15afbe19fe1
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:02 +0800

    function_name_only_imports_spell js_add_reference_pieces_replace_code

--- js/js_add_reference_pieces_replace_code.mjs
+++ js/js_add_reference_pieces_replace_code.mjs
@@ -1,10 +1,10 @@
+import { fn_name } from "./fn_name.mjs";
 import { list_size_1 } from "./list_size_1.mjs";
 import { list_first } from "./list_first.mjs";
 import { js_code_join_comma_space } from "./js_code_join_comma_space.mjs";
 import { js_code_wrap_parenthesis } from "./js_code_wrap_parenthesis.mjs";
 import { js_code_wrap_brackets } from "./js_code_wrap_brackets.mjs";
 import { js_code_call_args } from "./js_code_call_args.mjs";
-import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function js_add_reference_pieces_replace_code(pieces, statement) {
   let s1 = list_size_1(pieces);
   if (s1) {
@@ -18,6 +18,6 @@ export function js_add_reference_pieces_replace_code(pieces, statement) {
   }
   let array_code = js_code_wrap_brackets(joined);
   let args = [array_code];
-  let call_code = js_code_call_args(text_combine_multiple.name, args);
+  let call_code = js_code_call_args(fn_name("text_combine_multiple"), args);
   return call_code;
 }
