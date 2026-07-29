063ebf638c7daeefc3d8bc5d0dd8f9bb7a3b230c
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:59 +0800

    function_name_only_imports_spell global_get

--- js/global_get.mjs
+++ js/global_get.mjs
@@ -1,11 +1,11 @@
+import { fn_name } from "./fn_name.mjs";
 import { property_get } from "./property_get.mjs";
 import { property_exists } from "./property_exists.mjs";
-import { global_alternate_set } from "./global_alternate_set.mjs";
 let global = {};
 export function global_get() {
-  let exists = property_exists(global, global_alternate_set.name);
+  let exists = property_exists(global, fn_name("global_alternate_set"));
   if (exists) {
-    let value = property_get(global, global_alternate_set.name);
+    let value = property_get(global, fn_name("global_alternate_set"));
     return value;
   }
   return global;
