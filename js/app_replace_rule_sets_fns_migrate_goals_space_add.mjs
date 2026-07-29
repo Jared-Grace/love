61a51050815908b75ac38a293eb8573cba01b248
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:23 +0800

    function_name_only_imports_spell app_replace_rule_sets_fns_migrate_goals_space_add

--- js/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
+++ js/app_replace_rule_sets_fns_migrate_goals_space_add.mjs
@@ -1,12 +1,12 @@
+import { fn_name } from "./fn_name.mjs";
 import { app_replace_rule_sets_fn_migrate_goals_space_add } from "./app_replace_rule_sets_fn_migrate_goals_space_add.mjs";
 import { each_async } from "./each_async.mjs";
 import { js_identifier_name } from "./js_identifier_name.mjs";
 import { list_map } from "./list_map.mjs";
 import { function_ast } from "./function_ast.mjs";
 import { js_array_expression_single_elements } from "./js_array_expression_single_elements.mjs";
-import { app_replace_rule_sets_fns } from "./app_replace_rule_sets_fns.mjs";
 export async function app_replace_rule_sets_fns_migrate_goals_space_add() {
-  let f_name = app_replace_rule_sets_fns.name;
+  let f_name = fn_name("app_replace_rule_sets_fns");
   let ast = await function_ast(f_name);
   let elements = js_array_expression_single_elements(ast);
   let mapped = list_map(elements, js_identifier_name);
