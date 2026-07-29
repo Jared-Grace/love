0c7f8e4f72c3a8cd9de9fe9c61869038089803e5
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:10 +0800

    function_name_only_imports_spell js_dollar_new_name

--- js/js_dollar_new_name.mjs
+++ js/js_dollar_new_name.mjs
@@ -1,6 +1,6 @@
-import { js_dollar } from "./js_dollar.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 export function js_dollar_new_name(code) {
-  let combined = function_name_combine(js_dollar.name, code);
+  let combined = function_name_combine(fn_name("js_dollar"), code);
   return combined;
 }
