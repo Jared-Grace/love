caa6a451a7a0b5946547640044b3244218faac1f
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 11:49:40 +0800

    ai

--- js/html_mobile_default.mjs
+++ js/html_mobile_default.mjs
@@ -1,7 +1,9 @@
 import { html_mobile_default_font_size_context } from "./html_mobile_default_font_size_context.mjs";
 import { html_font_sans_serif_set_html } from "./html_font_sans_serif_set_html.mjs";
+import { html_text_size_adjust_lock } from "./html_text_size_adjust_lock.mjs";
 export function html_mobile_default(context) {
   let root = html_mobile_default_font_size_context(context);
   html_font_sans_serif_set_html();
+  html_text_size_adjust_lock();
   return root;
 }
