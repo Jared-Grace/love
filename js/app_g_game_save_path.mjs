de20fd9d03cee71589d2c78a798ccd4196c95360
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:17 +0800

    function_name_only_imports_spell app_g_game_save_path

--- js/app_g_game_save_path.mjs
+++ js/app_g_game_save_path.mjs
@@ -1,9 +1,9 @@
-import { app_g } from "../../love/js/app_g.mjs";
-import { path_join } from "../../love/js/path_join.mjs";
-import { repo_love_path } from "../../love/js/repo_love_path.mjs";
-import { file_name_json_folder_gitignore } from "../../love/js/file_name_json_folder_gitignore.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { path_join } from "./path_join.mjs";
+import { repo_love_path } from "./repo_love_path.mjs";
+import { file_name_json_folder_gitignore } from "./file_name_json_folder_gitignore.mjs";
 export function app_g_game_save_path() {
-  let result = path_join([app_g.name, "1"]);
+  let result = path_join([fn_name("app_g"), "1"]);
   let f_path = file_name_json_folder_gitignore(result);
   let p = repo_love_path(f_path);
   return p;
