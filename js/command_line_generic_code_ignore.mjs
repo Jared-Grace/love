fad56a852e3b126e754f49d95e758a5e7e24c8dc
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:31 +0800

    function_name_only_imports_spell command_line_generic_code_ignore

--- js/command_line_generic_code_ignore.mjs
+++ js/command_line_generic_code_ignore.mjs
@@ -1,8 +1,8 @@
-import { command_line_generic } from "./command_line_generic.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { function_name_combine } from "./function_name_combine.mjs";
 export function command_line_generic_code_ignore() {
   let combined = function_name_combine(
-    command_line_generic.name,
+    fn_name("command_line_generic"),
     "code_ignore",
   );
   return combined;
