48b271dbf1cbba87f9ced2f36e4b670c00eb8a77
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:09 +0800

    function_name_only_imports_spell app_calendar_contacts_downloaded_backup

--- js/app_calendar_contacts_downloaded_backup.mjs
+++ js/app_calendar_contacts_downloaded_backup.mjs
@@ -1,8 +1,8 @@
-import { app_calendar_contacts_downloaded_path } from "./app_calendar_contacts_downloaded_path.mjs";
+import { fn_name } from "./fn_name.mjs";
 import { file_backup_fn_delete } from "./file_backup_fn_delete.mjs";
 export async function app_calendar_contacts_downloaded_backup() {
   let r = await file_backup_fn_delete(
-    app_calendar_contacts_downloaded_path.name,
+    fn_name("app_calendar_contacts_downloaded_path"),
   );
   return r;
 }
