98cfcaaea3cd0b150a8eb3d3a277b870c3f4e729
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:22 +0800

    function_name_only_imports_spell lock_wait_prompt

--- js/lock_wait_prompt.mjs
+++ js/lock_wait_prompt.mjs
@@ -1,6 +1,6 @@
-import { function_run_prompt } from "../../love/js/function_run_prompt.mjs";
-import { lock_wait } from "../../love/js/lock_wait.mjs";
+import { fn_name } from "./fn_name.mjs";
+import { lock_wait } from "./lock_wait.mjs";
 export async function lock_wait_prompt(lambda, who) {
-  let r2 = await lock_wait(function_run_prompt.name, lambda, who);
+  let r2 = await lock_wait(fn_name("function_run_prompt"), lambda, who);
   return r2;
 }
