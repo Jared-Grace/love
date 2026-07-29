81acde218c2a03df8c08cdd42cd263c940ee5f88
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:21:08 +0800

    function_name_only_imports_spell app_a_file_system_initialize_download

--- js/app_a_file_system_initialize_download.mjs
+++ js/app_a_file_system_initialize_download.mjs
@@ -4,7 +4,6 @@ import { indexeddb_get_all } from "./indexeddb_get_all.mjs";
 import { list_property_exists_not_error } from "./list_property_exists_not_error.mjs";
 import { list_to_dictionary_property } from "./list_to_dictionary_property.mjs";
 import { indexeddb_put_multiple } from "./indexeddb_put_multiple.mjs";
-import { app_a_file_system_initialize } from "./app_a_file_system_initialize.mjs";
 import { app_a } from "./app_a.mjs";
 import { storage_local_set } from "./storage_local_set.mjs";
 import { browser_files_store } from "./browser_files_store.mjs";
@@ -22,7 +21,7 @@ export async function app_a_file_system_initialize_download() {
   let dictionary = list_to_dictionary_property(r, "key");
   let store = browser_files_store();
   await indexeddb_put_multiple(db_get, store, dictionary);
-  storage_local_set(app_a, app_a_file_system_initialize.name, true);
+  storage_local_set(app_a, fn_name("app_a_file_system_initialize"), true);
   let all = await indexeddb_get_all(db_get, store);
   list_property_exists_not_error(all, "key");
 }
