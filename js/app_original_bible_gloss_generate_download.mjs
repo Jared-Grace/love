de509388a44e670bb5dcf9b0aa06e5d2c15578a1
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 11:32:17 +0800

    ai

--- js/app_original_bible_gloss_generate_download.mjs
+++ js/app_original_bible_gloss_generate_download.mjs
@@ -1,6 +1,5 @@
-import { firebase_storage_url_project_jg } from "./firebase_storage_url_project_jg.mjs";
+import { firebase_storage_download_json_decompress_project_jg } from "./firebase_storage_download_json_decompress_project_jg.mjs";
 import { app_original_bible_gloss_generate_upload_path } from "./app_original_bible_gloss_generate_upload_path.mjs";
-import { global_firebase_storage_download_json_decompress } from "./global_firebase_storage_download_json_decompress.mjs";
 export async function app_original_bible_gloss_generate_download(chapter_code) {
   let destination_get = app_original_bible_gloss_generate_upload_path;
   let fn = app_original_bible_gloss_generate_download;
