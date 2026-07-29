297d06604942b0b4fdb5ac26aeac96a364b00339
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 21:22:03 +0800

    function_name_only_imports_spell js_assert_arguments_args

--- js/js_assert_arguments_args.mjs
+++ js/js_assert_arguments_args.mjs
@@ -1,9 +1,9 @@
+import { fn_name } from "./fn_name.mjs";
 import { js_flo_params_get } from "./js_flo_params_get.mjs";
 import { list_replace_all } from "./list_replace_all.mjs";
 import { list_size } from "./list_size.mjs";
 import { js_special_arguments } from "./js_special_arguments.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
-import { arguments_assert } from "./arguments_assert.mjs";
 import { js_visit_calls_named } from "./js_visit_calls_named.mjs";
 export function js_assert_arguments_args(ast) {
   let params = js_flo_params_get(ast);
@@ -15,5 +15,5 @@ export function js_assert_arguments_args(ast) {
     let args_new = [args_expression, size_expression];
     list_replace_all(args, args_new);
   }
-  js_visit_calls_named(ast, arguments_assert.name, lambda);
+  js_visit_calls_named(ast, fn_name("arguments_assert"), lambda);
 }
