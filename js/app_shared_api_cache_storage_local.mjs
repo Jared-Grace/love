803a885b54cca87cf9926d8a7b3cdafe445c1d15
JESUS rose to life <username@users.noreply.github.com>
2026-07-29 20:23:45 +0800

    ai

--- js/app_shared_api_cache_storage_local.mjs
+++ js/app_shared_api_cache_storage_local.mjs
@@ -1,7 +1,12 @@
 import { app_shared_api_generic } from "./app_shared_api_generic.mjs";
 import { http_post_json_cache_storage_local } from "./http_post_json_cache_storage_local.mjs";
 export async function app_shared_api_cache_storage_local(f_name, args) {
-  let fn = http_post_json_cache_storage_local;
-  let result = await app_shared_api_generic(f_name, args, fn);
+  let fn_http = http_post_json_cache_storage_local;
+  let a = {
+    f_name,
+    args,
+    fn_http,
+  };
+  let result = await app_shared_api_generic(a);
   return result;
 }
