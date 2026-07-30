dc2252fb4e773cf8ee9cafa4f2373e2e8cc506c7
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 15:25:45 +0800

    ai

--- js/app_supper_home_generic.mjs
+++ js/app_supper_home_generic.mjs
@@ -1,5 +1,6 @@
+import { app_shared_spaced_small_gap } from "./app_shared_spaced_small_gap.mjs";
+import { app_shared_content_center_padding_gap } from "./app_shared_content_center_padding_gap.mjs";
 import { app_shared_column_max_width } from "./app_shared_column_max_width.mjs";
-import { app_shared_content_center_padding } from "./app_shared_content_center_padding.mjs";
 import { html_clear_context } from "./html_clear_context.mjs";
 import { html_bar_content_padded } from "./html_bar_content_padded.mjs";
 import { html_centered } from "./html_centered.mjs";
