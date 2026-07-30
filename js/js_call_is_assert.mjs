896b2d4212ade344ee4309db717e87d93474a3b9
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 20:24:47 +0800

    function_assert_json_get_lambda_collapse js_call_is_assert

--- js/js_call_is_assert.mjs
+++ js/js_call_is_assert.mjs
@@ -2,11 +2,8 @@ import { assert_json_get } from "./assert_json_get.mjs";
 import { js_call_is } from "./js_call_is.mjs";
 export function js_call_is_assert(node_call) {
   let b = js_call_is(node_call);
-  function lambda() {
-    let r = {
-      node_call,
-    };
-    return r;
-  }
-  assert_json_get(b, lambda);
+  let r = {
+    node_call,
+  };
+  assert_json(b, r);
 }
