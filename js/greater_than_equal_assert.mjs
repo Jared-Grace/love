16d1a31133660a3c8c34988a83cbe21d7b1d38bd
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 20:24:47 +0800

    function_assert_json_get_lambda_collapse greater_than_equal_assert

--- js/greater_than_equal_assert.mjs
+++ js/greater_than_equal_assert.mjs
@@ -2,12 +2,9 @@ import { assert_json_get } from "./assert_json_get.mjs";
 import { greater_than_equal } from "./greater_than_equal.mjs";
 export function greater_than_equal_assert(left, right) {
   let l = greater_than_equal(left, right);
-  function lambda() {
-    let r = {
-      left,
-      right,
-    };
-    return r;
-  }
-  assert_json_get(l, lambda);
+  let r = {
+    left,
+    right,
+  };
+  assert_json(l, r);
 }
