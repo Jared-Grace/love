69f663d064f21746016d097d6c2a90da5ce437e9
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 20:24:48 +0800

    function_assert_json_get_lambda_collapse text_includes_assert

--- js/text_includes_assert.mjs
+++ js/text_includes_assert.mjs
@@ -2,12 +2,9 @@ import { assert_json_get } from "./assert_json_get.mjs";
 import { text_includes } from "./text_includes.mjs";
 export function text_includes_assert(input, part) {
   let i = text_includes(input, part);
-  function lambda() {
-    let r = {
-      input,
-      part,
-    };
-    return r;
-  }
-  assert_json_get(i, lambda);
+  let r = {
+    input,
+    part,
+  };
+  assert_json(i, r);
 }
