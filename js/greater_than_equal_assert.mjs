a21f7df895eef3abfff74b5ce2fa71ee74e42aaa
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 20:28:30 +0800

    ai

--- js/greater_than_equal_assert.mjs
+++ js/greater_than_equal_assert.mjs
@@ -1,4 +1,4 @@
-import { assert_json_get } from "./assert_json_get.mjs";
+import { assert_json } from "./assert_json.mjs";
 import { greater_than_equal } from "./greater_than_equal.mjs";
 export function greater_than_equal_assert(left, right) {
   let l = greater_than_equal(left, right);
--- js/js_node_type_is_assert_json.mjs
+++ js/js_node_type_is_assert_json.mjs
@@ -1,4 +1,4 @@
-import { assert_json_get } from "./assert_json_get.mjs";
+import { assert_json } from "./assert_json.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_node_type_is_assert_json(node, type, json) {
   let type_is = js_node_type_is(node, type);
