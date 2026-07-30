0c2b9c1c2d7bbd9d5b201fff005c37b36f189030
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 20:24:46 +0800

    function_assert_json_get_lambda_collapse app_replace_rule_set_verify_from

--- js/app_replace_rule_set_verify_from.mjs
+++ js/app_replace_rule_set_verify_from.mjs
@@ -4,13 +4,10 @@ import { property_get } from "./property_get.mjs";
 export function app_replace_rule_set_verify_from(rules_parsed, start, end) {
   let dfs = app_replace_rule_set_verify_from_try(rules_parsed, start, end);
   let found = property_get(dfs, "found");
-  function lambda() {
-    let r = {
-      start,
-      end,
-    };
-    return r;
-  }
-  assert_json_get(found, lambda);
+  let r = {
+    start,
+    end,
+  };
+  assert_json(found, r);
   return dfs;
 }
