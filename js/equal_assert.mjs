b6aadd704c7ea220bdda30fd8b1eaaec006f34a9
JESUS rose to life <username@users.noreply.github.com>
2026-07-30 20:24:47 +0800

    function_assert_json_get_lambda_collapse equal_assert

--- js/equal_assert.mjs
+++ js/equal_assert.mjs
@@ -2,12 +2,9 @@ import { assert_json_get } from "./assert_json_get.mjs";
 import { equal } from "./equal.mjs";
 export function equal_assert(left, right) {
   let eq = equal(left, right);
-  function lambda() {
-    let r = {
-      left,
-      right,
-    };
-    return r;
-  }
-  assert_json_get(eq, lambda);
+  let r = {
+    left,
+    right,
+  };
+  assert_json(eq, r);
 }
