31c71564ba3cd13c61776789a9c49478a4ce3a1e
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:33:19 +0800

    function_parallel_mark folder_current

--- js/folder_current.mjs
+++ js/folder_current.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function folder_current() {
+  function_duplicate_kind_parallel();
   let current = ".";
   return current;
 }
