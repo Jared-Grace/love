71396dcd8c6247223b366cba2de5b825cc25df5a
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:33:59 +0800

    function_parallel_mark js_keyword_public

--- js/js_keyword_public.mjs
+++ js/js_keyword_public.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function js_keyword_public() {
+  function_duplicate_kind_parallel();
   let k = "public";
   return k;
 }
