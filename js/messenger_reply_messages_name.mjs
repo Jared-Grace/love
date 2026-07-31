f02e7c49a285ae6ebee214b4325de2e75573f418
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:37:42 +0800

    function_parallel_mark messenger_reply_messages_name

--- js/messenger_reply_messages_name.mjs
+++ js/messenger_reply_messages_name.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function messenger_reply_messages_name() {
+  function_duplicate_kind_parallel();
   let v = "name";
   return v;
 }
