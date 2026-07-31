e908d0f6f756cfdebbb97d7316bb2c40c73ea163
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:37:01 +0800

    function_parallel_mark digit_max

--- js/digit_max.mjs
+++ js/digit_max.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function digit_max() {
+  function_duplicate_kind_parallel();
   let r = 9;
   return r;
 }
