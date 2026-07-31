8ac3cca91f2ef7f4d37b6c6139b82e2478724dbd
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:37:34 +0800

    function_parallel_mark app_shared_language_sort_by_name

--- js/app_shared_language_sort_by_name.mjs
+++ js/app_shared_language_sort_by_name.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function app_shared_language_sort_by_name() {
+  function_duplicate_kind_parallel();
   let mode = "name";
   return mode;
 }
