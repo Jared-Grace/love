a77f548bf62400c3cdaa9c3e68fbdf878b511ff1
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 06:24:10 +0800

    function_fold list_next js_find_statement_after

--- js/js_find_statement_after.mjs
+++ js/js_find_statement_after.mjs
@@ -1,11 +1,9 @@
-import { list_get } from "./list_get.mjs";
+import { list_next } from "./list_next.mjs";
 import { js_flo_body } from "./js_flo_body.mjs";
 import { js_find_declaration_named } from "./js_find_declaration_named.mjs";
-import { list_index_of_next } from "./list_index_of_next.mjs";
 export function js_find_statement_after(ast, name) {
   let body = js_flo_body(ast);
   let statement = js_find_declaration_named(ast, name);
-  let after = list_index_of_next(body, statement);
-  let item = list_get(body, after);
+  let item = list_next(body, statement);
   return item;
 }
