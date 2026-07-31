c397a4a65619edd1e07994d85c985e2aa127b71c
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:29:41 +0800

    ai

--- js/ebible_verses_storage_browser.mjs
+++ js/ebible_verses_storage_browser.mjs
@@ -1,7 +1,5 @@
-import { firebase_storage_download_json_decompress_cache } from "./firebase_storage_download_json_decompress_cache.mjs";
+import { firebase_storage_verses_download_cache } from "./firebase_storage_verses_download_cache.mjs";
 import { ebible_firebase_upload_path } from "./ebible_firebase_upload_path.mjs";
-import { firebase_storage_url_project_jg } from "./firebase_storage_url_project_jg.mjs";
-import { property_get } from "./property_get.mjs";
 export async function ebible_verses_storage_browser(
   bible_folder,
   chapter_code,
