97a1d3b9fc249cdf738ab2697a67c773ceeaa007
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:33:42 +0800

    function_parallel_mark reply_word_in

--- js/reply_word_in.mjs
+++ js/reply_word_in.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function reply_word_in() {
+  function_duplicate_kind_parallel();
   let v = "in";
   return v;
 }
