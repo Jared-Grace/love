1d100a758f7936863e9005c67dda2b2442dcaac9
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:36:57 +0800

    function_parallel_mark app_replace_rule_set_verify_goal_depth_max

--- js/app_replace_rule_set_verify_goal_depth_max.mjs
+++ js/app_replace_rule_set_verify_goal_depth_max.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function app_replace_rule_set_verify_goal_depth_max() {
+  function_duplicate_kind_parallel();
   let max = 9;
   return max;
 }
