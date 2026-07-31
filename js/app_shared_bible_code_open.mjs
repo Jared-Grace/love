6d78bc73bda5411836ff755e92aa6983b1e546f7
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 21:55:59 +0800

    ai

--- js/app_shared_bible_code_open.mjs
+++ js/app_shared_bible_code_open.mjs
@@ -1,3 +1,5 @@
+import { app_shared_bible_mode_hash_key } from "./app_shared_bible_mode_hash_key.mjs";
+import { app_shared_bible_mode_chapter } from "./app_shared_bible_mode_chapter.mjs";
 import { html_hash_transform_reload } from "./html_hash_transform_reload.mjs";
 import { property_set } from "./property_set.mjs";
 export function app_shared_bible_code_open(chapter_code) {
@@ -6,6 +8,9 @@ export function app_shared_bible_code_open(chapter_code) {
     property_set(hash, "v", "");
     property_set(hash, "b", "");
     property_set(hash, "ref", "");
+    let property_name = app_shared_bible_mode_hash_key();
+    let value = app_shared_bible_mode_chapter();
+    property_set(hash, property_name, value);
   }
   html_hash_transform_reload(transform);
 }
