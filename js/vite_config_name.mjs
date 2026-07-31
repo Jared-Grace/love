e45df7c9bfd0bab83da68480761bdb42c5f9c66b
JESUS rose to life <username@users.noreply.github.com>
2026-07-31 22:37:50 +0800

    function_parallel_mark vite_config_name

--- js/vite_config_name.mjs
+++ js/vite_config_name.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function vite_config_name() {
+  function_duplicate_kind_parallel();
   let r = "name";
   return r;
 }
