28527a3d3e7b25ffe7c7149402fe5dc4d3979a96
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:44 +0800

    function_fold property_path_get_2 js_visit_function_nodes_names

--- js/js_visit_function_nodes_names.mjs
+++ js/js_visit_function_nodes_names.mjs
@@ -1,3 +1,4 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { js_visit_function_nodes } from "./js_visit_function_nodes.mjs";
 import { property_get } from "./property_get.mjs";
 import { js_identifier_is } from "./js_identifier_is.mjs";
@@ -5,8 +6,7 @@ import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_visit_function_nodes_names(ast) {
   function lambda2(la) {
     function lambda_declaration(v) {
-      let node = property_get(v, "node");
-      let id = property_get(node, "id");
+      let id = property_path_get_2(v, "node", "id");
       let is_identifier = js_identifier_is(id);
       if (is_identifier) {
         let name = property_get(id, "name");
