68bebf1463416418bd43c46dc7286eca19cbb913
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:14:29 +0800

    function_literal_route app_supper_passage_index_set app_supper_passage_hash_key

--- js/app_supper_passage_index_set.mjs
+++ js/app_supper_passage_index_set.mjs
@@ -1,4 +1,5 @@
+import { app_supper_passage_hash_key } from "./app_supper_passage_hash_key.mjs";
 import { html_hash_object_property_set } from "./html_hash_object_property_set.mjs";
 export function app_supper_passage_index_set(index) {
-  html_hash_object_property_set("sp", index);
+  html_hash_object_property_set(app_supper_passage_hash_key(), index);
 }
