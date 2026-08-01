63555559224f974e63c031070f96333b14489f95
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 12:13:00 +0800

    ai

--- js/list_empty_is.mjs
+++ js/list_empty_is.mjs
@@ -1,5 +1,7 @@
+import { equal } from "./equal.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_empty_is(list) {
-  let e = list_size(list) === 0;
+  let left = list_size(list);
+  let e = equal(left, 0);
   return e;
 }
