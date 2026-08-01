222bcb63eb302627501c11c8d00f0184d939b097
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:20:15 +0800

    function_fold property_path_get_2 app_replace_tests_run_e2e_normal_fn

--- js/app_replace_tests_run_e2e_normal_fn.mjs
+++ js/app_replace_tests_run_e2e_normal_fn.mjs
@@ -1,3 +1,4 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { equal_not } from "./equal_not.mjs";
 import { app_replace_rule_set_attribute_symbol } from "./app_replace_rule_set_attribute_symbol.mjs";
 import { app_replace_rule_set_attribute_refresh_click } from "./app_replace_rule_set_attribute_refresh_click.mjs";
@@ -8,8 +9,7 @@ export async function app_replace_tests_run_e2e_normal_fn(
   page,
   rule_original_previous,
 ) {
-  let rule = property_get(p, "rule");
-  let original = property_get(rule, "original");
+  let original = property_path_get_2(p, "rule", "original");
   if (equal_not(original, rule_original_previous)) {
     refresh_count = await app_replace_rule_set_attribute_refresh_click(
       page,
