4181fe130f3f74b8f01516930757627f4dcf35f8
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 17:05:30 +0800

    ai

--- js/js_storage_key_word_forwarded_is.mjs
+++ js/js_storage_key_word_forwarded_is.mjs
@@ -1,5 +1,5 @@
+import { js_call_argument_at_try } from "./js_call_argument_at_try.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { js_call_argument_at } from "./js_call_argument_at.mjs";
 import { js_identifier_name_try } from "./js_identifier_name_try.mjs";
 import { js_visit_calls_named_nodes } from "./js_visit_calls_named_nodes.mjs";
 import { not_equal } from "./not_equal.mjs";
