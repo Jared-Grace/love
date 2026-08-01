fa1f3e25200564fb7b419c9320a4bf5183c62dde
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 21:14:14 +0800

    function_fold property_list_size g_coordinates_path_shortest

--- js/g_coordinates_path_shortest.mjs
+++ js/g_coordinates_path_shortest.mjs
@@ -1,12 +1,10 @@
-import { graph_path_shortest_list } from "../../love/js/graph_path_shortest_list.mjs";
-import { g_coordinates_neighbors_walkable_get } from "../../love/js/g_coordinates_neighbors_walkable_get.mjs";
-import { g_coordinates_key } from "../../love/js/g_coordinates_key.mjs";
-import { property_get } from "../../love/js/property_get.mjs";
-import { list_size } from "../../love/js/list_size.mjs";
+import { property_list_size } from "./property_list_size.mjs";
+import { graph_path_shortest_list } from "./graph_path_shortest_list.mjs";
+import { g_coordinates_neighbors_walkable_get } from "./g_coordinates_neighbors_walkable_get.mjs";
+import { g_coordinates_key } from "./g_coordinates_key.mjs";
 export function g_coordinates_path_shortest(g, start, target) {
   let neighbors_get = g_coordinates_neighbors_walkable_get(g);
-  let coordinates = property_get(g, "coordinates");
-  let max_depth = list_size(coordinates);
+  let max_depth = property_list_size(g, "coordinates");
   let path = graph_path_shortest_list(
     start,
     neighbors_get,
