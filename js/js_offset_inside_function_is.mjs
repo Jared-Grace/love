329613d0d8f063e27580c366bc0420b2108fb8d9
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 18:13:57 +0800

    function_shadowing_rename js_offset_inside_function_is inside inside_inner

--- js/js_offset_inside_function_is.mjs
+++ js/js_offset_inside_function_is.mjs
@@ -16,8 +16,8 @@ export function js_offset_inside_function_is(ast, offset) {
     let end = property_get(node, "end");
     let after = greater_than(offset, start);
     let before = less_than(offset, end);
-    let inside = after && before;
-    return inside;
+    let inside_inner = after && before;
+    return inside_inner;
   }
   let inside = list_any(all, contains_is);
   return inside;
