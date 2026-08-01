a6cabe4901ced12c084d7bf1716c9998df427118
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:25 +0800

    function_fold property_equals js_imports_source_rename

--- js/js_imports_source_rename.mjs
+++ js/js_imports_source_rename.mjs
@@ -1,16 +1,15 @@
+import { property_equals } from "./property_equals.mjs";
 import { js_expression_string } from "./js_expression_string.mjs";
 import { js_imports_declarations } from "./js_imports_declarations.mjs";
 import { property_get } from "./property_get.mjs";
 import { property_set } from "./property_set.mjs";
-import { equal } from "./equal.mjs";
 import { each } from "./each.mjs";
 export function js_imports_source_rename(ast, source_before, source_after) {
   let imports_declarations = js_imports_declarations(ast);
   function lambda(i) {
     let declaration = property_get(i, "declaration");
     let source = property_get(declaration, "source");
-    let value = property_get(source, "value");
-    let b = equal(value, source_before);
+    let b = property_equals(source, "value", source_before);
     if (b) {
       let expression = js_expression_string(source_after);
       property_set(declaration, "source", expression);
