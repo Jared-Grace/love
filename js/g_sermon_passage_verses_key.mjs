35fefbca3c556e16e383a60b0b15b09f3b968afa
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 21:29:13 +0800

    function_fold property_list_join_comma g_sermon_passage_verses_key

--- js/g_sermon_passage_verses_key.mjs
+++ js/g_sermon_passage_verses_key.mjs
@@ -1,7 +1,5 @@
-import { property_get } from "./property_get.mjs";
-import { list_join_comma } from "./list_join_comma.mjs";
+import { property_list_join_comma } from "./property_list_join_comma.mjs";
 export function g_sermon_passage_verses_key(passage) {
-  let verse_numbers = property_get(passage, "verse_numbers");
-  let key = list_join_comma(verse_numbers);
+  let key = property_list_join_comma(passage, "verse_numbers");
   return key;
 }
