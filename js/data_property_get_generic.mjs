f70354cfd1ac01282f69c117c4618ea6b2becaf9
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:20:17 +0800

    function_fold property_path_get_2 data_property_get_generic

--- js/data_property_get_generic.mjs
+++ js/data_property_get_generic.mjs
@@ -1,8 +1,7 @@
-import { property_get } from "./property_get.mjs";
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { data_get } from "./data_get.mjs";
 export async function data_property_get_generic(d_path, property_name) {
   let v = await data_get(property_name, null, d_path);
-  let data = property_get(v, "data");
-  let value = property_get(data, property_name);
+  let value = property_path_get_2(v, "data", property_name);
   return value;
 }
