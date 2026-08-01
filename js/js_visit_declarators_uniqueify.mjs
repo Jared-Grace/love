f63d497208b2c6cadb9bdfa1c7f4aecb121a7237
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:43 +0800

    function_fold property_path_get_2 js_visit_declarators_uniqueify

--- js/js_visit_declarators_uniqueify.mjs
+++ js/js_visit_declarators_uniqueify.mjs
@@ -1,12 +1,11 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { js_visit_declarators } from "./js_visit_declarators.mjs";
 import { js_identifier_is_if } from "./js_identifier_is_if.mjs";
 import { property_transform } from "./property_transform.mjs";
 import { js_identifier_unique_ast } from "./js_identifier_unique_ast.mjs";
-import { property_get } from "./property_get.mjs";
 export function js_visit_declarators_uniqueify(ast, copy) {
   function lambda2(v) {
-    let node = property_get(v, "node");
-    let id = property_get(node, "id");
+    let id = property_path_get_2(v, "node", "id");
     function lambda4() {
       function lambda3(value) {
         let unique = js_identifier_unique_ast(ast, value);
