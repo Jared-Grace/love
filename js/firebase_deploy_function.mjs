7686f8fd09907740c236ef99cf6281d227581ee0
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:20:18 +0800

    function_fold property_path_get_2 firebase_deploy_function

--- js/firebase_deploy_function.mjs
+++ js/firebase_deploy_function.mjs
@@ -1,3 +1,4 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { function_dependencies_code_unaliased } from "./function_dependencies_code_unaliased.mjs";
 import { property_get } from "./property_get.mjs";
 import { firebase_deploy_function_destination_latest } from "./firebase_deploy_function_destination_latest.mjs";
@@ -7,8 +8,7 @@ import { date_now_file } from "./date_now_file.mjs";
 export async function firebase_deploy_function(f_name) {
   let v = await function_dependencies_code_unaliased(f_name);
   let unaliased = property_get(v, "unaliased");
-  let d = property_get(v, "d");
-  let code = property_get(d, "code");
+  let code = property_path_get_2(v, "d", "code");
   let now_file = date_now_file();
   let destination = firebase_deploy_function_destination_json(
     now_file,
