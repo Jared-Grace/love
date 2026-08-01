81b4215b1460475c4972e4249d53c08a379a45b3
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:36:52 +0800

    ai

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { path_pattern_covers_cases_gate_run } from "./path_pattern_covers_cases_gate_run.mjs";
 import { permission_settings_rule_gate_run } from "./permission_settings_rule_gate_run.mjs";
 import { storage_local_key_words_gate_run } from "./storage_local_key_words_gate_run.mjs";
 import { js_dot_name_object_name_cases_gate_run } from "./js_dot_name_object_name_cases_gate_run.mjs";
