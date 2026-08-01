857bd052ffa63c22c7dd69b1473da96fbbd5793c
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:42 +0800

    function_fold property_path_get_2 js_statement_expression_nodes

--- js/js_statement_expression_nodes.mjs
+++ js/js_statement_expression_nodes.mjs
@@ -1,11 +1,10 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { js_visit_type } from "./js_visit_type.mjs";
-import { property_get } from "./property_get.mjs";
 import { list_adder } from "./list_adder.mjs";
 export function js_statement_expression_nodes(ast) {
   function lambda2(la) {
     function lambda(v) {
-      let node = property_get(v, "node");
-      let expression = property_get(node, "expression");
+      let expression = property_path_get_2(v, "node", "expression");
       la(expression);
     }
     js_visit_type(ast, "ExpressionStatement", lambda);
