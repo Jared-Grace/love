429676b9a2ade02935cae75192acc91f42cc3d42
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:44 +0800

    function_fold property_path_get_2 js_visit_import_specifiers_names

--- js/js_visit_import_specifiers_names.mjs
+++ js/js_visit_import_specifiers_names.mjs
@@ -1,11 +1,11 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { js_visit_import_specifiers } from "./js_visit_import_specifiers.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_adder_unique } from "./list_adder_unique.mjs";
 export function js_visit_import_specifiers_names(ast) {
   function lambda2(la) {
     function lambda_import(v) {
-      let node = property_get(v, "node");
-      let local = property_get(node, "local");
+      let local = property_path_get_2(v, "node", "local");
       let name = property_get(local, "name");
       la(name);
     }
