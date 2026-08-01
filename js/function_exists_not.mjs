b7bd6bc48bebf66718e693a9c3db1c2fc891ba7e
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 21:14:16 +0800

    function_fold property_not function_exists_not

--- js/function_exists_not.mjs
+++ js/function_exists_not.mjs
@@ -1,9 +1,7 @@
+import { property_not } from "./property_not.mjs";
 import { function_exists } from "./function_exists.mjs";
-import { property_get } from "./property_get.mjs";
-import { not } from "./not.mjs";
 export async function function_exists_not(f_name) {
   let v = await function_exists(f_name);
-  let exists = property_get(v, "exists");
-  let n = not(exists);
+  let n = property_not(v, "exists");
   return n;
 }
