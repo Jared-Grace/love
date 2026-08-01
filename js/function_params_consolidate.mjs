dec843926a839c61b25b8e1d6c464dd2eb12945d
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:42:34 +0800

    function_fold js_flo_params_get function_params_consolidate

--- js/function_params_consolidate.mjs
+++ js/function_params_consolidate.mjs
@@ -1,16 +1,14 @@
+import { js_flo_params_get } from "./js_flo_params_get.mjs";
 import { js_property_identifier } from "./js_property_identifier.mjs";
 import { functions_names } from "./functions_names.mjs";
 import { list_clear } from "./list_clear.mjs";
 import { js_identifiers_to_names } from "./js_identifiers_to_names.mjs";
-import { js_function_declaration_params_get } from "./js_function_declaration_params_get.mjs";
-import { js_flo } from "./js_flo.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { list_map } from "./list_map.mjs";
 import { list_add } from "./list_add.mjs";
 export async function function_params_consolidate(f_name) {
   async function lambda(ast) {
-    let declaration = js_flo(ast);
-    let params = js_function_declaration_params_get(declaration);
+    let params = js_flo_params_get(ast);
     let params_names = js_identifiers_to_names(params);
     function lambda2(param_name) {
       let v = js_property_identifier(param_name);
