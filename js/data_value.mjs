4b436e9c121b119e24abda70f47fb073c3b71b2b
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:20:18 +0800

    function_fold property_path_get_2 data_value

--- js/data_value.mjs
+++ js/data_value.mjs
@@ -1,8 +1,7 @@
-import { property_get } from "./property_get.mjs";
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { data_all } from "./data_all.mjs";
 export async function data_value(property_name, d_path) {
   var v = await data_all(d_path);
-  let data = property_get(v, "data");
-  let value = property_get(data, property_name);
+  let value = property_path_get_2(v, "data", property_name);
   return value;
 }
