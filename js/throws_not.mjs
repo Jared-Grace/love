98f0fcace8c3896865f8bdd6f11014dc710166e7
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 21:14:18 +0800

    function_fold property_not throws_not

--- js/throws_not.mjs
+++ js/throws_not.mjs
@@ -1,9 +1,7 @@
-import { not } from "./not.mjs";
-import { property_get } from "./property_get.mjs";
+import { property_not } from "./property_not.mjs";
 import { lambda_throws } from "./lambda_throws.mjs";
 export function throws_not(lambda) {
   let r = lambda_throws(lambda);
-  let throws = property_get(r, "throws");
-  let valid = not(throws);
+  let valid = property_not(r, "throws");
   return valid;
 }
