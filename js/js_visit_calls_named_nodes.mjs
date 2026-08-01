bb6ddbab66b87f1419131782ae45857ae24676e7
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:42 +0800

    function_fold property_path_get_2 js_visit_calls_named_nodes

--- js/js_visit_calls_named_nodes.mjs
+++ js/js_visit_calls_named_nodes.mjs
@@ -1,9 +1,8 @@
-import { property_get } from "./property_get.mjs";
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { js_visit_calls_named } from "./js_visit_calls_named.mjs";
 export function js_visit_calls_named_nodes(ast, f_name, lambda) {
   function lambda2(a) {
-    let v = property_get(a, "v");
-    let node = property_get(v, "node");
+    let node = property_path_get_2(a, "v", "node");
     lambda(node);
   }
   js_visit_calls_named(ast, f_name, lambda2);
