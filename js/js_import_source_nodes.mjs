312f2efe33b48cdf8ab1b275148bfa86d9c1cd5c
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:43:40 +0800

    function_fold property_path_get_2 js_import_source_nodes

--- js/js_import_source_nodes.mjs
+++ js/js_import_source_nodes.mjs
@@ -1,11 +1,10 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { js_imports_all } from "./js_imports_all.mjs";
-import { property_get } from "./property_get.mjs";
 import { list_map } from "./list_map.mjs";
 export function js_import_source_nodes(ast) {
   let imports = js_imports_all(ast);
   function lambda(v) {
-    let node = property_get(v, "node");
-    let source = property_get(node, "source");
+    let source = property_path_get_2(v, "node", "source");
     return source;
   }
   let sources = list_map(imports, lambda);
