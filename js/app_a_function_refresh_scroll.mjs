48b183edf7729e8b77733116bea8703ad4856514
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:20:12 +0800

    function_fold property_path_get_2 app_a_function_refresh_scroll

--- js/app_a_function_refresh_scroll.mjs
+++ js/app_a_function_refresh_scroll.mjs
@@ -1,11 +1,10 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { html_scroll_top_get } from "./html_scroll_top_get.mjs";
 import { html_scroll_top_set } from "./html_scroll_top_set.mjs";
 import { app_a_function } from "./app_a_function.mjs";
-import { property_get } from "./property_get.mjs";
 export async function app_a_function_refresh_scroll(content, context) {
   let scroll_top = html_scroll_top_get(content);
   let r = await app_a_function(context);
-  let a = property_get(r, "a");
-  let content2 = property_get(a, "content");
+  let content2 = property_path_get_2(r, "a", "content");
   html_scroll_top_set(content2, scroll_top);
 }
