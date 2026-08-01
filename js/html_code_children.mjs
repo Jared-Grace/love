bc35ae68dee706e94ce79e54b3f053de61cf6c82
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 17:36:22 +0800

    function_shadowing_rename html_code_children r r2

--- js/html_code_children.mjs
+++ js/html_code_children.mjs
@@ -3,8 +3,8 @@ import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { text_combine } from "./text_combine.mjs";
 export function html_code_children(lines, indent) {
   function lambda(line) {
-    let r = text_combine_multiple(["\n", indent, line]);
-    return r;
+    let r2 = text_combine_multiple(["\n", indent, line]);
+    return r2;
   }
   let joined = list_map_join_empty(lines, lambda);
   let r = text_combine(joined, "\n");
