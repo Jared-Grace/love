963c6c11546f1b81e4aecb7f00196726caab525d
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 15:03:05 +0800

    ai

--- js/list_index_of_all.mjs
+++ js/list_index_of_all.mjs
@@ -1,8 +1,9 @@
+import { equal } from "./equal.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_reduce_index } from "./list_reduce_index.mjs";
 export function list_index_of_all(list, search) {
   function lambda(value, item, index) {
-    if (item === search) {
+    if (equal(item, search)) {
       list_add(value, index);
     }
     return value;
--- js/list_max.mjs
+++ js/list_max.mjs
@@ -1,7 +1,8 @@
+import { greater_than } from "./greater_than.mjs";
 import { list_reduce } from "./list_reduce.mjs";
 export function list_max(list) {
   function lambda(a, b) {
-    let v = a > b ? a : b;
+    let v = greater_than(a, b) ? a : b;
     return v;
   }
   let max = list_reduce(list, lambda, -Infinity);
