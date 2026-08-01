ce0d5f534d425f4b3300c194b71e0c9c34a85392
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 20:42:27 +0800

    function_fold equal_not list_without

--- js/list_without.mjs
+++ js/list_without.mjs
@@ -1,10 +1,8 @@
+import { equal_not } from "./equal_not.mjs";
 import { list_filter } from "./list_filter.mjs";
-import { equal } from "./equal.mjs";
-import { not } from "./not.mjs";
 export function list_without(list, item) {
   function lambda(other) {
-    let b = equal(other, item);
-    let eq = not(b);
+    let eq = equal_not(other, item);
     return eq;
   }
   let filtered = list_filter(list, lambda);
