32a3d69d240bbe84175812445e75af8c3ed0f7b2
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 21:14:16 +0800

    function_fold property_not function_unalias_exists_not

--- js/function_unalias_exists_not.mjs
+++ js/function_unalias_exists_not.mjs
@@ -1,9 +1,7 @@
-import { property_get } from "./property_get.mjs";
-import { not } from "./not.mjs";
+import { property_not } from "./property_not.mjs";
 import { function_unalias_exists } from "./function_unalias_exists.mjs";
 export async function function_unalias_exists_not(f_name) {
   let v = await function_unalias_exists(f_name);
-  let exists = property_get(v, "exists");
-  let n = not(exists);
+  let n = property_not(v, "exists");
   return n;
 }
