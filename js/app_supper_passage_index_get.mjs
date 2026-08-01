f11ca04fb1d85f49045e6ed9847a3ae3a88d8424
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:14:30 +0800

    function_literal_route app_supper_passage_index_get app_supper_passage_hash_key

--- js/app_supper_passage_index_get.mjs
+++ js/app_supper_passage_index_get.mjs
@@ -1,10 +1,11 @@
+import { app_supper_passage_hash_key } from "./app_supper_passage_hash_key.mjs";
 import { html_hash_object_get } from "./html_hash_object_get.mjs";
 import { property_get_or_null } from "./property_get_or_null.mjs";
 import { null_is } from "./null_is.mjs";
 import { integer_to } from "./integer_to.mjs";
 export function app_supper_passage_index_get() {
   let hash = html_hash_object_get();
-  let sp = property_get_or_null(hash, "sp");
+  let sp = property_get_or_null(hash, app_supper_passage_hash_key());
   let missing = null_is(sp);
   if (missing) {
     return 0;
