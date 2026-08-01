6affe3b74da1b927988e7ac3b16c97109b0e694d
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 12:31:28 +0800

    ai

--- js/app_a_home.mjs
+++ js/app_a_home.mjs
@@ -1,5 +1,5 @@
 import { app_a_functions } from "./app_a_functions.mjs";
 import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
-export function app_a_home(context) {
-  app_shared_screen_set(context, app_a_functions);
+export async function app_a_home(context) {
+  await app_shared_screen_set(context, app_a_functions);
 }
