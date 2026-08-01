2b4bcb9c402dc60bb1768951416f2d2a841a3889
JESUS rose to life <username@users.noreply.github.com>
2026-08-01 19:14:43 +0800

    function_literal_route app_supper_folders_get app_supper_folders_hash_key

--- js/app_supper_folders_get.mjs
+++ js/app_supper_folders_get.mjs
@@ -1,3 +1,4 @@
+import { app_supper_folders_hash_key } from "./app_supper_folders_hash_key.mjs";
 import { html_hash_object_get } from "./html_hash_object_get.mjs";
 import { property_get_or_null } from "./property_get_or_null.mjs";
 import { text_split_plus } from "./text_split_plus.mjs";
@@ -6,7 +7,7 @@ import { property_get } from "./property_get.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 export function app_supper_folders_get(context) {
   let hash = html_hash_object_get();
-  let sl = property_get_or_null(hash, "sl");
+  let sl = property_get_or_null(hash, app_supper_folders_hash_key());
   let missing = null_is(sl);
   if (missing) {
     let default_chosen = property_get(context, "supper_default_chosen");
