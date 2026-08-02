9ea85504d477a16c7072b4008c256a2f78a04df8
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 11:26:54 +0800

    function_shadowing_rename ebible_book_video_generate path_video path_video_chapter

--- js/ebible_book_video_generate.mjs
+++ js/ebible_book_video_generate.mjs
@@ -10,8 +10,8 @@ export async function ebible_book_video_generate(bible_folder, book_code) {
   );
   async function lambda(chapter_code) {
     let v = await ebible_chapter_videos_generate(bible_folder, chapter_code);
-    let path_video = property_get(v, "path_video");
-    return path_video;
+    let path_video_chapter = property_get(v, "path_video");
+    return path_video_chapter;
   }
   let paths_videos = await list_map_async(chapter_codes, lambda);
   let path_video = await bible_audio_folder_book_video_join(
