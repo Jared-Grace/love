6d73ad5827d539ee39b6212caca2f3a3a1cec765
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:54:06 +0800

    function_select_apply_args_auto qa_gates js_find_declaration_named gates js_array_identifier_add zzz_probe_wire_gate_run

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { zzz_probe_wire_gate_run } from "./zzz_probe_wire_gate_run.mjs";
 import { functions_locals_unread_gate_run } from "./functions_locals_unread_gate_run.mjs";
 import { qa_gates_repeated_gate_run } from "./qa_gates_repeated_gate_run.mjs";
 import { js_condition_bodies_empty_cases_gate_run } from "./js_condition_bodies_empty_cases_gate_run.mjs";
@@ -282,6 +283,7 @@ export function qa_gates() {
     functions_gate_run_unwired_gate_run,
     functions_cases_ungated_gate_run,
     qa_gates_repeated_gate_run,
+    zzz_probe_wire_gate_run,
   ];
   return gates;
 }
