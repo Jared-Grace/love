37722de0679b0e833719b37eda4be8629c88b3c9
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:56:47 +0800

    ai

--- js/app_a_functions_generic_f_names.mjs
+++ js/app_a_functions_generic_f_names.mjs
@@ -5,9 +5,9 @@ import { text_pad } from "./text_pad.mjs";
 import { functions_path } from "./functions_path.mjs";
 import { list_filter_ends_with } from "./list_filter_ends_with.mjs";
 import { function_name_extension } from "./function_name_extension.mjs";
-import { app_a_files_paths } from "./app_a_files_paths.mjs";
+import { browser_files_paths } from "./browser_files_paths.mjs";
 export async function app_a_functions_generic_f_names() {
-  let mapped = await app_a_files_paths();
+  let mapped = await browser_files_paths();
   let suffix = function_name_extension();
   let filtered3 = list_filter_ends_with(mapped, suffix);
   let joined = functions_path();
