a4165efe1e0715e6bce09823ac13a4464733bb12
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:08:17 +0800

    function_auto_checked html_scroll_settled

--- js/html_scroll_settled.mjs
+++ js/html_scroll_settled.mjs
@@ -1,6 +1,7 @@
+import { not_equal } from "./not_equal.mjs";
 export async function html_scroll_settled(element) {
   let settle = element.scroll_animation_settle;
-  let exists = settle !== undefined;
+  let exists = not_equal(settle, undefined);
   if (exists) {
     await settle;
   }
