e2833c6dd3519c3461954d911d1c14bca0f7ac7f
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:39:35 +0800

    function_rename app_bible_button_chapter_next app_shared_bible_button_chapter_next

--- js/app_shared_bible_button_chapter_next.mjs
+++ js/app_shared_bible_button_chapter_next.mjs
@@ -1,6 +1,10 @@
 import { app_shared_button_arrow_right } from "../../love/js/app_shared_button_arrow_right.mjs";
 import { app_bible_chapter_next } from "../../love/js/app_bible_chapter_next.mjs";
-export function app_bible_button_chapter_next(bar, context, chapter_code) {
+export function app_shared_bible_button_chapter_next(
+  bar,
+  context,
+  chapter_code,
+) {
   async function chapter_next() {
     await app_bible_chapter_next(context, chapter_code);
   }
