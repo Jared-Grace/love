6705d8a8c73090778fbf7b0eb7a9743f1dc296bd
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 12:02:22 +0800

    function_new_js selects_prose_add_generic

---
+++ js/js_selects_prose_add_before.mjs
@@ -0,0 +1,6 @@
+import { js_selects_prose_add_generic } from "./js_selects_prose_add_generic.mjs";
+export function js_selects_prose_add_before(ast, selects, sentence) {
+  "Lands the sentence on the line above the chosen statement taking its place in the block and pushing it down one";
+  let index_delta = 0;
+  js_selects_prose_add_generic(ast, selects, sentence, index_delta);
+}
