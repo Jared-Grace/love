3ae9deb407b3709391eccf6348db812fe09893c7
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:15:23 +0800

    function_literal_route_any ebible_language_to_bible_folder language_code_key

--- js/ebible_language_to_bible_folder.mjs
+++ js/ebible_language_to_bible_folder.mjs
@@ -1,3 +1,4 @@
+import { language_code_key } from "./language_code_key.mjs";
 import { bible_folder_key } from "./bible_folder_key.mjs";
 import { list_find_property_get } from "./list_find_property_get.mjs";
 import { ebible_languages } from "./ebible_languages.mjs";
@@ -5,7 +6,7 @@ export function ebible_language_to_bible_folder(language) {
   let languages_list = ebible_languages();
   let bible_folder = list_find_property_get(
     languages_list,
-    "language_code",
+    language_code_key(),
     language,
     bible_folder_key(),
   );
