ebd4a9830e1f8eb1cbd594a4b46fe90192e2f785
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 22:39:15 +0800

    function_auto_checked global_override

--- js/global_override.mjs
+++ js/global_override.mjs
@@ -1,14 +1,15 @@
+import { not } from "./not.mjs";
 import { property_get_or_null } from "./property_get_or_null.mjs";
 import { property_set } from "./property_set.mjs";
 export function global_override(fn) {
   let registry = property_get_or_null(globalThis, "overrides_global");
-  if (!registry) {
+  if (not(registry)) {
     registry = {};
     property_set(globalThis, "overrides_global", registry);
   }
   let key = fn.name;
   let overrides = property_get_or_null(registry, key);
-  if (!overrides) {
+  if (not(overrides)) {
     overrides = {};
     property_set(registry, key, overrides);
   }
