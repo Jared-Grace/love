43eb642677edffff11b8b7515b94f7e16a9815b9
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:38:21 +0800

    function_literal_route_any app_code_lesson_expression_string_hello app_code_category_expressions

--- js/app_code_lesson_expression_string_hello.mjs
+++ js/app_code_lesson_expression_string_hello.mjs
@@ -1,3 +1,4 @@
+import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 import { app_code_string_value_shape } from "./app_code_string_value_shape.mjs";
 import { app_code_lesson_expression_string_generic } from "./app_code_lesson_expression_string_generic.mjs";
 import { app_code_string_code } from "./app_code_string_code.mjs";
@@ -46,7 +47,7 @@ export function app_code_lesson_expression_string_hello() {
     let rights = ["string"];
     let built = app_code_lesson_name_id_generic(
       rights,
-      "expressions",
+      app_code_category_expressions(),
       title_get,
     );
     return built;
