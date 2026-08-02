c2a31443b6da6c13f62ce8c59680a530482d0f0b
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 10:47:15 +0800

    function_shadowing_rename js_calls_to_each nti nti2

--- js/js_calls_to_each.mjs
+++ js/js_calls_to_each.mjs
@@ -38,8 +38,8 @@ export function js_calls_to_each(ast) {
     let expression2 = js_statement_expression_get(next);
     let call2 = null;
     if (async_is) {
-      let nti = js_node_type_not_is(expression2, "AwaitExpression");
-      if (nti) {
+      let nti2 = js_node_type_not_is(expression2, "AwaitExpression");
+      if (nti2) {
         return;
       }
       call2 = property_get(expression2, "argument");
