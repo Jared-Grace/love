7cd314b72e8626022f1bcb73c6ebe4d6c1984755
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:40:21 +0800

    function_rename app_bible_offline app_shared_bible_offline

--- js/app_bible_screens_base.mjs
+++ js/app_bible_screens_base.mjs
@@ -3,7 +3,7 @@ import { app_bible_verses } from "./app_bible_verses.mjs";
 import { app_shared_bible_chapters } from "./app_shared_bible_chapters.mjs";
 import { app_shared_bible_books } from "./app_shared_bible_books.mjs";
 import { app_bible_settings } from "./app_bible_settings.mjs";
-import { app_bible_offline } from "./app_bible_offline.mjs";
+import { app_shared_bible_offline } from "./app_shared_bible_offline.mjs";
 import { list_concat } from "./list_concat.mjs";
 export function app_bible_screens_base(a) {
   let concated = list_concat(a, [
@@ -12,7 +12,7 @@ export function app_bible_screens_base(a) {
     app_bible_verses,
     app_shared_bible_languages,
     app_bible_settings,
-    app_bible_offline,
+    app_shared_bible_offline,
   ]);
   return concated;
 }
