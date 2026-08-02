f244ca9a3508dbc29baed5c0ee9fc39c249a2329
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 22:37:06 +0800

    function_auto_checked g_direction

--- js/g_direction.mjs
+++ js/g_direction.mjs
@@ -1,16 +1,25 @@
+import { equal } from "./equal.mjs";
 import { property_get } from "./property_get.mjs";
 import { subtract } from "./subtract.mjs";
 export function g_direction(from, to) {
-  let dx = subtract(property_get(to, "x"), property_get(from, "x"));
-  if (dx === 1) {
-    return "east";
+  let left = property_get(to, "x");
+  let right = property_get(from, "x");
+  let dx = subtract(left, right);
+  if (equal(dx, 1)) {
+    let r = "east";
+    return r;
   }
-  if (dx === -1) {
-    return "west";
+  if (equal(dx, -1)) {
+    let r2 = "west";
+    return r2;
   }
-  let dy = subtract(property_get(to, "y"), property_get(from, "y"));
-  if (dy === 1) {
-    return "south";
+  let left2 = property_get(to, "y");
+  let right2 = property_get(from, "y");
+  let dy = subtract(left2, right2);
+  if (equal(dy, 1)) {
+    let r3 = "south";
+    return r3;
   }
-  return "north";
+  let r4 = "north";
+  return r4;
 }
