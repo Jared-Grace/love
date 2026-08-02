b959b13d56688fd6fda3d8ccbb8035b13e6bd5d7
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 13:30:59 +0800

    ai

---
+++ js/js_wrap_probe_delete_me.mjs
@@ -0,0 +1,5 @@
+import { js_call_argument_named_text_set } from "./js_call_argument_named_text_set.mjs";
+export async function js_wrap_probe_delete_me(ast, selects, param_name, word) {
+  let r = await js_call_argument_named_text_set(ast, selects, param_name, word);
+  return r;
+}
