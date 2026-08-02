8654b4f349d70fc6ce02bc27f5d29f592bf278e0
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 10:59:00 +0800

    function_shadowing_rename app_calendar_paste_convert zone zone_inner

--- js/app_calendar_paste_convert.mjs
+++ js/app_calendar_paste_convert.mjs
@@ -62,11 +62,11 @@ export function app_calendar_paste_convert(input, country) {
   let formats = [converted_info, speaker_info];
   function lambda(item) {
     let start_inner = property_get(item, "start");
-    let zone = property_get(item, "zone");
+    let zone_inner = property_get(item, "zone");
     let name = property_get(item, "name");
     let parenthesis = property_get(item, "parenthesis");
     let flag = property_get(item, "flag");
-    let start_zoned = date_time_zone_set_zone(start_inner, zone);
+    let start_zoned = date_time_zone_set_zone(start_inner, zone_inner);
     let start_formatted = date_time_zone_format_to_time_space(start_zoned);
     let t = text_combine_multiple([
       name,
