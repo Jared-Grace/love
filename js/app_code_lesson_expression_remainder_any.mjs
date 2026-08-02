20598b3b1ab83155a077c67f5408c17d725e7726
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:39:39 +0800

    function_literal_route_any app_code_lesson_expression_remainder_any app_code_category_operators

--- js/app_code_lesson_expression_remainder_any.mjs
+++ js/app_code_lesson_expression_remainder_any.mjs
@@ -1,3 +1,4 @@
+import { app_code_category_operators } from "./app_code_category_operators.mjs";
 import { js_operator_percent_sign } from "./js_operator_percent_sign.mjs";
 import { divide_floor } from "./divide_floor.mjs";
 import { list_shuffle_take_map } from "./list_shuffle_take_map.mjs";
@@ -74,7 +75,11 @@ export function app_code_lesson_expression_remainder_any() {
       return render;
     }
     let rights = ["remainder"];
-    let built = app_code_lesson_name_id_generic(rights, "operators", title_get);
+    let built = app_code_lesson_name_id_generic(
+      rights,
+      app_code_category_operators(),
+      title_get,
+    );
     return built;
   }
   function above(root) {
