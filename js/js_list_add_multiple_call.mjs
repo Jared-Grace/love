17e25d2387cf1cb3af0ec37634db239eebce77c5
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 15:10:48 +0800

    ai

--- js/js_list_add_multiple_call.mjs
+++ js/js_list_add_multiple_call.mjs
@@ -1,12 +1,11 @@
-import { js_code_call } from "./js_code_call.mjs";
+import { js_code_call_parse_expression } from "./js_code_call_parse_expression.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { js_parse_expression } from "./js_parse_expression.mjs";
 import { js_call_arguments_get } from "./js_call_arguments_get.mjs";
 import { js_code_brackets_empty } from "./js_code_brackets_empty.mjs";
 import { js_array_expression_elements } from "./js_array_expression_elements.mjs";
 export function js_list_add_multiple_call(list_node, values) {
-  let code = js_code_call(list_add_multiple.name);
-  let call = js_parse_expression(code);
+  let call = js_code_call_parse_expression(list_add_multiple.name);
   let args = js_call_arguments_get(call);
   let brackets = js_code_brackets_empty();
   let array = js_parse_expression(brackets);
