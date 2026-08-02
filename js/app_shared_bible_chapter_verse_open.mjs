bdbb6e2fc434b09b5c36543bec31e72a8c7487ae
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:39:56 +0800

    function_rename app_bible_chapter_verse_open app_shared_bible_chapter_verse_open

--- js/app_shared_bible_chapter_verse_open.mjs
+++ js/app_shared_bible_chapter_verse_open.mjs
@@ -1,6 +1,6 @@
 import { app_bible_verse_open } from "./app_bible_verse_open.mjs";
 import { app_shared_bible_chapter_set } from "./app_shared_bible_chapter_set.mjs";
-export async function app_bible_chapter_verse_open(
+export async function app_shared_bible_chapter_verse_open(
   context,
   chapter,
   verse_number,
