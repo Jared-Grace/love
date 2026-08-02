b6db034ede6ee6174805ebb92f3e996ebab5bc38
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:12:06 +0800

    function_auto_checked equal_by_async

--- js/equal_by_async.mjs
+++ js/equal_by_async.mjs
@@ -1,4 +1,7 @@
+import { equal } from "./equal.mjs";
 export async function equal_by_async(a, b, lambda$item) {
-  let eq = (await lambda$item(a)) === (await lambda$item(b));
+  let left = await lambda$item(a);
+  let right = await lambda$item(b);
+  let eq = equal(left, right);
   return eq;
 }
