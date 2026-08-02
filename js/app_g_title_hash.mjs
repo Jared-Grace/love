56709b55fbc69e336dd52e765df47b070fe7d76c
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:13:55 +0800

    function_auto_checked app_g_title_hash

--- js/app_g_title_hash.mjs
+++ js/app_g_title_hash.mjs
@@ -1,8 +1,9 @@
+import { not_equal } from "./not_equal.mjs";
 import { html_hash_name_get } from "./html_hash_name_get.mjs";
 import { html_title_set } from "./html_title_set.mjs";
 export function app_g_title_hash() {
   let name = html_hash_name_get();
-  if (name !== "") {
+  if (not_equal(name, "")) {
     html_title_set(name);
   }
 }
