df45b95f29a75514d2a8ecf06b0ea591ea60323d
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:15:13 +0800

    function_literal_route_any app_reply_love language_code_key

--- js/app_reply_love.mjs
+++ js/app_reply_love.mjs
@@ -1,9 +1,10 @@
+import { language_code_key } from "./language_code_key.mjs";
 import { each_async } from "./each_async.mjs";
 import { list_find_property } from "./list_find_property.mjs";
 export async function app_reply_love(languages, language_choose) {
   let codes = ["tgl", "ceb"];
   async function lambda(code) {
-    let language = list_find_property(languages, "language_code", code);
+    let language = list_find_property(languages, language_code_key(), code);
     await language_choose(language);
   }
   await each_async(codes, lambda);
