4662f370df58b24651a19649eda30826ba81c3cf
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:28:09 +0800

    function_literal_route_any ebible_firebase_upload_verse verse_number_key

--- js/ebible_firebase_upload_verse.mjs
+++ js/ebible_firebase_upload_verse.mjs
@@ -1,3 +1,4 @@
+import { verse_number_key } from "./verse_number_key.mjs";
 import { ebible_verse_merge } from "./ebible_verse_merge.mjs";
 import { ebible_firebase_upload } from "./ebible_firebase_upload.mjs";
 import { ebible_verses_upload_name } from "./ebible_verses_upload_name.mjs";
@@ -7,7 +8,7 @@ export async function ebible_firebase_upload_verse(
   chapter_code,
   bible_folder,
 ) {
-  let verse_number = property_get(v, "verse_number");
+  let verse_number = property_get(v, verse_number_key());
   let joined = ebible_verses_upload_name(chapter_code, verse_number);
   let verse = ebible_verse_merge(bible_folder, chapter_code, v);
   await ebible_firebase_upload(bible_folder, joined, verse);
