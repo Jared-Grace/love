810237ec11afbe36ed0271944302ad587ab2fb34
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:04:54 +0800

    ai

--- js/html_loading_backdrop_style.mjs
+++ js/html_loading_backdrop_style.mjs
@@ -4,8 +4,8 @@ export function html_loading_backdrop_style() {
     position: "fixed",
     top: "0",
     left: "0",
-    width: "100vw",
-    height: "100vh",
+    width: html_viewport_width_full(),
+    height: html_viewport_height_full(),
     background: "rgba(0, 0, 0, 0.8)",
     "z-index": "1000",
   };
