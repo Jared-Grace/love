70d929295776a689568c70fcc4c2aec740ab0ae7
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 11:39:05 +0800

    ai

--- js/keyboard_typing_delay.mjs
+++ js/keyboard_typing_delay.mjs
@@ -1,12 +1,15 @@
+import { multiply_divide } from "./multiply_divide.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { multiply } from "./multiply.mjs";
-import { divide } from "./divide.mjs";
 export function keyboard_typing_delay() {
   let characters_per_minute = 500;
   let seconds_per_minute = 60;
   let ms_per_second = 1000;
-  let ms_per_minute = multiply(seconds_per_minute, ms_per_second);
-  let ms_per_character = divide(ms_per_minute, characters_per_minute);
+  let ms_per_character = multiply_divide(
+    seconds_per_minute,
+    ms_per_second,
+    characters_per_minute,
+  );
   let high = multiply(ms_per_character, 2);
   let delay_ms = integer_random(ms_per_character, high);
   return delay_ms;
