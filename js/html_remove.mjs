8de4be5e5ddcbdf806ae060eb1bf729a93011196
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:09:05 +0800

    function_auto_checked html_remove

--- js/html_remove.mjs
+++ js/html_remove.mjs
@@ -1,8 +1,9 @@
+import { not_equal } from "./not_equal.mjs";
 import { html_component_element_get } from "./html_component_element_get.mjs";
 export function html_remove(component) {
   let element = html_component_element_get(component);
   let remove_listeners = element.remove_listeners;
-  let has_listeners = remove_listeners !== undefined;
+  let has_listeners = not_equal(remove_listeners, undefined);
   if (has_listeners) {
     remove_listeners();
   }
