ab85d90900d7cdafbbd310bd95ff4babebf910a9
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:35:29 +0800

    function_literal_route_any app_code_lesson_expression_string_generic app_code_label_value_backwards

--- js/app_code_lesson_expression_string_generic.mjs
+++ js/app_code_lesson_expression_string_generic.mjs
@@ -1,3 +1,4 @@
+import { app_code_label_value_backwards } from "./app_code_label_value_backwards.mjs";
 import { app_code_label_value } from "./app_code_label_value.mjs";
 import { list_shuffle_take_map } from "./list_shuffle_take_map.mjs";
 import { app_code_lesson_expression_generic } from "./app_code_lesson_expression_generic.mjs";
@@ -26,7 +27,7 @@ export function app_code_lesson_expression_string_generic(params) {
     forwards_question_label: "The string: ",
     forwards_answer_label: app_code_label_value(),
     backwards_question_label: app_code_label_value(),
-    backwards_answer_label: "What code gives this value? ",
+    backwards_answer_label: app_code_label_value_backwards(),
     unscramble_label: "Build the code that gives this value: ",
   });
   return lesson;
