987e294d0c76130af26743cc874f194d6765e42c
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 15:43:40 +0800

    function_fold list_includes_not js_literals_used

--- js/js_literals_used.mjs
+++ js/js_literals_used.mjs
@@ -1,8 +1,7 @@
+import { list_includes_not } from "./list_includes_not.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { js_prose_literal_nodes } from "./js_prose_literal_nodes.mjs";
 import { js_list_type_nodes } from "./js_list_type_nodes.mjs";
-import { list_includes } from "./list_includes.mjs";
-import { not } from "./not.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function js_literals_used(ast) {
   "Every literal a function writes as a value rather than as an explanation.";
@@ -11,8 +10,7 @@ export function js_literals_used(ast) {
   let prose = js_prose_literal_nodes(ast);
   let literals = js_list_type_nodes(ast, "Literal");
   function written_as_value_is(node) {
-    let commented = list_includes(prose, node);
-    let used = not(commented);
+    let used = list_includes_not(prose, node);
     return used;
   }
   let r = list_filter(literals, written_as_value_is);
