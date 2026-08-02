7a408ad0af25704881d6de49ac671c1a3b398ff0
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:39:44 +0800

    function_literal_route_any app_code_lesson_validity_operator app_code_category_operators

--- js/app_code_lesson_validity_operator.mjs
+++ js/app_code_lesson_validity_operator.mjs
@@ -1,7 +1,17 @@
+import { app_code_category_operators } from "./app_code_category_operators.mjs";
 import { app_code_lesson_validity_code } from "./app_code_lesson_validity_code.mjs";
 import { app_code_lesson_name_id_symbol } from "./app_code_lesson_name_id_symbol.mjs";
-export function app_code_lesson_validity_operator(rights, symbol, batch, above) {
-  let name_id = app_code_lesson_name_id_symbol("operators", rights, symbol);
+export function app_code_lesson_validity_operator(
+  rights,
+  symbol,
+  batch,
+  above,
+) {
+  let name_id = app_code_lesson_name_id_symbol(
+    app_code_category_operators(),
+    rights,
+    symbol,
+  );
   let lesson = app_code_lesson_validity_code(batch, name_id, above);
   return lesson;
 }
