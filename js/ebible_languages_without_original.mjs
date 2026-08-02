bbb25c41871d35d58a24de819c7f308411bda13a
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:15:22 +0800

    function_literal_route_any ebible_languages_without_original language_code_key

--- js/ebible_languages_without_original.mjs
+++ js/ebible_languages_without_original.mjs
@@ -1,9 +1,10 @@
+import { language_code_key } from "./language_code_key.mjs";
 import { ebible_language_original_code } from "./ebible_language_original_code.mjs";
 import { list_remove_property } from "./list_remove_property.mjs";
 import { ebible_languages } from "./ebible_languages.mjs";
 export function ebible_languages_without_original() {
   let languages = ebible_languages();
   let original = ebible_language_original_code();
-  list_remove_property(languages, "language_code", original);
+  list_remove_property(languages, language_code_key(), original);
   return languages;
 }
