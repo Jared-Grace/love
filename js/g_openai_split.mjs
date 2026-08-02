d6514b7f8033f3d1d97f1411cf17c5640562d830
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:55:50 +0800

    function_rename app_g_openai_split g_openai_split

--- js/g_openai_split.mjs
+++ js/g_openai_split.mjs
@@ -4,7 +4,7 @@ import { list_map } from "./list_map.mjs";
 import { newline_windows } from "./newline_windows.mjs";
 import { text_split_multiple } from "./text_split_multiple.mjs";
 import { newline_windows_escaped } from "./newline_windows_escaped.mjs";
-export function app_g_openai_split(objections) {
+export function g_openai_split(objections) {
   let separator = newline_windows_escaped();
   let n = newline_windows();
   let split = text_split_multiple(objections, [separator, n]);
