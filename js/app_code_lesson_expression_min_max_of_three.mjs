7666c331ca18279f6988bcaff050ba8627db0917
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:33:07 +0800

    function_literal_route_any app_code_lesson_expression_min_max_of_three app_code_label_value

--- js/app_code_lesson_expression_min_max_of_three.mjs
+++ js/app_code_lesson_expression_min_max_of_three.mjs
@@ -1,3 +1,4 @@
+import { app_code_label_value } from "./app_code_label_value.mjs";
 import { equal_not } from "./equal_not.mjs";
 import { app_code_label_code_question } from "./app_code_label_code_question.mjs";
 import { html_div } from "./html_div.mjs";
@@ -83,8 +84,8 @@ export function app_code_lesson_expression_min_max_of_three() {
     example_count: 2,
     decoys,
     forwards_question_label: app_code_label_code_question(),
-    forwards_answer_label: "value: ",
-    backwards_question_label: "value: ",
+    forwards_answer_label: app_code_label_value(),
+    backwards_question_label: app_code_label_value(),
     backwards_answer_label: "What code gives this value? ",
     unscramble_label: "Build the code that gives this value: ",
   });
