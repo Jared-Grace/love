aeb076462cd8c532e8a3857384f152a70d1b993b
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 14:35:22 +0800

    file_math_calls_repair js/list_remove_end.mjs

--- js/list_remove_end.mjs
+++ js/list_remove_end.mjs
@@ -1,6 +1,7 @@
+import { math_max } from "./math_max.mjs";
 import { subtract } from "./subtract.mjs";
 export function list_remove_end(list, count) {
   let removed = list.slice(-count);
-  list.length = Math.max(0, subtract(list.length, count));
+  list.length = math_max(0, subtract(list.length, count));
   return removed;
 }
