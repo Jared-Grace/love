06e28f050e7303f6c80016d2072de8c5d4571982
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:10:34 +0800

    function_auto_checked g_sermon_passage_words

--- js/g_sermon_passage_words.mjs
+++ js/g_sermon_passage_words.mjs
@@ -1,10 +1,13 @@
+import { greater_than } from "./greater_than.mjs";
 import { text_split_space } from "./text_split_space.mjs";
 import { list_filter } from "./list_filter.mjs";
 export function g_sermon_passage_words(text) {
   let spaced = text.replace(/[-–—]+/g, " ");
   let split = text_split_space(spaced);
   function nonempty(word) {
-    return word.length > 0;
+    let g = greater_than(word.length, 0);
+    return g;
   }
-  return list_filter(split, nonempty);
+  let filtered = list_filter(split, nonempty);
+  return filtered;
 }
