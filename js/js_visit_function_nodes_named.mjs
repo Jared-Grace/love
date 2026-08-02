799f055965f3ab1721d2a87baeb23cf00028adec
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:23:15 +0800

    ai

--- js/js_visit_function_nodes_named.mjs
+++ js/js_visit_function_nodes_named.mjs
@@ -1,14 +1,10 @@
 import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { property_equals } from "./property_equals.mjs";
-import { log } from "./log.mjs";
 import { js_identifier_is_if } from "./js_identifier_is_if.mjs";
 import { js_visit_function_nodes } from "./js_visit_function_nodes.mjs";
 export function js_visit_function_nodes_named(ast, lambda$v, name) {
   function lambda(v) {
     let id = property_path_get_2(v, "node", "id");
-    log(js_visit_function_nodes_named.name, {
-      id,
-    });
     function lambda3() {
       let eq = property_equals(id, "name", name);
       if (eq) {
