e825c85489ba15c3307291a49d0371351522881b
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:07:50 +0800

    function_auto_checked js_html_style_assign_to_set_node

--- js/js_html_style_assign_to_set_node.mjs
+++ js/js_html_style_assign_to_set_node.mjs
@@ -1,3 +1,4 @@
+import { not } from "./not.mjs";
 import { js_code_call_parse_expression } from "./js_code_call_parse_expression.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { js_call_arguments_get } from "./js_call_arguments_get.mjs";
@@ -13,7 +14,8 @@ import { list_add_multiple } from "./list_add_multiple.mjs";
 import { object_replace } from "./object_replace.mjs";
 export function js_html_style_assign_to_set_node(node) {
   let args = js_call_arguments_get(node);
-  if (!list_size_2(args)) {
+  let b2 = list_size_2(args);
+  if (not(b2)) {
     return;
   }
   let b = list_first(args);
@@ -24,12 +26,11 @@ export function js_html_style_assign_to_set_node(node) {
   }
   let key_expression = js_property_key_string_expression(property);
   let value = js_property_value_get(property);
-  let expression = js_code_call_parse_expression(fn_name("html_style_set"));
+  let f_name = fn_name("html_style_set");
+  let expression = js_code_call_parse_expression(f_name);
   let arguments2 = js_call_arguments_get(expression);
-  list_add_multiple(arguments2, [
-    object_copy(b),
-    key_expression,
-    object_copy(value),
-  ]);
+  let copy = object_copy(b);
+  let copy2 = object_copy(value);
+  list_add_multiple(arguments2, [copy, key_expression, copy2]);
   object_replace(node, expression);
 }
