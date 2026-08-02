61318414f35cc87fd666bc8d994de02730b01636
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 22:36:58 +0800

    function_auto_checked localhost_is

--- js/localhost_is.mjs
+++ js/localhost_is.mjs
@@ -1,7 +1,8 @@
+import { equal } from "./equal.mjs";
 export function localhost_is() {
   let li =
-    location.hostname === "localhost" ||
-    location.hostname === "127.0.0.1" ||
-    location.hostname === "::1";
+    equal(location.hostname, "localhost") ||
+    equal(location.hostname, "127.0.0.1") ||
+    equal(location.hostname, "::1");
   return li;
 }
