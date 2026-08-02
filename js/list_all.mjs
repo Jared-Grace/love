2f872cf0d92249f53fd1028255fa3e2fba083350
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:22:56 +0800

    function_fold list_filter_size list_all

--- js/list_all.mjs
+++ js/list_all.mjs
@@ -1,9 +1,8 @@
+import { list_filter_size } from "./list_filter_size.mjs";
 import { equal } from "./equal.mjs";
-import { list_filter } from "./list_filter.mjs";
 import { list_size } from "./list_size.mjs";
 export function list_all(list, lambda$item) {
-  let list2 = list_filter(list, lambda$item);
-  let left = list_size(list2);
+  let left = list_filter_size(list, lambda$item);
   let right = list_size(list);
   let a = equal(left, right);
   return a;
