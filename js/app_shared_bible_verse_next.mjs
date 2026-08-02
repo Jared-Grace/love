c0f4f020336fa5b2af12299eb7d6e168e836e918
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:40:42 +0800

    function_rename app_bible_verse_next app_shared_bible_verse_next

--- js/app_shared_bible_verse_next.mjs
+++ js/app_shared_bible_verse_next.mjs
@@ -1,7 +1,7 @@
 import { app_shared_bible_verse_change } from "./app_shared_bible_verse_change.mjs";
 import { app_shared_bible_chapter_next } from "./app_shared_bible_chapter_next.mjs";
 import { list_next_try } from "./list_next_try.mjs";
-export async function app_bible_verse_next(
+export async function app_shared_bible_verse_next(
   context,
   chapter_code,
   verse_current,
