834b18f8fa28dbb7fb13bc82427e374d763cb94f
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:44:43 +0800

    ai

--- js/app_code_lesson_last_is.mjs
+++ js/app_code_lesson_last_is.mjs
@@ -3,7 +3,7 @@ import { app_code_lessons } from "./app_code_lessons.mjs";
 import { app_code_lesson_index_by_id } from "./app_code_lesson_index_by_id.mjs";
 export function app_code_lesson_last_is(context, lesson_id) {
   let lessons = app_code_lessons();
-  let index = app_code_lesson_index_by_id(context, lesson_id);
+  let index = app_code_lesson_index_by_id(lesson_id);
   let li = list_index_last_is(lessons, index);
   return li;
 }
