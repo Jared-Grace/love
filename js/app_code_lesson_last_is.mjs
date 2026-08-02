8cd06f0a27e5a6eefd4051c5604c2dbeceb438e5
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:45:32 +0800

    function_parameter_unread_remove app_code_lesson_last_is context

--- js/app_code_lesson_last_is.mjs
+++ js/app_code_lesson_last_is.mjs
@@ -1,7 +1,7 @@
 import { list_index_last_is } from "./list_index_last_is.mjs";
 import { app_code_lessons } from "./app_code_lessons.mjs";
 import { app_code_lesson_index_by_id } from "./app_code_lesson_index_by_id.mjs";
-export function app_code_lesson_last_is(context, lesson_id) {
+export function app_code_lesson_last_is(lesson_id) {
   let lessons = app_code_lessons();
   let index = app_code_lesson_index_by_id(lesson_id);
   let li = list_index_last_is(lessons, index);
