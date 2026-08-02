b3c21cfa92ebb3e67bc10d40e4838bba19f2e30b
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 21:08:41 +0800

    ai

--- js/g_game_prefix.mjs
+++ js/g_game_prefix.mjs
@@ -1,7 +1,7 @@
 import { g_folder_img } from "./g_folder_img.mjs";
-import { app_g_path_prefix } from "./app_g_path_prefix.mjs";
+import { g_path_prefix } from "./g_path_prefix.mjs";
 export function g_game_prefix() {
-  let path_prefix = app_g_path_prefix();
+  let path_prefix = g_path_prefix();
   let game_prefix = g_folder_img(path_prefix);
   return game_prefix;
 }
