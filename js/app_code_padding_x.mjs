11c2695a7be01875f7c5566716b2f4d8c382cbb2
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 16:42:21 +0800

    ai

--- js/app_code_padding_x.mjs
+++ js/app_code_padding_x.mjs
@@ -1,5 +1,6 @@
-import { app_shared_spaced_gap } from "./app_shared_spaced_gap.mjs";
+import { app_shared_content_edge_gap } from "./app_shared_content_edge_gap.mjs";
 import { html_style_padding_x } from "./html_style_padding_x.mjs";
 export function app_code_padding_x(component) {
-  html_style_padding_x(component, app_shared_spaced_gap());
+  let value = app_shared_content_edge_gap();
+  html_style_padding_x(component, value);
 }
