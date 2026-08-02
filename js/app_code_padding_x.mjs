6afd82997f23c6f52e78e40af8811671ce88b2ef
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 17:07:47 +0800

    function_replace app_code_padding_x html_page_padding_x

--- js/app_code_padding_x.mjs
+++
@@ -1,6 +0,0 @@
-import { app_shared_content_edge_gap } from "./app_shared_content_edge_gap.mjs";
-import { html_style_padding_x } from "./html_style_padding_x.mjs";
-export function app_code_padding_x(component) {
-  let value = app_shared_content_edge_gap();
-  html_style_padding_x(component, value);
-}
