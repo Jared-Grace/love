37599659f0a411b56719a56a98a4097ad705e420
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 14:35:23 +0800

    file_math_calls_repair js/path_extension.mjs

--- js/path_extension.mjs
+++ js/path_extension.mjs
@@ -1,10 +1,11 @@
+import { math_max } from "./math_max.mjs";
 import { equal } from "./equal.mjs";
 import { less_than } from "./less_than.mjs";
 export function path_extension(path) {
   let lastDot = path.lastIndexOf(".");
   let v = path.lastIndexOf("/");
   let v2 = path.lastIndexOf("\\");
-  let lastSlash = Math.max(v, v2);
+  let lastSlash = math_max(v, v2);
   if (equal(lastDot, -1) || less_than(lastDot, lastSlash)) {
     let r = "";
     return r;
