e1a7d467416f7dfd9eee48b9ac9a7aca971f9c36
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 13:04:16 +0800

    ai

--- js/js_call_arguments_same_names_set.mjs
+++ js/js_call_arguments_same_names_set.mjs
@@ -1,3 +1,4 @@
+import { js_function_declaration_params_names } from "./js_function_declaration_params_names.mjs";
 import { property_get } from "./property_get.mjs";
 import { function_parse_declaration } from "./function_parse_declaration.mjs";
 import { js_call_callee_name_try } from "./js_call_callee_name_try.mjs";
@@ -9,4 +10,5 @@ export async function js_call_arguments_same_names_set(ast, selects) {
   let f_name = js_call_callee_name_try(call);
   let d = await function_parse_declaration(f_name);
   let declaration = property_get(d, "declaration");
+  let params_names = js_function_declaration_params_names(declaration2);
 }
