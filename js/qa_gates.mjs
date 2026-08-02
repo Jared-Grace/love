4ca607217d35b1f8bee3487b2df5964eee349e72
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:35:29 +0800

    ai

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { functions_native_callback_gate_run } from "./functions_native_callback_gate_run.mjs";
 import { functions_parameters_unread_computed_gate_run } from "./functions_parameters_unread_computed_gate_run.mjs";
 import { cases_checked_gate_run_generic_gate_run } from "./cases_checked_gate_run_generic_gate_run.mjs";
 import { baseline_growth_guarded_gate_run } from "./baseline_growth_guarded_gate_run.mjs";
