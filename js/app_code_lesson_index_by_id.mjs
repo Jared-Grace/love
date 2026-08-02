d9aba505ffc13e720822e0871ecac575c986a2db
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:44:43 +0800

    ai

--- js/app_code_lesson_index_by_id.mjs
+++ js/app_code_lesson_index_by_id.mjs
@@ -1,7 +1,7 @@
 import { list_index_of } from "./list_index_of.mjs";
 import { app_code_lesson_find_by_id } from "./app_code_lesson_find_by_id.mjs";
 import { app_code_lessons } from "./app_code_lessons.mjs";
-export function app_code_lesson_index_by_id(context, lesson_id) {
+export function app_code_lesson_index_by_id(lesson_id) {
   let lessons = app_code_lessons();
   let lesson = app_code_lesson_find_by_id(lessons, lesson_id);
   let index = list_index_of(lessons, lesson);
