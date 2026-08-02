f29b34aadf8dd9a3f85bb467d95104c85989a0e8
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:55:47 +0800

    function_auto_checked storage_local_enabled

--- js/storage_local_enabled.mjs
+++ js/storage_local_enabled.mjs
@@ -1,8 +1,10 @@
+import { equal } from "./equal.mjs";
 import { global_function_get } from "./global_function_get.mjs";
 import { global_function_exists } from "./global_function_exists.mjs";
 import { not } from "./not.mjs";
 export function storage_local_enabled() {
   let set = global_function_exists(storage_local_enabled);
-  let enabled = not(set) || global_function_get(storage_local_enabled) === true;
+  let left = global_function_get(storage_local_enabled);
+  let enabled = not(set) || equal(left, true);
   return enabled;
 }
