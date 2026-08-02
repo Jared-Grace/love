27a77ed7e7128e785258b431219c8dcec44e8c6e
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:40:31 +0800

    function_rename app_bible_screen_content app_shared_bible_screen_content

--- js/app_shared_bible_books.mjs
+++ js/app_shared_bible_books.mjs
@@ -1,5 +1,5 @@
 import { app_shared_bible_chapter_hash_get } from "./app_shared_bible_chapter_hash_get.mjs";
-import { app_bible_screen_content } from "./app_bible_screen_content.mjs";
+import { app_shared_bible_screen_content } from "./app_shared_bible_screen_content.mjs";
 import { html_hash_object_get } from "./html_hash_object_get.mjs";
 import { ebible_chapter_code_to_book } from "./ebible_chapter_code_to_book.mjs";
 import { ebible_version_books_browser } from "./ebible_version_books_browser.mjs";
@@ -16,7 +16,7 @@ import { html_value_get } from "./html_value_get.mjs";
 import { html_on_input } from "./html_on_input.mjs";
 import { app_shared_bible_books_render } from "./app_shared_bible_books_render.mjs";
 export async function app_shared_bible_books(context) {
-  let opened = await app_bible_screen_content(context);
+  let opened = await app_shared_bible_screen_content(context);
   let content = property_get(opened, "content");
   let e = ebible_folder_english();
   let books = await ebible_version_books_browser(e);
