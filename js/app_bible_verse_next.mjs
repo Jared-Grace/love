e487a040bbfc6ede247a8daee596036e6da8ea21
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:39:42 +0800

    function_rename app_bible_chapter_next app_shared_bible_chapter_next

--- js/app_bible_verse_next.mjs
+++ js/app_bible_verse_next.mjs
@@ -1,5 +1,5 @@
 import { app_bible_verse_change } from "./app_bible_verse_change.mjs";
-import { app_bible_chapter_next } from "./app_bible_chapter_next.mjs";
+import { app_shared_bible_chapter_next } from "./app_shared_bible_chapter_next.mjs";
 import { list_next_try } from "./list_next_try.mjs";
 export async function app_bible_verse_next(
   context,
@@ -7,7 +7,7 @@ export async function app_bible_verse_next(
   verse_current,
 ) {
   let verse_get = list_next_try;
-  let chapter_change = app_bible_chapter_next;
+  let chapter_change = app_shared_bible_chapter_next;
   await app_bible_verse_change(
     context,
     chapter_code,
--- js/app_shared_bible_chapter_next.mjs
+++ js/app_shared_bible_chapter_next.mjs
@@ -1,7 +1,7 @@
 import { list_first } from "./list_first.mjs";
 import { list_next_wrap } from "./list_next_wrap.mjs";
 import { app_shared_bible_chapter_change } from "./app_shared_bible_chapter_change.mjs";
-export async function app_bible_chapter_next(context, chapter_code) {
+export async function app_shared_bible_chapter_next(context, chapter_code) {
   await app_shared_bible_chapter_change(
     context,
     chapter_code,
