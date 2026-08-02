baf56c39b050f865cb927c2881880bee1df5677a
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 22:10:16 +0800

    ai

--- js/function_parse.mjs
+++ js/function_parse.mjs
@@ -1,9 +1,7 @@
-import { function_name_to_path_search } from "./function_name_to_path_search.mjs";
-import { property_get } from "./property_get.mjs";
+import { function_name_to_path_found } from "./function_name_to_path_found.mjs";
 import { file_js_parse } from "./file_js_parse.mjs";
 export async function function_parse(f_name) {
-  let v = await function_name_to_path_search(f_name);
-  let f_path = property_get(v, "f_path");
+  let f_path = await function_name_to_path_found(f_name);
   let parsed = await file_js_parse(f_path);
   return parsed;
 }
