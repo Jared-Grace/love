1b91877a65f6aebeb307107cfbf7b6a38e1c22fb
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 11:46:27 +0800

    ai

--- js/marker_next_await.mjs
+++ js/marker_next_await.mjs
@@ -1,9 +1,9 @@
+import { marker_next_node } from "./marker_next_node.mjs";
 import { js_await } from "./js_await.mjs";
 import { property_get } from "./property_get.mjs";
 import { property_set } from "./property_set.mjs";
 import { js_node_type_is_assert_json } from "./js_node_type_is_assert_json.mjs";
 import { list_adder_async } from "./list_adder_async.mjs";
-import { marker_next_get } from "./marker_next_get.mjs";
 import { function_transform_marker } from "./function_transform_marker.mjs";
 import { function_current_get } from "./function_current_get.mjs";
 import { js_unparse } from "./js_unparse.mjs";
@@ -12,8 +12,7 @@ export async function marker_next_await() {
   async function lambda2(la) {
     await function_transform_marker(f_name, lambda);
     async function lambda(a) {
-      let v = marker_next_get(a);
-      let next = property_get(v, "next");
+      let next = marker_next_node(a);
       js_node_type_is_assert_json(next, "ExpressionStatement", {
         hint: "the marked statement should be an expression statement so its expression can be awaited",
       });
