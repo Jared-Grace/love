31c166dacb65b95f7243ff759d01b528d6f90299
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:38:25 +0800

    function_literal_route_any app_code_lesson_expression_string_equality app_code_category_expressions

--- js/app_code_lesson_expression_string_equality.mjs
+++ js/app_code_lesson_expression_string_equality.mjs
@@ -1,3 +1,4 @@
+import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 import { app_code_label_value_backwards } from "./app_code_label_value_backwards.mjs";
 import { app_code_label_value } from "./app_code_label_value.mjs";
 import { app_code_verse_words_clean_unique } from "./app_code_verse_words_clean_unique.mjs";
@@ -102,7 +103,7 @@ export function app_code_lesson_expression_string_equality() {
     let rights = ["string", "equality"];
     let built = app_code_lesson_name_id_generic(
       rights,
-      "expressions",
+      app_code_category_expressions(),
       title_get,
     );
     return built;
