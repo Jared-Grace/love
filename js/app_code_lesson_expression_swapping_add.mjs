1cfb3a49535481f689eee4aa7068681d6b38f1eb
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:38:20 +0800

    function_literal_route_any app_code_lesson_expression_swapping_add app_code_category_expressions

--- js/app_code_lesson_expression_swapping_add.mjs
+++ js/app_code_lesson_expression_swapping_add.mjs
@@ -1,3 +1,4 @@
+import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 import { app_code_dot_rectangle } from "./app_code_dot_rectangle.mjs";
 import { html_cycle_code } from "./html_cycle_code.mjs";
 import { app_code_lesson_swapping_generic } from "./app_code_lesson_swapping_generic.mjs";
@@ -28,7 +29,7 @@ export function app_code_lesson_expression_swapping_add() {
     let rights = ["swapping plus times"];
     let built = app_code_lesson_name_id_generic(
       rights,
-      "expressions",
+      app_code_category_expressions(),
       title_get,
     );
     return built;
