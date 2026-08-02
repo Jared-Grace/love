2f6c7280d0f22e72b94ed87c6c90a5aeec38e07f
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:55:25 +0800

    function_auto_checked text_words_quoted

--- js/text_words_quoted.mjs
+++ js/text_words_quoted.mjs
@@ -1,9 +1,10 @@
+import { equal } from "./equal.mjs";
 export function text_words_quoted(command) {
   let re = /"([^"]*)"|(\S+)/g;
   let out = [];
   let m = re.exec(command);
   while (m) {
-    out.push(m[1] === undefined ? m[2] : m[1]);
+    out.push(equal(m[1], undefined) ? m[2] : m[1]);
     m = re.exec(command);
   }
   return out;
