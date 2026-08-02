0efa9a5a9c51858c1b5dad0a55799e4bc04ea44b
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:14:43 +0800

    function_literal_route_any ebible_languages_bible_folder_exists_not_assert bible_folder_key

--- js/ebible_languages_bible_folder_exists_not_assert.mjs
+++ js/ebible_languages_bible_folder_exists_not_assert.mjs
@@ -1,10 +1,11 @@
+import { bible_folder_key } from "./bible_folder_key.mjs";
 import { false_is_assert_json } from "./false_is_assert_json.mjs";
 import { list_find_property_exists } from "./list_find_property_exists.mjs";
 import { ebible_languages } from "./ebible_languages.mjs";
 export function ebible_languages_bible_folder_exists_not_assert(bible_folder) {
   let exists = list_find_property_exists(
     ebible_languages(),
-    "bible_folder",
+    bible_folder_key(),
     bible_folder,
   );
   false_is_assert_json(exists, {
