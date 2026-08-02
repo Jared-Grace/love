8b9bf1d23311bb4a1a8895b832c85807d9ba1658
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:23:59 +0800

    ai

--- js/throws_assert_json.mjs
+++ js/throws_assert_json.mjs
@@ -3,11 +3,6 @@ import { lambda_throws } from "./lambda_throws.mjs";
 import { assert_json } from "./assert_json.mjs";
 export function throws_assert_json(lambda, json) {
   let r = lambda_throws(lambda);
-  let result = property_get(r, "result");
-  let throws = property_get(r, "throws");
-  assert_json(throws, {
-    result,
-    json,
-  });
+  let result = throws_verdict_assert_json(r, json);
   return result;
 }
