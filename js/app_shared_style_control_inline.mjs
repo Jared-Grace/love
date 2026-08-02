307a83428b19137fc49d45cd6fca43b634d37c32
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 17:11:35 +0800

    function_literal_route app_shared_style_control_inline app_shared_spaced_neighbor_gap

--- js/app_shared_style_control_inline.mjs
+++ js/app_shared_style_control_inline.mjs
@@ -1,3 +1,4 @@
+import { app_shared_spaced_neighbor_gap } from "./app_shared_spaced_neighbor_gap.mjs";
 import { app_shared_border_radius_large } from "./app_shared_border_radius_large.mjs";
 import { html_style_assign } from "./html_style_assign.mjs";
 export function app_shared_style_control_inline(component) {
@@ -7,6 +8,6 @@ export function app_shared_style_control_inline(component) {
     padding: "0.3em",
     display: "inline-block",
     "border-width": "0px",
-    margin: "0.15em",
+    margin: app_shared_spaced_neighbor_gap(),
   });
 }
