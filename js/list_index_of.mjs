60ce88dccb8dba0f82f1000e17ddec5ddb3fecd8
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:56:23 +0800

    function_auto_checked list_index_of

--- js/list_index_of.mjs
+++ js/list_index_of.mjs
@@ -1,7 +1,8 @@
+import { less_than_equal } from "./less_than_equal.mjs";
 import { error_json } from "./error_json.mjs";
 export function list_index_of(list, item) {
   let index = list.indexOf(item);
-  if (index <= -1) {
+  if (less_than_equal(index, -1)) {
     error_json({
       list,
       item,
