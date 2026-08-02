59608387ac45e6be058b44c74960f3bb3a997002
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:34:50 +0800

    ai

--- js/probe_fold_helper.mjs
+++ js/probe_fold_helper.mjs
@@ -1,3 +1,9 @@
+import { add_1 } from "./add_1.mjs";
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
+import { text_first_upper_to } from "./text_first_upper_to.mjs";
+import { html_div_text_centered } from "./html_div_text_centered.mjs";
+import { html_progress_caption_font_size } from "./html_progress_caption_font_size.mjs";
+import { html_style_font_size } from "./html_style_font_size.mjs";
 export function probe_fold_helper(root, count, total, name) {
   let a = add_1(count);
   let s = text_combine_multiple([name, " ", a, " out of ", total]);
