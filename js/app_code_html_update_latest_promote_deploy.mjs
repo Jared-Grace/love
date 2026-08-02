641f81e29c1299c5481fe7d7cba5cb6b48d725a8
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:10:57 +0800

    ai

--- js/app_code_html_update_latest_promote_deploy.mjs
+++ js/app_code_html_update_latest_promote_deploy.mjs
@@ -1,7 +1,7 @@
 import { app_code } from "./app_code.mjs";
 import { app_shared_name_prefix_without_fn } from "./app_shared_name_prefix_without_fn.mjs";
 import { html_update_latest_promote_deploy } from "./html_update_latest_promote_deploy.mjs";
-export async function html_update_latest_promote_deploy_code() {
+export async function app_code_html_update_latest_promote_deploy() {
   let without = app_shared_name_prefix_without_fn(app_code);
   let r = await html_update_latest_promote_deploy(without);
   return r;
