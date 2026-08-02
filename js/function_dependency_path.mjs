d420344a5d1f2c53a61fc6685788afd08874cf2e
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 22:39:23 +0800

    function_auto_checked function_dependency_path

--- js/function_dependency_path.mjs
+++ js/function_dependency_path.mjs
@@ -1,3 +1,4 @@
+import { equal } from "./equal.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_size_less_than_other } from "./list_size_less_than_other.mjs";
 import { function_name_unalias } from "./function_name_unalias.mjs";
@@ -19,9 +20,9 @@ export async function function_dependency_path(f_name_from, f_name_to) {
   await each_async([from, to], lambda2);
   function lambda(v) {
     let node = property_get(v, "node");
-    if (node === to) {
+    if (equal(node, to)) {
       let stack = property_get(v, "stack");
-      if (result === null || list_size_less_than_other(stack, result)) {
+      if (equal(result, null) || list_size_less_than_other(stack, result)) {
         result = stack;
       }
     }
