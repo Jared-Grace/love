dd271e3c96db74ecd59267730d63c8ab016f1739
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 17:14:35 +0800

    function_rename app_shared_new_fn_main apps_new_fn_main

--- js/app_shared_new_step_2_next.mjs
+++ js/app_shared_new_step_2_next.mjs
@@ -1,10 +1,10 @@
 import { apps_new_fn_latest } from "./apps_new_fn_latest.mjs";
 import { app_shared_latest_update } from "./app_shared_latest_update.mjs";
-import { app_shared_new_fn_main } from "./app_shared_new_fn_main.mjs";
+import { apps_new_fn_main } from "./apps_new_fn_main.mjs";
 import { app_shared_production_update } from "./app_shared_production_update.mjs";
 import { html_new } from "./html_new.mjs";
 export async function app_shared_new_step_2_next(name) {
-  await app_shared_new_fn_main(name);
+  await apps_new_fn_main(name);
   await apps_new_fn_latest(name);
   await app_shared_production_update(name);
   await app_shared_latest_update(name);
