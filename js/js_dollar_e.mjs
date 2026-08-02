e9a03271fe71ab5809d60a91afed0b615fee534d
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:07:54 +0800

    function_auto_checked js_dollar_e

--- js/js_dollar_e.mjs
+++ js/js_dollar_e.mjs
@@ -1,3 +1,4 @@
+import { not_equal } from "./not_equal.mjs";
 import { list_remove } from "./list_remove.mjs";
 import { property_set } from "./property_set.mjs";
 import { js_statement_block_new } from "./js_statement_block_new.mjs";
@@ -6,19 +7,19 @@ import { list_previous } from "./list_previous.mjs";
 export function js_dollar_e({
   remaining,
   node,
-  stack_1,
+  stack_,
   stack_2,
   stack_3,
   ast,
   afters,
 }) {
   let previous = null;
-  let alternate = list_previous(stack_2, stack_1);
-  while (alternate !== null) {
+  let alternate = list_previous(stack_2, stack_);
+  while (not_equal(alternate, null)) {
     previous = alternate;
     alternate = property_get(alternate, "alternate");
   }
   let b = js_statement_block_new([]);
   property_set(previous, "alternate", b);
-  list_remove(stack_2, stack_1);
+  list_remove(stack_2, stack_);
 }
