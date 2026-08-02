3581a8314453a8f1b22393c25b56691724b5b1d1
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 15:41:36 +0800

    function_param_plain_mark firebase_prod_asset_url file_name

--- js/firebase_prod_asset_url.mjs
+++ js/firebase_prod_asset_url.mjs
@@ -1,6 +1,7 @@
 import { firebase_project_url_jg } from "./firebase_project_url_jg.mjs";
 import { list_join_slash_forward } from "./list_join_slash_forward.mjs";
 export function firebase_prod_asset_url(file_name) {
+  "$plain file_name";
   let base = firebase_project_url_jg();
   let combined = list_join_slash_forward([base, file_name]);
   return combined;
