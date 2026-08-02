59436192e855c5db8aa9a208be781ec4c16346f6
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 16:49:30 +0800

    function_literal_route app_code_example_answer_label app_shared_content_edge_gap

--- js/app_code_example_answer_label.mjs
+++ js/app_code_example_answer_label.mjs
@@ -1,6 +1,7 @@
+import { app_shared_content_edge_gap } from "./app_shared_content_edge_gap.mjs";
 import { html_style_margin_top } from "./html_style_margin_top.mjs";
 import { app_code_label_text } from "./app_code_label_text.mjs";
 export function app_code_example_answer_label(a_container, quiz_label) {
   let d = app_code_label_text(a_container, quiz_label);
-  html_style_margin_top(d, "0.4em");
+  html_style_margin_top(d, app_shared_content_edge_gap());
 }
