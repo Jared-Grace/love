6b57d656f8d2f43b92e632ebd321cbbd4e273830
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:28:09 +0800

    function_literal_route_any ebible_verse verse_number_key

--- js/ebible_verse.mjs
+++ js/ebible_verse.mjs
@@ -1,7 +1,8 @@
+import { verse_number_key } from "./verse_number_key.mjs";
 import { list_find_property } from "./list_find_property.mjs";
 import { ebible_verses } from "./ebible_verses.mjs";
 export async function ebible_verse(bible_folder, chapter_code, verse_number) {
   let verses = await ebible_verses(bible_folder, chapter_code);
-  let result = list_find_property(verses, "verse_number", verse_number);
+  let result = list_find_property(verses, verse_number_key(), verse_number);
   return result;
 }
