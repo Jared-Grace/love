53a68ca76e642463ca71b4b54ce32cd10bcc31c3
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:10:30 +0800

    function_auto_checked g_sermon_write_coverage

--- js/g_sermon_write_coverage.mjs
+++ js/g_sermon_write_coverage.mjs
@@ -1,3 +1,4 @@
+import { equal } from "./equal.mjs";
 import { list_find_item_property } from "./list_find_item_property.mjs";
 import { property_get } from "./property_get.mjs";
 import { local_function_path_json } from "./local_function_path_json.mjs";
@@ -10,7 +11,9 @@ import { g_sermon_coverage } from "./g_sermon_coverage.mjs";
 export async function g_sermon_write_coverage(chapter_code, verse_numbers) {
   let key = list_join_comma(verse_numbers);
   function matches(passage) {
-    return g_sermon_passage_verses_key(passage) === key;
+    let left = g_sermon_passage_verses_key(passage);
+    let eq = equal(left, key);
+    return eq;
   }
   let generated_passages =
     await g_sermon_generate_chapter_passages_get(chapter_code);
@@ -19,5 +22,6 @@ export async function g_sermon_write_coverage(chapter_code, verse_numbers) {
   let write_chapter = await file_read_json(write_path);
   let write_passages = property_get(write_chapter, "passages");
   let lines = list_find_item_property(write_passages, matches, "lines");
-  return g_sermon_coverage(verse_text, lines);
+  let result = g_sermon_coverage(verse_text, lines);
+  return result;
 }
