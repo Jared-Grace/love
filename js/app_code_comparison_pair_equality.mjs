f1b68f5742c4707439047f8da630921d4ab5442b
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:14:09 +0800

    function_auto_checked app_code_comparison_pair_equality

--- js/app_code_comparison_pair_equality.mjs
+++ js/app_code_comparison_pair_equality.mjs
@@ -1,3 +1,4 @@
+import { subtract } from "./subtract.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { app_code_lesson_operators_value_max } from "./app_code_lesson_operators_value_max.mjs";
 export function app_code_comparison_pair_equality() {
@@ -12,7 +13,8 @@ export function app_code_comparison_pair_equality() {
       };
       return equal_pair;
     }
-    let left = integer_random(1, max - 1);
+    let max2 = subtract(max, 1);
+    let left = integer_random(1, max2);
     let right = integer_random(left + 1, max);
     let different_pair = {
       left,
