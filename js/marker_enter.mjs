a1a942c5af49c940fa8e9d834b30083dc8eb5a09
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 22:38:40 +0800

    function_auto_checked marker_enter

--- js/marker_enter.mjs
+++ js/marker_enter.mjs
@@ -1,3 +1,4 @@
+import { equal } from "./equal.mjs";
 import { js_statement_if_consequent_get } from "./js_statement_if_consequent_get.mjs";
 import { js_types_function_includes_node } from "./js_types_function_includes_node.mjs";
 import { list_get } from "./list_get.mjs";
@@ -12,18 +13,18 @@ import { function_transform_marker_current } from "./function_transform_marker_c
 export async function marker_enter() {
   async function lambda(a) {
     let v2 = marker_next_index(a);
-    let stack_2 = property_get(v2, "stack_2");
+    let stack_ = property_get(v2, "stack_2");
     let stack_1 = property_get(v2, "stack_1");
     let index = property_get(v2, "index");
     let body = null;
-    while (body === null) {
-      let next = list_get(stack_2, index);
+    while (equal(body, null)) {
+      let next = list_get(stack_, index);
       var i = js_types_function_includes_node(next);
       if (i) {
         body = js_function_declaration_to_block_body(next);
       } else {
         let nt = js_node_type(next);
-        if (nt === "IfStatement") {
+        if (equal(nt, "IfStatement")) {
           let consequent = js_statement_if_consequent_get(next);
           body = property_get(consequent, "body");
         }
@@ -33,7 +34,7 @@ export async function marker_enter() {
     let nn = null_not_is(body);
     if (nn) {
       list_add_first(body, stack_1);
-      list_remove(stack_2, stack_1);
+      list_remove(stack_, stack_1);
     }
   }
   let v = await function_transform_marker_current(lambda);
