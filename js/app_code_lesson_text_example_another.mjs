7bcd3cada44a0cab3dfe923c81d1828266f5ecb8
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:14:02 +0800

    function_auto_checked app_code_lesson_text_example_another

--- js/app_code_lesson_text_example_another.mjs
+++ js/app_code_lesson_text_example_another.mjs
@@ -1,13 +1,15 @@
+import { greater_than_equal } from "./greater_than_equal.mjs";
 import { word_plural } from "./word_plural.mjs";
 import { property_get } from "./property_get.mjs";
 import { text_combine } from "./text_combine.mjs";
 export function app_code_lesson_text_example_another(lesson) {
   let example_count = property_get(lesson, "example_count");
-  let plural = example_count >= 2;
+  let plural = greater_than_equal(example_count, 2);
   let root_word = "example";
   let another = null;
   if (plural) {
-    another = text_combine("some more ", word_plural(root_word));
+    let right = word_plural(root_word);
+    another = text_combine("some more ", right);
   } else {
     another = text_combine("another ", root_word);
   }
