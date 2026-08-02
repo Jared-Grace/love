a5976e304e55d3a4d2d1b17c370a789d32a13722
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 05:56:33 +0800

    function_auto_checked js_node_type_is

--- js/js_node_type_is.mjs
+++ js/js_node_type_is.mjs
@@ -1,6 +1,8 @@
+import { equal } from "./equal.mjs";
 import { js_node_is } from "./js_node_is.mjs";
 import { js_node_type } from "./js_node_type.mjs";
 export function js_node_type_is(node, type) {
-  let type_is = js_node_is(node) && js_node_type(node) === type;
+  let left = js_node_type(node);
+  let type_is = js_node_is(node) && equal(left, type);
   return type_is;
 }
