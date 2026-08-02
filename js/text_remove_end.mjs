ac74340ba6ca89eff53d5d1e9848f6260bb1cd63
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 14:35:23 +0800

    file_math_calls_repair js/text_remove_end.mjs

--- js/text_remove_end.mjs
+++ js/text_remove_end.mjs
@@ -1,6 +1,7 @@
+import { math_max } from "./math_max.mjs";
 import { subtract } from "./subtract.mjs";
 export function text_remove_end(t, count) {
-  let v = Math.max(0, subtract(t.length, count));
+  let v = math_max(0, subtract(t.length, count));
   let remaining = t.slice(0, v);
   return remaining;
 }
