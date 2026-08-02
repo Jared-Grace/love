e79c6806bc96e0bc3bf9c7d18e0fa3f525a61b43
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:00:32 +0800

    ai

--- js/app_g_player_coordinates_new_get.mjs
+++ js/app_g_player_coordinates_new_get.mjs
@@ -1,3 +1,5 @@
+import { app_g_game_save_get } from "./app_g_game_save_get.mjs";
+import { app_g_player_get } from "./app_g_player_get.mjs";
 import { g_coordinates_clicked_adjascent_nearest_player } from "./g_coordinates_clicked_adjascent_nearest_player.mjs";
 export async function app_g_player_coordinates_new_get(
   npc_clicked,
@@ -5,7 +7,11 @@ export async function app_g_player_coordinates_new_get(
 ) {
   let coordinates_move_to = clicked_coordinates;
   if (npc_clicked) {
-    coordinates_move_to = await g_coordinates_clicked_adjascent_nearest_player(
+    let g = await app_g_game_save_get();
+    let player = await app_g_player_get();
+    coordinates_move_to = g_coordinates_clicked_adjascent_nearest_player(
+      g,
+      player,
       clicked_coordinates,
     );
   }
