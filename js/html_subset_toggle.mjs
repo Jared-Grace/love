7f503066148691f4831af79f9546aadc2686524f
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 15:28:22 +0800

    ai

--- js/html_subset_toggle.mjs
+++ js/html_subset_toggle.mjs
@@ -1,7 +1,6 @@
+import { list_find_property_not_null_is } from "./list_find_property_not_null_is.mjs";
 import { list_map } from "./list_map.mjs";
 import { property_get } from "./property_get.mjs";
-import { list_find_property_or_null } from "./list_find_property_or_null.mjs";
-import { null_not_is } from "./null_not_is.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_remove_property } from "./list_remove_property.mjs";
 import { html_button_toggle } from "./html_button_toggle.mjs";
@@ -16,8 +15,7 @@ export function html_subset_toggle(
   function lambda(option) {
     let name = property_get(option, name_property);
     let key = property_get(option, key_property);
-    let found = list_find_property_or_null(chosen, key_property, key);
-    let on = null_not_is(found);
+    let on = list_find_property_not_null_is(chosen, key_property, key);
     function on_click() {
       if (on) {
         list_remove_property(chosen, key_property, key);
