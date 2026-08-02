93a90fd6c3a781c644688c145c9778a9198ed87f
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:20:11 +0800

    ai

--- js/app_bible_home_generic.mjs
+++ js/app_bible_home_generic.mjs
@@ -61,11 +61,7 @@ import { null_not_is } from "./null_not_is.mjs";
 import { catch_null_async } from "./catch_null_async.mjs";
 import { ebible_language_english } from "./ebible_language_english.mjs";
 import { list_multiple_is } from "./list_multiple_is.mjs";
-export async function app_shared_bible_home_generic(
-  context,
-  lambda$a,
-  bar_extra,
-) {
+export async function app_bible_home_generic(context, lambda$a, bar_extra) {
   let frame = app_shared_bar_content(context);
   let content = property_get(frame, "content");
   app_shared_content_column_pad(content);
