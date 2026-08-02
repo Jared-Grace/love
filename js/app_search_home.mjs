1904770afa93699cd2cb9b88b3afa63936f0297b
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 11:13:48 +0800

    function_shadowing_rename app_search_home query_hash query_hash_written

--- js/app_search_home.mjs
+++ js/app_search_home.mjs
@@ -78,8 +78,8 @@ export async function app_search_home(context) {
     property_set(context, "query", query);
     let key = app_search_query_hash_key();
     let plus = app_search_query_hash_word_gap();
-    let query_hash = text_replace_space_to(query, plus);
-    html_hash_object_property_set(key, query_hash);
+    let query_hash_written = text_replace_space_to(query, plus);
+    html_hash_object_property_set(key, query_hash_written);
     await app_search_results(context, div_results);
   }
   let property = app_search_query_hash_key();
