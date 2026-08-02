65f55e3f9664e7650f873f9c7a78080381f60bc0
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 15:10:48 +0800

    ai

--- js/js_html_style_assign_to_set_node.mjs
+++ js/js_html_style_assign_to_set_node.mjs
@@ -1,3 +1,4 @@
+import { js_code_call_parse_expression } from "./js_code_call_parse_expression.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { js_call_arguments_get } from "./js_call_arguments_get.mjs";
 import { list_size_2 } from "./list_size_2.mjs";
@@ -7,8 +8,6 @@ import { js_object_expression_property_single_try } from "./js_object_expression
 import { null_is } from "./null_is.mjs";
 import { js_property_value_get } from "./js_property_value_get.mjs";
 import { js_property_key_string_expression } from "./js_property_key_string_expression.mjs";
-import { js_code_call } from "./js_code_call.mjs";
-import { js_parse_expression } from "./js_parse_expression.mjs";
 import { object_copy } from "./object_copy.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { object_replace } from "./object_replace.mjs";
@@ -25,8 +24,7 @@ export function js_html_style_assign_to_set_node(node) {
   }
   let key_expression = js_property_key_string_expression(property);
   let value = js_property_value_get(property);
-  let code = js_code_call(fn_name("html_style_set"));
-  let expression = js_parse_expression(code);
+  let expression = js_code_call_parse_expression(fn_name("html_style_set"));
   let arguments2 = js_call_arguments_get(expression);
   list_add_multiple(arguments2, [
     object_copy(b),
