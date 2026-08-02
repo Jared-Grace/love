1a1a28add54f802690704f8eecd1706030ceb25c
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:01:50 +0800

    function_rename g_tutorials_each_remove_try app_g_tutorials_each_remove_try

--- js/app_g_tutorials_each_remove_try.mjs
+++ js/app_g_tutorials_each_remove_try.mjs
@@ -2,7 +2,7 @@ import { g_tutorials_each } from "../../love/js/g_tutorials_each.mjs";
 import { html_remove_if_not_null } from "../../love/js/html_remove_if_not_null.mjs";
 import { app_g } from "../../love/js/app_g.mjs";
 import { global_function_property_get } from "../../love/js/global_function_property_get.mjs";
-export function g_tutorials_each_remove_try() {
+export function app_g_tutorials_each_remove_try() {
   function lambda(tutorial) {
     let value = global_function_property_get(app_g, tutorial);
     html_remove_if_not_null(value);
