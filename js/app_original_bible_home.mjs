8459b9568fd0960dfbe07f1c13d851aa1060478e
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 20:30:08 +0800

    ai

--- js/app_original_bible_home.mjs
+++ js/app_original_bible_home.mjs
@@ -1,6 +1,12 @@
+import { app_g_bible } from "./app_g_bible.mjs";
 import { app_original_bible_gloss_generate_download } from "./app_original_bible_gloss_generate_download.mjs";
 import { app_shared_gloss_bible_home_generic } from "./app_shared_gloss_bible_home_generic.mjs";
 export async function app_original_bible_home(context) {
   let download = app_original_bible_gloss_generate_download;
-  await app_shared_gloss_bible_home_generic(context, download, false);
+  await app_shared_gloss_bible_home_generic(
+    context,
+    download,
+    false,
+    app_g_bible,
+  );
 }
