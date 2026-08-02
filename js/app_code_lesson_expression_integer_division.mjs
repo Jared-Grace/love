3ad3c6ab33d43efca548b9fdec53c9d60d615855
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:39:35 +0800

    function_literal_route_any app_code_lesson_expression_integer_division app_code_category_operators

--- js/app_code_lesson_expression_integer_division.mjs
+++ js/app_code_lesson_expression_integer_division.mjs
@@ -1,3 +1,4 @@
+import { app_code_category_operators } from "./app_code_category_operators.mjs";
 import { app_code_uneven_division_code } from "./app_code_uneven_division_code.mjs";
 import { app_code_lesson_expression_generic } from "./app_code_lesson_expression_generic.mjs";
 import { list_iterator_refillable } from "./list_iterator_refillable.mjs";
@@ -70,7 +71,11 @@ export function app_code_lesson_expression_integer_division() {
       return render;
     }
     let rights = ["integer division"];
-    let built = app_code_lesson_name_id_generic(rights, "operators", title_get);
+    let built = app_code_lesson_name_id_generic(
+      rights,
+      app_code_category_operators(),
+      title_get,
+    );
     return built;
   }
   function above(root) {
