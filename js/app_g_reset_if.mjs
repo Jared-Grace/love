56f46b28653b512477b65fd9ebd3d2d68115e3c0
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 06:13:57 +0800

    function_auto_checked app_g_reset_if

--- js/app_g_reset_if.mjs
+++ js/app_g_reset_if.mjs
@@ -1,9 +1,10 @@
+import { equal } from "./equal.mjs";
 import { html_hash_get } from "./html_hash_get.mjs";
 import { html_hash_set } from "./html_hash_set.mjs";
 import { app_g_game_initialize } from "./app_g_game_initialize.mjs";
 export async function app_g_reset_if() {
   let hash = html_hash_get();
-  if (hash === "#reset") {
+  if (equal(hash, "#reset")) {
     await app_g_game_initialize();
     html_hash_set("#");
   }
