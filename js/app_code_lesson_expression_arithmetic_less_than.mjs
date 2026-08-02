7d59c203931ecffc60983a6a80786029f73bd5c3
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 18:38:28 +0800

    function_literal_route_any app_code_lesson_expression_arithmetic_less_than app_code_category_expressions

--- js/app_code_lesson_expression_arithmetic_less_than.mjs
+++ js/app_code_lesson_expression_arithmetic_less_than.mjs
@@ -1,3 +1,4 @@
+import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 import { app_code_label_value_backwards } from "./app_code_label_value_backwards.mjs";
 import { app_code_label_value } from "./app_code_label_value.mjs";
 import { app_code_container_light_blue_div } from "./app_code_container_light_blue_div.mjs";
@@ -310,7 +311,7 @@ export function app_code_lesson_expression_arithmetic_less_than() {
     let rights = ["arithmetic less than"];
     let built = app_code_lesson_name_id_generic(
       rights,
-      "expressions",
+      app_code_category_expressions(),
       title_get,
     );
     return built;
