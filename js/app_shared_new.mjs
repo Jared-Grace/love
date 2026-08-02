eef3af1e5a691141bf6c25be87ba019c11371825
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 17:14:35 +0800

    function_rename app_shared_new_fn_main apps_new_fn_main

--- js/app_shared_new.mjs
+++ js/app_shared_new.mjs
@@ -1,7 +1,7 @@
 import { app_apps_all_main_fns_generate } from "./app_apps_all_main_fns_generate.mjs";
 import { app_shared_update } from "./app_shared_update.mjs";
 import { apps_new_fn_latest } from "./apps_new_fn_latest.mjs";
-import { app_shared_new_fn_main } from "./app_shared_new_fn_main.mjs";
+import { apps_new_fn_main } from "./apps_new_fn_main.mjs";
 import { text_and_empty_not_is_assert_json } from "./text_and_empty_not_is_assert_json.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_shared_new_fn } from "./app_shared_new_fn.mjs";
@@ -11,7 +11,7 @@ export async function app_shared_new(name) {
     hint: "the app name should be non-empty text — was it blank?",
   });
   await app_shared_new_fn(name);
-  await app_shared_new_fn_main(name);
+  await apps_new_fn_main(name);
   await apps_new_fn_latest(name);
   await app_shared_update(name);
   await app_apps_all_main_fns_generate();
