1db162913adeb71c52545f35549b6cd3c69ca542
JESUS rose to life <username@users.noreply.github.com>
2026-08-02 14:35:18 +0800

    file_math_calls_repair js/date_diff_ms.mjs

--- js/date_diff_ms.mjs
+++ js/date_diff_ms.mjs
@@ -1,6 +1,7 @@
+import { abs } from "./abs.mjs";
 import { subtract } from "./subtract.mjs";
 export function date_diff_ms(now, before) {
   let difference = subtract(now, before);
-  let v = Math.abs(difference);
+  let v = abs(difference);
   return v;
 }
