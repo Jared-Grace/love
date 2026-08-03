823f07f4bb15f060bb455b04d6fe957ee4b9aae2
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:01:40 +0800

    function_transform_single_auto js_text_combine_number_add js_stack_list_block_is

--- js/js_stack_list_block_is.mjs
+++ js/js_stack_list_block_is.mjs
@@ -1,15 +1,16 @@
+import { add } from "./add.mjs";
 import { js_block_statement_is } from "./js_block_statement_is.mjs";
 import { not } from "./not.mjs";
 import { list_is } from "./list_is.mjs";
 import { list_get_end } from "./list_get_end.mjs";
-import { text_combine } from "./text_combine.mjs";
 export function js_stack_list_block_is(stack, index_end) {
-  let stack_1 = list_get_end(stack, index_end);
-  let a = list_is(stack_1);
+  let stack_ = list_get_end(stack, index_end);
+  let a = list_is(stack_);
   if (not(a)) {
     return false;
   }
-  let stack_2 = list_get_end(stack, text_combine(index_end, 1));
+  let index_from_end = add(index_end, 1);
+  let stack_2 = list_get_end(stack, index_from_end);
   let a2 = js_block_statement_is(stack_2);
   if (not(a2)) {
     return false;
