e0dc30697723709b8126279b684516a6886be955
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:39:14 +0800

    function_declarations_unused_clear ebible_references_parse

--- js/ebible_references_parse.mjs
+++ js/ebible_references_parse.mjs
@@ -6,6 +6,6 @@ export async function ebible_references_parse(bible_folders, file_path) {
   let contents = await file_read(file_path);
   let split = text_split_newline(contents);
   let list = await ebible_references_parse_lines(bible_folders, split);
-  let joined = list_join_newline(list);
+  list_join_newline(list);
   return list;
 }
