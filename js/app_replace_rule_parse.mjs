204d9173cb0039605d9b6af33a2bad007dc92ad1
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:40:02 +0800

    function_declarations_unused_clear app_replace_rule_parse

--- js/app_replace_rule_parse.mjs
+++ js/app_replace_rule_parse.mjs
@@ -2,7 +2,7 @@ import { app_replace_rule_parse_left_right_only } from "./app_replace_rule_parse
 import { object_merge_set } from "./object_merge_set.mjs";
 export function app_replace_rule_parse(rule) {
   let p = app_replace_rule_parse_left_right_only(rule);
-  let to = object_merge_set(p, {
+  object_merge_set(p, {
     original: rule,
   });
   return p;
