6330832846ff0b2d44b9f56d92a00614343dc466
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:01:50 +0800

    function_transform_single_auto js_text_combine_number_add list_remove_last_multiple

--- js/list_remove_last_multiple.mjs
+++ js/list_remove_last_multiple.mjs
@@ -1,10 +1,11 @@
+import { add } from "./add.mjs";
 import { list_index_last } from "./list_index_last.mjs";
 import { list_remove_at_count } from "./list_remove_at_count.mjs";
-import { text_combine } from "./text_combine.mjs";
 import { subtract } from "./subtract.mjs";
 export function list_remove_last_multiple(list, count) {
   let index_last = list_index_last(list);
-  let start = text_combine(subtract(index_last, count), 1);
+  let left = subtract(index_last, count);
+  let start = add(left, 1);
   let e = list_remove_at_count(list, start, count);
   return e;
 }
