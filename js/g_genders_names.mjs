b3621fc2c14065acf4a3745e1f17ff345fcb871c
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:44:09 +0800

    r 1 g_sexes,genders_names

--- js/g_genders_names.mjs
+++ js/g_genders_names.mjs
@@ -1,6 +1,6 @@
 import { list_map_property } from "./list_map_property.mjs";
 import { g_genders_get } from "./g_genders_get.mjs";
-export function g_sexes() {
+export function g_genders_names() {
   let genders = g_genders_get();
   let mapped = list_map_property(genders, "name");
   return genders;
