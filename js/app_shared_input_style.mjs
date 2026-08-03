52aca473954057a96a435a6ba636236870568d68
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 22:59:28 +0800

    function_literal_route app_shared_input_style app_shared_spaced_frame_gap

--- js/app_shared_input_style.mjs
+++ js/app_shared_input_style.mjs
@@ -1,3 +1,4 @@
+import { app_shared_spaced_frame_gap } from "./app_shared_spaced_frame_gap.mjs";
 import { app_shared_style_control } from "./app_shared_style_control.mjs";
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 import { html_border } from "./html_border.mjs";
@@ -5,5 +6,9 @@ import { app_shared_container_blue_border_color } from "./app_shared_container_b
 export function app_shared_input_style(component) {
   app_shared_style_control(component);
   html_style_background_color_set(component, "white");
-  html_border(component, "0.1em", app_shared_container_blue_border_color());
+  html_border(
+    component,
+    app_shared_spaced_frame_gap(),
+    app_shared_container_blue_border_color(),
+  );
 }
