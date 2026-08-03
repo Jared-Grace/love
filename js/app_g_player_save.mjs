dba446b677e744d2ff76f39ec8bcb76b4e16453d
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:33:00 +0800

    function_declarations_unused_clear app_g_player_save

--- js/app_g_player_save.mjs
+++ js/app_g_player_save.mjs
@@ -3,7 +3,7 @@ import { app_g_game_save } from "./app_g_game_save.mjs";
 import { object_assign } from "./object_assign.mjs";
 export async function app_g_player_save(player) {
   let g = await app_g_game_save_get();
-  let a = object_assign(g, {
+  object_assign(g, {
     player,
   });
   await app_g_game_save(g);
