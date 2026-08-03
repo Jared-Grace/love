d1390a858c0a7f0b36feb8ef43d1ec4a8e547831
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 22:12:32 +0800

    function_prose_set firebase_storage_url_project_jg 1 The address files are read from in the storage project this repo's apps download their content out of.

--- js/firebase_storage_url_project_jg.mjs
+++ js/firebase_storage_url_project_jg.mjs
@@ -1,6 +1,7 @@
 import { firebase_storage_url_project } from "./firebase_storage_url_project.mjs";
 import { firebase_project_name_jg } from "./firebase_project_name_jg.mjs";
 export function firebase_storage_url_project_jg() {
+  "The address files are read from in the storage project this repo's apps download their content out of.";
   let project_name = firebase_project_name_jg();
   let project_url = firebase_storage_url_project(project_name);
   return project_url;
