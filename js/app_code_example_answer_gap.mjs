82bacd9808f65daceb9b0d3134ac5d41e336755b
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 22:59:24 +0800

    function_literal_route app_code_example_answer_gap app_shared_spaced_frame_gap

--- js/app_code_example_answer_gap.mjs
+++ js/app_code_example_answer_gap.mjs
@@ -1,4 +1,5 @@
+import { app_shared_spaced_frame_gap } from "./app_shared_spaced_frame_gap.mjs";
 import { html_style_gap } from "./html_style_gap.mjs";
 export function app_code_example_answer_gap(parent) {
-  html_style_gap(parent, "0.1em");
+  html_style_gap(parent, app_shared_spaced_frame_gap());
 }
