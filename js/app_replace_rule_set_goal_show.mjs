a9518d528989fe7a0bea65ce93c564792b067a6d
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:40:04 +0800

    function_declarations_unused_clear app_replace_rule_set_goal_show

--- js/app_replace_rule_set_goal_show.mjs
+++ js/app_replace_rule_set_goal_show.mjs
@@ -4,7 +4,7 @@ import { app_replace_button_side } from "../../love/js/app_replace_button_side.m
 import { app_replace_symbol_tile_valid_if_curried_right } from "../../love/js/app_replace_symbol_tile_valid_if_curried_right.mjs";
 import { each } from "../../love/js/each.mjs";
 export function app_replace_rule_set_goal_show(root, end) {
-  let label_goal = app_shared_text_body(root, "Goal:");
+  app_shared_text_body(root, "Goal:");
   let p_goal = html_p(root);
   let goal_list_symbols = app_replace_button_side(p_goal, end);
   let lambda4 = app_replace_symbol_tile_valid_if_curried_right(false, false);
