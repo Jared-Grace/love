5cbf3457784e7f0a942f0e19dc5001719bc04fc2
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:01:21 +0800

    g_arc_prompt a test 100

--- js/each_index_1.mjs
+++ js/each_index_1.mjs
@@ -2,7 +2,7 @@ import { each_index } from "./each_index.mjs";
 import { text_combine } from "./text_combine.mjs";
 export function each_index_1(list, lambda$item$index_1) {
   function lambda(item, index) {
-    lambda$item$index_1(item, text_combine(index, 1));
+    lambda$item$index_1(item, add(index, 1));
   }
   let r = each_index(list, lambda);
   return r;
