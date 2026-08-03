d017cfd2bfe626e97c133b9069b7fbb98649e190
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 20:28:55 +0800

    ps

--- js/function_run_from_process_argv.mjs
+++ js/function_run_from_process_argv.mjs
@@ -1,6 +1,8 @@
+import { process_folder_repos_stand } from "./process_folder_repos_stand.mjs";
 import { function_run } from "./function_run.mjs";
 export async function function_run_from_process_argv() {
   const [, , f_name, ...args] = process.argv;
+  await process_folder_repos_stand();
  let result=await function_run(f_name, args)
   return result
 }
