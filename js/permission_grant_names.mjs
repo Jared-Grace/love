c960263ff9d1ab6f9f283130e9bfdbe0a214553c
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:17:54 +0800

    function_rename unbound_baseline_read functions_unbound_baseline_read

--- js/permission_grant_names.mjs
+++ js/permission_grant_names.mjs
@@ -125,7 +125,7 @@ export function permission_grant_names() {
   let spelled122 = fn_name("function_worker_pool_run_try");
   let spelled123 = fn_name("function_work_none_is");
   let spelled124 = fn_name("functions_shadowing_baseline_read");
-  let spelled125 = fn_name("unbound_baseline_read");
+  let spelled125 = fn_name("functions_unbound_baseline_read");
   let spelled126 = fn_name("permission_grants_baseline_read");
   let spelled127 = fn_name("functions_names");
   let spelled128 = fn_name("functions_unbound_gate_run");
