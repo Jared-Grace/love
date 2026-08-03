6981fe3a7f9340e5c2fb75feb7f889baecf14bc3
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:27:08 +0800

    function_fold list_index_of_add messenger_reply_unreplied

--- js/messenger_reply_unreplied.mjs
+++ js/messenger_reply_unreplied.mjs
@@ -1,7 +1,6 @@
-import { add } from "./add.mjs";
+import { list_index_of_add } from "./list_index_of_add.mjs";
 import { sleep_forever } from "./sleep_forever.mjs";
 import { list_skip } from "./list_skip.mjs";
-import { list_index_of } from "./list_index_of.mjs";
 import { list_last } from "./list_last.mjs";
 import { list_filter_property } from "./list_filter_property.mjs";
 import { messenger_reply_messages_me } from "./messenger_reply_messages_me.mjs";
@@ -12,8 +11,7 @@ export async function messenger_reply_unreplied(messages) {
   let mine = list_filter_property(messages, property_name, property_value);
   await sleep_forever();
   let mine_last = list_last(mine);
-  let index_last = list_index_of(messages, mine_last);
-  let skip_count = add(index_last, 1);
+  let skip_count = list_index_of_add(messages, mine_last, 1);
   let unreplied = list_skip(messages, skip_count);
   let result = {
     mine_last,
