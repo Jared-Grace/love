6a8730a933d0bc4fb2e057c7b3fc28819522cc6e
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:46:57 +0800

    function_declarations_unused_clear function_param_swap_task_simple

--- js/function_param_swap_task_simple.mjs
+++ js/function_param_swap_task_simple.mjs
@@ -4,7 +4,6 @@ import { todo } from "./todo.mjs";
 import { text_combine } from "./text_combine.mjs";
 export async function function_param_swap_task_simple() {
   text_combine("todo a simple test case of ", function_param_swap.name);
-  let f_name = function_param_swap_task_simple;
   await function_new_open(f_name2);
   let f_name3 = todo();
   await function_param_swap(f_name3, param_name_a, param_name_b);
