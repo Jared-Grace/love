d322cc07109cb839671636047dd69cb92f68e799
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 20:44:39 +0800

    ai

--- js/js_operator_less_than.mjs
+++ js/js_operator_less_than.mjs
@@ -1,7 +1,7 @@
 import { less_than } from "./less_than.mjs";
 import { identity } from "./identity.mjs";
 export function js_operator_less_than() {
-  let operator = "<";
+  let operator = js_operator_less_than_symbol();
   let fn = less_than;
   let o = {
     fn,
