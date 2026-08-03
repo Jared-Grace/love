670b4e3f9a643e250a89f63da88bcc6fa0d934b0
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:40:11 +0800

    function_declarations_unused_clear app_replace_rule_sets_generate

--- js/app_replace_rule_sets_generate.mjs
+++ js/app_replace_rule_sets_generate.mjs
@@ -5,5 +5,5 @@ import { function_transform_fn } from "./function_transform_fn.mjs";
 export async function app_replace_rule_sets_generate() {
   let names = await app_replace_rule_sets_v_1_names();
   let r = js_flo_body_empty_return_identifiers_curried_right(names);
-  let output = await function_transform_fn(app_replace_rule_sets, r);
+  await function_transform_fn(app_replace_rule_sets, r);
 }
