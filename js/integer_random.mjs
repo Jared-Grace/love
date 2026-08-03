f7f0abf5ccb91b9bcd83ee044a735965bf9dff79
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:27:09 +0800

    function_fold multiply_floor integer_random

--- js/integer_random.mjs
+++ js/integer_random.mjs
@@ -1,10 +1,9 @@
+import { multiply_floor } from "./multiply_floor.mjs";
 import { add } from "./add.mjs";
 import { assert_multiple } from "./assert_multiple.mjs";
 import { integer_is } from "./integer_is.mjs";
 import { random } from "./random.mjs";
-import { floor } from "./floor.mjs";
 import { text_combine } from "./text_combine.mjs";
-import { multiply } from "./multiply.mjs";
 import { subtract } from "./subtract.mjs";
 export function integer_random(min, max) {
   let list = [min, max];
@@ -12,8 +11,7 @@ export function integer_random(min, max) {
   let left = random();
   let left2 = subtract(max, min);
   let right = add(left2, 1);
-  let p = multiply(left, right);
-  let left3 = floor(p);
+  let left3 = multiply_floor(left, right);
   let r = text_combine(left3, min);
   return r;
 }
