009c8d53503a4c2a843c364dfed03a5ede591b75
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:40:01 +0800

    function_declarations_unused_clear app_replace_deploy

--- js/app_replace_deploy.mjs
+++ js/app_replace_deploy.mjs
@@ -5,7 +5,7 @@ import { app_replace_rule_sets_fns_names } from "./app_replace_rule_sets_fns_nam
 import { app_replace_rule_set_rename } from "./app_replace_rule_set_rename.mjs";
 import { app_replace_rule_sets_fns_migrate_capitalization_upper } from "./app_replace_rule_sets_fns_migrate_capitalization_upper.mjs";
 export async function app_replace_deploy() {
-  let result = await app_replace_rule_sets_fns_migrate_capitalization_upper();
+  await app_replace_rule_sets_fns_migrate_capitalization_upper();
   let mapped = app_replace_rule_sets_fns_names();
   await each_async(mapped, app_replace_rule_set_rename);
   await command_line_node_r(fn_name("app_replace_deploy_inner"), []);
