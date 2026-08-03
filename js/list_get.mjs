b921aff3a3ec4c7c7a363cb80573417ef6de7725
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:28:12 +0800

    ai

--- js/list_get.mjs
+++ js/list_get.mjs
@@ -1,6 +1,4 @@
 import { indexed_get_generic } from "./indexed_get_generic.mjs";
-import { integer_is_assert_json } from "./integer_is_assert_json.mjs";
-import { undefined_not_is_assert_lambda } from "./undefined_not_is_assert_lambda.mjs";
 export function list_get(list, index) {
   let item = indexed_get_generic(list, index, "list", object_get);
   function object_get() {
