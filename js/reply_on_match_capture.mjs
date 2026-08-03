39fdc96c6d0e06864bb1719576fde206f3117ff8
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 17:40:12 +0800

    ai

--- js/reply_on_match_capture.mjs
+++ js/reply_on_match_capture.mjs
@@ -23,7 +23,7 @@ export function reply_on_match_capture(fn, lambda) {
   let after = function lambda3(filtered, u) {
     capture(filtered, "after", u);
   };
-  let on_args = function lambda4(args) {
+  let on_args = function lambda4(args, u) {
     list_add(args, u);
   };
   let matcher = reply_on_match_generic(fn, before, after, on_args, [lambda]);
