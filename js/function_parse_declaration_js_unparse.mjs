da3623a89613b08764cd33fe5446a477e1d90a37
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:46:58 +0800

    function_declarations_unused_clear function_parse_declaration_js_unparse

--- js/function_parse_declaration_js_unparse.mjs
+++ js/function_parse_declaration_js_unparse.mjs
@@ -4,7 +4,7 @@ import { properties_get } from "./properties_get.mjs";
 export async function function_parse_declaration_js_unparse(f_name) {
   "todo rename remove js";
   let { declaration } = await function_parse_declaration(f_name);
-  let properties = properties_get(declaration);
+  properties_get(declaration);
   let output = js_unparse(declaration);
   return output;
 }
