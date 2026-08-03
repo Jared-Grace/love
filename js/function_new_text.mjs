5266ca236efd78a4af8b2b4c64663f07537e361e
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:46:57 +0800

    function_declarations_unused_clear function_new_text

--- js/function_new_text.mjs
+++ js/function_new_text.mjs
@@ -4,5 +4,5 @@ import { js_expression_string } from "./js_expression_string.mjs";
 export async function function_new_text(f_name, text) {
   let e = js_expression_string(text);
   let lambda = js_flo_body_add_return_argument_curried_right(e);
-  let output = await function_new_open_transform(f_name, lambda);
+  await function_new_open_transform(f_name, lambda);
 }
