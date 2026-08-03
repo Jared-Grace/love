cb99664dabce0aad09afd592da2cfde5c97984b7
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:15:55 +0800

    function_declarations_unused_clear app_a_overlay_close_button

--- js/app_a_overlay_close_button.mjs
+++ js/app_a_overlay_close_button.mjs
@@ -8,5 +8,5 @@ export function app_a_overlay_close_button(app_a_overlay_result) {
     "overlay_close",
   );
   let text = app_a_overlay_close_text();
-  let component = app_a_button_wide(overlay, text, rename_overlay_close);
+  app_a_button_wide(overlay, text, rename_overlay_close);
 }
