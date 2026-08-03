fa63d9ee6dad0b48020d673c219fa63f1dbdeefc
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:01:52 +0800

    function_transform_single_auto js_text_combine_number_add list_shuffle_cycled_range

--- js/list_shuffle_cycled_range.mjs
+++ js/list_shuffle_cycled_range.mjs
@@ -1,7 +1,8 @@
+import { add } from "./add.mjs";
 import { list_shuffle_cycled } from "./list_shuffle_cycled.mjs";
-import { text_combine } from "./text_combine.mjs";
 import { subtract } from "./subtract.mjs";
 export function list_shuffle_cycled_range(list, min, max) {
-  let choices_count = text_combine(subtract(max, min), 1);
+  let left = subtract(max, min);
+  let choices_count = add(left, 1);
   list_shuffle_cycled(list, choices_count);
 }
