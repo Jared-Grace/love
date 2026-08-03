7a34cf50c54462e43680bf406e29a9ac19bc8e22
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:38:04 +0800

    ai

--- js/firebase_prod_asset_backup_destination.mjs
+++ js/firebase_prod_asset_backup_destination.mjs
@@ -1,9 +1,9 @@
-import { fn_name } from "./fn_name.mjs";
+import { firebase_prod_app_backup_namespace } from "./firebase_prod_app_backup_namespace.mjs";
 import { file_backup_path } from "./file_backup_path.mjs";
 import { firebase_deploy_function_destination } from "./firebase_deploy_function_destination.mjs";
 export function firebase_prod_asset_backup_destination(file_name) {
   let dated = file_backup_path(file_name);
-  let f_name = fn_name("firebase_prod_app_backup");
+  let f_name = firebase_prod_app_backup_namespace();
   let destination = firebase_deploy_function_destination(f_name, dated);
   return destination;
 }
