fe958ce6791dc3bb0ae22f52689d56c33c5c8a12
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 14:57:37 +0800

    function_declarations_unused_clear a_brackets_wrap

--- js/a_brackets_wrap.mjs
+++ js/a_brackets_wrap.mjs
@@ -3,8 +3,8 @@ import { html_span_text } from "./html_span_text.mjs";
 import { js_code_bracket_open } from "./js_code_bracket_open.mjs";
 export function a_brackets_wrap(parent, inner) {
   let b = js_code_bracket_open();
-  let span5 = html_span_text(parent, b);
+  html_span_text(parent, b);
   inner();
   let v = js_code_bracket_close();
-  let span6 = html_span_text(parent, v);
+  html_span_text(parent, v);
 }
