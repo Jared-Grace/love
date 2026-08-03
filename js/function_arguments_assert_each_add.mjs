c7d4acd317ce1212a944a511872d4ab8aab5a8a5
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 22:04:52 +0800

    function_prose_add function_arguments_assert_each_add Adds a check on every argument to a function, naming the test each one has to pass.

--- js/function_arguments_assert_each_add.mjs
+++ js/function_arguments_assert_each_add.mjs
@@ -1,7 +1,11 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { function_transform } from "./function_transform.mjs";
 import { function_arguments_assert_each_add_lambda } from "./function_arguments_assert_each_add_lambda.mjs";
-export async function function_arguments_assert_each_add(f_name, predicates_comma) {
+export async function function_arguments_assert_each_add(
+  f_name,
+  predicates_comma,
+) {
+  "Adds a check on every argument to a function, naming the test each one has to pass.";
   arguments_assert(arguments, 2);
   let lambda = function_arguments_assert_each_add_lambda(predicates_comma);
   await function_transform(f_name, lambda);
