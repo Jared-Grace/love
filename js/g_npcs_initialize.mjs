d8e5f417ea3d798e1790561ab6172f48c48070a2
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:49:56 +0800

    ao g_npcs_initialize

--- js/g_npcs_initialize.mjs
+++ js/g_npcs_initialize.mjs
@@ -1,4 +1,4 @@
-import { each } from "./each.mjs";
+import { property_transform_multiple } from "./property_transform_multiple.mjs";
 import { g_gender_pronouns } from "./g_gender_pronouns.mjs";
 import { g_conversation_key } from "./g_conversation_key.mjs";
 import { each_index } from "./each_index.mjs";
@@ -13,7 +13,8 @@ import { g_genders_get } from "./g_genders_get.mjs";
 import { g_conversation_generate } from "./g_conversation_generate.mjs";
 export function g_npcs_initialize(player_img, coordinates_land) {
   let genders = g_genders_get(player_img);
-property_transform_multiple
+  function lambda2() {}
+  property_transform_multiple(list, property, lambda2);
   let gender_count = list_size(genders);
   let npc_count = 30;
   let npcs = list_remove_end(coordinates_land, npc_count);
