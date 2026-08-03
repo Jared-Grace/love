fdd5d418904732937d772a5912ff09a59fce4944
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 22:17:18 +0800

    function_prose_add app_shared_dev_build_multiple Builds each of the named apps for dev, one after another.

--- js/app_shared_dev_build_multiple.mjs
+++ js/app_shared_dev_build_multiple.mjs
@@ -1,7 +1,9 @@
 import { app_shared_dev_build } from "./app_shared_dev_build.mjs";
 import { each_async } from "./each_async.mjs";
 export async function app_shared_dev_build_multiple(list) {
-  await each_async(list, async function lambda(item) {
+  "Builds each of the named apps for dev, one after another.";
+  async function lambda(item) {
     await app_shared_dev_build(item);
-  });
+  }
+  await each_async(list, lambda);
 }
