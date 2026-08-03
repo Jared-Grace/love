3328fedb5c1f233ddc80844afed291bb89b1eb0d
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:33:05 +0800

    function_declarations_unused_clear app_replace_button_home

--- js/app_replace_button_home.mjs
+++ js/app_replace_button_home.mjs
@@ -3,5 +3,5 @@ import { app_replace_home } from "../../love/js/app_replace_home.mjs";
 import { app_shared_screen_set_button } from "../../love/js/app_shared_screen_set_button.mjs";
 export function app_replace_button_home(root, context) {
   let text = app_shared_button_home_text();
-  let b = app_shared_screen_set_button(root, context, app_replace_home, text);
+  app_shared_screen_set_button(root, context, app_replace_home, text);
 }
