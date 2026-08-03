bbcb20ecec5bfa524ed34ee77711c660f19416f0
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:46:55 +0800

    function_declarations_unused_clear function_current_selects_apply_generic

--- js/function_current_selects_apply_generic.mjs
+++ js/function_current_selects_apply_generic.mjs
@@ -12,5 +12,5 @@ export async function function_current_selects_apply_generic(
     let selects = await function_current_selects_nodes_ast(ast);
     await imported_fn(ast, selects, ...args);
   }
-  let output = await function_transform(f_name_current, lambda);
+  await function_transform(f_name_current, lambda);
 }
