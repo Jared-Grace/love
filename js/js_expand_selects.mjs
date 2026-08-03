71f8ef181f841d63719bb1e9d52b2efb6dd0ce28
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 17:00:18 +0800

    function_declarations_unused_clear js_expand_selects

--- js/js_expand_selects.mjs
+++ js/js_expand_selects.mjs
@@ -6,5 +6,5 @@ export async function js_expand_selects(ast, selects) {
   let item = js_selects_block_item(ast, selects);
   let v = js_node_to_visitor(ast, item);
   let stack_2 = property_list_get_end_1(v, "stack");
-  let inserted = await js_expand_generic(item, stack_2, ast);
+  await js_expand_generic(item, stack_2, ast);
 }
