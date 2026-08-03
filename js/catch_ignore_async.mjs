6ab49f3935d511ba2bcbfdfa8bd7b89728a2e983
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:39:09 +0800

    function_declarations_unused_clear catch_ignore_async

--- js/catch_ignore_async.mjs
+++ js/catch_ignore_async.mjs
@@ -1,4 +1,4 @@
 import { catch_null_async } from "./catch_null_async.mjs";
 export async function catch_ignore_async(lambda) {
-  let r = await catch_null_async(lambda);
+  await catch_null_async(lambda);
 }
