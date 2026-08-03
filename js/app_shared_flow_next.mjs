31b11fc394277e4bea44f4eedcaf0e05e99c6312
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:01:19 +0800

    function_transform_single_auto js_text_combine_number_add app_shared_flow_next

--- js/app_shared_flow_next.mjs
+++ js/app_shared_flow_next.mjs
@@ -1,10 +1,11 @@
+import { add } from "./add.mjs";
 import { app_shared_flow } from "./app_shared_flow.mjs";
 import { list_first } from "./list_first.mjs";
 import { list_skip } from "./list_skip.mjs";
-import { text_combine } from "./text_combine.mjs";
 export async function app_shared_flow_next(context, screens) {
   let before_or_after = function lambda(screens_given, index) {
-    let skipped = list_skip(screens_given, add(index, 1));
+    let skip_count = add(index, 1);
+    let skipped = list_skip(screens_given, skip_count);
     return skipped;
   };
   let find = list_first;
