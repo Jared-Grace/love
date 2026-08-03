c78536a681176e554d93bd0a0258fc3ae306e144
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:39:18 +0800

    function_declarations_unused_clear file_exists_ensure

--- js/file_exists_ensure.mjs
+++ js/file_exists_ensure.mjs
@@ -3,6 +3,6 @@ import { file_exists_not } from "./file_exists_not.mjs";
 export async function file_exists_ensure(file_path) {
   let n = await file_exists_not(file_path);
   if (n) {
-    let r = await file_write(file_path, "");
+    await file_write(file_path, "");
   }
 }
