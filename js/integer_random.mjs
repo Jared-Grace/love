019bceadff067660994593d36fa0649c67987875
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 21:01:39 +0800

    function_transform_single_auto js_text_combine_number_add integer_random

--- js/integer_random.mjs
+++ js/integer_random.mjs
@@ -1,3 +1,4 @@
+import { add } from "./add.mjs";
 import { assert_multiple } from "./assert_multiple.mjs";
 import { integer_is } from "./integer_is.mjs";
 import { random } from "./random.mjs";
@@ -8,9 +9,11 @@ import { subtract } from "./subtract.mjs";
 export function integer_random(min, max) {
   let list = [min, max];
   assert_multiple(integer_is, list);
-  let r = text_combine(
-    floor(multiply(random(), text_combine(subtract(max, min), 1))),
-    min,
-  );
+  let left = random();
+  let left2 = subtract(max, min);
+  let right = add(left2, 1);
+  let p = multiply(left, right);
+  let left3 = floor(p);
+  let r = text_combine(left3, min);
   return r;
 }
