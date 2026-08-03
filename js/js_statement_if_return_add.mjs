e92efdeb5e563ee93a72f64ec54249940e31c95e
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 17:02:59 +0800

    function_declarations_unused_clear js_statement_if_return_add

--- js/js_statement_if_return_add.mjs
+++ js/js_statement_if_return_add.mjs
@@ -6,5 +6,5 @@ export function js_statement_if_return_add(ast, selects) {
   let statement_if = list_single(selects);
   let c = js_statement_if_consequent_get(statement_if);
   let value = js_block_body_get(c);
-  let r = js_statement_return_empty_add(value);
+  js_statement_return_empty_add(value);
 }
