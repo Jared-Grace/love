55a70c5b5a002ac50133c9879ca6fc8cbf68c45b
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:39:13 +0800

    function_declarations_unused_clear ebible_languages_books_upload

--- js/ebible_languages_books_upload.mjs
+++ js/ebible_languages_books_upload.mjs
@@ -3,6 +3,6 @@ import { ebible_version_books_upload } from "./ebible_version_books_upload.mjs";
 export async function ebible_languages_books_upload() {
   await ebible_languages_without_original_bible_folders_each(lambda);
   async function lambda(bible_folder) {
-    let books = await ebible_version_books_upload(bible_folder);
+    await ebible_version_books_upload(bible_folder);
   }
 }
