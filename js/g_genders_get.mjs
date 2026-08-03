17b30c3b4ef9e191cc8ff23f362234f4b730829c
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 16:40:49 +0800

    g_genders_get

--- js/g_genders_get.mjs
+++ js/g_genders_get.mjs
@@ -5,20 +5,20 @@ import { g_gender_female } from "./g_gender_female.mjs";
 import { bible_names_women } from "./bible_names_women.mjs";
 import { g_male_img_names } from "./g_male_img_names.mjs";
 import { g_female_img_names } from "./g_female_img_names.mjs";
-export function g_genders_get(player_img) {
+export function g_genders_get() {
   let imgs_women = g_female_img_names();
   let imgs_men = g_male_img_names();
   let names_women = bible_names_women();
   let female = {
     name: g_gender_female(),
     names: names_women,
-    imgs: list_without(imgs_women, player_img),
+    imgs: imgs_women,
   };
   let names_men = bible_names_men();
   let male = {
     name: g_gender_male(),
     names: names_men,
-    imgs: list_without(imgs_men, player_img),
+    imgs: imgs_men,
   };
   let genders = [male, female];
   return genders;
