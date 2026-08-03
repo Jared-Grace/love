5b1ac39a0328f455b3ad7a07025905360cd952df
JESUS rose to life <username@users.noreply.github.com>
2026-08-03 15:08:08 +0800

    function_fold property_list_join_comma functions_module_state_shadowed_gate_run

--- js/functions_module_state_shadowed_gate_run.mjs
+++ js/functions_module_state_shadowed_gate_run.mjs
@@ -1,6 +1,6 @@
+import { property_list_join_comma } from "./property_list_join_comma.mjs";
 import { functions_module_state_shadowed } from "./functions_module_state_shadowed.mjs";
 import { property_get } from "./property_get.mjs";
-import { list_join_comma } from "./list_join_comma.mjs";
 import { list_size } from "./list_size.mjs";
 import { fn_name } from "./fn_name.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
@@ -18,8 +18,7 @@ export async function functions_module_state_shadowed_gate_run() {
   let offenders = await functions_module_state_shadowed();
   for (let offender of offenders) {
     let f_name = property_get(offender, "name");
-    let hidden = property_get(offender, "hidden");
-    let joined = list_join_comma(hidden);
+    let joined = property_list_join_comma(offender, "hidden");
     console.log("SHARED STATE HIDDEN  " + f_name + "  " + joined);
   }
   let size = list_size(offenders);
