ac7ef5ec7b00dc0236a074597b6d0a15ea109861
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:29 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_absolute_value_title_name_id

--- js/app_code_lesson_expression_absolute_value_title_name_id.mjs
+++ js/app_code_lesson_expression_absolute_value_title_name_id.mjs
@@ -1,20 +1,15 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_expression_absolute_value_title_name_id() {
   arguments_assert(arguments, 0);
   ("the home title is console.log absolute value");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      html_span_text(parent, "Absolute value ");
-      html_span_text_code_dark(parent, "Math.abs");
-    }
-    return render;
+  function paint(parent) {
+    html_span_text(parent, "Absolute value ");
+    html_span_text_code_dark(parent, "Math.abs");
   }
   let rights = ["absolute value"];
-  let built = app_code_lesson_name_id_generic(rights, "functions", title_get);
+  let built = app_code_lesson_name_id_category_then(rights, "functions", paint);
   return built;
 }
