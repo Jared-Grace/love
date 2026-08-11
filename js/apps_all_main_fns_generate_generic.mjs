c84815272f04c2a25f608cb4307b10de18e5c04f
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 18:48:55 +0800

    function_rename app_apps_all_main_fns_generate_open apps_all_main_fns_generate_open

--- js/apps_all_main_fns_generate_generic.mjs
+++ js/apps_all_main_fns_generate_generic.mjs
@@ -1,10 +1,10 @@
-import { app_apps_all_main_fns_generate_open } from "../../love/js/app_apps_all_main_fns_generate_open.mjs";
-import { list_intersect_sort_text } from "../../love/js/list_intersect_sort_text.mjs";
-import { data_identifiers_get_properties } from "../../love/js/data_identifiers_get_properties.mjs";
-import { apps_names_prefixed } from "../../love/js/apps_names_prefixed.mjs";
+import { apps_all_main_fns_generate_open } from "./apps_all_main_fns_generate_open.mjs";
+import { list_intersect_sort_text } from "./list_intersect_sort_text.mjs";
+import { data_identifiers_get_properties } from "./data_identifiers_get_properties.mjs";
+import { apps_names_prefixed } from "./apps_names_prefixed.mjs";
 export async function apps_all_main_fns_generate_generic(generate_fn) {
   let prefixed = await apps_names_prefixed();
   let identifier_names = await data_identifiers_get_properties();
   let apps = list_intersect_sort_text(prefixed, identifier_names);
-  await generate_fn(app_apps_all_main_fns_generate_open, apps);
+  await generate_fn(apps_all_main_fns_generate_open, apps);
 }
