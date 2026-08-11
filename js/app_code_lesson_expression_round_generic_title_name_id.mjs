1fe787dce107b6f9d8c6552efd3de515b73ff5a6
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:35 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_round_generic_title_name_id

--- js/app_code_lesson_expression_round_generic_title_name_id.mjs
+++ js/app_code_lesson_expression_round_generic_title_name_id.mjs
@@ -1,27 +1,22 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
 import { text_combine } from "./text_combine.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_expression_round_generic_title_name_id(
   direction,
   called_name,
 ) {
   arguments_assert(arguments, 2);
   ("the home title: Round {direction} {fn}");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      let heading = text_combine_multiple(["Round ", direction, " "]);
-      html_span_text(parent, heading);
-      html_span_text_code_dark(parent, called_name);
-    }
-    return render;
+  function paint(parent) {
+    let heading = text_combine_multiple(["Round ", direction, " "]);
+    html_span_text(parent, heading);
+    html_span_text_code_dark(parent, called_name);
   }
   let heading_lower = text_combine("round ", direction);
   let rights = [heading_lower];
-  let built = app_code_lesson_name_id_generic(rights, "functions", title_get);
+  let built = app_code_lesson_name_id_category_then(rights, "functions", paint);
   return built;
 }
