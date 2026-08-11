9086865e2a5889e436b321a0bd5dc2623ea40e60
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:00:13 +0800

    function_rename app_reply_choices_praise reply_choices_praise

--- js/app_message_reply_praise_god.mjs
+++ js/app_message_reply_praise_god.mjs
@@ -1,11 +1,11 @@
 import { reply_sequence_outputs } from "./reply_sequence_outputs.mjs";
 import { reply_optional } from "./reply_optional.mjs";
 import { reply_word_god } from "./reply_word_god.mjs";
-import { app_reply_choices_praise } from "./app_reply_choices_praise.mjs";
+import { reply_choices_praise } from "./reply_choices_praise.mjs";
 import { prayer_end } from "./prayer_end.mjs";
 export function app_message_reply_praise_god() {
   let v = prayer_end();
-  let v2 = app_reply_choices_praise();
+  let v2 = reply_choices_praise();
   let god = reply_word_god();
   let fn11 = reply_optional("i");
   let fn10 = reply_sequence_outputs(["prai", fn11, "se", god], [v2, v]);
