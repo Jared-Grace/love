32693d18fe358d5c98cbdd1898b614f388888009
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 18:48:55 +0800

    function_rename app_apps_all_main_fns_generate_open apps_all_main_fns_generate_open

--- js/apps_all_main_fns_generate_open.mjs
+++ js/apps_all_main_fns_generate_open.mjs
@@ -1,6 +1,6 @@
 import { apps_all_main_fns_generate_generic } from "./apps_all_main_fns_generate_generic.mjs";
 import { function_list_generate_open } from "./function_list_generate_open.mjs";
-export async function app_apps_all_main_fns_generate_open() {
+export async function apps_all_main_fns_generate_open() {
   let generate_fn = function_list_generate_open;
   await apps_all_main_fns_generate_generic(generate_fn);
 }
