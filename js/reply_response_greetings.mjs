1e1d998541b65a90860afbf1581b1973c9e5f2df
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:00:31 +0800

    function_rename app_reply_response_greetings reply_response_greetings

--- js/reply_response_greetings.mjs
+++ js/reply_response_greetings.mjs
@@ -2,7 +2,7 @@ import { emoji_cross } from "./emoji_cross.mjs";
 import { emoji_smile } from "./emoji_smile.mjs";
 import { emoji_wave } from "./emoji_wave.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
-export function app_reply_response_greetings() {
+export function reply_response_greetings() {
   let greetings = text_combine_multiple([
     emoji_wave(),
     emoji_smile(),
