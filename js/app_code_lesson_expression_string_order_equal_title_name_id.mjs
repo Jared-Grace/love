4f3e7af2c7d3c892c7f3c7f7522096e1cee661a1
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:37 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_string_order_equal_title_name_id

--- js/app_code_lesson_expression_string_order_equal_title_name_id.mjs
+++ js/app_code_lesson_expression_string_order_equal_title_name_id.mjs
@@ -1,22 +1,17 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { app_code_string_operators_shape } from "./app_code_string_operators_shape.mjs";
 import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_expression_string_order_equal_title_name_id() {
   arguments_assert(arguments, 0);
   ("the home title: order or equal");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      html_span_text(parent, "Order or equal ");
-      app_code_string_operators_shape(parent, "<=", ">=");
-    }
-    return render;
+  function paint(parent) {
+    html_span_text(parent, "Order or equal ");
+    app_code_string_operators_shape(parent, "<=", ">=");
   }
   let rights = ["string", "order", "equal"];
   let left = app_code_category_expressions();
-  let built = app_code_lesson_name_id_generic(rights, left, title_get);
+  let built = app_code_lesson_name_id_category_then(rights, left, paint);
   return built;
 }
