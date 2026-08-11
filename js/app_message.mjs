05ac64cc3c378fd1d9093bf17062567608da9ba1
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 17:22:32 +0800

    function_nested_lift app_message refresh app_message_refresh

--- js/app_message.mjs
+++ js/app_message.mjs
@@ -1,24 +1,17 @@
+import { app_message_refresh } from "./app_message_refresh.mjs";
 import { app_message_messages_get } from "./app_message_messages_get.mjs";
-import { app_message_message_display } from "./app_message_message_display.mjs";
-import { list_first_property } from "./list_first_property.mjs";
-import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 import { html_font_sans_serif_set_html } from "./html_font_sans_serif_set_html.mjs";
-import { invoke_multiple_unordered_async } from "./invoke_multiple_unordered_async.mjs";
 import { html_textarea } from "./html_textarea.mjs";
 import { object_merge_set } from "./object_merge_set.mjs";
 import { html_value_set } from "./html_value_set.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
-import { html_div_text_multiple } from "./html_div_text_multiple.mjs";
 import { reply_messages_matches } from "./reply_messages_matches.mjs";
 import { property_get } from "./property_get.mjs";
 import { app_message_reply_choices } from "./app_message_reply_choices.mjs";
 import { app_shared_contact_send } from "./app_shared_contact_send.mjs";
-import { app_shared_button_uncolored_background_color } from "./app_shared_button_uncolored_background_color.mjs";
 import { storage_local_set_context } from "./storage_local_set_context.mjs";
 import { html_value_get } from "./html_value_get.mjs";
 import { list_add } from "./list_add.mjs";
-import { html_clear } from "./html_clear.mjs";
-import { html_text_set } from "./html_text_set.mjs";
 import { html_div } from "./html_div.mjs";
 import { html_check_empty_not } from "./html_check_empty_not.mjs";
 import { app_karate_screen_input_validate } from "./app_karate_screen_input_validate.mjs";
@@ -30,7 +23,6 @@ import { html_div_text } from "./html_div_text.mjs";
 import { app_shared_container } from "./app_shared_container.mjs";
 import { app_shared_font_size_refresh } from "./app_shared_font_size_refresh.mjs";
 import { app_shared_input_style } from "./app_shared_input_style.mjs";
-import { list_map } from "./list_map.mjs";
 import { text_combine } from "./text_combine.mjs";
 export async function app_message(context) {
   let messages_property = "messages";
@@ -43,7 +35,7 @@ export async function app_message(context) {
   html_font_sans_serif_set_html();
   let div_messages = html_div(root);
   let start = app_message_reply_choices();
-  await refresh();
+  await app_message_refresh(div_messages, context, messages_property, start);
   let div = app_shared_container(root);
   html_div_text(div, "Please enter your message for me:");
   let textarea = html_textarea(div);
@@ -58,37 +50,6 @@ export async function app_message(context) {
   app_karate_screen_input_validate(div, div_checks, [textarea], button_send, [
     v,
   ]);
-  async function refresh() {
-    html_clear(div_messages);
-    let messages = app_message_messages_get(context, messages_property);
-    function lambda(message) {
-      app_message_message_display("left", message, div_messages);
-      let right = app_message_message_display(
-        "right",
-        "(Loading...)",
-        div_messages,
-      );
-      let background = app_shared_button_uncolored_background_color();
-      html_style_background_color_set(right, background);
-      async function next() {
-        let results = await reply_messages_matches([message], start);
-        let e = list_empty_is(results);
-        html_clear(right);
-        if (e) {
-          html_text_set(
-            right,
-            "I have received your message. Lord-willing, I will answer. Please come back later to see if I have replied.",
-          );
-        } else {
-          let outputs = list_first_property(results, "outputs");
-          html_div_text_multiple(right, outputs);
-        }
-      }
-      return next;
-    }
-    let nexts = list_map(messages, lambda);
-    await invoke_multiple_unordered_async(nexts);
-  }
   async function on_send() {
     let message = html_value_get(textarea);
     html_value_set(textarea, "");
@@ -101,6 +62,6 @@ export async function app_message(context) {
     let messages = app_message_messages_get(context, messages_property);
     list_add(messages, message);
     storage_local_set_context(context, messages_property, messages);
-    await refresh();
+    await app_message_refresh(div_messages, context, messages_property, start);
   }
 }
