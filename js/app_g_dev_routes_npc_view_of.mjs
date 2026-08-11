8d62cf769a6fb2ed58f75c35c396554c9f5dbed6
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 17:03:24 +0800

    ai

--- js/app_g_dev_routes_npc_view_of.mjs
+++ js/app_g_dev_routes_npc_view_of.mjs
@@ -1,6 +1,5 @@
+import { app_g_view_npc } from "./app_g_view_npc.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_g_view_kind_npc } from "./app_g_view_kind_npc.mjs";
-import { property_get } from "./property_get.mjs";
 import { app_g_view_set } from "./app_g_view_set.mjs";
 export async function app_g_dev_routes_npc_view_of(npc, phase) {
   arguments_assert(arguments, 2);
