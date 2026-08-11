3c236a9f7843950628ea87d1c3af604bc28f02f1
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:12:57 +0800

    ai

--- js/app_code_lesson_expression_comparison_and_comparison.mjs
+++ js/app_code_lesson_expression_comparison_and_comparison.mjs
@@ -1,9 +1,8 @@
+import { js_code_operation } from "./js_code_operation.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { integer_random } from "./integer_random.mjs";
 import { add } from "./add.mjs";
 import { ternary } from "./ternary.mjs";
-import { text_to } from "./text_to.mjs";
-import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function app_code_lesson_expression_comparison_and_comparison(want) {
   arguments_assert(arguments, 1);
   ("a comparison code string that is `want`: low < high is true, low > high is false");
@@ -11,8 +10,6 @@ export function app_code_lesson_expression_comparison_and_comparison(want) {
   let min = add(low, 1);
   let high = integer_random(min, 9);
   let op = ternary(want, "<", ">");
-  let left = text_to(low);
-  let right = text_to(high);
-  let code = text_combine_multiple([left, " ", op, " ", right]);
+  let code = js_code_operation(low, op, high);
   return code;
 }
