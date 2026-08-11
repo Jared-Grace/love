83b719db6b6c3437e602e0f672177818422a1f7c
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:36 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_string_equality_title_name_id

--- js/app_code_lesson_expression_string_equality_title_name_id.mjs
+++ js/app_code_lesson_expression_string_equality_title_name_id.mjs
@@ -1,22 +1,17 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { app_code_string_operators_shape } from "./app_code_string_operators_shape.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 export function app_code_lesson_expression_string_equality_title_name_id() {
   arguments_assert(arguments, 0);
   ("the home title: comparing strings");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      html_span_text(parent, "Comparing strings ");
-      app_code_string_operators_shape(parent, "===", "!==");
-    }
-    return render;
+  function paint(parent) {
+    html_span_text(parent, "Comparing strings ");
+    app_code_string_operators_shape(parent, "===", "!==");
   }
   let rights = ["string", "equality"];
   let left = app_code_category_expressions();
-  let built = app_code_lesson_name_id_generic(rights, left, title_get);
+  let built = app_code_lesson_name_id_category_then(rights, left, paint);
   return built;
 }
