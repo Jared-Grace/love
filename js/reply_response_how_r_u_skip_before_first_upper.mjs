b657ad9740d81ecd9dcc7e25fd164ce6b9c69e02
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:00:40 +0800

    function_rename app_reply_response_how_r_u_skip_before_first_upper reply_response_how_r_u_skip_before_first_upper

--- js/reply_response_how_r_u_skip_before_first_upper.mjs
+++ js/reply_response_how_r_u_skip_before_first_upper.mjs
@@ -1,6 +1,6 @@
 import { app_reply_response_how_r_u_skip_before } from "./app_reply_response_how_r_u_skip_before.mjs";
 import { text_first_upper_to } from "./text_first_upper_to.mjs";
-export function app_reply_response_how_r_u_skip_before_first_upper() {
+export function reply_response_how_r_u_skip_before_first_upper() {
   let t = app_reply_response_how_r_u_skip_before();
   let u = text_first_upper_to(t);
   return u;
