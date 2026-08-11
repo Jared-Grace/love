7f6596214ce7e6c069260143e83dcbc2b6370f40
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 13:34:36 +0800

    ai

--- js/js_stack_list_block_is.mjs
+++ js/js_stack_list_block_is.mjs
@@ -1,19 +1,7 @@
-import { add } from "./add.mjs";
+import { js_stack_list_owner_or_null } from "./js_stack_list_owner_or_null.mjs";
 import { js_block_statement_is } from "./js_block_statement_is.mjs";
-import { not } from "./not.mjs";
-import { list_is } from "./list_is.mjs";
-import { list_get_end } from "./list_get_end.mjs";
 export function js_stack_list_block_is(stack, index_end) {
-  let stack_ = list_get_end(stack, index_end);
-  let a = list_is(stack_);
-  if (not(a)) {
-    return false;
-  }
-  let index_from_end = add(index_end, 1);
-  let stack_2 = list_get_end(stack, index_from_end);
-  let a2 = js_block_statement_is(stack_2);
-  if (not(a2)) {
-    return false;
-  }
-  return true;
+  let owner = js_stack_list_owner_or_null(stack, index_end);
+  let block_is = js_block_statement_is(owner);
+  return block_is;
 }
