213ea11158d0478124e8916666a5e6359d3a9619
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 17:15:48 +0800

    function_nested_lift app_code_lesson_expression_remainder_subtract title_name_id app_code_lesson_expression_remainder_subtract_title_name_id

---
+++ js/app_code_lesson_expression_remainder_subtract_title_name_id.mjs
@@ -0,0 +1,24 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
+import { html_span_text } from "./html_span_text.mjs";
+import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
+import { app_code_category_operators } from "./app_code_category_operators.mjs";
+import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
+export function app_code_lesson_expression_remainder_subtract_title_name_id(
+  percent,
+) {
+  arguments_assert(arguments, 1);
+  ("the home title is console.log remainder by subtracting %");
+  function title_get(lesson_name, left_upper) {
+    function render(parent) {
+      app_code_lesson_name_id_category(parent, left_upper);
+      html_span_text(parent, "Remainder by subtracting ");
+      html_span_text_code_dark(parent, percent);
+    }
+    return render;
+  }
+  let rights = ["remainder by subtracting"];
+  let left = app_code_category_operators();
+  let built = app_code_lesson_name_id_generic(rights, left, title_get);
+  return built;
+}
