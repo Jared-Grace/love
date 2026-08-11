a2371953e5b4f9a677efaf555736c89cced8a024
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:38 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_string_trichotomy_title_name_id

--- js/app_code_lesson_expression_string_trichotomy_title_name_id.mjs
+++ js/app_code_lesson_expression_string_trichotomy_title_name_id.mjs
@@ -1,10 +1,9 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { app_code_operators_shape_list } from "./app_code_operators_shape_list.mjs";
 import { app_code_placeholder_tile_string } from "./app_code_placeholder_tile_string.mjs";
 import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_expression_string_trichotomy_title_name_id(
   equals_operator,
   less_operator,
@@ -12,20 +11,16 @@ export function app_code_lesson_expression_string_trichotomy_title_name_id(
 ) {
   arguments_assert(arguments, 3);
   ("the home title: same, before, or after");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      html_span_text(parent, "Same, before, or after ");
-      app_code_operators_shape_list(parent, app_code_placeholder_tile_string, [
-        equals_operator,
-        less_operator,
-        greater_operator,
-      ]);
-    }
-    return render;
+  function paint(parent) {
+    html_span_text(parent, "Same, before, or after ");
+    app_code_operators_shape_list(parent, app_code_placeholder_tile_string, [
+      equals_operator,
+      less_operator,
+      greater_operator,
+    ]);
   }
   let rights = ["string", "trichotomy"];
   let left = app_code_category_expressions();
-  let built = app_code_lesson_name_id_generic(rights, left, title_get);
+  let built = app_code_lesson_name_id_category_then(rights, left, paint);
   return built;
 }
