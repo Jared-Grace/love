ba793065ca2445bd6efa31c111f9aab29ef0f5f9
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 16:52:00 +0800

    function_nested_lift app_code_lesson_expression_round_generic title_name_id app_code_lesson_expression_round_generic_title_name_id

--- js/app_code_lesson_expression_round_generic.mjs
+++ js/app_code_lesson_expression_round_generic.mjs
@@ -1,4 +1,4 @@
-import { arguments_assert } from "./arguments_assert.mjs";
+import { app_code_lesson_expression_round_generic_title_name_id } from "./app_code_lesson_expression_round_generic_title_name_id.mjs";
 import { app_code_between_two_wholes } from "./app_code_between_two_wholes.mjs";
 import { app_code_lesson_expression_generic } from "./app_code_lesson_expression_generic.mjs";
 import { list_iterator_refillable } from "./list_iterator_refillable.mjs";
@@ -10,11 +10,8 @@ import { list_map_index } from "./list_map_index.mjs";
 import { list_get } from "./list_get.mjs";
 import { integer_even_is } from "./integer_even_is.mjs";
 import { text_to } from "./text_to.mjs";
-import { text_combine } from "./text_combine.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { property_get } from "./property_get.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
 import { html_div } from "./html_div.mjs";
@@ -191,24 +188,4 @@ export function app_code_lesson_expression_round_generic(params) {
     let v3 = code(whole_stays);
     html_div_cycle_code(whole_para, ["For example, ", v3, " is ", whole_stays]);
   }
-}
-function app_code_lesson_expression_round_generic_title_name_id(
-  direction,
-  called_name,
-) {
-  arguments_assert(arguments, 2);
-  ("the home title: Round {direction} {fn}");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      let heading = text_combine_multiple(["Round ", direction, " "]);
-      html_span_text(parent, heading);
-      html_span_text_code_dark(parent, called_name);
-    }
-    return render;
-  }
-  let heading_lower = text_combine("round ", direction);
-  let rights = [heading_lower];
-  let built = app_code_lesson_name_id_generic(rights, "functions", title_get);
-  return built;
 }
