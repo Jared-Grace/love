b56d2a7d19501f61e0536a6ded43945b605034e4
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:29 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_console_log_nested_generic_title_name_id

--- js/app_code_lesson_console_log_nested_generic_title_name_id.mjs
+++ js/app_code_lesson_console_log_nested_generic_title_name_id.mjs
@@ -1,8 +1,7 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 export function app_code_lesson_console_log_nested_generic_title_name_id(
   symbol,
@@ -10,16 +9,12 @@ export function app_code_lesson_console_log_nested_generic_title_name_id(
 ) {
   arguments_assert(arguments, 2);
   ("the home title is console.log nested <operator>");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      html_span_text(parent, "Nested ");
-      html_span_text_code_dark(parent, symbol);
-    }
-    return render;
+  function paint(parent) {
+    html_span_text(parent, "Nested ");
+    html_span_text_code_dark(parent, symbol);
   }
   let rights = [word];
   let left = app_code_category_expressions();
-  let built = app_code_lesson_name_id_generic(rights, left, title_get);
+  let built = app_code_lesson_name_id_category_then(rights, left, paint);
   return built;
 }
