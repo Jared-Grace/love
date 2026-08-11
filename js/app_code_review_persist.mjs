3cdc218ad4bb8ba85856b45c1fa077522d5957b8
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 17:21:22 +0800

    function_nested_lift app_code_review persist app_code_review_persist

---
+++ js/app_code_review_persist.mjs
@@ -0,0 +1,10 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { storage_local_set_context } from "./storage_local_set_context.mjs";
+export function app_code_review_persist(context, key) {
+  arguments_assert(arguments, 2);
+  let state = {
+    passed,
+    seeds: queue,
+  };
+  storage_local_set_context(context, key, state);
+}
