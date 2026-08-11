f74c6a25159d3c4d8b3cba07b9d2fe6b00a7ff71
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:00:13 +0800

    function_rename app_reply_choices_praise reply_choices_praise

--- js/reply_choices_praise.mjs
+++ js/reply_choices_praise.mjs
@@ -1,5 +1,5 @@
 import { emojis_sing_wrap } from "./emojis_sing_wrap.mjs";
-export function app_reply_choices_praise() {
+export function reply_choices_praise() {
   let lyrics = "Praise the LORD our God in the name of Jesus Christ!";
   let v = emojis_sing_wrap(lyrics);
   return v;
