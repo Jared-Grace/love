5efe6b60c36bbec437a44b99b8128c6f9a78a888
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:41 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_name_id_function

--- js/app_code_lesson_name_id_function.mjs
+++ js/app_code_lesson_name_id_function.mjs
@@ -1,24 +1,16 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { html_span_text_multiple } from "./html_span_text_multiple.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_first_remaining } from "./list_first_remaining.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_name_id_function(left, rights) {
-  let name_get = function app_code_lesson_name_id_function_name_get(
-    lesson_name,
-    left_upper,
-  ) {
-    let r2 = function app_code_lesson_name_id_function_inner(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      let r = list_first_remaining(rights);
-      let remaining = property_get(r, "remaining");
-      let first = property_get(r, "first");
-      html_span_text_code_dark(parent, first);
-      html_span_text_multiple(parent, remaining);
-    };
-    return r2;
+  let name_get = function app_code_lesson_name_id_function_name_get(parent) {
+    let r = list_first_remaining(rights);
+    let remaining = property_get(r, "remaining");
+    let first = property_get(r, "first");
+    html_span_text_code_dark(parent, first);
+    html_span_text_multiple(parent, remaining);
   };
-  let name_id = app_code_lesson_name_id_generic(rights, left, name_get);
+  let name_id = app_code_lesson_name_id_category_then(rights, left, name_get);
   return name_id;
 }
