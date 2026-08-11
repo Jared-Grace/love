7a5db40371f5abef277a34e1899c71b9c1d9f370
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 17:42:17 +0800

    function_nested_lift app_code_lesson_quiz show_correction app_code_lesson_quiz_show_correction

---
+++ js/app_code_lesson_quiz_show_correction.mjs
@@ -0,0 +1,6 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { html_visibility_visible } from "./html_visibility_visible.mjs";
+export function app_code_lesson_quiz_show_correction(container_correction) {
+  arguments_assert(arguments, 1);
+  html_visibility_visible(container_correction);
+}
