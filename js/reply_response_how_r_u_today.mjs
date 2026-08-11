20e17b8d72f9c1b46b1b494f795793ffe263881d
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:00:58 +0800

    ai

--- js/reply_response_how_r_u_today.mjs
+++ js/reply_response_how_r_u_today.mjs
@@ -1,7 +1,7 @@
 import { reply_response_how_r_u_skip } from "./reply_response_how_r_u_skip.mjs";
 import { reply_response_how_r_u_before } from "./reply_response_how_r_u_before.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
-export function app_reply_response_how_r_u_today() {
+export function reply_response_how_r_u_today() {
   let hru = text_combine_multiple([
     reply_response_how_r_u_before(),
     "Today, ",
