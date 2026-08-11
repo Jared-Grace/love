2d7b3ef771a7e6aa2f4aec9b1013c1cb97c74598
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:34 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_remainder_divide_solve_title_name_id

--- js/app_code_lesson_expression_remainder_divide_solve_title_name_id.mjs
+++ js/app_code_lesson_expression_remainder_divide_solve_title_name_id.mjs
@@ -1,20 +1,15 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { html_span_text } from "./html_span_text.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 import { app_code_category_operators } from "./app_code_category_operators.mjs";
 export function app_code_lesson_expression_remainder_divide_solve_title_name_id() {
   arguments_assert(arguments, 0);
   ("the home title is console.log solve the remainder formula");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      html_span_text(parent, "Solve the remainder formula");
-    }
-    return render;
+  function paint(parent) {
+    html_span_text(parent, "Solve the remainder formula");
   }
   let rights = ["solve the remainder formula"];
   let left = app_code_category_operators();
-  let built = app_code_lesson_name_id_generic(rights, left, title_get);
+  let built = app_code_lesson_name_id_category_then(rights, left, paint);
   return built;
 }
