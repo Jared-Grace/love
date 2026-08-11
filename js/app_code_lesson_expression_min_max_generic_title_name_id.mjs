e6649f01ab21d6864e2e9730372b1ed7315ff573
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:40:31 +0800

    app_code_lesson_name_id_category_then_file app_code_lesson_expression_min_max_generic_title_name_id

--- js/app_code_lesson_expression_min_max_generic_title_name_id.mjs
+++ js/app_code_lesson_expression_min_max_generic_title_name_id.mjs
@@ -1,9 +1,8 @@
+import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_name_id_category } from "./app_code_lesson_name_id_category.mjs";
 import { text_combine } from "./text_combine.mjs";
 import { html_span_text } from "./html_span_text.mjs";
 import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
-import { app_code_lesson_name_id_generic } from "./app_code_lesson_name_id_generic.mjs";
 export function app_code_lesson_expression_min_max_generic_title_name_id(
   noun_upper,
   called_name,
@@ -11,17 +10,13 @@ export function app_code_lesson_expression_min_max_generic_title_name_id(
 ) {
   arguments_assert(arguments, 3);
   ("the home title: {noun_upper} of two {fn}");
-  function title_get(lesson_name, left_upper) {
-    function render(parent) {
-      app_code_lesson_name_id_category(parent, left_upper);
-      let heading = text_combine(noun_upper, " of two ");
-      html_span_text(parent, heading);
-      html_span_text_code_dark(parent, called_name);
-    }
-    return render;
+  function paint(parent) {
+    let heading = text_combine(noun_upper, " of two ");
+    html_span_text(parent, heading);
+    html_span_text_code_dark(parent, called_name);
   }
   let right = text_combine(noun, " of two");
   let rights = [right];
-  let built = app_code_lesson_name_id_generic(rights, "functions", title_get);
+  let built = app_code_lesson_name_id_category_then(rights, "functions", paint);
   return built;
 }
