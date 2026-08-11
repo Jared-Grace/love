0c6c1f6a69466cce9116b77b8bbee396d2727cb3
JESUS rose to life <username@users.noreply.github.com>
2026-08-11 19:00:08 +0800

    function_rename app_reply_choices_name reply_choices_name

--- js/app_message_reply_iam_titled_name.mjs
+++ js/app_message_reply_iam_titled_name.mjs
@@ -3,9 +3,9 @@ import { reply_phrase_i_am } from "./reply_phrase_i_am.mjs";
 import { reply_titles_ministry } from "./reply_titles_ministry.mjs";
 import { reply_once_or_more } from "./reply_once_or_more.mjs";
 import { reply_names } from "./reply_names.mjs";
-import { app_reply_choices_name } from "./app_reply_choices_name.mjs";
+import { reply_choices_name } from "./reply_choices_name.mjs";
 export function app_message_reply_iam_titled_name() {
-  let response = app_reply_choices_name();
+  let response = reply_choices_name();
   let names = reply_names();
   let names_once_or_more = reply_once_or_more(names);
   let titles = reply_titles_ministry();
