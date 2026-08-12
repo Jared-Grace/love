d7ecfd7bde406281db37f66df94fa13183df61fe
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 21:56:47 +0800

    ai

--- js/app_shared_button_numbered_progress.mjs
+++ js/app_shared_button_numbered_progress.mjs
@@ -1,3 +1,6 @@
+import { app_shared_text_category_color_variable } from "./app_shared_text_category_color_variable.mjs";
+import { app_shared_text_category_color_on_dark } from "./app_shared_text_category_color_on_dark.mjs";
+import { html_style_variable_set } from "./html_style_variable_set.mjs";
 import { property_get } from "./property_get.mjs";
 import { app_shared_button_numbered } from "./app_shared_button_numbered.mjs";
 import { html_span_text } from "./html_span_text.mjs";
