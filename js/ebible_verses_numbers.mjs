506f1897f7486d5a3a455c04bc93cbc509c615b1
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 22:24:36 +0800

    function_delete_unused ebible_verses_numbers

--- js/ebible_verses_numbers.mjs
+++
@@ -1,8 +0,0 @@
-import { verse_number_key } from "./verse_number_key.mjs";
-import { list_map_property } from "./list_map_property.mjs";
-import { ebible_verses } from "./ebible_verses.mjs";
-export async function ebible_verses_numbers(bible_folder, chapter_code) {
-  let verses = await ebible_verses(bible_folder, chapter_code);
-  let verse_numbers = list_map_property(verses, verse_number_key());
-  return verse_numbers;
-}
