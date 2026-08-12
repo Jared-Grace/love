e394993cc94ae3f622d1877be73a4b8be459a0ff
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 15:54:30 +0800

    function_fold property_js_parse js_imports_relative_named_cases_gate_run

--- js/js_imports_relative_named_cases_gate_run.mjs
+++ js/js_imports_relative_named_cases_gate_run.mjs
@@ -1,6 +1,5 @@
+import { property_js_parse } from "./property_js_parse.mjs";
 import { js_imports_relative_named_cases } from "./js_imports_relative_named_cases.mjs";
-import { property_get } from "./property_get.mjs";
-import { js_parse } from "./js_parse.mjs";
 import { js_imports_relative_named } from "./js_imports_relative_named.mjs";
 import { cases_gate_run_generic } from "./cases_gate_run_generic.mjs";
 export function js_imports_relative_named_cases_gate_run() {
@@ -9,8 +8,7 @@ export function js_imports_relative_named_cases_gate_run() {
   "Throws so the dispatcher seam exits nonzero";
   let cases = js_imports_relative_named_cases();
   function answer(c) {
-    let code = property_get(c, "code");
-    let ast = js_parse(code);
+    let ast = property_js_parse(c, "code");
     let asked = js_imports_relative_named(ast);
     return asked;
   }
