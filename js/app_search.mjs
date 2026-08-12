745ef24aa1ddf8f04f49ef0627e9cd99ecebb0e2
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 13:51:27 +0800

    ai

--- js/app_search.mjs
+++ js/app_search.mjs
@@ -1,6 +1,7 @@
+import { app_shared_app_fn_set } from "./app_shared_app_fn_set.mjs";
+import { app_shared_mobile_default_font_size } from "./app_shared_mobile_default_font_size.mjs";
 import { app_search_home } from "./app_search_home.mjs";
 import { property_set_exists_not } from "./property_set_exists_not.mjs";
-import { html_mobile_default } from "./html_mobile_default.mjs";
 export async function app_search(context) {
   app_shared_app_fn_set(context, app_search);
   app_shared_mobile_default_font_size(context);
