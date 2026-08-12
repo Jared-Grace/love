9f4e998fbe53e4036aa5bf1e3c2bef59b536f5e8
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 13:18:12 +0800

    function_rename app_replace_font_size_adjust_curried app_shared_font_size_adjust_curried

--- js/app_shared_font_size_adjust_curried.mjs
+++ js/app_shared_font_size_adjust_curried.mjs
@@ -1,5 +1,5 @@
 import { app_shared_font_size_adjust } from "../../love/js/app_shared_font_size_adjust.mjs";
-export function app_replace_font_size_adjust_curried(context) {
+export function app_shared_font_size_adjust_curried(context) {
   let r2 = async function app_replace_font_size_adjust_curried_result(
     value_get,
   ) {
