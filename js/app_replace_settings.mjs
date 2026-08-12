31f8cf6168b121c71d52a9905e25fa829708849a
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 13:23:19 +0800

    ai

--- js/app_replace_settings.mjs
+++ js/app_replace_settings.mjs
@@ -1,3 +1,4 @@
+import { app_shared_font_size_buttons } from "./app_shared_font_size_buttons.mjs";
 import { null_is } from "./null_is.mjs";
 import { app_replace_animation_duration_get } from "./app_replace_animation_duration_get.mjs";
 import { app_shared_screen_go } from "./app_shared_screen_go.mjs";
@@ -11,38 +12,12 @@ import { text_combine } from "./text_combine.mjs";
 import { html_div } from "./html_div.mjs";
 import { app_shared_button } from "./app_shared_button.mjs";
 import { app_replace_button_home } from "./app_replace_button_home.mjs";
-import { app_shared_font_size_adjust_curried } from "./app_shared_font_size_adjust_curried.mjs";
-import { multiply_curried_right } from "./multiply_curried_right.mjs";
-import { divide_curried_right } from "./divide_curried_right.mjs";
 import { property_get } from "./property_get.mjs";
-import { app_shared_font_size_factor } from "./app_shared_font_size_factor.mjs";
-import { emoji_font_smaller } from "./emoji_font_smaller.mjs";
-import { emoji_font_larger } from "./emoji_font_larger.mjs";
 export function app_replace_settings(context) {
   let root = property_get(context, "root");
   app_replace_button_home(root, context);
-  let factor = app_shared_font_size_factor();
-  let value_get_multiply = multiply_curried_right(factor);
-  let value_get_divide = divide_curried_right(factor);
+  app_shared_font_size_buttons(root, context);
   let div = html_div(root);
-  let c = app_shared_font_size_adjust_curried(context);
-  async function lambda2() {
-    await c(value_get_multiply);
-  }
-  app_shared_button(
-    div,
-    text_combine(emoji_font_larger(), " Font size larger"),
-    lambda2,
-  );
-  async function lambda3() {
-    await c(value_get_divide);
-  }
-  app_shared_button(
-    div,
-    text_combine(emoji_font_smaller(), " Font size smaller"),
-    lambda3,
-  );
-  let div2 = html_div(root);
   let left = emoji_clock();
   let highlight = app_shared_color_light_green();
   let duration = app_replace_animation_duration_get(context);
@@ -71,15 +46,15 @@ export function app_replace_settings(context) {
       let animation_duration = property_get(choice, "animation_duration");
       let combined2 = text_combine("Animations ", ending);
       let combined = text_combine(left, combined2);
-      function lambda4() {
-        app_shared_screen_go(
+      async function lambda4() {
+        await app_shared_screen_go(
           context,
           "animation_duration",
           animation_duration,
           app_replace_settings,
         );
       }
-      let b = app_shared_button(div2, combined, lambda4);
+      let b = app_shared_button(div, combined, lambda4);
       if (equal(animation_duration, duration)) {
         html_style_background_color_set(b, highlight);
       }
