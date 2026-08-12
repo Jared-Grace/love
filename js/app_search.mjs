6b6a16c8696eba95470b789c43496cb4c059da72
JESUS rose to life <username@users.noreply.github.com>
2026-08-12 13:51:10 +0800

    ai

--- js/app_search.mjs
+++ js/app_search.mjs
@@ -2,7 +2,8 @@ import { app_search_home } from "./app_search_home.mjs";
 import { property_set_exists_not } from "./property_set_exists_not.mjs";
 import { html_mobile_default } from "./html_mobile_default.mjs";
 export async function app_search(context) {
-  html_mobile_default(context);
+  app_shared_app_fn_set(context, app_search);
+  app_shared_mobile_default_font_size(context);
   let languages_chosen = [];
   property_set_exists_not(context, "languages_chosen", languages_chosen);
   await app_search_home(context);
