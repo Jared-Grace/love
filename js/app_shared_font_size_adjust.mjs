2d55ebd679590eb374bcf493cf9d75c43da1534d
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 14:22:44 +0800

    ai

--- js/app_shared_font_size_adjust.mjs
+++ js/app_shared_font_size_adjust.mjs
@@ -1,6 +1,7 @@
-import { app_shared_font_size_refresh } from "../../love/js/app_shared_font_size_refresh.mjs";
-import { storage_local_set_context } from "../../love/js/storage_local_set_context.mjs";
-import { app_shared_font_size } from "../../love/js/app_shared_font_size.mjs";
+import { app_shared_font_size_key } from "./app_shared_font_size_key.mjs";
+import { app_shared_font_size_refresh } from "./app_shared_font_size_refresh.mjs";
+import { storage_local_set_context } from "./storage_local_set_context.mjs";
+import { app_shared_font_size } from "./app_shared_font_size.mjs";
 export async function app_shared_font_size_adjust(context, value_get) {
   let value = await app_shared_font_size(context);
   value = value_get(value);
--- js/app_shared_font_size_refresh.mjs
+++ js/app_shared_font_size_refresh.mjs
@@ -1,7 +1,5 @@
-import { property_get } from "../../love/js/property_get.mjs";
-import { app_shared_font_size } from "../../love/js/app_shared_font_size.mjs";
-import { html_style_font_size } from "../../love/js/html_style_font_size.mjs";
-import { text_combine } from "../../love/js/text_combine.mjs";
+import { app_shared_font_size_refresh_generic } from "./app_shared_font_size_refresh_generic.mjs";
+import { app_shared_font_size } from "./app_shared_font_size.mjs";
 export function app_shared_font_size_refresh(context) {
   let value = app_shared_font_size(context);
   app_shared_font_size_refresh_generic(context, value);
