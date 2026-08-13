d5e9636b4ec4db9114182ac3ab557e4da2759485
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 14:47:10 +0800

    ai

--- js/function_run_output_file_temp_generic.mjs
+++ js/function_run_output_file_temp_generic.mjs
@@ -1,6 +1,7 @@
-import { file_temp_json_open } from "./file_temp_json_open.mjs";
+import { file_temp_value_open } from "./file_temp_value_open.mjs";
 import { function_run_unalias } from "./function_run_unalias.mjs";
 export async function function_run_output_file_temp_generic(f_name, args) {
   let result = await function_run_unalias(f_name, args);
-  await file_temp_json_open(result);
+  let temp_path = await file_temp_value_open(result);
+  return temp_path;
 }
