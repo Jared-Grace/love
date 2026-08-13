f90525204c90da199bcab01f943de03c8ae866f3
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 14:21:59 +0800

    ai

--- js/app_shared_font_size_refresh.mjs
+++ js/app_shared_font_size_refresh.mjs
@@ -3,7 +3,6 @@ import { app_shared_font_size } from "../../love/js/app_shared_font_size.mjs";
 import { html_style_font_size } from "../../love/js/html_style_font_size.mjs";
 import { text_combine } from "../../love/js/text_combine.mjs";
 export function app_shared_font_size_refresh(context) {
-  let root = property_get(context, "root");
   let value = app_shared_font_size(context);
-  html_style_font_size(root, text_combine(value, "px"));
+  app_shared_font_size_refresh_generic(context, value);
 }
