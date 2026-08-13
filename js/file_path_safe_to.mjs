35407826e930d94f2053818f94f624b64cd30bb7
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 14:47:33 +0800

    function_parallel_mark file_path_safe_to

--- js/file_path_safe_to.mjs
+++ js/file_path_safe_to.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function file_path_safe_to(fp) {
+  function_duplicate_kind_parallel();
   let safe = encodeURIComponent(fp);
   return safe;
 }
