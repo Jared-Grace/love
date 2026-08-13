4fdd0d31338fe9f1912edc06657549fe5bc8227c
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 17:17:06 +0800

    ai

--- js/app_replace_rule_set_expressions_member_and_access_abbreviations.mjs
+++ js/app_replace_rule_set_expressions_member_and_access_abbreviations.mjs
@@ -6,6 +6,6 @@ export function app_replace_rule_set_expressions_member_and_access_abbreviations
   app_replace_rule_set_expressions_primary_abbreviations(abbreviations);
   object_merge_set(abbreviations, {
     mae: ["", "m", "ember ", "a", "ccess ", "e", "xpression"],
-    mlh: ["", "m", "ember access ", "l", "eft-hand expression"],
+    mle: ["", "m", "ember access ", "l", "eft-hand ", "e", "xpression"],
   });
 }
