daa6549f5b67da553e35156b35136c11f927e81a
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 14:47:30 +0800

    function_parallel_mark text_combine_middle_space

--- js/text_combine_middle_space.mjs
+++ js/text_combine_middle_space.mjs
@@ -1,5 +1,7 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function text_combine_middle_space(left, right) {
+  function_duplicate_kind_parallel();
   let combined = text_combine_multiple([left, " ", right]);
   return combined;
 }
