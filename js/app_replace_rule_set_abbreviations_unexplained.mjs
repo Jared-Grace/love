56b5eecc66902fe9f4e234146da7c413354da375
JESUS rose to life <username@users.noreply.github.com>
2026-08-13 22:16:19 +0800

    function_fold list_size_greater_than app_replace_rule_set_abbreviations_unexplained

--- js/app_replace_rule_set_abbreviations_unexplained.mjs
+++ js/app_replace_rule_set_abbreviations_unexplained.mjs
@@ -1,3 +1,4 @@
+import { list_size_greater_than } from "./list_size_greater_than.mjs";
 import { property_get } from "./property_get.mjs";
 import { app_replace_rule_set_rules_get } from "./app_replace_rule_set_rules_get.mjs";
 import { app_replace_rules_symbols_rewritten } from "./app_replace_rules_symbols_rewritten.mjs";
@@ -6,7 +7,6 @@ import { object_to_list } from "./object_to_list.mjs";
 import { list_map_property } from "./list_map_property.mjs";
 import { list_difference } from "./list_difference.mjs";
 import { list_size } from "./list_size.mjs";
-import { greater_than } from "./greater_than.mjs";
 import { list_map } from "./list_map.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function app_replace_rule_set_abbreviations_unexplained(rs) {
@@ -20,8 +20,7 @@ export function app_replace_rule_set_abbreviations_unexplained(rs) {
   let explained = list_map_property(list, "key");
   let unexplained = list_difference(rewritten, explained);
   let size = list_size(unexplained);
-  let all = list_size(rewritten);
-  let takes_part = greater_than(all, size);
+  let takes_part = list_size_greater_than(rewritten, size);
   if (takes_part) {
     let rule_set_name = property_get(rs, "name");
     function lambda(symbol) {
