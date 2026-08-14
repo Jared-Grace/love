bdfc5a2d05f83f93f74b15f382fe6c184b26ead9
JESUS rose to life <username@users.noreply.github.com>
2026-08-14 17:27:23 +0800

    file_imports_repair js/app_code_lessons_fns.mjs

--- js/app_code_lessons_fns.mjs
+++ js/app_code_lessons_fns.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_expression_choose_order } from "./app_code_lesson_expression_choose_order.mjs";
 import { app_code_lesson_expression_which_part_first } from "./app_code_lesson_expression_which_part_first.mjs";
 import { app_code_lesson_expression_equal_number_string_true_false } from "./app_code_lesson_expression_equal_number_string_true_false.mjs";
 import { app_code_lesson_expression_parentheses_moved } from "./app_code_lesson_expression_parentheses_moved.mjs";
@@ -195,6 +196,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_expression_not_twice,
     app_code_lesson_expression_not_comparison,
     app_code_lesson_expression_not_equal_same,
+    app_code_lesson_expression_choose_order,
   ];
   return fns;
 }
