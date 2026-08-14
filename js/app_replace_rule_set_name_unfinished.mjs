afa18b3dc2ec9a8815e231669f58418cfa112a23
JESUS rose to life <username@users.noreply.github.com>
2026-08-14 09:32:03 +0800

    ai

---
+++ js/app_replace_rule_set_name_unfinished.mjs
@@ -0,0 +1,5 @@
+export function app_replace_rule_set_name_unfinished() {
+  "The name the blank exercise template is left carrying, so that a copy of it nobody has finished naming can be told apart from an exercise that is ready to be played.";
+  let name = "TODO";
+  return name;
+}
