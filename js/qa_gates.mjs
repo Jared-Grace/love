2294a8ac5c9f80b786df8af02866fa36bd161b6e
JESUS rose to life <username@users.noreply.github.com>
2026-08-14 09:21:31 +0800

    file_imports_repair js/qa_gates.mjs

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { g_npc_path_clear_facing_best_cases_gate_run } from "./g_npc_path_clear_facing_best_cases_gate_run.mjs";
 import { js_code_same_meaning_is_cases_gate_run } from "./js_code_same_meaning_is_cases_gate_run.mjs";
 import { app_g_player_walk_cases_gate_run } from "./app_g_player_walk_cases_gate_run.mjs";
 import { app_shared_bible_hash_field_reference_cases_gate_run } from "./app_shared_bible_hash_field_reference_cases_gate_run.mjs";
