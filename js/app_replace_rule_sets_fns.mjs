3d8cf245761dfa96950ee5c3a3ece311390538f2
JESUS rose to life <username@users.noreply.github.com>
2026-08-14 10:05:40 +0800

    ai

--- js/app_replace_rule_sets_fns.mjs
+++ js/app_replace_rule_sets_fns.mjs
@@ -1,5 +1,3 @@
-import { app_replace_rule_set_new_3 } from "./app_replace_rule_set_new_3.mjs";
-import { app_replace_rule_set_new_2 } from "./app_replace_rule_set_new_2.mjs";
 import { app_replace_rule_set_function_declarations } from "./app_replace_rule_set_function_declarations.mjs";
 import { app_replace_rule_set_statements_for } from "./app_replace_rule_set_statements_for.mjs";
 import { app_replace_rule_set_statements_while } from "./app_replace_rule_set_statements_while.mjs";
@@ -146,8 +144,6 @@ export function app_replace_rule_sets_fns() {
     app_replace_rule_set_statements_while,
     app_replace_rule_set_statements_for,
     app_replace_rule_set_function_declarations,
-    app_replace_rule_set_new_2,
-    app_replace_rule_set_new_3,
   ];
   return fns;
 }
