8fad773b9195116d109ba0b4f1a15a58f049af1f
JESUS rose to life <username@users.noreply.github.com>
2026-08-14 14:00:49 +0800

    function_rename app_code_lesson_expression_parentheses_both_sides_wrapped app_code_comparison_side_wrapped

--- js/app_code_comparison_side_wrapped.mjs
+++ js/app_code_comparison_side_wrapped.mjs
@@ -4,7 +4,7 @@ import { property_get } from "./property_get.mjs";
 import { js_code_parenthesis_left } from "./js_code_parenthesis_left.mjs";
 import { js_code_parenthesis_right } from "./js_code_parenthesis_right.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
-export function app_code_lesson_expression_parentheses_both_sides_wrapped() {
+export function app_code_comparison_side_wrapped() {
   arguments_assert(arguments, 0);
   ("one comparison already wrapped in ( and ), with the true or false it works out to");
   let side = app_code_comparison_side();
