a57b84280b4a8da7df5783444ed86cde3e8fc76e
JESUS rose to life <username@users.noreply.github.com>
2026-08-16 13:48:15 +0800

    ai

--- js/app_replace_rule_set_header.mjs
+++ js/app_replace_rule_set_header.mjs
@@ -21,16 +21,17 @@ export function app_replace_rule_set_header(
   let left = emoji_question();
   let hint_text = text_combine(left, "Hint");
   let hint_button = app_shared_button(root, hint_text, on_hint);
-  let value4 = app_replace_rule_set_attribute_hint();
-  html_data_set_test(hint_button, value4);
+  let value = app_replace_rule_set_attribute_hint();
+  html_data_set_test(hint_button, value);
   app_replace_rule_set_title(context);
   let progress = html_progress_bar(root, goal_index, goals_count, "goal");
   let container = property_get(progress, "container");
   html_style_margin_top(container, "0");
   let div_abbreviations = html_div(root);
   let label_rules = html_p(root);
-  return {
+  let r = {
     div_abbreviations,
     label_rules,
   };
+  return r;
 }
