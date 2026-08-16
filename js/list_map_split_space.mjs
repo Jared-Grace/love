8dde21f4ae175a51bbf273b55bf0d6a0a29d9a43
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 07:08:57 +0800

    function_forwarding_remove list_map_split_space

--- js/list_map_split_space.mjs
+++ js/list_map_split_space.mjs
@@ -1,10 +1,6 @@
 import { list_map } from "./list_map.mjs";
 import { text_split_space } from "./text_split_space.mjs";
 export function list_map_split_space(mapped2) {
-  function lambda(item) {
-    let split = text_split_space(item);
-    return split;
-  }
-  let mapped3 = list_map(mapped2, lambda);
+  let mapped3 = list_map(mapped2, text_split_space);
   return mapped3;
 }
