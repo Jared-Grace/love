5f47e16143434ea76b381cbc49c45231f56762fd
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 07:08:54 +0800

    function_forwarding_remove app_code_lesson_symbols_batch_digits

--- js/app_code_lesson_symbols_batch_digits.mjs
+++ js/app_code_lesson_symbols_batch_digits.mjs
@@ -4,11 +4,10 @@ import { integer_positive_random_digits_text } from "./integer_positive_random_d
 import { range_1 } from "./range_1.mjs";
 export function app_code_lesson_symbols_batch_digits() {
   let digit_counts = range_1(5);
-  function lambda(digit_count) {
-    let digit_texts = integer_positive_random_digits_text(digit_count);
-    return digit_texts;
-  }
-  let batch_digits = list_map(digit_counts, lambda);
+  let batch_digits = list_map(
+    digit_counts,
+    integer_positive_random_digits_text,
+  );
   list_shuffle(batch_digits);
   return batch_digits;
 }
