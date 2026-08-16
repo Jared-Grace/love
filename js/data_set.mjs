fcd2a0f1765dc3a38c7ed42d4159e44314d7f3f2
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 07:08:56 +0800

    function_forwarding_remove data_set

--- js/data_set.mjs
+++ js/data_set.mjs
@@ -1,8 +1,4 @@
 import { data_transform } from "./data_transform.mjs";
 export async function data_set(lambda$previous, property_name, d_path) {
-  function lambda(previous) {
-    let v = lambda$previous(previous);
-    return v;
-  }
-  await data_transform(property_name, null, lambda, d_path);
+  await data_transform(property_name, null, lambda$previous, d_path);
 }
