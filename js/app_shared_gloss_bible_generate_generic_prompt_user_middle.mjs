c2ae486c244f8754d2d28d7eb6b56311e9177aef
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 05:49:19 +0800

    ai

--- js/app_shared_gloss_bible_generate_generic_prompt_user_middle.mjs
+++ js/app_shared_gloss_bible_generate_generic_prompt_user_middle.mjs
@@ -6,7 +6,9 @@ export function app_shared_gloss_bible_generate_generic_prompt_user_middle(
   let r = text_combine_multiple([
     "Here is the ",
     language,
-    " passage to output English glosses for: ",
+    " passage to output ",
+    language_reader,
+    " glosses for: ",
   ]);
   return r;
 }
