cc9477a76aecd72eaf839348f8a9a0652a463835
JESUS rose to life <username@users.noreply.github.com>
2026-08-16 09:58:51 +0800

    ai

--- js/html_style_code_dark_nowrap.mjs
+++ js/html_style_code_dark_nowrap.mjs
@@ -1,8 +1,6 @@
-import { html_style_white_space } from "./html_style_white_space.mjs";
+import { html_style_code_nowrap_generic } from "./html_style_code_nowrap_generic.mjs";
 import { html_style_code_dark } from "./html_style_code_dark.mjs";
-import { html_display_inline } from "./html_display_inline.mjs";
 export function html_style_code_dark_nowrap(span) {
-  html_display_inline(span);
-  html_style_white_space(span, "nowrap");
-  html_style_code_dark(span);
+  "this is likely interweaving text and code, so code should not wrap";
+  html_style_code_nowrap_generic(span, html_style_code_dark);
 }
