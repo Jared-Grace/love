3817946225e325f91370ab04f30ee1a05007f236
JESUS rose to life <username@users.noreply.github.com>
2026-08-16 19:01:13 +0800

    ai

---
+++ js/app_code_expression_chosen_code_nowrap.mjs
@@ -0,0 +1,6 @@
+import { html_style_code_nowrap_generic } from "./html_style_code_nowrap_generic.mjs";
+import { app_code_expression_chosen_code } from "./app_code_expression_chosen_code.mjs";
+export function app_code_expression_chosen_code_nowrap(span) {
+  "a picked-out piece of code standing in a run of words, kept on one line";
+  html_style_code_nowrap_generic(span, app_code_expression_chosen_code);
+}
