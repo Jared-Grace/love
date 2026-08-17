ca08d0351cf7e2c77719a977d980b7548afabd39
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:06:47 +0800

    function_functionize app_reply_main_shortcuts r_te_ bn app_reply_main_shortcuts_bn

--- js/app_reply_main_shortcuts.mjs
+++ js/app_reply_main_shortcuts.mjs
@@ -1,4 +1,5 @@
 import { property_get } from "./property_get.mjs";
+import { app_reply_main_shortcuts_bn } from "./app_reply_main_shortcuts_bn.mjs";
 import { app_reply_main_shortcuts_r_te_ } from "./app_reply_main_shortcuts_r_te_.mjs";
 import { app_reply_main_shortcuts_r_te_base } from "./app_reply_main_shortcuts_r_te_base.mjs";
 import { app_reply_main_shortcuts_r_default } from "./app_reply_main_shortcuts_r_default.mjs";
@@ -9,7 +10,6 @@ import { app_reply_main_shortcuts_shortcut_extend_count } from "./app_reply_main
 import { app_reply_main_shortcuts_shortcut_extend_response } from "./app_reply_main_shortcuts_shortcut_extend_response.mjs";
 import { app_reply_main_shortcuts_shortcut_extend } from "./app_reply_main_shortcuts_shortcut_extend.mjs";
 import { ebible_language_arabic } from "./ebible_language_arabic.mjs";
-import { ebible_language_bengali } from "./ebible_language_bengali.mjs";
 import { ebible_language_english } from "./ebible_language_english.mjs";
 import { app_reply_languages_chosen_default } from "./app_reply_languages_chosen_default.mjs";
 import { each } from "./each.mjs";
@@ -26,25 +26,26 @@ export function app_reply_main_shortcuts(
   let r3 = app_reply_main_shortcuts_r_default(r2, languages_chosen_default);
   let r4 = app_reply_main_shortcuts_r_te_base(r3, languages_chosen_default);
   let r5 = app_reply_main_shortcuts_r_te_(r4);
-  let r_te_ = property_get(r5, "r_te_");
-  let r_default = property_get(r5, "r_default");
-  let r_ke_40 = property_get(r5, "r_ke_40");
-  let r_ug_40 = property_get(r5, "r_ug_40");
-  let r_ug_ = property_get(r5, "r_ug_");
-  let gl = property_get(r5, "gl");
-  let r_pk_ = property_get(r5, "r_pk_");
-  let r_pk_10 = property_get(r5, "r_pk_10");
-  let r_pk_10_g = property_get(r5, "r_pk_10_g");
-  let r_pk_10_gh = property_get(r5, "r_pk_10_gh");
-  let r_pk_10_h = property_get(r5, "r_pk_10_h");
-  let r_pk_1_c = property_get(r5, "r_pk_1_c");
-  let r_pk_1_m = property_get(r5, "r_pk_1_m");
-  let r_pk_1_w = property_get(r5, "r_pk_1_w");
-  let r_pk_10_glory = property_get(r5, "r_pk_10_glory");
-  let r_pk_20 = property_get(r5, "r_pk_20");
-  let r_pk_40 = property_get(r5, "r_pk_40");
-  let r_ke_ = property_get(r5, "r_ke_");
-  let bn = ebible_language_bengali();
+  let r6 = app_reply_main_shortcuts_bn(r5);
+  let bn = property_get(r6, "bn");
+  let r_ke_ = property_get(r6, "r_ke_");
+  let r_pk_40 = property_get(r6, "r_pk_40");
+  let r_pk_20 = property_get(r6, "r_pk_20");
+  let r_pk_10_glory = property_get(r6, "r_pk_10_glory");
+  let r_pk_1_w = property_get(r6, "r_pk_1_w");
+  let r_pk_1_m = property_get(r6, "r_pk_1_m");
+  let r_pk_1_c = property_get(r6, "r_pk_1_c");
+  let r_pk_10_h = property_get(r6, "r_pk_10_h");
+  let r_pk_10_gh = property_get(r6, "r_pk_10_gh");
+  let r_pk_10_g = property_get(r6, "r_pk_10_g");
+  let r_pk_10 = property_get(r6, "r_pk_10");
+  let r_pk_ = property_get(r6, "r_pk_");
+  let gl = property_get(r6, "gl");
+  let r_ug_ = property_get(r6, "r_ug_");
+  let r_ug_40 = property_get(r6, "r_ug_40");
+  let r_ke_40 = property_get(r6, "r_ke_40");
+  let r_default = property_get(r6, "r_default");
+  let r_te_ = property_get(r6, "r_te_");
   let r_bn_base = {
     name: "BN",
     languages: app_reply_main_shortcuts_languages_default_concat_single(
