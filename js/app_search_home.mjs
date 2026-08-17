f053912000b6373a86c0053e86427d856d8128a2
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:23:49 +0800

    function_functionize app_search_home r query_hash app_search_home_query_hash

--- js/app_search_home.mjs
+++ js/app_search_home.mjs
@@ -1,12 +1,10 @@
 import { property_get } from "./property_get.mjs";
-import { app_search_home_r } from "./app_search_home_r.mjs";
+import { app_search_home_query_hash } from "./app_search_home_query_hash.mjs";
 import { app_shared_language_hash_unknown_page_shown_is } from "./app_shared_language_hash_unknown_page_shown_is.mjs";
 import { app_shared_contact_button } from "./app_shared_contact_button.mjs";
 import { not_equal_loose } from "./not_equal_loose.mjs";
 import { html_hash_object_get } from "./html_hash_object_get.mjs";
-import { property_get_or_null } from "./property_get_or_null.mjs";
 import { html_value_set } from "./html_value_set.mjs";
-import { app_search_query_hash_key } from "./app_search_query_hash_key.mjs";
 import { app_search_query_hash_word_gap } from "./app_search_query_hash_word_gap.mjs";
 import { text_replace_to_space } from "./text_replace_to_space.mjs";
 export async function app_search_home(context) {
@@ -19,12 +17,11 @@ export async function app_search_home(context) {
   if (unknown_shown) {
     return;
   }
-  let r = app_search_home_r(context, hash);
-  let search = property_get(r, "search");
-  let input = property_get(r, "input");
+  let r = app_search_home_query_hash(context, hash);
+  let query_hash = property_get(r, "query_hash");
   let content = property_get(r, "content");
-  let property = app_search_query_hash_key();
-  let query_hash = property_get_or_null(hash, property);
+  let input = property_get(r, "input");
+  let search = property_get(r, "search");
   if (not_equal_loose(query_hash, null)) {
     let plus = app_search_query_hash_word_gap();
     let query_from_hash = text_replace_to_space(query_hash, plus);
