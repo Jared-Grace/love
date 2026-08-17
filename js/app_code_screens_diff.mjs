3cfc0c9756837aa14f8c1da5cb5ac9387233e0fe
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:40:01 +0800

    function_functionize app_code_screens_diff check_current removed app_code_screens_diff_removed

--- js/app_code_screens_diff.mjs
+++ js/app_code_screens_diff.mjs
@@ -1,4 +1,5 @@
 import { property_get } from "./property_get.mjs";
+import { app_code_screens_diff_removed } from "./app_code_screens_diff_removed.mjs";
 import { app_code_screens_diff_check_current } from "./app_code_screens_diff_check_current.mjs";
 import { app_code_screens_diff_added } from "./app_code_screens_diff_added.mjs";
 import { app_code_screens_diff_key_of } from "./app_code_screens_diff_key_of.mjs";
@@ -12,14 +13,12 @@ export async function app_code_screens_diff(baseline_path, current_path) {
   let baseline_json = await file_read(baseline_path);
   let r = await app_code_screens_diff_added(current_path, baseline_json);
   let r2 = app_code_screens_diff_check_current(r);
-  let check_current = property_get(r2, "check_current");
-  let changed = property_get(r2, "changed");
-  let current_map = property_get(r2, "current_map");
-  let current = property_get(r2, "current");
-  let baseline = property_get(r2, "baseline");
-  let added = property_get(r2, "added");
-  each(current, check_current);
-  let removed = [];
+  let r3 = app_code_screens_diff_removed(r2);
+  let removed = property_get(r3, "removed");
+  let added = property_get(r3, "added");
+  let baseline = property_get(r3, "baseline");
+  let current_map = property_get(r3, "current_map");
+  let changed = property_get(r3, "changed");
   function check_removed(record) {
     let key = app_code_screens_diff_key_of(record);
     let still = property_exists(current_map, key);
