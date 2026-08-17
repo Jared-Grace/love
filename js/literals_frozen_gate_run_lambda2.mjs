d3694ec8fee3603bd0d111a6098ef63f86454550
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 11:13:53 +0800

    function_nested_lift_or_wrapper_run literals_frozen_gate_run lambda2 literals_frozen_gate_run_lambda2

---
+++ js/literals_frozen_gate_run_lambda2.mjs
@@ -0,0 +1,9 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+import { list_includes_not } from "./list_includes_not.mjs";
+export function literals_frozen_gate_run_lambda2(f_name, recorded, arrived) {
+  arguments_assert(arguments, 3);
+  let was = property_get(recorded, f_name);
+  let n = list_includes_not(arrived, was);
+  return n;
+}
