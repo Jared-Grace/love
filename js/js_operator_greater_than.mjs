e41be45992e36b1d236da4ec4eb5e14780818455
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 18:38:18 +0800

    ai

--- js/js_operator_greater_than.mjs
+++ js/js_operator_greater_than.mjs
@@ -1,6 +1,6 @@
 import { greater_than } from "./greater_than.mjs";
 export function js_operator_greater_than() {
-  let operator = ">";
+  let operator = js_operator_greater_than_symbol();
   let fn = greater_than;
   let o = {
     fn,
