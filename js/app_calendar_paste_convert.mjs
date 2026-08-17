31a196a8fe59989bf9e66ef7843c2f3993b50e15
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:17:13 +0800

    function_functionize app_calendar_paste_convert r condition app_calendar_paste_convert_condition

--- js/app_calendar_paste_convert.mjs
+++ js/app_calendar_paste_convert.mjs
@@ -1,25 +1,18 @@
 import { property_get } from "./property_get.mjs";
-import { app_calendar_paste_convert_minutes_labelled } from "./app_calendar_paste_convert_minutes_labelled.mjs";
-import { greater_than_equal } from "./greater_than_equal.mjs";
+import { app_calendar_paste_convert_condition } from "./app_calendar_paste_convert_condition.mjs";
 import { country_philippines } from "./country_philippines.mjs";
-import { word_count_pluralize } from "./word_count_pluralize.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { ternary } from "./ternary.mjs";
 export function app_calendar_paste_convert(input, country) {
   let speaker_country = country_philippines();
-  let r = app_calendar_paste_convert_minutes_labelled(
-    speaker_country,
-    input,
-    country,
-  );
-  let minutes_labelled = property_get(r, "minutes_labelled");
-  let hours = property_get(r, "hours");
-  let start = property_get(r, "start");
+  let r = app_calendar_paste_convert_condition(speaker_country, input, country);
+  let condition = property_get(r, "condition");
+  let hours_labelled = property_get(r, "hours_labelled");
   let list = property_get(r, "list");
-  let hours_labelled = word_count_pluralize(hours, "hour");
-  let condition = greater_than_equal(hours, 1);
+  let start = property_get(r, "start");
+  let minutes_labelled = property_get(r, "minutes_labelled");
   let duration = ternary(condition, hours_labelled, minutes_labelled);
   let combined = text_combine_multiple([
     "Meeting is scheduled to last up to: ",
