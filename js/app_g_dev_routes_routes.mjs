4a3b5daa7fce56b3b22cbabc1ae286e4f2905dc8
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:36:03 +0800

    function_functionize app_g_dev_routes believe routes app_g_dev_routes_routes

--- js/app_g_dev_routes_routes.mjs
+++ js/app_g_dev_routes_routes.mjs
@@ -1,22 +1,22 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
-export function app_g_dev_routes_routes(r8, day_conversation, hru) {
+export function app_g_dev_routes_routes(r, day_conversation, hru) {
   arguments_assert(arguments, 3);
-  let believe = property_get(r8, "believe");
-  let gospel_share = property_get(r8, "gospel_share");
-  let quick = property_get(r8, "quick");
-  let unbeliever = property_get(r8, "unbeliever");
-  let day_unbelievers = property_get(r8, "day_unbelievers");
-  let study = property_get(r8, "study");
-  let pray = property_get(r8, "pray");
-  let day_parts = property_get(r8, "day_parts");
-  let day_hours = property_get(r8, "day_hours");
-  let day_baptisms_collect = property_get(r8, "day_baptisms_collect");
-  let discern = property_get(r8, "discern");
-  let dove = property_get(r8, "dove");
-  let gratitude = property_get(r8, "gratitude");
-  let disciple = property_get(r8, "disciple");
-  let characters = property_get(r8, "characters");
+  let believe = property_get(r, "believe");
+  let gospel_share = property_get(r, "gospel_share");
+  let quick = property_get(r, "quick");
+  let unbeliever = property_get(r, "unbeliever");
+  let day_unbelievers = property_get(r, "day_unbelievers");
+  let study = property_get(r, "study");
+  let pray = property_get(r, "pray");
+  let day_parts = property_get(r, "day_parts");
+  let day_hours = property_get(r, "day_hours");
+  let day_baptisms_collect = property_get(r, "day_baptisms_collect");
+  let discern = property_get(r, "discern");
+  let dove = property_get(r, "dove");
+  let gratitude = property_get(r, "gratitude");
+  let disciple = property_get(r, "disciple");
+  let characters = property_get(r, "characters");
   let routes = {
     study,
     unbeliever,
