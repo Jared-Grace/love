cc20e37370ff3e65e7bb3bd29a90a691229096e5
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:11:45 +0800

    function_functionize app_code_lesson_quiz r4 lcli app_code_lesson_quiz_lcli

--- js/app_code_lesson_quiz.mjs
+++ js/app_code_lesson_quiz.mjs
@@ -1,4 +1,4 @@
-import { app_code_lesson_quiz_quiz_index } from "./app_code_lesson_quiz_quiz_index.mjs";
+import { app_code_lesson_quiz_lcli } from "./app_code_lesson_quiz_lcli.mjs";
 import { app_code_lesson_quiz_render_next } from "./app_code_lesson_quiz_render_next.mjs";
 import { app_code_lesson_quiz_lambda } from "./app_code_lesson_quiz_lambda.mjs";
 import { app_code_lesson_quiz_on_next } from "./app_code_lesson_quiz_on_next.mjs";
@@ -6,21 +6,17 @@ import { list_size } from "./list_size.mjs";
 import { app_code_progress_quiz_correct_record } from "./app_code_progress_quiz_correct_record.mjs";
 import { app_code_lesson_quiz_show_correction } from "./app_code_lesson_quiz_show_correction.mjs";
 import { app_code_lesson_quiz_render_correction } from "./app_code_lesson_quiz_render_correction.mjs";
-import { app_code_review_due_is } from "./app_code_review_due_is.mjs";
 import { app_shared_button_wide_text_combine } from "./app_shared_button_wide_text_combine.mjs";
 import { sleep_success_color } from "./sleep_success_color.mjs";
 import { app_code_advance_or_no_more } from "./app_code_advance_or_no_more.mjs";
 import { not } from "./not.mjs";
-import { app_code_lesson_current_last_is } from "./app_code_lesson_current_last_is.mjs";
 import { app_code_lesson_quiz_qa_question } from "./app_code_lesson_quiz_qa_question.mjs";
 import { html_visibility_hidden } from "./html_visibility_hidden.mjs";
 import { html_visibility_visible } from "./html_visibility_visible.mjs";
 import { html_clear } from "./html_clear.mjs";
 import { app_shared_button_back_text } from "./app_shared_button_back_text.mjs";
-import { list_index_last_is } from "./list_index_last_is.mjs";
 import { greater_than_equal_1 } from "./greater_than_equal_1.mjs";
 import { app_shared_button_wide } from "./app_shared_button_wide.mjs";
-import { app_code_lesson_current_number } from "./app_code_lesson_current_number.mjs";
 import { app_shared_success_message } from "./app_shared_success_message.mjs";
 import { property_get } from "./property_get.mjs";
 import { text_combine } from "./text_combine.mjs";
@@ -40,32 +36,32 @@ export function app_code_lesson_quiz(
   next_get,
 ) {
   let question_label = property_get(info, "question_label");
-  let r4 = app_code_lesson_quiz_quiz_index(
+  let r4 = app_code_lesson_quiz_lcli(
     info,
     qa,
     container_blue_light,
     question_label,
     parent,
     context,
+    quizzes,
   );
-  let quiz_index = property_get(r4, "quiz_index");
-  let container_success_message = property_get(r4, "container_success_message");
-  let container_correction = property_get(r4, "container_correction");
-  let parent_container = property_get(r4, "parent_container");
-  let answers_div = property_get(r4, "answers_div");
-  let answer_label_set = property_get(r4, "answer_label_set");
-  let container_question = property_get(r4, "container_question");
-  let quiz_question = property_get(r4, "quiz_question");
-  let qa_for = property_get(r4, "qa_for");
-  let answer_property = property_get(r4, "answer_property");
-  let correction_render = property_get(r4, "correction_render");
-  let on_answer = property_get(r4, "on_answer");
-  let answer_label = property_get(r4, "answer_label");
+  let lcli = property_get(r4, "lcli");
+  let has_review = property_get(r4, "has_review");
+  let qli = property_get(r4, "qli");
   let on_question = property_get(r4, "on_question");
-  let qli = list_index_last_is(quizzes, quiz_index);
-  let number = app_code_lesson_current_number(context);
-  let has_review = app_code_review_due_is(number);
-  let lcli = app_code_lesson_current_last_is(context);
+  let answer_label = property_get(r4, "answer_label");
+  let on_answer = property_get(r4, "on_answer");
+  let correction_render = property_get(r4, "correction_render");
+  let answer_property = property_get(r4, "answer_property");
+  let qa_for = property_get(r4, "qa_for");
+  let quiz_question = property_get(r4, "quiz_question");
+  let container_question = property_get(r4, "container_question");
+  let answer_label_set = property_get(r4, "answer_label_set");
+  let answers_div = property_get(r4, "answers_div");
+  let parent_container = property_get(r4, "parent_container");
+  let container_correction = property_get(r4, "container_correction");
+  let container_success_message = property_get(r4, "container_success_message");
+  let quiz_index = property_get(r4, "quiz_index");
   let no_more = lcli && not(has_review);
   async function on_next() {
     let r = await app_code_lesson_quiz_on_next(qli, context, quizzes, refresh);
