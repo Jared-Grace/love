26bf9d7bb3937e6ea85257be312580ec2e03a64c
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:13:59 +0800

    function_functionize app_code_lesson_quiz r container_question app_code_lesson_quiz_container_question

--- js/app_code_lesson_quiz.mjs
+++ js/app_code_lesson_quiz.mjs
@@ -32,22 +32,7 @@ export function app_code_lesson_quiz(
   next_get,
 ) {
   let question_label = property_get(info, "question_label");
-  let {
-    on_question,
-    on_answer,
-    correction_render,
-    r3,
-    on_reveal,
-    qli,
-    answer_label,
-    quiz_index,
-    container_success_message,
-    container_correction,
-    parent_container,
-    answers_div,
-    answer_label_set,
-    container_question,
-  } = app_code_lesson_quiz_container_question(
+  let r = app_code_lesson_quiz_container_question(
     info,
     qa,
     container_blue_light,
@@ -57,6 +42,20 @@ export function app_code_lesson_quiz(
     quizzes,
     refresh,
   );
+  let container_question = property_get(r, "container_question");
+  let answer_label_set = property_get(r, "answer_label_set");
+  let answers_div = property_get(r, "answers_div");
+  let parent_container = property_get(r, "parent_container");
+  let container_correction = property_get(r, "container_correction");
+  let container_success_message = property_get(r, "container_success_message");
+  let quiz_index = property_get(r, "quiz_index");
+  let answer_label = property_get(r, "answer_label");
+  let qli = property_get(r, "qli");
+  let on_reveal = property_get(r, "on_reveal");
+  let r3 = property_get(r, "r3");
+  let correction_render = property_get(r, "correction_render");
+  let on_answer = property_get(r, "on_answer");
+  let on_question = property_get(r, "on_question");
   let quiz_question = property_get(r3, "quiz_question");
   let qa_for = property_get(r3, "qa_for");
   let answer_property = property_get(r3, "answer_property");
