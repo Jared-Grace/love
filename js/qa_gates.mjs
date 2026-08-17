6090e0097490247499404b4de7a1f0324230fbc5
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 07:31:31 +0800

    ai

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -130,6 +130,7 @@ import { js_atomize_function_cases_gate_run } from "./js_atomize_function_cases_
 import { js_function_forwarding_cases_gate_run } from "./js_function_forwarding_cases_gate_run.mjs";
 import { js_function_parameter_call_sizes_cases_gate_run } from "./js_function_parameter_call_sizes_cases_gate_run.mjs";
 import { js_function_forwarding_remove_cases_gate_run } from "./js_function_forwarding_remove_cases_gate_run.mjs";
+import { js_let_add_cases_gate_run } from "./js_let_add_cases_gate_run.mjs";
 import { js_page_serialized_import_uses_cases_gate_run } from "./js_page_serialized_import_uses_cases_gate_run.mjs";
 import { functions_page_auto_borrowing_gate_run } from "./functions_page_auto_borrowing_gate_run.mjs";
 import { functions_locals_unread_gate_run } from "./functions_locals_unread_gate_run.mjs";
@@ -364,6 +365,7 @@ export function qa_gates() {
     js_function_forwarding_cases_gate_run,
     js_function_parameter_call_sizes_cases_gate_run,
     js_function_forwarding_remove_cases_gate_run,
+    js_let_add_cases_gate_run,
     js_function_shape_cases_gate_run,
     js_identifier_rename_cases_gate_run,
     js_identifiers_renamed_without_naming_check_cases_gate_run,
