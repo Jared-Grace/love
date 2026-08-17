cbe45c12cd47198fffc9f5998208e2afc7572195
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:24:21 +0800

    function_functionize app_code_examples lesson example_another app_code_examples_example_another

--- js/app_code_examples.mjs
+++ js/app_code_examples.mjs
@@ -1,7 +1,7 @@
-import { app_code_examples_on_batch_item } from "./app_code_examples_on_batch_item.mjs";
+import { property_get } from "./property_get.mjs";
+import { app_code_examples_example_another } from "./app_code_examples_example_another.mjs";
 import { app_code_examples_lambda } from "./app_code_examples_lambda.mjs";
 import { app_code_lesson_examples_complete_is } from "./app_code_lesson_examples_complete_is.mjs";
-import { app_code_lesson_examples_plural_is } from "./app_code_lesson_examples_plural_is.mjs";
 import { list_add_multiple } from "./list_add_multiple.mjs";
 import { property_equals_not } from "./property_equals_not.mjs";
 import { app_code_lesson_previous_set } from "./app_code_lesson_previous_set.mjs";
@@ -12,59 +12,21 @@ import { app_code_go_back } from "./app_code_go_back.mjs";
 import { emoji_arrow_left } from "./emoji_arrow_left.mjs";
 import { app_code_lesson_first_id } from "./app_code_lesson_first_id.mjs";
 import { app_code_next } from "./app_code_next.mjs";
-import { app_code_quiz } from "./app_code_quiz.mjs";
 import { app_shared_screen_set } from "./app_shared_screen_set.mjs";
-import { noop } from "./noop.mjs";
-import { app_code_batch_on_refill } from "./app_code_batch_on_refill.mjs";
-import { app_code_batch_item_get } from "./app_code_batch_item_get.mjs";
-import { html_div_text } from "./html_div_text.mjs";
 import { text_combine } from "./text_combine.mjs";
-import { app_code_lesson_text_example_another } from "./app_code_lesson_text_example_another.mjs";
-import { app_code_example_answer_gap } from "./app_code_example_answer_gap.mjs";
-import { app_code_container_light_blue } from "./app_code_container_light_blue.mjs";
 import { app_code_container_padded_x } from "./app_code_container_padded_x.mjs";
-import { property_get } from "./property_get.mjs";
-import { app_code_lesson_current } from "./app_code_lesson_current.mjs";
-import { app_code_lesson_title_strip } from "./app_code_lesson_title_strip.mjs";
 import { app_code_button_skip_lesson } from "./app_code_button_skip_lesson.mjs";
 import { html_clear_context } from "./html_clear_context.mjs";
-import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { html_style_margin_top } from "./html_style_margin_top.mjs";
 import { app_shared_spaced_gap } from "./app_shared_spaced_gap.mjs";
 export function app_code_examples(context) {
   let root = html_clear_context(context);
-  let lesson = app_code_lesson_current(context);
-  app_code_lesson_title_strip(root, context, lesson);
-  let above = property_get(lesson, "above");
-  above(root);
-  let c = app_code_container_light_blue(root);
-  app_code_example_answer_gap(c);
-  let another = app_code_lesson_text_example_another(lesson);
-  let plural = app_code_lesson_examples_plural_is(lesson);
-  let root_word = "example";
-  let is_a = null;
-  if (plural) {
-    is_a = text_combine_multiple(["are some ", root_word, "s:"]);
-  } else {
-    is_a = text_combine_multiple(["is an ", root_word, ":"]);
-  }
-  let combined = text_combine("Here ", is_a);
-  html_div_text(c, combined);
-  let on_batch = app_code_batch_on_refill(noop);
-  let refresh = app_code_batch_item_get(
-    c,
-    lesson,
-    on_batch_item,
-    on_batch,
-    false,
-  );
-  function on_batch_item(container, bs) {
-    let r2 = app_code_examples_on_batch_item(container, bs);
-    return r2;
-  }
-  async function example_another() {
-    await app_shared_screen_set(context, app_code_quiz);
-  }
+  let r2 = app_code_examples_example_another(context, root);
+  let example_another = property_get(r2, "example_another");
+  let refresh = property_get(r2, "refresh");
+  let another = property_get(r2, "another");
+  let c = property_get(r2, "c");
+  let lesson = property_get(r2, "lesson");
   let value_initial = app_code_lesson_first_id();
   let lesson_first_not = property_equals_not(lesson, "id", value_initial);
   let on_back = null;
