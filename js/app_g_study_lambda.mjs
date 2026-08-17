7368443a157bfb3f7601dda2dbcb6d534aa21a33
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:37:07 +0800

    function_functionize app_g_study_lambda app_g_menu_clear_back c app_g_study_lambda_c

--- js/app_g_study_lambda.mjs
+++ js/app_g_study_lambda.mjs
@@ -1,14 +1,6 @@
-import { app_g_study_lambda_lambda5 } from "./app_g_study_lambda_lambda5.mjs";
-import { arguments_assert } from "./arguments_assert.mjs";
-import { app_g_menu_clear_back } from "./app_g_menu_clear_back.mjs";
-import { app_g_chapter_code } from "./app_g_chapter_code.mjs";
-import { global_function_property_nested_lambda } from "./global_function_property_nested_lambda.mjs";
-import { app_g_gospel } from "./app_g_gospel.mjs";
-import { list_first } from "./list_first.mjs";
 import { property_get } from "./property_get.mjs";
-import { list_find_property_json } from "./list_find_property_json.mjs";
-import { app_g_main_books } from "./app_g_main_books.mjs";
-import { app_g_passage_to_reference } from "./app_g_passage_to_reference.mjs";
+import { app_g_study_lambda_c } from "./app_g_study_lambda_c.mjs";
+import { arguments_assert } from "./arguments_assert.mjs";
 import { app_g_container_text } from "./app_g_container_text.mjs";
 import { html_bold_mild } from "./html_bold_mild.mjs";
 import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
@@ -29,29 +21,11 @@ import { app_g_button_wrong } from "./app_g_button_wrong.mjs";
 import { invoke_multiple_shuffle_2 } from "./invoke_multiple_shuffle_2.mjs";
 export async function app_g_study_lambda(overlay, player, review, close) {
   arguments_assert(arguments, 4);
-  app_g_menu_clear_back(overlay, player);
-  let chapter_code = app_g_chapter_code();
-  async function lambda5() {
-    let r3 = await app_g_study_lambda_lambda5(chapter_code);
-    return r3;
-  }
-  let sermons = await global_function_property_nested_lambda(
-    app_g_gospel,
-    "sermons",
-    chapter_code,
-    lambda5,
-  );
-  let r = list_first(review);
-  let verse_numbers = property_get(r, "verse_numbers");
-  let passages = property_get(sermons, "passages");
-  let passage = list_find_property_json(
-    passages,
-    "verse_numbers",
-    verse_numbers,
-  );
-  let books = app_g_main_books();
-  let text = app_g_passage_to_reference(passage, chapter_code, books);
-  let c = app_g_container_text(overlay, text);
+  let r3 = await app_g_study_lambda_c(overlay, player, review);
+  let c = property_get(r3, "c");
+  let passage = property_get(r3, "passage");
+  let passages = property_get(r3, "passages");
+  let r = property_get(r3, "r");
   html_bold_mild(c);
   html_style_background_color_set(c, "#ffffffcd");
   app_g_container_text(
