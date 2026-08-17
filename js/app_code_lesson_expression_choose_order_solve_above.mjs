a545898fa97aa2eeaf065b5068d5acaaabfb1a1f
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 13:31:17 +0800

    ai

--- js/app_code_lesson_expression_choose_order_solve_above.mjs
+++ js/app_code_lesson_expression_choose_order_solve_above.mjs
@@ -1,3 +1,5 @@
+import { app_shared_hr_spaced } from "./app_shared_hr_spaced.mjs";
+import { app_code_lesson_expression_choose_order_solve_intro } from "./app_code_lesson_expression_choose_order_solve_intro.mjs";
 import { app_code_container_light_blue } from "./app_code_container_light_blue.mjs";
 import { app_code_expression_code } from "./app_code_expression_code.mjs";
 import { app_code_expression_equals_text } from "./app_code_expression_equals_text.mjs";
