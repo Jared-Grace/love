39898d7d07fcb7d5082290759a38dbb611c22197
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:46:27 +0800

    ai

---
+++ js/js_function_forwarding_removed_name.mjs
@@ -0,0 +1,13 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { js_function_forwarding_removed_mentions } from "./js_function_forwarding_removed_mentions.mjs";
+import { property_get } from "./property_get.mjs";
+export function js_function_forwarding_removed_name(id, ast) {
+  arguments_assert(arguments, 2);
+  let r2 = js_function_forwarding_removed_mentions(id, ast);
+  let mentions = property_get(r2, "mentions");
+  let name = property_get(r2, "name");
+  return {
+    mentions,
+    name,
+  };
+}
