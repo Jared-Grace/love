489d9d2c49f15ef70d150bb2552c9ef7a8645078
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 11:09:54 +0800

    function_nested_lift_or_wrapper_run app_message_reply_choices lambda app_message_reply_choices_lambda

--- js/app_message_reply_choices.mjs
+++ js/app_message_reply_choices.mjs
@@ -1,7 +1,6 @@
-import { property_path_get_2 } from "./property_path_get_2.mjs";
+import { app_message_reply_choices_lambda } from "./app_message_reply_choices_lambda.mjs";
 import { reply_on_match_output_code } from "./reply_on_match_output_code.mjs";
 import { reply_on_match_capture } from "./reply_on_match_capture.mjs";
-import { reply_on_match_output_add_multiple } from "./reply_on_match_output_add_multiple.mjs";
 import { reply_sequence_optional } from "./reply_sequence_optional.mjs";
 import { reply_optional } from "./reply_optional.mjs";
 import { app_message_reply_give } from "./app_message_reply_give.mjs";
@@ -18,10 +17,6 @@ import { reply_choices_glory } from "./reply_choices_glory.mjs";
 import { reply_choice_output } from "./reply_choice_output.mjs";
 import { reply_choices_will_done_fragment } from "./reply_choices_will_done_fragment.mjs";
 import { reply_pray_response } from "./reply_pray_response.mjs";
-import { list_join_empty } from "./list_join_empty.mjs";
-import { list_slice } from "./list_slice.mjs";
-import { property_get } from "./property_get.mjs";
-import { each } from "./each.mjs";
 import { reply_roads } from "./reply_roads.mjs";
 import { digits } from "./digits.mjs";
 import { reply_choice_optional } from "./reply_choice_optional.mjs";
@@ -65,19 +60,8 @@ export function app_message_reply_choices() {
   let cannot_middle = reply_choice_optional(["'", "no"]);
   let fn20 = reply_sequence(["can", cannot_middle, "t"]);
   function lambda(filtered, u) {
-    function lambda2(possibility) {
-      let tokens = property_get(possibility, "tokens");
-      let v = property_path_get_2(possibility, "data", u);
-      let after = property_get(v, "after");
-      let before = property_get(v, "before");
-      let sliced = list_slice(tokens, before, after);
-      let quote = list_join_empty(sliced);
-      reply_on_match_output_add_multiple(possibility, [
-        "If you want, please change the wording of what you said and send me another message. Here is what you said: ",
-        quote,
-      ]);
-    }
-    each(filtered, lambda2);
+    let r2 = app_message_reply_choices_lambda(filtered, u);
+    return r2;
   }
   let matcher = reply_on_match_capture(fn20, lambda);
   let choices_main = reply_choice([
