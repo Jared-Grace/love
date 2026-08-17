258328453911b787b4d173adc5216ea33154ca88
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:17:25 +0800

    function_functionize app_calendar_paste_convert r combined app_calendar_paste_convert_combined

--- js/app_calendar_paste_convert.mjs
+++ js/app_calendar_paste_convert.mjs
@@ -1,20 +1,15 @@
 import { property_get } from "./property_get.mjs";
-import { app_calendar_paste_convert_duration } from "./app_calendar_paste_convert_duration.mjs";
+import { app_calendar_paste_convert_combined } from "./app_calendar_paste_convert_combined.mjs";
 import { country_philippines } from "./country_philippines.mjs";
 import { list_add } from "./list_add.mjs";
 import { list_join_newline } from "./list_join_newline.mjs";
-import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function app_calendar_paste_convert(input, country) {
   let speaker_country = country_philippines();
-  let r = app_calendar_paste_convert_duration(speaker_country, input, country);
-  let duration = property_get(r, "duration");
-  let start = property_get(r, "start");
+  let r = app_calendar_paste_convert_combined(speaker_country, input, country);
+  let combined = property_get(r, "combined");
   let list = property_get(r, "list");
-  let combined = text_combine_multiple([
-    "Meeting is scheduled to last up to: ",
-    duration,
-    " total",
-  ]);
+  let start = property_get(r, "start");
+  let duration = property_get(r, "duration");
   list_add(list, combined);
   let date_time_zones = list_join_newline(list);
   let r5 = {
