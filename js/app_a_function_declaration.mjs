3406e54e20a36c7d8b8da7623f7c62f7ce48eb0a
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 11:08:23 +0800

    function_nested_lift_or_wrapper_run app_a_function_declaration choices_get app_a_function_declaration_choices_get

--- js/app_a_function_declaration.mjs
+++ js/app_a_function_declaration.mjs
@@ -1,13 +1,5 @@
-import { js_node_to_visitor_stack } from "./js_node_to_visitor_stack.mjs";
+import { app_a_function_declaration_choices_get } from "./app_a_function_declaration_choices_get.mjs";
 import { list_size } from "./list_size.mjs";
-import { list_replace_multiple } from "./list_replace_multiple.mjs";
-import { app_a_function_on_change } from "./app_a_function_on_change.mjs";
-import { js_node_type_is_if } from "./js_node_type_is_if.mjs";
-import { list_get_end_2 } from "./list_get_end_2.mjs";
-import { list_get_end_1 } from "./list_get_end_1.mjs";
-import { js_function_declaration_to_block_body } from "./js_function_declaration_to_block_body.mjs";
-import { list_add } from "./list_add.mjs";
-import { app_a_functionize_choices_add } from "./app_a_functionize_choices_add.mjs";
 import { app_a_paste } from "./app_a_paste.mjs";
 import { app_a_overlay_choices } from "./app_a_overlay_choices.mjs";
 import { each } from "./each.mjs";
@@ -36,23 +28,8 @@ export function app_a_function_declaration(a) {
   let k = app_a_keyword_blue_space(parent, kf);
   let keyword = property_get(k, "keyword");
   function choices_get(o, choices) {
-    app_a_functionize_choices_add(choices, a, o);
-    let ast = property_get(a, "ast");
-    let stack = js_node_to_visitor_stack(ast, node);
-    let e2 = list_get_end_2(stack);
-    function lambda6() {
-      list_add(choices, {
-        shortcut: "l",
-        text: "Flatten",
-        fn: async function lambda4() {
-          let e1 = list_get_end_1(stack);
-          let body_block = js_function_declaration_to_block_body(node);
-          list_replace_multiple(e1, node, body_block);
-          await app_a_function_on_change(a, o);
-        },
-      });
-    }
-    js_node_type_is_if(e2, "BlockStatement", lambda6);
+    let r2 = app_a_function_declaration_choices_get(o, choices, a, node);
+    return r2;
   }
   app_a_overlay_choices(a, keyword, choices_get);
   let id = property_get(node, "id");
