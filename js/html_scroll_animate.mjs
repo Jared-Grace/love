e6826d91fc073ffbfb47fb96fa285689516a8195
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:43:35 +0800

    function_functionize html_scroll_animate from_left promise html_scroll_animate_promise

--- js/html_scroll_animate.mjs
+++ js/html_scroll_animate.mjs
@@ -1,15 +1,12 @@
-import { html_scroll_animate_animate } from "./html_scroll_animate_animate.mjs";
+import { html_scroll_animate_promise } from "./html_scroll_animate_promise.mjs";
 export function html_scroll_animate(element, target_left, target_top) {
   let duration = 350;
-  let from_left = element.scrollLeft;
-  let animate = html_scroll_animate_animate(
+  let promise = html_scroll_animate_promise(
     element,
     target_left,
     target_top,
     duration,
-    from_left,
   );
-  let promise = new Promise(animate);
   element.scroll_animation_settle = promise;
   return promise;
 }
---
+++ js/html_scroll_animate_promise.mjs
@@ -0,0 +1,20 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { html_scroll_animate_animate } from "./html_scroll_animate_animate.mjs";
+export function html_scroll_animate_promise(
+  element,
+  target_left,
+  target_top,
+  duration,
+) {
+  arguments_assert(arguments, 4);
+  let from_left = element.scrollLeft;
+  let animate = html_scroll_animate_animate(
+    element,
+    target_left,
+    target_top,
+    duration,
+    from_left,
+  );
+  let promise = new Promise(animate);
+  return promise;
+}
