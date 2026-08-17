cb2f4ed7ee5e5a8498d504098d7cc6af4a288691
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:36:58 +0800

    ai

--- js/g_greeting.mjs
+++ js/g_greeting.mjs
@@ -1,13 +1,14 @@
-import { g_greeting_npc_says } from "./g_greeting_npc_says.mjs";
-import { g_greeting_v } from "./g_greeting_v.mjs";
 import { property_get } from "./property_get.mjs";
+import { g_greeting_sky_remark } from "./g_greeting_sky_remark.mjs";
+import { g_greeting_npc_says } from "./g_greeting_npc_says.mjs";
 import { g_greeting_remark_maybe } from "./g_greeting_remark_maybe.mjs";
 export function g_greeting(met, name_player, time, christian) {
   let r = g_greeting_remark_maybe(time, christian, met);
-  let r3 = g_greeting_v(r, time, christian);
-  let v = property_get(r3, "v");
-  let meet_message = property_get(r3, "meet_message");
-  let sky_remark = property_get(r3, "sky_remark");
+  let r2 = g_greeting_sky_remark(r, time, christian);
+  let sky_remark = property_get(r2, "sky_remark");
+  let meet_message = property_get(r2, "meet_message");
+  let v = property_get(r2, "v");
+  let r3 = property_get(r2, "r3");
   let npc_says = g_greeting_npc_says(
     r3,
     sky_remark,
