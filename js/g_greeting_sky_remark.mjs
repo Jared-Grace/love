e42bb255b121947b5eb5913352f399898301ef89
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:36:58 +0800

    ai

---
+++ js/g_greeting_sky_remark.mjs
@@ -0,0 +1,16 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { g_greeting_v } from "./g_greeting_v.mjs";
+import { property_get } from "./property_get.mjs";
+export function g_greeting_sky_remark(r, time, christian) {
+  arguments_assert(arguments, 3);
+  let r3 = g_greeting_v(r, time, christian);
+  let v = property_get(r3, "v");
+  let meet_message = property_get(r3, "meet_message");
+  let sky_remark = property_get(r3, "sky_remark");
+  return {
+    r3,
+    v,
+    meet_message,
+    sky_remark,
+  };
+}
