0042ef9b967005f05ca6e17388afa4fa94217a37
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:35:27 +0800

    function_functionize app_g_dev_routes believe disciple app_g_dev_routes_disciple

---
+++ js/app_g_dev_routes_disciple.mjs
@@ -0,0 +1,37 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+export function app_g_dev_routes_disciple(r5) {
+  arguments_assert(arguments, 1);
+  let believe = property_get(r5, "believe");
+  let gospel_share = property_get(r5, "gospel_share");
+  let quick = property_get(r5, "quick");
+  let unbeliever = property_get(r5, "unbeliever");
+  let day_unbelievers = property_get(r5, "day_unbelievers");
+  let study = property_get(r5, "study");
+  let pray = property_get(r5, "pray");
+  let day_parts = property_get(r5, "day_parts");
+  let day_hours = property_get(r5, "day_hours");
+  let day_baptisms_collect = property_get(r5, "day_baptisms_collect");
+  let r2 = property_get(r5, "r2");
+  let discern = property_get(r5, "discern");
+  let dove = property_get(r5, "dove");
+  let gratitude = property_get(r5, "gratitude");
+  let disciple = property_get(r2, "disciple");
+  let r = {
+    believe,
+    gospel_share,
+    quick,
+    unbeliever,
+    day_unbelievers,
+    study,
+    pray,
+    day_parts,
+    day_hours,
+    day_baptisms_collect,
+    discern,
+    dove,
+    gratitude,
+    disciple,
+  };
+  return r;
+}
