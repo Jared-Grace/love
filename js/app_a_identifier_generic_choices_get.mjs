75892cb624a55ba3e6f44947a27b77d56caceb9f
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:38:30 +0800

    function_functionize app_a_identifier_generic_choices_get ast includes app_a_identifier_generic_choices_get_includes

--- js/app_a_identifier_generic_choices_get.mjs
+++ js/app_a_identifier_generic_choices_get.mjs
@@ -1,9 +1,9 @@
 import { property_get } from "./property_get.mjs";
+import { app_a_identifier_generic_choices_get_includes } from "./app_a_identifier_generic_choices_get_includes.mjs";
 import { app_a_identifier_generic_choices_get_ast } from "./app_a_identifier_generic_choices_get_ast.mjs";
 import { app_a_identifier_generic_choices_get_lambda4 } from "./app_a_identifier_generic_choices_get_lambda4.mjs";
 import { app_a_identifier_generic_choices_get_lambda6 } from "./app_a_identifier_generic_choices_get_lambda6.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { list_includes } from "./list_includes.mjs";
 import { app_a_function_on_keydown_remove } from "./app_a_function_on_keydown_remove.mjs";
 import { app_a_function_select } from "./app_a_function_select.mjs";
 import { list_add } from "./list_add.mjs";
@@ -30,13 +30,13 @@ export async function app_a_identifier_generic_choices_get(
     choices,
     replace,
   );
-  let ast = property_get(r3, "ast");
-  let stack = property_get(r3, "stack");
-  let f_names = property_get(r3, "f_names");
-  let context = property_get(r3, "context");
-  let overlay_close = property_get(r3, "overlay_close");
-  let e = property_get(r3, "e");
-  let includes = list_includes(f_names, name);
+  let r4 = app_a_identifier_generic_choices_get_includes(r3, name);
+  let includes = property_get(r4, "includes");
+  let e = property_get(r4, "e");
+  let overlay_close = property_get(r4, "overlay_close");
+  let context = property_get(r4, "context");
+  let stack = property_get(r4, "stack");
+  let ast = property_get(r4, "ast");
   if (includes) {
     let choice_function_open = {
       shortcut: "o",
