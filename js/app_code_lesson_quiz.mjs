dc87b1bdb2eafa80074b41dcd60399758fff190f
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:13:22 +0800

    ai

--- js/app_code_lesson_quiz.mjs
+++ js/app_code_lesson_quiz.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_quiz_on_question } from "./app_code_lesson_quiz_on_question.mjs";
 import { app_code_lesson_quiz_answer_label } from "./app_code_lesson_quiz_answer_label.mjs";
 import { app_code_lesson_quiz_lambda } from "./app_code_lesson_quiz_lambda.mjs";
 import { list_size } from "./list_size.mjs";
@@ -44,23 +45,25 @@ export function app_code_lesson_quiz(
     quizzes,
     refresh,
   );
-  let answer_label = property_get(r, "answer_label");
-  let r3 = property_get(r, "r3");
-  let has_next_step = property_get(r, "has_next_step");
-  let render_next = property_get(r, "render_next");
-  let quiz_index = property_get(r, "quiz_index");
-  let container_success_message = property_get(r, "container_success_message");
-  let container_correction = property_get(r, "container_correction");
-  let parent_container = property_get(r, "parent_container");
-  let answers_div = property_get(r, "answers_div");
-  let answer_label_set = property_get(r, "answer_label_set");
-  let container_question = property_get(r, "container_question");
-  let quiz_question = property_get(r, "quiz_question");
-  let qa_for = property_get(r, "qa_for");
-  let answer_property = property_get(r, "answer_property");
-  let correction_render = property_get(r, "correction_render");
-  let on_answer = property_get(r, "on_answer");
-  let on_question = property_get(r3, "on_question");
+  let {
+    answer_label,
+    r3,
+    has_next_step,
+    render_next,
+    quiz_index,
+    container_success_message,
+    container_correction,
+    parent_container,
+    answers_div,
+    answer_label_set,
+    container_question,
+    quiz_question,
+    qa_for,
+    answer_property,
+    correction_render,
+    on_answer,
+    on_question,
+  } = app_code_lesson_quiz_on_question(r);
   let qli = property_get(r3, "qli");
   app_code_advance_or_no_more(parent_container, has_next_step, render_next);
   function on_reveal() {
---
+++ js/app_code_lesson_quiz_on_question.mjs
@@ -0,0 +1,41 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+export function app_code_lesson_quiz_on_question(r) {
+  arguments_assert(arguments, 1);
+  let answer_label = property_get(r, "answer_label");
+  let r3 = property_get(r, "r3");
+  let has_next_step = property_get(r, "has_next_step");
+  let render_next = property_get(r, "render_next");
+  let quiz_index = property_get(r, "quiz_index");
+  let container_success_message = property_get(r, "container_success_message");
+  let container_correction = property_get(r, "container_correction");
+  let parent_container = property_get(r, "parent_container");
+  let answers_div = property_get(r, "answers_div");
+  let answer_label_set = property_get(r, "answer_label_set");
+  let container_question = property_get(r, "container_question");
+  let quiz_question = property_get(r, "quiz_question");
+  let qa_for = property_get(r, "qa_for");
+  let answer_property = property_get(r, "answer_property");
+  let correction_render = property_get(r, "correction_render");
+  let on_answer = property_get(r, "on_answer");
+  let on_question = property_get(r3, "on_question");
+  return {
+    answer_label,
+    r3,
+    has_next_step,
+    render_next,
+    quiz_index,
+    container_success_message,
+    container_correction,
+    parent_container,
+    answers_div,
+    answer_label_set,
+    container_question,
+    quiz_question,
+    qa_for,
+    answer_property,
+    correction_render,
+    on_answer,
+    on_question,
+  };
+}
