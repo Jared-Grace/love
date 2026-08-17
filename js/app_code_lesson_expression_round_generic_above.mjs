d3d8b77ccec92a95c4db17659f9b23d2dc11f2df
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:11:21 +0800

    function_functionize app_code_lesson_expression_round_generic_above r2 trap_render app_code_lesson_expression_round_generic_above_trap_render

--- js/app_code_lesson_expression_round_generic_above.mjs
+++ js/app_code_lesson_expression_round_generic_above.mjs
@@ -1,6 +1,4 @@
-import { property_get } from "./property_get.mjs";
-import { app_code_lesson_expression_round_generic_above_gives_suffix } from "./app_code_lesson_expression_round_generic_above_gives_suffix.mjs";
-import { app_code_lesson_expression_round_generic_above_v } from "./app_code_lesson_expression_round_generic_above_v.mjs";
+import { app_code_lesson_expression_round_generic_above_trap_render } from "./app_code_lesson_expression_round_generic_above_trap_render.mjs";
 import { app_code_lesson_expression_round_generic_above_rounds } from "./app_code_lesson_expression_round_generic_above_rounds.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_code_container_light_blue } from "./app_code_container_light_blue.mjs";
@@ -28,25 +26,13 @@ export function app_code_lesson_expression_round_generic_above(
     metaphor_render,
     introduce_whole_number,
   );
-  let r2 = app_code_lesson_expression_round_generic_above_v(
+  let whole_stays = app_code_lesson_expression_round_generic_above_trap_render(
     r,
     superlative,
     called_name,
-  );
-  let r3 = app_code_lesson_expression_round_generic_above_gives_suffix(
-    r2,
-    called_name,
     preposition,
+    trap_render,
   );
-  let gives_suffix = property_get(r3, "gives_suffix");
-  let rounds = property_get(r3, "rounds");
-  let whole_stays = property_get(r3, "whole_stays");
-  html_div_cycle_code(rounds, [
-    "If a number has a decimal, ",
-    called_name,
-    gives_suffix,
-  ]);
-  trap_render(rounds);
   let whole_para = app_code_container_light_blue(root);
   html_div_cycle_code(whole_para, [
     "",
