e8f7a3672e14d8e94c87b43cd5be664b5834d885
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:39:37 +0800

    function_functionize app_g_view_render_study render_words container app_g_view_render_study_container

--- js/app_g_view_render_study.mjs
+++ js/app_g_view_render_study.mjs
@@ -1,14 +1,15 @@
+import { property_get } from "./property_get.mjs";
+import { app_g_view_render_study_container } from "./app_g_view_render_study_container.mjs";
 import { app_g_view_render_study_render_words } from "./app_g_view_render_study_render_words.mjs";
 import { app_g_view_render_study_render_pray_gate } from "./app_g_view_render_study_render_pray_gate.mjs";
 import { app_g_view_get } from "./app_g_view_get.mjs";
-import { property_get } from "./property_get.mjs";
 export async function app_g_view_render_study(div_map) {
   let view = await app_g_view_get();
   let r2 = app_g_view_render_study_render_words(view, div_map);
-  let render_words = property_get(r2, "render_words");
-  let fresh = property_get(r2, "fresh");
-  let r = property_get(r2, "r");
+  let r = app_g_view_render_study_container(r2);
   let container = property_get(r, "container");
+  let fresh = property_get(r, "fresh");
+  let render_words = property_get(r, "render_words");
   if (fresh) {
     app_g_view_render_study_render_pray_gate(container, render_words);
   } else {
