14fd53015b4a5257490b77d8ceba23437ab4b00b
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:36:53 +0800

    function_functionize g_greeting sky_maybe npc_says g_greeting_npc_says

--- js/g_greeting.mjs
+++ js/g_greeting.mjs
@@ -1,36 +1,19 @@
+import { g_greeting_npc_says } from "./g_greeting_npc_says.mjs";
 import { g_greeting_v } from "./g_greeting_v.mjs";
 import { property_get } from "./property_get.mjs";
 import { g_greeting_remark_maybe } from "./g_greeting_remark_maybe.mjs";
-import { boolean_random_n } from "./boolean_random_n.mjs";
-import { g_random_dot_bang } from "./g_random_dot_bang.mjs";
-import { not } from "./not.mjs";
-import { text_combine } from "./text_combine.mjs";
-import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function g_greeting(met, name_player, time, christian) {
   let r = g_greeting_remark_maybe(time, christian, met);
   let r3 = g_greeting_v(r, time, christian);
   let v = property_get(r3, "v");
   let meet_message = property_get(r3, "meet_message");
   let sky_remark = property_get(r3, "sky_remark");
-  let sky_maybe = property_get(r3, "sky_maybe");
-  let b = property_get(r3, "b");
-  let r2 = property_get(r3, "r2");
-  let remark_maybe = property_get(r2, "remark_maybe");
-  if (not(b)) {
-    let rare = boolean_random_n(6);
-    if (rare) {
-      sky_maybe = text_combine(" ", sky_remark);
-    }
-  }
-  let r10 = g_random_dot_bang();
-  let npc_says = text_combine_multiple([
+  let npc_says = g_greeting_npc_says(
+    r3,
+    sky_remark,
     v,
-    " ",
     name_player,
-    r10,
-    remark_maybe,
-    sky_maybe,
     meet_message,
-  ]);
+  );
   return npc_says;
 }
---
+++ js/g_greeting_npc_says.mjs
@@ -0,0 +1,37 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+import { not } from "./not.mjs";
+import { boolean_random_n } from "./boolean_random_n.mjs";
+import { text_combine } from "./text_combine.mjs";
+import { g_random_dot_bang } from "./g_random_dot_bang.mjs";
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
+export function g_greeting_npc_says(
+  r,
+  sky_remark,
+  v,
+  name_player,
+  meet_message,
+) {
+  arguments_assert(arguments, 5);
+  let sky_maybe = property_get(r, "sky_maybe");
+  let b = property_get(r, "b");
+  let r2 = property_get(r, "r2");
+  let remark_maybe = property_get(r2, "remark_maybe");
+  if (not(b)) {
+    let rare = boolean_random_n(6);
+    if (rare) {
+      sky_maybe = text_combine(" ", sky_remark);
+    }
+  }
+  let r10 = g_random_dot_bang();
+  let npc_says = text_combine_multiple([
+    v,
+    " ",
+    name_player,
+    r10,
+    remark_maybe,
+    sky_maybe,
+    meet_message,
+  ]);
+  return npc_says;
+}
