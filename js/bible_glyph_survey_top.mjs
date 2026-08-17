79426a28dbbee64e581d2186b5d307542f57f9a3
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:27:05 +0800

    function_functionize bible_glyph_survey n top bible_glyph_survey_top

---
+++ js/bible_glyph_survey_top.mjs
@@ -0,0 +1,28 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+import { round } from "./round.mjs";
+export function bible_glyph_survey_top(r3) {
+  arguments_assert(arguments, 1);
+  let n = property_get(r3, "n");
+  let roots = property_get(r3, "roots");
+  let mapped = property_get(r3, "mapped");
+  let glyph_missing = property_get(r3, "glyph_missing");
+  let glyph_collisions = property_get(r3, "glyph_collisions");
+  let occurrences_total = property_get(r3, "occurrences_total");
+  let occurrences_mapped = property_get(r3, "occurrences_mapped");
+  let sense_spread = property_get(r3, "sense_spread");
+  let unmapped = property_get(r3, "unmapped");
+  let top = round(n);
+  let r = {
+    roots,
+    mapped,
+    glyph_missing,
+    glyph_collisions,
+    occurrences_total,
+    occurrences_mapped,
+    sense_spread,
+    unmapped,
+    top,
+  };
+  return r;
+}
