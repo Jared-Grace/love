fca010442c8bc06372bcf18d5bd004b669ad27d8
JESUS rose to life <username@users.noreply.github.com>
2026-08-17 21:06:37 +0800

    function_functionize app_reply_main_shortcuts r_te_base r_te_ app_reply_main_shortcuts_r_te_

---
+++ js/app_reply_main_shortcuts_r_te_.mjs
@@ -0,0 +1,46 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+import { app_reply_main_shortcuts_shortcut_extend_count } from "./app_reply_main_shortcuts_shortcut_extend_count.mjs";
+export function app_reply_main_shortcuts_r_te_(r4) {
+  arguments_assert(arguments, 1);
+  let r_te_base = property_get(r4, "r_te_base");
+  let r_ke_ = property_get(r4, "r_ke_");
+  let r_pk_40 = property_get(r4, "r_pk_40");
+  let r_pk_20 = property_get(r4, "r_pk_20");
+  let r_pk_10_glory = property_get(r4, "r_pk_10_glory");
+  let r_pk_1_w = property_get(r4, "r_pk_1_w");
+  let r_pk_1_m = property_get(r4, "r_pk_1_m");
+  let r_pk_1_c = property_get(r4, "r_pk_1_c");
+  let r_pk_10_h = property_get(r4, "r_pk_10_h");
+  let r_pk_10_gh = property_get(r4, "r_pk_10_gh");
+  let r_pk_10_g = property_get(r4, "r_pk_10_g");
+  let r_pk_10 = property_get(r4, "r_pk_10");
+  let r_pk_ = property_get(r4, "r_pk_");
+  let gl = property_get(r4, "gl");
+  let r_ug_ = property_get(r4, "r_ug_");
+  let r_ug_40 = property_get(r4, "r_ug_40");
+  let r_ke_40 = property_get(r4, "r_ke_40");
+  let r_default = property_get(r4, "r_default");
+  let r_te_ = app_reply_main_shortcuts_shortcut_extend_count(r_te_base, 10);
+  let r = {
+    r_ke_,
+    r_pk_40,
+    r_pk_20,
+    r_pk_10_glory,
+    r_pk_1_w,
+    r_pk_1_m,
+    r_pk_1_c,
+    r_pk_10_h,
+    r_pk_10_gh,
+    r_pk_10_g,
+    r_pk_10,
+    r_pk_,
+    gl,
+    r_ug_,
+    r_ug_40,
+    r_ke_40,
+    r_default,
+    r_te_,
+  };
+  return r;
+}
