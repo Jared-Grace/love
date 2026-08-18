963554598c6817f4efe4290e3dc37b6e3880a8f5
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:45:40 +0800

    function_functionize app_g_view_render_study_fresh r container app_g_view_render_study_fresh_container

--- js/app_g_view_render_study_fresh.mjs
+++ js/app_g_view_render_study_fresh.mjs
@@ -1,5 +1,5 @@
 import { property_get } from "./property_get.mjs";
-import { app_g_view_render_study_fresh_fresh } from "./app_g_view_render_study_fresh_fresh.mjs";
+import { app_g_view_render_study_fresh_container } from "./app_g_view_render_study_fresh_container.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 export function app_g_view_render_study_fresh(
   word_index,
@@ -8,10 +8,15 @@ export function app_g_view_render_study_fresh(
   words,
 ) {
   arguments_assert(arguments, 4);
-  let r = app_g_view_render_study_fresh_fresh(word_index, text, overlay, words);
-  let fresh = property_get(r, "fresh");
-  let render_words = property_get(r, "render_words");
+  let r = app_g_view_render_study_fresh_container(
+    word_index,
+    text,
+    overlay,
+    words,
+  );
   let container = property_get(r, "container");
+  let render_words = property_get(r, "render_words");
+  let fresh = property_get(r, "fresh");
   let r2 = {
     container,
     render_words,
---
+++ js/app_g_view_render_study_fresh_container.mjs
@@ -0,0 +1,21 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { app_g_view_render_study_fresh_fresh } from "./app_g_view_render_study_fresh_fresh.mjs";
+import { property_get } from "./property_get.mjs";
+export function app_g_view_render_study_fresh_container(
+  word_index,
+  text,
+  overlay,
+  words,
+) {
+  arguments_assert(arguments, 4);
+  let r = app_g_view_render_study_fresh_fresh(word_index, text, overlay, words);
+  let fresh = property_get(r, "fresh");
+  let render_words = property_get(r, "render_words");
+  let container = property_get(r, "container");
+  let r2 = {
+    fresh,
+    render_words,
+    container,
+  };
+  return r2;
+}
