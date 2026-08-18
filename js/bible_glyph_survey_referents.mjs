1788ec809611865a72cfb925b775bd3d8167981d
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 16:10:20 +0800

    file_imports_repair js/bible_glyph_survey_referents.mjs

--- js/bible_glyph_survey_referents.mjs
+++ js/bible_glyph_survey_referents.mjs
@@ -1,9 +1,10 @@
+import { bible_glyph_referents_testament_table } from "./bible_glyph_referents_testament_table.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { bible_glyph_survey_percent } from "./bible_glyph_survey_percent.mjs";
 import { property_get } from "./property_get.mjs";
 import { bible_glyph_referents } from "./bible_glyph_referents.mjs";
-export function bible_glyph_survey_referents(r) {
-  arguments_assert(arguments, 1);
+export function bible_glyph_survey_referents(r, testament_name) {
+  arguments_assert(arguments, 2);
   let r2 = bible_glyph_survey_percent(r);
   let percent = property_get(r2, "percent");
   let roots = property_get(r2, "roots");
@@ -12,7 +13,7 @@ export function bible_glyph_survey_referents(r) {
   let unmapped = property_get(r2, "unmapped");
   let sense_spread = property_get(r2, "sense_spread");
   let occurrences_mapped = property_get(r2, "occurrences_mapped");
-  let referents = bible_glyph_referents();
+  let referents = bible_glyph_referents_testament_table(testament_name);
   let r3 = {
     percent,
     roots,
