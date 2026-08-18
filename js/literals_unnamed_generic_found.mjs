1dc40502820b5991e5cd25be73cd80eb272ffd4b
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:49:55 +0800

    function_functionize literals_unnamed_generic_found r2 r literals_unnamed_generic_found_r

--- js/literals_unnamed_generic_found.mjs
+++ js/literals_unnamed_generic_found.mjs
@@ -1,15 +1,8 @@
-import { property_get } from "./property_get.mjs";
-import { literals_unnamed_generic_found_found } from "./literals_unnamed_generic_found_found.mjs";
+import { literals_unnamed_generic_found_r } from "./literals_unnamed_generic_found_r.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 export function literals_unnamed_generic_found(getters, codes) {
   arguments_assert(arguments, 2);
   let named = {};
-  let r2 = literals_unnamed_generic_found_found(getters, named, codes);
-  let found = property_get(r2, "found");
-  let files_by_literal = property_get(r2, "files_by_literal");
-  let r = {
-    files_by_literal,
-    found,
-  };
+  let r = literals_unnamed_generic_found_r(getters, named, codes);
   return r;
 }
