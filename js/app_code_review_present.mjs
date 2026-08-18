06f070879f372bd310b9d8725d539f37e91e0ddb
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:46:31 +0800

    function_functionize app_code_review_present key progress app_code_review_present_progress

--- js/app_code_review_present.mjs
+++ js/app_code_review_present.mjs
@@ -28,16 +28,15 @@ import { app_code_review_exercise } from "./app_code_review_exercise.mjs";
 export function app_code_review_present(r9, home_button, context) {
   arguments_assert(arguments, 3);
   let r3 = app_code_review_present_key(r9);
-  let {
-    key,
-    c,
-    success_container,
-    back_button,
-    has_next,
-    skip_button,
-    go_next,
-    progress,
-  } = app_code_review_present_progress(r3);
+  let r2 = app_code_review_present_progress(r3);
+  let progress = property_get(r2, "progress");
+  let go_next = property_get(r2, "go_next");
+  let skip_button = property_get(r2, "skip_button");
+  let has_next = property_get(r2, "has_next");
+  let back_button = property_get(r2, "back_button");
+  let success_container = property_get(r2, "success_container");
+  let c = property_get(r2, "c");
+  let key = property_get(r2, "key");
   let queue = property_get(r9, "queue");
   let passed = property_get(r9, "passed");
   let restart_button = property_get(r9, "restart_button");
