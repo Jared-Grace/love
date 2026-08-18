f7849d1353bf022771ef02fad9cfc2e0e46207f1
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:48:39 +0800

    function_functionize memory_index_hooks_compress_line dash r memory_index_hooks_compress_line_r

--- js/memory_index_hooks_compress_line.mjs
+++ js/memory_index_hooks_compress_line.mjs
@@ -1,4 +1,4 @@
-import { memory_index_hooks_compress_line_line } from "./memory_index_hooks_compress_line_line.mjs";
+import { memory_index_hooks_compress_line_r } from "./memory_index_hooks_compress_line_r.mjs";
 import { memory_index_hooks_compress_line_link_open } from "./memory_index_hooks_compress_line_link_open.mjs";
 import { property_get } from "./property_get.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
@@ -6,13 +6,6 @@ export function memory_index_hooks_compress_line(lines) {
   arguments_assert(arguments, 1);
   let r2 = memory_index_hooks_compress_line_link_open();
   let link_open = property_get(r2, "link_open");
-  let dash = property_get(r2, "dash");
-  let r3 = memory_index_hooks_compress_line_line(r2, lines, link_open, dash);
-  let shortened = property_get(r3, "shortened");
-  let kept = property_get(r3, "kept");
-  let r = {
-    kept,
-    shortened,
-  };
+  let r = memory_index_hooks_compress_line_r(r2, lines, link_open);
   return r;
 }
