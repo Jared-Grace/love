4d6764c41dd6dabe3be525ceb351c23874eb2439
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 20:22:41 +0800

    file_imports_repair js/literals_unnamed_generic_found.mjs

--- js/literals_unnamed_generic_found.mjs
+++ js/literals_unnamed_generic_found.mjs
@@ -1,3 +1,5 @@
+import { literals_unnamed_generic_found_found } from "./literals_unnamed_generic_found_found.mjs";
+import { property_get } from "./property_get.mjs";
 import { literals_unnamed_generic_found_r } from "./literals_unnamed_generic_found_r.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 export function literals_unnamed_generic_found(getters, codes) {
