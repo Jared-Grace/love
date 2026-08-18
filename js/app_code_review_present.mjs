1bc7c95fa83a3e759d18a3be494a405d4495b34c
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:47:03 +0800

    function_functionize app_code_review_present passed restart_button app_code_review_present_restart_button

--- js/app_code_review_present.mjs
+++ js/app_code_review_present.mjs
@@ -1,3 +1,4 @@
+import { app_code_review_present_restart_button } from "./app_code_review_present_restart_button.mjs";
 import { app_code_review_present_passed } from "./app_code_review_present_passed.mjs";
 import { app_code_review_present_has_next } from "./app_code_review_present_has_next.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
@@ -31,15 +32,16 @@ export function app_code_review_present(r9, home_button, context) {
   let has_next = property_get(r2, "has_next");
   let back_button = property_get(r2, "back_button");
   let r3 = app_code_review_present_passed(r2, r9);
-  let passed = property_get(r3, "passed");
-  let progress = property_get(r3, "progress");
-  let go_next = property_get(r3, "go_next");
-  let skip_button = property_get(r3, "skip_button");
-  let queue = property_get(r3, "queue");
-  let key = property_get(r3, "key");
-  let c = property_get(r3, "c");
-  let success_container = property_get(r3, "success_container");
-  let restart_button = property_get(r9, "restart_button");
+  let r4 = app_code_review_present_restart_button(r3, r9);
+  let restart_button = property_get(r4, "restart_button");
+  let success_container = property_get(r4, "success_container");
+  let c = property_get(r4, "c");
+  let key = property_get(r4, "key");
+  let queue = property_get(r4, "queue");
+  let skip_button = property_get(r4, "skip_button");
+  let go_next = property_get(r4, "go_next");
+  let progress = property_get(r4, "progress");
+  let passed = property_get(r4, "passed");
   app_shared_button_gap_above(home_button);
   function present() {
     html_clear(progress);
