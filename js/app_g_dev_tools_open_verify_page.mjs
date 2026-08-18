e8186b372cb7691c6516557db6a7e8e78e5e3a16
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 20:16:10 +0800

    functions_expand_all_delete app_g_dev_tools_open_verify_page_lines

--- js/app_g_dev_tools_open_verify_page.mjs
+++ js/app_g_dev_tools_open_verify_page.mjs
@@ -4,7 +4,17 @@ import { app_g_dev_tools_open_verify_page_engine } from "./app_g_dev_tools_open_
 import { arguments_assert } from "./arguments_assert.mjs";
 export async function app_g_dev_tools_open_verify_page(r) {
   arguments_assert(arguments, 1);
-  let r22 = app_g_dev_tools_open_verify_page_lines(r);
+  let told2 = property_get(r, "told");
+  let engine2 = property_get(r, "engine");
+  let url2 = property_get(r, "url");
+  let lines2 = property_get(r, "lines");
+  let r6 = {
+    told: told2,
+    engine: engine2,
+    url: url2,
+    lines: lines2,
+  };
+  let r22 = r6;
   let lines = property_get(r22, "lines");
   let url = property_get(r22, "url");
   let engine = property_get(r22, "engine");
