64da457e7f84894634a3cd9f2d01a93decaa9c48
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 14:31:18 +0800

    ai

--- js/app_shared_bible_read_chapter_code.mjs
+++ js/app_shared_bible_read_chapter_code.mjs
@@ -1,24 +1,21 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_shared_dismissable_message } from "./app_shared_dismissable_message.mjs";
 import { app_shared_bible_read } from "./app_shared_bible_read.mjs";
-import { app_shared_bible_read_count_foot } from "./app_shared_bible_read_count_foot.mjs";
 import { text_empty_is } from "./text_empty_is.mjs";
 import { app_shared_bible_chapter_code_default } from "./app_shared_bible_chapter_code_default.mjs";
-export function app_shared_bible_read_chapter_code(bar, help_text, shell, c) {
-  arguments_assert(arguments, 4);
+export function app_shared_bible_read_chapter_code(bar, help_text, c) {
+  arguments_assert(arguments, 3);
   let dismiss_help = app_shared_dismissable_message(
     app_shared_bible_read,
     "chapter_help_dismissed",
     bar,
     help_text,
   );
-  let count_status = app_shared_bible_read_count_foot(shell);
   let chapter_code = text_empty_is(c)
     ? app_shared_bible_chapter_code_default()
     : c;
   let r = {
     dismiss_help,
-    count_status,
     chapter_code,
   };
   return r;
--- js/app_shared_bible_read_dismiss_help.mjs
+++ js/app_shared_bible_read_dismiss_help.mjs
@@ -1,15 +1,13 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_shared_bible_read_chapter_code } from "./app_shared_bible_read_chapter_code.mjs";
 import { property_get } from "./property_get.mjs";
-export function app_shared_bible_read_dismiss_help(bar, help_text, shell, c) {
-  arguments_assert(arguments, 4);
-  let r5 = app_shared_bible_read_chapter_code(bar, help_text, shell, c);
+export function app_shared_bible_read_dismiss_help(bar, help_text, c) {
+  arguments_assert(arguments, 3);
+  let r5 = app_shared_bible_read_chapter_code(bar, help_text, c);
   let chapter_code = property_get(r5, "chapter_code");
-  let count_status = property_get(r5, "count_status");
   let dismiss_help = property_get(r5, "dismiss_help");
   let r = {
     chapter_code,
-    count_status,
     dismiss_help,
   };
   return r;
