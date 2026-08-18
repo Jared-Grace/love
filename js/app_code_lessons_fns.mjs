394b6a54947718eeed8e40181ed016376f757cec
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 20:39:54 +0800

    ai

--- js/app_code_lessons_fns.mjs
+++ js/app_code_lessons_fns.mjs
@@ -1,3 +1,4 @@
+import { app_code_lesson_expression_choose_order_both_sides } from "./app_code_lesson_expression_choose_order_both_sides.mjs";
 import { app_code_lesson_statement_name_compare } from "./app_code_lesson_statement_name_compare.mjs";
 import { app_code_lesson_statement_name_copy_kept } from "./app_code_lesson_statement_name_copy_kept.mjs";
 import { app_code_lesson_statement_name_one_more } from "./app_code_lesson_statement_name_one_more.mjs";
