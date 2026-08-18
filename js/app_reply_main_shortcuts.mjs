79821cd766adbd73604da085196426941737e045
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:39:21 +0800

    function_functionize app_reply_main_shortcuts r2 r_ke_ app_reply_main_shortcuts_r_ke_

--- js/app_reply_main_shortcuts.mjs
+++ js/app_reply_main_shortcuts.mjs
@@ -1,3 +1,4 @@
+import { property_get } from "./property_get.mjs";
 import { app_reply_main_shortcuts_r_ke_ } from "./app_reply_main_shortcuts_r_ke_.mjs";
 import { app_reply_main_shortcuts_shortcut_each } from "./app_reply_main_shortcuts_shortcut_each.mjs";
 import { app_reply_main_shortcuts_languages_default_concat_single } from "./app_reply_main_shortcuts_languages_default_concat_single.mjs";
@@ -18,25 +19,24 @@ export function app_reply_main_shortcuts(
   buttons_responses,
 ) {
   let languages_chosen_default = app_reply_languages_chosen_default();
-  let {
-    r_ug_40,
-    r_ug_,
-    gl,
-    te,
-    r_pk_,
-    r_pk_10,
-    r_pk_10_g,
-    r_pk_10_gh,
-    r_pk_10_h,
-    r_pk_1_c,
-    r_pk_1_m,
-    r_pk_1_w,
-    r_pk_10_glory,
-    r_pk_20,
-    r_pk_40,
-    r_ke_base,
-    r_ke_,
-  } = app_reply_main_shortcuts_r_ke_(root, languages_chosen_default);
+  let r2 = app_reply_main_shortcuts_r_ke_(root, languages_chosen_default);
+  let r_ke_ = property_get(r2, "r_ke_");
+  let r_ke_base = property_get(r2, "r_ke_base");
+  let r_pk_40 = property_get(r2, "r_pk_40");
+  let r_pk_20 = property_get(r2, "r_pk_20");
+  let r_pk_10_glory = property_get(r2, "r_pk_10_glory");
+  let r_pk_1_w = property_get(r2, "r_pk_1_w");
+  let r_pk_1_m = property_get(r2, "r_pk_1_m");
+  let r_pk_1_c = property_get(r2, "r_pk_1_c");
+  let r_pk_10_h = property_get(r2, "r_pk_10_h");
+  let r_pk_10_gh = property_get(r2, "r_pk_10_gh");
+  let r_pk_10_g = property_get(r2, "r_pk_10_g");
+  let r_pk_10 = property_get(r2, "r_pk_10");
+  let r_pk_ = property_get(r2, "r_pk_");
+  let te = property_get(r2, "te");
+  let gl = property_get(r2, "gl");
+  let r_ug_ = property_get(r2, "r_ug_");
+  let r_ug_40 = property_get(r2, "r_ug_40");
   let r_ke_40 = app_reply_main_shortcuts_shortcut_extend_count(r_ke_base, 40);
   let r_default = {
     name: "",
