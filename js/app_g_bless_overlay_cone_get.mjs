b409c5625002680e804d53c6217debe39775c477
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:43:07 +0800

    function_functionize app_g_bless_overlay walking cone_get app_g_bless_overlay_cone_get

---
+++ js/app_g_bless_overlay_cone_get.mjs
@@ -0,0 +1,45 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { property_get } from "./property_get.mjs";
+import { bless_depth_start } from "./bless_depth_start.mjs";
+import { bless_cone } from "./bless_cone.mjs";
+export function app_g_bless_overlay_cone_get(r) {
+  arguments_assert(arguments, 1);
+  let walking = property_get(r, "walking");
+  let world = property_get(r, "world");
+  let player = property_get(r, "player");
+  let npcs = property_get(r, "npcs");
+  let street = property_get(r, "street");
+  let div_map = property_get(r, "div_map");
+  let wash = property_get(r, "wash");
+  let player_img_c = property_get(r, "player_img_c");
+  let glows = property_get(r, "glows");
+  let bar = property_get(r, "bar");
+  let told = property_get(r, "told");
+  let unlocked = property_get(r, "unlocked");
+  let blessings = property_get(r, "blessings");
+  function cone_get() {
+    let x = property_get(player, "x");
+    let y = property_get(player, "y");
+    let direction = property_get(player, "direction");
+    let depth = bless_depth_start();
+    let cone = bless_cone(x, y, direction, depth);
+    return cone;
+  }
+  let r2 = {
+    walking,
+    world,
+    player,
+    npcs,
+    street,
+    div_map,
+    wash,
+    player_img_c,
+    glows,
+    bar,
+    told,
+    unlocked,
+    blessings,
+    cone_get,
+  };
+  return r2;
+}
