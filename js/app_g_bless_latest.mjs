233976090adcb1fdaf954de1b13413bb04025e81
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 17:11:47 +0800

    file_imports_repair js/app_g_bless_latest.mjs

--- js/app_g_bless_latest.mjs
+++ js/app_g_bless_latest.mjs
@@ -1,3 +1,4 @@
+import { app_shared_main_latest } from "./app_shared_main_latest.mjs";
 export async function app_g_bless_latest() {
   await app_shared_main_latest("app_g_bless", "jared-grace");
 }
