b070cc1dbda190a808e10b13dbfafe85de711d70
JESUS rose to life <username@users.noreply.github.com>
2026-08-18 18:43:40 +0800

    function_functionize app_g_bless_overlay r turned app_g_bless_overlay_turned

---
+++ js/app_g_bless_overlay_turned.mjs
@@ -0,0 +1,33 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { app_g_bless_overlay_player } from "./app_g_bless_overlay_player.mjs";
+import { property_get } from "./property_get.mjs";
+import { app_g_bless_overlay_render } from "./app_g_bless_overlay_render.mjs";
+import { app_g_character_face } from "./app_g_character_face.mjs";
+export function app_g_bless_overlay_turned(container_map) {
+  arguments_assert(arguments, 1);
+  let r = app_g_bless_overlay_player(container_map);
+  let player = property_get(r, "player");
+  let npcs = property_get(r, "npcs");
+  let r2 = app_g_bless_overlay_render(r, npcs);
+  let render = property_get(r2, "render");
+  let walking = property_get(r2, "walking");
+  let world = property_get(r2, "world");
+  let bar = property_get(r2, "bar");
+  let player_img_c = property_get(r2, "player_img_c");
+  let div_map = property_get(r2, "div_map");
+  function turned(way) {
+    app_g_character_face(player, player_img_c, way);
+    render();
+  }
+  let r3 = {
+    player,
+    render,
+    walking,
+    world,
+    bar,
+    player_img_c,
+    div_map,
+    turned,
+  };
+  return r3;
+}
