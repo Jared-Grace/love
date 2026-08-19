ebe0aba15dd75a5ab4945343be1de86cccd76001
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 21:17:25 +0800

    ai

--- js/youtube_innertube_browse.mjs
+++ js/youtube_innertube_browse.mjs
@@ -1,7 +1,7 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { http_post_options } from "./http_post_options.mjs";
 import { buffer_to_json } from "./buffer_to_json.mjs";
-import { json_text } from "./json_text.mjs";
+import { json_to } from "./json_to.mjs";
 import { object_merge_set } from "./object_merge_set.mjs";
 import { youtube_innertube_context } from "./youtube_innertube_context.mjs";
 export async function youtube_innertube_browse(ask) {
@@ -13,7 +13,7 @@ export async function youtube_innertube_browse(ask) {
   object_merge_set(body, ask);
   let context = youtube_innertube_context();
   object_merge_set(body, { context: context });
-  let text = json_text(body);
+  let text = json_to(body);
   let options_extra = {
     headers: { "Content-Type": "application/json" },
     sleep: false,
