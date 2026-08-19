a4b9fffa7f770f308e2eb3297cd205f68aae8069
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 16:43:16 +0800

    file_imports_repair js/app_code_lesson_code_logged.mjs

--- js/app_code_lesson_code_logged.mjs
+++ js/app_code_lesson_code_logged.mjs
@@ -1,8 +1,11 @@
+import { app_code_value_logged_output } from "./app_code_value_logged_output.mjs";
 import { app_code_lesson_code_generic } from "./app_code_lesson_code_generic.mjs";
 import { object_copy_assign } from "./object_copy_assign.mjs";
 export function app_code_lesson_code_logged(params) {
-  let value = "logged output";
-  let next_params = object_copy_assign(params, { value });
+  let value = app_code_value_logged_output();
+  let next_params = object_copy_assign(params, {
+    value,
+  });
   let lesson = app_code_lesson_code_generic(next_params);
   return lesson;
 }
