8b21e2a728d857751265e0d307fd18c9b1cade0d
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 09:35:13 +0800

    function_fold property_path_get_2 app_g_view_render_study_container

--- js/app_g_view_render_study_container.mjs
+++ js/app_g_view_render_study_container.mjs
@@ -1,11 +1,11 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 export function app_g_view_render_study_container(r2) {
   arguments_assert(arguments, 1);
   let render_words = property_get(r2, "render_words");
   let fresh = property_get(r2, "fresh");
-  let r = property_get(r2, "r");
-  let container = property_get(r, "container");
+  let container = property_path_get_2(r2, "r", "container");
   let r3 = {
     render_words,
     fresh,
