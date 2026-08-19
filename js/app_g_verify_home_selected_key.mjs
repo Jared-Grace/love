7bfce764c512f54db1aacb0d08529a532d201af7
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 13:09:23 +0800

    function_span_cut_or_undo app_g_verify_home r32 selected_key app_g_verify_home_selected_key

--- js/app_g_verify_home_selected_key.mjs
+++ js/app_g_verify_home_selected_key.mjs
@@ -4,33 +4,34 @@ import { property_get } from "./property_get.mjs";
 export function app_g_verify_home_selected_key() {
   arguments_assert(arguments, 0);
   let r32 = app_g_verify_home_view();
-  let view2 = property_get(r32, "view");
-  let chapter_codes2 = property_get(r32, "chapter_codes");
-  let chapter_code2 = property_get(r32, "chapter_code");
+  let view = property_get(r32, "view");
+  let chapter_codes = property_get(r32, "chapter_codes");
+  let chapter_code = property_get(r32, "chapter_code");
   let storage_key = property_get(r32, "storage_key");
   let selected_key2 = property_get(r32, "selected_key");
   let advanced_for2 = property_get(r32, "advanced_for");
   let chapter_advance_armed2 = property_get(r32, "chapter_advance_armed");
   let shown_json2 = property_get(r32, "shown_json");
-  let poll_timer3 = property_get(r32, "poll_timer");
-  let chapter3 = property_get(r32, "chapter");
-  let status2 = property_get(r32, "status");
+  let poll_timer = property_get(r32, "poll_timer");
+  let chapter = property_get(r32, "chapter");
+  let status = property_get(r32, "status");
   let shown_json = shown_json2;
   let chapter_advance_armed = chapter_advance_armed2;
   let advanced_for = advanced_for2;
   let selected_key = selected_key2;
-  return {
+  let r = {
     r32,
-    view2,
-    chapter_codes2,
-    chapter_code2,
+    view2: view,
+    chapter_codes2: chapter_codes,
+    chapter_code2: chapter_code,
     storage_key,
-    poll_timer3,
-    chapter3,
-    status2,
+    poll_timer3: poll_timer,
+    chapter3: chapter,
+    status2: status,
     shown_json,
     chapter_advance_armed,
     advanced_for,
     selected_key,
   };
+  return r;
 }
