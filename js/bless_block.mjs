0624d9d8cb90bab92de23c4da79cbf8100823082
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 14:23:44 +0800

    function_span_cut_or_undo bless_block r2 buildings bless_block_buildings

--- js/bless_block.mjs
+++ js/bless_block.mjs
@@ -1,4 +1,4 @@
-import { bless_block_buildings } from "./bless_block_buildings.mjs";
+import { bless_block_doors } from "./bless_block_doors.mjs";
 import { bless_block_walls } from "./bless_block_walls.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
@@ -34,7 +34,11 @@ export function bless_block(x, y) {
   ("block reachable from the pavement in front of it.");
   let r = bless_block_walls(x, y);
   let walls = property_get(r, "walls");
-  let { doors, alleys, sidewalk, buildings } = bless_block_buildings(r, x);
+  let r2 = bless_block_doors(r, x);
+  let doors = property_get(r2, "doors");
+  let alleys = property_get(r2, "alleys");
+  let sidewalk = property_get(r2, "sidewalk");
+  let buildings = property_get(r2, "buildings");
   let block = {
     buildings: buildings,
     walls: walls,
