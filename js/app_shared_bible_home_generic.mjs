af718088be01a9c34318564b81fe86107c1c5128
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 13:08:28 +0800

    ai

--- js/app_shared_bible_home_generic.mjs
+++ js/app_shared_bible_home_generic.mjs
@@ -37,19 +37,23 @@ export async function app_shared_bible_home_generic(
   if (unknown_shown) {
     return null;
   }
-  let {
-    chapter_code,
-    book_name,
-    chapter_name,
-    languages_verses,
-    languages_available,
-    text_languages,
-    verse_number,
-    verse_current,
-    top,
-    p_verse,
-    bottom,
-  } = await app_shared_bible_home_generic_bottom(hash, context, bar, content);
+  let r = await app_shared_bible_home_generic_bottom(
+    hash,
+    context,
+    bar,
+    content,
+  );
+  let bottom = property_get(r, "bottom");
+  let p_verse = property_get(r, "p_verse");
+  let top = property_get(r, "top");
+  let verse_current = property_get(r, "verse_current");
+  let verse_number = property_get(r, "verse_number");
+  let text_languages = property_get(r, "text_languages");
+  let languages_available = property_get(r, "languages_available");
+  let languages_verses = property_get(r, "languages_verses");
+  let chapter_name = property_get(r, "chapter_name");
+  let book_name = property_get(r, "book_name");
+  let chapter_code = property_get(r, "chapter_code");
   app_shared_bible_home_chapter_button(
     context,
     app_fn,
