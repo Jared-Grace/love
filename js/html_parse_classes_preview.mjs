6f21c83bd5b16165f7602946988a642a98a530d9
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 09:34:19 +0800

    function_fold html_parse_find_text html_parse_classes_preview

--- js/html_parse_classes_preview.mjs
+++ js/html_parse_classes_preview.mjs
@@ -1,11 +1,9 @@
+import { html_parse_find_text } from "./html_parse_find_text.mjs";
 import { list_to_dictionary_value } from "./list_to_dictionary_value.mjs";
-import { html_parse_text } from "./html_parse_text.mjs";
-import { html_parse_find } from "./html_parse_find.mjs";
 import { text_combine } from "./text_combine.mjs";
 export function html_parse_classes_preview(main, d, classes) {
   function lambda(c) {
-    let e = html_parse_find(main, text_combine(".", c));
-    let text = html_parse_text(d, e);
+    let text = html_parse_find_text(main, d, text_combine(".", c));
     return text;
   }
   let dictionary = list_to_dictionary_value(classes, lambda);
