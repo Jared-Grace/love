d2caf1184a5e1ef958ff98b5e2df265348b2de7e
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 09:34:42 +0800

    function_fold literals_frozen_gate_run_lambda2 app_shared_bible_books_matches_is_uncategorized

--- js/app_shared_bible_books_matches_is_uncategorized.mjs
+++ js/app_shared_bible_books_matches_is_uncategorized.mjs
@@ -1,12 +1,14 @@
+import { literals_frozen_gate_run_lambda2 } from "./literals_frozen_gate_run_lambda2.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { property_get } from "./property_get.mjs";
-import { list_includes_not } from "./list_includes_not.mjs";
 export function app_shared_bible_books_matches_is_uncategorized(
   book,
   known_codes,
 ) {
   arguments_assert(arguments, 2);
-  let code = property_get(book, "book_code");
-  let unknown = list_includes_not(known_codes, code);
+  let unknown = literals_frozen_gate_run_lambda2(
+    "book_code",
+    book,
+    known_codes,
+  );
   return unknown;
 }
