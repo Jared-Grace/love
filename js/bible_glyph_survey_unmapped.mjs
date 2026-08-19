860532680c2f62b5f22d941636d85037f3393b22
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 09:35:14 +0800

    function_fold property_path_get_2 bible_glyph_survey_unmapped

--- js/bible_glyph_survey_unmapped.mjs
+++ js/bible_glyph_survey_unmapped.mjs
@@ -1,3 +1,4 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 export function bible_glyph_survey_unmapped(r) {
@@ -10,8 +11,7 @@ export function bible_glyph_survey_unmapped(r) {
   let occurrences_mapped = property_get(r, "occurrences_mapped");
   let sense_spread = property_get(r, "sense_spread");
   let left = property_get(r, "left");
-  let r2 = property_get(r, "r2");
-  let unmapped = property_get(r2, "unmapped");
+  let unmapped = property_path_get_2(r, "r2", "unmapped");
   let r3 = {
     roots,
     mapped,
