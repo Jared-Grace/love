f2e977e9a9ccf40185717f6396cd8e8460f6143a
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 09:34:25 +0800

    function_fold list_empty_not_is js_stack_function_below_is

--- js/js_stack_function_below_is.mjs
+++ js/js_stack_function_below_is.mjs
@@ -1,10 +1,9 @@
+import { list_empty_not_is } from "./list_empty_not_is.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { list_get_end_1 } from "./list_get_end_1.mjs";
 import { list_slice_from } from "./list_slice_from.mjs";
 import { js_types_function_node } from "./js_types_function_node.mjs";
 import { js_stack_filtered_multiple } from "./js_stack_filtered_multiple.mjs";
-import { list_empty_is } from "./list_empty_is.mjs";
-import { not } from "./not.mjs";
 export function js_stack_function_below_is(stack, item) {
   arguments_assert(arguments, 2);
   ("Whether a function is written between the given statement and the name at the end of the walk - that is, whether the name stands inside a function written in that statement rather than in the statement's own run of work.");
@@ -13,7 +12,6 @@ export function js_stack_function_below_is(stack, item) {
   let below = list_slice_from(stack, item, last);
   let types = js_types_function_node();
   let functions = js_stack_filtered_multiple(below, types);
-  let empty_is = list_empty_is(functions);
-  let below_is = not(empty_is);
+  let below_is = list_empty_not_is(functions);
   return below_is;
 }
