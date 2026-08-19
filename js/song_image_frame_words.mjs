65a7c9ce344890e597b2502a962054f3115f7526
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 17:20:06 +0800

    function_shadowing_function_rename song_image_frame_words half half_is

--- js/song_image_frame_words.mjs
+++ js/song_image_frame_words.mjs
@@ -15,11 +15,11 @@ export function song_image_frame_words(frame, state, couplet) {
   html_style_set(words, "padding", "0 5%");
   html_style_set(words, "box-sizing", "border-box");
   html_style_set(words, "transform", "translateY(-50%)");
-  let half = equal(state.placement, "half");
-  html_style_set(words, "width", half && wide ? "50%" : "100%");
-  html_style_set(words, "left", half && wide && !state.flip ? "50%" : "0");
+  let half_is = equal(state.placement, "half");
+  html_style_set(words, "width", half_is && wide ? "50%" : "100%");
+  html_style_set(words, "left", half_is && wide && !state.flip ? "50%" : "0");
   let tall_top = state.flip ? "25%" : "75%";
-  html_style_set(words, "top", half && !wide ? tall_top : "50%");
+  html_style_set(words, "top", half_is && !wide ? tall_top : "50%");
   html_text_set(words, couplet.first + "<br>" + couplet.second);
   return words;
 }
