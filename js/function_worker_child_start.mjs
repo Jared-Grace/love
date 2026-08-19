cfa2095004df8ac6cb8dd882f018496e68a3172f
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 16:26:00 +0800

    function_rename worker_exited function_worker_exited

--- js/function_worker_child_start.mjs
+++ js/function_worker_child_start.mjs
@@ -1,5 +1,5 @@
 import { worker_results_read } from "./worker_results_read.mjs";
-import { worker_exited } from "./worker_exited.mjs";
+import { function_worker_exited } from "./function_worker_exited.mjs";
 import { property_get } from "./property_get.mjs";
 import { fn_name } from "./fn_name.mjs";
 export async function function_worker_child_start(worker) {
@@ -27,7 +27,7 @@ export async function function_worker_child_start(worker) {
   }
   started.stdio[4].on("data", lambda);
   function lambda2(code) {
-    worker_exited(worker, code);
+    function_worker_exited(worker, code);
   }
   started.on("exit", lambda2);
   return started;
