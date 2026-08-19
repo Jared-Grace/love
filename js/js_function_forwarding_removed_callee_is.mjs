98f298a7ddb58caf46964f8515f72d823e416663
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 09:35:19 +0800

    function_fold property_path_get_2 js_function_forwarding_removed_callee_is

--- js/js_function_forwarding_removed_callee_is.mjs
+++ js/js_function_forwarding_removed_callee_is.mjs
@@ -1,10 +1,9 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-import { property_get } from "./property_get.mjs";
 import { js_node_type_is } from "./js_node_type_is.mjs";
 export function js_function_forwarding_removed_callee_is(site) {
   arguments_assert(arguments, 1);
-  let call = property_get(site, "call");
-  let callee = property_get(call, "callee");
+  let callee = property_path_get_2(site, "call", "callee");
   let callee_is = js_node_type_is(callee, "Identifier");
   let r = {
     callee,
