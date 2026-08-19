5b1632ee7a3e5034826549c5f3e8046918bcffc9
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 16:26:26 +0800

    function_rename worker_results_read function_worker_results_read

--- js/function_worker_results_read.mjs
+++ js/function_worker_results_read.mjs
@@ -2,7 +2,7 @@ import { function_worker_reply_take } from "./function_worker_reply_take.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { equal } from "./equal.mjs";
 import { not } from "./not.mjs";
-export function worker_results_read(worker, chunk) {
+export function function_worker_results_read(worker, chunk) {
   worker.pending = text_combine_multiple([worker.pending, chunk.toString()]);
   let lines = worker.pending.split("\n");
   worker.pending = lines.pop();
