3d519636dc2f48c861a285c9cd0869e41525090a
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 14:24:06 +0800

    function_span_cut_or_undo app_g_view_render_npc x right app_g_view_render_npc_right

--- js/app_g_view_render_npc.mjs
+++ js/app_g_view_render_npc.mjs
@@ -1,18 +1,11 @@
+import { app_g_view_render_npc_right } from "./app_g_view_render_npc_right.mjs";
 import { equal } from "./equal.mjs";
 import { app_g_view_get } from "./app_g_view_get.mjs";
-import { app_g_npcs_get } from "./app_g_npcs_get.mjs";
-import { app_g_player_get } from "./app_g_player_get.mjs";
-import { app_g_view_set } from "./app_g_view_set.mjs";
 import { app_g_conversation } from "./app_g_conversation.mjs";
 import { app_g_gospel } from "./app_g_gospel.mjs";
-import { html_remove } from "./html_remove.mjs";
 import { property_get } from "./property_get.mjs";
-import { app_g_overlay } from "./app_g_overlay.mjs";
 import { app_g_click_npc_study } from "./app_g_click_npc_study.mjs";
 import { app_g_click_npc_pray } from "./app_g_click_npc_pray.mjs";
-import { list_filter_object_includes } from "./list_filter_object_includes.mjs";
-import { list_single } from "./list_single.mjs";
-import { app_g_view_phase_study } from "./app_g_view_phase_study.mjs";
 import { app_g_view_phase_pray } from "./app_g_view_phase_pray.mjs";
 import { app_g_view_phase_conversation } from "./app_g_view_phase_conversation.mjs";
 import { app_g_view_phase_gospel } from "./app_g_view_phase_gospel.mjs";
@@ -25,22 +18,13 @@ import { app_g_disciple } from "./app_g_disciple.mjs";
 import { error_json } from "./error_json.mjs";
 export async function app_g_view_render_npc(div_map) {
   let view = await app_g_view_get();
-  let x = property_get(view, "x");
-  let y = property_get(view, "y");
-  let phase = property_get(view, "phase");
-  let npcs = await app_g_npcs_get();
-  let matched = list_filter_object_includes(npcs, {
-    x,
-    y,
-  });
-  let npc = list_single(matched);
-  let player = await app_g_player_get();
-  let overlay = app_g_overlay(div_map);
-  async function overlay_close() {
-    html_remove(overlay);
-    await app_g_view_set(null);
-  }
-  let right = app_g_view_phase_study();
+  let r = await app_g_view_render_npc_right(view, div_map);
+  let right = property_get(r, "right");
+  let overlay_close = property_get(r, "overlay_close");
+  let overlay = property_get(r, "overlay");
+  let player = property_get(r, "player");
+  let npc = property_get(r, "npc");
+  let phase = property_get(r, "phase");
   if (equal(phase, right)) {
     await app_g_click_npc_study(player, overlay, overlay_close, div_map);
     return;
