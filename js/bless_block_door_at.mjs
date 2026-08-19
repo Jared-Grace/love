3ac4de1e187cc34cd7ab5e18d83dc171710af326
JESUS rose to life <username@users.noreply.github.com>
2026-08-19 15:41:49 +0800

    function_fold property_path_get_2 bless_block_door_at

--- js/bless_block_door_at.mjs
+++ js/bless_block_door_at.mjs
@@ -1,3 +1,4 @@
+import { property_path_get_2 } from "./property_path_get_2.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 import { bless_tiles_rectangle } from "./bless_tiles_rectangle.mjs";
@@ -6,8 +7,7 @@ export function bless_block_door_at(r, x) {
   let sidewalk_y = property_get(r, "sidewalk_y");
   let block_width = property_get(r, "block_width");
   let sidewalk_depth = property_get(r, "sidewalk_depth");
-  let r2 = property_get(r, "r2");
-  let alleys = property_get(r2, "alleys");
+  let alleys = property_path_get_2(r, "r2", "alleys");
   let sidewalk = bless_tiles_rectangle(
     x,
     sidewalk_y,
@@ -15,8 +15,7 @@ export function bless_block_door_at(r, x) {
     sidewalk_depth,
   );
   function door_at(building) {
-    let doorway = property_get(building, "doorway");
-    let at = property_get(doorway, "x");
+    let at = property_path_get_2(building, "doorway", "x");
     let door = {
       x: at,
       y: sidewalk_y,
