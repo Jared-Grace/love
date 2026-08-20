7ef0994e3d2880344e715404898e0f785de84a3a
JESUS rose to life <username@users.noreply.github.com>
2026-08-20 09:54:31 +0800

    ai

--- js/data_backup.mjs
+++ js/data_backup.mjs
@@ -1,4 +1,4 @@
-import { path_join } from "./path_join.mjs";
+import { folder_backups_join } from "./folder_backups_join.mjs";
 import { file_copy } from "./file_copy.mjs";
 import { date_now_file } from "./date_now_file.mjs";
 import { data_path_generic } from "./data_path_generic.mjs";
@@ -7,7 +7,8 @@ import { text_combine } from "./text_combine.mjs";
 export async function data_backup() {
   let f_path_from = data_path();
   let inner = date_now_file();
-  let f_path_to = data_path_generic(text_combine(".", inner), "data");
+  let suffix = text_combine(".", inner);
+  let f_path_to = data_path_generic(suffix, "data");
   let joined = folder_backups_join(f_path_to);
   await file_copy(f_path_from, joined);
 }
