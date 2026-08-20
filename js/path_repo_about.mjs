ab8f6ec81fd02c0d780f89f66bec1f3450627bfe
JESUS rose to life <username@users.noreply.github.com>
2026-08-20 14:24:33 +0800

    ai

--- js/path_repo_about.mjs
+++ js/path_repo_about.mjs
@@ -1,5 +1,4 @@
 import { repo_path_combine } from "./repo_path_combine.mjs";
-import { data_path_generic } from "./data_path_generic.mjs";
 export function path_repo_about(repo) {
   let f_path = "data/given/settings/about.json";
   let a_path = repo_path_combine(repo, f_path);
