9c0c17e0dcf7b8c6ffa9457ecefa5ba84616af64
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:28:22 +0800

    function_rename app_g_overlay_container app_shared_game_overlay_container

--- js/app_g_overlay.mjs
+++ js/app_g_overlay.mjs
@@ -1,7 +1,7 @@
-import { app_g_overlay_container } from "./app_g_overlay_container.mjs";
+import { app_shared_game_overlay_container } from "./app_shared_game_overlay_container.mjs";
 import { property_get } from "./property_get.mjs";
 export function app_g_overlay(div_map) {
   let container = property_get(div_map, "container");
-  let overlay = app_g_overlay_container(container);
+  let overlay = app_shared_game_overlay_container(container);
   return overlay;
 }
