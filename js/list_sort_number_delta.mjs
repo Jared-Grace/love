24dc65304ba6bc39ca0ccd52fc5d800e252a77c6
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:01:18 +0800

    function_rename list_sort_number_property list_sort_number_delta

--- js/list_sort_number_delta.mjs
+++ js/list_sort_number_delta.mjs
@@ -1,6 +1,6 @@
 import { list_sort_number_mapper } from "./list_sort_number_mapper.mjs";
 import { property_get_curried_right } from "./property_get_curried_right.mjs";
-export function list_sort_number_property(list) {
+export function list_sort_number_delta(list) {
   let f = property_get_curried_right("delta");
   list_sort_number_mapper(list, f);
   return list;
