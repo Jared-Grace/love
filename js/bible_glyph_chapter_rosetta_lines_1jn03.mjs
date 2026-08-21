4bc9b4417d64689d1985d2cf00c150d7681383e7
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:10:58 +0800

    bible_glyph_chapter_rosetta_lines_rewrite 1JN03

--- js/bible_glyph_chapter_rosetta_lines_1jn03.mjs
+++ js/bible_glyph_chapter_rosetta_lines_1jn03.mjs
@@ -11,21 +11,21 @@ export function bible_glyph_chapter_rosetta_lines_1jn03() {
         original:
           "Ἴδετε ποταπὴν ἀγάπην δέδωκεν ἡμῖν ὁ Πατὴρ ἵνα τέκνα Θεοῦ κληθῶμεν καὶ ἐσμέν διὰ τοῦτο ὁ κόσμος οὐ γινώσκει ἡμᾶς ὅτι οὐκ ἔγνω αὐτόν",
         english:
-          "Behold what manner of love has given to us the Father that children of God we should be called And [that is what] we are The reason the world does not know us [is] that it did not know Him",
+          "Behold what manner of love has given to us the Father that children of God we should be called And that is what we are The reason the world does not know us is that it did not know Him",
       },
       {
         verse_number: 2,
         original:
           "Ἀγαπητοί νῦν τέκνα Θεοῦ ἐσμεν καὶ οὔπω ἐφανερώθη τί ἐσόμεθα οἴδαμεν ὅτι ἐὰν φανερωθῇ ὅμοιοι αὐτῷ ἐσόμεθα ὅτι ὀψόμεθα αὐτὸν καθώς ἐστιν",
         english:
-          "Beloved now children of God we are and {has} not yet been revealed what we will be We know that when [Christ] appears like Him we will be for we will see Him as He is",
+          "Beloved now children of God we are and has not yet been revealed what we will be We know that when Christ appears like Him we will be for we will see Him as He is",
       },
       {
         verse_number: 3,
         original:
           "καὶ πᾶς ὁ ἔχων τὴν ἐλπίδα ταύτην ἐπ’ αὐτῷ ἁγνίζει ἑαυτὸν καθὼς ἐκεῖνος ἁγνός ἐστιν",
         english:
-          "And everyone who has hope this in Him purifies himself just as [ Christ ] pure is",
+          "And everyone who has hope this in Him purifies himself just as Christ pure is",
       },
       {
         verse_number: 4,
@@ -39,96 +39,96 @@ export function bible_glyph_chapter_rosetta_lines_1jn03() {
         original:
           "καὶ οἴδατε ὅτι ἐκεῖνος ἐφανερώθη ἵνα τὰς ἁμαρτίας ἄρῃ καὶ ἁμαρτία ἐν αὐτῷ οὐκ ἔστιν",
         english:
-          "[But] you know that [Christ] appeared to sins take away and sin in Him no there is",
+          "But you know that Christ appeared to sins take away and sin in Him no there is",
       },
       {
         verse_number: 6,
         original:
           "πᾶς ὁ ἐν αὐτῷ μένων οὐχ ἁμαρτάνει πᾶς ὁ ἁμαρτάνων οὐχ ἑώρακεν αὐτὸν οὐδὲ ἔγνωκεν αὐτόν",
         english:
-          "No one who in Him remains keeps on sinning No one who continues to sin has seen Him [or] known Him",
+          "No one who in Him remains keeps on sinning No one who continues to sin has seen Him or known Him",
       },
       {
         verse_number: 7,
         original:
           "Τεκνία μηδεὶς πλανάτω ὑμᾶς ὁ ποιῶν τὴν δικαιοσύνην δίκαιός ἐστιν καθὼς ἐκεῖνος δίκαιός ἐστιν",
         english:
-          "Little children {let} no one deceive you The [one who] practices righteousness righteous is just as [Christ] righteous is",
+          "Little children let no one deceive you The one who practices righteousness righteous is just as Christ righteous is",
       },
       {
         verse_number: 8,
         original:
           "ὁ ποιῶν τὴν ἁμαρτίαν ἐκ τοῦ διαβόλου ἐστίν ὅτι ἀπ’ ἀρχῆς ὁ διάβολος ἁμαρτάνει εἰς τοῦτο ἐφανερώθη ὁ Υἱὸς τοῦ Θεοῦ ἵνα λύσῃ τὰ ἔργα τοῦ διαβόλου",
         english:
-          "The [one who] practices sin of the devil is because from [the] very start the devil has been sinning This is why was revealed the Son of God to destroy the works of the devil",
+          "The one who practices sin of the devil is because from the very start the devil has been sinning This is why was revealed the Son of God to destroy the works of the devil",
       },
       {
         verse_number: 9,
         original:
           "Πᾶς ὁ γεγεννημένος ἐκ τοῦ Θεοῦ ἁμαρτίαν οὐ ποιεῖ ὅτι σπέρμα αὐτοῦ ἐν αὐτῷ μένει καὶ οὐ δύναται ἁμαρτάνειν ὅτι ἐκ τοῦ Θεοῦ γεγέννηται",
         english:
-          "Anyone born of God sin refuses to practice because seed [God’s] in him abides he cannot go on sinning because of God he has been born",
+          "Anyone born of God sin refuses to practice because seed God’s in him abides he cannot go on sinning because of God he has been born",
       },
       {
         verse_number: 10,
         original:
           "ἐν τούτῳ φανερά ἐστιν τὰ τέκνα τοῦ Θεοῦ καὶ τὰ τέκνα τοῦ διαβόλου πᾶς ὁ μὴ ποιῶν δικαιοσύνην οὐκ ἔστιν ἐκ τοῦ Θεοῦ καὶ ὁ μὴ ἀγαπῶν τὸν ἀδελφὸν αὐτοῦ",
         english:
-          "By this distinguished are the children of God [from] the children of the devil Anyone who does not practice righteousness not is of God [nor is] anyone who does not love brother his",
+          "By this distinguished are the children of God from the children of the devil Anyone who does not practice righteousness not is of God nor is anyone who does not love brother his",
       },
       {
         verse_number: 11,
         original:
           "Ὅτι αὕτη ἐστὶν ἡ ἀγγελία ἣν ἠκούσατε ἀπ’ ἀρχῆς ἵνα ἀγαπῶμεν ἀλλήλους",
         english:
-          "This is the message you have heard from [the] beginning We should love one another",
+          "This is the message you have heard from the beginning We should love one another",
       },
       {
         verse_number: 12,
         original:
           "οὐ καθὼς Κάϊν ἐκ τοῦ πονηροῦ ἦν καὶ ἔσφαξεν τὸν ἀδελφὸν αὐτοῦ καὶ χάριν τίνος ἔσφαξεν αὐτόν ὅτι τὰ ἔργα αὐτοῦ πονηρὰ ἦν τὰ δὲ τοῦ ἀδελφοῦ αὐτοῦ δίκαια",
         english:
-          "[Do] not [be] like Cain the evil [one] [who] belonged to and murdered brother his And why did [Cain] slay him Because [own] deeds his evil were those while of brother his [were] righteous",
+          "Do not be like Cain the evil one who belonged to and murdered brother his And why did Cain slay him Because own deeds his evil were those while of brother his were righteous",
       },
       {
         verse_number: 13,
         original: "Μὴ θαυμάζετε ἀδελφοί εἰ μισεῖ ὑμᾶς ὁ κόσμος",
-        english: "{do} not be surprised brothers if hates you the world",
+        english: "do not be surprised brothers if hates you the world",
       },
       {
         verse_number: 14,
         original:
           "Ἡμεῖς οἴδαμεν ὅτι μεταβεβήκαμεν ἐκ τοῦ θανάτου εἰς τὴν ζωήν ὅτι ἀγαπῶμεν τοὺς ἀδελφούς ὁ μὴ ἀγαπῶν μένει ἐν τῷ θανάτῳ",
         english:
-          "We know that we have passed from death to life because we love [our] brothers The [one who] does not love remains in death",
+          "We know that we have passed from death to life because we love our brothers The one who does not love remains in death",
       },
       {
         verse_number: 15,
         original:
           "πᾶς ὁ μισῶν τὸν ἀδελφὸν αὐτοῦ ἀνθρωποκτόνος ἐστίν καὶ οἴδατε ὅτι πᾶς ἀνθρωποκτόνος οὐκ ἔχει ζωὴν αἰώνιον ἐν αὐτῷ μένουσαν",
         english:
-          "Everyone who hates brother his a murderer is and you know that a murderer {does} not life eternal in reside",
+          "Everyone who hates brother his a murderer is and you know that a murderer does not life eternal in reside",
       },
       {
         verse_number: 16,
         original:
           "Ἐν τούτῳ ἐγνώκαμεν τὴν ἀγάπην ὅτι ἐκεῖνος ὑπὲρ ἡμῶν τὴν ψυχὴν αὐτοῦ ἔθηκεν καὶ ἡμεῖς ὀφείλομεν ὑπὲρ τῶν ἀδελφῶν τὰς ψυχὰς θεῖναι",
         english:
-          "By this we know what love [is] [Jesus] for us life His laid down and we ought for [our] brothers [our] lives to lay down",
+          "By this we know what love is Jesus for us life His laid down and we ought for our brothers our lives to lay down",
       },
       {
         verse_number: 17,
         original:
           "ὃς δ’ ἂν ἔχῃ τὸν βίον τοῦ κόσμου καὶ θεωρῇ τὸν ἀδελφὸν αὐτοῦ χρείαν ἔχοντα καὶ κλείσῃ τὰ σπλάγχνα αὐτοῦ ἀπ’ αὐτοῦ πῶς ἡ ἀγάπη τοῦ Θεοῦ μένει ἐν αὐτῷ",
         english:
-          "anyone If with possessions earthly sees brother his need [in] [but] withholds compassion his from him how {can} the love of God abide in him",
+          "anyone If with possessions earthly sees brother his need in but withholds compassion his from him how can the love of God abide in him",
       },
       {
         verse_number: 18,
         original:
           "Τεκνία μὴ ἀγαπῶμεν λόγῳ μηδὲ τῇ γλώσσῃ ἀλλὰ ἐν ἔργῳ καὶ ἀληθείᾳ",
         english:
-          "Little children not let us love in word [and] speech but in action and truth",
+          "Little children not let us love in word and speech but in action and truth",
       },
       {
         verse_number: 19,
@@ -142,14 +142,14 @@ export function bible_glyph_chapter_rosetta_lines_1jn03() {
         original:
           "ὅτι ἐὰν καταγινώσκῃ ἡμῶν ἡ καρδία ὅτι μείζων ἐστὶν ὁ Θεὸς τῆς καρδίας ἡμῶν καὶ γινώσκει πάντα",
         english:
-          "Even if condemn [us] our hearts greater than is God hearts our and He knows all things",
+          "Even if condemn us our hearts greater than is God hearts our and He knows all things",
       },
       {
         verse_number: 21,
         original:
           "Ἀγαπητοί ἐὰν ἡ καρδία μὴ καταγινώσκῃ παρρησίαν ἔχομεν πρὸς τὸν Θεόν",
         english:
-          "Beloved if hearts do not condemn [us] confidence we have before God",
+          "Beloved if hearts do not condemn us confidence we have before God",
       },
       {
         verse_number: 22,
@@ -170,7 +170,7 @@ export function bible_glyph_chapter_rosetta_lines_1jn03() {
         original:
           "καὶ ὁ τηρῶν τὰς ἐντολὰς αὐτοῦ ἐν αὐτῷ μένει καὶ αὐτὸς ἐν αὐτῷ καὶ ἐν τούτῳ γινώσκομεν ὅτι μένει ἐν ἡμῖν ἐκ τοῦ Πνεύματος οὗ ἡμῖν ἔδωκεν",
         english:
-          "Whoever keeps commandments His in [God] remains and [God] in him And by this we know that He remains in us by the Spirit us He has given",
+          "Whoever keeps commandments His in God remains and God in him And by this we know that He remains in us by the Spirit us He has given",
       },
     ],
   };
