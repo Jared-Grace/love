9c6e08c4bf6ca6d5de6f8c57928591f3f67dd948
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 19:57:02 +0800

    bible_glyph_chapter_rosetta_lines_rewrite JHN03

--- js/bible_glyph_chapter_rosetta_lines_jhn03.mjs
+++ js/bible_glyph_chapter_rosetta_lines_jhn03.mjs
@@ -18,47 +18,46 @@ export function bible_glyph_chapter_rosetta_lines_jhn03() {
         original:
           "οὗτος ἦλθεν πρὸς αὐτὸν νυκτὸς καὶ εἶπεν αὐτῷ Ῥαββί οἴδαμεν ὅτι ἀπὸ Θεοῦ ἐλήλυθας διδάσκαλος οὐδεὶς γὰρ δύναται ταῦτα τὰ σημεῖα ποιεῖν ἃ σὺ ποιεῖς ἐὰν μὴ ᾖ ὁ Θεὸς μετ’ αὐτοῦ",
         english:
-          "He came to [Jesus] at night and said - Rabbi we know that from God who has come [You are] a teacher no one For could [the] - signs perform - You are doing if not were - God with him",
+          "He came to [Jesus] at night and said Rabbi we know that from God who has come [You are] a teacher no one For could [the] signs perform You are doing if not were God with him",
       },
       {
         verse_number: 3,
         original:
           "Ἀπεκρίθη Ἰησοῦς καὶ εἶπεν αὐτῷ Ἀμὴν ἀμὴν λέγω σοι ἐὰν μή τις γεννηθῇ ἄνωθεν οὐ δύναται ἰδεῖν τὴν βασιλείαν τοῦ Θεοῦ",
         english:
-          "replied Jesus Truly truly I tell you unless one he is born again no can see the kingdom - of God",
+          "replied Jesus Truly truly I tell you unless one he is born again no can see the kingdom of God",
       },
       {
         verse_number: 4,
         original:
           "Λέγει πρὸς αὐτὸν ὁ Νικόδημος Πῶς δύναται ἄνθρωπος γεννηθῆναι γέρων ὤν μὴ δύναται εἰς τὴν κοιλίαν τῆς μητρὸς αὐτοῦ δεύτερον εἰσελθεῖν καὶ γεννηθῆναι",
         english:
-          "asked - - - Nicodemus How can a man be born old when he is Can he - womb - mother’s his a second time enter - to be born",
+          "asked Nicodemus How can a man be born old when he is Can he womb mother’s his a second time enter to be born",
       },
       {
         verse_number: 5,
         original:
           "Ἀπεκρίθη Ἰησοῦς Ἀμὴν ἀμὴν λέγω σοι ἐὰν μή τις γεννηθῇ ἐξ ὕδατος καὶ Πνεύματος οὐ δύναται εἰσελθεῖν εἰς τὴν βασιλείαν τοῦ Θεοῦ",
         english:
-          "answered Jesus Truly truly I tell you unless one he is born of water and [the] Spirit no can enter the kingdom - of God",
+          "answered Jesus Truly truly I tell you unless one he is born of water and [the] Spirit no can enter the kingdom of God",
       },
       {
         verse_number: 6,
         original:
           "τὸ γεγεννημένον ἐκ τῆς σαρκὸς σάρξ ἐστιν καὶ τὸ γεγεννημένον ἐκ τοῦ Πνεύματος πνεῦμά ἐστιν",
-        english:
-          "- born of - flesh Flesh is [but] - born of the Spirit spirit is",
+        english: "born of flesh Flesh is [but] born of the Spirit spirit is",
       },
       {
         verse_number: 7,
         original: "μὴ θαυμάσῃς ὅτι εἶπόν σοι Δεῖ ὑμᾶς γεννηθῆναι ἄνωθεν",
-        english: "Do not be amazed that I said - must You be born again",
+        english: "Do not be amazed that I said must You be born again",
       },
       {
         verse_number: 8,
         original:
           "τὸ πνεῦμα ὅπου θέλει πνεῖ καὶ τὴν φωνὴν αὐτοῦ ἀκούεις ἀλλ’ οὐκ οἶδας πόθεν ἔρχεται καὶ ποῦ ὑπάγει οὕτως ἐστὶν πᾶς ὁ γεγεννημένος ἐκ τοῦ Πνεύματος",
         english:
-          "The wind where it wishes blows - - sound its You hear but you do not know where it comes {from} [or] where it is going So it is with everyone - born of the Spirit",
+          "The wind where it wishes blows sound its You hear but you do not know where it comes {from} [or] where it is going So it is with everyone born of the Spirit",
       },
       {
         verse_number: 9,
@@ -71,14 +70,14 @@ export function bible_glyph_chapter_rosetta_lines_jhn03() {
         original:
           "Ἀπεκρίθη Ἰησοῦς καὶ εἶπεν αὐτῷ Σὺ εἶ ὁ διδάσκαλος τοῦ Ἰσραὴλ καὶ ταῦτα οὐ γινώσκεις",
         english:
-          "said Jesus You are - teacher - Israel’s and these things you do not understand",
+          "said Jesus You are teacher Israel’s and these things you do not understand",
       },
       {
         verse_number: 11,
         original:
           "ἀμὴν ἀμὴν λέγω σοι ὅτι ὃ οἴδαμεν λαλοῦμεν καὶ ὃ ἑωράκαμεν μαρτυροῦμεν καὶ τὴν μαρτυρίαν ἡμῶν οὐ λαμβάνετε",
         english:
-          "Truly truly I tell you - of what we know we speak and to what we have seen we testify and yet - testimony our you people do not accept",
+          "Truly truly I tell you of what we know we speak and to what we have seen we testify and yet testimony our you people do not accept",
       },
       {
         verse_number: 12,
@@ -92,14 +91,14 @@ export function bible_glyph_chapter_rosetta_lines_jhn03() {
         original:
           "καὶ οὐδεὶς ἀναβέβηκεν εἰς τὸν οὐρανὸν εἰ μὴ ὁ ἐκ τοῦ οὐρανοῦ καταβάς ὁ Υἱὸς τοῦ ἀνθρώπου",
         english:
-          "- No one has ascended into - heaven except the [One who] from - heaven descended the Son - of Man",
+          "No one has ascended into heaven except the [One who] from heaven descended the Son of Man",
       },
       {
         verse_number: 14,
         original:
           "καὶ καθὼς Μωϋσῆς ὕψωσεν τὸν ὄφιν ἐν τῇ ἐρήμῳ οὕτως ὑψωθῆναι δεῖ τὸν Υἱὸν τοῦ ἀνθρώπου",
         english:
-          "Just as Moses lifted up the snake in the wilderness so be lifted up must the Son - of Man",
+          "Just as Moses lifted up the snake in the wilderness so be lifted up must the Son of Man",
       },
       {
         verse_number: 15,
@@ -111,141 +110,141 @@ export function bible_glyph_chapter_rosetta_lines_jhn03() {
         original:
           "Οὕτως γὰρ ἠγάπησεν ὁ Θεὸς τὸν κόσμον ὥστε τὸν Υἱὸν τὸν μονογενῆ ἔδωκεν ἵνα πᾶς ὁ πιστεύων εἰς αὐτὸν μὴ ἀπόληται ἀλλ’ ἔχῃ ζωὴν αἰώνιον",
         english:
-          "so For loved - God the world that [His] Son - one and only He gave that everyone who believes in Him shall not perish but have life eternal",
+          "so For loved God the world that [His] Son one and only He gave that everyone who believes in Him shall not perish but have life eternal",
       },
       {
         verse_number: 17,
         original:
           "οὐ γὰρ ἀπέστειλεν ὁ Θεὸς τὸν Υἱὸν εἰς τὸν κόσμον ἵνα κρίνῃ τὸν κόσμον ἀλλ’ ἵνα σωθῇ ὁ κόσμος δι’ αὐτοῦ",
         english:
-          "{did} not For send - God - His Son into the world to condemn the world but to save the world through Him",
+          "{did} not For send God His Son into the world to condemn the world but to save the world through Him",
       },
       {
         verse_number: 18,
         original:
           "ὁ πιστεύων εἰς αὐτὸν οὐ κρίνεται ὁ μὴ πιστεύων ἤδη κέκριται ὅτι μὴ πεπίστευκεν εἰς τὸ ὄνομα τοῦ μονογενοῦς Υἱοῦ τοῦ Θεοῦ",
         english:
-          "Whoever believes in Him is not condemned whoever does not believe has already been condemned because he has not believed in the name - one and only Son - of God’s",
+          "Whoever believes in Him is not condemned whoever does not believe has already been condemned because he has not believed in the name one and only Son of God’s",
       },
       {
         verse_number: 19,
         original:
           "Αὕτη δέ ἐστιν ἡ κρίσις ὅτι τὸ φῶς ἐλήλυθεν εἰς τὸν κόσμον καὶ ἠγάπησαν οἱ ἄνθρωποι μᾶλλον τὸ σκότος ἢ τὸ φῶς ἦν γὰρ αὐτῶν πονηρὰ τὰ ἔργα",
         english:
-          "this And is the verdict - The Light has come into the world but loved - men rather the darkness than the Light were because their evil - deeds",
+          "this And is the verdict The Light has come into the world but loved men rather the darkness than the Light were because their evil deeds",
       },
       {
         verse_number: 20,
         original:
           "πᾶς γὰρ ὁ φαῦλα πράσσων μισεῖ τὸ φῶς καὶ οὐκ ἔρχεται πρὸς τὸ φῶς ἵνα μὴ ἐλεγχθῇ τὰ ἔργα αὐτοῦ",
         english:
-          "Everyone - who evil does hates the Light and {does} not come into the Light for fear that will be exposed - deeds his",
+          "Everyone who evil does hates the Light and {does} not come into the Light for fear that will be exposed deeds his",
       },
       {
         verse_number: 21,
         original:
           "ὁ δὲ ποιῶν τὴν ἀλήθειαν ἔρχεται πρὸς τὸ φῶς ἵνα φανερωθῇ αὐτοῦ τὰ ἔργα ὅτι ἐν Θεῷ ἐστιν εἰργασμένα",
         english:
-          "whoever But practices the truth comes into the Light so that it may be seen clearly [what] he - has done that in God has been accomplished",
+          "whoever But practices the truth comes into the Light so that it may be seen clearly [what] he has done that in God has been accomplished",
       },
       {
         verse_number: 22,
         original:
           "Μετὰ ταῦτα ἦλθεν ὁ Ἰησοῦς καὶ οἱ μαθηταὶ αὐτοῦ εἰς τὴν Ἰουδαίαν γῆν καὶ ἐκεῖ διέτριβεν μετ’ αὐτῶν καὶ ἐβάπτιζεν",
         english:
-          "After this went - Jesus and - disciples His into the Judean countryside - [where] He spent some time with them and baptized",
+          "After this went Jesus and disciples His into the Judean countryside [where] He spent some time with them and baptized",
       },
       {
         verse_number: 23,
         original:
           "Ἦν δὲ καὶ Ἰωάννης βαπτίζων ἐν Αἰνὼν ἐγγὺς τοῦ Σαλείμ ὅτι ὕδατα πολλὰ ἦν ἐκεῖ καὶ παρεγίνοντο καὶ ἐβαπτίζοντο",
         english:
-          "was Now also John baptizing at Aenon near - Salim because [the] water plentiful was there and [people] kept coming - to be baptized",
+          "was Now also John baptizing at Aenon near Salim because [the] water plentiful was there and [people] kept coming to be baptized",
       },
       {
         verse_number: 24,
         original: "οὔπω γὰρ ἦν βεβλημένος εἰς τὴν φυλακὴν Ἰωάννης",
-        english: "{had} not yet (For been thrown into - prison John",
+        english: "{had} not yet (For been thrown into prison John",
       },
       {
         verse_number: 25,
         original:
           "Ἐγένετο οὖν ζήτησις ἐκ τῶν μαθητῶν Ἰωάννου μετὰ Ἰουδαίου περὶ καθαρισμοῦ",
         english:
-          "arose Then a dispute between - disciples John’s [and] [a certain] Jew over [the issue of] ceremonial washing",
+          "arose Then a dispute between disciples John’s [and] [a certain] Jew over [the issue of] ceremonial washing",
       },
       {
         verse_number: 26,
         original:
           "καὶ ἦλθον πρὸς τὸν Ἰωάννην καὶ εἶπαν αὐτῷ Ῥαββί ὃς ἦν μετὰ σοῦ πέραν τοῦ Ἰορδάνου ᾧ σὺ μεμαρτύρηκας ἴδε οὗτος βαπτίζει καὶ πάντες ἔρχονται πρὸς αὐτόν",
         english:
-          "So [John’s disciples] came to - [him] and said Rabbi the [One who] was with you beyond the Jordan the One you testified about Look He is baptizing and everyone is going to Him",
+          "So [John’s disciples] came to [him] and said Rabbi the [One who] was with you beyond the Jordan the One you testified about Look He is baptizing and everyone is going to Him",
       },
       {
         verse_number: 27,
         original:
           "Ἀπεκρίθη Ἰωάννης καὶ εἶπεν Οὐ δύναται ἄνθρωπος λαμβάνειν ἐὰν μὴ ᾖ δεδομένον αὐτῷ ἐκ τοῦ οὐρανοῦ",
-        english: "replied John can A man receive is given him from - heaven",
+        english: "replied John can A man receive is given him from heaven",
       },
       {
         verse_number: 28,
         original:
           "αὐτοὶ ὑμεῖς μοι μαρτυρεῖτε ὅτι εἶπον Οὐκ εἰμὶ ἐγὼ ὁ Χριστός ἀλλ’ ὅτι Ἀπεσταλμένος εἰμὶ ἔμπροσθεν ἐκείνου",
         english:
-          "yourselves You - can testify that I said not am I the Christ but - sent am ahead of Him",
+          "yourselves You can testify that I said not am I the Christ but sent am ahead of Him",
       },
       {
         verse_number: 29,
         original:
           "Ὁ ἔχων τὴν νύμφην νυμφίος ἐστίν ὁ δὲ φίλος τοῦ νυμφίου ὁ ἑστηκὼς καὶ ἀκούων αὐτοῦ χαρᾷ χαίρει διὰ τὴν φωνὴν τοῦ νυμφίου αὕτη οὖν ἡ χαρὰ ἡ ἐμὴ πεπλήρωται",
         english:
-          "belongs to The bride [the] bridegroom The - friend of the bridegroom - stands and listens for him [and] is overjoyed [to hear] the voice bridegroom’s That - joy - [is] mine [and] it is now complete",
+          "belongs to The bride [the] bridegroom The friend of the bridegroom stands and listens for him [and] is overjoyed [to hear] the voice bridegroom’s That joy [is] mine [and] it is now complete",
       },
       {
         verse_number: 30,
         original: "ἐκεῖνον δεῖ αὐξάνειν ἐμὲ δὲ ἐλαττοῦσθαι",
-        english: "He must increase I - [must] decrease",
+        english: "He must increase I [must] decrease",
       },
       {
         verse_number: 31,
         original:
           "Ὁ ἄνωθεν ἐρχόμενος ἐπάνω πάντων ἐστίν ὁ ὢν ἐκ τῆς γῆς ἐκ τῆς γῆς ἐστιν καὶ ἐκ τῆς γῆς λαλεῖ ὁ ἐκ τοῦ οὐρανοῦ ἐρχόμενος ἐπάνω πάντων ἐστίν",
         english:
-          "The [One who] from above comes above all is The [one who] is from the earth to the earth belongs and from the earth speaks [as one] The [One who] from - heaven comes above all is",
+          "The [One who] from above comes above all is The [one who] is from the earth to the earth belongs and from the earth speaks [as one] The [One who] from heaven comes above all is",
       },
       {
         verse_number: 32,
         original:
           "ὃ ἑώρακεν καὶ ἤκουσεν τοῦτο μαρτυρεῖ καὶ τὴν μαρτυρίαν αὐτοῦ οὐδεὶς λαμβάνει",
         english:
-          "what He has seen and heard to He testifies yet - testimony His no one accepts",
+          "what He has seen and heard to He testifies yet testimony His no one accepts",
       },
       {
         verse_number: 33,
         original:
           "ὁ λαβὼν αὐτοῦ τὴν μαρτυρίαν ἐσφράγισεν ὅτι ὁ Θεὸς ἀληθής ἐστιν",
         english:
-          "Whoever accepts His - testimony has certified that - God truthful is",
+          "Whoever accepts His testimony has certified that God truthful is",
       },
       {
         verse_number: 34,
         original:
           "ὃν γὰρ ἀπέστειλεν ὁ Θεὸς τὰ ῥήματα τοῦ Θεοῦ λαλεῖ οὐ γὰρ ἐκ μέτρου δίδωσιν τὸ Πνεῦμα",
         english:
-          "the [One] For has sent whom God the words - of God speaks without for limit [God] gives the Spirit",
+          "the [One] For has sent whom God the words of God speaks without for limit [God] gives the Spirit",
       },
       {
         verse_number: 35,
         original: "Ὁ Πατὴρ ἀγαπᾷ τὸν Υἱόν καὶ πάντα δέδωκεν ἐν τῇ χειρὶ αὐτοῦ",
         english:
-          "The Father loves the Son and all things has placed in - hands His",
+          "The Father loves the Son and all things has placed in hands His",
       },
       {
         verse_number: 36,
         original:
           "ὁ πιστεύων εἰς τὸν Υἱὸν ἔχει ζωὴν αἰώνιον ὁ δὲ ἀπειθῶν τῷ Υἱῷ οὐκ ὄψεται ζωήν ἀλλ’ ἡ ὀργὴ τοῦ Θεοῦ μένει ἐπ’ αὐτόν",
         english:
-          "Whoever believes in the Son has life eternal Whoever - rejects the Son will not see life Instead the wrath - of God remains on him",
+          "Whoever believes in the Son has life eternal Whoever rejects the Son will not see life Instead the wrath of God remains on him",
       },
     ],
   };
