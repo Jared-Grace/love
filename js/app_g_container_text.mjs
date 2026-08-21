0024d159892ea08e5503a738f278d800eda0057e
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:26:30 +0800

    function_rename app_g_container app_shared_game_container

--- js/app_g_container_text.mjs
+++ js/app_g_container_text.mjs
@@ -1,7 +1,7 @@
 import { app_g_p_text } from "./app_g_p_text.mjs";
-import { app_g_container } from "./app_g_container.mjs";
+import { app_shared_game_container } from "./app_shared_game_container.mjs";
 export function app_g_container_text(overlay, text) {
-  let choices = app_g_container(overlay);
+  let choices = app_shared_game_container(overlay);
   app_g_p_text(choices, text);
   return choices;
 }
