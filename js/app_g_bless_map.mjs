458278ff2f5eb10b3dd1e4e6e2018a1f8414e7b5
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:28:02 +0800

    function_rename app_g_npc_img_set app_shared_game_npc_img_set

--- js/app_g_bless_map.mjs
+++ js/app_g_bless_map.mjs
@@ -6,7 +6,7 @@ import { property_set_exists_not } from "./property_set_exists_not.mjs";
 import { app_shared_game_div_map_style_rows } from "./app_shared_game_div_map_style_rows.mjs";
 import { app_shared_game_div_map_tiles_add_rows } from "./app_shared_game_div_map_tiles_add_rows.mjs";
 import { app_shared_game_map_room_new } from "./app_shared_game_map_room_new.mjs";
-import { app_g_npc_img_set } from "./app_g_npc_img_set.mjs";
+import { app_shared_game_npc_img_set } from "./app_shared_game_npc_img_set.mjs";
 import { g_character_img } from "./g_character_img.mjs";
 export function app_g_bless_map(container, world) {
   arguments_assert(arguments, 2);
@@ -37,7 +37,7 @@ export function app_g_bless_map(container, world) {
   ("remembered is a person the crowd can never take a step for.");
   function person_draw(person) {
     let ci = g_character_img(div_map, person);
-    app_g_npc_img_set(person, ci);
+    app_shared_game_npc_img_set(person, ci);
   }
   each(npcs, person_draw);
   let player_img_c = g_character_img(div_map, player);
