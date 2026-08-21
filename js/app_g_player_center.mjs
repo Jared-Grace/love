c40a83581a715cd26c0b8e55181e092afafad343
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:26:46 +0800

    function_rename app_g_div_map_container_get app_shared_game_div_map_container_get

--- js/app_g_player_center.mjs
+++ js/app_g_player_center.mjs
@@ -1,6 +1,6 @@
 import { html_scroll_center_coordinates } from "./html_scroll_center_coordinates.mjs";
-import { app_g_div_map_container_get } from "./app_g_div_map_container_get.mjs";
+import { app_shared_game_div_map_container_get } from "./app_shared_game_div_map_container_get.mjs";
 export function app_g_player_center(coordinates, player_img_c, div_map) {
-  let container = app_g_div_map_container_get(div_map);
+  let container = app_shared_game_div_map_container_get(div_map);
   return html_scroll_center_coordinates(coordinates, player_img_c, container);
 }
