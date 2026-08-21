a131e45e35a09acaf951285e9ae2a5d7487e9cba
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:25:02 +0800

    function_rename app_g_button_background_translucent app_shared_game_button_background_translucent

--- js/app_g_button_uncolored_style.mjs
+++ js/app_g_button_uncolored_style.mjs
@@ -1,7 +1,11 @@
 import { app_shared_button_uncolored_background_color } from "./app_shared_button_uncolored_background_color.mjs";
-import { app_g_button_background_translucent } from "./app_g_button_background_translucent.mjs";
+import { app_shared_game_button_background_translucent } from "./app_shared_game_button_background_translucent.mjs";
 import { html_font_color_set } from "./html_font_color_set.mjs";
 export function app_g_button_uncolored_style(component) {
-  app_g_button_background_translucent(component, app_shared_button_uncolored_background_color(), "dd");
+  app_shared_game_button_background_translucent(
+    component,
+    app_shared_button_uncolored_background_color(),
+    "dd",
+  );
   html_font_color_set(component, "black");
 }
