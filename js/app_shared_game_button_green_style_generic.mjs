a4f98e48bdf8b31321d7b3dbbc5ea20e0240b1dc
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:25:08 +0800

    function_rename app_g_button_green_style_generic app_shared_game_button_green_style_generic

--- js/app_shared_game_button_green_style_generic.mjs
+++ js/app_shared_game_button_green_style_generic.mjs
@@ -1,6 +1,9 @@
 import { app_shared_button_background } from "./app_shared_button_background.mjs";
 import { app_shared_game_button_background_translucent } from "./app_shared_game_button_background_translucent.mjs";
-export function app_g_button_green_style_generic(component, alpha_channel) {
+export function app_shared_game_button_green_style_generic(
+  component,
+  alpha_channel,
+) {
   app_shared_game_button_background_translucent(
     component,
     app_shared_button_background(),
