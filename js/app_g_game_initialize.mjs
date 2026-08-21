0a212c739983c6a752c31a0d10a307da322118ed
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:27:38 +0800

    function_rename app_g_map_generate app_shared_game_map_generate

--- js/app_g_game_initialize.mjs
+++ js/app_g_game_initialize.mjs
@@ -1,13 +1,13 @@
-import { g_player_initialize } from "../../love/js/g_player_initialize.mjs";
-import { g_npcs_initialize } from "../../love/js/g_npcs_initialize.mjs";
-import { g_player_img_get } from "../../love/js/g_player_img_get.mjs";
+import { g_player_initialize } from "./g_player_initialize.mjs";
+import { g_npcs_initialize } from "./g_npcs_initialize.mjs";
+import { g_player_img_get } from "./g_player_img_get.mjs";
 import { g_coordinates_land_reachable_get } from "./g_coordinates_land_reachable_get.mjs";
-import { app_g_game_save } from "../../love/js/app_g_game_save.mjs";
-import { app_g_map_save } from "../../love/js/app_g_map_save.mjs";
-import { g_coordinates } from "../../love/js/g_coordinates.mjs";
-import { app_g_map_generate } from "../../love/js/app_g_map_generate.mjs";
+import { app_g_game_save } from "./app_g_game_save.mjs";
+import { app_g_map_save } from "./app_g_map_save.mjs";
+import { g_coordinates } from "./g_coordinates.mjs";
+import { app_shared_game_map_generate } from "./app_shared_game_map_generate.mjs";
 export async function app_g_game_initialize() {
-  let rows = app_g_map_generate();
+  let rows = app_shared_game_map_generate();
   let coordinates = g_coordinates(rows);
   let coordinates_land = g_coordinates_land_reachable_get(coordinates);
   let player_img = g_player_img_get();
