cfd7f80d1c2505198e43cdf2e9607bd3b40e3824
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:28:42 +0800

    function_rename app_g_tile_coordinates_get app_shared_game_tile_coordinates_get

--- js/app_g_event_target_closest_tile_coordinates.mjs
+++ js/app_g_event_target_closest_tile_coordinates.mjs
@@ -1,7 +1,7 @@
-import { app_g_tile_coordinates_get } from "./app_g_tile_coordinates_get.mjs";
+import { app_shared_game_tile_coordinates_get } from "./app_shared_game_tile_coordinates_get.mjs";
 import { app_shared_game_event_target_closest_tile } from "./app_shared_game_event_target_closest_tile.mjs";
 export function app_g_event_target_closest_tile_coordinates(e) {
   let tile = app_shared_game_event_target_closest_tile(e);
-  let clicked_coordinates = app_g_tile_coordinates_get(tile);
+  let clicked_coordinates = app_shared_game_tile_coordinates_get(tile);
   return clicked_coordinates;
 }
