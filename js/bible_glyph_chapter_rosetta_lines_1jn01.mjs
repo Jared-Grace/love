51c7d7bb40101de4516d5fa8d1c0ef9425f05db1
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 19:57:03 +0800

    bible_glyph_chapter_rosetta_lines_rewrite 1JN01

--- js/bible_glyph_chapter_rosetta_lines_1jn01.mjs
+++ js/bible_glyph_chapter_rosetta_lines_1jn01.mjs
@@ -11,68 +11,68 @@ export function bible_glyph_chapter_rosetta_lines_1jn01() {
         original:
           "Ὃ ἦν ἀπ’ ἀρχῆς ὃ ἀκηκόαμεν ὃ ἑωράκαμεν τοῖς ὀφθαλμοῖς ἡμῶν ὃ ἐθεασάμεθα καὶ αἱ χεῖρες ἡμῶν ἐψηλάφησαν περὶ τοῦ Λόγου τῆς ζωῆς",
         english:
-          "That which was from [the] beginning which we have heard which we have seen with eyes our own which we have gazed upon and [own] hands our touched [with] [this is ] the Word - of life",
+          "That which was from [the] beginning which we have heard which we have seen with eyes our own which we have gazed upon and [own] hands our touched [with] [this is ] the Word of life",
       },
       {
         verse_number: 2,
         original:
           "καὶ ἡ ζωὴ ἐφανερώθη καὶ ἑωράκαμεν καὶ μαρτυροῦμεν καὶ ἀπαγγέλλομεν ὑμῖν τὴν ζωὴν τὴν αἰώνιον ἥτις ἦν πρὸς τὸν Πατέρα καὶ ἐφανερώθη ἡμῖν",
         english:
-          "And [this is] the life that was revealed - we have seen [it] and testified [to it] and we proclaim to you - life the eternal that was with the Father and was revealed to us",
+          "And [this is] the life that was revealed we have seen [it] and testified [to it] and we proclaim to you life the eternal that was with the Father and was revealed to us",
       },
       {
         verse_number: 3,
         original:
           "ὃ ἑωράκαμεν καὶ ἀκηκόαμεν ἀπαγγέλλομεν καὶ ὑμῖν ἵνα καὶ ὑμεῖς κοινωνίαν ἔχητε μεθ’ ἡμῶν καὶ ἡ κοινωνία δὲ ἡ ἡμετέρα μετὰ τοῦ Πατρὸς καὶ μετὰ τοῦ Υἱοῦ αὐτοῦ Ἰησοῦ Χριστοῦ",
         english:
-          "what we have seen and heard We proclaim - to you so that also you fellowship may have with us And [this] fellowship - - of ours [is] with the Father and with - Son His Jesus Christ",
+          "what we have seen and heard We proclaim to you so that also you fellowship may have with us And [this] fellowship of ours [is] with the Father and with Son His Jesus Christ",
       },
       {
         verse_number: 4,
         original: "καὶ ταῦτα γράφομεν ἡμεῖς ἵνα ἡ χαρὰ ἡμῶν ᾖ πεπληρωμένη",
-        english: "- these things write We so that - joy our may be complete",
+        english: "these things write We so that joy our may be complete",
       },
       {
         verse_number: 5,
         original:
           "Καὶ ἔστιν αὕτη ἡ ἀγγελία ἣν ἀκηκόαμεν ἀπ’ αὐτοῦ καὶ ἀναγγέλλομεν ὑμῖν ὅτι ὁ Θεὸς φῶς ἐστιν καὶ σκοτία ἐν αὐτῷ οὐκ ἔστιν οὐδεμία",
         english:
-          "And is this the message - we have heard from Him and announce to you - - God light is and darkness in Him no there is [at all]",
+          "And is this the message we have heard from Him and announce to you God light is and darkness in Him no there is [at all]",
       },
       {
         verse_number: 6,
         original:
           "Ἐὰν εἴπωμεν ὅτι κοινωνίαν ἔχομεν μετ’ αὐτοῦ καὶ ἐν τῷ σκότει περιπατῶμεν ψευδόμεθα καὶ οὐ ποιοῦμεν τὴν ἀλήθειαν",
         english:
-          "If we say - fellowship we have with Him yet in the darkness walk we lie and {do} not practice the truth",
+          "If we say fellowship we have with Him yet in the darkness walk we lie and {do} not practice the truth",
       },
       {
         verse_number: 7,
         original:
           "ἐὰν δὲ ἐν τῷ φωτὶ περιπατῶμεν ὡς αὐτός ἐστιν ἐν τῷ φωτί κοινωνίαν ἔχομεν μετ’ ἀλλήλων καὶ τὸ αἷμα Ἰησοῦ τοῦ Υἱοῦ αὐτοῦ καθαρίζει ἡμᾶς ἀπὸ πάσης ἁμαρτίας",
         english:
-          "if But in the light we walk as He is in the light fellowship we have with one another and the blood of Jesus - Son His cleanses us from all sin",
+          "if But in the light we walk as He is in the light fellowship we have with one another and the blood of Jesus Son His cleanses us from all sin",
       },
       {
         verse_number: 8,
         original:
           "Ἐὰν εἴπωμεν ὅτι ἁμαρτίαν οὐκ ἔχομεν ἑαυτοὺς πλανῶμεν καὶ ἡ ἀλήθεια οὐκ ἔστιν ἐν ἡμῖν",
         english:
-          "If we say - sin no we have ourselves we deceive and the truth not is in us",
+          "If we say sin no we have ourselves we deceive and the truth not is in us",
       },
       {
         verse_number: 9,
         original:
           "ἐὰν ὁμολογῶμεν τὰς ἁμαρτίας ἡμῶν πιστός ἐστιν καὶ δίκαιος ἵνα ἀφῇ ἡμῖν τὰς ἁμαρτίας καὶ καθαρίσῃ ἡμᾶς ἀπὸ πάσης ἀδικίας",
         english:
-          "If we confess - sins our faithful He is and just to forgive us [our] sins and to cleanse us from all unrighteousness",
+          "If we confess sins our faithful He is and just to forgive us [our] sins and to cleanse us from all unrighteousness",
       },
       {
         verse_number: 10,
         original:
           "ἐὰν εἴπωμεν ὅτι οὐχ ἡμαρτήκαμεν ψεύστην ποιοῦμεν αὐτὸν καὶ ὁ λόγος αὐτοῦ οὐκ ἔστιν ἐν ἡμῖν",
         english:
-          "If we say - we have not sinned a liar we make Him out to be and - word His not is in us",
+          "If we say we have not sinned a liar we make Him out to be and word His not is in us",
       },
     ],
   };
