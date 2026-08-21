46a5038c92d2af718a365528568fff4c90d4d297
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:27:29 +0800

    function_rename app_g_map_generate_waters_next app_shared_game_map_generate_waters_next

--- js/app_g_map_generate_waters.mjs
+++ js/app_g_map_generate_waters.mjs
@@ -4,7 +4,7 @@ import { each_range } from "./each_range.mjs";
 import { list_remove_add } from "./list_remove_add.mjs";
 import { list_set_nested_y_x } from "./list_set_nested_y_x.mjs";
 import { g_water } from "./g_water.mjs";
-import { app_g_map_generate_waters_next } from "./app_g_map_generate_waters_next.mjs";
+import { app_shared_game_map_generate_waters_next } from "./app_shared_game_map_generate_waters_next.mjs";
 import { floor } from "./floor.mjs";
 import { multiply } from "./multiply.mjs";
 export function app_g_map_generate_waters(rows) {
@@ -13,7 +13,7 @@ export function app_g_map_generate_waters(rows) {
   let water_count = floor(multiply(total, 0.3));
   let waters = [];
   function lambda(i) {
-    let r = app_g_map_generate_waters_next(waters, coordinates);
+    let r = app_shared_game_map_generate_waters_next(waters, coordinates);
     let w = g_water();
     list_set_nested_y_x(rows, r, w);
     list_remove_add(coordinates, waters, r);
--- js/app_shared_game_map_generate_waters_next.mjs
+++ js/app_shared_game_map_generate_waters_next.mjs
@@ -1,7 +1,7 @@
 import { g_distance_1_any_random } from "./g_distance_1_any_random.mjs";
 import { list_random_item } from "./list_random_item.mjs";
 import { list_empty_is } from "./list_empty_is.mjs";
-export function app_g_map_generate_waters_next(waters, coordinates) {
+export function app_shared_game_map_generate_waters_next(waters, coordinates) {
   let r = null;
   let e = list_empty_is(waters);
   if (e) {
