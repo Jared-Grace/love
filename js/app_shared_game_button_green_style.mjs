33a5f52bf6ac960852e5bdfbf6b89a9a1555352a
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:25:16 +0800

    function_rename app_g_button_green_style app_shared_game_button_green_style

--- js/app_shared_game_button_green_style.mjs
+++ js/app_shared_game_button_green_style.mjs
@@ -1,5 +1,5 @@
 import { app_shared_game_button_green_style_generic } from "./app_shared_game_button_green_style_generic.mjs";
-export function app_g_button_green_style(component) {
+export function app_shared_game_button_green_style(component) {
   let alpha_channel = "dd";
   app_shared_game_button_green_style_generic(component, alpha_channel);
 }
