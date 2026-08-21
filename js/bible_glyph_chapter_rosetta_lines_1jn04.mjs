bb98090c68a97c14b402ef4d8819f70a8178fcee
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 19:57:01 +0800

    bible_glyph_chapter_rosetta_lines_rewrite 1JN04

--- js/bible_glyph_chapter_rosetta_lines_1jn04.mjs
+++ js/bible_glyph_chapter_rosetta_lines_1jn04.mjs
@@ -11,28 +11,28 @@ export function bible_glyph_chapter_rosetta_lines_1jn04() {
         original:
           "Ἀγαπητοί μὴ παντὶ πνεύματι πιστεύετε ἀλλὰ δοκιμάζετε τὰ πνεύματα εἰ ἐκ τοῦ Θεοῦ ἐστιν ὅτι πολλοὶ ψευδοπροφῆται ἐξεληλύθασιν εἰς τὸν κόσμον",
         english:
-          "Beloved every spirit do not believe but test the spirits [to see] whether from - God they are For many false prophets have gone out into the world",
+          "Beloved every spirit do not believe but test the spirits [to see] whether from God they are For many false prophets have gone out into the world",
       },
       {
         verse_number: 2,
         original:
           "Ἐν τούτῳ γινώσκετε τὸ Πνεῦμα τοῦ Θεοῦ πᾶν πνεῦμα ὃ ὁμολογεῖ Ἰησοῦν Χριστὸν ἐν σαρκὶ ἐληλυθότα ἐκ τοῦ Θεοῦ ἐστιν",
         english:
-          "By this you will know the Spirit - of God Every spirit that confesses [that] Jesus Christ in [the] flesh has come from - God is",
+          "By this you will know the Spirit of God Every spirit that confesses [that] Jesus Christ in [the] flesh has come from God is",
       },
       {
         verse_number: 3,
         original:
           "καὶ πᾶν πνεῦμα ὃ μὴ ὁμολογεῖ τὸν Ἰησοῦν ἐκ τοῦ Θεοῦ οὐκ ἔστιν καὶ τοῦτό ἐστιν τὸ τοῦ ἀντιχρίστου ὃ ἀκηκόατε ὅτι ἔρχεται καὶ νῦν ἐν τῷ κόσμῳ ἐστὶν ἤδη",
         english:
-          "and every spirit that does not confess - Jesus from - God not is - This is the [spirit] of the antichrist which you have heard - is coming and at this time in the world [which] is already",
+          "and every spirit that does not confess Jesus from God not is This is the [spirit] of the antichrist which you have heard is coming and at this time in the world [which] is already",
       },
       {
         verse_number: 4,
         original:
           "Ὑμεῖς ἐκ τοῦ Θεοῦ ἐστε τεκνία καὶ νενικήκατε αὐτούς ὅτι μείζων ἐστὶν ὁ ἐν ὑμῖν ἢ ὁ ἐν τῷ κόσμῳ",
         english:
-          "You from - God are little children and have overcome them because greater is He who [is] in you than he who [is] in the world",
+          "You from God are little children and have overcome them because greater is He who [is] in you than he who [is] in the world",
       },
       {
         verse_number: 5,
@@ -46,55 +46,53 @@ export function bible_glyph_chapter_rosetta_lines_1jn04() {
         original:
           "ἡμεῖς ἐκ τοῦ Θεοῦ ἐσμεν ὁ γινώσκων τὸν Θεὸν ἀκούει ἡμῶν ὃς οὐκ ἔστιν ἐκ τοῦ Θεοῦ οὐκ ἀκούει ἡμῶν ἐκ τούτου γινώσκομεν τὸ πνεῦμα τῆς ἀληθείας καὶ τὸ πνεῦμα τῆς πλάνης",
         english:
-          "We from - God are Whoever knows - God listens to us whoever not is from - God does not listen to us That is how we know the Spirit - of truth and the spirit - of deception",
+          "We from God are Whoever knows God listens to us whoever not is from God does not listen to us That is how we know the Spirit of truth and the spirit of deception",
       },
       {
         verse_number: 7,
         original:
           "Ἀγαπητοί ἀγαπῶμεν ἀλλήλους ὅτι ἡ ἀγάπη ἐκ τοῦ Θεοῦ ἐστιν καὶ πᾶς ὁ ἀγαπῶν ἐκ τοῦ Θεοῦ γεγέννηται καὶ γινώσκει τὸν Θεόν",
         english:
-          "Beloved let us love one another because - love from - God comes - Everyone who loves of - God has been born and knows - God",
+          "Beloved let us love one another because love from God comes Everyone who loves of God has been born and knows God",
       },
       {
         verse_number: 8,
         original: "ὁ μὴ ἀγαπῶν οὐκ ἔγνω τὸν Θεόν ὅτι ὁ Θεὸς ἀγάπη ἐστίν",
-        english:
-          "Whoever does not love does not know - God because - God love is",
+        english: "Whoever does not love does not know God because God love is",
       },
       {
         verse_number: 9,
         original:
           "Ἐν τούτῳ ἐφανερώθη ἡ ἀγάπη τοῦ Θεοῦ ἐν ἡμῖν ὅτι τὸν Υἱὸν αὐτοῦ τὸν μονογενῆ ἀπέσταλκεν ὁ Θεὸς εἰς τὸν κόσμον ἵνα ζήσωμεν δι’ αὐτοῦ",
         english:
-          "This is how was revealed - love - God’s among us - - Son His - one and only sent - God into the world so that we might live through Him",
+          "This is how was revealed love God’s among us Son His one and only sent God into the world so that we might live through Him",
       },
       {
         verse_number: 10,
         original:
           "ἐν τούτῳ ἐστὶν ἡ ἀγάπη οὐχ ὅτι ἡμεῖς ἠγαπήκαμεν τὸν Θεόν ἀλλ’ ὅτι αὐτὸς ἠγάπησεν ἡμᾶς καὶ ἀπέστειλεν τὸν Υἱὸν αὐτοῦ ἱλασμὸν περὶ τῶν ἁμαρτιῶν ἡμῶν",
         english:
-          "in this consists - [And] love not that we loved - God but that He loved us and sent - Son His [as the] atoning sacrifice for - sins our",
+          "in this consists [And] love not that we loved God but that He loved us and sent Son His [as the] atoning sacrifice for sins our",
       },
       {
         verse_number: 11,
         original:
           "Ἀγαπητοί εἰ οὕτως ὁ Θεὸς ἠγάπησεν ἡμᾶς καὶ ἡμεῖς ὀφείλομεν ἀλλήλους ἀγαπᾶν",
-        english:
-          "Beloved if so - God loved us also we ought one another to love",
+        english: "Beloved if so God loved us also we ought one another to love",
       },
       {
         verse_number: 12,
         original:
           "Θεὸν οὐδεὶς πώποτε τεθέαται ἐὰν ἀγαπῶμεν ἀλλήλους ὁ Θεὸς ἐν ἡμῖν μένει καὶ ἡ ἀγάπη αὐτοῦ τετελειωμένη ἐν ἡμῖν ἐστιν",
         english:
-          "God No one has ever seen [but] if we love one another - God in us remains and - love His perfected in us is",
+          "God No one has ever seen [but] if we love one another God in us remains and love His perfected in us is",
       },
       {
         verse_number: 13,
         original:
           "ἐν τούτῳ γινώσκομεν ὅτι ἐν αὐτῷ μένομεν καὶ αὐτὸς ἐν ἡμῖν ὅτι ἐκ τοῦ Πνεύματος αὐτοῦ δέδωκεν ἡμῖν",
         english:
-          "By this we know that in Him we remain and He in us - of - Spirit His He has given us",
+          "By this we know that in Him we remain and He in us of Spirit His He has given us",
       },
       {
         verse_number: 14,
@@ -108,28 +106,28 @@ export function bible_glyph_chapter_rosetta_lines_1jn04() {
         original:
           "Ὃς ἐὰν ὁμολογήσῃ ὅτι Ἰησοῦς ἐστιν ὁ Υἱὸς τοῦ Θεοῦ ὁ Θεὸς ἐν αὐτῷ μένει καὶ αὐτὸς ἐν τῷ Θεῷ",
         english:
-          "anyone If confesses that Jesus is the Son - of God - God in him abides and he in - God",
+          "anyone If confesses that Jesus is the Son of God God in him abides and he in God",
       },
       {
         verse_number: 16,
         original:
           "καὶ ἡμεῖς ἐγνώκαμεν καὶ πεπιστεύκαμεν τὴν ἀγάπην ἣν ἔχει ὁ Θεὸς ἐν ἡμῖν Ὁ Θεὸς ἀγάπη ἐστίν καὶ ὁ μένων ἐν τῇ ἀγάπῃ ἐν τῷ Θεῷ μένει καὶ ὁ Θεὸς ἐν αὐτῷ μένει",
         english:
-          "And we have come to know and believe the love that has - God for us - God love is - whoever abides in - love in - God abides and - God in him -",
+          "And we have come to know and believe the love that has God for us God love is whoever abides in love in God abides and God in him",
       },
       {
         verse_number: 17,
         original:
           "Ἐν τούτῳ τετελείωται ἡ ἀγάπη μεθ’ ἡμῶν ἵνα παρρησίαν ἔχωμεν ἐν τῇ ἡμέρᾳ τῆς κρίσεως ὅτι καθὼς ἐκεῖνός ἐστιν καὶ ἡμεῖς ἐσμεν ἐν τῷ κόσμῳ τούτῳ",
         english:
-          "In this [way] has been perfected - love among us so that confidence we may have on the day - of judgment for just like Him - we are in - world this",
+          "In this [way] has been perfected love among us so that confidence we may have on the day of judgment for just like Him we are in world this",
       },
       {
         verse_number: 18,
         original:
           "Φόβος οὐκ ἔστιν ἐν τῇ ἀγάπῃ ἀλλ’ ἡ τελεία ἀγάπη ἔξω βάλλει τὸν φόβον ὅτι ὁ φόβος κόλασιν ἔχει ὁ δὲ φοβούμενος οὐ τετελείωται ἐν τῇ ἀγάπῃ",
         english:
-          "fear no There is in - love but - perfect love out drives - fear because - fear punishment involves The [one who] - fears has not been perfected in - love",
+          "fear no There is in love but perfect love out drives fear because fear punishment involves The [one who] fears has not been perfected in love",
       },
       {
         verse_number: 19,
@@ -141,14 +139,14 @@ export function bible_glyph_chapter_rosetta_lines_1jn04() {
         original:
           "Ἐάν τις εἴπῃ ὅτι Ἀγαπῶ τὸν Θεόν καὶ τὸν ἀδελφὸν αὐτοῦ μισῇ ψεύστης ἐστίν ὁ γὰρ μὴ ἀγαπῶν τὸν ἀδελφὸν αὐτοῦ ὃν ἑώρακεν τὸν Θεὸν ὃν οὐχ ἑώρακεν οὐ δύναται ἀγαπᾶν",
         english:
-          "If anyone says - I love - God [but] - brother his hates a liar he is anyone who For does not love - brother his whom he has seen - God whom he has not seen cannot love",
+          "If anyone says I love God [but] brother his hates a liar he is anyone who For does not love brother his whom he has seen God whom he has not seen cannot love",
       },
       {
         verse_number: 21,
         original:
           "καὶ ταύτην τὴν ἐντολὴν ἔχομεν ἀπ’ αὐτοῦ ἵνα ὁ ἀγαπῶν τὸν Θεὸν ἀγαπᾷ καὶ τὸν ἀδελφὸν αὐτοῦ",
         english:
-          "And this - commandment we have from Him - Whoever loves - God must love as well - brother his",
+          "And this commandment we have from Him Whoever loves God must love as well brother his",
       },
     ],
   };
