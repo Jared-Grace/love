333b0cb4d7e2cd4475ee57ddfc6507012e986f14
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 19:57:03 +0800

    bible_glyph_chapter_rosetta_lines_rewrite JHN14

--- js/bible_glyph_chapter_rosetta_lines_jhn14.mjs
+++ js/bible_glyph_chapter_rosetta_lines_jhn14.mjs
@@ -11,14 +11,14 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "Μὴ ταρασσέσθω ὑμῶν ἡ καρδία πιστεύετε εἰς τὸν Θεόν καὶ εἰς ἐμὲ πιστεύετε",
         english:
-          "{Do} not {let} be troubled your - hearts You believe in - God as well in Me believe",
+          "{Do} not {let} be troubled your hearts You believe in God as well in Me believe",
       },
       {
         verse_number: 2,
         original:
           "ἐν τῇ οἰκίᾳ τοῦ Πατρός μου μοναὶ πολλαί εἰσιν εἰ δὲ μή εἶπον ἂν ὑμῖν ὅτι πορεύομαι ἑτοιμάσαι τόπον ὑμῖν",
         english:
-          "In - house - Father’s My rooms many are If - [it were] not would I have told so you that I am going [there] to prepare a place for you",
+          "In house Father’s My rooms many are If [it were] not would I have told so you that I am going [there] to prepare a place for you",
       },
       {
         verse_number: 3,
@@ -30,28 +30,28 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
       {
         verse_number: 4,
         original: "καὶ ὅπου ἐγὼ ὑπάγω οἴδατε τὴν ὁδόν",
-        english: "- to the place where I am going You know the way",
+        english: "to the place where I am going You know the way",
       },
       {
         verse_number: 5,
         original:
           "Λέγει αὐτῷ Θωμᾶς Κύριε οὐκ οἴδαμεν ποῦ ὑπάγεις πῶς δυνάμεθα τὴν ὁδὸν",
         english:
-          "said - Thomas Lord we do not know where You are going [so] how can we the way",
+          "said Thomas Lord we do not know where You are going [so] how can we the way",
       },
       {
         verse_number: 6,
         original:
           "Λέγει αὐτῷ Ἰησοῦς Ἐγώ εἰμι ἡ ὁδὸς καὶ ἡ ἀλήθεια καὶ ἡ ζωή οὐδεὶς ἔρχεται πρὸς τὸν Πατέρα εἰ μὴ δι’ ἐμοῦ",
         english:
-          "answered - Jesus I am the way and the truth and the life No one comes to the Father except through Me",
+          "answered Jesus I am the way and the truth and the life No one comes to the Father except through Me",
       },
       {
         verse_number: 7,
         original:
           "εἰ ἐγνώκειτέ με καὶ τὸν Πατέρα μου ἀπ’ ἄρτι γινώσκετε αὐτὸν καὶ ἑωράκατε",
         english:
-          "If you had known Me - - Father My From now on you do know Him and have seen",
+          "If you had known Me Father My From now on you do know Him and have seen",
       },
       {
         verse_number: 8,
@@ -65,14 +65,14 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "Λέγει αὐτῷ ὁ Ἰησοῦς Τοσούτῳ χρόνῳ μεθ’ ὑμῶν εἰμι καὶ οὐκ ἔγνωκάς με Φίλιππε ὁ ἑωρακὼς ἐμὲ ἑώρακεν τὸν Πατέρα πῶς σὺ λέγεις Δεῖξον ἡμῖν τὸν Πατέρα",
         english:
-          "replied - Jesus all this time with you I have been and still you do not know Me Philip Anyone who has seen Me has seen the Father How {can} you say Show us the Father",
+          "replied Jesus all this time with you I have been and still you do not know Me Philip Anyone who has seen Me has seen the Father How {can} you say Show us the Father",
       },
       {
         verse_number: 10,
         original:
           "οὐ πιστεύεις ὅτι ἐγὼ ἐν τῷ Πατρὶ καὶ ὁ Πατὴρ ἐν ἐμοί ἐστιν τὰ ῥήματα ἃ ἐγὼ λέγω ὑμῖν ἀπ’ ἐμαυτοῦ οὐ λαλῶ ὁ δὲ Πατὴρ ἐν ἐμοὶ μένων ποιεῖ τὰ ἔργα αὐτοῦ",
         english:
-          "Do you not believe that I [am] in the Father and the Father in Me is The words - I say to you on My own I do not speak {it is} the Instead Father in Me dwelling performing - works His",
+          "Do you not believe that I [am] in the Father and the Father in Me is The words I say to you on My own I do not speak {it is} the Instead Father in Me dwelling performing works His",
       },
       {
         verse_number: 11,
@@ -93,17 +93,17 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "καὶ ὅ¦τι ἂν αἰτήσητε ἐν τῷ ὀνόματί μου τοῦτο ποιήσω ἵνα δοξασθῇ ὁ Πατὴρ ἐν τῷ Υἱῷ",
         english:
-          "And whatever you ask in - name My - I will do so that may be glorified the Father in the Son",
+          "And whatever you ask in name My I will do so that may be glorified the Father in the Son",
       },
       {
         verse_number: 14,
         original: "ἐάν τι αἰτήσητέ με ἐν τῷ ὀνόματί μου ἐγὼ ποιήσω",
-        english: "If for anything you ask Me in - name My I will do [it]",
+        english: "If for anything you ask Me in name My I will do [it]",
       },
       {
         verse_number: 15,
         original: "Ἐὰν ἀγαπᾶτέ με τὰς ἐντολὰς τὰς ἐμὰς τηρήσετε",
-        english: "If you love Me - commandments - My you will keep",
+        english: "If you love Me commandments My you will keep",
       },
       {
         verse_number: 16,
@@ -117,7 +117,7 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "τὸ Πνεῦμα τῆς ἀληθείας ὃ ὁ κόσμος οὐ δύναται λαβεῖν ὅτι οὐ θεωρεῖ αὐτὸ οὐδὲ γινώσκει ὑμεῖς γινώσκετε αὐτό ὅτι παρ’ ὑμῖν μένει καὶ ἐν ὑμῖν ἔσται",
         english:
-          "the Spirit - of truth [Him] The world cannot receive because it neither sees Him nor knows [Him] But you do know Him for with you He abides and in you will be",
+          "the Spirit of truth [Him] The world cannot receive because it neither sees Him nor knows [Him] But you do know Him for with you He abides and in you will be",
       },
       {
         verse_number: 18,
@@ -129,42 +129,42 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "ἔτι μικρὸν καὶ ὁ κόσμος με οὐκέτι θεωρεῖ ὑμεῖς δὲ θεωρεῖτέ με ὅτι ἐγὼ ζῶ καὶ ὑμεῖς ζήσετε",
         english:
-          "In a little while - the world Me no more will see you but will see Me Because I live also you will live",
+          "In a little while the world Me no more will see you but will see Me Because I live also you will live",
       },
       {
         verse_number: 20,
         original:
           "ἐν ἐκείνῃ τῇ ἡμέρᾳ γνώσεσθε ὑμεῖς ὅτι ἐγὼ ἐν τῷ Πατρί μου καὶ ὑμεῖς ἐν ἐμοὶ κἀγὼ ἐν ὑμῖν",
         english:
-          "On that - day will know you that I [am] in - Father My and you [are] in Me and I [am] in you",
+          "On that day will know you that I [am] in Father My and you [are] in Me and I [am] in you",
       },
       {
         verse_number: 21,
         original:
           "ὁ ἔχων τὰς ἐντολάς μου καὶ τηρῶν αὐτὰς ἐκεῖνός ἐστιν ὁ ἀγαπῶν με ὁ δὲ ἀγαπῶν με ἀγαπηθήσεται ὑπὸ τοῦ Πατρός μου κἀγὼ ἀγαπήσω αὐτὸν καὶ ἐμφανίσω αὐτῷ ἐμαυτόν",
         english:
-          "Whoever has - commandments My and keeps them - is the [one who] loves Me The [one who] - loves Me will be loved by - Father My and I will love him and reveal to him Myself",
+          "Whoever has commandments My and keeps them is the [one who] loves Me The [one who] loves Me will be loved by Father My and I will love him and reveal to him Myself",
       },
       {
         verse_number: 22,
         original:
           "Λέγει αὐτῷ Ἰούδας οὐχ ὁ Ἰσκαριώτης Κύριε καὶ τί γέγονεν ὅτι ἡμῖν μέλλεις ἐμφανίζειν σεαυτὸν καὶ οὐχὶ τῷ κόσμῳ",
         english:
-          "asked Him Judas (not - Iscariot) Lord - why to us are You going to reveal Yourself and not to the world",
+          "asked Him Judas (not Iscariot) Lord why to us are You going to reveal Yourself and not to the world",
       },
       {
         verse_number: 23,
         original:
           "Ἀπεκρίθη Ἰησοῦς καὶ εἶπεν αὐτῷ Ἐάν τις ἀγαπᾷ με τὸν λόγον μου τηρήσει καὶ ὁ Πατήρ μου ἀγαπήσει αὐτόν καὶ πρὸς αὐτὸν ἐλευσόμεθα καὶ μονὴν παρ’ αὐτῷ ποιησόμεθα",
         english:
-          "replied Jesus If anyone loves Me - word My he will keep - - Father My will love him and to him We will come and Our home with him make",
+          "replied Jesus If anyone loves Me word My he will keep Father My will love him and to him We will come and Our home with him make",
       },
       {
         verse_number: 24,
         original:
           "ὁ μὴ ἀγαπῶν με τοὺς λόγους μου οὐ τηρεῖ καὶ ὁ λόγος ὃν ἀκούετε οὐκ ἔστιν ἐμὸς ἀλλὰ τοῦ πέμψαντός με Πατρός",
         english:
-          "Whoever does not love Me - words My does not keep - The word that you hear not is My own but [it is ] from the who sent Me Father",
+          "Whoever does not love Me words My does not keep The word that you hear not is My own but [it is ] from the who sent Me Father",
       },
       {
         verse_number: 25,
@@ -176,21 +176,21 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "ὁ δὲ Παράκλητος τὸ Πνεῦμα τὸ Ἅγιον ὃ πέμψει ὁ Πατὴρ ἐν τῷ ὀνόματί μου ἐκεῖνος ὑμᾶς διδάξει πάντα καὶ ὑπομνήσει ὑμᾶς πάντα ἃ εἶπον ὑμῖν ἐγώ",
         english:
-          "the But Advocate the Spirit - Holy whom will send the Father in - name My - you will teach all things and will remind you of everything have told you I",
+          "the But Advocate the Spirit Holy whom will send the Father in name My you will teach all things and will remind you of everything have told you I",
       },
       {
         verse_number: 27,
         original:
           "Εἰρήνην ἀφίημι ὑμῖν εἰρήνην τὴν ἐμὴν δίδωμι ὑμῖν οὐ καθὼς ὁ κόσμος δίδωσιν ἐγὼ δίδωμι ὑμῖν μὴ ταρασσέσθω ὑμῶν ἡ καρδία μηδὲ δειλιάτω",
         english:
-          "Peace I leave with you peace - My I give to you not as the world gives I {do} give to you {Do} not {let} be troubled your - hearts {do} not be afraid",
+          "Peace I leave with you peace My I give to you not as the world gives I {do} give to you {Do} not {let} be troubled your hearts {do} not be afraid",
       },
       {
         verse_number: 28,
         original:
           "ἠκούσατε ὅτι ἐγὼ εἶπον ὑμῖν Ὑπάγω καὶ ἔρχομαι πρὸς ὑμᾶς εἰ ἠγαπᾶτέ με ἐχάρητε ἄν ὅτι πορεύομαι πρὸς τὸν Πατέρα ὅτι ὁ Πατὴρ μείζων μού ἐστιν",
         english:
-          "You heard - Me say - I am going away and I am coming [back] to you If you loved Me you would rejoice that I am going to the Father because the Father greater than I is",
+          "You heard Me say I am going away and I am coming [back] to you If you loved Me you would rejoice that I am going to the Father because the Father greater than I is",
       },
       {
         verse_number: 29,
@@ -211,7 +211,7 @@ export function bible_glyph_chapter_rosetta_lines_jhn14() {
         original:
           "ἀλλ’ ἵνα γνῷ ὁ κόσμος ὅτι ἀγαπῶ τὸν Πατέρα καὶ καθὼς ἐνετείλατο μοι ὁ Πατὴρ οὕτως ποιῶ Ἐγείρεσθε ἄγωμεν ἐντεῦθεν",
         english:
-          "But so that may know the world that I love the Father - exactly what has commanded Me the Father - I do Get up Let us go on from here",
+          "But so that may know the world that I love the Father exactly what has commanded Me the Father I do Get up Let us go on from here",
       },
     ],
   };
