3561db4897a8fb7c3590547c8335629f641563e4
JESUS rose to life <username@users.noreply.github.com>
2026-08-21 20:26:52 +0800

    function_rename app_g_div_map_style_rows app_shared_game_div_map_style_rows

--- js/app_g_div_map_style.mjs
+++ js/app_g_div_map_style.mjs
@@ -1,6 +1,6 @@
 import { app_g_rows_get } from "./app_g_rows_get.mjs";
-import { app_g_div_map_style_rows } from "./app_g_div_map_style_rows.mjs";
+import { app_shared_game_div_map_style_rows } from "./app_shared_game_div_map_style_rows.mjs";
 export async function app_g_div_map_style(div_map) {
   let rows = await app_g_rows_get();
-  app_g_div_map_style_rows(div_map, rows);
+  app_shared_game_div_map_style_rows(div_map, rows);
 }
