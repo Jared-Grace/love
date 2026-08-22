ef2c892242f387f46b5648375ee7bdc8458eb5d1
JESUS rose to life <username@users.noreply.github.com>
2026-08-22 08:58:49 +0800

    ai

--- js/webpack_watch_schedule_if_stale.mjs
+++ js/webpack_watch_schedule_if_stale.mjs
@@ -3,11 +3,10 @@ import { webpack_watch_bundle_stale_is } from "./webpack_watch_bundle_stale_is.m
 export async function webpack_watch_schedule_if_stale(
   ad,
   a_name_of,
-  dev_relative,
   build_schedule,
 ) {
-  arguments_assert(arguments, 4);
-  let stale = await webpack_watch_bundle_stale_is(ad, a_name_of, dev_relative);
+  arguments_assert(arguments, 3);
+  let stale = await webpack_watch_bundle_stale_is(ad, a_name_of);
   if (stale) {
     let a_name = a_name_of(ad);
     build_schedule(a_name);
