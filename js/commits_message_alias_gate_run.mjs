e2dd1c69c7b9fc653ec457a83491b8384d49a1fa
JESUS rose to life <username@users.noreply.github.com>
2026-08-23 09:25:05 +0800

    function_rename commits_message_alias_since commits_message_rules_since

--- js/commits_message_alias_gate_run.mjs
+++ js/commits_message_alias_gate_run.mjs
@@ -1,6 +1,6 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { commits_message_alias_named } from "./commits_message_alias_named.mjs";
-import { commits_message_alias_since } from "./commits_message_alias_since.mjs";
+import { commits_message_rules_since } from "./commits_message_rules_since.mjs";
 import { list_empty_is_assert_walked_generic } from "./list_empty_is_assert_walked_generic.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { property_get } from "./property_get.mjs";
@@ -12,8 +12,8 @@ export async function commits_message_alias_gate_run() {
   let told = await commits_message_alias_named();
   let walked = property_get(told, "walked");
   let offenders = property_get(told, "offenders");
-  let since = commits_message_alias_since();
-  let f_name = fn_name("commits_message_alias_since");
+  let since = commits_message_rules_since();
+  let f_name = fn_name("commits_message_rules_since");
   let f_name2 = fn_name("ai_git_command_args");
   let f_name3 = fn_name("ai_git");
   let hint = text_combine_multiple([
