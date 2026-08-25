fd38efff7c45022f44dcbf275426b3f71f1a748c
JESUS rose to life <username@users.noreply.github.com>
2026-08-25 19:12:38 +0800

    ai

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { public_chunks_orphaned_gate_run } from "./public_chunks_orphaned_gate_run.mjs";
 import { js_source_bare_token_kind_cases_gate_run } from "./js_source_bare_token_kind_cases_gate_run.mjs";
 import { list_starts_with_cases_gate_run } from "./list_starts_with_cases_gate_run.mjs";
 import { bible_versions_english_choices_words_none_everywhere_gate_run } from "./bible_versions_english_choices_words_none_everywhere_gate_run.mjs";
