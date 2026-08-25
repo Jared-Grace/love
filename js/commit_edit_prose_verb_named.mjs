d000e6197307e4ca8444ddfa0100e87ba987fcd2
JESUS rose to life <username@users.noreply.github.com>
2026-08-25 17:26:42 +0800

    file_imports_repair js/commit_edit_prose_verb_named.mjs

--- js/commit_edit_prose_verb_named.mjs
+++ js/commit_edit_prose_verb_named.mjs
@@ -1,3 +1,4 @@
+import { commit_edit_prose_counts } from "./commit_edit_prose_counts.mjs";
 import { diff_lines_kind_counts } from "./diff_lines_kind_counts.mjs";
 import { property_get } from "./property_get.mjs";
 import { equal_not } from "./equal_not.mjs";
