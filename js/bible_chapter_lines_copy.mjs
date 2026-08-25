c071c3e2333a98970bf8812b0329c6dabdcea685
JESUS rose to life <username@users.noreply.github.com>
2026-08-25 17:52:42 +0800

    ai

---
+++ js/bible_chapter_lines_copy.mjs
@@ -0,0 +1,5 @@
+import { bible_chapter_lines } from "./bible_chapter_lines.mjs";
+export async function bible_chapter_lines_copy(bible_folder, chapter_code) {
+  let r = await bible_chapter_lines(bible_folder, chapter_code);
+  return r;
+}
