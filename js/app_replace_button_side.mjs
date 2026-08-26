cb7d21710fdb4e070e538fec4a5e5cfaf7236266
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:09:31 +0800

    function_auto_checked app_replace_button_side

--- js/app_replace_button_side.mjs
+++ js/app_replace_button_side.mjs
@@ -1,5 +1,5 @@
-import { app_shared_symbol_tile } from "../../love/js/app_shared_symbol_tile.mjs";
-import { list_map } from "../../love/js/list_map.mjs";
+import { app_shared_symbol_tile } from "./app_shared_symbol_tile.mjs";
+import { list_map } from "./list_map.mjs";
 export function app_replace_button_side(parent, list_symbols) {
   function symbol_each(symbol) {
     let span = app_shared_symbol_tile(parent, symbol);
