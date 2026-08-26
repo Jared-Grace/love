a7384012e8ff4162049df12d498eb7ea98e49610
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:08:32 +0800

    function_auto_checked app_code_lesson_operators_value_max_range_1_next

--- js/app_code_lesson_operators_value_max_range_1_next.mjs
+++ js/app_code_lesson_operators_value_max_range_1_next.mjs
@@ -1,5 +1,5 @@
-import { range_1_next } from "../../love/js/range_1_next.mjs";
-import { app_code_lesson_operators_value_max } from "../../love/js/app_code_lesson_operators_value_max.mjs";
+import { range_1_next } from "./range_1_next.mjs";
+import { app_code_lesson_operators_value_max } from "./app_code_lesson_operators_value_max.mjs";
 export function app_code_lesson_operators_value_max_range_1_next() {
   let m = app_code_lesson_operators_value_max();
   let number_next = range_1_next(m);
