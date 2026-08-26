11185a4e15f5a4e40f9416450faa5941de925f33
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 18:04:02 +0800

    ai

--- js/app_code_lesson_quiz_token_select_buildable_kept.mjs
+++ js/app_code_lesson_quiz_token_select_buildable_kept.mjs
@@ -2,7 +2,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_sort_text } from "./list_sort_text.mjs";
-export function app_code_lesson_quiz_token_select_buttons(r) {
+export function app_code_lesson_quiz_token_select_buildable_kept(r) {
   arguments_assert(arguments, 1);
   let variation_buildable = property_get(r, "variation_buildable");
   let tokens_unique = property_get(r, "tokens_unique");
--- js/app_code_lesson_quiz_token_select_chosen.mjs
+++ js/app_code_lesson_quiz_token_select_chosen.mjs
@@ -1,9 +1,9 @@
 import { arguments_assert } from "./arguments_assert.mjs";
-import { app_code_lesson_quiz_token_select_buttons } from "./app_code_lesson_quiz_token_select_buttons.mjs";
+import { app_code_lesson_quiz_token_select_buildable_kept } from "./app_code_lesson_quiz_token_select_buildable_kept.mjs";
 import { property_get } from "./property_get.mjs";
 export function app_code_lesson_quiz_token_select_chosen(r) {
   arguments_assert(arguments, 1);
-  let r2 = app_code_lesson_quiz_token_select_buttons(r);
+  let r2 = app_code_lesson_quiz_token_select_buildable_kept(r);
   let buttons = property_get(r2, "buttons");
   let variations = property_get(r2, "variations");
   let tokens_unique = property_get(r2, "tokens_unique");
