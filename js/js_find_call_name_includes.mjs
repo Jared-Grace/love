2861663ddb9a10b4ab8683954d3ee2e3126ebc2b
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:14:02 +0800

    function_auto_checked js_find_call_name_includes

--- js/js_find_call_name_includes.mjs
+++ js/js_find_call_name_includes.mjs
@@ -1,6 +1,6 @@
-import { js_callee_name_includes_try_curried_right } from "../../love/js/js_callee_name_includes_try_curried_right.mjs";
-import { list_find } from "../../love/js/list_find.mjs";
-import { js_list_calls_nodes } from "../../love/js/js_list_calls_nodes.mjs";
+import { js_callee_name_includes_try_curried_right } from "./js_callee_name_includes_try_curried_right.mjs";
+import { list_find } from "./list_find.mjs";
+import { js_list_calls_nodes } from "./js_list_calls_nodes.mjs";
 export function js_find_call_name_includes(ast, part) {
   let nodes = js_list_calls_nodes(ast);
   let lambda = js_callee_name_includes_try_curried_right(part);
