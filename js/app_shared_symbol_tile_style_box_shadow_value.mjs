d4e26c692bc636c5fc6060cb98e698e16a578902
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:11:36 +0800

    function_auto_checked app_shared_symbol_tile_style_box_shadow_value

--- js/app_shared_symbol_tile_style_box_shadow_value.mjs
+++ js/app_shared_symbol_tile_style_box_shadow_value.mjs
@@ -1,4 +1,4 @@
-import { app_shared_symbol_tile_style_box_shadow_value_width } from "../../love/js/app_shared_symbol_tile_style_box_shadow_value_width.mjs";
+import { app_shared_symbol_tile_style_box_shadow_value_width } from "./app_shared_symbol_tile_style_box_shadow_value_width.mjs";
 export function app_shared_symbol_tile_style_box_shadow_value(color) {
   let width = "0.1";
   let r = app_shared_symbol_tile_style_box_shadow_value_width(color, width);
