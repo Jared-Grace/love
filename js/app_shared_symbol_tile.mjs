234f8b18c37e93d609c290a29361d0c1b20e4392
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:11:42 +0800

    function_auto_checked app_shared_symbol_tile

--- js/app_shared_symbol_tile.mjs
+++ js/app_shared_symbol_tile.mjs
@@ -1,5 +1,5 @@
-import { app_shared_symbol_tile_style } from "../../love/js/app_shared_symbol_tile_style.mjs";
-import { html_span_text } from "../../love/js/html_span_text.mjs";
+import { app_shared_symbol_tile_style } from "./app_shared_symbol_tile_style.mjs";
+import { html_span_text } from "./html_span_text.mjs";
 export function app_shared_symbol_tile(parent, symbol) {
   let span = html_span_text(parent, symbol);
   app_shared_symbol_tile_style(span);
