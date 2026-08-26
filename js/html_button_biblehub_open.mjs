9449630c3261e1d1216f8a464c9b45db939e624b
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:13:16 +0800

    function_auto_checked html_button_biblehub_open

--- js/html_button_biblehub_open.mjs
+++ js/html_button_biblehub_open.mjs
@@ -1,8 +1,8 @@
-import { app_shared_button } from "../../love/js/app_shared_button.mjs";
-import { window_open } from "../../love/js/window_open.mjs";
-import { equal } from "../../love/js/equal.mjs";
-import { text_replace_space_underscore_lower } from "../../love/js/text_replace_space_underscore_lower.mjs";
-import { text_combine_multiple } from "../../love/js/text_combine_multiple.mjs";
+import { app_shared_button } from "./app_shared_button.mjs";
+import { window_open } from "./window_open.mjs";
+import { equal } from "./equal.mjs";
+import { text_replace_space_underscore_lower } from "./text_replace_space_underscore_lower.mjs";
+import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function html_button_biblehub_open(
   parent,
   book_name,
@@ -17,19 +17,18 @@ export function html_button_biblehub_open(
     if (equal(replaced, "song")) {
       replaced = "songs";
     }
-    window_open(
-      text_combine_multiple([
-        "https://biblehub.com/",
-        folder,
-        replaced,
-        "/",
-        chapter_name,
-        "-",
-        verse_number,
-        ".htm",
-        ending,
-      ]),
-    );
+    let url = text_combine_multiple([
+      "https://biblehub.com/",
+      folder,
+      replaced,
+      "/",
+      chapter_name,
+      "-",
+      verse_number,
+      ".htm",
+      ending,
+    ]);
+    window_open(url);
   }
   let b = app_shared_button(parent, button_text, lambda);
   return b;
