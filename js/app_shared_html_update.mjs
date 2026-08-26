3e67a5a594d07f0829708a611bad3c7b3b947991
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:10:51 +0800

    function_auto_checked app_shared_html_update

--- js/app_shared_html_update.mjs
+++ js/app_shared_html_update.mjs
@@ -1,5 +1,5 @@
-import { html_public_exists_ensure } from "../../love/js/html_public_exists_ensure.mjs";
-import { html_update_dev_open } from "../../love/js/html_update_dev_open.mjs";
+import { html_public_exists_ensure } from "./html_public_exists_ensure.mjs";
+import { html_update_dev_open } from "./html_update_dev_open.mjs";
 export async function app_shared_html_update(name) {
   await html_public_exists_ensure(name);
   await html_update_dev_open(name);
