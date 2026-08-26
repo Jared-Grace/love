2f185cdb12ea9d11e805c416a922cd615071b05e
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:09:57 +0800

    function_auto_checked app_replace_symbol_tile_invalid

--- js/app_replace_symbol_tile_invalid.mjs
+++ js/app_replace_symbol_tile_invalid.mjs
@@ -1,5 +1,5 @@
-import { app_shared_symbol_tile_style_box_shadow } from "../../love/js/app_shared_symbol_tile_style_box_shadow.mjs";
-import { html_style_background_color_set } from "../../love/js/html_style_background_color_set.mjs";
+import { app_shared_symbol_tile_style_box_shadow } from "./app_shared_symbol_tile_style_box_shadow.mjs";
+import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
 export function app_replace_symbol_tile_invalid(sb) {
   html_style_background_color_set(sb, "#D10000");
   app_shared_symbol_tile_style_box_shadow(true, sb, "#FF8A8A");
