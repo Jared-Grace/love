05fe1805a6a63a0fb4e9256a46a53b52ee0abbd5
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:12:58 +0800

    function_auto_checked git_ac_call_folder_try_curried_right_2

--- js/git_ac_call_folder_try_curried_right_2.mjs
+++ js/git_ac_call_folder_try_curried_right_2.mjs
@@ -1,7 +1,8 @@
-import { git_ac_call_folder_try } from "../../love/js/git_ac_call_folder_try.mjs";
+import { git_ac_call_folder_try } from "./git_ac_call_folder_try.mjs";
 export function git_ac_call_folder_try_curried_right_2(f_name, args) {
   let c = async function git_ac_call_folder_try_curried_right_2_result(folder) {
-    return await git_ac_call_folder_try(folder, f_name, args);
+    let r = await git_ac_call_folder_try(folder, f_name, args);
+    return r;
   };
   return c;
 }
