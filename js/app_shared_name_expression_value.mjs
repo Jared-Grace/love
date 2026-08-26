83f4ea1c81189bf8536ab300105a11c782c2a6b9
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:10:57 +0800

    function_auto_checked app_shared_name_expression_value

--- js/app_shared_name_expression_value.mjs
+++ js/app_shared_name_expression_value.mjs
@@ -1,6 +1,6 @@
-import { app_shared_name_text_to_identifier } from "../../love/js/app_shared_name_text_to_identifier.mjs";
-import { null_is } from "../../love/js/null_is.mjs";
-import { app_shared_name_expression_find } from "../../love/js/app_shared_name_expression_find.mjs";
+import { app_shared_name_text_to_identifier } from "./app_shared_name_text_to_identifier.mjs";
+import { null_is } from "./null_is.mjs";
+import { app_shared_name_expression_find } from "./app_shared_name_expression_find.mjs";
 export function app_shared_name_expression_value(e, prefix) {
   let name = app_shared_name_expression_find(e);
   if (null_is(name)) {
