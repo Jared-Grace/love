3c97c0452161dc64a0108924409050cea1713027
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:11:06 +0800

    function_auto_checked app_shared_screen_set_button

--- js/app_shared_screen_set_button.mjs
+++ js/app_shared_screen_set_button.mjs
@@ -1,5 +1,5 @@
-import { app_shared_button } from "../../love/js/app_shared_button.mjs";
-import { app_shared_screen_later } from "../../love/js/app_shared_screen_later.mjs";
+import { app_shared_button } from "./app_shared_button.mjs";
+import { app_shared_screen_later } from "./app_shared_screen_later.mjs";
 export function app_shared_screen_set_button(parent, context, screen_fn, text) {
   let f = app_shared_screen_later(context, screen_fn);
   let b = app_shared_button(parent, text, f);
