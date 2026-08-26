4a819dc52131f852527d55e212dfe99f5122853a
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:14:49 +0800

    s bible,copy

--- js/sandbox_3.mjs
+++ js/sandbox_3.mjs
@@ -1,4 +1,4 @@
-import { g_sermon_write_coverage } from "../../love/js/g_sermon_write_coverage.mjs";
+import { g_sermon_write_coverage } from "./g_sermon_write_coverage.mjs";
 export async function sandbox_3() {
   let r = await g_sermon_write_coverage("1JN01", ["1"]);
   return r;
