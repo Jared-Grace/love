9c13ff2581fb2749946fd86358a7ee7cdc855810
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:13:24 +0800

    function_auto_checked html_button_toggle

--- js/html_button_toggle.mjs
+++ js/html_button_toggle.mjs
@@ -1,5 +1,5 @@
-import { app_shared_button } from "../../love/js/app_shared_button.mjs";
-import { app_shared_button_toggle_style } from "../../love/js/app_shared_button_toggle_style.mjs";
+import { app_shared_button } from "./app_shared_button.mjs";
+import { app_shared_button_toggle_style } from "./app_shared_button_toggle_style.mjs";
 export function html_button_toggle(parent, text, on, lambda) {
   let button = app_shared_button(parent, text, lambda);
   app_shared_button_toggle_style(on, button);
