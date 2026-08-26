279ac55959a02672601aaaa964beeb887f48b917
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:13:40 +0800

    function_auto_checked html_update_dev_open

--- js/html_update_dev_open.mjs
+++ js/html_update_dev_open.mjs
@@ -1,5 +1,5 @@
-import { html_update_dev } from "../../love/js/html_update_dev.mjs";
-import { file_open } from "../../love/js/file_open.mjs";
+import { html_update_dev } from "./html_update_dev.mjs";
+import { file_open } from "./file_open.mjs";
 export async function html_update_dev_open(name) {
   let path = await html_update_dev(name);
   await file_open(path);
