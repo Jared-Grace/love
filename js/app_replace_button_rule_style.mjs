2cab25d8afd7a67e4575992f28b475b4ca84abea
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:09:25 +0800

    function_auto_checked app_replace_button_rule_style

--- js/app_replace_button_rule_style.mjs
+++ js/app_replace_button_rule_style.mjs
@@ -1,6 +1,6 @@
-import { html_border_none } from "../../love/js/html_border_none.mjs";
-import { html_style_padding_y } from "../../love/js/html_style_padding_y.mjs";
-import { app_shared_symbol_tile_style } from "../../love/js/app_shared_symbol_tile_style.mjs";
+import { html_border_none } from "./html_border_none.mjs";
+import { html_style_padding_y } from "./html_style_padding_y.mjs";
+import { app_shared_symbol_tile_style } from "./app_shared_symbol_tile_style.mjs";
 export function app_replace_button_rule_style(b) {
   app_shared_symbol_tile_style(b);
   html_style_padding_y(b, "0.3em");
