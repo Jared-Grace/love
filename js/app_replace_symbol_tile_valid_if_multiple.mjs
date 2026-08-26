91c671bddbfe6ef72cca45531d324a2d9129bf57
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:10:10 +0800

    function_auto_checked app_replace_symbol_tile_valid_if_multiple

--- js/app_replace_symbol_tile_valid_if_multiple.mjs
+++ js/app_replace_symbol_tile_valid_if_multiple.mjs
@@ -1,5 +1,5 @@
-import { each } from "../../love/js/each.mjs";
-import { app_replace_symbol_tile_valid_if_curried_right } from "../../love/js/app_replace_symbol_tile_valid_if_curried_right.mjs";
+import { each } from "./each.mjs";
+import { app_replace_symbol_tile_valid_if_curried_right } from "./app_replace_symbol_tile_valid_if_curried_right.mjs";
 export function app_replace_symbol_tile_valid_if_multiple(
   lists,
   valid,
