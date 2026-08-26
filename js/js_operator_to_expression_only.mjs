93f292175255c4482b89242c72918a33bf13fcc6
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:14:40 +0800

    function_auto_checked js_operator_to_expression_only

--- js/js_operator_to_expression_only.mjs
+++ js/js_operator_to_expression_only.mjs
@@ -1,5 +1,5 @@
-import { property_get } from "../../love/js/property_get.mjs";
-import { js_operator_to_expression } from "../../love/js/js_operator_to_expression.mjs";
+import { property_get } from "./property_get.mjs";
+import { js_operator_to_expression } from "./js_operator_to_expression.mjs";
 export function js_operator_to_expression_only(o, next) {
   let r = js_operator_to_expression(o, next);
   let expression = property_get(r, "expression");
