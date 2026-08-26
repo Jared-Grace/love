5c4829aeddacd8a675206578b6ccb229089486e8
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:13:49 +0800

    function_auto_checked js_callee_name_includes_try_curried_right

--- js/js_callee_name_includes_try_curried_right.mjs
+++ js/js_callee_name_includes_try_curried_right.mjs
@@ -1,7 +1,8 @@
-import { js_callee_name_includes_try } from "../../love/js/js_callee_name_includes_try.mjs";
+import { js_callee_name_includes_try } from "./js_callee_name_includes_try.mjs";
 export function js_callee_name_includes_try_curried_right(part) {
   let c = function js_callee_name_includes_try_curried_right_result(n) {
-    return js_callee_name_includes_try(n, part);
+    let includes = js_callee_name_includes_try(n, part);
+    return includes;
   };
   return c;
 }
