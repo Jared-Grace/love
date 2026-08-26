df206b6c364cfd160396db9071a48cb9dedf6101
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:08:14 +0800

    function_auto_checked app_code_lesson_add

--- js/app_code_lesson_add.mjs
+++ js/app_code_lesson_add.mjs
@@ -1,5 +1,5 @@
-import { app_code_lessons_fns } from "../../love/js/app_code_lessons_fns.mjs";
-import { function_copy_list_add } from "../../love/js/function_copy_list_add.mjs";
+import { app_code_lessons_fns } from "./app_code_lessons_fns.mjs";
+import { function_copy_list_add } from "./function_copy_list_add.mjs";
 export async function app_code_lesson_add(fn_base_name) {
   let fns_list = app_code_lessons_fns;
   let name_new = await function_copy_list_add(fn_base_name, fns_list);
