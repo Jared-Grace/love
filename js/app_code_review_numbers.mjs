f5f783d395de1d717bf86837ab62f1dc48bd35ca
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 21:24:19 +0800

    function_fold app_code_review_due_is app_code_review_numbers

--- js/app_code_review_numbers.mjs
+++ js/app_code_review_numbers.mjs
@@ -1,8 +1,7 @@
+import { app_code_review_due_is } from "./app_code_review_due_is.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_code_lessons } from "./app_code_lessons.mjs";
 import { add_1 } from "./add_1.mjs";
-import { app_code_review_scope } from "./app_code_review_scope.mjs";
-import { null_not_is } from "./null_not_is.mjs";
 import { list_add } from "./list_add.mjs";
 import { each_index } from "./each_index.mjs";
 export function app_code_review_numbers() {
@@ -14,8 +13,7 @@ export function app_code_review_numbers() {
   let numbers = [];
   function lambda(lesson, index) {
     let number = add_1(index);
-    let scope = app_code_review_scope(number);
-    let has_review = null_not_is(scope);
+    let has_review = app_code_review_due_is(number);
     if (has_review) {
       list_add(numbers, number);
     }
