e9d281e0646b141661e6ab2ac7074c94cd325a97
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 18:22:46 +0800

    function_literal_route ebible_letter_accounted_path json_extension

--- js/ebible_letter_accounted_path.mjs
+++ js/ebible_letter_accounted_path.mjs
@@ -1,8 +1,9 @@
+import { json_extension } from "./json_extension.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 import { fn_name } from "./fn_name.mjs";
 export function ebible_letter_accounted_path() {
   "Where the record of what has already been said to eBible, and what was looked at and found to be no fault, is kept.";
   let f_name = fn_name("ebible_letter_accounted");
-  let path = text_combine_multiple(["data/given/", f_name, ".json"]);
+  let path = text_combine_multiple(["data/given/", f_name, json_extension()]);
   return path;
 }
