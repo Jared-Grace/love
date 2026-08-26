55afea53f83ab09c70a491665a054374354fff71
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:09:51 +0800

    function_auto_checked app_replace_symbol_tile_dead

--- js/app_replace_symbol_tile_dead.mjs
+++ js/app_replace_symbol_tile_dead.mjs
@@ -1,7 +1,7 @@
-import { html_style_background_color_set } from "../../love/js/html_style_background_color_set.mjs";
-import { html_box_shadow_set } from "../../love/js/html_box_shadow_set.mjs";
-import { html_font_color_set } from "../../love/js/html_font_color_set.mjs";
-import { app_replace_symbol_tile_dead_background_color } from "../../love/js/app_replace_symbol_tile_dead_background_color.mjs";
+import { html_style_background_color_set } from "./html_style_background_color_set.mjs";
+import { html_box_shadow_set } from "./html_box_shadow_set.mjs";
+import { html_font_color_set } from "./html_font_color_set.mjs";
+import { app_replace_symbol_tile_dead_background_color } from "./app_replace_symbol_tile_dead_background_color.mjs";
 export function app_replace_symbol_tile_dead(sb) {
   let bg = app_replace_symbol_tile_dead_background_color();
   html_style_background_color_set(sb, bg);
