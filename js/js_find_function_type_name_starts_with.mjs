f5b0569a5cc7f759f52d2003ee98e448433373ed
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:14:09 +0800

    function_auto_checked js_find_function_type_name_starts_with

--- js/js_find_function_type_name_starts_with.mjs
+++ js/js_find_function_type_name_starts_with.mjs
@@ -1,6 +1,6 @@
-import { js_id_name_starts_with_try_curried_right } from "../../love/js/js_id_name_starts_with_try_curried_right.mjs";
-import { list_find } from "../../love/js/list_find.mjs";
-import { js_list_function_nodes } from "../../love/js/js_list_function_nodes.mjs";
+import { js_id_name_starts_with_try_curried_right } from "./js_id_name_starts_with_try_curried_right.mjs";
+import { list_find } from "./list_find.mjs";
+import { js_list_function_nodes } from "./js_list_function_nodes.mjs";
 export function js_find_function_type_name_starts_with(ast, prefix) {
   let mapped = js_list_function_nodes(ast);
   let lambda = js_id_name_starts_with_try_curried_right(prefix);
