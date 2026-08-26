2667d1a2a0fea09c387742705fef315d7d6ee1f5
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:10:07 +0800

    function_auto_checked app_replace_symbol_tile_valid_if_multiple_nested

--- js/app_replace_symbol_tile_valid_if_multiple_nested.mjs
+++ js/app_replace_symbol_tile_valid_if_multiple_nested.mjs
@@ -1,5 +1,5 @@
-import { each_nested } from "../../love/js/each_nested.mjs";
-import { app_replace_symbol_tile_valid_if_curried_right } from "../../love/js/app_replace_symbol_tile_valid_if_curried_right.mjs";
+import { each_nested } from "./each_nested.mjs";
+import { app_replace_symbol_tile_valid_if_curried_right } from "./app_replace_symbol_tile_valid_if_curried_right.mjs";
 export function app_replace_symbol_tile_valid_if_multiple_nested(
   lists,
   valid,
