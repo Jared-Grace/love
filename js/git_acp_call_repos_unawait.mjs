680b5be45bb446ac8f79f479605354ff3b151a36
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:13:06 +0800

    function_auto_checked git_acp_call_repos_unawait

--- js/git_acp_call_repos_unawait.mjs
+++ js/git_acp_call_repos_unawait.mjs
@@ -1,6 +1,6 @@
-import { lock_wait_prompt } from "../../love/js/lock_wait_prompt.mjs";
-import { promise_later } from "../../love/js/promise_later.mjs";
-import { git_acp_call_repos } from "../../love/js/git_acp_call_repos.mjs";
+import { lock_wait_prompt } from "./lock_wait_prompt.mjs";
+import { promise_later } from "./promise_later.mjs";
+import { git_acp_call_repos } from "./git_acp_call_repos.mjs";
 export function git_acp_call_repos_unawait(f_name, args) {
   async function wrapped() {
     await git_acp_call_repos(f_name, args);
