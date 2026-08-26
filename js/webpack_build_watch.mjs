b469fa81f21f52d853263d10ca451700182daa76
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 20:02:05 +0800

    function_fold folder_public_dev webpack_build_watch

--- js/webpack_build_watch.mjs
+++ js/webpack_build_watch.mjs
@@ -1,10 +1,8 @@
-import { app_shared_name_dev_text } from "./app_shared_name_dev_text.mjs";
+import { folder_public_dev } from "./folder_public_dev.mjs";
 import { webpack_build_generic } from "./webpack_build_generic.mjs";
-import { folder_public_join } from "./folder_public_join.mjs";
 export async function webpack_build_watch(search) {
   let words_after = ["--watch"];
-  let folder = app_shared_name_dev_text();
-  let config_folder = folder_public_join(folder);
+  let config_folder = folder_public_dev();
   let result = await webpack_build_generic(search, config_folder, words_after);
   return result;
 }
