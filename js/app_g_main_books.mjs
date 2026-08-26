25d18ea0eb4611ee2a96356f41e9817cc12b550e
JESUS rose to life <username@users.noreply.github.com>
2026-08-26 15:09:04 +0800

    function_auto_checked app_g_main_books

--- js/app_g_main_books.mjs
+++ js/app_g_main_books.mjs
@@ -1,5 +1,5 @@
-import { app_g } from "../../love/js/app_g.mjs";
-import { global_function_property_get } from "../../love/js/global_function_property_get.mjs";
+import { app_g } from "./app_g.mjs";
+import { global_function_property_get } from "./global_function_property_get.mjs";
 export function app_g_main_books() {
   let value = global_function_property_get(app_g, "books");
   return value;
