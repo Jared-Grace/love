432f7fd5635c26767c08d584a7f7ce715b97ec63
JESUS rose to life <username@users.noreply.github.com>
2026-08-28 22:18:17 +0800

    ai

--- js/bible_usfm_version_chapter_clipboard_copy_numbered.mjs
+++ js/bible_usfm_version_chapter_clipboard_copy_numbered.mjs
@@ -1,3 +1,4 @@
+import { bible_usfm_version_text_clipboard_copy_withheld } from "./bible_usfm_version_text_clipboard_copy_withheld.mjs";
 import { bible_usfm_version_withheld_why_or_null } from "./bible_usfm_version_withheld_why_or_null.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { bible_usfm_version_chapter_paragraphed_text } from "./bible_usfm_version_chapter_paragraphed_text.mjs";
@@ -23,9 +24,9 @@ export async function bible_usfm_version_chapter_clipboard_copy_numbered(
   );
   await clipboard_copy(text);
   let withheld = bible_usfm_version_withheld_why_or_null(version);
-  let copied = {
-    text,
-    withheld,
-  };
+  let copied = await bible_usfm_version_text_clipboard_copy_withheld(
+    text2,
+    version2,
+  );
   return copied;
 }
