c3e99dce6caab1f33a103a3d3a9e5bedcb0f0bef
JESUS rose to life <username@users.noreply.github.com>
2026-08-28 21:19:18 +0800

    ai

--- js/app_code_review_present_fn.mjs
+++ js/app_code_review_present_fn.mjs
@@ -2,7 +2,7 @@ import { app_code_review_present_fn_present } from "./app_code_review_present_fn
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 export function app_code_review_present_fn(
-  r4,
+  r,
   home_button,
   success_container,
   back_button,
@@ -11,14 +11,14 @@ export function app_code_review_present_fn(
   context,
 ) {
   arguments_assert(arguments, 7);
-  let c = property_get(r4, "c");
-  let key = property_get(r4, "key");
-  let queue = property_get(r4, "queue");
-  let skip_button = property_get(r4, "skip_button");
-  let go_next = property_get(r4, "go_next");
-  let progress = property_get(r4, "progress");
+  let c = property_get(r, "c");
+  let key = property_get(r, "key");
+  let queue = property_get(r, "queue");
+  let skip_button = property_get(r, "skip_button");
+  let go_next = property_get(r, "go_next");
+  let progress = property_get(r, "progress");
   let present = app_code_review_present_fn_present(
-    r4,
+    r,
     home_button,
     progress,
     c,
