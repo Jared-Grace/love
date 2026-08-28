2b83a605e78f115b61ca31b120f686b9eeaaaf91
JESUS rose to life <username@users.noreply.github.com>
2026-08-28 21:28:36 +0800

    function_span_cut_or_undo js_ast_return_key_shapes_agree_function_visit node several js_ast_return_key_shapes_agree_function_visit_several

--- js/js_ast_return_key_shapes_agree_function_visit.mjs
+++ js/js_ast_return_key_shapes_agree_function_visit.mjs
@@ -1,23 +1,14 @@
-import { js_ast_return_key_shapes_agree_function_visit_record } from "./js_ast_return_key_shapes_agree_function_visit_record.mjs";
-import { js_ast_return_key_shapes_agree_function_visit_one } from "./js_ast_return_key_shapes_agree_function_visit_one.mjs";
-import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
-import { js_function_returns_own } from "./js_function_returns_own.mjs";
+import { js_ast_return_key_shapes_agree_function_visit_several } from "./js_ast_return_key_shapes_agree_function_visit_several.mjs";
+import { js_ast_return_key_shapes_agree_function_visit_record } from "./js_ast_return_key_shapes_agree_function_visit_record.mjs";
+import { arguments_assert } from "./arguments_assert.mjs";
 import { not } from "./not.mjs";
-import { list_size } from "./list_size.mjs";
-import { greater_than } from "./greater_than.mjs";
 export function js_ast_return_key_shapes_agree_function_visit(visited) {
   arguments_assert(arguments, 1);
-  let node = property_get(visited, "node");
-  let returns = js_function_returns_own(node);
-  let records = [];
-  let wanted = js_ast_return_key_shapes_agree_function_visit_one(
-    returns,
-    node,
-    records,
-  );
-  let count = list_size(records);
-  let several = greater_than(count, 1);
+  let r = js_ast_return_key_shapes_agree_function_visit_several(visited);
+  let several = property_get(r, "several");
+  let wanted = property_get(r, "wanted");
+  let records = property_get(r, "records");
   if (not(several)) {
     return;
   }
