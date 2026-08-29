e9f41be21cc970e1d4fd03300c9adeb073276c0a
JESUS rose to life <username@users.noreply.github.com>
2026-08-29 11:23:38 +0800

    ai

--- js/bible_glyph_chapter_references.mjs
+++ js/bible_glyph_chapter_references.mjs
@@ -53,6 +53,10 @@ export function bible_glyph_chapter_references() {
       reference: "Ezekiel 33",
     },
     {
+      chapter_code: "MRK05",
+      reference: "Mark 5",
+    },
+    {
       chapter_code: "JHN01",
       reference: "John 1",
     },
