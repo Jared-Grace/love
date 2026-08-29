c01f0b3e647c629fb16a11991e383619f1802ce8
JESUS rose to life <username@users.noreply.github.com>
2026-08-29 17:04:52 +0800

    function_fold app_code_lesson_name_id_operators app_code_lesson_expression_remainder_divide_solve_title_name_id

--- js/app_code_lesson_expression_remainder_divide_solve_title_name_id.mjs
+++ js/app_code_lesson_expression_remainder_divide_solve_title_name_id.mjs
@@ -1,14 +1,12 @@
-import { app_code_lesson_name_id_category_then } from "./app_code_lesson_name_id_category_then.mjs";
+import { app_code_lesson_name_id_operators } from "./app_code_lesson_name_id_operators.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { html_span_text } from "./html_span_text.mjs";
-import { app_code_category_operators } from "./app_code_category_operators.mjs";
 export function app_code_lesson_expression_remainder_divide_solve_title_name_id() {
   arguments_assert(arguments, 0);
   ("the home title is console.log solve the remainder formula");
   function paint(parent) {
     html_span_text(parent, "Solve the remainder formula");
   }
-  let left = app_code_category_operators();
-  let built = app_code_lesson_name_id_category_then(left, paint);
+  let built = app_code_lesson_name_id_operators(paint);
   return built;
 }
