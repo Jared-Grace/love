5075aa21f84758bf49f090dcf605e1bee30c450f
JESUS rose to life <username@users.noreply.github.com>
2026-08-29 11:48:14 +0800

    ai

--- js/app_code_lesson_console_log_nested_generic_title_name_id.mjs
+++ js/app_code_lesson_console_log_nested_generic_title_name_id.mjs
@@ -5,9 +5,8 @@ import { html_span_text_code_dark } from "./html_span_text_code_dark.mjs";
 import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 export function app_code_lesson_console_log_nested_generic_title_name_id(
   symbol,
-  word,
 ) {
-  arguments_assert(arguments, 2);
+  arguments_assert(arguments, 1);
   ("the home title is console.log nested <operator>");
   function paint(parent) {
     html_span_text(parent, "Nested ");
