3025b49c349843dd5c1f505da23555161b859107
JESUS rose to life <username@users.noreply.github.com>
2026-08-29 11:35:00 +0800

    function_parameter_unread_remove app_code_lesson_expression_true_false_compare_generic_title_name_id rights

--- js/app_code_lesson_expression_true_false_compare_generic_title_name_id.mjs
+++ js/app_code_lesson_expression_true_false_compare_generic_title_name_id.mjs
@@ -6,9 +6,8 @@ import { html_cycle_code } from "./html_cycle_code.mjs";
 import { app_code_category_expressions } from "./app_code_category_expressions.mjs";
 export function app_code_lesson_expression_true_false_compare_generic_title_name_id(
   symbol,
-  rights,
 ) {
-  arguments_assert(arguments, 2);
+  arguments_assert(arguments, 1);
   ("the home title: comparing true and false with this operator, an Expressions lesson");
   function paint(parent) {
     let t = js_keyword_true();
