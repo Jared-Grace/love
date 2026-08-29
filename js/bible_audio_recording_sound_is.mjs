228176a03f12e0716688d1e474f5935fa1cbaab2
JESUS rose to life <username@users.noreply.github.com>
2026-08-29 11:30:22 +0800

    function_fold list_any bible_audio_recording_sound_is

--- js/bible_audio_recording_sound_is.mjs
+++ js/bible_audio_recording_sound_is.mjs
@@ -1,11 +1,10 @@
+import { list_any } from "./list_any.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 import { bible_folder_key } from "./bible_folder_key.mjs";
 import { bible_audio_folder } from "./bible_audio_folder.mjs";
 import { folder_read_files } from "./folder_read_files.mjs";
 import { text_ends_with } from "./text_ends_with.mjs";
-import { list_filter } from "./list_filter.mjs";
-import { list_empty_not_is } from "./list_empty_not_is.mjs";
 export async function bible_audio_recording_sound_is(recording) {
   "$plain recording";
   "Whether a folder of recordings has any sound in it at all.";
@@ -20,7 +19,6 @@ export async function bible_audio_recording_sound_is(recording) {
     let is = text_ends_with(name, ".mp3");
     return is;
   }
-  let sounds = list_filter(files, sound_is);
-  let heard = list_empty_not_is(sounds);
+  let heard = list_any(files, sound_is);
   return heard;
 }
