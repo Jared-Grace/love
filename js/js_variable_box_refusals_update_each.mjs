56ae503bc34074965cbd10d3174f99471badb57a
JESUS rose to life <username@users.noreply.github.com>
2026-09-01 13:25:43 +0800

    function_span_cut_or_undo js_variable_box_refusals patterns update_each js_variable_box_refusals_update_each

---
+++ js/js_variable_box_refusals_update_each.mjs
@@ -0,0 +1,46 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { js_list_type_nodes } from "./js_list_type_nodes.mjs";
+import { property_get } from "./property_get.mjs";
+import { list_add } from "./list_add.mjs";
+import { each } from "./each.mjs";
+export function js_variable_box_refusals_update_each(ast, named_is, refusals) {
+  arguments_assert(arguments, 3);
+  let patterns = js_list_type_nodes(ast, "ObjectPattern");
+  function pattern_each(pattern) {
+    let properties = property_get(pattern, "properties");
+    function property_each(entry) {
+      let value = property_get(entry, "value");
+      let is = named_is(value);
+      if (is) {
+        list_add(refusals, "it is written by taking a record apart");
+      }
+    }
+    each(properties, property_each);
+  }
+  each(patterns, pattern_each);
+  let arrays = js_list_type_nodes(ast, "ArrayPattern");
+  function array_each(pattern2) {
+    let elements = property_get(pattern2, "elements");
+    function element_each(element) {
+      let is = named_is(element);
+      if (is) {
+        list_add(refusals, "it is written by taking a list apart");
+      }
+    }
+    each(elements, element_each);
+  }
+  each(arrays, array_each);
+  let updates = js_list_type_nodes(ast, "UpdateExpression");
+  function update_each(update) {
+    let argument = property_get(update, "argument");
+    let is = named_is(argument);
+    if (is) {
+      list_add(refusals, "it is counted up or down where it stands");
+    }
+  }
+  let r = {
+    updates,
+    update_each,
+  };
+  return r;
+}
