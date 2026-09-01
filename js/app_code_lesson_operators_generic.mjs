5a7b2c35f688df39c9ba830dbfe65694fe848b09
JESUS rose to life <username@users.noreply.github.com>
2026-09-01 11:24:41 +0800

    function_parameters_record app_code_lesson_operators_generic

--- js/app_code_lesson_operators_generic.mjs
+++ js/app_code_lesson_operators_generic.mjs
@@ -73,11 +73,8 @@ export function app_code_lesson_operators_generic({
     ]);
     html_div_cycle_code(c, [combined4, operator_js, combined5]);
   }
-  let name_id = app_code_lesson_name_id_symbol(
-    app_code_category_operators(),
-    [math_name],
-    operator_js,
-  );
+  let left = app_code_category_operators();
+  let name_id = app_code_lesson_name_id_symbol(left, [math_name], operator_js);
   let lesson = app_code_lesson_code_expression(batch, name_id, above);
   return lesson;
 }
