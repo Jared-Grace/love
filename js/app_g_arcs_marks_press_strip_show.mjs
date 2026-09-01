4099b110f00f236e7b29de70f371afbc50489261
JESUS rose to life <username@users.noreply.github.com>
2026-09-01 17:19:42 +0800

    functions_html_style_literals_migrate

--- js/app_g_arcs_marks_press_strip_show.mjs
+++ js/app_g_arcs_marks_press_strip_show.mjs
@@ -1,6 +1,6 @@
+import { html_display_set } from "./html_display_set.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_set } from "./property_set.mjs";
-import { html_style_set } from "./html_style_set.mjs";
 import { html_text_set } from "./html_text_set.mjs";
 export function app_g_arcs_marks_press_strip_show(open, at, strip, lister) {
   arguments_assert(arguments, 4);
@@ -11,6 +11,6 @@ export function app_g_arcs_marks_press_strip_show(open, at, strip, lister) {
     how = "flex";
     word = "hide";
   }
-  html_style_set(strip, "display", how);
+  html_display_set(strip, how);
   html_text_set(lister, word);
 }
