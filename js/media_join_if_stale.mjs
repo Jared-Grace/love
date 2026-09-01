7900cae3cff41a10eed8b40a5f70b53f31b92606
JESUS rose to life <username@users.noreply.github.com>
2026-09-01 12:20:19 +0800

    function_rename media_join_if_exists_not media_join_if_stale

--- js/media_join_if_stale.mjs
+++ js/media_join_if_stale.mjs
@@ -1,6 +1,6 @@
 import { media_join } from "./media_join.mjs";
 import { file_exists_not } from "./file_exists_not.mjs";
-export async function media_join_if_exists_not(path_output, paths_media) {
+export async function media_join_if_stale(path_output, paths_media) {
   let n = await file_exists_not(path_output);
   if (n) {
     await media_join(paths_media, path_output);
