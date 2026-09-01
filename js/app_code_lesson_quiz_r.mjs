628af3cd3a071c200f94bc277d13f59ecf9cfae9
JESUS rose to life <username@users.noreply.github.com>
2026-09-01 11:03:39 +0800

    function_parameters_record app_code_lesson_quiz_r

--- js/app_code_lesson_quiz_r.mjs
+++ js/app_code_lesson_quiz_r.mjs
@@ -1,7 +1,7 @@
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_code_lesson_quiz_container_success_message } from "./app_code_lesson_quiz_container_success_message.mjs";
 import { property_get } from "./property_get.mjs";
-export function app_code_lesson_quiz_r(
+export function app_code_lesson_quiz_r({
   info,
   qa,
   container_blue_light,
@@ -10,8 +10,8 @@ export function app_code_lesson_quiz_r(
   context,
   quizzes,
   refresh,
-) {
-  arguments_assert(arguments, 8);
+}) {
+  arguments_assert(arguments, 1);
   let r4 = app_code_lesson_quiz_container_success_message(
     info,
     qa,
