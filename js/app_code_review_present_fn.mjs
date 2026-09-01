88e9b41f54592f92df6a03d68de216fb9c2b4b6e
JESUS rose to life <username@users.noreply.github.com>
2026-09-01 11:23:16 +0800

    function_parameters_record app_code_review_present_fn

--- js/app_code_review_present_fn.mjs
+++ js/app_code_review_present_fn.mjs
@@ -1,7 +1,7 @@
 import { app_code_review_present_fn_present } from "./app_code_review_present_fn_present.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
-export function app_code_review_present_fn(
+export function app_code_review_present_fn({
   r,
   home_button,
   success_container,
@@ -9,8 +9,8 @@ export function app_code_review_present_fn(
   restart_button,
   has_next,
   context,
-) {
-  arguments_assert(arguments, 7);
+}) {
+  arguments_assert(arguments, 1);
   let c = property_get(r, "c");
   let key = property_get(r, "key");
   let queue = property_get(r, "queue");
