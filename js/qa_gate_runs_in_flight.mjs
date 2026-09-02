714b1e15b0a084a87c26b0a78c4dac1695938f60
JESUS rose to life <username@users.noreply.github.com>
2026-09-02 21:37:19 +0800

    function_span_cut_or_undo qa_gate_runs_in_flight row row qa_gate_runs_in_flight_row

--- js/qa_gate_runs_in_flight.mjs
+++ js/qa_gate_runs_in_flight.mjs
@@ -1,11 +1,7 @@
+import { qa_gate_runs_in_flight_row } from "./qa_gate_runs_in_flight_row.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { processes_dispatcher_report } from "./processes_dispatcher_report.mjs";
-import { property_text_split_space } from "./property_text_split_space.mjs";
 import { fn_name } from "./fn_name.mjs";
-import { list_includes } from "./list_includes.mjs";
-import { list_add } from "./list_add.mjs";
-import { property_get } from "./property_get.mjs";
-import { process_parent_id_or_null } from "./process_parent_id_or_null.mjs";
 import { null_is } from "./null_is.mjs";
 import { or } from "./or.mjs";
 import { cpu_count } from "./cpu_count.mjs";
@@ -34,38 +30,7 @@ export async function qa_gate_runs_in_flight() {
   let running = await processes_dispatcher_report();
   let shards = [];
   let runs = [];
-  for (let row of running) {
-    let words = property_text_split_space(row, "line");
-    let shard_name = fn_name("qa_gate_tree_shard_run");
-    let shard_is = list_includes(words, shard_name);
-    if (shard_is) {
-      list_add(shards, row);
-      let pid = property_get(row, "pid");
-      let parent = process_parent_id_or_null(pid);
-      if (null_is(parent)) {
-        continue;
-      }
-      let parent_known = list_includes(runs, parent);
-      if (parent_known) {
-        continue;
-      }
-      list_add(runs, parent);
-      continue;
-    }
-    let gates_name = fn_name("qa_gate_run");
-    let gates_is = list_includes(words, gates_name);
-    let judging_name = fn_name("qa_commit_named_at");
-    let judging_is = list_includes(words, judging_name);
-    let counts = or(gates_is, judging_is);
-    if (counts) {
-      let pid = property_get(row, "pid");
-      let known = list_includes(runs, pid);
-      if (known) {
-        continue;
-      }
-      list_add(runs, pid);
-    }
-  }
+  qa_gate_runs_in_flight_row(running, shards, runs);
   let cores = await cpu_count();
   let load = machine_load_average();
   let swap = await machine_swap_free_bytes_or_null();
