4d231e0cf3f39c08f5ea0b5e7ee0fca17e61d37a
JESUS rose to life <username@users.noreply.github.com>
2026-09-02 21:41:57 +0800

    function_rename app_g_bless_household_first_finished_is app_g_bless_family_first_finished_is

--- js/app_g_bless_blessed_head_start.mjs
+++ js/app_g_bless_blessed_head_start.mjs
@@ -4,7 +4,7 @@ import { app_g_bless_blessed_head_start_building } from "./app_g_bless_blessed_h
 import { arguments_assert } from "./arguments_assert.mjs";
 import { bless_rungs } from "./bless_rungs.mjs";
 import { list_first } from "./list_first.mjs";
-import { app_g_bless_household_first_finished_is } from "./app_g_bless_household_first_finished_is.mjs";
+import { app_g_bless_family_first_finished_is } from "./app_g_bless_family_first_finished_is.mjs";
 import { app_g_bless_household_first_is } from "./app_g_bless_household_first_is.mjs";
 import { or } from "./or.mjs";
 import { not } from "./not.mjs";
@@ -60,7 +60,7 @@ export function app_g_bless_blessed_head_start(blessed) {
     );
     return climbed_building;
   }
-  let done = app_g_bless_household_first_finished_is();
+  let done = app_g_bless_family_first_finished_is();
   let two_thirds = app_g_bless_household_first_is();
   let asked = or(done, two_thirds);
   if (not(asked)) {
