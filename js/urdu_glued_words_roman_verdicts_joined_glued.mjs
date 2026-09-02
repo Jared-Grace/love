eb609602dae2025959c2b254e3e4f66a1c3c42fc
JESUS rose to life <username@users.noreply.github.com>
2026-09-02 21:39:56 +0800

    function_span_cut_or_undo urdu_glued_words_roman_verdicts_joined_glued script_verdicts joined_apart urdu_glued_words_roman_verdicts_joined_glued_joined_apart

--- js/urdu_glued_words_roman_verdicts_joined_glued.mjs
+++ js/urdu_glued_words_roman_verdicts_joined_glued.mjs
@@ -1,9 +1,8 @@
+import { urdu_glued_words_roman_verdicts_joined_glued_joined_apart } from "./urdu_glued_words_roman_verdicts_joined_glued_joined_apart.mjs";
 import { property_get } from "./property_get.mjs";
 import { urdu_glued_words_roman_verdicts_joined_glued_spellings_of } from "./urdu_glued_words_roman_verdicts_joined_glued_spellings_of.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { ebible_folder_urdu_control_roman } from "./ebible_folder_urdu_control_roman.mjs";
-import { equal } from "./equal.mjs";
-import { list_add } from "./list_add.mjs";
 import { list_join } from "./list_join.mjs";
 export async function urdu_glued_words_roman_verdicts_joined_glued() {
   arguments_assert(arguments, 0);
@@ -13,24 +12,13 @@ export async function urdu_glued_words_roman_verdicts_joined_glued() {
   let spellings_of = property_get(r2, "spellings_of");
   let occurrences = property_get(r2, "occurrences");
   let split = property_get(r2, "split");
-  let script_verdicts = property_get(r2, "script_verdicts");
-  function choices_of(pieces) {
-    let choices = [];
-    for (let piece of pieces) {
-      let few = spellings_of(piece);
-      let unheard = equal(few, null);
-      if (unheard) {
-        let none = null;
-        return none;
-      }
-      list_add(choices, few);
-    }
-    return choices;
-  }
-  function joined_apart(chosen) {
-    let phrase = list_join(chosen, " ");
-    return phrase;
-  }
+  let r3 = urdu_glued_words_roman_verdicts_joined_glued_joined_apart(
+    r2,
+    spellings_of,
+  );
+  let joined_apart = property_get(r3, "joined_apart");
+  let choices_of = property_get(r3, "choices_of");
+  let script_verdicts = property_get(r3, "script_verdicts");
   function joined_glued(chosen) {
     let phrase = list_join(chosen, "");
     return phrase;
