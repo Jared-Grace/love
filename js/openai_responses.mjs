d30e4e6b7022808bd990b091adcd4fe0973c6845
JESUS rose to life <username@users.noreply.github.com>
2026-09-04 20:53:20 +0800

    function_rename openai_responses_file openai_off_responses_file

--- js/openai_responses.mjs
+++ js/openai_responses.mjs
@@ -1,7 +1,7 @@
-import { openai_responses_file } from "./openai_responses_file.mjs";
+import { openai_off_responses_file } from "./openai_off_responses_file.mjs";
 import { openai_generic } from "./openai_generic.mjs";
 export async function openai_responses(system, user) {
-  let fn = openai_responses_file;
+  let fn = openai_off_responses_file;
   let data = await openai_generic(fn, system, user);
   return data;
 }
