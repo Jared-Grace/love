f3ae98cb7415f14d357e6764c1f397006707c2b9
JESUS rose to life <username@users.noreply.github.com>
2026-09-04 21:04:01 +0800

    function_fold text_word_plain app_reply_buttons_refresh

--- js/app_reply_buttons_refresh.mjs
+++ js/app_reply_buttons_refresh.mjs
@@ -1,3 +1,4 @@
+import { text_word_plain } from "./text_word_plain.mjs";
 import { counter } from "./counter.mjs";
 import { html_display_none_or_inline } from "./html_display_none_or_inline.mjs";
 import { html_text_get } from "./html_text_get.mjs";
@@ -5,15 +6,12 @@ import { each } from "./each.mjs";
 import { not } from "./not.mjs";
 import { list_includes } from "./list_includes.mjs";
 import { text_starts_with } from "./text_starts_with.mjs";
-import { text_lower_to } from "./text_lower_to.mjs";
-import { text_letters_only } from "./text_letters_only.mjs";
 export function app_reply_buttons_refresh(typed_get, chosens, buttons) {
   let v = function lambda10() {
     function lambda(c) {
       function lambda2(item) {
         let text = html_text_get(item);
-        let letters = text_letters_only(text);
-        let lower = text_lower_to(letters);
+        let lower = text_word_plain(text);
         let prefix = typed_get();
         let sw = text_starts_with(lower, prefix);
         let includes = list_includes(chosens, item);
