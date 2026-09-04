4a32fd3650193bf43e4860ae3fc223bfa9201743
JESUS rose to life <username@users.noreply.github.com>
2026-09-04 20:52:54 +0800

    function_rename openai_chat_completions_file openai_off_chat_completions_file

--- js/openai_chat_completions.mjs
+++ js/openai_chat_completions.mjs
@@ -1,7 +1,7 @@
 import { openai_generic } from "./openai_generic.mjs";
-import { openai_chat_completions_file } from "./openai_chat_completions_file.mjs";
+import { openai_off_chat_completions_file } from "./openai_off_chat_completions_file.mjs";
 export async function openai_chat_completions(system, user) {
-  let fn = openai_chat_completions_file;
+  let fn = openai_off_chat_completions_file;
   let data = await openai_generic(fn, system, user);
   return data;
 }
