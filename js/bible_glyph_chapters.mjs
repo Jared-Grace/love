4e5c763abce98bb1cdf55f4e9e912ec086515a4f
JESUS rose to life <username@users.noreply.github.com>
2026-09-04 21:38:57 +0800

    ai

--- js/bible_glyph_chapters.mjs
+++ js/bible_glyph_chapters.mjs
@@ -204,6 +204,7 @@ export function bible_glyph_chapters() {
   let fifty_fifth = bible_glyph_chapter_psa096();
   let fifty_sixth = bible_glyph_chapter_psa099();
   let fifty_seventh = bible_glyph_chapter_psa101();
+  let fifty_eighth = bible_glyph_chapter_psa015();
   let chapters = [
     first,
     second,
