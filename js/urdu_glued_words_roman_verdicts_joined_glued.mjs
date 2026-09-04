1c5e53be0a90fbd66977cd9513de491678495d85
JESUS rose to life <username@users.noreply.github.com>
2026-09-04 10:07:28 +0800

    function_rename urdu_glued_words_roman_verdicts_joined_glued_joined_glued urdu_glued_words_roman_verdicts_joined_glued_phrase

--- js/urdu_glued_words_roman_verdicts_joined_glued.mjs
+++ js/urdu_glued_words_roman_verdicts_joined_glued.mjs
@@ -1,12 +1,11 @@
 import { property_get } from "./property_get.mjs";
-import { urdu_glued_words_roman_verdicts_joined_glued_joined_glued } from "./urdu_glued_words_roman_verdicts_joined_glued_joined_glued.mjs";
+import { urdu_glued_words_roman_verdicts_joined_glued_phrase } from "./urdu_glued_words_roman_verdicts_joined_glued_phrase.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { ebible_folder_urdu_control_roman } from "./ebible_folder_urdu_control_roman.mjs";
 export async function urdu_glued_words_roman_verdicts_joined_glued() {
   arguments_assert(arguments, 0);
   let control = ebible_folder_urdu_control_roman();
-  let r2 =
-    await urdu_glued_words_roman_verdicts_joined_glued_joined_glued(control);
+  let r2 = await urdu_glued_words_roman_verdicts_joined_glued_phrase(control);
   let joined_glued = property_get(r2, "joined_glued");
   let script_verdicts = property_get(r2, "script_verdicts");
   let choices_of = property_get(r2, "choices_of");
