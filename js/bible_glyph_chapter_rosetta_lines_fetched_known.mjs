84b27c0d2470d2b8ce3eb60ee84f5a7f146f7c53
JESUS rose to life <username@users.noreply.github.com>
2026-09-05 13:27:25 +0800

    bible_glyph_chapter_chains_write

--- js/bible_glyph_chapter_rosetta_lines_fetched_known.mjs
+++ js/bible_glyph_chapter_rosetta_lines_fetched_known.mjs
@@ -233,6 +233,12 @@ export async function bible_glyph_chapter_rosetta_lines_fetched_known(
     );
     found = lines_module.bible_glyph_chapter_rosetta_lines_jon01();
   }
+  if (equal(chapter_code, "JON02")) {
+    let lines_module = await import(
+      "./bible_glyph_chapter_rosetta_lines_jon02.mjs"
+    );
+    found = lines_module.bible_glyph_chapter_rosetta_lines_jon02();
+  }
   if (equal(chapter_code, "MAT05")) {
     let lines_module = await import(
       "./bible_glyph_chapter_rosetta_lines_mat05.mjs"
