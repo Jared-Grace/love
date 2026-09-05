ac54d374418d60193b208ceb2a5c8ad40bb45bee
JESUS rose to life <username@users.noreply.github.com>
2026-09-05 14:46:28 +0800

    bible_glyph_chapter_references_write

--- js/bible_glyph_chapter_references.mjs
+++ js/bible_glyph_chapter_references.mjs
@@ -45,6 +45,10 @@ export function bible_glyph_chapter_references() {
       reference: "Judges 13",
     },
     {
+      chapter_code: "RUT01",
+      reference: "Ruth 1",
+    },
+    {
       chapter_code: "1KI03",
       reference: "1 Kings 3",
     },
