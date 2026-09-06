567d9e765d18e5d0444fc0a3bf81007e68491678
JESUS rose to life <username@users.noreply.github.com>
2026-09-06 14:00:46 +0800

    ai

--- js/song_image_drawn_attempts_known.mjs
+++ js/song_image_drawn_attempts_known.mjs
@@ -525,7 +525,8 @@ export function song_image_drawn_attempts_known() {
   41,
   42,
   43,
-  44
+  44,
+  45
  ],
  "21": [
   1,
@@ -944,7 +945,8 @@ export function song_image_drawn_attempts_known() {
   27,
   28,
   29,
-  30
+  30,
+  31
  ],
  "26": [
   1,
