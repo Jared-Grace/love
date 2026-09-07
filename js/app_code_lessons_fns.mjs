8056777a2f61e15d2a1f7da5b75ead083a945557
JESUS rose to life <username@users.noreply.github.com>
2026-09-07 17:56:44 +0800

    function_rename app_code_lesson_expression_choose_order_brackets_moved app_code_lesson_expression_choose_order_parentheses_and_or

--- js/app_code_lessons_fns.mjs
+++ js/app_code_lessons_fns.mjs
@@ -109,7 +109,7 @@ import { app_code_lesson_expression_choose_order_and_before_or } from "./app_cod
 import { app_code_lesson_expression_and_before_or } from "./app_code_lesson_expression_and_before_or.mjs";
 import { app_code_lesson_expression_choose_order_brackets } from "./app_code_lesson_expression_choose_order_brackets.mjs";
 import { app_code_lesson_expression_brackets_or } from "./app_code_lesson_expression_brackets_or.mjs";
-import { app_code_lesson_expression_choose_order_brackets_moved } from "./app_code_lesson_expression_choose_order_brackets_moved.mjs";
+import { app_code_lesson_expression_choose_order_parentheses_and_or } from "./app_code_lesson_expression_choose_order_parentheses_and_or.mjs";
 import { app_code_lesson_expression_parentheses_and_or } from "./app_code_lesson_expression_parentheses_and_or.mjs";
 import { app_code_lesson_expression_choose_order_not_twice } from "./app_code_lesson_expression_choose_order_not_twice.mjs";
 import { app_code_lesson_expression_not_twice } from "./app_code_lesson_expression_not_twice.mjs";
@@ -246,7 +246,7 @@ export function app_code_lessons_fns() {
     app_code_lesson_expression_and_before_or,
     app_code_lesson_expression_choose_order_brackets,
     app_code_lesson_expression_brackets_or,
-    app_code_lesson_expression_choose_order_brackets_moved,
+    app_code_lesson_expression_choose_order_parentheses_and_or,
     app_code_lesson_expression_parentheses_and_or,
     app_code_lesson_expression_choose_order_not_twice,
     app_code_lesson_expression_not_twice,
