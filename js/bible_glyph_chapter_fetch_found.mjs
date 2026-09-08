dfb1639b41e7125903013022f41f641fa6c7ecf6
JESUS rose to life <username@users.noreply.github.com>
2026-09-08 11:23:02 +0800

    bible_glyph_chapter_chains_write

--- js/bible_glyph_chapter_fetch_found.mjs
+++ js/bible_glyph_chapter_fetch_found.mjs
@@ -109,6 +109,10 @@ export async function bible_glyph_chapter_fetch_found(chapter_code) {
     let chapter_module = await import("./bible_glyph_chapter_exo07.mjs");
     stored = chapter_module.bible_glyph_chapter_exo07();
   }
+  if (equal(chapter_code, "EXO08")) {
+    let chapter_module = await import("./bible_glyph_chapter_exo08.mjs");
+    stored = chapter_module.bible_glyph_chapter_exo08();
+  }
   if (equal(chapter_code, "EXO14")) {
     let chapter_module = await import("./bible_glyph_chapter_exo14.mjs");
     stored = chapter_module.bible_glyph_chapter_exo14();
