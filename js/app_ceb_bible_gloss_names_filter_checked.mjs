9db3b18b725d952d030adee3bebc8fa7fae3dc41
JESUS rose to life <username@users.noreply.github.com>
2026-09-08 14:54:25 +0800

    function_overwrite_from_temp app_ceb_bible_gloss_names_filter_checked

--- js/app_ceb_bible_gloss_names_filter_checked.mjs
+++ js/app_ceb_bible_gloss_names_filter_checked.mjs
@@ -3,7 +3,7 @@ import { app_ceb_bible_gloss_roots_disagreeing } from "./app_ceb_bible_gloss_roo
 import { property_get } from "./property_get.mjs";
 import { gloss_offenders_roots_silent_words } from "./gloss_offenders_roots_silent_words.mjs";
 import { ebible_folder_cebuano } from "./ebible_folder_cebuano.mjs";
-import { bible_words_names_apart } from "./bible_words_names_apart.mjs";
+import { bible_words_common } from "./bible_words_common.mjs";
 import { gloss_words_rows_names_apart } from "./gloss_words_rows_names_apart.mjs";
 import { gloss_names_filter_checked } from "./gloss_names_filter_checked.mjs";
 export async function app_ceb_bible_gloss_names_filter_checked() {
@@ -17,8 +17,7 @@ export async function app_ceb_bible_gloss_names_filter_checked() {
   let consulted = property_get(disagreeing, "consulted");
   let silent_words = gloss_offenders_roots_silent_words(offenders);
   let bible_folder = ebible_folder_cebuano();
-  let bible_apart = await bible_words_names_apart(bible_folder);
-  let common_words = property_get(bible_apart, "common");
+  let common_words = await bible_words_common(bible_folder);
   let apart = gloss_words_rows_names_apart(silent_words, common_words);
   let names_taken = property_get(apart, "names");
   let checked = gloss_names_filter_checked(offenders, names_taken);
