ea0d682e9a1e0240dcf747603170cd2efdfbeb8d
JESUS rose to life <username@users.noreply.github.com>
2026-09-08 10:35:03 +0800

    ai

--- js/g_icon.mjs
+++ js/g_icon.mjs
@@ -3,9 +3,8 @@ import { g_img_square_style_position } from "./g_img_square_style_position.mjs";
 import { html_click_none } from "./html_click_none.mjs";
 import { html_div } from "./html_div.mjs";
 export function g_icon(div_map, coordinates, text) {
-  let div = html_div(div_map);
+  let div = g_img_square_div(div_map, coordinates, "tutorial");
   html_click_none(div);
-  g_img_square_style_position(div, coordinates, "tutorial");
   html_text_set(div, text);
   return div;
 }
