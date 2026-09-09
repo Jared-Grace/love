5c36d7f7d9bfc8cbabf3b3e507e5b59cca7e7a26
JESUS rose to life <username@users.noreply.github.com>
2026-09-09 12:20:50 +0800

    bible_glyph_chapter_word_draw 1JN03 commanded scroll

--- js/bible_glyph_chapter_1jn03.mjs
+++ js/bible_glyph_chapter_1jn03.mjs
@@ -648,7 +648,7 @@ export function bible_glyph_chapter_1jn03() {
           "just",
           "as",
           "He",
-          "commanded",
+          "$scroll",
           "us.",
         ],
       },
