b058c4abb7473b4042e78e240ffa57197a87ddfb
JESUS rose to life <username@users.noreply.github.com>
2026-09-09 14:46:18 +0800

    ai

--- js/bible_glyph_chapter_exo06.mjs
+++ js/bible_glyph_chapter_exo06.mjs
@@ -569,7 +569,7 @@ export function bible_glyph_chapter_exo06() {
           "sister",
           "of",
           "his",
-          "$father,",
+          "father,",
           "as",
           "his",
           "$woman,",
