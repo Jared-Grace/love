4143fc455520ea8cbf5a114bceaf1fb58ef72a66
JESUS rose to life <username@users.noreply.github.com>
2026-09-09 11:51:10 +0800

    qa_gate_add app_ceb_bible_gloss_passages_respell_refused_gate_run

--- js/qa_gates.mjs
+++ js/qa_gates.mjs
@@ -1,3 +1,4 @@
+import { app_ceb_bible_gloss_passages_respell_refused_gate_run } from "./app_ceb_bible_gloss_passages_respell_refused_gate_run.mjs";
 import { app_code_lesson_titles_capital_gate_run } from "./app_code_lesson_titles_capital_gate_run.mjs";
 import { lyric_video_picture_scene_alternative_gate_run } from "./lyric_video_picture_scene_alternative_gate_run.mjs";
 import { guard_gate_run } from "./guard_gate_run.mjs";
@@ -1073,6 +1074,7 @@ export function qa_gates() {
     ebible_chapter_code_padded_cases_gate_run,
     lyric_video_picture_scene_alternative_gate_run,
     app_code_lesson_titles_capital_gate_run,
+    app_ceb_bible_gloss_passages_respell_refused_gate_run,
   ];
   return gates;
 }
