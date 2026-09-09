5ecb6573bc4b0029f8eb70d6ab75a7256806ff2a
JESUS rose to life <username@users.noreply.github.com>
2026-09-09 20:23:02 +0800

    ai

--- js/bible_glyph_chapter_references.mjs
+++ js/bible_glyph_chapter_references.mjs
@@ -517,6 +517,10 @@ export function bible_glyph_chapter_references() {
       reference: "Deuteronomy 11",
     },
     {
+      chapter_code: "DEU15",
+      reference: "Deuteronomy 15",
+    },
+    {
       chapter_code: "DEU16",
       reference: "Deuteronomy 16",
     },
