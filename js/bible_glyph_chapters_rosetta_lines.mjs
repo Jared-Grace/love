2fc3df777204c7fe51b21bd4a41e938bb1c2b38f
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 15:14:43 +0800

    ai

--- js/bible_glyph_chapters_rosetta_lines.mjs
+++ js/bible_glyph_chapters_rosetta_lines.mjs
@@ -1,3 +1,4 @@
+import { bible_glyph_chapter_rosetta_lines_1ki17 } from "./bible_glyph_chapter_rosetta_lines_1ki17.mjs";
 import { bible_glyph_chapter_rosetta_lines_isa49 } from "./bible_glyph_chapter_rosetta_lines_isa49.mjs";
 import { bible_glyph_chapter_rosetta_lines_deu14 } from "./bible_glyph_chapter_rosetta_lines_deu14.mjs";
 import { bible_glyph_chapter_rosetta_lines_deu01 } from "./bible_glyph_chapter_rosetta_lines_deu01.mjs";
@@ -627,6 +628,7 @@ export function bible_glyph_chapters_rosetta_lines() {
   let chapter308 = bible_glyph_chapter_rosetta_lines_deu29();
   let chapter309 = bible_glyph_chapter_rosetta_lines_deu01();
   let chapter310 = bible_glyph_chapter_rosetta_lines_deu14();
+  let chapter311 = bible_glyph_chapter_rosetta_lines_isa49();
   let chapters = [
     chapter,
     chapter2,
@@ -938,7 +940,8 @@ export function bible_glyph_chapters_rosetta_lines() {
     chapter308,
     chapter309,
     chapter310,
-    bible_glyph_chapter_rosetta_lines_isa49(),
+    chapter311,
+    bible_glyph_chapter_rosetta_lines_1ki17(),
   ];
   return chapters;
 }
