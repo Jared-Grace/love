1b43e9fd6a19ee0eaa04696a1d4ba74282f05c22
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:49:16 +0800

    function_fold list_size_equal gloss_chapters_roots_claimed_gathered

--- js/gloss_chapters_roots_claimed_gathered.mjs
+++ js/gloss_chapters_roots_claimed_gathered.mjs
@@ -1,8 +1,8 @@
+import { list_size_equal } from "./list_size_equal.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_shared_gloss_bible_generate_generic_word } from "./app_shared_gloss_bible_generate_generic_word.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_size } from "./list_size.mjs";
-import { equal } from "./equal.mjs";
 import { add } from "./add.mjs";
 import { text_lower_to } from "./text_lower_to.mjs";
 import { property_get_or_null } from "./property_get_or_null.mjs";
@@ -33,8 +33,7 @@ export async function gloss_chapters_roots_claimed_gathered(fn) {
     let entry = property_get(found, "entry");
     let explain = property_get(found, "explain");
     let claimed = property_get(found, "claimed");
-    let claimed_count = list_size(claimed);
-    let empty = equal(claimed_count, 0);
+    let empty = list_size_equal(claimed, 0);
     if (empty) {
       return;
     }
