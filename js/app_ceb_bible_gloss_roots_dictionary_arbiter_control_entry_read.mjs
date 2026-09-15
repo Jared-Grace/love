f7cd8af7d122f15fb3288d13a2d3a5eb5bd0dc56
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:49:00 +0800

    function_fold list_size_equal app_ceb_bible_gloss_roots_dictionary_arbiter_control_entry_read

--- js/app_ceb_bible_gloss_roots_dictionary_arbiter_control_entry_read.mjs
+++ js/app_ceb_bible_gloss_roots_dictionary_arbiter_control_entry_read.mjs
@@ -1,7 +1,6 @@
+import { list_size_equal } from "./list_size_equal.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
-import { list_size } from "./list_size.mjs";
-import { equal } from "./equal.mjs";
 import { text_lower_to } from "./text_lower_to.mjs";
 import { list_get } from "./list_get.mjs";
 import { property_initialize_list } from "./property_initialize_list.mjs";
@@ -14,8 +13,7 @@ export function app_ceb_bible_gloss_roots_dictionary_arbiter_control_entry_read(
   function entry_read(found) {
     let entry = property_get(found, "entry");
     let claimed = property_get(found, "claimed");
-    let count = list_size(claimed);
-    let empty = equal(count, 0);
+    let empty = list_size_equal(claimed, 0);
     if (empty) {
       return;
     }
