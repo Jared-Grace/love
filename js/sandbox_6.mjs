6764d90719de3a715a920fd4732e0be23a85936c
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 12:20:03 +0800

    function_parallel_mark sandbox_6

--- js/sandbox_6.mjs
+++ js/sandbox_6.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function sandbox_6() {
+  function_duplicate_kind_parallel();
   let v = 1;
   return v;
 }
