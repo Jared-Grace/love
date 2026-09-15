4b04528c2fb18de4f64cdb85d2f5c951bc5617d5
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 18:03:16 +0800

    function_select_multiple_apply_args app_g_bless_overlay_loaded js_statement_find_call_named html_imgs_load_wait html_remove js_selects_functionize app_g_bless_cover_lift

---
+++ js/app_g_bless_cover_lift.mjs
@@ -0,0 +1,12 @@
+import { arguments_assert } from "./arguments_assert.mjs";
+import { html_imgs_load_wait } from "./html_imgs_load_wait.mjs";
+import { html_style_opacity } from "./html_style_opacity.mjs";
+import { sleep } from "./sleep.mjs";
+import { html_remove } from "./html_remove.mjs";
+export async function app_g_bless_cover_lift(container_map, cover) {
+  arguments_assert(arguments, 2);
+  await html_imgs_load_wait(container_map);
+  html_style_opacity(cover, "0");
+  await sleep(150);
+  html_remove(cover);
+}
