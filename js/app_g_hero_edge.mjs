6c304ce8cf473403bcf4f65053d6fbcb06835313
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 19:48:30 +0800

    function_rename app_g_bless_edge bless_edge

--- js/app_g_hero_edge.mjs
+++ js/app_g_hero_edge.mjs
@@ -2,7 +2,7 @@ import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_filter_null_not_is } from "./list_filter_null_not_is.mjs";
 import { bless_view_of_people } from "./bless_view_of_people.mjs";
-import { app_g_bless_edge } from "./app_g_bless_edge.mjs";
+import { bless_edge } from "./bless_edge.mjs";
 export function app_g_hero_edge(hero) {
   arguments_assert(arguments, 1);
   ("The red arrow at the edge of the screen, aimed at the evil person while there is one. It hides itself when they can be seen, and when nobody is evil at all.");
@@ -12,5 +12,5 @@ export function app_g_hero_edge(hero) {
   let bar = property_get(hero, "bar");
   let people = list_filter_null_not_is([evil]);
   let view = bless_view_of_people(people);
-  app_g_bless_edge(edge, container_map, bar, view);
+  bless_edge(edge, container_map, bar, view);
 }
