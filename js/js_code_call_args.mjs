350510ec5da32cff6ed394f2c0b43bad501b1d49
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 14:55:50 +0800

    function_rename app_code_operator_code_called js_code_called

--- js/js_code_call_args.mjs
+++ js/js_code_call_args.mjs
@@ -1,9 +1,9 @@
-import { app_code_operator_code_called } from "./app_code_operator_code_called.mjs";
+import { js_code_called } from "./js_code_called.mjs";
 import { js_code_join_comma_space } from "./js_code_join_comma_space.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 export function js_code_call_args(f_name, args) {
   arguments_assert(arguments, 2);
   let joined = js_code_join_comma_space(args);
-  let code = app_code_operator_code_called(f_name, joined);
+  let code = js_code_called(f_name, joined);
   return code;
 }
