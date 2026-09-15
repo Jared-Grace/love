3433fc8da3e7c091e2c222c174a2475c16b56276
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 12:19:47 +0800

    function_parallel_mark marker_first_index

--- js/marker_first_index.mjs
+++ js/marker_first_index.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function marker_first_index() {
+  function_duplicate_kind_parallel();
   let index = 1;
   return index;
 }
