f43bb62fb59a70c38a9d63fac1470f9a2dcf318b
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:48:40 +0800

    function_fold list_size_equal app_ceb_bible_gloss_root_named_outside_word_counted

--- js/app_ceb_bible_gloss_root_named_outside_word_counted.mjs
+++ js/app_ceb_bible_gloss_root_named_outside_word_counted.mjs
@@ -1,10 +1,10 @@
+import { list_size_equal } from "./list_size_equal.mjs";
 import { equal_not } from "./equal_not.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { app_ceb_bible_gloss_generate } from "./app_ceb_bible_gloss_generate.mjs";
 import { app_shared_gloss_bible_generate_generic_word } from "./app_shared_gloss_bible_generate_generic_word.mjs";
 import { property_get } from "./property_get.mjs";
 import { list_size } from "./list_size.mjs";
-import { equal } from "./equal.mjs";
 import { gloss_explain_roots_named } from "./gloss_explain_roots_named.mjs";
 import { add } from "./add.mjs";
 import { list_get } from "./list_get.mjs";
@@ -41,8 +41,7 @@ export async function app_ceb_bible_gloss_root_named_outside_word_counted(
       return;
     }
     let named = gloss_explain_roots_named(explain);
-    let count = list_size(named);
-    let empty = equal(count, 0);
+    let empty = list_size_equal(named, 0);
     if (empty) {
       return;
     }
