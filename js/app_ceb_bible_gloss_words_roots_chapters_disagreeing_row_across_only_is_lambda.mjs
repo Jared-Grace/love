8437136de15260b67652f9d5d23d2c415565b801
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:53:42 +0800

    function_rename app_ceb_bible_gloss_words_roots_chapters_disagreeing_row_across_only_is_row_across_only_is app_ceb_bible_gloss_words_roots_chapters_disagreeing_row_across_only_is_lambda

--- js/app_ceb_bible_gloss_words_roots_chapters_disagreeing_row_across_only_is_lambda.mjs
+++ js/app_ceb_bible_gloss_words_roots_chapters_disagreeing_row_across_only_is_lambda.mjs
@@ -1,6 +1,6 @@
 import { property_not } from "./property_not.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
-export function app_ceb_bible_gloss_words_roots_chapters_disagreeing_row_across_only_is_row_across_only_is() {
+export function app_ceb_bible_gloss_words_roots_chapters_disagreeing_row_across_only_is_lambda() {
   arguments_assert(arguments, 0);
   function row_across_only_is(row) {
     let outside_chapter = property_not(row, "within_chapter");
