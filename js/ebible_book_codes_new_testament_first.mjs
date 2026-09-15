90ab21d1a2da3800033b1fc9d14d86bd6e2dc2a0
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:50:14 +0800

    function_fold property_equals ebible_book_codes_new_testament_first

--- js/ebible_book_codes_new_testament_first.mjs
+++ js/ebible_book_codes_new_testament_first.mjs
@@ -1,7 +1,7 @@
+import { property_equals } from "./property_equals.mjs";
 import { ebible_book_divisions } from "./ebible_book_divisions.mjs";
 import { ebible_testament_new_name } from "./ebible_testament_new_name.mjs";
 import { property_get } from "./property_get.mjs";
-import { equal } from "./equal.mjs";
 import { list_filter } from "./list_filter.mjs";
 import { list_filter_not } from "./list_filter_not.mjs";
 import { list_concat } from "./list_concat.mjs";
@@ -14,8 +14,7 @@ export function ebible_book_codes_new_testament_first() {
   let divisions = ebible_book_divisions();
   let name_new = ebible_testament_new_name();
   function division_new_is(division) {
-    let testament = property_get(division, "testament");
-    let is = equal(testament, name_new);
+    let is = property_equals(division, "testament", name_new);
     return is;
   }
   let divisions_new = list_filter(divisions, division_new_is);
