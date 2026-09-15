df62b6b1e14ff14bf4f47851275ac28cae898a33
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:49:08 +0800

    function_fold list_size_equal app_ceb_bible_gloss_roots_store_outvoted_chapters_entry_read

--- js/app_ceb_bible_gloss_roots_store_outvoted_chapters_entry_read.mjs
+++ js/app_ceb_bible_gloss_roots_store_outvoted_chapters_entry_read.mjs
@@ -1,7 +1,6 @@
+import { list_size_equal } from "./list_size_equal.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { property_get } from "./property_get.mjs";
-import { list_size } from "./list_size.mjs";
-import { equal } from "./equal.mjs";
 import { text_lower_to } from "./text_lower_to.mjs";
 import { list_get } from "./list_get.mjs";
 import { property_initialize_list } from "./property_initialize_list.mjs";
@@ -15,8 +14,7 @@ export function app_ceb_bible_gloss_roots_store_outvoted_chapters_entry_read(
     let chapter_code = property_get(found, "chapter_code");
     let entry = property_get(found, "entry");
     let claimed = property_get(found, "claimed");
-    let count = list_size(claimed);
-    let empty = equal(count, 0);
+    let empty = list_size_equal(claimed, 0);
     if (empty) {
       return;
     }
