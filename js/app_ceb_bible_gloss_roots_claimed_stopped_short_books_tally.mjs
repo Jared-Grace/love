af3e585c26d5a49349b4929be8a0ef74164621d7
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 13:51:07 +0800

    function_fold property_null_is app_ceb_bible_gloss_roots_claimed_stopped_short_books_tally

--- js/app_ceb_bible_gloss_roots_claimed_stopped_short_books_tally.mjs
+++ js/app_ceb_bible_gloss_roots_claimed_stopped_short_books_tally.mjs
@@ -1,9 +1,8 @@
+import { property_null_is } from "./property_null_is.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { binisaya_words_known } from "./binisaya_words_known.mjs";
 import { binisaya_words_known_roots_named } from "./binisaya_words_known_roots_named.mjs";
 import { gloss_word_folded } from "./gloss_word_folded.mjs";
-import { property_get_or_null } from "./property_get_or_null.mjs";
-import { null_is } from "./null_is.mjs";
 import { not } from "./not.mjs";
 import { ebible_chapter_code_to_book } from "./ebible_chapter_code_to_book.mjs";
 import { tally_number_add } from "./tally_number_add.mjs";
@@ -20,8 +19,7 @@ export async function app_ceb_bible_gloss_roots_claimed_stopped_short_books_tall
   let sightings_named = 0;
   function vouched_is(spelling) {
     let folded = gloss_word_folded(spelling);
-    let held = property_get_or_null(vouched, folded);
-    let b = null_is(held);
+    let b = property_null_is(vouched, folded);
     let there = not(b);
     return there;
   }
