5a73e264d9db9dfede691852115c7b67ec797842
JESUS rose to life <username@users.noreply.github.com>
2026-09-15 19:44:48 +0800

    function_rename app_g_bless_cover_lift bless_cover_lift

--- js/bless_cover_lift.mjs
+++ js/bless_cover_lift.mjs
@@ -3,7 +3,7 @@ import { html_imgs_load_wait } from "./html_imgs_load_wait.mjs";
 import { html_style_opacity } from "./html_style_opacity.mjs";
 import { sleep } from "./sleep.mjs";
 import { html_remove } from "./html_remove.mjs";
-export async function app_g_bless_cover_lift(container_map, cover) {
+export async function bless_cover_lift(container_map, cover) {
   arguments_assert(arguments, 2);
   await html_imgs_load_wait(container_map);
   html_style_opacity(cover, "0");
