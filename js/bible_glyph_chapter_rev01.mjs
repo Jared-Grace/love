2c6c0b7546be2619a7b4aa649382414b3ef2ae91
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:14:58 +0800

    bible_glyph_chapter_verse_word_replace REV01 9 on 1 $tray_in

--- js/bible_glyph_chapter_rev01.mjs
+++ js/bible_glyph_chapter_rev01.mjs
@@ -295,7 +295,7 @@ export function bible_glyph_chapter_rev01() {
           "$tray_in",
           "$cross,",
           "was",
-          "on",
+          "$tray_in",
           "the",
           "island",
           "of",
