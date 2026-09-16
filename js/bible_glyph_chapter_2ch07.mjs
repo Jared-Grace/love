1176a45379a36e72553818fd6d20b5704b29157e
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:15:05 +0800

    bible_glyph_chapter_verse_word_replace 2CH07 22 gods, 1 $fire,

--- js/bible_glyph_chapter_2ch07.mjs
+++ js/bible_glyph_chapter_2ch07.mjs
@@ -789,7 +789,7 @@ export function bible_glyph_chapter_2ch07() {
           "have",
           "embraced",
           "other",
-          "gods,",
+          "$fire,",
           "$bowing",
           "and",
           "$kneeling",
