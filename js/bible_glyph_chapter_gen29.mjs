f07c1ae77e1a105a0f4ea3e63e95c5182c668db0
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:15:07 +0800

    bible_glyph_chapter_verse_word_replace GEN29 19 than 1 $hands_giving

--- js/bible_glyph_chapter_gen29.mjs
+++ js/bible_glyph_chapter_gen29.mjs
@@ -510,7 +510,7 @@ export function bible_glyph_chapter_gen29() {
           "her",
           "to",
           "you",
-          "than",
+          "$hands_giving",
           "to",
           "another",
           "man.",
