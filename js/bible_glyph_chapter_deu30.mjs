d2e0e76e1fe5cd39f276af2c4c6922233d7fd838
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:02:42 +0800

    bible_glyph_chapter_verse_word_replace DEU30 6 Him 1 $name_tag

--- js/bible_glyph_chapter_deu30.mjs
+++ js/bible_glyph_chapter_deu30.mjs
@@ -218,7 +218,7 @@ export function bible_glyph_chapter_deu30() {
           "you",
           "may",
           "$heart_red",
-          "Him",
+          "$name_tag",
           "with",
           "all",
           "your",
