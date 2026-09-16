e6306d67ccbce3196d662c9ccfe0d10501d1014d
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:15:22 +0800

    bible_glyph_chapter_verse_word_replace GEN37 10 actually 1 $footprints

--- js/bible_glyph_chapter_gen37.mjs
+++ js/bible_glyph_chapter_gen37.mjs
@@ -309,7 +309,7 @@ export function bible_glyph_chapter_gen37() {
           "$brother",
           "and",
           "I",
-          "actually",
+          "$footprints",
           "$footprints",
           "and",
           "$bowing",
