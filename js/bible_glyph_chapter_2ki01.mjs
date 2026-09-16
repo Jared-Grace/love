973f08c98bcc7a0f31b1fa8eb41d145113c9bfc8
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:15:14 +0800

    bible_glyph_chapter_verse_word_replace 2KI01 6 god 1 $fire

--- js/bible_glyph_chapter_2ki01.mjs
+++ js/bible_glyph_chapter_2ki01.mjs
@@ -235,7 +235,7 @@ export function bible_glyph_chapter_2ki01() {
           "Baal",
           "zebub,",
           "the",
-          "god",
+          "$fire",
           "of",
           "Ekron?",
           "Therefore",
