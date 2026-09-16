26f6fd825a181e4e26f8a315d3598fbf7584b2a2
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 14:06:40 +0800

    bible_glyph_chapter_verse_word_replace LEV19 26 or 1 $no_entry

--- js/bible_glyph_chapter_lev19.mjs
+++ js/bible_glyph_chapter_lev19.mjs
@@ -738,7 +738,7 @@ export function bible_glyph_chapter_lev19() {
           "$no_entry",
           "practise",
           "divination",
-          "or",
+          "$no_entry",
           "sorcery.",
         ],
       },
