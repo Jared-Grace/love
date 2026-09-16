aa6d53ffc8466db33f2928f878924765f894dc58
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 14:06:26 +0800

    bible_glyph_chapter_verse_word_replace EZK18 6 or 1 $no_entry

--- js/bible_glyph_chapter_ezk18.mjs
+++ js/bible_glyph_chapter_ezk18.mjs
@@ -149,7 +149,7 @@ export function bible_glyph_chapter_ezk18() {
           "the",
           "$mountain",
           "shrines",
-          "or",
+          "$no_entry",
           "lift",
           "up",
           "his",
