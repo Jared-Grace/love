25614daef78b3429432dfb11795dc822920ce889
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:15:23 +0800

    bible_glyph_chapter_verse_word_replace JER31 34 or 1 $man_beard

--- js/bible_glyph_chapter_jer31.mjs
+++ js/bible_glyph_chapter_jer31.mjs
@@ -1143,7 +1143,7 @@ export function bible_glyph_chapter_jer31() {
           "teach",
           "his",
           "neighbor",
-          "or",
+          "$man_beard",
           "his",
           "$brother,",
           "$speech,",
