3fe5b3c6675c4299baf6d8af4d30b11558877762
JESUS rose to life <username@users.noreply.github.com>
2026-09-16 15:15:00 +0800

    bible_glyph_chapter_verse_word_replace GEN08 21 He 1 $name_tag

--- js/bible_glyph_chapter_gen08.mjs
+++ js/bible_glyph_chapter_gen08.mjs
@@ -579,7 +579,7 @@ export function bible_glyph_chapter_gen08() {
           "the",
           "pleasing",
           "aroma,",
-          "He",
+          "$name_tag",
           "$speech",
           "in",
           "His",
