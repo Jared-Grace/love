6c168892aea7dfbf93ce98450961f4e9eab3053a
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 17:43:41 +0800

    bible_glyph_chapter_verse_word_replace 2KI10 24 in 1 $footprints

--- js/bible_glyph_chapter_2ki10.mjs
+++ js/bible_glyph_chapter_2ki10.mjs
@@ -902,7 +902,7 @@ export function bible_glyph_chapter_2ki10() {
           "And",
           "they",
           "$footprints",
-          "in",
+          "$footprints",
           "to",
           "$hammer",
           "sacrifices",
