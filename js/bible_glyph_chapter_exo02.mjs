5b4a742129a0413b9b4abe27d89284696d819046
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 18:10:51 +0800

    bible_glyph_chapter_verse_word_mark EXO02 14 thing 1 speech

--- js/bible_glyph_chapter_exo02.mjs
+++ js/bible_glyph_chapter_exo02.mjs
@@ -439,7 +439,7 @@ export function bible_glyph_chapter_exo02() {
           "and",
           "$speech,",
           "This",
-          "thing",
+          "$speech",
           "I",
           "have",
           "done",
