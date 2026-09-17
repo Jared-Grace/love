630504528a9b2d02f1ec41ca419db6a66792aaaf
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 18:10:47 +0800

    ai

--- js/bible_glyph_chapter_1sa02.mjs
+++ js/bible_glyph_chapter_1sa02.mjs
@@ -737,7 +737,7 @@ export function bible_glyph_chapter_1sa02() {
           "you",
           "$hammer",
           "these",
-          "things?",
+          "$speech?",
           "I",
           "$ear",
           "about",
