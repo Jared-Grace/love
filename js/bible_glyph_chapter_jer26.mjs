e7101655569b18892de6bc2f49b8b853d49de09e
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 17:43:23 +0800

    bible_glyph_chapter_verse_word_replace JER26 20 name 1 $name_tag

--- js/bible_glyph_chapter_jer26.mjs
+++ js/bible_glyph_chapter_jer26.mjs
@@ -755,7 +755,7 @@ export function bible_glyph_chapter_jer26() {
           "prophesying",
           "in",
           "the",
-          "name",
+          "$name_tag",
           "of",
           "the",
           "$name_tag,",
