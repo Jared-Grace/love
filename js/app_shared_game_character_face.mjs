5cd504598eda8bae1b40a9529384980863053d1a
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 16:25:48 +0800

    function_delete_unused app_shared_game_character_face

--- js/app_shared_game_character_face.mjs
+++
@@ -1,6 +0,0 @@
-import { app_shared_game_character_picture_set } from "./app_shared_game_character_picture_set.mjs";
-import { g_character_face_set } from "./g_character_face_set.mjs";
-export function app_shared_game_character_face(character, img, direction) {
-  g_character_face_set(character, direction);
-  app_shared_game_character_picture_set(character, img, direction);
-}
