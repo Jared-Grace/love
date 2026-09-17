4a4655a80eb1859532af04e6ce6518b43bf947d8
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 17:43:49 +0800

    bible_glyph_chapter_verse_word_replace 2KI05 11 name 1 $name_tag

--- js/bible_glyph_chapter_2ki05.mjs
+++ js/bible_glyph_chapter_2ki05.mjs
@@ -403,7 +403,7 @@ export function bible_glyph_chapter_2ki05() {
           "call",
           "on",
           "the",
-          "name",
+          "$name_tag",
           "of",
           "the",
           "$name_tag",
