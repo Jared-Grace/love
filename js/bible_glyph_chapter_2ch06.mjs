bd58d83794c44340915af496acd397ac9d848b58
JESUS rose to life <username@users.noreply.github.com>
2026-09-17 17:43:45 +0800

    bible_glyph_chapter_verse_word_replace 2CH06 8 name, 1 $name_tag,

--- js/bible_glyph_chapter_2ch06.mjs
+++ js/bible_glyph_chapter_2ch06.mjs
@@ -246,7 +246,7 @@ export function bible_glyph_chapter_2ch06() {
           "$house",
           "for",
           "My",
-          "name,",
+          "$name_tag,",
           "you",
           "have",
           "done",
