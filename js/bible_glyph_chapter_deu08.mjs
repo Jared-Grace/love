3802876787e71106294f81e665fe4851a72dc03c
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:00:17 +0800

    bible_glyph_chapter_verse_word_replace DEU08 2 years 1 $year

--- js/bible_glyph_chapter_deu08.mjs
+++ js/bible_glyph_chapter_deu08.mjs
@@ -64,7 +64,7 @@ export function bible_glyph_chapter_deu08() {
           "$road",
           "these",
           "forty",
-          "years",
+          "$year",
           "in",
           "the",
           "wilderness,",
