673e67457862bdd3d3c02bf159cda3332bb9ba0d
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 17:21:29 +0800

    bible_glyph_chapter_name_badges_draw DEU08

--- js/bible_glyph_chapter_deu08.mjs
+++ js/bible_glyph_chapter_deu08.mjs
@@ -436,7 +436,7 @@ export function bible_glyph_chapter_deu08() {
           "the",
           "$earth",
           "of",
-          "Egypt,",
+          "$proper_name$Egypt,",
           "out",
           "of",
           "the",
