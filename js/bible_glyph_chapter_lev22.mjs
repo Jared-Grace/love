c10fa0c7a4fd99f9fe48914b865b5d165280a337
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:04:18 +0800

    bible_glyph_chapter_verse_word_replace LEV22 12 eat 1 $eating

--- js/bible_glyph_chapter_lev22.mjs
+++ js/bible_glyph_chapter_lev22.mjs
@@ -368,7 +368,7 @@ export function bible_glyph_chapter_lev22() {
           "is",
           "$no_entry",
           "to",
-          "eat",
+          "$eating",
           "the",
           "contributions",
           "of",
