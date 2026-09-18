637647126508021f93eb2d9ce49feb6f9026c8d5
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 17:46:25 +0800

    bible_glyph_chapter_name_badges_draw ISA61

--- js/bible_glyph_chapter_isa61.mjs
+++ js/bible_glyph_chapter_isa61.mjs
@@ -91,7 +91,7 @@ export function bible_glyph_chapter_isa61() {
           "the",
           "mourners",
           "in",
-          "Zion",
+          "$proper_name$Zion",
           "-",
           "to",
           "$hands_giving",
