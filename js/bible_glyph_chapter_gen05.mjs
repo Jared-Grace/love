b5e3acc39cdb865c3131b2d72d84d0077d4f72e9
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:01:33 +0800

    bible_glyph_chapter_verse_word_replace GEN05 4 years, 1 $year,

--- js/bible_glyph_chapter_gen05.mjs
+++ js/bible_glyph_chapter_gen05.mjs
@@ -109,7 +109,7 @@ export function bible_glyph_chapter_gen05() {
           "were",
           "eight",
           "hundred",
-          "years,",
+          "$year,",
           "and",
           "he",
           "fathered",
