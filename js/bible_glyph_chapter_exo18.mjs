8a0a5ae1030b466cf374a0a14ea1ad4e31a9dd7b
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:49:19 +0800

    bible_glyph_chapter_verse_word_replace EXO18 14 sit 1 $person+house

--- js/bible_glyph_chapter_exo18.mjs
+++ js/bible_glyph_chapter_exo18.mjs
@@ -455,7 +455,7 @@ export function bible_glyph_chapter_exo18() {
           "Why",
           "do",
           "you",
-          "sit",
+          "$person+house",
           "alone",
           "as",
           "judge,",
