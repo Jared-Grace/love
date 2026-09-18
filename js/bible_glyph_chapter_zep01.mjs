2f286ed3c68c2d08749027298231e8e053b91ba4
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:00:03 +0800

    bible_glyph_chapter_verse_word_replace ZEP01 18 dwell 1 $person+house

--- js/bible_glyph_chapter_zep01.mjs
+++ js/bible_glyph_chapter_zep01.mjs
@@ -609,7 +609,7 @@ export function bible_glyph_chapter_zep01() {
           "of",
           "all",
           "who",
-          "dwell",
+          "$person+house",
           "on",
           "the",
           "$earth.",
