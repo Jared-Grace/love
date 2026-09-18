69fd8e69f41011495a0bb039c0af6d099be1620d
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:26:04 +0800

    bible_glyph_chapter_verse_word_replace ISA06 8 Send 1 $hand_sending

--- js/bible_glyph_chapter_isa06.mjs
+++ js/bible_glyph_chapter_isa06.mjs
@@ -255,7 +255,7 @@ export function bible_glyph_chapter_isa06() {
           "Here",
           "am",
           "I.",
-          "Send",
+          "$hand_sending",
           "me!",
         ],
       },
