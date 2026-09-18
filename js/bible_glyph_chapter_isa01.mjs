6a0af1eb870b2b84112ed1dcc23c70e751f1a2b5
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:12:12 +0800

    ai

--- js/bible_glyph_chapter_isa01.mjs
+++ js/bible_glyph_chapter_isa01.mjs
@@ -147,7 +147,7 @@ export function bible_glyph_chapter_isa01() {
           "you",
           "want",
           "more",
-          "beatings?",
+          "$fist?",
           "Why",
           "do",
           "you",
