f826de287c1cdc97ae28f08012a9144b3fd80487
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:59:54 +0800

    bible_glyph_chapter_verse_word_replace LAM04 12 people 1 $person+house

--- js/bible_glyph_chapter_lam04.mjs
+++ js/bible_glyph_chapter_lam04.mjs
@@ -343,7 +343,7 @@ export function bible_glyph_chapter_lam04() {
           "$anchor,",
           "nor",
           "any",
-          "people",
+          "$person+house",
           "of",
           "the",
           "$globe,",
