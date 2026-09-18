2ff681a6c1d1010452ae4ed144b5d8336b06071f
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:57:15 +0800

    bible_glyph_chapter_verse_word_replace EXO19 13 approach 1 $walking+pointing_up

--- js/bible_glyph_chapter_exo19.mjs
+++ js/bible_glyph_chapter_exo19.mjs
@@ -448,7 +448,7 @@ export function bible_glyph_chapter_exo19() {
           "blast",
           "may",
           "they",
-          "approach",
+          "$walking+pointing_up",
           "the",
           "$mountain.",
         ],
