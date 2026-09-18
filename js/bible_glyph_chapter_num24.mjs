571a6bb5f86dfad8ba2a95cf8c90aa220caf2896
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 18:54:15 +0800

    bible_glyph_chapter_verse_word_replace NUM24 16 Almighty, 1 $might,

--- js/bible_glyph_chapter_num24.mjs
+++ js/bible_glyph_chapter_num24.mjs
@@ -492,7 +492,7 @@ export function bible_glyph_chapter_num24() {
           "vision",
           "from",
           "the",
-          "Almighty,",
+          "$might,",
           "who",
           "bows",
           "down",
