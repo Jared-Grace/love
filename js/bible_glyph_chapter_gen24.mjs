9c6914a649985ee39287917b02f8da746fa28639
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:28:42 +0800

    bible_glyph_chapter_verse_word_replace GEN24 18 jar 1 $jar

--- js/bible_glyph_chapter_gen24.mjs
+++ js/bible_glyph_chapter_gen24.mjs
@@ -586,7 +586,7 @@ export function bible_glyph_chapter_gen24() {
           "quickly",
           "lowered",
           "her",
-          "jar",
+          "$jar",
           "to",
           "her",
           "$hand",
