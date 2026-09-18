07208c9ac780715d0a33e3fde9328ba13b1b9dec
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 18:56:52 +0800

    bible_glyph_chapter_verse_word_replace 1JN04 14 Savior 1 $rescue

--- js/bible_glyph_chapter_1jn04.mjs
+++ js/bible_glyph_chapter_1jn04.mjs
@@ -399,7 +399,7 @@ export function bible_glyph_chapter_1jn04() {
           "to",
           "be",
           "the",
-          "Savior",
+          "$rescue",
           "of",
           "the",
           "$globe.",
