fd743b6dc639ee9cef3b1de3328cbe4986910de2
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:25:29 +0800

    bible_glyph_chapter_verse_word_replace EXO09 13 go, 1 $hand_sending,

--- js/bible_glyph_chapter_exo09.mjs
+++ js/bible_glyph_chapter_exo09.mjs
@@ -395,7 +395,7 @@ export function bible_glyph_chapter_exo09() {
           "Let",
           "My",
           "$family",
-          "go,",
+          "$hand_sending,",
           "so",
           "that",
           "they",
