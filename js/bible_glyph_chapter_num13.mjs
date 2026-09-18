a106b18f94d7f8091fb481a71cd787bb2ecbf411
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:57:36 +0800

    bible_glyph_chapter_verse_word_replace NUM13 30 go 1 $walking+pointing_up

--- js/bible_glyph_chapter_num13.mjs
+++ js/bible_glyph_chapter_num13.mjs
@@ -704,7 +704,7 @@ export function bible_glyph_chapter_num13() {
           "$speech,",
           "We",
           "must",
-          "go",
+          "$walking+pointing_up",
           "up",
           "and",
           "take",
