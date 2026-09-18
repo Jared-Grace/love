1df49bdb9d6ce5c90039cde327b440fa6dbf66bc
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:13:23 +0800

    bible_glyph_chapter_verse_word_replace GEN30 10 bore 1 $baby

--- js/bible_glyph_chapter_gen30.mjs
+++ js/bible_glyph_chapter_gen30.mjs
@@ -236,7 +236,7 @@ export function bible_glyph_chapter_gen30() {
           "servant",
           "of",
           "Leah,",
-          "bore",
+          "$baby",
           "$proper_name$Jacob",
           "$son.",
         ],
