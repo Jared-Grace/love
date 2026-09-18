9a4a0f16e1e332d62cde28b4812eca60900a3c6c
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:28:23 +0800

    bible_glyph_chapter_verse_word_replace MAL03 16 LORD 1 $i_am

--- js/bible_glyph_chapter_mal03.mjs
+++ js/bible_glyph_chapter_mal03.mjs
@@ -559,7 +559,7 @@ export function bible_glyph_chapter_mal03() {
           "who",
           "feared",
           "the",
-          "LORD",
+          "$i_am",
           "and",
           "honored",
           "His",
