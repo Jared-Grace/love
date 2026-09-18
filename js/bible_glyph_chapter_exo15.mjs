a09428a37a436830d55f0c5d00a764397cc1cae6
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:48:44 +0800

    bible_glyph_chapter_verse_word_replace EXO15 15 dwell 1 $person+house

--- js/bible_glyph_chapter_exo15.mjs
+++ js/bible_glyph_chapter_exo15.mjs
@@ -386,7 +386,7 @@ export function bible_glyph_chapter_exo15() {
           "$proper_name$Moab;",
           "those",
           "who",
-          "dwell",
+          "$person+house",
           "in",
           "$proper_name$Canaan",
           "will",
