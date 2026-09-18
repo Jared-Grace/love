08f60027880c502bd72cf4666f5aadd6bf3a0d99
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:28:02 +0800

    bible_glyph_chapter_verse_word_replace 1KI08 20 LORD, 1 $i_am,

--- js/bible_glyph_chapter_1ki08.mjs
+++ js/bible_glyph_chapter_1ki08.mjs
@@ -654,7 +654,7 @@ export function bible_glyph_chapter_1ki08() {
           "Name",
           "of",
           "the",
-          "LORD,",
+          "$i_am,",
           "the",
           "$fire",
           "of",
