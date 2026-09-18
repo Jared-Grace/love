3def534d4fd6aadd3c050740f1abc494ab58315f
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:28:18 +0800

    bible_glyph_chapter_verse_word_replace JER31 35 LORD 1 $i_am

--- js/bible_glyph_chapter_jer31.mjs
+++ js/bible_glyph_chapter_jer31.mjs
@@ -1221,7 +1221,7 @@ export function bible_glyph_chapter_jer31() {
           "roar",
           "-",
           "the",
-          "LORD",
+          "$i_am",
           "of",
           "Hosts",
           "is",
