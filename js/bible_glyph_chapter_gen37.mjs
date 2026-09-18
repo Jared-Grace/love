1eff5344eafedbb18a5e92989b077eb65b170807
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:05:12 +0800

    bible_glyph_chapter_verse_word_replace GEN37 25 eat 1 $eating

--- js/bible_glyph_chapter_gen37.mjs
+++ js/bible_glyph_chapter_gen37.mjs
@@ -717,7 +717,7 @@ export function bible_glyph_chapter_gen37() {
           "sat",
           "down",
           "to",
-          "eat",
+          "$eating",
           "a",
           "$bread,",
           "they",
