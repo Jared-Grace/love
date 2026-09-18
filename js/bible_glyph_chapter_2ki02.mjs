5b1ca41ebfbf91eb4771689f477671447a5c0da1
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:48:40 +0800

    bible_glyph_chapter_verse_word_replace 2KI02 4 stay 1 $person+house

--- js/bible_glyph_chapter_2ki02.mjs
+++ js/bible_glyph_chapter_2ki02.mjs
@@ -133,7 +133,7 @@ export function bible_glyph_chapter_2ki02() {
           "to",
           "him,",
           "Please",
-          "stay",
+          "$person+house",
           "here,",
           "for",
           "the",
