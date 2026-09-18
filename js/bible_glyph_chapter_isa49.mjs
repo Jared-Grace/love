717ed86260adb13e329a4005c28ed655ecde7974
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:06:39 +0800

    bible_glyph_chapter_verse_word_replace ISA49 18 Lift 1 $lifting

--- js/bible_glyph_chapter_isa49.mjs
+++ js/bible_glyph_chapter_isa49.mjs
@@ -586,7 +586,7 @@ export function bible_glyph_chapter_isa49() {
       {
         verse_number: 18,
         words: [
-          "Lift",
+          "$lifting",
           "up",
           "your",
           "$eyes",
