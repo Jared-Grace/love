68e2cf63ba92b6baf108aec8ea60418b189344a5
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:28:59 +0800

    bible_glyph_chapter_verse_word_replace EST01 11 Queen 1 $queen

--- js/bible_glyph_chapter_est01.mjs
+++ js/bible_glyph_chapter_est01.mjs
@@ -325,7 +325,7 @@ export function bible_glyph_chapter_est01() {
         words: [
           "to",
           "$footprints",
-          "Queen",
+          "$queen",
           "$proper_name$Vashti",
           "before",
           "$king,",
