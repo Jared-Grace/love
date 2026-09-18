c13acaf1a030a0fccb6bd97b11c15954d8ba8cd2
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 18:53:39 +0800

    bible_glyph_chapter_verse_word_replace JUD01 25 Savior, 1 $rescue,

--- js/bible_glyph_chapter_jud01.mjs
+++ js/bible_glyph_chapter_jud01.mjs
@@ -703,7 +703,7 @@ export function bible_glyph_chapter_jud01() {
           "only",
           "$fire",
           "our",
-          "Savior,",
+          "$rescue,",
           "through",
           "$cross",
           "$oil",
