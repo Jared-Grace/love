2f181ccf67ce081033e83fd66b0c7c1ba80f766d
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:06:04 +0800

    bible_glyph_chapter_verse_word_replace 2SA05 21 away. 1 $lifting.

--- js/bible_glyph_chapter_2sa05.mjs
+++ js/bible_glyph_chapter_2sa05.mjs
@@ -612,7 +612,7 @@ export function bible_glyph_chapter_2sa05() {
           "men",
           "carried",
           "them",
-          "away.",
+          "$lifting.",
         ],
       },
       {
