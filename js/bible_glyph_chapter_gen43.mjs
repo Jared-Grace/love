ebacdb5106441dae8e6dc8850a03501c67292c27
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:25:40 +0800

    bible_glyph_chapter_verse_word_replace GEN43 4 send 1 $hand_sending

--- js/bible_glyph_chapter_gen43.mjs
+++ js/bible_glyph_chapter_gen43.mjs
@@ -91,7 +91,7 @@ export function bible_glyph_chapter_gen43() {
           "If",
           "you",
           "will",
-          "send",
+          "$hand_sending",
           "our",
           "$brother",
           "with",
