2b7413f60402561f90e7f322a626b5ab59affde4
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:13:03 +0800

    bible_glyph_chapter_verse_word_replace DEU32 18 forth; 1 $baby;

--- js/bible_glyph_chapter_deu32.mjs
+++ js/bible_glyph_chapter_deu32.mjs
@@ -495,7 +495,7 @@ export function bible_glyph_chapter_deu32() {
           "who",
           "brought",
           "you",
-          "forth;",
+          "$baby;",
           "you",
           "forgot",
           "the",
