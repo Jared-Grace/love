207c2dac0480f8cee8d0baf0f60898e9b5738037
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:01:18 +0800

    bible_glyph_chapter_verse_word_replace 2KI10 36 years. 1 $year.

--- js/bible_glyph_chapter_2ki10.mjs
+++ js/bible_glyph_chapter_2ki10.mjs
@@ -1323,7 +1323,7 @@ export function bible_glyph_chapter_2ki10() {
           "were",
           "twenty",
           "eight",
-          "years.",
+          "$year.",
         ],
       },
     ],
