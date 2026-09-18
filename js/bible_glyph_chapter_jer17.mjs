699ed05657c19c08419a5ccf7bbfe8bbb635f918
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:03:10 +0800

    bible_glyph_chapter_verse_word_replace JER17 27 consume 1 $eating

--- js/bible_glyph_chapter_jer17.mjs
+++ js/bible_glyph_chapter_jer17.mjs
@@ -945,7 +945,7 @@ export function bible_glyph_chapter_jer17() {
           "its",
           "gates",
           "to",
-          "consume",
+          "$eating",
           "the",
           "citadels",
           "of",
