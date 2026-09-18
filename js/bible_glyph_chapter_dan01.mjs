174942fa51f65a4ff3d96f6b133ee3959b82ba50
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:05:01 +0800

    bible_glyph_chapter_verse_word_replace DAN01 12 eat 1 $eating

--- js/bible_glyph_chapter_dan01.mjs
+++ js/bible_glyph_chapter_dan01.mjs
@@ -383,7 +383,7 @@ export function bible_glyph_chapter_dan01() {
           "$tray_out",
           "vegetables",
           "to",
-          "eat",
+          "$eating",
           "and",
           "$water",
           "to",
