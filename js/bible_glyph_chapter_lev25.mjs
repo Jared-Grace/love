60422f4bc4add6d9426d0f08e3a5804f5f5ad809
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:01:50 +0800

    bible_glyph_chapter_verse_word_replace LEV25 11 year 1 $year

--- js/bible_glyph_chapter_lev25.mjs
+++ js/bible_glyph_chapter_lev25.mjs
@@ -325,7 +325,7 @@ export function bible_glyph_chapter_lev25() {
         words: [
           "The",
           "fiftieth",
-          "year",
+          "$year",
           "will",
           "be",
           "a",
