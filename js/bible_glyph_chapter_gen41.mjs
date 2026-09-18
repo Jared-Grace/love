4c92d07d971532f1e4a03bd19ae589930a7969f4
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:00:58 +0800

    bible_glyph_chapter_verse_word_replace GEN41 36 years 1 $year

--- js/bible_glyph_chapter_gen41.mjs
+++ js/bible_glyph_chapter_gen41.mjs
@@ -991,7 +991,7 @@ export function bible_glyph_chapter_gen41() {
           "during",
           "the",
           "seven",
-          "years",
+          "$year",
           "of",
           "famine",
           "to",
