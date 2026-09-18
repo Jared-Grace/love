f521c6299c9db059edcd8aba2b7c647af210e847
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:13:05 +0800

    bible_glyph_chapter_verse_word_replace GEN21 3 bore 1 $baby

--- js/bible_glyph_chapter_gen21.mjs
+++ js/bible_glyph_chapter_gen21.mjs
@@ -74,7 +74,7 @@ export function bible_glyph_chapter_gen21() {
           "the",
           "$son",
           "$proper_name$Sarah",
-          "bore",
+          "$baby",
           "to",
           "him.",
         ],
