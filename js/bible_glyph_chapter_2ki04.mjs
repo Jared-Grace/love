4501dab08c58c28e7495139a2ea5010e1bc8c6d6
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:03:05 +0800

    bible_glyph_chapter_verse_word_replace 2KI04 43 eat, 1 $eating,

--- js/bible_glyph_chapter_2ki04.mjs
+++ js/bible_glyph_chapter_2ki04.mjs
@@ -1442,7 +1442,7 @@ export function bible_glyph_chapter_2ki04() {
           "the",
           "$family",
           "to",
-          "eat,",
+          "$eating,",
           "Elisha",
           "$speech,",
           "for",
