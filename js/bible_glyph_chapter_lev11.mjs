850d4d7060e73679fad26df9fc9fa6a73f5cb8c2
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:04:05 +0800

    bible_glyph_chapter_verse_word_replace LEV11 3 eat 1 $eating

--- js/bible_glyph_chapter_lev11.mjs
+++ js/bible_glyph_chapter_lev11.mjs
@@ -58,7 +58,7 @@ export function bible_glyph_chapter_lev11() {
         words: [
           "You",
           "may",
-          "eat",
+          "$eating",
           "any",
           "animal",
           "that",
