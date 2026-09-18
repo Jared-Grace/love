09b584d0c1101a59f0799a321634055aba2ca7af
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:02:25 +0800

    bible_glyph_chapter_verse_word_replace JDG13 7 eat 1 $eating

--- js/bible_glyph_chapter_jdg13.mjs
+++ js/bible_glyph_chapter_jdg13.mjs
@@ -257,7 +257,7 @@ export function bible_glyph_chapter_jdg13() {
           "strong",
           "drink,",
           "and",
-          "eat",
+          "$eating",
           "$no_entry",
           "anything",
           "unclean,",
