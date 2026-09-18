52cab346bc7d021431b4cd1758d74269b67fecd4
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:04:43 +0800

    bible_glyph_chapter_verse_word_replace DEU12 23 eat 1 $eating

--- js/bible_glyph_chapter_deu12.mjs
+++ js/bible_glyph_chapter_deu12.mjs
@@ -788,7 +788,7 @@ export function bible_glyph_chapter_deu12() {
           "you",
           "must",
           "$no_entry",
-          "eat",
+          "$eating",
           "the",
           "$person_other",
           "with",
