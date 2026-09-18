86695da819789b38eb88546c4691b728f67dd321
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:02:00 +0800

    bible_glyph_chapter_verse_word_replace NUM09 1 year 1 $year

--- js/bible_glyph_chapter_num09.mjs
+++ js/bible_glyph_chapter_num09.mjs
@@ -30,7 +30,7 @@ export function bible_glyph_chapter_num09() {
           "of",
           "the",
           "second",
-          "year",
+          "$year",
           "after",
           "Israel",
           "had",
