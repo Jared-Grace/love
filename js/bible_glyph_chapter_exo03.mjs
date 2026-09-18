2b79d0cbfb6b0853500d5450a26a8c164aafafc0
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:55:35 +0800

    bible_glyph_chapter_verse_word_replace EXO03 12 out 1 $walking+tray_out

--- js/bible_glyph_chapter_exo03.mjs
+++ js/bible_glyph_chapter_exo03.mjs
@@ -400,7 +400,7 @@ export function bible_glyph_chapter_exo03() {
           "brought",
           "the",
           "$family",
-          "out",
+          "$walking+tray_out",
           "of",
           "$proper_name$Egypt,",
           "all",
