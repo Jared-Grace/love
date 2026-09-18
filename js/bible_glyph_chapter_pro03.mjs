8421c08c5a3e163214917e76431d132cfce2197e
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:02:22 +0800

    bible_glyph_chapter_verse_word_replace PRO03 2 years 1 $year

--- js/bible_glyph_chapter_pro03.mjs
+++ js/bible_glyph_chapter_pro03.mjs
@@ -42,7 +42,7 @@ export function bible_glyph_chapter_pro03() {
           "to",
           "your",
           "$sun,",
-          "years",
+          "$year",
           "to",
           "your",
           "$sprout,",
