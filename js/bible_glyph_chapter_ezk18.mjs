11efb02c94a6084a1c2ebe6b8090fef783ba2642
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:05:30 +0800

    bible_glyph_chapter_verse_word_replace EZK18 19 bear 1 $lifting

--- js/bible_glyph_chapter_ezk18.mjs
+++ js/bible_glyph_chapter_ezk18.mjs
@@ -575,7 +575,7 @@ export function bible_glyph_chapter_ezk18() {
           "shouldn’t",
           "the",
           "$son",
-          "bear",
+          "$lifting",
           "the",
           "iniquity",
           "of",
