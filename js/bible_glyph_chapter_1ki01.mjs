a11f03b55260419f694afc6e72a84987f5ec588d
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:28:54 +0800

    bible_glyph_chapter_verse_word_replace 1KI01 3 girl, 1 $girl,

--- js/bible_glyph_chapter_1ki01.mjs
+++ js/bible_glyph_chapter_1ki01.mjs
@@ -97,7 +97,7 @@ export function bible_glyph_chapter_1ki01() {
           "for",
           "a",
           "beautiful",
-          "girl,",
+          "$girl,",
           "and",
           "they",
           "found",
