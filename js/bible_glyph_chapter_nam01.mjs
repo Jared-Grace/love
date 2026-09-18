f8c9405409ac257734c573d9490dcc5a63a1f993
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:06:55 +0800

    bible_glyph_chapter_verse_word_replace NAM01 5 trembles 1 $lifting

--- js/bible_glyph_chapter_nam01.mjs
+++ js/bible_glyph_chapter_nam01.mjs
@@ -150,7 +150,7 @@ export function bible_glyph_chapter_nam01() {
           "away;",
           "the",
           "$earth",
-          "trembles",
+          "$lifting",
           "at",
           "His",
           "presence",
