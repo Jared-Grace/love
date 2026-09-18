301fe2b23c5c861f64eb865dfb674a10206edd33
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:57:08 +0800

    bible_glyph_chapter_verse_word_replace DEU29 23 growing 1 $walking+pointing_up

--- js/bible_glyph_chapter_deu29.mjs
+++ js/bible_glyph_chapter_deu29.mjs
@@ -727,7 +727,7 @@ export function bible_glyph_chapter_deu29() {
           "with",
           "$no_entry",
           "plant",
-          "growing",
+          "$walking+pointing_up",
           "on",
           "it,",
           "just",
