5d016a031e79f4f8c63af5e33a9edb3ea24fbd7f
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:28:53 +0800

    bible_glyph_chapter_verse_word_replace GEN02 14 name 1 $name_tag

--- js/bible_glyph_chapter_gen02.mjs
+++ js/bible_glyph_chapter_gen02.mjs
@@ -392,7 +392,7 @@ export function bible_glyph_chapter_gen02() {
         verse_number: 14,
         words: [
           "The",
-          "name",
+          "$name_tag",
           "of",
           "the",
           "third",
