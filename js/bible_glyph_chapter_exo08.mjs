4ec72406d307f8fc9c57061d8d7fee2a8d48d933
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:56:53 +0800

    bible_glyph_chapter_verse_word_replace EXO08 4 come 1 $walking+pointing_up

--- js/bible_glyph_chapter_exo08.mjs
+++ js/bible_glyph_chapter_exo08.mjs
@@ -109,7 +109,7 @@ export function bible_glyph_chapter_exo08() {
           "The",
           "$frog",
           "will",
-          "come",
+          "$walking+pointing_up",
           "up",
           "on",
           "you",
