c9d18a0423e0ac961e631a2c00c34d34cf805f43
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:56:43 +0800

    bible_glyph_chapter_verse_word_replace 1KI22 6 Go 1 $walking+pointing_up

--- js/bible_glyph_chapter_1ki22.mjs
+++ js/bible_glyph_chapter_1ki22.mjs
@@ -181,7 +181,7 @@ export function bible_glyph_chapter_1ki22() {
           "refrain?",
           "They",
           "$speech,",
-          "Go",
+          "$walking+pointing_up",
           "up,",
           "and",
           "the",
