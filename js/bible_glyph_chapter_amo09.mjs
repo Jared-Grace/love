c3835f699b032070be9e08dedb4810841395488c
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:03:15 +0800

    bible_glyph_chapter_verse_word_replace AMO09 14 eat 1 $eating

--- js/bible_glyph_chapter_amo09.mjs
+++ js/bible_glyph_chapter_amo09.mjs
@@ -540,7 +540,7 @@ export function bible_glyph_chapter_amo09() {
           "$hammer",
           "gardens",
           "and",
-          "eat",
+          "$eating",
           "their",
           "fruit.",
         ],
