d04e668fa47fa7704707aa1d9885b5fd766b18cb
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:06:57 +0800

    bible_glyph_chapter_verse_word_replace PSA121 1 lift 1 $lifting

--- js/bible_glyph_chapter_psa121.mjs
+++ js/bible_glyph_chapter_psa121.mjs
@@ -18,7 +18,7 @@ export function bible_glyph_chapter_psa121() {
           "of",
           "ascents.",
           "I",
-          "lift",
+          "$lifting",
           "up",
           "my",
           "$eyes",
