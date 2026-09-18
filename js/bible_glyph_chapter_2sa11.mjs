a30e141243682d8042ef28721ade7daef95941f2
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:24:44 +0800

    bible_glyph_chapter_verse_word_replace 2SA11 18 sent 1 $hand_sending

--- js/bible_glyph_chapter_2sa11.mjs
+++ js/bible_glyph_chapter_2sa11.mjs
@@ -606,7 +606,7 @@ export function bible_glyph_chapter_2sa11() {
         verse_number: 18,
         words: [
           "$proper_name$Joab",
-          "sent",
+          "$hand_sending",
           "to",
           "$proper_name$David",
           "all",
