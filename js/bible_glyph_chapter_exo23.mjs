1fa3903b0cb09ed80cded9eb6dd1cb34299243d4
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:06:15 +0800

    bible_glyph_chapter_verse_word_replace EXO23 21 forgive 1 $lifting

--- js/bible_glyph_chapter_exo23.mjs
+++ js/bible_glyph_chapter_exo23.mjs
@@ -610,7 +610,7 @@ export function bible_glyph_chapter_exo23() {
           "he",
           "will",
           "$no_entry",
-          "forgive",
+          "$lifting",
           "rebellion,",
           "since",
           "My",
