9aac58a7ea510c001b3fa24842de35b85b01e714
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:59:56 +0800

    bible_glyph_chapter_verse_word_replace MIC04 4 sit 1 $person+house

--- js/bible_glyph_chapter_mic04.mjs
+++ js/bible_glyph_chapter_mic04.mjs
@@ -169,7 +169,7 @@ export function bible_glyph_chapter_mic04() {
           "each",
           "$man_beard",
           "will",
-          "sit",
+          "$person+house",
           "under",
           "his",
           "own",
