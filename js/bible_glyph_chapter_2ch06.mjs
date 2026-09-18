1e386be6ee5300bd8876baaf7c0b951eac6ec10e
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:59:43 +0800

    bible_glyph_chapter_verse_word_replace 2CH06 18 dwell 1 $person+house

--- js/bible_glyph_chapter_2ch06.mjs
+++ js/bible_glyph_chapter_2ch06.mjs
@@ -603,7 +603,7 @@ export function bible_glyph_chapter_2ch06() {
           "will",
           "$fire",
           "indeed",
-          "dwell",
+          "$person+house",
           "with",
           "$person",
           "upon",
