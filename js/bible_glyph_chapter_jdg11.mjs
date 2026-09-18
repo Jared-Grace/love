54386876c185f4d0510bc7564554e3c1c2761ce0
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:01:13 +0800

    bible_glyph_chapter_verse_word_replace JDG11 26 years. 1 $year.

--- js/bible_glyph_chapter_jdg11.mjs
+++ js/bible_glyph_chapter_jdg11.mjs
@@ -854,7 +854,7 @@ export function bible_glyph_chapter_jdg11() {
           "for",
           "three",
           "hundred",
-          "years.",
+          "$year.",
           "Why",
           "did",
           "you",
