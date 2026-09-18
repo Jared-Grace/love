5e9aa5296f261817ef74e0f1b679433e2d681dd0
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:04:25 +0800

    bible_glyph_chapter_verse_word_replace NUM18 10 eat 1 $eating

--- js/bible_glyph_chapter_num18.mjs
+++ js/bible_glyph_chapter_num18.mjs
@@ -386,7 +386,7 @@ export function bible_glyph_chapter_num18() {
           "You",
           "are",
           "to",
-          "eat",
+          "$eating",
           "it",
           "as",
           "a",
