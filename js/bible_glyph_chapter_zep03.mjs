f12b983322379cfded741b2714e30aafb3d5cf55
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:28:26 +0800

    bible_glyph_chapter_verse_word_replace ZEP03 9 LORD 1 $i_am

--- js/bible_glyph_chapter_zep03.mjs
+++ js/bible_glyph_chapter_zep03.mjs
@@ -283,7 +283,7 @@ export function bible_glyph_chapter_zep03() {
           "name",
           "of",
           "the",
-          "LORD",
+          "$i_am",
           "and",
           "$kneeling",
           "shoulder",
