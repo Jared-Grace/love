cb7a9bf424a901dd55128b6213dcf1f6844fab80
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:25:23 +0800

    bible_glyph_chapter_verse_word_replace EXO07 14 go. 1 $hand_sending.

--- js/bible_glyph_chapter_exo07.mjs
+++ js/bible_glyph_chapter_exo07.mjs
@@ -386,7 +386,7 @@ export function bible_glyph_chapter_exo07() {
           "let",
           "the",
           "$family",
-          "go.",
+          "$hand_sending.",
         ],
       },
       {
