23e1270043c2bccd0d5ccc5501eedd8ae6dba213
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:05:21 +0800

    bible_glyph_chapter_verse_word_replace MIC03 3 eat 1 $eating

--- js/bible_glyph_chapter_mic03.mjs
+++ js/bible_glyph_chapter_mic03.mjs
@@ -65,7 +65,7 @@ export function bible_glyph_chapter_mic03() {
         verse_number: 3,
         words: [
           "You",
-          "eat",
+          "$eating",
           "the",
           "flesh",
           "of",
