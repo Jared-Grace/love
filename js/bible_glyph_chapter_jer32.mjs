b185b9a2558947c6531f0d7ee7735c05b2e4f23b
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:28:00 +0800

    bible_glyph_chapter_verse_word_replace JER32 18 LORD 1 $i_am

--- js/bible_glyph_chapter_jer32.mjs
+++ js/bible_glyph_chapter_jer32.mjs
@@ -612,7 +612,7 @@ export function bible_glyph_chapter_jer32() {
           "name",
           "is",
           "the",
-          "LORD",
+          "$i_am",
           "of",
           "Hosts,",
         ],
