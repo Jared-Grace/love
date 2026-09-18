9b04e173eb7298e0ef44b7057d90a207a58eeb30
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:49:28 +0800

    bible_glyph_chapter_verse_word_replace 1KI01 35 sit 1 $person+house

--- js/bible_glyph_chapter_1ki01.mjs
+++ js/bible_glyph_chapter_1ki01.mjs
@@ -1103,7 +1103,7 @@ export function bible_glyph_chapter_1ki01() {
           "to",
           "$footprints",
           "and",
-          "sit",
+          "$person+house",
           "on",
           "my",
           "throne",
