11a0eab87076b939c5252b32d7af42c0377fa3de
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:24:11 +0800

    bible_glyph_chapter_verse_word_replace 2CH07 10 away 1 $hand_sending

--- js/bible_glyph_chapter_2ch07.mjs
+++ js/bible_glyph_chapter_2ch07.mjs
@@ -342,7 +342,7 @@ export function bible_glyph_chapter_2ch07() {
           "sent",
           "the",
           "$family",
-          "away",
+          "$hand_sending",
           "to",
           "their",
           "homes,",
