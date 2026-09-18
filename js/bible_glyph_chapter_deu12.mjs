407af35df1bec8f1f6450e8be07adce254c4a669
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:04:38 +0800

    bible_glyph_chapter_verse_word_replace DEU12 16 eat 1 $eating

--- js/bible_glyph_chapter_deu12.mjs
+++ js/bible_glyph_chapter_deu12.mjs
@@ -519,7 +519,7 @@ export function bible_glyph_chapter_deu12() {
           "you",
           "must",
           "$no_entry",
-          "eat",
+          "$eating",
           "the",
           "$blood;",
           "pour",
