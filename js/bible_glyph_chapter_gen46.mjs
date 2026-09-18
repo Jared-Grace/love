51f69084b3e4b229276887755720dcc4f388d0d6
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:56:59 +0800

    bible_glyph_chapter_verse_word_replace GEN46 4 bring 1 $walking+pointing_up

--- js/bible_glyph_chapter_gen46.mjs
+++ js/bible_glyph_chapter_gen46.mjs
@@ -114,7 +114,7 @@ export function bible_glyph_chapter_gen46() {
           "I",
           "will",
           "surely",
-          "bring",
+          "$walking+pointing_up",
           "you",
           "back",
           "up",
