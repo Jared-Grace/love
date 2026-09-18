bbc4d82240fd26848622cc53d9ec89b3945d5351
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:05:38 +0800

    bible_glyph_chapter_verse_word_replace EZK34 29 bear 1 $lifting

--- js/bible_glyph_chapter_ezk34.mjs
+++ js/bible_glyph_chapter_ezk34.mjs
@@ -967,7 +967,7 @@ export function bible_glyph_chapter_ezk34() {
           "$earth",
           "or",
           "$no_entry",
-          "bear",
+          "$lifting",
           "the",
           "scorn",
           "of",
