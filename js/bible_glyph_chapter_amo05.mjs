127ce99e7544fefd6fe758d02816b88cd77183fc
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:05:00 +0800

    bible_glyph_chapter_verse_word_replace AMO05 6 devour 1 $eating

--- js/bible_glyph_chapter_amo05.mjs
+++ js/bible_glyph_chapter_amo05.mjs
@@ -173,7 +173,7 @@ export function bible_glyph_chapter_amo05() {
           "$proper_name$Joseph;",
           "it",
           "will",
-          "devour",
+          "$eating",
           "everything,",
           "with",
           "no",
