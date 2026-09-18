a6c8f1360a2a9b2b8ce5117001ba6605a7a0ad05
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:59:57 +0800

    bible_glyph_chapter_verse_word_replace MIC07 8 sit 1 $person+house

--- js/bible_glyph_chapter_mic07.mjs
+++ js/bible_glyph_chapter_mic07.mjs
@@ -263,7 +263,7 @@ export function bible_glyph_chapter_mic07() {
           "arise;",
           "though",
           "I",
-          "sit",
+          "$person+house",
           "in",
           "$darkness,",
           "the",
