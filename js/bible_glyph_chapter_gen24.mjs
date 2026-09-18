ae64724b27bf65e5685a81781b71f8a9e3ab4cd4
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:24:14 +0800

    bible_glyph_chapter_verse_word_replace GEN24 7 send 1 $hand_sending

--- js/bible_glyph_chapter_gen24.mjs
+++ js/bible_glyph_chapter_gen24.mjs
@@ -219,7 +219,7 @@ export function bible_glyph_chapter_gen24() {
           "$earth,",
           "He",
           "will",
-          "send",
+          "$hand_sending",
           "His",
           "$angel",
           "before",
