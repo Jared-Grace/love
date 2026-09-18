0563a55ac0d25b97e6bd5b2877e92497d7fc08d2
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 19:55:01 +0800

    bible_glyph_chapter_verse_word_replace DEU28 6 go 1 $walking+tray_out

--- js/bible_glyph_chapter_deu28.mjs
+++ js/bible_glyph_chapter_deu28.mjs
@@ -175,7 +175,7 @@ export function bible_glyph_chapter_deu28() {
           "$speech+thumbs_up",
           "when",
           "you",
-          "go",
+          "$walking+tray_out",
           "out.",
         ],
       },
