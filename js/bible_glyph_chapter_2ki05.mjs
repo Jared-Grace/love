afb9bd0e4506fcea670459902c6501a6b3e11d6d
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 20:25:54 +0800

    bible_glyph_chapter_verse_word_replace 2KI05 7 expects 1 $hand_sending

--- js/bible_glyph_chapter_2ki05.mjs
+++ js/bible_glyph_chapter_2ki05.mjs
@@ -252,7 +252,7 @@ export function bible_glyph_chapter_2ki05() {
           "that",
           "this",
           "man",
-          "expects",
+          "$hand_sending",
           "me",
           "to",
           "cure",
