35a170cd9d185b1040c40c1f55ec756705683ba8
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 15:29:19 +0800

    bible_glyph_chapter_verse_word_replace ZEC05 4 name. 1 $name_tag.

--- js/bible_glyph_chapter_zec05.mjs
+++ js/bible_glyph_chapter_zec05.mjs
@@ -136,7 +136,7 @@ export function bible_glyph_chapter_zec05() {
           "falsely",
           "by",
           "My",
-          "name.",
+          "$name_tag.",
           "It",
           "will",
           "remain",
