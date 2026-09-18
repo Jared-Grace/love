19feac21a8542bc663477eea54e4fecb8a5bb79c
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:04:32 +0800

    bible_glyph_chapter_verse_word_replace DEU31 17 consumed. 1 $eating.

--- js/bible_glyph_chapter_deu31.mjs
+++ js/bible_glyph_chapter_deu31.mjs
@@ -614,7 +614,7 @@ export function bible_glyph_chapter_deu31() {
           "they",
           "will",
           "be",
-          "consumed.",
+          "$eating.",
           "And",
           "many",
           "$thumbs_down",
