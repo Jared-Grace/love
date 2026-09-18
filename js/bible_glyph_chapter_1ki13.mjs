f61e734a82d4bcf7a1bac976381995c4bdbaa7d1
JESUS rose to life <username@users.noreply.github.com>
2026-09-18 21:02:34 +0800

    bible_glyph_chapter_verse_word_replace 1KI13 19 ate 1 $eating

--- js/bible_glyph_chapter_1ki13.mjs
+++ js/bible_glyph_chapter_1ki13.mjs
@@ -701,7 +701,7 @@ export function bible_glyph_chapter_1ki13() {
           "with",
           "him,",
           "and",
-          "ate",
+          "$eating",
           "$bread",
           "in",
           "his",
