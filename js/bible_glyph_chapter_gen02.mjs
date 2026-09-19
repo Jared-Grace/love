1bfa0e47a9be98cc4a8bac2b6f38ad179203a60a
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:05:45 +0800

    bible_glyph_chapter_verse_word_replace GEN02 19 field 1 $field

--- js/bible_glyph_chapter_gen02.mjs
+++ js/bible_glyph_chapter_gen02.mjs
@@ -540,7 +540,7 @@ export function bible_glyph_chapter_gen02() {
           "$sprout",
           "of",
           "the",
-          "field",
+          "$field",
           "and",
           "every",
           "$bird",
