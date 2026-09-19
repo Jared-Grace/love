bd86ab4c543d462fa54cabcc405efb84c2e6dbfc
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:59:02 +0800

    bible_glyph_chapter_verse_word_replace GEN29 10 mother 1 $mother

--- js/bible_glyph_chapter_gen29.mjs
+++ js/bible_glyph_chapter_gen29.mjs
@@ -286,7 +286,7 @@ export function bible_glyph_chapter_gen29() {
           "of",
           "$proper_name$Laban,",
           "his",
-          "mother",
+          "$mother",
           "$brother,",
           "and",
           "the",
