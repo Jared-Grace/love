737f81636deb28a31fe55f7f95e8a3879f0029fc
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:53:28 +0800

    bible_glyph_chapter_verse_word_replace JUD01 21 yourselves 1 $pointing_back+pointing_back

--- js/bible_glyph_chapter_jud01.mjs
+++ js/bible_glyph_chapter_jud01.mjs
@@ -612,7 +612,7 @@ export function bible_glyph_chapter_jud01() {
         verse_number: 21,
         words: [
           "keep",
-          "yourselves",
+          "$pointing_back+pointing_back",
           "$tray_in",
           "the",
           "$heart_red",
