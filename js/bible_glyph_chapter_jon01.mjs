c3950edeaf2aa37cfe3711a6f39519ded7b6445f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:59:57 +0800

    bible_glyph_chapter_verse_word_replace JON01 4 ship 1 $ship

--- js/bible_glyph_chapter_jon01.mjs
+++ js/bible_glyph_chapter_jon01.mjs
@@ -126,7 +126,7 @@ export function bible_glyph_chapter_jon01() {
           "arose",
           "that",
           "the",
-          "ship",
+          "$ship",
           "was",
           "in",
           "danger",
