e9b0bbb507ea5e24b379998d135ddb47a192ed6a
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:57:50 +0800

    bible_glyph_chapter_verse_word_replace EZK34 25 peace 1 $peace

--- js/bible_glyph_chapter_ezk34.mjs
+++ js/bible_glyph_chapter_ezk34.mjs
@@ -793,7 +793,7 @@ export function bible_glyph_chapter_ezk34() {
           "a",
           "covenant",
           "of",
-          "peace",
+          "$peace",
           "with",
           "them",
           "and",
