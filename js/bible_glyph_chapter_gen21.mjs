711f84646ca05a9ccea7d8c415b838faa15eb582
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:12 +0800

    bible_glyph_chapter_verse_word_replace GEN21 21 Wilderness 1 $wilderness

--- js/bible_glyph_chapter_gen21.mjs
+++ js/bible_glyph_chapter_gen21.mjs
@@ -563,7 +563,7 @@ export function bible_glyph_chapter_gen21() {
           "dwelling",
           "in",
           "the",
-          "Wilderness",
+          "$wilderness",
           "of",
           "$proper_name$Paran,",
           "his",
