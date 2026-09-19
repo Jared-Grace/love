75f8ea7748dbc9f6e293037e6ff98057f8c0c607
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:56:21 +0800

    bible_glyph_chapter_verse_word_replace 1KI08 7 place 1 $place

--- js/bible_glyph_chapter_1ki08.mjs
+++ js/bible_glyph_chapter_1ki08.mjs
@@ -224,7 +224,7 @@ export function bible_glyph_chapter_1ki08() {
           "wings",
           "over",
           "the",
-          "place",
+          "$place",
           "of",
           "the",
           "ark",
