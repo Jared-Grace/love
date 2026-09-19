94b866fe573334353e773ef71b4fa4ce38d6e401
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:09 +0800

    bible_glyph_chapter_verse_word_replace NUM14 6 clothes 1 $garment

--- js/bible_glyph_chapter_num14.mjs
+++ js/bible_glyph_chapter_num14.mjs
@@ -181,7 +181,7 @@ export function bible_glyph_chapter_num14() {
           "$earth,",
           "tore",
           "their",
-          "clothes",
+          "$garment",
         ],
       },
       {
