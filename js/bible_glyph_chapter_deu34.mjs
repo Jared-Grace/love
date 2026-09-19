c9cebf57cccc7f18c6cdd0ab9f7332a2b8205563
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:03:16 +0800

    bible_glyph_chapter_verse_word_replace DEU34 9 wisdom, 1 $wisdom,

--- js/bible_glyph_chapter_deu34.mjs
+++ js/bible_glyph_chapter_deu34.mjs
@@ -279,7 +279,7 @@ export function bible_glyph_chapter_deu34() {
           "the",
           "$wind",
           "of",
-          "wisdom,",
+          "$wisdom,",
           "because",
           "$proper_name$Moses",
           "had",
