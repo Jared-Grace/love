1c64ea78f0c98d8731a5b42417cdbfc5efa3f7f9
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:06 +0800

    bible_glyph_chapter_verse_word_replace GEN15 18 covenant 1 $covenant

--- js/bible_glyph_chapter_gen15.mjs
+++ js/bible_glyph_chapter_gen15.mjs
@@ -483,7 +483,7 @@ export function bible_glyph_chapter_gen15() {
           "$i_am",
           "made",
           "a",
-          "covenant",
+          "$covenant",
           "with",
           "$proper_name$Abram,",
           "$speech,",
