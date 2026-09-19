6d4ba91e402201e7732474c4238f357381f2e51b
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:58:44 +0800

    bible_glyph_chapter_verse_word_replace MRK10 30 mothers 1 $mother

--- js/bible_glyph_chapter_mrk10.mjs
+++ js/bible_glyph_chapter_mrk10.mjs
@@ -756,7 +756,7 @@ export function bible_glyph_chapter_mrk10() {
         "$plus",
         "sisters",
         "$plus",
-        "mothers",
+        "$mother",
         "$plus",
         "$child",
         "$plus",
