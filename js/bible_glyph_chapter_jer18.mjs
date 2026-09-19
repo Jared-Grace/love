8fd5a9c3f3d27e3dec67c58d4f003f3c7e57bb6e
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:26 +0800

    bible_glyph_chapter_verse_word_replace JER18 17 enemy. 1 $enemy.

--- js/bible_glyph_chapter_jer18.mjs
+++ js/bible_glyph_chapter_jer18.mjs
@@ -492,7 +492,7 @@ export function bible_glyph_chapter_jer18() {
           "them",
           "before",
           "the",
-          "enemy.",
+          "$enemy.",
           "I",
           "will",
           "show",
