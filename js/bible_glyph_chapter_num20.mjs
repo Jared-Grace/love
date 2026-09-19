330282970e53c172408eb61fc949f6d12779af57
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:56:03 +0800

    bible_glyph_chapter_verse_word_replace NUM20 5 place? 1 $place?

--- js/bible_glyph_chapter_num20.mjs
+++ js/bible_glyph_chapter_num20.mjs
@@ -128,7 +128,7 @@ export function bible_glyph_chapter_num20() {
           "to",
           "this",
           "$thumbs_down",
-          "place?",
+          "$place?",
           "It",
           "is",
           "$no_entry",
