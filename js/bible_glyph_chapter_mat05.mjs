be37577282268b9f1cf8a1d218d59173dc9a0d12
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:53:55 +0800

    bible_glyph_chapter_verse_word_replace MAT05 21 said 1 $speech

--- js/bible_glyph_chapter_mat05.mjs
+++ js/bible_glyph_chapter_mat05.mjs
@@ -515,7 +515,7 @@ export function bible_glyph_chapter_mat05() {
           "$pointing",
           "it",
           "was",
-          "said",
+          "$speech",
           "to",
           "the",
           "ancients,",
