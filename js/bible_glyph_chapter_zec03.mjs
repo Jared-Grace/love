d073a188a6cb1a6043b54b2e137b77a484966103
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:06 +0800

    bible_glyph_chapter_verse_word_replace ZEC03 4 clothes! 1 $garment!

--- js/bible_glyph_chapter_zec03.mjs
+++ js/bible_glyph_chapter_zec03.mjs
@@ -110,7 +110,7 @@ export function bible_glyph_chapter_zec03() {
           "off",
           "his",
           "filthy",
-          "clothes!",
+          "$garment!",
           "Then",
           "he",
           "$speech",
