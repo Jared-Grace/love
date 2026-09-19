a617139d566cd2fab7ce0ba92df826ca69f0c93f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:07:55 +0800

    bible_glyph_chapter_verse_word_replace 2KI17 35 covenant 1 $covenant

--- js/bible_glyph_chapter_2ki17.mjs
+++ js/bible_glyph_chapter_2ki17.mjs
@@ -1223,7 +1223,7 @@ export function bible_glyph_chapter_2ki17() {
           "had",
           "made",
           "a",
-          "covenant",
+          "$covenant",
           "with",
           "them",
           "and",
