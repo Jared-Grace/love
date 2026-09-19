d0529068f6b5e704eaf31896118f6100ca0d0372
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:14 +0800

    bible_glyph_chapter_verse_word_replace 2KI05 8 clothes, 1 $garment,

--- js/bible_glyph_chapter_2ki05.mjs
+++ js/bible_glyph_chapter_2ki05.mjs
@@ -293,7 +293,7 @@ export function bible_glyph_chapter_2ki05() {
           "had",
           "torn",
           "his",
-          "clothes,",
+          "$garment,",
           "he",
           "sent",
           "a",
