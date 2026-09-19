dc76f78e6138ef5e755c8a1d1b20a75291c3e0b1
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:25 +0800

    bible_glyph_chapter_verse_word_replace EXO34 1 stone 1 $stone

--- js/bible_glyph_chapter_exo34.mjs
+++ js/bible_glyph_chapter_exo34.mjs
@@ -24,7 +24,7 @@ export function bible_glyph_chapter_exo34() {
           "two",
           "tablets",
           "of",
-          "stone",
+          "$stone",
           "like",
           "the",
           "originals,",
