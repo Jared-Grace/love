6a54ca0e40dc67285f89f4b13e32b1979e9daaa6
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 12:03:13 +0800

    bible_glyph_chapter_verse_word_replace GEN07 16 Noah. 1 $pointing_down.

--- js/bible_glyph_chapter_gen07.mjs
+++ js/bible_glyph_chapter_gen07.mjs
@@ -432,7 +432,7 @@ export function bible_glyph_chapter_gen07() {
           "$fire",
           "had",
           "commanded",
-          "Noah.",
+          "$pointing_down.",
           "Then",
           "the",
           "$i_am",
