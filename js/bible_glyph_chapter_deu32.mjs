06c15beede2c997b7d2a9a77bb8fa55682973026
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:07 +0800

    bible_glyph_chapter_verse_word_replace DEU32 51 Wilderness 1 $wilderness

--- js/bible_glyph_chapter_deu32.mjs
+++ js/bible_glyph_chapter_deu32.mjs
@@ -1453,7 +1453,7 @@ export function bible_glyph_chapter_deu32() {
           "kadesh",
           "in",
           "the",
-          "Wilderness",
+          "$wilderness",
           "of",
           "$proper_name$Zin,",
           "by",
