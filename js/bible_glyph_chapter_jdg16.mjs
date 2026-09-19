8eab2c207dfed3c04177a3025d56d7324b27a81f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:23 +0800

    bible_glyph_chapter_verse_word_replace JDG16 24 enemy, 1 $enemy,

--- js/bible_glyph_chapter_jdg16.mjs
+++ js/bible_glyph_chapter_jdg16.mjs
@@ -940,7 +940,7 @@ export function bible_glyph_chapter_jdg16() {
           "our",
           "$hand",
           "our",
-          "enemy,",
+          "$enemy,",
           "who",
           "destroyed",
           "our",
