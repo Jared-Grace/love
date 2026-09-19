302060c92cbc69a1cd407433ed348517c8634418
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:53:58 +0800

    bible_glyph_chapter_verse_word_replace MAT05 43 said, 1 $speech,

--- js/bible_glyph_chapter_mat05.mjs
+++ js/bible_glyph_chapter_mat05.mjs
@@ -1118,7 +1118,7 @@ export function bible_glyph_chapter_mat05() {
           "$pointing",
           "it",
           "was",
-          "said,",
+          "$speech,",
           "$heart_red",
           "your",
           "neighbor",
