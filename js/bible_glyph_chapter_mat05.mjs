ae2a9df12e3d322e981b75db88288203f222bba7
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:54:20 +0800

    bible_glyph_chapter_verse_word_replace MAT05 15 house. 1 $house.

--- js/bible_glyph_chapter_mat05.mjs
+++ js/bible_glyph_chapter_mat05.mjs
@@ -340,7 +340,7 @@ export function bible_glyph_chapter_mat05() {
           "everyone",
           "$tray_in",
           "the",
-          "house.",
+          "$house.",
         ],
       },
       {
