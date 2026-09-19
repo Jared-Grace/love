7ad77c25315da553647eb00a77d7e702ffd4066b
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:58:49 +0800

    bible_glyph_chapter_verse_word_replace JHN02 5 mother 1 $mother

--- js/bible_glyph_chapter_jhn02.mjs
+++ js/bible_glyph_chapter_jhn02.mjs
@@ -106,7 +106,7 @@ export function bible_glyph_chapter_jhn02() {
         verse_number: 5,
         words: [
           "$pointing_back",
-          "mother",
+          "$mother",
           "$speech",
           "to",
           "the",
