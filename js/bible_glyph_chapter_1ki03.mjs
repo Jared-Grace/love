fb49ffbd5ca9eb0f121006b9fa9fdd2b68dea5c1
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:01:09 +0800

    bible_glyph_chapter_verse_word_replace 1KI03 19 night 1 $night

--- js/bible_glyph_chapter_1ki03.mjs
+++ js/bible_glyph_chapter_1ki03.mjs
@@ -659,7 +659,7 @@ export function bible_glyph_chapter_1ki03() {
         words: [
           "During",
           "the",
-          "night",
+          "$night",
           "the",
           "$son",
           "of",
