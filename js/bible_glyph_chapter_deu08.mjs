0213a5c653178609a2418dcd5e83e1a204e9c450
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:00 +0800

    bible_glyph_chapter_verse_word_replace DEU08 15 wilderness, 1 $wilderness,

--- js/bible_glyph_chapter_deu08.mjs
+++ js/bible_glyph_chapter_deu08.mjs
@@ -456,7 +456,7 @@ export function bible_glyph_chapter_deu08() {
           "vast",
           "and",
           "terrifying",
-          "wilderness,",
+          "$wilderness,",
           "with",
           "its",
           "venomous",
