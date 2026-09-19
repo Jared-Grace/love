6c5e6000649064f359c0b4f2bf6293f292f6d3a2
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:03:17 +0800

    bible_glyph_chapter_verse_word_replace PRO02 2 wisdom 1 $wisdom

--- js/bible_glyph_chapter_pro02.mjs
+++ js/bible_glyph_chapter_pro02.mjs
@@ -39,7 +39,7 @@ export function bible_glyph_chapter_pro02() {
           "your",
           "ear",
           "to",
-          "wisdom",
+          "$wisdom",
           "and",
           "direct",
           "your",
