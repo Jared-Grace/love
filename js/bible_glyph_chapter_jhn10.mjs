4bae0d2555f256adfba1cf2d728c37f65e69477e
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:19 +0800

    bible_glyph_chapter_verse_word_replace JHN10 31 stones 1 $stone

--- js/bible_glyph_chapter_jhn10.mjs
+++ js/bible_glyph_chapter_jhn10.mjs
@@ -763,7 +763,7 @@ export function bible_glyph_chapter_jhn10() {
           "$menorah",
           "picked",
           "up",
-          "stones",
+          "$stone",
           "again",
           "to",
           "stone",
