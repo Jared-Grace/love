4ba26e1a6d1029a0cd0e566b525f8cdb70eb3fd9
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:31 +0800

    bible_glyph_chapter_verse_word_replace 2SA12 30 stones, 1 $stone,

--- js/bible_glyph_chapter_2sa12.mjs
+++ js/bible_glyph_chapter_2sa12.mjs
@@ -993,7 +993,7 @@ export function bible_glyph_chapter_2sa12() {
           "set",
           "with",
           "precious",
-          "stones,",
+          "$stone,",
           "and",
           "it",
           "was",
