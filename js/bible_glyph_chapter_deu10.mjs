274fbe6fc40d06b10f72931b86d0a77b1d4d2699
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:39 +0800

    bible_glyph_chapter_verse_word_replace DEU10 3 stone 1 $stone

--- js/bible_glyph_chapter_deu10.mjs
+++ js/bible_glyph_chapter_deu10.mjs
@@ -100,7 +100,7 @@ export function bible_glyph_chapter_deu10() {
           "two",
           "tablets",
           "of",
-          "stone",
+          "$stone",
           "like",
           "the",
           "originals,",
