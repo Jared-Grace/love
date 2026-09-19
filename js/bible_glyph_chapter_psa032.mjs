08354c52a576cf0baae26a257beb2e5330305201
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:01:52 +0800

    bible_glyph_chapter_verse_word_replace PSA032 4 night 1 $night

--- js/bible_glyph_chapter_psa032.mjs
+++ js/bible_glyph_chapter_psa032.mjs
@@ -80,7 +80,7 @@ export function bible_glyph_chapter_psa032() {
           "For",
           "day",
           "and",
-          "night",
+          "$night",
           "Your",
           "$hand",
           "was",
