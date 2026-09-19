4b008d893c094b23119ba23c13107ad0abcf4d0b
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:57:07 +0800

    bible_glyph_chapter_verse_word_replace LUK07 34 eating 1 $eating

--- js/bible_glyph_chapter_luk07.mjs
+++ js/bible_glyph_chapter_luk07.mjs
@@ -985,7 +985,7 @@ export function bible_glyph_chapter_luk07() {
         "of",
         "$person",
         "$footprints",
-        "eating",
+        "$eating",
         "$plus",
         "drinking,",
         "$plus",
