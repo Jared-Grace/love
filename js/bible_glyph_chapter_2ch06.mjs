27507bb2a1b02595541ae4f32d9badfb93f6c460
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:43 +0800

    bible_glyph_chapter_verse_word_replace 2CH06 24 enemy 1 $enemy

--- js/bible_glyph_chapter_2ch06.mjs
+++ js/bible_glyph_chapter_2ch06.mjs
@@ -831,7 +831,7 @@ export function bible_glyph_chapter_2ch06() {
           "defeated",
           "before",
           "an",
-          "enemy",
+          "$enemy",
           "because",
           "they",
           "have",
