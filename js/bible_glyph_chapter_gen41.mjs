86d3e904883f3517cec2130f1701e5ab40fa247c
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:01:26 +0800

    bible_glyph_chapter_verse_word_replace GEN41 11 night 1 $night

--- js/bible_glyph_chapter_gen41.mjs
+++ js/bible_glyph_chapter_gen41.mjs
@@ -280,7 +280,7 @@ export function bible_glyph_chapter_gen41() {
         verse_number: 11,
         words: [
           "One",
-          "night",
+          "$night",
           "both",
           "he",
           "and",
