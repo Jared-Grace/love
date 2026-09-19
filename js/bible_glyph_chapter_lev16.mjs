76a693685b8e20b613117e56174a7cfd6ea05545
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:04 +0800

    bible_glyph_chapter_verse_word_replace LEV16 28 clothes 1 $garment

--- js/bible_glyph_chapter_lev16.mjs
+++ js/bible_glyph_chapter_lev16.mjs
@@ -1032,7 +1032,7 @@ export function bible_glyph_chapter_lev16() {
           "must",
           "wash",
           "his",
-          "clothes",
+          "$garment",
           "and",
           "bathe",
           "his",
