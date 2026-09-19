c85d5e1b54c2fb75bed6cd4ca9d0e208feb77005
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:56:20 +0800

    bible_glyph_chapter_verse_word_replace GEN24 31 place 1 $place

--- js/bible_glyph_chapter_gen24.mjs
+++ js/bible_glyph_chapter_gen24.mjs
@@ -951,7 +951,7 @@ export function bible_glyph_chapter_gen24() {
           "$house",
           "and",
           "a",
-          "place",
+          "$place",
           "for",
           "the",
           "camels.",
