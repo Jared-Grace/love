85427bbbd18a0862b6a0a2c4721f7d1dd8faaf74
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:00:49 +0800

    bible_glyph_chapter_verse_word_replace 1KI01 24 throne? 1 $throne?

--- js/bible_glyph_chapter_1ki01.mjs
+++ js/bible_glyph_chapter_1ki01.mjs
@@ -756,7 +756,7 @@ export function bible_glyph_chapter_1ki01() {
           "$person+house",
           "on",
           "my",
-          "throne?",
+          "$throne?",
         ],
       },
       {
