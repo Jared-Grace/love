56bf14b98fa9b8814d7855a10bd3a2cfad06063b
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:03:51 +0800

    bible_glyph_chapter_verse_word_replace EXO16 3 desert 1 $wilderness

--- js/bible_glyph_chapter_exo16.mjs
+++ js/bible_glyph_chapter_exo16.mjs
@@ -112,7 +112,7 @@ export function bible_glyph_chapter_exo16() {
           "us",
           "into",
           "this",
-          "desert",
+          "$wilderness",
           "to",
           "starve",
           "this",
