f69a3e3a0c2c9919ee5b5cbde3c7b9736c5aa3cc
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 12:02:42 +0800

    bible_glyph_chapter_verse_word_replace 1JN02 27 from 1 $away

--- js/bible_glyph_chapter_1jn02.mjs
+++ js/bible_glyph_chapter_1jn02.mjs
@@ -781,7 +781,7 @@ export function bible_glyph_chapter_1jn02() {
           "anointing",
           "you",
           "$hand_receiving",
-          "from",
+          "$away",
           "$pointing_back",
           "remains",
           "$tray_in",
