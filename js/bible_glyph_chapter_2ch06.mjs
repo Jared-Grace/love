baa39d143b87e500d048e72fa2ba991481fe9455
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:01 +0800

    bible_glyph_chapter_verse_word_replace 2CH06 14 covenant 1 $covenant

--- js/bible_glyph_chapter_2ch06.mjs
+++ js/bible_glyph_chapter_2ch06.mjs
@@ -465,7 +465,7 @@ export function bible_glyph_chapter_2ch06() {
           "$earth,",
           "keeping",
           "Your",
-          "covenant",
+          "$covenant",
           "of",
           "$gift",
           "with",
