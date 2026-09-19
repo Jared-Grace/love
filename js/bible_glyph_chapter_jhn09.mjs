68748963c9e77a7a997c9516c38e33c03b20e88b
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:54:24 +0800

    bible_glyph_chapter_verse_word_replace JHN09 9 No, 1 $no_entry,

--- js/bible_glyph_chapter_jhn09.mjs
+++ js/bible_glyph_chapter_jhn09.mjs
@@ -222,7 +222,7 @@ export function bible_glyph_chapter_jhn09() {
           "he.",
           "Others",
           "$speech,",
-          "No,",
+          "$no_entry,",
           "but",
           "he",
           "looks",
