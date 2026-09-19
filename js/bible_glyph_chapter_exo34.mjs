1199efe04f7d567000db018b022b0a2d00803054
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:07:19 +0800

    bible_glyph_chapter_verse_word_replace EXO34 15 covenant 1 $covenant

--- js/bible_glyph_chapter_exo34.mjs
+++ js/bible_glyph_chapter_exo34.mjs
@@ -473,7 +473,7 @@ export function bible_glyph_chapter_exo34() {
           "not",
           "make",
           "a",
-          "covenant",
+          "$covenant",
           "with",
           "the",
           "inhabitants",
