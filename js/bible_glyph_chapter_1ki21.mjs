a9a60aed8b8ea564ae56c7c5b6caabee602dfd68
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:06:11 +0800

    bible_glyph_chapter_verse_word_replace 1KI21 24 field 1 $field

--- js/bible_glyph_chapter_1ki21.mjs
+++ js/bible_glyph_chapter_1ki21.mjs
@@ -849,7 +849,7 @@ export function bible_glyph_chapter_1ki21() {
           "$skull",
           "in",
           "the",
-          "field",
+          "$field",
           "will",
           "be",
           "eaten",
