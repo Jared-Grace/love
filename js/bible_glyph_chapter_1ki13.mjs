5cec4c7267c1fa0f739b982a3283af1540b8b642
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:05:30 +0800

    bible_glyph_chapter_verse_word_replace 1KI13 22 tomb 1 $tomb

--- js/bible_glyph_chapter_1ki13.mjs
+++ js/bible_glyph_chapter_1ki13.mjs
@@ -817,7 +817,7 @@ export function bible_glyph_chapter_1ki13() {
           "$footprints",
           "to",
           "the",
-          "tomb",
+          "$tomb",
           "of",
           "your",
           "$father.",
