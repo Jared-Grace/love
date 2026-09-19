2511b01583c314070a31ab50779fc3f121e37dc0
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:58:12 +0800

    bible_glyph_chapter_verse_word_replace 2KI05 19 peace, 1 $peace,

--- js/bible_glyph_chapter_2ki05.mjs
+++ js/bible_glyph_chapter_2ki05.mjs
@@ -732,7 +732,7 @@ export function bible_glyph_chapter_2ki05() {
         words: [
           "$walking",
           "in",
-          "peace,",
+          "$peace,",
           "$speech",
           "Elisha.",
           "But",
