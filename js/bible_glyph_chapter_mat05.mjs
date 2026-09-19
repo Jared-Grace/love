9d0905111a582ba91abea3dd7707be46838a551e
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 12:02:50 +0800

    bible_glyph_chapter_verse_word_replace MAT05 30 away. 1 $away.

--- js/bible_glyph_chapter_mat05.mjs
+++ js/bible_glyph_chapter_mat05.mjs
@@ -796,7 +796,7 @@ export function bible_glyph_chapter_mat05() {
           "$plus",
           "throw",
           "it",
-          "away.",
+          "$away.",
           "It",
           "is",
           "better",
