6e1e2400cc6d7ff6e0502d5ba035bcd9fe87eecd
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:01:47 +0800

    bible_glyph_chapter_verse_word_replace JOS01 8 night, 1 $night,

--- js/bible_glyph_chapter_jos01.mjs
+++ js/bible_glyph_chapter_jos01.mjs
@@ -259,7 +259,7 @@ export function bible_glyph_chapter_jos01() {
           "it",
           "day",
           "and",
-          "night,",
+          "$night,",
           "so",
           "that",
           "you",
