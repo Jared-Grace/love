d52a543e08b785bda64bd6e3be3cbd9483c1cd70
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:57:05 +0800

    bible_glyph_chapter_verse_word_replace JHN04 32 eat 1 $eating

--- js/bible_glyph_chapter_jhn04.mjs
+++ js/bible_glyph_chapter_jhn04.mjs
@@ -771,7 +771,7 @@ export function bible_glyph_chapter_jhn04() {
           "have",
           "food",
           "to",
-          "eat",
+          "$eating",
           "that",
           "you",
           "$no_entry",
