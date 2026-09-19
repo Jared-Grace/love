802755d609bd5c2cfc7ac1f3b8381f68ade0227c
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:54:15 +0800

    bible_glyph_chapter_verse_word_replace MRK10 10 house, 1 $house,

--- js/bible_glyph_chapter_mrk10.mjs
+++ js/bible_glyph_chapter_mrk10.mjs
@@ -207,7 +207,7 @@ export function bible_glyph_chapter_mrk10() {
         "back",
         "inside",
         "the",
-        "house,",
+        "$house,",
         "the",
         "$learner",
         "asked",
