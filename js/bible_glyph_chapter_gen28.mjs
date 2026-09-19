416439223dbeaca09993405bc3541d09f66c5a7f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:56:54 +0800

    bible_glyph_chapter_verse_word_replace GEN28 16 place, 1 $place,

--- js/bible_glyph_chapter_gen28.mjs
+++ js/bible_glyph_chapter_gen28.mjs
@@ -508,7 +508,7 @@ export function bible_glyph_chapter_gen28() {
           "is",
           "in",
           "this",
-          "place,",
+          "$place,",
           "and",
           "I",
           "did",
