704d3adc8903f3e0b62cc41c5eddf05bf475e010
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:47 +0800

    bible_glyph_chapter_verse_word_replace GEN01 29 seed; 1 $seed;

--- js/bible_glyph_chapter_gen01.mjs
+++ js/bible_glyph_chapter_gen01.mjs
@@ -868,7 +868,7 @@ export function bible_glyph_chapter_gen01() {
           "with",
           "$fruit",
           "yielding",
-          "seed;",
+          "$seed;",
           "it",
           "shall",
           "be",
