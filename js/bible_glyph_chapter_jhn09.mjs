6f2e7fb7d872c8a6bdfa2141ff088ff5b41cae50
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:59:32 +0800

    bible_glyph_chapter_verse_word_replace JHN09 16 Sabbath. 1 $sabbath.

--- js/bible_glyph_chapter_jhn09.mjs
+++ js/bible_glyph_chapter_jhn09.mjs
@@ -403,7 +403,7 @@ export function bible_glyph_chapter_jhn09() {
           "$no_entry",
           "keep",
           "the",
-          "Sabbath.",
+          "$sabbath.",
           "But",
           "others",
           "$speech,",
