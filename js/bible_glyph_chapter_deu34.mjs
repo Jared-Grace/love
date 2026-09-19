3807eac9be7e1a9a2f020e7451acfea8609e3fc0
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:05:03 +0800

    bible_glyph_chapter_verse_word_replace DEU34 4 descendants.' 1 $seed.'

--- js/bible_glyph_chapter_deu34.mjs
+++ js/bible_glyph_chapter_deu34.mjs
@@ -130,7 +130,7 @@ export function bible_glyph_chapter_deu34() {
           "it",
           "to",
           "your",
-          "descendants.'",
+          "$seed.'",
           "I",
           "have",
           "let",
