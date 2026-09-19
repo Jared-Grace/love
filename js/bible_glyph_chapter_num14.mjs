b5385bbb1ba1a5bc428e50119805824ef97a7889
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:19 +0800

    bible_glyph_chapter_verse_word_replace NUM14 32 wilderness. 1 $wilderness.

--- js/bible_glyph_chapter_num14.mjs
+++ js/bible_glyph_chapter_num14.mjs
@@ -993,7 +993,7 @@ export function bible_glyph_chapter_num14() {
           "fall",
           "in",
           "this",
-          "wilderness.",
+          "$wilderness.",
         ],
       },
       {
