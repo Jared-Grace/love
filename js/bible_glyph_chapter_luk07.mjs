31e7e0fdc71939f66b61411e12f40381aa5b955a
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:03:46 +0800

    bible_glyph_chapter_verse_word_replace LUK07 24 wilderness 1 $wilderness

--- js/bible_glyph_chapter_luk07.mjs
+++ js/bible_glyph_chapter_luk07.mjs
@@ -704,7 +704,7 @@ export function bible_glyph_chapter_luk07() {
         "out",
         "into",
         "the",
-        "wilderness",
+        "$wilderness",
         "to",
         "see?",
         "A",
