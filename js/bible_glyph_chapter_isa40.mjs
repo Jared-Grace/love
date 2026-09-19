7daea650bc55a2614e7a0b220d129c84462e0741
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:34 +0800

    bible_glyph_chapter_verse_word_replace ISA40 3 wilderness, 1 $wilderness,

--- js/bible_glyph_chapter_isa40.mjs
+++ js/bible_glyph_chapter_isa40.mjs
@@ -73,7 +73,7 @@ export function bible_glyph_chapter_isa40() {
           "calling",
           "in",
           "the",
-          "wilderness,",
+          "$wilderness,",
           "Prepare",
           "the",
           "$road",
