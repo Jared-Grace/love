dbb6720cd497200d09694067543bd6efdb4bd7fc
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:59:09 +0800

    bible_glyph_chapter_verse_word_replace GEN27 13 mother 1 $mother

--- js/bible_glyph_chapter_gen27.mjs
+++ js/bible_glyph_chapter_gen27.mjs
@@ -344,7 +344,7 @@ export function bible_glyph_chapter_gen27() {
         verse_number: 13,
         words: [
           "His",
-          "mother",
+          "$mother",
           "$speech,",
           '"Your',
           "curse",
