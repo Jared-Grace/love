efc123ca7cfd505d35dcbd295fe7007738616d80
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:56:07 +0800

    bible_glyph_chapter_verse_word_replace JER17 12 place 1 $place

--- js/bible_glyph_chapter_jer17.mjs
+++ js/bible_glyph_chapter_jer17.mjs
@@ -389,7 +389,7 @@ export function bible_glyph_chapter_jer17() {
           "beginning,",
           "is",
           "the",
-          "place",
+          "$place",
           "of",
           "our",
           "sanctuary.",
