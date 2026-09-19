1f3f54041a666f446ea5bd84e277177b9b79a5e0
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:57:47 +0800

    bible_glyph_chapter_verse_word_replace 2TH01 2 peace 1 $peace

--- js/bible_glyph_chapter_2th01.mjs
+++ js/bible_glyph_chapter_2th01.mjs
@@ -45,7 +45,7 @@ export function bible_glyph_chapter_2th01() {
           "to",
           "you",
           "$plus",
-          "peace",
+          "$peace",
           "from",
           "$fire",
           "the",
