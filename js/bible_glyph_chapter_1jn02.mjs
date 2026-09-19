156c3d42514e77e0b257e2e2eb7874500ffa4ee4
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:51 +0800

    bible_glyph_chapter_verse_word_replace 1JN02 18 Children, 1 $baby,

--- js/bible_glyph_chapter_1jn02.mjs
+++ js/bible_glyph_chapter_1jn02.mjs
@@ -519,7 +519,7 @@ export function bible_glyph_chapter_1jn02() {
       {
         verse_number: 18,
         words: [
-          "Children,",
+          "$baby,",
           "it",
           "is",
           "the",
