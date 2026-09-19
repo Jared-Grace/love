9eedeb54b7bf6a41e586975c9989455d1c3f3b89
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:30 +0800

    bible_glyph_chapter_verse_word_replace EXO03 1 wilderness 1 $wilderness

--- js/bible_glyph_chapter_exo03.mjs
+++ js/bible_glyph_chapter_exo03.mjs
@@ -38,7 +38,7 @@ export function bible_glyph_chapter_exo03() {
           "side",
           "of",
           "the",
-          "wilderness",
+          "$wilderness",
           "and",
           "$footprints",
           "to",
