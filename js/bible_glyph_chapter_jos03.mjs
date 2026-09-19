06df90f952886e23e32c1ae68ec6caf18df80ce9
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:07:51 +0800

    bible_glyph_chapter_verse_word_replace JOS03 8 covenant, 1 $covenant,

--- js/bible_glyph_chapter_jos03.mjs
+++ js/bible_glyph_chapter_jos03.mjs
@@ -256,7 +256,7 @@ export function bible_glyph_chapter_jos03() {
           "ark",
           "of",
           "the",
-          "covenant,",
+          "$covenant,",
           "$speech,",
           "When",
           "you",
