9bc8938dcbb6daaf654c726bcff8af7516d86a7d
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:02:32 +0800

    bible_glyph_chapter_verse_word_replace GEN29 2 stone 1 $stone

--- js/bible_glyph_chapter_gen29.mjs
+++ js/bible_glyph_chapter_gen29.mjs
@@ -63,7 +63,7 @@ export function bible_glyph_chapter_gen29() {
           "And",
           "a",
           "large",
-          "stone",
+          "$stone",
           "covered",
           "the",
           "mouth",
