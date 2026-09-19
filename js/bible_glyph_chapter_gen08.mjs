b7b4e142b9aec3778ff0ffac5b87123185e3eb0b
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:52 +0800

    bible_glyph_chapter_verse_word_replace GEN08 22 seedtime 1 $seed

--- js/bible_glyph_chapter_gen08.mjs
+++ js/bible_glyph_chapter_gen08.mjs
@@ -634,7 +634,7 @@ export function bible_glyph_chapter_gen08() {
           "the",
           "$earth",
           "last,",
-          "seedtime",
+          "$seed",
           "and",
           "harvest,",
           "cold",
