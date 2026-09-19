46428fd95026b10d1a77d1afef768f101f7b69e8
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:56 +0800

    bible_glyph_chapter_verse_word_replace 2SA12 3 cup; 1 $cup;

--- js/bible_glyph_chapter_2sa12.mjs
+++ js/bible_glyph_chapter_2sa12.mjs
@@ -96,7 +96,7 @@ export function bible_glyph_chapter_2sa12() {
           "drank",
           "from",
           "his",
-          "cup;",
+          "$cup;",
           "it",
           "slept",
           "in",
