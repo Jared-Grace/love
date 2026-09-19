cdadae26e9481178a7b1aad633b152297876dc4f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 12:03:03 +0800

    bible_glyph_chapter_verse_word_replace 2TH01 12 according 1 $pointing_down

--- js/bible_glyph_chapter_2th01.mjs
+++ js/bible_glyph_chapter_2th01.mjs
@@ -358,7 +358,7 @@ export function bible_glyph_chapter_2th01() {
           "you",
           "$tray_in",
           "$pointing_back,",
-          "according",
+          "$pointing_down",
           "to",
           "the",
           "$gift",
