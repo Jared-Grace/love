315f475ae986752ee49121589ba92f901f0372e0
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:59:03 +0800

    bible_glyph_chapter_verse_word_replace EXO02 8 mother. 1 $mother.

--- js/bible_glyph_chapter_exo02.mjs
+++ js/bible_glyph_chapter_exo02.mjs
@@ -243,7 +243,7 @@ export function bible_glyph_chapter_exo02() {
           "called",
           "the",
           "$child",
-          "mother.",
+          "$mother.",
         ],
       },
       {
