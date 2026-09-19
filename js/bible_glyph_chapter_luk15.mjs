7ffbf14a32e4c7303250546ad2f9bdee022739ff
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:05:40 +0800

    bible_glyph_chapter_verse_word_replace LUK15 15 fields 1 $field

--- js/bible_glyph_chapter_luk15.mjs
+++ js/bible_glyph_chapter_luk15.mjs
@@ -400,7 +400,7 @@ export function bible_glyph_chapter_luk15() {
         "$pointing_back",
         "into",
         "$pointing_back",
-        "fields",
+        "$field",
         "to",
         "feed",
         "pigs.",
