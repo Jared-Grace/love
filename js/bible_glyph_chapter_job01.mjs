7f6d174d5abc7479a6392f7a950d3dbed448f7c7
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:07:02 +0800

    bible_glyph_chapter_verse_word_replace JOB01 13 wine 1 $wine

--- js/bible_glyph_chapter_job01.mjs
+++ js/bible_glyph_chapter_job01.mjs
@@ -398,7 +398,7 @@ export function bible_glyph_chapter_job01() {
           "eating",
           "and",
           "drinking",
-          "wine",
+          "$wine",
           "in",
           "the",
           "$house",
