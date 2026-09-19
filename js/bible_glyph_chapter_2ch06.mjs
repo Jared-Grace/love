b33337e21d56bdc6e7ff4c5c9a17a79b21dcafcc
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:01:36 +0800

    bible_glyph_chapter_verse_word_replace 2CH06 20 night, 1 $night,

--- js/bible_glyph_chapter_2ch06.mjs
+++ js/bible_glyph_chapter_2ch06.mjs
@@ -677,7 +677,7 @@ export function bible_glyph_chapter_2ch06() {
           "$house",
           "day",
           "and",
-          "night,",
+          "$night,",
           "toward",
           "the",
           "$place",
