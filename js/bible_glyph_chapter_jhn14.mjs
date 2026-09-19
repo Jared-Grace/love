164bcdbdfdf3c5578965f56695cf2e0a3738f20d
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 09:55:50 +0800

    bible_glyph_chapter_verse_word_replace JHN14 2 place 1 $place

--- js/bible_glyph_chapter_jhn14.mjs
+++ js/bible_glyph_chapter_jhn14.mjs
@@ -62,7 +62,7 @@ export function bible_glyph_chapter_jhn14() {
           "to",
           "prepare",
           "a",
-          "place",
+          "$place",
           "for",
           "you?",
         ],
