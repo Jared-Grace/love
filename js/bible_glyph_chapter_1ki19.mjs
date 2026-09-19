c3249305da8635241ca14455e95d39c3d2927b4f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:04:28 +0800

    bible_glyph_chapter_verse_word_replace 1KI19 4 wilderness. 1 $wilderness.

--- js/bible_glyph_chapter_1ki19.mjs
+++ js/bible_glyph_chapter_1ki19.mjs
@@ -115,7 +115,7 @@ export function bible_glyph_chapter_1ki19() {
           "$road",
           "into",
           "the",
-          "wilderness.",
+          "$wilderness.",
           "He",
           "sat",
           "down",
