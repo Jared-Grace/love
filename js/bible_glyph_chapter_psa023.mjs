1216f14e96e7a82b2da201e83d89dee14d3365c3
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:08:54 +0800

    bible_glyph_chapter_verse_word_replace PSA023 5 cup 1 $cup

--- js/bible_glyph_chapter_psa023.mjs
+++ js/bible_glyph_chapter_psa023.mjs
@@ -125,7 +125,7 @@ export function bible_glyph_chapter_psa023() {
           "with",
           "oil;",
           "my",
-          "cup",
+          "$cup",
           "overflows.",
         ],
       },
