8613ae68eca2cfa0797fc05d6777e24fb3fdd77f
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 12:02:39 +0800

    bible_glyph_chapter_verse_word_replace 1JN02 7 from 1 $away

--- js/bible_glyph_chapter_1jn02.mjs
+++ js/bible_glyph_chapter_1jn02.mjs
@@ -190,7 +190,7 @@ export function bible_glyph_chapter_1jn02() {
           "you",
           "have",
           "had",
-          "from",
+          "$away",
           "the",
           "beginning.",
           "This",
