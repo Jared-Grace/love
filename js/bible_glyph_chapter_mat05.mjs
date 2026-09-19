0bb8b6905b8db369f5b03b0d382b4667ad0b7cf6
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:06:30 +0800

    bible_glyph_chapter_verse_word_replace MAT05 3 poor 1 $poor

--- js/bible_glyph_chapter_mat05.mjs
+++ js/bible_glyph_chapter_mat05.mjs
@@ -61,7 +61,7 @@ export function bible_glyph_chapter_mat05() {
           "$gift+smile",
           "are",
           "the",
-          "poor",
+          "$poor",
           "in",
           "$wind,",
           "$pointing",
