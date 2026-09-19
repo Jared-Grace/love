079d730f653433fd4c37db93e3a33b6245f40c63
JESUS rose to life <username@users.noreply.github.com>
2026-09-19 10:01:29 +0800

    bible_glyph_chapter_verse_word_replace JOB03 6 night 1 $night

--- js/bible_glyph_chapter_job03.mjs
+++ js/bible_glyph_chapter_job03.mjs
@@ -117,7 +117,7 @@ export function bible_glyph_chapter_job03() {
           "had",
           "$hand_receiving",
           "that",
-          "night",
+          "$night",
           "away.",
           "May",
           "it",
