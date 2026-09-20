a7ab05ba6c3a194b59fe38f48c30f3ac9c73526f
JESUS rose to life <username@users.noreply.github.com>
2026-09-20 22:32:04 +0800

    data_prompts_display

--- js/list_all_assert.mjs
+++ js/list_all_assert.mjs
@@ -1,9 +1,9 @@
 import { assert_json_get } from "./assert_json_get.mjs";
 import { list_all } from "./list_all.mjs";
-export function list_all_assert(identifiers, fn) {
-  let a = list_all(identifiers, fn);
+export function list_all_assert(list, fn) {
+  let a = list_all(list, fn);
   function lambda() {
-    return identifiers;
+    return list;
   }
   assert_json_get(a, lambda);
 }
