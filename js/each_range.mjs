96da82f98f72d07b4f462a0386d439e974f352f5
JESUS rose to life <username@users.noreply.github.com>
2026-09-21 15:07:40 +0800

    function_auto_path /home/j/a/repos/love/js/each_range.mjs

--- js/each_range.mjs
+++ js/each_range.mjs
@@ -1,6 +1,8 @@
+import { integer_is_assert } from "./integer_is_assert.mjs";
 import { range } from "./range.mjs";
 import { each } from "./each.mjs";
 export function each_range(count, lambda$i) {
+  integer_is_assert(i);
   let list = range(count);
   each(list, lambda$i);
 }
