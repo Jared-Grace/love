2486c990c1e3d3f1ce931b5f83021da275dd3c38
JESUS rose to life <username@users.noreply.github.com>
2026-09-22 18:49:28 +0800

    file_imports_repair js/git_history_paths_rename_rehearse.mjs

--- js/git_history_paths_rename_rehearse.mjs
+++ js/git_history_paths_rename_rehearse.mjs
@@ -1,3 +1,4 @@
+import { equal_assert_json } from "./equal_assert_json.mjs";
 import { git_filter_repo_asked_start } from "./git_filter_repo_asked_start.mjs";
 import { arguments_assert } from "./arguments_assert.mjs";
 import { text_pairs_comma_before_after } from "./text_pairs_comma_before_after.mjs";
