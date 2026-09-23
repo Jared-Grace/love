370f848f4d8f7ca6a8c74acaf2da648910e0b0cc
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 09:22:42 +0800

    ai

--- js/webpack_build_code_import.mjs
+++ js/webpack_build_code_import.mjs
@@ -4,14 +4,12 @@ import { property_get } from "./property_get.mjs";
 import { repo_path_combine } from "./repo_path_combine.mjs";
 import { js_code_import_single } from "./js_code_import_single.mjs";
 import { js_code_string } from "./js_code_string.mjs";
-import { folder_previous_2_join } from "./folder_previous_2_join.mjs";
 export async function webpack_build_code_import(f_name) {
   let search = await function_name_to_path_search(f_name);
   let repo_name = property_get(search, "repo_name");
   let path = function_name_to_path_relative(f_name);
   let with_repo = repo_path_combine(repo_name, path);
-  let joined = folder_previous_2_join(with_repo);
-  let code_string = js_code_string(joined);
+  let code_string = js_code_string(with_repo);
   let i = js_code_import_single(f_name, code_string);
   return i;
 }
