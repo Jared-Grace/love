4aa37093497d231150fc0080cc9a80ec19fe4e80
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:57:28 +0800

    function_fn_name_declarations_inline qa_gate_remove

--- js/qa_gate_remove.mjs
+++ js/qa_gate_remove.mjs
@@ -16,7 +16,6 @@ export async function qa_gate_remove(gate_f_name) {
     let selects = list_ensure(found);
     js_array_identifier_remove(ast, selects, gate_f_name);
   }
-  let f_name = fn_name("qa_gates");
-  let output = await function_transform_auto(f_name, lambda);
+  let output = await function_transform_auto(fn_name("qa_gates"), lambda);
   return output;
 }
