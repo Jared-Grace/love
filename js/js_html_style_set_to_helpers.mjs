1cf022e2495bdb3f33d200ba88d03673fc22df59
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:48:28 +0800

    function_fn_name_declarations_inline js_html_style_set_to_helpers

--- js/js_html_style_set_to_helpers.mjs
+++ js/js_html_style_set_to_helpers.mjs
@@ -3,9 +3,8 @@ import { js_visit_calls_named_nodes } from "./js_visit_calls_named_nodes.mjs";
 import { js_html_style_set_to_helper_node } from "./js_html_style_set_to_helper_node.mjs";
 export function js_html_style_set_to_helpers(ast, helpers) {
   "Rewrite every style property set by name in this tree into the named helper for it.";
-  let f_name = fn_name("html_style_set");
   function lambda(node) {
     js_html_style_set_to_helper_node(node, helpers);
   }
-  js_visit_calls_named_nodes(ast, f_name, lambda);
+  js_visit_calls_named_nodes(ast, fn_name("html_style_set"), lambda);
 }
