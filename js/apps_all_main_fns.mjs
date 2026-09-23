359db4953b231b06680b5eac062f83e67d000fa7
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:42:37 +0800

    function_fn_name_declarations_inline apps_all_main_fns

--- js/apps_all_main_fns.mjs
+++ js/apps_all_main_fns.mjs
@@ -1,72 +1,39 @@
 import { fn_name } from "./fn_name.mjs";
 export function apps_all_main_fns() {
-  let f_name = fn_name("app_a");
-  let f_name2 = fn_name("app_apps_all");
-  let f_name3 = fn_name("app_autopray");
-  let f_name4 = fn_name("app_bible");
-  let f_name5 = fn_name("app_calendar");
-  let f_name6 = fn_name("app_calendar_paste");
-  let f_name7 = fn_name("app_ceb_bible");
-  let f_name8 = fn_name("app_code");
-  let f_name9 = fn_name("app_designs_universal");
-  let f_name10 = fn_name("app_emoji_bible");
-  let f_name11 = fn_name("app_en_learn_bible");
-  let f_name12 = fn_name("app_examples");
-  let f_name13 = fn_name("app_g");
-  let f_name14 = fn_name("app_g_bible");
-  let f_name15 = fn_name("app_g_bless");
-  let f_name16 = fn_name("app_g_hero");
-  let f_name17 = fn_name("app_g_verify");
-  let f_name18 = fn_name("app_index");
-  let f_name19 = fn_name("app_karate");
-  let f_name20 = fn_name("app_message");
-  let f_name21 = fn_name("app_music");
-  let f_name22 = fn_name("app_next");
-  let f_name23 = fn_name("app_original_bible");
-  let f_name24 = fn_name("app_privacy_policy");
-  let f_name25 = fn_name("app_receipts");
-  let f_name26 = fn_name("app_replace");
-  let f_name27 = fn_name("app_reply");
-  let f_name28 = fn_name("app_reply_local");
-  let f_name29 = fn_name("app_sandbox");
-  let f_name30 = fn_name("app_search");
-  let f_name31 = fn_name("app_supper");
-  let f_name32 = fn_name("app_supper_tl");
-  let f_name33 = fn_name("app_verses");
   let r = [
-    f_name,
-    f_name2,
-    f_name3,
-    f_name4,
-    f_name5,
-    f_name6,
-    f_name7,
-    f_name8,
-    f_name9,
-    f_name10,
-    f_name11,
-    f_name12,
-    f_name13,
-    f_name14,
-    f_name15,
-    f_name16,
-    f_name17,
-    f_name18,
-    f_name19,
-    f_name20,
-    f_name21,
-    f_name22,
-    f_name23,
-    f_name24,
-    f_name25,
-    f_name26,
-    f_name27,
-    f_name28,
-    f_name29,
-    f_name30,
-    f_name31,
-    f_name32,
-    f_name33,
+    fn_name("app_a"),
+    fn_name("app_apps_all"),
+    fn_name("app_autopray"),
+    fn_name("app_bible"),
+    fn_name("app_calendar"),
+    fn_name("app_calendar_paste"),
+    fn_name("app_ceb_bible"),
+    fn_name("app_code"),
+    fn_name("app_designs_universal"),
+    fn_name("app_emoji_bible"),
+    fn_name("app_en_learn_bible"),
+    fn_name("app_examples"),
+    fn_name("app_g"),
+    fn_name("app_g_bible"),
+    fn_name("app_g_bless"),
+    fn_name("app_g_hero"),
+    fn_name("app_g_verify"),
+    fn_name("app_index"),
+    fn_name("app_karate"),
+    fn_name("app_message"),
+    fn_name("app_music"),
+    fn_name("app_next"),
+    fn_name("app_original_bible"),
+    fn_name("app_privacy_policy"),
+    fn_name("app_receipts"),
+    fn_name("app_replace"),
+    fn_name("app_reply"),
+    fn_name("app_reply_local"),
+    fn_name("app_sandbox"),
+    fn_name("app_search"),
+    fn_name("app_supper"),
+    fn_name("app_supper_tl"),
+    fn_name("app_verses"),
   ];
   return r;
 }
