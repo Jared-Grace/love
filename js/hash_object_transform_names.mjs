665027a9179db0fbaaa2325ea357b7eab4bc835e
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:47:56 +0800

    function_fn_name_declarations_inline hash_object_transform_names

--- js/hash_object_transform_names.mjs
+++ js/hash_object_transform_names.mjs
@@ -4,8 +4,9 @@ export function hash_object_transform_names() {
   "The names of the functions that hand the address of a page to a function you wrote, so it can be changed.";
   "That function's first parameter is the address under whatever name it chose, which is one of the ways a reading comes by a name holding an address.";
   arguments_assert(arguments, 0);
-  let f_name = fn_name("html_hash_transform");
-  let f_name2 = fn_name("html_hash_transform_reload");
-  let names = [f_name, f_name2];
+  let names = [
+    fn_name("html_hash_transform"),
+    fn_name("html_hash_transform_reload"),
+  ];
   return names;
 }
