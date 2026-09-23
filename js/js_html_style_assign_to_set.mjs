1a83ee0799e8e238e0c7871233aeb4eece4a6fd5
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:48:26 +0800

    function_fn_name_declarations_inline js_html_style_assign_to_set

--- js/js_html_style_assign_to_set.mjs
+++ js/js_html_style_assign_to_set.mjs
@@ -2,6 +2,9 @@ import { fn_name } from "./fn_name.mjs";
 import { js_visit_calls_named_nodes } from "./js_visit_calls_named_nodes.mjs";
 import { js_html_style_assign_to_set_node } from "./js_html_style_assign_to_set_node.mjs";
 export function js_html_style_assign_to_set(ast) {
-  let f_name = fn_name("html_style_assign");
-  js_visit_calls_named_nodes(ast, f_name, js_html_style_assign_to_set_node);
+  js_visit_calls_named_nodes(
+    ast,
+    fn_name("html_style_assign"),
+    js_html_style_assign_to_set_node,
+  );
 }
