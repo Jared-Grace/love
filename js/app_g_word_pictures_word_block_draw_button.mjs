8ebd40915d68a1763eab86c5ba82265e5947a869
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:41:46 +0800

    function_fn_name_declarations_inline app_g_word_pictures_word_block_draw_button

--- js/app_g_word_pictures_word_block_draw_button.mjs
+++ js/app_g_word_pictures_word_block_draw_button.mjs
@@ -33,8 +33,7 @@ export function app_g_word_pictures_word_block_draw_button(
     ]);
     status_working(combined);
     try {
-      let f_name = fn_name("word_picture_draw");
-      await app_shared_api_named(f_name, [word]);
+      await app_shared_api_named(fn_name("word_picture_draw"), [word]);
       let combined5 = text_combine_multiple([
         "drew another attempt for ",
         word,
