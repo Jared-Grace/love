06f7ecb7c4e611cee883c88cf75bc929f5f22b1c
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:42:05 +0800

    function_fn_name_declarations_inline app_replace_rule_set_add

--- js/app_replace_rule_set_add.mjs
+++ js/app_replace_rule_set_add.mjs
@@ -2,7 +2,6 @@ import { fn_name } from "./fn_name.mjs";
 import { function_copy_list_add } from "./function_copy_list_add.mjs";
 import { app_replace_rule_sets_fns } from "./app_replace_rule_sets_fns.mjs";
 export async function app_replace_rule_set_add() {
-  let base = fn_name("app_replace_rule_set_new");
   let fns_list = app_replace_rule_sets_fns;
-  await function_copy_list_add(base, fns_list);
+  await function_copy_list_add(fn_name("app_replace_rule_set_new"), fns_list);
 }
