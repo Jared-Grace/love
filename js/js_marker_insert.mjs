ccb37de45568c0f2185a11c424cb750d066d96d5
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:48:34 +0800

    function_fn_name_declarations_inline js_marker_insert

--- js/js_marker_insert.mjs
+++ js/js_marker_insert.mjs
@@ -3,7 +3,6 @@ import { js_call_statement_insert } from "./js_call_statement_insert.mjs";
 import { js_code_string } from "./js_code_string.mjs";
 export async function js_marker_insert(name, list, index) {
   let code_string = await js_code_string(name);
-  let f_name = fn_name("marker");
   let args_code = [code_string];
-  js_call_statement_insert(f_name, args_code, list, index);
+  js_call_statement_insert(fn_name("marker"), args_code, list, index);
 }
