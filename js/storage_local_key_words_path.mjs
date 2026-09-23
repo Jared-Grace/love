e3aa37f4a5d9257e111bf2b1e42a86b42b5dd9fa
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:58:22 +0800

    function_fn_name_declarations_inline storage_local_key_words_path

--- js/storage_local_key_words_path.mjs
+++ js/storage_local_key_words_path.mjs
@@ -3,10 +3,9 @@ import { fn_name } from "./fn_name.mjs";
 import { text_combine_multiple } from "./text_combine_multiple.mjs";
 export function storage_local_key_words_path() {
   "Where the record of the words that have escaped into browser storage keys is kept.";
-  let f_name = fn_name("storage_local_key_words");
   let p = text_combine_multiple([
     "data/given/frozen/",
-    f_name,
+    fn_name("storage_local_key_words"),
     json_extension(),
   ]);
   return p;
