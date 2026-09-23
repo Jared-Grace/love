6d6d88d776579dd163a5e8822393afc001309eaf
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:41:41 +0800

    function_fn_name_declarations_inline app_g_hero_latest

--- js/app_g_hero_latest.mjs
+++ js/app_g_hero_latest.mjs
@@ -3,6 +3,5 @@ import { app_shared_main_latest } from "./app_shared_main_latest.mjs";
 import { fn_name } from "./fn_name.mjs";
 export async function app_g_hero_latest() {
   let firebase_name_value = firebase_project_name_jg();
-  let f_name = fn_name("app_g_hero");
-  await app_shared_main_latest(f_name, firebase_name_value);
+  await app_shared_main_latest(fn_name("app_g_hero"), firebase_name_value);
 }
