0c88dbf24d2bd9b62b9f7bab75f7982550df7d94
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:48:07 +0800

    function_fn_name_declarations_inline js_assert_json_get_call_point_at_record

--- js/js_assert_json_get_call_point_at_record.mjs
+++ js/js_assert_json_get_call_point_at_record.mjs
@@ -6,8 +6,7 @@ import { property_list_get } from "./property_list_get.mjs";
 export function js_assert_json_get_call_point_at_record(call, record_name) {
   arguments_assert(arguments, 2);
   let callee = property_get(call, "callee");
-  let value = fn_name("assert_json");
-  property_set(callee, "name", value);
+  property_set(callee, "name", fn_name("assert_json"));
   let second = property_list_get(call, "arguments", 1);
   property_set(second, "name", record_name);
 }
