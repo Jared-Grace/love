2099034d7a947161aa0a2837a07d49269bbd177d
JESUS rose to life <username@users.noreply.github.com>
2026-09-23 15:49:16 +0800

    function_fn_name_declarations_inline lyric_video_picture_notes

--- js/lyric_video_picture_notes.mjs
+++ js/lyric_video_picture_notes.mjs
@@ -5,7 +5,6 @@ export async function lyric_video_picture_notes(key) {
   "$plain key";
   "Every note standing against one background picture of a lyric video, in the order they were made.";
   arguments_assert(arguments, 1);
-  let store = fn_name("lyric_video_picture_note_add");
-  let notes = await notes_read(store, key);
+  let notes = await notes_read(fn_name("lyric_video_picture_note_add"), key);
   return notes;
 }
