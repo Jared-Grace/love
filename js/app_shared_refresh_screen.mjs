b38dd16436fa56c8fd9232fe4b1d91793434f9a3
JESUS rose to life <username@users.noreply.github.com>
2026-09-24 16:12:31 +0800

    ai

--- js/app_shared_refresh_screen.mjs
+++ js/app_shared_refresh_screen.mjs
@@ -1,6 +1,5 @@
-import { app_shared_history_push_if } from "./app_shared_history_push_if.mjs";
-import { app_shared_refresh } from "./app_shared_refresh.mjs";
-import { app_shared_screen_stored_set_context } from "./app_shared_screen_stored_set_context.mjs";
+import { app_shared_refresh_screen_write } from "./app_shared_refresh_screen_write.mjs";
+import { noop } from "./noop.mjs";
 export async function app_shared_refresh_screen(context, without) {
   await app_shared_refresh_screen_write(context, without, noop);
 }
