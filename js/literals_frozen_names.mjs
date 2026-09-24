103eea8d35b62a2ae25f223671ae64bb5ad8186d
JESUS rose to life <username@users.noreply.github.com>
2026-09-24 20:34:04 +0800

    literals_frozen_name_add app_receipts_store

--- js/literals_frozen_names.mjs
+++ js/literals_frozen_names.mjs
@@ -90,6 +90,7 @@ export function literals_frozen_names() {
     fn_name("app_replace_rule_set_hash_key"),
     fn_name("app_replace_goal_hash_key"),
     fn_name("app_receipts_database_name"),
+    fn_name("app_receipts_store"),
   ];
   return names;
 }
