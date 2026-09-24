5052fa566b8e3e6672b69ccd562b12fcb122722e
JESUS rose to life <username@users.noreply.github.com>
2026-09-24 18:48:33 +0800

    function_parallel_mark list_copy

--- js/list_copy.mjs
+++ js/list_copy.mjs
@@ -1,4 +1,6 @@
+import { function_duplicate_kind_parallel } from "./function_duplicate_kind_parallel.mjs";
 export function list_copy(original) {
+  function_duplicate_kind_parallel();
   let copy = [...original];
   return copy;
 }
