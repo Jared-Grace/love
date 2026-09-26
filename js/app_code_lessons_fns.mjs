811f93a40b45e424c4ef683f75349eeda93c1f35
JESUS rose to life <username@users.noreply.github.com>
2026-09-26 19:46:03 +0800

    function_select_apply_args_auto app_code_lessons_fns js_find_declaration_named fns js_array_identifier_move app_code_lesson_statement_name_or app_code_lesson_statement_name_not

--- js/app_code_lessons_fns.mjs
+++ js/app_code_lessons_fns.mjs
@@ -353,8 +353,8 @@ export function app_code_lessons_fns() {
     app_code_lesson_statement_name_equal,
     app_code_lesson_statement_name_not_equal,
     app_code_lesson_statement_name_not,
-    app_code_lesson_statement_name_and,
     app_code_lesson_statement_name_or,
+    app_code_lesson_statement_name_and,
   ];
   return fns;
 }
